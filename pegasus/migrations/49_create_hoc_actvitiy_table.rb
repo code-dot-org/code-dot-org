1abd9c86fc4131eb9d482d5cae0258ac7e996bfa
Geoffrey Elliott <geoffrey@code.org>
2014-09-24 11:23:04 -0700

    Fix a column name.

--- pegasus/migrations/49_create_hoc_actvitiy_table.rb
+++ pegasus/migrations/49_create_hoc_actvitiy_table.rb
@@ -1,29 +1,7 @@
 Sequel.migration do
   up do
-    create_table(:hoc_activity, charset:'utf8') do
-      primary_key :id
-      String :session, unique:true, null:false, index:true, size:50
-      String :referer, size:50
-      String :company, index:true, size:50
-      String :tutorial, index:true, null:false, size:50
-
-      DateTime :started_at
-      String :started_ip, size:50
-      DateTime :pixel_started_at
-      String :pixel_started_ip, size:50
-      DateTime :pixel_finsihed_at
-      String :pixel_finished_ip, size:50
-      DateTime :finished_at
-      String :finished_ip, size:50
-
-      String :country_code, index:true, size:2
-      String :state_code, index:true, size:2 # e.g.
-      String :city, index:true, size:50
-      String :location, size:50 # "lat,long"
-    end
   end
 
   down do
-    drop_table(:hoc_activity)
   end
 end
