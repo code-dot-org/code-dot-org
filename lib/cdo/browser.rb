8735a616a826ab667e234e98cf156f0b8f3f0b8a
Josh Lory <josh.lory@code.org>
2014-09-26 16:38:21 -0700

    Fix activity-monitor and lib/cdo extension

--- lib/cdo/browser.rb
+++ lib/cdo/browser.rb
@@ -1,30 +1,32 @@
 require 'browser'
 
 class Browser
-  # Change the browser.modern? rules to use a conservative blacklist instead of a whitelist
-  self.modern_rules.clear
+  # Browsers we don't support and never plan to support fully
+  def unsupported?
+    return ie? && version.to_i < 8
+  end
 
-  self.modern_rules << ->(b) {
-    ver = b.version.to_i
+  # Returns false for browsers known to have issues on studio.code.org
+  def cdo_modern?
+    ver = version.to_i
 
     # IE 9+
-    return false if b.ie? && ver < 9
+    return false if ie? && ver < 9
 
     # Firefox 10+
-    return false if b.firefox? && ver < 10
+    return false if firefox? && ver < 10
 
     # Opera 12+
-    return false if b.opera? && ver < 12
+    return false if opera? && ver < 12
 
     # Safari 5.1.x+
-    return false if b.safari && (ver < 5 || b.full_version =~ /5\.0/)
+    return false if safari? && (ver < 5 || full_version =~ /5\.0/)
 
     # Default: no browser support warning
     true
-  }
-
-  # Browsers we don't support and never plan to support fully
-  def self.unsupported?
-    return self.ie? && self.version.to_i < 8
   end
 end
+
+# Change the browser.modern? rules to use a conservative blacklist instead of a whitelist
+Browser.modern_rules.clear
+Browser.modern_rules << ->(b) { b.cdo_modern? }
