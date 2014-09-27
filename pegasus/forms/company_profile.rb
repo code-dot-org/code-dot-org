c78dccf397cb5a95a8dbed9fc51d4be9a038dc5e
David Bailey <dsb123@gmail.com>
2014-09-26 23:24:40 -0700

    preserve logo field when other fields are changed.

--- pegasus/forms/company_profile.rb
+++ pegasus/forms/company_profile.rb
@@ -9,9 +9,10 @@ class CompanyProfile
       result[:intro_top_copy_t] = nil_if_empty stripped data[:intro_top_copy_t]
       result[:intro_bottom_copy_t] = nil_if_empty stripped data[:intro_bottom_copy_t]
       result[:additional_actions_copy_t] = nil_if_empty stripped data[:additional_actions_copy_t]
+      result[:logo_path_s] = nil_if_empty stripped data[:logo_path_s]
 
       if FormError.detect_errors(result).empty?
-        result[:logo_path_s] = nil_if_empty uploaded_file data[:logo_file]
+        result[:logo_path_s] = default_if_empty uploaded_file(data[:logo_file]), result[:logo_path_s]
       end
     end
   end
