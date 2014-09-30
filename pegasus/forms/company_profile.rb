66d04517849326f04b0d3e57fb660433088b82e0
David Bailey <dsb123@gmail.com>
2014-09-29 19:00:20 -0700

    Employee Engagement: add optional 'Share on G+' button

--- pegasus/forms/company_profile.rb
+++ pegasus/forms/company_profile.rb
@@ -10,6 +10,7 @@ class CompanyProfile
       result[:intro_bottom_copy_t] = nil_if_empty stripped data[:intro_bottom_copy_t]
       result[:additional_actions_copy_t] = nil_if_empty stripped data[:additional_actions_copy_t]
       result[:logo_path_s] = nil_if_empty stripped data[:logo_path_s]
+      result[:gplus_b] = nil_if_empty data[:gplus_b]
 
       if FormError.detect_errors(result).empty?
         result[:logo_path_s] = default_if_empty uploaded_file(data[:logo_file]), result[:logo_path_s]
