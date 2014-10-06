e85ce07c7821432377d4f83ac063a888c0ce77f9
Continuous Integration <dev@code.org>
2014-10-06 19:14:08 +0000

    Automatically built.
    commit 059025056303944cde65112ad9f959e9b038c1f1
    Merge: 1224b2f a4382d2
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Oct 6 11:57:27 2014 -0700
    
        Merge pull request #111 from code-dot-org/MSM_blocks
    
        Functional blocks
    
    commit a4382d24f2024cbd71eeea084b25d2dc412d3a6f
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Oct 6 11:56:44 2014 -0700
    
        code review feedback
    
    commit 1224b2f838fa16a6ab4c06594940369de54ba7af
    Merge: e871129 af32144
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Oct 6 11:11:15 2014 -0700
    
        Merge pull request #117 from code-dot-org/revertImages
    
        Revert "Merge pull request #105 from code-dot-org/compress_images"
    
    commit af321448d0403c2e6bb1810a8f3a8db246ce3772
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Oct 6 11:07:24 2014 -0700
    
        Revert "Merge pull request #105 from code-dot-org/compress_images"
    
        This reverts commit a4c19d23f1c8b06378d6c9a82f4306d3435b3765, reversing
        changes made to 901993640a817097a256f1a2337b618f4ca18214.
    
    commit e871129f08811cf8f21df0da406e46998c80b622
    Author: Continuous Integration <dev@code.org>
    Date:   Mon Oct 6 18:02:47 2014 +0000
    
        Automatically built.
    
        commit c95860826c110606a403660932c763b41bc60719
        Merge: 662bf35 61d9fdd
        Author: Josh Lory <josh.lory@code.org>
        Date:   Mon Oct 6 10:48:14 2014 -0700
    
            Merge pull request #116 from code-dot-org/callout-styles
    
            Move callout styles to application.css so they're usable on non-blockly page
    
        commit 61d9fdd8c4061add5aab70890983dce59efa3c6f
        Author: Josh Lory <josh.lory@code.org>
        Date:   Mon Oct 6 10:47:00 2014 -0700
    
            Move callout styles to application.css so they're usable on non-blockly pages
    
    commit c95860826c110606a403660932c763b41bc60719
    Merge: 662bf35 61d9fdd
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon Oct 6 10:48:14 2014 -0700
    
        Merge pull request #116 from code-dot-org/callout-styles
    
        Move callout styles to application.css so they're usable on non-blockly page
    
    commit 61d9fdd8c4061add5aab70890983dce59efa3c6f
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon Oct 6 10:47:00 2014 -0700
    
        Move callout styles to application.css so they're usable on non-blockly pages
    
    commit 80270aa018d1f672836735f164fabe56b758c16f
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Oct 6 09:20:26 2014 -0700
    
        clean up some comments
    
    commit ae5d2279648595805a853dc65998147911e19cc5
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Fri Oct 3 15:58:03 2014 -0700
    
        return turtle level to original state

--- dashboard/public/blockly-package/js/en_ploc/turtle.js
+++ dashboard/public/blockly-package/js/en_ploc/turtle.js
@@ -8216,6 +8216,7 @@ function installDrawLowerWave(blockly, generator, gensym) {
 
 exports.install = function(blockly, generator, gensym) {
  installPlus(blockly, generator, gensym);
+ installMathNumber(blockly, generator, gensym);
 };
 
 function installPlus(blockly, generator, gensym) {
@@ -8227,18 +8228,32 @@ function installPlus(blockly, generator, gensym) {
       this.appendDummyInput()
           .appendTitle('+')
           .setAlign(Blockly.ALIGN_CENTRE);
-      this.appendValueInput('VALUE')
-          .setCheck('Number');
-      this.appendValueInput('VALUE')
-          .setCheck('Number')
+      this.appendFunctionalInput('VALUE1');
+      this.appendFunctionalInput('VALUE2')
           .setInline(true);
 
-      this.setPreviousStatement(true);
-      this.setNextStatement(true);
+      this.setFunctionalOutput(true, 'Number');
     }
   };
 
   generator.functional_plus = function() {
+    return '';
+  };
+}
+
+function installMathNumber(blockly, generator, gensym) {
+  blockly.Blocks.functional_math_number = {
+    // Numeric value.
+    init: function() {
+      this.setHSV(258, 0.35, 0.62);
+      this.appendDummyInput()
+          .appendTitle(new Blockly.FieldTextInput('0',
+          Blockly.FieldTextInput.numberValidator), 'NUM');
+      this.setFunctionalOutput(true, 'Number');
+    }
+  };
+
+  generator.functional_math_number = function() {
     return '';
   };
 }
