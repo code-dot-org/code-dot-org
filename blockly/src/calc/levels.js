a6ee79c00dec8d2456e1384f05533e426c66f203
Brent Van Minnen <bjvanminnen@gmail.com>
2014-10-06 16:02:23 -0700

    add example builtin level to 2014 script

--- blockly/src/calc/levels.js
+++ blockly/src/calc/levels.js
@@ -5,6 +5,20 @@ var blockUtils = require('../block_utils');
  * Information about level-specific requirements.
  */
 module.exports = {
+  'example1': {
+    answer: '',
+    ideal: 4,
+    toolbox: blockUtils.createToolbox(
+      blockUtils.blockOfType('functional_draw') +
+      blockUtils.blockOfType('functional_plus') +
+      blockUtils.blockOfType('functional_minus') +
+      blockUtils.blockOfType('functional_times') +
+      blockUtils.blockOfType('functional_dividedby')),
+    startBlocks: '',
+    requiredBlocks: '',
+    freePlay: false
+  },
+
   'custom': {
     answer: '',
     ideal: 4,
