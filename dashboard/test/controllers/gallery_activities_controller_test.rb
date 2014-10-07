da9b7dec493523528662f1e49d5a7994357b351e
Laurel Fan <laurel.fan@gmail.com>
2014-10-07 16:50:47 -0700

    actually set artist/playlab-ness on gallery activities

--- dashboard/test/controllers/gallery_activities_controller_test.rb
+++ dashboard/test/controllers/gallery_activities_controller_test.rb
@@ -3,12 +3,13 @@ require 'test_helper'
 class GalleryActivitiesControllerTest < ActionController::TestCase 
   setup do
     @user = create(:user)
-    @activity = create(:activity, user: @user)
+    @activity = create(:activity, user: @user, level: create(:level, game: Game.find_by_app(Game::ARTIST)))
     @gallery_activity = create(:gallery_activity, user: @user, activity: @activity, autosaved: false)
 
-    @playlab_gallery_activity = create(:gallery_activity, user: @user, app: Game::PLAYLAB, autosaved: false)
+    @playlab_activity = create(:activity, user: @user, level: create(:level, game: Game.find_by_app(Game::PLAYLAB)))
+    @playlab_gallery_activity = create(:gallery_activity, user: @user, activity: @playlab_activity, autosaved: false)
 
-    @new_activity = create(:activity, user: @user)
+    @new_activity = create(:activity, user: @user, level: create(:level, game: Game.find_by_app(Game::PLAYLAB)))
 
     @autosaved_gallery_activity = create(:gallery_activity, user: @user, autosaved: true)
   end
@@ -70,7 +71,8 @@ class GalleryActivitiesControllerTest < ActionController::TestCase
 
     assert_equal @user, assigns(:gallery_activity).user
     assert_equal @new_activity, assigns(:gallery_activity).activity
-    assert !assigns(:gallery_activity).autosaved
+    assert_equal false, assigns(:gallery_activity).autosaved
+    assert_equal 'studio', assigns(:gallery_activity).app
 
     assert_response :created
   end
@@ -85,6 +87,7 @@ class GalleryActivitiesControllerTest < ActionController::TestCase
     end
 
     assert_equal gallery_activity, assigns(:gallery_activity)
+    assert_equal false, assigns(:gallery_activity).autosaved
 
     assert_response :created
   end
