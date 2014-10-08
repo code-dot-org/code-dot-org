a680def1dfbd3d346ff6ee23beca80f3fa09c310
Andre Stackhouse <CaptainStack@outlook.com>
2014-10-07 17:14:31 -0700

    Removed double '+' sign.

--- pegasus/routes/indiegogo_apis.rb
+++ pegasus/routes/indiegogo_apis.rb
@@ -4,6 +4,6 @@ get '/v2/indiegogo_campaigns' do
 end
 
 get '/v2/indiegogo_contributions' do
-  uri = URI('https://api.indiegogo.com/1/campaigns/964232/contributions.json?api_token=' + + CDO.indiegogo_api_token)
+  uri = URI('https://api.indiegogo.com/1/campaigns/964232/contributions.json?api_token=' + CDO.indiegogo_api_token)
   Net::HTTP.get(uri) # => String
 end
