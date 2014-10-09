0d3d6a1a5f0c98bc26a34fe7b3e889068a43b228
Continuous Integration <dev@code.org>
2014-10-09 04:34:59 +0000

    Email blast parts.

--- lib/cdo/poste.rb
+++ lib/cdo/poste.rb
@@ -4,6 +4,7 @@ require 'mail'
 module Poste2
 
   @@url_cache = {}
+  @@message_id_cache = {}
 
   def self.email_address?(address)
     begin
@@ -68,18 +69,50 @@ module Poste2
     {id:contact[:id], email:address, name:name, ip_address:ip_address}
   end
 
+  def self.ensure_recipient(address, params={})
+    address = address.to_s.strip.downcase
+    raise ArgumentError, 'Invalid email address' unless email_address?(address)
+
+    name = params[:name].strip if params[:name]
+    ip_address = params[:ip_address]
+    now = DateTime.now
+
+    contacts = PEGASUS_DB[:contacts]
+
+    contact = contacts.where(email:address).first
+    unless contact
+      puts "create"
+      contacts.insert({}.tap do |contact|
+        contact[:email] = address
+        contact[:name] = name if name
+        contact[:created_at] = contact[:created_on] = now
+        contact[:created_ip] = ip_address
+        contact[:updated_at] = contact[:updated_on] = now
+        contact[:updated_ip] = ip_address
+      end)
+      contact = contacts.where(email:address).first
+    end
+
+    {id:contact[:id], email:address, name:name, ip_address:ip_address}
+  end
+
   def self.send_message(message_name, recipient, params)
     raise ArgumentError, 'No recipient' unless recipient && recipient[:id] && recipient[:email] && recipient[:ip_address]
 
-    message = DB[:poste_messages].where(name:message_name.to_s.strip).first
-    raise ArgumentError, "No #{message_name} message found." unless message
+    message_name = message_name.to_s.strip
+    message_id = @@message_id_cache[message_name]
+    unless message_id
+      message = DB[:poste_messages].where(name:message_name.to_s.strip).first
+      raise ArgumentError, "No #{message_name} message found." unless message
+      message_id = @@message_id_cache[message_name] = message[:id]
+    end
 
     DB[:poste_deliveries].insert({
       created_at:DateTime.now,
       created_ip:recipient[:ip_address],
       contact_id:recipient[:id],
       contact_email:recipient[:email],
-      message_id:message[:id],
+      message_id:message_id,
       params:(params||{}).to_json,
     })
   end
