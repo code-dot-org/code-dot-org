207609c79c44968b1eaf339ac73690d0aab07090
Josh Lory <josh.lory@code.org>
2014-10-14 12:13:50 -0700

    Code review feedback + unit tests

--- lib/cdo/user_agent_parser.rb
+++ lib/cdo/user_agent_parser.rb
@@ -2,11 +2,31 @@ require 'user_agent_parser'
 
 class UserAgentParser::UserAgent
   def mobile?
-    return name =~ /Mobile/
+    name =~ /Mobile/
+  end
+
+  def chrome?
+    name == 'Chrome'
+  end
+
+  def safari?
+    name == 'Safari'
+  end
+
+  def ie?
+    name == 'IE'
+  end
+
+  def firefox?
+    name == 'Firefox'
+  end
+
+  def opera?
+    name == 'Opera'
   end
 
   def cdo_unsupported?
-    return name == 'IE' && version && version.major < 8
+    return ie? && version && version.major < 8
   end
 
   def cdo_partially_supported?
@@ -14,16 +34,16 @@ class UserAgentParser::UserAgent
     ver = version.major
 
     # IE 9+
-    return true if name == 'IE' && ver < 9
+    return true if ie? && ver < 9
 
     # Firefox 10+
-    return true if name == 'Firefox' && ver < 10
+    return true if firefox? && ver < 10
 
     # Opera 12+
-    return true if name == 'Opera' && ver < 12
+    return true if opera? && ver < 12
 
     # Safari 5.1.x+
-    return true if name == 'Safari' && (ver < 5 || version.to_s =~ /^5\.0/)
+    return true if safari? && (ver < 5 || version.to_s =~ /^5\.0/)
 
     # Default: no browser support warning
     false
