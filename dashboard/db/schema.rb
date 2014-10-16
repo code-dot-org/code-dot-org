65ada6ed795daa40ac6970454f324d66de784b3c
Laurel Fan <laurel.fan@gmail.com>
2014-10-16 14:00:09 -0700

    migration to generate usernames for existing users that don't have one

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014222914) do
+ActiveRecord::Schema.define(version: 20141016193651) do
 
   create_table "activities", force: true do |t|
     t.integer  "user_id"
