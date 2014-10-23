a2f1067388ba01464b09b3a7e2e6a505768f56f8
Continuous Integration <dev@code.org>
2014-10-23 21:23:50 +0000

    Automatically built.
    commit b8c35e3d7fe1398bbaf3bffedceeb0b818a7a7f1
    Merge: 28dd550 0da4e2a
    Author: Brendan Reville <brendan@code.org>
    Date:   Thu Oct 23 14:16:38 2014 -0700
    
        Merge branch 'hoc' of github.com:code-dot-org/code-dot-private into hoc
    
    commit 28dd550e6a4310aac43345c93d3a19b9c047532e
    Author: Brendan Reville <brendan@code.org>
    Date:   Thu Oct 23 14:16:09 2014 -0700
    
        Separate skins for Anna and Elsa now.  Latest spritesheets and images used.

--- dashboard/public/blockly-package/js/en_us/turtle.js
+++ dashboard/public/blockly-package/js/en_us/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:def68e3aba043ad95dfcf7bf73e578eff2500e170d21a10f5460dab94fd4552d
-size 531441
+oid sha256:c78bd07895f52c358b44f04adb8a2d77d69c31024415bafcb111c53a2febe44c
+size 531998
