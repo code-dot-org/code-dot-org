dd2f57059f064b1abd1d4fc70e9e5c46ad96ce27
Brent Van Minnen <bjvanminnen@gmail.com>
2014-10-23 17:19:38 -0700

    place image

--- blockly/src/eval/levels.js
+++ blockly/src/eval/levels.js
@@ -6,7 +6,7 @@ var blockUtils = require('../block_utils');
  */
 module.exports = {
   'eval1': {
-    solutionBlocks: '<block type="functional_string"><title name="VAL">answer string</title></block>',
+    solutionBlocks: '<block type="functional_circle" inline="false"><functional_input name="COLOR"><block type="functional_string"><title name="VAL">red</title></block></functional_input><functional_input name="SIZE"><block type="functional_math_number"><title name="NUM">50</title></block></functional_input></block>',
     ideal: Infinity,
     toolbox: blockUtils.createToolbox(
       blockUtils.blockOfType('functional_plus') +
@@ -15,7 +15,8 @@ module.exports = {
       blockUtils.blockOfType('functional_dividedby') +
       blockUtils.blockOfType('functional_math_number') +
       blockUtils.blockOfType('functional_string') +
-      blockUtils.blockOfType('functional_circle')),
+      blockUtils.blockOfType('functional_circle') +
+      blockUtils.blockOfType('place_image')),
     startBlocks: '<block type="functional_circle" inline="false"><functional_input name="COLOR"><block type="functional_string"><title name="VAL">red</title></block></functional_input><functional_input name="SIZE"><block type="functional_math_number"><title name="NUM">50</title></block></functional_input></block>',
     requiredBlocks: '',
     freePlay: false
