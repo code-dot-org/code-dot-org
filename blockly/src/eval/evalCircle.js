e4bc986bdd0dc8e88c42cccb837915a126154e56
Brent Van Minnen <bjvanminnen@gmail.com>
2014-10-24 16:47:12 -0700

    move some things into a utils file

--- blockly/src/eval/evalCircle.js
+++ blockly/src/eval/evalCircle.js
@@ -1,46 +1,10 @@
 var EvalObject = require('./evalObject');
 var EvalString = require('./evalString');
-
-// todo - should live elsewhere?
-function getFill(style, color) {
-  // todo - i18n
-  // todo - 1asdf becomes 1 with parseInt
-  // for now, we treat anything we don't recognize as solid.
-  var alpha = parseInt(style, 10);
-  if (style !== "outline") {
-    return color;
-  }
-  return "none";
-}
-
-function getStroke(style, color) {
-  if (style === "outline") {
-    return color;
-  }
-  return "none";
-}
-
-function getOpacity(style, color) {
-  // todo - validate alpha is in range 0 - 255?
-  var alpha = parseInt(style, 10);
-  if (alpha !== undefined) {
-    return alpha / 255;
-  }
-  return 1.0;
-}
-
-// todo - stick in a utils file?
-function ensureEvalString(val) {
-  if (!(val instanceof EvalString)) {
-    // todo - better strategy than throwing?
-    throw new Error("expected EvalString");
-  }
-}
-
+var evalUtils = require('./evalUtils');
 
 var EvalCircle = function (radius, style, color) {
-  ensureEvalString(style);
-  ensureEvalString(color);
+  evalUtils.ensureType(style, EvalString);
+  evalUtils.ensureType(color, EvalString);
 
   EvalObject.apply(this);
 
@@ -67,7 +31,7 @@ EvalCircle.prototype.draw = function (parent) {
   this.element_.setAttribute('r', this.radius_);
 
   // todo - alpha
-  this.element_.setAttribute('fill', getFill(this.style_, this.color_));
-  this.element_.setAttribute('stroke', getStroke(this.style_, this.color_));
-  this.element_.setAttribute('opacity', getOpacity(this.style_, this.color_));
+  this.element_.setAttribute('fill', evalUtils.getFill(this.style_, this.color_));
+  this.element_.setAttribute('stroke', evalUtils.getStroke(this.style_, this.color_));
+  this.element_.setAttribute('opacity', evalUtils.getOpacity(this.style_, this.color_));
 };
