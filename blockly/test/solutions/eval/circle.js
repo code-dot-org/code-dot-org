016ed3ff5ee59620bc87e1363b8b96b63baf989b
Brent Van Minnen <bjvanminnen@gmail.com>
2014-10-27 16:04:15 -0700

    msm block generation

--- blockly/test/solutions/eval/circle.js
+++ blockly/test/solutions/eval/circle.js
@@ -5,7 +5,11 @@ module.exports = {
   app: "eval",
   skinId: 'eval',
   levelDefinition: {
-    solutionBlocks: '<block type="functional_circle" inline="false"><functional_input name="COLOR"><block type="functional_string"><title name="VAL">red</title></block></functional_input><functional_input name="SIZE"><block type="functional_math_number"><title name="NUM">50</title></block></functional_input></block>',
+    solutionBlocks: blockUtils.msmBlockXml('functional_circle', {
+      'COLOR': blockUtils.msmBlockXml('functional_string', null, { VAL: 'red' } ),
+      'STYLE': blockUtils.msmBlockXml('functional_string', null, { VAL: 'outline' }),
+      'SIZE': blockUtils.msmBlockXml('functional_math_number', null, { NUM: 50 } )
+    }),
     requiredBlocks: '',
     freePlay: false
   },
@@ -17,6 +21,59 @@ module.exports = {
         testResult: TestResults.APP_SPECIFIC_FAIL
       },
       xml: '<xml>' +
+      '</xml>'
+    },
+    {
+      description: "correct answer",
+      expected: {
+        result: true,
+        testResult: TestResults.ALL_PASS
+      },
+      customValidator: function (assert) {
+        var user = document.getElementById('user');
+        var circle = user.querySelector('circle');
+        var fill = circle.getAttribute('fill');
+        var stroke = circle.getAttribute('stroke');
+        assert(fill === 'none', 'fill: ' + fill);
+        assert(stroke === 'red', 'stroke: ' + stroke);
+        assert(circle.getAttribute('cx') === '200');
+        assert(circle.getAttribute('cy') === '200');
+        return true;
+      },
+      xml: '<xml>' +
+        blockUtils.msmBlockXml('functional_circle', {
+          'COLOR': blockUtils.msmBlockXml('functional_string', null, { VAL: 'red' } ),
+          'STYLE': blockUtils.msmBlockXml('functional_string', null, { VAL: 'outline' }),
+          'SIZE': blockUtils.msmBlockXml('functional_math_number', null, { NUM: 50 } )
+        }) +
+      '</xml>'
+    },
+    {
+      description: "correct answer with style block",
+      expected: {
+        result: true,
+        testResult: TestResults.ALL_PASS
+      },
+      xml: '<xml>' +
+        blockUtils.msmBlockXml('functional_circle', {
+          'COLOR': blockUtils.msmBlockXml('functional_string', null, { VAL: 'red' } ),
+          'STYLE': blockUtils.msmBlockXml('functional_style', null, { VAL: 'outline' }),
+          'SIZE': blockUtils.msmBlockXml('functional_math_number', null, { NUM: 50 } )
+        }) +
+      '</xml>'
+    },
+    {
+      description: "wrong color",
+      expected: {
+        result: false,
+        testResult: TestResults.APP_SPECIFIC_FAIL
+      },
+      xml: '<xml>' +
+        blockUtils.msmBlockXml('functional_circle', {
+          'COLOR': blockUtils.msmBlockXml('functional_string', null, { VAL: 'blue' } ),
+          'STYLE': blockUtils.msmBlockXml('functional_string', null, { VAL: 'outline' }),
+          'SIZE': blockUtils.msmBlockXml('functional_math_number', null, { NUM: 50 } )
+        }) +
       '</xml>'
     }
   ]
