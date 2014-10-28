e2520487a189f8b74f3f78a20c997673e1cd6e99
Continuous Integration <dev@code.org>
2014-10-28 22:42:54 +0000

    Automatically built.
    commit 06e9215ab75b0c034e7f47dc283a1266d120055f
    Merge: 6f8d2b4 f67aa13
    Author: Brendan Reville <brendan@code.org>
    Date:   Tue Oct 28 15:35:33 2014 -0700
    
        Merge branch 'hoc' of github.com:code-dot-org/code-dot-private into hoc
    
    commit 6f8d2b4508b0daf6cf007fe21016dd7f0d46eda6
    Author: Brendan Reville <brendan@code.org>
    Date:   Tue Oct 28 15:35:20 2014 -0700
    
        Adjust lines for Anna & Elsa.

--- dashboard/public/blockly-package/js/en_ploc/turtle.js
+++ dashboard/public/blockly-package/js/en_ploc/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21bda09c74d023bf88597f4a8ff01472cc75e1aa72f3175bb6048dd81ee70c38
-size 537390
+oid sha256:c4b9a9c4c8d627588e890cff1f810ab0c5f39c81a59d68e18dd1de8aef6c6ed5
+size 537438
