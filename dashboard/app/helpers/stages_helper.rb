a0f579e34fa5f6d7d76107e733808cae3897dd5e
Laurel Fan <laurel.fan@gmail.com>
2014-10-27 17:52:52 -0700

    progress

--- dashboard/app/helpers/stages_helper.rb
+++ dashboard/app/helpers/stages_helper.rb
@@ -1,4 +1,14 @@
 module StagesHelper
+  def stage_title(script, stage_or_game)
+    title = data_t_suffix('script.name', script.name, stage_or_game.name)
+    return title unless stage_or_game.instance_of? Game
+    if stage_or_game.instance_of? Game
+      title += ": "
+      title += "<span class='game-title'>" + (data_t('game.name', stage_or_game.name) || stage_or_game.name) + "</span>"
+    end
+    title.html_safe
+  end
+
   def lesson_plan_html_url(stage)
     "#{lesson_plan_base_url(stage)}/Teacher"
   end
