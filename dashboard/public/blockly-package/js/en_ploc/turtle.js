c7410c260cd1cf259972ef36886fcb94a784467f
Continuous Integration <dev@code.org>
2014-10-30 19:01:14 +0000

    Automatically built.
    commit 59c71a4455200c64c200ff0127c7cdfc3faa8a54
    Merge: 113e97e 78df1db
    Author: Brendan Reville <brendan@code.org>
    Date:   Thu Oct 30 11:53:51 2014 -0700
    
        Merge branch 'hoc' of github.com:code-dot-org/code-dot-private into hoc
    
    commit 113e97ea09e233ea907e9c3297279dc9ef9cada4
    Author: Brendan Reville <brendan@code.org>
    Date:   Thu Oct 30 11:53:10 2014 -0700
    
        Better sharing for frozen skins (including no frame with pencil person) but legacy artist remains same.

--- dashboard/public/blockly-package/js/en_ploc/turtle.js
+++ dashboard/public/blockly-package/js/en_ploc/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3d0b40944ad18d79238a12d555622bc2b0d0bbad03c4bb65733b310d994721b
-size 554261
+oid sha256:d7b5cc2c5e1b567334afc9fe4d5beac9e7f244927cab2196b229b5f58358ec1a
+size 554587
--- dashboard/public/blockly-package/js/en_us/turtle.js
+++ dashboard/public/blockly-package/js/en_us/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f919c760c6b33ce3a8d8d04909f1e98ab6ac5a938d7e286350bb9a4b4fb1d15c
-size 553256
+oid sha256:0f48f3235eaa168f2a4308716d1c8d3951cd9e8560636bcc748667727dc70fe6
+size 553582
