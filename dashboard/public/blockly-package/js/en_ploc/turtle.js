30f308e316a279bf067ab90d7ebc8e57918d477d
Continuous Integration <dev@code.org>
2014-11-03 22:33:18 +0000

    Automatically built.
    commit 6f71021df4ddc014548997087e70fa5ba8be86d4
    Author: Brendan Reville <brendan@code.org>
    Date:   Mon Nov 3 14:25:49 2014 -0800
    
        Updated Elsa sprite sheet with darker blue shadow.
    
    commit 4d1508336a7b3a1c7f5326743d556a67716bcc7a
    Merge: 907d1e4 9163952
    Author: Brendan Reville <brendan@code.org>
    Date:   Mon Nov 3 14:01:07 2014 -0800
    
        Merge branch 'hoc' of github.com:code-dot-org/code-dot-private into hoc
    
    commit 907d1e49864568e2145b681a865da0ed809f9ebb
    Author: Brendan Reville <brendan@code.org>
    Date:   Mon Nov 3 14:00:44 2014 -0800
    
        Update Elsa decoration, and handle non-multiple of 5 angles in artist.

--- dashboard/public/blockly-package/js/en_ploc/turtle.js
+++ dashboard/public/blockly-package/js/en_ploc/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9264645ed3b472caa53259c5ea5202b7e2bf1e79d8c9d3181c4b09677db28d3
-size 558180
+oid sha256:46ebca9183eca22ad3fad4fb35e64dbdcd33275cbfaf55f953a0e9f2ee21870a
+size 558492
--- dashboard/public/blockly-package/js/en_us/turtle.js
+++ dashboard/public/blockly-package/js/en_us/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4aafe10c02e1c1ca273d27bc09f221c252d54251a3887b8c2f3e9cfcf452426
-size 557169
+oid sha256:6094b539220574c959d068a1e1132d1e69dfaaaadcfe081b0ca48baf1a5c7fb9
+size 557481
