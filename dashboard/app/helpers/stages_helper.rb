5eec469d602f69b3ffd90edc1375625f6758f12b
Laurel Fan <laurel.fan@gmail.com>
2014-11-03 15:33:11 -0800

    make stage names consistent

--- dashboard/app/helpers/stages_helper.rb
+++ dashboard/app/helpers/stages_helper.rb
@@ -1,12 +1,17 @@
 module StagesHelper
   def stage_title(script, stage_or_game)
-    title = data_t_suffix('script.name', script.name, stage_or_game.name)
-    return title unless stage_or_game.instance_of? Game
-    if stage_or_game.instance_of? Game
-      title += ": "
+    if script.multiple_games?
+      title = data_t_suffix('script.name', script.name, stage_or_game.name)
+      return title unless stage_or_game.instance_of? Game
+      if stage_or_game.instance_of? Game
+        title += ": "
+      end
+      title += "<span class='game-title'>" + (data_t('game.name', stage_or_game.name) || stage_or_game.name) + "</span>"
+      title.html_safe
+    else
+      # script only has one stage/game, use the script name
+      data_t_suffix('script.name', @script.name, "title")
     end
-    title += "<span class='game-title'>" + (data_t('game.name', stage_or_game.name) || stage_or_game.name) + "</span>"
-    title.html_safe
   end
 
   def lesson_plan_html_url(stage)
