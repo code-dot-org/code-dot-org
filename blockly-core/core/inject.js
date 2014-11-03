0f8db6e8565eb8659e4efdceac34631069baa5b0
Josh Lory <josh.lory@code.org>
2014-11-03 12:53:06 -0800

    Add useModalFunctionEditor config option

--- blockly-core/core/inject.js
+++ blockly-core/core/inject.js
@@ -147,7 +147,7 @@ Blockly.parseOptions_ = function(options) {
     languageTree: tree,
     disableParamEditing: options['disableParamEditing'] || false,
     disableVariableEditing: options['disableVariableEditing'] || false,
-    useModalFunctionEditor: options['useModalFunctionEditor'] || true, // TODO(bjordan/jlory): default to false
+    useModalFunctionEditor: options['useModalFunctionEditor'] || false,
     grayOutUndeletableBlocks: grayOutUndeletableBlocks
   };
 };
