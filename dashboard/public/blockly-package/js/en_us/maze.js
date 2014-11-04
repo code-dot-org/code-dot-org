311cfba17055dcffe28b71c1a20f49130a7268e4
Continuous Integration <dev@code.org>
2014-11-04 21:35:23 +0000

    Automatically built.
    commit b922dbc6da45b4b8a8a78b010a1a5cbded17e115
    Author: Brendan Reville <brendan@code.org>
    Date:   Tue Nov 4 13:27:20 2014 -0800
    
        Fix jshint error.

--- dashboard/public/blockly-package/js/en_us/maze.js
+++ dashboard/public/blockly-package/js/en_us/maze.js
@@ -10003,6 +10003,17 @@ function drawMap () {
     });
   }
 
+  if (skin.celebrateAnimation) {
+    createPegmanAnimation({
+      idStr: 'celebrate',
+      pegmanImage: skin.celebrateAnimation,
+      row: Maze.start_.y,
+      col: Maze.start_.x,
+      direction: Direction.NORTH,
+      numColPegman: skin.celebratePegmanCol,
+      numRowPegman: skin.celebratePegmanRow
+    });
+  }
 
   // Add the hidden dazed pegman when hitting the wall.
   if (skin.wallPegmanAnimation) {
@@ -10533,6 +10544,11 @@ BlocklyApps.reset = function(first) {
     acorn.setAttribute('visibility', 'hidden');
   }
 
+  if (skin.celebrateAnimation) {
+    var celebrateAnimation = document.getElementById('celebratePegman');
+    celebrateAnimation.setAttribute('visibility', 'hidden');
+  }
+
   // Move the init dirt marker icons into position.
   resetDirt();
   resetDirtImages(false);
@@ -10953,7 +10969,11 @@ function animateAction (action, spotlightBlocks, timePerStep) {
         case BlocklyApps.TestResults.FREE_PLAY:
         case BlocklyApps.TestResults.TOO_MANY_BLOCKS_FAIL:
         case BlocklyApps.TestResults.ALL_PASS:
-          Maze.scheduleDance(true, timePerStep);
+          if (mazeUtils.isScratSkin(skin.id)) {
+            Maze.scheduleScratDance();
+          } else {
+            Maze.scheduleDance(true, timePerStep);
+          }
           break;
         default:
           timeoutList.setTimeout(function() {
@@ -11272,6 +11292,26 @@ function setPegmanTransparent() {
     pegmanFadeoutAnimation.beginElement();
   }
 }
+
+
+/**
+ * Schedule the animations for Scrat dancing.
+ */
+
+Maze.scheduleScratDance = function()
+{
+  var finishIcon = document.getElementById('finish');
+  if (finishIcon) {
+    finishIcon.setAttribute('visibility', 'hidden');
+  }
+
+  var timePerFrame = 100;
+  var start = {x: Maze.pegmanX, y: Maze.pegmanY};
+
+  scheduleSheetedMovement({x: start.x, y: start.y}, {x: 0, y: 0 },
+    skin.celebratePegmanRow, timePerFrame, 'celebrate', Direction.NORTH, true);
+};
+
 
 /**
  * Schedule the animations and sound for a dance.
@@ -11687,6 +11727,10 @@ var CONFIGS = {
     acornAnimation: 'splash.png',
     acornPegmanCol: 1,
     acornPegmanRow: 20,
+
+    celebrateAnimation: 'jump_acorn_sheet.png',
+    celebratePegmanCol: 1,
+    celebratePegmanRow: 9,
 
     movePegmanAnimation: 'move_avatar.png',
     movePegmanAnimationSpeedScale: 1.5,
