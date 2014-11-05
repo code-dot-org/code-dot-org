b7f1536a8bc10b3f3a4e7b0f35ddbe41ecbc19e2
Geoffrey Elliott <geoffrey@code.org>
2014-11-05 14:14:32 -0800

    Chef cookbooks, front-end almost complete.

---
+++ chef/cookbooks/cdo-repository/metadata.rb
@@ -0,0 +1,9 @@
+name             'cdo-repository'
+maintainer       'Code.org'
+maintainer_email 'geoffrey@code.org'
+license          'All rights reserved'
+description      'Installs/Configures cdo-repository'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'cdo-github-access'
