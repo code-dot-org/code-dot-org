c5e050ae49ef6a0ee684ff5383c951fd8e861eac
Continuous Integration <dev@code.org>
2014-11-07 23:52:29 +0000

    Automatically built.
    commit 227d4998a56230fa5eba4e0e99ec9dcc6df8d69a
    Merge: 5cc8214 1379f7d
    Author: Brendan Reville <brendan@code.org>
    Date:   Fri Nov 7 15:43:06 2014 -0800
    
        Merge branch 'hoc' of github.com:code-dot-org/code-dot-private into hoc
    
    commit 5cc821461e9e907829172785e45a725e2a8d42d7
    Author: Brendan Reville <brendan@code.org>
    Date:   Fri Nov 7 15:41:14 2014 -0800
    
        New Frozen art, and bitmap pattern for Anna too.

--- dashboard/public/blockly-package/js/en_ploc/turtle.js
+++ dashboard/public/blockly-package/js/en_ploc/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8005df0ea7b0781a884bfa9299994c1ac1becb631a3f1831905ab5943eeb7c6
-size 564789
+oid sha256:96a8f54c96522548780522c3f1675697f4a07c9bd528ef47fed28c9d49b316f8
+size 564952
