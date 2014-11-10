21d70dd61269412d89ca80d2448c52dea39dcb13
Continuous Integration <dev@code.org>
2014-11-10 21:54:29 +0000

    Automatically built.
    commit 5a543d546a9cdb39661c976739ba242b682ed990
    Merge: ae7b6d2 c9faaef
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Nov 10 13:47:14 2014 -0800
    
        Merge pull request #6 from code-dot-org/acornAnimate
    
        remove unused asset
    
    commit c9faaef2ef779dff5248e1313d2b4a9c68b9b854
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Nov 10 13:37:37 2014 -0800
    
        remove unused asset

--- dashboard/public/blockly-package/js/en_ploc/maze.js
+++ dashboard/public/blockly-package/js/en_ploc/maze.js
@@ -10003,18 +10003,6 @@ function drawMap () {
     }
   }
 
-  if (skin.acornAnimation) {
-    createPegmanAnimation({
-      idStr: 'acorn',
-      pegmanImage: skin.acornAnimation,
-      row: Maze.start_.y,
-      col: Maze.start_.x,
-      direction: Maze.startDirection,
-      numColPegman: skin.acornPegmanCol,
-      numRowPegman: skin.acornPegmanRow
-    });
-  }
-
   if (skin.celebrateAnimation) {
     createPegmanAnimation({
       idStr: 'celebrate',
@@ -10558,11 +10546,6 @@ BlocklyApps.reset = function(first) {
   if (skin.movePegmanAnimation) {
     var movePegmanIcon = document.getElementById('movePegman');
     movePegmanIcon.setAttribute('visibility', 'hidden');
-  }
-
-  if (skin.acornAnimation) {
-    var acorn = document.getElementById('acornPegman');
-    acorn.setAttribute('visibility', 'hidden');
   }
 
   if (skin.celebrateAnimation) {
@@ -11744,10 +11727,6 @@ var CONFIGS = {
     hittingWallAnimationSpeedScale: 1.5,
     wallPegmanCol: 1,
     wallPegmanRow: 20,
-
-    acornAnimation: 'splash.png',
-    acornPegmanCol: 1,
-    acornPegmanRow: 20,
 
     celebrateAnimation: 'jump_acorn_sheet.png',
     celebratePegmanCol: 1,
