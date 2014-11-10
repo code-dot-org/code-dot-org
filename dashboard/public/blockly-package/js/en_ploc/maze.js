47dcf0c6d6394bbd87599e25000311e3adaba24c
Continuous Integration <dev@code.org>
2014-11-10 19:52:34 +0000

    Automatically built.
    commit 93adbf0a1d9ae1661c608ddea08555db219009d6
    Merge: 7c673f4 0d58fe3
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Nov 10 11:45:10 2014 -0800
    
        Merge pull request #5 from code-dot-org/scratTiles
    
        allow non-land tiles to be adjacent to path
    
    commit 0d58fe3cb3385aab9c0a6bd85742e341390ff807
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Nov 10 11:40:18 2014 -0800
    
        allow non-land tiles to be adjacent to path

--- dashboard/public/blockly-package/js/en_ploc/maze.js
+++ dashboard/public/blockly-package/js/en_ploc/maze.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7bca7759a2ae118dffb1e9d678e5b9a3a606c2cc24c8020be7040bf43e701ac
-size 598767
+oid sha256:fca65d5a5fda1338a628dbf2d34dcd4934f0fa6f640498f297d127b6d2231e10
+size 599182
--- dashboard/public/blockly-package/js/en_us/maze.js
+++ dashboard/public/blockly-package/js/en_us/maze.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a0acfab78af75397cccdd81ed4f996742e445b8e6abff60b97be93bbcb7069f
-size 597694
+oid sha256:61e3a03fd499553d1fc38e741b9f433b319334e0b1c00d28fcc22ef520c80959
+size 598109
