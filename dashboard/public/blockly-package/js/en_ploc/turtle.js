1757913cbb089d3655a6de80fbb6e30bdc13e9a9
Continuous Integration <dev@code.org>
2014-11-12 22:31:45 +0000

    Automatically built.
    commit beb8a4bcf58120833a5a17895a325ed5a02dde5c
    Merge: 12fe5bb 9660745
    Author: Brendan Reville <brendan@code.org>
    Date:   Wed Nov 12 14:24:24 2014 -0800
    
        Merge branch 'hoc' of github.com:code-dot-org/code-dot-private into hoc
    
    commit 12fe5bba85d32069f6b2df29520685d7a66c3635
    Author: Brendan Reville <brendan@code.org>
    Date:   Wed Nov 12 14:23:52 2014 -0800
    
        Don't support retina pixel ratios other than 2.  (Sorry iPhone 6 Plus!)

--- dashboard/public/blockly-package/js/en_ploc/turtle.js
+++ dashboard/public/blockly-package/js/en_ploc/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2e6720099aa8a3fcf351b06109b240a1be29f4f992ced4c5e99c6d2c38689ce
-size 576408
+oid sha256:6a992fc5e0be9e1567a0aa1808906a77aa8a37f3610207fdcf4739f39c5f1199
+size 576530
--- dashboard/public/blockly-package/js/en_us/turtle.js
+++ dashboard/public/blockly-package/js/en_us/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:573445492b701df1eee966ac683f94c268ffc1f35add384bf9600a30958de216
-size 575367
+oid sha256:1d58ea9f08d61b3b74a51387d3687f7b5a4e277fc99e333cf6a17b2da744446e
+size 575489
