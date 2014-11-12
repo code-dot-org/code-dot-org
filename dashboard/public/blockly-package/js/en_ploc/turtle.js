1256e638d07351a1d04ef7c5893ba08b62695f31
Continuous Integration <dev@code.org>
2014-11-12 01:53:44 +0000

    Automatically built.
    commit 68015cc1fc83ae32dd94cccd3c1131b445546ebf
    Merge: 420b495 d5711ce
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Nov 11 17:46:28 2014 -0800
    
        Merge pull request #11 from code-dot-org/snowflakes
    
        add some more prebuilt snowflakes
    
    commit d5711ce1c56a2bce3bec7c2a1d078cff7896bf65
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Nov 11 17:42:45 2014 -0800
    
        add some more prebuilt snowflakes

--- dashboard/public/blockly-package/js/en_ploc/turtle.js
+++ dashboard/public/blockly-package/js/en_ploc/turtle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5ce4962a33b6d6f7da394973e80a656fcafb51cfc468483d32b42cc09401eca
-size 570156
+oid sha256:a2e6720099aa8a3fcf351b06109b240a1be29f4f992ced4c5e99c6d2c38689ce
+size 576408
