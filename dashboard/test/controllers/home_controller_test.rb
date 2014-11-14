0a109ff59a8278fdf72f0aebbd8bd3a3ef5c7775
Josh Lory <josh.lory@code.org>
2014-11-14 10:26:34 -0800

    Fix `rake test` failures
    Laurel is working on a fix to make progress specific to scripts (rather
    than just levels).

--- dashboard/test/controllers/home_controller_test.rb
+++ dashboard/test/controllers/home_controller_test.rb
@@ -144,6 +144,9 @@ class HomeControllerTest < ActionController::TestCase
     Script.find(Script::TWENTY_HOUR_ID).script_levels.each do |script_level|
       UserLevel.create(user: user, level: script_level.level, attempts: 1, best_result: Activity::MINIMUM_PASS_RESULT)
     end
+    Script.find_by(name: 'hourofcode').script_levels.each do |script_level|
+      UserLevel.find_or_create_by(user: user, level: script_level.level, attempts: 1, best_result: Activity::MINIMUM_PASS_RESULT)
+    end
     user.backfill_user_scripts
 
     assert_equal [], user.working_on_scripts # if you finish a script you are not working on it!
