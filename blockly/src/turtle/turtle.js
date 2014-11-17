dcf283b03b3e292ca20c1a8bfd3c3d6b61ffa651
Brendan Reville <brendan@code.org>
2014-11-16 23:34:52 -0800

    Fix artist level attempting to load 'undefined' as a default pattern image.

--- blockly/src/turtle/turtle.js
+++ blockly/src/turtle/turtle.js
@@ -256,10 +256,12 @@ Turtle.init = function(config) {
 
       for( var i = 0; i <  Blockly.Blocks.draw_line_style_pattern.Options.length; i++) {
         var pattern = Blockly.Blocks.draw_line_style_pattern.Options[i][1];
-        var img = new Image();
-        img.src = skin[pattern];
-        img.id = pattern;
-        imageContainer.appendChild(img);
+        if (pattern !== 'DEFAULT') {
+          var img = new Image();
+          img.src = skin[pattern];
+          img.id = pattern;
+          imageContainer.appendChild(img);
+        }
       }
     }
 
