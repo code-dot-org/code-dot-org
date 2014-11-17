cc077b07fc68b6821e017a4511321ac94d79ad62
Laurel Fan <laurel.fan@gmail.com>
2014-11-17 11:52:06 -0800

    multiple course homepage redesign, certificates, gallery pagination

--- dashboard/app/controllers/activities_controller.rb
+++ dashboard/app/controllers/activities_controller.rb
@@ -159,8 +159,6 @@ class ActivitiesController < ApplicationController
   end
 
   def track_progress_in_session
-    # TODO: this doesn't work for multiple scripts, especially if scripts share levels
-
     # hash of level_id => test_result
     test_result = params[:testResult].to_i
     session[:progress] ||= {}
@@ -177,6 +175,12 @@ class ActivitiesController < ApplicationController
     end
 
     @new_level_completed = true if !Activity.passing?(old_result) && Activity.passing?(test_result)
+
+    # track scripts
+    if @script_level.try(:script).try(:id)
+      session[:scripts] ||= []
+      session[:scripts] = session[:scripts].unshift(@script_level.script_id).uniq
+    end
   end
 
   def trophy_check(user)
