f0d1b7ca3f7af04040985345e1a02bf8bad10cf5
Geoffrey Elliott <geoffrey@code.org>
2014-11-18 02:42:34 -0800

    Set a cache time of 60 seconds for indiegogo api calls because they're expensive.

--- pegasus/routes/v2_indiegogo_routes.rb
+++ pegasus/routes/v2_indiegogo_routes.rb
@@ -1,9 +1,11 @@
 get '/v2/indiegogo_campaigns' do
+  cache_control :public, :must_revalidate, max_age:60
   uri = URI('https://api.indiegogo.com/1/campaigns/964232.json?api_token=' + CDO.indiegogo_api_token)
-  Net::HTTP.get(uri) # => String
+  Net::HTTP.get(uri)
 end
 
 get '/v2/indiegogo_contributions' do
+  cache_control :public, :must_revalidate, max_age:60
   uri = URI('https://api.indiegogo.com/1/campaigns/964232/contributions.json?api_token=' + CDO.indiegogo_api_token)
-  Net::HTTP.get(uri) # => String
+  Net::HTTP.get(uri)
 end
