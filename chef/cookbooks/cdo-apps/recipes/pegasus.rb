0903f17c48e645ccc6d9c46405836fe04de9b101
Geoffrey Elliott <geoffrey@code.org>
2014-11-17 23:03:38 -0800

    Simplify settings.

--- chef/cookbooks/cdo-apps/recipes/pegasus.rb
+++ chef/cookbooks/cdo-apps/recipes/pegasus.rb
@@ -9,7 +9,7 @@ template "/etc/init.d/pegasus" do
   variables ({
     src_file:"/home/#{node[:current_user]}/#{node.chef_environment}/pegasus/config/unicorn.rb",
     app_root:"/home/#{node[:current_user]}/#{node.chef_environment}/pegasus",
-    pid_file:"/home/#{node[:current_user]}/#{node.chef_environment}/aws/pegasus_unicorn.rb.pid",
+    pid_file:"/home/#{node[:current_user]}/#{node.chef_environment}/pegasus/config/unicorn.rb.pid",
     user:node[:current_user],
     env:node.chef_environment,
   })
