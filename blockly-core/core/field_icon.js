5e95b44f70a7b35aaf164fb47e4c7117f4b38794
Josh Lory <josh.lory@code.org>
2014-11-20 12:26:25 -0800

    Use default cursor (the same cursor is used for icons)

--- blockly-core/core/field_icon.js
+++ blockly-core/core/field_icon.js
@@ -9,10 +9,9 @@ goog.require('Blockly.FieldLabel');
 
 Blockly.FieldIcon = function(text) {
   Blockly.FieldIcon.superClass_.constructor.apply(this, arguments);
-  Blockly.removeClass_(this.fieldGroup_, 'blocklyEditableText');
   Blockly.addClass_(this.fieldGroup_, 'blocklyIconGroup');
   Blockly.addClass_(this.borderRect_, 'blocklyIconShield');
-  this.textElement_.setAttribute('style', 'font-size:9pt');
+  this.textElement_.setAttribute('style', 'font-size:9pt; cursor:default;');
 };
 goog.inherits(Blockly.FieldIcon, Blockly.Field);
 
@@ -20,3 +19,10 @@ goog.inherits(Blockly.FieldIcon, Blockly.Field);
  * Editable fields are saved by the XML renderer, non-editable fields are not.
  */
 Blockly.FieldIcon.prototype.EDITABLE = false;
+
+/**
+ * To be overridden by the instance.
+ * @private
+ */
+Blockly.FieldIcon.prototype.showEditor_ = function() {
+};
