ba43a8d5e4fac2e70d61c08781087e09fdaf08b8
Geoffrey Elliott <geoffrey@code.org>
2014-11-21 12:29:26 -0800

    Sync logs to S3 hourly

--- chef/cookbooks/cdo-apps/recipes/pegasus.rb
+++ chef/cookbooks/cdo-apps/recipes/pegasus.rb
@@ -64,8 +64,7 @@ end
 
 cron 'upload-pegasus-logs-to-s3' do
   action :create
-  minute '5'
-  hour '1'
+  minute '10'
   user node[:current_user]
   home "/home/#{node[:current_user]}"
   command "/home/#{node[:current_user]}/#{node.chef_environment}/aws/cronjob \"/home/#{node[:current_user]}/#{node.chef_environment}/bin/upload-logs-to-s3 pegasus\""
