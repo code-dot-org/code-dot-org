baa23bafe13318ad326684c105b6480c10403677
Laurel Fan <laurel.fan@gmail.com>
2014-11-25 10:56:40 -0800

    add sql commands in comments

--- dashboard/db/migrate/20141119223233_add_script_to_user_levels.rb
+++ dashboard/db/migrate/20141119223233_add_script_to_user_levels.rb
@@ -1,7 +1,11 @@
 class AddScriptToUserLevels < ActiveRecord::Migration
   def change
+    # sql commands in comments for manual migration
     add_column :user_levels, :script_id, :integer, null: true
+    # ALTER TABLE `user_levels` ADD `script_id` int(11)
     add_index :user_levels, [:user_id, :level_id, :script_id], unique: true
+    # CREATE UNIQUE INDEX `index_user_levels_on_user_id_and_level_id_and_script_id` ON `user_levels` (`user_id`, `level_id`, `script_id`)
     remove_index :user_levels, column: [:user_id, :level_id]
+    # DROP INDEX `index_user_levels_on_user_id_and_level_id` ON `user_levels`
   end
 end
