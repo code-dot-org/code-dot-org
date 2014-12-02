95fe1125ee2a1fd9c00a1f07bb939573ca5db71b
Will Jordan <will@code.org>
2014-12-02 14:35:04 -0800

    [Finishes #83360590] new Contract Match level type for MSM assessment

--- dashboard/app/models/game.rb
+++ dashboard/app/models/game.rb
@@ -108,6 +108,7 @@ class Game < ActiveRecord::Base
         ArtistEC:turtle:artist_intro
         TextMatch
         StudioEC:studio
+        ContractMatch
       ).each_with_index do |game, id|
         name, app, intro_video = game.split ':'
         Game.create!(id: id + 1, name: name, app: app, intro_video: Video.find_by_key(intro_video))
