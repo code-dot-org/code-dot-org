edfe6a667833bb5b44056b2ccf0451ce6aa481a4
CaptainStack <CaptainStack@outlook.com>
2014-12-04 12:12:44 -0800

    Added api. Modifying JS and view.

--- pegasus/sites.v3/code.org/public/js/print-functions.js
+++ pegasus/sites.v3/code.org/public/js/print-functions.js
@@ -1,5 +1,5 @@
 function createHiddenPrintWindow(src) {
-  var iframe = $('<iframe id="print_frame" ></iframe>'); // Created a hidden iframe with just the desired image as its contents
+  var iframe = $('<iframe id="print_frame" style="display: none"></iframe>'); // Created a hidden iframe with just the desired image as its contents
   iframe.appendTo("body");
   iframe[0].contentWindow.document.write("<img src='" + src + "' style='width: 100%'/>");
   iframe[0].contentWindow.document.write("<script>if (document.execCommand('print', false, null)) {  } else { window.print();  } </script>");
