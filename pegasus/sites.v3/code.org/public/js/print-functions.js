1c588a8de0fea7720911e96f7681da4c883eb1b2
CaptainStack <CaptainStack@outlook.com>
2014-12-04 13:53:35 -0800

    Added icon assets and inserted them into api. Deleted external JS file and put it into document.

--- pegasus/sites.v3/code.org/public/js/print-functions.js
+++
@@ -1,7 +0,0 @@
-function createHiddenPrintWindow(src) {
-  var iframe = $('<iframe id="print_frame" style="display: none"></iframe>'); // Created a hidden iframe with just the desired image as its contents
-  iframe.appendTo("body");
-  iframe[0].contentWindow.document.write("<img src='" + src + "' style='width: 100%'/>");
-  iframe[0].contentWindow.document.write("<script>if (document.execCommand('print', false, null)) {  } else { window.print();  } </script>");
-  $("#print_frame").remove(); // Remove the iframe when the print dialogue has been launched
-}
