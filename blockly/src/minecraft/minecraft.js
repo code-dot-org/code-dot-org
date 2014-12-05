da488a2189f6d7ea36a6a4d4362c79d2adbc3c81
Will Jordan <will@code.org>
2014-12-05 09:55:16 -0800

    change styling on visualization container use updated js bundle

--- blockly/src/minecraft/minecraft.js
+++ blockly/src/minecraft/minecraft.js
@@ -98,7 +98,8 @@ Minecraft.init = function(config) {
     // Adjust visualizationColumn width.
     var visualizationColumn = document.getElementById('visualizationColumn');
     visualizationColumn.style.width = '400px';
-    visualizationColumn.appendChild(document.getElementById('container'));
+    var visualization = document.getElementById('visualization');
+    visualization.appendChild(document.getElementById('container'));
 
     // base's BlocklyApps.resetButtonClick will be called first
     var resetButton = document.getElementById('resetButton');
