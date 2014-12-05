51a83fe4d930dd52461b0aa1eed93266254bc0e8
Laurel Fan <laurel.fan@gmail.com>
2014-12-04 23:57:39 -0800

    fix integration test

--- dashboard/test/integration/session_cookie_test.rb
+++ dashboard/test/integration/session_cookie_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 class SessionCookieTest < ActionDispatch::IntegrationTest
   test 'session cookie name contains environment' do
-    get '/'
+    get '/reset_session'
 
     assert cookies['_learn_session_test']
   end
+
+  test 'no cookies if you do not do anything' do
+    get '/'
+
+    assert_equal nil, cookies['_learn_session_test']
+  end
+
 end
