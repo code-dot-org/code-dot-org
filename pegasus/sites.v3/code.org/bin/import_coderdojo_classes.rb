0c8da3d3a805f55f4e371a36889889413df95ebf
Rick Hawkins <rick@alchemydes.com>
2014-12-06 10:19:35 -0800

    Enabled script.

--- pegasus/sites.v3/code.org/bin/import_coderdojo_classes.rb
+++ pegasus/sites.v3/code.org/bin/import_coderdojo_classes.rb
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:349e0bb92f2f88ef797041c636bcf0de7b113685e1b8252a75e3affe697a6dcd
-size 768
+oid sha256:25206e5b3c842289571b1a484d3bfb6abce441846e10b3a32a354225e385edda
+size 766
