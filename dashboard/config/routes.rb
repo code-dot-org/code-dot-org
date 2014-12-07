feb53407e9f6e73f5ddc1919075f6680ad4521a8
Brian Jordan <bcjordan@gmail.com>
2014-12-06 22:50:12 -0800

    Redirect any request for the voxel_textures asset zip

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -6,8 +6,7 @@ Dashboard::Application.routes.draw do
     end
   end
 
-  get '/s/voxel/stage/1/puzzle/voxel_textures.zip', to: redirect('/voxel_textures.zip')
-  get '/levels/voxel_textures.zip', to: redirect('/voxel_textures.zip')
+  get '*voxel_textures.zip', to: redirect('/voxel_textures.zip')
 
   resources :activity_hints, only: [:update]
   resources :callouts
