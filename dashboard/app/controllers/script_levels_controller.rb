3e1594224a8bc8b466752f3ede982bfbccebcb84
cpirich <chris@code.org>
2014-12-08 14:05:27 -0800

    cache fallback_responses

--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -102,7 +102,9 @@ private
 
     @callback = milestone_url(user_id: current_user.try(:id) || 0, script_level_id: @script_level)
     @full_width = true
-    @fallback_response = {
+
+    @@fallback_responses ||= []
+    @fallback_response = @@fallback_responses[@script_level.id] ||= {
       success: milestone_response(script_level: @script_level, solved?: true),
       failure: milestone_response(script_level: @script_level, solved?: false)
     }
