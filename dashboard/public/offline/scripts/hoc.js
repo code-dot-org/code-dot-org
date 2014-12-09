d8192671b5a5190538e67c14ca36da515da1c7ca
Josh Lory <josh.lory@code.org>
2014-12-09 09:46:33 -0800

    Missing hoc script file

---
+++ dashboard/public/offline/scripts/hoc.js
@@ -0,0 +1,28 @@
+var scripts = scripts || {};
+scripts.hoc = {
+  title: 'Hour of Code',
+  app: 'maze',
+  finishedRedirectURL: 'http://code.org/api/hour/finish/hourofcode',
+  puzzles: [
+    'hoc/1',
+    'hoc/2',
+    'hoc/3',
+    'hoc/4',
+    'hoc/5',
+    'hoc/6',
+    'hoc/7',
+    'hoc/8',
+    'hoc/9',
+    'hoc/10',
+    'hoc/11',
+    'hoc/12',
+    'hoc/13',
+    'hoc/14',
+    'hoc/15',
+    'hoc/16',
+    'hoc/17',
+    'hoc/18',
+    'hoc/19',
+    'hoc/20'
+  ]
+};
