280d78e9f7e0d83c8742456a5e9a6b581b5ba0af
Brian Jordan <bcjordan@gmail.com>
2014-12-09 14:18:40 -0800

    Remove pegman cachebusting for offline build maze.js

--- dashboard/public/offline/js/maze.js
+++ dashboard/public/offline/js/maze.js
@@ -917,7 +917,7 @@ var createPegmanAnimation = function(options) {
   svg.appendChild(clip);
   // Create image.
   // Add a random number to force it to reload everytime.
-  var imgSrc = options.pegmanImage + '?time=' + (new Date()).getTime();
+  var imgSrc = options.pegmanImage;
   var img = document.createElementNS(SVG_NS, 'image');
   img.setAttributeNS(
       'http://www.w3.org/1999/xlink', 'xlink:href', imgSrc);
