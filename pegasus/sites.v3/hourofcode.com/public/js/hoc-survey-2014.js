870a1aecb338969f484b51a571d97dc054f986d4
Rick Hawkins <rick@alchemydes.com>
2014-12-18 11:18:02 -0800

    More form updates.

--- pegasus/sites.v3/hourofcode.com/public/js/hoc-survey-2014.js
+++ pegasus/sites.v3/hourofcode.com/public/js/hoc-survey-2014.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
   $('#hoc-survey-form select').selectize({
     plugins: ['fast_click']
   });
+
+  $('#teacher-how-heard').change(function() {
+    if ($.inArray('Other', $(this).val()) > -1) {
+      $('#teacher-how-heard-other-wrapper').show();
+    } else {
+      $('#teacher-how-heard-other').val('');
+      $('#teacher-how-heard-other-wrapper').hide();
+    }
+    console.log(this);
+  }).triggerHandler('change');
 });
