fb52aa94524b0804421259e90cca7e764b887bb7
Geoffrey Elliott <geoffrey@code.org>
2015-01-07 22:52:44 -0800

    PropertyBags API (for Dave)

--- shared/middleware/apps_api.rb
+++ shared/middleware/apps_api.rb
@@ -1,22 +1,10 @@
-require 'sinatra/base'
+require 'apps_api/properties_api'
 
+#
+# AppsApi aggregates modular API components into a unified API component.
+#
 class AppsApi < Sinatra::Base
-
-  configure do
-  end
-
-  before do
-  end
-
-  after do
-  end
-
-  helpers do
-  end
-
-  get '/v3/apps/hello/*' do |what|
-    content_type :json
-    JSON.pretty_generate({hello:what})
-  end
-
+  
+  use PropertiesApi
+  
 end
