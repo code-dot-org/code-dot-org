1108f3bf9053e44bc91507816f8ecd8a4bf9ffd5
Brent Van Minnen <bjvanminnen@gmail.com>
2015-01-09 14:02:45 -0800

    Continue properly extracting FeedbackBlocks

--- apps/src/feedbackBlocks.js
+++ apps/src/feedbackBlocks.js
@@ -1,14 +1,18 @@
-var FeedbackBlocks = function(options) {
+var constants = require('./constants');
+var readonly = require('./templates/readonly.html');
+
+TestResults = constants.TestResults;
+
+// TODO (br-pair): can we not pass in the studioAppSingleton
+var FeedbackBlocks = function(options, missingRequiredBlocks, studioAppSingleton) {
   // Check whether blocks are embedded in the hint returned from dashboard.
   // See below comment for format.
   var embeddedBlocks = options.response && options.response.hint &&
       options.response.hint.indexOf("[{") !== 0;
   if (!embeddedBlocks &&
-      options.feedbackType !==
-      TestResults.MISSING_BLOCK_UNFINISHED &&
-      options.feedbackType !==
-      TestResults.MISSING_BLOCK_FINISHED) {
-      return;
+      options.feedbackType !== TestResults.MISSING_BLOCK_UNFINISHED &&
+      options.feedbackType !== TestResults.MISSING_BLOCK_FINISHED) {
+    return;
   }
 
   var blocksToDisplay = [];
@@ -28,7 +32,6 @@ var FeedbackBlocks = function(options) {
       return;
     }
   } else {
-    var missingRequiredBlocks = getMissingRequiredBlocks();
     blocksToDisplay = missingRequiredBlocks.blocksToDisplay;
     if (missingRequiredBlocks.message) {
       options.message = missingRequiredBlocks.message;
@@ -51,7 +54,7 @@ var FeedbackBlocks = function(options) {
       cacheBust: studioAppSingleton.CACHE_BUST,
       skinId: options.skin,
       level: options.level,
-      blocks: generateXMLForBlocks(blocksToDisplay)
+      blocks: this.generateXMLForBlocks_(blocksToDisplay)
     }
   });
   this.iframe = document.createElement('iframe');
@@ -70,4 +73,55 @@ FeedbackBlocks.prototype.show = function() {
     doc.write(this.html);
     doc.close();
   }
+};
+
+/**
+ * Creates the XML for blocks to be displayed in a read-only frame.
+ * @param {Array} blocks An array of blocks to display (with optional args).
+ * @return {string} The generated string of XML.
+ */
+FeedbackBlocks.prototype.generateXMLForBlocks_ = function(blocks) {
+  var blockXMLStrings = [];
+  var blockX = 10;  // Prevent left output plugs from being cut off.
+  var blockY = 0;
+  var blockXPadding = 200;
+  var blockYPadding = 120;
+  var blocksPerLine = 2;
+  var k, name;
+  for (var i = 0; i < blocks.length; i++) {
+    var block = blocks[i];
+    if (block.blockDisplayXML) {
+      blockXMLStrings.push(block.blockDisplayXML);
+      continue;
+    }
+    blockXMLStrings.push('<block', ' type="', block.type, '" x="',
+                        blockX.toString(), '" y="', blockY, '">');
+    if (block.titles) {
+      var titleNames = Object.keys(block.titles);
+      for (k = 0; k < titleNames.length; k++) {
+        name = titleNames[k];
+        blockXMLStrings.push('<title name="', name, '">',
+                            block.titles[name], '</title>');
+      }
+    }
+    if (block.values) {
+      var valueNames = Object.keys(block.values);
+      for (k = 0; k < valueNames.length; k++) {
+        name = valueNames[k];
+        blockXMLStrings.push('<value name="', name, '">',
+                            block.values[name], '</value>');
+      }
+    }
+    if (block.extra) {
+      blockXMLStrings.push(block.extra);
+    }
+    blockXMLStrings.push('</block>');
+    if ((i + 1) % blocksPerLine === 0) {
+      blockY += blockYPadding;
+      blockX = 0;
+    } else {
+      blockX += blockXPadding;
+    }
+  }
+  return blockXMLStrings.join('');
 };
