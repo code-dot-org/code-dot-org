babeb474525c995828e901243fb4808eff257f60
Tanya Parker <tanyacparker@gmail.com>
2015-01-09 11:45:48 -0800

    Revert "update pegasus add new string process"; all locale keys were overwritten to en-US
    This reverts commit 6aa7b93a5d55447d116e709ff97e921aceccb8c3.

--- i18n/code.org/sync-pegasus.sh
+++ i18n/code.org/sync-pegasus.sh
@@ -2,30 +2,17 @@
 
 set -e
 
-function cp_in() {
-  echo "$1 => $2"
-  cp $1 $2
-}
-
 locales=$(ls ../locales | grep -v 'en-US')
 
 for locale in $locales; do
 
-	# Bring in new strings to i18n/locales
-	orig_dir=../../pegasus/cache/i18n
-	loc_dir=../locales/$locale/pegasus
-	mkdir -p $loc_dir
+  ### Pegasus
+  orig_dir=../../pegasus/cache/i18n
+  loc_dir=../locales/$locale/pegasus
+  en_dir=../locales/en-US/pegasus
 
-	perl -i ./lib/fix-ruby-yml.pl $orig_dir/en-US.yml
-	cp_in $orig_dir/en-US.yml $loc_dir/mobile.yml
-
-	# Export to pegasus/cache/i18n/locales
-	orig_dir=../../pegasus/cache/i18n
-	loc_dir=../locales/$locale/pegasus
-	en_dir=../locales/en-US/pegasus
-
-	# Merge YML file.
-	ruby ./lib/merge-translation.rb "yml" $en_dir/mobile.yml $loc_dir/mobile.yml $orig_dir/$locale.yml
-	perl -i ./lib/fix-ruby-yml.pl $orig_dir/$locale.yml
+  # Merge YML file.
+  ruby ./lib/merge-translation.rb "yml" $en_dir/mobile.yml $loc_dir/mobile.yml $orig_dir/$locale.yml
+  perl -i ./lib/fix-ruby-yml.pl $orig_dir/$locale.yml
 
 done
