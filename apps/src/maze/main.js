bfb6286ed0f0053328522edba89133852ef6a649
Brad Buchanan <brad@code.org>
2015-01-13 12:34:36 -0800

    Remove StudioApp global un-define in maze/main.js

--- apps/src/maze/main.js
+++ apps/src/maze/main.js
@@ -1,8 +1,4 @@
 var appMain = require('../appMain');
-// TODO (br-pair): We're doing this so that other apps can still have
-// in the global namespace, while ensuring that we don't. Ultimately nobody
-// should have it, and we can remove this.
-window.StudioApp = undefined;
 window.Maze = require('./maze');
 if (typeof global !== 'undefined') {
   global.Maze = window.Maze;
