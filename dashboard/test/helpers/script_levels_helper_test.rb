87de59416868be0bf80fcb7014c54686d4e00571
Josh Lory <josh.lory@code.org>
2015-01-13 13:38:45 -0800

    Remove Script::SPECIAL_NAME

--- dashboard/test/helpers/script_levels_helper_test.rb
+++ dashboard/test/helpers/script_levels_helper_test.rb
@@ -3,18 +3,16 @@ require 'test_helper'
 class ScriptLevelsHelperTest < ActionView::TestCase
 
   test 'tracking_pixel_url' do
-    # hoc and special
+    # hoc
     assert_equal '//test.code.org/api/hour/begin_codeorg.png', tracking_pixel_url(Script.find(Script::HOC_ID))
-    assert_equal '//test.code.org/api/hour/begin_codeorg.png', tracking_pixel_url(Script.find_by_name(Script::SPECIAL_NAME))
 
     assert_equal '//test.code.org/api/hour/begin_frozen.png', tracking_pixel_url(Script.find_by_name('frozen'))
     assert_equal '//test.code.org/api/hour/begin_course4.png', tracking_pixel_url(Script.find_by_name('course4'))
   end
 
   test 'hoc_finish_url' do
-    # hoc and special
+    # hoc
     assert_equal '//test.code.org/api/hour/finish', hoc_finish_url(Script.find(Script::HOC_ID))
-    assert_equal '//test.code.org/api/hour/finish', hoc_finish_url(Script.find_by_name(Script::SPECIAL_NAME))
 
     assert_equal '//test.code.org/api/hour/finish/frozen', hoc_finish_url(Script.find_by_name('frozen'))
     assert_equal '//test.code.org/api/hour/finish/course4', hoc_finish_url(Script.find_by_name('course4'))
