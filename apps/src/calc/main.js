2aec75e429592307b3e76f47bd34cd670333b21d
Brent Van Minnen <bjvanminnen@gmail.com>
2015-01-12 16:04:13 -0800

    singleton for eval and calc

--- apps/src/calc/main.js
+++ apps/src/calc/main.js
@@ -1,4 +1,8 @@
 var appMain = require('../appMain');
+// TODO (br-pair): We're doing this so that other apps can still have
+// in the global namespace, while ensuring that we don't. Ultimately nobody
+// should have it, and we can remove this.
+window.StudioApp = undefined;
 window.Calc = require('./calc');
 var blocks = require('./blocks');
 var skins = require('../skins');
