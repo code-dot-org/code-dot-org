645ef9f282d3eec0900b1e3fdc9dc8f73ed2e575
Brad Buchanan <brad@code.org>
2015-01-13 17:11:14 -0800

    Bubble requiredBlocksToFlagCount_ references up and out of FeedbackUtils.

--- apps/src/StudioApp.js
+++ apps/src/StudioApp.js
@@ -824,7 +824,7 @@ StudioApp.prototype.displayFeedback = function(options) {
     options.feedbackType = this.TestResults.EDIT_BLOCKS;
   }
 
-  this.feedback_.displayFeedback(options);
+  this.feedback_.displayFeedback(options, this.requiredBlocksToFlagCount_);
 };
 
 /**
