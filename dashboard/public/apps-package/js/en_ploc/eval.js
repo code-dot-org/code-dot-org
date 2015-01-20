ccacb0d1f3f65739c6107cf5d30f6561a26f095e
Continuous Integration <dev@code.org>
2015-01-20 21:27:05 +0000

    Automatically built.
    commit beadf767375ba63782c30497ffdc98e546455342
    Merge: c53263f 8a63a78
    Author: Josh Caldwell <jpcaldwell@gmail.com>
    Date:   Tue Jan 20 13:01:35 2015 -0800
    
        Merge pull request #1095 from code-dot-org/scale-params
    
        Fix scale param list
    
    commit 8a63a789fbc6cc42e1a93de34fb5ffe5911081b2
    Author: Josh Caldwell <josh@code.org>
    Date:   Tue Jan 20 12:57:50 2015 -0800
    
        Fix scale param list
    
        The functional scale block only listed factor param, added image.

--- dashboard/public/apps-package/js/en_ploc/eval.js
+++ dashboard/public/apps-package/js/en_ploc/eval.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd0fe0bc7776a19555bb6c55b1922888eb33a4793d2507b5d5d5e9abe7581d4c
-size 600465
+oid sha256:5bca07be4760e4b79474e04cd0959a7ace93009e9b582dd1b37c91fce36731b4
+size 600472
--- dashboard/public/apps-package/js/en_us/eval.js
+++ dashboard/public/apps-package/js/en_us/eval.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd7a4682c8a35b6062b155785dd2b7413c62b6fcfc42e2fdf34b0c2da1ad494a
-size 599702
+oid sha256:ce8d1307d14930dd13dc597045f495bbed4b6ba22876b474df2af95903587f78
+size 599709
