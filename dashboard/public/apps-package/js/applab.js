92ddd157456d43569adfe5a2a9b36f3458f92df5
Continuous Integration <dev@code.org>
2015-01-23 23:58:28 +0000

    Automatically built.
    commit 92f8f82a97d5f32c9693ce044db12acfd5f184e3
    Merge: 653c8d7 72c6d91
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Fri Jan 23 15:51:49 2015 -0800
    
        Merge pull request #1133 from code-dot-org/create_netsim_app
    
        Create netsim app
    
    commit 653c8d7bf75b481f5b387abdb713328a586e1227
    Author: Rick Hawkins <rick@alchemydes.com>
    Date:   Fri Jan 23 15:16:04 2015 -0800
    
        Make sure there's always at least one workshop when searching.
    
    commit 62c14c34f3be49832b1f30d5b043f92d1750dd90
    Author: Laurel Fan <laurel@code.org>
    Date:   Fri Jan 23 15:09:49 2015 -0800
    
        delete the wrong travis
    
    commit 4210dd16c8342eaf0280c3f6f5b0e4235e89ed97
    Author: Laurel Fan <laurel@code.org>
    Date:   Fri Jan 23 14:58:38 2015 -0800
    
        wrong travis?
    
    commit 5ce00f47b84dcf76bab243a5a360ff3358707d80
    Author: Laurel Fan <laurel@code.org>
    Date:   Fri Jan 23 14:27:46 2015 -0800
    
        fix tests in development
    
    commit 0deca211f63b10114a7bc5ec13286ffc72c7d7da
    Merge: 3ea0e19 5211243
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Fri Jan 23 14:00:52 2015 -0800
    
        Merge pull request #1138 from code-dot-org/flappy-instructions
    
        fix instructions text on flappy/9
    
    commit 521124339b150ab9045b53c6864cf6f41088ee2f
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Dec 9 17:03:10 2014 -0800
    
        fix instructions text on flappy/9
    
    commit 3ea0e196cfd7988796c917acdd7e8b7e6aba9364
    Merge: afa45c8 260a8c5
    Author: Laurel Fan <laurel@code.org>
    Date:   Fri Jan 23 13:25:21 2015 -0800
    
        Merge staging
    
    commit afa45c84401b8178956c6893a219b2f414cd2115
    Author: Laurel Fan <laurel@code.org>
    Date:   Fri Jan 23 13:17:40 2015 -0800
    
        fix travis
    
    commit 72c6d91d9ded00c863d6b6a372cd97b970ea4775
    Merge: aba83f4 260a8c5
    Author: Brad Buchanan <brad@code.org>
    Date:   Fri Jan 23 12:56:26 2015 -0800
    
        Merge branch 'staging' into create_netsim_app

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d24e88d19a9cf24027c87451109d7acd311454789b51dfaa8af76eb14871066e
+oid sha256:659dc0dd3c1784a2a1cd5f90034d8da34030639ba44619942b465b8de64ecd4b
 size 107598
--- dashboard/public/apps-package/js/calc.js
+++ dashboard/public/apps-package/js/calc.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a74a342b0a236b39ec9cc1ce07cae7a087dff5f8375350e47397c1a06767a98
+oid sha256:b4fef7858e41fc0a759005ab557a4bbfc0ee28fc64625aa0869a387ef60e65bb
 size 34556
