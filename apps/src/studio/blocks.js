7a9940fa4e837e727e277f3412cba9d4becf9f83
Brendan Reville <brendan@code.org>
2015-01-28 10:23:11 +1100

    Two small fixes for the dropdown say box.

--- apps/src/studio/blocks.js
+++ apps/src/studio/blocks.js
@@ -1502,7 +1502,7 @@ exports.install = function(blockly, blockInstallOptions) {
         this.appendDummyInput()
           .appendTitle(msg.saySprite());
       }
-      if (options.choices) {
+      if (options.restrictedDialog) {
         var dropdown = new blockly.FieldDropdown(
           [[msg.saySpriteChoices_1(), msg.saySpriteChoices_1()],
            [msg.saySpriteChoices_2(), msg.saySpriteChoices_2()],
@@ -1536,7 +1536,7 @@ exports.install = function(blockly, blockInstallOptions) {
   };
 
   blockly.Blocks.studio_saySprite = initSayBlock({});
-  blockly.Blocks.studio_saySpriteChoices = initSayBlock({'choices': true});
+  blockly.Blocks.studio_saySpriteChoices = initSayBlock({'restrictedDialog': true});
   blockly.Blocks.studio_saySpriteParams = initSayBlock({'params': true});
   blockly.Blocks.studio_saySpriteParamsTime = initSayBlock({'params': true, 'time': true});
 
