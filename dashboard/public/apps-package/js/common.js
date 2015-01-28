aac99e6876938fb6f5a08f564a71cbe2f71de4a2
Continuous Integration <dev@code.org>
2015-01-28 19:11:37 +0000

    Automatically built.
    commit 5879b8df6ce102fe6d14ca2f464f3faa9694a7be
    Merge: 8d56545 ca71c35
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Wed Jan 28 11:05:06 2015 -0800
    
        Merge pull request #1165 from code-dot-org/applab-init-failures
    
        gracefully recover from a few applab init failures.
    
    commit 8d56545542ce0c3b14b579263ccf664659d61132
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Jan 28 18:43:36 2015 +0000
    
        Automatically built.
    
        commit a789b529d321a6758a87537dd5005ec83b37e88c
        Merge: 8408418 9f344e4
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Wed Jan 28 10:36:24 2015 -0800
    
            Merge pull request #1167 from code-dot-org/jigsawIE9
    
            [Fixes #87130402] couple jigsaw levels not showing goal in IE9
    
        commit 84084189c002017c3560cb9a0ad58d552de593d7
        Author: Continuous Integration <dev@code.org>
        Date:   Wed Jan 28 18:31:08 2015 +0000
    
            Automatically built.
    
            commit e48ac6bb6d3495acaacc0d4d5672249aad819fcb
            Merge: 7fd247f 14231e1
            Author: Chris Pirich <chris@code.org>
            Date:   Wed Jan 28 10:22:53 2015 -0800
    
                Merge pull request #1166 from code-dot-org/applab_easy_level_config
    
                Applab: easy config of palette for each level
    
            commit 7fd247f60dcb2470237985d1c446ec71c48656e8
            Merge: 5177690 bc3fa2a
            Author: Laurel Fan <laurel@code.org>
            Date:   Wed Jan 28 10:08:47 2015 -0800
    
                merge staging
    
    commit a789b529d321a6758a87537dd5005ec83b37e88c
    Merge: 8408418 9f344e4
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Jan 28 10:36:24 2015 -0800
    
        Merge pull request #1167 from code-dot-org/jigsawIE9
    
        [Fixes #87130402] couple jigsaw levels not showing goal in IE9

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -4107,7 +4107,7 @@ module.exports = function(app, levels, options) {
           // for editCode levels, we have to delay the onInitialize callback
           // until the droplet editor has loaded.
           // TODO: build a proper state machine with onEditorReady() callback
-          setTimeout(options.onInitialize, 0);
+          setTimeout(options.onInitialize.bind(options), 0);
         } else {
           options.onInitialize();
         }
@@ -5351,7 +5351,7 @@ StudioApp.prototype.resizeHeaders = function (fullWorkspaceWidth) {
   if (toolboxHeader) {
     if (this.editCode) {
       // If in the droplet editor, but not using blocks, keep categoryWidth at 0
-      if (this.editor.currentlyUsingBlocks) {
+      if (this.editor && this.editor.currentlyUsingBlocks) {
         // Set toolboxWidth based on the block palette width:
         var categories = document.querySelector('.droplet-palette-wrapper');
         toolboxWidth = parseInt(window.getComputedStyle(categories).width, 10);
