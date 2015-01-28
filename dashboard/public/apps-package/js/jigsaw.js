3e22d450b65065b5e87df982cc6ba77b9a71819c
Continuous Integration <dev@code.org>
2015-01-28 18:43:36 +0000

    Automatically built.
    commit a789b529d321a6758a87537dd5005ec83b37e88c
    Merge: 8408418 9f344e4
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Jan 28 10:36:24 2015 -0800
    
        Merge pull request #1167 from code-dot-org/jigsawIE9
    
        [Fixes #87130402] couple jigsaw levels not showing goal in IE9
    
    commit 84084189c002017c3560cb9a0ad58d552de593d7
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Jan 28 18:31:08 2015 +0000
    
        Automatically built.
    
        commit e48ac6bb6d3495acaacc0d4d5672249aad819fcb
        Merge: 7fd247f 14231e1
        Author: Chris Pirich <chris@code.org>
        Date:   Wed Jan 28 10:22:53 2015 -0800
    
            Merge pull request #1166 from code-dot-org/applab_easy_level_config
    
            Applab: easy config of palette for each level
    
        commit 7fd247f60dcb2470237985d1c446ec71c48656e8
        Merge: 5177690 bc3fa2a
        Author: Laurel Fan <laurel@code.org>
        Date:   Wed Jan 28 10:08:47 2015 -0800
    
            merge staging

--- dashboard/public/apps-package/js/jigsaw.js
+++ dashboard/public/apps-package/js/jigsaw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:725e36c146a729c230b0e1e891b8a20a7357ea7bd6fd5cc6dde28fbb8d663047
-size 27475
+oid sha256:05870f16aa89e6e96426f18cac4919457f529ef475153922a65bfced0c89da7f
+size 27551
