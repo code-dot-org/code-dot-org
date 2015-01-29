d630516a4eb5a7de5fd97996746351ec8899672a
Laurel Fan <laurel@code.org>
2015-01-29 12:49:33 -0800

    fix pegasus test

--- pegasus/test/test_only_one_running.rb
+++ pegasus/test/test_only_one_running.rb
@@ -2,6 +2,9 @@ require_relative '../../lib/cdo/pegasus'
 require 'minitest/autorun'
 
 class OnlyOneRunningTest < Minitest::Unit::TestCase
+  setup do
+    @name = "#{__FILE__}_#{rand(100000)}_#{Time.now.to_i}}"
+  end
   def test_initially_true
     assert only_one_running?(__FILE__)
   end
