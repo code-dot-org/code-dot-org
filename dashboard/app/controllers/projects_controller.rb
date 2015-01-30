88c7fc445471c304ed5b40534ba8699d2c0cb698
Laurel Fan <laurel@code.org>
2015-01-30 14:10:04 -0800

    testing and cleanup

--- dashboard/app/controllers/projects_controller.rb
+++ dashboard/app/controllers/projects_controller.rb
@@ -1,8 +1,13 @@
 class ProjectsController < ApplicationController
+  before_filter :authenticate_user!
+  check_authorization
+
   def index
+    authorize! :read, :reports
   end
 
   def template
+    authorize! :read, :reports
     render template: "projects/" + params[:template], layout: nil
   end
 end
