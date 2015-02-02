6eae949dc4715b9e4eece2757d588ee89b6cc7b4
cpirich <chris@code.org>
2015-02-02 11:34:11 -0800

    PR feedback

--- dashboard/app/assets/javascripts/soundEffects.js
+++ dashboard/app/assets/javascripts/soundEffects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9534deeab2a8f3fb3406ae3d1439630af4e768152d3b0c047bb5a2b08c123db5
-size 6907
+oid sha256:af19d885cc9e21707bff86b1045fdf9ba90fde3dfcec098c353adbb320169289
+size 7089
