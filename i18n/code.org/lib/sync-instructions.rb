b76e8e32871d5ae1cde016fa5143e73208048c78
Tanya Parker <tanyacparker@gmail.com>
2015-02-02 17:08:21 -0800

    move pulling instruction strings to scripts

---
+++ i18n/code.org/lib/sync-instructions.rb
@@ -0,0 +1,47 @@
+#! /usr/bin/env ruby
+require 'yaml'
+
+dir = ../../../dashboard/config
+
+def find_instruction(f)
+	pattern = "\"instructions\": \""
+	f.each_line do |line|
+		# does this level have an instruction?
+		if !(line.match(pattern)).nil?
+			# then trim to only get the instruction string
+			line.strip
+			if line[-2,1] == ","
+				line = line[21...-3]
+			else
+				line = line[21...-2]
+			end
+			return line
+		end
+	end
+	nil
+end
+
+def copy_instructions_to_yml(instructions)		
+  File.open("#{dir}/locales/instructions.en.yml", "w+") do |f|
+		f.write(({ "en" => { "data" => { "instructions" => instructions }}}).to_yaml)
+		File.write(f, File.read(f))
+	end
+end
+
+def localize_instructions
+	level_instructions = Hash.new
+
+	Dir.glob("#{dir}/scripts/levels/*.level").each do |file|
+		level = File.basename(file, ".*" ) + "_instruction"
+
+		File.open(file) do |f|
+			instruction = find_instruction(f)
+			if instruction
+				level_instructions[level] = instruction
+			end
+		end
+	end 
+
+	copy_instructions_to_yml(level_instructions)
+end
+localize_instructions
