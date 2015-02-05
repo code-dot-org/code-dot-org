238bd6b97f6045b934a92333e7bbbef9008e34a0
Tanya Parker <tanyacparker@gmail.com>
2015-02-05 13:01:37 -0800

    sync instructions with new quote formatting

--- i18n/code.org/lib/sync-instructions.rb
+++ i18n/code.org/lib/sync-instructions.rb
@@ -1,12 +1,13 @@
 #! /usr/bin/env ruby
 require 'yaml'
+require 'tempfile'
 
 def find_instruction(f)
-  pattern = /^\s*"instructions": "(.*?)",?\n$/
+  pattern = /^\s*"instructions": (".*?"),?\n$/
   f.each_line do |line|
     matches = line.match pattern
     if matches
-      return matches.captures.first.gsub('\"', '"').gsub('\r', '').gsub('\n', '')
+      return matches.captures.first
     end
   end
   nil
@@ -17,6 +18,15 @@ def copy_instructions_to_yml(instructions)
     f.write(({ "en" => { "data" => { "instructions" => instructions }}}).to_yaml)
     File.write(f, File.read(f))
   end
+end
+
+def reformat_quotes
+  temp_file = Tempfile.new("tempinstructions.yml")
+  File.open("../../dashboard/config/locales/instructions.en.yml", "r") do |f|
+    f.each_line { |line| temp_file.puts line.gsub("'\"", '"').gsub("\"'", '"').gsub("''", "'")}
+  end
+  temp_file.close
+  FileUtils.mv(temp_file.path, "../../dashboard/config/locales/instructions.en.yml")
 end
 
 def localize_instructions
@@ -33,5 +43,6 @@ def localize_instructions
     end
   end 
   copy_instructions_to_yml(level_instructions)
+  reformat_quotes
 end
 localize_instructions
