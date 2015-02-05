620c169cf2948cbf94c365f29bae7542ef799552
Continuous Integration <dev@code.org>
2015-02-05 22:53:42 +0000

    Automatically built.
    commit 7f30d928875d5082f8d4b341ab0366f11671e73f
    Merge: afffd4e 14863bc
    Author: Chris Pirich <chris@code.org>
    Date:   Thu Feb 5 14:46:27 2015 -0800
    
        Merge pull request #1223 from code-dot-org/droplet_category_changes
    
        change droplet categories and applab block categorization
    
    commit afffd4e2eefe006120b7e256d69d11ba4fd068d4
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Feb 5 22:33:09 2015 +0000
    
        Automatically built.
    
        commit 26850e25ece89e70e844f4a5c23309cf7c2a16bb
        Merge: a579925 c5c1696
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Thu Feb 5 14:26:11 2015 -0800
    
            Merge pull request #1226 from code-dot-org/flyout_visibility_separate_css
    
            Add separate CSS tag for flyout visibility
    
        commit c5c16961183f586ed8ae07b2d85c866887853d79
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Thu Feb 5 14:25:11 2015 -0800
    
            Add separate CSS tag  with "display: none !important;" for flyout visibility
            - Add comments for softHide and softShow

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba808435dacf7cb4cccb7280c0a859d821bcc0387108262490a07d271a594548
-size 529824
+oid sha256:ef3de87df3adcd4605b65c2ca53f458dd289b423b3cdb7ac8d1bd7dabc363651
+size 530427
