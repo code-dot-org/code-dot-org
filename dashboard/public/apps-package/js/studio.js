61c22f63bb28a73f1dbd01d767206975ea98d5dc
Continuous Integration <dev@code.org>
2015-02-05 04:37:52 +0000

    Automatically built.
    commit 366486e5c742c00362d4586a9eb164b62c1f327c
    Merge: b7e7826 07c84d4
    Author: Josh Caldwell <jpcaldwell@gmail.com>
    Date:   Wed Feb 4 20:30:24 2015 -0800
    
        Merge pull request #1215 from code-dot-org/msm_game_blocks
    
        Functional Start Blocks for Big Game
    
    commit b7e7826d3837f099c9e9af2e0aa62fbd793f70cf
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Feb 4 17:29:18 2015 -0800
    
        Remove case conflicted script

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1426291985bbe71566c33d2c4b465d9d66e25cf76fc3885fbc75707e3c7eeac7
-size 269217
+oid sha256:ab1a0a07217f450c9ed778b03f1742fc426315f514457cc20f351c119b9c2be7
+size 270939
