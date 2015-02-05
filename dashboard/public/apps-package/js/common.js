65460cb9fb23c8d86a856a162d46a9a884a254f6
Continuous Integration <dev@code.org>
2015-02-05 22:09:43 +0000

    Automatically built.
    commit 2a5e6c49c177aa596ccc47d4e8175b2d3e76eadd
    Merge: a07f9cb 5bc6249
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Feb 5 14:02:51 2015 -0800
    
        Merge pull request #1213 from code-dot-org/deduplicate_touch_events
    
        Deduplicate touch event registration
    
    commit a07f9cba3fd091cf5e649bc57b47c70954435783
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Thu Feb 5 13:48:20 2015 -0800
    
        fix zendesk doc formatting

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaccaf28ca0fbceb8792146f305a3377b70f258a619b6cc8f119e2db5090adf5
-size 529812
+oid sha256:ba808435dacf7cb4cccb7280c0a859d821bcc0387108262490a07d271a594548
+size 529824
