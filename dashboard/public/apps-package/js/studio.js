38f38a2708dd9d9217cb6a12fef08a15bd208547
Continuous Integration <dev@code.org>
2015-02-06 02:13:44 +0000

    Automatically built.
    commit 492f5eb0181390c2fae792b5201edeb2f90906ba
    Merge: 46f56c1 348cec6
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Feb 5 18:06:49 2015 -0800
    
        Merge pull request #1232 from code-dot-org/studioBlocks
    
        add some new studio blocks
    
    commit 46f56c14ef2740100a7ef28ff53ec739269ea6fa
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Thu Feb 5 17:57:26 2015 -0800
    
        update failing tests debugging to DoTW doc

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab1a0a07217f450c9ed778b03f1742fc426315f514457cc20f351c119b9c2be7
-size 270939
+oid sha256:e54604e4edd433e20992f693ee3566ebe3b3087a7d67fe68c9dba69f72bfd54f
+size 274012
