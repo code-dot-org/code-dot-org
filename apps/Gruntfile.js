d47fd4dc37145403f540c0291713da5da0a7fa01
Brad Buchanan <brad@code.org>
2015-02-06 14:58:14 -0800

    Give NetSimTables a simple local cache and change detection.

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -179,7 +179,7 @@ config.lodash = {
       'include': [
         'debounce', 'reject', 'map', 'value', 'range', 'without', 'sample',
         'create', 'flatten', 'isEmpty', 'wrap', 'size', 'bind', 'contains',
-        'last', 'clone']
+        'last', 'clone', 'isEqual']
     }
   }
 };
