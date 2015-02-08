38d59e7f3b1b5bd4d7c140368ef982b66e10a28d
Josh Lory <josh.lory@code.org>
2015-02-07 18:39:00 -0800

    Remove length error message

--- dashboard/public/pixelation/pixelationScript4.js
+++ dashboard/public/pixelation/pixelationScript4.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8ce806b58bf9d4091f49101252a7ab4cf550e74bbf80510faef757773ad79b7
-size 11175
+oid sha256:8af1ec57c41417c4c50f57344ef479fe42556a21f633867588cf3c81a6797ff1
+size 10934
