bf521ee0b660a6165ac64c092fd5bd1021a3c71e
Josh Lory <josh.lory@code.org>
2015-02-07 21:38:48 -0800

    Add auto-zoom and remove zoom slider

--- dashboard/public/pixelation/pixelationScript4.js
+++ dashboard/public/pixelation/pixelationScript4.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11c67cf65d081e41ce594b9dddfeed2cb7d25c2d6a4eb35889fe3afa5488ee68
-size 10194
+oid sha256:f5ffda5cecae5e587a86d425a60d3bc4a5a68b902c05a83a4d1a99a2c42c5099
+size 10064
