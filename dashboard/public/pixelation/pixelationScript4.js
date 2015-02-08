3ddefa31a20f3e9eb29a92a9e47b78fbd2c4d046
Josh Lory <josh.lory@code.org>
2015-02-07 19:25:58 -0800

    More code cleanup, keep image centered, and remove pixel border checkbox

--- dashboard/public/pixelation/pixelationScript4.js
+++ dashboard/public/pixelation/pixelationScript4.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8af1ec57c41417c4c50f57344ef479fe42556a21f633867588cf3c81a6797ff1
-size 10934
+oid sha256:11c67cf65d081e41ce594b9dddfeed2cb7d25c2d6a4eb35889fe3afa5488ee68
+size 10194
