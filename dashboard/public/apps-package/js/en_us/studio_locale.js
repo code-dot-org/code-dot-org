d26b32c305d9983c164e37bf22257264e28d9d08
Continuous Integration <dev@code.org>
2015-02-09 03:30:41 +0000

    Automatically built.
    commit 491d6a2df25970a71aa9905f2377a5d2693ff13d
    Merge: 437b472 98ac020
    Author: Josh Caldwell <jpcaldwell@gmail.com>
    Date:   Sun Feb 8 19:22:51 2015 -0800
    
        Merge pull request #1242 from code-dot-org/functional_start_block_titles
    
        Functional start block titles
    
    commit 98ac02055d28ca60803a1104fa54d3248fcfca14
    Author: Josh Caldwell <josh@code.org>
    Date:   Sun Feb 8 19:21:25 2015 -0800
    
        Change order of sprite args
    
    commit bc4f59da1282acc774229b26ddece5f6f1978d73
    Author: Josh Caldwell <josh@code.org>
    Date:   Sun Feb 8 14:36:01 2015 -0800
    
        Fixed some function titles

--- dashboard/public/apps-package/js/en_us/studio_locale.js
+++ dashboard/public/apps-package/js/en_us/studio_locale.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0d5dda3aceda52ca5f576b637f20633a3b66aec974ce14058161b3a1fe8fbb0
-size 24619
+oid sha256:97dde93488a573b06cb372df068785342d91cc4a91723815d69b7cf7a16b6a23
+size 24605
--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:659d11c85455324f0682d13fc3679d39556c62e74920fd645ac9e0b89aa782e0
-size 274013
+oid sha256:f240fe60be3d0de5df23b38b44da7aba38e06dd192d2f72d0a4d0d886cd191f1
+size 273997
