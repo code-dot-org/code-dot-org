d0a207edba4a4e1e2fced6c11c463346eac0f4ca
Continuous Integration <dev@code.org>
2015-02-10 23:22:06 +0000

    Automatically built.
    commit 5e49d285d9b536e3948fed6c310e01ac32d9db63
    Merge: 264ab7a 007894d
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Tue Feb 10 15:13:23 2015 -0800
    
        Merge pull request #1264 from code-dot-org/handle_audiocontext_failure
    
        Use shared Sounds for StudioApps and Blockly Core (was: handle failures in Chrome AudioContext initialization [resolves #88069582])
    
    commit 007894d14cef0f17777da9dc55cb3317c6d3255d
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Tue Feb 10 14:33:42 2015 -0800
    
        Rename filename to URLs
    
    commit 264ab7a94a91b9ea819b6970b090c13993c3cb63
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Tue Feb 10 14:31:27 2015 -0800
    
        removing 8a.level
    
    commit b6c37052290fa37a21f30a1de38b02ce5e3f32b1
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Tue Feb 10 14:27:31 2015 -0800
    
        Add some clarifying comments
    
    commit 4619ba6bcf17460356c7127bd0bb26b2ba558559
    Author: Geoffrey Elliott <geoffrey@code.org>
    Date:   Tue Feb 10 14:25:58 2015 -0800
    
        Docs for deploy-config - a command that shows you all of the settings in place on the current instance.
    
    commit aefec032d3bf69130e1702c3c793a01b6cb409ba
    Author: Geoffrey Elliott <geoffrey@code.org>
    Date:   Tue Feb 10 14:22:47 2015 -0800
    
        docs for  count-lines-of-code-from-milestone-logs
    
    commit 53e3b6984303eaa50996856c5ed7ab7476dd942e
    Author: Geoffrey Elliott <geoffrey@code.org>
    Date:   Tue Feb 10 14:13:46 2015 -0800
    
        Documentation for aptitude-full-upgrade
    
    commit b7f4102f301ca3acbe36cfc44a3258093539a508
    Merge: 5232d1c 9ef7782
    Author: Rick Hawkins <rick@alchemydes.com>
    Date:   Tue Feb 10 14:09:46 2015 -0800
    
        Merge branch 'staging' of https://github.com/code-dot-org/code-dot-org into staging

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e584bd1a0e49f3c58e790c7faa1f374c2f9b7bd6b1b0b0895ddae2a75768f5e2
-size 531973
+oid sha256:827ccff6ef90d7b748dc3e25885893089290237a1814eec0cf145850405f22b5
+size 532124
