2c00ad7fc7dc1ab82a5bf5f959acc1e9d42d8109
Brian Jordan <bcjordan@gmail.com>
2015-02-10 14:27:31 -0800

    Add some clarifying comments

--- dashboard/app/assets/javascripts/soundEffects.js
+++ dashboard/app/assets/javascripts/soundEffects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa4f744ef41dbf5181d0ffa6380a2ab58861b8837cf7be50114e2124cc966f43
-size 8487
+oid sha256:628af081eedb30d594d2cd4b305c825547a140398404b1097996f082f431d73d
+size 8711
