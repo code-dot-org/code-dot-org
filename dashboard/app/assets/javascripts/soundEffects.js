d909fd9b957fc80a1ae07d9fdfb2ce531de449b2
Brian Jordan <bcjordan@gmail.com>
2015-02-10 13:10:35 -0800

    Use only one Audio Player - Add AudioPlayer interface for Sounds - Pass AudioPlayer into blockly core for UI sounds - Remove Blockly core audio playback logic - Move array of sound files audio registry helper from StudioApp into Sounds

--- dashboard/app/assets/javascripts/soundEffects.js
+++ dashboard/app/assets/javascripts/soundEffects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af19d885cc9e21707bff86b1045fdf9ba90fde3dfcec098c353adbb320169289
-size 7089
+oid sha256:fa4f744ef41dbf5181d0ffa6380a2ab58861b8837cf7be50114e2124cc966f43
+size 8487
