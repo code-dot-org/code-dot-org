348c6b7d129f7fa6372a2ddbea96d26bd5ae1126
Josh Lory <josh.lory@code.org>
2015-02-10 13:00:06 -0800

    Add 'Actual size' checkbox

--- dashboard/public/pixelation/pixelationScript4.js
+++ dashboard/public/pixelation/pixelationScript4.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39e662856e90b54c51fc6f6985010e356f1da7084e9a662546409038fba7b3b3
-size 10433
+oid sha256:b5561805ebf9cc2c74ca2bb790fa9902acdd2b5dbd576545cbb303762940e9f4
+size 10571
