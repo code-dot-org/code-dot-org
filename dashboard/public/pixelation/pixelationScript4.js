cd1290c8e256fb02df7805ce8b08134ba0c75d49
Josh Lory <josh.lory@code.org>
2015-02-10 17:33:48 -0800

    Use `options.version` to change widget behavior for version: 1, 2, 3

--- dashboard/public/pixelation/pixelationScript4.js
+++ dashboard/public/pixelation/pixelationScript4.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d602bb91bab53d15b36ae7fb28d68f24883bfc2ecfa3c43ff8b0c09fb410f08
-size 10572
+oid sha256:c8665e7c9c1f2e2ed5088682c3990ba2d17426c2235b2d57e0ed80a120ac09eb
+size 11195
