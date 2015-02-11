6f068908547b91999aba1dab5f47182e4f603c89
Josh Lory <josh.lory@code.org>
2015-02-11 15:15:49 -0800

    Code review feedback

--- dashboard/public/pixelation/pixelation.js
+++ dashboard/public/pixelation/pixelation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8665e7c9c1f2e2ed5088682c3990ba2d17426c2235b2d57e0ed80a120ac09eb
-size 11195
+oid sha256:392bf4c2c174c5c010607b1407a96a8ccd8dd50b297cfed1583a5f423d49b105
+size 10963
