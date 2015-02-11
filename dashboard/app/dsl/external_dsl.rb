0ff3f17d26dc3893fd4d6ead00665aff393be451
Josh Lory <josh.lory@code.org>
2015-02-10 16:46:57 -0800

    Add custom-key support to the 'external' DSL that maps to a JS hash

--- dashboard/app/dsl/external_dsl.rb
+++ dashboard/app/dsl/external_dsl.rb
@@ -1,10 +1,14 @@
 class ExternalDSL < ContentDSL
   def initialize
-    @hash = {href: ''}
+    @hash = {href: '', options: {}}
   end
 
   def href(url)
     @hash[:href] = url
+  end
+
+  def method_missing(key, *args)
+    @hash[:options][key.to_sym] = args.first
   end
 
   def parse_output
