898feac104632f8242fab22c380da101222b33bc
Continuous Integration <dev@code.org>
2015-02-11 21:29:43 +0000

    Automatically built.
    commit 00005d18725dadd631e0488abe952b0759773528
    Merge: 057d875 335488f
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Feb 11 13:21:35 2015 -0800
    
        Merge pull request #1277 from code-dot-org/calcErrors
    
        Calc errors
    
    commit 335488f473a10aaff1cf7084aeffe7ef0defb372
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Wed Feb 11 13:21:06 2015 -0800
    
        remove unused param
    
    commit 057d875fdd9571e8dd1fcca22c79f2da6560c3d5
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Feb 11 21:17:23 2015 +0000
    
        Automatically built.
    
        commit 4f0562a70f33be673bfe3743f5e34e46c72a335d
        Merge: 3918fa5 f8312da
        Author: Chris Pirich <chris@code.org>
        Date:   Wed Feb 11 13:08:57 2015 -0800
    
            Merge pull request #1275 from code-dot-org/droplet_global_functions
    
            re-implement droplet global functions, plus prompt, and other block changes
    
        commit f8312da15f4266a00a6db96487e481b830f593f2
        Author: cpirich <chris@code.org>
        Date:   Wed Feb 11 12:36:58 2015 -0800
    
            add var
    
        commit 3918fa523708c9868f7cf8a03b0f1e919f15de2d
        Author: Tanya Parker <tanyacparker@gmail.com>
        Date:   Wed Feb 11 11:25:36 2015 -0800
    
            add faq how we count to hoc.com
    
    commit 4f0562a70f33be673bfe3743f5e34e46c72a335d
    Merge: 3918fa5 f8312da
    Author: Chris Pirich <chris@code.org>
    Date:   Wed Feb 11 13:08:57 2015 -0800
    
        Merge pull request #1275 from code-dot-org/droplet_global_functions
    
        re-implement droplet global functions, plus prompt, and other block changes
    
    commit f8312da15f4266a00a6db96487e481b830f593f2
    Author: cpirich <chris@code.org>
    Date:   Wed Feb 11 12:36:58 2015 -0800
    
        add var
    
    commit 8e28f2a3a9f7a90e8332db60e2ab9ebf61135c3d
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Wed Feb 11 11:09:10 2015 -0800
    
        different message if it's compute block with empty input
    
    commit 3918fa523708c9868f7cf8a03b0f1e919f15de2d
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Wed Feb 11 11:25:36 2015 -0800
    
        add faq how we count to hoc.com

--- dashboard/public/apps-package/js/en_us/eval_locale.js
+++ dashboard/public/apps-package/js/en_us/eval_locale.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c022e1fa2e1b94002f5bf7a2733125ece701163e0d771e45c3b1bb5bda6fed6e
-size 7503
+oid sha256:9052c08e1f6751559c0ffb0bd16ba1f1f9a4cb6fdb3f2bf224794119f38647e7
+size 7590
