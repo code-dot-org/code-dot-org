5b55ab2fba86d935c97bdb05c73e2c70ee260182
Brad Buchanan <brad@code.org>
2015-02-11 12:46:25 -0800

    Extract NetSimTable to own file, test NetSimTable

---
+++ apps/src/netsim/NetSimTable.js
@@ -0,0 +1,186 @@
+/**
+ * Copyright 2015 Code.org
+ * http://code.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* jshint
+ funcscope: true,
+ newcap: true,
+ nonew: true,
+ shadow: false,
+ unused: true,
+
+ maxlen: 90,
+ maxparams: 3,
+ maxstatements: 200
+ */
+'use strict';
+
+var _ = require('../utils').getLodash();
+var ObservableEvent = require('./ObservableEvent');
+
+/**
+ * Maximum time (in milliseconds) that tables should wait between full cache
+ * updates from the server.
+ * @type {number}
+ */
+var POLLING_DELAY_MS = 5000;
+
+/**
+ * Wraps the app storage table API in an object with local
+ * cacheing and callbacks, which provides a notification API to the rest
+ * of the NetSim code.
+ * @param {!SharedTable} storageTable - The remote storage table to wrap.
+ * @constructor
+ */
+var NetSimTable = module.exports = function (storageTable) {
+  /**
+   * Actual API to the remote shared table.
+   * @type {SharedTable}
+   * @private
+   */
+  this.remoteTable_ = storageTable;
+
+
+  /**
+   * Event that fires when full table updates indicate a change,
+   * when rows are added, or when rows are removed, or when rows change.
+   * @type {ObservableEvent}
+   */
+  this.tableChangeEvent = new ObservableEvent();
+
+  /**
+   * Store table contents locally, so we can detect when changes occur.
+   * @type {Object}
+   * @private
+   */
+  this.cache_ = {};
+
+  /**
+   * Unix timestamp for last time this table's cache contents were fully
+   * updated.  Used to determine when to poll the server for changes.
+   * @type {number}
+   * @private
+   */
+  this.lastFullUpdateTime_ = 0;
+};
+
+NetSimTable.prototype.readAll = function (callback) {
+  var self = this;
+  this.remoteTable_.readAll(function (data) {
+    callback(data);
+    if (data !== null) {
+      self.fullCacheUpdate_(data);
+    }
+  });
+};
+
+NetSimTable.prototype.read = function (id, callback) {
+  var self = this;
+  this.remoteTable_.read(id, function (data) {
+    callback(data);
+    if (data !== undefined) {
+      self.updateCacheRow_(id, data);
+    }
+  });
+};
+
+NetSimTable.prototype.create = function (value, callback) {
+  var self = this;
+  this.remoteTable_.create(value, function (data) {
+    callback(data);
+    if (data !== undefined) {
+      self.addRowToCache_(data);
+    }
+  });
+};
+
+NetSimTable.prototype.update = function (id, value, callback) {
+  var self = this;
+  this.remoteTable_.update(id, value, function (success) {
+    callback(success);
+    if (success) {
+      self.updateCacheRow_(id, value);
+    }
+  });
+};
+
+NetSimTable.prototype.delete = function (id, callback) {
+  var self = this;
+  this.remoteTable_.delete(id, function (success) {
+    callback(success);
+    if (success) {
+      self.removeRowFromCache_(id);
+    }
+  });
+};
+
+NetSimTable.prototype.fullCacheUpdate_ = function (allRows) {
+  // Rebuild entire cache
+  var newCache = allRows.reduce(function (prev, currentRow) {
+    prev[currentRow.id] = currentRow;
+    return prev;
+  }, {});
+
+  // Check for changes, if anything changed notify all observers on table.
+  if (!_.isEqual(this.cache_, newCache)) {
+    this.cache_ = newCache;
+    this.tableChangeEvent.notifyObservers(this.arrayFromCache_());
+  }
+
+  this.lastFullUpdateTime_ = Date.now();
+};
+
+NetSimTable.prototype.addRowToCache_ = function (row) {
+  this.cache_[row.id] = row;
+  this.tableChangeEvent.notifyObservers(this.arrayFromCache_());
+};
+
+NetSimTable.prototype.removeRowFromCache_ = function (id) {
+  if (this.cache_[id] !== undefined) {
+    delete this.cache_[id];
+    this.tableChangeEvent.notifyObservers(this.arrayFromCache_());
+  }
+};
+
+NetSimTable.prototype.updateCacheRow_ = function (id, row) {
+  var oldRow = this.cache_[id];
+  var newRow = row;
+
+  // Manually apply ID which should be present in row.
+  newRow.id = id;
+
+  if (!_.isEqual(oldRow, newRow)) {
+    this.cache_[id] = newRow;
+    this.tableChangeEvent.notifyObservers(this.arrayFromCache_());
+  }
+};
+
+NetSimTable.prototype.arrayFromCache_ = function () {
+  var result = [];
+  for (var k in this.cache_) {
+    if (this.cache_.hasOwnProperty(k)) {
+      result.push(this.cache_[k]);
+    }
+  }
+  return result;
+};
+
+/** Polls server for updates, if it's been long enough. */
+NetSimTable.prototype.tick = function () {
+  if (Date.now() - this.lastFullUpdateTime_ > POLLING_DELAY_MS) {
+    this.readAll(function () {});
+  }
+};
