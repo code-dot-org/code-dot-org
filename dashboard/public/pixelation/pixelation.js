0d87b67b1ba1163ff516df0cc25c50b4d650ddba
Josh Lory <josh.lory@code.org>
2015-02-11 15:42:41 -0800

    Typo in c7f36549b17617219559da76c7f3d1ae5dbbbeaf

--- dashboard/public/pixelation/pixelation.js
+++ dashboard/public/pixelation/pixelation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:392bf4c2c174c5c010607b1407a96a8ccd8dd50b297cfed1583a5f423d49b105
-size 10963
+oid sha256:1607cdcaf46d79f7522acf9428f820a60e9031102aceb92ce404be51603044b9
+size 10964
