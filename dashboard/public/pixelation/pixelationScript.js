803dad7046dceec54387f3182397203519805b09
Josh Lory <josh.lory@code.org>
2015-02-10 17:35:22 -0800

    Remove unused assets (behavior has been merged into a single page)
    Images have been replaced with markup for future i18n.

--- dashboard/public/pixelation/pixelationScript.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9f1577b4f361b444a9e53aba8d920033abbde0296539f63c132be3d43f409fbf
-size 1507
--- dashboard/public/pixelation/pixelationScript3.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:27eb1299e3b648b02e2cfb7117b59bfee283618f6a32a98bbf50d3dd3b0a5544
-size 16043
