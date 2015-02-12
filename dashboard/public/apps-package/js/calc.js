279e950a7345ab615b189a42dcc23c21c24f6b14
Continuous Integration <dev@code.org>
2015-02-12 01:17:42 +0000

    Automatically built.
    commit 234a1f941f0b3023e622f4bb107af3d2e5ddf1d5
    Merge: e50dfc2 ba47755
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Feb 11 17:09:11 2015 -0800
    
        Merge pull request #1282 from code-dot-org/calcGenericError
    
        change calc's generic error to make sense
    
    commit ba477557648d39dbe00274b1d8c78b795c3d6155
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Wed Feb 11 17:00:44 2015 -0800
    
        change calc's generic error to make sense

--- dashboard/public/apps-package/js/calc.js
+++ dashboard/public/apps-package/js/calc.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8957f818719a1faee6367aa7458607b0785494809b82be9bce234597cb54dd0b
-size 52712
+oid sha256:a1019fab248a127bdeb0d424bb4abff4ea3f36558cd4c22ca800b47a950699a1
+size 52923
