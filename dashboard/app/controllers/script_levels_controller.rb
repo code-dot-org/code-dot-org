45a5af25242a90e425fc0920f9917ec8496253fd
Josh Lory <josh.lory@code.org>
2015-02-14 15:10:20 -0800

    Turn autosave and autoload on by default
    Puzzles will now load the last attempt for logged in users.

--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -99,12 +99,12 @@ private
 
     return unless current_user
 
-    if params[:load_previous]
-      @start_blocks = current_user.last_attempt(@level).try(:level_source).try(:data)
-    elsif params[:level_source_id]
+    if params[:level_source_id]
       level_source = LevelSource.find(params[:level_source_id])
       # we do multiple level projects, so we don't check that the level_source.level_id matches the loaded level
       @start_blocks = level_source.data
+    else
+      @start_blocks = current_user.last_attempt(@level).try(:level_source).try(:data)
     end
   end
 
