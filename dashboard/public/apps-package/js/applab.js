d7b6208e26b69e59eda2de2b7e604bdc34b8e31f
Continuous Integration <dev@code.org>
2015-02-14 05:28:47 +0000

    Automatically built.
    commit 3cfea508a309090325f7c4f3b64eff6d2cf77e30
    Merge: 24bdb7c 0bc347b
    Author: Chris Pirich <chris@code.org>
    Date:   Fri Feb 13 21:21:06 2015 -0800
    
        Merge pull request #1296 from code-dot-org/applab_body_events
    
        applab: allow attaching events to the entire app surface
    
    commit 0bc347b5b3da005c2c110a6f5ee4d5fca515738b
    Author: cpirich <chris@code.org>
    Date:   Fri Feb 13 15:21:27 2015 -0800
    
        allow attaching events to whole app surface

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9eb27c41b03a61757f160cee4059d251acbc0cc8b29558d882513e47b71fc742
-size 133219
+oid sha256:e6e88d970c5956646abf2c11f351dd28a8a70d690e03cdcae936ffb6eca5a7ab
+size 133653
