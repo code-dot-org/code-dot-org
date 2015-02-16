027a7109186f2a40b219b4bf214cb0b2e1ad3aa7
Brian Jordan <bcjordan@gmail.com>
2015-02-16 15:38:44 -0800

    Remove unnecessary import

--- apps/src/functionalBlockUtils.js
+++ apps/src/functionalBlockUtils.js
@@ -1,6 +1,3 @@
-var utils = require('./utils');
-var _ = utils.getLodash();
-
 module.exports.colors = Blockly.FunctionalTypeColors;
 module.exports.initTitledFunctionalBlock = Blockly.FunctionalBlockUtils.initTitledFunctionalBlock;
 module.exports.installFunctionalApiCallBlock = Blockly.FunctionalBlockUtils.installFunctionalApiCallBlock;
