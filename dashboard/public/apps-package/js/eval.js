5690b12c9f0913c8d926bed3bdcc54e8bc46def5
Continuous Integration <dev@code.org>
2015-02-17 20:40:50 +0000

    Automatically built.
    commit bd2abdb135242f6bee305a89cd5b69844b28182f
    Merge: 28df990 3dc4c6d
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Feb 17 12:33:12 2015 -0800
    
        Merge pull request #1328 from code-dot-org/evalFreeplay
    
        result is true for freeplay
    
    commit 3dc4c6d5ec9796be0bfbaa69cd7b73d9d6f131fb
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Feb 17 12:28:36 2015 -0800
    
        result is true for freeplay

--- dashboard/public/apps-package/js/eval.js
+++ dashboard/public/apps-package/js/eval.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dea8450330ebd1520f148d6c76833e30ba2266e77e55c69441814d4df9301da3
-size 42822
+oid sha256:549d4a89b3a3ca902a612e26e0406383de60b3f222cc8a50cdc5778b7b6a3bc3
+size 42848
