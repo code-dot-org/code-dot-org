c8b05195a63f94d12e921e5177e8db63d7c78d01
Continuous Integration <dev@code.org>
2015-02-18 03:41:47 +0000

    Automatically built.
    commit f30a2c7850e787570be1db023ef9497dc621c89f
    Merge: 891c65e a52b073
    Author: cpirich <chris@code.org>
    Date:   Tue Feb 17 19:33:29 2015 -0800
    
        Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
    commit 891c65e7e4508872f6c47dd4f19712d937b5aa31
    Author: cpirich <chris@code.org>
    Date:   Tue Feb 17 19:33:19 2015 -0800
    
        applab: fix bug where penColor() didn't impact dot() while pen was up

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b530f7d468b270b4e6fc1fb47ba7baac52590568b32ecd0710e47909127b39f
-size 135092
+oid sha256:8ebf0b992bf0aaa2d726da8a7577cdd3b8aea090301eb0da5c6a922269392c62
+size 135368
