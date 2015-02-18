7ad58ae249742d97cd95b6db0fa81860673c4547
Brad Buchanan <brad@code.org>
2015-02-18 12:53:11 -0800

    Initial ascii/binary conversion

--- apps/src/netsim/NetSimSendWidget.js
+++ apps/src/netsim/NetSimSendWidget.js
@@ -15,6 +15,38 @@
 var markup = require('./NetSimSendWidget.html');
 var dom = require('../dom');
 
+function binaryStringToUnsignedInteger(binaryString) {
+  return parseInt(binaryString, 2);
+}
+
+function unsignedIntegerToBinaryString(integer, size) {
+  var binary = integer.toString(2);
+  while (binary.length < size) {
+    binary = '0' + binary;
+  }
+  // TODO: Deal with overflow?
+  return binary;
+}
+
+function binaryStringToAscii(binaryString) {
+  var result = '';
+  for (var i = 0; i < binaryString.length; i += 8) {
+    var chunk = binaryString.slice(i, i+8);
+    while (chunk.length < 8) {
+      chunk += '0';
+    }
+    result += String.fromCharCode(parseInt(chunk, 2));
+  }
+  return result;
+}
+
+function asciiToBinaryString(ascii) {
+  var result = '';
+  for (var i = 0; i < ascii.length; i++) {
+    result += unsignedIntegerToBinaryString(ascii.charCodeAt(i), 8);
+  }
+  return result;
+}
 
 /**
  * Generator and controller for message sending view.
@@ -30,6 +62,8 @@ var NetSimSendWidget = module.exports = function (connection) {
   this.connection_ = connection;
   this.connection_.statusChanges
       .register(this.onConnectionStatusChange_.bind(this));
+
+  this.packetBinary = '';
 };
 
 /**
@@ -54,7 +88,28 @@ NetSimSendWidget.createWithin = function (element, connection) {
 NetSimSendWidget.prototype.bindElements_ = function () {
   this.rootDiv_ = $('#netsim_send_widget');
   this.toAddressTextbox_ = this.rootDiv_.find('#to_address');
-  this.payloadTextbox_ = this.rootDiv_.find('#binary_payload');
+  this.fromAddressTextbox_ = this.rootDiv_.find('#from_address');
+
+  this.binaryPayloadTextbox_ = this.rootDiv_.find('#binary_payload');
+  this.binaryPayloadTextbox_.change(this.onBinaryPayloadChange_.bind(this));
+  this.binaryPayloadTextbox_.bind('keyup', this.onBinaryPayloadChange_.bind(this));
+  this.binaryPayloadTextbox_.bind('keypress', function (event) {
+    var charCode = !event.charCode ? event.which : event.charCode;
+
+    // Prevent VISIBLE characters that aren't on our whitelist
+    if (charCode >= 32 && charCode <= 126) {
+      var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+      if (!/[01]/.test(key)) {
+        event.preventDefault();
+        return false;
+      }
+    }
+  }.bind(this));
+
+  this.asciiPayloadTextbox_ = this.rootDiv_.find('#ascii_payload');
+  this.asciiPayloadTextbox_.change(this.onAsciiPayloadChange_.bind(this));
+  this.asciiPayloadTextbox_.bind('keyup', this.onAsciiPayloadChange_.bind(this));
+
   this.sendButton_ = this.rootDiv_.find('#send_button');
 
   dom.addClickTouchEvent(this.sendButton_[0], this.onSendButtonPress_.bind(this));
@@ -69,12 +124,49 @@ NetSimSendWidget.prototype.onConnectionStatusChange_ = function () {
 
 };
 
+NetSimSendWidget.prototype.onBinaryPayloadChange_ = function () {
+  this.packetBinary = unsignedIntegerToBinaryString(this.toAddressTextbox_.val(), 4);
+  this.packetBinary += unsignedIntegerToBinaryString(this.fromAddressTextbox_.val(), 4);
+  this.packetBinary += this.binaryPayloadTextbox_.val();
+  this.refresh();
+};
+
+NetSimSendWidget.prototype.onAsciiPayloadChange_ = function () {
+  this.packetBinary = unsignedIntegerToBinaryString(this.toAddressTextbox_.val(), 4);
+  this.packetBinary += unsignedIntegerToBinaryString(this.fromAddressTextbox_.val(), 4);
+  this.packetBinary += asciiToBinaryString(this.asciiPayloadTextbox_.val());
+  this.refresh();
+};
+
 /** Update send widget display */
 NetSimSendWidget.prototype.refresh = function () {
   // Non-interactive right now
   this.rootDiv_.find('#from_address').val('?');
   this.rootDiv_.find('#packet_index').val(1);
   this.rootDiv_.find('#packet_count').val(1);
+
+  // Extract to address from packetBinary
+  var toAddressBinary = this.packetBinary.slice(0, 4);
+  while (toAddressBinary.length < 4) {
+    toAddressBinary += '0';
+  }
+  var toAddress = binaryStringToUnsignedInteger(toAddressBinary);
+  this.toAddressTextbox_.val(toAddress);
+
+  // Extract from address from packetBinary
+  var fromAddressBinary = this.packetBinary.slice(4, 8);
+  while (fromAddressBinary.length < 4) {
+    fromAddressBinary += '0';
+  }
+  var fromAddress = binaryStringToUnsignedInteger(fromAddressBinary);
+  this.fromAddressTextbox_.val(fromAddress);
+
+  // Extract message body from packetBinary
+  var binaryPayload = this.packetBinary.slice(8);
+  this.binaryPayloadTextbox_.val(binaryPayload);
+
+  var asciiPayload = binaryStringToAscii(binaryPayload);
+  this.asciiPayloadTextbox_.val(asciiPayload);
 };
 
 /** Send message to connected remote */
@@ -84,8 +176,5 @@ NetSimSendWidget.prototype.onSendButtonPress_ = function () {
     return;
   }
 
-  myNode.sendMessage({
-    toAddress: parseInt(this.toAddressTextbox_.val(), 10),
-    messageContent: this.payloadTextbox_.val()
-  });
+  myNode.sendMessage(this.packetBinary);
 };
