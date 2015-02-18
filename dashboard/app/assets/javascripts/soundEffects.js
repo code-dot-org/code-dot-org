78ce84bb9eb18de172c23e29812b34940cf532b6
Brian Jordan <bcjordan@gmail.com>
2015-02-18 11:51:27 -0800

    Fix IE9 version check (new Number(9) !== 9) [resolves #88641830]

--- dashboard/app/assets/javascripts/soundEffects.js
+++ dashboard/app/assets/javascripts/soundEffects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ef7747d0e09255a292f34af1fde94d171d57bfc96945bab416634fa849baeb4
-size 8751
+oid sha256:0cde72618682e64c74734cb3045d586de2bbfc518e572b166fc58f35244e0826
+size 8788
