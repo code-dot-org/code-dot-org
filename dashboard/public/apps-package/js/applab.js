579ed67f9853f412541c04497ca92021ca89a01a
Continuous Integration <dev@code.org>
2015-02-19 23:03:43 +0000

    Automatically built.
    commit 9fad22a6b160fde19f76b6acda65131cb6a5c996
    Merge: eb0c98d f974e97
    Author: Chris Pirich <chris@code.org>
    Date:   Thu Feb 19 14:55:58 2015 -0800
    
        Merge pull request #1357 from code-dot-org/applab_level_builder
    
        enable level builder for applab
    
    commit f974e97a8c7264aa5c9212d4742c19d8f3c2d454
    Author: cpirich <chris@code.org>
    Date:   Thu Feb 19 13:53:19 2015 -0800
    
        fix uses_droplet? issue during seeding
    
    commit 8d4fb2de450445eb82ad0b4f65b8392b3142bb86
    Author: cpirich <chris@code.org>
    Date:   Thu Feb 19 13:22:18 2015 -0800
    
        enable level builder for applab

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:234ec4d515dc7ec4883b8ab2856c13911fc3a5a053a193ff622a9260011fb977
-size 135372
+oid sha256:c999ca6e9218eaba8e4abbc8c0ff49891495a7336edc5523d32dd0e9ad3657b5
+size 135394
