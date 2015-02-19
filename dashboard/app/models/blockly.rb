f974e97a8c7264aa5c9212d4742c19d8f3c2d454
cpirich <chris@code.org>
2015-02-19 13:53:19 -0800

    fix uses_droplet? issue during seeding

--- dashboard/app/models/blockly.rb
+++ dashboard/app/models/blockly.rb
@@ -39,7 +39,7 @@ class Blockly < Level
 
   # These serialized fields will be serialized/deserialized as straight XML
   def xml_blocks
-    return %w(start_blocks toolbox_blocks required_blocks) unless self.game.uses_droplet?
+    return %w(start_blocks toolbox_blocks required_blocks) unless self.game.try(:uses_droplet?)
     %w()
   end
 
