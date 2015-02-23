1ee1e7925fcd1a300915b56057cce6763c2f2464
Brad Buchanan <brad@code.org>
2015-02-23 11:25:46 -0800

    Table display in message logs.

--- apps/src/netsim/NetSimLogWidget.js
+++ apps/src/netsim/NetSimLogWidget.js
@@ -14,7 +14,15 @@
 
 var markup = require('./NetSimLogWidget.html');
 var dom = require('../dom');
-
+var PacketEncoder = require('./PacketEncoder');
+var dataConverters = require('./dataConverters');
+var formatBinary = dataConverters.formatBinary;
+var formatHex = dataConverters.formatHex;
+var alignDecimal = dataConverters.alignDecimal;
+var binaryToInt = dataConverters.binaryToInt;
+var binaryToHex = dataConverters.binaryToHex;
+var binaryToDecimal = dataConverters.binaryToDecimal;
+var binaryToAscii = dataConverters.binaryToAscii;
 
 /**
  * Generator and controller for message log.
@@ -55,26 +63,125 @@ NetSimLogWidget.createWithin = function (element, title) {
  */
 NetSimLogWidget.prototype.bindElements_ = function (instanceID) {
   this.rootDiv_ = $('#netsim_log_widget_' + instanceID);
-  this.scrollArea_ = this.rootDiv_.find('#scroll_area');
-  this.clearButton_ = this.rootDiv_.find('#clear_button');
+  this.scrollArea_ = this.rootDiv_.find('.scroll_area');
+  this.tableBody_ = this.scrollArea_.find('tbody');
+  this.clearButton_ = this.rootDiv_.find('.clear_button');
 
   dom.addClickTouchEvent(this.clearButton_[0], this.onClearButtonPress_.bind(this));
 };
 
 NetSimLogWidget.prototype.onClearButtonPress_ = function () {
-  this.scrollArea_.empty();
+  this.tableBody_.empty();
 };
+
+/**
+ * Format router uses to decode packet.
+ * TODO (bbuchanan): Pull this from a common location; should be fixed across
+ *                   simulation.
+ * @type {PacketEncoder}
+ */
+var packetEncoder = new PacketEncoder([
+  { key: 'toAddress', bits: 4 },
+  { key: 'fromAddress', bits: 4 },
+  { key: 'packetIndex', bits: 4 },
+  { key: 'packetCount', bits: 4 },
+  { key: 'message', bits: Infinity }
+]);
 
 /**
  * Put a message into the log.
  */
-NetSimLogWidget.prototype.log = function (message) {
+NetSimLogWidget.prototype.log = function (packet) {
   var scrollArea = this.scrollArea_;
   var wasScrolledToEnd =
       scrollArea[0].scrollHeight - scrollArea[0].scrollTop <=
       scrollArea.outerHeight();
 
-  scrollArea.val(this.scrollArea_.val() + message + '\n');
+  var toAddress = packetEncoder.getField('toAddress', packet);
+  var fromAddress = packetEncoder.getField('fromAddress', packet);
+  var packetIndex = packetEncoder.getField('packetIndex', packet);
+  var packetCount = packetEncoder.getField('packetCount', packet);
+  var message = packetEncoder.getField('message', packet);
+
+  // Create log rows
+  var asciiRow = $('<tr>').addClass('ascii');
+  $('<td nowrap>')
+      .addClass('toAddress')
+      .html(binaryToInt(toAddress))
+      .appendTo(asciiRow);
+  $('<td nowrap>')
+      .addClass('fromAddress')
+      .html(binaryToInt(fromAddress))
+      .appendTo(asciiRow);
+  $('<td nowrap>')
+      .addClass('packetInfo')
+      .html(binaryToInt(packetIndex) + " of " + binaryToInt(packetCount))
+      .appendTo(asciiRow);
+  $('<td>')
+      .addClass('message')
+      .html(binaryToAscii(message, 8))
+      .appendTo(asciiRow);
+
+  var decimalRow = $('<tr>').addClass('decimal');
+  $('<td nowrap>')
+      .addClass('toAddress')
+      .html(binaryToInt(toAddress))
+      .appendTo(decimalRow);
+  $('<td nowrap>')
+      .addClass('fromAddress')
+      .html(binaryToInt(fromAddress))
+      .appendTo(decimalRow);
+  $('<td nowrap>')
+      .addClass('packetInfo')
+      .html(binaryToInt(packetIndex) + " of " + binaryToInt(packetCount))
+      .appendTo(decimalRow);
+  // TODO (bbuchanan): Parse at selected bytesize
+  $('<td>')
+      .addClass('message')
+      .html(alignDecimal(binaryToDecimal(message, 8)))
+      .appendTo(decimalRow);
+
+  var hexRow = $('<tr>').addClass('hexadecimal');
+  $('<td nowrap>')
+      .addClass('toAddress')
+      .html(binaryToHex(toAddress))
+      .appendTo(hexRow);
+  $('<td nowrap>')
+      .addClass('fromAddress')
+      .html(binaryToHex(fromAddress))
+      .appendTo(hexRow);
+  $('<td nowrap>')
+      .addClass('packetInfo')
+      .html(binaryToHex(packetIndex) + " of " + binaryToHex(packetCount))
+      .appendTo(hexRow);
+  $('<td>')
+      .addClass('message')
+      .html(formatHex(binaryToHex(message), 2))
+      .appendTo(hexRow);
+
+  var binaryRow = $('<tr>').addClass('binary');
+  $('<td nowrap>')
+      .addClass('toAddress')
+      .html(formatBinary(toAddress, 4))
+      .appendTo(binaryRow);
+  $('<td nowrap>')
+      .addClass('fromAddress')
+      .html(formatBinary(fromAddress, 4))
+      .appendTo(binaryRow);
+  $('<td nowrap>')
+      .addClass('packetInfo')
+      .html(formatBinary(packetIndex + packetCount, 4))
+      .appendTo(binaryRow);
+  // TODO (bbuchanan): Format to selected bytesize
+  $('<td>')
+      .addClass('message')
+      .html(formatBinary(message, 8))
+      .appendTo(binaryRow);
+
+  asciiRow.appendTo(this.tableBody_);
+  decimalRow.appendTo(this.tableBody_);
+  hexRow.appendTo(this.tableBody_);
+  binaryRow.appendTo(this.tableBody_);
 
   // Auto-scroll
   if (wasScrolledToEnd) {
