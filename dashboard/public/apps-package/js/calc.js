f273738676974d5722d562cef092810cf5f511fa
Continuous Integration <dev@code.org>
2015-02-23 23:18:07 +0000

    Automatically built.
    commit b4a5360443adf6df2f75d6066a29d00114980ead
    Merge: b4f1be3 3f10dd5
    Author: breville <breville@users.noreply.github.com>
    Date:   Tue Feb 24 10:07:57 2015 +1100
    
        Merge pull request #1374 from code-dot-org/infinitypreloadimages
    
        Preload images for Infinity Play Lab
    
    commit b4f1be37284532ba50d68c5d80d82c37fb71b9ba
    Merge: 9e2b5cb 382166f
    Author: Brendan Reville <brendan@code.org>
    Date:   Tue Feb 24 10:01:23 2015 +1100
    
        Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
    commit 9e2b5cbae4cbe1a8e8d3d574d226ef5ce283e05b
    Author: Brendan Reville <brendan@code.org>
    Date:   Tue Feb 24 10:00:36 2015 +1100
    
        Try dark tinted caption backgrounds on /index2.
    
    commit 382166f790391c9a3284e4b6a0c99fad57c83b29
    Merge: 015adc7 f0eedb9
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Feb 23 14:58:27 2015 -0800
    
        Merge pull request #1383 from code-dot-org/calcError
    
        handle null response from server
    
    commit f0eedb9f79bee71f2ea7c10cb32fc7341fe9eb04
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Feb 23 14:53:15 2015 -0800
    
        handle null response from server

--- dashboard/public/apps-package/js/calc.js
+++ dashboard/public/apps-package/js/calc.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26e3d319f4477b94c092d04d4fcc910e18d41753daabd4b7c099f2b503d5b231
-size 59712
+oid sha256:4164068dc7609eddee4add1d061eb01c291832b2edde6ee4d2300af41f955440
+size 59856
