1aeb31055906b62836c88d3e54322c778b5145c0
Continuous Integration <dev@code.org>
2015-02-23 21:41:50 +0000

    Automatically built.
    commit de27a84cfbfd05493f14581ff8340869c52d8a13
    Merge: a82fdc4 6c4c0b8
    Author: Chris Pirich <chris@code.org>
    Date:   Mon Feb 23 13:33:26 2015 -0800
    
        Merge pull request #1345 from code-dot-org/droplet_update_object.function
    
        Droplet update object.function
    
    commit a82fdc4790aa13450c12012346313ef3800234b9
    Merge: e1ef084 1bb8728
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Mon Feb 23 13:28:31 2015 -0800
    
        Merge pull request #1378 from code-dot-org/yellow-current
    
        Update colors in progress views
    
    commit 1bb8728038bd72a1ae1d6cfac6103df2e69fcde9
    Merge: d12690c e1ef084
    Author: Laurel Fan <laurel@code.org>
    Date:   Mon Feb 23 12:56:00 2015 -0800
    
        merge staging

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78ecc8fe1fee459ff7b65197b83e3fb58ba20cad5ad64ed9886798e1dd222c71
-size 537850
+oid sha256:654070ba2fe9d83c118eec85a3c3367cd6217401659ca93ebddeee0bbfb16063
+size 538086
