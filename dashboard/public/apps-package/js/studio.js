66b69df08842c775b01beef41787a2cb416ee6cc
Continuous Integration <dev@code.org>
2015-02-24 00:57:49 +0000

    Automatically built.
    commit 5b6dd2fe5b66d770bd5afc1c3a153016bf845be6
    Merge: bd6b1a0 d2900fc
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Feb 23 16:49:38 2015 -0800
    
        Merge pull request #1385 from code-dot-org/evalGrid
    
        center labels, share the code
    
    commit d2900fc3a397dc59d2f9e747e5b071b49347d8f5
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Feb 23 16:06:31 2015 -0800
    
        center labels, share the code

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3331f3cdcc4c6a414af33957edde23c82d03412d1c3778839d10cec475eb85f8
-size 297819
+oid sha256:3829b79a9e9a57df9602cf8e0ee817e169c25625eaeeb65d3b1d9cb82f118597
+size 296832
