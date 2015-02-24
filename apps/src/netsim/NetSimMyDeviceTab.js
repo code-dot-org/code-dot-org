a248bdefb8e794ed70308cdbda5e1037efd1895a
Brad Buchanan <brad@code.org>
2015-02-24 11:54:29 -0800

    Extract NetSimChunkSizeControl from NetSimMyDeviceTab

--- apps/src/netsim/NetSimMyDeviceTab.js
+++ apps/src/netsim/NetSimMyDeviceTab.js
@@ -12,11 +12,10 @@
 'use strict';
 
 var markup = require('./NetSimMyDeviceTab.html');
+var NetSimChunkSizeControl = require('./NetSimChunkSizeControl');
 
 /**
- * Generator and controller for message encoding selector: A dropdown that
- * controls whether messages are displayed in some combination of binary, hex,
- * decimal, ascii, etc.
+ * Generator and controller for "My Device" tab.
  * @param {jQuery} rootDiv
  * @param {function} chunkSizeChangeCallback
  * @constructor
@@ -36,11 +35,10 @@ var NetSimMyDeviceTab = module.exports = function (rootDiv, chunkSizeChangeCallb
   this.chunkSizeChangeCallback_ = chunkSizeChangeCallback;
 
   /**
-   *
-   * @type {number}
+   * @type {NetSimChunkSizeControl}
    * @private
    */
-  this.currentChunkSize_ = 8;
+  this.chunkSizeControl_ = null;
 
   this.render();
 };
@@ -51,38 +49,9 @@ var NetSimMyDeviceTab = module.exports = function (rootDiv, chunkSizeChangeCallb
 NetSimMyDeviceTab.prototype.render = function () {
   var renderedMarkup = $(markup({}));
   this.rootDiv_.html(renderedMarkup);
-  this.bindElements_();
-};
-
-/**
- * Get relevant elements from the page and bind them to local variables.
- * @private
- */
-NetSimMyDeviceTab.prototype.bindElements_ = function () {
-  var rootDiv = this.rootDiv_;
-  rootDiv.find('.chunk_size_slider').slider({
-    value: this.currentChunkSize_,
-    min: 1,
-    max: 32,
-    step: 1,
-    slide: this.onChunkSizeChange_.bind(this)
-  });
-  this.setChunkSize(this.currentChunkSize_);
-};
-
-/**
- * Change handler for jQueryUI slider control.
- * @param {Event} event
- * @param {Object} ui
- * @param {jQuery} ui.handle - The jQuery object representing the handle that
- *        was changed.
- * @param {number} ui.value - The current value of the slider.
- * @private
- */
-NetSimMyDeviceTab.prototype.onChunkSizeChange_ = function (event, ui) {
-  var newChunkSize = ui.value;
-  this.setChunkSize(newChunkSize);
-  this.chunkSizeChangeCallback_(newChunkSize);
+  this.chunkSizeControl_ = new NetSimChunkSizeControl(
+      this.rootDiv_.find('.chunk_size'),
+      this.chunkSizeChangeCallback_);
 };
 
 /**
@@ -90,10 +59,5 @@ NetSimMyDeviceTab.prototype.onChunkSizeChange_ = function (event, ui) {
  * @param {number} newChunkSize
  */
 NetSimMyDeviceTab.prototype.setChunkSize = function (newChunkSize) {
-  var rootDiv = this.rootDiv_;
-  this.currentChunkSize_ = newChunkSize;
-  rootDiv.find('.chunk_size_slider').slider('option', 'value', newChunkSize);
-  rootDiv.find('.chunk_size_value').html(newChunkSize + ' bits');
+  this.chunkSizeControl_.setChunkSize(newChunkSize);
 };
-
-
