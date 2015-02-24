5e76a75844de9234f33ecc2b1ffddfef99e4e95b
Josh Lory <josh.lory@code.org>
2015-02-23 23:06:23 -0800

    Code review feedback

--- dashboard/app/helpers/levels_helper.rb
+++ dashboard/app/helpers/levels_helper.rb
@@ -233,7 +233,7 @@ module LevelsHelper
       project_template_level_name
       scrollbars
       original_start_blocks
-      is_project
+      is_project_level
     ).map{ |x| x.include?(':') ? x.split(':') : [x,x.camelize(:lower)]}]
     .each do |dashboard, blockly|
       # Select first valid value from 1. local_assigns, 2. property of @level object, 3. named instance variable, 4. properties json
