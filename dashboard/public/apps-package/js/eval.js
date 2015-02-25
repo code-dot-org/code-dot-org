652a78a632839ff68398f97d71c1b7634b6c98b3
Continuous Integration <dev@code.org>
2015-02-25 20:42:59 +0000

    Automatically built.
    commit db3aaa7e4b60ee84444ab828e4804cb1ff2c009a
    Merge: 91891a9 8028b64
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Feb 25 12:32:44 2015 -0800
    
        Merge pull request #1417 from code-dot-org/evalCaseMismatch
    
        [Finishes #88063228] Specific error for eval case mismatch
    
    commit 91891a90c0699795dcf00f9bd2644bb9f4d0a9d1
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Feb 25 20:28:04 2015 +0000
    
        Dropbox changes
    
    commit 8028b64e47e8c5d608ea7e497c2f08fffc60b371
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Wed Feb 25 12:17:41 2015 -0800
    
        code review feedback

--- dashboard/public/apps-package/js/eval.js
+++ dashboard/public/apps-package/js/eval.js
@@ -48,6 +48,7 @@ var page = require('../templates/page.html');
 var dom = require('../dom');
 var blockUtils = require('../block_utils');
 var CustomEvalError = require('./evalError');
+var EvalText = require('./evalText');
 
 var ResultType = studioApp.ResultType;
 var TestResults = studioApp.TestResults;
@@ -66,6 +67,8 @@ var CANVAS_WIDTH = 400;
 
 // This property is set in the api call to draw, and extracted in evalCode
 Eval.displayedObject = null;
+
+Eval.answerObject = null;
 
 /**
  * Initialize Blockly and the Eval.  Called on page load.
@@ -136,6 +139,8 @@ Eval.init = function(config) {
 
       var answerObject = getDrawableFromBlockXml(solutionBlocks);
       if (answerObject && answerObject.draw) {
+        // store object for later analysis
+        Eval.answerObject = answerObject;
         answerObject.draw(document.getElementById('answer'));
       }
     }
@@ -243,6 +248,57 @@ function getDrawableFromBlockXml(blockXml) {
 }
 
 /**
+ * Recursively parse an EvalObject looking for EvalText objects. For each one,
+ * extract the text content.
+ */
+Eval.getTextStringsFromObject_ = function (evalObject) {
+  if (!evalObject) {
+    return [];
+  }
+
+  var strs = [];
+  if (evalObject instanceof EvalText) {
+    strs.push(evalObject.getText());
+  }
+
+  evalObject.getChildren().forEach(function (child) {
+    strs = strs.concat(Eval.getTextStringsFromObject_(child));
+  });
+  return strs;
+};
+
+/**
+ * @returns True if two eval objects have sets of text strings that differ
+ *   only in case
+ */
+Eval.haveCaseMismatch_ = function (object1, object2) {
+  var strs1 = Eval.getTextStringsFromObject_(object1);
+  var strs2 = Eval.getTextStringsFromObject_(object2);
+
+  if (strs1.length !== strs2.length) {
+    return false;
+  }
+
+  strs1.sort();
+  strs2.sort();
+
+  var caseMismatch = false;
+
+  for (var i = 0; i < strs1.length; i++) {
+    var str1 = strs1[i];
+    var str2 = strs2[i];
+    if (str1 !== str2) {
+      if (str1.toLowerCase() === str2.toLowerCase()) {
+        caseMismatch  = true;
+      } else {
+        return false; // strings differ by more than case
+      }
+    }
+  }
+  return caseMismatch;
+};
+
+/**
  * Execute the user's code.  Heaven help us...
  */
 Eval.execute = function() {
@@ -266,6 +322,11 @@ Eval.execute = function() {
       Eval.testResults = TestResults.APP_SPECIFIC_FAIL;
 
       Eval.message = userObject.feedbackMessage;
+    } else if (Eval.haveCaseMismatch_(userObject, Eval.answerObject)) {
+      Eval.result = false;
+      Eval.testResults = TestResults.APP_SPECIFIC_FAIL;
+
+      Eval.message = evalMsg.stringMismatchError();
     } else {
       // We got an EvalImage back, compare it to our target
       Eval.result = evaluateAnswer();
@@ -370,7 +431,7 @@ function onReportComplete(response) {
 }
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"../../locale/current/common":205,"../../locale/current/eval":206,"../StudioApp":4,"../block_utils":18,"../canvg/canvg.js":41,"../codegen":44,"../dom":47,"../skins":154,"../templates/page.html":179,"./api":49,"./controls.html":51,"./evalError":55,"./levels":64,"./visualization.html":66}],66:[function(require,module,exports){
+},{"../../locale/current/common":205,"../../locale/current/eval":206,"../StudioApp":4,"../block_utils":18,"../canvg/canvg.js":41,"../codegen":44,"../dom":47,"../skins":154,"../templates/page.html":179,"./api":49,"./controls.html":51,"./evalError":55,"./evalText":61,"./levels":64,"./visualization.html":66}],66:[function(require,module,exports){
 module.exports= (function() {
   var t = function anonymous(locals, filters, escape) {
 escape = escape || function (html){
@@ -983,6 +1044,10 @@ EvalText.prototype.draw = function (parent) {
   EvalImage.prototype.draw.apply(this, arguments);
 };
 
+EvalText.prototype.getText = function () {
+  return this.text_;
+};
+
 },{"./evalImage":56,"./evalUtils":63}],60:[function(require,module,exports){
 var EvalImage = require('./evalImage');
 var evalUtils = require('./evalUtils');
@@ -1147,6 +1212,10 @@ EvalMulti.prototype.draw = function (parent) {
   EvalImage.prototype.draw.apply(this, arguments);
 };
 
+EvalImage.prototype.getChildren = function () {
+  return [this.image1_, this.image2_];
+};
+
 },{"./evalImage":56,"./evalUtils":63}],54:[function(require,module,exports){
 var EvalImage = require('./evalImage');
 var evalUtils = require('./evalUtils');
@@ -1274,6 +1343,13 @@ EvalImage.prototype.rotate = function (degrees) {
 EvalImage.prototype.scale = function (scaleX, scaleY) {
   this.scaleX_ = scaleX;
   this.scaleY_ = scaleY;
+};
+
+/**
+ * Get child EvalObjects. overridden by children
+ */
+EvalImage.prototype.getChildren = function () {
+  return [];
 };
 
 },{"./evalUtils":63}],63:[function(require,module,exports){
