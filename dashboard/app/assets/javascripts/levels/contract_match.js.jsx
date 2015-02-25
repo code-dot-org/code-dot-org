7c0d2b17d0e070e0eac0e64adf7c6485517273f8
Brian Jordan <bcjordan@gmail.com>
2015-02-25 12:28:44 -0800

    Change result checker to work with new form

--- dashboard/app/assets/javascripts/levels/contract_match.js.jsx
+++ dashboard/app/assets/javascripts/levels/contract_match.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33f70efcc66325c22c1b90e6110d1b02ff2fe1f429714a23abfab511da86647a
-size 7851
+oid sha256:b47493fcfb329a32c8709c1116c36e0ac1eb7f2688c497fbb1aabeefb50d995f
+size 7407
