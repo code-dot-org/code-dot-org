0cc6beb62f8406c8dfab9971649c498647259cec
Brian Jordan <bcjordan@gmail.com>
2015-02-24 16:42:27 -0800

    Move rest of form into React

--- dashboard/app/assets/javascripts/levels/contract_match.js.jsx
+++ dashboard/app/assets/javascripts/levels/contract_match.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c73c81bd5b61f394f15f314325c65a1bf472b6138321a78ec02a94395ed6a2c
-size 7086
+oid sha256:33f70efcc66325c22c1b90e6110d1b02ff2fe1f429714a23abfab511da86647a
+size 7851
