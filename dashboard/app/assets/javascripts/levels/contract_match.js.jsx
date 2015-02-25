e727d4681e9fd40f9779b5b3ce0a22b9f573cd46
Brian Jordan <bcjordan@gmail.com>
2015-02-25 14:36:35 -0800

    Add "none" type which prompts for a selection

--- dashboard/app/assets/javascripts/levels/contract_match.js.jsx
+++ dashboard/app/assets/javascripts/levels/contract_match.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bebb114a3e0814d30e65a4b64a7fd1655a3973251c3774f82f6ef570a97e5122
-size 7444
+oid sha256:5f88a8538f94bbb2be3c770495bc56be1b782f84bffaa1d5b2c60b115c8ab54f
+size 7533
