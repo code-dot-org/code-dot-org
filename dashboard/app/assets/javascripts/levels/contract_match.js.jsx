0ecdbb317a331424a1a897a1ce8893105534c198
Brian Jordan <bcjordan@gmail.com>
2015-02-25 15:22:50 -0800

    Use Array.prototype.map instead of $.map [ci skip]

--- dashboard/app/assets/javascripts/levels/contract_match.js.jsx
+++ dashboard/app/assets/javascripts/levels/contract_match.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16450d107aefcccdbd96e378150a4f705f289a242851cc589171f368915b0364
-size 7754
+oid sha256:6ff1dfa561f82d5568bcf33c921a4620bf5a5914703fe3df6f4aee1187dedba4
+size 7874
