a269296c93197240e06d0af2ab8c7f3b8b85d265
Josh Lory <josh.lory@code.org>
2015-02-24 16:29:57 -0800

    Droplet levels don't have Blockly defined

--- apps/src/StudioApp.js
+++ apps/src/StudioApp.js
@@ -1114,7 +1114,10 @@ StudioApp.prototype.configureDom = function (config) {
   var runButton = container.querySelector('#runButton');
   var resetButton = container.querySelector('#resetButton');
   var throttledRunClick = _.debounce(function () {
-    Blockly.fireUiEvent(window, 'run_button_pressed');
+    if (window.Blockly) {
+      // TODO: (Josh L.) use $.trigger once we add jQuery
+      Blockly.fireUiEvent(window, 'run_button_pressed');
+    }
     this.runButtonClick();
   }, 250, true);
   dom.addClickTouchEvent(runButton, _.bind(throttledRunClick, this));
