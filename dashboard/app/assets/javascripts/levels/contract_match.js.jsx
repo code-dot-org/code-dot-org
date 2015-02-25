14144256a29a0438e448f8c27c0eb8e2f11a1099
Brian Jordan <bcjordan@gmail.com>
2015-02-25 15:40:37 -0800

    [ci skip] Add explanatory comment about getResult generator - Add some comments about plans for file and components - Remove remaining references to domain names

--- dashboard/app/assets/javascripts/levels/contract_match.js.jsx
+++ dashboard/app/assets/javascripts/levels/contract_match.js.jsx
@@ -1,3 +1,4 @@
+/** TODO(bjordan): Move this all into StudioApps */
 $(window).load(function () {
 
   /**
@@ -189,10 +190,13 @@ $(window).load(function () {
   var contractForm = React.render(<ContractForm />, document.getElementById('contractForm'));
 
   /**
+   * Creates a getResult function compatible with _dialog.html.haml's getResult call
+   * Generating this rather than passing directly to be explicit about inputs for now
    * @param {ContractForm} contractForm
    * @param {Object} levelData
+   * @returns {Function} getResult function
    */
-  window.getResult = function (contractForm, levelData) {
+  var generateGetResultFunction = function (contractForm, levelData) {
     return function () {
       /** @type {ContractForm} */
       var functionName = contractForm.getName();
@@ -222,17 +226,28 @@ $(window).load(function () {
         errorType: errorType
       };
     };
-  }(contractForm, window.levelData);
+  };
+
+  /**
+   * Set the getResult used by _dialog.html.haml
+   * @return {Object} response, result, error type
+   */
+  window.getResult = generateGetResultFunction(contractForm, window.levelData);
 
   /**
    * Given the user's submission and a correct answer, returns the error type,
    * or empty string if correct.
+   * @param {string} functionName
+   * @param {string} rangeInput
+   * @param {string} domainInput
+   * @param {string} correctAnswer
+   * @returns {string}
    */
   function checkAnswer(functionName, rangeInput, domainInput, correctAnswer) {
-    var items = correctAnswer.split('|');
-    var correctName = items[0];
-    var correctRange = items[1];
-    var correctDomain = items.slice(2);
+    var correctAnswerItems = correctAnswer.split('|');
+    var correctName = correctAnswerItems[0];
+    var correctRange = correctAnswerItems[1];
+    var correctDomain = correctAnswerItems.slice(2);
     var domainInputItems = domainInput.split('|');
 
     if (correctName !== functionName) {
@@ -245,13 +260,8 @@ $(window).load(function () {
       return 'baddomainsize';
     }
     for (var i = 0; i < correctDomain.length; i++) {
-      var correctDomainName = correctDomain[i].split(':')[0];
-      var correctDomainType = correctDomain[i].split(':')[1];
-      var domainName = domainInputItems[i].split(':')[0];
-      var domainType = domainInputItems[i].split(':')[1];
-      if (correctDomainName !== domainName) {
-        return 'baddomainname';
-      }
+      var correctDomainType = correctDomain[i];
+      var domainType = domainInputItems[i];
       if (correctDomainType !== domainType) {
         return 'baddomaintype';
       }
