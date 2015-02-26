9a836921ea587bdab32eb43e2583c61ed950c467
cpirich <chris@code.org>
2015-02-26 15:12:35 -0800

    misc applab requests

--- apps/src/timeoutList.js
+++ apps/src/timeoutList.js
@@ -14,3 +14,45 @@ exports.clearTimeouts = function () {
   list.forEach(window.clearTimeout, window);
   list = [];
 };
+
+/**
+ * Clears a timeout and removes the item from the list
+ */
+exports.clearTimeout = function (id) {
+  window.clearTimeout(id);
+  // List removal requires IE9+
+  var index = list.indexOf(id);
+  if (index > -1) {
+    list.splice(index, 1);
+  }
+};
+
+var intervalList = [];
+
+/**
+ * call setInterval and track the returned id
+ */
+exports.setInterval = function (fn, time) {
+  intervalList.push(window.setInterval.apply(window, arguments));
+};
+
+/**
+ * Clears all interval timeouts in our list and resets the list
+ */
+exports.clearIntervals = function () {
+  intervalList.forEach(window.clearInterval, window);
+  intervalList = [];
+};
+
+/**
+ * Clears a timeout and removes the item from the list
+ */
+exports.clearInterval = function (id) {
+  window.clearInterval(id);
+  // List removal requires IE9+
+  var index = intervalList.indexOf(id);
+  if (index > -1) {
+    intervalList.splice(index, 1);
+  }
+};
+
