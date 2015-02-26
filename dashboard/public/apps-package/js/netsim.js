1e779a531a895603417317c5dcf634d8c4222481
Continuous Integration <dev@code.org>
2015-02-26 22:33:54 +0000

    Automatically built.
    commit d87917bf8f83e1efb028b543344b1e9228cf2965
    Merge: 14c9523 1176e21
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Thu Feb 26 14:26:10 2015 -0800
    
        Merge pull request #1441 from code-dot-org/netsim_performance
    
        Netsim: Performance Tweaks
    
    commit 14c95230884cbb84d01bb11393db6750f67f8377
    Author: Josh Lory <josh.lory@code.org>
    Date:   Thu Feb 26 14:10:55 2015 -0800
    
        I18n 'Delete Project'
    
    commit ff6469d4fc47b928c6300b75a588f82878c96fe2
    Author: Josh Lory <josh.lory@code.org>
    Date:   Thu Feb 26 14:06:27 2015 -0800
    
        Implement 'Delete Project'
    
    commit 1176e218e69b2c53188af2bf7e678cce23c584eb
    Author: Brad Buchanan <brad@code.org>
    Date:   Thu Feb 26 14:09:19 2015 -0800
    
        Allow variable heartbeat intervals, slow down router heartbeats since they are provided by multiple clients

--- dashboard/public/apps-package/js/netsim.js
+++ dashboard/public/apps-package/js/netsim.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e12ff7a0afbd0a896101e1cdb9a6298f627a5b4de9cde1cde02e502ce7ec1b1b
-size 207983
+oid sha256:7d3c073b485b8573c24b64a1e56e88804fecb67314fe06527670178c3e0aa49f
+size 209629
