f8aa3c99cc6086a680b186ce0be760b57f40c636
Josh Lory <josh.lory@code.org>
2015-02-25 17:35:58 -0800

    @last_attempt and not modifying @start_blocks makes more sense
    This approach plays nicer with 'Clear Puzzle' and legacy level.startBlocks
    from levels.js.

--- apps/src/StudioApp.js
+++ apps/src/StudioApp.js
@@ -398,9 +398,6 @@ StudioApp.prototype.init = function(config) {
           Blockly.functionEditor.hideIfOpen();
         }
         Blockly.mainBlockSpace.clear();
-        if (config.level.originalStartBlocks) {
-          config.level.startBlocks = config.level.originalStartBlocks;
-        }
         this.setStartBlocks_(config);
       }).bind(this));
     }).bind(this));
@@ -1261,9 +1258,13 @@ StudioApp.prototype.setCheckForEmptyBlocks = function (checkBlocks) {
 
 /**
  * Add the starting block(s).
+ * @param loadLastAttempt If true, try to load config.lastAttempt.
  */
-StudioApp.prototype.setStartBlocks_ = function (config) {
+StudioApp.prototype.setStartBlocks_ = function (config, loadLastAttempt) {
   var startBlocks = config.level.startBlocks || '';
+  if (loadLastAttempt) {
+    startBlocks = config.level.lastAttempt || startBlocks;
+  }
   if (config.forceInsertTopBlock) {
     startBlocks = blockUtils.forceInsertTopBlock(startBlocks,
         config.forceInsertTopBlock);
@@ -1326,7 +1327,7 @@ StudioApp.prototype.handleUsingBlockly_ = function (config) {
   if (config.afterInject) {
     config.afterInject();
   }
-  this.setStartBlocks_(config);
+  this.setStartBlocks_(config, true);
 };
 
 /**
--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -103,13 +103,9 @@ private
     if params[:level_source_id]
       level_source = LevelSource.find(params[:level_source_id])
       # we do multiple level projects, so we don't check that the level_source.level_id matches the loaded level
-      @start_blocks = level_source.data
-    elsif false # current_user.try(:admin?)
-      last_attempt = current_user.last_attempt(@level).try(:level_source).try(:data)
-      if last_attempt
-        @original_start_blocks = @start_blocks.presence || '<xml/>'
-        @start_blocks = last_attempt
-      end
+      @last_attempt = level_source.data
+    elsif current_user.try(:admin?)
+      @last_attempt = current_user.last_attempt(@level).try(:level_source).try(:data)
     end
   end
 
