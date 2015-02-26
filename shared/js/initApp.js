ff6469d4fc47b928c6300b75a588f82878c96fe2
Josh Lory <josh.lory@code.org>
2015-02-26 14:06:27 -0800

    Implement 'Delete Project'

--- shared/js/initApp.js
+++ shared/js/initApp.js
@@ -178,6 +178,17 @@ dashboard.saveProject = function(callback) {
   }
 };
 
+dashboard.deleteProject = function(callback) {
+  var app_id = dashboard.currentApp.id;
+  if (app_id) {
+    storageApps().delete(app_id, function(data) {
+      callbackSafe(callback, data);
+    });
+  } else {
+    callbackSafe(callback, false);
+  }
+};
+
 dashboard.loadEmbeddedProject = function(projectTemplateLevelName) {
   var deferred = new $.Deferred();
   // get all projects (TODO: filter on server side?)
@@ -213,7 +224,7 @@ dashboard.loadEmbeddedProject = function(projectTemplateLevelName) {
     }
   });
   return deferred;
-}
+};
 
 function initApp() {
   if (appOptions.level.isProjectLevel || dashboard.currentApp) {
