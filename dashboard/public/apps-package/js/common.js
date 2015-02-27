ebbe0bcf8ded3bd16af28e235b8b269f7f5e55d9
Continuous Integration <dev@code.org>
2015-02-27 00:17:04 +0000

    Automatically built.
    commit 17d0e2bf3306fac1351a72cd19cb68378ecf52a5
    Merge: ccfb8c4 3ec8ce0
    Author: Chris Pirich <chris@code.org>
    Date:   Thu Feb 26 16:04:58 2015 -0800
    
        Merge pull request #1442 from code-dot-org/applab_requests
    
        misc applab requests
    
    commit 3ec8ce01dfd27982b07777ab557da86bbb69f478
    Author: cpirich <chris@code.org>
    Date:   Thu Feb 26 15:34:03 2015 -0800
    
        droplet style fix to avoid gutters that extend outside workspace
    
    commit d54a7acc0e128820e37613ebaf22927a54a4f638
    Author: cpirich <chris@code.org>
    Date:   Thu Feb 26 15:33:25 2015 -0800
    
        PR feedback
    
    commit ccfb8c4fe3cca1ea095ac09e4b56ec9208d2945a
    Merge: 43cff5c e12335e
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Thu Feb 26 15:31:01 2015 -0800
    
        Merge pull request #1440 from code-dot-org/header-timestamp
    
        show updated at timestamp in the header

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0bb0b04426462717938a73b0261be66fd5a05915a31020f50446ca66aab3dc4
-size 543372
+oid sha256:757ca251f78e6a8b3281feaab5a78668a3253b682aca1e3485fb14714caeac85
+size 544356
