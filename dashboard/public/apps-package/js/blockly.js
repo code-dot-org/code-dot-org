e0e6ab95445ed8be534475816131ae634bf39fa4
Continuous Integration <dev@code.org>
2015-02-27 19:39:05 +0000

    Automatically built.
    commit a6feba7a03bde9c9a5bda2d56c4da61ba64ba06a
    Merge: 11100d4 3fcf940
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Feb 27 11:16:31 2015 -0800
    
        Merge pull request #1458 from code-dot-org/innerHTML
    
        textContent instead of innerHTML
    
    commit 3fcf9406d653b2e2499db5fb536da4b4262fbe07
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Fri Feb 27 11:12:32 2015 -0800
    
        textContent instead of innerHTML

--- dashboard/public/apps-package/js/blockly.js
+++ dashboard/public/apps-package/js/blockly.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75a24753dce5654a70ca9d963d9326d371139ceaf131f61e1f19d976ff8520a1
-size 742731
+oid sha256:9ce3d2c2eb71682e22b73724c26d549e708a5746b6761fbaa022fea7e6e28fc6
+size 742735
