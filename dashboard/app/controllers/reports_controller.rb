d09e68303168f6cb681bd468803c7c54f0bef5b4
Steve Eisner <steveeisner@gmail.com>
2015-02-27 10:59:16 -0800

    Fix flappy, hourofcode

--- dashboard/app/controllers/reports_controller.rb
+++ dashboard/app/controllers/reports_controller.rb
@@ -45,7 +45,7 @@ SQL
     stage_id = params[:stage_id]
     level_id = params[:level_id]
 
-    if script.default_script? && !stage_id
+    if script.default_script? && script.id != 1
       script_level = script.get_script_level_by_chapter(level_id)
     else
       script_level = script.get_script_level_by_stage_and_position(stage_id, level_id)
