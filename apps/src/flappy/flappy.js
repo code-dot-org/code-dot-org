d9f236e9fc16a5525334ed5ecc4f909fe3f0ca66
Brent Van Minnen <bjvanminnen@gmail.com>
2015-03-02 09:58:16 -0800

    use undefOr util

--- apps/src/flappy/flappy.js
+++ apps/src/flappy/flappy.js
@@ -81,7 +81,7 @@ var AVATAR_Y_OFFSET = constants.AVATAR_Y_OFFSET;
 
 var loadLevel = function() {
   // Load maps.
-  infoText = (level.infoText === undefined ? true : level.infoText);
+  infoText = utils.undefOr(level.infoText, true);
   if (!infoText) {
     Flappy.gameState = Flappy.GameStates.ACTIVE;
   }
