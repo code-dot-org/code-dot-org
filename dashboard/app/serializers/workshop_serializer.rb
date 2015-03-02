e55b3fd0b303e0058bfbad3acc01dea7ab03a5ca
Will Jordan <will@code.org>
2015-03-02 10:08:23 -0800

    add cohort_id to workshop serializer

--- dashboard/app/serializers/workshop_serializer.rb
+++ dashboard/app/serializers/workshop_serializer.rb
@@ -1,5 +1,5 @@
 class WorkshopSerializer < ActiveModel::Serializer
-  attributes :id, :name, :program_type, :location, :instructions
+  attributes :id, :name, :program_type, :location, :instructions, :cohort_id
   has_many :segments
   has_many :facilitators, serializer: TeacherSerializer
 end
