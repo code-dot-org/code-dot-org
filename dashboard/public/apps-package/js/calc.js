301e6c3b157daaeb6fb4dd9c415f43c25240c705
Continuous Integration <dev@code.org>
2015-03-03 23:08:48 +0000

    Automatically built.
    commit 9911319242c2d5893a36d5344c07df7c51912c26
    Merge: a29048b ba5d536
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Mar 3 15:01:21 2015 -0800
    
        Merge pull request #1464 from code-dot-org/undefOr
    
        [Finishes #89135988] use undefOr util
    
    commit a29048b10567d3b7bfdf05e70fb7a044b77cb865
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Mar 3 22:56:50 2015 +0000
    
        Automatically built.
    
        commit 10ae316a1587c044b0941446f3f357d25dd8b352
        Merge: f3b3c68 0c0b920
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Tue Mar 3 14:49:06 2015 -0800
    
            Merge pull request #1486 from code-dot-org/studioFix
    
            explicitly set enableShowCode to false if editCode is undefined
    
        commit f3b3c68b7b753b7ea989504cdb95cdef0d102112
        Merge: 7b3dfb2 0167603
        Author: Laurel <laurel.fan@gmail.com>
        Date:   Tue Mar 3 14:40:21 2015 -0800
    
            Merge pull request #1487 from code-dot-org/teacher-dashboard-overflow
    
            fix teacher dashboard overflow [Finishes #76100360]
    
    commit ba5d5369126c695f8fdc054cdf78b2c6599ce41e
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Mar 3 14:53:32 2015 -0800
    
        rename to valueOr

--- dashboard/public/apps-package/js/calc.js
+++ dashboard/public/apps-package/js/calc.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e8925f346234549da87f5ce6db3a336ff70abe26bf95cdff112413c4006bfb6
+oid sha256:c99b60f82f8fae3489414c4e4bc11d10334aec4dc2e8d1d833b3022bd55c1d33
 size 61049
--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53c11a05836e775125442baea87581de8159e6a3717cace01ccd9d3999055fbc
+oid sha256:d6012daa8e8e40dc868cedd6b0664f802834751760fc3b16f3cf4deb53642514
 size 301331
