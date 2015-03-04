4b8bf34bfe894b1eb02c940c0a43e6face45c76a
Josh Lory <josh.lory@code.org>
2015-03-03 21:11:42 -0800

    Code review feedback

--- dashboard/app/helpers/levels_helper.rb
+++ dashboard/app/helpers/levels_helper.rb
@@ -345,7 +345,11 @@ module LevelsHelper
 
   def level_title
     if @script_level
-      script = data_t_suffix('script.name', @script_level.script.name, 'title')
+      script = if @script_level.script.flappy?
+        data_t 'game.name', @game.name
+      else
+        data_t_suffix 'script.name', @script_level.script.name, 'title'
+      end
       stage = @script_level.name
       position = @script_level.position
       if @script_level.script.stages.many?
@@ -353,7 +357,7 @@ module LevelsHelper
       elsif @script_level.position != 1
         "#{script} ##{position}"
       else
-        "#{script}"
+        script
       end
     else
       @level.key
