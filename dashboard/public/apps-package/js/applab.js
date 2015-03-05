598cc7e78f467d64b14dcb08e6a1e683966171d8
Continuous Integration <dev@code.org>
2015-03-05 22:41:49 +0000

    Automatically built.
    commit 16b4a0026064b20aceda5a08d0003cbb00d943f8
    Merge: 1fa8c53 a7cd07d
    Author: Chris Pirich <chris@code.org>
    Date:   Thu Mar 5 14:32:56 2015 -0800
    
        Merge pull request #1503 from code-dot-org/applab_execution_optimizations
    
        applab interpreter execution optimizations
    
    commit 1fa8c53f12a9d4552031fc7d65027a69f45152ec
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Mar 5 21:51:46 2015 +0000
    
        Automatically built.
    
        commit 02213113b149730407894242ce49e9b479da9f89
        Merge: 4a70afa 717a9ea
        Author: David Bailey <dsb123@gmail.com>
        Date:   Thu Mar 5 13:43:15 2015 -0800
    
            Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
        commit 4a70afa5e9787477e6eae12b89239153ce70b4f2
        Author: David Bailey <dsb123@gmail.com>
        Date:   Thu Mar 5 13:42:32 2015 -0800
    
            applab s/createTextLabel/textLabel/

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae93c493215555b0cdf277de40eeb392055be2bb47c8f7686937ca885501e7bf
-size 139354
+oid sha256:c4fc1c3113d3b5fda3810ad01b647bfcf6c557e9b46b09a6482b7427706410f7
+size 139626
