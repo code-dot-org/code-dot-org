e8c3fc96f7d76ad95912930192c249b397066175
Brendan Reville <brendan@code.org>
2015-03-09 16:02:33 +1100

    Initial work on final Infinity phrases & scrolling dropdowns.

--- blockly-core/core/ui/fields/field_dropdown.js
+++ blockly-core/core/ui/fields/field_dropdown.js
@@ -133,7 +133,9 @@ Blockly.FieldDropdown.prototype.showEditor_ = function() {
   menuDom.style.borderColor = 'hsla(' + this.sourceBlock_.getColour() + ', ' +
     this.sourceBlock_.getSaturation() * 100 + '%, ' +
     this.sourceBlock_.getValue() * 100 + '%' + ', 0.5)';
-
+  menuDom.style.overflow = "scroll";
+  menuDom.style["max-height"] = "250px";
+  
   // Record menuSize after adding menu.
   var menuSize = goog.style.getSize(menuDom);
 
