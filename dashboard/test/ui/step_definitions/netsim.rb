9425fd001e6720905990674d1b7ba79a40c5d496
Brad Buchanan <brad@code.org>
2015-03-11 11:17:43 -0700

    Fix UI tests

--- dashboard/test/ui/step_definitions/netsim.rb
+++ dashboard/test/ui/step_definitions/netsim.rb
@@ -1,6 +1,7 @@
 When /^I load netsim$/ do
+  # Stage 3 puzzle 4 is the "Everything is Enabled" level, for now.
   steps %q{
-    And I am on "http://learn.code.org/s/netsim/stage/1/puzzle/1?disableCleaning=true"
+    And I am on "http://learn.code.org/s/netsim/stage/3/puzzle/4?disableCleaning=true"
     And I wait to see ".netsim_lobby"
   }
 end
@@ -18,7 +19,9 @@ When /^I add a router$/ do
     When I press the "Add Router" button
     And I wait to see a ".router_row"
     And I wait until ".router_row" contains text "Ready"
+    And I wait for 0.25 seconds
   }
+  # All the waiting is important for the stability of the tests
 end
 
 When /^I select the first router$/ do
