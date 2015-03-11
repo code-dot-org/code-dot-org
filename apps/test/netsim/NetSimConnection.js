017e73248e94a7608b44f3bc7615ccce9d8b4241
Brad Buchanan <brad@code.org>
2015-03-11 10:29:05 -0700

    Pass options hash to NetSimConnection constructor

--- apps/test/netsim/NetSimConnection.js
+++ apps/test/netsim/NetSimConnection.js
@@ -24,7 +24,10 @@ describe("NetSimConnection", function () {
     testWindow = {
       addEventListener: function () {}
     };
-    connection = new NetSimConnection(testWindow, {}, null, null, false);
+    connection = new NetSimConnection({
+      window: testWindow,
+      enableCleanup: false
+    });
   });
 
   describe("getAllNodes", function () {
