4176824647ae1887cbe5a9f1114b7b9626ff3bdc
Continuous Integration <dev@code.org>
2015-03-13 21:14:49 +0000

    Automatically built.
    commit f498f462ada46ab8fd7c78e7ea6369518e1d70e0
    Author: cpirich <chris@code.org>
    Date:   Fri Mar 13 14:06:22 2015 -0700
    
        set minHeight for container in case vizColumn is taller than MIN_WORKSPACE_HEIGHT, remove flappy hardcoded witdth

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ded5a18180196c353dfb3487dc9472fa0f43ea67fb0125afea7ffc2a211fc7f
-size 547054
+oid sha256:eec230319ba4b0e917027ef9a94726c43d70af26c65ec94263229cfa055366fe
+size 547004
