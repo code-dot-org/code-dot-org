5b7e4b920e0cf03d6a25852d68af8566451b2809
Josh Lory <josh.lory@code.org>
2015-03-13 16:05:14 -0700

    Add test_update_channel rake test

--- shared/test/test_channels.rb
+++ shared/test/test_channels.rb
@@ -13,13 +13,29 @@ class ChannelsTest < Minitest::Unit::TestCase
   end
 
   def test_create_channel
-    post '/v3/channels', {'hello' => 'world'}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
+    post '/v3/channels', {hello: 'world'}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
     assert last_response.redirection?
     follow_redirect!
 
     response = JSON.parse(last_response.body)
     assert last_request.url.end_with? "/#{response['id']}"
     assert_equal 'world', response['hello']
+  end
+
+  def test_update_channel
+    post '/v3/channels', {abc: 123}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
+    channel_id = last_response.location.split('/').last
+
+    get "/v3/channels/#{channel_id}"
+    assert last_response.ok?
+    assert_equal 123, JSON.parse(last_response.body)['abc']
+
+    post "/v3/channels/#{channel_id}", {abc: 456}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
+    assert last_response.successful?
+
+    get "/v3/channels/#{channel_id}"
+    assert last_response.ok?
+    assert_equal 456, JSON.parse(last_response.body)['abc']
   end
 
   def test_delete_channel
