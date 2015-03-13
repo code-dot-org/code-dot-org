0e222f41bfcd85b7e9a03e7a020e6f462ccb1578
Josh Lory <josh.lory@code.org>
2015-03-13 14:48:31 -0700

    Update channel tests (manually parsing auth cookie for now)

--- shared/test/test_channels.rb
+++ shared/test/test_channels.rb
@@ -16,6 +16,25 @@ class ChannelsTest < Minitest::Unit::TestCase
     post '/v3/channels', {'hello' => 'world'}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
     assert last_response.redirection?
     follow_redirect!
-    assert_equal JSON.parse(last_response.body)['hello'], 'world'
+
+    response = JSON.parse(last_response.body)
+    assert last_request.url.end_with? "/#{response['id']}"
+    assert_equal 'world', response['hello']
+  end
+
+  def test_delete_channel
+    post '/v3/channels', {}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
+    cookie = last_response.header['Set-Cookie'].split(';').first
+    channel_id = last_response.location.split('/').last
+
+    get "/v3/channels/#{channel_id}"
+    assert last_response.ok?
+
+    set_cookie cookie
+    delete "/v3/channels/#{channel_id}"
+    assert last_response.successful?
+
+    get "/v3/channels/#{channel_id}"
+    assert last_response.not_found?
   end
 end
