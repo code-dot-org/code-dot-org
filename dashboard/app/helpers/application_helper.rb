9314a77077dff0823ec48f39287c632c79a20c05
Laurel Fan <laurel@code.org>
2015-03-14 00:31:05 -0700

    fix /c/ urls

--- dashboard/app/helpers/application_helper.rb
+++ dashboard/app/helpers/application_helper.rb
@@ -116,6 +116,7 @@ module ApplicationHelper
     
     # playlab/studio and artist/turtle can have images
     if opts[:level_source].try(:level_source_image).try(:image)
+      level_source = opts[:level_source]
       if level_source.level_source_image.s3?
         if app == Game::ARTIST then
           level_source.level_source_image.s3_framed_url
