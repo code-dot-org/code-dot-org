3c774d3b632c34044c2ff3c8190d60060c5cbc45
Geoffrey Elliott <geoffrey@code.org>
2015-03-14 08:42:46 -0700

    Move middleware above ActionDispatch::ParamsParser which, I suspect, is eating the invalid parameters. If I'm correct, this will cause the HoneyBadger alert to fire again.

--- dashboard/config/application.rb
+++ dashboard/config/application.rb
@@ -19,15 +19,14 @@ Bundler.require(:default, Rails.env)
 module Dashboard
   class Application < Rails::Application
 
-    # Make sure these middleware are at the very top of the stack and in the correct order:
-    config.middleware.use VarnishEnvironment
-    config.middleware.use ChannelsApi
-    config.middleware.use PropertiesApi
-    config.middleware.use TablesApi
-    config.middleware.use SharedResources
+    config.middleware.insert_before ActionDispatch::ParamsParser, VarnishEnvironment
+    config.middleware.insert_after VarnishEnvironment, ChannelsApi
+    config.middleware.insert_after ChannelsApi, PropertiesApi
+    config.middleware.insert_after PropertiesApi, TablesApi
+    config.middleware.insert_after TablesApi, SharedResources
     if CDO.dashboard_enable_pegasus
       require 'pegasus_sites'
-      config.middleware.use PegasusSites
+      config.middleware.insert_after SharedResources, PegasusSites
     end
 
     config.encoding = 'utf-8'
