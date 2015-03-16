e8e9dbca4146ede5bf9d8b74a258a63313ba8dfe
Brendan Reville <brendan@code.org>
2015-03-16 11:35:56 +1100

    Donor slider now has See All & Donate links, and shows one slot on mobile layout. Includes special casing to avoid infinite loop when there's only one slot showing and user is hovering over it.

--- pegasus/sites.v3/code.org/public/js/donor_slider2.js
+++ pegasus/sites.v3/code.org/public/js/donor_slider2.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
   var badges = $(".badge");
 
-  var numberSlotsShowing = 3; // How many slots are shown at a time
   var totalDonors = badges.length;
  
   // We will update one dest slot at a time, with the next source slot to be shown.
   var destIndex = -1;
-  var donorIndex = numberSlotsShowing;
+  var donorIndex = 0;
 
   var interval = 3000;
-  var timer = setInterval(updateDonorSlides, interval);
+  setInterval(updateDonorSlides, interval);
   updateDonorSlides();
 
   var hoverIndex = -1;
 
   function updateDonorSlides() {
-    // Fade out the current badge, and then discard it.
-    // Copy the next badge into that place, and fade it on.
 
+    // There are 1 or 3 slots showing.
+    var numberSlotsShowing = $("#badgedest_1").css("display") == "none" ? 1 : 3;
+
+    // If we are hovering on the 1 slot, then there's nothing to do
+    if (numberSlotsShowing == 1 && hoverIndex == 0) {
+      return;
+    }
+
+    // Don't use a destination if user is hovering over it.
     do {
       destIndex = (destIndex + 1) % numberSlotsShowing;
     } while (destIndex == hoverIndex);
@@ -28,6 +34,9 @@ $(document).ready(function() {
 
     var srcClone = $("#donorbadge_" + donorIndex).clone();
     var badgeDest = $("#badgedest_" + destIndex);
+
+    // Fade out the current badge, and then discard it.
+    // Copy the next badge into that place, and fade it on.
 
     badgeDest.animate({opacity:0}, {complete: function() {
       badgeDest.empty();
@@ -41,7 +50,7 @@ $(document).ready(function() {
   function donorHoverStart(index) { hoverIndex = index; };
   function donorHoverEnd()        { hoverIndex = -1; };
 
-  for (var i = 0; i < numberSlotsShowing; i++) {
+  for (var i = 0; i < 3; i++) {
     // bind will fail on IE8 but that's okay, this is kind of optional functionality.
     $("#badgedest_" + i).hover(donorHoverStart.bind(null, i), donorHoverEnd);
   }
