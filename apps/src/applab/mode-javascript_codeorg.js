51dcf9d6300ac9f86f8aa227af36d880cff5c851
Brent Van Minnen <bjvanminnen@gmail.com>
2015-03-16 13:53:20 -0700

    make jshint more strict, and ability to map error strings

--- apps/src/applab/mode-javascript_codeorg.js
+++ apps/src/applab/mode-javascript_codeorg.js
@@ -1,6 +1,4 @@
-console.log('define codeorg');
-
-// define ourselves for ace, s that it knows where to get us
+// define ourselves for ace, so that it knows where to get us
 ace.define("ace/mode/javascript_codeorg",["require","exports","module","ace/lib/oop","ace/mode/javascript","ace/mode/javascript_highlight_rules","ace/worker/worker_client","ace/mode/matching_brace_outdent","ace/mode/behaviour/cstyle","ace/mode/folding/cstyle","ace/config","ace/lib/net"], function(acerequire, exports, module) {
 
 var oop = acerequire("ace/lib/oop");
@@ -11,8 +9,6 @@ var MatchingBraceOutdent = acerequire("./matching_brace_outdent").MatchingBraceO
 var CstyleBehaviour = acerequire("./behaviour/cstyle").CstyleBehaviour;
 var CStyleFoldMode = acerequire("./folding/cstyle").FoldMode;
 
-console.log('mode-javascript_codeorg');
-
 var Mode = function() {
     this.HighlightRules = JavaScriptHighlightRules;
     this.$outdent = new MatchingBraceOutdent();
@@ -22,16 +18,24 @@ var Mode = function() {
 oop.inherits(Mode, JavaScriptMode);
 
 (function() {
-  console.log('anon');
+  var errorMap = {};
+  errorMap["Assignment in conditional expression"] = "For conditionals, use the comparison operator (==) to check if two things are equal.";
 
   this.createWorker = function(session) {
-    console.log('createWorker');
-
     var worker = new WorkerClient(["ace"], "ace/mode/javascript_worker", "JavaScriptWorker");
     worker.attachToDocument(session.getDocument());
+    worker.send("changeOptions", [{
+      unused: true
+    }]);
 
     worker.on("jslint", function(results) {
-      console.log('jslint');
+      results.data.forEach(function (item) {
+        var errorText = errorMap[item.raw];
+        if (errorText) {
+          // replace existing text
+          item.text = errorText;
+        }
+      });
       session.setAnnotations(results.data);
     });
 
