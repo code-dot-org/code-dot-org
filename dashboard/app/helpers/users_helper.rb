aa66afabb1e5c4f71ec3c3e51c362ea8d4c7a855
Josh Lory <josh.lory@code.org>
2015-03-16 16:36:35 -0700

    Minor cleanup

--- dashboard/app/helpers/users_helper.rb
+++ dashboard/app/helpers/users_helper.rb
@@ -2,45 +2,37 @@ module UsersHelper
 
   # Summarize the current user's progress within a certain script.
   def summarize_user_progress(script)
+    user_data = {}
     if current_user
-      user_data = {
-          linesOfCode: current_user.total_lines,
-          linesOfCodeText: t('nav.popup.lines', lines: current_user.total_lines),
-      }
+      lines = current_user.total_lines
+      script_levels = current_user.levels_from_script(script)
 
       user_data[:disableSocialShare] = true if current_user.under_13?
 
       if script.trophies
         progress = current_user.progress(script)
-        concepts = current_user.concept_progress(script)
-
         user_data[:trophies] = {
             current: progress['current_trophies'],
             of: t(:of),
-            max: progress['max_trophies']
+            max: progress['max_trophies'],
         }
 
-        concepts.each_pair do |concept, counts|
+        current_user.concept_progress(script).each_pair do |concept, counts|
           user_data[:trophies][concept.name] = counts[:current].to_f / counts[:max]
         end
-
       end
-
-      # Get all user_levels
-      levels = current_user.levels_from_script(script)
     else
       lines = session[:lines] || 0
-      user_data = {
-        linesOfCode: lines,
-        linesOfCodeText: t('nav.popup.lines', lines: lines),
-      }
-
-      # Check all levels for progress
-      levels = script.script_levels
+      script_levels = script.script_levels
     end
 
+    user_data.merge!(
+        linesOfCode: lines,
+        linesOfCodeText: t('nav.popup.lines', lines: lines),
+    )
+
     user_data[:levels] = {}
-    levels.each do |sl|
+    script_levels.each do |sl|
       completion_status, _ = level_info(current_user, sl)
       if completion_status != 'not_tried'
         user_data[:levels][sl.level.id] = {
