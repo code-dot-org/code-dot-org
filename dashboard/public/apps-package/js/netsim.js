f01f475af6492a362fe97d5960eec962a5bcd0af
Continuous Integration <dev@code.org>
2015-03-16 21:25:44 +0000

    Automatically built.
    commit 45712bd1a9965393467f8572687dd93dc48a5431
    Merge: b9188bd fc1fb42
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Mon Mar 16 14:17:45 2015 -0700
    
        Merge pull request #1616 from code-dot-org/netsim_level_instructions
    
        NetSim: Put different, localized, instructions in each level
    
    commit fc1fb42867208f9fdebcf71dfec651b313ad120e
    Author: Brad Buchanan <brad@code.org>
    Date:   Mon Mar 16 13:39:35 2015 -0700
    
        Use existing instructions+localization system, and basic skin for instrutions dialog icon
    
    commit b9188bd9feb8f897aeaf4cab0cc988edca52e262
    Author: Continuous Integration <dev@code.org>
    Date:   Mon Mar 16 20:13:06 2015 +0000
    
        Automatically built.
    
        commit 6d865eacf1afe1813e40df3941610d8a22fdc989
        Merge: 3a76fb9 bb90cc5
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Mon Mar 16 13:04:35 2015 -0700
    
            Merge pull request #1626 from code-dot-org/fix_functional_edit_while_editor_open
    
            Fix: when contract editor is open and you try to open it for another function or the same function, weird stuff starts to happen [resolves #90445568]
    
        commit 3a76fb99d58ec678f58d3a0887505c620049b66a
        Author: Josh Lory <josh.lory@code.org>
        Date:   Mon Mar 16 12:54:10 2015 -0700
    
            Require a hash in the channel storage API, so metadata can be merged
    
            Use JSON.parse when a hash is expected so we throw sooner if the data is bad.
            Raise 400 Bad Request if the client tries to save something that isn't a hash.
    
    commit 6d865eacf1afe1813e40df3941610d8a22fdc989
    Merge: 3a76fb9 bb90cc5
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Mon Mar 16 13:04:35 2015 -0700
    
        Merge pull request #1626 from code-dot-org/fix_functional_edit_while_editor_open
    
        Fix: when contract editor is open and you try to open it for another function or the same function, weird stuff starts to happen [resolves #90445568]

--- dashboard/public/apps-package/js/netsim.js
+++ dashboard/public/apps-package/js/netsim.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ae6f4fe21391c6c40d6ada75eea7f21e5d192555afce93a7d95e60105ea8d14
-size 276544
+oid sha256:b934704f8e509820703082dd443ee2da1c931c04e653cc24abc2f0aae18ee344
+size 277116
