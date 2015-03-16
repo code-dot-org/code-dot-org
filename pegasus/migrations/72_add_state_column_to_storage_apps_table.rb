a4ae3f64b266106161a5bd910e8422cdf49e7e60
Josh Lory <josh.lory@code.org>
2015-03-15 21:23:56 -0700

    Set a default so queries don't need to handle null

--- pegasus/migrations/72_add_state_column_to_storage_apps_table.rb
+++ pegasus/migrations/72_add_state_column_to_storage_apps_table.rb
@@ -1,6 +1,6 @@
 Sequel.migration do
   up do
-    add_column :storage_apps, :state, String, size: 50
+    add_column :storage_apps, :state, String, size: 50, default: 'active', null: false
   end
 
   down do
