4e8b0132ac5f528b4ddfbbed05d7c09ad48c208e
Continuous Integration <dev@code.org>
2015-03-17 17:51:38 +0000

    Automatically built.
    commit 507587e1276966d5b0accb096cadf81eae4edff8
    Merge: 2f70b67 8ef7e87
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Mar 17 10:43:13 2015 -0700
    
        Merge pull request #1628 from code-dot-org/aceJSMode
    
        Custom JS mode for code.org
    
    commit 8ef7e8775f6c40eb4d378713d77fcdc78f9feefa
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Mar 17 10:42:34 2015 -0700
    
        dont bother with source maps
    
    commit 2f70b67df0c701377ca884fd0734cdea967e4e40
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Mar 17 17:38:05 2015 +0000
    
        Automatically built.
    
        commit bae626e1b6e077da38e464b8108d7873cb324764
        Merge: e4964e0 fd19570
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Tue Mar 17 10:29:39 2015 -0700
    
            Merge pull request #1641 from code-dot-org/editParams
    
            no icon when editing params via modal editor
    
        commit e4964e0807fd0506edef05b71fc098e51a13dfd2
        Author: Josh Lory <josh.lory@code.org>
        Date:   Mon Mar 16 22:58:46 2015 -0700
    
            Rubocop warnings
    
            Avoid rescuing the Exception class.
            Redundant use of Object#to_s in interpolation.
            Unused method argument.
            Unused block argument.
            File.exists? is deprecated in favor of File.exist?.
            Shadowing outer local variable.
            Useless assignment to variable.
            Useless private access modifier.
    
    commit bae626e1b6e077da38e464b8108d7873cb324764
    Merge: e4964e0 fd19570
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Mar 17 10:29:39 2015 -0700
    
        Merge pull request #1641 from code-dot-org/editParams
    
        no icon when editing params via modal editor
    
    commit e4964e0807fd0506edef05b71fc098e51a13dfd2
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon Mar 16 22:58:46 2015 -0700
    
        Rubocop warnings
    
        Avoid rescuing the Exception class.
        Redundant use of Object#to_s in interpolation.
        Unused method argument.
        Unused block argument.
        File.exists? is deprecated in favor of File.exist?.
        Shadowing outer local variable.
        Useless assignment to variable.
        Useless private access modifier.

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b79a68a8b6ac3ae41f182bc5961d812d258515bbb5736be31c4717508daa719
-size 140274
+oid sha256:4855136362d4af492b078877e2e565a66001b7cfd8f012b1de6a4f8d833e4f5a
+size 142416
--- dashboard/public/apps-package/js/netsim.js
+++ dashboard/public/apps-package/js/netsim.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b934704f8e509820703082dd443ee2da1c931c04e653cc24abc2f0aae18ee344
+oid sha256:f4656cc4ebbf1dde937eb4a654ba4eee8bc8d10e4c9257761d4965c2760b40b5
 size 277116
