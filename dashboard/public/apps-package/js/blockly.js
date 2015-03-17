eb9c7a0ec4a80ca4b8f5f392637399c093c84c15
Continuous Integration <dev@code.org>
2015-03-17 00:21:01 +0000

    Automatically built.
    commit 91ca8bd0c1a84c9ed2529c4aa22f871264752b63
    Merge: 65b981e 445169c
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Mar 16 17:12:22 2015 -0700
    
        Merge pull request #1634 from code-dot-org/visibleFunctions
    
        hide function defs when using modal editor
    
    commit 445169cab8f525fa9882e7fc03832c1d29672591
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Mar 16 17:02:03 2015 -0700
    
        hide function defs when using modal editor

--- dashboard/public/apps-package/js/blockly.js
+++ dashboard/public/apps-package/js/blockly.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:766fff6bf7b3a56765a4013952b27712ea7794fdf491609f1be8470e05e7168c
-size 744143
+oid sha256:5c9ea92a5da2db5145e28a8a8dcb8938ee87eb6192110e1e144d7973d769a757
+size 744398
