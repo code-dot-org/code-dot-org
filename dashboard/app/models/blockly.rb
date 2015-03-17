38cf251633c9b7fa64141eb3a2447f4e4f8f6a40
Josh Lory <josh.lory@code.org>
2015-03-16 20:10:26 -0700

    Fix rake test

--- dashboard/app/models/blockly.rb
+++ dashboard/app/models/blockly.rb
@@ -41,7 +41,11 @@ class Blockly < Level
 
   # These serialized fields will be serialized/deserialized as straight XML
   def xml_blocks
-    %w(start_blocks toolbox_blocks required_blocks) unless game.uses_droplet?
+    if game && game.uses_droplet?
+      []
+    else
+      %w(start_blocks toolbox_blocks required_blocks)
+    end
   end
 
   def to_xml(options={})
