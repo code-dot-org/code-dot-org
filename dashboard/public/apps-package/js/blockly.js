bf1cb45709ae09a33158ca2fd596d5c52a2a8b2c
Continuous Integration <dev@code.org>
2015-03-17 00:53:01 +0000

    Automatically built.
    commit 14e4918fdd3fd6495fcae7b165f1f20ecaeb6670
    Merge: 927ea40 1515b5a
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Mon Mar 16 17:44:41 2015 -0700
    
        Merge pull request #1637 from code-dot-org/render_blocks_after_deserialization
    
        Force a render after function editor deserialization
    
    commit 1515b5a6835eb004a380060096a24bc649f12360
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Mon Mar 16 17:38:10 2015 -0700
    
        Force a render after function editor deserialization

--- dashboard/public/apps-package/js/blockly.js
+++ dashboard/public/apps-package/js/blockly.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c9ea92a5da2db5145e28a8a8dcb8938ee87eb6192110e1e144d7973d769a757
-size 744398
+oid sha256:9291e75ce0cd3b03beef4b74ef9476d3510bf0e096b00f89936be66893209800
+size 744464
