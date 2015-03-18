9db9889de3cb8386fbfbcb63628ea7fe9c3eb2ac
Continuous Integration <dev@code.org>
2015-03-18 16:56:32 +0000

    Automatically built.
    commit e10927282504d107e8a376b13780e3966b08e135
    Merge: 97f51dd c41e1d5
    Author: Chris Pirich <chris@code.org>
    Date:   Wed Mar 18 09:48:48 2015 -0700
    
        Merge pull request #1597 from code-dot-org/studio_header_cleanup
    
        update header resize to primarily use CSS, enable clear puzzle for droplet
    
    commit 97f51dd5b00adc445fe288f866e801e28ef86594
    Author: Brendan Reville <brendan@code.org>
    Date:   Wed Mar 18 21:23:02 2015 +1100
    
        Add another /index2 variant with a mission description.

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03bfbea6f6a2e66307d5ea6cc693973a10baa08bf36fcc61eb46b584d814da3a
-size 547462
+oid sha256:c9db86fc140a85db5ed10221d5e287b20d91fcce420b5a39b5d4a1a4618d6f2a
+size 545386
