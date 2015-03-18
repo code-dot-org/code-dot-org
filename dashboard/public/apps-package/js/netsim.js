2bc1922cc2bba2a48e7dbe20f1f0e9554f47a4ca
Continuous Integration <dev@code.org>
2015-03-18 19:37:33 +0000

    Automatically built.
    commit ecf70407b7aade9f8ec41f3fdd2b593c328b1f77
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Mar 18 12:29:36 2015 -0700
    
        Fix UI tests, continue to use correct style for css class-names

--- dashboard/public/apps-package/js/netsim.js
+++ dashboard/public/apps-package/js/netsim.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a0cbc783f4e10bdce9a3003fde761fe2b6838bb6538944bc1453b63791318ed
+oid sha256:47c19c90e43d9b5adc45d7df02edf78841d2d05a86bb580e3e1720cba2c93049
 size 283156
