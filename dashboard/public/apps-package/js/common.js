1f85e1a6fb57cacb60eea0aa68de100da400a32c
Continuous Integration <dev@code.org>
2015-03-18 17:56:35 +0000

    Automatically built.
    commit c2f628cff5ebd010b1477df9f208a88074de7fca
    Merge: 91f94e5 ead565b
    Author: Chris Pirich <chris@code.org>
    Date:   Wed Mar 18 10:48:29 2015 -0700
    
        Merge pull request #1661 from code-dot-org/applab_droplet_integration
    
        Applab droplet integration
    
    commit 91f94e5fd3365471bd423a69fc4344abac9a2e02
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Mar 18 17:44:30 2015 +0000
    
        Automatically built.
    
        commit 683ccbf5010169a6a9e8f4d2b33bce261ac2d4ca
        Author: cpirich <chris@code.org>
        Date:   Wed Mar 18 10:34:26 2015 -0700
    
            Update droplet through commit b36950cc933fb64a0d640726ae964439792d64c5
    
    commit ead565bc58abc279e8bf96d13ff0ac84bb44eaf3
    Author: cpirich <chris@code.org>
    Date:   Wed Mar 18 10:36:57 2015 -0700
    
        use colors with droplet per spec
    
    commit 2d1717725759f0576306727b6e2b0a5c18bcfb35
    Author: cpirich <chris@code.org>
    Date:   Wed Mar 18 10:36:34 2015 -0700
    
        fix applab positioning in the noheader case

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9db86fc140a85db5ed10221d5e287b20d91fcce420b5a39b5d4a1a4618d6f2a
-size 545386
+oid sha256:f15ddd019268bbea400a45245f0b9febe5878af4831879165c35181f67415b12
+size 545473
