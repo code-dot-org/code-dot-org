4a625c57f91f5b57f4957fb5402f5945790a371c
Continuous Integration <dev@code.org>
2015-03-19 04:28:29 +0000

    Automatically built.
    commit 972fd352431649026fff5d13e1d18cdfab57ed3a
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Mar 18 21:19:52 2015 -0700
    
        Fix jshint

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72657aec7cf04624f7c7013468300772cb43271946cc527aad4feaf767e18dab
-size 301112
+oid sha256:b0176f265fb30f825ca6f966ead678ac4a99ffcbf7e17cce543ac93c2b326e33
+size 301259
