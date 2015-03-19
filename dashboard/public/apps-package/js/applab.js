aadb0a9255190a52356fda5f17c04c35842dc4eb
Continuous Integration <dev@code.org>
2015-03-19 18:49:30 +0000

    Automatically built.
    commit d5f35581bddbefedc693d37043c1891af4a95fdd
    Merge: 43254f6 8ea4fbc
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Mar 19 11:37:12 2015 -0700
    
        Merge pull request #1685 from code-dot-org/applabAutocomplete
    
        better naming
    
    commit 8ea4fbc0e498db23e302d14127c6860d16fbf92e
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Mar 19 11:35:53 2015 -0700
    
        better naming

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a4b8a7cbf134a97c7ab5960c771cca68874d52baa2d3a8d8f888826e207d3d3
-size 144468
+oid sha256:1634dc56cab380673eb53bfcda999a4b269c483e4b819b746bec266429472d78
+size 145338
