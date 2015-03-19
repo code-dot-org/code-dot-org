aa00f6583ce3a0d269cd11f0b4b7f48ae608d6f7
Continuous Integration <dev@code.org>
2015-03-19 19:25:32 +0000

    Automatically built.
    commit 45b375c6fd193a2d4ddb1e3e1791316bbfa2cf30
    Merge: 1bc1bc6 4e847a8
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Mar 19 12:15:54 2015 -0700
    
        Merge pull request #1688 from code-dot-org/keepPalette2
    
        Keep palette
    
    commit 1bc1bc63de8f945a6a49a944a7d5f3652a42adc4
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Mar 19 19:15:29 2015 +0000
    
        Automatically built.
    
        commit a9029806dcc3470d82b42fc9876171554e5923f6
        Author: cpirich <chris@code.org>
        Date:   Thu Mar 19 12:07:21 2015 -0700
    
            update droplet toolset with npm install and rebuild
    
    commit 4e847a8f262a3a87736ed3f3e9ca5e0265100685
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Mar 19 11:50:50 2015 -0700
    
        always show palette

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4683e9ce0022df920acb3caf0b71d1b5eacdb7e88d79ea8b85a2b660ca61e12
-size 546598
+oid sha256:00bdaaa1d458204d229cfde6f1535270e2c87493c9f5bc3d58a5aec6c96abf64
+size 546629
--- dashboard/public/apps-package/js/droplet/droplet-full.js
+++ dashboard/public/apps-package/js/droplet/droplet-full.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce08c5eeceefee1a2b5de21b2a6c5f449ac3b42b58b6ad57a534d6d146313849
-size 462574
+oid sha256:52465b51072e42f5f309343454f87f8537e192139024f96f66a9420649dbdd4c
+size 462793
