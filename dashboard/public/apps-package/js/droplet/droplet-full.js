46b6a8e7f6c2781c6be24c2724c3aebdfeb08411
Continuous Integration <dev@code.org>
2015-03-19 19:15:29 +0000

    Automatically built.
    commit a9029806dcc3470d82b42fc9876171554e5923f6
    Author: cpirich <chris@code.org>
    Date:   Thu Mar 19 12:07:21 2015 -0700
    
        update droplet toolset with npm install and rebuild

--- dashboard/public/apps-package/js/droplet/droplet-full.js
+++ dashboard/public/apps-package/js/droplet/droplet-full.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69dabf775147242109fcb582c5da4684c79171fd42f45023010254141b4a4819
-size 462707
+oid sha256:ce08c5eeceefee1a2b5de21b2a6c5f449ac3b42b58b6ad57a534d6d146313849
+size 462574
