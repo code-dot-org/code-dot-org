acc19c40a895601ba9f8af23633ca4ae1ed0ffd8
Continuous Integration <dev@code.org>
2015-03-19 19:25:32 +0000

    Automatically built.
    commit 45b375c6fd193a2d4ddb1e3e1791316bbfa2cf30
    Merge: 1bc1bc6 4e847a8
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Mar 19 12:15:54 2015 -0700
    
        Merge pull request #1688 from code-dot-org/keepPalette2
    
        Keep palette
    
    commit 1bc1bc63de8f945a6a49a944a7d5f3652a42adc4
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Mar 19 19:15:29 2015 +0000
    
        Automatically built.
    
        commit a9029806dcc3470d82b42fc9876171554e5923f6
        Author: cpirich <chris@code.org>
        Date:   Thu Mar 19 12:07:21 2015 -0700
    
            update droplet toolset with npm install and rebuild
    
    commit 4e847a8f262a3a87736ed3f3e9ca5e0265100685
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Mar 19 11:50:50 2015 -0700
    
        always show palette

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -5651,7 +5651,8 @@ StudioApp.prototype.handleEditCode_ = function (options) {
       mode: 'javascript',
       modeOptions: dropletUtils.generateDropletModeOptions(options.dropletConfig),
       palette: dropletUtils.generateDropletPalette(options.codeFunctions,
-        options.dropletConfig)
+        options.dropletConfig),
+      alwaysShowPalette: true
     });
 
     this.editor.aceEditor.setShowPrintMargin(false);
