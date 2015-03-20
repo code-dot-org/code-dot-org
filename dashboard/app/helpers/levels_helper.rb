46cc368d625e318b5bbf3e09de9e5754b1984eec
Brent Van Minnen <bjvanminnen@gmail.com>
2015-03-20 15:26:54 -0700

    cleanup input output table

--- dashboard/app/helpers/levels_helper.rb
+++ dashboard/app/helpers/levels_helper.rb
@@ -256,7 +256,7 @@ module LevelsHelper
     level['scale'] = {'stepSpeed' =>  @level.properties['step_speed'].to_i } if @level.properties['step_speed'].present?
 
     # Blockly requires these fields to be objects not strings
-    %w(map initialDirt finalDirt goal soft_buttons).each do |x|
+    %w(map initialDirt finalDirt goal soft_buttons inputOutputTable).each do |x|
       level[x] = JSON.parse(level[x]) if level[x].is_a? String
     end
 
