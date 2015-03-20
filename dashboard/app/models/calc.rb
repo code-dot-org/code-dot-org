3037e1a738d1cb767b3dddaa6f10652ba13914f2
Brent Van Minnen <bjvanminnen@gmail.com>
2015-03-20 15:26:54 -0700

    cleanup input output table

--- dashboard/app/models/calc.rb
+++ dashboard/app/models/calc.rb
@@ -6,7 +6,6 @@ class Calc < Blockly
   )
 
   before_save :update_ideal_level_source
-  before_save :update_input_output_table
 
   def xml_blocks
     super + %w(solution_blocks)
@@ -28,12 +27,6 @@ class Calc < Blockly
           use_contract_editor: true
         }
     ))
-  end
-
-  def update_input_output_table
-    if self.input_output_table.present? && self.input_output_table.is_a?(String)
-      self.input_output_table = JSON.parse(self.input_output_table)
-    end
   end
 
   def self.toolbox
