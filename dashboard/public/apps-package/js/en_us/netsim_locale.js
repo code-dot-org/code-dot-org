6f3292565d09de63fc20aaf1652192ecc782ef78
Continuous Integration <dev@code.org>
2015-03-23 23:10:20 +0000

    Automatically built.
    commit bc12a73b1e6e9e7a5f4419b0c321060927cf3487
    Merge: db88ad8 0141126
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Mon Mar 23 16:02:13 2015 -0700
    
        Merge pull request #1725 from code-dot-org/netsim_automatic_dns
    
        NetSim: Cleanup, prep for auto-dns work
    
    commit db88ad8e425957ee15595ebfe6133e6e768081e1
    Author: Continuous Integration <dev@code.org>
    Date:   Mon Mar 23 22:32:37 2015 +0000
    
        dropbox changes
    
    commit b9ead18679925437271269adc2734fa331395f5d
    Merge: 64caba0 5ff1127
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Mar 23 15:16:58 2015 -0700
    
        Merge pull request #1727 from code-dot-org/artistWaitForClose
    
        have artist wait for xclose so that it's definitely loaded
    
    commit 5ff1127a523e8939e535b08df3d6d35e4bba66a1
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Mar 23 15:13:02 2015 -0700
    
        have artist wait for xclose so that it's definitely loaded
    
    commit 64caba0176ef60c8fc8fe9ba9a01ed96db8836e0
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Mon Mar 23 15:02:09 2015 -0700
    
        add frozen and other tutorial videos for translation
    
    commit 0141126fd6501d3222ecc0e359c7eb07673a9823
    Author: Brad Buchanan <brad@code.org>
    Date:   Mon Mar 23 14:54:49 2015 -0700
    
        Rename test from PacketEncoder to Packet

--- dashboard/public/apps-package/js/en_us/netsim_locale.js
+++ dashboard/public/apps-package/js/en_us/netsim_locale.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d19cbc6fdbaef204894d87e15d93099e2dfb5374269e196583389e422e7de1d4
-size 7385
+oid sha256:c9fc9b06e3d3887d5531ffc40dab9ef12c97f98fceda0bb62e661a49d304c8d5
+size 7445
