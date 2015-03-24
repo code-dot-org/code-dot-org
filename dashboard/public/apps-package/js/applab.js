02d95959dbe95e397af3507e63b77d48d62580d6
Continuous Integration <dev@code.org>
2015-03-24 01:47:14 +0000

    Automatically built.
    commit b7315165c9b6013ca83a7ac6399f9f6071703235
    Merge: 3faf2f3 4dd49fa
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Mon Mar 23 18:39:27 2015 -0700
    
        Merge pull request #1729 from code-dot-org/get-key-undefined
    
        applab: return undefined when getKeyValue key is not found.
    
    commit 4dd49fa0c6b2e2e362df216114e42ae8ca61baec
    Author: David Bailey <dsb123@gmail.com>
    Date:   Mon Mar 23 18:37:01 2015 -0700
    
        provide fallback value if XmlHttpRequest.DONE is undefined
    
    commit 3faf2f369044e3f3c8031e6343479fed9d3ebc67
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Mar 24 01:33:16 2015 +0000
    
        Automatically built.
    
        commit 51567e0f36ed50286c5e05e45703b3ce0437c628
        Merge: 53a9819 f39ead3
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Mon Mar 23 18:25:18 2015 -0700
    
            Merge pull request #1730 from code-dot-org/noFloat
    
            [Finishes #90794312] Calc should display ints instead of floats for integers
    
        commit 53a9819683bac59cade2142de9a5bc53e0e01a55
        Author: Continuous Integration <dev@code.org>
        Date:   Tue Mar 24 01:20:46 2015 +0000
    
            Automatically built.
    
            commit 5c0e942659bfc6e0844b2709145daf776c26ab1d
            Author: Tanya Parker <tanyacparker@gmail.com>
            Date:   Mon Mar 23 18:12:14 2015 -0700
    
                code.org translations 03/23
    
    commit 51567e0f36ed50286c5e05e45703b3ce0437c628
    Merge: 53a9819 f39ead3
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Mar 23 18:25:18 2015 -0700
    
        Merge pull request #1730 from code-dot-org/noFloat
    
        [Finishes #90794312] Calc should display ints instead of floats for integers

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90b433ab881fa7e179d6361b6ef3d1b95c293b7fda980a294fe49fe85be0ac28
-size 146103
+oid sha256:275cc894851cdf28576e4a4eef70c8934e120643fa9e7f62b8e42863469d99a6
+size 146426
