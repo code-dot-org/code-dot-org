8bf0526c49e65899bad4940ecb28315766b76c71
Continuous Integration <dev@code.org>
2015-03-24 19:32:21 +0000

    Automatically built.
    commit 20be763c6c1cea34c777870a93b7d94dfcc44fbd
    Merge: b4cfa42 e3da3fa
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Mar 24 12:22:47 2015 -0700
    
        Merge pull request #1736 from code-dot-org/calcStringFix
    
        [Finishes #91023316] fix string in calc
    
    commit e3da3fa20255a50ca15d2e0b657e0e2bb8d52b74
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Mar 24 12:21:26 2015 -0700
    
        fix string in calc

--- dashboard/public/apps-package/js/en_us/calc_locale.js
+++ dashboard/public/apps-package/js/en_us/calc_locale.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbb7a7dda202499e520b8febb93eae9a879ed978976822a38240255966f00241
-size 7009
+oid sha256:74cdf4bac5c262f98408090a02ceab5e9d2d9d18ba647e09b84f88099bde4df4
+size 7011
