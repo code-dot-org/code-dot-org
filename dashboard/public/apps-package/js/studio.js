ea5b949d540ebc5a0dbf1d016b07e6fedb1f6663
Continuous Integration <dev@code.org>
2015-03-24 18:32:20 +0000

    Automatically built.
    commit fadabb96b4b0fd1efcb53e9ecf013602b37cdfb8
    Merge: fc28e27 cad398e
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Mar 24 11:23:24 2015 -0700
    
        Merge pull request #1733 from code-dot-org/rocketman
    
        [Finishes #91030302] rocketman should handle undefined height
    
    commit cad398e8aa8d076aa8ae1800ad4b330f424f8182
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Mar 24 11:07:08 2015 -0700
    
        rocketman should handle undefined height

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:150c0ae26d31871e6086450a4f1fff323fcf29c6840ae88c20290078bf44bf97
-size 301259
+oid sha256:fc5f890b98bdd1cd85e9c0fbd14afccea4db569253c4434f9d24bf254ccc79c4
+size 301258
