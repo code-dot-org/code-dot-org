d9e84fe113e9320d267bdfb76b426051fe9c7c5c
Continuous Integration <dev@code.org>
2015-03-25 19:41:23 +0000

    Automatically built.
    commit 615543f0d49835fb5d4602fafd30178ccf69efc1
    Merge: bbbbe68 8855656
    Author: Chris Pirich <chris@code.org>
    Date:   Wed Mar 25 12:29:57 2015 -0700
    
        Merge pull request #1744 from code-dot-org/embed_hidesource
    
        embed: only center when hideSource is true
    
    commit bbbbe685a3fd79262e1dd60cffe60831ee53c312
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Mar 25 18:43:14 2015 +0000
    
        Automatically built.
    
        commit a2995a4397e9f78ba896e19cecd3b89a85d3caa2
        Merge: 308b44b eef6e40
        Author: David Bailey <dsb123@gmail.com>
        Date:   Wed Mar 25 11:33:19 2015 -0700
    
            Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
        commit 308b44bd82eec169e387830e3ed81a3914a698a8
        Author: David Bailey <dsb123@gmail.com>
        Date:   Wed Mar 25 11:33:11 2015 -0700
    
            fix readRecords params

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cbc52e5d635bc09a16d2b593ea63b107815699c987bdca5f0e5440b95427fd8
-size 547545
+oid sha256:b3a62bba676ca367ba6f657e74491de4b6d1e99723d5a4eb0e109ed18c2c9173
+size 547577
