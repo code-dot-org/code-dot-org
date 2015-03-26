eae0e5871c251873059f49a3383ef4d95d5c1baf
Continuous Integration <dev@code.org>
2015-03-26 20:24:12 +0000

    Automatically built.
    commit c71718cbf0b581d2cc454e6ce6b1e463cfdb865e
    Merge: eb44a4c 36e3211
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Mar 26 13:13:46 2015 -0700
    
        Merge pull request #1770 from code-dot-org/dontBumpHiddenBlocks
    
        dont bump hidden blocks
    
    commit eb44a4c2278a6d68961ac38250c9b0fb713e0ad6
    Merge: a28efbc 43b559a
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Mar 26 13:11:06 2015 -0700
    
        Merge pull request #1769 from code-dot-org/measure_https_blocking
    
        Measure https blocking with an analytics event
    
    commit 43b559a91d4690ce36e2e2160f13efece1c96bfd
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Mar 26 13:05:53 2015 -0700
    
        Run https checker after window load
    
    commit a28efbcb94076e662515b771813e5c07ce2d5152
    Merge: c5c05a4 89f08ae
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Thu Mar 26 13:03:43 2015 -0700
    
        Merge pull request #1764 from code-dot-org/design-mode-store-html
    
        Load, display, store and rudimentarily edit HTML for applab apps using design mode.
    
    commit 89f08ae4cc6d97503ba3b95935d1b07429469b3d
    Author: David Bailey <dsb123@gmail.com>
    Date:   Thu Mar 26 13:02:37 2015 -0700
    
        fix jshint

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb58962bafe3785abe6725a58449a3295f5666f3fb16d99aa8a1deeeb1cf973d
-size 150404
+oid sha256:db3a3e5b264346ccbb7b94ded9e9616899557e892ae93631097c3bb5a7d262f4
+size 152001
--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22a39fa6c93b98b6a8d606fbdfb16f7d840424e1263b1d8afe3d242090a0a0f7
-size 547392
+oid sha256:4d7ba3270b8dc58efad6dd8858c353df7489563a988072bf489a2f0df696b56a
+size 547829
