455fc33c10fb10db2c209ab56a7be1b9adba20f8
Brad Buchanan <brad@code.org>
2015-03-26 14:20:44 -0700

    Use lodash find instead of unsupported Array.prototype.find.  Oops

--- apps/src/netsim/netsimUtils.js
+++ apps/src/netsim/netsimUtils.js
@@ -123,7 +123,7 @@ var isExactlyNaN = function (val) {
  * @returns {number|string}
  */
 exports.serializeNumber = function (num) {
-  var rule = NUMBER_SERIALIZATION_RULES.find(function (rule) {
+  var rule = _.find(NUMBER_SERIALIZATION_RULES, function (rule) {
     return rule.jsVal === num || (isExactlyNaN(rule.jsVal) && isExactlyNaN(num));
   });
   return rule ? rule.jsonVal : num;
@@ -137,7 +137,7 @@ exports.serializeNumber = function (num) {
  * @returns {number|NaN}
  */
 exports.deserializeNumber = function (storedNum) {
-  var rule = NUMBER_SERIALIZATION_RULES.find(function (rule) {
+  var rule = _.find(NUMBER_SERIALIZATION_RULES, function (rule) {
     return rule.jsonVal === storedNum;
   });
   return rule ? rule.jsVal : storedNum;
