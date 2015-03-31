65a39b7acd313fdf8dc1e38167f5352c7a704093
Laurel Fan <laurel@code.org>
2015-03-31 15:38:08 -0700

    update content of invitation emails, don't set a password

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323201349) do
+ActiveRecord::Schema.define(version: 20150331173141) do
 
   create_table "activities", force: true do |t|
     t.integer  "user_id"
@@ -401,7 +401,7 @@ ActiveRecord::Schema.define(version: 20150323201349) do
 
   create_table "users", force: true do |t|
     t.string   "email",                                   default: "",      null: false
-    t.string   "encrypted_password",                      default: "",      null: false
+    t.string   "encrypted_password",                      default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -443,11 +443,22 @@ ActiveRecord::Schema.define(version: 20150323201349) do
     t.datetime "deleted_at"
     t.string   "secret_words"
     t.text     "properties"
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
+    t.integer  "invitations_count",                       default: 0
   end
 
   add_index "users", ["confirmation_token", "deleted_at"], name: "index_users_on_confirmation_token_and_deleted_at", unique: true, using: :btree
   add_index "users", ["email", "deleted_at"], name: "index_users_on_email_and_deleted_at", using: :btree
   add_index "users", ["hashed_email", "deleted_at"], name: "index_users_on_hashed_email_and_deleted_at", using: :btree
+  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
+  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
+  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["prize_id", "deleted_at"], name: "index_users_on_prize_id_and_deleted_at", unique: true, using: :btree
   add_index "users", ["provider", "uid", "deleted_at"], name: "index_users_on_provider_and_uid_and_deleted_at", unique: true, using: :btree
   add_index "users", ["reset_password_token", "deleted_at"], name: "index_users_on_reset_password_token_and_deleted_at", unique: true, using: :btree
