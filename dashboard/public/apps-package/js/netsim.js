9ef30f52d50023915fdf571a2bbbc9cd10e5283d
Continuous Integration <dev@code.org>
2015-04-01 18:47:36 +0000

    Automatically built.
    commit ed68bf844a3e50b21c7e20832b2f50a90b9e74fc
    Merge: 95deb17 a1fbe32
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Wed Apr 1 11:38:43 2015 -0700
    
        Merge pull request #1844 from code-dot-org/netsim_limit_router_memory
    
        NetSim: Limit router memory
    
    commit 95deb17f8c0f03c85ff2281a3ea7b5c430cd86b7
    Merge: ba24e26 5bba300
    Author: CaptainStack <CaptainStack@outlook.com>
    Date:   Wed Apr 1 11:09:59 2015 -0700
    
        Merge pull request #1852 from code-dot-org/ops-filter-alphabetical
    
        Ordered districts by name in dropdown menu on add-cohorts page.
    
    commit 5bba300ee48f64c2171687a0b2083ff9572e6235
    Author: CaptainStack <CaptainStack@outlook.com>
    Date:   Wed Apr 1 11:09:29 2015 -0700
    
        Ordered districts by name in dropdown menu on add-cohorts page.
    
    commit ba24e26e48730f9e8fdf8072795304c213b524b9
    Author: Laurel Fan <laurel@code.org>
    Date:   Wed Apr 1 10:48:12 2015 -0700
    
        fix test file in wrong place
    
    commit e7b5359ab2f69a1e0595b14b3c1373d83c299a1e
    Merge: 2e4804f e378d6e
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Wed Apr 1 11:37:07 2015 -0600
    
        Merge pull request #1850 from code-dot-org/cohort-teachers-csv
    
        fix ops dashboard teacher cohort csv upload
    
    commit e378d6e27c7e91664140d4d35ed3064c93262ea6
    Author: Laurel Fan <laurel@code.org>
    Date:   Wed Apr 1 10:36:55 2015 -0700
    
        text explaining that the heading line of the csv is required
    
    commit 2e4804fb3752edbf2638dcb544286d7ba528adc7
    Merge: d05abbd 2f3304d
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Wed Apr 1 11:36:54 2015 -0600
    
        Merge pull request #1822 from code-dot-org/notification-added-teachers
    
        send notification to ops team when district contacts add teachers to coh...
    
    commit b9cb221b7c72dcef69505a3748bef89f3f804eb1
    Author: Laurel Fan <laurel@code.org>
    Date:   Wed Apr 1 10:33:22 2015 -0700
    
        fix ops dashboard teacher cohort csv upload
    
    commit a1fbe3247eaaa8b0423e5ff72e893d12f9042524
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Apr 1 10:32:06 2015 -0700
    
        Comment some oddities
    
    commit 998c107edc9812d9d23bce006635e3e22d4c6a93
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Apr 1 10:16:59 2015 -0700
    
        Requested style changes.
    
    commit d05abbd687c1f2b3cf202ca10d8beeda1a68d5fa
    Author: Laurel Fan <laurel@code.org>
    Date:   Wed Apr 1 09:59:07 2015 -0700
    
        fix API DASHBOARDAPI already defined warnings
    
    commit 10c8cc26011dd26e4509b63b85958eb208d1a370
    Merge: ce4bdfa 68a0c72
    Author: CaptainStack <CaptainStack@outlook.com>
    Date:   Wed Apr 1 09:49:29 2015 -0700
    
        Merge pull request #1849 from code-dot-org/ops-column-sizing
    
        Better column sizing on teacher management page.
    
    commit 68a0c72a32f0f0ee33b7b5e781fe4e3eec717030
    Author: CaptainStack <CaptainStack@outlook.com>
    Date:   Wed Apr 1 09:48:41 2015 -0700
    
        Better column sizing on teacher management page.

--- dashboard/public/apps-package/js/netsim.js
+++ dashboard/public/apps-package/js/netsim.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1afb8f2a2bec2ab6d9547593a0baae6d3488cb5c8f13daf06cdc5125966cab33
-size 339064
+oid sha256:a44badbdc1782b8f634d43d6062999ad3fd7ed45686bf6252d768e577c62208f
+size 353842
