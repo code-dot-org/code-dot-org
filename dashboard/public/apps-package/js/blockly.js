0b271f819e8ae9ba1749cbc5d87e26b47f7668fc
Continuous Integration <dev@code.org>
2015-04-02 19:21:01 +0000

    Automatically built.
    commit 8d4d79fede032e0d06020ecdd32bbde1c7bd0180
    Merge: 6d61b14 cce87df
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Apr 2 12:11:09 2015 -0700
    
        Merge pull request #1887 from code-dot-org/unfilledInputs
    
        [Finishes #91645318] Check for unfilled inputs in MSM studio
    
    commit 6d61b142fd748344f77afe49be5144613821ac66
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Apr 2 19:05:37 2015 +0000
    
        Automatically built.
    
        commit 616e7d9a2fd0dad1ec3e1ccd2489fab7b79c43d3
        Merge: 465d681 36ce46a
        Author: Chris Pirich <chris@code.org>
        Date:   Thu Apr 2 11:55:42 2015 -0700
    
            Merge pull request #1893 from code-dot-org/droplet_disappearing_fix
    
            Update to latest https://github.com/dabbler0/droplet/pull/43
    
        commit 465d68196ba912187919eea4985a86afc2e9c6f4
        Author: Continuous Integration <dev@code.org>
        Date:   Thu Apr 2 18:53:40 2015 +0000
    
            Automatically built.
    
            commit 7b6e84bbe75cce3507c38450293db5cdddfd4d07
            Merge: 33c864f 8053bb9
            Author: Josh Lory <josh.lory@code.org>
            Date:   Thu Apr 2 11:35:04 2015 -0700
    
                Merge pull request #1851 from code-dot-org/dedupe-modal-css
    
                Deduplicate modal dialog CSS between Apps and Dashboard
    
            commit 33c864faa02a8a0cf3e45e808b0d2e2ec26ad0d7
            Merge: e317d70 33f25a1
            Author: Bjvanminnen <Bjvanminnen@gmail.com>
            Date:   Thu Apr 2 11:33:44 2015 -0700
    
                Merge pull request #1891 from code-dot-org/playlabInputOutput
    
                [finishes #91644306] input/output table for playlab
    
            commit 33f25a12315e3f1a219c1c09c63ff5d2bddb775b
            Author: Brent Van Minnen <bjvanminnen@gmail.com>
            Date:   Thu Apr 2 11:28:00 2015 -0700
    
                input/output table for playlab
    
    commit cce87dfb6d1c91e2ddc99d4bd5811cb522d302e9
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Apr 2 11:53:29 2015 -0700
    
        only check for unfilled functional blocks
    
    commit 616e7d9a2fd0dad1ec3e1ccd2489fab7b79c43d3
    Merge: 465d681 36ce46a
    Author: Chris Pirich <chris@code.org>
    Date:   Thu Apr 2 11:55:42 2015 -0700
    
        Merge pull request #1893 from code-dot-org/droplet_disappearing_fix
    
        Update to latest https://github.com/dabbler0/droplet/pull/43

--- dashboard/public/apps-package/js/blockly.js
+++ dashboard/public/apps-package/js/blockly.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7460dd0e837aa9d4758c58c3783594eb5c6b529900fddf0746663b507a04c961
-size 748571
+oid sha256:1d510bba2aee43aa16936add7977dd1ac5163132249bf17c807dd21eeb6bf378
+size 748616
