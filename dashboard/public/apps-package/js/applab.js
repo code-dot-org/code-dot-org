f6a0d4e5e9db5088d036655e670871b181907c63
Continuous Integration <dev@code.org>
2015-04-07 18:27:11 +0000

    Automatically built.
    commit eab977bf5e2a527d976b7058fff12ec5c1e93af3
    Merge: 7c4fe7c 048a6b0
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Apr 7 11:14:10 2015 -0700
    
        Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
    commit 7c4fe7cc90ba73ace23c11e76631327dce3a2645
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Apr 7 11:13:56 2015 -0700
    
        add 'record' param to updateRecord() callback

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f2380d6ff09ac817ccc41bf144bca0ed39348919fad2ef045c1db2e50c11f16
-size 154646
+oid sha256:258191bb6ef1e288da7c0529bc3e80ef2bd7b2b7c2939302d71334c9fceac6b2
+size 154672
