36e8b7868f0b666d47fc18781ef8c18d7f96d01e
Continuous Integration <dev@code.org>
2015-04-07 18:45:28 +0000

    Automatically built.
    commit 2cfd259598757a1646419c99ebe1a6cc124ed188
    Merge: 5ddd738 6b4badf
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Apr 7 11:36:21 2015 -0700
    
        Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
    commit 5ddd73881089be9c7bae6e439ace7667190d1453
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Apr 7 11:36:16 2015 -0700
    
        fix error msg when deleteRecord id is not found

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:258191bb6ef1e288da7c0529bc3e80ef2bd7b2b7c2939302d71334c9fceac6b2
-size 154672
+oid sha256:55edd073387c22f436fa477c58939bcf372d3fd7a178011ec934014366410e33
+size 154665
