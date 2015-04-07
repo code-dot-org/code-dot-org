b300dc55cb6856cef004dc3930275f4a4c7c4c8b
Continuous Integration <dev@code.org>
2015-04-07 23:55:48 +0000

    Automatically built.
    commit 0bef8a61e8f102c0e076f54cb46ee664b3e4fb81
    Merge: 3422ad7 8aa0aec
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Apr 7 16:45:59 2015 -0700
    
        Merge pull request #1970 from code-dot-org/safariColors
    
        [Finishes #91644992] safari doesnt like innerHTML on xml
    
    commit 3422ad74d06fe797ecd47703e05bca68dfe435bb
    Merge: ff30095 c97d0e1
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Tue Apr 7 16:45:50 2015 -0700
    
        Merge pull request #1968 from code-dot-org/netsim_remove_template_levels
    
        Rename default template from 'default' => 'custom' to match other apps, ...
    
    commit 8aa0aececff692af00b146ffddbc81d62bc8cef2
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Apr 7 16:39:10 2015 -0700
    
        safari doesnt like innerHTML on xml
    
    commit c97d0e1ca2ea050631c951e370faaad1f783b13a
    Author: Brad Buchanan <brad@code.org>
    Date:   Tue Apr 7 16:33:48 2015 -0700
    
        Missed a few important details
    
    commit 9cbf37d61e6f270561739a47f46807afe9ce3bef
    Author: Brad Buchanan <brad@code.org>
    Date:   Tue Apr 7 16:12:18 2015 -0700
    
        Rename default template from 'default' => 'custom' to match other apps, remove additional templates, fill out level files with values for all parameters.

--- dashboard/public/apps-package/js/blockly.js
+++ dashboard/public/apps-package/js/blockly.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:696505e21c785e6fecd8ee1a86cbe37deac2a20bbd47ccd257d4ab275dbbaa97
-size 749645
+oid sha256:e6bf16e837f8844814674c2b06327d86b2a2faeb4145c5ab42b0f2ff65065da4
+size 749653
