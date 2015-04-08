20f065e9013282c50c793dc221b861bba6e200d0
Continuous Integration <dev@code.org>
2015-04-08 00:23:10 +0000

    Automatically built.
    commit 9c5cbe767f0773b7548cdea2455cdf79ae1f6b70
    Merge: 3658a1b 194c3a5
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Apr 7 17:03:56 2015 -0700
    
        Merge pull request #1939 from code-dot-org/dontRenderInvis
    
        [Finishes #91560928] dont render blocks that arent visible
    
    commit 194c3a5b4cdd463d380360a557918497ed4f98fb
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Apr 7 14:19:39 2015 -0700
    
        move blockspace check into isVisible
    
    commit 48275d3d0e349b975440f3266baa51e4879f0859
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Apr 6 12:59:44 2015 -0700
    
        dont render blocks that arent visible

--- dashboard/public/apps-package/js/blockly.js
+++ dashboard/public/apps-package/js/blockly.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6bf16e837f8844814674c2b06327d86b2a2faeb4145c5ab42b0f2ff65065da4
-size 749653
+oid sha256:9e5e829c11c3c075aafcea39f41d687dc52959ee3c2cf0bd3b4979c214ba2ae6
+size 749787
