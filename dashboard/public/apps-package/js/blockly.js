7378727f842d6811308ffdb6061307874e16973c
Continuous Integration <dev@code.org>
2015-04-08 19:06:42 +0000

    Automatically built.
    commit 16b615bab81d3569eaa4f6be4cf17840e687722e
    Merge: b84e718 8ffd483
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Apr 8 11:50:31 2015 -0700
    
        Merge pull request #1976 from code-dot-org/nonEditableDefine
    
        [Finishes #91559784] Define block should not be editable in function editor
    
    commit b84e718c8cb01a1c8f4c7bf05f3ec6a789808a3b
    Merge: 91af8e5 f7a7b77
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Apr 8 11:50:09 2015 -0700
    
        Merge pull request #1975 from code-dot-org/contractErrors
    
        [Finishes #91544716, 91796444] trim whitespace and give error for case sensitivity
    
    commit 91af8e5b7b24a18fa16c74c34976ae5fdb650f65
    Merge: d75116f 2459064
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Wed Apr 8 11:46:39 2015 -0700
    
        Merge pull request #1977 from code-dot-org/jquery_ui_upgrade
    
        Update jQuery UI and React versions
    
    commit d75116f3e095c6965c11d55a0c3e7d5ef1939e06
    Merge: a9fcf44 753a33f
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Wed Apr 8 11:46:26 2015 -0700
    
        Merge branch 'test' into staging
    
    commit 753a33fa0d2d57f98bc96201b9c83b8f3660da9e
    Merge: a4ec19c 1f6bac0
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Wed Apr 8 11:46:15 2015 -0700
    
        Merge branch 'production' into test
    
    commit 1f6bac0c154e256b118b8893d6a363625ad33d83
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Apr 8 18:44:24 2015 +0000
    
        hotfix /action/UT
    
    commit a9fcf44a0dd5ec17a85067972553770f44965251
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Apr 8 18:44:24 2015 +0000
    
        hotfix /action/UT
    
    commit 9d91da67078f3d1d7a8e2d4caa97cd6db01bfb77
    Merge: b3a1d16 f6319d1
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Apr 8 11:44:00 2015 -0700
    
        Merge pull request #1973 from code-dot-org/immovableConnections
    
        [Finishes #91559606] dont allow connection that would bump out immovable block
    
    commit b3a1d1638bf358c37b22d47a9b6a01f165cd8ea2
    Author: Geoffrey Elliott <geoffrey@code.org>
    Date:   Wed Apr 8 11:37:49 2015 -0700
    
        Move poste stuff from /private to /poste and allow staging users to access /poste unauthenticated.
    
    commit 2459064aff7a02527cd18bafb90d548a9bda25fd
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Wed Apr 8 11:21:37 2015 -0700
    
        Update version of react-rails to 1.0 to get React 0.13 to get React.findDOMNode to use jQuery selectmenu for #89659872

--- dashboard/public/apps-package/js/blockly.js
+++ dashboard/public/apps-package/js/blockly.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e5e829c11c3c075aafcea39f41d687dc52959ee3c2cf0bd3b4979c214ba2ae6
-size 749787
+oid sha256:db2880bdb2a8f03d5f16bbb4647ffd22f23def13aa1a0614b62ed4b61faefcab
+size 749882
