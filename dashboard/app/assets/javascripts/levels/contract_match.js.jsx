5d1d31d704e3e1e064e165d79a5806898c2b2363
Brian Jordan <bcjordan@gmail.com>
2015-04-08 17:58:00 -0700

    - Use jQuery UI selectmenu for contract assessment dropdowns - Default to 0 domain items in assessment (to match full editor) - Move "Add" buttons to newline in assessment (to match full editor)

--- dashboard/app/assets/javascripts/levels/contract_match.js.jsx
+++ dashboard/app/assets/javascripts/levels/contract_match.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43ff3f92b2dc4e85ade08fd449433f0be435b96f52cb7eb441ef3e692a9067a8
-size 8434
+oid sha256:ded49267b387b4a7de13aa7f5046614caaa52059825130e2a4f2ae53ba7e8512
+size 8334
