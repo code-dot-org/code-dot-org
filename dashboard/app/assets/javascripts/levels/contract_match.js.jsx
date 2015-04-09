77e14c62ded81cf99176a5a322a1189eabd27a69
Brian Jordan <bcjordan@gmail.com>
2015-04-09 11:33:00 -0700

    Rename function for clarity

--- dashboard/app/assets/javascripts/levels/contract_match.js.jsx
+++ dashboard/app/assets/javascripts/levels/contract_match.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:029eb0d9c914281fe5a2850190a7d784d178cf612983f91a71e890630894de30
-size 10031
+oid sha256:d2eb88efd7ed25fb0ab170722b8010a7f4ccf83dad0981e590bd95935b274444
+size 10061
