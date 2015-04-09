08159277e6f979e01f4f68b1eab1c48b290df0f3
cpirich <chris@code.org>
2015-04-08 21:29:52 -0700

    applab: validate dom id's

--- apps/src/applab/applab.js
+++ apps/src/applab/applab.js
@@ -353,6 +353,27 @@ function apiValidateActiveCanvas(opts, funcName) {
   }
 }
 
+function apiValidateDomIdExistence(divApplab, opts, funcName, varName, id, shouldExist) {
+  var validatedTypeKey = 'validated_type_' + varName;
+  var validatedDomKey = 'validated_id_' + varName;
+  apiValidateType(opts, funcName, varName, id, 'string');
+  if (opts[validatedTypeKey] && typeof opts[validatedDomKey] === 'undefined') {
+    var element = document.getElementById(id);
+    var exists = Boolean(element && divApplab.contains(element));
+    var valid = exists == shouldExist;
+    if (!valid) {
+      var line = codegen.getNearestUserCodeLine(Applab.interpreter,
+                                                Applab.cumulativeLength,
+                                                Applab.userCodeStartOffset,
+                                                Applab.userCodeLength);
+      apiWarn("WARNING: Line " + (line + 1) + ": " + funcName + "() " + varName +
+              " parameter refers to an id (" + id + ") which " +
+              (exists ? "already exists." : "does not exist."));
+    }
+    opts[validatedDomKey] = valid;
+  }
+}
+
 function onDebugInputKeyDown(e) {
   if (e.keyCode == KeyCodes.ENTER) {
     var input = e.target.textContent;
@@ -1673,11 +1694,11 @@ Applab.write = function (opts) {
 };
 
 Applab.button = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'button', 'id', opts.elementId, 'string');
-  apiValidateType(opts, 'button', 'text', opts.text, 'string');
-
   var divApplab = document.getElementById('divApplab');
+
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'button', 'id', opts.elementId, false);
+  apiValidateType(opts, 'button', 'text', opts.text, 'string');
 
   var newButton = document.createElement("button");
   var textNode = document.createTextNode(opts.text);
@@ -1993,8 +2014,8 @@ Applab.speed = function (opts) {
 };
 
 Applab.createCanvas = function (opts) {
-  apiValidateType(opts, 'createCanvas', 'canvasId', opts.elementId, 'string');
   var divApplab = document.getElementById('divApplab');
+  apiValidateDomIdExistence(divApplab, opts, 'createCanvas', 'canvasId', opts.elementId, false);
 
   var newElement = document.createElement("canvas");
   var ctx = newElement.getContext("2d");
@@ -2027,9 +2048,9 @@ Applab.createCanvas = function (opts) {
 };
 
 Applab.setActiveCanvas = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'setActiveCanvas', 'canvasId', opts.elementId, 'string');
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'setActiveCanvas', 'canvasId', opts.elementId, true);
   var canvas = document.getElementById(opts.elementId);
   if (divApplab.contains(canvas)) {
     Applab.activeCanvas = canvas;
@@ -2136,12 +2157,12 @@ Applab.clearCanvas = function (opts) {
 };
 
 Applab.drawImage = function (opts) {
+  var divApplab = document.getElementById('divApplab');
   // TODO: cpirich: may need to update param name
   apiValidateActiveCanvas(opts, 'drawImage');
-  apiValidateType(opts, 'drawImage', 'imageId', opts.imageId, 'string');
+  apiValidateDomIdExistence(divApplab, opts, 'drawImage', 'imageId', opts.imageId, true);
   apiValidateType(opts, 'drawImage', 'x', opts.x, 'number');
   apiValidateType(opts, 'drawImage', 'y', opts.y, 'number');
-  var divApplab = document.getElementById('divApplab');
   var image = document.getElementById(opts.imageId);
   var ctx = Applab.activeCanvas && Applab.activeCanvas.getContext("2d");
   if (ctx && divApplab.contains(image)) {
@@ -2194,11 +2215,10 @@ Applab.putImageData = function (opts) {
 };
 
 Applab.textInput = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'textInput', 'id', opts.elementId, 'string');
-  apiValidateType(opts, 'textInput', 'text', opts.text, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'textInput', 'id', opts.elementId, false);
+  apiValidateType(opts, 'textInput', 'text', opts.text, 'string');
 
   var newInput = document.createElement("input");
   newInput.value = opts.text;
@@ -2208,12 +2228,11 @@ Applab.textInput = function (opts) {
 };
 
 Applab.textLabel = function (opts) {
+  var divApplab = document.getElementById('divApplab');
   // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'textLabel', 'id', opts.elementId, 'string');
+  apiValidateDomIdExistence(divApplab, opts, 'textLabel', 'id', opts.elementId, false);
   apiValidateType(opts, 'textLabel', 'text', opts.text, 'string');
   apiValidateType(opts, 'textLabel', 'forId', opts.forId, 'string', OPTIONAL);
-
-  var divApplab = document.getElementById('divApplab');
 
   var newLabel = document.createElement("label");
   var textNode = document.createTextNode(opts.text);
@@ -2228,11 +2247,10 @@ Applab.textLabel = function (opts) {
 };
 
 Applab.checkbox = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'checkbox', 'id', opts.elementId, 'string');
-  // apiValidateType(opts, 'checkbox', 'checked', opts.checked, 'boolean');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'checkbox', 'id', opts.elementId, false);
+  // apiValidateType(opts, 'checkbox', 'checked', opts.checked, 'boolean');
 
   var newCheckbox = document.createElement("input");
   newCheckbox.setAttribute("type", "checkbox");
@@ -2243,12 +2261,11 @@ Applab.checkbox = function (opts) {
 };
 
 Applab.radioButton = function (opts) {
+  var divApplab = document.getElementById('divApplab');
   // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'radioButton', 'id', opts.elementId, 'string');
+  apiValidateDomIdExistence(divApplab, opts, 'radioButton', 'id', opts.elementId, false);
   // apiValidateType(opts, 'radioButton', 'checked', opts.checked, 'boolean');
   apiValidateType(opts, 'radioButton', 'group', opts.name, 'string', OPTIONAL);
-
-  var divApplab = document.getElementById('divApplab');
 
   var newRadio = document.createElement("input");
   newRadio.setAttribute("type", "radio");
@@ -2260,10 +2277,9 @@ Applab.radioButton = function (opts) {
 };
 
 Applab.dropdown = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'dropdown', 'id', opts.elementId, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'dropdown', 'id', opts.elementId, false);
 
   var newSelect = document.createElement("select");
 
@@ -2304,10 +2320,10 @@ Applab.setAttribute = function (opts) {
 };
 
 Applab.getText = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'getText', 'id', opts.elementId, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'getText', 'id', opts.elementId, true);
+
   var element = document.getElementById(opts.elementId);
   if (divApplab.contains(element)) {
     if (element.tagName === 'INPUT' || element.tagName === 'SELECT') {
@@ -2322,11 +2338,11 @@ Applab.getText = function (opts) {
 };
 
 Applab.setText = function (opts) {
+  var divApplab = document.getElementById('divApplab');
   // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'setText', 'id', opts.elementId, 'string');
+  apiValidateDomIdExistence(divApplab, opts, 'setText', 'id', opts.elementId, true);
   apiValidateType(opts, 'setText', 'text', opts.text, 'string');
 
-  var divApplab = document.getElementById('divApplab');
   var element = document.getElementById(opts.elementId);
   if (divApplab.contains(element)) {
     if (element.tagName === 'INPUT' || element.tagName === 'SELECT') {
@@ -2342,10 +2358,10 @@ Applab.setText = function (opts) {
 };
 
 Applab.getChecked = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'getChecked', 'id', opts.elementId, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'getChecked', 'id', opts.elementId, true);
+
   var element = document.getElementById(opts.elementId);
   if (divApplab.contains(element) && element.tagName === 'INPUT') {
     return element.checked;
@@ -2354,11 +2370,11 @@ Applab.getChecked = function (opts) {
 };
 
 Applab.setChecked = function (opts) {
+  var divApplab = document.getElementById('divApplab');
   // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'setChecked', 'id', opts.elementId, 'string');
+  apiValidateDomIdExistence(divApplab, opts, 'setChecked', 'id', opts.elementId, true);
   // apiValidateType(opts, 'setChecked', 'checked', opts.checked, 'boolean');
 
-  var divApplab = document.getElementById('divApplab');
   var element = document.getElementById(opts.elementId);
   if (divApplab.contains(element) && element.tagName === 'INPUT') {
     element.checked = opts.checked;
@@ -2368,10 +2384,10 @@ Applab.setChecked = function (opts) {
 };
 
 Applab.getImageURL = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'getImageURL', 'id', opts.elementId, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'getImageURL', 'id', opts.elementId, true);
+
   var element = document.getElementById(opts.elementId);
   if (divApplab.contains(element)) {
     // We return a URL if it is an IMG element or our special img-upload label
@@ -2387,11 +2403,11 @@ Applab.getImageURL = function (opts) {
 };
 
 Applab.setImageURL = function (opts) {
+  var divApplab = document.getElementById('divApplab');
   // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'setImageURL', 'id', opts.elementId, 'string');
+  apiValidateDomIdExistence(divApplab, opts, 'setImageURL', 'id', opts.elementId, true);
   apiValidateType(opts, 'setImageURL', 'src', opts.src, 'string');
 
-  var divApplab = document.getElementById('divApplab');
   var element = document.getElementById(opts.elementId);
   if (divApplab.contains(element) && element.tagName === 'IMG') {
     element.src = opts.src;
@@ -2424,10 +2440,10 @@ Applab.innerHTML = function (opts) {
 };
 
 Applab.deleteElement = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'deleteElement', 'id', opts.elementId, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'deleteElement', 'id', opts.elementId, true);
+
   var div = document.getElementById(opts.elementId);
   if (divApplab.contains(div)) {
     // Special check to see if the active canvas is being deleted
@@ -2440,10 +2456,10 @@ Applab.deleteElement = function (opts) {
 };
 
 Applab.showElement = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'showElement', 'id', opts.elementId, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'showElement', 'id', opts.elementId, true);
+
   var div = document.getElementById(opts.elementId);
   if (divApplab.contains(div)) {
     div.style.visibility = 'visible';
@@ -2453,10 +2469,10 @@ Applab.showElement = function (opts) {
 };
 
 Applab.hideElement = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'hideElement', 'id', opts.elementId, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'hideElement', 'id', opts.elementId, true);
+
   var div = document.getElementById(opts.elementId);
   if (divApplab.contains(div)) {
     div.style.visibility = 'hidden';
@@ -2487,14 +2503,14 @@ Applab.setParent = function (opts) {
 };
 
 Applab.setPosition = function (opts) {
+  var divApplab = document.getElementById('divApplab');
   // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'setPosition', 'id', opts.elementId, 'string');
+  apiValidateDomIdExistence(divApplab, opts, 'setPosition', 'id', opts.elementId, true);
   apiValidateType(opts, 'setPosition', 'left', opts.left, 'number');
   apiValidateType(opts, 'setPosition', 'top', opts.top, 'number');
   apiValidateType(opts, 'setPosition', 'width', opts.width, 'number');
   apiValidateType(opts, 'setPosition', 'height', opts.height, 'number');
 
-  var divApplab = document.getElementById('divApplab');
   var div = document.getElementById(opts.elementId);
   if (divApplab.contains(div)) {
     div.style.position = 'absolute';
@@ -2508,10 +2524,10 @@ Applab.setPosition = function (opts) {
 };
 
 Applab.getXPosition = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'getXPosition', 'id', opts.elementId, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'getXPosition', 'id', opts.elementId, true);
+
   var div = document.getElementById(opts.elementId);
   if (divApplab.contains(div)) {
     var x = div.offsetLeft;
@@ -2525,10 +2541,10 @@ Applab.getXPosition = function (opts) {
 };
 
 Applab.getYPosition = function (opts) {
-  // TODO: cpirich: may need to update param name
-  apiValidateType(opts, 'getYPosition', 'id', opts.elementId, 'string');
-
   var divApplab = document.getElementById('divApplab');
+  // TODO: cpirich: may need to update param name
+  apiValidateDomIdExistence(divApplab, opts, 'getYPosition', 'id', opts.elementId, true);
+
   var div = document.getElementById(opts.elementId);
   if (divApplab.contains(div)) {
     var y = div.offsetTop;
@@ -2567,10 +2583,10 @@ Applab.onEventFired = function (opts, e) {
 };
 
 Applab.onEvent = function (opts) {
-  apiValidateType(opts, 'onEvent', 'id', opts.elementId, 'string');
+  var divApplab = document.getElementById('divApplab');
+  apiValidateDomIdExistence(divApplab, opts, 'onEvent', 'id', opts.elementId, true);
   apiValidateType(opts, 'onEvent', 'event', opts.eventName, 'string');
   apiValidateType(opts, 'onEvent', 'function', opts.func, 'function');
-  var divApplab = document.getElementById('divApplab');
   // Special case the id of 'body' to mean the app's container (divApplab)
   // TODO (cpirich): apply this logic more broadly (setStyle, etc.)
   if (opts.elementId === 'body') {
