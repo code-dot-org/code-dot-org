70a9dd397bd02acef68ba3328065c5f7b5b13eb5
Continuous Integration <dev@code.org>
2015-04-09 04:39:25 +0000

    Automatically built.
    commit 08159277e6f979e01f4f68b1eab1c48b290df0f3
    Author: cpirich <chris@code.org>
    Date:   Wed Apr 8 21:29:52 2015 -0700
    
        applab: validate dom id's

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:926f9d849f5aaa67f1e5d7e90e6c17bbb276dca9974bbaeb297ea0a801713602
-size 174965
+oid sha256:b8d8dcb369aef1fb8d7b7efdef2ae0e8ca7bda9569df30a94cf25ef60a20f829
+size 176393
