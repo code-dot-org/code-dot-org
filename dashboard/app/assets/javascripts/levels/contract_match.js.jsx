06c41992558d1cfc43343814dfde474c36a42b24
Brian Jordan <bcjordan@gmail.com>
2015-04-09 11:35:46 -0700

    From code review feedback, move $ up to variable declaration and rename

--- dashboard/app/assets/javascripts/levels/contract_match.js.jsx
+++ dashboard/app/assets/javascripts/levels/contract_match.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2eb88efd7ed25fb0ab170722b8010a7f4ccf83dad0981e590bd95935b274444
-size 10061
+oid sha256:79d486bb06fb477c12265938382864c0da9e0cdfbcce367eeffae0e4abcecf8d
+size 10055
