cf82cb48b415a8ba0fa823f7a9dc9182d98d69b8
Brendan Reville <brendan@code.org>
2015-04-14 11:30:14 -0700

    Initial work to move pegasus metrics up into lib/cdo. Just works for leaderboards page so far.. other pages will be broken.

--- pegasus/src/database.rb
+++ pegasus/src/database.rb
@@ -1,34 +1,6 @@
 require 'cdo/db'
 DB = PEGASUS_DB
 
-class Properties
-
-  def initialize()
-    @table = DB[:properties]
-  end
-
-  def get(key)
-    i = @table.where(key:key.to_s).first
-    return nil unless i
-    JSON.load(StringIO.new(i[:value]))
-  end
-
-  def set(key, value)
-    key = key.to_s
-
-    i = @table.where(key:key).first
-    if i.nil?
-      @table.insert(key:key, value:value.to_json)
-    else
-      @table.where(key:key).update(value:value.to_json)
-    end
-
-    value
-  end
-
-end
-PROPERTIES = Properties.new
-
 class Tutorials
 
   def initialize(table)
@@ -106,33 +78,6 @@ def zip_code_from_code(code)
 end
 def zip_code?(code)
   !zip_code_from_code(code).nil?
-end
-
-def fetch_metrics()
-  metrics = PROPERTIES.get(:metrics)||{
-    'created_at'=>"2013-12-31T23:59:59+00:00",
-    'created_on'=>"2013-12-31",
-    'csedweek_organizers'=>0,
-    'csedweek_teachers'=>0,
-    'csedweek_entire_schools'=>0,
-    'csedweek_students'=>0,
-    'csedweek_countries'=>0,
-    'petition_signatures'=>0,
-    'lines_of_code'=>0,
-  }
-  metrics
-end
-
-def fetch_hoc_metrics()
-  metrics = PROPERTIES.get(:hoc_metrics)||{
-    'started'=>0,
-    'finished'=>0,
-    'tutorials'=>{'codeorg'=>0},
-    'cities'=>{'Seattle'=>0},
-    'countries'=>{'United States'=>0},
-  }
-  metrics['started'] += 409216
-  metrics
 end
 
 require 'cdo/geocoder'
