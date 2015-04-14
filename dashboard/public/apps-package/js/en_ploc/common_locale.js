48adbab2f8e198e8ee488b09edd4bc72bb998511
Continuous Integration <dev@code.org>
2015-04-14 22:20:10 +0000

    Automatically built.
    commit 9d57f7a05af92b4ff7372377fd3d3d1c1978f5df
    Merge: 79f8d19 3c9049e
    Author: Sarah Filman <sarah.filman@code.org>
    Date:   Tue Apr 14 15:10:39 2015 -0700
    
        Merge pull request #2056 from code-dot-org/batch3_
    
        Batch 3 of updated droplet tooltips
    
    commit 3c9049e696c79d2e2253edf26c566352efa33f41
    Author: Sarah Filman <sarah.filman@code.org>
    Date:   Tue Apr 14 15:04:50 2015 -0700
    
        Fixed tooltip typo
    
    commit 532af85afd1c84931bf072c53b84c8f541a6104e
    Author: Sarah Filman <sarah.filman@code.org>
    Date:   Tue Apr 14 13:57:47 2015 -0700
    
        Batch 3 of updated droplet tooltips

--- dashboard/public/apps-package/js/en_ploc/common_locale.js
+++ dashboard/public/apps-package/js/en_ploc/common_locale.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4386bbbe123788f87dc6269a6e692803380524a136e0a94ac700d882c65b2744
-size 37326
+oid sha256:4b8ec6ee3624f4dbb001a2331d19360824f6ca8bfe60cbcfb12735f152ab0283
+size 43808
--- dashboard/public/apps-package/js/en_us/common_locale.js
+++ dashboard/public/apps-package/js/en_us/common_locale.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18590153fa8fa1f7b91aed8297168464fff597f05a9f1f00832474a0f375ddf4
-size 35451
+oid sha256:dcf0cc5f760b3e7c798acc130cd56b10df0504cdf701c1db1a5ba80ea190cade
+size 41555
