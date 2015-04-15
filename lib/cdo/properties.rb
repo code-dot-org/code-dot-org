b4abaad1152a1f362c7a6a299b44e7e45dd4c6a9
Brendan Reville <brendan@code.org>
2015-04-15 11:40:20 -0700

    Add lines of code count to studio.code.org.

--- lib/cdo/properties.rb
+++ lib/cdo/properties.rb
@@ -27,32 +27,32 @@ class Properties
     value
   end
 
-  def fetch_metrics()
-    metrics = PROPERTIES.get(:metrics)||{
-      'created_at'=>"2013-12-31T23:59:59+00:00",
-      'created_on'=>"2013-12-31",
-      'csedweek_organizers'=>0,
-      'csedweek_teachers'=>0,
-      'csedweek_entire_schools'=>0,
-      'csedweek_students'=>0,
-      'csedweek_countries'=>0,
-      'petition_signatures'=>0,
-      'lines_of_code'=>0,
-    }
-    metrics
-  end
-
-  def fetch_hoc_metrics()
-    metrics = PROPERTIES.get(:hoc_metrics)||{
-      'started'=>0,
-      'finished'=>0,
-      'tutorials'=>{'codeorg'=>0},
-      'cities'=>{'Seattle'=>0},
-      'countries'=>{'United States'=>0},
-    }
-    metrics['started'] += 409216
-    metrics
-  end
-
 end
 PROPERTIES = Properties.new
+
+def fetch_metrics()
+  metrics = PROPERTIES.get(:metrics)||{
+    'created_at'=>"2013-12-31T23:59:59+00:00",
+    'created_on'=>"2013-12-31",
+    'csedweek_organizers'=>0,
+    'csedweek_teachers'=>0,
+    'csedweek_entire_schools'=>0,
+    'csedweek_students'=>0,
+    'csedweek_countries'=>0,
+    'petition_signatures'=>0,
+    'lines_of_code'=>0,
+  }
+  metrics
+end
+
+def fetch_hoc_metrics()
+  metrics = PROPERTIES.get(:hoc_metrics)||{
+    'started'=>0,
+    'finished'=>0,
+    'tutorials'=>{'codeorg'=>0},
+    'cities'=>{'Seattle'=>0},
+    'countries'=>{'United States'=>0},
+  }
+  metrics['started'] += 409216
+  metrics
+end
