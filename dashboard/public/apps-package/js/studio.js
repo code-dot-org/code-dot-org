190aa128f1e21167698cf34a1f14c5326ffb3d38
Continuous Integration <dev@code.org>
2015-04-15 17:54:04 +0000

    Automatically built.
    commit 22c009ee62b85f064acc49d8b58c978f1d3f8f7f
    Merge: b03ae6f 93dcd46
    Author: Josh Caldwell <jpcaldwell@gmail.com>
    Date:   Wed Apr 15 10:44:41 2015 -0700
    
        Merge pull request #2068 from code-dot-org/biggame_collision_reset
    
        Fix for reset after collision
    
    commit b03ae6fdc33b4cf92ef6c596bf90213fbb7eb91e
    Merge: 980419a cca8053
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Apr 15 10:35:16 2015 -0700
    
        Merge pull request #2069 from code-dot-org/dsls
    
        commit dsls.en.yml
    
    commit cca8053251ae8cd13f7fb80d265aa0f9af21f22f
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Wed Apr 15 10:25:22 2015 -0700
    
        commit dsls.en.yml

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d94e1f7dd673e73a738330976c1d72fe7d3eab4562ba138697aa2fdfa30bfb4
-size 272596
+oid sha256:312930acea2e16bc3726e2f981103c7415f00e371a6e4208f88674973ee99913
+size 272712
