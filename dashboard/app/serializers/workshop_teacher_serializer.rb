31a89a912749c844e20478b929afa723fd0e7eeb
CaptainStack <CaptainStack@outlook.com>
2015-04-16 11:05:29 -0700

    Removing unused code.

--- dashboard/app/serializers/workshop_teacher_serializer.rb
+++
@@ -1,7 +0,0 @@
-class WorkshopTeacherSerializer < ActiveModel::Serializer
-  attributes :id, :email, :ops_first_name, :ops_last_name, :district, :ops_school, :ops_gender, :attendances
-
-  def attendances
-    # object.attendances.map{|attendance| WorkshopTeacher.new(attendance).attributes}
-  end
-end
