174342646b6a46d5db4bf2b53a7c96eb33fce254
Continuous Integration <dev@code.org>
2015-04-16 19:05:44 +0000

    Automatically built.
    commit 8838b87d34df9b1bbeeb263b56f83efcf198df35
    Merge: 51e6469 2407ecf
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Apr 16 11:54:10 2015 -0700
    
        Merge pull request #2089 from code-dot-org/grayStudioBlocks
    
        gray out undeletable blocks in algebra playlab
    
    commit 2407ecf3081c994f4b82e9334ccd9198f5044a7e
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Apr 16 11:49:05 2015 -0700
    
        gray out undeletable blocks in algebra playlab

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abfd815cc9f877663e30ae695d4195ec110911ef418c2af16d0bbb2ecffea877
-size 272084
+oid sha256:5fa0c7518567b3d2f096fe7510af6d15778c204285afe2558d3270efa8dcfb6c
+size 272324
