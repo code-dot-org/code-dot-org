9b6bed05a676d0e1962d0dd81fa6d6fbe8c7f2d1
Josh Lory <josh.lory@code.org>
2015-04-16 18:19:56 -0700

    Don't make level builders set flip-x-close, calculate it for them
    Adds the 'flip-x-close' class to callouts where the button would otherwise overlap the qtip.

--- dashboard/app/assets/javascripts/callouts.js
+++ dashboard/app/assets/javascripts/callouts.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a81508e94ab0f32aafc14f0fea45ca19fd8c6208fd8f5cc490c2a88e8de81b2c
-size 2062
+oid sha256:2aa5624dbdfc0221cb403f1a3e7eacb16dcbf521d58231132c835aa9d16bd47f
+size 2258
