213d7ca11131a773009a8e3cf8892b7a68b4688b
Continuous Integration <dev@code.org>
2015-04-17 01:59:56 +0000

    Automatically built.
    commit 77af3fffd9548751bad307fb17e7f664d931e021
    Merge: 626b303 1f0dc5d
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Apr 16 18:50:24 2015 -0700
    
        Merge pull request #2111 from code-dot-org/override_tooltip_delay
    
        Set default tooltip config to have a delay
    
    commit 1f0dc5dda394910b9208b26302a7a06fa2731aea
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Apr 16 18:48:47 2015 -0700
    
        Set default tooltip config to have a delay

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b107bcfa8ee58758b506549ef1ac3a294e63760443e3ed1c37ce3f72e9f6a6f
-size 594634
+oid sha256:581b38c36efdfc6f7ccb64199cab10b83df698389895b133539b45bfbc3c35f8
+size 594648
