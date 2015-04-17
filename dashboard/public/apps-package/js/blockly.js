87ea19d2ddd5b877c2c74e098eda2e75e16c226e
Continuous Integration <dev@code.org>
2015-04-17 16:19:35 +0000

    Automatically built.
    commit 42bc9755b9a581c2aa46944f1e37576daefdd109
    Merge: d134c01 b739942
    Author: Josh Lory <josh.lory@code.org>
    Date:   Fri Apr 17 09:10:12 2015 -0700
    
        Merge pull request #2105 from code-dot-org/autosave-interval
    
        Autosave projects every 30 seconds if the code has changed
    
    commit b739942948f1539e6068d7b63e1338c99a9c34e9
    Author: Josh Lory <josh.lory@code.org>
    Date:   Fri Apr 17 09:09:50 2015 -0700
    
        Code review feedback [ci skip]
    
    commit d134c01d76c19302d6c6a1ed1e7ead6adda50900
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Apr 17 01:52:04 2015 +0000
    
        Revert "Merge pull request #1530 from code-dot-org/blockly_level_options"
    
        This reverts commit 1da1b54406522e5060c1a7d0bd4f9d36e1f36fde, reversing
        changes made to a5e7faa09aedfea3681751bb893ba5a743e01f46.
    
        Conflicts:
        	dashboard/app/models/blockly.rb
    
    commit 2124fa7f31e9134ab97e390e2388bfccda465540
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Apr 17 01:50:14 2015 +0000
    
        Revert "Merge pull request #2053 from code-dot-org/blockly_level_options_fix"
    
        This reverts commit 15ce5b6e420e310e47a3ec8e8401da9df857dd05, reversing
        changes made to 03b6e80afa97eefe8ee6e7f33062f3d103b692f4.
    
    commit 8536b17cd7ae08f98380f19ce9f44c428280af34
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Apr 17 01:49:59 2015 +0000
    
        Revert "Merge pull request #2074 from code-dot-org/fix_embed"
    
        This reverts commit 21e15892f3103a964b8257fc08f7d4cc5a69a32a, reversing
        changes made to 556890894de752744c86e4a440c74d7aac8ee90f.
    
        Conflicts:
        	dashboard/test/helpers/levels_helper_test.rb
    
    commit 8d41bd719cf976aa068a832988f28aef611816bc
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Apr 17 01:59:56 2015 +0000
    
        Automatically built.
    
        commit 77af3fffd9548751bad307fb17e7f664d931e021
        Merge: 626b303 1f0dc5d
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Thu Apr 16 18:50:24 2015 -0700
    
            Merge pull request #2111 from code-dot-org/override_tooltip_delay
    
            Set default tooltip config to have a delay
    
        commit 1f0dc5dda394910b9208b26302a7a06fa2731aea
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Thu Apr 16 18:48:47 2015 -0700
    
            Set default tooltip config to have a delay
    
    commit 77af3fffd9548751bad307fb17e7f664d931e021
    Merge: 626b303 1f0dc5d
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Apr 16 18:50:24 2015 -0700
    
        Merge pull request #2111 from code-dot-org/override_tooltip_delay
    
        Set default tooltip config to have a delay

--- dashboard/public/apps-package/js/blockly.js
+++ dashboard/public/apps-package/js/blockly.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7469a34d61fd8105654707ea990e8e8e70c8bc727204af40c6613212996bd5b4
-size 750216
+oid sha256:7678774ae274566b8a1ccaf15df9ed67bd47ad19313b34a5bc3b441ff8a4a652
+size 750425
