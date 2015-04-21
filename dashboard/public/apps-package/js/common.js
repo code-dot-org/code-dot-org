c7830eb3f978058e88a5878d22a011683929cea9
Continuous Integration <dev@code.org>
2015-04-21 22:33:47 +0000

    Automatically built.
    commit f4f1ec5a395b95ba8c693978a3a99caab61b28eb
    Merge: 38712a3 c8ac0fa
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Apr 21 15:24:44 2015 -0700
    
        Merge pull request #2147 from code-dot-org/noLastAttemptForEditBlocks
    
        dont use lastAttempt if we're editing blocks
    
    commit c8ac0fa9251f2644b7f16e2e2d81ebd372e69be6
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Apr 21 13:19:21 2015 -0700
    
        set blocks to empty xml instead of nil
    
    commit 38712a37a4eecab82eec36af3618dd77878fdbf9
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Apr 21 22:01:19 2015 +0000
    
        Automatically built.
    
        commit 9a950a6038de6e2460f486cb04ac9cd0d9b3b612
        Merge: 9096a6e 2c1afb1
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Tue Apr 21 14:52:06 2015 -0700
    
            Merge pull request #2153 from code-dot-org/revert-2152-revert-2148-function_example_colors
    
            Fix eyes regression from #2148
    
        commit 2c1afb17288bd6d515cf7439498501fae0eebb3a
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Tue Apr 21 14:48:00 2015 -0700
    
            Fix exception on creating a new function
            - Add unit test for creating a new function
            - Change openWithNewVariable to no longer use block creation callback
            - Add unit test for creating a new variable
    
        commit 9096a6e3ef3f006bdbb46ac12a30cafcb3bd5e63
        Author: Continuous Integration <dev@code.org>
        Date:   Tue Apr 21 20:41:12 2015 +0000
    
            Automatically built.
    
            commit eb4dd706a7b18fb4b99c870382d8c020866917e0
            Merge: 3c7f72e 1f5f5ac
            Author: Brian Jordan <bcjordan@gmail.com>
            Date:   Tue Apr 21 13:31:23 2015 -0700
    
                Merge pull request #2152 from code-dot-org/revert-2148-function_example_colors
    
                Revert #2148
    
            commit 1f5f5ac5c22de0a899082e87567aa142c5664aa3
            Author: Brian Jordan <bcjordan@gmail.com>
            Date:   Tue Apr 21 13:30:33 2015 -0700
    
                Revert "Add colorization and type checking for function definition and example blocks [finishes #91039770] [finishes #91039520] "

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -6931,6 +6931,9 @@ StudioApp.prototype.setCheckForEmptyBlocks = function (checkBlocks) {
  * @param loadLastAttempt If true, try to load config.lastAttempt.
  */
 StudioApp.prototype.setStartBlocks_ = function (config, loadLastAttempt) {
+  if (config.level.edit_blocks) {
+    loadLastAttempt = false;
+  }
   var startBlocks = config.level.startBlocks || '';
   if (loadLastAttempt) {
     startBlocks = config.level.lastAttempt || startBlocks;
