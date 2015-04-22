5a1a2924904cc402036fe2a861af3052373bf34b
Brent Van Minnen <bjvanminnen@gmail.com>
2015-04-22 13:05:15 -0700

    show runtime errors in gutter

--- apps/src/applab/acemode/mode-javascript_codeorg.js
+++ apps/src/applab/acemode/mode-javascript_codeorg.js
@@ -1,6 +1,7 @@
 var dropletConfig = require('../dropletConfig');
 var dropletUtils = require('../../dropletUtils');
 var errorMapper = require('./errorMapper');
+var annotationList = require('./annotationList');
 
 // define ourselves for ace, so that it knows where to get us
 ace.define("ace/mode/javascript_codeorg",["require","exports","module","ace/lib/oop","ace/mode/javascript","ace/mode/javascript_highlight_rules","ace/worker/worker_client","ace/mode/matching_brace_outdent","ace/mode/behaviour/cstyle","ace/mode/folding/cstyle","ace/config","ace/lib/net"], function(acerequire, exports, module) {
@@ -75,12 +76,14 @@ oop.inherits(Mode, JavaScriptMode);
       newOptions.predef[block.func] = false;
     });
 
+    annotationList.attachToSession(session);
+
     worker.send("changeOptions", [newOptions]);
 
     worker.on("jslint", function(results) {
       errorMapper.processResults(results);
 
-      session.setAnnotations(results.data);
+      annotationList.setJSLintAnnotations(results.data);
     });
 
     worker.on("terminate", function() {
