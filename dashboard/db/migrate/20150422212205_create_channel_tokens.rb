3ce918f39abb7d4e0369b7ba7385e1a5dac68cce
Josh Lory <josh.lory@code.org>
2015-04-22 14:34:19 -0700

    Add ChannelToken model

---
+++ dashboard/db/migrate/20150422212205_create_channel_tokens.rb
@@ -0,0 +1,12 @@
+class CreateChannelTokens < ActiveRecord::Migration
+  def change
+    create_table :channel_tokens do |t|
+      t.string :channel, null: false
+      t.references :user, null: false
+      t.references :level, null: false
+      t.index [:user_id, :level_id], unique: true
+
+      t.timestamps
+    end
+  end
+end
