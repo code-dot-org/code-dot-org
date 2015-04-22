5cdd135d311482c86f8c760f112dc88be78e8f20
Brent Van Minnen <bjvanminnen@gmail.com>
2015-04-22 13:05:15 -0700

    show runtime errors in gutter

---
+++ apps/src/applab/acemode/annotationList.js
@@ -0,0 +1,49 @@
+var annotations = [];
+var aceSession;
+
+/**
+ * Update gutter with our annotation list
+ * @private
+ */
+function updateGutter() {
+  if (!aceSession) {
+    return;
+  }
+  aceSession.setAnnotations(annotations);
+}
+
+/**
+ * Object for tracking annotations placed in gutter. General design is as
+ * follows:
+ * When jslint runs (i.e. code changes) display just jslint errors
+ * When code runs, display jslint errors and runtime errors. Runtime errors will
+ * go away the next time jstlint gets run (when code changes)
+ */
+module.exports = {
+  attachToSession: function (session) {
+    aceSession = session;
+  },
+
+  setJSLintAnnotations: function (jslintAnnotations) {
+    // clone annotations
+    annotations = jslintAnnotations.slice();
+    updateGutter();
+  },
+
+  /**
+   * @param {string} level
+   * @param {number} lineNumber One index line number
+   * @param {string} text Error string
+   */
+  addRuntimeAnnotation: function (level, lineNumber, text) {
+    var annotation = {
+      row: lineNumber - 1,
+      col: 0,
+      raw: text,
+      text: text,
+      type: level.toLowerCase()
+    };
+    annotations.push(annotation);
+    updateGutter();
+  },
+};
