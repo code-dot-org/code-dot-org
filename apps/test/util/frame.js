24cd4262eefe958cff025af4cf451afe9ac1dd0e
Brian Jordan <bcjordan@gmail.com>
2015-04-23 14:37:32 -0700

    Revert "Flesh out test stubs for getBBox's `x` and `y` properties, re-add fix from reverted PR #2182"

--- apps/test/util/frame.js
+++ apps/test/util/frame.js
@@ -33,9 +33,7 @@ function setGlobals () {
   jsdomRoot.dom.level3.html.HTMLElement.prototype.getBBox = function () {
     return {
       height: 0,
-      width: 0,
-      x: 0,
-      y: 0
+      width: 0
     };
   };
 }
