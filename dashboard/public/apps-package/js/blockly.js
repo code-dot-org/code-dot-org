09f4e95e30cae0d65dbb3b892024fae80f11b542
Continuous Integration <dev@code.org>
2015-04-23 20:36:10 +0000

    Automatically built.
    commit 61aba169d9d23e17fab3d9c612c30869996dbc41
    Merge: 82c7d69 269e57b
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Apr 23 13:26:29 2015 -0700
    
        Merge pull request #2185 from code-dot-org/getbbox_stubs
    
        Flesh out test stubs for getBBox's `x` and `y` properties, re-add fix from reverted PR #2182
    
    commit 269e57bf6f5df126d9c6223ae0b2170c2c4cebad
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Apr 23 13:22:43 2015 -0700
    
        Re-add fix for "Calculate block height without including any distance above the top of the block [finishes #90425824]"
    
    commit 382528624cf46f8379839dbbe955e6bc12077eef
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Apr 23 13:22:08 2015 -0700
    
        Add `x` and `y` properties to getBBox test stubs.

--- dashboard/public/apps-package/js/blockly.js
+++ dashboard/public/apps-package/js/blockly.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68a2c51fc3a06114023576173e5a59df4af204c1f2fbf5908c70eef7ccf9dad9
-size 751322
+oid sha256:3ef527ce80924ec90b74854979da81a7c10fc503e0aff1d874c03c4ee92769f7
+size 751356
