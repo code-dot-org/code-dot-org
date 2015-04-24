ce122af2555782fa10478e2de4aa48180f14f21f
Brad Buchanan <brad@code.org>
2015-04-24 14:56:33 -0700

    Define Game.has_footer? helper

--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -114,7 +114,7 @@ class ScriptLevelsController < ApplicationController
     @callback = milestone_url(user_id: current_user.try(:id) || 0, script_level_id: @script_level)
     view_options(
       full_width: true,
-      no_footer: (@game == Game.applab || @game == Game.netsim)
+      no_footer: !@game.has_footer?
     )
 
     @@fallback_responses ||= {}
