16dd97129d93a6a9e164c5be2b42795abb071ccb
Continuous Integration <dev@code.org>
2015-04-24 23:13:39 +0000

    Automatically built.
    commit 7fd210d4433d2f9a37d15a71377120dd4978a5e4
    Merge: 7dadd32 7b08f8f
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Apr 24 16:04:47 2015 -0700
    
        Merge pull request #2208 from code-dot-org/beeNames
    
        [Finishes #93124320] add donor names to wordsearch levels
    
    commit 7b08f8f34433a24ccb31a23d8f125dff5477954d
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Fri Apr 24 15:48:59 2015 -0700
    
        add donor names to wordsearch levels

--- dashboard/public/apps-package/js/maze.js
+++ dashboard/public/apps-package/js/maze.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e8a2cf113c7d08e553b7c2e512b0dd3f36a7d959feb1fe8b7b62965efade2a9
-size 236899
+oid sha256:a13cc5aab0c371fbabbed4f8ed80851d894e8b3e56ec89fc333351a00ea06e0a
+size 237100
