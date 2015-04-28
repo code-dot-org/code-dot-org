e6ee211ce27d50ac1926930cd5adde79377e7575
Will Jordan <will@code.org>
2015-04-27 17:25:02 -0700

    Add "Avg. Time on Page" to /admin/levels stats

--- dashboard/app/controllers/reports_controller.rb
+++ dashboard/app/controllers/reports_controller.rb
@@ -228,8 +228,10 @@ SQL
       output_data[key]['Perceived Dropout'] = output_data[key]['UniqueAttempt'].to_f - output_data[key]['UniqueSuccess'].to_f
     end
 
+    page_data = Hash[GAClient.query_ga(@start_date, @end_date, 'ga:pagePath', 'ga:avgTimeOnPage', 'ga:pagePath=~^/s/|^/flappy/|^/hoc/').data.rows]
+
     @data_array = output_data.map do |key, value|
-      {'Puzzle' => key}.merge(value)
+      {'Puzzle' => key}.merge(value).merge('timeOnSite' => page_data[key] && page_data[key].to_i)
     end
     require 'naturally'
     @data_array = @data_array.select{|x| x['TotalAttempt'].to_i > 10}.sort_by{|i| Naturally.normalize(i.send(:fetch, 'Puzzle'))}
