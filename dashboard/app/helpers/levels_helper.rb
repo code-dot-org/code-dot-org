0a8560ad50339b3f7c1f179c1afb103e1ca4e085
Josh Lory <josh.lory@code.org>
2015-04-28 16:05:53 -0700

    Model doesn't have access to `request`, move back to helper

--- dashboard/app/helpers/levels_helper.rb
+++ dashboard/app/helpers/levels_helper.rb
@@ -29,7 +29,22 @@ module LevelsHelper
     # Otherwise the current level.
     host_level = @level.try(:project_template_level) || @level
 
-    view_options channel: ChannelToken.unique_for_user_and_level(current_user, host_level)
+    # If `create` fails because it was beat by a competing request, a second
+    # `find_by` should succeed.
+    channel_token = retryable on: [Mysql2::Error, ActiveRecord::RecordNotUnique], matching: /Duplicate entry/ do
+      ChannelToken.find_or_create_by!(level: host_level, user: current_user) do |ct|
+        # Get a new channel_id.
+        ct.channel = ChannelsApi.call(request.env.merge(
+          'REQUEST_METHOD' => 'POST',
+          'PATH_INFO' => '/v3/channels',
+          'REQUEST_PATH' => '/v3/channels',
+          'CONTENT_TYPE' => 'application/json;charset=utf-8',
+          'rack.input' => StringIO.new('{"hidden":"true"}')
+        ))[1]['Location'].split('/').last
+      end
+    end
+
+    view_options channel: channel_token.channel
   end
 
   def select_and_track_autoplay_video
