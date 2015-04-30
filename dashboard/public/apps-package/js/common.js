70184394048631bb0e9d841dbeaef418effa1110
Continuous Integration <dev@code.org>
2015-04-30 19:58:00 +0000

    Automatically built.
    commit df9a9514973555621efb30f5b36027f8af3cdda4
    Merge: 22f0372 d9b3d7d
    Author: Chris Pirich <chris@code.org>
    Date:   Thu Apr 30 12:45:03 2015 -0700
    
        Merge pull request #2256 from code-dot-org/applab_debug_resize
    
        Applab debug resize
    
    commit 22f037288147ace169764977ab35031d80f1afaf
    Author: Laurel Fan <laurel@code.org>
    Date:   Thu Apr 30 12:42:20 2015 -0700
    
        fix poste/clicks
    
    commit df5bc17daa90cdde55a0d34640fcdaeed33dd447
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Apr 30 18:58:55 2015 +0000
    
        Dropbox changes
    
    commit b20bc9cd0b553bd38af5c156935fbc9fd57586f0
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Apr 30 18:35:01 2015 +0000
    
        Automatically built.
    
        commit 2a57bca92bf304a34749e7d7f52d0a0b2f8bf4ce
        Merge: 64fcc5d 60e5088
        Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
        Date:   Thu Apr 30 11:23:18 2015 -0700
    
            Merge pull request #2255 from code-dot-org/netsim_long_display_names
    
            NetSim: Long display names
    
        commit 64fcc5d1fe537d68092b3955d15763ca3e5301a8
        Author: Continuous Integration <dev@code.org>
        Date:   Thu Apr 30 17:59:27 2015 +0000
    
            Automatically built.
    
            commit f3c8fea9e2283ec53cb9e7139743c86e966c5c93
            Merge: f2a223a 040a362
            Author: Brian Jordan <bcjordan@gmail.com>
            Date:   Thu Apr 30 10:49:57 2015 -0700
    
                Merge pull request #2230 from code-dot-org/revert-2228-revert-2213-params_from_md_files
    
                Fix parens issue from #2213, re-add changes [WIP]
    
            commit 040a36295ca7b14f91ed883391f062cce21319f5
            Author: Brian Jordan <bcjordan@gmail.com>
            Date:   Thu Apr 30 10:46:13 2015 -0700
    
                Remove descriptions over 140 characters
                - Escape { and } with \\ for MessageFormat processing
    
            commit f2a223aecf6f4e20b13568d11ba1f839855ef8e4
            Author: Laurel Fan <laurel@code.org>
            Date:   Wed Apr 29 20:06:19 2015 -0700
    
                add pd@code.org to script assignment email
    
        commit f3c8fea9e2283ec53cb9e7139743c86e966c5c93
        Merge: f2a223a 040a362
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Thu Apr 30 10:49:57 2015 -0700
    
            Merge pull request #2230 from code-dot-org/revert-2228-revert-2213-params_from_md_files
    
            Fix parens issue from #2213, re-add changes [WIP]
    
        commit 040a36295ca7b14f91ed883391f062cce21319f5
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Thu Apr 30 10:46:13 2015 -0700
    
            Remove descriptions over 140 characters
            - Escape { and } with \\ for MessageFormat processing
    
        commit 60e50881d162b27202cd5c895a4989c85bd6aea5
        Author: Brad Buchanan <brad@code.org>
        Date:   Thu Apr 30 10:32:37 2015 -0700
    
            Collapse empty JSDoc
    
        commit f2a223aecf6f4e20b13568d11ba1f839855ef8e4
        Author: Laurel Fan <laurel@code.org>
        Date:   Wed Apr 29 20:06:19 2015 -0700
    
            add pd@code.org to script assignment email
    
        commit ed124ba53b64ae83fdf6dbf1b80593a4cacc8b66
        Author: Brad Buchanan <brad@code.org>
        Date:   Wed Apr 29 17:32:26 2015 -0700
    
            Don't show address bubble when node doesn't have an address, as in P2P connections.
    
        commit dd47a69ed7be6d9ed8fecf490de6688496f999af
        Author: Brad Buchanan <brad@code.org>
        Date:   Wed Apr 29 17:04:48 2015 -0700
    
            Make sure visualization nodes are given the DNS mode when they are created.
    
    commit 49c16759d82c9674c7f59ef0e96233f2655b4aab
    Merge: 2a57bca 99b98e1
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Apr 30 11:25:37 2015 -0700
    
        Merge pull request #2259 from code-dot-org/ui_test_script_stub
    
        Add UI test script stub with a handful of edit-code levels

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7d8cd5a3407d776fcdc931dea0b5fcb496787a9dd7875f4ca09435de2586b53
-size 598940
+oid sha256:ee19cb753a78f812348188d28d23d0d3db366aaa511d9c9ae35ba5c6c12dc24a
+size 599125
