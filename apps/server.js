7d2c7fd304816806e4a92eb94530da8cc6937b27
Brent Van Minnen <bjvanminnen@gmail.com>
2015-04-30 15:39:49 -0700

    prevent double live reload

--- apps/server.js
+++ apps/server.js
@@ -7,13 +7,28 @@ var app = require('./src/dev/server.js');
 app.use(express.static('./build/package'));
 app.listen(PORT);
 tinylr.listen(35729);
+
+// There's some weirdness where making a single change results in two change
+// events. If we did nothing, that would result in us double loading the page.
+// I think this has to do with watchify and factor-bundle not playing super well
+// together.
+// I've introduced a hack where we wait 1000ms after a change event to see if we
+// get another one, and then do the reload with whichever change came through last.
+var changeInfo = null;
 gaze(['./build/**/*'], function(err, watcher) {
   this.on('all', function(event, path) {
-    tinylr.changed({
+    if (!changeInfo) {
+      setTimeout(function () {
+        tinylr.changed(changeInfo);
+        changeInfo = null;
+      }, 1000);
+    }
+
+    changeInfo = {
       body: {
         files: [path]
       }
-    });
+    };
   });
 });
 console.log('Started dev server on port ' + PORT);
