a5cf46b284a4993251b94ffc652c1fc91d28659b
Brent Van Minnen <bjvanminnen@gmail.com>
2015-04-30 15:15:59 -0700

    dev server race condition

--- apps/server.js
+++ apps/server.js
@@ -1,14 +1,19 @@
 // Basic Express server with LiveReload watcher
-var PORT = process.env.npm_package_config_port;
+var PORT = process.env.npm_package_config_port || 8000;
 var tinylr = require('tiny-lr')();
 var express = require('express');
+var gaze = require('gaze');
 var app = require('./src/dev/server.js');
 app.use(express.static('./build/package'));
 app.listen(PORT);
 tinylr.listen(35729);
-require('gaze')(['./build/**/*', './src/dev/**/*'], function(err, watcher) {
+gaze(['./build/**/*'], function(err, watcher) {
   this.on('all', function(event, path) {
-    tinylr.changed({body: {files: [path]}});
+    tinylr.changed({
+      body: {
+        files: [path]
+      }
+    });
   });
 });
 console.log('Started dev server on port ' + PORT);
