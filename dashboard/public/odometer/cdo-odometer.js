e116d669150905a1a4b2e9511ad86dbf0f0ee4a4
Josh Lory <josh.lory@code.org>
2015-05-04 15:47:14 -0700

    Fix changing radix on the arbitrary base odometer

--- dashboard/public/odometer/cdo-odometer.js
+++ dashboard/public/odometer/cdo-odometer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6caef59e49391846326a5b5626f0e682d6e5508e49deca7a4d58d37fb25c1b66
-size 1319
+oid sha256:35c06284146eb268b47a8f1838ffedb93a9a0168f3c8372a9d1d54112620584d
+size 1494
