7458f1363b4dff8b0b865cf4e8ccd580bed3ad46
Josh Lory <josh.lory@code.org>
2015-05-04 15:04:00 -0700

    Style changes: use different gradient

--- dashboard/public/odometer/cdo-odometer.js
+++ dashboard/public/odometer/cdo-odometer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60fab6b95bfbec4baf679df90a1a2d773d740d82620144ac098f7cb8815f7d0d
-size 1291
+oid sha256:6caef59e49391846326a5b5626f0e682d6e5508e49deca7a4d58d37fb25c1b66
+size 1319
