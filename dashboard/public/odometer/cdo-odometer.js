e86912b1beff95a4733f70b606cb520fc66df939
Josh Lory <josh.lory@code.org>
2015-05-04 13:26:37 -0700

    Initial odometer DOM

--- dashboard/public/odometer/cdo-odometer.js
+++ dashboard/public/odometer/cdo-odometer.js
@@ -1,4 +1,12 @@
-function Odometer() {
+function Odometer(parent, digits, radix) {
+
+  // Create DOM.
+  var odometer = $("<div class='odometer'>");
+  for (var i = 0; i < digits; i++) {
+    odometer.append("<div class='digit'><div class='digit-current'>0</div><div class='digit-next'>1</div>")
+  }
+  $(parent).append(odometer);
+
   this.set = function() {
     //
   }
