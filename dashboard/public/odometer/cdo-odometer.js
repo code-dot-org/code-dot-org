8ce73aba044009caadec95e1b6ac67414f938256
Josh Lory <josh.lory@code.org>
2015-05-04 14:15:38 -0700

    Odometer updating but not yet scrolling

--- dashboard/public/odometer/cdo-odometer.js
+++ dashboard/public/odometer/cdo-odometer.js
@@ -1,13 +1,30 @@
-function Odometer(parent, digits, radix) {
+function Odometer(config) {
+
+  var DIGIT_HEIGHT = 32;
+  var digits = [];
+  var last = null;
 
   // Create DOM.
   var odometer = $("<div class='odometer'>");
-  for (var i = 0; i < digits; i++) {
-    odometer.append("<div class='digit'><div class='digit-current'>0</div><div class='digit-next'>1</div>")
+  for (var i = 0; i < config.digits; i++) {
+    var digit = $("<div class='digit'><div class='digit-current'>0</div><div class='digit-next'>1</div>");
+    digits.push(digit);
+    odometer.append(digit);
   }
-  $(parent).append(odometer);
+  $(config.parent).addClass('odometerParent').append(odometer);
 
-  this.set = function() {
-    //
+  this.set = function(value) {
+    var scrollAmount = (value % 1) * DIGIT_HEIGHT;
+    value = Math.floor(value);
+    if (value !== last) {
+      console.log('updating');
+      last = value;
+      var current = value.toString(config.radix);
+      var next = (value + 1).toString(config.radix);
+      for (var i = 0; i < config.digits; i++) {
+        digits[i].find('.digit-current').text(current[i - (config.digits - current.length)] || 0);
+        digits[i].find('.digit-next').text(next[i - (config.digits - next.length)] || 0);
+      }
+    }
   }
 }
