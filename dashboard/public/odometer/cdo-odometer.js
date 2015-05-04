794eea01e8f630a6888948d0fc8fbf0e83cca2f8
Josh Lory <josh.lory@code.org>
2015-05-04 16:48:36 -0700

    Fix changing radix

--- dashboard/public/odometer/cdo-odometer.js
+++ dashboard/public/odometer/cdo-odometer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91727503f6a69b9cf5f67b70a6a4d003d1da3819444f466d8dbe8713063557b0
-size 2173
+oid sha256:cbecbc1d0987ba7873a3fa390a897f1decbf18509c85ba69cea9f945137bfcc2
+size 2263
