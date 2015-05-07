bc60d2435e0cdc1d6b789f39a3f8298214bce191
CaptainStack <CaptainStack@outlook.com>
2015-05-07 14:31:57 -0700

    changed name of table

--- dashboard/db/migrate/20150507195442_multiple_cohorts_to_workshop.rb
+++ dashboard/db/migrate/20150507195442_multiple_cohorts_to_workshop.rb
@@ -1,6 +1,6 @@
 class MultipleCohortsToWorkshop < ActiveRecord::Migration
   def change
-    create_table :workshops_cohorts do |t|
+    create_table :workshop_cohorts do |t|
       t.integer :workshop_id
       t.integer :cohort_id
       t.timestamps
