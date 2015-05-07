3bd25bc752592170cd7f8f4cf4f3fb8f545a2053
CaptainStack <CaptainStack@outlook.com>
2015-05-07 13:35:14 -0700

    Adding timestamps

--- dashboard/db/migrate/20150507195442_multiple_cohorts_to_workshop.rb
+++ dashboard/db/migrate/20150507195442_multiple_cohorts_to_workshop.rb
@@ -3,6 +3,7 @@ class MultipleCohortsToWorkshop < ActiveRecord::Migration
     create_table :workshops_cohorts do |t|
       t.integer :workshop_id
       t.integer :cohort_id
+      t.timestamps
     end
   end
 end
