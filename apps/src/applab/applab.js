536088434920bc86e0b2834762ca2e9a8234335d
cpirich <chris@code.org>
2015-05-10 15:31:39 -0700

    Revert "Revert "add touch support to resize bars""
    This reverts commit 6126c553190551eb180758584f598070c2391fed.

--- apps/src/applab/applab.js
+++ apps/src/applab/applab.js
@@ -988,10 +988,10 @@ Applab.init = function(config) {
 
   var debugResizeBar = document.getElementById('debugResizeBar');
   if (debugResizeBar) {
-    debugResizeBar.addEventListener('mousedown',
-                                    Applab.onMouseDownDebugResizeBar);
-    document.body.addEventListener('mouseup',
-                                   Applab.onMouseUpDebugResizeBar);
+    dom.addMouseDownTouchEvent(debugResizeBar,
+                               Applab.onMouseDownDebugResizeBar);
+    dom.addMouseUpTouchEvent(document.body,
+                             Applab.onMouseUpDebugResizeBar);
   }
 
   var finishButton = document.getElementById('finishButton');
@@ -1072,6 +1072,11 @@ Applab.onMouseDownDebugResizeBar = function (event) {
   if (event.srcElement.id === 'debugResizeBar') {
     Applab.draggingDebugResizeBar = true;
     document.body.addEventListener('mousemove', Applab.onMouseMoveDebugResizeBar);
+    Applab.mouseMoveTouchEventName = dom.getTouchEventName('mousemove');
+    if (Applab.mouseMoveTouchEventName) {
+      document.body.addEventListener(Applab.mouseMoveTouchEventName,
+                                     Applab.onMouseMoveDebugResizeBar);
+    }
 
     event.preventDefault();
   }
@@ -1114,6 +1119,10 @@ Applab.onMouseUpDebugResizeBar = function (event) {
   // If we have been tracking mouse moves, remove the handler now:
   if (Applab.draggingDebugResizeBar) {
     document.body.removeEventListener('mousemove', Applab.onMouseMoveDebugResizeBar);
+    if (Applab.mouseMoveTouchEventName) {
+      document.body.removeEventListener(Applab.mouseMoveTouchEventName,
+                                        Applab.onMouseMoveDebugResizeBar);
+    }
     Applab.draggingDebugResizeBar = false;
   }
 };
