c9158aea7cd57a1934424f5e2b94feac147ae9eb
Brent Van Minnen <bjvanminnen@gmail.com>
2015-05-13 16:38:07 -0700

    use localpaths + linklocal to manage dependencies between packages in same repo

--- apps/test/util/runTests.js
+++ apps/test/util/runTests.js
@@ -5,18 +5,10 @@
 var which = require('npm-which')(__dirname + '/../..').sync;
 var mochify = require('mochify');
 var http = require('http');
-
-// TODO (brent) When we move to npm/gulp, we will want to take care of this
-// logic in our gulpfile instead
-// Right now we manually create a symbolic link to @cdo/apps
 var fs = require('fs');
+
 var exec = require('child_process').exec;
-var target = __dirname + '/../../src/';
-var nodePath = __dirname + '/../../node_modules/@cdo';
-var command = '';
-if (!fs.existsSync(nodePath)) {
-  command = 'mkdir -p ' + nodePath + ' && ln -s ' + target + ' ' + nodePath + '/apps';
-}
+command = which('linklocal');
 
 /**
  * Mochify uses phantomic to run a server that it connects to, which serves up
