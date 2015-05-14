b0f8862253129eff7bcb3028310dd9e425f1e11f
Continuous Integration <dev@code.org>
2015-05-14 18:47:50 +0000

    Automatically built.
    commit 4dc5c877e4a9b92e54adcf0caaaa790cdf342b74
    Author: cpirich <chris@code.org>
    Date:   Thu May 14 11:40:00 2015 -0700
    
        applab: reset debugOutput with textContent instead of value

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:474626b7b662d152bd0ba99f10b54f11a67defe0cebd71f207118676cf25de7c
-size 842905
+oid sha256:2ccc9eefd24cbe8bb8fbb6898e07e9a9e7c2ab1dca30c30a87a3de0411b32428
+size 842911
