4893da259625671d291af132422162ec54a40958
Continuous Integration <dev@code.org>
2015-05-14 16:54:24 +0000

    Automatically built.
    commit 7235520d187b74cc62a7a804d52669c11d2dfafa
    Author: cpirich <chris@code.org>
    Date:   Thu May 14 09:40:59 2015 -0700
    
        skip apiValidateDomIdExistence check for onEvent with body element, fix forId check, default to 320x480 for custom levels

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dbc7cec400cc148b9a4d017b905d78407bbf17dbc0bb995b8261e3f81ebe947
-size 842893
+oid sha256:474626b7b662d152bd0ba99f10b54f11a67defe0cebd71f207118676cf25de7c
+size 842905
