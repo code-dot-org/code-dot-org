76b874cd37b6490777413d4a700bb634c9dcc582
Continuous Integration <dev@code.org>
2015-05-15 20:33:14 +0000

    Automatically built.
    commit 35436591752a1abc2c1d444c59800b99d271e9e8
    Merge: b6f348f afaabca
    Author: Continuous Integration <dev@code.org>
    Date:   Fri May 15 20:16:12 2015 +0000
    
        Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
    commit afaabca19e062553426937a6f9c2b8087d4f22fc
    Merge: 2e2b58b 74c611b
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Fri May 15 13:16:37 2015 -0700
    
        Merge pull request #2431 from code-dot-org/fix-design-mode-scaling
    
        fix design mode scaling when browser is zoomed; applab area is resiz…

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1653beadc63b800556117951d2fee8b352caa2f60912bf1a6b668fc64761424
-size 842626
+oid sha256:1732ef4930f45ed2e5daab06b5d9f1744fe21888fb63f0524923ba648aa8b9d4
+size 842400
