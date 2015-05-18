a3ad76fffb49fb62d03b5a35d6e86b8b0453b65f
Continuous Integration <dev@code.org>
2015-05-18 17:46:10 +0000

    Automatically built.
    commit 1f678b34a4c99e480dccca5594669827660fd7fb
    Merge: 0988094 2267122
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon May 18 10:46:26 2015 -0700
    
        Merge pull request #2444 from code-dot-org/fix-project-share
    
        Fix project share
    
    commit 2267122ed203863aa400d45b82a65aecd4575213
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon May 18 10:12:50 2015 -0700
    
        Fix `appToProjectUrl` which was removed from the global namespace

--- dashboard/public/shared-package/js/shared.js
+++ dashboard/public/shared-package/js/shared.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7754601601adf985a33128643cc66d6205adb0897b09923f7a9b350c9582423d
-size 18083
+oid sha256:2a599728fc01dafc282b8e21f9aee8b6d32bdafee0ed30d4781d5e4d84af1505
+size 18117
