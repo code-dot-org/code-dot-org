aac397500db9015c2d9a7135f58d8a478d752e1f
David Bailey <dsb123@gmail.com>
2015-05-19 11:18:45 -0700

    implement PUT, DELETE and list operations for /v3/assets.

--- shared/middleware/assets_api.rb
+++ shared/middleware/assets_api.rb
@@ -1,5 +1,5 @@
 require 'cdo/aws/s3'
-#require 'cdo/rack/request'
+require 'cdo/rack/request'
 require 'sinatra/base'
 
 class AssetsApi < Sinatra::Base
@@ -13,12 +13,44 @@ class AssetsApi < Sinatra::Base
     end
   end
 
+  @allowed_file_types = [
+    'jpg',
+    'jpeg',
+    'gif',
+    'png',
+    'mp3'
+  ]
+
+  #
+  # GET /v3/assets/<channel-id>
+  #
+  # List filenames and sizes.
+  #
+  get %r{/v3/assets/([^/]+)$} do |encrypted_channel_id|
+    dont_cache
+    content_type :json
+
+    owner_id, channel_id = storage_decrypt_channel_id(encrypted_channel_id)
+    prefix = "#{CDO.assets_s3_directory}/#{owner_id}/#{channel_id}"
+    s3 = Aws::S3::Client.new(region: 'us-east-1')
+    s3.list_objects(bucket:CDO.assets_s3_bucket, prefix:prefix).contents.map do |fileinfo|
+      filename = %r{#{prefix}/(.+)$}.match(fileinfo.key)[1]
+      {filename:filename, size:fileinfo.size}
+    end.to_json
+  end
+
+  #
+  # GET /v3/assets/<channel-id>/<filename>
+  #
+  # Read a file.
+  #
   get %r{/v3/assets/([^/]+)/([^/]+)$} do |encrypted_channel_id, filename|
     dont_cache
+    not_found unless type = filename.split('.').last
+    content_type type
+
     # storage_decrypt_channel_id('niR2mv8Utp9BOKv8sN2E8A') --> [1, 371]
     owner_id, channel_id = storage_decrypt_channel_id(encrypted_channel_id)
-    ## infer content_type from filename extension
-    content_type :gif
     s3 = Aws::S3::Client.new(region: 'us-east-1')
     key = "#{CDO.assets_s3_directory}/#{owner_id}/#{channel_id}/#{filename}"
     begin
@@ -26,6 +58,45 @@ class AssetsApi < Sinatra::Base
     rescue Aws::S3::Errors::NoSuchKey
       not_found
     end
+  end
+
+  #
+  # PUT /v3/assets/<channel-id>/<filename>
+  #
+  # Create a file.
+  #
+  put %r{/v3/assets/([^/]+)/([^/]+)$} do |encrypted_channel_id, filename|
+    dont_cache
+    file_type = filename.split('.').last
+    unsupported_media_type unless file_type
+    mime_type = request.content_type.to_s.split(';').first
+    unsupported_media_type unless  mime_type == Sinatra::Base.mime_type(file_type)
+
+    owner_id, channel_id = storage_decrypt_channel_id(encrypted_channel_id)
+
+    s3 = Aws::S3::Client.new(region: 'us-east-1')
+    key = "#{CDO.assets_s3_directory}/#{owner_id}/#{channel_id}/#{filename}"
+    s3.put_object(bucket:CDO.assets_s3_bucket, key:key, body:request.body.read)
+    no_content
+  end
+
+  #
+  # DELETE /v3/assets/<channel-id>/<filename>
+  #
+  # Delete a file.
+  #
+  delete %r{/v3/assets/([^/]+)/([^/]+)$} do |encrypted_channel_id, filename|
+    dont_cache
+    owner_id, channel_id = storage_decrypt_channel_id(encrypted_channel_id)
+    s3 = Aws::S3::Client.new(region: 'us-east-1')
+    key = "#{CDO.assets_s3_directory}/#{owner_id}/#{channel_id}/#{filename}"
+
+    begin
+      s3.delete_object(bucket:CDO.assets_s3_bucket, key:key)
+    rescue Aws::S3::Errors::NoSuchKey
+      not_found
+    end
+    no_content
   end
 
 end
