2dc89e318835a682fff4c393420c34bee392def9
Brent Van Minnen <bjvanminnen@gmail.com>
2015-05-18 17:38:28 -0700

    add more properties, including boolean ones

--- apps/src/applab/designElements/PropertyRow.jsx
+++ apps/src/applab/designElements/PropertyRow.jsx
@@ -6,21 +6,44 @@ var PropertyRow = React.createClass({
       React.PropTypes.string,
       React.PropTypes.number
     ]).isRequired,
+    // TODO (brent) another option instead of toggles would be different types
+    // of property row (text/color picker/image chooser/etc)
+    hasColorPicker: React.PropTypes.bool,
+    hasImageChooser: React.PropTypes.bool,
     handleChange: React.PropTypes.func
+  },
+
+  getInitialState: function () {
+    return {
+      value: this.props.initialValue
+    };
   },
 
   handleChangeInternal: function(event) {
     var value = event.target.value;
     this.props.handleChange(value);
+    this.setState({value: value});
   },
+  
   render: function() {
+    var colorPicker, imageChooser;
+    if (this.props.hasColorPicker) {
+      colorPicker = <div>PICK_COLOR</div>
+    }
+
+    if (this.props.imageChooser) {
+      imageChooser = <div>Choose...</div>
+    }
+
     return (
       <tr>
         <td>{this.props.desc}</td>
         <td>
           <input
-            defaultValue={this.props.initialValue}
+            value={this.state.value}
             onChange={this.handleChangeInternal}/>
+          {colorPicker}
+          {imageChooser}
         </td>
       </tr>
     );
