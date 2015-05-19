0c59ea55baca68f11d73d9cf4694dfb927385d67
Brent Van Minnen <bjvanminnen@gmail.com>
2015-05-19 11:01:29 -0700

    basic color picker working

--- apps/src/applab/applab.js
+++ apps/src/applab/applab.js
@@ -35,6 +35,7 @@ var apiTimeoutList = require('../timeoutList');
 var RGBColor = require('./rgbcolor.js');
 var annotationList = require('./acemode/annotationList');
 var React = require('react');
+
 // TODO (brent) - make it so that we dont need to specify .jsx. This currently
 // works in our grunt build, but not in tests
 var DesignProperties = require('./designProperties.jsx');
