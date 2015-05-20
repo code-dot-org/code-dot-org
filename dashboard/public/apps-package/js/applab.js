547d89d89085565a3173aa6cd94f678940faf2b6
Continuous Integration <dev@code.org>
2015-05-20 17:02:33 +0000

    Automatically built.
    commit 051bd10ca975617d55afd0d7d060dca18ffdfeb5
    Merge: f3de02f 6bbd3f1
    Author: Chris Pirich <chris@code.org>
    Date:   Wed May 20 09:55:10 2015 -0700
    
        Merge pull request #2473 from code-dot-org/applab_setPosition_changes
    
        don't require setPosition width/height in specific cases
    
    commit 6bbd3f1c6ca88cf10b4c3cf115fedbe51cf1a936
    Author: cpirich <chris@code.org>
    Date:   Wed May 20 09:39:29 2015 -0700
    
        don't require setPosition width/height in specific cases

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b4962ea65c4333f319dc3a353ca686bc54de06a86655eed89daf65e43fadc39
-size 852686
+oid sha256:193405d574fcb88dcf2f17ecfd32f829d6bd5d6d98688f13b9b3373e51ecefc8
+size 853213
