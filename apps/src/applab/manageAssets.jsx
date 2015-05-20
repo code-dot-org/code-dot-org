60a193b4a55f1815d175af0be670e51488219e2a
Josh Lory <josh.lory@code.org>
2015-05-20 12:33:11 -0700

    Scrollable asset list and mock uploader

--- apps/src/applab/manageAssets.jsx
+++ apps/src/applab/manageAssets.jsx
@@ -7,21 +7,24 @@ module.exports = React.createClass({
     assets: React.PropTypes.instanceOf(Array)
   },
 
+  fileUploadClicked: function () {
+    var uploader = document.querySelector('#uploader');
+    uploader.click();
+  },
+
   render: function() {
     return (
       <div className="modal-content" style={{margin: 0}}>
         <p className="dialog-title">Manage Assets</p>
-        <table style={{width: '100%', margin: '1em 0'}}>
-          <tr>
-            <th width="50">Thumbnail</th>
-            <th>Filename</th>
-            <th width="250">Actions</th>
-          </tr>
-          {this.props.assets.map(function (asset) {
-            return <AssetRow name={asset.name} type={asset.type} src={asset.src}/>;
-          })}
-        </table>
-        <button className="share"><i className="fa fa-upload"></i> Upload File</button>
+        <div style={{maxHeight: '330px', overflow: 'scroll', margin: '1em 0'}}>
+          <table style={{width: '100%'}}>
+            {this.props.assets.map(function (asset) {
+              return <AssetRow name={asset.name} type={asset.type} src={asset.src}/>;
+            })}
+          </table>
+        </div>
+        <input type="file" id="uploader" style={{display: 'none'}}/>
+        <button onClick={this.fileUploadClicked} className="share"><i className="fa fa-upload"></i> Upload File</button>
       </div>
     );
   }
