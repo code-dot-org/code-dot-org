9ec267f1817725b9726525e95b48efe09392c088
Continuous Integration <dev@code.org>
2015-05-21 14:13:40 +0000

    Automatically built.
    commit c2e28807ed2e90aca8c95b01b07b69ae4c1c1053
    Merge: edf76fd d66fabd
    Author: Chris Pirich <chris@code.org>
    Date:   Thu May 21 07:05:46 2015 -0700
    
        Merge pull request #2482 from code-dot-org/applab_debugger_fixes
    
        applab debugger fixes: switch statements, highlighting
    
    commit edf76fd0c35f57a2416ee5e234d1938867fc9af2
    Author: Continuous Integration <dev@code.org>
    Date:   Thu May 21 05:57:32 2015 +0000
    
        Automatically built.
    
        commit 83e61a61ad5e20a570a5d8a9feafa287fae9ba3e
        Merge: e0a5243 7a19db1
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Wed May 20 22:49:51 2015 -0700
    
            Merge pull request #2479 from code-dot-org/designMode_text
    
            Design mode text/label
    
        commit 7a19db11ddda188a852394c69a0a2b93257e34af
        Author: Brent Van Minnen <bjvanminnen@gmail.com>
        Date:   Wed May 20 22:48:43 2015 -0700
    
            address code review, add a couple TODOs
    
        commit e0a5243e7798180480628165223edd42beca6284
        Merge: 4e9f7ec 9c96d9e
        Author: davidsbailey <davidsbailey@users.noreply.github.com>
        Date:   Wed May 20 20:33:48 2015 -0700
    
            Merge pull request #2481 from code-dot-org/assets-client-lib
    
            add category (image/sound) to readAll response. add metadata including file size to PUT response.
    
        commit 9c96d9ed3e77f878c743f0d086c9272de9865f3e
        Author: David Bailey <dsb123@gmail.com>
        Date:   Wed May 20 20:31:48 2015 -0700
    
            touch up
    
        commit bb17051130c1b9c0ac2781091cc16ed1491d0320
        Author: Brent Van Minnen <bjvanminnen@gmail.com>
        Date:   Wed May 20 17:07:17 2015 -0700
    
            add checkbox
    
        commit cfc9032cf341ad60f68bbe11285ab210ae6bc193
        Author: David Bailey <dsb123@gmail.com>
        Date:   Wed May 20 17:00:10 2015 -0700
    
            add comments

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab521e221e023e497af816cea382a605f35d32793e47ebe88fae0908ddec1b8a
-size 883197
+oid sha256:f937a685cda859932964cf9c9c83bd6f9b5ffa29b6ae9c2899a33d658dbd523a
+size 883291
--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42220f848775abace47c574b3d948603bb9a4c35a88790099dbae44151226ee3
-size 620210
+oid sha256:b423189e52e47e6a5b3adbd88a5c6c1890be995ffb3dfcc948b621e68c3bd736
+size 620689
