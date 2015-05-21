ce09ba648af9a02f4b1131503b70e668837afe4c
Laurel Fan <laurel@code.org>
2015-05-21 16:21:33 -0700

    Do not show teacher answer viewer in pd scripts

--- dashboard/app/models/concerns/serialized_properties.rb
+++ dashboard/app/models/concerns/serialized_properties.rb
@@ -42,6 +42,7 @@ module SerializedProperties
     def init_internals
       sti_hierarchy.map { |x| serialized_properties[x.to_s] || [] }.flatten.each do |property|
         define_method(property) { read_attribute('properties')[property.to_s] }
+        define_method("#{property}?") { read_attribute('properties')[property.to_s] }
         define_method("#{property}=") { |value| read_attribute('properties')[property.to_s] = value }
       end
     end
