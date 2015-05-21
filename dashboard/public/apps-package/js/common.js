b1319147d92a7a4775983d4af9a351e0f1a9c650
Continuous Integration <dev@code.org>
2015-05-21 17:10:38 +0000

    Automatically built.
    commit c0f2b3caba29bbbf3f8ec1aec0e36bb7f219852c
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu May 21 09:53:02 2015 -0700
    
        add back width/height for checkbox

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b423189e52e47e6a5b3adbd88a5c6c1890be995ffb3dfcc948b621e68c3bd736
-size 620689
+oid sha256:96a917ef4e040aae332903fee2d42dbb173bb9a0f635bec8b2bd8238fda7f22f
+size 620871
