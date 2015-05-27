9ea32480f5ac79332b9d1109684bc2c2f7f1bfd6
David Bailey <dsb123@gmail.com>
2015-05-27 16:02:55 -0700

    fix layout.

--- apps/src/applab/designModeBox.jsx
+++ apps/src/applab/designModeBox.jsx
@@ -7,35 +7,23 @@ module.exports = React.createClass({
   },
   render: function() {
     return (
-      <div id="designModeBoxContainer" width="100%">
-        <table width="100%">
-          <colgroup>
-            <col width="270px"/>
-            <col/>
-          </colgroup>
-          <tr>
-            <td>
-              <h3>{applabMsg.designToolboxDescription}</h3>
-              <div id="design-elements">
-                {/* TODO (brent) better approach than storing this as a data on the element? */}
-                <div data-element-type="BUTTON" className="new-design-element">button</div>
-                <div data-element-type="LABEL" className="new-design-element">label</div>
-                <div data-element-type="TEXT_INPUT" className="new-design-element">input</div>
-                <div data-element-type="TEXT_AREA" className="new-design-element">text area</div>
-                <div data-element-type="CHECKBOX" className="new-design-element">checkbox</div>
-                <div data-element-type="RADIO_BUTTON" className="new-design-element">radio button</div>
-                <div data-element-type="DROPDOWN" className="new-design-element">dropdown</div>
-                <div data-element-type="IMAGE" className="new-design-element">image</div>
-                <div data-element-type="CANVAS" className="new-design-element">canvas</div>
-                <button id="designModeClear" className="share">Clear</button><br/>
-              </div>
-            </td>
-            <td>
-              <div id="design-properties">
-              </div>
-            </td>
-          </tr>
-        </table>
+      <div id="design-mode-container">
+        <div id="design-elements">
+          <p>{applabMsg.designToolboxDescription()}</p>
+          {/* TODO (brent) better approach than storing this as a data on the element? */}
+          <div data-element-type="BUTTON" className="new-design-element">button</div>
+          <div data-element-type="LABEL" className="new-design-element">label</div>
+          <div data-element-type="TEXT_INPUT" className="new-design-element">input</div>
+          <div data-element-type="TEXT_AREA" className="new-design-element">text area</div>
+          <div data-element-type="CHECKBOX" className="new-design-element">checkbox</div>
+          <div data-element-type="RADIO_BUTTON" className="new-design-element">radio button</div>
+          <div data-element-type="DROPDOWN" className="new-design-element">dropdown</div>
+          <div data-element-type="IMAGE" className="new-design-element">image</div>
+          <div data-element-type="CANVAS" className="new-design-element">canvas</div>
+          <button id="designModeClear" className="share">Clear</button><br/>
+        </div>
+        <div id="design-properties">
+        </div>
       </div>
     )
   },
