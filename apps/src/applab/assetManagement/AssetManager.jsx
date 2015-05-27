fb497b05c4ab34859b90459d20e2c34bf66c595b
Josh Lory <josh.lory@code.org>
2015-05-26 18:04:11 -0700

    Fix jshint [ci skip]

--- apps/src/applab/assetManagement/AssetManager.jsx
+++ apps/src/applab/assetManagement/AssetManager.jsx
@@ -132,7 +132,7 @@ module.exports = React.createClass({
         <div style={{margin: '1em 0', textAlign: 'center'}}>
           <i className="fa fa-spinner fa-spin" style={{fontSize: '32px'}}></i>
         </div>
-      )
+      );
     } else if (this.state.assets.length === 0) {
       assetList = (
         <div>
