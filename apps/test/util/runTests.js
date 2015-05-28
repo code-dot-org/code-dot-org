7d62ba40c33b8d7ec9e508a4a082157f922c6183
Brent Van Minnen <bjvanminnen@gmail.com>
2015-05-27 18:24:49 -0700

    jquery draggable working

--- apps/test/util/runTests.js
+++ apps/test/util/runTests.js
@@ -26,7 +26,6 @@ function httpServer(port, callback) {
 
     // navigate from root
     var filepath = filepath = __dirname + '/../../..' + url;
-    console.log(filepath);
     if (!fs.existsSync(filepath)) {
       console.log('404: ' + filepath);
       res.writeHead(404);
