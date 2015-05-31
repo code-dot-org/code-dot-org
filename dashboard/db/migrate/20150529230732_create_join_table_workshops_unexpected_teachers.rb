9263f09d9e5526aa16f77afc82bd5316d1bd38ae
CaptainStack <CaptainStack@outlook.com>
2015-05-31 12:43:24 -0700

    removing old commented code (not necessary anymore).

--- dashboard/db/migrate/20150529230732_create_join_table_workshops_unexpected_teachers.rb
+++ dashboard/db/migrate/20150529230732_create_join_table_workshops_unexpected_teachers.rb
@@ -1,8 +1,6 @@
 class CreateJoinTableWorkshopsUnexpectedTeachers < ActiveRecord::Migration
   def change
     create_join_table :workshops, :unexpected_teachers do |t|
-      # t.references :workshops, index: true, null: false
-      # t.references :unexpected_teachers, index: true, null: false
       t.index [:workshop_id]
       t.index [:unexpected_teacher_id]
     end
