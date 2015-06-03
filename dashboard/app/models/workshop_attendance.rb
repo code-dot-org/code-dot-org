95380bd11a0e3fe66e710850c02779d6fab1c334
CaptainStack <CaptainStack@outlook.com>
2015-06-02 17:28:45 -0700

    Download CSV for attendance

--- dashboard/app/models/workshop_attendance.rb
+++ dashboard/app/models/workshop_attendance.rb
@@ -2,4 +2,13 @@ class WorkshopAttendance < ActiveRecord::Base
   self.table_name = 'workshop_attendance'
   belongs_to :segment
   belongs_to :teacher, class_name: 'User'
+
+  def WorkshopAttendance.csv_attributes
+    # same as in UserSerializer
+    [:teacher_id, :status, :notes]
+  end
+
+  def to_csv
+    WorkshopAttendance.csv_attributes.map{ |attr| self.send(attr) }
+  end
 end
