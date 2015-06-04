66ab10a8a7cbba5218d7195b717e797b6d52c0a1
Will Jordan <will@code.org>
2015-06-04 14:47:17 -0700

    Update Varnish - Set Varnish to 3.0.6 from official repository - Add precompiled libvmod-cookie and libvmod-header vmods from varnish-3.0-vmods repository

--- cookbooks/cdo-varnish/recipes/default.rb
+++ cookbooks/cdo-varnish/recipes/default.rb
@@ -2,7 +2,28 @@
 # Cookbook Name:: cdo-varnish
 # Recipe:: default
 #
-apt_package "varnish"
+
+# From https://www.varnish-cache.org/installation/ubuntu
+apt_package 'apt-transport-https'
+
+apt_repository 'varnish' do
+  repo_name "varnish-3.0"
+  uri 'https://repo.varnish-cache.org/ubuntu/'
+  distribution 'trusty'
+  components ['varnish-3.0']
+  key 'https://repo.varnish-cache.org/GPG-key.txt'
+end
+apt_package 'varnish' do
+  source 'http://repo.varnish-cache.org/ubuntu/'
+  version '3.0.6-1~trusty'
+  options '--force-yes'
+end
+
+apt_repository 'varnish-3.0-vmods' do
+  uri          'ppa:cmcdermottroe/varnish-3.0-vmods'
+  distribution 'trusty'
+end
+apt_package ['libvmod-cookie', 'libvmod-header']
 
 service "varnish" do
   action :nothing
