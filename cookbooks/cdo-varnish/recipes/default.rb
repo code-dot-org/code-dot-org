9f16373ad423cd5067cd37cadd14e3629d9a09fb
Will Jordan <will@code.org>
2015-06-04 15:18:16 -0700

    update cdo-varnish cookbook

--- cookbooks/cdo-varnish/recipes/default.rb
+++ cookbooks/cdo-varnish/recipes/default.rb
@@ -14,16 +14,18 @@ apt_repository 'varnish' do
   key 'https://repo.varnish-cache.org/GPG-key.txt'
 end
 apt_package 'varnish' do
-  source 'http://repo.varnish-cache.org/ubuntu/'
   version '3.0.6-1~trusty'
   options '--force-yes'
+  # Keep overwritten package-maintained config files on upgrade
+  options '-o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"'
 end
 
 apt_repository 'varnish-3.0-vmods' do
   uri          'ppa:cmcdermottroe/varnish-3.0-vmods'
   distribution 'trusty'
 end
-apt_package ['libvmod-cookie', 'libvmod-header']
+apt_package 'libvmod-cookie'
+apt_package 'libvmod-header'
 
 service "varnish" do
   action :nothing
