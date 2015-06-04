c83ba8b22ac71a829e6af60a7f3849af10191e36
Continuous Integration <dev@code.org>
2015-06-04 17:59:10 +0000

    Automatically built.
    commit e988ba189ea0d5616f58894548765eefb6ce922d
    Merge: 0da539e 613ce9e
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Jun 4 10:51:00 2015 -0700
    
        Merge pull request #2651 from code-dot-org/setScreenTooltip
    
        [Finishes #96216224] tooltips for setScreen
    
    commit 613ce9ec0de96890b8d37a2d23edce7affa1b733
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Jun 4 10:48:17 2015 -0700
    
        tooltips for setScreen

--- dashboard/public/apps-package/js/en_ploc/common_locale.js
+++ dashboard/public/apps-package/js/en_ploc/common_locale.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3be9fc7ddb4d92925f01393c1889e299fb469a38cc1d14dcf2a877ee47bf3140
-size 71892
+oid sha256:74dfd05e89be48152cb3985a8d129b2e31928a261f7e02e190ffce4f34ef1f86
+size 72066
