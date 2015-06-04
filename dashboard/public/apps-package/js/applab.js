cc55be59376f076c68b17b81099151a6946cf1a5
Continuous Integration <dev@code.org>
2015-06-04 17:44:11 +0000

    Automatically built.
    commit 50af5a228c10ae6c481bcbf42d0f7fd1e6252e1d
    Author: Josh Lory <josh.lory@code.org>
    Date:   Thu Jun 4 10:29:12 2015 -0700
    
        Fix ref: `renderDesignModeBox` -> `renderDesignWorkspace`

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e47f518e403c88ed4b4eac6a75d282419a9587933deac0e8a5a63f1a7b1c5c6c
-size 9275
+oid sha256:f40ac972ef1318793b9bb83d53264b0ef7a240a73ee0d02204188a8359bade58
+size 952818
