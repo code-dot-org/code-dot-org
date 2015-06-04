16760e8168fc385cd7dc493656207ee2254e29ce
Will Jordan <will@code.org>
2015-06-03 20:34:02 -0700

    First pass at cookie-based HTTPS redirection - use augmented rack-ssl-enforcer-based Rack middleware for HTTPS redirecting logic - add 'onURLBlocked' code to Pegasus page headers The 'https_ok' cookie is set on the first successful HTTPS load, then HTTPS redirection is enforced from then on, using the session cookie and HSTS header.

---
+++ lib/cdo/rack/https_redirect.rb
@@ -0,0 +1,33 @@
+# Rack middleware for enforcing HTTPS when on a compatible connection.
+# Compatibility is marked by the presence of an 'https_ok' cookie set on any successful HTTPS page load.
+# The cookie test a transitional measure until we switch our site to 100% HTTPS, when we will remove the cookie test and always redirect.
+require 'rack/ssl-enforcer'
+
+module Rack
+  class HTTPSRedirect < SslEnforcer
+
+    def initialize(app, options = {})
+      super(app, options.merge(hsts: true))
+    end
+
+    def call(env)
+      status, headers, body = super(env)
+      if ssl_request? && !https_ok?
+        response = Rack::Response.new body, status, headers
+        response.set_cookie('https_ok', {:value => '1', :path => '/', :expires => Time.now+24*60*60})
+        response.finish
+      else
+        [status, headers, body]
+      end
+    end
+
+  private
+    def https_ok?
+      @request.cookies['https_ok']
+    end
+
+    def enforce_ssl?
+      https_ok? && super
+    end
+  end
+end
