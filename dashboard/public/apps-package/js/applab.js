144d85f2eb57c3ab43f9167ded8f2ec1314b0c10
Continuous Integration <dev@code.org>
2015-06-05 23:54:59 +0000

    Automatically built.
    commit 02e4526ace5e8f1f6efe3d8307d0f94207b6e13e
    Merge: deb437b 063acd5
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Jun 5 16:37:05 2015 -0700
    
        Merge pull request #2681 from code-dot-org/deleteConfirm
    
        deletion confirmation experience for screen
    
    commit deb437b3a7dd13cc44917224e780b9a9b199cca8
    Merge: 49d3da7 f0c8e1c
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Fri Jun 5 16:35:04 2015 -0700
    
        Merge pull request #2678 from code-dot-org/no_packet_header_warning_in_broadcast_mode
    
        Don't warn about missing packet headers in broadcast mode.
    
    commit 063acd50c8e74941a8fea8541e4b93e6838d600c
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Fri Jun 5 16:09:05 2015 -0700
    
        code review feedback and fixes tests

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a0c787b9384aedb80ebb9f25ad3dd25d0455b8224c93a39870a811a84379612
-size 939272
+oid sha256:2aadda09b993ae7d72756f8d23b2c08f3cdd2d8352b1aa9f800912a545daa224
+size 940774
