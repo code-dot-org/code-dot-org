497880ea50488f8e7b4438d301ec854f50cd6fa3
Continuous Integration <dev@code.org>
2015-06-05 00:12:05 +0000

    Automatically built.
    commit 4af1378769192879fc093db986c00be3ab004f68
    Merge: bcf40fa 1a0232f
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Jun 4 16:59:12 2015 -0700
    
        Merge pull request #2655 from code-dot-org/moreScreenStuff
    
        [Finishes #96123512] select screen on divapplab click
    
    commit bcf40fa8a23f26aa874e7fe07332a0ada94a5d4c
    Merge: 297e5bf a8e09bc
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Jun 4 16:52:15 2015 -0700
    
        Merge pull request #2664 from code-dot-org/zorder
    
        zorder cleanup

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6760f4b1442d9b403a1c20a9c4a4352608e5f066759b08a8a07a1e46a8e45ef
-size 937188
+oid sha256:bd1b2a15cd70aa930c0efdac1f47d563ad486094ecfa88ec1230449c2e386d69
+size 937953
