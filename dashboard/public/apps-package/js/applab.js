a626313571ca14a02721ed261c959c1ed9ca8ebf
Continuous Integration <dev@code.org>
2015-06-08 18:44:55 +0000

    Automatically built.
    commit bc40aa3cc3ed085b482558e8279c5384160d3160
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Jun 8 11:35:19 2015 -0700
    
        small design mode fixes

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e8ec3d4ef0d7a342a2e368166706c8291789f366fa89731f603c75743a7338a
-size 942608
+oid sha256:104aeef7320ac28cd819ab003bd72da25200d069aa131dbbe0a6fb3b51335787
+size 942622
