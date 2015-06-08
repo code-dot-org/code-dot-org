cda4ce34652a2c7f00266650ab65b61f732983c2
Continuous Integration <dev@code.org>
2015-06-08 23:48:01 +0000

    Automatically built.
    commit 622f9ab78f10b9fdc7eba92cbdaac042ff93d800
    Merge: 052f3b1 2057e41
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Jun 8 16:37:29 2015 -0700
    
        Merge pull request #2695 from code-dot-org/serializationTiming
    
        [Finishes #96502314] dont serialize when toggling into design mode
    
    commit 2057e410b2d5e63ee96e2ba770d20b4487dba1fd
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Jun 8 16:30:53 2015 -0700
    
        dont serialize when toggling into design mode

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:886245d8f5abc6a42500f9fc14f2a06bae057ae7b8fc1473b6da726e1949c3db
-size 943345
+oid sha256:5ea0c7695d05ca8e0d7f8c9827e39b409c144d4ba9155844a1ac6f501775d525
+size 943344
