fd5d38be0c9e30c7cf10ef10fd07ee38688cd46d
Continuous Integration <dev@code.org>
2015-06-10 22:29:54 +0000

    Automatically built.
    commit c7e3a107f94ed2e5831ae3ea0cf18e1d2aa7414d
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Wed Jun 10 15:21:29 2015 -0700
    
        fix mochaTest

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71a836f521602ba2a32540255f21ede0d465ef514d1fca73cf5804f6dfed3dbf
+oid sha256:f1c636838f35dc4d8121ef2e16b40b16bbb56c1e5878af28c14585bbc95e0682
 size 944480
