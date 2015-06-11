931b216be9b06e718c6c3e2b7986815fe4941b21
Continuous Integration <dev@code.org>
2015-06-11 19:07:59 +0000

    Automatically built.
    commit ec1bca0130d859e175fa4617fdc79684d5d51d8f
    Merge: 275d9f3 dcc8ca9
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Jun 11 11:59:55 2015 -0700
    
        Merge pull request #2739 from code-dot-org/serializationTiming
    
        [Finishes #96614714] serialize before saving
    
    commit dcc8ca9c771f96ad0eafe60ba9112dc850f4d0b9
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Jun 11 11:53:03 2015 -0700
    
        still trigger run_button_pressed
    
    commit 275d9f33b037c29da64df51d115ce7c025230111
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Jun 11 18:44:46 2015 +0000
    
        Automatically built.
    
        commit 9a2113520e9e460ab405c8b46ecbeca5cb47e74c
        Merge: 7214951 2ade92a
        Author: Anthony Bau <dab1998@gmail.com>
        Date:   Thu Jun 11 11:28:34 2015 -0700
    
            Merge pull request #2741 from code-dot-org/droplet_update_20150611
    
            Update from droplet commit cf03d18ae9d31c72145c50a9a4c2ce8bcd60cf20
    
        commit 72149515a0bf31ae5db85878dc776c6740a25c14
        Merge: 55aeb10 f3b14c1
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Thu Jun 11 11:24:34 2015 -0700
    
            Merge pull request #2742 from code-dot-org/checkboxChanges
    
            bigger non-native checkbox in our property table

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8a7acf2238fddfa0f6b3b66f32cca3e8dcf154dee5d915f05ed32d3985240ff
-size 944724
+oid sha256:2ea1e635695565aaf2df8c4cf0385df8bb0c2e98ef5973bac5cfddb608afac25
+size 945115
--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f424cc271a70fceeacf96313148b2ba6d73dcb7353186ad3bb2565ab511dfc24
-size 647865
+oid sha256:346f6d9dc34576ebd1038433c741ef7b16d1e6fd1463e627b4f9394b22c0f38b
+size 647904
