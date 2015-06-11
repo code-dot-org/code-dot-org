65fd025630f452812a20bfe6f7b62e309972f55a
Continuous Integration <dev@code.org>
2015-06-11 18:44:46 +0000

    Automatically built.
    commit 9a2113520e9e460ab405c8b46ecbeca5cb47e74c
    Merge: 7214951 2ade92a
    Author: Anthony Bau <dab1998@gmail.com>
    Date:   Thu Jun 11 11:28:34 2015 -0700
    
        Merge pull request #2741 from code-dot-org/droplet_update_20150611
    
        Update from droplet commit cf03d18ae9d31c72145c50a9a4c2ce8bcd60cf20
    
    commit 72149515a0bf31ae5db85878dc776c6740a25c14
    Merge: 55aeb10 f3b14c1
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Jun 11 11:24:34 2015 -0700
    
        Merge pull request #2742 from code-dot-org/checkboxChanges
    
        bigger non-native checkbox in our property table

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bbff778d63971d8cc8f55c987f8a892d5876513b1e5b02264c658e4871aaa4b
-size 944512
+oid sha256:c8a7acf2238fddfa0f6b3b66f32cca3e8dcf154dee5d915f05ed32d3985240ff
+size 944724
