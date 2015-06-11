fba877340afcbe9e12a7d439748915ac3a4cfcd0
Continuous Integration <dev@code.org>
2015-06-11 00:19:50 +0000

    Automatically built.
    commit ea1eeb031b03251f4b25b11f8e685e11358ec23e
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Jun 10 17:10:00 2015 -0700
    
        Fix dropdown margin in design mode
    
    commit 5882874ae9006ce5a19401fcd2b073689fa80c1e
    Author: CaptainStack <CaptainStack@outlook.com>
    Date:   Wed Jun 10 17:09:15 2015 -0700
    
        Removing

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1c636838f35dc4d8121ef2e16b40b16bbb56c1e5878af28c14585bbc95e0682
-size 944480
+oid sha256:2bbff778d63971d8cc8f55c987f8a892d5876513b1e5b02264c658e4871aaa4b
+size 944512
