9fdb6730b48a5f3fc59d007736ac126185ed5562
Continuous Integration <dev@code.org>
2015-06-11 23:29:44 +0000

    Automatically built.
    commit 2b46c5dd7cd9cc9a52d27abc844eea312d759221
    Merge: 5688ddc de82572
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Thu Jun 11 16:12:03 2015 -0700
    
        Merge pull request #2747 from code-dot-org/netsim_timestamp_format
    
        Change timestamps in log browser to include milliseconds
    
    commit 5688ddcc1c8946c38693129bfa135770235770f9
    Author: Josh Lory <josh.lory@code.org>
    Date:   Thu Jun 11 16:09:32 2015 -0700
    
        HoundCI turned on haml checking without telling anyone

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:745fe47a51d938105e96a13bd1858358503a7b0929dcf2113337814f34d64fef
-size 945220
+oid sha256:bb7e9201c0b4facbe518fa3b6b1ae91ef5f9dc516f641220b16675255e10d159
+size 945288
--- dashboard/public/apps-package/js/netsim.js
+++ dashboard/public/apps-package/js/netsim.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e2fc10ef8ed3a3e9100c63a5d8c62815d997bed2aacbe368abb34db9675b481
-size 1140687
+oid sha256:60b7b98ec1e97b737f21ed8152b50382a3a2d5e4a59bb9f580548ad2401ef665
+size 1140710
