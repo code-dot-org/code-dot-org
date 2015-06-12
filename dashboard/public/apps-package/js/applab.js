cf45f02bc936fa00162a8c55a6b0a071b58b9feb
Continuous Integration <dev@code.org>
2015-06-12 20:31:13 +0000

    Automatically built.
    commit 75d1ecaf557aea4c2aee3e688f18551482ff2014
    Merge: 92bfd4c 938f51e
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Fri Jun 12 13:23:01 2015 -0700
    
        Merge pull request #2755 from code-dot-org/applab_slim_header
    
        Slim header everywhere
    
    commit 92bfd4c5d0631a716164d54018706a5e6aa1d95f
    Merge: e537fc9 72fcc73
    Author: Sarah Filman <sarah.filman@code.org>
    Date:   Fri Jun 12 13:14:17 2015 -0700
    
        Merge pull request #2764 from code-dot-org/add-cspunit2-to-list-of-scr
    
        Add cspunit2 to list of scripts with lesson plans
    
    commit e537fc924516ac88b5d92a272cdec03cd5ad5ed8
    Merge: d8988d1 b9c0e87
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Fri Jun 12 13:09:03 2015 -0700
    
        Merge pull request #2765 from code-dot-org/levelbuilder
    
        Levelbuilder into staging
    
    commit 72fcc73553ad94c80e3f44401a464d3f2c0b45fc
    Author: Sarah Filman <sarah.filman@code.org>
    Date:   Fri Jun 12 13:08:32 2015 -0700
    
        Add cspunit2 to list of scripts with lesson plans
    
    commit b9c0e87d23c5f0b7d7c7ea0b282a2dabd3d84143
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Jun 12 20:06:55 2015 +0000
    
        levelbuilder changes committed by brian

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2fbff46da9061bfe194203d4575afb4155cf92ff7dd020be2b291ace0bf5a87
-size 939769
+oid sha256:0bdf25a6421f17965b7e9bdd0a1ab3480b70dfd36228ddc7d2aaa2f4bb68e639
+size 939707
