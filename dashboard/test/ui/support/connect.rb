a096781ab77d7cd81b54a20ac881cf9b95195d44
Laurel Fan <laurel@code.org>
2015-06-12 11:37:15 -0700

    saucelabs config [WIP]

--- dashboard/test/ui/support/connect.rb
+++ dashboard/test/ui/support/connect.rb
@@ -16,6 +16,8 @@ class Object
   end
 end
 
+$browser_configs = JSON.load(open("browsers.json"))
+
 if ENV['TEST_LOCAL'] == 'true'
   # This drives a local installation of ChromeDriver running on port 9515, instead of BrowserStack.
   browser = Selenium::WebDriver.for :chrome, :url=>"http://127.0.0.1:9515"
@@ -25,24 +27,34 @@ if ENV['TEST_LOCAL'] == 'true'
     browser.manage.window.resize_to(max_width, max_height)
   end
 else
-  # BrowserStack
-  if browserstack_username.nil_or_empty? || browserstack_authkey.nil_or_empty?
-    raise "Missing BrowserStack credentials in environment."
+  if CDO.saucelabs_username.nil_or_empty?
+    raise "Please define CDO.saucelabs_username"
   end
 
-  url = "http://#{browserstack_username}:#{browserstack_authkey}@hub.browserstack.com/wd/hub"
+  if CDO.saucelabs_authkey.nil_or_empty?
+    raise "Please define CDO.saucelabs_authkey"
+  end
+
+  url = "http://#{CDO.saucelabs_username}:#{CDO.saucelabs_authkey}@ondemand.saucelabs.com:80/wd/hub"
 
   capabilities = Selenium::WebDriver::Remote::Capabilities.new
-  capabilities['os'] = ENV['BS_AUTOMATE_OS'] || 'windows'
-  capabilities['os_version'] = ENV['BS_AUTOMATE_OS_VERSION'] || '7'
-  capabilities['browser'] = ENV['SELENIUM_BROWSER'] || 'chrome'
-  capabilities['browser_version'] = ENV['SELENIUM_VERSION'] || '31'
+  browser_config = $browser_configs.detect {|b| b['name'] == ENV['BROWSER_CONFIG'] }
+
+  browser_config.each do |key, value|
+    next if key == 'device'
+    capabilities[key] = value
+  end
+
+  # capabilities['os'] = ENV['BS_AUTOMATE_OS'] || 'windows'
+  # capabilities['os_version'] = ENV['BS_AUTOMATE_OS_VERSION'] || '7'
+  # capabilities['browser'] = ENV['SELENIUM_BROWSER'] || 'chrome'
+  # capabilities['browser_version'] = ENV['SELENIUM_VERSION'] || '31'
 
   capabilities['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
   capabilities['build'] = ENV['BS_AUTOMATE_BUILD'] if ENV['BS_AUTOMATE_BUILD']
 
-  capabilities['rotatable'] = ENV['BS_ROTATABLE'] if ENV['BS_ROTATABLE']
-  capabilities['deviceOrientation'] = ENV['BS_ORIENTATION'] if ENV['BS_ORIENTATION']
+  # capabilities['rotatable'] = ENV['BS_ROTATABLE'] if ENV['BS_ROTATABLE']
+  # capabilities['deviceOrientation'] = ENV['BS_ORIENTATION'] if ENV['BS_ORIENTATION']
 
   capabilities['browserstack.tunnel'] = 'true' if ENV['TEST_TUNNEL'] == 'true'
 
@@ -50,7 +62,7 @@ else
   capabilities["realMobile"] = ENV['TEST_REALMOBILE'] == 'true' ? 'true' : 'false'
   capabilities["resolution"] = '1280x1024'
   capabilities[:javascript_enabled] = 'true'
-
+  capabilities[:name] = ENV['TEST_RUN_NAME']
 
   browser = Selenium::WebDriver.for(:remote, :url => url, :desired_capabilities => capabilities)
 
@@ -67,6 +79,9 @@ browser.manage.timeouts.implicit_wait = 25
 Before do
   @browser = browser
   @browser.manage.delete_all_cookies
+
+  sauce_session_id = @browser.send(:bridge).capabilities["webdriver.remote.sessionid"]
+  puts 'visual log on sauce labs: https://saucelabs.com/tests/' + sauce_session_id
 end
 
 at_exit do
