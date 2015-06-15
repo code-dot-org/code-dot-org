d1c4ad6bbdd8f514ba8d801a71cf0f71ccefbd99
Continuous Integration <dev@code.org>
2015-06-15 17:06:16 +0000

    Automatically built.
    commit 80d7d603d8579d971778300af41835f3a4cbb4bf
    Merge: ed62045 151ae1e
    Author: Chris Pirich <chris@code.org>
    Date:   Mon Jun 15 09:57:31 2015 -0700
    
        Merge pull request #2777 from code-dot-org/playlab_auto_handlers
    
        Playlab auto handlers
    
    commit 151ae1ec617385fcab825ffb2793c22254752ab8
    Author: cpirich <chris@code.org>
    Date:   Fri Jun 12 18:27:32 2015 -0700
    
        add findGlobalFunction, initAutoHandlers
    
    commit 8e1b1834d2d9b422853b79bdf7951e927856c1e8
    Author: cpirich <chris@code.org>
    Date:   Fri Jun 12 18:25:32 2015 -0700
    
        move playlab specific strings into studio locale file

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1eac8c9121f4e84211e99aa859714ae8c1f6a31108d57cff4479443f5a988d8
-size 655453
+oid sha256:24373fccefb63108c1a7de8c86f67db69bdb90def71f9f174f8202cb1bf0ed08
+size 655844
--- dashboard/public/apps-package/js/en_ploc/common_locale.js
+++ dashboard/public/apps-package/js/en_ploc/common_locale.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2615122077331fbe3a66de0e842d004f4dae89ef77b98919b4914ff1566b47d2
-size 28075
+oid sha256:fb3fb3db573ce09daf54bb2c693b47d3bd3a08b90efe0e7a78e4d0d6c8640969
+size 27084
