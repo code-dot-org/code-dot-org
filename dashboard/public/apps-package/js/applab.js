b77355e36fbc373b6b0c65903544abd9985e6333
Continuous Integration <dev@code.org>
2015-06-15 19:12:46 +0000

    Automatically built.
    commit c081550d5313288e2c60881405c6edf513317e02
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon Jun 15 12:04:22 2015 -0700
    
        Don't preload asset list if there's no channel

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:720ea68a10e36780107f44010daf3680bd90d45f5608c87acea2dd933b9ddbe3
-size 940091
+oid sha256:eeedf55f123cc70f644b55050fd674dacd98767faacef164cac9f348aef9d363
+size 940129
