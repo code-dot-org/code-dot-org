e149dad166bac151e7ce8658f12e1bddaccb4a84
Continuous Integration <dev@code.org>
2015-06-15 21:10:42 +0000

    Automatically built.
    commit fd9bfd974259a1bd709d7d6cd3052cf06aab9f48
    Merge: eb1ab79 679e1c7
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Jun 15 13:53:31 2015 -0700
    
        Merge pull request #2785 from code-dot-org/canvasButton
    
        [Finishes #97024836] API canvas shouldn't be relative
    
    commit eb1ab79667a8f9d93f32a15b0697b7d94864de90
    Merge: f023d35 d8adc86
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon Jun 15 13:46:20 2015 -0700
    
        Merge pull request #2773 from code-dot-org/asset-tooltip
    
        [Finishes #95930328] Allow users to chose assets in code mode from a paramter tooltip

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0305231ac665edd023126fd923392972d1d1074f963e545975765b9367c2ad3
-size 655254
+oid sha256:e0162d99ee2fa5382cabea8a7b169ff80cc5660bc7e82520b5af1724acca7787
+size 656062
