d998cd7d4cc40287581c902c41ba061f3a11fffa
Continuous Integration <dev@code.org>
2015-06-16 21:10:37 +0000

    Automatically built.
    commit 30f3dbd990b547deb89ee893db26549b980810ea
    Merge: 3f36a1e 2d99d5a
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Jun 16 13:55:05 2015 -0700
    
        Merge pull request #2807 from code-dot-org/safariFixes
    
        [Finishes #96247620] some safari fixes for design mode
    
    commit 3f36a1ef002fc006a30d1156f59d0860aa00c267
    Merge: 229478a 15b9d56
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Jun 16 13:47:46 2015 -0700
    
        Merge pull request #2808 from code-dot-org/removeClear
    
        [Finishes #96723570] get rid of clear button from design mode

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:796baac58dbe438afae858bdcc06e89d948985b9f68127b7aae14b3b70690ff1
-size 941269
+oid sha256:2e8ec09e2c27cb342ba9a3251230d9a68693a916b37296de62b55b2dd97dd77e
+size 940350
