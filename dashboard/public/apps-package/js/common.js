01ce38927d8eae377833a0ae5277898977059fc8
Continuous Integration <dev@code.org>
2015-06-17 18:37:10 +0000

    Automatically built.
    commit a1aaa23aa97068e105086ea4260b520ae646bf15
    Merge: 559d741 85c0e3a
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Jun 17 11:28:27 2015 -0700
    
        Merge pull request #2829 from code-dot-org/tooltip-cleanup
    
        Tooltip cleanup
    
    commit 85c0e3a5ff0598011ea26aadaf9daf403ea9298c
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Jun 17 11:11:31 2015 -0700
    
        Need `this` context [ci skip]
    
    commit d4a6d61ea1eb1c140616d932bf83e509273172b3
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Jun 17 11:04:22 2015 -0700
    
        Update jsdoc
    
    commit 559d741c4ddad30c355c47546659809c7421eaed
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Jun 17 17:59:37 2015 +0000
    
        Dropbox changes
    
    commit 5b92a175c9c43cd109c895b837cd4f7ac5b806a1
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Jun 17 10:53:10 2015 -0700
    
        Refactor out `appMsg || msg` localization [ci skip]

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99820bd1ee133334f2f4d9126770957d352959c27056c76fc0af70c4ca71e490
-size 656613
+oid sha256:c817bf5e247066b54426b6cb097c1e52f0df172154dbe67ec7c96e4c81f5c5cd
+size 656623
