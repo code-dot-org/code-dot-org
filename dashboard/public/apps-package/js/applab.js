faf6bb0eb391eff551d4e68d778b0ad4fa2b9a42
Continuous Integration <dev@code.org>
2015-06-18 22:27:36 +0000

    Automatically built.
    commit 791272c1b4c6f8fcb0ec5d6b9e704f792adc6c46
    Merge: d12473c 5ed8c5e
    Author: Josh Lory <josh.lory@code.org>
    Date:   Thu Jun 18 15:20:04 2015 -0700
    
        Merge pull request #2844 from code-dot-org/applab-modern-style
    
        [Finishes #95928438] Modern style for Applab
    
    commit 5ed8c5e6ad24346ed9532739b8a1d88d38f5dfe8
    Author: Josh Lory <josh.lory@code.org>
    Date:   Thu Jun 18 14:28:40 2015 -0700
    
        Fix box-sizing resize bug [ci skip]
    
    commit d12473c1a9162c119f8c104b3ec4ad85e8c4e187
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Jun 18 19:51:25 2015 +0000
    
        dropbox and gsheet changes
    
    commit 3fc31268c70c648a64c227aaa72dc9c5a7ca8777
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Jun 18 19:42:36 2015 +0000
    
        Automatically built.
    
        commit 4b3af5789ffba83785cc979b82b2607eae8b30b9
        Merge: 0d0eb01 6b3f4a3
        Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
        Date:   Thu Jun 18 12:35:11 2015 -0700
    
            Merge pull request #2847 from code-dot-org/applab_temporarily_duplicate_view_data_button
    
            Restore "View Data" button
    
        commit 6b3f4a31450f47ed2464e651832b9f05dbb72a6a
        Author: Brad Buchanan <brad@code.org>
        Date:   Thu Jun 18 12:29:03 2015 -0700
    
            Put view data button back in old location temporarily, since new location is hidden from regular users right now.
    
    commit 4b3af5789ffba83785cc979b82b2607eae8b30b9
    Merge: 0d0eb01 6b3f4a3
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Thu Jun 18 12:35:11 2015 -0700
    
        Merge pull request #2847 from code-dot-org/applab_temporarily_duplicate_view_data_button
    
        Restore "View Data" button
    
    commit 6b3f4a31450f47ed2464e651832b9f05dbb72a6a
    Author: Brad Buchanan <brad@code.org>
    Date:   Thu Jun 18 12:29:03 2015 -0700
    
        Put view data button back in old location temporarily, since new location is hidden from regular users right now.
    
    commit 0d0eb01ed6cb58f0255210a63082952be2c4e4ed
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Jun 18 19:14:31 2015 +0000
    
        Automatically built.
    
        commit a566a1dfe369015b6536d35e8c43aefc46bcb7c7
        Merge: ae05469 1aa9c54
        Author: Tanya Parker <tanyacparker@gmail.com>
        Date:   Thu Jun 18 12:03:15 2015 -0700
    
            Merge pull request #2845 from code-dot-org/i18n
    
            code.org translations 06/18
    
        commit 1aa9c54ccf5e9faf9e62842526c33577b344156c
        Author: Tanya Parker <tanyacparker@gmail.com>
        Date:   Thu Jun 18 12:02:20 2015 -0700
    
            code.org translations 06/18
    
        commit ae0546909efec235578df5bd77c766b7631a6b32
        Author: Sarah Filman <sarah.filman@code.org>
        Date:   Thu Jun 18 11:50:06 2015 -0700
    
            Fix netsim tutorial youtube link
    
    commit de4b3c56360bceed69bb378eba3375b125205f0c
    Merge: a566a1d 4864700
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Jun 18 12:04:54 2015 -0700
    
        Merge pull request #2846 from code-dot-org/levelbuilder
    
        Levelbuilder into staging
    
    commit a566a1dfe369015b6536d35e8c43aefc46bcb7c7
    Merge: ae05469 1aa9c54
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Thu Jun 18 12:03:15 2015 -0700
    
        Merge pull request #2845 from code-dot-org/i18n
    
        code.org translations 06/18
    
    commit 486470069734913b3128715507f7761f1404bb8a
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Jun 18 19:02:23 2015 +0000
    
        levelbuilder changes committed by bcjordan

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1a6b8ee7a79470cfae7bf877fb1b15e91bf28f88754af1fb3731ff285b00261
-size 947617
+oid sha256:a7ee7979ffcf6c33056ef6ddaf5f3087712270194a7add87c69e8c88be1001ea
+size 947974
