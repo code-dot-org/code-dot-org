d577264a01b9c3dd9373a4db04db7ce8563bf0c1
Continuous Integration <dev@code.org>
2015-06-19 18:34:38 +0000

    Automatically built.
    commit ffded534618fac0bc71669aadbd48c72bb7b9929
    Merge: 0ba9142 36f084a
    Author: Sarah Filman <sarah.filman@code.org>
    Date:   Fri Jun 19 11:26:50 2015 -0700
    
        Merge pull request #2859 from code-dot-org/plain-text-to-label-string-change
    
        update "plain text" string to "label" in Design Mode Toolbox
    
    commit 36f084aceeab3768cf134dc3dfe946aab4bd1713
    Author: Sarah Filman <sarah.filman@code.org>
    Date:   Fri Jun 19 11:25:58 2015 -0700
    
        update plain text string to label

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1758b92be2a7cd6adf85c6a5803608e6577512b9ebf38701f214464f5368c31d
-size 953686
+oid sha256:404dce143760a1b206d9abaaf83b3e8c15d8b4ab3a75e5776ad4bfcb75b7460d
+size 953681
