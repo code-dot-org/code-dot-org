9e86d2acbb390d100e6732a7220f0ffabe29433c
Continuous Integration <dev@code.org>
2015-06-23 22:30:20 +0000

    Automatically built.
    commit f755131a56e364bb07f20a7dddd3fa908244e66c
    Merge: fa1e2b5 3e05fb0
    Author: Anthony Bau <dab1998@gmail.com>
    Date:   Tue Jun 23 15:12:36 2015 -0700
    
        Merge pull request #2903 from code-dot-org/dropletBeginnerMode
    
        Droplet beginner mode
    
    commit fa1e2b5f97c754309e4b37d49752e0ae1710f7b8
    Merge: c39263c cc0436b
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Jun 23 15:10:12 2015 -0700
    
        Merge pull request #2913 from code-dot-org/labelFix
    
        [Finishes #97676378] fix label sizing
    
    commit cc0436bf63fc9475ddfcbf3b2b0d6940e1926d31
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Jun 23 14:54:56 2015 -0700
    
        fix label sizing
    
    commit c39263c25e42a804a03ed1db8e534295f277cb14
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Jun 23 21:52:23 2015 +0000
    
        Automatically built.
    
        commit ba9c5a30383d8402198545dca54725899415d4a6
        Merge: 21c6cd9 8d81961
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Tue Jun 23 14:44:52 2015 -0700
    
            Merge pull request #2912 from code-dot-org/chromeHack
    
            [Finishes #97227736] add back chrome rerender hack
    
        commit 8d81961dbb194cb3786121888b4c11f14199fa5e
        Author: Brent Van Minnen <bjvanminnen@gmail.com>
        Date:   Tue Jun 23 14:43:32 2015 -0700
    
            add back chrome rerender hack

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94e1afadaef8037b82815f0204ddcd6483224c7ea224ba0d380d2f0a5daed053
-size 955008
+oid sha256:2a72a59f723596953a6d492700005ca8a23a6e6f5f2faf7efc041c667f6e0820
+size 955092
