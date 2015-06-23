67327f6280513914ab757191870cde8acf509942
Brad Buchanan <brad@code.org>
2015-06-23 14:42:56 -0700

    Move package.json up to blockly-core

--- blockly-core/deploy.sh
+++ blockly-core/deploy.sh
@@ -42,7 +42,8 @@ else
   sed -i.bak -e "s/var Blockly={Blocks:{}};//g" build-output/blocks_compressed.js
   rm build-output/blocks_compressed.js.bak
   echo "Done building build-output/blockly_compressed.js, build-output/javascript_compressed.js and build-output/blocks_compressed.js"
-
-  # Run blockly-core tests with every release build
-  ./test.sh
 fi
+
+# Run blockly-core tests with every build
+./test.sh
+
