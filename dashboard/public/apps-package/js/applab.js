62853b14d6c78f01f0e39970efc63eba71c8f843
Continuous Integration <dev@code.org>
2015-06-23 21:52:23 +0000

    Automatically built.
    commit ba9c5a30383d8402198545dca54725899415d4a6
    Merge: 21c6cd9 8d81961
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Jun 23 14:44:52 2015 -0700
    
        Merge pull request #2912 from code-dot-org/chromeHack
    
        [Finishes #97227736] add back chrome rerender hack
    
    commit 8d81961dbb194cb3786121888b4c11f14199fa5e
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Jun 23 14:43:32 2015 -0700
    
        add back chrome rerender hack

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4b671f1144c899bed0e196c79df08b0befb4e8fa46f7ada26c7f3ee200f7bcc
-size 954835
+oid sha256:94e1afadaef8037b82815f0204ddcd6483224c7ea224ba0d380d2f0a5daed053
+size 955008
