96bdfa162dbe5b755d92a12dff3d49d2fb492d4c
Brad Buchanan <brad@code.org>
2015-06-23 14:50:41 -0700

    Run blockly-core npm install from rake task, not inside test script

--- blockly-core/test.sh
+++ blockly-core/test.sh
@@ -2,8 +2,8 @@
 
 # Make sure phantomjs 2+ is installed
 if [ ! -x "node_modules/phantomjs/bin/phantomjs" ]; then
-  # Should just produce an error and quit here?
-  npm install
+  echo 'PhantomJS not found.  Run `npm install` from blockly-core first.'
+  exit 1;
 fi
 
 # Use phantomjs to run the test page wrapper
