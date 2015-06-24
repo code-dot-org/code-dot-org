1206a0b0aa4ed8f95427ea567cdc8d4d6f0061ab
Continuous Integration <dev@code.org>
2015-06-24 17:47:22 +0000

    Automatically built.
    commit 62de844401b962ebf19384eadde05c76b5e170e8
    Merge: 053031c cedc06a
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Jun 24 10:27:55 2015 -0700
    
        Merge pull request #2874 from code-dot-org/projectChanges
    
        Project changes
    
    commit 053031c8861be877aa7b64cb43d3b4f49f146289
    Merge: 3118e66 95ce274
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Wed Jun 24 10:26:41 2015 -0700
    
        Merge pull request #2911 from code-dot-org/test_blockly_core_in_build
    
        Test blockly core in build
    
    commit 3118e66fe05d08e7deeb865598781178f5f93677
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Jun 24 17:21:15 2015 +0000
    
        Automatically built.
    
        commit 12fcae07872a4cabe5a0a226c0f60ae3d45ad7c3
        Merge: 41e1c03 cbf2e38
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Wed Jun 24 10:13:44 2015 -0700
    
            Merge pull request #2917 from code-dot-org/canvasWidthHeight
    
            [Finishes #97234150] use width/height attribute for canvas
    
        commit cbf2e382a53a43bbd54d44998ef8d207757195f8
        Author: Brent Van Minnen <bjvanminnen@gmail.com>
        Date:   Wed Jun 24 10:01:00 2015 -0700
    
            use width/height attribute for canvas
    
    commit 12fcae07872a4cabe5a0a226c0f60ae3d45ad7c3
    Merge: 41e1c03 cbf2e38
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Jun 24 10:13:44 2015 -0700
    
        Merge pull request #2917 from code-dot-org/canvasWidthHeight
    
        [Finishes #97234150] use width/height attribute for canvas
    
    commit cedc06a9fb77781123ad43a92d3af3a342329342
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Jun 23 09:42:33 2015 -0700
    
        add back current.level (renamed as projectUrl)
    
    commit 0c9c519dc1da1d35b03648547a059d30118c402d
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Jun 22 15:48:11 2015 -0700
    
        make isEditing a private so we don't have to worry about our this context

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fa3ae13c0056005bbbf56fb339300c81b3f2d01d09b5a5199462a8d65ca74a5
-size 955859
+oid sha256:f9549b08ad6f48fb6aecdd15a0d8e43eee812d4ff5dc2cc5bb26c388aa98e410
+size 955792
