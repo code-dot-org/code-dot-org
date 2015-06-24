e1f8d2e79235f1363981f58a6117c7a59f7f35c9
Continuous Integration <dev@code.org>
2015-06-24 17:21:15 +0000

    Automatically built.
    commit 12fcae07872a4cabe5a0a226c0f60ae3d45ad7c3
    Merge: 41e1c03 cbf2e38
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Jun 24 10:13:44 2015 -0700
    
        Merge pull request #2917 from code-dot-org/canvasWidthHeight
    
        [Finishes #97234150] use width/height attribute for canvas
    
    commit cbf2e382a53a43bbd54d44998ef8d207757195f8
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Wed Jun 24 10:01:00 2015 -0700
    
        use width/height attribute for canvas

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3c83409c70699dcdac761b53aa4e48753c194a2a76369d17e44cdc9ecc47cf5
-size 955529
+oid sha256:0fa3ae13c0056005bbbf56fb339300c81b3f2d01d09b5a5199462a8d65ca74a5
+size 955859
