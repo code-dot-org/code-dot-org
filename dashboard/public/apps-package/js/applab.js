a345c6f6fbf0c0e1fe5a2c74945b2fae6400a405
Continuous Integration <dev@code.org>
2015-06-25 00:39:15 +0000

    Automatically built.
    commit 41d409433a84479a4c24c1e8ff57b5eaaf85514a
    Merge: f5edf5f d764fd3
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Wed Jun 24 17:31:21 2015 -0700
    
        Merge pull request #2932 from code-dot-org/short-asset-name
    
        show relative filename when selecting images.
    
    commit d764fd3025e9a8595d5b9f20dae92da9db962c82
    Author: David Bailey <dsb123@gmail.com>
    Date:   Wed Jun 24 16:50:57 2015 -0700
    
        touch up
    
    commit f5edf5f1034bb0de1a76a6b5ea3686382a3c0b9f
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Jun 25 00:05:13 2015 +0000
    
        Automatically built.
    
        commit 3a3eb1d36e15e3f00e8f77b0164389136e70bb93
        Author: Brendan Reville <brendan@code.org>
        Date:   Wed Jun 24 16:57:32 2015 -0700
    
            Increase ideal blocks from 13->15 to allow for color in this level.

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9549b08ad6f48fb6aecdd15a0d8e43eee812d4ff5dc2cc5bb26c388aa98e410
-size 955792
+oid sha256:10fc7ce6e273343a9569863d80549abb5d9248a7cd275414b058ecb7e2f68ff1
+size 956595
