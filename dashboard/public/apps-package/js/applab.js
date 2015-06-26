bf78cc1191f5f329f28ed85ed2807ccd4216c4a9
Continuous Integration <dev@code.org>
2015-06-26 21:11:28 +0000

    Automatically built.
    commit 51c244f7dc91a4013b282035c08419e505f8a308
    Merge: b1b3682 32430a7
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Jun 26 14:03:23 2015 -0700
    
        Merge pull request #2961 from code-dot-org/autosaveLevelHtml
    
        [Finishes #95291102] Autosave level html
    
    commit 32430a7b68af39801bdd86edef9f8b4a6577a3bf
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Fri Jun 26 13:47:07 2015 -0700
    
        serialize levelHtml on autosave
    
    commit 666b8e90432c75c6fb9ccabce08ba9eeb53d5d10
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Fri Jun 26 13:08:13 2015 -0700
    
        move getEditorSource into projects
    
    commit b1b368217fca18b0528f848765b69f6fbfbf24ab
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Jun 26 20:20:00 2015 +0000
    
        dropbox and gsheet changes

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83d602c7e36d8115a7d8c69c59a1de39c973731e50d4e92ffb4fb66d195e86bb
-size 956666
+oid sha256:b95878ebbf5c5c48bc1f084180b17e4c5e244d381db46950b3eb3f66223b7f4f
+size 956891
