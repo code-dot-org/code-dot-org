47cf2a57bf6606eb968739dbf5023ef4952e1cb7
Continuous Integration <dev@code.org>
2015-06-27 00:58:21 +0000

    Automatically built.
    commit 8a3100c720a0e24606b6c7b452f30eb26c9b9b56
    Merge: c74c827 a32b62e
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Jun 26 17:50:02 2015 -0700
    
        Merge pull request #2964 from code-dot-org/keyboardMovesElements
    
        [Finishes #95068682] can move design mode elements using arrow keys
    
    commit c74c82780811e813810ee05cfaa23b9a0ae8a7e4
    Merge: 6a70bc0 b3fab88
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Jun 26 17:47:32 2015 -0700
    
        Merge pull request #2958 from code-dot-org/levelbuilderToolbox
    
        show proper toolbox blocks in LB

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b95878ebbf5c5c48bc1f084180b17e4c5e244d381db46950b3eb3f66223b7f4f
-size 956891
+oid sha256:f0ffdb6aecc23293088e3668f017ae4b5781b51f0d9224029e9076aa4f99fb81
+size 958177
