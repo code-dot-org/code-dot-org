28140ba0872c4aae595fd2fe3f693b64f2236291
Continuous Integration <dev@code.org>
2015-06-29 17:48:14 +0000

    Automatically built.
    commit 2ea49813a777c55295f3abd59337233036be3bba
    Merge: 2f04308 2f4bc83
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Jun 29 10:40:51 2015 -0700
    
        Merge pull request #2975 from code-dot-org/datasetIE10
    
        [Finishes #97247344] element.dataset not supported in IE 10
    
    commit 2f4bc83e83aa4a03fa087d5aebf374b5b17d675e
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Jun 29 10:36:06 2015 -0700
    
        element.dataset not supported in IE 10

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0ffdb6aecc23293088e3668f017ae4b5781b51f0d9224029e9076aa4f99fb81
-size 958177
+oid sha256:e359e121d93dab09f782164bfde74002877135c99d1617d3b4a848aefc07a3e0
+size 958278
