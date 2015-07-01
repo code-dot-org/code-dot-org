a908794d6fcc7f36b298fb9aa007edc71f3c945b
Brent Van Minnen <bjvanminnen@gmail.com>
2015-07-01 14:10:42 -0700

    some fixes for images in design mode

--- apps/src/applab/designMode.js
+++ apps/src/applab/designMode.js
@@ -196,12 +196,20 @@ designMode.onPropertyChange = function(element, name, value) {
       break;
 
     case 'picture':
+      var originalSrc = element.src;
       element.src = Applab.maybeAddAssetPathPrefix(value);
       element.setAttribute('data-canonical-image-url', value);
       element.onload = function () {
+        if (element.src === originalSrc) {
+          return;
+        }
         // naturalWidth/Height aren't populated until image has loaded.
         element.style.width = element.naturalWidth + 'px';
         element.style.height = element.naturalHeight + 'px';
+        if ($(element.parentNode).is('.ui-resizable')) {
+          element.parentNode.style.width = element.naturalWidth + 'px';
+          element.parentNode.style.height = element.naturalHeight + 'px';
+        }
         // Re-render properties
         if (currentlyEditedElement === element) {
           designMode.editElementProperties(element);
@@ -442,8 +450,15 @@ function makeDraggable (jqueryElements) {
         elm.outerWidth(wrapper.width());
         elm.outerHeight(wrapper.height());
         var element = elm[0];
-        designMode.onPropertyChange(element, 'width', element.style.width);
-        designMode.onPropertyChange(element, 'height', element.style.height);
+        // canvas uses width/height. other elements use style.width/style.height
+        var widthProperty = 'style-width';
+        var heightProperty = 'style-height';
+        if (element.hasAttribute('width') || element.hasAttribute('height')) {
+          widthProperty = 'width';
+          heightProperty = 'height';
+        }
+        designMode.onPropertyChange(element, widthProperty, element.style.width);
+        designMode.onPropertyChange(element, heightProperty, element.style.height);
       },
       grid: [GRID_SIZE, GRID_SIZE],
       containment: 'parent'
@@ -492,7 +507,9 @@ function makeDraggable (jqueryElements) {
 
     wrapper.css({
       top: elm.css('top'),
-      left: elm.css('left')
+      left: elm.css('left'),
+      width: parseInt(elm[0].style.width, 10) + parseInt(elm[0].style.margin, 10),
+      height: parseInt(elm[0].style.height, 10) + parseInt(elm[0].style.margin, 10)
     });
 
     // Chrome/Safari both have issues where they don't properly render the
