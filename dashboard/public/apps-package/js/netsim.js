9c5e42e5c1f21fa13458a51cf1a15d0f3dfbc33c
Continuous Integration <dev@code.org>
2015-07-01 21:11:05 +0000

    Automatically built.
    commit b36b5686e0db13822d83641ac20fedbe70291769
    Merge: b9a95b8 b564268
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Wed Jul 1 14:03:03 2015 -0700
    
        Merge pull request #2998 from code-dot-org/netsim_detached_tick
    
        NetSim: Desynchronize tick and render loops
    
    commit b5642680c15795aa3371bd9d11fae5ae5b123438
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Jul 1 13:23:45 2015 -0700
    
        Comment metronome progress update code
    
    commit b9a95b8127489aeceb582c0840ac72bf99bb113f
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Jul 1 20:22:06 2015 +0000
    
        Automatically built.
    
        commit eebac7299714c771159364751f83038456715ff1
        Merge: 7223d57 0717c14
        Author: davidsbailey <davidsbailey@users.noreply.github.com>
        Date:   Wed Jul 1 11:54:05 2015 -0700
    
            Merge pull request #2993 from code-dot-org/events-tab
    
            Implement events tab for most design elements.
    
    commit 8c1422374f04ccc9764b41add0ef93312de52e1e
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Jul 1 11:09:53 2015 -0700
    
        Metronome only uses the render loop
    
    commit 478c37c704cafde14e8797bb5f65c65ac155f710
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Jul 1 11:03:51 2015 -0700
    
        Move name/address box resize on viznodes to render loop
    
    commit 8d7d0434ef20a1c677ba3f86b09ea21217e0291a
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Jul 1 10:58:27 2015 -0700
    
        Run viznode tweens on render so they are smooth

--- dashboard/public/apps-package/js/netsim.js
+++ dashboard/public/apps-package/js/netsim.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4f77a0ce91160f60a907de2b3ff89e4436d607ddbfad46da5892e9132374d5a
-size 1137334
+oid sha256:89f8cacfdc18b35839abdef4fed3954c1aa659b01c99da54115d197b2b741a46
+size 1139188
