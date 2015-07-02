611d8129c48ff19cc3adb6a50bd1afff61bf0b7e
Continuous Integration <dev@code.org>
2015-07-02 18:39:52 +0000

    Automatically built.
    commit bbf81a26804169e6605ea5d5a6bd1cc10c146a65
    Merge: 9409750 5f912a2
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Jul 2 11:40:55 2015 -0700
    
        Merge pull request #3012 from code-dot-org/remixUnsaved
    
        no error if we try to remix unsaved project
    
    commit 5f912a2b32d1b2c94ae4e5157b57bf1a207b08a3
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Jul 2 11:24:35 2015 -0700
    
        no error if we try to remix unsaved project

--- dashboard/public/shared-package/js/shared.js
+++ dashboard/public/shared-package/js/shared.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48711757077bf09684f2d0b5ea837b97f50a6e6ff46f19f0eea5fb94f9ac0deb
-size 22604
+oid sha256:9fef6a2329ff8c7431fbbce353bf7ec954fb7c24ae3abf93655f8d4ba3082c39
+size 22680
