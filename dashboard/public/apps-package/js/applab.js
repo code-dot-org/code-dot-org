8438dcaaab9aee8a6c4a31968020a59a300d543e
Continuous Integration <dev@code.org>
2015-07-06 19:10:55 +0000

    Automatically built.
    commit 29d37cd014293289f7381449a744a56c41ed9d4b
    Merge: 3df4252 a42a3da
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Jul 6 12:03:12 2015 -0700
    
        Merge pull request #3019 from code-dot-org/imageResize
    
        [Finishes #98332908] setImageURL shouldn't resize
    
    commit 3df4252c8448cbc689d8d4e8cae8e5a53a308ba6
    Author: Continuous Integration <dev@code.org>
    Date:   Mon Jul 6 18:33:40 2015 +0000
    
        Automatically built.
    
        commit 370f507d74bed5739dfe7bd03bae01006daf0369
        Author: Brent Van Minnen <bjvanminnen@gmail.com>
        Date:   Mon Jul 6 11:35:10 2015 -0700
    
            Revert "Merge pull request #2992 from code-dot-org/apps-digest"
    
            This reverts commit 6ad644114ae871b3ce3ec1b20bf7b706412f56dd, reversing
            changes made to fac267eb401b6696631f83aa8f5e3fa386fb5fb3.
    
    commit 370f507d74bed5739dfe7bd03bae01006daf0369
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Jul 6 11:35:10 2015 -0700
    
        Revert "Merge pull request #2992 from code-dot-org/apps-digest"
    
        This reverts commit 6ad644114ae871b3ce3ec1b20bf7b706412f56dd, reversing
        changes made to fac267eb401b6696631f83aa8f5e3fa386fb5fb3.

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae4de01a3883a87dfa27120ce1a7810a6fe8b84ac86a9aa7f8ab6808b0e3e849
-size 986851
+oid sha256:cddb9557740038384afd3f2785692d121ff770309d69aa2a7a8bafdb899198a1
+size 987001
