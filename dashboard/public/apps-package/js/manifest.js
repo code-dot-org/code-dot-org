6ba936b74c912d7c707090ce75624031670bc9c9
Continuous Integration <dev@code.org>
2015-07-07 22:04:48 +0000

    Automatically built.
    commit 43d7f0717cbe7a483ffea9546c7db9ea071fc390
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Jul 7 14:49:13 2015 -0700
    
        no margin on turtleImage
    
    commit 739bba3f7e13cb5a6ab33aed2b51cd77851c983b
    Merge: 0719e25 0dc5755
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Tue Jul 7 14:46:57 2015 -0700
    
        Merge pull request #3044 from code-dot-org/applab-answers
    
        show student answers in applab
    
    commit 0dc5755f73a04412af7f31243d45c261ea12cd84
    Author: Laurel Fan <laurel@code.org>
    Date:   Tue Jul 7 14:47:37 2015 -0700
    
        formatting
    
    commit 0719e25843ee15d32ca49b809e6cb12e96df32bf
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Tue Jul 7 14:44:14 2015 -0700
    
        shrink turn phone images
    
    commit b05153420587a357dfa0bcea6cde731541dc2d01
    Merge: 106ceff bc3e7e9
    Author: Andre Stackhouse <CaptainStack@outlook.com>
    Date:   Tue Jul 7 14:33:23 2015 -0700
    
        Merge pull request #3049 from code-dot-org/displayh-cohorts-loading
    
        Displayh cohorts loading
    
    commit bc3e7e9e6da6de08f371bb7386400db65547aba5
    Author: CaptainStack <CaptainStack@outlook.com>
    Date:   Tue Jul 7 14:33:03 2015 -0700
    
        removed change to schema.rb

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d52e9f4b157d051890ebc7f2b441e36af0a2d4048d9bded6a5b36ba32f16fdc
+oid sha256:6f0c112819616d0261c9fd40e90a4cc1a802c675de0d9de4940c9fce13bb34b0
 size 72289
--- dashboard/public/apps-package/js/netsim-ffc040ca621e947d2f00ab7db8792c38.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:536c3b3e2ad1f80c482fa1ea2ddd06ae08a9aeceb4e2b78a3c5b3350cdae8725
-size 1140287
