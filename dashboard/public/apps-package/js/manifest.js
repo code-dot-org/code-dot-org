b0c8fdf8cef6b46c618d04516acf88567b786cb7
Continuous Integration <dev@code.org>
2015-07-13 23:42:33 +0000

    Automatically built.
    commit c7be590de87e5dc3365d211bd9a0746ba53b7636
    Merge: 8ae37db cee5b6e
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Mon Jul 13 16:34:53 2015 -0700
    
        Merge pull request #3103 from code-dot-org/pass_environment_to_apps
    
        Pass current environment to NetSim, use it to set channels API key
    
    commit cee5b6ee44b8a72f8706b18902480f59a6f4c823
    Author: Brad Buchanan <brad@code.org>
    Date:   Mon Jul 13 16:34:12 2015 -0700
    
        Corrected naming convention and doc comment.
    
    commit 1633234c67911518119b6cd289ad19b4f1948242
    Author: Brad Buchanan <brad@code.org>
    Date:   Mon Jul 13 16:24:39 2015 -0700
    
        Pass current environment to NetSim, use it to set channels API public key

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3e9d62dfb983eaa1f81208954a042cc484aafb647bc9c9fd9cb7afa3734c0d3
+oid sha256:645b92df4e6a53d0429ad7dafea912794e65422763b2bb607baccabb0aaf087c
 size 73347
