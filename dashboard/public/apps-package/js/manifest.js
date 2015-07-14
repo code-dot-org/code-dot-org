0711a0c5dfc27e6c25ca2b44acf0181607170673
Continuous Integration <dev@code.org>
2015-07-14 21:01:35 +0000

    Automatically built.
    commit e4127ba4a9b61391dfd2847b7b1b854f5a576698
    Merge: 5c775b7 5430a30
    Author: Anthony Bau <dab1998@gmail.com>
    Date:   Tue Jul 14 16:53:47 2015 -0400
    
        Merge pull request #3108 from code-dot-org/droplet-update-20150713
    
        New droplet update adding back clearUndoStack
    
    commit 5c775b7eacd8413280d6bfd56ae458c0b35f0b59
    Merge: 3b40183 c49ffb7
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Tue Jul 14 13:03:27 2015 -0700
    
        Merge pull request #3110 from code-dot-org/beta-text
    
        Add BETA text to header in applab.

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb425e0153373706e2db0b06a9486f60c1132431b279aab1aa522b3da2be41b7
+oid sha256:ae046f5933a192a2a51d53314af89797489c424ae9bb70a2d022318ab2a752a5
 size 73347
