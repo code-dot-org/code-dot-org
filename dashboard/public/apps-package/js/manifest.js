2dfd0a41e01f2da6564d1224eed8329df5fbf702
Continuous Integration <dev@code.org>
2015-07-14 00:54:35 +0000

    Automatically built.
    commit 7a7b9095acf7af4e98fac2ec052c48faf33d9e22
    Merge: a8f15f4 3c56c1d
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon Jul 13 17:46:52 2015 -0700
    
        Merge pull request #3105 from code-dot-org/assets-ensure-channel
    
        [Finishes #98998114] Ensure we have a channel ID before showing the asset manager
    
    commit 3c56c1dc44d815d7fc2cc83c95b4caf95ad9e7a3
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon Jul 13 17:33:57 2015 -0700
    
        Ensure we have a channel ID before showing the asset manager
    
        This is temporary until Pivotal #90626454 lands. [ci skip]

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:645b92df4e6a53d0429ad7dafea912794e65422763b2bb607baccabb0aaf087c
+oid sha256:c9b05708d3f1ec6b81f660f07b7313bc30cea60e17fe040de614daf7a179a5cd
 size 73347
