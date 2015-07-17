13a0087fc20bea1fcdfb71d10d2ef18fa23ea2c3
Josh Lory <josh.lory@code.org>
2015-07-17 14:07:02 -0700

    Fix redundant use of .to_s in string interpolation (HoundCI)

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -91,11 +91,11 @@ Dashboard::Application.routes.draw do
   resources :projects, path: '/projects/', only: [:index] do
     collection do
       ProjectsController::STANDALONE_PROJECTS.each do |key, _|
-        get '/' + key.to_s, to: 'projects#edit', key: key, as: "#{key.to_s}_project"
-        get '/' + key.to_s + '/:channel_id', to: 'projects#show', key: key, as: "#{key.to_s}_project_share", share: true
-        get '/' + key.to_s + '/:channel_id/edit', to: 'projects#edit', key: key, as: "#{key.to_s}_project_edit"
-        get '/' + key.to_s + '/:channel_id/view', to: 'projects#show', key: key, as: "#{key.to_s}_project_view", readonly: true
-        get '/' + key.to_s + '/:channel_id/remix', to: 'projects#remix', key: key, as: "#{key.to_s}_project_remix"
+        get '/' + key.to_s, to: 'projects#edit', key: key, as: "#{key}_project"
+        get '/' + key.to_s + '/:channel_id', to: 'projects#show', key: key, as: "#{key}_project_share", share: true
+        get '/' + key.to_s + '/:channel_id/edit', to: 'projects#edit', key: key, as: "#{key}_project_edit"
+        get '/' + key.to_s + '/:channel_id/view', to: 'projects#show', key: key, as: "#{key}_project_view", readonly: true
+        get '/' + key.to_s + '/:channel_id/remix', to: 'projects#remix', key: key, as: "#{key}_project_remix"
       end
     end
     # TODO - do i need template?
