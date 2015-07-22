d46caafcf408587e16069db3bb5399d6dd7f1fec
Josh Lory <josh.lory@code.org>
2015-07-22 11:38:02 -0700

    Hound: uncecessary .to_s

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -91,11 +91,11 @@ Dashboard::Application.routes.draw do
   resources :projects, path: '/projects/', only: [:index] do
     collection do
       ProjectsController::STANDALONE_PROJECTS.each do |key, _|
-        get "/#{key.to_s}", to: 'projects#edit', key: key.to_s, as: "#{key}_project"
-        get "/#{key.to_s}/:channel_id", to: 'projects#show', key: key.to_s, as: "#{key}_project_share", share: true
-        get "/#{key.to_s}/:channel_id/edit", to: 'projects#edit', key: key.to_s, as: "#{key}_project_edit"
-        get "/#{key.to_s}/:channel_id/view", to: 'projects#show', key: key.to_s, as: "#{key}_project_view", readonly: true
-        get "/#{key.to_s}/:channel_id/remix", to: 'projects#remix', key: key.to_s, as: "#{key}_project_remix"
+        get "/#{key}", to: 'projects#edit', key: key.to_s, as: "#{key}_project"
+        get "/#{key}/:channel_id", to: 'projects#show', key: key.to_s, as: "#{key}_project_share", share: true
+        get "/#{key}/:channel_id/edit", to: 'projects#edit', key: key.to_s, as: "#{key}_project_edit"
+        get "/#{key}/:channel_id/view", to: 'projects#show', key: key.to_s, as: "#{key}_project_view", readonly: true
+        get "/#{key}/:channel_id/remix", to: 'projects#remix', key: key.to_s, as: "#{key}_project_remix"
       end
       get '/angular', to: 'projects#angular'
     end
