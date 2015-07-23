ffb170b41e9c3fd3e7ef601d41077c489e1004a3
Continuous Integration <dev@code.org>
2015-07-23 16:43:48 +0000

    Automatically built.
    commit 4bcac22e0a6cc3eaeec2465aef8a3034ed427fb0
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Jul 23 09:45:12 2015 -0700
    
        IE9 can have /projects#channel_id urls. handle this

--- dashboard/public/shared-package/js/shared.js
+++ dashboard/public/shared-package/js/shared.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c232c69e6c446164122db6eb103b5a5ddacc5a2db0d318e4efbab08625d225ba
-size 26716
+oid sha256:473bb34340202ba3cdbce9f22548724f5464d21d84eb7eee80be43f456d993cd
+size 27363
