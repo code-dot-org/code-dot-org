319fb3fcaa95b865443baf7e8d9c266a31ff2346
Josh Lory <josh.lory@code.org>
2015-07-28 14:34:01 -0700

    Code review feedback: s3_path helper [ci skip]

--- shared/middleware/helpers/assets.rb
+++ shared/middleware/helpers/assets.rb
@@ -13,9 +13,9 @@ class AssetBucket
 
   def list(encrypted_channel_id)
     owner_id, channel_id = storage_decrypt_channel_id(encrypted_channel_id)
-    prefix = "#{CDO.assets_s3_directory}/#{owner_id}/#{channel_id}"
+    prefix = s3_path owner_id, channel_id
     @s3.list_objects(bucket:CDO.assets_s3_bucket, prefix:prefix).contents.map do |fileinfo|
-      filename = %r{#{prefix}/(.+)$}.match(fileinfo.key)[1]
+      filename = %r{#{prefix}(.+)$}.match(fileinfo.key)[1]
       mime_type = Sinatra::Base.mime_type(File.extname(filename))
       category = mime_type.split('/').first  # e.g. 'image' or 'audio'
       {filename:filename, category:category, size:fileinfo.size}
@@ -24,7 +24,7 @@ class AssetBucket
 
   def get(encrypted_channel_id, filename)
     owner_id, channel_id = storage_decrypt_channel_id(encrypted_channel_id)
-    key = "#{CDO.assets_s3_directory}/#{owner_id}/#{channel_id}/#{filename}"
+    key = s3_path owner_id, channel_id, filename
     begin
       @s3.get_object(bucket:CDO.assets_s3_bucket, key:key).body
     rescue Aws::S3::Errors::NoSuchKey
@@ -36,14 +36,14 @@ class AssetBucket
     src_owner_id, src_channel_id = storage_decrypt_channel_id(src_channel)
     dest_owner_id, dest_channel_id = storage_decrypt_channel_id(dest_channel)
 
-    src_prefix = "#{CDO.assets_s3_directory}/#{src_owner_id}/#{src_channel_id}/"
+    src_prefix = s3_path src_owner_id, src_channel_id
     @s3.list_objects(bucket:CDO.assets_s3_bucket, prefix:src_prefix).contents.map do |fileinfo|
       filename = %r{#{src_prefix}(.+)$}.match(fileinfo.key)[1]
       mime_type = Sinatra::Base.mime_type(File.extname(filename))
       category = mime_type.split('/').first  # e.g. 'image' or 'audio'
 
       src = "#{CDO.assets_s3_bucket}/#{src_prefix}#{filename}"
-      dest = "#{CDO.assets_s3_directory}/#{dest_owner_id}/#{dest_channel_id}/#{filename}"
+      dest = s3_path dest_owner_id, dest_channel_id, filename
       @s3.copy_object(bucket:CDO.assets_s3_bucket, key:dest, copy_source:src)
 
       {filename:filename, category:category, size:fileinfo.size}
@@ -53,14 +53,18 @@ class AssetBucket
   def create_or_replace(encrypted_channel_id, filename, body)
     owner_id, channel_id = storage_decrypt_channel_id(encrypted_channel_id)
 
-    key = "#{CDO.assets_s3_directory}/#{owner_id}/#{channel_id}/#{filename}"
+    key = s3_path owner_id, channel_id, filename
     @s3.put_object(bucket:CDO.assets_s3_bucket, key:key, body:body)
   end
 
   def delete(encrypted_channel_id, filename)
     owner_id, channel_id = storage_decrypt_channel_id(encrypted_channel_id)
-    key = "#{CDO.assets_s3_directory}/#{owner_id}/#{channel_id}/#{filename}"
+    key = s3_path owner_id, channel_id, filename
 
     @s3.delete_object(bucket:CDO.assets_s3_bucket, key:key)
+  end
+
+  def s3_path(owner_id, channel_id, filename = nil)
+    "#{CDO.assets_s3_directory}/#{owner_id}/#{channel_id}/#{filename}"
   end
 end
