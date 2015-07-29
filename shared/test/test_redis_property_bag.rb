9ed11513932068541676cb6f69f0af41b06ff9fc
Phil Bogle <phil@code.org>
2015-07-29 00:00:01 -0700

    Add fake redis client

--- shared/test/test_redis_property_bag.rb
+++ shared/test/test_redis_property_bag.rb
@@ -1,3 +1,7 @@
+# Unit tests for the RedisPropertyBag.
+# This uses a fake Redis service by default, but can be configured to use a
+# a real instance on localhost by setting the USE_REAL_REDIS environment variable.
+
 require 'minitest/autorun'
 require 'rack/test'
 require File.expand_path '../../../deployment', __FILE__
@@ -6,28 +10,91 @@ require File.expand_path '../../middleware/helpers/redis_property_bag', __FILE__
 class RedisPropertyBagTest < Minitest::Unit::TestCase
 
   def test_property_bags
-    redis = create_local_redis()
+    redis = create_redis()
 
     uniquifier = Random.rand()
     bag1 = RedisPropertyBag.new(redis, "bag1_#{uniquifier}")
     bag2 = RedisPropertyBag.new(redis, "bag2_#{uniquifier}}")
 
+    # Make sure the bag initially has nothing in it.
     assert_nil bag1.get("foo")
-    assert_equals {}, bag1.to_hash
-    assert_equals [], bag1.items
+    empty_hash = {}
+    assert_equal empty_hash, bag1.to_hash
+    assert_equal [], bag1.items
 
+    # Set a value and make sure we can fetch it correctly
     bag1.set("foo", "value")
     assert_equal "value", bag1.get("foo")
+    assert_equal({"foo" => "value"}, bag1.to_hash)
+    assert_equal([{:name => "foo", :value => "value" }], bag1.items)
+
+    # The second bag should still be empty though.
     assert_nil bag2.get("foo")
 
+    # Make sure value updates work correctly
+    bag1.set("added", "added value")
     bag1.set("foo", "updated_value")
-    assert_equal  "updated_value", bag1.get("foo")
+    assert_equal "updated_value", bag1.get("foo")
+    assert_equal "added value", bag1.get("added")
+    assert_equal({"foo" => "updated_value", "added" => "added value"}, bag1.to_hash)
+    assert_equal([{:name => "foo", :value => "updated_value"},
+                  {:name => "added", :value => "added value"}], bag1.items)
+
+    bag2.set("foo", "2")
+    assert_equal "2", bag2.get("foo")
+
+    assert_equal true, bag1.delete("foo")
+    assert_equal({"added" => "added value"}, bag1.to_hash)
+    assert_equal true, bag1.delete("added")
+    assert_equal(empty_hash, bag1.to_hash)
+
+    begin
+      bag1.delete("added")
+      raise "Should have thrown"
+    rescue RedisPropertyBag::NotFound
+      # Expected
+    end
 
   end
 
-  # Create a real Redis client running against a server on localhost.
-  def create_local_redis
-    Redis.new({host: "localhost"})
+  # Create a redis client.
+  # If the USE_REAL_REDIS environment variable is true, creates a real cient running
+  # aainst localhost, otherwise creates a fake client.
+  def create_redis
+    return ENV["USE_REAL_REDIS"] ? Redis.new({host: "localhost"}) : FakeRedisClient.new()
+  end
+
+end
+
+# A fake redis client implementation that uses a local hash.
+# Note that this does not implement the entire Redis client API,
+# only that portion required by the RedisPropertyBag.
+class FakeRedisClient
+
+  def initialize
+    @hash = Hash.new()
+  end
+
+  def get_hash_for_key(key)
+    @hash[key] ||= {}
+  end
+
+  def hget(key, field)
+    return get_hash_for_key(key)[field]
+  end
+
+  def hset(key, field, value)
+    get_hash_for_key(key)[field] = value
+    return value
+  end
+
+  def hgetall(key)
+    return get_hash_for_key(key).clone()
+  end
+
+  def hdel(key, field)
+    value = get_hash_for_key(key).delete(field)
+    return value ? 1 : 0
   end
 
 end
