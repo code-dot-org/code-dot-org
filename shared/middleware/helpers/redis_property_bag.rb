c8caddf6e45b71b8bef8f912b360ad76cdfab8b1
Phil Bogle <phil@code.org>
2015-07-29 00:00:01 -0700

    Add fake redis client

--- shared/middleware/helpers/redis_property_bag.rb
+++ shared/middleware/helpers/redis_property_bag.rb
@@ -21,40 +21,42 @@ class RedisPropertyBag
     @key = "_pbag_#{id}"
   end
 
-    # Returns a collection of all of the items in the property bag, representing each entry
-    # as a hash with "name" and "value" keys.
-    def items()
-      to_hash.collect {|k, v| {name: k, value: v} }
-    end
+  # Get all the fields and values in a hash.
+  #
+  # @return [Hash<String, String>]
+  def to_hash()
+    @redis.hgetall(@key)
+  end
 
-    # Deletes the property with the given name.
-    # @raise NotFound if the given key does not exist
-    def delete(name)
-      delete_count = @redis.hdel(@key, name)
-      raise NotFound, "property `#{name}` not found" unless delete_count > 0
-      true
-    end
+  # Returns a collection of all of the items in the property bag, representing each entry
+  # as a hash with "name" and "value" keys.
+  def items()
+    to_hash.collect {|k, v| {name: k, value: v} }
+  end
 
-    # Returns the property with the given name
-    # @param {String} name
-    # @return {String}
-    def get(name)
-      @redis.hget(@key, name)
-    end
+  # Deletes the property with the given name.
+  # @raise NotFound if the given key does not exist
+  def delete(name)
+    delete_count = @redis.hdel(@key, name)
+    raise NotFound, "property `#{name}` not found" unless delete_count > 0
+    true
+  end
 
-    # Sets the value of the property with the given name
-    # @param {String} name
-    # @param {String} value
-    # @return {String} the set value
-    def set(name, value, ignored=nil)
-      @redis.hset(@key, name, value)
-      value
-    end
+  # Returns the property with the given name
+  # @param {String} name
+  # @return {String}
+  def get(name)
+    @redis.hget(@key, name)
+  end
 
-    # Get all the fields and values in a hash.
-    #
-    # @return [Hash<String, String>]
-    def to_hash()
-      @redis.hgetall(@key)
-    end
+  # Sets the value of the property with the given name
+  # @param {String} name
+  # @param {String} value
+  # @return {String} the set value
+  def set(name, value, ignored=nil)
+    @redis.hset(@key, name, value)
+    value
+  end
+
+
 end
