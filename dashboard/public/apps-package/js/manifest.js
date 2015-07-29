7818d1577836e63d203c55d17045bc14d83e6971
Continuous Integration <dev@code.org>
2015-07-29 01:06:04 +0000

    Automatically built.
    commit af878faa1fc5506cbb055ad12c79ce9ce70dd334
    Merge: 2402d5c e8eca10
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Tue Jul 28 17:59:01 2015 -0700
    
        Merge pull request #3242 from code-dot-org/netsim-no-dialog-icon
    
        NetSim: No dialog icon
    
    commit 2402d5c8fbead228b0c8912516a3aaea0e0c37d0
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Jul 28 23:41:15 2015 +0000
    
        Automatically built.
    
        commit d117296fa7fed8fec82ac637500ed1104b7326ff
        Merge: 4f9a696 4a0cfa8
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Tue Jul 28 16:34:11 2015 -0700
    
            Merge pull request #3272 from code-dot-org/explodeOnFF
    
            [Finishes #91628134] explosion animation works on FF
    
        commit 4f9a69626c07f326bc7553d276dc3e24388e4aff
        Merge: 325ed75 da16381
        Author: Tanya Parker <tanyacparker@gmail.com>
        Date:   Tue Jul 28 16:27:03 2015 -0700
    
            Merge pull request #3274 from code-dot-org/cspunit1banner
    
            add banner image to cspunit1 course overview
    
        commit da163816faf714551e1d0ff92ecf91d2c902e0b7
        Author: Tanya Parker <tanyacparker@gmail.com>
        Date:   Tue Jul 28 16:25:50 2015 -0700
    
            add banner image to cspunit1 course overview
    
        commit 4a0cfa81e1cb506991067cd88b080ddbf878cb96
        Author: Brent Van Minnen <bjvanminnen@gmail.com>
        Date:   Tue Jul 28 16:23:37 2015 -0700
    
            address code review
    
    commit d117296fa7fed8fec82ac637500ed1104b7326ff
    Merge: 4f9a696 4a0cfa8
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Jul 28 16:34:11 2015 -0700
    
        Merge pull request #3272 from code-dot-org/explodeOnFF
    
        [Finishes #91628134] explosion animation works on FF
    
    commit 4f9a69626c07f326bc7553d276dc3e24388e4aff
    Merge: 325ed75 da16381
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Tue Jul 28 16:27:03 2015 -0700
    
        Merge pull request #3274 from code-dot-org/cspunit1banner
    
        add banner image to cspunit1 course overview

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6a5b5c67fb12689f53272830fa8861e504ed9cd841874ad861682ffe1b1935b
+oid sha256:ea73b1b95c89a3718ad5e46d394e3b65e34140b550ac77ca0f4ce3e42774f96c
 size 73347
--- dashboard/public/apps-package/js/netsim.min-1bec960a76228b79002442e213d378c5.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8a5d3d4e2041fab9ea6eec39a8ae11e33cd20cd4b274c61f8df7e6b95813fd0a
-size 501871
