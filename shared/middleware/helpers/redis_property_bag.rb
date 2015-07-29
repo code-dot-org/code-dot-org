de5fa9ac5a1ff6bf1155b4baaebb20933b66b34a
Phil Bogle <phil@code.org>
2015-07-29 00:17:32 -0700

    better comments

--- shared/middleware/helpers/redis_property_bag.rb
+++ shared/middleware/helpers/redis_property_bag.rb
@@ -5,14 +5,15 @@
 # bag without having to make multiple requests to Redis.
 
 require 'sinatra/base'
+require 'redis'
 
 class RedisPropertyBag
-  require 'redis'
 
   class NotFound < Sinatra::NotFound
   end
 
-  # Initializes the Netsim API
+  # Initializes a Redis property bag.
+  #
   # @param [Redis] redis_client A Redis client instance
   # @param [String] key A unique id for the property bag.
   def initialize(redis_client, id)
@@ -25,38 +26,45 @@ class RedisPropertyBag
   #
   # @return [Hash<String, String>]
   def to_hash()
-    @redis.hgetall(@key)
+    return @redis.hgetall(@key)
   end
 
   # Returns a collection of all of the items in the property bag, representing each entry
   # as a hash with "name" and "value" keys.
+  #
+  # @return Array<Hash<String, String>>
   def items()
-    to_hash.collect {|k, v| {name: k, value: v} }
+    return to_hash.collect {|k, v| {name: k, value: v}}
   end
 
   # Deletes the property with the given name.
+  #
   # @raise NotFound if the given key does not exist
   def delete(name)
     delete_count = @redis.hdel(@key, name)
-    raise NotFound, "property `#{name}` not found" unless delete_count > 0
-    true
+    if delete_count > 0
+      return true
+    else
+      raise NotFound, "property `#{name}` not found"
+    end
   end
 
-  # Returns the property with the given name
+  # Returns value of the property with the given name.
+  #
   # @param {String} name
   # @return {String}
   def get(name)
-    @redis.hget(@key, name)
+    return @redis.hget(@key, name)
   end
 
-  # Sets the value of the property with the given name
+  # Sets the value of the property with the given name.
+  #
   # @param {String} name
   # @param {String} value
   # @return {String} the set value
   def set(name, value, ignored=nil)
     @redis.hset(@key, name, value)
-    value
+    return value
   end
-
 
 end
