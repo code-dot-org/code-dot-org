936060a1bcad7c9ae1dc6cdda401e021dbda3965
Phil Bogle <phil@code.org>
2015-07-29 10:37:32 -0700

    Respond to Brad's comments. Add delete_all command and change delete to return false rather than throwing exceptions. Incorporate Ruby style suggestions from Rubymine

--- shared/middleware/helpers/redis_property_bag.rb
+++ shared/middleware/helpers/redis_property_bag.rb
@@ -1,58 +1,28 @@
-# A property bag implementation backed by Redis.
-
+# A simple property bag backed by Redis.
+#
 # Implementation notes:  The entire property bag is stored in a single
 # value as a Redis hash. This makes it efficient to fetch tne entire property
 # bag without having to make multiple requests to Redis.
 
-require 'sinatra/base'
 require 'redis'
 
 class RedisPropertyBag
 
   # A unique prefix for Redis property bag keys.
-  PROPERTY_BAG_KEY_PREFIX = "_pbag__"
-
-  class NotFound < Sinatra::NotFound
-  end
+  PROPERTY_BAG_KEY_PREFIX = '__pbag__'
 
   # Initializes a Redis property bag.
   #
   # @param [Redis] redis_client A Redis client instance
-  # @param [String] key A unique id for the property bag.
+  # @param [String] id A unique id for the property bag.
   def initialize(redis_client, id)
     @redis = redis_client
     # The redis key for storing the table.
     @key = "#{PROPERTY_BAG_KEY_PREFIX}#{id}"
   end
 
-  # Get all the fields and values in a hash.
-  #
-  # @return [Hash<String, String>]
-  def to_hash()
-    @redis.hgetall(@key)
-  end
-
-  # Returns a collection of all of the items in the property bag, representing each entry
-  # as a hash with "name" and "value" keys.
-  #
-  # @return Array<Hash<String, String>>
-  def items()
-    to_hash.collect {|k, v| {name: k, value: v}}
-  end
-
-  # Deletes the property with the given name.
-  #
-  # @raise NotFound if the given key does not exist
-  def delete(name)
-    delete_count = @redis.hdel(@key, name)
-    if delete_count > 0
-      return true
-    else
-      raise NotFound, "property `#{name}` not found"
-    end
-  end
-
-  # Returns value of the property with the given name.
+  # Returns value of the property with the given name, or nil
+  # if no such property exists.
   #
   # @param {String} name
   # @return {String}
@@ -65,9 +35,30 @@ class RedisPropertyBag
   # @param {String} name
   # @param {String} value
   # @return {String} the set value
-  def set(name, value, ignored=nil)
+  def set(name, value)
     @redis.hset(@key, name, value)
-    return value
+    value
+  end
+
+  # Deletes the property with the given name.
+  #
+  # @param {String} name
+  # @return {Boolean} true if the value was present before deletion.
+  def delete(name)
+    delete_count = @redis.hdel(@key, name)
+    delete_count > 0
+  end
+
+  # Get all the fields and values of the bag as a hash.
+  #
+  # @return [Hash<String, String>]
+  def to_hash
+    @redis.hgetall(@key)
+  end
+
+  # Delete all properties in the property bag.
+  def delete_all
+    @redis.del(@key)
   end
 
 end
