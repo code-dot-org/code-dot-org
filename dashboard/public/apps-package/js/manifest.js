8c607b43ff4f6a9f08061e05d422ada2d4ac472c
Continuous Integration <dev@code.org>
2015-07-30 19:55:02 +0000

    Automatically built.
    commit d46e0206295b2f211da1a74ccf8de4f6729b4f5e
    Merge: c6047c0 c6472a9
    Author: Brendan Reville <breville@users.noreply.github.com>
    Date:   Thu Jul 30 12:47:29 2015 -0700
    
        Merge pull request #3303 from code-dot-org/unpin-blockly-levels
    
        Unpin blockly levels
    
    commit c6472a9d2ec045f087edebc70a2ea4882c95687e
    Author: Brad Buchanan <brad@code.org>
    Date:   Thu Jul 30 12:39:44 2015 -0700
    
        Unpin artist (turtle) levels
    
    commit 5c333d97b124776bd71dff2194f76a810c33378a
    Author: Brad Buchanan <brad@code.org>
    Date:   Thu Jul 30 12:37:47 2015 -0700
    
        Unpin playlab (studio) levels

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1666e75012028a22798ab0209d0bf3bee0ffbaaaee0cad73fa6fec263749a3bd
+oid sha256:6608f8fc528ebfec80a452e30496f58b466b0c22b7a1c95f39dd86e5f211938d
 size 73347
