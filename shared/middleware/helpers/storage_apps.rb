5bf46d3c575e713535620f8ea991c618d7e03908
Josh Lory <josh.lory@code.org>
2015-07-29 17:29:59 -0700

    Don't remove extra metadata until we can verify the new column

--- shared/middleware/helpers/storage_apps.rb
+++ shared/middleware/helpers/storage_apps.rb
@@ -44,7 +44,7 @@ class StorageApps
     row = @table.where(id:id).exclude(state:'deleted').first
     raise NotFound, "channel `#{channel_id}` not found" unless row
 
-    JSON.parse(row[:value]).merge(id: channel_id, isOwner: owner == @storage_id, createdAt: row[:created_at], updatedAt: row[:updated_at])
+    JSON.parse(row[:value]).merge(id: channel_id, isOwner: owner == @storage_id)
   end
 
   def update(channel_id, value, ip_address)
@@ -59,14 +59,14 @@ class StorageApps
     update_count = @table.where(id:id).exclude(state:'deleted').update(row)
     raise NotFound, "channel `#{channel_id}` not found" if update_count == 0
 
-    JSON.parse(row[:value]).merge(id: channel_id, isOwner: owner == @storage_id, createdAt: row[:created_at], updatedAt: row[:updated_at])
+    JSON.parse(row[:value]).merge(id: channel_id, isOwner: owner == @storage_id)
   end
 
   def to_a()
     @table.where(storage_id:@storage_id).exclude(state:'deleted').map do |i|
       channel_id = storage_encrypt_channel_id(i[:storage_id], i[:id])
       begin
-        JSON.parse(i[:value]).merge(id: channel_id, isOwner: true, createdAt: i[:created_at], updatedAt: i[:updated_at])
+        JSON.parse(i[:value]).merge(id: channel_id, isOwner: true)
       rescue JSON::ParserError
         nil
       end
