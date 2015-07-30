ea911978a4ca82137babdcb695fe9d76549f285a
Phil Bogle <phil@code.org>
2015-07-30 02:43:11 -0700

    Undo null_pub_sub_api changes

--- shared/middleware/helpers/null_pub_sub_api.rb
+++ shared/middleware/helpers/null_pub_sub_api.rb
@@ -1,20 +1,13 @@
 # Base class that defines an interface for publishing messages to a Pubsub
-# system. This class does nothing other than record the published messages
-# for testing.  Real implementations should provide actual behavior for
-# publish.
-class NullPubSubApi
-  @@published_messages = []
+# system. This class does nothing, derived classes should override publish
+# with actual behavior
 
+class NullPubSubApi
   # Publishes an event to a a channel using the Pub/Sub system.
   #
   # @param [String] channel a single channel name that the event is to be published on
   # @param [String] event - the name of the event to be triggered
   # @param [Hash] data - the data to be sent with the event
   def self.publish(channel, event, data)
-    @@published_messages << [channel, event, data]
-  end
-
-  def self.published_messages
-    @@published_messages
   end
 end
