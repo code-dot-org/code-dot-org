0178ffb9baf673276a72d4e857887833a962815a
Continuous Integration <dev@code.org>
2015-07-31 21:49:57 +0000

    Automatically built.
    commit 0b3d8417b7940bf234b2aea0a951a19da70e6152
    Merge: 340e54e 941d6c9
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Fri Jul 31 14:42:50 2015 -0700
    
        Merge pull request #3324 from code-dot-org/netsim-AB-animations
    
        Calls NetSimVizWire.render with clock arguments
    
    commit 340e54e00a4e175569fee66ef2bcb2611461d6c8
    Merge: 7c31217 86cc491
    Author: philbogle <phil@code.org>
    Date:   Fri Jul 31 14:28:01 2015 -0700
    
        Merge pull request #3326 from code-dot-org/id_fix
    
        Bug fix.  Replace :id with 'id' so that sorting by id works correctly.
    
    commit 86cc491f93acc3059d7da5c5e44a200e9c4566f8
    Author: Phil Bogle <phil@code.org>
    Date:   Fri Jul 31 14:19:58 2015 -0700
    
        Bug fix.  Replace :id with 'id' so that sorting by id works correctly.

--- dashboard/public/apps-package/js/netsim-be2ce76ec845b4e6f02e3d6b37be0a76.js
+++ dashboard/public/apps-package/js/netsim-be2ce76ec845b4e6f02e3d6b37be0a76.js
@@ -2856,9 +2856,10 @@ NetSimVizWire.inherits(NetSimVizElement);
 
 /**
  * Update path data for wire.
+ * @param {RunLoop.Clock} [clock] - somtimes omitted during setup
  */
-NetSimVizWire.prototype.render = function () {
-  NetSimVizWire.superPrototype.render.call(this);
+NetSimVizWire.prototype.render = function (clock) {
+  NetSimVizWire.superPrototype.render.call(this, clock);
 
   var pathData = 'M 0 0';
   var wireCenter = { x: 0, y: 0 };
