be33c8122540caad15d95ddbddc4d322fa8322b4
Josh Lory <josh.lory@code.org>
2015-07-31 17:39:33 -0700

    This migration is a fix for devs who have already done the bad migration

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727190540) do
+ActiveRecord::Schema.define(version: 20150730233042) do
 
   create_table "activities", force: true do |t|
     t.integer  "user_id"
