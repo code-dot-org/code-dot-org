976e400b3451c63bf73e497c4f6d1a212c756f03
Josh Lory <josh.lory@code.org>
2015-07-31 17:01:50 -0700

    Modify failed migration to make it reversible
    I will run `rake db:migrate:down VERSION=20150730233039` on each machine that ran the migration.

--- dashboard/db/migrate/20150730233039_change_phase_type_in_workshops.rb
+++ dashboard/db/migrate/20150730233039_change_phase_type_in_workshops.rb
@@ -1,5 +1,9 @@
 class ChangePhaseTypeInWorkshops < ActiveRecord::Migration
-  def change
+  def up
     change_column :workshops, :phase, :integer
+  end
+
+  def down
+    change_column :workshops, :phase, :text
   end
 end
