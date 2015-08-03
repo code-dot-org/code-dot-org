62df78b78676bd7944e5f11d1d87ddc88dcc10d5
Continuous Integration <dev@code.org>
2015-08-03 17:04:48 +0000

    Automatically built.
    commit 4b89d49dc0f1677559ed078003a996f55c426e43
    Merge: 412a778 2f8f058
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Aug 3 09:57:49 2015 -0700
    
        Merge pull request #3349 from code-dot-org/studioIcon
    
        no instructions icon for math playlab levels
    
    commit 2f8f0586949658a3c07662d4ec636252829cb388
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Mon Aug 3 09:47:54 2015 -0700
    
        no instructions icon for math playlab levels

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c4d651995e81279af28be3ce7639ebce60248de7fe8f700eea8c744f0d6e6cc
+oid sha256:6d2323865c264f922f6474b83a47cf88e91e1cfe30a1ad06b1d10b6e9abfa210
 size 73347
