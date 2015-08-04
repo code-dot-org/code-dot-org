3c7eb4544d2c1912fd91d6a58999428d9766dd16
Continuous Integration <dev@code.org>
2015-08-04 16:46:47 +0000

    Automatically built.
    commit f1d414dd61e07deefd90f9c73398db3a5d7dde8c
    Merge: 4006813 891a0b6
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Tue Aug 4 09:39:20 2015 -0700
    
        Merge pull request #3367 from code-dot-org/freeplaymessage
    
        Freeplaymessage for eval
    
    commit 891a0b619f327235a00cacf7335a1fb584583228
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Tue Aug 4 09:32:32 2015 -0700
    
        fix eval freeplay message
    
    commit 51b35e6a81ebcc1c9dacfc44a252b009dda025f3
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Tue Aug 4 09:27:48 2015 -0700
    
        translate latam date

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5fba1ca4b7ac60e258940aa7420e84af0b0f2aaa5e0ae3b0d7b135a86672657
+oid sha256:7ceb1938ae4e2775aaf6ec2d41de806cfdb697ed08e1b18869da3eb39f85728b
 size 73347
--- dashboard/public/apps-package/js/studio.min-d16160f30ac5c3383630d4656e6fa3cc.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5294f36b4b5b88cfb9af854ffe0f4798b634bbe4ae62a3fb371556af177f8502
-size 161586
