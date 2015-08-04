dcf72b013ad06877bbbc06d3cac4a00c04be9035
Continuous Integration <dev@code.org>
2015-08-04 02:49:23 +0000

    Automatically built.
    commit 921ae04701b8e95d43dd2268378d7e10f3965c3b
    Merge: dfc6aa9 1150973
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Mon Aug 3 19:41:37 2015 -0700
    
        Merge pull request #3365 from code-dot-org/unique_id_for_contract_param_deletion
    
        Use unique ID rather than name for contract parameter removal [fixes #100459502]
    
    commit 1150973a659e98caf68fe4a22f0e6ca61f5bb4b7
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Mon Aug 3 19:29:26 2015 -0700
    
        Remove removed param name mention from comment [ci skip]
    
    commit d70f8a6149cf9c387b74b4ee6d04f89f067d1aa5
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Mon Aug 3 19:26:59 2015 -0700
    
        Use unique ID rather than name for contract parameter removal. [fixes #100459502]

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d36237a85150838e86266beb02fb9cb7159ec2dae11771248be8e2e9018efe8
+oid sha256:5b71e3af3edb7295b33ff92ae2688c398b28611b9e3851b968219e3f9a8ff43e
 size 73347
