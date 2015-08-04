ba7cf6ebd16c68069c955f31336770c235832efa
David Bailey <dsb123@gmail.com>
2015-08-04 10:38:54 -0700

    skip projects init and project save if channel is absent.

--- dashboard/public/pixelation/pixelation.js
+++ dashboard/public/pixelation/pixelation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:983cefa722b096cdccaf7c8a7b8b91e9f79c1236d8066ff055b4e448c1f71b60
-size 11361
+oid sha256:5ee052c9a13b0121c642ef069bd7599cece80c167df359823315cab9191192c9
+size 11352
