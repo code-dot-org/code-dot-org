824775e0252c1fcddf001251455d9bffb09df09e
Brian Jordan <bcjordan@gmail.com>
2015-08-06 12:29:22 -0700

    Add start to block disconnection test.

---
+++ blockly-core/tests/blocks_test.js
@@ -0,0 +1,36 @@
+/**
+ * Visual Blocks Editor
+ *
+ * Copyright 2011 Google Inc.
+ * http://blockly.googlecode.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+function test_block_connections() {
+  var containerDiv = document.createElement('div');
+  var blockSpace = new Blockly.BlockSpace(
+      new Blockly.BlockSpaceEditor(containerDiv)
+  );
+  var block1 = new Blockly.Block(blockSpace);
+  var block2 = new Blockly.Block(blockSpace);
+  block2.setFunctionalOutput(true);
+  assertNotNull(block1);
+  assertEquals(2, blockSpace.topBlocks_.length);
+
+  block1.appendFunctionalInput("myInputName");
+  block1.attachBlockToInputName(block2, "myInputName");
+  block2.setCanDisconnectFromParent(false);
+  assertFalse(block2.canDisconnectFromParent());
+}
