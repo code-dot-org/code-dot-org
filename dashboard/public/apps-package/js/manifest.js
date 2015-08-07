a4e0c8a1a838f20f505a3c1b6b74ab0797dca579
Continuous Integration <dev@code.org>
2015-08-07 17:49:33 +0000

    Automatically built.
    commit 31abfb0b6cc4d7c1b4fa7a6b9e03d564b72050d7
    Merge: 7fe257e 5795340
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Fri Aug 7 10:38:58 2015 -0700
    
        Merge pull request #3436 from code-dot-org/netsim-multi-delete-client
    
        NetSim: Simple use of delete_many
    
    commit 7fe257e9809fbf0a4e3553ac4bb2754ebf7f8203
    Merge: ce4280e f8ea95a
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Fri Aug 7 10:15:49 2015 -0700
    
        Merge pull request #3434 from code-dot-org/script-cache
    
        put scripts in cache after seeding in all environments
    
    commit ce4280e62f8a8e97c8b5e8ed7ec09e05257f4d8d
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Aug 7 17:09:54 2015 +0000
    
        Automatically built.
    
        commit c7a9941619504d19a8a81ef9bbb40ecc648eec03
        Merge: 2c02f36 ef75ec0
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Fri Aug 7 10:09:39 2015 -0700
    
            Merge pull request #3442 from code-dot-org/projectsIE9
    
            stop adding hash to url
    
        commit 2c02f36b3262c56efecec314bf00970475dcdee3
        Merge: e4f788c 0c0ff10
        Author: Tanya Parker <tanyacparker@gmail.com>
        Date:   Fri Aug 7 09:38:59 2015 -0700
    
            Merge pull request #3439 from code-dot-org/hoclatam
    
            fix homepage for latam, es, br, ar
    
    commit c7a9941619504d19a8a81ef9bbb40ecc648eec03
    Merge: 2c02f36 ef75ec0
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Aug 7 10:09:39 2015 -0700
    
        Merge pull request #3442 from code-dot-org/projectsIE9
    
        stop adding hash to url
    
    commit 2c02f36b3262c56efecec314bf00970475dcdee3
    Merge: e4f788c 0c0ff10
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Fri Aug 7 09:38:59 2015 -0700
    
        Merge pull request #3439 from code-dot-org/hoclatam
    
        fix homepage for latam, es, br, ar
    
    commit 0c0ff10ca40e99636ad5dac97d2e42b6745e06b9
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Fri Aug 7 09:37:30 2015 -0700
    
        fix homepage for latam, es, br, ar

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f48d51303974baee344539c160bfdc1abeff28ef09a212ca93d0f4cf50ae34f6
+oid sha256:a08167b5af739cd29459dba851881944c70c0feac56ff8ee9eabdcb1b9c828b9
 size 73347
