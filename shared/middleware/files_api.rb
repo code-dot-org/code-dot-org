d423e8711d50839904985f6e951b4a443d238674
Josh Lory <josh.lory@code.org>
2015-08-07 13:38:18 -0700

    Allow replacing of versions on /v3/sources
    This lets us update a project source in place when the user clicks the "Run" button, without creating an additional version on each change.

--- shared/middleware/files_api.rb
+++ shared/middleware/files_api.rb
@@ -74,9 +74,9 @@ class FilesApi < Sinatra::Base
   end
 
   #
-  # PUT /v3/(assets|sources)/<channel-id>/<filename>
+  # PUT /v3/(assets|sources)/<channel-id>/<filename>?version=<version-id>
   #
-  # Create or replace a file.
+  # Create or replace a file. Optionally overwrite a specific version.
   #
   put %r{/v3/(assets|sources)/([^/]+)/([^/]+)$} do |endpoint, encrypted_channel_id, filename|
     dont_cache
@@ -92,11 +92,11 @@ class FilesApi < Sinatra::Base
     # when serving assets.
     mime_type = Sinatra::Base.mime_type(file_type)
 
-    get_bucket_impl(endpoint).new.create_or_replace(encrypted_channel_id, filename, body)
+    response = get_bucket_impl(endpoint).new.create_or_replace(encrypted_channel_id, filename, body, request.GET['version'])
 
     content_type :json
     category = mime_type.split('/').first
-    {filename: filename, category: category, size: body.length}.to_json
+    {filename: filename, category: category, size: body.length, versionId: response.version_id}.to_json
   end
 
   #
