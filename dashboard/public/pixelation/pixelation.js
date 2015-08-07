9dbb25839fea4c3a39db41b8d4660882628050eb
David Bailey <dsb123@gmail.com>
2015-08-06 17:14:32 -0700

    Revert "remove duplicate button enabling/disabling logic via onBeforeSubmit callback."
    This reverts commit 42a5ab9bf4b86248598b83bf68fe26837a452317.

--- dashboard/public/pixelation/pixelation.js
+++ dashboard/public/pixelation/pixelation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7964e202af9d23c94a2176a664d70fab1873d2e5025cae44fa1a16298c322d40
-size 11681
+oid sha256:8f3948b584dcdcf913bc226025086246c9feb7684f9c98eb0f28db7cf7f9ce1c
+size 11976
