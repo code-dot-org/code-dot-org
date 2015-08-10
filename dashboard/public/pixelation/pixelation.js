78b612d1a10a9cb995ed6127797d1b0528efe9fb
David Bailey <dsb123@gmail.com>
2015-08-10 16:47:49 -0700

    add Start Over button to Pixelation levels, without confirmation dialog.

--- dashboard/public/pixelation/pixelation.js
+++ dashboard/public/pixelation/pixelation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c69216190983758d715b5bc7b7600a85d313cde52191da892830bfa79d72dec
-size 12060
+oid sha256:6e1015c238758c819dd7d533dd164dfff82773064bc1c7cbafc1e9baa16008fb
+size 12382
