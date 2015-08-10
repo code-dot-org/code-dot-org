4732d7c1599b21343053f5ae37f9556d9ed65484
Continuous Integration <dev@code.org>
2015-08-10 23:21:31 +0000

    Automatically built.
    commit 2158aa653314fddd0126c8776b638ba84c82b6bc
    Merge: 9a2617d 68198a6
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Aug 10 16:11:27 2015 -0700
    
        Merge pull request #3459 from code-dot-org/calcExamples
    
        [Finishes #100846594] show calc expressions when running equations
    
    commit 9a2617dba7981da347ade738ba24da2038e99d2f
    Author: Continuous Integration <dev@code.org>
    Date:   Mon Aug 10 22:54:21 2015 +0000
    
        Content changes from staging

--- dashboard/public/apps-package/js/calc-9d8a5e19517901203e25fecc1f33868a.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6c0641f5c194eb0c45aaa69a3dadca5bc2efabfa2983d6cf42f0b89ae4e08dd5
-size 447119
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39e1b8deb4e591d829818f47662daab8bd9bcb82222cbc02128332eafdbe1429
+oid sha256:8b526ae059dcefcf2b25834cc3f293121c411a9fc70c2d76f0221edb31e09930
 size 73347
