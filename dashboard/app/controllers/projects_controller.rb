1be526e3c7acf4f89b64213a63e7f0b4845c8dd0
Josh Lory <josh.lory@code.org>
2015-08-10 11:53:58 -0700

    Also copy sources on remix

--- dashboard/app/controllers/projects_controller.rb
+++ dashboard/app/controllers/projects_controller.rb
@@ -69,6 +69,7 @@ class ProjectsController < ApplicationController
     src_channel_id = params[:channel_id]
     new_channel_id = create_channel nil, src_channel_id
     AssetBucket.new.copy_assets src_channel_id, new_channel_id
+    SourceBucket.new.copy_assets src_channel_id, new_channel_id
     redirect_to action: 'edit', channel_id: new_channel_id
   end
 
