a69b137a3a4f2997b7eaf0650609fdee6ea89989
Continuous Integration <dev@code.org>
2015-08-11 23:28:43 +0000

    Automatically built.
    commit 583d218ffab000db20e4111a8cf39cceaeab3c49
    Merge: 1d3804d c05ad05
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Tue Aug 11 16:13:49 2015 -0700
    
        Merge pull request #3494 from code-dot-org/pan_drag_unmovable
    
        Pan drag through unmovable blocks
    
    commit 1d3804d56a9b0710978ad80c88af9f6bd91df626
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Aug 11 23:04:43 2015 +0000
    
        Automatically built.
    
        commit eb1fb76ea4af233d7ba238fe66cc879e0c7bb2e2
        Author: Josh Lory <josh.lory@code.org>
        Date:   Tue Aug 11 15:51:12 2015 -0700
    
            Fix jshint

--- dashboard/public/apps-package/js/blockly-339a2aadf4d98e9585002b9b44bbd790.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7cb803ffd25ee01c499688f4e2805729c2597b93cb519ca5b127addf50a670b1
-size 780965
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5ebe61e2c56a9b5255bd06facebf3b9caf5dac0d0cea67b986881dbb320de64
+oid sha256:b5ae0cb58466a9cec16a38a2c66410c23cbfc273ddf647448ab9e04c7f079365
 size 73347
