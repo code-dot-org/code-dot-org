dd9b0d90c02a4f3e0ee7f72a5d037fa075f26f11
Brian Jordan <bcjordan@gmail.com>
2015-08-11 15:12:29 -0700

    Don't lay out variable editor with grid and example block sizing logic. - Add some test coverage of definition section function/variable re-layout.

--- blockly-core/core/ui/contract_editor/contract_definition_section.js
+++ blockly-core/core/ui/contract_editor/contract_definition_section.js
@@ -4,11 +4,18 @@ goog.provide('Blockly.ContractDefinitionSection');
 goog.require('Blockly.BlockSpaceEditor');
 
 /**
- * Margin to leave above the function block below the flyout in pixels.
+ * Margin in pixels to leave between the function block and the flyout.
  * @type {number}
  * @const
  */
 var FUNCTION_BLOCK_VERTICAL_MARGIN = Blockly.BlockSpaceEditor.BUMP_PADDING_TOP;
+/**
+ * Margin to leave to the left of the function definition block.
+ * @type {number}
+ * @const
+ */
+var FUNCTION_BLOCK_MIN_HORIZONTAL_MARGIN =
+    Blockly.BlockSpaceEditor.BUMP_PADDING_LEFT;
 
 /**
  * Handles UI state and layout for the contract editor definition section/block
@@ -46,7 +53,7 @@ Blockly.ContractDefinitionSection.prototype.handleCollapse = function (isNowColl
 
 /**
  * Lays out the contract definition section. Places the definition block and
- * draws grid lines to match example section.
+ * (if not in variable mode) draws grid lines to match the example section.
  * Coordinates are all relative to the parent canvas.
  * @param {number} currentY - starting Y coordinate for
  * @param {number} verticalMidlineX - X offset for the column separator
@@ -57,31 +64,44 @@ Blockly.ContractDefinitionSection.prototype.handleCollapse = function (isNowColl
  */
 Blockly.ContractDefinitionSection.prototype.placeContent = function (currentY,
     verticalMidlineX, fullWidth, functionDefinitionBlock) {
+  if (!functionDefinitionBlock) {
+    return currentY;
+  }
+
   var verticalMidlineY = currentY;
-  this.horizontalDefinitionTopLine.setAttribute('transform',
-      'translate(' + 0 + ',' + verticalMidlineY + ')');
-  this.verticalDefinitionMidline.setAttribute('transform',
-      'translate(' + verticalMidlineX + ',' + verticalMidlineY + ')');
-  this.grayDefinitionBackground.setAttribute('transform',
-      'translate(' + 0 + ',' + currentY + ')');
-  this.horizontalDefinitionTopLine.setAttribute('width', fullWidth);
 
-  currentY += FUNCTION_BLOCK_VERTICAL_MARGIN;
-
-  if (functionDefinitionBlock) {
-    functionDefinitionBlock.moveTo(verticalMidlineX +
-        Blockly.BlockSvg.SEP_SPACE_X, currentY);
-    currentY += functionDefinitionBlock.getHeightWidth().height;
+  if (functionDefinitionBlock.isVariable()) {
+    this.definitionTableGroup.style.display = 'none';
+  } else {
+    this.definitionTableGroup.style.display = 'block';
+    this.horizontalDefinitionTopLine.setAttribute('transform',
+        'translate(' + 0 + ',' + verticalMidlineY + ')');
+    this.verticalDefinitionMidline.setAttribute('transform',
+        'translate(' + verticalMidlineX + ',' + verticalMidlineY + ')');
+    this.grayDefinitionBackground.setAttribute('transform',
+        'translate(' + 0 + ',' + currentY + ')');
+    this.horizontalDefinitionTopLine.setAttribute('width', fullWidth);
   }
 
   currentY += FUNCTION_BLOCK_VERTICAL_MARGIN;
 
-  this.horizontalDefinitionBottomLine.setAttribute('transform',
-      'translate(' + 0 + ',' + currentY + ')');
-  this.horizontalDefinitionBottomLine.setAttribute('width', fullWidth);
-  this.verticalDefinitionMidline.setAttribute('height', currentY - verticalMidlineY);
-  this.grayDefinitionBackground.setAttribute('height', currentY - verticalMidlineY);
-  this.grayDefinitionBackground.setAttribute('width', verticalMidlineX);
+  var xOffset = functionDefinitionBlock.isVariable() ?
+      FUNCTION_BLOCK_MIN_HORIZONTAL_MARGIN :
+      FUNCTION_BLOCK_MIN_HORIZONTAL_MARGIN + verticalMidlineX;
+  functionDefinitionBlock.moveTo(xOffset, currentY);
+
+  currentY += functionDefinitionBlock.getHeightWidth().height;
+
+  currentY += FUNCTION_BLOCK_VERTICAL_MARGIN;
+
+  if (!functionDefinitionBlock.isVariable()) {
+    this.horizontalDefinitionBottomLine.setAttribute('transform',
+        'translate(' + 0 + ',' + currentY + ')');
+    this.horizontalDefinitionBottomLine.setAttribute('width', fullWidth);
+    this.verticalDefinitionMidline.setAttribute('height', currentY - verticalMidlineY);
+    this.grayDefinitionBackground.setAttribute('height', currentY - verticalMidlineY);
+    this.grayDefinitionBackground.setAttribute('width', verticalMidlineX);
+  }
 
   return currentY;
 };
