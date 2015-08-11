d6e9fdfd83b213ba9c232b710f487ef409cb1253
Continuous Integration <dev@code.org>
2015-08-11 19:40:10 +0000

    Automatically built.
    commit 24125d431465c44720c98db96787b81df230c488
    Merge: 10a22d7 62e4621
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Tue Aug 11 12:29:00 2015 -0700
    
        Merge pull request #3438 from code-dot-org/contract_definition_relayout
    
        Simple version of definition section re-layout.
    
    commit 10a22d7953c7d0f5bb711b9324971c9d2de57ecb
    Merge: 980c418 cf963e5
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Tue Aug 11 12:21:58 2015 -0700
    
        Merge pull request #3482 from code-dot-org/encrypted-fix
    
        encrypted level bugfixes
    
    commit cf963e5c795564107e2d04539acc9df82592d9e1
    Merge: 23028e8 980c418
    Author: Laurel Fan <laurel@code.org>
    Date:   Tue Aug 11 12:12:10 2015 -0700
    
        Merge remote-tracking branch 'origin' into encrypted-fix

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1502f703f253f7d6d194ae32a95fe79afd37ba34512badc37034f1b3544cec19
+oid sha256:7a34ed7e25b4f925bf44ab184f7ccdc2cc0ee3019fd30916c91d4bf8395a90d0
 size 73347
