070c0a6c19a16da7db7b2c1732b4428a6181487a
Continuous Integration <dev@code.org>
2015-08-11 19:53:34 +0000

    Automatically built.
    commit 43e2b2a34eaad84195a7d398b368dfc4e6f13693
    Merge: ea77d22 2649b16
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Tue Aug 11 12:40:30 2015 -0700
    
        Merge pull request #3485 from code-dot-org/fix_jigsaw_ie9_10
    
        Use Blockly helper for <g> element appending
    
    commit ea77d2209d58b0af33cac77cb4fe4880454067f8
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Aug 11 19:40:10 2015 +0000
    
        Automatically built.
    
        commit 24125d431465c44720c98db96787b81df230c488
        Merge: 10a22d7 62e4621
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Tue Aug 11 12:29:00 2015 -0700
    
            Merge pull request #3438 from code-dot-org/contract_definition_relayout
    
            Simple version of definition section re-layout.
    
        commit 10a22d7953c7d0f5bb711b9324971c9d2de57ecb
        Merge: 980c418 cf963e5
        Author: Laurel <laurel.fan@gmail.com>
        Date:   Tue Aug 11 12:21:58 2015 -0700
    
            Merge pull request #3482 from code-dot-org/encrypted-fix
    
            encrypted level bugfixes
    
        commit cf963e5c795564107e2d04539acc9df82592d9e1
        Merge: 23028e8 980c418
        Author: Laurel Fan <laurel@code.org>
        Date:   Tue Aug 11 12:12:10 2015 -0700
    
            Merge remote-tracking branch 'origin' into encrypted-fix

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a34ed7e25b4f925bf44ab184f7ccdc2cc0ee3019fd30916c91d4bf8395a90d0
+oid sha256:2ae0a303bf20e9fff9b7a03b6cb103edc54e639080b37a00365175cdb72a23bb
 size 73347
