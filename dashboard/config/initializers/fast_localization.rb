405c9ba43dc868497b23900a7e30aa7376aa30ba
Will Jordan <will@code.org>
2015-08-12 22:45:54 +0000

    Load i18n translations pre-fork (faster Spring runners)

--- dashboard/config/initializers/fast_localization.rb
+++ dashboard/config/initializers/fast_localization.rb
@@ -4,3 +4,4 @@ if Rails.env.development? && (!CDO.load_locales)
   locale_paths = Dashboard::Application.paths['config/locales'].expanded.select{|x| x.include?('en')}
   Dashboard::Application.config.i18n.railties_load_path = locale_paths
 end
+I18n.t 'hello'
