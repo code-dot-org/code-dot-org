d6c348a24f8f9deac7ddd0284fd55157fa53a4f2
Continuous Integration <dev@code.org>
2015-08-13 21:55:28 +0000

    Automatically built.
    commit 79a8006ed0eab01a923af3aa522201f6141e7b6f
    Merge: 942dd66 f284fb9
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Thu Aug 13 14:44:28 2015 -0700
    
        Merge pull request #3531 from code-dot-org/evalCommas
    
        [Finishes #91645198] show commas for large numbers
    
    commit 942dd669799905ffda6f336f55936d0f6df10deb
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Aug 13 21:42:04 2015 +0000
    
        Automatically built.
    
        commit 665fb4361719132c5c56c9692a6abc78e9060776
        Merge: 8c95d3c c7d1173
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Thu Aug 13 14:31:31 2015 -0700
    
            Merge pull request #3530 from code-dot-org/example_lines_charcoal_gray
    
            [Finishes #101227818] Change contract editor grid line color to dark gray.
    
        commit c7d11731c9d7c751e710d0e2ce649c003ff99512
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Thu Aug 13 14:30:14 2015 -0700
    
             Change contract editor grid line color to dark gray.

---
+++ dashboard/public/apps-package/js/eval-fe8964923c299db51cfc9ece2a8e33bd.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7433b77247fb51df710463bdf9b497d9a23a5a2e157c7efd16844c6aa580eeba
+size 53337
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f10b8bdbbe191c6d74682e3e7a818fa816327fe5da980c70222ff07ff61d539e
+oid sha256:e173367303f8fced8e654130afd134437f03eb509cbf4969c5b4c0f39656930d
 size 73347
