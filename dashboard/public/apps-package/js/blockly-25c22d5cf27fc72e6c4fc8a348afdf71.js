dd70c2341d756b99e6640357755115b9c8a0b22c
Continuous Integration <dev@code.org>
2015-08-13 20:57:03 +0000

    Automatically built.
    commit e78dd75a6584509aeb23482e0fbbc5c5048f00e3
    Merge: b55eed7 a6fcdc7
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Aug 13 13:46:05 2015 -0700
    
        Merge pull request #3524 from code-dot-org/update_scroll_size_import
    
        [Finishes #100994038] Update scrollable size on blocks import
    
    commit a6fcdc767917c0d213753aa5f3d95c32d16e1875
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Thu Aug 13 13:38:27 2015 -0700
    
        Inline (short method with only one usage)
    
    commit b55eed73a4b05bf4b587a35ad702c920bb46fea8
    Merge: e7a9fb2 670e8ee
    Author: Andre Stackhouse <CaptainStack@outlook.com>
    Date:   Thu Aug 13 13:37:28 2015 -0700
    
        Merge pull request #3529 from code-dot-org/ops-email-content-update
    
        Email content update

--- dashboard/public/apps-package/js/blockly-25c22d5cf27fc72e6c4fc8a348afdf71.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0db30bf4753542e0a2ca7796844259209dbab052d357b4eb582ffac9849f4508
-size 782652
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a41b9b1a4492ee1f0b32728375760d0f14cb3f8a0f8d00e812e8335eed59aa8e
+oid sha256:da27dc7587047ad75fc3226173e24141c8d12cc1985bd9af5efdbabc264829f9
 size 73347
