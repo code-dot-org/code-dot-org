634a38bd3068fdb03394bcd24ebea47d28a3162c
David Bailey <dsb123@gmail.com>
2015-08-14 14:56:37 -0700

    fix text compression ui tests

--- dashboard/public/text-compression/text-compression.js
+++ dashboard/public/text-compression/text-compression.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05973fa7218d5a0386d36823cb03cef57e5e99126a7f3fe9aa3aadf0f0a3a7ac
-size 6632
+oid sha256:7294321b47ea9fd4a1bcca1c86d93d078855fc97d64cfabbf220cf356c933bac
+size 6635
