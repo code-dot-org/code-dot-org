705c690a59edcba0fd86f9c4a8b806877ed64084
Continuous Integration <dev@code.org>
2015-08-14 00:25:39 +0000

    Automatically built.
    commit c460c4f17958d30cdc18a90feb445269357d5198
    Merge: 8a30866 fc985bc
    Author: Brendan Reville <breville@users.noreply.github.com>
    Date:   Thu Aug 13 17:10:42 2015 -0700
    
        Merge pull request #3520 from code-dot-org/trashlocation
    
        Center trashcan on grey bar for IE & Firefox.
    
    commit 8a308669c70e22968bb5a680ce839b123fb861bc
    Merge: 9f516f3 26a1717
    Author: Brendan Reville <breville@users.noreply.github.com>
    Date:   Thu Aug 13 17:05:37 2015 -0700
    
        Merge pull request #3521 from code-dot-org/dialogalign
    
        Use existing left-alignment feature for dashboard-driven dialogs.

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ab5c9e8614307fe55031007c0417cd7c59b12a8c7697a52bf3b788bedcd1df6
+oid sha256:b86b92bbc134de8fd6710cf45b6456d7f01bc391d21b3d083484298a0260ed83
 size 73347
