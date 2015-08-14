60f475be027def8cd9cca477379ea8e0f0a81db8
Continuous Integration <dev@code.org>
2015-08-14 00:04:30 +0000

    Automatically built.
    commit b105f46842ec5312fc35b659d837d113281768c1
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Aug 13 16:54:29 2015 -0700
    
        dont assume blockly

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e173367303f8fced8e654130afd134437f03eb509cbf4969c5b4c0f39656930d
+oid sha256:4ab5c9e8614307fe55031007c0417cd7c59b12a8c7697a52bf3b788bedcd1df6
 size 73347
