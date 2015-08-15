dd1fcbedfca244e2540dde66221080720a1e8c5f
Continuous Integration <dev@code.org>
2015-08-15 01:00:28 +0000

    Automatically built.
    commit 61d6478ed56f90987bb72a846bdb225f940747ff
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Fri Aug 14 17:49:54 2015 -0700
    
        remove duplicate string

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4af8718ca9d9a5167939c15c2ef9bc52bcc10bc029aa7667b0e42f7c98971d95
+oid sha256:f997c6b51f28d8a6ad645790df3478ed5e3f8202e47c2486a7cc965e6fc2a1a4
 size 73347
