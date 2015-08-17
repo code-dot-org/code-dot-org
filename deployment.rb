5c7a979412064e9bb87db1730cdec369a6d3bfe5
Brad Buchanan <brad@code.org>
2015-08-17 09:44:00 -0700

    Rename 'netsim_shard_expire_time' => 'netsim_shard_expiry_seconds'

--- deployment.rb
+++ deployment.rb
@@ -82,7 +82,7 @@ def load_configuration()
     'assets_s3_directory'         => rack_env == :production ? 'assets' : "assets_#{rack_env}",
     'sources_s3_bucket'           => 'cdo-v3-sources',
     'sources_s3_directory'        => rack_env == :production ? 'sources' : "sources_#{rack_env}",
-    'netsim_shard_expire_time'    => 7200,
+    'netsim_shard_expiry_seconds' => 7200,
     'use_pusher'                  => false,
     'pusher_app_id'               => 'fake_app_id',
     'pusher_application_key'      => 'fake_application_key',
