1b67c68157aa6eefa7e7a7febbfe19366c45320a
Josh Lory <josh.lory@code.org>
2015-08-17 16:10:54 -0700

    Only bind the extra `drawGraph` in IE9 [ci skip]

--- dashboard/public/pixelation/pixelation.js
+++ dashboard/public/pixelation/pixelation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edc90a9797131d63b07760e68f4661bc0d42b6dcafe00ea219a0beedca2203b0
+oid sha256:079be3ab47519959fe550076cd30117783a5a408ba426eb53bfac179d64321b0
 size 13304
