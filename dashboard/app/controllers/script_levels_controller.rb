f3cc03a05ef6fcfee62b3eb6afca6bfd62e39754
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-08-17 09:49:23 -0700

    Adds a "Has i18n" level option
    Currently false for Applab and Netsim.
    
    Also situationally hides the language picker in small-footer when this
    option is false

--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -143,7 +143,8 @@ class ScriptLevelsController < ApplicationController
     view_options(
       full_width: true,
       no_footer: !@game.has_footer?,
-      small_footer: @game.uses_small_footer? || enable_scrolling?
+      small_footer: @game.uses_small_footer? || enable_scrolling?,
+      has_i18n: @game.has_i18n?
     )
 
     @@fallback_responses ||= {}
