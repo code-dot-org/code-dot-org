db9b2598e37941256cfa21bdb0026b5864eef5e2
Continuous Integration <dev@code.org>
2015-08-18 21:54:55 +0000

    Automatically built.
    commit ab75f0317a678e2c809a76ea96c66e91d7b8effa
    Merge: e290f7f f1bb973
    Author: Will Jordan <wjordan@users.noreply.github.com>
    Date:   Tue Aug 18 14:35:46 2015 -0700
    
        Merge pull request #3614 from code-dot-org/twilio_api_test
    
        Fix send to phone on /projects sharing pages
    
    commit e290f7f454a9f603556780a1a55233209524b116
    Merge: 9002705 1491a87
    Author: Josh Lory <josh.lory@code.org>
    Date:   Tue Aug 18 14:31:16 2015 -0700
    
        Merge pull request #3615 from code-dot-org/applab-console-log-null
    
        [Finishes #101425226] `console.log(null)` shouldn't throw an exception
    
    commit 9002705246a585aae2655f658fa2a0f84276871a
    Merge: 248fdd7 dea0fe0
    Author: Josh Lory <josh.lory@code.org>
    Date:   Tue Aug 18 14:28:34 2015 -0700
    
        Merge pull request #3618 from code-dot-org/applab-console-user-select
    
        [Finishes #101425308] Allow copy/paste from applab debug console
    
    commit 1491a87a39dd68ccfc10de8bcf19c3f7715a8a5a
    Author: Josh Lory <josh.lory@code.org>
    Date:   Tue Aug 18 14:27:18 2015 -0700
    
        Code review feedback [ci skip]
    
    commit 248fdd775b0ef3e062764f2169079a54978783e0
    Merge: c7246af 8aeaeff
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Aug 18 14:18:28 2015 -0700
    
        Merge pull request #3621 from code-dot-org/levelbuilder
    
        Levelbuilder -> staging
    
    commit 8aeaeff202898b38817637190b27671929d3249e
    Merge: e93ef3f c7246af
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Aug 18 14:17:41 2015 -0700
    
        Merge pull request #3620 from code-dot-org/staging
    
        staging -> levelbuilder
    
    commit e93ef3f213f64b65d637ff8150fdf4477e32686b
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Aug 18 21:17:06 2015 +0000
    
        levelbuilder changes (committed by brent)

--- dashboard/public/apps-package/js/applab-d0a221a1549e98cb9d1b25eca79b45b9.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:95b512b6a3c80d8ed0da18ef29449f5771bf4d404a5df3c0b9eb95eb2dbab0ef
-size 372106
--- dashboard/public/apps-package/js/blockly-3e3664b66707dd682c819b71a50d4d6f.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c5d93aeafeb242a493b5708a23be64a9f2d8f69f6affc50ecc8d75709fa93333
-size 782968
