a0fe52a5ff18af3ed99ed614e2eabb7038e84ed2
Brian Jordan <bcjordan@gmail.com>
2015-08-18 13:26:56 -0700

    [Finishes #101248440] Fix case where scrolling while examples are being re-laid-out results in errors. - Move Block.EVENTS.AFTER_DROPPED trigger after block is set to not be dragging - Refresh scrolling size after example block is dropped back into its slot

--- blockly-core/core/ui/contract_editor/example_view.js
+++ blockly-core/core/ui/contract_editor/example_view.js
@@ -136,22 +136,24 @@ Blockly.ExampleView.prototype.placeExampleAndGetNewY = function (
   var commonMargin = marginBelow / 2;
   newY += commonMargin;
 
-  var input = block.getInput(Blockly.ContractEditor.EXAMPLE_BLOCK_ACTUAL_INPUT_NAME);
-  if (input.type == Blockly.FUNCTIONAL_INPUT) {
-    var originalExtraSpace = input.extraSpace;
-    var width = 40; // TODO(bjordan): actually calculate empty input
-    var functionCallBlock = block.getInputTargetBlock(
-      Blockly.ContractEditor.EXAMPLE_BLOCK_ACTUAL_INPUT_NAME);
-    if (functionCallBlock) {
-      width = functionCallBlock.getHeightWidth().width;
+  if (!block.isCurrentlyBeingDragged()) {
+    var input = block.getInput(Blockly.ContractEditor.EXAMPLE_BLOCK_ACTUAL_INPUT_NAME);
+    if (input.type == Blockly.FUNCTIONAL_INPUT) {
+      var originalExtraSpace = input.extraSpace;
+      var width = 40; // TODO(bjordan): actually calculate empty input
+      var functionCallBlock = block.getInputTargetBlock(
+        Blockly.ContractEditor.EXAMPLE_BLOCK_ACTUAL_INPUT_NAME);
+      if (functionCallBlock) {
+        width = functionCallBlock.getHeightWidth().width;
+      }
+      input.extraSpace = exampleMaxInputWidth - width;
+      if (input.extraSpace !== originalExtraSpace) {
+        block.getSvgRenderer().render(true);
+      }
     }
-    input.extraSpace = exampleMaxInputWidth - width;
-    if (input.extraSpace !== originalExtraSpace) {
-      block.getSvgRenderer().render(true);
-    }
-  }
 
-  block.moveTo(marginLeft, newY);
+    block.moveTo(marginLeft, newY);
+  }
 
   newY += block.getHeightWidth().height;
 
