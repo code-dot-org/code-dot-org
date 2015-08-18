797cdeec5aa5876d7b4b5dee7e53559f5113abec
Continuous Integration <dev@code.org>
2015-08-18 23:59:58 +0000

    Automatically built.
    commit a855fe8c7f33e401519a655528b2a46ee57979cd
    Merge: b9d429e c7f63ac
    Author: Brendan Reville <brendan@code.org>
    Date:   Tue Aug 18 16:37:54 2015 -0700
    
        Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
    commit b9d429e55417e4d5603a168c64e23efcac34f61c
    Author: Brendan Reville <brendan@code.org>
    Date:   Tue Aug 18 16:37:36 2015 -0700
    
        Increase a couple mochaTest timeouts to make them work on my machine.

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea6c4c48466880b49474cd50fafbe94831db52e008863d2e339f0c9c5dcc87b4
+oid sha256:69773a493db644f69f7390d40aa1bbaf4b350e0aad40adbb24eac7f3bb76f486
 size 73347
--- dashboard/public/apps-package/js/studio.min-b644bc13331f25f51de7cb5857dc8806.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0c9eccded7d66ad06f277f363b2639ea314bf753ac6b38521fb046437a5a5e3b
-size 163468
