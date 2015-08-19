6c8de289ff81ee76518763e92324e9e4f4e4fdb9
CaptainStack <CaptainStack@outlook.com>
2015-08-18 18:36:36 -0700

    Defined a create_workshop helper to make tests less redundant. Included tests for workshops well out of range of the automated responses. Stylistic changes.

--- dashboard/test/mailers/ops_mailer_test.rb
+++ dashboard/test/mailers/ops_mailer_test.rb
@@ -17,107 +17,106 @@ class OpsMailerTest < ActionMailer::TestCase
 
   test "workshop_in_2_weeks_reminder" do
     # Create a workshop that starts 14 days from now. Include other segments for authenticity
-    @workshop_1 = create(:workshop, phase: 2, cohorts: [create(:cohort, id: 1,
-      teachers: [create(:teacher, email: 'abc@code.org'), create(:teacher, email: 'xyz@code.org')])],
-      facilitators: [create(:facilitator, email: 'facilitator_1@code.org')])
-    @workshop_1.segments.clear
-    @workshop_1.segments << create(:segment, workshop_id: @workshop_1.id, start: Date.today + 14.day + 9.hour, end: DateTime.now + 14.day + 10.hour)
-    @workshop_1.segments << create(:segment, workshop_id: @workshop_1.id, start: Date.today + 15.day + 9.hour, end: DateTime.now + 15.day + 10.hour)
-    @workshop_1.segments << create(:segment, workshop_id: @workshop_1.id, start: Date.today + 16.day + 9.hour, end: DateTime.now + 16.day + 10.hour)
+    @workshop_1 = create_workshop(['abc@code.org', 'xyz@code.org'], 'facilitator_1@code.org', Date.today + 14.day)
+
     # Create a workshop that starts 13 days from now but which includes a later segment 14 days from now to verify
     # that users are not incorrectly notified.
-    @workshop_2 = create(:workshop, phase: 8, cohorts: [create(:cohort, id: 2,
-      teachers: [create(:teacher, email: '123@code.org'), create(:teacher, email: '987@code.org')])],
-      facilitators: [create(:facilitator, email: 'facilitator_2@code.org')])
-    @workshop_2.segments << create(:segment, workshop_id: @workshop_2.id, start: DateTime.now + 13.day + 9.hour, end: DateTime.now + 13.day + 10.hour)
-    @workshop_2.segments << create(:segment, workshop_id: @workshop_2.id, start: DateTime.now + 14.day + 9.hour, end: DateTime.now + 14.day + 10.hour)
-    @workshop_2.segments << create(:segment, workshop_id: @workshop_2.id, start: DateTime.now + 15.day + 9.hour, end: DateTime.now + 15.day + 10.hour)
+    @workshop_2 = create_workshop(['123@code.org', '987@code.org'], 'facilitator_2@code.org', Date.today + 13.day)
+
+    # Create a workshop that starts entirely after 14 days from now to verify that users are not incorrectly notified.
+    @workshop_3 = create_workshop(['xxx@code.org', 'yyy@code.org'], 'facilitator_3@code.org', Date.today + 30.day)
 
     ActionMailer::Base.deliveries.clear
     Workshop.send_automated_emails
 
-    recipients = Set.new
+    correct_recipients = Set.new
+    incorrect_recipients = Set.new(%w{
+      123@code.org 987@code.org facilitator_2@code.org xxx@code.org yyy@code.org facilitator_3@code.org
+    })
 
     ActionMailer::Base.deliveries.each do |mail|
-      recipients << mail.to[0]
+      correct_recipients << mail.to[0]
     end
 
-    assert_equal Set.new(['abc@code.org', 'xyz@code.org', 'facilitator_1@code.org']), recipients
+    assert_equal Set.new(%w{abc@code.org xyz@code.org facilitator_1@code.org}), correct_recipients
 
-    ['123@code.org', '987@code.org', 'facilitator_2@code.org'].each do |email|
-      assert_equal false, recipients.include?(email)
+    incorrect_recipients.each do |email|
+      assert_equal false, correct_recipients.include?(email)
     end
     assert_equal [@workshop_1], Workshop.workshops_in_2_weeks
   end
 
   test "workshop_in_3_days_reminder" do
     # Create a workshop that starts 3 days from now. Include other segments for authenticity
-    @workshop_1 = create(:workshop, phase: 2, cohorts: [create(:cohort, id: 1,
-      teachers: [create(:teacher, email: 'abc@code.org'), create(:teacher, email: 'xyz@code.org')])],
-      facilitators: [create(:facilitator, email: 'facilitator_1@code.org')])
-    @workshop_1.segments.clear
-    @workshop_1.segments << create(:segment, workshop_id: @workshop_1.id, start: Date.today + 3.day + 9.hour, end: Date.today + 3.day + 10.hour)
-    @workshop_1.segments << create(:segment, workshop_id: @workshop_1.id, start: Date.today + 4.day + 9.hour, end: Date.today + 4.day + 10.hour)
-    @workshop_1.segments << create(:segment, workshop_id: @workshop_1.id, start: Date.today + 5.day + 9.hour, end: Date.today + 5.day + 10.hour)
+    @workshop_1 = create_workshop(['abc@code.org', 'xyz@code.org'], 'facilitator_1@code.org', Date.today + 14.day)
+
     # Create a workshop that starts 2 days from now but which includes a later segment 3 days from now to verify
     # that users are not incorrectly notified.
-    @workshop_2 = create(:workshop, phase: 8, cohorts: [create(:cohort, id: 2,
-      teachers: [create(:teacher, email: '123@code.org'), create(:teacher, email: '987@code.org')])],
-      facilitators: [create(:facilitator, email: 'facilitator_2@code.org')])
-    @workshop_2.segments << create(:segment, workshop_id: @workshop_2.id, start: Date.today + 2.day + 9.hour, end: Date.today + 2.day + 10.hour)
-    @workshop_2.segments << create(:segment, workshop_id: @workshop_2.id, start: Date.today + 3.day + 9.hour, end: Date.today + 3.day + 10.hour)
-    @workshop_2.segments << create(:segment, workshop_id: @workshop_2.id, start: Date.today + 4.day + 9.hour, end: Date.today + 4.day + 10.hour)
+    @workshop_2 = create_workshop(['123@code.org', '987@code.org'], 'facilitator_2@code.org', Date.today + 13.day)
+
+    # Create a workshop that ends completely before 3 days from now to verify that users are not incorrectly notified.
+    @workshop_3 = create_workshop(['xxx@code.org', 'yyy@code.org'], 'facilitator_3@code.org', Date.today - 30.day)
 
     ActionMailer::Base.deliveries.clear
     Workshop.send_automated_emails
 
-    recipients = Set.new
+    correct_recipients = Set.new
+    incorrect_recipients = Set.new(%w{
+      123@code.org 987@code.org facilitator_2@code.org xxx@code.org yyy@code.org facilitator_3@code.org
+    })
 
     ActionMailer::Base.deliveries.each do |mail|
-      recipients << mail.to[0]
+      correct_recipients << mail.to[0]
     end
 
-    assert_equal Set.new(['abc@code.org', 'xyz@code.org', 'facilitator_1@code.org']), recipients
+    assert_equal Set.new(%w{abc@code.org xyz@code.org facilitator_1@code.org}), correct_recipients
 
-    ['123@code.org', '987@code.org', 'facilitator_2@code.org'].each do |email|
-      assert_equal false, recipients.include?(email)
+    incorrect_recipients.each do |email|
+      assert_equal false, correct_recipients.include?(email)
     end
     assert_equal [@workshop_1], Workshop.workshops_in_3_days
   end
 
   test "exit_survey_information" do
     # Create a workshop that ends today. Include other segments for authenticity
-    @workshop_1 = create(:workshop, phase: 2, cohorts: [create(:cohort, id: 1,
-      teachers: [create(:teacher, email: 'abc@code.org'), create(:teacher, email: 'xyz@code.org')])],
-      facilitators: [create(:facilitator, email: 'facilitator_1@code.org')])
-    @workshop_1.segments.clear
-    @workshop_1.segments << create(:segment, workshop_id: @workshop_1.id, start: Date.today - 2.day + 9.hour, end: DateTime.now - 2.day + 10.hour)
-    @workshop_1.segments << create(:segment, workshop_id: @workshop_1.id, start: Date.today - 1.day + 9.hour, end: DateTime.now - 1.day + 10.hour)
-    @workshop_1.segments << create(:segment, workshop_id: @workshop_1.id, start: Date.today + 9.hour, end: DateTime.now + 10.hour)
-    # Create a workshop that ends tomorrow but which includes a segment that ends today to verify
-    # that users are not incorrectly notified.
-    @workshop_2 = create(:workshop, phase: 8, cohorts: [create(:cohort, id: 2,
-      teachers: [create(:teacher, email: '123@code.org'), create(:teacher, email: '987@code.org')])],
-      facilitators: [create(:facilitator, email: 'facilitator_2@code.org')])
-    @workshop_2.segments << create(:segment, workshop_id: @workshop_2.id, start: Date.today + - 1.day + 9.hour, end: DateTime.now - 1.day + 10.hour)
-    @workshop_2.segments << create(:segment, workshop_id: @workshop_2.id, start: Date.today + 9.hour, end: DateTime.now + 10.hour)
-    @workshop_2.segments << create(:segment, workshop_id: @workshop_2.id, start: Date.today + 1.day + 9.hour, end: DateTime.now + 1.day + 10.hour)
+    @workshop_1 = create_workshop(['abc@code.org', 'xyz@code.org'], 'facilitator_1@code.org', Date.today - 2.day)
 
+    # Create a workshop that ends tomorrow but which includes a segment today to verify that users are not incorrectly notified.
+    @workshop_2 = create_workshop(['123@code.org', '987@code.org'], 'facilitator_2@code.org', Date.today - 1.day)
+
+    # Create a workshop that starts well after today to verify that users are not incorrectly notified.
+    @workshop_3 = create_workshop(['xxx@code.org', 'yyy@code.org'], 'facilitator_3@code.org', Date.today + 30.day)
 
     ActionMailer::Base.deliveries.clear
     Workshop.send_automated_emails
 
-    recipients = Set.new
+    correct_recipients = Set.new
+    incorrect_recipients = Set.new(%w{
+      123@code.org 987@code.org facilitator_2@code.org xxx@code.org yyy@code.org facilitator_3@code.org
+    })
 
     ActionMailer::Base.deliveries.each do |mail|
-      recipients << mail.to[0]
+      correct_recipients << mail.to[0]
     end
 
-    assert_equal Set.new(['abc@code.org', 'xyz@code.org', 'facilitator_1@code.org']), recipients
+    assert_equal Set.new(%w{abc@code.org xyz@code.org facilitator_1@code.org}), correct_recipients
 
-    ['123@code.org', '987@code.org', 'facilitator_2@code.org'].each do |email|
-      assert_equal false, recipients.include?(email)
+    incorrect_recipients.each do |email|
+      assert_equal false, correct_recipients.include?(email)
     end
     assert_equal [@workshop_1], Workshop.workshops_ending_today
+  end
+
+  # Only supports two teachers as an array and one facilitator as a string.
+  # Creates 3 segments starting on the startDate from 9am to 10am, and then at the same time on the following two days
+  def create_workshop(teachers, facilitator, start_date)
+    @workshop = create(:workshop, phase: 2, cohorts: [create(:cohort,
+      teachers: [create(:teacher, email: teachers[0]), create(:teacher, email: teachers[1])])],
+      facilitators: [create(:facilitator, email: facilitator)])
+    @workshop.segments.clear
+    @workshop.segments << create(:segment, workshop_id: @workshop.id, start: start_date + 9.hour, end: DateTime.now + 10.hour)
+    @workshop.segments << create(:segment, workshop_id: @workshop.id, start: start_date + 1.day + 9.hour, end: DateTime.now + 1.day + 10.hour)
+    @workshop.segments << create(:segment, workshop_id: @workshop.id, start: start_date + 2.day + 9.hour, end: DateTime.now + 2.day + 10.hour)
+    @workshop
   end
 end
