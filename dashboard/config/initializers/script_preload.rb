f67324f1305a44b1870b1b3960e531f9f09453b3
Will Jordan <will@code.org>
2015-08-18 18:54:11 -0700

    script_preload: remove Spring check [fixes issue with #3568 where Spring was defined by 'bundle exec' but not being used by command]

--- dashboard/config/initializers/script_preload.rb
+++ dashboard/config/initializers/script_preload.rb
@@ -2,7 +2,7 @@
 # This speeds up load time of new Unicorn child worker processes
 # and Spring application preloader (Rails console, unit tests).
 
-# Skip if this is running a Rake task outside of Spring (e.g. rake db:setup)
-unless File.basename($0) == 'rake' && !defined?(Spring)
+# Skip if this is running a Rake task (e.g. rake db:setup)
+unless File.basename($0) == 'rake'
   Script.script_cache
 end
