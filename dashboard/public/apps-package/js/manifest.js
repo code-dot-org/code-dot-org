e5af9f27d4af69471542c48be5a4c31e2fc5163f
Continuous Integration <dev@code.org>
2015-08-19 17:41:08 +0000

    Automatically built.
    commit e98c06d168ab931ae824c914c529c3c8f378a6e9
    Merge: 7d25c93 5bb2402
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Aug 19 10:30:48 2015 -0700
    
        Merge pull request #3639 from code-dot-org/fixQuestionMarks
    
        [Finishes #101467546] detect side-notched blocks as extra top blocks
    
    commit 7d25c93b8fff43631c34079fefe2fcc5fc9c2145
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Aug 19 17:25:30 2015 +0000
    
        Automatically built.
    
        commit 6af1964b0770202050ef53d9f5ae83366341921d
        Merge: ef8cc5b e08958e
        Author: Brian Jordan <bcjordan@gmail.com>
        Date:   Wed Aug 19 10:14:26 2015 -0700
    
            Merge pull request #3629 from code-dot-org/no_pan_editable
    
            Don't trigger pan-drag when clicking in to editable-but-unmovable blocks (e.g. /s/course4/stage/10/puzzle/3)
    
        commit ef8cc5b74a9eaef5af06fd448c2f157d5e83b938
        Merge: 0947e25 df09cc5
        Author: Bjvanminnen <Bjvanminnen@gmail.com>
        Date:   Wed Aug 19 09:58:28 2015 -0700
    
            Merge pull request #3638 from code-dot-org/digestSharing
    
            Re-enable digest on sharing.js, with precompilation configured
    
        commit df09cc5fa694fcf87c26d1f74723fcd4acddf68c
        Author: Brent Van Minnen <bjvanminnen@gmail.com>
        Date:   Wed Aug 19 09:52:19 2015 -0700
    
            precompile sharing.js

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87c8b297ed75046a9d1bee5873b6e2c37747d5662e60cd4f460e546eccec948a
+oid sha256:e22a840fc8c3f13b37f513890b89430bd344fc7c08162156e0a944aa00924fe7
 size 73347
