0ca6c3fe6cff7960ff5a6583df57581819da9258
CaptainStack <CaptainStack@outlook.com>
2015-08-18 18:37:52 -0700

    reverting changes to activity_constants (already done on different branch)

--- lib/cdo/activity_constants.rb
+++ lib/cdo/activity_constants.rb
@@ -22,10 +22,10 @@ module ActivityConstants
       1 => {id: 1, short_name: 'Phase 1', long_name: 'Phase 1: Online Introduction'},
       2 => {id: 2, short_name: 'Phase 2', long_name: 'Phase 2: Blended Summer Study', prerequisite_phase: 1},
       3 => {id: 3, short_name: 'Phase 2 Online', long_name: 'Phase 2 Online: Blended Summer Study'},
-      4 => {id: 4, short_name: 'Phase 3A', long_name: 'Phase 3: Part A'},
-      5 => {id: 5, short_name: 'Phase 3B', long_name: 'Phase 3: Part B'},
-      6 => {id: 6, short_name: 'Phase 3C', long_name: 'Phase 3: Part C'},
-      7 => {id: 7, short_name: 'Phase 3D', long_name: 'Phase 3: Part D'},
+      4 => {id: 4, short_name: 'Phase 3A', long_name: 'Phase 3A'},
+      5 => {id: 5, short_name: 'Phase 3B', long_name: 'Phase 3B'},
+      6 => {id: 6, short_name: 'Phase 3C', long_name: 'Phase 3C'},
+      7 => {id: 7, short_name: 'Phase 3D', long_name: 'Phase 3D'},
       8 => {id: 8, short_name: 'Phase 4', long_name: 'Phase 4: Summer Wrap Up'}
   }
 end
