f87ce4316dea78a79a92e1a52d06f3a4aebd302f
Continuous Integration <dev@code.org>
2015-08-20 05:02:38 +0000

    Automatically built.
    commit 9b76334dd487d1e939045d8f38861a384f7756de
    Merge: 35e7874 465a3f2
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Wed Aug 19 21:51:07 2015 -0700
    
        Merge pull request #3664 from code-dot-org/no_extra_space_flyout
    
        [Finishes #101652146] Don't add extra vertical space when getting scrollable size for flyout
    
    commit 465a3f233f9335a4a12e0843882376c50b8ec636
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Wed Aug 19 21:46:14 2015 -0700
    
        [Finishes #101652146] Don't add extra vertical space when getting scrollable size for flyout

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e1032d892262b25ebaff6ec26ad8091ade033aa85d39ade3cadcb6a22bf5a92
+oid sha256:103156228899e27da0175966ced9ed58423428ad15d6aec9be97e98a3fc32961
 size 73347
