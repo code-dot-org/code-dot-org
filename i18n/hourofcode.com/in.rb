2b37d8047f79c143273ed7541bee6d9f7e974441
Tanya Parker <tanyacparker@gmail.com>
2015-08-20 14:40:06 -0700

    translate email, update posters

--- i18n/hourofcode.com/in.rb
+++ i18n/hourofcode.com/in.rb
@@ -7,6 +7,14 @@ loc_dir = "../locales/source/hourofcode/i18n"
 orig_file = "../../pegasus/sites.v3/hourofcode.com/i18n/en.yml"
 FileUtils.cp(orig_file, loc_dir)
 
+loc_dir = "../locales/source/hourofcode/emails"
+orig_file = "../../pegasus/emails/hoc_signup_2015_receipt.md"
+FileUtils.cp(orig_file, loc_dir)
+
+loc_dir = "../locales/source/hourofcode/i18n"
+orig_file = "../../pegasus/sites.v3/hourofcode.com/i18n/en.yml"
+FileUtils.cp(orig_file, loc_dir)
+
 orig_dir = Dir["../../pegasus/sites.v3/hourofcode.com/public/*.md"]
 orig_dir.each do |file|
   loc_dir = "../locales/source/hourofcode/public"
