b9bc21c527c46194f296cce7807e7ae801825378
Continuous Integration <dev@code.org>
2015-08-20 16:57:54 +0000

    Automatically built.
    commit 10225eadb1ca33327869682cfb6681f0e98c41ae
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Thu Aug 20 09:47:39 2015 -0700
    
        jshint fix

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:103156228899e27da0175966ced9ed58423428ad15d6aec9be97e98a3fc32961
+oid sha256:2b6085502b5dc7f290512ded87bea3d010bfd00338453aaacb66fe072215b33d
 size 73347
