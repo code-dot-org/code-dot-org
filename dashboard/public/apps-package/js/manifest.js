18b9425e501c7364189453f828dd42efb0ea19f3
Continuous Integration <dev@code.org>
2015-08-20 23:00:50 +0000

    Automatically built.
    commit a063bf3d544c4725d7d0bf1ce44f8ddcf4969f4b
    Merge: 81a7052 c022023
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Thu Aug 20 15:50:25 2015 -0700
    
        Merge pull request #3676 from code-dot-org/netsim-lobby-layout
    
        Netsim lobby layout fixes
    
    commit c0220231f018d3020e1c754888c037c5c57f72b0
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Thu Aug 20 14:21:06 2015 -0700
    
        Splits RemoteNodeSelectionPanel into three tables
    
        So that we can make the Lobby table scroll indepdendently of the other
        two. Also add UpdateLayout logic to NetSimRemoteNodeSelectionPanel to
        adjust the height of the lobby table so that it takes on as much of the
        burden of scrolling as possible.
    
    commit 81a7052e5bbf22a840b89c9023ecd6cdf894c162
    Merge: cddea69 af462da
    Author: Josh Lory <josh.lory@code.org>
    Date:   Thu Aug 20 14:13:25 2015 -0700
    
        Merge pull request #3666 from code-dot-org/upgrade-frontend-ff-only
    
        Only allow fast-forward during frontend pulls (no merges)

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b6085502b5dc7f290512ded87bea3d010bfd00338453aaacb66fe072215b33d
+oid sha256:856c833218cb2fd9c92b100bf5b887244e3aeb19fd810a4fb1bfcab7bb2398f0
 size 73347
