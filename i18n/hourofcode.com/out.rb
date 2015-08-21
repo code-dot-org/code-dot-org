560d58b303a1a7dc64a3d140232afd18fdf86f0b
Tanya Parker <tanyacparker@gmail.com>
2015-08-20 19:47:39 -0700

    update script to copy files from i18n to hoc.com

--- i18n/hourofcode.com/out.rb
+++ i18n/hourofcode.com/out.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 require 'fileutils'
 
-# language => [locale, code, customcode]
+# language => [locale, code, crowdincode]
 languages = {
   'Albanian' => ['sq-AL', 'sq'],
   'Arabic' => ['ar-SA', 'ar'],
@@ -70,6 +70,13 @@ languages = {
   'Vietnamese' => ['vi-VN', 'vi']
 }
 
+
+#########################################################################################
+##                                                                                     ##
+## rename folders and files from crowdin codes to our codes                            ##
+##                                                                                     ##
+#########################################################################################
+
 languages.each_pair do |name, codes|
   # rename downloaded folders from language to locale
   if File.directory?("../locales/#{name}/")
@@ -89,32 +96,49 @@ end
 
 
 
+#########################################################################################
+##                                                                                     ##
+## copy files from i18n to hoc.com                                                     ##
+##                                                                                     ##
+#########################################################################################
 
-# # copy xx.yml files to hourofcode.com/i18n
-# my_dir = Dir["../locales/**/hourofcode/*.yml"]
-# my_dir.each do |file|
-#   dest = "../../pegasus/sites.v3/hourofcode.com/i18n"
-#   FileUtils.cp(file, dest)
-# end
+languages.each_pair do |name, codes|
+	i18n_dir = Dir["../locales/#{codes[0]}/hourofcode"]
+	hoc_dir = Dir["../../pegasus/sites.v3/hourofcode.com/i18n"]
 
-# # remove all metadata
-# Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/*.md").each do |file|
-#   File.write(file, File.read(file).gsub(/^.*\*\s\*\s\*/m, ""))
-# end
+	FileUtils.cp(i18n_dir + "#{codes[1]}.yml", hoc_dir)
 
-# # remove broken social tags from thanks.md
-# Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/thanks.md").each do |file|
-#   File.write(file, File.read(file).gsub(/<% facebook.+?\%>/m, ""))
-# end
+	i18n_dir.each do |file|
+		FileUtils.cp(i18n_dir + "/**/*.md", hoc_dir + "/public/#{codes[1]}")
+	end
+end
 
-# # add social tags to thanks.md
-# Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/thanks.md").each do |file|
-#   File.write(file, '<% facebook = {:u=>"http://#{request.host}/us"}
-#                       twitter = {:url=>"http://hourofcode.com", :related=>"codeorg", :hashtags=>"", :text=>hoc_s(:twitter_default_text)}
-#                       twitter[:hashtags] = "HourOfCode" unless hoc_s(:twitter_default_text).include? "#HourOfCode" %>' + File.read(file))
-# end
 
-# # add metadata to resources.md
-# Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/resources.md").each do |file|
-#   File.write(file, "---\nlayout: wide\nnav: resources_nav\n---" + File.read(file))
-# end
+
+#########################################################################################
+##                                                                                     ##
+## clean up crowdin markdown errors                                                    ##
+##                                                                                     ##
+#########################################################################################
+
+# remove all metadata
+Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/*.md").each do |file|
+  File.write(file, File.read(file).gsub(/^.*\*\s\*\s\*/m, ""))
+end
+
+# remove broken social tags from thanks.md
+Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/thanks.md").each do |file|
+  File.write(file, File.read(file).gsub(/<% facebook.+?\%>/m, ""))
+end
+
+# add social tags to thanks.md
+Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/thanks.md").each do |file|
+  File.write(file, '<% facebook = {:u=>"http://#{request.host}/us"}
+                      twitter = {:url=>"http://hourofcode.com", :related=>"codeorg", :hashtags=>"", :text=>hoc_s(:twitter_default_text)}
+                      twitter[:hashtags] = "HourOfCode" unless hoc_s(:twitter_default_text).include? "#HourOfCode" %>' + File.read(file))
+end
+
+# add metadata to resources.md
+Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/resources.md").each do |file|
+  File.write(file, "---\nlayout: wide\nnav: resources_nav\n---" + File.read(file))
+end
