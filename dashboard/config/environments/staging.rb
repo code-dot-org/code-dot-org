232e76855f064b4688fc31ce52578686f2e47a33
Laurel Fan <laurel@code.org>
2015-08-21 12:15:25 -0700

    fix developers running tests

--- dashboard/config/environments/staging.rb
+++ dashboard/config/environments/staging.rb
@@ -63,8 +63,6 @@ Dashboard::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = Poste2::DeliveryMethod
-  # use https://staging-studio.code.org urls in mails
-  Dashboard::Application.config.action_mailer.default_url_options = { host: CDO.canonical_hostname('studio.code.org'), protocol: 'https' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
