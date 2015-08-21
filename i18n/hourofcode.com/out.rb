de3f0a1a1a69781734b0c82c8d99fdae606dec58
Tanya Parker <tanyacparker@gmail.com>
2015-08-20 20:07:40 -0700

    uncomment out.rb

--- i18n/hourofcode.com/out.rb
+++ i18n/hourofcode.com/out.rb
@@ -96,49 +96,49 @@ end
 
 
 
-# #########################################################################################
-# ##                                                                                     ##
-# ## copy files from i18n to hoc.com                                                     ##
-# ##                                                                                     ##
-# #########################################################################################
+#########################################################################################
+##                                                                                     ##
+## copy files from i18n to hoc.com                                                     ##
+##                                                                                     ##
+#########################################################################################
 
-# languages.each_pair do |name, codes|
-# 	i18n_dir = Dir["../locales/#{codes[0]}/hourofcode"]
-# 	hoc_dir = Dir["../../pegasus/sites.v3/hourofcode.com/i18n"]
+languages.each_pair do |name, codes|
+	i18n_dir = Dir["../locales/#{codes[0]}/hourofcode"]
+	hoc_dir = Dir["../../pegasus/sites.v3/hourofcode.com/i18n"]
 
-# 	FileUtils.cp(i18n_dir + "#{codes[1]}.yml", hoc_dir)
+	FileUtils.cp(i18n_dir + "#{codes[1]}.yml", hoc_dir)
 
-# 	i18n_dir.each do |file|
-# 		FileUtils.cp(i18n_dir + "/**/*.md", hoc_dir + "/public/#{codes[1]}")
-# 	end
-# end
+	i18n_dir.each do |file|
+		FileUtils.cp(i18n_dir + "/**/*.md", hoc_dir + "/public/#{codes[1]}")
+	end
+end
 
 
 
-# #########################################################################################
-# ##                                                                                     ##
-# ## clean up crowdin markdown errors                                                    ##
-# ##                                                                                     ##
-# #########################################################################################
+#########################################################################################
+##                                                                                     ##
+## clean up crowdin markdown errors                                                    ##
+##                                                                                     ##
+#########################################################################################
 
-# # remove all metadata
-# Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/*.md").each do |file|
-#   File.write(file, File.read(file).gsub(/^.*\*\s\*\s\*/m, ""))
-# end
+# remove all metadata
+Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/*.md").each do |file|
+  File.write(file, File.read(file).gsub(/^.*\*\s\*\s\*/m, ""))
+end
 
-# # remove broken social tags from thanks.md
-# Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/thanks.md").each do |file|
-#   File.write(file, File.read(file).gsub(/<% facebook.+?\%>/m, ""))
-# end
+# remove broken social tags from thanks.md
+Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/thanks.md").each do |file|
+  File.write(file, File.read(file).gsub(/<% facebook.+?\%>/m, ""))
+end
 
-# # add social tags to thanks.md
-# Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/thanks.md").each do |file|
-#   File.write(file, '<% facebook = {:u=>"http://#{request.host}/us"}
-#                       twitter = {:url=>"http://hourofcode.com", :related=>"codeorg", :hashtags=>"", :text=>hoc_s(:twitter_default_text)}
-#                       twitter[:hashtags] = "HourOfCode" unless hoc_s(:twitter_default_text).include? "#HourOfCode" %>' + File.read(file))
-# end
+# add social tags to thanks.md
+Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/thanks.md").each do |file|
+  File.write(file, '<% facebook = {:u=>"http://#{request.host}/us"}
+                      twitter = {:url=>"http://hourofcode.com", :related=>"codeorg", :hashtags=>"", :text=>hoc_s(:twitter_default_text)}
+                      twitter[:hashtags] = "HourOfCode" unless hoc_s(:twitter_default_text).include? "#HourOfCode" %>' + File.read(file))
+end
 
-# # add metadata to resources.md
-# Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/resources.md").each do |file|
-#   File.write(file, "---\nlayout: wide\nnav: resources_nav\n---" + File.read(file))
-# end
+# add metadata to resources.md
+Dir.glob("../../pegasus/sites.v3/hourofcode.com/public/us/**/resources.md").each do |file|
+  File.write(file, "---\nlayout: wide\nnav: resources_nav\n---" + File.read(file))
+end
