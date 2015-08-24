b5c1128fb8b3373d5cd3eee664bb0ae150828874
David Bailey <dsb123@gmail.com>
2015-08-24 14:59:19 -0700

    Move pixelation globals to callbacks on appOptions.dialog.

--- dashboard/public/pixelation/pixelation.js
+++ dashboard/public/pixelation/pixelation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:079be3ab47519959fe550076cd30117783a5a408ba426eb53bfac179d64321b0
-size 13304
+oid sha256:feffc555020e605ffd14d501d9cebdf088cc553ba82ee5ee9c769f92b494eeaa
+size 13358
