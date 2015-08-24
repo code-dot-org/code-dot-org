7cd448147412e570360e63d0e5bfa2c73de47dbe
Continuous Integration <dev@code.org>
2015-08-24 20:46:45 +0000

    Automatically built.
    commit 78f3817fcb3acc3b25f08f998bdfd2fca306ace5
    Merge: 83870fd dd44bcb
    Author: Tanya Parker <tanya@code.org>
    Date:   Mon Aug 24 13:36:03 2015 -0700
    
        Merge pull request #3711 from code-dot-org/netsim-lobby-layout
    
        jshint fixes
    
    commit dd44bcbaab1dedf243ebbcdcfa2ce4538148ecd7
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Mon Aug 24 13:33:42 2015 -0700
    
        jshint fixes

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:456dd490939543fb4ab887b77f052c6dee0e844226abbc8084fa757e1e3a7b3a
+oid sha256:8a75b6811dcbb11443a0b792e9daf49e8dd8a3876fb99c2f9bd7f2fa7b209fb7
 size 73347
