fb31830bb52bb6595098e41f3538d20f1427e398
Continuous Integration <dev@code.org>
2015-08-26 21:46:39 +0000

    Automatically built.
    commit f4beb59d4d2c947a2273d041172accec2202d078
    Merge: 9937069 835a3aa
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Wed Aug 26 14:42:40 2015 -0700
    
        Merge pull request #3742 from code-dot-org/studioResize
    
        [Finishes #102104608] trigger resize after adding extra row of buttons
    
    commit 835a3aa23377790e0f5beb8c7a0d6e6510d2e72e
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Wed Aug 26 14:00:53 2015 -0700
    
        trigger resize after adding extra row of buttons

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fb81355a72ab11b5765d3c9d53e382981b7294f7116423db5f540b725f748b5
+oid sha256:3b8cb7de4520c5e10c2bc65b42cf2c136a248c1f4558cdf3bd4105d89374748a
 size 73347
--- dashboard/public/apps-package/js/studio.min-7200c4962bc29c165f3b5afea57b6aa8.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6c9ac536ea1eb84342b0f1ecd637fc683e854678a15b5d5ba51ef1d46359b6f2
-size 173860
