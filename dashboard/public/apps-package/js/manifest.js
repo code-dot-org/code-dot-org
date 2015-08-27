6d3707e4912156409a323523696ea3b898066f8c
Continuous Integration <dev@code.org>
2015-08-27 17:36:16 +0000

    Automatically built.
    commit 33d010fb682faaff35c9cf3216a9d0ccecb555b7
    Merge: 5daaa38 6d387b5
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Aug 27 10:31:16 2015 -0700
    
        Merge pull request #3746 from code-dot-org/count-with-text-to-four
    
        Changed the text on CONTROLS_FOR_INPUT_WITH from 'count with' to 'for'
    
    commit 5daaa38984c10bf20fb2337fe07fa2b90b72fcc8
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Aug 27 17:10:42 2015 +0000
    
        content changes (elijah)
    
    commit 42d927d7e5ff3a8addccd2422f48eb91e7e6b4b1
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Wed Aug 26 18:57:50 2015 -0700
    
        jk wait to launch Georgian next week

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b8cb7de4520c5e10c2bc65b42cf2c136a248c1f4558cdf3bd4105d89374748a
+oid sha256:7cba6957f2a9764c919f9bbca4bac8752e1b4f3735c7e3fbd6fd97d7fd53b4ff
 size 73347
