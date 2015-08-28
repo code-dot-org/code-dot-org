c013e5e3f410421723ea1cb7a95b9ac831026a5f
Josh Lory <josh.lory@code.org>
2015-08-28 16:56:31 -0700

    Create levelbuilder edit view for Odometer

--- dashboard/app/models/odometer.rb
+++ dashboard/app/models/odometer.rb
@@ -3,4 +3,8 @@ class Odometer < Widget
   before_validation do
     self.href = 'odometer/odometer.html'
   end
+
+  def self.create_from_level_builder(params, level_params)
+    create!(level_params.merge(user: params[:user], game: Game.odometer, level_num: 'custom'))
+  end
 end
