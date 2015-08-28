10a2c00a5293191a447b0366498fa101e902ab45
Continuous Integration <dev@code.org>
2015-08-28 18:46:50 +0000

    Automatically built.
    commit 3806680811e278d4c8a22f79358d26cd0716e798
    Author: Laurel Fan <laurel@code.org>
    Date:   Fri Aug 28 11:46:29 2015 -0700
    
        remove some unnecessary checks

--- dashboard/public/shared-package/js/shared.js
+++ dashboard/public/shared-package/js/shared.js
@@ -640,9 +640,7 @@ var projects = module.exports = {
   },
 
   showAdmin: function() {
-    if (typeof dashboard.admin.showProjectAdmin === 'function') {
-      dashboard.admin.showProjectAdmin();
-    }
+    dashboard.admin.showProjectAdmin();
   },
 
   showMinimalProjectHeader: function() {
