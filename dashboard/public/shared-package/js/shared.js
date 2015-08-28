535c4918d3de9e0a03f32ed2f8f03f22643398ba
Continuous Integration <dev@code.org>
2015-08-28 17:35:50 +0000

    Automatically built.
    commit 604f179ad7425ee315ab9dc13bd70dc8d1a3ae37
    Merge: 11ba66f ca8c477
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Fri Aug 28 10:34:57 2015 -0700
    
        Merge pull request #3748 from code-dot-org/applab-examples
    
        Applab examples
    
    commit 11ba66f87d06618cab64f147edd4af00816510a3
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Thu Aug 27 18:01:02 2015 -0700
    
        make hoc.com buttons all the same orange
    
    commit 7101d6b7606cead919b412b0f08704fc28b432f2
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Thu Aug 27 17:50:35 2015 -0700
    
        change text from Educator Resources to How to Teach an Hour of Code
    
    commit 8fe245c2dce899241afeeaf3d134c883dd0c3540
    Merge: d8d63a8 7f451e7
    Author: Tanya Parker <tanya@code.org>
    Date:   Thu Aug 27 17:42:37 2015 -0700
    
        Merge pull request #3761 from code-dot-org/hourofcode
    
        hoc i18n updates
    
    commit 7f451e7b967c9ebf6ff3b4ad63f5e144e6afe770
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Thu Aug 27 17:40:00 2015 -0700
    
        add sign up buttons to home page, by map and at bottom
    
    commit d8d63a83303e0bb9acf27c1c9526c3aa2816e582
    Merge: b346519 1eb39aa
    Author: Tanya Parker <tanya@code.org>
    Date:   Thu Aug 27 17:27:06 2015 -0700
    
        Merge pull request #3760 from code-dot-org/fixenus
    
        fix quotes in pegasus en-US.yml
    
    commit ecff599cd2fb5ee043c363fb39b0ba8d064a1fb3
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Thu Aug 27 17:26:36 2015 -0700
    
        localize titles of all hoc.com pages

--- dashboard/public/shared-package/js/shared.js
+++ dashboard/public/shared-package/js/shared.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00a941bfcc9f30e39eaed32c07bb5ec0cb7dc08165da0d2086ba7e78cca107ee
-size 33398
+oid sha256:f26e8d145bb8577962a7a9a3a66a21dec8d1779e72903de74a7070ee495ef83b
+size 34347
