493a0211bfe381caf289ef6e6576d7e7d0232d4b
Continuous Integration <dev@code.org>
2015-08-28 23:26:39 +0000

    Automatically built.
    commit 9b5da8dbd38a06dc1e1752319e18f3122c65e8aa
    Merge: 0675c7e 52d2b56
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Aug 28 16:21:58 2015 -0700
    
        Merge pull request #3768 from code-dot-org/guardLevelHtml
    
        [Finishes #102280132] throw if we try to blow away levelHtml
    
    commit 0675c7ed75918a29398fedec9babae9793d14fc4
    Merge: c237432 376233c
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Aug 28 16:21:45 2015 -0700
    
        Merge pull request #3770 from code-dot-org/hardenDroplet
    
        [Finishes #102281176] catch some droplets exceptions
    
    commit 376233cc3f5efa655804eef57a333553e13f829e
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Fri Aug 28 14:57:35 2015 -0700
    
        catch some droplets exceptions
    
    commit c237432b270bd18d3854c583cc1b7c1038ab4e8e
    Merge: db308bc e90907e
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Fri Aug 28 16:17:33 2015 -0700
    
        Merge pull request #3773 from code-dot-org/fixRakeTest
    
        fix rake test

--- dashboard/public/apps-package/js/common-c4d00903bffc7760d1d7f35f5d82b4df.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9e03b9f541ceb1df8fd0b5fc9e518eb2261c8f3623ac2801130223f2ec7d88d1
-size 1301088
--- dashboard/public/apps-package/js/common.min-cd41c1e333a51fecbc254515d58faab7.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2ebe995109ebe539aabc3dfe6227c7d91e9112ef0eef43b5b22aaf2573e16389
-size 446486
