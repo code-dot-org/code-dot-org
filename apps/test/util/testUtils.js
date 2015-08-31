53e8ce7c0637bed6f7d916a60013a54e7e6b27e9
Brian Jordan <bcjordan@gmail.com>
2015-08-31 17:06:26 -0400

    Error message on examples' empty "Call" and "Result" block inputs - Extract shared example throw code into its own method with some test coverage - Expose Chai.expect to apps tests

--- apps/test/util/testUtils.js
+++ apps/test/util/testUtils.js
@@ -2,6 +2,8 @@ var chai = require('chai');
 chai.config.includeStack = true;
 var assert = chai.assert;
 exports.assert = assert;
+exports.expect = chai.expect;
+
 require('require-globify');
 
 var $ = require('jquery');
