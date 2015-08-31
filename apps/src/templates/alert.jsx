69e93d17d835612c615277b96f856892726e4fd7
Brent Van Minnen <bjvanminnen@gmail.com>
2015-08-31 15:37:18 -0700

    add new alert component

---
+++ apps/src/templates/alert.jsx
@@ -0,0 +1,34 @@
+var React = require('React');
+
+/**
+ * Simple boot-strapped style alert.
+ */
+module.exports = React.createClass({
+  propTypes: {
+    body: React.PropTypes.string.isRequired,
+    className: React.PropTypes.string,
+    style: React.PropTypes.object,
+    onClose: React.PropTypes.func.isRequired
+  },
+
+  render: function () {
+    var style = $.extend({}, {
+      position: 'absolute',
+      zIndex: 9999,
+      margin: 0
+    }, this.props.style);
+
+    return (
+      <div style={style}>
+        <div className={"alert fade in " + (this.props.className || '')}>
+          <button type="button"
+            className="alert-button close"
+            style={{ margin: 0 }}>
+            <span onClick={this.props.onClose}>&times;</span>
+          </button>
+          {this.props.body}
+        </div>
+      </div>
+    );
+  }
+});
