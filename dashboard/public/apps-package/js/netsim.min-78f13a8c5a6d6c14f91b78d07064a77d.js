2049dcf16842998d174ed48c8a4e6f2120b8d9be
Continuous Integration <dev@code.org>
2015-09-01 21:51:29 +0000

    Automatically built.
    commit a4881f312b2ee818e09e8339601993d69da07d12
    Merge: 8c83a34 2ef334b
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Tue Sep 1 14:46:44 2015 -0700
    
        Merge pull request #3820 from code-dot-org/netsim-footer-width-fix
    
        smallFooter width changes now account for padding
    
    commit 8c83a34e1714c114bc7423a6f793fd858c57d515
    Author: Laurel Fan <laurel@code.org>
    Date:   Tue Sep 1 14:33:20 2015 -0700
    
        typo

---
+++ dashboard/public/apps-package/js/netsim.min-78f13a8c5a6d6c14f91b78d07064a77d.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63a7439403f62199d43abc8a9b4764fc363fa8d79363fb209be5728a050b37bb
+size 539957
--- dashboard/public/apps-package/js/netsim.min-7ce40c2478c8547e44d84e26a0625aa0.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4ece65682647f7ac60210e84fe1642c94f17c2ce4571b6a0bad53365464fa6ab
-size 539902
