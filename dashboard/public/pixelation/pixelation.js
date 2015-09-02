732844052c918696acf61615239e5c2158845ff2
David Bailey <dsb123@gmail.com>
2015-09-02 16:33:30 -0700

    extract remaining JS from pixelation.html.

--- dashboard/public/pixelation/pixelation.js
+++ dashboard/public/pixelation/pixelation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82e8dc62d4ceae92def138be84b1c4e1941af46a03ff9b89c9127c590a0e2a26
-size 14254
+oid sha256:dca7c39397afff8d7eff44212138d161b4fca201907c0235745c8cd0a9cd51d1
+size 16034
