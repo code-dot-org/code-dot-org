a1d88e55b8083496b4fcfae5718bd81a0038b30b
Continuous Integration <dev@code.org>
2015-09-03 00:25:37 +0000

    Automatically built.
    commit 069c5860c26183db95d85057c0c6ebab208647e3
    Merge: 6234d6b c59e926
    Author: Trevor Berg <trevor@code.org>
    Date:   Wed Sep 2 17:17:06 2015 -0700
    
        Merge pull request #3845 from code-dot-org/fix-deleting-last-char
    
        Fix being able to delete the last character of a number field in applab design mode
    
    commit c59e926b8112c17cca6ef5b86c2179860ddd0953
    Author: Trevor Berg <trevor@code.org>
    Date:   Wed Sep 2 16:58:47 2015 -0700
    
        Cleanup from Brents suggestions
    
    commit 1c8c8102eb528b9606c72db3ab2494e0c34355e7
    Author: Trevor Berg <trevor@code.org>
    Date:   Wed Sep 2 16:39:53 2015 -0700
    
        Fix being able to delete the last character of a number field in applab design mode
    
    commit 6234d6b2d5b2aa45de8384c599d53ad09f8d2959
    Author: Brian Jordan <bcjordan@gmail.com>
    Date:   Wed Sep 2 19:22:52 2015 -0400
    
        Skip test while bug open
    
    commit 4d5302a4e0c49cbb73f1c9f9a4f0515a36c0301d
    Merge: 2aecd2a 8490335
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Wed Sep 2 16:11:34 2015 -0700
    
        Merge pull request #3836 from code-dot-org/pixelation-teacher-view
    
        disable UI controls when viewing pixelation level as a teacher.

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1d1ff0bf14a3166b7b591270bb525a5395d074d5cd42a8311396f3f8a6d4d99
+oid sha256:beb91945afae4915fdacb4a72e6c1c7330c14c2cadad3bc8701b8cb4ca0cd1c0
 size 73347
