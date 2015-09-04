740c4032dc73de0d297a50005750a75021c1c982
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-03 18:12:49 -0700

    re-linked substitutions, minor visual tweaks

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8eb009b098203d08b935f62a2ac327763b62f768f0d1eb2f2e06bb0d4fbf959
-size 16577
+oid sha256:57a459c9e2d27806ef80e93f4e153cea8dd2eea34b2d483464e05543bbad73d0
+size 19528
