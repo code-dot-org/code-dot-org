dc0738d17fd4698e7320b23995916790cdd7d4c7
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-03 21:34:43 -0700

    removed yellow highlighting

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a09b3817eeb920050e64ba111048a868b8717b61a410af35423d7c3f38fcc03b
-size 19410
+oid sha256:f54b37595b0480d1994227f2bc02519802eca2cc6abef9e39ee9bc5248498776
+size 19431
