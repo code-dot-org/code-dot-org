2b9344d09cbf872ce49a432edc4b38931e3a77a7
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-03 22:11:18 -0700

    rearranged a few things

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f54b37595b0480d1994227f2bc02519802eca2cc6abef9e39ee9bc5248498776
-size 19431
+oid sha256:5afe2d61d936b3146d6cea3aa60e3d331752681194e69e9986f7be8033ee8937
+size 19257
