b98014243468d07268401792156057a570a604bb
Brent Van Minnen <bjvanminnen@gmail.com>
2015-09-04 09:43:07 -0700

    show abuse box when going to share page

---
+++ dashboard/app/assets/javascripts/components/exclamation_box.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ada7fdb7237c681730c43c638a702d0d97b75ce75a262b1e3ddd00cd233c4df2
+size 1551
--- dashboard/app/assets/javascripts/components/share_dialog_body.jsx
+++ dashboard/app/assets/javascripts/components/share_dialog_body.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24966ec380da07f8c3004024484437d71931afca5bec8ffddc16584e1b94a18f
-size 3672
+oid sha256:5fa6c9a603f91b96fd1da28e344ea96d8b0d45e2ff13c936ec00f4c3430f336f
+size 3699
