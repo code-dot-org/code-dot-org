568670e81c79f2cef40706a4d6957035850000d3
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-03 20:29:37 -0700

    set up shift vs sort UI

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57a459c9e2d27806ef80e93f4e153cea8dd2eea34b2d483464e05543bbad73d0
-size 19528
+oid sha256:601aea14a650e6a30c20983b96e07beec643c35c55c6cc57a5e9c3f166beceb8
+size 19545
