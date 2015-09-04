0c5c2b674a7f01ff86b8c63cbb72110b85dff152
Continuous Integration <dev@code.org>
2015-09-04 21:36:11 +0000

    Automatically built.
    commit d0ac3095b3a06260e7caa48ef340181e69007a08
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Fri Sep 4 14:33:39 2015 -0700
    
        better regex

--- dashboard/public/shared-package/js/shared.js
+++ dashboard/public/shared-package/js/shared.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d59af586b4e550ffe036b3d4d41838bc7cdfad09c523a7f8973e894bfaee1ac3
-size 34621
+oid sha256:bb87f4b10f5b135ac3b5aad89ebf17e768b321d20a3c3346a2fbd769861c560c
+size 34591
