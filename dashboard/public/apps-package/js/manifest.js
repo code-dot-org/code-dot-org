6dda43981088bc24259d0b0be1d91f727d6f5bed
Continuous Integration <dev@code.org>
2015-09-04 16:57:58 +0000

    Automatically built.
    commit e24aace049adf7220f282ccef59671f50079885d
    Merge: 6095865 62c45f4
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Fri Sep 4 09:53:42 2015 -0700
    
        Merge pull request #3872 from code-dot-org/remote-node-in-status-bar
    
        Show remote node name in status bar.
    
    commit 62c45f4a113628cecf62a833484978a47834a2c9
    Author: Brad Buchanan <brad@code.org>
    Date:   Fri Sep 4 09:36:41 2015 -0700
    
        Show remote node name in status bar.
    
        Instead of just saying "Connected" show the name of the remote
        node in the top-left above the visualization.
    
        In broadcast mode this allows you to know what room you are in.
    
        In other modes you can also get this information from the visualization,
        but putting it next to the "Disconnect" button helps reinforce what
        you are disconnecting from.
    
        Finishes Pivotal #101320054
    
        [ci skip]

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:219f309995467d9ed1fdfe4736ba5fe62f60de7ed7884fa078c0ab29f733e34b
+oid sha256:dda81e5fa52081011eda511e18072e27cf1170b48857be52a6c5349d9f4883d2
 size 73347
---
+++ dashboard/public/apps-package/js/netsim-62c509c5e9782988fbcdd909d200d6b4.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ce056ee36ded80f2c7d92446729c600bb964e725956f38c4efc3d31a492845e3
+size 1353738
