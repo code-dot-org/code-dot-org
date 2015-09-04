85fb37706c87afa9f27696b3fee7fcbd28a16f71
Brent Van Minnen <bjvanminnen@gmail.com>
2015-09-04 11:06:57 -0700

    combine abuse box and exclamation into single component

--- dashboard/app/assets/javascripts/components/abuse_box.jsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ef854e2e8c9148e40521d86e963ca4c7556d8daeb5a699841b41974585bf4a7d
-size 1477
---
+++ dashboard/app/assets/javascripts/components/abuse_exclamation.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7a43dc3e949bdceb603e9b4f16b95cbf12265437cf7e472a39e5cab4070017f
+size 2350
