93463818198624101cb1edd610ca602b4d29130f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-04 17:36:30 -0700

    rearranging some initialization work and ui elements

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4686b1f67c2092c8792696c9c240a15ac8caa36c01007ab32797d4386c79d54
-size 19520
+oid sha256:e3bd1934a0dddb8618f83e875a5d00d93a0573739d62c3c86be3bae9bc89caf5
+size 20100
