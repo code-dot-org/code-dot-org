f3d979e0b2aa569d08e65c08322d9b29d05b393b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-04 17:51:14 -0700

    removing an errant console.log

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3bd1934a0dddb8618f83e875a5d00d93a0573739d62c3c86be3bae9bc89caf5
-size 20100
+oid sha256:1ef65cb4b36f414fc45dfa93200e2e196ff549513db79d460684a2e55670484b
+size 20005
