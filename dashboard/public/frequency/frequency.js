9d95a6adc3c6de8cbe97ee587f9e114970c9ee83
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-09 11:44:14 -0700

    added locks

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ef65cb4b36f414fc45dfa93200e2e196ff549513db79d460684a2e55670484b
-size 20005
+oid sha256:77e5ca788344a0e8f6076239964d2c6a9aeb26c0d5de67f676d12715d45fb594
+size 21859
