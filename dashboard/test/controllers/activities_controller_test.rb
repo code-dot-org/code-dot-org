cde82142f7612273eef25dd0e272eae07da08981
Laurel Fan <laurel.fan@gmail.com>
2015-09-10 12:52:49 -0700

    fix test

--- dashboard/test/controllers/activities_controller_test.rb
+++ dashboard/test/controllers/activities_controller_test.rb
@@ -435,8 +435,6 @@ class ActivitiesControllerTest < ActionController::TestCase
       end
     end
 
-    assert LevelSourceImage.last.s3?
-
     assert_response :success
 
     expected_response = build_expected_response(
