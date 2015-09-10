1c6709529e6181dff8b119fe8307a2d9a75a6f1d
David Bailey <dsb123@gmail.com>
2015-09-10 00:17:20 -0700

    clean up tests

--- shared/test/test_assets.rb
+++ shared/test/test_assets.rb
@@ -13,63 +13,63 @@ class AssetsTest < Minitest::Test
   end
 
   def test_assets
-    channel_id = create_channel
+    channel_id = create_channel(@channels)
 
     ensure_aws_credentials(channel_id)
 
     image_filename = 'dog.jpg'
     image_body = 'stub-image-contents'
 
-    actual_image_info = JSON.parse(put(channel_id, image_filename, image_body, 'image/jpeg'))
+    actual_image_info = JSON.parse(put(@assets, channel_id, image_filename, image_body, 'image/jpeg'))
     expected_image_info = {'filename' => image_filename, 'category' => 'image', 'size' => image_body.length}
     assert_fileinfo_equal(expected_image_info, actual_image_info)
 
     sound_filename = 'woof.mp3'
     sound_body = 'stub-sound-contents'
 
-    actual_sound_info = JSON.parse(put(channel_id, sound_filename, sound_body, 'audio/mpeg'))
+    actual_sound_info = JSON.parse(put(@assets, channel_id, sound_filename, sound_body, 'audio/mpeg'))
     expected_sound_info = {'filename' =>  sound_filename, 'category' => 'audio', 'size' => sound_body.length}
     assert_fileinfo_equal(expected_sound_info, actual_sound_info)
 
-    file_infos = JSON.parse(list(channel_id))
+    file_infos = JSON.parse(list(@assets, channel_id))
     assert_fileinfo_equal(actual_image_info, file_infos[0])
     assert_fileinfo_equal(actual_sound_info, file_infos[1])
 
-    get(channel_id, image_filename)
+    get(@assets, channel_id, image_filename)
     assert_equal 'public, max-age=3600', @assets.last_response['Cache-Control']
 
-    delete(channel_id, image_filename)
+    delete(@assets, channel_id, image_filename)
     assert @assets.last_response.successful?
 
-    delete(channel_id, sound_filename)
+    delete(@assets, channel_id, sound_filename)
     assert @assets.last_response.successful?
 
     # unsupported media type
-    put(channel_id, 'filename.exe', 'stub-contents', 'application/x-msdownload')
+    put(@assets, channel_id, 'filename.exe', 'stub-contents', 'application/x-msdownload')
     assert_equal 415, @assets.last_response.status
 
     # mismatched file extension and mime type
-    put(channel_id, 'filename.jpg', 'stub-contents', 'application/gif')
+    put(@assets, channel_id, 'filename.jpg', 'stub-contents', 'application/gif')
     assert @assets.last_response.successful?
-    delete(channel_id, 'filename.jpg')
+    delete(@assets, channel_id, 'filename.jpg')
     assert @assets.last_response.successful?
 
     # invalid files are not uploaded
-    file_infos = JSON.parse(list(channel_id))
+    file_infos = JSON.parse(list(@assets, channel_id))
     assert_equal 0, file_infos.length
 
-    delete(channel_id, 'nonexistent.jpg')
+    delete(@assets, channel_id, 'nonexistent.jpg')
     assert @assets.last_response.successful?
 
-    get(channel_id, 'nonexistent.jpg')
+    get(@assets, channel_id, 'nonexistent.jpg')
     assert @assets.last_response.not_found?
 
-    delete_channel(channel_id)
+    delete_channel(@channels, channel_id)
   end
 
   def test_assets_copy_all
-    src_channel_id = create_channel
-    dest_channel_id = create_channel
+    src_channel_id = create_channel(@channels)
+    dest_channel_id = create_channel(@channels)
 
     image_filename = 'dog.jpg'
     image_body = 'stub-image-contents'
@@ -78,31 +78,31 @@ class AssetsTest < Minitest::Test
     sound_body = 'stub-sound-contents'
     expected_sound_info = {'filename' =>  sound_filename, 'category' => 'audio', 'size' => sound_body.length}
 
-    put(src_channel_id, image_filename, image_body, 'image/jpeg')
-    put(src_channel_id, sound_filename, sound_body, 'audio/mpeg')
-    copy_file_infos = JSON.parse(copy_all(src_channel_id, dest_channel_id))
-    dest_file_infos = JSON.parse(list(dest_channel_id))
+    put(@assets, src_channel_id, image_filename, image_body, 'image/jpeg')
+    put(@assets, src_channel_id, sound_filename, sound_body, 'audio/mpeg')
+    copy_file_infos = JSON.parse(copy_all(@assets, src_channel_id, dest_channel_id))
+    dest_file_infos = JSON.parse(list(@assets, dest_channel_id))
 
     assert_fileinfo_equal(expected_image_info, copy_file_infos[0])
     assert_fileinfo_equal(expected_sound_info, copy_file_infos[1])
     assert_fileinfo_equal(expected_image_info, dest_file_infos[0])
     assert_fileinfo_equal(expected_sound_info, dest_file_infos[1])
 
-    delete(src_channel_id, image_filename)
-    delete(src_channel_id, sound_filename)
-    delete(dest_channel_id, image_filename)
-    delete(dest_channel_id, sound_filename)
-    delete_channel(src_channel_id)
-    delete_channel(dest_channel_id)
+    delete(@assets, src_channel_id, image_filename)
+    delete(@assets, src_channel_id, sound_filename)
+    delete(@assets, dest_channel_id, image_filename)
+    delete(@assets, dest_channel_id, sound_filename)
+    delete_channel(@channels, src_channel_id)
+    delete_channel(@channels, dest_channel_id)
   end
 
   def test_copy_all_with_no_src
-    copy_all(nil, create_channel)
+    copy_all(@assets, nil, create_channel(@channels))
     assert @assets.last_response.bad_request?
   end
 
   def test_assets_auth
-    owner_channel_id = create_channel
+    owner_channel_id = create_channel(@channels)
 
     non_owner_assets = Rack::Test::Session.new(Rack::MockSession.new(FilesApi, "studio.code.org"))
 
@@ -110,32 +110,31 @@ class AssetsTest < Minitest::Test
     body = 'stub-image-contents'
     content_type = 'image/jpeg'
 
-    @assets.put("/v3/assets/#{owner_channel_id}/#{filename}", body, 'CONTENT_TYPE' => content_type)
+    put(@assets, owner_channel_id, filename, body, content_type)
     assert @assets.last_response.successful?, 'Owner can add a file'
 
-    non_owner_assets.get "/v3/assets/#{owner_channel_id}/#{filename}"
+    get(non_owner_assets, owner_channel_id, filename)
     assert non_owner_assets.last_response.successful?, 'Non-owner can read a file'
 
-    non_owner_assets.put("/v3/assets/#{owner_channel_id}/#{filename}", body, 'CONTENT_TYPE' => content_type)
+    put(non_owner_assets, owner_channel_id, filename, body, content_type)
     assert !non_owner_assets.last_response.successful?, 'Non-owner cannot write a file'
 
-    non_owner_assets.delete("/v3/assets/#{owner_channel_id}/#{filename}", body, 'CONTENT_TYPE' => content_type)
+    delete(non_owner_assets, owner_channel_id, filename)
     assert !non_owner_assets.last_response.successful?, 'Non-owner cannot delete a file'
 
     # other_channel_id isn't owned by either user of the assets API.
     other_channels = Rack::Test::Session.new(Rack::MockSession.new(ChannelsApi, "studio.code.org"))
-    other_channels.post '/v3/channels', {}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
-    other_channel_id = other_channels.last_response.location.split('/').last
+    other_channel_id = create_channel(other_channels)
 
-    copy_all(other_channel_id, owner_channel_id)
+    copy_all(@assets, other_channel_id, owner_channel_id)
     assert @assets.last_response.successful?, 'User who owns the destination channel can copy files.'
 
-    copy_all(owner_channel_id, other_channel_id)
+    copy_all(@assets, owner_channel_id, other_channel_id)
     assert @assets.last_response.not_found?, 'User who does not own the destination channel cannot copy files.'
 
-    delete(owner_channel_id, filename)
+    delete(@assets, owner_channel_id, filename)
 
-    other_channels.delete "/v3/channels/#{other_channel_id}"
+    delete_channel(other_channels, other_channel_id)
   end
 
   # Methods below this line are test utilities, not actual tests
@@ -152,18 +151,18 @@ class AssetsTest < Minitest::Test
     @assets ||= Rack::Test::Session.new(assets_mock_session)
   end
 
-  def create_channel
-    @channels.post '/v3/channels', {}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
-    @channels.last_response.location.split('/').last
+  def create_channel(channels)
+    channels.post '/v3/channels', {}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
+    channels.last_response.location.split('/').last
   end
 
-  def delete_channel(channel_id)
-    @channels.delete "/v3/channels/#{channel_id}"
-    assert @channels.last_response.successful?
+  def delete_channel(channels, channel_id)
+    channels.delete "/v3/channels/#{channel_id}"
+    assert channels.last_response.successful?
   end
 
   def ensure_aws_credentials(channel_id)
-    list(channel_id)
+    list(@assets, channel_id)
     credentials_missing =
         !@assets.last_response.successful? &&
             @assets.last_response.body.index('Aws::Errors::MissingCredentialsError')
@@ -174,25 +173,25 @@ class AssetsTest < Minitest::Test
     flunk credentials_msg if credentials_missing
   end
 
-  def list(channel_id)
-    @assets.get("/v3/assets/#{channel_id}").body
+  def list(assets, channel_id)
+    assets.get("/v3/assets/#{channel_id}").body
   end
 
-  def put(channel_id, filename, body, content_type)
-    @assets.put("/v3/assets/#{channel_id}/#{filename}", body, 'CONTENT_TYPE' => content_type).body
+  def put(assets, channel_id, filename, body, content_type)
+    assets.put("/v3/assets/#{channel_id}/#{filename}", body, 'CONTENT_TYPE' => content_type).body
   end
 
-  def get(channel_id, filename)
-    @assets.get "/v3/assets/#{channel_id}/#{filename}"
-    @assets.last_response.body
+  def get(assets, channel_id, filename)
+    assets.get "/v3/assets/#{channel_id}/#{filename}"
+    assets.last_response.body
   end
 
-  def delete(channel_id, filename)
-    @assets.delete "/v3/assets/#{channel_id}/#{filename}"
+  def delete(assets, channel_id, filename)
+    assets.delete "/v3/assets/#{channel_id}/#{filename}"
   end
 
-  def copy_all(src_channel_id, dest_channel_id)
-    @assets.put("/v3/assets/#{dest_channel_id}?src=#{src_channel_id}").body
+  def copy_all(assets, src_channel_id, dest_channel_id)
+    assets.put("/v3/assets/#{dest_channel_id}?src=#{src_channel_id}").body
   end
 
   def assert_fileinfo_equal(expected, actual)
