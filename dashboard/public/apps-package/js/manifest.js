eec720ae8d4a245aaf25855a836eba86f647db80
Continuous Integration <dev@code.org>
2015-09-10 17:24:46 +0000

    Automatically built.
    commit 881d5a6731f10b7abdcb36b0534341e00e99541e
    Merge: e972bc2 ee248ed
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Sep 10 17:20:52 2015 +0000
    
        Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
    commit ee248ed358ed7087ffbddae233626b9620309eb0
    Merge: 5b711cf da70dab
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Thu Sep 10 10:13:56 2015 -0700
    
        Merge pull request #3911 from code-dot-org/better-send-animation
    
        NetSim: Better send animation
    
    commit e972bc2617f8fca69b3f026e8d4fe98fe42e37f0
    Author: Continuous Integration <dev@code.org>
    Date:   Thu Sep 10 17:10:52 2015 +0000
    
        content changes (elijah)
    
    commit 5b711cf63815100cae12b404e965e90b2878df2e
    Merge: c67481f 23d1f04
    Author: ashercodeorg <asher@code.org>
    Date:   Thu Sep 10 10:06:56 2015 -0500
    
        Merge pull request #3924 from code-dot-org/fix-contains-sampled-data
    
        Fixes syntax errors: changes containsSampledData to contains_sampled_data and changes throw to raise.
    
    commit 23d1f044c143745985470afd6b4baa52dd30d44a
    Author: Asher Kach <asher@code.org>
    Date:   Thu Sep 10 08:27:17 2015 -0500
    
        Changes throw to raise.
    
    commit c67481f1a0db36fef47b10208715c2dd00ea8fc7
    Merge: 01d8906 3ef907c
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Wed Sep 9 22:50:03 2015 -0700
    
        Merge pull request #3929 from code-dot-org/add-dashboard-host-option
    
        Add configurable dashboard_host option
    
    commit da70dab25e3f9ee2c12bc55110e71003e14408f2
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Sep 9 17:52:36 2015 -0700
    
        Explicitly type UI element map.
    
    commit c3f6e24a8a4b14b4848d81f831189dca11b2beb5
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Sep 9 17:10:03 2015 -0700
    
        Reuse positive modulo utility function.
    
    commit 3ef907c07754457a31d0fb8715c9e488a30d1b5c
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Wed Sep 9 17:01:30 2015 -0700
    
        Add configurable dashboard_host option
    
        `rackup`'s default host changed from `0.0.0.0` in 1.5.3 to `localhost` in
        1.6.4. It is useful to be able to optionally serve on `0.0.0.0`, and is
        nearly essential for anyone who developes from a VM.
    
        This commit adds the possibility of setting `dashboard_host: '0.0.0.0'`
        in `locals.yml` to regain the old functionality.
    
    commit 01d8906282552c855f2ab58a2e4caabc383ab0f3
    Merge: 8d35129 f54ca5d
    Author: Trevor Berg <trevor@code.org>
    Date:   Wed Sep 9 16:49:34 2015 -0700
    
        Merge pull request #3927 from code-dot-org/delete-table
    
        Add delete table functionality to the data browser
    
    commit 8d35129a5f3a6c8990d8cb769ffacd3942339c36
    Merge: 7089ee9 7250c68
    Author: Andre Stackhouse <CaptainStack@outlook.com>
    Date:   Wed Sep 9 16:34:21 2015 -0700
    
        Merge pull request #3928 from code-dot-org/ops-email-automation-deployment
    
        Enabling auto-sending of ops email reminders
    
    commit 3bf60caeb1d357fc0e7bd0e9657e32b139066613
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Sep 9 16:29:59 2015 -0700
    
        Animate formatting offset for hex.
    
    commit c67033b9d39e727f111a6ab42f67cdee76828001
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Sep 9 16:13:13 2015 -0700
    
        Animate formatting offset for A/B encoding
    
    commit a310ce50fb7ebd223d36322de75ebb4586cc736c
    Author: Brad Buchanan <brad@code.org>
    Date:   Wed Sep 9 16:06:22 2015 -0700
    
        Animate format offset for binary.

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72d7e0cf4c483215418a1ed36bb320f3f6176770e0ff10d31e98e3c2a1b99d4a
+oid sha256:c0a804da610a53ae33c5da63cecfba21b3caf5a4e4c8c38b6ad55fafc0b0a590
 size 73347
--- dashboard/public/apps-package/js/netsim-e7ddddaf45900f5f2ab8809ffc968160.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b15492aed8a873eaffbab9514314057b026f3718b9801111fb1fd68fcda2e06f
-size 1355385
