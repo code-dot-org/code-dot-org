d08777ef1372c4d14814b6535141c95ebfe96159
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-09 17:43:34 -0700

    s/JQuery/jQuery

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a754236b598636ff0bbbf079d7ad700ccd87f64e061d66bf4ef70e076e56231
+oid sha256:9172598e460a81129c026dae3c446d459f72b27ab271502faa0d1246888ebe0e
 size 27322
