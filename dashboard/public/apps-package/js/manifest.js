43da8b9af2b10d4ab7ed785a6fa536f8437a4bae
Continuous Integration <dev@code.org>
2015-09-14 22:06:41 +0000

    Automatically built.
    commit 569ce87ae29d49eac775869a021f78bc9e14cdc8
    Merge: 5d1e332 4d327b6
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Mon Sep 14 15:01:55 2015 -0700
    
        Merge pull request #3975 from code-dot-org/netsim-new-message-scroll
    
        Situationally prevents autoscrolling
    
    commit 5d1e3321fc379ebc160dcc8d278242431b0fa70e
    Author: mehalshah <mehal@code.org>
    Date:   Mon Sep 14 14:33:46 2015 -0700
    
        Update README.md
    
        Clarified what .profile needs to be edited

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81a9f575a950e639b255987ed1cb459c2e20df489386f1711930bf5ccce4c6e0
+oid sha256:c6b177da76af58e0eeb03d3a15bd716978a4bcfcc21f9d7ff3e5758c69849ba2
 size 73347
---
+++ dashboard/public/apps-package/js/netsim.min-47d1b01c6a54ee66033b3e8366eda824.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:39e0ab3701a6a4f97375257757dfe65edbf89a656cbe47cf06d2cc7befcf217d
+size 652499
