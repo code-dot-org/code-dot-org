0163663b67bbdaa145f01e4d9e6d7144ddb04a53
Laurel Fan <laurel@code.org>
2015-09-15 13:12:46 -0700

    chomp so things actually work

--- tools/hooks/lint.rb
+++ tools/hooks/lint.rb
@@ -3,14 +3,14 @@ APPS_DIR = "#{REPO_DIR}/apps"
 
 def get_modified_files
   Dir.chdir REPO_DIR
-  `git diff --cached --name-only`.split('\n')
+  `git diff --cached --name-only`.split('\n').map(&:chomp)
 end
 
 def filter_grunt_jshint(modified_files)
   modified_files.select { |f| f.start_with?(APPS_DIR) and f.end_with?(".js") }
 end
 
-def filter_rubicop(modified_files)
+def filter_rubocop(modified_files)
   modified_files.select { |f| f.end_with?(".rb") }
 end
 
@@ -38,14 +38,14 @@ end
 
 modified_files = get_modified_files()
 puts "\nFound #{modified_files.length} modified files", "-" * 30
-puts modified_files
+p modified_files
 
 jshint_files = filter_grunt_jshint(modified_files)
 puts "\nRunning #{jshint_files.length} files through grunt jshint", "-" * 30
 puts jshint_files
 raise "JSHint failed" unless jshint_files.empty? or run_jshint(jshint_files)
 
-ruby_files = filter_rubicop(modified_files)
+ruby_files = filter_rubocop(modified_files)
 puts "\nRunning #{ruby_files.length} through rubocop", "-" * 30
 puts ruby_files
 raise "Rubocop failed" unless ruby_files.empty? or run_rubocop(ruby_files)
