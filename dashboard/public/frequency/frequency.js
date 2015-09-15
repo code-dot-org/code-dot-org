f77689bf315758811c1e6521c678d640fc640775
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-15 13:11:35 -0700

    Multiple layout changes
    - turned control toggle buttons into tabs
    - moved message data sort into tab
    - moved legend to above graph and replaced HTML h4 with SVG text
    - generalized sorting

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd3df48730d0ab896487901691c5cd1142cd82934f1bc3538180c94fbe5a520c
-size 30757
+oid sha256:872cf53ab115aadbfe3e58e74414fb28cebacb80a6ac8cdec8f26b6bb4ecb96e
+size 34102
