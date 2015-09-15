b52b1333bff8e61e64f08d25233fa5539ed466ab
Trevor Berg <trevor@code.org>
2015-09-14 21:50:57 -0700

    Change levels.all to maze.all

--- dashboard/scripts/maze_show_step_button.rb
+++ dashboard/scripts/maze_show_step_button.rb
@@ -7,8 +7,7 @@ require_relative '../config/environment'
 
 puts "Starting"
 
-mazes = Level.all.select { |x| x.is_a? Maze }
-mazes_without_step = mazes.select{|x| x.step_mode == 0}
+mazes_without_step = Maze.all.select{|x| x.step_mode == 0}
 puts "Found #{mazes_without_step.length} levels to update"
 
 mazes_without_step.each do |m|
