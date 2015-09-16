ad6f696586eefb62af920ecf54836ac5d3da8761
Continuous Integration <dev@code.org>
2015-09-16 21:36:35 +0000

    Automatically built.
    commit e1eda2ce8800912ff571943fa70aa09001a78206
    Merge: bf6638b 940357f
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Wed Sep 16 14:31:53 2015 -0700
    
        Merge pull request #3944 from code-dot-org/assets-quota
    
        Enforce assets quota limits.
    
    commit 940357f87ed95bbd6fded0a23b851bb6c07af04f
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Sep 15 14:46:00 2015 -0700
    
        properly stub max file/app size
    
    commit 6ecaef79ac9cb2adb956092bdab4f1dc1cc77101
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Sep 15 14:13:33 2015 -0700
    
        add tests for assets quota
    
    commit e7ea2e280387f7f5c4aa9cbbbc2b07330a969cdb
    Author: David Bailey <dsb123@gmail.com>
    Date:   Thu Sep 10 15:58:03 2015 -0700
    
        fix rubocop errors and address code review feedback
    
    commit 33fdf8c4da2a325652128284b8ac99e4a078a3d6
    Author: David Bailey <dsb123@gmail.com>
    Date:   Thu Sep 10 15:46:45 2015 -0700
    
        [finishes #95124100] Enforce assets quota limits.

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4b5c8a695f20e9b30db688d22f63658b09d835a6bf3b717d00a91de98fab369
+oid sha256:4ad308a5ee66d2145a433eb092b2dd465ab7420e486d71a90236a41c925f521e
 size 73347
