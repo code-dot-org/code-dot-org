eeb430b7ceeb5e850e9f30180985817a56c9a017
Continuous Integration <dev@code.org>
2015-09-16 20:47:38 +0000

    Automatically built.
    commit 08a3acaf3b09dfa6c32c81838c2357d105f2bbce
    Merge: fef17dc e90a335
    Author: Brendan Reville <brendan@code.org>
    Date:   Wed Sep 16 13:42:35 2015 -0700
    
        Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging
    
    commit fef17dc65438a87b80e385a9dbca054141c26323
    Author: Brendan Reville <brendan@code.org>
    Date:   Wed Sep 16 13:41:43 2015 -0700
    
        Add dialog-title CSS class to markdown dialogs.
        This will help some UI tests to pass.  When the levels were changed to use markdown dialogs they stopped passing because the UI tests expected this CSS class.

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8a1eb599d0f6e1b2face90d34665864c32be3ca4da5c152c93eee122835b42c
+oid sha256:e4b5c8a695f20e9b30db688d22f63658b09d835a6bf3b717d00a91de98fab369
 size 73347
