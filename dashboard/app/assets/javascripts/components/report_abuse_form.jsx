26fea515faab06db994494a1415d2f7f15cb3938
Brent Van Minnen <bjvanminnen@gmail.com>
2015-09-17 10:29:11 -0700

    i18n for abuse form

--- dashboard/app/assets/javascripts/components/report_abuse_form.jsx
+++ dashboard/app/assets/javascripts/components/report_abuse_form.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f793838ba3e629597e20249dab4247dfef29a6e458d26ed7f63bff1d04267d92
-size 4637
+oid sha256:7cc3077ff7968f64b521d77707564dce134ad8befba5ba2ba2e41fd610e04bdd
+size 6790
