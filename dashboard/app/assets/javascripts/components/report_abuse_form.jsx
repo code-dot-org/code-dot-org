5f73b1485a4f56fc267e8808aa5b5f8a2210fc5b
Brent Van Minnen <bjvanminnen@gmail.com>
2015-09-17 11:06:19 -0700

    i18n from dashboard

--- dashboard/app/assets/javascripts/components/report_abuse_form.jsx
+++ dashboard/app/assets/javascripts/components/report_abuse_form.jsx
@@ -11,58 +11,6 @@ window.dashboard.ReportAbuseForm = (function (React) {
   // padding)
   var DROPDOWN_WIDTH = 514;
 
-  // TODO - temp i18n
-  var strs = {
-    submit: 'Submit',
-    report_abuse: {
-      validation: {
-        email: 'Please provide an email address',
-        age: 'Please specify an age',
-        abuseType: 'Please answer how this content violates the Terms of Service',
-        abuseDetail: 'Please provide details regarding the content you are reporting'
-      },
-      email: 'Email',
-      age: 'Age',
-      abusiveUrl: 'Abusive URL',
-      abuseType: {
-        question: 'How does this content violate the %{link_start}Terms of Service%{link_end}?',
-        harassment: 'Threats, cyberbullying, harassement',
-        offensive: 'Offensive content',
-        infringement: 'Copyright Infringement',
-        other: 'Other'
-      },
-      detail: 'Please provide as much detail as possible regarding the content you are reporting.',
-      acknowledge: 'By submitting this information, you acknowledge it will be handled in accordance with the terms of the %{link_start_privacy}Privacy Policy%{link_end} and the %{link_start_tos}Terms of Service%{link_end}'
-    }
-  };
-
-  var i18n = {
-    /**
-     * Mimics dashboard's I18n.t, getting a string from the hierarchy
-     * @param {string} selector Selector the desired string, i.e. "project.abuse.tos"
-     */
-    t: function (selector, params) {
-      var hierarchy = selector.split('.');
-      var current = strs;
-      hierarchy.forEach(function(item) {
-        current = current[item];
-        if (!current) {
-          throw new Error('i18n failed to find string: ' + selector);
-        }
-      });
-      if (typeof(current) !== 'string') {
-        throw new Error('i18n not a leaf: ' + selector);
-      }
-
-      Object.keys(params || {}).forEach(function (key) {
-        current = current.replace(new RegExp('%{' + key + '}', 'g'), params[key]);
-      });
-
-      return current;
-    }
-  };
-
-
   /**
    * A dropdown with the set of ages we use across our site (4-20, 21+)
    */
@@ -93,6 +41,7 @@ window.dashboard.ReportAbuseForm = (function (React) {
 
   return React.createClass({
     propTypes: {
+      i18n: React.PropTypes.object.isRequired,
       csrfToken: React.PropTypes.string.isRequired,
       abuseUrl: React.PropTypes.string.isRequired,
       name: React.PropTypes.string,
@@ -109,68 +58,73 @@ window.dashboard.ReportAbuseForm = (function (React) {
       return match && match[1];
     },
 
-    handleSubmit: function () {
+    handleSubmit: function (event) {
+      var i18n = this.props.i18n;
       if (React.findDOMNode(this.refs.email).value === '') {
-        alert(i18n.t('report_abuse.validation.email'));
-        return false;
+        alert(i18n.t('project.abuse.report_abuse_form.validation.email'));
+        event.preventDefault();
+        return;
       }
 
       if (React.findDOMNode(this.refs.age).value === '') {
-        alert(i18n.t('report_abuse.validation.age'));
-        return false;
+        alert(i18n.t('project.abuse.report_abuse_form.validation.age'));
+        event.preventDefault();
+        return;
       }
 
-      if (React.findDOMNode(this.refs.abuseType).value === '') {
-        alert(i18n.t('report_abuse.validation.abuseType'));
-        return false;
+      if (React.findDOMNode(this.refs.abuse_type).value === '') {
+        alert(i18n.t('project.abuse.report_abuse_form.validation.abuse_type'));
+        event.preventDefault();
+        return;
       }
 
-      if (React.findDOMNode(this.refs.abuseDetail).value === '') {
-        alert(i18n.t('report_abuse.validation.abuseDetail'));
-        return false;
+      if (React.findDOMNode(this.refs.abuse_detail).value === '') {
+        alert(i18n.t('project.abuse.report_abuse_form.validation.abuse_detail'));
+        event.preventDefault();
+        return;
       }
-      return true;
     },
 
     render: function () {
+      var i18n = this.props.i18n;
       return (
-        <form action="/zendesk_report_abuse" method="post">
+        <form action="/report_abuse" method="post">
           <input type="hidden" name="authenticity_token" value={this.props.csrfToken}/>
           <input type="hidden" name="channel_id" value={this.getChannelId()}/>
           <input type="hidden" name="name" value={this.props.name}/>
           <div style={{display: this.props.email ? 'none' : 'block'}}>
-            <div>{i18n.t('report_abuse.email')}</div>
+            <div>{i18n.t('activerecord.attributes.user.email')}</div>
             <input type="text" style={{width: INPUT_WIDTH}} defaultValue={this.props.email} name="email" ref="email"/>
           </div>
 
           <div style={{display: this.props.age ? 'none' : 'block'}}>
-            <div>{i18n.t('report_abuse.age')}</div>
+            <div>{i18n.t('activerecord.attributes.user.age')}</div>
             <AgeDropdown age={this.props.age} ref="age"/>
           </div>
 
-          <div>{i18n.t('report_abuse.abusiveUrl')}</div>
+          <div>{i18n.t('project.abuse.report_abuse_form.abusive_url')}</div>
           <input type="text" readOnly style={{width: INPUT_WIDTH}} defaultValue={this.props.abuseUrl} name="abuse_url"/>
 
           {/* we dangerouslySetInnerHTML because our string has html in it*/ }
           <div dangerouslySetInnerHTML={{
-            __html: i18n.t('report_abuse.abuseType.question', {
+            __html: i18n.t('project.abuse.report_abuse_form.abuse_type.question', {
               link_start: '<a href="http://code.org/tos" target="_blank">',
               link_end: '</a>'
             })}}/>
-          <select style={{width: DROPDOWN_WIDTH}} name="abuse_type" ref="abuseType">
+          <select style={{width: DROPDOWN_WIDTH}} name="abuse_type" ref="abuse_type">
             <option value=""></option>
-            <option value="harassment">{i18n.t('report_abuse.abuseType.harassment')}</option>
-            <option value="offensive">{i18n.t('report_abuse.abuseType.offensive')}</option>
-            <option value="infringement">{i18n.t('report_abuse.abuseType.infringement')}</option>
-            <option value="other">{i18n.t('report_abuse.abuseType.other')}</option>
+            <option value="harassment">{i18n.t('project.abuse.report_abuse_form.abuse_type.harassment')}</option>
+            <option value="offensive">{i18n.t('project.abuse.report_abuse_form.abuse_type.offensive')}</option>
+            <option value="infringement">{i18n.t('project.abuse.report_abuse_form.abuse_type.infringement')}</option>
+            <option value="other">{i18n.t('project.abuse.report_abuse_form.abuse_type.other')}</option>
           </select>
 
-          <div>{i18n.t('report_abuse.detail')}</div>
-          <textarea style={{width: INPUT_WIDTH, height: 100}} name="abuse_detail" ref="abuseDetail"/>
+          <div>{i18n.t('project.abuse.report_abuse_form.detail')}</div>
+          <textarea style={{width: INPUT_WIDTH, height: 100}} name="abuse_detail" ref="abuse_detail"/>
 
           {/* we dangerouslySetInnerHTML because our string has html in it*/ }
           <div dangerouslySetInnerHTML={{
-            __html: i18n.t('report_abuse.acknowledge', {
+            __html: i18n.t('project.abuse.report_abuse_form.acknowledge', {
               link_start_privacy: '<a href="http://code.org/privacy" target="_blank">',
               link_start_tos: '<a href="http://code.org/tos" target="_blank">',
               link_end: '</a>'
