ac95e2d3af98bc66117f134eab790eed207fdfac
Geoffrey Elliott <geo@madronalabs.co>
2015-09-17 11:33:09 -0700

    Fix routing for /de/resources and /de/de/resources

--- pegasus/helpers/hourofcode_helpers.rb
+++ pegasus/helpers/hourofcode_helpers.rb
@@ -69,11 +69,8 @@ def hoc_canonicalized_i18n_path(uri)
   end
 
   # We no longer want the country to be part of the path we use to search:
-  _, search_uri = uri.split('/', 2)
-  search_uri = File.join('/', search_uri)
-
-  path = uri if resolve_document(search_uri)
-
+  search_uri = File.join('/', [@language, path].compact)
+  return search_uri if resolve_document(search_uri)
   return "/#{path}"
 end
 
