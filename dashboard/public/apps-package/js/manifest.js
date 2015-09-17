052c88f20b8c5c088f1356bcb3babf97d178b0bb
Continuous Integration <dev@code.org>
2015-09-17 18:25:36 +0000

    Automatically built.
    commit 5ee2be59259c498131bf583a7edd5d5224f47fc9
    Merge: b549e5c f1575bb
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Sep 17 11:21:30 2015 -0700
    
        Merge pull request #4014 from code-dot-org/kv-populate
    
        Add the ability to seed KV data from level builder
    
    commit f1575bb525fea69985acc066f38bf4a6d8d53b29
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Sep 17 11:19:43 2015 -0700
    
        Perf improvement
    
    commit 30ba4c074b051a6ff99d9bab6d1978f86defd427
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Sep 17 11:08:15 2015 -0700
    
        Kv -> Key/Value && data_kv -> data_proprties
    
    commit b549e5cbfd315e79fb9f221d322544621b3b1162
    Merge: 3282336 54593c2
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Sep 17 10:31:42 2015 -0700
    
        Merge pull request #4021 from code-dot-org/fix-tests
    
        Fix exists check for mysql data tables

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dac0cbcd11344463088856abae51bf9b85306c5c5aac4302d147d954869fea00
+oid sha256:f2aa74065d4243276e89e9e62fda980faff6cde93b7f76615df87a1cc62a622e
 size 73347
