566731d69f92908cb24db1fc9314ae0079397291
Continuous Integration <dev@code.org>
2015-09-17 01:06:41 +0000

    Automatically built.
    commit 003d616d1143a4ab2877107f20a34676bcb1f1c7
    Merge: 25ab7ca 6710a55
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Sep 16 18:02:29 2015 -0700
    
        Merge pull request #3999 from code-dot-org/big-game-start-over
    
        [Finishes #101159920] "Delete Progress" option for Big Game
    
    commit 6710a55739151be94965b6ad5afd4123538f3534
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Sep 16 17:14:19 2015 -0700
    
        Add UI test for versions
    
    commit 1b0b080b824fa488e958d594a87aed75eba4496b
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Sep 16 16:26:45 2015 -0700
    
        Fix running UI tests locally
    
    commit 25ab7ca9ab4c1b58902a4ca2c0fd7f7eca934213
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Sep 16 16:26:45 2015 -0700
    
        Fix running UI tests locally
    
    commit bc14b5aec27826041dd29e28f59f324c461323af
    Merge: d03aed1 5c4c53a
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Wed Sep 16 16:12:46 2015 -0700
    
        Merge pull request #4003 from code-dot-org/data-browser-auth
    
        restrict applab data browser pages to channel owner
    
    commit 5c4c53a3299ba871a65574001440581ebc2dcaea
    Author: David Bailey <dsb123@gmail.com>
    Date:   Wed Sep 16 16:11:32 2015 -0700
    
        add comment about ruby warnings
    
    commit e1adc62174c4afd0b113056f052e959a7da7f35c
    Author: Josh Lory <josh.lory@code.org>
    Date:   Wed Sep 16 15:51:00 2015 -0700
    
        Deleting progress should always cut a new version
    
    commit d03aed1a739231cf801c6717a3d7b764dd36077d
    Author: Laurel Fan <laurel@code.org>
    Date:   Wed Sep 16 15:49:46 2015 -0700
    
        ok ipad does take more than 2 minutes to connect

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ad308a5ee66d2145a433eb092b2dd465ab7420e486d71a90236a41c925f521e
+oid sha256:dac0cbcd11344463088856abae51bf9b85306c5c5aac4302d147d954869fea00
 size 73347
