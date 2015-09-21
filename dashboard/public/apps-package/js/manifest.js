7e66538340f12e73f29e474e84a3eff6a668473a
Continuous Integration <dev@code.org>
2015-09-21 20:25:29 +0000

    Automatically built.
    commit b7f749652813ed3a3cdb796fbb67bffb8f2c21df
    Merge: 47f3b78 f1b2f94
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Mon Sep 21 13:18:38 2015 -0700
    
        Merge pull request #4081 from code-dot-org/netsim-bit-mode-fix
    
        NetSim: Send a single bit when pressing enter in bit-sending levels
    
    commit 47f3b78a9911d3d4b18869ded4fdde87beae44ba
    Author: Continuous Integration <dev@code.org>
    Date:   Mon Sep 21 20:16:28 2015 +0000
    
        Automatically built.
    
        commit 8298e2ade8c288c8130b1eac909b5d7de6be0773
        Author: Josh Lory <josh.lory@code.org>
        Date:   Mon Sep 21 13:11:50 2015 -0700
    
            Fix mochaTest
    
    commit 8298e2ade8c288c8130b1eac909b5d7de6be0773
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon Sep 21 13:11:50 2015 -0700
    
        Fix mochaTest
    
    commit f1b2f9403c74624d052ea639944102f4487c9031
    Author: Brad Buchanan <brad@code.org>
    Date:   Mon Sep 21 13:02:39 2015 -0700
    
        Remove spaces after describe/it/beforeEach because they violate style guide.
    
    commit e84008de1ec34f4d3815ede3f8df4d842e5cb103
    Author: Brad Buchanan <brad@code.org>
    Date:   Mon Sep 21 13:00:00 2015 -0700
    
        Triple-equals check for keyCode

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f817ac54029524bf1901c13edfa82f5b9881cba95b43bbf7214c83988bf2114
+oid sha256:be06359739528ed9e40b4cc3f6cd971534d10e9e1deed7ebd78e122e365deed7
 size 73347
