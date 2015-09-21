085a33a5ab6b14bbc2d7787ae41bb150be6131df
Continuous Integration <dev@code.org>
2015-09-21 20:53:33 +0000

    Automatically built.
    commit 2b127d701f3a0a834ac16e66d2f3e2f35f8af2b8
    Author: Josh Lory <josh.lory@code.org>
    Date:   Mon Sep 21 13:29:12 2015 -0700
    
        Use the current host's media proxy

---
+++ dashboard/public/apps-package/js/applab-2622845a3b54e8841afe092039728168.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:243d1cb91b5d39b4eb81afdb838e19ce6a7dca797b286f16211dbdb3ce393afd
+size 395070
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be06359739528ed9e40b4cc3f6cd971534d10e9e1deed7ebd78e122e365deed7
+oid sha256:16a1d3f4b558f7eb49f79b3de50c48b80f9585c080364872369b55d85504e33b
 size 73347
