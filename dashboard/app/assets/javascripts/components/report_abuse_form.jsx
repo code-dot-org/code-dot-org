8b95b62fd23764b7a65f77249fe8d22d6bf6494a
Brent Van Minnen <bjvanminnen@gmail.com>
2015-09-21 10:01:18 -0700

    update strings

--- dashboard/app/assets/javascripts/components/report_abuse_form.jsx
+++ dashboard/app/assets/javascripts/components/report_abuse_form.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fc5baba6393396b0f64d7bcf3387d3d28eb7f5e3e7c248968e6c8179b2bd401
-size 5465
+oid sha256:26794a57b671b8e0ad30ab02feeb7f75bf6fe86ea249b63c905f28b30cf0b6a5
+size 5740
