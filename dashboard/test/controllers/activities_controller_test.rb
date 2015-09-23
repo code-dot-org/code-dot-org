5811114eb05ea4d13f026891e71339a4ddcf25de
Phil Bogle <phil@code.org>
2015-09-23 11:28:48 -0700

    activities_controller_test merge fix

--- dashboard/test/controllers/activities_controller_test.rb
+++ dashboard/test/controllers/activities_controller_test.rb
@@ -9,11 +9,7 @@ class ActivitiesControllerTest < ActionController::TestCase
   include Mocha::API
 
   setup do
-<<<<<<< HEAD
-    session_reset
-=======
     client_state.reset
->>>>>>> user_session_helpers
 
     LevelSourceImage # make sure this is loaded before we mess around with mocking S3...
     CDO.disable_s3_image_uploads = true # make sure image uploads are disabled unless specified in individual tests
