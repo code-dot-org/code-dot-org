61a62d03c065850d7d97c35c0da218f89d9a087b
Phil Bogle <phil@code.org>
2015-09-23 17:26:06 -0700

    Checkpoint work in progress

--- dashboard/lib/image_lib.rb
+++ dashboard/lib/image_lib.rb
@@ -1,25 +1,41 @@
+require 'mini_magick'
+
 module ImageLib
 
-  def self.overlay_image(params)
-    if params[:background_url].nil?
-      background = Magick::ImageList.new
-      background.from_blob(params[:background_blob])
-    else
-      background = Magick::ImageList.new(params[:background_url])
+# Overlay a foreground and background image as described by params, where
+# the background image is resized to 154x154 pixels and centered.
+#
+# @param [Hash] parameters
+# params[:background_blob] - A binary string for the background image.
+# params[:background_url]  - A local file path for the background image.
+# params[:foreground_blob] - A binary string for the foreground image.
+# params[:foreground_url]  - A local file path for the foreground image.
+#
+# @return [RMagick::ImageList]
+def self.overlay_image(params)
+    background = read_or_open_image(params[:background_blob], params[:background_url])
+    foreground = read_or_open_image(params[:foreground_blob], params[:foreground_url])
+
+    background.combine_options do |c|
+      c.gravity = 'center'
+      c.geometry = '154x154+0+0'
     end
 
-    if params[:foreground_url].nil?
-      foreground = Magick::ImageList.new
-      foreground.from_blob(params[:foreground_blob])
-    else
-      foreground = Magick::ImageList.new(params[:foreground_url])
+    drawing_on_background = background.composite(foreground) do |c|
+      c.compose "Over" # OverCompositeOp
     end
 
-    background.gravity = Magick::CenterGravity
-    background.geometry = '154x154+0+0'
-    drawing_on_background = background.composite_layers(foreground, Magick::OverCompositeOp)
-    drawing_on_background.format = 'png'
     drawing_on_background
+  end
+
+  private
+
+  def read_or_open_image(blob, url)
+    if url.nil?
+      MiniMagick::Image.read(blob)
+    else
+      MiniMagick::Image.open(url)
+    end
   end
 
 end
