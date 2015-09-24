d4efef4e57f15a07532f8a13566e857ef55e087c
Continuous Integration <dev@code.org>
2015-09-24 22:24:25 +0000

    Automatically built.
    commit 1d43e05a1e152600dd79fe4a0a67f01160a22218
    Merge: 44124bb 9c1d139
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Thu Sep 24 15:20:18 2015 -0700
    
        Merge pull request #4129 from code-dot-org/fix-applab-auth
    
        Re-enable data browser auth checks and corresponding test.
    
    commit 44124bb08d837c3fb758a4e1158f3fd7dc008b1e
    Merge: f6356bd 440acee
    Author: Andre Stackhouse <CaptainStack@outlook.com>
    Date:   Thu Sep 24 15:19:30 2015 -0700
    
        Merge pull request #4175 from code-dot-org/test
    
        Dashboard email template hotfix back-merge to staging
    
    commit 440acee8d81b4297deba866e5623eefade7f586c
    Merge: 97a5fe7 5ff174b
    Author: Andre Stackhouse <CaptainStack@outlook.com>
    Date:   Thu Sep 24 15:18:00 2015 -0700
    
        Merge pull request #4174 from code-dot-org/production
    
        Hotfix back-merge to test
    
    commit 5ff174be6a932c51656132f5572cf3e62bd1a861
    Merge: c545367 5d1657c
    Author: Andre Stackhouse <CaptainStack@outlook.com>
    Date:   Thu Sep 24 15:13:32 2015 -0700
    
        Merge pull request #4172 from code-dot-org/revert-dashboard-email-template-update
    
        Rolling back update to this template file. Causing poste errors.
    
    commit 5d1657c984f30f8991dec983d728592c10384cc8
    Author: CaptainStack <CaptainStack@outlook.com>
    Date:   Thu Sep 24 15:09:01 2015 -0700
    
        Rolling back update to this template file. Causing poste errors.
    
    commit 9c1d139577d6ad4bcf7d451b8d0a064bed40880e
    Author: David Bailey <dsb123@gmail.com>
    Date:   Thu Sep 24 13:10:22 2015 -0700
    
        add /v3 prefix to edit-csp-* paths
    
    commit 8cd9444ff4244f67b7a3d868cb07dd13ef06387a
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Sep 22 19:27:44 2015 -0700
    
        fix and extend EditCspTest
    
    commit daab78a72530d4e0c22a9d574bd20689465b1585
    Author: David Bailey <dsb123@gmail.com>
    Date:   Mon Sep 21 12:58:35 2015 -0700
    
        Revert "disable applab data browser auth checks"
    
        This reverts commit 7b03834486ce95c30bf8b8faff65e0ed56bb9dd6.
    
    commit f407434b7f269b1deefdc3967a5c1d491d8648cd
    Author: David Bailey <dsb123@gmail.com>
    Date:   Mon Sep 21 12:57:16 2015 -0700
    
        Revert "temporarily remove test_edit_csp to unblock deploy."
    
        This reverts commit 9243de7fd42a0133f7d38fb68003afaa458ab137.
    
    commit f6356bdf081233c7f20f0399b37b43683dded959
    Merge: d621f96 da7cbbd
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Sep 24 14:53:42 2015 -0700
    
        Merge pull request #4165 from code-dot-org/data-browser-changes
    
        A couple changes to the UI of the table data browser
    
    commit c5453677984f9e8a8511d89da530cc18d856aa22
    Merge: b00eb2d 97a5fe7
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Thu Sep 24 14:47:30 2015 -0700
    
        Merge pull request #4171 from code-dot-org/test
    
        DTP
    
    commit 97a5fe7c1afb4d2c1f8223f88d97a32a07ca6437
    Merge: 7d76c88 d621f96
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Thu Sep 24 13:24:30 2015 -0700
    
        Merge pull request #4164 from code-dot-org/staging
    
        DTT
    
    commit da7cbbdb9ab81e430a438f1662895c37728d30b2
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Sep 24 13:19:42 2015 -0700
    
        A couple changes to the UI of the table data browser
    
    commit d621f96c19f33a456595feade3ef32d8dfda784e
    Merge: 1870307 5acfce0
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Sep 24 13:03:34 2015 -0700
    
        Merge pull request #4163 from code-dot-org/fixes
    
        real fix for markdown issue
    
    commit 5acfce0fdc8cfd72ac9489f294abd608d246a286
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Sep 24 12:34:48 2015 -0700
    
        real fix for markdown issue

---
+++ dashboard/public/apps-package/js/applab-2dde9bc54ad46dbc824adfe6c5a1178a.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a0d362a0678c83dad2088bfd2d8e6e9cf975273676bde3e4b71a23b0ea043db
+size 398060
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cf37322b4c0c28daeddb76aeac3757f36a9acfc17e02b312b8104d8b8d7e89a
+oid sha256:1dccfd61614113aca42cfdf63232b4b8b4ccd74e237056217df0eed1b46dd24d
 size 73347
