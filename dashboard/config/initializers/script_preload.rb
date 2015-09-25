1a8dc4c9000a5cf39e6d267499c0c936f91d30cb
Phil Bogle <phil@code.org>
2015-09-24 22:28:33 -0700

    work in progress

--- dashboard/config/initializers/script_preload.rb
+++ dashboard/config/initializers/script_preload.rb
@@ -2,7 +2,8 @@
 # This speeds up load time of new Unicorn child worker processes
 # and Spring application preloader (Rails console, unit tests).
 
-# Skip if this is running a Rake task (e.g. rake db:setup)
-unless File.basename($0) == 'rake'
+# Skip if this is running a Rake task (e.g. rake db:setup), or we're in
+# test mode and the FAST_TEST environment variable is set.
+unless File.basename($0) == 'rake' || (ENV['FAST_TEST'] && Rails.env.test?)
   Script.script_cache
 end
--- dashboard/lib/image_lib.rb
+++ dashboard/lib/image_lib.rb
@@ -19,8 +19,8 @@ module ImageLib
   # @throws ArgumentError if neither the _blob or _url parameter is provided
   #         for the foreground and background.
   def self.overlay_image(opts)
-    background = read_or_open_image(opts[:background_blob], opts[:background_url])
-    foreground = read_or_open_image(opts[:foreground_blob], opts[:foreground_url])
+    background = get_image(opts[:background_blob], opts[:background_url])
+    foreground = get_image(opts[:foreground_blob], opts[:foreground_url])
 
     background.geometry('154x154+0+0')
 
@@ -32,56 +32,12 @@ module ImageLib
 
   private
 
-  def self.read_or_open_image(blob, path)
-    puts "read_or_open_image(#{(blob || "").length}, #{path})"
+  def self.get_image(blob, path)
     if blob.present?
-      raise 'Invalid blob type' unless blob.is_a?(String)
-      puts "encoding=#{blob.encoding}"
       MiniMagick::Image.read(blob)
-    elsif path.present?
-      MiniMagick::Image.open(path)
     else
-      raise ArgumentError, 'Either blob or url is required for the foreground and background.'
+      MiniMagick::Image.open(path)
     end
   end
-
-  def self.test_overlay_image
-    fg_blob = test_image('foreground_overlay.png').to_blob
-    bg_url = test_image_path('blank_sharing_drawing_anna.png')
-    framed_image = ImageLib::overlay_image(
-        background_url: bg_url, foreground_blob: fg_blob)
-
-    expected_image_name = 'expected_overlaid_image.png'
-    expected_image_path = test_image_path(expected_image_name)
-    expected_image = test_image(expected_image_name)
-
-    date = Time.now.strftime("%Y-%m-%d-%H%M%S")
-    generated_image_path = "/tmp/generated_image_#{date}.png"
-
-    File.open(generated_image_path, 'w') do |file|
-      file.write(framed_image)
-    end
-
-    diff = MiniMagick::Tool::Compare.new do |compare|
-      compare.metric('ae')
-      compare << expected_image_path
-      compare << generated_image_path
-      puts compare.inspect
-    end
-
-    puts "Actual image: #{generated_image_path}"
-    puts "Expected image: #{expected_image_path}."
-
-    raise "Overlaid image did not match expected value" unless diff == 0
-  end
-
-  def self.test_image_path(name)
-    "/Users/pbogle/code-dot-org/dashboard/test/fixtures/#{name}"
-  end
-
-  def self.test_image(name)
-    MiniMagick::Image.open(test_image_path(name))
-  end
-
 
 end
