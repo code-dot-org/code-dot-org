3d1da5d58584f6d88f14da24239fb9351e3c9a6f
Continuous Integration <dev@code.org>
2015-09-25 23:06:54 +0000

    Automatically built.
    commit cf4210bec056f870d0969253710af921158f6d1c
    Merge: 723166e 49413c8
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Fri Sep 25 15:57:27 2015 -0700
    
        Merge pull request #4209 from code-dot-org/blockly-tune-autoscrolling
    
        Blockly: Tune autoscrolling
    
    commit 723166ece7d3c9b2ad6faedeef15b53c009ba7f5
    Author: Laurel Fan <laurel@code.org>
    Date:   Fri Sep 25 15:38:26 2015 -0700
    
        turns it ipad is way slower if you need to get a new ipad for every feature
    
    commit d0b4cd4ec13c6f24809f525d3d8d0d8d16e74c1b
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Sep 25 22:39:26 2015 +0000
    
        Automatically built.
    
        commit ce123bf4a10aff9431352f2f31c017373baf9803
        Merge: 99a9ac2 eb18b6e
        Author: davidsbailey <davidsbailey@users.noreply.github.com>
        Date:   Fri Sep 25 15:35:23 2015 -0700
    
            Merge pull request #4218 from code-dot-org/revert-4210-applab-ignore-html
    
            Revert "Fix canvas issues preventing turtle from drawing lines with r…
    
        commit eb18b6e9476576cfc6e3edeb7864e5988c178925
        Author: davidsbailey <davidsbailey@users.noreply.github.com>
        Date:   Fri Sep 25 15:28:36 2015 -0700
    
            Revert "Fix canvas issues preventing turtle from drawing lines with rounded ends"
    
    commit 12c6fe57fb8e15fbdaef29bdc3380532f29efcf4
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Sep 25 22:35:30 2015 +0000
    
        Automatically built.
    
        commit ce123bf4a10aff9431352f2f31c017373baf9803
        Merge: 99a9ac2 eb18b6e
        Author: davidsbailey <davidsbailey@users.noreply.github.com>
        Date:   Fri Sep 25 15:35:23 2015 -0700
    
            Merge pull request #4218 from code-dot-org/revert-4210-applab-ignore-html
    
            Revert "Fix canvas issues preventing turtle from drawing lines with r…
    
        commit eb18b6e9476576cfc6e3edeb7864e5988c178925
        Author: davidsbailey <davidsbailey@users.noreply.github.com>
        Date:   Fri Sep 25 15:28:36 2015 -0700
    
            Revert "Fix canvas issues preventing turtle from drawing lines with rounded ends"

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28efd0def68583c0c08235a739fd9014c7fa67cbd03136a5a08803864e2cea86
+oid sha256:7ec523cbad55dff35aaebb33791b5269c376bcd7fd001e8b15f11e607d91dc51
 size 73347
