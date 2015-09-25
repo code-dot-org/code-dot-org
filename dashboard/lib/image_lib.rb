ae4b390252ffb1a1af27e0ddbf933c5b1a874baf
Phil Bogle <phil@code.org>
2015-09-25 00:01:22 -0700

    opts -> params

--- dashboard/lib/image_lib.rb
+++ dashboard/lib/image_lib.rb
@@ -3,14 +3,14 @@ require 'mini_magick/image'
 
 module ImageLib
 
-  # Overlay a foreground and background image as described by opts, where
+  # Overlay a foreground and background image as described by params, where
   # the background image is resized to 154x154 pixels and centered.
   #
   # @param [Hash] parameters
-  #   opts[:background_blob] - A binary string for the background image.
-  #   opts[:background_url]  - A local file path for the background image.
-  #   opts[:foreground_blob] - A binary string for the foreground image.
-  #   opts[:foreground_url]  - A local file path for the foreground image.
+  #   params[:background_blob] - A binary string for the background image.
+  #   params[:background_url]  - A local file path for the background image.
+  #   params[:foreground_blob] - A binary string for the foreground image.
+  #   params[:foreground_url]  - A local file path for the foreground image.
   #
   # @return [String] The new image as a blob.
   #
@@ -18,9 +18,9 @@ module ImageLib
   # @throws MiniMagic::Error if a minimagic error occurs.
   # @throws ArgumentError if neither the _blob or _url parameter is provided
   #         for the foreground and background.
-  def self.overlay_image(opts)
-    background = get_image(opts[:background_blob], opts[:background_url])
-    foreground = get_image(opts[:foreground_blob], opts[:foreground_url])
+  def self.overlay_image(params)
+    background = get_image(params[:background_blob], params[:background_url])
+    foreground = get_image(params[:foreground_blob], params[:foreground_url])
 
     background.geometry('154x154+0+0')
 
