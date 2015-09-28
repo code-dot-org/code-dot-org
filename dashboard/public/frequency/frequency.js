21392539513ac87c297600a771f962431914cd29
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-09-28 11:03:52 -0700

    nitfix: s/appOptions/appOptions.level

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf255fd24ead7582566751a45c4254be779cf6fcd77d9798025cabf86e68de73
-size 36852
+oid sha256:dffe0a25ca2b57d4f6b1787241e7068f8f71fd223e2a38df2c2368b1ad432b67
+size 36858
