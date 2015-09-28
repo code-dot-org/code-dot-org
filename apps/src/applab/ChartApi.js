feb193ce75736c1f742be909571393bf64bcb800
Brad Buchanan <brad@code.org>
2015-09-28 15:26:43 -0700

    Refactor!
    Specify small differences between chart types by inheritance.
    Also abstracts dependency on Google charts API one more layer.
    Working toward a better error/warning handling system.
    [ci skip]

--- apps/src/applab/ChartApi.js
+++ apps/src/applab/ChartApi.js
@@ -1,8 +1,8 @@
 /**
  * @file Core implementation of Applab commands related to the Chart design element.
  *
- * Uses the Google Charts API.
- * @see https://developers.google.com/chart/
+ * For now, uses Google charts.
+ * @see {GoogleChart}
  */
 /* jshint
  funcscope: true,
@@ -17,24 +17,26 @@
  maxstatements: 200
  */
 'use strict';
-/* global google */
 
 var AppStorage = require('./appStorage');
 var Promise = require('es6-promise').Promise;
+var GoogleChart = require('./GoogleChart');
 
 /**
- * Api for requesting/generating charts in Applab, for now using the Google
- * Charts API.
+ * API for requesting/generating charts in Applab.
+ *
  * @constructor
  * @param {Document} [docContext] - default is 'document'
- * @param [providingApi] - default is 'google'
  * @param [appStorage] - default is AppStorage
  */
-var ChartApi = module.exports = function (docContext, providingApi, appStorage) {
+var ChartApi = module.exports = function (docContext, appStorage) {
   this.document_ = docContext || document;
-  this.api_ = providingApi || google;
   this.appStorage_ = appStorage || AppStorage;
 };
+
+// When adding a new type, provide an entry in the string enum (for clean code)
+// and an entry in the TypeNameToType map (allows us to easily remap different
+// implementations to the same type name).
 
 /** @enum {string} */
 ChartApi.ChartType = {
@@ -44,74 +46,20 @@ ChartApi.ChartType = {
   SCATTER: 'scatter'
 };
 
-/**
- * @param {ChartType} chartType
- * @returns {string[]} A list of google.visualization package dependencies
- *          required to load the given chart type.
- * @throws {Error} if no dependencies are defined for the given chart type.
- */
-ChartApi.getDependenciesForType = function (chartType) {
-  switch (chartType.toLowerCase()) {
-    case ChartApi.ChartType.BAR:
-      // material design; use 'corechart' for standard
-      return ['bar'];
-    case ChartApi.ChartType.PIE:
-      return ['corechart'];
-    case ChartApi.ChartType.LINE:
-      // material design: use 'corechart' for standard
-      return ['line'];
-    case ChartApi.ChartType.SCATTER:
-      // material design: use 'corechart' for standard
-      return ['scatter'];
-  }
-  throw new Error('Package dependencies are not defined for chart type "' +
-      chartType  + '".');
-};
-
-/**
- * @param {ChartType} chartType
- * @returns {function} A constructor function for the requested chart type.
- * @throws {Error} if no dependencies are defined for the given chart type.
- */
-ChartApi.prototype.getConstructorForType = function (chartType) {
-  switch (chartType.toLowerCase()) {
-    case ChartApi.ChartType.BAR:
-      // material design; alt. google.visualization.BarChart
-      return this.api_.charts.Bar;
-    case ChartApi.ChartType.PIE:
-      return this.api_.visualization.PieChart;
-    case ChartApi.ChartType.LINE:
-      // material design; alt. google.visualization.LineChart
-      return this.api_.charts.Line;
-    case ChartApi.ChartType.SCATTER:
-      // material design; alt. google.visualization.ScatterChart
-      return this.api_.charts.Scatter;
-  }
-  throw new Error('Constructor is not defined for chart type "' +
-      chartType  + '".');
-};
-
-/**
- * @param {ChartType} chartType
- * @returns {number} an upper limit on the number of columns a particular
- *          chart type will support.
- */
-ChartApi.getMaxColumnsForType = function (chartType) {
-  switch (chartType.toLowerCase()) {
-    case ChartApi.ChartType.PIE:
-      return 2;
-  }
-  return Infinity;
+/** @type {Object.<string, GoogleChart>} */
+ChartApi.TypeNameToType = {
+  'bar': GoogleChart.MaterialBarChart,
+  'pie': GoogleChart.PieChart,
+  'line': GoogleChart.MaterialLineChart,
+  'scatter': GoogleChart.MaterialScatterChart
 };
 
 /**
  * Get an array of all the chart type strings.
  * @returns {string[]}
  */
-ChartApi.getChartTypes = function () {
-  return Object.getOwnPropertyNames(ChartApi.ChartType).map(function (key) {
-    return ChartApi.ChartType[key];
-  });
+ChartApi.getChartTypeNames = function () {
+  return Object.getOwnPropertyNames(ChartApi.TypeNameToType);
 };
 
 /**
@@ -119,7 +67,7 @@ ChartApi.getChartTypes = function () {
  * @returns {boolean} TRUE if the given type is in the known list of chart types.
  */
 ChartApi.supportsType = function (chartType) {
-  return ChartApi.getChartTypes().indexOf(chartType.toLowerCase()) !== -1;
+  return ChartApi.getChartTypeNames().indexOf(chartType.toLowerCase()) !== -1;
 };
 
 /**
@@ -127,12 +75,35 @@ ChartApi.supportsType = function (chartType) {
  *         Droplet parameter dropdown.
  */
 ChartApi.getChartTypeDropdown = function () {
-  return ChartApi.getChartTypes().map(quote).sort();
+  return ChartApi.getChartTypeNames().map(quote).sort();
 };
 
 function quote(str) {
   return '"' + str + '"';
 }
+
+/**
+ *
+ * @param chartId
+ * @param chartType
+ * @param chartData
+ * @param options
+ * @returns {Promise}
+ */
+ChartApi.prototype.drawChart = function (chartId, chartType, chartData, options) {
+  try {
+    var chart = this.createChart_(chartId, chartType);
+    chart.loadData(chartData).then(function () {
+      return chart.drawChart(options);
+    }).then(function () {
+      // Promise resolves to warnings.
+      // TODO: Change this so we populate a local property with warnings.
+      return chart.warnings;
+    });
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
 
 /**
  * @param {string} chartId
@@ -145,114 +116,70 @@ function quote(str) {
  */
 ChartApi.prototype.drawChartFromRecords = function (chartId, chartType,
     tableName, columns, options) {
-  var warnings = [];
-
   try {
-    // Ensure target element exists and is usable.
-    var targetElement = this.document_.getElementById(chartId);
-    if (!targetElement || 'div' !== targetElement.tagName.toLowerCase()) {
-      return Promise.reject(new Error('Unable to render chart into element "' +
-          chartId + '".'));
-    }
-
-    // Ensure chart type is currently supported
-    if (!ChartApi.supportsType(chartType)) {
-      return Promise.reject(new Error('Unsupported chart type "' + chartType + '".'));
-    }
-
-    // Ensure sufficient columns for chart type
-    var minimumColumnsForType = 2;
-    if (columns.length < minimumColumnsForType) {
-      return Promise.reject(new Error('Not enough columns defined for chart type "' +
-          chartType + '"; expected at least ' + minimumColumnsForType + '.'));
-    }
-
-    // Warn if provided more columns than chart type can use
-    var maxColumnsForType = ChartApi.getMaxColumnsForType(chartType);
-    if (columns.length > maxColumnsForType) {
-      warnings.push(new Error('Provided ' + columns.length +
-          ' columns but chart type "' + chartType + '" can only use up to ' +
-          maxColumnsForType + '.'));
-    }
-  } catch (err) {
-    return Promise.reject(err);
+    var chart = this.createChart_(chartId, chartType);
+    return Promise.all([
+      chart.loadDependencies(),
+      this.fetchTableData_(tableName)
+    ]).then(function (results) {
+      return chart.loadData(results[1], columns);
+    }).then(function () {
+      return chart.drawChart(options);
+    }).then(function () {
+      return chart.warnings;
+    });
+  } catch (e) {
+    return Promise.reject(e);
   }
-
-  // Passed all the pre-command validation, let's get to work:
-  //   1. In parallel:
-  //     - Load required chart libraries.
-  //     - Fetch data from storage table.
-  //   2. Wait for both of the above tasks to complete.
-  //   3. Convert received data to the chart API format.
-  //   4. Render the chart.
-  // Note any exceptions thrown
-  return Promise.all([
-    this.loadApiForType_(chartType),
-    this.fetchTableData_(tableName)
-  ]).then(function (resultsArray) {
-    var rawData = resultsArray[1];
-
-    var Chart = this.getConstructorForType(chartType);
-
-    // Do some validation on data returned by query
-    if (rawData.length === 0) {
-      warnings.push(new Error('No rows returned for table "' + tableName + '".'));
-    } else {
-      columns.forEach(function (columnName) {
-        var existsInSomeRow = rawData.some(function (row) {
-          return row.hasOwnProperty(columnName);
-        });
-
-        if (!existsInSomeRow) {
-          warnings.push(new Error('Column "' + columnName +
-              '" not found in table "' + tableName + '".'));
-        }
-      });
-    }
-
-    // If possible, convert the options object for better compatability with new
-    // material design charts.
-    if ('function' === typeof Chart.convertOptions) {
-      options = Chart.convertOptions(options);
-    }
-
-    // Finally, render the chart
-    var dataTable = this.rawDataToDataTable_(rawData, columns);
-    var chart = new Chart(targetElement);
-    chart.draw(dataTable, options);
-
-    // Pass any warnings back along with the resolved promise.
-    return Promise.resolve(warnings);
-  }.bind(this));
 };
 
 /**
- * Dynamically load needed dependencies for the requested chart type.
- *
- * @param {ChartType} chartType - The requested chart type, which determines
- *        which packages we need to load.
- * @returns {Promise}
+ * Create a chart object of the requested type, for the requested target element.
+ * @param {string} elementId
+ * @param {string} typeName
+ * @returns {GoogleChart}
+ * @throws {Error} if target element or chart type are not found.
  * @private
  */
-ChartApi.prototype.loadApiForType_ = function (chartType) {
-  return new Promise(function (resolve, reject) {
-    try {
-      // Dynamically load the google.visualization library,
-      //   at the latest stable version (that's what '1' means)
-      //   plus any needed packages for the requested chart type
-      //   then call onLoad()
-      this.api_.load('visualization', '1', {
-        packages: ChartApi.getDependenciesForType(chartType),
-        callback: resolve
-      });
-      // google.load caches loaded packages internally, so we can call this
-      // on every drawChart request without incurring unnecessary load time.
-      //    https://developers.google.com/chart/interactive/docs
-      //           /library_loading_enhancements#advanced-library-loading
-    } catch (e) {
-      reject(new Error('Unable to load Charts API.  Please try again later.'));
-    }
-  }.bind(this));
+ChartApi.prototype.createChart_ = function (elementId, typeName) {
+  var targetElement = this.getTargetElement_(elementId);
+  var FoundType = ChartApi.getChartTypeByName_(typeName);
+  return new FoundType(targetElement);
+};
+
+/**
+ * Get the DOM Element with the given ID.
+ * @param {string} elementId
+ * @returns {Element}
+ * @throws {Error} if the requested element is not found, or is not a div.
+ * @private
+ */
+ChartApi.prototype.getTargetElement_ = function (elementId) {
+  var targetElement = this.document_.getElementById(elementId);
+  if (!targetElement || 'div' !== targetElement.tagName.toLowerCase()) {
+    throw new Error('Unable to render chart into element "' + elementId + '".');
+  }
+  return targetElement;
+};
+
+/**
+ * Get the constructor function for the requested chart type.
+ * @param {string} typeName
+ * @returns {GoogleChart}
+ * @throws {Error} if requested type is not found/supported.
+ * @private
+ */
+ChartApi.getChartTypeByName_= function (typeName) {
+  if (typeof typeName !== 'string') {
+    throw new Error('Unknown chart type.');
+  }
+
+  var type = ChartApi.TypeNameToType[typeName.toLowerCase()];
+  if (!type) {
+    throw new Error('Unsupported chart type "' + typeName + '".');
+  }
+
+  return type;
 };
 
 /**
@@ -268,21 +195,4 @@ ChartApi.prototype.fetchTableData_ = function (tableName) {
       reject(new Error(errorMsg));
     });
   }.bind(this));
-};
-
-/**
- * Convert table data from the format returned by the AppStorage API to
- * a google.visualization DataTable filtered to the requested columns.
- * @param {Object[]} rawData
- * @param {string[]} columns - must correspond to keys in rawData rows.
- * @returns {google.visualization.DataTable}
- * @private
- */
-ChartApi.prototype.rawDataToDataTable_ = function (rawData, columns) {
-  var dataArray = rawData.map(function (row) {
-    return columns.map(function (key) {
-      return row[key];
-    });
-  });
-  return this.api_.visualization.arrayToDataTable([columns].concat(dataArray));
 };
