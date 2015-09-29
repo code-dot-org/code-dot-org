1679b78efb259ad12e6a0dae5a014313374f687c
Continuous Integration <dev@code.org>
2015-09-29 21:17:11 +0000

    Automatically built.
    commit 18f0a2f38cb883e35c34c8825a9bcc9b4c4766c9
    Merge: 2234a90 d89be8f
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Tue Sep 29 14:16:57 2015 -0700
    
        Merge pull request #4219 from code-dot-org/revert-4218-revert-4210-applab-ignore-html
    
        Revert "Revert "Fix canvas issues preventing turtle from drawing line…
    
    commit 2234a90454bfff1e45af09a06c246a74bc5a3769
    Merge: db9b5d1 f36eb42
    Author: Will Jordan <wjordan@users.noreply.github.com>
    Date:   Tue Sep 29 13:59:20 2015 -0700
    
        Merge pull request #4246 from pickettd/updates-for-sqlite
    
        Update README instructions and cdo-apps cookbook for new sqlite3 gem
    
    commit f36eb428cdfdf8373d73bc67051f9da9cd17bae5
    Author: David Pickett <pickettd@gmail.com>
    Date:   Tue Sep 29 10:48:31 2015 -1000
    
        Update README instructions and cdo-apps cookbook for new sqlite gem (requires packages to build native extensions).
    
    commit d89be8f4f166a40cd98e1289180e4133550d926c
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Sep 29 13:47:15 2015 -0700
    
        fix cspunit3 test
    
    commit 799a538c5d1f07d757946c00677fe212c02af128
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Sep 29 13:47:02 2015 -0700
    
        add diagram of expected pixels in cspunit3 test

--- dashboard/public/shared-package/js/shared.js
+++ dashboard/public/shared-package/js/shared.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:100c12e5a7defc1e79bacdfe199de66e8328cab338f2a535263b451c7d1ded86
-size 38180
+oid sha256:aada6ac787be66e9a782a8f3793ade58661a4bbe45f1dc729bbedbb5921f8d87
+size 38522
