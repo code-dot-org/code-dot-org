49cec9d7c420ca41a0ace142bb425bc33e50ba3a
Phil Bogle <phil@code.org>
2015-09-29 18:38:19 -0700

    Whitespace fixes

--- dashboard/spec/javascripts/client_state_test.js
+++ dashboard/spec/javascripts/client_state_test.js
@@ -1,20 +1,20 @@
 //= require client_state
 
 describe("clientState#levelProgress", function() {
-	beforeEach(function() {
-		dashboard.clientState.reset();
-	});
+  beforeEach(function() {
+    dashboard.clientState.reset();
+  });
 
-	it("records level progress", function() {
-		dashboard.clientState.levelProgress(100).should.equal(0);
-		dashboard.clientState.setLevelProgress(100, 50);
-		dashboard.clientState.levelProgress(100).should.equal(50);
-		dashboard.clientState.levelProgress(200).should.equal(0);
-		dashboard.clientState.setLevelProgress(100, 100);
-		dashboard.clientState.setLevelProgress(200, 50);
-		dashboard.clientState.levelProgress(100).should.equal(100);
-		dashboard.clientState.levelProgress(200).should.equal(50);
-	});
+  it("records level progress", function() {
+    dashboard.clientState.levelProgress(100).should.equal(0);
+    dashboard.clientState.setLevelProgress(100, 50);
+    dashboard.clientState.levelProgress(100).should.equal(50);
+    dashboard.clientState.levelProgress(200).should.equal(0);
+    dashboard.clientState.setLevelProgress(100, 100);
+    dashboard.clientState.setLevelProgress(200, 50);
+    dashboard.clientState.levelProgress(100).should.equal(100);
+    dashboard.clientState.levelProgress(200).should.equal(50);
+  });
 });
 
 describe("clientState#lines", function() {
