17ab8ea3aec046c5c7f68de3c8ad78c3afcd4f45
Phil Bogle <phil@code.org>
2015-09-29 18:39:29 -0700

    Remove spurious logging

--- dashboard/app/assets/javascripts/client_state.js
+++ dashboard/app/assets/javascripts/client_state.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc81df62723c80df496b9da3cf32ecbbc7cb49c0f392cfdd13c88098d340253f
-size 2020
+oid sha256:e015d6f71208f5ccdee71629905b8a63c2efdc9bda9392bb6bf860bec2d06758
+size 1993
