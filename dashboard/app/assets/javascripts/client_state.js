695ead4718dca72e79ba8a794914528cb28d9ea2
Phil Bogle <phil@code.org>
2015-09-30 11:39:38 -0700

    Use phantomjs

--- dashboard/app/assets/javascripts/client_state.js
+++ dashboard/app/assets/javascripts/client_state.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e015d6f71208f5ccdee71629905b8a63c2efdc9bda9392bb6bf860bec2d06758
-size 1993
+oid sha256:2d7909091398133cb49ad7e0af8c56a3260f78a6887a42b65ac2e0f011da766a
+size 2008
