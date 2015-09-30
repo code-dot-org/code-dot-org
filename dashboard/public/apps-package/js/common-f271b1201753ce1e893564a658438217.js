31ccfcb0a3372903273d385e05526666b9b7993c
Continuous Integration <dev@code.org>
2015-09-30 18:16:58 +0000

    Automatically built.
    commit d0f93191feb0f866e44a103da73c11bde5c1d428
    Merge: 140abd1 17bb34b
    Author: Chris Pirich <chris@code.org>
    Date:   Wed Sep 30 11:12:17 2015 -0700
    
        Merge pull request #4243 from code-dot-org/ace_param_dropdowns
    
        enable ace autocomplete dropdowns for parameters for playlab
    
    commit 140abd14a280790658e118b733802b0b2dbc6f72
    Author: Laurel Fan <laurel@code.org>
    Date:   Wed Sep 30 11:01:55 2015 -0700
    
        fix lint
    
    commit 065f75469bc04340a1add5ac0735e06c1ab538fd
    Merge: 706ac2b 4adf44f
    Author: Laurel <laurel.fan@gmail.com>
    Date:   Wed Sep 30 10:53:19 2015 -0700
    
        Merge pull request #4261 from code-dot-org/test
    
        backmerge production changes

---
+++ dashboard/public/apps-package/js/common-f271b1201753ce1e893564a658438217.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d1f08327671e43dd5ec4af844a0695bd6b3264b603f4de8dbbb9b0854bf4ab2b
+size 1371597
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:052bb15bde6653075d826b8d4a4df02c2999fcce474996724ae523253c8932a2
+oid sha256:20dce59a2c7ec9af1d4443aead187a5945f9b36b973b7ea6f3fb7e42af85a613
 size 73347
