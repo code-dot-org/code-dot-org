124edbedee4f32a02afa51d5209c01531b447e2d
Continuous Integration <dev@code.org>
2015-10-01 20:26:04 +0000

    Automatically built.
    commit 1ea5efd6534bb9bc566c3ec97d8dd4d2a3d03eef
    Merge: 5702b7b a54054b
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Thu Oct 1 13:13:14 2015 -0700
    
        Merge pull request #4290 from code-dot-org/trashcan-regression
    
        Fix trash can fade layering
    
    commit a54054be65cd3dd742d1e700a9a3bf34d9c544c2
    Author: Brad Buchanan <brad@code.org>
    Date:   Thu Oct 1 12:00:23 2015 -0700
    
        Minimum fix for regression.

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15f1fa6ac7e6ce6116c448dbd8d031bef3160cbfc875cb744a6f7d84121ea21c
+oid sha256:be805056c013cb585ef99031c77b38292837743a26e7164642b6c25aa85257d2
 size 73347
