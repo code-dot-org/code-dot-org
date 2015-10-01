c50d0919d2c89f7215b6a915428d010a95dc0582
Brad Buchanan <brad@code.org>
2015-09-30 17:38:47 -0700

    Revert "Revert "DTP from 12:48 DTT""
    This reverts commit b24cf3f3cb9d451dff6e72efb98e0b357941b7a9.

--- dashboard/public/apps-package/js/common-8fa3c1260906aafe2118f934adae69fd.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:233b875f99a5bef244ef164f88f091a6b3dca79664a42aceb38a61e886241e8f
-size 1367210
---
+++ dashboard/public/apps-package/js/studio-7e380d6f7358c2ba2c8edbd3c2991e58.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb78ecb386a3150a43e68fdb3013118f110a185cd48c48e8c8f74a5dc8b3e7b4
+size 360330
