ae937d6848bc066378f1f04d5d52a69d508cbc0a
Brent Van Minnen <bjvanminnen@gmail.com>
2015-10-02 12:52:16 -0700

    get lint in dashboard passing again

--- dashboard/app/assets/javascripts/small_footer.js.jsx
+++ dashboard/app/assets/javascripts/small_footer.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59f52ca0db8a62381db9ea64f598bfbe4b372638c6872d4f85d98872bbbf0ebe
-size 8540
+oid sha256:aacd84f1cf4d94c34339fe22333d4ce6f6ed419ecd8e08296c8599d2025502e8
+size 8558
