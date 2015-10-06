7107d641ae588a80f9b93f5be284dfa04d32cc9c
Continuous Integration <dev@code.org>
2015-10-06 01:08:54 +0000

    Automatically built.
    commit 581ce114f785a528cbcb360070c6a24a62843f68
    Merge: f3bccba 35f15e1
    Author: Brendan Reville <breville@users.noreply.github.com>
    Date:   Mon Oct 5 18:04:10 2015 -0700
    
        Merge pull request #4354 from code-dot-org/htmlinstructions
    
        hoc2015: move to levels.js.  Also instructions2 option in a level.
    
    commit 35f15e1a2fb8e5ad3aa5121565fdc81dc76ac0f3
    Author: Brendan Reville <brendan@code.org>
    Date:   Mon Oct 5 18:00:02 2015 -0700
    
        hoc2015: move all levels to levels.js.  Also add instructions2 level option.
    
    commit f3bccbaee510c33e3c48c136e7a776c8d3741c81
    Author: Continuous Integration <dev@code.org>
    Date:   Tue Oct 6 00:55:23 2015 +0000
    
        Automatically built.
    
        commit 5c619a385bfabaed9df759b54095296821306d71
        Merge: ac7ed0b f3d4897
        Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
        Date:   Mon Oct 5 17:50:32 2015 -0700
    
            Merge pull request #4352 from code-dot-org/revert-4252-position_start_blocks_with_blockly
    
            Revert "Positioning start blocks using Blockly"
    
        commit f3d48979e85658031806929ff38c14fa0aa329f2
        Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
        Date:   Mon Oct 5 17:48:35 2015 -0700
    
            Revert "Positioning start blocks using Blockly"
    
    commit 5c619a385bfabaed9df759b54095296821306d71
    Merge: ac7ed0b f3d4897
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Mon Oct 5 17:50:32 2015 -0700
    
        Merge pull request #4352 from code-dot-org/revert-4252-position_start_blocks_with_blockly
    
        Revert "Positioning start blocks using Blockly"

--- dashboard/public/apps-package/js/common.min-2a2340f874fe94c2441d0c5fd032f3a2.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f5fd80e8b9c6df9e73652c198aef6df2141e81fa1328a9cbc37f0e236ed35274
-size 274418
---
+++ dashboard/public/apps-package/js/studio.min-eab16d507f90ed48cf09cb4e539d86f5.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff898a3b67b3cc65082be610fa956b3a1052a43b7068698bd6ba445d37c33723
+size 209205
