10f6c88c41cf9a17712192546cb8cc69f9cf780b
Continuous Integration <dev@code.org>
2015-10-06 22:47:01 +0000

    Automatically built.
    commit 3e8f4ccd228d2002cfd01db8517b5aea9ff865a8
    Merge: 5ce58e1 ce4c396
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Tue Oct 6 15:42:50 2015 -0700
    
        Merge pull request #4374 from code-dot-org/twoReacts
    
        use apps copy of react if not provided one from dashboard
    
    commit ce4c3962093475f7a995fd7f59140ee7f5c5e04a
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Tue Oct 6 15:17:04 2015 -0700
    
        use apps copy of react if not provided one from dashboard

---
+++ dashboard/public/apps-package/js/common.min-7da71c40ceb90ebaebe5c51b6ddaea09.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eab52124a670b446a9cc3f74a8e730e4b6a812acedf683a2293c06a6dbbad7dc
+size 511122
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25b956de116e6353c38fbed0feaae4330040b2c37f41fa8ea741e4588070bec0
+oid sha256:e0c807b91e9b2d6b319b802744959c22695295a7fee3d8c0101a627c88198d21
 size 73347
