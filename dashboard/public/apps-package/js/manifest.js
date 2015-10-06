344df1aa8b58541f596fe2afec5e5993e45a5cfa
Continuous Integration <dev@code.org>
2015-10-06 21:56:09 +0000

    Automatically built.
    commit 6ccd0b111237bd78afdbaad25adbf970723e5584
    Merge: 20b7fad dc95d19
    Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
    Date:   Tue Oct 6 14:43:36 2015 -0700
    
        Merge pull request #4338 from code-dot-org/disable-next-connection
    
        Adds option to disable block.nextConnection
    
    commit 20b7fadfca0be605064aba90fe747cd23f8de923
    Author: Brad Buchanan <brad@code.org>
    Date:   Tue Oct 6 14:37:50 2015 -0700
    
        Disable share page textarea test for Safari.
    
        Test is broken, feature is okay. Disabling only for safari while I troubleshoot.
        Over-shoulder review by mshah.
    
    commit cbabb3df3c83530bfbcba3e0319b624d43ddc9fb
    Merge: 0c52788 e4a928a
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Tue Oct 6 14:31:12 2015 -0700
    
        Merge pull request #4370 from code-dot-org/pixelation-autosave
    
        Fix pixelation autosave
    
    commit e4a928a7878362fe52867479c316399aaa73b675
    Author: David Bailey <dsb123@gmail.com>
    Date:   Tue Oct 6 14:05:44 2015 -0700
    
        [finishes ##104954876] fix pixelation project init sequence

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1e1f4040ed4f0ab8e7f6f0dda99eeabe33d706ff953a9e1f7ba61b0c6c253e9
+oid sha256:58f7b7c851cfab732edba9ef1dbbe20e6af1cd589b3cd4a3ff745847ce015bfe
 size 73347
