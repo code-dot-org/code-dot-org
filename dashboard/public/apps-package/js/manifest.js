e5566989010547b809cbe51ca3ea17c7afe15e0f
Continuous Integration <dev@code.org>
2015-10-08 21:17:01 +0000

    Automatically built.
    commit 3cb817ce2d7ba41354bad5624416eea88851751e
    Merge: 1b2f6a7 34590b5
    Author: Josh Lory <josh.lory@code.org>
    Date:   Thu Oct 8 14:11:38 2015 -0700
    
        Merge pull request #4437 from code-dot-org/responsive-design-toggle-row
    
        [Finishes #99080292] Responsive View Data button and Screen select
    
    commit 1b2f6a75764dc9939be14dec59e379ace2b89076
    Merge: 5b1b1e4 c9224a1
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Oct 8 14:06:19 2015 -0700
    
        Merge pull request #4446 from code-dot-org/lint-fix
    
        Fix lint hook bug where we couldn't match against non ascii files
    
    commit c9224a11a69b14093c8cb2fae317120e98efa1cb
    Author: Trevor Berg <trevor@code.org>
    Date:   Thu Oct 8 13:52:29 2015 -0700
    
        Fix lint hook bug where we couldn't match against non ascii files

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31ec7815327763008a1884fea874531843f49bd6355c3b41509cdf4d19ffd460
+oid sha256:58fd3fcb9dda34d1f7d200eda92c35c18e8a2713cd37a97d73348e1a8246a792
 size 73347
