8bafe5b5441749b97e5e061ec7126fcc3f3b56e5
Brent Van Minnen <bjvanminnen@gmail.com>
2015-10-09 15:40:00 -0700

    basic example of react component inside jquery dialog

--- apps/src/applab/applab.js
+++ apps/src/applab/applab.js
@@ -42,6 +42,7 @@ var assetsApi = require('../clientApi').assets;
 var assetListStore = require('./assetManagement/assetListStore');
 var showAssetManager = require('./assetManagement/show.js');
 var DebugArea = require('./DebugArea');
+var ShareWarnings = require('../templates/ShareWarnings.jsx');
 
 var applabConstants = require('./constants');
 
@@ -326,6 +327,26 @@ var drawDiv = function () {
 
   if (studioApp.share) {
     renderFooterInSharedGame();
+
+    // TODO - can i move btnselector out
+    studioApp.showDialogWithReactComponent(ShareWarnings, {
+      signedIn: true,
+      storesData: true
+    }, '#foo');
+
+    // var dialog;
+    // var div = document.createElement('div');
+    // React.render(React.createElement(ShareWarnings, {
+    //   signedIn: true,
+    //   storesData: true,
+    //   onClose: function () {
+    //     console.log('close');
+    //     if (dialog) {
+    //       dialog.hide();
+    //     }
+    //   }
+    // }), div);
+    // dialog = studioApp.showDivInDialog(div);
   }
 };
 
