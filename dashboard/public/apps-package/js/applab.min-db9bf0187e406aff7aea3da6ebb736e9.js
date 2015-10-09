981ce6270617194ee170b9e8ea77b3c4ab04abc4
Continuous Integration <dev@code.org>
2015-10-09 00:38:00 +0000

    Automatically built.
    commit 3ef77f490208960e0df699aa35c8cbf207a95434
    Merge: 7783653 459ae09
    Author: Josh Lory <josh.lory@code.org>
    Date:   Thu Oct 8 17:33:54 2015 -0700
    
        Merge pull request #4456 from code-dot-org/always-show-screen
    
        [Finishes #98064218] Show screen properties when nothing selected
    
    commit 778365300ba5603695db82f11806f8d1929d68f7
    Merge: 6f756ff 0d897fb
    Author: Mehal Shah <mehal@code.org>
    Date:   Thu Oct 8 17:17:46 2015 -0700
    
        Merge pull request #4452 from code-dot-org/progress_for_anonymous_users_2
    
        Progress for anonymous users on client side
    
    commit 6f756ff63e99359bd06ee3d588bae8664c4278fd
    Author: Continuous Integration <dev@code.org>
    Date:   Fri Oct 9 00:14:59 2015 +0000
    
        Automatically built.
    
        commit 8ea897ca4beffcd84140c73d9f034744ee25e5d2
        Merge: b8c6e22 12429ab
        Author: Trevor Berg <trevor@code.org>
        Date:   Thu Oct 8 17:10:35 2015 -0700
    
            Merge pull request #4457 from code-dot-org/skipk16
    
            Disable k1_6 test until we can figure out why it randomly fails
    
        commit 12429ab046d6083f8b3fff428bccbfd125cd91c8
        Author: Trevor Berg <trevor@code.org>
        Date:   Thu Oct 8 16:59:38 2015 -0700
    
            Disable k1_6 test until we can figure out why it randomly fails

--- dashboard/public/apps-package/js/applab.min-db9bf0187e406aff7aea3da6ebb736e9.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4d4da5e1eb84936f89ec22d80a3bec1dbbacfdce2fd9cd94bab9b09015a15a80
-size 236726
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58fd3fcb9dda34d1f7d200eda92c35c18e8a2713cd37a97d73348e1a8246a792
+oid sha256:c47a067a8ff04e584c6c1c6a283bfec4bfb45696f73e9908301c3a5e69200479
 size 73347
