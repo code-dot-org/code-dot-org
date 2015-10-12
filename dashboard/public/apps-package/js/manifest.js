75cf8f682eed3d51343ef04779ce3956d4d557a5
Continuous Integration <dev@code.org>
2015-10-12 23:51:51 +0000

    Automatically built.
    commit d3ccd8462f729948bd720a969df5218bd918e049
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Mon Oct 12 16:47:17 2015 -0700
    
        Updated apps strings

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb75854b763fc32cc5fcc4f54b820693a0634a5bfde0ac37f8c899f7b1689c8c
+oid sha256:d38873c8fa86a22ab71a585661dff91d4c1fe2eacdc731ea735b2d3c7314cbd4
 size 73347
