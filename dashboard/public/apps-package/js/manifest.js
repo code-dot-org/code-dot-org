197d037b7f02e2ec3fc0d45287541c7621bde38d
Continuous Integration <dev@code.org>
2015-10-13 18:30:54 +0000

    Automatically built.
    commit c5fa65a9e78bd18df6622508141ab5f5cbd2a16f
    Merge: 296b108 57eee01
    Author: Josh Lory <josh.lory@code.org>
    Date:   Tue Oct 13 11:25:50 2015 -0700
    
        Merge pull request #4519 from code-dot-org/apps-jquery-text
    
        Use $.text in /apps instead of custom getText/setText
    
    commit 57eee013dd28471a4fc5c166d81af3e4c8a1c0a2
    Author: Josh Lory <josh.lory@code.org>
    Date:   Tue Oct 13 10:58:06 2015 -0700
    
        Use $.text in /apps instead of custom getText/setText
    
        Fixes i18n UI tests failures for extra <br> in instructions.

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41c5a15f39ec95729fad40cd558fbf3d76b78f78d7fc4c564652c53e0c75b3d1
+oid sha256:73de48ccd8f17818d220e7221b7ffe04d6b80be9c2750b385377cfc6ddd82071
 size 73347
