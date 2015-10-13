9491433a1f53112e3bdaa71dc8e83b15049fc06d
Continuous Integration <dev@code.org>
2015-10-13 17:07:42 +0000

    Automatically built.
    commit 904e029aaa6b1e4ae97644428979ccb5ffa583d1
    Author: cpirich <chris@code.org>
    Date:   Tue Oct 13 10:01:23 2015 -0700
    
        fix change that broke levels.playlab_4

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acf89d0e050d60156ae005e2d4817e85053730fc6fc021de6b13cd59b8843544
+oid sha256:41c5a15f39ec95729fad40cd558fbf3d76b78f78d7fc4c564652c53e0c75b3d1
 size 73347
---
+++ dashboard/public/apps-package/js/studio-f734dc6ed7627e7d7d1a14b06eae6008.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d2032afd7abc01e1070f433621f1b07a529d4bd595d11cfda72e0c91fe2b15de
+size 403125
