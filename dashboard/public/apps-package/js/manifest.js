e3c414f76eeacc5147f01db0b2f1374bc7b80e42
Continuous Integration <dev@code.org>
2015-10-13 14:26:54 +0000

    Automatically built.
    commit 5cb0308ccb56e636bb7c4d04d3c5af6a47912b10
    Merge: d1a3140 7ff9cb2
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Tue Oct 13 07:21:54 2015 -0700
    
        Merge pull request #4464 from code-dot-org/design-mode-viz
    
        separate design mode content out of divApplab
    
    commit 7ff9cb275446ba79cb2cb2544eb0769f8ecac107
    Author: David Bailey <dsb123@gmail.com>
    Date:   Mon Oct 12 17:33:41 2015 -0700
    
        fix dimming regression. add tests.
    
    commit d1a3140d15646ac6c19f676198bc4375efd7705f
    Merge: 89530d2 bd42943
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Oct 12 17:30:49 2015 -0700
    
        Merge pull request #4470 from code-dot-org/appsGitIgnore
    
        move apps .gitignore contents into root
    
    commit 89530d2fd19e24d12663f9206c5da40938e3cf8b
    Merge: b9f6b17 74d18d1
    Author: Bjvanminnen <Bjvanminnen@gmail.com>
    Date:   Mon Oct 12 17:27:43 2015 -0700
    
        Merge pull request #4468 from code-dot-org/under13Applab
    
        logged in under 13 cant acccess applab shares or /view
    
    commit b9f6b17d9316cba7914ae8134f187cbacbf720ba
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Mon Oct 12 17:22:39 2015 -0700
    
        update artist.feature to remove unnecessary space in error message
    
    commit 74d18d164c3849e69d02d67c17d2074febda9d58
    Author: Brent Van Minnen <bjvanminnen@gmail.com>
    Date:   Fri Oct 9 12:32:30 2015 -0700
    
        logged in under 13 cant acccess applab shares or /view
    
    commit f410bca7ca5131ef109e46db8d1227c7cb3cbe20
    Merge: 07ba0a0 abd05cf
    Author: Caley Brock <caleybrock7@gmail.com>
    Date:   Mon Oct 12 17:07:33 2015 -0700
    
        Merge pull request #4510 from code-dot-org/promote-pages
    
        Link state fact pages to code.org/promote page
    
    commit 07ba0a036db4a4458dac17ef49543ebeb0448cb2
    Author: Continuous Integration <dev@code.org>
    Date:   Mon Oct 12 23:51:51 2015 +0000
    
        Automatically built.
    
        commit d3ccd8462f729948bd720a969df5218bd918e049
        Author: Tanya Parker <tanyacparker@gmail.com>
        Date:   Mon Oct 12 16:47:17 2015 -0700
    
            Updated apps strings
    
    commit d3ccd8462f729948bd720a969df5218bd918e049
    Author: Tanya Parker <tanyacparker@gmail.com>
    Date:   Mon Oct 12 16:47:17 2015 -0700
    
        Updated apps strings
    
    commit 4346cb4ed8e5a99831896c82a8254c47dbfe0342
    Merge: 2498416 e8a7d34
    Author: Caley Brock <caleybrock7@gmail.com>
    Date:   Mon Oct 12 16:37:25 2015 -0700
    
        Merge pull request #4509 from code-dot-org/reviews
    
        Move review special events to teacher-dashboard

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d38873c8fa86a22ab71a585661dff91d4c1fe2eacdc731ea735b2d3c7314cbd4
+oid sha256:7cece84561c34dd7ab8ce33ebd57d30e1456dff36939a51001a38863af823da7
 size 73347
