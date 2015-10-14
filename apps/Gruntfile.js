10fb7ea3de640c6e916134039246dfbfbf30650f
Brad Buchanan <brad@code.org>
2015-10-14 14:19:36 -0700

    Remove dead code in Gruntfile.

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -32,8 +32,7 @@ var envOptions = {
   minify: (process.env.MOOC_MINIFY === '1'),
   localize: (process.env.MOOC_LOCALIZE === '1'),
   dev: (process.env.MOOC_DEV === '1'),
-  digest: false //(process.env.MOOC_DIGEST === '1')
-
+  digest: false // Handled by Rails now
 };
 
 var LOCALES = (envOptions.localize ? [
