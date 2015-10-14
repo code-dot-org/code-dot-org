6c736a5ea03e6a914147c4e21081fed1bc2a656c
Continuous Integration <dev@code.org>
2015-10-14 04:29:52 +0000

    Automatically built.
    commit d1b8e206ddb87ca88762f9fc692be66b4a296087
    Merge: 30a0c41 6be1903
    Author: Trevor Berg <trevor@code.org>
    Date:   Tue Oct 13 21:25:20 2015 -0700
    
        Merge pull request #4533 from code-dot-org/autocomplete-tooltips-rebase
    
        Add the ability to not show droplet tooltips on a per level basis
    
    commit 30a0c412f0b52628742346a4a2ad4e1ef1c1d339
    Merge: 60ed967 04ac1cd
    Author: davidsbailey <davidsbailey@users.noreply.github.com>
    Date:   Tue Oct 13 21:24:41 2015 -0700
    
        Merge pull request #4547 from code-dot-org/fix-id-dropdowns
    
        Fix id dropdowns
    
    commit 60ed967058447bf6fda80a02524438ce41c0e5c9
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Oct 14 04:19:55 2015 +0000
    
        Automatically built.
    
        commit 91bd1502f822f194106510ae422fbe5266e96dea
        Merge: 3acdc4f 1db88fa
        Author: Trevor Berg <trevor@code.org>
        Date:   Tue Oct 13 21:10:06 2015 -0700
    
            Merge pull request #4531 from code-dot-org/fix-debug-icon
    
            Get rid of hacks for debugger hide/show state
    
        commit 3acdc4f69f9c340eb27b10357c382dd2a34b20cc
        Merge: 867ec7f 1c7c553
        Author: Trevor Berg <trevor@code.org>
        Date:   Tue Oct 13 21:08:46 2015 -0700
    
            Merge pull request #4548 from code-dot-org/xframe
    
            Rewrite documentation urls to forward on the embedded flag
    
        commit 867ec7ff05112ea31bd4913570c709e3bc78dc6c
        Author: Continuous Integration <dev@code.org>
        Date:   Wed Oct 14 01:05:32 2015 +0000
    
            Automatically built.
    
            commit 9764e23ad614dfee94c8af6d64b068d1df282a92
            Merge: b37971a 4842714
            Author: Elijah Hamovitz <elijahhamovitz@gmail.com>
            Date:   Tue Oct 13 18:00:05 2015 -0700
    
                Merge pull request #4459 from code-dot-org/readonly-function-editor
    
                Add Blockly.readOnly check to function editor
    
            commit b37971a8141c07993c9ad519209473869544c44a
            Author: Josh Lory <josh.lory@code.org>
            Date:   Tue Oct 13 17:46:30 2015 -0700
    
                Cucumber doesn't like inline comments
    
    commit 91bd1502f822f194106510ae422fbe5266e96dea
    Merge: 3acdc4f 1db88fa
    Author: Trevor Berg <trevor@code.org>
    Date:   Tue Oct 13 21:10:06 2015 -0700
    
        Merge pull request #4531 from code-dot-org/fix-debug-icon
    
        Get rid of hacks for debugger hide/show state
    
    commit 3acdc4f69f9c340eb27b10357c382dd2a34b20cc
    Merge: 867ec7f 1c7c553
    Author: Trevor Berg <trevor@code.org>
    Date:   Tue Oct 13 21:08:46 2015 -0700
    
        Merge pull request #4548 from code-dot-org/xframe
    
        Rewrite documentation urls to forward on the embedded flag

--- dashboard/public/apps-package/js/applab.min-94e67623104156bd2957ec7365d42cb0.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f9bf3c308346e3560807118fbcb37f4290faa3ec6ce8958af37179f91c9708ec
-size 239693
--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45cfc81011d01392282acaa2d3a1c259f4cb76a91e2efcc3d892219e47585cb5
+oid sha256:6a8667d94758e13203d07d2d46fcbd2a943f75c8ebacf2eab4435be61bbd60a4
 size 73347
