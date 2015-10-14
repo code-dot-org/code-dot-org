33e2ebfc00d10ca0b03d282aa66ef3725809dcaf
Phil Bogle <phil@code.org>
2015-10-14 12:06:39 -0700

    Increase ajax timeout to 15 seconds

--- dashboard/app/assets/javascripts/sendReport.js
+++ dashboard/app/assets/javascripts/sendReport.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57259bb58b4c50e0fa2c135436b2ac208fa3b872871194185e338e6d45506fa2
-size 3633
+oid sha256:ebaaed62e0994e719da97ed3ef3381816dbc51eed6ce4d16f3a689285c3c9448
+size 3636
