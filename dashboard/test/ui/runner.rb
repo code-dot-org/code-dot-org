773c4933f3cee4429625edb312679bdfc8f59a15
Brad Buchanan <brad@code.org>
2015-10-15 11:39:53 -0700

    Don't spam developers room with UI test results from production or adhoc instances.

--- dashboard/test/ui/runner.rb
+++ dashboard/test/ui/runner.rb
@@ -321,7 +321,9 @@ Parallel.map(lambda { browser_features.pop || Parallel::Stop }, :in_processes =>
 
     message += "<br/><i>rerun: ./runner.rb -c #{browser_name} -f #{feature} --html</i>"
     HipChat.log message, color: 'red'
-    HipChat.developers short_message, color: 'red' if CDO.hip_chat_logging
+    if CDO.hip_chat_logging && !(['adhoc', 'production'].include? Rails.env)
+      HipChat.developers short_message, color: 'red'
+    end
   end
   result_string =
     if scenario_count == 0
