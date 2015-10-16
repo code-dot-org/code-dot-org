67fd3094f9f95acdf471fc130d10b6fd0a4a34e7
Continuous Integration <dev@code.org>
2015-10-16 19:34:04 +0000

    Automatically built.
    commit 46158fd23b3730eb7cef7eb90a63fdf19adba1de
    Merge: 69137ae 4969c23
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Fri Oct 16 12:26:59 2015 -0700
    
        Merge pull request #4607 from code-dot-org/applab-setText-respects-newlines
    
        Applab: setText and getText respect newlines
    
    commit 69137ae99be3f5028dabeb607b24344cfdfad9b8
    Author: Laurel Fan <laurel@code.org>
    Date:   Fri Oct 16 12:21:32 2015 -0700
    
        scripts to send engineers mail
    
    commit 4969c2343bd792bd134d2971d71cd7444facc42d
    Merge: 9fe926d 461fd9d
    Author: Brad Buchanan <brad@code.org>
    Date:   Fri Oct 16 12:01:19 2015 -0700
    
        Merge remote-tracking branch 'origin/staging' into applab-setText-respects-newlines
    
        Conflicts:
        	apps/test/applabTest.js
    
    commit 461fd9d4bf7eba29264b1adfab8cae4c5a2d160c
    Merge: d1b6d42 d6f6b1f
    Author: Laurel Fan <laurel@code.org>
    Date:   Fri Oct 16 11:44:37 2015 -0700
    
        merge test
    
    commit d1b6d42e20f12f0c4791d842c8b117011fb82d9f
    Merge: 2221b07 cb460bb
    Author: Caley Brock <caleybrock7@gmail.com>
    Date:   Fri Oct 16 11:41:32 2015 -0700
    
        Merge pull request #4612 from code-dot-org/revert-4605-special-approval
    
        Revert "Special events approval by location"

--- dashboard/public/apps-package/js/manifest.js
+++ dashboard/public/apps-package/js/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebb03eee25adb8ccb6f43b20ccd534aa8c54690d7c09dcd901b84636ce667e7e
+oid sha256:9cd123aab340b5111e65f4a0f117f1b9f9898f9b304794a757e8274e7029ec6b
 size 73347
