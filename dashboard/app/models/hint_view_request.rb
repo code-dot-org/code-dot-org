734dbfc577c2e6e9c66129b6501d42bf200844e0
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-10-15 18:00:54 -0700

    added ENABLED flag to HintViewRequest so we can disable it for HoC, changed feedback_xml to a text rather than string field, and made script_ and level_ ids composite

--- dashboard/app/models/hint_view_request.rb
+++ dashboard/app/models/hint_view_request.rb
@@ -7,15 +7,14 @@
 #  script_id     :integer
 #  level_id      :integer
 #  feedback_type :integer
-#  feedback_xml  :string(255)
+#  feedback_xml  :text(65535)
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #
 # Indexes
 #
-#  index_hint_view_requests_on_level_id   (level_id)
-#  index_hint_view_requests_on_script_id  (script_id)
-#  index_hint_view_requests_on_user_id    (user_id)
+#  index_hint_view_requests_on_script_id_and_level_id  (script_id,level_id)
+#  index_hint_view_requests_on_user_id                 (user_id)
 #
 
 class HintViewRequest < ActiveRecord::Base
@@ -23,15 +22,23 @@ class HintViewRequest < ActiveRecord::Base
   belongs_to :script
   belongs_to :level
 
+  validates :user, :presence => true
+  validates :script, :presence => true
+  validates :level, :presence => true
+
+  ENABLED = true
+
   # Returns an array of serializable hashes representing all of a given
   # user's hint view requests for a given script and level
   # called by ApplicationController.milestone_response
   # used by Studio's feedback modal to decide whether or not to show a
   # user a hint based on the already-viewed hints.
   def HintViewRequest.milestone_response(script, level, user)
-    return HintViewRequest.where(script: script, level: level, user: user).find_each.map do |hint_view_request|
-      hint_view_request.serializable_hash(only: [:feedback_type, :feedback_xml])
-    end
+    HintViewRequest::ENABLED ? HintViewRequest.where(script: script, level: level, user: user).
+      pluck(:feedback_type, :feedback_xml).
+      map do |feedback_type, feedback_xml|
+        {feedback_type: feedback_type, feedback_xml: feedback_xml}
+      end : []
   end
 
 end
