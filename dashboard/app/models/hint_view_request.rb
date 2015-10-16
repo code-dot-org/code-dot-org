f5c75cd014c1cab0088e46bc8b794fb7f41be58f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-10-16 11:48:09 -0700

    readability/linting fixes

--- dashboard/app/models/hint_view_request.rb
+++ dashboard/app/models/hint_view_request.rb
@@ -34,10 +34,12 @@ class HintViewRequest < ActiveRecord::Base
   # used by Studio's feedback modal to decide whether or not to show a
   # user a hint based on the already-viewed hints.
   def HintViewRequest.milestone_response(script, level, user)
-    HintViewRequest::ENABLED ? HintViewRequest.where(script: script, level: level, user: user)
-      .pluck(:feedback_type, :feedback_xml)
-      .map do |feedback_type, feedback_xml|
+    return [] unless HintViewRequest::ENABLED
+    HintViewRequest.
+      where(script: script, level: level, user: user).
+      pluck(:feedback_type, :feedback_xml).
+      map { |feedback_type, feedback_xml|
         {feedback_type: feedback_type, feedback_xml: feedback_xml}
-      end : []
+      }
   end
 end
