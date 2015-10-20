d14c6898af6029b10bdcd6a24d26c238769ab610
Continuous Integration <dev@code.org>
2015-10-20 18:41:16 +0000

    Automatically built.
    commit 013bfed1446754852ba30858ee278e027b511ffe
    Merge: 0013c5d f208911
    Author: Chris Pirich <chris@code.org>
    Date:   Tue Oct 20 11:36:22 2015 -0700
    
        Merge pull request #4672 from code-dot-org/playlab_validation_and_sound_changes
    
        change playlab sounds, validate playSound,changeScore,setBotSpeed
    
    commit 0013c5d487fc05c20b2210ac032bd8b8ce49ab51
    Merge: 4c3c62b 39a72c8
    Author: Brad Buchanan <bradley.c.buchanan@gmail.com>
    Date:   Tue Oct 20 11:17:46 2015 -0700
    
        Merge pull request #4671 from code-dot-org/levelbuilder
    
        DTS (levelbuilder => staging) for Kiki's new script
    
    commit f20891167a210f0ff3ad5d524fd78c52d2b3391a
    Author: cpirich <chris@code.org>
    Date:   Tue Oct 20 11:16:47 2015 -0700
    
        add random to playSound dropdown

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d72fd12218303eadd82b02831ce37fcdaaebcbf54554aac776d93ddb4654d85
-size 407830
+oid sha256:a5674209ca7320bd4a0e2a30a36396b0ae0d1b6af371e17cf424d4431dc9b125
+size 406612
