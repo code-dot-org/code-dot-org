755fe325c1eaa9aeb6f7e1c10cd6c093193c7eb0
Continuous Integration <dev@code.org>
2015-10-21 20:10:10 +0000

    Automatically built.
    commit 155c7d53470b87e86340ccda59944a62b452c12a
    Merge: 010c627 fd38413
    Author: Mehal Shah <mehal@code.org>
    Date:   Wed Oct 21 13:05:16 2015 -0700
    
        Merge pull request #4684 from code-dot-org/label_text_wrap
    
        Impose a max-width on labels
    
    commit fd38413c3675530a922895089db65ec1b5b0838b
    Author: Mehal Shah <mehal@code.org>
    Date:   Wed Oct 21 11:49:39 2015 -0700
    
        [finishes #101090866]Fix label text wrapping
    
    commit 010c62761645a8c6e02163c939e827c0288e4f74
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Oct 21 19:51:12 2015 +0000
    
        Automatically built.
    
        commit 32ca69cd6510e35fd0474e7b0b4e829e8cbb29e2
        Author: cpirich <chris@code.org>
        Date:   Wed Oct 21 12:46:02 2015 -0700
    
            hoc2015: tweak level 12

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -8929,6 +8929,7 @@ module.exports = {
     element.textContent = 'text';
     element.style.color = '#333333';
     element.style.backgroundColor = '';
+    element.style.maxWidth = Applab.appWidth + 'px';
 
     this.resizeToFitText(element);
     return element;
@@ -8939,13 +8940,15 @@ module.exports = {
       position: 'absolute',
       visibility: 'hidden',
       width: 'auto',
-      height: 'auto'
+      height: 'auto',
+      maxWidth: (Applab.appWidth - parseInt(element.style.left, 10)) + 'px',
     }).appendTo($(document.body));
 
     var padding = parseInt(element.style.padding, 10);
 
     if ($(element).data('lock-width') !== PropertyRow.LockState.LOCKED) {
-      element.style.width = clone.width() + 1 + 2 * padding + 'px';
+      //Truncate the width before it runs off the edge of the screen
+      element.style.width = Math.min(clone.width() + 1 + 2 * padding, Applab.appWidth - clone.position().left) + 'px';
     }
     if ($(element).data('lock-height') !== PropertyRow.LockState.LOCKED) {
       element.style.height = clone.height() + 1 + 2 * padding + 'px';
