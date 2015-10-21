ab23713fe083bd382874dbce7bef63437ab680c9
Continuous Integration <dev@code.org>
2015-10-21 20:10:10 +0000

    Automatically built.
    commit 155c7d53470b87e86340ccda59944a62b452c12a
    Merge: 010c627 fd38413
    Author: Mehal Shah <mehal@code.org>
    Date:   Wed Oct 21 13:05:16 2015 -0700
    
        Merge pull request #4684 from code-dot-org/label_text_wrap
    
        Impose a max-width on labels
    
    commit fd38413c3675530a922895089db65ec1b5b0838b
    Author: Mehal Shah <mehal@code.org>
    Date:   Wed Oct 21 11:49:39 2015 -0700
    
        [finishes #101090866]Fix label text wrapping
    
    commit 010c62761645a8c6e02163c939e827c0288e4f74
    Author: Continuous Integration <dev@code.org>
    Date:   Wed Oct 21 19:51:12 2015 +0000
    
        Automatically built.
    
        commit 32ca69cd6510e35fd0474e7b0b4e829e8cbb29e2
        Author: cpirich <chris@code.org>
        Date:   Wed Oct 21 12:46:02 2015 -0700
    
            hoc2015: tweak level 12

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f86c5f5e19b218527af5575885e2e7a0366cccf3db2e421281bbd35c52a79081
-size 486413
+oid sha256:539f066b8cb19844e59c1d98e49093dea9949d851ab485f6beef023a0379f758
+size 486664
