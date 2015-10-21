5cd3f4f1231ed41a3bd69b98b3ac9916924b8a5e
Continuous Integration <dev@code.org>
2015-10-21 22:36:11 +0000

    Automatically built.
    commit f08f6be356ee7544ba02db3b6548b209fcdd8cd5
    Merge: 3e4968c 537e24c
    Author: Mehal Shah <mehal@code.org>
    Date:   Wed Oct 21 15:31:02 2015 -0700
    
        Merge pull request #4711 from code-dot-org/fix_missing_gear
    
        [finishes #104554352] Gear wasn't showing up on FF because float span…
    
    commit 537e24c2ad2a1c0e59f54b8217d78451d568b9ba
    Author: Mehal Shah <mehal@code.org>
    Date:   Wed Oct 21 15:11:15 2015 -0700
    
        [finishes #104554352] Gear wasn't showing up on FF because float spans need to be before non float divs in the dom

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:539f066b8cb19844e59c1d98e49093dea9949d851ab485f6beef023a0379f758
+oid sha256:d7e36b4fc8233ef452441343fddf8a7ec674f8878b17fee721b361fe7b13ef2a
 size 486664
