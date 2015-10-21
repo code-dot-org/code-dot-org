2670f6376ccbb685c4003d8518b7f3c4bc53201e
Continuous Integration <dev@code.org>
2015-10-21 19:23:12 +0000

    Automatically built.
    commit 68668286572516be75adb4b6a410928ccef34f6a
    Author: cpirich <chris@code.org>
    Date:   Wed Oct 21 12:14:09 2015 -0700
    
        hoc2015 blockly levels 7-8

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a340b9b430a2aa54a29773798ebba089782698ea2b26bfd421bab54f7518d0d
-size 431944
+oid sha256:b1f3c05251a3f5bcc500f1369007f3000ef59b7cacd23174016a4ed2093c9467
+size 433946
