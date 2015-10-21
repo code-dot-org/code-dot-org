ffb8b7c7c8caa7c3aef9508b59026bca5c2481ba
Continuous Integration <dev@code.org>
2015-10-21 23:10:11 +0000

    Automatically built.
    commit 169d32154b039e70968d59f26b8894b90bfe1c8e
    Merge: d61a3b0 96e1b30
    Author: Brendan Reville <breville@users.noreply.github.com>
    Date:   Wed Oct 21 16:04:59 2015 -0700
    
        Merge pull request #4715 from code-dot-org/hoc2015placeholder
    
        hoc2015: adjust placeholder assets.
    
    commit 96e1b3044d100b84041d20e1853630bfc161357e
    Author: Brendan Reville <brendan@code.org>
    Date:   Wed Oct 21 16:04:13 2015 -0700
    
        hoc2015: adjust placeholder assets.

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5f074647cb79666ed6cb1a6f4f8cbb6dc4cb6cb3731d1927ba96deefe9b001e
-size 433815
+oid sha256:855cabdf466eecb89d276f9bd0a3230c229a15578054761cbbcec2dd0db3e1d3
+size 433814
