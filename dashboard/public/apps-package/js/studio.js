33ab0e62d64dc5c55adb4ac85f06eb8e26ce3b16
Continuous Integration <dev@code.org>
2015-10-22 22:52:18 +0000

    Automatically built.
    686c0cf Merge pull request #4752 from code-dot-org/character-start-positions (Brad Buchanan)
    141cf12 add back db:schema:load in test script (Laurel Fan)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e58fbce4ff709d452994e2b6a7e91be6823bd9e39e6c1cb2e53fda77a29d6d89
-size 447377
+oid sha256:b955dffb8ba1f56f4727d5a4194e4a136a20902b7b5bff162e084b82e626da74
+size 447310
