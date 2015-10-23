99d0618e824385950d2a7715e5f21f063781a266
Continuous Integration <dev@code.org>
2015-10-23 22:33:42 +0000

    Fix typo

--- deployment.rb
+++ deployment.rb
@@ -144,7 +144,7 @@ class CDOImpl < OpenStruct
   end
 
   def canonical_hostname(domain)
-    return "adhoc-studio.org" if self.name == 'adhoc-dashboard.code.org'
+    return "adhoc-studio.code.org" if self.name == 'adhoc-dashboard.code.org'
     return "#{self.name}.#{domain}" if ['console', 'hoc-levels'].include?(self.name)
     return domain if rack_env?(:production)
 
