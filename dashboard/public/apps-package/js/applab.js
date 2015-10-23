9303316d67f383f870b7f2ef4c7306b0c3cedf3a
Continuous Integration <dev@code.org>
2015-10-23 22:27:28 +0000

    Automatically built.
    eacad10 fix applab tests (David Bailey)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fc2529191104a5b4ea1ade0876e541c400fd4584e450c8e73be4947517d9e09
-size 487979
+oid sha256:0ce3913c751b5bbe9bed6f08a89061f5edadf53a955ed2d204cf381b9915851a
+size 486098
