06ceabdf4f71e61f17fd388328e5eaffd77bd5d8
Will Jordan <will@code.org>
2015-10-23 12:27:57 -0700

    update apt in Berksfile fix cdo-varnish ppa uri

--- cookbooks/cdo-varnish/recipes/default.rb
+++ cookbooks/cdo-varnish/recipes/default.rb
@@ -21,7 +21,7 @@ apt_package 'varnish' do
 end
 
 apt_repository 'varnish-3.0-vmods' do
-  uri          'http://ppa.launchpad.net/cmcdermottroe/varnish-3.0-vmods'
+  uri          'ppa:cmcdermottroe/varnish-3.0-vmods'
   distribution 'trusty'
 end
 apt_package 'libvmod-cookie'
