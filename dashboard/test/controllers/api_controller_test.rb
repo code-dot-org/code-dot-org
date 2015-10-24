7e876d576e915433de4c7247a3880ca552af3a2c
Phil Bogle <phil@code.org>
2015-10-23 22:32:05 -0700

    Move home controller tests to api controller

--- dashboard/test/controllers/api_controller_test.rb
+++ dashboard/test/controllers/api_controller_test.rb
@@ -218,6 +218,28 @@ class ApiControllerTest < ActionController::TestCase
       local_school_link: I18n.t('home.local_school'))
   end
 
+  test 'should show teacher-dashboard link when a teacher' do
+    teacher = create :teacher
+    sign_in teacher
+
+    get :user_menu
+
+    assert_response :success
+    assert_select 'a[href="//test.code.org/teacher-dashboard"]', 'Teacher Home Page'
+  end
+
+  test 'student does not see links to ops dashboard or teacher dashboard' do
+    student = create :student
+    sign_in student
+
+    get :user_menu
+
+    assert_response :success
+    assert_select 'a[href="//test.code.org/ops-dashboard"]', 0
+    assert_select 'a[href="//test.code.org/teacher-dashboard"]', 0
+  end
+
+
   test 'api routing' do
     # /dashboardapi urls
     assert_routing({method: "get", path: "/dashboardapi/user_menu"},
