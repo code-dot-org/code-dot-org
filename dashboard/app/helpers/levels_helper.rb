ff58a1be1bd30ead36b2534929c4cbe0e04ecfe1
Continuous Integration <dev@code.org>
2015-10-24 22:41:07 +0000

    camel case

--- dashboard/app/helpers/levels_helper.rb
+++ dashboard/app/helpers/levels_helper.rb
@@ -151,10 +151,10 @@ module LevelsHelper
       # currently, all levels are Blockly or DSLDefined except for Unplugged
       @app_options = view_options.camelize_keys
     end
-    baseUrl = @app_options['baseUrl']
+    base_url = @app_options['baseUrl']
 
-    # Fix up app_options are configured with a baseUrl with a host name but no leading '//'
-    @app_options['baseUrl'] = "//#{baseUrl}" unless baseUrl.blank? || baseUrl.start_with?('/')
+    # Fix up app_options that are configured with a baseUrl with a host name but no leading '//'
+    @app_options['baseUrl'] = "//#{baseUrl}" unless base_url.blank? || base_url.start_with?('/')
 
     @app_options
   end
