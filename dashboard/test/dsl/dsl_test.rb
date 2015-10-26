37c8724c728571862d663a4b0f0b94f013cb7110
Laurel Fan <laurel@code.org>
2015-10-26 12:12:44 -0700

    also test admin_required 'true'

--- dashboard/test/dsl/dsl_test.rb
+++ dashboard/test/dsl/dsl_test.rb
@@ -31,9 +31,20 @@ level 'Level 5'
     assert_equal i18n_expected, i18n
   end
 
-  test 'test Script DSL admin_required ' do
+  test 'test Script DSL admin_required as boolean' do
     input_dsl = "
 admin_required true
+"
+    output, _ = ScriptDSL.parse(input_dsl, 'test.script', 'test')
+
+    expected = {id: nil, stages: [], hidden: true, trophies: false, wrapup_video: nil, login_required: false, admin_required: true, pd: false}
+
+    assert_equal expected, output
+  end
+
+  test 'test Script DSL admin_required as string' do
+    input_dsl = "
+admin_required 'true'
 "
     output, _ = ScriptDSL.parse(input_dsl, 'test.script', 'test')
 
