b09744277f7a764f82c6f9d6141fcc53d11e6149
Continuous Integration <dev@code.org>
2015-10-26 23:56:10 +0000

    Automatically built.
    76c547b Merge pull request #4836 from code-dot-org/position-category-headers (Elijah Hamovitz)
    5688e86 also updated blockly-core tests to reflect changed positioning logic (Elijah Hamovitz)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79b885bbd77c8999bc015d2e1970eafeb60b6787f7bb0b006326431bed13689a
-size 1379200
+oid sha256:9e197fd6b101992d0d150d95090eedc8b3ea6cdeadb20c3b22f120353a94241a
+size 1379296
