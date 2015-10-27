ed157633f0ac2fa9cea973b88792d5d9d40fff70
Continuous Integration <dev@code.org>
2015-10-27 17:08:09 +0000

    Automatically built.
    562fd6f Merge pull request #4829 from code-dot-org/revertRevertConfigCleanup (Bjvanminnen)
    994b3de Merge pull request #4848 from code-dot-org/path_optimizations (philbogle)
    d0c0464 Update README.md (David Bailey)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de056e56324a0b0c6505b1fcea6dec9f5799efc3aa20b889d98f14f9bd82300b
-size 486422
+oid sha256:77e46d9e5844ae669bea3d9d17b095ccb685697e3752cabddc489c6d140d00eb
+size 486494
