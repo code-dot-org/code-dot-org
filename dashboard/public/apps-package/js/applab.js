e45c5a75e3fcd6131a749f95a8dd8afaf0108a3a
Continuous Integration <dev@code.org>
2015-10-27 17:21:07 +0000

    Automatically built.
    6f104cf Merge pull request #4841 from code-dot-org/design-mode-outline-fix (Josh Lory)
    e20edbc Automatically built. (Continuous Integration)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77e46d9e5844ae669bea3d9d17b095ccb685697e3752cabddc489c6d140d00eb
-size 486494
+oid sha256:5e61dc8c8d07d8a1112f2cc2ad45dbc6643bf88bda1f1fffc5c8c85628e29bfa
+size 486504
