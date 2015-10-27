c48cafea708eb63b476217ce84097e41d2e740c9
Continuous Integration <dev@code.org>
2015-10-27 00:10:33 +0000

    Automatically built.
    16ebc5f Merge pull request #4831 from code-dot-org/fix-app-modern (David Bailey)
    6c0e9bc Automatically built. (Continuous Integration)
    76c547b Merge pull request #4836 from code-dot-org/position-category-headers (Elijah Hamovitz)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3a10eeeb2cff1a7c5a9e2e21199a8c768f9e23fe0e68de7bd365bcef039ff21
-size 486348
+oid sha256:16a6e5053ef0acde408420750b9158101423d2548e5eeeff9f25c48a5c6f5faa
+size 486364
