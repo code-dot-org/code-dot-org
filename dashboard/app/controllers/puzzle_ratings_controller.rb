32eff45992c4740a7f7466bbbc741b5c3cdc9bf1
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-10-27 15:35:26 -0700

    early return from PuzzleRating:create when disabled

--- dashboard/app/controllers/puzzle_ratings_controller.rb
+++ dashboard/app/controllers/puzzle_ratings_controller.rb
@@ -1,15 +1,14 @@
 class PuzzleRatingsController < ApplicationController
   def create
-    status_code = :unauthorized
-    if PuzzleRating.enabled?
-      valid_request = PuzzleRating.create(
-        script_id: params[:script_id],
-        level_id: params[:level_id],
-        user: current_user,
-        rating: params[:rating],
-      ).valid?
-      status_code = valid_request ? :created : :bad_request
-    end
+    return head :unauthorized unless PuzzleRating.enabled?
+
+    valid_request = PuzzleRating.create(
+      script_id: params[:script_id],
+      level_id: params[:level_id],
+      user: current_user,
+      rating: params[:rating],
+    ).valid?
+    status_code = valid_request ? :created : :bad_request
     head status_code
   end
 end
