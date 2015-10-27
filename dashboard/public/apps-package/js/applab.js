1323da246ec4370ffc1b3df592455629a5996fb1
Continuous Integration <dev@code.org>
2015-10-27 16:14:55 +0000

    Automatically built.
    7276abb Merge pull request #4820 from code-dot-org/mobileFooter (Bjvanminnen)
    e50c1e9 Automatically built. (Continuous Integration)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16a6e5053ef0acde408420750b9158101423d2548e5eeeff9f25c48a5c6f5faa
-size 486364
+oid sha256:de056e56324a0b0c6505b1fcea6dec9f5799efc3aa20b889d98f14f9bd82300b
+size 486422
