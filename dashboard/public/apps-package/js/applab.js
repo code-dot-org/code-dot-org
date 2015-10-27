9ac908ab20aac0d3c612a8881bab9eb199ac9787
Continuous Integration <dev@code.org>
2015-10-27 16:14:55 +0000

    Automatically built.
    7276abb Merge pull request #4820 from code-dot-org/mobileFooter (Bjvanminnen)
    e50c1e9 Automatically built. (Continuous Integration)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -673,7 +673,8 @@ function renderFooterInSharedGame() {
     },
     {
       text: applabMsg.makeMyOwnApp(),
-      link: '/projects/applab'
+      link: '/projects/applab',
+      hideOnMobile: true
     },
     {
       text: commonMsg.openWorkspace(),
@@ -691,7 +692,9 @@ function renderFooterInSharedGame() {
     }
   ];
   if (dom.isMobile()) {
-    menuItems.splice(0, 1); // no make my own app on mobile
+    menuItems = menuItems.filter(function (item) {
+      return !item.hideOnMobile;
+    });
   }
 
   window.dashboard.footer.render(React, {
