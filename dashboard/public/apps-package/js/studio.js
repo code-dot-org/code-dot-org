3c8ae3191ca2bb0366b1047b4d2262f772f0162d
Continuous Integration <dev@code.org>
2015-10-27 05:50:31 +0000

    Automatically built.
    61a3c98 Merge pull request #4846 from code-dot-org/match-placeholder-asset-names (Brad Buchanan)
    bae052f Merge remote-tracking branch 'origin/staging' into match-placeholder-asset-names (Brad Buchanan)
    17bbc2e Fix bug with threesliceaudio - end sound wasn't playing. (Brad Buchanan)
    e8ee086 Unify placeholder asset names and config with real assets and config (Brad Buchanan)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:633f26c52cb4a7c495bb286b57c1728a302c5ca506855a6ce0d527c8d499f051
-size 469450
+oid sha256:dd1fa0e8fe6da341443a8febb04c76ea281a60d478de1d619f5aa1bb0b854c0d
+size 469577
