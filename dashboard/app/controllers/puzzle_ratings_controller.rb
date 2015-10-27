e8dd75070675a41937596f1ebef0ca14dfc4fe37
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-10-27 16:47:30 -0700

    added retry handler to deal with potential race condition on uniqueness constraint

--- dashboard/app/controllers/puzzle_ratings_controller.rb
+++ dashboard/app/controllers/puzzle_ratings_controller.rb
@@ -2,12 +2,15 @@ class PuzzleRatingsController < ApplicationController
   def create
     return head :unauthorized unless PuzzleRating.enabled?
 
-    valid_request = PuzzleRating.create(
-      script_id: params[:script_id],
-      level_id: params[:level_id],
-      user: current_user,
-      rating: params[:rating],
-    ).valid?
+    valid_request = retryable on: [Mysql2::Error, ActiveRecord::RecordNotUnique], matching: /Duplicate entry/ do
+      PuzzleRating.create(
+        script_id: params[:script_id],
+        level_id: params[:level_id],
+        user: current_user,
+        rating: params[:rating]
+      ).valid?
+    end
+
     status_code = valid_request ? :created : :bad_request
     head status_code
   end
