5a747a4b31e045b8b255a646af9b7f42fb40f6bd
Continuous Integration <dev@code.org>
2015-10-27 20:59:11 +0000

    Automatically built.
    b5e1957 Merge pull request #4838 from code-dot-org/nonAutompletedBlocks (Bjvanminnen)
    09b489a Merge pull request #4859 from code-dot-org/levelbuilder (Bjvanminnen)
    d182559 brent modifying allthethings script (Continuous Integration)
    cb11944 brent adding some new levels for UI tests (Continuous Integration)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e61dc8c8d07d8a1112f2cc2ad45dbc6643bf88bda1f1fffc5c8c85628e29bfa
-size 486504
+oid sha256:c28fac86bd357c64626f1cb806edf22776c0ee4c8aeab4c6744ce0e8e18f42f3
+size 486766
