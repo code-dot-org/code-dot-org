b17114b60fbc600fe96bf8d65af9f49dd4ba0765
Continuous Integration <dev@code.org>
2015-10-27 23:41:56 +0000

    Automatically built.
    d6c639d Merge pull request #4865 from code-dot-org/fade-music-on-video (Brad Buchanan)
    2f068ca Removed trailing newline so lint passes (Andrew Oberhardt)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2bed1b32d98ca68a6fe227b17f744ef4e648a29854e476adfa890b9941d4959
-size 475964
+oid sha256:36de8f878418e7662f38d1c824c1dd28db7929a7bf78a6ee41a77b1d14b87c2d
+size 475966
