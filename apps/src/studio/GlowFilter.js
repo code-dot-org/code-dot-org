588e021d1800b85b1eb7c6cebcc2ffb212aca9b7
Brad Buchanan <brad@code.org>
2015-10-28 12:13:36 -0700

    Replace GlowFilter SMIL animation with javascript animation

--- apps/src/studio/GlowFilter.js
+++ apps/src/studio/GlowFilter.js
@@ -12,7 +12,7 @@
  */
 'use strict';
 
-require('../utils');
+var utils = require('../utils');
 var SVG_NS = require('../constants').SVG_NS;
 var ImageFilter = require('./ImageFilter');
 
@@ -24,6 +24,12 @@ var ImageFilter = require('./ImageFilter');
  */
 var GlowFilter = function (svg) {
   ImageFilter.call(this, svg);
+
+  /** @private {SVGElement} */
+  this.feCompositeLayers_ = null;
+
+  /** @private {function} */
+  this.curve_ = makeNormalOscillation(3000, 3, 0.1, 1.0);
 };
 GlowFilter.inherits(ImageFilter);
 module.exports = GlowFilter;
@@ -83,14 +89,12 @@ GlowFilter.prototype.createFilterSteps_ = function () {
   feCompositeLayers.setAttribute('k2', 1); // Always show 100% of original image
   feCompositeLayers.setAttribute('k3', 0);
   feCompositeLayers.setAttribute('k4', 0);
+  this.feCompositeLayers_ = feCompositeLayers;
 
-  // TODO: Replace this SMIL animation with a JS-driven one
-  var glowAnimation = document.createElementNS(SVG_NS, 'animate');
-  glowAnimation.setAttribute('attributeName', 'k3');
-  glowAnimation.setAttribute('values', [0, 0.2, 1, 0.2, 0].join(';'));
-  glowAnimation.setAttribute('dur', '3s');
-  glowAnimation.setAttribute('repeatCount', 'indefinite');
-  feCompositeLayers.appendChild(glowAnimation);
+  // Establish 30FPS update interval
+  this.intervalId_ = window.setInterval(function () {
+    this.update(new Date().getTime());
+  }.bind(this), 1000/30);
 
   return [
     feFloodWhite,
@@ -101,3 +105,41 @@ GlowFilter.prototype.createFilterSteps_ = function () {
     feCompositeLayers
   ];
 };
+
+/**
+ * Update animated filter effect.
+ * @param {number} t - simulation time value
+ */
+GlowFilter.prototype.update = function (t) {
+  if (this.feCompositeLayers_) {
+    this.feCompositeLayers_.setAttribute('k3', this.curve_(t));
+  }
+};
+
+/**
+ * Generates a function that given a time value "t" will produce a number
+ * between zero and one (inclusive) following a given curve between them.
+ *
+ * @param {number} period - the t-value for one complete cycle, from max to
+ *        min and back to max.  Must be nonzero.
+ * @param {number} [curve] - Determines the sharpness of the curve in the
+ *        oscillation.
+ *        Default 1 gives a triangle wave (no curve, just linear interpolation)
+ *        (0-1) gives a curve that spends more time above halfway than below it.
+ *        (1+) gives a curve that spends more time below halfway than above it
+ *             (like a repeated y=x*x curve)
+ *        May not work well for certain values of curve - make sure to test!
+ * @param {number} [min] - Smallest value of oscillation, default 0
+ * @param {number} [max] - Largest value of oscillation, default 1
+ */
+function makeNormalOscillation(period, curve, min, max) {
+  curve = utils.valueOr(curve, 1);
+  min = utils.valueOr(min, 0);
+  max = utils.valueOr(max, 1);
+  var delta = max - min;
+  var coefficient = delta * Math.pow(2 / period, curve);
+  var halfPeriod = period / 2;
+  return function (t) {
+    return min + coefficient * Math.abs(Math.pow((t % period) - halfPeriod, curve));
+  };
+}
