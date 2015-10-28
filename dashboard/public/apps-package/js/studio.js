685560d6343952744e8a7ad57806352483990dde
Continuous Integration <dev@code.org>
2015-10-28 21:40:07 +0000

    Automatically built.
    b3bbe93 Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging (Continuous Integration)
    baee630 content changes (-will) (Continuous Integration)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e7e7d1e62ddafe0bf7ad01fd6df22f64c948c50b5621e4d65cc718e6ddcda9e
-size 476030
+oid sha256:399265ffebfc504fab643c58c4ad1b88684c9d3960cf4a9d47a06660c1a17bcc
+size 475782
