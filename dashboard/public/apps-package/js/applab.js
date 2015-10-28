e47219fbff2a5f2d37ad4e858951678d37d436e7
Continuous Integration <dev@code.org>
2015-10-28 21:29:47 +0000

    Automatically built.
    349eeae Merge pull request #4894 from code-dot-org/improve-block-positioning-logic (Elijah Hamovitz)
    3bd5c3f Update README.md (David Bailey)
    bc71abc Merge pull request #4883 from code-dot-org/blockly_base_url2 (philbogle)
    f58b4bb Merge pull request #4905 from code-dot-org/revert-4822-project-expect-channel (Josh Lory)
    1f18dca Revert "Always expect a channel in `project.js`" (Josh Lory)
    402a74d Change to use stubs (Phil Bogle)
    8ba71fe Merge pull request #4863 from code-dot-org/templateBackedApplabTest (Bjvanminnen)
    4988b3f Merge pull request #4891 from code-dot-org/newProjectFromFooter (Bjvanminnen)
    1dafa5a Automatically built. (Continuous Integration)
    32a9e8f Merge pull request #4753 from code-dot-org/fun-o-meter (Elijah Hamovitz)
    d764685 Automatically built. (Continuous Integration)
    2331dfa Merge pull request #4886 from code-dot-org/hoc2015calloutfixes (Brendan Reville)
    e5110a2 Merge pull request #4898 from code-dot-org/hourofcode (Tanya Parker)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce860e6be310d3f4111a2ce21e8e94aaaee78ddb79d53f5a510dc6600dd7d0c9
-size 488238
+oid sha256:d57fbe193c313772abcfc5d91f2759f0a659a7a120d9884d3ae24a813d18defb
+size 488242
