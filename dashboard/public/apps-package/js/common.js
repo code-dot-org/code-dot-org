b12ba01c16c156e50b67120d10389c45801211d6
Continuous Integration <dev@code.org>
2015-10-29 16:59:55 +0000

    Automatically built.
    224dc67 Merge pull request #4786 from code-dot-org/pulse-item (Brad Buchanan)
    d619792 Automatically built. (Continuous Integration)
    8403a32 Merge pull request #4920 from code-dot-org/testfixes (Elijah Hamovitz)
    0ff348c Temporarily removing funOMeter test (Elijah Hamovitz)
    b457e24 Merge pull request #4916 from code-dot-org/ie10tests (Bjvanminnen)
    5b89e35 more IE10 (Brent Van Minnen)
    1876a7f o Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging (Trevor Berg)
    541fdc5 Fix issues with JSONFileDataAdapter (Trevor Berg)
    0f13914 iOS requires directly-bound click events and 'cursor: pointer' style for click events to work (Elijah Hamovitz)
    5b6b40f Feature-check SVG filter support before creating/applying a filter. (Brad Buchanan)
    656f7c2 Merge pull request #4877 from code-dot-org/bundle_install_frontends (Will Jordan)
    b7be14d Automatically built. (Continuous Integration)
    16bc449 Merge pull request #4903 from code-dot-org/only_setup_local_pegasus_db (philbogle)
    7460930 Merge pull request #4911 from code-dot-org/remerge-project-expect-channel (Josh Lory)
    f53452b start running applab tests on IE 10 (Brent Van Minnen)
    4907658 Merge pull request #4915 from code-dot-org/revert-4723-ajax_sign_in (philbogle)
    c266d62 Revert "Render the sign-in/sign-out button in the Dashboard header using Ajax" (philbogle)
    0dc2fb9 Header (and 'Remix' button) doesn't show up on mobile [ci skip] (Josh Lory)
    7317599 Merge pull request #4913 from code-dot-org/all-responses (Laurel)
    3130e6c Merge remote-tracking branch 'origin/staging' into pulse-item (Brad Buchanan)
    e28f355 Deduplicate functions and remove last SMIL TODO comment. (Brad Buchanan)
    9ac35c2 Merge pull request #4723 from code-dot-org/ajax_sign_in (philbogle)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46952a45cd7446829313012ee6e7f9d7d5f67c9b68cc6dca98795126a18932dd
-size 1391334
+oid sha256:d29cced59180c590e72abddd0aaf15de154cc58493ffcb7ef66e4e7dbd6d550f
+size 1391471
--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:399265ffebfc504fab643c58c4ad1b88684c9d3960cf4a9d47a06660c1a17bcc
-size 475782
+oid sha256:fd7ace64830f7d4b56558b60d0b98d2ddd51d9dd985e06639cd9365908a13c96
+size 496316
