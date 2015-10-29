bdd2bac59a33e34cb00da2b45d3a37dc4808acf3
Continuous Integration <dev@code.org>
2015-10-29 20:04:21 +0000

    Automatically built.
    c396487 Merge pull request #4933 from code-dot-org/music-metadata (Brad Buchanan)
    69d33c0 Automatically built. (Continuous Integration)
    0800e97 hoc2015: a few tiny level tweaks. (Brendan Reville)
    8901c70 Merge pull request #4828 from code-dot-org/hocsignup-count (Caley Brock)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23b7bcaeba17552cb48a04aa6c45f22f34d3068e4e75facdc8947f32573cdd70
-size 502382
+oid sha256:5589b24018c8f565d5bcef6d2360590cca81cc4965a56118bc95568adf27796d
+size 504153
