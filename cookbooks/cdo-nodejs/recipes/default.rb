0810bc1f23dca89c6f842bcb8af1e7803e90288b
Will Jordan <will@code.org>
2015-10-29 13:24:02 -0700

    update cdo-nodejs with conservative version attributes. add cdo-nodejs attribute documentation.

--- cookbooks/cdo-nodejs/recipes/default.rb
+++ cookbooks/cdo-nodejs/recipes/default.rb
@@ -3,6 +3,11 @@
 # Recipe:: default
 #
 
+node.default['nodejs']['repo'] = "https://deb.nodesource.com/node_#{node['cdo-nodejs']['version']}"
 include_recipe 'nodejs'
-nodejs_npm 'npm'
+
+nodejs_npm 'npm' do
+  version node['cdo-nodejs']['npm_version']
+end
+
 nodejs_npm 'grunt-cli'
