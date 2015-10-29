fba8ab0f1b533a65d42a3771419d8260070e4ecc
Will Jordan <will@code.org>
2015-10-29 13:24:02 -0700

    update cdo-nodejs with conservative version attributes. add cdo-nodejs attribute documentation.

--- cookbooks/cdo-nodejs/attributes/default.rb
+++ cookbooks/cdo-nodejs/attributes/default.rb
@@ -1,1 +1,2 @@
-default['nodejs']['repo'] = 'https://deb.nodesource.com/node_4.x'
+default['cdo-nodejs']['version'] = '0.12'
+default['cdo-nodejs']['npm_version'] = '2'
--- cookbooks/cdo-nodejs/test/integration/default/serverspec/node_spec.rb
+++ cookbooks/cdo-nodejs/test/integration/default/serverspec/node_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../../../kitchen/data/helper_spec'
 
 file_exist '/usr/bin/node'
-cmd 'node -v', 'v4.1.1'
-cmd 'npm -v', '3.3.4'
+cmd 'node -v', 'v0.12'
+cmd 'npm -v', '2.'
