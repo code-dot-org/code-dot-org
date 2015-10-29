4be8e514e9c21acb77f2c69a3913c2a884b0368f
Continuous Integration <dev@code.org>
2015-10-29 19:54:17 +0000

    Automatically built.
    0800e97 hoc2015: a few tiny level tweaks. (Brendan Reville)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac64ddb2fd913b2c207642ce6a777a4ba1af170ac8d5f2d0be4f7c4f9060bf06
-size 502407
+oid sha256:23b7bcaeba17552cb48a04aa6c45f22f34d3068e4e75facdc8947f32573cdd70
+size 502382
