d062cd93f618612eb79c3a3fad7da52743053658
Continuous Integration <dev@code.org>
2015-10-30 16:40:09 +0000

    Automatically built.
    a6611e4 Merge pull request #4962 from code-dot-org/unique-screen-ids (David Bailey)
    9535103 Merge pull request #4967 from code-dot-org/hourofcode (Tanya Parker)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -9954,106 +9954,7 @@ module.exports = {
 };
 
 
-},{"./BooleanPropertyRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/BooleanPropertyRow.jsx","./ColorPickerPropertyRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/ColorPickerPropertyRow.jsx","./EventHeaderRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/EventHeaderRow.jsx","./EventRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/EventRow.jsx","./ImagePickerPropertyRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/ImagePickerPropertyRow.jsx","./PropertyRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/PropertyRow.jsx","./ZOrderRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/ZOrderRow.jsx","./elementUtils":"/home/ubuntu/staging/apps/build/js/applab/designElements/elementUtils.js"}],"/home/ubuntu/staging/apps/build/js/applab/designElements/elementUtils.js":[function(require,module,exports){
-var constants = require('../constants');
-var utils = require('../../utils');
-
-// Taken from http://stackoverflow.com/a/3627747/2506748
-module.exports.rgb2hex = function (rgb) {
-  if (rgb === '') {
-    return rgb;
-  }
-  var parsed = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-  if (parsed === null) {
-    return rgb;
-  }
-  function hex(x) {
-    return ("0" + parseInt(x).toString(16)).slice(-2);
-  }
-  return "#" + hex(parsed[1]) + hex(parsed[2]) + hex(parsed[3]);
-};
-
-/**
- * Gets the element's id, stripping the prefix.
- * @param element {Element}
- * @param prefix {string} Optional. Defaults to DESIGN_ELEMENT_ID_PREFIX.
- * @returns {string} The element id with prefix stripped, or null if it had no id.
- */
-var getId = module.exports.getId = function (element, prefix) {
-  var elementId = element.getAttribute('id');
-  if (elementId === null) {
-    return null;
-  }
-  prefix = utils.valueOr(prefix, constants.DESIGN_ELEMENT_ID_PREFIX);
-  checkId(element, prefix);
-  return elementId.substr(prefix.length);
-};
-
-/**
- * Sets the element's id, adding the prefix.
- * @param element {Element}
- * @param value (string)
- * @param prefix {string} Optional. Defaults to DESIGN_ELEMENT_ID_PREFIX.
- */
-var setId = module.exports.setId = function (element, value, prefix) {
-  if (value === null) {
-    return;
-  }
-  prefix = utils.valueOr(prefix, constants.DESIGN_ELEMENT_ID_PREFIX);
-  element.setAttribute('id', prefix + value);
-};
-
-/**
- * Throws an error if the element's id does not start with the prefix.
- * @param element {Element}
- * @param prefix {string}
- */
-function checkId(element, prefix) {
-  if (element.id.substr(0, prefix.length) !== prefix) {
-    throw new Error('element.id "' + element.id + '" does not start with prefix "' + prefix + '".');
-  }
-}
-
-/**
- * Add the prefix to the elementId and returns the element with that id.
- * @param elementId {string}
- * @param prefix {string} Optional. Defaults to DESIGN_ELEMENT_ID_PREFIX.
- * @returns {Element}
- */
-module.exports.getPrefixedElementById = function(elementId, prefix) {
-  prefix = prefix === undefined ? constants.DESIGN_ELEMENT_ID_PREFIX : prefix;
-  return document.getElementById(prefix + elementId);
-};
-
-/**
- * Adds the prefix to the element's id.
- * @param element {Element}
- * @param prefix {string} Optional prefix to add. Defaults to ''.
- * @returns {Element}
- */
-module.exports.addIdPrefix = function (element, prefix) {
-  prefix = utils.valueOr(prefix, '');
-  setId(element, element.getAttribute('id'), prefix);
-};
-
-/**
- * Removes the DESIGN_ELEMENT_ID_PREFIX from the element's id.
- * @param element {Element}
- * @returns {Element}
- */
-module.exports.removeIdPrefix = function (element) {
-  element.setAttribute('id', getId(element));
-};
-
-
-},{"../../utils":"/home/ubuntu/staging/apps/build/js/utils.js","../constants":"/home/ubuntu/staging/apps/build/js/applab/constants.js"}],"/home/ubuntu/staging/apps/build/js/applab/constants.js":[function(require,module,exports){
-module.exports = {
-  FOOTER_HEIGHT: 30,
-  DESIGN_ELEMENT_ID_PREFIX: 'design_'
-};
-
-
-},{}],"/home/ubuntu/staging/apps/build/js/applab/designElements/ZOrderRow.jsx":[function(require,module,exports){
+},{"./BooleanPropertyRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/BooleanPropertyRow.jsx","./ColorPickerPropertyRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/ColorPickerPropertyRow.jsx","./EventHeaderRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/EventHeaderRow.jsx","./EventRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/EventRow.jsx","./ImagePickerPropertyRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/ImagePickerPropertyRow.jsx","./PropertyRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/PropertyRow.jsx","./ZOrderRow.jsx":"/home/ubuntu/staging/apps/build/js/applab/designElements/ZOrderRow.jsx","./elementUtils":"/home/ubuntu/staging/apps/build/js/applab/designElements/elementUtils.js"}],"/home/ubuntu/staging/apps/build/js/applab/designElements/ZOrderRow.jsx":[function(require,module,exports){
 var rowStyle = require('./rowStyle');
 
 var ZOrderRow = React.createClass({displayName: "ZOrderRow",
@@ -10142,6 +10043,8 @@ module.exports = ZOrderRow;
 },{"./rowStyle":"/home/ubuntu/staging/apps/build/js/applab/designElements/rowStyle.js"}],"/home/ubuntu/staging/apps/build/js/applab/designElements/PropertyRow.jsx":[function(require,module,exports){
 /* global $ */
 var rowStyle = require('./rowStyle');
+var elementUtils = require('./elementUtils');
+var utils = require('../../utils');
 
 var LockState = {
   LOCKED: 'LOCKED',
@@ -10165,18 +10068,39 @@ var PropertyRow = React.createClass({displayName: "PropertyRow",
 
   getInitialState: function () {
     return {
-      value: this.props.initialValue
+      value: this.props.initialValue,
+      isValidValue: true
     };
   },
 
   componentWillReceiveProps: function (newProps) {
-    this.setState({value: newProps.initialValue});
+    this.setState({
+      value: newProps.initialValue,
+      isValidValue: true
+    });
   },
 
   handleChangeInternal: function(event) {
     var value = event.target.value;
-    this.props.handleChange(value);
-    this.setState({value: value});
+    var isValidValue = !this.props.isIdRow || elementUtils.isIdAvailable(value, this.props.initialValue);
+    this.setValue(value, isValidValue);
+  },
+
+  /**
+   * Updates this component's state, and calls the change handler
+   * only if the new value is valid.
+   * @param value {string} The new value of the property row.
+   * @param isValidValue {boolean} Whether the value is valid. Default: true.
+   */
+  setValue: function (value, isValidValue) {
+    isValidValue = utils.valueOr(isValidValue, true);
+    this.setState({
+      value: value,
+      isValidValue: isValidValue
+    });
+    if (isValidValue) {
+      this.props.handleChange(value);
+    }
   },
 
   handleClickLock: function () {
@@ -10187,10 +10111,20 @@ var PropertyRow = React.createClass({displayName: "PropertyRow",
     }
   },
 
+  onIdRowBlur: function() {
+    if (!this.state.isValidValue) {
+      var value = this.props.initialValue;
+      this.setValue(value);
+    }
+  },
+
   render: function() {
     var idRowStyle = $.extend({}, rowStyle.container, rowStyle.maxWidth, {
       backgroundColor: '#a69bc1',
       paddingBottom: 10
+    });
+    var inputStyle = $.extend({}, rowStyle.input, {
+      backgroundColor: this.state.isValidValue ? null : "#ffcccc"
     });
 
     var inputElement;
@@ -10203,7 +10137,8 @@ var PropertyRow = React.createClass({displayName: "PropertyRow",
         type: this.props.isNumber ? 'number' : undefined, 
         value: this.state.value, 
         onChange: this.handleChangeInternal, 
-        style: rowStyle.input});
+        onBlur: this.props.isIdRow ? this.onIdRowBlur : null, 
+        style: inputStyle});
     }
 
     var lockStyle = {
@@ -10238,7 +10173,139 @@ PropertyRow.LockState = LockState;
 module.exports = PropertyRow;
 
 
-},{"./rowStyle":"/home/ubuntu/staging/apps/build/js/applab/designElements/rowStyle.js"}],"/home/ubuntu/staging/apps/build/js/applab/designElements/ImagePickerPropertyRow.jsx":[function(require,module,exports){
+},{"../../utils":"/home/ubuntu/staging/apps/build/js/utils.js","./elementUtils":"/home/ubuntu/staging/apps/build/js/applab/designElements/elementUtils.js","./rowStyle":"/home/ubuntu/staging/apps/build/js/applab/designElements/rowStyle.js"}],"/home/ubuntu/staging/apps/build/js/applab/designElements/elementUtils.js":[function(require,module,exports){
+var constants = require('../constants');
+var utils = require('../../utils');
+
+// Taken from http://stackoverflow.com/a/3627747/2506748
+module.exports.rgb2hex = function (rgb) {
+  if (rgb === '') {
+    return rgb;
+  }
+  var parsed = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  if (parsed === null) {
+    return rgb;
+  }
+  function hex(x) {
+    return ("0" + parseInt(x).toString(16)).slice(-2);
+  }
+  return "#" + hex(parsed[1]) + hex(parsed[2]) + hex(parsed[3]);
+};
+
+/**
+ * Gets the element's id, stripping the prefix.
+ * @param element {Element}
+ * @param prefix {string} Optional. Defaults to DESIGN_ELEMENT_ID_PREFIX.
+ * @returns {string} The element id with prefix stripped, or null if it had no id.
+ */
+var getId = module.exports.getId = function (element, prefix) {
+  var elementId = element.getAttribute('id');
+  if (elementId === null) {
+    return null;
+  }
+  prefix = utils.valueOr(prefix, constants.DESIGN_ELEMENT_ID_PREFIX);
+  checkId(element, prefix);
+  return elementId.substr(prefix.length);
+};
+
+/**
+ * Sets the element's id, adding the prefix.
+ * @param element {Element}
+ * @param value (string)
+ * @param prefix {string} Optional. Defaults to DESIGN_ELEMENT_ID_PREFIX.
+ */
+var setId = module.exports.setId = function (element, value, prefix) {
+  if (value === null) {
+    return;
+  }
+  prefix = utils.valueOr(prefix, constants.DESIGN_ELEMENT_ID_PREFIX);
+  element.setAttribute('id', prefix + value);
+};
+
+/**
+ * Throws an error if the element's id does not start with the prefix.
+ * @param element {Element}
+ * @param prefix {string}
+ */
+function checkId(element, prefix) {
+  if (element.id.substr(0, prefix.length) !== prefix) {
+    throw new Error('element.id "' + element.id + '" does not start with prefix "' + prefix + '".');
+  }
+}
+
+/**
+ * Add the prefix to the elementId and returns the element with that id.
+ * @param elementId {string}
+ * @param prefix {string} Optional. Defaults to DESIGN_ELEMENT_ID_PREFIX.
+ * @returns {Element}
+ */
+var getPrefixedElementById = module.exports.getPrefixedElementById = function(elementId, prefix) {
+  prefix = prefix === undefined ? constants.DESIGN_ELEMENT_ID_PREFIX : prefix;
+  return document.getElementById(prefix + elementId);
+};
+
+/**
+ * Adds the prefix to the element's id.
+ * @param element {Element}
+ * @param prefix {string} Optional prefix to add. Defaults to ''.
+ * @returns {Element}
+ */
+module.exports.addIdPrefix = function (element, prefix) {
+  prefix = utils.valueOr(prefix, '');
+  setId(element, element.getAttribute('id'), prefix);
+};
+
+/**
+ * Removes the DESIGN_ELEMENT_ID_PREFIX from the element's id.
+ * @param element {Element}
+ * @returns {Element}
+ */
+module.exports.removeIdPrefix = function (element) {
+  element.setAttribute('id', getId(element));
+};
+
+/**
+ * Returns true if newId is available and won't collide with
+ * other elements in design mode or the rest of applab.
+ * @param {string} newId The id to evaluate.
+ * @param {string} originalId The original id of this element.
+ *     This will always be a valid value for newId.
+ * @returns {boolean}
+ */
+module.exports.isIdAvailable = function(newId, originalId) {
+  if (!newId) {
+    return false;
+  }
+  if (newId === originalId) {
+    return true;
+  }
+
+  // Don't allow if any other element in design mode has this prefixed id.
+  if (getPrefixedElementById(newId)) {
+    return false;
+  }
+
+  // Don't allow if any element outside of divApplab has this id.
+  //
+  // Elements in divApplab must be ignored since divApplab may be stale
+  // with respect to what's in design mode, and we already caught any
+  // collisions with design mode elements in the previous condition.
+  var element = document.getElementById(newId);
+  if (element && !$('#divApplab').find(element)[0]) {
+    return false;
+  }
+
+  return true;
+};
+
+},{"../../utils":"/home/ubuntu/staging/apps/build/js/utils.js","../constants":"/home/ubuntu/staging/apps/build/js/applab/constants.js"}],"/home/ubuntu/staging/apps/build/js/applab/constants.js":[function(require,module,exports){
+module.exports = {
+  FOOTER_HEIGHT: 30,
+  DESIGN_ELEMENT_ID_PREFIX: 'design_'
+};
+
+
+},{}],"/home/ubuntu/staging/apps/build/js/applab/designElements/ImagePickerPropertyRow.jsx":[function(require,module,exports){
 var showAssetManager = require('../assetManagement/show.js');
 var rowStyle = require('./rowStyle');
 
