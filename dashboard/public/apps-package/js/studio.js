fdb821a752b4ff14c468dfa270c9999e876af4bc
Continuous Integration <dev@code.org>
2015-10-30 23:44:50 +0000

    Automatically built.
    e47e2c7 Merge pull request #4997 from code-dot-org/fix-studio-keep-playing (Brad Buchanan)
    580e570 Merge pull request #4999 from code-dot-org/change-script-settings (Caley Brock)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08d2c1ed65b3be31da67922fc8a0bc0f90e5c486776f4538fe0a847710bbb7c0
-size 509111
+oid sha256:b0a6487f5963382a4cbe0bf0a6729a8004ebbb0349ba991d291695875dff1c55
+size 509113
