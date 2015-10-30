963c4931424d9d36eaf74fc52f4889782311f018
Continuous Integration <dev@code.org>
2015-10-30 20:52:05 +0000

    Automatically built.
    091094c Merge pull request #4981 from code-dot-org/all-responses-csv (Laurel)
    3d25a87 Merge branch 'staging' of github.com:code-dot-org/code-dot-org into staging (Brendan Reville)
    12f9509 Remove trailing whitespace. (Brendan Reville)
    bcbdbf2 Automatically built. (Continuous Integration)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9df1c93a881992708043124720e60cea6f2dc509b1db3e672d4b6de4f2057b9
-size 509211
+oid sha256:b2f5a59669648a2fa6ce1441cfa180ad281cc26c283510c50ba960c14deba28b
+size 509157
