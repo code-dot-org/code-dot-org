cc8fb2dbd487e0763b4811ef3e2ea7435285e00e
Mehal Shah <mehal@code.org>
2015-10-30 12:27:45 -0700

    CR feedback

--- dashboard/app/assets/javascripts/sendReport.js
+++ dashboard/app/assets/javascripts/sendReport.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86da3a70b31ba8fbc137062d90900b52f4ffacd24ebd6a4e9b4f430b85d49f06
-size 4086
+oid sha256:8752a9164e974a8ee88ef092374c3dee908614f7141e54341fb605a1de59b2f9
+size 4372
