49d7c41f9f3a839113b4b8c878f5757c551eccc7
Continuous Integration <dev@code.org>
2015-10-30 20:35:39 +0000

    Automatically built.
    52e8643 Merge pull request #4983 from code-dot-org/droplet-update-2015-10-30 (Josh Lory)
    a4b3fd8 Merge pull request #4982 from code-dot-org/fix_missing_semicolon (Mehal Shah)
    decb304 Fix missing semicolon (Mehal Shah)

--- dashboard/public/apps-package/js/droplet/droplet-full.js
+++ dashboard/public/apps-package/js/droplet/droplet-full.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23e0061c5b6f6e279cda11109c9ec574dcdbd89cccb5d476f0622dab6ac4fb5f
-size 1571114
+oid sha256:b25f30613ab019e95917069f742cc1a1778a52949bb7c328da190f1547b07f14
+size 307757
