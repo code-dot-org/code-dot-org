a39215a179105e3e596e0ab22798d34717078647
Mehal Shah <mehal@code.org>
2015-10-29 17:38:35 -0700

    Allow Gatekeeper to block milestone posting for specific script level names

--- dashboard/app/assets/javascripts/sendReport.js
+++ dashboard/app/assets/javascripts/sendReport.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebaaed62e0994e719da97ed3ef3381816dbc51eed6ce4d16f3a689285c3c9448
-size 3636
+oid sha256:86da3a70b31ba8fbc137062d90900b52f4ffacd24ebd6a4e9b4f430b85d49f06
+size 4086
