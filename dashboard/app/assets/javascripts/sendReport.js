478028bf65ebcd3d698986e39fdcc133c0a39133
Mehal Shah <mehal@code.org>
2015-10-30 13:11:33 -0700

    Fix missing semicolon

--- dashboard/app/assets/javascripts/sendReport.js
+++ dashboard/app/assets/javascripts/sendReport.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8752a9164e974a8ee88ef092374c3dee908614f7141e54341fb605a1de59b2f9
-size 4372
+oid sha256:c2c3c87a725c81a8b383b2011cedb71ef5687d2cdecfb832423c9ecef3bc1567
+size 4373
