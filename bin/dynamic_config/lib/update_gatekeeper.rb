80e9b20da7804f82cc8c7b3e61f407cb316f2d07
Continuous Integration <dev@code.org>
2015-11-02 23:33:25 +0000

    Work in any path

--- bin/dynamic_config/lib/update_gatekeeper.rb
+++ bin/dynamic_config/lib/update_gatekeeper.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 puts "Loading environment..."
-require_relative '../../adhoc/dashboard/config/environment'
+require_relative '../../../dashboard/config/environment'
 require 'dynamic_config/loaders/gatekeeper_loader'
 
 puts "Updating config..."
--- bin/dynamic_config/update_gatekeeper.sh
+++ bin/dynamic_config/update_gatekeeper.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
+SCRIPT=$(readlink -f $0)
+SCRIPT_PATH=`dirname $SCRIPT`
 FULL_CONFIG_PATH=`readlink -f "$1"`
-cd /home/ubuntu/adhoc/dashboard
-bundle exec ruby /home/ubuntu/dynamic_config/lib/update_gatekeeper.rb $FULL_CONFIG_PATH
-cd -
+
+cd $SCRIPT_PATH/../..
+bundle exec ruby $SCRIPT_PATH/lib/update_gatekeeper.rb $FULL_CONFIG_PATH
