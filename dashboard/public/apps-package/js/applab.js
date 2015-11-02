00f2ae24bcfba5a0d0de853879cf000bf2ea933c
Continuous Integration <dev@code.org>
2015-11-02 19:00:38 +0000

    Automatically built.
    be81509 Merge pull request #5018 from code-dot-org/firefox-console (Trevor Berg)
    428695e content changes (-asher) (Continuous Integration)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9213261f426c2fe639806ec5e99ea188c8dd1c632e89dcbfa70a032c78ca3655
-size 490909
+oid sha256:11a5197d726022e7450de5fb35548421e743f227deb2ada5ec7274e8e11c499b
+size 490963
