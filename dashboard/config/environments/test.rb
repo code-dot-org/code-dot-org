6ccc6c163eab9d6a9b66d5e2d6146eaad22e5411
Phil Bogle <phil@code.org>
2015-11-02 13:46:01 -0800

    Use consistent memory cache store in all environments and increase size to 256MB

--- dashboard/config/environments/test.rb
+++ dashboard/config/environments/test.rb
@@ -19,7 +19,6 @@ Dashboard::Application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.cache_store = :memory_store
 
 #  config.action_mailer.raise_delivery_errors = true
 #  config.action_mailer.delivery_method = :smtp
