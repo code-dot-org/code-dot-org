208c690b025cdade239d76f24dce3d8a7813b957
Phil Bogle <phil@code.org>
2015-11-02 15:00:19 -0800

    Remove environment changes

--- dashboard/config/environment.rb
+++ dashboard/config/environment.rb
@@ -7,16 +7,3 @@ Encoding.default_internal = Encoding::UTF_8
 
 # Initialize the Rails application.
 Dashboard::Application.initialize!
-
-Dashboard::Application.configure do
-
-  MAX_CACHED_BYTES = 256.megabytes
-  if CDO.memcached_hosts.present?
-    config.cache_store = :mem_cache_store, CDO.memcached_hosts, {
-      value_max_bytes: MAX_CACHED_BYTES
-    }
-  else
-    config.cache_store = :memory_store, { size: MAX_CACHED_BYTES }
-  end
-
-end
