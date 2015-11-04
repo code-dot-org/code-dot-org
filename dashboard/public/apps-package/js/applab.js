d9edeab32413507eb663c39260028bfef3237598
Continuous Integration <dev@code.org>
2015-11-04 00:52:37 +0000

    Automatically built.
    2870eae Merge pull request #5052 from code-dot-org/applab-remove-finish-icon (Josh Lory)
    b1b0f19 Merge pull request #5066 from code-dot-org/fixmaplegend (Tanya Parker)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fba106576b3ddab2923224186de80f09ee7ec159c921d7f9c55f991ff16e5b9e
-size 491669
+oid sha256:148f88b24f4136c1e39606dc92f30e951c2e384fd3aca0224245abf5e9ff36b6
+size 491723
