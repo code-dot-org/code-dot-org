d12989b7deddfc17a0a4bfc425a42c841abf6660
Continuous Integration <dev@code.org>
2015-11-04 18:31:28 +0000

    Automatically built.
    0bbab9c Merge pull request #5064 from code-dot-org/new-hazard-reaction (Brad Buchanan)
    de887b1 Ignore .ruby-version since not everyone manages ruby with rbenv. (Geoffrey Elliott)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:775a441a124fb68370fc1f933fe1eb11e8cae06534a7fef6f3821430ec2e6e46
-size 517818
+oid sha256:ac62418ce9732882f12e624e84a420c5c508830ca367d92c93a7e5688fd3803f
+size 517982
