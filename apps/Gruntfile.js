1b9ac47f545cefb67600c2094a703087b63b0dc6
Brent Van Minnen <bjvanminnen@gmail.com>
2015-11-05 11:01:41 -0800

    minified version of jquery fileupload libs

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -199,8 +199,15 @@ config.copy = {
       {
         expand: true,
         cwd: 'lib/fileupload',
-        src: ['*.js'],
-        dest: 'build/package/js/fileupload/'
+        src: [
+          'jquery.fileupload' + dotMinIfNotDev + '.js',
+          'jquery.iframe-transport' + dotMinIfNotDev + '.js'
+        ],
+        dest: 'build/package/js/fileupload/',
+        rename: function (src, dest) {
+          // dest name should be the same, whether or not minified
+          return src + dest.replace(/\.min.js$/, '.js');
+        }
       },
       {
         expand: true,
