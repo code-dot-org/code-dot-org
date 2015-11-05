4f6995fd22a228cfecb7a20c7f9042d1fb429c85
Will Jordan <will@code.org>
2015-11-05 14:26:28 -0800

    remove unused database methods from hourofcode_helpers

--- pegasus/helpers/hourofcode_helpers.rb
+++ pegasus/helpers/hourofcode_helpers.rb
@@ -149,10 +149,6 @@ def campaign_date(format)
   end
 end
 
-def all_unique_events
-  DB[:forms].where(kind: 'HocSignup2015').group(:email, :name).select(:name, :email, :processed_data, :data)
-end
-
 def company_count
   return fetch_hoc_metrics['hoc_company_totals'][@company]
 end
@@ -169,8 +165,4 @@ end
 
 def country_full_name
   return HOC_COUNTRIES[@country]['full_name']
-end
-
-def total_hoc_count
-  DB[:forms].where(kind: 'HocSignup2015').group(:email, :name).select(:email, :name).count
 end
