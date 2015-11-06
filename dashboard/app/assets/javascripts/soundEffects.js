be719c297dd0f7e0fbe32c73e2ac4731dce08f64
Brad Buchanan <brad@code.org>
2015-11-05 16:03:08 -0800

    Minimum gain > 0 is only necessary for WebAudio

--- dashboard/app/assets/javascripts/soundEffects.js
+++ dashboard/app/assets/javascripts/soundEffects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:621980c114bd4c840b690818b5f1876abf32d850eb14032b1f8be3a1d9fe3c7e
+oid sha256:da68e161fe5cf9823f43e9654dbb4c92dad89cfa5088a91f70b5b4c5968d68a8
 size 13469
