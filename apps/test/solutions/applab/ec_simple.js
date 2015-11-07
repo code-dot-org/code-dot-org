a8483b6e2a9d81438c872a1dc9176c4c66702b73
Brent Van Minnen <bjvanminnen@gmail.com>
2015-11-06 16:44:46 -0800

    more basic coverage of applab APIs from mochaTest

--- apps/test/solutions/applab/ec_simple.js
+++ apps/test/solutions/applab/ec_simple.js
@@ -26,6 +26,148 @@ module.exports = {
       },
     },
 
+    // These exercise all of the blocks in Control category
+    // It does not validate that they behave correctly, just that we don't end
+    // up with an errors
+    {
+      description: "Control",
+      editCode: true,
+      xml:
+        'var count = 0;' +
+        'for (var i = 0; i < 4; i++) {' +
+        '  count++;' +
+        '}' +
+        'i = 5;' +
+        'while(i > 0) {' +
+        '  count++;' +
+        '  i--;' +
+        '}' +
+        'if (count > 0) {' +
+        '  count++;' +
+        '}' +
+        'if (count < 0) {' +
+        '  count++;' +
+        '} else {' +
+        '  count--;' +
+        '}' +
+        'getTime();' +
+        'var interval = setInterval(function() {' +
+        '  count++;' +
+        '}, 100);' +
+        'clearInterval(interval);' +
+        'var timeout = setTimeout(function() {' +
+        '  console.log(count);' +
+        '  clearTimeout(timeout);' +
+        '}, 200);',
+
+      runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        testUtils.runOnAppTick(Applab, 200, function () {
+          Applab.onPuzzleComplete();
+        });
+      },
+      customValidator: function (assert) {
+        // No errors in output console
+        var debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, "9");
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      },
+    },
+
+    // These exercise all of the blocks in Control category
+    // It does not validate that they behave correctly, just that we don't end
+    // up with an errors
+    {
+      description: "Variables",
+      editCode: true,
+      xml:
+      // prompt and promptNum are covered in dropletUtilsTest
+        'var x = 1;\n' +
+        'var y;\n' +
+        'y = 2;\n' +
+        'console.log("message");\n' +
+        'var str = "Hello World";\n' +
+        'var a = str.substring(6, 11);\n' +
+        'var b = str.indexOf("World");\n' +
+        'var c = str.includes("World");\n' +
+        'var d = str.length;\n' +
+        'var e = str.toUpperCase();\n' +
+        'var f = str.toLowerCase();\n' +
+        'var list = ["a", "b", "d"];\n' +
+        'var g = list.length;\n' +
+        'insertItem(list, 2, "c");\n' +
+        'appendItem(list, "f");\n' +
+        'removeItem(list, 0);\n',
+
+      runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        testUtils.runOnAppTick(Applab, 2, function () {
+          Applab.onPuzzleComplete();
+        });
+      },
+      customValidator: function (assert) {
+        // No errors in output console
+        var debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, "message");
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      },
+    },
+
+    // These exercise all of the blocks in canvas category
+    // It does not validate that they behave correctly, just that we don't end
+    // up with an errors
+    {
+      description: "Canvas",
+      editCode: true,
+      xml:
+        'createCanvas("id", 320, 480);\n' +
+        'setActiveCanvas("id");\n' +
+        'line(0, 0, 160, 240);\n' +
+        'circle(160, 240, 100);\n' +
+        'rect(80, 120, 160, 240);\n' +
+        'setStrokeWidth(3);\n' +
+        'setStrokeColor("red");\n' +
+        'setFillColor("yellow");\n' +
+        'drawImage("id", 0, 0);\n' +
+        'var imgData = getImageData(0, 0, 320, 480);\n' +
+        'putImageData(imgData, 0, 0);\n' +
+        'clearCanvas();\n' +
+        'getRed(imgData, 0, 0);\n' +
+        'getGreen(imgData, 0, 0);\n' +
+        'getBlue(imgData, 0, 0);\n' +
+        'getAlpha(imgData, 0, 0);\n' +
+        'setRed(imgData, 0, 0, 255);\n' +
+        'setGreen(imgData, 0, 0, 255);\n' +
+        'setBlue(imgData, 0, 0, 255);\n' +
+        'setAlpha(imgData, 0, 0, 255);\n' +
+        'setRGB(imgData, 0, 0, 255, 255, 255);\n',
+
+      runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        testUtils.runOnAppTick(Applab, 2, function () {
+          Applab.onPuzzleComplete();
+        });
+      },
+      customValidator: function (assert) {
+        // No errors in output console
+        var debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, "");
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      }
+    },
+
     // These exercise all of the blocks in Turtle category
     // It does not validate that they behave correctly, just that we don't end
     // up with an errors
@@ -54,6 +196,40 @@ module.exports = {
         'show();\n' +
         'hide();\n' +
         'speed(50);\n',
+
+      runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        testUtils.runOnAppTick(Applab, 2, function () {
+          Applab.onPuzzleComplete();
+        });
+      },
+      customValidator: function (assert) {
+        // No errors in output console
+        var debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, "");
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      }
+    },
+
+    // These exercise all of the blocks in Turtle category
+    // It does not validate that they behave correctly, just that we don't end
+    // up with an errors
+    {
+      description: "Functions",
+      editCode: true,
+      xml:
+        'function myFunction() {\n' +
+        '  \n' +
+        '}\n' +
+        'function myFunction2(n) {\n' +
+        '  return n;\n' +
+        '}\n' +
+        'myFunction();\n' +
+        'myFunction2(1);\n',
 
       runBeforeClick: function (assert) {
         // add a completion on timeout since this is a freeplay level
