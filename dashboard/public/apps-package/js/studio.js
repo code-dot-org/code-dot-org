6e4d21ae711b086d1e4e14306430e7295aab9a3a
Continuous Integration <dev@code.org>
2015-11-07 08:56:04 +0000

    Automatically built.
    1aca5df Merge pull request #5163 from code-dot-org/disable-glow-in-safari (Brad Buchanan)
    cba7f8f Define cliprect inside <g> element as Safari workaround. (Brad Buchanan)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ca2b7cbe20b1f71e5328abaf3fd80c700506500d38c84f75da10fb5a6f41de2
-size 542221
+oid sha256:821d54422655b0d16a152f37cb721a5cf403cc21a30c60207f8907ad72338521
+size 541996
