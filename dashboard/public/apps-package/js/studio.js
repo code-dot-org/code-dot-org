abd7bb684a3a7d51a0c220eb6374fdd81ae193ea
Continuous Integration <dev@code.org>
2015-11-09 21:31:54 +0000

    Automatically built.
    e1af3fd Merge pull request #5230 from code-dot-org/hoc2015roundclouds (Brendan Reville)
    af983be nm only run 85 tests (Laurel Fan)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a4cd09c775f1f91829383a6175616a89078ba0b09d27853a9bd8bf35e8d5472
-size 543372
+oid sha256:ea71286d53ae3ae501893643fcd57d9509e160d5cba5681a0bb9f7d57a94ec96
+size 543396
