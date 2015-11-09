194041ad6debb17c1a5bb12bc5c707249bfbb476
Continuous Integration <dev@code.org>
2015-11-09 08:38:57 +0000

    Automatically built.
    1b8bcf0 Merge pull request #5208 from code-dot-org/hoc2015disableclouds (Brendan Reville)
    2a8756b hoc20215: disable clouds temporarily (Brendan Reville)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eae4a0c88da36d716e704c8659455e85cec71a5c20026b442f87c2037b627291
-size 544081
+oid sha256:7220fbee57e4240482da01fedd30f29ae16469b304875f6d59c676c2c155de5e
+size 544111
