6af01ee6be324edf49ce04bfb4a894eeae080cff
Continuous Integration <dev@code.org>
2015-11-10 23:04:23 +0000

    Automatically built.
    9a82f73 Merge pull request #5255 from code-dot-org/music-on-hide-instructions (Brad Buchanan)
    9cba823 Automatically built. (Continuous Integration)
    19700f8 Merge pull request #5235 from code-dot-org/no-standalone-project-no-remix (Josh Lory)
    a992640 New star wars strings (caley) (Continuous Integration)
    dc7a401 Merge pull request #5261 from code-dot-org/pagemode (philbogle)
    7bfe1c8 Merge pull request #5249 from code-dot-org/funOMeterDashboard (ashercodeorg)
    c5fcfe5 Merge pull request #5183 from code-dot-org/addShareEvents (ashercodeorg)
    b3f162b Automatically built. (Continuous Integration)
    1e9cf41 Merge pull request #5262 from code-dot-org/hoc2015_block_setDroid (Chris Pirich)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9149676efd2b646e9f21d5a79bf643f521e8b72abee3b699014bf3aee7b189a9
-size 549256
+oid sha256:37c8a12689747b2ecaf095f07dad7db377051d4e8ff7b728285bdd0d2320d5f2
+size 549767
