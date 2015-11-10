fa37bb8e89af343d27c5f306d7ff2f6c39a54816
Asher Kach <asher@code.org>
2015-11-10 11:52:26 -0600

    [WIP] Refactors the ReportsController, breaking its overgrown content into multiple controllers based on access permissioning and functionality.

--- dashboard/app/controllers/reports_controller.rb
+++ dashboard/app/controllers/reports_controller.rb
@@ -80,30 +80,6 @@ SQL
     render locals: {percentages_by_day: percentages_by_day.to_a.map{|k,v|[k.to_s,v.to_f]}}
   end
 
-  def search_for_teachers
-    authorize! :read, :reports
-
-    email_filter = "%#{params[:emailFilter]}%"
-    address_filter = "%#{params[:addressFilter]}%"
-
-    # TODO(asher): Determine whether we should be doing an inner join or a left
-    # outer join.
-    @teachers = User.where(user_type: 'teacher').where("email LIKE ?", email_filter).where("full_address LIKE ?", address_filter).joins(:followers).group('followers.user_id')
-
-    # If requested, join with the workshop_attendance table to filter out based
-    # on PD attendance.
-    if params[:pd] == "pd"
-      @teachers = @teachers.joins("INNER JOIN workshop_attendance ON users.id = workshop_attendance.teacher_id").distinct
-    elsif params[:pd] == "nopd"
-      @teachers = @teachers.joins("LEFT OUTER JOIN workshop_attendance ON users.id = workshop_attendance.teacher_id").where("workshop_attendance.teacher_id IS NULL").distinct
-    end
-
-    # Prune the set of fields to those that will be displayed.
-    @teacher_limit = 100
-    @headers = ['ID', 'Name', 'Email', 'Address', 'Num Students']
-    @teachers = @teachers.limit(@teacher_limit).pluck('id', 'name', 'email', 'full_address', 'COUNT(followers.id) AS num_students')
-  end
-
   def csp_pd_responses
     authorize! :read, :reports
 
@@ -261,14 +237,6 @@ SQL
     else
       flash[:alert] = 'User not found'
       render :assume_identity_form
-    end
-  end
-
-  def lookup_section
-    authorize! :manage, :all
-    @section = Section.find_by_code params[:section_code]
-    if params[:section_code] && @section.nil?
-      flash[:alert] = 'Section code not found'
     end
   end
 
