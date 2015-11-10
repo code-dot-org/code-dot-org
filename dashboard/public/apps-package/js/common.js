7c9391b65db8325bd30647e254c0cf99bb316f53
Continuous Integration <dev@code.org>
2015-11-10 17:16:06 +0000

    Automatically built.
    15d8374 Merge pull request #5243 from code-dot-org/autocompleteExclusions (Bjvanminnen)
    8a8b896 Merge pull request #5247 from code-dot-org/generate-username (Laurel)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96fbe02d307596be6419fdc947b4bcb3dddb7ee92c432d28f2b7d8a0c41a6d3c
-size 1397709
+oid sha256:6a86a7d70462dc3dcd2ce55151ca09682eca560351a9a292980c9a38c912f5f2
+size 1398036
