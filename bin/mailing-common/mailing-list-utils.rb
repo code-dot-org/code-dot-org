7bd6810bc44bc61486a77f014c650ce93e7b1557
Andrew Oberhardt <Andrew@Code.org>
2015-11-09 16:54:39 -0800

    Added scripts for Nov 9 mailings

--- bin/mailing-common/mailing-list-utils.rb
+++ bin/mailing-common/mailing-list-utils.rb
@@ -4,6 +4,10 @@ require src_dir 'database'
 
 SOLR = Solr::Server.new(host: 'ec2-54-83-22-254.compute-1.amazonaws.com')
 
+def common_script_path(name)
+  File.join(File.dirname(__FILE__), name)
+end
+
 UNSUBSCRIBERS = {}.tap do |results|
   DB[:contacts].where('unsubscribed_at IS NOT NULL').each do |i|
     email = i[:email].downcase.strip
@@ -75,28 +79,99 @@ def query_subscribed_contacts(params)
   end
 end
 
-EMAIL = 'Email Address'
-NAME = 'Name'
-COUNTRY_CODE = 'CC'
-COUNTRY_CODE_US = 'US'
+
+# Params:
+#   file: csv file path to read
+#   email: email column name
+#   name: name column name
+#   country_code: country code column name
+#   country_code_us: country code value for US (otherwise international)
+#   skip_emails: array of emails to skip
+def include_csv(results, params)
+  CSV.foreach(params[:file], headers: true) do |row|
+    email = row[params[:email]]
+    next if params.has_key?(:skip_emails) && params[:skip_emails.include?(email)]
+    international = !(row[params[:country_code]] && row[params[:country_code]] != '' && row[params[:country_code]] == params[:country_code_us])
+    processed = {email: row[params[:email]], name: row[params[:name]], international: international.to_s}
+
+    results[email] ||= processed unless UNSUBSCRIBERS[email] || ALL[email] # don't override duplicates
+  end
+
+  ALL.merge! results
+end
 
 def include_mailchimp_engineers(results)
   # read the mailchimp engineers csv and include new contacts in results
 
-  # headers:
+  # Mailchimp CSV headers:
   # "Email Address",Name,"ZIP code","Help time",Country,UNSUB_CAMPAIGN_ID,UNSUB_REASON,UNSUB_TIME,UNSUB_CAMPAIGN_TITLE,UNSUB_CAMPAIGN_ID,UNSUB_REASON,UNSUB_REASON_OTHER,"MailChimp Integration",Languages,"Help Location",MEMBER_RATING,OPTIN_TIME,OPTIN_IP,CONFIRM_TIME,CONFIRM_IP,LATITUDE,LONGITUDE,GMTOFF,DSTOFF,TIMEZONE,CC,REGION,LAST_CHANGED,LEID,EUID,NOTES
 
   # example rows:
   # rick@alchemydes.com,"Rick Hawkins",98110,,,,,,,,,,,"JavaScript, Perl, PHP","On-site at a school",2,,,"2013-06-25 17:38:27",,47.6614000,-122.2930000,-8,-7,America/Los_Angeles,US,WA,"2013-06-27 12:11:50",44877845,c2cb0681af,
   # hadipartovi@gmail.com,"hadi partovi test",,5hr/wk,Chile,,,,,,,,,"Java, C++, JavaScript, Perl, PHP, Python, Ruby, Scratch, Alice","On-site at a school, Mentoring students remotely, Developing curriculum or exercises",2,,,"2013-06-25 18:29:10",,33.7767000,-118.1460000,-8,-7,America/Los_Angeles,US,CA,"2013-06-27 12:13:08",44965861,77fd352c33,
 
-  CSV.foreach('mailchimp_engineers.csv', headers: true) do |row|
-    international = !(row[COUNTRY_CODE] && row[COUNTRY_CODE] != '' && row[COUNTRY_CODE] == COUNTRY_CODE_US)
-    email = row[EMAIL]
-    processed = {email: row[EMAIL], name: row[NAME], international: international.to_s}
+  include_csv(results,
+    file: 'mailchimp_engineers.csv',
+    email: 'Email Address',
+    name: 'Name',
+    country_code: 'CC',
+    country_code_us: 'US'
+  )
+end
 
-    results[email] ||= processed unless UNSUBSCRIBERS[email] || ALL[email] # don't override duplicates
+def include_indiegogo_donors(results)
+  # read the indiegogo donors csv
+  # headers
+  #Perk ID,Order No.,Pledge ID,Fulfillment Status,Funding Date,Payment Method,Appearance,Name,Email,Amount,Perk,Shipping Name,Shipping Address,Shipping Address 2,Shipping City,Shipping State/Province,Shipping Zip/Postal Code,Shipping Country
+
+  # example rows:
+  # "","",13620908,No Perk,2015-10-05 01:02:55 -0700,,Visible,Code.org Major Donors ,majordonors@code.org,$30,"","","","","","","",""
+  # 2375299,1129,13620900,Order Placed,2015-10-05 01:00:27 -0700,FirstGiving,Visible,Muhannad Ahmad,muhannad@7distribution.com,$30,Hour of Code Sticker Pack,Muhannad Mustafa,"Business bay Dubai , U-Bowra Tower Office 402",,Dubai,,"=""00971""",United Arab Emirates
+  # "","",13570557,No Perk,2015-09-30 10:30:57 -0700,,Visible,Code.org Major Donors ,majordonors@code.org,$80,"","","","","","","",""
+  # 2375305,1128,13570533,Order Placed,2015-09-30 10:29:27 -0700,FirstGiving,Visible,Olivier Greoli,olivier@greoli.be,$80,Code.org Hat + Laptop Sticker,Olivier Greoli,Rue des ChÃ¢lets 4,,GrivegnÃ©e,,"=""4030""",Belgium
+
+  include_csv(results,
+    email: 'Email',
+    name: 'Name',
+    country_code: 'Shipping Country',
+    country_code_us: 'United States',
+    skip_emails: ['majordonors@code.org']
+  )
+end
+
+ALL_FILES = []
+
+# naming convention:
+# script to generate the csv:
+#  generate-whoever
+# csv
+#  whoever.csv
+# deduped csv
+#  whoever-deduped.csv
+#
+# returns the deduped csv filename
+def generate(name)
+  puts "\n---" + name
+  csv = name + '.csv'
+  script = 'generate-' + name
+  raise "Could not find #{script}" unless File.exist?(script)
+  puts `time ./#{script}`
+  raise "#{script} did not generate #{csv}" unless File.exist?(csv)
+  puts `wc #{csv}`
+
+  puts "deduping"
+  deduped_csv = name + '-deduped.csv'
+  if ALL_FILES.empty?
+    puts `cp #{csv} #{deduped_csv}`
+  else
+    puts `#{common_script_path('subtract')} #{csv} #{ALL_FILES.join(' ')} #{deduped_csv}`
   end
+  puts `wc #{deduped_csv}`
 
-  ALL.merge! results
+  puts 'international:'
+  puts `grep true #{deduped_csv} | wc`
+
+  ALL_FILES << deduped_csv # list of csvs for deduping future csvs
+
+  deduped_csv
 end
