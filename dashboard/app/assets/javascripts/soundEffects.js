979f3c0c392c3099a067096ecab7173d0f1e0ef3
Brad Buchanan <brad@code.org>
2015-11-09 17:15:51 -0800

    Show instructions even if video is cancelled

--- dashboard/app/assets/javascripts/soundEffects.js
+++ dashboard/app/assets/javascripts/soundEffects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc3504eace44880b950e3d9b5079a30be0b1935abe95715ca08ff219ae036ec2
-size 14890
+oid sha256:cbe470e5c363920c1c700d21542c54e658defc9a06cfafc7a402d4de689dd250
+size 14987
