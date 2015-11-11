2f564993f05e98593a1ebb06c6964b56772d1855
Asher Kach <asher@code.org>
2015-11-11 14:17:03 -0600

    [WIP] More refactoring of the ReportsController, breaking apart a AdminReportsController.

---
+++ dashboard/test/controllers/admin_reports_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class AdminReportsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  generate_admin_only_tests_for :all_usage
+  generate_admin_only_tests_for :admin_concepts
+  generate_admin_only_tests_for :admin_progress
+  generate_admin_only_tests_for :admin_stats
+  generate_admin_only_tests_for :funometer
+  generate_admin_only_tests_for :level_completions
+  generate_admin_only_tests_for :pd_progress
+
+  test 'should get admin progress' do
+    get :admin_progress
+    assert_select 'h1', 'Admin progress'
+  end
+
+end
