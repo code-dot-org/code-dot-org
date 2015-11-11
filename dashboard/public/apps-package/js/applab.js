08d7fa443d598aa120eaa592626a458e26324d9d
Continuous Integration <dev@code.org>
2015-11-11 00:22:08 +0000

    Automatically built.
    d726435 Merge pull request #5085 from code-dot-org/ie9upload (Bjvanminnen)
    ac41b1b Automatically built. (Continuous Integration)
    073c29a Merge pull request #5068 from code-dot-org/hideSourceApplab (Bjvanminnen)
    0f03aac add trailing slash (Brent Van Minnen)
    16be307 put .jsx in require (Brent Van Minnen)
    a91223d get rid of zoom from css (Brent Van Minnen)
    71f4748 split out asset thumbnail (Brent Van Minnen)
    4cb9a25 code review feedback (Brent Van Minnen)
    e192ceb fix some spacing (Brent Van Minnen)
    1b9ac47 minified version of jquery fileupload libs (Brent Van Minnen)
    feb27ed fix test with unicode character in filename (Brent Van Minnen)
    4b6c9d8 fix shared tests (Brent Van Minnen)
    2443ea4 clean up some edge cases (Brent Van Minnen)
    8ea7b8a IE9 asset uploaded appears to be working (Brent Van Minnen)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b21b674269fa50724e5c3885f3efaa85bafd6fd303bb1e1fe8fd2fb2bb35499b
-size 495134
+oid sha256:42955191bc088ea474d182f8d9c2be232bca31b60cba4b93180c9aa77ef7f1a8
+size 496793
