bbec27c5e5c1154cf792e22106ed86c160593dca
Continuous Integration <dev@code.org>
2015-11-11 17:56:09 +0000

    Automatically built.
    138a231 Merge pull request #5283 from code-dot-org/swap-tracks-9-10 (Brad Buchanan)
    bd0f221 Merge pull request #5257 from code-dot-org/inspire-video-updates (Brendan Reville)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:581cf85b2f0a35c721d8ecf5d6a1de28fdb9e309521f7150a0e29fc7a0c869e4
+oid sha256:3cccb4c5ab93790b4feec730d9c5c5c56638757359881e04577dce9a143875f6
 size 559945
