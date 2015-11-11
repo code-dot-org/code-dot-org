b13c4b2bf3e0a80754fdf51ac4c075d4731d98d9
David Bailey <davidsbailey@users.noreply.github.com>
2015-11-10 16:15:10 -0800

    touch up

--- apps/src/applab/designElements/dropdown.jsx
+++ apps/src/applab/designElements/dropdown.jsx
@@ -148,8 +148,9 @@ module.exports = {
   },
 
   onDeserialize: function (element) {
-    // Don't interfere with clicks in design mode, where the dropdown already doesn't open.
-    if ($('#divApplab').find(element)) {
+    // Don't interfere with focus or clicks in design mode, where the dropdown is
+    // already disabled by jQuery.draggable({cancel: false}).
+    if ($('#divApplab').find(element)[0]) {
       $(element).on('mousedown', function(e) {
         if (!Applab.isRunning()) {
           // Disable dropdown menu unless running
