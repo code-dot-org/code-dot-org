494097086bb29c6db686bad13b3d69c3dd2ccdc4
Josh Lory <josh.lory@code.org>
2015-11-13 13:31:07 -0800

    Extract SendToPhone React component for reuse

---
+++ dashboard/app/assets/javascripts/components/send_to_phone.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec78e14a04112f65a462de9d095c1ecc09d8799e23c55547d926ec3efecaf0fc
+size 769
--- dashboard/app/assets/javascripts/components/share_dialog_body.jsx
+++ dashboard/app/assets/javascripts/components/share_dialog_body.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:add1df49d58286deb441ab3c4d37fc0c176d342d9794d1c53470b6902d29ba8e
-size 3769
+oid sha256:213198902275d7aef6d5473551a0b608bab629969c0901127d8df3196f41a56f
+size 3321
