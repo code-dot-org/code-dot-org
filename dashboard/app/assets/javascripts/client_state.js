8c77022b76fda7e9f0d82d440df9f4dece8c1894
Josh Lory <josh.lory@code.org>
2015-11-16 17:58:02 -0800

    Add methods to client_state.js for storing and retrieving sources

--- dashboard/app/assets/javascripts/client_state.js
+++ dashboard/app/assets/javascripts/client_state.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8c05f62ce3e6b411e60e1fefaac3c00ac941f0325e79b33f642322829fc4750
-size 5342
+oid sha256:cc70f1be06fdfb4fe8a8cae546c5961c12d780ed26a7f57bb6fe0811396fcfec
+size 6356
