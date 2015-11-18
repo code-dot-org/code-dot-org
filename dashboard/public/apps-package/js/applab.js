06e5825d5f91fab11d43eae8cbb79d0b45a30eba
Continuous Integration <dev@code.org>
2015-11-18 00:29:35 +0000

    Automatically built.
    432d035 Merge pull request #5459 from code-dot-org/boxSizing (Bjvanminnen)
    041b6df moz-box-sizing (Brent Van Minnen)
    0fcd153 Automatically built. (Continuous Integration)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95ce8aac8c30e3c104e1b2e180d891c5a091ae0d4fc7b92dda9ae79e492cb058
-size 2173697
+oid sha256:1a97f5557897ec13954d3e9cb4eafd74240eaf30b19eb8edce057674d9f32a81
+size 2173969
