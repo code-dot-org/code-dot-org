2641d7e6d4aa94271b03d7afa3913d84154717ce
Continuous Integration <dev@code.org>
2015-11-19 01:12:40 +0000

    Automatically built.
    e36e176 Merge pull request #5377 from code-dot-org/simplify-share-page (Josh Lory)
    d5de841 Merge pull request #5493 from code-dot-org/ui-tests-sign-in-out-via-tag (Brad Buchanan)
    7a26518 Merge pull request #5464 from code-dot-org/new-redirects (Caley Brock)
    a2654b8 Automatically built. (Continuous Integration)
    b9413dc Merge pull request #5470 from code-dot-org/disable-development-caching (Josh Lory)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34c9a0d6446e7ff9afbad4d5749a6c20785bba97480f1cc5faa5c1c2e35c813d
-size 2176232
+oid sha256:a09f51e38625ebba33675e811d3506afe86daee85a72ff82a0d16d57339a9851
+size 2176075
