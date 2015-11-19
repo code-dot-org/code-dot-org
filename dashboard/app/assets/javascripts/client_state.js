815e503b6977d55fb423af318b3ef4c7b1345806
Josh Lory <josh.lory@code.org>
2015-11-19 15:15:01 -0800

    Use sessionStorage instead of localStorage due to shorter lifetime
    Seeing a video or callout again isn't bad, but seeing someone elses code or progress is a bad experience.

--- dashboard/app/assets/javascripts/client_state.js
+++ dashboard/app/assets/javascripts/client_state.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fabb9b48d7a1baa8fa68f8434f2eea4597aa4b30d82dc126111931c762f21bf2
-size 7193
+oid sha256:57716cd0128ae69595f2c894a9ee8144f2636c8cc157352e7f295313e7002738
+size 7211
