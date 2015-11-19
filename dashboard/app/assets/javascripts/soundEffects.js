2fa0eb301ae0c5a40c8b7a374787665bd7322a3a
Brad Buchanan <brad@code.org>
2015-11-19 14:57:44 -0800

    Hybrid approach to detecting audio unlock

--- dashboard/app/assets/javascripts/soundEffects.js
+++ dashboard/app/assets/javascripts/soundEffects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcfde05a4fe79329a597d5f0f22912320b383a2da22376ce387ddd1f186d7a47
-size 16717
+oid sha256:32d13a078272a2fe4e74417fb713521eff67343ce4ab0bdf4bd777ee505eaea5
+size 19764
