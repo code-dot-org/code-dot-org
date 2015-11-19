01f57da2c8bb2d0c5b8c71b5daf617c5a5d16574
Josh Lory <josh.lory@code.org>
2015-11-19 14:10:46 -0800

    Code review feedback: helper to standardize key names

--- dashboard/app/assets/javascripts/client_state.js
+++ dashboard/app/assets/javascripts/client_state.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e307b19d5a5f383bea3446ff7ac80f093c4d108b2654cdd267d32bc025c0431
-size 6930
+oid sha256:30481b9e6adf398bb3ecf96a7c1736a4a10cea0e67a43e8cd5ef63f503f7572c
+size 7243
