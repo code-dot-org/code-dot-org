7e72c2f838345a355cf16c0f4ee01ba255cfe9d3
David Bailey <davidsbailey@users.noreply.github.com>
2015-11-20 15:23:06 -0800

    Migrate data from app_properties in Pegasus DB to CDO.dynamo_properties_table in DynamoDB.

---
+++ pegasus/migrations/79_migrate_properties_to_dynamodb.rb
@@ -0,0 +1,22 @@
+require 'cdo/hip_chat'
+require 'properties_api'
+
+Sequel.migration do
+  up do
+    HipChat.log 'Copying <b>>key value pairs</b> to DynamoDB...'
+
+    # There are currently < 1000 rows in production. Don't bother batching.
+    rows = from(:app_properties)
+    rows.each do |row|
+      value = PropertyBag.parse_value(row[:value])
+      property_bag = DynamoPropertyBag.new(row[:app_id], row[:storage_id])
+      property_bag.set(row[:name], value, row[:updated_ip], row[:updated_at])
+    end
+
+    HipChat.log "Finished copying <b>#{rows.count} key value pairs</b> to DynamoDB."
+  end
+
+  # This migration is idempotent, so no need to remove the added data if we roll back.
+  down do
+  end
+end
