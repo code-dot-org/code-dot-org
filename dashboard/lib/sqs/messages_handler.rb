9449c3751e6ecf82ab83d245d3cfcc2cbe9ee7a5
Phil Bogle <phil@code.org>
2015-11-19 23:30:09 -0800

    Only pass string message bodies

--- dashboard/lib/sqs/messages_handler.rb
+++ dashboard/lib/sqs/messages_handler.rb
@@ -3,10 +3,10 @@ module SQS
   # An abstract base class for processing a batch of messages.
   # Handler implementers should subclass this class and override `handle`.
   class MessagesHandler
-    # Handles an array of SQS messages.  If this method raises an exception, the
+    # Handles an array of message bodies.  If this method raises an exception, the
     # operation will be retried according to the queue configuration.
-    # @param <Array<Aws::SQS::Message>> messages
-    def handle(messages)
+    # @param <Array<String>> messages
+    def handle(bodies)
       raise 'Handle must be implemented by subclasses'
     end
   end
