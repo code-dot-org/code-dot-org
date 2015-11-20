9373a3dfc891b6f1b1effc3d8912d07f1e1a3482
Brad Buchanan <brad@code.org>
2015-11-20 11:59:26 -0800

    Remove audio diagnostic

--- dashboard/app/assets/javascripts/soundEffects.js
+++ dashboard/app/assets/javascripts/soundEffects.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32d13a078272a2fe4e74417fb713521eff67343ce4ab0bdf4bd777ee505eaea5
-size 19764
+oid sha256:b27f34fadc25f4f5a20d2004e26a9b2adeef0abd32a7dc2736378b4b10eaf4b3
+size 18168
