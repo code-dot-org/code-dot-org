85c786320814c565d6c1de3bc61e0484e4fdb111
Josh Lory <josh.lory@code.org>
2015-11-20 12:23:59 -0800

    This is actually the `current_level_id`

--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcc4faa8568638c42b75bbbe240d082b2554a76ff5f6b4951aac275c4ffe17bd
-size 16351
+oid sha256:12db363fce66591029a85d0c20639f130dd6b1e74d7ecbf0451aa12f089b3319
+size 16352
