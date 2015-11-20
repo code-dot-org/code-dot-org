007e108c15b8c22fe77a72f335d2bf6940b884a1
Continuous Integration <dev@code.org>
2015-11-20 22:10:39 +0000

    Automatically built.
    d367880 Merge pull request #5552 from code-dot-org/delete_first_screen (Mehal Shah)
    c9c307d Automatically built. (Continuous Integration)
    952c8fe Merge pull request #5501 from code-dot-org/hoc2015erroravatar (Brendan Reville)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbe4c15f1ad8dbb502521c7050111740bb33937cab24f1e43669cea8b2f1cfec
-size 2184536
+oid sha256:b762bf25f97a8ff1a0e34e9f80640d538f1dcac0927c80b7d7c711428add57af
+size 2184549
