79e1239589c0f67be8d2c303052570a1036761c7
Continuous Integration <dev@code.org>
2015-11-20 01:00:39 +0000

    Automatically built.
    f443dc8 Merge pull request #5531 from code-dot-org/applabShareFooter (Bjvanminnen)
    f38d016 Merge pull request #5532 from code-dot-org/levelbuilder (Bjvanminnen)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db3c49234204049c8c4e3abeef1368ff8a72efe1936801366060ad96b62103a2
-size 2184138
+oid sha256:dbe4c15f1ad8dbb502521c7050111740bb33937cab24f1e43669cea8b2f1cfec
+size 2184536
