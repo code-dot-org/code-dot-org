e11e66c7694b30e1cd32b07fb6918d5bf7371082
Phil Bogle <phil@code.org>
2015-11-21 10:37:28 -0800

    add synchronous queue and sqs queue classes

--- dashboard/app/models/activity.rb
+++ dashboard/app/models/activity.rb
@@ -68,6 +68,38 @@ class Activity < ActiveRecord::Base
     Activity.finished? test_result
   end
 
+  # Returns a new Activity and queues an SQS message to asynchronously create it in
+  # the database. Note that this id, created_at, and updated_at attributes will not
+  # # be populated. Also, if the object does not pass validation, an immediate
+  # exception will be thrown and the object will not be saved.
+  def Activity.create_async!(attributes, queue)
+    Activity.new(attributes).tap do |activity|
+      activity.validate!
+
+      async_create_json = {
+          'model' => 'Activity',
+          'action' => 'create',
+          'attributes' => attributes}.to_json
+
+      @sqs_client.send_message(queue_url: queue_url, message_body: async_json)
+    end
+  end
+
+  def Activity.apply_async_json(async_json)
+    op = JSON::parse(async_json)
+
+    if op[:model] != 'Activity'
+      raise "Model must be Activity, but was #{op[:model]} in #{async_json}"
+    end
+
+    case op['action']
+      when 'create'
+          Activity.create!(op[:attributes])
+      else
+        raise "Unknown action #{op[:action]} in #{async_json}"
+    end
+  end
+
   def Activity.recent(limit)
     # yeah, this is a lot like .last -- but I want a dataset not an array
     Activity.order('id desc').limit(limit)
