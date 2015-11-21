80efc6e937c40dc690c45fdc0285b9c43a78452d
Phil Bogle <phil@code.org>
2015-11-20 17:52:06 -0800

    Comment fix

--- dashboard/lib/sqs/messages_handler.rb
+++ dashboard/lib/sqs/messages_handler.rb
@@ -3,7 +3,7 @@ module SQS
   # An abstract base class for processing a batch of messages.
   # Handler implementers should subclass this class and override `handle`.
   class MessagesHandler
-    # Handles an array of message bodies.  If this method raises an exception, the
+    # Handles an array of SQS::Messages. If this method raises an exception, the
     # operation will be retried according to the queue configuration.
     # @param <Array<SQS::Message>> messages
     def handle(messages)
