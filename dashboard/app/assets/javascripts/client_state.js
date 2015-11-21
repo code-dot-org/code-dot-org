03f266c1e08522903cc186562fee67432993da91
Josh Lory <josh.lory@code.org>
2015-11-20 16:50:38 -0800

    Fix anonymous header progress to use both scriptName and levelId

--- dashboard/app/assets/javascripts/client_state.js
+++ dashboard/app/assets/javascripts/client_state.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e352921b36ae2332ad6bb5c687bcff3d797829e2eede2e9cbfe66b72ea3fc58
-size 7192
+oid sha256:e56984ed17d901f2906ac2c336043600cec8c152619e0c917e18abd5f4ffcaf4
+size 7249
--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12db363fce66591029a85d0c20639f130dd6b1e74d7ecbf0451aa12f089b3319
-size 16352
+oid sha256:fd2ce4ee8958406867e2a4ba4932422e2ec9ded3974f3f8322cdf5ac398899e1
+size 16458
