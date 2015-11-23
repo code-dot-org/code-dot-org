63a69f35dc236643ebf991b7a288545fa3e928a1
Brent Van Minnen <bjvanminnen@gmail.com>
2015-11-23 15:11:58 -0800

    fix header for level shared from script

--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcc4faa8568638c42b75bbbe240d082b2554a76ff5f6b4951aac275c4ffe17bd
-size 16351
+oid sha256:1529b07833463de882677593fcaef32e52b528db49414f26706b6323b8ab93da
+size 16317
