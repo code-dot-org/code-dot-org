66d8d604c713d3f45ca0d560df853d58f00288c1
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-11-23 14:19:50 -0800

    also camelCased the controller test

--- dashboard/app/controllers/authored_hint_view_requests_controller.rb
+++ dashboard/app/controllers/authored_hint_view_requests_controller.rb
@@ -3,14 +3,12 @@ class AuthoredHintViewRequestsController < ApplicationController
     return head :unauthorized unless AuthoredHintViewRequest.enabled?
     return head :bad_request unless params.key?("hints") && params["hints"].respond_to?(:to_a)
 
-    puts params["hints"]
     hints = params["hints"].to_a.map do |hint|
-      # convert camelCase strings to snake_case symbols
-      hint = Hash[hint.map{ |key, value| [key.underscore.to_sym, value] }]
       # add :user
       hint[:user] = current_user
+      # convert camelCase strings to snake_case symbols
+      hint.map{|key, value| {key.underscore.to_sym => value}}.reduce(:merge)
     end
-    puts hints
 
     objects = AuthoredHintViewRequest.create(hints)
     all_valid = objects.all? do |object|
