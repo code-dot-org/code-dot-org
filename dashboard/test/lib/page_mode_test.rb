7f8527535d74804787b9996a740aa18474d4b7c7
Tanya Parker <tanyacparker@gmail.com>
2015-11-22 23:24:20 -0800

    fix page_mode tests

--- dashboard/test/lib/page_mode_test.rb
+++ dashboard/test/lib/page_mode_test.rb
@@ -2,8 +2,10 @@ require 'test_helper'
 require 'dynamic_config/page_mode'
 
 class PageModeTest < ActionController::TestCase
-  test 'returns default if unset' do
-    assert_equal PageModeBase::DEFAULT_PAGE_MODE, PageMode.get(@request)
+  test 'returns sw or mc if unset' do
+    assert_block do
+      ['feature-starwars', 'feature-mc'].include?(PageMode.get(@request))
+    end
   end
 
   test 'get and set' do
