827e9aafaa46e0bb6fd5784962000ca3f4054927
Continuous Integration <dev@code.org>
2015-11-24 17:43:30 +0000

    Automatically built.
    c6c097f Merge pull request #5606 from code-dot-org/playlab_polishCodeHook (Chris Pirich)
    088ac60 content changes -tanya (Continuous Integration)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e0b86652a5a73edba5129c5af550ddbf8b89c5df283b9cf39eb71fbb9d33d62
-size 3907372
+oid sha256:be2a3c62e489029e33dfa448c2e8b2c5a4eb363637dcf1fe0e1e5a43596411f9
+size 3908857
--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b305745809f7b9ba21a9b050aac3ceb5566cc875eebad9ce2e668c5b4a5e3ab5
-size 1979777
+oid sha256:78bda2651050b72be44d5d8ac6c1893059cee8426e4dda2e6a5eebe2e3a9ab76
+size 1982523
