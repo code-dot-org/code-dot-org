6a12cd690fcdb710c39f4a20794f6dc85c8c8945
Mehal Shah <mehal@code.org>
2015-11-24 00:52:47 -0800

    Parallel server improvements

--- bin/add_frontend_server.rb
+++ bin/add_frontend_server.rb
@@ -96,6 +96,8 @@ def determine_frontend_instance_distribution
 
   instance_distribution = frontend_instances.each_with_object(Hash.new(0)) { |(instance, _), instance_distribution|
     instance_distribution[instance.placement.availability_zone] += 1 }
+
+  instance_distribution
 end
 
 # Return an array of objects that have the names and zones of instances to create
