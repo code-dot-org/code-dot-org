5e7dc166b5e650dca617acacc84438bfa78379ea
Josh Lory <josh.lory@code.org>
2015-11-25 13:55:14 -0800

    Also merge progress for header progress dots

--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca7427f0f53161d7e8f1edef3d1edfd9d992d2e96235bf9c2e8c1aa2f4b67186
-size 16756
+oid sha256:6378a262087d90e8b755e525baed9ba8daf0720508527c51148736a173c98a4d
+size 16429
