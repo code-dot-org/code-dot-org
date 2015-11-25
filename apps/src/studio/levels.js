107b46f5536bef41c58f2902bca8db8edd7af737
caleybrock <caleybrock7@gmail.com>
2015-11-24 17:23:12 -0800

    Iceberg name change

--- apps/src/studio/levels.js
+++ apps/src/studio/levels.js
@@ -245,7 +245,7 @@ levels.playlab_1 = utils.extend(levels.dog_hello, {
 });
 
 levels.iceage_1 = utils.extend(levels.playlab_1, {
-  background: 'ice',
+  background: 'iceberg',
   firstSpriteIndex: 0, // manny
 });
 levels.gumball_hello1 = utils.extend(levels.playlab_1, {
@@ -607,7 +607,7 @@ levels.playlab_5 = utils.extend(levels.click_hello, {
    '<block type="studio_whenSpriteClicked" deletable="false" x="20" y="20"></block>'
 });
 levels.iceage_5 = utils.extend(levels.playlab_5, {
-  background: 'ice',
+  background: 'iceberg',
   firstSpriteIndex: 1, // sid
 });
 
@@ -896,7 +896,7 @@ levels.playlab_7 = {
   ],
 };
 levels.iceage_7 = utils.extend(levels.playlab_7, {
-  background: 'ice',
+  background: 'iceberg',
   firstSpriteIndex: 1, // sid
 });
 levels.gumball_repeat = utils.extend(levels.playlab_7, {
