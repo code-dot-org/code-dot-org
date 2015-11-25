78473fd97150d8d6ccc07bdbd66c966a8b056fae
Josh Lory <josh.lory@code.org>
2015-11-25 11:01:40 -0800

    Load progress via AJAX and populate on client for course overview

--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e66ff4a22b3a3799af41bb8982dfbc7b845678e2b6daca2e29122e575c90ac6
-size 16259
+oid sha256:5f14664ff087ff93c022783731af4aeca3b243e7154eea042ec8a5b3806cf2c5
+size 16775
