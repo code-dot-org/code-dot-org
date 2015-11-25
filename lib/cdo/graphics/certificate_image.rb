2521838a7b5f06b38e94be510db6a10b9f3b6336
Brian Jordan <bcjordan@gmail.com>
2015-11-24 17:23:09 -0800

    More tests for certificate image / routes - Consolidate points of truth for script identification / naming away from dashboard application helper towards certificate_image.rb - Separate out script name / script title fields in URL encoded certificate flow

--- lib/cdo/graphics/certificate_image.rb
+++ lib/cdo/graphics/certificate_image.rb
@@ -25,17 +25,19 @@ def create_certificate_image2(image_path, name, params={})
 end
 
 def create_course_certificate_image(name, course=nil, sponsor=nil)
+  course = 'hoc' if course.nil? || course.empty?
+
   name = name.gsub(/@/,'\@')
   name = ' ' if name.empty?
 
+  template_file = cert_template_for_course(course)
+
   if prefilled_title_course?(course)
     # only need to fill in name
-    image_file = prefilled_title_course_template(course)
     vertical_offset = course == '20hours' || course == '20-hour' ? -115 : -110
-    image = create_certificate_image2(pegasus_dir('sites.v3', 'code.org', 'public', 'images', image_file), name, y: vertical_offset)
+    image = create_certificate_image2(pegasus_dir('sites.v3', 'code.org', 'public', 'images', template_file), name, y: vertical_offset)
   else # all other courses use a certificate image where the course name is also blank
-    image_file = 'blank_certificate.png'
-    image = Magick::Image.read(pegasus_dir('sites.v3', 'code.org', 'public', 'images', image_file)).first
+    image = Magick::Image.read(pegasus_dir('sites.v3', 'code.org', 'public', 'images', template_file)).first
 
     # student name
     name_vertical_offset = 445
@@ -48,31 +50,8 @@ def create_course_certificate_image(name, course=nil, sponsor=nil)
       self.fill = 'rgb(118,101,160)' # purple
     end
 
-    # Take course name and convert it to a nicer full course name for the certificate
-    case course
-      when 'artist'
-        full_course_name = 'Artist'
-      when 'course1'
-        full_course_name = 'Course 1'
-      when 'course2'
-        full_course_name = 'Course 2'
-      when 'course3'
-        full_course_name = 'Course 3'
-      when 'course3'
-        full_course_name = 'Course 4'
-      when 'frozen'
-        full_course_name = 'Frozen'
-      when 'playlab'
-        full_course_name = 'Play Lab'
-      when 'flappy'
-        full_course_name = 'Flappy Bird'
-      else
-        full_course_name = course
-    end
-
-    # course name
     course_vertical_offset = 610
-    Magick::Draw.new.annotate(image, 0, 0, 0, course_vertical_offset, full_course_name) do
+    Magick::Draw.new.annotate(image, 0, 0, 0, course_vertical_offset, cert_display_name_for_course(course)) do
       self.gravity = Magick::NorthGravity
       self.pointsize = 60
       self.font_family = 'Helvetica'
@@ -99,17 +78,38 @@ def create_course_certificate_image(name, course=nil, sponsor=nil)
   image
 end
 
+def cert_display_name_for_course(course)
+  case course
+    when 'artist'
+      'Artist'
+    when 'course1'
+      'Course 1'
+    when 'course2'
+      'Course 2'
+    when 'course3'
+      'Course 3'
+    when 'course4'
+      'Course 4'
+    else
+      course
+  end
+end
+
 def prefilled_title_course?(course)
   course == '20hours' ||
       course == 'hoc' ||
       course == '20-hour' ||
       course == 'hourofcode' ||
+      course == 'Hour of Code' ||
       course == 'starwars' ||
       course == 'mc' ||
+      course == 'flappy' ||
       course == 'frozen'
 end
 
-def prefilled_title_course_template(course)
+def cert_template_for_course(course)
+  return 'blank_certificate.png' unless prefilled_title_course?(course)
+
   if course == '20hours' || course == '20-hour'
     '20hours_certificate.jpg'
   elsif course == 'mc'
@@ -121,7 +121,8 @@ end
 
 # generate a url for a certificate image, given options:
 #   name: student name
-#   course: '20hours', 'hoc' or the course title
+#   course: course name
+#   course_title: course title
 #   sponsor: (optional)
 def certificate_image_url(opts = {})
   encoded = Base64.urlsafe_encode64(JSON.pretty_generate(opts))
