12e8d587c5c2a299f96a7ff2aae47251dd4550b3
Brent Van Minnen <bjvanminnen@gmail.com>
2015-11-25 14:30:49 -0800

    dont duplicate buttons

--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ad9078d3513a3ea15a7c096ea054ada74372f8d5ee6dddd249a9507404e2e47
-size 17498
+oid sha256:2df7525d0b4814377da0bb2c7891500ad11d9cc5dcc1fe1aab930a75de087937
+size 17570
