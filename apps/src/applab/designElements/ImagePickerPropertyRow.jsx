665ce178c822028b59d636085e350e0077e69edc
Brent Van Minnen <bjvanminnen@gmail.com>
2015-11-25 11:11:14 -0800

    avoid execessive non-useful GETs for images

--- apps/src/applab/designElements/ImagePickerPropertyRow.jsx
+++ apps/src/applab/designElements/ImagePickerPropertyRow.jsx
@@ -1,6 +1,13 @@
 var showAssetManager = require('../assetManagement/show.js');
 var rowStyle = require('./rowStyle');
 
+// We'd prefer not to make GET requests every time someone types a character.
+// This is the amount of time that must pass between edits before we'll do a GET
+// I expect that the vast majority of time, people will be copy/pasting URLs
+// instead of typing them manually, which will result in an immediate GET,
+// unless they pasted within WAIT_TIME ms of editing the field manually
+const WAIT_TIME = 1500;
+
 var PropertyRow = React.createClass({
   propTypes: {
     initialValue: React.PropTypes.string.isRequired,
@@ -9,12 +16,31 @@ var PropertyRow = React.createClass({
 
   getInitialState: function () {
     return {
-      value: this.props.initialValue
+      value: this.props.initialValue,
+      lastEdit: new Date()
     };
   },
 
+  changeUnlessEditing: function (filename) {
+    var now = new Date();
+    if (now - this.state.lastEdit >= WAIT_TIME) {
+      this.changeImage(filename);
+    }
+  },
+
   handleChangeInternal: function (event) {
-    this.changeImage(event.target.value);
+    var filename = event.target.value;
+    this.changeUnlessEditing(filename);
+
+    this.setState({
+      value: filename,
+      lastEdit: new Date()
+    });
+
+    // We may not have changed file yet (if we still actively editing)
+    setTimeout(function () {
+      this.changeUnlessEditing(this.state.value);
+    }.bind(this), WAIT_TIME);
   },
 
   handleButtonClick: function () {
