dcaebecb71f8c2878c3c1b11dc0755f65b231f9c
Josh Lory <josh.lory@code.org>
2015-11-24 16:31:00 -0800

    Fix jshint

--- dashboard/app/assets/javascripts/client_state.js
+++ dashboard/app/assets/javascripts/client_state.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:511a5b9dbf4522e9b92c2b5def5a08e7f182704128674a9d58a42ad90710c9bd
-size 7290
+oid sha256:a5d24869c9f38c9447ef5e9a01afef32ccc4451354e2bec1f8f8af20a34b53f0
+size 7291
