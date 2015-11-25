2bace3f4c8b517dfa9a435745769349a1f17fcf3
Continuous Integration <dev@code.org>
2015-11-25 21:08:25 +0000

    Automatically built.
    118cfbd Merge pull request #5659 from code-dot-org/temp-app-lab-audio-fix (Brad Buchanan)
    a2c5a96 Merge pull request #5642 from code-dot-org/applab_mobile_testing (Mehal Shah)
    805aa33 content changes -tanya (Continuous Integration)
    9cf71f9 Merge remote-tracking branch 'origin/staging' into temp-app-lab-audio-fix (Brad Buchanan)
    d5a11c8 Automatically built. (Continuous Integration)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a4ee1808c8eb41be860da0c595bcdc22fb073c6da33e52fe9e74a7369b65519
-size 2196896
+oid sha256:362ed715a3520ee76fd8861166ad53589abca955be6b3e15d4005d5f75e3109a
+size 2199199
