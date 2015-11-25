f4d0f8521e8b800217e677e4892a294a8817b09e
cpirich <chris@code.org>
2015-11-24 19:37:22 -0800

    detect local functions and fail if the name matches an auto-handler

--- apps/src/JSInterpreter.js
+++ apps/src/JSInterpreter.js
@@ -505,3 +505,26 @@ JSInterpreter.prototype.getGlobalFunctionNames = function () {
   }
   return names;
 };
+
+/**
+ * Returns an array containing the names of all of the functions defined
+ * inside other functions.
+ */
+JSInterpreter.prototype.getLocalFunctionNames = function (scope) {
+  if (!scope) {
+    scope = this.globalScope;
+  }
+  var names = [];
+  for (var objName in scope.properties) {
+    var object = scope.properties[objName];
+    if (object.type === 'function' && !object.nativeFunc && object.node) {
+      if (scope !== this.globalScope) {
+        names.push(objName);
+      }
+      var localScope = this.interpreter.createScope(object.node.body, object.parentScope);
+      var localNames = this.getLocalFunctionNames(localScope);
+      names = names.concat(localNames);
+    }
+  }
+  return names;
+};
