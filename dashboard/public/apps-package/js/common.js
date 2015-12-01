b380ab2e166ddf5176ce8eadb5ec1eb99585d9ae
Continuous Integration <dev@code.org>
2015-12-01 16:09:11 +0000

    Automatically built.
    eaf1f7e Merge pull request #5726 from code-dot-org/fix-firefox-applab (David Bailey)
    c05ede4 catch Firefox SecurityError when parsing stylesheets (David Bailey)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:750591dc803ef35a1eb10e604f629d77ec857ad10a52f6fb2fb4c7ce8997abe0
-size 3915598
+oid sha256:c4162f610a6f47f43138569de35c4715b4adfc3aad1919132af1b63f03b895b9
+size 3915600
