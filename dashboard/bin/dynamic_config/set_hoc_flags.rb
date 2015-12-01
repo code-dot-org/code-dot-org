329233af952f1762a58935460da524401d5356ee
Phil Bogle <phil@code.org>
2015-12-01 06:24:07 -0800

    tweaks

--- dashboard/bin/dynamic_config/set_hoc_flags.rb
+++ dashboard/bin/dynamic_config/set_hoc_flags.rb
@@ -4,8 +4,10 @@
 # several predefined modes:
 #
 # - red ("read-only" disaster mode for HOC scripts)
-# - yellow (non-essential hint and puzzle rating features disabled for HOC scripts)
+# - yellow (non-essential features disabled for HOC scripts)
 # - green (all features enabled)
+#
+# See the documentation below for more details.
 
 require_relative '../../config/environment'
 require 'pp'
@@ -19,9 +21,9 @@ def main(argv)
     puts <<-EOF
 Usage: #{$0}: red|yellow|green
 
- - red: No HOC sharing, no HOC hints, no HOC activity tracking, no HOC milestone posts, no HOC puzzle rating.
- - yellow: No HOC puzzle rating, no HOC hints.
- - green: all features turned on. SHOULD NOT BE USED UNTIL HOC IS OVER"
+ - red: No HOC sharing, no HOC hints, no hoc_activity writes, no signed-in user progress saving, no HOC puzzle rating.
+ - yellow: No HOC puzzle rating, no HOC hints, no hoc_activity writes.
+ - green: all features turned on. SHOULD NOT BE USED UNTIL HOC IS OVER
 
  Public caching is enabled for all HOC scripts in all cases because this doesn't take away functionality
 EOF
@@ -41,8 +43,8 @@ EOF
       DCDO.set('activity_max_rate', 100)
     when 'yellow'
       Gatekeeper.set('postMilestone', value: true)
-      Gatekeeper.set('hoc_activity_writes_disabled', value: false)
-      Gatekeeper.set('tracking_pixel_enabled', value: true)
+      Gatekeeper.set('hoc_activity_writes_disabled', value: true)
+      Gatekeeper.set('tracking_pixel_enabled', value: false)
       Gatekeeper.set('async_activity_writes', value: true)
       Gatekeeper.set('puzzle_rating', value: false)
       DCDO.set('activity_max_rate', 3000)  # Max async op rate per queue processor.
