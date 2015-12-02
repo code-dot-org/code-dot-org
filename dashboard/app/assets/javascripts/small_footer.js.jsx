0f96141f27c3298c5930d0c8903650e63cd65fd2
Brad Buchanan <brad@code.org>
2015-12-02 15:04:38 -0800

    Correct copyright area width in mobile share cases.

--- dashboard/app/assets/javascripts/small_footer.js.jsx
+++ dashboard/app/assets/javascripts/small_footer.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91e10fc204f85d6eb3ec5c7b9862a8dbdcd1849af692eaff1b063ba2c7b3d618
-size 8967
+oid sha256:2a3637257d311fe81c7d6ed15d92311ee888ab86faf8db7d5b5eaa51f3f5f3d5
+size 9141
