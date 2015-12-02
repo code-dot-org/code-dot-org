af51e5046b0ed254d476753d7c68d5c2d4c207e9
Brad Buchanan <brad@code.org>
2015-12-01 17:02:02 -0800

    Add inner scroll area for copyright footer

--- dashboard/app/assets/javascripts/small_footer.js.jsx
+++ dashboard/app/assets/javascripts/small_footer.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aacd84f1cf4d94c34339fe22333d4ce6f6ed419ecd8e08296c8599d2025502e8
-size 8558
+oid sha256:fb4b041170145899b84ea532a58ce61b488f8884efd598d10c6661098bcfe5ac
+size 8743
