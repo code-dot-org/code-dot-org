c35f94ffd7c6e1991510aa9267ba80e5fbcd0cf5
Brad Buchanan <brad@code.org>
2015-12-02 15:17:29 -0800

    Lower copyright area so it starts at same height as more menu

--- dashboard/app/assets/javascripts/small_footer.js.jsx
+++ dashboard/app/assets/javascripts/small_footer.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a3637257d311fe81c7d6ed15d92311ee888ab86faf8db7d5b5eaa51f3f5f3d5
-size 9141
+oid sha256:7169d04d964f75eb8c28f7c4a8b97d9e07e21b904cea47852efdf3df28c9354d
+size 9105
