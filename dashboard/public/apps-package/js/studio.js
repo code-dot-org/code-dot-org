a88394598d61c50f8efc53897fdbff14a0f5e352
Continuous Integration <dev@code.org>
2015-12-02 18:03:41 +0000

    Automatically built.
    3e14e04 Merge pull request #5761 from code-dot-org/hoc2015settingssuccess (Brendan Reville)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8f274ba4178756fc6234e324c04eb9a5382168b9113edb312cf10a5423d5a07
-size 2014235
+oid sha256:e8dfb254845e5e94f5cdcaad82444e8fc847f884bee08b787c9c08dab1ae78cd
+size 2013956
