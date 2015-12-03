fdf212139c9290f58d19d57cb4729a51fe9f0d89
Continuous Integration <dev@code.org>
2015-12-03 19:57:30 +0000

    Automatically built.
    8d99dcf Merge pull request #5833 from code-dot-org/moreCallouts (Bjvanminnen)
    1b5b110 change id (Brent Van Minnen)
    c5f7e99 draw attention to instructions on click to say hello level (Brent Van Minnen)
    9e58168 Automatically built. (Continuous Integration)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ba47ceb3646c95167d138c62ed39fe6bb5a28e8089edfdbada7ded095a9431e
-size 2028198
+oid sha256:3898e72cb20e72268341e4de3f212ab843f5cc11580a254444b635271d38f999
+size 2029474
