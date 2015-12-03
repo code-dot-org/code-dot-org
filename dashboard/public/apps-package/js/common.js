5fcb87c95bc472c961d6ff4f67af5bd056d33a54
Continuous Integration <dev@code.org>
2015-12-03 01:18:26 +0000

    Automatically built.
    3ff02c8 Merge pull request #5810 from code-dot-org/playlabcollisions (Brendan Reville)
    6e6381a Merge pull request #5793 from code-dot-org/ajax-last-attempt (Josh Lory)
    fb7d84d loadLastAttemptFromSessionStorage() already calls callback() (Josh Lory)
    263ac2a Merge pull request #5766 from code-dot-org/improve_async_feedback_blocks (Elijah Hamovitz)
    2a773bf Merge pull request #5811 from code-dot-org/playlabgalleryquant (Brendan Reville)
    e20d3d3 Run pngquant on homepage playlab gallery image. (Brendan Reville)
    d62ba7c Merge pull request #5718 from code-dot-org/set_hoc_flags (philbogle)
    99e84fe Merge pull request #5699 from code-dot-org/public_caching (philbogle)
    d008921 Merge pull request #5808 from code-dot-org/droplet-force-ltr (Josh Lory)
    5844d9b gumball/iceage: much smaller sprite-sprite collision rectangles. (Brendan Reville)
    b8ce091 Merge pull request #5800 from code-dot-org/update-gallery-thumbnails (Charing Wong)
    e2d9490 Automatically built. (Continuous Integration)
    63d5c89 Droplet doesn't support RTL, so always force LTR (Josh Lory)
    a227dca Merge pull request #5806 from code-dot-org/playlabdonebutton (Brendan Reville)
    a1c06fe Merge pull request #5807 from code-dot-org/new-level-background (Caley Brock)
    126de3f Automatically built. (Continuous Integration)
    c0a8657 Merge pull request #5332 from code-dot-org/swipe-overlay (Andrew Oberhardt)
    5a1a33a Quick fixes from bug bash playlab (caleybrock)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f304c7472e5dff2c5eca793eef77a7db56de5714de07a255c24fb0c70a711f4
-size 3913647
+oid sha256:ea19f70b55a669170ed49617da4a0a8190db606d670cdd08fcb7e662e8d7fbcc
+size 3911069
--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:451d0e3e1e3e59e2bf8db3fc9cb1399aa67fd811e54cea03659c011fb91df058
-size 2026207
+oid sha256:edc7c4a5ecaddcd49a598cb4b86b8598840611de40902a7543f7ea91e1f089ab
+size 2028110
