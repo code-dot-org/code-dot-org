4cfc5b6f17f8e280e51e87006e2a119516cbc460
Brad Buchanan <brad@code.org>
2015-12-03 15:07:15 -0800

    Fix small_footer menu positions in tiny stacked mode.

--- dashboard/app/assets/javascripts/small_footer.js.jsx
+++ dashboard/app/assets/javascripts/small_footer.js.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7169d04d964f75eb8c28f7c4a8b97d9e07e21b904cea47852efdf3df28c9354d
-size 9105
+oid sha256:86bd71b51bb63b87fea46a68ee7920c5011a0b7e181e2c61043c5d8e233b7cab
+size 9020
