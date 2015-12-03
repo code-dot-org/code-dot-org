8355c4fe6ec3d032888686efa082fdca598a9b8b
Continuous Integration <dev@code.org>
2015-12-03 04:14:30 +0000

    Automatically built.
    45babbd Merge pull request #5828 from code-dot-org/fix-signed-in-progress (Josh Lory)
    91175d4 Use AJAX to load progress for all scripts, not just publicly-cached (Josh Lory)

--- dashboard/public/shared-package/js/shared.js
+++ dashboard/public/shared-package/js/shared.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e2033c63e428f29fd15e36c94bb992c66392e8b18a9ffae02b103eb5c4b81d5
-size 42800
+oid sha256:2c9a15183cbc2d32f08a519c0e245ac003876d3bfce70dc7ef5bce02f7d085ed
+size 42772
