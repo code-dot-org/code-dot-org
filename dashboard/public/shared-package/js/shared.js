06efe5c8474d54234ac15d4787cd29c74e7a7060
Continuous Integration <dev@code.org>
2015-12-03 21:53:27 +0000

    Automatically built.
    714614b Merge pull request #5832 from code-dot-org/twilio-copy-link-fix (Brian Jordan)
    a91b494 Merge pull request #5846 from code-dot-org/fix-teacher-view (Josh Lory)
    74cabc9 Merge pull request #5754 from code-dot-org/clear-session-on-signin (Josh Lory)
    5df3f3e Merge pull request #5845 from code-dot-org/hocremovebeta (Brendan Reville)
    505fb54 Merge pull request #5840 from code-dot-org/reposition-actors (Caley Brock)
    2531a75 Merge pull request #5809 from code-dot-org/player-delay-by-code (Brian Jordan)

--- dashboard/public/shared-package/js/shared.js
+++ dashboard/public/shared-package/js/shared.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7acb61f160c311915129ba608d97b325ad718d0664005ea0b844eebfdbe4165e
-size 43709
+oid sha256:e87a09ba8ba385f4859d1a94f09d2c362844babd713ff9ab314a750622ba9508
+size 43815
