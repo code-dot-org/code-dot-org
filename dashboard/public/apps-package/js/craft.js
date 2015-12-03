ccbbcfd08bd95cb963274815df61f16b3360a4f1
Continuous Integration <dev@code.org>
2015-12-03 23:09:58 +0000

    Automatically built.
    3ad1a13 Merge pull request #5852 from code-dot-org/mc-events (Brian Jordan)
    4f5ffc9 Merge pull request #5847 from code-dot-org/reset-redirect-page (Brian Jordan)
    2df8864 Merge pull request #5817 from code-dot-org/rtl-styling-changes (Brian Jordan)
    512b777 Merge pull request #5827 from code-dot-org/house-selection-styling (Brian Jordan)

--- dashboard/public/apps-package/js/craft.js
+++ dashboard/public/apps-package/js/craft.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d583ec92ccd456c4b07389a7b6b95e6efc2d5ebd9f03876d8d29f9741c309664
-size 902317
+oid sha256:e45399df7b44a62de897cfe80c16aa386aa09fb42120910f489fa7303ef2b232
+size 902835
