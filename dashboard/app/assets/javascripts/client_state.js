4d7975182ab1f212cf9a12e597148ddf964bdeb0
Josh Lory <josh.lory@code.org>
2015-12-03 11:07:28 -0800

    Don't load current_user progress when ?solution=true

--- dashboard/app/assets/javascripts/client_state.js
+++ dashboard/app/assets/javascripts/client_state.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2ddb83fb8d3fd16c6d3b94258f93faaa1c4420c972f8f2c67ded9b3a7068e8c
-size 7643
+oid sha256:f89a4456b2303a750e3f4d3b201455805e1a85426b06ae3647044f0ff6ea78e7
+size 7977
