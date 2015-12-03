ce32dceceab3f95d3835f05f1f53135108f595b9
Continuous Integration <dev@code.org>
2015-12-03 21:53:30 +0000

    Automatically built.
    5df3f3e Merge pull request #5845 from code-dot-org/hocremovebeta (Brendan Reville)
    505fb54 Merge pull request #5840 from code-dot-org/reposition-actors (Caley Brock)
    2531a75 Merge pull request #5809 from code-dot-org/player-delay-by-code (Brian Jordan)
    4a1d09d Merge pull request #5849 from code-dot-org/instruction-updates (Caley Brock)
    cbadd02 Merge pull request #5848 from code-dot-org/fix-send-to-phone (Laurel)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c18887974d5152f1bbb7b1ace1a58d18072c64f61086180bb2282f58c1fb9bf4
-size 2029797
+oid sha256:f748c664557003b2feadb74146b3627e118481661be1b5ccf3fa29cb63d35ae8
+size 2032925
