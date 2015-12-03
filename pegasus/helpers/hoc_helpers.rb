9c751f9e33be58fef1cdb75787fb3c024c648170
Phil Bogle <phil@code.org>
2015-12-02 18:01:41 -0800

    Sampled_hoc_activity

--- pegasus/helpers/hoc_helpers.rb
+++ pegasus/helpers/hoc_helpers.rb
@@ -2,11 +2,9 @@ require 'rmagick'
 require 'cdo/graphics/certificate_image'
 require 'dynamic_config/gatekeeper'
 
-def hoc_activity_writes_disabled
-  Gatekeeper.allows('hoc_activity_writes_disabled')
-end
+UNSAMPLED_HOC_COOKIE = 'HOC_UNSAMPLED'
 
-def create_session_row(row)
+def create_session_row(row, weight: 1.0)
   retries = 3
 
   begin
@@ -15,20 +13,20 @@ def create_session_row(row)
   end while row[:id] == 0 && (retries -= 1) > 0
 
   raise "Couldn't create a unique session row." if row[:id] == 0
+  set_hour_of_code_cookie_for_row(row[:session])
+end
 
+def create_session_row_if_in_sample(row)
+  p = DCDO.get('hoc_activity_sample_proportion', default: 1.0).to_f
+  p = 1.0 if p == 0.0  # Don't sample if the proportion is invalid.
+  if rand() < p
+    row = create_session_row(row, weight: 1.0 / p)
+  else
+    set_hour_of_code_cookie_for_row({session: HOC_UNSAMPLED})
+    row = nil
+  end
   row
 end
-
-def maybe_create_session_row(row)
-  # Fraction
-  sampling_proportion = DCDO.get('hoc_activity_sampling_proportion', default: 1).to_i
-  if sample_fraction > 0 || rand() <= sampling_proportion
-    weight = 1 / sample_rate
-
-
-  end
-end
-
 
 def session_status_for_row(row)
   row ||= {}
@@ -50,11 +48,16 @@ def set_hour_of_code_cookie_for_row(row)
   response.set_cookie('hour_of_code', {value: row[:session], domain: '.code.org', path: '/api/hour/'})
 end
 
+def set_unsampled_hour_of_code_cookie
+  set_hour_of_code_cookie_for_row({session: HOC_UNSAMPLED})
+end
+
 def complete_tutorial(tutorial={})
+  hoc_cookie = request.cookies['hour_of_code']
   unless settings.read_only
     # We intentionally allow this DB write even when hoc_activity_writes_disabled
     # is set so we can generate personalized, shareable certificates.
-    row = DB[:hoc_activity].where(session: request.cookies['hour_of_code']).first
+    row = DB[:hoc_activity].where(session: hoc_cookie).first unless
     if row
       DB[:hoc_activity].where(id: row[:id]).update(
         finished_at: DateTime.now,
@@ -67,7 +70,6 @@ def complete_tutorial(tutorial={})
         finished_at: DateTime.now,
         finished_ip: request.ip,
       )
-      set_hour_of_code_cookie_for_row(row)
     end
     destination = "http://#{row[:referer]}/congrats?i=#{row[:session]}"
     destination += "&co=#{row[:company]}" unless row[:company].blank?
@@ -79,7 +81,7 @@ def complete_tutorial(tutorial={})
 end
 
 def complete_tutorial_pixel(tutorial={})
-  unless settings.read_only || hoc_activity_writes_disabled
+  unless settings.read_only
     row = DB[:hoc_activity].where(session: request.cookies['hour_of_code']).first
     if row && !row[:pixel_finished_at] && !row[:finished_at]
       DB[:hoc_activity].where(id: row[:id]).update(
@@ -87,13 +89,12 @@ def complete_tutorial_pixel(tutorial={})
         pixel_finished_ip: request.ip,
       )
     else
-      row = create_session_row(
+      row = create_session_row_if_in_sample(
         referer: request.host_with_port,
         tutorial: tutorial[:code],
         pixel_finished_at: DateTime.now,
         pixel_finished_ip: request.ip,
       )
-      set_hour_of_code_cookie_for_row(row)
     end
   end
 
@@ -102,15 +103,14 @@ def complete_tutorial_pixel(tutorial={})
 end
 
 def launch_tutorial(tutorial,params={})
-  unless settings.read_only || hoc_activity_writes_disabled
-    row = create_session_row(
+  unless settings.read_only
+    row = create_session_row_if_in_sample(
       referer: request.referer_site_with_port,
       tutorial: tutorial[:code],
       company: params[:company],
       started_at: DateTime.now,
       started_ip: request.ip,
     )
-    set_hour_of_code_cookie_for_row(row)
   end
 
   dont_cache
@@ -126,14 +126,13 @@ def launch_tutorial_pixel(tutorial)
         pixel_started_ip: request.ip,
       )
     else
-      row = create_session_row(
+      row = create_session_row_if_in_sample(
         referer: request.host_with_port,
         tutorial: tutorial[:code],
         company: params[:company],
         pixel_started_at: DateTime.now,
         pixel_started_ip: request.ip,
       )
-      set_hour_of_code_cookie_for_row(row)
     end
   end
 
