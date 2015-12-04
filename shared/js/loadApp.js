237a4048fdb0d72c646650b4d7a8f1f404f48656
Josh Lory <josh.lory@code.org>
2015-12-03 17:41:58 -0800

    Load disableSocialShare setting via AJAX for publicly-cached pages

--- shared/js/loadApp.js
+++ shared/js/loadApp.js
@@ -33,7 +33,14 @@ module.exports = function (callback) {
   var isViewingStudentAnswer = !!dashboard.clientState.queryParams('user_id');
 
   if (!appOptions.channel && !isViewingSolution && !isViewingStudentAnswer) {
+
+    if (appOptions.publicCaching) {
+      appOptions.disableSocialShare = true;
+    }
+
     $.ajax('/api/user_progress/' + appOptions.scriptName + '/' + appOptions.stagePosition + '/' + appOptions.levelPosition).done(function (data) {
+      appOptions.disableSocialShare = data.disableSocialShare;
+
       // Merge progress from server (loaded via AJAX)
       var serverProgress = data.progress || {};
       var clientProgress = dashboard.clientState.allLevelsProgress()[appOptions.scriptName] || {};
