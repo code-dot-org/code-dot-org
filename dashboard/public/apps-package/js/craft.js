fcf437cbe1698e9f284e530713ac4f241c795975
Continuous Integration <dev@code.org>
2015-12-04 05:24:59 +0000

    Automatically built.
    a7821b4 Merge pull request #5884 from code-dot-org/disable-final-stage-message (Brian Jordan)
    d710131 content changes -tanya (Continuous Integration)
    95427a9 Guard against missing appStrings. - Add space only when extra string (Brian Jordan)

--- dashboard/public/apps-package/js/craft.js
+++ dashboard/public/apps-package/js/craft.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94c2dc0a6e346cfedb729d7ba61f35e561b460dedd9c195d833eb83ae4a725ea
-size 905813
+oid sha256:6683670b863b9ff42db3a3c0f9cef5e9044c21c5047f11e1795ef3100de0c3f8
+size 905998
