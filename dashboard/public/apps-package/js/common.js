82f83aeffd19e8f3de2ec592d66646d74bb25160
Continuous Integration <dev@code.org>
2015-12-04 00:03:33 +0000

    Automatically built.
    b3d87d6 Merge pull request #5859 from code-dot-org/last-level-dialog (Laurel)
    41195d2 stage -> puzzle (Laurel Fan)
    41f22b6 Merge pull request #5836 from code-dot-org/playlab-announce (Caley Brock)
    14ac35b Merge pull request #5851 from code-dot-org/classicmaze (Brendan Reville)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be0564f1968ac90ed9657c163ba4e78fbcc4fa4ed789c0bba91dc7a02e37c956
-size 3910990
+oid sha256:d91eeb1515c700a6a24cd92e45e8c0d79d4fa6e56aa242126c5d7cd13d9595af
+size 3911909
