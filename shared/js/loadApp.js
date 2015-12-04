4dd29d19c353a63383444e0beceedafdcebdcfc7
Josh Lory <josh.lory@code.org>
2015-12-04 10:32:32 -0800

    Code review feedback [ci skip]

--- shared/js/loadApp.js
+++ shared/js/loadApp.js
@@ -35,6 +35,9 @@ module.exports = function (callback) {
   if (!appOptions.channel && !isViewingSolution && !isViewingStudentAnswer) {
 
     if (appOptions.publicCaching) {
+      // Disable social share by default on publicly-cached pages, because we don't know
+      // if the user is underage until we get data back from /api/user_progress/ and we
+      // should err on the side of not showing social links
       appOptions.disableSocialShare = true;
     }
 
