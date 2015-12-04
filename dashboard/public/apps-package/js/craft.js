7bc7e94e82e95df4cc2bf5c180f8701d09c1b139
Continuous Integration <dev@code.org>
2015-12-04 00:55:57 +0000

    Automatically built.
    28263bb Merge pull request #5843 from code-dot-org/mc-teacher-dashboard (Brian Jordan)
    5e6b61a Merge pull request #5835 from code-dot-org/finish-on-button (Brian Jordan)
    7a67ef7 Merge pull request #5868 from code-dot-org/promoteca (Brendan Reville)
    05f539c Re-add special California notice to /promote. (Brendan Reville)
    d987235 Merge branch 'test' into staging (Josh Lory)
    8d088c5 Automatically built. (Continuous Integration)
    574b69e Merge branch 'production' into test (Josh Lory)
    90758f3 Merge pull request #5834 from code-dot-org/twilio_messaging_service (Will Jordan)
    c22946d Merge pull request #5864 from code-dot-org/test (Josh Lory)

--- dashboard/public/apps-package/js/craft.js
+++ dashboard/public/apps-package/js/craft.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e45399df7b44a62de897cfe80c16aa386aa09fb42120910f489fa7303ef2b232
-size 902835
+oid sha256:b853eb098e10f35856ceb47db9cf8c6f6d8b9867c6285123e7f8e73d48ddc626
+size 905315
