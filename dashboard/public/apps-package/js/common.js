870946cde04da63dcde908c5d732d48492381c4b
Continuous Integration <dev@code.org>
2015-12-05 04:07:58 +0000

    Automatically built.
    74bbab0 Merge pull request #5938 from code-dot-org/revert-staging (Brad Buchanan)
    bb950b2 Unrevert "Merge pull request #5872 from code-dot-org/artist-gallery-quant" (Brad Buchanan)
    8a28730 Unrevert "Merge pull request #5875 from code-dot-org/minecraft-footer-flyout-color" (Brad Buchanan)
    f5d2804 Unrevert "Merge pull request #5886 from code-dot-org/mc-share-scale-fix" (Brad Buchanan)
    4517f5a Unrevert "Merge pull request #5891 from code-dot-org/levelbuilder" (Brad Buchanan)
    f65182c Unrevert "Merge pull request #5884 from code-dot-org/disable-final-stage-message" (Brad Buchanan)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3589bd66516aff12a24adb493b765a6739d11958eba6889e34f814b9a04621ff
-size 3912300
+oid sha256:90d03daecf956246c4ef409b38b3045af75e506101fa34cb58806aabfa356101
+size 3913035
