8395565984c24d00c5d056deb722f11ad00ef396
Laurel Fan <laurel@code.org>
2015-12-08 15:15:44 -0800

    some comments explaining pandas

--- dashboard/test/integration/charset_test.rb
+++ dashboard/test/integration/charset_test.rb
@@ -1,5 +1,9 @@
 require 'test_helper'
 
+# UTF8 4 byte characters (which include many emoji) really confuse our
+# database -- they cause exceptions on writes and cause indexes to be
+# not used when they should on reads. These tests test that they are
+# rejected before attempting to read or write them from the db.
 class CharsetTest < ActionDispatch::IntegrationTest
   def no_database
     Rails.logger.info '--------------'
@@ -17,7 +21,6 @@ class CharsetTest < ActionDispatch::IntegrationTest
 
     no_database
 
-    panda_panda = "Panda \u{1F43C}"
     post '/users/sign_in', login: panda_panda, password: 'not a password'
     assert_response :success
     assert_select 'div.alert', 'Invalid email or password.'
