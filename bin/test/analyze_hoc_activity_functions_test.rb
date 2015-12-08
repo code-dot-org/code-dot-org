e7065f64035bddc95b9224b2d7686b176a370310
Asher Kach <asher@code.org>
2015-12-07 19:21:37 -0600

    Refactors analyze_activity_functions, pulling methods outside of the cron job. Also adds tests for some of the functions.

---
+++ bin/test/analyze_hoc_activity_functions_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'rack/test'
+
+require_relative '../analyze_hoc_activity_functions'
+
+class AnalyzeHocActivityFunctionsTest < Minitest::Test
+  def test_add_count_to_hash_existing_key
+    a = {'k' => 10}
+    add_count_to_hash a, 'k', 1
+    assert_equal 11, a['k']
+  end
+
+  def test_add_count_to_hash_new_key
+    a = {'k' => 10}
+    add_count_to_hash a, 'l', 1
+    assert_equal 10, a['k']
+    assert_equal 1, a['l']
+  end
+
+  def test_add_hashes_disjoint_keys
+    a = {'j' => 1, 'k' => 2}
+    b = {'l' => 3}
+    sum = add_hashes(a,b)
+    assert_equal 1, sum['j']
+    assert_equal 2, sum['k']
+    assert_equal 3, sum['l']
+  end
+
+  def test_add_hashes_intersecting_keys
+    a = {'j' => 1, 'k' => 2}
+    b = {'j' => 3}
+    sum = add_hashes(a,b)
+    assert_equal 4, sum['j']
+    assert_equal 2, sum['k']
+  end
+end
