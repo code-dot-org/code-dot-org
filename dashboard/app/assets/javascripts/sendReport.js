b5224d36aaa2d1f387a1bc76e084dc6fb8ff68c8
Josh Lory <josh.lory@code.org>
2015-12-08 14:40:55 -0800

    Don't compare `undefined === undefined`

--- dashboard/app/assets/javascripts/sendReport.js
+++ dashboard/app/assets/javascripts/sendReport.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:714385fa40100ccb4b63f360bd1044105ebfbe202738fb0cfdca30fa3a81c00c
-size 4628
+oid sha256:3738335d4f520a1596552b8a40d319c5f6d56dbc109abf7214ccc2618cd9c0e5
+size 4662
