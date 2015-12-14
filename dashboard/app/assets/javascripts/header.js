aa28f46b60d0656cffd69ab701af36881a6202de
Josh Lory <josh.lory@code.org>
2015-12-14 15:27:54 -0800

    Preserve whole query string in teacher view
    This way we don't have to manually copy over userId and sectionId. Also makes ?solution=true carry over when navigating via the progress dots.

--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac5433297a192ae3f97708a3aaa8cdb6b3515e3cbc20964571deec2ae71b2313
-size 17876
+oid sha256:54c1cc9eb49fc557723fcbce4b7993a38fab92aa34877da21dc6020364d0b53c
+size 17727
