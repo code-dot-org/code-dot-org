b54aff86e8c64e1939811fc39ee37acd8e3a011d
Asher Kach <asher@code.org>
2015-12-15 15:30:23 -0600

    Add ruby declaration.

--- aws/emr/cdo-logs/script/elb_mapper.rb
+++ aws/emr/cdo-logs/script/elb_mapper.rb
@@ -1,7 +1,9 @@
+#!/usr/bin/ruby
+
 ARGF.each do |line|
   # Determine whether the log is a GET or POST request, emitting the appropriate
   # key/value.
-  if line.include? "GET"
+  if line && (line.include? "GET")
     puts "GET" + "\t" + "1"
   elsif line.include? "POST"
     puts "POST" + "\t" + "1"
