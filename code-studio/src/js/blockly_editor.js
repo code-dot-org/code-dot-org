b10819611382f8dd9fe3fea3bef9a38bc554f3f8
Brad Buchanan <brad@code.org>
2015-12-14 17:42:30 -0800

    Move JS from _studio.html.haml to blockly_editor.js

--- code-studio/src/js/blockly_editor.js
+++ code-studio/src/js/blockly_editor.js
@@ -142,4 +142,22 @@ $(function () {
     updateCalloutJSON();
   });
 
+  var jQuerySuccessConditionBox = $('#level_success_condition');
+  if (jQuerySuccessConditionBox.length) {
+    CodeMirror.fromTextArea(jQuerySuccessConditionBox.get(0), {
+      mode: 'javascript',
+      viewportMargin: Infinity,
+      matchBrackets: true
+    });
+  }
+
+  var jQueryFailureConditionBox = $('#level_failure_condition');
+  if (jQueryFailureConditionBox.length) {
+    CodeMirror.fromTextArea(jQueryFailureConditionBox.get(0), {
+      mode: 'javascript',
+      viewportMargin: Infinity,
+      matchBrackets: true
+    });
+  }
+
 });
