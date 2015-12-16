9305aaab9d14795aa04fdff62b76fefa8d1399ec
Continuous Integration <dev@code.org>
2015-12-16 21:37:53 +0000

    Automatically built.
    f2bfe0e Merge pull request #6098 from code-dot-org/applab-success-callback (David Bailey)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64a554c0d0b0155e4776705a2e30bbe608b69d46c5718f9a140ad5ffd6efddf9
-size 2212443
+oid sha256:5cbf794c53a60cbb10c5e753190ad9d3f57480c0583af3bf55f7196709654c7b
+size 2212389
