f0086392290cc0bb8f3083a9f2ffef1286c8cb12
Andrew Oberhardt <Andrew@Code.org>
2015-12-17 21:13:03 -0800

    Added mailing scripts

--- pegasus/forms/hoc_survey_prize_2015.rb
+++ pegasus/forms/hoc_survey_prize_2015.rb
@@ -11,7 +11,7 @@ class HocSurveyPrize2015
 
   def self.process(data)
     {}.tap do |results|
-      results['prize_code_s'] = claim_prize_code(data['prize_choice_s'], data['email_s'], '');
+      results['prize_code_s'] = claim_prize_code(data['prize_choice_s'], data['email_s'], 'Hoc2015');
     end
   end
 
