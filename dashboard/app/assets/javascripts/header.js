e9d93cdcc44a6a2cf9ba7ad708108d23ffe96d22
Brent Van Minnen <bjvanminnen@gmail.com>
2015-12-23 22:41:51 -0500

    update comments

--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f65a308b4e69248a96acd10d887f5b139398aa157773dff6477c0f86134b614
-size 16313
+oid sha256:135820036ffb7009a864330aba6fc813088a00cc98c3e99fef33a411af42daa6
+size 15979
