82565af90222f5431465cabe368492fe35b87f63
Brent Van Minnen <bjvanminnen@gmail.com>
2015-12-23 23:50:42 -0500

    dialogs appear identical

--- dashboard/app/assets/javascripts/components/share_dialog.jsx
+++ dashboard/app/assets/javascripts/components/share_dialog.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c4d3fe0f46a18f878bdbb86bf7496aedffa9c9a0dc4612888a18a6b262f0e14
-size 2781
+oid sha256:741fd7baedf91676865ffaa1681b0cf155e137a82d8bf10ac2c4585d0afa80d8
+size 2777
