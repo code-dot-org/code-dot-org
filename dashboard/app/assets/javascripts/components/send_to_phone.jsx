99eb7a624666f06222a849a3c6515c2807214823
Brent Van Minnen <bjvanminnen@gmail.com>
2015-12-26 14:44:02 -0500

    extract wireframe specific portion

--- dashboard/app/assets/javascripts/components/send_to_phone.jsx
+++ dashboard/app/assets/javascripts/components/send_to_phone.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d94e77412c57c8c3bc69e633de0e6d27a705d0bc9f455674bd739f8f86ca9b4
-size 3896
+oid sha256:1707c5d7dd1ad7863023fa868ecdf565896e614896ee18779cea7906091cf384
+size 3257
--- dashboard/app/assets/javascripts/components/share_dialog_body.jsx
+++ dashboard/app/assets/javascripts/components/share_dialog_body.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fb1c762e473829a4fb82498bfa6812a83556747e4bd6b7a60033207b04bec64
-size 4155
+oid sha256:d6afb5d9824e9874dc7f3204aea425e7ced6c83e6786f6dfbc49722c22eb39d3
+size 4138
