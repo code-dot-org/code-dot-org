398d155ba263c655559604e8da10e215b589543a
Brent Van Minnen <bjvanminnen@gmail.com>
2015-12-26 14:15:13 -0500

    remove dead code

--- dashboard/app/assets/javascripts/components/send_to_phone.jsx
+++ dashboard/app/assets/javascripts/components/send_to_phone.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:901027a37015d9171d5cb3c16500b7b1efb49ad993673989651903b9eab35bec
-size 3946
+oid sha256:4d94e77412c57c8c3bc69e633de0e6d27a705d0bc9f455674bd739f8f86ca9b4
+size 3896
--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b285c49a83e173c9c975eed0c171b183ce324b1aec759273bee5c84899f62d1
-size 14528
+oid sha256:b54edb67d9683845584a4a66380de40c9f477dccf733c043cc6098695eafcfb6
+size 12759
