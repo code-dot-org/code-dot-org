9ab7d48ae1622d9d4fbcbb8de01578d5124b6d5a
Brent Van Minnen <bjvanminnen@gmail.com>
2015-12-25 21:38:11 -0500

    start fully Reactifying SendToPhone

--- dashboard/app/assets/javascripts/components/send_to_phone.jsx
+++ dashboard/app/assets/javascripts/components/send_to_phone.jsx
@@ -7,14 +7,53 @@ window.dashboard = window.dashboard || {};
  */
 window.dashboard.SendToPhone = (function (React) {
   return React.createClass({
-    propTypes: {},
+    propTypes: {
+      showLead: React.PropTypes.bool.isRequired
+    },
+
+    getInitialState: function () {
+      return {
+        showLead: this.props.showLead,
+        canSubmit: false
+      };
+    },
+
+    componentDidMount: function () {
+      if (this.refs.phone) {
+        this.refs.phone.focus();
+      }
+    },
+
+    componentDidUpdate: function () {
+      if (this.refs.phone) {
+        this.refs.phone.focus();
+      }
+    },
+
+    clickLead: function (event) {
+      this.setState({showLead: false});
+    },
+
+    handleSubmit: function () {
+    },
 
     render: function () {
+      if (this.state.showLead) {
+        return (
+          <div onClick={this.clickLead}>
+            <i className="fa fa-mobile"></i> See this app on your phone
+          </div>
+        );
+      }
+
+      // TODO - can i get rid of some ids?
       return (
-        <div id="send-to-phone" className="sharing" style={{display: 'none'}}>
+        <div id="send-to-phone" className="sharing">
           <label htmlFor="phone">Enter a US phone number:</label>
-          <input type="text" id="phone" name="phone"></input>
-          <button id="phone-submit">Send</button>
+          <input type="text" id="phone" ref="phone" name="phone"></input>
+          <button id="phone-submit" disabled={!this.state.canSubmit}>
+            Send
+          </button>
           <div id="phone-charges">
             A text message will be sent via <a href="http://twilio.com">Twilio</a>.
             Charges may apply to the recipient.
