d7bb5ac3a1857e0cf7738fde0c010a6b04ad1787
Continuous Integration <dev@code.org>
2016-01-04 20:29:36 +0000

    Automatically built.
    1cb5a62 Merge pull request #6149 from code-dot-org/flesh-out-hint-authoring (Elijah Hamovitz)
    301c69e Automatically built. (Continuous Integration)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22e89eb357c23c146ab4768dbf0deb7d48cbe26f77688c3b3211236dd69dbcf9
-size 4043045
+oid sha256:1d71aa8ac4c57c6bfdbdbcfb3625dcacc072544e6fc57b161c2e83c7ab5cc689
+size 4043560
