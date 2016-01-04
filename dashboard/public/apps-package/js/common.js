060ad2a0159e0ebdfb53d23d26ba62cae0f37c16
Continuous Integration <dev@code.org>
2016-01-04 21:03:35 +0000

    Automatically built.
    5670bdd Merge pull request #6088 from code-dot-org/move-feedback-blocks-to-own-blockspace (Elijah Hamovitz)
    be371f6 Merge pull request #6139 from code-dot-org/tutorialEmr3 (ashercodeorg)
    192975d Automatically built. (Continuous Integration)
    1cb5a62 Merge pull request #6149 from code-dot-org/flesh-out-hint-authoring (Elijah Hamovitz)
    301c69e Automatically built. (Continuous Integration)
    28ad03e removed Blockly.readOnly hack; now using new Blockly.isReadOnly() functionality (Elijah Hamovitz)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d71aa8ac4c57c6bfdbdbcfb3625dcacc072544e6fc57b161c2e83c7ab5cc689
-size 4043560
+oid sha256:d4afdb0b54e85ef003c3600eb6922d66b1c9a7123fc5dd34422b68fcf3329a82
+size 4041971
