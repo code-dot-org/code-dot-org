fab60f80a47d8653fa65638d3c3334cb76e25b02
Continuous Integration <dev@code.org>
2016-01-05 23:29:59 +0000

    Automatically built.
    850875b Merge pull request #6207 from code-dot-org/delegate-jsoneditor-events (Elijah Hamovitz)
    f8d8b16 Automatically built. (Continuous Integration)

--- dashboard/public/code-studio-package/js/levelbuilder.js
+++ dashboard/public/code-studio-package/js/levelbuilder.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8f60e4aebe600bf04d271c0fb97bf8c90b023cb1e4b7ac8eba7e475c2a6acf9
-size 1072260
+oid sha256:841d22ae5f6072735af43287d470c56ab06de7ffd831cf637e65d8dde06e9f22
+size 1072394
