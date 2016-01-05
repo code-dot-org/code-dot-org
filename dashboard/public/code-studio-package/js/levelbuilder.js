eb96e4695d7d3bbdde8d8f9de5b1a1059c030954
Continuous Integration <dev@code.org>
2016-01-05 23:59:59 +0000

    Automatically built.
    97b3bd7 Merge pull request #6196 from code-dot-org/move-levels-widget-js-erb (Brad Buchanan)
    68b93c9 Merge pull request #5368 from code-dot-org/rack-cache-development (Will Jordan)
    89a99d9 Automatically built. (Continuous Integration)
    850875b Merge pull request #6207 from code-dot-org/delegate-jsoneditor-events (Elijah Hamovitz)
    21f1801 s/program/commander to match package name (Brad Buchanan)
    63a9266 Remove main() functions from build scripts. (Brad Buchanan)
    8d2da01 Correct widget level initialization order. (Brad Buchanan)
    f8d8b16 Automatically built. (Continuous Integration)
    5d064ab delegate jsonEditor events on dynamically-created elements (Elijah Hamovitz)
    ab93302 workaround bundle bug with identical file name & contents (cpirich)
    648a923 Merge branch 'staging' into move-levels-widget-js-erb (Brad Buchanan)
    a01995f Merge pull request #6204 from code-dot-org/levelbuilder (philbogle)
    95a7773 content changes (-Phil) (Continuous Integration)

--- dashboard/public/code-studio-package/js/levelbuilder.js
+++ dashboard/public/code-studio-package/js/levelbuilder.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:841d22ae5f6072735af43287d470c56ab06de7ffd831cf637e65d8dde06e9f22
-size 1072394
+oid sha256:76eb5cb26077030d8d20bedbe6c585f3a69c81a5ba9fb64cb1e4b3c799accd8f
+size 12761
