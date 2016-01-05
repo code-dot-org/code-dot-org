820ca5bb4fe8b52fa7c137b84d67cee278f3f33a
Brad Buchanan <brad@code.org>
2016-01-04 16:17:37 -0800

    Move onload js out of widget view into JS file, and move JS file to head.

--- code-studio/src/js/leveltype_widget.js
+++ code-studio/src/js/leveltype_widget.js
@@ -1,6 +1,11 @@
 /**
  * @file JavaScript loaded in all Widget-type levels.
  */
+/* global dashboard */
 'use strict';
 
 window.CodeMirror = require('codemirror');
+
+$(document).ready(function () {
+  $('#bubble').click(dashboard.dialog.showInstructionsDialog);
+});
