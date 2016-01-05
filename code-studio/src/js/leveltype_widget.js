48ab8cd78005bb1b8870f55e33a631c6f56ceb3a
Brad Buchanan <brad@code.org>
2016-01-04 16:26:46 -0800

    Move the rest of JS out of widget level view and rest of includes into head.

--- code-studio/src/js/leveltype_widget.js
+++ code-studio/src/js/leveltype_widget.js
@@ -1,11 +1,22 @@
 /**
  * @file JavaScript loaded in all Widget-type levels.
  */
-/* global dashboard */
+/* global apps, appOptions, dashboard */
 'use strict';
 
 window.CodeMirror = require('codemirror');
 
 $(document).ready(function () {
+  window.script_path = location.pathname;
+  apps.setupApp(appOptions);
+  appOptions.showInstructionsWrapper(dashboard.dialog.showInstructionsDialog);
+  window.getResult = function () {
+    return {
+      response: 'ok',
+      result: true
+    };
+  };
+  window.options = appOptions.level;
+
   $('#bubble').click(dashboard.dialog.showInstructionsDialog);
 });
