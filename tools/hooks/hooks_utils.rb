9ac5cba92ffa11cd882577672b0dc4bac8388c8c
Mehal Shah <mehal@code.org>
2016-01-06 10:09:39 -0800

    CR feedback

--- tools/hooks/hooks_utils.rb
+++ tools/hooks/hooks_utils.rb
@@ -1,6 +1,6 @@
 class HooksUtils
-  def self.get_modified_files
-    Dir.chdir REPO_DIR
+  def self.get_modified_files(repo_directory)
+    Dir.chdir repo_directory
     `git diff --cached --name-only --diff-filter AM`.split("\n").map(&:chomp).map { |x| File.expand_path("../../../#{x}", __FILE__)}
   end
 end
