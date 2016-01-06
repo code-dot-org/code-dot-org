71fff0795727b1ee3bfcd836d6182ad3ee0355b7
Continuous Integration <dev@code.org>
2016-01-06 02:13:45 +0000

    Automatically built.
    85b320e Merge pull request #6215 from code-dot-org/applab-getNumber-setNumber (Brad Buchanan)
    1eebe3d Fix generateDropletModeOptions test (Brad Buchanan)
    c789d09 Add getNumber/setNumber commands to Applab (Brad Buchanan)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9f9242c6de357afe4ea10d7c255f7ecd78d1c8968a8db8c496afdcb9fec9ec2
-size 2219258
+oid sha256:25e00d93a8f5db5675feb2cdfe8290ff939afcdd5ab0909cbb9e56d5007ff228
+size 2225203
