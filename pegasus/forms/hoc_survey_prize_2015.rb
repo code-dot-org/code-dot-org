e11107ae8da6419367347af86f3fbd30ce67f66f
Andrew Oberhardt <Andrew@Code.org>
2016-01-07 15:47:13 -0800

    Now set hoc_survey_prize claimed_ip from prize selection form. Added script to update existing entries

--- pegasus/forms/hoc_survey_prize_2015.rb
+++ pegasus/forms/hoc_survey_prize_2015.rb
@@ -9,9 +9,10 @@ class HocSurveyPrize2015
     result
   end
 
-  def self.process(data)
+  def self.process_(row)
+    data = JSON.load(row[:data])
     {}.tap do |results|
-      results['prize_code_s'] = claim_prize_code(data['prize_choice_s'], data['email_s'], 'Hoc2015');
+      results['prize_code_s'] = claim_prize_code(data['prize_choice_s'], data['email_s'], 'Hoc2015', ip_address: row[:created_ip]);
     end
   end
 
