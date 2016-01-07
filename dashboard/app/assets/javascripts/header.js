3be612c36e8e539229f419088bb55c71b7016a25
Josh Lory <josh.lory@code.org>
2016-01-07 15:13:32 -0800

    Fix progress for signed out users (params removed from caller)

--- dashboard/app/assets/javascripts/header.js
+++ dashboard/app/assets/javascripts/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83ee3db3a53b087b52867699295a4cdbedd08f5eb0f4d37600b18e768e9df754
-size 15173
+oid sha256:119ce18b20ff563bcd822f3cbac9a2b45b64c537fe71c49392696c4caf72e85f
+size 15230
