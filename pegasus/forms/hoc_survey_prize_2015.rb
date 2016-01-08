b73423ba1d91cda91479211e012401c967899f64
Andrew Oberhardt <Andrew@Code.org>
2016-01-07 16:34:55 -0800

    Changed param row to form for clarity

--- pegasus/forms/hoc_survey_prize_2015.rb
+++ pegasus/forms/hoc_survey_prize_2015.rb
@@ -9,10 +9,10 @@ class HocSurveyPrize2015
     result
   end
 
-  def self.process_(row)
-    data = JSON.load(row[:data])
+  def self.process_(form)
+    data = JSON.load(form[:data])
     {}.tap do |results|
-      results['prize_code_s'] = claim_prize_code(data['prize_choice_s'], data['email_s'], 'Hoc2015', ip_address: row[:created_ip]);
+      results['prize_code_s'] = claim_prize_code(data['prize_choice_s'], data['email_s'], 'Hoc2015', ip_address: form[:created_ip]);
     end
   end
 
