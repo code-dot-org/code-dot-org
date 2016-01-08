3e0fcadf1735e253c6d737384d970849bb380823
Brad Buchanan <brad@code.org>
2016-01-07 16:32:38 -0800

    Rename sassCommand => sass (like browserify)

--- code-studio/build-css.js
+++ code-studio/build-css.js
@@ -32,7 +32,7 @@ commander
 // Run build (exits on failure)
 build_commands.execute([
   build_commands.ensureDirectoryExists(BUILD_PATH),
-  build_commands.sassCommand(SRC_PATH, BUILD_PATH, 'levelbuilder.scss', INCLUDE_PATHS, commander.min),
-  build_commands.sassCommand(SRC_PATH, BUILD_PATH, 'leveltype_widget.scss', INCLUDE_PATHS, commander.min)
+  build_commands.sass(SRC_PATH, BUILD_PATH, 'levelbuilder.scss', INCLUDE_PATHS, commander.min),
+  build_commands.sass(SRC_PATH, BUILD_PATH, 'leveltype_widget.scss', INCLUDE_PATHS, commander.min)
 ]);
 build_commands.logBoxedMessage("code-studio css built");
