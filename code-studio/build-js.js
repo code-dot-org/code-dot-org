0f34e5f0050c7d4abdc3211b6b6653a6c3b94932
Brad Buchanan <brad@code.org>
2016-01-11 11:08:48 -0800

    Remove some duplication in build-js.js

--- code-studio/build-js.js
+++ code-studio/build-js.js
@@ -3,6 +3,7 @@
     by dashboard (our "Code Studio" Rails app). */
 'use strict';
 
+var _ = require('lodash');
 var build_commands = require('./build-commands');
 var commander = require('commander');
 
@@ -20,14 +21,24 @@ commander
     .option('--watch', 'Watch file system', false)
     .parse(process.argv);
 
+var defaultOptions = {
+  srcPath: './src/js/',
+  buildPath: './build/js/',
+  shouldFactor: false,
+  shouldMinify: commander.min,
+  shouldWatch: commander.watch
+};
+
 // Run build (exits on failure)
 // TODO - think about how watchify will work for multiple commands
 build_commands.execute([
   build_commands.ensureDirectoryExists(BUILD_PATH),
   build_commands.ensureDirectoryExists(BUILD_PATH + 'levels/'),
-  build_commands.browserify({
-    srcPath: SRC_PATH,
-    buildPath: BUILD_PATH,
+
+  // Code in code-studio.js and any common code factored out of this bundle
+  // gets included into every page in dashboard.
+  // @see application.html.haml
+  build_commands.browserify(_.extend({}, defaultOptions, {
     filenames: [
       'code-studio.js',
       'levelbuilder.js',
@@ -37,41 +48,32 @@ build_commands.execute([
       'levels/widget.js'
     ],
     commonFile: 'code-studio-common',
-    shouldFactor: true,
-    shouldMinify: commander.min,
-    shouldWatch: commander.watch
-  }),
+    shouldFactor: true
+  })),
 
   // For now, build initApp (formerly the 'shared' package) as its own
   // build step, skipping factor-bundle.  Eventually since this and
   // code-studio-common can be included on the same page, we may want to try
   // factoring out common modules to optimize download size.
-  build_commands.browserify({
-    srcPath: SRC_PATH + 'initApp/',
-    buildPath: BUILD_PATH,
+  // @see _apps_dependencies.html.haml
+  build_commands.browserify(_.extend({}, defaultOptions, {
+    srcPath: './src/js/initApp/',
     filenames: [
       'initApp.js'
     ],
-    commonFile: 'initApp',
-    shouldFactor: false,
-    shouldMinify: commander.min,
-    shouldWatch: commander.watch
-  }),
+    commonFile: 'initApp'
+  })),
 
   // Build embedVideo.js in its own step (skipping factor-bundle) so that
   // we don't have to include the large code-studio-common file in the
   // embedded video page, keeping it fairly lightweight.
   // (I wonder how much more we could slim it down by removing jQuery!)
-  build_commands.browserify({
-    srcPath: SRC_PATH,
-    buildPath: BUILD_PATH,
+  // @see embed.html.haml
+  build_commands.browserify(_.extend({}, defaultOptions, {
     filenames: [
       'embedVideo.js'
     ],
-    commonFile: 'embedVideo',
-    shouldFactor: false,
-    shouldMinify: commander.min,
-    shouldWatch: commander.watch
-  })
+    commonFile: 'embedVideo'
+  }))
 ]);
 build_commands.logBoxedMessage("code-studio js built");
