0b8d22b1d07c7b36d3bee994dd91c4e0a33a5d39
Phil Bogle <phil@code.org>
2016-01-11 10:33:45 -0800

    Introduce gatekeeper general settings

--- dashboard/lib/feature_mode_manager.rb
+++ dashboard/lib/feature_mode_manager.rb
@@ -3,20 +3,25 @@
 #
 # There are currently three modes defined, intended to be easy to understand and reason about.
 #
-# - normal: All features enabled for all of the given scripts.
-# - scale: milestone posts disabled excepting for sharing levels, sharing enabled,
-#     puzzle rating and hints disabled. Sampled hoc activity logging at 10%.
-# - emergency: scale features, plus sharing disabled. Sampled hoc activity logging at 10%.
+# normal: All features enabled for all of the given scripts.
+# scale: Puzzle ratings and hints disabled on all tutorials.
+#       Milestone posts disabled but sharing enabled for high scale (HOC) tutorials.
+#       Hoc activity logging sampled at 10%.
+# emergency: scale features, plus sharing disabled. Sampled hoc activity logging at 10%.
 class FeatureModeManager
 
-  # A map from mode symbol to gatekeeper_settings and dcdo_settings.
+  # A map from mode symbol to gatekeeper and dcdo settings. The gatekeeper settings are
+  # broken down into gatekeeper_general_settings (which apply to all tutorials) and
+  # gatekeeper_hoc_tutorial_settings (which apply to high-scale HOC tutorials only).
   MODE_SETTINGS_MAP = {
     normal: {
-        gatekeeper_settings: {
+        gatekeeper_general_settings: {
+            puzzle_rating: true,
+            hint_view_request: true
+        },
+        gatekeeper_hoc_tutorial_settings: {
           postMilestone: true,
           shareEnabled: true,
-          puzzle_rating: true,
-          hint_view_request: true
         },
         dcdo_settings: {
           hoc_activity_sample_weight: 1,
@@ -25,11 +30,13 @@ class FeatureModeManager
         }
     },
     scale: {
-      gatekeeper_settings: {
+      gatekeeper_general_settings: {
+          puzzle_rating: false,
+          hint_view_request: false
+      },
+      gatekeeper_hoc_tutorial_settings: {
         postMilestone: false,
         shareEnabled: true,
-        puzzle_rating: false,
-        hint_view_request: false
       },
       dcdo_settings: {
         hoc_activity_sample_weight: 10,
@@ -38,11 +45,13 @@ class FeatureModeManager
       }
     },
     emergency: {
-      gatekeeper_settings: {
+      gatekeeper_general_settings: {
+          puzzle_rating: false,
+          hint_view_request: false
+      },
+      gatekeeper_hoc_tutorial_settings: {
         postMilestone: false,
         shareEnabled: false,
-        puzzle_rating: false,
-        hint_view_request: false
       },
       dcdo_settings: {
         hoc_activity_sample_weight: 10,
@@ -63,7 +72,11 @@ class FeatureModeManager
       dcdo.set(key, value)
     end
 
-    settings[:gatekeeper_settings].each do |feature, value|
+    settings[:gatekeeper_general_settings].each do |feature, value|
+      gatekeeper.set(feature,  value: value)
+    end
+
+    settings[:gatekeeper_hoc_tutorial_settings].each do |feature, value|
       script_names.each do |script_name|
         gatekeeper.set(feature,  where: {script_name: script_name}, value: value)
       end
@@ -75,7 +88,8 @@ class FeatureModeManager
   def self.get_mode(gatekeeper, dcdo, script_names)
     MODES.detect do |mode|
       dcdo_matches_mode?(dcdo, mode) &&
-        gatekeeper_matches_mode?(gatekeeper, mode, script_names)
+        gatekeeper_general_settings_match_mode?(gatekeeper, mode) &&
+        gatekeeper_hoc_tutorials_match_mode?(gatekeeper, mode, script_names)
     end
   end
 
@@ -86,8 +100,15 @@ class FeatureModeManager
     end
   end
 
-  def self.gatekeeper_matches_mode?(gatekeeper, mode, script_names)
-    expected_settings = MODE_SETTINGS_MAP[mode][:gatekeeper_settings]
+  def self.gatekeeper_general_settings_match_mode?(gatekeeper, mode)
+    expected_settings = MODE_SETTINGS_MAP[mode][:gatekeeper_general_settings]
+    expected_settings.all? do |feature, expected_value|
+      expected_value == gatekeeper.allows(feature)
+    end
+  end
+
+  def self.gatekeeper_hoc_tutorials_match_mode?(gatekeeper, mode, script_names)
+    expected_settings = MODE_SETTINGS_MAP[mode][:gatekeeper_hoc_tutorial_settings]
     expected_settings.all? do |feature, expected_value|
       script_names.all? do |script|
         expected_value == gatekeeper.allows(feature, where: {script_name: script})
