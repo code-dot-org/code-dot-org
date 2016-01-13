60966b3e55ea80b244ff734c478230bc103056b9
Phil Bogle <phil@code.org>
2016-01-12 16:22:05 -0800

    Revert change

--- dashboard/test/controllers/feature_mode_controller_test.rb
+++ dashboard/test/controllers/feature_mode_controller_test.rb
@@ -42,5 +42,15 @@ class FeatureModeControllerTest < ActionController::TestCase
       assert_response :success
       assert_select '#scale[checked="checked"]'
     end
+
+    # Advance time so that the pending change has expired, and verify that the show page goes
+    # back to showing the value returned by the FeatureModeManager rather than pending value in the
+    # session.
+    Timecop.travel Time.local(2013, 9, 1, 12, 1, 0) do
+      get :show
+      assert_response :success
+      assert_select '#normal[checked="checked"]'
+    end
   end
+
 end
