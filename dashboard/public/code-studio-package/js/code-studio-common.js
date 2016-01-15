f7b63edfe02dc139faf13c715a1cd52129f54f0a
Continuous Integration <dev@code.org>
2016-01-15 01:37:44 +0000

    Automatically built.
    91df568 Merge pull request #6340 from code-dot-org/revert-6338-revert-6337-revert-6323-revert-6321-revert-6290-move-react-to-code-studio (Brad Buchanan)
    2a26c88 Revert ""Move react to code studio" attempt #3" (Brad Buchanan)

--- dashboard/public/code-studio-package/js/code-studio-common.js
+++ dashboard/public/code-studio-package/js/code-studio-common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40d635bb72b0d2651fd53369e925021109dc0edd159a5074d80569965bdaa06a
-size 2321360
+oid sha256:fd89b115f5ac265a8dae3d8aa11cc8220099bcf287406d7a81c64dd03a69f73a
+size 2320087
---
+++ dashboard/public/code-studio-package/js/levels/widget.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bdf050a29c7ad0268f26aee32f4513885dad6afd8c4451529a23f386c0bfc311
+size 452
