438d02936b1aa583f3889489868a2c731bdac5ad
Josh Lory <josh.lory@code.org>
2016-01-15 11:07:24 -0800

    For scripts with trophies, fall back to the Haml partial

--- dashboard/app/assets/javascripts/progress.js
+++ dashboard/app/assets/javascripts/progress.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db87d632889a3cde2437d9290f3ac504a269c2de8f9c22e9e44f88fddb5820cf
-size 3776
+oid sha256:84bc0db96c18b3c56605ca6acad35da3e68cc33e13e969880d150bcffe2daaf9
+size 3772
