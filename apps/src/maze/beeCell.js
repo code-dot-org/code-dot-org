e8ac9799d5232ddee11df29c96da3f9963804853
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-01-15 15:21:49 -0800

    First draft of randomized conditionals
    Adds a set of new syntax to Bee levels for randomized conditional cells;
    that is, cells that can contain one of a number of kinds of content.
    This will let the cells hidden by clouds be a true mystery.
    
    This also means that levels that use these cells will have a number of
    possible grids; if that's the case, we run the user's code against all
    possible grids. If their code succeeds on all, then we randomly select
    one of the grids to animate on top of. If their code fails on any, we
    randomly select from one of the failing grids to display.

---
+++ apps/src/maze/beeCell.js
@@ -0,0 +1,125 @@
+// FC is short for FlowerComb, which we were originally using instead of cloud
+var CLOUD_MARKER = 'FC';
+
+var Cell = function (value, clouded, prefix, color) {
+  this.clouded_ = clouded;
+  this.prefix_ = prefix;
+  this.color_ = color;
+
+  this.originalValue_ = value;
+  this.currentValue_ = undefined;
+  this.resetCurrentValue();
+};
+
+module.exports = Cell;
+
+Cell.prototype.clone = function () {
+  var newCell = new Cell(this.originalValue_, this.clouded_, this.prefix_, this.color_);
+  newCell.setCurrentValue(this.currentValue_);
+  return newCell;
+};
+
+Cell.prototype.getCurrentValue = function () {
+  return this.currentValue_;
+};
+
+Cell.prototype.setCurrentValue = function (val) {
+  this.currentValue_ = val;
+};
+
+Cell.prototype.resetCurrentValue = function () {
+  if (this.prefix_) {
+    this.currentValue_ = parseInt(this.prefix_ + this.originalValue_);
+  } else {
+    this.currentValue_ = 0;
+  }
+};
+
+Cell.prototype.isFlower = function () {
+  return this.prefix_ === '+';
+};
+
+Cell.prototype.isHive = function () {
+  return this.prefix_ === '-';
+};
+
+Cell.prototype.isClouded = function () {
+  return this.clouded_ === CLOUD_MARKER;
+};
+
+Cell.prototype.getColor = function () {
+  return this.color_;
+};
+
+Cell.prototype.isVariable = function () {
+  // Valid values for clouded include 'FC', which is an old-style,
+  // static cloud. C and Cany represent new, variable clouds
+  return (this.clouded_ === 'C' || this.clouded_ === 'Cany');
+};
+
+Cell.prototype.getPossibleGridAssets = function () {
+  // Possible values are:
+  //   Flower or nothing: +nC
+  //   Honeycomb or nothing: -nC
+  //   Flower or Honeycomb: nC
+  //   Flower, Honeycomb, or Nothing: nCany
+
+  if (this.isVariable()) {
+    var thisCanBeA = {
+      flower: true,
+      honeycomb: true,
+      nothing: false,
+    };
+    if (this.clouded_ === 'Cany') {
+      thisCanBeA.nothing = true;
+    } else if (this.prefix_) {
+      thisCanBeA.nothing = true;
+      thisCanBeA.flower = this.isFlower();
+      thisCanBeA.honeycomb = this.isHive();
+    }
+
+    var possibilities = [];
+    if (thisCanBeA.flower) {
+      possibilities.push(new Cell(this.originalValue_, CLOUD_MARKER, '+', this.color_));
+    }
+    if (thisCanBeA.honeycomb) {
+      possibilities.push(new Cell(this.originalValue_, CLOUD_MARKER, '-'));
+    }
+    if (thisCanBeA.nothing) {
+      possibilities.push(new Cell(0, CLOUD_MARKER));
+    }
+    return possibilities;
+  }
+
+  return [this];
+};
+
+
+Cell.parse = function (string) {
+  var matches = string.match && string.match(/^(\+|-)?(\d+)(R|P)?(FC|C|Cany)?$/);
+  var value, clouded, prefix, color;
+  if (matches) {
+    prefix = matches[1];
+    value = parseInt(matches[2]);
+    color = matches[3];
+    clouded = matches[4];
+  } else {
+    value = string;
+  }
+
+  // TODO: when we add numeric ranges, this becomes:
+  /*
+  var matches = cell.match && cell.match(/^(\+|-)?(\d+)(,\d+)?(R|P)?(FC|C|Cany)?$/);
+  return {
+    prefix: matches[1],
+    value: matches[2],
+    range: matches[3],
+    color: matches[4],
+    clouded: matches[5],
+  };
+  */
+
+  return new Cell(value, clouded, prefix, color);
+};
+
+
