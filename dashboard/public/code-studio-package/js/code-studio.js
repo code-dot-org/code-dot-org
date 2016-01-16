434d46c6b453af6a19aacade3148850f867212bf
Continuous Integration <dev@code.org>
2016-01-16 00:20:49 +0000

    Automatically built.
    e3e44f2 Merge pull request #6354 from code-dot-org/revert-6350-code-studio-js-watchify (Brad Buchanan)
    a032dcf Revert "code-studio JS watchify" (Brad Buchanan)

--- dashboard/public/code-studio-package/js/code-studio.js
+++ dashboard/public/code-studio-package/js/code-studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a37e2c791a9390d2c14d278b2caa397ce73f41e1cc3d9dd57f074c9f64c67ad8
-size 214270
+oid sha256:17e3281cd55a842349a7ae25b32ddd2a7a571f6b2501ecc064dcd54ec4a5044f
+size 1895311
