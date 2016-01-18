60c48c6286a9c59d7ded2fd2ed48aff6982ed932
Laurel Fan <laurel@code.org>
2016-01-18 15:50:09 -0800

    update omniauth-clever and its dependencies to use latest clever api version

--- dashboard/test/controllers/omniauth_callbacks_controller_test.rb
+++ dashboard/test/controllers/omniauth_callbacks_controller_test.rb
@@ -69,6 +69,30 @@ class OmniauthCallbacksControllerTest < ActionController::TestCase
     assert_equal nil, user.gender
   end
 
+  test "authorizing with unknown clever teacher account needs additional information" do
+    auth = stub('auth',
+                uid: '1111',
+                provider: 'clever',
+                info: stub('info', nickname: '',
+                           name: {'first' => 'Hat', 'last' => 'Cat'},
+                           email: nil,
+                           user_type: 'teacher',
+                           dob: nil,
+                           gender: nil))
+
+    @request.env['omniauth.auth'] = auth
+    @request.env['omniauth.params'] = {}
+
+    assert_does_not_create(User) do
+      get :clever
+    end
+
+    assert_redirected_to 'http://test.host/users/sign_up'
+    attributes =  session['devise.user_attributes']
+
+    assert_equal nil, attributes['email']
+  end
+
   test "authorizing with unknown clever student account" do
     auth = stub('auth',
                 uid: '111133',
@@ -92,7 +116,6 @@ class OmniauthCallbacksControllerTest < ActionController::TestCase
     assert_equal User::TYPE_STUDENT, user.user_type
     assert_equal 10, user.age
     assert_equal 'f', user.gender
-
   end
 
 end
