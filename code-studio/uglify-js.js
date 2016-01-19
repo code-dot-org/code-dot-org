10f8032ec5c5fd9e6c0dbb0867e1d70c0ec7eb6e
Brad Buchanan <brad@code.org>
2016-01-19 14:32:43 -0800

    Add uglify step and build:dist task to code-studio

---
+++ code-studio/uglify-js.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+/** @file Uglify previously-built JS assets in the code-studio package, which is
+ *        loaded by dashboard (our "Code Studio" Rails app). */
+'use strict';
+
+var build_commands = require('./build-commands');
+var fs = require('fs');
+var recursiveReaddirSync = require('recursive-readdir-sync');
+
+/** @const {string} */
+var BUILD_PATH = './build/js/';
+
+// Get the list of files in the build directory
+var builtFiles = recursiveReaddirSync(BUILD_PATH).filter(function (path) {
+  return /\.js$/i.test(path);
+});
+
+var uglifyCommands = builtFiles.map(function (path) {
+  return [
+    'uglifyjs ' + path,
+    '--compress warnings=false',
+    '--mangle',
+    '--output ' + path
+  ].join(' \\\n    ');
+});
+
+// Run build (exits on failure)
+build_commands.execute(uglifyCommands);
+build_commands.logBoxedMessage("code-studio js uglified");
