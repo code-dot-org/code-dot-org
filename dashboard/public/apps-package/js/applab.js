3de8d89e47799258c206209839fd851122876acc
Continuous Integration <dev@code.org>
2016-01-20 19:51:07 +0000

    Automatically built.
    61e658c Merge pull request #6395 from code-dot-org/pegasus_test_fix (Will Jordan)
    5ece068 fixes for pegasus tests (Will Jordan)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:087f189ffe6c566e1ac0255ca0d962c35c9d03bbe42a604f769dd1e09e6d89ea
-size 2247540
+oid sha256:d5327509c6174fa648e878b5af45533af3443ebd4d37bbd036380bf25c07f3b9
+size 2238056
--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8b926c5416b920dc277f2f0e082ba84c518d75aacabbde23839de1bb199c318
-size 4060179
+oid sha256:d8b523de795e130ec2095d3fca103fda2b4a750a3263e7e2e6e2f2e7021e2718
+size 4078244
