a020cd774ac059c8a3a0e4dc25cf465473728b71
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-01-21 14:22:38 -0800

    typo fix

--- apps/src/templates/Hint.jsx
+++ apps/src/templates/Hint.jsx
@@ -1,7 +1,7 @@
 module.exports = React.createClass({
 
   propTypes: {
-    hint: React.PropTypes.objectisRequired
+    hint: React.PropTypes.object.isRequired
   },
 
   renderBlocklyHint: function () {
