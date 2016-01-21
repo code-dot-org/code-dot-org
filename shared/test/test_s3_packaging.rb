8ffeab9de1e75e924b3028eefac20f204de32fd3
Brent Van Minnen <bjvanminnen@gmail.com>
2016-01-20 16:38:01 -0800

    some more tests

--- shared/test/test_s3_packaging.rb
+++ shared/test/test_s3_packaging.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'rack/test'
+require 'securerandom'
 
 require_relative '../../lib/cdo/aws/s3_packaging'
 require_relative '../../deployment'
@@ -47,6 +48,15 @@ class S3PackagingTest < Minitest::Test
     assert_equal s3_key, 'test-package/fake-hash.tar.gz'
   end
 
+  def test_target_commit_hash
+    # starts out as nil, since we don't have a commit_hash file
+    assert @packager.send(:target_commit_hash).nil?
+
+    IO.write(@target_location + '/commit_hash', 'manual-hash')
+    assert_equal @packager.send(:target_commit_hash), 'manual-hash'
+    FileUtils.rm(@target_location + '/commit_hash')
+  end
+
   def test_create_and_decompress_package
     package = @packager.send(:create_package, 'build')
     assert package.is_a?(Tempfile)
@@ -67,25 +77,99 @@ class S3PackagingTest < Minitest::Test
 
     @packager.send(:upload_package, package)
 
+    # TODO - we could have a race condition with two people running this test at once
+    # that might just go away once this is all VCRed
     downloaded = @packager.send(:download_package)
     assert FileUtils.compare_file(package, downloaded)
   end
 
   def test_download_nonexistent
-    @packager.instance_variable_set(:@commit_hash, 'fake-nonexistent-hash')
-    threw = false
-    begin
-      @packager.send(:download_package)
-    rescue Aws::S3::Errors::NoSuchKey
-      threw = true
-    end
-    assert threw
+    alt_source_loc, alt_target_loc, alt_packager = create_packager('fake-nonexistent-hash')
 
-    @packager.instance_variable_set(:@commit_hash, ORIGINAL_HASH)
+    begin
+      threw = false
+      begin
+        alt_packager.send(:download_package)
+      rescue Aws::S3::Errors::NoSuchKey
+        threw = true
+      end
+      assert threw
+    ensure
+      cleanup_packager(alt_source_loc, alt_target_loc)
+    end
   end
 
   def test_ensure_updated_package
+    alt_source_loc, alt_target_loc, alt_packager = create_packager('alternate-hash')
+    begin
+      # upload package for ORIGINAL_HASH
+      original_package = @packager.send(:create_package, 'build')
+      @packager.send(:upload_package, original_package)
 
+      # upload package for "alternate-hash"
+      alt_package = alt_packager.send(:create_package, 'build')
+      alt_packager.send(:upload_package, alt_package)
+
+      # we have no package, so we download one
+      FileUtils.rm_rf(@target_location + '/*')
+      assert @packager.send(:target_commit_hash).nil?
+      @packager.send(:ensure_updated_package)
+      assert_equal @packager.send(:target_commit_hash), ORIGINAL_HASH
+
+      # trash output.js, but keep our commit_hash.
+      FileUtils.rm_rf(@target_location + '/output.js')
+      @packager.send(:ensure_updated_package)
+      # we shouldn't have redownloaded output, because commit_hash still claims we're up to date
+      assert_equal @packager.send(:target_commit_hash), ORIGINAL_HASH
+      assert !File.exist?(@target_location + '/output.js')
+
+      # if we have the wrong package, we download
+      FileUtils.cp(@target_location + '/commit_hash', alt_target_loc)
+      assert_equal alt_packager.send(:target_commit_hash), ORIGINAL_HASH
+      alt_packager.send(:ensure_updated_package)
+      assert_equal alt_packager.send(:target_commit_hash), 'alternate-hash'
+      assert File.exist?(alt_target_loc + '/output.js')
+
+      # if there is no package to download, we throw
+      cleanup_packager(alt_source_loc, alt_target_loc)
+      alt_source_loc, alt_target_loc, alt_packager = create_packager('fake-nonexistent-hash')
+      threw = false
+      begin
+        alt_packager.send(:ensure_updated_package)
+      rescue Aws::S3::Errors::NoSuchKey
+        threw = true
+      end
+      assert threw
+    ensure
+      cleanup_packager(alt_source_loc, alt_target_loc)
+    end
+  end
+
+  def test_upload_as_package
+    # create a random hash so that we dont have to worry about colliding with
+    # others on s3
+    random_hash = SecureRandom.hex
+    alt_source_loc, alt_target_loc, alt_packager = create_packager(random_hash)
+
+    begin
+      # upload a package
+      assert alt_packager.upload_as_package('/build')
+
+      # upload the same package again, it works
+      assert alt_packager.upload_as_package('/build')
+
+      # try uploading a different package under the same name, it fails
+      threw = false
+      begin
+        assert !alt_packager.upload_as_package('/src')
+      rescue
+        threw = true
+      end
+      assert threw
+
+    ensure
+      cleanup_packager(alt_source_loc, alt_target_loc)
+    end
   end
 
 end
