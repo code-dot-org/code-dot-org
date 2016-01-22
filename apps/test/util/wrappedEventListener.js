ecd207eb01a5167e1caef36deaa02433a05b5170
David Bailey <davidsbailey@users.noreply.github.com>
2016-01-21 18:30:07 -0800

    extract isNativeCode

--- apps/test/util/wrappedEventListener.js
+++ apps/test/util/wrappedEventListener.js
@@ -1,3 +1,7 @@
+function isNativeCode(func) {
+  return func && func.toString().indexOf('[native code]') > -1;
+}
+
 /**
  * Wraps window.addEventListener to catch all added listeners, allowing us to
  * dispose of them when our test finishes
@@ -7,7 +11,7 @@ module.exports = {
    * Attach our wrapper, tracking all added listeners
    */
   attach: function () {
-    if (window.addEventListener.toString().indexOf('[native code]') === -1) {
+    if (!isNativeCode(window.addEventListener)) {
       throw new Error('addEventListener already wrapped');
     }
     if (window.originalAddEventListener) {
@@ -30,9 +34,8 @@ module.exports = {
    * Detach our wrapper, removing all added listeners
    */
   detach: function () {
-    if (!window.originalAddEventListener ||
-        window.originalAddEventListener.toString().indexOf('[native code]') === -1) {
-      throw new Error('window.originalAddEventListener is not a native function');
+    if (!isNativeCode(window.originalAddEventListener)) {
+      throw new Error('originalAddEventListener is not a native function');
     }
     window.addEventListener = window.originalAddEventListener;
     window.originalAddEventListener = null;
