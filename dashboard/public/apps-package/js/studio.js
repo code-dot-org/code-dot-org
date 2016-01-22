7e72aa54d0fad26256bd5b8b8b4c867927934cb8
Continuous Integration <dev@code.org>
2016-01-22 21:36:26 +0000

    Automatically built.
    8d58a2b Revert "Merge pull request #6377 from code-dot-org/gamelab_p5play" (Andrew Oberhardt)

--- dashboard/public/apps-package/js/studio.js
+++ dashboard/public/apps-package/js/studio.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f03cf3cbfd9dd0961e9f38fbe4d2e550a04b30fd4e52fba204256392f6cbcade
-size 2090671
+oid sha256:6cf318c2ca50fc112a60893905159ba010232eeeca0ad122a6589c621ff189b4
+size 2090433
