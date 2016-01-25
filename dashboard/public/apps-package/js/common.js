f52c13db94e5f365bf4db117766d0c33bdb0ce85
Continuous Integration <dev@code.org>
2016-01-25 22:40:19 +0000

    Automatically built.
    d9a84cf Merge pull request #6442 from code-dot-org/gamelab_changes (Chris Pirich)
    245e9df staging content changes (-brendan) (Continuous Integration)
    bca9ac0 Merge pull request #6468 from code-dot-org/adminize_and_hide_sec (Laurel)
    d59926f remove newline (Laurel)
    4a4b317 adminize and hide secrets in /admin/debug (Laurel Fan)
    f545cba Merge pull request #6428 from code-dot-org/auto-hooks (Brad Buchanan)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57f5b5ea93ab2ba27acec7e2bb67276123f1aad4ca712c12fe8b7534d466a6c2
-size 4415149
+oid sha256:27dd2fae84fc664331e5a2890067233842ed1f44f6cd1584542764383f6d4680
+size 4424885
