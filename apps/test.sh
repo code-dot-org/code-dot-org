c85659ab85bdc8439015e270aaf06ec6b0bf3a6c
Will Jordan <will@code.org>
2016-01-25 16:56:02 -0800

    Skip /apps tests if files are unchanged compared to staging.

--- apps/test.sh
+++ apps/test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Skip tests if the current branch's diff-tree compared to its staging merge-base doesn't touch any files in /apps.
+# Always run tests if the current branch is 'staging'.
 
-for i in 1 2; do grunt test && break; done
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+BRANCH_COMMIT=$(git merge-base HEAD refs/remotes/origin/staging)
+if [ ${BRANCH} == 'staging' ] || git diff-tree HEAD ${BRANCH_COMMIT} --name-only | grep -q "apps$"; then
+  for i in 1 2; do grunt test && break; done
+fi
