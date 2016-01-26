8a29dd4a85336b9bdaa03f806993530673c77411
Josh Lory <josh.lory@code.org>
2016-01-25 16:37:23 -0800

    Unify level.link || level.url

--- code-studio/src/js/components/progress/stage_progress_type.js
+++ code-studio/src/js/components/progress/stage_progress_type.js
@@ -4,6 +4,6 @@ module.exports = React.PropTypes.arrayOf(React.PropTypes.shape({
   title: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
   status: React.PropTypes.string,
   kind: React.PropTypes.oneOf(['unplugged', 'assessment', 'puzzle']),
-  link: React.PropTypes.string,
+  url: React.PropTypes.string,
   id: React.PropTypes.number
 }));
--- dashboard/app/assets/javascripts/progress.js
+++ dashboard/app/assets/javascripts/progress.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c4cbef8192de5c2260c15b937dfb967ce59ef69eca30de5fc1d6194c50ce9f8
-size 3915
+oid sha256:6f3bfaf0f435ade15f88ff981ea8e47ec0552862260a45da1d46d90fe3dc7daa
+size 3914
