583011c6680f6a3412e5f68380e301e1c57565ce
Continuous Integration <dev@code.org>
2016-01-27 17:21:32 +0000

    Automatically built.
    3c9c18d Merge pull request #6466 from code-dot-org/revertRevertDocFunc (Brent Van Minnen)
    6b7fe35 Merge pull request #6499 from code-dot-org/add_gamelab_model_only (Chris Pirich)
    9fcd3e0 Merge pull request #6503 from code-dot-org/contributing-email (Tanya Parker)
    38798d1 update with contributing@code.org mailing group (Tanya Parker)
    9055824 Merge pull request #6501 from code-dot-org/removeCodeStudioPackage (Brent Van Minnen)
    8ffc08a remove code-studio-package (Brent Van Minnen)

--- dashboard/public/apps-package/js/lv_lv/common_locale.js
+++ dashboard/public/apps-package/js/lv_lv/common_locale.js
@@ -403,4 +403,5 @@ s:function(d,k,p){common_locale.c(d,k);return d[k] in p?p[d[k]]:p.other}};
 "when":function(d){return "kad"},
 "whenRun":function(d){return "kad izpilda"},
 "workspaceHeaderShort":function(d){return "Darba virsma: "},
+"dropletBlock_randomNumber_description":function(d){return "Returns a random number in the closed range from min to max."},
 "recommendedBlockContextualHintTitle":function(d){return "Try using a block like this to solve the puzzle."}};
