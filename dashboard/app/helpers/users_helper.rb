cf2eda26df1572d95e656d8a6d8b90993f8e9c91
Phil Bogle <phil@code.org>
2016-01-26 17:33:07 -0800

    Add API to get progress for all tutorials

--- dashboard/app/helpers/users_helper.rb
+++ dashboard/app/helpers/users_helper.rb
@@ -1,18 +1,43 @@
 module UsersHelper
   include ApplicationHelper
 
-  # Summarize the current user's progress within a certain script.
+  # Summarize a user and his or progress progress within a certain script.
   def summarize_user_progress(script, user = current_user, exclude_level_progress = false)
-    user_data = {}
-    uls = {}
-    script_levels = script.script_levels
+    user_summary(user).merge(script_progress(user, script, exclude_level_progress))
+  end
+
+  # Summarize a user and his or her progress across all scripts.
+  def summarize_user_progress_for_all_scripts(user)
+    user_summary(user).merge(scripts: scripts_progress(user))
+  end
+
+  private def scripts_progress(user)
+    progress_entries = UserLevel.where(user_id: user.id).map do |ul|
+      script_id = ul.script_id
+      script = Script.get_from_cache(script_id)
+      progress = script_progress(user, script)
+      [script_id, { name: script.name, progress: progress }]
+    end
+    Hash[progress_entries]
+  end
+
+  private def user_summary(user)
+    result = {}
     if user
-      lines = user.total_lines
+      result[:disableSocialShare] = true if user.under_13?
+      result[:isTeacher] = true if user.teacher?
+      result[:linesOfCode] = user.total_lines
+    else
+      result[:linesOfCode] = client_state.lines
+    end
+    result[:linesOfCodeText] = I18n.t('nav.popup.lines', lines: result[:linesOfCode])
+    result
+  end
 
-      uls = user.user_levels_by_level(script)
-      user_data[:disableSocialShare] = true if user.under_13?
-      user_data[:isTeacher] = true if user.teacher?
-
+  private def script_progress(user, script, exclude_level_progress = false)
+    user_data = {}
+    if user
+      # Populate trophies data if the script has trophies.
       if script.trophies
         progress = user.progress(script)
         user_data[:trophies] = {
@@ -25,25 +50,19 @@ module UsersHelper
           user_data[:trophies][concept.name] = counts[:current].to_f / counts[:max]
         end
       end
-    else
-      lines = client_state.lines
-      script_levels = script.script_levels
     end
 
-    user_data.merge!(
-        linesOfCode: lines,
-        linesOfCodeText: I18n.t('nav.popup.lines', lines: lines),
-    )
-
     unless exclude_level_progress
+      uls = user.user_levels_by_level(script)
+      script_levels = script.script_levels
       user_data[:levels] = {}
       script_levels.each do |sl|
         result = level_info(user, sl, uls)
         completion_status = activity_css_class result
         if completion_status != 'not_tried'
           user_data[:levels][sl.level_id] = {
-            status: completion_status,
-            result: result
+              status: completion_status,
+              result: result
           }
         end
       end
