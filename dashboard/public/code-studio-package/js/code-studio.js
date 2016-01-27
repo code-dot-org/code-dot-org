cb2b5fe6ffadb8416048b573ca74926a90d28738
Brent Van Minnen <bjvanminnen@gmail.com>
2016-01-26 16:19:36 -0800

    remove code-studio-package

--- dashboard/public/code-studio-package/js/code-studio.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:32fdfe025ce389fc1a68adfe301d6368a86526128df6ac1b4e842e490e582365
-size 273416
--- dashboard/public/code-studio-package/js/levelbuilder_studio.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5a31d8ab3edf7cf6bed1ce7ea7fe240583d0d83b1366df9b0cd105e00108d2f0
-size 815
