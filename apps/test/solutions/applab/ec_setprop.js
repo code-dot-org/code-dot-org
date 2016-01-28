395b3015dca78c8643c1bba61577e2983a991950
Brent Van Minnen <bjvanminnen@gmail.com>
2016-01-28 14:48:28 -0800

    more tests

--- apps/test/solutions/applab/ec_setprop.js
+++ apps/test/solutions/applab/ec_setprop.js
@@ -14,7 +14,7 @@ module.exports = {
   levelId: "ec_simple",
   tests: [
     {
-      description: "setProperty on Image",
+      description: "setProperty on API created Image",
       editCode: true,
       xml:
         'image("my_image", "' + flappyImage + '");' +
@@ -23,8 +23,7 @@ module.exports = {
         'setProperty("my_image", "x", 13);' +
         'setProperty("my_image", "y", 14);' +
         'setProperty("my_image", "picture", "' + facebookImage + '");' +
-        'setProperty("my_image", "hidden", true);'
-      ,
+        'setProperty("my_image", "hidden", true);',
       runBeforeClick: function (assert) {
         // add a completion on timeout since this is a freeplay level
         testUtils.runOnAppTick(Applab, 2, function () {
@@ -60,12 +59,120 @@ module.exports = {
     },
 
     {
+      description: "setProperty on design mode created Image",
+      editCode: true,
+      levelHtml:
+        '<div xmlns="http://www.w3.org/1999/xhtml" id="designModeViz" class="appModern withCrosshair" style="width: 320px; height: 450px; display: none;"><div class="screen" tabindex="1" id="screen1" style="display: block; height: 450px; width: 320px; left: 0px; top: 0px; position: absolute; z-index: 0;"><img src="/blockly/media/1x1.gif" data-canonical-image-url="" id="my_image" style="height: 100px; width: 100px; position: absolute; left: 80px; top: 75px; margin: 0px;" /></div></div>',
+      xml:
+        'setProperty("my_image", "width", 11);' +
+        'setProperty("my_image", "height", 12);' +
+        'setProperty("my_image", "x", 13);' +
+        'setProperty("my_image", "y", 14);' +
+        'setProperty("my_image", "picture", "' + facebookImage + '");' +
+        'setProperty("my_image", "hidden", true);',
+      runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        testUtils.runOnAppTick(Applab, 2, function () {
+          var image = document.getElementById('my_image');
+
+          // we set style.width/height instead of the width/height attributes
+          assert.equal(image.style.width, '11px');
+          assert.equal(image.style.height, '12px');
+          assert.equal(image.getAttribute('width'), null);
+          assert.equal(image.getAttribute('height'), null);
+
+          assert.equal(image.style.left, '13px');
+          assert.equal(image.style.top, '14px');
+
+          assert(/facebook_purple.png$/.test(image.src));
+
+          // visibility is set via a class, so use getComputedStyle
+          assert(window.getComputedStyle(image).visibility, 'hidden');
+
+          Applab.onPuzzleComplete();
+        });
+      },
+      customValidator: function (assert) {
+        // No errors in output console
+        var debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, "");
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      }
+    },
+
+    {
+      description: "setProperty on API created dropdown",
+      editCode: true,
+      xml:
+        'dropdown("my_drop", "option1", "option2");' +
+        'setProperty("my_drop", "options", ["one", "two", "three"]);',
+      runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        testUtils.runOnAppTick(Applab, 2, function () {
+          var dropdown = $("#my_drop");
+
+          assert.equal(dropdown.children().length, 3);
+          assert.equal(dropdown.children().eq(0).text(), 'one');
+          assert.equal(dropdown.children().eq(1).text(), 'two');
+          assert.equal(dropdown.children().eq(2).text(), 'three');
+
+          Applab.onPuzzleComplete();
+        });
+      },
+      customValidator: function (assert) {
+        // No errors in output console
+        var debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, "");
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      }
+    },
+
+    {
+      description: "setProperty on design mode created dropdown",
+      editCode: true,
+      levelHtml:
+        '<div xmlns="http://www.w3.org/1999/xhtml" id="designModeViz" class="appModern withCrosshair" style="width: 320px; height: 450px;"><div class="screen" tabindex="1" id="screen1" style="display: block; height: 450px; width: 320px; left: 0px; top: 0px; position: absolute; z-index: 0;"><select id="my_drop" style="width: 200px; height: 30px; font-size: 14px; margin: 0px; color: rgb(255, 255, 255); position: absolute; left: 35px; top: 75px; background-color: rgb(26, 188, 156);"><option>Option 1</option><option>Option 2</option></select></div></div>',
+      xml:
+        'setProperty("my_drop", "options", ["one", "two", "three"]);',
+      runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        testUtils.runOnAppTick(Applab, 2, function () {
+          var dropdown = $("#my_drop");
+
+          assert.equal(dropdown.children().length, 3);
+          assert.equal(dropdown.children().eq(0).text(), 'one');
+          assert.equal(dropdown.children().eq(1).text(), 'two');
+          assert.equal(dropdown.children().eq(2).text(), 'three');
+
+          Applab.onPuzzleComplete();
+        });
+      },
+      customValidator: function (assert) {
+        // No errors in output console
+        var debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, "");
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      }
+    },
+
+    {
       description: "setProperty invalid prop",
       editCode: true,
       xml:
         'image("my_image", "' + flappyImage + '");' +
-        'setProperty("my_image", "cant_set_this", 11);'
-      ,
+        'setProperty("my_image", "cant_set_this", 11);',
       runBeforeClick: function (assert) {
         // add a completion on timeout since this is a freeplay level
         testUtils.runOnAppTick(Applab, 2, function () {
@@ -92,8 +199,7 @@ module.exports = {
       editCode: true,
       xml:
         'image("my_image", "' + flappyImage + '");' +
-        'setProperty("my_image", "hidden", "true");'
-      ,
+        'setProperty("my_image", "hidden", "true");',
       runBeforeClick: function (assert) {
         // add a completion on timeout since this is a freeplay level
         testUtils.runOnAppTick(Applab, 2, function () {
