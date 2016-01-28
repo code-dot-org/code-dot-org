b5b070b38174b833f817eeecb90688e691701462
Continuous Integration <dev@code.org>
2016-01-28 03:18:12 +0000

    Automatically built.
    806467b Merge pull request #6492 from code-dot-org/gamelab_palette (Chris Pirich)
    bff4fca Merge pull request #6527 from code-dot-org/add-cspunit4-to-LP-links (Sarah Filman)
    16f8dc1 Make cspunit4 script beta and support lesson plan links (Sarah Filman)
    c685ef6 Merge pull request #6519 from code-dot-org/enable_magic_retry__o (Laurel)
    f8461fd Merge pull request #6524 from code-dot-org/socialimage (Brendan Reville)
    fdf17d1 Merge pull request #6523 from code-dot-org/update-footer (Tanya Parker)
    02df30b Pegasus now has a default og:image for all non-csedweek.org domains. (Brendan Reville)

--- dashboard/public/apps-package/js/gamelab.js
+++ dashboard/public/apps-package/js/gamelab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a709bf54f4c2b0ae037118805c3ba3e2d482d7dc176d35cc99561a036e1b9a9
-size 103154
+oid sha256:064f9f8a17bdbf703703c969849a66f9a5cb72699d226ece55d4ef56c63b5611
+size 122372
