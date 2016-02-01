45c8d2d9048a568dfb10f9c50ae15fc326c40d5c
Brendan Reville <brendan@code.org>
2016-02-01 13:03:34 +1100

    Ethnicity & foodstamps stats now write to database.

--- dashboard/app/controllers/survey_results_controller.rb
+++ dashboard/app/controllers/survey_results_controller.rb
@@ -2,6 +2,21 @@ class SurveyResultsController < ApplicationController
 
   def create
     # puts params[:survey].to_json
+    @survey_result = SurveyResult.new(survey_result_params)
+
+    respond_to do |format|
+      if @survey_result.save
+        format.json { head :ok }
+      else
+        format.json { render json: @survey_result.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def survey_result_params
+    params.require(:survey).permit(:survey2016_ethnicity_indian, :survey2016_ethnicity_indian, :survey2016_ethnicity_asian, :survey2016_ethnicity_black, :survey2016_ethnicity_hispanic, :survey2016_ethnicity_hawaiian, :survey2016_ethnicity_white, :survey2016_foodstamps)
   end
 
 end
--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -6,7 +6,7 @@ module OPS
 end
 
 Dashboard::Application.routes.draw do
-  resources :survey_results, only: [:create]
+  resources :survey_results, only: [:create], defaults: { format: 'json' }
 
   def redirect_to_teacher_dashboard
     redirect CDO.code_org_url('/teacher-dashboard')
