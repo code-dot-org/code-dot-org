3db626ff7e60c3423660e918f52cf6c173df9da1
Continuous Integration <dev@code.org>
2016-02-02 20:10:46 +0000

    Automatically built.
    fdbc9ca Merge pull request #6605 from code-dot-org/designModeView (Brent Van Minnen)
    e73fe29 Merge pull request #6601 from code-dot-org/cronTheFunometerCron (ashercodeorg)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02cef82bfc8fb046558de4672b293c39a8eff0532231e956a391c10d98fb4e89
-size 2734187
+oid sha256:5d463f417060379ff7706211802f58a8c1ffffee9cf40a2bdfb12b2595773648
+size 2734366
