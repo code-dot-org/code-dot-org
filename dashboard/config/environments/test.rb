039b8146af20f7057ac83d6306c4a3de2e014c2e
Brent Van Minnen <Bjvanminnen@gmail.com>
2016-02-01 22:27:11 -0800

    Revert "[Finishes #112558401] make some assets related config on test more like prod"

--- dashboard/config/environments/test.rb
+++ dashboard/config/environments/test.rb
@@ -16,19 +16,6 @@ Dashboard::Application.configure do
   config.serve_static_files  = true
   config.static_cache_control = "public, max-age=3600, s-maxage=1800"
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # Generate digests for assets URLs.
-  config.assets.digest = true
-
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
