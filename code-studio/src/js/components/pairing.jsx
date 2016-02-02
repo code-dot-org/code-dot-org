92ed75d821f300f4b876218f0a0606a5da09dc76
Laurel Fan <laurel@code.org>
2016-02-02 13:43:41 -0800

    componentize

--- code-studio/src/js/components/pairing.jsx
+++ code-studio/src/js/components/pairing.jsx
@@ -2,6 +2,50 @@
  * A component for managing pair programming
  */
 var Pairing = function (React) {
+
+  var SectionSelector = React.createClass({
+    render: function() {
+      if (this.props.sections.length == 1) {
+        return null;
+      }
+
+      return (
+          <select name="sectionId" value={this.props.selectedSectionId} onChange={this.handleSectionChange}>
+          <option key="blank" value="">Choose your section</option>
+          {
+            this.props.sections.map(function (section) {
+              return <option key={section.id} value={section.id}>{section.name}</option>;
+            })
+          }
+        </select>
+      );
+    },
+  });
+
+  var StudentSelector = React.createClass({
+    render: function() {
+      if (!this.props.students || this.props.students.length == 0) {
+        return null;
+      }
+
+      var studentDivs = this.props.students.map(function (student) {
+        var className = "student";
+        //      if (this.state.selectedStudentIds[student.id]) {
+        //        className = "student selected";
+        //      }
+        return <div key={student.id} data-id={student.id} className={className} onClick={this.handleStudentClicked}>{student.name}</div>;
+      }.bind(this));
+
+      return (
+          <div>
+           {studentDivs}
+            <div className="clear"/>
+            <button onClick={this.handleSubmit}>Submit</button>
+          </div>
+      );
+    },
+  });
+
   var INPUT_WIDTH = 500;
   // dropdown width is wider so that it still lines up with inputs (which have
   // padding)
@@ -31,18 +75,9 @@ var Pairing = function (React) {
     },
 
     handleSectionChange: function(event) {
-      console.log(event);
-
-      var section = this.props.sections.find(function (s) {
-        return (s.id == event.target.value);
-      });
-
-      if (section) {
-        this.setState({sectionId: section.id,
-                       studentsInSection: section.students,
-                       selectedStudentIds: {}
-                      });
-      }
+      this.setState({sectionId: event.target.value,
+                     selectedStudentIds: {}
+                    });
     },
 
     handleStudentClicked: function(event) {
@@ -61,43 +96,40 @@ var Pairing = function (React) {
       event.preventDefault();
     },
 
-    render: function () {
-      var sectionSelector =
-        <select name="sectionId" value={this.state.sectionId} onChange={this.handleSectionChange}>
-        <option key="blank" value="">Choose your section</option>
-        {
-          this.props.sections.map(function (section) {
-            return <option key={section.id} value={section.id}>{section.name}</option>;
-          })
-        }
-        </select>;
-
-      var studentSelector = "";
-
-      if (this.state.sectionId) {
-        studentSelector =
-          this.state.studentsInSection.map(function (student) {
-            var className = "student";
-            if (this.state.selectedStudentIds[student.id]) {
-              className = "student selected";
-            }
-            return <div key={student.id} data-id={student.id} className={className} onClick={this.handleStudentClicked}>{student.name}</div>;
-          }.bind(this));
+    selectedSectionId: function() {
+      if (this.props.sections.length == 1) {
+        return this.props.sections[0].id;
+      } else {
+        return this.state.selectedSectionId;
       }
+    },
 
+    studentsInSection: function() {
+      if (this.selectedSectionId()) {
+        var section = this.props.sections.find(function (s) {
+          return true;
+//          return (s.id == this.selectedSectionId());
+        });
+        if (section) {
+          return section.students;
+        } else {
+          return null;
+        }
+      } else {
+        return null;
+      }
+    },
+
+    render: function () {
       return (
         <div style={{width: DROPDOWN_WIDTH}}>
           <h2>Pair programming</h2>
           <br/>
           <form>
           <input type="hidden" name="authenticity_token" value={this.props.csrfToken}/>
-          {sectionSelector}
+          <SectionSelector sections={this.props.sections} selectedSectionId={this.selectedSectionId()}/>
           <div className="clear"/>
-          {studentSelector}
-          <div className="clear"/>
-          <button onClick={this.handleSubmit}>
-            Submit
-          </button>
+          <StudentSelector students={this.studentsInSection()}/>
           </form>
         </div>
       );
