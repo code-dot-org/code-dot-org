26869782503c6292f3b1c54fa1d4b11c1d7a90a9
Continuous Integration <dev@code.org>
2016-02-02 21:44:46 +0000

    Automatically built.
    22931c3 Merge pull request #6610 from code-dot-org/responsive-design-workspace-description (Josh Lory)
    1bb6037 Merge pull request #6552 from code-dot-org/lscache (philbogle)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cd49ebee942febc356ab9db5be83a080422b24f2e6b1df672c078901271c824
-size 2743956
+oid sha256:917f3286e3046b812b7bb7715366a227cefcae9061d547d7c68968b68f42f22c
+size 2744735
