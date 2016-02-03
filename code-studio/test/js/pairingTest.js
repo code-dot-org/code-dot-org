39045dc3d347cecece8426bf1af9db3d947a5ceb
Laurel Fan <laurel@code.org>
2016-02-02 16:27:30 -0800

    ooh, testing clicks

--- code-studio/test/js/pairingTest.js
+++ code-studio/test/js/pairingTest.js
@@ -26,7 +26,7 @@ describe('Pairing component', function(){
     var props = {
       sections: [{id: 1, name: "A section", students: [{id: 11, name: "First student"}, {id: 12, name: "Second Student"}]},
                  {id: 15, name: "Anotther section"}],
-      pairings: [{id: 123, name: 'a student'}]
+      pairings: []
     };
 
     render(props);
@@ -59,6 +59,44 @@ describe('Pairing component', function(){
     assert.equal(2, TestUtils.scryRenderedDOMComponentsWithClass(component, 'student').length);
     // no selected students
     assert.equal(0, TestUtils.scryRenderedDOMComponentsWithClass(component, 'selected').length);
+  });
+
+  it('should selected a student when clicking on it', function() {
+    var props = {
+      sections: [{id: 1, name: "A section", students: [{id: 11, name: "First student"}, {id: 12, name: "Second Student"}]}],
+      pairings: []
+    };
+
+    render(props);
+
+    // 2 students
+    assert.equal(2, TestUtils.scryRenderedDOMComponentsWithClass(component, 'student').length);
+    // no selected students
+    assert.equal(0, TestUtils.scryRenderedDOMComponentsWithClass(component, 'selected').length);
+
+    // click on first student to select
+    TestUtils.Simulate.click(TestUtils.scryRenderedDOMComponentsWithClass(component, 'student')[0]);
+
+    // 2 students
+    assert.equal(2, TestUtils.scryRenderedDOMComponentsWithClass(component, 'student').length);
+    // 1 selected student
+    assert.equal(1, TestUtils.scryRenderedDOMComponentsWithClass(component, 'selected').length);
+
+    // click on second student to select
+    TestUtils.Simulate.click(TestUtils.scryRenderedDOMComponentsWithClass(component, 'student')[1]);
+
+    // 2 students
+    assert.equal(2, TestUtils.scryRenderedDOMComponentsWithClass(component, 'student').length);
+    // 2 selected students
+    assert.equal(2, TestUtils.scryRenderedDOMComponentsWithClass(component, 'selected').length);
+
+    // click on first student again to unselect
+    TestUtils.Simulate.click(TestUtils.scryRenderedDOMComponentsWithClass(component, 'student')[1]);
+
+    // 2 students
+    assert.equal(2, TestUtils.scryRenderedDOMComponentsWithClass(component, 'student').length);
+    // 1 selected student
+    assert.equal(1, TestUtils.scryRenderedDOMComponentsWithClass(component, 'selected').length);
   });
 
 });
