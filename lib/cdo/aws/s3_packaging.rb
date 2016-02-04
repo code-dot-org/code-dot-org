1bb84aee0f45b4eb3afb22688134ea9b2c05feb1
Will Jordan <will@code.org>
2016-02-04 14:59:42 -0800

    update download_package to always use public URL for download.

--- lib/cdo/aws/s3_packaging.rb
+++ lib/cdo/aws/s3_packaging.rb
@@ -142,21 +142,19 @@ class S3Packaging
     diff.empty?
   end
 
-  # Downloads package from S3, throws error if given package doesn't exist on s3
+  # Downloads package from S3 using public URL.
+  # Throws a NoSuchKey error if given package doesn't exist on s3, or if the object is private.
   # @return tempfile for the downloaded package
   private def download_package
     package = Tempfile.new(@commit_hash)
 
     @logger.info "Attempting to download: #{s3_key}\nto #{package.path}"
+    url = Aws::S3::Bucket.new(BUCKET_NAME).object(s3_key).public_url
     File.open(package, 'wb') do |file|
       begin
-        @client.get_object(bucket: BUCKET_NAME, key: s3_key) do |chunk|
-          file.write(chunk)
-        end
-      rescue Aws::Errors::MissingCredentialsError, Aws::S3::Errors::ServiceError
-        # Fallback to public URL download if credentials are not provided or invalid.
-        url = Aws::S3::Bucket.new(BUCKET_NAME).object(s3_key).public_url
         IO.copy_stream open(url), file
+      rescue OpenURI::HTTPError
+        raise Aws::S3::Errors::NoSuchKey.new(nil, file)
       end
     end
     @logger.info "Downloaded"
