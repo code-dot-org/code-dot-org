4190847144758c6c8527ca91112374d18acec18c
Mehal Shah <mehal@code.org>
2016-02-04 11:13:00 -0800

    Removing migrations that weren't needed, removing unnecessary column

--- dashboard/db/migrate/20160202225240_add_module_enrollment_to_artifact_assignment.rb
+++
@@ -1,5 +0,0 @@
-class AddModuleEnrollmentToArtifactAssignment < ActiveRecord::Migration
-  def change
-    add_reference :artifact_assignments, :user_enrollment_module_assignment, foreign_key: true
-  end
-end
