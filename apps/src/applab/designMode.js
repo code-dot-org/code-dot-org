9aa1fc4e6c1dc766d75b280663d013476a4d9324
David Bailey <davidsbailey@users.noreply.github.com>
2016-02-04 10:08:21 -0800

    truncate long log messages

--- apps/src/applab/designMode.js
+++ apps/src/applab/designMode.js
@@ -508,9 +508,9 @@ designMode.parseFromLevelHtml = function(rootEl, allowDragging, prefix) {
       "\noriginal html:\n" + unsafe + "\nmodified html:\n" + safe;
     console.log(msg);
     logToCloud.addPageAction(logToCloud.PageAction.SanitizedLevelHtml, {
-      removedHtml: removed,
-      unsafeHtml: unsafe,
-      safeHtml: safe
+      removedHtml: removed.substring(0, logToCloud.MAX_LENGTH),
+      unsafeHtml: unsafe.substring(0, logToCloud.MAX_LENGTH),
+      safeHtml: safe.substring(0, logToCloud.MAX_LENGTH)
     });
   }
   sanitizeHtml(Applab.levelHtml, reportUnsafeHtml);
