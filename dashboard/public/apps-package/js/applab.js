e89d4ca853be4da7bca1b45dac7b1e88cea919b6
Continuous Integration <dev@code.org>
2016-02-05 22:19:28 +0000

    Automatically built.
    84c8341 Merge pull request #6680 from code-dot-org/sprintf (Brent Van Minnen)
    a25f417 Merge pull request #6681 from code-dot-org/disable-flaky-tests (Brad Buchanan)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb1661072fdaed3dfa8396ead3acbfda752975d8ba735eda1719234baa3e6cf7
-size 2770647
+oid sha256:11ef37d1e806318bf90aed0e4bfd1d23704010c1623ec2dca76b0ecc7cbcb9fe
+size 2766466
