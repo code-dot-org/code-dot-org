28d2829e043bb1151c6cd51fd727ed8d7161f70e
Will Jordan <will@code.org>
2016-02-05 13:21:25 -0800

    Fallback to GitHub API query for determining latest commit hash for subfolder. Useful when shallow clones don't contain enough local revision history. [ci skip]

--- lib/cdo/rake_utils.rb
+++ lib/cdo/rake_utils.rb
@@ -86,6 +86,10 @@ module RakeUtils
     `git rev-parse HEAD`.strip
   end
 
+  def self.git_first_revision
+    `git rev-list --max-parents=0 HEAD`.strip
+  end
+
   def self.git_update_count()
     count = `git rev-list ..@{u} | wc -l`.strip.to_i
     return 0 if $?.exitstatus != 0
@@ -100,9 +104,28 @@ module RakeUtils
     `git status --porcelain 2>/dev/null | egrep \"^(M|A|D)\" | wc -l`.strip.to_i > 0
   end
 
-  # Gets the commit hash for the given directory
+  # Gets the commit hash for the given directory.
   def self.git_latest_commit_hash(dir)
-    `git log -n 1 --format="%H" -- #{dir}`.strip
+    rev = `git log -n 1 --format="%H" -- #{dir}`.strip
+    if rev == git_first_revision
+      # Fallback to github query if the first revision is returned,
+      # returning first revision if github query fails.
+      github_latest_commit_hash(dir) || rev
+    else
+      rev
+    end
+  end
+
+  # Gets the commit hash for the given directory using the GitHub API.
+  # Use when a shallow clone with a limited revision history does not contain the latest commit hash locally.
+  # Returns nil on any failure (e.g., GitHub isn't configured as the origin remote, or if the API query fails)
+  def self.github_latest_commit_hash(dir)
+    # Get the github slug of the current repo from the git config's remote-origin url.
+    github_slug = `git config --get remote.origin.url`.match(/github.com\/(.*).git$/)[1]
+    api_output = open("https://api.github.com/repos/#{github_slug}/commits?path=#{dir}&sha=#{git_revision}").read
+    JSON.parse(api_output).first['sha']
+    rescue
+      nil
   end
 
   def self.ln_s(source, target)
