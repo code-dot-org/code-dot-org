ad09de338980abde3d7c21c88d6bc18de3975550
Mehal Shah <mehal@code.org>
2016-02-05 12:54:05 -0800

    Fix index name for circleCI

--- dashboard/db/migrate/20160202010837_create_user_course_enrollments.rb
+++ dashboard/db/migrate/20160202010837_create_user_course_enrollments.rb
@@ -1,7 +1,7 @@
 class CreateUserCourseEnrollments < ActiveRecord::Migration
   def change
     create_table :user_course_enrollments do |t|
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: {:name => 'user_course_enrollment_user_index'}, foreign_key: true
       t.references :professional_learning_course, index: true, foreign_key: true
 
       t.timestamps null: false
