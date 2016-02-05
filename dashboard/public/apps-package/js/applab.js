5083dea1f3335c054a280d5937e86a00ee2ca9a7
Continuous Integration <dev@code.org>
2016-02-05 19:40:24 +0000

    Automatically built.
    5bd316d Merge pull request #6642 from code-dot-org/jshint-config-update (Brad Buchanan)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:695ae811641bbad198a336bed017e2839277c26e869fba0ad3382fc2c7b7a97d
-size 2770604
+oid sha256:3280c0987172a456afaaff34a6e1abda14ce8a9295d12abba9fbd49d59f031c9
+size 2769987
--- dashboard/public/apps-package/js/gamelab.js
+++ dashboard/public/apps-package/js/gamelab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59908d3fd2111e4e37e5a4a972bbd06fbca75ccd46ccfe34b0f9c4e666227753
-size 178899
+oid sha256:6c978e7d2e6f784231ab22645298108da7cb714216a9d9a5d5f62fac7fbbd42a
+size 179028
