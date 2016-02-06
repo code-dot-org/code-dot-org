93fc38b9f0c3960b3964ab312cc29e1f3dac8179
Will Jordan <will@code.org>
2016-02-05 18:47:38 -0800

    stringify bundle_bootstrap keys for proper yaml-config output

--- cookbooks/cdo-apps/recipes/bundle_bootstrap.rb
+++ cookbooks/cdo-apps/recipes/bundle_bootstrap.rb
@@ -11,16 +11,16 @@ rack_envs = [:development, :production, :adhoc, :staging, :test, :levelbuilder, 
 without = rack_envs - [env.to_sym]
 cores = ENV['SPROCKETS_DERAILLEUR_WORKER_COUNT'] || node['cpu']['total']
 env = {
-  BUNDLE_WITHOUT: without.join(':'),
-  BUNDLE_GEMFILE: "#{root}/Gemfile",
-  BUNDLE_JOBS: cores.to_s,
+  'BUNDLE_WITHOUT' => without.join(':'),
+  'BUNDLE_GEMFILE' => "#{root}/Gemfile",
+  'BUNDLE_JOBS' => cores.to_s,
   # Ignore any existing 'remembered options' in favor of the provided environment.
   # Ref:
   # http://bundler.io/man/bundle-install.1.html#REMEMBERED-OPTIONS
   # http://bundler.io/man/bundle-config.1.html
-  BUNDLE_IGNORE_CONFIG: '1',
+  'BUNDLE_IGNORE_CONFIG' => '1',
   # Avoid writing 'remembered options' to the default local config (./bundle/config).
-  BUNDLE_APP_CONFIG: Chef::Config[:file_cache_path]
+  'BUNDLE_APP_CONFIG' => Chef::Config[:file_cache_path]
 }
 node.default['cdo-apps']['bundle_env'] = env
 
