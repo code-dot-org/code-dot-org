a104d76eecced7641e646a998f72d6d8ca409e80
Mehal Shah <mehal@code.org>
2016-02-05 17:18:32 -0800

    Kick Circle CI

--- dashboard/db/migrate/20160202010837_create_user_course_enrollments.rb
+++ dashboard/db/migrate/20160202010837_create_user_course_enrollments.rb
@@ -3,7 +3,6 @@ class CreateUserCourseEnrollments < ActiveRecord::Migration
     create_table :user_course_enrollments do |t|
       t.references :user, index: {:name => 'user_course_enrollment_user_index'}, foreign_key: true
       t.references :professional_learning_course, index: {:name => 'user_course_enrollment_plc_id_index'}, foreign_key: true
-
       t.timestamps null: false
     end
   end
