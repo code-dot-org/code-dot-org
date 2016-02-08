05007db08dd01fffc3e538b5fe730dd1bc436654
Mehal Shah <mehal@code.org>
2016-02-08 15:47:38 -0800

    Renamed some things, redid and consolidated the migrations

--- dashboard/db/migrate/20160201214011_create_artifact_submissions.rb
+++
@@ -1,9 +0,0 @@
-class CreateArtifactSubmissions < ActiveRecord::Migration
-  def change
-    create_table :artifact_submissions do |t|
-      t.string :type
-
-      t.timestamps null: false
-    end
-  end
-end
--- dashboard/db/migrate/20160203004941_add_status_to_user_module_artifact_assignment.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToUserModuleArtifactAssignment < ActiveRecord::Migration
-  def change
-    add_column :user_module_artifact_assignments, :status, :string
-  end
-end
