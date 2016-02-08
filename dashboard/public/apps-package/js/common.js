9fad79aa61f9cf1548e5705c2d580a09af983d00
Continuous Integration <dev@code.org>
2016-02-08 20:39:21 +0000

    Automatically built.
    2d55961 Merge pull request #6696 from code-dot-org/input-autocomplete (David Bailey)
    5f67580 tweak SanitizedLevelHtml logging and allowed tags (David Bailey)

--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f5ba57c942215e6af8cf0e31543da249cfad58ba2e357945a20646e6f974c48
+oid sha256:6fb2c4cca1ca498e6296bb43909a93ca529e6bc2293e0998c62a90a0ddeed080
 size 5187142
