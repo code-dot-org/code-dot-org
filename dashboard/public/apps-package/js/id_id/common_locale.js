51d60060aadce13e2a12bd05524c65c0b4531a36
Continuous Integration <dev@code.org>
2016-02-09 01:54:01 +0000

    Automatically built.
    08efdb7 Merge pull request #6709 from code-dot-org/sync-i18n (Tanya Parker)
    d6c380b code.org translations 02/08 (Tanya Parker)

--- dashboard/public/apps-package/js/id_id/common_locale.js
+++ dashboard/public/apps-package/js/id_id/common_locale.js
@@ -202,6 +202,8 @@ s:function(d,k,p){common_locale.c(d,k);return d[k] in p?p[d[k]]:p.other}};
 "dropletBlock_callMyFunction_n_description":function(d){return "panggil sebuah nama fungsi yang mengambil satu parameter atau lebih"},
 "dropletBlock_callMyFunction_n_signatureOverride":function(d){return "Panggil sebuah fungsi dengan parameter"},
 "dropletBlock_callMyFunction_signatureOverride":function(d){return "Panggil sebuah fungsi"},
+"dropletBlock_comment_description":function(d){return "Write a description of some code"},
+"dropletBlock_comment_signatureOverride":function(d){return "comment"},
 "dropletBlock_declareAssign_x_array_1_4_description":function(d){return "Mendeklarasikan variabel dan menetapkannya ke sebuah array dengan nilai awal yang diberikan"},
 "dropletBlock_declareAssign_x_array_1_4_param0":function(d){return "x"},
 "dropletBlock_declareAssign_x_array_1_4_param0_description":function(d){return "The name you will use in the program to reference the variable"},
@@ -408,6 +410,4 @@ s:function(d,k,p){common_locale.c(d,k);return d[k] in p?p[d[k]]:p.other}};
 "watchVideo":function(d){return "Tonton Videonya"},
 "when":function(d){return "ketika"},
 "whenRun":function(d){return "ketika dijalankan"},
-"workspaceHeaderShort":function(d){return "Area kerja: "},
-"dropletBlock_comment_description":function(d){return "Write a description of some code"},
-"dropletBlock_comment_signatureOverride":function(d){return "comment"}};
+"workspaceHeaderShort":function(d){return "Area kerja: "}};
