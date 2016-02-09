d044b7958e29614cd1b214b086185c8ee0083483
Mehal Shah <mehal@code.org>
2016-02-08 16:27:33 -0800

    Use objects, not IDs to refer to things

--- dashboard/app/models/user_professional_learning_course_enrollment.rb
+++ dashboard/app/models/user_professional_learning_course_enrollment.rb
@@ -23,10 +23,10 @@ class UserProfessionalLearningCourseEnrollment < ActiveRecord::Base
     enrollment = UserProfessionalLearningCourseEnrollment.find_or_create_by(user: user, professional_learning_course: course, status: :in_progress)
 
     learning_modules.each do |learning_module|
-      module_assignment = UserEnrollmentModuleAssignment.find_or_create_by(user_professional_learning_course_enrollment_id: enrollment.id, professional_learning_module: learning_module)
+      module_assignment = UserEnrollmentModuleAssignment.find_or_create_by(user_professional_learning_course_enrollment: enrollment, professional_learning_module: learning_module)
 
       learning_module.professional_learning_tasks.each do |task|
-        UserModuleTaskAssignment.find_or_create_by(user_enrollment_module_assignment: module_assignment, professional_learning_task_id: task.id, status: :not_started)
+        UserModuleTaskAssignment.find_or_create_by(user_enrollment_module_assignment: module_assignment, professional_learning_task: task, status: :not_started)
       end
     end
 
