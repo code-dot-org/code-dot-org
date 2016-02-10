39cab75eda446122efb6bf14736c623a7f3ed6aa
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-02-09 19:00:56 -0800

    updated levelbuilder React components to only be global when they have to be, and added some styling

--- code-studio/src/js/components/CellEditor.jsx
+++ code-studio/src/js/components/CellEditor.jsx
@@ -2,53 +2,49 @@
 
 var Cell = require('blockly-mooc/src/maze/cell');
 
-window.dashboard = window.dashboard || {};
+module.exports = React.createClass({
+  propTypes: {
+    cell: React.PropTypes.object.isRequired,
+    row: React.PropTypes.number.isRequired,
+    col: React.PropTypes.number.isRequired,
+    onUpdate: React.PropTypes.func.isRequired,
+  },
 
-window.dashboard.CellEditor = (function (React) {
-  return React.createClass({
-    propTypes: {
-      cell: React.PropTypes.object.isRequired,
-      row: React.PropTypes.number.isRequired,
-      col: React.PropTypes.number.isRequired,
-      onUpdate: React.PropTypes.func.isRequired,
-    },
+  handleChange: function (event) {
+    var values = this.props.cell.serialize();
+    values[event.target.name] = (event.target.value === 'undefined' || event.target.value === '') ? undefined : parseInt(event.target.value);
+    var newCell = Cell.deserialize(values);
+    this.props.onUpdate(newCell);
+    this.props.cell = newCell;
+  },
 
-    handleChange: function (event) {
-      var values = this.props.cell.serialize();
-      values[event.target.name] = (event.target.value === 'undefined' || event.target.value === '') ? undefined : parseInt(event.target.value);
-      var newCell = Cell.deserialize(values);
-      this.props.onUpdate(newCell);
-      this.props.cell = newCell;
-    },
-
-    render: function () {
-      var values = this.props.cell.serialize();
-      for (var value in values) {
-        if (values[value] === undefined) {
-          values[value] = 'undefined';
-        }
+  render: function () {
+    var values = this.props.cell.serialize();
+    for (var value in values) {
+      if (values[value] === undefined) {
+        values[value] = 'undefined';
       }
-      return (
-        <form className="span4 offset1">
-          <header>
-            <strong>Editing Cell ({this.props.row}, {this.props.col})</strong>
-          </header>
+    }
+    return (
+      <form className="span4 offset1">
+        <header>
+          <strong>Editing Cell ({this.props.row}, {this.props.col})</strong>
+        </header>
 
-          <label htmlFor="tileType">Tile Type (required):</label>
-          <select name="tileType" value={values.tileType} onChange={this.handleChange}>
-            <option value="0">wall</option>
-            <option value="1">open</option>
-            <option value="2">start</option>
-            <option value="3">finish</option>
-            <option value="4">obstacle</option>
-            <option value="5">startandfinish</option>
-          </select>
+        <label htmlFor="tileType">Tile Type (required):</label>
+        <select name="tileType" value={values.tileType} onChange={this.handleChange}>
+          <option value="0">wall</option>
+          <option value="1">open</option>
+          <option value="2">start</option>
+          <option value="3">finish</option>
+          <option value="4">obstacle</option>
+          <option value="5">startandfinish</option>
+        </select>
 
-          <label htmlFor="value">Value:</label>
-          <input type="number" name="value" value={values.value} onChange={this.handleChange} />
+        <label htmlFor="value">Value:</label>
+        <input type="number" name="value" value={values.value} onChange={this.handleChange} />
 
-        </form>
-      );
-    },
-  });
-})(React);
+      </form>
+    );
+  },
+});
