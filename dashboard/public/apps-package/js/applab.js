333d4521156b2aadcfe0d8067b03ed5ee1ac509e
Continuous Integration <dev@code.org>
2016-02-11 18:32:25 +0000

    Automatically built.
    d085ade Merge pull request #6752 from code-dot-org/test-event-sanitization (Brad Buchanan)
    cff10b7 Correct typo in test name [ci skip] (Brad Buchanan)
    b14a00e Merge pull request #6757 from code-dot-org/killDeadHelperTests (ashercodeorg)
    cb6592f Removes empty tests. (Asher Kach)
    941beec Merge pull request #6755 from code-dot-org/localize-congrats (Tanya Parker)
    8495528 sync new congrats strings for all locales (Tanya Parker)
    a6fc8d3 content changes -tanya (Continuous Integration)
    c15109b Merge pull request #6754 from code-dot-org/congrats-pages (Tanya Parker)
    70d3fdd localize recommended 3rd party tutorials (Tanya Parker)
    c2c0383 update third party blocks to follow course block styling, update pictures (Tanya Parker)
    fedf992 Merge pull request #6730 from code-dot-org/congrats-pages (Tanya Parker)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc7de68d7bcb55c3ace528f5a9b036c76fac300e166fbd08e0373c136c2f47c9
-size 2623397
+oid sha256:6a3796a83bc9a996719feea041577c2ab33a21f662f02f4bd01cef182df8da67
+size 2631097
