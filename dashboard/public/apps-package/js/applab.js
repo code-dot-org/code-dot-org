70771fd142efa8ce1e80e32882ac50895bd739ac
Continuous Integration <dev@code.org>
2016-02-11 01:04:16 +0000

    Automatically built.
    fa38c5f Fix possible null ref in 0a3e916eaa5f2c2cd08a1fcb0a37a1bed5c29437 (Josh Lory)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:236c43f03fd6356fcc9ed609afca39285c5ed8c7cc0f7c140963fb970b92ba08
-size 2623257
+oid sha256:bc7de68d7bcb55c3ace528f5a9b036c76fac300e166fbd08e0373c136c2f47c9
+size 2623397
