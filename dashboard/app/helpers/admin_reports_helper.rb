86ff9192aab6313670811e8fd094b95d8e98f3b9
Asher Kach <asher@code.org>
2016-02-11 10:19:27 -0600

    Improves readability of admin_stats.

--- dashboard/app/helpers/admin_reports_helper.rb
+++ dashboard/app/helpers/admin_reports_helper.rb
@@ -2,8 +2,13 @@ module AdminReportsHelper
   # Computes the ratio of this_gender to this_gender and other_gender, returning the percentage as
   # a string with a trailing percentage sign.
   def get_gender_percentage(this_gender_count, other_gender_count)
-    return number_with_precision(100.0 * this_gender_count /
-        [this_gender_count + other_gender_count, 1].max,
-       precision: 2).to_s + "%"
+    return get_and_round_percentage(
+      this_gender_count.to_f / [this_gender_count + other_gender_count, 1].max.to_f)
+  end
+
+  # Computes and rounds to two decimal places a percentage (0% to 100%) from a ratio (0.0 to 1.0).
+  # Returns the string representation of the percentage, with a percent sign.
+  def get_and_round_percentage(ratio)
+    return number_with_precision(100.0 * ratio, precision: 2).to_s + '%'
   end
 end
