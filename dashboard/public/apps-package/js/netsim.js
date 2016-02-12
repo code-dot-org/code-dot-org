07b0ecf614afc439f11a36676427fee9166ffb69
Continuous Integration <dev@code.org>
2016-02-12 23:43:22 +0000

    Automatically built.
    fa3b5e8 Merge pull request #6782 from code-dot-org/allow-span (David Bailey)
    01edd5d allow span tags during html sanitization (David Bailey)

--- dashboard/public/apps-package/js/netsim.js
+++ dashboard/public/apps-package/js/netsim.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c8b54462cebc4e34c534f1b3b6242aa57b25d3ade906cc34eb422909ed98c74
-size 4144036
+oid sha256:975d820fa6cd23f565b504ec78af9997cf6db082762e90032b9571ba78ed1bb6
+size 4144211
