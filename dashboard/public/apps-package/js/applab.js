c354a81debf5c1399485113351394427eaedface
Continuous Integration <dev@code.org>
2016-02-13 00:18:13 +0000

    Automatically built.
    bd2493d Merge pull request #6772 from code-dot-org/standardize-event-properties (Brad Buchanan)
    aba1410 Merge pull request #6775 from code-dot-org/teacher-recruitment-email (Andrew Oberhardt)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10bed391976544a09f37ba4d7ff38edd2e1a7a3560153926413553a2547c7c6a
-size 2638102
+oid sha256:aeb6b17cbb7a71913683993e810eed9dc5d81e4994890d72a5da49a342d258bb
+size 2641964
