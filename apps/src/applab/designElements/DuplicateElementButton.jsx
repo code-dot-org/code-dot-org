09cd7106b4c7ccd71f47ea0355577c095ab8affb
Ruchi Varshney <varshney.ruchi@gmail.com>
2016-02-15 00:34:04 -0800

    Copy paste functionality for elements in applab design mode

---
+++ apps/src/applab/designElements/DuplicateElementButton.jsx
@@ -0,0 +1,36 @@
+var rowStyle = require('./rowStyle');
+
+var DuplicateElementButton = React.createClass({
+  propTypes: {
+    handleDuplicate: React.PropTypes.func.isRequired
+  },
+
+  handleDuplicate: function(event) {
+    this.props.handleDuplicate();
+  },
+
+  render: function () {
+    var buttonStyle = {
+      paddingTop: '5px',
+      paddingBottom: '5px',
+      fontSize: '14px',
+    };
+
+    var duplicateButtonStyle = $.extend({}, buttonStyle, {
+      backgroundColor: '#0aa',
+      color: 'white'
+    });
+
+    return (
+      <div style={{marginLeft: 15}}>
+        <button
+        style={duplicateButtonStyle}
+        onClick={this.handleDuplicate}>
+        Duplicate
+      </button>
+    </div>
+    );
+  }
+});
+
+module.exports = DuplicateElementButton;
