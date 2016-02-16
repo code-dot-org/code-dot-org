65bf7d1ab27906f6437c8789c37d20c98ba6a99b
Mehal Shah <mehal@code.org>
2016-02-15 23:52:04 -0800

    Remove useless tests, fix schema

--- dashboard/test/models/plc_enrollment_evaluation_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PlcEnrollmentEvaluationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- dashboard/test/models/plc_evaluation_answer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PlcEvaluationAnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
