892760c5befbc9aa4aa8701c045efbf0f3d70238
Mehal Shah <mehal@code.org>
2016-02-16 11:08:00 -0800

    Make foreign key rollback idempotent

--- dashboard/db/migrate/20160210025746_create_plc_evaluation_answers.rb
+++ dashboard/db/migrate/20160210025746_create_plc_evaluation_answers.rb
@@ -1,9 +1,9 @@
 class CreatePlcEvaluationAnswers < ActiveRecord::Migration
   def change
     create_table :plc_evaluation_answers do |t|
-      t.references :plc_evaluation_question, index: {name: 'plc_answer_question_index'}, foreign_key: true
+      t.references :plc_evaluation_question, index: {name: 'plc_answer_question_index'}
       t.string :answer
-      t.references :professional_learning_task, index: {name: 'plc_answer_plc_index'}, foreign_key: true
+      t.references :professional_learning_task, index: {name: 'plc_answer_plc_index'}
 
       t.timestamps null: false
     end
