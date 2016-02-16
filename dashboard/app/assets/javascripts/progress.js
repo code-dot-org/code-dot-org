4e67362b92deb79a70475957b19ee80bc0da6a1b
Brendan Reville <brendan@code.org>
2016-02-16 11:22:55 +1100

    Protect against JS error in browser.

--- dashboard/app/assets/javascripts/progress.js
+++ dashboard/app/assets/javascripts/progress.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3d145c7d5f54eaac39ad0fc27da6758e0e64e951f7df36beb07a753209ab219
-size 4242
+oid sha256:1564b9ffaffa2e95c9bfdcd52355f44c42988b1d9bce9526be263a73090b7f83
+size 4288
