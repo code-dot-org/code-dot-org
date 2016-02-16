45514b7255c4316e1bc41083a1ca837bc97343bf
Josh Lory <josh.lory@code.org>
2016-02-16 15:00:48 -0800

    Add "steps a switch statement" JSInterpreter test

--- apps/test/JSInterpreterTest.js
+++ apps/test/JSInterpreterTest.js
@@ -9,6 +9,25 @@ describe("JSInterpreter", function () {
   window.acorn = require('../lib/jsinterpreter/acorn');
   require('../lib/jsinterpreter/interpreter');
 
+  function initWithCode(code) {
+    // Setup a jsInterpreter instance with `hideSource: true` so an editor isn't
+    // needed. Mock `getUserCodeLine` so the code is considered inUserCode.
+    jsInterpreter = new JSInterpreter({
+      studioApp: {hideSource: true}
+    });
+    jsInterpreter.getUserCodeLine = function () { return 0; };
+
+    // Initialize a test program
+    jsInterpreter.parse({code: code});
+
+    assert(jsInterpreter.initialized());
+
+    // Step in
+    jsInterpreter.paused = true;
+    jsInterpreter.nextStep = JSInterpreter.StepType.IN;
+    jsInterpreter.executeInterpreter(true);
+  }
+
   function assertCurrentState(expected) {
     var state = jsInterpreter.interpreter.stateStack[0];
     assert.containSubset(state, expected);
@@ -27,26 +46,7 @@ describe("JSInterpreter", function () {
   }
 
   it("steps a `for` loop", function () {
-
-    // Setup a jsInterpreter instance with `hideSource: true` so an editor isn't
-    // needed. Mock `getUserCodeLine` so the code is considered inUserCode.
-    jsInterpreter = new JSInterpreter({
-      studioApp: {hideSource: true}
-    });
-    jsInterpreter.getUserCodeLine = function () { return 0; };
-
-    // Initialize a test program
-    jsInterpreter.parse({
-      code: 'for (var i = 0; i < 2; i++) { 1; }'
-    });
-
-    assert(jsInterpreter.initialized());
-
-    // Step in
-    jsInterpreter.paused = true;
-    jsInterpreter.nextStep = JSInterpreter.StepType.IN;
-    jsInterpreter.executeInterpreter(true);
-
+    initWithCode('for (var i = 0; i < 2; i++) { 1; }');
     assertCurrentState({node: {type: 'ForStatement'}, mode: undefined});
 
     // Continue stepping
@@ -58,6 +58,18 @@ describe("JSInterpreter", function () {
       {node: {type: 'ExpressionStatement'}},   // (body) 1;
       {node: {type: 'ForStatement'}, mode: 3}, // (update) i++
       {node: {type: 'ForStatement'}, mode: 1}  // (test) i < 2;
+    ]);
+  });
+
+  it("steps a `switch` statement", function () {
+    initWithCode('switch (5) { case 5: 1; } 2;');
+    assertCurrentState({node: {type: 'SwitchStatement'}});
+
+    // Continue stepping
+    verifyStepSequence([
+      {node: {type: 'SwitchStatement'}},
+      {node: {type: 'ExpressionStatement', expression: {value: 1}}},
+      {node: {type: 'ExpressionStatement', expression: {value: 2}}}
     ]);
   });
 
