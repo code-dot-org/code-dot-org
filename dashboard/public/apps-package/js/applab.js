c339a21a64c3534a9b6899ee0d593cf0d8af847a
Continuous Integration <dev@code.org>
2016-02-16 19:55:23 +0000

    Automatically built.
    c9f1e36 Merge pull request #6807 from code-dot-org/fix-jsinterpreter-memory-leak (Brad Buchanan)
    3a4c710 fix congrats string -tanya (Continuous Integration)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e01b1221df7bb62c2fb614ecaadaf6707114b439f696987b7b970e2eae70fadb
-size 2642614
+oid sha256:4157a930bee144141cabcfa612cec0376c6768560f64064b00f3d8655bcecd55
+size 2642754
