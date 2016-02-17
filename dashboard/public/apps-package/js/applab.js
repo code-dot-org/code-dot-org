df9cf34d34cd41a5228fa4cf74d30b5aa4d06343
Continuous Integration <dev@code.org>
2016-02-17 18:33:20 +0000

    Automatically built.
    5a12811 Merge pull request #6825 from stevekrenzel/custom_level_blocks_cleanup (Brent Van Minnen)
    2fbdbb3 Merge pull request #6814 from code-dot-org/addUserLevelScopes (ashercodeorg)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e4d22bedefe913b96b11cd3a2a91268d500507083d4137be178f07da95fcf9b
-size 2644212
+oid sha256:701a0b322cbcef7dab88fd56677ca061ce2dedcf712870c99f9f2acb62f5e517
+size 2644352
