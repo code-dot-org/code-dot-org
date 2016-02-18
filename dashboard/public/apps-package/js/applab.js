09cb146c034be90a08ddef36e80148e29ef49b20
Continuous Integration <dev@code.org>
2016-02-18 01:21:05 +0000

    Automatically built.
    85d84de Merge pull request #6842 from code-dot-org/improve-gruntfile (Brad Buchanan)
    bfa0733 Merge pull request #6846 from code-dot-org/update-volunteer-guide (Tanya Parker)
    c2c28c1 Update apps README with npm test with arguments [ci skip] (Brad Buchanan)
    c9bdd47 mochaTest owns its own dependencies, can run without prior build. (Brad Buchanan)
    e658c8b Merge pull request #6843 from code-dot-org/reorganize-educate (Tanya Parker)
    12628da update volunteer guide (Tanya Parker)
    f1d4530 staging content changes (-tanya) (Continuous Integration)
    9f77daf Merge pull request #6818 from code-dot-org/disable-iphone-footer-eyes (Tanya Parker)
    b535031 reorganize curriculum, hoc, and resources in educate (Tanya Parker)
    d6a3025 Reformat file (Brian Jordan)
    411f016 Always output sass 'nested' rather than 'compressed' (same behavior as before this PR) (Brian Jordan)
    8e3ec5b Change apps `grunt build` usages to corresponding `npm run` equivalents - Have rake build:core_and_apps_dev use equivalent ./build_with_core.sh build script (Brian Jordan)
    8cc5d4b Explicitly un-set MOOC_DEV for dist mode, run grunt clean before build (Brian Jordan)
    40b7ca6 Remove unused .travis.yml (Brian Jordan)
    6e26402 Add npm scripts for apps (Brad Buchanan)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:478393bd7bb2aa44b6d3846a7cb23cde0aec8fa13eabdfcd956310543ccae4fe
-size 2644492
+oid sha256:d92b489d2f4cd3452ecb58d8d674def80db094f401a1bc677ce15cd473450e19
+size 2640992
