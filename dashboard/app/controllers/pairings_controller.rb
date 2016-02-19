c2ce42364b5e561241154500a06ebfde5850579f
Laurel Fan <laurel@code.org>
2016-02-18 16:26:51 -0800

    save pairing info in session; popup dialog instead of navigating to new page with dialog

--- dashboard/app/controllers/pairings_controller.rb
+++ dashboard/app/controllers/pairings_controller.rb
@@ -6,13 +6,41 @@ class PairingsController < ApplicationController
         @props = {source: url_for()}.to_json
         render
       end
-      format.js { render json: pairings_summary }
+      format.js { render json: summary }
     end
   end
 
+  def update
+    # check permissions
+
+    logger.debug params
+    logger.debug params[:pairings]
+    if params[:pairings].present?
+      self.pairings = params[:pairings]
+    else
+      self.pairings = nil
+    end
+
+    head :ok
+  end
 
   private
+
+  def pairings=(pairings_from_params)
+    session[:pairings] = pairings_from_params.map do |pairing_param|
+      User.find(pairing_param[:id])
+    end
+  end
+
   def pairings_summary
+    return [] if session[:pairings].blank?
+
+    session[:pairings].map do |user|
+      {id: user.id, name: user.name}
+    end
+  end
+
+  def summary
     # [{id: 1, name: "A section"}, {id: 15, name: "Anotther section"}]
     sections = current_user.sections_as_student.map do |section|
       {id: section.id, name: section.name, students:
@@ -21,8 +49,6 @@ class PairingsController < ApplicationController
        end
       }
     end
-
-    pairings = [] # read/write this from session
 
     {pairings: pairings, sections: sections}
   end
--- dashboard/test/controllers/pairings_controller_test.rb
+++ dashboard/test/controllers/pairings_controller_test.rb
@@ -8,8 +8,75 @@ class PairingsControllerTest < ActionController::TestCase
 
   test 'should get show for logged in user' do
     sign_in create(:user)
-    get :show
+
+    xhr :get, :show
     assert_response :success
+
+    expected_response = {"pairings" => [], "sections" => []}
+    assert_equal expected_response, JSON.parse(response.body)
+  end
+
+
+  test 'should get show for logged in user with sections' do
+    sign_in user = create(:user)
+    section_1 = create(:follower, student_user: user).section
+    section_2 = create(:follower, student_user: user).section
+
+    xhr :get, :show
+    assert_response :success
+
+    expected_response = {"pairings" => [], "sections" => [{"id" => section_1.id, "name" => section_1.name, "students" => []},
+                                                         {"id" => section_2.id, "name" => section_2.name, "students" => []}]}
+    assert_equal expected_response, JSON.parse(response.body)
+  end
+
+  test 'should get show for logged in user with sections and pairings' do
+    sign_in user = create(:user)
+    section = create(:follower, student_user: user).section
+    classmate = create(:follower, section: section).student_user
+    session[:pairings] = [classmate]
+
+    xhr :get, :show
+    assert_response :success
+
+    expected_response = {"pairings" => [{id: classmate.id, name: classmate.name}],
+                         "sections" => [{"id" => section.id, "name" => section.name, "students" => [{id: classmate.id, name: classmate.name}]}]}
+    assert_equal expected_response, JSON.parse(response.body)
+  end
+
+  test 'should set pairings in session if they are valid classmates' do
+    sign_in user = create(:user)
+    section = create(:follower, student_user: user).section
+    classmate_1 = create(:follower, section: section).student_user
+    classmate_2 = create(:follower, section: section).student_user
+
+    xhr :put, :update, pairings: [{id: classmate_1.id}, {id: classmate_2.id}]
+
+    assert_response :success
+    assert_equal [classmate_1, classmate_2], session[:pairings]
+  end
+
+
+  test 'should not set pairings in session if they are not valid classmates' do
+    sign_in user = create(:user)
+    section = create(:follower, student_user: user).section
+    classmate = create(:follower, section: section).student_user
+    invalid_user = create(:student)
+
+    xhr :put, :update, pairings: [{id: classmate.id}, {id: invalid_user.id}]
+
+    assert_response :success
+    # the invalid user is silently rejected
+    assert_equal [classmate_1], session[:pairings]
+  end
+
+
+  test 'should remove pairings in session' do
+    sign_in create(:user)
+    xhr :put, :update, pairings: []
+
+    assert_response :success
+    assert_equal nil, session[:pairings]
   end
 
 end
