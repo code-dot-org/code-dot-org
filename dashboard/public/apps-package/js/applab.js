cb32367fec4566699e2691a0b79feb5875730891
Continuous Integration <dev@code.org>
2016-02-19 00:33:02 +0000

    Automatically built.
    8445d9e Merge pull request #6870 from code-dot-org/self-closing-tags (David Bailey)
    b76dfbf Merge pull request #6863 from code-dot-org/update-team-photos (Tanya Parker)
    0beafdd fix mocha tests (David Bailey)
    90a18db Don't let XMLSerializer add bogus self-closing tags to our levelHtml (David Bailey)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66205941526f81096aad1aa5658be163daf8ead81331f2bc3d6dbe6e52694432
-size 2641073
+oid sha256:d597177aa5b6bb2f27f5aded8535f1803e71a89bd3ae17955ed83c34d83e7c5b
+size 2641085
