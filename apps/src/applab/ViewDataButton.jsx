a0eb9e763409d88fd0935b043b4eb83a80759718
Brad Buchanan <brad@code.org>
2016-02-19 18:27:00 -0800

    Extract ViewDataButton

---
+++ apps/src/applab/ViewDataButton.jsx
@@ -0,0 +1,51 @@
+/** @file Button that opens the data browser */
+// Strict linting: Absorb into global config when possible
+/* jshint
+ unused: true,
+ eqeqeq: true,
+ maxlen: 120
+ */
+/* global $ */
+
+var applabMsg = require('./locale');
+var styles = require('./DesignToggleRowStyles');
+
+/**
+ * The button above the visualization that opens the data browser.
+ * @type {function}
+ */
+var ViewDataButton = React.createClass({
+  propTypes: {
+    onClick: React.PropTypes.func.isRequired,
+    hide: React.PropTypes.bool.isRequired
+  },
+
+  render: function () {
+
+    var showDataButtonStyle = $.extend(
+        {
+          float: 'right',
+          textAlign: 'left',
+          maxWidth: '100%',
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis'
+        },
+        styles.buttonStyle,
+        styles.inactiveStyle,
+        this.props.hide ? styles.hiddenStyle : null
+    );
+
+    return (
+        <button
+            id='viewDataButton'
+            style={showDataButtonStyle}
+            className='no-outline'
+            onClick={this.props.onClick}>
+          <i className='fa fa-database' style={styles.iconStyle}></i>
+          {applabMsg.viewData()}
+        </button>
+    );
+  }
+});
+module.exports = ViewDataButton;
