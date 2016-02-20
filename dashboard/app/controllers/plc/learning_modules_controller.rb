3923ab07c9d55c1e61b1e562256b9ac4bdf2df30
Mehal Shah <mehal@code.org>
2016-02-19 17:37:25 -0800

    PR feedback - fixed the cancan problem, moved js out

--- dashboard/app/controllers/plc/learning_modules_controller.rb
+++ dashboard/app/controllers/plc/learning_modules_controller.rb
@@ -1,11 +1,8 @@
 class Plc::LearningModulesController < ApplicationController
   load_and_authorize_resource
-  before_action :set_plc_learning_module, only: [:show, :edit, :update, :destroy]
-
   # GET /plc/learning_modules
   # GET /plc/learning_modules.json
   def index
-    @plc_learning_modules = Plc::LearningModule.all
   end
 
   # GET /plc/learning_modules/1
@@ -15,7 +12,6 @@ class Plc::LearningModulesController < ApplicationController
 
   # GET /plc/learning_modules/new
   def new
-    @plc_learning_module = Plc::LearningModule.new
   end
 
   # GET /plc/learning_modules/1/edit
@@ -25,10 +21,10 @@ class Plc::LearningModulesController < ApplicationController
   # POST /plc/learning_modules
   # POST /plc/learning_modules.json
   def create
-    @plc_learning_module = Plc::LearningModule.new(plc_learning_module_params)
+    @learning_module = Plc::LearningModule.new(learning_module_params)
 
-    if @plc_learning_module.save
-      redirect_to @plc_learning_module, notice: 'Learning module was successfully created.'
+    if @learning_module.save
+      redirect_to @learning_module, notice: 'Learning module was successfully created.'
     else
       redirect_to action: :new
     end
@@ -37,8 +33,8 @@ class Plc::LearningModulesController < ApplicationController
   # PATCH/PUT /plc/learning_modules/1
   # PATCH/PUT /plc/learning_modules/1.json
   def update
-    if @plc_learning_module.update(plc_learning_module_params)
-      redirect_to @plc_learning_module, notice: 'Learning module was successfully updated.'
+    if @learning_module.update(learning_module_params)
+      redirect_to @learning_module, notice: 'Learning module was successfully updated.'
     else
       redirect_to action: :edit
     end
@@ -47,18 +43,18 @@ class Plc::LearningModulesController < ApplicationController
   # DELETE /plc/learning_modules/1
   # DELETE /plc/learning_modules/1.json
   def destroy
-    @plc_learning_module.destroy
+    @learning_module.destroy
     redirect_to action: :index
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_plc_learning_module
-    @plc_learning_module = Plc::LearningModule.find(params[:id])
+    @learning_module = Plc::LearningModule.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
-  def plc_learning_module_params
+  def learning_module_params
     params.require(:plc_learning_module).permit(:name, :learning_module_type)
   end
 end
