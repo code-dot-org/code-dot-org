0f3368569723390f790a05b32d5158095a7f0396
Will Jordan <will@code.org>
2016-02-22 14:49:33 -0800

    Refactor chef cookbooks for chef-client upgrade (v11 -> v12) - add `sudo-user` cookbook - refactor `cdo-apps` cookbook for more self-contained execution - Add Test Kitchen integration-test harness for cdo-apps (.kitchen.yml, Berksfile) - don't generate `cdo-github-access` files when Chef attributes are not provided - Add fix for mysql service stop (with test) - Update cdo-postfix with simplified configuration and via third-party `postfix` cookbook. Allows installation to proceed when valid account credentials are not provided. - refactor cdo-repository to support shallow git clones - include omnibus_updater for managing chef-client upgrade

--- cookbooks/cdo-postfix/metadata.rb
+++ cookbooks/cdo-postfix/metadata.rb
@@ -1,7 +1,10 @@
 name             'cdo-postfix'
 maintainer       'Code.org'
-maintainer_email 'geoffrey@code.org'
+maintainer_email 'will@code.org'
 license          'All rights reserved'
 description      'Installs/Configures cdo-postfix'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
+
+depends 'apt'
+depends 'postfix'
