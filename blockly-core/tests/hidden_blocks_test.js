4b1e80478e847e4ad5ab32542506ff06fc152a9c
Ram Kandasamy <ram@code.org>
2016-02-22 12:04:19 -0800

    A couple more test cases for hidden block codegen.

--- blockly-core/tests/hidden_blocks_test.js
+++ blockly-core/tests/hidden_blocks_test.js
@@ -6,36 +6,101 @@
 /* global assertEquals */
 'use strict';
 
+var HIDDEN_VAR_REASSIGNMENT =
+  '<xml>' +
+    '<block type="variables_set" inline="false">' +
+      '<title name="VAR">x</title>' +
+      '<value name="VALUE">' +
+        '<block type="math_number">' +
+          '<title name="NUM">1</title>' +
+        '</block>' +
+      '</value>' +
+      '<next>' +
+        '<block type="variables_set" inline="false" uservisible="false">' +
+          '<title name="VAR">x</title>' +
+          '<value name="VALUE">' +
+            '<block type="math_number" uservisible="false">' +
+              '<title name="NUM">2</title>' +
+            '</block>' +
+          '</value>' +
+        '</block>' +
+      '</next>' +
+    '</block>' +
+  '</xml>';
+
+var NESTED_HIDDEN_VAR_REASSIGNMENT =
+  '<xml>' +
+    '<block type="variables_set" inline="false">' +
+      '<title name="VAR">x</title>' +
+      '<value name="VALUE">' +
+        '<block type="math_number">' +
+          '<title name="NUM">1</title>' +
+        '</block>' +
+      '</value>' +
+      '<next>' +
+        '<block type="controls_if" inline="false">' +
+          '<value name="IF0">' +
+            '<block type="logic_compare" inline="true" movable="false">' +
+              '<title name="OP">EQ</title>' +
+              '<value name="A">' +
+                '<block type="variables_get" movable="false">' +
+                  '<title name="VAR">x</title>' +
+                '</block>' +
+              '</value>' +
+              '<value name="B">' +
+                '<block type="variables_get">' +
+                  '<title name="VAR">x</title>' +
+                '</block>' +
+              '</value>' +
+            '</block>' +
+          '</value>' +
+          '<statement name="DO0">' +
+            '<block type="variables_set" inline="false">' +
+              '<title name="VAR">x</title>' +
+              '<value name="VALUE">' +
+                '<block type="math_number" uservisible="false">' +
+                  '<title name="NUM">2</title>' +
+                '</block>' +
+              '</value>' +
+              '<next>' +
+                '<block type="variables_set" inline="false" uservisible="false">' +
+                  '<title name="VAR">x</title>' +
+                  '<value name="VALUE">' +
+                    '<block type="math_number" uservisible="false">' +
+                      '<title name="NUM">3</title>' +
+                    '</block>' +
+                  '</value>' +
+                '</block>' +
+              '</next>' +
+            '</block>' +
+          '</statement>' +
+        '</block>' +
+      '</next>' +
+    '</block>' +
+  '</xml>';
+
 function test_showHiddenTrue() {
   Blockly.Test.initializeBlockSpaceEditor();
   var blockSpace = Blockly.mainBlockSpace;
 
   Blockly.Xml.domToBlockSpace(blockSpace, Blockly.Xml.textToDom(
-    '<xml>' +
-      '<block type="variables_set" inline="false">' +
-        '<title name="VAR">x</title>' +
-        '<value name="VALUE">' +
-          '<block type="math_number">' +
-            '<title name="NUM">1</title>' +
-          '</block>' +
-        '</value>' +
-        '<next>' +
-          '<block type="variables_set" inline="false" uservisible="false">' +
-            '<title name="VAR">x</title>' +
-            '<value name="VALUE">' +
-              '<block type="math_number" uservisible="false">' +
-                '<title name="NUM">2</title>' +
-              '</block>' +
-            '</value>' +
-          '</block>' +
-        '</next>' +
-      '</block>' +
-    '</xml>'
-  ));
+      HIDDEN_VAR_REASSIGNMENT));
 
-  var generatedCode = 
+  var generatedCode =
       Blockly.Generator.blockSpaceToCode('JavaScript', null, true);
   assertEquals(2, eval(generatedCode));
+}
+
+function test_showHiddenTrue_nested() {
+  Blockly.Test.initializeBlockSpaceEditor();
+  var blockSpace = Blockly.mainBlockSpace;
+
+  Blockly.Xml.domToBlockSpace(blockSpace, Blockly.Xml.textToDom(
+      NESTED_HIDDEN_VAR_REASSIGNMENT));
+
+  var generatedCode =
+      Blockly.Generator.blockSpaceToCode('JavaScript', null, true);
+  assertEquals(3, eval(generatedCode));
 }
 
 function test_showHiddenFalse() {
@@ -43,30 +108,22 @@ function test_showHiddenFalse() {
   var blockSpace = Blockly.mainBlockSpace;
 
   Blockly.Xml.domToBlockSpace(blockSpace, Blockly.Xml.textToDom(
-    '<xml>' +
-      '<block type="variables_set" inline="false">' +
-        '<title name="VAR">x</title>' +
-        '<value name="VALUE">' +
-          '<block type="math_number">' +
-            '<title name="NUM">1</title>' +
-          '</block>' +
-        '</value>' +
-        '<next>' +
-          '<block type="variables_set" inline="false" uservisible="false">' +
-            '<title name="VAR">x</title>' +
-            '<value name="VALUE">' +
-              '<block type="math_number" uservisible="false">' +
-                '<title name="NUM">2</title>' +
-              '</block>' +
-            '</value>' +
-          '</block>' +
-        '</next>' +
-      '</block>' +
-    '</xml>'
-  ));
+      HIDDEN_VAR_REASSIGNMENT));
 
-  var generatedCode = 
+  var generatedCode =
       Blockly.Generator.blockSpaceToCode('JavaScript', null, false)
   assertEquals(1, eval(generatedCode));
+}
+
+function test_showHiddenFalse_nested() {
+  Blockly.Test.initializeBlockSpaceEditor();
+  var blockSpace = Blockly.mainBlockSpace;
+
+  Blockly.Xml.domToBlockSpace(blockSpace, Blockly.Xml.textToDom(
+      NESTED_HIDDEN_VAR_REASSIGNMENT));
+
+  var generatedCode =
+      Blockly.Generator.blockSpaceToCode('JavaScript', null, false)
+  assertEquals(3, eval(generatedCode));
 }
 
