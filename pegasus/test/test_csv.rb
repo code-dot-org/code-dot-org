42b598c9c6e4c4e9c43a7fb535effb4d2c0fca6a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-02-23 12:54:33 -0800

    added explanatory comment

--- pegasus/test/test_csv.rb
+++ pegasus/test/test_csv.rb
@@ -1,6 +1,14 @@
 require_relative '../../lib/cdo/pegasus'
 require 'minitest/autorun'
 
+# cdo-donors has a weight column whose maximum value must be 1.0 in
+# order for our weighted random select to work. Even though the gsheet
+# that this data is coming from has algorithms that should enforce this,
+# it's gotten messed up on occasion, and that mistake has in at least
+# one case made it through to production.
+#
+# This test is intended to catch such a glitch before it makes it to
+# production.
 class CSVTest < Minitest::Test
   def test_cdo_donors_csv
     donors_csv = CSV.read('./data/cdo-donors.csv')
