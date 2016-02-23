99607ddc561f283bdf2d25a8d4b7f0345d6f1f51
Continuous Integration <dev@code.org>
2016-02-23 05:00:10 +0000

    Automatically built.
    af7d8ca Merge pull request #6895 from code-dot-org/allow-more-tags (David Bailey)
    c734f38 content changes -tanya (Continuous Integration)
    8a09ec3 fix style [ci skip] (Will Jordan)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0285e0aac2fee434941ad62a16235aa138e68cc4273d3b7e1c85e6211925b01
-size 2642908
+oid sha256:bea0b546685d0bda95cd2cc3650caef6fa4280023c89321ca8d4fdcc4287e164
+size 2643080
