f0b2f48bb03f7d0cc525b2b9c767d1972674693d
Ram Kandasamy <ram@code.org>
2016-02-25 14:54:44 -0800

    Refactor tests for toolbox autopopulate

--- dashboard/test/models/artist_test.rb
+++ dashboard/test/models/artist_test.rb
@@ -6,21 +6,6 @@ class ArtistTest < ActiveSupport::TestCase
 <xml>
 </xml>
 XML
-    @toolbox_with_draw_constant_xml = <<XML
-<xml>
-  <block type="draw_move_by_constant"/>
-</xml>
-XML
-    @toolbox_with_draw_constant_id_xml = <<XML
-<xml>
-  <block type="draw_move_by_constant" id="foo"/>
-</xml>
-XML
-    @toolbox_with_draw_xml = <<XML
-<xml>
-  <block type="draw_move"/>
-</xml>
-XML
     @toolbox_with_category_xml = <<XML
 <xml>
   <category name="example"/>
@@ -29,14 +14,6 @@ XML
     @toolbox_with_new_category_xml = <<XML
 <xml>
   <category name="NEW BLOCKS"/>
-</xml>
-XML
-    @toolbox_with_draw_dropdown_xml = <<XML
-<xml>
-  <block type="draw_move_by_constant_dropdown">
-    <title name="DIR">moveForward</title>
-    <title name="VALUE" config="50,100,150,200,300">50</title>
-  </block>
 </xml>
 XML
     @solution_xml = <<XML
@@ -63,74 +40,84 @@ XML
   </block>
 </xml>
 XML
-    @solution_with_id_xml = <<XML
-<xml>
-  <block type="when_run" deletable="false" movable="false">
-    <next>
-      <block type="draw_move_by_constant" id="bar">
-        <title name="DIR">moveForward</title>
-        <title name="VALUE">100</title>
-      </block>
-    </next>
-  </block>
-</xml>
+    @draw_block_xml = <<XML
+<block type="draw_move_by_constant">
+  <title name="DIR">moveForward</title>
+  <title name="VALUE">100</title>
+</block>
 XML
-    @solution_with_inline_xml = <<XML
-<xml>
-  <block type="when_run" deletable="false" movable="false">
-    <next>
-      <block type="draw_move_by_constant" inline="true">
-        <title name="DIR">moveForward</title>
-        <title name="VALUE">100</title>
-      </block>
-    </next>
-  </block>
-</xml>
+    @draw_block_with_id_foo_xml = <<XML
+<block type="draw_move_by_constant" id="foo">
+  <title name="DIR">moveForward</title>
+  <title name="VALUE">100</title>
+</block>
 XML
-    @solution_with_next_xml = <<XML
-<xml>
-  <block type="when_run" deletable="false" movable="false">
-    <next>
-      <block type="draw_move_by_constant" inline="true">
-        <title name="DIR">moveForward</title>
-        <title name="VALUE">100</title>
-        <next>
-          <block type="draw_move_by_constant" inline="true">
-            <title name="DIR">moveForward</title>
-            <title name="VALUE">100</title>
-          </block>
-        </next>
-      </block>
-    </next>
-  </block>
-</xml>
+    @draw_block_with_id_bar_xml = <<XML
+<block type="draw_move_by_constant" id="bar">
+  <title name="DIR">moveForward</title>
+  <title name="VALUE">100</title>
+</block>
 XML
-    @solution_with_non_constant_draw_xml = <<XML
-<xml>
-  <block type="when_run" deletable="false" movable="false">
-    <next>
-      <block type="draw_move" inline="true">
-        <title name="DIR">moveForward</title>
-        <value name="VALUE">
-          <block type="math_number">
-            <title name="NUM">100</title>
-          </block>
-        </value>
-        <next>
-          <block type="draw_move" inline="true">
-            <title name="DIR">moveForward</title>
-            <value name="VALUE">
-              <block type="math_number">
-                <title name="NUM">50</title>
-              </block>
-            </value>
-          </block>
-        </next>
-      </block>
-    </next>
-  </block>
-</xml>
+    @draw_block_with_inline_xml = <<XML
+<block type="draw_move_by_constant" inline="true">
+  <title name="DIR">moveForward</title>
+  <title name="VALUE">100</title>
+</block>
 XML
+    @draw_block_with_next_xml = <<XML
+<block type="draw_move_by_constant" inline="true">
+  <title name="DIR">moveForward</title>
+  <title name="VALUE">100</title>
+  <next>
+    <block type="draw_move_by_constant" inline="true">
+      <title name="DIR">moveForward</title>
+      <title name="VALUE">100</title>
+    </block>
+  </next>
+</block>
+XML
+    @draw_100_block_xml = <<XML
+<block type="draw_move" inline="true">
+  <title name="DIR">moveForward</title>
+  <value name="VALUE">
+    <block type="math_number">
+      <title name="NUM">100</title>
+    </block>
+  </value>
+</block>
+XML
+
+    @draw_50_block_xml = <<XML
+<block type="draw_move" inline="true">
+  <title name="DIR">moveForward</title>
+  <value name="VALUE">
+    <block type="math_number">
+      <title name="NUM">50</title>
+    </block>
+  </value>
+</block>
+XML
+    @value_100_block_xml = <<XML
+<block type="math_number">
+  <title name="NUM">100</title>
+</block>
+XML
+
+    @value_50_block_xml = <<XML
+<block type="math_number">
+  <title name="NUM">50</title>
+</block>
+XML
+    @draw_with_dropdown_xml = <<XML
+<block type="draw_move_by_constant_dropdown">
+  <title name="DIR">moveForward</title>
+  <title name="VALUE" config="50,100,150,200,300">50</title>
+</block>
+XML
+
+    @toolbox_doc = Nokogiri::XML '<xml></xml>'
+    @solution_doc = Nokogiri::XML '<xml></xml>'
+    @level = Artist.new
   end
 
   def make_level(toolbox_xml, solution_xml)
@@ -140,9 +127,21 @@ XML
     level
   end
 
+  def make_toolbox_node(xml)
+    @toolbox_doc.root.add_child(xml).first
+  end
+
+  def make_solution_node(xml)
+    @solution_doc.root.add_child(xml).first
+  end
+
   def assert_blocktype_in_toolbox(level, type, occurrances=1)
     blocks = Nokogiri::XML(level.properties['toolbox_blocks']).xpath("//block[@type=\"#{type}\"]")
     assert_equal occurrances, blocks.length
+  end
+
+  def assert_blocks_match(block1, block2)
+    assert_equal @level.strip_block(block1), @level.strip_block(block2)
   end
 
   test 'add missing block' do
@@ -151,46 +150,52 @@ XML
     assert_blocktype_in_toolbox level, "draw_move_by_constant"
   end
 
-  test 'do not add already present block' do
-    level = make_level(@toolbox_with_draw_constant_xml, @solution_xml)
-    level.add_missing_toolbox_blocks
-    assert_equal @toolbox_with_draw_constant_xml, level.properties['toolbox_blocks']
-  end
-
   test 'do not add non-editable solution blocks' do
     level = make_level(@empty_toolbox_xml, @non_editable_solution_xml)
     level.add_missing_toolbox_blocks
     assert_equal @empty_toolbox_xml, level.properties['toolbox_blocks']
   end
 
-  test 'do not add duplicate blocks because of differing ids' do
-    level = make_level(@toolbox_with_draw_constant_id_xml, @solution_with_id_xml)
-    level.add_missing_toolbox_blocks
-    assert_equal @toolbox_with_draw_constant_id_xml, level.properties['toolbox_blocks']
+  test 'identical blocks match' do
+    toolbox_block = make_toolbox_node @draw_block_xml
+    solution_block = make_solution_node @draw_block_xml
+    assert_blocks_match toolbox_block, solution_block
   end
 
-  test 'do not add duplicate blocks because of differing inline attribute' do
-    level = make_level(@toolbox_with_draw_constant_xml, @solution_with_inline_xml)
-    level.add_missing_toolbox_blocks
-    assert_equal @toolbox_with_draw_constant_xml, level.properties['toolbox_blocks']
+  test 'blocks with different IDs match' do
+    toolbox_block = make_toolbox_node @draw_block_with_id_foo_xml
+    solution_block = make_solution_node @draw_block_with_id_bar_xml
+    assert_blocks_match toolbox_block, solution_block
   end
 
-  test 'do not add duplicate blocks because of next child node' do
-    level = make_level(@toolbox_with_draw_constant_xml, @solution_with_next_xml)
-    level.add_missing_toolbox_blocks
-    assert_equal @toolbox_with_draw_constant_xml, level.properties['toolbox_blocks']
+  test 'blocks with different inline attribute match' do
+    toolbox_block = make_toolbox_node @draw_block_xml
+    solution_block = make_solution_node @draw_block_with_inline_xml
+    assert_blocks_match toolbox_block, solution_block
   end
 
-  test 'do not add duplicate blocks for differing title values' do
-    level = make_level(@empty_toolbox_xml, @solution_with_non_constant_draw_xml)
-    level.add_missing_toolbox_blocks
-    assert_blocktype_in_toolbox level, 'draw_move'
+  test 'blocks with different next nodes match' do
+    toolbox_block = make_toolbox_node @draw_block_xml
+    solution_block = make_solution_node @draw_block_with_next_xml
+    assert_blocks_match toolbox_block, solution_block
   end
 
-  test 'do not add jump/move_by_constant blocks if *_dropdown version is already present' do
-    level = make_level(@toolbox_with_draw_dropdown_xml, @solution_xml)
-    level.add_missing_toolbox_blocks
-    assert_equal @toolbox_with_draw_dropdown_xml, level.properties['toolbox_blocks']
+  test 'blocks called with different values match' do
+    toolbox_block = make_toolbox_node @draw_50_block_xml
+    solution_block = make_solution_node @draw_100_block_xml
+    assert_blocks_match toolbox_block, solution_block
+  end
+
+  test 'math_value blocks with different values match' do
+    toolbox_block = make_toolbox_node @value_50_block_xml
+    solution_block = make_solution_node @value_100_block_xml
+    assert_blocks_match toolbox_block, solution_block
+  end
+
+  test '*_constant_dropdown blocks match *_constant blocks' do
+    toolbox_block = make_toolbox_node @draw_with_dropdown_xml
+    solution_block = make_solution_node @draw_block_xml
+    assert_blocks_match toolbox_block, solution_block
   end
 
   test 'create "NEW CATEGORY" if adding a block and using categories' do
