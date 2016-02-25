c0ee960e0dfc3606918ea13d1ce5ddcc615d87fb
Phil Bogle <phil@code.org>
2016-02-24 17:40:37 -0800

    Improve comments, renaming Gatekeeper variable to slogging

--- dashboard/app/controllers/api_controller.rb
+++ dashboard/app/controllers/api_controller.rb
@@ -84,6 +84,11 @@ class ApiController < ApplicationController
     end
   end
 
+  # Return the JSON details of the users progress on a particular script
+  # level and marks the user as having started that level. (Because of the
+  # later side effect, this should only be called when the user sees the level,
+  # to avoid spurious activity monitor warnings about the level being started
+  # but not completed.)
   def user_progress_for_stage
     response = {}
 
