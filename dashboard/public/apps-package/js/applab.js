e1add4e65d9da40681864657ddde2ec2dec36bc9
Continuous Integration <dev@code.org>
2016-02-29 06:29:48 +0000

    Automatically built.
    4af2657 Merge pull request #6702 from code-dot-org/assess (Brendan Reville)
    e9e53d5 survey2016: Update thanks text & button. (Brendan Reville)
    9a94952 survey2016: Polishing things up. (Brendan Reville)
    18cc540 Merge branch 'staging' into assess (Brendan Reville)
    82ba61e assess: Make submittable multi UI test work again. (Brendan Reville)
    506f5e0 assess: .multi can now be "submittable: true" (Brendan Reville)
    42a9aed Merge pull request #7027 from code-dot-org/move_plc_js (Mehal Shah)
    3166a20 Move the javascript for plc to the code-studio package directory (Mehal Shah)
    18dd8f7 Merge pull request #7014 from code-dot-org/congrats-app-lab (Tanya Parker)
    656a9a6 run 'bundle install' bootstrap as root. (encountered a git-gem root-permissions error when running as user via chef knife-bootstrap, possibly environment-variables related.) (Will Jordan)
    f5a3e7a code review, simplify equal comparison (Tanya Parker)
    266849d Merge pull request #7025 from code-dot-org/applab-content (Tanya Parker)
    002ef56 update app lab picture on code studio (Tanya Parker)
    1448a58 update app lab content (Tanya Parker)
    3ab5040 Merge pull request #7017 from code-dot-org/page-mode (Tanya Parker)
    371bd3b Merge pull request #7022 from code-dot-org/fix_plc_evaluation_button_groups (Mehal Shah)
    67d30d3 Forgot to group the buttons correctly when doing all of the refactoring (Mehal Shah)
    778b9fb Merge pull request #7019 from code-dot-org/cdo-nginx (Will Jordan)
    57f7ea3 update Berksfile.lock (Will Jordan)
    d967f6d update cdo-nginx documentation based on feedback (Will Jordan)
    95ea824 init.d.erb -> unicorn.sh.erb update keepalive_timeout to 30 refactor nginx_test cookbook (Will Jordan)
    f528b4a Automatically built. (Continuous Integration)
    ded796a Merge pull request #7011 from code-dot-org/rubocopBlockEndNewLine (ashercodeorg)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8db309f0aa6e31a1052a1ef7fc5d3a00e7ebe6d5b167199ea85c8cb6b38b8d76
-size 2695163
+oid sha256:ce225c7bf1044edaef730f1040800a21d2f88948f26f4ac4fc7b5dfe7147b21e
+size 2694994
