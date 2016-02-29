b879b52d797d451b0ebd3063f673df72f2c850bd
Brent Van Minnen <bjvanminnen@gmail.com>
2016-02-29 11:42:18 -0800

    address code review feedback

--- pegasus/migrations/83_create_channel_table_metadata.rb
+++ pegasus/migrations/83_create_channel_table_metadata.rb
@@ -1,16 +1,18 @@
 Sequel.migration do
   up do
-    create_table?(:channel_table_metadata, charset: 'utf8') do
-      primary_key :id, unsigned: true, null: false
-      foreign_key :app_id, index: true, null: false
-      String :table_type, size: 50, index: true, null: false
-      String :table_name, size: 50, index: true, null: false
+    unless [:staging, :adhoc, :test, :production].include?(rack_env)
+      create_table?(:channel_table_metadata, charset: 'utf8') do
+        primary_key :id, unsigned: true, null: false
+        foreign_key :app_id, index: true, null: false
+        String :table_type, size: 50, index: true, null: false
+        String :table_name, size: 50, index: true, null: false
 
-      Text :column_list
+        Text :column_list
 
-      DateTime :updated_at, null: false
+        DateTime :updated_at, null: false
 
-      index [:channel_id,:storage_id,:table_name], unique: true
+        index [:channel_id,:storage_id,:table_name], unique: true
+      end
     end
   end
 
