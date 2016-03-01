0e0454ab60eb0b81528c7e8d133a9786c4f0e1f2
Mehal Shah <mehal@code.org>
2016-02-29 17:20:28 -0800

    Undo some changes

--- dashboard/test/models/plc/enrollment_task_assignment_test.rb
+++ dashboard/test/models/plc/enrollment_task_assignment_test.rb
@@ -4,16 +4,16 @@ class Plc::EnrollmentTaskAssignmentTest < ActiveSupport::TestCase
   setup do
     @user = create :teacher
     @plc = create :plc_course
-    @learning_module = create(:plc_learning_module, name: 'Module1')
+    @learning_module1 = create(:plc_learning_module, name: 'Module1')
     @learning_module2 = create(:plc_learning_module, name: 'Module2')
-    @task1 = create(:plc_task, name: 'Task1', plc_learning_module: @learning_module)
-    @task2 = create(:plc_task, name: 'Task2', plc_learning_module: @learning_module)
+    @task1 = create(:plc_task, name: 'Task1', plc_learning_module: @learning_module1)
+    @task2 = create(:plc_task, name: 'Task2', plc_learning_module: @learning_module1)
     @task3 = create(:plc_task, name: 'Task3', plc_learning_module: @learning_module2)
   end
 
   test 'Completing tasks does not mark module / course complete until all are complete' do
     enrollment = Plc::UserCourseEnrollment.find_or_create_by(user: @user, plc_course: @plc)
-    enrollment.enroll_user_in_course_with_learning_modules([@learning_module, @learning_module2])
+    enrollment.enroll_user_in_course_with_learning_modules([@learning_module1, @learning_module2])
     task_assignments = enrollment.plc_task_assignments
 
     assert_not_equal :completed, enrollment.status
