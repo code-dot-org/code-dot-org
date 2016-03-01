8392803f59c32923d1581d66e4ff73a675c605bc
Continuous Integration <dev@code.org>
2016-03-01 00:53:53 +0000

    Automatically built.
    48ce748 Merge pull request #7050 from code-dot-org/copyrightSize (Brent Van Minnen)
    73b648a Automatically built. (Continuous Integration)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:023d915953bcf214a5757a3fe59a3e667e8dd1a9de8b93a819cdcd9eb3a597d2
-size 2695161
+oid sha256:6dd89f49522c7c17e036fd0bd753baa21b38733e29694ce9041c43e9a5d4e90f
+size 2695244
--- dashboard/public/apps-package/js/common.js
+++ dashboard/public/apps-package/js/common.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:287c071a2ec3bbea111f704df01c4b48d80c10935d4c1365fffb493aa900a072
-size 3671900
+oid sha256:f771b2ba9bbcaf3f9e9153fa8a9f6bde015ac9db7571d4e043e20a93ade25ce2
+size 3671983
