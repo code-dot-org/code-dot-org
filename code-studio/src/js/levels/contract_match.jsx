1b22d1be4aa00d5089c86de941296491f7c05ad5
Brad Buchanan <brad@code.org>
2016-03-02 13:49:27 -0800

    Fix contract_match.jsx React warnings.
    Warning: Failed form propType: You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`. Check the render method of `TypeChooser`.
    Fix: Changed select "value" to "defaultValue".
    
    Warning: Each child in an array or iterator should have a unique "key" prop. Check the render method of `exports`. See https://fb.me/react-warning-keys for more information.
    Fix: Add "key" property to <div> returned by map function.
    
    FYI @bcjordan

--- code-studio/src/js/levels/contract_match.jsx
+++ code-studio/src/js/levels/contract_match.jsx
@@ -188,7 +188,7 @@ $(window).load(function () {
       });
       var typeChoiceNodes = sortedDomains.map(function (object) {
         return (
-          <div className="clear">
+          <div className="clear" key={object.key}>
             <TypeChooser
               order={object.order}
               type={object.type}
@@ -216,7 +216,7 @@ $(window).load(function () {
         backgroundColor: typesToColors[this.props.type]
       };
       return (
-        <select value={this.props.type} style={divStyle}>
+        <select defaultValue={this.props.type} style={divStyle}>
           <option data-color={typesToColors[blockValueType.NUMBER]} value={blockValueType.NUMBER}>{blockValueType.NUMBER}</option>
           <option data-color={typesToColors[blockValueType.STRING]} value={blockValueType.STRING}>{blockValueType.STRING}</option>
           <option data-color={typesToColors[blockValueType.IMAGE]} value={blockValueType.IMAGE}>{blockValueType.IMAGE}</option>
