2667b2b28fb99240eaf8accf6cc559cfab250d4f
Continuous Integration <dev@code.org>
2016-03-02 21:29:14 +0000

    Automatically built.
    0477ac0 Merge pull request #7052 from code-dot-org/throttle-data-requests (David Bailey)

--- dashboard/public/apps-package/js/applab.js
+++ dashboard/public/apps-package/js/applab.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca0ff633e6f42b1d5ad3df1de16dc93ed25685d09932ac1ad6ed636eccf51096
-size 2706947
+oid sha256:0f22b2511cc01a398170310980cc8f8e002298bebba8694f9fac8c7d8f834187
+size 2709210
