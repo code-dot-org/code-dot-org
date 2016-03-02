a3cae1851db5175d2caef7b7f4d610a49a60677a
Brad Buchanan <brad@code.org>
2016-03-02 13:39:36 -0800

    Remove another case of renderToStaticMarkup.
    Again replacing with creating a mountPoint and using ReactDOM.render() to render inside it.
    
    FYI @joshlory

--- dashboard/app/assets/javascripts/progress.js
+++ dashboard/app/assets/javascripts/progress.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91a1ae02dacec8811bcd831bd4832cbf37fd02a0780e25a961a84847d81b855a
-size 4360
+oid sha256:02b04574780cf3b7ab5211193078b5d4da8aa1b06b8086d89b2aaa8de01e3cbc
+size 4473
