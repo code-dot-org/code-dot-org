ae35044a15a5eb10505cc0381be2ad74b6f1af80
David Bailey <davidsbailey@users.noreply.github.com>
2016-03-03 21:22:21 -0800

    test the array of limits and periods generated

--- shared/test/test_rack_attack.rb
+++ shared/test/test_rack_attack.rb
@@ -24,6 +24,24 @@ class RackAttackTest < Minitest::Test
     @channels.delete "/v3/channels/#{@channel_id}"
   end
 
+  def test_limits
+    expected_limits = [[1200, 15], [2400, 60], [4800, 240]]
+    actual_limits = Rack::Attack.limits Rack::Attack::MAX_TABLE_READS_PER_MIN
+    assert_equal expected_limits, actual_limits, "Max table read limits and periods are set correctly"
+
+    expected_limits = [[2400, 15], [4800, 60], [9600, 240]]
+    actual_limits = Rack::Attack.limits Rack::Attack::MAX_TABLE_WRITES_PER_MIN
+    assert_equal expected_limits, actual_limits, "Max table write limits and periods are set correctly"
+
+    expected_limits = [[2400, 15], [4800, 60], [9600, 240]]
+    actual_limits = Rack::Attack.limits Rack::Attack::MAX_PROPERTY_READS_PER_MIN
+    assert_equal expected_limits, actual_limits, "Max property read limits and periods are set correctly"
+
+    expected_limits = [[2400, 15], [4800, 60], [9600, 240]]
+    actual_limits = Rack::Attack.limits Rack::Attack::MAX_PROPERTY_WRITES_PER_MIN
+    assert_equal expected_limits, actual_limits, "Max property write limits and periods are set correctly"
+  end
+
   def test_rate_limit
     read_records
     assert last_response.ok?, '1st read succeeds'
