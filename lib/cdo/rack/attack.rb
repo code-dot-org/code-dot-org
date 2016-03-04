0b371a55777c7f5f1144e21d1c3f2d615417233f
David Bailey <davidsbailey@users.noreply.github.com>
2016-03-03 21:37:32 -0800

    extract rate limits as CDO params

--- lib/cdo/rack/attack.rb
+++ lib/cdo/rack/attack.rb
@@ -4,12 +4,7 @@ require 'redis'
 require 'active_support/core_ext/numeric/time'
 
 class Rack::Attack
-  # maximum requests per minute over a 4-minute stretch.
   # Rack::Attack returns a 429 for requests that exceed the rate limit.
-  MAX_TABLE_READS_PER_SEC = 20
-  MAX_TABLE_WRITES_PER_SEC = 40
-  MAX_PROPERTY_READS_PER_SEC = 40
-  MAX_PROPERTY_WRITES_PER_SEC = 40
 
   redis_url = CDO.geocoder_redis_url || 'redis://localhost:6379'
   cache.store = StoreProxy::RedisStoreProxy.new(Redis.new(url: redis_url))
@@ -32,7 +27,7 @@ class Rack::Attack
     end
   end
 
-  limits(MAX_TABLE_READS_PER_SEC).each do |limit, period|
+  limits(CDO.max_table_reads_per_sec).each do |limit, period|
     throttle("shared-tables/reads/#{period}", :limit => limit, :period => period.seconds) do |req|
       # extract the channel id for get requests to shared tables. Only match full
       # table reads, because createRecord commands get redirected to individual row
@@ -41,7 +36,7 @@ class Rack::Attack
     end
   end
 
-  limits(MAX_TABLE_WRITES_PER_SEC).each do |limit, period|
+  limits(CDO.max_table_writes_per_sec).each do |limit, period|
     throttle("shared-tables/writes/#{period}", :limit => limit, :period => period.seconds) do |req|
       # extract the channel id for create, update and delete requests to shared tables.
       # TODO(dave): Make imports and other table/column operations not fall into this bucket
@@ -50,14 +45,14 @@ class Rack::Attack
     end
   end
 
-  limits(MAX_PROPERTY_READS_PER_SEC).each do |limit, period|
+  limits(CDO.max_property_reads_per_sec).each do |limit, period|
     throttle("shared-properties/reads/#{period}", :limit => limit, :period => period.seconds) do |req|
       # extract the channel id for reads of shared properties.
       req.get? && %r{^/v3/shared-properties/([^/]+)/[^/]+$}.match(req.path).try(:[], 0)
     end
   end
 
-  limits(MAX_PROPERTY_WRITES_PER_SEC).each do |limit, period|
+  limits(CDO.max_property_writes_per_sec).each do |limit, period|
     throttle("shared-properties/writes/#{period}", :limit => limit, :period => period.seconds) do |req|
       # extract the channel id for writes to shared properties.
       req.write? && %r{^/v3/shared-properties/([^/]+)/[^/]+$}.match(req.path).try(:[], 0)
--- shared/test/test_rack_attack.rb
+++ shared/test/test_rack_attack.rb
@@ -26,19 +26,19 @@ class RackAttackTest < Minitest::Test
 
   def test_limits
     expected_limits = [[1200, 15], [2400, 60], [4800, 240]]
-    actual_limits = Rack::Attack.limits Rack::Attack::MAX_TABLE_READS_PER_SEC
+    actual_limits = Rack::Attack.limits CDO.max_table_reads_per_sec
     assert_equal expected_limits, actual_limits, "Max table read limits and periods are set correctly"
 
     expected_limits = [[2400, 15], [4800, 60], [9600, 240]]
-    actual_limits = Rack::Attack.limits Rack::Attack::MAX_TABLE_WRITES_PER_SEC
+    actual_limits = Rack::Attack.limits CDO.max_table_writes_per_sec
     assert_equal expected_limits, actual_limits, "Max table write limits and periods are set correctly"
 
     expected_limits = [[2400, 15], [4800, 60], [9600, 240]]
-    actual_limits = Rack::Attack.limits Rack::Attack::MAX_PROPERTY_READS_PER_SEC
+    actual_limits = Rack::Attack.limits CDO.max_property_reads_per_sec
     assert_equal expected_limits, actual_limits, "Max property read limits and periods are set correctly"
 
     expected_limits = [[2400, 15], [4800, 60], [9600, 240]]
-    actual_limits = Rack::Attack.limits Rack::Attack::MAX_PROPERTY_WRITES_PER_SEC
+    actual_limits = Rack::Attack.limits CDO.max_property_writes_per_sec
     assert_equal expected_limits, actual_limits, "Max property write limits and periods are set correctly"
   end
 
