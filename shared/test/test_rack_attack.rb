a5c1934bf9bc15f095a252fdf69cdc0f340439af
David Bailey <davidsbailey@users.noreply.github.com>
2016-03-04 14:17:38 -0800

    add a test for exponential backoff.

--- shared/test/test_rack_attack.rb
+++ shared/test/test_rack_attack.rb
@@ -4,6 +4,16 @@ require 'channels_api'
 require 'tables_api'
 require 'properties_api'
 require "fakeredis"
+require "timecop"
+
+# The redis cache is a class property of Rack::Attack. This means that
+# the counts and time (as modified by timecop) used by each throttling rule
+# (table reads, etc) for each channel id are shared across all tests.
+#
+# Today, all tests cases in shared/test use the same channel id because of the way
+# pegasus DB transaction rollbacks are done. If we want to add multiple tests for each
+# throttling rule, The simplest way to isolate them would be to obtain a different
+# channel id in each test.
 
 # Allow 3 requests of each type in the first 15 seconds
 REDUCED_RATE_LIMIT_FOR_TESTING = 3.0 / 60
@@ -62,15 +72,52 @@ class RackAttackTest < Minitest::Test
     assert_equal expected_limits, actual_limits, "Reduced rate limits for testing are set correctly"
   end
 
-  def test_table_read_limit_enforced
-    read_records
-    assert last_response.ok?, '1st read succeeds'
-    read_records
-    assert last_response.ok?, '2nd read succeeds'
-    read_records
-    assert last_response.ok?, '3rd read succeeds'
-    read_records
-    assert_equal 429, last_response.status, "4th read is rate limited."
+  SUCCESSFUL = 200
+  RATE_LIMITED = 429
+
+  def test_table_read_limits_with_exponential_backoff
+    Timecop.freeze rounded_time_now
+
+    assert_read_records 1, SUCCESSFUL
+    assert_read_records 2, SUCCESSFUL
+    assert_read_records 3, SUCCESSFUL
+    # don't increment the index here because throttled requests don't affect the counts.
+    assert_read_records 3, RATE_LIMITED
+
+    Timecop.travel 16
+
+    assert_read_records 4, SUCCESSFUL, '15s rate limit expires'
+    assert_read_records 5, SUCCESSFUL
+    assert_read_records 6, SUCCESSFUL
+    assert_read_records 6, RATE_LIMITED, '15s rate limit takes effect a second time'
+
+    Timecop.travel 16 # elapsed time: 32s
+    assert_read_records 6, RATE_LIMITED, '60s rate limit takes effect'
+
+    Timecop.travel 61 # elapsed time: 93s
+
+    assert_read_records 7, SUCCESSFUL
+    assert_read_records 8, SUCCESSFUL
+    assert_read_records 9, SUCCESSFUL
+
+    # avoid triggering 15s limit. elapsed time: 107s
+    Timecop.travel 16
+
+    assert_read_records 10, SUCCESSFUL
+    assert_read_records 11, SUCCESSFUL
+    assert_read_records 12, SUCCESSFUL
+
+    assert_read_records 12, RATE_LIMITED, '60s rate limit takes effect a second time'
+
+    Timecop.travel 61 # elapsed time: 168s
+
+    assert_read_records 12, RATE_LIMITED, '240s rate limit takes effect'
+
+    Timecop.travel 241
+
+    assert_read_records 13, SUCCESSFUL, '240s rate limit expires'
+  ensure
+    Timecop.return
   end
 
   def test_table_write_limit_enforced
@@ -111,6 +158,22 @@ class RackAttackTest < Minitest::Test
 
   # Helper methods
 
+  # Rack::Attack::Cache computes expiry time in a way that may allow counters
+  # to expire in a shorter amount of time than their corresponding period:
+  #
+  #     expiry_time = (Time.now.i % period) + period + 1
+  #
+  # Round off to the last time that was divisible by 4 minutes so that expiry times are
+  # initially approximately equal to the length of the period.
+  #
+  # Do not go more than 15 minutes back or forward, or AWS will complain
+  # with Aws::DynamoDB::Errors::InvalidSignatureException.
+  def rounded_time_now
+    now = Time.now
+    rounded_min = now.min - (now.min % 4)
+    Time.new(now.year, now.month, now.day, now.hour, rounded_min)
+  end
+
   def table_path
     "/v3/shared-tables/#{@channel_id}/#{TABLE_NAME}"
   end
@@ -127,6 +190,11 @@ class RackAttackTest < Minitest::Test
 
   def read_records
     get table_path
+  end
+
+  def assert_read_records(index, expected_status, msg = nil)
+    read_records
+    assert_equal expected_status, last_response.status, "request #{index} expected #{expected_status} #{msg}"
   end
 
   def update_record(id)
