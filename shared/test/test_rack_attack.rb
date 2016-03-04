5ac147277808f1e6c37758956aaddc5d25f5a3b2
David Bailey <davidsbailey@users.noreply.github.com>
2016-03-03 23:11:28 -0800

    simplify init sequence

--- shared/test/test_rack_attack.rb
+++ shared/test/test_rack_attack.rb
@@ -24,17 +24,18 @@ class RackAttackTest < Minitest::Test
   TABLE_NAME = 'stub_rack_attack_table'
 
   def build_rack_mock_session
-    @session = Rack::MockSession.new(Rack::Attack.new(TablesApi.new(PropertiesApi)), 'studio.code.org')
+    # TODO(dave): chain middleware components like this in other middleware tests to reduce complexity.
+    middleware = Rack::Attack.new(ChannelsApi.new(PropertiesApi.new(TablesApi)))
+    @session = Rack::MockSession.new(middleware, 'studio.code.org')
   end
 
   def setup
-    @channels = Rack::Test::Session.new(Rack::MockSession.new(ChannelsApi, "studio.code.org"))
-    @channels.post '/v3/channels', {}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
-    @channel_id = @channels.last_response.location.split('/').last
+    post '/v3/channels', {}.to_json, 'CONTENT_TYPE' => 'application/json;charset=utf-8'
+    @channel_id = last_response.location.split('/').last
   end
 
   def teardown
-    @channels.delete "/v3/channels/#{@channel_id}"
+    delete "/v3/channels/#{@channel_id}"
   end
 
   # non-stubbed rate limits from CDO are used here
