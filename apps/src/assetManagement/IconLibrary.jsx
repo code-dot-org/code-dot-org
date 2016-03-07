3e715f8e625094185a70864ba4f9391caf7e9f07
Josh Lory <josh.lory@code.org>
2016-03-07 12:44:52 -0800

    Add IconLibrary skeleton

---
+++ apps/src/assetManagement/IconLibrary.jsx
@@ -0,0 +1,13 @@
+/**
+ * A component for managing icons.
+ */
+module.exports = React.createClass({
+  propTypes: {
+  },
+
+  render: function () {
+    return (
+      <p>Icon Library</p>
+    );
+  }
+});
--- apps/src/assetManagement/ImagePicker.jsx
+++ apps/src/assetManagement/ImagePicker.jsx
@@ -1,4 +1,5 @@
 var AssetManager = require('./AssetManager.jsx');
+var IconLibrary = require('./IconLibrary.jsx');
 
 /**
  * A component for managing hosted assets.
@@ -11,25 +12,41 @@ module.exports = React.createClass({
     uploadsEnabled: React.PropTypes.bool.isRequired
   },
 
+  getInitialState: function () {
+    return {mode: 'files'};
+  },
+
+  setIconMode: function () {
+    this.setState({mode: 'icons'});
+  },
+
+  setFileMode: function () {
+    this.setState({mode: 'files'});
+  },
+
   render: function () {
 
     var title = this.props.assetChosen ?
       <p className="dialog-title">Choose Assets</p> :
       <p className="dialog-title">Manage Assets</p>;
 
+    var body = this.state.mode === 'files' ?
+      <AssetManager
+        assetChosen={this.props.assetChosen}
+        typeFilter={this.props.typeFilter}
+        channelId={this.props.channelId}
+        uploadsEnabled={this.props.uploadsEnabled}/> :
+      <IconLibrary/>;
+
     return (
       <div className="modal-content" style={{margin: "0 0 0 -10px"}}>
         {title}
         <div>
-          <p style={{float: 'left', margin: '0 20px 0 0', fontSize: '14px'}}>My Files</p>
-          <p style={{fontSize: '16px', margin: '0', color: '#999'}}>Icons</p>
+          <p onClick={this.setFileMode} style={{float: 'left', margin: '0 20px 0 0', fontSize: '14px', cursor: 'pointer'}}>My Files</p>
+          <p onClick={this.setIconMode} style={{fontSize: '16px', margin: '0', color: '#999', cursor: 'pointer'}}>Icons</p>
           <hr style={{borderColor: '#7665a0', margin: '5px 0'}}/>
         </div>
-        <AssetManager
-          assetChosen={this.props.assetChosen}
-          typeFilter={this.props.typeFilter}
-          channelId={this.props.channelId}
-          uploadsEnabled={this.props.uploadsEnabled}/>
+        {body}
       </div>
     );
   }
