f252377f6672c6844e575ce00046952dd5b88e95
Josh Lory <josh.lory@code.org>
2016-03-07 11:52:13 -0800

    Add toggle for icon library

--- apps/src/assetManagement/ImagePicker.jsx
+++ apps/src/assetManagement/ImagePicker.jsx
@@ -20,6 +20,11 @@ module.exports = React.createClass({
     return (
       <div className="modal-content" style={{margin: "0 0 0 -10px"}}>
         {title}
+        <div>
+          <p style={{float: 'left', margin: '0 20px 0 0', fontSize: '14px'}}>My Files</p>
+          <p style={{fontSize: '16px', margin: '0', color: '#999'}}>Icons</p>
+          <hr style={{borderColor: '#7665a0', margin: '5px 0'}}/>
+        </div>
         <AssetManager
           assetChosen={this.props.assetChosen}
           typeFilter={this.props.typeFilter}
