392e6fa284c3a3223abc9f54d62b434a841269cd
Phil Bogle <phil@code.org>
2016-03-08 13:03:14 -0800

    Add change listener functionality to dcdo and data_store_cache

--- lib/dynamic_config/datastore_cache.rb
+++ lib/dynamic_config/datastore_cache.rb
@@ -11,10 +11,32 @@ class DatastoreCache
     @datastore = datastore
     @cache_expiration = cache_expiration
 
+    # A list of change listeners.
+    @listeners = []
+
     # Note we intentionally do this before spawning the background thread
     # to make sure the cache is seeded successfully on init
     update_cache
     @update_thread = spawn_update_thread
+  end
+
+  # Adds a listener that will be invoked whenever the store changes.
+  # The listener must an object support a non-blocking
+  # on_change() method which will be invoked in an arbitrary thread.
+  def add_change_listener(listener)
+    @listeners << listener
+  end
+
+  # Notifies each of the listeners that the store might have changed,
+  # catching and logging exceptions.
+  def notify_change_listeners
+    @listeners.each do |listener|
+      begin
+        listener.on_change()
+      rescue => exception
+        Honeybadger.notify(exception)
+      end
+    end
   end
 
   # Gets the data associated with a given key
@@ -32,6 +54,7 @@ class DatastoreCache
     raise ArgumentError unless key.is_a? String
     @datastore.set(key, value)
     set_local(key, value)
+    notify_change_listeners()
   end
 
   # Return all cached elements
@@ -43,6 +66,7 @@ class DatastoreCache
   def clear
     @cache = {}
     @datastore.clear
+    notify_change_listeners()
   end
 
   # When unicorn preload the app and then forks worker processes the update_thread
@@ -54,12 +78,18 @@ class DatastoreCache
   # Pulls all values from the datastore and populates the local cache
   def update_cache
     tries ||= 3
-    @datastore.all.each do |k, v|
-      set_local(k, v)
+    updated = false
+    begin
+      @datastore.all.each do |key, value|
+        old_value = @cache[key]
+        set_local(key, value)
+        updated = true if value != old_value
+      end
+      notify_change_listeners if updated
+    rescue => exc
+      retry unless (tries -= 1).zero?
+      Honeybadger.notify(exc)
     end
-  rescue => exc
-    retry unless (tries -= 1).zero?
-    Honeybadger.notify(exc)
   end
 
   private
