a1e9b40bf94df83cd4eb875586060e4a529dd2f7
Josh Lory <josh.lory@code.org>
2016-03-07 22:29:53 -0800

    Initial searchable icon library component
    Each icon can be searched by name, or by one of its keyword aliases. The matching part of the name or keyword is highlighted in each match.

---
+++ apps/src/assetManagement/IconList.jsx
@@ -0,0 +1,51 @@
+var IconListEntry = require('./IconListEntry.jsx');
+
+var icons = require('./icons');
+var iconKeys = Object.keys(icons);
+var MAX_RESULTS = 50;
+
+/**
+ * A component for managing icons.
+ */
+module.exports = React.createClass({
+  propTypes: {
+    search: React.PropTypes.string.isRequired
+  },
+
+  render: function () {
+    var query = new RegExp(this.props.search), results = {};
+
+    for (var i = 0; i < iconKeys.length; i++) {
+      var keyword = iconKeys[i];
+      if (query.test(keyword)) {
+        var matches = icons[keyword];
+        for (var match in matches) {
+          if (!results[match]) {
+            results[match] = {
+              unicode: matches[match],
+              alt: keyword
+            };
+          }
+        }
+      }
+    }
+
+    var list = [];
+
+    for (var iconId in results) {
+      var result = results[iconId];
+      list.push(<IconListEntry
+        unicode={result.unicode}
+        iconId={iconId}
+        altMatch={result.alt}
+        search={this.props.search}/>
+      );
+    }
+
+    return (
+      <div>
+        {list}
+      </div>
+    );
+  }
+});
---
+++ apps/src/assetManagement/IconListEntry.jsx
@@ -0,0 +1,37 @@
+/**
+ * A component for managing icons.
+ */
+module.exports = React.createClass({
+  propTypes: {
+    unicode: React.PropTypes.string.isRequired,
+    iconId: React.PropTypes.string.isRequired,
+    altMatch: React.PropTypes.string.isRequired,
+    search: React.PropTypes.string.isRequired
+  },
+
+  highlightSearch: function (str) {
+    var offset = str.indexOf(this.props.search);
+    if (offset === -1) {
+      return str;
+    }
+    var left = str.substr(0, offset), right = str.substr(offset + this.props.search.length);
+    return [left, <span style={{backgroundColor: '#ffc'}}>{this.props.search}</span>, right];
+  },
+
+  render: function () {
+
+    var query = new RegExp(this.props.search), altMatchText;
+    if (!query.test(this.props.iconId)) {
+      // We matched based on an alternate keyword, show that keyword next to the icon ID.
+      altMatchText = <p style={{float: 'left', fontSize: '13px', color: '#999'}}>({this.highlightSearch(this.props.altMatch)})</p>;
+    }
+
+    return (
+      <div style={{float: 'left', width: '33%', height: '35px'}}>
+        <i className={'fa fa-' + this.props.iconId} style={{float: 'left', fontSize: '24px'}}></i>
+        <p style={{float: 'left', margin: '0 5px', fontSize: '13px', color: '#000'}}>{this.highlightSearch(this.props.iconId)}</p>
+        {altMatchText}
+      </div>
+    );
+  }
+});
