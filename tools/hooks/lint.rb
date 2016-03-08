5b8f3068900543c1df6fbfd77e8f0c98478621da
Brent Van Minnen <bjvanminnen@gmail.com>
2016-03-08 14:35:57 -0800

    join files with spaces

--- tools/hooks/lint.rb
+++ tools/hooks/lint.rb
@@ -38,7 +38,7 @@ def run_rubocop(files)
 end
 
 def run_eslint(files)
-  run("./node_modules/.bin/eslint -c .eslintrc.js #{files.join(",")}", APPS_DIR)
+  run("./node_modules/.bin/eslint -c .eslintrc.js #{files.join(" ")}", APPS_DIR)
 end
 
 def run_haml(files)
