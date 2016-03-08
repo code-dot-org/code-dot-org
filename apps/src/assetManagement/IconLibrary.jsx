0fe52345d5c392d5d8a540c9411d338585573bd7
Josh Lory <josh.lory@code.org>
2016-03-07 22:29:53 -0800

    Initial searchable icon library component
    Each icon can be searched by name, or by one of its keyword aliases. The matching part of the name or keyword is highlighted in each match.

--- apps/src/assetManagement/IconLibrary.jsx
+++ apps/src/assetManagement/IconLibrary.jsx
@@ -1,3 +1,5 @@
+var IconList = require('./IconList.jsx');
+
 /**
  * A component for managing icons.
  */
@@ -5,9 +7,20 @@ module.exports = React.createClass({
   propTypes: {
   },
 
+  getInitialState: function() {
+    return {search: ''};
+  },
+
+  search: function (e) {
+    this.setState({search: e.target.value});
+  },
+
   render: function () {
     return (
-      <p>Icon Library</p>
+      <div>
+        <input onChange={this.search}></input>
+        <IconList search={this.state.search}/>
+      </div>
     );
   }
 });
