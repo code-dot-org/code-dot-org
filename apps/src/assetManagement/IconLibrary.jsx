82fbfdce55ded668cbabe6e43f8ab64dc10e14cd
Josh Lory <josh.lory@code.org>
2016-03-08 14:17:35 -0800

    Make icon library layout match the mocks

--- apps/src/assetManagement/IconLibrary.jsx
+++ apps/src/assetManagement/IconLibrary.jsx
@@ -18,7 +18,21 @@ module.exports = React.createClass({
   render: function () {
     return (
       <div>
-        <input onChange={this.search}></input>
+        <div style={{float: 'right', position: 'relative', margin: '10px 0'}}>
+          <input onChange={this.search} style={{
+            width: '300px',
+            border: '1px solid #999',
+            borderRadius: '4px',
+            padding: '3px 7px'
+          }}/>
+          <i className="fa fa-search" style={{
+            position: 'absolute',
+            right: '5px',
+            top: '5px',
+            fontSize: '16px',
+            color: '#999'
+          }}/>
+        </div>
         <IconList search={this.state.search}/>
       </div>
     );
