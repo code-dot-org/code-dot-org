4575e955d2562d1a08f4bee70b374bd4d0d66c99
Asher Kach <asher@code.org>
2016-03-09 13:49:58 -0600

    Fix tests and allow copy-and-paste (use spans).

--- dashboard/test/controllers/admin_hoc_controller_test.rb
+++ dashboard/test/controllers/admin_hoc_controller_test.rb
@@ -4,8 +4,10 @@ class AdminHocControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
   setup do
-    # Stub the DB[:forms] table (used by :hoc_signups).
+    # Stub the DB[:forms] table (used by :event_signups).
     DB.stubs(:[]).returns(stub(:where => stub(:group => stub(:group_and_count => stub(:order => stub(:all => []))))))
+    # Stub the Properties table (used by :students_served).
+    Properties.stubs(:get).returns(nil)
 
     @admin = create(:admin)
     sign_in(@admin)
