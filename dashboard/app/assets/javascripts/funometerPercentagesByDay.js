51cafcbffd3677b398ce7087b547fc1104842884
Asher Kach <asher@code.org>
2016-03-09 13:00:26 -0600

    WIP.

--- dashboard/app/assets/javascripts/funometerPercentagesByDay.js
+++ dashboard/app/assets/javascripts/funometerPercentagesByDay.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acdaa3dc8b82d3da76bd151591885ae4c38b6cfd8ebc4d7e6351dab2c8396c19
-size 1036
+oid sha256:2427c2c7e42684763828b580d393d928bd176e243dc3008e47f8c50d532f18cd
+size 1059
