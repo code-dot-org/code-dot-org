dcc6c4b2e3315d456e6f5c5969e40b716f96f274
Laurel Fan <laurel@code.org>
2016-03-10 15:50:52 -0800

    move admin/assume_identity to new AdminUsersController; add confirm_email admin tool

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -211,8 +211,11 @@ Dashboard::Application.routes.draw do
   get '/admin/feature_mode', :to => 'feature_mode#show', as: 'feature_mode'
   post '/admin/feature_mode', :to => 'feature_mode#update', as: 'feature_mode_update'
 
-  get '/admin/assume_identity', to: 'reports#assume_identity_form', as: 'assume_identity_form'
-  post '/admin/assume_identity', to: 'reports#assume_identity', as: 'assume_identity'
+  get '/admin/assume_identity', to: 'admin_users#assume_identity_form', as: 'assume_identity_form'
+  post '/admin/assume_identity', to: 'admin_users#assume_identity', as: 'assume_identity'
+  get '/admin/confirm_email', to: 'admin_users#confirm_email_form', as: 'confirm_email_form'
+  post '/admin/confirm_email', to: 'admin_users#confirm_email', as: 'confirm_email'
+
   get '/admin/gatekeeper', :to => 'dynamic_config#gatekeeper_show', as: 'gatekeeper_show'
   post '/admin/gatekeeper/delete', :to => 'dynamic_config#gatekeeper_delete', as: 'gatekeeper_delete'
   post '/admin/gatekeeper/set', :to => 'dynamic_config#gatekeeper_set', as: 'gatekeeper_set'
