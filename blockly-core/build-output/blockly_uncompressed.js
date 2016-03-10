392bfaf0da62ebe0f55177c46beb19d9072ee421
Continuous Integration <dev@code.org>
2016-03-10 22:57:14 +0000

    Automatically built.
    a1285ed Merge pull request #7229 from code-dot-org/crosshair-read-only (Charing Wong)
    79677a7 dropbox changes.. just scooping fixed cd-donors.csv. (Continuous Integration)

--- blockly-core/build-output/blockly_uncompressed.js
+++ blockly-core/build-output/blockly_uncompressed.js
@@ -20286,6 +20286,9 @@ Blockly.FunctionEditor.prototype.renameParameter = function(oldName, newName) {
     if(block.firstElementChild && block.firstElementChild.textContent === oldName) {
       block.firstElementChild.textContent = newName
     }
+  });
+  this.forEachParameterGetBlock(oldName, function(block) {
+    block.setTitleValue(newName, "VAR")
   })
 };
 Blockly.FunctionEditor.prototype.changeParameterTypeInFlyoutXML = function(name, newType) {
@@ -20307,6 +20310,9 @@ Blockly.FunctionEditor.prototype.removeParameter = function(nameToRemove) {
   keysToDelete.forEach(function(key) {
     this.orderedParamIDsToBlocks_.remove(key)
   }, this);
+  this.forEachParameterGetBlock(nameToRemove, function(block) {
+    block.dispose(true, false)
+  });
   this.refreshParamsEverywhere()
 };
 Blockly.FunctionEditor.prototype.refreshParamsEverywhere = function() {
@@ -20337,6 +20343,13 @@ Blockly.FunctionEditor.prototype.paramsAsParallelArrays_ = function() {
     }
   }, this);
   return{paramNames:paramNames, paramIDs:paramIDs, paramTypes:paramTypes}
+};
+Blockly.FunctionEditor.prototype.forEachParameterGetBlock = function(paramName, callback) {
+  this.functionDefinitionBlock.getDescendants().forEach(function(block) {
+    if(block.type == "parameters_get" && Blockly.Names.equals(paramName, block.getTitleValue("VAR"))) {
+      callback(block)
+    }
+  })
 };
 Blockly.FunctionEditor.prototype.show = function() {
   this.ensureCreated_();
