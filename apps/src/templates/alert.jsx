7d400e8e6ec67a7a6a8cab946e6e2d13c9f71901
Brent Van Minnen <bjvanminnen@gmail.com>
2016-03-10 10:09:10 -0800

    alert uses entirely inlined styles

--- apps/src/templates/alert.jsx
+++ apps/src/templates/alert.jsx
@@ -4,29 +4,58 @@
 /**
  * Simple boot-strapped style alert.
  */
-module.exports = React.createClass({
+var Alert = React.createClass({
   propTypes: {
     body: React.PropTypes.oneOfType([
       React.PropTypes.string,
       React.PropTypes.element
     ]).isRequired,
-    className: React.PropTypes.string,
     style: React.PropTypes.object,
     onClose: React.PropTypes.func.isRequired
   },
 
   render: function () {
-    var style = $.extend({}, {
-      position: 'absolute',
-      zIndex: 1000
-    }, this.props.style);
+    var styles = {
+      main: $.extend({}, {
+        position: 'absolute',
+        zIndex: 1000
+      }, this.props.style),
+      child: {
+        // from bootstrap's alert
+        padding: '8px 35px 8px 14px',
+        marginBottom: 20,
+        textShadoow: '0 1px 0 rgba(255, 255, 255, 0.5)',
+        border: '1px solid #fbeed5',
+        borderRadius: 4,
+        // from alert-error
+        backgroundColor: '#f2dede', // TODO - put colors in shared location?
+        color: '#b94a48'
+      },
+      closeButton: {
+        margin: 0,
+        // from bootstrap's close (note: we've lost :hover)
+        padding: 0,
+        cursor: 'pointer',
+        background: 'transparent',
+        border: 0,
+        WebkitAppearance: 'none',
+        float: 'right',
+        fontSize: 20,
+        fontWeight: 'bold',
+        lineHeight: '20px',
+        color: 'black',
+        textShadow: '0 1px 0 white',
+        opacity: 0.2,
+        position: 'relative',
+        top: -2,
+        right: -21
+      }
+    };
 
     return (
-      <div style={style}>
-        <div className={"alert fade in " + (this.props.className || '')}>
-          <button type="button"
-            className="alert-button close"
-            style={{ margin: 0 }}>
+      <div style={styles.main}>
+        <div style={styles.child}>
+          <button type="button" style={styles.closeButton}>
             <span onClick={this.props.onClose}>&times;</span>
           </button>
           {this.props.body}
@@ -35,3 +64,5 @@ module.exports = React.createClass({
     );
   }
 });
+
+module.exports = Alert;
