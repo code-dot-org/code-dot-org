edfc1bc35d28ef112fad4bcb6f35739c60e4ac63
Brad Buchanan <brad@code.org>
2016-03-14 15:56:58 -0700

    Make toggle hide code mode and render empty div

--- apps/src/gamelab/GamelabView.jsx
+++ apps/src/gamelab/GamelabView.jsx
@@ -1,7 +1,9 @@
 /** @file Top-level view for Gamelab */
 'use strict';
 
+var connect = require('react-redux').connect;
 var ConnectedStudioAppWrapper = require('../templates/ConnectedStudioAppWrapper.jsx');
+var GameLabInterfaceMode = require('./constants').GameLabInterfaceMode;
 var GameLabVisualizationHeader = require('./GameLabVisualizationHeader.jsx');
 var ProtectedStatefulDiv = require('../templates/ProtectedStatefulDiv.jsx');
 
@@ -10,7 +12,9 @@ var ProtectedStatefulDiv = require('../templates/ProtectedStatefulDiv.jsx');
  */
 var GamelabView = React.createClass({
   propTypes: {
-    isReadOnlyWorkspace: React.PropTypes.bool.isRequired,
+    interfaceMode: React.PropTypes.bool.isRequired,
+    isEmbedView: React.PropTypes.bool.isRequired,
+    isShareView: React.PropTypes.bool.isRequired,
     renderCodeWorkspace: React.PropTypes.func.isRequired,
     renderVisualizationColumn: React.PropTypes.func.isRequired,
     onMount: React.PropTypes.func.isRequired
@@ -21,27 +25,57 @@ var GamelabView = React.createClass({
   },
 
   render: function () {
-    var leftColumnHeader;
-    if (!this.props.isReadOnlyWorkspace) {
-      leftColumnHeader = <GameLabVisualizationHeader
-          isEditingProject={this.props.isEditingProject}
-          screenIds={this.props.screenIds}
-          onViewDataButton={this.props.onViewDataButton}
-          onScreenCreate={this.props.onScreenCreate} />;
+    return (
+      <ConnectedStudioAppWrapper>
+        {this.renderCodeMode()}
+        {this.renderAnimationMode()}
+      </ConnectedStudioAppWrapper>
+    );
+  },
+
+  renderCodeMode: function () {
+    // Code mode contains protected (non-React) content.  We have to always
+    // render it, so when we're not in code mode use CSS to hide it.
+    var codeModeStyle = {};
+    if (this.props.interfaceMode !== GameLabInterfaceMode.CODE) {
+      codeModeStyle.display = 'none';
     }
 
     return (
-      <ConnectedStudioAppWrapper>
+      <div style={codeModeStyle}>
         <div id="visualizationColumn">
-          {leftColumnHeader}
+          {this.shouldShowHeader() && <GameLabVisualizationHeader/>}
           <ProtectedStatefulDiv renderContents={this.props.renderVisualizationColumn} />
         </div>
         <ProtectedStatefulDiv id="visualizationResizeBar" className="fa fa-ellipsis-v" />
         <ProtectedStatefulDiv id="codeWorkspace">
           <ProtectedStatefulDiv id="codeWorkspaceWrapper" renderContents={this.props.renderCodeWorkspace}/>
         </ProtectedStatefulDiv>
-      </ConnectedStudioAppWrapper>
+      </div>
     );
+  },
+
+  renderAnimationMode: function () {
+    // Don't render animation mode when we're not in that mode (pure React!)
+    if (this.props.interfaceMode !== GameLabInterfaceMode.ANIMATION) {
+      return undefined;
+    }
+
+    return (
+      <div>
+        <GameLabVisualizationHeader/>
+      </div>
+    );
+  },
+
+  shouldShowHeader: function () {
+    return !(this.props.isEmbedView || this.props.isShareView);
   }
 });
-module.exports = GamelabView;
+module.exports = connect(function propsFromStore(state) {
+  return {
+    interfaceMode: state.interfaceMode,
+    isEmbedView: state.level.isEmbedView,
+    isShareView: state.level.isShareView
+  };
+})(GamelabView);
