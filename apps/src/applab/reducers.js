6f3a689229029d1b6955c1e4e30b708be0fa41de
Brad Buchanan <brad@code.org>
2016-03-14 13:00:57 -0700

    Remove jshint block from reducers.js [ci skip]

--- apps/src/applab/reducers.js
+++ apps/src/applab/reducers.js
@@ -1,11 +1,5 @@
 /** @file Redux reducer functions for App Lab.
  *  @see http://redux.js.org/docs/basics/Reducers.html */
-// Strict linting: Absorb into global config when possible
-/* jshint
- unused: true,
- eqeqeq: true,
- maxlen: 120
- */
 'use strict';
 
 var ActionType = require('./actions').ActionType;
