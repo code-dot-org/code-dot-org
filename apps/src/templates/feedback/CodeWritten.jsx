31bd0b66f4e26367e39a538aa041b3effe8f017a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-03-14 11:06:28 -0700

    Redesign "Show Code" feedback
    Also refactors both the feedback dialog "Show Code" and the standalone
    "show code" button to use React

---
+++ apps/src/templates/feedback/CodeWritten.jsx
@@ -0,0 +1,38 @@
+var msg = require('../../locale');
+
+var CodeWritten = React.createClass({
+  
+  propTypes: {
+    numLinesWritten: React.PropTypes.number.isRequired,
+    totalNumLinesWritten: React.PropTypes.number.isRequired,
+    generatedCodeElement: React.PropTypes.element.isRequired
+  },
+
+  render: function () {
+    var lines, totalLines, showCode;
+
+    lines = (<p id="num-lines-of-code" className="lines-of-code-message">
+      {msg.numLinesOfCodeWritten({ numLines: this.props.numLinesWritten })}
+    </p>);
+
+    if (this.props.totalNumLinesWritten !== 0) {
+      totalLines = (<p id="total-num-lines-of-code" className="lines-of-code-message">
+        {msg.totalNumLinesOfCodeWritten({ numLines: this.props.totalNumLinesWritten })}
+      </p>);
+    }
+
+    showCode = (<details className="show-code">
+      <summary role="button">
+        <b>{msg.showGeneratedCode()}</b>
+      </summary>
+      {this.props.generatedCodeElement}
+    </details>);
+
+    return (<div>
+      {lines}
+      {totalLines}
+      {showCode}
+    </div>);
+  }
+});
+module.exports = CodeWritten;
