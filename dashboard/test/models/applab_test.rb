13d2a785464c3f15950eebc4c5dfccfd8523b0df
Brad Buchanan <brad@code.org>
2016-03-15 16:05:52 -0700

    Extract assert_same_keys helper

--- dashboard/test/models/applab_test.rb
+++ dashboard/test/models/applab_test.rb
@@ -10,25 +10,15 @@ class ApplabTest < ActiveSupport::TestCase
     # from levels.custom object in applab/levels.js
     # See regex demo at http://rubular.com/r/iXOyE8dAPj
     code_functions = /\s*
-      levels\.custom\s*=   # Find assignment of levels.custom
-      [^}]*                # Skip other properties up to codeFunctions
+      levels\.custom\s*=       # Find assignment of levels.custom
+      [^}]*                    # Skip other properties up to codeFunctions
       '?codeFunctions'?\s*:\s* # Find codeFunctions property
-      (\{[^}]*\})          # Capture value of property
+      (\{[^}]*\})              # Capture value of property
     /mx.match(file_contents)[1]
 
-    from_levels_js = JSON.parse(code_functions)
-    from_applab_rb = JSON.parse(Applab.palette)
-
-    # Check that keys match in both directions, collecting up errors so we
-    # can display them all in one test run if any are found.
-    errors = []
-    from_levels_js.keys.each do |key|
-      errors.push("Palette entry '#{key}' missing from applab.rb") unless from_applab_rb.key? key
-    end
-    from_applab_rb.keys.each do |key|
-      errors.push("codeFunctions entry '#{key}' missing from applab/levels.js") unless from_levels_js.key? key
-    end
-    assert errors.empty?, "Ruby-JavaScript palette mismatch:\n#{errors.join("\n")}"
+    assert_same_keys(JSON.parse(code_functions), JSON.parse(Applab.palette),
+                     'applab/levels.js', 'applab.rb',
+                     "Ruby-JavaScript palette mismatch")
   end
 
 end
--- dashboard/test/models/gamelab_test.rb
+++ dashboard/test/models/gamelab_test.rb
@@ -10,25 +10,15 @@ class GamelabTest < ActiveSupport::TestCase
     # from levels.custom object in gamelab/levels.js
     # See regex demo at http://rubular.com/r/iXOyE8dAPj
     code_functions = /\s*
-      levels\.custom\s*=   # Find assignment of levels.custom
-      [^}]*                # Skip other properties up to codeFunctions
+      levels\.custom\s*=       # Find assignment of levels.custom
+      [^}]*                    # Skip other properties up to codeFunctions
       '?codeFunctions'?\s*:\s* # Find codeFunctions property
-      (\{[^}]*\})          # Capture value of property
+      (\{[^}]*\})              # Capture value of property
     /mx.match(file_contents)[1]
 
-    from_levels_js = JSON.parse(code_functions)
-    from_gamelab_rb = JSON.parse(Gamelab.palette)
-
-    # Check that keys match in both directions, collecting up errors so we
-    # can display them all in one test run if any are found.
-    errors = []
-    from_levels_js.keys.each do |key|
-      errors.push("Palette entry '#{key}' missing from gamelab.rb") unless from_gamelab_rb.key? key
-    end
-    from_gamelab_rb.keys.each do |key|
-      errors.push("codeFunctions entry '#{key}' missing from gamelab/levels.js") unless from_levels_js.key? key
-    end
-    assert errors.empty?, "Ruby-JavaScript palette mismatch:\n#{errors.join("\n")}"
+    assert_same_keys(JSON.parse(code_functions), JSON.parse(Gamelab.palette),
+                     'gamelab/levels.js', 'gamelab.rb',
+                     "Ruby-JavaScript palette mismatch")
   end
 
 end
