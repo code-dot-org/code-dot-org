45a516a21b79b17bca517d138055e688615c7d18
Brent Van Minnen <bjvanminnen@gmail.com>
2016-03-16 16:19:10 -0700

    move admin to code-studio

--- code-studio/src/js/code-studio.js
+++ code-studio/src/js/code-studio.js
@@ -35,6 +35,7 @@ $.fn.modal.Constructor.prototype.enforceFocus = function () {};
 var jquery = require('jquery-shim');
 window.dashboard = window.dashboard || {};
 window.dashboard.clientState = require('./clientState.js')(window.sessionStorage, jquery);
+window.dashboard.admin = require('./admin');
 
 // Wrap existing window onerror caller with a script error check.  If we have a
 // script error and a url, throw that so that we have the info in new relic.
--- dashboard/app/assets/javascripts/admin.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0c6e71cc751e8f2532b42d0250c6cc635f0d12ac4ff787dc8141cd668d1983c9
-size 1269
