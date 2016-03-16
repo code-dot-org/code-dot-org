0618fc6eb5a7dafe3e8392bb165a3c06f63b8b26
Asher Kach <asher@code.org>
2016-03-16 16:22:08 -0500

    Make comments JSDoc comments.

--- dashboard/app/assets/javascripts/levelCompletions.js
+++ dashboard/app/assets/javascripts/levelCompletions.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a17b2cd11c471bc82bc39c545525e7a57aaa03e348c4b01d343092bf573f4e2d
-size 3967
+oid sha256:2a390c54f1dc1c3788d80592587b10cb084526c15d3aee4889d32ae64527e448
+size 3998
