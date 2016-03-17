33971ebdcceaf0ac8f881ea491bbd44a1f974e74
Asher Kach <asher@code.org>
2016-03-17 09:02:48 -0500

    Fix JS lint errors.

--- dashboard/app/assets/javascripts/admin/levelCompletions.js
+++ dashboard/app/assets/javascripts/admin/levelCompletions.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5da1fd16db80072025b7215dc97bcf85b11b5ae0269d48eb927b05b03016f8ae
-size 3997
+oid sha256:64a96a38a1dff9a5706c2895fb3756282a1fa2e77e618d3ead6508ebe4616305
+size 4008
