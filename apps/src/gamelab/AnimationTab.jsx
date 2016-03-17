837ce7439cb4c18e846ed87355b81fc311269f8e
Brad Buchanan <brad@code.org>
2016-03-16 17:54:44 -0700

    Three-column layout for animation tab

--- apps/src/gamelab/AnimationTab.jsx
+++ apps/src/gamelab/AnimationTab.jsx
@@ -2,6 +2,7 @@
 'use strict';
 
 var GameLabVisualizationHeader = require('./GameLabVisualizationHeader.jsx');
+var ResizablePanes = require('./ResizablePanes.jsx');
 
 /**
  * Root of the animation editor interface mode for GameLab
@@ -11,10 +12,22 @@ var AnimationTab = React.createClass({
   },
 
   render: function () {
+    var fillAbsoluteParentStyle = {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0
+    };
+
     return (
-      <div>
-        <GameLabVisualizationHeader/>
-      </div>
+      <ResizablePanes style={fillAbsoluteParentStyle}>
+        <div style={{backgroundColor: 'rgba(255,0,0,0.2)', flex: '0 0 150px', minWidth: 150, maxWidth: 300}}>
+          <GameLabVisualizationHeader/>
+        </div>
+        <div style={{backgroundColor: 'rgba(0,255,0,0.2)', flex: '0 0 200px', minWidth: 150, maxWidth: 300}}></div>
+        <div style={{backgroundColor: 'rgba(0,0,255,0.2)'}}></div>
+      </ResizablePanes>
     );
   }
 });
