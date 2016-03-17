7a52c3ce48d23fcc53ed47dc17e6b6ee3dd277cc
Asher Kach <asher@code.org>
2016-03-17 09:04:03 -0500

    OOPS.

--- dashboard/app/assets/javascripts/admin/levelCompletions.js
+++ dashboard/app/assets/javascripts/admin/levelCompletions.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64a96a38a1dff9a5706c2895fb3756282a1fa2e77e618d3ead6508ebe4616305
-size 4008
+oid sha256:ff52ee760f8b94288f3080bbaf4dc763f3327f2b9a9340c7e00c070382861468
+size 4012
