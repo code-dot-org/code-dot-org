773e6994d32609f754d7dacde11bb677369f72a5
Brad Buchanan <brad@code.org>
2016-03-18 10:45:40 -0700

    Style pass on AnimationFrameList.jsx

--- apps/src/gamelab/AnimationFrameList.jsx
+++ apps/src/gamelab/AnimationFrameList.jsx
@@ -1,19 +1,26 @@
+/** @file Vertical scrolling list of animation frames. */
 'use strict';
 
 var AnimationFrameTile = require('./AnimationFrameTile.jsx');
 var NewItemTile = require('./NewItemTile.jsx');
 var ScrollableList = require('./ScrollableList.jsx');
 
+/**
+ * Vertical scrolling list of animation frames for the selected animation.
+ * This component should be unique within the animation tab.
+ */
 var AnimationFrameList = React.createClass({
   render: function () {
-    return <ScrollableList className="animation-frame-list">
-      <AnimationFrameTile index={1}/>
-      <AnimationFrameTile index={2}/>
-      <AnimationFrameTile index={3} isSelected={true}/>
-      <AnimationFrameTile index={4}/>
-      <AnimationFrameTile index={151}/>
-      <NewItemTile label="new frame"/>
-    </ScrollableList>;
+    return (
+      <ScrollableList className="animation-frame-list">
+        <AnimationFrameTile index={1} />
+        <AnimationFrameTile index={2} />
+        <AnimationFrameTile index={3} isSelected />
+        <AnimationFrameTile index={4} />
+        <AnimationFrameTile index={151} />
+        <NewItemTile label="new frame" />
+      </ScrollableList>
+    );
   }
 });
 module.exports = AnimationFrameList;
