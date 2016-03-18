f4c611d903db5904b7a31ce6ff52af7320bcb94c
Brad Buchanan <brad@code.org>
2016-03-18 10:52:22 -0700

    Style pass on AnimationFrameTile.jsx

--- apps/src/gamelab/AnimationFrameTile.jsx
+++ apps/src/gamelab/AnimationFrameTile.jsx
@@ -1,3 +1,4 @@
+/** @file Single list item representing an animation frame. */
 'use strict';
 
 var color = require('../color');
@@ -5,32 +6,40 @@ var connect = require('react-redux').connect;
 var TileButtons = require('./TileButtons.jsx');
 var TileThumbnail = require('./TileThumbnail.jsx');
 
+/**
+ * Single list item in the AnimationFrameList, representing a single frame
+ * of the selected animation sequence.  Displays a thumbnail of the frame
+ * with the frame index overlaid, and if selected also shows controls for
+ * deleting or duplicating the frame.
+ */
 var AnimationFrameTile = React.createClass({
   propTypes: {
     assetUrl: React.PropTypes.func.isRequired,
-    index: React.PropTypes.number,
+    index: React.PropTypes.number.isRequired,
     isSelected: React.PropTypes.bool
   },
 
   render: function () {
-    var tileStyle = {
-      width: '100%',
-
-      backgroundColor: this.props.isSelected ? color.purple : 'none',
-      borderRadius: 10,
-      paddingTop: 4,
-      paddingBottom: 4,
-      marginBottom: 4
+    var styles = {
+      tile: {
+        width: '100%',
+        backgroundColor: this.props.isSelected ? color.purple : 'none',
+        borderRadius: 10,
+        paddingTop: 4,
+        paddingBottom: 4,
+        marginBottom: 4
+      }
     };
 
-    return <div style={tileStyle}>
-      <TileThumbnail
-          index={this.props.index}
-          isSelected={this.props.isSelected}
-          src={this.props.assetUrl('media/common_images/draw-east.png')}
-      />
-      {this.props.isSelected && <TileButtons/>}
-    </div>;
+    return (
+      <div style={styles.tile}>
+        <TileThumbnail
+            index={this.props.index}
+            isSelected={this.props.isSelected}
+            src={this.props.assetUrl('media/common_images/draw-east.png')} />
+        {this.props.isSelected && <TileButtons />}
+      </div>
+    );
   }
 });
 module.exports = connect(function propsFromStore(state) {
