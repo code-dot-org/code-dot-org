804307b0519db41020b0697ba610fbb39c086114
Brad Buchanan <brad@code.org>
2016-03-18 11:12:05 -0700

    Style pass on GameLabVisualizationHeader.jsx

--- apps/src/gamelab/GameLabVisualizationHeader.jsx
+++ apps/src/gamelab/GameLabVisualizationHeader.jsx
@@ -1,31 +1,41 @@
 /** @file Row of controls above the visualization. */
 var actions = require('./actions');
-var msg = require('../locale');
 var connect = require('react-redux').connect;
-var constants = require('./constants');
+var GameLabInterfaceMode = require('./constants').GameLabInterfaceMode;
+var msg = require('../locale');
 var ToggleGroup = require('../templates/ToggleGroup.jsx');
-var GameLabInterfaceMode = constants.GameLabInterfaceMode;
 
+/**
+ * Controls above the visualization header, including the code/animation toggle.
+ */
 var GameLabVisualizationHeader = React.createClass({
   propTypes: {
     isShareView: React.PropTypes.bool.isRequired,
-    interfaceMode: React.PropTypes.oneOf([GameLabInterfaceMode.CODE, GameLabInterfaceMode.ANIMATION]).isRequired,
+    interfaceMode: React.PropTypes
+        .oneOf([GameLabInterfaceMode.CODE, GameLabInterfaceMode.ANIMATION])
+        .isRequired,
     onInterfaceModeChange: React.PropTypes.func.isRequired
+  },
+
+  shouldHideToggle: function () {
+    return this.props.isShareView;
   },
 
   render: function () {
     return (
       <div className="workspace-header-height">
-        <ToggleGroup selected={this.props.interfaceMode} onChange={this.props.onInterfaceModeChange}>
-          <button value={GameLabInterfaceMode.CODE}>{msg.codeMode()}</button>
-          <button value={GameLabInterfaceMode.ANIMATION}>{msg.animationMode()}</button>
+        <ToggleGroup
+            selected={this.props.interfaceMode}
+            onChange={this.props.onInterfaceModeChange}>
+          <button value={GameLabInterfaceMode.CODE}>
+            {msg.codeMode()}
+          </button>
+          <button value={GameLabInterfaceMode.ANIMATION}>
+            {msg.animationMode()}
+          </button>
         </ToggleGroup>
       </div>
     );
-  },
-
-  shouldHideToggle: function () {
-    return this.props.isShareView;
   }
 });
 module.exports = connect(function propsFromStore(state) {
