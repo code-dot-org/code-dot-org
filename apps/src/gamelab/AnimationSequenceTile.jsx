c4aee97786b65e0c840cc734351c903a97856626
Brad Buchanan <brad@code.org>
2016-03-18 11:01:57 -0700

    Style pass on AnimationSequenceTile.jsx

--- apps/src/gamelab/AnimationSequenceTile.jsx
+++ apps/src/gamelab/AnimationSequenceTile.jsx
@@ -1,3 +1,4 @@
+/** A single list item representing an animation sequence. */
 'use strict';
 
 var color = require('../color');
@@ -5,6 +6,11 @@ var connect = require('react-redux').connect;
 var TileButtons = require('./TileButtons.jsx');
 var TileThumbnail = require('./TileThumbnail.jsx');
 
+/**
+ * A single list item representing an animation sequence.  Displays an
+ * animated thumbnail of the sequence, along with the sequence name and
+ * (if currently selected) controls for deleting or duplicating the sequence.
+ */
 var AnimationSequenceTile = React.createClass({
   propTypes: {
     assetUrl: React.PropTypes.func.isRequired,
@@ -13,36 +19,62 @@ var AnimationSequenceTile = React.createClass({
   },
 
   render: function () {
-    var tileStyle = {
-      width: '100%',
+    var styles = {
+      tile: {
+        width: '100%',
 
-      backgroundColor: this.props.isSelected ? color.purple : 'none',
-      borderRadius: 10,
-      // Use vertical padding because we flow vertically, but require
-      // children to use margins horizontally.
-      paddingTop: 4,
-      paddingBottom: 4,
-      marginBottom: 4
+        backgroundColor: this.props.isSelected ? color.purple : 'none',
+        borderRadius: 10,
+
+        // Provide vertical padding because we flow vertically, but require
+        // children to use margins horizontally.
+        paddingTop: 4,
+        paddingBottom: 4,
+        marginBottom: 4
+      },
+      nameLabel: {
+        marginLeft: 4,
+        marginRight: 4,
+        marginTop: 4,
+        textAlign: 'center',
+        userSelect: 'none'
+      },
+      nameInputWrapper: {
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 4
+      },
+      nameInput: {
+        width: '100%',
+        margin: 0,
+        padding: 0,
+        textAlign: 'center',
+        border: 'none',
+        borderRadius: 9
+      }
     };
 
     var sequenceName;
     if (this.props.isSelected) {
-      sequenceName = <div style={{marginLeft: 5, marginRight: 5, marginTop: 4}}>
-          <input type="text" style={{width: '100%', margin: 0, padding: 0, textAlign: 'center', border: 'none', borderRadius: 9}} value={this.props.sequenceName} />
-        </div>;
+      sequenceName = (
+        <div style={styles.nameInputWrapper}>
+          <input type="text" style={styles.nameInput} value={this.props.sequenceName} />
+        </div>
+      );
     } else {
-      sequenceName = <div className="sequence-name" style={{marginLeft: 4, marginRight: 4, marginTop: 4, textAlign: 'center', userSelect: 'none'}}>{this.props.sequenceName}</div>;
+      sequenceName = <div style={styles.nameLabel}>{this.props.sequenceName}</div>;
     }
 
 
-    return <div style={tileStyle}>
-      <TileThumbnail
-          isSelected={this.props.isSelected}
-          src={this.props.assetUrl('media/common_images/draw-east.png')}
-      />
-      {sequenceName}
-      {this.props.isSelected && <TileButtons/>}
-    </div>;
+    return (
+      <div style={styles.tile}>
+        <TileThumbnail
+            isSelected={this.props.isSelected}
+            src={this.props.assetUrl('media/common_images/draw-east.png')} />
+        {sequenceName}
+        {this.props.isSelected && <TileButtons />}
+      </div>
+    );
   }
 });
 module.exports = connect(function propsFromStore(state) {
