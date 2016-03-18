af36cf5b0aa5999cdb910ece434fdb008ecdf998
Brad Buchanan <brad@code.org>
2016-03-18 11:42:13 -0700

    Style pass on TileButtons.jsx

--- apps/src/gamelab/TileButtons.jsx
+++ apps/src/gamelab/TileButtons.jsx
@@ -1,13 +1,33 @@
+/** @file controls below an animation thumbnail */
 'use strict';
 
 var color = require('../color');
 
+/**
+ * The delete and duplicate controls beneath an animation or frame thumbnail.
+ */
 var TileButtons = React.createClass({
   render: function () {
-    return <div style={{marginLeft: 4, marginRight: 4, marginTop: 6, textAlign: 'center', color: color.white, fontSize: 24}}>
-      <i className="fa fa-trash-o" style={{marginRight: 12}}></i>
-      <i className="fa fa-clone"></i>
-    </div>;
+    var styles = {
+      root: {
+        marginLeft: 4,
+        marginRight: 4,
+        marginTop: 6,
+        textAlign: 'center',
+        color: color.white,
+        fontSize: 24
+      },
+      trash: {
+        marginRight: 12
+      }
+    };
+
+    return (
+      <div style={styles.root}>
+        <i className="fa fa-trash-o" style={styles.trash} />
+        <i className="fa fa-clone" />
+      </div>
+    );
   }
 });
 module.exports = TileButtons;
