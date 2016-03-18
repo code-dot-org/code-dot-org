6bffd3fc0a0adcf386f3dff19fd25c9faa70161b
Brad Buchanan <brad@code.org>
2016-03-18 11:06:18 -0700

    Style pass on AnimationTab.jsx

--- apps/src/gamelab/AnimationTab.jsx
+++ apps/src/gamelab/AnimationTab.jsx
@@ -11,27 +11,45 @@ var ResizablePanes = require('./ResizablePanes.jsx');
  */
 var AnimationTab = React.createClass({
   render: function () {
-    var fillAbsoluteParentStyle = {
-      position: 'absolute',
-      top: 0,
-      bottom: 20,
-      left: 0,
-      right: 0
+    var styles = {
+      root: {
+        position: 'absolute',
+        top: 0,
+        bottom: 20,
+        left: 0,
+        right: 0
+      },
+      sequencesColumn: {
+        flex: '0 0 150px' // sets initial width
+      },
+      framesColumn: {
+        flex: '0 0 250px' // sets initial width
+      },
+      editorRegion: {
+        flex: '1 0',
+        backgroundColor: 'white',
+        textAlign: 'center',
+        paddingTop:'48%'
+      }
     };
 
     return (
-      <ResizablePanes style={fillAbsoluteParentStyle}>
-        <div id='sequences-column' style={{flex: '0 0 150px'}}>
-          <GameLabVisualizationHeader/>
-          <AnimationSequenceList/>
+      <ResizablePanes style={styles.root}>
+        <div id='sequences-column' style={styles.sequencesColumn}>
+          <GameLabVisualizationHeader />
+          <AnimationSequenceList />
         </div>
-        <div id='frames-column' style={{flex: '0 0 250px'}}>
-          <div className="purple-header workspace-header"><span>Frames</span></div>
-          <AnimationFrameList/>
+        <div id='frames-column' style={styles.framesColumn}>
+          <div className="purple-header workspace-header">
+            <span>Frames</span>
+          </div>
+          <AnimationFrameList />
         </div>
         <div id='editor-column'>
-          <div className="purple-header workspace-header"><span>Workspace</span></div>
-          <div style={{flex: '1 0', backgroundColor: 'white', textAlign: 'center', paddingTop:'48%'}}>
+          <div className="purple-header workspace-header">
+            <span>Workspace</span>
+          </div>
+          <div style={styles.editorRegion}>
             TODO: Piskel editor goes here!
           </div>
         </div>
