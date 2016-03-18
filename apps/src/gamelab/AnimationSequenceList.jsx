5c47fb1676bd897eb6227af76a15cb33c6a279c2
Brad Buchanan <brad@code.org>
2016-03-18 11:54:55 -0700

    Style pass on AnimationSequenceList.jsx

--- apps/src/gamelab/AnimationSequenceList.jsx
+++ apps/src/gamelab/AnimationSequenceList.jsx
@@ -1,16 +1,23 @@
+/** @file Vertical scrolling list of animation sequences */
 'use strict';
 
 var AnimationSequenceTile = require('./AnimationSequenceTile.jsx');
 var NewItemTile = require('./NewItemTile.jsx');
 var ScrollableList = require('./ScrollableList.jsx');
 
+/**
+ * Vertical scrolling list of animation sequences associated with the
+ * project.
+ */
 var AnimationSequenceList = React.createClass({
   render: function () {
-    return <ScrollableList className="animation-sequence-list">
-      <AnimationSequenceTile sequenceName="sequence01"/>
-      <AnimationSequenceTile sequenceName="sequence02" isSelected={true}/>
-      <NewItemTile label="new sequence"/>
-    </ScrollableList>;
+    return (
+      <ScrollableList className="animation-sequence-list">
+        <AnimationSequenceTile sequenceName="sequence01" />
+        <AnimationSequenceTile sequenceName="sequence02" isSelected />
+        <NewItemTile label="new sequence" />
+      </ScrollableList>
+    );
   }
 });
 module.exports = AnimationSequenceList;
