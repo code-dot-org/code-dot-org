c9349ee9f9dd0034dd1e8e1d466ef14971d976a3
Paul Carduner <paul@carduner.net>
2016-03-19 09:21:53 -0700

    Bunch of small tweaks based on PR feedback

--- code-studio/src/js/components/share_dialog.jsx
+++ code-studio/src/js/components/share_dialog.jsx
@@ -19,6 +19,7 @@ var ShareDialog = React.createClass({
     channelId: React.PropTypes.string.isRequired,
     appType: React.PropTypes.string.isRequired,
     onClickPopup: React.PropTypes.func.isRequired,
+    onClickExport: React.PropTypes.func.isRequired,
   },
 
   getInitialState: function () {
@@ -51,6 +52,7 @@ var ShareDialog = React.createClass({
           appType={this.props.appType}
           onClickPopup={this.props.onClickPopup}
           onClickClose={this.close}
+          onClickExport={this.props.onClickExport}
           />
       </Dialog>
     );
