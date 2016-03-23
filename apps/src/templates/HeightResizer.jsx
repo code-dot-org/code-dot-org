e84b9fb958db26fea2194f938a3e59ce3b52ee9b
Brent Van Minnen <bjvanminnen@gmail.com>
2016-03-23 16:37:42 -0700

    add (not yet functioning) grippy

---
+++ apps/src/templates/HeightResizer.jsx
@@ -0,0 +1,29 @@
+var _ = require('lodash');
+var color = require('../color');
+
+var HeightResizer = React.createClass({
+
+  propTypes: {
+    style: React.PropTypes.object
+  },
+
+  render: function () {
+    var style = {
+      width: '100%',
+      color: color.lighter_gray,
+      fontSize: 24,
+      textAlign: 'center',
+      cursor: 'ns-resize',
+      whiteSpace: 'nowrap',
+      lineHeight: this.props.style.height + 'px'
+    };
+
+    return (
+      <div style={this.props.style} className="workspace-right">
+        <div style={style} className="fa fa-ellipsis-h"/>
+      </div>
+    );
+  }
+});
+
+module.exports = HeightResizer;
