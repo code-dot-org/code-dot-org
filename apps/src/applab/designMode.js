aec82247fc0538078137ebda6f6352b7d4aa4c89
David Bailey <davidsbailey@users.noreply.github.com>
2016-03-24 14:42:14 -0700

    sanitize appOptions.level.levelHtml

--- apps/src/applab/designMode.js
+++ apps/src/applab/designMode.js
@@ -553,6 +553,23 @@ designMode.serializeToLevelHtml = function () {
   Applab.levelHtml = serialization;
 };
 
+
+function getUnsafeHtmlReporter(sanitizationTarget) {
+  function reportUnsafeHtml(sanitizationTarget, removed, unsafe, safe) {
+    var msg = "The following lines of HTML were modified or removed:\n" + removed +
+      "\noriginal html:\n" + unsafe + "\nmodified html:\n" + safe + "\ntarget: " + sanitizationTarget;
+    console.log(msg);
+    logToCloud.addPageAction(logToCloud.PageAction.SanitizedLevelHtml, {
+      removedHtml: removed,
+      unsafeHtml: unsafe,
+      safeHtml: safe,
+      sanitizationTarget: sanitizationTarget
+    });
+  }
+
+  return reportUnsafeHtml.bind(this, sanitizationTarget);
+}
+
 /**
  * Replace the contents of rootEl with the children of the DOM node obtained by
  * parsing Applab.levelHtml (the root node in the levelHtml is ignored).
@@ -572,18 +589,8 @@ designMode.parseFromLevelHtml = function(rootEl, allowDragging, prefix) {
   if (!Applab.levelHtml) {
     return;
   }
-  function reportUnsafeHtml(removed, unsafe, safe) {
-    var msg = "The following lines of HTML were modified or removed:\n" + removed +
-      "\noriginal html:\n" + unsafe + "\nmodified html:\n" + safe + "\ntarget: " + rootEl.id;
-    console.log(msg);
-    logToCloud.addPageAction(logToCloud.PageAction.SanitizedLevelHtml, {
-      removedHtml: removed,
-      unsafeHtml: unsafe,
-      safeHtml: safe,
-      sanitizationTarget: rootEl.id
-    });
-  }
 
+  var reportUnsafeHtml = getUnsafeHtmlReporter(rootEl.id);
   var levelDom = $.parseHTML(sanitizeHtml(Applab.levelHtml, reportUnsafeHtml));
   var children = $(levelDom).children();
 
@@ -976,6 +983,8 @@ designMode.renderDesignWorkspace = function(element) {
  * screen app.
  */
 designMode.addScreenIfNecessary = function(html) {
+  var reportUnsafeHtml = getUnsafeHtmlReporter('levelHtml');
+  html = sanitizeHtml(html, reportUnsafeHtml);
   var rootDiv = $(html);
   if (rootDiv.children().length === 0 ||
       rootDiv.children().eq(0).hasClass('screen')) {
