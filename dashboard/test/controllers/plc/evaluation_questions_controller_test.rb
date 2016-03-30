e48b92a16f645b5812a6eb6d071ebbfea414d75b
Mehal Shah <mehal@code.org>
2016-03-29 17:23:19 -0700

    Content creators should be able to delete questions in the assessment creation screen

---
+++ dashboard/test/controllers/plc/evaluation_questions_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Plc::EvaluationQuestionsControllerTest < ActionController::TestCase
+  setup do
+    user = create (:admin)
+    sign_in(user)
+    course = create(:plc_course)
+    @course_unit = create(:plc_course_unit, plc_course: course)
+    @plc_evaluation_question = create(:plc_evaluation_question, plc_course_unit: @course_unit)
+  end
+
+  test "should destroy plc_evaluation_question" do
+    assert_difference('Plc::EvaluationQuestion.count', -1) do
+      delete :destroy, id: @plc_evaluation_question
+    end
+
+    assert_redirected_to plc_course_unit_path(@course_unit)
+  end
+end
