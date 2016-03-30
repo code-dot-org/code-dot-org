3d775d24a134a52338734f2eeb434e3faea49c72
Asher Kach <asher@code.org>
2016-03-30 09:50:07 -0500

    Restructure the logic for showing the survey.

--- dashboard/app/helpers/survey_results_helper.rb
+++ dashboard/app/helpers/survey_results_helper.rb
@@ -1,24 +1,37 @@
 module SurveyResultsHelper
-
   def show_survey?
-    # English only for now
-    if language == "en"
-      # US only
-      if request.location.try(:country_code) == 'US' || (!Rails.env.production? && request.location.try(:country_code) == 'RD')
-        # If signed in, and user hasn't already filled out this survey
-        if current_user && !SurveyResult.where(user_id: current_user.id).exists?
-          # If they're a teacher.
-          if current_user.teacher?
-            # If at least 13, and account is at least 14 days old
-            if !current_user.under_13? && (DateTime.now - current_user.created_at.to_datetime) >= 14
-              return true
-            end
-          end
-        end
-      end
+    # Reasons we would not show the survey.
+    if !current_user
+      return false
+    end
+    if !current_user.teacher?
+      return false
+    end
+    if current_user.under_13?
+      return false
+    end
+    if language != "en"
+      return false
+    end
+    if SurveyResult.where(user_id: current_user.id).exists?
+      return false
     end
 
-    false
+    # For testing purposes, we show the survey in non-production environments
+    # regardless of account age or request location.
+    if !Rails.env.production?
+      return true
+    end
 
+    # More reasons we would not show the survey.
+    if DateTime.now - current_user.created_at.to_datetime < 14
+      return false
+    end
+    if request.location.try(:country_code) != 'US'
+      return false
+    end
+
+    # There is no reason not to show the survey, so show the survey.
+    return true
   end
 end
