ad97c04275f4c4364d73f0ed654b3f3c598f995c
Brent Van Minnen <bjvanminnen@gmail.com>
2016-03-31 11:36:59 -0700

    generalize color conversion

--- apps/script/build-color-js.js
+++ apps/script/build-color-js.js
@@ -1,66 +1,71 @@
 #!/usr/bin/env node
-// Script to generate color.js from color.scss
+
+/**
+ * Script that processes an scss file containing a set of variables, and
+ * generates the equivalent set in JS.
+ */
 var fs = require('fs');
 var path = require('path');
 var readline = require('readline');
 
-var colorScssPath = path.resolve('../shared/css/color.scss');
-var colorJsPath = path.resolve('./src/color.js');
-
 // Regular expression to capture a color definition from SCSS
-var colorRe = /\$(\w+)\s*:\s*([^;]+);/;
-var cachedColors = {};
+var variableRe = /\$(\w+)\s*:\s*([^;]+);/;
+var cachedVariables = {};
 
 // In color.scss some color definitions reference previous
 // color definitions.  Since we're reading them in order,
 // we can recursively resolve these referential defintions
 // until we land on an actual color.
-function resolveColor(value) {
+function resolveVariable(value) {
   var originalValue = value;
   while (/^\$/.test(value)) {
-    value = cachedColors[value.substring(1)];
+    value = cachedVariables[value.substring(1)];
   }
   return value;
 }
 
-// Generate color.js while reading color.scss line-by-line
-var out = fs.createWriteStream(colorJsPath);
-out.write([
-  '// color.js',
-  '// GENERATED FILE: DO NOT MODIFY DIRECTLY',
-  '// This generated file exports all colors defined in color.scss',
-  '// for use in JavaScript. The generator script is build-color-js.js',
-  'module.exports = {\n'
-].join('\n'));
+function convertScssToJs(scssPath, jsPath) {
+  // Generate <foo>.js while reading <foo>.scss line-by-line
+  var out = fs.createWriteStream(jsPath);
+  out.write([
+    '// color.js',
+    '// GENERATED FILE: DO NOT MODIFY DIRECTLY',
+    '// This generated file exports all colors defined in color.scss',
+    '// for use in JavaScript. The generator script is build-color-js.js',
+    'module.exports = {\n'
+  ].join('\n'));
 
-var rl = readline.createInterface({
-  input: fs.createReadStream(colorScssPath),
-  terminal: false
-});
+  var rl = readline.createInterface({
+    input: fs.createReadStream(scssPath),
+    terminal: false
+  });
 
-var currentLine = 0;
-rl.on('line', function (line) {
-  currentLine++;
-  var match = colorRe.exec(line);
-  if (match === null) {
-    return;
-  }
+  var currentLine = 0;
+  rl.on('line', function (line) {
+    currentLine++;
+    var match = variableRe.exec(line);
+    if (match === null) {
+      return;
+    }
 
-  var colorName = match[1];
-  var colorValue = resolveColor(match[2]);
-  if (typeof colorValue === 'undefined') {
-    throw new Error([
-        'Unable to resolve color ' + colorName,
-        colorScssPath + ':' + currentLine,
-        line,
-        ' ^'
-        ].join('\n'));
-  }
-  cachedColors[colorName] = colorValue;
+    var variableName = match[1];
+    var variableValue = resolveVariable(match[2]);
+    if (typeof variableValue === 'undefined') {
+      throw new Error([
+          'Unable to resolve color ' + variableName,
+          scssPath + ':' + currentLine,
+          line,
+          ' ^'
+          ].join('\n'));
+    }
+    cachedVariables[variableName] = variableValue;
 
-  out.write('  ' + colorName + ': "' + colorValue + '",\n');
-});
-rl.on('close', function () {
-  out.write('};\n');
-});
+    out.write('  ' + variableName + ': "' + variableValue + '",\n');
+  });
+  rl.on('close', function () {
+    out.write('};\n');
+  });
 
+}
+
+convertScssToJs(path.resolve('../shared/css/color.scss'), path.resolve('./src/color.js'));
