11a4caec0f4ec40ec39412050dd533d53c079226
Brad Buchanan <brad@code.org>
2016-04-01 16:51:32 -0700

    Move the rest of the common rules over

--- code-studio/.eslintrc.js
+++ code-studio/.eslintrc.js
@@ -9,14 +9,5 @@ module.exports = {
     "jQuery": true,
     "React": true,
     "ReactDOM": true
-  },
-  "rules": {
-    // 0 means disabled, 1 means warn, 2 means error
-    "no-undef": 2,
-    "no-unused-vars": 0,
-    "comma-dangle": 0,
-    "no-empty": 0,
-    "semi": 2,
-    "no-console": 0
   }
 };
