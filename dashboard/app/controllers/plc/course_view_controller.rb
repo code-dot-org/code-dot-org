f32177e83f36cb9a15dd6d0411f57ef6b7428653
Mehal Shah <mehal@code.org>
2016-04-01 14:17:27 -0700

    Add one thing

--- dashboard/app/controllers/plc/course_view_controller.rb
+++ dashboard/app/controllers/plc/course_view_controller.rb
@@ -1,7 +1,13 @@
 class Plc::CourseViewController < ApplicationController
-  before_filter :require_teacher
+  before_filter :dashboard_view_requirements
 
   def render_dashboard
     @enrollments = current_user.plc_enrollments
+  end
+
+  private
+  def dashboard_view_requirements
+    authenticate_user!
+    require_teacher
   end
 end
