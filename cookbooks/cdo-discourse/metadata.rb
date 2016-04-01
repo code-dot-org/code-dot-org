b4777ac30f052a17955c6496661f9313d4bfb088
Will Jordan <will@code.org>
2016-04-01 14:00:06 -0700

    small tweaks to cdo-discourse

--- cookbooks/cdo-discourse/metadata.rb
+++ cookbooks/cdo-discourse/metadata.rb
@@ -6,6 +6,5 @@ description      'Installs/Configures Discourse'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
 
-depends 'apt'
 depends 'cdo-postfix'
 depends 'docker'
--- cookbooks/cdo-discourse/recipes/default.rb
+++ cookbooks/cdo-discourse/recipes/default.rb
@@ -7,13 +7,16 @@ end
 git '/var/discourse' do
   repository 'https://github.com/discourse/discourse_docker.git'
   revision 'a85e554333bffaac984eb585e25a74812982bfe6' # last updated 3/13/2016
+  notifies :run, 'execute[launcher rebuild]'
 end
 
 template '/var/discourse/containers/app.yml' do
   source 'app.yml.erb'
+  notifies :run, 'execute[launcher rebuild]'
 end
 
 execute 'launcher rebuild' do
   command './launcher rebuild app'
   cwd '/var/discourse'
+  action :nothing
 end
