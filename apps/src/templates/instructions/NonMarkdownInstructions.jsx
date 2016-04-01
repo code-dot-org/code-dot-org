465928265e44950b93a4b81abcfefa3e5ca8f23d
Brent Van Minnen <bjvanminnen@gmail.com>
2016-04-01 12:12:52 -0700

    basic test for non-markdown instructions

--- apps/src/templates/instructions/NonMarkdownInstructions.jsx
+++ apps/src/templates/instructions/NonMarkdownInstructions.jsx
@@ -7,22 +7,20 @@ var styles = {
 
 var NonMarkdownInstructions = function (props) {
   return (
-    <div>
-      <div style={styles.main}>
-        <p className='dialog-title'>{props.puzzleTitle}</p>
-        {props.instructions &&
-          <p className='instructions' dangerouslySetInnerHTML={{ __html: props.instructions }}/>
-        }
-        {props.instructions2 &&
-          <p className='instructions2' dangerouslySetInnerHTML={{ __html: props.instructions2 }}/>
-        }
-      </div>
+    <div style={styles.main}>
+      <p className='dialog-title'>{props.puzzleTitle}</p>
+      {props.instructions &&
+        <p className='instructions' dangerouslySetInnerHTML={{ __html: props.instructions }}/>
+      }
+      {props.instructions2 &&
+        <p className='instructions2' dangerouslySetInnerHTML={{ __html: props.instructions2 }}/>
+      }
     </div>
   );
 };
 
 NonMarkdownInstructions.propTypes = {
-  puzzleTitle: React.PropTypes.string,
+  puzzleTitle: React.PropTypes.string.isRequired,
   instructions: React.PropTypes.string,
   instructions2: React.PropTypes.string
 };
