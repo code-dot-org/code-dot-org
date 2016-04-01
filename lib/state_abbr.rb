d846c9f9366e42464ed9b0b1fcfe326f80d1ae4b
ashercodeorg <asher@code.org>
2016-04-01 13:39:19 -0500

    Removes more dependencies on pegasus geography_us_states table.
    * Renames state methods.
    
    * Delete deprecated DB methods.

--- lib/state_abbr.rb
+++ lib/state_abbr.rb
@@ -5,19 +5,19 @@ end
 
 # Returns the state name associated with the state abbreviation abbr.
 # abbr: A two character (uppercase or lowercase) symbol or string.
-def get_state_name_from_abbr(abbr)
+def get_us_state_from_abbr(abbr)
   return STATE_ABBR_HASH[get_uppercase_symbol(abbr)]
 end
 
 # Returns the state name associated with the state (including Washington DC)
 # abbreviation abbr.
 # abbr: A two character (uppercase or lowercase) symbol or string.
-def get_state_name_with_dc_from_abbr(abbr)
+def get_us_state_with_dc_from_abbr(abbr)
   abbr = get_uppercase_symbol(abbr)
   if abbr == :DC
     return 'Washington DC'
   end
-  return get_state_name_from_abbr(abbr)
+  return get_us_state_from_abbr(abbr)
 end
 
 # Returns whether the abbreviation is a state (including Washington DC)
