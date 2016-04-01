a6ddb97eb330790418876b4f5339e1cc3ac70978
Brad Buchanan <brad@code.org>
2016-04-01 15:01:04 -0700

    Enforce no-new-object

--- apps/.eslintrc.js
+++ apps/.eslintrc.js
@@ -43,6 +43,7 @@ module.exports = {
     "no-eval": "error",
     "no-extra-boolean-cast": "off",
     "no-implicit-globals": "error",
+    "no-new-object": "error",
     "no-undef": "error",
     "no-unused-vars": "off",
     "no-with": "error",
