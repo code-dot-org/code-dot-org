56d534d0fa69a8ba68232a93daf62cc4e8ef2443
Brad Buchanan <brad@code.org>
2016-04-01 17:20:58 -0700

    Enable eol-last

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
   "rules": {
     "comma-dangle": "off",
     "curly": "error",
+    "eol-last": "error",
     "no-array-constructor": "error",
     "no-console": "off",
     "no-empty": "off",
