fde17502b1f1749f4cd73a5d1be00110d52830b1
Brent Van Minnen <bjvanminnen@gmail.com>
2016-04-04 11:18:38 -0700

    use codeworkspacecontainer in AppView, fix some rtl stuff

--- apps/src/templates/CodeWorkspaceContainer.jsx
+++ apps/src/templates/CodeWorkspaceContainer.jsx
@@ -1,9 +1,11 @@
+/* global appOptions */
+
 /**
  * A non-protected div that wraps our ProtectedStatefulDiv codeWorkspace, allowing
  * us to position it vertically. Causes resize events to fire when receiving new props
  */
 
-var _ = require('../lodash');
+var Radium = require('radium');
 var ProtectedStatefulDiv = require('./ProtectedStatefulDiv.jsx');
 var utils = require('../utils');
 
@@ -16,7 +18,11 @@ var styles = {
     bottom: 0,
     marginLeft: 15, // margin gives space for vertical resizer
   },
-
+  mainRTL: {
+    right: undefined,
+    left: 0,
+    marginRight: 15
+  },
   hidden: {
     display: 'none'
   },
@@ -31,40 +37,51 @@ var styles = {
     overflow: 'hidden',
     zIndex: 0
   },
+  noVisualization: {
+    left: 0,
+    marginLeft: 0
+  }
 };
 
 var CodeWorkspaceContainer = React.createClass({
   propTypes: {
     topMargin: React.PropTypes.number.isRequired,
     generateCodeWorkspaceHtml: React.PropTypes.func.isRequired,
-    onSizeChange: React.PropTypes.func.isRequired
+    onSizeChange: React.PropTypes.func
   },
 
   componentDidUpdate: function (prevProps) {
-    if (this.props.topMargin !== prevProps.topMargin) {
+    if (this.props.onSizeChange && this.props.topMargin !== prevProps.topMargin) {
       this.props.onSizeChange();
     }
   },
 
   render: function () {
-    var mainStyle = _.assign({}, styles.main, {
+    // TODO - These probably better belong as props (and possibly in the redux
+    // store), but I'd like to get the quick and dirty fix in ASAP as RTL is
+    // currently broken
+    var noViz = appOptions.app === 'jigsaw';
+    var rtl = document.querySelector('html').getAttribute('dir') === 'rtl';
+
+    var mainStyle = [styles.main, {
       top: this.props.topMargin
-    });
+    },
+      noViz && styles.noVisualization,
+      rtl && styles.mainRTL
+    ];
 
     return (
       <div style={mainStyle} className="editor-column">
         <ProtectedStatefulDiv
             id="codeWorkspace"
-            style={styles.codeWorkspace}
-            className="applab">
+            style={styles.codeWorkspace}>
           <ProtectedStatefulDiv
               id="codeWorkspaceWrapper"
               contentFunction={this.props.generateCodeWorkspaceHtml}/>
-          {!this.props.isReadOnlyWorkspace &&
-            <ProtectedStatefulDiv id="designWorkspace" style={styles.hidden} />}
+          <ProtectedStatefulDiv id="designWorkspace" style={styles.hidden}/>
         </ProtectedStatefulDiv>
       </div>
     );
   }
 });
-module.exports = CodeWorkspaceContainer;
+module.exports = Radium(CodeWorkspaceContainer);
