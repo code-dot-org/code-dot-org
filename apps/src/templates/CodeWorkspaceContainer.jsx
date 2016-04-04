f96020d02bebbd58d6bf0a69cfff43678d5fca1e
Brent Van Minnen <bjvanminnen@gmail.com>
2016-04-04 13:07:56 -0700

    noviz and rtl as props. some rtl tests

--- apps/src/templates/CodeWorkspaceContainer.jsx
+++ apps/src/templates/CodeWorkspaceContainer.jsx
@@ -50,7 +50,10 @@ var styles = {
 var CodeWorkspaceContainer = React.createClass({
   propTypes: {
     topMargin: React.PropTypes.number.isRequired,
+<<<<<<< HEAD
     hidden: React.PropTypes.bool,
+=======
+>>>>>>> noviz and rtl as props. some rtl tests
     isRtl: React.PropTypes.bool.isRequired,
     noVisualization: React.PropTypes.bool.isRequired,
     generateCodeWorkspaceHtml: React.PropTypes.func.isRequired,
@@ -69,8 +72,12 @@ var CodeWorkspaceContainer = React.createClass({
     },
       this.props.noVisualization && styles.noVisualization,
       this.props.isRtl && styles.mainRtl,
+<<<<<<< HEAD
       this.props.noVisualization && this.props.isRtl && styles.noVisualizationRtl,
       this.props.hidden && styles.hidden
+=======
+      this.props.noVisualization && this.props.isRtl && styles.noVisualizationRtl
+>>>>>>> noviz and rtl as props. some rtl tests
     ];
 
     return (
