ae2ab382ff745ec64cf0589c5f5e76c2eab16cfe
Brad Buchanan <brad@code.org>
2016-04-06 14:49:40 -0700

    Add handlers to open file picker

--- apps/src/gamelab/AnimationPicker/index.jsx
+++ apps/src/gamelab/AnimationPicker/index.jsx
@@ -1,15 +1,70 @@
 var AnimationPickerBody = require('./AnimationPickerBody.jsx');
 var Dialog = require('../../templates/DialogComponent.jsx');
+var HiddenUploader = require('../../assetManagement/HiddenUploader.jsx');
+var styles = require('./styles');
+
+/** @enum {string} */
+var View = {
+  PICKER: 'PICKER',
+  UPLOAD_IN_PROGRESS: 'UPLOAD_IN_PROGRESS',
+  ERROR: 'ERROR'
+};
 
 var AnimationPicker = React.createClass({
   propTypes: {
-    handleClose: React.PropTypes.func.isRequired
+    channelId: React.PropTypes.string.isRequired,
+    handleClose: React.PropTypes.func.isRequired,
+    typeFilter: React.PropTypes.string
+  },
+
+  getInitialState: function () {
+    return {view: View.PICKER};
+  },
+
+  onUploadClick: function () {
+    this.refs.uploader.openFileChooser();
+  },
+
+  onUploadStart: function () {
+    console.log('onUploadStart');
+    this.setState({view: View.UPLOAD_IN_PROGRESS});
+  },
+
+  onUploadDone: function (result) {
+    console.log('onUploadDone');
+    console.log(result);
+    this.setState({view: View.PICKER});
+  },
+
+  onUploadError: function (status) {
+    console.log('onUploadError');
+    console.log(status);
+    this.setState({view: View.PICKER});
   },
 
   render: function () {
+    var visibleBody;
+    if (this.state.view === View.PICKER) {
+      visibleBody = <AnimationPickerBody onUploadClick={this.onUploadClick} />;
+    } else if (this.state.view === View.UPLOAD_IN_PROGRESS) {
+      visibleBody = <h1 style={styles.title}>Uploading...</h1>;
+    } else if (this.state.view === View.ERROR) {
+      visibleBody = <h1>Error!</h1>;
+    }
+
     return (
-      <Dialog handleClose={this.props.handleClose} isOpen>
-        <AnimationPickerBody />
+      <Dialog
+          isOpen
+          handleClose={this.props.handleClose}
+          uncloseable={this.state.view === View.UPLOAD_IN_PROGRESS}>
+        <HiddenUploader
+            ref="uploader"
+            onUploadStart={this.onUploadStart}
+            onUploadDone={this.onUploadDone}
+            onUploadError={this.onUploadError}
+            channelId={this.props.channelId}
+            typeFilter={this.props.typeFilter} />
+        {visibleBody}
       </Dialog>
     );
   }
