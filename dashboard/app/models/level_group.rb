d6b63c1ad318ac994f613c13942b87191d6d2120
Brendan Reville <brendan@code.org>
2016-04-06 22:38:15 +1000

    assessment: more improvements to long assessment.
    Logic around pagination of multi/match questions has been hoisted out of the LevelGroup view, with the multi-page (or single-page for /level/ URLs) arrangement determined by the appropriate controller, with the LevelGroup model providing them with the relevant data.
    
    Added a unit test for the model.
    
    Fixed some issues around hiding the multi/match heading when presented in a LevelGroup, though other content strings may now show properly.

--- dashboard/app/models/level_group.rb
+++ dashboard/app/models/level_group.rb
@@ -52,4 +52,36 @@ ruby
     Level.where(name: level_names)
   end
 
+  class LevelGroupPage
+    def initialize(page_properties, page_number, offset)
+      @levels = []
+      page_properties["levels"].each do |level_name|
+        level = Level.find_by_name(level_name)
+        @levels << level
+      end
+      @offset = offset
+      @page_number = page_number
+    end
+
+    attr_reader :levels
+    attr_reader :page_number
+    attr_reader :offset
+  end
+
+  # Returns an array of pages LevelGroupPage objects, each of which contains:
+  #   levels: an array of Levels.
+  #   page_number: the 1-based page number (corresponding to the /page/X URL).
+  #   page_offset: the count of questions occurring on prior pages.
+
+  def pages
+    offset = 0
+    page_count = 0
+    @pages ||= properties['pages'].map do |page|
+      page_count += 1
+      page_object = LevelGroupPage.new(page, page_count, offset)
+      offset += page_object.levels.count
+      page_object
+    end
+  end
+
 end
