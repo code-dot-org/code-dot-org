0294e4bf4ccae6574b0aec86f87690a1aeccf763
Will Jordan <will@code.org>
2016-04-06 16:30:38 -0700

    S3Packaging#download_package: use sdk client for s3 download when credentials are provided, http fallback otherwise. Ref: aws/aws-sdk-ruby#1149

--- lib/cdo/aws/s3_packaging.rb
+++ lib/cdo/aws/s3_packaging.rb
@@ -150,19 +150,26 @@ class S3Packaging
     diff.empty?
   end
 
-  # Downloads package from S3 using public URL.
+  # Downloads package from S3.
   # Throws a NoSuchKey error if given package doesn't exist on s3, or if the object is private.
   # @return tempfile for the downloaded package
   private def download_package
     package = Tempfile.new(@commit_hash)
 
     @logger.info "Attempting to download: #{s3_key}\nto #{package.path}"
-    url = Aws::S3::Bucket.new(BUCKET_NAME).object(s3_key).public_url
-    File.open(package, 'wb') do |file|
-      begin
-        IO.copy_stream open(url), file
-      rescue OpenURI::HTTPError
-        raise Aws::S3::Errors::NoSuchKey.new(nil, file)
+    begin
+      @client.get_object({bucket: BUCKET_NAME, key: s3_key}, target: package)
+    rescue Aws::Errors::MissingCredentialsError, Aws::S3::Errors::ServiceError
+      # Fallback to public-URL download over HTTP if credentials are not provided or invalid.
+      # TODO use aws-sdk to leverage aws-client optimizations once unsigned requests are supported:
+      # https://github.com/aws/aws-sdk-ruby/issues/1149
+      url = Aws::S3::Bucket.new(BUCKET_NAME).object(s3_key).public_url
+      File.open(package, 'wb') do |file|
+        begin
+            IO.copy_stream open(url), file
+        rescue OpenURI::HTTPError
+          raise Aws::S3::Errors::NoSuchKey.new(nil, file)
+        end
       end
     end
     @logger.info "Downloaded"
