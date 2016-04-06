4dd32c0c5e92bbef8d2cfa959b37dfc50af14492
Josh Lory <josh.lory@code.org>
2016-04-06 16:40:29 -0700

    Update droplet through https://github.com/droplet-editor/droplet/commit/8c1059707824e00f8baf57d4bad2e5972b7ec985

--- apps/lib/droplet/droplet-full.js
+++ apps/lib/droplet/droplet-full.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2016 Anthony Bau.
  * MIT License.
  *
- * Date: 2016-03-29
+ * Date: 2016-04-06
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.droplet = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
@@ -7265,10 +7265,12 @@ hook('mousedown', 4, function(point, event, state) {
     if ((hitTestBlock.addButtonRect != null) && hitTestBlock.addButtonRect.contains(mainPoint)) {
       line = this.mode.handleButton(str, 'add-button', hitTestResult.getReader());
       this.populateBlock(hitTestResult, line);
+      this.redrawMain();
       return state.consumedHitTest = true;
     } else if ((hitTestBlock.subtractButtonRect != null) && hitTestBlock.subtractButtonRect.contains(mainPoint)) {
       line = this.mode.handleButton(str, 'subtract-button', hitTestResult.getReader());
       this.populateBlock(hitTestResult, line);
+      this.redrawMain();
       return state.consumedHitTest = true;
     }
   }
