fe7c4017d2cbdb92f1b81aeb89cdb47c33f53ac2
Brad Buchanan <brad@code.org>
2016-04-06 18:11:54 -0700

    Convert to FSCs in a couple of obvious places

--- apps/src/gamelab/AnimationTab/AnimationFrameList.jsx
+++ apps/src/gamelab/AnimationTab/AnimationFrameList.jsx
@@ -15,18 +15,16 @@ var styles = {
  * Vertical scrolling list of animation frames for the selected animation.
  * This component should be unique within the animation tab.
  */
-var AnimationFrameList = React.createClass({
-  render: function () {
-    return (
-      <ScrollableList style={styles.root} className="animation-frame-list">
-        <AnimationFrameListItem index={1} />
-        <AnimationFrameListItem index={2} />
-        <AnimationFrameListItem index={3} isSelected />
-        <AnimationFrameListItem index={4} />
-        <AnimationFrameListItem index={151} />
-        <NewListItem label="new frame" onClick={function () {}} />
-      </ScrollableList>
-    );
-  }
-});
+var AnimationFrameList = function () {
+  return (
+    <ScrollableList style={styles.root} className="animation-frame-list">
+      <AnimationFrameListItem index={1} />
+      <AnimationFrameListItem index={2} />
+      <AnimationFrameListItem index={3} isSelected />
+      <AnimationFrameListItem index={4} />
+      <AnimationFrameListItem index={151} />
+      <NewListItem label="new frame" onClick={function () {}} />
+    </ScrollableList>
+  );
+};
 module.exports = AnimationFrameList;
