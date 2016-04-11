0d5075fdfc62747c716c0cb2daf7d3aa02008f4c
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-04-11 10:49:58 -0700

    alter level_concept_difficulties to use null rather than 0 for the null case

--- dashboard/db/migrate/20160404233038_create_level_concept_difficulties.rb
+++ dashboard/db/migrate/20160404233038_create_level_concept_difficulties.rb
@@ -4,16 +4,16 @@ class CreateLevelConceptDifficulties < ActiveRecord::Migration
       t.references :level, index: true, foreign_key: true
       t.timestamps null: false
 
-      t.integer :sequencing, default: 0, null: false
-      t.integer :debugging, default: 0, null: false
-      t.integer :repeat_loops, default: 0, null: false
-      t.integer :repeat_until_while, default: 0, null: false
-      t.integer :for_loops, default: 0, null: false
-      t.integer :events, default: 0, null: false
-      t.integer :variables, default: 0, null: false
-      t.integer :functions, default: 0, null: false
-      t.integer :functions_with_params, default: 0, null: false
-      t.integer :conditionals, default: 0, null: false
+      t.integer :sequencing, null: true
+      t.integer :debugging, null: true
+      t.integer :repeat_loops, null: true
+      t.integer :repeat_until_while, null: true
+      t.integer :for_loops, null: true
+      t.integer :events, null: true
+      t.integer :variables, null: true
+      t.integer :functions, null: true
+      t.integer :functions_with_params, null: true
+      t.integer :conditionals, null: true
     end
   end
 end
