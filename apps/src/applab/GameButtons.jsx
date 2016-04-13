ff8b5f36ec4df25099e08f568a9b654a74dd1b1c
Brent Van Minnen <bjvanminnen@gmail.com>
2016-04-13 13:05:31 -0700

    protect stuff

--- apps/src/applab/GameButtons.jsx
+++ apps/src/applab/GameButtons.jsx
@@ -1,11 +1,12 @@
 var msg = require('../locale');
 
 var CompletionButton = require('./CompletionButton');
+var ProtectedStatefulDiv = require('../templates/ProtectedStatefulDiv');
 var commonStyles = require('../commonStyles');
 
 var GameButtons = function (props) {
   return (
-    <div id="gameButtons">
+    <ProtectedStatefulDiv id="gameButtons">
       <button id="runButton" className="launch blocklyLaunch">
         <div>{msg.runProgram()}</div>
         <img src="/blockly/media/1x1.gif" className="run26"/>
@@ -16,7 +17,7 @@ var GameButtons = function (props) {
       </button>
       {" " /* Explicitly insert whitespace so that this behaves like our ejs file*/}
       <CompletionButton/>
-    </div>
+    </ProtectedStatefulDiv>
   );
 };
 
