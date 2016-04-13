cd6c63e56eddaf282776bf08ea6f6101428c35f2
Mehal Shah <mehal@code.org>
2016-04-13 15:26:10 -0700

    Use CodeMirror for editing markdown for written task assignments.
    CodeMirror was heavily assuming that everything using it was a level and appending level_ when it shouldn't

--- code-studio/src/js/plc/task_creation.js
+++ code-studio/src/js/plc/task_creation.js
@@ -1,6 +1,8 @@
 /* globals dashboard */
 
 var IconLibrary = require('../components/IconLibrary');
+var initializeCodeMirror = require('../initializeCodeMirror');
+var marked = require ('marked');
 
 $(window).load(function () {
   $('#toggleIconLibrary').click(function () {
@@ -12,6 +14,14 @@ $(window).load(function () {
     $('#previewIcon').removeClass();
     $('#iconDiv').toggle();
   });
+
+  if ($('#plc_written_assignment_task_assignment_description').length) {
+    var markdownEditor = initializeCodeMirror('plc_written_assignment_task_assignment_description', 'markdown', function (editor, change) {
+      $('#assignment_description_preview').html(marked(editor.getValue())).children('details').details();
+    }, true);
+
+    $('#assignment_description_preview').html(marked(markdownEditor.getValue())).children('details').details();
+  }
 });
 
 // TODO: Don't use one monolithic js file
