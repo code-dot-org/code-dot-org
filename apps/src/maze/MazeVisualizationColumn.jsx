945d50cb4a0283f86184a57912147c087e68872b
Brent Van Minnen <bjvanminnen@gmail.com>
2016-04-15 12:35:39 -0700

    maze specific viz col

---
+++ apps/src/maze/MazeVisualizationColumn.jsx
@@ -0,0 +1,29 @@
+var msg = require('../locale');
+var commonStyles = require('../commonStyles');
+
+var Visualization = require('./Visualization');
+var StepButton = require('./StepButton');
+var SpellingControls = require('./SpellingControls');
+var GameButtons = require('../templates/GameButtons');
+var BelowVisualization = require('../templates/BelowVisualization');
+
+var MazeVisualizationColumn = function (props) {
+  return (
+    <span>
+      <Visualization/>
+      <GameButtons hideRunButton={props.hideRunButton}>
+        <StepButton showStepButton={props.showStepButton}/>
+      </GameButtons>
+      {props.searchWord && <SpellingControls searchWord={props.searchWord}/>}
+      <BelowVisualization/>
+    </span>
+  );
+};
+
+MazeVisualizationColumn.propTypes = {
+  hideRunButton: React.PropTypes.bool.isRequired,
+  showStepButton: React.PropTypes.bool.isRequired,
+  searchWord: React.PropTypes.string
+};
+
+module.exports = MazeVisualizationColumn;
