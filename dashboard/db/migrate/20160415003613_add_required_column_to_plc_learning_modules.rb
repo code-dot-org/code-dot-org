513c3ac3fe5f4ff9e81f421e25196e0dac8bec78
Mehal Shah <mehal@code.org>
2016-04-14 17:52:26 -0700

    Kick stuff so it works

--- dashboard/db/migrate/20160415003613_add_required_column_to_plc_learning_modules.rb
+++ dashboard/db/migrate/20160415003613_add_required_column_to_plc_learning_modules.rb
@@ -1,5 +1,5 @@
 class AddRequiredColumnToPlcLearningModules < ActiveRecord::Migration
   def change
-    add_column :plc_learning_modules, :required, :boolean
+    add_column :plc_learning_modules, :required, :boolean, null: false, default: false
   end
 end
