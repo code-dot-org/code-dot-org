792f896ba240b3c8e267629bbd99faf9f046820b
Brent Van Minnen <bjvanminnen@gmail.com>
2016-04-19 12:07:48 -0700

    visualization column for calc

---
+++ apps/src/calc/CalcVisualizationColumn.jsx
@@ -0,0 +1,31 @@
+var msg = require('../locale');
+var commonStyles = require('../commonStyles');
+
+var GameButtons = require('../templates/GameButtons');
+var BelowVisualization = require('../templates/BelowVisualization');
+
+var CalcVisualizationColumn = function (props) {
+  return (
+    <span>
+      <div id="visualization">
+        <svg version="1.1" id="svgCalc">
+          <image id="background" height="400" width="400" x="0" y="0" xlinkHref="/blockly/media/skins/calc/background.png"></image>
+          <g id="userExpression" className="expr" transform="translate(0, 100)">
+          </g>
+          <g id="answerExpression" className="expr" transform="translate(0, 350)">
+          </g>
+        </svg>
+      </div>
+      <GameButtons hideRunButton={false}/>
+      <BelowVisualization inputOutputTable={props.inputOutputTable}/>
+    </span>
+  );
+};
+
+CalcVisualizationColumn.propTypes = {
+  inputOutputTable: React.PropTypes.arrayOf(
+    React.PropTypes.arrayOf(React.PropTypes.number)
+  ).isRequired
+};
+
+module.exports = CalcVisualizationColumn;
