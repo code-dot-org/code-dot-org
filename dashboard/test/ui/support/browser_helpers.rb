d802090cb623f2e87695395e5fec3dc59ecb4f71
Brian Jordan <bcjordan@gmail.com>
2016-04-19 11:33:26 -0700

    Fix i18n, conditionally require rails environment

--- dashboard/test/ui/support/browser_helpers.rb
+++ dashboard/test/ui/support/browser_helpers.rb
@@ -12,6 +12,7 @@ module BrowserHelpers
   end
 
   def element_has_i18n_text(selector, language, loc_key)
+    require_rails_env
     loc_key.gsub!('\"', '"')
     text = @browser.execute_script("return $(\"#{selector}\").text();")
     text.should eq I18n.t loc_key, locale: language
