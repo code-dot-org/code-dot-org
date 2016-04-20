f9da372ef93e1ea541748f35525c709657999c07
Brad Buchanan <brad@code.org>
2016-04-20 16:03:12 -0700

    Remove onComplete,onCancel from store

--- apps/src/gamelab/AnimationPicker/reducers.js
+++ apps/src/gamelab/AnimationPicker/reducers.js
@@ -18,36 +18,8 @@ function isShowing(state, action) {
   }
 }
 
-function onComplete(state, action) {
-  state = state || function () {};
-
-  switch (action.type) {
-    case AnimationPickerAction.SHOW_ANIMATION_PICKER:
-      return action.onComplete;
-    case AnimationPickerAction.HIDE_ANIMATION_PICKER:
-      return function () {};
-    default:
-      return state;
-  }
-}
-
-function onCancel(state, action) {
-  state = state || function () {};
-
-  switch (action.type) {
-    case AnimationPickerAction.SHOW_ANIMATION_PICKER:
-      return action.onCancel;
-    case AnimationPickerAction.HIDE_ANIMATION_PICKER:
-      return function () {};
-    default:
-      return state;
-  }
-}
-
 module.exports = {
   animationPicker: combineReducers({
     isShowing: isShowing,
-    onComplete: onComplete,
-    onCancel: onCancel
   })
 };
