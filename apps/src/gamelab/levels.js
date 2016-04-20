cd407de6ebcfc0c8e32a7cb2e07ce7426d54b2e1
cpirich <chris@code.org>
2016-04-20 15:41:56 -0700

    add new dpad, on by default, disable softButton by default

--- apps/src/gamelab/levels.js
+++ apps/src/gamelab/levels.js
@@ -17,12 +17,6 @@ levels.sandbox =  {
   scale: {
     'snapRadius': 2
   },
-  softButtons: [
-    'leftButton',
-    'rightButton',
-    'downButton',
-    'upButton'
-  ],
   freePlay: true,
   toolbox:
     tb(blockOfType('gamelab_foo')),
@@ -33,12 +27,6 @@ levels.sandbox =  {
 // Base config for levels created via levelbuilder
 levels.custom = utils.extend(levels.sandbox, {
   editCode: true,
-  softButtons: [
-    'leftButton',
-    'rightButton',
-    'downButton',
-    'upButton'
-  ],
   codeFunctions: {
     // Game Lab
     "draw": null,
@@ -207,6 +195,7 @@ levels.custom = utils.extend(levels.sandbox, {
     "callMyFunction_n": null,
     "return": null
   },
+  showDPad: true,
   startBlocks: null,
 });
 
