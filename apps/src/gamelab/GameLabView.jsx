38f35d2b682f6489b94e72eb6b71d9211ae1067b
Brent Van Minnen <bjvanminnen@gmail.com>
2016-04-20 17:04:54 -0700

    visualization column in react for gamelab

--- apps/src/gamelab/GameLabView.jsx
+++ apps/src/gamelab/GameLabView.jsx
@@ -7,6 +7,7 @@ var connect = require('react-redux').connect;
 var ConnectedStudioAppWrapper = require('../templates/ConnectedStudioAppWrapper');
 var GameLabInterfaceMode = require('./constants').GameLabInterfaceMode;
 var GameLabVisualizationHeader = require('./GameLabVisualizationHeader');
+var GameLabVisualizationColumn = require('./GameLabVisualizationColumn');
 var ProtectedStatefulDiv = require('../templates/ProtectedStatefulDiv');
 var InstructionsWithWorkspace = require('../templates/instructions/InstructionsWithWorkspace');
 
@@ -20,7 +21,7 @@ var GameLabView = React.createClass({
     isEmbedView: React.PropTypes.bool.isRequired,
     isShareView: React.PropTypes.bool.isRequired,
     generateCodeWorkspaceHtml: React.PropTypes.func.isRequired,
-    generateVisualizationColumnHtml: React.PropTypes.func.isRequired,
+    showFinishButton: React.PropTypes.bool.isRequired,
     onMount: React.PropTypes.func.isRequired
   },
 
@@ -40,7 +41,7 @@ var GameLabView = React.createClass({
       <div style={codeModeStyle}>
         <div id="visualizationColumn">
           {this.shouldShowHeader() && <GameLabVisualizationHeader />}
-          <ProtectedStatefulDiv contentFunction={this.props.generateVisualizationColumnHtml} />
+          <GameLabVisualizationColumn finishButton={this.props.showFinishButton}/>
         </div>
         <ProtectedStatefulDiv id="visualizationResizeBar" className="fa fa-ellipsis-v" />
         <InstructionsWithWorkspace
