65fa0880ca1c1ecde222d8637d7c45c3b88e4a4a
Brent Van Minnen <bjvanminnen@gmail.com>
2016-04-21 10:18:32 -0700

    arrow buttons component

--- apps/src/bounce/BounceVisualizationColumn.jsx
+++ apps/src/bounce/BounceVisualizationColumn.jsx
@@ -2,6 +2,7 @@ var msg = require('../locale');
 var commonStyles = require('../commonStyles');
 
 var GameButtons = require('../templates/GameButtons');
+var ArrowButtons = require('../templates/ArrowButtons');
 var BelowVisualization = require('../templates/BelowVisualization');
 
 var BounceVisualizationColumn = function (props) {
@@ -14,24 +15,10 @@ var BounceVisualizationColumn = function (props) {
         </div>
       </div>
       <GameButtons hideRunButton={false}>
-        <div id="soft-buttons" className="soft-buttons-none">
-          <button id="leftButton" disabled className="arrow">
-            <img src="/blockly/media/1x1.gif" className="left-btn icon21"/>
-          </button>
-          {" " /* Explicitly insert whitespace so that this behaves like our ejs file*/}
-          <button id="rightButton" disabled className="arrow">
-            <img src="/blockly/media/1x1.gif" className="right-btn icon21"/>
-          </button>
-          {" " /* Explicitly insert whitespace so that this behaves like our ejs file*/}
-          <button id="upButton" disabled className="arrow">
-            <img src="/blockly/media/1x1.gif" className="up-btn icon21"/>
-          </button>
-          {" " /* Explicitly insert whitespace so that this behaves like our ejs file*/}
-          <button id="downButton" disabled className="arrow">
-            <img src="/blockly/media/1x1.gif" className="down-btn icon21"/>
-          </button>
-        </div>
+        <ArrowButtons/>
+
         {" " /* Explicitly insert whitespace so that this behaves like our ejs file*/}
+
         <div id="share-cell-wrapper">
           <div id="share-cell" className="share-cell-none">
             <button id="finishButton" className="share">
