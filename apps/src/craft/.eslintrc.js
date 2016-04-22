bf0aba7bb993c3bd7d8bdae21bd1bf9ab5ceba12
Brad Buchanan <brad@code.org>
2016-04-22 12:27:05 -0700

    Convert eslint config files to yaml

--- apps/src/craft/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "parserOptions": {
-    // Ideally we would only support ES6 in craft until we've said we're going
-    // to support it everywhere.
-    "ecmaVersion": 6
-  }
-};
