5b7ac874a62d65fc2a8ce2bedb7a764c2dd513ef
Will Jordan <will@code.org>
2016-04-21 17:20:10 -0700

    Manage New Relic alert policies on instance startup/shutdown from cdo-newrelic cookbook prerequisite to auto-scaling instances. [ci skip]

--- cookbooks/cdo-newrelic/attributes/default.rb
+++ cookbooks/cdo-newrelic/attributes/default.rb
@@ -1,3 +1,6 @@
 default['cdo-newrelic'] = {
   'license-key'=>'0000000000000000000000000000000000000000',
+  'api-key'=>'00000000000000000000000000000000000000000000000',
+  'enabled_alert_policy_id' => 368270,
+  'disabled_alert_policy_id' => 355700
 }
