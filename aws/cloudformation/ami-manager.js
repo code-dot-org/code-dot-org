0f08846d9878d266a9f4a5622fbcb783c0942a5e
Will Jordan <will@code.org>
2016-04-22 14:44:42 -0700

    cleanup Lambda-js code - add comments - minify javascript passed to ZipFile-based Lambda functions to minimize size. - add length check to ZipFile-based Lambda function code.

--- aws/cloudformation/ami-manager.js
+++ aws/cloudformation/ami-manager.js
@@ -3,6 +3,8 @@
  * @see {@link https://blogs.aws.amazon.com/application-management/post/Tx38Z5CAM5WWRXW|Faster Auto Scaling in AWS CloudFormation Stacks with Lambda-backed Custom Resources}
  */
 
+// This module is automatically provided to ZipFile-based Lambda functions.
+// Ref: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-lambda-function-code.html#cfn-lambda-function-code-cfnresponsemodule
 var response = require('cfn-response');
 
 /** Takes an AWS CloudFormation stack name and instance id and returns the newly-created AMI ID. **/
@@ -13,19 +15,20 @@ exports.handler = function (event, context) {
   var instanceId = event.ResourceProperties.InstanceId;
   var instanceRegion = event.ResourceProperties.Region;
 
-  var responseStatus = "FAILED";
   var responseData = {};
   var physicalId = event.PhysicalResourceId;
 
   function error(err, msg) {
     responseData = {Error: msg};
     console.log(responseData.Error + ":\n", err);
-    response.send(event, context, responseStatus, responseData, physicalId);
+    response.send(event, context, response.FAILED, responseData, physicalId);
   }
 
   var AWS = require("aws-sdk");
   var ec2 = new AWS.EC2({region: instanceRegion});
 
+  // Valid RequestTypes: "Create", "Delete", "Update".
+  // Ref: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/crpg-ref-requesttypes.html
   console.log("REQUEST TYPE:", event.RequestType);
   if (event.RequestType == "Delete") {
     var params = {
@@ -35,8 +38,8 @@ exports.handler = function (event, context) {
       if (err) {
         error(err, "DescribeImages call failed");
       } else if (data.Images.length === 0) {
-        response.send(event, context, "SUCCESS", {Info: "Nothing to delete"});
-      } else {
+        response.send(event, context, response.SUCCESS, {Info: "Nothing to delete"});
+      } else if (data.Images.length == 1) {
         var imageId = data.Images[0].ImageId;
         console.log("DELETING:", data.Images[0]);
         ec2.deregisterImage({ImageId: imageId}, function (err, data) {
@@ -51,68 +54,69 @@ exports.handler = function (event, context) {
               if (err) {
                 error(err, "DescribeSnapshots call failed");
               } else if (data.Images.length === 0) {
-                response.send(event, context, "SUCCESS", {Info: "No snapshot to delete"});
+                response.send(event, context, response.SUCCESS, {Info: "No snapshot to delete"});
               } else {
                 ec2.deleteSnapshot({SnapshotId: data.Snapshots[0].SnapshotId}, function (err, data) {
                   if (err) {
                     error(err, "DeleteSnapshot call failed");
                   } else {
-                    responseStatus = "SUCCESS";
-                    response.send(event, context, responseStatus, responseData, imageId);
+                    response.send(event, context, response.SUCCESS, responseData, imageId);
                   }
                 });
               }
             });
           }
         });
+      } else {
+        error(null, "DescribeImages returned multiple Images, expected one");
       }
     });
-    return;
-  }
+  } else if (event.RequestType == "Update" || event.RequestType == "Create") {
+    if (stackName && instanceId && instanceRegion) {
+      ec2.createImage(
+        {
+          InstanceId: instanceId,
+          Name: stackName + '-' + instanceId + '-' + event.RequestId
+        }, function (err, data) {
+          if (err) {
+            error(err, "CreateImage call failed");
+          } else {
+            var imageId = data.ImageId;
+            physicalId = imageId;
+            console.log('SUCCESS: ', "ImageId - " + imageId);
 
-  if (stackName && instanceId && instanceRegion) {
-    ec2.createImage(
-      {
-        InstanceId: instanceId,
-        Name: stackName + '-' + instanceId + '-' + event.RequestId
-      }, function (err, data) {
-        if (err) {
-          error(err, "CreateImage call failed");
-        } else {
-          var imageId = data.ImageId;
-          physicalId = imageId;
-          console.log('SUCCESS: ', "ImageId - " + imageId);
-
-          var params = {
-            Resources: [imageId],
-            Tags: [
-              {
-                Key: 'cloudformation:amimanager:stack-name',
-                Value: stackName
-              },
-              {
-                Key: 'cloudformation:amimanager:stack-id',
-                Value: event.StackId
-              },
-              {
-                Key: 'cloudformation:amimanager:logical-id',
-                Value: event.LogicalResourceId
+            var params = {
+              Resources: [imageId],
+              Tags: [
+                {
+                  Key: 'cloudformation:amimanager:stack-name',
+                  Value: stackName
+                },
+                {
+                  Key: 'cloudformation:amimanager:stack-id',
+                  Value: event.StackId
+                },
+                {
+                  Key: 'cloudformation:amimanager:logical-id',
+                  Value: event.LogicalResourceId
+                }
+              ]
+            };
+            ec2.createTags(params, function (err, data) {
+              if (err) {
+                error(err, "Create tags call failed");
+              } else {
+                responseData.ImageId = imageId;
+                response.send(event, context, response.SUCCESS, responseData, imageId);
               }
-            ]
-          };
-          ec2.createTags(params, function (err, data) {
-            if (err) {
-              error(err, "Create tags call failed");
-            } else {
-              responseStatus = "SUCCESS";
-              responseData.ImageId = imageId;
-              response.send(event, context, responseStatus, responseData, imageId);
-            }
-          });
+            });
+          }
         }
-      }
-    );
+      );
+    } else {
+      error(null, "StackName, InstanceId or InstanceRegion not specified");
+    }
   } else {
-    error(null, "StackName, InstanceId or InstanceRegion not specified");
+    error(null, "Invalid RequestType: " + event.RequestType);
   }
 };
--- aws/cloudformation/stack-xref.js
+++ aws/cloudformation/stack-xref.js
@@ -5,7 +5,10 @@
  * @see {@link http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/walkthrough-custom-resources-lambda-cross-stack-ref.html|Walkthrough: Refer to Resources in Another Stack}
  */
 
+// This module is automatically provided to ZipFile-based Lambda functions.
+// Ref: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-lambda-function-code.html#cfn-lambda-function-code-cfnresponsemodule
 var response = require('cfn-response');
+
 function parseJson(str) {
   try {
     return JSON.parse(str);
@@ -13,33 +16,34 @@ function parseJson(str) {
     return str;
   }
 }
-exports.handler = function(event, context) {
-    console.log('REQUEST RECEIVED:\\n', JSON.stringify(event));
-    if (event.RequestType == 'Delete') {
-        response.send(event, context, response.SUCCESS);
-        return;
-    }
-    var stackName = event.ResourceProperties.StackName;
-    var responseData = {};
-    if (stackName) {
-        var aws = require('aws-sdk');
-        var cfn = new aws.CloudFormation();
-        cfn.describeStacks({StackName: stackName}, function(err, data) {
-            if (err) {
-                responseData = {Error: 'DescribeStacks call failed'};
-                console.log(responseData.Error + ':\\n', err);
-                response.send(event, context, response.FAILED, responseData);
-            }
-            else {
-                data.Stacks[0].Outputs.forEach(function(output) {
-                    responseData[output.OutputKey] = parseJson(output.OutputValue);
-                });
-                response.send(event, context, response.SUCCESS, responseData);
-            }
-        });
-    } else {
-        responseData = {Error: 'Stack name not specified'};
-        console.log(responseData.Error);
+
+exports.handler = function (event, context) {
+  console.log('REQUEST RECEIVED:\\n', JSON.stringify(event));
+  if (event.RequestType == 'Delete') {
+    response.send(event, context, response.SUCCESS);
+    return;
+  }
+  var stackName = event.ResourceProperties.StackName;
+  var responseData = {};
+  if (stackName) {
+    var aws = require('aws-sdk');
+    var cfn = new aws.CloudFormation();
+    cfn.describeStacks({StackName: stackName}, function (err, data) {
+      if (err) {
+        responseData = {Error: 'DescribeStacks call failed'};
+        console.log(responseData.Error + ':\\n', err);
         response.send(event, context, response.FAILED, responseData);
-    }
+      }
+      else {
+        data.Stacks[0].Outputs.forEach(function (output) {
+          responseData[output.OutputKey] = parseJson(output.OutputValue);
+        });
+        response.send(event, context, response.SUCCESS, responseData);
+      }
+    });
+  } else {
+    responseData = {Error: 'Stack name not specified'};
+    console.log(responseData.Error);
+    response.send(event, context, response.FAILED, responseData);
+  }
 };
