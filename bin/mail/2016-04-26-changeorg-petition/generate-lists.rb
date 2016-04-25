34e2364dfe0225e59c443ea53c3e78315866a659
Andrew Oberhardt <Andrew@Code.org>
2016-04-25 16:16:55 -0700

    Appease the Hound

--- bin/mail/2016-04-26-changeorg-petition/generate-lists.rb
+++ bin/mail/2016-04-26-changeorg-petition/generate-lists.rb
@@ -3,16 +3,16 @@ require_relative '../mailing-common/mailing-list-utils'
 
 # All US contacts
 us_contacts_query = %q(
-  !(role_s:student) && (
-    create_ip_country_s:"United States" ||
-    hoc_country_s:"us" ||
-    location_country_code_s:"US" ||
-    location_country_s:"United States"
+  !(role_s: student) && (
+    create_ip_country_s: "United States" ||
+    hoc_country_s: "us" ||
+    location_country_code_s: "US" ||
+    location_country_s: "United States"
   )
 )
 
 fields = %w[email_s name_s]
-us_contacts = query_subscribed_contacts(q: us_contacts_query, fl:fields)
+us_contacts = query_subscribed_contacts(q: us_contacts_query, fl: fields)
 
 puts "#{us-contacts.length} US contacts."
 export_contacts_to_csv us_contacts, 'us-contacts.csv'
