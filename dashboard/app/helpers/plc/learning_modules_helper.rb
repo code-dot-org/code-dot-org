6a1d92abf20340e7d331e8150b1d3e1eeb95c185
Laurel Fan <laurel@code.org>
2016-04-26 09:46:30 -0700

    merge staging

--- dashboard/app/helpers/plc/learning_modules_helper.rb
+++ dashboard/app/helpers/plc/learning_modules_helper.rb
@@ -2,4 +2,11 @@ module Plc::LearningModulesHelper
   def options_for_plc_task_learning_modules
     Plc::LearningModule.all.pluck(:name, :id).sort
   end
+
+  def options_for_evaluation_answer_modules(course_unit)
+    course_unit.plc_learning_modules.order(:required, :name).pluck(:id, :required, :name).map do |id, required, name|
+      # Boo Ruby, you can't add nil to a string without choking?
+      ["#{name}#{' - Required' if required}", id]
+    end
+  end
 end
