74ba3a2b51f5aa05492488deb7387d109e54de41
David Bailey <davidsbailey@users.noreply.github.com>
2016-04-27 15:39:06 -0700

    remove unused and dangerous apps/deploy.sh

--- apps/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require_relative '../deployment'
-require 'cdo/rake_utils'
-
-RakeUtils.sudo 'npm', 'install', '-g' 'grunt-cli'
-RakeUtils.sudo 'npm', 'install'
-RakeUtils.sudo 'grunt'
-RakeUtils.sudo 'chown' '-R' 'ubuntu:ubuntu' 'build/package'
