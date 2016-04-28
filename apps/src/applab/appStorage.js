a10a57c9663c656a2224af14224cb909143dd5ea
David Bailey <davidsbailey@users.noreply.github.com>
2016-04-28 11:14:29 -0700

    appease eslint

--- apps/src/applab/appStorage.js
+++ apps/src/applab/appStorage.js
@@ -47,13 +47,12 @@ function getTable(channelId, tableName) {
  */
 function getCounter(channelId, counterName, onSuccess, onError) {
   var counters = getDatabase(channelId).child('counters');
-  counters.child(counterName).transaction(function(currentValue) {
+  counters.child(counterName).transaction(function (currentValue) {
     return (currentValue || 0) + 1;
-  }, function(err, committed, data) {
-    if(err) {
+  }, function (err, committed, data) {
+    if (err) {
       onError(err);
-    }
-    else if (committed) {
+    } else if (committed) {
       // This should always be available if there was no error, since our
       // update functions always returns a value.
       onSuccess(data.val());
@@ -79,7 +78,7 @@ function getStatusDescription(status) {
 
 /**
  * Calls onError with a message generated from commandName and status and the status code
- * @param {function(string, number)} onError Function to call with error message and http status.
+ * @param {function (string, number)} onError Function to call with error message and http status.
  * @param {string} commandName App Lab command name to include in error message.
  * @param {number} status Http status code.
  * @param {string?} detailedErrorMessage Optional detailed error message.
@@ -107,13 +106,13 @@ function onErrorStatus(onError, commandName, status, detailedErrorMessage) {
 /**
  * Reads the value associated with the key, accessible to all users of the app.
  * @param {string} key The name of the key.
- * @param {function(Object)} onSuccess Function to call on success with the
+ * @param {function (Object)} onSuccess Function to call on success with the
        value retrieved from storage.
- * @param {function(string, number)} onError Function to call on error with error msg and http status.
+ * @param {function (string, number)} onError Function to call on error with error msg and http status.
  */
-AppStorage.getKeyValue = function(key, onSuccess, onError) {
+AppStorage.getKeyValue = function (key, onSuccess, onError) {
   var entry = getKeyValues(Applab.channelId).child(key);
-  entry.once("value", function(object) {
+  entry.once("value", function (object) {
     onSuccess(object.val());
   }, onError);
 };
@@ -122,13 +121,13 @@ AppStorage.getKeyValue = function(key, onSuccess, onError) {
  * Saves the value associated with the key, accessible to all users of the app.
  * @param {string} key The name of the key.
  * @param {Object} value The value to associate with the key.
- * @param {function()} onSuccess Function to call on success.
- * @param {function(string, number)} onError Function to call on error with error msg and
+ * @param {function ()} onSuccess Function to call on success.
+ * @param {function (string, number)} onError Function to call on error with error msg and
  *    http status.
  */
-AppStorage.setKeyValue = function(key, value, onSuccess, onError) {
+AppStorage.setKeyValue = function (key, value, onSuccess, onError) {
   var entries = getKeyValues(Applab.channelId);
-  entries.update(createEntry(key, value), function(error) {
+  entries.update(createEntry(key, value), function (error) {
     if (error) {
       onError();
     } else {
@@ -151,18 +150,18 @@ function createEntry(key, value) {
  * @param {string} tableName The name of the table to read from.
  * @param {Object} record Object containing other properties to store
  *     on the record.
- * @param {function(Object)} onSuccess Function to call with the new record.
- * @param {function(string, number)} onError Function to call with an error message
+ * @param {function (Object)} onSuccess Function to call with the new record.
+ * @param {function (string, number)} onError Function to call with an error message
  *    and http status in case of failure.
  */
-AppStorage.createRecord = function(tableName, record, onSuccess, onError) {
+AppStorage.createRecord = function (tableName, record, onSuccess, onError) {
   // Assign a unique id for the new record.
   var idCounter = tableName + '_id';
-  getCounter(Applab.channelId, idCounter, function(counter) {
+  getCounter(Applab.channelId, idCounter, function (counter) {
     record.id = counter;
     console.log(counter);
     var recordRef = getTable(Applab.channelId, tableName).child(counter);
-    recordRef.update(record, function(error) {
+    recordRef.update(record, function (error) {
       if (!error) {
 	onSuccess(record);
       } else {
@@ -178,7 +177,7 @@ AppStorage.createRecord = function(tableName, record, onSuccess, onError) {
  */
 function matchesSearch(record, searchParams) {
   var matches = true;
-  Object.keys(searchParams || {}).forEach(function(key) {
+  Object.keys(searchParams || {}).forEach(function (key) {
     matches = matches && (record[key] == searchParams[key]);
   });
   return matches;
@@ -191,12 +190,12 @@ function matchesSearch(record, searchParams) {
  * @param {string} searchParams.id Optional id of record to read.
  * @param {Object} searchParams Other search criteria. Only records
  *     whose contents match all criteria will be returned.
- * @param {function(Array)} onSuccess Function to call with an array of record
+ * @param {function (Array)} onSuccess Function to call with an array of record
        objects.
- * @param {function(string, number)} onError Function to call with an error message
+ * @param {function (string, number)} onError Function to call with an error message
  *     and http status in case of failure.
  */
-AppStorage.readRecords = function(tableName, searchParams, onSuccess, onError) {
+AppStorage.readRecords = function (tableName, searchParams, onSuccess, onError) {
   var table = getTable(Applab.channelId, tableName);
   var searchKeys = searchParams ? Object.keys(searchParams) : [];
 
@@ -204,9 +203,9 @@ AppStorage.readRecords = function(tableName, searchParams, onSuccess, onError) {
      // If there is only a single search parameter, use the firebase equalTo
      // query to return only the records matching that parameter from the server.
      var key = searchKeys[0];
-     table.orderByChild(key).equalTo(searchParams[key]).once('value', function(recordsRef) {
+     table.orderByChild(key).equalTo(searchParams[key]).once('value', function (recordsRef) {
        var recordMap = recordsRef.val() || {};
-       var records = Object.keys(recordMap).map(function(id) { return recordMap[id]; });
+       var records = Object.keys(recordMap).map(function (id) { return recordMap[id]; });
        onSuccess(records);
      }, onError);
    } else {
@@ -233,14 +232,14 @@ AppStorage.readRecords = function(tableName, searchParams, onSuccess, onError) {
  * @param {string} record.id The id of the row to update.
  * @param {Object} record Object containing other properties to update
  *     on the record.
- * @param {function(Object, boolean)} onComplete Function to call on success,
+ * @param {function (Object, boolean)} onComplete Function to call on success,
  *     or if the record id is not found.
- * @param {function(string, number)} onError Function to call with an error message
+ * @param {function (string, number)} onError Function to call with an error message
  *     and http status in case of other types of failures.
  */
-AppStorage.updateRecord = function(tableName, record, onComplete, onError) {
+AppStorage.updateRecord = function (tableName, record, onComplete, onError) {
   var recordRef = getTable(Applab.channelId, tableName).child(record.id);
-  recordRef.set(record, function(error) {
+  recordRef.set(record, function (error) {
     if (!error) {
       // TODO: We need to handle the 404 case, probably by attempting a read.
       onComplete(record, true);
@@ -255,14 +254,14 @@ AppStorage.updateRecord = function(tableName, record, onComplete, onError) {
  * @param {string} tableName The name of the table to delete from.
  * @param {string} record.id The id of the record to delete.
  * @param {Object} record Object whose other properties are ignored.
- * @param {function(boolean)} onComplete Function to call on success, or if the
+ * @param {function (boolean)} onComplete Function to call on success, or if the
  *     record id is not found.
- * @param {function(string, number)} onError Function to call with an error message
+ * @param {function (string, number)} onError Function to call with an error message
  *     and http status in case of other types of failures.
  */
-AppStorage.deleteRecord = function(tableName, record, onComplete, onError) {
+AppStorage.deleteRecord = function (tableName, record, onComplete, onError) {
   var table = getTable(Applab.channelId, tableName);
-  table.child(record.id).remove(function(error) {
+  table.child(record.id).remove(function (error) {
     if (error) {
       onError(error);
     } else {
@@ -278,9 +277,9 @@ AppStorage.deleteRecord = function(tableName, record, onComplete, onError) {
  * - for 'update' events, returns the updated record
  * - for 'delete' events, returns a record containing the id of the deleted record
  * @param {string} tableName Table to listen to.
- * @param {function(Object, RecordListener.EventType)} onRecord Callback to call when
+ * @param {function (Object, RecordListener.EventType)} onRecord Callback to call when
  * a change occurs with the record object (described above) and event type.
- * @param {function(string, number)} onError Callback to call with an error to show to the user and
+ * @param {function (string, number)} onError Callback to call with an error to show to the user and
  *   http status code.
  */
 AppStorage.onRecordEvent = function (tableName, onRecord, onError) {
@@ -295,15 +294,15 @@ AppStorage.onRecordEvent = function (tableName, onRecord, onError) {
   var table = getTable(Applab.channelId, tableName);
   // CONSIDER: Do we need to make sure a client doesn't hear about updates that it triggered?
 
-  table.on('child_added', function(childSnapshot, prevChildKey) {
+  table.on('child_added', function (childSnapshot, prevChildKey) {
     onRecord(childSnapshot.val(), 'create');
   });
 
-  table.on('value', function(dataSnapshot) {
+  table.on('value', function (dataSnapshot) {
     onRecord(dataSnapshot.val(), 'update');
   });
 
-  table.on('child_removed', function(oldChildSnapshot) {
+  table.on('child_removed', function (oldChildSnapshot) {
     onRecord(oldChildSnapshot.val(), 'delete');
   });
 };
@@ -320,8 +319,8 @@ AppStorage.resetRecordListener = function () {
  *     "table_name2": [{ "city": "Seattle", "state": "WA" }, { "city": "Chicago", "state": "IL"}]
  *   }
  * @param {bool} overwrite Whether to overwrite a table if it already exists.
- * @param {function()} onSuccess Function to call on success.
- * @param {function(string, number)} onError Function to call with an error message
+ * @param {function ()} onSuccess Function to call on success.
+ * @param {function (string, number)} onError Function to call with an error message
  *    and http status in case of failure.
  */
 AppStorage.populateTable = function (jsonData, overwrite, onSuccess, onError) {
@@ -331,7 +330,7 @@ AppStorage.populateTable = function (jsonData, overwrite, onSuccess, onError) {
   var tables = getDatabase(Applab.channelId).child('tables');
 
   // TODO: Respect overwrite
-  tables.set(jsonData, function(error) {
+  tables.set(jsonData, function (error) {
     if (error) {
       onError(error);
     } else {
@@ -348,8 +347,8 @@ AppStorage.populateTable = function (jsonData, overwrite, onSuccess, onError) {
  *     "button_color": "blue"
  *   }
  * @param {bool} overwrite Whether to overwrite a table if it already exists.
- * @param {function()} onSuccess Function to call on success.
- * @param {function(string, number)} onError Function to call with an error message
+ * @param {function ()} onSuccess Function to call on success.
+ * @param {function (string, number)} onError Function to call with an error message
  *    and http status in case of failure.
  */
 AppStorage.populateKeyValue = function (jsonData, overwrite, onSuccess, onError) {
@@ -359,7 +358,7 @@ AppStorage.populateKeyValue = function (jsonData, overwrite, onSuccess, onError)
   // TODO: Test this; Implement overwrite.
   var tables = getDatabase(Applab.channelId).child('tables');
   var keyValues = getKeyValues(Applab.channelId);
-  tables.set(jsonData, function(error) {
+  tables.set(jsonData, function (error) {
     if (!error) {
       onSuccess();
     } else {
