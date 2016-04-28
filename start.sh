9db1fa45a14565208de5b8247a68f396252b781a
David Pickett <pickettd@gmail.com>
2016-04-27 16:29:32 -1000

    Update logic to just remove dashboard and pegasus sock files, not whole unicorn directory. And suppress output of top command (top is used to keep container running for developing against the CDO system)

--- start.sh
+++ start.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-#service mysql start && echo 'ruby' && which ruby && echo 'chef' && ls /tmp/chef
-
-#echo 'trying to set'
-
-#echo $(id -u ubuntu) > /proc/self/loginuid
-
-#echo 'after set'
-
-#echo $(ls /var/chef/)
-
-#sudo -u ubuntu -H /bin/bash -c 'sudo chef-client -z -c /tmp/chef/zero.rb -j /tmp/chef/first-boot.json' ; top -b
-
-sudo rm -rf /var/run/unicorn
+sudo rm /var/run/unicorn/dashboard.sock
+sudo rm /var/run/unicorn/pegasus.sock
 
 sudo rm /home/ubuntu/adhoc/dashboard/config/unicorn.rb.pid
-
 sudo rm /home/ubuntu/adhoc/pegasus/config/unicorn.rb.pid
 
-sudo chef-client -z -c /tmp/chef/zero.rb -j /tmp/chef/first-boot.json ; top -b
+sudo chef-client -z -c /tmp/chef/zero.rb -j /tmp/chef/first-boot.json
+
+top -b >/dev/null 2>&1
