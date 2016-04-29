5f2ee0e248695a5adefb4a028a3d11e62dc70d74
Brent Van Minnen <Bjvanminnen@gmail.com>
2016-04-29 14:10:36 -0700

    Revert "Run mode indicators"

--- apps/src/templates/CodeWorkspace.jsx
+++ apps/src/templates/CodeWorkspace.jsx
@@ -1,188 +1,71 @@
-var Radium = require('radium');
 var ProtectedStatefulDiv = require('./ProtectedStatefulDiv');
 var JsDebugger = require('./JsDebugger');
 var msg = require('../locale');
 var commonStyles = require('../commonStyles');
-var styleConstants = require('../styleConstants');
-var color = require('../color');
-var experiments = require('../experiments');
 
 var styles = {
-  workspaceHeader: {
-    textAlign: 'center',
-    whiteSpace: 'nowrap',
-    overflowX: 'hidden',
-    height: styleConstants["workspace-headers-height"],
-    lineHeight: styleConstants["workspace-headers-height"] + 'px',
-  },
-  workspaceHeaderButton: {
-    cursor: 'pointer',
-    float: 'right',
-    overflow: 'hidden',
-    backgroundColor: color.light_purple,
-    marginTop: 3,
-    marginBottom: 3,
-    marginRight: 3,
-    marginLeft: 0,
-    height: 24,
-    borderRadius: 4,
-    fontFamily: '"Gotham 5r", sans-serif',
-    lineHeight: '18px',
-    ':hover': {
-      backgroundColor: color.cyan
-    }
-  },
-  workspaceHeaderButtonRunning: {
-    backgroundColor: color.lightest_purple
-  },
-  headerButtonSpan: {
-    paddingLeft: 12,
-    paddingRight: 12,
-    paddingTop: 0,
-    paddingBottom: 0
-  },
-  blocksGlyph: {
-    display: 'none',
-    height: 18,
-    verticalAlign: 'text-bottom',
-    paddingRight: 8
-  },
-  headerIcon: {
-    fontSize: 18
-  },
-  headerButtonIcon: {
-    lineHeight: '24px',
-    paddingRight: 8,
-    fontSize: 15
-  },
   noPadding: {
     padding: 0
-  },
-  bold: {
-    fontWeight: 'bold'
-  },
-  chevron: {
-    fontSize: 18,
-  },
-  runningChevron: {
-    color: color.dark_charcoal
   }
 };
 
-var CodeWorkspace = React.createClass({
-  propTypes: {
-    localeDirection: React.PropTypes.oneOf(['rtl', 'ltr']).isRequired,
-    editCode: React.PropTypes.bool.isRequired,
-    readonlyWorkspace: React.PropTypes.bool.isRequired,
-    showDebugger: React.PropTypes.bool,
-    isRunning: React.PropTypes.bool
-  },
-
-  shouldComponentUpdate: function (nextProps) {
-    // This component is current semi-protected. We don't want to completely
-    // disallow rerendering, since that would prevent us from being able to
-    // update styles. However, we do want to prevent property changes that would
-    // change the DOM structure.
-    Object.keys(nextProps).forEach(function (key) {
-      // isRunning only affects style, and can be updated
-      if (key === 'isRunning') {
-        return;
-      }
-
-      if (nextProps[key] !== this.props[key]) {
-        throw new Error('Attempting to change key ' + key + ' in CodeWorkspace');
-      }
-    }.bind(this));
-
-    // Should be no need to update unless we have runModeIndicators enabled
-    return experiments.isEnabled('runModeIndicators');
-  },
-
-  render: function () {
-    var props = this.props;
-
-    var runModeIndicators = experiments.isEnabled('runModeIndicators');
-
-    var chevronStyle = [
-      commonStyles.hidden,
-      styles.chevron,
-      runModeIndicators && props.isRunning && styles.runningChevron
-    ];
-
-    var headerButtonStyle = [
-      styles.workspaceHeaderButton,
-      runModeIndicators && props.isRunning && styles.workspaceHeaderButtonRunning
-    ];
-
-    return (
-      <span id="codeWorkspaceWrapper">
-        <div
-            id="headers"
-            dir={props.localeDirection}
-            style={[
-              commonStyles.purpleHeader,
-              props.readonlyWorkspace && commonStyles.purpleHeaderReadOnly,
-              runModeIndicators && props.isRunning && commonStyles.purpleHeaderRunning
-            ]}
-        >
-          <div id="codeModeHeaders">
-            <div id="toolbox-header" style={styles.workspaceHeader}>
-              <i id="hide-toolbox-icon" style={chevronStyle} className="fa fa-chevron-circle-right"/>
-              <span>{props.editCode ? msg.toolboxHeaderDroplet() : msg.toolboxHeader()}</span>
-            </div>
-            <div id="show-toolbox-header" style={[styles.workspaceHeader, commonStyles.hidden]}>
-              <i id="show-toolbox-icon" styles={styles.headerIcon} className="fa fa-chevron-circle-right"/>
-              <span>{msg.showToolbox()}</span>
-            </div>
-            <div
-                id="show-code-header"
-                key="show-code-header"
-                style={[styles.workspaceHeader, headerButtonStyle]}>
-              <span style={styles.headerButtonSpan}>
-                <img src="/blockly/media/applab/blocks_glyph.gif" style={styles.blocksGlyph} />
-                <i className="fa fa-code" style={[styles.bold, styles.headerButtonIcon]}/>
-                <span style={styles.noPadding}>{msg.showCodeHeader()}</span>
-              </span>
-            </div>
-            {!props.readonlyWorkspace &&
-            <div
-                id="clear-puzzle-header"
-                key="clear-puzzle-header"
-                style={[styles.workspaceHeader, headerButtonStyle]}>
-              <span style={styles.headerButtonSpan}>
-                <i className="fa fa-undo" style={styles.headerButtonIcon}/>
-                <span style={styles.noPadding}>{msg.clearPuzzle()}</span>
-              </span>
-            </div>
-            }
-            <div
-                id="versions-header"
-                key="versions-header"
-                style={[styles.workspaceHeader, headerButtonStyle]}>
-              <span style={styles.headerButtonSpan}>
-                <i className="fa fa-clock-o" style={styles.headerButtonIcon}/>
-                <span style={styles.noPadding}>{msg.showVersionsHeader()}</span>
-              </span>
-            </div>
-            <div id="workspace-header" style={styles.workspaceHeader}>
-              <span id="workspace-header-span">
-                {props.readonlyWorkspace ? msg.readonlyWorkspaceHeader() : msg.workspaceHeaderShort()}
-              </span>
-              <div id="blockCounter">
-                <div id="blockUsed" className='block-counter-default'>
-                </div>
-                <span> / </span>
-                <span id="idealBlockNumber"></span>
-                <span>{" " + msg.blocks()}</span>
+var CodeWorkspace = function (props) {
+  return (
+    <span id="codeWorkspaceWrapper">
+      <ProtectedStatefulDiv id="headers" dir={props.localeDirection}>
+        <div id="codeModeHeaders">
+          <div id="toolbox-header" className="workspace-header">
+            <i id="hide-toolbox-icon" style={commonStyles.hidden} className="fa fa-chevron-circle-right"/>
+            <span>{props.editCode ? msg.toolboxHeaderDroplet() : msg.toolboxHeader()}</span>
+          </div>
+          <div id="show-toolbox-header" className="workspace-header" style={commonStyles.hidden}>
+            <i id="show-toolbox-icon" className="fa fa-chevron-circle-right"></i>
+            <span>{msg.showToolbox()}</span>
+          </div>
+          <div id="show-code-header" className="workspace-header workspace-header-button">
+            <span>
+              <img src="/blockly/media/applab/blocks_glyph.gif" className="blocks-glyph" />
+              <i className="fa fa-code"/>
+              <span style={styles.noPadding}>{msg.showCodeHeader()}</span>
+            </span>
+          </div>
+          {!props.readonlyWorkspace && <div id="clear-puzzle-header" className="workspace-header workspace-header-button">
+            <span>
+              <i className="fa fa-undo"/>
+              <span style={styles.noPadding}>{msg.clearPuzzle()}</span>
+            </span>
+          </div>}
+          <div id="versions-header" className="workspace-header workspace-header-button">
+            <span>
+              <i className="fa fa-clock-o"/>
+              <span style={styles.noPadding}>{msg.showVersionsHeader()}</span>
+            </span>
+          </div>
+          <div id="workspace-header" className="workspace-header">
+            <span id="workspace-header-span">
+              {props.readonlyWorkspace ? msg.readonlyWorkspaceHeader() : msg.workspaceHeaderShort()}
+            </span>
+            <div id="blockCounter">
+              <div id="blockUsed" className='block-counter-default'>
               </div>
+              <span> / </span>
+              <span id="idealBlockNumber"></span>
+              <span>{" " + msg.blocks()}</span>
             </div>
           </div>
         </div>
-        {props.editCode && <ProtectedStatefulDiv id="codeTextbox"/>}
-        {props.showDebugger && <JsDebugger/>}
-      </span>
-    );
-  }
-});
+      </ProtectedStatefulDiv>
+      {props.editCode && <ProtectedStatefulDiv id="codeTextbox"/>}
+      {props.showDebugger && <JsDebugger/>}
+    </span>
+  );
+};
 
-module.exports = Radium(CodeWorkspace);
+CodeWorkspace.propTypes = {
+  localeDirection: React.PropTypes.oneOf(['rtl', 'ltr']).isRequired,
+  editCode: React.PropTypes.bool.isRequired,
+  readonlyWorkspace: React.PropTypes.bool.isRequired,
+  showDebugger: React.PropTypes.bool
+};
+
+module.exports = CodeWorkspace;
