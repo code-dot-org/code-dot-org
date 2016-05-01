5c65243cd5a3b5f4fc24688ef4268f1dc95e8141
Josh Lory <josh.lory@code.org>
2016-04-30 21:43:46 -0700

    Update rake tests

--- dashboard/test/dsl/dsl_test.rb
+++ dashboard/test/dsl/dsl_test.rb
@@ -24,7 +24,8 @@ level 'Level 5'
         {:stage=>"Stage2", :levels=>[{:name=>"Level 4", :stage=>"Stage2"},
                                      {:name=>"Level 5", :stage=>"Stage2"}]}],
                 :hidden=>true, :trophies=>false, :wrapup_video=>nil,
-                :login_required=>false, admin_required: false, :pd=>false}
+                :login_required=>false, admin_required: false, :pd=>false,
+                :professional_learning_course => nil}
 
     i18n_expected = {'en'=>{'data'=>{'script'=>{'name'=>{'test'=>{
         'Stage1'=>'Stage1',
@@ -40,7 +41,7 @@ admin_required true
 "
     output, _ = ScriptDSL.parse(input_dsl, 'test.script', 'test')
 
-    expected = {id: nil, stages: [], hidden: true, trophies: false, wrapup_video: nil, login_required: false, admin_required: true, pd: false}
+    expected = {id: nil, stages: [], hidden: true, trophies: false, wrapup_video: nil, login_required: false, admin_required: true, pd: false, professional_learning_course: nil}
 
     assert_equal expected, output
   end
@@ -51,7 +52,7 @@ admin_required 'true'
 "
     output, _ = ScriptDSL.parse(input_dsl, 'test.script', 'test')
 
-    expected = {id: nil, stages: [], hidden: true, trophies: false, wrapup_video: nil, login_required: false, admin_required: true, pd: false}
+    expected = {id: nil, stages: [], hidden: true, trophies: false, wrapup_video: nil, login_required: false, admin_required: true, pd: false, professional_learning_course: nil}
 
     assert_equal expected, output
   end
