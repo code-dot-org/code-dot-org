9ed813f94114e465c3b5e9db25c21f81ba073dcc
Josh Lory <josh.lory@code.org>
2016-05-02 14:25:35 -0700

    Code review feedback: destroy old PLC objects manually

--- dashboard/db/migrate/20160430000244_add_script_level_to_tasks.rb
+++ dashboard/db/migrate/20160430000244_add_script_level_to_tasks.rb
@@ -1,6 +1,5 @@
 class AddScriptLevelToTasks < ActiveRecord::Migration
   def change
-    Plc::Task.destroy_all
-    add_reference :plc_tasks, :script_level, index: true, foreign_key: true, required: true
+    add_reference :plc_tasks, :script_level, index: true, foreign_key: true
   end
 end
