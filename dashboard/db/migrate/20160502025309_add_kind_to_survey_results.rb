cdc493162ca837dd352bc9ddd249864445e47718
Asher Kach <asher@code.org>
2016-05-02 07:24:12 -0500

    Adds, populates, and indexes the kind field (column) of the SurveyResult's model (database).

---
+++ dashboard/db/migrate/20160502025309_add_kind_to_survey_results.rb
@@ -0,0 +1,15 @@
+class AddKindToSurveyResults < ActiveRecord::Migration
+  def change
+    # The `kind` column will be used to store the kind of survey, e.g.,
+    # DiversityYYYY, NetPromoterScoreYYYY, etc.
+    add_column :survey_results, :kind, :string, :after => :user_id
+    add_index :survey_results, :kind
+
+    reversible do |direction|
+      direction.up do
+        # On 2016-04-30, existing rows are the diversity survey from March 2016.
+        SurveyResult.update_all(kind: 'Diversity2016')
+      end
+    end 
+  end
+end
