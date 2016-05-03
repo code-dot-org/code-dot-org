4947a0c08f11b24357b3bbc1ed86c6b82091edce
Asher Kach <asher@code.org>
2016-05-03 10:42:40 -0500

    Add tests.

--- dashboard/test/controllers/survey_results_controller_test.rb
+++ dashboard/test/controllers/survey_results_controller_test.rb
@@ -6,7 +6,7 @@ class SurveyResultsControllerTest < ActionController::TestCase
     @teacher = create(:teacher)
   end
 
-  test "post survey results" do
+  test 'post diveristy survey results' do
     sign_in @teacher
 
     assert_creates(SurveyResult) do
@@ -18,6 +18,35 @@ class SurveyResultsControllerTest < ActionController::TestCase
     assert_equal 'Diversity2016', survey_result.kind
     assert_equal 22, survey_result["properties"]["survey2016_ethnicity_asian"]
     assert_equal 3, survey_result["properties"]["survey2016_foodstamps"]
+  end
+
+  test 'post net promoter score survey results' do
+    sign_in @teacher
+
+    NPS_VALUE = 10
+    NPS_COMMENT = "Rock on"
+    assert_creates(SurveyResult) do
+      post :create, {survey: {kind: 'NetPromoterScore2015', nps_value: NPS_VALUE, nps_comment: NPS_COMMENT}, format: :json}
+    end
+
+    survey_result = SurveyResult.where(user: @teacher).first
+    assert survey_result
+    assert_equal 'NetPromoterScore2015', survey_result.kind
+    assert_equal NPS_VALUE, survey_result.properties['nps_value']
+    assert_equal NPS_COMMENT, survey_result.properties['nps_comment']
+  end
+
+  test 'blocks non-whitelisted parameters' do
+    sign_in @teacher
+
+    assert_creates(SurveyResult) do
+      post :create, {survey: {kind: 'Diversity2016', nonwhitelisted: 'untrusted data'}, format: :json}
+    end
+
+    survey_result = SurveyResult.where(user: @teacher).first
+    assert survey_result
+    assert_equal 'Diversity2016', survey_result.kind
+    assert survey_result['properties']['nonwhitelisted'].nil?
   end
 
 end
