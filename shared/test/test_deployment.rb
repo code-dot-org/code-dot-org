a7bcaa86a6d983aaedd92e9586a0757eb6eca6e6
Will Jordan <will@code.org>
2016-05-04 10:57:03 -0700

    add test

---
+++ shared/test/test_deployment.rb
@@ -0,0 +1,50 @@
+require_relative 'test_helper'
+require 'cdo/rake_utils'
+require 'rspec/expectations'
+require 'rspec/mocks'
+
+class DeploymentTest < Minitest::Test
+
+  # Use rspec-style mocks in Minitest.
+  include ::RSpec::Mocks::ExampleMethods
+
+  def before_setup
+    RSpec::Mocks.setup
+  end
+
+  def after_teardown
+    begin
+      RSpec::Mocks.verify
+    ensure
+      RSpec::Mocks.teardown
+    end
+  end
+
+  # Approximates the parts of the JSON output from `knife search node` that we use.
+  KNIFE_SERVERS = {
+    results: 2,
+    rows: [
+      {
+        a: {
+          cloud_v2: {
+            local_hostname: 'host_a'
+          }
+        }
+      },
+      {
+        b: {
+          cloud_v2: {
+            local_hostname: 'host_b'
+          }
+        }
+      }
+    ]
+  }.to_json
+
+  def test_app_servers
+    allow(RakeUtils).to receive(:with_bundle_dir){|&blk| blk.call}
+    allow(CDO).to receive(:`).and_return(KNIFE_SERVERS)
+
+    assert_equal({'a' => 'host_a', 'b' => 'host_b'}, CDO.app_servers)
+  end
+end
