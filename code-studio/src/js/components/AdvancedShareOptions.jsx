e13cd5051f1907ad2766ddf47f2275788835897d
Paul Carduner <paul@carduner.net>
2016-05-04 14:14:01 -0700

    add a note about shared width and height constants

--- code-studio/src/js/components/AdvancedShareOptions.jsx
+++ code-studio/src/js/components/AdvancedShareOptions.jsx
@@ -75,6 +75,8 @@ var AdvancedShareOptions = Radium(React.createClass({
 
   renderEmbedTab() {
     var url = window.location.href.replace('edit','embed');
+    // If you change this width and height, make sure to update the
+    // #visualizationColumn.wireframeShare css
     var iframeHtml = '<iframe width="352" height="612" style="border: 1px solid black;" src="' +
           url + '"></iframe>';
     return (
