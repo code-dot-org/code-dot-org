f812618d234d2e4874d30e96ed1e5af68c072b79
Josh Lory <josh.lory@code.org>
2016-05-05 12:41:35 -0700

    Commit modified schema and annotations

--- dashboard/db/migrate/20160505193334_create_peer_reviews.rb
+++ dashboard/db/migrate/20160505193334_create_peer_reviews.rb
@@ -2,10 +2,10 @@ class CreatePeerReviews < ActiveRecord::Migration
   def change
     create_table :peer_reviews do |t|
       t.references :user, index: true, foreign_key: true
-      t.boolean :from_instructor
-      t.references :script, index: true, foreign_key: true
-      t.references :level, index: true, foreign_key: true
-      t.references :level_source, index: true, foreign_key: true
+      t.boolean :from_instructor, null: false, default: false
+      t.references :script, index: true, foreign_key: true, null: false
+      t.references :level, index: true, foreign_key: true, null: false
+      t.references :level_source, index: true, foreign_key: true, null: false
       t.text :data
       t.integer :status
 
