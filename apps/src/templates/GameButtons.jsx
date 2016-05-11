1f3893c20b7f1fb5f751335506b5e0f8b67ecd5e
Brent Van Minnen <bjvanminnen@gmail.com>
2016-05-11 11:44:16 -0700

    move run/reset into frame

--- apps/src/templates/GameButtons.jsx
+++ apps/src/templates/GameButtons.jsx
@@ -2,6 +2,9 @@ var msg = require('../locale');
 
 var ProtectedStatefulDiv = require('./ProtectedStatefulDiv');
 var commonStyles = require('../commonStyles');
+var experiments = require('../experiments');
+var classNames = require('classnames');
+var Radium = require('radium');
 
 var styles = {
   instructionsInTopPane: {
@@ -12,28 +15,56 @@ var styles = {
   }
 };
 
+var RunButton = function (props) {
+  return (
+    <button
+        id="runButton"
+        className={classNames(['launch', 'blocklyLaunch', props.hidden && 'invisible'])}
+        style={props.style}
+    >
+      <div>{msg.runProgram()}</div>
+      <img src="/blockly/media/1x1.gif" className="run26"/>
+    </button>
+  );
+};
+RunButton.propTypes = {
+  hidden: React.PropTypes.bool,
+  style: React.PropTypes.object
+};
+
+var ResetButton = Radium(function (props) {
+  return (
+    <button
+        id="resetButton"
+        className="launch blocklyLaunch"
+        style={[commonStyles.hidden, props.style]}
+    >
+      <div>{msg.resetProgram()}</div>
+      <img src="/blockly/media/1x1.gif" className="reset26"/>
+    </button>
+  );
+});
+ResetButton.propTypes = {
+  style: React.PropTypes.object
+};
+
 /**
  * A set of game buttons that consist of a run/reset button, and potentially a
  * set of children that we expect to be additional buttons.
  */
 var GameButtons = function (props) {
-  var runButtonClasses = "launch blocklyLaunch";
-  if (props.hideRunButton) {
-    runButtonClasses += " invisible";
-  }
+  var phoneFrame = experiments.isEnabled('phoneFrame');
 
   return (
     <ProtectedStatefulDiv
         id="gameButtons"
         style={props.instructionsInTopPane ? styles.instructionsInTopPane : undefined}>
-      <button id="runButton" className={runButtonClasses}>
-        <div>{msg.runProgram()}</div>
-        <img src="/blockly/media/1x1.gif" className="run26"/>
-      </button>
-      <button id="resetButton" className="launch blocklyLaunch" style={commonStyles.hidden}>
-        <div>{msg.resetProgram()}</div>
-        <img src="/blockly/media/1x1.gif" className="reset26"/>
-      </button>
+      {!phoneFrame &&
+      <RunButton hidden={props.hideRunButton}/>
+      }
+      {!phoneFrame &&
+      <ResetButton/>
+      }
       {" " /* Explicitly insert whitespace so that this behaves like our ejs file*/}
       {props.children}
     </ProtectedStatefulDiv>
@@ -46,3 +77,5 @@ GameButtons.propTypes = {
 };
 
 module.exports = GameButtons;
+GameButtons.RunButton = RunButton;
+GameButtons.ResetButton = ResetButton;
