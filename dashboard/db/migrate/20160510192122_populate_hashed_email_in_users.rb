5b68d5bb7b436a4cf05cf57e3c8b680a31261986
Asher Kach <asher@code.org>
2016-05-11 13:43:10 -0500

    merging

--- dashboard/db/migrate/20160510192122_populate_hashed_email_in_users.rb
+++ dashboard/db/migrate/20160510192122_populate_hashed_email_in_users.rb
@@ -2,17 +2,13 @@
 # for users having an (non-empty) email address.
 
 class PopulateHashedEmailInUsers < ActiveRecord::Migration
-  def change
-    reversible do |direction|
-      direction.up do
-        User.with_deleted.
-            where('email IS NOT NULL AND email <> ""').
-            where('hashed_email IS NULL OR hashed_email = ""').
-            find_each do |user|
-          user.hash_email
-          user.save!
-        end
-      end
+  def up
+    User.with_deleted.
+        where('email IS NOT NULL AND email <> ""').
+        where('hashed_email IS NULL OR hashed_email = ""').
+        find_each do |user|
+      user.hash_email
+      user.save!
     end
   end
 end
