6d92c4087158f3ed3849c46c1311483750425749
Brad Buchanan <brad@code.org>
2016-05-13 11:41:46 -0700

    Don't run httpServer for unit tests

--- apps/test/util/runTests.js
+++ apps/test/util/runTests.js
@@ -4,43 +4,9 @@
 // current directory to be our root
 var which = require('npm-which')(__dirname + '/../..').sync;
 var mochify = require('mochify');
-var http = require('http');
-var fs = require('fs');
 
 var exec = require('child_process').exec;
 var command = which('linklocal');
-
-/**
- * Mochify uses phantomic to run a server that it connects to, which serves up
- * the generated bundle. I couldn't find a way to get it to serve up non-bundle
- * code as well. Instead, I'm creating my own server which serves up files
- * from cdo/apps/lib. This way I can script inject some necessary files.
- */
-function httpServer(port, callback) {
-  var server = http.createServer(function (req, res) {
-    var url = req.url;
-    var p = url.indexOf('?');
-    if (p !== -1) {
-      url = url.substring(0, p);
-    }
-
-    // navigate from root
-    var filepath = __dirname + '/../../..' + url;
-    if (!fs.existsSync(filepath)) {
-      console.log('404: ' + filepath);
-      res.writeHead(404);
-      res.end();
-    } else {
-      res.writeHead(200);
-      fs.createReadStream(filepath).pipe(res);
-    }
-  });
-  server.timeout = 0;
-  server.listen(port);
-  return server;
-}
-
-var libServer = httpServer(8001);
 
 exec(command, function (err, stdout, stderr) {
   if (err) {
@@ -92,8 +58,5 @@ exec(command, function (err, stdout, stderr) {
    console.trace();
    process.exit(1);
   })
-  .bundle()
-  .on('end', function () {
-    libServer.close();
-  });
+  .bundle();
 });
