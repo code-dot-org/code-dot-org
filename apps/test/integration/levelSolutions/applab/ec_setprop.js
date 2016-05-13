59c1e8b2d27b3bcc50e2a74f01b6c2e9512c0acf
Brad Buchanan <brad@code.org>
2016-05-13 15:17:12 -0700

    Move tickWrapper to integration tests

--- apps/test/integration/levelSolutions/applab/ec_setprop.js
+++ apps/test/integration/levelSolutions/applab/ec_setprop.js
@@ -1,5 +1,5 @@
 var testUtils = require('../../../util/testUtils');
-var tickWrapper = require('../../../util/tickWrapper');
+var tickWrapper = require('../../util/tickWrapper');
 var TestResults = require('@cdo/apps/constants').TestResults;
 var _ = require('@cdo/apps/lodash');
 
