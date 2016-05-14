2b7b79347d3992394b1ef7cf3c8bb1d5fb7287a4
Brendan Reville <brendan@code.org>
2016-05-13 22:36:14 -0700

    district dropdown: Minor cleanup.

--- dashboard/app/controllers/api/v1/school_districts_controller.rb
+++ dashboard/app/controllers/api/v1/school_districts_controller.rb
@@ -3,18 +3,6 @@ class Api::V1::SchoolDistrictsController < ApplicationController
   # GET /api/v1/school_district.csv
   def index
     render json: ActiveModel::Serializer.new(SchoolDistrict.where(state: params[:state]), each_serializer: Api::V1::SchoolDistrictSerializer)
-
-    #respond_to do |format|
-      #render json: ActiveModel::Serializer.new(SchoolDistrict.where(state: "CA"), each_serializer: Api::V1::SchoolDistrictSerializer).to_json
-      # .where("name LIKE :prefix", prefix: "S%")
-      #format.json {render json: SchoolDistrict.where(state: "CA"), each_serializer: Api::V1::SchoolDistrictSerializer}
-    #end
-  end
-
-  def default_serializer_options
-  {
-    root: false
-  }
   end
 
 end
