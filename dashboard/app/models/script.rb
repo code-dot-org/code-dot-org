91f30ea235e1a76b2aa8be964bda442e50681a56
Tanya Parker <tanyacparker@gmail.com>
2016-05-16 11:18:20 -0700

    add banner images for courses csp1 and csp2

--- dashboard/app/models/script.rb
+++ dashboard/app/models/script.rb
@@ -339,7 +339,7 @@ class Script < ActiveRecord::Base
   end
 
   def has_banner?
-    k5_course? || %w(cspunit1 cspunit2 cspunit3).include?(self.name)
+    k5_course? || %w(csp1 csp2 cspunit1 cspunit2 cspunit3).include?(self.name)
   end
 
   def freeplay_links
