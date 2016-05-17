1097d08d930117cf0fb423c7097e76b90b87e76b
Brad Buchanan <brad@code.org>
2016-05-16 17:32:50 -0700

    Move unit tests into test/unit

--- apps/test/ejsTest.js
+++
@@ -1,8 +0,0 @@
-import {assert} from './util/configuredChai';
-// nonstandard EJS behavior we rely upon in our templates
-describe("ejs test", function () {
-  it("renders empty string on undefined object property access", function () {
-    var ejs = require('ejs').render('<%- data.test %>', {data:{}});
-    assert.equal(ejs, '');
-  });
-});
