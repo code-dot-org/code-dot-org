fb92626f10d16e8519a4279ae1ad186bad35c2ec
Brendan Reville <brendan@code.org>
2016-05-17 14:35:35 -0700

    district dropdown: code review tidying

--- dashboard/app/models/school_district.rb
+++ dashboard/app/models/school_district.rb
@@ -27,7 +27,7 @@ class SchoolDistrict < ActiveRecord::Base
 
   # Use the zero byte as the quote character to allow importing double quotes
   #   via http://stackoverflow.com/questions/8073920/importing-csv-quoting-error-is-driving-me-nuts
-  CSV_IMPORT_OPTIONS = { col_sep: "\t", headers: true, :quote_char => "\x00" }
+  CSV_IMPORT_OPTIONS = { col_sep: "\t", headers: true, quote_char: "\x00" }
 
   def self.find_or_create_all_from_tsv!(filename)
     created = []
@@ -38,11 +38,12 @@ class SchoolDistrict < ActiveRecord::Base
   end
 
   def self.first_or_create_from_tsv_row!(row_data)
-    params = {id: row_data[CSV_HEADERS[:id]],
-              name: row_data[CSV_HEADERS[:name]],
-              city: row_data[CSV_HEADERS[:city]],
-              state: row_data[CSV_HEADERS[:state]],
-              zip: row_data[CSV_HEADERS[:zip]]}
+    params = {
+      id: row_data[CSV_HEADERS[:id]],
+      name: row_data[CSV_HEADERS[:name]],
+      city: row_data[CSV_HEADERS[:city]],
+      state: row_data[CSV_HEADERS[:state]],
+      zip: row_data[CSV_HEADERS[:zip]]}
     SchoolDistrict.where(params).first_or_create!
   end
 
