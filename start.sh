4eeb7e2880092db3be2b3181a1eaf46ea24e33f5
David Pickett <pickettd@gmail.com>
2016-05-17 09:41:28 -1000

    Update start script to make sure new migrations are run on container start

--- start.sh
+++ start.sh
@@ -6,6 +6,12 @@ sudo rm /var/run/unicorn/pegasus.sock
 sudo rm /home/ubuntu/adhoc/dashboard/config/unicorn.rb.pid
 sudo rm /home/ubuntu/adhoc/pegasus/config/unicorn.rb.pid
 
+cd /home/ubuntu/adhoc/dashboard
+sudo bundle exec rake db:migrate
+
+cd /home/ubuntu/adhoc/pegasus
+sudo bundle exec rake db:migrate
+
 sudo chef-client -z -c /tmp/chef/zero.rb -j /tmp/chef/first-boot.json
 
 top -b >/dev/null 2>&1
