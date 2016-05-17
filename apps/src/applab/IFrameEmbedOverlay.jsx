011e765e2d6b66ae57fa6f2e9fa4159033be70fc
Paul Carduner <paul@carduner.net>
2016-05-16 17:37:09 -0700

    Update design for embed

--- apps/src/applab/IFrameEmbedOverlay.jsx
+++ apps/src/applab/IFrameEmbedOverlay.jsx
@@ -3,30 +3,69 @@ var studioApp = require('../StudioApp').singleton;
 var applabConstants = require('./constants');
 var msg = require('../locale');
 
+const PHONE_MARGIN = 68;
+
 var styles = {
   overlay: {
     wrapper: {
-      backgroundColor: 'rgba(0,0,0,0.5)',
       position: 'absolute',
-      top: 68,
+      top: PHONE_MARGIN,
       left: 16,
       width: applabConstants.APP_WIDTH,
       height: applabConstants.APP_HEIGHT,
       zIndex: 5,
       textAlign: 'center',
     },
+    clickText: {
+      position: 'absolute',
+      width: '100%',
+      textAlign: 'center',
+      fontSize: 'x-large',
+      color: 'white',
+      bottom: 50,
+      textShadow: '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000',
+      cursor: 'default',
+    },
+  },
+  playButtonWrapper: {
+    color: 'white',
+    position: 'absolute',
+    bottom: -PHONE_MARGIN/2 - 26/2 - 5,
+    left: applabConstants.APP_WIDTH/2 - 26/2 - 7/2,
+    fontSize: 22,
+    height: 26,
+    width: 26,
+    lineHeight: '26px',
+    padding: 7,
+    borderRadius: 5,
+    backgroundColor: '#4d575f',
+    borderColor: '#4d575f',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    cursor: 'pointer',
+    ':hover': {
+      boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)',
+    },
+    ':active': {
+      borderWidth: 1,
+      borderStyle: 'solid',
+      borderColor: '#888',
+    },
   },
   playButton: {
-    color: 'white',
-    fontSize: 200,
-    lineHeight: applabConstants.APP_HEIGHT+'px',
+    paddingLeft: 4,
   },
 };
 
 var IFrameEmbedOverlay = React.createClass({
 
-  getDefaultState() {
-    return {tooYoung: false};
+  propTypes: {
+  },
+
+  getInitialState() {
+    return {
+      tooYoung: false
+    };
   },
 
   handleTooYoung() {
@@ -48,7 +87,12 @@ var IFrameEmbedOverlay = React.createClass({
           <div className="alert alert-danger">
             {window.dashboard.i18n.t('errors.messages.too_young')}
           </div> :
-          <span className="fa fa-play" style={styles.playButton} />
+          <div>
+            <div style={styles.overlay.clickText}>Tap or click to run</div>
+            <div style={styles.playButtonWrapper}>
+              <span className="fa fa-play" style={styles.playButton} />
+            </div>
+          </div>
         }
       </div>
     );
