d2225b6c274ac3ac55e1c30ffbdafef48512a854
Andrew Oberhardt <Andrew@Code.org>
2016-05-18 00:50:38 -0700

    Added main workshop dashboard UI (reports will come next)

--- code-studio/scripts/build-commands.js
+++ code-studio/scripts/build-commands.js
@@ -35,6 +35,8 @@ var watchify = require('watchify');
  * @param {boolean} [config.forDistribution] - If true, this bundle step will be
  *        treated as part of a build for distribution, with certain environment
  *        variables inlined, and dead code and whitespace removed.
+ * @param {object} [config.browserifyGlobalShim] - If supplied, applied as options
+ *        to a browserifyGlobalShim transform.
  * @returns {Promise} that resolves after the build completes or fails - even
  *          if the build fails, the promise should resolve, but get an Error
  *          object as its result.  If watch is enabled, the promise resolves
@@ -50,6 +52,7 @@ exports.bundle = function (config) {
   var shouldFactor = config.shouldFactor;
   var shouldWatch = config.shouldWatch;
   var forDistribution = config.forDistribution;
+  var browserifyGlobalShim = config.browserifyGlobalShim;
 
   var outPath = function (inPath) {
     return path
@@ -118,6 +121,11 @@ exports.bundle = function (config) {
         return outPath(srcPath + file);
       })
     });
+  }
+
+  if (browserifyGlobalShim) {
+    var globalShim = require('browserify-global-shim').configure(browserifyGlobalShim);
+    bundler.transform({global: true}, globalShim);
   }
 
   // Optionally enable watch/rebuild loop
