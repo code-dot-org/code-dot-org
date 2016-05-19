d8bc5b84b32c74fef8dd8eeca621857914218481
Andrew Oberhardt <Andrew@Code.org>
2016-05-19 08:18:41 -0700

    PR feedback

--- dashboard/app/controllers/pd/mail_preview_controller.rb
+++ dashboard/app/controllers/pd/mail_preview_controller.rb
@@ -1,7 +1,13 @@
-class Pd::MailPreviewController < MailView
-  # Only in development environments, allows previewing mail rendering with fixed test data
+# This is a developer aid that allows previewing rendered mail views with fixed test data.
+# The route is restricted so it only exists in development mode.
+# See https://github.com/basecamp/mail_view
+#
+# Ideally, there should be the minimum sufficient previews to cover all unique mail renderings.
+# A navigable list of previews is available at /pd/mail_preview/ (on development only)
 
+class Pd::MailPreviewController < MailView
   # Teacher enrollment receipt
+  # Each course and subject below have variations in rendering.
   # CSF
   def teacher_enrollment_receipt_csf
     workshop = workshop(Pd::Workshop::COURSE_CSF)
@@ -72,43 +78,48 @@ class Pd::MailPreviewController < MailView
   end
 
   # Organizer enrollment receipt
+  # This renders the same regardless of course, so the course choice here is arbitrary.
   def organizer_enrollment_receipt
     workshop = workshop(Pd::Workshop::COURSE_CSF)
     Pd::WorkshopMailer.organizer_enrollment_receipt enrollment(workshop)
   end
 
   # Teacher cancel receipt
-  # csf
+  # This has a variation for CSF. It's the same for all other courses.
+  # CSP
+  def teacher_cancel_receipt_general
+    workshop = workshop(Pd::Workshop::COURSE_CSP)
+    Pd::WorkshopMailer.teacher_cancel_receipt enrollment(workshop)
+  end
+
+  # CSF
   def teacher_cancel_receipt_csf
     workshop = workshop(Pd::Workshop::COURSE_CSF)
     Pd::WorkshopMailer.teacher_cancel_receipt enrollment(workshop)
   end
 
-  # csf
-  def teacher_cancel_receipt_other
-    workshop = workshop(Pd::Workshop::COURSE_CSP)
-    Pd::WorkshopMailer.teacher_cancel_receipt enrollment(workshop)
-  end
-
   # Organizer cancel receipt
+  # No rendering variations. Course selection is arbitrary.
   def organizer_cancel_receipt
     workshop = workshop(Pd::Workshop::COURSE_CSP)
     Pd::WorkshopMailer.organizer_cancel_receipt enrollment(workshop)
   end
 
   # Exit survey
+  # This has a variation for CSF. It's the same for all other courses.
+  def exit_survey_general
+    workshop = workshop(Pd::Workshop::COURSE_CSP)
+    Pd::WorkshopMailer.exit_survey workshop, teacher, enrollment(workshop)
+  end
+
   def exit_survey_csf
     workshop = workshop(Pd::Workshop::COURSE_CSF)
     Pd::WorkshopMailer.exit_survey workshop, teacher, enrollment(workshop)
   end
 
-  def exit_survey_other
-    workshop = workshop(Pd::Workshop::COURSE_CSP)
-    Pd::WorkshopMailer.exit_survey workshop, teacher, enrollment(workshop)
-  end
-
   private
 
+  # Dummy data for testing out the mail view rendering:
   def workshop(course, options = {})
     organizer = User.new name: 'Oscar Organizer', email: 'oscar_organizer@example.net'
     params = {
@@ -141,8 +152,6 @@ class Pd::MailPreviewController < MailView
       workshop: workshop,
       name: 'Tracy Teacher',
       email: 'tracy_teacher@example.net',
-      district_name: 'Seattle Public Schools',
-      school: 'Code.org school.'
     ).tap(&:assign_code)
   end
 
@@ -155,5 +164,4 @@ class Pd::MailPreviewController < MailView
       self + h.hours
     end
   end
-
 end
--- dashboard/app/mailers/pd/workshop_mailer.rb
+++ dashboard/app/mailers/pd/workshop_mailer.rb
@@ -3,13 +3,44 @@ class Pd::WorkshopMailer < ActionMailer::Base
 
   SUPPORTED_TECH_URL = 'https://support.code.org/hc/en-us/articles/202591743-What-kind-of-operating-system-and-browser-do-I-need-to-use-Code-org-s-online-learning-system-'
 
+  # Name of partial view for workshop details organized by course, then subject.
+  # (views/pd/workshop_mailer/workshop_details/_<name>.html.haml)
+  DETAILS_PARTIALS = {
+    Pd::Workshop::COURSE_CS_IN_S => {
+      Pd::Workshop::SUBJECT_CS_IN_S_PHASE_2 => 'phase_2',
+      Pd::Workshop::SUBJECT_CS_IN_S_PHASE_3_SEMESTER_1 => 'cs_in_s_phase_3_semester_1',
+      Pd::Workshop::SUBJECT_CS_IN_S_PHASE_3_SEMESTER_2 => 'cs_in_s_phase_3_semester_2'
+    },
+    Pd::Workshop::COURSE_CS_IN_A => {
+      Pd::Workshop::SUBJECT_CS_IN_A_PHASE_2 => 'phase_2',
+      Pd::Workshop::SUBJECT_CS_IN_A_PHASE_3 => 'cs_in_a_phase_3'
+    },
+    Pd::Workshop::COURSE_ECS => {
+      Pd::Workshop::SUBJECT_ECS_PHASE_2 => 'phase_2',
+      Pd::Workshop::SUBJECT_ECS_UNIT_3 => 'ecs_unit_3',
+      Pd::Workshop::SUBJECT_ECS_UNIT_4 => 'ecs_unit_4',
+      Pd::Workshop::SUBJECT_ECS_UNIT_5 => 'ecs_unit_5',
+      Pd::Workshop::SUBJECT_ECS_UNIT_6 => 'ecs_unit_6',
+      Pd::Workshop::SUBJECT_ECS_PHASE_4 => 'ecs_phase_4'
+    }
+  }
+
+  # Online URL used in the details partials, organized by course.
+  ONLINE_URL = {
+    Pd::Workshop::COURSE_CS_IN_S => 'https://studio.code.org/s/sciencepd1',
+    Pd::Workshop::COURSE_CS_IN_A => 'https://studio.code.org/s/algebrapd1',
+    Pd::Workshop::COURSE_ECS => 'https://studio.code.org/s/ecspd1'
+  }
+
   def teacher_enrollment_receipt(enrollment)
     @enrollment = enrollment
     @workshop = enrollment.workshop
     @cancel_url = url_for controller: 'pd/workshop_enrollment', action: :cancel, code: enrollment.code
+    @details_partial = get_details_partial @workshop.course, @workshop.subject
+    @online_url = ONLINE_URL[@workshop.course]
 
     mail content_type: 'text/html',
-      subject: 'Your upcoming Code.org workshop and next steps.',
+      subject: 'Your upcoming Code.org workshop and next steps',
       to: email_address(@enrollment.name, @enrollment.email),
       reply_to: email_address(@workshop.organizer.name, @workshop.organizer.email)
   end
@@ -20,7 +51,7 @@ class Pd::WorkshopMailer < ActionMailer::Base
     @teacher_dashboard_url = CDO.code_org_url "/teacher-dashboard#/sections/#{@workshop.section_id}/manage"
 
     mail content_type: 'text/html',
-      subject: 'Code.org workshop registration.',
+      subject: 'Code.org workshop registration',
       to: email_address(@workshop.organizer.name, @workshop.organizer.email),
       reply_to: 'noreply@code.org'
   end
@@ -30,7 +61,7 @@ class Pd::WorkshopMailer < ActionMailer::Base
     @workshop = enrollment.workshop
 
     mail content_type: 'text/html',
-      subject: 'Code.org workshop cancellation.',
+      subject: 'Code.org workshop cancellation',
       to: email_address(@enrollment.name, @enrollment.email),
       reply_to: 'noreply@code.org'
   end
@@ -40,7 +71,7 @@ class Pd::WorkshopMailer < ActionMailer::Base
     @workshop = enrollment.workshop
 
     mail content_type: 'text/html',
-      subject: 'Code.org workshop cancellation.',
+      subject: 'Code.org workshop cancellation',
       to: email_address(@workshop.organizer.name, @workshop.organizer.email),
       reply_to: 'noreply@code.org'
   end
@@ -51,7 +82,7 @@ class Pd::WorkshopMailer < ActionMailer::Base
     @cancel_url = url_for controller: 'pd/workshop_enrollment', action: :cancel, code: enrollment.code
 
     mail content_type: 'text/html',
-      subject: 'Your upcoming Code.org workshop and next steps.',
+      subject: 'Your upcoming Code.org workshop and next steps',
       to: email_address(@enrollment.name, @enrollment.email),
       reply_to: email_address(@workshop.organizer.name, @workshop.organizer.email)
   end
@@ -76,5 +107,11 @@ class Pd::WorkshopMailer < ActionMailer::Base
     Mail::Address.new(email).tap do |address|
       address.display_name = display_name
     end.format
+  end
+
+  def get_details_partial(course, subject)
+    return 'csf' if course == Pd::Workshop::COURSE_CSF
+    return DETAILS_PARTIALS[course][subject] if DETAILS_PARTIALS[course] && DETAILS_PARTIALS[course][subject]
+    nil
   end
 end
