ee27a41bf6d13c59fe5a04af9a6d4648af29bbcd
Paul Carduner <paul@carduner.net>
2016-05-23 10:43:02 -0700

    Make sure webpack build times get logged to newrelic

--- apps/script/log-build-times.js
+++ apps/script/log-build-times.js
@@ -116,7 +116,7 @@ function parseCollectedData(data) {
         logTimestamp: timestamp
       };
     }).filter(function (data) {
-      return /^exec/.test(data.task);
+      return /^(exec|webpack)/.test(data.task);
     });
     return collectedData.concat(formattedRecords);
   }, []);
