c9665bc0c9af2cfffc25493b35216be5856eca6e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-05-24 12:40:56 -0700

    normalization and styles

--- code-studio/src/js/components/playzone.jsx
+++ code-studio/src/js/components/playzone.jsx
@@ -2,6 +2,7 @@
  * @overview React for the End-of-Stage Experience
  */
 /* global React */
+var color = require('@cdo/apps/color');
 
 export class PlayZone extends React.Component {
   constructor(props) {
@@ -9,29 +10,38 @@ export class PlayZone extends React.Component {
     this.links = [{
       href: "/projects/artist/new",
       img: "/shared/images/courses/logo_tall_artist.jpg",
-      title: "Artist",
+      title: "New Drawing",
       className: "artist",
       description: "Draw cool pictures and designs with the Artist!"
     }, {
-      href: "/projects/applab/new",
-      img: "/shared/images/courses/logo_tall_applab.jpg",
-      title: "App Lab",
-      className: "applab",
-      description: "Design an app, code with blocks or JavaScript to make it work, then share your app in seconds."
-    }, {
       href: "/projects/playlab/new",
       img: "/shared/images/courses/logo_tall_playlab.jpg",
-      title: "Play Lab",
+      title: "New App",
       className: "playlab",
       description: "Create a story or make a game with Play Lab!"
+    }, {
+      href: "/projects",
+      img: "/shared/images/courses/logo_tall_applab.jpg",
+      title: "Open a project",
+      className: "projects",
+      description: "Revisit one of your existing projects."
     }];
+
+    this.styles = {
+      container: {margin: '20px'},
+      primaryHeader: {fontSize: '200%'},
+      secondaryHeader: {color: color.charcoal},
+      courseblockContainer: {width: '720px', paddingTop: '20px'},
+      continueButton: {marginTop: '20px', marginRight: 0}
+    };
   }
+
   render() {
     return (
-      <div style={{margin: "20px"}}>
-        <h1 style={{fontSize: "200%"}}>{`Congratulations! You finished ${this.props.stageName}!`}</h1>
-        <h4>Ask your teacher what to do next</h4>
-        <div className="center" style={{width: '720px', paddingTop: '20px'}}>{this.links.map(link =>
+      <div style={this.styles.container}>
+        <h1 style={this.styles.primaryHeader}>{`Congratulations! You finished ${this.props.stageName}!`}</h1>
+        <h4 style={this.styles.secondaryHeader}>Ask your teacher what to do next</h4>
+        <div className="center" style={this.styles.courseblockContainer}>{this.links.map(link =>
           <div key={link.className} className="courseblock-noaction courseblock-span3 courseblock-tall">
             <a href={link.href}>
               <div className="imgspan">
@@ -45,7 +55,7 @@ export class PlayZone extends React.Component {
           </div>
         )}</div>
         <div className="farSide">
-          <button id="ok-button" className="secondary" onClick={this.props.onContinue} style={{marginTop: '20px', marginRight: 0}}>
+          <button id="ok-button" onClick={this.props.onContinue} style={this.styles.continueButton}>
             Go on to the next Stage
           </button>
         </div>
@@ -58,4 +68,3 @@ PlayZone.propTypes = {
   stageName: React.PropTypes.string.isRequired,
   onContinue: React.PropTypes.func.isRequired,
 };
-
