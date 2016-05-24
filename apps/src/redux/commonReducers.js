e350c41def6110caa30dd8f42fad7ccfc6631794
Brent Van Minnen <bjvanminnen@gmail.com>
2016-05-24 09:47:52 -0700

    es6 runState

--- apps/src/redux/commonReducers.js
+++ apps/src/redux/commonReducers.js
@@ -2,7 +2,7 @@
  * A set of reducers that are used across all of our apps
  */
 
-var runState = require('./runState').default;
+import runState from './runState';
 import pageConstants from './pageConstants';
 import instructions from './instructions';
 
