d9b1298b37257e6225e9a5c67f443c1cbf84477f
Brad Buchanan <brad@code.org>
2016-05-23 17:47:49 -0700

    Push React globals down into package configs

---
+++ code-studio/.eslintrc.js
@@ -0,0 +1,8 @@
+// This config only defines globals available especially
+// in code-studio.  See the root .eslintrc.js for linting rules.
+module.exports = {
+  "globals": {
+    "React": true,
+    "ReactDOM": true
+  }
+};
