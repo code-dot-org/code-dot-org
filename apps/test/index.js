edffb1cc86f8744b601be359e99ab755ed09c826
Paul Carduner <paul@carduner.net>
2016-05-24 17:49:21 -0700

    Get loads of integration tests passing!

--- apps/test/index.js
+++ apps/test/index.js
@@ -1,2 +1,5 @@
 var testsContext = require.context("./unit", true, /\.js$/);
 testsContext.keys().forEach(testsContext);
+
+var integrationContext = require.context("./integration", false, /Tests?\.js$/);
+integrationContext.keys().forEach(integrationContext);
