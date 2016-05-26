40118a102151ebe658916fb49b6489b70bd93db7
Mehal Shah <mehal@code.org>
2016-05-26 10:50:54 -0700

    PR feedback

--- dashboard/test/factories.rb
+++ dashboard/test/factories.rb
@@ -349,7 +349,7 @@ FactoryGirl.define do
     plc_user_course_enrollment nil
     plc_course_unit nil
     status Plc::EnrollmentUnitAssignment::START_BLOCKED
-    user nil
+    user {self.plc_user_course_enrollment.try(:user)}
   end
 
   factory :plc_course_unit, :class => 'Plc::CourseUnit' do
