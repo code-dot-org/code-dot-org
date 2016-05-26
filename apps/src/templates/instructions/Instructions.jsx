db8b9a89dc8c1f629eb701df6aadc12ec28815f3
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-05-26 16:18:14 -0700

    add TTS prototype demo to instructions

--- apps/src/templates/instructions/Instructions.jsx
+++ apps/src/templates/instructions/Instructions.jsx
@@ -1,6 +1,18 @@
 var MarkdownInstructions = require('./MarkdownInstructions');
 var NonMarkdownInstructions = require('./NonMarkdownInstructions');
 
+const styles = {
+  icon: {
+    cursor: "pointer",
+    padding: "5px 10px",
+    margin: "0 10px"
+  },
+  audio: {
+    verticalAlign: "middle",
+    margin: "0 10px",
+  }
+};
+
 var Instructions = React.createClass({
 
   propTypes: {
@@ -11,7 +23,43 @@ var Instructions = React.createClass({
     markdownClassicMargins: React.PropTypes.bool,
     aniGifURL: React.PropTypes.string,
     authoredHints: React.PropTypes.element,
-    onResize: React.PropTypes.func
+    onResize: React.PropTypes.func,
+    acapelaSettings: React.PropTypes.shape({
+      login: React.PropTypes.string.isRequired,
+      app: React.PropTypes.string.isRequired,
+      password: React.PropTypes.string.isRequired,
+    })
+  },
+
+  getInitialState: function () {
+    return {
+      voices: [{
+        id: "micah22k",
+        name: "Micah"
+      }, {
+        id: "nelly22k",
+        name: "Nelly"
+      }, {
+        id: "willlittlecreature22k",
+        name: "Yodit"
+      }],
+      lastVoice: "micah22k"
+    };
+  },
+
+  playAudio: function (text, voice) {
+    $.getJSON("https://vaas.acapela-group.com/Services/UrlMaker?jsoncallback=?", {
+      cl_login: this.props.acapelaSettings.login,
+      cl_app: this.props.acapelaSettings.app,
+      cl_pwd: this.props.acapelaSettings.password,
+      req_voice: voice,
+      req_text: text,
+    }, function (data) {
+      this.setState({
+        lastVoice: voice,
+        audioSrc: data.snd_url
+      });
+    }.bind(this));
   },
 
   render: function () {
@@ -26,6 +74,7 @@ var Instructions = React.createClass({
     // These instructions may contain spans and images as determined by
     // StudioApp.substituteInstructionImages
     var instructions;
+    var text;
     if (this.props.renderedMarkdown) {
       instructions = (
         <MarkdownInstructions
@@ -36,6 +85,10 @@ var Instructions = React.createClass({
           inTopPane={this.props.inTopPane}
         />
       );
+
+      var temporaryElement = document.createElement('div');
+      temporaryElement.innerHTML = this.props.renderedMarkdown;
+      text = temporaryElement.textContent;
     } else {
       instructions = (
         <NonMarkdownInstructions
@@ -44,13 +97,23 @@ var Instructions = React.createClass({
           instructions2={this.props.instructions2}
         />
       );
+      text = this.props.instructions;
     }
     return (
       <div>
         {instructions}
-        {this.props.aniGifURL &&
-          <img className="aniGif example-image" src={ this.props.aniGifURL }/>
-        }
+        {this.props.aniGifURL && <img className="aniGif example-image" src={this.props.aniGifURL}/>}
+        {this.props.acapelaSettings && (<div>
+          <p style={{lineHeight: "14px", fontSize: "12px"}}>Note that in trial mode, we don't have access to the high-quality children's voices that we would probably want to use in production</p>
+          <div className="btn-group dropup">
+            <a className="btn btn-primary" onClick={this.playAudio.bind(this, text, this.state.lastVoice)}><i className="icon-bullhorn icon-white"></i></a>
+            <a className="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span className="caret"></span></a>
+            <ul className="dropdown-menu">
+              {this.state.voices.map(voice => <li key={voice.id}><a onClick={this.playAudio.bind(this, text, voice.id)}>{voice.name}</a></li>)}
+            </ul>
+          </div>
+          {this.state.audioSrc && <audio style={styles.audio} src={this.state.audioSrc} controls='controls' />}
+        </div>)}
         {this.props.authoredHints}
       </div>
     );
