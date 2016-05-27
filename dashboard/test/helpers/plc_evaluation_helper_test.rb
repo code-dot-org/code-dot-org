af0e07b892d95549c4d8eedec3816a0551b8521d
Mehal Shah <mehal@code.org>
2016-05-27 16:37:57 -0700

    Hey we have tests

--- dashboard/test/helpers/plc_evaluation_helper_test.rb
+++ dashboard/test/helpers/plc_evaluation_helper_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class PlcEvaluationHelperTest < ActionView::TestCase
+  include Plc::EvaluationsHelper
+
   setup do
     course = create :plc_course
     @course_unit = create(:plc_course_unit, plc_course: course)
@@ -20,7 +22,6 @@ class PlcEvaluationHelperTest < ActionView::TestCase
 
     @module_honesty = create(:plc_learning_module, name: 'Answering questions honestly', plc_course_unit: @course_unit, module_type: Plc::LearningModule::PRACTICE_MODULE, stage: @stage_honesty)
     @module_no_nickname = create(:plc_learning_module, name: 'Not revealing your nickname', plc_course_unit: @course_unit, module_type: Plc::LearningModule::PRACTICE_MODULE, stage: @stage_no_nickname)
-
 
     q1_dsl = <<DSL
     name 'Question 1'
@@ -61,7 +62,59 @@ DSL
     answer 'What do you mean, an African or European Swallow?', weight: 1, stage_name: '#{@stage_ornithology.name}'
     answer '15 m/s'
 DSL
-  
-    
+
+    @evaluation_multi1 = EvaluationMulti.create_from_level_builder({name: 'Question 1'}, {dsl_text: q1_dsl})
+    @evaluation_multi2 = EvaluationMulti.create_from_level_builder({name: 'Question 2'}, {dsl_text: q2_dsl})
+    @evaluation_multi3 = EvaluationMulti.create_from_level_builder({name: 'Question 3'}, {dsl_text: q3_dsl})
+    @evaluation_multi4 = EvaluationMulti.create_from_level_builder({name: 'Question 4'}, {dsl_text: q4_dsl})
+    @evaluation_multi5 = EvaluationMulti.create_from_level_builder({name: 'Question 5'}, {dsl_text: q5_dsl})
+
+    levelgroup_dsl = <<DSL
+    name 'Crossing the bridge of death'
+    title 'Bridge of Death Evaluation'
+    submittable 'true'
+
+    page
+    level 'Question 1'
+    level 'Question 2'
+    level 'Question 3'
+    level 'Question 4'
+    level 'Question 5'
+DSL
+
+    @evaluation = LevelGroup.create_from_level_builder({name: 'evaluation'}, {dsl_text: levelgroup_dsl})
+  end
+
+  test 'submit evaluation enrolls user in appropriate modules' do
+    assert_equal [@module_blue.id, @module_honesty.id], get_preferred_modules_for_answers([[@evaluation_multi1, 'Sir Lancelot'],
+                                                                                           [@evaluation_multi2, 'I seek the grail'],
+                                                                                           [@evaluation_multi3, 'Blue']])
+
+    assert_equal [@module_ignorance.id, @module_no_nickname.id], get_preferred_modules_for_answers([[@evaluation_multi1, 'Sir Robin'],
+                                                                                           [@evaluation_multi2, 'Yes, yes, I seek the Grail'],
+                                                                                           [@evaluation_multi4, 'I dont know that!']])
+
+    assert_equal [@module_cliffs.id, @module_honesty.id], get_preferred_modules_for_answers([[@evaluation_multi1, 'Sir Galahad'],
+                                                                                           [@evaluation_multi2, 'I seek the grail'],
+                                                                                           [@evaluation_multi3, 'Yellow - no, blue']])
+
+    assert_equal [@module_ornithology.id, @module_honesty.id], get_preferred_modules_for_answers([[@evaluation_multi1, 'King Arthur'],
+                                                                                           [@evaluation_multi2, 'I seek the grail'],
+                                                                                           [@evaluation_multi5, 'What do you mean, an African or European Swallow?']])
+
+    assert_equal [], get_preferred_modules_for_answers([[@evaluation_multi1, 'Mr Edgecase'],
+                                                                                           [@evaluation_multi2, 'I seek something else'],
+                                                                                           [@evaluation_multi5, '15 m/s']])
+  end
+
+  def get_preferred_modules_for_answers answers_map
+    answers_data = Hash.new()
+
+    answers_map.each do |evaluation_multi, answer|
+      answers_data[evaluation_multi.id] = {'result': evaluation_multi.answers.find_index {|x| x['text'] == answer}.to_s}
+    end
+
+    level_source = LevelSource.create(level: @evaluation, data: answers_data.to_json, md5: Digest::MD5.hexdigest(answers_data.to_json))
+    determine_preferred_learning_modules level_source, @course_unit.script
   end
 end
