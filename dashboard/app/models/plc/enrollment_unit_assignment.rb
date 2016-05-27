f1aee91403ee305048450f4925cdaa17e83de53f
Mehal Shah <mehal@code.org>
2016-05-27 12:01:45 -0700

    Get migration working again

--- dashboard/app/models/plc/enrollment_unit_assignment.rb
+++ dashboard/app/models/plc/enrollment_unit_assignment.rb
@@ -9,7 +9,7 @@
 #  status                        :string(255)
 #  created_at                    :datetime         not null
 #  updated_at                    :datetime         not null
-#  user_id                       :integer          not null
+#  user_id                       :integer
 #
 # Indexes
 #
