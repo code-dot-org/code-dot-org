f85337599a065dc3800ea22189dbadad391879a3
Brent Van Minnen <bjvanminnen@gmail.com>
2016-05-31 14:08:29 -0700

    connect BelowVisualization and dont add bubble when topInstructions

--- apps/src/templates/BelowVisualization.jsx
+++ apps/src/templates/BelowVisualization.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+import { connect } from 'react-redux';
 var commonStyles = require('../commonStyles');
 var ProtectedStatefulDiv = require('./ProtectedStatefulDiv');
 var InputOutputTable = require('./InputOutputTable');
@@ -6,44 +7,50 @@ var InputOutputTable = require('./InputOutputTable');
 /**
  * The area below our visualization that is share dby all apps.
  */
-var BelowVisualization = function (props) {
-  return (
-    <ProtectedStatefulDiv id="belowVisualization">
-      <div
-          id="bubble"
-          className="clearfix"
-          style={props.instructionsInTopPane ? commonStyles.hidden : undefined}>
-        <table id="prompt-table">
-          <tbody>
-            <tr>
-              <td id="prompt-icon-cell" style={commonStyles.hidden}>
-                <img id="prompt-icon"/>
-              </td>
-              <td id="prompt-cell">
-                <p id="prompt"/>
-                <p id="prompt2" style={commonStyles.hidden}/>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+const BelowVisualization = React.createClass({
+  propTypes: {
+    inputOutputTable: React.PropTypes.arrayOf(
+      React.PropTypes.arrayOf(React.PropTypes.number)
+    ),
+    instructionsInTopPane: React.PropTypes.bool.isRequired
+  },
 
-        {props.inputOutputTable && <InputOutputTable data={props.inputOutputTable}/>}
+  render() {
+    const { inputOutputTable, instructionsInTopPane } = this.props;
+    return (
+      <ProtectedStatefulDiv id="belowVisualization">
+        {!instructionsInTopPane &&
+          <div
+              id="bubble"
+              className="clearfix"
+          >
+            <table id="prompt-table">
+              <tbody>
+                <tr>
+                  <td id="prompt-icon-cell" style={commonStyles.hidden}>
+                    <img id="prompt-icon"/>
+                  </td>
+                  <td id="prompt-cell">
+                    <p id="prompt"/>
+                    <p id="prompt2" style={commonStyles.hidden}/>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
 
-        <div id="ani-gif-preview-wrapper" style={commonStyles.hidden}>
-          <div id="ani-gif-preview">
+            {inputOutputTable && <InputOutputTable data={inputOutputTable}/>}
+
+            <div id="ani-gif-preview-wrapper" style={commonStyles.hidden}>
+              <div id="ani-gif-preview">
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </ProtectedStatefulDiv>
-  );
-};
+        }
+      </ProtectedStatefulDiv>
+    );
+  }
+});
 
-BelowVisualization.propTypes = {
-  inputOutputTable: React.PropTypes.arrayOf(
-    React.PropTypes.arrayOf(React.PropTypes.number)
-  ),
-  instructionsInTopPane: React.PropTypes.bool
-};
-
-
-module.exports = BelowVisualization;
+export default connect(state => ({
+  instructionsInTopPane: state.pageConstants.instructionsInTopPane
+}))(BelowVisualization);
