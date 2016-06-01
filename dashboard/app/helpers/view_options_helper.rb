6824a808ab1f0f719d85f9ddfad824af1a54e7bc
Ram Kandasamy <ram@code.org>
2016-06-01 16:38:45 -0700

    Modify app_options to work with multiple levels

--- dashboard/app/helpers/view_options_helper.rb
+++ dashboard/app/helpers/view_options_helper.rb
@@ -25,6 +25,21 @@ module ViewOptionsHelper
     public_caching
     is_13_plus
   )
+
+  LevelViewOptions = Struct.new(*%i(
+    success_condition
+    start_blocks
+    toolbox_blocks
+    edit_blocks
+    skip_instructions_popup
+    embed
+    share
+    hide_source
+    submitted
+    unsubmit_url
+    iframe_embed
+  ))
+
   # Sets custom options to be used by the view layer. The option hash is frozen once read.
   def view_options(opts = nil)
     @view_options ||= AppViewOptions.new
@@ -34,4 +49,21 @@ module ViewOptionsHelper
       opts.each{|k, v| @view_options[k] = v}
     end
   end
+
+  # Sets custom level options to be used by the view layer. The option hash is frozen once read.
+  def level_view_options(opts = nil)
+    @level_view_options ||= LevelViewOptions.new
+    if opts.blank?
+      @level_view_options.freeze.to_h.delete_if { |_k, v| v.nil? }
+    else
+      opts.each{|k, v| @level_view_options[k] = v}
+    end
+  end
+
+  # Unfreeze and wipe values from view_options and level_view_options
+  def reset_view_options
+    @view_options = AppViewOptions.new
+    @level_view_options = LevelViewOptions.new
+  end
+
 end
