577a1a8927da1f5fb2f5794c27e75a5c0b0fe50d
Paul Carduner <paul@carduner.net>
2016-06-01 11:00:16 -0700

    use mocha_entry environment variable instead of TEST_FILE for consistency sake

--- apps/test/unit-tests.js
+++ apps/test/unit-tests.js
@@ -1,4 +1,4 @@
 var testsContext = require.context("./unit", true, /\.js$/);
 testsContext.keys()
-  .filter(key => !process.env.TEST_FILE || key.indexOf(process.env.TEST_FILE) >= 0)
+  .filter(key => !process.env.mocha_entry || key.indexOf(process.env.TEST_FILE) >= 0)
   .forEach(testsContext);
