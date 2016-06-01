47f7e9e6bc8b654b7c8ea55537987fcf1e645753
Paul Carduner <paul@carduner.net>
2016-06-01 11:00:16 -0700

    use mocha_entry environment variable instead of TEST_FILE for consistency sake

--- apps/test/integration-tests.js
+++ apps/test/integration-tests.js
@@ -1,4 +1,4 @@
 var integrationContext = require.context("./integration", false, /Tests?\.js$/);
 integrationContext.keys()
-  .filter(key => !process.env.TEST_FILE || key.indexOf(process.env.TEST_FILE) >= 0)
+  .filter(key => !process.env.mocha_entry || key.indexOf(process.env.TEST_FILE) >= 0)
   .forEach(integrationContext);
