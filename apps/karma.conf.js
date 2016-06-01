34e4fef215b84c72cf43ea05bc77003f204b90ed
Paul Carduner <paul@carduner.net>
2016-05-31 17:24:25 -0700

    generate junit style reports for circleci consumption

--- apps/karma.conf.js
+++ apps/karma.conf.js
@@ -68,7 +68,14 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: process.env.CIRCLECI ? [
+      'junit',
+      'mocha',
+    ] : ['mocha'],
+
+    junitReporter: {
+      outputDir: process.env.CIRCLECI ? process.env.CIRCLE_TEST_REPORTS : '',
+    },
 
 
     // web server port
