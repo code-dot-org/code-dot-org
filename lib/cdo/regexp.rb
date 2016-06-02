b8ed7ede36c070bd5e5bee6456dc734ba0ed8360
Andrew Oberhardt <Andrew@Code.org>
2016-06-01 17:17:13 -0700

    PR feedback (added phone number, zip code, and email validation) Opened plp-conditional questions to all. Converted single-select from dropdown to radio buttons.

--- lib/cdo/regexp.rb
+++ lib/cdo/regexp.rb
@@ -4,7 +4,20 @@ require 'json'
 
 module RegexpUtils
   EMAIL_REGEXP = /([^@\s]+@(?:[-a-z0-9]+\.)+[a-z]{2,})/
-  US_PHONE_NUMBER_REGEXP = /([\(]?[0-9]{3}[\)]?[-\s\.\_]?[0-9]{3}[-\s\.\_][0-9]{4})/
+  US_PHONE_NUMBER_REGEXP = /[\(]?(\d{3})[\)]?[-\s\.\_]?(\d{3})[-\s\.\_]?(\d{4})/
+  US_PHONE_NUMBER_ONLY_REGEXP = Regexp.new('^' + US_PHONE_NUMBER_REGEXP.source + '$')
+
+  # returns true if the entire text matches a US phone number
+  def self.us_phone_number?(text)
+    US_PHONE_NUMBER_ONLY_REGEXP === text
+  end
+
+  # extracts the 10 digits in a US phone number and returns as a string (without formatting)
+  def self.extract_us_phone_number_digits(text)
+    match = US_PHONE_NUMBER_ONLY_REGEXP.match(text)
+    raise "Not a US phone number: #{text}" unless match
+    match.captures.join
+  end
 
   def self.find_potential_email(text)
     addresses = text.scan EMAIL_REGEXP
