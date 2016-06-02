5ba0c393891b846b1912ac7d984f9e29c7f4b2a2
Andrew Oberhardt <Andrew@Code.org>
2016-06-01 17:17:13 -0700

    PR feedback (added phone number, zip code, and email validation) Opened plp-conditional questions to all. Converted single-select from dropdown to radio buttons.

--- pegasus/forms/pd_workshop_materials.rb
+++ pegasus/forms/pd_workshop_materials.rb
@@ -11,9 +11,9 @@ class PdWorkshopMaterials
     result[:address_2_s] = stripped data[:address_2_s]
     result[:city_s] = required stripped data[:city_s]
     result[:state_s] = required stripped data[:state_s]
-    result[:zip_code_s] = required stripped data[:zip_code_s]
-    result[:email_s] = required stripped data[:email_s]
-    result[:phone_number_s] = required stripped data[:phone_number_s]
+    result[:zip_code_s] = required zip_code data[:zip_code_s]
+    result[:email_s] = required email_address data[:email_s]
+    result[:phone_number_s] = required us_phone_number data[:phone_number_s]
 
     result
   end
