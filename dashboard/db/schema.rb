f71d9480c5d30b47214fc6479c229a9b042f3ad9
Asher Kach <asher@code.org>
2016-06-03 10:38:21 -0500

    Migration wiping student plaintext emails.

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527181440) do
+ActiveRecord::Schema.define(version: 20160603144608) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id",         limit: 4
