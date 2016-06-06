5f3a4c3e6666be967d53ec9bbca0d0ebb0c80ca6
Brad Buchanan <brad@code.org>
2016-06-06 14:26:18 -0700

    Render Piskel inside Gamelab via npm
    Makes the new [@code-dot-org/piskel](https://www.npmjs.com/package/@code-dot-org/piskel) npm package an apps dependency, copies its contents to the built apps package, and renders it within an iframe on the animation tab.  No other interaction yet.

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -1,4 +1,5 @@
 var chalk = require('chalk');
+var child_process = require('child_process');
 var path = require('path');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
@@ -63,6 +64,7 @@ module.exports = function (grunt) {
 
   var ace_suffix = envOptions.dev ? '' : '-min';
   var dotMinIfNotDev = envOptions.dev ? '' : '.min';
+  var piskelRoot = String(child_process.execSync('`npm bin`/piskel-root')).replace(/\s+$/g,'');
 
   config.copy = {
     src: {
@@ -122,6 +124,12 @@ module.exports = function (grunt) {
           cwd: 'lib/p5play',
           src: ['*.js'],
           dest: 'build/package/js/p5play/'
+        },
+        {
+          expand: true,
+          cwd: piskelRoot,
+          src: '**',
+          dest: 'build/package/js/piskel/'
         },
         {
           expand: true,
