68e91825c6c9267a2b88f5b434e91d2a80241cab
Mehal Shah <mehal@code.org>
2016-06-06 16:11:46 -0700

    Hide sidebar and solutions for teachers in plc scripts [test ui]

--- dashboard/app/helpers/levels_helper.rb
+++ dashboard/app/helpers/levels_helper.rb
@@ -574,4 +574,10 @@ module LevelsHelper
       return true
     end
   end
+
+  def can_view_solution?
+    if current_user && @level.try(:ideal_level_source_id) && @script_level && !@script.hide_solutions?
+      current_user.admin? || (current_user.teacher? && !@script.professional_learning_course)
+    end
+  end
 end
