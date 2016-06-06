b7c7839ad97ed9c2f196c06bf88671d96b0f0c5e
Brent Van Minnen <bjvanminnen@gmail.com>
2016-06-06 10:58:16 -0700

    dont resize prompt icon

--- apps/src/templates/instructions/PromptIcon.jsx
+++ apps/src/templates/instructions/PromptIcon.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+const styles = {
+  main: {
+    // The outer container gets sized at either 50px or 80px depending on
+    // whether or not we have authored hints. In both cases, we want to display
+    // this icon at 50px.
+    maxWidth: 50
+  }
+};
+
 /**
  * Simple component for our icon for hints.
  */
 const PromptIcon = (props) => (
-  <img src={props.src} id="prompt-icon"/>
+  <img
+      src={props.src}
+      id="prompt-icon"
+      style={styles.main}
+  />
 );
 PromptIcon.propTypes = {
   src: React.PropTypes.string.isRequired
