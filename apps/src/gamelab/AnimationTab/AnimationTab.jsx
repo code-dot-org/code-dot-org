cd14074e9c9694ded967b6c0448e50133cd688f1
Brad Buchanan <brad@code.org>
2016-06-07 10:52:36 -0700

    Load Piskel via nonfingerprinted path
    We were getting Piskel via /assets which is fingerprinted via the Rails Asset Pipeline in certain environments.  Now we get it via /blockly (which should really be renamed to /apps) where it's served as simple static assets.

--- apps/src/gamelab/AnimationTab/AnimationTab.jsx
+++ apps/src/gamelab/AnimationTab/AnimationTab.jsx
@@ -56,7 +56,7 @@ const AnimationTab = React.createClass({
             <GameLabVisualizationHeader />
             <AnimationList />
           </div>
-          <iframe id="piskel-frame" style={styles.editorColumn} src="/assets/js/piskel/index.html" />
+          <iframe id="piskel-frame" style={styles.editorColumn} src="/blockly/js/piskel/index.html" />
         </ResizablePanes>
         <AnimationPicker channelId={this.props.channelId}/>
       </div>
