e995b152988a20b7f9606b2b05ed9e4d6b6838e5
Mehal Shah <mehal@code.org>
2016-06-08 17:50:11 -0700

    PR feedback

--- code-studio/src/js/plc/header.jsx
+++ code-studio/src/js/plc/header.jsx
@@ -12,26 +12,26 @@ var PlcHeader = React.createClass({
   },
 
   render: function () {
-    let breadcrumbContainerStyle = {
-      marginBottom: '20'
-    };
-
-    let breadcrumbIconStyle = {
-      margin: 5,
-      color: color.charcoal
-    };
-
-    let currentPageStyle = {
-      color: color.orange
+    let breadcrumbStyle = {
+      container: {
+        marginBottom: '20'
+      },
+      icon: {
+        margin: 5,
+        color: color.charcoal
+      },
+      currentPage: {
+        color: color.orange
+      }
     };
 
     return (
-      <div style={breadcrumbContainerStyle}>
+      <div style={breadcrumbStyle.container}>
         <a href={this.props.course_view_path}>
           My Learning Plan
         </a>
-        <span className='fa fa-caret-right' style={breadcrumbIconStyle}/>
-        <span style={currentPageStyle}>
+        <span className='fa fa-caret-right' style={breadcrumbStyle.icon}/>
+        <span style={breadcrumbStyle.currentPage}>
           {this.props.unit_name}
         </span>
       </div>
