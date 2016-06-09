72161ed5ff8ed9e7b7aeb68308f01576d86bf61f
Josh Lory <josh.lory@code.org>
2016-06-09 12:41:22 -0700

    Add blue "Focus Area" ribbon

--- code-studio/src/js/components/progress/course_progress_row.jsx
+++ code-studio/src/js/components/progress/course_progress_row.jsx
@@ -6,6 +6,7 @@ import color from '../../color';
 
 const styles = {
   row: {
+    position: 'relative',
     boxSizing: 'border-box',
     margin: '2px 0',
     borderWidth: 1,
@@ -14,11 +15,11 @@ const styles = {
     borderRadius: 5,
     background: color.lightest_gray,
     display: 'table',
-    tableLayout: 'fixed',
     padding: 10,
     width: '100%'
   },
   focusAreaRow: {
+    minHeight: 90,
     borderWidth: 3,
     borderColor: color.cyan,
     padding: 8
@@ -28,6 +29,25 @@ const styles = {
     width: 200,
     verticalAlign: 'middle',
     paddingRight: 10
+  },
+  ribbonWrapper: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    width: 90,
+    height: 90,
+    overflow: 'hidden'
+  },
+  ribbon: {
+    position: 'absolute',
+    top: 16,
+    right: -32,
+    fontSize: 12,
+    whiteSpace: 'nowrap',
+    background: color.cyan,
+    color: color.white,
+    padding: '5px 25px',
+    transform: 'rotate(45deg)'
   }
 };
 
@@ -46,15 +66,18 @@ const CourseProgressRow = React.createClass({
     const stage = this.props.stage;
 
     let rowStyle = Object.assign({}, styles.row);
+    let ribbon;
     if (this.props.professionalLearningCourse) {
       Object.assign(rowStyle, {background: color.white});
     }
     if (this.props.isFocusArea) {
       Object.assign(rowStyle, styles.focusAreaRow);
+      ribbon = <div style={styles.ribbonWrapper}><div style={styles.ribbon}>Focus Area</div></div>;
     }
 
     return (
       <div style={rowStyle}>
+        {ribbon}
         <div style={styles.stageName}>
           {stage.title}
           <div className='stage-lesson-plan-link' style={{display: 'none'}}>
