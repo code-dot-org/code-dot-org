5f3c5626e7d90a41031fd7efd32da05610d29945
David Bailey <davidsbailey@users.noreply.github.com>
2016-06-09 14:45:46 -0700

    basic compiling (using bolt) and uploading of firebase rules

---
+++ apps/firebase/upload_rules.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+require_relative '../../deployment'
+
+# Upload firebase rules from rules.json to the firebase specified in
+# CDO.firebase_name, using CDO.firebase_secret for authentication.
+
+url = "https://#{CDO.firebase_name}.firebaseio.com/.settings/rules.json?auth=#{CDO.firebase_secret}"
+system("curl -X PUT -T rules.json '#{url}'")
+puts ''
