b7e8bda989cfaa859dc80407dd9c164f4993c833
Josh Lory <josh.lory@code.org>
2016-06-10 11:05:40 -0700

    Code review feedback: use Rails path helper

--- code-studio/src/js/progress.js
+++ code-studio/src/js/progress.js
@@ -139,6 +139,7 @@ progress.renderCourseProgress = function (scriptData, currentLevelId) {
     if (data.focusAreaPositions) {
       store.dispatch({
         type: 'UPDATE_FOCUS_AREAS',
+        changeFocusAreaPath: data.changeFocusAreaPath,
         focusAreaPositions: data.focusAreaPositions
       });
     }
@@ -170,6 +171,7 @@ function loadProgress(scriptData, currentLevelId) {
         currentLevelId: state.currentLevelId,
         professionalLearningCourse: state.professionalLearningCourse,
         progress: newProgress,
+        changeFocusAreaPath: state.changeFocusAreaPath,
         focusAreaPositions: state.focusAreaPositions,
         stages: state.stages.map(stage => Object.assign({}, stage, {levels: stage.levels.map(level => {
           let id = level.uid || level.id;
@@ -179,13 +181,17 @@ function loadProgress(scriptData, currentLevelId) {
         })}))
       };
     } else if (action.type === 'UPDATE_FOCUS_AREAS') {
-      return Object.assign(state, {focusAreaPositions: action.focusAreaPositions});
+      return Object.assign(state, {
+        changeFocusAreaPath: action.changeFocusAreaPath,
+        focusAreaPositions: action.focusAreaPositions
+      });
     }
     return state;
   }, {
     currentLevelId: currentLevelId,
     professionalLearningCourse: scriptData.plc,
     progress: {},
+    changeFocusAreaPath: null,
     focusAreaPositions: [],
     stages: scriptData.stages
   });
