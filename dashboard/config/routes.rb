b0b97c231935a2857b819b293dd493a4dc2c2216
Brad Buchanan <brad@code.org>
2016-06-13 11:49:59 -0700

    [WIP] Test run status dashboard

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -338,6 +338,9 @@ Dashboard::Application.routes.draw do
   get '/api/user_progress', to: 'api#user_progress_for_all_scripts', as: 'user_progress_for_all_scripts'
   get '/api/:action', controller: 'api'
 
+  get '/test_status/:branch', to: 'test_status#test_run_status'
+  get '/test_status/:branch/:name', to: 'test_status#test_status'
+
   namespace :api do
     namespace :v1 do
       get 'school-districts/:state', to: 'school_districts#index', defaults: { format: 'json' }
