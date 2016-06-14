24b58a5bf9c3786a746d2e6d3b9f51f2e0c2f63a
Paul Carduner <paul@carduner.net>
2016-06-13 19:31:05 -0700

    Add enzyme and sinon-chai libraries for testing

--- apps/test/util/configuredChai.js
+++ apps/test/util/configuredChai.js
@@ -1,7 +1,9 @@
 /** @file Import chai and configure it for our own test style. */
 import chai from 'chai';
 import chaiSubset from 'chai-subset';
+import sinonChai from 'sinon-chai';
 chai.use(chaiSubset);
+chai.use(sinonChai);
 chai.includeStack = true;
 export default chai;
 export const assert = chai.assert;
