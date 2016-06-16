18dead1da72d814d008124a39a906a065dcd1d99
Brian Jordan <bcjordan@gmail.com>
2016-06-16 15:16:15 -0700

    Extract util for dynamic dropdowns to dropletUtils.

--- apps/test/unit/applab/setPropertyDropdownTest.js
+++ apps/test/unit/applab/setPropertyDropdownTest.js
@@ -6,32 +6,7 @@ testUtils.setExternalGlobals();
 var setPropertyDropdown = require('@cdo/apps/applab/setPropertyDropdown');
 
 describe("setPropertyDropdown", function () {
-  var getFirstSetPropertyParamFromCode = setPropertyDropdown.__TestInterface.getFirstSetPropertyParamFromCode;
   var stripQuotes = setPropertyDropdown.__TestInterface.stripQuotes;
-
-  it("getFirstSetPropertyParamFromCode", function () {
-    var code;
-
-    // single quotes
-    code = "setProperty('element1', ";
-    assert.equal(getFirstSetPropertyParamFromCode(code), 'element1');
-
-    // double quotes
-    code = 'setProperty("element1", ';
-    assert.equal(getFirstSetPropertyParamFromCode(code), 'element1');
-
-    // mix quotes
-    code = 'setProperty("element1\', ';
-    assert.equal(getFirstSetPropertyParamFromCode(code), null);
-
-    // no trailing space
-    code = "setProperty('element1',";
-    assert.equal(getFirstSetPropertyParamFromCode(code), 'element1');
-
-    // multiple setProperty
-    code = "setProperty('element1', 'width', 100); setProperty('element2', ";
-    assert.equal(getFirstSetPropertyParamFromCode(code), 'element2');
-  });
 
   it('stripQuotes', function () {
     assert.equal(stripQuotes('"double"'), 'double');
