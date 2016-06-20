f6821a7a6bb2bb2a3ba36ac593785f15b4f0ef79
Brad Buchanan <brad@code.org>
2016-06-20 13:14:10 -0700

    Use empty image dataurl as placeholder

--- apps/src/gamelab/constants.js
+++ apps/src/gamelab/constants.js
@@ -14,3 +14,9 @@ module.exports.GAME_WIDTH = 400;
 
 /** @const {number} */
 module.exports.GAME_HEIGHT = 400;
+
+/**
+ * DataURL for a 1x1 transparent gif image.
+ * @const {string}
+ */
+module.exports.EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
