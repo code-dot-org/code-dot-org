4058ee5ece106923321bcd0fac02d1c769f209f5
Continuous Integration <dev@code.org>
2016-06-21 21:08:36 +0000

    Updated cookbook versions

--- cookbooks/cdo-freegeoip/metadata.rb
+++ cookbooks/cdo-freegeoip/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'will@code.org'
 license          'Apache 2.0'
 description      'Installs/Configures cdo-freegeoip'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'ark'
 depends 'poise-service'
