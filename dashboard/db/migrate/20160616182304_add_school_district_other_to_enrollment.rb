057609b1fa0784dc5cb3ecb4c2e7f10c9c8d240b
Brendan Reville <brendan@code.org>
2016-06-20 21:14:45 -0400

    district dropdown: add comment to migration.

--- dashboard/db/migrate/20160616182304_add_school_district_other_to_enrollment.rb
+++ dashboard/db/migrate/20160616182304_add_school_district_other_to_enrollment.rb
@@ -1,5 +1,7 @@
 class AddSchoolDistrictOtherToEnrollment < ActiveRecord::Migration
   def change
+    # school_district_other is true if the user explicitly stated that their
+    # district is not in the list provided for their state.
     add_column :pd_enrollments, :school_district_other, :boolean
   end
 end
