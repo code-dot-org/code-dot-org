28c5759fc63f2a6f72452c006a041d138f6bf63a
Brad Buchanan <brad@code.org>
2016-06-22 14:02:45 -0700

    Rename TestStatusController / TestLogsController

--- dashboard/app/controllers/api/v1/test_logs_controller.rb
+++ dashboard/app/controllers/api/v1/test_logs_controller.rb
@@ -4,7 +4,7 @@ require 'date'
 # API that serves UI test status based on uploaded test run S3 logs and their
 # metadata.  See runner.rb, test_status.haml and test_status.js for more
 # information.
-class Api::V1::TestStatusController < ApplicationController
+class Api::V1::TestLogsController < ApplicationController
   # GET /api/v1/test_logs/<branch>/since/<timestamp>
   def test_run_status_since
     boundary_time = Time.at(params[:time].to_i)
