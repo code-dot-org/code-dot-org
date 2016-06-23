176e14d8f2409ddb3f8748ac88afa9c5bb6546ed
Paul Carduner <paul@carduner.net>
2016-06-22 17:13:41 -0700

    More documentation and styling for the storybook

--- apps/.storybook/config.js
+++ apps/.storybook/config.js
@@ -1,68 +1,30 @@
 import React from 'react';
 import * as storybook from '@kadira/storybook';
-import centered from '@kadira/react-storybook-decorator-centered';
 import infoAddon from '@kadira/react-storybook-addon-info';
 import addStoriesGroup from 'react-storybook-addon-add-stories-group';
 import {setExternalGlobals, setupLocale} from '../test/util/testUtils';
-import color from '../src/color';
 setExternalGlobals();
 
 function addStyleguideExamples(subcomponent) {
   if (subcomponent && subcomponent.styleGuideExamples) {
-    subcomponent.styleGuideExamples(
-      storybook,
-      {centered}
-    );
+    subcomponent.styleGuideExamples(storybook);
   }
 }
 
 const styles = {
-  colorWrapper: {
-    display: 'inline-block',
-    whiteSpace: 'nowrap',
-    width: '33%',
+  centeredStory: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  colorBox: {
-    display: 'inline-block',
-    border: '1px solid black',
-    width: 50,
-    height: 50,
-  },
-  colorDoc: {
-    display: 'inline-block',
-    position: 'relative',
-    bottom: 12.5,
-    paddingLeft: 10
-  }
 };
 
 function loadStories() {
-  storybook
-    .storiesOf('Colors', module)
-    .add('yay', () => (
-      <div>
-        <h2>Colors</h2>
-        <p>
-          These are the standard colors available in SCSS and javascript.
-        </p>
-        <p>
-          JavaScript usage is as follows:
-        </p>
-        <pre>
-          {`import colors from '@cdo/apps/colors';
-console.log("the hex value for purple is:", colors.purple);`}
-        </pre>
-        {Object.keys(color).map(colorKey => (
-           <div key={colorKey} style={styles.colorWrapper}>
-             <div style={Object.assign({}, styles.colorBox, {backgroundColor: color[colorKey]})}/>
-             <div style={styles.colorDoc}>
-               <div>{colorKey}</div>
-               <div>{color[colorKey]}</div>
-             </div>
-           </div>
-         ))}
-      </div>
-    ));
+  require('./about');
+  require('./colors');
 
   var context = require.context("../src/", true, /\.jsx$/);
   context.keys().forEach(key => {
@@ -83,7 +45,16 @@ console.log("the hex value for purple is:", colors.purple);`}
   });
 }
 
+function Centered({children}) {
+  return <div style={styles.centeredStory}>{children}</div>;
+}
+
 storybook.setAddon(infoAddon);
 storybook.setAddon(addStoriesGroup);
-storybook.addDecorator(centered);
+storybook.addDecorator(story => {
+  var rendered = story();
+  return (
+    <Centered>{rendered}</Centered>
+  );
+});
 storybook.configure(loadStories, module);
