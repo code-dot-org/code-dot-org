48054a147c801edd27c66537617a2e0e95a51e51
David Bailey <davidsbailey@users.noreply.github.com>
2016-06-23 14:52:50 -0700

    add 'Data' mode button when firebase is enabled. show existing code playspace and a new data workspace when clicked. [test ui]

---
+++ apps/src/templates/DataWorkspace.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+
+var DataWorkspace = React.createClass({
+  render: function () {
+    return <div id="dataWorkspaceWrapper">
+      Data workspace
+    </div>;
+  }
+});
+module.exports = DataWorkspace;
