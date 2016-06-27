41c7bb1120a19a7d0cb1ec7ec3d93bda1cc8366a
Asher Kach <asher@code.org>
2016-06-27 16:27:06 -0500

    Fixes violations of and reenables Style/UnneededInterpolation.

--- dashboard/lib/single_sign_on.rb
+++ dashboard/lib/single_sign_on.rb
@@ -73,7 +73,7 @@ class SingleSignOn
   end
 
   def to_url(base_url=nil)
-    base = "#{base_url || sso_url}"
+    base = (base_url || sso_url).to_s
     "#{base}#{base.include?('?') ? '&' : '?'}#{payload}"
   end
 
