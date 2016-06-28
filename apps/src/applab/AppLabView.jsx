df7769a2c436a64ba7aa4aaa8f494a6ee45cb5ad
David Bailey <davidsbailey@users.noreply.github.com>
2016-06-28 16:52:51 -0700

    extract workspaces out to per-app React views

--- apps/src/applab/AppLabView.jsx
+++ apps/src/applab/AppLabView.jsx
@@ -6,12 +6,17 @@ var ApplabVisualizationColumn = require('./ApplabVisualizationColumn');
 var ProtectedStatefulDiv = require('../templates/ProtectedStatefulDiv');
 var StudioAppWrapper = require('../templates/StudioAppWrapper');
 var InstructionsWithWorkspace = require('../templates/instructions/InstructionsWithWorkspace');
+import CodeWorkspace from '../templates/CodeWorkspace';
+import DataWorkspace from '../templates/DataWorkspace';
+import { connect } from 'react-redux';
 
 /**
  * Top-level React wrapper for App Lab.
  */
 var AppLabView = React.createClass({
   propTypes: {
+    hasDataMode: React.PropTypes.bool.isRequired,
+    hasDesignMode: React.PropTypes.bool.isRequired,
     isEditingProject: React.PropTypes.bool.isRequired,
 
     screenIds: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
@@ -34,10 +39,18 @@ var AppLabView = React.createClass({
         <ProtectedStatefulDiv
             id="visualizationResizeBar"
             className="fa fa-ellipsis-v" />
-        <InstructionsWithWorkspace/>
+        <InstructionsWithWorkspace>
+          <CodeWorkspace/>
+          {this.props.hasDesignMode &&
+              <ProtectedStatefulDiv id="designWorkspace" style={{display: 'none'}}/>}
+          {this.props.hasDataMode && <DataWorkspace/>}
+        </InstructionsWithWorkspace>
       </StudioAppWrapper>
     );
   }
 });
 
-module.exports = AppLabView;
+module.exports = connect(state => ({
+  hasDataMode: state.pageConstants.hasDataMode || false,
+  hasDesignMode: state.pageConstants.hasDesignMode || false
+}))(AppLabView);
