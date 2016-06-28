cfb38ca7c1dffbb64fe7d4301930c219d2bcdbe6
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-06-28 16:41:10 -0700

    cleanup and comments

--- apps/src/maze/collectorDrawer.js
+++ apps/src/maze/collectorDrawer.js
@@ -1,32 +1,16 @@
 import Drawer from './drawer';
+const SQUARE_SIZE = 50;
 let SVG_NS = require('../constants').SVG_NS;
 let cellId = require('./mazeUtils').cellId;
 
-const SQUARE_SIZE = 50;
-
+/**
+ * Extends Drawer to draw collectables for Collector
+ */
 export default class CollectorDrawer extends Drawer {
-  shouldUpdateItemImage(row, col, running) {
-    return this.map_.currentStaticGrid[row][col].getOriginalValue();
-  }
 
-  updateItemImage(row, col, running) {
-    if (this.shouldUpdateItemImage(row, col, running)) {
-      super.updateItemImage(row, col, running);
-      this.updateCounter_(row, col);
-    }
-  }
-
-  updateCounter_(row, col) {
-    let prefix = 'counter';
-    let counterText = this.map_.getValue(row, col) || null;
-    var counterElement = document.getElementById(cellId(prefix, row, col));
-    if (!counterElement) {
-      // we want an element, so let's create one
-      counterElement = this.createText(prefix, row, col, counterText);
-    }
-    counterElement.firstChild.nodeValue = counterText;
-  }
-
+  /**
+   * @override
+   */
   updateImageWithIndex_(prefix, row, col) {
     let img = super.updateImageWithIndex_(prefix, row, col);
     let val = this.map_.getValue(row, col);
@@ -34,14 +18,57 @@ export default class CollectorDrawer extends Drawer {
     return img;
   }
 
+  /**
+   * @override
+   */
+  updateItemImage(row, col, running) {
+    if (this.shouldUpdateItemImage(row, col, running)) {
+      super.updateItemImage(row, col);
+      this.updateCounter_(row, col);
+    }
+  }
+
+  /**
+   * Should the specified row and column be updated?
+   * @param {number} row
+   * @param {number} col
+   * @return {boolean}
+   */
+  shouldUpdateItemImage(row, col) {
+    let cell = this.map_.getCell(row, col);
+    return cell && cell.getOriginalValue();
+  }
+
+  /**
+   * Update the counter at the given row, col with the appropriate value
+   * @param {number} row
+   * @param {number} col
+   */
+  updateCounter_(row, col) {
+    let prefix = 'counter';
+    let counterText = this.map_.getValue(row, col) || null;
+    // get or create the appropriate counter element
+    let counterElement = document.getElementById(cellId(prefix, row, col))
+        || this.createText(prefix, row, col, counterText);
+    counterElement.firstChild.nodeValue = counterText;
+  }
+
+  /**
+   * Create a new text element at the specified row and column with the
+   * appropriate text
+   * @param {string} prefix
+   * @param {number} row
+   * @param {number} col
+   * @param {string} counterText
+   */
   createText(prefix, row, col, counterText) {
-    var pegmanElement = document.getElementsByClassName('pegman-location')[0];
-    var svg = document.getElementById('svgMaze');
+    let pegmanElement = document.getElementsByClassName('pegman-location')[0];
+    let svg = document.getElementById('svgMaze');
 
     // Create text.
-    var hPadding = 2;
-    var vPadding = 2;
-    var text = document.createElementNS(SVG_NS, 'text');
+    let hPadding = 2;
+    let vPadding = 2;
+    let text = document.createElementNS(SVG_NS, 'text');
     // Position text just inside the bottom right corner.
     text.setAttribute('x', (col + 1) * SQUARE_SIZE - hPadding);
     text.setAttribute('y', (row + 1) * SQUARE_SIZE - vPadding);
@@ -52,5 +79,4 @@ export default class CollectorDrawer extends Drawer {
 
     return text;
   }
-
 }
