987eb384ac314a770834f2c9d1e7feeb0ac41c6f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-06-28 15:39:55 -0700

    Creates a generic Maze Drawer class [test ui]
    And refactors BeeItemDrawer and DirtDrawer to extend it.
    
    Previously, BeeItemDrawer inherited from DirtDrawer, although DirtDrawer
    contained a lot of functionality that BeeItemDrawer intentionally
    discarded. This refactor plus ES6 class implementation makes their
    shared vs individual functionality a bit cleaner, and will also make it
    easier to add a new Drawer for the new WIP Collector Level type.

--- apps/src/maze/dirtDrawer.js
+++ apps/src/maze/dirtDrawer.js
@@ -1,129 +1,83 @@
-var cellId = require('./mazeUtils').cellId;
+import Drawer from './drawer';
 
 // The number line is [-inf, min, min+1, ... no zero ..., max-1, max, +inf]
-var DIRT_MAX = 10;
-var DIRT_COUNT = DIRT_MAX * 2 + 2;
+const DIRT_MAX = 10;
+const DIRT_COUNT = DIRT_MAX * 2 + 2;
 
-// Duplicated from maze.js so that I don't need a dependency
-var SQUARE_SIZE = 50;
-
-var SVG_NS = require('../constants').SVG_NS;
-
-var DirtDrawer = module.exports = function (map, dirtAsset) {
-  this.map_ = map;
-
-  this.dirtImageInfo_ = {
-    href: dirtAsset,
-    unclippedWidth: SQUARE_SIZE * DIRT_COUNT
-  };
-};
-
+const SQUARE_SIZE = 50;
 
 /**
- * Intentional noop function; BeeItemDrawer needs to be able to reset
- * between runs, so we implement a shared reset function so that we can
- * call Maze.gridItemDrawer.reset() blindly. Overridden by BeeItemDrawer
+ * Extends Drawer to draw dirt piles for Farmer.
  */
-DirtDrawer.prototype.reset = function () {};
-
-/**
- * Update the image at the given row,col by determining the spriteIndex for the
- * current value
- */
-DirtDrawer.prototype.updateItemImage = function (row, col, running) {
-  var val = this.map_.getValue(row, col) || 0;
-
-  // If the cell is a variable cell and we are not currently running,
-  // draw it as either a max-height pile or a max-depth pit.
-  if (this.map_.getVariableCell(row, col).isVariable() && !running) {
-    val = (val < 0) ? -11 : 11;
+export default class DirtDrawer extends Drawer {
+  constructor(map, dirtAsset) {
+    super(map, dirtAsset);
+    this.assetUnclippedWidth = SQUARE_SIZE * DIRT_COUNT;
   }
 
-  this.updateImageWithIndex_('dirt', row, col, this.dirtImageInfo_,
-    spriteIndexForDirt(val));
-};
+  /**
+   * @override
+   */
+  updateItemImage(row, col, running) {
+    let val = this.map_.getValue(row, col) || 0;
 
-/**
- * Update the image at the given row,col with the provided spriteIndex.
- */
-DirtDrawer.prototype.updateImageWithIndex_ = function (prefix, row, col, imageInfo, spriteIndex) {
-  var hiddenImage = (spriteIndex < 0 || imageInfo.href === null);
+    // If the cell is a variable cell and we are not currently running,
+    // draw it as either a max-height pile or a max-depth pit.
+    if (this.map_.getVariableCell(row, col).isVariable() && !running) {
+      val = (val < 0) ? -11 : 11;
+    }
 
-  var img = document.getElementById(cellId(prefix, row, col));
-  if (!img) {
-    // we don't need any image
-    if (hiddenImage) {
+    this.updateImageWithIndex_('dirt', row, col, this.spriteIndexForDirt_(val));
+  }
+
+  /**
+   * @override
+   */
+  updateImageWithIndex_(prefix, row, col, spriteIndex) {
+    let img = super.updateImageWithIndex_(prefix, row, col);
+    if (!img) {
       return;
     }
-    // we want an image, so let's create one
-    img = createImage(prefix, row, col, imageInfo);
-  } else if (imageInfo.href) {
-    //update img
-    img.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', imageInfo.href);
+
+    let hiddenImage = spriteIndex < 0;
+    img.setAttribute('visibility', hiddenImage ? 'hidden' : 'visible');
+    if (!hiddenImage) {
+      img.setAttribute('x', SQUARE_SIZE * (col - spriteIndex));
+    }
+    return img;
   }
 
-  img.setAttribute('visibility', hiddenImage ? 'hidden' : 'visible');
-  if (!hiddenImage) {
-    img.setAttribute('x', SQUARE_SIZE * (col - spriteIndex));
-    img.setAttribute('y', SQUARE_SIZE * row);
+  /**
+   * @override
+   */
+  createImage_(prefix, row, col) {
+    let img = super.createImage_(prefix, row, col);
+    img.setAttribute('width', this.assetUnclippedWidth);
+    return img;
   }
-};
 
-function createImage(prefix, row, col, imageInfo) {
-  var pegmanElement = document.getElementsByClassName('pegman-location')[0];
-  var svg = document.getElementById('svgMaze');
+  /**
+   * Given a dirt value, returns the index of the sprite to use in our spritesheet.
+   * Returns -1 if we want to display no sprite.
+   * @param {number} val
+   * @return {number}
+   */
+  spriteIndexForDirt_(val) {
+    let spriteIndex;
 
-  var clipId = cellId(prefix + 'Clip', row, col);
-  var imgId = cellId(prefix, row, col);
+    if (val === 0) {
+      spriteIndex = -1;
+    } else if (val < -DIRT_MAX) {
+      spriteIndex = 0;
+    } else if (val < 0) {
+      spriteIndex = DIRT_MAX + val + 1;
+    } else if (val > DIRT_MAX) {
+      spriteIndex = DIRT_COUNT - 1;
+    } else if (val > 0) {
+      spriteIndex = DIRT_MAX + val;
+    }
 
-  // Create clip path.
-  var clip = document.createElementNS(SVG_NS, 'clipPath');
-  clip.setAttribute('id', clipId);
-  var rect = document.createElementNS(SVG_NS, 'rect');
-  rect.setAttribute('x', col * SQUARE_SIZE);
-  rect.setAttribute('y', row * SQUARE_SIZE);
-  rect.setAttribute('width', SQUARE_SIZE);
-  rect.setAttribute('height', SQUARE_SIZE);
-  clip.appendChild(rect);
-  svg.insertBefore(clip, pegmanElement);
-  // Create image.
-  var img = document.createElementNS(SVG_NS, 'image');
-  img.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', imageInfo.href);
-  img.setAttribute('height', SQUARE_SIZE);
-  img.setAttribute('width', imageInfo.unclippedWidth);
-  img.setAttribute('clip-path', 'url(#' + clipId + ')');
-  img.setAttribute('id', imgId);
-  svg.insertBefore(img, pegmanElement);
+    return spriteIndex;
+  }
 
-  return img;
 }
-
-/**
- * Given a dirt value, returns the index of the sprite to use in our spritesheet.
- * Returns -1 if we want to display no sprite.
- */
-function spriteIndexForDirt(val) {
-  var spriteIndex;
-
-  if (val === 0) {
-    spriteIndex = -1;
-  } else if (val < -DIRT_MAX) {
-    spriteIndex = 0;
-  } else if (val < 0) {
-    spriteIndex = DIRT_MAX + val + 1;
-  } else if (val > DIRT_MAX) {
-    spriteIndex = DIRT_COUNT - 1;
-  } else if (val > 0) {
-    spriteIndex = DIRT_MAX + val;
-  }
-
-  return spriteIndex;
-}
-
-/* start-test-block */
-// export private function(s) to expose to unit testing
-DirtDrawer.__testonly__ = {
-  spriteIndexForDirt: spriteIndexForDirt,
-  createImage: createImage
-};
-/* end-test-block */
