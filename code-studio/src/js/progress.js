9a1f297153f4970d4844eaf7732c31aaafc0ae22
cpirich <chris@code.org>
2016-06-28 07:58:07 -0700

    use progress.refreshStageProgress to merge changes after clientState changes

--- code-studio/src/js/progress.js
+++ code-studio/src/js/progress.js
@@ -62,6 +62,14 @@ progress.renderStageProgress = function (stageData, progressData, scriptName, cu
     progress: _.mapValues(progressData.levels, level => level.submitted ? SUBMITTED_RESULT : level.result)
   });
 
+  // Provied a function that can be called later to merge in progress now saved on the client.
+  progress.refreshStageProgress = function () {
+    store.dispatch({
+      type: 'MERGE_PROGRESS',
+      progress: clientState.allLevelsProgress()[scriptName] || {}
+    });
+  };
+
   ReactDOM.render(
     <Provider store={store}>
       <StageProgress />
