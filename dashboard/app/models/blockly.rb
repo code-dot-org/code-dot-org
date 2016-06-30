d831d91e737f57dee409fe30bee890a1d248143f
Ram Kandasamy <ram@code.org>
2016-06-30 12:02:21 -0700

    remove level = self

--- dashboard/app/models/blockly.rb
+++ dashboard/app/models/blockly.rb
@@ -224,7 +224,6 @@ class Blockly < Level
   # Return a Blockly-formatted 'appOptions' hash derived from the level contents
   def blockly_options
     options = Rails.cache.fetch("#{cache_key}/blockly_level_options/v2") do
-      level = self
       level_prop = {}
 
       # Map Dashboard-style names to Blockly-style names in level object.
@@ -241,28 +240,28 @@ class Blockly < Level
         success_condition: 'fn_successCondition',
         failure_condition: 'fn_failureCondition',
       }
-      level.properties.keys.each do |dashboard|
+      properties.keys.each do |dashboard|
         blockly = overrides[dashboard.to_sym] || dashboard.camelize(:lower)
         # Select value from properties json
         # Don't override existing valid (non-nil/empty) values
-        value = JSONValue.value(level.properties[dashboard].presence)
+        value = JSONValue.value(properties[dashboard].presence)
         level_prop[blockly] = value unless value.nil? # make sure we convert false
       end
 
       # Set some specific values
 
-      if level.is_a? Blockly
-        level_prop['startBlocks'] = level.try(:project_template_level).try(:start_blocks) || level.start_blocks
-        level_prop['toolbox'] = level.try(:project_template_level).try(:toolbox_blocks) || level.toolbox_blocks
-        level_prop['codeFunctions'] = level.try(:project_template_level).try(:code_functions) || level.code_functions
+      if is_a? Blockly
+        level_prop['startBlocks'] = try(:project_template_level).try(:start_blocks) || start_blocks
+        level_prop['toolbox'] = try(:project_template_level).try(:toolbox_blocks) || toolbox_blocks
+        level_prop['codeFunctions'] = try(:project_template_level).try(:code_functions) || code_functions
       end
 
-      if level.is_a? Applab
-        level_prop['startHtml'] = level.try(:project_template_level).try(:start_html) || level.start_html
+      if is_a? Applab
+        level_prop['startHtml'] = try(:project_template_level).try(:start_html) || start_html
       end
 
-      if level.is_a?(Maze) && level.step_mode
-        step_mode = JSONValue.value(level.step_mode)
+      if is_a?(Maze) && step_mode
+        step_mode = JSONValue.value(step_mode)
         level_prop['step'] = step_mode == 1 || step_mode == 2
         level_prop['stepOnly'] = step_mode == 2
       end
@@ -291,17 +290,17 @@ class Blockly < Level
 
       app_options = {}
 
-      app_options[:levelGameName] = level.game.name if level.game
-      app_options[:skinId] = level.skin if level.is_a?(Blockly)
+      app_options[:levelGameName] = game.name if game
+      app_options[:skinId] = skin if is_a?(Blockly)
 
       # Set some values that Blockly expects on the root of its options string
       non_nil_level_prop = level_prop.reject!{|_, value| value.nil?}
       app_options.merge!({
                              baseUrl: Blockly.base_url,
-                             app: level.game.try(:app),
-                             levelId: level.level_num,
+                             app: game.try(:app),
+                             levelId: level_num,
                              level: non_nil_level_prop,
-                             droplet: level.game.try(:uses_droplet?),
+                             droplet: game.try(:uses_droplet?),
                              pretty: Rails.configuration.pretty_apps ? '' : '.min',
                          })
     end
