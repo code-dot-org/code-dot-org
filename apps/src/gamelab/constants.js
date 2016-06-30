c29eba601836b619c0b617cc565ec1272f4f02b2
Brad Buchanan <brad@code.org>
2016-06-30 12:31:11 -0700

    More correct blank image constant

--- apps/src/gamelab/constants.js
+++ apps/src/gamelab/constants.js
@@ -19,4 +19,4 @@ module.exports.GAME_HEIGHT = 400;
  * DataURL for a 1x1 transparent gif image.
  * @const {string}
  */
-module.exports.EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+module.exports.EMPTY_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
