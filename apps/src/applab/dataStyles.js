9510669c3e5d8f5f7a7ad9b78a20497f20b80418
David Bailey <davidsbailey@users.noreply.github.com>
2016-06-30 15:12:34 -0700

    extract common styles

---
+++ apps/src/applab/dataStyles.js
@@ -0,0 +1,61 @@
+/**
+ * @file Styles shared by various react components in the applab data browser.
+ */
+
+import color from '../color';
+
+const rowHeight = 45;
+const cellPadding = 10;
+
+const editRow = {
+  height: rowHeight
+};
+
+const addRow = {
+  height: rowHeight,
+  backgroundColor: color.lighter_purple
+};
+
+const cell = {
+  padding: cellPadding,
+  border: '1px solid gray',
+  fontSize: 14
+};
+
+const headerCell = {
+  padding: cellPadding,
+  border: '1px solid gray',
+  backgroundColor: color.teal
+};
+
+const link = {
+  color: color.purple,
+  fontFamily: "'Gotham 7r', sans-serif"
+};
+
+const button = {
+  margin: 0
+};
+
+const editButton = [button, {
+  marginRight: 10
+}];
+
+const input = {
+  width: 'calc(100% - 14px)',
+  height: 20,
+  border: '1px solid gray',
+  borderRadius: 5,
+  padding: '4px 6px'
+};
+
+export default {
+  editRow,
+  addRow,
+  cell,
+  headerCell,
+  link,
+  button,
+  editButton,
+  input
+};
