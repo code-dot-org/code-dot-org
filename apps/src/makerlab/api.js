71a533d130a7cadb505cc4398f6a3ca2cfc2ec99
Brian Jordan <bcjordan@gmail.com>
2016-07-06 16:41:05 -0700

    Use ES6 implicit object property names.

--- apps/src/makerlab/api.js
+++ apps/src/makerlab/api.js
@@ -1,54 +1,23 @@
 exports.pinMode = function (pin, mode) {
-  return Applab.executeCmd(null,
-      'pinMode',
-      {
-        'pin': pin,
-        'mode': mode
-      });
+  return Applab.executeCmd(null, 'pinMode', {pin, mode});
 };
 
 exports.digitalWrite = function (pin, value) {
-  return Applab.executeCmd(null,
-      'digitalWrite',
-      {
-        'pin': pin,
-        'value': value
-      });
+  return Applab.executeCmd(null, 'digitalWrite', {pin, value});
 };
 
 exports.digitalRead = function (pin, callback) {
-  return Applab.executeCmd(null,
-      'digitalRead',
-      {
-        'pin': pin,
-        'callback': callback
-      });
+  return Applab.executeCmd(null, 'digitalRead', {pin, callback});
 };
 
 exports.analogWrite = function (pin, value) {
-  return Applab.executeCmd(null,
-      'analogWrite',
-      {
-        'pin': pin,
-        'value': value
-      });
+  return Applab.executeCmd(null, 'analogWrite', {pin, value});
 };
 
 exports.analogRead = function (pin, callback) {
-  return Applab.executeCmd(null,
-      'analogRead',
-      {
-        'pin': pin,
-        'callback': callback
-      });
+  return Applab.executeCmd(null, 'analogRead', {pin, callback});
 };
 
 exports.onBoardEvent = function (component, event, callback) {
-  return Applab.executeCmd(null,
-      'onBoardEvent',
-      {
-        'component': component,
-        'event': event,
-        'callback': callback
-      });
+  return Applab.executeCmd(null, 'onBoardEvent', {component, event, callback});
 };
