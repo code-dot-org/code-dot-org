6864f4a608cacaf59387897e226c6c6f08a5c394
Brian Jordan <bcjordan@gmail.com>
2016-07-06 12:43:05 -0700

    Auto-format file.

--- apps/src/makerlab/api.js
+++ apps/src/makerlab/api.js
@@ -1,44 +1,56 @@
 exports.pinMode = function (pin, mode) {
   return Applab.executeCmd(null,
       'pinMode',
-      {'pin': pin,
-        'mode': mode });
+      {
+        'pin': pin,
+        'mode': mode
+      });
 };
 
 exports.digitalWrite = function (pin, value) {
   return Applab.executeCmd(null,
       'digitalWrite',
-      {'pin': pin,
-        'value': value });
+      {
+        'pin': pin,
+        'value': value
+      });
 };
 
 exports.digitalRead = function (pin, callback) {
   return Applab.executeCmd(null,
       'digitalRead',
-      {'pin': pin,
-        'callback': callback });
+      {
+        'pin': pin,
+        'callback': callback
+      });
 };
 
 exports.analogWrite = function (pin, value) {
   return Applab.executeCmd(null,
       'analogWrite',
-      {'pin': pin,
-        'value': value });
+      {
+        'pin': pin,
+        'value': value
+      });
 };
 
 exports.analogRead = function (pin, callback) {
   return Applab.executeCmd(null,
       'analogRead',
-      {'pin': pin,
-        'callback': callback });
+      {
+        'pin': pin,
+        'callback': callback
+      });
 };
 
 exports.onBoardEvent = function (component, event, callback) {
   var extraArgs = Array.prototype.slice.call(arguments).slice(3);
   return Applab.executeCmd(null,
       'onBoardEvent',
-      {'component': component,
+      {
+        'component': component,
         'event': event,
         'callback': callback,
-        'extraArgs': extraArgs});
+        'extraArgs': extraArgs
+      });
 };
