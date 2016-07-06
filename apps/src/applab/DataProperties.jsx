b95dc29708cb23aef083432f599af4abd04af9bb
David Bailey <davidsbailey@users.noreply.github.com>
2016-07-06 13:34:11 -0700

    extract AddKeyRow and EditKeyRow

--- apps/src/applab/DataProperties.jsx
+++ apps/src/applab/DataProperties.jsx
@@ -2,11 +2,11 @@
  * @overview Component for editing key/value pairs.
  */
 
+import AddKeyRow from './AddKeyRow';
 import { DataView } from './constants';
-import FirebaseStorage from './firebaseStorage';
+import EditKeyRow from './EditKeyRow';
 import Radium from 'radium';
 import React from 'react';
-import { castValue } from './dataUtils';
 import { changeView } from './redux/data';
 import { connect } from 'react-redux';
 import * as dataStyles from './dataStyles';
@@ -40,7 +40,6 @@ const DataProperties = React.createClass({
          &nbsp;&gt; Key/value pairs
         </h4>
 
-        {/* placeholder display of key-value pairs */}
         <table>
           <colgroup>
             <col width='200'/>
@@ -67,150 +66,6 @@ const DataProperties = React.createClass({
     );
   }
 });
-
-const AddKeyRow = Radium(React.createClass({
-  getInitialState() {
-    return {
-      key: '',
-      value: ''
-    };
-  },
-
-  handleKeyChange(event) {
-    this.setState({key: event.target.value});
-  },
-
-  handleValueChange(event) {
-    this.setState({value: event.target.value});
-  },
-
-  handleAdd() {
-    FirebaseStorage.setKeyValue(
-      this.state.key,
-      castValue(this.state.value),
-      () => this.setState(this.getInitialState()),
-      msg => console.warn(msg));
-  },
-
-  render() {
-    return (
-      <tr style={dataStyles.addRow}>
-        <td style={dataStyles.cell}>
-          <input
-              style={dataStyles.input}
-              onChange={this.handleKeyChange}
-              value={this.state.key}
-          />
-        </td>
-        <td style={dataStyles.cell}>
-          <input
-              style={dataStyles.input}
-              onChange={this.handleValueChange}
-              value={this.state.value}
-          />
-        </td>
-        <td style={dataStyles.cell}>
-          <button
-              className="btn btn-primary"
-              style={dataStyles.button}
-              onClick={this.handleAdd}
-          >
-            Add pair
-          </button>
-        </td>
-      </tr>
-    );
-  }
-}));
-
-const EditKeyRow = Radium(React.createClass({
-  propTypes: {
-    keyName: React.PropTypes.string.isRequired,
-    value: React.PropTypes.any.isRequired
-  },
-
-  getInitialState() {
-    return {
-      isEditing: false,
-      newValue: this.props.value
-    };
-  },
-
-  handleChange(event) {
-    this.setState({newValue: event.target.value});
-  },
-
-  handleEdit() {
-    this.setState({
-      isEditing: true,
-      newValue: this.props.value
-    });
-  },
-
-  handleSave() {
-    FirebaseStorage.setKeyValue(
-      this.props.keyName,
-      castValue(this.state.newValue),
-      this.handleSaveComplete,
-      this.handleError);
-  },
-
-  handleSaveComplete() {
-    this.setState({isEditing: false});
-  },
-
-  handleError(msg) {
-    console.warn(msg);
-  },
-
-  handleDelete() {
-    FirebaseStorage.deleteKeyValue(this.props.keyName, undefined, this.handleError);
-  },
-
-  render() {
-    return (
-      <tr style={dataStyles.editRow}>
-        <td style={dataStyles.cell}>{this.props.keyName}</td>
-        <td style={dataStyles.cell}>
-          {this.state.isEditing ?
-            <input
-                style={dataStyles.input}
-                value={this.state.newValue}
-                onChange={this.handleChange}
-            /> :
-            this.props.value}
-        </td>
-        <td style={dataStyles.cell}>
-          {
-            this.state.isEditing ?
-              <button
-                  className="btn btn-primary"
-                  style={dataStyles.editButton}
-                  onClick={this.handleSave}
-              >
-                Save
-              </button> :
-              <button
-                className="btn"
-                style={dataStyles.editButton}
-                onClick={this.handleEdit}
-              >
-                Edit
-              </button>
-          }
-
-          <button
-              className="btn btn-danger"
-              style={dataStyles.button}
-              onClick={this.handleDelete}
-          >
-            Delete
-          </button>
-        </td>
-      </tr>
-    );
-  }
-}));
 
 export default connect(state => ({
   view: state.data.view,
