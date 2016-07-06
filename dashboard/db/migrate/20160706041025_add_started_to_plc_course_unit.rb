102e8e6eb7319ce80c71ed0af11ad59a9d47b992
Mehal Shah <mehal@code.org>
2016-07-06 09:56:33 -0700

    PR feedback

--- dashboard/db/migrate/20160706041025_add_started_to_plc_course_unit.rb
+++ dashboard/db/migrate/20160706041025_add_started_to_plc_course_unit.rb
@@ -1,7 +1,5 @@
 class AddStartedToPlcCourseUnit < ActiveRecord::Migration
   def change
-    add_column :plc_course_units, :started, :boolean
-
-    Plc::CourseUnit.update_all(started: true)
+    add_column :plc_course_units, :started, :boolean, null: false, default: false
   end
 end
