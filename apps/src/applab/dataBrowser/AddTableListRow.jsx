ff9633b39a3d546b2f7fc85abb36097a4d30dcb7
David Bailey <davidsbailey@users.noreply.github.com>
2016-07-06 17:52:16 -0700

    extract EditLink, EditTableListRow, and AddTableListRow

---
+++ apps/src/applab/dataBrowser/AddTableListRow.jsx
@@ -0,0 +1,52 @@
+import Radium from 'radium';
+import React from 'react';
+import msg from '../../locale';
+import * as dataStyles from './dataStyles';
+
+const AddTableListRow = React.createClass({
+  propTypes: {
+    onTableAdd: React.PropTypes.func.isRequired
+  },
+
+  getInitialState() {
+    return {
+      newTableName: ''
+    };
+  },
+
+  handleAdd() {
+    if (this.state.newTableName) {
+      this.props.onTableAdd(this.state.newTableName);
+      this.setState(this.getInitialState());
+    }
+  },
+
+  handleChange(event) {
+    this.setState({newTableName: event.target.value});
+  },
+
+  render() {
+    return (
+      <tr style={dataStyles.addRow}>
+        <td style={dataStyles.cell}>
+          <input
+            style={dataStyles.input}
+            placeholder={msg.dataTableNamePlaceholder()}
+            value={this.state.newTableName}
+            onChange={this.handleChange}/>
+        </td>
+        <td style={dataStyles.cell}>
+          <button
+            className='btn btn-primary'
+            style={dataStyles.button}
+            onClick={this.handleAdd}
+          >
+            Add
+          </button>
+        </td>
+      </tr>
+    );
+  }
+});
+
+export default Radium(AddTableListRow);
---
+++ apps/src/applab/dataBrowser/EditTableListRow.jsx
@@ -0,0 +1,42 @@
+import { DataView } from '../constants';
+import EditLink from './EditLink';
+import Radium from 'radium';
+import React from 'react';
+import * as dataStyles from './dataStyles';
+
+const EditTableListRow = React.createClass({
+  propTypes: {
+    onTableDelete: React.PropTypes.func.isRequired,
+    onViewChange: React.PropTypes.func.isRequired,
+    tableName: React.PropTypes.string.isRequired
+  },
+
+  handleEdit() {
+    this.props.onViewChange(DataView.TABLE, this.props.tableName);
+  },
+
+  handleDelete() {
+    this.props.onTableDelete(this.props.tableName);
+  },
+
+  render() {
+    return (
+      <tr style={dataStyles.editRow}>
+        <td style={dataStyles.cell}>
+          <EditLink name={this.props.tableName} onClick={this.handleEdit}/>
+        </td>
+        <td style={dataStyles.cell}>
+          <button
+            className='btn btn-danger'
+            style={dataStyles.button}
+            onClick={this.handleDelete}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  }
+});
+
+export default Radium(EditTableListRow);
