0e671427a01c13c8731a72fc3f7c25f506b9a503
David Bailey <davidsbailey@users.noreply.github.com>
2016-07-06 17:31:30 -0700

    populate and live-update table list. implement add/delete table.

--- apps/src/applab/redux/data.js
+++ apps/src/applab/redux/data.js
@@ -6,12 +6,15 @@
 import { Record } from 'immutable';
 import { DataView } from '../constants';
 
+const ADD_TABLE_NAME = 'data/ADD_TABLE_NAME';
 const CHANGE_VIEW = 'data/CHANGE_VIEW';
+const DELETE_TABLE_NAME = 'data/DELETE_TABLE_NAME';
 const UPDATE_TABLE_DATA = 'data/UPDATE_TABLE_DATA';
 const UPDATE_KEY_VALUE_DATA = 'data/UPDATE_KEY_VALUE_DATA';
 
 const DataState = Record({
   view: DataView.OVERVIEW,
+  tableListMap: {},
   tableName: '',
   tableData: {},
   keyValueData: {}
@@ -20,7 +23,12 @@ const DataState = Record({
 const initialState = new DataState();
 
 export default function (state = initialState, action) {
+  let map;
   switch (action.type) {
+    case ADD_TABLE_NAME:
+      map = {};
+      map[action.tableName] = true;
+      return state.set('tableListMap', Object.assign({}, state.tableListMap, map));
     case CHANGE_VIEW:
       // Discard table data when not viewing a table, so that we don't momentarily
       // show data for the wrong table when we return to the table view.
@@ -28,6 +36,10 @@ export default function (state = initialState, action) {
         state = state.set('tableData', {});
       }
       return state.set('view', action.view).set('tableName', action.tableName);
+    case DELETE_TABLE_NAME:
+      map = Object.assign({}, state.tableListMap);
+      delete map[action.tableName];
+      return state.set('tableListMap', map);
     case UPDATE_KEY_VALUE_DATA:
       return state.set('keyValueData', action.keyValueData);
     case UPDATE_TABLE_DATA:
@@ -37,9 +49,28 @@ export default function (state = initialState, action) {
   }
 }
 
+/**
+ * Action which adds a table name to the table list map, if it doesn't exist already.
+ * @param {string} tableName
+ */
+export const addTableName = tableName => ({
+  type: ADD_TABLE_NAME,
+  tableName
+});
+
 export const changeView = (view, tableName) => ({
   type: CHANGE_VIEW,
   view,
+  tableName
+});
+
+/**
+ * Action which deletes a table name from the table list map, if it exists in the map.
+ * @param {string} tableName
+ */
+
+export const deleteTableName = tableName => ({
+  type: DELETE_TABLE_NAME,
   tableName
 });
 
