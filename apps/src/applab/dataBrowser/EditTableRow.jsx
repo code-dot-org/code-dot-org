2d8dbde4dd6ed273fbdf779097bab9a87aa28dcd
David Bailey <davidsbailey@users.noreply.github.com>
2016-07-07 17:19:34 -0700

    display table records in an html table and make it look like the mocks. provide 'records' child of table data directly via redux.

---
+++ apps/src/applab/dataBrowser/EditTableRow.jsx
@@ -0,0 +1,42 @@
+import Radium from 'radium';
+import React from 'react';
+import * as dataStyles from './dataStyles';
+
+const EditTableRow = React.createClass({
+  propTypes: {
+    columnNames: React.PropTypes.array.isRequired,
+    record: React.PropTypes.object.isRequired
+  },
+
+  render() {
+    return (
+      <tr style={dataStyles.editRow}>
+        {
+          this.props.columnNames.map(columnName => (
+            <td key={columnName} style={dataStyles.cell}>
+              {this.props.record[columnName]}
+            </td>
+          ))
+        }
+
+        <td style={dataStyles.cell}>
+          <button
+            className="btn"
+            style={dataStyles.editButton}
+          >
+            Edit
+          </button>
+
+          <button
+            className="btn btn-danger"
+            style={dataStyles.button}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  }
+});
+
+export default Radium(EditTableRow);
