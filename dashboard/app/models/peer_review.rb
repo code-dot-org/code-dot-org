ddfa4fe91e634c456fb52467e26d207a2d0b75ca
Mehal Shah <mehal@code.org>
2016-07-08 13:14:03 -0700

    More feedback

--- dashboard/app/models/peer_review.rb
+++ dashboard/app/models/peer_review.rb
@@ -141,16 +141,14 @@ class PeerReview < ActiveRecord::Base
   end
 
   def self.get_peer_review_summaries(user, script)
-    peer_review_summaries = []
-
     if user &&
         script.professional_learning_course? &&
         Plc::EnrollmentUnitAssignment.exists?(user: user, plc_course_unit: script.plc_course_unit)
 
-      peer_review_summaries = PeerReview.where(reviewer: user, script: script).map(&:summarize)
+      PeerReview.where(reviewer: user, script: script).map(&:summarize)
+    else
+      []
     end
-
-    peer_review_summaries
   end
 
   def summarize
