2e7ffedbffc50d90127a7a147f182906db52ec83
Paul Carduner <paul@carduner.net>
2016-07-11 13:30:22 -0700

    Add custom whenNoChildOfTypes react prop type

--- apps/src/propTypes.js
+++ apps/src/propTypes.js
@@ -48,3 +48,53 @@ export function childrenOfType(...validChildrenTypes) {
     return error;
   };
 }
+
+/**
+ * A custom React PropType for a prop that can only be specified
+ * when a child node of the corresponding type has not been specified.
+ * This is useful for having an API where your component accepts "configuration"
+ * either in the form of a prop, or as a child node. For example, to allow for:
+ *
+ * <IconButton>
+ *   <Icon src="/images/icons/cow.png"/>
+ *   Click the cow!
+ * </IconButton>
+ *
+ * --- or ---
+ *
+ * <IconButton icon="/images/icons/cow.png">Click the cow!</IconButton>
+ *
+ * Why would you want to do this? It allows callsites to use non-default Icon
+ * components if they want. Like so:
+ *
+ * <IconButton>
+ *   <AnimatedIcon animation="spin" svg="/images/icons/cow.svg"/>
+ *   Click the cow!
+ * </IconButton>
+ *
+ * Example:
+ *
+ *   propTypes: {
+ *     children: whenNoChildOfTypes(Icon, AnimatedIcon)
+ *   }
+ *
+ */
+export function whenNoChildOfTypes(...unexpectedChildTypes) {
+  return function (props, propName, componentName) {
+    if (!props.children || !props[propName]) {
+      return;
+    }
+    let error;
+    const actualChildrenTypes = React.Children.map(props['children'], el => el.type);
+    for (const childType of actualChildrenTypes) {
+      if (unexpectedChildTypes.includes(childType)) {
+        error = new Error(
+          `${componentName} was given a ${propName} prop and a ` +
+          `<${childType.name}> child, but only one of those is allowed.`
+        );
+        break;
+      }
+    }
+    return error;
+  };
+}
