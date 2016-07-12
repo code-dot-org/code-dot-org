b74c18f77ea145b12946c1bde83425ad4d0ddb58
Caley Brock <caleybrock@Caleys-MacBook-Pro.local>
2016-07-12 15:09:04 -0700

    Update comments and merge in current p5play lib

--- apps/src/gamelab/GameLabSprite.js
+++ apps/src/gamelab/GameLabSprite.js
@@ -105,33 +105,37 @@ module.exports.createSprite = function (x, y, width, height) {
     }
   });
 
+  // p5.play stores width unscaled, but users in
+  // Game Lab should access and set a scaled version.
   Object.defineProperty(s, 'width', {
     enumerable: true,
     configurable: true,
     get: function () {
-      if (s.internalWidth === undefined) {
+      if (s._internalWidth === undefined) {
         return 100;
       } else {
-        return s.internalWidth * this.scale;
+        return s._internalWidth * this.scale;
       }
     },
     set: function (value) {
-      s.internalWidth = value / this.scale;
+      s._internalWidth = value / this.scale;
     }
   });
 
+  // p5.play stores heigth unscaled, but users in
+  // Game Lab should access and set a scaled version.
   Object.defineProperty(s, 'height', {
     enumerable: true,
     configurable: true,
     get: function () {
-      if (s.internalHeight === undefined) {
+      if (s._internalHeight === undefined) {
         return 100;
       } else {
-        return s.internalHeight * this.scale;
+        return s._internalHeight * this.scale;
       }
     },
     set: function (value) {
-      s.internalHeight =  value / this.scale;
+      s._internalHeight =  value / this.scale;
     }
   });
 
