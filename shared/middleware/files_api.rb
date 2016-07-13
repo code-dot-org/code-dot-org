717ddbed6e17860bc66293fb5bb6616b7c562983
Brian Jordan <bcjordan@gmail.com>
2016-07-13 11:31:36 -0700

    WIP refactor, extract profanity privacy violation checker into its own helper

--- shared/middleware/files_api.rb
+++ shared/middleware/files_api.rb
@@ -1,11 +1,8 @@
 require 'cdo/aws/s3'
-require 'cdo/share_filtering'
 require 'cdo/rack/request'
 require 'sinatra/base'
 
 class FilesApi < Sinatra::Base
-  BLOCKLY_SOURCE_FILENAME = 'main.json'
-
   def max_file_size
     5_000_000 # 5 MB
   end
@@ -105,28 +102,6 @@ class FilesApi < Sinatra::Base
     content_type :json
 
     get_bucket_impl(endpoint).new.list(encrypted_channel_id).to_json
-  end
-
-  def profanity_privacy_violation?(filename, body)
-    return false unless filename == BLOCKLY_SOURCE_FILENAME
-
-    body_string = body.string
-
-    begin
-      parsed_json = JSON.parse(body_string)
-    rescue JSON::ParserError
-      return false
-    end
-
-    blockly_source = parsed_json['source']
-    return false unless blockly_source
-
-    begin
-      return ShareFiltering.find_share_failure(blockly_source, request.locale)
-    rescue OpenURI::HTTPError, IO::EAGAINWaitReadable
-      # If WebPurify or Geocoder are unavailable, default to viewable
-      return false
-    end
   end
 
   #
