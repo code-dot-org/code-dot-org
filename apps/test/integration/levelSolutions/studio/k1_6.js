4429405f79520d553e5b000902c54c4cf0868a2b
Ram Kandasamy <ram@code.org>
2016-07-14 13:09:51 -0700

    Add tests for diagonal blocks and normalize

--- apps/test/integration/levelSolutions/studio/k1_6.js
+++ apps/test/integration/levelSolutions/studio/k1_6.js
@@ -1,5 +1,4 @@
 var testUtils = require('../../../util/testUtils');
-var tickWrapper = require('../../../util/tickWrapper');
 var TestResults = require('@cdo/apps/constants.js').TestResults;
 
 module.exports = {
@@ -34,13 +33,12 @@ module.exports = {
         '</xml>',
       runBeforeClick: function (assert) {
         // add a completion on timeout since this is a freeplay level
-        tickWrapper.runOnAppTick(Studio, 30, function () {
+        setTimeout(function () {
           Studio.onPuzzleComplete();
-        });
+        }, 10000);
       },
       customValidator: function (assert) {
-        assert(document.getElementById('sprite0').getAttribute('visibility') === 'hidden',
-          'sprite is hidden');
+        assert.isFalse(window.Studio.sprite[0].visible, 'sprite is hidden');
         return true;
       },
       expected: {
@@ -48,6 +46,93 @@ module.exports = {
         testResult: TestResults.FREE_PLAY
       }
     },
-
+    {
+      // Move south, east, and northwest, and ensure we end up at 0,0
+      description: "Diagonal move is long enough",
+      xml: '<xml>' +
+        '  <block type="when_run" deletable="false">' +
+        '    <next>' +
+        '      <block type="studio_setSprite">' +
+        '        <title name="VALUE">"visible"</title>' +
+        '        <title name="SPRITE">0</title>' +
+        '        <next>' +
+        '          <block type="studio_moveSouthDistance">' +
+        '            <title name="SPRITE">0</title>' +
+        '            <next>' +
+        '              <block type="studio_moveEastDistance">' +
+        '                <title name="SPRITE">0</title>' +
+        '                <next>' +
+        '                  <block type="studio_moveNorthWestDistance">' +
+        '                    <title name="SPRITE">0</title>' +
+        '                  </block>' +
+        '                </next>' +
+        '              </block>' +
+        '            </next>' +
+        '          </block>' +
+        '        </next>' +
+        '      </block>' +
+        '    </next>' +
+        '  </block>' +
+        '</xml>',
+    runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        setTimeout(function () {
+          Studio.onPuzzleComplete();
+        }, 10000);
+      },
+      customValidator: function (assert) {
+        assert.equal(window.Studio.sprite[0].x, 0, 'sprite is at the far left');
+        assert.equal(window.Studio.sprite[0].y, 0, 'sprite is at the top');
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      }
+    },
+    {
+      // Move souteast, west, and north, and ensure we end up at 0,0
+      description: "Diagonal move is not too long",
+      xml: '<xml>' +
+        '  <block type="when_run" deletable="false">' +
+        '    <next>' +
+        '      <block type="studio_setSprite">' +
+        '        <title name="VALUE">"visible"</title>' +
+        '        <title name="SPRITE">0</title>' +
+        '        <next>' +
+        '          <block type="studio_moveSouthEastDistance">' +
+        '            <title name="SPRITE">0</title>' +
+        '            <next>' +
+        '              <block type="studio_moveWestDistance">' +
+        '                <title name="SPRITE">0</title>' +
+        '                <next>' +
+        '                  <block type="studio_moveNorthDistance">' +
+        '                    <title name="SPRITE">0</title>' +
+        '                  </block>' +
+        '                </next>' +
+        '              </block>' +
+        '            </next>' +
+        '          </block>' +
+        '        </next>' +
+        '      </block>' +
+        '    </next>' +
+        '  </block>' +
+        '</xml>',
+    runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        setTimeout(function () {
+          Studio.onPuzzleComplete();
+        }, 10000);
+      },
+      customValidator: function (assert) {
+        assert.equal(window.Studio.sprite[0].x, 0, 'sprite is at the far left');
+        assert.equal(window.Studio.sprite[0].y, 0, 'sprite is at the top');
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      }
+    }
   ]
 };
