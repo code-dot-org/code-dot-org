1a86140c91e64bab8f969c21806075842ee0fe02
Brad Buchanan <brad@code.org>
2016-07-15 12:39:44 -0700

    Add a couple of tests

--- apps/test/unit/utilsTest.js
+++ apps/test/unit/utilsTest.js
@@ -1,4 +1,4 @@
-import {isSubsequence} from '@cdo/apps/utils';
+import {isSubsequence, shallowCopy, cloneWithoutFunctions} from '@cdo/apps/utils';
 import {expect} from '../util/configuredChai';
 
 describe('utils modules', () => {
@@ -19,6 +19,32 @@ describe('utils modules', () => {
       expect(isSubsequence([1,2,3], [2,3])).to.be.true;
       expect(isSubsequence([1,2,3], [1,3])).to.be.true;
       expect(isSubsequence([1,2,3], [1,2,3])).to.be.true;
+    });
+  });
+
+  describe('shallowCopy', function () {
+    it("makes a shallow copy of an object", function () {
+      const originalObject = {
+        num: 2,
+        obj: {}
+      };
+      const newObject = shallowCopy(originalObject);
+      expect(newObject).not.to.equal(originalObject);
+      expect(newObject).to.deep.equal(originalObject);
+      expect(newObject.obj).to.equal(originalObject.obj);
+    });
+  });
+
+  describe('cloneWithoutFunctions', function () {
+    it('strips functions from an object', function () {
+      const originalObject = {
+        num: 2,
+        func: function () {}
+      };
+      const newObject = cloneWithoutFunctions(originalObject);
+      expect(newObject).not.to.equal(originalObject);
+      expect(newObject.num).to.equal(originalObject.num);
+      expect(newObject.func).to.be.undefined;
     });
   });
 });
