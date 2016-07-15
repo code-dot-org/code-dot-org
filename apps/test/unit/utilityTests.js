2a52892e1e618190fc73494f2ca1e64fbb8bdd69
Brad Buchanan <brad@code.org>
2016-07-15 12:58:38 -0700

    Remove dead code in utilityTests.js

--- apps/test/unit/utilityTests.js
+++ apps/test/unit/utilityTests.js
@@ -1,16 +1,7 @@
 import {assert} from '../util/configuredChai';
-var testUtils = require('./../util/testUtils');
-var xml = require('@cdo/apps/xml');
-
-testUtils.setupLocales();
-
-var utils = require('@cdo/apps/utils');
-var _ = require('lodash');
-var mazeUtils = require('@cdo/apps/maze/mazeUtils');
+import {cellId} from '@cdo/apps/maze/mazeUtils';
 
 describe("mazeUtils", function () {
-  var cellId = mazeUtils.cellId;
-
   it("can generate the correct cellIds", function () {
     assert.equal(cellId('dirt', 0, 0), 'dirt_0_0');
     assert.equal(cellId('dirt', 2, 4), 'dirt_2_4');
