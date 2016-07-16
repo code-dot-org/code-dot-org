b6159c6444890fb3722f3f412f82fa05cf5f7d63
Andrew Oberhardt <Andrew@Code.org>
2016-07-15 17:44:25 -0700

    Updated column name

--- dashboard/db/migrate/20160715015742_set_type_for_pd_workshop_sections.rb
+++ dashboard/db/migrate/20160715015742_set_type_for_pd_workshop_sections.rb
@@ -1,13 +1,13 @@
 class SetTypeForPdWorkshopSections < ActiveRecord::Migration
   def up
     Pd::Workshop.where.not(section:nil).each do |workshop|
-      workshop.section.update!(type: Section::TYPE_PD_WORKSHOP)
+      workshop.section.update!(section_type: Section::TYPE_PD_WORKSHOP)
     end
   end
 
   def down
-    Section.where(type: Section::TYPE_PD_WORKSHOP).each do |section|
-      section.update!(type: nil)
+    Section.where(section_type: Section::TYPE_PD_WORKSHOP).each do |section|
+      section.update!(section_type: nil)
     end
   end
 end
