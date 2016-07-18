53afe3120270670971db47a7ef2c1c5c5a0e5dc0
Brad Buchanan <brad@code.org>
2016-07-18 14:59:03 -0700

    Test findChildrenOfType helper

--- apps/test/unit/testUtilsTest.js
+++ apps/test/unit/testUtilsTest.js
@@ -1,6 +1,7 @@
 /** @file Who watches the watchers? */
+import React from 'react';
 import {expect} from '../util/configuredChai';
-import {forEveryBooleanPermutation} from '../util/testUtils';
+import {forEveryBooleanPermutation, findChildrenOfType} from '../util/testUtils';
 
 describe('forEveryBooleanPermutation', function () {
   it('invokes a function with no arguments once', function () {
@@ -53,5 +54,81 @@ describe('forEveryBooleanPermutation', function () {
       expectedInvocations.shift();
     });
     expect(expectedInvocations).to.be.empty();
+  });
+});
+
+describe('findChildrenOfType', function () {
+  const Foo = () => null;
+  const Bar = () => null;
+
+  it('throws when given no first argument', function () {
+    expect(() => {
+      findChildrenOfType(undefined, 'div');
+    }).to.throw(TypeError);
+  });
+
+  it('Does not return passed root component', function () {
+    const root = (
+        <Foo searchTarget>
+          <Bar/>
+        </Foo>
+    );
+    const foundChildren = findChildrenOfType(root, Foo);
+    expect(foundChildren.length).to.equal(0);
+  });
+
+  it('Locates child component', function () {
+    const root = (
+        <Foo>
+          <Bar searchTarget/>
+        </Foo>
+    );
+    const foundChildren = findChildrenOfType(root, Bar);
+    expect(foundChildren.length).to.equal(1);
+    expect(foundChildren[0].props.searchTarget).to.be.true;
+  });
+
+  it('Finds deeply-nested components', function () {
+    const root = (
+        <Foo>
+          <Bar>
+            <Foo searchTarget/>
+          </Bar>
+        </Foo>
+    );
+    const foundChildren = findChildrenOfType(root, Foo);
+    expect(foundChildren.length).to.equal(1);
+    expect(foundChildren[0].props.searchTarget).to.be.true;
+  });
+
+  it('Finds multiple components', function () {
+    const root = (
+        <Foo>
+          <Bar/>
+          <Bar/>
+          <Bar/>
+        </Foo>
+    );
+    const foundChildren = findChildrenOfType(root, Bar);
+    expect(foundChildren.length).to.equal(3);
+  });
+
+  it('Returns components nested at different levels in a flat list', function () {
+    const root = (
+        <Foo>
+          <Bar/>
+          <Foo>
+            <Bar/>
+          </Foo>
+          <Foo>
+            <Foo>
+              <Bar/>
+            </Foo>
+          </Foo>
+        </Foo>
+    );
+    const foundChildren = findChildrenOfType(root, Bar);
+    expect(foundChildren.length).to.equal(3);
+    expect(foundChildren.every(child => child.type === Bar)).to.be.true;
   });
 });
