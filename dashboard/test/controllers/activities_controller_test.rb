8bcf8ea7619924fa99b6309de4bebe9cd8507249
Brian Jordan <bcjordan@gmail.com>
2016-07-18 15:00:03 -0400

    Revert "Check for sharing policy violations (privacy / profanity) in project Play Lab apps"

--- dashboard/test/controllers/activities_controller_test.rb
+++ dashboard/test/controllers/activities_controller_test.rb
@@ -946,7 +946,7 @@ class ActivitiesControllerTest < ActionController::TestCase
     # end
 
     assert_does_not_create(LevelSource, GalleryActivity) do
-      post :milestone, user_id: @user.id, script_level_id: create(:script_level, :playlab).id, program: studio_program_with_text('shit')
+      post :milestone, user_id: @user.id, script_level_id: @script_level.id, :program => studio_program_with_text('shit')
     end
     assert_response :success
     expected_response = {
@@ -967,9 +967,7 @@ class ActivitiesControllerTest < ActionController::TestCase
 
     with_default_locale(:de) do
       assert_does_not_create(LevelSource, GalleryActivity) do
-        post :milestone, @milestone_params.merge(
-          script_level_id: create(:script_level, :playlab).id,
-          program: studio_program_with_text('scheiße'))
+        post :milestone, @milestone_params.merge(program: studio_program_with_text('scheiße'))
       end
     end
     assert_response :success
@@ -980,16 +978,14 @@ class ActivitiesControllerTest < ActionController::TestCase
   test 'sharing program with http error slogs' do
     # allow sharing when there's an error, slog so it's possible to look up and review later
 
-    WebPurify.stubs(:find_potential_profanity).raises(OpenURI::HTTPError.new('something broke', 'fake io'))
+    @controller.stubs(:find_share_failure).raises(OpenURI::HTTPError.new('something broke', 'fake io'))
     @controller.expects(:slog).with(:tag, :error, :level_source_id) do |params|
       params[:tag] == 'share_checking_error' && params[:error] == 'OpenURI::HTTPError: something broke' && !params[:level_source_id].nil?
     end
     @controller.expects(:slog).with(:tag) {|params| params[:tag] == 'activity_finish' }
 
     assert_creates(LevelSource) do
-      post :milestone, @milestone_params.merge(
-        script_level_id: create(:script_level, :playlab).id,
-        program: studio_program_with_text('shit'))
+      post :milestone, @milestone_params.merge(program: studio_program_with_text('shit'))
     end
 
     assert_response :success
@@ -1005,9 +1001,7 @@ class ActivitiesControllerTest < ActionController::TestCase
     @controller.expects(:slog).with(:tag) {|params| params[:tag] == 'activity_finish' }
 
     assert_creates(LevelSource) do
-      post :milestone, @milestone_params.merge(
-        script_level_id: create(:script_level, :playlab).id,
-        program: studio_program_with_text('shit'))
+      post :milestone, @milestone_params.merge(program: studio_program_with_text('shit'))
     end
 
     assert_response :success
@@ -1021,9 +1015,7 @@ class ActivitiesControllerTest < ActionController::TestCase
 
     with_default_locale(:es) do
       assert_does_not_create(LevelSource, GalleryActivity) do
-        post :milestone, @milestone_params.merge(
-          script_level_id: create(:script_level, :playlab).id,
-          program: studio_program_with_text('putamadre'))
+        post :milestone, @milestone_params.merge(program: studio_program_with_text('putamadre'))
       end
     end
     assert_response :success
@@ -1033,9 +1025,7 @@ class ActivitiesControllerTest < ActionController::TestCase
 
   test 'sharing program with phone number' do
     assert_does_not_create(LevelSource, GalleryActivity) do
-      post :milestone, @milestone_params.merge(
-        script_level_id: create(:script_level, :playlab).id,
-        program: studio_program_with_text('800-555-5555'))
+      post :milestone, @milestone_params.merge(program: studio_program_with_text('800-555-5555'))
     end
     assert_response :success
 
@@ -1051,12 +1041,9 @@ class ActivitiesControllerTest < ActionController::TestCase
   end
 
   test 'sharing when gatekeeper has disabled sharing does not work' do
-    script_level = create(:script_level, :playlab)
-    Gatekeeper.set('shareEnabled', where: {script_name: script_level.script.name}, value: false)
+    Gatekeeper.set('shareEnabled', where: {script_name: @script.name}, value: false)
 
-    post :milestone, @milestone_params.merge(
-      script_level_id: script_level.id,
-      program: studio_program_with_text('hey some text'))
+    post :milestone, @milestone_params.merge(program: studio_program_with_text('hey some text'))
 
     assert_response :success
     response = JSON.parse(@response.body)
@@ -1069,9 +1056,7 @@ class ActivitiesControllerTest < ActionController::TestCase
     WebPurify.stubs(:find_potential_profanity).returns false
     Gatekeeper.set('shareEnabled', where: {script_name: 'Best script ever'}, value: false)
 
-    post :milestone, @milestone_params.merge(
-      script_level_id: create(:script_level, :playlab).id,
-      program: studio_program_with_text('hey some text'))
+    post :milestone, @milestone_params.merge(program: studio_program_with_text('hey some text'))
 
     assert_response :success
     response = JSON.parse(@response.body)
