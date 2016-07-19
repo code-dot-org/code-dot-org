fc9c6b6b0412f24c562be6a2632fec3004729b89
Brad Buchanan <brad@code.org>
2016-07-19 12:51:46 -0700

    Add throwOnConsoleErrors utility
    Useful for enforcing valid React PropTypes in tests (normally failed PropType validation logs an error but does not cause a test failure).
    
    Usage:
    ```javascript
    describe('my feature', function () {
      throwOnConsoleErrors();
      it('does something that could call console.error()', function () {
        foo(); // If console.error() is called, this will throw.
      });
    });
    ```

--- apps/test/unit/testUtilsTest.js
+++ apps/test/unit/testUtilsTest.js
@@ -1,6 +1,11 @@
 /** @file Who watches the watchers? */
+import sinon from 'sinon';
 import {expect} from '../util/configuredChai';
-import {forEveryBooleanPermutation} from '../util/testUtils';
+import {
+    forEveryBooleanPermutation,
+    throwOnConsoleErrors
+} from '../util/testUtils';
+
 
 describe('forEveryBooleanPermutation', function () {
   it('invokes a function with no arguments once', function () {
@@ -53,5 +58,22 @@ describe('forEveryBooleanPermutation', function () {
       expectedInvocations.shift();
     });
     expect(expectedInvocations).to.be.empty();
+  });
+});
+
+describe('throwOnConsoleErrors', function () {
+  describe('without it', function () {
+    it('console.error does not throw an exception', function () {
+        expect(() => console.error('This console.error call is intentional.'))
+            .not.to.throw();
+    });
+  });
+
+  describe('with it', function () {
+    throwOnConsoleErrors();
+    it('console.error does throw an exception', function () {
+      expect(() => console.error('should throw'))
+          .to.throw(Error, 'should throw');
+    });
   });
 });
