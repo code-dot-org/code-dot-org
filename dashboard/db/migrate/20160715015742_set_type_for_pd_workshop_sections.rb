88af3fdc17edf8d72f8dc1c6e8d52bc1c9d8fbdd
Andrew Oberhardt <Andrew@Code.org>
2016-07-19 16:05:33 -0700

    PR feedback accidentally left out from https://github.com/code-dot-org/code-dot-org/pull/9549, and updated schema

--- dashboard/db/migrate/20160715015742_set_type_for_pd_workshop_sections.rb
+++ dashboard/db/migrate/20160715015742_set_type_for_pd_workshop_sections.rb
@@ -1,12 +1,12 @@
 class SetTypeForPdWorkshopSections < ActiveRecord::Migration
   def up
-    Pd::Workshop.where.not(section:nil).each do |workshop|
+    Pd::Workshop.where.not(section: nil).find_each do |workshop|
       workshop.section.update!(section_type: Section::TYPE_PD_WORKSHOP)
     end
   end
 
   def down
-    Section.where(section_type: Section::TYPE_PD_WORKSHOP).each do |section|
+    Section.where(section_type: Section::TYPE_PD_WORKSHOP).find_each do |section|
       section.update!(section_type: nil)
     end
   end
