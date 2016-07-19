570835f79215155337c8f62427213172bd843a36
Brad Buchanan <brad@code.org>
2016-07-19 14:39:15 -0700

    Use enzyme to test Dialog

--- apps/test/unit/templates/DialogTest.js
+++ apps/test/unit/templates/DialogTest.js
@@ -1,43 +1,28 @@
 /** @file Tests for Dialog component */
 import React from 'react';
-import ReactTestUtils from 'react-addons-test-utils';
+import {shallow} from 'enzyme';
 import {expect} from '../../util/configuredChai';
 import {setupLocales} from '../../util/testUtils';
-import {findChildrenWithClass} from '../../util/testUtils';
 
 // Import order matters here :(
 setupLocales();
-var Dialog = require('@cdo/apps/templates/Dialog').default;
+const Dialog = require('@cdo/apps/templates/Dialog').default;
 
 describe('Dialog', function () {
   describe('fullWidth option', function () {
-    function getModalDiv(fromRoot) {
-      const modals = findChildrenWithClass(fromRoot, 'modal');
-      expect(modals.length).to.equal(1);
-      return modals[0];
-    }
-
     it('has only the modal class (no explicit width) by default', function () {
       // Shallow-render two layers to see width properties rendered by BaseDialog
-      const baseDialog = shallowRender(<Dialog isOpen/>);
-      const result = shallowRender(baseDialog);
-      expect(getModalDiv(result).props.style).to.be.undefined;
+      const result = shallow(<Dialog isOpen/>).shallow();
+      expect(result.find('.modal').props().style).to.be.undefined;
     });
 
     it('has 90% width and -45% left margin if fullWidth is provided', function () {
       // Shallow-render two layers to see width properties rendered by BaseDialog
-      const baseDialog = shallowRender(<Dialog isOpen fullWidth/>);
-      const result = shallowRender(baseDialog);
-      expect(getModalDiv(result).props.style).to.deep.equal({
+      const result = shallow(<Dialog isOpen fullWidth/>).shallow();
+      expect(result.find('.modal').props().style).to.deep.equal({
         width: '90%',
         marginLeft: '-45%'
       });
     });
   });
 });
-
-function shallowRender(component) {
-  let renderer = ReactTestUtils.createRenderer();
-  renderer.render(component);
-  return renderer.getRenderOutput();
-}
