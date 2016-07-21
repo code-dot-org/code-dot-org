331aed78a5a1fc7b54c6ab02b555f037bee9553d
Brad Buchanan <brad@code.org>
2016-07-21 16:05:30 -0700

    Remove locale loading stuff unneeded after rebase

--- apps/test/unit/netsim/NetSimLogBrowser.js
+++ apps/test/unit/netsim/NetSimLogBrowser.js
@@ -2,10 +2,7 @@
 import React from 'react';
 import ReactTestUtils from 'react-addons-test-utils';
 import {expect} from '../../util/configuredChai';
-// TODO: Figure out how to `import` these and have the require order work out.
-var testUtils = require('../../util/testUtils');
-testUtils.setupLocale('netsim');
-var NetSimLogBrowser = require('@cdo/apps/netsim/NetSimLogBrowser');
+import NetSimLogBrowser from '@cdo/apps/netsim/NetSimLogBrowser';
 
 describe('NetSimLogBrowser', function () {
   it('renders warning-free with the least possible parameters', function () {
