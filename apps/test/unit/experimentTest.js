290d8914adc91f154360d914e72c859e42b822ff
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-07-21 15:24:57 -0700

    remove deprecated testUtils.setupLocales call [test ui] [test all]

--- apps/test/unit/experimentTest.js
+++ apps/test/unit/experimentTest.js
@@ -1,6 +1,5 @@
 import {assert} from '../util/configuredChai';
 var testUtils = require('../util/testUtils');
-testUtils.setupLocales();
 testUtils.setExternalGlobals();
 
 var experiments = require('@cdo/apps/experiments');
