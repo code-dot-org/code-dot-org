f8e5781395df102f5e558404c195f0d55e6381fd
Jeremy Stone <jeremy@code.org>
2016-07-21 09:16:05 -0700

    move test keys into constants, force teacher age in LTI-created accounts to be 21 to match normal teacher accounts, fix lint warnings

--- dashboard/test/controllers/lti_provider_controller_test.rb
+++ dashboard/test/controllers/lti_provider_controller_test.rb
@@ -3,19 +3,22 @@ require 'test_helper'
 class LtiProviderControllerTest < ActionController::TestCase
   include Mocha::API
 
+  TEST_PROVIDER_KEY = "dc3872a4b605f1f36242a837172ce2c0"
+  TEST_CONSUMER_KEY = "f10ee9fc082219227976f2c1603a3d77"
+
   setup do
-    CDO.lti_credentials["f10ee9fc082219227976f2c1603a3d77"] = \
-      "dc3872a4b605f1f36242a837172ce2c0"
+    CDO.lti_credentials[TEST_CONSUMER_KEY] = TEST_PROVIDER_KEY
   end
 
   def lti_consumer_params(consumer_key, consumer_secret, test_name, *args)
-
     # match URL used by MiniTest post :sso call
     tc = IMS::LTI::ToolConfig.new(title: 'Test LTI Provider',
                                   launch_url: 'http://test.host/auth/lti')
 
-    @consumer = IMS::LTI::ToolConsumer.new(consumer_key,
-                                           consumer_secret, args[0])
+    @consumer = IMS::LTI::ToolConsumer.new(
+      consumer_key,
+      consumer_secret,
+      args[0])
     @consumer.set_config(tc)
 
     # Set some launch data, see:
@@ -36,15 +39,14 @@ class LtiProviderControllerTest < ActionController::TestCase
   end
 
   test "LTI sign-in with existing CDO account, signs in" do
-    consumer_key = "f10ee9fc082219227976f2c1603a3d77"
     user = create(:student)
-    user.provider = "lti_#{consumer_key}"
+    user.provider = "lti_#{TEST_CONSUMER_KEY}"
     user.uid = "12345"
     user.save
 
     params = lti_consumer_params(
-      consumer_key,
-      "dc3872a4b605f1f36242a837172ce2c0", method_name,
+      TEST_CONSUMER_KEY,
+      TEST_PROVIDER_KEY, method_name,
       "user_id" => user.uid,
       "custom_age" => user.age,
       "lis_person_name_full" => user.name)
@@ -56,8 +58,8 @@ class LtiProviderControllerTest < ActionController::TestCase
 
   test "LTI sign-in with new user_id and no :role, creates new Student account" do
     params = lti_consumer_params(
-      "f10ee9fc082219227976f2c1603a3d77",
-      "dc3872a4b605f1f36242a837172ce2c0", method_name,
+      TEST_CONSUMER_KEY,
+      TEST_PROVIDER_KEY, method_name,
       "custom_age" => 11,
       "user_id" => "12345",
       "lis_person_name_full" => "Cat Hat")
@@ -79,8 +81,8 @@ class LtiProviderControllerTest < ActionController::TestCase
 
   test "LTI sign-in with new user_id can also create new Teacher account" do
     params = lti_consumer_params(
-      "f10ee9fc082219227976f2c1603a3d77",
-      "dc3872a4b605f1f36242a837172ce2c0", method_name,
+      TEST_CONSUMER_KEY,
+      TEST_PROVIDER_KEY, method_name,
       "user_id" => "12345",
       "lis_person_name_full" => "Cat Hat",
       "roles" => "Instructor")
@@ -99,8 +101,8 @@ class LtiProviderControllerTest < ActionController::TestCase
 
   test "LTI sign-in can also handle multiple LTI roles" do
     params = lti_consumer_params(
-      "f10ee9fc082219227976f2c1603a3d77",
-      "dc3872a4b605f1f36242a837172ce2c0", method_name,
+      TEST_CONSUMER_KEY,
+      TEST_PROVIDER_KEY,  method_name,
       "user_id" => "12345",
       "lis_person_name_full" => "Cat Hat",
       "roles" => "Instructor, TeachingAssistant, ContentDeveloper")
@@ -117,8 +119,8 @@ class LtiProviderControllerTest < ActionController::TestCase
 
   test "LTI sign-in can also handle multiple Learner roles (edge case)" do
     params = lti_consumer_params(
-      "f10ee9fc082219227976f2c1603a3d77",
-      "dc3872a4b605f1f36242a837172ce2c0", method_name,
+      TEST_CONSUMER_KEY,
+      TEST_PROVIDER_KEY,  method_name,
       "user_id" => "12345",
       "lis_person_name_full" => "Cat Hat",
       "custom_age" => 9,
@@ -136,11 +138,9 @@ class LtiProviderControllerTest < ActionController::TestCase
   end
 
   test "LTI sign-in creates new User with no password and provider set" do
-    consumer_key = "f10ee9fc082219227976f2c1603a3d77"
-
     params = lti_consumer_params(
-      consumer_key,
-      "dc3872a4b605f1f36242a837172ce2c0", method_name,
+      TEST_CONSUMER_KEY,
+      TEST_PROVIDER_KEY,  method_name,
       "user_id" => "12345",
       "lis_person_name_full" => "Cat Hat",
       "custom_age" => 8,
@@ -151,17 +151,15 @@ class LtiProviderControllerTest < ActionController::TestCase
     end
 
     user = User.last
-    assert_equal "lti_#{consumer_key}", user.provider
+    assert_equal "lti_#{TEST_CONSUMER_KEY}", user.provider
     assert_equal "12345", user.uid
     assert_nil user.password
   end
 
-
-
   test "LTI sign-in with no user_id does not sign in" do
     params = lti_consumer_params(
-      "f10ee9fc082219227976f2c1603a3d77",
-      "dc3872a4b605f1f36242a837172ce2c0", method_name,
+      TEST_CONSUMER_KEY,
+      TEST_PROVIDER_KEY, method_name,
       "custom_age" => 8,
       "lis_person_name_full" => "Cat Hat")
 
@@ -175,8 +173,8 @@ class LtiProviderControllerTest < ActionController::TestCase
 
   test "LTI sign-in with bad oauth_timestamp does not sign in" do
     params = lti_consumer_params(
-      "f10ee9fc082219227976f2c1603a3d77",
-      "dc3872a4b605f1f36242a837172ce2c0", method_name,
+      TEST_CONSUMER_KEY,
+      TEST_PROVIDER_KEY,  method_name,
       "oauth_timestamp" => Time.now.to_i - 6.minutes,
       "custom_age" => 8,
       "user_id" => "12345",
@@ -192,8 +190,8 @@ class LtiProviderControllerTest < ActionController::TestCase
 
   test "LTI sign-in with no Age for Student does not sign in" do
     params = lti_consumer_params(
-      "f10ee9fc082219227976f2c1603a3d77",
-      "dc3872a4b605f1f36242a837172ce2c0", method_name,
+      TEST_CONSUMER_KEY,
+      TEST_PROVIDER_KEY, method_name,
       "custom_age" => nil,
       "user_id" => "12345",
       "lis_person_name_full" => "Cat Hat")
