f64d5f4ba38f27c14b3b327f8d31b3de423d5d32
Jeremy Stone <jeremy@code.org>
2016-07-21 11:18:30 -0700

    test updates

--- dashboard/app/helpers/lti_helper.rb
+++ dashboard/app/helpers/lti_helper.rb
@@ -33,7 +33,8 @@ module LtiHelper
     end
 
     consumer_key = params[:oauth_consumer_key]
-    consumer_secret = CDO.lti_credentials[consumer_key]
+    lti_credentials = CDO.lti_credentials || {}
+    consumer_secret = lti_credentials[consumer_key]
     if consumer_secret.blank?
       redirect_to new_user_registration_path,
         alert: "Sorry, invalid consumer_key [#{consumer_key}]"
