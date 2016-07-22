dd18c0868573b4574993e85c7b40b47fb2a75e86
Brian Jordan <bcjordan@gmail.com>
2016-07-22 17:15:26 -0400

    Use React for class application.

--- apps/src/code-studio/makerlab/setupPage.js
+++ apps/src/code-studio/makerlab/setupPage.js
@@ -8,20 +8,125 @@ const WAITING = 1;
 const ATTEMPTING = 2;
 const SUCCEEDED = 3;
 const FAILED = 4;
-const STEP_STATUSES = [HIDDEN, WAITING, ATTEMPTING, SUCCEEDED, FAILED];
+const CELEBRATING = 5;
+const STEP_STATUSES = [HIDDEN, WAITING, ATTEMPTING, SUCCEEDED, FAILED, CELEBRATING];
 
 const BoardSetupStatus = React.createClass({
+  getInitialState() {
+    return {
+      "status-is-chrome": WAITING,
+      "status-app-installed": WAITING,
+      "status-windows-drivers": WAITING,
+      "status-board-plug": WAITING,
+      "status-board-connect": WAITING,
+      "status-board-components": WAITING
+    }
+  },
+
   render() {
     return (
         <div className="setup-status">
-          <SetupStep stepStatus={WAITING} stepId="is-chrome" stepName="Chrome version 33+"/>
-          <SetupStep stepStatus={WAITING} stepId="app-installed" stepName="Chrome App installed"/>
-          <SetupStep stepStatus={WAITING} stepId="windows-drivers" stepName="Windows drivers installed"/>
-          <SetupStep stepStatus={WAITING} stepId="board-plug" stepName="Board plugged in"/>
-          <SetupStep stepStatus={WAITING} stepId="board-connect" stepName="Board connectable"/>
-          <SetupStep stepStatus={WAITING} stepId="board-components" stepName="Board components usable"/>
+          <SetupStep stepStatus={this.state['status-is-chrome']} stepId="is-chrome" stepName="Chrome version 33+"/>
+          <SetupStep stepStatus={this.state['status-app-installed']} stepId="app-installed" stepName="Chrome App installed"/>
+          <SetupStep stepStatus={this.state['status-windows-drivers']} stepId="windows-drivers" stepName="Windows drivers installed"/>
+          <SetupStep stepStatus={this.state['status-board-plug']} stepId="board-plug" stepName="Board plugged in"/>
+          <SetupStep stepStatus={this.state['status-board-connect']} stepId="board-connect" stepName="Board connectable"/>
+          <SetupStep stepStatus={this.state['status-board-components']} stepId="board-components" stepName="Board components usable"/>
         </div>
     );
+  },
+
+  hide(selector) {
+    this.setState({[`status-${selector}`]: HIDDEN});
+  },
+
+  fail(selector) {
+    this.setState({[`status-${selector}`]: FAILED});
+  },
+
+  spin(selector) {
+    this.setState({[`status-${selector}`]: ATTEMPTING});
+  },
+
+  succeed(selector) {
+    this.setState({[`status-${selector}`]: SUCCEEDED});
+  },
+
+  thumb(selector) {
+    this.setState({[`status-${selector}`]: CELEBRATING});
+  },
+
+  componentDidMount() {
+    const isChrome = !!window.chrome;
+    const gtChrome33 = isChrome && getChromeVersion() >= 33;
+
+    function getChromeVersion() {
+      const raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+      return raw ? parseInt(raw[2], 10) : false;
+    }
+
+    function isMacintosh() {
+      return navigator.platform.indexOf('Mac') > -1;
+    }
+
+    function isWindows() {
+      return navigator.platform.indexOf('Win') > -1;
+    }
+
+    if (isChrome) {
+      if (gtChrome33) {
+        this.succeed('is-chrome');
+      } else {
+        $('is-chrome').append("Your Chrome version is " + getChromeVersion() + ", please upgrade to at least version 33");
+      }
+
+      const bc = new BoardController();
+      Promise.resolve().then(() => {
+        this.spin('app-installed');
+        return bc.ensureAppInstalled()
+            .then(() => this.succeed('app-installed'))
+            .catch((error) => this.fail('app-installed'));
+      }).then(() => {
+        this.spin('board-plug');
+        return BoardController.getDevicePort()
+            .then(() => this.succeed('board-plug'))
+            .catch((error) => this.fail('board-plug'));
+      }).then(() => {
+        this.spin('board-connect');
+        return bc.ensureBoardConnected()
+            .then(() => this.succeed('board-connect'))
+            .catch((error) => this.fail('board-connect'));
+      }).then(() => {
+        this.spin('board-components');
+        return bc.connectWithComponents()
+            .then(() => this.thumb('board-components'))
+            .then(() => {
+              bc.prewiredComponents.buzzer.play({
+                song: [
+                  ["G3", 100], ["C4", 100], ["E4", 100], ["G4", 50],
+                  [null, 150], ["E4", 75], ["G4", 400]
+                ], tempo: 45000
+              });
+              bc.prewiredComponents.colorLeds.forEach(l => l.color('green'));
+            })
+            .then(() => promiseWaitFor(1600))
+            .then(() => {
+              bc.prewiredComponents.colorLeds.forEach(l => l.off());
+              bc.prewiredComponents.buzzer.play({
+                song: [["C4", 100]],
+                tempo: 45000
+              });
+            })
+            .then(() => this.succeed('board-components'))
+            .catch((error) => this.fail('board-components'));
+      });
+    } else {
+      this.fail('is-chrome');
+    }
+
+    if (!isWindows()) {
+      this.hide('windows-drivers');
+    }
   }
 });
 
@@ -31,91 +136,57 @@ const SetupStep = React.createClass({
     stepId: React.PropTypes.string.isRequired,
     stepStatus: React.PropTypes.oneOf(STEP_STATUSES).isRequired,
   },
+
+  iconFor(stepStatus) {
+    switch (stepStatus) {
+      case WAITING:
+        return "fa fa-fw fa-clock-o";
+      case ATTEMPTING:
+        return "fa fa-fw fa-spinner fa-spin";
+      case SUCCEEDED:
+        return "fa fa-fw fa-check-circle";
+      case CELEBRATING:
+        return "fa fa-fw fa-thumbs-o-up";
+      case FAILED:
+        return "fa fa-fw fa-times-circle";
+      default:
+        throw new Error('Unknown step status.');
+    }
+  },
+
+  classFor(stepStatus) {
+    switch(stepStatus) {
+      case WAITING:
+        return 'waiting';
+      case SUCCEEDED:
+      case CELEBRATING:
+        return 'complete';
+      case ATTEMPTING:
+        return 'waiting';
+        return 'waiting';
+      case HIDDEN:
+        return 'hidden';
+      default:
+        return 'incomplete';
+    }
+  },
+
   render() {
+    if (this.props.stepStatus === HIDDEN) {
+      return null;
+    }
     return (
-        <div id={this.props.stepId} className="waiting"><i className="fa fa-fw fa-clock-o"/><span>{this.props.stepName}</span></div>
+        <div id={this.props.stepId} className={this.classFor(this.props.stepStatus)}><i className={this.iconFor(this.props.stepStatus)}/><span>{this.props.stepName}</span></div>
     );
   }
 });
 
 $(function () {
-  ReactDOM.render((
+  const boardStatus = ReactDOM.render((
         <BoardSetupStatus/>
   ), document.getElementById('setup-status-mount'));
 
   $('.maker-setup a').attr('target', '_blank');
-
-  const isChrome = !!window.chrome;
-  const gtChrome33 = isChrome && getChromeVersion() >= 33;
-
-  function getChromeVersion() {
-    const raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
-
-    return raw ? parseInt(raw[2], 10) : false;
-  }
-
-  function isMacintosh() {
-    return navigator.platform.indexOf('Mac') > -1;
-  }
-
-  function isWindows() {
-    return navigator.platform.indexOf('Win') > -1;
-  }
-
-  if (isChrome) {
-    if (gtChrome33) {
-      check('#is-chrome');
-    } else {
-      $('#is-chrome').append("Your Chrome version is " + getChromeVersion() + ", please upgrade to at least version 33");
-    }
-
-    const bc = new BoardController();
-    Promise.resolve().then(() => {
-      spin('#app-installed');
-      return bc.ensureAppInstalled()
-          .then(() => check('#app-installed'))
-          .catch((error) => fail('#app-installed'));
-    }).then(() => {
-      spin('#board-plug');
-      return BoardController.getDevicePort()
-          .then(() => check('#board-plug'))
-          .catch((error) => fail('#board-plug'));
-    }).then(() => {
-      spin('#board-connect');
-      return bc.ensureBoardConnected()
-          .then(() => check('#board-connect'))
-          .catch((error) => fail('#board-connect'));
-    }).then(() => {
-      spin('#board-components');
-      return bc.connectWithComponents()
-          .then(() => thumb('#board-components'))
-          .then(() => {
-            bc.prewiredComponents.buzzer.play({
-              song: [
-                ["G3", 100], ["C4", 100], ["E4", 100], ["G4", 50],
-                [null, 150], ["E4", 75], ["G4", 400]
-              ], tempo: 45000
-            });
-            bc.prewiredComponents.colorLeds.forEach(l => l.color('green'));
-          })
-          .then(() => promiseWaitFor(1600))
-          .then(() => {
-            bc.prewiredComponents.colorLeds.forEach(l => l.off());
-            bc.prewiredComponents.buzzer.play({
-              song: [["C4", 100]],
-              tempo: 45000
-            });
-          })
-          .then(() => check('#board-components'))
-          .catch((error) => fail('#board-components'));
-    });
-  } else {
-    fail('#is-chrome');
-  }
-
-  if (isWindows()) {
-    $('#windows-drivers').removeClass('hidden').addClass('incomplete');
-  }
 });
 
 function promiseWaitFor(ms) {
@@ -124,25 +195,4 @@ function promiseWaitFor(ms) {
   });
 }
 
-function fail(selector, e) {
-  $(selector).addClass('incomplete').removeClass('complete waiting');
-  $(selector + ' i').removeClass('fa-spinner fa-spin').addClass('fa fa-times-circle');
-  if (e) {
-    console.error(e);
-  }
-}
 
-function spin(selector) {
-  $(selector).removeClass('waiting').addClass('complete');
-  $(selector + ' i').removeClass('fa-times-circle fa-clock-o').addClass('fa-spinner fa-spin');
-}
-
-function check(selector) {
-  $(selector).removeClass('waiting').addClass('complete');
-  $(selector + ' i').removeClass('fa-times-circle fa-clock-o fa-spinner fa-thumbs-o-up fa-spin').addClass('fa-check-circle');
-}
-
-function thumb(selector) {
-  $(selector).removeClass('waiting').addClass('complete');
-  $(selector + ' i').removeClass('fa-times-circle fa-clock-o fa-spinner fa-spin').addClass('fa-thumbs-o-up');
-}
