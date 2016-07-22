b46bef3f3ac1b441a69c1e9d351e97e82be39709
Brian Jordan <bcjordan@gmail.com>
2016-07-22 12:00:25 -0400

    Refactor to use BoardController connection methods on setup page. - Add default "waiting" state for steps which haven't been reached. - Refactor to use a promise chain for spin/check/fail handling - Turn off success LEDs after a bit, add extremely necessary thumbs up during song and extra beep at the end

--- apps/src/code-studio/makerlab/setupPage.js
+++ apps/src/code-studio/makerlab/setupPage.js
@@ -1,7 +1,4 @@
-import five from 'johnny-five';
-import PlaygroundIO from 'playground-io';
-import ChromeSerialport from 'chrome-serialport';
-//import BoardController from '@cdo/apps/makerlab/BoardController';
+import BoardController from '@cdo/apps/makerlab/BoardController';
 
 $(function () {
   $('.maker-setup a').attr('target', '_blank');
@@ -29,29 +26,53 @@ $(function () {
     } else {
       $('#is-chrome').append("Your Chrome version is " + getChromeVersion() + ", please upgrade to at least version 33");
     }
-    const CHROME_APP_ID = 'ncmmhcpckfejllekofcacodljhdhibkg';
-    ChromeSerialport.extensionId = CHROME_APP_ID;
 
-    spin('#app-installed');
-    try {
-     ChromeSerialport.isInstalled((error) => {
-        if (error) {
-          fail('#app-installed');
-          return;
-        }
-        check('#app-installed');
-
-        return getDevicePort().then(port => {
-          check('#board-plug');
-          spin('#board-connect');
-          connectToBoard(port).then(board => {
-            check('#board-connect');
-          }).catch(e => {fail('#board-connect', e);});
-        }).catch(e => {fail('#board-plug', e);});
-      });
-    } catch (e) {
-      fail('#app-installed', e);
-    }
+    const bc = new BoardController();
+    Promise.resolve().then(() => {
+      return Promise.resolve()
+          .then(() => spin('#app-installed'))
+          .then(bc.ensureAppInstalled.bind(bc))
+          .then(() => check('#app-installed'))
+          .catch((error) => fail('#app-installed'));
+    }).then(() => {
+      return Promise.resolve()
+          .then(() => spin('#board-plug'))
+          .then(BoardController.getDevicePort)
+          .then(() => check('#board-plug'))
+          .catch((error) => fail('#board-plug'));
+    }).then(() => {
+      return Promise.resolve()
+          .then(() => spin('#board-connect'))
+          .then(bc.ensureBoardConnected.bind(bc))
+          .then(() => check('#board-connect'))
+          .catch((error) => fail('#board-connect'));
+    }).then(() => {
+      return Promise.resolve()
+          .then(() => spin('#board-components'))
+          .then(bc.connectWithComponents.bind(bc))
+          .then(() => thumb('#board-components'))
+          .then(() => {
+            bc.prewiredComponents.buzzer.play({
+              song: [
+                ["G3", 100], ["C4", 100], ["E4", 100], ["G4", 50],
+                [null, 150], ["E4", 75], ["G4", 400]
+              ], tempo: 45000
+            });
+            bc.prewiredComponents.colorLeds.forEach(l => l.color('green'));
+          })
+          .then(() => new Promise((resolve) => {
+            setTimeout(resolve, 1600)
+          }))
+          .then(() => {
+            bc.prewiredComponents.colorLeds.forEach(l => l.off());
+            bc.prewiredComponents.buzzer.play({
+              song: [["C4", 100]],
+              tempo: 45000
+            });
+          })
+          .then(() => check('#board-components'))
+          .catch((error) => fail('#board-components'));
+    });
   } else {
     fail('#is-chrome');
   }
@@ -62,70 +83,8 @@ $(function () {
 
 });
 
-function deviceOnPortAppearsUsable(port) {
-  const comNameRegex = /usb|acm|^com/i;
-  return comNameRegex.test(port.comName);
-}
-
-function getDevicePort() {
-  return new Promise(function (resolve, reject) {
-   ChromeSerialport.list(function (error, list) {
-      if (error) {
-        reject(error);
-        return;
-      }
-
-      const prewiredBoards = list.filter(function (port) {
-        return deviceOnPortAppearsUsable(port);
-      });
-
-      if (prewiredBoards.length > 0) {
-        resolve(prewiredBoards[0].comName);
-      } else {
-        reject('Could not get device port.');
-      }
-    });
-  }.bind(this));
-}
-
-function connectToBoard(portId) {
-  return new Promise(function (resolve, reject) {
-    const serialPort = new ChromeSerialport.SerialPort(portId, {
-      bitrate: 57600
-    }, true);
-    const io = new PlaygroundIO({port: serialPort});
-    const board = new five.Board({io: io, repl: false});
-    board.once('ready', function () {
-      // Play "Charge!"
-      new five.Piezo({
-        pin: '5',
-        controller: PlaygroundIO.Piezo
-      }).play({
-        song: [
-          ["G3", 100],
-          ["C4", 100],
-          ["E4", 100],
-          ["G4", 50],
-          [null, 150],
-          ["E4", 75],
-          ["G4", 400],
-          [null, 50]
-        ],
-        tempo: 45000
-      });
-      const colorLeds = _.range(10).map(index => new five.Led.RGB({
-        controller: PlaygroundIO.Pixel,
-        pin: index
-      }));
-      colorLeds.forEach(l => l.color('green'));
-      resolve(board);
-    });
-    board.once('error', reject);
-  }.bind(this));
-}
-
 function fail(selector, e) {
-  $(selector).addClass('incomplete').removeClass('complete');
+  $(selector).addClass('incomplete').removeClass('complete waiting');
   $(selector + ' i').removeClass('fa-spinner fa-spin').addClass('fa fa-times-circle');
   if (e) {
     console.error(e);
@@ -133,11 +92,16 @@ function fail(selector, e) {
 }
 
 function spin(selector) {
-  $(selector).removeClass('incomplete').addClass('complete');
-  $(selector + ' i').removeClass('fa-times-circle').addClass('fa-spinner fa-spin');
+  $(selector).removeClass('waiting').addClass('complete');
+  $(selector + ' i').removeClass('fa-times-circle fa-clock-o').addClass('fa-spinner fa-spin');
 }
 
 function check(selector) {
-  $(selector).removeClass('incomplete').addClass('complete');
-  $(selector + ' i').removeClass('fa-times-circle fa-spinner fa-spin').addClass('fa-check-circle');
+  $(selector).removeClass('waiting').addClass('complete');
+  $(selector + ' i').removeClass('fa-times-circle fa-clock-o fa-spinner fa-thumbs-o-up fa-spin').addClass('fa-check-circle');
+}
+
+function thumb(selector) {
+  $(selector).removeClass('waiting').addClass('complete');
+  $(selector + ' i').removeClass('fa-times-circle fa-clock-o fa-spinner fa-spin').addClass('fa-thumbs-o-up');
 }
