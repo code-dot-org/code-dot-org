538463fb70838d707642644a911843585ea18a47
Paul Carduner <paul@carduner.net>
2016-07-25 13:55:34 -0700

    Fix storybook webpack config
    When we switched to automagically importing test locales, we
    inadvertantly broke the storybook. This fixes it by just having a
    separate webpack config for storybook and karma.
    
    I also went ahead and upgraded storybook, which fixes a couple bugs we
    were encountering.

--- apps/.storybook/webpack.config.js
+++ apps/.storybook/webpack.config.js
@@ -1,6 +1,6 @@
 var ProgressBarPlugin = require('progress-bar-webpack-plugin');
 var chalk = require('chalk');
-var config = require('../webpack').karmaConfig;
+var config = require('../webpack').storybookConfig;
 config.plugins.push(
   new ProgressBarPlugin({
     format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds) :msg' + ' ',
