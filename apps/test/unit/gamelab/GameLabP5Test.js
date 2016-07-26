4a1b85b5568a85001ea9172657ab94f093eedbba
Caley Brock <caleybrock@Caleys-MacBook-Pro.local>
2016-07-26 14:30:27 -0700

    Add real tests for mouseIsOver in gamelab

--- apps/test/unit/gamelab/GameLabP5Test.js
+++ apps/test/unit/gamelab/GameLabP5Test.js
@@ -7,23 +7,37 @@ import {expect} from '../../util/configuredChai';
 describe('GameLabP5', function () {
   describe('mouseIsOver method', function () {
     var gameLabP5;
+    var sprite;
 
     beforeEach(function () {
       gameLabP5 = new GameLabP5();
       gameLabP5.init({onExecutionStarting: sinon.spy(), onPreload: sinon.spy(), onSetup: sinon.spy(), onDraw: sinon.spy()});
       gameLabP5.startExecution();
+
+      sprite = gameLabP5.p5.createSprite(0, 0);
+      sprite.setCollider("circle", 0, 0, 100);
     });
 
     afterEach(function () {
       gameLabP5.resetExecution();
     });
 
-    it('does not throw when checking a sprite with a circle collider', function () {
-      expect(function () {
-        var sprite = gameLabP5.p5.createSprite();
-        sprite.setCollider("circle");
-        gameLabP5.p5.mouseIsOver(sprite);
-      }).not.to.throw();
+    it('returns true when the mouse is within the circle collider', function () {
+      gameLabP5.p5.mouseX = 0;
+      gameLabP5.p5.mouseY = 0;
+      expect(gameLabP5.p5.mouseIsOver(sprite)).to.equal(true);
+      gameLabP5.p5.mouseX = 99;
+      gameLabP5.p5.mouseY = 0;
+      expect(gameLabP5.p5.mouseIsOver(sprite)).to.equal(true);
+    });
+
+    it('returns false when the mouse is outside the circle collider', function () {
+      gameLabP5.p5.mouseX = 200;
+      gameLabP5.p5.mouseY = 200;
+      expect(gameLabP5.p5.mouseIsOver(sprite)).to.equal(false);
+      gameLabP5.p5.mouseX = 100;
+      gameLabP5.p5.mouseY = 0;
+      expect(gameLabP5.p5.mouseIsOver(sprite)).to.equal(false);
     });
 
   });
