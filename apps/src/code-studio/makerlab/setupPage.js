f791974505a29e1787df39a5474d7575360b8cc0
Brian Jordan <bcjordan@gmail.com>
2016-07-27 09:57:34 -0700

    Move Chrome warning into prop. Remove stepIds

--- apps/src/code-studio/makerlab/setupPage.js
+++ apps/src/code-studio/makerlab/setupPage.js
@@ -1,7 +1,6 @@
 import BoardController from '@cdo/apps/makerlab/BoardController';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import classNames from 'classnames';
 
 const HIDDEN = 0;
 const WAITING = 1;
@@ -28,32 +27,28 @@ const BoardSetupStatus = React.createClass({
         <div className="setup-status" style={{'fontSize': '26px'}}>
           <SetupStep
             stepStatus={this.state['status-is-chrome']}
-            stepId="is-chrome"
             stepName="Chrome version 33+"
-          />
+          >
+            {isChrome() && " - Your Chrome version is " + getChromeVersion() + ", please upgrade to at least version 33"}
+          </SetupStep>
           <SetupStep
             stepStatus={this.state['status-app-installed']}
-            stepId="app-installed"
             stepName="Chrome App installed"
           />
           <SetupStep
             stepStatus={this.state['status-windows-drivers']}
-            stepId="windows-drivers"
             stepName="Windows drivers installed"
           />
           <SetupStep
             stepStatus={this.state['status-board-plug']}
-            stepId="board-plug"
             stepName="Board plugged in"
           />
           <SetupStep
             stepStatus={this.state['status-board-connect']}
-            stepId="board-connect"
             stepName="Board connectable"
           />
           <SetupStep
             stepStatus={this.state['status-board-components']}
-            stepId="board-components"
             stepName="Board components usable"
           />
         </div>
@@ -81,79 +76,61 @@ const BoardSetupStatus = React.createClass({
   },
 
   componentDidMount() {
-    const isChrome = !!window.chrome;
-    const gtChrome33 = isChrome && getChromeVersion() >= 33;
-
-    function getChromeVersion() {
-      const raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
-      return raw ? parseInt(raw[2], 10) : false;
-    }
-
-    function isWindows() {
-      return navigator.platform.indexOf('Win') > -1;
-    }
-
-    if (isChrome) {
-      if (gtChrome33) {
-        this.succeed('is-chrome');
-      } else {
-        $('is-chrome').append("Your Chrome version is " + getChromeVersion() + ", please upgrade to at least version 33");
-      }
-
-      const bc = new BoardController();
-      Promise.resolve().then(() => {
-        this.spin('app-installed');
-        return bc.ensureAppInstalled()
-            .then(() => this.succeed('app-installed'))
-            .catch((error) => this.fail('app-installed'));
-      }).then(() => {
-        this.spin('board-plug');
-        return BoardController.getDevicePort()
-            .then(() => this.succeed('board-plug'))
-            .catch((error) => this.fail('board-plug'));
-      }).then(() => {
-        this.spin('board-connect');
-        return bc.ensureBoardConnected()
-            .then(() => this.succeed('board-connect'))
-            .catch((error) => this.fail('board-connect'));
-      }).then(() => {
-        this.spin('board-components');
-        return bc.connectWithComponents()
-            .then(() => this.thumb('board-components'))
-            .then(() => {
-              bc.prewiredComponents.buzzer.play({
-                song: [
-                  ["G3", 100], ["C4", 100], ["E4", 100], ["G4", 50],
-                  [null, 150], ["E4", 75], ["G4", 400]
-                ], tempo: 45000
-              });
-              bc.prewiredComponents.colorLeds.forEach(l => l.color('green'));
-            })
-            .then(() => promiseWaitFor(1600))
-            .then(() => {
-              bc.prewiredComponents.colorLeds.forEach(l => l.off());
-              bc.prewiredComponents.buzzer.play({
-                song: [["C4", 100]],
-                tempo: 45000
-              });
-            })
-            .then(() => this.succeed('board-components'))
-            .catch((error) => this.fail('board-components'));
-      });
-    } else {
+    if (!isChrome()) {
       this.fail('is-chrome');
+      return;
     }
 
     if (!isWindows()) {
       this.hide('windows-drivers');
     }
+
+    if (gtChrome33()) {
+      this.succeed('is-chrome');
+    } else {
+      this.fail('is-chrome');
+    }
+
+    const bc = new BoardController();
+    Promise.resolve().then(() => {
+      this.spin('app-installed');
+      return bc.ensureAppInstalled()
+          .then(() => this.succeed('app-installed'))
+          .catch((error) => this.fail('app-installed'));
+    }).then(() => {
+      this.spin('board-plug');
+      return BoardController.getDevicePort()
+          .then(() => this.succeed('board-plug'))
+          .catch((error) => this.fail('board-plug'));
+    }).then(() => {
+      this.spin('board-connect');
+      return bc.ensureBoardConnected()
+          .then(() => this.succeed('board-connect'))
+          .catch((error) => this.fail('board-connect'));
+    }).then(() => {
+      this.spin('board-components');
+      return bc.connectWithComponents()
+          .then(() => this.thumb('board-components'))
+          .then(() => {
+            bc.prewiredComponents.buzzer.play({
+              song: [
+                ["G3", 100], ["C4", 100], ["E4", 100], ["G4", 50],
+                [null, 150], ["E4", 75], ["G4", 400]
+              ], tempo: 45000
+            });
+            bc.prewiredComponents.colorLeds.forEach(l => l.color('green'));
+          })
+          .then(() => promiseWaitFor(1600))
+          .then(() => bc.prewiredComponents.colorLeds.forEach(l => l.off()))
+          .then(() => this.succeed('board-components'))
+          .catch((error) => this.fail('board-components'));
+    });
   }
 });
 
 const SetupStep = React.createClass({
   propTypes: {
     stepName: React.PropTypes.string.isRequired,
-    stepId: React.PropTypes.string.isRequired,
     stepStatus: React.PropTypes.oneOf(STEP_STATUSES).isRequired
   },
 
@@ -194,10 +171,10 @@ const SetupStep = React.createClass({
       return null;
     }
     return (
-        <div id={this.props.stepId}
-             style={Object.assign({margin: '15px 0'}, this.styleFor(this.props.stepStatus))}>
+        <div style={Object.assign({margin: '15px 0'}, this.styleFor(this.props.stepStatus))}>
           <i style={{'marginRight': '6px'}} className={this.iconFor(this.props.stepStatus)}/>
           <span>{this.props.stepName}</span>
+          {this.props.stepStatus === FAILED && this.props.children}
         </div>
     );
   }
@@ -216,4 +193,19 @@ function promiseWaitFor(ms) {
   });
 }
 
+function isChrome() {
+  return !!window.chrome;
+}
 
+function getChromeVersion() {
+  const raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+  return raw ? parseInt(raw[2], 10) : false;
+}
+
+function gtChrome33() {
+  return getChromeVersion() >= 33;
+}
+
+function isWindows() {
+  return navigator.platform.indexOf('Win') > -1;
+}
