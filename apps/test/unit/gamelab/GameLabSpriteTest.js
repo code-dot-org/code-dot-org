9fba967c8862fa9e85868a25fa135ec5f387a81c
Caley Brock <caleybrock@Caleys-MacBook-Pro.local>
2016-07-28 16:12:16 -0700

    Clean up test syntax from code review

--- apps/test/unit/gamelab/GameLabSpriteTest.js
+++ apps/test/unit/gamelab/GameLabSpriteTest.js
@@ -1,5 +1,5 @@
-var P5 = require("script!@cdo/apps/../lib/p5play/p5");
-var P5Play = require("script!@cdo/apps/../lib/p5play/p5.play");
+import "script!@cdo/apps/../lib/p5play/p5";
+import "script!@cdo/apps/../lib/p5play/p5.play";
 import GameLabP5 from '@cdo/apps/gamelab/GameLabP5';
 import JSInterpreter from '@cdo/apps/JSInterpreter';
 import {injectJSInterpreter} from '@cdo/apps/gamelab/GameLabSprite';
@@ -8,6 +8,7 @@ import {expect} from '../../util/configuredChai';
 
 describe('GameLabSprite', function () {
   var gameLabP5;
+  let createSprite;
 
   beforeEach(function () {
     gameLabP5 = new GameLabP5();
@@ -16,6 +17,7 @@ describe('GameLabSprite', function () {
 
     var interpreter = {getCurrentState: function () {return {};}};
     injectJSInterpreter(interpreter);
+    createSprite = gameLabP5.p5.createSprite.bind(gameLabP5.p5);
   });
 
   afterEach(function () {
@@ -24,33 +26,30 @@ describe('GameLabSprite', function () {
 
   describe('isTouching', function () {
     it('returns false if the collider and colliding sprite dont overlap', function () {
-      var sprite1 = gameLabP5.p5.createSprite(0, 0, 100, 100);
-      var sprite2 = gameLabP5.p5.createSprite(200, 200, 100, 100);
+      var sprite1 = createSprite(0, 0, 100, 100);
+      var sprite2 = createSprite(200, 200, 100, 100);
       var isTouching1to2 = sprite1.isTouching(sprite2);
       var isTouching2to1 = sprite2.isTouching(sprite1);
-      expect(isTouching1to2).to.equal(false);
-      expect(isTouching1to2).to.equal(isTouching2to1);
+      expect(isTouching1to2).to.equal(false).and.to.equal(isTouching2to1);
     });
 
     it('returns true if the collider and colliding sprite overlap', function () {
-      var sprite3 = gameLabP5.p5.createSprite(150, 150, 100, 100);
-      var sprite4 = gameLabP5.p5.createSprite(200, 200, 100, 100);
+      var sprite3 = createSprite(150, 150, 100, 100);
+      var sprite4 = createSprite(200, 200, 100, 100);
       var isTouching3to4 = sprite3.isTouching(sprite4);
       var isTouching4to3 = sprite4.isTouching(sprite3);
-      expect(isTouching3to4).to.equal(true);
-      expect(isTouching4to3).to.equal(isTouching3to4);
+      expect(isTouching3to4).to.equal(true).and.to.equal(isTouching3to4);
 
-      var sprite5 = gameLabP5.p5.createSprite(101, 101, 100, 100);
-      var sprite6 = gameLabP5.p5.createSprite(200, 200, 100, 100);
+      var sprite5 = createSprite(101, 101, 100, 100);
+      var sprite6 = createSprite(200, 200, 100, 100);
       var isTouching5to6 = sprite5.isTouching(sprite6);
       var isTouching6to5 = sprite6.isTouching(sprite5);
-      expect(isTouching5to6).to.equal(true);
-      expect(isTouching6to5).to.equal(isTouching5to6);
+      expect(isTouching5to6).to.equal(true).and.to.equal(isTouching5to6);
     });
 
     it('does not affect the location or velocity of the sprite', function () {
-      var sprite1 = gameLabP5.p5.createSprite(170, 170, 100, 100);
-      var sprite2 = gameLabP5.p5.createSprite(200, 200, 100, 100);
+      var sprite1 = createSprite(170, 170, 100, 100);
+      var sprite2 = createSprite(200, 200, 100, 100);
       var isTouching1to2 = sprite1.isTouching(sprite2);
       expect(isTouching1to2).to.equal(true);
       expect(sprite1.x).to.equal(170);
