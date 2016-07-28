c7129a04385cdd76bf7935e516b20608085e4bff
Brad Buchanan <brad@code.org>
2016-07-27 17:05:26 -0700

    Better minimum test with enzyme

--- apps/test/unit/netsim/NetSimLogBrowser.js
+++ apps/test/unit/netsim/NetSimLogBrowser.js
@@ -1,20 +1,24 @@
 /** @file NetSimLogBrowser tests */
 import React from 'react';
-import ReactTestUtils from 'react-addons-test-utils';
+import {shallow} from 'enzyme';
+import {spy} from 'sinon';
 import {expect} from '../../util/configuredChai';
+import {throwOnConsoleErrors} from '../../util/testUtils';
 import NetSimLogBrowser from '@cdo/apps/netsim/NetSimLogBrowser';
 
 describe('NetSimLogBrowser', function () {
+  throwOnConsoleErrors();
+
   it('renders warning-free with the least possible parameters', function () {
-    let shallowResult = shallowRender(
-        <NetSimLogBrowser/>
-    );
-    expect(shallowResult).not.to.be.null;
+    let shallowResult = shallow(
+      <NetSimLogBrowser
+        i18n={{}}
+        setRouterLogMode={spy()}
+        currentTrafficFilter="none"
+        setTrafficFilter={spy()}
+        headerFields={[]}
+        logRows={[]}
+      />);
+    expect(shallowResult).not.to.be.empty;
   });
 });
-
-function shallowRender(component) {
-  let renderer = ReactTestUtils.createRenderer();
-  renderer.render(component);
-  return renderer.getRenderOutput();
-}
