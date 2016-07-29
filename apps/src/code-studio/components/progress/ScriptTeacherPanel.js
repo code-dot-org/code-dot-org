5a6834d5eca4b1a455e726f00884fe4377741408
Brent Van Minnen <bjvanminnen@gmail.com>
2016-07-29 11:52:19 -0700

    TeacherPanel for script in react

---
+++ apps/src/code-studio/components/progress/ScriptTeacherPanel.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import TeacherPanel from '../TeacherPanel';
+import ToggleGroup from '@cdo/apps/templates/ToggleGroup';
+import FontAwesome from '@cdo/apps/templates/FontAwesome';
+import { makeEnum } from '@cdo/apps/utils';
+
+const styles = {
+  viewAs: {
+    fontSize: 16,
+    margin: 10
+  },
+  toggleGroup: {
+    margin: 10
+  },
+  select: {
+    margin: 10,
+    width: 180
+  },
+  text: {
+    margin: 10
+  },
+  exclamation: {
+    color: 'red'
+  },
+  dontForget: {
+    display: 'inline',
+    marginLeft: 10,
+    fontSize: 16,
+    fontFamily: '"Gotham 7r", sans-serif'
+  }
+};
+
+const ViewType = makeEnum('Student', 'Teacher');
+
+const ScriptTeacherPanel = React.createClass({
+  getInitialState() {
+    return {
+      // TODO - i think eventually this moves to redux
+      viewAs: ViewType.Teacher
+    };
+  },
+
+  handleChange(viewAs) {
+    this.setState({ viewAs });
+  },
+
+  render() {
+    // TODO - i18n
+    // TODO - don't forget section is conditional on having unlocked stages
+    return (
+      <TeacherPanel>
+        <h3>Teacher Panel</h3>
+        <div className="content">
+          <div className="non-scrollable-wrapper">
+            <div style={styles.viewAs}>
+              View page as:
+            </div>
+            <div style={styles.toggleGroup}>
+              <ToggleGroup selected={this.state.viewAs} onChange={this.handleChange}>
+                <button value={ViewType.Student}>Student</button>
+                <button value={ViewType.Teacher}>Teacher</button>
+              </ToggleGroup>
+            </div>
+          </div>
+          {this.state.viewAs === ViewType.Teacher &&
+            <div>
+              <select name="sections" style={styles.select}>
+                <option>Select a Section</option>
+              </select>
+              <div style={styles.text}>
+                Select a section to be able to lock and unlock assessments or
+                surveys. Click the lock settings button in the stage to the left.
+              </div>
+              <div style={styles.text}>
+                <FontAwesome icon="exclamation-triangle" style={styles.exclamation}/>
+                <div style={styles.dontForget}>Don't forget</div>
+              </div>
+              <div style={styles.text}>
+                Lock the following stages that are currently unlocked:
+              </div>
+            </div>
+          }
+        </div>
+      </TeacherPanel>
+    );
+  }
+});
+
+export default ScriptTeacherPanel;
