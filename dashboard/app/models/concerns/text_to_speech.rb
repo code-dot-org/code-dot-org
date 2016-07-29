95a4af67888235328357b72e3d4503f0816dc334
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-07-29 11:31:27 -0700

    allow for both instructions and markdown instructions to be speechified

--- dashboard/app/models/concerns/text_to_speech.rb
+++ dashboard/app/models/concerns/text_to_speech.rb
@@ -1,6 +1,8 @@
 require 'acapela'
 require 'net/http'
 require 'uri'
+require 'redcarpet'
+require 'redcarpet/render_strip'
 
 TTS_BUCKET = 'cdo-tts'
 
@@ -14,7 +16,8 @@ module TextToSpeech
     after_save :tts_update
 
     serialized_attrs %w(
-      custom_tts
+      tts_instructions_override
+      tts_markdown_instructions_override
     )
   end
 
@@ -26,23 +29,40 @@ module TextToSpeech
     }
   end
 
-  def tts_text
-    self.custom_tts || self.instructions
+  def tts_instructions_text
+    self.tts_instructions_override || self.instructions || ""
   end
 
-  def tts_should_update?
-    changed = self.property_changed?(self.custom_tts ? 'custom_tts' : 'instructions')
+  def tts_should_update_instructions?
+    changed = self.property_changed?(self.tts_instructions_override ? 'tts_instructions_override' : 'instructions')
     changed && write_to_file? && self.published
   end
 
-  def tts_audio_file
-    content_hash = Digest::MD5.hexdigest(self.tts_text)
+  def tts_markdown_instructions_text
+    self.tts_markdown_instructions_override || Redcarpet::Markdown.new(Redcarpet::Render::StripDown).render(self.markdown_instructions || "")
+  end
+
+  def tts_should_update_markdown_instructions?
+    changed = self.property_changed?(self.tts_markdown_instructions_override ? 'tts_markdown_instructions_override' : 'markdown_instructions')
+    changed && write_to_file? && self.published
+  end
+
+  def tts_instructions_audio_file
+    content_hash = Digest::MD5.hexdigest(self.tts_instructions_text)
+    "#{content_hash}/#{self.name}.mp3"
+  end
+
+  def tts_markdown_instructions_audio_file
+    content_hash = Digest::MD5.hexdigest(self.tts_markdown_instructions_text)
     "#{content_hash}/#{self.name}.mp3"
   end
 
   def tts_update
-    if self.tts_should_update?
-      TextToSpeech.tts_upload_to_s3(self.tts_text, self.tts_audio_file)
+    if self.tts_should_update_instructions?
+      TextToSpeech.tts_upload_to_s3(self.tts_instructions_text, self.tts_instructions_audio_file)
+    end
+    if self.tts_should_update_markdown_instructions?
+      TextToSpeech.tts_upload_to_s3(self.tts_markdown_instructions_text, self.tts_markdown_instructions_audio_file)
     end
   end
 end
