b45121341403f0605d2df9309fbf7a7ea43fae3c
Asher Kach <asher@code.org>
2016-08-02 10:20:04 -0500

    Adds comments to user_permission.rb.

--- dashboard/app/models/user_permission.rb
+++ dashboard/app/models/user_permission.rb
@@ -14,10 +14,24 @@
 #
 
 class UserPermission < ActiveRecord::Base
+  # Grants access to viewing all workshops in all cohorts in their district.
   DISTRICT_CONTACT = 'district_contact'
+
+  # Grants access to managing workshops and workshop attendance.
   FACILITATOR = 'facilitator'
-  HINT_ACCESS = 'hint_access'
-  LEVELBUILDER = 'levelbuilder'
-  WORKSHOP_ORGANIZER = 'workshop_organizer'
+
+  # Grants access to viewing hidden scripts.
   HIDDEN_SCRIPT_ACCESS = 'hidden_script_access'
+
+  # Grants access to managing (e.g., editting) LevelSourceHint's and
+  # FrequentUnsuccessfulLevelSource's.
+  HINT_ACCESS = 'hint_access'
+
+  # Grants access to managing (e.g., editing) levels, stages, scripts, etc.
+  # Also grants access to viewing extra links related to editing these.
+  LEVELBUILDER = 'levelbuilder'
+
+  # Grants access to managing professional development workshops and
+  # professional development workshop attendance.
+  WORKSHOP_ORGANIZER = 'workshop_organizer'
 end
