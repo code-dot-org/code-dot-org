e3c92b6d2b77eb7a6b39f6b93f97bfe6828692a2
Will Jordan <will@code.org>
2016-08-04 12:08:40 -0700

    fix style

--- dashboard/config/initializers/fast_localization.rb
+++ dashboard/config/initializers/fast_localization.rb
@@ -29,7 +29,7 @@ module UpdateCheckerExt
 
   # Pass list of updated files as a block parameter.
   def execute
-    @last_watched   = watched
+    @last_watched = watched
     @last_updated = @last_watched.reject{|path| File.mtime(path) < @last_update_at}
     @last_update_at = updated_at(@last_watched)
     @block.arity == 1 ? @block.call(@last_updated) : @block.call
