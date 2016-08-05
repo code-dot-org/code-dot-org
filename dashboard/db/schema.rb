432aca1a2f480f66113582d1db78b1c6f2c1c5fa
Asher Kach <asher@code.org>
2016-08-05 14:16:46 -0500

    Drops the user_trophies and trophies tables.

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804153118) do
+ActiveRecord::Schema.define(version: 20160805190505) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id",         limit: 4
@@ -706,15 +706,6 @@ ActiveRecord::Schema.define(version: 20160804153118) do
   add_index "teacher_prizes", ["prize_provider_id"], name: "index_teacher_prizes_on_prize_provider_id", using: :btree
   add_index "teacher_prizes", ["user_id"], name: "index_teacher_prizes_on_user_id", using: :btree
 
-  create_table "trophies", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "image_name", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "trophies", ["name"], name: "index_trophies_on_name", unique: true, using: :btree
-
   create_table "unexpected_teachers_workshops", id: false, force: :cascade do |t|
     t.integer "workshop_id",           limit: 4, null: false
     t.integer "unexpected_teacher_id", limit: 4, null: false
@@ -845,16 +836,6 @@ ActiveRecord::Schema.define(version: 20160804153118) do
 
   add_index "user_scripts", ["script_id"], name: "index_user_scripts_on_script_id", using: :btree
   add_index "user_scripts", ["user_id", "script_id"], name: "index_user_scripts_on_user_id_and_script_id", unique: true, using: :btree
-
-  create_table "user_trophies", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4, null: false
-    t.integer  "trophy_id",  limit: 4, null: false
-    t.integer  "concept_id", limit: 4
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "user_trophies", ["user_id", "trophy_id", "concept_id"], name: "index_user_trophies_on_user_id_and_trophy_id_and_concept_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                      limit: 255,   default: "",      null: false
