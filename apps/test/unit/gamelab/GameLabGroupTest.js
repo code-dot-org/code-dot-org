6a02c6d3da90174c392f0f3bce13cee14884fd11
Brad Buchanan <brad@code.org>
2016-08-08 16:47:34 -0700

    One test of group.isTouching(sprite) case
    Also provides utility for testing with a stateful mock interpreter.
    
    pair: caley

--- apps/test/unit/gamelab/GameLabGroupTest.js
+++ apps/test/unit/gamelab/GameLabGroupTest.js
@@ -1,13 +1,13 @@
 /** @file Tests for GameLabGroup, our extension of p5.play Group */
 import {spy} from 'sinon';
 import {expect} from '../../util/configuredChai';
-import createGameLabP5 from '../../util/gamelab/TestableGameLabP5';
+import {createStatefulGameLabP5} from '../../util/gamelab/TestableGameLabP5';
 
 describe('GameLabGroup', function () {
   let gameLabP5, createSprite, createGroup;
 
   beforeEach(function () {
-    gameLabP5 = createGameLabP5();
+    gameLabP5 = createStatefulGameLabP5();
     createSprite = gameLabP5.p5.createSprite.bind(gameLabP5.p5);
     createGroup = gameLabP5.p5.createGroup.bind(gameLabP5.p5);
   });
@@ -31,6 +31,24 @@ describe('GameLabGroup', function () {
       expect(sprite2.setSpeedAndDirection.calledOnce).to.be.true;
       expect(sprite1.setSpeedAndDirection.calledWith(speed, direction)).to.be.true;
       expect(sprite2.setSpeedAndDirection.calledWith(speed, direction)).to.be.true;
+    });
+  });
+
+  describe('collision methods', function () {
+    describe('isTouching', function () {
+      it('returns false if no sprite in group overlaps target sprite', function () {
+        var sprite1 = createSprite(0, 0, 100, 100);
+        var sprite2 = createSprite(400, 400, 100, 100);
+        var targetSprite = createSprite(200, 200, 100, 100);
+        var group = createGroup();
+        group.add(sprite1);
+        group.add(sprite2);
+        let result;
+        for (let i = 0; i < group.length + 1; i++) {
+          result = group.isTouching(targetSprite);
+        }
+        expect(result).to.equal(false);
+      });
     });
   });
 });
