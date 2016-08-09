069aee14cfc48a799fcc692804508077a3161ec1
Brent Van Minnen <bjvanminnen@gmail.com>
2016-08-09 13:56:50 -0700

    update schema version

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804153118) do
+ActiveRecord::Schema.define(version: 20160809170542) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id",         limit: 4
