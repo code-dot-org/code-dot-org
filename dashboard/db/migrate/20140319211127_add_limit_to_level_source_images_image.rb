b55182013ebf1f470a5fb3dc3a0a8f2daac98b9e
Asher Kach <asher@code.org>
2016-08-09 12:05:04 -0500

    Remove rubocop blacklisting of dashboard/db/ directories.

--- dashboard/db/migrate/20140319211127_add_limit_to_level_source_images_image.rb
+++ dashboard/db/migrate/20140319211127_add_limit_to_level_source_images_image.rb
@@ -1,5 +1,5 @@
 class AddLimitToLevelSourceImagesImage < ActiveRecord::Migration
   def change
-    change_column :level_source_images, :image, :binary, limit: 16777215
+    change_column :level_source_images, :image, :binary, limit: 16_777_215
   end
 end
