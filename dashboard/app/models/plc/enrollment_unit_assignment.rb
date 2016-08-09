2c1e5255ad2491ae96557f8be3769353b6f78867
Mehal Shah <mehal@code.org>
2016-08-09 14:37:15 -0700

    The overall course view should have a section for peer review

--- dashboard/app/models/plc/enrollment_unit_assignment.rb
+++ dashboard/app/models/plc/enrollment_unit_assignment.rb
@@ -95,6 +95,14 @@ class Plc::EnrollmentUnitAssignment < ActiveRecord::Base
       end
     end
 
+    # If there are peer reviews, summarize that progress as well
+    if plc_course_unit.script.peer_reviews_to_complete.try(:>, 0)
+      summary << {
+          category: 'peer_review',
+          status: PeerReview.get_review_completion_status(user, plc_course_unit.script)
+      }
+    end
+
     summary
   end
 
