6ced5e120cc2653478acc058230aa1a5fa663db1
Brad Buchanan <brad@code.org>
2016-08-10 15:55:54 -0700

    Hyperlinks

--- apps/src/publicKeyCryptography/PublicKeyCryptographyWidget.jsx
+++ apps/src/publicKeyCryptography/PublicKeyCryptographyWidget.jsx
@@ -3,7 +3,55 @@ import React from 'react';
 
 const PublicKeyCryptographyWidget = React.createClass({
   render() {
-    return <div>This space intentionally left blank</div>;
+    return (
+      <div>
+        <HyperlinksList>
+          <a href="https://docs.google.com/document/d/1rcXn-3B0JWY3ifeZkhVB3OFHNYClsY80LCIVIOOpExs/edit">Instructions</a>
+          <a href="https://docs.google.com/document/d/1d6mEbpykWsFKP2PAC5cj7ak_ouoPrwhMcFpYf7xd_Yw/edit">How the math works</a>
+        </HyperlinksList>
+        <Characters>
+          <Alice/>
+          <Eve/>
+          <Bob/>
+        </Characters>
+        <ModuloClock/>
+      </div>);
   }
 });
 export default PublicKeyCryptographyWidget;
+
+function HyperlinksList(props) {
+  let children = React.Children.toArray(props.children);
+  for (let i = children.length - 1; i > 0; i--) {
+    children.splice(i, 0, ' | ');
+  }
+  return (
+    <div>
+      {children}
+    </div>);
+}
+HyperlinksList.propTypes = {
+  children: React.PropTypes.oneOfType([
+    React.PropTypes.element,
+    React.PropTypes.arrayOf(React.PropTypes.element)])
+};
+
+function Characters() {
+  return <div/>;
+}
+
+function Alice() {
+  return <div/>;
+}
+
+function Eve() {
+  return <div/>;
+}
+
+function Bob() {
+  return <div/>;
+}
+
+function ModuloClock() {
+  return <div/>;
+}
