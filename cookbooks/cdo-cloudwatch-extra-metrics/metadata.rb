d82212b49d82294b44a665f7db0008342a8baf4b
Will Jordan <will@code.org>
2016-08-09 17:10:21 -0700

    refactor cdo-cloudwatch-extra-metrics - use ark instead of manual fetch/unpack - use IAM instance profile instead of credentials file - add test-kitchen EC2 integration tests

--- cookbooks/cdo-cloudwatch-extra-metrics/metadata.rb
+++ cookbooks/cdo-cloudwatch-extra-metrics/metadata.rb
@@ -4,7 +4,6 @@ maintainer_email 'elijah@code.org'
 license          'All rights reserved'
 description      'Installs/Configures cdo-cloudwatch-extra-metrics'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
-depends 'apt'
-depends 'build-essential'
+depends 'ark'
--- cookbooks/cdo-cloudwatch-extra-metrics/recipes/default.rb
+++ cookbooks/cdo-cloudwatch-extra-metrics/recipes/default.rb
@@ -5,25 +5,6 @@
 
 apt_package ['unzip', 'libwww-perl', 'libdatetime-perl']
 
-remote_file "#{Chef::Config[:file_cache_path]}/CloudWatchMonitoringScripts-1.2.1.zip" do
-  source "http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip"
-  user node[:current_user]
-  group node[:current_user]
-end
-
-execute "unzip #{Chef::Config[:file_cache_path]}/CloudWatchMonitoringScripts-1.2.1.zip" do
-  command "unzip #{Chef::Config[:file_cache_path]}/CloudWatchMonitoringScripts-1.2.1.zip"
-  user node[:current_user]
-  group node[:current_user]
-  cwd "/home/#{node[:current_user]}"
-  creates "/home/#{node[:current_user]}/aws-scripts-mon"
-end
-
-aws_config = node['cdo-awscli']
-template "/home/#{node[:current_user]}/aws-scripts-mon/awscreds.template" do
-  not_if { aws_config.nil? }
-  source 'awscreds.template.erb'
-  variables(config: aws_config)
-  user node[:current_user]
-  group node[:current_user]
+ark 'aws-scripts-mon' do
+  url 'http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip'
 end
