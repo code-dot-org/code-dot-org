c2ea8c13bb5bde5250518c444dbf23fddd49dbc7
Brad Buchanan <brad@code.org>
2016-08-10 15:29:17 -0700

    Add a root widget component

---
+++ apps/src/publicKeyCryptography/PublicKeyCryptographyWidget.jsx
@@ -0,0 +1,9 @@
+/** @file Root component for Public Key Cryptography widget */
+import React from 'react';
+
+const PublicKeyCryptographyWidget = React.createClass({
+  render() {
+    return <div>This space intentionally left blank</div>;
+  }
+});
+export default PublicKeyCryptographyWidget;
