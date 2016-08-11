4ad83e2ac882b494bf1fb2f7f642abd7c1f8d34f
Will Jordan <will@code.org>
2016-08-10 17:57:11 -0700

    Update devise and devise_invitable

--- dashboard/app/controllers/registrations_controller.rb
+++ dashboard/app/controllers/registrations_controller.rb
@@ -24,7 +24,7 @@ class RegistrationsController < Devise::RegistrationsController
       if successfully_updated
         set_locale_cookie(@user.locale)
         # Sign in the user bypassing validation in case his password changed
-        sign_in @user, :bypass => true
+        bypass_sign_in @user
 
         format.html do
           set_flash_message :notice, @user.pending_reconfirmation? ? :update_needs_confirmation : :updated
