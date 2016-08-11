882f06f83843ef2b278b073c06a668cf9d5b03f5
Brad Buchanan <brad@code.org>
2016-08-10 18:03:24 -0700

    Linked public modulus dropdowns

--- apps/src/publicKeyCryptography/Characters.jsx
+++ apps/src/publicKeyCryptography/Characters.jsx
@@ -3,84 +3,124 @@ import React from 'react';
 import CollapsiblePanel from './CollapsiblePanel';
 import NumberedSteps from './NumberedSteps';
 
-export function Alice() {
-  return (
-    <CollapsiblePanel title="Alice">
-      <NumberedSteps>
-        <div>
-          Enter public modulus
-        </div>
-        <div>
-          Set a private key
-          <br/>Your computed public key is ??
-        </div>
-        <div>
-          Enter Bob's public number:
-        </div>
-        <div>
-          Calculate Bob's secret number.
-          <div>
-            (?? x ?) MOD ?? <button>Go</button>
-          </div>
-          <div>
-            Bob's secret number is ??!
-          </div>
-        </div>
-      </NumberedSteps>
-    </CollapsiblePanel>);
-}
+export const Alice = React.createClass({
+  propTypes: {
+    publicModulus: React.PropTypes.number,
+    onChangePublicModulus: React.PropTypes.func.isRequired
+  },
 
-export function Eve() {
-  return (
-    <CollapsiblePanel title="Eve">
-      <NumberedSteps>
-        <div>
-          Set a public modulus:
-        </div>
-        <div>
-          Enter Alice's public key
-        </div>
-        <div>
-          Crack Alice's private key:
+  render() {
+    const {publicModulus, onChangePublicModulus} = this.props;
+    return (
+      <CollapsiblePanel title="Alice">
+        <NumberedSteps>
           <div>
-            (?? x ??)MOD ?? = 1 (??)
+            Enter public modulus: <PublicModulusDropdown value={publicModulus} onChange={onChangePublicModulus}/>
           </div>
-        </div>
-        <div>
-          Enter Bob's public number: ??
-        </div>
-        <div>
-          Crack Bob's secret number:
           <div>
-            (?? x ??)MOD ?? = ?? (??)
+            Set a private key
+            <br/>Your computed public key is ??
           </div>
-        </div>
-      </NumberedSteps>
-    </CollapsiblePanel>);
-}
+          <div>
+            Enter Bob's public number:
+          </div>
+          <div>
+            Calculate Bob's secret number.
+            <div>
+              (?? x ?) MOD ??
+              <button>Go</button>
+            </div>
+            <div>
+              Bob's secret number is ??!
+            </div>
+          </div>
+        </NumberedSteps>
+      </CollapsiblePanel>);
+  }
+});
 
-export function Bob() {
+export const Eve = React.createClass({
+  propTypes: {
+    publicModulus: React.PropTypes.number,
+    onChangePublicModulus: React.PropTypes.func.isRequired
+  },
+
+  render() {
+    const {publicModulus, onChangePublicModulus} = this.props;
+    return (
+      <CollapsiblePanel title="Eve">
+        <NumberedSteps>
+          <div>
+            Set a public modulus: <PublicModulusDropdown value={publicModulus} onChange={onChangePublicModulus}/>
+          </div>
+          <div>
+            Enter Alice's public key
+          </div>
+          <div>
+            Crack Alice's private key:
+            <div>
+              (?? x ??)MOD ?? = 1 (??)
+            </div>
+          </div>
+          <div>
+            Enter Bob's public number: ??
+          </div>
+          <div>
+            Crack Bob's secret number:
+            <div>
+              (?? x ??)MOD ?? = ?? (??)
+            </div>
+          </div>
+        </NumberedSteps>
+      </CollapsiblePanel>);
+  }
+});
+
+export const Bob = React.createClass({
+  propTypes: {
+    publicModulus: React.PropTypes.number,
+    onChangePublicModulus: React.PropTypes.func.isRequired
+  },
+
+  render() {
+    const {publicModulus, onChangePublicModulus} = this.props;
+    return (
+      <CollapsiblePanel title="Bob">
+        <NumberedSteps>
+          <div>
+            Enter public modulus: <PublicModulusDropdown value={publicModulus} onChange={onChangePublicModulus}/>
+          </div>
+          <div>
+            Enter Alice's public key:
+          </div>
+          <div>
+            Pick your secret number:
+          </div>
+          <div>
+            Calculate your public number:
+            <div>
+              (?? x ??)MOD ?? <button>Go</button>
+            </div>
+            <div>
+              Your computed public number is ??
+            </div>
+          </div>
+        </NumberedSteps>
+      </CollapsiblePanel>);
+  }
+});
+
+function PublicModulusDropdown(props) {
   return (
-    <CollapsiblePanel title="Bob">
-      <NumberedSteps>
-        <div>
-          Enter public modulus:
-        </div>
-        <div>
-          Enter Alice's public key:
-        </div>
-        <div>
-          Pick your secret number:
-        </div>
-        <div>
-          Calculate your public number:
-          <div>
-            (?? x ??)MOD ?? <button>Go</button>
-          </div>
-          <div>
-            Your computed public number is ??
-          </div>
-        </div>
-      </NumberedSteps>
-    </CollapsiblePanel>);
+    <select value={props.value} onChange={props.onChange}>
+      <option value=""></option>
+      <option value="3">3</option>
+      <option value="5">5</option>
+      <option value="7">7</option>
+    </select>
+  );
 }
+PublicModulusDropdown.propTypes = {
+  value: React.PropTypes.number,
+  onChange: React.PropTypes.func.isRequired
+};
