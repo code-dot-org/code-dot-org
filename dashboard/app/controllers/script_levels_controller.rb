ebcc773fa6f391d04d77ce31e57cba2f14d74bc3
Brent Van Minnen <bjvanminnen@gmail.com>
2016-08-12 12:29:15 -0700

    lockable level should still have readonly view options

--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -165,8 +165,7 @@ class ScriptLevelsController < ApplicationController
           submitted: true,
           unsubmit_url: url_for(user_level)
         )
-        # if lockable and submitted, we don't want to be readonly since we end up just rendering a different view
-        readonly_view_options unless @script_level.stage.lockable?
+        readonly_view_options
       end
       readonly_view_options if user_level && user_level.view_answers?
     end
