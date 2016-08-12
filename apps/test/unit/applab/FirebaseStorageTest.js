5e916ab47276c3a04ca3ca477a435f42e2e809a1
David Bailey <davidsbailey@users.noreply.github.com>
2016-08-12 13:20:03 -0700

    fix dataCallbacksTests

--- apps/test/unit/applab/FirebaseStorageTest.js
+++ apps/test/unit/applab/FirebaseStorageTest.js
@@ -4,11 +4,11 @@ import { getDatabase, getFirebase } from '@cdo/apps/applab/firebaseUtils';
 
 describe('FirebaseStorage', () => {
   beforeEach(() => {
-    window.Applab = {
+    window.Applab = Object.assign({}, window.Applab, {
       channelId: "test-firebase-channel-id",
       firebaseName: 'test-firebase-name',
       firebaseAuthToken: 'test-firebase-auth-token',
-    };
+    });
   });
 
   describe('populateTable', () => {
