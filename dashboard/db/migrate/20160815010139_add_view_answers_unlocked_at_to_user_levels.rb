ff541b764dfa795064d9e73375b00eaa730341b9
Brent Van Minnen <bjvanminnen@gmail.com>
2016-08-15 14:10:12 -0700

    better column name

--- dashboard/db/migrate/20160815010139_add_view_answers_unlocked_at_to_user_levels.rb
+++ dashboard/db/migrate/20160815010139_add_view_answers_unlocked_at_to_user_levels.rb
@@ -1,6 +1,6 @@
 class AddViewAnswersUnlockedAtToUserLevels < ActiveRecord::Migration
   def change
-    add_column :user_levels, :view_answers, :boolean
+    add_column :user_levels, :readonly_answers, :boolean
     add_column :user_levels, :unlocked_at, :datetime
   end
 end
