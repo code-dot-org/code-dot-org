c02809407212da18e96f6e8c45bef79d50974690
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-08-17 16:53:35 -0700

    add harvester-specific errors

--- apps/src/maze/harvester.js
+++ apps/src/maze/harvester.js
@@ -1,7 +1,9 @@
 import Gatherer from './gatherer';
-import HarvesterDrawer from './harvesterDrawer';
 import HarvesterCell from './harvesterCell';
-import { BeeTerminationValue as TerminationValue } from '../constants.js';
+import HarvesterDrawer from './harvesterDrawer';
+import mazeMsg from './locale';
+import { HarvesterTerminationValue } from '../constants.js';
+import { TestResults } from '../constants.js';
 
 export default class Harvester extends Gatherer {
 
@@ -87,12 +89,12 @@ export default class Harvester extends Gatherer {
     const cell = this.getCell(row, col);
 
     if (cell.featureType() !== crop) {
-      this.maze_.executionInfo.terminateWithValue(TerminationValue.NOT_AT_FLOWER);
+      this.maze_.executionInfo.terminateWithValue(HarvesterTerminationValue.WRONG_CROP);
       return;
     }
 
     if (cell.getCurrentValue() === 0) {
-      this.maze_.executionInfo.terminateWithValue(TerminationValue.FLOWER_EMPTY);
+      this.maze_.executionInfo.terminateWithValue(HarvesterTerminationValue.EMPTY_CROP);
       return;
     }
 
@@ -131,5 +133,68 @@ export default class Harvester extends Gatherer {
     this.gotHarvestAt(row, col);
 
     this.maze_.gridItemDrawer.updateItemImage(row, col, true);
+  }
+
+  /**
+   * Called after user's code has finished executing. Gives us a chance to
+   * terminate with app-specific values, such as unchecked cloud/purple flowers.
+   */
+  onExecutionFinish() {
+    const executionInfo = this.maze_.executionInfo;
+    if (executionInfo.isTerminated()) {
+      return;
+    }
+    if (this.finished()) {
+      return;
+    }
+
+    // we didn't finish. look to see if we need to give an app specific error
+    if (!this.collectedEverything()) {
+      executionInfo.terminateWithValue(HarvesterTerminationValue.DID_NOT_COLLECT_EVERYTHING);
+    }
+  }
+
+  /**
+   * Get the test results based on the termination value.  If there is
+   * no app-specific failure, this returns StudioApp.getTestResults().
+   * @override
+   */
+  getTestResults(terminationValue) {
+    switch (terminationValue) {
+      case HarvesterTerminationValue.WRONG_CROP:
+      case HarvesterTerminationValue.EMPTY_CROP:
+        return TestResults.APP_SPECIFIC_FAIL;
+
+      case HarvesterTerminationValue.DID_NOT_COLLECT_EVERYTHING:
+        var testResults = this.studioApp_.getTestResults(true);
+        // If we have a non-app specific failure, we want that to take precedence.
+        // Values over TOO_MANY_BLOCKS_FAIL are not true failures, but indicate
+        // a suboptimal solution, so in those cases we want to return our
+        // app specific fail
+        if (testResults >= TestResults.TOO_MANY_BLOCKS_FAIL) {
+          testResults = TestResults.APP_SPECIFIC_FAIL;
+        }
+        return testResults;
+    }
+
+    return this.studioApp_.getTestResults(false);
+  }
+
+  /**
+   * Get any app-specific message, based on the termination value,
+   * or return null if none applies.
+   * @override
+   */
+  getMessage(terminationValue) {
+    switch (terminationValue) {
+      case HarvesterTerminationValue.WRONG_CROP:
+        return mazeMsg.wrongCropError();
+      case HarvesterTerminationValue.EMPTY_CROP:
+        return mazeMsg.emptyCropError();
+      case HarvesterTerminationValue.DID_NOT_COLLECT_EVERYTHING:
+        return mazeMsg.didNotCollectAllCrops();
+      default:
+        return null;
+    }
   }
 }
