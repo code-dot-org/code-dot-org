2e8af15882b67d287179f98e3f9d05534a759aa0
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-08-19 15:54:38 -0700

    add new Planter skin

--- dashboard/app/models/karel.rb
+++ dashboard/app/models/karel.rb
@@ -27,7 +27,7 @@ class Karel < Maze
 
   # List of possible skins, the first is used as a default.
   def self.skins
-    %w(farmer farmer_night bee bee_night collector harvester)
+    %w(farmer farmer_night bee bee_night collector harvester planter)
   end
 
   # List of possible flower types
