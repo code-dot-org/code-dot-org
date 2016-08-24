cddfd526be5c49628a7d44c9626af88c79e03a7c
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-08-23 17:02:30 -0700

    Add more functionality to Drawer
    Specifically, move a bunch of functionality that used to be in Maze into
    Drawer. Also move some functionality that used to be in WordSearch into
    new WordSearchDrawer.
    
    Also move Maze.gridItemDrawer to Maze.subtype.drawer

--- apps/src/maze/drawer.js
+++ apps/src/maze/drawer.js
@@ -5,7 +5,6 @@
  */
 
 import { SVG_NS } from '../constants';
-import { cellId } from './mazeUtils';
 
 const SQUARE_SIZE = 50;
 
@@ -21,6 +20,13 @@ export default class Drawer {
   }
 
   /**
+  * Generalized function for generating ids for cells in a table
+  */
+  static cellId(row, col, prefix='') {
+    return prefix + '_' + row + '_' + col;
+  }
+
+  /**
    * Return the appropriate asset url for the given location. Overridden
    * by child classes to do much more interesting things.
    * @param {string} prefix
@@ -28,14 +34,14 @@ export default class Drawer {
    * @param {number} col
    * @return {string} asset url
    */
-  getAsset(prefix, row, col) {
+  getAsset(row, col, prefix='') {
     return this.asset_;
   }
 
   /**
    * Intentional noop function; BeeItemDrawer needs to be able to reset
    * between runs, so we implement a shared reset function so that we can
-   * call Maze.gridItemDrawer.reset() blindly. Overridden by BeeItemDrawer
+   * call drawer.reset() blindly. Overridden by BeeItemDrawer
    */
   reset() {}
 
@@ -46,19 +52,19 @@ export default class Drawer {
    * @param {boolean} running
    */
   updateItemImage(row, col, running) {
-    this.updateImageWithIndex_('', row, col);
+    return this.drawImage_(row, col);
   }
 
   /**
    * Creates/Update the image at the given row,col with the given prefix
-   * @param {string} prefix
    * @param {number} row
    * @param {number} col
+   * @param {string} prefix
    * @return {Element} img
    */
-  updateImageWithIndex_(prefix, row, col) {
-    let img = document.getElementById(cellId(prefix, row, col));
-    let href = this.getAsset(prefix, row, col);
+  drawImage_(row, col, prefix='') {
+    let img = document.getElementById(Drawer.cellId(row, col, prefix));
+    let href = this.getAsset(row, col, prefix);
 
     // if we have not already created this image and don't want one,
     // return
@@ -69,7 +75,7 @@ export default class Drawer {
     // otherwise create the image if we don't already have one, update
     // the href to whatever we want it to be, and hide it if we don't
     // have one
-    img = img || this.createImage_(prefix, row, col);
+    img = this.getOrCreateImage_(row, col, prefix);
     img.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', href || '');
     img.setAttribute('visibility', href ? 'visible' : 'hidden');
 
@@ -77,41 +83,46 @@ export default class Drawer {
   }
 
   /**
-   * Creates a new image and clipPath
+   * Creates a new image and optional clipPath, or returns the image if
+   * it already exists
    * @param {string} prefix
    * @param {number} row
    * @param {number} col
+   * @param {boolean} createClipPath
    * @return {Element} img
    */
-  createImage_(prefix, row, col) {
-    let href = this.getAsset(prefix, row, col);
+  getOrCreateImage_(row, col, prefix='', createClipPath=true) {
+    let href = this.getAsset(row, col, prefix);
 
     // Don't create an empty image
     if (!href) {
       return;
     }
 
+    let imgId = Drawer.cellId(row, col, prefix);
+
     // Don't create an image if one with this identifier already exists
-    if (document.getElementById(cellId(prefix, row, col))) {
-      return;
+    if (document.getElementById(imgId)) {
+      return document.getElementById(imgId);
     }
 
     let pegmanElement = document.getElementsByClassName('pegman-location')[0];
     let svg = document.getElementById('svgMaze');
 
-    let clipId = cellId(prefix + 'Clip', row, col);
-    let imgId = cellId(prefix, row, col);
-
+    let clipId;
     // Create clip path.
-    let clip = document.createElementNS(SVG_NS, 'clipPath');
-    clip.setAttribute('id', clipId);
-    let rect = document.createElementNS(SVG_NS, 'rect');
-    rect.setAttribute('x', col * SQUARE_SIZE);
-    rect.setAttribute('y', row * SQUARE_SIZE);
-    rect.setAttribute('width', SQUARE_SIZE);
-    rect.setAttribute('height', SQUARE_SIZE);
-    clip.appendChild(rect);
-    svg.insertBefore(clip, pegmanElement);
+    if (createClipPath) {
+      clipId = Drawer.cellId(row, col, prefix + 'Clip');
+      let clip = document.createElementNS(SVG_NS, 'clipPath');
+      clip.setAttribute('id', clipId);
+      let rect = document.createElementNS(SVG_NS, 'rect');
+      rect.setAttribute('x', col * SQUARE_SIZE);
+      rect.setAttribute('y', row * SQUARE_SIZE);
+      rect.setAttribute('width', SQUARE_SIZE);
+      rect.setAttribute('height', SQUARE_SIZE);
+      clip.appendChild(rect);
+      svg.insertBefore(clip, pegmanElement);
+    }
 
     // Create image.
     let img = document.createElementNS(SVG_NS, 'image');
@@ -120,10 +131,61 @@ export default class Drawer {
     img.setAttribute('width', SQUARE_SIZE);
     img.setAttribute('x', SQUARE_SIZE * col);
     img.setAttribute('y', SQUARE_SIZE * row);
-    img.setAttribute('clip-path', 'url(#' + clipId + ')');
     img.setAttribute('id', imgId);
+    if (createClipPath) {
+      img.setAttribute('clip-path', 'url(#' + clipId + ')');
+    }
     svg.insertBefore(img, pegmanElement);
 
     return img;
   }
+
+  /**
+   * Draw the given tile at row, col
+   */
+  drawTile(svg, tileSheetLocation, row, col, tileId, tileSheetHref) {
+    const [left, top] = tileSheetLocation;
+
+    const tileSheetWidth = SQUARE_SIZE * 5;
+    const tileSheetHeight = SQUARE_SIZE * 4;
+
+    // Tile's clipPath element.
+    const tileClip = document.createElementNS(SVG_NS, 'clipPath');
+    tileClip.setAttribute('id', 'tileClipPath' + tileId);
+    const tileClipRect = document.createElementNS(SVG_NS, 'rect');
+    tileClipRect.setAttribute('width', SQUARE_SIZE);
+    tileClipRect.setAttribute('height', SQUARE_SIZE);
+
+    tileClipRect.setAttribute('x', col * SQUARE_SIZE);
+    tileClipRect.setAttribute('y', row * SQUARE_SIZE);
+    tileClip.appendChild(tileClipRect);
+    svg.appendChild(tileClip);
+
+    // Tile sprite.
+    const tileElement = document.createElementNS(SVG_NS, 'image');
+    tileElement.setAttribute('id', 'tileElement' + tileId);
+    tileElement.setAttributeNS('http://www.w3.org/1999/xlink',
+                              'xlink:href', tileSheetHref);
+    tileElement.setAttribute('height', tileSheetHeight);
+    tileElement.setAttribute('width', tileSheetWidth);
+    tileElement.setAttribute('clip-path',
+                            'url(#tileClipPath' + tileId + ')');
+    tileElement.setAttribute('x', (col - left) * SQUARE_SIZE);
+    tileElement.setAttribute('y', (row - top) * SQUARE_SIZE);
+    svg.appendChild(tileElement);
+
+    // Tile animation
+    const tileAnimation = document.createElementNS(SVG_NS, 'animate');
+    tileAnimation.setAttribute('id', 'tileAnimation' + tileId);
+    tileAnimation.setAttribute('attributeType', 'CSS');
+    tileAnimation.setAttribute('attributeName', 'opacity');
+    tileAnimation.setAttribute('from', 1);
+    tileAnimation.setAttribute('to', 0);
+    tileAnimation.setAttribute('dur', '1s');
+    tileAnimation.setAttribute('begin', 'indefinite');
+    tileElement.appendChild(tileAnimation);
+  }
 }
+
+Drawer.SVG_NS = SVG_NS;
+Drawer.SQUARE_SIZE = SQUARE_SIZE;
