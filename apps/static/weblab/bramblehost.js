95fd3afe767f257fdba88aad2242e90ef8d3d473
Jeremy Stone <jeremy@code.org>
2016-08-24 08:44:11 -0700

    WIP

--- apps/static/weblab/bramblehost.js
+++ apps/static/weblab/bramblehost.js
@@ -24,35 +24,44 @@
 
     // expose object for parent window to talk to us through
     var brambleHost = {
+        // return file data from the Bramble editor
         getBrambleCode: function (callback) {
+
             var fs = theBramble.getFileSystem();
             var sh = new fs.Shell();
             var Path = theBramble.Filer.Path;
 
+            // enumerate files in the file system off the project root
             sh.ls(projectRoot, function (err, entries) {
                 if (err) {
                     throw err;
                 }
                 var fileList = [];
 
+                // async-chained enumeration: get the file data for i-th file
                 function getFileData(i, callback) {
                     if (i < entries.length) {
                         var entry = entries[i];
                         var path = Path.join(projectRoot, entry.path);
+                        // read file data
                         fs.readFile(path, 'utf8', function (err, fileData) {
                             if (err) {
                                 throw err;
                             }
+                            // add file data to list that gets returned
                             var file = { name: entry.path, data: fileData};
                             fileList.push(file);
+                            // continue on to next item in list
                             getFileData(i+1, callback);
                         });
                     } else {
                         var code = { files: fileList };
+                        // end of list, call completion callback
                         callback(code);
                     }
                 }
 
+                // start an async-chained walk through the file list
                 getFileData(0,callback);
             });
         }
@@ -61,21 +70,19 @@
     // Give our interface to our parent
     webLab.setBrambleHost(brambleHost);
 
-    function installDefaultFiles(Bramble, sources, callback) {
+    function loadFiles(Bramble, sources, callback) {
         var fs = Bramble.getFileSystem();
         var sh = new fs.Shell();
         var Path = Bramble.Filer.Path;
 
-        // Simulate a more complex root, like Thimble does
         sh.rm(projectRoot, {recursive: true}, function (err) {
+            // create project root directory
             sh.mkdirp(projectRoot, function (err) {
-                // If we run this multiple times, the dir will exist
                 if (err && err.code !== "EEXIST") {
                     throw err;
                 }
 
-                // Stick things in the project root
-                function writeProjectFile(path, data, callback) {
+                function writeFileData(path, data, callback) {
                     path = Path.join(projectRoot, path);
                     fs.writeFile(path, data, function (err) {
                         if (err) {
@@ -85,26 +92,22 @@
                     });
                 }
 
-                function writeStartSource(sources, i, callback) {
+                // async-chained enumeration: write the i-th file into Bramble file system
+                function writeSourceFile(sources, i, callback) {
                     if (i < sources.files.length) {
                         var file = sources.files[i];
-                        writeProjectFile(file.name, file.data, function () {
-                            writeStartSource(sources, i + 1, callback);
+                        writeFileData(file.name, file.data, function () {
+                            writeSourceFile(sources, i + 1, callback);
                         });
                     } else {
+                        // end of list, call completion callback
                         callback();
                     }
                 }
 
-                writeStartSource(sources, 0, callback);
+                writeSourceFile(sources, 0, callback);
             });
         });
-    }
-
-    function ensureFiles(Bramble, sources, callback) {
-        var fs = Bramble.getFileSystem();
-
-        return installDefaultFiles(Bramble, sources, callback);
     }
 
     var inspectorEnabled = false;
@@ -169,7 +172,7 @@
         var startSources = webLab.getStartSources();
 
         // Setup the filesystem while Bramble is loading
-        ensureFiles(Bramble, startSources, function () {
+        loadFiles(Bramble, startSources, function () {
             // Now that fs is setup, tell Bramble which root dir to mount
             // and which file within that root to open on startup.
             Bramble.mount(projectRoot);
