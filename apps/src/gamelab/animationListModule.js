fdc9cb24b9ea970c7584d9ed52fc99d0480bb1e5
Caley Brock <caleybrock@Caleys-MacBook-Pro.local>
2016-08-25 12:05:35 -0700

    Make looping required and other list items required

--- apps/src/gamelab/animationListModule.js
+++ apps/src/gamelab/animationListModule.js
@@ -194,6 +194,8 @@ export function setInitialAnimationList(serializedAnimationList) {
       } else {
         animation.frameDelay = 2;
       }
+    } if (animation.looping === undefined) {
+      animation.looping = true;
     }
   }
 
