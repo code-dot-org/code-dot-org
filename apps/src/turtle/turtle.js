8ec15dfe2305589fa102190648101378eacdceb5
Josh Lory <josh.lory@code.org>
2016-08-25 13:52:29 -0700

    '+' characters in the base64-encoded image were getting converted to ' '
    It's not clear to me when or why this broke, but we're definitely posting invalid base64 to /milestone. Unescaped '+' characters in the image base64 were concatenated directly into the form-urlencoded POST data, where they turned into ' ' characters.

--- apps/src/turtle/turtle.js
+++ apps/src/turtle/turtle.js
@@ -1507,7 +1507,7 @@ Artist.prototype.checkAnswer = function () {
   // Get the canvas data for feedback.
   if (this.testResults >= this.studioApp_.TestResults.TOO_MANY_BLOCKS_FAIL &&
     !isFrozen && (level.freePlay || level.impressive)) {
-    reportData.image = this.getFeedbackImage_().split(',')[1];
+    reportData.image = encodeURIComponent(this.getFeedbackImage_().split(',')[1]);
   }
 
   this.studioApp_.report(reportData);
