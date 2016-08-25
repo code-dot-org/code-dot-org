02be4b13e53ef67c191bfe1122209f12aa05a208
Brent Van Minnen <bjvanminnen@gmail.com>
2016-08-25 11:14:06 -0700

    add a bunch of react-storybook items for progress dot

--- apps/src/code-studio/components/progress/progress_dot.jsx
+++ apps/src/code-studio/components/progress/progress_dot.jsx
@@ -132,23 +132,16 @@ export const BubbleInterior = React.createClass({
   },
 
   render() {
-    let bubbleInterior;
-
-    if (this.props.showingLevelName) {
-      if (this.props.showingIcon) {
-        bubbleInterior = '';
+    if (!this.props.showingIcon) {
+      if (this.props.showingLevelName) {
+        // nbsp
+        return <span>{'\u00a0'}</span>;
       } else {
-        bubbleInterior = '\u00a0';
+        return <span>{this.props.title}</span>;
       }
-    } else if (!this.props.showingIcon) {
-      bubbleInterior = this.props.title;
     }
-
-    return (
-      <span>
-        {bubbleInterior}
-      </span>
-    );
+    // Icon is shown via parent div's className. No need to do anything here
+    return <span/>;
   }
 });
 
@@ -256,3 +249,296 @@ export default connect(state => ({
   currentLevelId: state.progress.currentLevelId,
   saveAnswersBeforeNavigation: state.progress.saveAnswersBeforeNavigation
 }))(ProgressDot);
+
+
+
+
+if (BUILD_STYLEGUIDE) {
+  ProgressDot.styleGuideExamples = storybook => {
+    storybook
+      .storiesOf('ProgressDot', module)
+      .addStoryTable([
+        {
+          name: 'assessment in course overview',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: null,
+                ids: [5275, []],
+                kind: 'assessment',
+                next: [2, 1],
+                position: 1,
+                previous: false,
+                status: 'not_tried',
+                title: 1,
+                uid: '5275_0',
+                url: 'http://localhost-studio.code.org:3000/s/csp1/lockable/1/puzzle/1/page/1'
+              }}
+            />
+          )
+        },
+        {
+          name: 'locked assessment in course overview',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: null,
+                ids: [5275, []],
+                kind: 'assessment',
+                next: [2, 1],
+                position: 1,
+                previous: false,
+                status: 'locked',
+                title: 1,
+                uid: '5275_0',
+                url: 'http://localhost-studio.code.org:3000/s/csp1/lockable/1/puzzle/1/page/1'
+              }}
+            />
+          )
+        },
+        {
+          name: 'submitted assessment in course overview',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: null,
+                ids: [5275, []],
+                kind: 'assessment',
+                next: [2, 1],
+                position: 1,
+                previous: false,
+                status: 'submitted',
+                title: 1,
+                uid: '5275_0',
+                url: 'http://localhost-studio.code.org:3000/s/csp1/lockable/1/puzzle/1/page/1'
+              }}
+            />
+          )
+        },
+        {
+          name: 'attempted puzzle in course overview',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: null,
+                ids: [5275, []],
+                kind: 'puzzle',
+                next: [2, 1],
+                position: 1,
+                previous: [7,15],
+                status: 'attempted',
+                title: 1,
+                url: 'http://localhost-studio.code.org:3000/s/course1/stage/8/puzzle/1'
+              }}
+            />
+          )
+        },
+        {
+          name: 'completed puzzle in course overview',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: null,
+                ids: [2288, []],
+                kind: 'puzzle',
+                next: [2, 1],
+                position: 6,
+                status: 'perfect',
+                title: 6,
+                url: 'http://localhost-studio.code.org:3000/s/course1/stage/11/puzzle/6'
+              }}
+            />
+          )
+        },
+        {
+          name: 'unlplugged puzzle in course overview',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: null,
+                ids: [2094, []],
+                kind: 'unplugged',
+                previous: [1, 2],
+                position: 1,
+                status: 'not_tried',
+                title: 'Unplugged Activity',
+                url: 'http://localhost-studio.code.org:3000/s/course1/stage/2/puzzle/1'
+              }}
+            />
+          )
+        },
+        {
+          name: 'puzzle with icon in course overview',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: 'fa-file-text',
+                ids: [1379, []],
+                kind: 'puzzle',
+                name: 'CSP Pre-survey',
+                position: 2,
+                status: 'not_tried',
+                title: 2,
+                url: 'http://localhost-studio.code.org:3000/s/csp1/stage/1/puzzle/2'
+              }}
+            />
+          )
+        },
+        {
+          name: 'named level in course overview',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: 'fa-file-text',
+                ids: [5442, []],
+                kind: 'named_level',
+                name: 'CSP Pre-survey',
+                position: 1,
+                previous: [1,1],
+                status: 'not_tried',
+                title: 1,
+                url: 'http://localhost-studio.code.org:3000/s/csp1/stage/1/puzzle/1'
+              }}
+            />
+          )
+        },
+        {
+          name: 'completed named video level in course overview',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: 'fa-video-camera',
+                ids: [5078, []],
+                kind: 'named_level',
+                name: 'Computer Science is Changing Everything',
+                position: 3,
+                previous: [1,1],
+                status: 'perfect',
+                title: 3,
+                url: 'http://localhost-studio.code.org:3000/s/csp1/stage/1/puzzle/3'
+              }}
+            />
+          )
+        },
+        {
+          name: 'named video level in course overview with no icon',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={true}
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: null,
+                ids: [5096, []],
+                kind: 'named_level',
+                name: 'Internet Simulator: sending binary messages',
+                position: 2,
+                status: 'not_tried',
+                title: 2,
+                url: 'http://localhost-studio.code.org:3000/s/csp1/stage/3/puzzle/2'
+              }}
+            />
+          )
+        },
+        {
+          name: 'small named level in header',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={false}
+              currentLevelId="1379"
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: 'fa-file-text',
+                ids: [5442, []],
+                kind: 'named_level',
+                name: 'CSP Pre-survey',
+                position: 1,
+                previous: [1,1],
+                status: 'not_tried',
+                title: 1,
+                url: 'http://localhost-studio.code.org:3000/s/csp1/stage/1/puzzle/1'
+              }}
+            />
+          )
+        },
+        {
+          name: 'completed small named video level in header',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={false}
+              currentLevelId="1379"
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: 'fa-video-camera',
+                ids: [5078, []],
+                kind: 'named_level',
+                name: 'Computer Science is Changing Everything',
+                position: 3,
+                previous: [1,1],
+                status: 'perfect',
+                title: 3,
+                url: 'http://localhost-studio.code.org:3000/s/csp1/stage/1/puzzle/3'
+              }}
+            />
+          )
+        },
+        {
+          name: 'small puzzle in header',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={false}
+              currentLevelId="1379"
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: null,
+                ids: [2049, []],
+                kind: 'puzzle',
+                position: 4,
+                next: [3,1],
+                status: 'not_tried',
+                title: 4,
+                url: 'http://localhost-studio.code.org:3000/s/csp1/stage/1/puzzle/4'
+              }}
+            />
+          )
+        },
+        {
+          name: 'current leve puzzle in header',
+          story: () => (
+            <ProgressDot
+              courseOverviewPage={false}
+              currentLevelId="2049"
+              saveAnswersBeforeNavigation={false}
+              level={{
+                icon: null,
+                ids: [2049, []],
+                kind: 'puzzle',
+                position: 4,
+                next: [3,1],
+                status: 'not_tried',
+                title: 4,
+                url: 'http://localhost-studio.code.org:3000/s/csp1/stage/1/puzzle/4'
+              }}
+            />
+          )
+        },
+      ]);
+  };
+}
