6838e31079d60641f216b89e0674d8a756285b7b
Paul Carduner <paul@carduner.net>
2016-08-30 16:00:13 -0700

    try some stuff

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -8,6 +8,7 @@ var _ = require('lodash');
 var logBuildTimes = require('./script/log-build-times');
 var webpackConfig = require('./webpack');
 var envConstants = require('./envConstants');
+var ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
 
 module.exports = function (grunt) {
   // Decorate grunt to record and report build durations.
@@ -342,104 +343,95 @@ module.exports = function (grunt) {
   };
 
 
+  var appsEntries = _.fromPairs(appsToBuild.map(function (app) {
+    return [app, './src/' + app + '/main.js'];
+  }).concat(appsToBuild.indexOf('applab') === -1 ? [] :
+            [['applab-api', './src/applab/api-entry.js']]
+  ));
+  var codeStudioEntries = {
+    'code-studio': './src/code-studio/code-studio.js',
+    'levelbuilder': './src/code-studio/levelbuilder.js',
+    'levelbuilder_markdown': './src/code-studio/levelbuilder_markdown.js',
+    'levelbuilder_studio': './src/code-studio/levelbuilder_studio.js',
+    'levelbuilder_gamelab': './src/code-studio/levelbuilder_gamelab.js',
+    'levelbuilder_applab': './src/code-studio/levelbuilder_applab.js',
+    'makerlab/setupPage': './src/code-studio/makerlab/setupPage.js',
+    'districtDropdown': './src/code-studio/districtDropdown.js',
+    'signup': './src/code-studio/signup.js',
+    'termsInterstitial': './src/code-studio/termsInterstitial.js',
+    'levels/contract_match': './src/code-studio/levels/contract_match.jsx',
+    'levels/widget': './src/code-studio/levels/widget.js',
+    'levels/external': './src/code-studio/levels/external.js',
+    // put these entry points in arrays so that they can be required elsewhere
+    // https://github.com/webpack/webpack/issues/300
+    'levels/multi': ['./src/code-studio/levels/multi.js'],
+    'levels/textMatch': ['./src/code-studio/levels/textMatch.js'],
+    'levels/levelGroup': './src/code-studio/levels/levelGroup.js',
+    'levels/dialogHelper': './src/code-studio/levels/dialogHelper.js',
+    'initApp/initApp': './src/code-studio/initApp/initApp.js'
+  };
   var bundles = [
     {
       uniqueName: 'apps',
-      entries: _.fromPairs(appsToBuild.map(function (app) {
-        return [app, './src/' + app + '/main.js'];
-      }).concat(appsToBuild.indexOf('applab') === -1 ? [] :
-        [['applab-api', './src/applab/api-entry.js']]
-      )),
-      commonFile: 'common'
-    },
+      entries: _.extend(
+        {},
+        appsEntries,
+        codeStudioEntries,
+        {
+          minimal: [
+            'marked',
+            'jquery',
+            'react',
+            'react-dom',
+            'radium',
+          ],
 
-    {
-      uniqueName: 'codeStudio',
-      entries: {
-        'code-studio': './src/code-studio/code-studio.js',
-        'levelbuilder': './src/code-studio/levelbuilder.js',
-        'levelbuilder_markdown': './src/code-studio/levelbuilder_markdown.js',
-        'levelbuilder_studio': './src/code-studio/levelbuilder_studio.js',
-        'levelbuilder_gamelab': './src/code-studio/levelbuilder_gamelab.js',
-        'levelbuilder_applab': './src/code-studio/levelbuilder_applab.js',
-        'makerlab/setupPage': './src/code-studio/makerlab/setupPage.js',
-        'districtDropdown': './src/code-studio/districtDropdown.js',
-        'signup': './src/code-studio/signup.js',
-        'termsInterstitial': './src/code-studio/termsInterstitial.js',
-        'levels/contract_match': './src/code-studio/levels/contract_match.jsx',
-        'levels/widget': './src/code-studio/levels/widget.js',
-        'levels/external': './src/code-studio/levels/external.js',
-        // put these entry points in arrays so that they can be required elsewhere
-        // https://github.com/webpack/webpack/issues/300
-        'levels/multi': ['./src/code-studio/levels/multi.js'],
-        'levels/textMatch': ['./src/code-studio/levels/textMatch.js'],
-        'levels/levelGroup': './src/code-studio/levels/levelGroup.js',
-        'levels/dialogHelper': './src/code-studio/levels/dialogHelper.js',
-        'initApp/initApp': './src/code-studio/initApp/initApp.js'
-      },
-      commonFile: 'code-studio-common',
-      provides: ['marked', 'react', 'react-dom', 'radium']
-    },
+          plc: './src/code-studio/plc/plc.js',
 
-    {
-      uniqueName: 'plc',
-      entries: {
-        plc: './src/code-studio/plc/plc.js'
-      },
-      provides: ['marked']
-    },
 
-    // Build embedVideo.js in its own step (skipping factor-bundle) so that
-    // we don't have to include the large code-studio-common file in the
-    // embedded video page, keeping it fairly lightweight.
-    // (I wonder how much more we could slim it down by removing jQuery!)
-    // @see embed.html.haml
-    {
-      uniqueName: 'embedVideo',
-      entries: {
-        embedVideo: './src/code-studio/embedVideo.js'
-      },
-      provides: ['jquery']
-    },
-    // embedBlocks.js is just React, the babel-polyfill, and a few other dependencies
-    // in a bundle to minimize the amound of stuff we need when loading blocks
-    // in an iframe.
-    {
-      uniqueName: 'embedBlocks',
-      entries: {
-        embedBlocks: './src/code-studio/embedBlocks.js'
-      },
-      provides: ['react', 'react-dom', 'radium']
-    },
-    // tutorialExplorer for code.org/learn 2016 edition.
-    {
-      uniqueName: 'tutorialExplorer',
-      entries: {
-        tutorialExplorer: './src/tutorialExplorer/tutorialExplorer.js'
-      },
-      provides: ['react', 'react-dom']
-    },
-    {
-      uniqueName: 'makerlabDependencies',
-      entries: {
-        makerlab: './src/code-studio/makerlab/makerlabDependencies.js'
-      },
-      provides: ['johnny-five', 'playground-io', 'chrome-serialport']
-    },
+          // Build embedVideo.js in its own step (skipping factor-bundle) so that
+          // we don't have to include the large code-studio-common file in the
+          // embedded video page, keeping it fairly lightweight.
+          // (I wonder how much more we could slim it down by removing jQuery!)
+          // @see embed.html.haml
+          embedVideo: './src/code-studio/embedVideo.js',
 
-    {
-      uniqueName: 'pd',
-      entries: {
-        pd: './src/code-studio/pd/workshop_dashboard/workshop_dashboard.jsx'
-      }
-    },
+          // embedBlocks.js is just React, the babel-polyfill, and a few other dependencies
+          // in a bundle to minimize the amound of stuff we need when loading blocks
+          // in an iframe.
+          embedBlocks: './src/code-studio/embedBlocks.js',
 
-    {
-      uniqueName: 'publicKeyCryptography',
-      entries: {
-        publicKeyCryptography: './src/publicKeyCryptography/main.js'
-      },
-      provides: ['react', 'react-dom', 'radium']
+          // tutorialExplorer for code.org/learn 2016 edition.
+          tutorialExplorer: './src/tutorialExplorer/tutorialExplorer.js',
+
+          makerlab: './src/code-studio/makerlab/makerlabDependencies.js',
+
+          pd: './src/code-studio/pd/workshop_dashboard/workshop_dashboard.jsx',
+
+          publicKeyCryptography: './src/publicKeyCryptography/main.js',
+        }
+      ),
+      externals: [],
+      plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+          name: 'minimal',
+          minChunks: Infinity
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+          name: 'code-studio-common',
+          chunks: _.keys(codeStudioEntries).concat(['apps-common']),
+          minChunks: 2
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+          name: 'apps-common',
+          chunks: _.keys(appsEntries),
+          minChunks: 2
+        }),
+        new ChunkManifestPlugin({
+          filename: 'apps-chunk-manifest.json',
+          manifestVariable: 'appsWebpackManifest'
+        }),
+      ],
     },
 
     {
@@ -460,12 +452,16 @@ module.exports = function (grunt) {
       var entries = bundle.entries;
       var commonFile = bundle.commonFile;
       var provides = bundle.provides;
+      var plugins = bundle.plugins;
 
       return webpackConfig.create({
         uniqueName: bundle.uniqueName,
         output: path.resolve(__dirname, OUTPUT_DIR),
         entries: entries,
+        externals: bundle.externals,
+        thirdParty: bundle.thirdParty,
         commonFile: commonFile,
+        plugins: plugins,
         provides: provides,
         minify: minify,
         watch: watch,
