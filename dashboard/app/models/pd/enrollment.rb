438a358eda01c063e7e44024162b86d2e1017cf0
Andrew Oberhardt <Andrew@Code.org>
2016-08-30 19:04:44 -0700

    Reordered validations, fixed and renamed failing unit test, and removed another outdated test

--- dashboard/app/models/pd/enrollment.rb
+++ dashboard/app/models/pd/enrollment.rb
@@ -34,14 +34,14 @@ class Pd::Enrollment < ActiveRecord::Base
   # Allow overriding the school and school_info requirements.
   attr_accessor :skip_school_validation
 
-  validates_presence_of :school, unless: :skip_school_validation
-  validates_presence_of :school_info, unless: :skip_school_validation
-
   accepts_nested_attributes_for :school_info, reject_if: :check_school_info
   validates_associated :school_info
 
   validates :name, :email, presence: true
   validates_confirmation_of :email
+
+  validates_presence_of :school, unless: :skip_school_validation
+  validates_presence_of :school_info, unless: :skip_school_validation
 
   def has_user?
     self.user_id
