c109100c5ab74f0ff9c40a40078d62fe802af258
Brad Buchanan <brad@code.org>
2016-09-01 13:13:50 -0700

    Add styleguide entry for NumberedSteps

--- apps/src/publicKeyCryptography/NumberedSteps.jsx
+++ apps/src/publicKeyCryptography/NumberedSteps.jsx
@@ -52,3 +52,92 @@ Step.propTypes = {
 Step.defaultProps = {
   requires: []
 };
+
+if (BUILD_STYLEGUIDE) {
+  NumberedSteps.styleGuideExamples = storybook => {
+    return storybook
+      .storiesOf('NumberedSteps', module)
+      .addStoryTable([
+        {
+          name: 'Steps with prerequisites',
+          description: `You can pass requirements to each step.  Steps with
+          unmet requirements (null or undefined) will be displayed at 20%
+          opacity.`,
+          story: () => (
+            <NumberedSteps>
+              <Step requires={['something', 'something else']}>
+                Step with all requirements met.
+              </Step>
+              <Step requires={[true, true, null]}>
+                Step with an unmet requirement
+              </Step>
+            </NumberedSteps>)
+        },
+        {
+          name: 'Fade effect',
+          description: `Steps fade in and out as their requirements are met/unmet`,
+          story: () => {
+            const FadeEffectExample = React.createClass({
+              getInitialState() {
+                return {};
+              },
+
+              onToggleStep1() {
+                this.setState({
+                  step1Done: this.state.step1Done ? null : true
+                });
+              },
+
+              onToggleStep2() {
+                this.setState({
+                  step2Done: this.state.step2Done ? null : true
+                });
+              },
+
+              onToggleStep3() {
+                this.setState({
+                  step3Done: this.state.step3Done ? null : true
+                });
+              },
+
+              render() {
+                return (
+                  <NumberedSteps>
+                    <Step>
+                      Click to complete this step: <a href="#" onClick={this.onToggleStep1}>{this.state.step1Done ? '' : 'Not'} Done</a>
+                    </Step>
+                    <Step requires={[this.state.step1Done]}>
+                      This step depends only on step one.
+                      <br/>Click to complete this step: <a href="#" onClick={this.onToggleStep2}>{this.state.step2Done ? '' : 'Not'} Done</a>
+                    </Step>
+                    <Step requires={[this.state.step2Done]}>
+                      This step depends only on step two.
+                      <br/>Click to complete this step: <a href="#" onClick={this.onToggleStep3}>{this.state.step3Done ? '' : 'Not'} Done</a>
+                    </Step>
+                    <Step requires={[this.state.step1Done, this.state.step2Done, this.state.step3Done]}>
+                      This step depends on all preceding steps.
+                    </Step>
+                  </NumberedSteps>);
+              }
+            });
+            return <FadeEffectExample/>;
+          }
+        },
+        {
+          name: 'Holds any content',
+          description: `By default, NumberedSteps just works like an <ol> element
+          with particular spacing and styling.`,
+          story: () => (
+            <NumberedSteps>
+              <Step>
+                Arbitrary content here.
+                <p>With multiple lines.</p>
+              </Step>
+              <Step>
+                More arbitrary content here.
+              </Step>
+            </NumberedSteps>)
+        }
+      ]);
+  };
+}
