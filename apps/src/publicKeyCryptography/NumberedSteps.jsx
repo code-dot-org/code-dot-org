0c8357b6dcf9d2c185f3f0d9ec0575bd3888ac6d
Brad Buchanan <brad@code.org>
2016-09-01 14:05:30 -0700

    Table row is rendered by Step component

--- apps/src/publicKeyCryptography/NumberedSteps.jsx
+++ apps/src/publicKeyCryptography/NumberedSteps.jsx
@@ -17,19 +17,7 @@ export default function NumberedSteps(props) {
   return (
     <table>
       <tbody>
-        {React.Children.map(props.children, (child, index) => {
-          const stepRequirements = child.props && child.props.requires || [];
-          const isStepEnabled = stepRequirements.every(x => x !== null && x !== undefined);
-          const trStyle = {
-            transition: 'opacity 0.5s',
-            opacity: isStepEnabled ? 1 : 0.2
-          };
-          return (
-            <tr style={trStyle}>
-              <td style={style.td}>{(index + 1) + ')'}</td>
-              <td style={style.td}>{child}</td>
-            </tr>);
-        })}
+        {React.Children.map(props.children, (child, index) => React.cloneElement(child, {index}))}
       </tbody>
     </table>);
 }
@@ -43,13 +31,24 @@ NumberedSteps.propTypes = {
  * If any of those values are null or undefined, the step will be faded out.
  */
 export function Step(props) {
-  return <div>{props.children}</div>;
+  const isStepEnabled = (props.requires || []).every(x => x !== null && x !== undefined);
+  const trStyle = {
+    transition: 'opacity 0.5s',
+    opacity: isStepEnabled ? 1 : 0.2
+  };
+  return (
+    <tr style={trStyle}>
+      <td style={style.td}>{(props.index + 1) + ')'}</td>
+      <td style={style.td}>{props.children}</td>
+    </tr>);
 }
 Step.propTypes = {
+  index: React.PropTypes.number.isRequired,
   children: AnyChildren,
   requires: React.PropTypes.arrayOf(React.PropTypes.any)
 };
 Step.defaultProps = {
+  index: 0,
   requires: []
 };
 
