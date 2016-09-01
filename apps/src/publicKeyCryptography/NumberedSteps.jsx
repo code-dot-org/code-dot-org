9cf7824b2007df20032f441c56dbfd9b2f0042a1
Brad Buchanan <brad@code.org>
2016-08-31 17:19:15 -0700

    Reveal steps gradually
    Lets steps in the NumberedSteps list have 'requirements' - a set of values that must be available for the step to fade in.  Steps fade in when their requirements are met, and out again when their requirements are unavailable.

--- apps/src/publicKeyCryptography/NumberedSteps.jsx
+++ apps/src/publicKeyCryptography/NumberedSteps.jsx
@@ -17,15 +17,38 @@ export default function NumberedSteps(props) {
   return (
     <table>
       <tbody>
-        {React.Children.map(props.children, (child, index) => (
-          <tr>
-            <td style={style.td}>{(index + 1) + ')'}</td>
-            <td style={style.td}>{child}</td>
-          </tr>
-        ))}
+        {React.Children.map(props.children, (child, index) => {
+          const stepRequirements = child.props && child.props.requires || [];
+          const isStepEnabled = stepRequirements.every(x => x !== null && x !== undefined);
+          const trStyle = {
+            transition: 'opacity 0.5s',
+            opacity: isStepEnabled ? 1 : 0.2
+          };
+          return (
+            <tr style={trStyle}>
+              <td style={style.td}>{(index + 1) + ')'}</td>
+              <td style={style.td}>{child}</td>
+            </tr>);
+        })}
       </tbody>
     </table>);
 }
 NumberedSteps.propTypes = {
   children: AnyChildren
+};
+
+/**
+ * Replacement for a 'div' in a NumberedSteps list.
+ * Accepts a 'requires' prop that takes an array of values.
+ * If any of those values are null or undefined, the step will be faded out.
+ */
+export function Step(props) {
+  return <div>{props.children}</div>;
+}
+Step.propTypes = {
+  children: AnyChildren,
+  requires: React.PropTypes.arrayOf(React.PropTypes.any)
+};
+Step.defaultProps = {
+  requires: []
 };
