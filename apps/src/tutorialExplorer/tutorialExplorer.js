dc46af35cd5867be73fe869399e17a4e46e47563
Brendan Reville <brendan@code.org>
2016-09-03 09:06:05 -0700

    learn2016: User filtering now works.  Just dummy data so far.

--- apps/src/tutorialExplorer/tutorialExplorer.js
+++ apps/src/tutorialExplorer/tutorialExplorer.js
@@ -12,10 +12,12 @@ window.TutorialExplorerManager = function (options) {
   this.options = options;
   var self = this;
 
+  options.tutorials.contents = options.tutorials.contents.splice(0,5);
+
   const FilterChoice = React.createClass({
     handleChange: function() {
       this.props.onUserInput(
-        "grade", this.props.name,
+        this.props.groupName, this.props.name,
         this.refs.isCheckedInput.checked
       );
     },
@@ -30,8 +32,9 @@ window.TutorialExplorerManager = function (options) {
               checked={this.props.selected}
               ref="isCheckedInput"
               onChange={this.handleChange}
+              style={{marginRight: '5px'}}
             />
-            {this.props.name}
+            {this.props.text}
           </label>
         </div>
       )
@@ -43,9 +46,9 @@ window.TutorialExplorerManager = function (options) {
       return (
         <div style={{paddingBottom: '20px'}}>
           <div>
-            {this.props.name}
+            {this.props.text}
           </div>
-          {this.props.filterEntries.map(item => <FilterChoice name={item.name} selected={this.props.selection && this.props.selection.includes(item.name)} onUserInput={this.props.onUserInput} key={item.name}/>)}
+          {this.props.filterEntries.map(item => <FilterChoice groupName={this.props.name} name={item.name} text={item.text} selected={this.props.selection && this.props.selection.includes(item.name)} onUserInput={this.props.onUserInput} key={item.name}/>)}
         </div>
       )
     }
@@ -56,7 +59,7 @@ window.TutorialExplorerManager = function (options) {
       return (
         <div>
           <div style={{position: 'fixed'}}>
-            {this.props.filterGroups.map(item => <FilterGroup name={item.name} filterEntries={item.entries} onUserInput={this.props.onUserInput} selection={this.props.selection[item.name]} key={item.name}/>)}
+            {this.props.filterGroups.map(item => <FilterGroup name={item.name} text={item.text} filterEntries={item.entries} onUserInput={this.props.onUserInput} selection={this.props.selection[item.name]} key={item.name}/>)}
           </div>
           <div className='col-25'>
             &nbsp;
@@ -68,22 +71,18 @@ window.TutorialExplorerManager = function (options) {
 
   const Tutorial = React.createClass({
     propTypes: {
-      itemCode: React.PropTypes.string.isRequired
+      item: React.PropTypes.object.isRequired
     },
 
-    render() {
-      if (this.props.filters.grade.includes("k-5") || this.props.itemCode.match("^k")) {
-        return (
-          <div style={{width: '25%', float: 'left', padding: '2px'}}>
-            <div style={{backgroundColor: 'grey', color: 'white', padding: '5px'}}>
-              <img src={this.props.imageUrl} style={{width: '100%'}}/>
-              {this.props.itemCode}
-            </div>
+  render() {
+      return (
+        <div style={{width: '25%', float: 'left', padding: '2px'}}>
+          <div style={{backgroundColor: 'grey', color: 'white', padding: '5px'}}>
+            <img src={this.props.item.image} style={{width: '100%'}}/>
+            {this.props.item.code}
           </div>
-        )
-      } else {
-        return null;
-      }
+        </div>
+      )
     }
   });
 
@@ -95,9 +94,67 @@ window.TutorialExplorerManager = function (options) {
     },
 
     render() {
+      // Should we show this item based on current filter settings?
+
+      // Go through all active filter categories.
+        // No filters set for a category, then show everything that might match.
+        // Tutorial has no tags, then it'll show.
+        // But if we actually have filters for a category, and the tutorial does too,
+        // then each filter must have a tag.
+        // If there are any misses, then the tutorial is hidden.
+
+      function filterFn(tutorial, index, array) {
+        //console.log("filterFn", tutorial, index, this.props.filters);
+
+        var filterMiss = false;
+
+        for (var filterGroupName in this.props.filters) {
+
+          var tutorialTags = tutorial["tags_" + filterGroupName];
+
+          if (tutorialTags && tutorialTags.length > 0) {
+            console.log("  filterGroupName - ", filterGroupName, " - for tutorial - ", tutorial["tags_" + filterGroupName]);
+
+            var tutorialTagsSplit = tutorialTags.split(',');
+            console.log("  tutorialTagsSplit", tutorialTagsSplit);
+
+            // now check all the filter group's tags
+
+            var filterGroup = this.props.filters[filterGroupName];
+            console.log("      filterGroup", filterGroup);
+
+            // For this filter group, we've not yet found a matching tag between
+            // user selected otions and tutorial tags.
+            var filterGroupHit = false;
+
+            for (var filterName of filterGroup) {
+              console.log("        filterName", filterName);
+
+              if (tutorialTagsSplit.includes(filterName)) {
+                console.log("          tutorial tag match");
+
+                // The tutorial had a matching tag.
+                filterGroupHit = true;
+              } else {
+                console.log("          tutorial tag MISS");
+                //filterMiss = true;
+              }
+            }
+
+            // Each filter group needs at least one user-selected filter to hit
+            // on the tutorial.
+            if (filterGroup.length !== 0 && !filterGroupHit) {
+              filterMiss = true;
+            }
+          }
+        }
+
+        return !filterMiss;
+      }
+
       return (
         <div style={{width: '75%', float: 'left'}}>
-          {this.props.tutorials.map(item => <Tutorial itemCode={item.code} imageUrl={item.image} filters={this.props.filters} key={item.code}/>)}
+          {this.props.tutorials.filter(filterFn, this).map(item => <Tutorial item={item} filters={this.props.filters} key={item.code}/>)}
         </div>
       )
     }
@@ -105,8 +162,14 @@ window.TutorialExplorerManager = function (options) {
 
   const TutorialExplorer = React.createClass({
     getInitialState: function() {
+      var filters = {};
+
+      for (let filterGroup of this.props.filterGroups) {
+        filters[filterGroup.name] = [];
+
+      }
       return {
-        filters: { grade: ["k-5"] }
+        filters: filters
       };
     },
 
@@ -116,10 +179,16 @@ window.TutorialExplorerManager = function (options) {
       if (value) {
         // Add value to end of array.
         filterEntryChange["$push"] = [filterEntry];
+
       } else {
+        var itemIndex = this.state.filters[filterGroup].indexOf(filterEntry);
+
+        console.log("Removing value ", filterEntry, "at index", itemIndex, "from", this.state.filters[filterGroup]);
+
         // Find and remove specific value from array.
-        filterEntryChange["$splice"] = [[0, 1]];
+        filterEntryChange["$splice"] = [[itemIndex, 1]];
       }
+
 
       var filterGroupChange = {};
       filterGroupChange[filterGroup] = filterEntryChange;
@@ -129,26 +198,23 @@ window.TutorialExplorerManager = function (options) {
 
       var newState = update(this.state, stateChange);
 
-      //var newState = React.addons.update(this.state, {
-      //  filterGroup: {
-      //    name: { (value ? $push : $unshift): filterEntry }
-      //  }
-      //});
+      console.log("new state", newState);
+
       this.setState(newState);
     },
 
     render() {
       return (
         <div>
-          <FilterSet filterGroups={options.filters} onUserInput={this.handleUserInput} selection={this.state.filters}/>
-          <TutorialSet tutorials={options.tutorials.contents} filters={this.state.filters}/>
+          <FilterSet filterGroups={this.props.filterGroups} onUserInput={this.handleUserInput} selection={this.state.filters}/>
+          <TutorialSet tutorials={this.props.tutorials} filters={this.state.filters}/>
         </div>
       )
     }
   });
 
   window.ReactDOM.render(
-    <TutorialExplorer/>,
+    <TutorialExplorer filterGroups={options.filters} tutorials={options.tutorials.contents}/>,
     document.getElementById('tutorials')
   );
 
