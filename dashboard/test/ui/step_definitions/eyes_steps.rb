f0479ff2be2875808392a102538d7eb2ec92528c
Brian Jordan <bcjordan@gmail.com>
2016-09-06 13:15:36 -0400

    Update usage of circle_pr_branch_base_no_origin

--- dashboard/test/ui/step_definitions/eyes_steps.rb
+++ dashboard/test/ui/step_definitions/eyes_steps.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../../../../lib', __FILE__)
 require 'eyes_selenium'
 require 'cdo/git_utils'
+require 'cdo/circle_github_utils'
 require 'open-uri'
 require 'json'
 
@@ -16,7 +17,7 @@ When(/^I open my eyes to test "([^"]*)"$/) do |test_name|
 
   @eyes.branch_name = GitUtils.current_branch
 
-  pr_base = GitUtils.circle_pr_branch_base_no_origin
+  pr_base = CircleGitHubUtils.circle_pr_branch_base_no_origin
   if pr_base
     puts "Branch is #{pr_base}"
     @eyes.parent_branch_name = pr_base
