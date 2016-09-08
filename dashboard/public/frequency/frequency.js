026f38ac2d421e08e20532b021db2f7f2dcbaa34
Asher Kach <asher@code.org>
2016-09-08 10:38:18 -0500

    Fix bug, adding missing quotes.

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f81bcc6eb01537db9cc16578fdb44bd39ab119b0b21731a7eca2c442b057dff5
-size 37224
+oid sha256:e6a8e28d1b8b008361ec9f03addf32fb73c1a9e904fadd9fce0d0e89f95bd63b
+size 37226
