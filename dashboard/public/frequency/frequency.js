03c028e59f21c72054d58c55043f61d78f927c57
Asher Kach <asher@code.org>
2016-09-08 13:46:21 -0500

    Add another pair of missing quotes.

--- dashboard/public/frequency/frequency.js
+++ dashboard/public/frequency/frequency.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6a8e28d1b8b008361ec9f03addf32fb73c1a9e904fadd9fce0d0e89f95bd63b
-size 37226
+oid sha256:6528d5a8cb6ced0fd10934eaacf979ec4e1d99e1fbaca1d886464a8821c3a2d2
+size 37228
