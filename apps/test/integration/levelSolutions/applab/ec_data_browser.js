7ee6d81fe0f283bcaa93cfca2c208c5b303311cd
David Bailey <davidsbailey@users.noreply.github.com>
2016-09-09 16:12:03 -0700

    add Data Browser level tests for add table/row/column and edit table/row

--- apps/test/integration/levelSolutions/applab/ec_data_browser.js
+++ apps/test/integration/levelSolutions/applab/ec_data_browser.js
@@ -67,6 +67,156 @@ module.exports = {
       },
     },
 
+    {
+      description: "Data Browser can add tables, columns and rows",
+      editCode: true,
+      useFirebase: true,
+
+      runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        tickWrapper.runOnAppTick(Applab, 2, function () {
+          // Overview
+          $("#dataModeButton").click();
+          const dataOverview = $('#dataOverview');
+          assert.equal(dataOverview.is(':visible'), true, 'dataOverview is visible');
+          let tableLink = dataOverview.find('a:contains(mytable)');
+          assert.equal(tableLink.length, 0, 'mytable link does not exist yet');
+
+          // Create a new table from the overview
+          const newTableInput = dataOverview.find('input');
+          ReactTestUtils.Simulate.change(newTableInput[0],
+            { target: { value: 'mytable' } });
+          const addTableButton = dataOverview.find('button:contains(Add)');
+          ReactTestUtils.Simulate.click(addTableButton[0]);
+          setTimeout(() => {
+            const dataTable = $('#dataTable');
+            assert.equal(dataTable.is(':visible'), true, 'dataTable is visible');
+
+            // rename initial column
+            const column1NameInput = dataTable.find('th > input[value="column1"]');
+            assert.equal(column1NameInput.is(':visible'), true, 'column1 name input is visible');
+            ReactTestUtils.Simulate.change(column1NameInput[0], { target: { value: 'firstname' } });
+            const enterKeyEvent = {key: "Enter", keyCode: 13, which: 13};
+            ReactTestUtils.Simulate.keyUp(column1NameInput[0], enterKeyEvent);
+            assert.equal(column1NameInput.is(':visible'), false, 'column1 name input is hidden');
+            assert.equal(dataTable.find('th > div:contains(firstname)').is(':visible'), true, 'column1 renamed to firstname');
+
+            // add new column
+            ReactTestUtils.Simulate.click($('#addColumnButton')[0]);
+            setTimeout(() => {
+              const column2NameInput = dataTable.find('th > input[value="column2"]');
+              assert.equal(column2NameInput.is(':visible'), true, 'column2 name input is visible');
+              ReactTestUtils.Simulate.change(column2NameInput[0], { target: { value: 'age' } });
+              ReactTestUtils.Simulate.keyUp(column2NameInput[0], enterKeyEvent);
+              assert.equal(dataTable.find('th > div:contains(age)').is(':visible'), true, 'column1 renamed to age');
+
+              // add new row
+              const addRow = $('#dataTable').find('tr:contains(Add Row)');
+              ReactTestUtils.Simulate.change(addRow.find('input')[0], { target: { value: 'bob' } });
+              ReactTestUtils.Simulate.change(addRow.find('input')[1], { target: { value: '8' } });
+              ReactTestUtils.Simulate.keyUp(addRow.find('input')[1], enterKeyEvent);
+              setTimeout(() => {
+                const dataRow1 = $('#dataTable').find('tr:contains(Edit)');
+                assert.equal(dataRow1.find('td')[0].innerHTML, '1', 'id cell value');
+                assert.equal(dataRow1.find('td')[1].innerHTML, '"bob"', 'firstname cell value');
+                assert.equal(dataRow1.find('td')[2].innerHTML, '8', 'age cell value');
+
+                // add another row
+                ReactTestUtils.Simulate.change(addRow.find('input')[0], { target: { value: 'charlie' } });
+                ReactTestUtils.Simulate.change(addRow.find('input')[1], { target: { value: '9' } });
+                ReactTestUtils.Simulate.keyUp(addRow.find('input')[1], enterKeyEvent);
+                setTimeout(() => {
+                  const dataRow2 = $('#dataTable').find('tr:contains(Edit)');
+                  assert.equal(dataRow2.find('td')[0].innerHTML, '1', 'id cell value');
+                  assert.equal(dataRow2.find('td')[1].innerHTML, '"bob"', 'firstname cell value');
+                  assert.equal(dataRow2.find('td')[2].innerHTML, '8', 'age cell value');
+
+                  Applab.onPuzzleComplete();
+                }, 100);
+              }, 100);
+            }, 100);
+          }, 100);
+        });
+      },
+      customValidator: function (assert) {
+        // No errors in output console
+        var debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, '');
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      },
+    },
+
+    {
+      description: "Data Browser can edit table rows",
+      editCode: true,
+      useFirebase: true,
+      xml:`
+        createRecord('mytable', {name:'Alice', age:7, male:false}, function () {
+          console.log('created record 1');
+          createRecord('mytable', {name:'Bob', age:8, male:true}, function () {
+            console.log('created record 2');
+          });
+        });`,
+
+      runBeforeClick: function (assert) {
+        // add a completion on timeout since this is a freeplay level
+        tickWrapper.runOnAppTick(Applab, 200, function () {
+          // Overview
+          $("#dataModeButton").click();
+          const dataOverview = $('#dataOverview');
+          assert.equal(dataOverview.is(':visible'), true, 'dataOverview is visible');
+          const tableLink = dataOverview.find('a:contains(mytable)');
+          assert.equal(tableLink.is(':visible'), true, 'table link is visible');
+
+          // view table
+          ReactTestUtils.Simulate.click(tableLink[0]);
+          const dataTable = $('#dataTable');
+          assert.equal(dataTable.is(':visible'), true, 'dataTable is visible');
+          const record1Row = dataTable.find('tr:contains(Alice)');
+          assert.equal(record1Row.is(':visible'), true, 'record 1 appears in the grid');
+          const record2Row = dataTable.find('tr:contains(Bob)');
+          assert.equal(record2Row.is(':visible'), true, 'record 2 appears in the grid');
+
+          // edit row
+          let editButton = record1Row.find('button:contains(Edit)');
+          assert.equal(editButton.is(':visible'), true, 'edit row button visible');
+          ReactTestUtils.Simulate.click(editButton[0]);
+          const saveButton = record1Row.find('button:contains(Save)');
+          editButton = record1Row.find('button:contains(Edit)');
+          assert.equal(editButton.is(':visible'), false, 'edit row button hidden');
+          assert.equal(saveButton.is(':visible'), true, 'save row button visible');
+          const ageInput = record1Row.find('input')[1];
+          assert.equal(ageInput.value, '7', 'age input cell value in edit row');
+          ReactTestUtils.Simulate.change(ageInput, { target: { value: '9' } });
+          const enterKeyEvent = {key: "Enter", keyCode: 13, which: 13};
+          ReactTestUtils.Simulate.keyUp(ageInput, enterKeyEvent);
+          setTimeout(() => {
+            editButton = record1Row.find('button:contains(Edit)');
+            assert.equal(editButton.is(':visible'), true, 'edit row button visible');
+            const ageCell = record1Row.find('td')[2];
+            assert.equal(ageCell.innerHTML, '9', 'age cell saved value');
+
+            Applab.onPuzzleComplete();
+          }, 100);
+        });
+      },
+      customValidator: function (assert) {
+        // No errors in output console
+        var debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, 'created record 1\ncreated record 2');
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      },
+    },
+
+
     // The data browser uses Firebase.off() somewhat broadly in order to stop
     // listening for data changes as you switch between data browser views. This
     // could potentially interfere with the listeners used by the onRecordEvent
@@ -123,34 +273,37 @@ module.exports = {
 
             // view other table
             ReactTestUtils.Simulate.click(addTableButton[0]);
-            recordRow = dataTable.find('tr:contains(Alice)');
-            assert.equal(recordRow.length, 0, 'no table rows in other table');
-
-            // create record 2
-            $('#createRecord').click();
             setTimeout(() => {
+              assert.equal(dataTable.is(':visible'), true, 'other table is visible');
               recordRow = dataTable.find('tr:contains(Alice)');
-              assert.equal(recordRow.length, 0, 'still no table rows in other table');
-              assert.equal(debugOutput.textContent, 'created record 1\ncreated record 2');
+              assert.equal(recordRow.length, 0, 'no table rows in other table');
 
-              // back to overview
-              ReactTestUtils.Simulate.click($('#tableBackToOverview')[0]);
-
-              // view mytable
-              ReactTestUtils.Simulate.click(tableLink[0]);
-              recordRow = dataTable.find('tr:contains(Alice)');
-              assert.equal(recordRow.length, 2, 'two table rows in mytable');
-              assert.equal(debugOutput.textContent, 'created record 1\ncreated record 2');
-
-              // create record 3
+              // create record 2
               $('#createRecord').click();
               setTimeout(() => {
                 recordRow = dataTable.find('tr:contains(Alice)');
-                assert.equal(recordRow.length, 3, 'three table rows in mytable');
-                assert.equal(debugOutput.textContent,
-                  'created record 1\ncreated record 2\ncreated record 3');
+                assert.equal(recordRow.length, 0, 'still no table rows in other table');
+                assert.equal(debugOutput.textContent, 'created record 1\ncreated record 2');
 
-                Applab.onPuzzleComplete();
+                // back to overview
+                ReactTestUtils.Simulate.click($('#tableBackToOverview')[0]);
+
+                // view mytable
+                ReactTestUtils.Simulate.click(tableLink[0]);
+                recordRow = dataTable.find('tr:contains(Alice)');
+                assert.equal(recordRow.length, 2, 'two table rows in mytable');
+                assert.equal(debugOutput.textContent, 'created record 1\ncreated record 2');
+
+                // create record 3
+                $('#createRecord').click();
+                setTimeout(() => {
+                  recordRow = dataTable.find('tr:contains(Alice)');
+                  assert.equal(recordRow.length, 3, 'three table rows in mytable');
+                  assert.equal(debugOutput.textContent,
+                    'created record 1\ncreated record 2\ncreated record 3');
+
+                  Applab.onPuzzleComplete();
+                }, 100);
               }, 100);
             }, 100);
           }, 100);
