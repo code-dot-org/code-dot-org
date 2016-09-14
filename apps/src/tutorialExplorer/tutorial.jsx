81b6d71f78a0a37927e92f8b958d63abd114fa18
Brendan Reville <brendan@code.org>
2016-09-14 10:47:16 -0700

    learn2016: use multiple files.

---
+++ apps/src/tutorialExplorer/tutorial.jsx
@@ -0,0 +1,79 @@
+/* A single tutorial shown in TutorialExplorer.  Displays a thumbnail image, name, and some extra information.
+ */
+
+import React from 'react';
+import FilterSet from './filterSet';
+import TutorialDetail from './tutorialDetail';
+import shapes from './shapes';
+import getTagString from './util';
+
+const styles = {
+  tutorialName: {
+    fontFamily: '"Gotham 5r", sans-serif',
+    fontSize: 15,
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden'
+  },
+  tutorialSub: {
+    fontFamily: '"Gotham 3r", sans-serif',
+    fontSize: 12,
+    paddingBottom: 20
+  }
+};
+
+const Tutorial = React.createClass({
+  propTypes: {
+    item: shapes.tutorial.isRequired
+  },
+
+  getInitialState: function () {
+    return {
+      showingDetail: false
+    };
+  },
+
+  tutorialClicked: function () {
+    this.setState({showingDetail: true});
+  },
+
+  tutorialDetailClosed: function () {
+    this.setState({showingDetail: false});
+  },
+
+  render() {
+    return (
+      <div>
+        <TutorialDetail
+          showing={this.state.showingDetail}
+          item={this.props.item}
+          closeClicked={this.tutorialDetailClosed}
+        />
+        <div
+          className="col-33"
+          style={{float: 'left', padding: 2}}
+        >
+          <div style={{padding: 5}}>
+            <div
+              style={{cursor: 'pointer'}}
+              onClick={this.tutorialClicked}
+            >
+              <img
+                src={this.props.item.image}
+                style={{width: '100%', height: 180}}
+              />
+              <div style={styles.tutorialName}>
+                {this.props.item.name}
+              </div>
+              <div style={styles.tutorialSub}>
+                {getTagString("grade", this.props.item.tags_grade)} | {getTagString("programming_language", this.props.item.tags_programming_language)}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+export default Tutorial;
