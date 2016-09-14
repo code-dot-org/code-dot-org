d1c03a8ab6bff39b315d48bf75508988f2a601d8
Caley Brock <caleybrock@Caleys-MacBook-Pro.local>
2016-09-14 12:52:05 -0700

    Test if selecting an animation on load works

--- apps/test/unit/gamelab/animationListModuleTest.js
+++ apps/test/unit/gamelab/animationListModuleTest.js
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux';
+import sinon from 'sinon';
 import reducer, {
     START_LOADING_FROM_SOURCE,
-    DONE_LOADING_FROM_SOURCE
+    DONE_LOADING_FROM_SOURCE,
+    setInitialAnimationList
 } from '@cdo/apps/gamelab/animationListModule';
+import animationTab from '@cdo/apps/gamelab/AnimationTab/animationTabModule';
 import {EMPTY_IMAGE} from '@cdo/apps/gamelab/constants';
 import {createStore} from '@cdo/apps/redux';
 import {expect} from '../../util/configuredChai';
@@ -82,4 +85,63 @@ describe('animationListModule', function () {
       expect(store.getState().animationList.propsByKey[key].sourceSize).not.to.be.undefined;
     });
   });
+
+  describe('action: set initial animationList', function () {
+    let oldWindowDashboard, server;
+    beforeEach(function () {
+      oldWindowDashboard = window.dashboard;
+      window.dashboard = {
+        project: {
+          getCurrentId() {return '';}
+        }
+      };
+
+      server = sinon.fakeServer.create();
+      server.respondWith('imageBody');
+    });
+
+    afterEach(function () {
+      server.restore();
+      window.dashboard = oldWindowDashboard;
+    });
+
+    it('when animationList has 1 item, selectedAnimation should be the animation', function () {
+      const key0 = 'anim0';
+      let animationList = {
+        orderedKeys: [key0],
+        propsByKey: {
+          [key0]: {
+            name: 'New animation',
+            sourceUrl: null,
+            frameSize: {x: 100, y: 100},
+            frameCount: 1,
+            looping: true,
+            frameDelay: 4,
+            version: null,
+            loadedFromSource: true,
+            saved: false,
+            blob: null,
+            dataURI: null,
+            hasNewVersionThisSession: false
+          }
+        }
+      };
+
+      let store = createStore(combineReducers({animationList: reducer, animationTab}), {});
+      store.dispatch(setInitialAnimationList(animationList));
+      expect(store.getState().animationTab.selectedAnimation).to.equal(key0);
+    });
+
+    it('when animationList has 0 items, selectedAnimation should be null', function () {
+      const key0 = 'anim0';
+      let animationList = {
+        orderedKeys: [],
+        propsByKey: {}
+      };
+      let store = createStore(combineReducers({animationList: reducer, animationTab}), {});
+      store.dispatch(setInitialAnimationList(animationList));
+      expect(store.getState().animationTab.selectedAnimation).to.be.null;
+    });
+  });
+
 });
