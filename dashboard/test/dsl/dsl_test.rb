df55ceb9b435cb0a2bba84669d1b027a6fff770a
Josh Lory <josh.lory@code.org>
2016-09-13 18:08:25 -0700

    Update rake tests

--- dashboard/test/dsl/dsl_test.rb
+++ dashboard/test/dsl/dsl_test.rb
@@ -46,10 +46,10 @@ class DslTest < ActiveSupport::TestCase
       peer_reviews_to_complete: nil
     }
 
-    i18n_expected = {'en' => {'data' => {'script' => {'name' => {'test' => {
+    i18n_expected = {'en' => {'data' => {'script' => {'name' => {'test' => {'stage' => {
         'Stage1' => 'Stage1',
         'Stage2' => 'Stage2'
-    }}}}}}
+    }}}}}}}
     assert_equal expected, output
     assert_equal i18n_expected, i18n
   end
