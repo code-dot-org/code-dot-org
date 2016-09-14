9475fd203b8b975fa77f6527c604532f712970df
Andrew Oberhardt <Andrew@Code.org>
2016-09-14 14:24:03 -0700

    PR feedback: Added missing countries and moved latam country list to lib/ and moved query_from_list to mailing-common

--- bin/mail/mailing-common/mailing-list-utils.rb
+++ bin/mail/mailing-common/mailing-list-utils.rb
@@ -189,3 +189,18 @@ def query_all_emails_at_domain(domain)
     ALL.merge! results
   end
 end
+
+# Accepts a hash of SOLR queries in the form: {query_name: query}
+# Executes each query in order, and returns the resulting contacts, merged and deduped.
+def query_from_list(queries)
+  {}.tap do |contacts|
+    # Reverse sort to start with more recent years.
+    # query_subscribed_contacts dedupes by rejecting duplicate emails.
+    queries.keys.sort.reverse_each do |query_name|
+      query = queries[query_name]
+      new_contacts = query_subscribed_contacts(q: query)
+      puts "#{query_name}: #{new_contacts.count} contacts"
+      contacts.merge! new_contacts
+    end
+  end
+end
