7d1468241d74382aa4f548df1a58204d9ea88008
Brad Buchanan <brad@code.org>
2016-09-15 16:19:27 -0700

    Add sourceSize to manifest
    Make manifest generation script read source size for images and include it in the manifest.

--- tools/scripts/rebuildAnimationLibraryManifest.rb
+++ tools/scripts/rebuildAnimationLibraryManifest.rb
@@ -15,6 +15,9 @@
 #
 # See also: The Animation Library Tech Spec (requires login):
 # https://docs.google.com/document/d/18-LVuvKd0jKTUiGo5GYReUWM5oFWCyKRyEQURJ5HCOM/edit
+#
+# TODO: Optimize: Read existing manifest and don't do full object reads for
+#       entries whose modify date hasn't changed.
 require 'aws-sdk'
 require 'optparse'
 require_relative '../../deployment'
@@ -62,6 +65,13 @@ class ManifestBuilder
       metadata = JSON.parse(json_response.body.read)
       # TODO: Validate metadata, ensure it has everything it needs
       metadata['version'] = objects['png'].object.version_id
+
+      # Populate sourceSize if not already present
+      unless metadata.key?('sourceSize')
+        png_body = objects['png'].object.get.body.read
+        metadata['sourceSize'] = dimensions_from_png(png_body)
+      end
+
       animation_metadata_by_name[name] = metadata
       verbose <<-EOS
 #{bold name} @ #{metadata['version']}
@@ -125,6 +135,29 @@ class ManifestBuilder
   def warn(s)
     puts(s)
   end
+end
+
+# Given the complete body of a PNG file, returns hash of source dimensions:
+# {"x": _, "y": _}
+def dimensions_from_png(png_body)
+  # Read the first eight bytes of the IHDR Chunk, which must always be the
+  # first chunk of the PNG file.
+  #
+  # PNG Header takes 8 bytes (0x00-0x07)
+  # IHDR chunk length takes 4 bytes (0x08-0x0b)
+  # IHDR chunk type code takes 4 bytes (0x0c-0x0f)
+  # IHDR chunk data begins at 0x10
+  #
+  # The IHDR chunk begins with
+  # 4 bytes for width (0x10-0x13) followed by
+  # 4 bytes for height (0x14-0x17)
+  #
+  # PNG File Structure
+  # http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html
+  # IHDR Chunk Layout
+  # http://www.libpng.org/pub/png/spec/1.2/PNG-Chunks.html#C.IHDR
+  dimensions = png_body[0x10..0x18].unpack('NN')
+  {'x': dimensions[0], 'y': dimensions[1]}
 end
 
 # Utility to strip consistent leading whitespace from heredoc strings, allowing
