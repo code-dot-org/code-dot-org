26e1da5a296613f0910792a2ef1bba92e3f5cc72
Brad Buchanan <brad@code.org>
2016-09-16 14:18:26 -0700

    Extract common CLI utils

--- tools/scripts/rebuildAnimationLibraryManifest.rb
+++ tools/scripts/rebuildAnimationLibraryManifest.rb
@@ -23,6 +23,8 @@ require 'ruby-progressbar'
 require 'optparse'
 require 'parallel'
 require_relative '../../deployment'
+require_relative '../../lib/cdo/cdo_cli'
+include CdoCli
 
 DEFAULT_S3_BUCKET = 'cdo-animation-library'
 DEFAULT_OUTPUT_FILE = 'apps/src/gamelab/animationLibrary.json'
@@ -178,34 +180,6 @@ def dimensions_from_png(png_body)
   dimensions = png_body[0x10..0x18].unpack('NN')
   {'x': dimensions[0], 'y': dimensions[1]}
 end
-
-# Utility to strip consistent leading whitespace from heredoc strings, allowing
-# you to format your code more readably.
-#
-# Usage:
-#   <<-DOC.unindent
-#     Some text or other that I want to start in column 1.
-#         An actually indented line.
-#     This line still unindented.
-#   DOC
-#
-# from http://stackoverflow.com/a/5638187/5000129
-class String
-  # Strip leading whitespace from each line that is the same as the
-  # amount of whitespace on the least-indented line of the string.
-  def unindent
-    gsub /^#{scan(/^[ \t]+/).min_by(&:length)}/, ''
-  end
-end
-
-# Terminal style utilities for fashionable output
-def stylize(text, color_code)
-  "\e[#{color_code}m#{text}\e[0m"
-end
-
-def bold(text); stylize(text, 1); end
-def dim(text); stylize(text, 2); end
-def underline(text); stylize(text, 4); end
 
 # Parse command-line options and then start the rebuild process
 options = {}
