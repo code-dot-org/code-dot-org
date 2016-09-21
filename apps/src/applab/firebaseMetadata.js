fd133542bac084bb907df296404f83e1ef562d84
David Bailey <davidsbailey@users.noreply.github.com>
2016-09-21 13:43:41 -0700

    extract getColumnNamesFromRecords from getColumnNames

--- apps/src/applab/firebaseMetadata.js
+++ apps/src/applab/firebaseMetadata.js
@@ -2,10 +2,8 @@
 
 import Firebase from 'firebase';
 
-export function getColumnNames(records, columns) {
-  // Make sure 'id' is the first column.
-  let columnNames = ['id'];
-
+function getColumnNamesFromRecords(records) {
+  const columnNames = ['id'];
   Object.keys(records).forEach(id => {
     const record = JSON.parse(records[id]);
     Object.keys(record).forEach(columnName => {
@@ -14,6 +12,16 @@ export function getColumnNames(records, columns) {
       }
     });
   });
+  return columnNames;
+}
+
+/**
+ * @param {Array} records Array of JSON-encoded records.
+ * @param {string} columns Array of column names.
+ */
+export function getColumnNames(records, columns) {
+  // Make sure 'id' is the first column.
+  const columnNames = getColumnNamesFromRecords(records);
 
   columns.forEach(columnName => {
     if (columnNames.indexOf(columnName) === -1) {
