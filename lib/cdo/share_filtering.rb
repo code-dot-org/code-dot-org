24935854939400b07a131ced3693c5f27187d274
Asher Kach <asher@code.org>
2016-09-21 07:08:24 -0500

    Flattens code for readability.

--- lib/cdo/share_filtering.rb
+++ lib/cdo/share_filtering.rb
@@ -28,15 +28,18 @@ module ShareFiltering
     xml_tag_regexp = /<[^>]*>/
     program_tags_removed = program.gsub(xml_tag_regexp, "\n")
 
-    if email = RegexpUtils.find_potential_email(program_tags_removed)
-      return ShareFailure.new(FailureType::EMAIL, email)
-    elsif street_address = Geocoder.find_potential_street_address(program_tags_removed)
-      return ShareFailure.new(FailureType::ADDRESS, street_address)
-    elsif phone_number = RegexpUtils.find_potential_phone_number(program_tags_removed)
-      return ShareFailure.new(FailureType::PHONE, phone_number)
-    elsif expletive = WebPurify.find_potential_profanity(program_tags_removed, ['en', locale])
-      return ShareFailure.new(FailureType::PROFANITY, expletive)
-    end
+    email = RegexpUtils.find_potential_email(program_tags_removed)
+    return ShareFailure.new(FailureType::EMAIL, email) if email
+
+    street_address = Geocoder.find_potential_street_address(program_tags_removed)
+    return ShareFailure.new(FailureType::ADDRESS, street_address) if street_address
+
+    phone_number = RegexpUtils.find_potential_phone_number(program_tags_removed)
+    return ShareFailure.new(FailureType::PHONE, phone_number) if phone_number
+
+    expletive = WebPurify.find_potential_profanity(program_tags_removed, ['en', locale])
+    return ShareFailure.new(FailureType::PROFANITY, expletive) if expletive
+
     nil
   end
 
