047841d6f692e8ab4b97a12499bfdcca7030cf22
Mehal Shah <mehal@code.org>
2016-09-22 11:30:15 -0700

    Checkpoint

---
+++ apps/src/code-studio/pd/workshop_dashboard/survey_results.jsx
@@ -0,0 +1,16 @@
+/** Workshop Survey page. Displays survey results. ...*/
+import React from 'react';
+
+const SurveyResults = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  render() {
+    return (
+      <div> Whatever </div>
+    );
+  }
+});
+
+export default SurveyResults;
