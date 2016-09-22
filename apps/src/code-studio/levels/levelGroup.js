28b3ef4265bfd35cf5a90fdaaa338a7c581b1e60
Brent Van Minnen <bjvanminnen@gmail.com>
2016-09-22 12:42:46 -0700

    getResult doesnt live on window namespace

--- apps/src/code-studio/levels/levelGroup.js
+++ apps/src/code-studio/levels/levelGroup.js
@@ -2,6 +2,7 @@
 
 import $ from 'jquery';
 import throttle from 'lodash/throttle';
+import * as codeStudioLevels from './codeStudioLevels';
 require('./multi.js');
 require('./textMatch.js');
 var saveAnswers = require('./saveAnswers.js').saveAnswers;
@@ -19,7 +20,7 @@ window.initLevelGroup = function (
   // edge").  Any pending throttled calls are cancelled when we go to a new page
   // and save for that reason.
 
-  window.getResult = getResult;
+  codeStudioLevels.registerGetResult(getAggregatedResults);
 
   function submitSublevelResults(completion, subLevelIdChanged) {
     var levels = window.levelGroup.levels;
@@ -69,14 +70,15 @@ window.initLevelGroup = function (
       submitSublevelResults(saveAnswers, subLevelId);
     }, 20 * 1000);
 
-  var lastResponse = window.getResult().response;
+  var lastResponse = getAggregatedResults().response;
 
   window.levelGroup.answerChangedFn = function (levelId, saveThisAnswer) {
     if (!saveThisAnswer) {
       // Ignore typing events before focus change (when commit will be true)
       return;
     }
-    var currentResponse = window.getResult().response;
+    var currentResponse = getAggregatedResults().response;
+    // TODO - this always be true?
     if (lastResponse !== currentResponse) {
       throttledSaveAnswers(levelId);
     }
@@ -91,7 +93,7 @@ window.initLevelGroup = function (
    *  "2007": {"result": "-1", "valid": false},
    *  "1939": {"result": "2,1", "valid": true}}
    */
-  function getResult() {
+  function getAggregatedResults() {
     // Add any new results to the existing lastAttempt results.
     var levels = window.levelGroup.levels;
     Object.keys(levels).forEach(function (levelId) {
@@ -112,12 +114,12 @@ window.initLevelGroup = function (
     var showConfirmationDialog = "levelgroup-submit-" + completeString;
 
     return {
-      "response": encodeURIComponent(JSON.stringify(lastAttempt)),
-      "result": true,
-      "errorType": null,
-      "submitted": window.appOptions.level.submittable,
-      "showConfirmationDialog": showConfirmationDialog,
-      "beforeProcessResultsHook": submitSublevelResults
+      response: encodeURIComponent(JSON.stringify(lastAttempt)),
+      result: true,
+      errorType: null,
+      submitted: window.appOptions.level.submittable,
+      showConfirmationDialog: showConfirmationDialog,
+      beforeProcessResultsHook: submitSublevelResults
     };
   }
 
