5e8e67aab27b974d0828e23c1095e55f62b86cad
Andrew Oberhardt <Andrew@Code.org>
2016-09-29 15:18:23 -0700

    Generalize WorkshopTableLoader

--- apps/src/code-studio/pd/workshop_dashboard/survey_results.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/survey_results.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import WorkshopTableLoader from './components/workshop_table_loader';
+import SurveyResultsHeader from './components/survey_results_header';
 
 const SurveyResults = React.createClass({
   render() {
     return (
       <div>
-        <WorkshopTableLoader
-          queryUrl="/api/v1/pd/workshops/?state=Ended"
-          componentToLoad="surveyResults"
-        />
+        <WorkshopTableLoader queryUrl="/api/v1/pd/workshops/?state=Ended">
+          <SurveyResultsHeader />
+        </WorkshopTableLoader>
       </div>
     );
   }
