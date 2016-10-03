537605ce03da0e1dccafd0e50210f7f20818ec08
Brent Van Minnen <bjvanminnen@gmail.com>
2016-10-03 14:49:40 -0700

    dont use window.dashboard.codeStudioLevels where we can import

--- apps/src/code-studio/levels/multi.js
+++ apps/src/code-studio/levels/multi.js
@@ -1,6 +1,6 @@
 /* global Dialog, appOptions, CDOSounds */
 import $ from 'jquery';
-import { registerGetResult } from './codeStudioLevels';
+import { registerGetResult, onAnswerChanged } from './codeStudioLevels';
 
 var Multi = window.Multi = function (levelId, id, app, standalone, numAnswers, answers, answersFeedback, lastAttemptString, containedMode) {
 
@@ -63,7 +63,7 @@ Multi.prototype.choiceClicked = function (button) {
 
   this.clickItem(index);
 
-  window.dashboard.codeStudioLevels.onAnswerChanged(this.levelId, true);
+  onAnswerChanged(this.levelId, true);
 };
 
 
