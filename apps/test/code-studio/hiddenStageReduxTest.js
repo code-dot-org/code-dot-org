8ae38168d1208c8847001143f22d14234dd8ed89
Brian Jordan <bcjordan@gmail.com>
2016-10-03 11:03:55 -0400

    Revert "Hidden stages"

--- apps/test/code-studio/hiddenStageReduxTest.js
+++ apps/test/code-studio/hiddenStageReduxTest.js
@@ -2,7 +2,6 @@ import { assert } from 'chai';
 import { createStore } from '@cdo/apps/redux';
 import { combineReducers } from 'redux';
 import sinon from 'sinon';
-import Immutable from 'immutable';
 
 import reducer, { toggleHidden, getHiddenStages } from '@cdo/apps/code-studio/hiddenStageRedux';
 import experiments from '@cdo/apps/experiments';
@@ -41,132 +40,57 @@ describe('hiddenStage reducer tests', () => {
     xhr.restore();
   });
 
-  it('initializes with server results for student after calling getHiddenStages', () => {
+  it('initializes with server results after calling getHiddenStages', () => {
     const state = store.getState().hiddenStage;
-    assert.deepEqual(state.toJS(), {
-      initialized: false,
-      bySection: {}
+    assert.deepEqual(state, {
+      initialized: false
     });
 
     const action = getHiddenStages('scriptName');
     store.dispatch(action);
 
-    lastRequest.respond(200, { "Content-Type": "application/json" },
-      JSON.stringify([123, 456]));
+    lastRequest.respond(200, { "Content-Type": "application/json" }, JSON.stringify([123, 456]));
 
     const nextState = store.getState().hiddenStage;
-    assert.deepEqual(nextState.toJS(), {
+    assert.deepEqual(nextState, {
       initialized: true,
-      bySection: {
-        STUDENT: {
-          123: true,
-          456: true
-        }
-      }
-    });
-  });
-
-  it('initializes with server results for teacher after calling getHiddenStages', () => {
-    const state = store.getState().hiddenStage;
-    assert.deepEqual(state.toJS(), {
-      initialized: false,
-      bySection: {}
-    });
-
-    const action = getHiddenStages('scriptName');
-    store.dispatch(action);
-
-    lastRequest.respond(200, { "Content-Type": "application/json" },
-      JSON.stringify({
-        10: [123, 456],
-        11: [123]
-      })
-    );
-
-    const nextState = store.getState().hiddenStage;
-    assert.deepEqual(nextState.toJS(), {
-      initialized: true,
-      bySection: {
-        10: {
-          123: true,
-          456: true
-        },
-        11: {
-          123: true
-        }
-      }
+      123: true,
+      456: true
     });
   });
 
   it('can toggle hidden state', () => {
     const state = store.getState().hiddenStage;
-    assert.deepEqual(state.toJS(), {
-      initialized: false,
-      bySection: {}
+    assert.deepEqual(state, {
+      initialized: false
     });
 
     let action, nextState;
 
-    // hide a stage
-    action = toggleHidden('scriptName', 10, 123, true);
+    action = toggleHidden('scriptName', 123, true);
     store.dispatch(action);
     nextState = store.getState().hiddenStage;
-    assert.deepEqual(nextState.toJS(), {
+    assert.deepEqual(nextState, {
       initialized: false,
-      bySection: {
-        10: {
-          123: true
-        }
-      }
+      123: true
     });
 
-    // hide the same stage in a different section
-    action = toggleHidden('scriptName', 11, 123, true);
+    action = toggleHidden('scriptName', 123, false);
     store.dispatch(action);
     nextState = store.getState().hiddenStage;
-    assert.deepEqual(nextState.toJS(), {
+    assert.deepEqual(nextState, {
       initialized: false,
-      bySection: {
-        10: {
-          123: true
-        },
-        11: {
-          123: true
-        }
-      }
+      123: false
     });
 
-    // unhide the stage in one section
-    action = toggleHidden('scriptName', 10, 123, false);
+    action = toggleHidden('scriptName', 345, true);
     store.dispatch(action);
     nextState = store.getState().hiddenStage;
-    assert.deepEqual(nextState.toJS(), {
+    console.log(nextState);
+    assert.deepEqual(nextState, {
       initialized: false,
-      bySection: {
-        10: {
-          123: false
-        },
-        11: {
-          123: true
-        }
-      }
-    });
-
-    // hide another stage
-    action = toggleHidden('scriptName', 10, 345, true);
-    store.dispatch(action);
-    nextState = store.getState().hiddenStage;
-    assert.deepEqual(nextState.toJS(), {
-      initialized: false,
-      bySection: {
-        10: {
-          123: false,
-          345: true
-        },
-        11: {
-          123: true
-        }
-      }
+      123: false,
+      345: true
     });
   });
 });
