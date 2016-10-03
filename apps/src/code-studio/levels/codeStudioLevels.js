6c19cb757c39ffc29c40ec1794bcd5145c0e325f
Brent Van Minnen <bjvanminnen@gmail.com>
2016-10-03 14:45:06 -0700

    update comments and simplify lockContainedLevelAnswers

--- apps/src/code-studio/levels/codeStudioLevels.js
+++ apps/src/code-studio/levels/codeStudioLevels.js
@@ -101,17 +101,24 @@ export function getLevelIds() {
   return Object.keys(levelGroup);
 }
 
-// TODO - There should be only one contiained level. In the future, I will simplify
-// these next two methods to reflect that
+/**
+ * Lock the answer fo the contained level
+ */
 export function lockContainedLevelAnswers() {
   const levelIds = getLevelIds();
-  levelIds.forEach(levelId => getLevel(levelId).lockAnswers());
+  if (levelIds.length !== 1) {
+    throw `Expected exactly one contained level. Got ${levelIds.length}`;
+  }
+  getLevel(levelIds[0]).lockAnswers();
 }
 
+/**
+ * Get the result of the single contained level.
+ */
 export function getContainedLevelResult() {
   const levelIds = getLevelIds();
   if (levelIds.length !== 1) {
-    throw "Exactly one contained level result is currently required.";
+    throw `Expected exactly one contained level. Got ${levelIds.length}`;
   }
 
   const level = getLevel(levelIds[0]);
@@ -124,6 +131,9 @@ export function getContainedLevelResult() {
   };
 }
 
+/**
+ * @returns {boolean} True if the contained level has a valid result.
+ */
 export function hasValidContainedLevelResult() {
   return getContainedLevelResult().result.valid;
 }
