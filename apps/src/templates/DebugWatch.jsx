98e2c1e04fc98887dd12d5b5159f66fa669133eb
Brian Jordan <bcjordan@gmail.com>
2016-10-05 18:51:18 -0400

    Use original watcher DOM/CSS [WIP]

--- apps/src/templates/DebugWatch.jsx
+++ apps/src/templates/DebugWatch.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Immutable from 'immutable';
 import {connect} from 'react-redux';
+import i18n from '@cdo/locale';
 
 /**
  * A "watchers" window for our debugger.
@@ -15,11 +16,27 @@ const DebugWatch = React.createClass({
     let classes = 'debug-watch';
     return (
         <div id="debug-watch" className={classes}>
-          <ul>
           {
-              this.props.watchedExpressions.map(wv => <li key={wv.get('uuid')}>{wv.get('expression')}: {wv.get('lastValue')}</li>)
-          }
-          </ul>
+              this.props.watchedExpressions.map(wv => {
+                  const varName = wv.get('expression');
+                  const varValue = wv.get('lastValue');
+                  return (
+                  <div className="watch-expression" key={wv.get('uuid')}>
+                    <span
+                        className="watch-variable">{varName}</span>
+                    <span
+                        className="watch-separator">: </span>
+                    {varValue instanceof Error &&
+                    <span
+                        className="watch-value watch-unavailable">{i18n.debugWatchNotAvailable()}</span>
+                        }
+                    {(!varValue instanceof Error) &&
+                    <span className="watch-value">{varValue}</span>
+                        }
+                  </div>
+                      );
+                  })
+              }
         </div>
     );
   }
