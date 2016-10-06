1aef991d600df2d61bc9573bc307fc3cbe6eab06
Brian Jordan <bcjordan@gmail.com>
2016-10-06 12:15:01 -0400

    Extract value renderer helper. Fix uuid. Fix bad value rendering.

--- apps/src/templates/DebugWatch.jsx
+++ apps/src/templates/DebugWatch.jsx
@@ -12,6 +12,39 @@ const DebugWatch = React.createClass({
     watchedExpressions: React.PropTypes.instanceOf(Immutable.List)
   },
 
+  // http://stackoverflow.com/a/7390612
+  nonValueDescriptor(obj) {
+    return {}.toString.call(obj).split(' ')[1].slice(0, -1).toLowerCase();
+  },
+
+  /**
+   * Gets text to display for given value
+   * @param obj
+   * @returns {*}
+   */
+  renderValue(obj) {
+    const descriptor = this.nonValueDescriptor(obj);
+    const isError = obj instanceof Error;
+
+    if (isError) {
+      return <span
+        className="watch-value watch-unavailable">{i18n.debugWatchNotAvailable()}</span>;
+    }
+
+    switch(descriptor) {
+      case 'null':
+      case 'undefined':
+        return <span className="watch-value">{descriptor}</span>;
+      case 'regexp':
+        return <span className="watch-value">[regexp]</span>;
+      case 'function':
+        // [function MyFunctionName]
+        return <span className="watch-value">{`[${obj.toString().match(/(.*)\(/)[1]}]`}</span>;
+      default:
+        return <span className="watch-value">{obj.toString()}</span>;
+    }
+  },
+
   render() {
     let classes = 'debug-watch';
     return (
@@ -21,18 +54,12 @@ const DebugWatch = React.createClass({
                   const varName = wv.get('expression');
                   const varValue = wv.get('lastValue');
                   return (
-                  <div className="watch-expression" key={wv.get('uuid')}>
+                  <div className="debug-watch-item" key={wv.get('uuid')}>
                     <span
                         className="watch-variable">{varName}</span>
                     <span
                         className="watch-separator">: </span>
-                    {varValue instanceof Error &&
-                    <span
-                        className="watch-value watch-unavailable">{i18n.debugWatchNotAvailable()}</span>
-                        }
-                    {(!varValue instanceof Error) &&
-                    <span className="watch-value">{varValue}</span>
-                        }
+                    {this.renderValue(varValue)}
                   </div>
                       );
                   })
