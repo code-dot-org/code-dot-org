fe29b02c079e1eb8c9e178ef992cafe94285003d
Brent Van Minnen <bjvanminnen@gmail.com>
2016-10-13 14:36:07 -0700

    remove dead code

--- apps/src/code-studio/utils.js
+++ apps/src/code-studio/utils.js
@@ -6,22 +6,6 @@ module.exports = {
    * @param name {string=} Optionally pull a specific param.
    * @return {object|string} Hash of params, or param string if `name` is specified.
    */
-  // TODO - validate this doesnt break existing usages
-  // queryParams: function (name) {
-  //   var pairs = location.search.substr(1).split('&');
-  //   var params = {};
-  //   pairs.forEach(function (pair) {
-  //     var split = pair.split('=');
-  //     if (split.length === 2) {
-  //       params[split[0]] = split[1];
-  //     }
-  //   });
-  //
-  //   if (name) {
-  //     return params[name];
-  //   }
-  //   return params;
-  // },
   queryParams(name) {
     const parsed = queryString.parse(window.location.search);
     if (name) {
