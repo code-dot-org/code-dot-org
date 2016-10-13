57f5fc2344e428950c781efc487bf9145c463092
ewjordan <ewjordan@gmail.com>
2016-10-13 15:33:23 -0700

    Add basic tests to backup pruning script

--- tools/scripts/prune-rds-backups.js
+++ tools/scripts/prune-rds-backups.js
@@ -6,7 +6,6 @@ var AWS = require('aws-sdk');
 //var credentials = new AWS.SharedIniFileCredentials({profile: 'rds-snapshot-deleter'});
 //AWS.config.credentials = credentials;
 
-var rds = new AWS.RDS();
 var numberOfSnapshotsToKeep = 5;
 var identifier = 'production';
 
@@ -22,16 +21,9 @@ var identifier = 'production';
  * is idempotent, but once a day is fine, that's how often backups are backed up.
  */
 exports.handler = function (event, context, callback) {
-    // In this particular situation, we know that there will never be very many
-    // snapshots, so we don't filter anything at the API level
-    var params = {};
-    rds.describeDBSnapshots(params, function (err, data) {
-        var snapshots = data.DBSnapshots;
-        var snapshotsToDelete = getSnapshotsToDelete(snapshots);
-        console.log('Deleting ' + snapshotsToDelete.length + ' snapshots');
-        deleteSnapshots(snapshotsToDelete);
-        callback(null, "success");
-    });
+    var test = event['test'] === true;
+
+    var rds = new AWS.RDS();
 
     // Delete each of the snapshots in the list
     var deleteSnapshots = function (snapshots) {
@@ -74,4 +66,154 @@ exports.handler = function (event, context, callback) {
         var filteredAndSorted = filtered.sort(snapshotSortFunction);
         return getAllButNewestNSnapshots(numberOfSnapshotsToKeep, filteredAndSorted);
     };
+
+    /************* Tests *************/
+
+    /*
+    To run, test the Lambda function with the following input:
+     {
+        "test": true
+     }
+    */
+
+    var runTests = function () {
+        console.log("Running tests");
+        testSnapshotFilter();
+        testGetSnapshotsToDelete();
+    };
+
+    var assertTrue = function (boolValue, message) {
+        if (!boolValue) {
+            console.log("Failed test: " + message);
+            callback(message);
+        } else {
+            console.log("Passed test: " + message);
+        }
+    };
+
+    var testGetSnapshotsToDelete = function () {
+        var snapshots = [
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-0',
+                'Status': 'available',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(100)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-1',
+                'Status': 'available',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(12)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-2',
+                'Status': 'available',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(15)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-3',
+                'Status': 'available',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(13)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-4',
+                'Status': 'available',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(200)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-retain',
+                'Status': 'available',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(1)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-skip',
+                'Status': 'çreating',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(2)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-skip-2',
+                'Status': 'available',
+                'SnapshotType': 'automatic',
+                'SnapshotCreateTime': new Date(3)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-5',
+                'Status': 'available',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(300)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-6',
+                'Status': 'available',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(250)
+            },
+            {
+                'DBSnapshotIdentifier': 'production-snapshot-7',
+                'Status': 'available',
+                'SnapshotType': 'manual',
+                'SnapshotCreateTime': new Date(20)
+            }
+        ];
+        var expected = [
+            'production-snapshot-2',
+            'production-snapshot-3',
+            'production-snapshot-1'
+        ];
+        var toDelete = getSnapshotsToDelete(snapshots);
+        var deleteCount = toDelete.length;
+        for (var i = 0; i < deleteCount; ++i) {
+            assertTrue(toDelete[i].DBSnapshotIdentifier === expected[i],
+                'Expected ' + expected[i] + ', got ' + toDelete[i].DBSnapshotIdentifier
+            );
+        }
+        assertTrue(deleteCount === expected.length, 'Expected ' + expected.length + ' elements, got ' + deleteCount);
+    };
+
+    var testSnapshotFilter = function () {
+        var s = {
+            'DBSnapshotIdentifier': 'production-snapshot-0',
+            'Status': 'available',
+            'SnapshotType': 'manual'
+        };
+        assertTrue(snapshotFilterFunction(s), "Filter should accept " + s.DBSnapshotIdentifier);
+        s = {
+            'DBSnapshotIdentifier': 'production-snapshot-0-retain',
+            'Status': 'available',
+            'SnapshotType': 'manual'
+        };
+        assertTrue(!snapshotFilterFunction(s), "Filter should reject snapshots marked 'retain'");
+        s = {
+            'DBSnapshotIdentifier': 'production-snapshot-0',
+            'Status': 'creating',
+            'SnapshotType': 'manual'
+        };
+        assertTrue(!snapshotFilterFunction(s), "Filter should reject snapshots that aren't 'available'");
+        s = {
+            'DBSnapshotIdentifier': 'production-snapshot-0',
+            'Status': 'available',
+            'SnapshotType': 'automatic'
+        };
+        assertTrue(!snapshotFilterFunction(s), "Filter should reject automatic snapshots");
+    };
+
+    if (test) {
+        runTests();
+    } else {
+        // In this particular situation, we know that there will never be very many
+        // snapshots, so we don't filter anything at the API level
+        var params = {};
+        rds.describeDBSnapshots(params, function (err, data) {
+            var snapshots = data.DBSnapshots;
+            var snapshotsToDelete = getSnapshotsToDelete(snapshots);
+            console.log('Deleting ' + snapshotsToDelete.length + ' snapshots');
+            deleteSnapshots(snapshotsToDelete);
+            callback(null, "success");
+        });
+    }
 };
