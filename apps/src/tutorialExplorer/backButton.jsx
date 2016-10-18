0b7ed4563c7ed2dd6eb5376b52389a7a38dd4ad9
Brendan Reville <brendan@code.org>
2016-10-18 11:41:28 -0700

    learn2016: use stateless function components for buttons.

--- apps/src/tutorialExplorer/backButton.jsx
+++ apps/src/tutorialExplorer/backButton.jsx
@@ -3,18 +3,14 @@
 
 import React from 'react';
 
-const BackButton = React.createClass({
-  render() {
-    return (
-      <a href="/learn2016">
-        <button style={{marginTop: 7, marginBottom: 10}}>
-          <i className="fa fa-arrow-left" aria-hidden={true}/>
-          &nbsp;
-          Back to all tutorials
-        </button>
-      </a>
-    );
-  }
-});
+const BackButton = (props) => (
+  <a href="/learn2016">
+    <button style={{marginTop: 7, marginBottom: 10}}>
+      <i className="fa fa-arrow-left" aria-hidden={true}/>
+      &nbsp;
+      Back to all tutorials
+    </button>
+  </a>
+);
 
 export default BackButton;
