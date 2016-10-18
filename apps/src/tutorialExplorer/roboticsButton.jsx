66fe513de9223183fbf081092baf5c7410ff8a07
Brendan Reville <brendan@code.org>
2016-10-18 11:41:28 -0700

    learn2016: use stateless function components for buttons.

--- apps/src/tutorialExplorer/roboticsButton.jsx
+++ apps/src/tutorialExplorer/roboticsButton.jsx
@@ -3,14 +3,10 @@
 
 import React from 'react';
 
-const RoboticsButton = React.createClass({
-  render() {
-    return (
-      <a href="/learn2016/robotics">
-        <img src="/images/learn/robotics-link.png" style={{marginTop: 10}}/>
-      </a>
-    );
-  }
-});
+const RoboticsButton = (props) => (
+  <a href="/learn2016/robotics">
+    <img src="/images/learn/robotics-link.png" style={{marginTop: 10}}/>
+  </a>
+);
 
 export default RoboticsButton;
