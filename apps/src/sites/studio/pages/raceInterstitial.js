4fa489f4b5b0698b1d59a7bab9e0dd178ec11a33
ewjordan <ewjordan@gmail.com>
2016-10-21 15:25:47 -0700

    Clear and disable other selections if user chooses "Prefer not to say" for race

--- apps/src/sites/studio/pages/raceInterstitial.js
+++ apps/src/sites/studio/pages/raceInterstitial.js
@@ -4,11 +4,28 @@ import experiments from '@cdo/apps/experiments';
 $(document).ready(function () {
     $("#edit_user").on('change', function (event) {
         var shouldEnableSubmit = false;
-        $('.race-checkbox').each(function (i) {
+        var optOutSelected = false;
+        var raceCheckboxes = $('.race-checkbox');
+        raceCheckboxes.each(function (i) {
             if (this.checked) {
                 shouldEnableSubmit = true;
+                if (this.id === 'user_races_opt_out') {
+                    optOutSelected = true;
+                }
             }
         });
+        if (optOutSelected) {
+            // Disable and clear all non-opt-out checkboxes and gray out labels
+            var others = raceCheckboxes.not('#user_races_opt_out');
+            others.attr('checked', false);
+            others.attr('disabled', true);
+            others.parent().addClass('disabled'); // gray out labels
+        } else {
+            // Re-enable
+            raceCheckboxes.attr('disabled', false);
+            raceCheckboxes.parent().removeClass('disabled');
+        }
+
         if (shouldEnableSubmit) {
             $("#race-submit").prop('disabled', false).removeClass("disabled-button");
         } else {
