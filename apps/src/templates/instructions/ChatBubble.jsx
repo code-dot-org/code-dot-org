2e8eb97cd7a954c17598cbf7d79a58934fdce634
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-10-21 11:39:14 -0700

    add TTS to authored hints

--- apps/src/templates/instructions/ChatBubble.jsx
+++ apps/src/templates/instructions/ChatBubble.jsx
@@ -3,31 +3,52 @@ import {connect} from 'react-redux';
 import Radium from 'radium';
 import ChatBubbleTip from './ChatBubbleTip';
 import { shouldDisplayChatTips } from './utils';
+import experiments from '../../experiments';
+import InlineAudio from './InlineAudio';
 
-// Minecraft-specific styles
-const craftStyle = {
-  backgroundColor: '#3B3B3B',
-  borderRadius: 4,
-  borderWidth: 0
+const styles = {
+  main: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    margin: '5px 0',
+    padding: '5px 10px',
+    position: 'relative',
+    borderStyle: 'dashed',
+    borderWidth: 1,
+  },
+
+  minecraft: {
+    backgroundColor: '#3B3B3B',
+    borderRadius: 4,
+    borderWidth: 0
+  },
+
+  withAudioControls: {
+    paddingRight: 76
+  },
+
+  audioControls: {
+    position: 'absolute',
+    top: 7,
+    right: 12
+  }
 };
 
-const style = {
-  backgroundColor: 'white',
-  borderRadius: 10,
-  margin: '5px 0',
-  padding: '5px 10px',
-  position: 'relative',
-  borderStyle: 'dashed',
-  borderWidth: 1,
-};
-
-const ChatBubble = ({ children, isMinecraft, skinId, borderColor }) => {
+const ChatBubble = ({ children, isMinecraft, skinId, borderColor, ttsUrl, ttsMessage }) => {
   borderColor = borderColor || 'white';
+  const showAudioControls = experiments.isEnabled('tts') && (ttsUrl || ttsMessage);
 
   return (
-    <div style={[style, isMinecraft && craftStyle, { borderColor }]}>
-      {children}
-      {shouldDisplayChatTips(skinId) && <ChatBubbleTip color={borderColor} />}
+    <div style={{position: 'relative'}}>
+      <div style={[styles.main, isMinecraft && styles.minecraft, showAudioControls && styles.withAudioControls, { borderColor }]}>
+        {children}
+        {shouldDisplayChatTips(skinId) && <ChatBubbleTip color={borderColor} />}
+      </div>
+      {showAudioControls &&
+        <div style={[styles.audioControls]}>
+          <InlineAudio src={ttsUrl} message={ttsMessage} />
+        </div>
+      }
     </div>
   );
 };
@@ -36,7 +57,9 @@ ChatBubble.propTypes = {
   borderColor: React.PropTypes.string,
   children: React.PropTypes.arrayOf(React.PropTypes.node).isRequired,
   isMinecraft: React.PropTypes.bool,
-  skinId: React.PropTypes.string
+  skinId: React.PropTypes.string,
+  ttsUrl: React.PropTypes.string,
+  ttsMessage: React.PropTypes.string,
 };
 
 export default connect(state => {
