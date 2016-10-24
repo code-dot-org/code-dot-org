6ed921be3b47b97156ff26d0164645e93cdc618e
David Bailey <davidsbailey@users.noreply.github.com>
2016-10-24 12:13:41 -0700

    fix assert argument order

--- dashboard/test/models/school_test.rb
+++ dashboard/test/models/school_test.rb
@@ -5,7 +5,7 @@ class SchoolTest < ActiveSupport::TestCase
 
   test "schools initialized from tsv" do
     schools = School.find_or_create_all_from_tsv('test/fixtures/schools.tsv')
-    assert_equal(schools.size, 16, 'test data contains 16 schools')
+    assert_equal(16, schools.size, 'test data contains 16 schools')
     assert_not_nil School.find_by({
       id: 10_000_500_871,
       school_district_id: 100005,
@@ -19,8 +19,8 @@ class SchoolTest < ActiveSupport::TestCase
 
   # See CHARTR in http://nces.ed.gov/ccd/pdf/2015150_sc132a_Documentation_052716.pdf
   test "school_type parses charter values properly" do
-    assert_equal School.send(:school_type, 'N'), 'public'
-    assert_equal School.send(:school_type, '1'), 'charter'
-    assert_equal School.send(:school_type, '2'), 'public'
+    assert_equal 'public', School.send(:school_type, 'N')
+    assert_equal 'charter', School.send(:school_type, '1')
+    assert_equal 'public', School.send(:school_type, '2')
   end
 end
