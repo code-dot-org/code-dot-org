c3c1c6c3009f6369cda3effb69997cb23e60f61c
David Bailey <davidsbailey@users.noreply.github.com>
2016-10-24 09:53:24 -0700

    update schools migration timestamp

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021202609) do
+ActiveRecord::Schema.define(version: 20161024164952) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
