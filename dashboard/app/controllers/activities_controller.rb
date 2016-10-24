38349fb8164169bd219862a9c4add934cab710d1
Ram Kandasamy <ram@code.org>
2016-10-24 16:51:10 -0700

    Remove a couple uses of ScriptLevel#level

--- dashboard/app/controllers/activities_controller.rb
+++ dashboard/app/controllers/activities_controller.rb
@@ -56,7 +56,7 @@ class ActivitiesController < ApplicationController
     end
 
     if current_user && @script_level && @script_level.stage.lockable?
-      user_level = UserLevel.find_by(user_id: current_user.id, level_id: @script_level.level.id, script_id: @script_level.script.id)
+      user_level = UserLevel.find_by(user_id: current_user.id, level_id: @level.id, script_id: @script_level.script.id)
       # we have a lockable stage, and user_level is locked. disallow milestone requests
       if user_level.nil? || user_level.locked?(@script_level.stage) || user_level.try(:readonly_answers?)
         return head 403
