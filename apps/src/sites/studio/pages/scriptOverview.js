b57ba2b52c3349148c1dc27e0c352f63abb367d8
Brent Van Minnen <bjvanminnen@gmail.com>
2016-10-25 12:25:51 -0700

    pass info to script via data tag

--- apps/src/sites/studio/pages/scriptOverview.js
+++ apps/src/sites/studio/pages/scriptOverview.js
@@ -1,19 +1,27 @@
+import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PlcHeader from '@cdo/apps/code-studio/plc/header';
 import { renderCourseProgress } from '@cdo/apps/code-studio/progress';
 
-window.dashboard = window.dashboard || {};
+$(document).ready(() => initPage());
 
-const scriptOverview = window.dashboard.scriptOverview = {};
+function initPage() {
+  const script = document.querySelector('script[data-scriptoverview]');
+  const config = JSON.parse(script.dataset.scriptoverview);
 
-scriptOverview.renderPlcBreadcrumb = function (props, element) {
+  const { scriptData, plcBreadcrumb } = config;
+
+  if (plcBreadcrumb) {
+    renderPlcBreadcrumb(plcBreadcrumb, document.getElementById('breadcrumb'));
+  }
+
+  renderCourseProgress(scriptData);
+}
+
+function renderPlcBreadcrumb(props, element) {
   ReactDOM.render(
     <PlcHeader {...props}/>,
     element
   );
-};
-
-scriptOverview.renderCourseProgress = function (scriptData) {
-  renderCourseProgress(scriptData);
-};
+}
