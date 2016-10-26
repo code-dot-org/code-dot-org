e92396bafb25f2ac1a87994d4c776ccacd6457b3
ewjordan <ewjordan@gmail.com>
2016-10-26 09:27:03 -0700

    Javascript style changes: - 4->2 space indent - prefer single quotes to double - prefer ES6 fat arrow syntax where possible (changes 'this' scope, so couldn't replace all) - pull up a couple duplicate jquery selectors

--- apps/src/sites/studio/pages/raceInterstitial.js
+++ apps/src/sites/studio/pages/raceInterstitial.js
@@ -1,67 +1,67 @@
 import $ from 'jquery';
 import experiments from '@cdo/apps/experiments';
 
-$(document).ready(function () {
-    $("#edit_user").on('change', function (event) {
-        var shouldEnableSubmit = false;
-        var optOutSelected = false;
-        var raceCheckboxes = $('.race-checkbox');
-        raceCheckboxes.each(function (i) {
-            if (this.checked) {
-                shouldEnableSubmit = true;
-                if (this.id === 'user_races_opt_out') {
-                    optOutSelected = true;
-                }
-            }
-        });
-        if (optOutSelected) {
-            // Disable and clear all non-opt-out checkboxes and gray out labels
-            var others = raceCheckboxes.not('#user_races_opt_out');
-            others.prop('checked', false);
-            others.prop('disabled', true);
-            others.parent().addClass('disabled'); // gray out labels
-        } else {
-            // Re-enable
-            raceCheckboxes.prop('disabled', false);
-            raceCheckboxes.parent().removeClass('disabled');
-        }
+$(document).ready(() => {
+  var editUser = $('#edit_user');
+  var raceCheckboxes = $('.race-checkbox');
 
-        if (shouldEnableSubmit) {
-            $("#race-submit").prop('disabled', false).removeClass("disabled-button");
-        } else {
-            $("#race-submit").prop('disabled', true).addClass("disabled-button");
+  editUser.on('change', () => {
+    var shouldEnableSubmit = false;
+    var optOutSelected = false;
+    raceCheckboxes.each(function (i) {
+      if (this.checked) {
+        shouldEnableSubmit = true;
+        if (this.id === 'user_races_opt_out') {
+          optOutSelected = true;
         }
+      }
     });
-
-    function submitCheckboxData(form) {
-        $.ajax({
-            type: 'POST',
-            url: form.attr('action'),
-            data: form.serialize(),
-            dataType: 'json',
-            success: function (data) {
-                $("#race-modal").modal('hide');
-            }
-        });
+    if (optOutSelected) {
+      // Disable and clear all non-opt-out checkboxes and gray out labels
+      var others = raceCheckboxes.not('#user_races_opt_out');
+      others.prop('checked', false);
+      others.prop('disabled', true);
+      others.parent().addClass('disabled'); // gray out labels
+    } else {
+      // Re-enable
+      raceCheckboxes.prop('disabled', false);
+      raceCheckboxes.parent().removeClass('disabled');
     }
 
-    $("#edit_user").submit(function (event) {
-        event.preventDefault();
-        submitCheckboxData($(this));
-    });
+    if (shouldEnableSubmit) {
+      $('#race-submit').prop('disabled', false).removeClass('disabled-button');
+    } else {
+      $('#race-submit').prop('disabled', true).addClass('disabled-button');
+    }
+  });
 
-    $('#later-link').click(function () {
-        var raceCheckboxes = $('.race-checkbox');
-        raceCheckboxes.prop('checked', false);
-        $('#user_races_closed_dialog').prop('checked', true);
-        submitCheckboxData($("#edit_user"));
-        $("#race-modal").modal('hide');
+  function submitCheckboxData(form) {
+    $.ajax({
+      type: 'POST',
+      url: form.prop('action'),
+      data: form.serialize(),
+      dataType: 'json',
+      success: data => $('#race-modal').modal('hide')
     });
+  }
+
+  editUser.submit((event) => {
+    event.preventDefault();
+    submitCheckboxData(editUser);
+  });
+
+  $('#later-link').click(() => {
+    // Clear out all checkboxes, check 'closed_dialog'
+    raceCheckboxes.prop('checked', false);
+    $('#user_races_closed_dialog').prop('checked', true);
+    submitCheckboxData(editUser);
+    $('#race-modal').modal('hide');
+  });
 });
 
-$(document).ready(function () {
-    if (experiments.isEnabled('raceInterstitial')) {
-        $('#race-modal').modal('show');
-        $('#closed-dialog-label').hide();
-    }
+$(document).ready(() => {
+  if (experiments.isEnabled('raceInterstitial')) {
+    $('#race-modal').modal('show');
+    $('#closed-dialog-label').hide();
+  }
 });
