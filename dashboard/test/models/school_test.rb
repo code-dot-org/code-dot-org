48681d6effd3ae00c9d2e5d54507d1852baf4e0b
David Bailey <davidsbailey@users.noreply.github.com>
2016-10-25 17:29:04 -0700

    populate school districts in school model test

--- dashboard/test/models/school_test.rb
+++ dashboard/test/models/school_test.rb
@@ -4,6 +4,9 @@ class SchoolTest < ActiveSupport::TestCase
   include ActiveSupport::Testing::Stream
 
   test "schools initialized from tsv" do
+    # Populate school districts, since schools depends on them as a foreign key.
+    SchoolDistrict.find_or_create_all_from_tsv!('test/fixtures/school_districts.tsv')
+
     schools = School.find_or_create_all_from_tsv('test/fixtures/schools.tsv')
     assert_equal(16, schools.size, 'test data contains 16 schools')
     assert_not_nil School.find_by({
