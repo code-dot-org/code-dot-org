ff04fccc5d957dc1da18543c05a6a44a77bdeb39
Brian Jordan <bcjordan@gmail.com>
2016-10-28 16:53:14 -0400

    WIP. Better keyboard handling. Add hide icon

--- apps/src/templates/DebugWatch.jsx
+++ apps/src/templates/DebugWatch.jsx
@@ -20,6 +20,7 @@ const DebugWatch = React.createClass({
   getInitialState : function() {
     return {
       text : "",
+      history : [],
       editing : false,
     };
   },
@@ -81,28 +82,59 @@ const DebugWatch = React.createClass({
     }
   },
 
-  //onSubmit(editing: boolean) {
-  //  var value = this.state.text;
-  //
-  //}
+  scrollToBottom() {
+    window.requestAnimationFrame(() => {
+      this.refs.scrollableContainer.scrollTop = this.refs.scrollableContainer.scrollHeight;
+    })
+  },
 
-  onKeyDown(e: DOMEvent) {
+  addFromInput() {
+    const inputText = this.state.text;
+    this.props.dispatch(add(inputText));
+    this.setState({
+      history: this.state.history.concat(inputText)
+    });
+    this.setState({
+      editing: false,
+      text: ''
+    });
+    this.refs.debugInput.focus();
+    this.forceUpdate();
+    this.scrollToBottom();
+  },
+
+  onKeyDown(e) {
     if (e.key === 'Enter') {
-      this.props.dispatch(add(e.target.value));
-      this.setState({
-        editing: false,
-        text: ''
-      });
-      this.refs.debugInput.blur();
+      this.addFromInput();
     }
     if (e.key === 'Escape') {
       this.setState({
         editing: false,
       });
     }
+    if (e.key === 'ArrowUp') {
+      this.setState({
+        editing: false,
+        text: this.state.history[this.state.history.length - 1]
+      });
+      this.setState({
+        editing: true
+      });
+      e.preventDefault();
+    }
+    if (e.key === 'ArrowDown') {
+      this.setState({
+        editing: false,
+        text: ''
+      });
+      this.setState({
+        editing: true
+      });
+      e.preventDefault();
+    }
   },
 
-  onChange(e: DOMEvent) {
+  onChange(e) {
     this.setState({
       text: e.target.value,
     });
@@ -111,81 +143,100 @@ const DebugWatch = React.createClass({
   render() {
     let classes = 'debug-watch';
     return (
-        <div id="debug-watch" className={classes}>
-          {
-              this.props.watchedExpressions.map(wv => {
-                  const varName = wv.get('expression');
-                  const varValue = wv.get('lastValue');
-                  return (
-                  <div className="debug-watch-item" key={wv.get('uuid')}>
-                    <div
-                        style={{
-                          fontSize: '18px',
-                          float: 'right',
-                          cursor: 'pointer',
-                          width: '25px',
-                          backgroundColor: '#be0712',
-                          color: 'white',
-                          padding: '6px',
-                          paddingRight: '0px',
-                          paddingLeft: '10px'
-                        //  style="font-size: 18px;
-                        // float: right;
-                        // width: 25px;
-                        // background-color: red;
-                        // color: white;
-                        // padding: 6px;padding-right: 0px;padding-left: 10px;"
-                        }}
-                        onClick={()=>this.props.dispatch(remove(wv.get('expression')))}>
-                      x
+        <div id="debugger-watch-container" style={{
+          width: '100%',
+          height: '100%'
+        }}>
+          <div id="debug-watch" ref="scrollableContainer" className={classes}>
+            {
+                this.props.watchedExpressions.map(wv => {
+                    const varName = wv.get('expression');
+                    const varValue = wv.get('lastValue');
+                    return (
+                    <div className="debug-watch-item" key={wv.get('uuid')}>
+                      <div
+                          style={{
+                            fontSize: '18px',
+                            float: 'right',
+                            cursor: 'pointer',
+                            width: '25px',
+                            backgroundColor: '#be0712',
+                            color: 'white',
+                            padding: '6px',
+                            paddingRight: '0px',
+                            paddingLeft: '10px'
+                          //  style="font-size: 18px;
+                          // float: right;
+                          // width: 25px;
+                          // background-color: red;
+                          // color: white;
+                          // padding: 6px;padding-right: 0px;padding-left: 10px;"
+                          }}
+                          onClick={()=>this.props.dispatch(remove(wv.get('expression')))}>
+                        x
+                      </div>
+                      <div style={{
+                        float: 'left',
+                        marginTop: '7px',
+                        marginLeft: '2px',
+                        whiteSpace: 'nowrap',
+                        overflow: 'scroll',
+                        width: '160px',
+                        height: '21px',
+                      }}>
+                        <span
+                            className="watch-variable">{varName}</span>
+                        <span
+                            className="watch-separator">: </span>
+                        {this.renderValue(varValue)}
+                      </div>
                     </div>
-                    <div style={{
-                      float: 'left',
-                      marginTop: '7px',
-                      marginLeft: '2px',
-                      whiteSpace: 'nowrap',
-                      overflow: 'scroll',
-                      width: '160px',
-                      height: '21px',
-                    }}>
-                      <span
-                          className="watch-variable">{varName}</span>
-                      <span
-                          className="watch-separator">: </span>
-                      {this.renderValue(varValue)}
-                    </div>
-                  </div>
-                      );
-                  })
-              }
-          <div style={{clear: 'both'}}>
-            <div
-                style={{
-                          fontSize: '18px',
-                          float: 'right',
-                          cursor: 'pointer',
-                          width: '25px',
-                          backgroundColor: '#1e93cd',
-                          color: 'white',
-                          padding: '6px',
-                          paddingRight: '0px',
-                          paddingLeft: '10px'
-                        }}
-                onClick={()=>this.props.dispatch(remove(wv.get('expression')))}>
-              +
+                        );
+                    })
+                }
+            <div style={{clear: 'both'}}>
+              <div
+                  style={{
+                            fontSize: '18px',
+                            float: 'right',
+                            cursor: 'pointer',
+                            width: '25px',
+                            backgroundColor: '#1e93cd',
+                            color: 'white',
+                            padding: '6px',
+                            paddingRight: '0px',
+                            paddingLeft: '10px'
+                          }}
+                  onClick={()=>this.addFromInput()}>
+                +
+              </div>
+              <input
+                  ref="debugInput"
+                  placeholder="Variable / Property"
+                  onKeyDown={this.onKeyDown}
+                  onChange={this.onChange}
+                  value={this.state.text}
+                  style={{
+                    width: '159px',
+                    marginTop: '0px',
+                    height: '25px',
+                  }}
+              >
+              </input>
             </div>
-            <input
-                ref="debugInput"
-                onKeyDown={this.onKeyDown}
-                onChange={this.onChange}
-                value={this.state.text}
-                style={{
-                  width: '159px',
-                  marginTop: '0px',
-                  height: '25px',
-                }}
-            >
-            </input>
+          </div>
+          <div id="autocomplete-panel" style={{
+              display: 'none',
+              width: '200px',
+              height: '200px',
+              position: 'absolute',
+              background: 'gray',
+              right: '-10px',
+              bottom: '76px',
+          }}>
+            <div className="autocomplete-option">Option 1</div>
+            <div className="autocomplete-option">Option 2</div>
+            <div className="autocomplete-option">Option 3</div>
           </div>
         </div>
     );
