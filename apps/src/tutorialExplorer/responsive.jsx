f26e76f4089b5b36405b943d68e42c22f2eb235e
Brendan Reville <brendan@code.org>
2016-10-30 21:45:26 -0700

    hoc2016: initial work on React-based responsive.  Works on both domains.

---
+++ apps/src/tutorialExplorer/responsive.jsx
@@ -0,0 +1,29 @@
+function getContainerWidth(windowWidth) {
+  if (windowWidth >= 1200) {
+    return 1170;
+  } else if (windowWidth >= 992) {
+    return 970;
+  } else if (windowWidth >= 768) {
+    return 750;
+  } else {
+    return "100%";
+  }
+}
+
+function getItemWidth(itemWidth, windowWidth) {
+  if (windowWidth >= 600) {
+    return `${itemWidth}%`;
+  } else {
+    return "100%";
+  }
+}
+
+function getItemWidthMobile(itemWidth, windowWidth) {
+  if (windowWidth < 600) {
+    return `${itemWidth}%`;
+  } else {
+    return "";
+  }
+}
+
+export { getContainerWidth, getItemWidth, getItemWidthMobile };
--- apps/src/tutorialExplorer/tutorialSet.jsx
+++ apps/src/tutorialExplorer/tutorialSet.jsx
@@ -4,24 +4,24 @@
 import React from 'react';
 import Tutorial from './tutorial';
 import shapes from './shapes';
+import { getContainerWidth, getItemWidth } from './responsive';
 
 const TutorialSet = React.createClass({
   propTypes: {
     tutorials: React.PropTypes.arrayOf(shapes.tutorial.isRequired).isRequired,
-    filters: React.PropTypes.objectOf(React.PropTypes.arrayOf(React.PropTypes.string)).isRequired
+    filters: React.PropTypes.objectOf(React.PropTypes.arrayOf(React.PropTypes.string)).isRequired,
+    windowWidth: React.PropTypes.number.isRequired
   },
 
   render() {
     return (
-      <div
-        className="col-80"
-        style={{float: 'left'}}
-      >
+      <div style={{float: 'left', width: getItemWidth(80, this.props.windowWidth)}}>
         {this.props.tutorials.map(item => (
           <Tutorial
             item={item}
             filters={this.props.filters}
             key={item.code}
+            windowWidth={this.props.windowWidth}
           />
         ))}
       </div>
