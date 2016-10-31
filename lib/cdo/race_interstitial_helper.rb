d719e1f74b821596526114aa2cb862d60ee00bd9
ewjordan <ewjordan@gmail.com>
2016-10-31 10:41:23 -0700

    Code style (prefer 'return X if Y')

--- lib/cdo/race_interstitial_helper.rb
+++ lib/cdo/race_interstitial_helper.rb
@@ -1,19 +1,13 @@
 module RaceInterstitialHelper
   # Determine whether or not to show the race interstitial popup to a user
   def self.show_race_interstitial?(user)
-    if user.teacher?
-      return false
-    end
-    if user.under_13?
-      return false
-    end
-    if user.account_age_days < 7
-      return false
-    end
+    return false if user.teacher?
+    return false if user.under_13?
+    return false if user.account_age_days < 7
     # If there is race information at all, that means we shouldn't show the dialog
-    if user.races && !user.races.empty?
-      return false
-    end
+    return false if user.races && !user.races.empty?
+
+    # Restrict to US
     if defined?(request)
       location = Geocoder.search(request.ip).first
       if location && location.country_code.to_s.downcase != 'us'
