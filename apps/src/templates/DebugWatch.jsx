7893f07feed880e4428f81b051b1bc15f3777f1f
Brian Jordan <bcjordan@gmail.com>
2016-11-01 17:21:09 -0400

    Add autocomplete

--- apps/src/templates/DebugWatch.jsx
+++ apps/src/templates/DebugWatch.jsx
@@ -3,9 +3,72 @@ import Immutable from 'immutable';
 import {connect} from 'react-redux';
 import i18n from '@cdo/locale';
 import {add, update, remove} from '../redux/watchedExpressions';
+import TetherComponent from 'react-tether'
 
 const WATCH_TIMER_PERIOD = 50;
 const WATCH_VALUE_NOT_RUNNING = "undefined";
+
+const AutocompleteSelector = React.createClass({
+  getInitialState() {
+    return {
+      selectedOption: this.props.options.length - 1,
+    };
+  },
+
+  render() {
+    // http://stackoverflow.com/a/6633271
+    // Giving div keyboard focus
+    // range of matches
+    //    http://stackoverflow.com/a/2295681
+
+    let index = 0;
+    return (
+            <div id="autocomplete-panel" style={{
+              width: '162px',
+              height: 'initial',
+              background: 'white',
+              color: '#808080',
+              border: '1px gray solid',
+              padding: '0px',
+              marginTop: '-2px',
+              marginLeft: '-1px',
+            }}>
+          {this.props.options.map((option) => {
+              const isSelected = index === this.props.currentIndex;
+              const myIndex = index;
+              index++;
+              const selectedStyle = {
+                backgroundColor: '#cad6fa',
+                color: 'black',
+              };
+              return (
+                <div
+                    key={option}
+                    className="autocomplete-option"
+                    onClick={() => this.props.onOptionClicked(option)}
+                    onMouseOver={() => this.props.onOptionHovered(myIndex)}
+                    style={Object.assign({}, {
+                      cursor: 'pointer',
+                      marginLeft: '2px'
+                      //':hover': selectedStyle,
+                    }, isSelected ? selectedStyle : {})}
+                >
+                  {option}
+                </div>
+              );
+          })}
+        </div>
+    );
+  },
+
+  propTypes: {
+    currentText: React.PropTypes.string,
+    currentIndex: React.PropTypes.number,
+    options: React.PropTypes.arrayOf(React.PropTypes.string),
+    onOptionClicked: React.PropTypes.func,
+    onOptionHovered: React.PropTypes.func
+  },
+});
 
 /**
  * A "watchers" window for our debugger.
@@ -22,12 +85,26 @@ const DebugWatch = React.createClass({
       text : "",
       history : [],
       editing : false,
+      autocompleteOpen: false,
+      autocompleteIndex: this.autocompleteOptions.length - 1,
+      autocompleteOptions: this.autocompleteOptions,
+      historyIndex: -1
     };
   },
+
+  autocompleteOptions: [
+    'Game.mouseX',
+    'Game.mouseY',
+    'Game.width',
+    'Game.height',
+    'Game.frameRate',
+    'Game.frameCount',
+  ],
 
   componentDidMount() {
     this.wasRunning = null;
     this.intervalId_ = setInterval(() => {
+      console.log("Watching!");
       const justStoppedRunning = this.wasRunning && !this.props.isRunning;
       this.wasRunning = this.props.isRunning;
 
@@ -89,50 +166,105 @@ const DebugWatch = React.createClass({
     });
   },
 
-  addFromInput() {
-    const inputText = this.state.text;
+  addButtonClick() {
+    if (this.state.text === '') {
+      this.setState({
+        autocompleteOpen: true
+      });
+    } else {
+      this.addFromInput();
+    }
+  },
+
+  addFromInput(inputText = this.state.text) {
+    if (inputText === '') {
+      return;
+    }
     this.props.dispatch(add(inputText));
     this.setState({
-      history: this.state.history.concat(inputText)
-    });
-    this.setState({
+      history: this.state.history.concat(inputText),
       editing: false,
       text: ''
+    }, () => {
+      this.forceUpdate();
+      this.scrollToBottom();
+      this.filterOptions();
+      // TODO(bjordan): add re-focusing (seemingly having trouble with duplicate refs)
     });
-    this.refs.debugInput.focus();
-    this.forceUpdate();
-    this.scrollToBottom();
   },
 
   onKeyDown(e) {
     if (e.key === 'Enter') {
-      this.addFromInput();
+      if (this.state.autocompleteOpen) {
+        this.addFromInput(this.state.autocompleteOptions[this.state.autocompleteIndex]);
+      } else {
+        this.addFromInput();
+      }
     }
     if (e.key === 'Escape') {
       this.setState({
         editing: false,
+        autocompleteOpen: false
       });
     }
     if (e.key === 'ArrowUp') {
-      this.setState({
-        editing: false,
-        text: this.state.history[this.state.history.length - 1]
-      });
-      this.setState({
-        editing: true
-      });
+      if (this.state.autocompleteOpen) {
+        const newIndex = (this.state.autocompleteIndex - 1 + this.state.autocompleteOptions.length) % this.state.autocompleteOptions.length;
+        this.setState({
+          autocompleteIndex: newIndex,
+        });
+      } else {
+        const historyIndex = (this.state.historyIndex - 1 + this.state.history.length) % this.state.history.length
+        this.setState({
+          editing: false,
+          text: this.state.history[historyIndex],
+          historyIndex: historyIndex
+        });
+        this.setState({
+          editing: true
+        });
+      }
       e.preventDefault();
     }
     if (e.key === 'ArrowDown') {
-      this.setState({
-        editing: false,
-        text: ''
-      });
-      this.setState({
-        editing: true
-      });
+      if (this.state.autocompleteOpen) {
+        const newIndex = (this.state.autocompleteIndex + 1) % this.state.autocompleteOptions.length;
+        this.setState({
+          autocompleteIndex: newIndex,
+        });
+      } else {
+        this.setState({
+          editing: false,
+          text: ''
+        }, () => {
+          this.setState({
+            editing: true
+          });
+        });
+      }
       e.preventDefault();
     }
+  },
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.text !== this.state.text) {
+      this.filterOptions();
+    }
+  },
+
+  filterOptions() {
+    const text = this.state.text;
+    const filteredOptions = this.autocompleteOptions.filter((option) => option.match(new RegExp(text, 'i')));
+    const completeMatch = filteredOptions.length === 1 && filteredOptions[0] === text;
+    this.setState({
+      autocompleteIndex: this.state.autocompleteIndex > filteredOptions.length ? 0 : this.state.autocompleteIndex,
+      autocompleteOptions: filteredOptions,
+      autocompleteOpen: text.length && filteredOptions.length && !completeMatch
+    });
+  },
+
+  onAutocompleteOptionClicked(text) {
+    this.addFromInput(text);
   },
 
   onChange(e) {
@@ -143,89 +275,113 @@ const DebugWatch = React.createClass({
 
   render() {
     let classes = 'debug-watch';
+
     return (
-        <div id="debug-watch" ref="scrollableContainer" className={classes}>
-          {
-              this.props.watchedExpressions.map(wv => {
-                  const varName = wv.get('expression');
-                  const varValue = wv.get('lastValue');
-                  return (
-                  <div className="debug-watch-item" key={wv.get('uuid')}>
-                    <div
-                      style={{
-                          fontSize: '18px',
-                          float: 'right',
-                          cursor: 'pointer',
-                          width: '25px',
-                          backgroundColor: '#be0712',
-                          color: 'white',
-                          padding: '6px',
-                          paddingRight: '0px',
-                          paddingLeft: '10px'
-                        //  style="font-size: 18px;
-                        // float: right;
-                        // width: 25px;
-                        // background-color: red;
-                        // color: white;
-                        // padding: 6px;padding-right: 0px;padding-left: 10px;"
-                        }}
-                      onClick={()=>this.props.dispatch(remove(wv.get('expression')))}
-                    >
-                      x
+        <div id="debugger-watch-container" style={{
+          width: '100%',
+          height: '100%'
+        }}>
+          <div id="debug-watch" ref="scrollableContainer" className={classes}>
+            {
+                this.props.watchedExpressions.map(wv => {
+                    const varName = wv.get('expression');
+                    const varValue = wv.get('lastValue');
+                    return (
+                    <div className="debug-watch-item" key={wv.get('uuid')}>
+                      <div
+                        style={{
+                            fontSize: '18px',
+                            float: 'right',
+                            cursor: 'pointer',
+                            width: '25px',
+                            backgroundColor: '#be0712',
+                            color: 'white',
+                            padding: '6px',
+                            paddingRight: '0px',
+                            paddingLeft: '10px'
+                          //  style="font-size: 18px;
+                          // float: right;
+                          // width: 25px;
+                          // background-color: red;
+                          // color: white;
+                          // padding: 6px;padding-right: 0px;padding-left: 10px;"
+                          }}
+                        onClick={()=>this.props.dispatch(remove(wv.get('expression')))}
+                      >
+                        x
+                      </div>
+                      <div style={{
+                        float: 'left',
+                        marginTop: '7px',
+                        marginLeft: '2px',
+                        whiteSpace: 'nowrap',
+                        overflow: 'scroll',
+                        width: '160px',
+                        height: '21px',
+                      }}>
+                        <span
+                          className="watch-variable"
+                        >{varName}</span>
+                        <span
+                          className="watch-separator"
+                        >: </span>
+                        {this.renderValue(varValue)}
+                      </div>
                     </div>
-                    <div style={{
-                      float: 'left',
-                      marginTop: '7px',
-                      marginLeft: '2px',
-                      whiteSpace: 'nowrap',
-                      overflow: 'scroll',
-                      width: '160px',
-                      height: '21px',
-                    }}>
-                      <span
-                        className="watch-variable"
-                      >{varName}</span>
-                      <span
-                        className="watch-separator"
-                      >: </span>
-                      {this.renderValue(varValue)}
-                    </div>
-                  </div>
-                      );
-                  })
-              }
-          <div style={{clear: 'both'}}>
-            <div
-              style={{
-                          fontSize: '18px',
-                          float: 'right',
-                          cursor: 'pointer',
-                          width: '25px',
-                          backgroundColor: '#1e93cd',
-                          color: 'white',
-                          padding: '6px',
-                          paddingRight: '0px',
-                          paddingLeft: '10px'
-                        }}
-              onClick={()=>this.addFromInput()}
-            >
-              +
-            </div>
-            <input
-              ref="debugInput"
-              placeholder="Variable / Property"
-              onKeyDown={this.onKeyDown}
-              onChange={this.onChange}
-              value={this.state.text}
-              style={{
-                  width: '159px',
-                  marginTop: '0px',
-                  height: '25px',
-                  fontFamily: 'monospace',
-                  fontSize: '12px',
+                        );
+                    })
+                }
+            <div style={{clear: 'both'}}>
+              <div
+                style={{
+                  fontSize: '18px',
+                  float: 'right',
+                  cursor: 'pointer',
+                  width: '25px',
+                  backgroundColor: '#1e93cd',
+                  color: 'white',
+                  padding: '6px',
+                  paddingRight: '0px',
+                  paddingLeft: '10px'
                 }}
-            >
-            </input>
+                onClick={()=>this.addButtonClick()}
+              >
+                +
+              </div>
+              <TetherComponent
+                  attachment="top center"
+                  constraints={[{
+                to: 'scrollParent',
+                attachment: 'together'
+              }]}
+              >
+                <input
+                    placeholder="Variable / Property"
+                    ref='debugInput'
+                    onKeyDown={this.onKeyDown}
+                    onChange={this.onChange}
+                    onClick={() => this.setState({autocompleteOpen: true})}
+                    value={this.state.text}
+                    style={{
+                    width: '159px',
+                    marginTop: '0px',
+                    height: '25px',
+                    fontFamily: 'monospace',
+                    fontSize: '12px',
+                  }}
+                />
+                {this.state.autocompleteOpen &&
+                    <AutocompleteSelector
+                    options={this.state.autocompleteOptions}
+                    currentIndex={this.state.autocompleteIndex}
+                    currentText={this.state.text}
+                    onOptionClicked={this.onAutocompleteOptionClicked}
+                    onOptionHovered={(index) => {
+                      this.setState({autocompleteIndex: index})
+                    }}
+                />}
+              </TetherComponent>
+            </div>
           </div>
         </div>
     );
