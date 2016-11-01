8c4517cf6eaf64b1ee54a1857d46ea326776341d
David Bailey <davidsbailey@users.noreply.github.com>
2016-11-01 10:43:26 -0700

    warn when using the master production db

--- lib/cdo/mysql_console_helper.rb
+++ lib/cdo/mysql_console_helper.rb
@@ -11,6 +11,14 @@ module MysqlConsoleHelper
     command << "--execute=\"#{args}\"" unless args.empty?
     command << "--password=#{db.password}" unless db.password.nil?
 
+    warning =
+      "****************************************************************\n"\
+      "*** You are connecting to the master production database.    ***\n"\
+      "*** For simple queries, please use the reporting db instead. ***\n"\
+      "*** e.g. bin/dashboard-replica-sql, bin/pegasus-replica-sql  ***\n"\
+      "****************************************************************"
+    puts warning if db.host.start_with?('production')
+
     system(command.join(' '))
   end
 end
