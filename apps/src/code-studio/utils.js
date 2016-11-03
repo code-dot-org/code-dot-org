decb95774edb16ef56bf3b207e98b48891757238
Brent Van Minnen <bjvanminnen@gmail.com>
2016-11-03 13:11:22 -0700

    tests for query parameters utils

--- apps/src/code-studio/utils.js
+++ apps/src/code-studio/utils.js
@@ -1,34 +1,42 @@
 import queryString from 'query-string';
 
-module.exports = {
-  /**
-   * Gets the URL querystring params.
-   * @param name {string=} Optionally pull a specific param.
-   * @return {object|string} Hash of params, or param string if `name` is specified.
-   */
-  queryParams(name) {
-    const parsed = queryString.parse(window.location.search);
-    if (name) {
-      return parsed[name];
-    }
-    return parsed;
-  },
+// Provide methods that allow tests to mock window.location
+let windowLocation = window.location;
+export function setWindowLocation(fakeLocation) {
+  windowLocation = fakeLocation;
+}
 
-  /**
-   * Updates a query parameter in the URL via pushState (i.e. doesn't force a
-   * reload).
-   */
-  updateQueryParam(param, value) {
-    const newString = queryString.stringify({
-      ...queryString.parse(location.search),
-      [param]: value
-    });
+export function resetWindowLocation() {
+  windowLocation = window.location;
+}
 
-    let newLocation = window.location.pathname;
-    // Don't append ? unless we actually have a value
-    if (newString) {
-      newLocation += '?' + newString;
-    }
-    window.history.pushState(null, document.title, newLocation);
+/**
+ * Gets the URL querystring params.
+ * @param name {string=} Optionally pull a specific param.
+ * @return {object|string} Hash of params, or param string if `name` is specified.
+ */
+export function queryParams(name) {
+  const parsed = queryString.parse(windowLocation.search);
+  if (name) {
+    return parsed[name];
   }
-};
+  return parsed;
+}
+
+/**
+ * Updates a query parameter in the URL via pushState (i.e. doesn't force a
+ * reload).
+ */
+export function updateQueryParam(param, value) {
+  const newString = queryString.stringify({
+    ...queryString.parse(windowLocation.search),
+    [param]: value
+  });
+
+  let newLocation = windowLocation.pathname;
+  // Don't append ? unless we actually have a value
+  if (newString) {
+    newLocation += '?' + newString;
+  }
+  window.history.pushState(null, document.title, newLocation);
+}
