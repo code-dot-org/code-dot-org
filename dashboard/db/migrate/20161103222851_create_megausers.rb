eb61d21d2388e30e9baed4a5f02d3669d00a28fe
Asher Kach <asher@code.org>
2016-11-03 13:12:32 -0500

    Rename persons to megausers.

--- dashboard/db/migrate/20161103222851_create_megausers.rb
+++ dashboard/db/migrate/20161103222851_create_megausers.rb
@@ -1,8 +1,8 @@
-class CreatePersons < ActiveRecord::Migration[5.0]
+class CreateMegausers < ActiveRecord::Migration[5.0]
   def change
-    # The persons table will contain information that spans multiple users, as
+    # The megausers table will contain information that spans multiple users, as
     # well as provide a mechanism to link users.
-    create_table :persons do |t|
+    create_table :megausers do |t|
       t.timestamps
       # Though each linked user has an email address, we include this here so as
       # to be able to include email addresses not associated with any Code
@@ -10,9 +10,9 @@ class CreatePersons < ActiveRecord::Migration[5.0]
       t.string :emails
     end
 
-    # Each user now belongs to a person.
+    # Each user now belongs to a megauser.
     change_table :users do |t|
-      t.belongs_to :person, after: :id
+      t.belongs_to :megauser, after: :id
     end
   end
 end
