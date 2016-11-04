e82331bc5f752627e6bc422e27d5358dc793a4d0
Brendan Reville <brendan@code.org>
2016-11-04 10:16:24 -0700

    learn2016: working i18n for all strings

---
+++ apps/src/tutorialExplorer/locale.js
@@ -0,0 +1,11 @@
+/**
+ * DO NOT IMPORT THIS DIRECTLY. Instead do:
+ *   ```
+ *   import i18n from '@cdo/tutorialExplorer/locale'.
+ *   ```
+ * This allows the webpack config to determine how locales should be loaded,
+ * which is important for making locale setup work seamlessly in tests.
+ */
+// locale for tutorialExplorer
+
+export default window.blockly.tutorialExplorer_locale;
