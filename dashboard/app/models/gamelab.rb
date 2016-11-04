a9ffa8fc1ee577123da0af9ae8ed70c939b26013
Brian Jordan <bcjordan@gmail.com>
2016-11-04 16:51:20 -0400

    Rename watchers_options -> watchers_prepopulated. Add prepopulated watchers to App Lab

--- dashboard/app/models/gamelab.rb
+++ dashboard/app/models/gamelab.rb
@@ -38,7 +38,7 @@ class Gamelab < Blockly
     data_properties
     hide_view_data_button
     show_debug_watch
-    watchers_options
+    watchers_prepopulated
     debugger_disabled
     pause_animations_by_default
     start_animations
