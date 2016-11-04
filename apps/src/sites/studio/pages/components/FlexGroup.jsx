1a86fc73fd50d20a34e3d9d45959a486689fe9e9
Josh Lory <josh.lory@code.org>
2016-11-03 18:11:26 -0700

    Break up components into separate files

---
+++ apps/src/sites/studio/pages/components/FlexGroup.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import _ from 'lodash';
+import color from '../../../../color';
+import { borderRadius } from './constants';
+import OrderControls from './OrderControls';
+import StageCard from './StageCard';
+
+const styles = {
+  groupHeader: {
+    fontSize: 18,
+    color: 'white',
+    background: color.cyan,
+    borderTopLeftRadius: borderRadius,
+    borderTopRightRadius: borderRadius,
+    padding: 10
+  },
+  groupBody: {
+    background: color.lightest_cyan,
+    borderBottomLeftRadius: borderRadius,
+    borderBottomRightRadius: borderRadius,
+    padding: 10,
+    marginBottom: 20
+  },
+  addGroup: {
+    fontSize: 14,
+    color: 'white',
+    background: color.cyan,
+    border: `1px solid ${color.cyan}`,
+    boxShadow: 'none',
+    margin: '0 0 30px 0'
+  },
+  addStage: {
+    fontSize: 14,
+    color: '#5b6770',
+    background: 'white',
+    border: '1px solid #ccc',
+    boxShadow: 'none',
+    margin: '0 10px 10px 10px'
+  }
+};
+
+const FlexGroup = React.createClass({
+  propTypes: {
+    handleAction: React.PropTypes.func.isRequired,
+    stages: React.PropTypes.array.isRequired
+  },
+
+  handleAddGroup() {
+    this.props.handleAction('ADD_GROUP');
+  },
+
+  handleAddStage(position) {
+    this.props.handleAction('ADD_STAGE', {position});
+  },
+
+  render() {
+    const groups = _.groupBy(this.props.stages, stage => (stage.flex_category || 'Default'));
+    let count = 0;
+    let afterStage = 1;
+
+    return (
+      <div>
+        {_.map(groups, (stages, group) => {
+          return (
+            <div key={group}>
+              <div style={styles.groupHeader}>
+                Group {++count}: {group}
+                <OrderControls
+                  handleAction={this.props.handleAction}
+                  type="GROUP"
+                  position={afterStage}
+                  total={Object.keys(groups).length}
+                />
+              </div>
+              <div style={styles.groupBody}>
+                {stages.map(stage => {
+                  afterStage++;
+                  return (
+                    <StageCard
+                      handleAction={this.props.handleAction}
+                      key={stage.id}
+                      stagesCount={this.props.stages.length}
+                      stage={stage}
+                    />
+                  );
+                })}
+                <button onMouseDown={this.handleAddStage.bind(null, afterStage - 1)} className="btn" style={styles.addStage} type="button">
+                  <i style={{marginRight: 7}} className="fa fa-plus-circle" />
+                  Add Stage
+                </button>
+              </div>
+            </div>
+          );
+        })}
+        <button onMouseDown={this.handleAddGroup} className="btn" style={styles.addGroup} type="button">
+          <i style={{marginRight: 7}} className="fa fa-plus-circle" />
+          Add Group
+        </button>
+      </div>
+    );
+  }
+});
+
+export default FlexGroup;
---
+++ apps/src/sites/studio/pages/components/LevelToken.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import {Motion, spring} from 'react-motion';
+import color from '../../../../color';
+import { borderRadius, levelTokenMargin } from './constants';
+import LevelTokenDetails from './LevelTokenDetails';
+
+const styles = {
+  levelToken: {
+    position: 'relative',
+    fontSize: 13,
+    background: '#eee',
+    borderRadius: borderRadius,
+    margin: `${levelTokenMargin}px 0`
+  },
+  reorder: {
+    fontSize: 16,
+    display: 'table-cell',
+    background: '#ddd',
+    border: '1px solid #bbb',
+    boxShadow: 'inset 0 1px 0 0 rgba(255, 255, 255, 0.6)',
+    padding: '7px 15px',
+    borderTopLeftRadius: borderRadius,
+    borderBottomLeftRadius: borderRadius,
+    cursor: 'ns-resize'
+  },
+  levelTokenName: {
+    padding: 7,
+    display: 'table-cell',
+    boxShadow: 'inset 0 1px 0 0 rgba(255, 255, 255, 0.8)',
+    width: '100%',
+    borderTop: '1px solid #ddd',
+    borderBottom: '1px solid #ddd',
+    cursor: 'text'
+  },
+  variants: {
+    color: 'white',
+    background: color.purple,
+    padding: '3px 5px',
+    lineHeight: '12px',
+    borderRadius: 5,
+    float: 'right'
+  },
+  remove: {
+    fontSize: 14,
+    display: 'table-cell',
+    color: 'white',
+    background: '#c00',
+    border: '1px solid #a00',
+    boxShadow: 'inset 0 1px 0 0 rgba(255, 255, 255, 0.6)',
+    padding: '7px 13px',
+    borderTopRightRadius: borderRadius,
+    borderBottomRightRadius: borderRadius,
+    cursor: 'pointer'
+  }
+};
+
+const LevelToken = React.createClass({
+  propTypes: {
+    handleAction: React.PropTypes.func.isRequired,
+    level: React.PropTypes.object.isRequired,
+    stagePosition: React.PropTypes.number.isRequired,
+    dragging: React.PropTypes.bool.isRequired,
+    drag: React.PropTypes.bool.isRequired,
+    delta: React.PropTypes.number,
+    handleDragStart: React.PropTypes.func.isRequired
+  },
+
+  toggleExpand() {
+    this.props.handleAction('TOGGLE_EXPAND', {stage: this.props.stagePosition, level: this.props.level.position});
+  },
+
+  handleRemove() {
+    this.props.handleAction('REMOVE_LEVEL', {stage: this.props.stagePosition, level: this.props.level.position});
+  },
+
+  render() {
+    return (
+      <Motion
+        style={this.props.drag ? {
+          y: this.props.dragging ? this.props.delta : 0,
+          scale: spring(1.02, {stiffness: 1000, damping: 80}),
+          shadow: spring(5, {stiffness: 1000, damping: 80})
+        } : {
+          y: this.props.dragging ? spring(this.props.delta, {stiffness: 1000, damping: 80}) : 0,
+          scale: 1,
+          shadow: 0
+        }} key={this.props.level.position}
+      >
+        {({y, scale, shadow}) =>
+          <div
+            style={Object.assign({}, styles.levelToken, {
+              transform: `translate3d(0, ${y}px, 0) scale(${scale})`,
+              boxShadow: `${color.shadow} 0 ${shadow}px ${shadow * 3}px`,
+              zIndex: this.props.drag ? 1000 : 500 - this.props.level.position
+            })}
+          >
+            <div style={styles.reorder} onMouseDown={this.props.handleDragStart.bind(null, this.props.level.position)}>
+              <i className="fa fa-arrows-v"/>
+            </div>
+            <span style={styles.levelTokenName} onMouseDown={this.toggleExpand}>
+              {this.props.level.key}
+              {this.props.level.ids.length > 1 &&
+              <span style={styles.variants}>{this.props.level.ids.length} variants</span>
+              }
+            </span>
+            <div style={styles.remove} onMouseDown={this.handleRemove}>
+              <i className="fa fa-times"/>
+            </div>
+            {this.props.level.expand &&
+            <LevelTokenDetails
+              handleAction={this.props.handleAction}
+              level={this.props.level}
+              stagePosition={this.props.stagePosition}
+            />
+            }
+          </div>
+        }
+      </Motion>
+    );
+  }
+});
+
+export default LevelToken;
