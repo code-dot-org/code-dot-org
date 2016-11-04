551b3b6bc04e0c3bbfdfd74c1fe471cc379df008
Will Jordan <will@code.org>
2016-11-04 15:46:48 -0400

    Move hostname-setting to cdo-apps::hostname recipe

--- cookbooks/cdo-apps/recipes/default.rb
+++ cookbooks/cdo-apps/recipes/default.rb
@@ -10,9 +10,7 @@ include_recipe 'apt'
 include_recipe 'sudo-user'
 include_recipe 'cdo-networking'
 
-# Set hostname to the Chef node name (via chef_hostname cookbook)
-HOSTNAME_INVALID_CHAR = /[^[:alnum:]-]/
-hostname node.name.downcase.gsub(HOSTNAME_INVALID_CHAR, '-')
+include_recipe 'cdo-apps::hostname'
 
 # These packages are used by Gems we install via Bundler.
 
