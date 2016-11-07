ef0f092bd18ce311a96c6404516974132014856a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-11-07 12:19:27 -0800

    retain showInstructionsWrapper, as it is used for some non-instructions dialogs

--- apps/src/templates/BelowVisualization.jsx
+++ apps/src/templates/BelowVisualization.jsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import ProtectedStatefulDiv from './ProtectedStatefulDiv';
+
+/**
+ * The area below our visualization that is shared by all apps. Used by
+ * code-studio/reference_area and StudioApp to mount the
+ * dashboard-generated Reference Area on the page.
+ */
 export default function BelowVisualization() {
   return <ProtectedStatefulDiv id="belowVisualization" />;
 }
