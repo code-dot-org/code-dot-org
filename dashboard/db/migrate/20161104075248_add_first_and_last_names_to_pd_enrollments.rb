f0900561c7b6e71298f7d0481c5e6af019dd0afb
Andrew Oberhardt <Andrew@Code.org>
2016-11-08 10:14:39 -0800

    PR feedback [skip ui]

--- dashboard/db/migrate/20161104075248_add_first_and_last_names_to_pd_enrollments.rb
+++ dashboard/db/migrate/20161104075248_add_first_and_last_names_to_pd_enrollments.rb
@@ -4,16 +4,16 @@ class AddFirstAndLastNamesToPdEnrollments < ActiveRecord::Migration[5.0]
 
   def change
     change_table(:pd_enrollments) do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, after: :name
+      t.string :last_name, after: :first_name
     end
 
     # Migrate the existing data: Split each (full) name on the first space.
     reversible do |dir|
       dir.up do
         Pd::Enrollment.find_each do |enrollment|
-          name_split = enrollment.name.split(' ', 2)
-          enrollment.first_name = name_split[0]
+          name_split = (enrollment.name || '').split(' ', 2)
+          enrollment.first_name = name_split[0] || ''
           enrollment.last_name = name_split[1] || ''
           enrollment.save!
         end
@@ -22,9 +22,5 @@ class AddFirstAndLastNamesToPdEnrollments < ActiveRecord::Migration[5.0]
 
     # Allow null on existing name column
     change_column_null :pd_enrollments, :name, true
-
-    # Don't allow null on new first_name and last_name columns
-    change_column_null :pd_enrollments, :first_name, false
-    change_column_null :pd_enrollments, :last_name, false
   end
 end
