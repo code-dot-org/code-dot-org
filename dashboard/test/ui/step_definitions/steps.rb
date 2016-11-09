a683c43c6c8200b72417d1e59c8e7fc31fde3935
Brent Van Minnen <bjvanminnen@gmail.com>
2016-11-09 11:32:23 -0800

    improve flakiness in eyes test

--- dashboard/test/ui/step_definitions/steps.rb
+++ dashboard/test/ui/step_definitions/steps.rb
@@ -999,7 +999,7 @@ Then /^I upload the file named "(.*?)"$/ do |filename|
 end
 
 Then /^I scroll our lockable stage into view$/ do
-  wait_with_short_timeout.until { @browser.execute_script('return $(".react_stage").length') >= 31 }
+  wait_with_short_timeout.until { @browser.execute_script('return $(".uitest-locked").length') > 0 }
   @browser.execute_script('$(".react_stage")[30] && $(".react_stage")[30].scrollIntoView(true)')
 end
 
