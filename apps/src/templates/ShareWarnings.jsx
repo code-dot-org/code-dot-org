5843821ad0934d998af41c7c2b089ff0ea758083
Brent Van Minnen <bjvanminnen@gmail.com>
2016-11-10 13:35:19 -0800

    Bring code-style more inline with recent styles

--- apps/src/templates/ShareWarnings.jsx
+++ apps/src/templates/ShareWarnings.jsx
@@ -1,17 +1,36 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var color = require("../util/color");
-var AgeDropdown = require('./AgeDropdown');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import color from "../util/color";
+import AgeDropdown from './AgeDropdown';
+import commonMsg from '@cdo/locale';
 
-var commonMsg = require('@cdo/locale');
+const styles = {
+  dataMessage: {
+    fontSize: 18,
+    marginBottom: 30
+  },
+  ageMessage: {
+    fontSize: 18,
+    marginBottom: 10
+  },
+  moreInfo: {
+    marginLeft: 0,
+    fontSize: 15
+  },
+  ok: {
+    backgroundColor: color.orange,
+    border: '1px solid ' + color.orange,
+    color: color.white,
+    float: 'right'
+  }
+};
 
 /**
  * Dialog contents for when you visit a shred Applab page. If not signed in,
  * it will ask your age. If the app stores data, it will also alert you to
  * that.
  */
-module.exports = React.createClass({
-  displayName: 'ShareWarnings',
+const ShareWarnings = React.createClass({
   propTypes: {
     is13Plus: React.PropTypes.bool.isRequired,
     showStoreDataAlert: React.PropTypes.bool.isRequired,
@@ -19,19 +38,19 @@ module.exports = React.createClass({
     handleTooYoung: React.PropTypes.func.isRequired
   },
 
-  handleOk: function () {
+  handleOk() {
     if (this.props.is13Plus) {
       this.props.handleClose();
       return;
     }
 
-    var ageElement = ReactDOM.findDOMNode(this.refs.age);
+    const ageElement = ReactDOM.findDOMNode(this.refs.age);
     if (ageElement.value === '') {
       // ignore close if we haven't selected a value from dropdown
       return;
     }
 
-    var age = parseInt(ageElement.value, 10);
+    const age = parseInt(ageElement.value, 10);
     if (age >= 13) {
       this.props.handleClose();
     } else {
@@ -39,58 +58,25 @@ module.exports = React.createClass({
     }
   },
 
-  render: function () {
-    var styles = {
-      dataMessage: {
-        fontSize: 18,
-        marginBottom: 30
-      },
-      ageMessage: {
-        fontSize: 18,
-        marginBottom: 10
-      },
-      ageDropdown: {
-
-      },
-      moreInfo: {
-        marginLeft: 0,
-        fontSize: 15
-      },
-      ok: {
-        backgroundColor: color.orange,
-        border: '1px solid ' + color.orange,
-        color: color.white,
-        float: 'right'
-      }
-    };
-
-    var i18n = {
-      storeDataMsg: commonMsg.shareWarningsStoreData(),
-      ageMsg: commonMsg.shareWarningsAge(),
-      moreInfo: commonMsg.shareWarningsMoreInfo(),
-      ok: commonMsg.dialogOK()
-    };
-
-    var dataPrompt, agePrompt;
-    if (this.props.showStoreDataAlert) {
-      dataPrompt = <div style={styles.dataMessage}>{i18n.storeDataMsg}</div>;
-    }
-    if (!this.props.is13Plus) {
-      agePrompt = (<div>
-        <div style={styles.ageMessage}>{i18n.ageMsg}</div>
-        <AgeDropdown style={styles.ageDropdonw} ref="age"/>
-      </div>);
-    }
-
+  render() {
     return (
       <div>
-        {dataPrompt}
-        {agePrompt}
+        {this.props.showStoreDataAlert &&
+          <div style={styles.dataMessage}>{commonMsg.shareWarningsStoreData()}</div>
+        }
+        {!this.props.is13Plus &&
+          <div>
+            <div style={styles.ageMessage}>{commonMsg.shareWarningsAge()}</div>
+            <AgeDropdown style={styles.ageDropdonw} ref="age"/>
+          </div>
+        }
         <div>
-          <a style={styles.moreInfo} target="_blank" href="https://code.org/privacy">{i18n.moreInfo}</a>
-          <button style={styles.ok} onClick={this.handleOk}>{i18n.ok}</button>
+          <a style={styles.moreInfo} target="_blank" href="https://code.org/privacy">{commonMsg.shareWarningsMoreInfo()}</a>
+          <button style={styles.ok} onClick={this.handleOk}>{commonMsg.dialogOK()}</button>
         </div>
       </div>
     );
   }
 });
+
+export default ShareWarnings;
