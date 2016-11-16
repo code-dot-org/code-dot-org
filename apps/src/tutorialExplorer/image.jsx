6ceedb5c439d8c195f241d2c116b78cd679c2226
Brendan Reville <brendan@code.org>
2016-11-16 14:30:27 -0800

    learn2016: code review feedback

--- apps/src/tutorialExplorer/image.jsx
+++ apps/src/tutorialExplorer/image.jsx
@@ -21,14 +21,18 @@ const Image = React.createClass({
     const imgTag = ReactDOM.findDOMNode(this.refs.img);
     const imgSrc = imgTag.getAttribute('src');
     const img = new window.Image();
-    img.onload = this.onImageLoad;
     img.src = imgSrc;
   },
 
   render() {
     const rootClassName = 'image ' + (this.state.loaded ? 'image-loaded' : '');
     return (
-      <img ref="img" {...this.props} className={rootClassName} />
+      <img
+        ref="img"
+        {...this.props}
+        className={rootClassName}
+        onLoad={this.onImageLoad}
+      />
     );
   }
 });
