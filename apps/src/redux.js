7643edcd849696c058c2f8290337ae34aba36be8
Paul Carduner <paul@carduner.net>
2016-11-15 17:09:12 -0800

    Add a comment explaining what this file is about

--- apps/src/redux.js
+++ apps/src/redux.js
@@ -1,3 +1,30 @@
+/**
+ *                                                _______________________
+ *      _______________________-------------------                       `\
+ *    /:--__                                                              |
+ *   ||< > |                                   ___________________________/
+ *   | \__/_________________-------------------                         |
+ *   |                                                                  |
+ *    |                   THE LORD OF THE REDUX STORES                   |
+ *    |                                                                  |
+ *    |     "Three Stores for the Elven-kings under the sky,             |
+ *     |       Seven for the Dwarf-lords in their halls of stone,         |
+ *     |     Nine for Mortal Men doomed to die,                           |
+ *     |       One for the Dark Lord on his dark throne                   |
+ *     |     In the Land of Mordor where the Shadows lie.                  |
+ *      |      One Store to rule them all, One Store to find them,         |
+ *      |      One Store to bring them all and in the darkness bind them   |
+ *      |    In the Land of Mordor where the Shadows lie.                 |
+ *     |                                              ____________________|_
+ *     |  ___________________-------------------------                      `\
+ *     |/`--_                                                                 |
+ *     ||[ ]||                                            ___________________/
+ *      \===/___________________--------------------------
+ *
+ *
+ * @file The global redux store - "There can be only one"
+ */
+
 import Immutable from 'immutable';
 import experiments from "./util/experiments";
 import * as redux from 'redux';
