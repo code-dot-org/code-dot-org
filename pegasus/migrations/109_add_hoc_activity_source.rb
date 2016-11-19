79c2bb48a74bdde8296ef61e6566f6c2fce702d1
Elijah Hamovitz <elijahhamovitz@gmail.com>
2016-11-19 14:38:22 -0800

    review feedback

--- pegasus/migrations/109_add_hoc_activity_source.rb
+++ pegasus/migrations/109_add_hoc_activity_source.rb
@@ -1,11 +1,6 @@
 Sequel.migration do
-  up do
+  change do
     add_column :hoc_activity, :source, String, size: 50
     add_index :hoc_activity, :source
-  end
-
-  down do
-    drop_index :hoc_activity, :source
-    drop_column :hoc_activity, :source
   end
 end
