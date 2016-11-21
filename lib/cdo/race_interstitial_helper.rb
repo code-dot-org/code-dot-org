acae4b4820b8cd42c1ab8942c75fe9ac5aba65c5
ewjordan <ewjordan@gmail.com>
2016-11-21 10:26:30 -0800

    Style

--- lib/cdo/race_interstitial_helper.rb
+++ lib/cdo/race_interstitial_helper.rb
@@ -9,10 +9,8 @@ module RaceInterstitialHelper
 
     # Restrict to cases where we can successfully geolocate to the US
     return false if request_ip.nil?
-    if request_ip == 'allow'
-      # For tests, we can't actually call Geocoder, so 'allow' pretends that check would pass
-      return true
-    end
+    # For tests, we can't actually call Geocoder, so 'allow' pretends that check would pass
+    return true if request_ip == 'allow'
     location = Geocoder.search(request_ip).first
     return false unless location
     return false if location.country_code.to_s.downcase != 'us'
