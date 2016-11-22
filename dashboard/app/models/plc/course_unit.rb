261bc5feb234ea0725ce15aaf627a778b3c579b0
Mehal Shah <mehal@code.org>
2016-11-22 14:31:03 -0800

    Make it easier to launch plc course units. Instead of a bunch of manual commands, all I have to do now is fire up a console and type Plc::CourseUnit.find_by(name: 'desired name').launch

--- dashboard/app/models/plc/course_unit.rb
+++ dashboard/app/models/plc/course_unit.rb
@@ -68,4 +68,14 @@ class Plc::CourseUnit < ActiveRecord::Base
 
     default_module_assignments
   end
+
+  def launch
+    update(started: true)
+
+    #All users who are enrolled in the course for this course unit need to be enrolled in it
+    plc_course.plc_enrollments.each do |enrollment|
+      assignment = Plc::EnrollmentUnitAssignment.find_or_create_by(user: enrollment.user, plc_user_course_enrollment: enrollment, plc_course_unit: self)
+      assignment.update(status: Plc::EnrollmentUnitAssignment::IN_PROGRESS)
+    end
+  end
 end
