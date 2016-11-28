3eae06dcc04a75afb4184dacf5b76d0eca48da0c
Brad Buchanan <brad@code.org>
2016-11-28 12:06:35 -0800

    Whitelist cookbooks and schema_cache.dump

--- tools/hooks/restrict_staging_changes.rb
+++ tools/hooks/restrict_staging_changes.rb
@@ -1,6 +1,18 @@
 require_relative 'hooks_utils.rb'
 
 REPO_DIR = File.expand_path('../../../', __FILE__)
+SCHEMA_CACHE_DUMP = File.expand_path(REPO_DIR + '/dashboard/db/schema_cache.dump', __FILE__)
+COOKBOOKS_DIR = File.expand_path(REPO_DIR + '/cookbooks', __FILE__)
+ERROR_MESSAGE = <<ERR
+Commit blocked!
+
+The staging branch is currently LOCKED for Hour of Code.  If you absolutely must
+commit to staging, please retry your commit with the --no-verify flag to bypass
+this lock.  Otherwise, new changes should go to the staging-next branch.
+
+  d[ o_0 ]b
+
+ERR
 
 Dir.chdir REPO_DIR
 branchname = `git rev-parse --abbrev-ref HEAD`.strip
@@ -8,12 +20,12 @@ branchname = `git rev-parse --abbrev-ref HEAD`.strip
 exit(0) unless branchname == 'staging'
 staged_files = HooksUtils.get_staged_files
 
-unless staged_files.empty?
-  raise <<ERR
-The staging branch is currently LOCKED for Hour of Code.  If you absolutely must
-commit to staging, please retry your commit with the --no-verify flag to bypass
-this lock.  Otherwise, new changes should go to the staging-next branch.
+staged_files.each do |filename|
+  # Allow schema_cache.dump file, which gets automatically committed by staging.
+  next if filename == SCHEMA_CACHE_DUMP
 
-  d[ o_0 ]b
-ERR
+  # Allow changes to cookbooks, which get automatically committed by staging.
+  next if filename.start_with?(COOKBOOKS_DIR)
+
+  raise ERROR_MESSAGE
 end
