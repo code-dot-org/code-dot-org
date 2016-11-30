02f8dc89df1be2789b223e546d17a644e079d828
Paul Carduner <paul@carduner.net>
2016-11-30 21:44:18 +0000

    Add tests for levelbuilder edit script

--- apps/src/sites/studio/pages/levelbuilder_edit_script.js
+++ apps/src/sites/studio/pages/levelbuilder_edit_script.js
@@ -8,42 +8,47 @@ import getScriptData from '@cdo/apps/util/getScriptData';
 import reducers, {init} from '@cdo/apps/lib/script-editor/editorRedux';
 import ScriptEditor from '@cdo/apps/lib/script-editor/ScriptEditor';
 
-const scriptEditorData = getScriptData('levelBuilderEditScript');
-const scriptData = scriptEditorData.script;
-const stages = scriptData.stages.filter(stage => stage.id).map(stage => ({
-  position: stage.position,
-  flex_category: stage.flex_category,
-  lockable: stage.lockable,
-  name: stage.name,
-  // Only include the first level of an assessment (uid ending with "_0").
-  levels: stage.levels.filter(level => !level.uid || /_0$/.test(level.uid)).map(level => ({
-    position: level.position,
-    activeId: level.activeId,
-    ids: level.ids.slice(),
-    kind: level.kind,
-    skin: level.skin,
-    videoKey: level.videoKey,
-    concepts: level.concepts,
-    conceptDifficulty: level.conceptDifficulty
-  }))
-}));
+export default function initPage(scriptEditorData) {
+  const scriptData = scriptEditorData.script;
+  const stages = scriptData.stages.filter(stage => stage.id).map(stage => ({
+    position: stage.position,
+    flex_category: stage.flex_category,
+    lockable: stage.lockable,
+    name: stage.name,
+    // Only include the first level of an assessment (uid ending with "_0").
+    levels: stage.levels.filter(level => !level.uid || /_0$/.test(level.uid)).map(level => ({
+      position: level.position,
+      activeId: level.activeId,
+      ids: level.ids.slice(),
+      kind: level.kind,
+      skin: level.skin,
+      videoKey: level.videoKey,
+      concepts: level.concepts,
+      conceptDifficulty: level.conceptDifficulty
+    }))
+  }));
 
-registerReducers(reducers);
-const store = getStore();
-store.dispatch(init(stages, scriptEditorData.levelKeyList));
+  registerReducers(reducers);
+  const store = getStore();
+  store.dispatch(init(stages, scriptEditorData.levelKeyList));
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ScriptEditor
-      beta={scriptEditorData.beta}
-      i18nData={scriptEditorData.i18n}
-      hidden={scriptData.hidden}
-      loginRequired={scriptData.loginRequired}
-      hideableStages={scriptData.hideable_stages}
-      professionalLearningCourse={scriptData.professionalLearningCourse}
-      peerReviewsRequired={scriptData.peerReviewsRequired}
-      wrapupVideo={scriptData.wrapupVideo}
-    />
-  </Provider>,
-  document.querySelector('.edit_container')
-);
+  ReactDOM.render(
+    <Provider store={store}>
+      <ScriptEditor
+        beta={scriptEditorData.beta}
+        i18nData={scriptEditorData.i18n}
+        hidden={scriptData.hidden}
+        loginRequired={scriptData.loginRequired}
+        hideableStages={scriptData.hideable_stages}
+        professionalLearningCourse={scriptData.professionalLearningCourse}
+        peerReviewsRequired={scriptData.peerReviewsRequired}
+        wrapupVideo={scriptData.wrapupVideo}
+      />
+    </Provider>,
+    document.querySelector('.edit_container')
+  );
+}
+
+if (!IN_UNIT_TEST) {
+  initPage(getScriptData('levelBuilderEditScript'));
+}
