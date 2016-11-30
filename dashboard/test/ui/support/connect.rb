e0fb309d726da512c8b1a96760faaca414d66956
Josh Lory <josh.lory@code.org>
2016-11-30 15:47:33 -0800

    Remove Selenium script_timeout
    See http://stackoverflow.com/a/31121340. This only affects execute_async_script which we don't use.

--- dashboard/test/ui/support/connect.rb
+++ dashboard/test/ui/support/connect.rb
@@ -68,7 +68,6 @@ def saucelabs_browser
 
   # let's allow much longer timeouts when searching for an element
   browser.manage.timeouts.implicit_wait = 2 * 60
-  browser.manage.timeouts.script_timeout = 1 * 60
 
   browser
 end
