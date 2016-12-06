da825e2f8df508adae9ba4ff4dace99c7fb4b86b
David Bailey <davidsbailey@users.noreply.github.com>
2016-12-06 15:30:40 -0800

    rescue errors when requiring config/environment

--- dashboard/test/ui/support/dashboard_helpers.rb
+++ dashboard/test/ui/support/dashboard_helpers.rb
@@ -3,9 +3,14 @@ module DashboardHelpers
   def require_rails_env
     start_time = Time.now
     puts "require_rails_env starting at #{start_time}"
-    require File.expand_path('../../../../config/environment.rb', __FILE__)
+    begin
+      require File.expand_path('../../../../config/environment.rb', __FILE__)
+    rescue
+      end_time = Time.now
+      puts "require_rails_env aborted at #{end_time} (#{end_time - start_time} seconds)"
+    end
     end_time = Time.now
-    puts "require_rails_env finished at #{end_time} (completed in #{end_time - start_time} seconds)"
+    puts "require_rails_env finished at #{end_time} (#{end_time - start_time} seconds)"
   end
 end
 
