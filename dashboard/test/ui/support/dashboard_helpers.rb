b423d252b547b0008cb600a4644dbacb29179123
David Bailey <davidsbailey@users.noreply.github.com>
2016-12-06 15:26:42 -0800

    add logging to help debug problems with require_rails_env affecting plc and teacher dashboard ui tests

--- dashboard/test/ui/support/dashboard_helpers.rb
+++ dashboard/test/ui/support/dashboard_helpers.rb
@@ -1,7 +1,11 @@
 module DashboardHelpers
   # Requires the full rails environment. Use sparingly, known to take 20-30s.
   def require_rails_env
+    start_time = Time.now
+    puts "require_rails_env starting at #{start_time}"
     require File.expand_path('../../../../config/environment.rb', __FILE__)
+    end_time = Time.now
+    puts "require_rails_env finished at #{end_time} (completed in #{end_time - start_time} seconds)"
   end
 end
 
