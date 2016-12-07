2143d3a758334505531ec141f17b3628145fd1de
David Bailey <davidsbailey@users.noreply.github.com>
2016-12-07 10:28:49 -0800

    add more logging to environment.rb

--- dashboard/config/environment.rb
+++ dashboard/config/environment.rb
@@ -1,13 +1,20 @@
 # Load the Rails application.
+last_time = Time.now
 require File.expand_path('../application', __FILE__)
+puts "required application in #{last_time - Time.now} seconds"
 
 # Force UTF-8 Encodings.
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
+last_time = Time.now
+
 # Explicitly load the abstract_mysql_adapter.  This is required when
 # loading a cached schema dump.
 require 'active_record/connection_adapters/abstract_mysql_adapter'
+puts "required active_record/connection_adapters/abstract_mysql_adapter in #{last_time - Time.now} seconds"
+last_time = Time.now
 
 # Initialize the Rails application.
 Dashboard::Application.initialize!
+puts "called Dashboard::Application.initialize! in #{last_time - Time.now} seconds"
