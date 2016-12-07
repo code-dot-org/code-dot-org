02c57a6d68d9c258561c20185c0209e66c103ffa
David Bailey <davidsbailey@users.noreply.github.com>
2016-12-07 13:06:19 -0800

    disable calling I18n.backend.init_translations during init for CircleCI

--- dashboard/config/initializers/fast_localization.rb
+++ dashboard/config/initializers/fast_localization.rb
@@ -14,7 +14,7 @@ end
 # Preload translations (before application fork, after i18n_railtie initializer)
 Dashboard::Application.config.after_initialize do |_|
   last_time = Time.now
-  I18n.backend.init_translations if I18n.backend.respond_to? :init_translations
+  I18n.backend.init_translations if (I18n.backend.respond_to? :init_translations) && !ENV['CI']
   I18n.t 'hello'
   $stderr.puts "I18n.backend.init_translations completed in #{(Time.now - last_time).to_i} seconds"
 end
