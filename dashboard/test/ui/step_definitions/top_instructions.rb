f2c89caa60d6e7d3312ab275d194f1860817000f
Josh Lory <josh.lory@code.org>
2016-12-07 16:25:14 -0800

    Update text-to-speech UI tests to avoid missing the pause button
    Sometimes WebDriver is too slow to catch the pause button when playing a short text-to-speech clip. The text-to-speech player is hidden on error, so just check that it's still visible after clicking "play".

--- dashboard/test/ui/step_definitions/top_instructions.rb
+++ dashboard/test/ui/step_definitions/top_instructions.rb
@@ -10,10 +10,9 @@ end
 # it all the way through, by clicking on play, waiting for "play" to
 # turn into "pause", and then waiting for "pause" to turn back into
 # "play"
-Then /^I listen to the entirety of the "(\d*)"th inline audio element$/ do |n|
+Then /^I listen to the (\d+)(?:st|nd|rd|th) inline audio element$/ do |n|
   steps <<-STEPS
     Then I click selector ".csf-top-instructions .inline-audio:eq(#{n}) button"
-    Then I wait until element ".csf-top-instructions .inline-audio:eq(#{n}) button img" is an img with src "/blockly/media/common_images/pause.png"
-    Then I wait until element ".csf-top-instructions .inline-audio:eq(#{n}) button img" is an img with src "/blockly/media/common_images/play.png"
+    And element ".csf-top-instructions .inline-audio:eq(#{n}) button img" is visible
   STEPS
 end
