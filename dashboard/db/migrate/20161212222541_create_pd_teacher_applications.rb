99de9b20e26d5cb6ce50d31cec9b314dd9b9deb7
Andrew Oberhardt <Andrew@Code.org>
2016-12-12 14:46:32 -0800

    Added migration to create pd_teacher_applications table

---
+++ dashboard/db/migrate/20161212222541_create_pd_teacher_applications.rb
@@ -0,0 +1,15 @@
+class CreatePdTeacherApplications < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pd_teacher_applications do |t|
+      t.timestamps
+
+      # Setting index false initially to create a unique index below. The unique param here is ignored.
+      t.belongs_to :user, null: false, index: false
+      t.index :user_id, unique: true
+
+      t.string :personal_email, null: false, index: true
+      t.string :school_email, null: false, index: true
+      t.json :application, null: false
+    end
+  end
+end
--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114020106) do
+ActiveRecord::Schema.define(version: 20161212222541) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -324,6 +324,18 @@ ActiveRecord::Schema.define(version: 20161114020106) do
     t.datetime "updated_at"
     t.datetime "deleted_at"
     t.index ["pd_workshop_id"], name: "index_pd_sessions_on_pd_workshop_id", using: :btree
+  end
+
+  create_table "pd_teacher_applications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.integer  "user_id",        null: false
+    t.string   "personal_email", null: false
+    t.string   "school_email",   null: false
+    t.json     "application",    null: false
+    t.index ["personal_email"], name: "index_pd_teacher_applications_on_personal_email", using: :btree
+    t.index ["school_email"], name: "index_pd_teacher_applications_on_school_email", using: :btree
+    t.index ["user_id"], name: "index_pd_teacher_applications_on_user_id", unique: true, using: :btree
   end
 
   create_table "pd_workshops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
