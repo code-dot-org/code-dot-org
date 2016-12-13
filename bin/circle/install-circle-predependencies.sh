f2853fd0736bc9a5864997d66f2b9811d15c1e5c
Brad Buchanan <brad@code.org>
2016-12-13 13:29:42 -0800

    Combine chrome and yarn scripts
    Allows us to only run `apt-get update` once saving us a few seconds per run.

---
+++ bin/circle/install-circle-predependencies.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Add google-chrome source
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+# Add yarn source
+# See docs at https://yarnpkg.com/en/docs/install#linux
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+# Retrieve updated source info only once
+sudo apt-get update
+
+# Update google chrome; Selenium requires that Chrome version must be >= 52.0.2743.0
+sudo apt-get --only-upgrade install google-chrome-stable
+
+# Install yarn
+sudo apt-get install yarn=0.16.1-1
