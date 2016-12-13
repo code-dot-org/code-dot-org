ee3b90bb91d267b4bbeffea8ee657e4b2f5e0ce0
Brad Buchanan <brad@code.org>
2016-12-12 16:10:53 -0800

    Be more Slack-idiomatic
    Check a token for the integration before doing anything.
    
    Post confirmation via the request response, not through our `HipChat` API wrapper.
    
    Respond directly to requester if their request is redundant, but post to whole room when request goes through.
    
    Mention requesting user's name in confirmation message.

--- pegasus/test/test_dev_routes.rb
+++ pegasus/test/test_dev_routes.rb
@@ -4,11 +4,15 @@ require 'mocha/mini_test'
 require_relative 'fixtures/mock_pegasus'
 
 BUILD_STARTED_PATH = deploy_dir('build-started')
+FAKE_START_BUILD_TOKEN = 'fake-start-build-token'
 
 class DevRoutesTest < Minitest::Test
   describe '/api/dev/start-build' do
     before do
       $log.level = Logger::ERROR # Pegasus spams debug logging otherwise
+
+      # Fake slack token for tests
+      CDO.slack_start_build_token = FAKE_START_BUILD_TOKEN
 
       # Should have no build-started file at the beginning of tests
       system "rm -f #{BUILD_STARTED_PATH}"
@@ -27,7 +31,10 @@ class DevRoutesTest < Minitest::Test
       in_rack_env(forbidden_env) do
         pegasus = make_test_pegasus
 
-        pegasus.post '/api/dev/start-build'
+        pegasus.post('/api/dev/start-build', {
+          token: FAKE_START_BUILD_TOKEN,
+          user_name: 'Dave'
+        })
         assert_equal 403, pegasus.last_response.status
         refute File.file? BUILD_STARTED_PATH
       end
@@ -50,11 +57,13 @@ class DevRoutesTest < Minitest::Test
     end
 
     def assert_allowed_on_rack_env(allowed_env)
-      HipChat.stubs(:log) # Suppress output
       in_rack_env(allowed_env) do
         pegasus = make_test_pegasus
         begin
-          pegasus.post '/api/dev/start-build'
+          pegasus.post('/api/dev/start-build', {
+              token: FAKE_START_BUILD_TOKEN,
+              user_name: 'Dave'
+          })
           assert_equal 200, pegasus.last_response.status
         ensure
           system "rm -f #{BUILD_STARTED_PATH}"
@@ -62,15 +71,43 @@ class DevRoutesTest < Minitest::Test
       end
     end
 
+    it 'is forbidden with a missing token' do
+      in_rack_env(:staging) do
+        pegasus = make_test_pegasus
+        pegasus.post('/api/dev/start-build', {
+            user_name: 'Dave'
+        })
+        assert_equal 403, pegasus.last_response.status
+      end
+    end
+
+    it 'is forbidden with an incorrect token' do
+      in_rack_env(:staging) do
+        pegasus = make_test_pegasus
+        pegasus.post('/api/dev/start-build', {
+            token: 'incorrect-token',
+            user_name: 'Dave'
+        })
+        assert_equal 403, pegasus.last_response.status
+      end
+    end
+
     it 'generates a start_build file if none exists' do
-      HipChat.expects(:log).with('Build restarted via /api/dev/start-build')
       in_rack_env(:test) do
         pegasus = make_test_pegasus
         begin
           refute File.file? BUILD_STARTED_PATH
-          pegasus.post '/api/dev/start-build'
-          assert_equal 200, pegasus.last_response.status
+          pegasus.post('/api/dev/start-build', {
+              token: FAKE_START_BUILD_TOKEN,
+              user_name: 'Dave'
+          })
           assert File.file? BUILD_STARTED_PATH
+
+          # Check appropriate response to whole room, too
+          assert_equal 200, pegasus.last_response.status
+          response_body = JSON.parse(pegasus.last_response.body)
+          assert_equal 'Test build restarted by Dave', response_body['text']
+          assert_equal 'in_channel', response_body['response_type']
         ensure
           system "rm -f #{BUILD_STARTED_PATH}"
         end
@@ -78,7 +115,6 @@ class DevRoutesTest < Minitest::Test
     end
 
     it 'succeeds without action if start_build exists' do
-      HipChat.expects(:log).with('Build restart requested via /api/dev/start-build was ignored - a build is already queued')
       in_rack_env(:test) do
         pegasus = make_test_pegasus
         begin
@@ -86,10 +122,16 @@ class DevRoutesTest < Minitest::Test
           assert File.file? BUILD_STARTED_PATH
           original_modify_time = File.mtime(BUILD_STARTED_PATH)
 
-          pegasus.post '/api/dev/start-build'
-          assert_equal 200, pegasus.last_response.status
+          pegasus.post('/api/dev/start-build', {
+              token: FAKE_START_BUILD_TOKEN,
+              user_name: 'Dave'
+          })
           assert File.file? BUILD_STARTED_PATH
           assert_equal original_modify_time, File.mtime(BUILD_STARTED_PATH)
+
+          # Check response to requester
+          assert_equal 200, pegasus.last_response.status
+          assert_equal "I can't do that Dave - a build is already queued", pegasus.last_response.body
         ensure
           system "rm -f #{BUILD_STARTED_PATH}"
         end
