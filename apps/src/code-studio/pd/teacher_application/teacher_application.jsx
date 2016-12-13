7b63a600d8bbceb0324e729db7d62a3ef015df14
Mehal Shah <mehal@code.org>
2016-12-13 15:29:05 -0800

    Remove block comment

--- apps/src/code-studio/pd/teacher_application/teacher_application.jsx
+++ apps/src/code-studio/pd/teacher_application/teacher_application.jsx
@@ -491,59 +491,6 @@ const TeacherApplication = React.createClass({
       </div>
     );
   },
-/*
-  getDistrictDropdownValues() {
-    //The district dropdown is not a react component like the rest of this form's components.
-    //That's why we're doing it separately here
-    const districtValues = {
-      ['us-or-international']: document.getElementById('us-or-international').value,
-      ['school-type']: document.getElementById('school-type').value,
-      ['school-state']: document.getElementById('school-state').value,
-      ['school-district']: document.querySelector('#school-district input').value,
-      ['school']: document.querySelector('#school input').value
-    };
-
-    if (document.getElementById('school-district-other').checked) {
-      _.assign(districtValues, {
-        ['school-district-name']: document.getElementById('school-district-name').value
-      });
-    }
-
-    if (document.getElementById('school-district-other').checked ||
-      document.getElementById('school-other').checked) {
-      _.assign(districtValues, {
-        ['school-name']: document.getElementById('school-name').value,
-        ['school-zipcode']: document.getElementById('school-zipcode').value
-      });
-    }
-
-    return districtValues;
-  },
-
-  validateDistrictData(formData) {
-    //Because the district dropdown isn't in react, we have to validate it differently. We can do this by looking at the
-    //form data that we pulled in getDistrictDropdowns and seeing if all of it has been filled out.
-
-    //These three must always be filled out
-    const baseDistrictDataCompleted = !!(formData['us-or-international'] && formData['school-type'] && formData['school-state']);
-
-    if (!baseDistrictDataCompleted) {
-      return false;
-    }
-
-    if (document.getElementById('school-district-other').checked) {
-      //If you clicked "other district" then expect these three to be filled out
-      return !!(formData['school-district-name'] && formData['school-name'] && formData['school-zipcode']);
-    } else if (document.getElementById('school-other').checked) {
-      //If you clicked "other school in this district" then expect these three to be filled out
-      return !!(formData['school-district'] && formData['school-name'] && formData['school-zipcode']);
-    } else {
-      //If you clicked neither, then expect these two to be filled out
-      return !!(formData['school-district'] && formData['school']);
-    }
-
-  },
-  */
 
   onSubmitButtonClick() {
     const districtValues = getDistrictDropdownValues();
