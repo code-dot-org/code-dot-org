0cd9a8e52a8b4623fe7207fcfa49b68aaa5bca40
Paul Carduner <paul@carduner.net>
2016-12-14 18:44:46 +0000

    add more links to the right places

--- dashboard/test/mailers/previews/pd_teacher_application_preview.rb
+++ dashboard/test/mailers/previews/pd_teacher_application_preview.rb
@@ -17,8 +17,10 @@ class Pd::TeacherApplicationMailerPreview < ActionMailer::Preview
       "Minerva",
       "McGonagall",
       "minerva@hogwarts.co.uk",
+      "https://www.google.com/url?q=https://docs.google.com/forms/d/e/1FAIpQLScVReYg18EYXvOFN2mQkDpDFgoVqKVv0bWOSE1LFSY34kyEHQ/viewform?entry.1124819666%3DTEACHER%2BNAME%26entry.1772278630%3DSCHOOL%2BNAME%26entry.1885703098%26entry.1693544%26entry.164045958%26entry.2063346846%3DAPPLICATION%2BID&sa=D&ust=1481731427499000&usg=AFQjCNEaaz0F4Z29N64LiCxkUwHudH5gig",
       "Rubeus Hagrid",
-      "Computer Science Discoveries"
+      "CS Discoveries",
+      "https://code.org/educate/professional-learning/cs-discoveries"
     )
   end
 
