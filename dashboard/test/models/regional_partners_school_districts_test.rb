172c292af9c790f266adec8c60667ed7e8ea3dac
David Bailey <davidsbailey@users.noreply.github.com>
2016-12-13 22:51:16 -0800

    add unit tests

---
+++ dashboard/test/models/regional_partners_school_districts_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class RegionalPartnersSchoolDistrictTest < ActiveSupport::TestCase
+  include ActiveSupport::Testing::Stream
+
+  test "regional partners initialized from tsv" do
+    # populate school_districts and regional_partners, which we depend on as foreign keys
+    RegionalPartner.find_or_create_all_from_tsv('test/fixtures/regional_partners.tsv')
+    SchoolDistrict.find_or_create_all_from_tsv('test/fixtures/school_districts.tsv')
+
+    RegionalPartnersSchoolDistrict.find_or_create_all_from_tsv('test/fixtures/regional_partners_school_districts.tsv')
+
+    district = SchoolDistrict.find(100002)
+    partner = district.regional_partner
+    assert_not_nil partner
+    assert_equal partner.name, 'A+ College Ready'
+    assert_equal partner.group, 1
+
+    district = SchoolDistrict.find(200001)
+    partner = district.regional_partner
+    assert_nil partner
+
+    # rubocop:disable Style/NumericLiterals
+    district = SchoolDistrict.find(4800004)
+    partner = district.regional_partner
+    assert_not_nil partner
+    assert_equal partner.name, 'Center for STEM Education, The University of Texas at Austin'
+    assert_equal partner.group, 2
+  end
+end
