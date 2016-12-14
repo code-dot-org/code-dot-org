5aeb61e2062d632b9883e60582420a341a8d1a4f
Mehal Shah <mehal@code.org>
2016-12-14 10:11:19 -0800

    Checkpoint

--- apps/src/sites/studio/pages/pd/teacher_application/new.js
+++ apps/src/sites/studio/pages/pd/teacher_application/new.js
@@ -3,3 +3,20 @@ import ReactDOM from 'react-dom';
 import TeacherApplication from '@cdo/apps/code-studio/pd/teacher_application/teacher_application.jsx';
 
 ReactDOM.render(<TeacherApplication/>, document.getElementById('application-container'));
+
+$('#school-district').change(function () {
+  let districtValue = $('#school-district input').val();
+
+  if (districtValue) {
+    $.ajax({
+      method: "GET",
+      url: `/api/v1/regional-partners/${districtValue}.json`
+    }).done(data => {
+      let regionalPartnerGroup = data['group']
+      ReactDOM.render(<TeacherApplication regionalPartnerGroup={regionalPartnerGroup}/>, document.getElementById('application-container'));
+    }).fail(data => {
+      console.log(`error: ${data}`)
+    })
+  }
+})
+
