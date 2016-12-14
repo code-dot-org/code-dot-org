b8c39fe1a4309dfe51e3c76629ef10a2113de401
Mehal Shah <mehal@code.org>
2016-12-14 13:26:01 -0800

    Undo haml change, undo rails controller stubbing

--- dashboard/app/controllers/api/v1/regional_partners_controller.rb
+++ dashboard/app/controllers/api/v1/regional_partners_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::RegionalPartnersController < ApplicationController
   # GET /api/v1/regional-partner/<school_district_id>
   def index
     school_district = SchoolDistrict.find(params[:school_district_id])
-    render json: {group: 1}#school_district.regional_partner
+    render json: school_district.regional_partner
   end
 end
