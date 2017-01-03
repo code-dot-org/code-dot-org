de09a3da723d7ff82613e5c220c67a8311918186
Josh Lory <josh.lory@code.org>
2017-01-02 22:24:21 -0800

    Redirect back to /pd/teacher_application after authorizing from that URL
    If a user isn't logged in when they visit /pd/teacher_application, make sure we return to this page after they log in or sign up.

--- dashboard/app/controllers/pd/teacher_application_controller.rb
+++ dashboard/app/controllers/pd/teacher_application_controller.rb
@@ -1,5 +1,6 @@
 class Pd::TeacherApplicationController < ApplicationController
   def new
+    session[:return_to] = pd_teacher_application_path
     authorize! :create, Pd::TeacherApplication
 
     if Pd::TeacherApplication.exists?(user: current_user)
