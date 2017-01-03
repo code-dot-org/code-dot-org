98dbfa7623a6d396d14ce2556b2d9074af533253
Josh Lory <josh.lory@code.org>
2017-01-03 10:36:07 -0800

    Code review feedback: clear `session[:return_to]` if already logged in

--- dashboard/app/controllers/pd/teacher_application_controller.rb
+++ dashboard/app/controllers/pd/teacher_application_controller.rb
@@ -2,6 +2,7 @@ class Pd::TeacherApplicationController < ApplicationController
   def new
     session[:return_to] = pd_teacher_application_path
     authorize! :create, Pd::TeacherApplication
+    session.delete :return_to
 
     if Pd::TeacherApplication.exists?(user: current_user)
       render :submitted
