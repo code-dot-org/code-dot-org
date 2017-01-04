e4241073af9d996d7ca13e0a3cdafd97062ce42d
Paul Carduner <paul@carduner.net>
2017-01-04 00:57:45 +0000

    Refactor minecraft styling logic and make it show up correctly in the storybook

--- apps/src/templates/GameButtons.story.jsx
+++ apps/src/templates/GameButtons.story.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import {getStore, registerReducers} from '../redux';
-import * as commonReducers from '../redux/commonReducers';
-import GameButtons, {RunButton, ResetButton} from './GameButtons';
+import {UnconnectedGameButtons, RunButton, ResetButton} from './GameButtons';
 
-registerReducers(commonReducers);
-
+// le sigh... we can't just import the craft stylesheet because
+// it changes global elements. So instead we have to import the below
+// sass stylesheet which scopes all the craft rules to only work
+// inside the specific story cells that need them.
+import './GameButtons.story.scss';
 
 export default function (storybook) {
   RunButton.displayName = 'RunButton';
@@ -20,8 +20,16 @@ export default function (storybook) {
       },
       {
         name: 'minecraft version',
-        description: 'this is special for some reason',
-        story: () => <div className="minecraft"><RunButton isMinecraft/></div>,
+        description: `
+          this shows up if you wrap the button in a ".minecraft" element
+          and include the appropriate minecraft stylesheet.
+        `,
+        storyCellClass: 'GameButtonsStory loadCraftStyleSheet',
+        story: () => (
+          <div className="minecraft">
+            <RunButton />
+          </div>
+        ),
       },
     ]);
 
@@ -35,13 +43,31 @@ export default function (storybook) {
       },
       {
         name: 'minecraft version',
-        description: 'this is special for some reason',
-        story: () => <div className="minecraft"><ResetButton style={{display: 'block'}} isMinecraft /></div>
+        description: `
+          this shows up if you wrap the button in a ".minecraft" element
+          and include the appropriate minecraft stylesheet.
+        `,
+        storyCellClass: 'GameButtonsStory loadCraftStyleSheet',
+        story: () => (
+          <div className="minecraft">
+            <ResetButton style={{display: 'block'}} />
+          </div>
+        )
       },
       {
         name: 'text hidden',
         description: 'You can hide the text with the hideText prop',
         story: () => <ResetButton style={{display: 'block'}} hideText />
+      },
+      {
+        name: 'minecraft text hidden',
+        description: 'You can hide the text with the hideText prop',
+        storyCellClass: 'GameButtonsStory loadCraftStyleSheet',
+        story: () => (
+          <div className="minecraft">
+            <ResetButton style={{display: 'block'}} hideText />
+          </div>
+        ),
       }
     ]);
 
@@ -50,11 +76,45 @@ export default function (storybook) {
     .addStoryTable([
       {
         name: 'default',
-        description: 'You have to explicitly set display: block to make this show up. It is hidden by default?!',
+        description: 'The default version with no props',
         story: () => (
-          <Provider store={getStore()}>
-            <GameButtons />
-          </Provider>
+          <UnconnectedGameButtons />
+        ),
+      },
+      {
+        name: 'playspace phone frame',
+        description: 'the playspace phone frame just renders an empty div...',
+        story: () => (
+          <UnconnectedGameButtons playspacePhoneFrame />
+        ),
+      },
+      {
+        name: 'hideRunButton',
+        description: 'the run button is hidden when this prop is supplied',
+        story: () => (
+          <UnconnectedGameButtons hideRunButton />
+        ),
+      },
+      {
+        name: 'minecraft version',
+        description: `
+          this shows up if you wrap the button in a ".minecraft" element
+          and include the appropriate minecraft stylesheet.
+        `,
+        storyCellClass: 'GameButtonsStory loadCraftStyleSheet',
+        story: () => (
+          <div className="minecraft">
+            <UnconnectedGameButtons />
+          </div>
+        ),
+      },
+      {
+        name: 'with extra content',
+        description: 'Additional children can be provided',
+        story: () => (
+          <UnconnectedGameButtons>
+            <button>another button</button>
+          </UnconnectedGameButtons>
         ),
       },
     ]);
