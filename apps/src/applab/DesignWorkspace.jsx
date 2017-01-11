2cc5c5e2dbbfd3792eff4448fe5d2bd2ac4da9f4
David Bailey <davidsbailey@users.noreply.github.com>
2017-01-11 14:47:15 -0800

    dim the design mode headers when the app is running and supply rtl params

--- apps/src/applab/DesignWorkspace.jsx
+++ apps/src/applab/DesignWorkspace.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import DesignModeBox from './DesignModeBox';
 import DesignModeHeaders from './DesignModeHeaders';
+import {connect} from 'react-redux';
 
-export default React.createClass({
+const DesignWorkspace = React.createClass({
   propTypes: {
     handleManageAssets: React.PropTypes.func.isRequired,
     handleVersionHistory: React.PropTypes.func.isRequired,
@@ -15,7 +16,11 @@ export default React.createClass({
     onDuplicate: React.PropTypes.func.isRequired,
     onDelete: React.PropTypes.func.isRequired,
     onInsertEvent: React.PropTypes.func.isRequired,
-    isDimmed: React.PropTypes.bool.isRequired
+    isDimmed: React.PropTypes.bool.isRequired,
+
+    // provided by redux
+    isRunning: React.PropTypes.bool.isRequired,
+    localeDirection: React.PropTypes.oneOf(['rtl', 'ltr']).isRequired,
   },
 
   getInitialState: function () {
@@ -37,6 +42,8 @@ export default React.createClass({
         handleVersionHistory={this.props.handleVersionHistory}
         onToggleToolbox={this.onToggleToolbox}
         isToolboxVisible={this.state.isToolboxVisible}
+        localeDirection={this.props.localeDirection}
+        isRunning={this.props.isRunning}
       />
       <DesignModeBox
         handleDragStart={this.props.handleDragStart}
@@ -54,3 +61,7 @@ export default React.createClass({
     </div>);
   }
 });
+export default connect(state => ({
+  localeDirection: state.pageConstants.localeDirection,
+  isRunning: !!state.runState.isRunning,
+}))(DesignWorkspace);
