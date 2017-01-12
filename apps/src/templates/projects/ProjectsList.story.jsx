ca6fe80812dba4e2b4bd3f3610580a3a86f68d00
David Bailey <davidsbailey@users.noreply.github.com>
2017-01-12 10:09:36 -0800

    render stub data in grid using reactabular

--- apps/src/templates/projects/ProjectsList.story.jsx
+++ apps/src/templates/projects/ProjectsList.story.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ProjectsList from './ProjectsList';
 
+const STUB_PROJECTS_DATA = [
+  {studentName: 'Alice', name: 'Antelope Freeway', type: 'applab', updatedAt: '2017-01-13'},
+  {studentName: 'Bob', name: 'Batyote', type: 'gamelab', updatedAt: '2016-12-31'},
+];
+
 export default storybook => {
   return storybook
     .storiesOf('ProjectsList', module)
@@ -9,7 +14,7 @@ export default storybook => {
         name: 'basic projects list',
         description: `This is a simple projects list with stub data.`,
         story: () => (
-          <ProjectsList/>
+          <ProjectsList projectsData={STUB_PROJECTS_DATA}/>
         )
       },
     ]);
