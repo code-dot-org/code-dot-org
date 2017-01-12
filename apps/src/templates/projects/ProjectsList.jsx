909672a67aae95666f079d78d7367fcaf919f9fe
David Bailey <davidsbailey@users.noreply.github.com>
2017-01-12 10:09:36 -0800

    render stub data in grid using reactabular

--- apps/src/templates/projects/ProjectsList.jsx
+++ apps/src/templates/projects/ProjectsList.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import {Table} from 'reactabular';
 
 const ProjectsList = React.createClass({
+  propTypes: {
+    projectsData: React.PropTypes.array.isRequired,
+  },
+
+  getColumns() {
+    return [
+      {
+        property: 'studentName',
+        header: {
+          label: 'Student Name',
+        }
+      },
+      {
+        property: 'name',
+        header: {
+          label: 'Project Name',
+        }
+      },
+      {
+        property: 'type',
+        header: {
+          label: 'Project Type',
+        }
+      },
+      {
+        property: 'updatedAt',
+        header: {
+          label: 'Last Edited',
+        }
+      },
+    ];
+  },
+
   render() {
     return (
-      <div>my projects list</div>
+      <div>
+        <h2>my projects list</h2>
+        <Table.Provider
+          className="pure-table pure-table-striped"
+          columns={this.getColumns()}
+        >
+          <Table.Header />
+
+          <Table.Body rows={this.props.projectsData} rowKey="updatedAt" />
+        </Table.Provider>
+      </div>
     );
   }
 });
