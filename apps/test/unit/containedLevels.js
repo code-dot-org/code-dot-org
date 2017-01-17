f28b3fe8c9593708e3ba1619d28aa144c97668fa
Brent Van Minnen <bjvanminnen@gmail.com>
2017-01-17 11:21:04 -0800

    add some tests

---
+++ apps/test/unit/containedLevels.js
@@ -0,0 +1,43 @@
+import assert from 'assert';
+import sinon from 'sinon';
+import * as codeStudioLevels from '@cdo/apps/code-studio/levels/codeStudioLevels';
+import { getContainedLevelResultInfo } from '@cdo/apps/containedLevels';
+import { TestResults } from '@cdo/apps/constants';
+
+describe('getContainedLevelResultInfo', () => {
+  const containedLevelResult = {
+    id: 6669,
+    app: 'multi',
+    callback: 'http://localhost-studio.code.org:3000/milestone/2023/16504/6669',
+    result: {
+      response: 1,
+      result: false,
+      errorType: null,
+      submitted: false,
+      valid: true
+    },
+    feedback: 'This is feedback'
+  };
+
+  before(() => {
+    sinon.stub(codeStudioLevels, 'getContainedLevelResult', () => containedLevelResult);
+  });
+
+  after(() => {
+    codeStudioLevels.getContainedLevelResult.restore();
+  });
+
+  it('getContainedLevelResultInfo', () => {
+      const info = getContainedLevelResultInfo();
+      assert.deepEqual(info, {
+        app: 'multi',
+        level: 6669,
+        callback: 'http://localhost-studio.code.org:3000/milestone/2023/16504/6669',
+        result: true,
+        testResult: TestResults.UNVALIDATED_SUBLEVEL,
+        program: 1,
+        feedback: 'This is feedback',
+        submitted: false
+      });
+  });
+});
