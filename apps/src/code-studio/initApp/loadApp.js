3cfdca2595a59534e3da4c730275f4cfcdf3c681
Brian Jordan <bcjordan@gmail.com>
2017-01-18 15:04:58 -0800

    Stub out new methods for pixelation projects stub. Add warning. Add eslint ignore and note.

--- apps/src/code-studio/initApp/loadApp.js
+++ apps/src/code-studio/initApp/loadApp.js
@@ -371,6 +371,10 @@ window.apps = {
   // Define blockly/droplet-specific callbacks for projects to access
   // level source, HTML and headers.
   sourceHandler: {
+    /**
+     * NOTE: when adding a new method here, ensure that all other sourceHandlers
+     * (e.g. in pixelation.js) have that same method defined.
+     */
     setMakerAPIsEnabled: function (enableMakerAPIs) {
       getAppOptions().level.makerlabEnabled = enableMakerAPIs;
     },
