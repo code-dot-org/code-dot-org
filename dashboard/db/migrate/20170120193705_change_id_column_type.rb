96b2adaf4685f57b67f6ae3e270b6a36a62a60e1
ashercodeorg <asher@code.org>
2017-01-20 14:08:04 -0600

    Moves from change to up and down for rollback.

--- dashboard/db/migrate/20170120193705_change_id_column_type.rb
+++ dashboard/db/migrate/20170120193705_change_id_column_type.rb
@@ -1,5 +1,9 @@
 class ChangeIdColumnType < ActiveRecord::Migration[5.0]
-  def change
+  def up
     change_column :activities, :id, :bigint
+  end
+
+  def down
+    change_column :activities, :id, :integer
   end
 end
