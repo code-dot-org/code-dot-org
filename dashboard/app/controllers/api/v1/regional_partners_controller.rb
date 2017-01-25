e08e7224ba7757b98adc6ab7d615ae4a374aa185
Mehal Shah <mehal@code.org>
2017-01-24 19:12:08 -0800

    Bug fixes

--- dashboard/app/controllers/api/v1/regional_partners_controller.rb
+++ dashboard/app/controllers/api/v1/regional_partners_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::RegionalPartnersController < ApplicationController
     if params[:course] == 'unselected'
       render json: school_district.regional_partners_school_districts.first, include: :regional_partner
     else
-      regional_partner_mapping = school_district.regional_partners_school_districts.find_by(course: params[:course]) || school_district.regional_partners_school_districts.first
+      regional_partner_mapping = school_district.regional_partners_school_districts.find_by(course: params[:course]) || school_district.regional_partners_school_districts.find_by(course: nil)
 
       render json: regional_partner_mapping, include: :regional_partner
     end
