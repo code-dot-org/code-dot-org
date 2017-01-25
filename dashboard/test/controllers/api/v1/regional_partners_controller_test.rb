6edbb60e3ce4ceea7744321e27515cddff688c74
Mehal Shah <mehal@code.org>
2017-01-24 17:39:46 -0800

    Fix the seeding, and unit tests

--- dashboard/test/controllers/api/v1/regional_partners_controller_test.rb
+++ dashboard/test/controllers/api/v1/regional_partners_controller_test.rb
@@ -25,4 +25,22 @@ class Api::V1::RegionalPartnersControllerTest < ActionController::TestCase
     response = JSON.parse(@response.body)
     assert_not_nil response['regional_partner']['name']
   end
+
+  test 'index finds the regional partner and workshop days for a given district and course' do
+    [
+        ['csd', '200001', 'A+ College Ready', 'June 10-15, 2017'],
+        ['csp', '200001', 'A+ College Ready', 'June 10-15, 2017'],
+        ['unselected', '200001', 'A+ College Ready', 'June 10-15, 2017'],
+        ['csd', '200003', 'A+ College Ready', 'June 16-20, 2017'],
+        ['csp', '200003', 'A+ College Ready', 'June 16-20, 2017'],
+        ['unselected', '200003', 'A+ College Ready', 'June 16-20, 2017'],
+        ['csd', '600001', 'A+ College Ready', 'June 1-5, 2017'],
+        ['csp', '600001', 'A+ College Ready', 'June 21-25, 2017'],
+    ].each do |course, district, regional_partner, workshop_days|
+      get :index, params: {school_district_id: district, course: course}
+      response = JSON.parse(@response.body)
+      assert_equal regional_partner, response['regional_partner']['name']
+      assert_equal workshop_days, response['workshop_days']
+    end
+  end
 end
