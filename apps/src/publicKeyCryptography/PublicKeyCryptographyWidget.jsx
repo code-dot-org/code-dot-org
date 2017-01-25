3e346447172ff14a35e98e2552c98c67aa658064
Brad Buchanan <brad@code.org>
2017-01-24 16:22:53 -0800

    Add single-character views
    Adds single-character views and a view toggle to the Public Key Cryptography Widget.  Adds instructions for each character.  Moves the "Start Over" and "Finish" buttons to the top of the view.
    
    Note: This change leaves the widget in a broken state - there are a number of handlers that assume a particular character will always be onscreen, which leads to uncaught exceptions.

--- apps/src/publicKeyCryptography/PublicKeyCryptographyWidget.jsx
+++ apps/src/publicKeyCryptography/PublicKeyCryptographyWidget.jsx
@@ -1,13 +1,24 @@
 /** @file Root component for Public Key Cryptography widget */
 import React from 'react';
 import i18n from '@cdo/locale';
+import CollapsiblePanel from './CollapsiblePanel';
 import EqualColumns from './EqualColumns';
 import Alice from './Alice';
-import Bob from './Bob';
+import AliceInstructions from './AliceInstructions';
 import Eve from './Eve';
+import EveInstructions from './EveInstructions';
+import Bob from './Bob';
+import BobInstructions from './BobInstructions';
 import ModuloClock from './ModuloClock';
 import Dialog from '../templates/Dialog';
 import WidgetContinueButton from '../templates/WidgetContinueButton';
+import ToggleGroup from '../templates/ToggleGroup';
+
+// Magic strings for view modes
+const ALICE_VIEW = 'alice';
+const EVE_VIEW = 'eve';
+const BOB_VIEW = 'bob';
+const ALL_VIEW = 'all';
 
 const style = {
   root: {
@@ -26,8 +37,13 @@ const PublicKeyCryptographyWidget = React.createClass({
   getInitialState() {
     return {
       animating: false,
-      publicModulus: null
+      publicModulus: null,
+      selectedCharacter: null
     };
+  },
+
+  setSelectedCharacter(selectedCharacter) {
+    this.setState({selectedCharacter});
   },
 
   setPublicModulus(publicModulus) {
@@ -66,41 +82,145 @@ const PublicKeyCryptographyWidget = React.createClass({
     this.setState({publicModulus: null});
   },
 
+  renderCharacterView() {
+    if (ALICE_VIEW === this.state.selectedCharacter) {
+      return (
+        <EqualColumns intercolumnarDistance={20}>
+          <AliceInstructions/>
+          {this.renderAliceControls()}
+          {this.renderModuloClockColumn()}
+        </EqualColumns>
+      );
+    } else if (EVE_VIEW === this.state.selectedCharacter) {
+      return (
+        <EqualColumns intercolumnarDistance={20}>
+          <EveInstructions/>
+          {this.renderEveControls()}
+          {this.renderModuloClockColumn()}
+        </EqualColumns>
+      );
+    } else if (BOB_VIEW === this.state.selectedCharacter) {
+      return (
+        <EqualColumns intercolumnarDistance={20}>
+          <BobInstructions/>
+          {this.renderBobControls()}
+          {this.renderModuloClockColumn()}
+        </EqualColumns>
+      );
+    } else if (ALL_VIEW === this.state.selectedCharacter) {
+      return (
+        <span>
+          <EqualColumns intercolumnarDistance={20}>
+            {this.renderAliceControls()}
+            {this.renderEveControls()}
+            {this.renderBobControls()}
+          </EqualColumns>
+          {this.renderModuloClock()}
+        </span>
+      );
+    }
+    return (
+      <div style={{textAlign: 'center'}}>
+        Select a character.
+      </div>
+    );
+  },
+
+  renderAliceControls() {
+    return (
+      <Alice
+        ref={x => this.alice = x}
+        disabled={this.state.animating}
+        setPublicModulus={this.setPublicModulus}
+        setPublicKey={this.setPublicKey}
+        runModuloClock={this.runModuloClock}
+      />
+    );
+  },
+
+  renderEveControls() {
+    return (
+      <Eve
+        ref={x => this.eve = x}
+        disabled={this.state.animating}
+        setPublicModulus={this.setPublicModulus}
+        runModuloClock={this.runModuloClock}
+      />
+    );
+  },
+
+  renderBobControls() {
+    return (
+      <Bob
+        ref={x => this.bob = x}
+        disabled={this.state.animating}
+        setPublicModulus={this.setPublicModulus}
+        setPublicNumber={this.setPublicNumber}
+        runModuloClock={this.runModuloClock}
+      />
+    );
+  },
+
+  renderModuloClockColumn() {
+    return (
+      <CollapsiblePanel title="Modulo clock">
+        {this.renderModuloClock()}
+      </CollapsiblePanel>
+    );
+  },
+
+  renderModuloClock() {
+    return (
+      <ModuloClock
+        ref={x => this.moduloClock = x}
+        modulus={this.state.publicModulus || 1}
+      />
+    );
+  },
+
   render() {
     return (
       <div style={style.root}>
-        <EqualColumns intercolumnarDistance={20}>
-          <Alice
-            ref={x => this.alice = x}
-            disabled={this.state.animating}
-            setPublicModulus={this.setPublicModulus}
-            setPublicKey={this.setPublicKey}
-            runModuloClock={this.runModuloClock}
-          />
-          <Eve
-            ref={x => this.eve = x}
-            disabled={this.state.animating}
-            setPublicModulus={this.setPublicModulus}
-            runModuloClock={this.runModuloClock}
-          />
-          <Bob
-            ref={x => this.bob = x}
-            disabled={this.state.animating}
-            setPublicModulus={this.setPublicModulus}
-            setPublicNumber={this.setPublicNumber}
-            runModuloClock={this.runModuloClock}
-          />
-        </EqualColumns>
-        <ModuloClock
-          ref={x => this.moduloClock = x}
-          modulus={this.state.publicModulus || 1}
+        <CharacterSelect
+          selectedCharacter={this.state.selectedCharacter}
+          onChange={this.setSelectedCharacter}
         />
         <WidgetContinueButton/>
         <StartOverButton onClick={this.onStartOverClick}/>
+        <div style={{clear: 'both', marginTop: 10}}>
+          {this.renderCharacterView()}
+        </div>
       </div>);
   }
 });
 export default PublicKeyCryptographyWidget;
+
+/**
+ * Toggle group of character view options: Alice|Eve|Bob|All
+ */
+const CharacterSelect = props => (
+  <span>
+    <strong style={characterSelectTextStyle}>Pick a character:</strong>
+    <ToggleGroup
+      selected={props.selectedCharacter}
+      onChange={props.onChange}
+    >
+      <button value={ALICE_VIEW}>Alice</button>
+      <button value={EVE_VIEW}>Eve</button>
+      <button value={BOB_VIEW}>Bob</button>
+      <button value={ALL_VIEW}>All</button>
+    </ToggleGroup>
+  </span>
+);
+CharacterSelect.propTypes = {
+  selectedCharacter: React.PropTypes.string,
+  onChange: React.PropTypes.func.isRequired
+};
+const characterSelectTextStyle = {
+  lineHeight: '26px',
+  verticalAlign: 'baseline',
+  marginRight: 8
+};
 
 const StartOverButton = React.createClass({
   propTypes: {
