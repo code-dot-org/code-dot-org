2d6d71844a37f255c4dd0371223d44fcef09fdb4
Brent Van Minnen <bjvanminnen@gmail.com>
2017-01-25 15:18:29 -0800

    ProgressBubbleSet improvements

--- apps/src/templates/progress/ProgressBubbleSet.jsx
+++ apps/src/templates/progress/ProgressBubbleSet.jsx
@@ -8,20 +8,20 @@ import color from "@cdo/apps/util/color";
 
 const styles = {
   main: {
-    position: 'absolute',
+    position: 'relative',
+    display: 'inline-block'
   },
   background: {
-    // This causes us to be centered
-    marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
     height: 10,
     backgroundColor: color.lighter_gray,
+    position: 'absolute',
+    left: 10,
+    right: 10,
+    top: 10,
+    bottom: 10,
   },
   bubbles: {
-    position: 'absolute',
-    top: 0,
-    left: 0
+    position: 'relative'
   }
 };
 
@@ -37,12 +37,9 @@ const ProgressBubbleSet = React.createClass({
     const { startingNumber, statuses } = this.props;
 
     // TODO - handle case where our set wraps onto a second line
-    const width = statuses.length * ProgressBubble.size -
-      styles.background.marginLeft - styles.background.marginRight;
-
     return (
       <div style={styles.main}>
-        <div style={{...styles.background, width}}/>
+        <div style={styles.background}/>
         <div style={styles.bubbles}>
           {statuses.map((status, index) => (
             <ProgressBubble
