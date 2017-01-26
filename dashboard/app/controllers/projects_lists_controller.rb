d418f2c63d6ab9f19c5b3b3165faf585e44bcaf5
David Bailey <davidsbailey@users.noreply.github.com>
2017-01-25 18:47:32 -0800

    authenticate using cancan

--- dashboard/app/controllers/projects_lists_controller.rb
+++ dashboard/app/controllers/projects_lists_controller.rb
@@ -1,11 +1,7 @@
 class ProjectsListsController < ApplicationController
-  include LevelsHelper
+  load_and_authorize_resource :section
 
   def index
-    section_id = params[:section_id]
-    section_owner_id = Section.find(section_id).user_id
-    raise "section can only be accessed by its owner" unless current_user && current_user.id == section_owner_id
-
-    @projects_list_data = ProjectsList.fetch_section_projects(section_id)
+    @projects_list_data = ProjectsList.fetch_section_projects(@section)
   end
 end
