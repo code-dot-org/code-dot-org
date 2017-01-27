8574fcd7ee25749cd5ceffa4a32dd5dbab6f8646
Brent Van Minnen <bjvanminnen@gmail.com>
2017-01-27 15:15:50 -0800

    more work on ProgressStageStep

--- apps/src/templates/progress/ProgressStageStep.story.jsx
+++ apps/src/templates/progress/ProgressStageStep.story.jsx
@@ -10,7 +10,78 @@ export default storybook => {
         status: 'perfect',
         story: () => (
           <ProgressStageStep
+            start={1}
+            name="Images, Pixels, and RGB"
+            levels={[
+              {
+                status: 'perfect',
+                url: '/foo/level1',
+              }
+            ]}
+          />
+        )
+      },
+      {
+        name:'multiple puzzle step',
+        status: 'perfect',
+        story: () => (
+          <ProgressStageStep
+            start={1}
             name="Writing Exercises"
+            levels={[
+              {
+                status: 'perfect',
+                url: '/foo/level1',
+              },
+              {
+                status: 'not_tried',
+                url: '/foo/level2',
+              },
+              {
+                status: 'not_tried',
+                url: '/foo/level3',
+              },
+              {
+                status: 'not_tried',
+                url: '/foo/level4',
+              },
+              {
+                status: 'not_tried',
+                url: '/foo/level5',
+              },
+            ]}
+          />
+        )
+      },
+      {
+        name:'non first step',
+        status: 'perfect',
+        story: () => (
+          <ProgressStageStep
+            start={4}
+            name="Writing Exercises"
+            levels={[
+              {
+                status: 'perfect',
+                url: '/foo/level1',
+              },
+              {
+                status: 'not_tried',
+                url: '/foo/level2',
+              },
+              {
+                status: 'not_tried',
+                url: '/foo/level3',
+              },
+              {
+                status: 'not_tried',
+                url: '/foo/level4',
+              },
+              {
+                status: 'not_tried',
+                url: '/foo/level5',
+              },
+            ]}
           />
         )
       }
