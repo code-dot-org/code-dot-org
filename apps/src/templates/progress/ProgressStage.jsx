fb09e442991e4b65c9906d15a89dc0e2bcae99b9
Brent Van Minnen <bjvanminnen@gmail.com>
2017-01-30 13:03:02 -0800

    collapsible stage

--- apps/src/templates/progress/ProgressStage.jsx
+++ apps/src/templates/progress/ProgressStage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ProgressStageContent from './ProgressStageContent';
 import FontAwesome from '../FontAwesome';
+import color from "@cdo/apps/util/color";
 
 const styles = {
   main: {
-    background: '#F6F6F6',
-    border: '1px solid #BBBBBB',
+    background: color.lightest_gray,
+    border: '1px solid ' + color.border_gray,
     borderRadius: 2,
-    padding: 20
+    padding: 20,
   },
   heading: {
     fontSize: 18,
@@ -21,14 +22,27 @@ const styles = {
 
 // TODO - collapsing
 const ProgressStage = React.createClass({
+  getInitialState() {
+    return {
+      collapsed: false
+    };
+  },
+
+  toggleCollapsed() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  },
+
   render() {
+    const icon = this.state.collapsed ? "caret-right" : "caret-down";
     return (
       <div style={styles.main}>
-        <div style={styles.heading}>
-          <FontAwesome icon="caret-down"/>
+        <div style={styles.heading} onClick={this.toggleCollapsed}>
+          <FontAwesome icon={icon}/>
           <span style={styles.headingText}>Stage 4: Encoding Color Images</span>
         </div>
-        <ProgressStageContent/>
+        {!this.state.collapsed && <ProgressStageContent/>}
       </div>
     );
   }
