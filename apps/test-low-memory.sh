f8e5d48bb435e1c37967ab6e778adfe7f78aa331
Paul Carduner <paul@carduner.net>
2017-01-31 19:21:18 +0000

    Set flags for different types of tests

--- apps/test-low-memory.sh
+++ apps/test-low-memory.sh
@@ -4,10 +4,15 @@
 # We break them up here to support more granular retries.
 npm run lint && \
 for i in 1 2; do node --max_old_space_size=4096 `npm bin`/grunt unitTest && break; done && \
-for i in 1 2; do node --max_old_space_size=4096 `npm bin`/grunt integrationTest && break; done
-
 if [ -z "$CIRCLECI" ]; then
-    echo "Uploading coverage reports in"
+    echo "Uploading unit test coverage reports in"
     ls coverage
-    ./node_modules/.bin/codecov
+    bash <(curl -s https://codecov.io/bash) -cF unit
+fi
+
+for i in 1 2; do node --max_old_space_size=4096 `npm bin`/grunt integrationTest && break; done
+if [ -z "$CIRCLECI" ]; then
+    echo "Uploading integration test coverage reports in"
+    ls coverage
+    bash <(curl -s https://codecov.io/bash) -cF integration
 fi
