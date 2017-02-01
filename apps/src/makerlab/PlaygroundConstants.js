06ffcd807ca30884371b0a16b1ea9e47892ef2aa
Brad Buchanan <brad@code.org>
2017-01-31 16:18:10 -0800

    Smaller diff in PlaygroundConstants

--- apps/src/makerlab/PlaygroundConstants.js
+++ apps/src/makerlab/PlaygroundConstants.js
@@ -3,18 +3,17 @@ export const TOUCH_PINS = [0, 1, 2, 3, 6, 9, 10, 12];
 
 export const SENSOR_VARS = ['soundSensor', 'lightSensor', 'tempSensor'];
 export const BUTTON_VARS = ['buttonL', 'buttonR'];
+const TOUCH_PAD_VARS = TOUCH_PINS.map(pin => `touchPad${pin}`);
 
 const BUTTON_EVENTS = ['press', 'down', 'up'];
 const SENSOR_EVENTS = ['change', 'data'];
-const TOUCH_PIN_EVENTS = {};
-TOUCH_PINS.forEach(pin => {
-  TOUCH_PIN_EVENTS[`touchPad${pin}`] = ['down', 'up'];
-});
+const TOUCH_EVENTS = ['down', 'up'];
 
-export const COMPONENT_EVENTS = Object.assign({}, TOUCH_PIN_EVENTS, {
+export const COMPONENT_EVENTS = {
   buttonL: BUTTON_EVENTS,
   buttonR: BUTTON_EVENTS,
   toggleSwitch: ['open', 'close'],
   accelerometer: ['change', 'data']
-});
+};
 SENSOR_VARS.forEach(s => COMPONENT_EVENTS[s] = SENSOR_EVENTS);
+TOUCH_PAD_VARS.forEach(s => COMPONENT_EVENTS[s] = TOUCH_EVENTS);
