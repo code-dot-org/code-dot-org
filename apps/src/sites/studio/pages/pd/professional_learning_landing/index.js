5a55fa591c5cdaff3af37b3a83bdf2db6d8816e1
Mehal Shah <mehal@code.org>
2017-02-01 15:12:21 -0800

    Create initial elements

--- apps/src/sites/studio/pages/pd/professional_learning_landing/index.js
+++ apps/src/sites/studio/pages/pd/professional_learning_landing/index.js
@@ -1,4 +1,6 @@
-import LandingPage from 'landing_page'
+import LandingPage from '@cdo/apps/code-studio/pd/professional_learning_landing/landing_page';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <LandingPage/>,
