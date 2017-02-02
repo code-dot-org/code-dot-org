1c3d7151775f8aa157ae2870dd18c027f516a7f8
Brad Buchanan <brad@code.org>
2017-02-02 14:43:18 -0800

    Rename some variables

--- apps/src/lib/kits/maker/dropletConfig.js
+++ apps/src/lib/kits/maker/dropletConfig.js
@@ -24,7 +24,7 @@ const playSongConfig = {
   tempo: 100000
 };
 
-const MAKERLAB_CATEGORY = 'Maker Lab';
+const MAKER_CATEGORY = 'Maker Lab';
 const CIRCUIT_CATEGORY = 'Circuit';
 
 const pixelType = '[ColorLed].';
@@ -73,11 +73,11 @@ export const blocks = [
   /**
    * Generic Johnny-Five / Firmata blocks
    */
-  {func: 'pinMode', parent: api, category: MAKERLAB_CATEGORY, paletteParams: ['pin', 'mode'], params: ['13', '"output"'], dropdown: { 1: ['"output"', '"input"', '"analog"'] }},
-  {func: 'digitalWrite', parent: api, category: MAKERLAB_CATEGORY, paletteParams: ['pin', 'value'], params: ['13', '1'], dropdown: { 1: ['1', '0'] }},
-  {func: 'digitalRead', parent: api, category: MAKERLAB_CATEGORY, type: 'value', nativeIsAsync: true, paletteParams: ['pin'], params: ['"D4"']},
-  {func: 'analogWrite', parent: api, category: MAKERLAB_CATEGORY, paletteParams: ['pin', 'value'], params: ['5', '150']},
-  {func: 'analogRead', parent: api, category: MAKERLAB_CATEGORY, type: 'value', nativeIsAsync: true, paletteParams: ['pin'], params: ['5']},
+  {func: 'pinMode', parent: api, category: MAKER_CATEGORY, paletteParams: ['pin', 'mode'], params: ['13', '"output"'], dropdown: { 1: ['"output"', '"input"', '"analog"'] }},
+  {func: 'digitalWrite', parent: api, category: MAKER_CATEGORY, paletteParams: ['pin', 'value'], params: ['13', '1'], dropdown: { 1: ['1', '0'] }},
+  {func: 'digitalRead', parent: api, category: MAKER_CATEGORY, type: 'value', nativeIsAsync: true, paletteParams: ['pin'], params: ['"D4"']},
+  {func: 'analogWrite', parent: api, category: MAKER_CATEGORY, paletteParams: ['pin', 'value'], params: ['5', '150']},
+  {func: 'analogRead', parent: api, category: MAKER_CATEGORY, type: 'value', nativeIsAsync: true, paletteParams: ['pin'], params: ['5']},
 
   /**
    * Circuit-Playground-specific blocks
@@ -129,7 +129,7 @@ export const blocks = [
 ];
 
 export const categories = {
-  [MAKERLAB_CATEGORY]: {
+  [MAKER_CATEGORY]: {
     color: 'cyan',
     rgb: COLOR_CYAN,
     blocks: []
