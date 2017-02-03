f2c330c943f14e3283bc6272a79e20ed3b102437
Brad Buchanan <brad@code.org>
2017-02-03 14:10:13 -0800

    Maker board connects much faster
    pair: @bcjordan
    
    Discovered today that by default Firmata waits 5000ms on initial connection for the board to report aversion, which our Circuit Playground Firmata-ready boards don't seem to do - we were waiting for the entire five-second timeout on initial connection every time.
    
    It's likely that the _correct_ solution to this is to get the board to report its version right away on connection, but in the meantime it speeds things up a lot just to lower this timeout from 5000ms to 200ms, and doesn't seem to cause any issues.
    
    To do this we had to modify the `PlaygroundIO` wrapper around `Firmata` so that it can pass Firmata options through to that constructor - Firmata already had an option for overriding this timeout, it just wasn't available.  So we're temporarily depending on a [code-dot-org fork of playground-io](https://github.com/code-dot-org/playground-io) and have already [opened a pull request upstream](https://github.com/rwaldron/playground-io/pull/4) so hopefully we can switch back soon.  Also notable: We needed to change the package version in package.json on our fork or `yarn` will keep whatever version it has cached, even though the package.json is now pointing to a repository - hence the [`0.4.0-cdo.0` version on our fork.](https://github.com/code-dot-org/playground-io/commit/b537ca04679be0317e1e44324c04de6e11477cac)

--- apps/src/lib/kits/maker/BoardController.js
+++ apps/src/lib/kits/maker/BoardController.js
@@ -174,7 +174,14 @@ export default class BoardController {
       const serialPort = new ChromeSerialPort.SerialPort(portId, {
         bitrate: SERIAL_BAUD
       }, true);
-      const io = new PlaygroundIO({port: serialPort});
+      const io = new PlaygroundIO({
+        port: serialPort,
+        // Circuit Playground Firmata does not seem to proactively report its
+        // version, meaning we were hitting the default 5000ms timeout waiting
+        // for this on every connection attempt.  Lowering that timeout to
+        // 200ms seems to work just fine.
+        reportVersionTimeout: 200
+      });
       const board = new five.Board({io: io, repl: false});
       board.once('ready', () => resolve(board));
       board.once('error', reject);
