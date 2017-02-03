0a44d5f08797ccbec14a9239ff44e389d58c8402
Mehal Shah <mehal@code.org>
2017-02-03 11:29:58 -0800

    More stuff on the landing page

--- apps/src/code-studio/pd/professional_learning_landing/landing_page.jsx
+++ apps/src/code-studio/pd/professional_learning_landing/landing_page.jsx
@@ -9,10 +9,36 @@ const LandingPage = React.createClass({
 
   },
 
+  renderHeaderImage() {
+    return (
+      <div style={{
+        width: '100%',
+        height: '400px',
+        background: `url(https://code.org/images/homepage/sheryl.jpg) no-repeat`,
+        backgroundSize: 'cover',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <div style={{
+          backgroundColor: 'rgba(0, 0, 0, .5)',
+          alignSelf: 'flex-end',
+          width: '100%',
+          textAlign: 'center',
+          padding: '30px',
+          fontSize: '40px',
+          color: 'white'
+        }}>
+          My Professional Learning
+        </div>
+      </div>
+    )
+  },
+
   render() {
     return (
       <div>
-        Landing page
+        {this.renderHeaderImage()}
       </div>
     );
   }
