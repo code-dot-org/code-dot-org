17251b9f196084501b0c03a198bfc133fbd9f824
Mehal Shah <mehal@code.org>
2017-02-03 13:06:39 -0800

    Linting

--- apps/src/code-studio/pd/professional_learning_landing/landing_page.jsx
+++ apps/src/code-studio/pd/professional_learning_landing/landing_page.jsx
@@ -11,28 +11,32 @@ const LandingPage = React.createClass({
 
   renderHeaderImage() {
     return (
-      <div style={{
-        width: '100%',
-        height: '400px',
-        background: `url(https://code.org/images/homepage/sheryl.jpg) no-repeat`,
-        backgroundSize: 'cover',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}>
-        <div style={{
-          backgroundColor: 'rgba(0, 0, 0, .5)',
-          alignSelf: 'flex-end',
+      <div
+        style={{
           width: '100%',
-          textAlign: 'center',
-          padding: '30px',
-          fontSize: '40px',
-          color: 'white'
-        }}>
+          height: '400px',
+          background: `url(https://code.org/images/homepage/sheryl.jpg) no-repeat`,
+          backgroundSize: 'cover',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: 'rgba(0, 0, 0, .5)',
+            alignSelf: 'flex-end',
+            width: '100%',
+            textAlign: 'center',
+            padding: '30px',
+            fontSize: '40px',
+            color: 'white'
+          }}
+        >
           My Professional Learning
         </div>
       </div>
-    )
+    );
   },
 
   render() {
