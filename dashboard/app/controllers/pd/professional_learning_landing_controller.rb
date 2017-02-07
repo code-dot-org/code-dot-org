5b6cdf6c1a56217dc3d301b9edd6efba8f3b4cde
Mehal Shah <mehal@code.org>
2017-02-07 13:57:50 -0800

    First bit of server data getting rendered on the professional learning landing page. And with tests!

--- dashboard/app/controllers/pd/professional_learning_landing_controller.rb
+++ dashboard/app/controllers/pd/professional_learning_landing_controller.rb
@@ -3,20 +3,12 @@ class Pd::ProfessionalLearningLandingController < ApplicationController
 
   def index
     # Get workshops that the user is enrolled in with an outstanding survey
-    enrollments = Pd::Enrollment.where(email: current_user.email)
     workshops = Pd::Workshop.enrolled_in_by(current_user)
-
-    unsurveyed_enrollments = enrollments.where(completed_survey_id: nil).where.not(survey_sent_at: nil).order(ended_at: :desc)
+    courses_teaching = workshops.pluck(:course).uniq
 
     # Link to the certificate
-
-    # Has the teacher taken a CSF PD class?
-
-    return {
-        csf_teacher: Pd::Workshop.enrolled_in_by(current_user).exists?(course: COURSE_CSF),
-        survey_link: unsurveyed_enrollments.first,
-        certificate_link: '',
-        most_recent_workshop: workshops.sort { |x, y| x.start > y.start}.first
+    @landing_page_data = {
+      courses_teaching: courses_teaching
     }
   end
 end
