d103dee5f28a71adadb33d9c3971a7ea60edf615
Mehal Shah <mehal@code.org>
2017-02-07 17:26:27 -0800

    Checkpoint

--- apps/src/code-studio/pd/professional_learning_landing/csPrinciplesAndDiscoveriesSection.jsx
+++ apps/src/code-studio/pd/professional_learning_landing/csPrinciplesAndDiscoveriesSection.jsx
@@ -1,16 +1,34 @@
 /** Section for CS Principles and CS Discoveries */
 
 import React from 'react';
+import {TwoPartBanner, imageStyle, textStyle, leftStyle, rightStyle} from './twoPartBanner'
 
 const CsPrinciplesAndDiscoveriesSection = React.createClass({
   propTypes: {
-
+    lastWorkshopSurveyUrl: React.PropTypes.string
   },
 
   render() {
+    // const leftHalfStyle = Object.assign({}, imageStyle)
+    // console.log(leftHalfStyle);
+
     return (
       <div>
-        Section for CS Principles and CS Discoveries
+        <br/>
+        <TwoPartBanner
+          leftHalf={(
+            <div style={Object.assign({}, imageStyle, leftStyle, {
+              backgroundImage: `url('https://code.org/images/email/BJC4NYC.jpg')`,
+            })}/>
+          )}
+          rightHalf={(
+           <div style={Object.assign({}, textStyle, rightStyle, {})}>
+             <div style={{marginTop: '20px'}}>
+               Hey more text
+             </div>
+           </div>
+          )}
+        />
       </div>
     );
   }
