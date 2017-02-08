c8e3ee7b9b4853281335abffb0aca499ec9e962c
Brad Buchanan <brad@code.org>
2017-02-07 17:29:26 -0800

    Supply exit() function to timedLoop by default
    Autofills an `exit()` call inside a newly dragged out `timedLoop()` block, and teaches the editor that the `exit()` call has no arguments, but does _not_ add it to the toolbox.

--- apps/src/lib/kits/maker/dropletConfig.js
+++ apps/src/lib/kits/maker/dropletConfig.js
@@ -76,7 +76,8 @@ export const blocks = [
   {func: 'digitalRead', parent: api, category: MAKER_CATEGORY, type: 'value', nativeIsAsync: true, paletteParams: ['pin'], params: ['"D4"']},
   {func: 'analogWrite', parent: api, category: MAKER_CATEGORY, paletteParams: ['pin', 'value'], params: ['5', '150']},
   {func: 'analogRead', parent: api, category: MAKER_CATEGORY, type: 'value', nativeIsAsync: true, paletteParams: ['pin'], params: ['5']},
-  {func: 'timedLoop', parent: api, category: MAKER_CATEGORY, paletteParams: ['ms', 'callback'], params: ['1000', 'function(exit) {\n  \n}']},
+  {func: 'timedLoop', parent: api, category: MAKER_CATEGORY, paletteParams: ['ms', 'callback'], params: ['1000', 'function(exit) {\n  // Call exit() to stop looping\n  exit();\n}']},
+  {func: 'exit', category: MAKER_CATEGORY, noAutocomplete: true},
 
   /**
    * Circuit-Playground-specific blocks
