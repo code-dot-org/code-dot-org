d0ec7db85de568155d2e93d059220ba5296820b8
Mehal Shah <mehal@code.org>
2017-02-14 15:34:04 -0800

    PR feedback

--- dashboard/test/controllers/pd/professional_learning_landing_controller_test.rb
+++ dashboard/test/controllers/pd/professional_learning_landing_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class Pd::ProfessionalLearningLandingControllerTest < ::ActionController::TestCase
   setup do
     @csf_workshop = create :pd_ended_workshop, num_sessions: 3, course: Pd::Workshop::COURSE_CSF, subject: nil
-    @csd_workshop = create :pd_workshop, num_sessions: 3, course: Pd::Workshop::COURSE_CSD, subject: nil
+    @csd_workshop = create :pd_ended_workshop, num_sessions: 3, course: Pd::Workshop::COURSE_CSD, subject: nil
     @csp_workshop = create :pd_workshop, num_sessions: 3, course: Pd::Workshop::COURSE_CSP, subject: Pd::Workshop::SUBJECT_CSP_SUMMER_WORKSHOP
 
     @teacher = create(:admin, email: 'test_email@foo.com', user_type: 'teacher')
@@ -15,6 +15,7 @@ class Pd::ProfessionalLearningLandingControllerTest < ::ActionController::TestCa
 
     @ended_enrollment = create :pd_enrollment, email: @teacher.email, workshop: @csf_workshop, survey_sent_at: Date.today + 5.days
     other_enrollment = create :pd_enrollment, email: @teacher.email, workshop: @csd_workshop, survey_sent_at: Date.today + 2.days
+    create :pd_enrollment, email: @teacher.email, workshop: @csp_workshop
 
     Pd::Enrollment.stubs(:filter_for_survey_completion).returns([@ended_enrollment, other_enrollment])
   end
@@ -28,8 +29,8 @@ class Pd::ProfessionalLearningLandingControllerTest < ::ActionController::TestCa
 
     puts response
 
-    assert_equal [Pd::Workshop::COURSE_CSF, Pd::Workshop::COURSE_CSD], response[:courses_teaching]
-    assert_equal [Pd::Workshop::COURSE_CSF], response[:courses_completed]
+    assert_equal [Pd::Workshop::COURSE_CSF, Pd::Workshop::COURSE_CSD, Pd::Workshop::COURSE_CSP], response[:courses_teaching]
+    assert_equal [Pd::Workshop::COURSE_CSF, Pd::Workshop::COURSE_CSD], response[:courses_completed]
     assert_equal "#{CDO.code_org_url}/pd-workshop-survey/#{@ended_enrollment.code}", response[:last_workshop_survey_url]
   end
 
