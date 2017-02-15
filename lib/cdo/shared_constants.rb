2ee992bc0050b0a7e2f4eacdeed2d25b3af5595c
Brent Van Minnen <bjvanminnen@gmail.com>
2017-02-15 13:31:26 -0800

    Example of what shared constants could look like

--- lib/cdo/shared_constants.rb
+++ lib/cdo/shared_constants.rb
@@ -1,17 +1,16 @@
 # A set of constants shared between ruby and JS via /shared/config/sharedConstants.json
 require 'json'
 
-def constants
-  return @@constants
-  file = File.read(File.join(__dir__, '../../shared/config/constants.json'))
-  json = JSON.parse(file)
-
 module SharedConstants
-  def constants
-    return @@constants if @@constants
+  @@json = nil
 
+  def self.json
+    return @@json if @@json
+
+    file = File.read(File.join(__dir__, '../../shared/config/constants.json'))
+    # @@json = JSON.parse(file)
+    @@json = JSON.parse(file, object_class: OpenStruct)
   end
-  LevelKind = Hash[json['LevelKinds'].map{|kind| [kind, kind]}]
-end
 
-puts SharedConstants::LevelKind
+  LevelKind = self.json.LevelKind
+end
