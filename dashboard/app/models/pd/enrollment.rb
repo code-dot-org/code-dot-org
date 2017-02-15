c9be06e3d3d93a08902cb3944cc73b026ad2c125
Mehal Shah <mehal@code.org>
2017-02-14 17:09:04 -0800

    Add annotated stuff

--- dashboard/app/models/pd/enrollment.rb
+++ dashboard/app/models/pd/enrollment.rb
@@ -21,6 +21,7 @@
 # Indexes
 #
 #  index_pd_enrollments_on_code            (code) UNIQUE
+#  index_pd_enrollments_on_email           (email)
 #  index_pd_enrollments_on_pd_workshop_id  (pd_workshop_id)
 #
 
