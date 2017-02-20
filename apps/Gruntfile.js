a2c7da7610091d9f75b732ded5328cfc5d135e79
Caley Brock <caleybrock7@gmail.com>
2017-02-20 13:15:25 -0800

    Revert "Revert "Tech debt: Run ruby block tests when changing JS in circleCI""

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -719,6 +719,11 @@ testsContext.keys().forEach(testsContext);
     child_process.execSync('`npm bin`/firebase-bolt < ./firebase/rules.bolt > ./build/package/firebase/rules.json');
   });
 
+  grunt.registerTask('ruby-block-tests', function () {
+    child_process.execSync('cd ../dashboard; rails test test/models/gamelab_test.rb');
+    child_process.execSync('cd ../dashboard; rails test test/models/applab_test.rb');
+  });
+
   grunt.registerTask('postbuild', [
     'newer:copy:static',
     'newer:concat',
@@ -767,6 +772,7 @@ testsContext.keys().forEach(testsContext);
 
   // Note: Be sure if you add additional test types, you also up date test-low-memory.sh
   grunt.registerTask('test', [
+    'ruby-block-tests',
     'preconcat',
     'concat',
     'karma:all'
