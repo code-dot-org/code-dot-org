c43b6cb722d5e6ca2e93145fdd84523e0c0be53c
ewjordan <ewjordan@gmail.com>
2017-02-21 01:18:28 -0800

    Clean up attribute filtering

--- dashboard/lib/school_info_deduplicator.rb
+++ dashboard/lib/school_info_deduplicator.rb
@@ -18,28 +18,15 @@ module SchoolInfoDeduplicator
 
   # Processes school info attributes (as they come in from a form) to be passed into SchoolInfo.new
   def process_school_info_attributes(school_info_attr)
-    attr = {
-      country: school_info_attr['country'],
-      school_type: school_info_attr['school_type'],
-      state: school_info_attr['school_state'],
-      zip: school_info_attr['school_zip'],
-      school_district_id: school_info_attr['school_district_id'],
-      school_district_other: school_info_attr['school_district_other'],
-      school_district_name: school_info_attr['school_district_name'],
-      school_id: school_info_attr['school_id'],
-      school_other: school_info_attr['school_other'],
-      school_name: school_info_attr['school_name'],
-      full_address: school_info_attr['full_address'],
-      validation_type: school_info_attr['validation_type']
-    }
+    attr = school_info_attr.symbolize_keys
 
     # Remove empty attributes.  Notably school_district_id can come through
     # as an empty string when we don't want anything.
     attr.delete_if { |_, e| e.blank? }
 
     # The checkbox comes through as "true" when we really want true.
-    attr[:school_district_other] = true if attr[:school_district_other] == "true"
-    attr[:school_other] = true if attr[:school_other] == "true"
+    attr[:school_district_other] &&= attr[:school_district_other].to_bool
+    attr[:school_other] &&= attr[:school_other].to_bool
     attr
   end
 end
