7e43e17b0c43b032632f2ffc3c8a175f138b4280
David Bailey <davidsbailey@users.noreply.github.com>
2017-02-20 16:48:13 -0800

    Confirm existence of manifest file

--- pegasus/helpers/asset_helpers.rb
+++ pegasus/helpers/asset_helpers.rb
@@ -4,7 +4,7 @@ require 'sprockets'
 def load_asset_map
   $asset_map = nil
   manifest_filename = Sprockets::ManifestUtils.find_directory_manifest(CDO.dashboard_assets_dir)
-  return unless manifest_filename
+  return unless manifest_filename && File.exist?(manifest_filename)
   json = File.read(manifest_filename)
   $asset_map = JSON.parse(json)['assets']
 end
