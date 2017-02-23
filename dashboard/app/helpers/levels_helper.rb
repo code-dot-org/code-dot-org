7b82ef4813275f8e05a172bce0dca789e517b61f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-02-23 12:07:15 -0800

    check for script before returning viewed authored hints

--- dashboard/app/helpers/levels_helper.rb
+++ dashboard/app/helpers/levels_helper.rb
@@ -174,7 +174,7 @@ module LevelsHelper
 
     if AuthoredHintViewRequest.enabled?
       view_options(authored_hint_view_requests_url: authored_hint_view_requests_path(format: :json))
-      if current_user
+      if current_user && @script
         view_options(authored_hints_used_ids: Set.new(AuthoredHintViewRequest.hints_used(current_user.id, @script.id, @level.id).map(&:hint_id)))
       end
     end
