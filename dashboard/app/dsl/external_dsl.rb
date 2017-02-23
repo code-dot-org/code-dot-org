0d23ef5a0b3cf9c04ad74677e890d74b2712095d
Brent Van Minnen <Bjvanminnen@gmail.com>
2017-02-22 16:02:26 -0800

    Revert "cleanup hrefs from external levels"

--- dashboard/app/dsl/external_dsl.rb
+++ dashboard/app/dsl/external_dsl.rb
@@ -3,6 +3,10 @@ class ExternalDSL < ContentDSL
     @hash = {href: '', options: {skip_dialog: true}}
   end
 
+  def href(url)
+    @hash[:href] = url
+  end
+
   def parse_output
     {name: @name, properties: @hash}
   end
