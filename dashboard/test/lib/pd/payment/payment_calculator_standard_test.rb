c13ea49d2e491f3883e474d33652846bb10c576d
Will Jordan <will@code.org>
2017-02-23 13:09:44 -0800

    speed up PaymentCalculatorStandardTest

--- dashboard/test/lib/pd/payment/payment_calculator_standard_test.rb
+++ dashboard/test/lib/pd/payment/payment_calculator_standard_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 module Pd::Payment
   class PaymentCalculatorStandardTest < ActiveSupport::TestCase
-    setup do
+    self.fixture_table_names = []
+
+    setup_all do
       # TIME_CONSTRAINTS_BY_SUBJECT: SUBJECT_ECS_PHASE_4 => {min_days: 2, max_days: 3, max_hours: 18}
       @workshop = create :pd_ended_workshop,
         workshop_type: Pd::Workshop::TYPE_PUBLIC,
