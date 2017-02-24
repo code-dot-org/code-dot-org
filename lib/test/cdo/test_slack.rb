96ef8c9a24dfa56308ee3ada3223ba4c6682a914
ashercodeorg <asher@code.org>
2017-02-24 07:26:42 -0600

    Create SlackTest skeleton.

---
+++ lib/test/cdo/test_slack.rb
@@ -0,0 +1,28 @@
+require_relative '../../../shared/test/test_helper'
+require 'cdo/slack'
+
+class SlackTest < Minitest::Test
+  def test_get_topic
+  end
+
+  def test_get_topic_with_channel_map
+  end
+
+  def test_get_topic_on_error
+  end
+
+  def test_update_topic
+  end
+
+  def test_update_topic_on_error
+  end
+
+  def test_message
+  end
+
+  def test_message_with_html
+  end
+
+  def test_message_on_error
+  end
+end
