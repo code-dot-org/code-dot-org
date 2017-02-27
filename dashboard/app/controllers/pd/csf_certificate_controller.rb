a4bcd43cdd3a81cfd6e7b116d7dd69ee5f430530
Mehal Shah <mehal@code.org>
2017-02-27 15:30:08 -0800

    Require an enrollment code when generating the certificate

--- dashboard/app/controllers/pd/csf_certificate_controller.rb
+++ dashboard/app/controllers/pd/csf_certificate_controller.rb
@@ -2,9 +2,11 @@ class Pd::CsfCertificateController < ApplicationController
   before_action :authenticate_user!
 
   def generate_certificate
+    enrollment = params[:enrollment_code] ? Pd::Enrollment.find_by(code: params[:enrollment_code]) : nil
+
     image = create_certificate_image2(
       dashboard_dir('app', 'assets', 'images', 'pd_workshop_certificate_csf.png'),
-      current_user.name || '',
+      enrollment.try(:name) || '',
       y: 444,
       height: 100
     )
