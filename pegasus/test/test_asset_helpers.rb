8cf2ad57764a2db01f52d75133f5a28b4390dd22
David Bailey <davidsbailey@users.noreply.github.com>
2017-02-27 12:36:10 -0800

    fix local development using pegasus_skip_asset_map

--- pegasus/test/test_asset_helpers.rb
+++ pegasus/test/test_asset_helpers.rb
@@ -6,12 +6,14 @@ require_relative '../helpers/asset_helpers'
 
 class AssetHelpersTest < Minitest::Test
   UNMINIFIED_ASSET_NAME = 'js/teacher-dashboard/index.js'
+  UNDIGESTED_ASSET_PATH = "#{CDO.studio_url}/assets/js/teacher-dashboard/index.js"
   UNMINIFIED_ASSET_PATH = "#{CDO.studio_url}/assets/js/teacher-dashboard/"\
     'index-ef90e2acd9003ff8b8bac522e6ce107da641d3b85aba5f58c77d5d28f77a496a.js'
   MINIFIED_ASSET_PATH = "#{CDO.studio_url}/assets/js/teacher-dashboard/"\
     'index.min-5bb3b68c6f92cf8409eb7d0649cf572ffa0c66fca1b02b887b4454cab553daef.js'
   UNMINIFIED_ASSET_NOT_IN_MAP = 'foo.js'
   MINIFIED_ASSET_NOT_IN_MAP = 'foo.min.js'
+  UNDIGESTED_ASSET_PATH_NOT_IN_MAP = "#{CDO.studio_url}/assets/foo.js"
 
   def setup
     CDO.stubs(:pegasus_skip_asset_map).returns(false)
@@ -21,7 +23,7 @@ class AssetHelpersTest < Minitest::Test
 
   def test_asset_map_pretty
     CDO.stubs(:pretty_js).returns(true)
-    assert_equal UNMINIFIED_ASSET_PATH, asset_path(UNMINIFIED_ASSET_NAME),
+    assert_equal UNMINIFIED_ASSET_PATH, @asset_map.asset_path(UNMINIFIED_ASSET_NAME),
       "incorrect unminifiable asset path"
     # Should return the unminified path because pretty_js is true
     assert_equal UNMINIFIED_ASSET_PATH, minifiable_asset_path(UNMINIFIED_ASSET_NAME),
@@ -61,9 +63,9 @@ class AssetHelpersTest < Minitest::Test
     Singleton.__init__(AssetMap)
     CDO.stubs(:pegasus_skip_asset_map).returns(true)
     CDO.stubs(:pretty_js).returns(true)
-    assert_equal UNMINIFIED_ASSET_NAME, asset_path(UNMINIFIED_ASSET_NAME),
+    assert_equal UNDIGESTED_ASSET_PATH, asset_path(UNMINIFIED_ASSET_NAME),
       "should recover gracefully when no asset map for unminifiable asset"
-    assert_equal UNMINIFIED_ASSET_NAME, minifiable_asset_path(UNMINIFIED_ASSET_NAME),
+    assert_equal UNDIGESTED_ASSET_PATH, minifiable_asset_path(UNMINIFIED_ASSET_NAME),
       "should recover gracefully when no asset map for minifiable asset"
   end
 
@@ -84,9 +86,9 @@ class AssetHelpersTest < Minitest::Test
   def test_skip_flag_when_asset_not_in_map
     CDO.stubs(:pegasus_skip_asset_map).returns(true)
     CDO.stubs(:pretty_js).returns(true)
-    assert_equal UNMINIFIED_ASSET_NOT_IN_MAP, asset_path(UNMINIFIED_ASSET_NOT_IN_MAP),
+    assert_equal UNDIGESTED_ASSET_PATH_NOT_IN_MAP, asset_path(UNMINIFIED_ASSET_NOT_IN_MAP),
       "should recover gracefully when unminifiable asset is not found"
-    assert_equal UNMINIFIED_ASSET_NOT_IN_MAP, minifiable_asset_path(UNMINIFIED_ASSET_NOT_IN_MAP),
+    assert_equal UNDIGESTED_ASSET_PATH_NOT_IN_MAP, minifiable_asset_path(UNMINIFIED_ASSET_NOT_IN_MAP),
       "should recover gracefully when minifiable asset is not found"
   end
 end
