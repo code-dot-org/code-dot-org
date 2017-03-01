f4e57a2d44dd58cf72afb73bb30e3903f64ba632
Brent Van Minnen <bjvanminnen@gmail.com>
2017-03-01 12:22:57 -0800

    more storybook

--- apps/src/templates/progress/ProgressLessonTeacherInfo.story.jsx
+++ apps/src/templates/progress/ProgressLessonTeacherInfo.story.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { createStoreWithReducers } from '@cdo/apps/redux';
 import ProgressLessonTeacherInfo from './ProgressLessonTeacherInfo';
 import { LevelKind } from '@cdo/apps/util/sharedConstants';
-import { initProgress, lessons, levelsByLesson, showTeacherInfo } from '@cdo/apps/code-studio/progressRedux';
+import { initProgress, lessons, showTeacherInfo } from '@cdo/apps/code-studio/progressRedux';
 import { authorizeLockable, setViewType, ViewType } from '@cdo/apps/code-studio/stageLockRedux';
 import { setSections } from '@cdo/apps/code-studio/sectionsRedux';
 import { setInitialized } from '@cdo/apps/code-studio/hiddenStageRedux';
@@ -21,33 +21,43 @@ const lockableStage = {
   position: 1,
 };
 
-const createStore = (id, {preload=false, allowHidden=true} = {}) => {
+const nonLockableStage = {
+  ...lockableStage,
+  id: 124,
+  lockable: false,
+  lesson_plan_html_url: 'lesson_plan.html'
+};
+
+const createStore = ({preload=false, allowHidden=true} = {}) => {
   const store = createStoreWithReducers();
+  const stages = [
+    lockableStage,
+    nonLockableStage
+  ];
   store.dispatch(initProgress({
     scriptName: 'csp1',
-    stages: [
-      lockableStage
-    ]
+    stages
   }));
   store.dispatch(authorizeLockable());
   store.dispatch(showTeacherInfo());
   store.dispatch(setViewType(ViewType.Teacher));
   store.dispatch(setInitialized(allowHidden));
   if (!preload) {
-    console.log('setSections', id);
-    store.dispatch(setSections({
+    const sections = {
       '11': {
         section_id: 11,
         section_name: 'test_section',
-        stages: {
-          [lockableStage.id]: [0,1,2].map(id => ({
-            locked: true,
-            name: `student${id}`,
-            readonly_answers: false
-          }))
-        }
+        stages: {}
       }
-    }));
+    };
+    stages.forEach(stage => {
+      sections[11].stages[stage.id] = [0,1,2].map(id => ({
+        locked: true,
+        name: `student${id}`,
+        readonly_answers: false
+      }));
+    });
+    store.dispatch(setSections(sections));
   }
   return store;
 };
@@ -59,15 +69,13 @@ export default storybook => {
       {
         name:'loading',
         story: () => {
-          const store = createStore(1, { preload: true });
+          const store = createStore({ preload: true });
           const state = store.getState();
           return (
             <Provider store={store}>
               <div style={{width: 200, height: 200}}>
                 <ProgressLessonTeacherInfo
                   lesson={lessons(state.progress)[0]}
-                  lessonNumber={1}
-                  levels={levelsByLesson(state.progress)[0]}
                 />
               </div>
             </Provider>
@@ -75,17 +83,50 @@ export default storybook => {
         }
       },
       {
-        name:'visible, lockable lesson with no lesson plan',
+        name:'hideable allowed, lockable lesson with no lesson plan',
         story: () => {
-          const store = createStore(2);
+          const store = createStore();
           const state = store.getState();
           return (
             <Provider store={store}>
               <div style={{width: 200, height: 200}}>
                 <ProgressLessonTeacherInfo
                   lesson={lessons(state.progress)[0]}
-                  lessonNumber={1}
-                  levels={levelsByLesson(state.progress)[0]}
+                />
+              </div>
+            </Provider>
+          );
+        }
+      },
+
+      {
+        name:'hideable allowed, nonlockable lesson with lesson plan',
+        story: () => {
+          const store = createStore({allowHidden: true});
+          const state = store.getState();
+          return (
+            <Provider store={store}>
+              <div style={{width: 200, height: 200}}>
+                <ProgressLessonTeacherInfo
+                  lesson={lessons(state.progress)[1]}
+                />
+              </div>
+            </Provider>
+          );
+        }
+      },
+
+      {
+        name:'hideable not allowed, nonlockable lesson with no lesson plan',
+        description: 'shouldnt render anything',
+        story: () => {
+          const store = createStore({allowHidden: false});
+          const state = store.getState();
+          return (
+            <Provider store={store}>
+              <div style={{width: 200, height: 200}}>
+                <ProgressLessonTeacherInfo
+                  lesson={lessons(state.progress)[1]}
                 />
               </div>
             </Provider>
