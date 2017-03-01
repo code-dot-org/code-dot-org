23252f247b751f8127551af5f19f0551c3eec694
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-02-28 18:26:33 -0800

    accomodate empty authored_hints translation data

--- dashboard/app/models/blockly.rb
+++ dashboard/app/models/blockly.rb
@@ -318,11 +318,15 @@ class Blockly < Level
 
   def localized_authored_hints
     if should_localize? && authored_hints
-      localized_hints = JSON.parse(authored_hints).map do |hint|
-        translated_text = I18n.t("data.authored_hints").
-            try(:[], "#{name}_authored_hint".to_sym).
-            try(:[], hint['hint_id'].to_sym)
+      translations = I18n.t("data.authored_hints").
+        try(:[], "#{name}_authored_hint".to_sym)
 
+      return unless translations.instance_of? Hash
+
+      localized_hints = JSON.parse(authored_hints).map do |hint|
+        next if hint['hint_markdown'].nil? || hint['hint_id'].nil?
+
+        translated_text = translations.try(:[], hint['hint_id'].to_sym)
         original_text = hint['hint_markdown']
 
         if translated_text != original_text
