ab857b10e72e3b552ea885728675db3ba5da5059
David Bailey <davidsbailey@users.noreply.github.com>
2017-03-01 17:28:57 -0800

    add dropdown to filter section projects list by student name

--- apps/src/templates/projects/SectionProjectsList.jsx
+++ apps/src/templates/projects/SectionProjectsList.jsx
@@ -1,13 +1,70 @@
 import React from 'react';
 import ProjectsList from './ProjectsList';
+import _ from 'lodash';
 
 class SectionProjectsList extends React.Component {
   constructor(props) {
     super(props);
+
+    const studentNames = this.getStudentNames(props.projectsData);
+
+    this.state = {
+      studentNames,
+      // Show all students
+      selectedStudent: '',
+    };
+  }
+
+  getStudentNames(projectsData) {
+    const studentsMap = {};
+    projectsData.forEach(project => {
+      const studentName = project['studentName'];
+      if (studentName) {
+        studentsMap[studentName] = true;
+      }
+    });
+    return Object.keys(studentsMap);
+  }
+
+  onChangeStudent(event) {
+    const selectedStudent = event.target.value;
+    this.setState({selectedStudent});
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (_.isEqual(nextProps, this.props)) {
+      return;
+    }
+
+    const studentNames = this.getStudentNames(nextProps.projectsData);
+    this.setState({studentNames});
   }
 
   render() {
-    return <ProjectsList {...this.props} />;
+    const filteredProjectsData = this.props.projectsData.filter(project => (
+      !this.state.selectedStudent ||
+        (this.state.selectedStudent === project['studentName'])
+    ));
+
+    return (
+      <div>
+        <select
+          value={this.state.selectedStudent}
+          onChange={this.onChangeStudent.bind(this)}
+        >
+          <option value="">All Students</option>
+          {
+            this.state.studentNames.map(studentName => (
+              <option value={studentName}>{studentName}</option>
+            ))
+          }
+        </select>
+        <ProjectsList
+          projectsData={filteredProjectsData}
+          studioUrlPrefix={this.props.studioUrlPrefix}
+        />
+      </div>
+    );
   }
 }
 
