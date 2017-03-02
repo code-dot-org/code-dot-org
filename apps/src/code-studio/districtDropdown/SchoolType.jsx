33564bdb5ea549ab31f7a20fed856167bec77652
ewjordan <ewjordan@gmail.com>
2017-03-01 16:53:40 -0800

    Pull school types into const array

--- apps/src/code-studio/districtDropdown/SchoolType.jsx
+++ apps/src/code-studio/districtDropdown/SchoolType.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 
+const SCHOOL_TYPES = [
+  {value:"charter", text:"Charter"},
+  {value:"private", text:"Private"},
+  {value:"public", text:"Public"},
+  {value:"homeschool", text:"Homeschool"},
+  {value:"afterschool", text:"After School"},
+  {value:"other", text:"Other"}
+];
+
 function SchoolType(props) {
   return (
     <div className="itemblock">
       <div className="labelblock">School Type</div>
       <select className="form-control fieldblock" id="school-type" name="user[school_info_attributes][school_type]" type="select" value={props.schoolType} onChange={props.onChange}>
         <option disabled="true" value=""/>
-        <option value="charter">Charter</option>
-        <option value="private">Private</option>
-        <option value="public">Public</option>
-        <option value="homeschool">Homeschool</option>
-        <option value="afterschool">After School</option>
-        <option value="other">Other</option>
+        {
+          SCHOOL_TYPES.map((type) =>
+            <option
+              key={type.value}
+              value={type.value}
+            >
+              {type.text}
+            </option>
+          )
+        }
       </select>
     </div>
   );
