ba844bb28ccd533f98e49a7e09e77c790d0c7495
ewjordan <ewjordan@gmail.com>
2017-03-03 15:32:27 -0800

    Explicitly serialize rather than using each_serializer

--- dashboard/app/controllers/api/v1/schools_controller.rb
+++ dashboard/app/controllers/api/v1/schools_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::SchoolsController < ApplicationController
   # GET /api/v1/school/<school_district_id>/<school_type>
   def index
     schools = School.where(school_district_id: params[:school_district_id], school_type: params[:school_type])
-    render json: schools, each_serializer: Api::V1::SchoolSerializer
+    serialized_schools = schools.map do |school|
+      Api::V1::SchoolSerializer.new(school).attributes
+    end
+    render json: serialized_schools
   end
 end
