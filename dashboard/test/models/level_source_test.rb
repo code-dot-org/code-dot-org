fcc7e8ad28e5350e43047430166fd91f8f95b562
ashercodeorg <asher@code.org>
2017-03-03 11:49:08 -0600

    Compute LevelSource.md5 before save.

--- dashboard/test/models/level_source_test.rb
+++ dashboard/test/models/level_source_test.rb
@@ -1,11 +1,20 @@
 require 'test_helper'
 
+require 'digest/md5'
+
 class LevelSourceTest < ActiveSupport::TestCase
   self.use_transactional_test_case = true
 
   setup_all do
     @level = create :level
     @level_source = create(:level_source, level_id: @level.id, data: 'data')
+  end
+
+  test 'should auto-compute md5 on save' do
+    new_data = 'different data'
+    @level_source.update!(data: new_data)
+    @level_source.reload
+    assert_equal Digest::MD5.hexdigest(new_data), @level_source.md5
   end
 
   test "should not create level source with utf8mb8" do
