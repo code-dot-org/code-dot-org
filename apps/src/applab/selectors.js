a097aa5d654bbf3ef05ed5e48a82daac7668b911
Brad Buchanan <brad@code.org>
2017-03-03 16:42:40 -0800

    Move applab selectors into dux module

--- apps/src/applab/selectors.js
+++
@@ -1,8 +0,0 @@
-/** @file App Lab redux selectors */
-import _ from 'lodash';
-import {selectors as makerSelectors} from '../lib/kits/maker/redux';
-
-export default {
-  // Curry maker selectors so they accept the applab root state.
-  maker: _.mapValues(makerSelectors, selector => state => selector(state.maker))
-};
