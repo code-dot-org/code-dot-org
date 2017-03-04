fd2b848841404cad62b12a6ec51a41c2a8f4fc8e
Brad Buchanan <brad@code.org>
2017-03-03 16:46:43 -0800

    Remove ActionType enum

--- apps/src/applab/redux.js
+++ apps/src/applab/redux.js
@@ -1,6 +1,5 @@
 /** @file App Lab redux module */
 import _ from 'lodash';
-import * as utils from '../utils';
 import { ApplabInterfaceMode } from './constants';
 import data from '../storage/redux/data';
 import screens from './redux/screens';
@@ -11,9 +10,8 @@ import {
   selectors as makerSelectors,
 } from '../lib/kits/maker/redux';
 
-// State model?
-
 // Selectors
+
 export const selectors = {
   // Curry maker selectors so they accept the root state.
   maker: _.mapValues(makerSelectors, selector => state => selector(state.maker)),
@@ -22,21 +20,19 @@ export const selectors = {
 // Actions
 
 /** @enum {string} */
-export const ActionType = utils.makeEnum(
-    'CHANGE_INTERFACE_MODE'
-);
+const CHANGE_INTERFACE_MODE = 'applab/CHANGE_INTERFACE_MODE';
 
 /**
  * Change the interface mode between Design Mode and Code Mode
  * @param {!ApplabInterfaceMode} interfaceMode
- * @returns {{type: ActionType, interfaceMode: ApplabInterfaceMode}}
+ * @returns {{type: string, interfaceMode: ApplabInterfaceMode}}
  */
 export function changeInterfaceMode(interfaceMode) {
   if (!interfaceMode) {
     throw new Error("Expected an interace mode!");
   }
   return {
-    type: ActionType.CHANGE_INTERFACE_MODE,
+    type: CHANGE_INTERFACE_MODE,
     interfaceMode: interfaceMode
   };
 }
@@ -47,11 +43,12 @@ export const actions = {
 };
 
 // Reducers
+
 function interfaceMode(state, action) {
   state = state || ApplabInterfaceMode.CODE;
 
   switch (action.type) {
-    case ActionType.CHANGE_INTERFACE_MODE:
+    case CHANGE_INTERFACE_MODE:
       return action.interfaceMode;
     default:
       return state;
