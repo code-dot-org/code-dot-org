3e335c61be28fbf5fed1bde36ad631514724d238
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-03-07 12:29:48 -0800

    make drop table reversible

--- dashboard/db/migrate/20170307020200_drop_professional_learning_partner.rb
+++ dashboard/db/migrate/20170307020200_drop_professional_learning_partner.rb
@@ -3,6 +3,12 @@ class DropProfessionalLearningPartner < ActiveRecord::Migration[5.0]
     # note that in the migration prior to this one, we transfer all data
     # in this table to (and from) another table. Taken together, these
     # two migrations should be fully reversible.
-    drop_table :professional_learning_partners
+    drop_table :professional_learning_partners do |t|
+      t.string :name, null: false
+      t.integer :contact_id, null: false
+      t.boolean :urban
+
+      t.index [:name, :contact_id], unique: true
+    end
   end
 end
