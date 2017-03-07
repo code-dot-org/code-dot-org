40b351078739fa4bb06f1494f4cb1170a3a8bad2
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-03-06 16:13:49 -0800

    add PLP fields to regional partner

--- dashboard/app/models/regional_partner.rb
+++ dashboard/app/models/regional_partner.rb
@@ -2,14 +2,21 @@
 #
 # Table name: regional_partners
 #
-#  id    :integer          not null, primary key
-#  name  :string(255)      not null
-#  group :integer
+#  id         :integer          not null, primary key
+#  name       :string(255)      not null
+#  group      :integer
 #  contact_id :integer
+#  urban      :boolean
+#
+# Indexes
+#
+#  index_regional_partners_on_name_and_contact_id  (name,contact_id) UNIQUE
 #
 
 class RegionalPartner < ActiveRecord::Base
   CSV_IMPORT_OPTIONS = { col_sep: "\t", headers: true }
+
+  belongs_to :contact, class_name: 'User'
 
   def self.find_or_create_all_from_tsv(filename)
     CSV.read(filename, CSV_IMPORT_OPTIONS).each do |row|
