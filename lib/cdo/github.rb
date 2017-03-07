c46e8c487a576a19f69022b708dcb4f1a5e5d378
ashercodeorg <asher@code.org>
2017-03-07 14:57:23 -0600

    Create deploy_to_test cron script.

--- lib/cdo/github.rb
+++ lib/cdo/github.rb
@@ -1,29 +1,22 @@
 require 'cdo/chat_client'
 require 'net/http'
 
+# This module serves as a wrapper around the GitHub API, offering convenience
+# methods for pull request functionality
 module GitHub
   PULL_API_BASE = 'https://api.github.com/repos/code-dot-org/code-dot-org/pulls'.freeze
 
-  # Returns whether "DTT: yes" is a substring of the topic in the #developers
-  # room.
-  def self.dtt_yes?
-    current_topic = Slack.get_topic('developers')
-    current_topic.include? 'DTT: yes'
-  end
-
-  # Updates the #developers topic to reflect robo-DTT in progress.
-  # "DTT: no (robo-DTT in progress)".
-  def self.update_dtt
-  end
-
   # GitHub API: https://developer.github.com/v3/pulls/#create-a-pull-request
-  # @return [nil | Integer] Returns the PR number of the newly created DTT if
-  #   successful, or nil if unsuccessful.
-  def self.create_dtt_pull_request
+  # @param title [String] The title of the requested pull request.
+  # @param head [String] The head branch of the requested pull request.
+  # @param base [String] The base branch of the requested pull request.
+  # @return [nil | Integer] The PR number of the newly created DTT if successful
+  #   or nil if unsuccessful.
+  def self.create_pull_request(title:, head:, base:)
     params = {
-      title: 'DTT (Staging > Test) [robo-dtt]',
-      head: 'staging',
-      base: 'test'
+      title: title,
+      head: head,
+      base: base
     }
 
     uri = URI.parse PULL_API_BASE
@@ -43,8 +36,10 @@ module GitHub
   # GitHub API: https://developer.github.com/v3/pulls/#merge-a-pull-request-merge-button
   # @param pr_number [Integer] The PR number to be merged.
   # @return [Boolean] Whether the PR was merged.
-  def self.merge_dtt_pull_request(pr_number)
-    return if is_merged?(pr_number)
+  def self.merge_pull_request(pr_number)
+    if merged?(pr_number)
+      raise ArgumentError("PR#{pr_number} is already merged")
+    end
 
     uri = URI.parse "{PULL_API_BASE}/{#pr_number}/merge"
     http = Net::HTTP.new uri.host, uri.port
@@ -53,7 +48,7 @@ module GitHub
     request['Authorization'] = "token #{CDO.github_access_token}"
     response = http.request(request)
 
-    return response.code == '200'
+    response.code == '200'
   end
 
   # @param pr_number [Integer] The number of the pull request to check.
@@ -65,6 +60,6 @@ module GitHub
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
 
-    response.code.to_i == 204
+    response.code.to_i == '204'
   end
 end
