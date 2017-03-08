926d22fd57cdb893581d514e0af8c192dd3cfab0
Andrew Oberhardt <Andrew@Code.org>
2017-03-08 13:01:59 -0800

    Add optional clear button to date picker control

--- apps/src/code-studio/pd/workshop_dashboard/components/date_picker.story.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/date_picker.story.jsx
@@ -5,18 +5,31 @@ import DatePicker from './date_picker';
 export default storybook => {
   return storybook
     .storiesOf('DatePicker', module)
-    .add(
-      'Basic',
+    .addDecorator((story) => (
       // Currently the Bootstrap 3 styles required by React-Bootstrap are only applied inside div#workshop-container.
       // This is to prevent conflicts with other parts of Code Studio using Bootstrap 2.
       // See pd.scss. Without this container div it won't render properly.
+      <div id="workshop-container" style={{width:300}}>
+        {story()}
+      </div>
+    ))
+    .add(
+      'Basic',
       () => (
-        <div id="workshop-container">
-          <DatePicker
-            date={moment()}
-            onChange={storybook.action('changed')}
-          />
-        </div>
+        <DatePicker
+          date={moment()}
+          onChange={storybook.action('changed')}
+        />
+      )
+    )
+    .add(
+      'Clearable',
+      () => (
+        <DatePicker
+          date={moment()}
+          onChange={storybook.action('changed')}
+          clearable
+        />
       )
     );
 };
