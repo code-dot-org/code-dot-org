764a83bd3d6e180ba2a4ed300e66f76b1cbd4024
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-03-08 15:14:43 -0800

    add reference to specific migration

--- dashboard/db/migrate/20170307020200_drop_professional_learning_partner.rb
+++ dashboard/db/migrate/20170307020200_drop_professional_learning_partner.rb
@@ -1,8 +1,9 @@
 class DropProfessionalLearningPartner < ActiveRecord::Migration[5.0]
   def change
-    # note that in the migration prior to this one, we transfer all data
-    # in this table to (and from) another table. Taken together, these
-    # two migrations should be fully reversible.
+    # note that in migration
+    # 20170306221907_merge_professional_learning_partner_into_regional_partner,
+    # we transfer all data in this table to (and from) another table.
+    # Taken together, these two migrations should be fully reversible.
     drop_table :professional_learning_partners do |t|
       t.string :name, null: false
       t.integer :contact_id, null: false
