5443f6e3456a1b9661eb24b296603fc837beaa65
Continuous Integration <dev@code.org>
2017-03-08 19:24:44 +0000

    Add a filter to last workshop url wq

--- dashboard/app/controllers/pd/professional_learning_landing_controller.rb
+++ dashboard/app/controllers/pd/professional_learning_landing_controller.rb
@@ -10,7 +10,7 @@ class Pd::ProfessionalLearningLandingController < ApplicationController
     courses_completed = ended_workshops.pluck(:course).uniq
 
     enrollments = Pd::Enrollment.where(email: current_user.email)
-    surveys_pending_enrollment = Pd::Enrollment.filter_for_survey_completion(enrollments, false)
+    surveys_pending_enrollment = Pd::Enrollment.filter_for_survey_completion(enrollments.where.not(survey_sent_at: nil), false)
     last_pending_enrollment = surveys_pending_enrollment.any? && surveys_pending_enrollment.last
 
     summarized_plc_enrollments = Plc::UserCourseEnrollment.where(user: current_user).map(&:summarize)
