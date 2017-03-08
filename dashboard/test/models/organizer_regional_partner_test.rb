78c01593e9edd6e7aa0a64a71375ecc971171d39
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-03-07 18:17:12 -0800

    update workshop payment unit tests

---
+++ dashboard/test/models/organizer_regional_partner_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OrganizerRegionalPartnerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ dashboard/test/models/regional_partner_organizer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RegionalPartnerOrganizerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
