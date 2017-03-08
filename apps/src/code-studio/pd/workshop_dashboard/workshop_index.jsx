2d405b912cf33635f8e86fe51ee3c52daa88a0bc
Andrew Oberhardt <Andrew@Code.org>
2017-03-08 13:15:58 -0800

    Add pd-workshop filter UI

--- apps/src/code-studio/pd/workshop_dashboard/workshop_index.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/workshop_index.jsx
@@ -7,6 +7,9 @@ import {Button, ButtonToolbar} from 'react-bootstrap';
 import WorkshopTable from './components/workshop_table';
 import WorkshopTableLoader from './components/workshop_table_loader';
 
+const MAX_WORKSHOPS_PER_TABLE = 10;
+const FILTER_URL = "/api/v1/pd/workshops/filter";
+
 const WorkshopIndex = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired
@@ -28,6 +31,10 @@ const WorkshopIndex = React.createClass({
     this.context.router.push('/survey_results');
   },
 
+  generateFilterUrl(state) {
+    return `/workshops/filter?${$.param({state})}`;
+  },
+
   render() {
     const permission = window.dashboard.workshop.permission;
     const isAdmin = permission === "admin";
@@ -35,6 +42,10 @@ const WorkshopIndex = React.createClass({
     const isOrganizer = permission.indexOf('organizer') >= 0;
     const isPlp = permission.indexOf('plp') >= 0;
     const showOrganizer = isAdmin;
+    const commonFilters = {
+      limit: MAX_WORKSHOPS_PER_TABLE,
+      date_order: 'desc'
+    };
 
     return (
       <div>
@@ -49,17 +60,20 @@ const WorkshopIndex = React.createClass({
         </ButtonToolbar>
         <h2>In Progress</h2>
         <WorkshopTableLoader
-          queryUrl="/api/v1/pd/workshops/?state=In%20Progress"
+          queryUrl={FILTER_URL}
+          params={{...commonFilters, state: 'In Progress'}}
           canDelete
         >
           <WorkshopTable
             tableId="inProgressWorkshopsTable"
             showOrganizer={showOrganizer}
+            moreUrl={this.generateFilterUrl('In Progress')}
           />
         </WorkshopTableLoader>
         <h2>Upcoming</h2>
         <WorkshopTableLoader
-          queryUrl="/api/v1/pd/workshops/?state=Not%20Started"
+          queryUrl={FILTER_URL}
+          params={{...commonFilters, state: 'Not Started'}}
           canDelete
         >
           <WorkshopTable
@@ -67,14 +81,19 @@ const WorkshopIndex = React.createClass({
             canEdit
             showSignupUrl
             showOrganizer={showOrganizer}
+            moreUrl={this.generateFilterUrl('Not Started')}
           />
         </WorkshopTableLoader>
         <h2>Past</h2>
-        <WorkshopTableLoader queryUrl="/api/v1/pd/workshops/?state=Ended">
+        <WorkshopTableLoader
+          queryUrl={FILTER_URL}
+          params={{...commonFilters, state: 'Ended'}}
+        >
           <WorkshopTable
             tableId="endedWorkshopsTable"
             showOrganizer={showOrganizer}
             surveyBaseUrl={isPlp ? "/organizer_survey_results" : "/survey_results"}
+            moreUrl={this.generateFilterUrl('Ended')}
           />
         </WorkshopTableLoader>
       </div>
