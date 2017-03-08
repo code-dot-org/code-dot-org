c806750539106356fee0d76133baade2060b24be
ashercodeorg <asher@code.org>
2017-03-08 08:14:25 -0600

    PR feedback (use octokit) for GitHub.

--- lib/cdo/github.rb
+++ lib/cdo/github.rb
@@ -1,66 +1,48 @@
 require 'cdo/chat_client'
 require 'net/http'
+require 'octokit'
 
-# This module serves as a wrapper around the GitHub API, offering convenience
-# methods for pull request functionality
+# This module serves as a thin wrapper around Octokit, itself a wrapper around
+# the GitHub API.
 module GitHub
-  PULL_API_BASE = 'https://api.github.com/repos/code-dot-org/code-dot-org/pulls'.freeze
+  REPO = "code-dot-org/code-dot-org".freeze
 
-  # GitHub API: https://developer.github.com/v3/pulls/#create-a-pull-request
-  # @param title [String] The title of the requested pull request.
-  # @param head [String] The head branch of the requested pull request.
-  # @param base [String] The base branch of the requested pull request.
-  # @return [nil | Integer] The PR number of the newly created DTT if successful
-  #   or nil if unsuccessful.
-  def self.create_pull_request(title:, head:, base:)
-    params = {
-      title: title,
-      head: head,
-      base: base
-    }
-
-    uri = URI.parse PULL_API_BASE
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request['Authorization'] = "token #{CDO.github_access_token}"
-    request.body = params.to_json
-    response = http.request(request)
-
-    return nil if response.code != '201'
-
-    response_body = JSON.parse(response.body)
-    response_body['number']
+  # Configures Octokit with our GitHub access token.
+  def self.configure_octokit
+    raise unless CDO.github_access_token
+    Octokit.configure do |client|
+      client.access_token = CDO.github_access_token
+    end
   end
 
-  # GitHub API: https://developer.github.com/v3/pulls/#merge-a-pull-request-merge-button
+  # Octokit Documentation: http://octokit.github.io/octokit.rb/Octokit/Client/PullRequests.html#create_pull_request-instance_method
+  # @param base [String] The base branch of the requested pull request.
+  # @param head [String] The head branch of the requested pull request.
+  # @param title [String] The title of the requested pull request.
+  # @return [nil | Integer] The PR number of the newly created DTT if successful
+  def self.create_pull_request(base:, head:, title:)
+    configure_octokit
+    response = Octokit.create_pull_request(REPO, base, head, title)
+
+    response['number']
+  end
+
+  # Octokit Documentation: http://octokit.github.io/octokit.rb/Octokit/Client/PullRequests.html#merge_pull_request-instance_method
   # @param pr_number [Integer] The PR number to be merged.
   # @return [Boolean] Whether the PR was merged.
   def self.merge_pull_request(pr_number)
-    if merged?(pr_number)
-      raise ArgumentError("PR#{pr_number} is already merged")
+    if pull_merged?(pr_number)
+      raise ArgumentError.new("PR\##{pr_number} is already merged")
     end
-
-    uri = URI.parse "#{PULL_API_BASE}/#{pr_number}/merge"
-    http = Net::HTTP.new uri.host, uri.port
-    http.use_ssl = true
-    request = Net::HTTP::Put.new(uri.request_uri)
-    request['Authorization'] = "token #{CDO.github_access_token}"
-    response = http.request(request)
-
-    response.code == '200'
+    configure_octokit
+    response = Octokit.merge_pull_request(REPO, pr_number)
+    response['merged']
   end
 
-  # GitHub API: https://developer.github.com/v3/pulls/#get-if-a-pull-request-has-been-merged
+  # Octokit Documentation: http://octokit.github.io/octokit.rb/Octokit/Client/PullRequests.html#pull_merged
   # @param pr_number [Integer] The number of the pull request to check.
   # @return [Boolean] Whether the pull request has been merged.
-  def self.merged?(pr_number)
-    uri = URI.parse "#{PULL_API_BASE}/#{pr_number}/merge"
-    http = Net::HTTP.new uri.host, uri.port
-    http.use_ssl = true
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
-
-    response.code == '204'
+  def self.pull_merged?(pr_number)
+    Octokit.pull_merged?(REPO, pr_number)
   end
 end
