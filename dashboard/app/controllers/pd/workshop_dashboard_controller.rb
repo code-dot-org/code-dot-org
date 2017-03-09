c166623f76bd5604528493110fc3388425acc517
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-03-09 14:40:59 -0800

    grant plp permission to all organizers associated with a RP, not just contact

--- dashboard/app/controllers/pd/workshop_dashboard_controller.rb
+++ dashboard/app/controllers/pd/workshop_dashboard_controller.rb
@@ -4,7 +4,12 @@ module Pd
     def index
       @permission = nil
 
+      @available_regional_partners = current_user.regional_partners.to_a
       contact_rp = RegionalPartner.find_by(contact: current_user)
+      @available_regional_partners << contact_rp if contact_rp
+      @available_regional_partners.map! do |rp|
+        {id: rp.id, name: rp.name}
+      end
 
       if current_user.admin?
         @permission = :admin
@@ -12,19 +17,13 @@ module Pd
         permission_list = []
         permission_list << :workshop_organizer if current_user.workshop_organizer?
         permission_list << :facilitator if current_user.facilitator?
-        permission_list << :plp if contact_rp
+        permission_list << :plp unless @available_regional_partners.empty?
         @permission = permission_list unless permission_list.empty?
       end
 
       unless @permission
         render_404
         return
-      end
-
-      @available_regional_partners = current_user.regional_partners.to_a
-      @available_regional_partners << contact_rp if contact_rp
-      @available_regional_partners.map! do |rp|
-        {id: rp.id, name: rp.name}
       end
 
       view_options(full_width: true)
