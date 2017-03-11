093302f30cfe5045c590668c2cbad4ce7ffc58b0
Brad Buchanan <brad@code.org>
2017-03-10 18:35:42 -0800

    Display connecting/error overlays when needed

--- apps/test/unit/lib/kits/maker/reduxTest.js
+++ apps/test/unit/lib/kits/maker/reduxTest.js
@@ -1,7 +1,18 @@
 /** @file Tests for Maker Toolkit redux module */
 import {expect} from '../../../../util/configuredChai';
 import {createStore, combineReducers} from 'redux';
-import {reducer, enable, isEnabled} from '@cdo/apps/lib/kits/maker/redux';
+import {
+  reducer,
+  enable,
+  startConnecting,
+  reportConnected,
+  reportConnectionError,
+  disconnect,
+  isEnabled,
+  isConnecting,
+  isConnected,
+  hasConnectionError
+} from '@cdo/apps/lib/kits/maker/redux';
 
 describe('Maker Toolkit redux module', () => {
   let store;
@@ -14,12 +25,48 @@ describe('Maker Toolkit redux module', () => {
     it('is disabled', () => {
       expect(isEnabled(store.getState())).to.be.false;
     });
+
+    it('is disconnected', () => {
+      expect(isConnected(store.getState())).to.be.false;
+      expect(isConnecting(store.getState())).to.be.false;
+      expect(hasConnectionError(store.getState())).to.be.false;
+    });
   });
 
   describe('the enable action', () => {
     it('enables maker', () => {
       store.dispatch(enable());
       expect(isEnabled(store.getState())).to.be.true;
+    });
+  });
+
+  describe('the startConnecting action', () => {
+    it('sets the maker state to connecting', () => {
+      store.dispatch(startConnecting());
+      expect(isConnecting(store.getState())).to.be.true;
+      expect(isConnected(store.getState())).to.be.false;
+    });
+  });
+
+  describe('the reportConnected action', () => {
+    it('sets the maker state to connected', () => {
+      store.dispatch(reportConnected());
+      expect(isConnecting(store.getState())).to.be.false;
+      expect(isConnected(store.getState())).to.be.true;
+    });
+  });
+
+  describe('the reportConnectionError action', () => {
+    it('sets the maker state to connection error', () => {
+      store.dispatch(reportConnectionError());
+      expect(hasConnectionError(store.getState())).to.be.true;
+    });
+  });
+
+  describe('the disconnect action', () => {
+    it('disconnects maker', () => {
+      store.dispatch(disconnect());
+      expect(isConnected(store.getState())).to.be.false;
     });
   });
 });
