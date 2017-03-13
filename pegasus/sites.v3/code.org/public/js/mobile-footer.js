8288bc5c3347ad41a8643926417fd40dec700b84
Erin Bond <erin.bond@code.org>
2017-03-13 11:10:52 -0700

    Show links when the screen is enlarged even if the link menu is closed on small screen

--- pegasus/sites.v3/code.org/public/js/mobile-footer.js
+++ pegasus/sites.v3/code.org/public/js/mobile-footer.js
@@ -4,8 +4,17 @@ $(function () {
     $('#footer-links ul').slideToggle();
   });
 
-  document.querySelector( "#footer-toggle" )
+  document.querySelector( '#footer-toggle' )
     .addEventListener( "click", function () {
-    this.classList.toggle( "active" );
+    this.classList.toggle( 'active' );
+  });
+
+//Handles cases when user resizes the screen
+  addEventListener('resize', function() {
+    if (innerWidth >= 600) {
+      $('#footer-links ul').show();
+    }  else {
+      $('#footer-links ul').hide();
+    }
   });
 });
