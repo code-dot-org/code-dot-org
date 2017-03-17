95f5f8d77f83d84c20bde59e9426b65582234bbd
ewjordan <ewjordan@gmail.com>
2017-03-17 03:16:50 -0700

    Bugfix: make sure checkbox value is passed through form correctly

--- apps/src/sites/studio/pages/schoolInfo.js
+++ apps/src/sites/studio/pages/schoolInfo.js
@@ -293,6 +293,7 @@ window.SchoolInfoManager = function (existingOptions) {
   });
 
   $('#school-district-other').change(function () {
+    $(this).val($(this).prop('checked')); // Form data is sent from value, not 'checked' property
     if ($(this).prop('checked')) {
       // Disable districts.
       enableDistrictDropdown(false);
