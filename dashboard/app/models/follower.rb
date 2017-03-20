5b70c99e1c6daa95f11858e8d6b6fc8cbac5ef18
ashercodeorg <asher@code.org>
2017-03-20 13:01:03 -0500

    Make followers.user_id optional (nullable).

--- dashboard/app/models/follower.rb
+++ dashboard/app/models/follower.rb
@@ -3,7 +3,7 @@
 # Table name: followers
 #
 #  id              :integer          not null, primary key
-#  user_id         :integer          not null
+#  user_id         :integer
 #  student_user_id :integer          not null
 #  created_at      :datetime
 #  updated_at      :datetime
