0ae6997df0b3fb059c4f45bba6c8ad2a2369371c
Brendan Reville <brendan@code.org>
2017-03-20 11:54:16 -0700

    Diversity survey 2017: more work to match spec

--- dashboard/test/models/survey_result_test.rb
+++ dashboard/test/models/survey_result_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class SurveyResultTest < ActiveSupport::TestCase
   test 'proper SurveyResult members' do
     assert SurveyResult::ETHNICITIES['asian'] == 'Asian'
-    assert SurveyResult::ALL_ATTRS.include? 'survey2016_ethnicity_asian'
-    assert SurveyResult::ALL_ATTRS.include? 'survey2016_foodstamps'
+    assert SurveyResult::ALL_ATTRS.include? 'survey2017_ethnicity_asian'
+    assert SurveyResult::ALL_ATTRS.include? 'survey2017_foodstamps'
     assert SurveyResult::ALL_ATTRS.include? 'nps_value'
     assert SurveyResult::ALL_ATTRS.include? 'nps_comment'
   end
