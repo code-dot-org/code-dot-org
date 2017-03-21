5a8a07334973890edd422e7faea6bb10f0425b0c
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-03-20 19:06:39 -0700

    output stderr as well when running standalone scripts

--- bin/i18n/i18n_script_utils.rb
+++ bin/i18n/i18n_script_utils.rb
@@ -1,8 +1,11 @@
 require 'open3'
 
 def run_standalone_script(location)
-  Open3.popen3(location) do |_stdin, stdout, _stderr, _wait_thread|
+  Open3.popen3(location) do |_stdin, stdout, stderr, _wait_thread|
     while line = stdout.gets
+      puts(line)
+    end
+    while line = stderr.gets
       puts(line)
     end
   end
