e91f64f614ad4340398dea6a724406c12427c1ac
Brad Buchanan <brad@code.org>
2017-03-22 10:34:52 -0700

    StubSetupChecker extends real SetupChecker

--- apps/test/unit/lib/kits/maker/ui/BoardSetupCheckTest.js
+++ apps/test/unit/lib/kits/maker/ui/BoardSetupCheckTest.js
@@ -1,13 +1,15 @@
 /** @file Test BoardSetupCheck component */
 import React from 'react';
+import sinon from 'sinon';
 import {expect} from '../../../../../util/configuredChai';
 import {mount} from 'enzyme';
 import BoardSetupCheck from '@cdo/apps/lib/kits/maker/ui/BoardSetupCheck';
+import SetupChecker from '@cdo/apps/lib/kits/maker/util/SetupChecker';
 
 describe('BoardSetupCheck', () => {
   it('renders', done => {
-    const stubChecker = new StubSetupChecker();
-    const wrapper = mount(<BoardSetupCheck setupChecker={stubChecker}/>);
+    const checker = new StubSetupChecker();
+    const wrapper = mount(<BoardSetupCheck setupChecker={checker}/>);
     expect(wrapper).not.to.be.null;
     setTimeout(() => {
       expect(wrapper).not.to.be.null;
@@ -16,30 +18,21 @@ describe('BoardSetupCheck', () => {
   });
 });
 
-class StubSetupChecker {
-  detectChromeVersion() {
-    return Promise.resolve();
+/**
+ * SetupChecker with all methods stubbed and by default everything succeeds.
+ * Since methods are sinon stubs, individual tests can modify stub behavior
+ * as needed.
+ * @see http://sinonjs.org/releases/v2.1.0/stubs/#defining-stub-behavior-on-consecutive-calls
+ */
+class StubSetupChecker extends SetupChecker {
+  constructor() {
+    super();
+    sinon.stub(this, 'detectChromeVersion').returns(Promise.resolve());
+    sinon.stub(this, 'detectChromeAppInstalled').returns(Promise.resolve());
+    sinon.stub(this, 'detectBoardPluggedIn').returns(Promise.resolve());
+    sinon.stub(this, 'detectCorrectFirmware').returns(Promise.resolve());
+    sinon.stub(this, 'detectComponentsInitialize').returns(Promise.resolve());
+    sinon.stub(this, 'celebrate').returns(Promise.resolve());
+    sinon.stub(this, 'teardown');
   }
-
-  detectChromeAppInstalled() {
-    return Promise.resolve();
-  }
-
-  detectBoardPluggedIn() {
-    return Promise.resolve();
-  }
-
-  detectCorrectFirmware() {
-    return Promise.resolve();
-  }
-
-  detectComponentsInitialize() {
-    return Promise.resolve();
-  }
-
-  celebrate() {
-    return Promise.resolve();
-  }
-
-  teardown() {}
 }
