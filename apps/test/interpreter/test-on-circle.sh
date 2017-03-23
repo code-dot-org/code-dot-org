3c2db27b175ad6e86db8c07fa894dc3db48f8506
Paul Carduner <paul@carduner.net>
2017-03-23 14:28:14 -0700

    Automatically run interpreter tests when we change our patches

---
+++ apps/test/interpreter/test-on-circle.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+./node_modules/@code-dot-org/js-interpreter-tyrant/bin/run.js --threads 1 --run --diff --verbose
+t1=$?
+mv test/interpreter/test-results-new.json $CIRCLE_ARTIFACTS
+exit $t1
