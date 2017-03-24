be9bff615f72b35b5afde658ae22c5406a4aaaaf
Brad Buchanan <brad@code.org>
2017-03-24 15:51:12 -0700

    Restyle

--- apps/src/lib/ui/SettingsMenu.jsx
+++ apps/src/lib/ui/SettingsMenu.jsx
@@ -5,27 +5,41 @@ import {showAssetManager} from '../../code-studio/assets';
 import color from '../../util/color';
 import * as maker from '../kits/maker/toolkit';
 
-const SETTINGS_MENU_WIDTH = 250;
+const SETTINGS_MENU_WIDTH = 220;
+const TAIL_WIDTH = 14;
+const TAIL_HEIGHT = 12;
+const BACKGROUND_COLOR  = color.white;
+const BORDER_COLOR = color.light_gray;
 
 const style = {
   menu: {
     position: 'absolute',
     zIndex: 20,
     width: SETTINGS_MENU_WIDTH,
-    borderStyle: 'solid',
-    borderWidth: 2,
-    borderColor: color.purple,
-    backgroundColor: 'white',
+    border: `1px solid ${BORDER_COLOR}`,
+    backgroundColor: BACKGROUND_COLOR,
+    borderRadius: 2,
+    boxShadow: "3px 3px 3px gray",
+    marginTop: 5,
+    textAlign: 'center',
   },
-  arrow: {
-    position: 'absolute',
-    bottom: '100%',
-    left: '50%',
-    marginLeft: -7,
-    borderWidth: 7,
-    borderStyle: 'solid',
-    borderColor: `transparent transparent ${color.purple} transparent`,
-  }
+};
+const tailBorderStyle = {
+  position: 'absolute',
+  bottom: '100%',
+  left: '50%',
+  marginLeft: -TAIL_WIDTH / 2,
+  borderTopWidth: 0,
+  borderBottomWidth: TAIL_HEIGHT,
+  borderLeftWidth: TAIL_WIDTH / 2,
+  borderRightWidth: TAIL_WIDTH / 2,
+  borderStyle: 'solid',
+  borderColor: `transparent transparent ${BORDER_COLOR} transparent`,
+};
+const tailFillStyle = {
+  ...tailBorderStyle,
+  bottom: 'calc(100% - 2px)',
+  borderColor: `transparent transparent ${BACKGROUND_COLOR} transparent`,
 };
 
 class SettingsMenu extends Component {
@@ -57,8 +71,8 @@ class SettingsMenu extends Component {
     const {targetRect} = this.props;
     const menuStyle = {
       ...style.menu,
-      top: targetRect.bottom + 5,
-      left: targetRect.left + (targetRect.width / 2) - (SETTINGS_MENU_WIDTH / 2),
+      top: targetRect.bottom,
+      left: targetRect.left + (targetRect.width / 2) - (SETTINGS_MENU_WIDTH / 2) - 1,
     };
 
     return (
@@ -75,7 +89,9 @@ class SettingsMenu extends Component {
             />
           }
         </div>
-        <span style={style.arrow}/>
+        {/* These elements are used to draw the 'tail' with CSS */}
+        <span style={tailBorderStyle}/>
+        <span style={tailFillStyle}/>
       </div>
     );
   }
@@ -89,7 +105,10 @@ class MenuItem_ extends Component {
   };
 
   static style = {
-    color: color.black,
+    color: color.dark_charcoal,
+    paddingLeft: '1em',
+    paddingRight: '1em',
+    cursor: 'pointer',
     ':hover': {
       backgroundColor: color.lightest_gray,
     }
