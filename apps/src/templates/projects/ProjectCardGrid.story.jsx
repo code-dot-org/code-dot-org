ca2f208148e85b49058747e46011856652cae357
Erin Bond <erin.bond@code.org>
2017-03-24 11:01:13 -0700

    Refactor ProjectCardGrid to include a helper function to generate projects for public gallery

--- apps/src/templates/projects/ProjectCardGrid.story.jsx
+++ apps/src/templates/projects/ProjectCardGrid.story.jsx
@@ -1,15 +1,53 @@
 import React from 'react';
 import ProjectCardGrid from './ProjectCardGrid';
 
-// function generateFakeProject(overrideData) {
-//   return {
-//     ...defaultProject,
-//     ...overrideData
-//   };
-// }
-//
-// // elsewhere
-// generateFakeProject({publishedToClass: true});
+let projectTypes = [
+  'applab',
+  'gamelab',
+  'artist',
+  'playlab',
+  'weblab'
+];
+
+const defaultProject = {
+  projectData: {
+    channel: 'ABCDEFGHIJKLM01234',
+    projectName: 'Puppy Playdate',
+    studentName: 'Penelope',
+    studentAge: 8,
+    type: 'applab',
+    updatedAt: '2016-10-31T23:59:59.999-08:00',
+    publishedToPublic: true,
+    publishedToClass: false
+  },
+  currentGallery: "public"
+};
+
+function generateFakeProject(overrideData) {
+  return {
+    ...defaultProject,
+    projectData: {
+      ...defaultProject.projectData,
+      ...overrideData
+    },
+    currentGallery: {
+      ...defaultProject.currentGallery,
+      ...overrideData
+    }
+  };
+}
+
+function generateFakePublicProjects() {
+  let publicProjects = [];
+  let i = 0;
+    projectTypes.forEach(type => {
+      for (i=0; i < 5; i++) {
+        publicProjects.push(generateFakeProject({type: type, projectName: type}));
+      }
+    });
+  return publicProjects;
+}
+
 
 const CLASS_PROJECTS = [
   {
@@ -53,333 +91,6 @@ const CLASS_PROJECTS = [
   },
 ];
 
-const PUBLIC_PROJECTS = [
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Applab 1',
-      studentName: 'Penelope',
-      studentAge: 4,
-      type: 'applab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Applab 2',
-      studentName: 'Penelope',
-      studentAge: 8,
-      type: 'applab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Applab 3',
-      studentName: 'Penelope',
-      studentAge: 13,
-      type: 'applab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Applab 4',
-      studentName: 'Penelope',
-      studentAge: 18,
-      type: 'applab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Applab 5',
-      studentName: 'Penelope',
-      studentAge: 14,
-      type: 'applab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Gamelab 1',
-      studentName: 'Penelope',
-      studentAge: 4,
-      type: 'gamelab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Gamelab 2',
-      studentName: 'Penelope',
-      studentAge: 8,
-      type: 'gamelab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Gamelab 3',
-      studentName: 'Penelope',
-      studentAge: 13,
-      type: 'gamelab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Gamelab 4',
-      studentName: 'Penelope',
-      studentAge: 18,
-      type: 'gamelab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Gamelab 5',
-      studentName: 'Penelope',
-      studentAge: 14,
-      type: 'gamelab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Weblab 1',
-      studentName: 'Penelope',
-      studentAge: 4,
-      type: 'weblab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Weblab 2',
-      studentName: 'Penelope',
-      studentAge: 8,
-      type: 'weblab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Weblab 3',
-      studentName: 'Penelope',
-      studentAge: 13,
-      type: 'weblab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Weblab 4',
-      studentName: 'Penelope',
-      studentAge: 18,
-      type: 'weblab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Weblab 5',
-      studentName: 'Penelope',
-      studentAge: 14,
-      type: 'weblab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Artist 1',
-      studentName: 'Penelope',
-      studentAge: 4,
-      type: 'artist',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Artist 2',
-      studentName: 'Penelope',
-      studentAge: 8,
-      type: 'artist',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Artist 3',
-      studentName: 'Penelope',
-      studentAge: 13,
-      type: 'artist',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Artist 4',
-      studentName: 'Penelope',
-      studentAge: 18,
-      type: 'artist',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Artist 5',
-      studentName: 'Penelope',
-      studentAge: 14,
-      type: 'artist',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Playlab 1',
-      studentName: 'Penelope',
-      studentAge: 4,
-      type: 'playlab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Playlab 2',
-      studentName: 'Penelope',
-      studentAge: 8,
-      type: 'playlab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Playlab 3',
-      studentName: 'Penelope',
-      studentAge: 13,
-      type: 'playlab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Playlab 4',
-      studentName: 'Penelope',
-      studentAge: 18,
-      type: 'playlab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-  {
-    projectData: {
-      channel: 'ABCDEFGHIJKLM01234',
-      projectName: 'Playlab 5',
-      studentName: 'Penelope',
-      studentAge: 14,
-      type: 'playlab',
-      updatedAt: '2016-12-31T23:59:59.999-08:00',
-      publishedToPublic: true,
-      publishedToClass: false
-    },
-    currentGallery: "public"
-  },
-];
 
 const PERSONAL_PROJECTS = [
   {
@@ -475,7 +186,7 @@ export default storybook => {
         description: 'Public gallery sorted by project type AND recency of when the project was added.',
         story: () => (
           <ProjectCardGrid
-            projects = {PUBLIC_PROJECTS}
+            projects = {generateFakePublicProjects()}
             galleryType = "public"
           />
         )
