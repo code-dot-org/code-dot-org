f4be1def1830b5194210c0d92da3845111e0f7eb
ewjordan <ewjordan@gmail.com>
2017-03-24 11:57:57 -0700

    Add cron_helper, switch backup script to log there

---
+++ lib/cdo/cron_helper.rb
@@ -0,0 +1,11 @@
+require 'cdo/chat_client'
+
+class CronHelper
+  CRON_REPORT_ROOM = 'cron-daily'
+  def self.report_cron_daily_result(task_name, success)
+    status = success ? 'success' : 'failure'
+    result_string = "#{task_name} : #{status}"
+    color = success ? 'green' : 'red'
+    ChatClient.message CRON_REPORT_ROOM, result_string, color: color
+  end
+end
