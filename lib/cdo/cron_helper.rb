29217084cbe4f09ecee3c213d80b510799ee01d4
ewjordan <ewjordan@gmail.com>
2017-03-24 13:42:53 -0700

    Style changes

--- lib/cdo/cron_helper.rb
+++ lib/cdo/cron_helper.rb
@@ -2,9 +2,10 @@ require 'cdo/chat_client'
 
 class CronHelper
   CRON_REPORT_ROOM = 'cron-daily'.freeze
+
   def self.report_cron_daily_result(task_name, success)
     status = success ? 'success' : 'failure'
-    result_string = "#{task_name} : #{status}"
+    result_string = "#{task_name}: #{status}"
     color = success ? 'green' : 'red'
     ChatClient.message CRON_REPORT_ROOM, result_string, color: color
   end
