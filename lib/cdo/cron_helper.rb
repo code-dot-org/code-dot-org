61c3d51a75bb97693e82642b67ce28be0f99abea
ewjordan <ewjordan@gmail.com>
2017-03-24 23:37:46 -0700

    Switch from HipChat to ChatClient, and update log4j logging configuration

--- lib/cdo/cron_helper.rb
+++ lib/cdo/cron_helper.rb
@@ -1,11 +1,12 @@
-require 'cdo/hip_chat'
+require 'cdo/chat_client'
 
 class CronHelper
   CRON_REPORT_ROOM = 'cron-daily'
   def self.report_cron_daily_result(task_name, success)
     status = success ? 'success' : 'failure'
-    result_string = "#{task_name} : #{status}"
+
+    result_string = "#{task_name}: #{status}"
     color = success ? Slack::COLOR_MAP[:green] : Slack::COLOR_MAP[:red]
-    HipChat.message CRON_REPORT_ROOM, result_string, color: color
+    ChatClient.message CRON_REPORT_ROOM, result_string, color: color
   end
 end
