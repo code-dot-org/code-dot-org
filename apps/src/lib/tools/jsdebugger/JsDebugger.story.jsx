27176d043100bdd3df769edd127c608846840922
Brad Buchanan <brad@code.org>
2017-03-27 11:30:01 -0700

    Move storybook redux stores into story functions

--- apps/src/lib/tools/jsdebugger/JsDebugger.story.jsx
+++ apps/src/lib/tools/jsdebugger/JsDebugger.story.jsx
@@ -21,95 +21,99 @@ export default storybook => {
     width: '100%',
   };
 
-  storyTable.push(
-    {
-      name: 'empty',
-      story: () => (
-        <Provider store={createApplabStore()}>
-          <JsDebugger
-            style={storybookStyle}
-          />
+  storyTable.push({
+    name: 'empty',
+    story: () => (
+      <Provider store={createApplabStore()}>
+        <JsDebugger
+          style={storybookStyle}
+        />
+      </Provider>
+    )
+  });
+
+  storyTable.push({
+    name: 'with only debug console',
+    story: () => {
+      const withDebugConsoleStore = createApplabStore();
+      withDebugConsoleStore.dispatch(setPageConstants({
+        showDebugConsole: true,
+      }));
+      return (
+        <div style={{height: 200}}>
+          <Provider store={withDebugConsoleStore}>
+            <JsDebugger style={storybookStyle}/>
+          </Provider>
+        </div>
+      );
+    }
+  });
+
+  storyTable.push({
+    name: 'with speed slider',
+    story: () => {
+      const withDebugSliderStore = createApplabStore();
+      withDebugSliderStore.dispatch(setPageConstants({
+        showDebugConsole: true,
+        showDebugSlider: true,
+      }));
+      return (
+        <Provider store={withDebugSliderStore}>
+          <JsDebugger style={storybookStyle}/>
         </Provider>
-      )
-    });
+      );
+    }
+  });
 
-  const withDebugConsoleStore = createApplabStore();
-  withDebugConsoleStore.dispatch(setPageConstants({
-    showDebugConsole: true,
-  }));
-  storyTable.push(
-      {
-        name: 'with only debug console',
-        story: () => (
-            <div style={{height: 200}}>
-              <Provider store={withDebugConsoleStore}>
-                <JsDebugger style={storybookStyle}/>
-              </Provider>
-            </div>
-        )
-      });
-
-  const withDebugSliderStore = createApplabStore();
-  withDebugSliderStore.dispatch(setPageConstants({
-    showDebugConsole: true,
-    showDebugSlider: true,
-  }));
-  storyTable.push(
-      {
-        name: 'with speed slider',
-        story: () => (
-            <Provider store={withDebugSliderStore}>
-              <JsDebugger style={storybookStyle}/>
-            </Provider>
-        )
-      });
-
-  const withDebugButtonsStore = createApplabStore();
-  withDebugButtonsStore.dispatch(setPageConstants({
-    showDebugConsole: true,
-    showDebugButtons: true,
-  }));
-  storyTable.push(
-    {
-      name: 'with debug buttons',
-      story: () => (
+  storyTable.push({
+    name: 'with debug buttons',
+    story: () => {
+      const withDebugButtonsStore = createApplabStore();
+      withDebugButtonsStore.dispatch(setPageConstants({
+        showDebugConsole: true,
+        showDebugButtons: true,
+      }));
+      return (
         <Provider store={withDebugButtonsStore}>
           <JsDebugger style={storybookStyle}/>
         </Provider>
-      )
-    });
+      );
+    }
+  });
 
-  const withDebugWatchStore = createApplabStore();
-  withDebugWatchStore.dispatch(setPageConstants({
-    showDebugConsole: true,
-    showDebugWatch: true,
-  }));
-  storyTable.push(
-      {
-        name: 'with debug watch',
-        story: () => (
-            <Provider store={withDebugWatchStore}>
-              <JsDebugger style={storybookStyle}/>
-            </Provider>
-        )
-      });
+  storyTable.push({
+    name: 'with debug watch',
+    story: () => {
+      const withDebugWatchStore = createApplabStore();
+      withDebugWatchStore.dispatch(setPageConstants({
+        showDebugConsole: true,
+        showDebugWatch: true,
+      }));
+      return (
+        <Provider store={withDebugWatchStore}>
+          <JsDebugger style={storybookStyle}/>
+        </Provider>
+      );
+    }
+  });
 
-  const showAllStore = createApplabStore();
-  showAllStore.dispatch(setPageConstants({
-    showDebugButtons: true,
-    showDebugConsole: true,
-    showDebugWatch: true,
-    showDebugSlider: true
-  }));
-  storyTable.push(
-    {
-      name: 'connected to redux stores with everything enabled',
-      story: () => (
+  storyTable.push({
+    name: 'connected to redux stores with everything enabled',
+    story: () => {
+      const showAllStore = createApplabStore();
+      showAllStore.dispatch(setPageConstants({
+        showDebugButtons: true,
+        showDebugConsole: true,
+        showDebugWatch: true,
+        showDebugSlider: true
+      }));
+      return (
         <Provider store={showAllStore}>
           <JsDebugger style={storybookStyle} debugWatch/>
         </Provider>
-      )
-    });
+      );
+    }
+  });
 
   storybook
     .storiesOf('JsDebugger', JsDebugger)
