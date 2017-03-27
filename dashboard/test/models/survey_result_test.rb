e464145565712a85f461001740439bd3382fd63e
ashercodeorg <asher@code.org>
2017-03-27 16:27:23 -0500

    Remove survey prefix.

--- dashboard/test/models/survey_result_test.rb
+++ dashboard/test/models/survey_result_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class SurveyResultTest < ActiveSupport::TestCase
   test 'proper SurveyResult members' do
     assert SurveyResult::ETHNICITIES['asian'] == 'Asian'
-    assert SurveyResult::ALL_ATTRS.include? 'survey_ethnicity_asian'
-    assert SurveyResult::ALL_ATTRS.include? 'survey_foodstamps'
+    assert SurveyResult::ALL_ATTRS.include? 'ethnicity_asian'
+    assert SurveyResult::ALL_ATTRS.include? 'foodstamps'
     assert SurveyResult::ALL_ATTRS.include? 'nps_value'
     assert SurveyResult::ALL_ATTRS.include? 'nps_comment'
   end
