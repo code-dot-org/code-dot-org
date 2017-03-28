d65ff15a7ddd50f64fc754c69e297b5776598d17
Mehal Shah <mehal@code.org>
2017-03-28 14:20:49 -0700

    Use script cache

--- dashboard/app/controllers/scripts_controller.rb
+++ dashboard/app/controllers/scripts_controller.rb
@@ -8,7 +8,7 @@ class ScriptsController < ApplicationController
 
   def show
     if @script.redirect_to?
-      redirect_to script_path(@script.redirect_to)
+      redirect_to :show, Script.get_from_cache(params[@script.redirect_to])
       return
     end
 
