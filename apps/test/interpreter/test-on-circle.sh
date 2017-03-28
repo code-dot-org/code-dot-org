a53f1d8ff4d9edf73d4bf21b247e093f34b97fcc
Paul Carduner <paul@carduner.net>
2017-03-28 01:34:12 +0000

    fix running of test262 suite on circle

--- apps/test/interpreter/test-on-circle.sh
+++ apps/test/interpreter/test-on-circle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-./node_modules/@code-dot-org/js-interpreter-tyrant/bin/run.js --threads 1 --run --diff --verbose
+./node_modules/@code-dot-org/js-interpreter-tyrant/bin/run.js --threads 1 --run --diff --verbose --root test/interpreter
 t1=$?
 mv test/interpreter/test-results-new.json $CIRCLE_ARTIFACTS
 exit $t1
