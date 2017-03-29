9470027c2c917143bc989c3fc66bcbe4d81c5cd5
Brent Van Minnen <bjvanminnen@gmail.com>
2017-03-29 16:27:55 -0700

    redesigned miniview (with and without experiment)

--- apps/src/code-studio/components/progress/MiniView.jsx
+++ apps/src/code-studio/components/progress/MiniView.jsx
@@ -1,6 +1,18 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import CourseProgress from './CourseProgress';
+import MiniViewTopRow from './MiniViewTopRow';
+import experiments from '@cdo/apps/util/experiments';
+
+const progressRedesignEnabled = experiments.isEnabled('progressRedesign');
+
+const styles = {
+  // We want a margin for the non-redesigned course progress
+  oldCourseProgress: {
+    marginLeft: 10,
+    marginRight: 10
+  }
+};
 
 /**
  * The course progress dropdown you get when you click the arrow in the header.
@@ -13,15 +25,31 @@ const MiniView = React.createClass({
   render() {
     const { hasFullProgress } = this.props;
 
+    let body;
     if (!hasFullProgress) {
       // Ideally we would specify inline CSS instead of using a classname here,
       // but the image used here gets digested by rails, and we don't know the
       // digested path
-      return <div className="loading"/>;
+      body = <div className="loading"/>;
+    } else {
+      body = (
+        <div style={progressRedesignEnabled ? undefined : styles.oldCourseProgress}>
+          <CourseProgress onOverviewPage={false}/>
+        </div>
+      );
     }
 
+    // TODO
+    const linesOfCode = 118;
+    const scriptName = "course1";
     return (
-      <CourseProgress onOverviewPage={false}/>
+      <div>
+        <MiniViewTopRow
+          scriptName={scriptName}
+          linesOfCode={linesOfCode}
+        />
+        {body}
+      </div>
     );
   }
 });
