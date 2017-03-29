ce9c6f0c2f98107b8e3aa25abe1cfa51eaac0b43
Brad Buchanan <brad@code.org>
2017-03-29 16:50:47 -0700

    RFC: Storybook unit tests

---
+++ apps/test/unit/lib/ui/PopUpMenuStoryTest.js
@@ -0,0 +1,4 @@
+/** @file cover PopUpMenu.story.jsx with unit tests */
+import testStorybook from '../../../util/testStorybook';
+import storiesOfPopUpMenu from '@cdo/apps/lib/ui/PopUpMenu.story';
+testStorybook(storiesOfPopUpMenu);
