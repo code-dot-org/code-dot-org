24d3d9573756fecabaf8b688adec1f6fadf6b722
Erin Bond <erin.bond@code.org>
2017-03-30 10:30:44 -0700

    GradientNavCard sucessfully pulls info from example data in story

--- apps/src/templates/GradientNavCard.jsx
+++ apps/src/templates/GradientNavCard.jsx
@@ -1,31 +1,49 @@
 import React from 'react';
-// import color from "./../util/color";
+import color from "./../util/color";
 // import i18n from "@cdo/locale";
-//
-// const styles = {
-//   card: {
-//
-//   },
-//   image: {
-//
-//   },
-//   title: {
-//
-//   },
-//   description: {
-//
-//   }
-// }
+// import Button from './Button';
+
+// let imgUrl = this.props.cardData.image
+
+const styles = {
+  card: {
+    flex: 1,
+    // backgroundImage: 'url(' + imgUrl + ')',
+    backgroundSize: 'cover',
+    overflow: 'hidden',
+  },
+  image: {
+    flex: 1
+
+  },
+  title: {
+    paddingLeft: 15,
+    paddingRight: 10,
+    paddingTop: 10,
+    paddingBottom: 5,
+    fontSize: 16,
+    fontFamily: '"Gotham 5r", sans-serif',
+   color: color.red
+  },
+  description: {
+
+  }
+};
 
 const GradientNavCard = React.createClass({
   propTypes: {
-    projectData: React.PropTypes,
-    currentGallery: React.PropTypes.string,
+    cardData: React.PropTypes
   },
 
   render() {
+    const { cardData } = this.props;
+
     return (
-      <div>Hey, this is where the card goes.</div>
+      <div style={styles.card}>
+          <h5>Hey, this is where the card goes.</h5>
+          <h2 style={styles.title}>{cardData.title}</h2>
+      </div>
+
     );
   }
 
