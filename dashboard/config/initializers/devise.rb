017eafc08f4158604cbe420785dd023158c8b1ec
Brendan Reville <brendan@code.org>
2017-03-29 17:45:46 -0700

    Initial idea for a user_type cookie for signed-in users

--- dashboard/config/initializers/devise.rb
+++ dashboard/config/initializers/devise.rb
@@ -315,6 +315,14 @@ Devise.setup do |config|
     manager.failure_app = CustomDeviseFailure
   end
 
+  Warden::Manager.after_authentication do |user|
+    auth.cookies[:user_type] = user.teacher? ? "teacher" : "student"
+  end
+
+  Warden::Manager.before_logout do
+    auth.cookies.delete :user_type
+  end
+
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
