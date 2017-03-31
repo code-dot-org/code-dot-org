c86d1d24ef789cd2aad15e4840cee0acf44f8d53
ewjordan <ewjordan@gmail.com>
2017-03-31 11:35:13 -0700

    Add comments

--- dashboard/app/controllers/sessions_controller.rb
+++ dashboard/app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < Devise::SessionsController
     session[:return_to] ||= params[:return_to]
     @already_hoc_registered = params[:already_hoc_registered]
     if params[:providerNotLinked]
+      # This code is only reached through the oauth flow when the user already has an email account.
+      # Usually email would not be available for students, this is a special case where oauth fills it in.
       flash.now[:alert] = I18n.t 'auth.not_linked', provider: I18n.t("auth.#{params[:providerNotLinked]}")
       @email = params[:email]
     end
