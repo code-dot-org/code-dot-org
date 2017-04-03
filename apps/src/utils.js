3943cbb9f00e4a79a84fd3b340c87e3f74276e5a
David Bailey <davidsbailey@users.noreply.github.com>
2017-04-03 14:41:07 -0700

    add a polyfill for HTMLCanvasElement.prototype.toBlob

--- apps/src/utils.js
+++ apps/src/utils.js
@@ -237,6 +237,26 @@ if (!String.prototype.repeat) {
 }
 
 /**
+ * A low-performance polyfill for toBlob based on toDataURL. Adapted from:
+ * https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob
+ */
+if (!HTMLCanvasElement.prototype.toBlob) {
+  Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+    value: function (callback, type, quality) {
+      const binStr = atob(this.toDataURL(type, quality).split(',')[1]);
+      const arr = new Uint8Array(binStr.length);
+
+      for (let i = 0; i < binStr.length; i++) {
+        arr[i] = binStr.charCodeAt(i);
+      }
+
+      const blob = new Blob([arr], {type: type || 'image/png'});
+      callback(blob);
+    }
+  });
+}
+
+/**
  * Similar to val || defaultVal, except it's gated on whether or not val is
  * undefined instead of whether val is falsey.
  * @returns {*} val if not undefined, otherwise defaultVal
