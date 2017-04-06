5be38fb59cbb9aa040eaaa80f5f3db30d7c068d1
ashercodeorg <asher@code.org>
2017-04-06 11:04:33 -0500

    Move SectionHelpers test from SectionApiHelpersTest to own file.

---
+++ lib/test/cdo/test_section_helpers.rb
@@ -0,0 +1,17 @@
+require_relative '../test_helper'
+require 'cdo/section_helpers'
+
+class SectionHelpersTest < Minitest::Test
+  describe 'random code' do
+    # TODO(asher): This test is wrong (probabilistically). Fix it.
+    it 'does not generate the same code twice' do
+      codes = 10.times.map {SectionHelpers.random_code}
+      assert_equal 10, codes.uniq.length
+    end
+
+    it 'does not generate vowels' do
+      codes = 10.times.map {SectionHelpers.random_code}
+      assert codes.grep(/[AEIOU]/).empty?
+    end
+  end
+end
