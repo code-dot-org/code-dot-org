73c0c18a6c3abe68627432855eda27a26d92a506
Brad Buchanan <brad@code.org>
2017-04-06 16:00:32 -0700

    Unblacklist most progress stories

--- apps/test/storybook/renderStoriesTest.js
+++ apps/test/storybook/renderStoriesTest.js
@@ -4,18 +4,7 @@ import {throwOnConsoleErrors, throwOnConsoleWarnings} from '../util/testUtils';
 import testStorybook from './util/testStorybook';
 
 const BLACKLIST = [
-  'templates/progress/ProgressButton.story.jsx',
   'templates/progress/ProgressLessonTeacherInfo.story.jsx',
-  'templates/progress/ProgressGroup.story.jsx',
-  'templates/progress/SummaryProgressTable.story.jsx',
-  'templates/progress/ProgressDetailToggle.story.jsx',
-  'templates/progress/ProgressLessonContent.story.jsx',
-  'templates/progress/ProgressLevelSet.story.jsx',
-  'templates/progress/ProgressLesson.story.jsx',
-  'templates/progress/ProgressPill.story.jsx',
-  'templates/progress/ProgressBubbleSet.story.jsx',
-  'templates/progress/DetailProgressTable.story.jsx',
-  'templates/progress/ProgressBubble.story.jsx',
   'templates/MultiCheckboxSelector.story.jsx',
   'templates/GameButtons.story.jsx',
   'templates/SpeedSlider.story.jsx',
