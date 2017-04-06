f5972fb1e60be4d0a9198f91b4240cd4656a8873
Brad Buchanan <brad@code.org>
2017-04-06 14:08:02 -0700

    Add storybook test target

--- apps/test-low-memory.sh
+++ apps/test-low-memory.sh
@@ -7,6 +7,10 @@ for i in 1 2; do node --max_old_space_size=4096 `npm bin`/grunt unitTest && brea
 if [ -n "$CIRCLECI" ]; then \
     bash <(curl -s https://codecov.io/bash) -cF unit; \
 fi &&
+for i in 1 2; do node --max_old_space_size=4096 `npm bin`/grunt storybookTest && break; done && \
+if [-n "$CIRCLECI" ]; then \
+    bash <(curl -s https://codecov.io/bash) -cF storybook; \
+fi
 for i in 1 2; do node --max_old_space_size=4096 `npm bin`/grunt integrationTest && break; done && \
 if [ -n "$CIRCLECI" ]; then \
     bash <(curl -s https://codecov.io/bash) -cF integration; \
---
+++ apps/test/storybook/renderStoriesTest.js
@@ -0,0 +1,57 @@
+// Load all .story.jsx files and render them with enzyme, checking for
+// render errors and other problems.
+import testStorybook from './util/testStorybook';
+
+const BLACKLIST = [
+  'applab/ImportProjectDialog.story.jsx',
+  'applab/ImportScreensDialog.story.jsx',
+  'applab/ApplabVisualizationColumn.story.jsx',
+  'code-studio/components/ImagePicker.story.jsx',
+  'code-studio/components/ShareDialog.story.jsx',
+  'code-studio/components/progress/MiniViewTopRow.story.jsx',
+  'code-studio/components/progress/ProgressDot.story.jsx',
+  'code-studio/components/progress/StatusProgressDot.story.jsx',
+  'code-studio/pd/workshop_dashboard/components/date_picker.story.jsx',
+  'lib/tools/jsdebugger/JsDebugger.story.jsx',
+  'publicKeyCryptography/NumberedSteps.story.jsx',
+  'publicKeyCryptography/PublicKeyCryptographyWidget.story.jsx',
+  'storage/dataBrowser/ConfirmDeleteButton.story.jsx',
+  'templates/projects/ProjectsList.story.jsx',
+  'templates/projects/SectionProjectsList.story.jsx',
+  'templates/DialogButtons.story.jsx',
+  'templates/Dialog.story.jsx',
+  'templates/BaseDialog.story.jsx',
+  'templates/Lightbulb.story.jsx',
+  'templates/watchers/AutocompleteSelector.story.jsx',
+  'templates/watchers/Watchers.story.jsx',
+  'templates/VersionHistory.jsx',
+  'templates/PaginationWrapper.story.jsx',
+  'templates/Button.story.jsx',
+  'templates/HrefButton.story.jsx',
+  'templates/progress/ProgressButton.story.jsx',
+  'templates/progress/ProgressLessonTeacherInfo.story.jsx',
+  'templates/progress/ProgressGroup.story.jsx',
+  'templates/progress/SummaryProgressTable.story.jsx',
+  'templates/progress/ProgressDetailToggle.story.jsx',
+  'templates/progress/ProgressLessonContent.story.jsx',
+  'templates/progress/ProgressLevelSet.story.jsx',
+  'templates/progress/ProgressLesson.story.jsx',
+  'templates/progress/ProgressPill.story.jsx',
+  'templates/progress/ProgressBubbleSet.story.jsx',
+  'templates/progress/DetailProgressTable.story.jsx',
+  'templates/progress/ProgressBubble.story.jsx',
+  'templates/MultiCheckboxSelector.story.jsx',
+  'templates/GameButtons.story.jsx',
+  'templates/SpeedSlider.story.jsx',
+];
+
+describe('Rendering stories', () => {
+  const context = require.context('../../src/', true, /.*\.story\.jsx$/);
+  context.keys()
+      .filter(storyFile => !BLACKLIST.some(taboo => storyFile.includes(taboo)))
+      .forEach(storyFile => {
+        describe(storyFile, () => {
+          testStorybook(context(storyFile));
+        });
+      });
+});
