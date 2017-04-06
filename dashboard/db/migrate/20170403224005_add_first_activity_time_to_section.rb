c1796092c82f66f1c42b2e29e5c3514531dff8b1
Ram Kandasamy <ram@code.org>
2017-04-06 14:45:44 -0700

    s/first_activity_time/first_activity_at

--- dashboard/db/migrate/20170403224005_add_first_activity_time_to_section.rb
+++ dashboard/db/migrate/20170403224005_add_first_activity_time_to_section.rb
@@ -1,8 +1,8 @@
 class AddFirstActivityTimeToSection < ActiveRecord::Migration[5.0]
   def change
-    # Give existing rows a fake first_activity_time that's in the past, but
+    # Give existing rows a fake first_activity_at that's in the past, but
     # newly created rows should default to nil
-    add_column :sections, :first_activity_time, :datetime, default: DateTime.parse('1st Jan 1970')
-    change_column_default :sections, :first_activity_time, from: DateTime.parse('1st Jan 1970'), to: nil
+    add_column :sections, :first_activity_at, :datetime, default: DateTime.parse('1st Jan 1970')
+    change_column_default :sections, :first_activity_at, from: DateTime.parse('1st Jan 1970'), to: nil
   end
 end
