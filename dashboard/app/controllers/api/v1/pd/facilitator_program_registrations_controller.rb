68c35f0d99d1606df3dae4c1f27e283ece11425b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-04-10 14:49:11 -0700

    remove unnecessary index view

--- dashboard/app/controllers/api/v1/pd/facilitator_program_registrations_controller.rb
+++ dashboard/app/controllers/api/v1/pd/facilitator_program_registrations_controller.rb
@@ -1,24 +1,6 @@
 class Api::V1::Pd::FacilitatorProgramRegistrationsController < ApplicationController
   authorize_resource class: 'Pd::FacilitatorProgramRegistration', only: :create
 
-  def index
-    # Require admin or a matching secret_key param
-    unless current_user.try(:admin?)
-      secret_key = CDO.pd_facilitator_program_registration_list_secret_key
-      unless secret_key && secret_key == params[:secret_key]
-        render_404
-        return
-      end
-    end
-
-    @facilitator_program_registrations = ::Pd::FacilitatorProgramRegistration
-    if params[:after_id]
-      @facilitator_program_registrations = @facilitator_program_registrations.where('id > ?', params[:after_id])
-    end
-
-    render json: @facilitator_program_registrations.all.map(&:to_expanded_json)
-  end
-
   def create
     form_data_hash = params.try(:[], :form_data) || {}
     form_data_json = form_data_hash.to_unsafe_h.to_json.strip_utf8mb4
