9be01ea11b0f1cae83109eb20b9763a6b266b4c6
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-04-10 13:07:05 -0700

    make facilitator registration form specific to a single teachercon

--- dashboard/app/controllers/pd/facilitator_program_registration_controller.rb
+++ dashboard/app/controllers/pd/facilitator_program_registration_controller.rb
@@ -8,9 +8,29 @@ class Pd::FacilitatorProgramRegistrationController < ApplicationController
     authorize! :create, Pd::FacilitatorProgramRegistration
     session.delete :return_to
 
-    if Pd::FacilitatorProgramRegistration.exists?(user: current_user)
-      @registration = Pd::FacilitatorProgramRegistration.find_by(user: current_user)
-      render :submitted
+    begin
+      @teachercon = params.require(:teachercon).to_i
+    rescue ActionController::ParameterMissing
+      redirect_to teachercon: 1
+      return
     end
+
+    if Pd::FacilitatorProgramRegistration.exists?(user: current_user, teachercon: @teachercon)
+      @registration = Pd::FacilitatorProgramRegistration.find_by(user: current_user, teachercon: @teachercon)
+      render :submitted
+      return
+    end
+
+    dates = Pd::FacilitatorProgramRegistration.attendance_dates(current_user, @teachercon)
+    if dates.nil?
+      render :not_attending
+      return
+    end
+
+    @script_data = {
+      options: Pd::FacilitatorProgramRegistration::OPTIONS.camelize_keys.to_json,
+      attendanceDates: dates.to_json,
+      teacherconLocation: Pd::FacilitatorProgramRegistration::LOCATIONS[@teachercon - 1].to_json
+    }
   end
 end
