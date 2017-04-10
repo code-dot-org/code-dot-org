cc6b9eff9fbedf5a86c1d2e3a30a081bdf7b3fae
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-04-10 13:19:24 -0700

    remove unnecessary line continuation escapes

--- dashboard/db/migrate/20170410181336_add_teachercon_to_pd_facilitator_program_registrations.rb
+++ dashboard/db/migrate/20170410181336_add_teachercon_to_pd_facilitator_program_registrations.rb
@@ -8,9 +8,9 @@ class AddTeacherconToPdFacilitatorProgramRegistrations < ActiveRecord::Migration
 
     # index on user_id and new teachercon column. Give it a custom name, as the
     # default-generated one is longer than 64 characters
-    add_index :pd_facilitator_program_registrations, \
-      [:user_id, :teachercon], \
-      unique: true, \
+    add_index :pd_facilitator_program_registrations,
+      [:user_id, :teachercon],
+      unique: true,
       name: :index_pd_fac_prog_reg_on_user_id_and_teachercon
   end
 end
