a3908528109c5314b4d8d595201b8afd4bb08c48
Brendan Reville <brendan@code.org>
2017-04-10 18:30:55 -0700

    New header: single header for pegasus with hamburger
    No more two partials (one which for desktop, one shown for mobile) with headers for Pegasus.  Now it's a single header.  This makes it much easier to have the one hamburger menu for all widths of Pegasus.

--- shared/js/hamburger.js
+++ shared/js/hamburger.js
@@ -1,27 +1,5 @@
 $(function () {
 
-/*  var mainHeaderAreaAvailable;
-
-  if (typeof appOptions !== 'undefined') {
-    console.log(appOptions);
-    mainHeaderAreaAvailable = false;
-  } else {
-    console.log('no appOptions');
-    mainHeaderAreaAvailable = true;
-  }
-
-  if (!mainHeaderAreaAvailable) {
-    // we are going to force the show-always options to show.
-    // and we are going to disable the show-mobile options because they will be duplicates.
-    //$(".show-always").show();
-    //$(".show-mobile").hide();
-
-    $('.show-mobile').removeClass('show-mobile').addClass('show-always');
-
-  } else {
-    $('.headerlinks').animate({opacity: 1});
-  } */
-
   $('#nav-hamburger').click(function (e){
     $('#nav ul').slideToggle();
     e.preventDefault();
@@ -30,7 +8,7 @@ $(function () {
   $(document).on('click',function (e) {
     var nav = $('#nav');
     if (!nav.is(e.target)
-        && nav.has(e.target).length === 0){
+        && nav.has(e.target).length === 0) {
       nav.children('ul').hide();
       $('#nav-hamburger').removeClass('active');
     }
