276ec517ffa892cda80ef60fb9d721bf6bb3f328
Ram Kandasamy <ram@code.org>
2017-04-11 15:13:53 -0700

    Catch errors, clobber old data

--- bin/oneoff/backfill_data/backfill_section_start.rb
+++ bin/oneoff/backfill_data/backfill_section_start.rb
@@ -1,15 +1,19 @@
 #!/usr/bin/env ruby
-require_relative '../config/environment'
+require_relative '../../../dashboard/config/environment'
 
 ActiveRecord::Base.record_timestamps = false
-Section.where(first_activity_at: nil).find_in_batches do |group|
+Section.find_in_batches do |group|
   puts "Processing sections #{group.first.id} - #{group.last.id}"
   group.each do |section|
     first_activity_at = section.students.map do |student|
       user_scripts = student.user_scripts
       user_scripts = user_scripts.where(script_id: section.script_id) if section.script_id
-      user_scripts.minimum(:started_at)
+      user_scripts.where('started_at > ?', section.created_at).minimum(:started_at)
     end.compact.min
-    section.update!(first_activity_at: first_activity_at) if first_activity_at
+    begin
+      section.update!(first_activity_at: first_activity_at) if first_activity_at
+    rescue
+      puts "Failed to update section #{section.id}"
+    end
   end
 end
