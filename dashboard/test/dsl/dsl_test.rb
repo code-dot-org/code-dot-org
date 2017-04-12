7364705cd28e12585121e6409cb2270227916075
Brent Van Minnen <bjvanminnen@gmail.com>
2017-04-12 14:22:24 -0700

    fix test

--- dashboard/test/dsl/dsl_test.rb
+++ dashboard/test/dsl/dsl_test.rb
@@ -45,9 +45,9 @@ class DslTest < ActiveSupport::TestCase
       peer_reviews_to_complete: nil
     }
 
-    i18n_expected = {'en' => {'data' => {'script' => {'name' => {'test' => {'stage' => {
-      'Stage1' => 'Stage1',
-      'Stage2' => 'Stage2'
+    i18n_expected = {'en' => {'data' => {'script' => {'name' => {'test' => {'stages' => {
+      'Stage1' => {'name' => 'Stage1'},
+      'Stage2' => {'name' => 'Stage2'}
     }}}}}}}
     assert_equal expected, output
     assert_equal i18n_expected, i18n
