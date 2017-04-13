d5747889245841647f31a29247fd2f596fea81b1
Andrew Oberhardt <Andrew@Code.org>
2017-04-13 16:18:52 -0700

    Replace CSP workshop placeholder subject names with the real names.

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413183424) do
+ActiveRecord::Schema.define(version: 20170413194726) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
