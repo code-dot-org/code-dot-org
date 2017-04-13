bd2b7de6dd53f541f623b00cec4b8b2cde52310c
Caley Brock <caleybrock7@gmail.com>
2017-04-12 18:13:02 -0700

    Check that deleted sound returns with a 200

--- shared/test/test_sound_library_api.rb
+++ shared/test/test_sound_library_api.rb
@@ -1,6 +1,5 @@
 require_relative 'test_helper'
 require 'sound_library_api'
-require 'pp'
 
 class SoundLibraryTest < Minitest::Test
   include Rack::Test::Methods
@@ -13,7 +12,12 @@ class SoundLibraryTest < Minitest::Test
   def test_get_sound
     stubs(:cache_for).returns(nil)
     get '/api/v1/sound-library/test_sound.mp3'
-    pp last_response
+    assert last_response.ok?
+  end
+
+  def test_get_deleted_sound
+    stubs(:cache_for).returns(nil)
+    get '/api/v1/sound-library/test_sound.mp3'
     assert last_response.ok?
   end
 end
