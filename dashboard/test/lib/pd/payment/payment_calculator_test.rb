2dd3862ef518dacfb3fcdeae93485c8d586d45e7
Mehal Shah <mehal@code.org>
2017-04-14 15:59:55 -0700

    PR feedback

--- dashboard/test/lib/pd/payment/payment_calculator_test.rb
+++ dashboard/test/lib/pd/payment/payment_calculator_test.rb
@@ -4,19 +4,6 @@ module Pd::Payment
   class PaymentCalculatorTest < ActiveSupport::TestCase
     self.use_transactional_test_case = true
 
-    setup_all do
-      @regional_partner = create :regional_partner
-
-      @csf_workshop = create(:pd_ended_workshop, course: Pd::Workshop::COURSE_CSF, enrolled_and_attending_users: 20)
-      @csp_workshop = create(:pd_ended_workshop, course: Pd::Workshop::COURSE_CSP, subject: Pd::Workshop::SUBJECT_CSP_WORKSHOP_1, enrolled_and_attending_users: 20)
-
-      @csf_workshop.enrollments.map(&:user).each do |user|
-        PaymentCalculator::MINIMUM_PUZZLES_FOR_CSF_QUALIFICATION.times do
-          create(:user_level, user: user, best_result: ActivityConstants::PASSING_VALUES.sample)
-        end
-      end
-    end
-
     test 'Raise error if workshop is not ended' do
       workshop = create(:pd_workshop, course: Pd::Workshop::COURSE_CSF, enrolled_and_attending_users: 20)
       error = assert_raises(RuntimeError) do
@@ -27,7 +14,11 @@ module Pd::Payment
     end
 
     test 'Calculate CSF Workshop payment' do
-      assert_equal 1000, PaymentCalculator.instance.calculate(@csf_workshop)
+      workshop = create(:pd_ended_workshop, course: Pd::Workshop::COURSE_CSF, enrolled_and_attending_users: 20)
+
+      create_passed_levels(workshop.enrollments)
+
+      assert_equal 1000, PaymentCalculator.instance.calculate(workshop)
     end
 
     test 'Calculate CSF Workshop with insufficient attendees' do
@@ -38,7 +29,18 @@ module Pd::Payment
 
     test 'Calculate CSF Workshop with insufficient teachers who did puzzles' do
       insufficient_puzzles = create(:pd_ended_workshop, course: Pd::Workshop::COURSE_CSF, enrolled_and_attending_users: 20)
+      create_passed_levels(insufficient_puzzles.enrollments[0..5])
       assert_equal 0, PaymentCalculator.instance.calculate(insufficient_puzzles)
+    end
+
+    private
+
+    def create_passed_levels(enrollments)
+      enrollments.map(&:user).each do |user|
+        PaymentCalculator::MINIMUM_PUZZLES_FOR_CSF_QUALIFICATION.times do
+          create(:user_level, user: user, best_result: ActivityConstants::MINIMUM_PASS_RESULT)
+        end
+      end
     end
   end
 end
