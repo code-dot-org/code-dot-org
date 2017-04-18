f0a42af4029cd9c3475fdec2db8a3bece9dc1cbe
ashercodeorg <asher@code.org>
2017-04-18 09:47:37 -0500

    Gate workshop admin access on UserPermission::WORKSHOP_ADMIN, not User.admin.

--- dashboard/app/models/ability.rb
+++ dashboard/app/models/ability.rb
@@ -121,6 +121,15 @@ class Ability
         can :read, :pd_workshop_summary_report
         can :read, :pd_teacher_attendance_report
       end
+
+      if user.permission?(UserPermission::WORKSHOP_ADMIN)
+        can :manage, Pd::Workshop
+        can :manage, Pd::Workshop
+        can :manage, Pd::CourseFacilitator
+        can :manage, :workshop_organizer_survey_report
+        can :manage, :pd_workshop_summary_report
+        can :manage, :pd_teacher_attendance_report
+      end
     end
 
     # Override Script and ScriptLevel.
