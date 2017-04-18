1226e8cf7e03e700fbefcf3aca63e29e8e8b9755
Brent Van Minnen <bjvanminnen@gmail.com>
2017-04-18 16:48:30 -0700

    add a test

---
+++ dashboard/test/models/user_id_replace_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UserIdReplaceTest < ActiveSupport::TestCase
+  test "replaces <user_id/> with user's id" do
+    dsl_text = <<DSL
+name 'user_id_replace'
+title 'title for user_id_replace'
+markdown 'this is the markdown for <user_id/>'
+DSL
+    level = UserIdReplace.create_from_level_builder({}, {name: 'my_user_id_replace', dsl_text: dsl_text})
+
+    user1 = create :user
+    user2 = create :user
+    properties1 = level.properties_with_replaced_markdown(user1)
+    properties2 = level.properties_with_replaced_markdown(user2)
+
+    assert_equal("this is the markdown for #{user1.id}", properties1['markdown'])
+    assert_equal("this is the markdown for #{user2.id}", properties2['markdown'])
+
+    # make sure we didn't lose other properties
+    assert_equal('title for user_id_replace', properties1['title'])
+  end
+end
