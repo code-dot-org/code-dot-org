e89b048c32da56c7f8760d8c01244718d5e63b49
Erin <erin.bond@code.org>
2017-04-17 20:16:33 -0700

    RecentCourses and SetUpMessage components (#14368)

---
+++ apps/src/templates/teacherDashboard/SetUpMessage.story.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SetUpMessage from './SetUpMessage';
+
+export default storybook => {
+  return storybook
+    .storiesOf('SetUpMessage', module)
+    .addStoryTable([
+      {
+        name: 'Set Up Message for Courses',
+        description: `Information box if the teacher doesn't have any courses yet`,
+        story: () => (
+          <SetUpMessage type="courses"/>
+        )
+      },
+      {
+        name: 'Set Up Message for Sections',
+        description: `Information box if the teacher doesn't have any sections yet`,
+        story: () => (
+          <SetUpMessage type="sections"/>
+        )
+      },
+    ]);
+};
