0a9c9fd5349325e7853e58cf5da8b486c75fa7f9
David Bailey <davidsbailey@users.noreply.github.com>
2017-04-21 14:32:04 -0700

    only capture playlab thumbnails once per minute

--- apps/src/studio/studioThumbnail.js
+++ apps/src/studio/studioThumbnail.js
@@ -3,11 +3,26 @@ import {createThumbnail} from '../util/thumbnail';
 import {canvasFromImage, canvasToBlob, imageFromURI, svgToDataURI} from '../imageUtils';
 import {getStore} from '../redux';
 
+// Minimum time to wait after capturing a thumbnail image before capturing
+// another thumbnail.
+const MIN_CAPTURE_INTERVAL_MS = 60000;
+
+/**
+ * @type {number} The last time at which a screenshot capture was attempted.
+ */
+let lastCaptureTimeMs = 0;
+
 export function captureScreenshot() {
   const {isShareView, isEmbedView} = getStore().getState().pageConstants;
   if (!project.getCurrentId() || !project.isOwner || isShareView || isEmbedView) {
     return;
   }
+
+  // Skip capturing a screenshot if we just captured one recently.
+  if (Date.now() - lastCaptureTimeMs < MIN_CAPTURE_INTERVAL_MS) {
+    return;
+  }
+  lastCaptureTimeMs = Date.now();
 
   const svg = document.getElementById('svgStudio');
   if (!svg) {
