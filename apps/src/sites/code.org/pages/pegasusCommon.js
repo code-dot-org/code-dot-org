3889254e2f5a9a8a0f8f4e2185ccd781eafc0b70
Caley Brock <caleybrock7@gmail.com>
2017-04-24 16:50:38 -0700

    Add hamburger.js to pegasusCommon entry point

--- apps/src/sites/code.org/pages/pegasusCommon.js
+++ apps/src/sites/code.org/pages/pegasusCommon.js
@@ -1,3 +1,5 @@
 import {isUnsupportedBrowser} from '@cdo/apps/util/browser-detector';
+import {setupHamburgerMenu} from '@cdo/apps/hamburger/hamburger.js';
 
 window.isUnsupportedBrowser = isUnsupportedBrowser;
+setupHamburgerMenu();
