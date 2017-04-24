23b5c6f3de57fe106a63e441ec7760f6cbfb2c35
David Bailey <davidsbailey@users.noreply.github.com>
2017-04-24 10:06:55 -0700

    remove unused minCaptureIntervalMs params [test eyes]

--- apps/src/applab/constants.js
+++ apps/src/applab/constants.js
@@ -15,7 +15,3 @@ export const IMPORT_SCREEN = "Import screen...";
 // 300 ticks equates to approximately 1-1.5 seconds in apps that become idle
 // after the first few ticks, or 10-15 seconds in apps that draw constantly.
 export const CAPTURE_TICK_COUNT = 300;
-
-// Minimum time to wait after capturing a thumbnail image before capturing
-// another thumbnail.
-export const MIN_CAPTURE_INTERVAL_MS = 60000;
