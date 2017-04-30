c4147193fb13a23b70445465711e55870e4f5656
David Bailey <davidsbailey@users.noreply.github.com>
2017-04-30 12:57:06 -0700

    a component to switch between private and public galleries, hidden behind an experiment flag

--- apps/src/sites/studio/pages/projects/index.js
+++ apps/src/sites/studio/pages/projects/index.js
@@ -1,10 +1,25 @@
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GallerySwitcher, {Galleries} from '@cdo/apps/templates/projects/GallerySwitcher';
 import PublicGallery from '@cdo/apps/templates/projects/PublicGallery';
+import experiments from '@cdo/apps/util/experiments';
 
 $(document).ready(() => {
-  const element = document.getElementById('public-gallery');
+  if (experiments.isEnabled('publicGallery')) {
+    const gallerySwitcher = document.getElementById('gallery-switcher');
+    ReactDOM.render(
+      <GallerySwitcher
+        initialGallery={Galleries.PRIVATE}
+        showGallery={showGallery}
+      />, gallerySwitcher);
 
-  ReactDOM.render(<PublicGallery/>, element);
+    const publicGallery = document.getElementById('public-gallery');
+    ReactDOM.render(<PublicGallery/>, publicGallery);
+  }
 });
+
+function showGallery(gallery) {
+  $('#angular-my-projects-wrapper').toggle(gallery === Galleries.PRIVATE);
+  $('#public-gallery-wrapper').toggle(gallery === Galleries.PUBLIC);
+}
