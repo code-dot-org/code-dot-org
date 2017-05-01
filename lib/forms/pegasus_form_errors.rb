2fb2cade7146d228c8e85825463c2607402bb19d
Andrew Oberhardt <Andrew@Code.org>
2017-05-01 16:02:26 -0700

    Extract Pegasus Form validation into lib

---
+++ lib/forms/pegasus_form_errors.rb
@@ -0,0 +1,29 @@
+module PegasusFormErrors
+  class FormError < ArgumentError
+    attr_reader :errors
+
+    def self.detect_errors(data)
+      errors = {}
+      data.each_pair do |key, value|
+        errors[key] = [value.message] if value.class == FieldError
+      end
+      errors
+    end
+
+    def initialize(kind, errors, logger = nil)
+      @kind = kind
+      @errors = errors
+
+      logger.warn "FormError[#{@kind}]: #{@errors.to_json}" if logger
+    end
+  end
+
+  class FieldError
+    attr_accessor :value, :message
+
+    def initialize(value, message)
+      @value = value
+      @message = message
+    end
+  end
+end
