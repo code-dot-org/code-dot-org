a54c8af02cc8cd707a4a01d0edcd41c3647a7c2b
David Bailey <davidsbailey@users.noreply.github.com>
2017-05-01 19:55:23 -0700

    add dashboard v1 api route for listing public gallery project data

---
+++ dashboard/app/controllers/api/v1/projects/public_gallery_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Projects::PublicGalleryController < ApplicationController
+  # GET /api/v1/projects/gallery/<project_type>/<limit>
+  def index
+    render json: ProjectsList.fetch_published_projects(params[:project_type], params[:limit], params[:offset])
+  rescue ArgumentError => e
+    render json: {error: e.message}, status: :bad_request
+  end
+end
