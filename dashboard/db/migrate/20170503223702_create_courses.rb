baa7d23365d4d82acd4a15821cde8fa283a0ee3a
Brent Van Minnen <bjvanminnen@gmail.com>
2017-05-03 16:06:38 -0700

    new courses table

---
+++ dashboard/db/migrate/20170503223702_create_courses.rb
@@ -0,0 +1,15 @@
+class CreateCourses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.text :properties
+      t.integer :plc_course_id, index: true, foreign_key: true
+
+      t.timestamps
+    end
+
+    Plc::Course.all.each do |plc_course|
+      Course.create!(name: plc_course.name, plc_course_id: plc_course.id)
+    end
+  end
+end
