a22813ca48c41aa83a45c4bc4ff53c0da455748f
Paul Carduner <paul@carduner.net>
2017-05-03 16:07:06 -0700

    Add tests for codegen.evalWithEvents
    There were some interesting cases to test in here!

--- apps/test/unit/codegenTest.js
+++ apps/test/unit/codegenTest.js
@@ -22,6 +22,106 @@ describe("codegen", function () {
     });
   });
 
+  describe("evalWithEvents function", () => {
+    let hooks, globals;
+    beforeEach(() => {
+      globals = {
+        a: sinon.spy(),
+        b: sinon.spy(),
+        c: sinon.spy(),
+      };
+    });
+    it("will return an empty array of hooks if no events are provided", () => {
+      expect(codegen.evalWithEvents({}, {})).to.deep.equal([]);
+    });
+
+    describe("when given event handlers that accept arguments", () => {
+      beforeEach(() => {
+        hooks = codegen.evalWithEvents(
+          globals,
+          {
+            takesArgs: {code: 'c(foo*2, bar/3)', args: ['foo', 'bar']},
+          });
+      });
+      it("will pass the args provided to the hook along to the event handler", () => {
+        hooks[0].func(5, 6);
+        expect(globals.c).to.have.been.calledWith(10, 2);
+      });
+    });
+
+    describe("when given event handlers that return values", () => {
+      beforeEach(() => {
+        hooks = codegen.evalWithEvents(
+          globals,
+          {
+            returnsValues: {code: 'return 5'},
+          });
+      });
+      it("will return the values returned by the event handler back to the caller of the hook", () => {
+        expect(hooks[0].func()).to.equal(5);
+      });
+    });
+
+    describe("when given additional code to evaluate", () => {
+      beforeEach(() => {
+        hooks = codegen.evalWithEvents(
+          globals,
+          {
+            someEvent: {code: 'b()'},
+          },
+          'a();',
+        );
+      });
+
+      it("will evaluate that code immediately", () => {
+        expect(globals.a).to.have.been.called;
+      });
+
+      it("will continue waiting for the various hooks to be called", () => {
+        hooks[0].func();
+        expect(globals.b).to.have.been.called;
+      });
+
+      it("will throw an unexpected token exception when given code that does not end with a semicolon", () => {
+        expect(() => codegen.evalWithEvents(globals, {someEvent: {code: 'b()'}}, 'a()')).to.throw('Unexpected token');
+      });
+    });
+
+    describe("when given a set of events handlers that call the provided global functions", () => {
+      beforeEach(() => {
+        hooks = codegen.evalWithEvents(
+          globals,
+          {
+            someEvent: {code: ['a()', 'b()']},
+            someOtherEvent: {code: 'c()'},
+          });
+      });
+
+      it("will return a hook for each event handler of each event type that was given", () => {
+        expect(hooks.length).to.equal(3);
+      });
+
+      it("each returned hook will have a name property corresponding to the event name it is connected to", () => {
+        expect(hooks.map(hook => hook.name)).to.deep.equal([
+          'someEvent',
+          'someEvent',
+          'someOtherEvent',
+        ]);
+      });
+
+      it("will call the appropriate event handlers when those hooks get called", () => {
+        expect(hooks[0].name).to.equal('someEvent');
+        hooks[0].func();
+        expect(globals.a).to.have.been.called;
+        expect(globals.b).not.to.have.been.called;
+        expect(globals.c).not.to.have.been.called;
+        hooks[2].func();
+        expect(globals.c).to.have.been.called;
+      });
+    });
+
+  });
+
   describe("evalWith function", () => {
     let options;
     beforeEach(() => {
