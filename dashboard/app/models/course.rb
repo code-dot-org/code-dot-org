8e28275355c0e3bdd439e3101da5a1677e3188ec
Brent Van Minnen <bjvanminnen@gmail.com>
2017-05-03 22:56:57 -0700

    create association between Course and Plc::Course

--- dashboard/app/models/course.rb
+++ dashboard/app/models/course.rb
@@ -14,4 +14,6 @@
 #
 
 class Course < ApplicationRecord
+  # Some Courses will have an associated Plc::Course, most will not
+  belongs_to :plc_course, class_name: 'Plc::Course'
 end
--- dashboard/app/models/plc/course.rb
+++ dashboard/app/models/plc/course.rb
@@ -16,6 +16,7 @@
 class Plc::Course < ActiveRecord::Base
   has_many :plc_enrollments, class_name: '::Plc::UserCourseEnrollment', foreign_key: 'plc_course_id', dependent: :destroy
   has_many :plc_course_units, class_name: '::Plc::CourseUnit', foreign_key: 'plc_course_id', dependent: :destroy
+  has_one :course, class_name: '::Course', foreign_key: 'plc_course_id', dependent: :destroy, required: true
 
   def get_url_name
     name.downcase.tr(' ', '-')
