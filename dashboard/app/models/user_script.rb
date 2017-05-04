ba89c619ee4b03b911a53d7d40e6db6f71b55ffe
ashercodeorg <asher@code.org>
2017-05-04 12:11:55 -0500

    Guard against deleted user in UserScript#check_completed?.

--- dashboard/app/models/user_script.rb
+++ dashboard/app/models/user_script.rb
@@ -26,10 +26,11 @@ class UserScript < ActiveRecord::Base
     Script.get_from_cache(script_id)
   end
 
+  # @return [Boolean] Whether the user completed the script, e.g., if there are no more progression
+  #   levels to be completed (note unplugged levels are an example of non-progress levels). Also
+  #   returns false if the associated user has been soft-deleted.
   def check_completed?
-    # the script is completed if there are no more "progression levels" to be completed
-    # (unplugged levels are not progression levels, for one)
-    user.completed_progression_levels? script
+    user && user.completed_progression_levels?(script)
   end
 
   def empty?
