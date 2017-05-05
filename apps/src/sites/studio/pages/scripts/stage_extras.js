37e18ad5a6955ed4613455139c0a04acc600213f
Josh Lory <josh.lory@code.org>
2017-05-05 13:05:55 -0700

    Add extracted `CreateSomething` component to new stage extras page

---
+++ apps/src/sites/studio/pages/scripts/stage_extras.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CreateSomething from '@cdo/apps/code-studio/components/stageExtras/CreateSomething';
+
+ReactDOM.render(<CreateSomething />, document.querySelector('.create-something'));
