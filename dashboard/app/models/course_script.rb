bac6bb2659d8ce4ff28bf3da246f206597166511
Brent Van Minnen <bjvanminnen@gmail.com>
2017-05-05 16:47:28 -0700

    CourseScripts model

---
+++ dashboard/app/models/course_script.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: course_scripts
+#
+#  id        :integer          not null, primary key
+#  course_id :integer          not null
+#  script_id :integer          not null
+#  position  :integer
+#
+# Indexes
+#
+#  index_course_scripts_on_course_id  (course_id)
+#  index_course_scripts_on_script_id  (script_id)
+#
+
+class CourseScript < ApplicationRecord
+  belongs_to :course
+  belongs_to :script
+end
