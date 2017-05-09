42f90c2ec5a23c2e7d6b65ea8c2489c81aa762a9
Brent Van Minnen <bjvanminnen@gmail.com>
2017-05-09 12:38:51 -0700

    fill out data we send to client

--- apps/src/templates/courseOverview/CourseOverview.js
+++ apps/src/templates/courseOverview/CourseOverview.js
@@ -3,20 +3,20 @@ import { ViewType } from '@cdo/apps/code-studio/stageLockRedux';
 
 export default class CourseOverview extends Component {
   static propTypes = {
-    friendlyName: PropTypes.string.isRequired,
-    studentDescription: PropTypes.string,
-    teacherDescription: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    descriptionStudent: PropTypes.string,
+    descriptionTeacher: PropTypes.string,
     viewAs: React.PropTypes.oneOf(Object.values(ViewType)).isRequired,
   };
 
   render() {
-    // TODO: h1 component
-    const { friendlyName, studentDescription, teacherDescription, viewAs } = this.props;
+    const { title, descriptionStudent, descriptionTeacher, viewAs } = this.props;
+    // TODO: create an h1 component instead of depending on CSS styling for h1
     return (
       <div>
-        <h1>{friendlyName}</h1>
+        <h1>{title}</h1>
         <div>
-          {viewAs === ViewType.Student ? studentDescription : teacherDescription}
+          {viewAs === ViewType.Student ? descriptionStudent : descriptionTeacher}
         </div>
       </div>
     );
--- dashboard/app/models/course.rb
+++ dashboard/app/models/course.rb
@@ -20,9 +20,14 @@ class Course < ApplicationRecord
 
   def summarize
     {
-      # TODO: i18n
       name: name,
-      scripts: course_scripts.map(&:script).map {|script| script.summarize(false)}
+      title: I18n.t("data.course.name.#{name}.title"),
+      description_student: I18n.t("data.course.name.#{name}.description_student"),
+      description_teacher: I18n.t("data.course.name.#{name}.description_teacher"),
+      scripts: course_scripts.map(&:script).map do |script|
+        include_stages = false
+        script.summarize(include_stages).merge!(script.summarize_i18n(include_stages))
+      end
     }
   end
 end
