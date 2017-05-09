2ca502fe34c77e2702110800b193691a5980e3b8
Andrew Oberhardt <Andrew@Code.org>
2017-05-09 11:48:15 -0700

    Remove now-empty SectionHelpers module, and use CodeGeneration directly from Section

--- lib/cdo/section_helpers.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'code_generation'
-
-module SectionHelpers
-  # @return [String] A semi-random vowelless string of length six.
-  # @param reject_if [Proc] return true if a code should be rejected and replaced by a new one (optional)
-  def self.random_code(reject_if: nil)
-    CodeGeneration.random_code(6, reject_if)
-  end
-end
