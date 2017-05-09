532733eb9f5228f848a5711f1e257458e39ea2f0
Mehal Shah <mehal@code.org>
2017-05-09 13:50:46 -0700

    Only require rails once

--- dashboard/test/ui/support/dashboard_helpers.rb
+++ dashboard/test/ui/support/dashboard_helpers.rb
@@ -1,7 +1,11 @@
 module DashboardHelpers
+  @@required_called = false
+
   # Requires the full rails environment. Use sparingly, known to take 20-30s.
   def require_rails_env
+    return if @@required_called = true
     require File.expand_path('../../../../config/environment.rb', __FILE__)
+    @@required_called = true
   end
 end
 
