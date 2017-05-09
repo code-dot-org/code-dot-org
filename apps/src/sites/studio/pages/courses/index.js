295b7a3466e42e8055ab2be6a5a5473efd674fbb
Brent Van Minnen <bjvanminnen@gmail.com>
2017-05-09 16:00:55 -0700

    clearer names

--- apps/src/sites/studio/pages/courses/index.js
+++ apps/src/sites/studio/pages/courses/index.js
@@ -5,17 +5,17 @@ import CourseOverview from '@cdo/apps/templates/courseOverview/CourseOverview';
 $(document).ready(showCourseOverview);
 
 function showCourseOverview() {
-  const courseData = document.querySelector('script[data-coursedata]');
-  const configCourse = JSON.parse(courseData.dataset.coursedata);
+  const scriptData = document.querySelector('script[data-course-summary]');
+  const courseSummary = JSON.parse(scriptData.dataset.courseSummary);
 
   // Eventually we want to do this all via redux
   ReactDOM.render(
     <CourseOverview
-      title={configCourse.title}
-      descriptionStudent={configCourse.description_student}
-      descriptionTeacher={configCourse.description_teacher}
+      title={courseSummary.title}
+      descriptionStudent={courseSummary.description_student}
+      descriptionTeacher={courseSummary.description_teacher}
       viewAs="Teacher"
-      scripts={configCourse.scripts}
+      scripts={courseSummary.scripts}
     />,
   document.getElementById('course_index'));
 }
