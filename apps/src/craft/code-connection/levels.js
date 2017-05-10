e88757e459ab293339868e0d7a79fe5939f02a6b
Josh Lory <josh.lory@code.org>
2017-05-10 12:53:18 -0700

    Add `Craft` variant for the code-connection extension

---
+++ apps/src/craft/code-connection/levels.js
@@ -0,0 +1,19 @@
+import { createToolbox } from '../../block_utils';
+
+function craftBlock(type) {
+  return block("craft_" + type);
+}
+
+function block(type) {
+  return '<block type="' + type + '"></block>';
+}
+
+const log = craftBlock('log');
+
+module.exports = {
+  'custom': {
+    'requiredBlocks': [],
+    'freePlay': false,
+    'toolbox': createToolbox(log + block('text'))
+  }
+};
