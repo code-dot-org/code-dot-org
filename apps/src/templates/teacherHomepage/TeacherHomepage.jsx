81598d7be8bf5a2c6c10f9d3bf42d311bd7e57db
Erin <erin.bond@code.org>
2017-05-10 15:13:42 -0700

    Resources container (#14957)
    * Rename GradientNavCard to ResourceCard
    
    * Make a TeacherResources component that renders ResourceCards to tidy up the TeacherHomepage component
    
    * Use TeacherResources on TeacherHomepage
    
    * Update story for TeacherResource

--- apps/src/templates/teacherHomepage/TeacherHomepage.jsx
+++ apps/src/templates/teacherHomepage/TeacherHomepage.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import AnnouncementsCollapsible from './AnnouncementsCollapsible';
 import RecentCoursesCollapsible from './RecentCoursesCollapsible';
 import ManageSectionsCollapsible from './ManageSectionsCollapsible';
-import CollapsibleSection from './CollapsibleSection';
-import GradientNavCard from './GradientNavCard';
-import i18n from "@cdo/locale";
+import TeacherResources from './TeacherResources';
 
 const TeacherHomepage = React.createClass({
   propTypes: {
@@ -19,41 +17,9 @@ const TeacherHomepage = React.createClass({
     return (
       <div>
         <AnnouncementsCollapsible announcements={announcements}/>
-
         <RecentCoursesCollapsible courses={courses}/>
-
         <ManageSectionsCollapsible sections={sections}/>
-
-        <CollapsibleSection header={i18n.resources()}>
-          <GradientNavCard
-            title={i18n.teacherCommunity()}
-            description={i18n.teacherCommunityDescription()}
-            image="../../static/navcard-placeholder.png"
-            buttonText={i18n.joinCommunity()}
-            link="https://forum.code.org"
-          />
-          <GradientNavCard
-            title={i18n.professionalLearning()}
-            description={i18n.professionalLearningDescription()}
-            image="../../static/navcard-placeholder.png"
-            buttonText={i18n.learnMore()}
-            link="/my-professional-learning"
-          />
-          <GradientNavCard
-            title={i18n.standardsAndFramework()}
-            description={i18n.standardsAndFrameworkDescription()}
-            image="../../static/navcard-placeholder.png"
-            buttonText={i18n.reviewDocuments()}
-            link="https://code.org/teacher-dashboard#/plan"
-          />
-          <GradientNavCard
-            title={i18n.findGuestSpeaker()}
-            description={i18n.findGuestSpeakerDescription()}
-            image="../../static/navcard-placeholder.png"
-            buttonText={i18n.inspireStudents()}
-            link="https://code.org/volunteer/local"
-          />
-        </CollapsibleSection>
+        <TeacherResources/>
       </div>
     );
   }
