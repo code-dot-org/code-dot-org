605d5828774630538187ad921cd4fb0d530590e7
Caley Brock <caleybrock7@gmail.com>
2017-05-11 13:37:18 -0700

    Show single app view

--- apps/src/templates/projects/ProjectCardGrid.jsx
+++ apps/src/templates/projects/ProjectCardGrid.jsx
@@ -21,42 +21,109 @@ const ProjectCardGrid = React.createClass({
     galleryType: PropTypes.oneOf(['personal', 'class', 'public']).isRequired
   },
 
+  getInitialState() {
+    return {
+      showAll: true,
+      showApp: ''
+    };
+  },
+
+  onSelectApp(appType) {
+    this.setState({showAll: false, showApp: appType});
+  },
+
+  viewAllProjects() {
+    this.setState({showAll: true, showApp: ''});
+  },
+
   render() {
     const { projectLists } = this.props;
+    const numProjects = this.state.showAll ? 4 : 12;
 
     return (
       <div style={styles.grid}>
-        <ProjectAppTypeArea
-          labName={i18n.projectTypeApplab()}
-          labViewMoreString="View more App Lab projects"
-          projectList={projectLists.applab}
-          numProjectsToShow={4}
-          galleryType={this.props.galleryType}
-        />
+        {(this.state.showAll) &&
+          <div>
+            <ProjectAppTypeArea
+              labName={i18n.projectTypeApplab()}
+              labViewMoreString="View more App Lab projects"
+              projectList={projectLists.applab}
+              numProjectsToShow={numProjects}
+              galleryType={this.props.galleryType}
+              navigateFunction={this.onSelectApp}
+            />
+            <ProjectAppTypeArea
+              labName={i18n.projectTypeGamelab()}
+              labViewMoreString="View more Game Lab projects"
+              projectList={projectLists.gamelab}
+              numProjectsToShow={numProjects}
+              galleryType={this.props.galleryType}
+              navigateFunction={this.onSelectApp}
+            />
+            <ProjectAppTypeArea
+              labName={i18n.projectTypeArtist()}
+              labViewMoreString="View more Artist projects"
+              projectList={projectLists.artist}
+              numProjectsToShow={numProjects}
+              galleryType={this.props.galleryType}
+              navigateFunction={this.onSelectApp}
+            />
+            <ProjectAppTypeArea
+              labName={i18n.projectTypePlaylab()}
+              labViewMoreString="View more Play Lab projects"
+              projectList={projectLists.playlab}
+              numProjectsToShow={numProjects}
+              galleryType={this.props.galleryType}
+              navigateFunction={this.onSelectApp}
+            />
+          </div>
+        }
 
-        <ProjectAppTypeArea
-          labName={i18n.projectTypeGamelab()}
-          labViewMoreString="View more Game Lab projects"
-          projectList={projectLists.gamelab}
-          numProjectsToShow={4}
-          galleryType={this.props.galleryType}
-        />
+        {(!this.state.showAll) &&
+          <div>
+            {this.state.showApp === 'applab' &&
+              <ProjectAppTypeArea
+                labName="All App Lab Projects"
+                labViewMoreString="View all projects"
+                projectList={projectLists.applab}
+                numProjectsToShow={numProjects}
+                galleryType={this.props.galleryType}
+                navigateFunction={this.viewAllProjects}
+              />
+            }
+            {this.state.showApp === 'gamelab' &&
+              <ProjectAppTypeArea
+                labName="All Game Lab Projects"
+                labViewMoreString="View all projects"
+                projectList={projectLists.gamelab}
+                numProjectsToShow={numProjects}
+                galleryType={this.props.galleryType}
+                navigateFunction={this.viewAllProjects}
+              />
+            }
+            {this.state.showApp === 'artist' &&
+              <ProjectAppTypeArea
+                labName="All Artist Projects"
+                labViewMoreString="View all projects"
+                projectList={projectLists.artist}
+                numProjectsToShow={numProjects}
+                galleryType={this.props.galleryType}
+                navigateFunction={this.viewAllProjects}
+              />
+            }
+            {this.state.showApp === 'playlab' &&
+              <ProjectAppTypeArea
+                labName="All Play Lab Projects"
+                labViewMoreString="View all projects"
+                projectList={projectLists.playlab}
+                numProjectsToShow={numProjects}
+                galleryType={this.props.galleryType}
+                navigateFunction={this.viewAllProjects}
+              />
+            }
+          </div>
+        }
 
-        <ProjectAppTypeArea
-          labName={i18n.projectTypeArtist()}
-          labViewMoreString="View more Artist projects"
-          projectList={projectLists.gamelab}
-          numProjectsToShow={4}
-          galleryType={this.props.galleryType}
-        />
-
-        <ProjectAppTypeArea
-          labName={i18n.projectTypePlaylab()}
-          labViewMoreString="View more Play Lab projects"
-          projectList={projectLists.playlab}
-          numProjectsToShow={4}
-          galleryType={this.props.galleryType}
-        />
       </div>
     );
   }
