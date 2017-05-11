bf7ace27120b9e2ed9474492a7277fecd043d858
Erin <erin.bond@code.org>
2017-05-11 11:48:34 -0700

    Announcements Carousel component (#14989)
    * Announcements Carousel component that lets the user click through multiple announcements
    
    * Refactor AnnouncementsCarousel onClick functions
    
    * Add light gray hover to arrow icons

--- apps/src/templates/teacherHomepage/AnnouncementsCollapsible.story.jsx
+++ apps/src/templates/teacherHomepage/AnnouncementsCollapsible.story.jsx
@@ -16,7 +16,35 @@ export default storybook => {
                 buttonText: "Go Beyond",
                 description: "Go Beyond an Hour of Code and explore computer science concepts with your students every week. Code.org offers curriculum, lesson plans, high quality professional learning programs, and tons of great tools for all grade levels - and it's free. No experience required - find the next step that's right for your classroom.",
                 link: "to wherever"
-              }
+              },
+            ]}
+          />
+        )
+      },
+      {
+        name: 'Announcement Collapsible - 3 announcements',
+        description: 'This is an example of of the Announcements section for the teacher homepage.',
+        story: () => (
+          <AnnouncementsCollapsible
+            announcements={[
+              {
+                heading: "Go beyond an Hour of Code",
+                buttonText: "Go Beyond",
+                description: "Go Beyond an Hour of Code and explore computer science concepts with your students every week. Code.org offers curriculum, lesson plans, high quality professional learning programs, and tons of great tools for all grade levels - and it's free. No experience required - find the next step that's right for your classroom.",
+                link: "to wherever"
+              },
+              {
+                heading: "Go way beyond an Hour of Code",
+                buttonText: "Go Beyond",
+                description: "Go Beyond an Hour of Code and explore computer science concepts with your students every week. Code.org offers curriculum, lesson plans, high quality professional learning programs, and tons of great tools for all grade levels - and it's free. No experience required - find the next step that's right for your classroom.",
+                link: "to wherever"
+              },
+              {
+                heading: "Go way, way beyond an Hour of Code",
+                buttonText: "Go Beyond",
+                description: "Go Beyond an Hour of Code and explore computer science concepts with your students every week. Code.org offers curriculum, lesson plans, high quality professional learning programs, and tons of great tools for all grade levels - and it's free. No experience required - find the next step that's right for your classroom.",
+                link: "to wherever"
+              },
             ]}
           />
         )
