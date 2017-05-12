8ba9444eca5125caf02aec06dcf4e6d529d2efd9
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-05-11 18:23:13 -0700

    add new Pd::WorkshopSurvey serverside components

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -338,6 +338,8 @@ Dashboard::Application.routes.draw do
 
       post :facilitator_program_registrations, to: 'facilitator_program_registrations#create'
       post :regional_partner_program_registrations, to: 'regional_partner_program_registrations#create'
+
+      post :workshop_surveys, to: 'workshop_surveys#create'
     end
   end
 
@@ -372,6 +374,8 @@ Dashboard::Application.routes.draw do
     get 'workshop_materials/:enrollment_code', action: 'new', controller: 'workshop_material_orders'
     post 'workshop_materials/:enrollment_code', action: 'create', controller: 'workshop_material_orders'
     get 'workshop_materials', action: 'admin_index', controller: 'workshop_material_orders'
+
+    get 'workshop_survey/:enrollment_code', action: 'new', controller: 'workshop_survey', as: 'new_workshop_survey'
 
     get 'generate_csf_certificate/:enrollment_code', controller: 'csf_certificate', action: 'generate_certificate'
     get 'generate_workshop_certificate/:enrollment_code', controller: 'workshop_certificate', action: 'generate_certificate'
