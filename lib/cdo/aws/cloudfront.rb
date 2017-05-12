3463a7d4f2fa1661009347ed9deda4d409270ea6
Will Jordan <will@code.org>
2017-05-12 11:58:27 -0700

    migrate CloudFront distributions to CloudFormation Remove code to manually create/update CloudFront distributions via the AWS SDK for Ruby.

--- lib/cdo/aws/cloudfront.rb
+++ lib/cdo/aws/cloudfront.rb
@@ -6,7 +6,7 @@ require_relative '../../../cookbooks/cdo-varnish/libraries/http_cache'
 require_relative '../../../cookbooks/cdo-varnish/libraries/helpers'
 require 'active_support/core_ext/object/try'
 
-# Manages application-specific configuration and deployment of AWS CloudFront distributions.
+# Manages application-specific configuration of AWS CloudFront distributions.
 module AWS
   class CloudFront
     ALLOWED_METHODS = %w(HEAD DELETE POST GET OPTIONS PUT PATCH).freeze
@@ -69,20 +69,6 @@ module AWS
       puts "CONFIG: #{CONFIG}"
     end
 
-    # Manually sorts the array-types in the distribution config object,
-    # so we can compare against the existing config to detect whether an update is needed.
-    def self.sort_config!(config)
-      config[:cache_behaviors][:items].sort_by! {|item| item[:path_pattern]}
-      config[:cache_behaviors][:items].concat([config[:default_cache_behavior]]).each do |item|
-        item[:forwarded_values][:headers][:items].sort!
-        name = item[:forwarded_values][:cookies][:whitelisted_names]
-        name[:items].sort! if name
-      end
-      config[:aliases][:items].sort!
-      config[:origins][:items].sort_by! {|o| o[:id]}
-      config[:custom_error_responses][:items].sort_by! {|e| e[:error_code]}
-    end
-
     # File path for caching mappings from CloudFront Distribution id to alias CNAMEs.
     # Reduces number of required API calls to ListDistributions.
     CLOUDFRONT_ALIAS_CACHE = pegasus_dir 'cache', 'cloudfront_aliases.json'
@@ -101,7 +87,7 @@ module AWS
       )
       invalidations = CONFIG.keys.map do |app|
         hostname = CDO.method("#{app}_hostname").call
-        id = get_distribution_id_with_retry(cloudfront, hostname)
+        id = get_distribution_id(cloudfront, hostname)
         invalidation = cloudfront.create_invalidation(
           {
             distribution_id: id,
@@ -126,213 +112,18 @@ module AWS
       end
     end
 
-    # Creates or updates the CloudFront distribution based on the current configuration.
-    # Calls to this method should be idempotent, however CloudFront distribution updates can take ~15 minutes to finish.
-    #
-    # Ref: http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/HowToUpdateDistribution.html
-    # "Your changes don't propagate to every edge location instantaneously; propagation to all edge locations can take
-    #  15 minutes. When propagation is complete, the status of your distribution changes from InProgress to Deployed.
-    #  While CloudFront is propagating your changes to edge locations, we cannot determine whether a given edge
-    #  location is serving your content based on the previous configuration or the new configuration."
-    def self.create_or_update
-      cloudfront = Aws::CloudFront::Client.new(
-        logger: Logger.new(dashboard_dir('log/cloudfront.log')),
-        log_level: :debug,
-        http_wire_trace: true
-      )
-      ids = CONFIG.keys.map do |app|
-        hostname = CDO.method("#{app}_hostname").call
-        id, distribution_config = get_distribution_config(cloudfront, hostname)
-        if distribution_config
-          old_config = distribution_config.distribution_config.to_hash
-          new_config = config(app, distribution_config.distribution_config.caller_reference)
-          sort_config! old_config
-          sort_config! new_config
-          if old_config == new_config
-            puts "#{app} distribution not modified."
-          else
-            cloudfront.update_distribution(
-              id: id,
-              if_match: distribution_config.etag,
-              distribution_config: config(app, distribution_config.distribution_config.caller_reference)
-            )
-            puts "#{app} distribution updated!"
-          end
-        else
-          resp = cloudfront.create_distribution(
-            distribution_config: config(app)
-          )
-          id = resp.distribution.id
-          puts "#{app} distribution created!"
-        end
-        [app, id]
+    # Returns the distribution ID matching a given hostname.
+    def self.get_distribution_id(cloudfront, hostname)
+      distributions = cloudfront.list_distributions.distribution_list.items.map do |dist|
+        [dist.id, dist.aliases.items]
       end
-
-      ids.map do |app, id|
-        cloudfront.wait_until(:distribution_deployed, id: id) do |waiter|
-          waiter.max_attempts = 60 # wait up to an hour for CloudFront distribution to deploy
-          waiter.before_wait {|_| puts "Waiting for #{app} distribution to deploy.."}
-        end
-        puts "#{app} distribution deployed!"
-      end
+      distributions.to_h.select {|_, v| v.include? hostname}.keys.first
     end
 
-    # Returns the distribution ID and fetched DistributionConfig object for a given hostname.
-    # Uses cached mappings first if available.
-    def self.get_distribution_config(cloudfront, hostname)
-      id = get_distribution_id_with_retry(cloudfront, hostname)
-      [id, id && cloudfront.get_distribution_config(id: id)]
-    rescue Aws::CloudFront::Errors::NoSuchDistribution
-      # Cached id may be stale, try again with an uncached id.
-      id = get_distribution_id(false, cloudfront, hostname)
-      [id, id && cloudfront.get_distribution_config(id: id)]
-    end
-
-    # Calls get_distribution_id, retrying without cache if the result is nil.
-    def self.get_distribution_id_with_retry(*args)
-      get_distribution_id(true, *args) || get_distribution_id(false, *args)
-    end
-
-    # Returns the distribution ID for a given hostname.
-    # Uses cached id-to-hostname mappings if cached=true.
-    def self.get_distribution_id(cached, cloudfront, hostname)
-      # Use cached value first if available.
-      mapping = JSON.parse(IO.read(alias_cache)) if cached && File.file?(alias_cache)
-      # Otherwise fall back to API call.
-      unless mapping
-        dists = cloudfront.list_distributions.distribution_list.items.map do |dist|
-          [dist.id, dist.aliases.items]
-        end
-        mapping = dists.to_h.tap do |out|
-          # Write result to cache.
-          IO.write(alias_cache, JSON.pretty_generate(out))
-        end
-      end
-      mapping.select {|_, v| v.include? hostname}.keys.first
-    end
-
-    # Returns a CloudFront DistributionConfig Hash compatible with the AWS SDK for Ruby v2.
-    # Syntax reference: http://docs.aws.amazon.com/sdkforruby/api/Aws/CloudFront/Types/DistributionConfig.html
-    # `app` is a symbol containing the app name (:pegasus, :dashboard or :hourofcode)
-    def self.config(app, reference = nil)
-      behaviors, cloudfront, config = get_app_config(app, method(:cache_behavior))
-      ssl_cert = cloudfront[:ssl_cert]
-      # Lookup ACM Certificate ARN from server certificate name
-      acm = Aws::ACM::Client.new
-      certificate_arn = acm.
-        list_certificates(certificate_statuses: ['ISSUED']).
-        certificate_summary_list.
-        select {|cert| cert.domain_name == "*.#{ssl_cert}" || cert.domain_name == ssl_cert}.
-        max_by {|cert| acm.describe_certificate(certificate_arn: cert.certificate_arn).certificate.not_after}.
-        try(:certificate_arn)
-      {
-        is_ipv6_enabled: false,
-        aliases: {
-          quantity: cloudfront[:aliases].length, # required
-          items: cloudfront[:aliases].empty? ? nil : cloudfront[:aliases],
-        },
-        default_root_object: '',
-        origins: { # required
-          quantity: 2, # required
-          items: [
-            {
-              id: 'cdo', # required
-              domain_name: cloudfront[:origin], # required
-              origin_path: '',
-              custom_origin_config: {
-                http_port: 80, # required
-                https_port: 443, # required
-                origin_protocol_policy: 'match-viewer', # required, accepts http-only, match-viewer
-                origin_ssl_protocols: {
-                  quantity: 2,
-                  items: %w(SSLv3 TLSv1)
-                },
-                origin_read_timeout: 30,
-                origin_keepalive_timeout: 5
-              },
-              custom_headers: {
-                quantity: 0
-              }
-            },
-            {
-              id: 'cdo-assets',
-              domain_name: "#{CDO.assets_bucket}.s3.amazonaws.com",
-              origin_path: "/#{rack_env}",
-              s3_origin_config: {
-                origin_access_identity: ''
-              },
-              custom_headers: {
-                quantity: 0
-              },
-            },
-          ],
-        },
-        default_cache_behavior: cache_behavior(config[:default]),
-        cache_behaviors: {
-          quantity: behaviors.length, # required
-          items: behaviors.empty? ? nil : behaviors,
-        },
-        custom_error_responses: {}.tap do |hash|
-          hash[:items] = ERROR_CODES.map do |error|
-            {
-              error_code: error,
-              response_code: '',
-              response_page_path: '',
-              error_caching_min_ttl: 0
-            }
-          end
-          hash[:quantity] = ERROR_CODES.length
-        end,
-        comment: '', # required
-        logging: {
-          enabled: !!cloudfront[:log], # required
-          include_cookies: false, # required
-          bucket: cloudfront[:log] && "#{cloudfront[:log][:bucket]}.s3.amazonaws.com", # required
-          prefix: cloudfront[:log] && cloudfront[:log][:prefix], # required
-        },
-        price_class: 'PriceClass_All', # accepts PriceClass_100, PriceClass_200, PriceClass_All
-        enabled: true, # required
-        viewer_certificate: ssl_cert ? {
-          acm_certificate_arn: certificate_arn,
-          certificate: certificate_arn,
-          certificate_source: 'acm',
-          ssl_support_method: 'sni-only', # accepts sni-only, vip
-          minimum_protocol_version: 'TLSv1' # accepts SSLv3, TLSv1
-        } : {
-          cloud_front_default_certificate: true,
-          minimum_protocol_version: 'TLSv1' # accepts SSLv3, TLSv1
-        },
-        restrictions: {
-          geo_restriction: { # required
-            restriction_type: 'none', # required, accepts blacklist, whitelist, none
-            quantity: 0 # required
-          },
-        },
-        web_acl_id: '',
-        http_version: 'http2'
-      }.tap do |cf|
-        cf[:caller_reference] = reference || Digest::MD5.hexdigest(Marshal.dump(config)) # required
-      end
-    end
-
-    def self.get_app_config(app, behavior_method)
-      config = app == :hourofcode ? HTTP_CACHE[:pegasus] : HTTP_CACHE[app]
-      cloudfront = CONFIG[app]
-      behaviors = config[:behaviors].map do |behavior|
-        paths = behavior[:path]
-        paths = [paths] unless paths.is_a? Array
-        validate_paths paths
-        paths.map do |path|
-          behavior_method.call(behavior, path)
-        end
-      end.flatten
-      return behaviors, cloudfront, config
-    end
-
-    # Same as #config, except returns a CloudFormation JSON.
+    # Returns a CloudFront DistributionConfig in CloudFormation syntax.
     # `app` is a symbol containing the app name (:pegasus, :dashboard or :hourofcode)
     def self.config_cloudformation(app, origin, aliases, ssl_cert=nil)
-      behaviors, cloudfront, config = get_app_config(app, method(:cache_behavior_cloudformation))
+      behaviors, cloudfront, config = get_app_config(app)
       {
         Aliases: aliases,
         CacheBehaviors: behaviors,
@@ -343,7 +134,7 @@ module AWS
             ErrorCode: error,
           }
         end,
-        DefaultCacheBehavior: cache_behavior_cloudformation(config[:default]),
+        DefaultCacheBehavior: cache_behavior(config[:default]),
         DefaultRootObject: '',
         Enabled: true,
         Logging: {
@@ -384,69 +175,22 @@ module AWS
       }.to_json
     end
 
-    # Returns a CloudFront CacheBehavior Hash compatible with the AWS SDK for Ruby v2.
-    # Syntax reference: http://docs.aws.amazon.com/sdkforruby/api/Aws/CloudFront/Types/CacheBehavior.html
-    # `behavior_config` contains `headers` and `cookies` whitelists.
-    def self.cache_behavior(behavior_config, path=nil)
-      s3 = behavior_config[:proxy] == 'cdo-assets'
-      cookie_config = if behavior_config[:cookies].is_a?(Array)
-                        { # required
-                          forward: 'whitelist', # required, accepts none, whitelist, all
-                          whitelisted_names: {
-                            quantity: behavior_config[:cookies].length, # required
-                            items: (behavior_config[:cookies].empty? ? nil : behavior_config[:cookies]),
-                          }
-                        }
-                      else
-                        {
-                          forward: behavior_config[:cookies]
-                        }
-                      end
-      # Include Host header in CloudFront's cache key to match Varnish for custom origins.
-      # Include S3 forward headers for s3 origins.
-      headers = behavior_config[:headers] +
-        (s3 ? S3_FORWARD_HEADERS : ['Host'])
-      behavior = { # required
-        target_origin_id: (s3 ? behavior_config[:proxy] : 'cdo'), # required
-        forwarded_values: { # required
-          query_string: true, # required
-          cookies: cookie_config,
-          headers: {
-            quantity: headers.length, # required
-            items: headers.empty? ? nil : headers,
-          },
-          query_string_cache_keys: {
-            quantity: 0
-          },
-        },
-        trusted_signers: { # required
-          enabled: false, # required
-          quantity: 0
-        },
-        viewer_protocol_policy: 'redirect-to-https', # required, accepts allow-all, https-only, redirect-to-https
-        min_ttl: 0, # required
-        allowed_methods: {
-          quantity: 7, # required
-          items: ALLOWED_METHODS, # required, accepts GET, HEAD, POST, PUT, PATCH, OPTIONS, DELETE
-          cached_methods: {
-            quantity: 3, # required
-            items: CACHED_METHODS, # required, accepts GET, HEAD, POST, PUT, PATCH, OPTIONS, DELETE
-          },
-        },
-        smooth_streaming: false,
-        default_ttl: 0,
-        max_ttl: 31_536_000, # =1 year
-        compress: true, # Serve gzip-compressed assets
-        lambda_function_associations: {
-          quantity: 0,
-        },
-      }
-      behavior[:path_pattern] = path if path
-      behavior
+    def self.get_app_config(app)
+      config = app == :hourofcode ? HTTP_CACHE[:pegasus] : HTTP_CACHE[app]
+      cloudfront = CONFIG[app]
+      behaviors = config[:behaviors].map do |behavior|
+        paths = behavior[:path]
+        paths = [paths] unless paths.is_a? Array
+        validate_paths paths
+        paths.map do |path|
+          cache_behavior(behavior, path)
+        end
+      end.flatten
+      return behaviors, cloudfront, config
     end
 
     # Returns a CloudFront CacheBehavior Hash compatible with AWS CloudFormation.
-    def self.cache_behavior_cloudformation(behavior_config, path=nil)
+    def self.cache_behavior(behavior_config, path=nil)
       s3 = behavior_config[:proxy] == 'cdo-assets'
       # Include Host header in CloudFront's cache key to match Varnish for custom origins.
       # Include S3 forward headers for s3 origins.
