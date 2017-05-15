324b5f3591bafc22b26b37416e1340aeafaf2c82
Josh Lory <josh.lory@code.org>
2017-05-15 16:49:11 -0700

    Show "2x" text instead of zooming image in the editor

--- apps/src/code-studio/components/Grid.jsx
+++ apps/src/code-studio/components/Grid.jsx
@@ -61,12 +61,12 @@ var Cell = React.createClass({
         tdStyle.backgroundImage = "url('/blockly/media/skins/studio/" + studioAvatarList[cell.sprite_] + "_spritesheet_200px.png')";
       }
     } else if (this.props.skin === 'starwarsgrid') {
-      const zoom = !!(WallTypeMask & cell.tileType_);
+      text = WallTypeMask & cell.tileType_ ? '2x' : '';
       const x = (WallCoordColMask & cell.tileType_) >> WallCoordColShift;
       const y = (WallCoordRowMask & cell.tileType_) >> WallCoordRowShift;
       tdStyle.backgroundImage = "url('/blockly/media/skins/hoc2015x/tiles_background1.png')";
-      tdStyle.backgroundSize = zoom ? "1600% 1600%" : "800% 800%";
-      tdStyle.backgroundPosition = `-${x * CELL_WIDTH * (zoom + 1)}px -${y * CELL_HEIGHT * (zoom + 1)}px`;
+      tdStyle.backgroundSize = "800% 800%";
+      tdStyle.backgroundPosition = `-${x * CELL_WIDTH}px -${y * CELL_HEIGHT}px`;
     } else {
       classNames.push(karelTiles[cell.tileType_]);
 
