59ef77224e77eb11737f3c6e45533002bdd22692
Brendan Reville <brendan@code.org>
2017-05-18 14:42:07 -0700

    New header: banner image on studio.code.org/ and /courses
    The image & text are just placeholders so far.  The header HAML is responsible for determining whether to show the image, and the page content is responsible for displaying heading & subheading text on top of that banner, and regular page content below that.

--- apps/src/templates/teacherHomepage/StudentHomepage.jsx
+++ apps/src/templates/teacherHomepage/StudentHomepage.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import HeadingBanner from '../HeadingBanner';
 import RecentCoursesCollapsible from './RecentCoursesCollapsible';
 import CollapsibleSection from './CollapsibleSection';
 import ResourceCard from './ResourceCard';
 import shapes from './shapes';
+import ProtectedStatefulDiv from '../ProtectedStatefulDiv';
 import i18n from "@cdo/locale";
+
+const styles = {
+  userHero: {
+    paddingTop: 10
+  }
+};
 
 const StudentHomepage = React.createClass({
   propTypes: {
     courses: shapes.courses
+  },
+
+  componentDidMount() {
+    // The component used here is implemented in legacy HAML/CSS rather than React.
+    $('#user_hero').appendTo(ReactDOM.findDOMNode(this.refs.userHero)).show();
   },
 
   render() {
@@ -15,6 +29,16 @@ const StudentHomepage = React.createClass({
 
     return (
       <div>
+        <HeadingBanner
+          headingText={i18n.studentHomepageHeadingText()}
+          subHeadingText={i18n.studentHomepageSubHeadingText()}
+        />
+
+        <ProtectedStatefulDiv
+          style={styles.userHero}
+          ref="userHero"
+        />
+
         <RecentCoursesCollapsible
           courses={courses}
           showAllCoursesLink={true}
