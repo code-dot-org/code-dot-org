fbec1ce88796c52a67c23ec56de1b7c06bb7a1ac
Brad Buchanan <brad@code.org>
2017-05-17 17:28:53 -0700

    Wrap RGB in new NeoPixel class (failing tests)

---
+++ apps/src/lib/kits/maker/NeoPixel.js
@@ -0,0 +1,57 @@
+/** @file Wrapper around Johnny-Five NeoPixel component */
+import five from '@code-dot-org/johnny-five';
+
+export const PASS_THRU_PROPERTIES = [
+    'board',
+    'pin',
+];
+
+export const PASS_THRU_METHODS = [
+  'blink',
+  'color',
+  'intensity',
+  'stop',
+  'toggle',
+];
+
+/**
+ * Wrap Johnny-Five's Led.RGB (neopixel) component to modify the behavior of
+ * its on() and off() functions.
+ * @param args - see five.Led.RGB
+ * @constructor
+ */
+export default function NeoPixel(...args) {
+  /** @private {five.Led.RGB} */
+  this.rgb_ = new five.Led.RGB(...args);
+
+  // Set of properties that simply pass-through to wrapped Led controller.
+  PASS_THRU_PROPERTIES.forEach(prop => Object.defineProperty(this, prop, {
+    get: () => this.rgb_[prop],
+    set: val => this.rgb_[prop] = val,
+  }));
+}
+
+// Set of functions that simply pass-through to the wrapped Led controller.
+PASS_THRU_METHODS.forEach(fnName => {
+  NeoPixel.prototype[fnName] = function (...args) {
+    return this.rgb_[fnName](...args);
+  };
+});
+
+/**
+ * The on() function should call stop() to cancel any LED animation (blinking,
+ * etc) before turning the LED on.
+ */
+NeoPixel.prototype.on = function (...args) {
+  this.rgb_.stop();
+  return this.rgb_.on(...args);
+};
+
+/**
+ * The off() function should call stop() to cancel any LED animation (blinking,
+ * etc.) before turning the LED off.
+ */
+NeoPixel.prototype.off = function (...args) {
+  this.rgb_.stop();
+  return this.rgb_.off(...args);
+};
