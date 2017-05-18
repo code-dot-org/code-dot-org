c0620a7271a20803b6801aa4bcaff39873309ff8
Josh Lory <josh.lory@code.org>
2017-05-17 18:13:53 -0700

    Restore redux after test

--- apps/test/unit/craft/craftTest.js
+++ apps/test/unit/craft/craftTest.js
@@ -1,6 +1,6 @@
 import {assert} from '../../util/configuredChai';
 import sinon from 'sinon';
-import {getStore, registerReducers, stubRedux} from '@cdo/apps/redux';
+import {getStore, registerReducers, stubRedux, restoreRedux} from '@cdo/apps/redux';
 import pageConstants from '@cdo/apps/redux/pageConstants';
 import Craft from '@cdo/apps/craft/code-connection/craft';
 
@@ -8,8 +8,10 @@ describe('Craft', () => {
   before(() => sinon.stub(Craft, 'render'));
   after(() => Craft.render.restore());
 
+  beforeEach(stubRedux);
+  afterEach(restoreRedux);
+
   it('app init', () => {
-    stubRedux();
     registerReducers({pageConstants});
     const config = {
       level: {},
