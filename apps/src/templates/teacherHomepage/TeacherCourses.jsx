75c4a458fa2792708c2d51e7eb45d08f198752bf
Brendan Reville <brendan@code.org>
2017-05-22 02:30:52 -0700

    Courses: proper links to curriculum for teachers, improved argument passing
    ...and parsing.

--- apps/src/templates/teacherHomepage/TeacherCourses.jsx
+++ apps/src/templates/teacherHomepage/TeacherCourses.jsx
@@ -4,8 +4,12 @@ import CollapsibleSection from './CollapsibleSection';
 import i18n from "@cdo/locale";
 
 const TeacherCourses = React.createClass({
+  propTypes: {
+    codeOrgUrlPrefix: React.PropTypes.string.isRequired
+  },
 
   render() {
+    const codeOrgUrlPrefix = this.props.codeOrgUrlPrefix;
 
     return (
       <CollapsibleSection header={i18n.resources()}>
@@ -14,28 +18,28 @@ const TeacherCourses = React.createClass({
           description={""}
           image="../../static/navcard-placeholder.png"
           buttonText={i18n.learnMore()}
-          link="https://hourofcode.com"
+          link={`${codeOrgUrlPrefix}/hourofcode/all`}
         />
         <ResourceCard
           title={i18n.teacherCourseElementary()}
           description={""}
           image="../../static/navcard-placeholder.png"
           buttonText={i18n.learnMore()}
-          link="/courses"
+          link={`${codeOrgUrlPrefix}/educate/curriculum/elementary-school`}
         />
         <ResourceCard
           title={i18n.teacherCourseMiddle()}
           description={""}
           image="../../static/navcard-placeholder.png"
           buttonText={i18n.learnMore()}
-          link="/courses"
+          link={`${codeOrgUrlPrefix}/educate/curriculum/middle-school`}
         />
         <ResourceCard
           title={i18n.teacherCourseHighOlder()}
           description={""}
           image="../../static/navcard-placeholder.png"
           buttonText={i18n.learnMore()}
-          link="/courses"
+          link={`${codeOrgUrlPrefix}/educate/curriculum/high-school`}
         />
       </CollapsibleSection>
     );
