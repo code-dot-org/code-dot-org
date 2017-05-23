2440179393848102c4be502360038b586fc53e3d
Brent Van Minnen <bjvanminnen@gmail.com>
2017-05-23 12:48:02 -0700

    add foreign key for course_id

--- dashboard/db/migrate/20170522221144_add_course_id_to_sections.rb
+++ dashboard/db/migrate/20170522221144_add_course_id_to_sections.rb
@@ -1,5 +1,6 @@
 class AddCourseIdToSections < ActiveRecord::Migration[5.0]
   def change
-    add_column :sections, :course_id, :integer
+    add_column :sections, :course_id, :integer, after: :script_id
+    add_foreign_key :sections, :courses
   end
 end
