8963b4f5692a0deb6e0559ebf4277c4793c6abdc
Will Jordan <will@code.org>
2017-05-24 14:01:43 -0700

    lazy-load video embeds and carousel images insert videojs script tag only once per page keep image hidden at start

--- apps/src/util/loadVideos.js
+++ apps/src/util/loadVideos.js
@@ -22,6 +22,24 @@ export function loadVideos(force_fallback) {
 // Parameter player is either "youtube" or "fallback".
 function setupVideos(player) {
 
+  if (player == "fallback") {
+    var doc = document,
+      video_css = doc.createElement('link'),
+      video = doc.createElement("script"),
+      video_ie = doc.createElement("script");
+
+    video_css.type="text/css";
+    video_css.href="/css/video-js.css";
+    video_css.rel="stylesheet";
+    doc.getElementsByTagName('head')[0].appendChild(video_css);
+    video_ie.type = "text/javascript";
+    video_ie.src = "/js/videojs-ie8.min.js";
+    doc.body.appendChild(video_ie);
+    video.type = "text/javascript";
+    video.src = "/js/video.min.js";
+    doc.body.appendChild(video);
+  }
+
   $(".insert_video_player").each(function() {
     var downloadPath = $(this).data("download-path");
 
@@ -30,27 +48,11 @@ function setupVideos(player) {
     // it relies upon a missing addEventListener.
     if (player == "fallback" && downloadPath && window.addEventListener)
     {
-      var doc = document,
-        video_css = doc.createElement('link'),
-        video = doc.createElement("script"),
-        video_ie = doc.createElement("script");
-
-      video_css.type="text/css";
-      video_css.href="/css/video-js.css";
-      video_css.rel="stylesheet";
-      doc.getElementsByTagName('head')[0].appendChild(video_css);
-      video_ie.type = "text/javascript";
-      video_ie.src = "/js/videojs-ie8.min.js";
-      doc.body.appendChild(video_ie);
-      video.type = "text/javascript";
-      video.src = "/js/video.min.js";
-      doc.body.appendChild(video);
-
       $(this).parent().append(
         '<video ' +
         'style="position:absolute; top: 0; left: 0; width: 100%; height: 100%"' +
         'width="100%" height="100%" ' +
-        'class="video-js" ' +
+        'class="video-js lazyload" ' +
         'preload="none"' +
         'controls preload="auto" >' +
         '  <source src="' + downloadPath + '" type="video/mp4"/>' +
@@ -60,7 +62,7 @@ function setupVideos(player) {
     else
     {
       $(this).parent().append(
-        '<iframe style="position:absolute; top: 0; left: 0; width: 100%; height: 100%" src="https://www.youtube.com/embed/' + $(this).data("video-code") + '?iv_load_policy=3&rel=0&autohide=1&showinfo=0&enablejsapi=1" frameborder="0" allowfullscreen=true>');
+        '<iframe class="lazyload" style="position:absolute; top: 0; left: 0; width: 100%; height: 100%" data-src="https://www.youtube.com/embed/' + $(this).data("video-code") + '?iv_load_policy=3&rel=0&autohide=1&showinfo=0&enablejsapi=1" frameborder="0" allowfullscreen=true>');
     }
   });
 }
