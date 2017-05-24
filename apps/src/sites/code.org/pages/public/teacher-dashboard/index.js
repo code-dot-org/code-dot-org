ed0d1fc62fb38774b1f845a9908027a07d3814d0
Brent Van Minnen <bjvanminnen@gmail.com>
2017-05-23 17:04:07 -0700

    dont check experiment at top level

--- apps/src/sites/code.org/pages/public/teacher-dashboard/index.js
+++ apps/src/sites/code.org/pages/public/teacher-dashboard/index.js
@@ -8,12 +8,9 @@ import ReactDOM from 'react-dom';
 import SectionProjectsList from '@cdo/apps/templates/projects/SectionProjectsList';
 import experiments from '@cdo/apps/util/experiments';
 
-// Check the experiment at the top level, so that the enableExperiments and
-// disableExperiments url params will cause a persistent setting to be stored
-// from any page in teacher dashboard.
-const showProjectThumbnails = experiments.isEnabled('showProjectThumbnails');
-
 function renderSectionProjects(sectionId) {
+  const showProjectThumbnails = experiments.isEnabled('showProjectThumbnails');
+
   const dataUrl = `/dashboardapi/v1/projects/section/${sectionId}`;
   const element = document.getElementById('projects-list');
 
