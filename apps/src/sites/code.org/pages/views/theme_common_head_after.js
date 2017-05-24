06656bf138ca63e667d7f22bab2aaefbd29ae3e3
Will Jordan <will@code.org>
2017-05-24 13:59:48 -0700

    refactor video_player.js.haml into loadVideos.js include javascript code in theme_common_head_after.js bundle.

--- apps/src/sites/code.org/pages/views/theme_common_head_after.js
+++ apps/src/sites/code.org/pages/views/theme_common_head_after.js
@@ -1,5 +1,6 @@
 import {isUnsupportedBrowser} from '@cdo/apps/util/browser-detector';
-import {initHamburger} from '@cdo/apps/hamburger/hamburger.js';
+import {initHamburger} from '@cdo/apps/hamburger/hamburger';
+import {loadVideos} from '@cdo/apps/util/loadVideos';
 
 // Prevent filtered errors from being passed to New Relic.
  if (window.newrelic) {
@@ -16,3 +17,4 @@ $(document).ready(function () {
 });
 
 initHamburger();
+window.loadVideos = loadVideos;
