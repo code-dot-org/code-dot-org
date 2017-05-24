2ad93bb8972cdc827f716fc9c875a3a72cddaf2f
Jingyu Lee <v-leejin@microsoft.com>
2017-05-24 14:06:06 -0700

    Unit test cases for code connection extension

--- apps/test/unit/craft/code-connection/ccTest.js
+++ apps/test/unit/craft/code-connection/ccTest.js
@@ -3,8 +3,8 @@ import sinon from 'sinon';
 import {singleton as studioApp} from '@cdo/apps/StudioApp';
 import {executeUserCode} from '@cdo/apps/craft/code-connection/craft';
 
-describe('Craft', () => {
-  it('move forward block', done => {
+describe('Code Connection extension', () => {
+  it('move forward block to verify single key-value parsing', done => {
     sinon.stub(studioApp(), 'highlight');
 
     const mockClient = {
@@ -14,5 +14,28 @@ describe('Craft', () => {
       }
     };
     executeUserCode(mockClient, 'move(0, "forward")');
+  });
+
+
+  it('place forward block to verify multiple key-value parsing', done => {
+
+    const mockClient = {
+      async_command: command => {
+        expect(command).to.eql('place?slotNum=0&direction=forward');
+        done();
+      }
+    };
+    executeUserCode(mockClient, 'place(0, "0", "forward")');
+  });
+
+  it('give block to verify item type', done => {
+
+    const mockClient = {
+      async_command: command => {
+        expect(command).to.eql('give?player=steve&itemName=stone&data=1&amount=2');
+        done();
+      }
+    };
+    executeUserCode(mockClient, `give(0, "steve", item(0, "stone", "1"), "2")`);
   });
 });
