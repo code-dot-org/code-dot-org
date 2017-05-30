ede363b270adcdef2a6c5b1521df079072a1e677
Erin <erin.bond@code.org>
2017-05-29 19:10:06 -0700

    Resource card images (#15441)
    * Images for resource cards
    
    * Resource card images for /courses
    
    * Resource cards for Teacher /courses
    
    * /static shortcut in webpack

--- apps/src/templates/teacherHomepage/Courses.jsx
+++ apps/src/templates/teacherHomepage/Courses.jsx
@@ -19,6 +19,12 @@ const styles = {
     zIndex: 2,
     color: color.charcoal,
     width: 940
+  },
+  spacer: {
+    height: 50,
+    width: 940,
+    float: 'left',
+    color: color.white
   }
 };
 
@@ -81,6 +87,7 @@ const Courses = React.createClass({
               {i18n.courseExplorerDescription()}
             </div>
             <ProtectedStatefulDiv ref="courseExplorer"/>
+            <div style={styles.spacer}>.</div>
 
             <TeacherCourses codeOrgUrlPrefix={codeOrgUrlPrefix}/>
 
