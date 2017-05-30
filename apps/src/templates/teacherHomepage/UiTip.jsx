fc1844ad87e20ac056502c1765a2878c1f3df480
Brendan Reville <brendan@code.org>
2017-05-29 21:05:12 -0700

    New UI tips: add a little X to the tip

--- apps/src/templates/teacherHomepage/UiTip.jsx
+++ apps/src/templates/teacherHomepage/UiTip.jsx
@@ -16,12 +16,19 @@ const UiTip = React.createClass({
     const { index, position, text } = this.props;
 
     return (
-      <div
-        className="arrow_box_up"
-        onClick={this.closeClicked.bind(this, index)}
-        style={position}
-      >
-        {text}
+      <div>
+        <div
+          className="arrow_box_up"
+          onClick={this.closeClicked.bind(this, index)}
+          style={position}
+        >
+          <div style={{textAlign: "right"}}>
+            <i className="fa fa-times"/>
+          </div>
+          <div>
+            {text}
+          </div>
+        </div>
       </div>
     );
   }
