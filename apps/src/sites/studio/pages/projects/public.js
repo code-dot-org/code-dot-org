d22b301ba2e9783b8c58c50ba0c6433afddc395a
David Bailey <davidsbailey@users.noreply.github.com>
2017-05-30 15:32:05 -0700

    fix styling for /projects/public with all flags while signed out

--- apps/src/sites/studio/pages/projects/public.js
+++ apps/src/sites/studio/pages/projects/public.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PublicGallery from '@cdo/apps/templates/projects/PublicGallery';
+import HeadingBanner from '@cdo/apps/templates/HeadingBanner';
+import i18n from "@cdo/locale";
 
 const MAX_PROJECTS_PER_CATEGORY = 100;
 
@@ -13,7 +15,12 @@ $(document).ready(() => {
   }).done(projectLists => {
     const publicGallery = document.getElementById('public-gallery');
     ReactDOM.render(
-      <PublicGallery projectLists={projectLists}/>,
+      <div>
+        <HeadingBanner
+          headingText={i18n.projectGalleryHeader()}
+        />
+        <PublicGallery projectLists={projectLists}/>
+      </div>,
       publicGallery);
   });
 });
