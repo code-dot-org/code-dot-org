f87ff37378ee7bf4cab44b609e1c0e450545ebc8
David Bailey <davidsbailey@users.noreply.github.com>
2017-06-01 12:34:09 -0700

    Revert "Merge pull request #15355 from code-dot-org/combine-projects-public"
    This reverts commit 01eb144f5682eab94c12522ce3c8c8c1da8c56d1, reversing
    changes made to 132969a29af101068569d2083827de0d0fdb3c0a.

--- apps/src/sites/studio/pages/projects/index.js
+++ apps/src/sites/studio/pages/projects/index.js
@@ -8,7 +8,6 @@ import i18n from '@cdo/locale';
 import {Galleries} from '@cdo/apps/templates/projects/GallerySwitcher';
 
 const MAX_PROJECTS_PER_CATEGORY = 100;
-const isPublic = window.location.pathname.startsWith('/projects/public');
 
 $(document).ready(() => {
   // We need to see whether the experiment is enabled from angularProjects.js,
@@ -16,7 +15,7 @@ $(document).ready(() => {
   $('#angular-my-projects-wrapper').attr('data-isPublicGalleryEnabled', 'true');
 
   const projectsHeader = document.getElementById('projects-header');
-  ReactDOM.render(<ProjectHeader showGallery={showGallery} isPublic={isPublic}/>, projectsHeader);
+  ReactDOM.render(<ProjectHeader showGallery={showGallery} isPublic={false}/>, projectsHeader);
 
   $.ajax({
     method: 'GET',
@@ -31,14 +30,8 @@ $(document).ready(() => {
 });
 
 function showGallery(gallery) {
-  updateLocation(gallery);
   $('#angular-my-projects-wrapper').toggle(gallery === Galleries.PRIVATE);
   $('#public-gallery-wrapper').toggle(gallery === Galleries.PUBLIC);
-}
-
-function updateLocation(gallery) {
-  const path = (gallery === Galleries.PUBLIC) ? '/projects/public' : '/projects';
-  window.history.pushState(null, document.title, path);
 }
 
 function onShowConfirmPublishDialog(callback) {
