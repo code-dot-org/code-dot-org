8a885cc0f24c8d1323974833cc73e834afcddfeb
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-06-01 17:20:58 -0700

    add dependent required fields

--- apps/src/code-studio/pd/teachercon_survey/FridayOnly.jsx
+++ apps/src/code-studio/pd/teachercon_survey/FridayOnly.jsx
@@ -50,6 +50,7 @@ export default class FridayOnly extends FormComponent {
             componentClass: "textarea",
             label: this.labelFor("howCouldImprove"),
             name: "howCouldImprove",
+            required: true
           })
         }
 
