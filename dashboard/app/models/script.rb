85a9ce80eff951ec1638c524af8efab4d74819e5
Ram Kandasamy <ram@code.org>
2017-06-02 11:01:37 -0700

    Add tests and fix the bug they found

--- dashboard/app/models/script.rb
+++ dashboard/app/models/script.rb
@@ -320,7 +320,7 @@ class Script < ActiveRecord::Base
     script_levels.map do |script_level|
       script_level.levels.map do |level|
         next if level.contained_levels.empty? ||
-          !TEXT_RESPONSE_TYPES.include?(level.contained_levels.first.type)
+          !TEXT_RESPONSE_TYPES.include?(level.contained_levels.first.class)
         text_response_levels << {
           script_level: script_level,
           levels: [level.contained_levels.first]
