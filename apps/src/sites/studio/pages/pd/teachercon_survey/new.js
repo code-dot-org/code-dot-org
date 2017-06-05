f1aebe8034e3315cac1f343ca054321ed0fef2cb
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-06-05 13:19:20 -0700

    don't use currentScript

--- apps/src/sites/studio/pages/pd/teachercon_survey/new.js
+++ apps/src/sites/studio/pages/pd/teachercon_survey/new.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TeacherconSurvey from '@cdo/apps/code-studio/pd/teachercon_survey/TeacherconSurvey';
-import {getCurrentScriptData} from '@cdo/apps/util/getScriptData';
-
-const data = getCurrentScriptData();
+import getScriptData from '@cdo/apps/util/getScriptData';
 
 document.addEventListener("DOMContentLoaded", function (event) {
   ReactDOM.render(
     <TeacherconSurvey
-      {...data.props}
+      {...getScriptData('props')}
     />,
     document.getElementById('application-container')
   );
