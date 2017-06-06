3f6c9836e742ae41a8153deba9057321789520fc
Josh Lory <josh.lory@code.org>
2017-06-05 17:25:45 -0700

    Add "StageExtras" story to React Storybook

---
+++ apps/src/code-studio/components/stageExtras/StageExtras.story.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import StageExtras from './StageExtras';
+
+export default storybook => {
+  storybook
+    .storiesOf('StageExtras', module)
+    .addWithInfo(
+      'default',
+      'This is the StageExtras component.',
+      () => (
+        <StageExtras
+          stageNumber="Test Stage"
+          nextLevelPath="#"
+        />
+      )
+    );
+};
