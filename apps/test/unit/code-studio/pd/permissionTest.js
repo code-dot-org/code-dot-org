b30fdacbc6d17aa7fa7c7f20115a35e2331a25e2
Andrew Oberhardt <Andrew@Code.org>
2017-06-07 14:54:00 -0700

    Fix test

--- apps/test/unit/code-studio/pd/permissionTest.js
+++ apps/test/unit/code-studio/pd/permissionTest.js
@@ -13,7 +13,7 @@ describe("Tests for Permission class", () => {
   it("Detects admin", () => {
     setGlobalPermissionString("workshop_admin");
     const permission = new Permission();
-    expect(permission.isAdmin).to.be.true;
+    expect(permission.isWorkshopAdmin).to.be.true;
     expect(permission.isFacilitator).to.be.false;
     expect(permission.isOrganizer).to.be.false;
     expect(permission.isPartner).to.be.false;
@@ -22,7 +22,7 @@ describe("Tests for Permission class", () => {
   it("Detects facilitator", () => {
     setGlobalPermissionString("[facilitator]");
     const permission = new Permission();
-    expect(permission.isAdmin).to.be.false;
+    expect(permission.isWorkshopAdmin).to.be.false;
     expect(permission.isFacilitator).to.be.true;
     expect(permission.isOrganizer).to.be.false;
     expect(permission.isPartner).to.be.false;
@@ -31,7 +31,7 @@ describe("Tests for Permission class", () => {
   it("Detects organizer", () => {
     setGlobalPermissionString("[workshop_organizer]");
     const permission = new Permission();
-    expect(permission.isAdmin).to.be.false;
+    expect(permission.isWorkshopAdmin).to.be.false;
     expect(permission.isFacilitator).to.be.false;
     expect(permission.isOrganizer).to.be.true;
     expect(permission.isPartner).to.be.false;
@@ -40,7 +40,7 @@ describe("Tests for Permission class", () => {
   it("Detects partner", () => {
     setGlobalPermissionString("[partner]");
     const permission = new Permission();
-    expect(permission.isAdmin).to.be.false;
+    expect(permission.isWorkshopAdmin).to.be.false;
     expect(permission.isFacilitator).to.be.false;
     expect(permission.isOrganizer).to.be.false;
     expect(permission.isPartner).to.be.true;
@@ -49,7 +49,7 @@ describe("Tests for Permission class", () => {
   it("Detects multiple permissions for organizer-partners", () => {
     setGlobalPermissionString("[workshop_organizer,partner]");
     const permission = new Permission();
-    expect(permission.isAdmin).to.be.false;
+    expect(permission.isWorkshopAdmin).to.be.false;
     expect(permission.isFacilitator).to.be.false;
     expect(permission.isOrganizer).to.be.true;
     expect(permission.isPartner).to.be.true;
@@ -58,7 +58,7 @@ describe("Tests for Permission class", () => {
   it("Detects multiple permissions for CSF Facilitators", () => {
     setGlobalPermissionString("[facilitator,workshop_organizer]");
     const permission = new Permission();
-    expect(permission.isAdmin).to.be.false;
+    expect(permission.isWorkshopAdmin).to.be.false;
     expect(permission.isFacilitator).to.be.true;
     expect(permission.isOrganizer).to.be.true;
     expect(permission.isPartner).to.be.false;
