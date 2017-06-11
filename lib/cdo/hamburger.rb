092e440099b69346eb5f2b2b77586f31bef1336a
Brendan Reville <brendan@code.org>
2017-06-11 00:21:00 -0700

    Hamburger: use constants for CSS classes

--- lib/cdo/hamburger.rb
+++ lib/cdo/hamburger.rb
@@ -1,30 +1,36 @@
 class Hamburger
+  # These are the CSS classes applied to items in the hamburger,
+  # and to the hamburger itself.
+  SHOW_ALWAYS = "show-always".freeze
+  HIDE_ALWAYS = "hide-always".freeze
+  SHOW_MOBILE = "show-mobile".freeze
+
   def self.get_visibility(options)
-    show_teacher_options = "hide-always"
-    show_student_options = "hide-always"
-    show_signed_out_options = "hide-always"
-    show_pegasus_options = "hide-always"
-    show_help_options = "hide-always"
+    show_teacher_options = HIDE_ALWAYS
+    show_student_options = HIDE_ALWAYS
+    show_signed_out_options = HIDE_ALWAYS
+    show_pegasus_options = HIDE_ALWAYS
+    show_help_options = HIDE_ALWAYS
 
     hamburger_class = "none"
 
     if options[:level]
       # The header is taken over by level-related UI, so we need the hamburger
       # to show whatever would show up in the header at desktop (and mobile) widths.
-      show_help_options = "show-always"
+      show_help_options = SHOW_ALWAYS
 
       if options[:user_type] == "teacher"
-        show_teacher_options = "show-always"
+        show_teacher_options = SHOW_ALWAYS
       elsif options[:user_type] == "student"
-        show_student_options = "show-always"
+        show_student_options = SHOW_ALWAYS
       else
-        show_signed_out_options = "show-always"
+        show_signed_out_options = SHOW_ALWAYS
       end
 
       # Regardless of user type, if they are English, then they also need the pegasus
       # options in the hamburger.
       if options[:language] == "en"
-        show_pegasus_options = "show-always"
+        show_pegasus_options = SHOW_ALWAYS
       end
 
     else
@@ -32,36 +38,36 @@ class Hamburger
       # The header is available for showing whichever options we want, but they should
       # appear in the hamburger at mobile widths.
       if options[:user_type] == "teacher"
-        show_teacher_options = "show-mobile"
+        show_teacher_options = SHOW_MOBILE
       elsif options[:user_type] == "student"
-        show_student_options = "show-mobile"
+        show_student_options = SHOW_MOBILE
       else
-        show_signed_out_options = "show-mobile"
+        show_signed_out_options = SHOW_MOBILE
       end
 
       if options[:language] == "en"
         # We want to show the pegasus options.  They're in the hamburger for desktop
         # if they didn't fit on the header, or they're just in it for mobile if they did.
         if options[:user_type] == "teacher" || options[:user_type] == "student"
-          show_pegasus_options = "show-always"
-          show_help_options = "show-always"
+          show_pegasus_options = SHOW_ALWAYS
+          show_help_options = SHOW_ALWAYS
         else
-          show_pegasus_options = "show-mobile"
-          show_help_options = "show-mobile"
+          show_pegasus_options = SHOW_MOBILE
+          show_help_options = SHOW_MOBILE
         end
       else
-        show_help_options = "show-always"
+        show_help_options = SHOW_ALWAYS
       end
     end
 
     # Do we show hamburger on all widths, only mobile, or not at all?
     show_set = [show_teacher_options, show_student_options, show_signed_out_options, show_pegasus_options, show_help_options]
-    if show_set.include? "show-always"
-      hamburger_class = "show-always"
-    elsif show_set.include? "show-mobile"
-      hamburger_class = "show-mobile"
+    if show_set.include? SHOW_ALWAYS
+      hamburger_class = SHOW_ALWAYS
+    elsif show_set.include? SHOW_MOBILE
+      hamburger_class = SHOW_MOBILE
     else
-      hamburger_class = "hide-always"
+      hamburger_class = HIDE_ALWAYS
     end
 
     # Return the various visibility styles.
--- lib/test/test_hamburger.rb
+++ lib/test/test_hamburger.rb
@@ -5,132 +5,132 @@ class HamburgerTest < Minitest::Test
   def test_level_teacher_en
     visibility = Hamburger.get_visibility({level: true, user_type: "teacher", language: "en"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "show-always"
-    assert visibility[:show_student_options]    == "hide-always"
-    assert visibility[:show_signed_out_options] == "hide-always"
-    assert visibility[:show_pegasus_options]    == "show-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_level_teacher_nonen
     visibility = Hamburger.get_visibility({level: true, user_type: "teacher", language: "fr"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "show-always"
-    assert visibility[:show_student_options]    == "hide-always"
-    assert visibility[:show_signed_out_options] == "hide-always"
-    assert visibility[:show_pegasus_options]    == "hide-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_level_student_en
     visibility = Hamburger.get_visibility({level: true, user_type: "student", language: "en"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "hide-always"
-    assert visibility[:show_student_options]    == "show-always"
-    assert visibility[:show_signed_out_options] == "hide-always"
-    assert visibility[:show_pegasus_options]    == "show-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_level_student_nonen
     visibility = Hamburger.get_visibility({level: true, user_type: "student", language: "fr"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "hide-always"
-    assert visibility[:show_student_options]    == "show-always"
-    assert visibility[:show_signed_out_options] == "hide-always"
-    assert visibility[:show_pegasus_options]    == "hide-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_level_nobody_en
     visibility = Hamburger.get_visibility({level: true, user_type: nil, language: "en"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "hide-always"
-    assert visibility[:show_student_options]    == "hide-always"
-    assert visibility[:show_signed_out_options] == "show-always"
-    assert visibility[:show_pegasus_options]    == "show-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_level_nobody_nonen
     visibility = Hamburger.get_visibility({level: true, user_type: nil, language: "fr"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "hide-always"
-    assert visibility[:show_student_options]    == "hide-always"
-    assert visibility[:show_signed_out_options] == "show-always"
-    assert visibility[:show_pegasus_options]    == "hide-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_teacher_en
     visibility = Hamburger.get_visibility({level: false, user_type: "teacher", language: "en"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "show-mobile"
-    assert visibility[:show_student_options]    == "hide-always"
-    assert visibility[:show_signed_out_options] == "hide-always"
-    assert visibility[:show_pegasus_options]    == "show-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::SHOW_MOBILE
+    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_teacher_nonen
     visibility = Hamburger.get_visibility({level: false, user_type: "teacher", language: "fr"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "show-mobile"
-    assert visibility[:show_student_options]    == "hide-always"
-    assert visibility[:show_signed_out_options] == "hide-always"
-    assert visibility[:show_pegasus_options]    == "hide-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::SHOW_MOBILE
+    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_student_en
     visibility = Hamburger.get_visibility({level: false, user_type: "student", language: "en"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "hide-always"
-    assert visibility[:show_student_options]    == "show-mobile"
-    assert visibility[:show_signed_out_options] == "hide-always"
-    assert visibility[:show_pegasus_options]    == "show-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::SHOW_MOBILE
+    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_student_nonen
     visibility = Hamburger.get_visibility({level: false, user_type: "student", language: "fr"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "hide-always"
-    assert visibility[:show_student_options]    == "show-mobile"
-    assert visibility[:show_signed_out_options] == "hide-always"
-    assert visibility[:show_pegasus_options]    == "hide-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::SHOW_MOBILE
+    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_nobody_en
     visibility = Hamburger.get_visibility({level: false, user_type: nil, language: "en"})
 
-    assert visibility[:hamburger_class]         == "show-mobile"
-    assert visibility[:show_teacher_options]    == "hide-always"
-    assert visibility[:show_student_options]    == "hide-always"
-    assert visibility[:show_signed_out_options] == "show-mobile"
-    assert visibility[:show_pegasus_options]    == "show-mobile"
-    assert visibility[:show_help_options]       == "show-mobile"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_MOBILE
+    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::SHOW_MOBILE
+    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_MOBILE
+    assert visibility[:show_help_options]       == Hamburger::SHOW_MOBILE
   end
 
   def test_nonlevel_nobody_nonen
     visibility = Hamburger.get_visibility({level: false, user_type: nil, language: "fr"})
 
-    assert visibility[:hamburger_class]         == "show-always"
-    assert visibility[:show_teacher_options]    == "hide-always"
-    assert visibility[:show_student_options]    == "hide-always"
-    assert visibility[:show_signed_out_options] == "show-mobile"
-    assert visibility[:show_pegasus_options]    == "hide-always"
-    assert visibility[:show_help_options]       == "show-always"
+    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
+    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_signed_out_options] == Hamburger::SHOW_MOBILE
+    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
+    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
   end
 end
