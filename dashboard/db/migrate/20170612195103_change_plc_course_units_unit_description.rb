ab43fcb53eab85a8809249f24e51818ecc76e063
Ram Kandasamy <ram@code.org>
2017-06-13 09:13:52 -0700

    Truncate course descriptions when downing migration

--- dashboard/db/migrate/20170612195103_change_plc_course_units_unit_description.rb
+++ dashboard/db/migrate/20170612195103_change_plc_course_units_unit_description.rb
@@ -7,6 +7,9 @@ class ChangePlcCourseUnitsUnitDescription < ActiveRecord::Migration[5.0]
 
   def down
     change_table :plc_course_units do |t|
+      ActiveRecord::Base.connection.execute(
+        'UPDATE plc_course_units SET unit_description = SUBSTRING(unit_description, 1, 255)'
+      )
       t.change :unit_description, :string
     end
   end
