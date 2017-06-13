976fb3fc2d1e3cb240f601656d16a9a6366b219d
Erin Peach <erin@code.org>
2017-06-13 15:16:55 -0700

    Updated default location for line, circle, and rect in Applab canvas. Clipped elements on first shift into design mode. Tested.

--- apps/test/unit/applab/dropletConfigTest.js
+++ apps/test/unit/applab/dropletConfigTest.js
@@ -87,3 +87,17 @@ describe(`stopTimedLoop([key])`, () => {
     expect(commands.stopTimedLoop).to.be.a('function');
   });
 });
+
+describe(`Applab droplet configuration`, () => {
+  it('uses half the max height for the default location of lines, circles, and rectangles', () => {
+    const lineBlocks = blocks.filter(block => block.func === 'line');
+    const circleBlocks = blocks.filter(block => block.func === 'circle');
+    const rectBlocks = blocks.filter(block => block.func === 'rect');
+    expect(lineBlocks).to.have.length(1);
+    expect(circleBlocks).to.have.length(1);
+    expect(rectBlocks).to.have.length(1);
+    expect(lineBlocks[0].params[3]).to.equal(225);
+    expect(circleBlocks[0].params[1]).to.equal(225);
+    expect(rectBlocks[0].params[3]).to.equal(225);
+  });
+});
