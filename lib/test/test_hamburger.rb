aa4d2675f394d7ea42cf344c96f5eadd3aa1c9dd
Brendan Reville <brendan@code.org>
2017-06-13 10:58:07 -0700

    Hamburger: update IDs for UI tests & some code review feedback

--- lib/test/test_hamburger.rb
+++ lib/test/test_hamburger.rb
@@ -71,133 +71,133 @@ class HamburgerTest < Minitest::Test
   def test_level_teacher_en
     visibility = Hamburger.get_visibility({level: true, user_type: "teacher", language: "en"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_level_teacher_nonen
     visibility = Hamburger.get_visibility({level: true, user_type: "teacher", language: "fr"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_level_student_en
     visibility = Hamburger.get_visibility({level: true, user_type: "student", language: "en"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_level_student_nonen
     visibility = Hamburger.get_visibility({level: true, user_type: "student", language: "fr"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_level_nobody_en
     visibility = Hamburger.get_visibility({level: true, user_type: nil, language: "en"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_level_nobody_nonen
     visibility = Hamburger.get_visibility({level: true, user_type: nil, language: "fr"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_teacher_en
     visibility = Hamburger.get_visibility({level: false, user_type: "teacher", language: "en"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::SHOW_MOBILE
-    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::SHOW_MOBILE
+    assert_equal visibility[:show_student_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_teacher_nonen
     visibility = Hamburger.get_visibility({level: false, user_type: "teacher", language: "fr"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::SHOW_MOBILE
-    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::SHOW_MOBILE
+    assert_equal visibility[:show_student_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_student_en
     visibility = Hamburger.get_visibility({level: false, user_type: "student", language: "en"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::SHOW_MOBILE
-    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::SHOW_MOBILE
+    assert_equal visibility[:show_signed_out_options],  Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_student_nonen
     visibility = Hamburger.get_visibility({level: false, user_type: "student", language: "fr"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::SHOW_MOBILE
-    assert visibility[:show_signed_out_options] == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::SHOW_MOBILE
+    assert_equal visibility[:show_signed_out_options],  Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_pegasus_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   def test_nonlevel_nobody_en
     visibility = Hamburger.get_visibility({level: false, user_type: nil, language: "en"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_MOBILE
-    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::SHOW_MOBILE
-    assert visibility[:show_pegasus_options]    == Hamburger::SHOW_MOBILE
-    assert visibility[:show_help_options]       == Hamburger::SHOW_MOBILE
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_MOBILE
+    assert_equal visibility[:show_teacher_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::SHOW_MOBILE
+    assert_equal visibility[:show_pegasus_options],     Hamburger::SHOW_MOBILE
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_MOBILE
   end
 
   def test_nonlevel_nobody_nonen
     visibility = Hamburger.get_visibility({level: false, user_type: nil, language: "fr"})
 
-    assert visibility[:hamburger_class]         == Hamburger::SHOW_ALWAYS
-    assert visibility[:show_teacher_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_student_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_signed_out_options] == Hamburger::SHOW_MOBILE
-    assert visibility[:show_pegasus_options]    == Hamburger::HIDE_ALWAYS
-    assert visibility[:show_help_options]       == Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:hamburger_class],          Hamburger::SHOW_ALWAYS
+    assert_equal visibility[:show_teacher_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_student_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_signed_out_options],  Hamburger::SHOW_MOBILE
+    assert_equal visibility[:show_pegasus_options],     Hamburger::HIDE_ALWAYS
+    assert_equal visibility[:show_help_options],        Hamburger::SHOW_ALWAYS
   end
 
   # Hamburger content tests.
@@ -229,18 +229,18 @@ class HamburgerTest < Minitest::Test
 
   def test_hamburger_content_nolevel
     contents = Hamburger.get_hamburger_contents({level: nil, script_level: nil, user_type: nil, language: "en"})
-    assert contents[:entries].find {|e| e[:id] == "hamburger-learn"}
+    assert contents[:entries].find {|e| e[:id] == "learn"}
   end
 
   def test_hamburger_content_gamelab_project_level
     contents = Hamburger.get_hamburger_contents({level: LevelGameLab.new, script_level: nil, user_type: nil, language: "en"})
-    assert contents[:entries].find {|e| e[:id] == "hamburger-gamelab-documentation"}
+    assert contents[:entries].find {|e| e[:id] == "gamelab-docs"}
   end
 
   def test_hamburger_content_applab_project_level
     contents = Hamburger.get_hamburger_contents({level: LevelAppLab.new, script_level: nil, user_type: nil, language: "en"})
-    assert contents[:entries].find {|e| e[:id] == "hamburger-applab-documentation"}
-    assert contents[:entries].find {|e| e[:id] == "hamburger-applab-tutorials"}
+    assert contents[:entries].find {|e| e[:id] == "applab-docs"}
+    assert contents[:entries].find {|e| e[:id] == "applab-tutorials"}
   end
 
   def test_hamburger_content_expandable
