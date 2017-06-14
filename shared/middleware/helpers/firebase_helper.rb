c661a7b1c827bced05d98e02bf158afaddd2cc2d
ashercodeorg <asher@code.org>
2017-06-14 16:27:18 -0500

    Move table_name from FirebaseHelper#initialize to FirebaseHelper#table_as_csv.

--- shared/middleware/helpers/firebase_helper.rb
+++ shared/middleware/helpers/firebase_helper.rb
@@ -2,18 +2,20 @@ require 'csv'
 require 'firebase'
 
 class FirebaseHelper
-  def initialize(channel_id, table_name)
+  def initialize(channel_id)
     raise "CDO.firebase_name not defined" unless CDO.firebase_name
     raise "CDO.firebase_secret not defined" unless CDO.firebase_secret
 
     base_uri = "https://#{CDO.firebase_name}.firebaseio.com/"
     @firebase = Firebase::Client.new(base_uri, CDO.firebase_secret)
     @channel_id = channel_id + CDO.firebase_channel_id_suffix
-    @table_name = URI.escape(table_name)
   end
 
-  def table_as_csv
-    response = @firebase.get("/v3/channels/#{@channel_id}/storage/tables/#{@table_name}/records")
+  # @param [String] table_name The name of the table to query.
+  # @return [String] A representation of the table (its columns and its data) as a CSV string.
+  def table_as_csv(table_name)
+    escaped_table_name = URI.escape(table_name)
+    response = @firebase.get("/v3/channels/#{@channel_id}/storage/tables/#{escaped_table_name}/records")
     records = response.body || []
 
     # The firebase response could be a Hash or a sparse Array
