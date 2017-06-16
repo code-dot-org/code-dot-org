b0a960be2a77f547f3f9e51a84bed37972bdffb6
Suresh Chanmugam <me@suresh.com>
2017-06-16 09:44:57 -0700

    clean up handling of scenarios where lookup via email fails or a user doesn't have any permissions

--- dashboard/app/controllers/pd/user_admin_controller.rb
+++ dashboard/app/controllers/pd/user_admin_controller.rb
@@ -2,5 +2,15 @@ class Pd::UserAdminController < ApplicationController
   authorize_resource class: :pd_user_admin
 
   def find_user
+    @permissions = Array.new
+    email = params[:email]
+    if email
+      @user = User.where(email: email).first # use the first result if there are multiple users with the same email address
+    end
+    user_id = params[:user_id]
+    if user_id
+      @user = User.find(user_id)
+    end
+    @permissions = @user.permissions if @user
   end
 end
