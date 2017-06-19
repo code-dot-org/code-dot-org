2be93e63720a08210385fa64ba7bb018132e3f2c
Suresh Chanmugam <me@suresh.com>
2017-06-19 13:06:16 -0700

    Whitelist HTTP request parameters

--- dashboard/app/controllers/pd/user_admin_controller.rb
+++ dashboard/app/controllers/pd/user_admin_controller.rb
@@ -3,14 +3,27 @@ class Pd::UserAdminController < ApplicationController
 
   def find_user
     @permissions = Array.new
-    email = params[:email]
+    email = user_admin_params[:email]
     if email
       @user = User.where(email: email).first # use the first result if there are multiple users with the same email address
     end
-    user_id = params[:user_id]
+    user_id = user_admin_params[:user_id]
     if user_id
       @user = User.find(user_id)
     end
     @permissions = @user.permissions if @user
+  end
+
+  def assign_permission
+    user_id = user_admin_params[:user_id]
+    @permission = user_admin_params[:permission]
+    @user = User.find(user_id)
+  end
+
+  private
+
+  # white list permitted request parameters
+  def user_admin_params
+    params.permit(:user_id, :email, :permission)
   end
 end
