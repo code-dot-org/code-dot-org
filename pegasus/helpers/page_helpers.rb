f48ec83af90ba5813cb16bd6f9ba40e014168ed3
Will Jordan <will@code.org>
2017-06-19 15:14:31 -0700

    inline external CSS

--- pegasus/helpers/page_helpers.rb
+++ pegasus/helpers/page_helpers.rb
@@ -13,3 +13,28 @@ def partner_site?
   partner_sites = CDO.partners.map {|x| x + '.code.org'}
   return partner_sites.include?(request.site)
 end
+
+def inline_css(css)
+  path = resolve_static('public', "css/#{css}")
+  path ||= shared_dir('css', css)
+  path = pegasus_dir('cache', 'css', css) unless File.file?(path)
+  raise "CSS not found: #{css}" unless File.file?(path)
+  css_string = Sass::Engine.new(File.read(path),
+    syntax: :scss,
+    style: rack_env?(:development) ? :none : :compressed
+  ).render.chomp
+
+  if rack_env?(:development)
+    max_inline_css = 1024 * 10
+    @total_css ||= 0
+    @total_css += Zlib::Deflate.deflate(css_string).bytesize
+    $log.warn "Too much inlined CSS in page! [#{@total_css} bytes]" if @total_css > max_inline_css
+  end
+
+  # rubocop:disable Layout/IndentHeredoc
+  <<-HTML
+<style>
+#{css_string.indent(2)}
+</style>
+  HTML
+end
