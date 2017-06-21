b09422bfd0a598c88cb8c39746fb63958c9b0f87
Suresh Chanmugam <me@suresh.com>
2017-06-21 12:10:44 -0700

    Add Assign Permission action and cleanup error error handling when user not found by ID

--- dashboard/app/controllers/pd/user_admin_controller.rb
+++ dashboard/app/controllers/pd/user_admin_controller.rb
@@ -6,7 +6,7 @@ class Pd::UserAdminController < ApplicationController
     search_term = user_admin_params[:search_term]
     if search_term =~ /^\d+$/
       user_id = search_term
-      @user = User.find(user_id)
+      @user = User.find_by(id: user_id)
     else
       email = search_term
       @user = User.find_by(email: email) # This retrieves the first result if there are multiple users with the same email address
@@ -16,14 +16,15 @@ class Pd::UserAdminController < ApplicationController
 
   def assign_permission
     user_id = user_admin_params[:user_id]
-    @permission = user_admin_params[:permission]
     @user = User.find(user_id)
+    @user.permission = user_admin_params[:pd_user_permission_id]
+    redirect_to action: "find_user", search_term: user_id
   end
 
   private
 
   # white list permitted request parameters
   def user_admin_params
-    params.permit(:search_term, :user_id, :permission)
+    params.permit(:search_term, :user_id, :pd_user_permission_id)
   end
 end
