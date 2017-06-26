a0f2f995ad0f5f69aa03cbaac217ade5bf35c7b8
Suresh Chanmugam <me@suresh.com>
2017-06-26 13:09:36 -0700

    test find_user action (without parameters) with anonymous, workshop admin, and non workshop admin users

--- dashboard/test/controllers/pd/user_admin_controller_test.rb
+++ dashboard/test/controllers/pd/user_admin_controller_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
-class Pd::UserAdminControllerTest < ActionDispatch::IntegrationTest
-  test "should get find user" do
-    get pd_user_admin_find_user_url
-    assert_response :success
+class Pd::UserAdminControllerTest < ::ActionController::TestCase
+  self.use_transactional_test_case = true
+  setup_all do
+    @workshop_admin = create :workshop_admin
+    @teacher = create :teacher
   end
+
+  def self.test_workshop_admin_only(method, action, success_response, params = nil)
+    test_user_gets_response_for action, user: -> {@teacher}, method: method, params: params, response: :forbidden
+    test_user_gets_response_for action, user: -> {@workshop_admin}, method: method, params: params, response: success_response
+  end
+
+  # test anonymous user redirect to sign in
+  test_redirect_to_sign_in_for :find_user
+  # test find_user start page is forbidden to teachers and permitted for workshop admins
+  test_workshop_admin_only :get, :find_user, :success
 end
