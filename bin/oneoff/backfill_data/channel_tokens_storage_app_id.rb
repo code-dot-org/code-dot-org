cede388af5cfd98e01644f09d8c3743a613e850d
ashercodeorg <asher@code.org>
2017-06-27 11:49:21 -0500

    Add transactions to storage_app_id backfill script.

--- bin/oneoff/backfill_data/channel_tokens_storage_app_id.rb
+++ bin/oneoff/backfill_data/channel_tokens_storage_app_id.rb
@@ -5,8 +5,18 @@
 require_relative '../../../dashboard/config/environment'
 require_relative '../../../shared/middleware/helpers/storage_id'
 
-ChannelToken.find_each do |channel_token|
-  _owner_id, channel_id = storage_decrypt_channel_id(channel_token.channel)
-  channel_token.storage_app_id = channel_id
-  channel_token.save!
+slice = 0
+
+ChannelToken.find_in_batches(batch_size: 500) do |channel_tokens_batch|
+  puts "PROCESSING: slice #{slice}..."
+  ActiveRecord::Base.transaction do
+    channel_tokens_batch.each do |channel_token|
+      _owner_id, channel_id = storage_decrypt_channel_id(channel_token.channel)
+      channel_token.storage_app_id = channel_id
+      channel_token.save!
+    end
+  end
+  puts "PROCESSED: slice #{slice}..."
+
+  slice += 1
 end
