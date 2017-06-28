7673c61643f7fc2218d4914397206525e034fa6f
Will Jordan <will@code.org>
2017-06-28 12:13:07 -0700

    format use_min_stylesheet?

--- pegasus/helpers/page_helpers.rb
+++ pegasus/helpers/page_helpers.rb
@@ -48,14 +48,28 @@ def inline_css(css)
 end
 
 def use_min_stylesheet?
-  pages_verified = {'Anyone can learn' => '/', 'About Us' => '/about', 'Donors' => '/about/donors',
-    'Leadership' => '/about/leadership', 'Partners' => '/about/partners', 'Educator Overview' => '/educate',
-    'Community and Support' => '/educate/community', 'Computer Science Principles' => '/educate/csp',
-    'CS Fundamentals for grades K-5' => '/educate/curriculum/elementary-school',
-    '3rd Party Educator Resources' => '/educate/curriculum/3rd-party', 'Tools and videos' => '/educate/resources/videos',
-    'How to help' => '/help', 'Teacher Resources-Star Wars' => '/hourofcode/starwars',
-    'Teacher Resources-MINECRAFT' => '/hourofcode/mc', 'Learn' => '/learn', 'Promote Computer Science' => '/promote',
-    'Minecraft' => '/minecraft', 'Star Wars' => '/starwars', 'Student Overview' => '/student', 'Help Translate' => '/translate'}
+  pages_verified = {
+    'Anyone can learn': '/',
+    'About Us': '/about',
+    'Donors': '/about/donors',
+    'Leadership': '/about/leadership',
+    'Partners': '/about/partners',
+    'Educator Overview': '/educate',
+    'Community and Support': '/educate/community',
+    'Computer Science Principles': '/educate/csp',
+    'CS Fundamentals for grades K-5': '/educate/curriculum/elementary-school',
+    '3rd Party Educator Resources': '/educate/curriculum/3rd-party',
+    'Tools and videos': '/educate/resources/videos',
+    'How to help': '/help',
+    'Teacher Resources-Star Wars': '/hourofcode/starwars',
+    'Teacher Resources-MINECRAFT': '/hourofcode/mc',
+    'Learn': '/learn',
+    'Promote Computer Science': '/promote',
+    'Minecraft': '/minecraft',
+    'Star Wars': '/starwars',
+    'Student Overview': '/student',
+    'Help Translate': '/translate'
+  }
   pages_verified.value?(request.path_info)
 end
 
