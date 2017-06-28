95dee27c33100b5dbe0d2649cd894981ddb65f0b
Brendan Reville <brendan@code.org>
2017-06-29 09:30:58 +1000

    RTL: fix one story to pass required parameter

--- apps/src/templates/studioHomepages/TeacherAssignablesCatalog.story.jsx
+++ apps/src/templates/studioHomepages/TeacherAssignablesCatalog.story.jsx
@@ -9,7 +9,10 @@ export default storybook => {
         name: 'Catalog of courses and scripts (assignables) for teachers',
         description: `Collection of ResourceCards displaying information about courses and scripts for different grade levels that will be on /courses for teachers`,
         story: () => (
-          <TeacherAssignablesCatalog codeOrgUrlPrefix="http://localhost:3000/"/>
+          <TeacherAssignablesCatalog
+            codeOrgUrlPrefix="http://localhost:3000/"
+            rtl={false}
+          />
         )
       },
     ]);
