b7d9bf465bf9f51f97ed61e3363187d9eec28bc8
Mehal Shah <mehal@code.org>
2017-06-29 11:43:34 -0700

    Checkpoint

--- dashboard/app/helpers/pd/workshop_survey_results_helper.rb
+++ dashboard/app/helpers/pd/workshop_survey_results_helper.rb
@@ -1,20 +1,19 @@
 module Pd::WorkshopSurveyResultsHelper
   # Summarize an array of workshop surveys that are of the same type
-  # @param surveys List of either TeacherconSurveys of LocalSummerWorkshopSurveys
+  # @param surveys List of TeacherconSurveys
   # @param options List of the questions the survey uses.
   # @returns Hash representing an average of all the respones, or array of free text responses
   def summarize_workshop_surveys(surveys, options)
     # Works on arrays where everything is either a teachercon survey or workshop survey
     # (but not both)
-    raise 'Cannot summarize different kinds of workshop surveys' unless
-      surveys.all? {|survey| survey.is_a? Pd::TeacherconSurvey} ||
-      surveys.all? {|survey| survey.is_a? Pd::LocalSummerWorkshopSurvey}
+    raise 'Currently just sumarizes Teachercon surveys' unless
+      surveys.all? {|survey| survey.is_a? Pd::TeacherconSurvey}
 
     # Hash representing overall score sums
     sum_hash = Hash.new(0)
 
     surveys.each do |response|
-      response_hash = JSON.parse(response.form_data).transform_keys {|k| k.underscore.to_sym}
+      response_hash = response.sanitize_form_data_hash
 
       response_hash.each do |k, v|
         # Is this a multiple choice question?
@@ -22,7 +21,15 @@ module Pd::WorkshopSurveyResultsHelper
           sum_hash[k] += options[k].index(v) + 1
         else
           # If not, concat it to the list of responses
-          sum_hash[k] == 0 ? sum_hash[k] = [v] : sum_hash[k] << v
+          if v.is_a? Hash
+            if sum_hash[k] == 0
+              sum_hash[k] = [facilitator_hash_to_string(v)]
+            else
+              sum_hash.concat facilitator_hash_to_string(v)
+            end
+          else
+            sum_hash[k] == 0 ? sum_hash[k] = [v] : sum_hash[k] << v
+          end
         end
       end
     end
@@ -32,5 +39,11 @@ module Pd::WorkshopSurveyResultsHelper
         sum_hash[k] = v / surveys.count.to_f
       end
     end
+  end
+
+  private
+
+  def convert_facilitator_hash_to_string(facilitator_hash)
+    facilitator_hash.map {|k, v| "#{k}: #{v}"}
   end
 end
--- dashboard/test/helpers/workshop_survey_results_helper_test.rb
+++ dashboard/test/helpers/workshop_survey_results_helper_test.rb
@@ -4,6 +4,8 @@ class Pd::WorkshopSurveyResultsHelperTest < ActionView::TestCase
   include Pd::WorkshopSurveyResultsHelper
 
   test 'summarize summarizes teachercons as expected' do
+    enrollment = create :pd_enrollment
+    enrollment.workshop.facilitators << create(:faciliator, name: 'Facilitator Bob')
     survey_1 = create :pd_teachercon_survey
     hash_2 = build :pd_teachercon_survey_hash
     hash_2[:personalLearningNeedsMet] = 'Strongly Disagree'
@@ -15,19 +17,6 @@ class Pd::WorkshopSurveyResultsHelperTest < ActionView::TestCase
     assert_equal 3.5, result_hash[:personal_learning_needs_met]
     assert_equal 1, result_hash[:have_ideas_about_formative]
     assert_equal ['venue feedback', 'more venue feedback'], result_hash[:venue_feedback]
-  end
-
-  test 'summarize summarizes workshop surveys as expected' do
-    survey_1 = create :pd_local_summer_workshop_survey
-    hash_2 = build :pd_local_summer_workshop_survey_hash
-    hash_2[:receivedClearCommunication] = 'Strongly Disagree'
-    hash_2[:venueFeedback] = 'venue was great'
-    survey_2 = create :pd_local_summer_workshop_survey, form_data: hash_2.to_json
-
-    result_hash = summarize_workshop_surveys([survey_1, survey_2], Pd::LocalSummerWorkshopSurvey.options)
-    assert_equal 3.5, result_hash[:received_clear_communication]
-    assert_equal 5, result_hash[:how_much_learned]
-    assert_equal ['venue feedback', 'venue was great'], result_hash[:venue_feedback]
   end
 
   test 'get an error if summarizing a mix of workshop surveys' do
