4a337b285d109480f7eb0b4f21601b6f41a70da7
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-06-29 15:30:08 -0700

    update MazeThumbnail to allow scaling of svg

--- apps/src/code-studio/components/stageExtras/MazeThumbnail.jsx
+++ apps/src/code-studio/components/stageExtras/MazeThumbnail.jsx
@@ -12,6 +12,11 @@ export default class MazeThumbnail extends React.Component {
     map: React.PropTypes.array.isRequired,
     skin: React.PropTypes.string.isRequired,
     startDirection: React.PropTypes.number.isRequired,
+    scale: React.PropTypes.number
+  }
+
+  static defaultProps = {
+    scale: 1
   }
 
   componentDidMount() {
@@ -34,11 +39,22 @@ export default class MazeThumbnail extends React.Component {
   render() {
     return (
       <ProtectedStatefulDiv>
-        <svg
-          width="400"
-          height="400"
-          ref={c => {this.svg = c;}}
-        />
+        <div
+          style={{
+            maxWidth: 400 * this.props.scale,
+            maxHeight: 400 * this.props.scale
+          }}
+        >
+          <svg
+            width="400"
+            height="400"
+            ref={c => {this.svg = c;}}
+            style={{
+              transform: `scale(${this.props.scale})`,
+              transformOrigin: '0 0'
+            }}
+          />
+        </div>
       </ProtectedStatefulDiv>
     );
   }
