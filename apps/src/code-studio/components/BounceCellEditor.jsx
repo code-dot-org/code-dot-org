aefd214a64e84317e76d5e2deba63d998cd04f38
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-06-29 12:12:39 -0700

    update BounceCellEditor to extend CellEditor

--- apps/src/code-studio/components/BounceCellEditor.jsx
+++ apps/src/code-studio/components/BounceCellEditor.jsx
@@ -4,56 +4,19 @@
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import CellEditor from './CellEditor';
 import { SquareType } from '@cdo/apps/bounce/tiles';
 
-export default class BounceCellEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
+export default class BounceCellEditor extends CellEditor  {
 
-  handleChange(event) {
-    const serializedArray = $(ReactDOM.findDOMNode(this)).serializeArray();
-    const values = serializedArray.reduce((prev, curr) => {
-      const value = isNaN(curr.value) ? undefined : Number(curr.value);
-      prev[curr.name] = value;
-      return prev;
-    }, {});
-    this.props.onUpdate(values);
-  }
-
-  render() {
-    const values = this.props.cell.serialize();
-
-    // We want undefined values that are going to be in <selects> to
-    // actually be the STRING 'undefined' rather than the value.
-    ['tileType'].forEach(function (value) {
-      if (values[value] === undefined) {
-        values[value] = 'undefined';
-      }
-    });
-
+  /**
+   * @override
+   */
+  renderFields(values) {
     return (
-      <form className="span4 offset1">
-        <header>
-          <strong>Editing Cell ({this.props.row}, {this.props.col})</strong>
-        </header>
-
-        <label htmlFor="tileType">Tile Type (required):</label>
-        <select name="tileType" value={values.tileType} onChange={this.handleChange}>
-          {Object.keys(SquareType).map(type => (
-            <option key={type} value={SquareType[type]}>{type.toLowerCase()}</option>
-          ))}
-        </select>
-      </form>
+      <div>
+        {super.renderTileTypes(values, SquareType)}
+      </div>
     );
   }
 }
-
-BounceCellEditor.propTypes = {
-  cell: React.PropTypes.object.isRequired,
-  row: React.PropTypes.number.isRequired,
-  col: React.PropTypes.number.isRequired,
-  onUpdate: React.PropTypes.func.isRequired,
-};
