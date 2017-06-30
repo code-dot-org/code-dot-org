a5d55409a20cc4427cb1aa091457f1f6baf14175
Suresh Chanmugam <me@suresh.com>
2017-06-30 09:59:11 -0700

    test assign and remove permission

--- dashboard/test/controllers/pd/user_admin_controller_test.rb
+++ dashboard/test/controllers/pd/user_admin_controller_test.rb
@@ -5,9 +5,12 @@ class Pd::UserAdminControllerTest < ::ActionController::TestCase
   setup_all do
     @workshop_admin = create :workshop_admin
     @teacher = create :teacher
+    @student = create :student, name: 'student'
+    @facilitator = create :facilitator
   end
 
   def self.test_workshop_admin_only(method, action, success_response, params = nil)
+    test_user_gets_response_for action, user: :student, method: method, params: params, response: :forbidden
     test_user_gets_response_for action, user: -> {@teacher}, method: method, params: params, response: :forbidden
     test_user_gets_response_for action, user: -> {@workshop_admin}, method: method, params: params, response: success_response
   end
@@ -16,4 +19,20 @@ class Pd::UserAdminControllerTest < ::ActionController::TestCase
   test_redirect_to_sign_in_for :find_user
   # test find_user start page is forbidden to teachers and permitted for workshop admins
   test_workshop_admin_only :get, :find_user, :success
+
+  test 'assign workshop admin permission to teacher' do
+    #refute @teacher.permission?(UserPermission::WORKSHOP_ADMIN), "User already has Workshop Admin permission"
+    sign_in @workshop_admin
+    get :assign_permission, params: {user_id: @teacher.id, pd_user_permission_id: UserPermission::WORKSHOP_ADMIN}
+    assert_redirected_to action: :find_user, params: {search_term: @teacher.id}
+    assert @teacher.permission?(UserPermission::WORKSHOP_ADMIN), "User does not have Workshop Admin permission"
+  end
+
+  test 'remove facilitator permission from user' do
+    #assert @facilitator.permission?(UserPermission::FACILITATOR)
+    sign_in @workshop_admin
+    get :remove_permission, params: {user_id: @facilitator.id, pd_user_permission_id: UserPermission::FACILITATOR}
+    assert_redirected_to action: :find_user, params: {search_term: @facilitator.id}
+    refute @facilitator.permission?(UserPermission::FACILITATOR), "User still has the Facilitator permission"
+  end
 end
