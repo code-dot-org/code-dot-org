25168a51823491045ce3e95d76026045fe75f66e
Josh Lory <josh.lory@code.org>
2017-07-06 13:30:15 -0700

    Show a link to authorize Google Classroom when rosters fail to load

--- apps/src/templates/teacherDashboard/googleClassroomRedux.js
+++ apps/src/templates/teacherDashboard/googleClassroomRedux.js
@@ -1,11 +1,17 @@
 const SET_CLASSROOM_LIST = 'teacherDashboard/SET_CLASSROOM_LIST';
+const FAILED_LOAD = 'teacherDashboard/FAILED_LOAD';
 
 export const loadClassroomList = () => {
   return dispatch => {
-    $.ajax('/dashboardapi/google_classrooms').success(response => dispatch(setClassroomList(response.courses)));
+    $.ajax('/dashboardapi/google_classrooms')
+      .success(response => dispatch(setClassroomList(response.courses)))
+      .fail(() => dispatch(failedLoad()));
   };
 };
+
 export const setClassroomList = classrooms => ({ type: SET_CLASSROOM_LIST, classrooms });
+
+export const failedLoad = () => ({ type: FAILED_LOAD });
 
 const initialState = {
   classrooms: null,
@@ -16,6 +22,13 @@ export default function googleClassroom(state = initialState, action) {
     return {
       ...state,
       classrooms: action.classrooms.slice(),
+    };
+  }
+
+  if (action.type === FAILED_LOAD) {
+    return {
+      ...state,
+      classrooms: false,
     };
   }
 
