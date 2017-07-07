7e4e3da72b0d58f5d858c3ed89f1820f241ca256
Suresh Chanmugam <me@suresh.com>
2017-07-07 06:43:47 -0700

    Add basic Find Facilitator functionality

--- dashboard/app/controllers/pd/workshop_user_admin_controller.rb
+++ dashboard/app/controllers/pd/workshop_user_admin_controller.rb
@@ -1,4 +1,25 @@
 class Pd::WorkshopUserAdminController < ApplicationController
+  authorize_resource class: :pd_workshop_user_admin
+
+  # display the Facilitator search form, Facilitators found, and Courses for first Facilitator found
+  # assign / remove Courses actions redirect back to this form to display the updated course list
   def facilitator_courses_form
+    search_term = facilitator_course_params[:search_term]
+    if search_term =~ /^\d+$/
+      user_id = search_term
+      @facilitators = User.where(id: user_id)
+    elsif search_term
+      email = search_term
+      hashed_email = User.hash_email(email)
+      @facilitators = User.where(hashed_email: hashed_email)
+    end
+    @facilitator = @facilitators.first if @facilitators
+  end
+
+  private
+
+  # white list permitted request parameters
+  def facilitator_course_params
+    params.permit(:search_term, :user_id, :course)
   end
 end
--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -395,6 +395,8 @@ Dashboard::Application.routes.draw do
     post 'attend/:session_code/upgrade', controller: 'session_attendance', action: 'confirm_upgrade_account'
 
     get 'workshop_user_admin/facilitator_courses', controller: 'workshop_user_admin', action: 'facilitator_courses_form'
+    post 'workshop_user_admin/assign_course', controller: 'workshop_user_admin', action: 'assign_course'
+    get 'workshop_user_admin/remove_course', controller: 'workshop_user_admin', action: 'remove_course'
   end
 
   get '/dashboardapi/section_progress/:section_id', to: 'api#section_progress'
