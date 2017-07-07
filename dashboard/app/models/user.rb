09f4f9f2bf12d55bb7a77116f7893805a5b68841
ashercodeorg <asher@code.org>
2017-07-07 13:49:49 -0500

    Fix suffix generation to yield five character string.

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -1334,9 +1334,10 @@ class User < ActiveRecord::Base
   # WARNING: This (permanently) destroys data and cannot be undone.
   # WARNING: This does not purge the user, only marks them as such.
   def clear_user_and_mark_purged
+    random_suffix = (('0'..'9').to_a + ('a'..'z').to_a).sample(5).join
+
     self.name = nil
-    # The latter part yields a random string of length five from the characters 0 to 9 and a to z.
-    self.username = "#{SYSTEM_DELETED_USERNAME}_#{rand(36**5).to_s(36)}"
+    self.username = "#{SYSTEM_DELETED_USERNAME}_#{random_suffix}"
     self.current_sign_in_ip = nil
     self.last_sign_in_ip = nil
     self.email = ''
