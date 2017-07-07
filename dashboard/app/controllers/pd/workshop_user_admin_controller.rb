d1241669117c807e0ffbb9d9600ee60b50045bef
Suresh Chanmugam <me@suresh.com>
2017-07-07 08:29:55 -0700

    implement assign_course and remove_course actions

--- dashboard/app/controllers/pd/workshop_user_admin_controller.rb
+++ dashboard/app/controllers/pd/workshop_user_admin_controller.rb
@@ -6,20 +6,36 @@ class Pd::WorkshopUserAdminController < ApplicationController
   def facilitator_courses_form
     search_term = facilitator_course_params[:search_term]
     if search_term =~ /^\d+$/
-      user_id = search_term
-      @facilitators = User.where(id: user_id)
+      facilitator_id = search_term
+      @facilitators = User.where(id: facilitator_id)
     elsif search_term
       email = search_term
       hashed_email = User.hash_email(email)
       @facilitators = User.where(hashed_email: hashed_email)
     end
     @facilitator = @facilitators.first if @facilitators
+    @courses = Pd::CourseFacilitator.where(facilitator_id: @facilitator.id)
+  end
+
+  def assign_course
+    facilitator_id = facilitator_course_params[:facilitator_id]
+    course = facilitator_course_params[:course]
+    Pd::CourseFacilitator.create(facilitator_id: facilitator_id, course: course)
+    redirect_to action: "facilitator_courses_form", search_term: facilitator_id
+  end
+
+  def remove_course
+    facilitator_id = facilitator_course_params[:facilitator_id]
+    course_facilitator_id = facilitator_course_params[:course_facilitator_id]
+    facilitator_course = Pd::CourseFacilitator.find(course_facilitator_id)
+    facilitator_course.destroy
+    redirect_to action: "facilitator_courses_form", search_term: facilitator_id
   end
 
   private
 
   # white list permitted request parameters
   def facilitator_course_params
-    params.permit(:search_term, :user_id, :course)
+    params.permit(:search_term, :facilitator_id, :course, :course_facilitator_id)
   end
 end
