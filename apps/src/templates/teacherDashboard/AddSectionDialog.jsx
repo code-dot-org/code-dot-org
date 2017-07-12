60253fc9e94d53ca549b91a65125e97149baece7
Brad Buchanan <brad@code.org>
2017-07-12 11:23:18 -0700

    BaseDialog can take fixedWith and fixedHeight
    pair: epeach

--- apps/src/templates/teacherDashboard/AddSectionDialog.jsx
+++ apps/src/templates/teacherDashboard/AddSectionDialog.jsx
@@ -5,54 +5,7 @@ import color from '../../util/color';
 import AddInitialStudentsView from './AddInitialStudentsView';
 
 const styles = {
-  title: {
-    position: 'absolute',
-    left: 20,
-    color: color.dark_charcoal,
-    margin: '15px 0',
-  },
-  content: {
-    position: 'absolute',
-    left: 20,
-    top: 50,
-    right: 20,
-    bottom: 70,
-    overflowY: 'scroll',
-  },
-  classroomRow: {
-    padding: 10,
-    cursor: 'pointer',
-  },
-  highlightRow: {
-    backgroundColor: color.default_blue,
-    color: color.white,
-  },
-  footer: {
-    position: 'absolute',
-    bottom: 15,
-    right: 20,
-    left: 20,
-  },
-  buttonPrimary: {
-    float: 'right',
-    background: color.orange,
-    color: color.white,
-    border: '1px solid #b07202',
-    borderRadius: 3,
-    boxShadow: 'inset 0 1px 0 0 rgba(255, 255, 255, 0.63)',
-    fontSize: 14,
-    padding: '8px 20px',
-  },
-  buttonSecondary: {
-    float: 'left',
-    background: '#eee',
-    color: '#5b6770',
-    border: '1px solid #c5c5c5',
-  },
-  error: {
-    fontSize: 10,
-    color: color.light_gray,
-  },
+
 };
 
 export default class AddSectionDialog extends Component {
@@ -71,6 +24,7 @@ export default class AddSectionDialog extends Component {
     return (
       <BaseDialog
         useUpdatedStyles
+        fixedWidth={1010}
         isOpen={this.props.isOpen}
         handleClose={this.handleClose}
         assetUrl={() => ''}
