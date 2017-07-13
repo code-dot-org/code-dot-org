54769208035d81e5f8f22ce8e100c1828e9bd501
Ram Kandasamy <ram@code.org>
2017-07-13 13:31:34 -0700

    Include explicit check for freePlay levels

--- apps/src/studio/studio.js
+++ apps/src/studio/studio.js
@@ -2748,7 +2748,7 @@ Studio.runButtonClick = function () {
 Studio.displayFeedback = function () {
   var tryAgainText;
   // For free play, show keep playing, unless it's a big game level
-  if (Studio.testResults >= TestResults.MINIMUM_OPTIMAL_RESULT &&
+  if ((level.freePlay || Studio.testResults >= TestResults.MINIMUM_OPTIMAL_RESULT) &&
       !(Studio.customLogic instanceof BigGameLogic)) {
     tryAgainText = commonMsg.keepPlaying();
   } else {
