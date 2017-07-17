95e5fb46a3147e5e4ed0078d0c32922151101b12
Brad Buchanan <brad@code.org>
2017-07-17 13:11:51 -0700

    Introduce index action to SectionsController

--- dashboard/app/controllers/sections_controller.rb
+++ dashboard/app/controllers/sections_controller.rb
@@ -1,5 +1,17 @@
 class SectionsController < ApplicationController
-  before_action :load_section_by_code, except: :update
+  layout false, only: :index
+  before_action :load_section_by_code, only: [:log_in, :show]
+
+  # Get the set of sections owned by the current user
+  def index
+    if !current_user
+      head :forbidden
+    elsif current_user.teacher?
+      render json: current_user.sections.map(&:summarize)
+    else
+      render json: []
+    end
+  end
 
   def show
     @secret_pictures = SecretPicture.all.shuffle
