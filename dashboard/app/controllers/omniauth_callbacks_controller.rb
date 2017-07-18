c1c823b4f8976cb69b5f64816b4c76218e9a09a4
eric <ewjordan@gmail.com>
2017-07-18 02:35:14 -0700

    Allow silent login via oauth for Google, Microsoft, and Facebook accounts

--- dashboard/app/controllers/omniauth_callbacks_controller.rb
+++ dashboard/app/controllers/omniauth_callbacks_controller.rb
@@ -12,8 +12,10 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
 
     if @user.persisted?
-      flash.notice = I18n.t('auth.signed_in')
-      sign_in_and_redirect @user
+      sign_in_user
+    elsif allows_silent_takeover(@user.provider) && User.find_by_email_or_hashed_email(@user.email)
+      @user = User.find_by_email_or_hashed_email(@user.email)
+      sign_in_user
     elsif User.find_by_email_or_hashed_email(@user.email)
       # Note that @user.email is populated by User.from_omniauth even for students
       redirect_to "/users/sign_in?providerNotLinked=#{@user.provider}&email=#{@user.email}"
@@ -25,5 +27,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   User::OAUTH_PROVIDERS.each do |provider|
     alias_method provider.to_sym, :all
+  end
+
+  private
+
+  def sign_in_user
+    flash.notice = I18n.t('auth.signed_in')
+    sign_in_and_redirect @user
+  end
+
+  def allows_silent_takeover(provider)
+    %w(facebook google_oauth2 windowslive).include?(provider)
   end
 end
