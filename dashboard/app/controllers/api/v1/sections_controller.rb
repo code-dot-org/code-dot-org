12f8c5525bf1eb8b675b9f579174a3da8fed1078
Brad Buchanan <brad@code.org>
2017-07-17 17:43:53 -0700

    Comments

--- dashboard/app/controllers/api/v1/sections_controller.rb
+++ dashboard/app/controllers/api/v1/sections_controller.rb
@@ -1,12 +1,15 @@
 class Api::V1::SectionsController < ApplicationController
   layout false
 
+  # GET /api/v1/sections
   # Get the set of sections owned by the current user
   def index
     return head :forbidden unless current_user
     render json: current_user.sections.map(&:summarize)
   end
 
+  # GET /api/v1/sections/<id>
+  # Get complete details of a particular section
   def show
     if !current_user || !current_user.teacher?
       head :forbidden
