e065afa66c86f9bfd5e7ff6f68cd9b9878e0a6ed
Brad Buchanan <brad@code.org>
2017-07-18 15:57:02 -0700

    User summarizes for API, controller merges in passed level count

--- dashboard/app/controllers/api/v1/sections_students_controller.rb
+++ dashboard/app/controllers/api/v1/sections_students_controller.rb
@@ -7,6 +7,12 @@ class Api::V1::SectionsStudentsController < ApplicationController
 
     section = Section.find(params[:section_id])
     authorize! :manage, section
-    render json: section.students.map(&:summarize)
+
+    passing_level_counts = UserLevel.count_passed_levels_for_users(section.students.map(&:id))
+    render json: (section.students.map do |student|
+      student.summarize.merge(
+        completed_levels_count: passing_level_counts[student.id] || 0,
+      )
+    end)
   end
 end
