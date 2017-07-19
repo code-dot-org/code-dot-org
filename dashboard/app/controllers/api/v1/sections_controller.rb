2f07d90f751100ffc5ce2811d7023d8cdf9e6bf4
Brad Buchanan <brad@code.org>
2017-07-18 17:03:05 -0700

    Add Section create action (though incomplete)

--- dashboard/app/controllers/api/v1/sections_controller.rb
+++ dashboard/app/controllers/api/v1/sections_controller.rb
@@ -12,4 +12,27 @@ class Api::V1::SectionsController < Api::V1::JsonApiController
   def show
     render json: @section.summarize
   end
+
+  # POST /api/v1/sections
+  # Create a new section
+  def create
+    authorize! :create, Section
+    section = Section.create!(
+      {
+        user_id: current_user.id,
+        name: !params[:name].to_s.empty? ? params[:name].to_s : 'New Section',
+        login_type: params[:login_type].to_s == 'none' ? 'email' : params[:login_type].to_s,
+        grade: Section.valid_grade?(params[:grade].to_s) ? params[:grade].to_s : nil,
+        script_id: params[:script] && valid_script_id?(params[:script][:id]) ?
+          params[:script][:id].to_i : params[:script_id],
+        course_id: params[:course_id] && valid_course_id?(params[:course_id]) ?
+          params[:course_id].to_i : nil,
+        code: CodeGeneration.random_unique_code(length: 6),
+        stage_extras: params[:stage_extras] ? params[:stage_extras] : false,
+        pairing_allowed: params[:pairing_allowed].nil? ? true : params[:pairing_allowed]
+      }
+    )
+
+    render json: section.summarize
+  end
 end
