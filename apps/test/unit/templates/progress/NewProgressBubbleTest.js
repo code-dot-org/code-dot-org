8f3b2e093bda03148a1b87088bf369de4e3ffa0c
Brent Van Minnen <bjvanminnen@gmail.com>
2017-07-19 23:31:40 -0700

    Fix test

--- apps/test/unit/templates/progress/NewProgressBubbleTest.js
+++ apps/test/unit/templates/progress/NewProgressBubbleTest.js
@@ -218,7 +218,7 @@ describe('NewProgressBubble', () => {
       />
     );
     assert.equal(wrapper.find('ProgressPill').length, 1);
-    assert(!!wrapper.find('ProgressPill').props.tooltip);
+    assert(!!wrapper.find('ProgressPill').props().tooltip);
   });
 
   it('does not render a progress pill for unplugged when small', () => {
