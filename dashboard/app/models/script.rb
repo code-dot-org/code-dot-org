7292e53bf6d34e84ac67dd7cbfecffacdd2ad39b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-07-20 16:04:29 -0700

    Enable TTS for non-draft CSF2 courses
    Also expand the definition of `k1?` to include non-draft A&B

--- dashboard/app/models/script.rb
+++ dashboard/app/models/script.rb
@@ -407,7 +407,16 @@ class Script < ActiveRecord::Base
     [
       Script::COURSEA_DRAFT_NAME,
       Script::COURSEB_DRAFT_NAME,
+      Script::COURSEA_NAME,
+      Script::COURSEB_NAME,
       Script::COURSE1_NAME
+    ].include?(name)
+  end
+
+  private def csf_tts_level?
+    k1? || [
+      Script::COURSEC_NAME,
+      Script::COURSEC_DRAFT_NAME,
     ].include?(name)
   end
 
@@ -429,7 +438,7 @@ class Script < ActiveRecord::Base
   end
 
   def text_to_speech_enabled?
-    k1? || name == Script::COURSEC_DRAFT_NAME || csd_tts_level? || csp_tts_level? || name == Script::TTS_NAME
+    csf_tts_level? || csd_tts_level? || csp_tts_level? || name == Script::TTS_NAME
   end
 
   def hide_solutions?
