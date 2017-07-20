fb35947f41d341eefbce891dfb51e491c5b411f5
Brad Buchanan <brad@code.org>
2017-07-20 08:32:41 -0700

    Use load_and_authorize_resource

--- dashboard/app/controllers/api/v1/sections_students_controller.rb
+++ dashboard/app/controllers/api/v1/sections_students_controller.rb
@@ -1,15 +1,16 @@
 class Api::V1::SectionsStudentsController < ApplicationController
+  load_and_authorize_resource :section
   layout false
+
+  # Don't bother redirecting to login when denying access to the JSON APIs
+  rescue_from CanCan::AccessDenied do
+    head :forbidden
+  end
 
   # GET /sections/<section_id>/students
   def index
-    return head :forbidden unless current_user
-
-    section = Section.find(params[:section_id])
-    authorize! :manage, section
-
-    passing_level_counts = UserLevel.count_passed_levels_for_users(section.students.map(&:id))
-    render json: (section.students.map do |student|
+    passing_level_counts = UserLevel.count_passed_levels_for_users(@section.students.map(&:id))
+    render json: (@section.students.map do |student|
       student.summarize.merge(
         completed_levels_count: passing_level_counts[student.id] || 0,
       )
