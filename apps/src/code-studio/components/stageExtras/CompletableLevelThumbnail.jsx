171b3551b345bcafeb69eab4d4eb6f099e404d5c
Josh Lory <josh.lory@code.org>
2017-07-20 14:51:25 -0700

    Cleanup, make completed status settable via props

--- apps/src/code-studio/components/stageExtras/CompletableLevelThumbnail.jsx
+++ apps/src/code-studio/components/stageExtras/CompletableLevelThumbnail.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
+const styles = {
+  wrapper: {
+    display: 'inline-block',
+    position: 'relative',
+    transformOrigin: '0 0',
+  },
+  overlay: {
+    width: 400,
+    height: 400,
+    backgroundColor: 'rgba(0, 255, 0, 0.3)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    textAlign: 'center',
+  },
+  check: {
+    fontSize: 350,
+    lineHeight: `400px`,
+    color: '#fff',
+    opacity: '0.8',
+  },
+};
+
 export default class CompletableLevelThumbnail extends React.Component {
   static propTypes = {
     children: React.PropTypes.element.isRequired,
     width: React.PropTypes.number,
+    completed: React.PropTypes.boolean,
   }
 
   render() {
@@ -11,34 +35,19 @@ export default class CompletableLevelThumbnail extends React.Component {
     return (
       <div
         style={{
-          display: 'inline-block',
-          position: 'relative',
           transform: `scale(${scale})`,
-          transformOrigin: '0 0',
+          ...styles.wrapper
         }}
       >
         {this.props.children}
-        <div
-          style={{
-            width: 400,
-            height: 400,
-            backgroundColor: 'rgba(0, 255, 0, 0.3)',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            textAlign: 'center',
-          }}
-        >
-          <i
-            className="fa fa-check"
-            style={{
-              fontSize: 350,
-              lineHeight: `400px`,
-              color: '#fff',
-              opacity: '0.8',
-            }}
-          />
-        </div>
+        {this.props.completed &&
+          <div style={styles.overlay}>
+            <i
+              className="fa fa-check"
+              style={styles.check}
+            />
+          </div>
+        }
       </div>
     );
   }
