09d9c7fad99df3a33afb27dd305539dc606d3bae
Josh Lory <josh.lory@code.org>
2017-07-20 15:12:06 -0700

    Update stories

--- apps/src/code-studio/components/stageExtras/CompletableLevelThumbnail.story.jsx
+++ apps/src/code-studio/components/stageExtras/CompletableLevelThumbnail.story.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CompletableLevelThumbnail from './CompletableLevelThumbnail';
 import MazeThumbnail from './MazeThumbnail';
 
-const sampleMap = [
+const sampleMapA = [
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 1, 0, 1, 0, 0],
   [0, 0, 2, 1, 1, 1, 0, 0],
@@ -13,20 +13,49 @@ const sampleMap = [
   [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
+const sampleMapB = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 4, 2, 4, 0],
+  [0, 0, 0, 4, 1, 1, 0, 0],
+  [0, 4, 4, 1, 1, 1, 0, 0],
+  [0, 0, 1, 1, 0, 1, 4, 0],
+  [0, 0, 1, 1, 0, 4, 0, 0],
+  [0, 0, 1, 3, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+];
+
 export default storybook => {
   storybook
     .storiesOf('CompletableLevelThumbnail', module)
-    .addStoryTable([{
-      name: 'Angry Birds',
-      description: 'This is the CompletableLevelThumbnail component.',
-      story: () => (
-        <CompletableLevelThumbnail width={200} completed={true}>
-          <MazeThumbnail
-            map={sampleMap}
-            startDirection={1}
-            skin="birds"
-          />
-        </CompletableLevelThumbnail>
+    .addWithInfo(
+      'Overview',
+      'This is the CompletableLevelThumbnail component.',
+      () => (
+        <div>
+          <CompletableLevelThumbnail size={200} completed={true} style={{float: 'left'}}>
+            <MazeThumbnail
+              map={sampleMapA}
+              startDirection={1}
+              skin="birds"
+            />
+          </CompletableLevelThumbnail>
+          &nbsp;
+          <CompletableLevelThumbnail size={200} completed={true}>
+            <MazeThumbnail
+              map={sampleMapB}
+              startDirection={2}
+              skin="scrat"
+            />
+          </CompletableLevelThumbnail>
+          &nbsp;
+          <CompletableLevelThumbnail size={200} completed={false}>
+            <MazeThumbnail
+              map={sampleMapA}
+              startDirection={2}
+              skin="farmer_night"
+            />
+          </CompletableLevelThumbnail>
+        </div>
       ),
-    }]);
+    );
 };
