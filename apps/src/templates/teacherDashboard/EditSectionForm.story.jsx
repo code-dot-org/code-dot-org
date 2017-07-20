cefcb9d086d36dec5ad5734276f5fc77a4befd74
Brad Buchanan <brad@code.org>
2017-07-20 10:58:46 -0700

    Fixup storybooks

--- apps/src/templates/teacherDashboard/EditSectionForm.story.jsx
+++ apps/src/templates/teacherDashboard/EditSectionForm.story.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import EditSectionForm from "./EditSectionForm";
+import {UnconnectedEditSectionForm} from "./EditSectionForm";
 
 export default storybook => storybook
   .storiesOf('EditSectionForm', module)
   .add('Basic options', () => {
     return (
-      <EditSectionForm
-        handleSave={() => {}}
+      <UnconnectedEditSectionForm
+        handleSave={storybook.action('handleSave')}
+        handleClose={storybook.action('handleClose')}
+        handleName={storybook.action('handleName')}
+        handleGrade={storybook.action('handleGrade')}
+        handleExtras={storybook.action('handleExtras')}
+        handlePairing={storybook.action('handlePairing')}
+        assignmentRef={() => {}}
+        validGrades={['K', '1', '2', '3']}
+        validAssignments={{}}
+        primaryAssignmentIds={[]}
+        sections={{}}
       />
     );
   });
