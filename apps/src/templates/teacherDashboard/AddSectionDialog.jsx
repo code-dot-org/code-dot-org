4c499906f59880661780956bea7383997580552f
Brad Buchanan <brad@code.org>
2017-07-21 09:53:40 -0700

    Destructure state

--- apps/src/templates/teacherDashboard/AddSectionDialog.jsx
+++ apps/src/templates/teacherDashboard/AddSectionDialog.jsx
@@ -50,18 +50,19 @@ export class AddSectionDialog extends Component {
 
   handlePairingChange = (pairing) => {
     this.setState({pairing});
-  }
+  };
 
   onClickEditSave = () => {
     const {updateSection} = this.props;
+    const {name, loginType, grade, extras, pairing} = this.state;
 
     const selectedAssignment = this.assignment.getSelectedAssignment();
     const data = {
-      name: this.state.name,
-      login_type: this.state.loginType,
-      grade: this.state.grade,
-      stage_extras: this.state.extras === 'yes',
-      pairing_allowed: this.state.pairing === 'yes',
+      name: name,
+      login_type: loginType,
+      grade: grade,
+      stage_extras: extras === 'yes',
+      pairing_allowed: pairing === 'yes',
       course_id: selectedAssignment ? selectedAssignment.courseId : null,
     };
 
@@ -92,8 +93,9 @@ export class AddSectionDialog extends Component {
   };
 
   renderContent() {
+    const {name, grade, loginType, extras, pairing} = this.state;
     const title = i18n.newSection();
-    if (!this.state.loginType) {
+    if (!loginType) {
       return (
         <AddInitialStudentsView
           title={title}
@@ -108,13 +110,13 @@ export class AddSectionDialog extends Component {
           assignmentRef = {(element) => this.assignment = element}
           handleSave={this.onClickEditSave}
           handleClose={this.handleClose}
-          name={this.state.name}
+          name={name}
           handleName={this.handleNameChange}
-          grade={this.state.grade}
+          grade={grade}
           handleGrade={this.handleGradeChange}
-          extras={this.state.extras}
+          extras={extras}
           handleExtras={this.handleExtrasChange}
-          pairing={this.state.pairing}
+          pairing={pairing}
           handlePairing={this.handlePairingChange}
         />
       );
