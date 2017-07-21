0e349467703e8cbe9f8b5d0abc4169bea7c95a91
Josh Lory <josh.lory@code.org>
2017-07-21 16:09:00 -0700

    Don't allow manually adding students to OAuth-imported sections

--- dashboard/app/models/sections/omni_auth_section.rb
+++ dashboard/app/models/sections/omni_auth_section.rb
@@ -27,7 +27,7 @@
 
 class OmniAuthSection < Section
   def self.from_omniauth(code:, type:, owner_id:, students:)
-    oauth_section = where(code: code).first_or_create do |section|
+    oauth_section = where(code: code).first_or_create! do |section|
       section.name = 'New Section'
       section.user_id = owner_id
       section.login_type = type
@@ -41,5 +41,9 @@ class OmniAuthSection < Section
 
   def provider_managed?
     true
+  end
+
+  def add_student(_)
+    raise 'Cannot add student to a provider-managed section'
   end
 end
