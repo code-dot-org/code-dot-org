f97128bf60d0c12d8bc1efa991c66633491e704d
eric <ewjordan@gmail.com>
2017-07-26 12:22:33 -0700

    Fix incorrect comment

--- dashboard/app/mailers/teacher_mailer.rb
+++ dashboard/app/mailers/teacher_mailer.rb
@@ -1,7 +1,7 @@
 class TeacherMailer < ActionMailer::Base
   default from: 'Hadi Partovi <hadi_partovi@code.org>'
 
-  # Email a parent when a student upgrades to a username/password account by adding a parent email
+  # Send newly registered teachers a welcome email
   def new_teacher_email(teacher)
     @teacher = teacher
     mail to: teacher.email, subject: I18n.t('teacher_mailer.new_teacher_subject')
