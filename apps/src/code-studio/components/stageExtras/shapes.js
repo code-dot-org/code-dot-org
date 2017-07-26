7fba8756d418dbea1c42ba4e69ace4cfdcfd17dc
Josh Lory <josh.lory@code.org>
2017-07-26 13:13:33 -0700

    Use `CompletableLevelThumbnail` in Stage Extras

---
+++ apps/src/code-studio/components/stageExtras/shapes.js
@@ -0,0 +1,10 @@
+import { PropTypes } from 'react';
+
+export const bonusLevel = {
+  id: PropTypes.number.isRequired,
+  map: PropTypes.array,
+  name: PropTypes.string.isRequired,
+  skin: PropTypes.string,
+  startDirection: PropTypes.number,
+  type: PropTypes.string.isRequired,
+};
