ee0dc4735238f54e8ea8fe56f1f7bb6dcd889a1f
Caley Brock <caleybrock7@gmail.com>
2017-07-27 10:01:36 -0700

    Fix storybook errors

--- apps/src/templates/projects/generateFakePersonalProjects.js
+++ apps/src/templates/projects/generateFakePersonalProjects.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
 
 // Generate n fake personal projects for the project widget.
+// updatedAt data is normally a timestamp, but for storybook
+// display purposes, use a descriptive string
 export function generateFakePersonalProjects(n) {
   return _.range(n).map(i => (
     {
       name: "Personal " + i,
-      updatedAt: Date.now() - i * 60 * 1000,
+      updatedAt: 'a day ago',
       projectType: 'gamelab',
       id: 'abcd'
     }
