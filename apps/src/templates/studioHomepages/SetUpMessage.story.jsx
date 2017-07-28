9fdad94d60e327551e167156e01b28c7ebee9ac8
Brad Buchanan <brad@code.org>
2017-07-28 12:08:11 -0700

    Fix SetUpMessage.story.jsx

--- apps/src/templates/studioHomepages/SetUpMessage.story.jsx
+++ apps/src/templates/studioHomepages/SetUpMessage.story.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {getStore, registerReducers} from '@cdo/apps/redux';
+import teacherSections from '../teacherDashboard/teacherSectionsRedux';
 import SetUpMessage from './SetUpMessage';
 
 export default storybook => {
@@ -8,38 +11,53 @@ export default storybook => {
       {
         name: 'Set Up Message for Courses for Teachers',
         description: `Information box if the teacher doesn't have any courses yet`,
-        story: () => (
-          <SetUpMessage
-            type="courses"
-            codeOrgUrlPrefix="http://code.org/"
-            isRtl={false}
-            isTeacher={true}
-          />
-        )
+        story: () => {
+          registerReducers({teacherSections});
+          return (
+            <Provider store={getStore()}>
+              <SetUpMessage
+                type="courses"
+                codeOrgUrlPrefix="http://code.org/"
+                isRtl={false}
+                isTeacher={true}
+              />
+            </Provider>
+          );
+        }
       },
       {
         name: 'Set Up Message for Sections for Teachers',
         description: `Information box if the teacher doesn't have any sections yet`,
-        story: () => (
-          <SetUpMessage
-            type="sections"
-            codeOrgUrlPrefix="http://code.org/"
-            isRtl={false}
-            isTeacher={true}
-          />
-        )
+        story: () => {
+          registerReducers({teacherSections});
+          return (
+            <Provider store={getStore()}>
+              <SetUpMessage
+                type="sections"
+                codeOrgUrlPrefix="http://code.org/"
+                isRtl={false}
+                isTeacher={true}
+              />
+            </Provider>
+          );
+        }
       },
       {
         name: 'Set Up Message for Courses for Students',
         description: `Information box if the teacher doesn't have any courses yet`,
-        story: () => (
-          <SetUpMessage
-            type="courses"
-            codeOrgUrlPrefix="http://code.org/"
-            isRtl={false}
-            isTeacher={false}
-          />
-        )
+        story: () => {
+          registerReducers({teacherSections});
+          return (
+            <Provider store={getStore()}>
+              <SetUpMessage
+                type="courses"
+                codeOrgUrlPrefix="http://code.org/"
+                isRtl={false}
+                isTeacher={false}
+              />
+            </Provider>
+          );
+        }
       },
     ]);
 };
