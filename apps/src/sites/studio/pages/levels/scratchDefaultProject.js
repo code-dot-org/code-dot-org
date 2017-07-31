6827e6458522139655ef759ed308cc2af7cdb587
Josh Lory <josh.lory@code.org>
2017-07-31 12:24:21 -0700

    Update default Scratch project to SB3 format

--- apps/src/sites/studio/pages/levels/scratchDefaultProject.js
+++ apps/src/sites/studio/pages/levels/scratchDefaultProject.js
@@ -1,47 +1,72 @@
 export const scratchDefaultProject = JSON.stringify({
-  "objName": "Stage",
-  "sounds": [],
-  "costumes": [{
-    "costumeName": "backdrop1",
-    "baseLayerID": 2,
-    "baseLayerMD5": "739b5e2a2435f6e1ec2993791b423146.png",
-    "bitmapResolution": 1,
-    "rotationCenterX": 240,
-    "rotationCenterY": 180
-  }],
-  "currentCostumeIndex": 0,
-  "penLayerMD5": "5c81a336fab8be57adc039a8a2b33ca9.png",
-  "penLayerID": -1,
-  "tempoBPM": 60,
-  "videoAlpha": 0.5,
-  "children": [{
-    "objName": "Sprite1",
-    "sounds": [],
-    "costumes": [{
-      "costumeName": "costume1",
-      "baseLayerID": 0,
-      "baseLayerMD5": "09dc888b0b7df19f70d81588ae73420e.svg",
-      "bitmapResolution": 1,
-      "rotationCenterX": 47,
-      "rotationCenterY": 55
-    }, {
-      "costumeName": "costume2",
-      "baseLayerID": 1,
-      "baseLayerMD5": "3696356a03a8d938318876a593572843.svg",
-      "bitmapResolution": 1,
-      "rotationCenterX": 47,
-      "rotationCenterY": 55
-    }],
-    "currentCostumeIndex": 0,
-    "scratchX": 0,
-    "scratchY": 0,
-    "scale": 1,
-    "direction": 90,
-    "rotationStyle": "normal",
-    "isDraggable": false,
-    "indexInLibrary": 1,
-    "visible": true,
-    "spriteInfo": {
+  "targets": [
+    {
+      "name": "Stage",
+      "isStage": true,
+      "x": 0,
+      "y": 0,
+      "size": 100,
+      "direction": 90,
+      "draggable": false,
+      "currentCostume": 0,
+      "costumeCount": 1,
+      "visible": true,
+      "rotationStyle": "all around",
+      "blocks": {},
+      "variables": {},
+      "lists": {},
+      "costumes": [
+        {
+          "name": "backdrop1",
+          "bitmapResolution": 1,
+          "rotationCenterX": 240,
+          "rotationCenterY": 180,
+          "skinId": 2,
+          "assetId": "739b5e2a2435f6e1ec2993791b423146",
+          "dataFormat": "png"
+        }
+      ],
+      "sounds": []
+    },
+    {
+      "name": "Sprite1",
+      "isStage": false,
+      "x": 0,
+      "y": 0,
+      "size": 100,
+      "direction": 90,
+      "draggable": false,
+      "currentCostume": 0,
+      "costumeCount": 2,
+      "visible": true,
+      "rotationStyle": "all around",
+      "blocks": {},
+      "variables": {},
+      "lists": {},
+      "costumes": [
+        {
+          "name": "costume1",
+          "bitmapResolution": 1,
+          "rotationCenterX": 47,
+          "rotationCenterY": 55,
+          "skinId": 1,
+          "assetId": "09dc888b0b7df19f70d81588ae73420e",
+          "dataFormat": "svg"
+        },
+        {
+          "name": "costume2",
+          "bitmapResolution": 1,
+          "rotationCenterX": 47,
+          "rotationCenterY": 55,
+          "skinId": 0,
+          "assetId": "3696356a03a8d938318876a593572843",
+          "dataFormat": "svg"
+        }
+      ],
+      "sounds": []
     }
-  }]
+  ],
+  "meta": {
+    "semver": "3.0.0"
+  }
 });
