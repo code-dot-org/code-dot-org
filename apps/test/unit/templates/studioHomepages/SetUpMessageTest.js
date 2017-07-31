7c539bdf03c6281436f7833e6e311471490bd518
Brad Buchanan <brad@code.org>
2017-07-31 13:13:21 -0700

    Add SectionsSetUpMessage test

--- apps/test/unit/templates/studioHomepages/SetUpMessageTest.js
+++ apps/test/unit/templates/studioHomepages/SetUpMessageTest.js
@@ -3,6 +3,7 @@ import {mount} from 'enzyme';
 import {assert} from '../../../util/configuredChai';
 import {
   CoursesSetUpMessage,
+  SectionsSetUpMessage,
 } from '@cdo/apps/templates/studioHomepages/SetUpMessage';
 
 describe('CoursesSetUpMessage', () => {
@@ -16,5 +17,19 @@ describe('CoursesSetUpMessage', () => {
     assert.equal(coursesSetUpMessage.childAt(2).name(), 'Button');
     assert.equal(coursesSetUpMessage.childAt(2).props().href, '/courses');
     assert.equal(coursesSetUpMessage.childAt(2).props().text, 'Find a course');
+  });
+});
+
+describe('SectionsSetUpMessage', () => {
+  it('renders as expected for a teacher', () => {
+    const sectionsSetUpMessage = mount(
+      <SectionsSetUpMessage isRtl={false} codeOrgUrlPrefix="http://localhost:3000/"/>
+    );
+    assert.equal(sectionsSetUpMessage.name(), 'SectionsSetUpMessage');
+    assert.equal(sectionsSetUpMessage.childAt(0).text(), 'Set up your classroom');
+    assert.equal(sectionsSetUpMessage.childAt(1).text(), 'Create a new classroom section to start assigning courses and seeing your student progress.');
+    assert.equal(sectionsSetUpMessage.childAt(2).name(), 'Button');
+    assert.equal(sectionsSetUpMessage.childAt(2).props().href, 'http://localhost:3000//teacher-dashboard#/sections');
+    assert.equal(sectionsSetUpMessage.childAt(2).props().text, 'Create section');
   });
 });
