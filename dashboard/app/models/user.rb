00a605ffa161cfff8a944f058615efc8b8f3df1e
Mehal Shah <mehal@code.org>
2017-08-02 15:49:22 -0700

    Bugfix - display all the facilitators instead of just the current user. Required a workaround for a piece in CanCanCan

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -233,6 +233,10 @@ class User < ActiveRecord::Base
     permission? UserPermission::WORKSHOP_ORGANIZER
   end
 
+  def workshop_admin?
+    permission? UserPermission::WORKSHOP_ADMIN
+  end
+
   # assign a course to a facilitator that is qualified to teach it
   def course_as_facilitator=(course)
     courses_as_facilitator << courses_as_facilitator.find_or_create_by(facilitator_id: id, course: course)
