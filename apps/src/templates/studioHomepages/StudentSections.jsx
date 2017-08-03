2487b4346df4e8f57164a5d0041a4b470cab2eb4
Brad Buchanan <brad@code.org>
2017-08-03 12:51:27 -0700

    Simplify StudentSections component

--- apps/src/templates/studioHomepages/StudentSections.jsx
+++ apps/src/templates/studioHomepages/StudentSections.jsx
@@ -1,32 +1,15 @@
-import React, {PropTypes} from 'react';
-import {connect} from 'react-redux';
+import React from 'react';
 import i18n from "@cdo/locale";
-import experiments, {SECTION_FLOW_2017} from '@cdo/apps/util/experiments';
 import ContentContainer from '../ContentContainer';
-import {SectionsSetUpMessage} from './SetUpMessage';
 import JoinSection from './JoinSection';
 import JoinSectionNotifications from './JoinSectionNotifications';
-import OwnedSections from '../teacherDashboard/OwnedSections';
 import SectionsTable from '../studioHomepages/SectionsTable';
-import {asyncLoadSectionData} from '../teacherDashboard/teacherSectionsRedux';
-import AddSectionDialog from "../teacherDashboard/AddSectionDialog";
 
 const StudentSections = React.createClass({
   propTypes: {
     sections: React.PropTypes.array, // student sections!  Teacher sections are in redux
-    codeOrgUrlPrefix: React.PropTypes.string.isRequired,
     isRtl: React.PropTypes.bool.isRequired,
-    isTeacher: React.PropTypes.bool.isRequired,
     canLeave: React.PropTypes.bool.isRequired,
-    teacherHomepage: React.PropTypes.bool,
-
-    //Redux provided
-    numTeacherSections: PropTypes.number.isRequired,
-    asyncLoadSectionData: PropTypes.func.isRequired,
-  },
-
-  componentDidMount(){
-    this.props.asyncLoadSectionData();
   },
 
   getInitialState() {
@@ -51,22 +34,16 @@ const StudentSections = React.createClass({
   },
 
   render() {
-    const {numTeacherSections, codeOrgUrlPrefix, isRtl, isTeacher, canLeave} = this.props;
+    const {isRtl, canLeave} = this.props;
     const studentSections = this.state.studentSections;
     const numStudentSections = studentSections.length;
-    const isStudent = !isTeacher;
-    const editSectionsUrl = `${codeOrgUrlPrefix}/teacher-dashboard#/sections`;
     const enrolledInASection = studentSections.length > 0;
-    const enrollmentDescription = isTeacher ? "" : i18n.enrollmentDescription();
-    const sectionFlow2017 = experiments.isEnabled(SECTION_FLOW_2017);
+    const enrollmentDescription = i18n.enrollmentDescription();
 
     return (
       <div className="sectionsContainer">
         <ContentContainer
           heading={i18n.sectionsTitle()}
-          linkText={i18n.manageSections()}
-          link={editSectionsUrl}
-          showLink={isTeacher && !sectionFlow2017}
           isRtl={isRtl}
           description={enrollmentDescription}
         >
@@ -75,43 +52,24 @@ const StudentSections = React.createClass({
             result={this.state.sectionsResult}
             nameOrId={this.state.sectionsResultName}
           />
-          {isTeacher && numTeacherSections > 0 && sectionFlow2017 && (
-            <OwnedSections/>
-          )}
-          {(
-            (isTeacher && numTeacherSections > 0 && !sectionFlow2017)
-            ||
-            (isStudent && numStudentSections > 0)
-          ) && (
+          {numStudentSections > 0 &&
             <SectionsTable
               sections={studentSections}
               isRtl={isRtl}
-              isTeacher={isTeacher}
+              isTeacher={false}
               canLeave={canLeave}
               updateSections={this.updateSections}
               updateSectionsResult={this.updateSectionsResult}
             />
-          )}
-          {isTeacher && numTeacherSections === 0 && (
-            <SectionsSetUpMessage isRtl={isRtl}/>
-          )}
-          {isTeacher && numTeacherSections === 0 && sectionFlow2017 && (
-            <AddSectionDialog handleImportOpen={() => {}/* TODO */}/>
-          )}
-          {isStudent && (
-            <JoinSection
-              enrolledInASection={enrolledInASection}
-              updateSections={this.updateSections}
-              updateSectionsResult={this.updateSectionsResult}
-            />
-          )}
+          }
+          <JoinSection
+            enrolledInASection={enrolledInASection}
+            updateSections={this.updateSections}
+            updateSectionsResult={this.updateSectionsResult}
+          />
         </ContentContainer>
       </div>
     );
   }
 });
-export default connect(state => ({
-  numTeacherSections: state.teacherSections.sectionIds.length
-}), {
-  asyncLoadSectionData,
-})(StudentSections);
+export default StudentSections;
--- apps/src/templates/studioHomepages/StudentSections.story.jsx
+++ apps/src/templates/studioHomepages/StudentSections.story.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import {createStoreWithReducers, registerReducers} from '@cdo/apps/redux';
-import teacherSections, {setSections, serverSectionFromSection} from '../teacherDashboard/teacherSectionsRedux';
-import oauthClassroom from '../teacherDashboard/oauthClassroomRedux';
 import StudentSections from './StudentSections';
 
 const sections = [
@@ -37,7 +33,6 @@ const sections = [
     code: "HPRWHG"
   },
 ];
-const serverSections = sections.map(serverSectionFromSection);
 
 export default storybook => storybook
   .storiesOf('StudentSections', module)
@@ -45,59 +40,34 @@ export default storybook => storybook
     {
       name: 'Sections - student, no sections yet',
       description: 'shows a join sections component with attention-grabbing dashed border',
-      story: () => {
-        registerReducers({teacherSections, oauthClassroom});
-        return (
-          <Provider store={createStoreWithReducers()}>
-            <StudentSections
-              sections={[]}
-              codeOrgUrlPrefix = "http://code.org/"
-              isRtl={false}
-              isTeacher={false}
-              canLeave={false}
-            />
-          </Provider>
-        );
-      }
+      story: () => (
+        <StudentSections
+          sections={[]}
+          isRtl={false}
+          canLeave={false}
+        />
+      )
     },
     {
       name: 'Sections - student, enrolled in sections but does NOT have permission to leave the sections',
       description: 'shows a sections table, no column for leave buttons, and a solid border join section component',
-      story: () => {
-        registerReducers({teacherSections, oauthClassroom});
-        const store = createStoreWithReducers();
-        store.dispatch(setSections(serverSections));
-        return (
-          <Provider store={store}>
-            <StudentSections
-              sections={sections}
-              codeOrgUrlPrefix = "http://code.org/"
-              isRtl={false}
-              isTeacher={false}
-              canLeave={false}
-            />
-          </Provider>
-        );
-      }
+      story: () => (
+        <StudentSections
+          sections={sections}
+          isRtl={false}
+          canLeave={false}
+        />
+      )
     },
     {
       name: 'Sections - student, enrolled in sections and does have permission to leave the sections',
       description: 'shows a sections table, including a column for leave buttons, and a solid border join section component',
-      story: () => {
-        registerReducers({teacherSections, oauthClassroom});
-        const store = createStoreWithReducers();
-        store.dispatch(setSections(serverSections));
-        return (
-          <Provider store={store}>
-            <StudentSections
-              sections={sections}
-              codeOrgUrlPrefix = "http://code.org/"
-              isRtl={false}
-              isTeacher={false}
-              canLeave={true}
-            />
-          </Provider>
-        );
-      }
+      story: () => (
+        <StudentSections
+          sections={sections}
+          isRtl={false}
+          canLeave={true}
+        />
+      )
     },
   ]);
