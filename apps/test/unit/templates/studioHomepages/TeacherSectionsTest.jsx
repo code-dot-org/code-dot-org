97787efea1165c999cc07744a9efced63c66f0bb
Brad Buchanan <brad@code.org>
2017-08-03 16:52:48 -0700

    Fix TeacherSections test by providing missing prop

--- apps/test/unit/templates/studioHomepages/TeacherSectionsTest.jsx
+++ apps/test/unit/templates/studioHomepages/TeacherSectionsTest.jsx
@@ -69,6 +69,7 @@ describe('TeacherSections', () => {
           isRtl={defaultProps.isRtl}
           isTeacher
           canLeave={false}
+          codeOrgUrlPrefix={defaultProps.codeOrgUrlPrefix}
         />
       </ContentContainer>
     );
