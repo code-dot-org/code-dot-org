4735062d9b1bc173f26727cc689095f40ccd8436
Mehal Shah <mehal@code.org>
2017-08-08 15:55:00 -0700

    Stories and tests for the FreeResponse element and Survey results table

---
+++ apps/src/code-studio/pd/workshop_dashboard/components/survey_results/free_response_section.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const FreeResponseSection = React.createClass({
+  propTypes: {
+    questions: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+    responseData: React.PropTypes.object.isRequired
+  },
+
+  renderFacilitatorsAndAnswers(answers) {
+    return Object.keys(answers).map((facilitator_name, i) => {
+      return (
+        <li key={i}>
+          {facilitator_name}
+          <ul>
+            {
+              answers[facilitator_name].map((feedback, j) => {
+                return (
+                  <li key={j}>
+                    {feedback}
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </li>
+      );
+    });
+  },
+
+  renderAnswers(answers) {
+    return answers.map((answer, i) => {
+      return (
+        <li key={i}>
+          {answer}
+        </li>
+      );
+    });
+  },
+
+  render() {
+    return (
+      <div>
+        {
+          this.props.questions.map((question, i) => {
+            return (
+              <div className="well" key={i}>
+                <b>
+                  {question['text']}
+                </b>
+                {
+                  Array.isArray(this.props.responseData[question['key']]) ?
+                    this.renderAnswers(this.props.responseData[question['key']]) :
+                    this.renderFacilitatorsAndAnswers(this.props.responseData[question['key']])
+                }
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
+});
+
+export default FreeResponseSection;
