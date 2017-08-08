b2138234fbad0cfb9901cc64f6705e6bb6fc72fa
Brad Buchanan <brad@code.org>
2017-08-08 11:42:50 -0700

    Fix local_summer_workshop_survey_resultsTest

--- apps/src/code-studio/pd/workshop_dashboard/components/spinner.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/spinner.jsx
@@ -3,12 +3,8 @@
  */
 
 import React from 'react';
-import createReactClass from 'create-react-class';
 import FontAwesome from '@cdo/apps/templates/FontAwesome';
 
-const Spinner = createReactClass({
-  render() {
-    return <FontAwesome icon="spinner" className="fa-pulse fa-3x"/>;
-  }
-});
-export default Spinner;
+export default function Spinner() {
+  return <FontAwesome icon="spinner" className="fa-pulse fa-3x"/>;
+}
