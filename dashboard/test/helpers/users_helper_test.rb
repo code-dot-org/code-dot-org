b4a173e5876325da872a0156509f7897531bf20a
Ram Kandasamy <ram@code.org>
2017-08-09 13:28:38 -0700

    Update summarize_user_progress tests

--- dashboard/test/helpers/users_helper_test.rb
+++ dashboard/test/helpers/users_helper_test.rb
@@ -17,7 +17,8 @@ class UsersHelperTest < ActionView::TestCase
         lockableAuthorized: false,
         levels: {},
         # second stage because first is unplugged
-        current_stage: script.stages[1].id
+        current_stage: script.stages[1].id,
+        completed: false,
       },
       summarize_user_progress(script, user)
     )
@@ -45,7 +46,8 @@ class UsersHelperTest < ActionView::TestCase
           ul1.level_id => {status: LEVEL_STATUS.perfect, result: ActivityConstants::BEST_PASS_RESULT},
           ul3.level_id => {status: LEVEL_STATUS.passed, result: 20}
         },
-        current_stage: script.stages[1].id
+        current_stage: script.stages[1].id,
+        completed: false,
       },
       summarize_user_progress(script, user)
     )
@@ -56,7 +58,8 @@ class UsersHelperTest < ActionView::TestCase
       {
         linesOfCode: 42,
         lockableAuthorized: false,
-        linesOfCodeText: 'Total lines of code: 42'
+        linesOfCodeText: 'Total lines of code: 42',
+        completed: false,
       },
       summarize_user_progress(script, user, exclude_level_progress)
     )
@@ -156,7 +159,8 @@ class UsersHelperTest < ActionView::TestCase
           "#{ul.level_id}_1" => {}
         },
         # second stage because first is unplugged
-        current_stage: script_level.stage.id
+        current_stage: script_level.stage.id,
+        completed: false
       },
       summarize_user_progress(script, user)
     )
