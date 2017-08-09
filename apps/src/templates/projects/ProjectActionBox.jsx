e7fa7c8bf09616eddc4e8678f0926affb1ecf655
Caley Brock <caleybrock7@gmail.com>
2017-08-09 14:35:11 -0700

    Create new project action component

---
+++ apps/src/templates/projects/ProjectActionBox.jsx
@@ -0,0 +1,68 @@
+import React, {Component, PropTypes} from 'react';
+import i18n from '@cdo/locale';
+import color from "../../util/color";
+import FontAwesome from '../FontAwesome';
+
+const styles = {
+  arrowIcon: {
+    paddingRight: 8
+  },
+  actionBox: {
+    width: 180,
+    paddingLeft: 15,
+    paddingRight: 15,
+    border: '1px solid ' + color.lighter_gray,
+    borderRadius: 2,
+    backgroundColor: color.white,
+    boxShadow: "2px 2px 2px " + color.light_gray,
+    marginTop: 5,
+  },
+  actionText: {
+    fontSize: 14,
+    fontFamily: '"Gotham", sans-serif',
+    color: color.charcoal,
+    padding: '12px 0px 2px 0px',
+  },
+  delete: {
+    color: color.red,
+    borderTop: '1px solid ' + color.lighter_gray,
+    marginTop: 8,
+    padding: '10px 0px 10px 0px',
+    fontSize: 14,
+  },
+  xIcon: {
+    paddingRight: 5
+  },
+  bold: {
+    fontFamily: '"Gotham 5r", sans-serif'
+  }
+};
+
+export default class ProjectActionBox extends Component {
+  static propTypes = {
+    projectData: PropTypes.object
+  };
+
+  getAvailableActions = function () {
+    let actions = [i18n.rename(), i18n.remix()];
+    this.props.projectData.isPublished ?
+    actions.push(i18n.removeFromPublicGallery()) : actions.push(i18n.publishToPublicGallery());
+    return actions;
+  };
+
+  render() {
+    return (
+      <div style={styles.actionBox}>
+        {(this.getAvailableActions()).map((action, index) => (
+          <div key={index} style={styles.actionText}>
+            {action}
+          </div>
+        ))}
+        <div style={styles.delete}>
+          <FontAwesome icon=" fa-times-circle" style={styles.xIcon}/>
+          {i18n.deleteProject()}
+        </div>
+      </div>
+    );
+  }
+}
