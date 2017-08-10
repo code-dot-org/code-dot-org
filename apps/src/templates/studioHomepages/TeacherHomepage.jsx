6600790cf0d6f03288d34121577dab36e3b77756
Erin <erin.bond@code.org>
2017-08-10 15:59:31 -0700

    rtl notification (#16905)
    * rtl notification
    
    * update notification test with isRtl
    
    * fix RecentCoursesTest and pass isRtl to Notification everywhere
    
    * Fix teacherhomepagetest
    
    * header banner still says home

--- apps/src/templates/studioHomepages/TeacherHomepage.jsx
+++ apps/src/templates/studioHomepages/TeacherHomepage.jsx
@@ -59,6 +59,7 @@ export default class TeacherHomepage extends React.Component {
               buttonLink={announcements[0].link}
               newWindow={true}
               analyticId={announcements[0].id}
+              isRtl={isRtl}
             />
             <div style={styles.clear}/>
           </div>
