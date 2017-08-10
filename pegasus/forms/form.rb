40a38a926cc11eaac4204bd88d1c046706f5b6f5
ashercodeorg <asher@code.org>
2017-08-10 17:02:57 -0500

    Define Form#update_on_upsert base method.

--- pegasus/forms/form.rb
+++ pegasus/forms/form.rb
@@ -1,4 +1,10 @@
 # A base class for the `pegasus.forms` table.
 
 class Form
+  # @param [Hash] new_data The new data to be saved to the DB.
+  # @return [Integer, nil] The form ID to be updated with the upsert, or nil if the upsert should
+  #   be an insert.
+  def self.update_on_upsert(new_data)
+    nil
+  end
 end
