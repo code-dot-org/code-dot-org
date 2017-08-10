67c39cd2004916e21ec9d5c3188a367ab3c03c11
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-08-10 14:47:09 -0700

    add turnSingle method and GridTurn action

--- apps/src/studio/studio.js
+++ apps/src/studio/studio.js
@@ -39,7 +39,7 @@ import dropletConfig from './dropletConfig';
 import paramLists from './paramLists.js';
 import studioCell from './cell';
 import studioMsg from './locale';
-import { GridMove, GridMoveAndCancel } from './spriteActions';
+import { GridTurn, GridMove, GridMoveAndCancel } from './spriteActions';
 import { Provider } from 'react-redux';
 import { singleton as studioApp } from '../StudioApp';
 import {
@@ -4301,11 +4301,17 @@ Studio.callCmd = function (cmd) {
       break;
     case 'turnRight':
       studioApp().highlight(cmd.id);
-      Studio.lastMoveSingleDir = turnRight90(Studio.lastMoveSingleDir);
+      Studio.turnSingle({
+        spriteIndex: Studio.protagonistSpriteIndex || 0,
+        dir: turnRight90(Studio.lastMoveSingleDir),
+      });
       break;
     case 'turnLeft':
       studioApp().highlight(cmd.id);
-      Studio.lastMoveSingleDir = turnLeft90(Studio.lastMoveSingleDir);
+      Studio.turnSingle({
+        spriteIndex: Studio.protagonistSpriteIndex || 0,
+        dir: turnLeft90(Studio.lastMoveSingleDir),
+      });
       break;
     case 'moveDistance':
       if (!cmd.opts.started) {
@@ -5858,6 +5864,54 @@ Studio.getSkin = function () {
 };
 
 /**
+ * For grid-aligned movement, we want a single movement action to take place
+ * over several ticks (as opposed to normal movement, which takes place on a
+ * per-tick basis). We therefore yield control for the movement duration.
+ *
+ * @see Studio.turnSingle
+ * @see Studio.moveSingle
+ */
+Studio.yieldGridAlignedTicks = function () {
+  Studio.yieldExecutionTicks += (1 + Studio.gridAlignedExtraPauseSteps);
+  if (Studio.JSInterpreter) {
+    // Stop executing the interpreter in a tight loop and yield the current
+    // execution tick:
+    Studio.JSInterpreter.yield();
+    // Highlight the code in the editor so the student can see the progress
+    // of their program:
+    Studio.JSInterpreter.selectCurrentCode();
+  }
+
+  Studio.movementAudioOn();
+};
+
+/**
+ * For executing a single "goLeft" or "goNorth" sort of command in student code.
+ * Moves the avatar by a different amount.
+ * Has slightly different behaviors depending on whether the level is configured
+ * for discrete, grid-based movement or free movement.
+ * @param {Object} opts
+ * @param {Direction} opts.dir - The direction in which the sprite should move.
+ * @param {number} opts.spriteIndex
+ * @param {boolean} opts.backward - whether the sprite should move toward
+ *        (default) or away from the given direction
+ */
+Studio.turnSingle = function (opts) {
+  const sprite = Studio.sprite[opts.spriteIndex];
+  sprite.lastMove = Studio.tickCount;
+
+  if (skin.gridAlignedMovement) {
+    sprite.setActivity(constants.BEHAVIOR_GRID_ALIGNED);
+    sprite.addAction(new GridTurn(opts.dir, skin.slowExecutionFactor));
+    Studio.yieldGridAlignedTicks();
+  } else {
+    throw new TypeError("Studio.turnSingle is only valid in grid-aligned mode");
+  }
+
+  Studio.lastMoveSingleDir = opts.dir;
+};
+
+/**
  * For executing a single "goLeft" or "goNorth" sort of command in student code.
  * Moves the avatar by a different amount.
  * Has slightly different behaviors depending on whether the level is configured
@@ -5924,17 +5978,7 @@ Studio.moveSingle = function (opts) {
           deltaX, deltaY, skin.slowExecutionFactor, opts.backward));
     }
 
-    Studio.yieldExecutionTicks += (1 + Studio.gridAlignedExtraPauseSteps);
-    if (Studio.JSInterpreter) {
-      // Stop executing the interpreter in a tight loop and yield the current
-      // execution tick:
-      Studio.JSInterpreter.yield();
-      // Highlight the code in the editor so the student can see the progress
-      // of their program:
-      Studio.JSInterpreter.selectCurrentCode();
-    }
-
-    Studio.movementAudioOn();
+    Studio.yieldGridAlignedTicks();
   } else {
     if (!wallCollision) {
       if (playspaceEdgeCollision) {
