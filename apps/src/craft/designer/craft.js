c6ca5a360e8a6158a25b85d8958884256795e1b8
Josh Lory <josh.lory@code.org>
2017-08-14 18:16:14 -0700

    Don't modify .level files, instead follow the pattern in Designer
    I should've implemented it this way to begin with... will make it easier to make integration tests pass.

--- apps/src/craft/designer/craft.js
+++ apps/src/craft/designer/craft.js
@@ -9,6 +9,7 @@ import craftMsg from './locale';
 import CustomMarshalingInterpreter from '../../lib/tools/jsinterpreter/CustomMarshalingInterpreter';
 import GameController from './game/GameController';
 import FacingDirection from './game/LevelMVC/FacingDirection';
+import {convertActionPlaneEntitiesToConfig} from './game/LevelMVC/Utils';
 import dom from '../../dom';
 import eventsLevelbuilderOverrides from './eventsLevelbuilderOverrides';
 import MusicController from '../../MusicController';
@@ -568,46 +569,6 @@ Craft.niceToHaveAssetsForLevel = function (levelNumber) {
   }
   return ['allAssetsMinusPlayer'];
 };
-
-/**
- * Converts entities found within the levelConfig.actionPlane to a
- * levelConfig.entities suitable for loading by the game initializer.
- *
- * ['sheepRight', 'creeperUp] -> [['sheep', 0, 0, 1], ['creeper', 1, 0, 0]]
- *
- * @param levelConfig
- */
-function convertActionPlaneEntitiesToConfig(levelConfig) {
-  const [width, height] = levelConfig.gridWidth && levelConfig.gridHeight ?
-      [levelConfig.gridWidth, levelConfig.gridHeight] : [10, 10];
-
-  var planesToCustomize = [levelConfig.actionPlane];
-  planesToCustomize.forEach(function (plane) {
-    for (var i = 0; i < plane.length; i++) {
-      var item = plane[i];
-
-      if (item.match(/sheep|zombie|ironGolem|creeper|cow|chicken/)) {
-        const suffixToDirection = {
-          Up: FacingDirection.Up,
-          Down: FacingDirection.Down,
-          Left: FacingDirection.Left,
-          Right: FacingDirection.Right,
-        };
-
-        levelConfig.entities = levelConfig.entities || [];
-        const x = i % width;
-        const y = Math.floor(i / height);
-
-        const directionMatch = item.match(/(.*)(Right|Left|Up|Down)/);
-        const directionToUse = directionMatch ?
-            suffixToDirection[directionMatch[2]] : FacingDirection.Right;
-        const entityToUse = directionMatch ? directionMatch[1] : item;
-        levelConfig.entities.push([entityToUse, x, y, directionToUse]);
-        plane[i] = '';
-      }
-    }
-  });
-}
 
 /**
  * Reset the app to the start position and kill any pending animation tasks.
