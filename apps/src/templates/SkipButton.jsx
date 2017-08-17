cb52a6dc1f69bc9adcb898ed3fab2c8f3547e9f0
Ram Kandasamy <ram@code.org>
2017-08-16 18:33:48 -0700

    Report skips

--- apps/src/templates/SkipButton.jsx
+++ apps/src/templates/SkipButton.jsx
@@ -8,16 +8,11 @@ export default class SkipButton extends React.Component {
     nextLevelUrl: React.PropTypes.string.isRequired,
   }
 
-  onClick() {
-    window.location.href = this.props.nextLevelUrl;
-  }
-
   render() {
     return (
       <button
         id="skipButton"
         className={LAUNCH_CLASS}
-        onClick={this.onClick.bind(this)}
       >
         {msg.skipPuzzle()}
       </button>
