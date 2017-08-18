4673b576948cfa8d223baab7022b7eae4fec9580
Erin <erin.bond@code.org>
2017-08-18 09:45:12 -0700

    new ToolCard component, RTL styling for ToolCard and ResourceCard (#17136)
    * new ToolCard component, RTL styling for ToolCard and ResourceCard
    
    * Rename ResourceCard to ImageResourceCard, rename ToolCard to ResourceCard

---
+++ apps/src/templates/studioHomepages/ImageResourceCard.jsx
@@ -0,0 +1,109 @@
+import React, {Component, PropTypes} from 'react';
+import Radium from 'radium';
+import Button from '../Button';
+import color from "../../util/color";
+
+const styles = {
+  card: {
+    overflow: 'hidden',
+    position: 'relative',
+    height: 200,
+    width: 473,
+    float: 'left',
+    marginBottom: 20,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: color.border_gray,
+    background: color.teal
+  },
+  image: {
+    position: 'absolute',
+  },
+  textbox: {
+    position: 'absolute',
+    width: 275,
+    padding: 20
+  },
+  title: {
+    fontSize: 18,
+    paddingBottom: 10,
+    fontFamily:'"Gotham 4r", sans-serif',
+    color: color.white,
+    fontWeight: 'bold'
+  },
+  description: {
+    fontSize: 14,
+    lineHeight: "21px",
+    fontFamily: '"Gotham 4r", sans-serif',
+    color: color.white,
+    height: 80
+  },
+  ltr: {
+    float: 'left',
+  },
+  rtl: {
+    float: 'right',
+  },
+  rtlMargin: {
+    marginRight: 160
+  },
+  ltrMargin: {
+    marginRight: 0
+  }
+};
+
+class ImageResourceCard extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    buttonText: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    isRtl: PropTypes.bool.isRequired
+  };
+
+  render() {
+
+    const { title, description, buttonText, link, image, isRtl } = this.props;
+    const localeStyle = isRtl ? styles.rtl : styles.ltr;
+    const uncoverImage = isRtl ? styles.rtlMargin : styles.ltrMargin;
+
+    const filenameToImgUrl = {
+      "teacher-community": require('@cdo/static/resource_cards/teachercommunity.png'),
+      "guest-speaker": require('@cdo/static/resource_cards/findguestspeaker.png'),
+      "professional-learning": require('@cdo/static/resource_cards/professionallearning.png'),
+      "standards-framework": require('@cdo/static/resource_cards/standardsandframework.png'),
+      "elementary": require('@cdo/static/resource_cards/elementary.png'),
+      "middleschool": require('@cdo/static/resource_cards/middleschool.png'),
+      "highschool": require('@cdo/static/resource_cards/highschool.png'),
+      "hourofcode": require('@cdo/static/resource_cards/hourofcode.png'),
+      "hourofcode2": require('@cdo/static/resource_cards/hourofcode2.png'),
+    };
+    const imgSrc = filenameToImgUrl[image];
+
+    return (
+      <div style={[styles.card, localeStyle]}>
+        <div style={styles.image}>
+          <img src={imgSrc}/>
+        </div>
+        <div style={[styles.textbox, localeStyle, uncoverImage]}>
+          <div style={styles.title}>
+            {title}
+          </div>
+          <div style={styles.description}>
+            {description}
+          </div>
+          <br/>
+          <Button
+            href={link}
+            color={Button.ButtonColor.gray}
+            text={buttonText}
+            style={styles.button}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Radium(ImageResourceCard);
--- apps/src/templates/studioHomepages/ImageResourceCard.story.jsx
+++ apps/src/templates/studioHomepages/ImageResourceCard.story.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import ResourceCard from './ResourceCard';
+import ImageResourceCard from './ImageResourceCard';
 
 export default storybook => {
   return storybook
-    .storiesOf('ResourceCard', module)
+    .storiesOf('ImageResourceCard', module)
     .addStoryTable([
       {
-        name: 'tool card',
-        description: `This is an example tool card.`,
+        name: 'basic resource card',
+        description: `This is an example resource card.`,
         story: () => (
-          <ResourceCard
+          <ImageResourceCard
             title= "Teacher Community"
             description="Ask questions about curriculum, share ideas from your lessons, and get help from other teachers"
+            image="teacher-community"
             buttonText= "Connect Today"
             link= "link to teacher community"
             isRtl={false}
@@ -19,12 +20,13 @@ export default storybook => {
         )
       },
       {
-        name: 'tool card - RTL',
-        description: `This is an example tool card with RTL styling.`,
+        name: 'basic resource card - RTL',
+        description: `This is an example resource card with RTL styling.`,
         story: () => (
-          <ResourceCard
+          <ImageResourceCard
             title= "Teacher Community"
             description="Ask questions about curriculum, share ideas from your lessons, and get help from other teachers"
+            image="teacher-community"
             buttonText= "Connect Today"
             link= "link to teacher community"
             isRtl={true}
