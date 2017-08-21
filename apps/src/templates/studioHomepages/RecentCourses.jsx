95a780520b6dae1364af92bb8d008d2be55ef7aa
Erin <erin.bond@code.org>
2017-08-21 10:14:33 -0700

    adjust use of 'flex' to fix hoc tiles (#17169)
    * adjust use of 'flex' to fix hoc tiles
    
    * Fix RecentCoursesTest

--- apps/src/templates/studioHomepages/RecentCourses.jsx
+++ apps/src/templates/studioHomepages/RecentCourses.jsx
@@ -5,8 +5,20 @@ import SetUpCourses from './SetUpCourses';
 import SeeMoreCourses from './SeeMoreCourses';
 import TopCourse from './TopCourse';
 import Notification from '@cdo/apps/templates/Notification';
+import styleConstants from '../../styleConstants';
 import i18n from "@cdo/locale";
 import shapes from './shapes';
+
+const contentWidth = styleConstants['content-width'];
+
+const styles = {
+  container: {
+    width: contentWidth,
+    display: "flex",
+    justifyContent: "space-between",
+    flexWrap: 'wrap'
+  },
+};
 
 const RecentCourses = React.createClass({
   propTypes: {
@@ -38,16 +50,18 @@ const RecentCourses = React.createClass({
             />
           )}
           {topFourCourses.length > 0 && (
-            topFourCourses.map((course, index) =>
-            <div key={index}>
-              <CourseCard
-                title={course.title}
-                description={course.description}
-                link={course.link}
-                isRtl={isRtl}
-              />
+            <div style={styles.container}>
+              {topFourCourses.map((course, index) =>
+                <div key={index}>
+                  <CourseCard
+                    title={course.title}
+                    description={course.description}
+                    link={course.link}
+                    isRtl={isRtl}
+                  />
+                </div>
+              )}
             </div>
-            )
           )}
           {moreCourses.length > 0 && (
             <SeeMoreCourses
