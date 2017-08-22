7de86b4bea3d5ddae0965d942af333dd68b8f503
David Bailey <davidsbailey@users.noreply.github.com>
2017-08-22 15:16:19 -0700

    DO NOT MERGE -- run only applab level tests [skip ui]

--- apps/test-low-memory.sh
+++ apps/test-low-memory.sh
@@ -1,36 +1,3 @@
 #!/bin/bash
 
-# 'npm test' normally does all three of these things.
-# We break them up here so they each run in isolation.
-
-npm run lint && \
-
-node --max_old_space_size=4096 `npm bin`/grunt unitTest && \
-if [ -n "$CIRCLECI" ]; then \
-    bash <(curl -s https://codecov.io/bash) -cF unit; \
-fi && \
-
-node --max_old_space_size=4096 `npm bin`/grunt storybookTest && \
-if [ -n "$CIRCLECI" ]; then \
-    bash <(curl -s https://codecov.io/bash) -cF storybook; \
-fi && \
-
-node --max_old_space_size=4096 `npm bin`/grunt scratchTest && \
-if [ -n "$CIRCLECI" ]; then \
-    bash <(curl -s https://codecov.io/bash) -cF scratch; \
-fi && \
-
-LEVEL_TYPE='maze|turtle|bounce|calc|eval|flappy|studio' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-if [ -n "$CIRCLECI" ]; then \
-    bash <(curl -s https://codecov.io/bash) -cF integration; \
-fi && \
-
-LEVEL_TYPE='applab|gamelab' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-if [ -n "$CIRCLECI" ]; then \
-    bash <(curl -s https://codecov.io/bash) -cF integration; \
-fi && \
-
-LEVEL_TYPE='craft' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-if [ -n "$CIRCLECI" ]; then \
-    bash <(curl -s https://codecov.io/bash) -cF integration; \
-fi
+LEVEL_TYPE='applab' node --max_old_space_size=4096 `npm bin`/grunt integrationTest
