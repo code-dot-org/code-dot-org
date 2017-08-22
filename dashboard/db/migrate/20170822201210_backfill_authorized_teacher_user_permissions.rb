e6afbd70e0cafe98704e7cc659e634385250cb30
Clare Constantine <clare@code.org>
2017-08-22 16:33:27 -0700

    adjust whitespace for linting

--- dashboard/db/migrate/20170822201210_backfill_authorized_teacher_user_permissions.rb
+++ dashboard/db/migrate/20170822201210_backfill_authorized_teacher_user_permissions.rb
@@ -1,19 +1,17 @@
 class BackfillAuthorizedTeacherUserPermissions < ActiveRecord::Migration[5.0]
-
   def up
-  	Plc::UserCourseEnrollment.find_each do |enrollment|
-  		UserPermission.create(permission: UserPermission::AUTHORIZED_TEACHER, user_id: enrollment.user_id)
-  	end
+    Plc::UserCourseEnrollment.find_each do |enrollment|
+      UserPermission.create(permission: UserPermission::AUTHORIZED_TEACHER, user_id: enrollment.user_id)
+    end
 
-  	Cohort.find_each do |cohort|
-  		cohort.teachers.each do |teacher|
-  			UserPermission.create(permission: UserPermission::AUTHORIZED_TEACHER, user_id: teacher.id)
-  		end
-  	end
+    Cohort.find_each do |cohort|
+      cohort.teachers.each do |teacher|
+        UserPermission.create(permission: UserPermission::AUTHORIZED_TEACHER, user_id: teacher.id)
+      end
+    end
   end
 
-
   def down
-  	UserPermission.where(permission: UserPermission::AUTHORIZED_TEACHER).destroy_all
+    UserPermission.where(permission: UserPermission::AUTHORIZED_TEACHER).destroy_all
   end
 end
