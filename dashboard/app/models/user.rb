64b6d9e5c161145826faccbcd5a72638326ef61c
eric <ewjordan@gmail.com>
2017-08-23 16:41:48 -0400

    Remove allow_blank: false (which is the default)

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -404,7 +404,7 @@ class User < ActiveRecord::Base
   validates_length_of :username, within: 5..20, allow_blank: true
   validates_format_of :username, with: USERNAME_REGEX, on: :create, allow_blank: true
   validates_uniqueness_of :username, allow_blank: true, case_sensitive: false, on: :create, if: 'errors.blank?'
-  validates_uniqueness_of :username, allow_blank: false, case_sensitive: false, on: :update, if: 'errors.blank? && username_changed?'
+  validates_uniqueness_of :username, false, case_sensitive: false, on: :update, if: 'errors.blank? && username_changed?'
   validates_presence_of :username, if: :username_required?
   before_validation :generate_username, on: :create
 
