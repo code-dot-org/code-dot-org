5f8e6296e267ad6a5725f821163708c315b5b937
Joanne Polsky <joanne@code.org>
2017-08-23 14:50:44 -0700

    Add new school search API with default implementation

--- dashboard/app/controllers/api/v1/schools_controller.rb
+++ dashboard/app/controllers/api/v1/schools_controller.rb
@@ -1,7 +1,19 @@
+require 'cdo/chat_client'
+
 class Api::V1::SchoolsController < ApplicationController
   # GET /api/v1/school/<school_district_id>/<school_type>
   def index
     schools = School.where(school_district_id: params[:school_district_id], school_type: params[:school_type])
+    serialized_schools = schools.map do |school|
+      Api::V1::SchoolSerializer.new(school).attributes
+    end
+    render json: serialized_schools
+  end
+
+  # GET /dashboardapi/v1/schoolsearch/:criteria/:limit
+  def search
+    schools = School.where("name LIKE ?", "%#{params[:q].upcase}%").order(:name).limit(params[:limit])
+    #ChatClient.log "#{schools.to_s}"
     serialized_schools = schools.map do |school|
       Api::V1::SchoolSerializer.new(school).attributes
     end
