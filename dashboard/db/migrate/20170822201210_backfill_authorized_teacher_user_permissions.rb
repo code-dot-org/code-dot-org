5127f9550f1155b7960f92a05cb4af103a9a22de
Clare Constantine <clare@code.org>
2017-08-23 15:37:14 -0700

    use default (empty) down method; use find_or_create_by for clarity

--- dashboard/db/migrate/20170822201210_backfill_authorized_teacher_user_permissions.rb
+++ dashboard/db/migrate/20170822201210_backfill_authorized_teacher_user_permissions.rb
@@ -1,17 +1,16 @@
 class BackfillAuthorizedTeacherUserPermissions < ActiveRecord::Migration[5.0]
   def up
     Plc::UserCourseEnrollment.find_each do |enrollment|
-      UserPermission.create(permission: UserPermission::AUTHORIZED_TEACHER, user_id: enrollment.user_id)
+      UserPermission.find_or_create_by(permission: UserPermission::AUTHORIZED_TEACHER, user_id: enrollment.user_id)
     end
 
     Cohort.find_each do |cohort|
       cohort.teachers.each do |teacher|
-        UserPermission.create(permission: UserPermission::AUTHORIZED_TEACHER, user_id: teacher.id)
+        UserPermission.find_or_create_by(permission: UserPermission::AUTHORIZED_TEACHER, user_id: teacher.id)
       end
     end
   end
 
   def down
-    UserPermission.where(permission: UserPermission::AUTHORIZED_TEACHER).destroy_all
   end
 end
