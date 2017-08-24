efa03253c2ab500f8f5df0a57c4b0317da3625e2
ashercodeorg <asher@code.org>
2017-08-24 15:33:37 -0500

    Define factory for Pd::RegionalPartnerProgramRegistration. Fix test.

--- dashboard/test/models/pd/regional_partner_program_registration_test.rb
+++ dashboard/test/models/pd/regional_partner_program_registration_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class Pd::RegionalPartnerProgramRegistrationTest < ActiveSupport::TestCase
   test 'required fields are optional for deleted users' do
     registration = create :pd_regional_partner_program_registration
-    user.destroy!
+    registration.user.destroy!
     registration.clear_data
     assert registration.valid?
   end
