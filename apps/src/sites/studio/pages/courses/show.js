806ad687641c356d8a1abc0b19aa569d10e76601
Brent Van Minnen <bjvanminnen@gmail.com>
2017-08-28 09:57:19 -0700

    wrap course overview in a provider

--- apps/src/sites/studio/pages/courses/show.js
+++ apps/src/sites/studio/pages/courses/show.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import CourseOverview from '@cdo/apps/templates/courseOverview/CourseOverview';
 import { ViewType } from '@cdo/apps/code-studio/stageLockRedux';
+import { getStore } from '@cdo/apps/code-studio/redux';
 
 $(document).ready(showCourseOverview);
 
@@ -12,19 +14,22 @@ function showCourseOverview() {
 
   const teacherResources = (courseSummary.teacher_resources || []).map(
     ([type, link]) => ({type, link}));
+  const store = getStore();
 
   // Eventually we want to do this all via redux
   ReactDOM.render(
-    <CourseOverview
-      title={courseSummary.title}
-      id={courseSummary.id}
-      descriptionStudent={courseSummary.description_student}
-      descriptionTeacher={courseSummary.description_teacher}
-      sectionsInfo={scriptData.sections}
-      teacherResources={teacherResources}
-      isTeacher={scriptData.is_teacher}
-      viewAs={ViewType.Teacher}
-      scripts={courseSummary.scripts}
-    />,
+    <Provider store={store}>
+      <CourseOverview
+        title={courseSummary.title}
+        id={courseSummary.id}
+        descriptionStudent={courseSummary.description_student}
+        descriptionTeacher={courseSummary.description_teacher}
+        sectionsInfo={scriptData.sections}
+        teacherResources={teacherResources}
+        isTeacher={scriptData.is_teacher}
+        viewAs={ViewType.Teacher}
+        scripts={courseSummary.scripts}
+      />
+    </Provider>,
   document.getElementById('course_overview'));
 }
