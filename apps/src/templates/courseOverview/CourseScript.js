0a5cc434e2204557fd984e564d0d65fdb42559c8
Brent Van Minnen <bjvanminnen@gmail.com>
2017-08-28 16:01:56 -0700

    prepare for having hidden/visible toggle

--- apps/src/templates/courseOverview/CourseScript.js
+++ apps/src/templates/courseOverview/CourseScript.js
@@ -1,8 +1,10 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import color from "@cdo/apps/util/color";
 import i18n from '@cdo/locale';
 import Button from '../Button';
 import TeacherInfoBox from '@cdo/apps/templates/progress/TeacherInfoBox';
+import { ViewType } from '@cdo/apps/code-studio/stageLockRedux';
 
 const styles = {
   main: {
@@ -29,14 +31,18 @@ const styles = {
   }
 };
 
-export default class CourseScript extends Component {
+class CourseScript extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     name: PropTypes.string,
     description: PropTypes.string,
+
+    // redux provided
+    viewAs: PropTypes.oneOf(Object.values(ViewType)).isRequired,
+    selectedSectionId: PropTypes.string.isRequired,
   };
   render() {
-    const { title, name, description } = this.props;
+    const { title, name, description, viewAs, selectedSectionId } = this.props;
     return (
       <div style={styles.main}>
         <div style={styles.content}>
@@ -48,10 +54,21 @@ export default class CourseScript extends Component {
             color={Button.ButtonColor.gray}
           />
         </div>
-        <TeacherInfoBox>
-          <div>Info will go here when you have a section selected</div>
-        </TeacherInfoBox>
+        {viewAs === ViewType.Teacher && (
+          <TeacherInfoBox>
+            <div style={{opacity: selectedSectionId ? 1 : 0.3}}>
+              This will be a toggle for hiding/showing the unit, and will be disabled
+              if we have no section selected.
+            </div>
+          </TeacherInfoBox>
+        )}
       </div>
     );
   }
 }
+
+export default connect(state => ({
+  // TODO: move viewAs out of stage lock
+  viewAs: state.stageLock.viewAs,
+  selectedSectionId: state.teacherSections.selectedSectionId,
+}))(CourseScript);
