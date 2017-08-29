bba30d8a18c5b6dcb1bacd2cb47c2b7bf47a23cb
Brent Van Minnen <bjvanminnen@gmail.com>
2017-08-29 13:12:26 -0700

    un-connect CourseScriptTeacherInfo

--- apps/src/templates/courseOverview/CourseScriptTeacherInfo.js
+++ apps/src/templates/courseOverview/CourseScriptTeacherInfo.js
@@ -1,61 +1,45 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
 import ReactTooltip from 'react-tooltip';
 import _ from 'lodash';
 import TeacherInfoBox from '@cdo/apps/templates/progress/TeacherInfoBox';
 import HiddenStageToggle from '@cdo/apps/templates/progress/HiddenStageToggle';
-import { ViewType } from '@cdo/apps/code-studio/viewAsRedux';
-import { isHiddenForSection } from '@cdo/apps/code-studio/hiddenStageRedux';
 import i18n from '@cdo/locale';
 import experiments from '@cdo/apps/util/experiments';
 
-class CourseScriptTeacherInfo extends Component {
+export default class CourseScriptTeacherInfo extends Component {
   static propTypes = {
-    courseId: PropTypes.number.isRequired,
-    // redux provided
-    viewAs: PropTypes.oneOf(Object.values(ViewType)).isRequired,
-    hasNoSections: PropTypes.bool.isRequired,
-    selectedSectionId: PropTypes.string.isRequired,
-    hiddenStageState: PropTypes.object.isRequired,
+    disabled: PropTypes.bool.isRequired,
+    isHidden: PropTypes.bool.isRequired,
   };
 
   render() {
-    const { courseId, viewAs, hasNoSections, selectedSectionId, hiddenStageState } = this.props;
+    const { disabled, isHidden } = this.props;
     if (!experiments.isEnabled('hidden-scripts')) {
       return null;
     }
 
-    if (viewAs !== ViewType.Teacher) {
-      return null;
-    }
-
     // Note: Students should always have no (owned) sections
-    const showHiddenScriptToggle = !hasNoSections;
     const tooltipId = _.uniqueId();
-
-    const isHidden = isHiddenForSection(hiddenStageState, selectedSectionId, courseId);
 
     return (
       <TeacherInfoBox>
-        {showHiddenScriptToggle &&
-          <div
-            data-tip
-            data-for={tooltipId}
-            aria-describedby={tooltipId}
-          >
-            <HiddenStageToggle
-              hidden={isHidden}
-              disabled={!selectedSectionId}
-              onChange={() => console.log('change')}
-            />
-          </div>
-        }
+        <div
+          data-tip
+          data-for={tooltipId}
+          aria-describedby={tooltipId}
+        >
+          <HiddenStageToggle
+            hidden={isHidden}
+            disabled={disabled}
+            onChange={() => console.log('change')}
+          />
+        </div>
         <ReactTooltip
           id={tooltipId}
           role="tooltip"
           wrapper="span"
           effect="solid"
-          disable={!!selectedSectionId}
+          disable={!disabled}
         >
           {i18n.hiddenScriptTooltip()}
         </ReactTooltip>
@@ -63,11 +47,3 @@ class CourseScriptTeacherInfo extends Component {
     );
   }
 }
-
-export default connect(state => ({
-  viewAs: state.viewAs,
-  hasNoSections: state.teacherSections.sectionsAreLoaded &&
-    state.teacherSections.sectionIds.length === 0,
-  selectedSectionId: state.teacherSections.selectedSectionId,
-  hiddenStageState: state.hiddenStage,
-}))(CourseScriptTeacherInfo);
