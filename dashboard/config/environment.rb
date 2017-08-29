421655d4f9997d22d89884e14861289450107a41
Mehal Shah <mehal@code.org>
2017-08-29 11:51:43 -0700

    PR feedback

--- dashboard/config/environment.rb
+++ dashboard/config/environment.rb
@@ -12,4 +12,9 @@ require 'active_record/connection_adapters/abstract_mysql_adapter'
 # Initialize the Rails application.
 Dashboard::Application.initialize!
 
+# overflow_activities was a backup table created on the production db, and not through
+# the standard table creation process in rails. The table only exists on production
+# and we won't be migrating it to other environments. However, it shouldn't belong in
+# the schema, and we don't want it to show up when generating the schema on the prod
+# database. This line will ignore that table.
 ActiveRecord::SchemaDumper.ignore_tables = ['overflow_activities']
