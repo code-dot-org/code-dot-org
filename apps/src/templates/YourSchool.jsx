07a6748073937a378aaae45bd01c512694a214ca
Erin Bond <erin.bond@code.org>
2017-08-30 12:19:19 -0700

    entry point for yourschool

---
+++ apps/src/templates/YourSchool.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class YourSchool extends React.Component {
+  render() {
+    return (
+      <div>
+        Hey, look! This is where the census form will be.
+      </div>
+    );
+  }
+}
