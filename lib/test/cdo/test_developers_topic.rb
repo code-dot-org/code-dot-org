8ab3349406f9c9126c99daf055cbd354db424f92
ashercodeorg <asher@code.org>
2017-08-30 13:58:38 -0500

    Define DevelopersTopic#dotd.

---
+++ lib/test/cdo/test_developers_topic.rb
@@ -0,0 +1,18 @@
+require_relative '../test_helper'
+require 'cdo/developers_topic'
+
+class DevelopersTopicTest < Minitest::Test
+  describe 'dotd' do
+    it 'returns the dotd (including @ symbol)' do
+      Slack.stubs(:get_topic).returns('DOTD: @someone; DTS: yes; DTT: yes; DTP: yes; DTL: yes')
+
+      assert_equal 'someone', DevelopersTopic.dotd
+    end
+
+    it 'raises an exception if topic is malformed' do
+      Slack.stubs(:get_topic).returns('DTS: yes; DTT: yes; DTP: yes; DTL: yes')
+
+      assert_raises {DevelopersTopic.dotd}
+    end
+  end
+end
