c4f8917dd2360a6ee580767024dfbf1e05191744
Erin <erin.bond@code.org>
2017-08-30 11:15:28 -0700

    redirect to next lesson in student assigned courses (#17350)
    * redirect to next lesson in student assigned courses
    
    * test redirect
    
    * incorporate feedback

--- dashboard/test/controllers/home_controller_test.rb
+++ dashboard/test/controllers/home_controller_test.rb
@@ -11,12 +11,22 @@ class HomeControllerTest < ActionController::TestCase
     Properties.stubs(:get).returns nil
   end
 
-  test "redirect index when signed in" do
+  test "redirect to /home when signed in and not a student with course progress" do
     user = create(:user)
     sign_in user
     get :index
 
     assert_redirected_to '/home'
+  end
+
+  test "student with course progress is redirected to next lesson" do
+    student = create :student
+    script = create :script
+    sign_in student
+    User.any_instance.expects(:primary_script).returns(script).twice
+    get :index
+
+    assert_redirected_to script_next_path(script)
   end
 
   test "redirect index when signed out" do
