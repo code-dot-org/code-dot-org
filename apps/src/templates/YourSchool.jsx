83b9587d345427e5dcf51f21a6002d3e8c98d623
Erin Bond <erin.bond@code.org>
2017-08-30 12:48:15 -0700

    move headers into React

--- apps/src/templates/YourSchool.jsx
+++ apps/src/templates/YourSchool.jsx
@@ -4,6 +4,16 @@ export default class YourSchool extends React.Component {
   render() {
     return (
       <div>
+        <h1>
+          Bring Computer Science to your school district
+        </h1>
+        <h3>
+          Our vision is that every student in every school should have the opportunity to learn computer science.
+          Help us by letting us know how computer science is taught at your school and by pledging to do more.
+        </h3>
+        <h2>
+          Tell us about your school
+        </h2>
         Hey, look! This is where the census form will be.
       </div>
     );
