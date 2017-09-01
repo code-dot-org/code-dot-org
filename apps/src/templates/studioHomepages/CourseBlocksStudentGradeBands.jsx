0dcad8b15bf76e4d9d300e2f80221440a23cd7af
Brendan Reville <brendan@code.org>
2017-09-01 11:39:56 -0700

    Revert "Courses: make responsive (#17256)"
    This reverts commit d941831777f4ae358acdb4fcd39d4159b639f6eb.

--- apps/src/templates/studioHomepages/CourseBlocksStudentGradeBands.jsx
+++ apps/src/templates/studioHomepages/CourseBlocksStudentGradeBands.jsx
@@ -1,30 +1,28 @@
 import React, {Component, PropTypes} from 'react';
 import CourseBlocksGradeBands from './CourseBlocksGradeBands';
-import Responsive from '../../responsive';
 import ContentContainer from '../ContentContainer';
 import i18n from "@cdo/locale";
 
 class CourseBlocksStudentGradeBands extends Component {
   static propTypes = {
-    isRtl: PropTypes.bool.isRequired,
-    responsive: PropTypes.instanceOf(Responsive).isRequired
+    isRtl: PropTypes.bool.isRequired
   };
 
   cards = [
     {
-      heading: i18n.courseBlocksGradeBandsElementary(),
-      description: i18n.courseBlocksGradeBandsElementaryDescription(),
-      path: '/educate/curriculum/elementary-school'
+      heading: i18n.courseBlocksGradeBandsK5(),
+      description: i18n.courseBlocksGradeBandsK5Description(),
+      path: '/student/elementary'
     },
     {
-      heading: i18n.courseBlocksGradeBandsMiddle(),
-      description: i18n.courseBlocksGradeBandsMiddleDescription(),
-      path: '/educate/curriculum/middle-school'
+      heading: i18n.courseBlocksGradeBands612(),
+      description: i18n.courseBlocksGradeBands612Description(),
+      path: '/student/middle-high'
     },
     {
-      heading: i18n.courseBlocksGradeBandsHigh(),
-      description: i18n.courseBlocksGradeBandsHighDescription(),
-      path: '/educate/curriculum/high-school'
+      heading: i18n.courseBlocksGradeBandsUniversity(),
+      description: i18n.courseBlocksGradeBandsUniversityDescription(),
+      path: '/student/university'
     }
   ];
 
@@ -36,12 +34,10 @@ class CourseBlocksStudentGradeBands extends Component {
         heading={i18n.courseBlocksGradeBandsContainerHeading()}
         description={i18n.courseBlocksGradeBandsContainerDescription()}
         isRtl={this.props.isRtl}
-        responsive={this.props.responsive}
       >
         <CourseBlocksGradeBands
           cards={this.cards}
           isRtl={this.props.isRtl}
-          responsive={this.props.responsive}
         />
       </ContentContainer>
     );
