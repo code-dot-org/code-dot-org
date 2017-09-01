code-dot-org/code-dot-org

commit d6c7e689b16ea89ffaf5e6660653cb900dbeada9

Courses: make responsive (#17256)
* Courses: make responsive

Initial work.  More to come.

* Courses: remove redundant container

* Courses: make course explorer optionally responsive

Still works on both dashboard & pegasus.  When there is a smaller course explorer available, opening/closing items in it also opens/closes the same items in the desktop width
version, and vice versa.

* Courses: make header responsive

* Courses: fix responsive stories

* Courses: code review feedback

* Course Explorer: ensure course image small on responsive pegasus

* Courses: fix content container for RTL & non-responsive pages

* Course: default responsive widths

apps/src/templates/studioHomepages/CourseBlocksStudentGradeBands.jsx
@@ -1,28 +1,30 @@
 import React, {Component, PropTypes} from 'react';
 import CourseBlocksGradeBands from './CourseBlocksGradeBands';
+import Responsive from '../../responsive';
 import ContentContainer from '../ContentContainer';
 import i18n from "@cdo/locale";
 
 class CourseBlocksStudentGradeBands extends Component {
   static propTypes = {
-    isRtl: PropTypes.bool.isRequired
+    isRtl: PropTypes.bool.isRequired,
+    responsive: PropTypes.instanceOf(Responsive).isRequired
   };
 
   cards = [
     {
-      heading: i18n.courseBlocksGradeBandsK5(),
-      description: i18n.courseBlocksGradeBandsK5Description(),
-      path: '/student/elementary'
+      heading: i18n.courseBlocksGradeBandsElementary(),
+      description: i18n.courseBlocksGradeBandsElementaryDescription(),
+      path: '/educate/curriculum/elementary-school'
     },
     {
-      heading: i18n.courseBlocksGradeBands612(),
-      description: i18n.courseBlocksGradeBands612Description(),
-      path: '/student/middle-high'
+      heading: i18n.courseBlocksGradeBandsMiddle(),
+      description: i18n.courseBlocksGradeBandsMiddleDescription(),
+      path: '/educate/curriculum/middle-school'
     },
     {
-      heading: i18n.courseBlocksGradeBandsUniversity(),
-      description: i18n.courseBlocksGradeBandsUniversityDescription(),
-      path: '/student/university'
+      heading: i18n.courseBlocksGradeBandsHigh(),
+      description: i18n.courseBlocksGradeBandsHighDescription(),
+      path: '/educate/curriculum/high-school'
     }
   ];
 
@@ -34,10 +36,12 @@ class CourseBlocksStudentGradeBands extends Component {
         heading={i18n.courseBlocksGradeBandsContainerHeading()}
         description={i18n.courseBlocksGradeBandsContainerDescription()}
         isRtl={this.props.isRtl}
+        responsive={this.props.responsive}
       >
         <CourseBlocksGradeBands
           cards={this.cards}
           isRtl={this.props.isRtl}
+          responsive={this.props.responsive}
         />
       </ContentContainer>
     );

apps/src/templates/studioHomepages/CourseBlocksTeacherGradeBands.jsx
@@ -1,28 +1,30 @@
 import React, {Component, PropTypes} from 'react';
 import ContentContainer from '../ContentContainer';
 import CourseBlocksGradeBands from './CourseBlocksGradeBands';
+import Responsive from '../../responsive';
 import i18n from "@cdo/locale";
 
 class CourseBlocksTeacherGradeBands extends Component {
   static propTypes = {
-    isRtl: PropTypes.bool.isRequired
+    isRtl: PropTypes.bool.isRequired,
+    responsive: PropTypes.instanceOf(Responsive).isRequired
   };
 
   cards = [
     {
-      heading: i18n.courseBlocksGradeBandsElementary(),
-      description: i18n.courseBlocksGradeBandsElementaryDescription(),
-      path: '/educate/curriculum/elementary-school'
+      heading: i18n.courseBlocksGradeBandsK5(),
+      description: i18n.courseBlocksGradeBandsK5Description(),
+      path: '/student/elementary'
     },
     {
-      heading: i18n.courseBlocksGradeBandsMiddle(),
-      description: i18n.courseBlocksGradeBandsMiddleDescription(),
-      path: '/educate/curriculum/middle-school'
+      heading: i18n.courseBlocksGradeBands612(),
+      description: i18n.courseBlocksGradeBands612Description(),
+      path: '/student/middle-high'
     },
     {
-      heading: i18n.courseBlocksGradeBandsHigh(),
-      description: i18n.courseBlocksGradeBandsHighDescription(),
-      path: '/educate/curriculum/high-school'
+      heading: i18n.courseBlocksGradeBandsUniversity(),
+      description: i18n.courseBlocksGradeBandsUniversityDescription(),
+      path: '/student/university'
     }
   ];
 
@@ -32,10 +34,12 @@ class CourseBlocksTeacherGradeBands extends Component {
         heading={i18n.courseBlocksGradeBandsContainerHeading()}
         description={i18n.courseBlocksGradeBandsContainerDescription()}
         isRtl={this.props.isRtl}
+        responsive={this.props.responsive}
       >
         <CourseBlocksGradeBands
           cards={this.cards}
           isRtl={this.props.isRtl}
+          responsive={this.props.responsive}
         />
       </ContentContainer>
     );

dashboard/app/assets/stylesheets/application.scss
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7894298c7190c3559932c82a66df50592a9a9a2a58739376ca8f19261a4ad77d
+oid sha256:4b74c7c4cd76a03754cf9fc8849729322dca50cd9e3297bfe3e7aa210cbadbc6
 size 54370

apps/src/courseExplorer/courseExplorer.js
@@ -12,24 +12,24 @@ module.exports.initCourseExplorer = function () {
     const index = ($(this).data('index'));
 
     if (toolShowingIndex === -1) {
-      $('#detailrow-' + row).slideDown();
-      $('#toolsextra-' + index).fadeIn();
+      $('.detailrow-' + row).slideDown();
+      $('.toolsextra-' + index).fadeIn();
       detailRowShowing = row;
       toolShowingIndex = index;
     } else if (toolShowingIndex === index) {
-      $('#detailrow-' + detailRowShowing).slideUp();
-      $('#toolsextra-' + index).fadeOut();
+      $('.detailrow-' + detailRowShowing).slideUp();
+      $('.toolsextra-' + index).fadeOut();
       detailRowShowing = -1;
       toolShowingIndex = -1;
     } else if (detailRowShowing === row) {
-      $('#toolsextra-' + toolShowingIndex).fadeOut();
-      $('#toolsextra-' + index).fadeIn();
+      $('.toolsextra-' + toolShowingIndex).fadeOut();
+      $('.toolsextra-' + index).fadeIn();
       toolShowingIndex = index;
     } else {
-      $('#detailrow-' + detailRowShowing).slideUp();
-      $('#toolsextra-' + toolShowingIndex).fadeOut();
-      $('#detailrow-' + row).slideDown();
-      $('#toolsextra-' + index).fadeIn();
+      $('.detailrow-' + detailRowShowing).slideUp();
+      $('.toolsextra-' + toolShowingIndex).fadeOut();
+      $('.detailrow-' + row).slideDown();
+      $('.toolsextra-' + index).fadeIn();
       detailRowShowing = row;
       toolShowingIndex = index;
     }
@@ -47,29 +47,29 @@ module.exports.initCourseExplorer = function () {
     // so that we can apply margin to the top.
 
     if (courseShowingIndex === -1) {
-      $('#courseextra-' + courseIndex).slideDown('slow');
-      $('#course-' + courseIndex + ' .arrow-down').hide();
-      $('#course-' + courseIndex + ' .arrow-up').css('display', 'inline-block');
+      $('.courseextra-' + courseIndex).slideDown('slow');
+      $('.course-' + courseIndex + ' .arrow-down').hide();
+      $('.course-' + courseIndex + ' .arrow-up').css('display', 'inline-block');
       courseShowingIndex = courseIndex;
     } else if (courseShowingIndex === courseIndex) {
-      $('#courseextra-' + courseIndex).slideUp('slow');
-      $('#course-' + courseIndex + ' .arrow-down').css('display', 'inline-block');
-      $('#course-' + courseIndex + ' .arrow-up').hide();
+      $('.courseextra-' + courseIndex).slideUp('slow');
+      $('.course-' + courseIndex + ' .arrow-down').css('display', 'inline-block');
+      $('.course-' + courseIndex + ' .arrow-up').hide();
       courseShowingIndex = -1;
     } else {
-      $('#courseextra-' + courseShowingIndex).slideUp('slow');
-      $('#course-' + courseShowingIndex + ' .arrow-down').css('display', 'inline-block');
-      $('#course-' + courseShowingIndex + ' .arrow-up').hide();
-      $('#courseextra-' + courseIndex).slideDown('slow');
-      $('#course-' + courseIndex + ' .arrow-down').hide();
-      $('#course-' + courseIndex + ' .arrow-up').css('display', 'inline-block');
+      $('.courseextra-' + courseShowingIndex).slideUp('slow');
+      $('.course-' + courseShowingIndex + ' .arrow-down').css('display', 'inline-block');
+      $('.course-' + courseShowingIndex + ' .arrow-up').hide();
+      $('.courseextra-' + courseIndex).slideDown('slow');
+      $('.course-' + courseIndex + ' .arrow-down').hide();
+      $('.course-' + courseIndex + ' .arrow-up').css('display', 'inline-block');
       courseShowingIndex = courseIndex;
     }
   });
 
   $('.arrow_box_close').click(function () {
     const courseIndex = $(this).attr("data-courseindex");
-    $('#courseextra-' + courseIndex).slideUp();
+    $('.courseextra-' + courseIndex).slideUp();
     courseShowingIndex = -1;
   });
 };

apps/src/responsive.jsx
@@ -1,0 +1,142 @@
+/* A very simple responsive layout system.
+ */
+
+import $ from 'jquery';
+import { makeEnum } from '@cdo/apps/utils';
+import styleConstants from './styleConstants';
+
+export default class Responsive {
+
+  constructor(responsiveWindowWidth) {
+    if (responsiveWindowWidth) {
+      this.responsiveWindowWidth = responsiveWindowWidth;
+    } else {
+      this.responsiveWindowWidth = Responsive.DefaultResponsiveWindowWidths;
+    }
+  }
+
+  static ResponsiveSize = makeEnum('lg', 'md', 'sm', 'xs');
+
+  // Default window widths that are the starting points for each width category.
+  static DefaultResponsiveWindowWidths = {
+    [Responsive.ResponsiveSize.lg]: 1024,
+    [Responsive.ResponsiveSize.md]: 720,
+    [Responsive.ResponsiveSize.sm]: 650,
+    [Responsive.ResponsiveSize.xs]: 0
+  };
+
+  /**
+   * Gets the container width.
+   * Returns either a number (e.g. 970) or a string (e.g. "97%").
+   */
+  getResponsiveContainerWidth() {
+    const windowWidth = $(window).width();
+
+    // The content will be capped at this width.
+    const contentWidth = styleConstants['content-width'];
+
+    // Because we don't want content touching the edge of the browser window,
+    // we want the browser window to be this additional number of pixels wide
+    // to create a margin on the edges of the content.
+    const contentAdditionalWidth = 50;
+
+    if (windowWidth >= contentWidth + contentAdditionalWidth) {
+      // The content container will be at its full potential width.
+      return contentWidth;
+    } else {
+      // The content container will be less than its full potential width,
+      // and 3% of the window width will be used as left/right margins so that
+      // the content doesn't touch the edges of the window.
+      return "97%";
+    }
+  }
+
+  /**
+   * Returns the window width that is the starting point for a width category.
+   *
+   * @param {string} id - "xs", "sm", "md", or "lg"
+   */
+  getResponsiveWindowWidth(category) {
+    return this.responsiveWindowWidth[category];
+  }
+
+  /**
+   * Returns whether provided category is active, given current window width.
+   * e.g. called with "md" when window width >= 820px returns true.
+   *
+   * @param {string} id - "xs", "sm", "md", or "lg"
+   */
+  isResponsiveCategoryActive(category) {
+    return $(window).width() >= this.responsiveWindowWidth[category];
+  }
+
+  /**
+   * Returns whether provided category is inactive, given current window width.
+   * e.g. called with "md" when window width < 820px returns false.
+   *
+   * @param {string} id - "xs", "sm", "md", or "lg"
+   */
+  isResponsiveCategoryInactive(category) {
+    return $(window).width() < this.responsiveWindowWidth[category];
+  }
+
+  /**
+   * From a set of values provided, returns the appropriate one for the current
+   * window width.
+   * Note that we default to the largest-provided value that is not for a width
+   * that's greater than the current window width.  e.g. If the window width is
+   * "md" then we use the provided "md" width, otherwise the provided "sm" width,
+   * otherwise the provided "xs" width.
+   * Note also that when the value being returned is a number, it's converted into
+   * a percentage string.  e.g. 4 becomes "4%"
+   *
+   * @param {Object} values - A set of values from which we want one.
+   * @param {number|string} values.xs - Value returned on extra-small layout.
+   * @param {number|string} values.sm - Value returned on small layout.
+   * @param {number|string} values.md - Value returned on medium layout.
+   * @param {number|string} values.lg - Value returned on large layout.
+   */
+
+  getResponsiveValue(values) {
+    const windowWidth = $(window).width();
+
+    let value;
+    if (windowWidth >= this.responsiveWindowWidth[Responsive.ResponsiveSize.lg]) {
+      if (values.lg) {
+        value = values.lg;
+      } else if (values.md) {
+         value = values.md;
+      } else if (values.sm) {
+         value = values.sm;
+      } else {
+        value = values.xs;
+      }
+    } else if (windowWidth >= this.responsiveWindowWidth[Responsive.ResponsiveSize.md]) {
+      if (values.md) {
+         value = values.md;
+      } else if (values.sm) {
+         value = values.sm;
+      } else {
+        value = values.xs;
+      }
+    } else if (windowWidth >= this.responsiveWindowWidth[Responsive.ResponsiveSize.sm]) {
+      if (values.sm) {
+         value = values.sm;
+      } else {
+        value = values.xs;
+      }
+    } else if (values.xs) {
+      value = values.xs;
+    }
+
+    if (value) {
+      if (typeof(value) === "number") {
+        return value;
+      } else if (typeof(value) === "string") {
+        return value;
+      } else if (typeof(value) === "object") {
+        return value;
+      }
+    }
+  }
+}

apps/src/templates/ContentContainer.jsx
@@ -1,7 +1,9 @@
 import React, {Component, PropTypes} from 'react';
+import Responsive from '../responsive';
+import styleConstants from '../styleConstants';
 import FontAwesome from './FontAwesome';
 import color from "../util/color";
-import styleConstants from '../styleConstants';
+import Radium from 'radium';
 
 // ContentContainer provides a full-width container which will render whatever
 // children are passed to it. The component is useful for creating clear,
@@ -14,17 +16,62 @@ const contentWidth = styleConstants['content-width'];
 const styles = {
   box: {
     width: contentWidth,
-    marginBottom: 60,
+    marginBottom: 60
   },
-  heading: {
+  boxResponsive: {
+    width: '100%',
+    marginBottom: 60
+  },
+  headingBox: {
     paddingRight: 10,
     paddingTop: 10,
     paddingBottom: 20,
-    fontSize: 24,
-    fontFamily: 'Gotham 3r',
+    overflow: 'hidden',
     zIndex: 2,
+    position: 'relative'
+  },
+  headingText: {
+    fontFamily: 'Gotham 3r',
+    fontSize: 24,
+    lineHeight: '26px',
     color: color.charcoal,
-    width: contentWidth
+    float: 'left',
+    paddingRight: 20
+  },
+  headingTextRtl: {
+    fontFamily: 'Gotham 3r',
+    fontSize: 24,
+    lineHeight: '26px',
+    color: color.charcoal,
+    float: 'right',
+    paddingLeft: 20
+  },
+  standaloneLinkBox: {
+    paddingTop: 10,
+    position: 'relative',
+    clear: 'both'
+  },
+  linkBox: {
+    display: 'inline',
+    position: 'absolute',
+    bottom: 20,
+    right: 0
+  },
+  linkBoxRtl: {
+    display: 'inline',
+    float: 'left',
+    paddingLeft: 10,
+    position: 'absolute',
+    bottom: 20,
+    left: 0
+  },
+  linkBoxBottom: {
+    display: 'inline',
+    left: 0
+  },
+  linkBoxRtlBottom: {
+    display: 'inline',
+    right: 0
   },
   description: {
     fontSize: 14,
@@ -32,9 +79,10 @@ const styles = {
     fontFamily: 'Gotham 3r',
     zIndex: 2,
     color: color.charcoal,
-    width: contentWidth,
+    width: '100%',
     marginTop: -10,
-    marginBottom: 10
+    marginBottom: 10,
+    clear: 'both'
   },
   linkToViewAll: {
     color: color.teal,
@@ -57,15 +105,6 @@ const styles = {
     fontWeight: 'bold',
     marginRight: 15,
   },
-  linkBox: {
-    display: 'inline',
-    float: 'right',
-  },
-  linkBoxRtl: {
-    display: 'inline',
-    float: 'left',
-    paddingLeft: 10,
-  },
   children: {
     justifyContent: 'space-between',
     flexWrap: 'wrap'
@@ -86,30 +125,33 @@ class ContentContainer extends Component {
     link: PropTypes.string,
     isRtl: PropTypes.bool.isRequired,
     description: PropTypes.string,
+    responsive: PropTypes.instanceOf(Responsive)
   };
 
   render() {
-    const { heading, link, linkText, description, isRtl }= this.props;
-    const icon = isRtl ? "chevron-left" : "chevron-right";
+    const { heading, link, linkText, description, isRtl, responsive }= this.props;
+
+    const showLinkTop =
+      (!responsive || responsive.isResponsiveCategoryActive('lg')) &&
+      link && linkText;
+    const showLinkBottom =
+      responsive && responsive.isResponsiveCategoryInactive('lg') &&
+      link && linkText;
 
     return (
-      <div style={styles.box}>
+      <div style={responsive ? styles.boxResponsive : styles.box}>
         {(heading || (link && linkText)) && (
-          <div style={styles.heading}>
-            {heading}
-            {link && linkText &&
-              <div style={isRtl ? styles.linkBoxRtl : styles.linkBox}>
-                <a href={link}>
-                  {isRtl && <FontAwesome icon={icon} style={styles.chevronRtl}/>}
-                  <div style={styles.linkToViewAll}>
-                    {linkText}
-                  </div>
-                </a>
-                <a href={link} style={{textDecoration:'none'}}>
-                  {!isRtl && <FontAwesome icon={icon} style={styles.chevron}/>}
-                </a>
-              </div>
-            }
+          <div style={styles.headingBox}>
+            <div style={isRtl ? styles.headingTextRtl : styles.headingText}>
+              {heading}
+            </div>
+            {showLinkTop && (
+              <Link
+                link={link}
+                linkText={linkText}
+                isRtl={isRtl}
+              />
+            )}
           </div>
         )}
         {description && (
@@ -126,10 +168,54 @@ class ContentContainer extends Component {
             );
           })}
         </div>
+        {showLinkBottom && (
+          <div style={styles.standaloneLinkBox}>
+            <Link
+              link={link}
+              linkText={linkText}
+              isRtl={isRtl}
+              bottom={true}
+            />
+          </div>
+        )}
         <div style={styles.clear}/>
       </div>
     );
   }
 }
 
-export default ContentContainer;
+class Link extends Component {
+  static propTypes = {
+    linkText: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    isRtl: PropTypes.bool.isRequired,
+    bottom: PropTypes.bool
+  };
+
+  render() {
+    const { link, linkText, isRtl, bottom }= this.props;
+    let linkBoxStyle;
+    if (isRtl) {
+      linkBoxStyle = bottom ? styles.linkBoxRtlBottom : styles.linkBoxRtl;
+    } else {
+      linkBoxStyle = bottom ? styles.linkBoxBottom : styles.linkBox;
+    }
+    const icon = isRtl ? "chevron-left" : "chevron-right";
+
+    return (
+      <div style={linkBoxStyle}>
+        <a href={link}>
+          {isRtl && <FontAwesome icon={icon} style={styles.chevronRtl}/>}
+          <div style={styles.linkToViewAll}>
+            {linkText}
+          </div>
+        </a>
+        <a href={link} style={{textDecoration:'none'}}>
+          {!isRtl && <FontAwesome icon={icon} style={styles.chevron}/>}
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Radium(ContentContainer);

apps/src/templates/HeaderBanner.jsx
@@ -5,13 +5,20 @@
 
 import React from 'react';
 import color from "../util/color";
+import Responsive from '../responsive';
 
 const styles = {
   headerBanner: {
     height: 260
   },
+  headerBannerResponsive: {
+    marginBottom: 61
+  },
   headerBannerShort: {
     height: 140
+  },
+  headerBannerShortResponsive: {
+    marginBottom: 61
   },
   bannerHeading: {
     fontFamily: '"Gotham 7r", sans-serif',
@@ -19,6 +26,14 @@ const styles = {
     fontSize: 32,
     marginBottom: 10,
     lineHeight: '40px'
+  },
+  bannerHeadingResponsive: {
+    fontFamily: '"Gotham 7r", sans-serif',
+    color: color.white,
+    fontSize: 32,
+    marginBottom: 10,
+    lineHeight: '40px',
+    height: 240
   },
   bannerHeadingShort: {
     fontFamily: '"Gotham 7r", sans-serif',
@@ -28,18 +43,43 @@ const styles = {
     lineHeight: '40px',
     marginTop: -20
   },
+  bannerHeadingShortResponsive: {
+    fontFamily: '"Gotham 7r", sans-serif',
+    color: color.white,
+    fontSize: 32,
+    marginBottom: 10,
+    lineHeight: '40px',
+    marginTop: -20,
+    height: 120
+  },
   bannerSubHeading: {
     fontFamily: '"Gotham 4r", sans-serif',
     color: color.white,
     fontSize: 16,
-    lineHeight: '32px',
+    lineHeight: '21px',
+    marginBottom: 10
+  },
+  bannerSubHeadingResponsive: {
+    fontFamily: '"Gotham 4r", sans-serif',
+    color: color.dark_charcoal,
+    fontSize: 16,
+    lineHeight: '21px',
     marginBottom: 10
   },
   bannerDescription: {
     fontFamily: '"Gotham 4r", sans-serif',
     color: color.white,
     fontSize: 16,
+    lineHeight: '21px',
     width: 600,
+    marginBottom: 20,
+  },
+  bannerDescriptionResponsive: {
+    fontFamily: '"Gotham 4r", sans-serif',
+    color: color.dark_charcoal,
+    fontSize: 16,
+    lineHeight: '21px',
+    width: '80%',
     marginBottom: 20,
   }
 };
@@ -50,22 +90,36 @@ const HeaderBanner = React.createClass({
     subHeadingText: React.PropTypes.string,
     description: React.PropTypes.string,
     children: React.PropTypes.node,
-    short: React.PropTypes.bool
+    short: React.PropTypes.bool,
+    responsive: React.PropTypes.instanceOf(Responsive)
   },
 
   render() {
-    const {short, headingText, subHeadingText, description} = this.props;
+    const {short, headingText, subHeadingText, description, responsive} = this.props;
+
+    let headerStyle, headingStyle, subHeadingStyle, descriptionStyle;
+    if (responsive && responsive.isResponsiveCategoryInactive('md')) {
+      headerStyle = short ? styles.headerBannerShortResponsive : styles.headerBannerResponsive;
+      headingStyle = short ? styles.bannerHeadingShortResponsive : styles.bannerHeadingResponsive;
+      subHeadingStyle = styles.bannerSubHeadingResponsive;
+      descriptionStyle = styles.bannerDescriptionResponsive;
+    } else {
+      headerStyle = short ? styles.headerBannerShort : styles.headerBanner;
+      headingStyle = short ? styles.bannerHeadingShort : styles.bannerHeading;
+      subHeadingStyle = styles.bannerSubHeading;
+      descriptionStyle = styles.bannerDescription;
+    }
 
     return (
-      <div style={short ? styles.headerBannerShort : styles.headerBanner}>
-        <div style={short ? styles.bannerHeadingShort : styles.bannerHeading}>
+      <div style={headerStyle}>
+        <div style={headingStyle}>
           {headingText || <span>&nbsp;</span>}
         </div>
-        <div style={styles.bannerSubHeading}>
+        <div style={subHeadingStyle}>
           {subHeadingText || <span>&nbsp;</span>}
         </div>
         {description && (
-          <div style={styles.bannerDescription}>
+          <div style={descriptionStyle}>
             {description}
           </div>
         )}

apps/src/templates/studioHomepages/CourseBlocks.jsx
@@ -4,14 +4,9 @@ import ReactDOM from 'react-dom';
 import ContentContainer from '../ContentContainer';
 import CourseBlocksTools from './CourseBlocksTools';
 import ProtectedStatefulDiv from '../ProtectedStatefulDiv';
+import Responsive from '../../responsive';
 import i18n from "@cdo/locale";
 import {pegasus} from '@cdo/apps/lib/util/urlHelpers';
-
-const styles = {
-  paddedBottom: {
-    marginBottom: 60
-  }
-};
 
 export class CourseBlocksCsf extends Component {
   static propTypes = {
@@ -94,7 +89,7 @@ export class CourseBlocksCsfNonEnglish extends Component {
 
   render() {
     return (
-      <div style={styles.paddedBottom}>
+      <div>
         <div className="row">
           <ProtectedStatefulDiv ref="course1"/>
           <ProtectedStatefulDiv ref="course2"/>
@@ -158,7 +153,8 @@ export class CourseBlocksHoc extends Component {
 export class CourseBlocksAll extends Component {
   static propTypes = {
     isEnglish: PropTypes.bool.isRequired,
-    isRtl: PropTypes.bool.isRequired
+    isRtl: PropTypes.bool.isRequired,
+    responsive: PropTypes.instanceOf(Responsive).isRequired
   };
 
   componentDidMount() {
@@ -174,6 +170,7 @@ export class CourseBlocksAll extends Component {
           link={'/home/#recent-courses'}
           linkText={i18n.viewMyRecentCourses()}
           isRtl={this.props.isRtl}
+          responsive={this.props.responsive}
         >
           <CourseBlocksCsf isEnglish={this.props.isEnglish}/>
         </ContentContainer>
@@ -184,6 +181,7 @@ export class CourseBlocksAll extends Component {
           isRtl={this.props.isRtl}
           linkText={i18n.teacherCourseHocLinkText()}
           link={pegasus('/hourofcode/overview')}
+          responsive={this.props.responsive}
         >
           <CourseBlocksHoc rowCount={1}/>
         </ContentContainer>
@@ -191,6 +189,7 @@ export class CourseBlocksAll extends Component {
         <CourseBlocksTools
           isEnglish={this.props.isEnglish}
           isRtl={this.props.isRtl}
+          responsive={this.props.responsive}
         />
       </div>
     );

commit 17b50cf9450ddf45fa867d1c0ff4ef3e208d9354

Courses: fix recent changes
Fix two regressions introduced in https://github.com/code-dot-org/code-dot-org/pull/17256.  The course blocks shown for teachers and students were unnecessarily swapped.  And the header image was being cropped at the top when the browser got small.

apps/src/templates/studioHomepages/CourseBlocksStudentGradeBands.jsx
@@ -12,19 +12,19 @@ class CourseBlocksStudentGradeBands extends Component {
 
   cards = [
     {
-      heading: i18n.courseBlocksGradeBandsElementary(),
-      description: i18n.courseBlocksGradeBandsElementaryDescription(),
-      path: '/educate/curriculum/elementary-school'
+      heading: i18n.courseBlocksGradeBandsK5(),
+      description: i18n.courseBlocksGradeBandsK5Description(),
+      path: '/student/elementary'
     },
     {
-      heading: i18n.courseBlocksGradeBandsMiddle(),
-      description: i18n.courseBlocksGradeBandsMiddleDescription(),
-      path: '/educate/curriculum/middle-school'
+      heading: i18n.courseBlocksGradeBands612(),
+      description: i18n.courseBlocksGradeBands612Description(),
+      path: '/student/middle-high'
     },
     {
-      heading: i18n.courseBlocksGradeBandsHigh(),
-      description: i18n.courseBlocksGradeBandsHighDescription(),
-      path: '/educate/curriculum/high-school'
+      heading: i18n.courseBlocksGradeBandsUniversity(),
+      description: i18n.courseBlocksGradeBandsUniversityDescription(),
+      path: '/student/university'
     }
   ];
 

apps/src/templates/studioHomepages/CourseBlocksTeacherGradeBands.jsx
@@ -12,19 +12,19 @@ class CourseBlocksTeacherGradeBands extends Component {
 
   cards = [
     {
-      heading: i18n.courseBlocksGradeBandsK5(),
-      description: i18n.courseBlocksGradeBandsK5Description(),
-      path: '/student/elementary'
+      heading: i18n.courseBlocksGradeBandsElementary(),
+      description: i18n.courseBlocksGradeBandsElementaryDescription(),
+      path: '/educate/curriculum/elementary-school'
     },
     {
-      heading: i18n.courseBlocksGradeBands612(),
-      description: i18n.courseBlocksGradeBands612Description(),
-      path: '/student/middle-high'
+      heading: i18n.courseBlocksGradeBandsMiddle(),
+      description: i18n.courseBlocksGradeBandsMiddleDescription(),
+      path: '/educate/curriculum/middle-school'
     },
     {
-      heading: i18n.courseBlocksGradeBandsUniversity(),
-      description: i18n.courseBlocksGradeBandsUniversityDescription(),
-      path: '/student/university'
+      heading: i18n.courseBlocksGradeBandsHigh(),
+      description: i18n.courseBlocksGradeBandsHighDescription(),
+      path: '/educate/curriculum/high-school'
     }
   ];
 

dashboard/app/assets/stylesheets/application.scss
@@ -474,7 +474,7 @@ img.video_thumbnail {
   width: 100%;
   height: 320px;
   z-index: -1000;
-  background-position: 90% 50%;
+  background-position: 90% 10%;
   background-size: cover;
 
   &.header-banner-short {