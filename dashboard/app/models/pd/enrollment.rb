2c18587d2a75837156d3b7ee9b3ab44e2e7c31ed
ashercodeorg <asher@code.org>
2017-09-01 12:53:26 -0500

    Define Pd::Enrollment#clear_data.

--- dashboard/app/models/pd/enrollment.rb
+++ dashboard/app/models/pd/enrollment.rb
@@ -44,22 +44,22 @@ class Pd::Enrollment < ActiveRecord::Base
   accepts_nested_attributes_for :school_info, reject_if: :check_school_info
   validates_associated :school_info
 
-  validates_presence_of :first_name
+  validates_presence_of :first_name, unless: :owner_deleted?
 
   # Some old enrollments, from before the first/last name split, don't have last names.
   # Require on all new enrollments.
-  validates_presence_of :last_name, unless: :created_before_name_split?
+  validates_presence_of :last_name, unless: -> {owner_deleted? || created_before_name_split?}
 
-  validates_presence_of :email
-  validates_confirmation_of :email
+  validates_presence_of :email, unless: :owner_deleted?
+  validates_confirmation_of :email, unless: :owner_deleted?
   validates_email_format_of :email, allow_blank: true
 
   validate :school_forbidden, if: -> {new_record? || school_changed?}
-  validates_presence_of :school_info, unless: :created_before_school_info?
-  validate :school_info_country_required, if: -> {new_record? || school_info_id_changed?}
+  validates_presence_of :school_info, unless: -> {owner_deleted? || created_before_school_info?}
+  validate :school_info_country_required, if: -> {!owner_deleted? && (new_record? || school_info_id_changed?)}
 
   before_validation :autoupdate_user_field
-  after_save :enroll_in_corresponding_online_learning, if: -> {user_id_changed? || email_changed?}
+  after_save :enroll_in_corresponding_online_learning, if: -> {!owner_deleted? && (user_id_changed? || email_changed?)}
 
   def self.for_user(user)
     where('email = ? OR user_id = ?', user.email, user.id)
@@ -96,6 +96,12 @@ class Pd::Enrollment < ActiveRecord::Base
 
   def has_user?
     user_id
+  end
+
+  # Returns whether the enrollment owner is deleted. If there is no owner, returns false.
+  # @return [Boolean] Whether the enrollment owner is deleted.
+  def owner_deleted?
+    user_id && User.with_deleted.find_by_id(user_id).deleted?
   end
 
   def completed_survey?
@@ -235,10 +241,21 @@ class Pd::Enrollment < ActiveRecord::Base
     school_info.try :effective_school_district_name
   end
 
+  # Removes the name and email information stored within this Pd::Enrollment.
+  def clear_data
+    update!(
+      name: '',
+      first_name: nil,
+      last_name: nil,
+      email: ''
+    )
+  end
+
   protected
 
   def autoupdate_user_field
-    self.user = user || resolve_user
+    resolved_user = resolve_user
+    self.user = resolve_user if resolved_user
   end
 
   def enroll_in_corresponding_online_learning
