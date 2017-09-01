d6c7e689b16ea89ffaf5e6660653cb900dbeada9
Brendan Reville <breville@users.noreply.github.com>
2017-08-31 19:39:08 -0700

    Courses: make responsive (#17256)
    * Courses: make responsive
    
    Initial work.  More to come.
    
    * Courses: remove redundant container
    
    * Courses: make course explorer optionally responsive
    
    Still works on both dashboard & pegasus.  When there is a smaller course explorer available, opening/closing items in it also opens/closes the same items in the desktop width
    version, and vice versa.
    
    * Courses: make header responsive
    
    * Courses: fix responsive stories
    
    * Courses: code review feedback
    
    * Course Explorer: ensure course image small on responsive pegasus
    
    * Courses: fix content container for RTL & non-responsive pages
    
    * Course: default responsive widths

--- apps/src/templates/studioHomepages/CourseBlocksTeacherGradeBands.jsx
+++ apps/src/templates/studioHomepages/CourseBlocksTeacherGradeBands.jsx
@@ -1,28 +1,30 @@
 import React, {Component, PropTypes} from 'react';
 import ContentContainer from '../ContentContainer';
 import CourseBlocksGradeBands from './CourseBlocksGradeBands';
+import Responsive from '../../responsive';
 import i18n from "@cdo/locale";
 
 class CourseBlocksTeacherGradeBands extends Component {
   static propTypes = {
-    isRtl: PropTypes.bool.isRequired
+    isRtl: PropTypes.bool.isRequired,
+    responsive: PropTypes.instanceOf(Responsive).isRequired
   };
 
   cards = [
     {
-      heading: i18n.courseBlocksGradeBandsElementary(),
-      description: i18n.courseBlocksGradeBandsElementaryDescription(),
-      path: '/educate/curriculum/elementary-school'
+      heading: i18n.courseBlocksGradeBandsK5(),
+      description: i18n.courseBlocksGradeBandsK5Description(),
+      path: '/student/elementary'
     },
     {
-      heading: i18n.courseBlocksGradeBandsMiddle(),
-      description: i18n.courseBlocksGradeBandsMiddleDescription(),
-      path: '/educate/curriculum/middle-school'
+      heading: i18n.courseBlocksGradeBands612(),
+      description: i18n.courseBlocksGradeBands612Description(),
+      path: '/student/middle-high'
     },
     {
-      heading: i18n.courseBlocksGradeBandsHigh(),
-      description: i18n.courseBlocksGradeBandsHighDescription(),
-      path: '/educate/curriculum/high-school'
+      heading: i18n.courseBlocksGradeBandsUniversity(),
+      description: i18n.courseBlocksGradeBandsUniversityDescription(),
+      path: '/student/university'
     }
   ];
 
@@ -32,10 +34,12 @@ class CourseBlocksTeacherGradeBands extends Component {
         heading={i18n.courseBlocksGradeBandsContainerHeading()}
         description={i18n.courseBlocksGradeBandsContainerDescription()}
         isRtl={this.props.isRtl}
+        responsive={this.props.responsive}
       >
         <CourseBlocksGradeBands
           cards={this.cards}
           isRtl={this.props.isRtl}
+          responsive={this.props.responsive}
         />
       </ContentContainer>
     );
--- apps/src/templates/studioHomepages/GridContainer.jsx
+++ apps/src/templates/studioHomepages/GridContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styleConstants from '../../styleConstants';
+import Responsive from '../../responsive';
 
 const styles = {
   container: {
-    width: styleConstants['content-width'],
+    width: '100%',
   },
   item: {
     float: "left",
@@ -17,12 +17,19 @@ const GridContainer = React.createClass({
   propTypes: {
     numColumns: React.PropTypes.number.isRequired,
     children: React.PropTypes.arrayOf(React.PropTypes.node),
-    isRtl: React.PropTypes.bool.isRequired
+    isRtl: React.PropTypes.bool.isRequired,
+    responsive: React.PropTypes.instanceOf(Responsive).isRequired
   },
 
   render() {
-    const { numColumns, isRtl } = this.props;
-    const itemWidth = styles.container.width / numColumns;
+    const { numColumns, isRtl, responsive } = this.props;
+
+    // Calculate the width of each column, in percentage value.
+    const nonResponsiveWidthPercent = 100 / numColumns;
+
+    // Then determine the percentage string for each item.
+    const itemWidth = responsive.getResponsiveValue({lg: `${nonResponsiveWidthPercent}%` , md: '100%'});
+
     const itemStyle = isRtl ? styles.itemRtl : styles.item;
 
     return (
