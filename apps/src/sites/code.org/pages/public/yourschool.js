dee6c972302712f39d32aae15c499a7760db49d8
Jeremy Stone <jeremy@code.org>
2017-09-06 16:59:24 -0700

    Refactor how SchoolInfoManager JS is included

--- apps/src/sites/code.org/pages/public/yourschool.js
+++ apps/src/sites/code.org/pages/public/yourschool.js
@@ -2,6 +2,9 @@ import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import YourSchool from '@cdo/apps/templates/census2017/YourSchool';
+import SchoolInfoManager from '@cdo/apps/schoolInfoManager';
+
+window.SchoolInfoManager = SchoolInfoManager;
 
 $(document).ready(showYourSchool);
 
