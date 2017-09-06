ca974e9e08bc6c2bfd67d1f47d5e7b24ca585650
Brad Buchanan <brad@code.org>
2017-09-06 14:28:51 -0700

    Import PropTypes separately in applab/*
    Import PropTypes separately in AppLabCrosshairOverlay.jsx
    
    Import PropTypes separately in AppLabTooltipOverlay.jsx
    
    Import PropTypes separately in AppLabView.jsx
    
    Import PropTypes separately in AppLabVisualizationColumn.jsx
    
    Import PropTypes separately in DesignModeBox.jsx
    
    Import PropTypes separately in DesignModeHeaders.jsx
    
    Import PropTypes separately in designProperties.jsx
    
    Import PropTypes separately in DesignToolbox.jsx
    
    Import PropTypes separately in DesignToolboxElement.jsx
    
    Import PropTypes separately in DesignWorkspace.jsx
    
    Import PropTypes separately in ElementSelect.jsx
    
    Import PropTypes separately in ImportProjectDialog.jsx
    
    Import PropTypes separately in ImportScreensDialog.jsx
    
    Import PropTypes separately in PhoneFrame.jsx
    
    Import PropTypes separately in PlaySpaceHeader.jsx
    
    Import PropTypes separately in ProtectedDesignWorkspace.jsx
    
    Import PropTypes separately in ScreenSelector.jsx
    
    Import PropTypes separately in Visualization.jsx

--- apps/src/applab/PlaySpaceHeader.jsx
+++ apps/src/applab/PlaySpaceHeader.jsx
@@ -1,6 +1,6 @@
 /** @file Row of controls above the visualization. */
 
-import React from 'react';
+import React, {PropTypes} from 'react';
 
 import {ApplabInterfaceMode} from './constants';
 import msg from '@cdo/locale';
@@ -11,20 +11,20 @@ import ToggleGroup from '../templates/ToggleGroup';
 
 var PlaySpaceHeader = React.createClass({
   propTypes: {
-    channelId: React.PropTypes.string.isRequired,
-    hasDataMode: React.PropTypes.bool.isRequired,
-    hasDesignMode: React.PropTypes.bool.isRequired,
-    isEditingProject: React.PropTypes.bool.isRequired,
-    isShareView: React.PropTypes.bool.isRequired,
-    interfaceMode: React.PropTypes.oneOf([
+    channelId: PropTypes.string.isRequired,
+    hasDataMode: PropTypes.bool.isRequired,
+    hasDesignMode: PropTypes.bool.isRequired,
+    isEditingProject: PropTypes.bool.isRequired,
+    isShareView: PropTypes.bool.isRequired,
+    interfaceMode: PropTypes.oneOf([
       ApplabInterfaceMode.CODE,
       ApplabInterfaceMode.DESIGN,
       ApplabInterfaceMode.DATA
     ]).isRequired,
-    playspacePhoneFrame: React.PropTypes.bool,
-    screenIds: React.PropTypes.array.isRequired,
-    onScreenCreate: React.PropTypes.func.isRequired,
-    onInterfaceModeChange: React.PropTypes.func.isRequired
+    playspacePhoneFrame: PropTypes.bool,
+    screenIds: PropTypes.array.isRequired,
+    onScreenCreate: PropTypes.func.isRequired,
+    onInterfaceModeChange: PropTypes.func.isRequired
   },
 
   render: function () {
