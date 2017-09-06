8f735133e15477a7680094ef4fd96d67aa31b2a5
Brent Van Minnen <bjvanminnen@gmail.com>
2017-09-06 14:27:40 -0700

    UI for hidden/unhidden section tables

--- apps/src/templates/teacherDashboard/OwnedSections.jsx
+++ apps/src/templates/teacherDashboard/OwnedSections.jsx
@@ -11,6 +11,8 @@ import {
   beginImportRosterFlow,
 } from './teacherSectionsRedux';
 import i18n from '@cdo/locale';
+import color from '@cdo/apps/util/color';
+import styleConstants from '@cdo/apps/styleConstants';
 import AddSectionDialog from "./AddSectionDialog";
 import EditSectionDialog from "./EditSectionDialog";
 import SetUpSections from '../studioHomepages/SetUpSections';
@@ -19,6 +21,17 @@ const styles = {
   button: {
     marginBottom: 20,
     marginRight: 5,
+  },
+  buttonContainer: {
+    width: styleConstants['content-width'],
+    textAlign: 'right',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  hiddenSectionLabel: {
+    fontSize: 14,
+    paddingBottom: 5,
+    color: color.charcoal
   }
 };
 
@@ -29,10 +42,15 @@ class OwnedSections extends React.Component {
 
     // redux provided
     sectionIds: PropTypes.arrayOf(PropTypes.number).isRequired,
+    sections: PropTypes.object.isRequired,
     asyncLoadComplete: PropTypes.bool.isRequired,
     beginEditingNewSection: PropTypes.func.isRequired,
     beginEditingSection: PropTypes.func.isRequired,
     beginImportRosterFlow: PropTypes.func.isRequired,
+  };
+
+  state = {
+    viewHidden: false
   };
 
   componentDidMount() {
@@ -51,17 +69,36 @@ class OwnedSections extends React.Component {
 
   beginEditingSection = section => this.props.beginEditingSection(section.id);
 
+  toggleViewHidden = () => {
+    this.setState({
+      viewHidden: !this.state.viewHidden
+    });
+  }
+
   render() {
     const {
       isRtl,
       sectionIds,
+      sections,
       asyncLoadComplete,
     } = this.props;
+    const { viewHidden } = this.state;
+
     if (!asyncLoadComplete) {
       return null;
     }
 
     const hasSections = sectionIds.length > 0;
+
+    let hiddenSectionIds = [];
+    let unhiddenSectionIds = [];
+    sectionIds.forEach(id => {
+      if (sections[id].hidden) {
+        hiddenSectionIds.push(id);
+      } else {
+        unhiddenSectionIds.push(id);
+      }
+    });
 
     return (
       <div className="uitest-owned-sections">
@@ -78,9 +115,31 @@ class OwnedSections extends React.Component {
               color={Button.ButtonColor.gray}
             />
             <SectionTable
-              sectionIds={sectionIds}
+              sectionIds={unhiddenSectionIds}
               onEdit={this.beginEditingSection}
             />
+            <div style={styles.buttonContainer}>
+              {/* TODO: i18n */}
+              {hiddenSectionIds.length > 0 && (
+                <Button
+                  onClick={this.toggleViewHidden}
+                  icon={viewHidden ? "caret-up" : "caret-down"}
+                  text={viewHidden ? "Hide hidden sections" : "View hidden sections"}
+                  color={Button.ButtonColor.gray}
+                />
+              )}
+            </div>
+            {viewHidden &&
+              <div>
+                <div style={styles.hiddenSectionLabel}>
+                  Hidden Sections
+                </div>
+                <SectionTable
+                  sectionIds={hiddenSectionIds}
+                  onEdit={this.beginEditingSection}
+                />
+              </div>
+            }
           </div>
         )}
         <RosterDialog/>
@@ -94,6 +153,7 @@ export const UnconnectedOwnedSections = OwnedSections;
 
 export default connect(state => ({
   sectionIds: state.teacherSections.sectionIds,
+  sections: state.teacherSections.sections,
   asyncLoadComplete: state.teacherSections.asyncLoadComplete,
 }), {
   beginEditingNewSection,
--- apps/test/unit/templates/teacherDashboard/OwnedSectionsTest.js
+++ apps/test/unit/templates/teacherDashboard/OwnedSectionsTest.js
@@ -10,11 +10,55 @@ import Button from '@cdo/apps/templates/Button';
 import RosterDialog from "@cdo/apps/templates/teacherDashboard/RosterDialog";
 import AddSectionDialog from "@cdo/apps/templates/teacherDashboard/AddSectionDialog";
 import EditSectionDialog from "@cdo/apps/templates/teacherDashboard/EditSectionDialog";
-import SectionTable from '@cdo/apps/templates/teacherDashboard/SectionTable';
 import SetUpSections from '@cdo/apps/templates/studioHomepages/SetUpSections';
 
+const sections = {
+  11: {
+    id: 11,
+    courseId: 29,
+    scriptId: null,
+    name: "my_section",
+    loginType: "word",
+    grade: "3",
+    providerManaged: false,
+    stageExtras: false,
+    pairingAllowed: true,
+    studentCount: 10,
+    code: "PMTKVH",
+    hidden: false,
+  },
+  12: {
+    id: 12,
+    courseId: 29,
+    scriptId: 168,
+    name: "section_with_course_and_script",
+    loginType: "google_classroom",
+    grade: "3",
+    providerManaged: true,
+    stageExtras: false,
+    pairingAllowed: true,
+    studentCount: 0,
+    code: "G-1234567",
+    hidden: false,
+  },
+  13: {
+    id: 13,
+    name: 'foo',
+    loginType: 'email',
+    providerManaged: false,
+    stageExtras: false,
+    pairingAllowed: false,
+    studentCount: 0,
+    code: 'asdf',
+    courseId: null,
+    scriptId: 36,
+    hidden: true,
+  }
+};
+
 const defaultProps = {
-  sectionIds: [1,2,3],
+  sectionIds: [11, 12, 13],
+  sections: sections,
   asyncLoadComplete: true,
   beginEditingNewSection: () => {},
   beginEditingSection: () => {},
@@ -41,27 +85,53 @@ describe('OwnedSections', () => {
     );
   });
 
-  it('renders SectionTable when there are sections', () => {
+  it('renders a SectionTable with no extra button if no hidden sections', () => {
+    const wrapper = shallow(
+      <OwnedSections
+        {...defaultProps}
+        sectionIds={[11,12]}
+      />
+    );
+    expect(wrapper.find('SectionTable').length).to.equal(1);
+    // No second button to view hidden
+    expect(wrapper.find('Button').length).to.equal(1);
+  });
+
+  it('renders a SectionTable with view button if hidden sections', () => {
     const wrapper = shallow(
       <OwnedSections
         {...defaultProps}
       />
     );
-    const instance = wrapper.instance();
-    expect(wrapper).to.containMatchingElement(
-      <div>
-        <div>
-          <Button
-            text="New section"
-            onClick={instance.addSection}
-          />
-        <SectionTable sectionIds={[1,2,3]} onEdit={instance.handleEditRequest}/>
-        </div>
-        <RosterDialog/>
-        <AddSectionDialog/>
-        <EditSectionDialog/>
-      </div>
+    expect(wrapper.find('SectionTable').length).to.equal(1);
+    // Second button to view hidden
+    expect(wrapper.find('Button').length).to.equal(2);
+    expect(wrapper.find('Button').at(1).props().text, 'View hidden sections');
+  });
+
+  it('renders two SectionsTables if view hidden sections clicked', () => {
+    const wrapper = shallow(
+      <OwnedSections
+        {...defaultProps}
+      />
     );
+    wrapper.find('Button').at(1).simulate('click');
+    expect(wrapper.find('SectionTable').length).to.equal(2);
+    expect(wrapper.find('SectionTable').at(0).props().sectionIds).to.deep.equal([11,12]);
+    expect(wrapper.find('SectionTable').at(1).props().sectionIds).to.deep.equal([13]);
+    expect(wrapper.find('Button').at(1).props().text).to.equal('Hide hidden sections');
+  });
+
+  it('renders just unhidden SectionsTable if hide sections clicked', () => {
+    const wrapper = shallow(
+      <OwnedSections
+        {...defaultProps}
+      />
+    );
+    wrapper.find('Button').at(1).simulate('click');
+    wrapper.find('Button').at(1).simulate('click');
+    expect(wrapper.find('SectionTable').length).to.equal(1);
+    expect(wrapper.find('SectionTable').props().sectionIds).to.deep.equal([11,12]);
   });
 
   it('calls beginEditingNewSection with no arguments when button is clicked', () => {
@@ -74,7 +144,7 @@ describe('OwnedSections', () => {
     );
     expect(spy).not.to.have.been.called;
 
-    wrapper.find(Button).simulate('click', {fake: 'event'});
+    wrapper.find(Button).at(0).simulate('click', {fake: 'event'});
     expect(spy).to.have.been.calledOnce;
     expect(spy.firstCall.args).to.be.empty;
   });
