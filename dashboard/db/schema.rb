3bfda0440168aeeceec9da535deac2598fe06d83
Brent Van Minnen <bjvanminnen@gmail.com>
2017-09-08 14:20:52 -0700

    migration adding hidden column to section table

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905185009) do
+ActiveRecord::Schema.define(version: 20170908211608) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -824,6 +824,7 @@ ActiveRecord::Schema.define(version: 20170905185009) do
     t.datetime "first_activity_at"
     t.boolean  "pairing_allowed",   default: true,    null: false
     t.boolean  "sharing_disabled",  default: false,   null: false, comment: "Flag indicates the default sharing setting for a section and is used to determine students share setting when adding a new student to the section."
+    t.boolean  "hidden",            default: false,   null: false
     t.index ["code"], name: "index_sections_on_code", unique: true, using: :btree
     t.index ["course_id"], name: "fk_rails_20b1e5de46", using: :btree
     t.index ["user_id"], name: "index_sections_on_user_id", using: :btree
