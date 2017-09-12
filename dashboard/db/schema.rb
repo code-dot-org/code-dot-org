45c33968997944caa2e624e3b10e5e29f11ea0d1
Continuous Integration <dev@code.org>
2017-09-12 19:00:13 +0000

    staging content changes (-robo-commit)

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170908211608) do
+
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
     t.integer  "level_id"
