3054da68ae425a761cf7134674b7b10610884072
Clare Constantine <clare@code.org>
2017-09-13 10:32:44 -0700

    add missing teacher application mailer link verification test

--- dashboard/test/mailers/pd/teacher_application_mailer_test.rb
+++ dashboard/test/mailers/pd/teacher_application_mailer_test.rb
@@ -15,8 +15,17 @@ class TeacherApplicationMailerTest < ActionMailer::TestCase
     assert links_are_complete_urls?(email)
   end
 
-  test 'principal approval request links are valid urls' do
+  test 'principal approval request csd links are valid urls' do
     application = create :pd_teacher_application
+    School.expects(find: build(:public_school, name: 'Hogwarts School of Witchcraft & Wizardry'))
+    email = Pd::TeacherApplicationMailer.principal_approval_request(application)
+
+    assert links_are_complete_urls?(email)
+  end
+
+  test 'principal approval request csp links are valid urls' do
+    application_hash = build :pd_teacher_application_hash, course: 'csp'
+    application = build :pd_teacher_application, application_hash: application_hash, id: 123
     School.expects(find: build(:public_school, name: 'Hogwarts School of Witchcraft & Wizardry'))
     email = Pd::TeacherApplicationMailer.principal_approval_request(application)
 
