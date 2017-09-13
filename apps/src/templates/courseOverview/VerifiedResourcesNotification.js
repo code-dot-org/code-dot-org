6696ba040c9719ebdc9cc0b2c6275b890f7451f1
Brent Van Minnen <bjvanminnen@gmail.com>
2017-09-13 13:02:03 -0700

    i18n

--- apps/src/templates/courseOverview/VerifiedResourcesNotification.js
+++ apps/src/templates/courseOverview/VerifiedResourcesNotification.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Notification, { NotificationType } from '@cdo/apps/templates/Notification';
+import i18n from '@cdo/locale';
 
 export default function VerifiedResourcesAnnounce() {
   return (
     <Notification
       type={NotificationType.information}
-      notice="Accessing locked lessons and answer keys"
-      details="This course provides extra resources which are only available to verified teachers."
-      buttonText="Learn More"
+      notice={i18n.verifiedResourcesNotice()}
+      details={i18n.verifiedResourcesDetails()}
+      buttonText={i18n.learnMore()}
       buttonLink="https://support.code.org/hc/en-us/articles/115001550131"
       dismissible={true}
       isRtl={false}
