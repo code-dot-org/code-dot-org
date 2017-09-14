813b5a514ff62072c1d7147a6a7a0d91ff3f4164
Mehal Shah <mehal@code.org>
2017-09-13 18:14:43 -0700

    Minor refactor

--- dashboard/app/models/peer_review.rb
+++ dashboard/app/models/peer_review.rb
@@ -115,14 +115,7 @@ class PeerReview < ActiveRecord::Base
 
     if escalated? && user_level.best_result == Activity::UNREVIEWED_SUBMISSION_RESULT && !from_instructor
       # If this has been escalated, create a review for an instructor to review
-      PeerReview.find_or_create_by(
-        submitter: submitter,
-        reviewer: nil,
-        script: script,
-        level: level,
-        level_source_id: level_source_id,
-        status: 2
-      )
+      create_escalated_duplicate
       return
     end
 
@@ -152,14 +145,7 @@ class PeerReview < ActiveRecord::Base
       update_column :audit_trail, append_audit_trail("REJECTED by user id #{reviewer_id}")
     else
       # No consensus: escalate the review (i.e. create an escalated review based on this one)
-      PeerReview.find_or_create_by(
-        submitter: submitter,
-        reviewer: nil,
-        script: script,
-        level: level,
-        level_source_id: level_source_id,
-        status: 2
-      )
+      create_escalated_duplicate
       update_column :audit_trail, append_audit_trail("NO CONSENSUS after review by user id #{reviewer_id}")
     end
   end
@@ -274,5 +260,16 @@ class PeerReview < ActiveRecord::Base
 
   def append_audit_trail(message)
     self.audit_trail = (audit_trail || '') + "#{message} at #{Time.zone.now}\n"
+  end
+
+  def create_escalated_duplicate
+    PeerReview.find_or_create_by(
+      submitter: submitter,
+      reviewer: nil,
+      script: script,
+      level: level,
+      level_source_id: level_source_id,
+      status: 2
+    )
   end
 end
