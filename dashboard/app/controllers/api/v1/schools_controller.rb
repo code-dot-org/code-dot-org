efacf61890dde176f93441f925adfd8e762bf8e1
joannepolsky <joanne@code.org>
2017-09-15 14:18:07 -0700

    Fix rubocop offences (#17761)

--- dashboard/app/controllers/api/v1/schools_controller.rb
+++ dashboard/app/controllers/api/v1/schools_controller.rb
@@ -1,7 +1,6 @@
 require 'cdo/school_autocomplete'
 
 class Api::V1::SchoolsController < ApplicationController
-
   # GET /api/v1/school/<school_district_id>/<school_type>
   def index
     schools = School.where(school_district_id: params[:school_district_id], school_type: params[:school_type])
@@ -17,5 +16,4 @@ class Api::V1::SchoolsController < ApplicationController
     limit = [40, Integer(params[:limit])].min
     render json: SchoolAutocomplete.instance.get_matches(query, limit)
   end
-
 end
