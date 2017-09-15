3680e858350af80f5e4506245ae873976cd1550d
joannepolsky <joanne@code.org>
2017-09-15 13:36:36 -0700

    move school name suggestion logic f for auto-complete out of the cont… (#17633)

---
+++ lib/cdo/school_autocomplete.rb
@@ -0,0 +1,35 @@
+require 'singleton'
+require 'triez'
+
+class SchoolAutocomplete
+  include Singleton
+
+  def initialize
+    @schools = Triez.new value_type: :object
+    School.all.each do |school|
+      key = "#{school[:name]} #{school[:city]} #{school[:zip]}".downcase
+      @schools.change_all(:suffix, key) {school}
+    end
+  end
+
+  def get_matches(query, limit)
+    results = []
+    if query.length >= 3
+      @schools.search_with_prefix(query.downcase, limit: limit) do |_, school|
+        results << Serializer.new(school).attributes
+      end
+    end
+    return results
+  end
+
+  class Serializer < ActiveModel::Serializer
+    attributes :id, :name, :city, :state, :zip, :school_district_id
+    def name
+      object.name.titleize
+    end
+    def city
+      object.city.titleize
+    end
+  end
+
+end
