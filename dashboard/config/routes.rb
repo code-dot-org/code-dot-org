55ecf1eaa4277c2c396e935de772dd1dde04331c
Suresh Chanmugam <me@suresh.com>
2017-09-15 13:03:09 -0700

    Create controller with noop action and verify authorization is restricted to workshop admin role.

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -435,6 +435,8 @@ Dashboard::Application.routes.draw do
 
     get 'regional_partner_contact/new', to: 'regional_partner_contact#new'
     get 'regional_partner_contact/:contact_id/thanks', to: 'regional_partner_contact#thanks'
+
+    get 'regional_partner', to: 'regional_partner#search'
   end
 
   get '/dashboardapi/section_progress/:section_id', to: 'api#section_progress'
