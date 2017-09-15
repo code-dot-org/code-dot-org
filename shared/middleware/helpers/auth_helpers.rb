9a7664d28d1a341e7fe07d0cee09c19360425021
Caley Brock <caleybrock7@gmail.com>
2017-09-15 14:27:59 -0700

    Show error message if can't share

--- shared/middleware/helpers/auth_helpers.rb
+++ shared/middleware/helpers/auth_helpers.rb
@@ -62,13 +62,13 @@ def owns_section?(section_id)
 end
 
 # @param [Integer] student_id
-# @returns [Boolean] true iff the current user is the teacher for the student of the given id
-def teaches_student?(student_id)
-  return false unless student_id && current_user_id
+# @returns [Boolean] true iff the current user, or given user, is the teacher for the student of the given id
+def teaches_student?(student_id, user_id = current_user_id)
+  return false unless student_id && user_id
   DASHBOARD_DB[:sections].
       join(:followers, section_id: :sections__id).
       join(:users, id: :followers__student_user_id).
-      where(sections__user_id: current_user_id, sections__deleted_at: nil).
+      where(sections__user_id: user_id, sections__deleted_at: nil).
       where(followers__student_user_id: student_id, followers__deleted_at: nil).
       where(users__deleted_at: nil).
       any?
