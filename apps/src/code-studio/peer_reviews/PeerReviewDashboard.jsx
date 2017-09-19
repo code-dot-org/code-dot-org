c3aa8bcc6b05216d862bc15da6acd0ba0b2addb3
Mehal Shah <mehal@code.org>
2017-09-19 16:55:29 -0700

    PR feedback - I moved a lot of stuff from the dashboard class to the submission class to reduce code reuse

--- apps/src/code-studio/peer_reviews/PeerReviewDashboard.jsx
+++ apps/src/code-studio/peer_reviews/PeerReviewDashboard.jsx
@@ -1,72 +1,25 @@
 import React from 'react';
-import Spinner from '../pd/workshop_dashboard/components/spinner';
 import PeerReviewSubmissions from './PeerReviewSubmissions';
 import {Tabs, Tab} from 'react-bootstrap';
 
 class PeerReviewDashboard extends React.Component {
-  state = {}
-
-  componentDidMount() {
-    this.loadRequest = $.ajax({
-      method: 'GET',
-      url: '/api/v1/peer_review_submissions/index?filter=escalated',
-      dataType: 'json'
-    }).done(data => {
-      this.setState({
-        escalatedSubmissions: data
-      });
-    });
-
-    this.loadRequest = $.ajax({
-      method: 'GET',
-      url: '/api/v1/peer_review_submissions/index?filter=open',
-      dataType: 'json'
-    }).done(data => {
-      this.setState({
-        openSubmissions: data
-      });
-    });
-
-    this.loadRequest = $.ajax({
-      method: 'GET',
-      url: '/api/v1/peer_review_submissions/index',
-      dataType: 'json'
-    }).done(data => {
-      this.setState({
-        allSubmissions: data
-      });
-    });
-  }
-
   render() {
     return (
       <Tabs id="peer review dashboard" defaultActiveKey={1}>
         <Tab eventKey={1} title="Escalated Submissions">
-          {
-            this.state.escalatedSubmissions ? (
-              <PeerReviewSubmissions
-                submissions={this.state.escalatedSubmissions}
-              />
-            ) : (<Spinner/>)
-          }
+          <PeerReviewSubmissions
+            filterType="escalated"
+          />
         </Tab>
         <Tab eventKey={2} title="Open Submissions">
-          {
-            this.state.openSubmissions ? (
-              <PeerReviewSubmissions
-                submissions={this.state.openSubmissions}
-              />
-            ) : (<Spinner/>)
-          }
+          <PeerReviewSubmissions
+            filterType="open"
+          />
         </Tab>
         <Tab eventKey={3} title="All Submissions">
-          {
-            this.state.allSubmissions ? (
-              <PeerReviewSubmissions
-                submissions={this.state.allSubmissions}
-              />
-            ) : (<Spinner/>)
-          }
+          <PeerReviewSubmissions
+            filterType="all"
+          />
         </Tab>
       </Tabs>
     );
