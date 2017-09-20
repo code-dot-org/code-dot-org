79fff1e71ed59940e2fc8f15233cbe11599b9cdb
Mehal Shah <mehal@code.org>
2017-09-20 14:20:15 -0700

    PR feedback

--- apps/src/code-studio/peer_reviews/PeerReviewSubmissionData.story.jsx
+++ apps/src/code-studio/peer_reviews/PeerReviewSubmissionData.story.jsx
@@ -20,7 +20,7 @@ export default storybook => {
                 level_name: 'Documenting all that you know',
                 submission_date: '2/19/2017',
                 escalation_date: '4/3/2/2017',
-                id: 2
+                review_id: 1
               },
               {
                 submitter: 'Daenerys Targaryen',
@@ -29,7 +29,7 @@ export default storybook => {
                 level_name: 'Intro to Dracarys',
                 submission_date: '2/19/2017',
                 escalation_date: '4/3/2/2017',
-                id: 2
+                review_id: 2
               }
             ]}
           />
