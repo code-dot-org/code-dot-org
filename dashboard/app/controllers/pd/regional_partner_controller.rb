230d1711647f06aafde234691dcaf54517c4e65d
Suresh Chanmugam <me@suresh.com>
2017-09-20 10:12:40 -0700

    Enable search for Regional Partners by id or name.

--- dashboard/app/controllers/pd/regional_partner_controller.rb
+++ dashboard/app/controllers/pd/regional_partner_controller.rb
@@ -3,5 +3,17 @@ class Pd::RegionalPartnerController < ApplicationController
 
   # GET /pd/regional_partner/search
   def search
+    search_term = params[:search_term]
+    if search_term =~ /^\d+$/
+      @regional_partners = RegionalPartner.where(id: search_term)
+    elsif search_term
+      @regional_partners = RegionalPartner.where("name LIKE :partial_name", {partial_name: "%#{search_term}%"})
+    end
+
+    unless @regional_partners.try(:any?) || search_term.blank?
+      flash[:notice] = "No matching Regional Partners found. Showing all."
+    end
+    # display all regional partners if search does not return any results
+    @regional_partners = RegionalPartner.all unless @regional_partners.try(:any?)
   end
 end
