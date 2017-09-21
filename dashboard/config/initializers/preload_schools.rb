f2aaf38d9aa900d60011387326d4d2ddaa74edaa
Joanne Polsky <joanne@code.org>
2017-09-20 19:33:53 -0700

    Use CDO.log.info instead of puts

--- dashboard/config/initializers/preload_schools.rb
+++ dashboard/config/initializers/preload_schools.rb
@@ -3,9 +3,10 @@ require 'cdo/school_autocomplete'
 # Preload the SchoolAutocomplete singleton instance.
 # Skip if this is running a Rake task (e.g. rake db:setup) or when caching is disabled
 
-if File.basename($0) != 'rake' && !Rails.application.config.skip_preload_schools
-  startTime = Time.now
-  SchoolAutocomplete.instance
-  endTime = Time.now
-  puts "[#{endTime.strftime('%c')}] Preloaded schools in #{endTime - startTime} second(s)"
+unless File.basename($0) == 'rake'
+  unless Rails.application.config.skip_preload_schools
+    startTime = Time.now
+    SchoolAutocomplete.instance
+    CDO.log.info "Preloaded schools in #{Time.now - startTime} second(s)"
+  end
 end
