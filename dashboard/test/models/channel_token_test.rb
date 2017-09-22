ceec219eea6bf25556754bbc1878cd356f971104
Brent Van Minnen <bjvanminnen@gmail.com>
2017-09-22 16:58:55 -0700

    write storage_id for new channel_token rows

--- dashboard/test/models/channel_token_test.rb
+++ dashboard/test/models/channel_token_test.rb
@@ -10,7 +10,7 @@ class ChannelTokenTest < ActiveSupport::TestCase
     @storage_app = StorageApps.new storage_id('user')
   end
 
-  test 'find_or_create_channel_token sets storage_app_id' do
+  test 'find_or_create_channel_token sets storage_id and storage_app_id' do
     channel_token = ChannelToken.find_or_create_channel_token(
       @level,
       @user,
@@ -18,9 +18,8 @@ class ChannelTokenTest < ActiveSupport::TestCase
       @storage_app
     )
 
-    assert_equal(
-      (storage_decrypt_channel_id channel_token.channel).second.to_i,
-      channel_token.storage_app_id
-    )
+    storage_id, storage_app_id = storage_decrypt_channel_id(channel_token.channel)
+    assert_equal storage_id, channel_token.storage_id
+    assert_equal storage_app_id, channel_token.storage_app_id
   end
 end
