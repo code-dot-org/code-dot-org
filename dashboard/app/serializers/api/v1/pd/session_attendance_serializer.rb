783dad9d8b4f1bd4644da3caefd44f94c7a42444
Clare Constantine <clare@code.org>
2017-09-27 10:03:43 -0700

    incorporate code review feedback

--- dashboard/app/serializers/api/v1/pd/session_attendance_serializer.rb
+++ dashboard/app/serializers/api/v1/pd/session_attendance_serializer.rb
@@ -16,7 +16,7 @@ class Api::V1::Pd::SessionAttendanceSerializer < ActiveModel::Serializer
         email: enrollment.email,
         enrollment_id: enrollment.id,
         user_id: id_or_nil(enrollment.user),
-        verified_teacher_account: !!User.find_by_id(enrollment.user.try(:id)).try(:authorized_teacher?),
+        verified_teacher_account: !!enrollment.user.try(:verified_teacher?),
         attended: attended?(enrollment),
         puzzles_completed: UserLevel.where(user_id: enrollment.user_id).passing.count
       }
