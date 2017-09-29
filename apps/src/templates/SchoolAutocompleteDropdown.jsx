59cf6238362c35de19e66889df20fafee25e69e8
Erin Bond <erin.bond@code.org>
2017-09-28 17:21:51 -0700

    School autocomplete dropdown

---
+++ apps/src/templates/SchoolAutocompleteDropdown.jsx
@@ -0,0 +1,60 @@
+import React, { PropTypes, Component } from 'react';
+import ProtectedStatefulDiv from '../templates/ProtectedStatefulDiv';
+import $ from 'jquery';
+import 'selectize';
+
+const styles = {
+  searchBox: {
+    width: 500,
+    height: 50
+  }
+};
+
+export default class SchoolAutocompleteDropdown extends Component {
+  static propTypes = {
+    name: PropTypes.string,
+    id: PropTypes.string
+  };
+
+  componentDidMount() {
+    $('#' + this.props.id).selectize({
+      maxItems: 1,
+      valueField: 'text',
+      load: function (q, callback) {
+        if (!q.length) {
+          return callback();
+        }
+        $.ajax({
+          url: '/dashboardapi/v1/schoolsearch/' + encodeURIComponent(q) + '/40',
+          type: 'GET',
+          error: function () {
+            callback();
+          },
+          success: function (data) {
+            var schools = [];
+            for (var i = 0; i < data.length; i++) {
+              schools.push({
+                value: data[i].id,
+                text: data[i].name + ' - ' + data[i].city + ', ' + data[i].state + ' ' + data[i].zip
+              });
+            }
+            callback(schools);
+          }
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ProtectedStatefulDiv>
+          <select
+            id={this.props.id}
+            name={this.props.name} style={styles.searchBox}
+          />
+        </ProtectedStatefulDiv>
+      </div>
+    );
+  }
+}
