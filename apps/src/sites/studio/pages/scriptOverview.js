aa831047489e0946152b7f7224f6f4029592b386
Brent Van Minnen <bjvanminnen@gmail.com>
2017-10-05 16:10:46 -0700

    temporarily put a notification on the top of certain scripts

--- apps/src/sites/studio/pages/scriptOverview.js
+++ apps/src/sites/studio/pages/scriptOverview.js
@@ -5,6 +5,8 @@ import PlcHeader from '@cdo/apps/code-studio/plc/header';
 import { renderCourseProgress } from '@cdo/apps/code-studio/progress';
 import { setVerifiedResources } from '@cdo/apps/code-studio/verifiedTeacherRedux';
 import { getStore } from '@cdo/apps/code-studio/redux';
+import Notification, { NotificationType } from '@cdo/apps/templates/Notification';
+import i18n from '@cdo/locale';
 
 $(document).ready(initPage);
 
@@ -23,6 +25,30 @@ function initPage() {
     store.dispatch(setVerifiedResources(true));
   }
 
+  // This notification is a temporary hack we want to stick on a few courses. We
+  // expect to remove it in the near future, and build a longer term solution
+  // for adding notifications to specific scripts that is LB based.
+  const announcementCourses = [
+    'coursee',
+    'coursef',
+    'express'
+  ];
+
+  if (announcementCourses.includes(scriptData.name)) {
+    ReactDOM.render(
+      <Notification
+        type={NotificationType.information}
+        notice="This course has recently been updated!"
+        details="See what changed and how it may affect your classroom"
+        buttonText={i18n.learnMore()}
+        buttonLink="TODO"
+        dismissible={true}
+        isRtl={false}
+        width={1100}
+      />,
+      document.getElementById('notification')
+    );
+  }
   renderCourseProgress(scriptData);
 }
 
