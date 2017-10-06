363fe23a7230903394de3f9f019589573086f578
Joanne Polsky <joanne@code.org>
2017-10-06 11:09:30 -0700

    More namespace changes

--- dashboard/test/lib/api/v1/school_autocomplete_test.rb
+++ dashboard/test/lib/api/v1/school_autocomplete_test.rb
@@ -1,15 +1,17 @@
 require 'test_helper'
 
-class SchoolAutocompleteTest < ActiveSupport::TestCase
-  test 'to search string with two words' do
-    assert_equal '+ABC +DEF*', Api::V1::SchoolAutocomplete.instance.to_search_string('abc def')
-  end
+module Api::V1
+  class SchoolAutocompleteTest < ActiveSupport::TestCase
+    test 'to search string with two words' do
+      assert_equal '+ABC +DEF*', SchoolAutocomplete.instance.to_search_string('abc def')
+    end
 
-  test 'to search string with special chars before second word' do
-    assert_equal '+ABC +DEF*', Api::V1::SchoolAutocomplete.instance.to_search_string('abc +def')
-  end
+    test 'to search string with special chars before second word' do
+      assert_equal '+ABC +DEF*', SchoolAutocomplete.instance.to_search_string('abc +def')
+    end
 
-  test 'to search string with special characters between two words' do
-    assert_equal '+ABC +DEF*', Api::V1::SchoolAutocomplete.instance.to_search_string('abc + def')
+    test 'to search string with special characters between two words' do
+      assert_equal '+ABC +DEF*', SchoolAutocomplete.instance.to_search_string('abc + def')
+    end
   end
 end
