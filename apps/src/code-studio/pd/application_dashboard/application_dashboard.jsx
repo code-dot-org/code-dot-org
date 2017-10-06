6f46e625ea3cb51fa0cc1e0df430770431ded46b
Clare Constantine <clare@code.org>
2017-10-06 09:55:38 -0700

    Add application dashboard page with root react element

---
+++ apps/src/code-studio/pd/application_dashboard/application_dashboard.jsx
@@ -0,0 +1,16 @@
+/* global window */
+
+/**
+ * Main landing page and router for the application dashboard.
+ */
+import React from 'react';
+// import Header from './header';
+
+const ApplicationDashboard = ()=> (
+  <div>
+    <h1>Application Dashboard</h1>
+  </div>
+);
+
+export default ApplicationDashboard;
+window.ApplicationDashboard = ApplicationDashboard;
