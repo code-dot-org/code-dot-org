947d31d67eef111ac34ec34e1ad0d1b407b64135
Joanne Polsky <joanne@code.org>
2017-10-06 11:21:00 -0700

    Remove module and put namespace in class

--- dashboard/lib/api/v1/school_autocomplete.rb
+++ dashboard/lib/api/v1/school_autocomplete.rb
@@ -1,56 +1,54 @@
 require 'singleton'
 
-module Api::V1
-  class SchoolAutocomplete
-    include Singleton
+class Api::V1::SchoolAutocomplete
+  include Singleton
 
-    # The minimum query length.
-    MIN_QUERY_LENGTH = 4
+  # The minimum query length.
+  MIN_QUERY_LENGTH = 4
 
-    # Queries the schools lookup table for schools that match the user-defined
-    # search criteria.
-    # @param query [String] the user-define query string
-    # @param limit [int] the maximum number of results to return
-    # @return [Array] an array of JSON formatted schools
-    def get_matches(query, limit)
-      return [] if query.length < MIN_QUERY_LENGTH
-      schools = School.
-        where("MATCH(name,city) AGAINST(? IN BOOLEAN MODE)", to_search_string(query)).
-        limit(limit)
-      results = schools.map do |school|
-        Serializer.new(school).attributes
-      end
-      return results
+  # Queries the schools lookup table for schools that match the user-defined
+  # search criteria.
+  # @param query [String] the user-define query string
+  # @param limit [int] the maximum number of results to return
+  # @return [Array] an array of JSON formatted schools
+  def get_matches(query, limit)
+    return [] if query.length < MIN_QUERY_LENGTH
+    schools = School.
+      where("MATCH(name,city) AGAINST(? IN BOOLEAN MODE)", to_search_string(query)).
+      limit(limit)
+    results = schools.map do |school|
+      Serializer.new(school).attributes
+    end
+    return results
+  end
+
+  # Formats the query string for boolean full-text search.
+  # For instance, if the user-defined query string is 'abc def',
+  # the string is reformatted to '+ABC +DEF*'.
+  # @see https://dev.mysql.com/doc/refman/5.6/en/fulltext-boolean.html
+  # @param query [String] the user-defined query string.
+  # @return [String] the formatted query string
+  def to_search_string(query)
+    words = query.strip.split(/\s+/).map do |w|
+      w.gsub(/\W/, '').upcase
+    end.map(&:presence).compact
+    return words.empty? ? "" : "+#{words.join(' +')}*"
+  end
+
+  # JSON serializer used by get_matches.
+  class Serializer < ActiveModel::Serializer
+    attributes :nces_id, :name, :city, :state, :zip
+
+    def nces_id
+      object.id.to_s
     end
 
-    # Formats the query string for boolean full-text search.
-    # For instance, if the user-defined query string is 'abc def',
-    # the string is reformatted to '+ABC +DEF*'.
-    # @see https://dev.mysql.com/doc/refman/5.6/en/fulltext-boolean.html
-    # @param query [String] the user-defined query string.
-    # @return [String] the formatted query string
-    def to_search_string(query)
-      words = query.strip.split(/\s+/).map do |w|
-        w.gsub(/\W/, '').upcase
-      end.map(&:presence).compact
-      return words.empty? ? "" : "+#{words.join(' +')}*"
+    def name
+      object.name.titleize
     end
 
-    # JSON serializer used by get_matches.
-    class Serializer < ActiveModel::Serializer
-      attributes :nces_id, :name, :city, :state, :zip
-
-      def nces_id
-        object.id.to_s
-      end
-
-      def name
-        object.name.titleize
-      end
-
-      def city
-        object.city.titleize
-      end
+    def city
+      object.city.titleize
     end
   end
 end
--- dashboard/test/lib/api/v1/school_autocomplete_test.rb
+++ dashboard/test/lib/api/v1/school_autocomplete_test.rb
@@ -1,17 +1,15 @@
 require 'test_helper'
 
-module Api::V1
-  class SchoolAutocompleteTest < ActiveSupport::TestCase
-    test 'to search string with two words' do
-      assert_equal '+ABC +DEF*', SchoolAutocomplete.instance.to_search_string('abc def')
-    end
+class Api::V1::SchoolAutocompleteTest < ActiveSupport::TestCase
+  test 'to search string with two words' do
+    assert_equal '+ABC +DEF*', Api::V1::SchoolAutocomplete.instance.to_search_string('abc def')
+  end
 
-    test 'to search string with special chars before second word' do
-      assert_equal '+ABC +DEF*', SchoolAutocomplete.instance.to_search_string('abc +def')
-    end
+  test 'to search string with special chars before second word' do
+    assert_equal '+ABC +DEF*', Api::V1::SchoolAutocomplete.instance.to_search_string('abc +def')
+  end
 
-    test 'to search string with special characters between two words' do
-      assert_equal '+ABC +DEF*', SchoolAutocomplete.instance.to_search_string('abc + def')
-    end
+  test 'to search string with special characters between two words' do
+    assert_equal '+ABC +DEF*', Api::V1::SchoolAutocomplete.instance.to_search_string('abc + def')
   end
 end
