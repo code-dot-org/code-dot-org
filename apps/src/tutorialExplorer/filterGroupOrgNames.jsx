066ac39a5073cb0364d7291fce3390bbd9ce3b8f
Brendan Reville <brendan@code.org>
2017-10-11 17:07:41 +1100

    Tutorial explorer: small improvements

--- apps/src/tutorialExplorer/filterGroupOrgNames.jsx
+++ apps/src/tutorialExplorer/filterGroupOrgNames.jsx
@@ -3,24 +3,20 @@
 
 import React, {PropTypes} from 'react';
 import FilterGroupContainer from './filterGroupContainer';
+import { TutorialsOrgName } from './util';
 import i18n from './locale';
 
 const styles = {
-  tutorialSetNoTutorials: {
-    backgroundColor: "#d6d6d6",
-    padding: 20,
-    margin: 60,
-    whiteSpace: "pre-wrap"
-  },
   select: {
     width: '100%',
-    marginTop: 10
+    marginTop: 10,
+    height: 26
   }
 };
 
 const FilterGroupOrgNames = React.createClass({
   propTypes: {
-    orgName: PropTypes.string,
+    orgName: PropTypes.string.isRequired,
     uniqueOrgNames: PropTypes.arrayOf(PropTypes.string).isRequired,
     onUserInput: PropTypes.func.isRequired
   },
@@ -40,7 +36,10 @@ const FilterGroupOrgNames = React.createClass({
           style={styles.select}
           className="noFocusButton"
         >
-          <option key="all" value="all">
+          <option
+            key={TutorialsOrgName.all}
+            value={TutorialsOrgName.all}
+          >
             {i18n.filterOrgNamesAll()}
           </option>
           {this.props.uniqueOrgNames.map(item => (
