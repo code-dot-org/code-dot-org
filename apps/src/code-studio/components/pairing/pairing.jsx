6da49b43287ef492eb5e9faa9c39b436d1c08b4d
Brad Buchanan <brad@code.org>
2017-10-10 17:54:22 -0700

    ES6ify Pairing

--- apps/src/code-studio/components/pairing/pairing.jsx
+++ apps/src/code-studio/components/pairing/pairing.jsx
@@ -9,54 +9,50 @@ import StudentSelector from './student_selector.jsx';
 /**
  * A component for managing pair programming.
  */
-const Pairing = React.createClass({
-  propTypes: {
+export default class Pairing extends React.Component {
+  static propTypes = {
     source: PropTypes.string,
     handleClose: PropTypes.func
-  },
+  };
 
-  getInitialState() {
-    return {
-      pairings: [],
-      sections: []
-    };
-  },
+  state = {
+    pairings: [],
+    sections: []
+  };
 
   componentWillMount() {
     $.ajax({
       url: this.props.source,
       method: 'GET',
       dataType: 'json'
-    }).done(function (result) {
+    }).done((result) => {
       this.setState({
         pairings: result.pairings,
         sections: result.sections
       });
-    }.bind(this)).fail(function (result) {
+    }).fail((result) => {
       // TODO what to do here?
-    }.bind(this));
-  },
+    });
+  }
 
-  handleSectionChange(event) {
+  handleSectionChange = (event) => {
     this.setState({
       pairings: [],
       sections: this.state.sections,
       selectedSectionId: +event.target.value
     });
-  },
+  };
 
-  refreshUserMenu() {
+  refreshUserMenu = () => {
     $.ajax({
       type: 'GET',
       url: '/dashboardapi/user_menu',
-      success: function (data) {
-        $('#sign_in_or_user').html(data);
-      }
+      success: data => $('#sign_in_or_user').html(data)
     });
-  },
+  };
 
-  handleAddPartners(studentIds) {
-    var pairings = this.selectedSection().students.filter(
+  handleAddPartners = (studentIds) => {
+    const pairings = this.selectedSection().students.filter(
       student => studentIds.indexOf(student.id) !== -1
     );
 
@@ -72,9 +68,9 @@ const Pairing = React.createClass({
     }).done(this.refreshUserMenu).fail((jqXHR, textStatus, errorThrown) => {
       // TODO what to do here?
     });
-  },
+  };
 
-  handleStop(event) {
+  handleStop = (event) => {
     this.setState({
       pairings: []
     });
@@ -93,7 +89,7 @@ const Pairing = React.createClass({
     });
 
     event.preventDefault();
-  },
+  };
 
   selectedSectionId() {
     if (this.state.sections.length === 1) {
@@ -101,26 +97,26 @@ const Pairing = React.createClass({
     } else {
       return this.state.selectedSectionId;
     }
-  },
+  }
 
   selectedSection() {
     if (this.selectedSectionId()) {
       // todo use jquery find
-      for (var i = 0; i < this.state.sections.length; i++) {
+      for (let i = 0; i < this.state.sections.length; i++) {
         if (this.state.sections[i].id === this.selectedSectionId()) {
           return this.state.sections[i];
         }
       }
     }
     return null;
-  },
+  }
 
   studentsInSection() {
     if (this.selectedSection()) {
       return this.selectedSection().students;
     }
     return null;
-  },
+  }
 
   renderPairingSelector() {
     return (
@@ -139,7 +135,7 @@ const Pairing = React.createClass({
         </form>
       </div>
     );
-  },
+  }
 
   renderPairingState() {
     return (
@@ -154,7 +150,7 @@ const Pairing = React.createClass({
         <button className="ok" onClick={this.props.handleClose}>OK</button>
       </div>
     );
-  },
+  }
 
   render() {
     if (this.state.pairings.length === 0) {
@@ -163,5 +159,4 @@ const Pairing = React.createClass({
       return this.renderPairingState();
     }
   }
-});
-export default Pairing;
+}
