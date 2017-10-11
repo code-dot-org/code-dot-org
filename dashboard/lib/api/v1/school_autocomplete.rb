d5a78da2c51d34fb53e06cf20ad5aa07d78ea314
Joanne Polsky <joanne@code.org>
2017-10-11 15:46:38 -0700

    Add extended ZIP cases and truncate on search

--- dashboard/lib/api/v1/school_autocomplete.rb
+++ dashboard/lib/api/v1/school_autocomplete.rb
@@ -12,12 +12,12 @@ class Api::V1::SchoolAutocomplete
   # @param limit [int] the maximum number of results to return
   # @return [Array] an array of JSON formatted schools
   def get_matches(query, limit)
-    return [] if query.length < MIN_QUERY_LENGTH
+    return [] if (query = query.strip).length < MIN_QUERY_LENGTH
     schools = School.limit(limit)
-    if query.match(/^\d{,5}$/).nil?
-      schools = schools.where("MATCH(name,city) AGAINST(? IN BOOLEAN MODE)", to_search_string(query))
+    if zip_search?(query)
+      schools = schools.where("zip LIKE ?", "#{query[0, 5]}%")
     else
-      schools = schools.where("zip LIKE ?", "#{query}%")
+      schools = schools.where("MATCH(name,city) AGAINST(? IN BOOLEAN MODE)", to_search_string(query))
     end
     results = schools.map do |school|
       Serializer.new(school).attributes
@@ -25,11 +25,19 @@ class Api::V1::SchoolAutocomplete
     return results
   end
 
+  # Determines if we should perform a search by ZIP code rather than by school
+  # name or city.
+  # @param query [String] the user-define query string
+  # @return [boolean] true if it might be a ZIP, false otherwise
+  def zip_search?(query)
+    return !!(query =~ /^(\d{,5}|(\d{5}-\d{,4}))$/)
+  end
+
   # Formats the query string for boolean full-text search.
   # For instance, if the user-defined query string is 'abc def',
   # the string is reformatted to '+ABC +DEF*'.
   # @see https://dev.mysql.com/doc/refman/5.6/en/fulltext-boolean.html
-  # @param query [String] the user-defined query string.
+  # @param query [String] the user-defined query string
   # @return [String] the formatted query string
   def to_search_string(query)
     words = query.strip.split(/\s+/).map do |w|
