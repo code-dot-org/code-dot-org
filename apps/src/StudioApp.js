25d740f482d759224428bc0fa5e61689929516cb
Erin <erin@code.org>
2017-10-10 17:27:06 -0700

    Code cleanliness

--- apps/src/StudioApp.js
+++ apps/src/StudioApp.js
@@ -1794,8 +1794,8 @@ StudioApp.prototype.configureDom = function (config) {
   // TODO (cpirich): make conditional for applab
   var belowViz = document.getElementById('belowVisualization');
   var referenceArea = document.getElementById('reference_area');
-  //TODO : this will get less ugly after we remove the flag - epeach
-  if ((config.app && (!config.app.includes('lab') || (!experiments.isEnabled('resourcesTab') && config.app.includes('lab'))) || !config.app) && referenceArea) {
+  // The check for instructionsWhenCollapsed checks for whether the app uses CSPTopInstructions
+  if ((!config.noInstructionsWhenCollapsed || !experiments.isEnabled('resourcesTab')) && referenceArea) {
     belowViz.appendChild(referenceArea);
   }
 
