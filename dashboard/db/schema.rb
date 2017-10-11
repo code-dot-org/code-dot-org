ac425f8531de4a61bd4cdb88aa3745f6fd5d5167
Joanne Polsky <joanne@code.org>
2017-10-10 21:41:53 -0700

    Update migration

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010035830) do
+ActiveRecord::Schema.define(version: 20171011043757) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
