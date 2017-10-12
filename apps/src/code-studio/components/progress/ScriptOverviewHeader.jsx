57566c35419b47e86cf0393857a03d63407ebc55
Brent Van Minnen <bjvanminnen@gmail.com>
2017-10-12 12:50:26 -0700

    plc breadcrumb under root react tree

--- apps/src/code-studio/components/progress/ScriptOverviewHeader.jsx
+++ apps/src/code-studio/components/progress/ScriptOverviewHeader.jsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
 import ProtectedStatefulDiv from '@cdo/apps/templates/ProtectedStatefulDiv';
+import PlcHeader from '@cdo/apps/code-studio/plc/header';
 
 /**
  * This component takes some of the HAML generated content on the script overview
@@ -10,16 +12,37 @@ import ProtectedStatefulDiv from '@cdo/apps/templates/ProtectedStatefulDiv';
  * client the data and have React generate the DOM. Doing so should not be super
  * difficult in this case
  */
-export default class ScriptOverviewHeader extends Component {
+class ScriptOverviewHeader extends Component {
+  static propTypes = {
+    plcHeaderProps: PropTypes.shape({
+      unitName: PropTypes.string.isRequired,
+      courseViewPath: PropTypes.string.isRequired,
+    })
+  };
+
   componentDidMount() {
-    $('#react-scriptoverview-header').appendTo(ReactDOM.findDOMNode(this.root));
+    $('#react-scriptoverview-header').appendTo(ReactDOM.findDOMNode(this.protected));
   }
 
   render() {
+    const { plcHeaderProps } = this.props;
+
     return (
-      <ProtectedStatefulDiv
-        ref={element => this.root = element}
-      />
+      <div>
+        {plcHeaderProps &&
+          <PlcHeader
+            unit_name={plcHeaderProps.unitName}
+            course_view_path={plcHeaderProps.courseViewPath}
+          />
+        }
+        <ProtectedStatefulDiv
+          ref={element => this.protected = element}
+        />
+      </div>
     );
   }
 }
+
+export default connect(state => ({
+  plcHeaderProps: state.plcHeader
+}))(ScriptOverviewHeader);
