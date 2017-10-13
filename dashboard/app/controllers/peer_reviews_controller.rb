3fa4d84e0f14032b0bb9a52a48f448da35f8705e
Mehal Shah <mehal@code.org>
2017-10-13 13:12:15 -0700

    Peer Reviews should log to honeybadger for failed updates

--- dashboard/app/controllers/peer_reviews_controller.rb
+++ dashboard/app/controllers/peer_reviews_controller.rb
@@ -47,16 +47,20 @@ class PeerReviewsController < ApplicationController
   end
 
   def update
-    if @peer_review.update(peer_review_params.merge(reviewer: current_user, from_instructor: current_user.permission?('plc_reviewer')))
-      flash[:notice] = t('peer_review.review_submitted')
+    begin
+      @peer_review.update!(peer_review_params.merge(reviewer: current_user, from_instructor: current_user.permission?('plc_reviewer')))
+    rescue ActiveRecord::RecordNotSaved, ActiveRecord::RecordInvalid => error
+      Honeybadger.notify(error, error_message: "Error updating peer review", context: {peer_review: @peer_review})
+      redirect_to peer_review_path(@peer_review)
+      return
+    end
 
-      if current_user.permission?('plc_reviewer')
-        redirect_to peer_reviews_dashboard_path
-      else
-        redirect_to script_path(@peer_review.script)
-      end
+    flash[:notice] = t('peer_review.review_submitted')
+
+    if current_user.permission?('plc_reviewer')
+      redirect_to peer_reviews_dashboard_path
     else
-      render action: :show
+      redirect_to script_path(@peer_review.script)
     end
   end
 
