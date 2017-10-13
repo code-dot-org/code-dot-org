74098295ef65647616e8f420369ffec7efae85a3
Brent Van Minnen <bjvanminnen@gmail.com>
2017-10-13 15:28:28 -0700

    cleanup

--- apps/src/sites/studio/pages/scriptOverview.js
+++ apps/src/sites/studio/pages/scriptOverview.js
@@ -26,25 +26,6 @@ function initPage() {
     store.dispatch(setVerifiedResources(true));
   }
 
-  // This notification is a temporary hack we want to stick on a few courses. In
-  // the future this will instead be LB configurable and we will get this data
-  // from the server
-  // const announcementCourses = [
-  //   'coursee',
-  //   'coursef',
-  //   'express'
-  // ];
-  //
-  // let announcements;
-  // if (announcementCourses.includes(scriptData.name)) {
-  //   announcements = [{
-  //     notice: "This course has recently been updated!",
-  //     details: "See what changed and how it may affect your classroom.",
-  //     link: "https://support.code.org/hc/en-us/articles/115001931251",
-  //     type: 'information',
-  //   }];
-  // }
-
   if (scriptData.script_announcements) {
     const announcements = JSON.parse(scriptData.script_announcements);
     registerReducers({scriptAnnouncements: scriptAnnouncementReducer});
