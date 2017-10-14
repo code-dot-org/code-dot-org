536b618af89cd7a85a66c1e7cd10bb64387fad64
Brad Buchanan <brad@code.org>
2017-10-13 17:28:10 -0700

    Move constant out of render method

--- apps/src/code-studio/plc/header.jsx
+++ apps/src/code-studio/plc/header.jsx
@@ -5,6 +5,19 @@
 import React, {PropTypes} from 'react';
 import color from "../../util/color";
 
+const breadcrumbStyle = {
+  container: {
+    marginBottom: 20
+  },
+  icon: {
+    margin: 5,
+    color: color.charcoal
+  },
+  currentPage: {
+    color: color.orange
+  }
+};
+
 export default class PlcHeader extends React.Component {
   static propTypes = {
     unit_name: PropTypes.string,
@@ -14,19 +27,6 @@ export default class PlcHeader extends React.Component {
   };
 
   render() {
-    let breadcrumbStyle = {
-      container: {
-        marginBottom: 20
-      },
-      icon: {
-        margin: 5,
-        color: color.charcoal
-      },
-      currentPage: {
-        color: color.orange
-      }
-    };
-
     return (
       <div style={breadcrumbStyle.container} className="uitest-plcbreadcrumb">
         <a href={this.props.course_view_path}>
@@ -50,7 +50,6 @@ export default class PlcHeader extends React.Component {
             </span>
           )
         }
-
       </div>
     );
   }
