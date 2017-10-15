7a53ed8df699502cf9dc0e2b246ecb312bc88a0c
Joanne Polsky <joanne@code.org>
2017-10-15 11:09:31 -0700

    Format schools.tsv

--- dashboard/app/models/school.rb
+++ dashboard/app/models/school.rb
@@ -30,48 +30,33 @@ class School < ActiveRecord::Base
 
   belongs_to :school_district
 
-  # The listing of all US schools comes from http://nces.ed.gov/ccd/pubagency.asp
-  # and is then exported into a tab-separated file.
-  # The data format is described at http://nces.ed.gov/ccd/pdf/2015150_sc132a_Documentation_052716.pdf
-  CSV_HEADERS = {
-    id: 'NCESSCH',
-    school_district_id: 'LEAID',
-    name: 'SCHNAM',
-    city: 'LCITY',
-    state: 'LSTATE',
-    zip: 'LZIP',
-    charter_status: 'CHARTR',
-  }.freeze
-
-  # Use the zero byte as the quote character to allow importing double quotes
-  #   via http://stackoverflow.com/questions/8073920/importing-csv-quoting-error-is-driving-me-nuts
-  CSV_IMPORT_OPTIONS = {col_sep: "\t", headers: true, quote_char: "\x00"}.freeze
-
   def self.find_or_create_all_from_tsv(filename)
-    CSV.read(filename, CSV_IMPORT_OPTIONS).each do |row|
-      first_or_create_from_tsv_row(row)
+    CSV.read(filename, {col_sep: "\t", headers: true, quote_char: "\x00"}).each do |row|
+      School.where(row).first_or_create!
     end
   end
 
-  SCHOOL_TYPE = {
-    public: 'public',
-    charter: 'charter'
-  }.freeze
-
-  private_class_method def self.school_type(charter_status)
-    charter_status == '1' ? SCHOOL_TYPE[:charter] : SCHOOL_TYPE[:public]
-  end
-
-  private_class_method def self.first_or_create_from_tsv_row(row_data)
-    params = {
-      id: row_data[CSV_HEADERS[:id]],
-      school_district_id: row_data[CSV_HEADERS[:school_district_id]],
-      name: row_data[CSV_HEADERS[:name]],
-      city: row_data[CSV_HEADERS[:city]],
-      state: row_data[CSV_HEADERS[:state]],
-      zip: row_data[CSV_HEADERS[:zip]],
-      school_type: school_type(row_data[CSV_HEADERS[:charter_status]]),
-    }
-    School.where(params).first_or_create!
+  # Download the data in the schools table to a TSV file.
+  # @param filename [String] the TSC file name
+  # @return [String} the TSV file name
+  def self.all_to_tsv(filename)
+    CSV.open(filename, 'w', {col_sep: "\t", headers: true, quote_char: "\x00"}) do |csv|
+      csv << %w(id school_district_id school_type name address_line1 address_line2 address_line3 city state zip)
+      School.order(:id).map do |school|
+        csv << [
+          school[:id],
+          school[:school_district_id],
+          school[:school_type],
+          school[:name],
+          school[:address_line1],
+          school[:address_line2],
+          school[:address_line3],
+          school[:city],
+          school[:state],
+          school[:zip]
+        ]
+      end
+    end
+    return filename
   end
 end
