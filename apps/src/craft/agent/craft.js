157b62e352a36f2735ce3296757b1a300be8377e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-10-17 16:50:10 -0700

    add 'onDragEnd' listeners for Hammer Minecraft movement

--- apps/src/craft/agent/craft.js
+++ apps/src/craft/agent/craft.js
@@ -262,15 +262,25 @@ export default class Craft {
             dom.addClickTouchEvent(resetButton, Craft.resetButtonClick);
 
             const phaserGame = document.getElementById('phaser-game');
+            const hammerToButton = {
+              [Hammer.DIRECTION_LEFT]: 'leftButton',
+              [Hammer.DIRECTION_RIGHT]: 'rightButton',
+              [Hammer.DIRECTION_UP]: 'upButton',
+              [Hammer.DIRECTION_DOWN]: 'downButton',
+            };
+
             const onDrag = function (e) {
-              const hammerToButton = {
-                [Hammer.DIRECTION_LEFT]: 'leftButton',
-                [Hammer.DIRECTION_RIGHT]: 'rightButton',
-                [Hammer.DIRECTION_UP]: 'upButton',
-                [Hammer.DIRECTION_DOWN]: 'downButton',
-              };
               if (hammerToButton[e.direction]) {
                 Craft.gameController.codeOrgAPI.arrowDown(
+                  directionToFacing[hammerToButton[e.direction]],
+                );
+              }
+              e.preventDefault();
+            };
+
+            const onDragEnd = function (e) {
+              if (hammerToButton[e.direction]) {
+                Craft.gameController.codeOrgAPI.arrowUp(
                   directionToFacing[hammerToButton[e.direction]],
                 );
               }
@@ -282,6 +292,7 @@ export default class Craft {
             mc.add(new Hammer.Press({ time: 150 }));
             mc.add(new Hammer.Tap());
             mc.on('pan', onDrag);
+            mc.on('panend pancancel', onDragEnd);
             mc.on('press', () =>
               Craft.gameController.codeOrgAPI.clickDown(() => {}),
             );
