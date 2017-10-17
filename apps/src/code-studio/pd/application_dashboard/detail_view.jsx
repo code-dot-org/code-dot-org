6e03c262a74faf58579269f1fe3e926be28a650d
Mehal Shah <mehal@code.org>
2017-10-17 13:58:47 -0700

    Better version of the detail view, with tests

--- apps/src/code-studio/pd/application_dashboard/detail_view.jsx
+++ apps/src/code-studio/pd/application_dashboard/detail_view.jsx
@@ -8,37 +8,13 @@
 
 import React, {PropTypes} from 'react';
 import {Button, FormControl} from 'react-bootstrap';
-import _ from 'lodash';
 import {Facilitator1819Program} from './detail_view_facilitator_specific_components';
-
-
-export default class DetailView extends React.Component {
-  static propTypes = {
-    params: PropTypes.shape({
-      applicationId: PropTypes.string.isRequired
-    }).isRequired
-  };
-
-  render() {
-    return (
-      <div>
-        Detail view!
-      </div>
-    );
-  }
-}
-
-const lineItemKeys = {
-  planToTeachThisYear1819: 'Do you plan on teaching this course in the 2018-19 school year?',
-  rateAbility: 'How would you rate your ability to meet the requirements for your focus area?',
-  canAttendFIT: 'Can attend FIT Training?'
-};
 
 const renderLineItem = (key, value) => {
   return value && (
     <div>
       <span style={{fontFamily: '"Gotham 7r"', marginRight: '10px'}}>
-        {lineItemKeys[key] || _.startCase(key)}:
+        {key}:
       </span>
       {value}
     </div>
@@ -55,12 +31,12 @@ class DetailViewContents extends React.Component {
       title: PropTypes.string,
       preferredFirstName: PropTypes.string,
       accountEmail: PropTypes.string.isRequired,
-      alternateEmail: PropTypes.string.isRequired,
+      alternateEmail: PropTypes.string,
       phone: PropTypes.string.isRequired,
-      district: PropTypes.string.isRequired,
-      school: PropTypes.string.isRequired,
+      district: PropTypes.string,
+      school: PropTypes.string,
       course: PropTypes.string.isRequired,
-      regionalPartner: PropTypes.string.isRequired,
+      regionalPartner: PropTypes.string,
       program: PropTypes.string.isRequired,
       planToTeachThisYear: PropTypes.string.isRequired,
       rateAbility: PropTypes.string.isRequired,
@@ -105,12 +81,12 @@ class DetailViewContents extends React.Component {
 
   renderHeader = () => {
     return (
-      <div style={{display: 'flex', alignItems: 'center'}}>
+      <div style={{display: 'flex', alignItems: 'baseline'}}>
         <h1>
           {this.props.applicationData.name}
         </h1>
 
-        <div style={{float: 'right', display: 'flex'}}>
+        <div id="DetailViewHeader" style={{display: 'flex', marginLeft: 'auto'}}>
           <FormControl componentClass="select" disabled={!this.state.editing} placeholder={this.props.applicationData.acceptance}>
             <option value="accepted">
               Accepted
@@ -154,16 +130,16 @@ class DetailViewContents extends React.Component {
         <h3>
           About You
         </h3>
-        {renderLineItem('title', this.props.applicationData.title)}
-        {renderLineItem('preferredFirstName', this.props.applicationData.preferredFirstName)}
-        {renderLineItem('accountEmail', this.props.applicationData.accountEmail)}
-        {renderLineItem('alternateEmail', this.props.applicationData.alternateEmail)}
-        {renderLineItem('phone', this.props.applicationData.phone)}
+        {renderLineItem('Title', this.props.applicationData.title)}
+        {renderLineItem('Preferred First Name', this.props.applicationData.preferredFirstName)}
+        {renderLineItem('Account Email', this.props.applicationData.accountEmail)}
+        {renderLineItem('Alternate Email', this.props.applicationData.alternateEmail)}
+        {renderLineItem('Phone', this.props.applicationData.phone)}
         <br/>
-        {renderLineItem('district', this.props.applicationData.district)}
-        {renderLineItem('school', this.props.applicationData.school)}
-        {renderLineItem('course', this.props.applicationData.course)}
-        {renderLineItem('regionalPartner', this.props.applicationData.regionalPartner)}
+        {renderLineItem('District', this.props.applicationData.district)}
+        {renderLineItem('School', this.props.applicationData.school)}
+        {renderLineItem('Course', this.props.applicationData.course)}
+        {renderLineItem('Regional Partner', this.props.applicationData.regionalPartner)}
       </div>
     );
   }
@@ -174,7 +150,7 @@ class DetailViewContents extends React.Component {
         <h3>
           Choose Your Program
         </h3>
-        {renderLineItem('program', this.props.applicationData.program)}
+        {renderLineItem('Program', this.props.applicationData.program)}
         <Facilitator1819Program
           planToTeachThisYear1819={this.props.applicationData.planToTeachThisYear}
           rateAbility={this.props.applicationData.rateAbility}
--- apps/src/code-studio/pd/application_dashboard/detail_view_facilitator_specific_components.jsx
+++ apps/src/code-studio/pd/application_dashboard/detail_view_facilitator_specific_components.jsx
@@ -1,6 +1,12 @@
 import React, {PropTypes} from 'react';
 import {renderLineItem} from './detail_view';
 
+const lineItemKeys = {
+  planToTeachThisYear1819: 'Do you plan on teaching this course in the 2018-19 school year?',
+  rateAbility: 'How would you rate your ability to meet the requirements for your focus area?',
+  canAttendFIT: 'Can attend FIT Training?'
+};
+
 class Facilitator1819Program extends React.Component {
   static propTypes = {
     planToTeachThisYear1819: PropTypes.string.isRequired,
@@ -11,9 +17,9 @@ class Facilitator1819Program extends React.Component {
   render() {
     return  (
       <div>
-        {renderLineItem('planToTeachThisYear1819', this.props.planToTeachThisYear1819)}
-        {renderLineItem('rateAbility', this.props.rateAbility)}
-        {renderLineItem('canAttendFIT', this.props.canAttendFIT)}
+        {renderLineItem(lineItemKeys['planToTeachThisYear1819'], this.props.planToTeachThisYear1819)}
+        {renderLineItem(lineItemKeys['rateAbility'], this.props.rateAbility)}
+        {renderLineItem(lineItemKeys['canAttendFIT'], this.props.canAttendFIT)}
       </div>
     );
   }
