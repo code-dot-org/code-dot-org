989de8b3fa971f7a8bcd373a4a20a95490fa8e6c
Ram Kandasamy <ram@code.org>
2017-10-17 13:47:35 -0700

    Add a properties column to stages

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018203624) do
+ActiveRecord::Schema.define(version: 20171019204123) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -960,14 +960,15 @@ ActiveRecord::Schema.define(version: 20171018203624) do
   end
 
   create_table "stages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string   "name",                              null: false
+    t.string   "name",                                            null: false
     t.integer  "absolute_position"
-    t.integer  "script_id",                         null: false
+    t.integer  "script_id",                                       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "flex_category"
-    t.boolean  "lockable",          default: false, null: false
-    t.integer  "relative_position",                 null: false
+    t.boolean  "lockable",                        default: false, null: false
+    t.integer  "relative_position",                               null: false
+    t.text     "properties",        limit: 65535
     t.index ["script_id"], name: "index_stages_on_script_id", using: :btree
   end
 
