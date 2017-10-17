033cc74d670631b5269440415539de4701533a1d
Will Jordan <wjordan@users.noreply.github.com>
2017-10-17 14:56:21 -0700

    Revert "lazy-loading locale I18n backend"

--- dashboard/test/lib/lazy_locale_backend_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LazyLocaleBackendTest < ActiveSupport::TestCase
-  test 'eager load all locales in load path' do
-    # Ensures all locale files in the load path load properly
-    # even when lazy-locale backend is in use.
-    I18n.backend.load_translations
-  end
-end
