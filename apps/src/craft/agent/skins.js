7463562b516f103b5737a84509163b4c966cef18
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-10-17 17:11:35 -0700

    update skins.js to es6

--- apps/src/craft/agent/skins.js
+++ apps/src/craft/agent/skins.js
@@ -1,6 +1,6 @@
-var skinsBase = require('../../skins');
+import skinsBase from '../../skins';
 
 exports.load = function (assetUrl, id) {
-  var skin = skinsBase.load(assetUrl, id);
+  const skin = skinsBase.load(assetUrl, id);
   return skin;
 };
