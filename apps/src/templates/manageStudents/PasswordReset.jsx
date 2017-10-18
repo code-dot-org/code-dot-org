1dde842fdd5b0eb7b4aa2a6654277a77b11b04fb
Caley Brock <caleybrock7@gmail.com>
2017-10-18 11:59:15 -0700

    Password reset component

--- apps/src/templates/manageStudents/PasswordReset.jsx
+++ apps/src/templates/manageStudents/PasswordReset.jsx
@@ -1,15 +1,68 @@
 import React, {Component, PropTypes} from 'react';
 import Button from '../Button';
 
+const styles = {
+  input: {
+    width: 100,
+    height: 29,
+    marginTop: '-25px',
+    marginRight: 10,
+  }
+};
+
 class PasswordReset extends Component {
-  static PropTypes = {
-    presetAction: PropTypes.func.isRequired,
+  static propTypes = {
+    resetAction: PropTypes.func.isRequired,
     isResetting: PropTypes.bool
+  };
+
+  state = {
+    isResetting: !!this.props.isResetting,
+    input: ''
+  };
+
+  reset = () => {
+    this.setState({
+      isResetting: true
+    });
+  };
+
+  save = () => {
+    //Todo: do we also want a cancel button?
+
+    //Save the inputted password
+    this.props.resetAction(this.state.input);
+
+    this.setState({
+      isResetting: false,
+      input: ''
+    });
+  };
+
+  updateInput = (event) => {
+    this.setState({
+      input: event.target.value
+    });
   };
 
   render() {
     return (
-      <Button onClick={() => {}} color="white" text="Reset password" />
+      <div>
+        {!this.state.isResetting &&
+          <Button onClick={this.reset} color="white" text="Reset password" />
+        }
+        {this.state.isResetting &&
+          <div>
+            <input
+              style={styles.input}
+              placeholder="new password"
+              value={this.state.input}
+              onChange={this.updateInput}
+            />
+            <Button onClick={this.save} color="blue" text="Save" />
+          </div>
+        }
+      </div>
     );
   }
 }
