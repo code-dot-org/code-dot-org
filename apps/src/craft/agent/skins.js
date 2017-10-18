c6f4122605e0d0515407577b9e661e2605ec367f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-10-17 17:12:07 -0700

    add replaceInstructions method

--- apps/src/craft/agent/skins.js
+++ apps/src/craft/agent/skins.js
@@ -1,6 +1,16 @@
 import skinsBase from '../../skins';
+import Craft from './craft.js';
 
 exports.load = function (assetUrl, id) {
   const skin = skinsBase.load(assetUrl, id);
+
+  skin.replaceInstructions = function (instructions) {
+    if (instructions) {
+      return instructions.replace(/{currentPlayerName}/g, Craft.getCurrentCharacter());
+    }
+
+    return instructions;
+  };
+
   return skin;
 };
