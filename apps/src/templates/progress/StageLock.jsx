e7d35efff498b9f91c07392c9b08204b193ca403
Brad Buchanan <brad@code.org>
2017-10-19 15:58:14 -0700

    Test StageLock

--- apps/src/templates/progress/StageLock.jsx
+++ apps/src/templates/progress/StageLock.jsx
@@ -67,6 +67,7 @@ const StageLock = React.createClass({
   }
 });
 
+export const UnconnectedStageLock = StageLock;
 export default connect(state => ({
   sectionId: state.teacherSections.selectedSectionId,
   sectionsAreLoaded: state.teacherSections.sectionsAreLoaded,
