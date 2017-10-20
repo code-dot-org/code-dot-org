33121c427d2860ec9f3783d4e1b49b0085548711
Suresh Chanmugam <me@suresh.com>
2017-10-20 15:45:59 -0700

    Eliminate join to associated Contact model and remove error handling that swallowed errors.

--- dashboard/db/migrate/20171013180932_backfill_regional_partner_program_managers_from_contacts.rb
+++ dashboard/db/migrate/20171013180932_backfill_regional_partner_program_managers_from_contacts.rb
@@ -1,13 +1,7 @@
 class BackfillRegionalPartnerProgramManagersFromContacts < ActiveRecord::Migration[5.0]
   def up
-    RegionalPartner.joins(:contact).each do |regional_partner|
-      begin
-        regional_partner.program_manager = regional_partner.contact.id
-      rescue RuntimeError => e
-        # Log the error but don't fail the migration if something goes wrong.
-        CDO.logger.error "Error converting Regional Partner (#{regional_partner.id})  "\
-          "Contact (#{regional_partner.contact.id}) into a Program Manager: #{e.message}"
-      end
+    RegionalPartner.where.not(contact_id: nil).each do |regional_partner|
+      regional_partner.program_manager = regional_partner.contact_id
     end
   end
 
