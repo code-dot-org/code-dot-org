502eef436250b52bd4c9fe7febe307443b3c5fbc
Clare Constantine <clare@code.org>
2017-10-21 11:57:52 -0700

    Spinner appears while data is loading; load data

--- apps/src/code-studio/pd/application_dashboard/summary.jsx
+++ apps/src/code-studio/pd/application_dashboard/summary.jsx
@@ -4,17 +4,49 @@
  */
 import React from 'react';
 import SummaryTable from './summary_table';
+import Spinner from '../components/spinner';
 
 export default class Summary extends React.Component {
+  state = {
+    loading: true,
+    applications: null
+  };
+
+  componentDidMount() {
+    this.load();
+  }
+
+  load = (props = this.props) => {
+    this.loadRequest = $.ajax({
+      method: 'GET',
+      url: '/api/v1/pd/applications',
+      dataType: 'json'
+    })
+    .done(data => {
+      this.setState({
+        loading: false,
+        applications: data
+      });
+    });
+  };
+
   render() {
-    return (
-      <div>
-        <h1>All Regional Partner Applications</h1>
-        <SummaryTable caption="CSD Facilitators"/>
-        <SummaryTable caption="CSP Facilitators"/>
-        <SummaryTable caption="CSD Teachers"/>
-        <SummaryTable caption="CSP Teachers"/>
-      </div>
-    );
+    if (this.state.loading) {
+      return (
+        <div>
+          <Spinner/>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>All Regional Partner Applications</h1>
+          <SummaryTable caption="CSD Facilitators" data={this.state.applications["csd_facilitators"]}/>
+          <SummaryTable caption="CSP Facilitators"/>
+          <SummaryTable caption="CSD Teachers"/>
+          <SummaryTable caption="CSP Teachers"/>
+        </div>
+      );
+    }
   }
 }
