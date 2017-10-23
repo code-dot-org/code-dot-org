b65a5edd41ea9e390d575ec64120213661111d23
Ram Kandasamy <ram@code.org>
2017-10-23 13:48:02 -0700

    Add orange hover flag

--- apps/src/templates/progress/StageExtrasProgressBubble.jsx
+++ apps/src/templates/progress/StageExtrasProgressBubble.jsx
@@ -10,15 +10,20 @@ const styles = {
     backgroundImage: `url('${assetUrl("media/common_images/flag_inactive.png")}')`,
     width: 18,
     height: 21,
-    ':hover': {
-      //backgroundImage: `url('${assetUrl("media/common_images/flag_hover.png")}')`,
-    },
     position: 'relative',
     top: -1,
   },
   focused: {
     backgroundImage: `url('${assetUrl("media/common_images/flag_active.png")}')`,
-  }
+  },
+  hoverOverlay: {
+    backgroundImage: `url('${assetUrl("media/common_images/flag_hover.png")}')`,
+    opacity: 0,
+    transition: 'opacity .2s ease-out',
+    ':hover': {
+      opacity: 1,
+    },
+  },
 };
 
 class StageExtrasProgressBubble extends Component {
@@ -40,6 +45,7 @@ class StageExtrasProgressBubble extends Component {
         data-tip data-for={tooltipId}
         aria-describedby={tooltipId}
       >
+        <div style={{...styles.main, ...styles.hoverOverlay}}/>
         <TooltipWithIcon
           tooltipId={tooltipId}
           icon={'flag'}
