f6c87e41f7b07a120821a0f53b6b52f582b79eea
Joanne Polsky <joanne@code.org>
2017-10-24 16:46:35 -0700

    Increase retry count in UI and Eyes tests

--- lib/cdo/test_flakiness.rb
+++ lib/cdo/test_flakiness.rb
@@ -14,14 +14,12 @@ class TestFlakiness
   MIN_SAMPLES = 10
   TEST_ACCOUNT_USERNAME = 'testcodeorg'.freeze
 
-  def self.calculate_test_flakiness
+  def self.get_jobs
     jobs = []
-
     NUM_REQUESTS.times do
       # docs for this API: https://wiki.saucelabs.com/display/DOCS/Job+Methods
       url =  "https://saucelabs.com/rest/v1/#{TEST_ACCOUNT_USERNAME}/jobs"
       url += "?" + URI.encode_www_form(limit: PER_REQUEST, full: 'true', skip: jobs.count)
-
       response = RestClient::Request.execute(
         method: :get,
         url: url,
@@ -30,20 +28,29 @@ class TestFlakiness
       )
       jobs += JSON.parse(response.body)
     end
+    return jobs
+  end
 
-    jobs_by_name = jobs.group_by {|job| job['name']}
+  def self.summarize_by_job
+    results = []
+    get_jobs.group_by {|job| job['name']}.each do |name, samples|
+      results << {
+        name: name,
+        total: samples.count,
+        failed: samples.count {|job| !job["passed"]}
+      }
+    end
+    return results
+  end
 
-    name_to_flakiness = {}
-
-    jobs_by_name.each do |name, samples|
-      total = samples.count
-      if total > MIN_SAMPLES
-        failed = samples.count {|job| !job["passed"]}
-        name_to_flakiness[name] = (1.0 * failed / total).round(2)
+  def self.calculate_test_flakiness
+    results = {}
+    summarize_by_job.each do |summary|
+      if summary[:total] > MIN_SAMPLES
+        results[summary[:name]] = (1.0 * summary[:failed] / summary[:total]).round(2)
       end
     end
-
-    name_to_flakiness
+    return results
   end
 
   CACHE_FILENAME = (File.dirname(__FILE__) + "/../../dashboard/tmp/cache/flakiness.json").freeze
