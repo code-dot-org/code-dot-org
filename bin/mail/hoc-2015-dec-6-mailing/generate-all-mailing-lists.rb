66b6340556434cf038e091b541f85b17a41a199a
Josh Lory <josh.lory@code.org>
2017-10-24 09:53:22 -0700

    Remove the old marketing mail scripts that were used before Pardot

--- bin/mail/hoc-2015-dec-6-mailing/generate-all-mailing-lists.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require_relative '../mailing-common/mailing-list-utils'
-
-# 2015 organizers (kind_s:HocSignup2015)
-generate('hoc-organizers')
-
-# All the rest
-generate('all')
