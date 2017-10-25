f7e798c5e80cd494e22512519b331c92e5bb734a
Clare Constantine <clare@code.org>
2017-10-25 15:25:30 -0700

    add name to quick view serializer

--- dashboard/app/serializers/api/v1/pd/application_quick_view_serializer.rb
+++ dashboard/app/serializers/api/v1/pd/application_quick_view_serializer.rb
@@ -1,5 +1,9 @@
 class Api::V1::Pd::ApplicationQuickViewSerializer < ActiveModel::Serializer
-  attributes :created_at, :district_name, :school_name, :status
+  attributes :created_at, :name, :district_name, :school_name, :status
+
+  def name
+    "#{object.sanitize_form_data_hash[:first_name]} #{object.sanitize_form_data_hash[:last_name]}"
+  end
 
   def school_name
     object.user.school_info.try(:effective_school_name).try(:titleize) || ''
