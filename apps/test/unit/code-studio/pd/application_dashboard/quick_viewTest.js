341665f83d4a3abf331a26e0bb837bc2de2096f4
Clare Constantine <clare@code.org>
2017-10-27 12:17:35 -0700

    basic ui tests for quick view

---
+++ apps/test/unit/code-studio/pd/application_dashboard/quick_viewTest.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import QuickView from '@cdo/apps/code-studio/pd/application_dashboard/quick_view';
+import {expect} from 'chai';
+import sinon from 'sinon';
+
+describe("Quick View", () => {
+  const fakeRouter = {
+    createHref() {}
+  };
+
+  const context = {
+    router: fakeRouter
+  };
+
+  it("Initially renders a spinner", () => {
+    let quickView = shallow(
+      <QuickView route={{'path':'csf_facilitators', 'title':'CSF Facilitators'}}/>, {context}
+    );
+
+    expect(quickView.find('Spinner')).to.have.length(1);
+  });
+
+  it("Generates 1 table after hearing from server", () => {
+    let server = sinon.fakeServer.create();
+
+    server.respondWith("GET", '/api/v1/pd/applications/quick_view?role=csf_facilitators',
+      [
+        200,
+        {"Content-Type": "application/json"},
+        JSON.stringify([{"id":8,"created_at":"2017-10-25T21:26:06.000Z","applicant_name":"Clare Constantine","district_name":null,"school_name":null,"status":"unreviewed"}])
+      ]
+    );
+
+    let quickView = shallow(
+      <QuickView route={{'path':'csf_facilitators', 'title':'CSF Facilitators'}}/>, {context}
+    );
+
+    server.respond();
+    expect(quickView.find('QuickViewTable')).to.have.length(1);
+    expect(quickView.find('Spinner')).to.have.length(0);
+  });
+});
