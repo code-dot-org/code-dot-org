1caa547d753e389b5f1d500b857ebb26846247bf
Clare Constantine <clare@code.org>
2017-10-30 15:18:58 -0700

    Display regional partner name on quick and summary views if not admin

--- apps/src/sites/studio/pages/pd/application_dashboard/index.js
+++ apps/src/sites/studio/pages/pd/application_dashboard/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApplicationDashboard from '@cdo/apps/code-studio/pd/application_dashboard/application_dashboard';
+import getScriptData from '@cdo/apps/util/getScriptData';
+
 
 document.addEventListener("DOMContentLoaded", function (event) {
   ReactDOM.render(
-    <ApplicationDashboard/>,
+    <ApplicationDashboard
+      partnerName={getScriptData("props")['partnerName']}
+    />,
     document.getElementById('application-container')
   );
 });
