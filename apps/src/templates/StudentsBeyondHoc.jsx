1194f3076d9c0d8fd83652a3b1ef708b80fdf482
Erin Bond <erin.bond@code.org>
2017-10-30 13:38:52 -0700

    CourseBlocksStudentGradeBands on Congrats

--- apps/src/templates/StudentsBeyondHoc.jsx
+++ apps/src/templates/StudentsBeyondHoc.jsx
@@ -1,25 +1,37 @@
 import React, { PropTypes, Component } from 'react';
-import i18n from "@cdo/locale";
-import color from "../util/color";
+import i18n from '@cdo/locale';
+import color from '../util/color';
+import Responsive from '../responsive';
+import CourseBlocksStudentGradeBands from './studioHomepages/CourseBlocksStudentGradeBands';
 
 const styles = {
   heading: {
-    color: color.teal
-  }
+    color: color.teal,
+    width: '100%'
+  },
 };
 
 export default class StudentsBeyondHoc extends Component {
   static propTypes = {
     completedTutorialType: PropTypes.oneOf(['applab', 'newMinecraft', 'oldMinecraft', 'other']).isRequired,
-    MCShareLink: PropTypes.string
+    MCShareLink: PropTypes.string,
+    isRtl: PropTypes.bool.isRequired,
+    responsive: PropTypes.instanceOf(Responsive).isRequired
   };
 
   render() {
+    const { isRtl, responsive } = this.props;
+
     return (
-      <div>
+      <div style={styles.container}>
         <h1 style={styles.heading}>
           {i18n.congratsStudentHeading()}
         </h1>
+        <CourseBlocksStudentGradeBands
+          isRtl={isRtl}
+          responsive={responsive}
+          showContainer={false}
+        />
       </div>
     );
   }
