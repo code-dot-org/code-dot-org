3f32630aacb3370835645a22ed88f1fddbd58bca
Joanne Polsky <joanne@code.org>
2017-10-31 16:27:35 -0700

    PR updates, use TSV to determine which IDs to re-write

--- dashboard/test/models/school_test.rb
+++ dashboard/test/models/school_test.rb
@@ -7,7 +7,7 @@ class SchoolTest < ActiveSupport::TestCase
     # Populate school districts, since schools depends on them as a foreign key.
     SchoolDistrict.seed_all(stub_school_data: true, force: true)
 
-    schools = School.merge_from_csv('test/fixtures/schools.tsv')
+    schools = School.merge_from_csv(School.get_seed_filename(true))
     assert_equal(16, schools.size, 'test data contains 16 schools')
     assert_not_nil School.find_by(
       {
