c28c4bcfd5c282c6daa99f2a74dce1b17dc43f7c
Drew Samnick <drew.samnick@code.org>
2017-10-31 15:37:03 -0700

    Create initial census data schema and models

---
+++ dashboard/test/models/hoc_census2017v2_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class HocCensus2017v2Test < ActiveSupport::TestCase
+  test "basic HOC census 2017 v2 submission" do
+    submission = build(:hoc_census2017v2)
+    assert submission.valid?, submission.errors.full_messages
+  end
+end
