5c3daaf4d164b6d0fabf7f898043dda9696b6f45
Brent Van Minnen <bjvanminnen@gmail.com>
2017-11-01 12:03:35 -0700

    fix test, address code review feedback

--- apps/src/code-studio/utils.js
+++ apps/src/code-studio/utils.js
@@ -59,7 +59,7 @@ export function updateQueryParam(param, value, useReplaceState=false) {
  * We have various cookies that we want to be environment specific. We accomplish
  * this by tacking on the rack_env (unless we're in prod). This helper gets the
  * appropriate cookie name
- * @param {string} Base cookie name
+ * @param {string} name - Base cookie name
  * @returns {string} Actual cookie name, with the rack_env appended
  */
 export function environmentSpecificCookieName(name) {
