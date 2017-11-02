bd3c4991f72e89b4afb299f384e290a9d51900c0
Brendan Reville <brendan@code.org>
2017-11-03 10:03:12 +1100

    Pegasus migrations: Tutorials now gets displayweight_pre & displayweight_25

--- pegasus/migrations/124_add_more_grade_displayweights_to_tutorials.rb
+++ pegasus/migrations/124_add_more_grade_displayweights_to_tutorials.rb
@@ -2,6 +2,7 @@ Sequel.migration do
   change do
     alter_table(:tutorials) do
       add_column :displayweight_pre, Integer, null: false
+      add_column :displayweight_25, Integer, null: false
     end
   end
 end
