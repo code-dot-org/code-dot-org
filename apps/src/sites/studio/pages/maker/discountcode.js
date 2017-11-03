20f8036f21cc7cda0bdaff696af9efe7de4e62d2
Brent Van Minnen <bjvanminnen@gmail.com>
2017-11-03 11:03:33 -0700

    passing data

--- apps/src/sites/studio/pages/maker/discountcode.js
+++ apps/src/sites/studio/pages/maker/discountcode.js
@@ -1,1 +1,14 @@
-console.log('Hello from discountcode');
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+$(document).ready(() => {
+  const script = document.querySelector('script[data-discountcode]');
+  const scriptData = JSON.parse(script.dataset.discountcode);
+  console.log(scriptData);
+
+  ReactDOM.render(
+    <div> test </div>
+    , document.getElementById('discountcode')
+  );
+});
