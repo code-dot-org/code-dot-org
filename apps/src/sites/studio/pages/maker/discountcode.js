a94020470e99e9b754a5d39ad7a9d7539ff14938
Brent Van Minnen <bjvanminnen@gmail.com>
2017-11-03 11:15:42 -0700

    use actual component

--- apps/src/sites/studio/pages/maker/discountcode.js
+++ apps/src/sites/studio/pages/maker/discountcode.js
@@ -1,14 +1,18 @@
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import EligibilityChecklist from '@cdo/apps/templates/EligibilityChecklist';
+import { Status } from '@cdo/apps/lib/kits/maker/ui/SetupStep';
 
 $(document).ready(() => {
   const script = document.querySelector('script[data-discountcode]');
   const scriptData = JSON.parse(script.dataset.discountcode);
-  console.log(scriptData);
 
   ReactDOM.render(
-    <div> test </div>
+    <EligibilityChecklist
+      statusPD={scriptData.eligibility_status.statusPD ? Status.SUCCEEDED : Status.FAILED}
+      statusStudentCount={scriptData.eligibility_status.statusStudentCount ? Status.SUCCEEDED : Status.FAILED}
+    />
     , document.getElementById('discountcode')
   );
 });
