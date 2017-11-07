f01fd5daaff21580fb97ff3c6373d06726a3baa7
Mei'lani <meyrehay@gmail.com>
2017-11-07 09:25:02 -0800

    Changed how the appendPx function is exported

--- apps/test/unit/applab/designModeTest.js
+++ apps/test/unit/applab/designModeTest.js
@@ -1,37 +1,49 @@
 import {expect} from '../../util/configuredChai';
-import {appendPx} from '@cdo/apps/applab/designMode';
+import designMode from '@cdo/apps/applab/designMode';
 
 describe("appendPx", () => {
   it('returns a valid css positive integer', function () {
-    const cssVal = appendPx(100);
+    const cssVal = designMode.appendPx(100);
     expect(cssVal).to.equal("100px");
   });
   it('returns 0 as a valid value', function () {
-    const cssVal = appendPx(0);
+    const cssVal = designMode.appendPx(0);
     expect(cssVal).to.equal("0px");
   });
   it('returns the given stringified integer as a valid value', function () {
-    const cssVal = appendPx("100");
+    const cssVal = designMode.appendPx("100");
     expect(cssVal).to.equal("100px");
   });
   it('returns a value with px as a valid value', function () {
-    const cssVal = appendPx("100px");
+    const cssVal = designMode.appendPx("100px");
     expect(cssVal).to.equal("100px");
   });
   it('returns an empty string', function () {
-    const cssVal = appendPx("one hundred");
+    const cssVal = designMode.appendPx("one hundred");
     expect(cssVal).to.equal("");
   });
   it('returns an empty string', function () {
-    const cssVal = appendPx({object : 100});
+    const cssVal = designMode.appendPx({object : 100});
     expect(cssVal).to.equal("");
   });
   it('returns an empty string', function () {
-    const cssVal = appendPx(["bark", 300, 400]);
+    const cssVal = designMode.appendPx(["bark", 300, 400]);
     expect(cssVal).to.equal("");
   });
   it('returns an empty string', function () {
-    const cssVal = appendPx([200, 300, 400]);
+    const cssVal = designMode.appendPx([200, 300, 400]);
+    expect(cssVal).to.equal("");
+  });
+  it('returns an empty string', function () {
+    const cssVal = designMode.appendPx();
+    expect(cssVal).to.equal("");
+  });
+  it('returns an empty string', function () {
+    const cssVal = designMode.appendPx(null);
+    expect(cssVal).to.equal("");
+  });
+  it('returns an empty string', function () {
+    const cssVal = designMode.appendPx(undefined);
     expect(cssVal).to.equal("");
   });
 });
