a5233840923a26d2e8f3d299d6e51caa72f9044c
Erin <erin@code.org>
2017-11-07 17:33:03 -0800

    Styling

--- apps/src/lib/kits/maker/ui/DiscountAdminOverride.jsx
+++ apps/src/lib/kits/maker/ui/DiscountAdminOverride.jsx
@@ -5,6 +5,12 @@ import SchoolAutocompleteDropdown from '../../../../templates/SchoolAutocomplete
 import Button from "../../../../templates/Button";
 import ValidationStep, {Status} from '../../../../lib/ui/ValidationStep';
 
+const styles = {
+  title: {
+    fontSize: 32,
+  },
+};
+
 export default class EligibilityChecklist extends Component {
   static propTypes = {
     statusPD: PropTypes.oneOf(Object.values(Status)).isRequired,
@@ -48,85 +54,87 @@ export default class EligibilityChecklist extends Component {
     });
   }
 
-  handleSubmitId = (event) => {
-    this.setState({teacherID : event.value});
+  handleSubmitId = () => {
+    this.setState({teacherID : this.teacherID.value});
   }
 
   render() {
     const {submission, errors} = this.state;
     return (
       <div>
-        <h1>Circuit Playground Kits Admin Override</h1>
+        <h1 style={styles.title}>Circuit Playground Kits Admin Override</h1>
         <form>
           <label>
             <div>Teacher email address or username (for the account they are using in the classroom):</div>
             <input
               value=""
-              onChange={() => {}}
+              ref={input => this.teacherID = input}
             />
             <Button
               color="orange"
               text="Submit"
-              onClick={() => {}}
+              onClick={this.handleSubmitId}
             />
           </label>
         </form>
-        <h2>Eligibility requirements for </h2>
-        <ValidationStep
-          stepName={i18n.eligibilityReqPD()}
-          stepStatus={this.props.statusPD}
-        />
-        <ValidationStep
-          stepName={i18n.eligibilityReqStudentCount()}
-          stepStatus={this.props.statusStudentCount}
-        />
-        <ValidationStep
-          stepName={i18n.eligibilityReqYear()}
-          stepStatus={this.state.statusYear}
-        />
-        <h2>School Data</h2>
-        <SchoolAutocompleteDropdown
-          setField={this.handleDropdownChange}
-          value={submission.nces}
-          showErrorMsg={errors.nces}
-        />
-        <h2>Admin Options</h2>
-        <h4>Option 1: Link teacher account with other accounts</h4>
-        <div>
-          If teacher meets the eligibity requirements but is simply using a
-           different email address for their account than what we have on file,
-           please go to the Studio Person ID admin page to link this acccount
-           to the saccount associated with the email address we have on file.
-        </div>
-        <h4>Option 2: Give teacher a discount code</h4>
-        <div>
-          If the teacher has already received their code and it was for the incorrect amount,
-          email adaaccounts@adafruit.com so they can disable that code first. We should not be
-          doing this override if the teacher has already used the incorrect code to purchase a kit.
-          <form>
-            <label>
-              <input
-                type="radio"
-                name="discountAmount"
-                value="full"
-              />
-              Teacher should receive 100% discount code (kit price would become $0)
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="discountAmount"
-                value="partial"
-              />
-              Teacher should receive partial discount code (kit price would become $97.50)
-            </label>
-            <Button
-              color="orange"
-              text="Submit"
-              onClick={() => {}}
+          <div>
+            <h2>Eligibility requirements for </h2>
+            <ValidationStep
+              stepName={i18n.eligibilityReqPD()}
+              stepStatus={this.props.statusPD}
             />
-          </form>
-        </div>
+            <ValidationStep
+              stepName={i18n.eligibilityReqStudentCount()}
+              stepStatus={this.props.statusStudentCount}
+            />
+            <ValidationStep
+              stepName={i18n.eligibilityReqYear()}
+              stepStatus={this.state.statusYear}
+            />
+            <h2>School Data</h2>
+            <SchoolAutocompleteDropdown
+              setField={this.handleDropdownChange}
+              value={submission.nces}
+              showErrorMsg={errors.nces}
+            />
+            <h2>Admin Options</h2>
+            <h4>Option 1: Link teacher account with other accounts</h4>
+            <div>
+              If teacher meets the eligibity requirements but is simply using a
+               different email address for their account than what we have on file,
+               please go to the Studio Person ID admin page to link this acccount
+               to the saccount associated with the email address we have on file.
+            </div>
+            <h4>Option 2: Give teacher a discount code</h4>
+            <div>
+              If the teacher has already received their code and it was for the incorrect amount,
+              email adaaccounts@adafruit.com so they can disable that code first. We should not be
+              doing this override if the teacher has already used the incorrect code to purchase a kit.
+              <form>
+                <label>
+                  <input
+                    type="radio"
+                    name="discountAmount"
+                    value="full"
+                  />
+                  Teacher should receive 100% discount code (kit price would become $0)
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="discountAmount"
+                    value="partial"
+                  />
+                  Teacher should receive partial discount code (kit price would become $97.50)
+                </label>
+                <Button
+                  color="orange"
+                  text="Submit"
+                  onClick={() => {}}
+                />
+              </form>
+            </div>
+          </div>
       </div>
     );
   }
