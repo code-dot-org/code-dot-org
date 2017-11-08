ed0130bb5c9da371c1c08ed29680159de762fc2c
Brent Van Minnen <bjvanminnen@gmail.com>
2017-11-08 14:34:43 -0800

    application_status lives in model

--- dashboard/test/models/circuit_playground_discount_application_test.rb
+++ dashboard/test/models/circuit_playground_discount_application_test.rb
@@ -44,4 +44,36 @@ class CircuitPlaygroundDiscountApplicationTest < ActiveSupport::TestCase
     assert_equal true, CircuitPlaygroundDiscountApplication.studio_person_pd_eligible?(user1)
     assert_equal true, CircuitPlaygroundDiscountApplication.studio_person_pd_eligible?(user2)
   end
+
+  test 'application_status for unstarted application' do
+    teacher = create :teacher
+
+    CircuitPlaygroundDiscountApplication.stubs(:studio_person_pd_eligible?).returns(true)
+    CircuitPlaygroundDiscountApplication.stubs(:student_progress_eligible?).returns(false)
+
+    expected = {
+      unit_6_intention: nil,
+      has_confirmed_school: false,
+      gets_full_discount: nil,
+      discount_code: nil,
+      is_pd_eligible: true,
+      is_progress_eligible: false,
+    }
+    assert_equal expected, CircuitPlaygroundDiscountApplication.application_status(teacher)
+  end
+
+  test 'application_status for existing application' do
+    teacher = create :teacher
+    create :circuit_playground_discount_application, user: teacher, unit_6_intention: 1
+
+    expected = {
+      unit_6_intention: 1,
+      has_confirmed_school: false,
+      gets_full_discount: nil,
+      discount_code: nil,
+      is_pd_eligible: true,
+      is_progress_eligible: true,
+    }
+    assert_equal expected, CircuitPlaygroundDiscountApplication.application_status(teacher)
+  end
 end
