df5b476f32dc843a1b9d068e75184209a0506fdc
Mei'lani <meyrehay@gmail.com>
2017-11-10 14:21:15 -0800

    Added new values to the dropdowns

--- apps/src/applab/setPropertyDropdown.js
+++ apps/src/applab/setPropertyDropdown.js
@@ -356,7 +356,8 @@ export function getInternalPropertyInfo(element, friendlyPropName) {
  * @returns {!Array<string> | function} droplet dropdown array or function
  */
 function getPropertyValueDropdown(param2) {
-  const dropletConfigDefaultValue = "100";
+  //This is probably what you want to make into an array of stuff
+  const dropletConfigDefaultValue = ["0", "25", "50", "75", "100", "150", "200"];
 
   if (!param2) {
     return [dropletConfigDefaultValue];
@@ -370,7 +371,7 @@ function getPropertyValueDropdown(param2) {
     case "text-color":
     case "background-color":
     case "icon-color":
-      return ['"red"', 'rgb(255,0,0)', 'rgb(255,0,0,0.5)', '"#FF0000"'];
+      return ['"white"', '"red"', '"green"', '"blue"', '"yellow"', 'rgb(255,0,0)', 'rgb(255,0,0,0.5)', '"#FF0000"'];
     case "text-align":
       return ['"left"', '"right"', '"center"', '"justify"'];
     case "fit":
@@ -386,7 +387,7 @@ function getPropertyValueDropdown(param2) {
     case "options":
       return ['["option1", "etc"]'];
     default:
-      return [dropletConfigDefaultValue];
+      return dropletConfigDefaultValue;
   }
 }
 
