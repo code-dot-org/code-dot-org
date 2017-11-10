57176c671d3142978a053b84b6d80083bca0a0ca
Josh Lory <josh.lory@code.org>
2017-11-09 16:02:08 -0800

    Use `convertXmlToBlockly` to transform all XML groups on the page

--- apps/src/sites/studio/pages/embedBlocks.js
+++ apps/src/sites/studio/pages/embedBlocks.js
@@ -2,17 +2,8 @@
  * Entry point to build a bundle containing a set of globals used when displaying
  * embedded blocks via blockly-in-an-iframe.
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReadOnlyBlockSpace from '@cdo/apps/templates/ReadOnlyBlockSpace';
+import { convertXmlToBlockly } from '@cdo/apps/templates/instructions/utils';
 
-const div = document.getElementById('embed-blocks');
-const xml = new DOMParser().parseFromString(div.firstElementChild.dataset.blocks, 'text/xml');
-div.removeAttribute('id');
-
-ReactDOM.render(
-  <ReadOnlyBlockSpace
-    block={xml}
-  />,
-  div,
-);
+window.addEventListener('load', () => {
+  convertXmlToBlockly(document.body);
+});
