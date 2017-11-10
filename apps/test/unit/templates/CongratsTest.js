fc7a1f4744f36320f8761873548b5a690f55de77
Erin Bond <erin.bond@code.org>
2017-11-10 10:39:12 -0800

    update CongratsTest

--- apps/test/unit/templates/CongratsTest.js
+++ apps/test/unit/templates/CongratsTest.js
@@ -1,36 +1,35 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import {expect} from '../../util/configuredChai';
 import Congrats from '@cdo/apps/templates/Congrats';
+import {Provider} from 'react-redux';
+import {combineReducers, createStore} from 'redux';
+import responsive from '@cdo/apps/code-studio/responsiveRedux';
+import isRtl from '@cdo/apps/code-studio/isRtlRedux';
 
 describe('Congrats', () => {
-  it('renders a Certificate component', () => {
-    const wrapper = shallow(
-      <Congrats
-        completedTutorialType="other"
-        isRtl={false}
-      />
+  let root;
+
+  beforeEach(() => {
+    const store = createStore(combineReducers({responsive, isRtl}));
+    root = mount(
+      <Provider store={store}>
+        <Congrats
+          completedTutorialType="other"
+        />
+      </Provider>
     );
-    expect(wrapper.find('Certificate').exists()).to.be.true;
+  });
+
+  it('renders a Certificate component', () => {
+    expect(root.find('Certificate').exists());
   });
 
   it('renders a StudentsBeyondHoc component', () => {
-    const wrapper = shallow(
-      <Congrats
-        completedTutorialType="other"
-        isRtl={false}
-      />
-    );
-    expect(wrapper.find('StudentsBeyondHoc').exists()).to.be.true;
+    expect(root.find('StudentsBeyondHoc').exists());
   });
 
   it('renders a TeachersBeyondHoc component', () => {
-    const wrapper = shallow(
-      <Congrats
-        completedTutorialType="other"
-        isRtl={false}
-      />
-    );
-    expect(wrapper.find('TeachersBeyondHoc').exists()).to.be.true;
+    expect(root.find('TeachersBeyondHoc').exists());
   });
 });
