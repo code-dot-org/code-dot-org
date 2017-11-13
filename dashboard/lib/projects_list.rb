828446eb6b98cea0c3ef0e9f89e9a21d7c157af9
David Bailey <davidsbailey@users.noreply.github.com>
2017-11-13 13:08:52 -0800

    show published minecraft hero projects in Public Gallery

--- dashboard/lib/projects_list.rb
+++ dashboard/lib/projects_list.rb
@@ -11,7 +11,7 @@ module ProjectsList
     gamelab: ['gamelab'],
     playlab: ['playlab', 'gumball', 'infinity', 'iceage'],
     artist: ['artist', 'frozen'],
-    minecraft: ['minecraft_adventurer', 'minecraft_designer'],
+    minecraft: ['minecraft_adventurer', 'minecraft_designer', 'minecraft_hero'],
     events: %w(starwars starwarsblocks starwarsblocks_hour flappy bounce sports basketball),
     k1: ['artist_k1', 'playlab_k1'],
   }.freeze
