dfc405f06eea6ec9abfe1358a1eda7a205e7da07
Josh Lory <josh.lory@code.org>
2017-11-16 13:14:51 -0800

    Fix Pegasus tests

--- pegasus/helpers/hoc_helpers.rb
+++ pegasus/helpers/hoc_helpers.rb
@@ -117,7 +117,7 @@ def complete_tutorial(tutorial={})
     end
 
     site = row[:referer]
-    if DCDO.get('new_congrats', false) && tutorial[:orgname].include?('Code.org')
+    if DCDO.get('new_congrats', false) && tutorial[:orgname].try(:include?, 'Code.org')
       site = CDO.studio_url
     end
 
