0dad8ad415f65322750a066913d205f801649491
Brent Van Minnen <bjvanminnen@gmail.com>
2017-11-16 14:09:02 -0800

    use cancan

--- dashboard/app/controllers/maker_controller.rb
+++ dashboard/app/controllers/maker_controller.rb
@@ -1,12 +1,10 @@
 class MakerController < ApplicationController
-  before_action :authenticate_user!, only: :discountcode
+  authorize_resource class: :maker_discount, only: :discountcode
 
   def setup
   end
 
   def discountcode
-    return head :not_found unless current_user.teacher?
-
     application_status = CircuitPlaygroundDiscountApplication.application_status(current_user)
     render 'discountcode', locals: {script_data: application_status}
   end
