72b28b5dce5ab777d00c0d7e7a84da20bbeb25c9
Mehal Shah <mehal@code.org>
2017-11-17 10:25:14 -0800

    PR feedback

--- dashboard/db/migrate/20171117004332_make_user_and_status_nullable_in_pd_applications.rb
+++ dashboard/db/migrate/20171117004332_make_user_and_status_nullable_in_pd_applications.rb
@@ -1,5 +1,13 @@
 class MakeUserAndStatusNullableInPdApplications < ActiveRecord::Migration[5.0]
-  def change
+  def up
+    change_column_null :pd_applications, :user_id, true
+    change_column_null :pd_applications, :status, true
+  end
+
+  def down
+    Pd::Application::ApplicationBase.where(user_id: nil).update(user_id: 0)
+    Pd::Application::ApplicationBase.where(status: nil).update(status: 'unreviewed')
+
     change_column_null :pd_applications, :user_id, false
     change_column_null :pd_applications, :status, false
   end
