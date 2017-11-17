635f91cf88f58fbfd60689bff7e172b732985d42
Brent Van Minnen <bjvanminnen@gmail.com>
2017-11-17 11:06:18 -0800

    tests for beginning application

--- dashboard/app/controllers/maker_controller.rb
+++ dashboard/app/controllers/maker_controller.rb
@@ -11,17 +11,20 @@ class MakerController < ApplicationController
 
   # begins a discount code application
   def apply
-    # TODO: write tests
     intention = params.require(:unit_6_intention)
 
     # check to see if we have an existing application for any users associated with
-    # this studio_person_id
+    # this studio_person_id (in which case we can't start another)
+    application = CircuitPlaygroundDiscountApplication.find_by_studio_person_id(current_user.studio_person_id)
+    return head :forbidden if application
 
     # validate that we're eligible
+    application_status = CircuitPlaygroundDiscountApplication.application_status(current_user)
+    return head :forbidden unless application_status[:is_pd_eligible] && application_status[:is_progress_eligible]
 
     # finally, create our application
     application = CircuitPlaygroundDiscountApplication.create!(user: current_user, unit_6_intention: intention)
 
-    render json: {valid: application.valid_unit_6_intention?}
+    render json: {eligible: application.eligible_unit_6_intention?}
   end
 end
