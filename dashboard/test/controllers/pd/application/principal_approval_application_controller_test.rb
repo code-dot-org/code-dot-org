03c5a28b3661b6dfc44976a2b1526a410e917bf9
Mehal Shah <mehal@code.org>
2017-11-17 12:33:46 -0800

    Basic controller actions with tests

--- dashboard/test/controllers/pd/application/principal_approval_application_controller_test.rb
+++ dashboard/test/controllers/pd/application/principal_approval_application_controller_test.rb
@@ -4,14 +4,34 @@ module Pd::Application
   class PrincipalApprovalApplicationControllerTest < ::ActionController::TestCase
     test 'invalid guid goes to not_found page' do
       get :new, params: {application_guid: 'invalid_guid'}
-      assert_redirected_to :not_found
+      assert_template :not_found
+      assert_response :success
     end
 
     test 'already completed principal application goes to submitted page' do
-      Pd::Application::PrincipalApproval1819Application.create
+      application_uuid = SecureRandom.uuid
+      create :pd_principal_approval1819_application, application_guid: application_uuid
+      get :new, params: {application_guid: application_uuid}
+      assert_template :submitted
+      assert_response :success
     end
 
     test 'completed teacher application but no principal application goes to new page' do
+      application_uuid = SecureRandom.uuid
+      teacher_application = create :pd_teacher1819_application, application_guid: application_uuid
+
+      get :new, params: {application_guid: application_uuid}
+      assert_template :new
+      assert_response :success
+      assert_equal teacher_application, assigns(:teacher_application)
+    end
+
+    test 'renders not available on production' do
+      Rails.env.stubs(:production?).returns(true)
+      get :new, params: {application_guid: 'some_guid'}
+      assert_template :not_available
+      assert_response :success
+      Rails.env.unstub(:production)
     end
   end
 end
