11b7ddb39750476621563e99bfe87275d240eeca
Josh Lory <josh.lory@code.org>
2017-11-20 13:48:32 -0800

    Add initial course tiles to /challenge

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -18,6 +18,7 @@ Dashboard::Application.routes.draw do
   get '/dashboardapi/terms-and-privacy', to: "home#terms_and_privacy"
   get '/dashboardapi/teacher-announcements', to: "home#teacher_announcements"
   get '/dashboardapi/hoc-courses-narrow', to: "home#hoc_courses_narrow"
+  get '/dashboardapi/hoc-courses-challenge', to: "home#hoc_courses_challenge"
 
   get "/home", to: "home#home"
 
