4f5dc7589a53bc8f6d52e5445befcf90b40792a5
Will Jordan <will@code.org>
2017-11-21 17:39:33 -0800

    Run self-hosted solr on non-production daemons

--- cookbooks/cdo-solr/metadata.rb
+++ cookbooks/cdo-solr/metadata.rb
@@ -6,4 +6,7 @@ description      'Installs/Configures cdo-solr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.7'
 
-depends          'cdo-java-7'
+depends 'cdo-java-7'
+depends 'ark'
+depends 'poise-service'
+depends 'apt'
