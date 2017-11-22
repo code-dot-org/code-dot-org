3d8a103eb665245c2872a6faf30dd758f743c793
Clare Constantine <clare@code.org>
2017-11-22 14:32:44 -0800

    regional partner dropdown only visible to workshop admins

--- dashboard/app/controllers/pd/application_dashboard_controller.rb
+++ dashboard/app/controllers/pd/application_dashboard_controller.rb
@@ -5,10 +5,12 @@ module Pd
     def index
       view_options(full_width: true)
 
+      is_workshop_admin = current_user.permission? UserPermission::WORKSHOP_ADMIN
       @script_data = {
         props: {
           regionalPartnerName: current_user.regional_partners.first.try(:name),
-          regionalPartners: RegionalPartner.select("id, name")
+          regionalPartners: RegionalPartner.select("id, name"),
+          isWorkshopAdmin: is_workshop_admin
         }.to_json
       }
     end
