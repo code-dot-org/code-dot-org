0544fe7faa35d04191af209b2511ded3cff67668
Andrew Oberhardt <Andrew@Code.org>
2017-11-28 14:48:52 -0800

    PR feedback: generalize solution and apply to teacher application

--- dashboard/app/models/pd/application/teacher1819_application.rb
+++ dashboard/app/models/pd/application/teacher1819_application.rb
@@ -282,5 +282,10 @@ module Pd::Application
     def state_code
       STATE_ABBR_WITH_DC_HASH.key(state_name).try(:to_s)
     end
+
+    # @override
+    def find_existing
+      Pd::Application::Teacher1819Application.find_by(user: user)
+    end
   end
 end
