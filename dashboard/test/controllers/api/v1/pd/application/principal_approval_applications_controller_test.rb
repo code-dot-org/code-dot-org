0b47c04dd11ad04b7e8e6e979f9885595a8ac4b9
Mehal Shah <mehal@code.org>
2017-11-30 19:40:36 -0800

    Principal Application answers (and scoring) appears at the bottom of the application

--- dashboard/test/controllers/api/v1/pd/application/principal_approval_applications_controller_test.rb
+++ dashboard/test/controllers/api/v1/pd/application/principal_approval_applications_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 module Api::V1::Pd::Application
   class PrincipalApprovalApplicationsControllerTest < ::ActionController::TestCase
     setup_all do
-      @teacher_application = create :pd_teacher1819_application
+      @teacher_application = create :pd_teacher1819_application, application_guid: SecureRandom.uuid
       @test_params = {
         form_data: build(:pd_principal_approval1819_application_hash),
         application_guid: @teacher_application.application_guid
@@ -32,6 +32,21 @@ module Api::V1::Pd::Application
         application_guid: @teacher_application.application_guid
       )
       assert_equal principal, application.user
+
+      @teacher_application.reload
+      expected_principal_fields = {
+        principal_approval: 'Yes',
+        schedule_confirmed: 'Yes',
+        diversity_recruitment: 'Yes',
+        free_lunch_percent: '50%',
+        underrepresented_minority_percent: '52.0',
+        wont_replace_existing_course: Pd::Application::PrincipalApproval1819Application.options[:replace_course][1]
+      }
+      actual_principal_fields = @teacher_application.sanitize_form_data_hash.select do |k, _|
+        expected_principal_fields.keys.include? k
+      end
+
+      assert_equal expected_principal_fields, actual_principal_fields
     end
 
     test 'Sends principal approval received email on successful create' do
