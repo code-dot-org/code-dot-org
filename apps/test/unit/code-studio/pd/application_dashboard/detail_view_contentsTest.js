3b056d5d58b8ce36840cf31b3324f5f22e04f467
Elijah Hamovitz <elijahhamovitz@gmail.com>
2017-12-01 11:59:07 -0800

    fix existing tests

--- apps/test/unit/code-studio/pd/application_dashboard/detail_view_contentsTest.js
+++ apps/test/unit/code-studio/pd/application_dashboard/detail_view_contentsTest.js
@@ -63,7 +63,9 @@ describe("DetailViewContents", () => {
       it(`the dropdown is disabled until the Edit button is clicked in ${applicationData.type}`, () => {
         const detailView = mountDetailView(applicationData.type);
 
-        expect(detailView.find('#DetailViewHeader Button')).to.have.length(1);
+        expect(detailView.find('#DetailViewHeader Button').map((button) => {
+          return button.text();
+        })).to.deep.equal(['Lock', 'Edit']);
         expect(detailView.find('#DetailViewHeader FormControl').prop('disabled')).to.be.true;
         expect(detailView.find('#Notes').prop('disabled')).to.be.true;
         if (applicationData.scoredQuestions) {
@@ -72,10 +74,10 @@ describe("DetailViewContents", () => {
           })).to.deep.equal([true, true]);
         }
 
-        detailView.find('#DetailViewHeader Button').simulate('click');
+        detailView.find('#DetailViewHeader Button').last().simulate('click');
         expect(detailView.find('#DetailViewHeader Button').map((button) => {
           return button.text();
-        })).to.deep.equal(['Save', 'Cancel']);
+        })).to.deep.equal(['Lock', 'Save', 'Cancel']);
         expect(detailView.find('#DetailViewHeader FormControl').prop('disabled')).to.be.false;
         expect(detailView.find('#Notes').prop('disabled')).to.be.false;
         if (applicationData.scoredQuestions) {
