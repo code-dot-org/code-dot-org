b67e3988500e24a2ee59bc8674a745e574467246
Brent Van Minnen <bjvanminnen@gmail.com>
2017-12-06 13:37:34 -0800

    helper debug code to make teachers eligible

--- bin/oneoff/populate_discount_codes.rb
+++ bin/oneoff/populate_discount_codes.rb
@@ -9,21 +9,6 @@ require 'csv'
 require_relative '../../dashboard/config/environment'
 require src_dir 'database'
 
-# @param {boolean} full_discount - Is this a full or partial discount?
-# @param {number} n - How many entries to crate
-def populate_fake_data(n, full_discount, expiration)
-  n.times do
-    # Create a bogus code
-    prefix = full_discount ? 'FAKE0_' : 'FAKE100_'
-    code = SecureRandom.hex(7)
-    CircuitPlaygroundDiscountCode.create!(
-      code: prefix + code,
-      full_discount: full_discount,
-      expiration: expiration
-    )
-  end
-end
-
 def populate_csv_data(filename, full_discount, expiration)
   csv_contents = CSV.read(filename)
   # skip header row
@@ -39,12 +24,11 @@ def populate_csv_data(filename, full_discount, expiration)
 end
 
 full_discount = ARGV[0]
-filename_or_n = ARGV[1]
+filename = ARGV[1]
 
-unless full_discount && filename_or_n
+unless full_discount && filename
   puts 'Expected usage: '
-  puts 'populate_discount_codes <full_discount?> <path_to_csv_file> # real data'
-  puts 'populate_discount_codes <full_discount?> <n> # fake data'
+  puts 'populate_discount_codes <full_discount?> <path_to_csv_file>'
   exit(-1)
 end
 
@@ -57,15 +41,72 @@ else
   exit(-1)
 end
 
-n = Integer(filename_or_n) rescue false
-if n
-  # If we're passed a number, just populate fake data
-  puts "Populating fake data: #{filename_or_n} codes with full_discount=#{full_discount}"
-  populate_fake_data(n, full_discount, Date.new(2018, 12, 31))
-else
-  # If we're passed a filename, parse the CSV and populate
-  puts "Reading #{filename_or_n}"
-  populate_csv_data(filename_or_n, full_discount, Date.new(2018, 12, 31))
-end
+populate_csv_data(filename, full_discount, Date.new(2018, 12, 31))
 
 puts 'Complete'
+
+#####
+# Begin debug helper methods
+#
+# What follows is a set of helper methods for discount code functionality that
+# can be copy/pasted into the console. They are designed to be used in non-prod
+# environments
+#####
+
+# This method is not used directly in this script, but can be used to populate
+# the db with fake discount codes. Usage looks something like:
+# populate_fake_data(10, false, Date.new(2018, 12, 31))
+# @param {boolean} full_discount - Is this a full or partial discount?
+# @param {number} n - How many entries to create
+def populate_fake_data(n, full_discount, expiration)
+  n.times do
+    # Create a bogus code
+    prefix = full_discount ? 'FAKE0_' : 'FAKE100_'
+    code = SecureRandom.hex(7)
+    CircuitPlaygroundDiscountCode.create!(
+      code: prefix + code,
+      full_discount: full_discount,
+      expiration: expiration
+    )
+  end
+end
+
+# This method is not used directly in this script, but can be used to add a teacher
+# to a CSD cohort, so that they will be considered PD eligible for discount codes.
+# Because this is modifying the cohort, it should not be run on prod (or if it is,
+# you need to be sure to clean up after yourself).
+def set_user_pd_eligible(teacher)
+  teacher.cohorts << Cohort.find_or_create_by('CSD-TeacherConHouston')
+end
+
+# This method is not used directly in this script, but can be used to create a
+# fake section for a teacher that causes our teacher to meet eligibility requirements
+# i.e. 10+ students with progress in 5+ levels in csd2 and csd3
+# Because it will create fake data, it should not be run on prod (or if it is,
+# you need to be sure to clean up after yourself).
+def create_discount_eligible_section(teacher, section_name = 'Discount Eligible Section')
+  section = Section.create!(name: section_name, user_id: teacher.id)
+  csd2 = Script.get_from_cache('csd2')
+  csd3 = Script.get_from_cache('csd3')
+
+  csd2_programming_level_ids = csd2.levels.select {|level| level.is_a?(Weblab)}.map(&:id).first(5)
+  csd3_programming_level_ids = csd3.levels.select {|level| level.is_a?(Gamelab)}.map(&:id).first(5)
+
+  10.times do |n|
+    student = User.create!(
+      name: "Student#{n}",
+      user_type: 'student',
+      age: 20,
+      password: 'secret',
+      provider: 'sponsored',
+    )
+    section.add_student(student)
+    csd2_programming_level_ids.each do |level_id|
+      UserLevel.find_or_create_by!(user_id: student.id, level_id: level_id, script_id: csd2.id)
+    end
+    csd3_programming_level_ids.each do |level_id|
+      UserLevel.find_or_create_by!(user_id: student.id, level_id: level_id, script_id: csd3.id)
+    end
+  end
+  section
+end
