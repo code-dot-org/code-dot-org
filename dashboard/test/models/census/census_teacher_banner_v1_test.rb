c4318589e3ab1c70ea109fb1732f40e963e3372a
Drew Samnick <drew.samnick@code.org>
2017-12-06 16:12:10 -0800

    Remove unneeded custom message from refutes

--- dashboard/test/models/census/census_teacher_banner_v1_test.rb
+++ dashboard/test/models/census/census_teacher_banner_v1_test.rb
@@ -8,12 +8,12 @@ class Census::CensusTeacherBannerV1Test < ActiveSupport::TestCase
 
   test "teacher banner v1 submission for non-teacher is invalid" do
     submission = build(:census_teacher_banner_v1, submitter_role: "PARENT")
-    refute submission.valid?, submission.errors.full_messages
+    refute submission.valid?
   end
 
   test "teacher banner v1 submission missing how_many_10_hours and how_many_20_hours is invalid" do
     submission = build(:census_teacher_banner_v1, how_many_10_hours: nil, how_many_20_hours: nil)
-    refute submission.valid?, submission.errors.full_messages
+    refute submission.valid?
   end
 
   test "teacher banner v1 submission with one of how_many_10_hours and how_many_20_hours is valid" do
