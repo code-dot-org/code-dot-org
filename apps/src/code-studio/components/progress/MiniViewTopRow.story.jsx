683284262af9c128f86be0c3fe4cff73a9b63285
Brad Buchanan <brad@code.org>
2017-12-07 17:39:22 -0800

    Simplify story

--- apps/src/code-studio/components/progress/MiniViewTopRow.story.jsx
+++ apps/src/code-studio/components/progress/MiniViewTopRow.story.jsx
@@ -2,48 +2,42 @@ import React from 'react';
 import MiniViewTopRow from './MiniViewTopRow';
 import progress from '@cdo/apps/code-studio/progressRedux';
 
-export default storybook => {
-  const initialState = {
-    progress: {
-      stages: [
-        {
-          levels: []
-        }
-      ],
-      focusAreaStageIds: [],
-      isSummaryView: false,
-      professionalLearningCourse: false
-    }
-  };
-
-  storybook
-    .storiesOf('MiniViewTopRow', module)
-    .withReduxStore({progress}, initialState)
-    .addStoryTable([
+const initialState = {
+  progress: {
+    stages: [
       {
-        name: 'basic',
-        story: () => {
-          return (
-            <div style={{width: 635, position: 'relative'}}>
-              <MiniViewTopRow
-                scriptName="course1"
-                linesOfCodeText="Total lines of code: 120"
-              />
-            </div>
-          );
-        }
-      },
-      {
-        name: 'no lines of text',
-        story: () => {
-          return (
-            <div style={{width: 635, position: 'relative'}}>
-              <MiniViewTopRow
-                scriptName="course1"
-              />
-            </div>
-          );
-        }
+        levels: []
       }
-    ]);
+    ],
+    focusAreaStageIds: [],
+    isSummaryView: false,
+    professionalLearningCourse: false
+  }
 };
+
+export default storybook => storybook
+  .storiesOf('MiniViewTopRow', module)
+  .withReduxStore({progress}, initialState)
+  .addStoryTable([
+    {
+      name: 'basic',
+      story: () => (
+        <div style={{width: 635, position: 'relative'}}>
+          <MiniViewTopRow
+            scriptName="course1"
+            linesOfCodeText="Total lines of code: 120"
+          />
+        </div>
+      )
+    },
+    {
+      name: 'no lines of text',
+      story: () => (
+        <div style={{width: 635, position: 'relative'}}>
+          <MiniViewTopRow
+            scriptName="course1"
+          />
+        </div>
+      )
+    }
+  ]);
