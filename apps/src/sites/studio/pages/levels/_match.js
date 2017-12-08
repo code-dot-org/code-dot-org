a2ae751960aea90c31b76eb5caaa47cf558e80e4
Brent Van Minnen <bjvanminnen@gmail.com>
2017-12-07 16:56:01 -0800

    fix lint errors

--- apps/src/sites/studio/pages/levels/_match.js
+++ apps/src/sites/studio/pages/levels/_match.js
@@ -1,8 +1,6 @@
-/*global jQuery*/
+/*global jQuery, CDOSounds */
 
-// TODO: import registerGetResult
-
-jQuery.fn.swap = function(b){
+jQuery.fn.swap = function (b) {
   // method from: http://blog.pengoworks.com/index.cfm/2008/9/24/A-quick-and-dirty-swap-method-for-jQuery
   b = jQuery(b)[0];
   var a = this[0];
@@ -21,22 +19,19 @@ $(function () {
     activeClass: "active",
     hoverClass: "hover",
     accept: ".answerlist,.answerslot",
-    drop: function(event, ui)
-    {
+    drop: function (event, ui) {
       CDOSounds.play('click');
       // once an answer is in the central list of slots, it will just swap with whatever it's dragged onto
-      if (ui.draggable.hasClass("answerslot"))
-      {
+      if (ui.draggable.hasClass("answerslot")) {
         // swap this empty slot and the answer dragged onto it
         ui.draggable.swap(event.target);
 
         // remove offset coordinates from this item
         ui.draggable.css({'top': 'auto','left': 'auto'});
-      }
-      // when an answer is in the rightmost list of answers, it can be dragged in to replace an empty slot
-      // in the central list of slots.
-      else
-      {
+      } else {
+        // when an answer is in the rightmost list of answers, it can be dragged in to replace an empty slot
+        // in the central list of slots.
+
         var movingItem = ui.draggable.detach();
 
         // replace target with this new item
@@ -58,8 +53,7 @@ $(function () {
         movingItem.droppable({
           accept: ".answerslot",
           activeClass: "active",
-          drop: function(event, ui)
-          {
+          drop: function (event, ui) {
             CDOSounds.play('whoosh');
 
             // remove offset coordinates from the dragged item
@@ -74,7 +68,7 @@ $(function () {
             ui.draggable.swap(event.target);
 
             // move the target object back to its old location for a moment
-            $(event.target).css({'top': -diffY + 'px'})
+            $(event.target).css({'top': -diffY + 'px'});
 
             // and animate back to its new location
             $(event.target).animate({'top': '0px'});
@@ -93,17 +87,17 @@ window.dashboard.codeStudioLevels.registerGetResult(
 
     var response = [];
 
-    for (index = 0; index < elements.length; index++) {
-      value = elements[index];
+    for (var index = 0; index < elements.length; index++) {
+      var value = elements[index];
 
       var originalIndex = value.getAttribute("originalIndex");
       response.push(originalIndex);
-      if (originalIndex == null) {
+      if (originalIndex === null) {
         // nothing dragged in this slot yet
         wrongAnswer = true;
 
         $("#xmark_" + index).hide();
-      } else if (originalIndex != String(index)) {
+      } else if (originalIndex !== String(index)) {
         // wrong answer
         wrongAnswer = true;
 
