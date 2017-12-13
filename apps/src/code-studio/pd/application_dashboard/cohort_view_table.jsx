f973e97e0afd8c81b22100f5f26ffae5dc7605d7
Mehal Shah <mehal@code.org>
2017-12-13 14:29:19 -0800

    Checkpoint

--- apps/src/code-studio/pd/application_dashboard/cohort_view_table.jsx
+++ apps/src/code-studio/pd/application_dashboard/cohort_view_table.jsx
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import Table from 'reactabular';
 
 export default class QuickViewTable extends React.Component {
   static propTypes = {
@@ -7,7 +8,54 @@ export default class QuickViewTable extends React.Component {
 
   constructColumns() {
     return [
-      {}, {}, {}, {}, {}, {}, {}
+      {
+        property: 'date_accepted',
+        header: {
+          label: 'Date Accepted'
+        },
+        cell: {
+          format: (date_accepted) => {
+            return new Date(date_accepted).toLocaleDateString('en-us', {month: 'long', day: 'numeric'});
+          }
+        }
+      }, {
+        property: 'name',
+        header: {
+          label: 'Name'
+        }
+      }, {
+        property: 'district_name',
+        header: {
+          label: 'School District'
+        }
+      }, {
+        property: 'school_name',
+        header: {
+          label: 'School Name'
+        }
+      }, {
+        property: 'email',
+        header: {
+          label: 'Email'
+        }
+      }, {
+        property: 'registered_for_summer_workshop',
+        header: {
+          label: 'Registered Summer Workshop'
+        }
+      }
     ];
+  }
+
+  render() {
+    return (
+      <Table.Provider
+        className="pure-table table-striped"
+        columns={this.constructColumns()}
+      >
+        <Table.Header />
+        <Table.Body rows={this.props.data} rowKey="email"/>
+      </Table.Provider>
+    );
   }
 }
