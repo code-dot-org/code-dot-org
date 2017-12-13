465ac6dcfc02e226f58ffef489de627e2b3c72c5
Josh Lory <josh.lory@code.org>
2017-12-13 12:13:24 -0800

    Add Enzyme unit test for `ProjectCardGrid`

---
+++ apps/test/unit/templates/projects/ProjectCardGridTest.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from '../../../util/configuredChai';
+import ProjectCardGrid from '@cdo/apps/templates/projects/ProjectCardGrid';
+import ProjectAppTypeArea from '@cdo/apps/templates/projects/ProjectAppTypeArea.jsx';
+import {projects} from './projectsTestData';
+import {combineReducers, createStore} from 'redux';
+import reducer from '@cdo/apps/templates/projects/projectsRedux';
+
+describe('ProjectCardGrid', () => {
+  const store = createStore(combineReducers({projects: reducer}));
+
+  it('filters by selected app type', () => {
+    const wrapper = shallow(
+      <ProjectCardGrid
+        projectLists={projects}
+        galleryType="public"
+      />, {context: {store}},
+    ).dive();
+
+    // Should show all project types.
+    const instance = wrapper.instance();
+    expect(wrapper).to.have.exactly(7).descendants(ProjectAppTypeArea);
+    expect(wrapper.find(ProjectAppTypeArea).first()).to.have.props({
+      labKey: "playlab",
+      labName: "Stories and Games with Play Lab",
+      numProjectsToShow: 4,
+    });
+
+    // Filter to only show Play Lab projects.
+    instance.onSelectApp('playlab');
+    expect(wrapper.find(ProjectAppTypeArea).length).to.equal(1);
+    expect(wrapper.find(ProjectAppTypeArea)).to.have.props({
+      labKey: "playlab",
+      labName: "All Play Lab Projects",
+      numProjectsToShow: 12,
+    });
+
+    // Show all project types.
+    instance.viewAllProjects();
+    expect(wrapper).to.have.exactly(7).descendants(ProjectAppTypeArea);
+
+    // Filter to only show Minecraft projects.
+    instance.onSelectApp('minecraft');
+    expect(wrapper).to.have.exactly(1).descendants(ProjectAppTypeArea);
+    expect(wrapper.find(ProjectAppTypeArea)).to.have.props({
+      labKey: "minecraft",
+      labName: "All Minecraft Projects",
+      numProjectsToShow: 12,
+    });
+  });
+});
