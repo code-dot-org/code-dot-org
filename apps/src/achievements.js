0df1a416d6dff60aa03bf75db070816a9934deb8
Ram Kandasamy <ram@code.org>
2017-12-13 12:40:17 -0800

    Add unit test

--- apps/src/achievements.js
+++ apps/src/achievements.js
@@ -8,60 +8,64 @@ export default function getAchievements(state) {
 }
 
 const POSSIBLE_ACHIEVEMENTS = [
-  function puzzleComplete(state) {
-    return {
-      isAchieved: true,
-      message: msg.puzzleCompleted(),
-      successIconUrl: '',
-    };
-  },
-
-  function numberOfBlocks(state) {
-    const blockLimit = state.feedback.blockLimit;
-    const blocksUsed = state.feedback.blocksUsed;
-    if (blockLimit === undefined || blockLimit === Infinity) {
-      return null;
-    }
-
-    let message, isAchieved;
-    if (blocksUsed < blockLimit) {
-      message = msg.fewerNumberOfBlocks({numBlocks: blockLimit});
-      isAchieved = true;
-    } else if (blocksUsed === blockLimit) {
-      message = msg.exactNumberOfBlocks({numBlocks: blockLimit});
-      isAchieved = true;
-    } else {
-      message = msg.usingTooManyBlocks();
-      isAchieved = false;
-    }
-
-    return {
-      isAchieved,
-      message,
-      successIconUrl: '',
-      failureIconUrl: '',
-    };
-  },
-
-  function usingHints(state) {
-    const hintsUsed = authoredHintUtils.currentOpenedHintCount(
-      state.pageConstants.serverLevelId);
-    let message, isAchieved;
-    if (hintsUsed === 0) {
-      message = msg.withoutHints();
-      isAchieved = true;
-    } else if (hintsUsed === 1) {
-      message = msg.usingOneHint();
-      isAchieved = true;
-    } else {
-      message = msg.usingHints();
-      isAchieved = false;
-    }
-    return {
-      isAchieved,
-      message,
-      successIconUrl: '',
-      failureIconUrl: '',
-    };
-  },
+  puzzleComplete,
+  numberOfBlocks,
+  usingHints,
 ];
+
+export function puzzleComplete(state) {
+  return {
+    isAchieved: true,
+    message: msg.puzzleCompleted(),
+    successIconUrl: '',
+  };
+}
+
+export function numberOfBlocks(state) {
+  const blockLimit = state.feedback.blockLimit;
+  const blocksUsed = state.feedback.blocksUsed;
+  if (blockLimit === undefined || blockLimit === Infinity) {
+    return null;
+  }
+
+  let message, isAchieved;
+  if (blocksUsed < blockLimit) {
+    message = msg.fewerNumberOfBlocks({numBlocks: blockLimit});
+    isAchieved = true;
+  } else if (blocksUsed === blockLimit) {
+    message = msg.exactNumberOfBlocks({numBlocks: blockLimit});
+    isAchieved = true;
+  } else {
+    message = msg.usingTooManyBlocks();
+    isAchieved = false;
+  }
+
+  return {
+    isAchieved,
+    message,
+    successIconUrl: '',
+    failureIconUrl: '',
+  };
+}
+
+export function usingHints(state) {
+  const hintsUsed = authoredHintUtils.currentOpenedHintCount(
+    state.pageConstants.serverLevelId);
+  let message, isAchieved;
+  if (hintsUsed === 0) {
+    message = msg.withoutHints();
+    isAchieved = true;
+  } else if (hintsUsed === 1) {
+    message = msg.usingOneHint();
+    isAchieved = true;
+  } else {
+    message = msg.usingHints();
+    isAchieved = false;
+  }
+  return {
+    isAchieved,
+    message,
+    successIconUrl: '',
+    failureIconUrl: '',
+  };
+}
