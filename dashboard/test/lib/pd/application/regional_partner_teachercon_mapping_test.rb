20bd01dddad7a564a911ade21c6707b708d3087c
Andrew Oberhardt <Andrew@Code.org>
2017-12-14 13:46:35 -0800

    Add find_teachercon_workshop method and test

--- dashboard/test/lib/pd/application/regional_partner_teachercon_mapping_test.rb
+++ dashboard/test/lib/pd/application/regional_partner_teachercon_mapping_test.rb
@@ -18,5 +18,37 @@ module Pd::Application
         build(:regional_partner, name: 'Mississippi State University')
       )
     end
+
+    test 'find_teachercon_workshop' do
+      tc_csd_phoenix = create :pd_workshop, course: Pd::Workshop::COURSE_CSD,
+        subject: Pd::Workshop::SUBJECT_TEACHER_CON, location_address: 'Some place in Phoenix, AZ',
+        num_sessions: 5, sessions_from: Time.new(2018, 7, 22, 9)
+
+      tc_csp_phoenix = create :pd_workshop, course: Pd::Workshop::COURSE_CSP,
+        subject: Pd::Workshop::SUBJECT_TEACHER_CON, location_address: 'Some place in Phoenix, AZ',
+        num_sessions: 5, sessions_from: Time.new(2018, 7, 22, 9)
+
+      tc_csd_atlanta = create :pd_workshop, course: Pd::Workshop::COURSE_CSD,
+        subject: Pd::Workshop::SUBJECT_TEACHER_CON, location_address: 'Some place in Atlanta, GA',
+        num_sessions: 5, sessions_from: Time.new(2018, 7, 22, 9)
+
+      tc_csp_atlanta = create :pd_workshop, course: Pd::Workshop::COURSE_CSP,
+        subject: Pd::Workshop::SUBJECT_TEACHER_CON, location_address: 'Some place in Atlanta, GA',
+        num_sessions: 5, sessions_from: Time.new(2018, 7, 22, 9)
+
+      # Last year, same city
+      create :pd_workshop, course: Pd::Workshop::COURSE_CSD,
+        subject: Pd::Workshop::SUBJECT_TEACHER_CON, location_address: 'Some place in Phoenix, AZ',
+        num_sessions: 5, sessions_from: Time.new(2017, 7, 22, 9)
+
+      create :pd_workshop, course: Pd::Workshop::COURSE_CSP,
+        subject: Pd::Workshop::SUBJECT_TEACHER_CON, location_address: 'Some place in Phoenix, AZ',
+        num_sessions: 5, sessions_from: Time.new(2017, 7, 22, 9)
+
+      assert_equal tc_csd_phoenix, find_teachercon_workshop(course: Pd::Workshop::COURSE_CSD, city: 'Phoenix')
+      assert_equal tc_csp_phoenix, find_teachercon_workshop(course: Pd::Workshop::COURSE_CSP, city: 'Phoenix')
+      assert_equal tc_csd_atlanta, find_teachercon_workshop(course: Pd::Workshop::COURSE_CSD, city: 'Atlanta')
+      assert_equal tc_csp_atlanta, find_teachercon_workshop(course: Pd::Workshop::COURSE_CSP, city: 'Atlanta')
+    end
   end
 end
