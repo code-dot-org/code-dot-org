5fce6e2450546b7ae7531d3938bf05ceb0faac09
Erin <erin@code.org>
2017-12-15 15:44:11 -0800

    Edge group collision tests

--- apps/test/integration/levelSolutions/gamelab/group_collision_blocks.js
+++ apps/test/integration/levelSolutions/gamelab/group_collision_blocks.js
@@ -106,5 +106,81 @@ module.exports = {
         assert.include(debugOutput.textContent, 'collided');
       }
     ),
+    testAsyncProgramGameLab(
+      'Not overlapping edge group and sprite collision not detected',
+      `
+        var sprite = createSprite(100, 100);
+        createEdgeSprites();
+        if(sprite.collide(edges)){
+          console.log('collided');
+        }
+        console.log('done');
+      `,
+      function isProgramDone(){
+        var debugOutput = document.getElementById('debug-output');
+        return debugOutput.textContent.includes('done');
+      },
+      function validateResult(assert) {
+        var debugOutput = document.getElementById('debug-output');
+        assert.notInclude(debugOutput.textContent, 'collided');
+      }
+    ),
+    testAsyncProgramGameLab(
+      'Overlapping edge group and sprite collision detected',
+      `
+        var sprite = createSprite(0, 0);
+        createEdgeSprites();
+        if(sprite.collide(edges)){
+          console.log('collided');
+        }
+        console.log('done');
+      `,
+      function isProgramDone(){
+        var debugOutput = document.getElementById('debug-output');
+        return debugOutput.textContent.includes('done');
+      },
+      function validateResult(assert) {
+        var debugOutput = document.getElementById('debug-output');
+        assert.include(debugOutput.textContent, 'collided');
+      }
+    ),
+    testAsyncProgramGameLab(
+      'Not overlapping edge and sprite collision not detected',
+      `
+        var sprite = createSprite(100, 100);
+        createEdgeSprites();
+        if(sprite.collide(leftEdge)){
+          console.log('collided');
+        }
+        console.log('done');
+      `,
+      function isProgramDone(){
+        var debugOutput = document.getElementById('debug-output');
+        return debugOutput.textContent.includes('done');
+      },
+      function validateResult(assert) {
+        var debugOutput = document.getElementById('debug-output');
+        assert.notInclude(debugOutput.textContent, 'collided');
+      }
+    ),
+    testAsyncProgramGameLab(
+      'Overlapping edge and sprite collision detected',
+      `
+        var sprite = createSprite(0, 0);
+        createEdgeSprites();
+        if(sprite.collide(leftEdge)){
+          console.log('collided');
+        }
+        console.log('done');
+      `,
+      function isProgramDone(){
+        var debugOutput = document.getElementById('debug-output');
+        return debugOutput.textContent.includes('done');
+      },
+      function validateResult(assert) {
+        var debugOutput = document.getElementById('debug-output');
+        assert.include(debugOutput.textContent, 'collided');
+      }
+    ),
   ]
 };
