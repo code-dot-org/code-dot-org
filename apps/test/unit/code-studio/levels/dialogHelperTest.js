426554af54cb382f76f25ba60a4fc3a794c2dd77
Brent Van Minnen <bjvanminnen@gmail.com>
2018-01-02 17:23:12 -0800

    tests

--- apps/test/unit/code-studio/levels/dialogHelperTest.js
+++ apps/test/unit/code-studio/levels/dialogHelperTest.js
@@ -3,7 +3,7 @@ import React from 'react';
 import 'jquery-ui/ui/effects/effect-drop';
 import $ from 'jquery';
 import sinon from 'sinon';
-import { showDialog } from '@cdo/apps/code-studio/levels/dialogHelper';
+import { showDialog, getSuccessDialog } from '@cdo/apps/code-studio/levels/dialogHelper';
 
 describe('dialogHelper', () => {
   let stashedWindowJquery;
@@ -88,6 +88,76 @@ describe('dialogHelper', () => {
       showDialog(<MyComponent/>, null, onHidden);
       $('#cancel-button').click();
       assert(onHidden.calledOnce);
+    });
+  });
+
+  describe('getSuccessDialog', () => {
+    const fakeAppOptions = (app, levelOptions) => ({
+      level: {
+        submittable: false,
+        answers: [
+          {
+            "text": "I feel like I could teach this right now",
+            "correct": true
+          },
+          {
+            "text": "I feel like I need to do SOME review of lessons, content, etc before I teach this",
+            "correct": true
+          },
+          {
+            "text": "I feel like I need to do A LOT of review of lessons, content, etc before I teach this",
+            "correct": true
+          }
+        ],
+        ...levelOptions,
+      },
+      dialog: {
+        app
+      }
+    });
+
+    it('has the right title/body when using success_title/success_body', () => {
+      const appOptions = fakeAppOptions('multi', {
+        options: {
+          success_title: 'Customized success title',
+          success_body: 'Customized success body'
+        }
+      });
+      const dialog = getSuccessDialog(appOptions);
+      assert.equal(dialog.props.title, 'Customized success title');
+      assert.equal(dialog.props.body, 'Customized success body');
+    });
+
+    it('has the right title/body for a standard multi', () => {
+      const appOptions = fakeAppOptions('multi');
+      const dialog = getSuccessDialog(appOptions);
+      assert.equal(dialog.props.title, 'Correct');
+      assert.equal(dialog.props.body, 'That is the correct answer.');
+    });
+
+    it('has the right title/body for a submittable multi', () => {
+      // I can only find on submittable multi, and it's in allthethings, so this
+      // probably could be cut if we wanted to simplify.
+      // Also, this tests accurately reflects the fact that submittable is the
+      // string "true" rather than boolean true here.
+      const appOptions = fakeAppOptions('multi', {
+        submittable: "true",
+        answers: undefined,
+      });
+      const dialog = getSuccessDialog(appOptions);
+      assert.equal(dialog.props.title, 'Thank you');
+      assert.equal(dialog.props.body, 'Thank you for submitting an answer.');
+    });
+
+    it('has the right title/body for a text_match without answers', () => {
+      // Worth noting that I only see 3 levels that meet this criteria and dont
+      // have skip_dialog: true
+      const appOptions = fakeAppOptions('text_match', {
+        answers: undefined,
+      });
+      const dialog = getSuccessDialog(appOptions);
+      assert.equal(dialog.props.title, 'Thank you');
+      assert.equal(dialog.props.body, 'Thanks for your response!');
     });
   });
 });
