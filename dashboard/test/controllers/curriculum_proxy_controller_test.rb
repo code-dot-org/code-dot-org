640b34f349b1f8b2f9513f92b4150bc02a23a80e
Brent Van Minnen <bjvanminnen@gmail.com>
2018-01-04 10:29:57 -0800

    proxy studio.code.org/curriculum to curriculum.code.org

--- dashboard/test/controllers/curriculum_proxy_controller_test.rb
+++ dashboard/test/controllers/curriculum_proxy_controller_test.rb
@@ -3,7 +3,7 @@ WebMock.disable_net_connect!(allow_localhost: true)
 require_relative '../../../shared/test/spy_newrelic_agent'
 require 'test_helper'
 
-class DocsProxyControllerTest < ActionController::TestCase
+class CurriculumProxyControllerTest < ActionController::TestCase
   test "should redirect from studio.code.org/docs path to docs.code.org path" do
     stub_request(:get, "https://docs.code.org/csd/maker_leds/index.html").
       to_return(body: 'docs.code.org content', headers: {})
@@ -12,5 +12,15 @@ class DocsProxyControllerTest < ActionController::TestCase
     get :get, params: {docs_route: 'csd/maker_leds/index.html'}
     assert_response :success
     assert_equal response.body, 'docs.code.org content'
+  end
+
+  test "should redirect from studio.code.org/curriculum path to curriculum.code.org path" do
+    stub_request(:get, "https://curriculum.code.org/csd/maker_leds/index.html").
+      to_return(body: 'curriculum.code.org content', headers: {})
+
+    request.host = "studio.code.org"
+    get :get, params: {curriculum_route: 'csd/maker_leds/index.html'}
+    assert_response :success
+    assert_equal response.body, 'curriculum.code.org content'
   end
 end
