f23b076aaf7fab59f5851e08438a0ecf07a47a17
Brent Van Minnen <bjvanminnen@gmail.com>
2018-01-05 16:26:53 -0800

    separate actions

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -60,8 +60,8 @@ Dashboard::Application.routes.draw do
 
   get 'redirected_url', to: 'redirect_proxy#get', format: false
 
-  get 'docs/*docs_path', to: 'curriculum_proxy#get'
-  get 'curriculum/*curriculum_path', to: 'curriculum_proxy#get'
+  get 'docs/*path', to: 'curriculum_proxy#get_doc'
+  get 'curriculum/*path', to: 'curriculum_proxy#get_curriculum'
 
   # User-facing section routes
   resources :sections, only: [:show, :update] do
