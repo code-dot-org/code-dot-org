7bae3f7fea12cecb67bde53364738dbe06ce93e3
Brad Buchanan <brad@code.org>
2018-01-08 14:04:13 -0800

    Extract Survey/Support section to own component

--- apps/src/lib/kits/maker/ui/SetupChecklist.jsx
+++ apps/src/lib/kits/maker/ui/SetupChecklist.jsx
@@ -250,19 +250,7 @@ export default class SetupChecklist extends Component {
               {surveyLink}
             </ValidationStep>
           </div>
-          <h2>Survey / Support</h2>
-          <div>
-            <p>Did it work? Having trouble?</p>
-
-            <a
-              href={this.getSurveyURL()}
-              style={{'fontSize': '20px', marginBottom: 14, marginTop: 12, display: 'block'}}
-            >
-              Submit our quick survey&nbsp;
-              <i className="fa fa-arrow-circle-o-right" />
-            </a>
-            <p>Results of setup status detection and browser/platform information will be pre-filled in the survey through the link above.</p>
-          </div>
+          <SurveySupportSection surveyUrl={this.getSurveyURL()}/>
         </div>
     );
   }
@@ -272,4 +260,30 @@ function promiseWaitFor(ms) {
   return new Promise(resolve => {
     setTimeout(resolve, ms);
   });
+}
+
+class SurveySupportSection extends React.Component {
+  static propTypes = {
+    surveyUrl: PropTypes.string.isRequired,
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Survey / Support</h2>
+        <div>
+          <p>Did it work? Having trouble?</p>
+
+          <a
+            href={this.props.surveyUrl}
+            style={{'fontSize': '20px', marginBottom: 14, marginTop: 12, display: 'block'}}
+          >
+            Submit our quick survey&nbsp;
+            <i className="fa fa-arrow-circle-o-right" />
+          </a>
+          <p>Results of setup status detection and browser/platform information will be pre-filled in the survey through the link above.</p>
+        </div>
+      </div>
+    );
+  }
 }
