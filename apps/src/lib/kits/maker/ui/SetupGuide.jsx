d3d1da6145fa0246b7e542c42b5e9f3d4edea085
Brad Buchanan <brad@code.org>
2018-01-08 14:45:02 -0800

    Scaffold different behaviors for different platforms

--- apps/src/lib/kits/maker/ui/SetupGuide.jsx
+++ apps/src/lib/kits/maker/ui/SetupGuide.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SetupChecklist from "./SetupChecklist";
 import SetupChecker from '../util/SetupChecker';
+import {isCodeOrgBrowser, isChromeOS, isOSX, isWindows} from '../util/browserChecks';
 
 export default class SetupGuide extends React.Component {
   constructor(props) {
@@ -9,8 +10,15 @@ export default class SetupGuide extends React.Component {
   }
 
   render() {
-    return (
-      <SetupChecklist setupChecker={this.setupChecker}/>
-    );
+    if (isCodeOrgBrowser() || isChromeOS()) {
+      return <SetupChecklist setupChecker={this.setupChecker}/>;
+    } else if (isOSX() || isWindows()) {
+      // TODO: Download view
+      // Link to download/install Maker Browser, and link to support resources.
+      return <div/>;
+    }
+    // TODO: Unsupported browser view
+    // List of supported platforms and link to support resources.
+    return <div/>;
   }
 }
