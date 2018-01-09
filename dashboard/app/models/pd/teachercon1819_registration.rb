d52622bcd1c8cf8246531ebbcb3c564d90d7f99f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-01-09 14:26:50 -0800

    Add the rest of the content for teachercon registration form

--- dashboard/app/models/pd/teachercon1819_registration.rb
+++ dashboard/app/models/pd/teachercon1819_registration.rb
@@ -18,6 +18,7 @@ class Pd::Teachercon1819Registration < ActiveRecord::Base
 
   YES = 'Yes'.freeze
   NO = 'No'.freeze
+  YES_OR_NO = [YES, NO].freeze
   OTHER = 'Other'.freeze
 
   def self.options
@@ -30,7 +31,7 @@ class Pd::Teachercon1819Registration < ActiveRecord::Base
         'Gluten Free',
         'Food Allergy',
       ],
-      liveFarAway: [YES, NO],
+      liveFarAway: YES_OR_NO,
       addressState: get_all_states_with_dc.to_h.values,
       howTraveling: [
         'I will drive by myself',
@@ -39,14 +40,16 @@ class Pd::Teachercon1819Registration < ActiveRecord::Base
         'Amtrak or regional train service',
         'Public transit (e.g., city bus or light rail)',
       ],
-      needHotel: [YES, NO],
+      needHotel: YES_OR_NO,
       needAda: [YES, NO, 'Other (please explain):'],
       teacherAcceptSeat: [
         "Yes, I accept my seat in the Professional Learning Program",
         "Yes, I want to participate, but I'm unable to attend my assigned summer workshop date. Please place me on your waitlist. I understand that I am not guaranteed a space in a different summer workshop.",
         "Yes, I want to participate, but I'm not able to for a different reason. Please place me on your waitlist. I understand that I am not guaranteed a space in a different summer workshop.",
         "No, I decline my seat in the Professional Learning Program.",
-      ]
+      ],
+      photoRelease: YES_OR_NO,
+      liabilityWaiver: YES_OR_NO,
     }.freeze
   end
 
