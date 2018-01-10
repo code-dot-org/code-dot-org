5d874c74b3beaf41419e63e8592ec4485ae1f0f9
Ram Kandasamy <ram@code.org>
2018-01-10 15:40:12 -0800

    Generate constants once [test all]

--- apps/test-low-memory.sh
+++ apps/test-low-memory.sh
@@ -4,22 +4,23 @@
 # We break them up here so they each run in isolation.
 
 #if [ -n "$CIRCLECI" ]; then
-  SHELL=/bin/bash parallel ::: "npm run lint" \
-  "node --max_old_space_size=4096 `npm bin`/grunt unitTest && \
-  echo bash <(curl -s https://codecov.io/bash) -cF unit" \
-  "echo hi"\
-  "node --max_old_space_size=4096 `npm bin`/grunt storybookTest && \
-  echo bash <(curl -s https://codecov.io/bash) -cF storybook" \
-  "node --max_old_space_size=4096 `npm bin`/grunt scratchTest && \
-  echo bash <(curl -s https://codecov.io/bash) -cF scratch" \
-  "LEVEL_TYPE='turtle' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-  echo bash <(curl -s https://codecov.io/bash) -cF integration" \
-  "LEVEL_TYPE='maze|bounce|calc|eval|flappy|studio' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-  echo bash <(curl -s https://codecov.io/bash) -cF integration" \
-  "LEVEL_TYPE='applab|gamelab' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-  echo bash <(curl -s https://codecov.io/bash) -cF integration" \
-  "LEVEL_TYPE='craft' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-  echo bash <(curl -s https://codecov.io/bash) -cF integration"
+node --max_old_space_size=4096 `npm bin`/grunt preconcat concat && \
+SHELL=/bin/bash parallel -j 4 ::: "npm run lint" \
+"node --max_old_space_size=4096 `npm bin`/grunt unitTest && \
+echo bash <(curl -s https://codecov.io/bash) -cF unit" \
+"echo hi"\
+"node --max_old_space_size=4096 `npm bin`/grunt storybookTest && \
+echo bash <(curl -s https://codecov.io/bash) -cF storybook" \
+"node --max_old_space_size=4096 `npm bin`/grunt scratchTest && \
+echo bash <(curl -s https://codecov.io/bash) -cF scratch" \
+"LEVEL_TYPE='turtle' node --max_old_space_size=4096 `npm bin`/grunt karma:integration && \
+echo bash <(curl -s https://codecov.io/bash) -cF integration" \
+"LEVEL_TYPE='maze|bounce|calc|eval|flappy|studio' node --max_old_space_size=4096 `npm bin`/grunt karma:integration && \
+echo bash <(curl -s https://codecov.io/bash) -cF integration" \
+"LEVEL_TYPE='applab|gamelab' node --max_old_space_size=4096 `npm bin`/grunt karma:integration && \
+echo bash <(curl -s https://codecov.io/bash) -cF integration" \
+"LEVEL_TYPE='craft' node --max_old_space_size=4096 `npm bin`/grunt karma:integration && \
+echo bash <(curl -s https://codecov.io/bash) -cF integration"
 #else
 #  npm run lint && \
 #
