eefd541eb55884ef556298ae8c23226f5834546f
Ram Kandasamy <ram@code.org>
2018-01-11 16:39:37 -0800

    A bit of test-low-memory.sh cleanup
    [skip ui]

--- apps/test-low-memory.sh
+++ apps/test-low-memory.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
+set -e
 
 # 'npm test' normally does all three of these things.
 # We break them up here so they each run in isolation.
 
+alias grunt="node --max_old_space_size=4096 `npm bin`/grunt"
+
 if [ -n "$CIRCLECI" ]; then
   mkdir -p log
-  echo "Logging output to apps/log/ directory, see apps-test-log under the artifacts tab on circle"
-  node --max_old_space_size=4096 `npm bin`/grunt preconcat concat && \
+  echo "See apps-test-log under the artifacts tab on circle for test output"
+
+  curl -s https://codecov.io/bash > codecov.sh
+  chmod +x codecov.sh
+
+  grunt preconcat concat
+
   SHELL=/bin/bash parallel -j 4 --joblog - ::: "npm run lint" \
-  "(node --max_old_space_size=4096 `npm bin`/grunt unitTest && \
-  bash <(curl -s https://codecov.io/bash) -cF unit) > log/unitTest.log" \
-  "(node --max_old_space_size=4096 `npm bin`/grunt storybookTest && \
-  bash <(curl -s https://codecov.io/bash) -cF storybook) > log/storybookTest.log" \
-  "(node --max_old_space_size=4096 `npm bin`/grunt scratchTest && \
-  bash <(curl -s https://codecov.io/bash) -cF scratch) > log/scratchTest.log" \
-  "(LEVEL_TYPE='turtle' node --max_old_space_size=4096 `npm bin`/grunt karma:integration && \
-  bash <(curl -s https://codecov.io/bash) -cF integration) > log/turtleTest.log" \
-  "(LEVEL_TYPE='maze|bounce|calc|eval|flappy|studio' node --max_old_space_size=4096 `npm bin`/grunt karma:integration && \
-  bash <(curl -s https://codecov.io/bash) -cF integration) > log/integrationTest.log" \
-  "(LEVEL_TYPE='applab|gamelab' node --max_old_space_size=4096 `npm bin`/grunt karma:integration && \
-  bash <(curl -s https://codecov.io/bash) -cF integration) > log/appLabgameLabTest.log" \
-  "(LEVEL_TYPE='craft' node --max_old_space_size=4096 `npm bin`/grunt karma:integration && \
-  bash <(curl -s https://codecov.io/bash) -cF integration) > log/craftTest.log"
+  "(grunt unitTest && ./codecov.sh -cF unit) > log/unitTest.log" \
+  "(grunt storybookTest && ./codecov.sh -cF storybook) > log/storybookTest.log" \
+  "(grunt scratchTest && ./codecov.sh -cF scratch) > log/scratchTest.log" \
+  "(LEVEL_TYPE='turtle' grunt karma:integration && \
+    ./codecov.sh -cF integration) > log/turtleTest.log" \
+  "(LEVEL_TYPE='maze|bounce|calc|eval|flappy|studio' grunt karma:integration && \
+    ./codecov.sh -cF integration) > log/integrationTest.log" \
+  "(LEVEL_TYPE='applab|gamelab' grunt karma:integration && \
+    ./codecov.sh -cF integration) > log/appLabgameLabTest.log" \
+  "(LEVEL_TYPE='craft' grunt karma:integration && \
+    ./codecov.sh -cF integration) > log/craftTest.log"
 else
-  npm run lint && \
-  node --max_old_space_size=4096 `npm bin`/grunt unitTest && \
-  node --max_old_space_size=4096 `npm bin`/grunt storybookTest && \
-  node --max_old_space_size=4096 `npm bin`/grunt scratchTest && \
-  LEVEL_TYPE='turtle' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-  LEVEL_TYPE='maze|bounce|calc|eval|flappy|studio' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-  LEVEL_TYPE='applab|gamelab' node --max_old_space_size=4096 `npm bin`/grunt integrationTest && \
-  LEVEL_TYPE='craft' node --max_old_space_size=4096 `npm bin`/grunt integrationTest
+  npm run lint
+  grunt unitTest
+  grunt storybookTest
+  grunt scratchTest
+  LEVEL_TYPE='turtle' grunt integrationTest
+  LEVEL_TYPE='maze|bounce|calc|eval|flappy|studio' grunt integrationTest
+  LEVEL_TYPE='applab|gamelab' grunt integrationTest
+  LEVEL_TYPE='craft' grunt integrationTest
 fi
