df530daf433709edec192246292596d133f7319e
Clare Constantine <clare@code.org>
2018-01-11 18:04:24 -0800

    Add 'other' field to currently teaching and teaching next year questions

--- apps/src/code-studio/pd/application/teacher1819/Section2YourSchool.jsx
+++ apps/src/code-studio/pd/application/teacher1819/Section2YourSchool.jsx
@@ -18,7 +18,9 @@ export default class Section2YourSchool extends LabeledFormComponent {
     ...Object.keys(PageLabels.section2YourSchool),
     "currentRole_other",
     "gradesTeaching_notTeachingExplanation",
+    "gradesTeaching_other",
     "gradesExpectToTeach_notExpectingToTeachExplanation",
+    "gradesExpectToTeach_other",
     "subjectsTeaching_other",
     "subjectsExpectToTeach_other",
     "subjectsLicensedToTeach_other",
@@ -105,11 +107,13 @@ export default class Section2YourSchool extends LabeledFormComponent {
         {this.checkBoxesFor("gradesAtSchool")}
 
         {this.checkBoxesWithAdditionalTextFieldsFor("gradesTeaching", {
-          "I'm not teaching this year (please explain):" : "notTeachingExplanation"
+          "I'm not teaching this year (Please Explain):" : "notTeachingExplanation",
+          "Other (Please Explain):" : "other"
         })}
 
         {this.checkBoxesWithAdditionalTextFieldsFor("gradesExpectToTeach", {
-          "I'm not teaching next year (please explain):" : "notExpectingToTeachExplanation"
+          "I'm not teaching next year (Please Explain):" : "notExpectingToTeachExplanation",
+          "Other (Please Explain):" : "other"
         })}
 
         {this.checkBoxesWithAdditionalTextFieldsFor("subjectsTeaching", {
