76821b5c1ce226119ba32d38ec16505c82fba3c4
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-01-15 13:52:43 -0800

    update tests to use traits instead of transient arguments for constructing teachercon registration variants

--- dashboard/test/models/pd/teachercon1819_registration_test.rb
+++ dashboard/test/models/pd/teachercon1819_registration_test.rb
@@ -36,30 +36,24 @@ class Pd::Teachercon1819RegistrationTest < ActiveSupport::TestCase
   end
 
   test 'declined application requires fewer fields' do
-    registration = create(:pd_teachercon1819_registration, accepted: false)
+    registration = create(:pd_teachercon1819_registration, hash_trait: :withdrawn)
 
     assert registration.valid?
     refute registration.sanitize_form_data_hash.key?(:contact_first_name)
     refute registration.sanitize_form_data_hash.key?(:dietary_needs)
   end
 
-  test 'withdrawing or declining in the registration will also update the application' do
-    {
-      Pd::Teachercon1819Registration::TEACHER_SEAT_ACCEPTANCE_OPTIONS[:accept] => "accepted",
-      Pd::Teachercon1819Registration::TEACHER_SEAT_ACCEPTANCE_OPTIONS[:withdraw_date] => "waitlisted",
-      Pd::Teachercon1819Registration::TEACHER_SEAT_ACCEPTANCE_OPTIONS[:withdraw_other] => "waitlisted",
-      Pd::Teachercon1819Registration::TEACHER_SEAT_ACCEPTANCE_OPTIONS[:decline] => "declined"
-    }.each do |accept_seat, status|
+  test 'waitlisting or withdrawing in the registration will also update the application' do
+    %w(
+      accepted
+      waitlisted
+      withdrawn
+    ).each do |status|
       application = create(:pd_teacher1819_application)
       application.status = "accepted"
       application.lock!
 
-      Pd::Teachercon1819Registration.create(
-        pd_application: application,
-        form_data: build(:pd_teachercon1819_registration_hash).merge(
-          teacher_accept_seat: accept_seat
-        ).to_json
-      )
+      create(:pd_teachercon1819_registration, pd_application: application, hash_trait: status.to_sym)
 
       assert_equal application.reload.status, status
     end
