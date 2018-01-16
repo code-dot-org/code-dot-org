b86740cd8024eddf2acb027491517af3e49747de
Ram Kandasamy <ram@code.org>
2018-01-16 12:58:09 -0800

    Cache codecov script in /tmp/

--- apps/test-low-memory.sh
+++ apps/test-low-memory.sh
@@ -10,23 +10,23 @@ if [ -n "$CIRCLECI" ]; then
   mkdir -p log
   echo "See apps-test-log under the artifacts tab on circle for test output"
 
-  curl -s https://codecov.io/bash > codecov.sh
-  chmod +x codecov.sh
+  curl -s https://codecov.io/bash > /tmp/codecov.sh
+  chmod +x /tmp/codecov.sh
 
   $GRUNT_CMD preconcat concat
 
   SHELL=/bin/bash parallel -j 4 --joblog - ::: "npm run lint" \
-  "(PORT=9876 $GRUNT_CMD unitTest && ./codecov.sh -cF unit) > log/unitTest.log" \
-  "(PORT=9877 $GRUNT_CMD storybookTest && ./codecov.sh -cF storybook) > log/storybookTest.log" \
-  "(PORT=9878 $GRUNT_CMD scratchTest && ./codecov.sh -cF scratch) > log/scratchTest.log" \
+  "(PORT=9876 $GRUNT_CMD unitTest && /tmp/codecov.sh -cF unit) > log/unitTest.log" \
+  "(PORT=9877 $GRUNT_CMD storybookTest && /tmp/codecov.sh -cF storybook) > log/storybookTest.log" \
+  "(PORT=9878 $GRUNT_CMD scratchTest && /tmp/codecov.sh -cF scratch) > log/scratchTest.log" \
   "(PORT=9879 LEVEL_TYPE='turtle' $GRUNT_CMD karma:integration && \
-    ./codecov.sh -cF integration) > log/turtleTest.log" \
+    /tmp/codecov.sh -cF integration) > log/turtleTest.log" \
   "(PORT=9880 LEVEL_TYPE='maze|bounce|calc|eval|flappy|studio' $GRUNT_CMD karma:integration && \
-    ./codecov.sh -cF integration) > log/integrationTest.log" \
+    /tmp/codecov.sh -cF integration) > log/integrationTest.log" \
   "(PORT=9881 LEVEL_TYPE='applab|gamelab' $GRUNT_CMD karma:integration && \
-    ./codecov.sh -cF integration) > log/appLabgameLabTest.log" \
+    /tmp/codecov.sh -cF integration) > log/appLabgameLabTest.log" \
   "(PORT=9882 LEVEL_TYPE='craft' $GRUNT_CMD karma:integration && \
-    ./codecov.sh -cF integration) > log/craftTest.log"
+    /tmp/codecov.sh -cF integration) > log/craftTest.log"
 else
   npm run lint
   $GRUNT_CMD unitTest
