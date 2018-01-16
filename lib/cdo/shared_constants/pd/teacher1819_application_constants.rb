e29dce2f58ab245d50b959f914852fcd8a960de4
Clare Constantine <clare@code.org>
2018-01-16 14:00:07 -0800

    add autoscoring for 'able to attend multiple ws' q

--- lib/cdo/shared_constants/pd/teacher1819_application_constants.rb
+++ lib/cdo/shared_constants/pd/teacher1819_application_constants.rb
@@ -186,6 +186,7 @@ module Teacher1819ApplicationConstants
     previous_yearlong_cdo_pd: YES_NO,
     committed: YES_NO,
     able_to_attend_single: YES_NO,
+    able_to_attend_multiple: YES_NO,
     csp_which_grades: YES_NO,
     csp_course_hours_per_year: YES_NO,
     csd_which_grades: YES_NO,
