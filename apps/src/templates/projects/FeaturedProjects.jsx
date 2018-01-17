0452eea92df26b64a96d2f6d868b573d6e190e07
Erin Bond <erin.bond@code.org>
2018-01-17 12:10:06 -0800

    new route and placeholder page for featured projects

---
+++ apps/src/templates/projects/FeaturedProjects.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class FeaturedProjects extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>
+          This is the future home of a handy table that shows which
+          projects are currently featured; will be visible only to
+          those with project_validator permissions.
+        </h3>
+      </div>
+    );
+  }
+}
