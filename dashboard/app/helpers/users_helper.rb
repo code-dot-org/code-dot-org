3a2a993a5fb1ff09b52c709ea815f6e7ce015238
Brent Van Minnen <bjvanminnen@gmail.com>
2018-01-18 10:08:29 -0800

    update comment [ci skip]

--- dashboard/app/helpers/users_helper.rb
+++ dashboard/app/helpers/users_helper.rb
@@ -91,7 +91,11 @@ module UsersHelper
     user_data
   end
 
-  # Merge the progress for the specified script and user into the user_data result hash.
+  # Get the progress for the specified script and user
+  # @param {User} user
+  # @param {Script} script
+  # @param {boolean} exclude_level_progress if true, script progress will not include
+  #   level progress (just some summary info)
   private def get_script_progress(user, script, exclude_level_progress = false)
     user_data = {}
     return user_data unless user
