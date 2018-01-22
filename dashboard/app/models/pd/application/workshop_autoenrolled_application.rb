1c1a70d44d22ada399e607155de728d88a2523e5
David Bailey <davidsbailey@users.noreply.github.com>
2018-01-22 12:58:41 -0800

    revert bogus dashboard model changes [ci skip]

--- dashboard/app/models/pd/application/workshop_autoenrolled_application.rb
+++ dashboard/app/models/pd/application/workshop_autoenrolled_application.rb
@@ -1,38 +1,3 @@
-# == Schema Information
-#
-# Table name: pd_applications
-#
-#  id                                  :integer          not null, primary key
-#  user_id                             :integer
-#  type                                :string(255)      not null
-#  application_year                    :string(255)      not null
-#  application_type                    :string(255)      not null
-#  regional_partner_id                 :integer
-#  status                              :string(255)
-#  locked_at                           :datetime
-#  notes                               :text(65535)
-#  form_data                           :text(65535)      not null
-#  created_at                          :datetime         not null
-#  updated_at                          :datetime         not null
-#  course                              :string(255)
-#  response_scores                     :text(65535)
-#  application_guid                    :string(255)
-#  decision_notification_email_sent_at :datetime
-#  accepted_at                         :datetime
-#  properties                          :text(65535)
-#
-# Indexes
-#
-#  index_pd_applications_on_application_guid     (application_guid)
-#  index_pd_applications_on_application_type     (application_type)
-#  index_pd_applications_on_application_year     (application_year)
-#  index_pd_applications_on_course               (course)
-#  index_pd_applications_on_regional_partner_id  (regional_partner_id)
-#  index_pd_applications_on_status               (status)
-#  index_pd_applications_on_type                 (type)
-#  index_pd_applications_on_user_id              (user_id)
-#
-
 # This subclass exists to allow Applications to be associated with a Workshop
 # and to automatically enroll the associated user in the workshop upon
 # acceptance
