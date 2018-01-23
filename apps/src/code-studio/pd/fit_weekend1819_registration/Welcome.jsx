9d319b5e4c593c264168241bc517e58725b876d2
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-01-23 15:27:14 -0800

    add initial apps content for FiT Registration form

---
+++ apps/src/code-studio/pd/fit_weekend1819_registration/Welcome.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {FormGroup} from 'react-bootstrap';
+import LabeledFormComponent from '../form_components/LabeledFormComponent';
+
+export default class Welcome extends LabeledFormComponent {
+  static associatedFields = [];
+
+  render() {
+    return (
+      <FormGroup>
+        <p>
+          Congratulations on your acceptance to Code.org’s Facilitator Development
+          Program for {this.props.course}! Please complete this registration
+          form <strong>within two weeks</strong>, and
+          contact <a href="mailto:facilitators@code.org">facilitators@code.org</a>
+          with any questions.
+        </p>
+      </FormGroup>
+    );
+  }
+}
