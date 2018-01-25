777f06a0d707c728930cba0bc39fcfbd4cbe1077
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-01-25 13:14:06 -0800

    fix some copy formatting

--- apps/src/code-studio/pd/fit_weekend1819_registration/Confirmation.jsx
+++ apps/src/code-studio/pd/fit_weekend1819_registration/Confirmation.jsx
@@ -13,7 +13,7 @@ export default class Confirmation extends LabeledFormComponent {
     return (
       <FormGroup>
         <p>
-          We're excited you’re planning to join us this summer! You will receive
+          We're excited you're planning to join us this summer! You will receive
           more information about travel approximately six weeks before the FiT
           Weekend. In the meantime, please <strong>do not</strong> book your flight,
           and make sure to contact <a href="mailto:facilitators@code.org">facilitators@code.org</a> with any questions. We look
