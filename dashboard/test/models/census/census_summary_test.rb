38ebabfe619e8155e646050856cf57310428086c
Drew Samnick <drew.samnick@code.org>
2018-01-29 15:42:46 -0800

    Create census_summaries table

---
+++ dashboard/test/models/census/census_summary_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class Census::CensusSummaryTest < ActiveSupport::TestCase
+  test "Basic summary creation succeeds" do
+    summary = build :census_summary
+    assert summary.valid?, summary.errors.full_messages
+  end
+
+  test "Summary creation with valid teaches_cs succeeds" do
+    summary = build :census_summary, :with_valid_teaches_cs
+    assert summary.valid?, summary.errors.full_messages
+  end
+
+  test "Summary creation with invalid teaches_cs fails" do
+    assert_raises ArgumentError do
+      build :census_summary, :with_invalid_teaches_cs
+    end
+  end
+
+  test "Summary creation without school year fails" do
+    summary = build :census_summary, :without_school_year
+    refute summary.valid?
+  end
+
+  test "Summary creation with invalid school_year fails" do
+    summary = build :census_summary, :with_invalid_school_year
+    refute summary.valid?
+  end
+
+  test "Summary creation without school fails" do
+    summary = build :census_summary, :without_school
+    refute summary.valid?
+  end
+
+  test "Summary creation without audit data fails" do
+    summary = build :census_summary, :without_audit_data
+    refute summary.valid?
+  end
+end
