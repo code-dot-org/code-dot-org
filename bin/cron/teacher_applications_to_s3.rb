fcd79d4dbe1a769fe86da3776a6d978d33226e18
bencodeorg <ben@code.org>
2018-01-29 16:45:00 -0800

    Fix rubocop violations

--- bin/cron/teacher_applications_to_s3.rb
+++ bin/cron/teacher_applications_to_s3.rb
@@ -4,14 +4,14 @@ require 'cdo/aws/s3'
 require 'csv'
 
 def main
-  applications = Pd::Application::Teacher1819Application.joins(:regional_partner).pluck(:course,:name,:locked_at,:status)
+  applications = Pd::Application::Teacher1819Application.joins(:regional_partner).pluck(:course, :name, :locked_at, :status)
 
   output = CSV.generate(col_sep: "\t") do |csv|
     csv << %w(course name locked_at status)
     applications.each {|row| csv << row}
   end
 
-  AWS::S3.upload_to_bucket('cdo-data-sharing-internal', 'pd_applications.tsv', output, no_random: true) 
+  AWS::S3.upload_to_bucket('cdo-data-sharing-internal', 'pd_applications.tsv', output, no_random: true)
 end
 
 main
