b07e07cc409d163d6396a3768ad51707687e3d43
Brad Buchanan <brad@code.org>
2018-01-31 14:33:20 -0800

    Test non-debounced getOptions and comments

--- apps/src/templates/SchoolAutocompleteDropdown.jsx
+++ apps/src/templates/SchoolAutocompleteDropdown.jsx
@@ -28,8 +28,20 @@ export default class SchoolAutocompleteDropdown extends Component {
     label: i18n.schoolNotFound()
   });
 
+  /**
+   * Debounced function that will request school search results from the server.
+   * Because this function is debounced it is not guaranteed to execute
+   * when it is called - there may be a delay of up to 200ms.
+   * @param {string} q - Search query
+   * @param {function(err, result)} callback - Function called when the server
+   *   returns results or a request error occurs.
+   */
   debouncedSearch = _.debounce((q, callback) => {
     const searchUrl = `/dashboardapi/v1/schoolsearch/${encodeURIComponent(q)}/40`;
+    // Note, we don't return the fetch promise chain because in a debounced
+    // function we're not guaranteed to return anything, and it's not a great
+    // interface to sometimes return undefined when there's still async work
+    // going on.
     fetch(searchUrl)
       .then(response => response.ok ? response.json() : [])
       .then(json => {
@@ -59,6 +71,8 @@ export default class SchoolAutocompleteDropdown extends Component {
       return Promise.resolve();
     }
 
+    // Wrap the debounced call in a Promise so we _always_ return a promise
+    // from this function, which resolves whenever results come back.
     return new Promise((resolve, reject) => {
       this.debouncedSearch(q, (err, result) => {
         if (err) {
