a4fb6fd87c0567e56feceb8de3aa866251b44560
Erin Bond <erin.bond@code.org>
2018-02-02 15:34:03 -0800

    bang find_or_create_by to throw more useful expection if it fails

--- dashboard/app/controllers/featured_projects_controller.rb
+++ dashboard/app/controllers/featured_projects_controller.rb
@@ -4,7 +4,7 @@ class FeaturedProjectsController < ApplicationController
   def feature
     _, channel_id = storage_decrypt_channel_id(params[:project_id])
     return render_404 unless channel_id
-    @featured_project = FeaturedProject.find_or_create_by(storage_app_id: channel_id)
+    @featured_project = FeaturedProject.find_or_create_by!(storage_app_id: channel_id)
     @featured_project.update_attribute(:unfeatured_at, nil)
     @featured_project.update_attribute(:featured_at, DateTime.now)
     @featured_project.save!
