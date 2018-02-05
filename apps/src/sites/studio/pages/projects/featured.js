61d39fce0dc84626fac38c64a927cf0a0a6145eb
Erin Bond <erin.bond@code.org>
2018-02-05 12:11:45 -0800

    pass real data to featured-projects

--- apps/src/sites/studio/pages/projects/featured.js
+++ apps/src/sites/studio/pages/projects/featured.js
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom';
 import FeaturedProjects from '@cdo/apps/templates/projects/FeaturedProjects';
 
 $(document).ready(function () {
+  // const script = document.querySelector('script[data-featuredprojects]');
+  // const featuredProjectsData = JSON.parse(script.dataset.featuredprojects);
+
+  // const test = featuredProjectsData.test
+
+
+
   ReactDOM.render(
     <FeaturedProjects/>,
     document.getElementById('featured-projects-container')
--- dashboard/app/controllers/featured_projects_controller.rb
+++ dashboard/app/controllers/featured_projects_controller.rb
@@ -1,18 +1,35 @@
 class FeaturedProjectsController < ApplicationController
-  authorize_resource
-
-  def feature
-    _, channel_id = storage_decrypt_channel_id(params[:project_id])
-    return render_404 unless channel_id
-    @featured_project = FeaturedProject.find_or_create_by!(storage_app_id: channel_id)
-    @featured_project.update! unfeatured_at: nil
-    @featured_project.update! featured_at: DateTime.now
+  def create
+    unless current_user.try(:permission?, UserPermission::PROJECT_VALIDATOR)
+      _, channel_id = storage_decrypt_channel_id(featured_project_params[:project_id])
+      @featured_project = FeaturedProject.create({storage_app_id: channel_id, who_featured_user_id: current_user.id})
+      @featured_project.save!
+    end
   end
 
-  def unfeature
-    _, channel_id = storage_decrypt_channel_id(params[:project_id])
-    return render_404 unless channel_id
-    @featured_project = FeaturedProject.find_by! storage_app_id: channel_id
-    @featured_project.update! unfeatured_at: DateTime.now
+  def destroy_by_project_id
+    unless current_user.try(:permission?, UserPermission::PROJECT_VALIDATOR)
+      _, channel_id = storage_decrypt_channel_id(params[:project_id])
+      @featured_project = FeaturedProject.find_by_storage_app_id(channel_id)
+      @featured_project.destroy
+    end
+  end
+
+  def sort_projects
+    @currently_featured_projects = []
+    @archive_unfeatured_projects = []
+    FeaturedProject.each do |project|
+      project.featured? ?
+      @currently_featured_projects << project : @archive_unfeatured_projects << project
+    end
+    puts @currently_featured_projects
+    puts
+    puts @archive_unfeatured_projects
+  end
+
+  private
+
+  def featured_project_params
+    params.require(:featured_project).permit(:storage_app_id, :project_id, :who_featured_user_id)
   end
 end
