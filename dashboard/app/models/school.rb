5864ccf7a7932566324cbc58272edb68268b4fb4
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-02-06 15:07:06 -0800

    remove accidentally-included schema changes

--- dashboard/app/models/school.rb
+++ dashboard/app/models/school.rb
@@ -21,6 +21,7 @@
 # Indexes
 #
 #  index_schools_on_id                  (id) UNIQUE
+#  index_schools_on_name_and_city       (name,city)
 #  index_schools_on_school_district_id  (school_district_id)
 #  index_schools_on_state_school_id     (state_school_id) UNIQUE
 #  index_schools_on_zip                 (zip)
