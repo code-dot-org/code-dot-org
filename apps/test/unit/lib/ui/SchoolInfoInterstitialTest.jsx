c2879114d5ea41e58ec0cfef1f0be3cc0d5156b3
Brad Buchanan <brad@code.org>
2018-02-07 17:57:08 -0800

    Test form submission

--- apps/test/unit/lib/ui/SchoolInfoInterstitialTest.jsx
+++ apps/test/unit/lib/ui/SchoolInfoInterstitialTest.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import sinon from 'sinon';
 import {expect} from '../../../util/configuredChai';
 import i18n from '@cdo/locale';
 import BaseDialog from '@cdo/apps/templates/BaseDialog';
@@ -218,6 +219,239 @@ describe('SchoolInfoInterstitial', () => {
           expect(wrapper.find(SchoolInfoInputs)).to.have.prop('ncesSchoolId', '-1');
         });
       });
+    });
+  });
+
+  describe('form submission', () => {
+    let server;
+
+    beforeEach(() => {
+      server = sinon.createFakeServer();
+    });
+
+    afterEach(() => {
+      server.restore();
+    });
+
+    it('submits with no info', () => {
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          scriptData={{
+            ...MINIMUM_PROPS.scriptData,
+            existingSchoolInfo: {},
+          }}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      expect(server.requests[0].requestBody).to.equal([
+        '_method=patch',
+        'auth_token=fake_auth_token',
+        'user%5Bschool_info_attributes%5D%5Bcountry%5D=',
+        'user%5Bschool_info_attributes%5D%5Bschool_type%5D=',
+        'user%5Bschool_info_attributes%5D%5Bschool_id%5D=',
+      ].join('&'));
+    });
+
+    it('submits with only a country', () => {
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          scriptData={{
+            ...MINIMUM_PROPS.scriptData,
+            existingSchoolInfo: {
+              country: 'United States',
+            },
+          }}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      expect(server.requests[0].requestBody).to.equal([
+        '_method=patch',
+        'auth_token=fake_auth_token',
+        'user%5Bschool_info_attributes%5D%5Bcountry%5D=United+States',
+        'user%5Bschool_info_attributes%5D%5Bschool_type%5D=',
+        'user%5Bschool_info_attributes%5D%5Bschool_id%5D=',
+      ].join('&'));
+    });
+
+    it('submits with a country and a school type', () => {
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          scriptData={{
+            ...MINIMUM_PROPS.scriptData,
+            existingSchoolInfo: {
+              country: 'United States',
+              school_type: 'public'
+            },
+          }}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      expect(server.requests[0].requestBody).to.equal([
+        '_method=patch',
+        'auth_token=fake_auth_token',
+        'user%5Bschool_info_attributes%5D%5Bcountry%5D=United+States',
+        'user%5Bschool_info_attributes%5D%5Bschool_type%5D=public',
+        'user%5Bschool_info_attributes%5D%5Bschool_id%5D=',
+      ].join('&'));
+    });
+
+    it('submits with a country, school type, and school id', () => {
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          scriptData={{
+            ...MINIMUM_PROPS.scriptData,
+            existingSchoolInfo: {
+              country: 'United States',
+              school_type: 'public',
+              school_id: '123'
+            },
+          }}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      expect(server.requests[0].requestBody).to.equal([
+        '_method=patch',
+        'auth_token=fake_auth_token',
+        'user%5Bschool_info_attributes%5D%5Bcountry%5D=United+States',
+        'user%5Bschool_info_attributes%5D%5Bschool_type%5D=public',
+        'user%5Bschool_info_attributes%5D%5Bschool_id%5D=123',
+      ].join('&'));
+    });
+
+    it('submits with a country, school type, and school name', () => {
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          scriptData={{
+            ...MINIMUM_PROPS.scriptData,
+            existingSchoolInfo: {
+              country: 'United States',
+              school_type: 'public',
+              school_name: 'Test School'
+            },
+          }}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      expect(server.requests[0].requestBody).to.equal([
+        '_method=patch',
+        'auth_token=fake_auth_token',
+        'user%5Bschool_info_attributes%5D%5Bcountry%5D=United+States',
+        'user%5Bschool_info_attributes%5D%5Bschool_type%5D=public',
+        'user%5Bschool_info_attributes%5D%5Bschool_name%5D=Test+School',
+        'user%5Bschool_info_attributes%5D%5Bschool_state%5D=',
+        'user%5Bschool_info_attributes%5D%5Bschool_zip%5D=',
+        'user%5Bschool_info_attributes%5D%5Bfull_address%5D=',
+      ].join('&'));
+    });
+
+    it('submits with a country, school type, name, state', () => {
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          scriptData={{
+            ...MINIMUM_PROPS.scriptData,
+            existingSchoolInfo: {
+              country: 'United States',
+              school_type: 'public',
+              school_name: 'Test School',
+              state: 'Washington',
+            },
+          }}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      expect(server.requests[0].requestBody).to.equal([
+        '_method=patch',
+        'auth_token=fake_auth_token',
+        'user%5Bschool_info_attributes%5D%5Bcountry%5D=United+States',
+        'user%5Bschool_info_attributes%5D%5Bschool_type%5D=public',
+        'user%5Bschool_info_attributes%5D%5Bschool_name%5D=Test+School',
+        'user%5Bschool_info_attributes%5D%5Bschool_state%5D=Washington',
+        'user%5Bschool_info_attributes%5D%5Bschool_zip%5D=',
+        'user%5Bschool_info_attributes%5D%5Bfull_address%5D=',
+      ].join('&'));
+    });
+
+    it('submits with a country, school type, name, state, zip', () => {
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          scriptData={{
+            ...MINIMUM_PROPS.scriptData,
+            existingSchoolInfo: {
+              country: 'United States',
+              school_type: 'public',
+              school_name: 'Test School',
+              state: 'Washington',
+              zip: '98109',
+            },
+          }}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      expect(server.requests[0].requestBody).to.equal([
+        '_method=patch',
+        'auth_token=fake_auth_token',
+        'user%5Bschool_info_attributes%5D%5Bcountry%5D=United+States',
+        'user%5Bschool_info_attributes%5D%5Bschool_type%5D=public',
+        'user%5Bschool_info_attributes%5D%5Bschool_name%5D=Test+School',
+        'user%5Bschool_info_attributes%5D%5Bschool_state%5D=Washington',
+        'user%5Bschool_info_attributes%5D%5Bschool_zip%5D=98109',
+        'user%5Bschool_info_attributes%5D%5Bfull_address%5D=',
+      ].join('&'));
+    });
+
+    it('closes the dialog on successful submission', () => {
+      const onClose = sinon.spy();
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          onClose={onClose}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      expect(onClose).not.to.have.been.called;
+
+      server.requests[0].respond(204, {}, '');
+      expect(onClose).to.have.been.calledOnce;
+    });
+
+    it('shows an error message on first failed submission', () => {
+      const onClose = sinon.spy();
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          onClose={onClose}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      server.requests[0].respond(404, {}, '');
+      expect(onClose).not.to.have.been.called;
+      expect(wrapper).to.containMatchingElement(
+        <p>We encountered an error with your submission. Please try again.</p>
+      );
+    });
+
+    it('closes the dialog on a second failed submission', () => {
+      const onClose = sinon.spy();
+      const wrapper = shallow(
+        <SchoolInfoInterstitial
+          {...MINIMUM_PROPS}
+          onClose={onClose}
+        />
+      );
+      wrapper.find(Button).simulate('click');
+      server.requests[0].respond(404, {}, '');
+      expect(onClose).not.to.have.been.called;
+
+      wrapper.find(Button).simulate('click');
+      server.requests[1].respond(404, {}, '');
+      expect(onClose).to.have.been.calledOnce;
     });
   });
 });
