02c6acaf7630f33850d217e8e4cd3e0859ea9bdd
Erin Bond <erin.bond@code.org>
2018-02-08 10:37:09 -0800

    add channel back in

--- apps/src/templates/projects/generateFakeProjects.js
+++ apps/src/templates/projects/generateFakeProjects.js
@@ -18,6 +18,7 @@ export const stubFakeFeaturedProjectData = [
   {
     projectName: "Featured Project 1",
     type: 'gamelab',
+    channel: 'abcd5',
     publishedAt: '2015-02-04T23:59:59.999-08:00',
     featuredAt: '2016-12-31T23:59:59.999-08:00',
     unfeaturedAt: 'null',
@@ -26,6 +27,7 @@ export const stubFakeFeaturedProjectData = [
   {
     projectName: "Featured Project 2",
     type: 'applab',
+    channel: 'abcd6',
     publishedAt: '2015-02-04T23:59:59.999-08:00',
     featuredAt: '2016-12-31T23:59:59.999-08:00',
     unfeaturedAt: 'null',
@@ -34,6 +36,7 @@ export const stubFakeFeaturedProjectData = [
   {
     projectName: "Featured Project 3 has a super extra long name let's see how it looks in the table",
     type: 'artist',
+    channel: 'abcd7',
     publishedAt: '2015-02-04T23:59:59.999-08:00',
     featuredAt: '2016-12-31T23:59:59.999-08:00',
     unfeaturedAt: 'null',
@@ -42,6 +45,7 @@ export const stubFakeFeaturedProjectData = [
   {
     projectName: "Featured Project 4",
     type: 'playlab',
+    channel: 'abcd8',
     publishedAt: '2015-02-04T23:59:59.999-08:00',
     featuredAt: '2016-12-31T23:59:59.999-08:00',
     unfeaturedAt: 'null',
@@ -53,6 +57,7 @@ export const stubFakeUnfeaturedProjectData = [
   {
     projectName: "Unfeatured Project 1",
     type: 'gamelab',
+    channel: 'abcd1',
     publishedAt: '2015-02-04T23:59:59.999-08:00',
     featuredAt: '2016-12-31T23:59:59.999-08:00',
     unfeaturedAt: '2017-12-31T23:59:59.999-08:00',
@@ -61,6 +66,7 @@ export const stubFakeUnfeaturedProjectData = [
   {
     projectName: "Unfeatured Project 2",
     type: 'applab',
+    channel: 'abcd2',
     publishedAt: '2015-02-04T23:59:59.999-08:00',
     featuredAt: '2016-12-31T23:59:59.999-08:00',
     unfeaturedAt: '2017-12-31T23:59:59.999-08:00',
