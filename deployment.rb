6f5b45f3af220410eef3150c0e96424994a20641
Drew Samnick <drew.samnick@code.org>
2018-02-12 15:30:10 -0800

    Only set census_map_table_id in production

--- deployment.rb
+++ deployment.rb
@@ -46,7 +46,7 @@ def load_configuration
     'build_apps'                  => false,
     'build_dashboard'             => true,
     'build_pegasus'               => true,
-    'census_map_table_id'         => '1AUZYRjLMI5NiQsDeDBGFsOIFpL_rLGsnxNpSyR13',
+    'census_map_table_id'         => rack_env == :production ? '1AUZYRjLMI5NiQsDeDBGFsOIFpL_rLGsnxNpSyR13' : nil,
     'chef_local_mode'             => rack_env == :adhoc,
     'dcdo_table_name'             => "dcdo_#{rack_env}",
     'dashboard_assets_dir'        => "#{root_dir}/dashboard/public/assets",
