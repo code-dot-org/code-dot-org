5142ce5c4cdf57dd42236f52742a6e9c28ec8f77
Drew Samnick <drew.samnick@code.org>
2018-02-12 15:20:06 -0800

    Automatically update the new fusion table for the new map

--- deployment.rb
+++ deployment.rb
@@ -46,6 +46,7 @@ def load_configuration
     'build_apps'                  => false,
     'build_dashboard'             => true,
     'build_pegasus'               => true,
+    'census_map_table_id'         => '1AUZYRjLMI5NiQsDeDBGFsOIFpL_rLGsnxNpSyR13',
     'chef_local_mode'             => rack_env == :adhoc,
     'dcdo_table_name'             => "dcdo_#{rack_env}",
     'dashboard_assets_dir'        => "#{root_dir}/dashboard/public/assets",
