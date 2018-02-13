2fc873e5b8b2b4e6fc8c2da471d908a882450a28
Continuous Integration <dev@code.org>
2018-02-13 20:38:26 +0000

    Updated cookbook versions

--- cookbooks/cdo-solr/metadata.rb
+++ cookbooks/cdo-solr/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@code.org'
 license          'All rights reserved'
 description      'Installs/Configures cdo-solr'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.2'
 
 depends 'cdo-java-7'
 depends 'ark'
