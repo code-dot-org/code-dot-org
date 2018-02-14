0aa67ccc35a997282b0ac85b23af99af946d40ea
bencodeorg <ben@code.org>
2018-02-14 15:52:48 -0800

    Update migration syntax

--- dashboard/db/migrate/20180214005255_add_deleted_at_to_regional_partners.rb
+++ dashboard/db/migrate/20180214005255_add_deleted_at_to_regional_partners.rb
@@ -1,5 +1,7 @@
 class AddDeletedAtToRegionalPartners < ActiveRecord::Migration[5.0]
   def change
-    add_column :deleted_at, :datetime, null: true
+    change_table :regional_partners do |t|
+      t.column :deleted_at, :datetime, null:true
+    end
   end
 end
