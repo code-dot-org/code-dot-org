303465422b8d0715af0b998cb6c2467dc926afae
bencodeorg <ben@code.org>
2018-02-15 09:42:28 -0800

    Fix linting error

--- dashboard/db/migrate/20180214005255_add_deleted_at_to_regional_partners.rb
+++ dashboard/db/migrate/20180214005255_add_deleted_at_to_regional_partners.rb
@@ -1,7 +1,7 @@
 class AddDeletedAtToRegionalPartners < ActiveRecord::Migration[5.0]
   def change
     change_table :regional_partners do |t|
-      t.column :deleted_at, :datetime, null:true
+      t.column :deleted_at, :datetime, null: true
     end
   end
 end
