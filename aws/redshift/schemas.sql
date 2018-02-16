3458ec5d1fe3a7635c1722b5a9c1de0d3f06b6f5
bencodeorg <ben@code.org>
2018-02-16 11:28:23 -0800

    Fix permissions issues in new analysis schemas [ci skip]

--- aws/redshift/schemas.sql
+++ aws/redshift/schemas.sql
@@ -1,3 +1,11 @@
 CREATE SCHEMA analysis_pii;
 GRANT ALL PRIVILEGES ON SCHEMA analysis_pii TO GROUP admin;
 GRANT SELECT ON ALL TABLES IN SCHEMA analysis_pii TO GROUP reader_pii;
+
+GRANT USAGE
+ON SCHEMA analysis
+TO GROUP reader, GROUP reader_pii, GROUP admin;
+
+GRANT USAGE
+ON SCHEMA analysis_pii
+TO GROUP reader_pii, GROUP admin;
