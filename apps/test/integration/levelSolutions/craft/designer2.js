933dcdab307fe97f9dba6fb123e67cd44b7e0433
Josh Lory <josh.lory@code.org>
2018-02-20 17:36:59 -0800

    Add integration test for Designer level 2

--- apps/test/integration/levelSolutions/craft/designer2.js
+++ apps/test/integration/levelSolutions/craft/designer2.js
@@ -6,18 +6,13 @@ const levelDef = {
   groundPlane: ["grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "dirt", "dirtCoarse", "dirt", "dirtCoarse", "dirt", "grass", "grass", "grass", "grass", "grass", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirtCoarse", "dirtCoarse", "grass", "grass", "grass", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirtCoarse", "dirtCoarse", "dirtCoarse", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "grass", "grass", "grass", "dirtCoarse", "dirt", "dirtCoarse", "dirtCoarse", "dirt", "dirt", "dirt", "grass", "grass", "grass", "grass", "grass", "grass", "dirtCoarse", "dirtCoarse", "dirt", "dirtCoarse", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "grass", "dirt"],
   groundDecorationPlane: ["", "", "tallGrass", "", "", "", "", "", "", "", "", "tallGrass", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "tallGrass", "", "", "", "", "", "", "", "", "", "", "tallGrass", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "flowerDandelion", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "tallGrass", "", "", "", "", "tallGrass", ""],
   actionPlane: ["grass", "grass", "", "", "", "", "", "", "grass", "grass", "grass", "", "", "", "", "", "", "grass", "grass", "grass", "", "", "", "", "", "", "", "", "", "grass", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "grass", "", "", "", "", "", "", "", "", "", "grass", "grass", "grass", "", "", "", "", "", "", "grass"],
-  entities: [["chicken", 4, 4, 1]],
+  entities: [["chicken", 3, 3, 1], ["chicken", 6, 3, 1], ["chicken", 3, 6, 1], ["chicken", 6, 6, 1]],
   usePlayer: false,
   levelVerificationTimeout: 5000,
   timeoutVerificationFunction: verificationAPI => (
-    verificationAPI.getCommandExecutedCount("moveForward") >= 1 &&
-    verificationAPI.getCommandExecutedCount("turn") >= 1
+    verificationAPI.getRepeatCommandExecutedCount("moveForward") > 0
   ),
-  verificationFunction: verificationAPI => (
-    !verificationAPI.isEntityTypeRunning("chicken") &&
-    verificationAPI.getCommandExecutedCount("moveForward") >= 1 &&
-    verificationAPI.getCommandExecutedCount("turn") >= 1
-  ),
+  verificationFunction: () => false,
 };
 
 export default {
@@ -26,32 +21,21 @@ export default {
   levelDefinition: levelDef,
   tests: [
     {
-      description: 'Craft Designer 1 fail',
+      description: 'Craft Designer 2',
       xml: `
         <xml>
           <block type="craft_chickenSpawned" deletable="false">
             <statement name="WHEN_SPAWNED">
-              <block type="craft_moveForward"></block>
-            </statement>
-          </block>
-        </xml>`,
-      expected: {
-        result: false,
-        testResult: TestResults.APP_SPECIFIC_FAIL,
-      },
-    },
-    {
-      description: 'Craft Designer 1 pass',
-      xml: `
-        <xml>
-          <block type="craft_chickenSpawned" deletable="false">
-            <statement name="WHEN_SPAWNED">
-              <block type="craft_moveForward">
-                <next>
-                  <block type="craft_entityTurnLR">
-                    <title name="DIR">left</title>
+              <block type="craft_forever">
+                <statement name="DO">
+                  <block type="craft_moveForward">
+                    <next>
+                      <block type="craft_entityTurnLR">
+                        <title name="DIR">left</title>
+                      </block>
+                    </next>
                   </block>
-                </next>
+                </statement>
               </block>
             </statement>
           </block>
