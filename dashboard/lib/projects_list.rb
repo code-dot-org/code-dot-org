6bdba5f347635a109da65329828dc6ceaccbe1b7
Erin Bond <erin.bond@code.org>
2018-02-26 11:49:06 -0800

    use Sequel.lit to actually get random featured projects

--- dashboard/lib/projects_list.rb
+++ dashboard/lib/projects_list.rb
@@ -118,7 +118,7 @@ module ProjectsList
         join(:users, id: Sequel[:user_storage_ids][:user_id]).
         where(unfeatured_at: nil, project_type: project_type.to_s, abuse_score: 0).
         exclude(published_at: nil).
-        order("RAND()").limit(4).all
+        order(Sequel.lit('RAND()')).limit(4).all
       extract_data_for_featured_project_cards(project_featured_project_user_combo_data)
     end
 
