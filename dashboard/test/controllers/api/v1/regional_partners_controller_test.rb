52fe7997a4481f632764cdff296a75a4ebe54f25
Mehal Shah <mehal@code.org>
2018-02-28 11:54:14 -0800

    Remove some dead code, fix breaking testS

--- dashboard/test/controllers/api/v1/regional_partners_controller_test.rb
+++ dashboard/test/controllers/api/v1/regional_partners_controller_test.rb
@@ -3,48 +3,7 @@ require 'test_helper'
 class Api::V1::RegionalPartnersControllerTest < ActionController::TestCase
   COURSES = ['csd', 'csp']
 
-  test 'index finds the regional partner for a normal district' do
-    (COURSES + ['unselected']).each do |course|
-      get :for_school_district_and_course, params: {school_district_id: '100002', course: course}
-      response = JSON.parse(@response.body)
-      assert_equal 'A+ College Ready', response['regional_partner']['name']
-    end
-  end
-
-  test 'index finds the regional partner for a given district and course combination' do
-    [
-      ['csp', 'A+ College Ready'],
-      ['csd', 'Academy for CS Education - Florida International University'],
-    ].each do |course, regional_partner|
-      get :for_school_district_and_course, params: {school_district_id: '1200390', course: course}
-      response = JSON.parse(@response.body)
-      assert_equal regional_partner, response['regional_partner']['name']
-    end
-
-    get :for_school_district_and_course, params: {school_district_id: '1200390', course: 'unselected'}
-    response = JSON.parse(@response.body)
-    assert_not_nil response['regional_partner']['name']
-  end
-
-  test 'index finds the regional partner and workshop days for a given district and course' do
-    [
-      ['csd', '200001', 'A+ College Ready', 'June 10-15, 2017'],
-      ['csp', '200001', 'A+ College Ready', 'June 10-15, 2017'],
-      ['unselected', '200001', 'A+ College Ready', 'June 10-15, 2017'],
-      ['csd', '200003', 'A+ College Ready', 'June 16-20, 2017'],
-      ['csp', '200003', 'A+ College Ready', 'June 16-20, 2017'],
-      ['unselected', '200003', 'A+ College Ready', 'June 16-20, 2017'],
-      ['csd', '600001', 'A+ College Ready', 'June 1-5, 2017'],
-      ['csp', '600001', 'A+ College Ready', 'June 21-25, 2017'],
-    ].each do |course, district, regional_partner, workshop_days|
-      get :for_school_district_and_course, params: {school_district_id: district, course: course}
-      response = JSON.parse(@response.body)
-      assert_equal regional_partner, response['regional_partner']['name']
-      assert_equal workshop_days, response['workshop_days']
-    end
-  end
-
-  test 'for_user gets regional partners for user' do
+  test 'index gets regional partners for user' do
     program_manager = create :teacher
 
     regional_partner_for_user = create :regional_partner, name: 'Regional Partner'
@@ -65,7 +24,7 @@ class Api::V1::RegionalPartnersControllerTest < ActionController::TestCase
     ], response
   end
 
-  test 'for_user gets all regional partners for workshop admin' do
+  test 'index gets all regional partners for workshop admin' do
     regional_partner = create :regional_partner, name: 'New regional partner'
     sign_in (create :workshop_admin)
 
