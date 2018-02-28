fb63ab999e20e6cd867f53cc55d04c7f9c8b0fe8
David Bailey <davidsbailey@users.noreply.github.com>
2018-02-28 12:52:05 -0800

    change indentation

--- apps/src/gamelab/animationListModule.js
+++ apps/src/gamelab/animationListModule.js
@@ -573,8 +573,12 @@ function loadAnimationFromSource(key, callback) {
             study_group: 'animation_no_load_v3',
             event: isOwner() ? 'animation_not_loaded_owner' : 'animation_not_loaded_viewer',
             project_id: getCurrentId(),
-            data_json: JSON.stringify({'sourceUrl': sourceUrl, 'version': state.propsByKey[key].version,
-              'animationName': state.propsByKey[key].name, 'error': err.message})
+            data_json: JSON.stringify({
+              'sourceUrl': sourceUrl,
+              'version': state.propsByKey[key].version,
+              'animationName': state.propsByKey[key].name,
+              'error': err.message
+            })
           },
           {includeUserId: true}
         );
