7e98cff5e3f191ae27c40cb7866dbc747316adb4
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-02-28 14:19:38 -0800

    update createResultsHandlerForSubtype util to use a name mapping, and add a test

--- apps/src/maze/results/utils.js
+++ apps/src/maze/results/utils.js
@@ -1,10 +1,3 @@
-const Farmer = require('../farmer');
-const Bee = require('../bee');
-const Collector = require('../collector');
-const Wordsearch = require('../wordsearch');
-const Harvester = require('../harvester');
-const Planter = require('../planter');
-
 const FarmerHandler = require('./farmer');
 const BeeHandler = require('./bee');
 const CollectorHandler = require('./collector');
@@ -13,22 +6,18 @@ const HarvesterHandler = require('./harvester');
 const PlanterHandler = require('./planter');
 const ResultsHandler = require('./resultsHandler');
 
-export function createResultsHandlerForSubtype(controller, config) {
-  let handler = ResultsHandler;
+const handlersBySubtypeName = {
+  "Farmer": FarmerHandler,
+  "Bee": BeeHandler,
+  "Collector": CollectorHandler,
+  "WordSearch": WordsearchHandler,
+  "Harvester": HarvesterHandler,
+  "Planter": PlanterHandler,
+};
 
-  if (controller.subtype instanceof Farmer) {
-    handler = FarmerHandler;
-  } else if (controller.subtype instanceof Bee) {
-    handler = BeeHandler;
-  } else if (controller.subtype instanceof Collector) {
-    handler = CollectorHandler;
-  } else if (controller.subtype instanceof Wordsearch) {
-    handler = WordsearchHandler;
-  } else if (controller.subtype instanceof Harvester) {
-    handler = HarvesterHandler;
-  } else if (controller.subtype instanceof Planter) {
-    handler = PlanterHandler;
-  }
+export function createResultsHandlerForSubtype(controller, config) {
+  const subtypeName = controller.subtype.constructor.name;
+  const handler = handlersBySubtypeName[subtypeName] || ResultsHandler;
 
   return new handler(controller, config);
 }
