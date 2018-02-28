6ea844985cf096d94ef33667d4b77c729bb9fa12
Tessa <tessa.wiedmann@code.org>
2018-02-28 15:39:03 -0800

    Test for regional partner contact logic in update_regional_partner

--- dashboard/test/models/pd/regional_partner_contact_test.rb
+++ dashboard/test/models/pd/regional_partner_contact_test.rb
@@ -91,4 +91,25 @@ class Pd::RegionalPartnerContactTest < ActiveSupport::TestCase
       ).to_json
     ).valid?
   end
+
+  test 'Matches regional partner' do
+    state = 'OH'
+    zip = '45242'
+
+    regional_partner = create :regional_partner, name: "partner_OH_45242"
+    regional_partner.mappings.find_or_create_by!(state: state)
+    regional_partner.mappings.find_or_create_by!(zip_code: zip)
+
+    regional_partner_contact = create :pd_regional_partner_contact, form_data: FORM_DATA.merge(
+      {
+        school_type: 'public',
+        school_district_other: false,
+        school_district: 'District',
+        school_state: state,
+        school_zipcode: zip
+      }
+    ).to_json
+
+    assert_equal regional_partner.id, regional_partner_contact.regional_partner_id
+  end
 end
