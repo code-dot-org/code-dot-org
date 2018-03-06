846db0a557eaa130935a9f32a6e65c0f201fa4bc
Erin <erin@code.org>
2018-03-05 19:58:29 -0800

    Blank animation loads from library

--- apps/src/gamelab/animationListModule.js
+++ apps/src/gamelab/animationListModule.js
@@ -323,27 +323,20 @@ export function setInitialAnimationList(serializedAnimationList) {
 export function addBlankAnimation() {
   const key = createUuid();
   return (dispatch, getState) => {
-    // Special behavior here:
-    // By pushing an animation that is "loadedFromSource" but has a null
-    // blob and dataURI, Piskel will know to create a new document with
-    // the given dimensions.
-    dispatch(addAnimationAction(
-      key,
-      {
-        name: generateAnimationName('animation', getState().animationList.propsByKey),
-        sourceUrl: null,
-        frameSize: {x: 100, y: 100},
-        frameCount: 1,
-        looping: true,
-        frameDelay: 4,
-        version: null,
-        loadedFromSource: true,
-        saved: false,
-        blob: new Blob(),
-        dataURI: null,
-        hasNewVersionThisSession: false
-      }));
-    dispatch(selectAnimation(key));
+    dispatch(addAnimationAction(key, {
+      name: generateAnimationName('animation', getState().animationList.propsByKey),
+      sourceUrl: '/api/v1/animation-library/mUlvnlbeZ5GHYr_Lb4NIuMwPs7kGxHWz/category_backgrounds/blank.png',
+      frameSize: {x: 100, y: 100},
+      frameCount: 1,
+      looping: true,
+      frameDelay: 4,
+      version: 'mUlvnlbeZ5GHYr_Lb4NIuMwPs7kGxHWz',
+      loadedFromSource: true,
+      saved: false
+    }));
+    dispatch(loadAnimationFromSource(key, () => {
+      dispatch(selectAnimation(key));
+    }));
     projectChanged();
   };
 }
