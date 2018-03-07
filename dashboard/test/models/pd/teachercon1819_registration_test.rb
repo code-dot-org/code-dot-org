595e45cd2c5e7669a7fa47e6cf523d1e05063148
Andrew Oberhardt <Andrew@Code.org>
2018-03-06 17:52:59 -0800

    Update application to withdrawn when fit weekend or teachercon registration is declined

--- dashboard/test/models/pd/teachercon1819_registration_test.rb
+++ dashboard/test/models/pd/teachercon1819_registration_test.rb
@@ -41,25 +41,23 @@ class Pd::Teachercon1819RegistrationTest < ActiveSupport::TestCase
   end
 
   test 'declined application requires fewer fields' do
-    registration = create(:pd_teachercon1819_registration, hash_trait: :withdrawn)
+    registration = create(:pd_teachercon1819_registration, hash_trait: :declined)
 
     assert registration.valid?
     refute registration.sanitize_form_data_hash.key?(:contact_first_name)
     refute registration.sanitize_form_data_hash.key?(:dietary_needs)
   end
 
-  test 'waitlisting or withdrawing in the registration will also update the application' do
-    %w(
-      accepted
-      waitlisted
-      withdrawn
-    ).each do |status|
-      application = create(:pd_teacher1819_application)
-      application.status = "accepted"
-      application.lock!
+  test 'waitlisting or declining the registration will also update the application status' do
+    {
+      accepted: 'accepted',
+      waitlisted: 'waitlisted',
+      declined: 'withdrawn'
+    }.each do |registration_status, expected_application_status|
+      application = create(:pd_teacher1819_application, :locked)
 
-      create(:pd_teachercon1819_registration, pd_application: application, hash_trait: status.to_sym)
-      assert_equal application.reload.status, status
+      create(:pd_teachercon1819_registration, pd_application: application, hash_trait: registration_status)
+      assert_equal expected_application_status, application.reload.status
     end
   end
 end
