c9ad050447703a1518ad6ba7050c8ac1adf2c652
Josh Lory <josh.lory@code.org>
2018-03-07 19:16:13 -0800

    Initial trophy progress skeleton

--- apps/src/templates/progress/StageTrophyProgressBubble.jsx
+++ apps/src/templates/progress/StageTrophyProgressBubble.jsx
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import trophy from './images/trophy.svg';
-
-const styles = {
-  main: {
-    backgroundImage: `url('${trophy}')`,
-    backgroundRepeat: 'none',
-    width: 21,
-    height: 19,
-  },
-};
+import trophy from '!!file-loader!./images/trophy.svg';
 
 export default class StageExtrasProgressBubble extends Component {
   render() {
     return (
-      <span
-        style={styles.main}
-      />
+      <svg
+        width="40"
+        height="40"
+      >
+        <path
+          d="M 20 0 A 20 20 0 1 1 0 20"
+          fill="none"
+          stroke="black"
+          strokeWidth="3"
+        />
+        <g transform="translate(8, 10)">
+          <use
+            href={`${trophy}#trophy`}
+            transform="scale(0.04)"
+          />
+        </g>
+      </svg>
     );
   }
 }
