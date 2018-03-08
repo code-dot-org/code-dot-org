cb2ec77a99128217565556f758062a064157b59a
Andrew Oberhardt <Andrew@Code.org>
2018-03-08 15:38:36 -0800

    Add assigned workshop and registered workshop to facilitator cohort view, clean up associated code and tests, and add tests

---
+++ dashboard/app/serializers/api/v1/pd/cohort_view_serializer_base.rb
@@ -0,0 +1,29 @@
+class Api::V1::Pd::CohortViewSerializerBase < ActiveModel::Serializer
+  attributes(
+    :id,
+    :date_accepted,
+    :applicant_name,
+    :district_name,
+    :school_name,
+    :email,
+    :assigned_workshop,
+    :registered_workshop,
+    :status
+  )
+
+  def date_accepted
+    object.accepted_at.try(:strftime, '%b %e')
+  end
+
+  def email
+    object.user.email
+  end
+
+  def assigned_workshop
+    object.workshop.try(&:date_and_location_name) || ''
+  end
+
+  def registered_workshop
+    object.registered_workshop? ? 'Yes' : 'No'
+  end
+end
