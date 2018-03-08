6f84751d2550b05b51b3f607813091d4ac18b564
Clare Constantine <clare@code.org>
2018-03-08 12:01:24 -0800

    move api call for regional partner cohort capacity into regional_partners_controller

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -554,6 +554,7 @@ Dashboard::Application.routes.draw do
       get 'schools/:id', to: 'schools#show', defaults: {format: 'json'}
       get 'regional_partners/:school_district_id/:course', to: 'regional_partners#for_school_district_and_course', defaults: {format: 'json'}
       get 'regional_partners', to: 'regional_partners#index', defaults: {format: 'json'}
+      get 'regional_partners/capacity', to: 'regional_partners#capacity'
 
       get 'projects/gallery/public/:project_type/:limit(/:published_before)', to: 'projects/public_gallery#index', defaults: {format: 'json'}
 
