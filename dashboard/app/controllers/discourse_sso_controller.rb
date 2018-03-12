5275d00b52ac4487223c1f6eab2c6559bbd075cc
Brendan Reville <brendan@code.org>
2018-03-12 11:10:17 -0700

    Forum: remove non-verified teachers from group upon forum sign-in
    Now, we actively remove non-verified teachers from the forum group for verified teachers upon forum sign-in.

--- dashboard/app/controllers/discourse_sso_controller.rb
+++ dashboard/app/controllers/discourse_sso_controller.rb
@@ -3,6 +3,8 @@ require 'single_sign_on'
 class DiscourseSsoController < ApplicationController
   before_action :authenticate_user! # ensures user must login
 
+  VERIFIED_TEACHERS_GROUP_NAME = 'Verified-Teachers'.freeze
+
   def sso
     secret = CDO.discourse_sso_secret
     sso = SingleSignOn.parse(request.query_string, secret)
@@ -12,7 +14,11 @@ class DiscourseSsoController < ApplicationController
     sso.external_id = current_user.id # from devise
     sso.sso_secret = secret
     sso.sso_url = CDO.discourse_sso_url
-    sso.add_groups = 'Verified-Teachers' if current_user.verified_teacher?
+    if current_user.verified_teacher?
+      sso.add_groups = DiscourseSsoController::VERIFIED_TEACHERS_GROUP_NAME
+    else
+      sso.remove_groups = DiscourseSsoController::VERIFIED_TEACHERS_GROUP_NAME
+    end
 
     redirect_to sso.to_url(CDO.discourse_sso_url)
   end
