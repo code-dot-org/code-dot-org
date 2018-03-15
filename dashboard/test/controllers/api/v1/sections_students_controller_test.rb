78e226d5d10759f61a1ecc1e7af83f35229c2ea9
Caley Brock <caleybrock7@gmail.com>
2018-03-15 16:03:09 -0700

    Basic test coverage for bulk add

--- dashboard/test/controllers/api/v1/sections_students_controller_test.rb
+++ dashboard/test/controllers/api/v1/sections_students_controller_test.rb
@@ -71,4 +71,42 @@ class Api::V1::SectionsStudentsControllerTest < ActionController::TestCase
     put :update, params: {section_id: @section.id, id: @student.id, student: {gender: 'f'}}
     assert_response :forbidden
   end
+
+  test 'teacher can add one student to a word section' do
+    sign_in @teacher
+    post :bulk_add, params: {section_id: @section.id, students: [{gender: 'f', age: 9, name: 'name'}]}
+    assert_response :success
+
+    parsed_response = JSON.parse(@response.body)
+    assert_equal 'name', parsed_response[0]['name']
+    assert_equal 9, parsed_response[0]['age']
+    assert_equal 'f', parsed_response[0]['gender']
+
+    new_student = User.find_by_id(parsed_response[0]['id'])
+
+    assert_equal 'name', new_student.name
+    assert_equal 9, new_student.age
+    assert_equal 'f', new_student.gender
+  end
+
+  test 'teacher can add multiple student to a word section' do
+    sign_in @teacher
+    post :bulk_add, params: {section_id: @section.id,
+      students: [{gender: 'f', age: 10, name: 'name1'}, {gender: 'm', age: 10, name: 'name2'}]}
+    assert_response :success
+    assert_equal 2, JSON.parse(@response.body).length
+  end
+
+  test 'non-owner can not add student' do
+    sign_in @other_teacher
+    post :bulk_add, params: {section_id: @section.id, students: [{gender: 'f', age: 9, name: 'name'}]}
+    assert_response :forbidden
+  end
+
+  test 'email section cannot add students' do
+    sign_in @teacher
+    @section = create(:section, user: @teacher, login_type: 'email')
+    post :bulk_add, params: {section_id: @section.id, students: [{gender: 'f', age: 9, name: 'name'}]}
+    assert_response :bad_request
+  end
 end
