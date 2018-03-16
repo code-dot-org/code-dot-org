dc55bb0f076cfc012b50a8c856bd5bb4c68e80c3
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-03-16 15:22:24 -0700

    Update sync-codeorg-all script to allow for non-interactive execution
    Specifically, give it some option parsing that'll let it distinguish
    between an interactive mode with `-i` (which works the same as it used
    to) and a command-specific mode with `-c` that will run just one of the
    steps.
    
    This will eventually be used by the i18n server to break the steps up so
    we can run them at different times with lots more flexibility.
    
    As a bonus, we get a nice `--help` output for free:
    
    ```bash
    $ bin/i18n/sync-codeorg-all.rb --help
      Usage: sync-codeorg-all [options]
    
      Commands:
        in:    Pull all updated source strings into i18n/locales/sources
        up:    Upload i18n/locales/sources to crowdin
        down:  Download translations from crowdin into i18n/locales
        out:   Distribute translations from i18n/locales out into codebase
    
      Options:
        -i, --interactive                Run through complete sync interactively
        -c, --command COMMAND            Run a single sync command
        -p, --with-pull-request          Automatically generate pull requests
    ```

--- bin/i18n/sync-codeorg-all.rb
+++ bin/i18n/sync-codeorg-all.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
-# Runs all steps necessary for a full i18n update
+# If run with the "interactive" flag, uns all steps necessary for a full i18n
+# update:
 #
 # First, to process newly-added strings, we:
 #   Sync In to collect various translations into a single source directory
@@ -14,6 +15,9 @@
 #
 # Note that this script will reset the repo in which it is run to staging
 # latest, and should therefore only be run with a clean working directory.
+#
+# Otherwise, if run with one of the "command" flags, will run a single step of
+# the full sync
 
 require_relative '../../lib/cdo/only_one'
 require_relative '../../lib/cdo/github'
@@ -25,8 +29,41 @@ require_relative 'sync-codeorg-up'
 require_relative 'sync-codeorg-down'
 require_relative 'sync-codeorg-out'
 
+require 'optparse'
+
 IN_UP_BRANCH = "i18n-sync-in-up-#{Date.today.strftime('%m-%d')}".freeze
 DOWN_OUT_BRANCH = "i18n-sync-down-out-#{Date.today.strftime('%m-%d')}".freeze
+
+def parse_options
+  options = {}
+  opt_parser = OptionParser.new do |opts|
+    opts.banner = <<-USAGE
+  Usage: sync-codeorg-all [options]
+
+  Commands:
+    in:    Pull all updated source strings into i18n/locales/sources
+    up:    Upload i18n/locales/sources to crowdin
+    down:  Download translations from crowdin into i18n/locales
+    out:   Distribute translations from i18n/locales out into codebase
+
+  Options:
+    USAGE
+
+    opts.on("-i", "--interactive", "Run through complete sync interactively") do
+      options[:interactive] = true
+    end
+
+    opts.on("-c", "--command COMMAND", %w(in up down out), "Run a single sync command") do |cmd|
+      options[:command] = cmd
+    end
+
+    opts.on("-p", "--with-pull-request", "Automatically generate pull requests") do
+      options[:with_pull_request] = true
+    end
+  end
+  opt_parser.parse!(ARGV)
+  options
+end
 
 def create_in_up_pr
   return unless should_i "create the in & up PR"
@@ -115,12 +152,37 @@ def create_down_out_pr
 end
 
 def main
-  sync_in if should_i "sync in"
-  sync_up if should_i "sync up"
-  create_in_up_pr
-  sync_down if should_i "sync down"
-  sync_out if should_i "sync out"
-  create_down_out_pr
+  options = parse_options
+
+  if options[:interactive]
+    sync_in if should_i "sync in"
+    sync_up if should_i "sync up"
+    create_in_up_pr if options[:with_pull_request]
+    sync_down if should_i "sync down"
+    sync_out if should_i "sync out"
+    create_down_out_pr if options[:with_pull_request]
+  elsif options[:command]
+    case options[:command]
+    when 'in'
+      puts "Pulling all updated source strings into i18n/locales/sources"
+      sync_in
+      if options[:with_pull_request]
+        create_in_up_pr
+      end
+    when 'up'
+      puts "Uploading i18n/locales/sources to crowdin"
+      sync_up
+    when 'down'
+      puts "Downloading translations from crowdin into i18n/locales"
+      sync_down
+    when 'out'
+      puts "Distributing translations from i18n/locales out into codebase"
+      sync_out
+      if options[:with_pull_request]
+        create_down_out_pr
+      end
+    end
+  end
 end
 
 main if __FILE__ == $0 && only_one_running?(__FILE__)
