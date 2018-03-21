3f5c6ca9307e7880b9d43f9d4f0b838ef0426851
Caley Brock <caleybrock7@gmail.com>
2018-03-21 16:15:37 -0700

    Update script based on code review

--- dashboard/scripts/update_under_13_students.rb
+++ dashboard/scripts/update_under_13_students.rb
@@ -3,20 +3,13 @@
 # sharing_disabled is contained in properties of the User model
 require_relative('../config/environment')
 
-CDO.log.info "Starting to batch update all students under 13"
-number_under_13 = 0
+puts "Starting to batch update all students under 13"
 
-User.find_in_batches do |users_batch|
-  ActiveRecord::Base.transaction do
-    users_batch.each do |user|
-      next unless user.under_13?
-      user.sharing_disabled = true
-      save_result = user.save
-      number_under_13 += 1
-      raise "ERROR: ID #{user.id}." unless save_result
-    end
-  end
+min_birthday = Date.today - 13.years
+users = User.where('birthday IS NULL OR birthday > ?', min_birthday)
+users.each do |user|
+  user.update! sharing_disabled: true
 end
 
-# How many total students were updated.
-CDO.log.info "Finished updating #{number_under_13} students."
+# Output how many total students were updated.
+puts "Finished updating #{users.count} students"
