8d6926417bfb0df62920b876462edcb1b7add5eb
David Bailey <davidsbailey@users.noreply.github.com>
2018-03-27 18:56:14 -0700

    allow empty dsl_text in tests

--- dashboard/app/models/levels/dsl_defined.rb
+++ dashboard/app/models/levels/dsl_defined.rb
@@ -127,8 +127,8 @@ class DSLDefined < Level
   def clone_with_name(new_name)
     raise "A level named '#{new_name}' already exists" if Level.find_by_name(new_name)
     level = super(new_name)
-    new_dsl = dsl_text.sub("name '#{name}'", "name '#{new_name}'")
-    level.update!(dsl_text: new_dsl)
+    new_dsl = dsl_text.try(:sub, "name '#{name}'", "name '#{new_name}'")
+    level.update!(dsl_text: new_dsl) if new_dsl
     level
   end
 
