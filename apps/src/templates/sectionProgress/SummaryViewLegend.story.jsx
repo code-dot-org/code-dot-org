f4267b00fa174554b78feb45817d13c8acf13f4d
Erin Bond <erin.bond@code.org>
2018-03-29 09:54:10 -0700

    WIP - SummaryViewLegend initial work
    wip

---
+++ apps/src/templates/sectionProgress/SummaryViewLegend.story.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import SummaryViewLegend from './SummaryViewLegend';
+
+export default storybook => {
+  return storybook
+    .storiesOf('Progress/SummaryViewLegend', module)
+    .addStoryTable([
+      {
+        name: 'CSF',
+        description: `CSF includes light green ProgressBox indicative of puzzles completed with too many blocks`,
+        story: () => (
+          <SummaryViewLegend
+            showCSFProgressBox={true}
+          />
+        )
+      },
+      {
+        name: 'CSD, CSP',
+        description: `excludes CSF-specific light green ProgressBox`,
+        story: () => (
+          <SummaryViewLegend
+            showCSFProgressBox={false}
+          />
+        )
+      },
+    ]);
+};
