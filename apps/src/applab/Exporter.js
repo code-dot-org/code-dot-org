e8262a2d12c51d3d12c0cc947b10c4c8ed2b9762
Chris Pirich <chris@code.org>
2018-03-29 11:00:13 -0700

    switch to using published snack-sdk

--- apps/src/applab/Exporter.js
+++ apps/src/applab/Exporter.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import _ from 'lodash';
 import JSZip from 'jszip';
 import {saveAs} from 'filesaver.js';
-import {SnackSession} from 'snack-sdk';
+import {SnackSession} from '@code-dot-org/snack-sdk';
 
 import * as assetPrefix from '../assetManagement/assetPrefix';
 import download from '../assetManagement/download';
