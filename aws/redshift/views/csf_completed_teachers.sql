4c0d5336b0075fc64a38982d3ddb759e103f5a57
bencodeorg <ben@code.org>
2018-03-30 13:57:08 -0700

    Improve readability of Redshift view SQL [ci skip]

--- aws/redshift/views/csf_completed_teachers.sql
+++ aws/redshift/views/csf_completed_teachers.sql
@@ -1,21 +1,23 @@
 drop view analysis.csf_completed_teachers;
 create view analysis.csf_completed_teachers as
-select user_id,
-school_year, 
-script_id,
-script_name,
-completed_at
+select 
+  user_id,
+  school_year, 
+  script_id,
+  script_name,
+  completed_at
 from
 (
-select se.user_id, 
-school_year,
-com.script_id,
-com.script_name,
-completed_at,
-row_number() over(partition by se.user_id, school_year order by completed_at asc) completed_at_order
-from analysis.csf_completed com
-join dashboard_production.followers f on f.student_user_id = com.user_id
-join dashboard_production.sections se on se.id = f.section_id
+  select 
+    se.user_id, 
+    school_year,
+    com.script_id,
+    com.script_name,
+    completed_at,
+    row_number() over(partition by se.user_id, school_year order by completed_at asc) completed_at_order
+  from analysis.csf_completed com
+    join dashboard_production.followers f on f.student_user_id = com.user_id
+    join dashboard_production.sections se on se.id = f.section_id
 )
 where completed_at_order = 5
 with no schema binding; 
