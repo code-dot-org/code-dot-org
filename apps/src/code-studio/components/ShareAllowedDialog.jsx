85e1c15738782d02ab729a81d0b61ea511e3a34f
Josh Lory <josh.lory@code.org>
2018-04-02 11:11:25 -0700

    Revert "Code review feedback"
    This reverts commit 93baee4489c9b653dc68c07cdb3379554f98efcd.

--- apps/src/code-studio/components/ShareAllowedDialog.jsx
+++ apps/src/code-studio/components/ShareAllowedDialog.jsx
@@ -1,3 +1,5 @@
+/* global dashboard */
+
 import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
 import BaseDialog from '../../templates/BaseDialog';
@@ -13,7 +15,6 @@ import { showPublishDialog } from '../../templates/publishDialog/publishDialogRe
 import PublishDialog from '../../templates/publishDialog/PublishDialog';
 import { createHiddenPrintWindow } from '@cdo/apps/utils';
 import i18n from '@cdo/locale';
-import {getCurrentId} from '@cdo/apps/code-studio/initApp/project';
 import firehoseClient from '@cdo/apps/lib/util/firehose';
 
 function recordShare(type) {
@@ -25,7 +26,7 @@ function recordShare(type) {
     study: 'finish-dialog-share',
     study_group: 'v1',
     event: 'project-share',
-    project_id: getCurrentId(),
+    project_id: dashboard.project && dashboard.project.getCurrentId(),
     data_string: type,
   }, {includeUserId: true});
 }
