6cc1a943ee77d2990b6b5a6af7f0263829ea3115
Caley Brock <caleybrock7@gmail.com>
2018-04-04 12:38:38 -0700

    Update script to save without validation

--- dashboard/scripts/update_under_13_students.rb
+++ dashboard/scripts/update_under_13_students.rb
@@ -5,13 +5,21 @@ require_relative('../config/environment')
 
 puts "Starting to batch update all students under 13."
 num_students_updated = 0
+num_student_failed = 0
 
 min_birthday = Date.today - 13.years
 User.where('birthday IS NULL OR birthday > ?', min_birthday).find_each do |user|
-  user.update! sharing_disabled: true
-  num_students_updated += 1
+  user.sharing_disabled = true
+  result = user.save(validate: false)
+  if result
+    num_students_updated += 1
+  else
+    puts "Failed to save user #{user.id}" unless result
+    num_student_failed += 1
+  end
   puts "Updated #{num_students_updated} students so far." if num_students_updated % 100000 == 0
 end
 
 # Output how many total students were updated.
 puts "Finished updating #{num_students_updated} students."
+puts "Failed updating #{num_student_failed} students."
