e94e909458b2650768fdebbf48a99be00897e441
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-04-09 15:54:43 -0700

    add support for embedding blocks with images

--- apps/src/templates/instructions/utils.js
+++ apps/src/templates/instructions/utils.js
@@ -37,45 +37,80 @@ export function scrollTo(element, scrollTop, animate=400) {
 }
 
 /**
+ * Shrink the DOM element containing the given blockSpace to the minimum size
+ * required to contain the block space
+ * @param {BlockSpace} blockSpace - the Blockly BlockSpace to resize
+ * @param {boolean} withPadding - whether or not to include padding
+ * @see convertXmlToBlockly
+ */
+function shrinkBlockSpaceContainer(blockSpace, withPadding) {
+  const container = blockSpace.blockSpaceEditor.getSVGElement().parentNode;
+
+  // calculate the minimum required size for the container
+  const metrics = blockSpace.getMetrics();
+  let height = metrics.contentHeight;
+  let width = metrics.contentWidth;
+
+  if (withPadding) {
+    height += metrics.contentTop * 2;
+    width += metrics.contentLeft;
+  }
+
+  // and shrink it, triggering a blockspace resize when we do so
+  container.style.height = height + "px";
+  container.style.width = width + "px";
+  blockSpace.blockSpaceEditor.svgResize();
+}
+
+/**
  * Converts any inline XML in the container element into embedded
  * readonly BlockSpaces
- * @param {Element} container The element in which to search for XML
+ * @param {Element} xmlContainer The element in which to search for XML
  */
-export function convertXmlToBlockly(container) {
-  const xmls = container.getElementsByTagName('xml');
+export function convertXmlToBlockly(xmlContainer) {
+  const xmls = xmlContainer.getElementsByTagName('xml');
+
   Array.prototype.forEach.call(xmls, function (xml) {
     // embedded blocks can be displayed either "inline" as part of a paragraph
     // or "block" all on their own. "block" is the default.
     const inline = xml.parentNode.tagName === "P";
 
     // create a container and insert the blockspace into it
-    const container = document.createElement(inline ? 'span' : 'div');
+    const blockSpaceContainer = document.createElement(inline ? 'span' : 'div');
     if (inline) {
       // SVGs don't play nicely if they're rendered into purely inline elements,
       // so if our container is a span it should be inline-block
-      container.style.display = "inline-block";
+      blockSpaceContainer.style.display = "inline-block";
     }
-    xml.parentNode.insertBefore(container, xml);
-    const blockSpace = Blockly.BlockSpace.createReadOnlyBlockSpace(container, xml, {
+    xml.parentNode.insertBefore(blockSpaceContainer, xml);
+    const blockSpace = Blockly.BlockSpace.createReadOnlyBlockSpace(blockSpaceContainer, xml, {
       noScrolling: true,
       inline: inline
     });
 
-    // then, calculate the minimum required size for the container
-    const metrics = blockSpace.getMetrics();
-    let height = metrics.contentHeight;
-    let width = metrics.contentWidth;
-
     // give block embeds more padding than inline
-    if (!inline) {
-      height += metrics.contentTop * 2;
-      width += metrics.contentLeft;
-    }
+    const withPadding = !inline;
 
-    // and shrink it, triggering a blockspace resize when we do so
-    container.style.height = height + "px";
-    container.style.width = width + "px";
-    blockSpace.blockSpaceEditor.svgResize();
+    // finally, shrink the container to exactly contain the blockspace. Note
+    // that some blocks (like K1 harvester blocks, which use FieldImages) can
+    // resize after initial render, so we also want to resize the container
+    // whenever a blockSpaceChange results in the content size changing.
+    let metrics = blockSpace.getMetrics();
+    blockSpace.getCanvas().addEventListener('blocklyBlockSpaceChange', function () {
+      const oldHeight = metrics.contentHeight;
+      const oldWidth = metrics.contentWidth;
+      const newHeight = blockSpace.getMetrics().contentHeight;
+      const newWidth = blockSpace.getMetrics().contentWidth;
+
+      // if the blockspace's content size has changed, kick off another sync and
+      // save the new metrics as the old ones
+      if (newHeight !== oldHeight || newWidth !== oldWidth) {
+        shrinkBlockSpaceContainer(blockSpace, withPadding);
+        metrics = blockSpace.getMetrics();
+      }
+    });
+
+    shrinkBlockSpaceContainer(blockSpace, withPadding);
   });
 }
 
