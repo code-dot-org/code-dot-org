1c27dde6a5aad44da66ab4ecac4b7ea62861c597
Caley Brock <caleybrock7@gmail.com>
2018-04-09 16:48:37 -0700

    First pass at tables styles

--- apps/src/templates/sectionProgress/VirtualizedSummaryView.jsx
+++ apps/src/templates/sectionProgress/VirtualizedSummaryView.jsx
@@ -3,33 +3,55 @@ import { MultiGrid } from 'react-virtualized';
 import styleConstants from '../../styleConstants';
 import { sectionDataPropType, scriptDataPropType, studentLevelProgressPropType } from './sectionProgressRedux';
 import StudentProgressSummaryCell from '../sectionProgress/StudentProgressSummaryCell';
+import color from "../../util/color";
 
 // TODO(caleybrock): share these styles with detail view
 const styles = {
-  cell: {
-    padding: 10,
-    width: '100%',
+  lessonHeading: {
+    fontFamily: '"Gotham 5r", sans-serif',
+  },
+  lessonNumberHeading: {
+    margin: '9px 16px',
+    fontFamily: '"Gotham 5r", sans-serif',
   },
   multigrid: {
-    border: '1px solid #ddd',
+    border: '1px solid',
+    borderColor: color.border_gray,
   },
   bottomLeft: {
-    borderRight: '2px solid #aaa',
-    backgroundColor: '#f7f7f7',
+    borderRight: '2px solid',
+    borderColor: color.border_gray,
   },
   topLeft: {
-    borderBottom: '2px solid #aaa',
-    borderRight: '2px solid #aaa',
-    fontWeight: 'bold',
+    borderBottom: '2px solid',
+    borderRight: '2px solid',
+    borderColor: color.border_gray,
+    padding: '8px 10px',
+    backgroundColor: color.table_header,
   },
   topRight: {
-    borderBottom: '2px solid #aaa',
-    fontWeight: 'bold',
+    borderBottom: '2px solid',
+    borderRight: '1px solid',
+    borderColor: color.border_gray,
+    backgroundColor: color.table_header,
   },
   icon: {
     width: 40,
     padding: 3,
     fontSize: 20,
+  },
+  link: {
+    color: color.teal,
+  },
+  summaryCell: {
+    margin: '8px 12px',
+  },
+  nameCell: {
+    margin: '10px',
+  },
+  cell: {
+    borderRight: '1px solid',
+    borderColor: color.border_gray,
   }
 };
 
@@ -57,30 +79,49 @@ export default class VirtualizedDetailView extends Component {
     // Subtract 1 to account for the student name column.
     const stageIdIndex = columnIndex-1;
 
+    // Override default cell style from multigrid
+    let cellStyle = {
+      ...style,
+      ...styles.cell,
+    };
+    // Alternate background colour of each row
+    if (studentStartIndex%2 === 1) {
+      cellStyle = {
+        ...cellStyle,
+        backgroundColor: color.background_gray,
+      };
+    }
+
     return (
-      <div className={styles.Cell} key={key} style={style}>
-        {(rowIndex === 0 && columnIndex === 0) && (
-          <span style={styles.cell}>Lesson</span>
-        )}
-        {(rowIndex === 0 && columnIndex >= 1) && (
-          <span style={styles.cell}>{columnIndex}</span>
-        )}
-        {(rowIndex >= 1 && columnIndex === 0) && (
-          <span style={styles.cell}>
-            <a href={`/teacher-dashboard#/sections/${section.id}/student/${section.students[rowIndex-1].id}/script/${scriptData.id}`}>
-              {section.students[rowIndex-1].name}
+      <div key={key} style={cellStyle}>
+        {(rowIndex === 0 && columnIndex === 0) &&
+          <span style={styles.lessonHeading}>Lesson</span>
+        }
+        {(rowIndex === 0 && columnIndex >= 1) &&
+          <div style={styles.lessonNumberHeading}>
+            {columnIndex}
+          </div>
+        }
+        {(rowIndex >= 1 && columnIndex === 0) &&
+          <div style={styles.nameCell}>
+            <a
+              href={`/teacher-dashboard#/sections/${section.id}/student/${section.students[studentStartIndex].id}/script/${scriptData.id}`}
+              style={styles.link}
+            >
+              {section.students[studentStartIndex].name}
             </a>
-          </span>
-        )}
-        {rowIndex >= 1 && columnIndex > 0 && (
+          </div>
+        }
+        {(rowIndex >= 1 && columnIndex > 0) &&
           <StudentProgressSummaryCell
             studentId={section.students[studentStartIndex].id}
             section={section}
             studentLevelProgress={studentLevelProgress}
             stageId={stageIdIndex}
             scriptData={scriptData}
+            style={styles.summaryCell}
           />
-        )}
+        }
       </div>
     );
   };
