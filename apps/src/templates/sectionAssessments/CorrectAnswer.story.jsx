ef9771da379e6dcdbbe7814728bc24b1752c83e5
nkiru onwuneme <generalhrh@gmail.com>
2018-04-11 18:02:22 -0700

    Remove comment

--- apps/src/templates/sectionAssessments/CorrectAnswer.story.jsx
+++ apps/src/templates/sectionAssessments/CorrectAnswer.story.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import CorrectAnswer from './CorrectAnswer';
 
-// Leave a comment
 export default storybook => {
   storybook
     .storiesOf('SectionAssessments/CorrectAnswer', module)
