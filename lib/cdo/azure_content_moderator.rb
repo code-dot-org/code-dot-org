470b6b323c6473cb7aa0d9162c182ebeb512a479
Brad Buchanan <brad@code.org>
2018-04-17 14:56:01 -0700

    Get azure ratings by image data

--- lib/cdo/azure_content_moderator.rb
+++ lib/cdo/azure_content_moderator.rb
@@ -13,11 +13,12 @@ class AzureContentModerator
   end
 
   #
-  # Give a publicly-accessible image URL, requests rating information from
-  # Azure Content Moderation and return a rating category.
+  # Given a publicly-accessible image URL, requests rating information from
+  # Azure Content Moderation and returns a rating category.
   #
   # @param [String] image_url - must be reachable from the Azure service.
   # @returns [:everyone|:racy|:adult]
+  #
   def rate_image(image_url)
     uri = URI(@endpoint + '/moderate/v1.0/ProcessImage/Evaluate')
 
@@ -32,6 +33,45 @@ class AzureContentModerator
     response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
       http.request(request)
     end
+
+    result = JSON.parse(response.body)
+    if result['IsImageAdultClassified']
+      :adult
+    elsif result['IsImageRacyClassified']
+      :racy
+    else
+      :everyone
+    end
+  end
+
+  #
+  # Give some binary image data and a content type, requests rating information
+  # from Azure Content Moderation and returns a rating category.
+  #
+  # @param [String] image_data - binary image data to be rated
+  # @param [String] content_type - one of image/bmp, image/gif, image/jpeg, image/png
+  # @returns [:everyone|:racy|:adult]
+  #
+  def rate_image_data(image_data, content_type)
+    raise StandardError.new("Cannot accept content-type #{content_type}") unless %w(
+      image/bmp
+      image/gif
+      image/jpeg
+      image/png
+    ).include? content_type
+
+    uri = URI(@endpoint + '/moderate/v1.0/ProcessImage/Evaluate')
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request['Content-Type'] = content_type
+    request['Ocp-Apim-Subscription-Key'] = @api_key
+    request.body = image_data
+
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    raise StandardError.new('Failed to retrieve a rating from Azure') unless response.is_a?(Net::HTTPSuccess)
 
     result = JSON.parse(response.body)
     if result['IsImageAdultClassified']
