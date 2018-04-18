3414b29aa97f01ec90daa449bdc889a610f9a57b
Erin Bond <erin.bond@code.org>
2018-04-18 10:35:01 -0700

    add comment explaining hover style and organize order of action creaters in redux

--- apps/src/templates/sectionProgress/multiGridConstants.js
+++ apps/src/templates/sectionProgress/multiGridConstants.js
@@ -11,6 +11,7 @@ export const progressStyles = {
   lessonHeading: {
     fontFamily: '"Gotham 5r", sans-serif',
   },
+  // For 'hover' to be applied, you should wrap the component using this style in Radium.
   lessonNumberHeading: {
     margin: '9px 16px',
     fontFamily: '"Gotham 5r", sans-serif',
