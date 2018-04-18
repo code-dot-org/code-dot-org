67a827faafbf2a453fd1919c6554e48d99625aa0
Brad Buchanan <brad@code.org>
2018-04-18 15:47:33 -0700

    Nicer error handling

--- lib/test/cdo/test_azure_content_moderator.rb
+++ lib/test/cdo/test_azure_content_moderator.rb
@@ -7,8 +7,8 @@ class AzureContentModeratorTest < Minitest::Test
   include SetupTest
 
   # Comment these out when regenerating VCR file.
-  CDO.azure_content_moderation_endpoint = 'https://region.api.cognitive.microsoft.com/contentmoderator'
-  CDO.azure_content_moderation_key = 'fake_azure_api_key'
+  # CDO.azure_content_moderation_endpoint = 'https://region.api.cognitive.microsoft.com/contentmoderator'
+  # CDO.azure_content_moderation_key = 'fake_azure_api_key'
 
   # Do additional VCR configuration so as to prevent the api key from being logged to the
   # YML cassette, instead replacing it with a placeholder string.
@@ -37,12 +37,15 @@ class AzureContentModeratorTest < Minitest::Test
   def test_raise_on_image_too_small
     # This image is smaller than the Azure content moderator's minimum size.
     image_data = open('https://code.org/images/icons/medium-monogram-white.png')
-    Net::HTTP.expects(:start).once
-    assert_raises {@acm.rate_image(image_data, 'image/png')}
+    assert_raises AzureContentModerator::RequestFailed do
+      @acm.rate_image(image_data, 'image/png')
+    end
   end
 
   def test_raise_on_unacceptable_content_type
     Net::HTTP.expects(:start).never
-    assert_raises {@acm.rate_image(StringIO.new('some text content'), 'text/plain')}
+    assert_raises AzureContentModerator::UnsupportedContentType do
+      @acm.rate_image(StringIO.new('some text content'), 'text/plain')
+    end
   end
 end
--- lib/test/cdo/test_image_moderation.rb
+++ lib/test/cdo/test_image_moderation.rb
@@ -3,15 +3,28 @@ require 'cdo/image_moderation'
 require 'cdo/azure_content_moderator'
 
 class ImageModerationTest < Minitest::Test
+  def setup
+    @image_body = StringIO.new('fake-image-body')
+    @content_type = 'image/png'
+  end
+
   def test_returns_everyone_when_missing_api_key
     CDO.azure_content_moderation_key = nil
     AzureContentModerator.expects(:rate_image).never
-    assert_equal :everyone, ImageModeration.rate_image(StringIO.new('fake-image-body'), 'image/png')
+    assert_equal :everyone, ImageModeration.rate_image(@image_body, @content_type)
   end
 
   def test_uses_azure_when_api_key_present
     CDO.azure_content_moderation_key = 'fakekey'
     AzureContentModerator.any_instance.stubs(:rate_image).returns(:adult).once
-    assert_equal :adult, ImageModeration.rate_image(StringIO.new('fake-image-body'), 'image/png')
+    assert_equal :adult, ImageModeration.rate_image(@image_body, @content_type)
+  end
+
+  def test_allow_everything_when_moderation_fails
+    CDO.azure_content_moderation_key = 'fakekey'
+    test_err = AzureContentModerator::AzureError.new('Test error')
+    AzureContentModerator.any_instance.stubs(:rate_image).raises(test_err)
+    Honeybadger.expects(:notify).once.with(test_err)
+    assert_equal :everyone, ImageModeration.rate_image(@image_body, @content_type)
   end
 end
