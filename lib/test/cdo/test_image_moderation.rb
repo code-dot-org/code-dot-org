417b587ee4481455c4daad69e320d9490f026d99
Brad Buchanan <brad@code.org>
2018-04-18 10:51:27 -0700

    Rename ImageModeration.rate_image_data => rate_image

--- lib/test/cdo/test_image_moderation.rb
+++ lib/test/cdo/test_image_moderation.rb
@@ -3,15 +3,15 @@ require 'cdo/image_moderation'
 require 'cdo/azure_content_moderator'
 
 class ImageModerationTest < Minitest::Test
-  def test_returns_everyone_when_missing_api_key_data
+  def test_returns_everyone_when_missing_api_key
     CDO.azure_content_moderation_key = nil
     AzureContentModerator.expects(:rate_image).never
-    assert_equal :everyone, ImageModeration.rate_image_data('fake-image-body', 'image/png')
+    assert_equal :everyone, ImageModeration.rate_image('fake-image-body', 'image/png')
   end
 
-  def test_uses_azure_when_api_key_present_data
+  def test_uses_azure_when_api_key_present
     CDO.azure_content_moderation_key = 'fakekey'
     AzureContentModerator.any_instance.stubs(:rate_image).returns(:adult).once
-    assert_equal :adult, ImageModeration.rate_image_data('fake-image-body', 'image/png')
+    assert_equal :adult, ImageModeration.rate_image('fake-image-body', 'image/png')
   end
 end
