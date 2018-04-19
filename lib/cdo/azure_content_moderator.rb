88e7657f54a62d08696e68e4ab56e7ce1a5580b1
Brad Buchanan <brad@code.org>
2018-04-18 18:04:08 -0700

    Log Azure content moderation activity to Redshift

--- lib/cdo/azure_content_moderator.rb
+++ lib/cdo/azure_content_moderator.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'cdo/firehose'
 
 #
 # Use Microsoft Azure Content Moderator to check images for adult or racy content.
@@ -17,12 +18,17 @@ class AzureContentModerator
   end
 
   #
-  # Give some binary image data and a content type, requests rating information
+  # Given some binary image data and a content type, requests rating information
   # from Azure Content Moderation and returns a rating category.
   #
   # @param [IO] image_data - binary image data to be rated
   # @param [String] content_type - one of image/bmp, image/gif, image/jpeg, image/png
   # @returns [:everyone|:racy|:adult]
+  # @raise [AzureContentModerator::RequestFailed] when the request is not
+  #   successful.
+  # @raise [AzureContentModerator::UnsupportedContentType] when no request is
+  #   made because the specified content type cannot be moderated by this
+  #   service.
   #
   def rate_image(image_data, content_type)
     raise UnsupportedContentType.new("Cannot accept content-type #{content_type}") unless %w(
@@ -32,8 +38,30 @@ class AzureContentModerator
       image/png
     ).include? content_type
 
-    uri = URI(@endpoint + '/moderate/v1.0/ProcessImage/Evaluate')
+    report_request
 
+    request_start_time = Time.now
+    result = make_request(image_data, content_type)
+    request_duration = Time.now - request_start_time
+
+    rating = rating_from_azure_result(result)
+    report_response(rating, result, request_duration)
+    rating
+  end
+
+  private
+
+  #
+  # Sends a request to Azure to moderate the image.
+  #
+  # @param [IO] image_data - binary image data to be rated
+  # @param [String] content_type - image/bmp, image/gif, image/jpeg, image/png
+  # @returns [Hash] the parsed response from Azure
+  # @raise [AzureContentModerator::RequestFailed] when the request is not
+  #   successful.
+  #
+  def make_request(image_data, content_type)
+    uri = URI(@endpoint + '/moderate/v1.0/ProcessImage/Evaluate')
     request = Net::HTTP::Post.new(uri.request_uri)
     request['Content-Type'] = content_type
     request['Ocp-Apim-Subscription-Key'] = @api_key
@@ -46,7 +74,17 @@ class AzureContentModerator
 
     raise RequestFailed.new(error_details(response)) unless response.is_a?(Net::HTTPSuccess)
 
-    result = JSON.parse(response.body)
+    JSON.parse(response.body)
+  end
+
+  #
+  # Given a raw Azure content moderation result, returns a simplified rating
+  # used in our system to decide how to proceed.
+  #
+  # @param [Hash] the parsed response from Auzre
+  # @returns [:everyone|:racy|:adult]
+  #
+  def rating_from_azure_result(result)
     if result['IsImageAdultClassified']
       :adult
     elsif result['IsImageRacyClassified']
@@ -56,7 +94,33 @@ class AzureContentModerator
     end
   end
 
-  private
+  # Report to Firehose that we're about to make a request to Azure
+  def report_request
+    FirehoseClient.instance.put_record(
+      study: 'azure-content-moderation',
+      study_group: 'v1',
+      event: 'moderation-request',
+    )
+  end
+
+  # Report the response we got from Azure to Firehose
+  def report_response(rating, data, request_duration)
+    FirehoseClient.instance.put_record(
+      study: 'azure-content-moderation',
+      study_group: 'v1',
+      event: 'moderation-result',
+      data_string: rating.to_s,
+      data_json: data.
+        slice(
+          'AdultClassificationScore',
+          'IsImageAdultClassified',
+          'RacyClassificationScore',
+          'IsImageRacyClassified'
+        ).
+        merge('RequestDuration' => request_duration).
+        to_json
+    )
+  end
 
   #
   # Extract error information from a content moderation failure response
--- lib/test/cdo/test_azure_content_moderator.rb
+++ lib/test/cdo/test_azure_content_moderator.rb
@@ -25,16 +25,21 @@ class AzureContentModeratorTest < Minitest::Test
   end
 
   def test_checks_jpg_image
+    expect_firehose_log_request
+    expect_firehose_log_result
     image_data = open('https://studio.code.org/notfound.jpg')
     assert_equal :everyone, @acm.rate_image(image_data, 'image/jpeg')
   end
 
   def test_checks_png_image
+    expect_firehose_log_request
+    expect_firehose_log_result
     image_data = open('https://code.org/images/infographics/fit-800/diversity-courses-updated-05-23.png')
     assert_equal :everyone, @acm.rate_image(image_data, 'image/png')
   end
 
   def test_raise_on_image_too_small
+    expect_firehose_log_request
     # This image is smaller than the Azure content moderator's minimum size.
     image_data = open('https://code.org/images/icons/medium-monogram-white.png')
     assert_raises AzureContentModerator::RequestFailed do
@@ -43,9 +48,40 @@ class AzureContentModeratorTest < Minitest::Test
   end
 
   def test_raise_on_unacceptable_content_type
+    expect_no_firehose
     Net::HTTP.expects(:start).never
     assert_raises AzureContentModerator::UnsupportedContentType do
       @acm.rate_image(StringIO.new('some text content'), 'text/plain')
     end
+  end
+
+  private
+
+  def expect_firehose_log_request
+    FirehoseClient.any_instance.expects(:put_record).with(
+      study: 'azure-content-moderation',
+      study_group: 'v1',
+      event: 'moderation-request'
+    )
+  end
+
+  def expect_firehose_log_result
+    FirehoseClient.any_instance.expects(:put_record).with do |data|
+      data[:study] == 'azure-content-moderation' &&
+        data[:study_group] == 'v1' &&
+        data[:event] == 'moderation-result' &&
+        %w(everyone racy adult).include?(data[:data_string]) &&
+        JSON.parse(data[:data_json]).keys == %w(
+          AdultClassificationScore
+          IsImageAdultClassified
+          RacyClassificationScore
+          IsImageRacyClassified
+          RequestDuration
+        )
+    end
+  end
+
+  def expect_no_firehose
+    FirehoseClient.any_instance.expects(:put_record).never
   end
 end
