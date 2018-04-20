12eac9b3bf583674713fbd96c7fe7bc9edb6b3b1
bencodeorg <ben@code.org>
2018-04-20 16:28:00 -0700

    Add last progress column to started course views [ci skip]

--- aws/redshift/views/csf_started_teachers.sql
+++ aws/redshift/views/csf_started_teachers.sql
@@ -4,7 +4,8 @@ select
   script_id,
   script_name,
   school_year, 
-  started_at
+  max(case when started_at_order = 5 then started_at else null end) as started_at,
+  max(case when started_at_order >= 5 then last_progress_at else null end) as last_progress_at
 from
 (
   select 
@@ -13,6 +14,7 @@ from
     st.script_name,
     sy.school_year,
     st.started_at::date,
+    st.last_progress_at::date as last_progress_at,
     row_number() over(partition by st.script_id, se.user_id, sy.school_year order by st.started_at asc) started_at_order
   from analysis.csf_started st
     join analysis.school_years sy on st.started_at between sy.started_at and sy.ended_at
@@ -20,7 +22,8 @@ from
     join dashboard_production.sections se on se.id = f.section_id 
       and (se.script_id IN (1,17,18,19,23,236,237,238,239,240,241,258,259) or se.script_id is null)
 )
-where started_at_order = 5
+group by 1,2,3,4
+having max(started_at_order) >= 5
 with no schema binding;
 
 GRANT ALL PRIVILEGES ON analysis.csf_started_teachers TO GROUP admin;
