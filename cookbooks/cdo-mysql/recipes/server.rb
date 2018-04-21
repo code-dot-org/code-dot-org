9d83216b6dde57239f4e12b0ea3a15c6d5adb9f0
Will Jordan <will@code.org>
2018-04-20 18:01:48 -0700

    fix mysql service resource MySQL community package only provides Debian service script. [ci skip]

--- cookbooks/cdo-mysql/recipes/server.rb
+++ cookbooks/cdo-mysql/recipes/server.rb
@@ -21,13 +21,7 @@ end
 
 service 'mysql' do
   action [:enable, :start]
-  # Detect if upstart service is running on Ubuntu 14.04.
-  # Upstart is running on ec2 instances but usually not running on local Docker.
-  upstart_booted = `test -x /sbin/initctl && /sbin/initctl --version`.include? 'upstart'
-  if upstart_booted
-    provider Chef::Provider::Service::Upstart
-  else
-    provider Chef::Provider::Service::Debian
-  end
+  # MySQL community package only provides Debian service script.
+  provider Chef::Provider::Service::Debian
 end
 node.override['cdo-apps']['local_mysql'] = true if node['cdo-apps']
