3699c0b8349514897b21c52afaf992e29787177a
Will Jordan <will@code.org>
2018-04-20 17:13:34 -0700

    Revert "Upgrade MySQL version to 5.7"
    This reverts commit 66a9b74725c239206a2820ae661952cc7601a4a1.

--- cookbooks/cdo-mysql/recipes/server.rb
+++ cookbooks/cdo-mysql/recipes/server.rb
@@ -1,24 +1,4 @@
-include_recipe 'cdo-mysql::repo'
-
-apt_package 'mysql-server' do
-  action :upgrade
-  notifies :run, 'execute[mysql-upgrade]', :delayed
-  notifies :run, 'execute[mysql-user]',    :delayed
-end
-
-execute 'mysql-upgrade' do
-  command 'mysql_upgrade --user=root'
-  action :nothing
-  notifies :restart, 'service[mysql]', :immediately
-end
-
-# MySQL 5.7 Ubuntu package uses auth_socket plugin for local user by default.
-# Revert to mysql_native_password plugin to authenticate from non-root shell.
-execute 'mysql-user' do
-  command "echo \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';\" | mysql"
-  action :nothing
-end
-
+apt_package 'mysql-server-5.6'
 service 'mysql' do
   action [:enable, :start]
   # Detect if upstart service is running on Ubuntu 14.04.
