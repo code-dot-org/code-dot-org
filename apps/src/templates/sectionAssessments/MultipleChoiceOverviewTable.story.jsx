5f4a268c5d3bc62c6792d7b6403362216a64ebc1
nkiru onwuneme <generalhrh@gmail.com>
2018-04-25 13:59:14 -0700

    Removed sorting from all columns except question column

--- apps/src/templates/sectionAssessments/MultipleChoiceOverviewTable.story.jsx
+++ apps/src/templates/sectionAssessments/MultipleChoiceOverviewTable.story.jsx
@@ -4,21 +4,21 @@ import MultipleChoiceOverviewTable from './MultipleChoiceOverviewTable';
 const multipleChoiceData = [
   {
     id: 1,
-    question: 'What is a variable?',
+    question: '1. What is a variable?',
     percentAnsweredOptionOne: '80%',
     percentAnsweredOptionTwo: '15%',
     notAnswered: '5%',
   },
   {
     id: 2,
-    question: 'What is a 4-bit number for the decimal number Ten(10)',
+    question: '2. What is a 4-bit number for the decimal number Ten(10)',
     percentAnsweredOptionOne: '20%',
     percentAnsweredOptionTwo: '60%',
     notAnswered: '20%'
   },
   {
     id: 3,
-    question: 'What is the minimum number of bits you will need to encode the 26 letters of the alphabet',
+    question: '3. What is the minimum number of bits you will need to encode the 26 letters of the alphabet',
     percentAnsweredOptionOne: '35%',
     percentAnsweredOptionTwo: '65%',
     notAnswered: '0%'
