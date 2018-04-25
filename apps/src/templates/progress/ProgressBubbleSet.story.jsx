a795a8a5a57032206bce1508c437d412e7024554
Caley Brock <caleybrock7@gmail.com>
2018-04-25 15:11:59 -0700

    Fix alignment of diamond bubbles

--- apps/src/templates/progress/ProgressBubbleSet.story.jsx
+++ apps/src/templates/progress/ProgressBubbleSet.story.jsx
@@ -16,6 +16,13 @@ const levels = fakeLevels(5).map((level, index) => ({
 }));
 levels[0].isConceptLevel = true;
 
+const diamondLevels = fakeLevels(2).map((level, index) => ({
+  ...level,
+  status: statusForLevel[index]
+}));
+diamondLevels[0].isConceptLevel = true;
+diamondLevels[1].isConceptLevel = true;
+
 export default storybook => {
   storybook
     .storiesOf('Progress/ProgressBubbleSet', module)
@@ -50,6 +57,16 @@ export default storybook => {
           <ProgressBubbleSet
             levels={levels}
             disabled={true}
+          />
+        )
+      },
+      {
+        name:'diamond bubbles only',
+        description: 'diamonds should be aligned',
+        story: () => (
+          <ProgressBubbleSet
+            levels={diamondLevels}
+            disabled={false}
           />
         )
       },
