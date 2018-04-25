67c1d5ca485519705dfb6990cec14f8c9236486e
nkiru onwuneme <generalhrh@gmail.com>
2018-04-25 12:41:35 -0700

    add column

--- apps/src/templates/sectionAssessments/MultipleChoiceOverviewTable.story.jsx
+++ apps/src/templates/sectionAssessments/MultipleChoiceOverviewTable.story.jsx
@@ -5,20 +5,23 @@ const multipleChoiceData = [
   {
     id: 1,
     question: 'What is a variable?',
-    answerOptionOne: '40%',
-    answerOptionTwo: '20%',
+    answerOptionOne: '80%',
+    answerOptionTwo: '15%',
+    notAnswered: '5%',
   },
   {
     id: 2,
     question: 'What is a 4-bit number for the decimal number Ten(10)',
-    answerOptionOne: '40%',
-    answerOptionTwo: '20%',
+    answerOptionOne: '20%',
+    answerOptionTwo: '60%',
+    notAnswered: '20%'
   },
   {
     id: 3,
     question: 'What is the minimum number of bits you will need to encode the 26 letters of the alphabet',
-    answerOptionOne: '40%',
-    answerOptionTwo: '20%',
+    answerOptionOne: '35%',
+    answerOptionTwo: '65%',
+    notAnswered: '0%'
   }
 ];
 
