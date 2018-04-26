ebb246cd37cd9fe67be1abc47122a2aecaeed620
Continuous Integration <dev@code.org>
2018-04-26 23:27:48 +0000

    Updated cookbook versions

--- cookbooks/cdo-nodejs/metadata.rb
+++ cookbooks/cdo-nodejs/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures cdo-nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.21'
+version          '0.2.22'
 
 depends 'nodejs'
