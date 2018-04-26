0b94897595283a4a17173d24d23b2599df46bd07
Brad Buchanan <bradley.c.buchanan@gmail.com>
2018-04-26 16:27:17 -0700

    Revert "Revert "Upgrade Yarn to v1.6""

--- cookbooks/cdo-nodejs/attributes/default.rb
+++ cookbooks/cdo-nodejs/attributes/default.rb
@@ -1,3 +1,3 @@
 default['cdo-nodejs']['version'] = '6.x'
 default['cdo-nodejs']['npm_version'] = '3'
-default['cdo-nodejs']['yarn_version'] = '0.23.2-1'
+default['cdo-nodejs']['yarn_version'] = '1.6.0-1'
