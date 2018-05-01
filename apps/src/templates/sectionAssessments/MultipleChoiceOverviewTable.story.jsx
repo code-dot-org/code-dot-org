96af446f612f8b854563c54b9e09136e079f5466
nkiru onwuneme <generalhrh@gmail.com>
2018-05-01 13:13:54 -0700

    remove empty objects in multiple choice data

--- apps/src/templates/sectionAssessments/MultipleChoiceOverviewTable.story.jsx
+++ apps/src/templates/sectionAssessments/MultipleChoiceOverviewTable.story.jsx
@@ -5,7 +5,7 @@ const multipleChoiceData = [
   {
     id: 1,
     question: '1. What is a variable?',
-    answers: [{},{percentAnswered: '40', isCorrectAnswer: true},
+    answers:    [{percentAnswered: '40', isCorrectAnswer: true},
                  {percentAnswered: '20', isCorrectAnswer: false},
                  {percentAnswered: '20', isCorrectAnswer: false},
                  {percentAnswered: '20', isCorrectAnswer: false},
@@ -14,7 +14,7 @@ const multipleChoiceData = [
   {
     id: 2,
     question: '2. What is a 4-bit number for the decimal number Ten(10)?',
-    answers: [{},{percentAnswered: '30', isCorrectAnswer: false},
+    answers:    [{percentAnswered: '30', isCorrectAnswer: false},
                  {percentAnswered: '10', isCorrectAnswer: true},
                  {percentAnswered: '10', isCorrectAnswer: false},
                  {percentAnswered: '10', isCorrectAnswer: false},
@@ -25,7 +25,7 @@ const multipleChoiceData = [
   {
     id: 3,
     question: '3. What is the minimum number of bits you will need to encode the 26 letters of the alphabet?',
-    answers: [{},{percentAnswered: '50', isCorrectAnswer: false},
+    answers:    [{percentAnswered: '50', isCorrectAnswer: false},
                  {percentAnswered: '15', isCorrectAnswer: false},
                  {percentAnswered: '20', isCorrectAnswer: true},
                  {percentAnswered: '5', isCorrectAnswer: false},
@@ -35,7 +35,7 @@ const multipleChoiceData = [
   {
     id: 4,
     question: '4. What is a function?',
-    answers: [{},{percentAnswered: '15', isCorrectAnswer: false},
+    answers:    [{percentAnswered: '15', isCorrectAnswer: false},
                  {percentAnswered: '18', isCorrectAnswer: false},
                  {percentAnswered: '10', isCorrectAnswer: false},
                  {percentAnswered: '9', isCorrectAnswer: false},
