c46dbffb717eb4fd9f0b4a49e5c06f3cf103d2ab
Mehal Shah <mehal@code.org>
2018-05-01 12:57:05 -0700

    Changes to the peer review system - When creating peer review objects, create one for a user to review and one for a plc_reviewer to review - Remove the buttons for regular users - Remove the escalate button for plc_reviewers

--- dashboard/test/controllers/api/v1/peer_review_submissions_controller_test.rb
+++ dashboard/test/controllers/api/v1/peer_review_submissions_controller_test.rb
@@ -5,34 +5,38 @@ class Api::V1::PeerReviewSubmissionsControllerTest < ActionController::TestCase
 
   setup_all do
     @course_unit = create :plc_course_unit
-    @level_1, @level_2, @level_3, @level_4 = create_list :free_response, 4, peer_reviewable: true
+    @level_1, @level_2, @level_3 = create_list :free_response, 3, peer_reviewable: true
 
-    [@level_1, @level_2, @level_3, @level_4].each do |level|
+    levels = [@level_1, @level_2, @level_3]
+
+    levels.each do |level|
       create :script_level, script: @course_unit.script, levels: [level]
     end
 
     @submitter = create :teacher
     create :plc_user_course_enrollment, user: @submitter, plc_course: @course_unit.plc_course
-    reviewer = create :teacher
+    @teacher_reviewer = create :teacher
+    create :plc_user_course_enrollment, user: @teacher_reviewer, plc_course: @course_unit.plc_course
+    @plc_reviewer = create :plc_reviewer
 
-    [@level_1, @level_2, @level_3, @level_4].each do |level|
+    levels.each do |level|
       create_peer_reviews_for_user_and_level(@submitter, level)
     end
 
-    @escalated_reviews = PeerReview.where(level: @level_1)
-    @escalated_reviews.first.update(status: :escalated, reviewer: nil)
-    @escalated_reviews.second.update(status: :escalated, reviewer: reviewer)
+    # Accept the second review
+    PeerReview.where(level: @level_2).first.update(data: 'lgtm', reviewer: @teacher_reviewer)
+    PeerReview.where(level: @level_2).second.update(status: :accepted, reviewer: @plc_reviewer, from_instructor: true)
 
-    PeerReview.where(level: @level_2).second.update(status: :accepted, reviewer: reviewer)
-    PeerReview.where(level: @level_3).update(status: :accepted, reviewer: reviewer)
+    # Perform a review for the third
+    PeerReview.where(level: @level_3).first.update(data: 'lgtm', reviewer: @teacher_reviewer)
 
+    @level_1_reviews = PeerReview.where(level: @level_1)
     @level_2_reviews = PeerReview.where(level: @level_2)
     @level_3_reviews = PeerReview.where(level: @level_3)
-    @level_4_reviews = PeerReview.where(level: @level_4)
 
-    @plc_reviewer = create :plc_reviewer
+    @escalated_reviews = PeerReview.where(level: levels)
 
-    assert_equal 8, PeerReview.count
+    assert_equal 6, PeerReview.count
   end
 
   setup do
@@ -43,17 +47,31 @@ class Api::V1::PeerReviewSubmissionsControllerTest < ActionController::TestCase
     get :index, params: {filter: 'escalated'}
     assert_response :success
     response = JSON.parse(@response.body)
-    expected_response = [{
-      submitter: @submitter.name,
-      course_name: @course_unit.plc_course.name,
-      unit_name: @course_unit.name,
-      level_name: @level_1.name,
-      review_ids: [[@escalated_reviews.first.id, 'escalated'], [@escalated_reviews.second.id, 'escalated']],
-      status: 'escalated',
-      accepted_reviews: 0,
-      rejected_reviews: 0,
-      escalated_review_id: @escalated_reviews.first.id,
-    }.stringify_keys]
+
+    expected_response = [
+      {
+        submitter: @submitter.name,
+        course_name: @course_unit.plc_course.name,
+        unit_name: @course_unit.name,
+        status: 'escalated',
+        accepted_reviews: 0,
+        rejected_reviews: 0,
+        level_name: @level_1.name,
+        escalated_review_id: @level_1_reviews.second.id,
+        review_ids: [[@level_1_reviews.first.id, nil], [@level_1_reviews.second.id, 'escalated']]
+      },
+      {
+        submitter: @submitter.name,
+        course_name: @course_unit.plc_course.name,
+        unit_name: @course_unit.name,
+        status: 'escalated',
+        accepted_reviews: 0,
+        rejected_reviews: 0,
+        level_name: @level_3.name,
+        escalated_review_id: @level_3_reviews.second.id,
+        review_ids: [[@level_3_reviews.first.id, nil], [@level_3_reviews.second.id, 'escalated']]
+      },
+    ].map(&:stringify_keys)
     assert_equal expected_response, response.map {|r| r.except('submission_date', 'escalation_date')}
   end
 
@@ -62,23 +80,22 @@ class Api::V1::PeerReviewSubmissionsControllerTest < ActionController::TestCase
     assert_response :success
     response = JSON.parse(@response.body)
     assert_equal [
-      [[@escalated_reviews.first.id, 'escalated'], [@escalated_reviews.second.id, 'escalated']],
-      [[@level_2_reviews.first.id, nil], [@level_2_reviews.second.id, 'accepted']],
-      [[@level_4_reviews.first.id, nil], [@level_4_reviews.second.id, nil]],
+      [[@level_1_reviews.first.id, nil], [@level_1_reviews.second.id, 'escalated']],
+      [[@level_3_reviews.first.id, nil], [@level_3_reviews.second.id, 'escalated']],
     ], response.map {|submission| submission['review_ids']}
   end
 
   test 'Open peer reviews with email filter only gets those peer reviews' do
     # Create some for another submitter
     other_submitter = create :teacher
-    create_peer_reviews_for_user_and_level(other_submitter, @level_4)
-    submissions = PeerReview.where(level: @level_4, submitter: other_submitter)
+    create_peer_reviews_for_user_and_level(other_submitter, @level_3)
+    submissions = PeerReview.where(level: @level_3, submitter: other_submitter)
 
     get :index, params: {filter: 'open', email: other_submitter.email}
     assert_response :success
     response = JSON.parse(@response.body)
     assert_equal [
-      [[submissions.first.id, nil], [submissions.second.id, nil]]
+      [[submissions.first.id, nil], [submissions.second.id, 'escalated']]
     ], response.map {|submission| submission['review_ids']}
   end
 
@@ -87,10 +104,9 @@ class Api::V1::PeerReviewSubmissionsControllerTest < ActionController::TestCase
     assert_response :success
     response = JSON.parse(@response.body)
     assert_equal [
-      [[@escalated_reviews.first.id, 'escalated'], [@escalated_reviews.second.id, 'escalated']],
+      [[@level_1_reviews.first.id, nil], [@escalated_reviews.second.id, 'escalated']],
       [[@level_2_reviews.first.id, nil], [@level_2_reviews.second.id, 'accepted']],
-      [[@level_3_reviews.first.id, 'accepted'], [@level_3_reviews.second.id, 'accepted']],
-      [[@level_4_reviews.first.id, nil], [@level_4_reviews.second.id, nil]],
+      [[@level_3_reviews.first.id, nil], [@level_3_reviews.second.id, 'escalated']],
     ], response.map {|submission| submission['review_ids']}
   end
 
@@ -112,7 +128,7 @@ class Api::V1::PeerReviewSubmissionsControllerTest < ActionController::TestCase
     response = CSV.parse(@response.body)
 
     expected_headers = ['Name', 'Email']
-    expected_headers << [@level_1, @level_2, @level_3, @level_4].map do |level|
+    expected_headers << [@level_1, @level_2, @level_3].map do |level|
       [level.name.titleize, "#{level.name.titleize} Submit Date"]
     end
     expected_headers << 'Reviews Performed'
@@ -121,7 +137,8 @@ class Api::V1::PeerReviewSubmissionsControllerTest < ActionController::TestCase
 
     assert_equal [
       expected_headers.flatten,
-      [@submitter.name, @submitter.email, 'Unsubmitted', date, 'Unsubmitted', date, 'Accepted', date, 'Unsubmitted', date, '1']
+      [@submitter.name, @submitter.email, 'Pending Review', date, 'Accepted', date, 'Pending Review', date, '1'],
+      [@teacher_reviewer.name, @teacher_reviewer.email, 'Unsubmitted', '', 'Unsubmitted', '', 'Unsubmitted', '', '2']
     ], response
   end
 
@@ -139,9 +156,8 @@ class Api::V1::PeerReviewSubmissionsControllerTest < ActionController::TestCase
 
   def create_peer_reviews_for_user_and_level(user, level)
     level_source = create :level_source, level: level
-    create :user_level, user: user, level: level, level_source: level_source
-    2.times do
-      PeerReview.create(submitter: user, script: @course_unit.script, level: level, data: nil, level_source_id: level_source.id)
-    end
+    user_level = create :user_level, user: user, level: level, level_source: level_source, script: @course_unit.script, best_result: ActivityConstants::UNREVIEWED_SUBMISSION_RESULT
+
+    PeerReview.create_for_submission(user_level, level_source.id)
   end
 end
