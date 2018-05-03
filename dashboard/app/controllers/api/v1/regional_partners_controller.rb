6bcd2a026adef1c48efb23576367c0b6c3cad395
Clare Constantine <clare@code.org>
2018-05-03 11:47:06 -0700

    all facilitators should see list of all partners

--- dashboard/app/controllers/api/v1/regional_partners_controller.rb
+++ dashboard/app/controllers/api/v1/regional_partners_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::RegionalPartnersController < ApplicationController
   # GET /api/v1/regional_partners
   def index
-    regional_partners = (csf_facilitator?(current_user) || current_user.workshop_admin?) ? RegionalPartner.all : current_user.regional_partners
+    regional_partners = (current_user.facilitator? || current_user.workshop_admin?) ? RegionalPartner.all : current_user.regional_partners
 
     render json: regional_partners.order(:name).map {|partner| {id: partner.id, name: partner.name}}
   end
@@ -31,9 +31,5 @@ class Api::V1::RegionalPartnersController < ApplicationController
       end
     end
     nil
-  end
-
-  def csf_facilitator?(user)
-    Pd::CourseFacilitator.where(facilitator: current_user).pluck(:course).include? Pd::Workshop::COURSE_CSF
   end
 end
