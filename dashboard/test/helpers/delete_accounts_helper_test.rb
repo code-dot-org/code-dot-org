0b8b71763b045d2a0f001540a8ef1db2a4800c0c
Brad Buchanan <brad@code.org>
2018-05-02 17:22:51 -0700

    Test: Clears user.*_sign_in_ip

--- dashboard/test/helpers/delete_accounts_helper_test.rb
+++ dashboard/test/helpers/delete_accounts_helper_test.rb
@@ -6,7 +6,7 @@ class DeleteAccountsHelperTest < ActionView::TestCase
     SolrHelper.stubs(:delete_document).once
   end
 
-  test 'removes user.name' do
+  test 'clears user.name' do
     user = create :student
 
     refute_nil user.name
@@ -27,6 +27,23 @@ class DeleteAccountsHelperTest < ActionView::TestCase
 
     user.reload
     assert_match /^sys_deleted_\w{8}$/, user.username
+  end
+
+  test 'clears user.*_sign_in_ip' do
+    user = create :student
+    user.current_sign_in_ip = '192.168.0.1'
+    user.last_sign_in_ip = '10.0.0.1'
+    user.save
+
+    user.reload
+    refute_nil user.current_sign_in_ip
+    refute_nil user.last_sign_in_ip
+
+    purge_user user
+
+    user.reload
+    assert_nil user.current_sign_in_ip
+    assert_nil user.last_sign_in_ip
   end
 
   private
