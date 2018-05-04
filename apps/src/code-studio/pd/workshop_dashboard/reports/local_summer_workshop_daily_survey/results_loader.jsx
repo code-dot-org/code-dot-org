345a5218fc723c3b597e13b683a027f21c6c2344
Mehal Shah <mehal@code.org>
2018-05-04 15:34:20 -0700

    Basic API for the daily survey summarization, and basic UI on top of it. For the time being, fake the data

---
+++ apps/src/code-studio/pd/workshop_dashboard/reports/local_summer_workshop_daily_survey/results_loader.jsx
@@ -0,0 +1,53 @@
+import React, {PropTypes} from 'react';
+import $ from "jquery";
+import Spinner from '../../../components/spinner';
+import Results from './results';
+import _ from 'lodash';
+
+export class ResultsLoader extends React.Component {
+  static propTypes = {
+    params: PropTypes.shape({
+      workshopId: PropTypes.string.isRequired
+    })
+  };
+
+  state = {loading: true};
+
+  componentDidMount() {
+    this.load();
+  }
+
+  load() {
+    const url = `/api/v1/pd/workshops/${this.props.params['workshopId']}/local_workshop_daily_survey_report`;
+
+    this.loadRequest = $.ajax({
+      method: 'GET',
+      url: url,
+      dataType: 'json'
+    }).done(data => {
+      this.setState({
+        loading: false,
+        questions: data['questions'],
+        thisWorkshop: data['this_workshop'],
+        allMyWorkshops: data['all_my_workshops'],
+        sessions: Object.keys(data['this_workshop'])
+      });
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div>
+          <Spinner/>
+        </div>
+      );
+    } else {
+      return (
+        <Results
+          {..._.omit(this.state, ['loading'])}
+        />
+      );
+    }
+  }
+}
