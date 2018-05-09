dab4349bddf9bd66335b4ba193344da50945f8a5
Brad Buchanan <brad@code.org>
2018-05-09 12:21:54 -0700

    Nonfunctional dialog layout with story

---
+++ apps/src/lib/ui/ChangeEmailModal.story.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChangeEmailModal from './ChangeEmailModal';
+import {action} from '@storybook/addon-actions';
+
+export default storybook => storybook
+  .storiesOf('ChangeEmailModal', module)
+  .add('overview', () => (
+    <ChangeEmailModal
+      isOpen
+      handleSubmit={action('handleSubmit callback')}
+      handleCancel={action('handleCancel callback')}
+    />
+  ));
+
