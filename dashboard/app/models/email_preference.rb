a71e1473b74c1bab9d7ca7555ee1aedff220a785
Suresh Chanmugam <suresh@code.org>
2018-05-09 08:24:44 -0700

    Don't change opt_in to 'no' if the user has previously chosen to be added to our mailing list.  Also, use a constant to control the list of permitted source values.

--- dashboard/app/models/email_preference.rb
+++ dashboard/app/models/email_preference.rb
@@ -22,14 +22,25 @@ class EmailPreference < ApplicationRecord
   validates_uniqueness_of :email
   validates_email_format_of :email
 
+  SOURCE_TYPES = [
+    REGISTRATION = 'Teacher account sign up',
+    HOUR_OF_CODE_FORM = 'Host Hour of Code form',
+    VOLUNTEER = 'Volunteer form'
+  ].freeze
+
+  validates_inclusion_of :source, in: SOURCE_TYPES
+
   def email=(value)
     super(value&.strip&.downcase)
   end
 
   def self.upsert!(email:, opt_in:, ip_address:, source:, form_kind:)
-    EmailPreference.find_or_initialize_by(email: email).update!(
+    email_preference = EmailPreference.find_or_initialize_by(email: email)
+    email_preference.update!(
       email: email,
-      opt_in: opt_in,
+      # Don't change opt_in to false if the record exists already.  We currently only allow user to opt out via Pardot
+      # unsubscribe link.
+      opt_in: (!email_preference.new_record? && !opt_in) ? email_preference.opt_in : opt_in,
       ip_address: ip_address,
       source: source,
       form_kind: form_kind
