998b5a7fcf2a305a558b6658fd64271d91f49d08
Clare Constantine <clare@code.org>
2018-05-10 14:57:52 -0700

    columns refresh when filter changes, values correct

--- dashboard/app/controllers/pd/application_dashboard_controller.rb
+++ dashboard/app/controllers/pd/application_dashboard_controller.rb
@@ -2,6 +2,9 @@ module Pd
   class ApplicationDashboardController < ApplicationController
     include Application::RegionalPartnerTeacherconMapping
 
+    TEACHERCON = "teachercon"
+    LOCAL_SUMMER = "local summer"
+
     authorize_resource class: 'Pd::Application::ApplicationBase'
 
     def index
@@ -11,7 +14,7 @@ module Pd
       regional_partners = is_workshop_admin ? RegionalPartner.all : current_user.regional_partners
       regional_partners = regional_partners.select(:id, :name, :group).map do |partner|
         processed_partner = partner.attributes
-        processed_partner["teachercon?"] = get_matching_teachercon(partner)
+        processed_partner["workshopType"] = get_matching_teachercon(partner) ? TEACHERCON : LOCAL_SUMMER
         processed_partner
       end
 
