f8aac8053a02bc7efd721210a2c7c9016d2f206e
Erin <erin@code.org>
2018-05-10 12:41:45 -0700

    Include comment tab in 2018 teacher experiment

--- apps/src/util/experiments.js
+++ apps/src/util/experiments.js
@@ -25,7 +25,8 @@ experiments.PROGRESS_TAB = 'sectionProgressRedesign';
 // On the front end we are treating it as an experiment group that contains
 // COURSE_VERSIONS and PROGRESS_TAB.
 experiments.TEACHER_EXP_2018 = '2018-teacher-experience';
-experiments.TEACHER_EXP_2018_LIST = [experiments.COURSE_VERSIONS, experiments.PROGRESS_TAB];
+experiments.TEACHER_EXP_2018_LIST = [experiments.COURSE_VERSIONS, experiments.PROGRESS_TAB,
+  experiments.COMMENT_BOX_TAB];
 
 /**
  * Get our query string. Provided as a method so that tests can mock this.
