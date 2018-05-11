b91f4f486550734a5954e6d8571b47b93c2a46e0
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-05-11 13:02:18 -0700

    directly parse level file as XML rather than with regex

--- bin/i18n/sync-codeorg-in.rb
+++ bin/i18n/sync-codeorg-in.rb
@@ -63,54 +63,38 @@ def localize_level_content
   level_authored_hints = Hash.new
   level_callouts = Hash.new
 
-  instruction_pattern = /^\s*"instructions": (".*?"),?\n$/
-  markdown_instruction_pattern = /^\s*"markdown_instructions": (".*?"),?\n$/
-  failure_message_override_pattern = /^\s*"failure_message_override": (".*?"),?\n$/
-  authored_hint_pattern = /^\s*"authored_hints": "(.*?)",?\n$/
-  callout_pattern = /^\s*"callout_json": "(.*?)",?\n$/
-
   Dir.glob("dashboard/config/scripts/levels/*.level").sort.each do |file|
-    level = File.basename(file, ".*") + "_instruction"
-    markdown_level = File.basename(file, ".*") + "_markdown_instruction"
-    failure_message_override_level = File.basename(file, ".*") + "_failure_message_override"
-    authored_hint_level = File.basename(file, ".*") + "_authored_hint"
-    callout_level = File.basename(file, ".*") + "_callout"
+    level_name = File.basename(file, ".*")
+    File.open(file) do |data|
+      level_xml = Nokogiri::XML(data, &:noblanks)
+      config = JSON.parse(level_xml.xpath('//../config').first.text)
 
-    File.open(file) do |f|
-      f.each_line do |line|
-        # Instructions
-        instruction_match = line.match instruction_pattern
-        if instruction_match
-          level_instructions[level] = sanitize instruction_match.captures.first
+      # Instructions
+      if instructions = config["properties"]["instructions"]
+        level_instructions["#{level_name}_instruction"] = sanitize(instructions)
+      end
+
+      # Markdown Instructions
+      if markdown_instructions = config["properties"]["markdown_instructions"]
+        level_markdown_instructions["#{level_name}_markdown_instruction"] = sanitize(markdown_instructions)
+      end
+
+      # Failure message overrides
+      if failure_message_overrides = config["properties"]["failure_message_override"]
+        level_failure_message_overrides["#{level_name}_failure_message_override"] = sanitize(failure_message_overrides)
+      end
+
+      # Authored Hints
+      if authored_hints_json = config["properties"]["authored_hints"]
+        level_authored_hints["#{level_name}_authored_hint"] = JSON.parse(authored_hints_json).reduce({}) do |memo, hint|
+          memo[hint['hint_id']] = hint['hint_markdown'] unless hint['hint_id'].empty?
+          memo
         end
+      end
 
-        # Markdown Instructions
-        markdown_instruction_match = line.match markdown_instruction_pattern
-        if markdown_instruction_match
-          level_markdown_instructions[markdown_level] = sanitize markdown_instruction_match.captures.first
-        end
-
-        # Failure message overrides
-        failure_message_override_match = line.match failure_message_override_pattern
-        if failure_message_override_match
-          level_failure_message_overrides[failure_message_override_level] = failure_message_override_match.captures.first
-        end
-
-        # Authored Hints
-        authored_hint_match = line.match authored_hint_pattern
-        if authored_hint_match
-          hint_json = JSON.load(%Q("#{authored_hint_match.captures.first}"))
-          level_authored_hints[authored_hint_level] = JSON.parse(hint_json).reduce({}) do |memo, hint|
-            memo[hint['hint_id']] = hint['hint_markdown'] unless hint['hint_id'].empty?
-            memo
-          end
-        end
-
-        # Callouts
-        callout_match = line.match callout_pattern
-        next unless callout_match
-        callout_json = JSON.load(%Q("#{callout_match.captures.first}"))
-        level_callouts[callout_level] = JSON.parse(callout_json).reduce({}) do |memo, callout|
+      # Callouts
+      if callouts_json = config["properties"]["callout_json"]
+        level_callouts["#{level_name}_callout"] = JSON.parse(callouts_json).reduce({}) do |memo, callout|
           memo[callout['localization_key']] = callout['callout_text'] unless callout['localization_key'].empty?
           memo
         end
