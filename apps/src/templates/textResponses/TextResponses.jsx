0c97aab3a6494be3105391288f1a1617987f2938
Maddie Kasula <madelynkasula@gmail.com>
2018-05-11 14:06:29 -0700

    Initial 'filter by stage' dropdown implementation

--- apps/src/templates/textResponses/TextResponses.jsx
+++ apps/src/templates/textResponses/TextResponses.jsx
@@ -1,5 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
+import _ from 'lodash';
 import i18n from '@cdo/locale';
 import ScriptSelector from '@cdo/apps/templates/sectionProgress/ScriptSelector';
 import {h3Style} from "../../lib/ui/Headings";
@@ -13,6 +14,14 @@ const styles = {
   },
   table: {
     paddingTop: 10
+  },
+  dropdown: {
+    display: 'block',
+    boxSizing: 'border-box',
+    fontSize: 'medium',
+    height: 34,
+    paddingLeft: 5,
+    paddingRight: 5
   }
 };
 
@@ -29,6 +38,10 @@ class TextResponses extends Component {
     asyncLoadTextResponses: PropTypes.func.isRequired
   };
 
+  state = {
+    filterByStageName: null
+  };
+
   onChangeScript = scriptId => {
     const {setScriptId, asyncLoadTextResponses, sectionId} = this.props;
     asyncLoadTextResponses(sectionId, scriptId, () => {
@@ -36,9 +49,46 @@ class TextResponses extends Component {
     });
   };
 
+  renderStageFilterDropdown = () => {
+    // TODO: i18n
+    return (
+      <div>
+        Filter by stage:
+        <select
+          style={styles.dropdown}
+          onChange={this.onChangeStageFilter}
+        >
+          <option key="All">All</option>
+          {this.getStages().map(stage => <option key={stage}>{stage}</option>)}
+        </select>
+      </div>
+    );
+  };
+
+  getStages = () => {
+    const {responses, scriptId} = this.props;
+    const stages = _.uniq(_.map(responses[scriptId], 'stage'));
+    return stages;
+  };
+
+  onChangeStageFilter = event => {
+    const filterByStageName = event.target.value === "All" ? null : event.target.value;
+    this.setState({filterByStageName});
+  };
+
+  getFilteredResponses = () => {
+    const {filterByStageName} = this.state;
+    const {responses, scriptId} = this.props;
+    let filteredResponses = [...responses[scriptId]];
+
+    if (filterByStageName) {
+      filteredResponses = _.filter(filteredResponses, ['stage', filterByStageName]);
+    }
+    return filteredResponses;
+  };
+
   render() {
-    const {validScripts, scriptId, responses, sectionId, isLoadingResponses} = this.props;
-    const scriptResponses = responses[scriptId];
+    const {validScripts, scriptId, sectionId, isLoadingResponses} = this.props;
 
     return (
       <div>
@@ -52,9 +102,10 @@ class TextResponses extends Component {
             onChange={this.onChangeScript}
           />
         </div>
+        {this.renderStageFilterDropdown()}
         <div style={styles.table}>
           <TextResponsesTable
-            responses={scriptResponses}
+            responses={this.getFilteredResponses()}
             sectionId={sectionId}
             isLoading={isLoadingResponses}
           />
