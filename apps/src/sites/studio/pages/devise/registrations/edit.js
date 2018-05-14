c5cacadfd1607d8a41934ba1bac56d187769454f
Brad Buchanan <brad@code.org>
2018-05-14 13:47:44 -0700

    New Account page UI for changing account type (nonfunctional)

--- apps/src/sites/studio/pages/devise/registrations/edit.js
+++ apps/src/sites/studio/pages/devise/registrations/edit.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ChangeEmailModal from '@cdo/apps/lib/ui/ChangeEmailModal';
 import color from '@cdo/apps/util/color';
+import getScriptData from '@cdo/apps/util/getScriptData';
+
+const scriptData = getScriptData('edit');
+const initialUserType = scriptData.userType;
 
 const changeEmailMountPoint = document.createElement('div');
 function showChangeEmailModal() {
@@ -40,6 +44,10 @@ function hideChangeEmailModal() {
   document.body.removeChild(changeEmailMountPoint);
 }
 
+function onUserTypeChange(selectedType) {
+  $('#change-user-type-button').prop('disabled', selectedType === initialUserType);
+}
+
 $(document).ready(() => {
   $( "#edit_user_create_personal_account" ).on("submit", function (e) {
     if ($('#create_personal_user_email').length) {
@@ -50,6 +58,10 @@ $(document).ready(() => {
       });
     }
   });
+
+  // Enable
+  $('#change-user-type_user_user_type').change(e => onUserTypeChange(e.target.value));
+  onUserTypeChange(initialUserType);
 
   $('#edit-email-link').click(showChangeEmailModal);
 });
