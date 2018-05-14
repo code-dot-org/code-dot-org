7a2f6bfc1d5b81c1d8197425c41b34508b0ddc7e
Brad Buchanan <brad@code.org>
2018-05-14 10:34:16 -0700

    Fix ChangeEmailModal story

--- apps/src/lib/ui/ChangeEmailModal.story.jsx
+++ apps/src/lib/ui/ChangeEmailModal.story.jsx
@@ -4,12 +4,16 @@ import {action} from '@storybook/addon-actions';
 
 export default storybook => storybook
   .storiesOf('ChangeEmailModal', module)
-  .add('overview', () => (
-    <ChangeEmailModal
-      isOpen
-      handleSubmit={action('handleSubmit callback')}
-      handleCancel={action('handleCancel callback')}
-      userAge={21}
-    />
-  ));
+  .add('overview', () => {
+    const railsForm = document.createElement('form');
+    return (
+      <ChangeEmailModal
+        isOpen
+        handleSubmit={action('handleSubmit callback')}
+        handleCancel={action('handleCancel callback')}
+        railsForm={railsForm}
+        userAge={21}
+      />
+    );
+  });
 
