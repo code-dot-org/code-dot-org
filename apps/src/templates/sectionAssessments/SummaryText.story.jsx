d8c154a08acddc189465fbf881fbadd9633a5ea2
nkiru onwuneme <generalhrh@gmail.com>
2018-05-14 23:12:54 -0700

    Update story

--- apps/src/templates/sectionAssessments/SummaryText.story.jsx
+++ apps/src/templates/sectionAssessments/SummaryText.story.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import SummaryText from './SummaryText';
 
+
+
 export default storybook => {
   storybook
     .storiesOf('SectionAssessments/SummaryText', module)
@@ -10,7 +12,8 @@ export default storybook => {
         description: 'Display total number of students that submitted the assessment.',
         story: () => (
           <SummaryText
-            numSubmissions="12/15"
+            numStudentSubmissions={12}
+            totalNumStudents={15}
           />
         )
       },
