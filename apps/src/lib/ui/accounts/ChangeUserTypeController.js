476833693ee894da90909c88ebda682fbf0fb933
Brad Buchanan <brad@code.org>
2018-05-15 14:23:12 -0700

    Extract submit logic to ChangeUserTypeController

---
+++ apps/src/lib/ui/accounts/ChangeUserTypeController.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import ChangeUserTypeModal from '../ChangeUserTypeModal';
+
+export default class ChangeUserTypeController {
+  /**
+   * Attach handlers and behaviors for the part of the accounts page that
+   * lets the user change their account type from student to teacher, or from
+   * teacher to student.
+   *
+   * @param {!jQuery} form - jQuery wrapper for the Form element that we'll use
+   *   to submit account type changes.  This module will only interact with
+   *   children of that form element.
+   * @param {!string} initialUserType
+   * @param {!boolean} isOauth
+   */
+  constructor(form, initialUserType, isOauth) {
+    this.form = form;
+    this.initialUserType = initialUserType;
+    this.isOauth = isOauth;
+    this.dropdown = form.find('#change-user-type_user_user_type');
+    this.button = form.find('#change-user-type-button');
+
+
+    // Attach handlers
+    this.dropdown.change(e => this.onUserTypeDropdownChange(e.target.value));
+    this.button.click(this.onUserTypeChangeButtonClick);
+
+    // Set initial state
+    this.onUserTypeDropdownChange(initialUserType);
+  }
+
+  // Enable or disable the submit button for changing user type based on
+  // whether the selected user type is actually different than the user's
+  // current type.
+  onUserTypeDropdownChange = (selectedType) => {
+    this.button.prop('disabled', selectedType === this.initialUserType);
+  };
+
+  onUserTypeChangeButtonClick = (e) => {
+    // Email confirmation is required when changing from a student account
+    // to a teacher account.
+    const needEmailConfirmation = this.dropdown.val() === 'teacher';
+    // Confirm the user's password for this change, unless the user doesn't
+    // have a password.
+    const needPasswordConfirmation = !this.isOauth;
+
+    if (needEmailConfirmation || needPasswordConfirmation) {
+      this.showChangeUserTypeModal({needEmailConfirmation, needPasswordConfirmation});
+    } else {
+      // Submit form now?
+    }
+
+    e.preventDefault();
+  };
+
+  showChangeUserTypeModal() {
+    const targetUserType = this.dropdown.val();
+    const userHashedEmail= document.getElementById('change-user-type_user_hashed_email').value;
+    const handleSubmit = (values) => (
+      this.submitUserTypeChange(values)
+        .then(() => window.location.reload())
+    );
+
+    this.mountPoint = document.createElement('div');
+    document.body.appendChild(this.mountPoint);
+    ReactDOM.render(
+      <ChangeUserTypeModal
+        targetUserType={targetUserType}
+        currentHashedEmail={userHashedEmail}
+        handleSubmit={handleSubmit}
+        handleCancel={this.hideChangeUserTypeModal}
+      />,
+      this.mountPoint
+    );
+  }
+
+  hideChangeUserTypeModal = () => {
+    ReactDOM.unmountComponentAtNode(this.mountPoint);
+    document.body.removeChild(this.mountPoint);
+  };
+
+  /**
+   * Submit a user type change using the Rails-generated async form.
+   * @param {{currentEmail: string, currentPassword: string}} values
+   * @return {Promise} which may reject with an error or object containing
+   *   serverErrors.
+   */
+  submitUserTypeChange(values) {
+    return new Promise((resolve, reject) => {
+      const onSuccess = () => {
+        detachHandlers();
+        resolve();
+      };
+
+      const onFailure = (_, xhr) => {
+        const validationErrors = xhr.responseJSON;
+        let error;
+        if (validationErrors) {
+          error = {
+            serverErrors: {
+              currentEmail: validationErrors.email && validationErrors.email[0],
+              currentPassword: validationErrors.current_password && validationErrors.current_password[0],
+            }
+          };
+        } else {
+          error = new Error('Unexpected failure: ' + xhr.status);
+        }
+        detachHandlers();
+        reject(error);
+      };
+
+      // Subscribe to jquery-ujs events before we submit, and unsubscribe after
+      // the request is complete.
+      const detachHandlers = () => {
+        this.form.on('ajax:success', onSuccess);
+        this.form.on('ajax:error', onFailure);
+      };
+      this.form.on('ajax:success', onSuccess);
+      this.form.on('ajax:error', onFailure);
+      this.form.find('#change-user-type_user_email').val(values.currentEmail);
+      this.form.find('#change-user-type_user_current_password').val(values.currentPassword);
+      this.form.submit();
+    });
+  }
+}
--- apps/src/sites/studio/pages/devise/registrations/edit.js
+++ apps/src/sites/studio/pages/devise/registrations/edit.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ChangeEmailModal from '@cdo/apps/lib/ui/ChangeEmailModal';
-import ChangeUserTypeModal from '@cdo/apps/lib/ui/ChangeUserTypeModal';
+import ChangeUserTypeController from '@cdo/apps/lib/ui/accounts/ChangeUserTypeController';
 import color from '@cdo/apps/util/color';
 import getScriptData from '@cdo/apps/util/getScriptData';
 
@@ -12,7 +12,7 @@ const isOauth = scriptData.isOauth;
 
 $(document).ready(() => {
   initializeChangeEmailControls();
-  initializeChangeUserTypeControls(initialUserType, isOauth);
+  new ChangeUserTypeController($('#change-user-type-modal-form'), initialUserType, isOauth);
   initializeCreatePersonalAccountControls();
 });
 
@@ -55,124 +55,6 @@ function initializeChangeEmailControls() {
   }
 
   $('#edit-email-link').click(showChangeEmailModal);
-}
-
-/**
- * Attach handlers and behaviors for the part of the accounts page that
- * lets the user change their account type from student to teacher, or from
- * teacher to student.
- *
- * @param {!string} initialUserType
- */
-function initializeChangeUserTypeControls(initialUserType, isOauth) {
-  const changeUserTypeModalMountPoint = document.createElement('div');
-  const form = $('#change-user-type-modal-form');
-  const dropdown = form.find('#change-user-type_user_user_type');
-  const button = form.find('#change-user-type-button');
-
-  // Enable or disable the submit button for changing user type based on
-  // whether the selected user type is actually different than the user's
-  // current type.
-  function onUserTypeDropdownChange(selectedType) {
-    button.prop('disabled', selectedType === initialUserType);
-  }
-
-  function onUserTypeChangeButtonClick(e) {
-    // Email confirmation is required when changing from a student account
-    // to a teacher account.
-    const needEmailConfirmation = dropdown.val() === 'teacher';
-    // Confirm the user's password for this change, unless the user doesn't
-    // have a password.
-    const needPasswordConfirmation = !isOauth;
-
-    if (needEmailConfirmation || needPasswordConfirmation) {
-      showChangeUserTypeModal({needEmailConfirmation, needPasswordConfirmation});
-    } else {
-      // Submit form now?
-    }
-
-    e.preventDefault();
-  }
-
-  function showChangeUserTypeModal() {
-
-    document.body.appendChild(changeUserTypeModalMountPoint);
-    const targetUserType = dropdown.val();
-    const userHashedEmail= document.getElementById('change-user-type_user_hashed_email').value;
-    const handleSubmit = (values) => onSubmitUserTypeChange(values)
-      // .then(hideChangeUserTypeModal)
-      // .then(() => form.effect('highlight', {
-      //   duration: 1500,
-      //   color: color.orange,
-      // }))
-      .then(() => window.location.reload());
-
-    ReactDOM.render(
-      <ChangeUserTypeModal
-        targetUserType={targetUserType}
-        currentHashedEmail={userHashedEmail}
-        handleSubmit={handleSubmit}
-        handleCancel={hideChangeUserTypeModal}
-      />,
-      changeUserTypeModalMountPoint
-    );
-  }
-
-  function hideChangeUserTypeModal() {
-    ReactDOM.unmountComponentAtNode(changeUserTypeModalMountPoint);
-    document.body.removeChild(changeUserTypeModalMountPoint);
-  }
-
-  /**
-   * Submit a user type change using the Rails-generated async form.
-   * @param {{currentEmail: string, currentPassword: string}} values
-   * @return {Promise} which may reject with an error or object containing
-   *   serverErrors.
-   */
-  function onSubmitUserTypeChange(values) {
-    return new Promise((resolve, reject) => {
-      const onSuccess = () => {
-        detachHandlers();
-        resolve();
-      };
-
-      const onFailure = (_, xhr) => {
-        const validationErrors = xhr.responseJSON;
-        let error;
-        if (validationErrors) {
-          error = {
-            serverErrors: {
-              currentEmail: validationErrors.email && validationErrors.email[0],
-              currentPassword: validationErrors.current_password && validationErrors.current_password[0],
-            }
-          };
-        } else {
-          error = new Error('Unexpected failure: ' + xhr.status);
-        }
-        detachHandlers();
-        reject(error);
-      };
-
-      // Subscribe to jquery-ujs events before we submit, and unsubscribe after
-      // the request is complete.
-      const detachHandlers = () => {
-        form.on('ajax:success', onSuccess);
-        form.on('ajax:error', onFailure);
-      };
-      form.on('ajax:success', onSuccess);
-      form.on('ajax:error', onFailure);
-      form.find('#change-user-type_user_email').val(values.currentEmail);
-      form.find('#change-user-type_user_current_password').val(values.currentPassword);
-      form.submit();
-    });
-  }
-
-  // Attach handlers
-  dropdown.change(e => onUserTypeDropdownChange(e.target.value));
-  button.click(onUserTypeChangeButtonClick);
-
-  // Set initial state
-  onUserTypeDropdownChange(initialUserType);
 }
 
 function initializeCreatePersonalAccountControls() {
