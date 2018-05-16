a3610f0f7e67898252cedcbf92bfe5c859089fe1
Caley Brock <caleybrock7@gmail.com>
2018-05-16 16:47:16 -0700

    Add email text to join section

--- apps/src/templates/manageStudents/ManageStudentsLoginInfo.jsx
+++ apps/src/templates/manageStudents/ManageStudentsLoginInfo.jsx
@@ -21,11 +21,6 @@ class ManageStudentsLoginInfo extends Component {
     const {loginType, sectionId, sectionCode} = this.props;
     return (
       <div style={styles.explination}>
-        <h2>Login type</h2>
-        <LoginTypeParagraph
-          sectionId={sectionId}
-          onLoginTypeChanged={() => window.location.reload()}
-        />
         <h2>Login information</h2>
         {(loginType === SectionLoginType.word || loginType === SectionLoginType.picture) &&
           <div>
@@ -53,8 +48,19 @@ class ManageStudentsLoginInfo extends Component {
         }
         {(loginType === SectionLoginType.email) &&
           <div>
+            <p>
+              {i18n.joinSectionAsk()}
+              <a target="_blank" href={`http://localhost-studio.code.org:3000/join/${sectionCode}`}>
+                {`http://localhost-studio.code.org:3000/join/${sectionCode}`}
+              </a>
+            </p>
           </div>
         }
+        <h2>Login type</h2>
+        <LoginTypeParagraph
+          sectionId={sectionId}
+          onLoginTypeChanged={() => window.location.reload()}
+        />
         <h2>Privacy</h2>
         <p>
           <a target="_blank" href={"/privacy/student-privacy"}>
