96e3af40227675c781d3984a209b5a47006b740b
Caley Brock <caleybrock7@gmail.com>
2018-05-15 17:03:41 -0700

    Fix sectionProgressRedux tests

--- apps/test/unit/templates/sectionProgress/sectionProgressReduxTest.js
+++ apps/test/unit/templates/sectionProgress/sectionProgressReduxTest.js
@@ -1,16 +1,15 @@
 import { assert } from '../../../util/configuredChai';
 import sectionProgress, {
   setSection,
-  setValidScripts,
   setCurrentView,
   ViewType,
-  setScriptId,
   addScriptData,
   addStudentLevelProgress,
   setLessonOfInterest,
   startLoadingProgress,
   finishLoadingProgress,
 } from '@cdo/apps/templates/sectionProgress/sectionProgressRedux';
+import {setScriptId} from '@cdo/apps/redux/scriptSelectionRedux';
 
 const fakeSectionData = {
   id: 123,
@@ -48,48 +47,6 @@ const sortedFakeSectionData = {
   }
 };
 
-const fakeValidScripts = [
-  {
-    category: 'category1',
-    category_priority: 1,
-    id: 456,
-    name: 'Script Name',
-    position: 23
-  },
-  {
-    category: 'csp',
-    category_priority: 1,
-    id: 300,
-    name: 'csp1',
-    position: 23
-  },
-  {
-    // use a different category to make sure we aren't relying on it to group
-    // units within courses.
-    category: 'other csp',
-    category_priority: 1,
-    id: 301,
-    name: 'csp2',
-    position: 23
-  },
-  // Include Express Course to use as default if needed
-  {
-    category: "CS Fundamentals",
-    category_priority: 1,
-    id: 182,
-    name: "Express Course",
-    position: 6,
-    script_name: "express",
-  },
-];
-
-const fakeValidCourses = [
-  {
-    id: 99,
-    script_ids: [300, 301]
-  }
-];
-
 const fakeScriptData789 = {
   id: 789,
   excludeCsfColumnInLegend: false,
@@ -123,16 +80,11 @@ describe('sectionProgressRedux', () => {
   const initialState = sectionProgress(undefined, {});
 
   describe('setScriptId', () => {
-    it('sets the script id', () => {
-      const action = setScriptId(130);
-      const nextState = sectionProgress(initialState, action);
-      assert.deepEqual(nextState.scriptId, 130);
-    });
-
     it('seting the script id resets the lesson of interest', () => {
       const action = setLessonOfInterest(lessonOfInterest);
       const nextState = sectionProgress(initialState, action);
 
+      // This action is from scriptSelectionRedux but affects sectionProgress
       const action2 = setScriptId(130);
       const nextState2 = sectionProgress(nextState, action2);
       assert.deepEqual(nextState2.lessonOfInterest, 1);
@@ -178,56 +130,6 @@ describe('sectionProgressRedux', () => {
       const nextState = sectionProgress({isLoadingProgress: true}, finishLoadingProgress());
       assert.deepEqual(nextState.isLoadingProgress, false);
     });
-  });
-
-  describe('setValidScripts', () => {
-
-    it('does not override already assigned scriptId', () => {
-      const studentScriptIds = [];
-      const validCourses = [];
-      const action = setValidScripts(fakeValidScripts, studentScriptIds, validCourses);
-      const nextState = sectionProgress({
-        ...initialState,
-        scriptId: 100
-      }, action);
-      assert.deepEqual(nextState.scriptId, 100);
-    });
-
-    it('includes Express Course if nothing assigned and no progress', () => {
-      const studentScriptIds = [];
-      const validCourses = [];
-      const action = setValidScripts(fakeValidScripts, studentScriptIds, validCourses);
-      const nextState = sectionProgress(initialState, action);
-      assert.deepEqual(nextState.validScripts, fakeValidScripts.filter(script => script.name === "Express Course"));
-    });
-
-    it('filters validScripts to those included in studentScriptIds', () => {
-      const studentScriptIds = [456];
-      const validCourses = [];
-      const action = setValidScripts(fakeValidScripts, studentScriptIds, validCourses);
-      const nextState = sectionProgress(initialState, action);
-      assert.deepEqual(nextState.validScripts, fakeValidScripts.filter(script => script.id === 456));
-    });
-
-    it('includes other course units when filtering validScripts', () => {
-      const studentScriptIds = [300];
-      const validCourses = fakeValidCourses;
-      const action = setValidScripts(fakeValidScripts, studentScriptIds, validCourses);
-      const nextState = sectionProgress(initialState, action);
-      const expectedScripts = [fakeValidScripts[1], fakeValidScripts[2]];
-      assert.deepEqual(expectedScripts, nextState.validScripts);
-    });
-
-    it('includes units of the assigned course when filtering validScripts', () => {
-      const studentScriptIds = [];
-      const validCourses = fakeValidCourses;
-      const assignedCourseId = 99;
-      const action = setValidScripts(fakeValidScripts, studentScriptIds, validCourses, assignedCourseId);
-      const nextState = sectionProgress(initialState, action);
-      const expectedScripts = [fakeValidScripts[1], fakeValidScripts[2]];
-      assert.deepEqual(expectedScripts, nextState.validScripts);
-    });
-
   });
 
   describe('setCurrentView', () => {
