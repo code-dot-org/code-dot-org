4965be653b2d766d5d557dbb1acab31684491401
Maddie Kasula <madelynkasula@gmail.com>
2018-05-17 16:18:44 -0700

    Add :valid_scripts as CanCan ability for all users

--- dashboard/app/models/ability.rb
+++ dashboard/app/models/ability.rb
@@ -69,7 +69,7 @@ class Ability
       can :read, Pd::Session
       can :manage, Pd::Enrollment, user_id: user.id
       can :workshops_user_enrolled_in, Pd::Workshop
-      can :index, Section, user_id: user.id
+      can [:index, :valid_scripts], Section, user_id: user.id
       can :membership, Section, followers: {student_user_id: user.id}
 
       if user.teacher?
