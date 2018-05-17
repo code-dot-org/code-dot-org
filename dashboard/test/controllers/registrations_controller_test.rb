69f14bb03a7df024357b47fb8e0b2a24c62ff684
Brad Buchanan <brad@code.org>
2018-05-17 14:23:14 -0700

    Remove opt-in logic from PUT /users

--- dashboard/test/controllers/registrations_controller_test.rb
+++ dashboard/test/controllers/registrations_controller_test.rb
@@ -642,60 +642,6 @@ class RegistrationsControllerTest < ActionController::TestCase
     assert_equal 'teacher', student.user_type
     assert_equal test_email, student.email
     assert_equal original_hashed_email, student.hashed_email
-
-    refute EmailPreference.find_by_email(test_email)
-  end
-
-  test "converting student to teacher with positive email opt-in" do
-    test_email = 'me@example.com'
-    student = create :student, email: test_email
-    sign_in student
-
-    request.headers['HTTP_ACCEPT'] = "application/json"
-    post :update, params: {
-      user: {
-        user_type: 'teacher',
-        email: test_email,
-        hashed_email: student.hashed_email,
-        email_opt_in: 'yes'
-      }
-    }
-    assert_response :success
-
-    preference = EmailPreference.find_by_email(test_email)
-    refute_nil preference
-    assert_equal true, preference.opt_in
-    assert_equal request.env['REMOTE_ADDR'], preference.ip_address
-    assert_equal EmailPreference::ACCOUNT_TYPE_CHANGE, preference.source
-    assert_equal "0", preference.form_kind
-  end
-
-  test "converting student to teacher with negative email opt-in" do
-    test_email = 'me@example.com'
-    student = create :student, email: test_email
-    sign_in student
-
-    request.headers['HTTP_ACCEPT'] = "application/json"
-    post :update, params: {
-      user: {
-        user_type: 'teacher',
-        email: test_email,
-        hashed_email: student.hashed_email,
-        email_opt_in: 'no'
-      }
-    }
-    assert_response :success
-
-    student.reload
-    assert_equal 'teacher', student.user_type
-    assert_equal test_email, student.email
-
-    preference = EmailPreference.find_by_email(test_email)
-    refute_nil preference
-    assert_equal false, preference.opt_in
-    assert_equal request.env['REMOTE_ADDR'], preference.ip_address
-    assert_equal EmailPreference::ACCOUNT_TYPE_CHANGE, preference.source
-    assert_equal "0", preference.form_kind
   end
 
   test "converting student to teacher fails when email doesn't match" do
@@ -718,27 +664,6 @@ class RegistrationsControllerTest < ActionController::TestCase
     assert_equal 'student', student.user_type
     assert_empty student.email
     assert_equal original_hashed_email, student.hashed_email
-
-    refute EmailPreference.find_by_email(test_email)
-  end
-
-  test "converting student to teacher doesn't cause email opt-in when email doesn't match" do
-    test_email = 'me@example.com'
-    student = create :student, email: test_email
-    sign_in student
-
-    request.headers['HTTP_ACCEPT'] = "application/json"
-    post :update, params: {
-      user: {
-        user_type: 'teacher',
-        email: 'wrong_email@example.com',
-        hashed_email: student.hashed_email,
-        email_opt_in: 'yes'
-      }
-    }
-    assert_response :unprocessable_entity
-
-    refute EmailPreference.find_by_email(test_email)
   end
 
   test "converting teacher to student without password succeeds" do
@@ -761,27 +686,6 @@ class RegistrationsControllerTest < ActionController::TestCase
     assert_equal 'student', teacher.user_type
     assert_empty teacher.email
     assert_equal original_hashed_email, teacher.hashed_email
-
-    refute EmailPreference.find_by_email(test_email)
-  end
-
-  test "converting teacher to student ignores email opt-in" do
-    test_email = 'me@example.com'
-    teacher = create :teacher, email: test_email
-    sign_in teacher
-
-    request.headers['HTTP_ACCEPT'] = "application/json"
-    post :update, params: {
-      user: {
-        user_type: 'student',
-        email: '',
-        hashed_email: teacher.hashed_email,
-        email_opt_in: 'yes'
-      }
-    }
-    assert_response :success
-
-    refute EmailPreference.find_by_email(test_email)
   end
 
   def can_edit_password_without_password(user)
