047c395b1d4076670ee6d538f9a28d8caf7c49ea
Mehal Shah <mehal@code.org>
2018-05-17 16:42:28 -0700

    General questions are now sourcing from JotForm

--- dashboard/app/models/concerns/pd/jot_form_backed_form.rb
+++ dashboard/app/models/concerns/pd/jot_form_backed_form.rb
@@ -1,22 +1,9 @@
-# Include in models that represent a JotForm (3rd party survey tool) form
-# This will enable syncing and data manipulation
-#
-# Required model attributes:
-#  - form_id: JotForm form id
-#  - submission_id: JotForm submission id
-#  - answers: JotForm submission answer data
 module Pd
   module JotFormBackedForm
     extend ActiveSupport::Concern
 
     included do
-      before_validation :map_answers_to_attributes, if: :answers_changed?
-
-      validates_presence_of(
-        :form_id,
-        :submission_id,
-        :answers
-      )
+      before_validation :map_form_data_to_attributes
     end
 
     class_methods do
@@ -45,39 +32,35 @@ module Pd
         end
       end
 
+      def get_form_questions(form_id)
+        survey_question = SurveyQuestion.find_by(form_id: form_id)
+        raise KeyError, "No survey questions for for form #{form_id}" unless survey_question
+        survey_question.form_questions
+      end
+
       # @return [Hash] mapping of model attributes to their respective JotForm question names,
       # from where they will be set before validation
-      # @see map_answers_to_attributes
+      # @see map_form_data_to_attributes
       def attribute_mapping
         {}
       end
 
-      # Retrieve all form ids to sync
-      # Override in included models
-      def all_form_ids
+      def form_ids
         []
       end
 
-      # Download new responses from JotForm for all relevant form ids
       def sync_all_from_jotform
-        all_form_ids.compact.map {|form_id| sync_from_jotform(form_id)}.sum
+        form_ids.map {|form_id| sync_from_jotform(form_id)}.sum
       end
 
-      def sync_questions_from_jotform(form_id)
-        Pd::SurveyQuestion.sync_from_jotform form_id
-      end
-
-      # Download new responses from JotForm
       def sync_from_jotform(form_id = nil)
         return sync_all_from_jotform unless form_id
-
-        sync_questions_from_jotform(form_id)
 
         JotForm::Translation.new(form_id).get_submissions(
           last_known_submission_id: get_last_known_submission_id(form_id),
           min_date: get_min_date(form_id)
         ).map do |submission|
-          # TODO(Andrew): don't stop the whole set when one fails
+          # TODO: don't stop the whole set when one fails
 
           answers = submission[:answers]
 
@@ -127,33 +110,24 @@ module Pd
       end
     end
 
-    # Supplies values for important model attributes from the JotForm-downloaded form answers.
-    # Run before validation.
-    # Override attribute_mapping in included models to designate attributes to map.
-    # @see attribute_mapping
-    def map_answers_to_attributes
+    def map_form_data_to_attributes
       self.class.attribute_mapping.each do |attribute, question_name|
         write_attribute attribute, form_data_hash[question_name.to_s]
       end
     end
 
-    # Get question for this form
     def questions
-      self.class.questions_for_form(form_id)
+      @questions ||= self.class.get_form_questions(form_id)
     end
 
-    # Answers json parsed in hash form
     def answers_hash
       JSON.parse answers
     end
 
-    # Form data JSON
     def form_data
       form_data_hash.to_json
     end
 
-    # Form data hash: answers data processed by up to date form questions
-    # @see FormQuestions.process_answers
     def form_data_hash
       @form_data_hash ||= begin
         questions.process_answers(answers_hash)
@@ -168,19 +142,13 @@ module Pd
     end
 
     def answers=(value)
+      @form_data_hash = nil
       write_attribute :answers, value
-      clear_memoized_values
     end
 
     def reload
-      super.tap do
-        clear_memoized_values
-      end
-    end
-
-    def clear_memoized_values
       @form_data_hash = nil
-      @sanitized_form_data_hash = nil
+      super
     end
   end
 end
