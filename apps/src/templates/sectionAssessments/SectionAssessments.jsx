681922affb40cfb2da3a15ea4b974545049ae0f7
Caley Brock <caleybrock7@gmail.com>
2018-05-17 14:16:58 -0700

    Create an experiment and render function for section assessments

--- apps/src/templates/sectionAssessments/SectionAssessments.jsx
+++ apps/src/templates/sectionAssessments/SectionAssessments.jsx
@@ -2,13 +2,16 @@ import React, {Component, PropTypes} from 'react';
 
 class SectionAssessments extends Component {
   static propTypes = {
-    section: PropTypes.number.isRequired,
+    section: PropTypes.object,
   };
 
   render() {
     return (
       <div>
         SectionAssessments will be built in react and go here.
+        <p>
+          {`The section id is ${this.props.section.id}`}
+        </p>
       </div>
     );
   }
