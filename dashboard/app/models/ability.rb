4b44a6dad2c6fef309b597f32d65797431b243ad
Maddie Kasula <madelynkasula@gmail.com>
2018-05-18 17:06:07 -0700

    Refactor ability to pluck section id

--- dashboard/app/models/ability.rb
+++ dashboard/app/models/ability.rb
@@ -70,9 +70,7 @@ class Ability
       can :manage, Pd::Enrollment, user_id: user.id
       can :workshops_user_enrolled_in, Pd::Workshop
       can :index, Section, user_id: user.id
-      can :membership, Section, Section.with_student(user) do |section|
-        section
-      end
+      can :membership, Section, id: Section.with_student(user).pluck(:id)
 
       if user.teacher?
         can :manage, Section, user_id: user.id
