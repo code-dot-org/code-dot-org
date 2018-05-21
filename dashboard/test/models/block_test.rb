8a28ec1396d9490f54b5023affa881edb0030caa
Ram Kandasamy <ram@code.org>
2018-05-21 13:59:49 -0700

    Also test without helper code

--- dashboard/test/models/block_test.rb
+++ dashboard/test/models/block_test.rb
@@ -17,4 +17,20 @@ class BlockTest < ActiveSupport::TestCase
     File.delete "#{base_path}.js"
     Dir.rmdir "config/blocks/fakeLevelType"
   end
+
+  test 'Block writes to and loads back from file without helper code' do
+    block = create :block, helper_code: nil
+    json_before = block.to_pretty_json
+    block.destroy
+    base_path = "config/blocks/#{block.level_type}/#{block.name}"
+
+    Block.load_block "#{base_path}.json"
+
+    seeded_block = Block.find_by(name: block.name)
+    assert_equal json_before, seeded_block.to_pretty_json
+
+    seeded_block.destroy
+    File.delete "#{base_path}.json"
+    Dir.rmdir "config/blocks/fakeLevelType"
+  end
 end
