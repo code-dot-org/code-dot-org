424b96c2fc1a52cf4fe6508d207604a6083e0edd
nkiru onwuneme <generalhrh@gmail.com>
2018-05-21 16:48:40 -0700

    create new component

---
+++ apps/src/templates/sectionAssessments/FreeResponsesAssessments.jsx
@@ -0,0 +1,115 @@
+import React, {Component, PropTypes} from 'react';
+import {Table, sort} from 'reactabular';
+import {tableLayoutStyles, sortableOptions} from "../tables/tableConstants";
+import i18n from '@cdo/locale';
+import wrappedSortable from '../tables/wrapped_sortable';
+import orderBy from 'lodash/orderBy';
+
+export const COLUMNS = {
+  STUDENT: 0,
+  RESPONSE: 1,
+};
+
+const studentAnswerDataPropType = PropTypes.shape({
+  id:  PropTypes.string,
+  name: PropTypes.string,
+  studentAnswers: PropTypes.array,
+});
+
+const answerDataPropType = PropTypes.shape({
+  multipleChoiceOption: PropTypes.string,
+  percentAnswered: PropTypes.number,
+  isCorrectAnswer: PropTypes.bool,
+});
+
+const questionDataPropType = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  question: PropTypes.string.isRequired,
+  answers: PropTypes.arrayOf(answerDataPropType),
+  notAnswered: PropTypes.number.isRequired,
+});
+
+class FreeResponsesAssessments extends Component {
+  static propTypes= {
+    questionAnswerData: PropTypes.arrayOf(questionDataPropType),
+    studentAnswerData: PropTypes.arrayOf(studentAnswerDataPropType)
+  };
+
+  state = {
+    [COLUMNS.NAME]: {
+      direction: 'desc',
+      position: 0
+    }
+  };
+
+  getSortingColumns = () => {
+    return this.state.sortingColumns || {};
+  };
+
+  onSort = (selectedColumn) => {
+    this.setState({
+      sortingColumns: sort.byColumn({
+        sortingColumns: this.state.sortingColumns,
+        sortingOrder: {
+          FIRST: 'asc',
+          asc: 'desc',
+          desc: 'asc',
+        },
+        selectedColumn
+      })
+    });
+  };
+
+  getColumns = (sortable) => {
+    let dataColumns = [
+      {
+        property: 'studentName',
+        header: {
+          label: i18n.studentName(),
+          props: {style: tableLayoutStyles.headerCell},
+          transforms: [sortable],
+        },
+        cell: {
+          props: {style:tableLayoutStyles.cell},
+        }
+      },
+      {
+        property: 'studentResponse',
+        header: {
+          label: i18n.response(),
+          props: {style: tableLayoutStyles.headerCell},
+        },
+        cell: {
+          // format: this.studentAnswerColumnFormatter,
+          props: {style:tableLayoutStyles.cell},
+        }
+      },
+    ];
+    return dataColumns;
+  };
+
+  render() {
+    // Define a sorting transform that can be applied to each column
+    const sortable = wrappedSortable(this.getSortingColumns, this.onSort, sortableOptions);
+    const columns = this.getColumns(sortable);
+    const sortingColumns = this.getSortingColumns();
+
+    const sortedRows = sort.sorter({
+      columns,
+      sortingColumns,
+      sort: orderBy,
+    })(this.props.questionAnswerData);
+
+    return (
+        <Table.Provider
+          columns={columns}
+          style={tableLayoutStyles.table}
+        >
+          <Table.Header />
+          <Table.Body rows={sortedRows} rowKey="id" />
+        </Table.Provider>
+    );
+  }
+}
+
+export default FreeResponsesAssessments;
