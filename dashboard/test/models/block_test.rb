084537368b6eb6a203040f7bdc47b26f02a309ac
Ram Kandasamy <ram@code.org>
2018-05-21 22:04:50 -0700

    Use file path to specify level type and block name

--- dashboard/test/models/block_test.rb
+++ dashboard/test/models/block_test.rb
@@ -3,14 +3,16 @@ require 'test_helper'
 class BlockTest < ActiveSupport::TestCase
   test 'Block writes to and loads back from file' do
     block = create :block
-    json_before = block.to_pretty_json
+    json_before = block.block_options
     block.destroy
     base_path = "config/blocks/#{block.level_type}/#{block.name}"
 
     Block.load_block "#{base_path}.json"
 
     seeded_block = Block.find_by(name: block.name)
-    assert_equal json_before, seeded_block.to_pretty_json
+    assert_equal json_before, seeded_block.block_options
+    assert_equal block.level_type, seeded_block.level_type
+    assert_equal block.helper_code, seeded_block.helper_code
 
     seeded_block.destroy
     File.delete "#{base_path}.json"
@@ -20,14 +22,16 @@ class BlockTest < ActiveSupport::TestCase
 
   test 'Block writes to and loads back from file without helper code' do
     block = create :block, helper_code: nil
-    json_before = block.to_pretty_json
+    json_before = block.block_options
     block.destroy
     base_path = "config/blocks/#{block.level_type}/#{block.name}"
 
     Block.load_block "#{base_path}.json"
 
     seeded_block = Block.find_by(name: block.name)
-    assert_equal json_before, seeded_block.to_pretty_json
+    assert_equal json_before, seeded_block.block_options
+    assert_equal block.level_type, seeded_block.level_type
+    assert_nil seeded_block.helper_code
 
     seeded_block.destroy
     File.delete "#{base_path}.json"
