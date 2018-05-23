f81fdb86b4463b738bc4e10ef5e85db1f89dc748
Ram Kandasamy <ram@code.org>
2018-05-22 17:17:54 -0700

    Support block editing

--- dashboard/app/controllers/blocks_controller.rb
+++ dashboard/app/controllers/blocks_controller.rb
@@ -1,2 +1,21 @@
 class BlocksController < ApplicationController
+  load_and_authorize_resource find_by: :name
+
+  def update
+    @block.update update_params
+
+    redirect_to(
+      edit_block_path(id: @block.name),
+      notice: 'Block saved',
+    )
+  rescue => e
+    redirect_back(
+      fallback_location: @block.name ? edit_block_path(id: @block.name) : new_block_path,
+      alert: "Error saving block: #{e}",
+    )
+  end
+
+  def update_params
+    params['block'].permit(:name, :level_type, :category, :config, :helper_code)
+  end
 end
