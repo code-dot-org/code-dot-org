6d0d9270e96ad05ac816b31ced52ca47bdfdde77
Ram Kandasamy <ram@code.org>
2018-05-24 14:30:09 -0700

    Add codemirror for block editor fields

--- apps/src/sites/studio/pages/blocks/edit.js
+++ apps/src/sites/studio/pages/blocks/edit.js
@@ -1,1 +1,9 @@
-console.log('Welcome to the block editor');
+import $ from 'jquery';
+import initializeCodeMirror, {
+  initializeCodeMirrorForJson,
+} from '@cdo/apps/code-studio/initializeCodeMirror';
+
+$(document).ready(() => {
+  initializeCodeMirrorForJson('block_config');
+  initializeCodeMirror('block_helper_code', 'javascript');
+});
