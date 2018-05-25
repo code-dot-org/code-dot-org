27f7734a98fb6cf00dfa59b6c1b5fcca201cca60
David Bailey <davidsbailey@users.noreply.github.com>
2018-05-24 17:29:27 -0700

    also serve scripts at their new_name

--- dashboard/app/models/script.rb
+++ dashboard/app/models/script.rb
@@ -346,6 +346,10 @@ class Script < ActiveRecord::Base
   end
 
   def self.get_without_cache(id_or_name)
+    # Also serve any script by its new_name, if it has one.
+    script = id_or_name && Script.find_by(new_name: id_or_name)
+    return script if script
+
     # a bit of trickery so we support both ids which are numbers and
     # names which are strings that may contain numbers (eg. 2-3)
     find_by = (id_or_name.to_i.to_s == id_or_name.to_s) ? :id : :name
