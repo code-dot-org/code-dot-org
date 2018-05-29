28d88481583e5b9110c2b08fb7f55dc30f80ff53
Brad Buchanan <brad@code.org>
2018-05-29 15:02:15 -0700

    Helper to purge all user accounts for an email address

--- dashboard/test/helpers/delete_accounts_helper_test.rb
+++ dashboard/test/helpers/delete_accounts_helper_test.rb
@@ -26,6 +26,33 @@ class DeleteAccountsHelperTest < ActionView::TestCase
     refute_nil user.purged_at
   end
 
+  test 'purges all accounts associated with email' do
+    email = 'fakeuser@example.com'
+    account1 = create :student, email: email
+    account1.destroy
+    account2 = create :teacher, email: email
+    account2.destroy
+    account3 = create :student, email: email
+
+    [account1, account2, account3].each(&:reload)
+    refute_nil account1.deleted_at
+    refute_nil account2.deleted_at
+    assert_nil account3.deleted_at
+    assert_nil account1.purged_at
+    assert_nil account2.purged_at
+    assert_nil account3.purged_at
+
+    purge_all_accounts_with_email email
+
+    [account1, account2, account3].each(&:reload)
+    refute_nil account1.deleted_at
+    refute_nil account2.deleted_at
+    refute_nil account3.deleted_at
+    refute_nil account1.purged_at
+    refute_nil account2.purged_at
+    refute_nil account3.purged_at
+  end
+
   test 'clears user.name' do
     user = create :student
     refute_nil user.name
@@ -438,5 +465,10 @@ class DeleteAccountsHelperTest < ActionView::TestCase
     SolrHelper.stubs(:delete_document).once
     DeleteAccountsHelper.new(solr: {}).purge_user(user)
     user.reload
+  end
+
+  def purge_all_accounts_with_email(email)
+    SolrHelper.stubs(:delete_document).at_least_once
+    DeleteAccountsHelper.new(solr: {}).purge_all_accounts_with_email(email)
   end
 end
