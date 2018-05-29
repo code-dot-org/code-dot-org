884e8029f72a3d1d91ce57d660378acbc9a5f413
bencodeorg <ben@code.org>
2018-05-29 15:55:44 -0700

    Add insert statement to course names table [ci skip]

--- aws/redshift/tables/course_names.sql
+++ aws/redshift/tables/course_names.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS course_names CASCADE;
+DROP TABLE IF EXISTS analysis.course_names CASCADE;
 
-CREATE TABLE course_names
+CREATE TABLE analysis.course_names
 (
    versioned_course_id    integer,
    versioned_course_name  varchar(256),
@@ -8,8 +8,14 @@ CREATE TABLE course_names
    course_name_long       varchar(256)
 );
 
-GRANT SELECT, UNKNOWN ON course_names TO group reader;
-GRANT SELECT, UNKNOWN, INSERT, DELETE, RULE, TRIGGER, REFERENCES, UPDATE ON course_names TO group admin;
-GRANT SELECT, UNKNOWN ON course_names TO group reader_pii;
+INSERT INTO analysis.course_names values
+(14,'csd-2017','csd','CS Discoveries'),
+(15,'csp-2017','csp','CS Principles'),
+(22,'csd-2018','csd','CS Discoveries'),
+(23,'csp-2018','csp','CS Principles');
+
+GRANT SELECT ON analysis.course_names TO group reader;
+GRANT SELECT, INSERT, DELETE, RULE, TRIGGER, REFERENCES, UPDATE ON analysis.course_names TO group admin;
+GRANT SELECT ON analysis.course_names TO group reader_pii;
 
 COMMIT;
