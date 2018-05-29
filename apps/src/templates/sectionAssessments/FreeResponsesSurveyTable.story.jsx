a9629c702baf04e2567899c6813eb797685cb12e
nkiru onwuneme <generalhrh@gmail.com>
2018-05-28 17:50:15 -0700

    update story and cell formatter

--- apps/src/templates/sectionAssessments/FreeResponsesSurveyTable.story.jsx
+++ apps/src/templates/sectionAssessments/FreeResponsesSurveyTable.story.jsx
@@ -6,33 +6,32 @@ const surveyOne = [
     id: 1,
     studentId: '210',
     name: 'Caley',
-    response: 'Row Row Row your boat',
+    response: 'Sea lettuce gumbo grape kale kombu cauliflower salsify kohlrabi okra sea lettuce broccoli celery lotus root carrot winter purslane turnip greens garlic.',
   },
   {
     id: 2,
     studentId: '211',
     name: 'Maddie',
-    response: 'Gently down the stream'
+    response: 'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.'
   },
   {
     id: 3,
     studentId: '212',
     name: 'Erin',
-    response: 'Merrily merrily merrily',
+    response: 'Pea horseradish azuki bean lettuce avocado asparagus okra. Kohlrabi radish okra azuki bean corn fava bean mustard tigernut jícama green bean celtuce collard greens avocado quandong fennel gumbo black-eyed pea.',
   },
   {
     id: 4,
     studentId: '213',
     name: 'Brendan',
-    response: 'Life is but a dream',
+    response: 'Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori.',
   },
   {
     id: 5,
     studentId: '214',
     name: 'Dave',
-    response: 'Ta da',
+    response: 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale.',
   },
-
 ];
 
 const surveyTwo = [
