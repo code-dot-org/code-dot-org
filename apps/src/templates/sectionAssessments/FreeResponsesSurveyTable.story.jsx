6047e1ec700beeff309fbedad24eda2e0af0423b
nkiru onwuneme <generalhrh@gmail.com>
2018-05-29 16:42:06 -0700

    move test data to assessmentsTestHelpers

--- apps/src/templates/sectionAssessments/FreeResponsesSurveyTable.story.jsx
+++ apps/src/templates/sectionAssessments/FreeResponsesSurveyTable.story.jsx
@@ -1,60 +1,6 @@
 import React from 'react';
 import FreeResponsesSurveyTable from './FreeResponsesSurveyTable';
-
-const surveyOne = [
-  {
-    id: 1,
-    studentId: '210',
-    name: 'Caley',
-    response: 'Sea lettuce gumbo grape kale kombu cauliflower salsify kohlrabi okra sea lettuce broccoli celery lotus root carrot winter purslane turnip greens garlic.',
-  },
-  {
-    id: 2,
-    studentId: '211',
-    name: 'Maddie',
-    response: 'Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.'
-  },
-  {
-    id: 3,
-    studentId: '212',
-    name: 'Erin',
-    response: 'Pea horseradish azuki bean lettuce avocado asparagus okra. Kohlrabi radish okra azuki bean corn fava bean mustard tigernut jícama green bean celtuce collard greens avocado quandong fennel gumbo black-eyed pea.',
-  },
-  {
-    id: 4,
-    studentId: '213',
-    name: 'Brendan',
-    response: 'Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori.',
-  },
-  {
-    id: 5,
-    studentId: '214',
-    name: 'Dave',
-    response: 'Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale.',
-  },
-];
-
-const surveyTwo = [
-  {
-    id: 1,
-    studentId: '210',
-    name: 'Caley',
-    response: 'In every walk with nature, one receives far more than one seeks',
-  },
-  {
-    id: 2,
-    studentId: '211',
-    name: 'Dave',
-    response: 'In every walk with nature, one receives far more than one seeks',
-  },
-  {
-    id: 3,
-    studentId: '212',
-    name: 'Erin',
-    response: 'In every walk with nature, one receives far more than one seeks',
-  },
-];
-
+import { surveyOne, surveyTwo } from './assessmentsTestHelpers';
 
 export default storybook => {
   return storybook
