33ca7beb76c883c2353fc5a70b80145905501ca6
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-05-29 14:34:37 -0700

    update i18n merge scripts to be callable OR requirable

--- bin/i18n-codeorg/lib/export-without-merge.rb
+++ bin/i18n-codeorg/lib/export-without-merge.rb
@@ -3,28 +3,26 @@
 require 'yaml'
 require 'json'
 
-file_type = ARGV[0]
-new_translation_path = ARGV[1]
-prev_translation_path = ARGV[2]
-
-# is this a new file being translated?
-if File.exist?(new_translation_path) && !File.exist?(prev_translation_path)
-  FileUtils.copy(new_translation_path, prev_translation_path)
-end
-
-# Translation begins
-if file_type == "yml"
-  new_translation = YAML.load_file(new_translation_path)
-
-  File.open(prev_translation_path, 'w+') do |f|
-    f.write(new_translation.to_yaml)
+def export_without_merge(file_type, new_translation_path, prev_translation_path)
+  # is this a new file being translated?
+  if File.exist?(new_translation_path) && !File.exist?(prev_translation_path)
+    FileUtils.copy(new_translation_path, prev_translation_path)
   end
-else
-  new_translation = JSON.parse(File.read(new_translation_path))
 
-  File.open(prev_translation_path, 'w+') do |f|
-    f.write(JSON.pretty_generate(new_translation))
+  # Translation begins
+  if file_type == "yml"
+    new_translation = YAML.load_file(new_translation_path)
+
+    File.open(prev_translation_path, 'w+') do |f|
+      f.write(new_translation.to_yaml)
+    end
+  else
+    new_translation = JSON.parse(File.read(new_translation_path))
+
+    File.open(prev_translation_path, 'w+') do |f|
+      f.write(JSON.pretty_generate(new_translation))
+    end
   end
 end
 
-puts "#{new_translation_path} => #{prev_translation_path}"
+export_without_merge(*ARGV) if __FILE__ == $0
