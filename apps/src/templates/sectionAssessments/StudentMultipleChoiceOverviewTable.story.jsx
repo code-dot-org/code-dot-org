37f63044a93406d653045f855cb67ed2c7ee47b9
nkiru onwuneme <generalhrh@gmail.com>
2018-05-30 10:10:53 -0700

    move studentOverviewData to assessmentsTestHelpers

--- apps/src/templates/sectionAssessments/StudentMultipleChoiceOverviewTable.story.jsx
+++ apps/src/templates/sectionAssessments/StudentMultipleChoiceOverviewTable.story.jsx
@@ -1,56 +1,6 @@
 import React from 'react';
 import StudentMultipleChoiceOverviewTable from './StudentMultipleChoiceOverviewTable';
-
-const studentOverviewData = [
-  {
-    id: 1,
-    name: 'Caley',
-    numMultipleChoiceCorrect: 7,
-    numMultipleChoice: 10,
-    percentCorrect: '70%',
-    submissionTimeStamp: '2/16/18 - 7:41 AM',
-  },
-  {
-    id: 2,
-    name: 'Maddie',
-    numMultipleChoiceCorrect: 3,
-    numMultipleChoice: 10,
-    percentCorrect: '',
-    submissionTimeStamp: 'In Progress',
-  },
-  {
-    id: 3,
-    name: 'Erin',
-    numMultipleChoiceCorrect: 8,
-    numMultipleChoice: 10,
-    percentCorrect: '80%',
-    submissionTimeStamp: '5/29/18 - 7:41 AM',
-  },
-  {
-    id: 4,
-    name: 'Dave',
-    numMultipleChoiceCorrect: 10,
-    numMultipleChoice: 10,
-    percentCorrect: '100%',
-    submissionTimeStamp: '5/29/18 - 8:00 AM',
-  },
-  {
-    id: 5,
-    name: 'Brad',
-    numMultipleChoiceCorrect: 0,
-    numMultipleChoice: 10,
-    percentCorrect: '',
-    submissionTimeStamp: 'Not Started',
-  },
-  {
-    id: 6,
-    name: 'Mike',
-    numMultipleChoiceCorrect: 0,
-    numMultipleChoice: 10,
-    percentCorrect: '0%',
-    submissionTimeStamp: '5/29/18 - 8:05 AM',
-  },
-];
+import {studentOverviewData} from './assessmentsTestHelpers';
 
 export default storybook => {
   return storybook
