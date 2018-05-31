2df6232258a28aa6c757a8178bbbcf8c51610cd5
David Bailey <davidsbailey@users.noreply.github.com>
2018-05-30 21:01:59 -0700

    remember section_id when redirecting from csd1 --> csd1-2017

--- dashboard/app/controllers/scripts_controller.rb
+++ dashboard/app/controllers/scripts_controller.rb
@@ -8,7 +8,9 @@ class ScriptsController < ApplicationController
 
   def show
     if @script.redirect_to?
-      redirect_to Script.get_from_cache(@script.redirect_to)
+      redirect_path = script_path(Script.get_from_cache(@script.redirect_to))
+      redirect_query_string = params[:section_id] ? "?section_id=#{params[:section_id]}" : ''
+      redirect_to "#{redirect_path}#{redirect_query_string}"
       return
     end
 
