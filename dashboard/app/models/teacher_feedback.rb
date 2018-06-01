f455ef212468c42a0c37adde555dcc9aa83918ca
Erin <erin@code.org>
2018-06-01 16:55:11 -0700

    WIP - DO NOT MERGE

--- dashboard/app/models/teacher_feedback.rb
+++ dashboard/app/models/teacher_feedback.rb
@@ -15,8 +15,18 @@
 #  index_feedback_on_student_and_level_and_section_id  (student_id,level_id,section_id)
 #
 
+#don't store sections id in model
+
 class TeacherFeedback < ApplicationRecord
-  validates :student_id, presence: true
-  validates :section_id, presence: true
-  validates :level_id, presence: true
+  validates_presence_of :student_id, :section_id, :level_id
+  belongs_to :student, class_name: 'User'
+  has_many :sections, through: :student
+  has_many :section_students, through: :sections, source: :students
+
+  def initialize(params)
+    puts "In initializer: #{params}"
+    super params
+    puts "DONE"
+    puts valid?
+  end
 end
