528973010bd799ca22437218be3e59cabb3495d3
nkiru onwuneme <generalhrh@gmail.com>
2018-06-01 16:41:42 -0700

    add multiple choice section file

---
+++ apps/src/templates/sectionAssessments/MultipleChoiceByStudentSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StudentMultipleChoiceOverviewTable from './StudentMultipleChoiceOverviewTable';
+
+
+<h2>Header</h2>
+
+
+
+
+export default StudentMultipleChoiceOverviewTable;
