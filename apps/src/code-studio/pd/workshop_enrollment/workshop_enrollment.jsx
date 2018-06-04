e31216bc4588ea917090c2362bcc955aee5bca76
Clare Constantine <clare@code.org>
2018-06-04 14:30:00 -0700

    add skeleton structure and use script data props in workshop enrollment component

--- apps/src/code-studio/pd/workshop_enrollment/workshop_enrollment.jsx
+++ apps/src/code-studio/pd/workshop_enrollment/workshop_enrollment.jsx
@@ -1,13 +1,47 @@
 /**
  * New workshop enrollment page
  */
-import React from 'react';
+import React, {PropTypes} from 'react';
 
 export default class WorkshopEnrollment extends React.Component {
+  static propTypes = {
+    workshop: PropTypes.object,
+    enrollment: PropTypes.object
+  };
 
   render() {
     return (
-      <h1>Workshop Enrollment</h1>
+      <div>
+        <h1>
+          {`Register for a ${this.props.workshop.course} workshop`}
+        </h1>
+        <p>
+          Taught by Code.org facilitators who are experienced computer science educators,
+          our workshops will prepare you to teach the Code.org curriculum.
+        </p>
+        <div className="container">
+          <div className="row">
+            {/* Left Column*/}
+            <div className ="span6">
+              <h2>Workshop Details Here</h2>
+
+              <h2>Facilitators</h2>
+
+            </div>
+            {/* Right Column*/}
+            <div className ="span6">
+              <div className="row">
+                <div className ="span6">
+                  <h2>Your Information</h2>
+
+                  <h2>Enrollment Form Here</h2>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 
