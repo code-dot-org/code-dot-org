5c9493258d4a6bdeaa36f99938d390e87d0d9ed6
Erin <erin@code.org>
2018-06-04 13:27:50 -0700

    Apply feedback changes

--- dashboard/app/controllers/api/v1/teacher_feedbacks_controller.rb
+++ dashboard/app/controllers/api/v1/teacher_feedbacks_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TeacherFeedbacksController < Api::V1::JsonApiController
-  load_and_authorize_resource
-  #authorize_resource
+  authorize_resource
+
   def show
     @teacher_feedback = TeacherFeedback.find(params[:id])
   end
@@ -8,24 +8,19 @@ class Api::V1::TeacherFeedbacksController < Api::V1::JsonApiController
   # POST /teacher_feedbacks
   # POST /teacher_feedbacks.json
   def create
-    #puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    #puts teacher_feedback_params
-    #@teacher_feedback = TeacherFeedback.new(teacher_feedback_params)
-    #puts "_______________________________________________-"
+    @teacher_feedback = TeacherFeedback.new(teacher_feedback_params)
 
-    puts "HERRRRRRRRRRRRRRRRRRRRRRRRRRREEEEEEEEEEEEEE"
     if @teacher_feedback.save
-
       head :created
     else
       head :bad_request
     end
   end
 
+  private
+
   # Never trust parameters from the scary internet, only allow the white list through.
   def teacher_feedback_params
-    puts "Params!!"
     params.require(:teacher_feedback).permit(:student_id, :level_id, :section_id, :comment)
-    puts 'HERE'
   end
 end
