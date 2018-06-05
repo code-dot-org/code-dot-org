7b8ac52c888686dd93df70e27cc36f54cf37484b
Caley Brock <caleybrock7@gmail.com>
2018-06-04 18:22:34 -0700

    Create a assessment selector for assessments tab

--- apps/src/templates/sectionAssessments/SectionAssessments.jsx
+++ apps/src/templates/sectionAssessments/SectionAssessments.jsx
@@ -2,12 +2,15 @@ import React, {Component, PropTypes} from 'react';
 import { setScriptId, validScriptPropType } from '@cdo/apps/redux/scriptSelectionRedux';
 import {
   asyncLoadAssessments,
-  getAssessmentsForCurrentScript
+  getAssessmentsForCurrentScript,
+  getAssessmentList,
+  setAssessmentId,
 } from '@cdo/apps/templates/sectionAssessments/sectionAssessmentsRedux';
 import {connect} from 'react-redux';
 import {h3Style} from "../../lib/ui/Headings";
 import i18n from '@cdo/locale';
 import ScriptSelector from '@cdo/apps/templates/sectionProgress/ScriptSelector';
+import AssessmentSelector from './AssessmentSelector';
 
 const styles = {
   header: {
@@ -22,8 +25,11 @@ class SectionAssessments extends Component {
     assessments: PropTypes.object,
     isLoadingAssessments: PropTypes.bool.isRequired,
     validScripts: PropTypes.arrayOf(validScriptPropType).isRequired,
+    assessmentList: PropTypes.array.isRequired,
     scriptId: PropTypes.number,
+    assessmentId: PropTypes.number,
     setScriptId: PropTypes.func.isRequired,
+    setAssessmentId: PropTypes.func.isRequired,
     asyncLoadAssessments: PropTypes.func.isRequired
   };
 
@@ -35,7 +41,7 @@ class SectionAssessments extends Component {
   };
 
   render() {
-    const {validScripts, scriptId} = this.props;
+    const {validScripts, scriptId, assessmentList, assessmentId} = this.props;
 
     return (
       <div>
@@ -47,6 +53,16 @@ class SectionAssessments extends Component {
             validScripts={validScripts}
             scriptId={scriptId}
             onChange={this.onChangeScript}
+          />
+        </div>
+        <div>
+          <div style={{...h3Style, ...styles.header}}>
+            Select an assessment or survey
+          </div>
+          <AssessmentSelector
+            assessmentList={assessmentList}
+            assessmentId={assessmentId}
+            onChange={this.props.setAssessmentId}
           />
         </div>
         <div>
@@ -67,12 +83,17 @@ export default connect(state => ({
   assessments: getAssessmentsForCurrentScript(state),
   isLoadingAssessments: state.sectionAssessments.isLoadingAssessments,
   validScripts: state.scriptSelection.validScripts,
+  assessmentList: getAssessmentList(state),
   scriptId: state.scriptSelection.scriptId,
+  assessmentId: state.sectionAssessments.assessmentId,
 }), dispatch => ({
   setScriptId(scriptId) {
     dispatch(setScriptId(scriptId));
   },
   asyncLoadAssessments(sectionId, scriptId) {
     return dispatch(asyncLoadAssessments(sectionId, scriptId));
-  }
+  },
+  setAssessmentId(assessmentId) {
+    dispatch(setAssessmentId(assessmentId));
+  },
 }))(SectionAssessments);
