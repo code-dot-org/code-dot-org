e2347551b351cc5a8e60ccbbb8cb870015ecfd24
Josh Lory <josh.lory@code.org>
2018-06-06 15:23:19 -0700

    Fix controller test

--- dashboard/test/controllers/shared_blockly_functions_controller_test.rb
+++ dashboard/test/controllers/shared_blockly_functions_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
-class SharedBlocklyFunctionsControllerTest < ActionDispatch::IntegrationTest
+class SharedBlocklyFunctionsControllerTest < ActionController::TestCase
+  setup do
+    @levelbuilder = create(:levelbuilder)
+  end
+
   test 'can list shared functions' do
+    Rails.application.config.stubs(:levelbuilder_mode).returns true
+    sign_in @levelbuilder
+
     get :index
     assert_response :success
   end
