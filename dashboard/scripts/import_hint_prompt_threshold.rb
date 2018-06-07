eee120951937a930ba74828c18cbb681274ae378
Josh Lory <josh.lory@code.org>
2018-06-07 14:29:54 -0700

    Report by level name, not ID

--- dashboard/scripts/import_hint_prompt_threshold.rb
+++ dashboard/scripts/import_hint_prompt_threshold.rb
@@ -5,8 +5,7 @@ require 'csv'
 # Data was generated in Redshift with the following SQL:
 #
 # select
-#   script_id,
-#   level_id,
+#   l.name,
 #   min(attempts) as percentile_75
 # from
 # (
@@ -15,7 +14,7 @@ require 'csv'
 #     ul.level_id,
 #     ul.user_id,
 #     ul.attempts,
-#   ntile(4) over(partition by script_id, level_id order by attempts asc) as quartile
+#     ntile(4) over(partition by script_id, level_id order by attempts asc) as quartile
 #   from dashboard_production.user_levels ul
 #   join dashboard_production_pii.users u on u.id = ul.user_id
 #   join dashboard_production.scripts sc on sc.id = ul.script_id
@@ -27,8 +26,9 @@ require 'csv'
 #   and ul.created_at::date >= '2016-08-01'
 #   group by 1, 2, 3, 4
 # )
+# join dashboard_production.levels l on l.id = level_id
 # where quartile = 4
-# group by 1, 2;
+# group by 1;
 
 thresholds_csv = ARGV[0]
 unless thresholds_csv
