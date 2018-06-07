de89949d2caa72c38d03582ddb1f59e7263a3f56
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-06-07 16:48:36 -0700

    update build scripts to include built version of craft from npm

--- apps/webpack.js
+++ apps/webpack.js
@@ -18,7 +18,6 @@ var toTranspileWithinNodeModules = [
   path.resolve(__dirname, 'node_modules', 'enzyme-wait'),
   path.resolve(__dirname, 'node_modules', 'json-parse-better-errors'),
   path.resolve(__dirname, 'node_modules', '@code-dot-org', 'artist'),
-  path.resolve(__dirname, 'node_modules', '@code-dot-org', 'craft'),
 ];
 
 const scssIncludePath = path.resolve(__dirname, '..', 'shared', 'css');
