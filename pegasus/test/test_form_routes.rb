00163b767378660e9f0042d7bd27eef6abfc9df1
Will Jordan <will@code.org>
2018-06-07 18:29:04 -0700

    [wip] volunteer engineer submission form solr_query rewrite

--- pegasus/test/test_form_routes.rb
+++ pegasus/test/test_form_routes.rb
@@ -1,13 +1,14 @@
 # Tests for the routes in form_routes.rb
 
-require 'minitest/autorun'
-require 'rack/test'
-require 'mocha/mini_test'
+require_relative './test_helper'
 require_relative 'fixtures/fake_dashboard'
 require_relative 'fixtures/mock_pegasus'
 require_relative 'sequel_test_case'
+require pegasus_dir 'helper_modules/forms'
 
 class FormRoutesTest < SequelTestCase
+  include SetupTest
+
   describe 'Form Routes' do
     before do
       $log.level = Logger::ERROR # Pegasus spams debug logging otherwise
@@ -27,5 +28,32 @@ class FormRoutesTest < SequelTestCase
         assert_equal 400, @pegasus.last_response.status
       end
     end
+  end
+
+  DEFAULT_DATA = {
+    email_s: 'fake@example.com',
+    name_s: 'fake_name',
+    experience_s: 'university_student_or_researcher',
+    location_s: 'somewhere',
+    location_flexibility_ss: 'onsite',
+    description_s: 'description',
+    allow_contact_b: '1',
+    age_18_plus_b: '1',
+    email_preference_opt_in_s: 'yes'
+  }.freeze
+
+  def test_volunteer_engineer_submission_2015
+    stubs(:dashboard_user).returns(nil)
+    Pegasus.stubs(:logger).returns(nil)
+    stubs(:request).returns(stub(ip: '1.2.3.4'))
+    row = insert_or_upsert_form('VolunteerEngineerSubmission2015', DEFAULT_DATA.dup)
+    @form = Form.find(id: row[:id])
+
+    @form.update(processed_data: {location_p: '-122.419416,37.774929'}.to_json)
+
+    assert_equal 0.8236209090344097,
+      VolunteerEngineerSubmission2015.solr_query(
+        'coordinates' => '-122.428760,37.774368'
+      ).order_by(:distance).last[:distance]
   end
 end
