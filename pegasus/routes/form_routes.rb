5bdb5127b2a90698b6ee22cd4d4ba66c6971ac73
Will Jordan <will@code.org>
2018-06-07 18:43:51 -0700

    [wip] working VolunteerEngineerSubmission2015 mysql query

--- pegasus/routes/form_routes.rb
+++ pegasus/routes/form_routes.rb
@@ -34,19 +34,8 @@ post '/forms/:kind/query' do |kind|
   end
   pass unless kind.respond_to?(:solr_query)
 
-  host, port = CDO.solr_server.to_s.split(':')
-  service_unavailable! if host.nil_or_empty?
-  port ||= '8983'
-
-  query = kind.solr_query(params)
-  uri = "/solr/query?#{solr_query_to_param(query)}"
-
-  request = Net::HTTP::Get.new(uri)
-  response = Net::HTTP.new(host, port).request(request)
-
-  status response.code
-  content_type response.content_type if response.content_type
-  response.body
+  content_type :json
+  kind.solr_query(params)
 end
 
 post "/forms/:kind/:secret" do |kind, secret|
