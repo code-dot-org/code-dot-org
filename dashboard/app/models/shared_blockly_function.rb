cb6c6a21bc29a9de76f01f83ec5d17d154c7e273
Ram Kandasamy <ram@code.org>
2018-06-08 14:44:44 -0700

    Read function file

--- dashboard/app/models/shared_blockly_function.rb
+++ dashboard/app/models/shared_blockly_function.rb
@@ -17,6 +17,8 @@ DEFINITION_BLOCK_TYPES = {
   behavior: 'behavior_definition',
 }
 
+BLOCK_TYPES_BY_DEFINITION_TYPE = DEFINITION_BLOCK_TYPES.invert
+
 class SharedBlocklyFunction < ApplicationRecord
   enum block_type: {
     function: 0,
@@ -45,6 +47,7 @@ class SharedBlocklyFunction < ApplicationRecord
           >
             <mutation>
               #{arguments_xml}
+              <description>#{description}</description>
             </mutation>
             <title name="NAME">#{name}</title>
             <statement name="STACK">
@@ -53,7 +56,7 @@ class SharedBlocklyFunction < ApplicationRecord
           </block>
         </xml>
       XML
-    ) {|config| config.default_xml.noblanks}
+    ) {|config| config.strict.noblanks}
     doc.root.to_xml(indent: 2)
   end
 
@@ -65,8 +68,16 @@ class SharedBlocklyFunction < ApplicationRecord
     end.join('')
   end
 
+  def self.arguments_from_xml(args_xml)
+    JSON.generate(
+      args_xml.map do |arg_xml|
+        [arg_xml.attribute('name'), arg_xml.attribute('type')]
+      end.to_h
+    )
+  end
+
   def xml_path(type=level_type, function_name=name)
-    Rails.root.join "config/shared_functions/#{type}/#{function_name}.js"
+    Rails.root.join "config/shared_functions/#{type}/#{function_name}.xml"
   end
 
   def write_file
@@ -78,5 +89,32 @@ class SharedBlocklyFunction < ApplicationRecord
   def delete_file(old_level_type=level_type, old_name=name)
     path = xml_path(old_level_type, old_name)
     File.delete path if File.exist? path
+  end
+
+  def self.load_functions
+    function_names = []
+    LevelLoader.for_each_file('config/shared_functions/**/*.xml') do |xml_path|
+      function_names << load_function(xml_path)
+    end
+  end
+
+  def self.load_function(xml_path)
+    level_type = File.basename(File.dirname(xml_path))
+    function_doc = File.open(xml_path) do |f|
+      Nokogiri.XML(f) {|config| config.strict.noblanks}
+    end
+    block_type = function_doc.xpath('/xml/block/@type').text
+    arguments = arguments_from_xml(function_doc.xpath('/xml/block/mutation/arg'))
+    description = function_doc.xpath('/xml/block/mutation/description').text
+    name = function_doc.xpath('/xml/block/title[@name="NAME"]/text()').text
+    stack = function_doc.xpath('/xml/block/statement[@name="STACK"]/*')
+
+    function = find_or_initialize_by(name: name)
+    function.level_type = level_type
+    function.block_type = BLOCK_TYPES_BY_DEFINITION_TYPE[block_type]
+    function.arguments = arguments
+    function.description = description
+    function.stack = stack
+    function.save!
   end
 end
