6c132df0ec0973afb38b2ad36843fc437340cf78
Ram Kandasamy <ram@code.org>
2018-06-07 18:12:22 -0700

    Generate XML for function definitions

--- dashboard/app/models/shared_blockly_function.rb
+++ dashboard/app/models/shared_blockly_function.rb
@@ -12,6 +12,11 @@
 #  stack       :text(65535)
 #
 
+DEFINITION_BLOCK_TYPES = {
+  function: 'procedures_defnoreturn',
+  behavior: 'behavior_definition',
+}
+
 class SharedBlocklyFunction < ApplicationRecord
   enum block_type: {
     function: 0,
@@ -24,5 +29,41 @@ class SharedBlocklyFunction < ApplicationRecord
   before_save do
     self.block_type = 'behavior'
     self.level_type = 'GameLabJr'
+  end
+
+  def file_xml
+    doc = Nokogiri.XML(
+      <<~XML
+        <xml>
+          <block
+            type="#{DEFINITION_BLOCK_TYPES[block_type.to_sym]}"
+            deletable="false"
+            movable="false"
+            editable="false"
+          >
+            <mutation>
+              #{arguments_xml}
+            </mutation>
+            <title name="NAME">#{name}</title>
+            <statement name="STACK">
+              #{stack}
+            </statement>
+          </block>
+        </xml>
+      XML
+    ) {|config| config.default_xml.noblanks}
+    doc.root.to_xml(indent: 2)
+  end
+
+  def arguments_xml
+    JSON.parse(arguments).map do |name, type|
+      <<~XML
+        <arg name="#{name}" type="#{type}"></arg>
+      XML
+    end.join('')
+  end
+
+  def xml_path(type=level_type, function_name=name)
+    Rails.root.join "config/shared_functions/#{type}/#{function_name}.js"
   end
 end
