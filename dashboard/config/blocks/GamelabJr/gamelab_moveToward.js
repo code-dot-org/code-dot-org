5970d15596f92bffa41c3103e9db91b65717190a
Continuous Integration <dev@code.org>
2018-06-11 19:00:06 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/blocks/GamelabJr/gamelab_moveToward.js
+++ dashboard/config/blocks/GamelabJr/gamelab_moveToward.js
@@ -1,4 +1,7 @@
 function moveToward(sprite,distance,target) {
+  if (!sprite || distance === undefined || !target) {
+    return;
+  }
   //The canvas coordinate system is different, hence the need to negate things
   var angleOfMovement=Math.atan2((-1*target.y+sprite.y),(-1*target.x+sprite.x));
   var dx = distance*Math.cos(angleOfMovement);
