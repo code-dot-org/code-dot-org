34aae8fbd8aef484467f3d74810b123d068817fd
Erin <erin@code.org>
2018-06-13 13:34:35 -0700

    Remove index and rename section to teacher

--- dashboard/db/migrate/20180525181121_create_teacher_feedbacks.rb
+++ dashboard/db/migrate/20180525181121_create_teacher_feedbacks.rb
@@ -4,11 +4,9 @@ class CreateTeacherFeedbacks < ActiveRecord::Migration[5.0]
       t.text :comment
       t.integer :student_id
       t.integer :level_id
-      t.integer :section_id
+      t.integer :teacher_id
 
       t.timestamps
     end
-
-    add_index :teacher_feedbacks, [:student_id, :level_id, :section_id], name: "index_feedback_on_student_and_level_and_section_id"
   end
 end
