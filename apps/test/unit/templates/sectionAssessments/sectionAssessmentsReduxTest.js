b2028703308621d97ee05397a7b311a34de79557
nkiru onwuneme <generalhrh@gmail.com>
2018-06-13 02:34:37 -0700

    modify sectionAssessments tests

--- apps/test/unit/templates/sectionAssessments/sectionAssessmentsReduxTest.js
+++ apps/test/unit/templates/sectionAssessments/sectionAssessmentsReduxTest.js
@@ -130,7 +130,7 @@ describe('sectionAssessmentsRedux', () => {
           }
         };
         const result = getMultipleChoiceStructureForCurrentAssessment(stateWithAssessment);
-        assert.deepEqual(result, [{correctAnswer: 'B', id: 456, question: 'What is a variable?'}]);
+        assert.deepEqual(result, [{correctAnswer: '', id: 456, question: 'What is a variable?'}]);
       });
     });
 
@@ -166,7 +166,7 @@ describe('sectionAssessmentsRedux', () => {
           }
         };
         const result = getStudentMCResponsesForCurrentAssessment(stateWithAssessment);
-        assert.deepEqual(result, [{id: '1', name: 'Saira', studentAnswers: [{answers: 'D', isCorrect: false}]}]);
+        assert.deepEqual(result, [{id: '1', name: 'Saira', studentResponses: [{responses: 'D', isCorrect: false}]}]);
       });
     });
   });
