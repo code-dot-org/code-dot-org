d26b0525370e3bb5c8f2ec66e8aa9bfede903d37
Mary <marybarnes37@gmail.com>
2018-06-14 12:02:09 -0700

    creates csp_d specific table to replace regional_partner_stats, relying on updated tables

---
+++ aws/redshift/tables/regional_partner_stats_csp_csd.sql
@@ -0,0 +1,130 @@
+--CHANGES
+-- addition of school years beyond 2017
+-- removed 'location'
+
+-- CHANGED DEPENDENCIES
+-- before using this code to update analysis.regional_partner_stats_csp_csd, certain other tables need to have their names changed, and their references updated here
+-- similarly, the tables analysis.teacher_most_progress and analysis.student_activity need to be updated and then their references in this file replaced with the originals
+-- these two also need to be updated on Github
+
+-- NOTES ON HOW TO UPDATE 
+-- after updating at the dependencies listed above and noted with 'PUBLIC' in the code....
+-- replace all instances of 'public.regional_partner_stats_csp_csd_test' with 'analysis.regional_partner_stats_csp_csd' and re-run table creation
+-- this table will be generated daily from code hosted on github, so need to talk to Ben about how to update that process
+
+;
+drop table if exists analysis.regional_partner_stats_csp_csd;
+
+create table analysis.regional_partner_stats_csp_csd 
+AS
+with completed as
+(
+  select 
+    studio_person_id,
+    case when cc.course_name = 'csp' then 'CS Principles'
+         when cc.course_name in ('csd','csd_half') then 'CS Discoveries'
+    end as course,
+    school_year
+  from csp_csd_completed_teachers cc
+  join users u on u.id = cc.user_id
+), 
+started as
+(
+  select 
+    studio_person_id,
+    case when cc.course_name = 'csp' then 'CS Principles'
+         when cc.course_name in ('csd','csd_half') then 'CS Discoveries'
+    end as course,
+    school_year
+  from csp_csd_started_teachers cc
+  join users u on u.id = cc.user_id
+),
+pd_enrollments_with_year as
+  ( select pd_workshop_id, first_name, last_name, email, user_id, school_year
+      from dashboard_production_pii.pd_enrollments pde
+      join dashboard_production_pii.pd_workshops pw
+      on pde.pd_workshop_id = pw.id
+      join analysis.school_years sy 
+      on pw.started_at between sy.started_at and sy.ended_at
+      and pw.deleted_at is null and pde.deleted_at is null
+  )
+  SELECT distinct 
+         u.id as user_id,
+         d.studio_person_id,
+         FIRST_VALUE(pde.first_name) OVER (PARTITION BY d.studio_person_id  ORDER BY (CASE WHEN  pde.first_name IS NULL THEN 2 ELSE 1 END), pde.pd_workshop_id ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING ) as first_name,
+         FIRST_VALUE(pde.last_name) OVER (PARTITION BY d.studio_person_id ORDER BY (CASE WHEN  pde.last_name IS NULL THEN 2 ELSE 1 END), pde.pd_workshop_id ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING ) as last_name,
+         FIRST_VALUE(pde.email) OVER (PARTITION BY d.studio_person_id ORDER BY (CASE WHEN  pde.email IS NULL THEN 2 ELSE 1 END), pde.pd_workshop_id ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING ) as email,
+         d.course,
+         d.location,
+         d.school_year as school_year_trained,
+         s.school_year as school_year_taught,
+         rp.name as regional_partner_name,
+         rp.id as regional_partner_id,
+         coalesce(d.school_id, ss_user.school_id) school_id,
+         coalesce(ss_summer_pd.school_name, ss_user.school_name) school_name,
+         coalesce(ss_summer_pd.city, ss_user.city) city,
+         coalesce(ss_summer_pd.state, ss_user.state) state,
+         coalesce(ss_summer_pd.school_district_name, ss_user.school_district_name) school_district_name,
+         coalesce(ss_summer_pd.school_district_id, ss_user.school_district_id) school_district_id,
+         coalesce(ss_summer_pd.high_needs, ss_user.high_needs) high_needs,
+         coalesce(ss_summer_pd.rural, ss_user.rural) rural,
+         qwa.q1,
+         qwa.q2,
+         qwa.q3,
+         qwa.q4,
+         case when (q1 + q2 + q3 + q4) >= 3 then 1 else 0 end as retained,
+         case when c.studio_person_id is not null then 1 else 0 end as completed,
+         case when s.studio_person_id is not null then 1 else 0 end as started,
+         tmp.script_most_progress,
+         tmp.students_script_most_progress,
+         sa.sections,
+         sa.students,
+         sa.students_female,
+         sa.students_gender,
+         sa.students_urm,
+         sa.students_black,
+         sa.students_hispanic,
+         sa.students_native,
+         sa.students_hawaiian,
+         sa.students_race
+  FROM analysis.csp_csd_teachers_trained d
+  LEFT JOIN dashboard_production_pii.users u 
+         ON d.studio_person_id = u.studio_person_id
+  -- schools
+  LEFT JOIN analysis.school_stats ss_summer_pd 
+         ON ss_summer_pd.school_id = d.school_id
+  LEFT JOIN dashboard_production.school_infos si_user
+         ON si_user.id = u.school_info_id
+  LEFT JOIN analysis.school_stats ss_user
+         ON ss_user.school_id = si_user.school_id
+  -- attendance
+  LEFT JOIN analysis.quarterly_workshop_attendance qwa 
+         ON qwa.studio_person_id = d.studio_person_id
+        AND qwa.course = d.course -- only include attendance at the workshop for which you were trained (?? not sure what this note from Ben meant)
+        AND qwa.school_year = d.school_year
+--pii tables (regional partner names, person names, emails, locations)
+  LEFT JOIN dashboard_production_pii.regional_partners rp  
+       ON d.regional_partner_id = rp.id 
+  LEFT JOIN pd_enrollments_with_year pde 
+        ON pde.user_id = u.id
+        AND pde.school_year = d.school_year 
+-- analysis tables
+ LEFT JOIN started s
+       ON s.studio_person_id = u.studio_person_id
+      AND s.course = d.course
+      AND s.school_year >= d.school_year
+  LEFT JOIN completed c
+         ON c.studio_person_id = u.studio_person_id
+        AND c.course = d.course   
+        AND c.school_year  = s.school_year  
+  LEFT JOIN analysis.teacher_most_progress_csp_csd tmp 
+         ON tmp.studio_person_id = d.studio_person_id
+         AND tmp.school_year = s.school_year
+  LEFT JOIN analysis.student_activity_csp_csd sa 
+         ON sa.studio_person_id = d.studio_person_id 
+         AND sa.school_year = s.school_year
+;
+
+GRANT ALL PRIVILEGES ON analysis.regional_partner_stats_csp_csd_test TO GROUP admin;
+GRANT SELECT ON analysis.regional_partner_stats_csp_csd_ TO GROUP reader_pii;
+
