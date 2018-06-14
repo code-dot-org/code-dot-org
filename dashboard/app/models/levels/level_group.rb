15b49a550c94ffe931b61ddfcd80e1e1b6df1626
Caley Brock <caleybrock7@gmail.com>
2018-06-14 11:06:00 -0700

    Update model tests

--- dashboard/app/models/levels/level_group.rb
+++ dashboard/app/models/levels/level_group.rb
@@ -266,8 +266,6 @@ ruby
   #           {result: ""},
   #           {result: "I like making games, and I also like the lifestyle."},
   #           {}]}]}]
-  #
-  # TODO(caleybrock): add tests.
   def self.get_summarized_survey_results(script, section)
     level_group_script_levels = script.script_levels.includes(:levels).where('levels.type' => 'LevelGroup')
 
