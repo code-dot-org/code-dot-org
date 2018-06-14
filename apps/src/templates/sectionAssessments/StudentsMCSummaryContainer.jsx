0baf94a5464d5c7b2996f5b5096c739bb175705f
nkiru onwuneme <generalhrh@gmail.com>
2018-06-14 10:39:39 -0700

    add comment for temporary placeholder for header

--- apps/src/templates/sectionAssessments/StudentsMCSummaryContainer.jsx
+++ apps/src/templates/sectionAssessments/StudentsMCSummaryContainer.jsx
@@ -13,6 +13,7 @@ class StudentsMCSummaryContainer extends Component {
   render() {
     return (
       <div>
+        {/* Temporary placeholder for the header of the table */}
         <h1>Students MC Summary Container </h1>
         <StudentsAssessmentsMCTable
           studentOverviewData={this.props.studentOverviewData}
