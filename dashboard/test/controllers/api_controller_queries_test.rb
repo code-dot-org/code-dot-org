74ffbce9bc9d81ee7e6bc0973675ef33e4add94f
Brad Buchanan <brad@code.org>
2018-06-15 14:47:03 -0700

    Make a request before we check query count

--- dashboard/test/controllers/api_controller_queries_test.rb
+++ dashboard/test/controllers/api_controller_queries_test.rb
@@ -8,16 +8,25 @@ class ApiControllerQueriesTest < ActionDispatch::IntegrationTest
   test "section_level_progress" do
     section = create(:section)
     50.times {|_| section.students << create(:student)}
-
-    sign_in section.teacher
+    sign_in_as section.teacher
 
     script = Script.find_by_name('algebra')
-    assert_queries 12 do
+    assert_queries 9 do
       get '/dashboardapi/section_level_progress', params: {
         section_id: section.id,
         script_id: script.id
       }
     end
     assert_response :success
+  end
+
+  private
+
+  def sign_in_as(user)
+    sign_in user
+    # Required become some queries are triggered on the first IntegrationTest
+    # request after a user signs in, and we don't want them to be counted in
+    # our tests.
+    get '/home'
   end
 end
