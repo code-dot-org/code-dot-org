e073e8b1c4845759f394fcd490b68197dda0de23
Caley Brock <caleybrock7@gmail.com>
2018-06-15 14:21:51 -0700

    Code review feedback and test update

--- dashboard/app/controllers/api/v1/assessments_controller.rb
+++ dashboard/app/controllers/api/v1/assessments_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AssessmentsController < Api::V1::JsonApiController
   include LevelsHelper
 
   before_filter :load_from_cache
-  load_and_authorize_resource :section
+  load_and_authorize_resource :section, only: [:section_responses, :section_surveys]
   load_and_authorize_resource :script
 
   def load_from_cache
