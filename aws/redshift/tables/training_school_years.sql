c7b60f0f44d6b5899539a4bdc15b5ac7c8b2a0d1
Mary <marybarnes37@gmail.com>
2018-06-15 09:56:47 -0700

    fixed training school years to start and end on 6/1

--- aws/redshift/tables/training_school_years.sql
+++ aws/redshift/tables/training_school_years.sql
@@ -13,10 +13,12 @@ GRANT SELECT, INSERT, DELETE, RULE, TRIGGER, REFERENCES, UPDATE ON analysis.trai
 GRANT INSERT, TRIGGER, UPDATE, REFERENCES, RULE, DELETE, SELECT ON analysis.training_school_years TO dev;
 GRANT SELECT ON analysis.training_school_years TO group reader_pii;
 
-INSERT INTO analysis.training_school_years VALUES ('2013-14',	'2013',	'2013-06-01',	'2014-05-31');
-INSERT INTO analysis.training_school_years VALUES ('2014-15',	'2014',	'2014-06-01',	'2015-05-31');
-INSERT INTO analysis.training_school_years VALUES ('2015-16',	'2015',	'2015-06-01',	'2016-05-31');
-INSERT INTO analysis.training_school_years VALUES ('2016-17',	'2016',	'2016-06-01',	'2017-05-31');
-INSERT INTO analysis.training_school_years VALUES ('2017-18',	'2017',	'2017-06-01',	'2018-05-31');
-INSERT INTO analysis.training_school_years VALUES ('2018-19',	'2018',	'2018-06-01',	'2019-05-31');
-INSERT INTO analysis.training_school_years VALUES ('2019-20',	'2019',	'2019-06-01',	'2020-05-31');
+INSERT INTO analysis.training_school_years VALUES ('2013-14',	'2013',	'2013-06-01',	'2014-06-01');
+INSERT INTO analysis.training_school_years VALUES ('2014-15',	'2014',	'2014-06-01',	'2015-06-01');
+INSERT INTO analysis.training_school_years VALUES ('2015-16',	'2015',	'2015-06-01',	'2016-06-01');
+INSERT INTO analysis.training_school_years VALUES ('2016-17',	'2016',	'2016-06-01',	'2017-06-01');
+INSERT INTO analysis.training_school_years VALUES ('2017-18',	'2017',	'2017-06-01',	'2018-06-01');
+INSERT INTO analysis.training_school_years VALUES ('2018-19',	'2018',	'2018-06-01',	'2019-06-01');
+INSERT INTO analysis.training_school_years VALUES ('2019-20',	'2019',	'2019-06-01',	'2020-06-01');
+
+select * from training_school_years;
