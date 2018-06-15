53bab16bdb99e9fbe29b3b7bdcc7b2b477f7a1d1
Brad Buchanan <brad@code.org>
2018-06-15 16:03:46 -0700

    PairedUserLevel.pairs_by_user

--- dashboard/app/models/paired_user_level.rb
+++ dashboard/app/models/paired_user_level.rb
@@ -30,4 +30,34 @@ class PairedUserLevel < ActiveRecord::Base
       pluck(:navigator_user_level_id)
     return drivers | navigators
   end
+
+  # With just a pair (ha!) of queries, retrieve the set of paired UserLevel ids
+  # for a group of users of arbitrary size.
+  # @param [Enumerable<User>] users
+  # @return [Hash<Integer, Set<Integer>>] Map of User ids to sets of UserLevel ids
+  # Example response, given 1, 2, 3 are user ids and 101, 102, 103 are userlevel ids:
+  # {
+  #   1 => Set[101, 102],
+  #   2 => Set[102],
+  #   3 => Set[]
+  # }
+  def self.pairs_by_user(users)
+    initial_hash = Hash[users.map {|user| [user.id, Set.new]}]
+    user_ids = users.map(&:id)
+    drivers = PairedUserLevel.
+      joins(:driver_user_level).
+      where(user_levels: {user_id: user_ids}).
+      pluck('user_levels.user_id', 'user_levels.id')
+    navigators = PairedUserLevel.
+      joins(:navigator_user_level).
+      where(user_levels: {user_id: user_ids}).
+      pluck('user_levels.user_id', 'user_levels.id')
+    drivers.
+      concat(navigators).
+      inject(initial_hash) do |memo, (user_id, user_level_id)|
+        memo[user_id] ||= Set.new
+        memo[user_id].add user_level_id
+        memo
+      end
+  end
 end
