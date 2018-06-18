3cedd8cd773f0e38bcd393dc3c264f58e3bbef20
Maddie Kasula <madelynkasula@gmail.com>
2018-06-17 17:45:34 -0700

    Revert "Only require user password in ChangeEmailForm if user has code.org password"
    This reverts commit ab896e661a0a1c84b87a11ae988c8e3cf236e53e.

--- apps/src/lib/ui/accounts/ChangeEmailController.js
+++ apps/src/lib/ui/accounts/ChangeEmailController.js
@@ -28,13 +28,12 @@ export default class ChangeEmailController {
    * @param {string} userType
    * @param {function(newEmail:string, newHashedEmail:string, emailOptIn:string)} emailChangedCallback
    */
-  constructor({form, link, displayedUserEmail, userAge, userType, hasPassword,
+  constructor({form, link, displayedUserEmail, userAge, userType,
     emailChangedCallback}) {
     this.form = form;
     this.displayedUserEmail = displayedUserEmail;
     this.userAge = userAge;
     this.userType = userType;
-    this.hasPassword = hasPassword;
     this.emailChangedCallback = emailChangedCallback;
     link.click(this.showChangeEmailModal);
   }
@@ -57,7 +56,6 @@ export default class ChangeEmailController {
         handleSubmit={handleSubmit}
         handleCancel={this.hideChangeEmailModal}
         userType={this.userType}
-        hasPassword={this.hasPassword}
         currentHashedEmail={userHashedEmail}
       />,
       this.mountPoint
