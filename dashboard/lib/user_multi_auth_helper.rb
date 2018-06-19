d75dbb4398e6656cb9d1ec1fe740cab01090be8a
Brad Buchanan <brad@code.org>
2018-06-19 15:58:49 -0700

    Convert email+password student

--- dashboard/lib/user_multi_auth_helper.rb
+++ dashboard/lib/user_multi_auth_helper.rb
@@ -3,17 +3,27 @@ module UserMultiAuthHelper
     return true if migrated?
 
     unless sponsored?
-      self.primary_authentication_option = AuthenticationOption.new(
-        user: self,
-        email: email,
-        credential_type: provider,
-        authentication_id: uid,
-        data: {
-          oauth_token: oauth_token,
-          oauth_token_expiration: oauth_token_expiration,
-          oauth_refresh_token: oauth_refresh_token
-        }.to_json
-      )
+      self.primary_authentication_option =
+        if provider == 'google_oauth2'
+          AuthenticationOption.new(
+            user: self,
+            email: email,
+            credential_type: provider,
+            authentication_id: uid,
+            data: {
+              oauth_token: oauth_token,
+              oauth_token_expiration: oauth_token_expiration,
+              oauth_refresh_token: oauth_refresh_token
+            }.to_json
+          )
+        else
+          AuthenticationOption.new(
+            user: self,
+            email: email,
+            hashed_email: hashed_email,
+            credential_type: 'email',
+          )
+        end
     end
     self.provider = 'migrated'
     save
