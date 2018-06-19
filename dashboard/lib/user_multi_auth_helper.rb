7630919308bfd6e36f517a775d6f1765217b18d5
Brad Buchanan <brad@code.org>
2018-06-19 14:00:27 -0700

    Migrate word and picture users

--- dashboard/lib/user_multi_auth_helper.rb
+++ dashboard/lib/user_multi_auth_helper.rb
@@ -1,17 +1,20 @@
 module UserMultiAuthHelper
   def migrate_to_multi_auth
-    return if provider == 'migrated'
-    self.primary_authentication_option = AuthenticationOption.new(
-      user: self,
-      email: email,
-      credential_type: provider,
-      authentication_id: uid,
-      data: {
-        oauth_token: oauth_token,
-        oauth_token_expiration: oauth_token_expiration,
-        oauth_refresh_token: oauth_refresh_token
-      }.to_json
-    )
+    return true if migrated?
+
+    unless sponsored?
+      self.primary_authentication_option = AuthenticationOption.new(
+        user: self,
+        email: email,
+        credential_type: provider,
+        authentication_id: uid,
+        data: {
+          oauth_token: oauth_token,
+          oauth_token_expiration: oauth_token_expiration,
+          oauth_refresh_token: oauth_refresh_token
+        }.to_json
+      )
+    end
     self.provider = 'migrated'
     save
   end
