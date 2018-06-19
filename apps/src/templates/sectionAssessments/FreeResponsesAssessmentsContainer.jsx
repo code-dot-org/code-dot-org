23331afc9a948fcdebbf9add43c44834f0195a49
nkiru onwuneme <generalhrh@gmail.com>
2018-06-19 14:32:22 -0700

    Modify free responses container to display free response questions.

--- apps/src/templates/sectionAssessments/FreeResponsesAssessmentsContainer.jsx
+++ apps/src/templates/sectionAssessments/FreeResponsesAssessmentsContainer.jsx
@@ -1,36 +1,27 @@
 import React, {Component, PropTypes} from 'react';
 import FreeResponsesAssessmentsTable from './FreeResponsesAssessmentsTable';
 import {
-  getStudentFreeResponsesAssessmentsAnswers,
   getFreeResponsesAssessmentsQuestions,
 } from './sectionAssessmentsRedux';
 import { connect } from 'react-redux';
 
-const freeResponseQuestionsPropType = PropTypes.shape({
-  questionText: PropTypes.string,
-});
-
-const freeResponsesByStudentResponsesPropType = PropTypes.shape({
-  id:  PropTypes.number,
-  name: PropTypes.string.isRequired,
-  response: PropTypes.string,
-});
-
 class FreeResponsesAssessmentsContainer extends Component {
   static propTypes= {
-    freeResponsesByStudentResponses: PropTypes.arrayOf(freeResponsesByStudentResponsesPropType),
-    freeResponseQuestions: PropTypes.arrayOf(PropTypes.string),
+    freeResponseQuestions: PropTypes.array,
   };
 
   render() {
-    const {freeResponsesByStudentResponses, freeResponseQuestions} = this.props;
-    console.log('output', freeResponseQuestions);
+    const {freeResponseQuestions} = this.props;
+
     return (
       <div>
         <h2>Free responses for Assessments</h2>
-        {freeResponseQuestions.map((questionText, index) => (
+        {freeResponseQuestions.map((question, index) => (
           <div key={index}>
-            <h3>{questionText}</h3>
+            <h3>{question.questionText}</h3>
+            <FreeResponsesAssessmentsTable
+              freeResponses={question.responses}
+            />
           </div>
         ))}
       </div>
@@ -41,7 +32,6 @@ class FreeResponsesAssessmentsContainer extends Component {
 export const UnconnectedFreeResponsesAssessmentsContainer = FreeResponsesAssessmentsContainer;
 
 export default connect(state => ({
-  freeResponsesByStudentResponses: getStudentFreeResponsesAssessmentsAnswers(state),
   freeResponseQuestions: getFreeResponsesAssessmentsQuestions(state),
 }))(FreeResponsesAssessmentsContainer);
 
