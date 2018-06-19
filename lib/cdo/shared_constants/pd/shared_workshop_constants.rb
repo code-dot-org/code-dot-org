136cec23bc3b04db29292350d333f81c6215ae95
Tanya Parker <tanyacparker@gmail.com>
2018-06-19 13:20:53 -0700

    add CSF 201 deep dive workshop, but hidden in workshop dashboard UI

--- lib/cdo/shared_constants/pd/shared_workshop_constants.rb
+++ lib/cdo/shared_constants/pd/shared_workshop_constants.rb
@@ -63,6 +63,7 @@ module Pd
       ],
       COURSE_CSF => [
         SUBJECT_CSF_101 = 'Intro Workshop'.freeze,
+        SUBJECT_CSF_201 = 'Deep Dive Workshop'.freeze,
         SUBJECT_CSF_FIT = SUBJECT_FIT
       ]
     }.freeze
