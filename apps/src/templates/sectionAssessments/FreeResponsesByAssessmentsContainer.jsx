6c0bd60caacec9baed76cc591b70b2af0912c91a
nkiru onwuneme <generalhrh@gmail.com>
2018-06-19 12:30:18 -0700

    display student name in free responses assessments table

--- apps/src/templates/sectionAssessments/FreeResponsesByAssessmentsContainer.jsx
+++ apps/src/templates/sectionAssessments/FreeResponsesByAssessmentsContainer.jsx
@@ -1,18 +1,45 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import FreeResponsesAssessmentsTable from './FreeResponsesAssessmentsTable';
+import {
+  getStudentFreeResponsesAssessmentsAnswers,
+  getFreeResponsesAssessmentsQuestions,
+} from './sectionAssessmentsRedux';
+import { connect } from 'react-redux';
+
+const freeResponseQuestionsPropType = PropTypes.shape({
+  questionText: PropTypes.string,
+});
+
+const freeResponsesByStudentResponsesPropType = PropTypes.shape({
+  id:  PropTypes.number,
+  name: PropTypes.string.isRequired,
+  response: PropTypes.string,
+});
 
 class FreeResponsesByAssessmentsContainer extends Component {
+  static propTypes= {
+    freeResponsesByStudentResponses: PropTypes.arrayOf(freeResponsesByStudentResponsesPropType),
+    freeResponsesByQuestions: PropTypes.arrayOf(freeResponseQuestionsPropType),
+  };
+
   render() {
+    const {freeResponsesByStudentResponses} = this.props;
+
     return (
       <div>
-        <h3>Free Responses Assessments Table</h3>
-        <FreeResponsesAssessmentsTable
-          freeResponses={[]}
-        />
-        </div>
-
+        <h2>Free responses for Assessments</h2>
+          <FreeResponsesAssessmentsTable
+            freeResponses={freeResponsesByStudentResponses}
+          />
+      </div>
     );
   }
 }
 
-export default FreeResponsesByAssessmentsContainer;
+export const UnconnectedFreeResponsesByAssessmentsContainer = FreeResponsesByAssessmentsContainer;
+
+export default connect(state => ({
+  freeResponsesByStudentResponses: getStudentFreeResponsesAssessmentsAnswers(state),
+  freeResponsesByQuestions: getFreeResponsesAssessmentsQuestions(state),
+}))(FreeResponsesByAssessmentsContainer);
+
