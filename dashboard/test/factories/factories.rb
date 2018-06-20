ad406b012bd151320a89485309e5e69df95d1ed0
Brad Buchanan <brad@code.org>
2018-06-20 10:49:44 -0700

    Refactor optionless tests for readability

--- dashboard/test/factories/factories.rb
+++ dashboard/test/factories/factories.rb
@@ -212,6 +212,12 @@ FactoryGirl.define do
         end
       end
 
+      factory :manual_username_password_student do
+        email nil
+        hashed_email nil
+        provider User::PROVIDER_MANUAL
+      end
+
       factory :student_in_word_section do
         encrypted_password nil
         provider 'sponsored'
