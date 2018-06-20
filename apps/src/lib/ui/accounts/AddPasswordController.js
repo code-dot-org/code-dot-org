1a300ef3ad5eed2e00ced1417408a333e61586cb
Maddie Kasula <madelynkasula@gmail.com>
2018-06-20 10:14:00 -0700

    Add call to server and basic success/failure handling

--- apps/src/lib/ui/accounts/AddPasswordController.js
+++ apps/src/lib/ui/accounts/AddPasswordController.js
@@ -3,7 +3,53 @@ import ReactDOM from 'react-dom';
 import AddPasswordForm from './AddPasswordForm';
 
 export default class AddPasswordController {
-  constructor(mountPoint) {
-    ReactDOM.render(<AddPasswordForm/>, mountPoint);
+  constructor(form, mountPoint) {
+    this.form = form;
+    this.renderAddPasswordForm(mountPoint);
   }
+
+  renderAddPasswordForm = (mountPoint) => {
+    ReactDOM.render(
+      <AddPasswordForm handleSubmit={this.submitAddPassword}/>,
+      mountPoint
+    );
+  };
+
+  submitAddPassword = (password, passwordConfirmation) => {
+    return new Promise((resolve, reject) => {
+      const onSuccess = () => {
+        detachHandlers();
+        resolve();
+      };
+
+      const onFailure = (_, xhr) => {
+        // const validationErrors = xhr.responseJSON;
+        // let error;
+        // if (validationErrors) {
+        //   error = {
+        //     serverErrors: {
+        //       newEmail: validationErrors.email && validationErrors.email[0],
+        //       currentPassword: validationErrors.current_password && validationErrors.current_password[0],
+        //     }
+        //   };
+        // } else {
+        //   error = new Error('Unexpected failure: ' + xhr.status);
+        // }
+        detachHandlers();
+        reject(xhr.responseJSON);
+      };
+
+      // Subscribe to jquery-ujs events before we submit, and unsubscribe after
+      // the request is complete.
+      const detachHandlers = () => {
+        this.form.on('ajax:success', onSuccess);
+        this.form.on('ajax:error', onFailure);
+      };
+      this.form.on('ajax:success', onSuccess);
+      this.form.on('ajax:error', onFailure);
+      this.form.find('#add-password_user_password').val(password);
+      this.form.find('#add-password_user_password_confirmation').val(passwordConfirmation);
+      this.form.submit();
+    });
+  };
 }
