84054b168ce194819bfd56c825dcdc7e4e5b9822
Erin <erin@code.org>
2018-06-20 16:31:21 -0700

    PR Feedback - syntax cleanup

--- dashboard/app/controllers/api/v1/teacher_feedbacks_controller.rb
+++ dashboard/app/controllers/api/v1/teacher_feedbacks_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::TeacherFeedbacksController < Api::V1::JsonApiController
-  load_and_authorize_resource
+  authorize_resource
+  load_resource only: :create
 
   # Use student_id, level_id, and teacher_id to lookup the feedback for a student on a particular level and provide the
   # most recent feedback left by that teacher
   def show_feedback_from_teacher
-    @feedback = TeacherFeedback.where(student_id: params[:student_id], level_id: params[:level_id], teacher_id: params[:teacher_id]).last
+    @feedback = TeacherFeedback.where(
+      student_id: params[:student_id],
+      level_id: params[:level_id],
+      teacher_id: params[:teacher_id]
+    ).order('id DESC').first
     render json: @feedback
   end
 
