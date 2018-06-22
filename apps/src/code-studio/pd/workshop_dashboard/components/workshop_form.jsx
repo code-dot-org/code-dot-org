7c633767bada5da8a91a21b89639587ea0153327
Tanya Parker <tanyacparker@gmail.com>
2018-06-21 17:19:37 -0700

    code review: rename CSF Intro Workshop to Intro

--- apps/src/code-studio/pd/workshop_dashboard/components/workshop_form.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/workshop_form.jsx
@@ -504,12 +504,13 @@ export class WorkshopForm extends React.Component {
 
   renderSubjectSelect(validation) {
     if (this.shouldRenderSubject()) {
-      const options = Subjects[this.state.course].map((subject, i) => {
-        // Temporarily hiding CSF 201 subject from workshop dashboard UI
-        // until the pilot is over in winter 2018
-        if (subject !== "Deep Dive Workshop") {
-          return (<option key={i} value={subject}>{subject}</option>);
-        }
+      // TODO(tanya): Show 201 subject in workshop dashboard UI
+      // Temporarily hiding CSF 201 subject from the workshop dashboard
+      // until the pilot is over in winter 2018
+      const included_subjects = Subjects[this.state.course]
+        .filter(subject => subject !== "Deep Dive Workshop");
+      const options = included_subjects.map((subject, i) => {
+        return (<option key={i} value={subject}>{subject}</option>);
       });
       const placeHolder = this.state.subject ? null : <option />;
       return (
