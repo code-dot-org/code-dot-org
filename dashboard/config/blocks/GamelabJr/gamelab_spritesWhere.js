4a6da04b2ef492015f5026d1fb5e70c309b5e9eb
Continuous Integration <dev@code.org>
2018-06-22 19:00:05 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/blocks/GamelabJr/gamelab_spritesWhere.js
+++ dashboard/config/blocks/GamelabJr/gamelab_spritesWhere.js
@@ -1,5 +1,6 @@
 function spritesWhere(property, value) {
 	var group = createGroup();
+
     if (property == "scale") { value/=100; }
 	for (var i=0; i < sprites.length; i++) {
 		var sprite = sprites[i];
