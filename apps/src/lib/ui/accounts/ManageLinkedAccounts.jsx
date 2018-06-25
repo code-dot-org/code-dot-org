ef1e2b8a04e4dd5c4e6c8f70bd304c547c2fc3ae
Brad Buchanan <brad@code.org>
2018-06-25 14:58:55 -0700

    Basic table UI

--- apps/src/lib/ui/accounts/ManageLinkedAccounts.jsx
+++ apps/src/lib/ui/accounts/ManageLinkedAccounts.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import i18n from '@cdo/locale';
+import Button from "../../../templates/Button";
 
 export default class ManageLinkedAccounts extends React.Component {
   render() {
@@ -9,7 +10,60 @@ export default class ManageLinkedAccounts extends React.Component {
         <h2 style={styles.header}>
           {i18n.manageLinkedAccounts()}
         </h2>
+        <table>
+          <thead>
+            <tr>
+              <th>{i18n.manageLinkedAccounts_loginType()}</th>
+              <th>{i18n.manageLinkedAccounts_emailAddress()}</th>
+              <th>{i18n.manageLinkedAccounts_actions()}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <GoogleRow
+              email={'brad@code.org'}
+            />
+            <GoogleRow
+              email={'*** encrypted ***'}
+            />
+            <GoogleRow
+              email={undefined}
+            />
+            <GoogleRow
+              email={'brad@code.org'}
+              cannotDisconnect
+            />
+          </tbody>
+        </table>
       </div>
+    );
+  }
+}
+
+class GoogleRow extends React.Component {
+  static propTypes = {
+    email: PropTypes.string,
+    cannotDisconnect: PropTypes.bool
+  };
+
+  render() {
+    const {email, cannotDisconnect} = this.props;
+    const buttonText = !!email ?
+      i18n.manageLinkedAccounts_disconnect() :
+      i18n.manageLinkedAccounts_connect();
+    return (
+      <tr>
+        <td>{i18n.manageLinkedAccounts_google_oauth2()}</td>
+        <td>{email || i18n.manageLinkedAccounts_notConnected()}</td>
+        <td>
+          <Button
+            text={buttonText}
+            onClick={() => {}}
+            size={Button.ButtonSize.large}
+            color={Button.ButtonColor.gray}
+            disabled={cannotDisconnect}
+          />
+        </td>
+      </tr>
     );
   }
 }
