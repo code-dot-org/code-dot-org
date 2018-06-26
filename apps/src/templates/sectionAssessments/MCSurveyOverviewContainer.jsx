7f7433197df03cacb00d5270825b060b05d1490a
nkiru onwuneme <generalhrh@gmail.com>
2018-06-26 13:54:54 -0700

    wip

--- apps/src/templates/sectionAssessments/MCSurveyOverviewContainer.jsx
+++ apps/src/templates/sectionAssessments/MCSurveyOverviewContainer.jsx
@@ -1,12 +1,13 @@
 import React, {Component, PropTypes} from 'react';
 import MultipleChoiceSurveyOverviewTable, {multipleChoiceSurveyDataPropType} from './MultipleChoiceSurveyOverviewTable';
-import {getMultipleChoiceSurveyResults} from './sectionAssessmentsRedux';
+import {getMultipleChoiceSurveyResults, checkCurrentIsSurvey} from './sectionAssessmentsRedux';
 import { connect } from 'react-redux';
-import i18n from "@cdo/locale";
+import i18n from '@cdo/locale';
 
 class MCSurveyOverviewContainer extends Component {
   static propTypes= {
     multipleChoiceSurveyData: PropTypes.arrayOf(multipleChoiceSurveyDataPropType),
+    // multipleChoiceSurveyResults:  PropTypes.arrayOf(multipleChoiceSurveyDataPropType)
   };
 
   render() {
@@ -14,12 +15,6 @@ class MCSurveyOverviewContainer extends Component {
     return (
       <div>
         <div>
-        {/* <h2>{i18n.emptySurveyOverviewTable()}</h2>
-          {multipleChoiceSurveyData.length < 5 &&
-            <MultipleChoiceSurveyOverviewTable
-            multipleChoiceSurveyData={[]}
-          />
-        } */}
         <h2>{i18n.multipleChoiceQuestionsOverview()}</h2>
           {multipleChoiceSurveyData.length > 0 &&
             <MultipleChoiceSurveyOverviewTable
@@ -36,4 +31,5 @@ export const UnconnectedMCSurveyOverviewContainer = MCSurveyOverviewContainer;
 
 export default connect(state => ({
   multipleChoiceSurveyData: getMultipleChoiceSurveyResults(state),
+  multipleChoiceSurveyData: checkCurrentIsSurvey(state),
 }))(MCSurveyOverviewContainer);
