76fa272ecb852a24cc9cc26dffbacffc40329040
Mehal Shah <mehal@code.org>
2018-06-29 10:15:15 -0700

    Checkpoint

--- apps/src/code-studio/pd/workshop_dashboard/reports/local_summer_workshop_daily_survey/results.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/reports/local_summer_workshop_daily_survey/results.jsx
@@ -1,19 +1,8 @@
 import React, {PropTypes} from 'react';
 import {Tab, Tabs} from 'react-bootstrap';
-import he from 'he';
 import SingleChoiceResponses from '../../components/survey_results/single_choice_responses';
 import TextResponses from '../../components/survey_results/text_responses';
 import _ from 'lodash';
-
-const styles = {
-  table: {
-    width: 'auto',
-    maxWidth: '50%'
-  },
-  facilitatorResponseList: {
-    listStyle: 'circle'
-  }
-};
 
 export default class Results extends React.Component {
   static propTypes = {
@@ -27,180 +16,10 @@ export default class Results extends React.Component {
     facilitatorIds: Object.keys(this.props.facilitators)
   };
 
-  renderSessionResultsTable(session) {
-    return (
-      <table className="table table-bordered" style={styles.table}>
-        <thead>
-          <tr>
-            <th/>
-            <th>This workshop</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            Object.entries(this.props.questions[session]['general']).map(([question_key, question_data], i) => {
-              if (['selectValue', 'none'].includes(question_data['answer_type'])) {
-                return (
-                  <tr key={i}>
-                    <td
-                      dangerouslySetInnerHTML={{__html: question_data['text']}}// eslint-disable-line react/no-danger
-                    />
-                    <td>
-                      {
-                        this.computeAverageFromAnswerObject(
-                          this.props.thisWorkshop[session]['general'][question_key],
-                          question_data['max_value'] || question_data['options'].length
-                        )
-                      }
-                    </td>
-                  </tr>
-                );
-              }
-            })
-          }
-        </tbody>
-      </table>
-    );
-  }
-
-  renderSessionResultsFreeResponse(session) {
-    return (
-      <div>
-        {
-          Object.entries(this.props.questions[session]['general']).map(([question_key, question_data], i) => {
-            if (question_data['answer_type'] === 'text') {
-              return (
-                <div key={i} className="well">
-                  {question_data['text']}
-                  {
-                    this.props.thisWorkshop[session]['general'][question_key].map((answer, j) => (
-                      <li key={j}>
-                        {he.decode(answer)}
-                      </li>
-                    ))
-                  }
-                </div>
-              );
-            }
-          })
-        }
-      </div>
-    );
-  }
-
-  renderFacilitatorSpecificResultsTable(session) {
-    const hasTableResponses = Object.values(this.props.questions[session]['facilitator']).some((question) => {
-      return question['answer_type'] === 'selectValue';
-    });
-
-    if (hasTableResponses) {
-      return (
-        <table className="table table-bordered" style={styles.table}>
-          <thead>
-          <tr>
-            <th/>
-            {this.state.facilitatorIds.map((id, i) => (
-              <th key={i}>
-                {this.props.facilitators[id]}
-              </th>
-            ))}
-          </tr>
-          </thead>
-          <tbody>
-          {
-            Object.entries(this.props.questions[session]['facilitator']).map(([question_key, question_data], i) => {
-              if (question_data['answer_type'] === 'selectValue') {
-                return (
-                  <tr key={i}>
-                    <td>
-                      {question_data['text']}
-                    </td>
-                    {
-                      this.state.facilitatorIds.map((id, j) => (
-                        <td key={j}>
-                          {he.decode(this.props.thisWorkshop[session]['facilitator'][question_key][id])}
-                        </td>
-                      ))
-                    }
-                  </tr>
-                );
-              }
-            })
-          }
-          </tbody>
-        </table>
-      );
-    } else {
-      return null;
-    }
-  }
-
-  renderFacilitatorSpecificFreeResponses(session) {
-    return (
-      <div>
-        {
-          Object.entries(this.props.questions[session]['facilitator']).map(([question_key, question_data], i) => {
-            if (question_data['answer_type'] === 'text') {
-              return (
-                <div key={i} className="well">
-                  {question_data['text']}
-                  {
-                    this.state.facilitatorIds.map((id) => {
-                      return this.renderFacilitatorSpecificBullets(
-                        this.props.thisWorkshop[session]['facilitator'][question_key],
-                        id
-                      );
-                    })
-                  }
-                </div>
-              );
-            }
-          })
-        }
-      </div>
-    );
-  }
-
-  renderFacilitatorSpecificBullets(responses, facilitatorId) {
-    const hasResponses = responses && responses[facilitatorId];
-    return (
-      <li key={facilitatorId}>
-        {this.props.facilitators[facilitatorId]}
-        <ul>
-          {
-            hasResponses && responses[facilitatorId].map((response, i) => (
-              <li key={i} style={styles.facilitatorResponseList}>
-                {he.decode(response)}
-              </li>
-            ))
-          }
-        </ul>
-      </li>
-    );
-  }
-
-  renderFacilitatorSpecificSection(session) {
-    return (
-      <div>
-        <hr/>
-        <h3>
-          Facilitator specific questions
-        </h3>
-        {this.renderFacilitatorSpecificResultsTable(session)}
-        {this.renderFacilitatorSpecificFreeResponses(session)}
-      </div>
-    );
-  }
-
   /**
    * Render results for either the facilitator specific or general questions
    */
   renderResultsForSessionQuestionSection(section, questions, answers) {
-    console.log('Questions');
-    console.log(questions);
-    console.log('Answers');
-    console.log(answers);
-
     return _.compact(Object.keys(questions).map((questionId, i) => {
       let question = questions[questionId];
 
@@ -278,24 +97,6 @@ export default class Results extends React.Component {
         {this.renderResultsForSession(session)}
       </Tab>
     ));
-  }
-
-  computeAverageFromAnswerObject(answerHash, maxValue) {
-    let sum = 0;
-    Object.keys(answerHash).map((key) => {
-      if (Number(key) > 0) {
-        sum += key * answerHash[key];
-      }
-    });
-
-    if (sum === 0) {
-      return '';
-    } else {
-      let average = sum / Object.values(answerHash).reduce((sum, x) => {
-        return sum + x;
-      });
-      return `${average.toFixed(2)} / ${maxValue}`;
-    }
   }
 
   render() {
--- apps/test/unit/code-studio/pd/workshop_dashboard/reports/local_summer_workshop_daily_survey/resultsTest.js
+++ apps/test/unit/code-studio/pd/workshop_dashboard/reports/local_summer_workshop_daily_survey/resultsTest.js
@@ -11,11 +11,24 @@ describe("Local Summer Workshop Daily Survey Results class", () => {
             'Pre Workshop': {
               'general': {
                 'q1': {text: 'Matrix header', answer_type: 'none'},
-                'q2': {text: 'Matrix 1', answer_type: 'selectValue', max_value: '5'},
-                'q3': {text: 'Matrix 2', answer_type: 'selectValue', max_value: '5'},
-                'q4': {text: 'Scale 1', answer_type: 'selectValue', max_value: '5'},
+                'q2': {text: 'Matrix header...Matrix 1', answer_type: 'singleSelect', options: ['Poor', 'Fair', 'Good', 'Great', 'Excellent']},
+                'q3': {text: 'Matrix header...Matrix 2', answer_type: 'singleSelect', options: ['Poor', 'Fair', 'Good', 'Great', 'Excellent']},
+                'q4': {text: 'Scale 1', answer_type: 'scale', max_value: '5', options: ['1', '2', '3', '4', '5']},
+                'f1': {text: 'General, Free Response 1', answer_type: 'text'},
+                'f2': {text: 'General, Free Response 2', answer_type: 'text'}
+              }
+            },
+            'Day 1': {
+              'general': {
+                'q1': {text: 'Day 1 Matrix header', answer_type: 'none'},
+                'q2': {text: 'Day 1 Matrix...Matrix 1', answer_type: 'singleSelect', options: ['Poor', 'Fair', 'Good', 'Great', 'Excellent']},
+                'q3': {text: 'Day 1 Matrix...Matrix 2', answer_type: 'singleSelect', options: ['Poor', 'Fair', 'Good', 'Great', 'Excellent']},
+                'q4': {text: 'Scale 2', answer_type: 'scale', max_value: '5', options: ['1', '2', '3', '4', '5']},
                 'f1': {text: 'Day 1, Free Response 1', answer_type: 'text'},
                 'f2': {text: 'Day 1, Free Response 2', answer_type: 'text'}
+              },
+              'facilitator': {
+                'q1': {text: 'Day 1 Facilitator question', answer_type: 'text'}
               }
             }
         }}
@@ -30,26 +43,25 @@ describe("Local Summer Workshop Daily Survey Results class", () => {
               'f2': ['d', 'e', 'f']
             },
             'response_count': 10
+          },
+          'Day 1': {
+            'general': {
+
+            },
+            'facilitator': {
+
+            },
+            response_count: 12
           }
         }}
-        sessions={['Pre Workshop']}
+        sessions={['Pre Workshop', 'Day 1']}
         facilitators={{
           1: 'Facilitator 1',
           2: 'Facilitator 2'
         }}
       />
     );
-    expect(results.find('Tab')).to.have.length(1);
-    expect(results.find('table')).to.have.length(1);
-    expect(results.find('td').map((x) => x.text())).to.deep.equal(
-      [
-        'Matrix header', '',
-        'Matrix 1', '3.86 / 5',
-        'Matrix 2', '2.33 / 5',
-        'Scale 1', '3.60 / 5',
-      ]
-    );
-    expect(results.find('.well')).to.have.length(2); // 2 general responses
-    expect(results.find('.well li').map((x) => x.text())).to.deep.equal('abcdef'.split(''));
+    expect(results.find('Tab')).to.have.length(2);
+    //expect(results.find('Tab').at(0).find('SingleChoiceResponse')).to.have.length(2);
   });
 });
