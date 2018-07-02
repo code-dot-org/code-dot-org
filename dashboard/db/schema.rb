1c0e370f68edba81d700a0d85c4897bb73530e5e
Erin <erin@code.org>
2018-07-02 13:22:14 -0700

    Add a deleted_at column to facilitate paranoid delete

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180627224545) do
+ActiveRecord::Schema.define(version: 20180702201106) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -1313,6 +1313,7 @@ ActiveRecord::Schema.define(version: 20180627224545) do
     t.integer  "teacher_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.datetime "deleted_at"
     t.index ["student_id", "level_id", "teacher_id"], name: "index_feedback_on_student_and_level_and_teacher_id", using: :btree
     t.index ["student_id", "level_id"], name: "index_feedback_on_student_and_level", using: :btree
   end
