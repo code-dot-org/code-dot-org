38f78b94d530acad1119adfa3e985d28d068ca04
David Bailey <davidsbailey@users.noreply.github.com>
2018-07-02 11:01:58 -0700

    disallow spaces in staging content filenames

--- tools/hooks/restrict_staging_changes.rb
+++ tools/hooks/restrict_staging_changes.rb
@@ -5,11 +5,13 @@ REPO_DIR = File.expand_path('../../../', __FILE__).freeze
 # Returns whether a filename should be prohibited from a staging commit. Reasons for this:
 #   * any file with an extension in {.mp4, .mov} (e.g., 'dashboard/dir/my_bad_file.mov')
 #   * any file with non-lowercase letters in the extension (e.g., 'dashboard/dir/my_bad_file.PnG')
+#   * any file with spaces in the name
 # @param filename [String] A filename.
 # @return [Boolean] Whether the filename should be prohibited in a commit.
 def prohibited?(filename)
   return true if ['.mp4', '.mov'].include? File.extname(filename)
   return true if File.extname(filename) != File.extname(filename).downcase
+  return true if filename.include?(' ')
   false
 end
 
