702ac9a818bd5b7dbca77f61db6ee0cbdf323fd9
Josh Lory <josh.lory@code.org>
2018-07-03 10:14:47 -0700

    Convert "blocks" table to utf8mb4 to accept 4 byte utf8 characters
    Also changed the "category" column from `t.text` -> `t.string`. Category should be a short string.

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180702201106) do
+ActiveRecord::Schema.define(version: 20180703165502) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -88,10 +88,10 @@ ActiveRecord::Schema.define(version: 20180702201106) do
     t.index ["user_id", "script_id", "level_id", "hint_id"], name: "index_authored_hint_view_requests_on_all_related_ids", using: :btree
   end
 
-  create_table "blocks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+  create_table "blocks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
     t.string   "name"
     t.string   "level_type"
-    t.text     "category",    limit: 65535
+    t.string   "category"
     t.text     "config",      limit: 65535
     t.text     "helper_code", limit: 65535
     t.datetime "created_at",                null: false
