4a0c47b3d4042de5c76263a02bb944c6630a55c1
Maddie Kasula <madelynkasula@gmail.com>
2018-07-05 12:37:32 -0700

    Restore navigateToHref stub to avoid breaking other tests

--- apps/test/unit/lib/ui/accounts/ManageLinkedAccountsControllerTest.js
+++ apps/test/unit/lib/ui/accounts/ManageLinkedAccountsControllerTest.js
@@ -44,6 +44,7 @@ describe('ManageLinkedAccountsController', () => {
       let arg = navigateToHrefStub.getCall(0).args[0];
       expect(navigateToHrefStub).to.have.been.calledOnce;
       expect(arg).to.equal('/users/auth/google_oauth2/connect');
+      utils.navigateToHref.restore();
     });
   });
 
