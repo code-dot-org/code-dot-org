a8e947ee6d5cd832c707d22088c9dbe544c3cbb2
Brendan Reville <brendan@code.org>
2018-07-06 09:12:07 -0700

    PD: international opt-in gets comment about people lists

--- dashboard/lib/international_opt_in_people.rb
+++ dashboard/lib/international_opt_in_people.rb
@@ -1,3 +1,8 @@
+#
+# These lists of international facilitators and partners are used by the form at
+# /pd/international_workshop which creates an InternationalOptIn.  They are
+# stored separately here so that they can be modified directly.
+#
 module InternationalOptInPeople
   INTERNATIONAL_OPT_IN_FACILITATORS = [
     "First facilitator",
