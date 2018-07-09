90eadd5c1aa49a8822b850b4c3153b6ec80052da
Maddie Kasula <madelynkasula@gmail.com>
2018-07-09 10:28:44 -0700

    Cleanup utils.navigateToHref stub and test

--- apps/test/unit/lib/ui/accounts/ManageLinkedAccountsTest.jsx
+++ apps/test/unit/lib/ui/accounts/ManageLinkedAccountsTest.jsx
@@ -76,16 +76,15 @@ describe('ManageLinkedAccounts', () => {
   });
 
   it('navigates to provider endpoint if authentication option is not connected', () => {
+    sinon.stub(utils, 'navigateToHref');
     const wrapper = mount(
       <ManageLinkedAccounts
         {...DEFAULT_PROPS}
       />
     );
-    const navigateToHrefStub = sinon.stub(utils, 'navigateToHref');
     wrapper.find('BootstrapButton').at(0).simulate('click');
-    let arg = navigateToHrefStub.getCall(0).args[0];
-    expect(navigateToHrefStub).to.have.been.calledOnce;
-    expect(arg).to.equal('/users/auth/google_oauth2/connect');
+    expect(utils.navigateToHref).to.have.been.calledOnce
+      .and.calledWith('/users/auth/google_oauth2/connect');
     utils.navigateToHref.restore();
   });
 
