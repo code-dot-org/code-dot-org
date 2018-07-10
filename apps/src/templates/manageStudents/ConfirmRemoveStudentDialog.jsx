7d45f6ab68b9d2e504a1502c709bc5d09190a488
Brad Buchanan <brad@code.org>
2018-07-10 14:16:47 -0700

    New ConfirmRemoveStudentDialog

--- apps/src/templates/manageStudents/ConfirmRemoveStudentDialog.jsx
+++ apps/src/templates/manageStudents/ConfirmRemoveStudentDialog.jsx
@@ -1,8 +1,9 @@
 import React, {PropTypes} from 'react';
 import i18n from '@cdo/locale';
+import {Header, ConfirmCancelFooter} from '../../lib/ui/SystemDialog/SystemDialog';
 import BaseDialog from '../BaseDialog';
 import Button from "../Button";
-import DialogFooter from "../teacherDashboard/DialogFooter";
+import color from "../../util/color";
 
 export default class ConfirmRemoveStudentDialog extends React.Component {
   static propTypes = {
@@ -17,33 +18,54 @@ export default class ConfirmRemoveStudentDialog extends React.Component {
     return (
       <BaseDialog
         useUpdatedStyles
-        uncloseable
         isOpen={isOpen}
-        style={{paddingLeft: 20, paddingRight: 20, paddingBottom: 20}}
+        handleClose={onCancel}
       >
-        <h2>{i18n.removeStudentHeader()}</h2>
-        <div>
-          {i18n.removeStudentConfirm1() + ' '}
-          <a target="_blank" href="https://support.code.org/hc/en-us/articles/115001475131-Adding-a-personal-login-to-a-teacher-created-account">
-            {i18n.removeStudentConfirm2()}
-          </a>
-          {' ' + i18n.removeStudentConfirm3()}
+        <div style={styles.container}>
+          <Header text={i18n.removeStudentHeader()}/>
+          <p>
+            <strong>{i18n.removeStudentBody1()}</strong>
+            {' '}
+            {i18n.removeStudentBody2()}
+          </p>
+          <p>
+            {i18n.removeStudentBody3()}
+          </p>
+          <Button
+            text={i18n.removeStudentSendHomeInstructions()}
+            target="_blank"
+            href="https://support.code.org/hc/en-us/articles/115001475131-Adding-a-personal-login-to-a-teacher-created-account"
+            color={Button.ButtonColor.blue}
+            size={Button.ButtonSize.large}
+            style={styles.sendHomeInstructionsButton}
+            tabIndex="1"
+          />
+          <p>
+            {i18n.removeStudentBody4()}
+          </p>
+          <ConfirmCancelFooter
+            confirmText={i18n.removeStudent()}
+            confirmColor={Button.ButtonColor.red}
+            onConfirm={onConfirm}
+            onCancel={onCancel}
+            disableConfirm={disabled}
+            disableCancel={disabled}
+            tabIndex="1"
+          />
         </div>
-        <DialogFooter>
-          <Button
-            text={i18n.dialogCancel()}
-            onClick={onCancel}
-            color={Button.ButtonColor.gray}
-            disabled={disabled}
-          />
-          <Button
-            text={i18n.removeStudent()}
-            onClick={onConfirm}
-            color={Button.ButtonColor.red}
-            disabled={disabled}
-          />
-        </DialogFooter>
       </BaseDialog>
     );
   }
 }
+
+const styles = {
+  container: {
+    margin: 20,
+    color: color.charcoal,
+  },
+  sendHomeInstructionsButton: {
+    display: 'block',
+    textAlign: 'center',
+    marginBottom: '1em',
+  }
+};
