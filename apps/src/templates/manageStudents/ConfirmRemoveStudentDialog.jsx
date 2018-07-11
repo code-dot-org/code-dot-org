023ab7dc48b089088fe77d754d33ee260cb6a0c0
Brad Buchanan <brad@code.org>
2018-07-10 17:46:36 -0700

    Minimal dialog if student has never signed in

--- apps/src/templates/manageStudents/ConfirmRemoveStudentDialog.jsx
+++ apps/src/templates/manageStudents/ConfirmRemoveStudentDialog.jsx
@@ -10,12 +10,19 @@ export default class ConfirmRemoveStudentDialog extends React.Component {
   static propTypes = {
     isOpen: BaseDialog.propTypes.isOpen,
     disabled: PropTypes.bool,
+    hasEverSignedIn: PropTypes.bool,
     onConfirm: PropTypes.func.isRequired,
     onCancel: PropTypes.func.isRequired,
   };
 
+  headerText() {
+    return this.props.hasEverSignedIn ?
+      i18n.removeStudentHeaderNew() :
+      i18n.removeStudentHeader();
+  }
+
   render() {
-    const {isOpen, disabled, onConfirm, onCancel} = this.props;
+    const {isOpen, disabled, hasEverSignedIn, onConfirm, onCancel} = this.props;
     return (
       <BaseDialog
         useUpdatedStyles
@@ -23,27 +30,31 @@ export default class ConfirmRemoveStudentDialog extends React.Component {
         handleClose={onCancel}
       >
         <div style={styles.container}>
-          <Header text={i18n.removeStudentHeaderNew()}/>
-          <p>
-            <strong>{i18n.removeStudentBody1()}</strong>
-            {' '}
-            {i18n.removeStudentBody2()}
-          </p>
-          <p>
-            {i18n.removeStudentBody3()}
-          </p>
-          <Button
-            text={i18n.removeStudentSendHomeInstructions()}
-            target="_blank"
-            href={ADD_A_PERSONAL_LOGIN_HELP_URL}
-            color={Button.ButtonColor.blue}
-            size={Button.ButtonSize.large}
-            style={styles.sendHomeInstructionsButton}
-            tabIndex="1"
-          />
-          <p>
-            {i18n.removeStudentBody4()}
-          </p>
+          <Header text={this.headerText()}/>
+          {hasEverSignedIn &&
+            <div>
+              <p>
+                <strong>{i18n.removeStudentBody1()}</strong>
+                {' '}
+                {i18n.removeStudentBody2()}
+              </p>
+              <p>
+                {i18n.removeStudentBody3()}
+              </p>
+              <Button
+                text={i18n.removeStudentSendHomeInstructions()}
+                target="_blank"
+                href={ADD_A_PERSONAL_LOGIN_HELP_URL}
+                color={Button.ButtonColor.blue}
+                size={Button.ButtonSize.large}
+                style={styles.sendHomeInstructionsButton}
+                tabIndex="1"
+              />
+              <p>
+                {i18n.removeStudentBody4()}
+              </p>
+            </div>
+          }
           <ConfirmCancelFooter
             confirmText={i18n.removeStudent()}
             confirmColor={Button.ButtonColor.red}
---
+++ apps/test/unit/templates/manageStudents/ConfirmRemoveStudentDialogTest.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import i18n from '@cdo/locale';
+import {expect} from '../../../util/configuredChai';
+import ConfirmRemoveStudentDialog from '@cdo/apps/templates/manageStudents/ConfirmRemoveStudentDialog';
+import Button from '@cdo/apps/templates/Button';
+import {Header, ConfirmCancelFooter} from '@cdo/apps/lib/ui/SystemDialog/SystemDialog';
+import {ADD_A_PERSONAL_LOGIN_HELP_URL} from '@cdo/apps/lib/util/urlHelpers';
+
+describe('ConfirmRemoveStudentDialog', () => {
+  const DEFAULT_PROPS = {
+    isOpen: true,
+    disabled: false,
+    hasEverSignedIn: true,
+    onConfirm: () => {},
+    onCancel: () => {},
+  };
+
+  it('renders nothing if not open', () => {
+    const wrapper = mount(
+      <ConfirmRemoveStudentDialog
+        {...DEFAULT_PROPS}
+        isOpen={false}
+      />
+    );
+    expect(wrapper).to.be.empty;
+  });
+
+  it('renders minimal content if student has never signed in', () => {
+    const wrapper = mount(
+      <ConfirmRemoveStudentDialog
+        {...DEFAULT_PROPS}
+        hasEverSignedIn={false}
+      />
+    );
+    expect(wrapper).to.containMatchingElement(
+      <div>
+        <Header text={i18n.removeStudentHeader()}/>
+        <ConfirmCancelFooter
+          confirmText={i18n.removeStudent()}
+          confirmColor={Button.ButtonColor.red}
+          onConfirm={DEFAULT_PROPS.onConfirm}
+          onCancel={DEFAULT_PROPS.onCancel}
+          disableConfirm={false}
+          disableCancel={false}
+          tabIndex="1"
+        />
+      </div>
+    );
+  });
+
+  it('renders full text if student has ever signed in', () => {
+    const wrapper = mount(
+      <ConfirmRemoveStudentDialog
+        {...DEFAULT_PROPS}
+        hasEverSignedIn={true}
+      />
+    );
+    expect(wrapper).to.containMatchingElement(
+      <div>
+        <Header text={i18n.removeStudentHeaderNew()}/>
+        <div>
+          <p>
+            <strong>{i18n.removeStudentBody1()}</strong>
+            {' '}
+            {i18n.removeStudentBody2()}
+          </p>
+          <p>
+            {i18n.removeStudentBody3()}
+          </p>
+          <Button
+            text={i18n.removeStudentSendHomeInstructions()}
+            target="_blank"
+            href={ADD_A_PERSONAL_LOGIN_HELP_URL}
+            color={Button.ButtonColor.blue}
+            size={Button.ButtonSize.large}
+            tabIndex="1"
+          />
+          <p>
+            {i18n.removeStudentBody4()}
+          </p>
+        </div>
+        <ConfirmCancelFooter
+          confirmText={i18n.removeStudent()}
+          confirmColor={Button.ButtonColor.red}
+          onConfirm={DEFAULT_PROPS.onConfirm}
+          onCancel={DEFAULT_PROPS.onCancel}
+          disableConfirm={false}
+          disableCancel={false}
+          tabIndex="1"
+        />
+      </div>
+    );
+  });
+});
