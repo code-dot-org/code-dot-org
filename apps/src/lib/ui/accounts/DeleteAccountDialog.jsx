7c7257f802f1f65cdb409df0fbf90245546d44b2
Maddie Kasula <madelynkasula@gmail.com>
2018-07-10 17:07:52 -0700

    Add dialog fields and style

--- apps/src/lib/ui/accounts/DeleteAccountDialog.jsx
+++ apps/src/lib/ui/accounts/DeleteAccountDialog.jsx
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react';
 import i18n from '@cdo/locale';
 import color from '@cdo/apps/util/color';
 import BaseDialog from '@cdo/apps/templates/BaseDialog';
-import {Header, ConfirmCancelFooter} from '../SystemDialog/SystemDialog';
+import {Header, Field, ConfirmCancelFooter} from '../SystemDialog/SystemDialog';
 import FontAwesome from '@cdo/apps/templates/FontAwesome';
 import Button from '@cdo/apps/templates/Button';
 
@@ -10,10 +10,13 @@ const GUTTER = 20;
 const styles = {
   container: {
     margin: GUTTER,
+    color: color.charcoal,
   },
   bodyContainer: {
     display: 'flex',
     alignItems: 'center',
+    paddingTop: GUTTER,
+    paddingBottom: GUTTER,
   },
   icon: {
     color: color.red,
@@ -22,6 +25,12 @@ const styles = {
   text: {
     fontSize: 16,
     paddingLeft: GUTTER,
+  },
+  dangerText: {
+    color: color.red,
+  },
+  input: {
+    width: 490
   },
 };
 
@@ -37,23 +46,45 @@ export default class DeleteAccountDialog extends React.Component {
     return (
       <BaseDialog
         useUpdatedStyles
-        fixedWidth={600}
+        fixedWidth={550}
         isOpen={isOpen}
         handleClose={onCancel}
       >
         <div style={styles.container}>
-          <Header text={i18n.deleteAccount_dialog_header()}/>
+          <Header text={i18n.deleteAccountDialog_header()}/>
           <div style={styles.bodyContainer}>
             <FontAwesome
               icon="exclamation-triangle"
               style={styles.icon}
             />
             <div style={styles.text}>
-              {i18n.deleteAccount_dialog_body()}
+              <strong>{i18n.deleteAccountDialog_body1()}</strong>
+              {i18n.deleteAccountDialog_body2()}
+              <strong style={styles.dangerText}>{i18n.deleteAccountDialog_body3()}</strong>
+              {i18n.deleteAccountDialog_body4()}
             </div>
           </div>
+          <Field
+            label={i18n.deleteAccountDialog_confirmPassword()}
+          >
+            <input
+              type="password"
+              style={styles.input}
+            />
+          </Field>
+          <Field
+            label={i18n.deleteAccountDialog_deleteAccountVerification()}
+          >
+            <input
+              type="text"
+              style={styles.input}
+            />
+          </Field>
+          <div>
+            {i18n.deleteAccountDialog_emailUs()}
+          </div>
           <ConfirmCancelFooter
-            confirmText={i18n.deleteAccount_dialog_button()}
+            confirmText={i18n.deleteAccountDialog_button()}
             confirmColor={Button.ButtonColor.red}
             onConfirm={() => {}}
             onCancel={onCancel}
