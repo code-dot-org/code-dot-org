71c7281c1a2b627add25d16a6615f829fd4c5453
Brad Buchanan <brad@code.org>
2018-07-11 13:37:25 -0700

    Missing key attribute in story

--- apps/src/templates/Button.story.jsx
+++ apps/src/templates/Button.story.jsx
@@ -61,7 +61,7 @@ export default storybook => {
         story: () => (
           <div>
             {Object.values(Button.ButtonSize).map((size) => (
-              <div>
+              <div key={`${size}-row`}>
                 {Object.values(Button.ButtonColor).map((color) => (
                   <Button
                     icon="file-text"
