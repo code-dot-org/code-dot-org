8d152d92caa577272cfff363367b7289aed1387e
Suresh Chanmugam <suresh@code.org>
2018-07-12 16:49:24 -0700

    Post migration.

--- dashboard/app/models/census/other_curriculum_offering.rb
+++ dashboard/app/models/census/other_curriculum_offering.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Table name: access_report_other_curriculum_offerings
+# Table name: other_curriculum_offerings
 #
 #  id                       :integer          not null, primary key
 #  curriculum_provider_name :string(255)      not null
@@ -12,8 +12,8 @@
 #
 # Indexes
 #
-#  fk_rails_a4c25f7f68                                    (school_id)
-#  index_access_report_other_curriculum_offerings_unique  (curriculum_provider_name,school_id,course,school_year) UNIQUE
+#  fk_rails_5682e60354                      (school_id)
+#  index_other_curriculum_offerings_unique  (curriculum_provider_name,school_id,course,school_year) UNIQUE
 #
 
 class Census::OtherCurriculumOffering < ApplicationRecord
