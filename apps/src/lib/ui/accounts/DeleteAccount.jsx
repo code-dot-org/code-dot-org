85945941c19b4450d30a7cf2ac012ee573b41c82
Maddie Kasula <madelynkasula@gmail.com>
2018-07-12 16:26:13 -0700

    Move DeleteAccount helpers into DeleteAccountHelpers

--- apps/src/lib/ui/accounts/DeleteAccount.jsx
+++ apps/src/lib/ui/accounts/DeleteAccount.jsx
@@ -2,7 +2,11 @@ import React, {PropTypes} from 'react';
 import $ from 'jquery';
 import i18n from '@cdo/locale';
 import color from '@cdo/apps/util/color';
-import {ADD_A_PERSONAL_LOGIN_HELP_URL} from '@cdo/apps/lib/util/urlHelpers';
+import {
+  TeacherWarning,
+  StudentWarning,
+  getLabelForCheckbox,
+} from './DeleteAccountHelpers';
 import {navigateToHref} from '@cdo/apps/utils';
 import BootstrapButton from './BootstrapButton';
 import DeleteAccountDialog from './DeleteAccountDialog';
@@ -28,49 +32,6 @@ const styles = {
     display: 'flex',
     justifyContent: 'flex-end',
   },
-};
-
-const getLabelForCheckbox = (id) => {
-  switch (id) {
-    case 1:
-      return (
-        <span>
-          <strong>{i18n.deleteAccountDialog_checkbox1_1()}</strong>
-          {i18n.deleteAccountDialog_checkbox1_2()}
-        </span>
-      );
-    case 2:
-      return (
-        <span>
-          {i18n.deleteAccountDialog_checkbox2_1()}
-          <a href={ADD_A_PERSONAL_LOGIN_HELP_URL} target="_blank">
-            {i18n.deleteAccountDialog_checkbox2_2()}
-          </a>
-          {i18n.deleteAccountDialog_checkbox2_3()}
-        </span>
-      );
-    case 3:
-      return (
-        <span>
-          {i18n.deleteAccountDialog_checkboxPreface()}
-          <strong>{i18n.deleteAccountDialog_checkbox3()}</strong>
-        </span>
-      );
-    case 4:
-      return (
-        <span>
-          {i18n.deleteAccountDialog_checkboxPreface()}
-          <strong>{i18n.deleteAccountDialog_checkbox4()}</strong>
-        </span>
-      );
-    case 5:
-      return (
-        <span>
-          {i18n.deleteAccountDialog_checkboxPreface()}
-          <strong>{i18n.deleteAccountDialog_checkbox5()}</strong>
-        </span>
-      );
-  }
 };
 
 export const buildCheckboxMap = () => {
@@ -181,26 +142,7 @@ export default class DeleteAccount extends React.Component {
           {i18n.deleteAccount()}
         </h2>
         <div style={styles.warning}>
-          {!this.props.isTeacher && i18n.deleteAccount_studentWarning()}
-          {this.props.isTeacher &&
-            <div>
-              <p>
-                {i18n.deleteAccount_teacherWarning1()}
-                <strong>{i18n.deleteAccount_teacherWarning2()}</strong>
-                {i18n.deleteAccount_teacherWarning3()}
-              </p>
-              <p>
-                {i18n.deleteAccount_teacherWarning4()}
-                <a
-                  href={ADD_A_PERSONAL_LOGIN_HELP_URL}
-                  target="_blank"
-                >
-                  {i18n.deleteAccount_teacherWarning5()}
-                </a>
-                {i18n.deleteAccount_teacherWarning6()}
-              </p>
-            </div>
-          }
+          {this.props.isTeacher ? <TeacherWarning/> : <StudentWarning/>}
         </div>
         <div style={styles.buttonContainer}>
           {/* This button intentionally uses BootstrapButton to match other account page buttons */}
