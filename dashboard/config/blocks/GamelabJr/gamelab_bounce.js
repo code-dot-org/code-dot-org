812031fc9c0da970aa62e4b06d5d6302cc027ee0
Continuous Integration <dev@code.org>
2018-07-12 19:00:05 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/blocks/GamelabJr/gamelab_bounce.js
+++ dashboard/config/blocks/GamelabJr/gamelab_bounce.js
@@ -1,8 +1,14 @@
+
 function bounce(sprite){
-  if(sprite.isTouching(edges)){
-    console.log("touched");
+  if(sprite.isTouching(leftEdge)){
+    console.log("touched left");
+  }
+  else if(sprite.isTouching(rightEdge)){
+    console.log("touched right");
+  }
+  else if(sprite.isTouching(topEdge)){
+    console.log("touched top");
   }
   edges.displace(sprite);
-  console.log(sprite.touching);
   sprite.direction += sprite.direction * 1.45;
 }
