e6e281c4de3fcce45d28dfd8929afdced081b3a5
Josh Lory <josh.lory@code.org>
2018-07-16 09:32:38 -0700

    Remove remaining Drupal site files

--- pegasus/sites.v3/drupal.code.org/public/sites/default/files/js/js_sCPJ6yHojUs4Bksbzi3sBhjseTqgqeFGXoaTzq5sQ_A.js
+++
@@ -1,6 +0,0 @@
-function adjustHeight(obj, offset) {
-	var helpFrame = jQuery("#" + obj.name);
-	var innerDoc = (helpFrame.get(0).contentDocument) ? helpFrame.get(0).contentDocument : helpFrame.get(0).contentWindow.document;
-	helpFrame.height(innerDoc.body.scrollHeight + offset);
-}
-;
--- pegasus/sites.v3/drupal.code.org/public/sites/default/files/karel_starter_OLD/README.md
+++
@@ -1,1 +0,0 @@
-Standalone karel project
