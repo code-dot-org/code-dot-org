8959072b13b912a3a3189a89b62f139ff1e69eaf
Will Jordan <will@code.org>
2018-07-17 11:36:25 -0700

    extract i18n subtrees usage from application Enables future support for optimized key-value i18n backends.

--- dashboard/test/helpers/notes_helper_test.rb
+++ dashboard/test/helpers/notes_helper_test.rb
@@ -8,13 +8,13 @@ class NotesHelperTest < ActionController::TestCase
     assert_nil slides
   end
 
-  test 'should raise NoMethodError if there is no slide in english' do
+  test 'should raise error if there is no slide in english' do
     # return an arbitrary set of slides for any language except english
-    fake_slides = {100 => {image: 'notes/flappy01.jpg', text: 'here is some text'}}
-    stubs(:try_t).returns(fake_slides)
+    fake_slides = {image: 'notes/flappy01.jpg', text: 'here is some text'}
+    stubs(:try_t).returns(fake_slides, fake_slides, nil)
     stubs(:try_t).with {|*args| args[1] && args[1][:locale] == :en}.returns(nil)
 
-    assert_raises(NoMethodError) do
+    assert_raises_matching(/Missing English slide/) do
       with_locale('it-IT') do
         get_slides_by_video_key('flappy_intro')
       end
