a50f4ed8441b0513458502470a000cffe95af677
Brad Buchanan <brad@code.org>
2018-07-18 14:16:28 -0700

    if the removed AuthenticationOption was primary and a replacement is available, replaces it

--- dashboard/app/controllers/authentication_options_controller.rb
+++ dashboard/app/controllers/authentication_options_controller.rb
@@ -11,8 +11,18 @@ class AuthenticationOptionsController < ApplicationController
   def disconnect
     return head(:bad_request) unless current_user&.migrated?
     auth_option = current_user.authentication_options.find(params[:id])
-    auth_option.destroy if auth_option.present?
 
-    return head(:no_content)
+    if auth_option.present?
+      auth_option.destroy
+
+      # Replace primary_contact_info if we just destroyed it
+      if current_user.primary_contact_info_id == auth_option.id
+        current_user.update primary_contact_info:
+          current_user.authentication_options.
+            find_by(hashed_email: auth_option.hashed_email)
+      end
+    end
+
+    head(:no_content)
   end
 end
