064027e1731d36ac16836648114eb3cff20e8af1
Brad Buchanan <brad@code.org>
2018-07-18 14:28:58 -0700

    if the removed AuthenticationOption was primary and no replacement is available, creates one

--- dashboard/test/controllers/authentication_options_controller_test.rb
+++ dashboard/test/controllers/authentication_options_controller_test.rb
@@ -127,6 +127,34 @@ class AuthenticationOptionsControllerTest < ActionDispatch::IntegrationTest
     assert_equal facebook_option, teacher.primary_contact_info
   end
 
+  test 'disconnect: if the removed AuthenticationOption was primary and no replacement is available, creates one' do
+    email = 'example@gmail.com'
+    teacher = create :teacher, :multi_auth_migrated
+    google_option = create :google_authentication_option, user: teacher, email: email
+    teacher.update(primary_contact_info: google_option)
+    teacher.reload
+
+    # Preconditions
+    assert_equal 1, teacher.authentication_options.size
+    assert_includes teacher.authentication_options, google_option
+    assert_equal google_option, teacher.primary_contact_info
+    refute_empty teacher.encrypted_password
+
+    sign_in teacher
+    delete "/users/auth/#{teacher.primary_contact_info_id}/disconnect"
+    assert_response :success
+
+    teacher.reload
+    assert_equal 1, teacher.authentication_options.size
+    refute_includes teacher.authentication_options, google_option
+    assert_authentication_option teacher.primary_contact_info,
+      user: teacher,
+      email: email,
+      hashed_email: User.hash_email(email),
+      credential_type: AuthenticationOption::EMAIL,
+      authentication_id: User.hash_email(email)
+  end
+
   test 'disconnect: returns not_found if the AuthenticationOption does not exist' do
     user = create :user, :multi_auth_migrated
     sign_in user
