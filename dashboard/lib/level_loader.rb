109b6cedf5a2e60d2194b72b168f60324e254291
Brad Buchanan <brad@code.org>
2018-07-19 13:26:02 -0700

    Bulk-load level files in LevelLoader

--- dashboard/lib/level_loader.rb
+++ dashboard/lib/level_loader.rb
@@ -1,13 +1,54 @@
+require 'set'
+
 class LevelLoader
+  def self.file_paths(glob)
+    Dir.glob(Rails.root.join(glob)).sort
+  end
+
   def self.for_each_file(path, &block)
-    Dir.glob(Rails.root.join(path)).sort.map(&block)
+    file_paths(path).map(&block)
   end
 
   def self.load_custom_levels
-    level_index = Level.includes(:game).to_a.index_by(&:name)
-    for_each_file('config/scripts/**/*.level') do |path|
-      load_custom_level(path, level_index)
+    # Use a transaction because loading levels requires two separate imports.
+    Level.transaction do
+      level_md5s_by_name = Hash[Level.pluck(:name, :md5)]
+      existing_level_names = level_md5s_by_name.keys.to_set
+      level_file_paths = file_paths('config/scripts/**/*.level')
+
+      # First, save stubs of any new levels - they'll need to have ids in
+      # order to create certain associations (in particular
+      # level_concept_difficulty) when we bulk-load the level properties.
+      new_level_names = level_file_paths.
+        map {|path| name_from_path path}.
+        reject {|name| existing_level_names.include? name}
+      Level.import! new_level_names.map {|name| {name: name}}
+
+      # Load level properties from disk and build a collection of levels that
+      # have changed.
+      changed_levels = level_file_paths.
+          map {|path| load_custom_level path, level_md5s_by_name}.
+          compact.
+          select(&:changed?)
+
+      # activerecord-import doesn't trigger before_save and before_create hooks
+      # for imported models, so we trigger these manually to make sure they're
+      # set up the same way they would be otherwise.
+      # @see https://github.com/zdennis/activerecord-import/wiki/Callbacks
+      changed_levels.each do |level|
+        level.run_callbacks(:save) {false}
+        level.run_callbacks(:create) {false}
+      end
+
+      # Bulk-import changed levels.
+      update_columns = Level.columns.map(&:name).map(&:to_sym).
+        reject {|column| %i(id name created_at).include? column}
+      Level.import! changed_levels, on_duplicate_key_update: update_columns
     end
+  end
+
+  def self.name_from_path(path)
+    File.basename(path, File.extname(path))
   end
 
   def self.level_file_path(name)
@@ -16,14 +57,19 @@ class LevelLoader
     level_paths.first || Rails.root.join("config/scripts/levels/#{name}.level")
   end
 
-  def self.load_custom_level(level_path, level_index = {})
-    name = File.basename(level_path, File.extname(level_path))
-    level = (level_index[name] || Level.find_or_create_by(name: name))
+  def self.load_custom_level(level_path, level_md5s_by_name = Hash[Level.pluck(:name, :md5)])
+    name = name_from_path level_path
     # Only reload level data when file contents change
     level_data = File.read(level_path)
-    level.md5 = Digest::MD5.hexdigest(level_data)
-    load_custom_level_xml(level_data, level) if level.changed?
-    level
+    md5 = Digest::MD5.hexdigest(level_data)
+    if level_md5s_by_name[name] != md5
+      level = Level.find_by_name(name) || Level.new(name: name)
+      level.md5 = md5
+      level = load_custom_level_xml(level_data, level)
+      level
+    else
+      nil
+    end
   rescue Exception => e
     # print filename for better debugging
     new_e = Exception.new("in level: #{level_path}: #{e.message}")
@@ -40,7 +86,6 @@ class LevelLoader
     level.send(:write_attribute, 'properties', {})
     level.assign_attributes(level.load_level_xml(xml_node))
 
-    level.save! if level.changed?
     level
   end
 
