b79af314b2b428c15a39f12d8f40713ece6c3cc4
Erin Peach <erin@code.org>
2018-07-19 16:40:29 -0700

    Remove teacher-only container

--- apps/src/templates/instructions/TeacherFeedback.jsx
+++ apps/src/templates/instructions/TeacherFeedback.jsx
@@ -1,27 +1,11 @@
 import React, {PropTypes, Component} from 'react';
 import { connect } from 'react-redux';
-import color from "../../util/color";
 import i18n from '@cdo/locale';
 import { ViewType } from '@cdo/apps/code-studio/viewAsRedux';
 import Button from '@cdo/apps/templates/Button';
 import FeedbacksList from './FeedbacksList';
 
 const styles = {
-  container: {
-    margin: 20,
-    borderWidth: 5,
-    borderStyle: 'solid',
-    borderColor: color.cyan,
-    backgroundColor: color.lightest_cyan,
-    borderRadius: 5
-  },
-  header: {
-    color: color.white,
-    backgroundColor: color.cyan,
-    padding: 5,
-    fontSize: 18,
-    fontFamily: '"Gotham 7r", sans-serif'
-  },
   content: {
     padding: 10
   },
@@ -130,8 +114,7 @@ class TeacherFeedback extends Component {
 
     // Placeholder for upcoming feedback input
     return (
-      <div style={styles.container}>
-        <div style={styles.header}>{i18n.forTeachersOnly()}</div>
+      <div>
         {!this.props.withUnreleasedFeatures &&
           <div style={styles.content}>
             Coming soon: You’ll be able to use this tab to give feedback to your students about their work.
