06934e79488f053c276f4ac4ee61b81d2a5369f3
Josh Lory <josh.lory@code.org>
2018-07-19 10:50:31 -0700

    Migration to add "pool" column to Block table (custom block sub-types)

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180716214410) do
+ActiveRecord::Schema.define(version: 20180719174755) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -94,8 +94,9 @@ ActiveRecord::Schema.define(version: 20180716214410) do
     t.string   "category"
     t.text     "config",      limit: 65535
     t.text     "helper_code", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "pool",                      default: "", null: false
   end
 
   create_table "callouts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
