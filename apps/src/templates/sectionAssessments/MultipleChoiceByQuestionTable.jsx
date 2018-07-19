296c12788921fc9743a211a02be78e1175345705
Caley Brock <caleybrock7@gmail.com>
2018-07-19 16:13:59 -0700

    Remove un-needed styling

--- apps/src/templates/sectionAssessments/MultipleChoiceByQuestionTable.jsx
+++ apps/src/templates/sectionAssessments/MultipleChoiceByQuestionTable.jsx
@@ -4,30 +4,7 @@ import {tableLayoutStyles, sortableOptions} from "../tables/tableConstants";
 import i18n from '@cdo/locale';
 import wrappedSortable from '../tables/wrapped_sortable';
 import orderBy from 'lodash/orderBy';
-import color from "@cdo/apps/util/color";
 import MultipleChoiceAnswerCell from './MultipleChoiceAnswerCell';
-
-const PADDING = 15;
-
-const styles = {
-  studentNameColumnHeader: {
-    padding: PADDING,
-  },
-  studentNameColumnCell : {
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    maxWidth: 200,
-    padding: PADDING,
-    verticalAlign: 'top',
-  },
-  responseColumnHeader: {
-    padding: PADDING,
-  },
-  noResponse: {
-    color: color.lighter_gray,
-  }
-};
 
 export const COLUMNS = {
   NAME: 0,
@@ -88,18 +65,12 @@ class MultipleChoiceByQuestionTable extends Component {
         header: {
           label: i18n.studentName(),
           props: {
-            style: {
-              ...tableLayoutStyles.headerCell,
-              ...styles.studentNameColumnHeader,
-            }
+            style: tableLayoutStyles.headerCell,
           },
         },
         cell: {
           props: {
-            style: {
-              ...tableLayoutStyles.cell,
-              ...styles.studentNameColumnCell,
-            }
+            style: tableLayoutStyles.cell,
           },
         }
       },
@@ -108,10 +79,7 @@ class MultipleChoiceByQuestionTable extends Component {
         header: {
           label: i18n.answer(),
           props: {
-            style: {
-              ...tableLayoutStyles.headerCell,
-              ...styles.responseHeaderCell,
-            }
+            style: tableLayoutStyles.headerCell,
           },
           transforms: [sortable],
         },
