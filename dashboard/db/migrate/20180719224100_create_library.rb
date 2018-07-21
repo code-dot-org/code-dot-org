f8feb22305880f8c79eedefe3befd742aaa2e397
Josh Lory <josh.lory@code.org>
2018-07-20 17:49:10 -0700

    Generate a new migration to try fixing CircleCI db setup failure

--- dashboard/db/migrate/20180719224100_create_library.rb
+++
@@ -1,8 +0,0 @@
-class CreateLibrary < ActiveRecord::Migration[5.0]
-  def change
-    create_table :libraries do |t|
-      t.string :name, index: true, null: false
-      t.mediumtext :content
-    end
-  end
-end
