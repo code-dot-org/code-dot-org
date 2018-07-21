9ccc5d82fa1c0c9e25f6bc0c6fa62fd1076b55dd
Josh Lory <josh.lory@code.org>
2018-07-20 18:10:32 -0700

    Manually invert

--- dashboard/db/migrate/20180721010612_rollback_utf8mb4.rb
+++ dashboard/db/migrate/20180721010612_rollback_utf8mb4.rb
@@ -1,11 +1,17 @@
-require 'db/migrate/20180703165502_change_blocks_to_utf8mb4'
-
 class RollbackUtf8mb4 < ActiveRecord::Migration[5.0]
   def up
-    ChangeBlocksToUtf8mb4.down
+    execute 'alter table blocks convert to charset utf8 collate utf8_unicode_ci'
+    execute 'alter table blocks modify name varchar(255) charset utf8 collate utf8_unicode_ci'
+    execute 'alter table blocks modify category varchar(255) charset utf8 collate utf8_unicode_ci'
+    execute 'alter table blocks modify config text(21845) charset utf8 collate utf8_unicode_ci'
+    execute 'alter table blocks modify helper_code text(21845) charset utf8 collate utf8_unicode_ci'
   end
 
   def down
-    ChangeBlocksToUtf8mb4.up
+    execute 'alter table blocks convert to charset utf8mb4 collate utf8mb4_unicode_ci'
+    execute 'alter table blocks modify name varchar(255) charset utf8mb4 collate utf8mb4_unicode_ci'
+    execute 'alter table blocks modify category varchar(255) charset utf8mb4 collate utf8mb4_unicode_ci'
+    execute 'alter table blocks modify config text(16383) charset utf8mb4 collate utf8mb4_unicode_ci'
+    execute 'alter table blocks modify helper_code text(16383) charset utf8mb4 collate utf8mb4_unicode_ci'
   end
 end
