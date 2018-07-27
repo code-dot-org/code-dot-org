46574bd4d66a15bfe5c175b7ec476310acd0f59e
Erin Peach <erin@code.org>
2018-07-27 11:24:00 -0700

    Add PaneSections to Header story

--- apps/src/templates/PaneHeader.story.jsx
+++ apps/src/templates/PaneHeader.story.jsx
@@ -1,28 +1,67 @@
 import React from 'react';
-import PaneHeader from './PaneHeader';
+import PaneHeader, {PaneSection} from './PaneHeader';
 
+const styles = {
+  header: {
+    borderLeft: "thick solid white",
+    paddingLeft: 30,
+    paddingRight: 30,
+    color: 'white'
+  },
+  flex: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+};
 
 export default storybook => {
 
   storybook
-    .storiesOf('PaneHeader', module)
+    .storiesOf('PaneHeaders with PaneSections', module)
     .addStoryTable([
       {
         name:'PaneHeader - has focus',
         story: () => (
-          <PaneHeader hasFocus={true}/>
+          <PaneHeader hasFocus={true}>
+            <div style={styles.flex}>
+              <PaneSection style={styles.header}>
+                <span>Section1</span>
+              </PaneSection>
+              <PaneSection style={styles.header}>
+                <span>Section2</span>
+              </PaneSection>
+            </div>
+          </PaneHeader>
         )
       },
       {
         name:'PaneHeader - does not have focus',
         story: () => (
-          <PaneHeader hasFocus={false}/>
+          <PaneHeader hasFocus={false}>
+            <div style={styles.flex}>
+              <PaneSection style={styles.header}>
+                <span>Section1</span>
+              </PaneSection>
+              <PaneSection style={styles.header}>
+                <span>Section2</span>
+              </PaneSection>
+            </div>
+          </PaneHeader>
         )
       },
       {
         name:'PaneHeader - teacher only',
         story: () => (
-          <PaneHeader hasFocus={true} teacherOnly={true}/>
+          <PaneHeader hasFocus={true} teacherOnly={true}>
+            <div style={styles.flex}>
+              <PaneSection style={styles.header}>
+                <span>Section1</span>
+              </PaneSection>
+              <PaneSection style={styles.header}>
+                <span>Section2</span>
+              </PaneSection>
+            </div>
+          </PaneHeader>
         )
       },
     ]);
