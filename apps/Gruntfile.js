0faf014ca9ac362e765d9cef726f9e6c41921f23
Josh Lory <josh.lory@code.org>
2018-07-27 16:24:40 -0700

    Add gif.js capture library

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -209,6 +209,12 @@ describe('entry tests', () => {
         },
         {
           expand: true,
+          cwd: './node_modules/gif.js/dist',
+          src: ['*.js'],
+          dest: 'build/package/js/gif.js/'
+        },
+        {
+          expand: true,
           // For some reason, if we provide piskel root as an absolute path here,
           // our dest ends up with an empty set of directories matching the path
           // If we provide it as a relative path, that does not happen
