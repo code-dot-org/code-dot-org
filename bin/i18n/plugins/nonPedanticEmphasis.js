e66b2b6348b4604e401fa974ebea90c66334220f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-07-31 12:34:40 -0700

    add comments explaining some of the more opaque parts of the redaction/restoration process

--- bin/i18n/plugins/nonPedanticEmphasis.js
+++ bin/i18n/plugins/nonPedanticEmphasis.js
@@ -1,5 +1,21 @@
 let originalTokenizer;
 
+/**
+ * Simple plugin to make remark parsing behave similarly to marked for emphasis.
+ * Specifically, marked is relatively flexible with situations in which it does
+ * and does not recognize emphasis. Examples include spaces between the markers
+ * and the string (`* content *`) and various ways of nesting emphases
+ * (`_*content*_`, etc).
+ *
+ * Remark generally recognizes those, but only when not in pedantic mode.
+ * Unfortunately, we specifically want to run remark in pedantic mode to support
+ * other, more complex differences (like nested lists). So, this plugin simply
+ * forces remark to behave nonpedantically for emphases.
+ *
+ * Eventually, when we use remark for rendering our content, or are able to use
+ * redaction and restoration in non-pedantic mode, we can get rid of this
+ * plugin.
+ */
 module.exports = function nonPedanticEmphasis() {
   const Parser = this.Parser;
   const tokenizers = Parser.prototype.inlineTokenizers;
