f2ce0e9c4ec5093c5fd83cd94f3342ee608dae8f
Continuous Integration <dev@code.org>
2018-08-01 19:00:06 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/libraries/DanceLab.interpreted.js
+++ dashboard/config/libraries/DanceLab.interpreted.js
@@ -1,13 +1,16 @@
 console.log('hello world!');
+var detectors = []; // array of beat detectors
 
-var callbacks = [];
+/*
+// Boo, can't create my own peak detectors
+detectors.push( new p5.PeakDetect(40, 120, 0.8, 20) );
 
-function register(callback) {
-  callbacks.push(callback);
-}
+// lowMid band : 140Hz-400Hz
+detectors.push( new p5.PeakDetect(140, 400, 0.7, 20) );
 
-function draw() {
-  callbacks.forEach(function (callback) {
-    callback();
-  });
-}
+// mid band : 400Hz-2.6kHz
+detectors.push( new p5.PeakDetect(400, 2600, 0.4, 20) );
+
+// high band : 2.7kHz-4kHz
+detectors.push( new p5.PeakDetect(2700, 4000, 0.5, 20) );
+*/
