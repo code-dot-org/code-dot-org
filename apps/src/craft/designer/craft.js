77e43e82fef823bd79cd3dc3cc26f77ce16f2312
Josh Lory <josh.lory@code.org>
2018-08-02 13:46:54 -0700

    Code review feedback

--- apps/src/craft/designer/craft.js
+++ apps/src/craft/designer/craft.js
@@ -290,6 +290,7 @@ Craft.init = function (config) {
           },
           earlyLoadNiceToHaveAssetPacks: Craft.niceToHaveAssetsForLevel(config.level.puzzle_number),
         });
+        Craft.gameController.game.input.touch.preventDefault = false;
 
         if (!config.level.showPopupOnLoad) {
           Craft.initializeAppLevel(config.level);
@@ -635,7 +636,6 @@ Craft.runButtonClick = function () {
   if (Craft.level.usePlayer) {
     Craft.showSoftButtons();
   }
-  Craft.gameController.game.input.touch.preventDefault = false;
 
   var runButton = document.getElementById('runButton');
   var resetButton = document.getElementById('resetButton');
