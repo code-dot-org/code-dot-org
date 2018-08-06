f54aaeb2ad4195dab1aa05af0e3d71c1c2245e99
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-08-06 16:24:41 -0700

    add instructions property override globally, not just for Blockly levels

--- dashboard/app/models/levels/blockly.rb
+++ dashboard/app/models/levels/blockly.rb
@@ -273,8 +273,6 @@ class Blockly < Level
         ani_gif_url: 'aniGifURL',
         success_condition: 'fn_successCondition',
         failure_condition: 'fn_failureCondition',
-        markdown_instructions: 'longInstructions',
-        instructions: 'shortInstructions',
       }
       properties.keys.each do |dashboard|
         blockly = overrides[dashboard.to_sym] || dashboard.camelize(:lower)
