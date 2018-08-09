7da6b76660386bcc184852187032ed2c7aba8a13
David Bailey <davidsbailey@users.noreply.github.com>
2018-08-08 21:39:53 -0400

    show a PopUpMenu when version selector is clicked

--- apps/src/templates/teacherDashboard/AssignmentVersionSelector.jsx
+++ apps/src/templates/teacherDashboard/AssignmentVersionSelector.jsx
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import i18n from '@cdo/locale';
 import {assignmentVersionShape} from './shapes';
+import PopUpMenu from "../../lib/ui/PopUpMenu";
 
 const styles = {
   version: {
@@ -19,6 +20,51 @@ export default class AssignmentVersionSelector extends Component {
     selectedVersion: assignmentVersionShape,
     versions: PropTypes.arrayOf(assignmentVersionShape),
     disabled: PropTypes.bool,
+    showVersionMenu: PropTypes.bool,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: false,
+      canMenuOpen: true,
+      targetPoint: {top: 0, left: 0},
+    };
+  }
+
+  handleMouseDown = e => {
+    if (this.props.showVersionMenu) {
+      // Prevent the native dropdown menu from opening.
+      e.preventDefault();
+    }
+  };
+
+  handleClick = () => {
+    if (!this.props.showVersionMenu) {
+      return;
+    }
+
+    if (!this.state.isMenuOpen && this.state.canMenuOpen) {
+      this.openMenu();
+    }
+  };
+
+  openMenu() {
+    const rect = this.select.getBoundingClientRect();
+    const targetPoint = {
+      top: rect.bottom + window.pageYOffset,
+      left: rect.left + window.pageXOffset,
+    };
+    this.setState({isMenuOpen: true, canMenuOpen: false, targetPoint});
+  }
+
+  beforeClose = (node, resetPortalState) => {
+    resetPortalState();
+    this.setState({isMenuOpen: false});
+    // Work around a bug in react-portal. see SettingsCog.jsx for details.
+    window.setTimeout(() => {
+      this.setState({canMenuOpen: true});
+    });
   };
 
   render() {
@@ -31,8 +77,11 @@ export default class AssignmentVersionSelector extends Component {
           id="assignment-version-year"
           value={selectedVersion.year}
           onChange={onChangeVersion}
+          onMouseDown={this.handleMouseDown}
+          onClick={this.handleClick}
           style={dropdownStyle}
           disabled={disabled}
+          ref={select => this.select = select}
         >
           {
             versions.map(version => (
@@ -45,6 +94,18 @@ export default class AssignmentVersionSelector extends Component {
             ))
           }
         </select>
+        <PopUpMenu
+          isOpen={this.state.isMenuOpen}
+          targetPoint={this.state.targetPoint}
+          beforeClose={this.beforeClose}
+        >
+          <PopUpMenu.Item onClick={() => {}}>
+            Option One
+          </PopUpMenu.Item>
+          <PopUpMenu.Item onClick={() => {}}>
+            Option Two
+          </PopUpMenu.Item>
+        </PopUpMenu>
       </span>
     );
   }
