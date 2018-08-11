15e9b0e7f2ddfd9f3be33aaf3334eef80a575225
David Bailey <davidsbailey@users.noreply.github.com>
2018-08-10 17:23:38 -0700

    display version.isSelected and format menu contents into columns

--- apps/src/templates/teacherDashboard/AssignmentVersionMenuItem.jsx
+++ apps/src/templates/teacherDashboard/AssignmentVersionMenuItem.jsx
@@ -2,6 +2,33 @@ import React, { Component, PropTypes } from 'react';
 import PopUpMenu from "../../lib/ui/PopUpMenu";
 import {assignmentVersionShape} from './shapes';
 import i18n from '@cdo/locale';
+import FontAwesome from './../FontAwesome';
+
+export const columnWidths = {
+  selected: 25,
+  title: 60,
+  status: 100
+};
+
+const style = {
+  wrapper: {
+    fontSize: 16,
+  },
+  selectedColumn: {
+    width: columnWidths.selected,
+    display: 'inline-block',
+    marginLeft: -10,
+  },
+  titleColumn: {
+    width: columnWidths.title,
+    display: 'inline-block',
+  },
+  statusColumn: {
+    width: columnWidths.status,
+    display: 'inline-block',
+    marginRight: -10,
+  },
+};
 
 export default class AssignmentVersionMenuItem extends Component {
   static propTypes = {
@@ -13,7 +40,17 @@ export default class AssignmentVersionMenuItem extends Component {
     const {version, onClick} = this.props;
     return (
       <PopUpMenu.Item onClick={onClick}>
-        {version.isRecommended ? `${version.title} (${i18n.recommended()})` : version.title}
+        <div style={style.wrapper}>
+          <span style={style.selectedColumn}>
+            {version.isSelected && <FontAwesome icon="check"/>}
+          </span>
+            <span style={style.titleColumn}>
+            {version.title}
+          </span>
+            <span style={style.statusColumn}>
+            {version.isRecommended && i18n.recommended()}
+          </span>
+        </div>
       </PopUpMenu.Item>
     );
   }
