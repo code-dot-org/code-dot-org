13085a3466f79072008e4e27c6d916c1d23310dc
Clare Constantine <clare@code.org>
2018-08-13 10:56:21 -0700

    fix merge errors

--- dashboard/app/controllers/api/v1/pd/workshop_enrollments_controller.rb
+++ dashboard/app/controllers/api/v1/pd/workshop_enrollments_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Pd::WorkshopEnrollmentsController < ApplicationController
   include Api::CsvDownload
   include ::Pd::WorkshopConstants
-  load_and_authorize_resource :workshop, class: 'Pd::Workshop', except: 'create', 'cancel'
+  load_and_authorize_resource :workshop, class: 'Pd::Workshop', except: ['create', 'cancel']
 
   RESPONSE_MESSAGES = {
     SUCCESS: "success".freeze,
@@ -82,6 +82,16 @@ class Api::V1::Pd::WorkshopEnrollmentsController < ApplicationController
     head :no_content
   end
 
+  # DELETE /api/v1/pd/enrollments/:enrollment_code
+  def cancel
+    enrollment = Pd::Enrollment.find_by(code: params[:enrollment_code])
+    return unless enrollment
+
+    enrollment.destroy!
+    Pd::WorkshopMailer.teacher_cancel_receipt(enrollment).deliver_now
+    Pd::WorkshopMailer.organizer_cancel_receipt(enrollment).deliver_now
+  end
+
   private
 
   def enrollment_params
@@ -123,14 +133,5 @@ class Api::V1::Pd::WorkshopEnrollmentsController < ApplicationController
   def workshop_owned_by?(user)
     return false unless user
     @workshop.organizer_or_facilitator? user
-
-  # DELETE /api/v1/pd/enrollments/:enrollment_code
-  def cancel
-    enrollment = Pd::Enrollment.find_by(code: params[:enrollment_code])
-    return unless enrollment
-
-    enrollment.destroy!
-    Pd::WorkshopMailer.teacher_cancel_receipt(enrollment).deliver_now
-    Pd::WorkshopMailer.organizer_cancel_receipt(enrollment).deliver_now
   end
 end
