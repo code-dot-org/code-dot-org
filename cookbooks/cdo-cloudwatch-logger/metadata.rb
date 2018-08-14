12129f355b18630039270b60efb6f1e85daad2f1
Continuous Integration <dev@code.org>
2018-08-14 18:44:55 +0000

    Updated cookbook versions

--- cookbooks/cdo-cloudwatch-logger/metadata.rb
+++ cookbooks/cdo-cloudwatch-logger/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'will@code.org'
 license          'All rights reserved'
 description      'Installs/Configures cdo-cloudwatch-logger'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.3'
 
 depends 'ark'
 depends 'poise-service'
