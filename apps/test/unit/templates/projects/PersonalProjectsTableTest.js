dcc38d24de05f64ee90bc232a3833e5f93b2f954
Erin Bond <erin.bond@code.org>
2018-08-13 18:08:52 -0700

    import projectConstants correctly in PersonalProjectsTableTest

--- apps/test/unit/templates/projects/PersonalProjectsTableTest.js
+++ apps/test/unit/templates/projects/PersonalProjectsTableTest.js
@@ -5,7 +5,7 @@ import {Provider} from 'react-redux';
 import {getStore} from '@cdo/apps/redux';
 import {UnconnectedPersonalProjectsTable as PersonalProjectsTable} from '@cdo/apps/templates/projects/PersonalProjectsTable';
 import {stubFakePersonalProjectData} from '@cdo/apps/templates/projects/generateFakeProjects';
-import {publishMethods} from './projectConstants';
+import {publishMethods} from '@cdo/apps/templates/projects/projectConstants';
 
 describe('PersonalProjectsTable', () => {
   it('renders a table', () => {
