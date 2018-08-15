1e63f249d9baf1cec6ee775186d998bc03fd53d6
Mehal Shah <mehal@code.org>
2018-08-15 10:37:10 -0700

    Facilitator Filter for workshops

--- dashboard/app/controllers/api/v1/pd/course_facilitators_controller.rb
+++ dashboard/app/controllers/api/v1/pd/course_facilitators_controller.rb
@@ -3,8 +3,7 @@ class Api::V1::Pd::CourseFacilitatorsController < ApplicationController
 
   # GET /api/v1/pd/course_facilitators
   def index
-    course = params.require(:course)
-    facilitators = Pd::CourseFacilitator.facilitators_for_course course
+    facilitators = params.has_key?(:course) ? Pd::CourseFacilitator.facilitators_for_course(params.require(:course)) : Pd::CourseFacilitator.all.map(&:facilitator)
     render json: facilitators, each_serializer: Api::V1::Pd::CourseFacilitatorSerializer
   end
 end
