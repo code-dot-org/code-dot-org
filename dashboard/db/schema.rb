c07638dd4da066db88fb9ae572c4afb39acd50a9
Eric Jordan <ewjordan@gmail.com>
2018-08-15 12:42:22 -0700

    Update schema.rb

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180806214228) do
+
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
     t.integer  "level_id"
