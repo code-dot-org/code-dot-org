4e9f972fe3de54955bf065defe1484ac92bc1b1f
Continuous Integration <dev@code.org>
2018-08-16 22:18:10 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/libraries/VectorLab.interpreted.js
+++ dashboard/config/libraries/VectorLab.interpreted.js
@@ -4,6 +4,27 @@ function register(callback) {
   callbacks.push(callback);
 }
 
+function Sprite() {
+  this.sprite = createSprite();
+}
+
+Sprite.prototype.teleportTo = function (vector) {
+  this.sprite.position = vector.copy();
+};
+
+Sprite.prototype.push = function (vector) {
+  this.sprite.velocity = vector.copy();
+};
+
+Sprite.prototype.setColor = function (color) {
+  this.sprite.shapeColor = color;
+};
+
+Sprite.prototype.setSize = function (width, height) {
+  this.sprite.width = width;
+  this.sprite.height = height;
+};
+
 function draw() {
   background('#444');
   callbacks.forEach(function (callback) {
