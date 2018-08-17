729f07442906f815d993ff6be5b9d4b694ec8e43
Will Jordan <will@code.org>
2018-08-17 16:45:36 -0700

    Cloudwatch centralized logging updates - Update cdo-awscli cookbook to install specific version from python-pip - Install awscli-cwlogs package from cdo-awscli - Use aws logs push to forward syslog entries to cloudwatch logs - Install rsyslog v8-stable from ppa - Update rsyslog configuration to log locally to fixed-size rotated file

--- cookbooks/cdo-cloudwatch-logger/metadata.rb
+++ cookbooks/cdo-cloudwatch-logger/metadata.rb
@@ -6,5 +6,6 @@ description      'Installs/Configures cdo-cloudwatch-logger'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.7'
 
-depends 'ark'
 depends 'poise-service'
+depends 'cdo-awscli'
+depends 'sudo-user'
