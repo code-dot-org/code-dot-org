b3a737f3a97101eb8da746f2488934cda7ad0463
Brad Buchanan <brad@code.org>
2018-08-23 17:31:29 -0700

    Remove all of user's rows from unexpected_teachers_workshops [skip ui]

--- lib/cdo/delete_accounts_helper.rb
+++ lib/cdo/delete_accounts_helper.rb
@@ -110,6 +110,13 @@ class DeleteAccountsHelper
 
     SurveyResult.where(user_id: user_id).destroy_all
 
+    # Most efficient query to find and remove records from many-to-many join
+    # table unexpected_teachers_workshops without a corresponding model
+    ActiveRecord::Base.connection.execute(<<-SQL)
+      DELETE FROM unexpected_teachers_workshops
+      WHERE unexpected_teacher_id = '#{user_id}'
+    SQL
+
     unless application_ids.empty?
       Pd::FitWeekend1819Registration.where(pd_application_id: application_ids).update_all(form_data: '{}')
       Pd::Application::ApplicationBase.with_deleted.where(id: application_ids).update_all(form_data: '{}', notes: nil)
