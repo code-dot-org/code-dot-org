95ac5fb7122cbb27405e75b4bc741f60683c921e
Andrew Oberhardt <Andrew@Code.org>
2018-08-26 11:44:36 -0700

    Fix tests

--- dashboard/test/ui/features/step_definitions/pd.rb
+++ dashboard/test/ui/features/step_definitions/pd.rb
@@ -125,7 +125,7 @@ Given(/^I navigate to the principal approval page for "([^"]*)"$/) do |name|
   require_rails_env
 
   user = User.find_by_email @users[name][:email]
-  application = Pd::Application::Teacher1819Application.find_by(user: user)
+  application = Pd::Application::Teacher1920Application.find_by(user: user)
 
   steps %Q{
     And I am on "http://studio.code.org/pd/application/principal_approval/#{application.application_guid}"
@@ -157,8 +157,8 @@ Given(/^I am a teacher named "([^"]*)" going to TeacherCon and am on the Teacher
 
   teacher = FactoryGirl.create :teacher, name: name, password: teacher_password, email: teacher_email, school_info: SchoolInfo.first
   teachercon = FactoryGirl.create :pd_workshop, :teachercon, num_sessions: 5, organizer: (FactoryGirl.create :workshop_organizer, email: "organizer_#{SecureRandom.hex}@code.org"), processed_location: {city: 'Seattle'}.to_json
-  application_hash = FactoryGirl.build :pd_teacher1819_application_hash, school: School.first, preferred_first_name: 'Minerva', last_name: 'McGonagall'
-  application = FactoryGirl.create :pd_teacher1819_application, :locked, user: teacher, form_data: application_hash.to_json
+  application_hash = FactoryGirl.build :pd_teacher1920_application_hash, school: School.first, preferred_first_name: 'Minerva', last_name: 'McGonagall'
+  application = FactoryGirl.create :pd_teacher1920_application, :locked, user: teacher, form_data: application_hash.to_json
   application.update(pd_workshop_id: teachercon.id)
 
   steps %Q{
@@ -206,13 +206,13 @@ And(/^I create some fake applications of each type and status$/) do
     end
   end
 
-  if Pd::Application::Teacher1819Application.count < 100
+  if Pd::Application::Teacher1920Application.count < 100
     %w(csd csp).each do |course|
       (Pd::Application::ApplicationBase.statuses.values - ['interview']).each do |status|
         10.times do
           teacher = FactoryGirl.create(:teacher, school_info: SchoolInfo.first, email: "teacher_#{SecureRandom.hex}@code.org")
-          application_hash = FactoryGirl.build(:pd_teacher1819_application_hash, course.to_sym, school: School.first)
-          application = FactoryGirl.create(:pd_teacher1819_application, form_data_hash: application_hash, course: course, user: teacher)
+          application_hash = FactoryGirl.build(:pd_teacher1920_application_hash, course.to_sym, school: School.first)
+          application = FactoryGirl.create(:pd_teacher1920_application, form_data_hash: application_hash, course: course, user: teacher)
           application.update(status: status)
         end
       end
