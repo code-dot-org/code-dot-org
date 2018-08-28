c8047009d40e0bc7c8625794aad245a58f283972
Clare Constantine <clare@code.org>
2018-08-28 15:24:06 -0700

    remove migration

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -651,7 +651,6 @@ ActiveRecord::Schema.define(version: 20180823163642) do
     t.integer  "completed_survey_id"
     t.integer  "school_info_id"
     t.datetime "deleted_at"
-    t.text     "properties",          limit: 65535
     t.index ["code"], name: "index_pd_enrollments_on_code", unique: true, using: :btree
     t.index ["email"], name: "index_pd_enrollments_on_email", using: :btree
     t.index ["pd_workshop_id"], name: "index_pd_enrollments_on_pd_workshop_id", using: :btree
