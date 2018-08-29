047014ff1851c7a3af8c815be0f02b5af513d311
Continuous Integration <dev@code.org>
2018-08-29 22:18:06 +0000

    levelbuilder content changes (-robo-commit)

---
+++ dashboard/config/blocks/DancelabAlpha/DancelabAlpha_startMapping.js
@@ -0,0 +1,20 @@
+function startMapping(sprite, property, range) {
+  var behavior = new Behavior(function(sprite) {
+    var energy = Dance.fft.getEnergy(range);
+    if (property == "x") {
+      energy = Math.round(map(energy, 0, 255, 50, 350));
+    } else if (property == "y") {
+      energy = Math.round(map(energy, 0, 255, 350, 50));
+    } else if (property == "scale") {
+      energy = map(energy, 0, 255, 0, 2);
+    } else if (property == "rotation" || property == "direction") {
+      energy = Math.round(map(energy, 0, 255, -180, 180));
+    } else if (property == "tint") {
+      energy = Math.round(map(energy, 0, 255, 0, 360));
+      energy = "hsb(" + energy + ",100%,100%)";
+    }
+    sprite[property] = energy;
+  }, [property, range]);
+  behavior.func.name = "mapping" + property + range;
+  addBehavior(sprite, behavior);
+}
