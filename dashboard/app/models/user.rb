9342190dff19ee5e670997811490351bc8e8d46d
David Bailey <davidsbailey@users.noreply.github.com>
2018-09-04 09:54:03 -0700

    don't default to hidden sections

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -1532,7 +1532,7 @@ class User < ActiveRecord::Base
 
   # return the id of the section the user most recently created.
   def last_section_id
-    teacher? ? sections.last&.id : nil
+    teacher? ? sections.where(hidden: false).last&.id : nil
   end
 
   # The section which the user most recently joined as a student, or nil if none exists.
