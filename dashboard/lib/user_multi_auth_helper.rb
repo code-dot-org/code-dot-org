07e9e35aa4af789c982c8845af93e1646dadd1e4
Maddie Kasula <madelynkasula@gmail.com>
2018-09-12 09:21:13 -0700

    Find authentication option by authentication_id instead of user_id

--- dashboard/lib/user_multi_auth_helper.rb
+++ dashboard/lib/user_multi_auth_helper.rb
@@ -30,7 +30,7 @@ module UserMultiAuthHelper
 
     credentials_hash = auth_hash[:credentials]
     if migrated?
-      auth_option = authentication_options.find_by(credential_type: auth_hash[:provider], user_id: id)
+      auth_option = authentication_options.find_by(credential_type: auth_hash[:provider], authentication_id: auth_hash[:uid])
       auth_option&.update_oauth_credential_tokens(credentials_hash)
     else
       self.oauth_refresh_token = credentials_hash[:refresh_token] if credentials_hash[:refresh_token].present?
