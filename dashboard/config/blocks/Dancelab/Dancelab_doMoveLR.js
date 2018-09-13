aec5548767de6c23caa33b65c67e9f013aee60e6
Continuous Integration <dev@code.org>
2018-09-13 22:18:05 +0000

    levelbuilder content changes (-robo-commit)

---
+++ dashboard/config/blocks/Dancelab/Dancelab_doMoveLR.js
@@ -0,0 +1,14 @@
+function doMoveLR(sprite, move, dir) {
+  if (move == "next") {
+    move = (sprite.current_move + 1) % dancers[sprite.style].length;
+  } else if (move == "prev") {
+    move = (sprite.current_move - 1) % dancers[sprite.style].length;
+  } else if (move == "rand") {
+    move = randomNumber(0, dancers[sprite.style].length - 1);
+  }
+  sprite.mirrorX(dir);
+  sprite.changeAnimation("anim" + move);
+  sprite.animation.looping = false;
+  sprite.animation.changeFrame(24);
+  sprite.animation.play();
+}
