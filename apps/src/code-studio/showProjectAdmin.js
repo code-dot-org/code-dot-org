b07bda846b69507c1fe5b8f78c9cb4403be6e202
Erin Bond <erin.bond@code.org>
2018-09-18 12:00:33 -0700

    remove alerts for en/disable auto content moderation

--- apps/src/code-studio/showProjectAdmin.js
+++ apps/src/code-studio/showProjectAdmin.js
@@ -84,7 +84,6 @@ export default () => {
 
   $('#disable-auto-moderation').click(async function () {
     await dashboard.project.disableAutoContentModeration();
-    alert("Our moderation filter is currently turned off for all projects, but your choice will be saved and applied when we re-enable the moderation service.");
     $('#disable-auto-moderation').hide();
     $('#moderation-explanation').hide();
     $('#enable-auto-moderation').show();
@@ -92,7 +91,6 @@ export default () => {
 
   $('#enable-auto-moderation').click(async function () {
     await dashboard.project.enableAutoContentModeration();
-    alert("Our moderation filter is currently turned off for all projects, but your choice will be saved and applied when we re-enable the moderation service.");
     $('#disable-auto-moderation').show();
     $('#moderation-explanation').show();
     $('#enable-auto-moderation').hide();
