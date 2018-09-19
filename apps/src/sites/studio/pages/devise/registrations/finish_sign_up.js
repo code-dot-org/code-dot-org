9be4a6cba365e2a54db05fd3c186565494c3f7f5
Maddie Kasula <madelynkasula@gmail.com>
2018-09-18 21:44:01 -0700

    Organize finish_sign_up fields into constants with helpers

--- apps/src/sites/studio/pages/devise/registrations/finish_sign_up.js
+++ apps/src/sites/studio/pages/devise/registrations/finish_sign_up.js
@@ -1,41 +1,47 @@
 import $ from 'jquery';
 
+const TEACHER_ONLY_FIELDS = ["#teacher-name-field", "#email-preference-dropdown"];
+const STUDENT_ONLY_FIELDS = ["#student-name-field", "#age-dropdown", "#student-consent"];
+const SHARED_FIELDS = ["#gender-dropdown", "#terms-of-service"];
+const ALL_FIELDS = [TEACHER_ONLY_FIELDS, STUDENT_ONLY_FIELDS, SHARED_FIELDS];
+
 $(document).ready(() => {
   $("#user_user_type").change(function () {
     var value = $(this).val();
     switch (value) {
       case "teacher":
-        showTeacher();
+        switchToTeacher();
         break;
       case "student":
-        showStudent();
+        switchToStudent();
         break;
       default:
-        // hide everything
+        hideFields(ALL_FIELDS); // Hide everything
     }
   });
 
-  function showTeacher() {
-    // Show teacher fields
-    $("#teacher-name-field").fadeIn();
-    $("#gender-dropdown").fadeIn();
-    $("#email-preference-dropdown").fadeIn();
-    $("#terms-of-service").fadeIn();
+  function switchToTeacher() {
+    // Show teacher and shared fields
+    fadeInFields(TEACHER_ONLY_FIELDS);
+    fadeInFields(SHARED_FIELDS);
+
     // Hide student fields
-    $("#student-name-field").hide();
-    $("#age-dropdown").hide();
-    $("#student-consent").hide();
+    hideFields(STUDENT_ONLY_FIELDS);
   }
 
-  function showStudent() {
-    // Show student fields
-    $("#student-name-field").fadeIn();
-    $("#age-dropdown").fadeIn();
-    $("#gender-dropdown").fadeIn();
-    $("#terms-of-service").fadeIn();
-    $("#student-consent").fadeIn();
-    // Hide teacher fields
-    $("#teacher-name-field").hide();
-    $("#email-preference-dropdown").hide();
+  function switchToStudent() {
+    // Show student and shared fields
+    fadeInFields(STUDENT_ONLY_FIELDS);
+    fadeInFields(SHARED_FIELDS);
+
+    hideFields(TEACHER_ONLY_FIELDS); // Hide teacher fields
+  }
+
+  function fadeInFields(fields) {
+    $(fields.join(', ')).fadeIn();
+  }
+
+  function hideFields(fields) {
+    $(fields.join(', ')).hide();
   }
 });
