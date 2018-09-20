d155ca3d34a7f9dfc9811d7cbab8e0afe094e434
Brendan Reville <brendan@code.org>
2018-09-20 00:05:07 -0700

    PL regional partner search: support for closed dates

--- dashboard/app/serializers/api/v1/pd/regional_partner_serializer.rb
+++ dashboard/app/serializers/api/v1/pd/regional_partner_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::Pd::RegionalPartnerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :group, :contact, :summer_workshops, :csd_cost, :csp_cost, :apps_open_date, :link_to_partner_application, :cost_scholarship_information, :additional_program_information
+  attributes :id, :name, :group, :contact, :summer_workshops, :csd_cost, :csp_cost, :apps_dates, :link_to_partner_application, :cost_scholarship_information, :additional_program_information
 
   def contact
     object.contact.slice(:email, :name)
@@ -9,10 +9,11 @@ class Api::V1::Pd::RegionalPartnerSerializer < ActiveModel::Serializer
     object.upcoming_summer_workshops
   end
 
-  def apps_open_date
+  def apps_dates
     {
       open_now: object.earliest_summer_workshop_apps_open_date < Time.zone.now,
-      earliest_open_date: object.earliest_summer_workshop_apps_open_date
+      earliest_open_date: object.earliest_summer_workshop_apps_open_date,
+      closed_now: object.latest_summer_workshop_apps_close_date < Time.zone.now
     }
   end
 end
