977fbec35674ca78885aeac4d19570ea3bb2f970
Brad Buchanan <brad@code.org>
2018-09-20 17:00:05 -0700

    Set up A/B test sending some Google and OAuth users to the finish_sign_up page

--- dashboard/test/controllers/registrations_controller/new_account_tracking_test.rb
+++ dashboard/test/controllers/registrations_controller/new_account_tracking_test.rb
@@ -35,7 +35,7 @@ module RegistrationsControllerTests
     setup do
       SecureRandom.stubs(:uuid).returns(UUID)
       OmniAuth.config.test_mode = true
-      OmniAuth.config.mock_auth[:google_oauth2] = generate_auth_user_hash(provider: 'google_oauth2')
+      SignUpTracking.stubs(:split_test_percentage).returns(0)
     end
 
     test 'loading sign up page sends Firehose page load event' do
@@ -48,10 +48,15 @@ module RegistrationsControllerTests
     end
 
     test 'successful email sign up sends Firehose success event' do
-      events = %w(load-sign-up-page email-sign-up-success)
-      FirehoseClient.instance.expects(:put_record).times(2).with do |data|
+      FirehoseClient.instance.expects(:put_record).with do |data|
         data[:study] == STUDY &&
-          data[:event] == events.shift &&
+          data[:event] == 'load-sign-up-page' &&
+          data[:data_string] == UUID
+      end
+      FirehoseClient.instance.expects(:put_record).with do |data|
+        data[:study] == STUDY &&
+          data[:study_group] == SignUpTracking::NOT_IN_STUDY_GROUP &&
+          data[:event] == 'email-sign-up-success' &&
           data[:data_string] == UUID
       end
 
@@ -65,10 +70,15 @@ module RegistrationsControllerTests
     end
 
     test 'email sign up with wrong password confirmation sends Firehose error event' do
-      events = %w(load-sign-up-page email-sign-up-error)
-      FirehoseClient.instance.expects(:put_record).times(2).with do |data|
+      FirehoseClient.instance.expects(:put_record).with do |data|
         data[:study] == STUDY &&
-          data[:event] == events.shift &&
+          data[:event] == 'load-sign-up-page' &&
+          data[:data_string] == UUID
+      end
+      FirehoseClient.instance.expects(:put_record).with do |data|
+        data[:study] == STUDY &&
+          data[:study_group] == SignUpTracking::NOT_IN_STUDY_GROUP &&
+          data[:event] == 'email-sign-up-error' &&
           data[:data_string] == UUID
       end
 
@@ -82,15 +92,22 @@ module RegistrationsControllerTests
     end
 
     test 'successful oauth sign up after hitting sign up page sends Firehose success events' do
-      events = %w(load-sign-up-page google_oauth2-sign-up-success)
-      FirehoseClient.instance.expects(:put_record).times(2).with do |data|
+      OmniAuth.config.mock_auth[:google_oauth2] = generate_auth_user_hash(
+        provider: 'google_oauth2'
+      )
+      FirehoseClient.instance.expects(:put_record).with do |data|
         data[:study] == STUDY &&
-          data[:event] == events.shift &&
+          data[:event] == 'load-sign-up-page' &&
           data[:data_string] == UUID
       end
-
       get '/users/sign_up'
 
+      FirehoseClient.instance.expects(:put_record).with do |data|
+        data[:study] == STUDY &&
+          data[:study_group] == SignUpTracking::CONTROL_GROUP &&
+          data[:event] == 'google_oauth2-sign-up-success' &&
+          data[:data_string] == UUID
+      end
       @request.env["devise.mapping"] = Devise.mappings[:user]
       assert_creates(User) do
         get '/users/auth/google_oauth2'
@@ -98,7 +115,54 @@ module RegistrationsControllerTests
       end
     end
 
+    test 'successful oauth sign up in split test reports split' do
+      # Google Oauth doesn't normally give us a user-type by default.
+      OmniAuth.config.mock_auth[:google_oauth2] = generate_auth_user_hash(
+        provider: 'google_oauth2',
+        user_type: ''
+      )
+      SignUpTracking.stubs(:split_test_percentage).returns(100)
+
+      # /users/sign_up reports a load event
+      FirehoseClient.instance.expects(:put_record).with do |data|
+        data[:study] == STUDY &&
+          data[:event] == 'load-sign-up-page' &&
+          data[:data_string] == UUID
+      end
+      get '/users/sign_up'
+
+      # The user clicks Google. The oauth endpoint (which is in test mode here)
+      # redirects to the oauth callback
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      get '/users/auth/google_oauth2'
+      assert_redirected_to '/users/auth/google_oauth2/callback'
+
+      # /users/auth/google_oauth2/callback reports a sign-up error
+      # because the user couldn't be immediately created (we might want
+      # to adjust this) and sends us to /users/finish_sign_up
+      FirehoseClient.instance.expects(:put_record).with do |data|
+        data[:study] == STUDY &&
+          data[:study_group] == SignUpTracking::NEW_SIGN_UP_GROUP &&
+          data[:event] == 'google_oauth2-sign-up-error' &&
+          data[:data_string] == UUID
+      end
+      follow_redirect!
+      assert_redirected_to '/users/finish_sign_up'
+
+      # We end up on the finish_sign_up page
+      follow_redirect!
+
+      # TODO: Update test to perform final user creation in this flow
+      # assert_creates User do
+      #   post '/users', params: {user: {user_type: 'teacher'}}
+      # end
+      # assert_redirected_to '/home'
+    end
+
     test 'login to existing account after hitting sign up page sends Firehose sign in event' do
+      OmniAuth.config.mock_auth[:google_oauth2] = generate_auth_user_hash(
+        provider: 'google_oauth2'
+      )
       create :teacher, :unmigrated_google_sso, uid: DEFAULT_UID, email: EMAIL
       events = %w(load-sign-up-page google_oauth2-sign-in)
       FirehoseClient.instance.expects(:put_record).times(2).with do |data|
@@ -116,6 +180,9 @@ module RegistrationsControllerTests
     end
 
     test 'tracking cookie is cleared when hitting another random page on the site' do
+      OmniAuth.config.mock_auth[:google_oauth2] = generate_auth_user_hash(
+        provider: 'google_oauth2'
+      )
       create :teacher, :unmigrated_google_sso, uid: DEFAULT_UID, email: EMAIL
       events = %w(load-sign-up-page)
       FirehoseClient.instance.expects(:put_record).once.with do |data|
