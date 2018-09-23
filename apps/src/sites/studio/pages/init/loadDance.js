20372e8e1c5afd2e6cb04f3af61fd20565e5c5b5
Josh Lory <josh.lory@code.org>
2018-09-22 19:58:20 -0700

    Add app skeleton for Dance

---
+++ apps/src/sites/studio/pages/init/loadDance.js
@@ -0,0 +1,14 @@
+import appMain from "@cdo/apps/appMain";
+import {singleton as studioApp} from '@cdo/apps/StudioApp';
+import Dance from "@cdo/apps/dance/Dance";
+import blocks from "@cdo/apps/gamelab/blocks";
+
+export default function loadGamelab(options) {
+  options.blocksModule = blocks;
+  const dance = new Dance();
+
+  dance.injectStudioApp(studioApp());
+  appMain(dance, {custom: {}}, options);
+
+  return dance;
+}
