5d9d0871eb47e8b34f69eb08d095e01d4798d603
Maddie Kasula <madelynkasula@gmail.com>
2018-09-24 11:08:32 -0700

    Update omniauth tokens for in google_oauth2 and clever callbacks for users signing in

--- dashboard/app/controllers/omniauth_callbacks_controller.rb
+++ dashboard/app/controllers/omniauth_callbacks_controller.rb
@@ -16,6 +16,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     user = find_user_by_credential
     if user
+      user.update_oauth_credential_tokens(auth_hash)
       sign_in_clever user
     else
       sign_up_clever
@@ -31,6 +32,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     user = find_user_by_credential
     if user
+      user.update_oauth_credential_tokens(auth_hash)
       sign_in_google_oauth2 user
     else
       sign_up_google_oauth2
