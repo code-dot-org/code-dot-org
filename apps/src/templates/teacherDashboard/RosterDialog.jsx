22d5e7ef2ea4538b8c22f0e4140115e1bf5d27ac
Maddie Kasula <madelynkasula@gmail.com>
2018-09-26 14:03:49 -0700

    Update LoadError in RosterDialog to include support article for troubleshooting

--- apps/src/templates/teacherDashboard/RosterDialog.jsx
+++ apps/src/templates/teacherDashboard/RosterDialog.jsx
@@ -55,11 +55,6 @@ const styles = {
     color: '#5b6770',
     border: '1px solid #c5c5c5',
   },
-  error: {
-    fontSize: 10,
-    color: color.light_gray,
-    display: 'none',
-  },
 };
 
 const ClassroomList = ({classrooms, onSelect, selectedId, rosterProvider}) => classrooms.length ?
@@ -123,26 +118,39 @@ NoClassroomsFound.propTypes = {
   rosterProvider: PropTypes.oneOf(Object.keys(OAuthSectionTypes)),
 };
 
-const LoadError = ({error, rosterProvider, loginType}) =>
-  <div>
-    <p>
-      {locale.errorLoadingRosteredSections({type: loginType})}
-    </p>
-    {rosterProvider === OAuthSectionTypes.google_classroom &&
-      <p>
-        {locale.authorizeGoogleClassroomsText()}
-        {' '}
-        <a href={`/users/auth/google_oauth2?scope=userinfo.email,userinfo.profile,classroom.courses.readonly,classroom.rosters.readonly`}>
-          {locale.authorizeGoogleClassrooms()}
-        </a>
-      </p>
-    }
-    <p style={styles.error}>
-      {error.status} {error.message}
-    </p>
-  </div>;
+const ROSTERED_SECTIONS_SUPPORT_URL = 'https://support.code.org/hc/en-us/articles/115001319312';
+const LoadError = ({rosterProvider, loginType}) => {
+  switch (rosterProvider) {
+    case OAuthSectionTypes.google_classroom:
+      return (
+        <div>
+          <p>
+            {locale.authorizeGoogleClassroomsText()}
+            {' '}
+            <a href={`/users/auth/google_oauth2?scope=userinfo.email,userinfo.profile,classroom.courses.readonly,classroom.rosters.readonly`}>
+              {locale.authorizeGoogleClassrooms()}
+            </a>
+          </p>
+          <p>
+            <a href={ROSTERED_SECTIONS_SUPPORT_URL}>
+              {locale.errorLoadingRosteredSectionsSupport()}
+            </a>
+          </p>
+        </div>
+      );
+    default:
+      return (
+        <p>
+          {locale.errorLoadingRosteredSections({type: loginType})}
+          {' '}
+          <a href={ROSTERED_SECTIONS_SUPPORT_URL}>
+            {locale.errorLoadingRosteredSectionsSupport()}
+          </a>
+        </p>
+      );
+  }
+};
 LoadError.propTypes = {
-  error: loadErrorShape,
   rosterProvider: PropTypes.string,
   loginType: PropTypes.string,
 };
@@ -206,7 +214,6 @@ class RosterDialog extends React.Component {
         <div style={styles.content}>
           {this.props.loadError ?
             <LoadError
-              error={this.props.loadError}
               rosterProvider={this.props.rosterProvider}
               loginType={loginType}
             /> :
