76c93cf99bcbe29f4a0d94b6e32a8ba9f9483531
Brad Buchanan <brad@code.org>
2018-09-27 15:34:34 -0700

    Extract common assert_credentials

--- dashboard/test/controllers/omniauth_callbacks_controller/utils.rb
+++ dashboard/test/controllers/omniauth_callbacks_controller/utils.rb
@@ -24,5 +24,13 @@ module OmniauthCallbacksControllerTests
       assert_redirected_to "/users/auth/#{provider}/callback"
       follow_redirect!
     end
+
+    def assert_credentials(from_auth_hash, on_created_user)
+      assert_equal from_auth_hash.provider, on_created_user.provider
+      assert_equal from_auth_hash.uid, on_created_user.uid
+      assert_equal from_auth_hash.credentials.token, on_created_user.oauth_token
+      assert_equal from_auth_hash.credentials.expires_at, on_created_user.oauth_token_expiration
+      assert_equal from_auth_hash.credentials.refresh_token, on_created_user.oauth_refresh_token
+    end
   end
 end
