759931f66d3537962825f2feabb7713e61a4d599
Brad Buchanan <brad@code.org>
2018-09-26 18:25:17 -0700

    Fix broken test

--- dashboard/test/controllers/registrations_controller/new_test.rb
+++ dashboard/test/controllers/registrations_controller/new_test.rb
@@ -5,7 +5,8 @@ module RegistrationsControllerTests
   # Tests over GET /users/sign_up
   #
   class NewTest < ActionDispatch::IntegrationTest
-    test 'renders finish_sign_up if partial registration is in progress' do
+    test 'renders finish_sign_up if new flow and partial registration is in progress' do
+      SignUpTracking.expects(:new_sign_up_experience?).returns(true)
       PartialRegistration.expects(:in_progress?).returns(true).twice
       User.expects(:new_with_session).returns(build(:user))
 
@@ -15,8 +16,19 @@ module RegistrationsControllerTests
     end
 
     test 'renders sign_up if partial registration is not in progress' do
+      SignUpTracking.expects(:new_sign_up_experience?).returns(true)
       SignUpTracking.expects(:begin_sign_up_tracking)
       FirehoseClient.instance.expects(:put_record)
+
+      get '/users/sign_up'
+      assert_response :success
+      assert_template partial: '_sign_up'
+    end
+
+    test 'renders sign_up if not in new flow' do
+      SignUpTracking.stubs(:new_sign_up_experience?).returns(false)
+      PartialRegistration.stubs(:in_progress?).returns(true)
+      User.expects(:new_with_session).returns(build(:user))
 
       get '/users/sign_up'
       assert_response :success
