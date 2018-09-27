48f1c1def556ad2d8c425f160d1d83627e859608
Brad Buchanan <brad@code.org>
2018-09-26 17:18:16 -0700

    student sign-up (new sign-up flow)

--- dashboard/test/controllers/omniauth_callbacks_controller/google_oauth2_test.rb
+++ dashboard/test/controllers/omniauth_callbacks_controller/google_oauth2_test.rb
@@ -25,7 +25,44 @@ module OmniauthCallbacksControllerTests
     end
 
     test "student sign-up (new sign-up flow)" do
-      skip 'not implemented'
+      mock_oauth
+      SignUpTracking.stubs(:split_test_percentage).returns(100)
+
+      # User visits the sign-up page
+      get '/users/sign_up'
+
+      # The user clicks "Sign in with Google Account".
+      # The oauth endpoint (which is mocked) redirects to the oauth callback,
+      # which in turn redirects to the finish-sign-up experience.
+      get '/users/auth/google_oauth2'
+      assert_redirected_to '/users/auth/google_oauth2/callback'
+      follow_redirect!
+      assert_redirected_to '/users/sign_up'
+      follow_redirect!
+      assert_template partial: '_finish_sign_up'
+
+      # The user fills out the form and clicks "Sign up"
+      assert_creates User do
+        post '/users', params: finish_sign_up_params(user_type: User::TYPE_STUDENT)
+      end
+      assert_redirected_to '/'
+      follow_redirect!
+      assert_redirected_to '/home'
+      assert_equal I18n.t('devise.registrations.signed_up'), flash[:notice]
+
+      # Locate new user, make sure it has all the oauth info we'd expect
+      created_user = User.find_by_credential(
+        type: @auth_hash.provider,
+        id: @auth_hash.uid
+      )
+      assert created_user.valid?
+      assert created_user.student?
+      assert_equal User.hash_email(@auth_hash.info.email), created_user.hashed_email
+      assert_equal @auth_hash.credentials.token, created_user.oauth_token
+      assert_equal @auth_hash.credentials.expires_at, created_user.oauth_token_expiration
+      assert_equal @auth_hash.credentials.refresh_token, created_user.oauth_refresh_token
+    ensure
+      created_user&.destroy!
     end
 
     test "teacher sign-up (new sign-up flow)" do
@@ -105,20 +142,38 @@ module OmniauthCallbacksControllerTests
     end
 
     def finish_sign_up_params(override_params)
-      {
-        user: {
-          locale: 'en-US',
-          user_type: User::TYPE_STUDENT,
-          name: @auth_hash.info.name,
-          age: '21+',
-          gender: nil,
-          school_info_attributes: {
-            country: 'US'
-          },
-          terms_of_service_version: 1,
-          email_preference_opt_in: 'yes',
-        }.merge(override_params)
-      }
+      user_type = override_params[:user_type] || User::TYPE_STUDENT
+      if user_type == User::TYPE_STUDENT
+        {
+          user: {
+            locale: 'en-US',
+            user_type: user_type,
+            name: @auth_hash.info.name,
+            age: '13',
+            gender: 'f',
+            school_info_attributes: {
+              country: 'US'
+            },
+            terms_of_service_version: 1,
+            email_preference_opt_in: nil,
+          }.merge(override_params)
+        }
+      else
+        {
+          user: {
+            locale: 'en-US',
+            user_type: user_type,
+            name: @auth_hash.info.name,
+            age: '21+',
+            gender: nil,
+            school_info_attributes: {
+              country: 'US'
+            },
+            terms_of_service_version: 1,
+            email_preference_opt_in: 'yes',
+          }.merge(override_params)
+        }
+      end
     end
   end
 end
