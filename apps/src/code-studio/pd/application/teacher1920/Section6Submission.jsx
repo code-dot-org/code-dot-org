45fffc4232d33ec5ce86134fa46cca98796a7fb7
Mehal Shah <mehal@code.org>
2018-09-27 10:17:19 -0700

    PR Feedback

--- apps/src/code-studio/pd/application/teacher1920/Section6Submission.jsx
+++ apps/src/code-studio/pd/application/teacher1920/Section6Submission.jsx
@@ -38,7 +38,7 @@ export default class Section6Submission extends LabeledFormComponent {
           with our Regional Partners - all information will be de-identified and
           aggregated. Our Regional Partners are contractually obliged to treat this
           information with the same level of confidentiality as Code.org. To see the full
-          Code.org privacy policy, visit {' '}
+          Code.org privacy policy, visit{' '}
           <a href="http://code.org/privacy" target="_blank">
             code.org/privacy
           </a>
