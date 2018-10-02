ba2695d0d695aeb1f6b5bae8578aa7b79d88f557
Chris Pirich <chris@code.org>
2018-10-02 14:08:20 -0700

    playSound() callback, audio preload improvements

--- apps/src/lib/util/audioApi.js
+++ apps/src/lib/util/audioApi.js
@@ -34,14 +34,20 @@ export const commands = {
    * _@param {boolean} [opts.allowMultiple] If false (default) this call will
    *        stop other instances of the same sound from playing.  If true,
    *        multiple instances of the sound may be played simultaneously.
+   * _@param {function} [opts.callback] Called back when the sound starts playing
+   *        with an argument of true. If the sound fails to play, called back
+   *        with an argument of false.
    */
   playSound(opts) {
     apiValidateType(opts, 'playSound', 'url', opts.url, 'string');
     apiValidateType(opts, 'playSound', 'loop', opts.loop, 'boolean', OPTIONAL);
+    apiValidateType(opts, 'playSound', 'callback', opts.callback, 'function', OPTIONAL);
 
     const url = assetPrefix.fixPath(opts.url);
     if (Sounds.getSingleton().isPlayingURL(url)) {
-      return;
+      if (opts.callback) {
+        opts.callback(false);
+      }
     }
 
     // TODO: Re-enable forceHTML5 after Varnish 4.1 upgrade.
@@ -73,7 +79,8 @@ export const commands = {
       volume: 1.0,
       loop: !!opts.loop,
       forceHTML5: forceHTML5,
-      allowHTML5Mobile: true
+      allowHTML5Mobile: true,
+      callback: opts.callback,
     });
   },
 
@@ -100,7 +107,7 @@ export const commands = {
  * arguments converted to an options object.
  */
 export const executors = {
-  playSound: (url, loop = false) => executeCmd(null, 'playSound', {url, loop}),
+  playSound: (url, loop = false, callback) => executeCmd(null, 'playSound', {url, loop, callback}),
   stopSound: (url) => executeCmd(null, 'stopSound', {url})
 };
 // Note to self - can we use _.zipObject to map argumentNames to arguments here?
@@ -120,6 +127,7 @@ export const dropletConfig = {
       0: () => getAssetDropdown('audio'),
       1: ["true", "false"]
     },
+    nativeCallsBackInterpreter: true,
     assetTooltip: { 0: chooseAsset.bind(null, 'audio') }
   },
   stopSound: {
