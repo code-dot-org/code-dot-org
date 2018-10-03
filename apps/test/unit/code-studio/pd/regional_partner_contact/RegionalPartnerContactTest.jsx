ee7f4339f97295f37bebd0a6dd7224b33810dbff
Brad Buchanan <brad@code.org>
2018-10-03 14:34:12 -0700

    Refactor a bit [skip ui]

--- apps/test/unit/code-studio/pd/regional_partner_contact/RegionalPartnerContactTest.jsx
+++ apps/test/unit/code-studio/pd/regional_partner_contact/RegionalPartnerContactTest.jsx
@@ -10,19 +10,22 @@ describe('RegionalPartnerContactTest', () => {
     gradeLevels: ['High School (9-12)', 'Middle School (6-8)', 'Elementary School (K-5)'],
     optIn: ['Yes', 'No']
   };
+  const FIELD_EXPECTATIONS = {
+    firstName: {type: 'FieldGroup', expectRequired: true},
+    lastName: {type: 'FieldGroup', expectRequired: true},
+    email: {type: 'FieldGroup', expectRequired: true},
+    role: {type: 'ButtonList', expectRequired: true},
+    jobTitle: {type: 'FieldGroup', expectRequired: false},
+    gradeLevels: {type: 'ButtonList', expectRequired: true},
+    notes: {type: 'FieldGroup', expectRequired: true},
+    optIn: {type: 'ButtonList', expectRequired: true},
+  };
 
   describe('Required fields', () => {
-    [
-      {fieldName: 'firstName', type: 'FieldGroup', expectedRequired: true},
-      {fieldName: 'lastName', type: 'FieldGroup', expectedRequired: true},
-      {fieldName: 'email', type: 'FieldGroup', expectedRequired: true},
-      {fieldName: 'role', type: 'ButtonList', expectedRequired: true},
-      {fieldName: 'jobTitle', type: 'FieldGroup', expectedRequired: false},
-      {fieldName: 'gradeLevels', type: 'ButtonList', expectedRequired: true},
-      {fieldName: 'notes', type: 'FieldGroup', expectedRequired: true},
-      {fieldName: 'optIn', type: 'ButtonList', expectedRequired: true},
-    ].forEach(({fieldName, type, expectedRequired}) => {
-      it(`${fieldName} is ${expectedRequired ? 'required' : 'optional'}`, () => {
+    Object.keys(FIELD_EXPECTATIONS).forEach((fieldName) => {
+      const expectRequired = FIELD_EXPECTATIONS[fieldName].expectRequired;
+
+      it(`${fieldName} is ${expectRequired ? 'required' : 'optional'}`, () => {
         const wrapper = mount(
           <RegionalPartnerContact
             apiEndpoint={API_ENDPOINT}
@@ -30,14 +33,17 @@ describe('RegionalPartnerContactTest', () => {
           />
         );
 
-        let field;
-        if (type === 'ButtonList') {
-          field = wrapper.find(type).filterWhere(c => c.prop('groupName') === fieldName);
-        } else {
-          field = wrapper.find(type).filterWhere(c => c.prop('id') === fieldName);
-        }
-        expect(field).to.have.prop('required', expectedRequired);
+        const field = findField(wrapper, fieldName);
+        expect(field).to.have.prop('required', expectRequired);
       });
     });
   });
+
+  function findField(wrapper, fieldName) {
+    const type = FIELD_EXPECTATIONS[fieldName].type;
+    if (type === 'ButtonList') {
+      return wrapper.find(type).filterWhere(c => c.prop('groupName') === fieldName);
+    }
+    return wrapper.find(type).filterWhere(c => c.prop('id') === fieldName);
+  }
 });
