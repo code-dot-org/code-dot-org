405a72cb7d81fedcb96ddcd2ad83760cd30f248a
Brad Buchanan <brad@code.org>
2018-10-03 14:27:00 -0700

    Test requiredness of all fields [ui skip]

--- apps/test/unit/code-studio/pd/regional_partner_contact/RegionalPartnerContactTest.jsx
+++ apps/test/unit/code-studio/pd/regional_partner_contact/RegionalPartnerContactTest.jsx
@@ -11,27 +11,33 @@ describe('RegionalPartnerContactTest', () => {
     optIn: ['Yes', 'No']
   };
 
-  it('Job Title is optional', () => {
-    const wrapper = mount(
-      <RegionalPartnerContact
-        apiEndpoint={API_ENDPOINT}
-        options={OPTIONS}
-      />
-    );
+  describe('Required fields', () => {
+    [
+      {fieldName: 'firstName', type: 'FieldGroup', expectedRequired: true},
+      {fieldName: 'lastName', type: 'FieldGroup', expectedRequired: true},
+      {fieldName: 'email', type: 'FieldGroup', expectedRequired: true},
+      {fieldName: 'role', type: 'ButtonList', expectedRequired: true},
+      {fieldName: 'jobTitle', type: 'FieldGroup', expectedRequired: false},
+      {fieldName: 'gradeLevels', type: 'ButtonList', expectedRequired: true},
+      {fieldName: 'notes', type: 'FieldGroup', expectedRequired: true},
+      {fieldName: 'optIn', type: 'ButtonList', expectedRequired: true},
+    ].forEach(({fieldName, type, expectedRequired}) => {
+      it(`${fieldName} is ${expectedRequired ? 'required' : 'optional'}`, () => {
+        const wrapper = mount(
+          <RegionalPartnerContact
+            apiEndpoint={API_ENDPOINT}
+            options={OPTIONS}
+          />
+        );
 
-    const fieldGroup = wrapper.find('FieldGroup').filterWhere(c => c.prop('id') === 'jobTitle');
-    expect(fieldGroup).to.have.prop('required', false);
-  });
-
-  it('Notes is required', () => {
-    const wrapper = mount(
-      <RegionalPartnerContact
-        apiEndpoint={API_ENDPOINT}
-        options={OPTIONS}
-      />
-    );
-
-    const fieldGroup = wrapper.find('FieldGroup').filterWhere(c => c.prop('id') === 'notes');
-    expect(fieldGroup).to.have.prop('required', true);
+        let field;
+        if (type === 'ButtonList') {
+          field = wrapper.find(type).filterWhere(c => c.prop('groupName') === fieldName);
+        } else {
+          field = wrapper.find(type).filterWhere(c => c.prop('id') === fieldName);
+        }
+        expect(field).to.have.prop('required', expectedRequired);
+      });
+    });
   });
 });
