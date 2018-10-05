22ff7d0a15ff443d04bf5eb671e557b13631ccbd
Maddie Kasula <madelynkasula@gmail.com>
2018-10-05 11:42:03 -0700

    Basic styling for new signup form

--- dashboard/app/controllers/registrations_controller.rb
+++ dashboard/app/controllers/registrations_controller.rb
@@ -20,7 +20,8 @@ class RegistrationsController < Devise::RegistrationsController
       @user = User.new_with_session(user_params, session)
     else
       @already_hoc_registered = params[:already_hoc_registered]
-      SignUpTracking.begin_sign_up_tracking(session)
+      SignUpTracking.begin_sign_up_tracking(session, split_test: true)
+      # SignUpTracking.begin_sign_up_tracking(session)
       super
     end
   end
