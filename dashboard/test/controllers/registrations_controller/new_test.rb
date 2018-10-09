0b034ac699d27708a3ededdbb0f4a9504a79e501
Maddie Kasula <madelynkasula@gmail.com>
2018-10-09 15:11:20 -0700

    Clean up GET /users/sign_up tests

--- dashboard/test/controllers/registrations_controller/new_test.rb
+++ dashboard/test/controllers/registrations_controller/new_test.rb
@@ -9,7 +9,7 @@ module RegistrationsControllerTests
     # OLD SIGNUP FLOW
     #
     test 'renders old signup form in old flow' do
-      SignUpTracking.expects(:new_sign_up_experience?).returns(false).times(3)
+      SignUpTracking.stubs(:new_sign_up_experience?).returns(false)
       PartialRegistration.stubs(:in_progress?).returns(true)
       User.expects(:new_with_session).returns(build(:user))
 
@@ -22,21 +22,21 @@ module RegistrationsControllerTests
     #
     # NEW SIGNUP FLOW
     #
-    test 'renders finish_sign_up partial registration is in progress in new flow' do
-      SignUpTracking.expects(:new_sign_up_experience?).returns(true).twice
-      PartialRegistration.expects(:in_progress?).returns(true).twice
-      User.expects(:new_with_session).returns(build(:user))
+    test 'renders new signup form if partial registration is not in progress in new flow' do
+      SignUpTracking.stubs(:new_sign_up_experience?).returns(true)
+      SignUpTracking.expects(:begin_sign_up_tracking)
 
       get '/users/sign_up'
       assert_response :success
-      assert_template partial: '_finish_sign_up'
+      assert_template partial: '_sign_up'
+      assert_template partial: '_new_sign_up_form'
     end
 
     # Note: The analogous test for the old signup flow is in registrations_controller_test.rb because
     # it requires access to the session, which ActionDispatch::IntegrationTest does not provide
     test "signup does not display errors on pageload in new flow" do
-      SignUpTracking.expects(:new_sign_up_experience?).returns(true).twice
-      PartialRegistration.expects(:in_progress?).returns(true).twice
+      SignUpTracking.stubs(:new_sign_up_experience?).returns(true)
+      PartialRegistration.stubs(:in_progress?).returns(true)
       user = build :student, provider: 'facebook', email: 'email@facebook.xx'
       User.expects(:new_with_session).returns(user)
 
@@ -47,15 +47,14 @@ module RegistrationsControllerTests
       assert_empty assigns(:user).errors.full_messages
     end
 
-    # Making sure new signup form is never rendered at this point, even if user is in new flow
-    test 'renders new signup form if partial registration is not in progress in new flow' do
-      SignUpTracking.expects(:new_sign_up_experience?).returns(true).times(3)
-      SignUpTracking.expects(:begin_sign_up_tracking)
+    test 'renders finish_sign_up partial registration is in progress in new flow' do
+      SignUpTracking.stubs(:new_sign_up_experience?).returns(true)
+      PartialRegistration.stubs(:in_progress?).returns(true)
+      User.expects(:new_with_session).returns(build(:user))
 
       get '/users/sign_up'
       assert_response :success
-      assert_template partial: '_sign_up'
-      assert_template partial: '_new_sign_up_form'
+      assert_template partial: '_finish_sign_up'
     end
   end
 end
