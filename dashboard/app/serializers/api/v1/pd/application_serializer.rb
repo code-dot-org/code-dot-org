5fd249b84128054243fdca38c2cf424cde2c52f6
Clare Constantine <clare@code.org>
2018-10-10 14:34:33 -0700

    fix test

--- dashboard/app/serializers/api/v1/pd/application_serializer.rb
+++ dashboard/app/serializers/api/v1/pd/application_serializer.rb
@@ -99,7 +99,7 @@ class Api::V1::Pd::ApplicationSerializer < ActiveModel::Serializer
   end
 
   def school_stats
-    if object&.school_id
+    if object.try(:school_id)
       stats = School.find_by_id(object.school_id).school_stats_by_year.order(school_year: :desc).first
       urm_total = stats.student_am_count + stats.student_hi_count + stats.student_bl_count + stats.student_hp_count
 
