293d97bec838bf80d7837efff32dc11d99f6e8ca
Brad Buchanan <brad@code.org>
2018-10-10 20:56:51 -0700

    Clean up initial test

--- pegasus/test/test_hoc_event_review.rb
+++ pegasus/test/test_hoc_event_review.rb
@@ -5,41 +5,60 @@ require_relative '../helpers/form_helpers'
 
 # Tests for the HocEventReview helpers module.
 class HocEventReviewTest < Minitest::Test
-  DEFAULT_DATA = {
-    email_s: 'fake@example.com',
-    name_s: 'fake_name',
-    organization_name_s: 'fake_org',
-    event_type_s: 'in_school',
-    hoc_country_s: 'us',
-    email_preference_opt_in_s: 'yes'
-  }.freeze
-
-  DEFAULT_KIND = 'HocSignup2018'
-
   describe 'HocEventReview' do
-    before do
-      DCDO.stubs(:get).with('hoc_year', 2017).returns(2018)
-      stubs(:dashboard_user).returns(nil)
-      Pegasus.stubs(:logger).returns(nil)
-      stubs(:request).returns(stub(ip: '1.2.3.4'))
-      row = insert_or_upsert_form(DEFAULT_KIND, DEFAULT_DATA.dup)
-      @form = Form.find(id: row[:id])
-    end
-
     it 'finds event counts by state' do
-      with_form DEFAULT_DATA do |form|
-        form.update(processed_data: {location_country_code_s: 'US', location_state_code_s: 'CA'}.to_json)
-        form.refresh
-        results = HocEventReview.event_counts_by_state
-        assert_equal [{state_code: 'CA', count: 1}], results
+      with_form location_country_code_s: 'US', location_state_code_s: 'CA' do
+        with_form location_country_code_s: 'US', location_state_code_s: 'CA' do
+          with_form location_country_code_s: 'US', location_state_code_s: 'OR' do
+            with_form location_country_code_s: 'MX' do
+              expected = [
+                {state_code: 'CA', count: 2},
+                {state_code: 'OR', count: 1},
+              ]
+              actual = HocEventReview.event_counts_by_state
+              assert_equal expected, actual
+            end
+          end
+        end
       end
     end
 
-    def with_form(form_data)
-      row = insert_or_upsert_form DEFAULT_KIND, form_data.dup
-      yield Form.find(id: row[:id])
+    # Helper to temporarily create a form row for testing retrieval methods.
+    DEFAULT_KIND = HocEventReview.send(:kind)
+    def with_form(processed_data)
+      # Necessary stubs for the insert_or_upsert_form helper
+      stubs(:dashboard_user).returns(nil)
+      Pegasus.stubs(:logger).returns(nil)
+      stubs(:request).returns(stub(ip: '1.2.3.4'))
+
+      # Add fake row
+      row = insert_or_upsert_form DEFAULT_KIND, HocEventReviewTest.fake_data
+
+      # Add fake processed data to fake row
+      form = Form.find(id: row[:id])
+      form.update(processed_data: processed_data.to_json)
+      form.refresh
+
+      # Run enclosed logic
+      yield form
     ensure
+      # Clean up fake row
       PEGASUS_DB[:forms].where(id: row[:id]).delete if row
     end
+  end
+
+  # Fake form data, with unique name/email because our form logic
+  # deduplicates submissions with the same name/email combo.
+  def self.fake_data
+    @@next_id ||= 0
+    @@next_id += 1
+    {
+      email_s: "fake#{@@next_id}@example.com",
+      name_s: "fake#{@@next_id}_name",
+      organization_name_s: 'fake_org',
+      event_type_s: 'in_school',
+      hoc_country_s: 'us',
+      email_preference_opt_in_s: 'yes'
+    }
   end
 end
