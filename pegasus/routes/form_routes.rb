b178f40716052fa086b564fc0ae834b0c96b6010
Brendan Reville <brendan@code.org>
2018-10-20 22:49:55 +1100

    volunteer search: restore functionality

--- pegasus/routes/form_routes.rb
+++ pegasus/routes/form_routes.rb
@@ -16,7 +16,11 @@ post '/forms/:kind' do |kind|
     content_type :json
     cache_control :private, :must_revalidate, max_age: 0
     form = insert_or_upsert_form(kind, params)
-    JSON.load(form[:data]).merge(secret: form[:secret]).to_json
+    data = JSON.load(form[:data]).merge(secret: form[:secret])
+    data.delete "volunteer_secret_s"
+    data.delete "volunteer_email_s"
+    data.delete "volunteer_id_i"
+    data.to_json
   rescue FormError => e
     halt 400, {'Content-Type' => 'text/json'}, e.errors.to_json
   rescue Sequel::UniqueConstraintViolation
