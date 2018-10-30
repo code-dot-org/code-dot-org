5e5380b6fb7b50c911372ca88ea670b2352f75da
Brad Buchanan <brad@code.org>
2018-10-30 15:02:44 -0700

    User.find_channel_owner returns nil for anonymous channel
    Fixes https://github.com/code-dot-org/dance-party/issues/76
    
    As a project validator I should be able to view an anonymous project.  Actual: 404
    Root cause was that _admin.html.haml was calling this helper which raised ActiveRecord::NotFound when the channel was anonymous.  Calling code is already safe for a nil return value

--- dashboard/test/models/user_test.rb
+++ dashboard/test/models/user_test.rb
@@ -4079,4 +4079,21 @@ class UserTest < ActiveSupport::TestCase
     section3.delete
     assert_equal section1.id, teacher.last_section_id
   end
+
+  test 'find_channel_owner finds channel owner' do
+    student = create :student
+    with_channel_for student do |channel_id, storage_id|
+      encrypted_channel_id = storage_encrypt_channel_id storage_id, channel_id
+      result = User.find_channel_owner encrypted_channel_id
+      assert_equal student, result
+    end
+  end
+
+  test 'find_channel_owner returns nil for channel with no owner' do
+    with_anonymous_channel do |channel_id, storage_id|
+      encrypted_channel_id = storage_encrypt_channel_id storage_id, channel_id
+      result = User.find_channel_owner encrypted_channel_id
+      assert_nil result
+    end
+  end
 end
