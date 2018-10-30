31b4b8918c4ee9053a60fd1125922bda20425fd4
Maddie Kasula <madelynkasula@gmail.com>
2018-10-30 15:35:21 -0700

    Check if message is undefined before attempting translation

--- apps/src/dance/Dance.js
+++ apps/src/dance/Dance.js
@@ -301,12 +301,14 @@ Dance.prototype.onPuzzleComplete = function (result, message) {
   // Stop everything on screen.
   this.reset();
 
+  const danceMessage = message ? danceMsg[message]() : '';
+
   if (result === true) {
     this.testResults = TestResults.ALL_PASS;
-    this.message = danceMsg[message]();
+    this.message = danceMessage;
   } else if (result === false) {
     this.testResults = TestResults.APP_SPECIFIC_FAIL;
-    this.message = danceMsg[message]();
+    this.message = danceMessage;
   } else {
     this.testResults = TestResults.FREE_PLAY;
   }
