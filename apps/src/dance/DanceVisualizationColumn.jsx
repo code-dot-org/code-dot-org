eef451c06d4b55f1afb675e309571e1be26b1feb
Erin Peach <erin@code.org>
2018-10-31 14:56:20 -0700

    age13Plus not required for signed out levels

--- apps/src/dance/DanceVisualizationColumn.jsx
+++ apps/src/dance/DanceVisualizationColumn.jsx
@@ -27,7 +27,7 @@ const SongSelector = Radium(class extends React.Component {
     selectedSong: PropTypes.string.isRequired,
     songManifest: PropTypes.arrayOf(PropTypes.object).isRequired,
     hasChannel: PropTypes.bool.isRequired,
-    is13Plus: PropTypes.bool.isRequired
+    is13Plus: PropTypes.bool
   };
 
   state = {
@@ -95,7 +95,7 @@ class DanceVisualizationColumn extends React.Component {
     isShareView: PropTypes.bool.isRequired,
     songManifest: PropTypes.arrayOf(PropTypes.object).isRequired,
     hasChannel: PropTypes.bool.isRequired,
-    is13Plus: PropTypes.bool.isRequired
+    is13Plus: PropTypes.bool
   };
 
   render() {
