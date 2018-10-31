de817607051d7e8af7820b20af75a515d19d00ac
Erin Bond <erin.bond@code.org>
2018-10-30 17:00:25 -0700

    remove DCDO control of Aquatic in the project widget

--- apps/src/sites/studio/pages/projects/public.js
+++ apps/src/sites/studio/pages/projects/public.js
@@ -35,7 +35,6 @@ $(document).ready(() => {
             canViewFullList
             canViewAdvancedTools
             includeDanceParty={projectsData.includeDanceParty}
-            includeMCAquatic={projectsData.includeMCAquatic}
           />
           <PublicGallery
             limitedGallery={projectsData.limitedGallery}
