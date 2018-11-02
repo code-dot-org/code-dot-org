a7e78f57e557176c66635e0f516751b43e29b0db
Larry Powelson <larrypo@gmail.com>
2018-11-02 14:58:53 -0700

    Fixed issues with locale and time zone by using Javascript Date.toLocaleString() method.

--- dashboard/test/controllers/script_levels_controller_test.rb
+++ dashboard/test/controllers/script_levels_controller_test.rb
@@ -882,7 +882,7 @@ class ScriptLevelsControllerTest < ActionController::TestCase
       section_id: @section.id
     }
 
-    assert_includes response.body, "<div>Submitted on:</div>\n<div class='timestamp'>12/15/2017, 6:05:08 PM</div>"
+    assert_includes response.body, "<div>Submitted on:</div>\n<div class='timestamp' data-timestamp='2017-12-15 18:05:08 UTC'></div>"
   end
 
   test 'shows last update time if you are a teacher viewing your student and they have a best result' do
@@ -915,7 +915,7 @@ class ScriptLevelsControllerTest < ActionController::TestCase
       section_id: @section.id
     }
 
-    assert_includes response.body, "<div>Last updated:</div>\n<div class='timestamp'>1/5/2000, 8:30:45 AM</div>"
+    assert_includes response.body, "<div>Last updated:</div>\n<div class='timestamp' data-timestamp='2000-01-05 08:30:45 UTC'></div>"
   end
 
   test 'loads applab if you are a project validator viewing a student and they have a channel id' do
