5b7af46add9a38822f425f56cec636b319b3103a
Brendan Reville <brendan@code.org>
2018-11-03 11:43:32 +1100

    hourofcode.com: Change shared video to be worldwide one, for all modes

--- pegasus/src/social_metadata.rb
+++ pegasus/src/social_metadata.rb
@@ -21,37 +21,37 @@ def get_social_metadata_for_page(request)
         title: hoc_s(:social_hoc2018_what_create),
         description: hoc_s(:social_hoc2018_every_student),
         image: {path: "/images/social-media/codeorg-2018-creativity.jpg", width: 846, height: 529},
-        video: videos[:creativity_is]
+        video: videos[:hour_of_code_worldwide]
       },
       "soon-hoc-mc" => {
         title: hoc_s(:social_hoc2018_what_create),
         description: hoc_s(:social_hoc2018_every_student),
         image: {path: "/images/social-media/codeorg-2018-creativity.jpg", width: 846, height: 529},
-        video: videos[:creativity_is]
+        video: videos[:hour_of_code_worldwide]
       },
       "soon-hoc-dance" => {
         title: hoc_s(:social_hoc2018_what_create),
         description: hoc_s(:social_hoc2018_every_student),
         image: {path: "/images/social-media/codeorg-2018-creativity.jpg", width: 846, height: 529},
-        video: videos[:creativity_is]
+        video: videos[:hour_of_code_worldwide]
       },
       "actual-hoc" => {
         title: I18n.t(:og_title_here),
         description: hoc_s(:social_hoc2018_every_student_try_what_create),
         image: {path: "/images/social-media/codeorg-2018-creativity.jpg", width: 846, height: 529},
-        video: videos[:creativity_is]
+        video: videos[:hour_of_code_worldwide]
       },
       "actual-hoc-dance" => {
         title: I18n.t(:og_title_here),
         description: hoc_s(:social_hoc2018_every_student_try_what_create),
         image: {path: "/images/social-media/codeorg-2018-creativity.jpg", width: 846, height: 529},
-        video: videos[:creativity_is]
+        video: videos[:hour_of_code_worldwide]
       },
       "default" => {
         title: hoc_s(:social_hoc2018_code_org_what_create),
         description: hoc_s(:social_hoc2018_every_student),
         image: {path: "/images/social-media/codeorg-2018-creativity.jpg", width: 846, height: 529},
-        video: videos[:creativity_is]
+        video: videos[:hour_of_code_worldwide]
       }
     },
     "csedweek.org" => {
