60dafce109103cd68992953f3b7b6129c32c38be
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-11-06 15:44:02 -0800

    add DCDO flags for disabling social share videos

--- dashboard/app/controllers/projects_controller.rb
+++ dashboard/app/controllers/projects_controller.rb
@@ -325,7 +325,7 @@ class ProjectsController < ApplicationController
 
     if params[:key] == 'artist'
       @project_image = CDO.studio_url "/v3/files/#{@view_options['channel']}/_share_image.png", 'https:'
-    elsif params[:key] == 'dance'
+    elsif params[:key] == 'dance' && DCDO.get('share_video_sharing_enabled', true)
       # TODO: elijah set up test subdomains for dance-api, and situationally
       # point to those here
       @project_video = "https://dance-api.code.org/videos/video-#{@view_options['channel']}.mp4"
