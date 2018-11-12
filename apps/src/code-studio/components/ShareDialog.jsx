0df8897892cf3335631b7fbcf6ba0b9e3ffe2ec1
Erin Peach <erin@code.org>
2018-11-12 09:56:33 -0800

    Remove dashboard dependency from ShareDialog

--- apps/src/code-studio/components/ShareDialog.jsx
+++ apps/src/code-studio/components/ShareDialog.jsx
@@ -1,4 +1,3 @@
-/* global dashboard*/
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { SignInState } from '@cdo/apps/code-studio/progressRedux';
@@ -9,16 +8,15 @@ class ShareDialog extends Component {
   static propTypes = {
     signInState: PropTypes.oneOf(Object.values(SignInState)),
     isProjectLevel: PropTypes.bool.isRequired,
+    isDance: PropTypes.bool,
     // Only applicable to Dance Party projects, used to Tweet at song artist.
     selectedSong: PropTypes.string,
   };
 
   render() {
-    const { signInState, isProjectLevel, ...otherProps } = this.props;
+    const { signInState, isProjectLevel, isDance, ...otherProps } = this.props;
     // If we're on a project level (i.e. /projects/appname), always show signed
     // in version of the dialog
-
-    const isDance = dashboard.project && dashboard.project.getStandaloneApp() === 'dance';
 
     if (signInState === SignInState.SignedIn || isProjectLevel || isDance) {
       return (
