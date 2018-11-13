9354fc86dd38cc4d54ecbad2effc2b637fd5e569
Clare Constantine <clare@code.org>
2018-11-13 13:17:18 -0800

    tag names must be unique

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -583,8 +583,10 @@ ActiveRecord::Schema.define(version: 20181113190811) do
   end
 
   create_table "pd_application_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.string "name", null: false
-    t.index ["name"], name: "index_pd_application_tags_on_name", using: :btree
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_pd_application_tags_on_name", unique: true, using: :btree
   end
 
   create_table "pd_applications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
