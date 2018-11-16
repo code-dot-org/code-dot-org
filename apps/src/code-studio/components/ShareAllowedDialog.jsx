2cd780c55700e3197f98cf6a619b166a68b41221
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-11-15 19:21:57 -0800

    check for video existence before downloading

--- apps/src/code-studio/components/ShareAllowedDialog.jsx
+++ apps/src/code-studio/components/ShareAllowedDialog.jsx
@@ -47,6 +47,25 @@ function select(event) {
   event.target.select();
 }
 
+function downloadRemoteUrl(url, downloadName) {
+  fetch(url, {
+    method: 'GET',
+  }).then(response => response.blob())
+    .then(blob => {
+      const element = document.createElement('a');
+      const url = URL.createObjectURL(blob);
+      element.setAttribute('href', url);
+      element.setAttribute('download', downloadName);
+      element.style.display = 'none';
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
 const styles = {
   modal: {
     width: 720,
@@ -173,6 +192,7 @@ class ShareAllowedDialog extends React.Component {
     exportError: null,
     isTwitterAvailable: false,
     isFacebookAvailable: false,
+    replayVideoExists: false,
   };
 
   componentDidMount() {
@@ -219,24 +239,30 @@ class ShareAllowedDialog extends React.Component {
     `https://dance-api.code.org/videos/video-${this.props.channelId}.mp4`;
 
   downloadReplayVideo = (event) => {
-    fetch(this.getReplayVideoUrl(), {
-      method: 'GET'
-    }).then((response) => response.blob())
-    .then((blob) => {
-      const element = document.createElement('a');
-      const url = URL.createObjectURL(blob);
-      element.setAttribute('href', url);
-      element.setAttribute('download', 'myvideo.mp4');
-      element.style.display = 'none';
-      document.body.appendChild(element);
-      element.click();
-      document.body.removeChild(element);
-    }).catch((error) => {
-      console.log(error);
-    });
+    if (this.state.replayVideoExists) {
+      downloadRemoteUrl(this.getReplayVideoUrl(), "myvideo.mp4");
+    } else {
+      this.checkReplayVideo();
+    }
 
     event.preventDefault();
     return false;
+  };
+
+  checkReplayVideo = (onSuccess = () => {}, onError = () => {}) => {
+    fetch(this.getReplayVideoUrl(), {
+      method: 'HEAD'
+    }).then((response) => {
+      this.setState({
+        replayVideoExists: true
+      });
+      onSuccess(response);
+    }).catch((error) => {
+      this.setState({
+        replayVideoExists: false
+      });
+      onError(error);
+    });
   };
 
   sharingDisabled = () =>
@@ -430,6 +456,7 @@ class ShareAllowedDialog extends React.Component {
                   <a
                     id="sharing-video"
                     href=""
+                    disabled={!this.state.replayVideoExists}
                     onClick={this.downloadReplayVideo}
                   >
                     <i className="fa fa-film" style={{fontSize: 28}}></i>
