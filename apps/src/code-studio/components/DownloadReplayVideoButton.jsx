65e90fb0f44fc6c20e3b8c132c12c9b79b270450
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-11-16 16:47:42 -0800

    improve download button UX

--- apps/src/code-studio/components/DownloadReplayVideoButton.jsx
+++ apps/src/code-studio/components/DownloadReplayVideoButton.jsx
@@ -1,4 +1,7 @@
 import React, {PropTypes} from 'react';
+import i18n from '@cdo/locale';
+
+import color from "../../util/color";
 
 function downloadRemoteUrl(url, downloadName) {
   fetch(url, {
@@ -20,7 +23,17 @@ function downloadRemoteUrl(url, downloadName) {
     });
 }
 
-const DOWNLOAD_NAME = 'myvideo.mp4';
+const DOWNLOAD_NAME = 'dance_party.mp4';
+
+const styles = {
+  disabledLink: {
+    backgroundColor: color.lighter_gray,
+    boxShadow: 'none',
+  },
+  icon: {
+    fontSize: 28
+  }
+};
 
 export default class DownloadReplayVideoButton extends React.Component {
   static propTypes = {
@@ -29,14 +42,45 @@ export default class DownloadReplayVideoButton extends React.Component {
 
   state = {
     videoExists: false,
+    downloadInitiated: false,
+    checkVideoUntilSuccessTimeout: null
   };
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // if the download has been initiated and we are updating from the video
+    // not existing to the video existing, trigger the actual download
+    if (
+      this.state.downloadInitiated &&
+      this.state.videoExists &&
+      !prevState.videoExists
+    ) {
+      this.tryDownloadVideo();
+    }
+  }
+
+  componentDidMount() {
+    this.checkVideoUntilSuccess();
+  }
 
   getVideoUrl = () =>
     `https://dance-api.code.org/videos/video-${this.props.channelId}.mp4`;
 
-  downloadVideo = (event) => {
+  // Show the button as enabled if we know the video exists; also show it as
+  // enabled on initial load, until the first time the user clicks it
+  buttonEnabled = () => this.state.videoExists || !this.state.downloadInitiated;
+
+  tryDownloadVideo = (event = {}) => {
+    if (!this.state.downloadInitiated) {
+      this.setState({
+        downloadInitiated: true
+      });
+    }
+
     if (this.state.videoExists) {
       downloadRemoteUrl(this.getVideoUrl(), DOWNLOAD_NAME);
+      this.setState({
+        downloadInitiated: false
+      });
     } else {
       this.checkVideo();
     }
@@ -45,34 +89,58 @@ export default class DownloadReplayVideoButton extends React.Component {
     return false;
   };
 
-  checkVideo = (onSuccess = () => {}, onError = () => {}) => {
-    fetch(this.getVideoUrl(), {
+  checkVideo = () => {
+    return fetch(this.getVideoUrl(), {
       method: 'HEAD',
-    })
-      .then(response => {
-        this.setState({
-          videoExists: true,
-        });
-        onSuccess(response);
-      })
-      .catch(error => {
-        this.setState({
-          videoExists: false,
-        });
-        onError(error);
+    }).then((response) => {
+      this.setState({
+        videoExists: response.ok,
       });
+
+      return response;
+    });
+  };
+
+  checkVideoUntilSuccess = (delay = 1000) => {
+    if (this.state.videoExists) {
+      return;
+    }
+
+    if (this.state.checkVideoUntilSuccessTimeout) {
+      clearTimeout(this.state.checkVideoUntilSuccessTimeout);
+    }
+
+    this.checkVideo().then((response) => {
+      let timeout = null;
+      if (!response.ok) {
+        timeout = setTimeout(this.checkVideoUntilSuccess, delay);
+      }
+
+      this.setState({
+        checkVideoUntilSuccessTimeout: timeout
+      });
+    });
   };
 
   render() {
+    let icon = "fa-download";
+    if (this.state.downloadInitiated) {
+      icon = "fa-spinner fa-pulse";
+    }
+
     return (
       <a
         className="download-replay-video-button"
-        href=""
-        disabled={!this.state.videoExists}
-        onClick={this.downloadVideo}
+        href="#"
+        style={this.buttonEnabled() ? {} : styles.disabledLink}
+        disabled={!this.buttonEnabled()}
+        onClick={this.tryDownloadVideo}
       >
-        <i className="fa fa-film" style={{fontSize: 28}} />
-        <span>Get Ma Video</span>
+        <i
+          className={`fa ${icon}`}
+          style={styles.icon}
+        />
+        <span>{i18n.downloadReplayVideoButtonDownload()}</span>
       </a>
     );
   }
