d104ac502cc4b091f3ef03cac7d0b6afac1779ab
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-11-19 14:37:21 -0800

    add a more thorough suite of unit tests

--- apps/test/unit/code-studio/components/DownloadReplayVideoButton.js
+++ apps/test/unit/code-studio/components/DownloadReplayVideoButton.js
@@ -14,7 +14,30 @@ describe('DownloadReplayVideoButton', () => {
   });
 
   it('initially renders as enabled', () => {
-    expect(wrapper.find('a').is('disabled')).to.equal(false);
+    expect(wrapper.instance().buttonEnabled()).to.equal(true);
+    expect(wrapper.find('a').props()).to.have.property('disabled', false);
+    expect(wrapper.find('i').hasClass('fa-download')).to.equal(true);
+  });
+
+  it('renders as disabled if the download has been initiated before the video has been found', () => {
+    wrapper.setState({
+      videoExists: false,
+      downloadInitiated: true
+    });
+
+    expect(wrapper.instance().buttonEnabled()).to.equal(false);
+    expect(wrapper.find('a').props()).to.have.property('disabled', true);
+    expect(wrapper.find('i').hasClass('fa-spinner')).to.equal(true);
+  });
+
+  it('renders as enabled if the download has been initiated after the video has been found', () => {
+    wrapper.setState({
+      videoExists: true,
+      downloadInitiated: true
+    });
+
+    expect(wrapper.instance().buttonEnabled()).to.equal(true);
+    expect(wrapper.find('a').props()).to.have.property('disabled', false);
     expect(wrapper.find('i').hasClass('fa-download')).to.equal(true);
   });
 
@@ -36,5 +59,45 @@ describe('DownloadReplayVideoButton', () => {
     fetchStub.restore();
     checkVideoSpy.restore();
     checkVideoUntilSuccessSpy.restore();
+  });
+
+  it('downloads video directly if it exists', () => {
+    const tryDownloadVideoSpy = sinon.spy(wrapper.instance(), 'tryDownloadVideo');
+    const fetchSpy = sinon.spy(window, 'fetch');
+
+    expect(tryDownloadVideoSpy.callCount).to.equal(0);
+    expect(fetchSpy.callCount).to.equal(0);
+
+    wrapper.setState({ videoExists: true });
+    wrapper.find('a').simulate('click');
+
+    expect(tryDownloadVideoSpy.callCount).to.equal(1);
+    expect(fetchSpy.callCount).to.equal(1);
+    expect(fetchSpy.calledWith(wrapper.instance().getVideoUrl(), {
+      method: 'GET'
+    })).to.equal(true);
+
+    tryDownloadVideoSpy.restore();
+    fetchSpy.restore();
+  });
+
+  it('disables button until video exists if it does not', () => {
+    const tryDownloadVideoSpy = sinon.spy(wrapper.instance(), 'tryDownloadVideo');
+    const fetchSpy = sinon.spy(window, 'fetch');
+
+    expect(tryDownloadVideoSpy.callCount).to.equal(0);
+    expect(fetchSpy.callCount).to.equal(0);
+
+    wrapper.setState({ videoExists: false });
+    wrapper.find('a').simulate('click');
+
+    expect(tryDownloadVideoSpy.callCount).to.equal(1);
+    expect(fetchSpy.callCount).to.equal(1);
+    expect(fetchSpy.calledWith(wrapper.instance().getVideoUrl(), {
+      method: 'HEAD'
+    })).to.equal(true);
+
+    tryDownloadVideoSpy.restore();
+    fetchSpy.restore();
   });
 });
