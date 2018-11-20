b346fa88663a9c48aab8ba3bf2e992d8b7603933
Maddie Kasula <madelynkasula@gmail.com>
2018-11-19 16:17:53 -0800

    Add unit test for resetting storage_id cookie

--- dashboard/test/controllers/script_levels_controller_test.rb
+++ dashboard/test/controllers/script_levels_controller_test.rb
@@ -600,6 +600,34 @@ class ScriptLevelsControllerTest < ActionController::TestCase
     refute session['warden.user.user.key']
   end
 
+  def get_storage_id_cookie(response)
+    storage_id_regex = /\A#{storage_id_cookie_name}=(\S+)/
+    match_data = response.header['Set-Cookie']&.match(storage_id_regex)
+    match_data.present? ? match_data[0] : nil
+  end
+
+  test "show with the reset param should create a new storage_id cookie when not logged in" do
+    get :reset, params: {script_id: Script::HOC_NAME}
+    assert_response 200
+    first_storage_id_cookie = get_storage_id_cookie(response)
+    refute_nil first_storage_id_cookie
+
+    get :reset, params: {script_id: Script::HOC_NAME}
+    assert_response 200
+    second_storage_id_cookie = get_storage_id_cookie(response)
+    refute_nil second_storage_id_cookie
+
+    refute_equal first_storage_id_cookie, second_storage_id_cookie
+  end
+
+  test "show with the reset param should not create a new storage_id cookie when logged in" do
+    sign_in(create(:user))
+
+    get :reset, params: {script_id: Script::HOC_NAME}
+    assert_response 302
+    assert_nil get_storage_id_cookie(response)
+  end
+
   test "show with the reset param should not reset session when logged in" do
     sign_in(create(:user))
     get :reset, params: {script_id: Script::HOC_NAME}
