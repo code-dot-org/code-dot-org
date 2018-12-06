1e1b7425f736090fee4bbecea6df5277244bf99b
Erin Bond <erin.bond@code.org>
2018-12-06 12:01:21 -0800

    initial work on teacher dashboard navigation

---
+++ apps/src/templates/NavigationBar.jsx
@@ -0,0 +1,53 @@
+import React, { PropTypes } from 'react';
+import color from "@cdo/apps/util/color";
+import NavigationBarLink from './NavigationBarLink';
+
+const styles = {
+  navBar: {
+    height: 50,
+    width: '100%',
+    backgroundColor: color.purple,
+  },
+  linkBox: {
+    display: 'inline',
+    paddingRight: 20,
+    paddingLeft: 20,
+  },
+};
+
+export default class NavigationBar extends React.Component {
+  static propTypes = {
+    links: PropTypes.array,
+    activeLink: PropTypes.string,
+  };
+
+  state = {
+    activeLink: this.props.activeLink,
+  };
+
+  changeActiveLink = (linkId) => {
+    event.preventDefault();
+    this.setState({activeLink: linkId});
+  };
+
+  render() {
+    const {links} = this.props;
+
+    return (
+      <div style={styles.navBar}>
+        {links.map(link => (
+          <div
+            key={link.id}
+            style={styles.linkBox}
+            onClick={() => this.changeActiveLink(link.id)}
+          >
+            <NavigationBarLink
+              label={link.label}
+              active={link.id === this.state.activeLink}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
