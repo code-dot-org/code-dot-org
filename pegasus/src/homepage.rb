84bd7d4ac4474bd247a5734eb45c905bba4528c3
Brendan Reville <brendan@code.org>
2018-12-12 07:36:57 +1100

    hoc2018: code review feedback

--- pegasus/src/homepage.rb
+++ pegasus/src/homepage.rb
@@ -416,4 +416,16 @@ class Homepage
       link_text: "homepage_below_hero_announcement_link_text"
     }
   end
+
+  def self.get_dance_stars
+    stars = [
+      "Ace of Base", "A-ha", "Ariana Grande", "Avicii and Aloe Blacc", "Bruce Springsteen", "Calvin Harris",
+      "Carly Rae Jepsen", "Ciara", "Coldplay", "Ed Sheeran", "Imagine Dragons",
+      "J Balvin and Willy William", "Justin Bieber", "Katy Perry", "Keith Urban", "Lady Antebellum", "Lady Gaga",
+      "Los del Río", "Luke Bryan", "Macklemore and Ryan Lewis", "Madonna", "Mark Ronson (ft. Bruno Mars)",
+      "MC Hammer", "Miley Cyrus", "OutKast", "Selena Gomez", "Sia", "Village People", "The Weeknd", "will.i.am",
+      "Yolanda Be Cool"
+    ]
+    DCDO.get("hoc2018_dance_stars", stars)
+  end
 end
