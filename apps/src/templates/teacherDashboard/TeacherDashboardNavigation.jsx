c0ec6b4c2ce3ace1f39668a913bb5d281375e2d6
Erin Bond <erin.bond@code.org>
2018-12-11 17:26:20 -0800

    dedupe teacher nav links array

--- apps/src/templates/teacherDashboard/TeacherDashboardNavigation.jsx
+++ apps/src/templates/teacherDashboard/TeacherDashboardNavigation.jsx
@@ -1,50 +1,43 @@
 import React, {Component, PropTypes} from 'react';
 import NavigationBar from '../NavigationBar';
 
+export const teacherDashboardLinks = [
+  {
+    label: "Manage Students",
+    url: "manage_students"
+  },
+  {
+    label: "Projects",
+    url: "projects"
+  },
+  {
+    label: "Stats",
+    url: "stats"
+  },
+  {
+    label: "Text Responses",
+    url: "text_responses"
+  },
+  {
+    label: "Progress",
+    url: "progress"
+  },
+  {
+    label: "Assessments/Surveys",
+    url: "assessments"
+  },
+];
+
 export default class TeacherDashboardNavigation extends Component {
   static propTypes = {
     defaultActiveLink: PropTypes.string,
   };
 
   render() {
-
-    const links = [
-      {
-        id: "manageStudents",
-        label: "Manage Students",
-        url: "manage_students"
-      },
-      {
-        id: "projects",
-        label: "Projects",
-        url: "projects"
-      },
-      {
-        id: "stats",
-        label: "Stats",
-        url: "stats"
-      },
-      {
-        id: "textResponses",
-        label: "Text Responses",
-        url: "text_responses"
-      },
-      {
-        id: "progress",
-        label: "Progress",
-        url: "progress"
-      },
-      {
-        id: "assessments",
-        label: "Assessments/Surveys",
-        url: "assessments"
-      },
-    ];
-
     return (
       <NavigationBar
         defaultActiveLink = {this.props.defaultActiveLink}
-        links = {links}
+        links = {teacherDashboardLinks}
       />
     );
   }
