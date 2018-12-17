da8483462343c3bbf58721201838ad97839870b0
Erin Bond <erin.bond@code.org>
2018-12-17 13:50:36 -0800

    content is based on the tab selected - wahoo

--- apps/src/templates/teacherDashboard/TeacherDashboard.jsx
+++ apps/src/templates/teacherDashboard/TeacherDashboard.jsx
@@ -1,71 +1,43 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch, withRouter} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import TeacherDashboardNavigation from './TeacherDashboardNavigation';
-import StatsTable from './StatsTable';
-
-class DebugRouter extends Router {
-  constructor(props) {
-    super(props);
-    console.log('initial history is: ', JSON.stringify(this.history, null,2));
-    this.history.listen((location, action)=>{
-      console.log(
-        `The current URL is ${location.pathname}${location.search}${location.hash}`
-      );
-      console.log(`The last navigation action was ${action}`, JSON.stringify(this.history, null,2));
-    });
-  }
-}
-
 
 export default class TeacherDashboard extends Component {
   render() {
-    const section = {
-      id: 1,
-      students: [
-        {
-          id: 1,
-          name: 'Student B',
-          total_lines: 1
-        },
-        {
-          id: 2,
-          name: 'Student C',
-          total_lines: 2
-        },
-        {
-          id: 3,
-          name: 'Student A',
-          total_lines: 3
-        },
-      ]
-    };
-
-    const studentsCompletedLevelCount = {
-      1: 2,
-      2: 3,
-      3: 1
-    };
-
     return (
       <div>
         <TeacherDashboardNavigation/>
-        <DebugRouter>
-          <Switch>
-            <Route
-              exact path="/"
-              component={props => <div>Index at /</div>}
-            />
-            <Route
-              path="/teacher_dashboard/sections/:section_id/stats"
-              component={withRouter(props => <StatsTable {...props} studentsCompletedLevelCount={studentsCompletedLevelCount}section={section}/>)}
-            />
-            <Route
-              path="/hello"
-              component={props => <div>Hello!</div>}
-            />
-            <Route component={props => <div>no match :( </div>} />
-          </Switch>
-        </DebugRouter>
+        <Switch>
+          <Route
+            exact path="/"
+            component={props => <div>Progress content goes here - default for now.</div>}
+          />
+          <Route
+            path="/stats"
+            component={props => <div>Stats content goes here!</div>}
+          />
+          <Route
+            path="/progress"
+            component={props => <div>Progress content goes here!</div>}
+          />
+          <Route
+            path="/manage_students"
+            component={props => <div>Manage Students content goes here!</div>}
+          />
+          <Route
+            path="/projects"
+            component={props => <div>Projects content goes here!</div>}
+          />
+          <Route
+            path="/text_responses"
+            component={props => <div>Text responses content goes here!</div>}
+          />
+          <Route
+            path="/assessments"
+            component={props => <div>Assessments/Surveys content goes here!</div>}
+          />
+          <Route component={props => <div>Progress content goes here - default for now.</div>} />
+        </Switch>
       </div>
     );
   }
