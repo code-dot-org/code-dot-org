987c6f4f1768d63666765b4c2bc370dd7a6c043b
David Bailey <davidsbailey@users.noreply.github.com>
2018-12-19 18:39:53 -0800

    add tests for dance cache headers and session cookies

--- dashboard/test/integration/routes_test.rb
+++ dashboard/test/integration/routes_test.rb
@@ -9,4 +9,28 @@ class RoutesTest < ActionDispatch::IntegrationTest
   def test_section_student_script_ids_routes
     assert_recognizes({controller: 'api/v1/sections', action: 'student_script_ids', id: '3'}, '/dashboardapi/sections/3/student_script_ids')
   end
+
+  def test_dance_session_cookie_and_cache_headers
+    script = create :script, name: 'dance'
+    stage = create :stage, script: script
+    create :script_level, script: script, stage: stage
+    create :script_level, script: script, stage: stage, position: 12
+    create :script_level, script: script, stage: stage, position: 13
+
+    get '/s/dance/stage/1/puzzle/1'
+    assert_caching_enabled response.headers['Cache-Control'],
+      ScriptLevelsController::DEFAULT_PUBLIC_CLIENT_MAX_AGE,
+      ScriptLevelsController::DEFAULT_PUBLIC_PROXY_MAX_AGE
+    assert_nil cookies['_learn_session_test']
+
+    get '/s/dance/stage/1/puzzle/12'
+    assert_caching_enabled response.headers['Cache-Control'],
+      ScriptLevelsController::DEFAULT_PUBLIC_CLIENT_MAX_AGE,
+      ScriptLevelsController::DEFAULT_PUBLIC_PROXY_MAX_AGE
+    assert_nil cookies['_learn_session_test']
+
+    get '/s/dance/stage/1/puzzle/13'
+    assert_caching_disabled response.headers['Cache-Control']
+    assert_not_nil cookies['_learn_session_test']
+  end
 end
