c83061bfe57d40307403dff9a2df28b20871dbfb
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-01-07 13:29:26 -0800

    apply Prettier formatting

--- apps/src/templates/studioHomepages/ResourceCard.jsx
+++ apps/src/templates/studioHomepages/ResourceCard.jsx
@@ -1,8 +1,8 @@
 import React, {Component, PropTypes} from 'react';
 import Radium from 'radium';
 import Button from '../Button';
-import color from "../../util/color";
-import { connect } from 'react-redux';
+import color from '../../util/color';
+import {connect} from 'react-redux';
 
 // If you want to include an image, you're probably looking for a ImageResourceCard.
 
@@ -10,10 +10,10 @@ const styles = {
   card: {
     height: 250,
     width: 310,
-    background: color.teal
+    background: color.teal,
   },
   cardAllowWrap: {
-    position: 'relative'
+    position: 'relative',
   },
   text: {
     paddingLeft: 20,
@@ -31,18 +31,18 @@ const styles = {
   titleNoWrap: {
     whiteSpace: 'nowrap',
     textOverflow: 'ellipsis',
-    overflow: 'hidden'
+    overflow: 'hidden',
   },
   titleAllowWrap: {
-    lineHeight: '1.1'
+    lineHeight: '1.1',
   },
   description: {
     fontFamily: '"Gotham 4r", sans-serif',
     fontSize: 14,
-    lineHeight: "21px",
+    lineHeight: '21px',
     height: 140,
     marginBottom: 5,
-    overflowY: 'auto'
+    overflowY: 'auto',
   },
   button: {
     marginLeft: 20,
@@ -70,12 +70,20 @@ class ResourceCard extends Component {
     isRtl: PropTypes.bool.isRequired,
     allowWrap: PropTypes.bool,
     linkId: PropTypes.string,
-    linkClass: PropTypes.string
+    linkClass: PropTypes.string,
   };
 
   render() {
-
-    const { title, description, buttonText, link, isRtl, allowWrap, linkId, linkClass} = this.props;
+    const {
+      title,
+      description,
+      buttonText,
+      link,
+      isRtl,
+      allowWrap,
+      linkId,
+      linkClass,
+    } = this.props;
     const localeStyle = isRtl ? styles.rtl : styles.ltr;
     let buttonStyles = [styles.button];
     let cardStyles = [styles.card, localeStyle];
@@ -92,13 +100,9 @@ class ResourceCard extends Component {
 
     return (
       <div style={cardStyles}>
-        <div style={titleStyles}>
-          {title}
-        </div>
-        <div style={descriptionStyles}>
-          {description}
-        </div>
-        <br/>
+        <div style={titleStyles}>{title}</div>
+        <div style={descriptionStyles}>{description}</div>
+        <br />
         <Button
           id={linkId}
           className={linkClass}
