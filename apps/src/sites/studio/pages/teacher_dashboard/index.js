249424a694361b9e1b347be32ec0514767af159c
Maddie Kasula <madelynkasula@gmail.com>
2019-01-10 11:14:28 -0800

    Register sectionData reducer and load section data in TeacherDashboard

--- apps/src/sites/studio/pages/teacher_dashboard/index.js
+++ apps/src/sites/studio/pages/teacher_dashboard/index.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {getStore, registerReducers} from '@cdo/apps/redux';
+import sectionData, {asyncSetSection} from '@cdo/apps/redux/sectionDataRedux';
 import TeacherDashboard from '@cdo/apps/templates/teacherDashboard/TeacherDashboard';
 
 const script = document.querySelector('script[data-dashboard]');
@@ -10,6 +12,10 @@ const sectionId = scriptData.section_id;
 const baseUrl = `/teacher_dashboard/sections/${sectionId}`;
 
 $(document).ready(function () {
+  registerReducers({sectionData});
+  const store = getStore();
+  store.dispatch(asyncSetSection(sectionId));
+
   ReactDOM.render(
     <div>
       <Router basename={baseUrl}>
