d6a82f1c760f2e5975d367a54dd959eec09cbef5
Maddie Kasula <madelynkasula@gmail.com>
2019-01-09 19:54:13 -0800

    Add async load begin/end and clear redux functions

--- apps/src/redux/sectionDataRedux.js
+++ apps/src/redux/sectionDataRedux.js
@@ -1,13 +1,23 @@
 import { PropTypes } from 'react';
+import $ from 'jquery';
 
-// Reducer for section data in teacher dashboard.
-// Tab specific reducers can import actions from this file
-// if they need to respond to a section changing.
+/**
+ * Reducer for section data in teacher dashboard.
+ * Tab specific reducers can import actions from this file
+ * if they need to respond to a section changing.
+ */
 
-// Action type constants
+/**
+ * Action type constants
+ */
 export const SET_SECTION = 'sectionData/SET_SECTION';
+const CLEAR_SECTION_DATA = 'sectionData/CLEAR_SECTION_DATA';
+const ASYNC_LOAD_BEGIN = 'sectionData/ASYNC_LOAD_BEGIN';
+const ASYNC_LOAD_END = 'sectionData/ASYNC_LOAD_END';
 
-// Action creators
+/**
+ * Action creators
+ */
 export const setSection = (section) => {
   // Sort section.students by name.
   const sortedStudents = section.students.sort((a, b) => a.name.localeCompare(b.name));
@@ -36,9 +46,12 @@ export const sectionDataPropType = PropTypes.shape({
   })).isRequired
 });
 
-// Initial state of sectionDataRedux
+/**
+ * Initial state of sectionDataRedux
+ */
 const initialState = {
   section: {},
+  isLoading: false,
 };
 
 export default function sectionData(state=initialState, action) {
@@ -52,14 +65,53 @@ export default function sectionData(state=initialState, action) {
     };
   }
 
+  if (action.type === CLEAR_SECTION_DATA) {
+    return initialState;
+  }
+
+  if (action.type === ASYNC_LOAD_BEGIN) {
+    return {
+      isLoading: true,
+    };
+  }
+
+  if (action.type === ASYNC_LOAD_END) {
+    return {
+      isLoading: false,
+    };
+  }
+
   return state;
 }
 
-// Selector functions
+/**
+ * Selector functions
+ */
 export const getTotalStudentCount = (state) => {
   return state.sectionData.section.students.length;
 };
 
 export const getStudentList = (state) => {
   return state.sectionData.section.students;
+};
+
+/**
+ * Helper functions
+ */
+export const asyncSetSection = (sectionId) => (dispatch) => {
+  // Clear any existing section data and being loading.
+  dispatch({ type: CLEAR_SECTION_DATA });
+  dispatch({ type: ASYNC_LOAD_BEGIN });
+
+  $.ajax({
+    url: `/api/v1/sections/${sectionId}`,
+    method: 'GET',
+    dataType: 'json'
+  }).done(section => {
+    dispatch(setSection(section));
+    dispatch({ type: ASYNC_LOAD_END });
+  }).fail((jqXhr, status) => {
+    // handle errors
+    dispatch({ type: ASYNC_LOAD_END });
+  });
 };
