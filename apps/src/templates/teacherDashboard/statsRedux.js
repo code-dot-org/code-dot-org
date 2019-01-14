9406029d40ffde62c171f4b8dcc2d0306a72f40f
Maddie Kasula <madelynkasula@gmail.com>
2019-01-14 09:47:20 -0800

    Reset statsRedux if SET_SECTION is triggered

--- apps/src/templates/teacherDashboard/statsRedux.js
+++ apps/src/templates/teacherDashboard/statsRedux.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import { SET_SECTION } from '@cdo/apps/redux/sectionDataRedux';
 
 /**
  * Reducer for stats tab in teacher dashboard.
@@ -25,6 +26,11 @@ const initialState = {
  * Reducer
  */
  export default function stats(state = initialState, action) {
+   // Reset state if section is set.
+   if (action.type === SET_SECTION) {
+     return initialState;
+   }
+
    if (action.type === SET_COMPLETED_LEVEL_COUNT) {
      return {
        ...state,
