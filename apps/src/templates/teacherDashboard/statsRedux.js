9644e0ccc1a954d927750efe82a88a03956c1ef4
Maddie Kasula <madelynkasula@gmail.com>
2019-01-14 11:58:07 -0800

    Don't reset state if section is set

--- apps/src/templates/teacherDashboard/statsRedux.js
+++ apps/src/templates/teacherDashboard/statsRedux.js
@@ -1,5 +1,4 @@
 import $ from 'jquery';
-import { SET_SECTION } from '@cdo/apps/redux/sectionDataRedux';
 
 /**
  * Reducer for stats tab in teacher dashboard.
@@ -26,11 +25,6 @@ const initialState = {
  * Reducer
  */
  export default function stats(state = initialState, action) {
-   // Reset state if section is set.
-   if (action.type === SET_SECTION) {
-     return initialState;
-   }
-
    if (action.type === SET_COMPLETED_LEVEL_COUNT) {
      return {
        ...state,
