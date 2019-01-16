3c1f7e6294e73b693240358727b8635e15a2bfc9
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-01-16 13:31:05 -0800

    replace student_dance_projects haml component with an equivalent React component

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -554,6 +554,7 @@ describe('entry tests', () => {
     'census_reviewers/review_reported_inaccuracies': './src/sites/studio/pages/census_reviewers/review_reported_inaccuracies.js',
 
     'code.org/public/yourschool': './src/sites/code.org/pages/public/yourschool.js',
+    'code.org/public/dance': './src/sites/code.org/pages/public/dance.js',
     'hourofcode.com/public/index': './src/sites/hourofcode.com/pages/public/index.js',
     'regionalPartnerSearch': './src/regionalPartnerSearch/regionalPartnerSearch',
 
