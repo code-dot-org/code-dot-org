ff3115cef9e65999af0a0c1793f038428c71989e
Winter Dong <winter@code.org>
2019-01-26 00:58:16 -0800

    Reenable git fetch of branches

--- docker/unit_tests.sh
+++ docker/unit_tests.sh
@@ -15,10 +15,9 @@ export LD_LIBRARY_PATH=/usr/local/lib
 # circle.rake has logic which depends on these branches existing. If we're doing a shallow clone, e.g.
 # in a CI environment, then they don't exist by default.
 if $(git rev-parse --is-shallow-repository); then
-    # git remote set-branches --add origin staging
-    # git remote set-branches --add origin test
-    # git remote set-branches --add origin production
-    # git fetch --depth 1 -v
+    git remote set-branches --add origin staging test production
+    git remote show origin
+    git fetch --depth 1 origin staging test production
     git branch -a
 fi
 
