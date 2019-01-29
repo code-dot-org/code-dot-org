1f93522075241b70c5b584a5cd89885526739b60
Winter Dong <winter@code.org>
2019-01-28 16:42:21 -0800

    Refactor out git fetch into common step

--- docker/ui_tests.sh
+++ docker/ui_tests.sh
@@ -18,15 +18,6 @@ export CIRCLE_ARTIFACTS=/home/circleci/artifacts
 
 mkdir $CIRCLE_ARTIFACTS
 
-# circle.rake has logic which depends on these branches existing. If we're doing a shallow clone, e.g.
-# in a CI environment, then they don't exist by default.
-if $(git rev-parse --is-shallow-repository); then
-    git remote set-branches --add origin staging test production
-    git remote show origin
-    mispipe "git fetch --depth 50 origin staging test production" ts
-    git branch -a
-fi
-
 mysql -V
 
 # rbenv-doctor https://github.com/rbenv/rbenv-installer#readme
