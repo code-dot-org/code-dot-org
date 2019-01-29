1520281a9225e6fa392dff4f8cb6ed5a548905c0
Winter Dong <winter@code.org>
2019-01-28 17:31:19 -0800

    Factor out install and build into common step

--- docker/ui_tests.sh
+++ docker/ui_tests.sh
@@ -23,45 +23,6 @@ mysql -V
 # rbenv-doctor https://github.com/rbenv/rbenv-installer#readme
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 
-mispipe "bundle install --verbose" ts
-
-# set up locals.yml
-# Need to actually write all the commented out lines also
-set +x
-echo "
-netsim_redis_groups:
-- master: redis://ui-tests-redis:6379
-bundler_use_sudo: false
-properties_encryption_key: $PROPERTIES_ENCRYPTION_KEY
-applitools_eyes_api_key: $APPLITOOLS_KEY
-cloudfront_key_pair_id: $CLOUDFRONT_KEY_PAIR_ID
-cloudfront_private_key: \"$CLOUDFRONT_PRIVATE_KEY\"
-saucelabs_username: $SAUCE_USERNAME
-saucelabs_authkey: $SAUCE_ACCESS_KEY
-ignore_eyes_mismatches: true
-disable_all_eyes_running: true
-firebase_name: $FIREBASE_NAME
-firebase_secret: $FIREBASE_SECRET
-use_my_apps: true
-use_my_shared_js: true
-build_blockly_core: true
-build_shared_js: true
-build_dashboard: true
-build_pegasus: true
-build_apps: true
-localize_apps: true
-dashboard_enable_pegasus: true
-dashboard_workers: 5
-skip_seed_all: true
-" >> locals.yml
-echo "Wrote secrets from env vars into locals.yml."
-set -x
-
-# name: rake install
-RAKE_VERBOSE=true mispipe "bundle exec rake install" "ts '[%Y-%m-%d %H:%M:%S]'"
-
-# name: rake build
-RAKE_VERBOSE=true mispipe "bundle exec rake build --trace" "ts '[%Y-%m-%d %H:%M:%S]'"
 
 # apply test settings for after unit tests
 echo "
