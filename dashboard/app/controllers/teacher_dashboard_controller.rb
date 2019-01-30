9a299d0d1150e2300ff709c03600fb904e7a3435
Maddie Kasula <madelynkasula@gmail.com>
2019-01-29 19:10:13 -0800

    Unit test teacher_dashboard#index

--- dashboard/app/controllers/teacher_dashboard_controller.rb
+++ dashboard/app/controllers/teacher_dashboard_controller.rb
@@ -2,7 +2,7 @@ class TeacherDashboardController < ApplicationController
   def index
     return head :forbidden unless current_user&.teacher?
     sections = current_user.sections
-    section = sections.find(params[:section_id])
+    section = sections.find_by(id: params[:section_id])
     return head :forbidden unless section
 
     @section = section.summarize
