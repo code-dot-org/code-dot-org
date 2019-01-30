e1b297d23bf08b434f8813121a9611e273001917
Maddie Kasula <madelynkasula@gmail.com>
2019-01-30 13:41:42 -0800

    Rename teacher_dashboard index -> show

--- dashboard/app/controllers/teacher_dashboard_controller.rb
+++ dashboard/app/controllers/teacher_dashboard_controller.rb
@@ -1,5 +1,5 @@
 class TeacherDashboardController < ApplicationController
-  def index
+  def show
     return head :forbidden unless current_user&.teacher?
     sections = current_user.sections
     section = sections.find_by(id: params[:section_id])
