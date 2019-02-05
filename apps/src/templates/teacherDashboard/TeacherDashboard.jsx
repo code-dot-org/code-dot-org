15551afc78c42457a05c3ae2ca040ff3790a9c71
Maddie Kasula <madelynkasula@gmail.com>
2019-02-04 16:48:50 -0800

    Import PropTypes from prop-types instead of react

--- apps/src/templates/teacherDashboard/TeacherDashboard.jsx
+++ apps/src/templates/teacherDashboard/TeacherDashboard.jsx
@@ -1,4 +1,5 @@
-import React, {PropTypes, Component} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {Route, Switch} from 'react-router-dom';
 import SelectSectionDropdown from './SelectSectionDropdown';
 import TeacherDashboardNavigation from './TeacherDashboardNavigation';
