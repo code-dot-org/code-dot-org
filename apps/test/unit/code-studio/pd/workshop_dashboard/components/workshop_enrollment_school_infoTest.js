c2ad7db3468652d7d5b361432ad131b15075f9d4
Clare Constantine <clare@code.org>
2019-02-07 14:17:24 -0800

    WIP UI tests

---
+++ apps/test/unit/code-studio/pd/workshop_dashboard/components/workshop_enrollment_school_infoTest.js
@@ -0,0 +1,31 @@
+import {WorkshopEnrollmentSchoolInfo} from '@cdo/apps/code-studio/pd/workshop_dashboard/components/workshop_enrollment_school_info';
+import React from 'react';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+
+describe("Workshop Enrollment School Info", () => {
+  const fakeRouter = {
+    createHref() {}
+  };
+
+  const context = {
+    router: fakeRouter
+  };
+
+  it("shows Total Attendance column for local summer workshop", () => {
+    let workshopEnrollmentSchoolInfo = shallow(
+      <WorkshopEnrollmentSchoolInfo
+        enrollments={[]}
+        accountRequiredForAttendance={true}
+        onDelete={()=>{}}
+        workshopCourse="CS Principles"
+        workshopSubject="5-day Summer"
+        numSessions={5}
+      />,
+      { context },
+    );
+
+   expect(workshopEnrollmentSchoolInfo.find('th').filterWhere(col => col.text().includes('Total Attendance'))).to.have.length(1);
+
+  });
+});
