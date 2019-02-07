be8088e4b4b19af7f58c702ab5f19e2707e09a8d
Brendan Reville <brendan@code.org>
2019-02-07 13:28:10 -0800

    PL: small fixes for teacher application analytics
    - Report pageview when page is switched to complete erroneous fields
    - Fix styling of an error message
    - Fix URL generation for pageview to not append a 1

--- apps/src/code-studio/pd/application/teacher1920/Teacher1920Application.jsx
+++ apps/src/code-studio/pd/application/teacher1920/Teacher1920Application.jsx
@@ -6,7 +6,7 @@ import Section3TeachingBackground from './Section3TeachingBackground';
 import Section4ProfessionalLearningProgramRequirements from './Section4ProfessionalLearningProgramRequirements';
 import Section5AdditionalDemographicInformation from './Section5AdditionalDemographicInformation';
 import Section6Submission from './Section6Submission';
-import firehoseClient from "@cdo/apps/lib/util/firehose";
+import firehoseClient from '@cdo/apps/lib/util/firehose';
 /* global ga */
 
 export default class Teacher1920Application extends FormController {
@@ -72,7 +72,7 @@ export default class Teacher1920Application extends FormController {
    */
   onSetPage(newPage) {
     // Report a unique page view to GA.
-    ga('set', 'page', '/pd/application/teacher/' + newPage + 1);
+    ga('set', 'page', '/pd/application/teacher/' + (newPage + 1));
     ga('send', 'pageview');
   }
 }
