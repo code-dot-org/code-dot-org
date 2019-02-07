055e871b8680661c256e4579f6a64db05d6e5cec
Continuous Integration <dev@code.org>
2019-02-07 20:00:06 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/blocks/aalab/aalab_createNewSpriteNoName.js
+++ dashboard/config/blocks/aalab/aalab_createNewSpriteNoName.js
@@ -1,10 +1,10 @@
-function createNewSpriteNoName(name,costume,location) {
+function createNewSpriteNoName(costume,location) {
   if (!location) {
     location = {x: 200, y: 200};
   }
   var newSprite = makeNewSprite(costume,location.x,location.y);
   newSprite.maxSpeed = 0;
-  newSprite.name = name;
+  //newSprite.name = name;
   if(costumeGroups.hasOwnProperty(costume)) {
      costumeGroups[costume].push(newSprite);
   } else {
