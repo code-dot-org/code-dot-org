db01a5ef02d48fdd2d229a2031125ff7485623a8
nkiru onwuneme <generalhrh@gmail.com>
2019-02-08 20:36:22 -0800

    Add user school info model and migration

---
+++ dashboard/app/models/user_school_info.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: user_school_infos
+#
+#  id                     :integer          not null, primary key
+#  user_id                :integer          not null
+#  start_date             :datetime         not null
+#  end_date               :datetime
+#  school_info_id         :integer          not null
+#  last_confirmation_date :datetime         not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
+class UserSchoolInfo < ApplicationRecord
+end
