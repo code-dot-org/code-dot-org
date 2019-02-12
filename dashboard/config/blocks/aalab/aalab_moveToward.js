f7d33f78088a2988a5efbaa2b5c85d240ebd7feb
Continuous Integration <dev@code.org>
2019-02-12 20:00:06 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/blocks/aalab/aalab_moveToward.js
+++ dashboard/config/blocks/aalab/aalab_moveToward.js
@@ -2,16 +2,19 @@ function moveToward(sprite,distance,target) {
   if (!sprite || distance === undefined || !target) {
     return;
   }
+  if(typeof target === "function") {
+  	target = target();
+  }
   var move = function(s) {
-    var dx = target.x - sprite.x;
-  	var dy = target.y - sprite.y;
+    var dx = target.x - s.x;
+  	var dy = target.y - s.y;
   	if (dx * dx + dy * dy > distance * distance) {
       var angleOfMovement=Math.atan2(dy, dx);
       dx = distance*Math.cos(angleOfMovement);
       dy = distance*Math.sin(angleOfMovement);
   	}
-  	sprite.x += dx;
-  	sprite.y += dy;
+  	s.x += dx;
+  	s.y += dy;
   };
   if(!Array.isArray(sprite)) {
     move(sprite);
