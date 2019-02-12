4ace5f09bc40eb8d4d21fe1f702fc7a59c84579b
Ha Nguyen <ha@code.org>
2019-02-11 17:18:44 -0800

    add tests for csf intro follow up email

--- dashboard/test/mailers/previews/pd_regional_partner_contact_mailer_preview.rb
+++ dashboard/test/mailers/previews/pd_regional_partner_contact_mailer_preview.rb
@@ -1,18 +1,18 @@
-# This can be viewed on non-production environments at /rails/mailers/pd/workshop_mailer
+# This can be viewed on non-production environments at /rails/mailers/pd/regional_partner_contact_mailer
 class Pd::RegionalPartnerContactMailerPreview < ActionMailer::Preview
   include FactoryGirl::Syntax::Methods
 
   def contact_receipt_with_partner
     rp = build :regional_partner
-    rpc = build :pd_regional_partner_contact, regional_partner: rp
-    form = rpc.sanitize_and_trim_form_data_hash
+    rp_contact = build :pd_regional_partner_contact, regional_partner: rp
+    form = rp_contact.sanitize_and_trim_form_data_hash
 
     Pd::RegionalPartnerContactMailer.receipt(form, rp)
   end
 
   def contact_receipt_no_partner
-    rpc = build :pd_regional_partner_contact
-    form = rpc.sanitize_and_trim_form_data_hash
+    rp_contact = build :pd_regional_partner_contact
+    form = rp_contact.sanitize_and_trim_form_data_hash
 
     Pd::RegionalPartnerContactMailer.receipt(form, nil)
   end
