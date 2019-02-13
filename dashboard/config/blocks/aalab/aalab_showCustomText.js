60cd2c806f7ed1297f43d6097188a4ff8052e75e
Continuous Integration <dev@code.org>
2019-02-13 23:18:06 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/blocks/aalab/aalab_showCustomText.js
+++ dashboard/config/blocks/aalab/aalab_showCustomText.js
@@ -1,12 +1,44 @@
 function showCustomText(text, location, size, color, duration) {
+  text = text ? text : "";
+  location = location ? location : {x: 200, y: 200};
+  size = size ? size : 25;
+  color = color ? color : "black";
   var overlapIndex = -1;
+  textSize(size);
+  var newTextWidth = textWidth(text);
+  var newTextHeight = textLeading();
   for(var i = 0; i < customText.length && overlapIndex < 0; i++) {
-    if(customText[i].location.x === location.x && customText[i].location.y === location.y) {
+    textSize(customText[i].size);
+    var customTextWidth = textWidth(customText[i].text);
+    var customTextHeight = textLeading();
+    var overlapping = checkTextOverlap(location, newTextWidth, newTextHeight, customText[i].location, customTextWidth, customTextHeight);
+    if(overlapping) {
       overlapIndex = i;
     }
   }
-  if(overlapIndex > 0) {
+  if(overlapIndex >= 0) {
     customText.splice(overlapIndex, 1);
   }
   customText.push({text: text, location: location, size: size, color: color, duration: parseInt(duration) * 1000, timeStarted: new Date().getTime()});
+}
+
+// Classic algorithm problem!
+function checkTextOverlap(topLeftPointA, widthA, heightA, topLeftPointB, widthB, heightB) {
+  var bottomRightPointA = {
+    x: topLeftPointA.x + widthA,
+    y: topLeftPointA.y + heightA
+  };
+  var bottomRightPointB = {
+    x: topLeftPointB.x + widthB,
+    y: topLeftPointB.y + heightB
+  };
+  // check if one text is above the other (first two) 
+  // or if one text is to the left of the other (last two)
+  if(bottomRightPointA.y < topLeftPointB.y ||
+     bottomRightPointB.y < topLeftPointA.y ||
+     bottomRightPointA.x < topLeftPointB.x ||
+     bottomRightPointB.x < topLeftPointA.x) {
+  	return false;
+  }
+  return true;
 }
