4abe6225613f5e70cc8279b94eb0088b85fcb414
Maddie Kasula <madelynkasula@gmail.com>
2019-02-13 16:29:57 -0800

    Add teacherViewingStudent to user_progress server response

--- dashboard/app/controllers/api_controller.rb
+++ dashboard/app/controllers/api_controller.rb
@@ -337,7 +337,8 @@ class ApiController < ApplicationController
     if current_user
       script = Script.get_from_cache(params[:script])
       user = params[:user_id].present? ? User.find(params[:user_id]) : current_user
-      render json: summarize_user_progress(script, user)
+      teacher_viewing_student = current_user.students.include?(user)
+      render json: summarize_user_progress(script, user).merge({teacherViewingStudent: teacher_viewing_student})
     else
       render json: {}
     end
