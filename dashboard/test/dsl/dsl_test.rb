dd774018de18dc44f22d7c99f253160429b9e0ab
David Bailey <davidsbailey@users.noreply.github.com>
2019-02-19 12:29:38 -0800

    fix dsl/dsl_test

--- dashboard/test/dsl/dsl_test.rb
+++ dashboard/test/dsl/dsl_test.rb
@@ -28,6 +28,7 @@ class DslTest < ActiveSupport::TestCase
     version_year: nil,
     is_stable: nil,
     supported_locales: [],
+    pilot_experiment: nil,
   }
 
   test 'test Script DSL' do
@@ -631,7 +632,6 @@ DSL
   end
 
   test 'serialize new_name, family_name, version_year and is_stable' do
-    puts 'test_serialize_new_name_and_family_name'
     script = create :script,
       {
         new_name: 'new name',
