07ee87a734427aff5a7189c421cc7cd3a504f686
Chris Pirich <chris@code.org>
2019-02-19 12:36:43 -0800

    provide reasonable dropdown defaults for font-size

--- apps/src/applab/setPropertyDropdown.js
+++ apps/src/applab/setPropertyDropdown.js
@@ -76,7 +76,7 @@ var PROP_INFO = {
     friendlyName: 'font-size',
     internalName: 'fontSize',
     type: 'number',
-    defaultValue: '100'
+    defaultValue: '14'
   },
   textAlign: {
     friendlyName: 'text-align',
@@ -524,6 +524,8 @@ function getPropertyValueDropdown(param2) {
       return ['"fill"', '"cover"', '"contain"', '"none"'];
     case 'hidden':
     case 'checked':
+    case 'font-size':
+      return ['8', '10', '12', '14', '18', '24', '36', '72'];
     case 'readonly':
       return ['true', 'false'];
     case 'text':
