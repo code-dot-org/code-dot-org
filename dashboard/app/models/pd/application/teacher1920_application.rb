d515803105992c7f18dfb6f87020dc6704e0f663
Brad Buchanan <brad@code.org>
2019-02-20 14:42:55 -0800

    Add I don't know to the pay_fee question in section 4

--- dashboard/app/models/pd/application/teacher1920_application.rb
+++ dashboard/app/models/pd/application/teacher1920_application.rb
@@ -210,7 +210,8 @@ module Pd::Application
             'Yes, my school or I will be able to pay the full program fee.',
             TEXT_FIELDS[:no_pay_fee_1920],
             'Not applicable: there is no program fee for teachers in my region.',
-            'Not applicable: there is no Regional Partner in my region.'
+            'Not applicable: there is no Regional Partner in my region.',
+            "I don't know."
           ],
           willing_to_travel: TeacherApplicationBase.options[:willing_to_travel] << 'I am unable to travel to the school year workshops',
           interested_in_online_program: [YES, NO]
