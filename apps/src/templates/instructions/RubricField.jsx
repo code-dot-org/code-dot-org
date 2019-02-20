2b2d40eaae17efe84e4a4b8f66e0150b61f461de
Dani McAvoy <danimcavoy@gmail.com>
2019-02-20 16:20:19 -0500

    Started moving each rubric field into its own component - not functional yet

---
+++ apps/src/templates/instructions/RubricField.jsx
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import color from '@cdo/apps/util/color';
+
+const styles = {
+  rubricHeader: {
+    fontSize: 13,
+    marginLeft: 10,
+    color: color.black,
+    fontFamily: '"Gotham 5r", sans-serif'
+  },
+  performanceLevelHeader: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    padding: '4px 10px'
+  },
+  performanceLevelHeaderSelected: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    backgroundColor: color.lightest_cyan,
+    borderRadius: 10,
+    padding: '4px 10px'
+  }
+};
+
+const rubricLevelHeaders = {
+  'exceeds': 'Exceeds',
+  'meets': 'Meets',
+  'approaches': 'Approaches',
+  'noEvidence': 'No Evidence'
+}
+
+export class RubricField extends Component {
+  static propTypes = {
+    showFeedbackInputAreas: PropTypes.bool,
+    rubricLevel: PropTypes.string,
+    rubricValue: PropTypes.string,
+    disableMode: PropTypes.bool,
+    onChange: PropTypes.func,
+    currentlyChecked: PropTypes.bool
+  };
+
+  handleRubricChange = event => {
+    this.props.onChange(event.target.value);
+  };
+
+  render() {
+    return (
+      <div
+        style={
+          this.props.currentlyChecked
+            ? styles.performanceLevelHeaderSelected
+            : styles.performanceLevelHeader
+        }
+      >
+        {this.props.showFeedbackInputAreas && (
+          <input
+            type={'checkbox'}
+            id={this.props.rubricLevel + '-input'}
+            name={'rubric'}
+            value={this.props.rubricLevel}
+            checked={this.props.currentlyChecked}
+            onChange={this.handleRubricChange}
+            disabled={this.props.disabledMode}
+          />
+        )}
+        <details>
+          <summary style={styles.rubricHeader}>Exceeds</summary>
+          <p>{this.props.rubricValue}</p>
+        </details>
+      </div>
+    );
+  }
+}
