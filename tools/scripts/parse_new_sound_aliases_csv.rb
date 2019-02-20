dee089436b1acbc76db826ba6503b6afe25c3295
Karis Kim <ouniiq@hotmail.com>
2019-02-20 09:56:08 -0800

    refactored code to clear all rubocop offenses. double checked to see if json is still created after refactoring and it does.

--- tools/scripts/parse_new_sound_aliases_csv.rb
+++ tools/scripts/parse_new_sound_aliases_csv.rb
@@ -7,7 +7,7 @@ class NewSoundAliasesMetadataBuilder
     @csv = CSV.parse(File.read(csv), headers: true)
   end
 
-  def parseFilename(csv)
+  def parse_filenames(csv)
     # csv_text = File.read(csv)
     # table = CSV.parse(File.read(csv), headers: true)
     filename = csv.by_col[1]
@@ -17,41 +17,40 @@ class NewSoundAliasesMetadataBuilder
     end
   end
 
-  def parseAliases(csv)
+  def parse_aliases(csv)
     # removed spaces to simplify
     # because csv file had aliases that had
     # more than one space or none
     aliases_with_spaces_removed = csv.by_col[3].each do |aliase|
       aliase.gsub!(/,\s+/, ',')
-      aliase.gsub!(/ /, ',')
+      aliase.tr!(" ", ',')
     end
-    return parsed_alises = aliases_with_spaces_removed.map do |aliase|
-      "\"#{aliase.split(/\,/).join("\", \"").gsub!(/,\s+/, ',')}\""
+    return aliases_with_spaces_removed.map do |aliase|
+      "\"#{aliase.split(/\,/).join('\', \'').gsub!(/,\s+/, ',')}\""
     end
   end
 
-  def createJSON()
-    filename = parseFilename(@csv)
-    parsed_alises = parseAliases(@csv)
-    
+  def create_json
+    filename = parse_filenames(@csv)
+    parsed_alises = parse_aliases(@csv)
+
     sound_hash = {}
     filename.zip(parsed_alises).each do |name, aliases|
-      sound_hash["#{name}"] = aliases
+      sound_hash[name.to_s] = aliases
     end
 
     dir_name = ARGV[0]
 
-
-    sound_hash.each do |filename, aliases|
+    sound_hash.each do |name, aliases|
       Dir["#{File.expand_path('~')}/Downloads/#{dir_name}/mp3_files/*"].each do |file_path|
-        regex = /^#{filename}$/
+        regex = /^#{name}$/
         if file_path.split('/')[-1].match(regex)
           aliases << ",\"category_#{dir_name.downcase}\""
-          puts `./generateSoundMetadata.rb --aliases "#{aliases}" #{File.expand_path('~')}/Downloads/"#{dir_name}"/mp3_files/"#{filename}"`
+          puts `./generateSoundMetadata.rb --aliases "#{aliases}" #{File.expand_path('~')}/Downloads/"#{dir_name}"/mp3_files/"#{name}"`
         end
       end
     end
   end
 end
 
-NewSoundAliasesMetadataBuilder.new('./sound_metadata.csv').createJSON()
+NewSoundAliasesMetadataBuilder.new('./sound_metadata.csv').create_json
