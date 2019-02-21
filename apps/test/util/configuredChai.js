8f3bd088c05c8905fb18343cf4d1ac0829a536bd
Jessica Kulwik <jessica@code.org>
2019-02-21 20:08:31 +0000

    Updated enzyme to v3

--- apps/test/util/configuredChai.js
+++ apps/test/util/configuredChai.js
@@ -1,7 +1,6 @@
 /** @file Import chai and configure it for our own test style. */
 import chai from 'chai';
 import chaiSubset from 'chai-subset';
-import chaiEnzyme from 'chai-enzyme';
 import sinonChai from 'sinon-chai';
 import sinonChaiInOrder from 'sinon-chai-in-order';
 import chaiAsPromised from 'chai-as-promised';
@@ -9,7 +8,6 @@ import chaiXml from 'chai-xml';
 
 chai.use(sinonChai);
 chai.use(sinonChaiInOrder);
-chai.use(chaiEnzyme());
 chai.use(chaiSubset);
 chai.use(chaiAsPromised);
 chai.use(chaiXml);
