54d55fb9513322104c33a4c84f3df9b543523890
Clare Constantine <clare@code.org>
2019-02-20 17:09:25 -0800

    fix test; don't update scholarship status for facilitator apps

--- dashboard/app/controllers/api/v1/pd/applications_controller.rb
+++ dashboard/app/controllers/api/v1/pd/applications_controller.rb
@@ -145,9 +145,10 @@ module Api::V1::Pd
         summer_workshop_changed = true
       end
 
-      if application_data[:scholarship_status] != @application.scholarship_status
+      if @application.application_type == TEACHER_APPLICATION && application_data[:scholarship_status] != @application.scholarship_status
         scholarship_status_changed = true
       end
+      scholarship_status = application_data.delete(:scholarship_status)
 
       if application_data[:response_scores]
         application_data[:response_scores] = JSON.parse(application_data[:response_scores]).transform_keys {|x| x.to_s.underscore}.to_json
@@ -192,7 +193,7 @@ module Api::V1::Pd
         return render status: :bad_request, json: {errors: @application.errors.full_messages}
       end
 
-      @application.update_scholarship_status(current_user, application_data[:scholarship_status]) if scholarship_status_changed
+      @application.update_scholarship_status(scholarship_status) if scholarship_status_changed
 
       @application.update_status_timestamp_change_log(current_user) if status_changed
       @application.log_fit_workshop_change(current_user) if fit_workshop_changed
