78d42cc5e6a5ed271a2d57f81bdd7adedcaf6331
Continuous Integration <dev@code.org>
2019-02-25 20:00:05 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/blocks/GamelabJr/gamelab_moveForward.js
+++
@@ -1,5 +0,0 @@
-function moveForward(sprite, distance) {
-  var direction = getDirection(sprite);
-  sprite.x += distance * Math.cos(direction * Math.PI / 180);
-  sprite.y += distance * Math.sin(direction * Math.PI / 180);
-}
