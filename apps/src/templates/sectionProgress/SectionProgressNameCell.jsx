689312166fa59edf3206a9436cd6e7b54d0ae844
Maddie Kasula <madelynkasula@gmail.com>
2019-02-25 15:50:54 -0800

    Use scriptUrlForStudent helper for progress student link

--- apps/src/templates/sectionProgress/SectionProgressNameCell.jsx
+++ apps/src/templates/sectionProgress/SectionProgressNameCell.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {progressStyles} from './multiGridConstants';
 import {getSelectedScriptName} from '@cdo/apps/redux/scriptSelectionRedux';
-import experiments from '@cdo/apps/util/experiments';
+import {scriptUrlForStudent} from '@cdo/apps/templates/teacherDasboard/urlHelpers';
 
 class SectionProgressNameCell extends Component {
   static propTypes = {
@@ -18,17 +18,16 @@ class SectionProgressNameCell extends Component {
 
   render() {
     const {name, studentId, sectionId, scriptId, scriptName} = this.props;
-
-    let scriptUrlForStudent = '';
-    if (experiments.isEnabled(experiments.TEACHER_DASHBOARD_REACT)) {
-      scriptUrlForStudent = `/s/${scriptName}?section_id=${sectionId}&user_id=${studentId}&viewAs=Teacher`;
-    } else {
-      scriptUrlForStudent = `/teacher-dashboard#/sections/${sectionId}/student/${studentId}/script/${scriptId}`;
-    }
+    const studentUrl = scriptUrlForStudent(
+      sectionId,
+      scriptId,
+      scriptName,
+      studentId
+    );
 
     return (
       <div style={progressStyles.nameCell}>
-        <a href={scriptUrlForStudent} style={progressStyles.link}>
+        <a href={studentUrl} style={progressStyles.link}>
           {name}
         </a>
       </div>
