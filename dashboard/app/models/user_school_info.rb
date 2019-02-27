03c026d5c18edde2212d78285260af2b83c50832
nkiru onwuneme <generalhrh@gmail.com>
2019-02-27 09:34:42 -0800

    add validation and active record association

--- dashboard/app/models/user_school_info.rb
+++ dashboard/app/models/user_school_info.rb
@@ -13,6 +13,10 @@
 #
 
 class UserSchoolInfo < ApplicationRecord
-  validates_presence_of :user
-  validates_presence_of :school_info
+  # validates_presence_of :user
+  # validates_presence_of :school_info
+  validates_presence_of :user, :school_info, :start_date, :last_confirmation_date
+
+  belongs_to :user
+  belongs_to :school_info
 end
