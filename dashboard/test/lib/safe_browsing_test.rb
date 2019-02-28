514870a30709197901e6bcb7e5a3d8d87bc7f12b
Erin Peach <erin@code.org>
2019-02-28 10:42:51 -0800

    Use VCR in tests

--- dashboard/test/lib/safe_browsing_test.rb
+++ dashboard/test/lib/safe_browsing_test.rb
@@ -1,6 +1,14 @@
-require 'test_helper'
+require_relative '../../../shared/test/test_helper'
+require_relative '../../lib/safe_browsing'
 
-class SafeBrowsingTest < ActiveSupport::TestCase
+class SafeBrowsingTest < Minitest::Test
+  include SetupTest
+  # Do additional VCR configuration so as to prevent the CDO.google_safe_browsing_key from being logged to the
+  # YML cassette, instead replacing it with a placeholder string.
+  VCR.configure do |c|
+    c.filter_sensitive_data('<API_KEY>') {CDO.google_safe_browsing_key}
+  end
+
   def test_determine_safe_website
     assert SafeBrowsing.determine_safe_to_open("https://code.org/")
   end
