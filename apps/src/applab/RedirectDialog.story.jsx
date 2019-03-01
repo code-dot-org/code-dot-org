c3f42ee54133081786e3c84571feb81fc58d890b
Erin Peach <erin@code.org>
2019-03-01 11:20:51 -0800

    Reject redirect dialog

--- apps/src/applab/RedirectDialog.story.jsx
+++ apps/src/applab/RedirectDialog.story.jsx
@@ -5,7 +5,11 @@ export default storybook => {
   storybook.storiesOf('RedirectDialog', module).addStoryTable([
     {
       name: 'Approved Site',
-      story: () => <RedirectDialog url={'https://code.org/'} isOpen={true} />
+      story: () => <RedirectDialog url={'https://code.org/'} approved isOpen />
+    },
+    {
+      name: 'Rejected Site',
+      story: () => <RedirectDialog url={'https://code.org/'} isOpen />
     }
   ]);
 };
