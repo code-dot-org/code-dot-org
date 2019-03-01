46970947ed23b927e8f1cea58849807d661a0542
Erin Peach <erin@code.org>
2019-03-01 11:20:51 -0800

    Reject redirect dialog

--- apps/src/applab/RedirectDialog.jsx
+++ apps/src/applab/RedirectDialog.jsx
@@ -8,6 +8,7 @@ import i18n from '@cdo/locale';
 export default class RedirectDialog extends React.Component {
   static propTypes = {
     url: PropTypes.string.isRequired,
+    approved: PropTypes.bool,
     isOpen: PropTypes.bool
   };
 
@@ -20,9 +21,12 @@ export default class RedirectDialog extends React.Component {
   };
 
   render() {
-    return (
-      <Dialog title={i18n.redirectTitle()} isOpen={this.state.isOpen}>
-        <Body>
+    let title, body, footer;
+
+    if (this.props.approved) {
+      title = i18n.redirectTitle();
+      body = (
+        <div>
           <h2>{i18n.redirectConfirm({url: this.props.url})}</h2>
           <p>
             {i18n.redirectExplanation()}
@@ -32,19 +36,42 @@ export default class RedirectDialog extends React.Component {
               </a>
             </span>
           </p>
-          <DialogFooter>
-            <Button
-              onClick={this.closeDialog}
-              text={i18n.goBack()}
-              color={Button.ButtonColor.gray}
-            />
-            <Button
-              href={this.props.url}
-              target={'_blank'}
-              text={i18n.continue()}
-              color={Button.ButtonColor.orange}
-            />
-          </DialogFooter>
+        </div>
+      );
+      footer = (
+        <DialogFooter>
+          <Button
+            onClick={this.closeDialog}
+            text={i18n.goBack()}
+            color={Button.ButtonColor.gray}
+          />
+          <Button
+            href={this.props.url}
+            target={'_blank'}
+            text={i18n.continue()}
+            color={Button.ButtonColor.orange}
+          />
+        </DialogFooter>
+      );
+    } else {
+      title = i18n.redirectRejectTitle();
+      body = <p>{i18n.redirectRejectExplanation()}</p>;
+      footer = (
+        <DialogFooter rightAlign>
+          <Button
+            onClick={this.closeDialog}
+            text={i18n.dialogOK()}
+            color={Button.ButtonColor.gray}
+          />
+        </DialogFooter>
+      );
+    }
+
+    return (
+      <Dialog title={title} isOpen={this.state.isOpen}>
+        <Body>
+          {body}
+          {footer}
         </Body>
       </Dialog>
     );
