23789f61985b23baff0f3e346806ca4d1b8baf66
Clare Constantine <clare@code.org>
2019-03-07 13:55:53 -0800

    misplaced paren

--- dashboard/app/models/pd/application/teacher1920_application.rb
+++ dashboard/app/models/pd/application/teacher1920_application.rb
@@ -515,7 +515,7 @@ module Pd::Application
         meets_scholarship_criteria_scores[:previous_yearlong_cdo_pd] = responses[:previous_yearlong_cdo_pd].exclude?('CS Principles') ? YES : NO
       end
 
-      bonus_points_scores[:taught_in_past] = (responses[:taught_in_past] & TAUGHT_IN_PAST_NO_BONUS_POINT_RESPONSES.any?) ? 0 : 2
+      bonus_points_scores[:taught_in_past] = (responses[:taught_in_past] & TAUGHT_IN_PAST_NO_BONUS_POINT_RESPONSES).any? ? 0 : 2
 
       # Section 4
       meets_minimum_criteria_scores[:committed] = responses[:committed] == options[:committed].first ? YES : NO
