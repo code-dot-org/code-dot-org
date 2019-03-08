249de486a7723488bb673c6973aa8db7aed8ecfa
Brendan Reville <brendan@code.org>
2019-03-08 00:17:51 -0800

    PL: workshop enrollment for local summer requires sign-in

--- dashboard/app/controllers/pd/workshop_enrollment_controller.rb
+++ dashboard/app/controllers/pd/workshop_enrollment_controller.rb
@@ -30,6 +30,8 @@ class Pd::WorkshopEnrollmentController < ApplicationController
           workshop_enrollment_status: "full"
         }.to_json
       }
+    elsif @workshop.local_summer? && !current_user
+      render :logged_out
     else
       @enrollment = ::Pd::Enrollment.new workshop: @workshop
       if current_user
