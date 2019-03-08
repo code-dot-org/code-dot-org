51ba096d8b577824b023dd1df5bad4b5131b0d2f
Clare Constantine <clare@code.org>
2019-03-07 16:06:50 -0800

    add UI tests

--- apps/test/unit/code-studio/pd/workshop_dashboard/components/workshop_enrollment_school_infoTest.js
+++ apps/test/unit/code-studio/pd/workshop_dashboard/components/workshop_enrollment_school_infoTest.js
@@ -1,4 +1,4 @@
-import {WorkshopEnrollmentSchoolInfo} from '@cdo/apps/code-studio/pd/workshop_dashboard/components/workshop_enrollment_school_info';
+import WorkshopEnrollmentSchoolInfo from '@cdo/apps/code-studio/pd/workshop_dashboard/components/workshop_enrollment_school_info';
 import React from 'react';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
@@ -30,5 +30,25 @@ describe('Workshop Enrollment School Info', () => {
         .find('th')
         .filterWhere(col => col.text().includes('Total Attendance'))
     ).to.have.length(1);
+  });
+
+  it('does not show Total Attendance column for non-local-summer workshop', () => {
+    let workshopEnrollmentSchoolInfo = shallow(
+      <WorkshopEnrollmentSchoolInfo
+        enrollments={[]}
+        accountRequiredForAttendance={true}
+        onDelete={() => {}}
+        workshopCourse="CS Discoveries"
+        workshopSubject="1-day Academic Year, Units 1 and 2"
+        numSessions={5}
+      />,
+      {context}
+    );
+
+    expect(
+      workshopEnrollmentSchoolInfo
+        .find('th')
+        .filterWhere(col => col.text().includes('Total Attendance'))
+    ).to.have.length(0);
   });
 });
