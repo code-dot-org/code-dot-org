361dc6d775b7491396c2eec84cbe77b658b9f1f9
Dani McAvoy <danimcavoy@gmail.com>
2019-03-12 11:16:08 -0400

    Only force resize of instructions area for the feedback tab. Keep instructions tab working as before

--- apps/src/templates/instructions/TopInstructionsCSP.jsx
+++ apps/src/templates/instructions/TopInstructionsCSP.jsx
@@ -243,7 +243,10 @@ class TopInstructions extends Component {
       RESIZER_HEIGHT;
 
     this.props.setInstructionsMaxHeightNeeded(maxNeededHeight);
-    this.props.setInstructionsRenderedHeight(maxNeededHeight);
+    // Force instruction area to resize to show full feedback area
+    if (this.state.tabSelected === TabType.COMMENTS) {
+      this.props.setInstructionsRenderedHeight(maxNeededHeight);
+    }
     return maxNeededHeight;
   };
 
