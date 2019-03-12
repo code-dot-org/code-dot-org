4c176a5f773b972bb799b3f2f522ac325d299db0
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-03-12 14:13:34 -0700

    populate ssh_config from existing AWS instances as well as from Chef nodes

--- cookbooks/cdo-users/metadata.rb
+++ cookbooks/cdo-users/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Creates users for the accounts defined in Chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.20'
+
+depends 'apt', '~> 2.6.0'
