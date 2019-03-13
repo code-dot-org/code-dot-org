c6c003241f0341f2eb9c4467ca8a46edc3c8a5b1
Maddie Kasula <madelynkasula@gmail.com>
2019-03-13 16:20:03 -0700

    Include level.bonus in processedLevel data

--- apps/src/templates/progress/progressHelpers.js
+++ apps/src/templates/progress/progressHelpers.js
@@ -148,6 +148,7 @@ export const processedLevel = level => {
     icon: level.icon,
     isUnplugged: level.kind === LevelKind.unplugged,
     levelNumber: level.kind === LevelKind.unplugged ? undefined : level.title,
-    isConceptLevel: level.is_concept_level
+    isConceptLevel: level.is_concept_level,
+    bonus: level.bonus
   };
 };
