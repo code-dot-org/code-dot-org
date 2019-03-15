ef64f3fcf810ffb9b2f92208fe17a294a3308be2
nkiru onwuneme <generalhrh@gmail.com>
2019-03-15 03:11:15 -0700

    fix diversion

--- apps/src/templates/projects/FeaturedProjectsTable.jsx
+++ apps/src/templates/projects/FeaturedProjectsTable.jsx
@@ -15,6 +15,8 @@ import QuickActionsCell from '../tables/QuickActionsCell';
 import {tableLayoutStyles, sortableOptions} from '../tables/tableConstants';
 import PopUpMenu from '@cdo/apps/lib/ui/PopUpMenu';
 
+lkjlkjlkjljljljlkjl
+
 const PROJECT_DEFAULT_IMAGE = '/blockly/media/projects/project_default.png';
 
 const THUMBNAIL_SIZE = 65;
