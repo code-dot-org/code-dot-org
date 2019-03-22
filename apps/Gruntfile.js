a24567a43739dded1edec1ae4d179837b8bb0d68
David Bailey <davidsbailey@users.noreply.github.com>
2019-03-22 14:13:30 -0700

    extract webpack-runtime and include it in dashboard and pegasus

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -720,7 +720,7 @@ describe('entry tests', () => {
           }
         ],
         runtimeChunk: {
-          name: 'essential'
+          name: 'webpack-runtime'
         },
         splitChunks: {
           cacheGroups: {
