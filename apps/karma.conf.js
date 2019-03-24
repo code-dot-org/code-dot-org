c3289fad8410fa07c362df28660fc4c9e01c0092
David Bailey <davidsbailey@users.noreply.github.com>
2019-03-24 13:06:14 -0700

    disable optimization and minification for karma test targets

--- apps/karma.conf.js
+++ apps/karma.conf.js
@@ -50,7 +50,7 @@ module.exports = function(config) {
       'test/scratch-tests.js': ['webpack']
     },
 
-    webpack: webpackConfig,
+    webpack: {...webpackConfig, optimization: undefined, mode: 'development'},
     webpackMiddleware: {
       noInfo: true,
       stats: {
