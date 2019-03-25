1d2e859ea6af0168d9a61603e13827bcec08f9e5
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-03-25 16:57:45 -0700

    Make GeneratedCodeTest more specific in what exactly it is testing

--- apps/test/unit/templates/feedback/GeneratedCodeTest.jsx
+++ apps/test/unit/templates/feedback/GeneratedCodeTest.jsx
@@ -1,23 +1,21 @@
-/* eslint-disable react/no-danger */
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from '../../../util/configuredChai';
 import GeneratedCode from '@cdo/apps/templates/feedback/GeneratedCode';
+import UnsafeRenderedMarkdown from '@cdo/apps/templates/UnsafeRenderedMarkdown';
 
 describe('GeneratedCode', () => {
-  it('renders successfully', () => {
-    const wrapper = shallow(
-      <GeneratedCode message="Test message" code="Test code" />
-    );
+  const wrapper = shallow(
+    <GeneratedCode message="Test message" code="Test code" />
+  );
+
+  it('renders code explicitly in ltr', () => {
+    expect(wrapper).to.containMatchingElement(<pre dir="ltr">Test code</pre>);
+  });
+
+  it('renders message with markdown support', () => {
     expect(wrapper).to.containMatchingElement(
-      <div className="generated-code-container">
-        <div className="generatedCodeMessage">
-          <p>Test message</p>
-        </div>
-        <pre className="generatedCode" dir="ltr">
-          Test code
-        </pre>
-      </div>
+      <UnsafeRenderedMarkdown markdown="Test message" />
     );
   });
 });
