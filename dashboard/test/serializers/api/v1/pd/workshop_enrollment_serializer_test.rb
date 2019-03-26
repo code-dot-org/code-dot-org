4982fd4036a8c240b300c9cd5db25d0d09ca1c5c
Clare Constantine <clare@code.org>
2019-03-26 15:23:35 -0700

    fix serializer test

--- dashboard/test/serializers/api/v1/pd/workshop_enrollment_serializer_test.rb
+++ dashboard/test/serializers/api/v1/pd/workshop_enrollment_serializer_test.rb
@@ -5,7 +5,10 @@ class Api::V1::Pd::WorkshopEnrollmentSerializerTest < ::ActionController::TestCa
     enrollment = create :pd_enrollment
     expected_attributes = [
       :id, :first_name, :last_name, :email, :district_name, :school, :role,
-      :grades_teaching, :user_id, :attended, :pre_workshop_survey, :attendances
+      :grades_teaching, :attended_csf_intro_workshop, :csf_course_experience,
+      :csf_courses_planned, :csf_has_physical_curriculum_guide, :user_id, :attended,
+      :pre_workshop_survey, :previous_courses, :replace_existing, :attendances,
+      :scholarship_status
     ]
 
     serialized = ::Api::V1::Pd::WorkshopEnrollmentSerializer.new(enrollment).attributes
