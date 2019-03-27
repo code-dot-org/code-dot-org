1facdee844b550328aa239f8cd6fe13575b9ab2e
Clare Constantine <clare@code.org>
2019-03-26 17:20:59 -0700

    remove breaking change to pd factory

--- dashboard/test/factories/pd_factories.rb
+++ dashboard/test/factories/pd_factories.rb
@@ -565,7 +565,6 @@ FactoryGirl.define do
   factory :pd_attendance, class: 'Pd::Attendance' do
     association :session, factory: :pd_session
     association :teacher
-    association :enrollment, factory: :pd_enrollment
   end
 
   factory :pd_attendance_no_account, class: 'Pd::Attendance' do
