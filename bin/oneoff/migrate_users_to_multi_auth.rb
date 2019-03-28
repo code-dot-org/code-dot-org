0739a1428762b9c108a5a1a99376e22cb9dc03ab
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-03-28 14:03:41 -0700

    don't bother to use transactions in this script, since the migration is already internally transaction-safe

--- bin/oneoff/migrate_users_to_multi_auth.rb
+++ bin/oneoff/migrate_users_to_multi_auth.rb
@@ -13,14 +13,12 @@ def migrate_batches(limit=nil)
   puts "Migrating #{limit || 'all'} slice(s)"
   unmigrated_users.find_in_batches(batch_size: SLICE_SIZE) do |slice|
     puts "\tSLICE_COUNT: #{slice_count + 1} of ~#{unmigrated_users_count / SLICE_SIZE}..."
-    ActiveRecord::Base.transaction do
-      successes = 0
-      slice.each do |user|
-        user.migrate_to_multi_auth
-        successes += 1 if user.valid? && user.provider == User::PROVIDER_MIGRATED
-      end
-      puts "\t\tsuccessful migrations: #{successes} / #{SLICE_SIZE}"
+    successes = 0
+    slice.each do |user|
+      user.migrate_to_multi_auth
+      successes += 1 if user.valid? && user.provider == User::PROVIDER_MIGRATED
     end
+    puts "\t\tsuccessful migrations: #{successes} / #{SLICE_SIZE}"
     slice_count += 1
 
     if limit && slice_count >= limit
