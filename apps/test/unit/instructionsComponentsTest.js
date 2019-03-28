83af60ad1e6538d8a4aee5fcd1cdde3df1504fb6
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-03-28 14:22:02 -0700

    remove test

--- apps/test/unit/instructionsComponentsTest.js
+++ apps/test/unit/instructionsComponentsTest.js
@@ -3,7 +3,6 @@ var testUtils = require('./../util/testUtils');
 import React from 'react';
 import {mount} from 'enzyme';
 import {StatelessMarkdownInstructions} from '@cdo/apps/templates/instructions/MarkdownInstructions';
-import NonMarkdownInstructions from '@cdo/apps/templates/instructions/NonMarkdownInstructions';
 
 describe('instructions components', () => {
   testUtils.setExternalGlobals();
@@ -39,44 +38,6 @@ describe('instructions components', () => {
       );
       var element = dom.find('.instructions-markdown').first();
       expect(element.props().style.paddingTop).to.equal(0);
-    });
-  });
-
-  describe('NonMarkdownInstructions', function() {
-    it('can have just instructions', function() {
-      var dom = mount(
-        <div>
-          <NonMarkdownInstructions
-            puzzleTitle="title"
-            shortInstructions="instructions"
-          />
-        </div>
-      );
-      var element = dom.find('p');
-      expect(element.length).to.equal(2);
-      expect(element.first())
-        .text()
-        .to.equal('title');
-      expect(element.last())
-        .text()
-        .to.equal('instructions');
-    });
-
-    it('can have both instructions and instructions2', function() {
-      var dom = mount(
-        <div>
-          <NonMarkdownInstructions
-            puzzleTitle="title"
-            shortInstructions="instructions"
-            instructions2="instructions2"
-          />
-        </div>
-      );
-      var element = dom.find('p');
-      expect(element.length).to.equal(3);
-      expect(element.get(0).textContent).to.equal('title');
-      expect(element.get(1).textContent).to.equal('instructions');
-      expect(element.get(2).textContent).to.equal('instructions2');
     });
   });
 });
