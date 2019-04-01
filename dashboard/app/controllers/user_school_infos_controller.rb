9143badfb0affec6e60024357607b1ba95a0eb71
nkiru onwuneme <generalhrh@gmail.com>
2019-04-01 07:04:01 -0700

    add update action

--- dashboard/app/controllers/user_school_infos_controller.rb
+++ dashboard/app/controllers/user_school_infos_controller.rb
@@ -1,2 +1,13 @@
 class UserSchoolInfosController < ApplicationController
+  def update
+    user_school_info = UserSchoolInfo.find([:id])
+    result = user_school_info.update ({
+      last_confirmation_date: DateTime.current
+    })
+    if result
+      head :no_content
+    else
+      render json: user_school_info.errors, status: :unprocessable_entity
+    end
+  end
 end
