b4c9eaa3470d3d7b57044a3ed67991e88e2639aa
Maddie Kasula <madelynkasula@gmail.com>
2019-04-03 13:13:21 -0700

    Remove experiment and scriptId param from scriptUrlForStudent

--- apps/src/templates/teacherDashboard/urlHelpers.js
+++ apps/src/templates/teacherDashboard/urlHelpers.js
@@ -37,19 +37,6 @@ export const teacherDashboardUrl = (sectionId, path = '') => {
   }
 };
 
-export const scriptUrlForStudent = (
-  sectionId,
-  scriptId,
-  scriptName,
-  studentId
-) => {
-  if (experiments.isEnabled(experiments.TEACHER_DASHBOARD_REACT)) {
-    return `/s/${scriptName}?section_id=${sectionId}&user_id=${studentId}&viewAs=Teacher`;
-  } else {
-    let url = `/teacher-dashboard#/sections/${sectionId}/student/${studentId}`;
-    if (scriptId) {
-      url += `/script/${scriptId}`;
-    }
-    return url;
-  }
+export const scriptUrlForStudent = (sectionId, scriptName, studentId) => {
+  return `/s/${scriptName}?section_id=${sectionId}&user_id=${studentId}&viewAs=Teacher`;
 };
