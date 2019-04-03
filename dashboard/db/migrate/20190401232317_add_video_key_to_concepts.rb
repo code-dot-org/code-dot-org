b84ae4b32c5d8296028ce18833461739be8e8b99
Bethany Connor <bethany@code.org>
2019-04-03 19:46:41 +0000

    Fix for failing ui tests on Drone

--- dashboard/db/migrate/20190401232317_add_video_key_to_concepts.rb
+++ dashboard/db/migrate/20190401232317_add_video_key_to_concepts.rb
@@ -2,5 +2,7 @@ class AddVideoKeyToConcepts < ActiveRecord::Migration[5.0]
   def change
     add_column :concepts, :video_key, :string
     remove_column :concepts, :video_id
+
+    Concept.reset_column_information
   end
 end
