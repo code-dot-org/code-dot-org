f3baf81b0ee28ce1e8c63140ca7eac381b27bc24
Bethany Connor <bethany@code.org>
2019-04-03 23:04:36 +0000

    More explicit migration

--- dashboard/db/migrate/20190401232317_add_video_key_to_concepts.rb
+++ dashboard/db/migrate/20190401232317_add_video_key_to_concepts.rb
@@ -1,8 +1,9 @@
 class AddVideoKeyToConcepts < ActiveRecord::Migration[5.0]
   def change
     add_column :concepts, :video_key, :string
-    remove_column :concepts, :video_id
-
-    Concept.reset_column_information
+    remove_index :concepts, :video_id
+    remove_foreign_key :concepts, :videos
+    remove_column :concepts, :video_id, :int
+    add_index :concepts, :video_key
   end
 end
