d3f85670e2570574df28e077f2d4a8edd99ff679
Bethany Connor <bethany@code.org>
2019-04-04 19:21:04 +0000

    Simpler migration

--- dashboard/db/migrate/20190401232317_add_video_key_to_concepts.rb
+++ dashboard/db/migrate/20190401232317_add_video_key_to_concepts.rb
@@ -1,9 +1,6 @@
 class AddVideoKeyToConcepts < ActiveRecord::Migration[5.0]
   def change
-    add_column :concepts, :video_key, :string
-    remove_index :concepts, :video_id
-    remove_foreign_key :concepts, :videos
-    remove_column :concepts, :video_id, :int
-    add_index :concepts, :video_key
+    rename_column :concepts, :video_id, :video_key
+    change_column :concepts, :video_key, :string
   end
 end
--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -246,6 +246,7 @@ ActiveRecord::Schema.define(version: 20190401232317) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "video_key"
+    t.index ["video_key"], name: "index_concepts_on_video_key", using: :btree
   end
 
   create_table "concepts_levels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
