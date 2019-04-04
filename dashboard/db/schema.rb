73313aec860a85644efb5f2f1b85fc1deeb550cd
Bethany Connor <bethany@code.org>
2019-04-04 20:03:34 +0000

    Add video_key as column in the concepts table

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190321174350) do
+ActiveRecord::Schema.define(version: 20190404195323) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -246,6 +246,7 @@ ActiveRecord::Schema.define(version: 20190321174350) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "video_id"
+    t.string   "video_key"
     t.index ["video_id"], name: "index_concepts_on_video_id", using: :btree
   end
 
