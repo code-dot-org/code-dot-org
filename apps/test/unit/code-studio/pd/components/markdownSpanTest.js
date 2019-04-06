f9016867a311006ab68e08496444163fd245edae
Brad Buchanan <brad@code.org>
2019-04-05 17:47:40 -0700

    to.have.html(a)

--- apps/test/unit/code-studio/pd/components/markdownSpanTest.js
+++ apps/test/unit/code-studio/pd/components/markdownSpanTest.js
@@ -9,7 +9,7 @@ describe('MarkdownSpan', () => {
       <MarkdownSpan>normal text *bold text*</MarkdownSpan>
     );
 
-    expect(markdownSpan).to.have.html(
+    expect(markdownSpan.html()).to.include(
       '<span>normal text <em>bold text</em></span>'
     );
   });
@@ -19,7 +19,7 @@ describe('MarkdownSpan', () => {
       <MarkdownSpan>This is a [link](https://code.org).</MarkdownSpan>
     );
 
-    expect(markdownSpan).to.have.html(
+    expect(markdownSpan.html()).to.include(
       '<span>This is a <a href="https://code.org" target="_blank">link</a>.</span>'
     );
   });
