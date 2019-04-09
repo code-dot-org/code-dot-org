e33c838c1db32ac373fc74cfc5853b35afa0577b
nkiru onwuneme <generalhrh@gmail.com>
2019-04-09 06:02:58 -0700

    new controller action to update end date and last seen school info interstitial

--- dashboard/app/controllers/user_school_infos_controller.rb
+++ dashboard/app/controllers/user_school_infos_controller.rb
@@ -1,6 +1,7 @@
 class UserSchoolInfosController < ApplicationController
+  # PATCH /api/v1/users_school_infos/<id>/update_last_confirmation_date
   def update
-    user_school_info = UserSchoolInfo.find([:id])
+    user_school_info = UserSchoolInfo.find(params[:id])
     result = user_school_info.update ({
       last_confirmation_date: DateTime.now
     })
@@ -9,5 +10,16 @@ class UserSchoolInfosController < ApplicationController
     else
       render json: user_school_info.errors, status: :unprocessable_entity
     end
+  end
+
+  # PATCH /api/v1/users_school_infos/<id>/update_end_date_last_seen_school_info_interstitial
+  def update_user_school_info
+    user_school_info = UserSchoolInfo.find(params[:id])
+
+    user_school_info.update!(end_date: DateTime.now)
+
+    current_user.update!(properties: {last_seen_school_info_interstitial: DateTime.now})
+
+    head :no_content
   end
 end
