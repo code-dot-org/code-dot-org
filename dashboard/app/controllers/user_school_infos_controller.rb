5b9e1c5e994da5f432cfeab5e324daf070882139
nkiru onwuneme <generalhrh@gmail.com>
2019-04-14 17:51:29 -0700

    method to update last confirmation date

--- dashboard/app/controllers/user_school_infos_controller.rb
+++ dashboard/app/controllers/user_school_infos_controller.rb
@@ -1,2 +1,14 @@
 class UserSchoolInfosController < ApplicationController
+  # PATCH /api/v1/users_school_infos/<id>/update_last_confirmation_date
+  def update_last_confirmation_date
+    user_school_info = UserSchoolInfo.find(params[:id])
+    result = user_school_info.update ({
+      last_confirmation_date: DateTime.now
+    })
+    if result
+      head :no_content
+    else
+      render json: user_school_info.errors, status: :unprocessable_entity
+    end
+  end
 end
