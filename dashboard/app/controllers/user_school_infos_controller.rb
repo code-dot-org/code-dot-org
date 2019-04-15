4c9994927bf49224383f911712a6a48eb1d5e88f
nkiru onwuneme <generalhrh@gmail.com>
2019-04-14 20:59:08 -0700

    add method to update end date and last seen school info interstitial

--- dashboard/app/controllers/user_school_infos_controller.rb
+++ dashboard/app/controllers/user_school_infos_controller.rb
@@ -22,4 +22,36 @@ class UserSchoolInfosController < ApplicationController
 
     head :no_content
   end
+
+  # PATCH /api/v1/users_school_infos/<id>/update_school_info_id
+  def update_school_info_history
+    ActiveRecord::Base.transaction do
+      if params[:add_new_school] == 1
+        school = School.create(new_school_params)
+        school.school_infos.create(school_info_params)
+      else
+        school = School.find(params[:school_id])
+      end
+
+      school_info = school.school_infos.order(created_at: :desc).first
+
+      # Always create a new user_school_info row
+      current_user.user_school_infos.create(user_school_info_params)
+
+      # Update the user(current_user) with the school_info_id
+      current_user.update school_info_id: school_info.id
+    end
+  end
+
+  private
+
+  def new_school_params
+    params.require(:school).permit(:name, :city, :state, :zip)
+  end
+
+  def school_info_params
+  end
+
+  def user_school_info_params
+  end
 end
