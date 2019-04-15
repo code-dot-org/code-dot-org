7feb0cc9c9a0a25ced1e917890fdef5d2534e5e5
nkiru onwuneme <generalhrh@gmail.com>
2019-04-14 17:55:34 -0700

    method to update end date and last seen interstitial

--- dashboard/app/controllers/user_school_infos_controller.rb
+++ dashboard/app/controllers/user_school_infos_controller.rb
@@ -11,4 +11,15 @@ class UserSchoolInfosController < ApplicationController
       render json: user_school_info.errors, status: :unprocessable_entity
     end
   end
+
+  # PATCH /api/v1/users_school_infos/<id>/update_end_date_last_seen_school_info_interstitial
+  def update_end_date_last_seen_school_info_interstitial
+    user_school_info = UserSchoolInfo.find(params[:id])
+
+    user_school_info.update!(end_date: DateTime.now)
+
+    current_user.update!(properties: {last_seen_school_info_interstitial: DateTime.now})
+
+    head :no_content
+  end
 end
