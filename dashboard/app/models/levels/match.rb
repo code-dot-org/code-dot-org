4e5d85f525c03a4899ef60e6890e41a598be4cae
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-04-16 17:02:54 -0700

    extract the various multi/match rendering options into their own methods

--- dashboard/app/models/levels/match.rb
+++ dashboard/app/models/levels/match.rb
@@ -74,4 +74,10 @@ ruby
   def icon
     'fa fa-list-ul'
   end
+
+  def localized_answer_text(answer)
+    # TODO: update this to use I18n.t with the smart option, instead of
+    # manually assembling the dotpath like this
+    data_t(type.underscore + '.' + name, answer, answer)
+  end
 end
