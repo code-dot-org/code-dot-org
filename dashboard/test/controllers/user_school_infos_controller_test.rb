ad9640eeb888e87c2fd190e647ff726371e5dedd
nkiru onwuneme <generalhrh@gmail.com>
2019-04-17 05:08:16 -0700

    add method and test to update school info id

--- dashboard/test/controllers/user_school_infos_controller_test.rb
+++ dashboard/test/controllers/user_school_infos_controller_test.rb
@@ -4,17 +4,28 @@ class UserSchoolInfosControllerTest < ActionDispatch::IntegrationTest
   test "last confirmation date in user school infos table is updated" do
     user_school_info = create :user_school_info
     sign_in user_school_info.user
-    patch "/api/v1/user_school_infos/#{user_school_info.user.id}/update_last_confirmation_date/", params: {
-      last_confirmation_date: DateTime.now
-    }
+    patch "/api/v1/user_school_infos/#{user_school_info.id}/update_last_confirmation_date/"
     assert_response :success
   end
 
-  test "end date and last seen school info interstitial is updated" do
+  test "404 is returned if id is invalid" do
+    patch "/api/v1/user_school_infos/#{-1}/update_last_confirmation_date/"
+    assert_response 404
+  end
+
+  test "end date is updated" do
     user_school_info = create :user_school_info
     sign_in user_school_info.user
-    patch "/api/v1/user_school_infos/#{user_school_info.user.id}/update_end_date", params: {
-      end_date: DateTime.now
+    patch "/api/v1/user_school_infos/#{user_school_info.id}/update_end_date"
+    assert_response :success
+  end
+
+  test "school_info_id is updated" do
+    user_school_info = create :user_school_info
+    sign_in user_school_info.user
+    patch "/api/v1/user_school_infos/#{user_school_info.id}/update_school_info_id", params: {
+      school: {name: 'C school', city: 'Hungtinton river', state: 'Caliii'},
+      school_info: {school_type: 'private', state: 'Jersey New', school_name: 'C school', country: 'US'}
     }
     assert_response :success
   end
