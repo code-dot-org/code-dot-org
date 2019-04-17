26559ba6625ea9b386d670e9b1c6aca5318738da
nkiru onwuneme <generalhrh@gmail.com>
2019-04-16 17:29:35 -0700

    remove parenthesis due to updating only one parameter

--- dashboard/app/controllers/user_school_infos_controller.rb
+++ dashboard/app/controllers/user_school_infos_controller.rb
@@ -2,9 +2,9 @@ class UserSchoolInfosController < ApplicationController
   # PATCH /api/v1/users_school_infos/<id>/update_last_confirmation_date
   def update_last_confirmation_date
     user_school_info = UserSchoolInfo.find(params[:id])
-    result = user_school_info.update ({
+    result = user_school_info.update (
       last_confirmation_date: DateTime.now
-    })
+    )
     if result
       head :no_content
     else
