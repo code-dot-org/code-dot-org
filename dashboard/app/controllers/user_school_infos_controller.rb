3596741499239a40ccb7413ed28cfef65a82e129
nkiru onwuneme <generalhrh@gmail.com>
2019-04-16 18:31:01 -0700

    add a callback to check if end date is updated before updating last seen school info interstitial

--- dashboard/app/controllers/user_school_infos_controller.rb
+++ dashboard/app/controllers/user_school_infos_controller.rb
@@ -2,9 +2,7 @@ class UserSchoolInfosController < ApplicationController
   # PATCH /api/v1/users_school_infos/<id>/update_last_confirmation_date
   def update_last_confirmation_date
     user_school_info = UserSchoolInfo.find(params[:id])
-    result = user_school_info.update (
-      last_confirmation_date: DateTime.now
-    )
+    result = user_school_info.update(last_confirmation_date: DateTime.now)
     if result
       head :no_content
     else
@@ -12,13 +10,11 @@ class UserSchoolInfosController < ApplicationController
     end
   end
 
-  # PATCH /api/v1/users_school_infos/<id>/update_end_date_last_seen_school_info_interstitial
-  def update_end_date_last_seen_school_info_interstitial
+  # PATCH /api/v1/users_school_infos/<id>/update_end_date
+  def update_end_date
     user_school_info = UserSchoolInfo.find(params[:id])
 
     user_school_info.update!(end_date: DateTime.now)
-
-    current_user.update!(properties: {last_seen_school_info_interstitial: DateTime.now})
 
     head :no_content
   end
