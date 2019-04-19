15037cd43162d75ad6a509292bda4712136b7d08
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-04-19 16:38:51 -0700

    render markdown sanitized, with the help of an AST

--- apps/src/templates/UnsafeRenderedMarkdown.jsx
+++ apps/src/templates/UnsafeRenderedMarkdown.jsx
@@ -3,29 +3,39 @@ import React from 'react';
 
 import Parser from '@code-dot-org/redactable-markdown';
 
+import remarkRehype from 'remark-rehype';
+import rehypeRaw from 'rehype-raw';
+import rehypeSanitize from 'rehype-sanitize';
+import rehypeReact from 'rehype-react';
+
 import expandableImages from './plugins/expandableImages';
 import xmlAsTopLevelBlock from './plugins/xmlAsTopLevelBlock';
-import stripStyles from './plugins/stripStyles';
 import externalLinks from './plugins/externalLinks';
 
-const commonPlugins = [xmlAsTopLevelBlock, expandableImages];
+const markdownToReact = Parser.create()
+  .getParser()
+  // include custom plugins
+  .use([xmlAsTopLevelBlock, expandableImages])
+  // convert markdown to an HTML Abstract Syntax Tree (HAST)
+  .use(remarkRehype, {
+    // include any raw HTML in the markdown as raw HTML nodes in the HAST
+    allowDangerousHTML: true
+  })
+  // parse the raw HTML nodes in the HAST to actual HAST nodes
+  .use(rehypeRaw)
+  // sanitize the HAST
+  .use(rehypeSanitize)
+  // convert the HAST to React
+  .use(rehypeReact, {
+    createElement: React.createElement
+  });
 
-const remarkParser = Parser.create();
-remarkParser.parser.use(commonPlugins);
-remarkParser.compilerPlugins.push(stripStyles);
-
-const extendedParser = Parser.create();
-extendedParser.parser.use(commonPlugins);
-extendedParser.parser.use(externalLinks, {links: 'all'});
-extendedParser.compilerPlugins.push(stripStyles);
+const markdownToReactExternalLinks = markdownToReact().use(externalLinks, {
+  links: 'all'
+});
 
 /**
- * Basic component for rendering a markdown string as HTML.
- *
- * Note that this component will render anything contained in the markdown into
- * the browser, including arbitrary html and script tags. It should be
- * considered unsafe to use for user-generated content until the markdown
- * renderer driving this component can be made safe.
+ * Basic component for rendering a markdown string as HTML, with sanitization
  */
 export default class UnsafeRenderedMarkdown extends React.Component {
   static propTypes = {
@@ -35,12 +45,19 @@ export default class UnsafeRenderedMarkdown extends React.Component {
 
   render() {
     const parser = this.props.openExternalLinksInNewTab
-      ? extendedParser
-      : remarkParser;
-    const processedMarkdown = parser.sourceToHtml(this.props.markdown);
+      ? markdownToReactExternalLinks
+      : markdownToReact;
 
-    /* eslint-disable react/no-danger */
-    return <div dangerouslySetInnerHTML={{__html: processedMarkdown}} />;
-    /* eslint-enable react/no-danger */
+    const rendered = parser.processSync(this.props.markdown).contents;
+
+    // rehype-react will only wrap the compiled markdown in a <div> tag
+    // if it needs to (ie, if there would otherwise be multiple elements
+    // returned). We prefer consistency over flexibility, so here we wrap
+    // the result in a div if it wasn't already
+    if (rendered && rendered.type === 'div') {
+      return rendered;
+    } else {
+      return <div>{rendered}</div>;
+    }
   }
 }
