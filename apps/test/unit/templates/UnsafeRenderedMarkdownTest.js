5244eef6e826dd0f987c6d122e3f14c870654860
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-04-19 16:38:51 -0700

    render markdown sanitized, with the help of an AST

--- apps/test/unit/templates/UnsafeRenderedMarkdownTest.js
+++ apps/test/unit/templates/UnsafeRenderedMarkdownTest.js
@@ -10,7 +10,7 @@ describe('UnsafeRenderedMarkdown', () => {
     );
 
     expect(wrapper.html()).to.equal(
-      '<div><p><strong>some</strong> <em>basic</em> <a href="markdown">inline</a></p>\n</div>'
+      '<div><p><strong>some</strong> <em>basic</em> <a href="markdown">inline</a></p></div>'
     );
   });
 
@@ -20,15 +20,16 @@ describe('UnsafeRenderedMarkdown', () => {
     );
 
     expect(basicWrapper.html()).to.equal(
-      '<div><p><strong>some</strong> <em>basic</em> <a href="markdown">inline</a></p>\n</div>'
+      '<div><p><strong>some</strong> <em>basic</em> <a href="markdown">inline</a></p></div>'
     );
 
     const advancedWrapper = shallow(
       <UnsafeRenderedMarkdown markdown="<table><thead><th>Some advanced html</th><th><strong>not</strong> usually supported by markdown</th></thead></table>" />
     );
 
+    // note the output has added <tr> tags as appropriate
     expect(advancedWrapper.html()).to.equal(
-      '<div><table><thead><th>Some advanced html</th><th><strong>not</strong> usually supported by markdown</th></thead></table>\n</div>'
+      '<div><table><thead><tr><th>Some advanced html</th><th><strong>not</strong> usually supported by markdown</th></tr></thead></table></div>'
     );
   });
 
@@ -38,7 +39,7 @@ describe('UnsafeRenderedMarkdown', () => {
     );
 
     expect(regularImage.html()).to.equal(
-      '<div><p><img src="http://example.com/img.jpg" alt="regular"></p>\n</div>'
+      '<div><p><img src="http://example.com/img.jpg" alt="regular"/></p></div>'
     );
 
     const expandableImage = shallow(
@@ -46,7 +47,7 @@ describe('UnsafeRenderedMarkdown', () => {
     );
 
     expect(expandableImage.html()).to.equal(
-      '<div><p><span data-url="http://example.com/img.jpg" class="expandable-image"></span></p>\n</div>'
+      '<div><p><span data-url="http://example.com/img.jpg" class="expandable-image"></span></p></div>'
     );
   });
 
@@ -56,7 +57,7 @@ describe('UnsafeRenderedMarkdown', () => {
     );
 
     expect(inlineXml.html()).to.equal(
-      "<div><p>Text with <xml><block type='xml'></block></xml> inline</p>\n</div>"
+      '<div><p>Text with <xml><block type="xml"></block></xml> inline</p></div>'
     );
 
     // Need to use markdown={} rather than markdown="" here so React doesn't
@@ -70,26 +71,64 @@ describe('UnsafeRenderedMarkdown', () => {
     );
 
     expect(blockXml.html()).to.equal(
-      "<div><p>Text with</p>\n<xml><block type='xml'></block></xml>\n<p>in its own block</p>\n</div>"
+      '<div><p>Text with</p>\n<xml><block type="xml"></block></xml>\n<p>in its own block</p></div>'
+    );
+  });
+
+  it('renders links normally by default', () => {
+    const externalLink = shallow(
+      <UnsafeRenderedMarkdown markdown="[external link](example.com)" />
+    );
+    expect(externalLink.html()).to.equal(
+      '<div><p><a href="example.com">external link</a></p></div>'
+    );
+
+    const internalLink = shallow(
+      <UnsafeRenderedMarkdown markdown="[internal link](code.org)" />
+    );
+    expect(internalLink.html()).to.equal(
+      '<div><p><a href="code.org">internal link</a></p></div>'
+    );
+  });
+
+  it('will open links in a new tab if specified', () => {
+    const externalLink = shallow(
+      <UnsafeRenderedMarkdown
+        openExternalLinksInNewTab
+        markdown="[external link](example.com)"
+      />
+    );
+    expect(externalLink.html()).to.equal(
+      '<div><p><a href="example.com" target="_blank" rel="noreferrer noopener">external link</a></p></div>'
+    );
+
+    const internalLink = shallow(
+      <UnsafeRenderedMarkdown
+        openExternalLinksInNewTab
+        markdown="[internal link](code.org)"
+      />
+    );
+    expect(internalLink.html()).to.equal(
+      '<div><p><a href="code.org" target="_blank" rel="noreferrer noopener">internal link</a></p></div>'
     );
   });
 
   // TODO let's make this test not be true anymore
   it('is vulnerable to JS injection', () => {
     const scriptTagInjection = shallow(
-      <UnsafeRenderedMarkdown markdown='<script type="text/javascript">alert("hello!")</script>' />
+      <UnsafeRenderedMarkdown markdown='<script type="text/javascript">alert(&#x22;hello!&#x22;)</script>' />
     );
 
     expect(scriptTagInjection.html()).to.equal(
-      '<div><script type="text/javascript">alert("hello!")</script>\n</div>'
+      '<div><script type="text/javascript">alert(&quot;hello!&quot;)</script></div>'
     );
 
     const inlineEventInjection = shallow(
-      <UnsafeRenderedMarkdown markdown='<div onmouseover="alert("hello!")"></div>' />
+      <UnsafeRenderedMarkdown markdown='<div onMouseOver="alert("hello!")"></div>' />
     );
 
     expect(inlineEventInjection.html()).to.equal(
-      '<div><div onmouseover="alert("hello!")"></div>\n</div>'
+      '<div><div onMouseOver="alert(&quot;hello!&quot;)"></div></div>'
     );
 
     const iframeInjection = shallow(
@@ -99,7 +138,7 @@ describe('UnsafeRenderedMarkdown', () => {
     );
 
     expect(iframeInjection.html()).to.equal(
-      '<div><iframe src="javascript:alert(\'hello\')"></iframe>\n</div>'
+      '<div><iframe src="javascript:alert(\'hello\')"></iframe></div>'
     );
   });
 });
