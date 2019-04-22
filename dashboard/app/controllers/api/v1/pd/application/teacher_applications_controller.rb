7268173d24c2c701aa7f94e9f92824361e412402
Brendan Reville <brendan@code.org>
2019-04-22 21:17:07 +1000

    PL: update getter function name

--- dashboard/app/controllers/api/v1/pd/application/teacher_applications_controller.rb
+++ dashboard/app/controllers/api/v1/pd/application/teacher_applications_controller.rb
@@ -12,7 +12,7 @@ module Api::V1::Pd::Application
     end
 
     def send_principal_approval
-      if @application.allow_sending_principal_email
+      if @application.allow_sending_principal_email?
         @application.queue_email :principal_approval, deliver_now: true
       end
       render json: {principal_approval: @application.principal_approval_state}
