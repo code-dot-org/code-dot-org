fe6bb831bcc6742025840054a99026c8f3e4472f
Bethany Connor <bethany@code.org>
2019-04-22 21:23:55 +0000

    Adding tests for HelpHeader

--- lib/test/test_helper.rb
+++ lib/test/test_helper.rb
@@ -6,3 +6,51 @@ if ENV['CIRCLECI']
   reporters << Minitest::Reporters::JUnitReporter.new("#{ENV['CIRCLE_TEST_REPORTS']}/lib")
 end
 Minitest::Reporters.use! reporters
+
+class Level
+  def report_bug_url(request)
+    "url"
+  end
+
+  def try(property)
+    false
+  end
+end
+
+class LevelAppLab
+  def report_bug_url(request)
+    "url"
+  end
+
+  def try(property)
+    property == :is_project_level
+  end
+
+  def game
+    "AppLab"
+  end
+end
+
+class LevelGameLab
+  def report_bug_url(request)
+    "url"
+  end
+
+  def try(property)
+    property == :is_project_level
+  end
+
+  def game
+    "GameLab"
+  end
+end
+
+class Game
+  def self.gamelab
+    "GameLab"
+  end
+
+  def self.applab
+    "AppLab"
+  end
+end
