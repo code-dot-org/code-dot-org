03493bf9afe3213c3f54c7f158efe6b16977e29f
Suresh Chanmugam <suresh@code.org>
2019-04-22 15:56:34 -0700

    Trying to fix the lock wait timeout when running all tests.

--- dashboard/test/integration/contact_rollups_test.rb
+++ dashboard/test/integration/contact_rollups_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 require 'cdo/contact_rollups'
 
 class ContactRollupsTest < ActiveSupport::TestCase
+  self.use_transactional_test_case = true
   def test_build_contact_rollups
     ContactRollups.build_contact_rollups
   end
