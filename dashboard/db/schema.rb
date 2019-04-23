f29c5edf57611d208225a050495115a30ebcc06f
Ben Brooks <ben@code.org>
2019-04-23 14:15:27 -0700

    Add question preamble to jotform processing

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190404195323) do
+ActiveRecord::Schema.define(version: 20190419233628) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -1336,6 +1336,29 @@ ActiveRecord::Schema.define(version: 20190404195323) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "emails"
+  end
+
+  create_table "survey_answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer  "user_id"
+    t.bigint   "form_id"
+    t.bigint   "submission_id"
+    t.string   "question_id"
+    t.text     "answer_value",  limit: 65535
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  create_table "survey_questions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.bigint   "form_id"
+    t.string   "question_id"
+    t.text     "preamble",       limit: 65535
+    t.text     "question_text",  limit: 65535
+    t.string   "answer_type"
+    t.text     "answer_options", limit: 65535
+    t.integer  "min_value"
+    t.integer  "max_value"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "survey_results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
