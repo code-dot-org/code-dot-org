85a9f72584248e933f95aedcee5951ae02a5ef72
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-04-23 11:54:37 -0700

    add logging

--- bin/oneoff/migrate_sponsored_users_to_multi_auth.rb
+++ bin/oneoff/migrate_sponsored_users_to_multi_auth.rb
@@ -7,6 +7,11 @@ require_relative '../../dashboard/config/environment'
 MAX_QUERY_EXECUTION_TIME = 1_200_000
 ActiveRecord::Base.connection.execute "SET SESSION MAX_EXECUTION_TIME=#{MAX_QUERY_EXECUTION_TIME};"
 
+# Conditionally enable verbose logging for better monitoring
+unless (ARGV & %w(-v --verbose)).empty?
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+end
+
 # The only effect that user.migrate_to_multi_auth has on sponsored users is to
 # update the "provider" field to "migrated", so for simplicity we do all those
 # directly with SQL
