938ba63b70dfab5438186dec308ee333c8d1ad07
David Bailey <davidsbailey@users.noreply.github.com>
2019-04-23 15:14:13 -0700

    test that student can view lockable script level after unlocking

--- dashboard/test/integration/stage_lock_test.rb
+++ dashboard/test/integration/stage_lock_test.rb
@@ -10,11 +10,11 @@ class StageLockTest < ActionDispatch::IntegrationTest
     Follower.create!(section_id: @section.id, student_user_id: @student.id, user: @teacher)
 
     @script = create :script
-    lockable_stage = create(:stage, script: @script, name: 'Lockable Stage', lockable: true)
+    @lockable_stage = create(:stage, script: @script, name: 'Lockable Stage', lockable: true)
     external = create(:external, name: 'markdown level')
-    @lockable_external_sl = create(:script_level, stage: lockable_stage, levels: [external])
-    level_group = create(:level_group, name: 'assessment 1')
-    @lockable_level_group_sl = create(:script_level, stage: lockable_stage, levels: [level_group], assessment: true)
+    @lockable_external_sl = create(:script_level, script: @script, stage: @lockable_stage, levels: [external])
+    @level_group = create(:level_group, name: 'assessment 1')
+    @lockable_level_group_sl = create(:script_level, script: @script, stage: @lockable_stage, levels: [@level_group], assessment: true)
   end
 
   test 'authorized teacher viewing lockable stage contents' do
@@ -41,6 +41,8 @@ class StageLockTest < ActionDispatch::IntegrationTest
 
   test 'student viewing lockable stage contents' do
     sign_in @student
+    assert @lockable_level_group_sl.locked?(@student)
+    assert @lockable_external_sl.locked?(@student)
 
     get build_script_level_path(@lockable_external_sl)
     assert_response :success
@@ -55,5 +57,24 @@ class StageLockTest < ActionDispatch::IntegrationTest
     assert_select '.level-group', 0
     assert_select "#locked-stage", 1
     assert_select "#locked-stage[data-hidden]", 0
+
+    # Unlock the stage.
+    UserLevel.update_lockable_state(
+      @student.id, @level_group.id, @script.id, false, false
+    )
+    refute @lockable_level_group_sl.locked?(@student)
+    refute @lockable_external_sl.locked?(@student)
+
+    get build_script_level_path(@lockable_external_sl)
+    assert_response :success
+    assert_includes response.body, 'lorem ipsum'
+    assert_select "#locked-stage", 0
+
+    get build_script_level_path(@lockable_level_group_sl)
+    assert_response :redirect
+    get URI.parse(@response.redirect_url).path
+    assert_response :success
+    assert_select '.level-group', 1
+    assert_select "#locked-stage", 0
   end
 end
