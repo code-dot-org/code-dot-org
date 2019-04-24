0bc932c8c37ba1c56b401d1b97d70d4c6433b551
Clare Constantine <clare@code.org>
2019-04-24 09:44:26 -0700

    no partners map to teachercon

--- dashboard/app/serializers/regional_partner_serializer.rb
+++ dashboard/app/serializers/regional_partner_serializer.rb
@@ -32,6 +32,6 @@ class RegionalPartnerSerializer < ActiveModel::Serializer
   attributes :id, :name, :group, :workshop_type
 
   def workshop_type
-    get_matching_teachercon(object) ? WORKSHOP_TYPES[:teachercon] : WORKSHOP_TYPES[:local_summer]
+    WORKSHOP_TYPES[:local_summer]
   end
 end
