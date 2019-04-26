ac147c1e0527be01e30814766c26764a2584af8c
Clare Constantine <clare@code.org>
2019-04-26 16:37:30 -0700

    add tests

--- dashboard/test/serializers/api/v1/pd/workshop_enrollment_serializer_test.rb
+++ dashboard/test/serializers/api/v1/pd/workshop_enrollment_serializer_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class Api::V1::Pd::WorkshopEnrollmentSerializerTest < ::ActionController::TestCase
+  include Pd::Application::ActiveApplicationModels
   test 'serialized workshop enrollment has expected attributes' do
     enrollment = create :pd_enrollment
     expected_attributes = [
@@ -8,7 +9,7 @@ class Api::V1::Pd::WorkshopEnrollmentSerializerTest < ::ActionController::TestCa
       :grades_teaching, :attended_csf_intro_workshop, :csf_course_experience,
       :csf_courses_planned, :csf_has_physical_curriculum_guide, :user_id, :attended,
       :pre_workshop_survey, :previous_courses, :replace_existing, :attendances,
-      :scholarship_status
+      :scholarship_status, :new_facilitator
     ]
 
     serialized = ::Api::V1::Pd::WorkshopEnrollmentSerializer.new(enrollment).attributes
@@ -22,5 +23,19 @@ class Api::V1::Pd::WorkshopEnrollmentSerializerTest < ::ActionController::TestCa
 
     serialized = ::Api::V1::Pd::WorkshopEnrollmentSerializer.new(workshop.enrollments.first).attributes
     assert_equal enrollment.attendances.count, serialized[:attendances]
+  end
+
+  test 'new_facilitator' do
+    summer_workshop = create :pd_workshop, :local_summer_workshop_upcoming
+    enrollment = create :pd_enrollment, :from_user, workshop: summer_workshop
+
+    serialized = ::Api::V1::Pd::WorkshopEnrollmentSerializer.new(summer_workshop.enrollments.first).attributes
+    refute serialized[:new_facilitator]
+
+    fac_app = create FACILITATOR_APPLICATION_FACTORY, user: enrollment.user
+    fac_app.update(status: 'accepted')
+
+    serialized = ::Api::V1::Pd::WorkshopEnrollmentSerializer.new(summer_workshop.enrollments.first).attributes
+    assert serialized[:new_facilitator]
   end
 end
