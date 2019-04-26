2d94eb4d6332825d720c89df9c468fe3f921b50d
Chris Pirich <chris@code.org>
2019-04-26 08:35:36 -0700

    update theme values to match design, create screens in current theme

--- apps/src/applab/designElements/screen.jsx
+++ apps/src/applab/designElements/screen.jsx
@@ -178,10 +178,11 @@ export default {
     element.style.position = 'absolute';
     element.style.zIndex = 0;
     if (experiments.isEnabled('applabThemes')) {
-      element.setAttribute(
-        'data-theme',
-        applabConstants.themeOptions[applabConstants.DEFAULT_THEME_INDEX]
+      // New screens are created with the same theme as is currently active
+      const currentTheme = elementLibrary.getCurrentTheme(
+        designMode.activeScreen()
       );
+      element.setAttribute('data-theme', currentTheme);
       elementLibrary.applyCurrentTheme(element, element);
     }
 
