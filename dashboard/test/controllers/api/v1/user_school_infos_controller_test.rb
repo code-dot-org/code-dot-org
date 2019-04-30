7333335060d31755d8376fa18eb4249c92aa1bcc
nkiru onwuneme <generalhrh@gmail.com>
2019-04-30 13:36:37 -0700

    update route for school id

--- dashboard/test/controllers/api/v1/user_school_infos_controller_test.rb
+++ dashboard/test/controllers/api/v1/user_school_infos_controller_test.rb
@@ -10,25 +10,43 @@ class UserSchoolInfosControllerTest < ActionDispatch::IntegrationTest
     user_school_info = create :user_school_info
     puts 'Before sign in'
     sign_in user_school_info.user
-    puts user_school_info.id
-    patch "/api/v1/user_school_infos/#{user_school_info.id}/update_last_confirmation_date/"
+    # puts user_school_info.inspect
+    patch "/api/v1/user_school_infos/#{user_school_info.id}/update_last_confirmation_date"
     assert_response :success
 
     assert user_school_info.last_confirmation_date.to_datetime, DateTime.current
   end
 
-  test "404 is returned if id is invalid" do
-    patch "/api/v1/user_school_infos/#{-1}/update_last_confirmation_date/"
+  test "last confirmation date is not updated for a user that is not signed in" do
+    patch "/api/v1/user_school_infos/-1/update_last_confirmation_date"
     assert_response 404
   end
 
-  test "end date is updated" do
+  test "update last confirmation will 404 if user id does not exist" do
+    user_school_info = create :user_school_info
+    sign_in user_school_info.user
+    patch "/api/v1/user_school_infos/-1/update_last_confirmation_date"
+    assert_response 404
+  end
+
+  test 'update last confirmation date will 403 if given a user id other than the person logged in' do
+    user_school_info1 = create :user_school_info
+    user_school_info2 = create :user_school_info
+    sign_in user_school_info2.user
+    patch "/api/v1/user_school_infos/#{user_school_info1.id}/update_last_confirmation_date"
+    assert_response 401
+  end
+
+  test "end_date and last_seen_school_info_interstitial is updated" do
     user_school_info = create :user_school_info
     sign_in user_school_info.user
     patch "/api/v1/user_school_infos/#{user_school_info.id}/update_end_date"
+    updated_user_school_info = UserSchoolInfo.find(user_school_info.id)
+    #puts updated_user_school_info.inspect
     assert_response :success
 
-    assert user_school_info.end_date.to_datetime, Date.current
+    assert updated_user_school_info.end_date.to_datetime, Date.current
+    assert updated_user_school_info.user.last_seen_school_info_interstitial.to_datetime, Date.current
   end
 
   test "school_info_id is updated" do
@@ -38,38 +56,26 @@ class UserSchoolInfosControllerTest < ActionDispatch::IntegrationTest
       school: {name: 'C school', city: 'Hungtinton river', state: 'Caliii'},
       school_info: {school_type: 'private', state: 'Jersey New', school_name: 'C school', country: 'US'}
     }
+    #updated_user_school_info = UserSchoolInfo.find(user_school_info.id)
+    new_user_school_info = UserSchoolInfo.last
+
+    assert_response :success
+
+    assert_difference UserSchoolInfo.count, 1
+    assert_not_equal new_user_school_info.id, user_school_info.id
+    assert new_user_school_info.school_info_id, user_school_info.school_info_id
+  end
+
+  # tests validating that non-whitelisted params are ignored
+  test "non-whitelisted params are ignored when school_info_id is updated" do
+    user_school_info = create :user_school_info
+    sign_in user_school_info.user
+    patch "/api/v1/user_school_infos/#{user_school_info.id}/update_school_info_id", params: {
+      school: {name: 'C school', city: 'Hungtinton river', state: 'Caliii', phone: '123'},
+      school_info: {school_type: 'private', state: 'Jersey New', school_name: 'C school', country: 'US'}
+    }
     assert_response :success
   end
-
-  # signed out user can't update UserSchoolInfo
-  test "last confirmation date is not updated for a user that is not signed in" do
-    patch :update_last_confirmation_date, params: {user_id: 234}
-    assert_response 403
-  end
-
-  test "update last confirmation will 403 if user id does not exist" do
-    sign_in user_school_info.user
-    patch :update_last_confirmation_date, params: {user_id: '-1'}
-    assert_response 403
-  end
-
-  # signed in user can only update UserSchoolInfo that belongs to them
-  test 'update last confirmation date will 403 if given a user id other than the person logged in' do
-    sign_in(@user)
-    patch :update_last_confirmation_date, params: {user_id: '456'}
-    assert_response 403
-  end
-
-  # # tests validating that non-whitelisted params are ignored
-  # test "non-whitelisted params are ignored when school_info_id is updated" do
-  #   user_school_info = create :user_school_info
-  #   sign_in user_school_info.user
-  #   patch "/api/v1/user_school_infos/#{user_school_info.id}/update_school_info_id", params: {
-  #     school: {name: 'C school', city: 'Hungtinton river', state: 'Caliii'},
-  #     school_info: {school_type: 'private', state: 'Jersey New', school_name: 'C school', country: 'US'}
-  #   }
-  #   assert_response :success
-  # end
 
   # these tests should all also verify that the actual data has been updated,
 
