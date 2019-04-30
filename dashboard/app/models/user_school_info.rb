f34f96a9c781e99e47128d5033069559d8beb877
nkiru onwuneme <generalhrh@gmail.com>
2019-04-30 15:44:31 -0700

    remove unused code

--- dashboard/app/models/user_school_info.rb
+++ dashboard/app/models/user_school_info.rb
@@ -17,12 +17,4 @@ class UserSchoolInfo < ApplicationRecord
 
   belongs_to :user
   belongs_to :school_info
-
-  #solution - 2 (check end date was updated before updating last seen school info interstitial)
-
-  # after_update :update_last_seen_school_info_interstitial, if: :end_date_changed?
-
-  # def update_last_seen_school_info_interstitial
-  #   user.update!(properties: {last_seen_school_info_interstitial: DateTime.now})
-  # end
 end
