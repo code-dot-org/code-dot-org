7287dcd4b9c5817956876ea21673cb90d220f953
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-05-03 13:51:41 -0700

    stop logging successes; it was unnecessarily noisy

--- bin/oneoff/migrate_users_to_multi_auth.rb
+++ bin/oneoff/migrate_users_to_multi_auth.rb
@@ -14,13 +14,8 @@ def migrate_batches(limit=nil)
   slice_count = 0
   puts "Migrating #{limit || 'all'} slice(s)"
   unmigrated_users.find_in_batches(batch_size: SLICE_SIZE) do |slice|
-    puts "\tSLICE_COUNT: #{slice_count + 1} of ~#{unmigrated_users_count / SLICE_SIZE}..."
-    successes = 0
-    slice.each do |user|
-      user.migrate_to_multi_auth
-      successes += 1 if user.valid? && user.provider == User::PROVIDER_MIGRATED
-    end
-    puts "\t\tsuccessful migrations: #{successes} / #{SLICE_SIZE}"
+    puts "\tSLICE_COUNT: #{slice_count + 1} of #{limit || "~#{unmigrated_users_count / SLICE_SIZE}"}..."
+    slice.each(&:migrate_to_multi_auth)
     slice_count += 1
 
     if limit && slice_count >= limit
