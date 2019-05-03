ed824b1b15cc3e5fbf8bfa18e6b31cc41f94d8ea
nkiru onwuneme <generalhrh@gmail.com>
2019-05-03 10:33:26 -0700

    Refactor code to use can can

--- dashboard/app/controllers/api/v1/user_school_infos_controller.rb
+++ dashboard/app/controllers/api/v1/user_school_infos_controller.rb
@@ -1,61 +1,45 @@
 class Api::V1::UserSchoolInfosController < ApplicationController
   before_action :authenticate_user!
   before_action :load_user_school_info
-
-  skip_before_action :verify_authenticity_token
+  load_and_authorize_resource
 
   # PATCH /api/v1/users_school_infos/<id>/update_last_confirmation_date
   def update_last_confirmation_date
-    if @user_school_info.user.id == current_user.id
-      if @user_school_info.update(last_confirmation_date: DateTime.now)
-        head :no_content
-      else
-        render json: @user_school_info.errors, status: :unprocessable_entity
-      end
+    if @user_school_info.update(last_confirmation_date: DateTime.now)
+      head :no_content
     else
-      head :forbidden
+      render json: @user_school_info.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH /api/v1/users_school_infos/<id>/update_end_date
   def update_end_date
-    if @user_school_info.user.id == current_user.id
-      if @user_school_info.update!(end_date: DateTime.now)
-        if @user_school_info.user.update!(properties: {last_seen_school_info_interstitial: DateTime.now})
-          head :no_content
-        else
-          render json: @user_school_info.errors, status: :unprocessable_entity
-        end
-      end
-    else
-      head :forbidden
-    end
+    @user_school_info.update!(end_date: DateTime.now, last_confirmation_date: DateTime.now)
+    @user_school_info.user.update!(properties: {last_seen_school_info_interstitial: DateTime.now})
+
+    head :no_content
   end
 
   # PATCH /api/v1/user_school_infos/<id>/update_school_info_id
   def update_school_info_id
-    if @user_school_info.user.id == current_user.id
-      ActiveRecord::Base.transaction do
-        school = @user_school_info.school_info.school
-        unless school
-          school = School.create!(new_school_params)
-          school.school_info.create!(school_info_params)
-        end
-
-        school_info = school.school_info.order(created_at: :desc).first
-
-        user = @user_school_info.user
-
-        user.user_school_infos.create!({school_info_id: school_info.id, last_confirmation_date: DateTime.now, start_date: user.created_at})
-
-        if user.update({school_info_id: school_info.id})
-          head :no_content
-        else
-          render json: user.errors, status: :unprocessable_entity
-        end
+    ActiveRecord::Base.transaction do
+      school = @user_school_info.school_info.school
+      unless school
+        school = School.create!(new_school_params)
+        school.school_info.create!(school_info_params)
       end
-    else
-      head :forbidden
+
+      school_info = school.school_info.order(created_at: :desc).first
+
+      user = @user_school_info.user
+
+      user.user_school_infos.create!({school_info_id: school_info.id, last_confirmation_date: DateTime.now, start_date: user.created_at})
+
+      if user.update({school_info_id: school_info.id})
+        head :no_content
+      else
+        render json: user.errors, status: :unprocessable_entity
+      end
     end
   end
 
