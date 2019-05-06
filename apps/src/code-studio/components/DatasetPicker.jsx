d67000ab1fb8d36a623e27adb3c2c083534a108b
Anjali <anjali@code.org>
2019-05-06 10:57:17 -0700

    Add click handler to Choose button. Imports csv to a firebase table and populates the getList parameter
    style dataset name

--- apps/src/code-studio/components/DatasetPicker.jsx
+++ apps/src/code-studio/components/DatasetPicker.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import color from '../../util/color';
 import i18n from '@cdo/locale';
 import DatasetListEntry from './DatasetListEntry';
+import FirebaseStorage from '../../storage/firebaseStorage';
 
 const styles = {
   root: {
@@ -26,20 +27,44 @@ export default class DatasetPicker extends React.Component {
   static propTypes = {
     assetChosen: PropTypes.func
   };
+
+  chooseAsset = (name, url) => {
+    console.log('here!!');
+    FirebaseStorage.createTable(
+      name,
+      () => console.log('createTable onSuccess'),
+      () => console.log('createTable onError')
+    );
+    FirebaseStorage.importCsv(
+      name,
+      url,
+      () => console.log('importCsv onSuccess'),
+      () => console.log('importCsv onError')
+    );
+    this.props.assetChosen(name);
+  };
+
   render() {
     let datasets = [
       {name: 'Dogs', description: 'something about dogs', url: 'dogUrl'},
-      {name: 'Words', description: 'something something words', url: 'wordUrl'}
+      {
+        name: 'Words',
+        description: 'something something words',
+        url:
+          'word,partOfSpeech,Frequency,Rank\nthe,article,22038615,1\nbe,verb,12545825,2\nand,conjunction,10741073,3'
+      }
     ];
 
     const datasetEntries = datasets.map(d => {
+      const choose =
+        this.chooseAsset && this.chooseAsset.bind(this, d.name, d.url);
       return (
         <DatasetListEntry
           key={d.name}
           name={d.name}
           description={d.description}
           url={d.url}
-          assetChosen={this.props.assetChosen}
+          onChoose={choose}
         />
       );
     });
