f6794e0c3a3510f8b8b9edbdc5149267c91699a2
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-05-07 16:34:43 -0700

    extract Script Name and Project Updated At components from jquery to react

--- apps/src/code-studio/headerRedux.js
+++ apps/src/code-studio/headerRedux.js
@@ -1,18 +1,33 @@
 const SHOW_MINIMAL_PROJECT_HEADER = 'header/SHOW_MINIMAL_PROJECT_HEADER';
+const SHOW_PROJECT_BACKED_HEADER = 'header/SHOW_PROJECT_BACKED_HEADER';
+const SHOW_PROJECT_UPDATED_AT = 'header/SHOW_PROJECT_UPDATED_AT';
+const SET_PROJECT_UPDATED_AT = 'header/SET_PROJECT_UPDATED_AT';
 const ENABLE_LEVEL_BUILDER_SAVE_BUTTON =
   'header/ENABLE_LEVEL_BUILDER_SAVE_BUTTON';
 
 const initialState = {
   showMinimalProjectHeader: false,
+  showProjectBackedHeader: false,
+  showProjectUpdatedAt: false,
+  projectUpdatedAt: undefined,
   showLevelBuilderSaveButton: false,
   getLevelBuilderChanges: undefined
 };
 
 export default (state = initialState, action) => {
+  // TODO: are the project-backed header and minimal project header mutually
+  // exclusive?
   if (action.type === SHOW_MINIMAL_PROJECT_HEADER) {
     return {
       ...state,
       showMinimalProjectHeader: true
+    };
+  }
+
+  if (action.type === SHOW_PROJECT_BACKED_HEADER) {
+    return {
+      ...state,
+      showProjectBackedHeader: true
     };
   }
 
@@ -24,6 +39,20 @@ export default (state = initialState, action) => {
     };
   }
 
+  if (action.type === SHOW_PROJECT_UPDATED_AT) {
+    return {
+      ...state,
+      showProjectUpdatedAt: true
+    };
+  }
+
+  if (action.type === SET_PROJECT_UPDATED_AT) {
+    return {
+      ...state,
+      projectUpdatedAt: action.updatedAt
+    };
+  }
+
   return state;
 };
 
@@ -31,7 +60,20 @@ export const showMinimalProjectHeader = () => ({
   type: SHOW_MINIMAL_PROJECT_HEADER
 });
 
+export const showProjectBackedHeader = () => ({
+  type: SHOW_PROJECT_BACKED_HEADER
+});
+
 export const showLevelBuilderSaveButton = getChanges => ({
   type: ENABLE_LEVEL_BUILDER_SAVE_BUTTON,
   getChanges
+});
+
+export const showProjectUpdatedAt = () => ({
+  type: SHOW_PROJECT_UPDATED_AT
+});
+
+export const setProjectUpdatedAt = updatedAt => ({
+  type: SET_PROJECT_UPDATED_AT,
+  updatedAt
 });
