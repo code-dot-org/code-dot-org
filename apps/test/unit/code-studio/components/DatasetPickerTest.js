ed545b8206110cdd4ac756aa553db2d5c669d39b
Anjali <anjali@code.org>
2019-05-07 16:10:53 -0700

    Add test for chooseAsset

--- apps/test/unit/code-studio/components/DatasetPickerTest.js
+++ apps/test/unit/code-studio/components/DatasetPickerTest.js
@@ -1,6 +1,8 @@
-import {expect} from '../../../util/reconfiguredChai';
+import {assert, expect} from '../../../util/reconfiguredChai';
 import React from 'react';
 import {mount} from 'enzyme';
+import sinon from 'sinon';
+import FirebaseStorage from '@cdo/apps/storage/firebaseStorage';
 import DatasetPicker from '@cdo/apps/code-studio/components/DatasetPicker';
 import datasetLibrary from '@cdo/apps/code-studio/datasetLibrary.json';
 
@@ -14,5 +16,23 @@ describe('DatasetPicker', () => {
     expect(wrapper.find('DatasetListEntry')).to.have.lengthOf(
       datasetLibrary.datasets.length
     );
+  });
+
+  it('calls Firebase on chooseAsset', () => {
+    const wrapper = mount(<DatasetPicker {...defaultProps} />);
+    var xhr = sinon.useFakeXMLHttpRequest();
+    var requests = [];
+    xhr.onCreate = function(xhr) {
+      requests.push(xhr);
+    };
+    var createStub = sinon.stub(FirebaseStorage, 'createTable');
+    var importStub = sinon.stub(FirebaseStorage, 'importCsv');
+
+    wrapper.instance().chooseAsset('name', 'url');
+    requests[0].respond(200);
+
+    assert.isTrue(createStub.called);
+    assert.isTrue(importStub.called);
+    xhr.restore();
   });
 });
