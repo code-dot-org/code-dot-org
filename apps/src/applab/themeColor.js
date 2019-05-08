8b16825640d01d67ccc55b95eddac7baf324f440
Chris Pirich <chris@code.org>
2019-05-08 10:21:07 -0700

    fix area51 dropdown background color to avoid transparent

--- apps/src/applab/themeColor.js
+++ apps/src/applab/themeColor.js
@@ -105,7 +105,7 @@ module.exports = {
     lemonade: '#5BB7E7',
     forest: '#A46C15',
     watermelon: '#FF8660',
-    area51: 'rgba(0, 0, 0, 0)',
+    area51: '#362F49',
     polar: '#EFFAFF',
     glowInTheDark: '#D9FFD9',
     bubblegum: '#FF7CD0',
