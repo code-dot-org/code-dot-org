9838d5bcc7d903f6f20c31ed3eb489b79f54744b
David Bailey <davidsbailey@users.noreply.github.com>
2019-05-08 16:06:30 -0700

    initialize viewAs redux

--- apps/src/sites/studio/pages/levels/_teacher_panel.js
+++ apps/src/sites/studio/pages/levels/_teacher_panel.js
@@ -1,10 +1,23 @@
 import $ from 'jquery';
+import {ViewType, setViewType} from '@cdo/apps/code-studio/viewAsRedux';
+import queryString from 'query-string';
+import {getStore} from '@cdo/apps/redux';
 
 $(document).ready(initPage);
 
 function initPage() {
+  const store = getStore();
+
+  initViewAs(store);
+
   const teacherPanel = document.getElementById('level-teacher-panel');
   if (teacherPanel) {
     console.log('found #level-teacher-panel');
   }
+}
+
+function initViewAs(store) {
+  const query = queryString.parse(location.search);
+  const initialViewAs = query.viewAs || ViewType.Teacher;
+  store.dispatch(setViewType(initialViewAs));
 }
