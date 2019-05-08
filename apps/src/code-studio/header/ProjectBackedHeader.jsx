d82ea6dc49a3d9e668a73b324e8a487573a3f595
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-05-07 17:55:20 -0700

    extract all implementations of the Share button to a single component

--- apps/src/code-studio/header/ProjectBackedHeader.jsx
+++ apps/src/code-studio/header/ProjectBackedHeader.jsx
@@ -1,26 +1,16 @@
-/* globals dashboard */
-
 import React from 'react';
 
+import ProjectShare from './ProjectShare';
 import ProjectRemix from './ProjectRemix';
 
 // Project header for script levels that are backed by a project. Shows a Share
 // and Remix button, and should be used with the version of ScriptName that
 // places a last_modified time below the stage name
 export default class ProjectBackedHeader extends React.Component {
-  shareProject = () => {
-    // TODO
-  };
-
   render() {
     return (
       <div>
-        <div
-          className="project_share header_button header_button_light"
-          onClick={this.shareProject}
-        >
-          {dashboard.i18n.t('project.share')}
-        </div>
+        <ProjectShare />
         <ProjectRemix lightStyle />
       </div>
     );
