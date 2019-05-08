35bd3edae60ff261d6f26fb8bd862e962caede71
David Bailey <davidsbailey@users.noreply.github.com>
2019-05-08 16:22:56 -0700

    render TeacherContentToggle

--- apps/src/sites/studio/pages/levels/_teacher_panel.js
+++ apps/src/sites/studio/pages/levels/_teacher_panel.js
@@ -1,7 +1,13 @@
+/* global appOptions */
+
 import $ from 'jquery';
 import {ViewType, setViewType} from '@cdo/apps/code-studio/viewAsRedux';
 import queryString from 'query-string';
 import {getStore} from '@cdo/apps/redux';
+import React from 'react';
+import {Provider} from 'react-redux';
+import ReactDOM from 'react-dom';
+import TeacherContentToggle from '@cdo/apps/code-studio/components/TeacherContentToggle';
 
 $(document).ready(initPage);
 
@@ -9,6 +15,7 @@ function initPage() {
   const store = getStore();
 
   initViewAs(store);
+  renderTeacherContentToggle(store);
 
   const teacherPanel = document.getElementById('level-teacher-panel');
   if (teacherPanel) {
@@ -20,4 +27,18 @@ function initViewAs(store) {
   const query = queryString.parse(location.search);
   const initialViewAs = query.viewAs || ViewType.Teacher;
   store.dispatch(setViewType(initialViewAs));
+}
+
+function renderTeacherContentToggle(store) {
+  const levelContent = $('#level-body');
+  const element = $('<div/>')
+    .css('height', '100%')
+    .insertAfter(levelContent)[0];
+
+  ReactDOM.render(
+    <Provider store={getStore()}>
+      <TeacherContentToggle isBlocklyOrDroplet={!!appOptions.app} />
+    </Provider>,
+    element
+  );
 }
