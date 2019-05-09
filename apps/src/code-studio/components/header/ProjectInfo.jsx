ce07ad682ad3bf600118d3eb61648ef39bd3b476
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-05-09 16:59:10 -0700

    reconfigure the various headers to be mutually exclusive

--- apps/src/code-studio/components/header/ProjectInfo.jsx
+++ apps/src/code-studio/components/header/ProjectInfo.jsx
@@ -7,29 +7,30 @@ import MinimalProjectHeader from './MinimalProjectHeader';
 import ProjectBackedHeader from './ProjectBackedHeader';
 import LevelBuilderSaveButton from './LevelBuilderSaveButton';
 
+import {possibleHeaders} from '../../headerRedux';
+
+const headerComponents = {
+  [possibleHeaders.project]: ProjectHeader,
+  [possibleHeaders.minimalProject]: MinimalProjectHeader,
+  [possibleHeaders.projectBacked]: ProjectBackedHeader,
+  [possibleHeaders.levelBuilderSave]: LevelBuilderSaveButton
+};
+
 class ProjectInfo extends React.Component {
   static propTypes = {
-    showProjectHeader: PropTypes.bool,
-    showMinimalProjectHeader: PropTypes.bool,
-    showProjectBackedHeader: PropTypes.bool,
-    showLevelBuilderSaveButton: PropTypes.bool
+    currentHeader: PropTypes.oneOf(Object.values(possibleHeaders))
   };
 
   render() {
-    return (
-      <div>
-        {this.props.showProjectHeader && <ProjectHeader />}
-        {this.props.showMinimalProjectHeader && <MinimalProjectHeader />}
-        {this.props.showProjectBackedHeader && <ProjectBackedHeader />}
-        {this.props.showLevelBuilderSaveButton && <LevelBuilderSaveButton />}
-      </div>
-    );
+    if (!this.props.currentHeader) {
+      return null;
+    }
+
+    const HeaderComponent = headerComponents[this.props.currentHeader];
+    return <HeaderComponent />;
   }
 }
 
 export default connect(state => ({
-  showProjectHeader: state.header.showProjectHeader,
-  showMinimalProjectHeader: state.header.showMinimalProjectHeader,
-  showProjectBackedHeader: state.header.showProjectBackedHeader,
-  showLevelBuilderSaveButton: state.header.showLevelBuilderSaveButton
+  currentHeader: state.header.currentHeader
 }))(ProjectInfo);
