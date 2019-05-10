bfb30d726be35f54f27e795b11d7216f03c77b25
David Bailey <davidsbailey@users.noreply.github.com>
2019-05-10 13:29:03 -0700

    remove useless section selection ui test step

--- dashboard/test/ui/features/step_definitions/steps.rb
+++ dashboard/test/ui/features/step_definitions/steps.rb
@@ -1496,15 +1496,6 @@ Then /^I show stage answers for students$/ do
   @browser.execute_script('$(".modal-body button:contains(Save)").click()')
 end
 
-Then /^I select the first section$/ do
-  steps %{
-    And I wait to see ".uitest-sectionselect"
-  }
-  @browser.execute_script(
-    "window.location.search = 'section_id=' + $('.content select').children().eq(1).val();"
-  )
-end
-
 def refute_bad_gateway_or_site_unreachable
   first_header_text = @browser.execute_script("var el = document.getElementsByTagName('h1')[0]; return el && el.textContent;")
   expect(first_header_text).not_to end_with('Bad Gateway')
