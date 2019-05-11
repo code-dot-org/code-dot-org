ab2784ecf75f0635a2f1d23033aa08e770d0369d
nkiru onwuneme <generalhrh@gmail.com>
2019-05-10 20:22:15 -0700

    add comment and change method name'

--- dashboard/app/models/school_info.rb
+++ dashboard/app/models/school_info.rb
@@ -291,7 +291,9 @@ class SchoolInfo < ActiveRecord::Base
     school_type.eql? SCHOOL_TYPE_CHARTER
   end
 
-  def completely_filled_out?
+  # Check if school info is complete to stop showing
+  # school info interstitial
+  def school_info_complete?
     school_type.present? && school_name.present? && country.eql?('US')
   end
 end
