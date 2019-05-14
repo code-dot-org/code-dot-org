5096d3b497e4df18ff938edc24e5338132e2cbd3
Erin Bond <erin.bond@code.org>
2019-05-13 21:28:06 -0700

    log tracking pixel to Redshift

--- dashboard/app/controllers/tracking_pixel_controller.rb
+++ dashboard/app/controllers/tracking_pixel_controller.rb
@@ -1,5 +1,22 @@
+require 'cdo/firehose'
+
 class TrackingPixelController < ApplicationController
   def index
+    prevent_caching
+    curriculum_page = URI.unescape(params[:from]).split('/').reverse.drop(1).reverse.join('/')
+    user_id = current_user&.id || nil
+
+    FirehoseClient.instance.put_record(
+      study: 'curriculum-builder-page-views',
+      study_group: 'v0',
+      event: 'curriculum-builder-page-view',
+      data_json: {
+        curriculumBuilderUrl: curriculum_page,
+        unplugged: curriculum_page.include?("unplugged"),
+        userId: user_id
+      }.to_json
+    )
+
     send_file pegasus_dir('sites.v3/code.org/public/images/1x1.png'), type: 'image/png'
   end
 end
