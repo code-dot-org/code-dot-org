4c9c6dfd579cc0d082d890576c85e64a6fda1715
Chris Pirich <chris@code.org>
2019-05-15 10:10:22 -0700

    honor data_tables from project template level

--- dashboard/app/models/levels/blockly.rb
+++ dashboard/app/models/levels/blockly.rb
@@ -360,6 +360,7 @@ class Blockly < Level
 
       if is_a? Applab
         level_prop['startHtml'] = try(:project_template_level).try(:start_html) || start_html
+        level_prop['dataTables'] = try(:project_template_level).try(:data_tables) || data_tables
         level_prop['name'] = name
       end
 
