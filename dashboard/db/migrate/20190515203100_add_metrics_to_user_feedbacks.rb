a0d5e3a9c05f90158b0f5b42c4c1b6437baf2cff
Maddie Kasula <madelynkasula@gmail.com>
2019-05-15 15:29:19 -0700

    Rename columns to student_first_visited_at and student_last_visited_at

--- dashboard/db/migrate/20190515203100_add_metrics_to_user_feedbacks.rb
+++ dashboard/db/migrate/20190515203100_add_metrics_to_user_feedbacks.rb
@@ -1,7 +1,7 @@
 class AddMetricsToUserFeedbacks < ActiveRecord::Migration[5.0]
   def change
     add_column :teacher_feedbacks, :student_visit_count, :integer
-    add_column :teacher_feedbacks, :student_initial_visited_at, :timestamp
-    add_column :teacher_feedbacks, :student_most_recent_visited_at, :timestamp
+    add_column :teacher_feedbacks, :student_first_visited_at, :timestamp
+    add_column :teacher_feedbacks, :student_last_visited_at, :timestamp
   end
 end
