14e84f233fd683c6b02e8f1282082da5b01a498d
Brendan Reville <brendan@code.org>
2019-05-16 09:22:33 -0700

    PL: revert small change that's not needed yet

--- dashboard/app/models/pd/post_course_survey.rb
+++ dashboard/app/models/pd/post_course_survey.rb
@@ -25,8 +25,7 @@ module Pd
     include JotFormBackedForm
 
     VALID_YEARS = [
-      YEAR_18_19 = '2018-2019'.freeze,
-      YEAR_19_20 = '2019-2020'.freeze
+      YEAR_18_19 = '2018-2019'.freeze
     ].freeze
 
     VALID_COURSES = [
