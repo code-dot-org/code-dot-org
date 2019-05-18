92a0ab0e4aee8f4a4f2180547fb39ba11213ea5e
Erin Bond <erin.bond@code.org>
2019-05-17 17:48:14 -0700

    firehose tests and handle cases without from param

--- dashboard/app/controllers/curriculum_tracking_pixel_controller.rb
+++ dashboard/app/controllers/curriculum_tracking_pixel_controller.rb
@@ -1,21 +1,26 @@
 require 'cdo/firehose'
 
 class CurriculumTrackingPixelController < ApplicationController
+  STUDY_NAME = 'curriculum-builder-page-views'
+  EVENT_NAME = 'curriculum-builder-page-view'
   def index
     prevent_caching
-    curriculum_page = URI.unescape(params[:from])
+    if params[:from]
+      curriculum_page = URI.unescape(params[:from])
+    end
     user_id = current_user&.id
 
-    FirehoseClient.instance.put_record(
-      study: 'curriculum-builder-page-views',
-      study_group: 'v0',
-      event: 'curriculum-builder-page-view',
-      data_json: {
-        curriculumBuilderUrl: curriculum_page,
-        unplugged: curriculum_page.include?("unplugged"),
-        userId: user_id
-      }.to_json
-    )
+    if curriculum_page
+      FirehoseClient.instance.put_record(
+        study: STUDY_NAME,
+        study_group: 'v0',
+        event: EVENT_NAME,
+        data_json: {
+          curriculumBuilderUrl: curriculum_page,
+          userId: user_id
+        }.to_json
+      )
+    end
 
     send_file dashboard_dir('app/assets/images/1x1.png'), type: 'image/png'
   end
