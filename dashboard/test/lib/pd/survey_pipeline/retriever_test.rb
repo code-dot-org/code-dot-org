bc539b7b2ff599b9fe7729b41be1fbf158b75251
Ha Nguyen <ha@code.org>
2019-05-24 15:09:46 -0700

    Fix PR comments, add more tests

--- dashboard/test/lib/pd/survey_pipeline/retriever_test.rb
+++ dashboard/test/lib/pd/survey_pipeline/retriever_test.rb
@@ -6,7 +6,7 @@ module Pd::SurveyPipeline
     include Pd::WorkshopConstants
 
     setup_all do
-      @form_ids = [11_000_000_000_000, 11_000_000_000_001]
+      @workshop_form_ids = [11_000_000_000_000, 11_000_000_000_001]
       @facilitator_form_ids = [22_000_000_000_000, 22_000_000_000_001]
       @workshops = create_list :pd_workshop, 2,
         course: COURSE_CSF, subject: SUBJECT_CSF_201, num_sessions: 2
@@ -16,20 +16,20 @@ module Pd::SurveyPipeline
       days = [0, 1]
 
       # Create workshop daily surveys
-      @workshop_surveys = @workshops.product(teachers, days, @form_ids).each do |ws, teacher, day, form|
+      ws_combinations = @workshops.product(teachers, days, @workshop_form_ids)
+      @workshop_submissions = ws_combinations.each do |ws, teacher, day, form|
         create :pd_workshop_daily_survey, pd_workshop: ws, user: teacher, day: day, form_id: form
       end
 
       # Create facilitator daily surveys
-      combinations = @workshops.product(teachers, days, @facilitator_form_ids, facilitators)
-
-      @facilitator_surveys = combinations.each do |ws, teacher, day, form, facilitator|
+      f_combinations = @workshops.product(teachers, days, @facilitator_form_ids, facilitators)
+      @facilitator_submissions = f_combinations.each do |ws, teacher, day, form, facilitator|
         create :pd_workshop_facilitator_daily_survey, pd_workshop: ws, user: teacher,
           day: day, form_id: form, pd_session: ws.sessions[day], facilitator: facilitator
       end
 
       # Create survey questions
-      @survey_questions = (@form_ids + @facilitator_form_ids).each do |form|
+      @survey_questions = (@workshop_form_ids + @facilitator_form_ids).each do |form|
         create :pd_survey_question, form_id: form
       end
 
@@ -45,49 +45,73 @@ module Pd::SurveyPipeline
     test 'can retrieve all data if no filter' do
       retriever = Pd::SurveyPipeline::DailySurveyRetriever.new
 
-      res = retriever.retrieve_data logger: @logger
+      result = retriever.retrieve_data logger: @logger
 
-      assert_equal @workshop_surveys.length, res.dig(:workshop_surveys)&.length
-      assert_equal @facilitator_surveys.length, res.dig(:facilitator_surveys)&.length
-      assert_equal @survey_questions.length, res.dig(:survey_questions)&.length
+      assert_equal @workshop_submissions.length, result[:workshop_submissions]&.length
+      assert_equal @facilitator_submissions.length, result[:facilitator_submissions]&.length
+      assert_equal @survey_questions.length, result[:survey_questions]&.length
     end
 
     test 'can retrieve data using workshop id filter' do
       filter = {workshop_ids: @workshops.first.id}
       retriever = Pd::SurveyPipeline::DailySurveyRetriever.new filter
 
-      res = retriever.retrieve_data logger: @logger
+      result = retriever.retrieve_data logger: @logger
 
-      assert_equal @workshop_surveys.length / @workshops.length,
-        res.dig(:workshop_surveys)&.length
-      assert_equal @facilitator_surveys.length / @workshops.length,
-        res.dig(:facilitator_surveys)&.length
-      assert_equal @form_ids.length + @facilitator_form_ids.length,
-        res.dig(:survey_questions)&.length
+      assert_equal @workshop_submissions.length / @workshops.length,
+        result[:workshop_submissions]&.length
+      assert_equal @facilitator_submissions.length / @workshops.length,
+        result[:facilitator_submissions]&.length
+      assert_equal @workshop_form_ids.length + @facilitator_form_ids.length,
+        result[:survey_questions]&.length
     end
 
     test 'can retrieve data using form id filter' do
-      filter = {form_ids: @form_ids.first}
+      filter = {form_ids: @workshop_form_ids.first}
       retriever = Pd::SurveyPipeline::DailySurveyRetriever.new filter
 
-      res = retriever.retrieve_data logger: @logger
+      result = retriever.retrieve_data logger: @logger
 
-      assert_equal @workshop_surveys.length / @form_ids.length,
-        res.dig(:workshop_surveys)&.length
-      assert_equal 0, res.dig(:facilitator_surveys)&.length
-      assert_equal 1, res.dig(:survey_questions)&.length
+      assert_equal @workshop_submissions.length / @workshop_form_ids.length,
+        result[:workshop_submissions]&.length
+      assert_equal 0, result[:facilitator_submissions]&.length
+      assert_equal 1, result[:survey_questions]&.length
     end
 
     test 'can retrieve data using both workshop id and form id filters' do
       filter = {workshop_ids: @workshops.first.id, form_ids: @facilitator_form_ids.first}
       retriever = Pd::SurveyPipeline::DailySurveyRetriever.new filter
 
-      res = retriever.retrieve_data logger: @logger
+      result = retriever.retrieve_data logger: @logger
 
-      assert_equal 0, res.dig(:workshop_surveys)&.length
-      assert_equal @facilitator_surveys.length / (@workshops.length * @facilitator_form_ids.length),
-        res.dig(:facilitator_surveys)&.length
-      assert_equal 1, res.dig(:survey_questions)&.length
+      assert_equal 0, result[:workshop_submissions]&.length
+      assert_equal @facilitator_submissions.size / (@workshops.length * @facilitator_form_ids.size),
+        result[:facilitator_submissions]&.length
+      assert_equal 1, result[:survey_questions]&.length
+    end
+
+    test 'return empty if workshop does not have submission' do
+      # Use non-existence workshop id as filter
+      filter = {workshop_ids: @workshops.pluck(:id).max + 1}
+      retriever = Pd::SurveyPipeline::DailySurveyRetriever.new filter
+
+      result = retriever.retrieve_data logger: @logger
+
+      assert_equal 0, result[:workshop_submissions]&.length
+      assert_equal 0, result[:facilitator_submissions]&.length
+      assert_equal 0, result[:survey_questions]&.length
+    end
+
+    test 'return empty if form does not have submission' do
+      # Use non-existence form id as filter
+      filter = {form_ids: @workshop_form_ids.max + 1}
+      retriever = Pd::SurveyPipeline::DailySurveyRetriever.new filter
+
+      result = retriever.retrieve_data logger: @logger
+
+      assert_equal 0, result[:workshop_submissions]&.length
+      assert_equal 0, result[:facilitator_submissions]&.length
+      assert_equal 0, result[:survey_questions]&.length
     end
   end
 end
