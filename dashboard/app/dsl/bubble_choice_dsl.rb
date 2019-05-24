58d30d8b548bd64112cfdc12884a9640c9cfebf3
Maddie Kasula <madelynkasula@gmail.com>
2019-05-23 18:36:57 -0700

    Add BubbleChoiceDsl with basic functionality

---
+++ dashboard/app/dsl/bubble_choice_dsl.rb
@@ -0,0 +1,32 @@
+class BubbleChoiceDSL < BaseDSL
+  def initialize
+    super
+    @hash[:sublevels] = []
+    @i18n_strings = Hash.new({})
+  end
+
+  def parse_output
+    {name: @name, properties: @hash}
+  end
+
+  def sublevels
+    @hash[:sublevels]
+  end
+
+  def level(name)
+    # Ensure level name hasn't already been used.
+    if @hash[:sublevels].include?(name)
+      raise "Don't use the same level twice in a BubbleChoice (#{name})."
+    end
+
+    @hash[:sublevels] << name
+  end
+
+  def i18n_strings
+    {'name' => {@name => @i18n_strings}}
+  end
+
+  def self.parse_file(filename)
+    super(filename, File.basename(filename, '.bubble_choice'))
+  end
+end
