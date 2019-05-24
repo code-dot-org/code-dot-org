73225f3217c3195be5cd3cea6c6ad57572c04340
Ha Nguyen <ha@code.org>
2019-05-23 22:47:11 -0700

    Create Retriever component, add tests

---
+++ dashboard/lib/pd/survey_pipeline/retriever.rb
@@ -0,0 +1,58 @@
+module Pd::SurveyPipeline
+  class RetrieverBase
+    def retrive_data(*)
+      raise 'Must override in derived class'
+    end
+  end
+
+  class DailySurveyRetriever < RetrieverBase
+    attr_reader :workshop_ids, :form_ids
+
+    # Set filters for this retriever.
+    # @param workshop_ids [Array<Integer>] array of selected workshop ids
+    # @param form_ids [Array<Integer>] array of selected form ids
+    def initialize(workshop_ids: nil, form_ids: nil)
+      @workshop_ids = workshop_ids
+      @form_ids = form_ids
+    end
+
+    # Retrieve data from Pd::SurveyQuestion, Pd::WorkshopDailySurvey and Pd::WorkshopDailySurvey.
+    # @return [Hash{survey_questions, workshop_surveys, facilitator_surveys}]
+    #   a hash contains questions and submissions
+    def retrieve_data(logger: nil)
+      logger&.info "RE: workshop_ids filter = #{workshop_ids}"
+      logger&.info "RE: form_ids filter = #{form_ids}"
+
+      # Build where clause from filter values
+      submission_filter = {}
+      submission_filter[:pd_workshop_id] = workshop_ids if workshop_ids.present?
+      submission_filter[:form_id] = form_ids if form_ids.present?
+
+      # Collect submissions
+      ws_submissions = Pd::WorkshopDailySurvey.where(submission_filter)
+      facilitator_submissions = Pd::WorkshopFacilitatorDailySurvey.where(submission_filter)
+
+      logger&.info "RE: ws_submissions.count = #{ws_submissions.count}"
+      logger&.debug "RE: ws_submissions = #{ws_submissions&.inspect}"
+      logger&.info "RE: facilitator_submissions.count = #{facilitator_submissions.count}"
+      logger&.debug "RE: facilitator_submissions = #{facilitator_submissions&.inspect}"
+
+      # Collect survey questions, ignore form that doesn't have submission.
+      form_ids_with_submissions =
+        ws_submissions.pluck(:form_id).uniq | facilitator_submissions.pluck(:form_id).uniq
+
+      question_filter = {}
+      question_filter[:form_id] = form_ids_with_submissions if form_ids_with_submissions.present?
+      questions = Pd::SurveyQuestion.where(question_filter)
+
+      logger&.info "RE: questions.count = #{questions.count}"
+      logger&.debug "RE: questions = #{questions&.inspect}"
+
+      {
+        survey_questions: questions,
+        workshop_surveys: ws_submissions,
+        facilitator_surveys: facilitator_submissions
+      }
+    end
+  end
+end
---
+++ dashboard/test/lib/pd/survey_pipeline/retriever_test.rb
@@ -0,0 +1,90 @@
+require 'test_helper'
+require 'pd/survey_pipeline/retriever.rb'
+
+module Pd::SurveyPipeline
+  class DailySurveyRetrieverTest < ActiveSupport::TestCase
+    include Pd::WorkshopConstants
+
+    setup_all do
+      @form_ids = [11_000_000_000_000, 11_000_000_000_001]
+      @facilitator_form_ids = [22_000_000_000_000, 22_000_000_000_001]
+      @workshops = create_list :pd_workshop, 2,
+        course: COURSE_CSF, subject: SUBJECT_CSF_201, num_sessions: 2
+
+      teachers = create_list :teacher, 2
+      facilitators = create_list :facilitator, 2
+      days = [0, 1]
+
+      # Create workshop daily surveys
+      @workshop_surveys = @workshops.product(teachers, days, @form_ids).each do |ws, teacher, day, form|
+        create :pd_workshop_daily_survey, pd_workshop: ws, user: teacher, day: day, form_id: form
+      end
+
+      # Create facilitator daily surveys
+      combinations = @workshops.product(teachers, days, @facilitator_form_ids, facilitators)
+
+      @facilitator_surveys = combinations.each do |ws, teacher, day, form, facilitator|
+        create :pd_workshop_facilitator_daily_survey, pd_workshop: ws, user: teacher,
+          day: day, form_id: form, pd_session: ws.sessions[day], facilitator: facilitator
+      end
+
+      # Create survey questions
+      @survey_questions = (@form_ids + @facilitator_form_ids).each do |form|
+        create :pd_survey_question, form_id: form
+      end
+
+      # TODO: remove logger in test and real code
+      log_file = File.new("#{File.dirname(__FILE__)}/log_retriever.txt", 'w')
+      log_file.sync = true
+      @logger = Logger.new(log_file, level: Logger::DEBUG)
+    end
+
+    test 'can retrieve all data if no filter' do
+      retriever = Pd::SurveyPipeline::DailySurveyRetriever.new
+
+      res = retriever.retrieve_data logger: @logger
+
+      assert_equal @workshop_surveys.length, res.dig(:workshop_surveys)&.length
+      assert_equal @facilitator_surveys.length, res.dig(:facilitator_surveys)&.length
+      assert_equal @survey_questions.length, res.dig(:survey_questions)&.length
+    end
+
+    test 'can retrieve data using workshop id filter' do
+      filter = {workshop_ids: @workshops.first.id}
+      retriever = Pd::SurveyPipeline::DailySurveyRetriever.new filter
+
+      res = retriever.retrieve_data logger: @logger
+
+      assert_equal @workshop_surveys.length / @workshops.length,
+        res.dig(:workshop_surveys)&.length
+      assert_equal @facilitator_surveys.length / @workshops.length,
+        res.dig(:facilitator_surveys)&.length
+      assert_equal @form_ids.length + @facilitator_form_ids.length,
+        res.dig(:survey_questions)&.length
+    end
+
+    test 'can retrieve data using form id filter' do
+      filter = {form_ids: @form_ids.first}
+      retriever = Pd::SurveyPipeline::DailySurveyRetriever.new filter
+
+      res = retriever.retrieve_data logger: @logger
+
+      assert_equal @workshop_surveys.length / @form_ids.length,
+        res.dig(:workshop_surveys)&.length
+      assert_equal 0, res.dig(:facilitator_surveys)&.length
+      assert_equal 1, res.dig(:survey_questions)&.length
+    end
+
+    test 'can retrieve data using both workshop id and form id filters' do
+      filter = {workshop_ids: @workshops.first.id, form_ids: @facilitator_form_ids.first}
+      retriever = Pd::SurveyPipeline::DailySurveyRetriever.new filter
+
+      res = retriever.retrieve_data logger: @logger
+
+      assert_equal 0, res.dig(:workshop_surveys)&.length
+      assert_equal @facilitator_surveys.length / (@workshops.length * @facilitator_form_ids.length),
+        res.dig(:facilitator_surveys)&.length
+      assert_equal 1, res.dig(:survey_questions)&.length
+    end
+  end
+end
