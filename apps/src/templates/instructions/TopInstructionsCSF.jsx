58122514e09b2c616148ad609b8339a3dfa7774f
Dani McAvoy <danimcavoy@gmail.com>
2019-05-28 13:10:09 -0400

    Position the CSF instructions correclty inside the instructions area

--- apps/src/templates/instructions/TopInstructionsCSF.jsx
+++ apps/src/templates/instructions/TopInstructionsCSF.jsx
@@ -88,7 +88,7 @@ const audioStyle = {
 
 const styles = {
   main: {
-    position: 'absolute',
+    position: 'relative',
     top: 0,
     right: 0
     // left handled by media queries for .editor-column
