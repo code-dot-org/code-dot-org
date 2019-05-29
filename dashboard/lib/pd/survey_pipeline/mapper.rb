07f490b6b3b9ffa35175f1e31f4d0618d35aeb62
Ha Nguyen <ha@code.org>
2019-05-29 14:51:54 -0700

    Add tests for mapper

--- dashboard/lib/pd/survey_pipeline/mapper.rb
+++ dashboard/lib/pd/survey_pipeline/mapper.rb
@@ -12,39 +12,52 @@ module Pd::SurveyPipeline
 
     # @param group_config [Array<key>] an array of keys in data going to be processed.
     # @param map_config [Array<Hash{:condition, :field, :reducers => lambda, key, Array}>]
-    #   :condition is a lambda
+    #   an array of rules that specify what reducers to apply on what fields when what
+    #   conditions are met.
+    #   :condition is a lambda.
     #   :field is a key in data going to be processed.
-    #   :reducers is an array of ReducerBase-derived classes
+    #   :reducers is an array of ReducerBase-derived classes.
     def initialize(group_config:, map_config:)
       @group_config = group_config
       @map_config = map_config
     end
 
-    # Summarize input data using groupping and map-reducing configurations.
-    # @param data [Array<Hash{}>] an array of hashes, each contains submission, question and answer info
-    # @return [Array<Hash>] an array of summarization results
+    # Summarize input data using groupping and mapping configurations.
+    # @param data [Array<Hash{}>] an array of hashes,
+    #   each contains submission, question, and answer info.
+    # @return [Array<Hash>] an array of summarization results.
     def map_reduce(data:, logger: nil)
-      return unless data.is_a? Array
+      return unless data.is_a? Enumerable
 
-      groups = data.group_by {|hash| hash.slice(*group_config)}
-      logger&.info "MP: groups.count = #{groups.count}"
-      logger&.debug "MP: groups = #{groups}"
+      groups = group_data(data)
+      logger&.info "MAP: groups.count = #{groups.count}"
+      logger&.debug "MAP: groups = #{groups}"
 
+      map_to_reducers(groups, logger)
+    end
+
+    # Break data into groups using groupping configuration.
+    def group_data(data)
+      data.group_by {|hash| hash.slice(*group_config)}
+    end
+
+    # Map groups to reducers using mapping configuration.
+    def map_to_reducers(groups, logger = nil)
       summaries = []
 
-      # Apply reducers on each group
       groups.each do |group_key, group_records|
-        logger&.debug "MP: group_key = #{group_key}"
-        logger&.debug "MP: group_records.count = #{group_records.count}"
+        logger&.debug "MAP: group_key = #{group_key}"
+        logger&.debug "MAP: group_records.count = #{group_records.count}"
 
+        # Apply matched reducers on each group. Add only non-empty result to the final summary.
         map_config.each do |condition:, field:, reducers:|
           logger&.debug "Match condition = #{condition.call(group_key)}"
           next unless condition.call(group_key)
 
-          logger&.debug "MP: reducers to apply = #{reducers.count}"
+          logger&.debug "MAP: reducers to apply = #{reducers.count}"
           reducers.each do |reducer|
             reducer_result = reducer.reduce group_records.pluck(field)
-            logger&.debug "MP: reducer.name = #{reducer.name}, result = #{reducer_result}"
+            logger&.debug "MAP: reducer.name = #{reducer.name}, result = #{reducer_result}"
 
             next unless reducer_result.present?
             summaries << group_key.merge({reducer: reducer.name, reducer_result: reducer_result})
