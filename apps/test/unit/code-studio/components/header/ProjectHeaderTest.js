7de91f384d3a15b84beecdf7a40b59d097080399
Chris Pirich <chris@code.org>
2019-05-30 13:44:32 -0700

    update ProjectHeaderTest to mount with Provider

--- apps/test/unit/code-studio/components/header/ProjectHeaderTest.js
+++ apps/test/unit/code-studio/components/header/ProjectHeaderTest.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+import {mount} from 'enzyme';
+
+import headerReducer, {
+  showProjectHeader
+} from '@cdo/apps/code-studio/headerRedux';
 
 import {expect} from '../../../../util/reconfiguredChai';
 import {replaceOnWindow, restoreOnWindow} from '../../../../util/testUtils';
@@ -8,10 +14,13 @@ import ProjectImport from '@cdo/apps/code-studio/components/header/ProjectImport
 import ProjectHeader from '@cdo/apps/code-studio/components/header/ProjectHeader';
 
 describe('ProjectHeader', () => {
+  let store;
   beforeEach(() => {
     replaceOnWindow('appOptions', {
       level: {}
     });
+    store = createStore(combineReducers({header: headerReducer}));
+    store.dispatch(showProjectHeader(false /* showExport */));
   });
 
   afterEach(() => {
@@ -19,12 +28,20 @@ describe('ProjectHeader', () => {
   });
 
   it('renders', () => {
-    shallow(<ProjectHeader />);
+    mount(
+      <Provider store={store}>
+        <ProjectHeader />
+      </Provider>
+    );
   });
 
   it('includes ProjectImport for Code Connection projects', () => {
     window.appOptions.level.isConnectionLevel = true;
-    const wrapper = shallow(<ProjectHeader />);
+    const wrapper = mount(
+      <Provider store={store}>
+        <ProjectHeader />
+      </Provider>
+    );
     expect(wrapper.find(ProjectImport)).to.have.lengthOf(1);
   });
 });
