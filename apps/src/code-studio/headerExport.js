5856ff5313bbb5d70f7be613f1e927c8a39b8cf3
Chris Pirich <chris@code.org>
2019-05-31 15:38:27 -0700

    PR feedback

--- apps/src/code-studio/headerExport.js
+++ apps/src/code-studio/headerExport.js
@@ -1,4 +1,4 @@
-/* globals dashboard, appOptions */
+/* globals appOptions */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -6,11 +6,11 @@ import ExportDialog from './components/ExportDialog';
 import {Provider} from 'react-redux';
 import {getStore} from '../redux';
 import {showExportDialog} from './components/exportDialogRedux';
+import project from './initApp/project';
+import i18n from './i18n';
 
 export function exportProject(shareUrl) {
-  dashboard.project.saveIfSourcesChanged().then(() => {
-    const i18n = window.dashboard.i18n;
-
+  project.saveIfSourcesChanged().then(() => {
     let dialogDom = document.getElementById('project-export-dialog');
     if (!dialogDom) {
       dialogDom = document.createElement('div');
@@ -18,19 +18,19 @@ export function exportProject(shareUrl) {
       document.body.appendChild(dialogDom);
     }
 
-    const appType = dashboard.project.getStandaloneApp();
+    const appType = project.getStandaloneApp();
     const pageConstants = getStore().getState().pageConstants;
     const canShareSocial = pageConstants.is13Plus || !pageConstants.isSignedIn;
 
     ReactDOM.render(
       <Provider store={getStore()}>
         <ExportDialog
-          isProjectLevel={!!dashboard.project.isProjectLevel()}
+          isProjectLevel={!!project.isProjectLevel()}
           i18n={i18n}
           shareUrl={shareUrl}
-          thumbnailUrl={dashboard.project.getThumbnailUrl()}
-          isAbusive={dashboard.project.exceedsAbuseThreshold()}
-          channelId={dashboard.project.getCurrentId()}
+          thumbnailUrl={project.getThumbnailUrl()}
+          isAbusive={project.exceedsAbuseThreshold()}
+          channelId={project.getCurrentId()}
           appType={appType}
           canShareSocial={canShareSocial}
           userSharingDisabled={appOptions.userSharingDisabled}
