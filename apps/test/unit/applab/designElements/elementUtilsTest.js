400b42dd205305a0133e7906392e64bf002e2437
Jessica Kulwik <jessica@code.org>
2019-06-04 00:00:15 +0000

    Revert "Revert "Merge pull request #28304 from code-dot-org/cpirich/applab-theme-padding""
    This reverts commit c76835d21ab21929494398e7f5cd4b2096381127.

---
+++ apps/test/unit/applab/designElements/elementUtilsTest.js
@@ -0,0 +1,115 @@
+import {expect} from '../../../util/configuredChai';
+
+import * as elementUtils from '@cdo/apps/applab/designElements/elementUtils';
+
+describe('Applab designElements/elementUtils', function() {
+  describe('calculatePadding', () => {
+    it('returns (0,0) for empty padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('');
+      expect(verticalPadding).is.equal(0);
+      expect(horizontalPadding).is.equal(0);
+    });
+
+    it('returns (0,0) for undefined padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding();
+      expect(verticalPadding).is.equal(0);
+      expect(horizontalPadding).is.equal(0);
+    });
+
+    it('returns (0,0) for "0" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('0');
+      expect(verticalPadding).is.equal(0);
+      expect(horizontalPadding).is.equal(0);
+    });
+
+    it('returns (0,0) for "0px" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('0px');
+      expect(verticalPadding).is.equal(0);
+      expect(horizontalPadding).is.equal(0);
+    });
+
+    it('returns (0,0) for "0 0px" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('0 0px');
+      expect(verticalPadding).is.equal(0);
+      expect(horizontalPadding).is.equal(0);
+    });
+
+    it('returns (0,0) for "0px 0" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('0px 0');
+      expect(verticalPadding).is.equal(0);
+      expect(horizontalPadding).is.equal(0);
+    });
+
+    it('returns (2,0) for "1px 0" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('1px 0');
+      expect(verticalPadding).is.equal(2);
+      expect(horizontalPadding).is.equal(0);
+    });
+
+    it('returns (0,2) for "0 1px" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('0 1px');
+      expect(verticalPadding).is.equal(0);
+      expect(horizontalPadding).is.equal(2);
+    });
+
+    it('returns (2,2) for "1px 1px" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('1px 1px');
+      expect(verticalPadding).is.equal(2);
+      expect(horizontalPadding).is.equal(2);
+    });
+
+    it('returns (3,0) for "1px 0 2px" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('1px 0 2px');
+      expect(verticalPadding).is.equal(3);
+      expect(horizontalPadding).is.equal(0);
+    });
+
+    it('returns (3,7) for "1px 0 2px 7px" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('1px 0 2px 7px');
+      expect(verticalPadding).is.equal(3);
+      expect(horizontalPadding).is.equal(7);
+    });
+
+    it('returns (3,17) for "1px 10px 2px 7px" padding string', () => {
+      const {
+        verticalPadding,
+        horizontalPadding
+      } = elementUtils.calculatePadding('1px 10px 2px 7px');
+      expect(verticalPadding).is.equal(3);
+      expect(horizontalPadding).is.equal(17);
+    });
+  });
+});
