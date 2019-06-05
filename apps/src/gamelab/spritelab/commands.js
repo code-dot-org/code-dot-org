dae0f5623d01ca9ec8f930a11b625275aed03f46
Anjali <anjali@code.org>
2019-06-05 11:59:24 -0400

    Location Commands

--- apps/src/gamelab/spritelab/commands.js
+++ apps/src/gamelab/spritelab/commands.js
@@ -1,9 +1,11 @@
 import {commands as actionCommands} from './actionCommands';
 import {commands as behaviorCommands} from './behaviorCommands';
+import {commands as locationCommands} from './locationCommands';
 import {commands as spriteCommands} from './spriteCommands';
 
 let commands = module.exports;
 
 Object.assign(commands, actionCommands);
 Object.assign(commands, behaviorCommands);
+Object.assign(commands, locationCommands);
 Object.assign(commands, spriteCommands);
