68c80f292d8219a182556ffc8d2b4ead1b3ff2af
Anjali <anjali@code.org>
2019-06-05 12:04:46 -0400

    Event Commands

--- apps/src/gamelab/spritelab/commands.js
+++ apps/src/gamelab/spritelab/commands.js
@@ -1,5 +1,6 @@
 import {commands as actionCommands} from './actionCommands';
 import {commands as behaviorCommands} from './behaviorCommands';
+import {commands as eventCommands} from './eventCommands';
 import {commands as locationCommands} from './locationCommands';
 import {commands as spriteCommands} from './spriteCommands';
 import {commands as worldCommands} from './worldCommands';
@@ -8,6 +9,7 @@ let commands = module.exports;
 
 Object.assign(commands, actionCommands);
 Object.assign(commands, behaviorCommands);
+Object.assign(commands, eventCommands);
 Object.assign(commands, locationCommands);
 Object.assign(commands, spriteCommands);
 Object.assign(commands, worldCommands);
