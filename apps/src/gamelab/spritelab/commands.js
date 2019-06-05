5fd3b7ac36963a974822d8dbebfedc739961df6c
Anjali <anjali@code.org>
2019-06-05 11:43:27 -0400

    Action Commands

--- apps/src/gamelab/spritelab/commands.js
+++ apps/src/gamelab/spritelab/commands.js
@@ -1,7 +1,9 @@
+import {commands as actionCommands} from './actionCommands';
 import {commands as behaviorCommands} from './behaviorCommands';
 import {commands as spriteCommands} from './spriteCommands';
 
 let commands = module.exports;
 
+Object.assign(commands, actionCommands);
 Object.assign(commands, behaviorCommands);
 Object.assign(commands, spriteCommands);
