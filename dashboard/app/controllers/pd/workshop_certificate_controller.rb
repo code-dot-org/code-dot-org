15c58120de14701b70b029e160ae0f9dce0a7091
Brad Buchanan <brad@code.org>
2019-06-06 15:26:41 -0700

    Remove TODO - I think we do want certificates to be public

--- dashboard/app/controllers/pd/workshop_certificate_controller.rb
+++ dashboard/app/controllers/pd/workshop_certificate_controller.rb
@@ -2,7 +2,6 @@ require 'pd/certificate_renderer'
 
 class Pd::WorkshopCertificateController < ApplicationController
   before_action :authenticate_user!
-  # TODO: Should this be load_and_authorize_resource?
   load_resource :enrollment, class: 'Pd::Enrollment',
     find_by: :code, id_param: :enrollment_code
 
