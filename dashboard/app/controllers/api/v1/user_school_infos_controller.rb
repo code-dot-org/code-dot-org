638eb378b7fb18a1d943f74f22d98cd1afe3eb77
Brad Buchanan <brad@code.org>
2019-06-05 17:00:44 -0700

    Collapse dropdown and non-dropdown cases in controller

--- dashboard/app/controllers/api/v1/user_school_infos_controller.rb
+++ dashboard/app/controllers/api/v1/user_school_infos_controller.rb
@@ -14,22 +14,13 @@ class Api::V1::UserSchoolInfosController < ApplicationController
     return unless school_info_params[:school_id].present? || school_info_params[:country].present?
 
     existing_school_info = current_user.school_info
-    new_school_info = SchoolInfo.where(school_info_params).first_or_create(validation_type: SchoolInfo::VALIDATION_NONE)
-    if school_info_params[:school_id].present?
-      existing_school_info&.assign_attributes(school_info_params)
-      if existing_school_info.nil? || existing_school_info.changed?
-        current_user.update!(school_info_id: new_school_info.id)
-      else
-        current_user.user_school_infos.where(school_info: new_school_info).update(last_confirmation_date: DateTime.now)
-      end
-    # partial school info entered, so update it
-    elsif school_info_params[:country].present?
-      existing_school_info&.assign_attributes(school_info_params)
-      if existing_school_info.nil? || existing_school_info.changed?
-        current_user.update!(school_info_id: new_school_info.id)
-      else
-        current_user.user_school_infos.where(school_info: existing_school_info).update(last_confirmation_date: DateTime.now)
-      end
+    existing_school_info&.assign_attributes school_info_params
+    if existing_school_info.nil? || existing_school_info.changed?
+      current_user.update! school_info: SchoolInfo.where(school_info_params).
+        first_or_create(validation_type: SchoolInfo::VALIDATION_NONE)
+    else
+      current_user.user_school_infos.where(school_info: existing_school_info).
+        update(last_confirmation_date: DateTime.now)
     end
   end
 
