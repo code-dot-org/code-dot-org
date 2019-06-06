c28378aff4e384c5621e12538f344bf9c25a0647
Brad Buchanan <brad@code.org>
2019-06-06 13:54:21 -0700

    Break out helper methods for field specs

--- dashboard/lib/pd/certificate_renderer.rb
+++ dashboard/lib/pd/certificate_renderer.rb
@@ -13,36 +13,88 @@ module Pd
     #
     # @param [Pd::Enrollment] a teacher's workshop enrollment
     def self.render_workshop_certificate(enrollment)
-      workshop = enrollment.workshop
+      create_workshop_certificate_image(
+        dashboard_dir('app', 'assets', 'images', 'pd_workshop_certificate_generic.png'),
+        [
+          *teacher_name(enrollment),
+          *pd_hours(enrollment.workshop),
+          *workshop_dates(enrollment.workshop),
+          *course_name(enrollment.workshop),
+          *facilitators(enrollment.workshop)
+        ]
+      )
+    end
 
-      course_name =
-        if workshop.csf?
-          [
-            {
-              string: workshop.course_name,
-              y: 780,
-              pointsize: 90,
-              height: 100,
-            },
-            {
-              string: workshop.friendly_subject,
-              y: 870,
-              pointsize: 80,
-              height: 90,
-            }
-          ]
-        else
-          [
-            {
-              string: workshop.course_name,
-              y: 800,
-              pointsize: 90,
-              height: 100,
-            }
-          ]
-        end
+    # The methods that follow generate field configurations for printing text
+    # onto the certificate image.
 
-      facilitator_fields = facilitator_names(workshop).each_with_index.map do |name, i|
+    private_class_method def self.teacher_name(enrollment)
+      [
+        {
+          string: enrollment.try(:full_name) || '',
+          pointsize: 90,
+          height: 100,
+          width: 1200,
+          x: 570,
+          y: 570,
+        }
+      ]
+    end
+
+    private_class_method def self.course_name(workshop)
+      if workshop.csf?
+        [
+          {
+            string: workshop.course_name,
+            y: 780,
+            pointsize: 90,
+            height: 100,
+          },
+          {
+            string: workshop.friendly_subject,
+            y: 870,
+            pointsize: 80,
+            height: 90,
+          }
+        ]
+      else
+        [
+          {
+            string: workshop.course_name,
+            y: 800,
+            pointsize: 90,
+            height: 100,
+          }
+        ]
+      end
+    end
+
+    private_class_method def self.pd_hours(workshop)
+      [
+        {
+          string: ActiveSupport::NumberHelper.number_to_rounded(workshop.effective_num_hours, precision: 1, strip_insignificant_zeros: true),
+          y: 975,
+          x: 1065,
+          height: 40,
+          width: 50,
+          pointsize: 40,
+        }
+      ]
+    end
+
+    private_class_method def self.workshop_dates(workshop)
+      [
+        {
+          string: workshop.workshop_date_range_string,
+          y: 1042,
+          height: 50,
+          pointsize: 45,
+        }
+      ]
+    end
+
+    private_class_method def self.facilitators(workshop)
+      facilitator_names(workshop).each_with_index.map do |name, i|
         {
           string: name,
           height: 50,
@@ -52,37 +104,9 @@ module Pd
           x: 1290,
         }
       end
-
-      create_workshop_certificate_image(
-        dashboard_dir('app', 'assets', 'images', 'pd_workshop_certificate_generic.png'),
-        [
-          {
-            string: enrollment.try(:full_name) || '',
-            pointsize: 90,
-            height: 100,
-            width: 1200,
-            x: 570,
-            y: 570,
-          },
-          {
-            string: ActiveSupport::NumberHelper.number_to_rounded(workshop.effective_num_hours, precision: 1, strip_insignificant_zeros: true),
-            y: 975,
-            x: 1065,
-            height: 40,
-            width: 50,
-            pointsize: 40,
-          },
-          {
-            string: workshop.workshop_date_range_string,
-            y: 1042,
-            height: 50,
-            pointsize: 45,
-          }
-        ] + course_name + facilitator_fields
-      )
     end
 
-    def self.facilitator_names(workshop)
+    private_class_method def self.facilitator_names(workshop)
       if workshop.teachercon?
         case workshop.course
         when Pd::Workshop::COURSE_CSD
