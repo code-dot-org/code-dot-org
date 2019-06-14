d445ac95f25ef27484de1e4b1f9271d4c19096dc
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-06-13 17:11:39 -0700

    update blockly redaction to use simple string matching rather than regex, and to support both block and inline blockly

--- bin/i18n/plugins/blockly.js
+++ bin/i18n/plugins/blockly.js
@@ -1,58 +1,79 @@
 let redact;
 
-const INSTRUCTIONBLOCK = "instructionblock";
-const INSTRUCTIONBLOCK_RE = /^<xml><block type="(.+?)".+?<\/block><\/xml>/;
+const BLOCKLY = 'blockly';
 
-module.exports = function instructionblocks() {
+module.exports = function blockly() {
   if (this.Parser) {
     const Parser = this.Parser;
     redact = Parser.prototype.options.redact;
-    const tokenizers = Parser.prototype.inlineTokenizers;
-    const methods = Parser.prototype.inlineMethods;
 
-    /* Add an inline tokenizer (defined in the following example). */
-    tokenizers[INSTRUCTIONBLOCK] = tokenizeInstructionBlock;
-
-    Parser.prototype.restorationMethods[INSTRUCTIONBLOCK] = function(
-      add,
-      node
-    ) {
+    Parser.prototype.restorationMethods[BLOCKLY] = function(add, node) {
       return add({
-        type: "rawtext",
+        type: 'rawtext',
         value: node.original
       });
     };
 
-    /* Run it just before `html`. */
-    methods.splice(methods.indexOf("html"), 0, INSTRUCTIONBLOCK);
-  }
-};
-tokenizeInstructionBlock.notInLink = true;
-tokenizeInstructionBlock.locator = locateInstructionBlock;
-
-function tokenizeInstructionBlock(eat, value, silent) {
-  const match = INSTRUCTIONBLOCK_RE.exec(value);
-
-  if (match && redact) {
-    const add = eat(match[0]);
-    const blockType = match[1];
-    const original = match[0];
-
-    return add({
-      type: "redaction",
-      redactionType: "instructionblock",
-      blockType,
-      original,
-      children: [
-        {
-          type: "text",
-          value: blockType
-        }
-      ]
+    ['inline', 'block'].forEach(type => {
+      Parser.prototype[`${type}Tokenizers`][BLOCKLY] = tokenizeBlockly;
+      const methods = Parser.prototype[`${type}Methods`];
+      methods.splice(methods.indexOf('html'), 0, BLOCKLY);
     });
   }
-}
+};
 
-function locateInstructionBlock(value, fromIndex) {
-  return value.indexOf("<", fromIndex);
+function tokenizeBlockly(eat, value, silent) {
+  // This plugin ONLY exists to redact Blockly blocks from translations, not to
+  // render anything.
+  if (!redact) {
+    return false;
+  }
+
+  if (!value.startsWith('<xml>')) {
+    return false;
+  }
+
+  // Search indiscriminately for next closing tag. Note that this precludes
+  // situations like:
+  //
+  // <xml>
+  //   <block>
+  //     <xml>
+  //       <block/>
+  //     </xml>
+  //   </block>
+  // </xml>
+  //
+  // Because we would match the inner closing tag with the outer opening tag.
+  // However, that's not valid XML anyway, so I don't think we care.
+  const closingTag = '</xml>';
+  const closingIndex = value.indexOf(closingTag);
+
+  if (closingIndex === -1) {
+    return false;
+  }
+
+  if (silent) {
+    return true;
+  }
+
+  // Eat to end of closing tag
+  const original = value.substring(0, closingIndex + closingTag.length);
+  return eat(original)({
+    type: 'redaction',
+    redactionType: BLOCKLY,
+    original,
+    children: [
+      {
+        type: 'text',
+        value: 'blockly block'
+      }
+    ]
+  });
+}
+tokenizeBlockly.notInLink = true;
+tokenizeBlockly.locator = locateBlockly;
+
+function locateBlockly(value, fromIndex) {
+  return value.indexOf('<', fromIndex);
 }
