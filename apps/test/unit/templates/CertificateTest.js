a7f2bd9e6144b48d5baa5043c4011399de7a1f0b
Brad Buchanan <brad@code.org>
2019-06-17 16:36:29 -0700

    Test each certificate type

--- apps/test/unit/templates/CertificateTest.js
+++ apps/test/unit/templates/CertificateTest.js
@@ -20,7 +20,16 @@ describe('Certificate', () => {
     window.dashboard = storedWindowDashboard;
   });
 
-  it('renders a Minecraft certificate for new Minecraft tutorials', () => {
+  it('renders Minecraft certificate for Minecraft Adventurer', () => {
+    const wrapper = shallow(<Certificate tutorial="mc" />, {
+      context: {store}
+    }).dive();
+    expect(wrapper.find('img').html()).to.include(
+      'MC_Hour_Of_Code_Certificate'
+    );
+  });
+
+  it('renders Minecraft certificate for Minecraft Designer', () => {
     const wrapper = shallow(<Certificate tutorial="minecraft" />, {
       context: {store}
     }).dive();
@@ -29,10 +38,30 @@ describe('Certificate', () => {
     );
   });
 
-  it('renders a default certificate for all other tutorials', () => {
-    const wrapper = shallow(<Certificate tutorial="frozen" />, {
+  it("renders unique certificate for Minecraft Hero's Journey", () => {
+    const wrapper = shallow(<Certificate tutorial="hero" />, {
       context: {store}
     }).dive();
-    expect(wrapper.find('img').html()).to.include('hour_of_code_certificate');
+    expect(wrapper.find('img').html()).to.include(
+      'MC_Hour_Of_Code_Certificate_Hero'
+    );
+  });
+
+  it('renders unique certificate for Minecraft Voyage Aquatic', () => {
+    const wrapper = shallow(<Certificate tutorial="aquatic" />, {
+      context: {store}
+    }).dive();
+    expect(wrapper.find('img').html()).to.include(
+      'MC_Hour_Of_Code_Certificate_Aquatic'
+    );
+  });
+
+  it('renders default certificate for all other tutorials', () => {
+    ['applab-intro', 'dance', 'flappy', 'frozen'].forEach(tutorial => {
+      const wrapper = shallow(<Certificate tutorial={tutorial} />, {
+        context: {store}
+      }).dive();
+      expect(wrapper.find('img').html()).to.include('hour_of_code_certificate');
+    });
   });
 });
