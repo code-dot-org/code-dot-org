793961fa45238678ed7904370b71b7f7c4cbf525
Ha Nguyen <ha@code.org>
2019-06-18 15:32:19 -0700

    Display errors on client side

--- dashboard/app/controllers/api/v1/pd/workshop_survey_report_controller.rb
+++ dashboard/app/controllers/api/v1/pd/workshop_survey_report_controller.rb
@@ -113,12 +113,17 @@ module Api::V1::Pd
 
     # GET /api/v1/pd/workshops/:id/generic_survey_report
     def generic_survey_report
+      # Default HTTP status code to return to client if
+      # we encouter an exception processing this request.
+      error_status_code = :internal_server_error
+
       return local_workshop_daily_survey_report if @workshop.summer? ||
         ([COURSE_CSP, COURSE_CSD].include?(@workshop.course) &&
         @workshop.workshop_starting_date > Date.new(2018, 8, 1))
 
       return create_csf_survey_report if @workshop.csf? && @workshop.subject == SUBJECT_CSF_201
 
+      error_status_code = :bad_request
       raise 'Action generic_survey_report should not be used for this workshop'
     rescue => e
       Honeybadger.notify(
@@ -130,7 +135,7 @@ module Api::V1::Pd
         }
       )
 
-      render status: :bad_request, json: {
+      render status: error_status_code, json: {
         errors: [
           {
             severity: Logger::Severity::ERROR,
