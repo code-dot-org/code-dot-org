3ed2d9b93dd973bc5e2efe384aecf630cbdac915
David Bailey <davidsbailey@users.noreply.github.com>
2019-06-19 19:47:53 -0700

    create AssessmentActivity model

---
+++ dashboard/app/models/assessment_activity.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: assessment_activities
+#
+#  id              :integer          not null, primary key
+#  user_id         :integer          not null
+#  level_id        :integer          not null
+#  script_id       :integer          not null
+#  level_source_id :integer
+#  attempt         :integer
+#  test_result     :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_assessment_activities_on_user_and_level_and_script  (user_id,level_id,script_id)
+#
+
+class AssessmentActivity < ActiveRecord::Base
+end
