89f1da134481ae1c2b28ec0911481836c2561976
Brad Buchanan <brad@code.org>
2019-06-20 17:36:20 -0700

    More tests that only need vanilla chai

--- apps/test/unit/applab/designElements/dropdownTest.js
+++ apps/test/unit/applab/designElements/dropdownTest.js
@@ -1,4 +1,4 @@
-import {expect} from '../../../util/configuredChai';
+import {expect} from 'chai';
 import dropdown from '@cdo/apps/applab/designElements/dropdown';
 import library from '@cdo/apps/applab/designElements/library';
 
