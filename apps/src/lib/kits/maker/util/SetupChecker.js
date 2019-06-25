89bf499c6463287fdb2af71d972458385bcca3f7
Erin Peach <erin@code.org>
2019-06-25 09:45:36 -0700

    Remove duplicate definition from merge

--- apps/src/lib/kits/maker/util/SetupChecker.js
+++ apps/src/lib/kits/maker/util/SetupChecker.js
@@ -1,12 +1,6 @@
 /** @file Stubbable core setup check behavior for the setup page. */
 import CircuitPlaygroundBoard from '../CircuitPlaygroundBoard';
-import {
-  ADAFRUIT_VID,
-  CIRCUIT_PLAYGROUND_EXPRESS_PID,
-  CIRCUIT_PLAYGROUND_PID,
-  ensureAppInstalled,
-  findPortWithViableDevice
-} from '../portScanning';
+import {ensureAppInstalled, findPortWithViableDevice} from '../portScanning';
 import {
   isCodeOrgBrowser,
   isChrome,
@@ -66,21 +60,6 @@ export default class SetupChecker {
    */
   detectComponentsInitialize() {
     return this.boardController.initializeComponents();
-  }
-
-  /**
-   * Placeholder function to detect device type
-   */
-  detectBoardType() {
-    if (parseInt(this.port.vendorId, 16) === ADAFRUIT_VID) {
-      if (parseInt(this.port.productId, 16) === CIRCUIT_PLAYGROUND_PID) {
-        console.log('Circuit Playground Classic');
-      } else if (
-        parseInt(this.port.productId, 16) === CIRCUIT_PLAYGROUND_EXPRESS_PID
-      ) {
-        console.log('Circuit Playground Express');
-      }
-    }
   }
 
   /**
