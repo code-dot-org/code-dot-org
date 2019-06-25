a4169eb5468c73ba0e7598a91c2c2449054e476e
Jessica Kulwik <jessica@code.org>
2019-06-25 23:35:52 +0000

    Revert "Merge pull request #29252 from code-dot-org/vanilla-chai-elsewhere"
    This reverts commit 02161903acb2294e2f273de1a45615bb6e63a47f, reversing
    changes made to 8fdebf072e9eef8238ba76dd79ff419a57b7bb4b.

--- apps/test/unit/templates/AppViewTest.jsx
+++ apps/test/unit/templates/AppViewTest.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {expect} from 'chai';
+import {expect} from '../../util/configuredChai';
 import {UnconnectedAppView as AppView} from '@cdo/apps/templates/AppView';
 
 describe('AppView', () => {
