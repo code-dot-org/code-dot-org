2ab8b5c959026cc22ef122f9943055af04f302b4
Karis Kim <ouniiq@hotmail.com>
2019-06-25 06:05:16 -0700

    refactored ec_uicontrols, ec_variables, helper_libraries, and timeout_api_blocks

--- apps/test/integration/levelSolutions/applab/timeout_api_blocks.js
+++ apps/test/integration/levelSolutions/applab/timeout_api_blocks.js
@@ -47,7 +47,7 @@ module.exports = {
         setTimeout(function() {
           console.log('done');
         }, 5);
-        
+
         var key = setTimeout(function() {
           console.log('do not expect this');
         }, 4);
@@ -59,7 +59,7 @@ module.exports = {
       },
       function validateResult(assert) {
         const debugOutput = document.getElementById('debug-output').textContent;
-        assert.equal(debugOutput, 'done');
+        assert.equal(debugOutput, '"done"');
       }
     ),
 
@@ -71,7 +71,7 @@ module.exports = {
           console.log('interval ' + i);
           i++;
         }, 5);
-        
+
         var key = setInterval(function() {
           console.log('do not expect this');
         }, 4);
@@ -98,7 +98,7 @@ module.exports = {
           console.log('timedLoop ' + i);
           i++;
         });
-        
+
         var key = timedLoop(4, function() {
           console.log('do not expect this');
         });
