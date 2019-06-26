bab728be4bb1f929e21844275af1911f713edbd6
Jessica Kulwik <jessica@code.org>
2019-06-26 23:10:51 +0000

    Revert "Merge pull request #28535 from code-dot-org/getList-cleanup"
    This reverts commit 1b019d247cef31f22d609282a6fbceadc032de89, reversing
    changes made to 384ff9d38f4c751f031293b388a5987388cbddaa.

--- apps/src/applab/getListDropdown.js
+++ apps/src/applab/getListDropdown.js
@@ -4,7 +4,7 @@ import {getFirstParam} from '../dropletUtils';
 export function getListColumnDropdown() {
   return function(aceEditor) {
     const tableName = getFirstParam('getList', this.parent, aceEditor);
-    const columns = getColumnsForTable(tableName, datasetLibrary.datasets);
+    const columns = getColumnsForTable(tableName);
     const opts = [];
     columns.forEach(columnName =>
       opts.push({display: `"${columnName}"`, text: `"${columnName}"`})
@@ -13,15 +13,11 @@ export function getListColumnDropdown() {
   };
 }
 
-function getColumnsForTable(tableName, datasets) {
-  let dataset = datasets.find(d => `"${d.name}"` === tableName);
-  if (dataset) {
-    return dataset.columns.split(',');
-  } else {
-    return [];
+function getColumnsForTable(tableName) {
+  for (let dataset of datasetLibrary.datasets) {
+    if (`"${dataset.name}"` === tableName) {
+      return dataset.columns.split(',');
+    }
   }
+  return [];
 }
-
-export var __TestInterface = {
-  getColumnsForTable: getColumnsForTable
-};
