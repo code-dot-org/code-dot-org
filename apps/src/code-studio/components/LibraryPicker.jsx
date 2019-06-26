12c18ccb0657da262c2bd261519509bc9744e349
Jessica Kulwik <jessica@code.org>
2019-06-26 23:19:43 +0000

    Revert "Merge pull request #28885 from code-dot-org/libraries-log-firehose"
    This reverts commit a4532108f64d23892efa00df1c3d42cab4947910, reversing
    changes made to 08416cdeade775d27dd8e39fcff31106eb5f6fad.

--- apps/src/code-studio/components/LibraryPicker.jsx
+++ apps/src/code-studio/components/LibraryPicker.jsx
@@ -6,7 +6,6 @@ import clientApi from '@cdo/apps/code-studio/initApp/clientApi';
 import {addApplabLibrary} from './applabLibraryRedux';
 import {connect} from 'react-redux';
 import project from '../initApp/project';
-import firehoseClient from '../../lib/util/firehose';
 
 const styles = {
   root: {
@@ -91,16 +90,6 @@ class LibraryPicker extends React.Component {
     libraryPilot.fetch(
       this.state.libraryId + '/library.json',
       (error, data) => {
-        firehoseClient.putRecord({
-          study: 'library_pilot',
-          study_group: 'pilot',
-          event: 'import_library',
-          data_json: JSON.stringify({
-            importedLibrary: this.state.libraryId,
-            projectId: project.getCurrentId()
-          })
-        });
-
         if (error) {
           this.setState({
             displayError: true,
