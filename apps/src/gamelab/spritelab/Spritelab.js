de073ce64fc7384c42690b23dc21cbae1e96f32b
Anjali Pal <ajpal@users.noreply.github.com>
2019-07-01 13:16:12 -0700

    Revert "Pull spritelab preview code out of gamelab.js"

--- apps/src/gamelab/spritelab/Spritelab.js
+++ apps/src/gamelab/spritelab/Spritelab.js
@@ -1,30 +1,8 @@
 import {commands} from './commands';
 import * as spriteUtils from './spriteUtils';
-import Sounds from '../../Sounds';
-import {getStore} from '../../redux';
-import {clearConsole} from '../textConsoleModule';
 
 var Spritelab = function() {
   this.reset = () => spriteUtils.reset();
-
-  this.preview = function() {
-    if (!this.areAnimationsReady_()) {
-      return;
-    }
-    spriteUtils.reset();
-    getStore().dispatch(clearConsole());
-    Sounds.getSingleton().muteURLs();
-    if (this.gameLabP5.p5 && this.JSInterpreter) {
-      this.gameLabP5.p5.allSprites.removeSprites();
-      this.JSInterpreter.deinitialize();
-      this.initInterpreter(false /* attachDebugger */);
-      this.onP5Setup();
-      this.gameLabP5.p5.redraw();
-    } else {
-      this.gameLabP5.startExecution();
-      this.gameLabP5.setLoop(false);
-    }
-  };
 
   this.commands = commands;
 };
