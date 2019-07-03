09d7d84dc7b53319213b8552737f753678a15002
Erin Bond <erin.bond@code.org>
2019-07-03 14:09:35 -0700

    comment clarification

--- dashboard/app/controllers/teacher_feedbacks_controller.rb
+++ dashboard/app/controllers/teacher_feedbacks_controller.rb
@@ -2,7 +2,7 @@ class TeacherFeedbacksController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
 
-  # Feedback from any teacher who has provided feedback to a
+  # Feedback from any teacher who has provided feedback to the current
   # student on any level
   def index
   end
