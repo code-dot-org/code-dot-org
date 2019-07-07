37db84ca5c74803138c111b0ebd7dee5b1c276b8
David Bailey <davidsbailey@users.noreply.github.com>
2019-07-07 16:34:11 -0700

    move from babel-polyfill to core-js

--- apps/src/util/idempotent-babel-polyfill.js
+++
@@ -1,9 +0,0 @@
-/**
- * babel-polyfill complains if it gets required on a page twice, but sadly
- * does not really provide a good way to just load it once for sure.
- * fortunately, it does set the _babelPolyfill variable in the global scope
- * to true when it loads, so we can check that to avoid loading it again.
- */
-if (!global._babelPolyfill) {
-  require('babel-polyfill');
-}
