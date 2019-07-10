35ef4bc5ba16527d5139ccbbc2045a876fb3c138
Erin Bond <erin.bond@code.org>
2019-07-10 13:37:51 -0700

    adjust lastUpdated props to ensure TimeAgo component renders without warnings

--- apps/src/templates/feedback/LevelFeedbackEntry.story.jsx
+++ apps/src/templates/feedback/LevelFeedbackEntry.story.jsx
@@ -8,7 +8,7 @@ const defaultProps = {
     linkToLevel: '/',
     unitName: 'CSP Unit 3 - Intro to Programming',
     linkToUnit: '/',
-    lastUpdated: new Date().toLocaleString(),
+    lastUpdated: new Date(),
     comment: 'Excellent work! You followed the directions closely.'
   }
 };
