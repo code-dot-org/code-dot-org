33ef925ca2871a0335b08b808dbca8b44586c4f5
Erin Bond <erin.bond@code.org>
2019-07-11 12:07:07 -0700

    use only a seen_at field and make it specific to feedback page

--- dashboard/db/migrate/20190711180756_add_seen_to_teacher_feedbacks.rb
+++
@@ -1,6 +0,0 @@
-class AddSeenToTeacherFeedbacks < ActiveRecord::Migration[5.0]
-  def change
-    add_column :teacher_feedbacks, :seen, :boolean, default: false
-    add_column :teacher_feedbacks, :seen_at, :datetime
-  end
-end
