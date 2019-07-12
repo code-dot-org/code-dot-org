63da977b5f1a2e52852e6bdad80879795924a79b
Brad Buchanan <brad@code.org>
2019-07-12 14:48:57 -0700

    Remove sinon-chai-in-order from configuredChai

--- apps/test/util/reconfiguredChai.js
+++ apps/test/util/reconfiguredChai.js
@@ -2,12 +2,10 @@
 import chai from 'chai';
 import chaiSubset from 'chai-subset';
 import sinonChai from 'sinon-chai';
-import sinonChaiInOrder from 'sinon-chai-in-order';
 import chaiAsPromised from 'chai-as-promised';
 import chaiXml from 'chai-xml';
 
 chai.use(sinonChai);
-chai.use(sinonChaiInOrder);
 chai.use(chaiSubset);
 chai.use(chaiAsPromised);
 chai.use(chaiXml);
