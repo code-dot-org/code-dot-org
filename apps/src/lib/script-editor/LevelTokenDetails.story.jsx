24289e15a0cd71726e2397fae6658304983a815e
David Bailey <davidsbailey@users.noreply.github.com>
2019-07-15 16:56:37 -0700

    create storybook for LevelTokenDetails

---
+++ apps/src/lib/script-editor/LevelTokenDetails.story.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {UnconnectedLevelTokenDetails as LevelTokenDetails} from './LevelTokenDetails';
+import {action} from '@storybook/addon-actions';
+
+const levelKeyList = {
+  1: 'Level One',
+  2: 'Level Two',
+  3: 'Level Three'
+};
+
+const defaultLevel = {
+  kind: 'puzzle',
+  ids: [2],
+  activeId: 2
+};
+
+export default storybook => {
+  storybook.storiesOf('LevelTokenDetails', module).addStoryTable([
+    {
+      name: 'level token details',
+      story: () => (
+        <div style={{width: 800}}>
+          <LevelTokenDetails
+            levelKeyList={levelKeyList}
+            chooseLevelType={action('chooseLevelType')}
+            chooseLevel={action('chooseLevel')}
+            addVariant={action('addVariant')}
+            setActiveVariant={action('setActiveVariant')}
+            setField={action('setField')}
+            level={defaultLevel}
+            stagePosition={1}
+          />
+        </div>
+      )
+    }
+  ]);
+};
