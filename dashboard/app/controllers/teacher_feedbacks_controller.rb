ae657ba81783d7faf72897d92ada52939194a938
Erin Bond <erin.bond@code.org>
2019-07-16 14:56:27 -0700

    count only includes unseen, update seen_on_feedback_page_at

--- dashboard/app/controllers/teacher_feedbacks_controller.rb
+++ dashboard/app/controllers/teacher_feedbacks_controller.rb
@@ -1,10 +1,15 @@
 class TeacherFeedbacksController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
+  after_action :set_seen_on_feedback_page_at, only: :index
 
   # Feedback from any teacher who has provided feedback to the current
   # student on any level
   def index
     @teacher_feedbacks = @teacher_feedbacks.map {|feedback| feedback.attributes.merge(feedback&.script_level&.summary_for_feedback)}
+  end
+
+  def set_seen_on_feedback_page_at
+    TeacherFeedback.where(student_id: current_user.id).update_all(seen_on_feedback_page_at: DateTime.now)
   end
 end
