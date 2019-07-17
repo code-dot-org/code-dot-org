afda56e8989e105078a6e401b0183c0fe0e2b648
Winter Dong <winter@code.org>
2019-07-17 10:31:52 -0700

    Fix linting

--- lib/cdo/aws/cloud_formation.rb
+++ lib/cdo/aws/cloud_formation.rb
@@ -565,8 +565,8 @@ module AWS
         object_exists = Aws::S3::Client.new.head_object(bucket: S3_BUCKET, key: key) rescue nil
         unless object_exists
           CDO.log.info("Uploading Lambda zip package to S3 (#{code_zip.length} bytes)...")
-          Aws::S3::Client.new(http_read_timeout: 30)
-              .put_object({bucket: S3_BUCKET, key: key, body: code_zip})
+          Aws::S3::Client.new(http_read_timeout: 30).
+              put_object({bucket: S3_BUCKET, key: key, body: code_zip})
         end
         {
           S3Bucket: S3_BUCKET,
