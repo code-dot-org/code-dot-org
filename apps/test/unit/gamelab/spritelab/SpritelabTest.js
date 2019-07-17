d6675c035a484ed3088c0c336444a023e3513bde
Anjali <anjali@code.org>
2019-07-17 09:19:45 -0700

    Fix test, add test

--- apps/test/unit/gamelab/spritelab/SpritelabTest.js
+++ apps/test/unit/gamelab/spritelab/SpritelabTest.js
@@ -1,11 +1,23 @@
 import sinon from 'sinon';
 import {expect} from '../../../util/reconfiguredChai';
+import {
+  getStore,
+  registerReducers,
+  stubRedux,
+  restoreRedux
+} from '@cdo/apps/redux';
+import commonReducers from '@cdo/apps/redux/commonReducers';
+import reducers from '@cdo/apps/gamelab/reducers';
+import {setIsRunning} from '@cdo/apps/redux/runState';
 import Spritelab from '@cdo/apps/gamelab/spritelab/Spritelab';
 import Sounds from '@cdo/apps/Sounds';
 
 describe('Spritelab Preview', () => {
+  beforeEach(stubRedux);
+  afterEach(restoreRedux);
   let gamelab, muteSpy;
   beforeEach(function() {
+    registerReducers({...commonReducers, ...reducers});
     gamelab = sinon.spy();
     gamelab.areAnimationsReady_ = sinon.stub().returns(true);
     gamelab.gameLabP5 = sinon.spy();
@@ -29,5 +41,12 @@ describe('Spritelab Preview', () => {
     spritelab.preview.apply(gamelab);
 
     expect(muteSpy).to.have.been.calledOnce;
+  });
+  it('Preview does not run if code is running', () => {
+    let spritelab = new Spritelab();
+    getStore().dispatch(setIsRunning(true));
+    spritelab.preview.apply(gamelab);
+
+    expect(muteSpy).to.not.have.been.called;
   });
 });
