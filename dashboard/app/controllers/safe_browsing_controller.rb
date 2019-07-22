2b3beeff2c7ee53d4384487f72b25c37b9963728
Jessica Kulwik <jessica@code.org>
2019-07-22 20:01:21 +0000

    Stop logging an error message if an empty string is passed to the safe browsing api

--- dashboard/app/controllers/safe_browsing_controller.rb
+++ dashboard/app/controllers/safe_browsing_controller.rb
@@ -2,8 +2,7 @@ require 'cdo/safe_browsing'
 
 class SafeBrowsingController < ApplicationController
   def safe_to_open
-    result = SafeBrowsing.determine_safe_to_open(params[:url])
-
+    result = params[:url].blank? ? true : SafeBrowsing.determine_safe_to_open(params[:url])
     render json: {approved: result}
   end
 end
