2792d6b28ccba8fce7a4ff9f547b983139b84104
Jessica Kulwik <jessica@code.org>
2019-07-23 00:30:06 +0000

    Fixed long-standing issue where scroll bar would display behind the gutter.

--- apps/lib/droplet/droplet-full.js
+++ apps/lib/droplet/droplet-full.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2019 Anthony Bau.
  * MIT License.
  *
- * Date: 2019-07-20
+ * Date: 2019-07-23
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.droplet = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
@@ -6240,6 +6240,7 @@ exports.Editor = Editor = (function() {
     this.sideScroller.style.position = 'absolute';
     this.sideScroller.style.top = 0;
     this.sideScroller.style.left = 0;
+    this.sideScroller.style.zIndex = 5;
     this.paletteWrapper = document.createElement('div');
     this.paletteWrapper.className = 'droplet-palette-wrapper';
     this.paletteElement = document.createElement('div');
