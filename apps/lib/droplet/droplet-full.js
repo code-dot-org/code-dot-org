27e2502871e97dc874ead7c3ab651dc5e86d9708
Jessica Kulwik <jessica@code.org>
2019-07-24 18:05:30 +0000

    Removed unneeded change in palette visibility

--- apps/lib/droplet/droplet-full.js
+++ apps/lib/droplet/droplet-full.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2019 Anthony Bau.
  * MIT License.
  *
- * Date: 2019-07-23
+ * Date: 2019-07-24
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.droplet = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
@@ -9727,12 +9727,10 @@ Editor.prototype.enablePalette = function(enabled) {
           _this.currentlyAnimating = false;
           _this.resize();
           _this.resizeNubby();
-          _this.paletteWrapper.style.visibility = "hidden";
           return _this.fireEvent('palettetoggledone', [_this.session.paletteEnabled]);
         };
       })(this)), 500);
     } else {
-      this.paletteWrapper.style.visibility = "visible";
       this.paletteWrapper.style.top = '0px';
       this.paletteHeader.style.zIndex = 257;
       return setTimeout(((function(_this) {
