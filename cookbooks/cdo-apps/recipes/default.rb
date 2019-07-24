df76edc19e976d764371b835104f1713c1627997
Bethany Connor <bethany@code.org>
2019-07-24 23:34:05 +0000

    Install Tippecanoe on Daemon servers

--- cookbooks/cdo-apps/recipes/default.rb
+++ cookbooks/cdo-apps/recipes/default.rb
@@ -115,8 +115,11 @@ include_recipe 'cdo-i18n' if node.name == 'i18n'
 
 # Production analytics utilities.
 include_recipe 'cdo-analytics' if %w[production-daemon production-console].include?(node.name)
+include_recipe 'cdo-tippecanoe' if %w[production-daemon production-console].include?(node.name)
 
 # Daemon-specific configuration for SSH access to frontend instances.
 include_recipe 'cdo-apps::daemon_ssh' if node['cdo-apps']['daemon'] && node['cdo-apps']['frontends']
 
 include_recipe 'cdo-apps::lighthouse' if node.chef_environment == 'test'
+
+include_recipe 'cdo-tippecanoe' if node['cdo-apps']['daemon']
