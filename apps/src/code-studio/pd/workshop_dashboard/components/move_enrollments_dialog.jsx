61efaebdfedb8a836d9851420e5e0b6f036c6016
Clare Constantine <clare@code.org>
2019-07-25 16:04:37 -0700

    list which enrollments will be moved in the dialog

--- apps/src/code-studio/pd/workshop_dashboard/components/move_enrollments_dialog.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/move_enrollments_dialog.jsx
@@ -10,13 +10,28 @@ export default class MoveEnrollmentsDialog extends React.Component {
     onConfirm: PropTypes.func
   };
 
+  renderSelectedEnrollments() {
+    const enrollments = this.props.selectedEnrollments.map((enrollment, i) => {
+      return (
+        <li key={i}>
+          {enrollment.first_name} {enrollment.last_name} ({enrollment.email})
+        </li>
+      );
+    });
+
+    return <ul>{enrollments}</ul>;
+  }
+
   render() {
     return (
       <Modal show={this.props.show} onHide={this.props.onCancel}>
         <Modal.Header closeButton>
           <Modal.Title>Move Enrollments</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Move Em!</Modal.Body>
+        <Modal.Body>
+          <h3>You are moving the following enrollments:</h3>
+          {this.renderSelectedEnrollments()}
+        </Modal.Body>
         <Modal.Footer>
           <Button bsStyle="primary" onClick={this.props.onConfirm}>
             Move
