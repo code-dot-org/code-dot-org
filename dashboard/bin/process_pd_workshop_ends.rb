1c85cb77734e8d1457f2350d4acc73a34bfb0155
Brendan Reville <brendan@code.org>
2019-07-26 15:52:06 -0700

    PL: fix comment

--- dashboard/bin/process_pd_workshop_ends.rb
+++ dashboard/bin/process_pd_workshop_ends.rb
@@ -3,7 +3,7 @@ require_relative '../../lib/cdo/only_one'
 require_relative '../config/environment'
 
 # When a workshop is ended, we might need to send exit survey emails.  They need
-# to be sent from production-daemon, and so we did it here.
+# to be sent from production-daemon, and so we do it here.
 
 def main
   Pd::Workshop.process_ends
