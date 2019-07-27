6b65bf10e7d8180384932d1eff7a216e876f36c7
Clare Constantine <clare@code.org>
2019-07-26 17:32:20 -0700

    restrict ability to move enrollments to workshop admins

--- dashboard/app/models/ability.rb
+++ dashboard/app/models/ability.rb
@@ -158,6 +158,7 @@ class Ability
         can :manage, Pd::Application::ApplicationBase
         can :manage, Pd::Application::Facilitator1920Application
         can :manage, Pd::Application::Teacher1920Application
+        can :move, :workshop_enrollments
       end
 
       if user.permission?(UserPermission::PROJECT_VALIDATOR)
