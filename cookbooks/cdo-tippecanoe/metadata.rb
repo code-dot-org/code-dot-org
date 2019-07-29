f373be4038de827f4ab18ce2e3ad2fe5462f5eae
Will Jordan <will@code.org>
2019-07-29 16:02:12 -0700

    update cookbook versions [ci skip]

--- cookbooks/cdo-tippecanoe/metadata.rb
+++ cookbooks/cdo-tippecanoe/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'will@code.org'
 license          'All rights reserved'
 description      'Installs/Upgrades Tippecanoe'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.2'
 
 depends 'ark'
