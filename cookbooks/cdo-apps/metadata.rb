7e37180aaedec2dcdde69c1c3a8caeb645b6a917
Continuous Integration <dev@code.org>
2019-07-30 18:21:18 +0000

    Updated cookbook versions

--- cookbooks/cdo-apps/metadata.rb
+++ cookbooks/cdo-apps/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'will@code.org'
 license          'All rights reserved'
 description      'Installs/Configures cdo-apps'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.353'
+version          '0.2.356'
 
 depends 'apt'
 depends 'build-essential'
