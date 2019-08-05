fd13e04e7125dd944440db3066631cb14015d7de
Erin Bond <erin.bond@code.org>
2019-08-05 14:57:54 -0700

    fix typo and add change_column_null

--- dashboard/db/migrate/20190710165640_add_script_level_id_to_teacher_feedbacks.rb
+++ dashboard/db/migrate/20190710165640_add_script_level_id_to_teacher_feedbacks.rb
@@ -1,5 +1,6 @@
 class AddScriptLeveLIdToTeacherFeedbacks < ActiveRecord::Migration[5.0]
   def change
     add_column :teacher_feedbacks, :script_level_id, :integer
+    change_column_null :teacher_feedbacks, :script_level_id, false
   end
 end
