69ec0991d59ef3ac76f51a66aae4b06664d26b6f
Erin Bond <erin.bond@code.org>
2019-08-06 12:56:54 -0700

    include level_id, student_id and teacher_id not null in migration

--- dashboard/db/migrate/20190805220125_change_teacher_feedbacks_script_level.rb
+++ dashboard/db/migrate/20190805220125_change_teacher_feedbacks_script_level.rb
@@ -1,5 +1,8 @@
 class ChangeTeacherFeedbacksScriptLevel < ActiveRecord::Migration[5.0]
   def change
     change_column_null :teacher_feedbacks, :script_level_id, false
+    change_column_null :teacher_feedbacks, :level_id, false
+    change_column_null :teacher_feedbacks, :student_id, false
+    change_column_null :teacher_feedbacks, :teacher_id, false
   end
 end
