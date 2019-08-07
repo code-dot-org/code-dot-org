4a2d77006f8189659cba0da872b1920f802464b4
Anjali <anjali@code.org>
2019-08-07 13:49:25 -0700

    Switch to using common i18n

--- apps/src/p5lab/AnimationPicker/AnimationPicker.jsx
+++ apps/src/p5lab/AnimationPicker/AnimationPicker.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {createUuid} from '@cdo/apps/utils';
 import {connect} from 'react-redux';
 import BaseDialog from '@cdo/apps/templates/BaseDialog.jsx';
-var msg = require('@cdo/gamelab/locale') || require('@cdo/spritelab/locale');
+var msg = require('@cdo/locale');
 import styles from './styles';
 import {
   hide,
