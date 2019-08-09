faeab7f963c144553523d655bbf4884071c1a541
Maddie Kasula <madelynkasula@gmail.com>
2019-08-09 15:41:20 -0700

    Add POST /level_starter_assets/:id endpoint to upload files

--- dashboard/app/controllers/level_starter_assets_controller.rb
+++ dashboard/app/controllers/level_starter_assets_controller.rb
@@ -1,4 +1,6 @@
 class LevelStarterAssetsController < ApplicationController
+  # TODO: ONLY ALLOW LEVELBUILDERS TO UPLOAD ASSETS
+
   S3_BUCKET = 'cdo-v3-assets'.freeze
   S3_PREFIX = 'starter_assets/'.freeze
 
@@ -6,17 +8,7 @@ class LevelStarterAssetsController < ApplicationController
   def show
     level_channel_id = params[:id]
     starter_assets = get_file_objects(level_channel_id).map do |file_obj|
-      if file_obj.size.zero?
-        nil
-      else
-        filename = filename(level_channel_id, file_obj)
-        {
-          filename: filename,
-          category: file_mime_type(File.extname(filename)),
-          size: file_obj.size,
-          timestamp: file_obj.last_modified
-        }
-      end
+      file_obj.size.zero? ? nil : summarize(level_channel_id, file_obj)
     end.compact
 
     render json: {starter_assets: starter_assets}
@@ -25,18 +17,52 @@ class LevelStarterAssetsController < ApplicationController
   # GET /level_starter_assets/:id/:filename
   # Returns requested file body as an IO stream.
   def file
+    # TODO: THIS BREAKS WHEN FILENAME HAS SPACES
     path = prefix("#{params[:id]}/#{params[:filename]}.#{params[:format]}")
-    file_obj = get_file_object(path)
+    file_obj = get_object(path)
     filename = filename(params[:id], file_obj)
     content_type = file_content_type(File.extname(filename))
     send_data read_file(file_obj), type: content_type, disposition: 'inline'
+  end
+
+  # POST /level_starter_assets/:id
+  def upload
+    # Client expects a single file upload, so raise an error if params[:files] contains more than one file.
+    if params[:files].length > 1
+      raise "One file upload expected. Actual: #{params[:files].length}"
+    end
+
+    upload = params[:files]&.first
+    prefix = prefix("#{params[:id]}/#{upload.original_filename}")
+    file_obj = get_object(prefix)
+    success = file_obj.upload_file(upload.tempfile.path)
+
+    if success
+      render json: summarize(params[:id], file_obj)
+    else
+      return head :unprocessable_entity
+    end
+  end
+
+  #
+  # HELPERS
+  #
+
+  def summarize(level_channel_id, file_obj)
+    filename = filename(level_channel_id, file_obj)
+    {
+      filename: filename,
+      category: file_mime_type(File.extname(filename)),
+      size: file_obj.size,
+      timestamp: file_obj.last_modified
+    }
   end
 
   def read_file(file_obj)
     file_obj.get.body.read
   end
 
-  def get_file_object(path)
+  def get_object(path)
     bucket.object(path)
   end
 
