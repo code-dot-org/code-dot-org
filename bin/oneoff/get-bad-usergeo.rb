395094f072cece1f568199562edfc9bd9c9ee43c
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-08-08 17:26:46 -0700

    create script to generate SQL for finding all bad usergeos

---
+++ bin/oneoff/get-bad-usergeo.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'ipaddr'
+require 'json'
+
+# found via:
+# mysql> select id from user_geos where created_at > "2017-11-17 12:25:11 -0800" limit 1;
+first_id = 23_153_485
+
+# found via:
+# mysql> select id from user_geos where created_at > "2019-05-17 16:34:31 -0700" limit 1;
+last_id = 40_626_687
+
+proxy_ip_ranges = JSON.parse(File.read('lib/cdo/trusted_proxies.json'))['ranges'].map do |ip|
+  IPAddr.new(ip).to_range
+end
+
+puts %(
+SELECT id, INET_ATON(ip_address) AS ip_num FROM user_geos
+  WHERE (
+    id >= #{first_id} AND
+    id <= #{last_id}
+  ) AND (
+    #{proxy_ip_ranges.map do |range|
+      "(ip_num BETWEEN #{range.first.to_i} AND #{range.last.to_i})"
+    end.join(" OR \n    ")}
+  );
+)
