d86c93253ef450a6712ea4f19867ed355bdb7392
Ha Nguyen <ha@code.org>
2019-08-09 16:09:06 -0700

    Fix case sensitive names

--- dashboard/db/migrate/20190808182650_rename_academic_year_workshop_names.rb
+++ dashboard/db/migrate/20190808182650_rename_academic_year_workshop_names.rb
@@ -1,22 +1,22 @@
 class RenameAcademicYearWorkshopNames < ActiveRecord::Migration[5.0]
   CSD_COURSE = 'CS Discoveries'.freeze
   CSD_SUBJECTS = [
-    {form: '1-Day Academic Year, Units 1 and 2', to: 'Workshop 1: Unit 3'},
-    {form: '1-Day Academic Year, Unit 3', to: 'Workshop 2: Unit 4'},
-    {form: '1-Day Academic Year, Units 4 and 5', to: 'Workshop 3: Unit 5'},
-    {form: '1-Day Academic Year, Unit 6', to: 'Workshop 4: Unit 6'},
-    {form: '2-Day Academic Year, Units 1 to 3', to: '2-day, Workshops 1+2: Units 3 and 4'},
-    {form: '2-Day Academic Year, Units 4 to 6', to: '2-day, Workshops 3+4: Units 5 and 6'},
+    {form: '1-day Academic Year, Units 1 and 2', to: 'Workshop 1: Unit 3'},
+    {form: '1-day Academic Year, Unit 3', to: 'Workshop 2: Unit 4'},
+    {form: '1-day Academic Year, Units 4 and 5', to: 'Workshop 3: Unit 5'},
+    {form: '1-day Academic Year, Unit 6', to: 'Workshop 4: Unit 6'},
+    {form: '2-day Academic Year, Units 1 to 3', to: '2-day, Workshops 1+2: Units 3 and 4'},
+    {form: '2-day Academic Year, Units 4 to 6', to: '2-day, Workshops 3+4: Units 5 and 6'},
   ].freeze
 
   CSP_COURSE = 'CS Principles'.freeze
   CSP_SUBJECTS = [
-    {from: '1-Day Academic Year, Units 1 and 2', to: 'Workshop 1: Unit 3'},
-    {from: '1-Day Academic Year, Unit 3', to: 'Workshop 2: Unit 4 and Explore Task'},
-    {from: '1-Day Academic Year, Unit 4 + Explore Prep', to: 'Workshop 3: Unit 5 and Create Task'},
-    {from: '1-Day Academic Year, Unit 5 + Create Prep', to: 'Workshop 4: Unit 5 and Multiple Choice Exam'},
-    {from: '2-Day Academic Year, Units 1 to 3', to: '2-day, Workshops 1+2: Units 3-4 and Explore Task'},
-    {from: '2-Day Academic Year, Units 4 and 5 + AP Prep', to: '2-day, Workshops 3+4: Unit 5, Create Task, and Multiple Choice Exam'},
+    {from: '1-day Academic Year, Units 1 and 2', to: 'Workshop 1: Unit 3'},
+    {from: '1-day Academic Year, Unit 3', to: 'Workshop 2: Unit 4 and Explore Task'},
+    {from: '1-day Academic Year, Unit 4 + Explore Prep', to: 'Workshop 3: Unit 5 and Create Task'},
+    {from: '1-day Academic Year, Unit 5 + Create Prep', to: 'Workshop 4: Unit 5 and Multiple Choice Exam'},
+    {from: '2-day Academic Year, Units 1 to 3', to: '2-day, Workshops 1+2: Units 3-4 and Explore Task'},
+    {from: '2-day Academic Year, Units 4 and 5 + AP Prep', to: '2-day, Workshops 3+4: Unit 5, Create Task, and Multiple Choice Exam'},
   ].freeze
 
   def up
