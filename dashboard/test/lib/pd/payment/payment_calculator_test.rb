a8dfd19213666a3249a2171a7736b9449819e331
Brad Buchanan <brad@code.org>
2019-08-09 17:01:56 -0700

    Use :ended trait in payment_calculator_test

--- dashboard/test/lib/pd/payment/payment_calculator_test.rb
+++ dashboard/test/lib/pd/payment/payment_calculator_test.rb
@@ -9,7 +9,7 @@ module Pd::Payment
       @program_manager = create :workshop_organizer
       @regional_partner = create :regional_partner, program_managers: [@program_manager]
 
-      @csp_workshop = create(:pd_ended_workshop, organizer: @program_manager, course: Pd::Workshop::COURSE_CSP, subject: Pd::Workshop::SUBJECT_CSP_WORKSHOP_1, enrolled_and_attending_users: 20, num_sessions: 2)
+      @csp_workshop = create(:workshop, :ended, organizer: @program_manager, course: Pd::Workshop::COURSE_CSP, subject: Pd::Workshop::SUBJECT_CSP_WORKSHOP_1, enrolled_and_attending_users: 20, num_sessions: 2)
 
       2.times do
         @csp_workshop.facilitators << create(:facilitator)
@@ -26,14 +26,14 @@ module Pd::Payment
     end
 
     test 'Calculate CSF Workshop payment' do
-      workshop = create(:pd_ended_workshop, :funded, course: Pd::Workshop::COURSE_CSF, enrolled_and_attending_users: 20)
+      workshop = create(:workshop, :ended, :funded, course: Pd::Workshop::COURSE_CSF, enrolled_and_attending_users: 20)
       create_passed_levels(workshop.enrollments[0..9])
 
       assert_equal 500, PaymentCalculator.instance.calculate(workshop)
     end
 
     test 'Calculate CSF Workshop with only some teachers who did puzzles' do
-      insufficient_puzzles = create(:pd_ended_workshop, :funded, course: Pd::Workshop::COURSE_CSF, enrolled_and_attending_users: 20)
+      insufficient_puzzles = create(:workshop, :ended, :funded, course: Pd::Workshop::COURSE_CSF, enrolled_and_attending_users: 20)
       create_passed_levels(insufficient_puzzles.enrollments[0..5])
       assert_equal 300, PaymentCalculator.instance.calculate(insufficient_puzzles)
     end
@@ -47,7 +47,7 @@ module Pd::Payment
     end
 
     test 'Error raised if workshop has no regional partner' do
-      unpartnered_workshop = create(:pd_ended_workshop, course: Pd::Workshop::COURSE_CSP, subject: Pd::Workshop::SUBJECT_CSP_WORKSHOP_1)
+      unpartnered_workshop = create(:workshop, :ended, course: Pd::Workshop::COURSE_CSP, subject: Pd::Workshop::SUBJECT_CSP_WORKSHOP_1)
       error = assert_raises(RuntimeError) do
         PaymentCalculator.instance.calculate(unpartnered_workshop)
       end
