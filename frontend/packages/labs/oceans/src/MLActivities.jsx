7ab5cbb2bc262e24e0ace0252bfe436fb4b5bbfe
Brian Jordan <bcjordan@gmail.com>
2019-08-11 23:49:59 -0400

    Add react-bootstrap with version to match code-dot-org/apps. Add simple top level layout, replace <button>s with <Button>s

--- frontend/packages/labs/oceans/src/MLActivities.jsx
+++ frontend/packages/labs/oceans/src/MLActivities.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import RPS from './activities/rps/RPS';
+import Button from 'react-bootstrap/lib/Button';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid';
 
 const Activity = Object.freeze({
   None: 0,
@@ -12,36 +16,44 @@ module.exports = class MLActivities extends React.Component {
   };
 
   render() {
-    return <div>
-      <h1>
-        ML Activities Playground
-      </h1>
-      {
-        this.state.currentActivity !== Activity.None &&
-        <button
-          style={{height: 40}}
-          onClick={() => this.setState({
-            currentActivity: Activity.None
-          })}
-        >
-          Pick Another Activity
-        </button>
-      }
-      {
-        this.state.currentActivity === Activity.None &&
-        <button
-          style={{height: 40}}
-          onClick={() => this.setState({
-            currentActivity: Activity.RPS
-          })}
-        >
-          Pick RPS Activity
-        </button>
-      }
-      {
-        this.state.currentActivity === Activity.RPS &&
-        <RPS/>
-      }
-    </div>;
+    return <Grid fluid>
+      <Row className="show-grid">
+        <Col sm={2}>
+        </Col>
+        <Col sm={8}>
+          <h1>
+            ML Activities Playground
+          </h1>
+          {
+            this.state.currentActivity !== Activity.None &&
+            <Button
+              bsSize="large"
+              onClick={() => this.setState({
+                currentActivity: Activity.None
+              })}
+            >
+              Pick Another Activity
+            </Button>
+          }
+          {
+            this.state.currentActivity === Activity.None &&
+            <Button
+              bsSize="large"
+              onClick={() => this.setState({
+                currentActivity: Activity.RPS
+              })}
+            >
+              Pick RPS Activity
+            </Button>
+          }
+          {
+            this.state.currentActivity === Activity.RPS &&
+            <RPS/>
+          }
+        </Col>
+        <Col sm={2}>
+        </Col>
+      </Row>
+    </Grid>;
   }
 };
--- frontend/packages/labs/oceans/src/activities/rps/RPS.jsx
+++ frontend/packages/labs/oceans/src/activities/rps/RPS.jsx
@@ -5,6 +5,9 @@
  */
 
 import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 
 import SimpleTrainer from '../../utils/SimpleTrainer';
 import Video from '../../utils/Video.js';
@@ -51,52 +54,64 @@ module.exports = class Main extends React.Component {
 
   render() {
     return <div>
-      <video ref={(el) => this.videoElementRef = el} autoPlay="" playsInline="" width={IMAGE_SIZE} height={IMAGE_SIZE}/>
-      <div style={{marginBottom: 10}}>
-        <i>Click each &lsquo;train&rsquo; button to train the computer on one frame from your camera</i><br/>
-      </div>
-      {!!this.state.roundResult && <div style={{marginBottom: 10}}>
-        {`You played...${this.rpsToEmoji(this.state.roundResult.playerPlayed)}`}<br/>
-        {`Computer played ${this.rpsToEmoji(this.state.roundResult.computerPlayed)}`}<br/>
-        {this.state.roundResult.winner > 0 ? 'YOU WIN' : this.state.roundResult.winner === 0 ? 'DRAW' : 'YOU LOSE'}
-      </div>}
-      <button
-        style={{position: 'absolute', left: '300px', top: '180px', width: '80px', height: '40px',}}
-        onClick={() => this.playRound()}
-      >
-        PLAY ROUND
-      </button>
-      {
-        [...Array(NUM_CLASSES).keys()].map((index) => {
-          let exampleCount = this.simpleTrainer.getExampleCount(index);
-          let confidence = this.state.confidencesByClassId[index] * 100;
-          return (<div key={index.toString()} style={{marginBottom: "10px"}}>
-            <button
-              style={{height: '40px'}}
-              onClick={() => {
-                this.trainExample(index);
-              }}
-            >
-              Train {CLASS_NAMES[index]}
-            </button>
-            {!confidence && !!exampleCount && <span style={{fontWeight: this.state.predictedClass === index ? "bold" : "normal"}}>
-              {
-                `${exampleCount} example${ exampleCount !== 1 ? 's' : '' } ${(exampleCount >= MIN_EXAMPLES ? '✅' : '')}`
-              }
-            </span>}
-            {!!confidence && <span style={{fontWeight: this.state.predictedClass === index ? "bold" : "normal"}}>
-              {
-                ` ${exampleCount} example${ exampleCount !== 1 ? 's' : '' } - ${confidence}%`
-              }
-            </span>}
-            {
-              this.state['trainingImages' + index].map((image, i) => {
-                return <img key={i} src={image} width={40} height={40}/>;
-              })
-            }
-          </div>);
-        })
-      }
+      <Row style={{
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+        <Col sm={6}>
+          <video ref={(el) => this.videoElementRef = el} autoPlay="" playsInline="" width={IMAGE_SIZE} height={IMAGE_SIZE}/>
+        </Col>
+        <Col sm={6}>
+          <Button bsSize="large"
+            onClick={() => this.playRound()}
+          >
+            Play Round
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div style={{marginBottom: 10}}>
+            <i>Click each &lsquo;train&rsquo; button to train the computer on one frame from your camera</i><br/>
+          </div>
+          {!!this.state.roundResult && <div style={{marginBottom: 10}}>
+            {`You played...${this.rpsToEmoji(this.state.roundResult.playerPlayed)}`}<br/>
+            {`Computer played ${this.rpsToEmoji(this.state.roundResult.computerPlayed)}`}<br/>
+            {this.state.roundResult.winner > 0 ? 'YOU WIN' : this.state.roundResult.winner === 0 ? 'DRAW' : 'YOU LOSE'}
+          </div>}
+
+          {
+            [...Array(NUM_CLASSES).keys()].map((index) => {
+              let exampleCount = this.simpleTrainer.getExampleCount(index);
+              let confidence = this.state.confidencesByClassId[index] * 100;
+              return (<div key={index.toString()} style={{marginBottom: "10px"}}>
+                <Button
+                  onClick={() => {
+                    this.trainExample(index);
+                  }}
+                >
+                  Train {CLASS_NAMES[index]}
+                </Button>
+                {!confidence && !!exampleCount && <span style={{fontWeight: this.state.predictedClass === index ? "bold" : "normal"}}>
+                  {
+                    `${exampleCount} example${ exampleCount !== 1 ? 's' : '' } ${(exampleCount >= MIN_EXAMPLES ? '✅' : '')}`
+                  }
+                </span>}
+                {!!confidence && <span style={{fontWeight: this.state.predictedClass === index ? "bold" : "normal"}}>
+                  {
+                    ` ${exampleCount} example${ exampleCount !== 1 ? 's' : '' } - ${confidence}%`
+                  }
+                </span>}
+                {
+                  this.state['trainingImages' + index].map((image, i) => {
+                    return <img key={i} src={image} width={40} height={40}/>;
+                  })
+                }
+              </div>);
+            })
+          }
+        </Col>
+      </Row>
     </div>;
   }
 
