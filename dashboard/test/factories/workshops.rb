00b5cf03b1f2d4b969573c7025d0333152248cbf
Brad Buchanan <brad@code.org>
2019-08-15 14:10:40 -0700

    Add a :csf_deep_dive_workshop factory

--- dashboard/test/factories/workshops.rb
+++ dashboard/test/factories/workshops.rb
@@ -129,14 +129,28 @@ FactoryGirl.define do
 
     # CSF Intro, also known as CSF 101
     # Our most common workshop type as of August 2019.
-    #
     factory :csf_intro_workshop do
       subject Pd::Workshop::SUBJECT_CSF_101
       course Pd::Workshop::COURSE_CSF
       location_name 'Walkerville Elementary School'
+
       capacity 30          # Average capacity
       on_map true          # About 60% are on the map
       funded               # About 90% are funded
+      num_sessions 1       # Most have 1 session
+      num_facilitators 1   # Most have 1 facilitator
+      each_session_hours 7 # The most common session length
+    end
+
+    # CSF Deep Dive, also known as CSF 201
+    factory :csf_deep_dive_workshop do
+      subject Pd::Workshop::SUBJECT_CSF_201
+      course Pd::Workshop::COURSE_CSF
+      location_name 'Third Street Elementary School'
+
+      capacity 28          # Average capacity
+      on_map true          # About 63% are on the map
+      funded               # About 87% are funded
       num_sessions 1       # Most have 1 session
       num_facilitators 1   # Most have 1 facilitator
       each_session_hours 7 # The most common session length
