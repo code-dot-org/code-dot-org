0b134495fe91c1bfa4d1038443c89acab89327bb
Winter Dong <winter@code.org>
2019-08-21 15:59:45 -0700

    Fix some home directory paths

--- cookbooks/cdo-github-access/recipes/default.rb
+++ cookbooks/cdo-github-access/recipes/default.rb
@@ -11,14 +11,14 @@ end
 
 apt_package 'git'
 
-cookbook_file "/home/#{node[:current_user]}/.gitconfig" do
+cookbook_file "#{node[:home]}/.gitconfig" do
   source 'gitconfig'
   mode '644'
   user node[:current_user]
   group node[:current_user]
 end
 
-directory "/home/#{node[:current_user]}/.ssh" do
+directory "#{node[:home]}/.ssh" do
   mode '0700'
   user node[:current_user]
   group node[:current_user]
@@ -29,7 +29,7 @@ end
   'id_rsa',
   'id_rsa.pub',
 ].each do |file|
-  template "/home/#{node[:current_user]}/.ssh/#{file}" do
+  template "#{node[:home]}/.ssh/#{file}" do
     source 'file.erb'
     mode '0600'
     user node[:current_user]
