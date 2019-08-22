47604aa7d39372711bbadbf5bb0d55892fb3ab2b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-08-22 16:19:23 -0700

    reconfigure i18n instrumentation to include more info and be disablable via DCDO flag

--- dashboard/config/initializers/i18n_instrument.rb
+++ dashboard/config/initializers/i18n_instrument.rb
@@ -1,26 +1,29 @@
 require 'cdo/firehose'
 
+# Configure I18n instrumentation to log calls to I18n.t to firehose.
+#
+# Can be disabled via DCDO flags
+#
+# see https://github.com/lumoslabs/i18n-instrument for more
 I18n::Instrument.configure do |config|
-  config.on_lookup do |key, value|
-    unless value.blank?
-      key_parts = key.split('.')
-      #puts "Looked up i18n key #{key_parts[1..-1].join('.')} and got value #{value} for locale #{I18n.locale.to_s}, translation key is #{key_parts[0]}"
-      FirehoseClient.instance.put_record(
-        study: 'dashboard_translation_coverage',
-        locale: I18n.locale.to_s,
-        translation_locale: key_parts[0],
-        data: {
-          i18n_key: key_parts[1..-1]
-        }
-      )
-    end
+  config.on_record do |params|
+    # The first "entry" in the key is always the locale; this is redundant for
+    # our purposes since we want to be able to track string usage across
+    # locales, so the first thing we do is pull that out
+    i18n_key = params[:key].
+      split(I18n.default_separator).
+      drop(1).
+      join(I18n.default_separator)
+
+    FirehoseClient.instance.put_record(
+      study: 'dashboard_translation_coverage',
+      event: 'lookup',
+      data_string: i18n_key,
+      data_json: params.to_json
+    )
   end
 
-  #config.on_record do |params|
-  #  puts params.inspect
-  #end
-
   config.on_check_enabled do
-    true
+    DCDO.get('i18n_instrumentation_enabled', true)
   end
 end
