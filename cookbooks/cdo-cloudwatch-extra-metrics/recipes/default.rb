362d2eee0081285dc954bc584daefb3d60b3bdd9
Winter Dong <winter@code.org>
2019-08-22 14:50:06 -0700

    Install rsync (required by cdo-cloudwatch-extra-metrics)

--- cookbooks/cdo-cloudwatch-extra-metrics/recipes/default.rb
+++ cookbooks/cdo-cloudwatch-extra-metrics/recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-apt_package ['unzip', 'libwww-perl', 'libdatetime-perl']
+apt_package ['unzip', 'libwww-perl', 'libdatetime-perl', 'rsync']
 
 ark 'aws-scripts-mon' do
   url 'http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip'
