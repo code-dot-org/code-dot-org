315bbf96bb1f8802e9f66290d8b2b0a1e4ce3f7e
dju90 <bryan@code.org>
2019-08-23 18:18:50 +0000

    adding index to user school infos table on user_id

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190822172626) do
+ActiveRecord::Schema.define(version: 20190823180844) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -1526,6 +1526,7 @@ ActiveRecord::Schema.define(version: 20190822172626) do
     t.datetime "last_confirmation_date", null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.index ["user_id"], name: "index_user_school_infos_on_user_id", using: :btree
   end
 
   create_table "user_scripts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
