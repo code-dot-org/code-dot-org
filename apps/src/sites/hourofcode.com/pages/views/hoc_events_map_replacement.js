d67ab590b1263234fd124c2baf111cccee07a41e
Bethany Connor <bethany@code.org>
2019-08-26 23:33:46 +0000

    Change the HOC map to use a Mapbox hosted style instead of a JS defined one

--- apps/src/sites/hourofcode.com/pages/views/hoc_events_map_replacement.js
+++ apps/src/sites/hourofcode.com/pages/views/hoc_events_map_replacement.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
   var map = new mapboxgl.Map({
     container: 'mapbox-map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/codeorg/cjz36duae88ds1cp7ll7smx6s',
     zoom: 1,
     minZoom: 1,
     center: [-98, 39]
@@ -11,32 +11,7 @@ $(document).ready(function() {
   map.dragRotate.disable();
 
   map.on('load', function() {
-    map.addLayer({
-      id: 'hoc-points',
-      type: 'circle',
-      source: {
-        type: 'vector',
-        url: 'mapbox://codeorg.hoctiles'
-      },
-      'source-layer': 'hoc',
-      layout: {
-        visibility: 'visible'
-      },
-      paint: {
-        'circle-radius': 5,
-        'circle-color': [
-          'match',
-          ['get', 'review'],
-          'approved',
-          '#55D5D5',
-          /* other */ '#CC9966'
-        ],
-        'circle-stroke-width': 1,
-        'circle-stroke-color': '#000000'
-      }
-    });
-
-    map.on('click', 'hoc-points', function(e) {
+    map.on('click', 'hoc-events', function(e) {
       var coordinates = e.features[0].geometry.coordinates.slice();
       var organization_name = e.features[0].properties.organization_name;
 
@@ -52,12 +27,37 @@ $(document).ready(function() {
         .addTo(map);
     });
 
-    map.on('mouseenter', 'hoc-points', function() {
+    map.on('click', 'hoc-special-events', function(e) {
+      var coordinates = e.features[0].geometry.coordinates.slice();
+      var organization_name = e.features[0].properties.organization_name;
+      var event_description = e.features[0].properties.description;
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setText(organization_name + '\n' + event_description)
+        .addTo(map);
+    });
+
+    map.on('mouseenter', 'hoc-events', function() {
+      map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseenter', 'hoc-special-events', function() {
       map.getCanvas().style.cursor = 'pointer';
     });
 
     // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'hoc-points', function() {
+    map.on('mouseleave', 'hoc-events', function() {
+      map.getCanvas().style.cursor = '';
+    });
+
+    map.on('mouseleave', 'hoc-special-events', function() {
       map.getCanvas().style.cursor = '';
     });
   });
