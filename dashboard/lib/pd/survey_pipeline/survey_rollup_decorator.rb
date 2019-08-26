08aeb8abb7b0b1e83bba8794364e3d2009e29cfa
Ha Nguyen <ha@code.org>
2019-08-26 15:24:12 -0700

    Direct academic survey view request to new survey pipeline. Fix minor bugs in decorating facilitator roll-up result.

--- dashboard/lib/pd/survey_pipeline/survey_rollup_decorator.rb
+++ dashboard/lib/pd/survey_pipeline/survey_rollup_decorator.rb
@@ -1,6 +1,6 @@
 module Pd::SurveyPipeline
-  module SurveyRollupDecorator
-    def decorate_facilitator_rollup(data)
+  class SurveyRollupDecorator
+    def self.decorate_facilitator_rollup(data)
       report = {
         facilitators: {},
         current_workshop: data[:current_workshop_id],
@@ -16,47 +16,53 @@ module Pd::SurveyPipeline
       report[:facilitators][facilitator_id] = facilitator_name
 
       # related_workshops: {facilitator_name => Array<Integer>}
-      report[:related_workshops][facilitator_name] = related_workshop_ids
+      report[:related_workshops][facilitator_name] = data[:related_workshop_ids]
 
       # facilitator_response_counts:
       #   {:this_workshop, :all_my_workshops => {factilitator_id => {submission_type => count}}}
       submissions = data[:facilitator_submissions] || data[:workshop_submissions]
-      report[:facilitator_response_counts] = get_submission_counts submissions, data[:facilitator_id], data[:current_workshop_id]
+      report[:facilitator_response_counts] =
+        get_submission_counts submissions, data[:facilitator_id], data[:current_workshop_id]
 
       # Convert question names to the format that the view expects.
-      # There is a different in the way we name sub-question between the controller and the (legacy) view.
-      # In the controller, sub-question name has format <original_question>_<hash_of_sub_question_text>.
-      # In the view, it expects question name in format <original_question>_<index_of_sub_question>.
+      # There is a difference in how we name sub-questions in the controller and in the (legacy) view.
+      # In the controller, sub-question names have <original_question>_<hash_of_sub_question_text> format.
+      # While in the view, it expects question names in format <original_question>_<index_of_sub_question>.
       # Conversion examples:
       # overall_success_<hash_string> -> overall_success_<index_number>
       # teacher_engagement_<hash_string> -> teacher_engagement_<index_number>
       # facilitator_effectiveness_<hash_string> -> facilitator_effectiveness__<index_number>
-      q_name_replacements = get_question_name_replacements data.get_values(:parsed_questions, :question_categories)
+      q_name_replacements =
+        get_question_name_replacements data[:parsed_questions], data[:question_categories]
 
       # Get list of questions to calculate averages.
       # Replace question names if needed.
       # facilitator_averages: {:questions => {question_name => question_text}}
       q_names_to_texts = map_question_name_to_text data[:parsed_questions]
       q_names_to_texts.transform_keys! {|q_name| q_name_replacements[q_name] || q_name}
+      # TODO: remove hidden questions and all questions that are not in summaries
       report[:facilitator_averages][:questions] = q_names_to_texts
 
       # Calculate question averages.
       # Replace question names if needed.
-      # facilitator_averages: {facilitator_name => {question_name => {:this_workshop, :all_my_workshops => score}}}
+      # facilitator_averages:
+      #   {facilitator_name => {question_name => {:this_workshop, :all_my_workshops => score}}}
       question_averages = get_question_averages(data[:summaries], data[:current_workshop_id])
       question_averages.transform_keys! {|q_name| q_name_replacements[q_name] || q_name}
       report[:facilitator_averages][facilitator_name] = question_averages
 
-      # Calculate category averages
-      # facilitator_averages: {facilitator_name => {question_category => {:this_workshop, :all_my_workshops => score}}}
-      report[:facilitator_averages][facilitator_name].merge! get_category_averages(data[:question_categories], question_averages)
+      # Calculate category averages.
+      # facilitator_averages:
+      #   {facilitator_name => {question_category => {:this_workshop, :all_my_workshops => score}}}
+      category_averages = get_category_averages data[:question_categories], question_averages
+      report[:facilitator_averages][facilitator_name].merge! category_averages
 
       report
     end
 
-    private
+    private_class_method
 
-    def get_category_averages(question_categories, question_averages)
+    def self.get_category_averages(question_categories, question_averages)
       result = {}
       question_categories.each do |category|
         # qnames_in_cateogry = qname_replacements.values.select {|val| val.start_with? category}
@@ -78,7 +84,7 @@ module Pd::SurveyPipeline
       result
     end
 
-    def get_question_averages(summaries, current_workshop_id)
+    def self.get_question_averages(summaries, current_workshop_id)
       result = {}
 
       summaries.each do |summary|
@@ -94,21 +100,22 @@ module Pd::SurveyPipeline
       result
     end
 
-    def map_question_name_to_text(parsed_questions)
+    def self.map_question_name_to_text(parsed_questions)
       # Create mappings from question name to question text for all parsed questions
       q_names_to_texts = {}
       parsed_questions&.each_pair do |_, form_questions|
         form_questions.each_pair do |_, q_content|
           # If there are multiple questions with the same name, we assume they are all the same
           # and keep the first one we find.
-          q_names_to_texts[q_content[:name]] = q_content[:text] unless q_names_to_texts.key?(q_content[:name])
+          q_names_to_texts[q_content[:name]] = q_content[:text] unless
+            q_names_to_texts.key?(q_content[:name])
         end
       end
 
       q_names_to_texts
     end
 
-    def get_question_names(parsed_questions)
+    def self.get_question_names(parsed_questions)
       q_names = Set[]
 
       parsed_questions&.each_pair do |_, form_questions|
@@ -120,7 +127,7 @@ module Pd::SurveyPipeline
       q_names.to_a
     end
 
-    def get_question_name_replacements(parsed_questions, question_categories)
+    def self.get_question_name_replacements(parsed_questions, question_categories)
       q_names = get_question_names parsed_questions
 
       # Create replacements for questions in selected categories
@@ -137,11 +144,12 @@ module Pd::SurveyPipeline
       q_name_replacements
     end
 
-    def get_submission_counts(submissions, facilitator_id, current_workshop_id)
+    def self.get_submission_counts(submissions, facilitator_id, current_workshop_id)
       {this_workshop: {}, all_my_workshops: {}}.tap do |result|
         next unless submissions.present?
 
-        submission_type = submissions.first.is_a?(Pd::WorkshopDailySurvey) ? 'Workshop surveys' : 'Facilitator surveys'
+        submission_type =
+          submissions.first.is_a?(Pd::WorkshopDailySurvey) ? 'Workshop surveys' : 'Facilitator surveys'
 
         result[:all_my_workshops][facilitator_id] = {submission_type => submissions.count}
         result[:this_workshop][facilitator_id] =
@@ -149,7 +157,7 @@ module Pd::SurveyPipeline
       end
     end
 
-    def get_user_name_by_id(id)
+    def self.get_user_name_by_id(id)
       User.find(id)&.name || "UserId_#{id}"
     end
   end
