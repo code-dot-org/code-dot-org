677bf4eaacb14a3ef46f58a0e6ed862b5467f8ad
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-08-27 17:13:45 -0700

    update race interstitial helper unit test to take advantage of the fact it's no longer contained in a larger test (use setup, etc)

--- dashboard/test/helpers/race_interstitial_helper_test.rb
+++ dashboard/test/helpers/race_interstitial_helper_test.rb
@@ -6,60 +6,52 @@ class RaceInterstitialHelperTest < ActionView::TestCase
     Geocoder.stubs(:search).returns([mock_us_object])
   end
 
-  test 'do not show race interstitial to teacher' do
+  def setup
     mock_geocoder_result('US')
-    teacher = create :teacher, created_at: DateTime.now - 8
-    refute RaceInterstitialHelper.show?(teacher)
+
+    # Create a student over 13 with an account more than 1 week old with an
+    # associated IP address. We expect that such a user would see the race
+    # interstitial helper.
+    @user = create :student, created_at: DateTime.now - 8
+    @user.current_sign_in_ip = "127.0.0.1"
+  end
+
+  test 'do not show race interstitial to teacher' do
+    @user.user_type = User::TYPE_TEACHER
+    refute RaceInterstitialHelper.show?(@user)
   end
 
   test 'do not show race interstitial to user accounts under 13' do
-    mock_geocoder_result('US')
-    student = create :student, created_at: DateTime.now - 8, age: 8
-    student.created_at = DateTime.now - 8
-    refute RaceInterstitialHelper.show?(student)
+    @user.age = 8
+    refute RaceInterstitialHelper.show?(@user)
   end
 
   test 'do not show race interstitial to user accounts less than one week old' do
-    mock_geocoder_result('US')
-    student = create :student, created_at: DateTime.now - 3
-    refute RaceInterstitialHelper.show?(student)
+    @user.created_at = DateTime.now - 3
+    refute RaceInterstitialHelper.show?(@user)
   end
 
   test 'do not show race interstitial to user accounts that have already entered race information' do
-    mock_geocoder_result('US')
-    student = create :student, created_at: DateTime.now - 8
-    student.update_columns(races: 'white,black')
-    refute RaceInterstitialHelper.show?(student)
+    @user.update_columns(races: 'white,black')
+    refute RaceInterstitialHelper.show?(@user)
   end
 
   test 'do not show race interstitial to user accounts that have closed the dialog already' do
-    mock_geocoder_result('US')
-    student = create :student, created_at: DateTime.now - 8
-    student.update_columns(races: 'closed_dialog')
-    refute RaceInterstitialHelper.show?(student)
+    @user.update_columns(races: 'closed_dialog')
+    refute RaceInterstitialHelper.show?(@user)
   end
 
   test 'do not show race interstitial if IP address is nil' do
-    mock_geocoder_result('US')
-    student = create :student, created_at: DateTime.now - 8, current_sign_in_ip: nil
-    refute RaceInterstitialHelper.show_race_interstitial?(student, mock_ip)
+    @user.current_sign_in_ip = nil
+    refute RaceInterstitialHelper.show?(@user)
   end
 
   test 'do not show race interstitial to non-US users' do
     mock_geocoder_result('CA')
-    student = create :student, created_at: DateTime.now - 8
-    refute RaceInterstitialHelper.show?(student)
+    refute RaceInterstitialHelper.show?(@user)
   end
 
   test 'show race interstitial to US users' do
-    mock_geocoder_result('US')
-    student = create :student, created_at: DateTime.now - 8
-    assert RaceInterstitialHelper.show?(student)
-  end
-
-  test 'show race interstitial for student over 13 with account more than 1 week old' do
-    mock_geocoder_result('US')
-    student = create :student, created_at: DateTime.now - 8
-    assert RaceInterstitialHelper.show?(student)
+    assert RaceInterstitialHelper.show?(@user)
   end
 end
