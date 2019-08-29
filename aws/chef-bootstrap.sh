bca4586f2a698c2e2be872404b6672bd30f3af60
Will Jordan <will@code.org>
2019-08-29 16:46:21 -0700

    update chef version in bootstrap scripts

--- aws/chef-bootstrap.sh
+++ aws/chef-bootstrap.sh
@@ -24,7 +24,7 @@ set -o errexit
 # Set script defaults
 ENVIRONMENT=adhoc
 BRANCH=staging
-CHEF_VERSION=12.7.2
+CHEF_VERSION=15.2.20
 RUN_LIST='["recipe[cdo-apps]"]'
 NODE_NAME=$(hostname)
 S3_BUCKET=cdo-dist
