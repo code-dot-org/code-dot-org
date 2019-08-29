300bab0953061e7c3cc29059d39d06620434b77b
Ha Nguyen <ha@code.org>
2019-08-29 15:51:31 -0700

    Direct academic year workshop to new survey pipeline. Use histogram reducer for scale questions.

--- dashboard/test/controllers/api/v1/pd/workshop_survey_report_controller_test.rb
+++ dashboard/test/controllers/api/v1/pd/workshop_survey_report_controller_test.rb
@@ -364,18 +364,6 @@ module Api::V1::Pd
       assert_response :success
     end
 
-    test 'generic_survey_report: academic-year workshop uses old pipeline' do
-      new_academic_ws = create :academic_year_workshop, started_at: Date.new(2019, 8, 1)
-
-      WorkshopSurveyReportController.any_instance.expects(:create_csf_survey_report).never
-      WorkshopSurveyReportController.any_instance.expects(:local_workshop_daily_survey_report)
-
-      sign_in @admin
-      get :generic_survey_report, params: {workshop_id: new_academic_ws.id}
-
-      assert_response :success
-    end
-
     private
 
     def assert_workshop_survey_report_facilitator_query(user:, expected_facilitator_name_filter:)
