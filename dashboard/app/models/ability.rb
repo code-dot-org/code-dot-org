bc22454bc296c0e013b6ca8fac0e44cbda62052c
David Bailey <davidsbailey@users.noreply.github.com>
2019-08-30 12:39:13 -0700

    allow platformization partners to create and edit new non-dsl levels

--- dashboard/app/models/ability.rb
+++ dashboard/app/models/ability.rb
@@ -230,6 +230,11 @@ class Ability
       can [:upload, :destroy], :level_starter_asset
     end
 
+    if user.persisted?
+      editor_experiment = Experiment.get_editor_experiment(user)
+      can :manage, Level, editor_experiment: editor_experiment
+    end
+
     if user.persisted? && user.permission?(UserPermission::PROJECT_VALIDATOR)
       # let them change the hidden state
       can :manage, LevelSource
