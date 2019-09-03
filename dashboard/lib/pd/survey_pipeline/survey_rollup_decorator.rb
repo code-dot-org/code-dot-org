719a6f0a61acbdd900aaf1a6dbb3630123ba21a5
Ha Nguyen <ha@code.org>
2019-09-03 15:55:43 -0700

    Only count submissions that contributed to category average scores. Add default values for function input params in case required data is missing (defensive). Add file comment. Make function comments shorter and clearer.

--- dashboard/lib/pd/survey_pipeline/survey_rollup_decorator.rb
+++ dashboard/lib/pd/survey_pipeline/survey_rollup_decorator.rb
@@ -1,19 +1,21 @@
+# This decorator combines pieces of survey roll-up data from previous steps of the survey pipeline
+# and organize them in a format that the client view can consume.
+
 module Pd::SurveyPipeline
   class SurveyRollupDecorator
-    # Create a roll up report to send to client view.
+    # Create roll-up report to send to client view.
     #
-    # @param [Hash] data a hash contains all important pieces of information from
-    #   previous steps in the pipeline.
+    # @param [Hash] data a hash contains pieces of information from previous steps in the pipeline.
     #
     # @return [Hash] a hash report contains following keys
     #   :facilitators => {factilitator_id => facilitator_name}
     #   :current_workshop => Integer
     #   :related_workshops: {facilitator_id => Array<Integer>}
-    #   :facilitator_response_counts =>
-    #     {workshop_scope => {factilitator_id => {submission_type => count}}}
     #   :facilitator_averages => a hash with following keys
     #     :questions => {question_name => question_text}
     #     facilitator_name => {question_name_or_category => {workshop_scope => score}}
+    #   :facilitator_response_counts =>
+    #     {workshop_scope => {factilitator_id => {submission_type => count}}}
     #   :errors => Array
     #
     def self.decorate_facilitator_rollup(data)
@@ -21,26 +23,19 @@ module Pd::SurveyPipeline
         facilitators: {},
         current_workshop: data[:current_workshop_id],
         related_workshops: {},
-        facilitator_response_counts: {},
         facilitator_averages: {},
+        facilitator_response_counts: {},
         errors: data[:errors]
       }
 
-      # Get values for :facilitators and :related_workshops keys
+      # Get facilitator name and related workshops
       facilitator_id = data[:facilitator_id]
       facilitator_name = get_user_name_by_id(facilitator_id)
       report[:facilitators][facilitator_id] = facilitator_name
       report[:related_workshops][facilitator_id] = data[:related_workshop_ids]
 
-      # Get value for :facilitator_response_counts key
-      # TODO: count only submissions that contribute to average scores
-      submissions = data[:facilitator_submissions] || data[:workshop_submissions]
-      report[:facilitator_response_counts] =
-        get_submission_counts submissions, data[:facilitator_id], data[:current_workshop_id]
-
-      # Get value for :questions key in :facilitator_averages key.
-      # The value contains all questions in selected categories, even if not all of them have responses.
-      # Also convert question names to the format that the UI expects.
+      # Get all questions in the selected categories, even if not all of them have responses.
+      # Then convert question names to the format that the UI expects.
       # E.g. overall_success_<hash_string> -> overall_success_<index_number>.
       category_questions =
         get_category_questions data[:parsed_questions], data[:question_categories]
@@ -49,15 +44,29 @@ module Pd::SurveyPipeline
       report[:facilitator_averages][:questions] =
         replace_question_name_keys category_questions, q_name_replacements
 
-      # Get question averages for this facilitator in :facilitator_averages key
-      # Replace question names if needed.
+      # Get question average scores
       question_averages = get_question_averages data[:summaries], data[:current_workshop_id]
       report[:facilitator_averages][facilitator_name] =
         replace_question_name_keys question_averages, q_name_replacements
 
-      # Get category averages for this facilitator in :facilitator_averages key
+      # Get category average scores
       category_averages = get_category_averages data[:question_categories], question_averages
       report[:facilitator_averages][facilitator_name].merge! category_averages
+
+      # Count number of submissions contributed to category average scores.
+      submission_type =
+        data[:facilitator_submissions] ? 'Facilitator-specific submissions' : 'Workshop submissions'
+
+      report[:facilitator_response_counts] = {
+        this_workshop: {facilitator_id => {}},
+        all_my_workshops: {facilitator_id => {}}
+      }
+      report[:facilitator_response_counts][:this_workshop][facilitator_id][submission_type] =
+        get_submission_counts(
+          data[:question_answer_joined], data[:question_categories], data[:current_workshop_id]
+        )
+      report[:facilitator_response_counts][:all_my_workshops][facilitator_id][submission_type] =
+        get_submission_counts data[:question_answer_joined], data[:question_categories]
 
       report
     end
@@ -68,35 +77,13 @@ module Pd::SurveyPipeline
       User.find(id)&.name || "UserId_#{id}"
     end
 
-    # Count number of submissions for a specific workshop and for all workshops.
+    # Get all questions in the selected categories.
     #
-    # @param [Array<Pd::WorkshopDailySurvey|Pd::WorkshopFacilitatorId>] submissions
-    # @param [Integer] facilitator_id
-    # @param [Integer] workshop_id
-    # @return [Hash] {workshop_scope => {facilitator_id => {submission_type => count}}
-    #
-    def self.get_submission_counts(submissions, facilitator_id, workshop_id)
-      result = {this_workshop: {facilitator_id => {}}, all_my_workshops: {facilitator_id => {}}}
-
-      if submissions.present?
-        submission_type =
-          submissions.first.is_a?(Pd::WorkshopDailySurvey) ? 'Workshop surveys' : 'Facilitator surveys'
-
-        result[:all_my_workshops][facilitator_id] = {submission_type => submissions.count}
-        result[:this_workshop][facilitator_id] =
-          {submission_type => submissions.where(pd_workshop_id: workshop_id).count}
-      end
-
-      result
-    end
-
-    # Get all questions in selected categories.
-    #
-    # @param [Hash{form_id => {question_id => question_content}}] parsed_questions
+    # @param [Hash] parsed_questions {form_id => {question_id => question_content}}
     # @param [Array<String>] categories
     # @return [Hash] {question_name => question_text}.
     #
-    def self.get_category_questions(parsed_questions, categories)
+    def self.get_category_questions(parsed_questions = {}, categories = [])
       result = {}
       parsed_questions&.each_pair do |_, form_questions|
         form_questions.each_pair do |_, q_content|
@@ -118,16 +105,13 @@ module Pd::SurveyPipeline
     # @param [Array<String>] categories
     # @return [Hash] {old_question_name => new_question_name}
     #
-    def self.get_question_name_replacements(questions, categories)
+    def self.get_question_name_replacements(questions = {}, categories = [])
       q_name_replacements = {}
       categories.each do |category|
-        # Questions in a category could also mean sub-questions of a the same parent question.
-        # In that case, we ignore the parent question and also replace names of the sub-questions.
-        # Sort question names by question texts so order of questions is always in
-        # alphabetical order.
+        # Sort question names by question texts so order of questions is always in alphabetical order.
         sorted_q_names = questions.
           select {|q_name, _| q_name.start_with? "#{category}_"}.
-          sort_by {|_, value| value}.
+          sort_by {|_, q_text| q_text}.
           pluck(0)
 
         sorted_q_names.each_with_index do |q_name, index|
@@ -138,30 +122,30 @@ module Pd::SurveyPipeline
       q_name_replacements
     end
 
-    # Replace keys for an input hash which uses question names as keys.
+    # Replace keys of an input hash which uses question names as keys.
     #
-    # @param [Hash<question_name, value>] question_hash
+    # @param [Hash<question_name, some_value>] question_hash
     # @param [Hash<old_name, new_name>] q_name_replacements
     # @return [Hash] a new hash with the same values as the original input hash
     #   but with the keys replaced.
     #
-    def self.replace_question_name_keys(question_hash, q_name_replacements)
+    def self.replace_question_name_keys(question_hash = {}, q_name_replacements = {})
       question_hash.
         select {|q_name, _| q_name_replacements.key? q_name}.
         transform_keys {|q_name| q_name_replacements[q_name]}
     end
 
-    # Get quetion average results for a specific workshop and for all workshops.
+    # Get question average scores for a specific workshop and for all workshops.
     #
     # @param [Array<Hash>] summaries
     # @param [Integer] workshop_id
-    # @return [Hash] {question_name => {workshop_scope => avg_result}}]
+    # @return [Hash] {question_name => {workshop_scope => avg_score}}]
     #
-    def self.get_question_averages(summaries, workshop_id)
+    def self.get_question_averages(summaries = [], workshop_id)
       result = {}
 
       summaries.each do |summary|
-        # Acceptable values for workshop_id is either nil or current_workshop_id
+        # Acceptable workshop id value is either nil or the specified workshop_id
         next if summary[:workshop_id] && summary[:workshop_id] != workshop_id
 
         scope = summary[:workshop_id] ? :this_workshop : :all_my_workshops
@@ -175,11 +159,11 @@ module Pd::SurveyPipeline
 
     # Get average results of all questions in the same category.
     #
-    # @param [Array<String>] categories
-    # @param [Hash{question_name => {workshop_scope => avg_result}}] question_averages
-    # @return [Hash] {category_name => {workshop_scope => avg_result}}
+    # @param [Array<String>] categories category names
+    # @param [Hash] question_averages {question_name => {workshop_scope => avg_score}}
+    # @return [Hash] {category_name => {workshop_scope => avg_score}}
     #
-    def self.get_category_averages(categories, question_averages)
+    def self.get_category_averages(categories = [], question_averages = {})
       result = {}
       categories.each do |category|
         category_scores = question_averages.select {|q_name, _| q_name.start_with? "#{category}_"}
@@ -194,6 +178,27 @@ module Pd::SurveyPipeline
       end
 
       result
+    end
+
+    # Count number of unique submissions that have answers for any question in
+    # the selected categories.
+    #
+    # @param [Hash] question_with_answers combination of questions and answers
+    # @param [Array<String>] categories category names
+    # @param [Integer] workshop_id limit counting to a specific workshop
+    # @return [Integer] submission count
+    #
+    def self.get_submission_counts(question_with_answers = [], categories = [], workshop_id = nil)
+      submissions_ids = Set[]
+      question_with_answers.each do |qa|
+        next if workshop_id && qa[:workshop_id] != workshop_id
+        next if qa[:answer].blank?
+        next unless categories.any? {|category| qa[:name].start_with? "#{category}_"}
+
+        submissions_ids.add qa[:submission_id]
+      end
+
+      submissions_ids.length
     end
   end
 end
