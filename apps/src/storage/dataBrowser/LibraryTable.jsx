d729b26b86544926dcbc19e4d8943912a9c54509
Anjali <anjali@code.org>
2019-09-05 11:38:19 -0700

    Static import

--- apps/src/storage/dataBrowser/LibraryTable.jsx
+++ apps/src/storage/dataBrowser/LibraryTable.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import Radium from 'radium';
 import React from 'react';
 import FontAwesome from '../../templates/FontAwesome';
+import FirebaseStorage from '../firebaseStorage';
 import msg from '@cdo/locale';
 
 const styles = {
@@ -13,7 +14,8 @@ const styles = {
 class LibraryTable extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired
+    description: PropTypes.string.isRequired,
+    current: PropTypes.bool.isRequired
   };
 
   state = {
@@ -24,6 +26,22 @@ class LibraryTable extends React.Component {
     this.setState({
       collapsed: !this.state.collapsed
     });
+
+  importTable = () => {
+    if (this.props.current) {
+      // TODO
+    } else {
+      FirebaseStorage.copyStaticTable(
+        this.props.name,
+        () => {
+          console.log('success');
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  };
 
   render() {
     const icon = this.state.collapsed ? 'caret-right' : 'caret-down';
@@ -37,7 +55,9 @@ class LibraryTable extends React.Component {
           <div>
             <div>{this.props.description}</div>
             <button type="button"> {msg.preview()} </button>
-            <button type="button"> {msg.import()} </button>
+            <button type="button" onClick={this.importTable}>
+              {msg.import()}
+            </button>
           </div>
         )}
       </div>
