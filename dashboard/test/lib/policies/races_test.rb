01a9976f723994ab45fd71a0b0032314b8ea243f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-09-09 15:10:13 -0700

    rename new policy objects to map directly to their new file structure, to make autoloading work

--- dashboard/test/lib/policies/races_test.rb
+++ dashboard/test/lib/policies/races_test.rb
@@ -1,29 +1,29 @@
 require 'test_helper'
 
-class RacesPolicyTest < ActiveSupport::TestCase
+class RacesTest < ActiveSupport::TestCase
   test 'any_urm? with nil' do
-    assert_nil RacesPolicy.any_urm?(nil)
+    assert_nil Policies::Races.any_urm?(nil)
   end
 
   test 'any_urm? with empty string' do
-    assert_nil RacesPolicy.any_urm?('')
+    assert_nil Policies::Races.any_urm?('')
   end
 
   test 'any_urm? with non-answer responses' do
     %w(opt_out nonsense closed_dialog).each do |response|
-      assert_nil RacesPolicy.any_urm?(response)
+      assert_nil Policies::Races.any_urm?(response)
     end
   end
 
   test 'any_urm? with urm responses' do
     ['white,black', 'hispanic,hawaiian', 'american_indian'].each do |response|
-      assert RacesPolicy.any_urm?(response)
+      assert Policies::Races.any_urm?(response)
     end
   end
 
   test 'any_urm? with non-urm response' do
     ['white', 'white,asian', 'asian'].each do |response|
-      refute RacesPolicy.any_urm?(response)
+      refute Policies::Races.any_urm?(response)
     end
   end
 end
