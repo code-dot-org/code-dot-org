5a2f8563a1098c2ca903c966b0057037faf9215c
Winter Dong <winter@code.org>
2019-09-10 15:36:23 -0700

    Remove commented / debug only code

--- cookbooks/cdo-varnish/test/cookbooks/varnish_test/recipes/default.rb
+++ cookbooks/cdo-varnish/test/cookbooks/varnish_test/recipes/default.rb
@@ -3,8 +3,7 @@ apt_package 'openjdk-7-jre-headless'
 
 require 'etc'
 user = Etc.getlogin
-#home = Etc.getpwnam(user).dir
-home = '/root'
+home = Etc.getpwnam(user).dir
 
 remote_file "#{home}/mock.jar" do
   source 'http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock/1.57/wiremock-1.57-standalone.jar'
