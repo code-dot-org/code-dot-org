06e7ca1bcc235166ba0ff0361f4aa8a3431bc02f
Brad Buchanan <brad@code.org>
2019-09-13 16:52:14 -0700

    Notify honeybadger when deprecated logic is used

--- dashboard/test/controllers/courses_controller_integration_test.rb
+++ dashboard/test/controllers/courses_controller_integration_test.rb
@@ -4,6 +4,7 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
   self.use_transactional_test_case = true
 
   test "show: plc courses get sent to user_course_enrollments_controller" do
+    Honeybadger.expects(:notify).never
     sign_in create :teacher
     # Note: We intentionally use a complex-ish course name here, similar to what
     # our real PLC course names look like.
@@ -14,6 +15,7 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
 
   # We'd like to deprecate this behavior as soon as we're confident nothing depends on it
   test "show: plc course names get titleized" do
+    Honeybadger.expects(:notify)
     sign_in create :teacher
     create :plc_course, name: "My Plc"
     get '/courses/my_plc'
