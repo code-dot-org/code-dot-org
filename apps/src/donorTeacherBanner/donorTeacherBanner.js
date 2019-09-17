7e85339c770b0a58f893d3e21caee08e9e475c44
Brendan Reville <brendan@code.org>
2019-09-17 14:19:01 +1000

    Donors: Teacher banner improvements
    The banner can now be dismissed on pegasus, though it only works if the user is signed in.  The banner will also now be hidden on pegasus if the user is signed in and has previously dismissed it.
    
    Note that the dashboard APIs responsible for this get and set will both return 403 errors if there is not a signed in user, but the user should not see anything amiss unless they're looking carefully at their browser developer tools.
    
    Signed out users will now also see the banner on pegasus.

--- apps/src/donorTeacherBanner/donorTeacherBanner.js
+++ apps/src/donorTeacherBanner/donorTeacherBanner.js
@@ -14,33 +14,40 @@ $(document).ready(initRegionalPartnerSearch);
 
 function showDonorTeacherBanner() {
   const donorTeacherBannerElement = $('#donor-teacher-banner-container');
-  let options;
+  let options = {};
+  let dismissed = false;
 
   $.ajax({
     type: 'GET',
     url: '/dashboardapi/v1/users/me/donor_teacher_banner_details'
   })
     .done(results => {
-      options = {
-        teacherFirstName: results.teacher_first_name,
-        teacherSecondName: results.teacher_second_name,
-        teacherEmail: results.teacher_email,
-        ncesSchoolId: results.nces_school_id,
-        schoolAddress1: results.school_address_1,
-        schoolAddress2: results.school_address_2,
-        schoolAddress3: results.school_address_2,
-        schoolCity: results.school_city,
-        schoolState: results.school_state,
-        schoolZip: results.school_zip
-      };
+      if (results.dismissed) {
+        dismissed = true;
+      } else {
+        options = {
+          teacherFirstName: results.teacher_first_name,
+          teacherSecondName: results.teacher_second_name,
+          teacherEmail: results.teacher_email,
+          ncesSchoolId: results.nces_school_id,
+          schoolAddress1: results.school_address_1,
+          schoolAddress2: results.school_address_2,
+          schoolAddress3: results.school_address_2,
+          schoolCity: results.school_city,
+          schoolState: results.school_state,
+          schoolZip: results.school_zip
+        };
+      }
     })
     .complete(() => {
-      ReactDOM.render(
-        <Provider store={getStore()}>
-          <DonorTeacherBanner options={options} showPegasusLink={false} />
-        </Provider>,
-        donorTeacherBannerElement[0]
-      );
+      if (!dismissed) {
+        ReactDOM.render(
+          <Provider store={getStore()}>
+            <DonorTeacherBanner options={options} showPegasusLink={false} />
+          </Provider>,
+          donorTeacherBannerElement[0]
+        );
+      }
     });
 }
 
