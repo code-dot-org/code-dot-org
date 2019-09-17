9ed247f637d7cdc9871627d11c20dd1ba6a15ae3
Brendan Reville <brendan@code.org>
2019-09-17 15:06:09 +1000

    Donors: Teacher banner works on pegasus when no school
    While the Donor Teacher Banner on dashboard only shows when we have found an appropriate school for that teacher, on pegasus we show the banner under all conditions, including when a teacher hasn't yet set their school.  Now, we cope with the missing school and its missing fields in the pre-population information.

--- dashboard/app/controllers/api/v1/users_controller.rb
+++ dashboard/app/controllers/api/v1/users_controller.rb
@@ -38,18 +38,18 @@ class Api::V1::UsersController < Api::V1::JsonApiController
     if current_user.donor_teacher_banner_dismissed
       render json: {dismissed: true}
     else
-      teachers_school = current_user.school_info.school
+      teachers_school = current_user.last_complete_school_info&.school
       render json: {
         teacher_first_name: current_user.short_name,
         teacher_second_name: current_user.second_name,
         teacher_email: current_user.email,
-        nces_school_id: teachers_school.id,
-        school_address_1: teachers_school.address_line1,
-        school_address_2: teachers_school.address_line2,
-        school_address_3: teachers_school.address_line3,
-        school_city: teachers_school.city,
-        school_state: teachers_school.state,
-        school_zip: teachers_school.zip
+        nces_school_id: teachers_school&.id,
+        school_address_1: teachers_school&.address_line1,
+        school_address_2: teachers_school&.address_line2,
+        school_address_3: teachers_school&.address_line3,
+        school_city: teachers_school&.city,
+        school_state: teachers_school&.state,
+        school_zip: teachers_school&.zip
       }
     end
   end
