3d49d91225b1d8a9cec22d399ad0d357ebcddc03
Dani McAvoy <danimcavoy@gmail.com>
2019-09-19 14:26:48 -0400

    Display answer for contained level in Teacher Only tab

---
+++ apps/src/templates/ContainedLevelAnswer.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import ProtectedStatefulDiv from './ProtectedStatefulDiv';
+
+const styles = {
+  main: {
+    marginBottom: '10px'
+  },
+  hidden: {
+    display: 'none'
+  }
+};
+
+class ContainedLevelAnswer extends Component {
+  // Note: This component modifies portions of the DOM outside of itself upon
+  // mounting. This is generally considered a bad practice, and should not be
+  // copied elsewhere.
+
+  static propTypes = {
+    hidden: PropTypes.bool
+  };
+
+  componentDidMount() {
+    // dashboard provides us our contained level at #containedLevel0
+    // Move it into this component once we mount.
+    const container = $(ReactDOM.findDOMNode(this));
+    $('#containedLevelAnswer0').appendTo(container);
+  }
+
+  render() {
+    return (
+      <div style={this.props.hidden ? styles.hidden : null}>
+        <ProtectedStatefulDiv style={styles.main} />
+      </div>
+    );
+  }
+}
+
+export default ContainedLevelAnswer;
