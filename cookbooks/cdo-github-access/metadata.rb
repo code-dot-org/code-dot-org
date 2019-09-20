9f1c3ccefd7b90c8ea5334ed946bef1c07868a81
Winter Dong <winter@code.org>
2019-09-20 15:49:33 -0700

    Revert "update cookbook versions"
    This reverts commit 77de914243338e4d96f0705c3f8ae18ecd840209.

--- cookbooks/cdo-github-access/metadata.rb
+++ cookbooks/cdo-github-access/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'dev@code.org'
 license          'All rights reserved'
 description      'Installs/Configures cdo-github-access'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.9'
+version          '0.1.8'
