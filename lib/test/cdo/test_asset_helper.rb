9b02582ea2670aa01a069e2854c474f61a085e43
David Bailey <davidsbailey@users.noreply.github.com>
2019-09-23 22:24:56 -0700

    skip manifest lookup in asset helper when using pretty_js

--- lib/test/cdo/test_asset_helper.rb
+++ lib/test/cdo/test_asset_helper.rb
@@ -5,6 +5,7 @@ class AssetHelpersTest < Minitest::Test
   def setup
     AssetHelper.any_instance.stubs(:webpack_manifest_path).returns('./test/fixtures/webpack-manifest.json')
     @asset_helper = AssetHelper.clone.instance
+    CDO.stubs(:pretty_js).returns(false)
   end
 
   def test_valid_asset
@@ -20,5 +21,29 @@ class AssetHelpersTest < Minitest::Test
       @asset_helper.webpack_asset_path('invalid.js')
     end
     assert_equal "Invalid webpack asset name: 'invalid.js'", e.message
+  end
+
+  def test_missing_manifest
+    AssetHelper.any_instance.stubs(:webpack_manifest_path).returns('./test/fixtures/nonexistent.json')
+    assert_raises Errno::ENOENT do
+      @asset_helper.webpack_asset_path('js/cookieBanner.js')
+    end
+  end
+
+  def test_valid_asset_with_pretty_js
+    CDO.stubs(:pretty_js).returns(true)
+    assert_equal(
+      '/blockly/js/cookieBanner.js',
+      @asset_helper.webpack_asset_path('js/cookieBanner.js')
+    )
+  end
+
+  def test_missing_manifest_with_pretty_js
+    CDO.stubs(:pretty_js).returns(true)
+    AssetHelper.any_instance.stubs(:webpack_manifest_path).returns('./test/fixtures/nonexistent.json')
+    assert_equal(
+      '/blockly/js/cookieBanner.js',
+      @asset_helper.webpack_asset_path('js/cookieBanner.js')
+    )
   end
 end
