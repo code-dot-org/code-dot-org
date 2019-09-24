2eacc18ea3348b3d5317769e70c54d8933457733
David Bailey <davidsbailey@users.noreply.github.com>
2019-09-23 22:24:56 -0700

    skip manifest lookup in asset helper when using pretty_js

--- lib/cdo/asset_helper.rb
+++ lib/cdo/asset_helper.rb
@@ -7,12 +7,18 @@ class AssetHelper
     ''
   end
 
-  def initialize
-    @webpack_manifest = JSON.parse(File.read(webpack_manifest_path))
+  def webpack_manifest
+    @webpack_manifest ||= JSON.parse(File.read(webpack_manifest_path))
   end
 
   def webpack_asset_path(asset)
-    path = @webpack_manifest[asset]
+    # Skip manifest lookup in certain environments where the manifest may not be
+    # available. In development, where the webpack bundle has been built in
+    # development mode, this produces valid url to an unminified, undigested
+    # asset. In unit tests, this often just needs to return a stub value without
+    # raising an exception.
+    return "/blockly/#{asset}" if CDO.pretty_js
+    path = webpack_manifest[asset]
     raise "Invalid webpack asset name: '#{asset}'" unless path
     path
   end
