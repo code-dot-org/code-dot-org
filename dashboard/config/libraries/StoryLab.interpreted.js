b81b7420f8350067b98dd38be2531d07649630b7
Continuous Integration <dev@code.org>
2019-09-25 22:18:06 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/libraries/StoryLab.interpreted.js
+++ dashboard/config/libraries/StoryLab.interpreted.js
@@ -1,4 +1,4 @@
-// s is not a sprite object but an id.
+// s is not a sprite object but an id (Number).
 var s = createNewSprite("bear", "bear", {x: 100, y: 100});
 console.log(s);
 var s2 = createNewSprite("bear2", "bear", {x: 250, y: 250});
@@ -11,6 +11,7 @@ function addBehaviorUntilBoolean(spriteID, behavior, condition) {
   }
 }
 
+/*
 function Behavior(func, extraArgs) {
   if (!extraArgs) {
     extraArgs = [];
@@ -21,3 +22,4 @@ function Behavior(func, extraArgs) {
   this.timeStarted = new Date().getTime();
   this.duration = Number.MAX_VALUE;
 }
+*/
