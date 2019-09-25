ebc0995a4de0266cf72571e21cc4b82da33b8361
Will Jordan <will@code.org>
2019-09-25 12:45:56 -0700

    chef fix: install aws-sdk-ec2 gem at compile time [ci skip]

--- cookbooks/cdo-users/metadata.rb
+++ cookbooks/cdo-users/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'will@code.org'
 license          'All rights reserved'
 description      'Creates users for the accounts defined in Chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.26'
+version          '0.1.27'
 
 depends 'apt', '~> 2.6.0'
