80a33f37e625112e35f14f63634d579eb7dac697
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-09-24 17:39:15 -0700

    Isolate user_school_info and school_info queries into their own POROs
    also drop the 'initialize with a scope' implementation

--- dashboard/lib/queries/user_school_info.rb
+++ dashboard/lib/queries/user_school_info.rb
@@ -1,19 +1,10 @@
 class Queries::UserSchoolInfo
-  def initialize(scope = UserSchoolInfo.all)
-    @scope = scope
-  end
-
-  # Most recently created user_school_info referring to a complete
-  # school_info entry
-  def last_complete
-    @scope.
+  def self.last_complete(user)
+    user.
+      user_school_infos.
       includes(:school_info).
       select {|usi| usi.school_info.complete?}.
       sort_by(&:created_at).
       last
-  end
-
-  def self.by_user(user)
-    new(user.user_school_infos)
   end
 end
