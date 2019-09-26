47ed0c309042df75599c9a4caf4d8a8e9b28f6d4
Bethany Connor <bethany@code.org>
2019-09-26 00:11:17 +0000

    Some stability improvements to file upload

--- frontend/packages/labs/oceans/src/activities/imageRecognition/MultiUpload.jsx
+++ frontend/packages/labs/oceans/src/activities/imageRecognition/MultiUpload.jsx
@@ -10,17 +10,23 @@ module.exports = class MultiUpload extends React.Component {
   }
 
   onUpload() {
-    console.log("hi");
     var files = document.getElementById(this.props.className + "file").files;
     for (var i = 0; i < files.length; ++i) {
       var file = files[i];
       if (!file) return;
       var url = URL.createObjectURL(file), // create an Object URL
         img = new Image(); // create a temp. image object
-
       var _this = this;
       img.onload = function() {
+        // The height and width doesn't always load so set them if they're 0
+        if (!img.width) {
+          img.width = 500;
+        }
+        if (!img.height) {
+          img.height = 500;
+        }
         _this.props.addTrainingExample(img);
+        URL.revokeObjectURL(url);
       };
 
       img.src = url; // start convertion file
