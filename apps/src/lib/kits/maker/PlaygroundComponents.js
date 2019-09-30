26d6857aefa348e9e7a40bc6dcc887b8157fffca
Erin Peach <erin@code.org>
2019-09-30 12:07:07 -0700

    Create pathway for re-setting components and include in test

--- apps/src/lib/kits/maker/PlaygroundComponents.js
+++ apps/src/lib/kits/maker/PlaygroundComponents.js
@@ -101,19 +101,24 @@ export function cleanupCircuitPlaygroundComponents(
     components.buzzer.stop();
   }
 
+  //Disable and clear sensors
   if (components.soundSensor) {
+    components.soundSensor.disable();
     components.soundSensor._events = {};
   }
 
   if (components.lightSensor) {
+    components.lightSensor.disable();
     components.lightSensor._events = {};
   }
 
   if (components.tempSensor) {
+    components.tempSensor.disable();
     components.tempSensor._events = {};
   }
 
   if (components.accelerometer) {
+    components.accelerometer.stop();
     components.accelerometer._events = {};
   }
   if (shouldDestroyComponents) {
@@ -139,6 +144,29 @@ export function cleanupCircuitPlaygroundComponents(
         delete components[`touchPad${pin}`];
       });
     }
+  }
+}
+
+/**
+ * Re-initializes sensor components and accelerometer
+ * @param {Object} components - map of components, as originally returned by
+ *   createCircuitPlaygroundComponents.
+ */
+export function enableCircuitPlaygroundComponents(components) {
+  if (components.soundSensor) {
+    components.soundSensor.enable();
+  }
+
+  if (components.lightSensor) {
+    components.lightSensor.enable();
+  }
+
+  if (components.tempSensor) {
+    components.tempSensor.enable();
+  }
+
+  if (components.accelerometer) {
+    components.accelerometer.start();
   }
 }
 
