bef0fb64a9a0df0fdbde04f0ca3fcac492400277
Ha Nguyen <ha@code.org>
2019-09-30 09:39:16 -0700

    Update question ids, workshop id, facilitator ids to make them easier to read. Add course_name.

--- apps/src/code-studio/pd/workshop_dashboard/components/survey_results/survey_rollup_table.story.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/survey_results/survey_rollup_table.story.jsx
@@ -4,69 +4,68 @@ import reactBootstrapStoryDecorator from '../../../reactBootstrapStoryDecorator'
 
 const facilitator_rollups = {
   facilitators: {
-    '1124': 'Facilitator Person 1',
-    '1125': 'Facilitator Person 2'
+    '1': 'Facilitator Person 1',
+    '2': 'Facilitator Person 2'
   },
   questions: {
-    facilitator_effectiveness_1494688268261028618:
-      'Demonstrated knowledge of the curriculum.',
-    facilitator_effectiveness_9145365597108923713:
+    facilitator_effectiveness_0: 'Demonstrated knowledge of the curriculum.',
+    facilitator_effectiveness_1:
       'Built and sustained an equitable learning environment in our workshop.',
-    facilitator_effectiveness_6627891197594983630:
-      'Kept the workshop and participants on track.',
-    facilitator_effectiveness_10623511283632440781:
-      'Supported productive workshop discussions.',
-    facilitator_effectiveness_8015784983354522873:
+    facilitator_effectiveness_2: 'Kept the workshop and participants on track.',
+    facilitator_effectiveness_3: 'Supported productive workshop discussions.',
+    facilitator_effectiveness_4:
       'Helped me see ways to create and support an equitable learning environment for my students.',
-    facilitator_effectiveness_16129913079128962044:
+    facilitator_effectiveness_5:
       'Demonstrated a healthy working relationship with their co-facilitator (if applicable).'
   },
   rollups: {
-    facilitator_1124_single_ws: {
-      facilitator_id: 1124,
-      workshop_id: 347,
+    facilitator_1_single_ws: {
+      facilitator_id: 1,
+      workshop_id: 1,
       response_count: 1,
       averages: {
-        facilitator_effectiveness_1494688268261028618: 1.0,
-        facilitator_effectiveness_6627891197594983630: 4.0,
-        facilitator_effectiveness_8015784983354522873: 3.0,
-        facilitator_effectiveness_16129913079128962044: 1.0,
+        facilitator_effectiveness_0: 1.0,
+        facilitator_effectiveness_2: 4.0,
+        facilitator_effectiveness_4: 3.0,
+        facilitator_effectiveness_5: 1.0,
         facilitator_effectiveness: 2.25
       }
     },
-    facilitator_1124_all_ws: {
-      facilitator_id: 1124,
-      all_workshop_ids: [347],
+    facilitator_1_all_ws: {
+      facilitator_id: 1,
+      all_workshop_ids: [1],
+      course_name: 'CS Principles',
       response_count: 1,
       averages: {
-        facilitator_effectiveness_1494688268261028618: 1.0,
-        facilitator_effectiveness_6627891197594983630: 4.0,
-        facilitator_effectiveness_8015784983354522873: 3.0,
-        facilitator_effectiveness_16129913079128962044: 1.0,
+        facilitator_effectiveness_0: 1.0,
+        facilitator_effectiveness_2: 4.0,
+        facilitator_effectiveness_4: 3.0,
+        facilitator_effectiveness_5: 1.0,
         facilitator_effectiveness: 2.25
       }
     },
-    facilitator_1125_single_ws: {
-      facilitator_id: 1125,
-      workshop_id: 347,
+    facilitator_2_single_ws: {
+      facilitator_id: 2,
+      workshop_id: 1,
       response_count: 1,
       averages: {
-        facilitator_effectiveness_1494688268261028618: 1.0,
-        facilitator_effectiveness_6627891197594983630: 4.0,
-        facilitator_effectiveness_8015784983354522873: 3.0,
-        facilitator_effectiveness_16129913079128962044: 1.0,
+        facilitator_effectiveness_0: 1.0,
+        facilitator_effectiveness_2: 4.0,
+        facilitator_effectiveness_4: 3.0,
+        facilitator_effectiveness_5: 1.0,
         facilitator_effectiveness: 2.25
       }
     },
-    facilitator_1125_all_ws: {
-      facilitator_id: 1125,
-      all_workshop_ids: [347],
+    facilitator_2_all_ws: {
+      facilitator_id: 2,
+      all_workshop_ids: [1],
+      course_name: 'CS Principles',
       response_count: 1,
       averages: {
-        facilitator_effectiveness_1494688268261028618: 1.0,
-        facilitator_effectiveness_6627891197594983630: 4.0,
-        facilitator_effectiveness_8015784983354522873: 3.0,
-        facilitator_effectiveness_16129913079128962044: 1.0,
+        facilitator_effectiveness_0: 1.0,
+        facilitator_effectiveness_2: 4.0,
+        facilitator_effectiveness_4: 3.0,
+        facilitator_effectiveness_5: 1.0,
         facilitator_effectiveness: 2.25
       }
     }
@@ -75,79 +74,81 @@ const facilitator_rollups = {
 
 const workshop_rollups = {
   facilitators: {
-    '1124': 'Facilitator Person 1',
-    '1125': 'Facilitator Person 2'
+    '1': 'Facilitator Person 1',
+    '2': 'Facilitator Person 2'
   },
   questions: {
-    overall_success_2136245491560413670:
+    overall_success_0:
       'I feel more prepared to teach the material covered in this workshop than before I came.',
-    overall_success_9121793211174253549:
+    overall_success_1:
       'I know where to go if I need help preparing to teach this material.',
-    overall_success_5502876428019550646:
+    overall_success_2:
       'This professional development was suitable for my level of experience with teaching CS.',
-    overall_success_3964718812856239438:
+    overall_success_3:
       'I feel connected to a community of computer science teachers.',
-    overall_success_6174705123632039779:
+    overall_success_4:
       'I would recommend this professional development to others',
-    teacher_engagement_8809902359007963123:
+    teacher_engagement_0:
       'I found the activities we did in this workshop interesting and engaging.',
-    teacher_engagement_12316562461560038168:
+    teacher_engagement_1:
       'I was highly active and participated a lot in the workshop activities.',
-    teacher_engagement_8566597854585674670:
+    teacher_engagement_2:
       'I frequently talk about ideas or content from the workshop with others.',
-    teacher_engagement_12383077849665641424:
+    teacher_engagement_3:
       'I am planning to make use of ideas and content covered in this workshop in my classroom.'
   },
   rollups: {
     this_ws: {
-      workshop_id: 347,
+      workshop_id: 1,
       response_count: 1,
       averages: {
-        overall_success_2136245491560413670: 6.0,
-        overall_success_9121793211174253549: 7.0,
-        overall_success_5502876428019550646: 6.0,
-        overall_success_3964718812856239438: 6.0,
-        overall_success_6174705123632039779: 7.0,
-        teacher_engagement_8809902359007963123: 7.0,
-        teacher_engagement_12316562461560038168: 6.0,
-        teacher_engagement_8566597854585674670: 6.0,
-        teacher_engagement_12383077849665641424: 7.0,
+        overall_success_0: 6.0,
+        overall_success_1: 7.0,
+        overall_success_2: 6.0,
+        overall_success_3: 6.0,
+        overall_success_4: 7.0,
+        teacher_engagement_0: 7.0,
+        teacher_engagement_1: 6.0,
+        teacher_engagement_2: 6.0,
+        teacher_engagement_3: 7.0,
         overall_success: 6.4,
         teacher_engagement: 6.5
       }
     },
-    facilitator_1124_all_ws: {
-      facilitator_id: 1124,
-      all_workshop_ids: [347],
+    facilitator_1_all_ws: {
+      facilitator_id: 1,
+      all_workshop_ids: [1],
+      course_name: 'CS Principles',
       response_count: 1,
       averages: {
-        overall_success_2136245491560413670: 6.0,
-        overall_success_9121793211174253549: 7.0,
-        overall_success_5502876428019550646: 6.0,
-        overall_success_3964718812856239438: 6.0,
-        overall_success_6174705123632039779: 7.0,
-        teacher_engagement_8809902359007963123: 7.0,
-        teacher_engagement_12316562461560038168: 6.0,
-        teacher_engagement_8566597854585674670: 6.0,
-        teacher_engagement_12383077849665641424: 7.0,
+        overall_success_0: 6.0,
+        overall_success_1: 7.0,
+        overall_success_2: 6.0,
+        overall_success_3: 6.0,
+        overall_success_4: 7.0,
+        teacher_engagement_0: 7.0,
+        teacher_engagement_1: 6.0,
+        teacher_engagement_2: 6.0,
+        teacher_engagement_3: 7.0,
         overall_success: 6.4,
         teacher_engagement: 6.5
       }
     },
-    facilitator_1125_all_ws: {
-      facilitator_id: 1125,
-      all_workshop_ids: [347],
+    facilitator_2_all_ws: {
+      facilitator_id: 2,
+      all_workshop_ids: [1],
+      course_name: 'CS Principles',
       response_count: 1,
       averages: {
-        overall_success_2136245491560413670: 6.0,
-        overall_success_9121793211174253549: 7.0,
-        overall_success_5502876428019550646: 6.0,
-        overall_success_3964718812856239438: 6.0,
-        overall_success_6174705123632039779: 7.0,
-        teacher_engagement_8809902359007963123: 7.0,
-        teacher_engagement_12316562461560038168: 6.0,
-        teacher_engagement_8566597854585674670: 6.0,
-        teacher_engagement_12383077849665641424: 7.0,
+        overall_success_0: 6.0,
+        overall_success_1: 7.0,
+        overall_success_2: 6.0,
+        overall_success_3: 6.0,
+        overall_success_4: 7.0,
+        teacher_engagement_0: 7.0,
+        teacher_engagement_1: 6.0,
+        teacher_engagement_2: 6.0,
+        teacher_engagement_3: 7.0,
         overall_success: 6.4,
         teacher_engagement: 6.5
       }
