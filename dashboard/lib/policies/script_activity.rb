c571ca09d8d99e373d9f3bd363899f205aa5267e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-10-01 13:59:48 -0700

    update a call to completed? using try

--- dashboard/lib/policies/script_activity.rb
+++ dashboard/lib/policies/script_activity.rb
@@ -1,5 +1,7 @@
 class Policies::ScriptActivity
   def self.completed?(user, script)
+    return false unless user
+    return false unless script
     user_script = user.user_scripts.find_by(script: script)
     return false unless user_script
     !!user_script.completed_at || user.completed_progression_levels?(script)
