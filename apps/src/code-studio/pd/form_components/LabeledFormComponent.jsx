24d3972c1c449e43b4287060fbcb69c92ead8bef
Brendan Reville <brendan@code.org>
2019-10-04 21:26:52 +1000

    PL: Convert teacher application MarkdownSpan -> SafeMarkdown
    Back in https://github.com/code-dot-org/code-dot-org/pull/27841 we attempted converting the markdown-based labels in the teacher application from MarkdownSpan to UnsafeRenderedMarkdown (which is happily now known as SafeMarkdown as of https://github.com/code-dot-org/code-dot-org/pull/29457).  However, the red asterisks for required fields were apparently not showing in the right place, as evidenced by the work in https://github.com/code-dot-org/code-dot-org/pull/28152 to move them to the fronts of the questions.
    
    This change converts the same labels from MarkdownSpan to SafeMarkdown, but this time leaves the red asterisks at the ends of the labels.  It does this by wrapping the label in a div named for whether the field is required or not, and then we use a little CSS trick to put the red asterisk at the end of the last <p> tag generated by the markdown for that label.

--- apps/src/code-studio/pd/form_components/LabeledFormComponent.jsx
+++ apps/src/code-studio/pd/form_components/LabeledFormComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import FormComponent from './FormComponent';
-import MarkdownSpan from '../components/markdownSpan';
+import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
 
 export default class LabeledFormComponent extends FormComponent {
   /**
@@ -16,7 +16,14 @@ export default class LabeledFormComponent extends FormComponent {
       return name;
     }
 
-    return <MarkdownSpan>{this.constructor.labels[name]}</MarkdownSpan>;
+    return (
+      <div style={{display: 'inline-block'}}>
+        <SafeMarkdown
+          openExternalLinksInNewTab
+          markdown={this.constructor.labels[name]}
+        />
+      </div>
+    );
   }
 
   indented(depth = 1) {
