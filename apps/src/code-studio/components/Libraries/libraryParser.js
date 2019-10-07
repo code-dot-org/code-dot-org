137c8b2906c73e3853bcc4bd30731c7d5428e14c
Jessica Kulwik <jessica@code.org>
2019-10-07 22:27:41 +0000

    Updated for CR comments

--- apps/src/code-studio/components/Libraries/libraryParser.js
+++ apps/src/code-studio/components/Libraries/libraryParser.js
@@ -10,12 +10,79 @@ export function getFunctions(code) {
 }
 
 /**
+ * Creates a droplet config for all functions in a library.
+ * The format will look like this:
+ * config: [
+ *   {
+ *     func: libName.funcName
+ *     category: Functions
+ *     params = [param1, param2]
+ *     paletteParams = [param1, param2]
+ *   }
+ *   {
+ *     func: ...
+ *
+ * @param {array} functions The list of functions and metadata to include
+ * @param {string} libraryName The name of the library
+ * @returns {null,object} null if there is an error. Else, the droplet config.
+ */
+function createDropletConfig(functions, libraryName) {
+  let fullConfig = [];
+  for (let currentFunction of functions) {
+    if (!currentFunction.functionName) {
+      return;
+    }
+
+    let individualConfig = {
+      func: `${libraryName}.${currentFunction.functionName}`,
+      category: 'Functions'
+    };
+
+    if (currentFunction.parameters && currentFunction.parameters.length > 0) {
+      individualConfig.params = currentFunction.parameters;
+      individualConfig.paletteParams = currentFunction.parameters;
+    }
+
+    fullConfig.push(individualConfig);
+  }
+
+  return fullConfig;
+}
+
+/**
+ * Creates a closure to contain the library
+ * The format will look like this:
+ * var libraryName = (function() {function myFunc{doSomething}; return {myFunc: myFunc}})();
+ * The library functions can be called with dot notation:
+ * libraryName.myFunc();
+ *
+ * @param {string} code All the code in the library
+ * @param {array} functions All functions that will be exported from the library
+ * @param {string} libraryName The name of the library
+ * @returns {null,string} null if there is an error. Else, the library closure
+ */
+function createLibraryClosure(code, functions, libraryName) {
+  let exportedFunctions = [];
+  for (let currentFunction of functions) {
+    if (!currentFunction.functionName) {
+      return;
+    }
+    exportedFunctions.push(
+      `${currentFunction.functionName}: ${currentFunction.functionName}`
+    );
+  }
+
+  let functionsInClosure = exportedFunctions.join(',');
+  return `var ${libraryName} = (function() {${code}; return {${functionsInClosure}}})();`;
+}
+
+/**
  * Takes the input library elements and creates a json string from them to be
  * uploaded to S3
  * @param {string} code The code that makes up the library
  * @param {array} selectedFunctions The list of functions that will be exported from the library
  * @param {string} libraryName The name of the library to be exported
- * @returns {string} A JSON string representign the library
+ * @returns {null,string} null if there is an error. Else, a JSON string representing the library
  */
 export function createLibraryJson(code, selectedFunctions, libraryName) {
   if (
@@ -27,35 +94,16 @@ export function createLibraryJson(code, selectedFunctions, libraryName) {
     return;
   }
 
-  let exportedFunctions = [];
-  let fullConfig = [];
-  for (let selectedFunction of selectedFunctions) {
-    if (!selectedFunction || !selectedFunction.functionName) {
-      return;
-    }
-    exportedFunctions.push(
-      `${selectedFunction.functionName}: ${selectedFunction.functionName}`
-    );
-    let individualConfig = {
-      func: `${libraryName}.${selectedFunction.functionName}`,
-      category: 'Functions'
-    };
-
-    if (selectedFunction.parameters && selectedFunction.parameters.length > 0) {
-      individualConfig.params = selectedFunction.parameters;
-      individualConfig.paletteParams = selectedFunction.parameters;
-    }
-
-    fullConfig.push(individualConfig);
+  let config = createDropletConfig(selectedFunctions, libraryName);
+  let closure = createLibraryClosure(code, selectedFunctions, libraryName);
+  if (!config || !closure) {
+    return;
   }
-
-  let functionsInClosure = exportedFunctions.join(',');
-  let libraryClosure = `var ${libraryName} = (function() {${code}; return {${functionsInClosure}}})();`;
 
   return JSON.stringify({
     name: libraryName,
-    dropletConfig: fullConfig,
-    source: libraryClosure
+    dropletConfig: config,
+    source: closure
   });
 }
 
