f5cbc1c2f92a64ed0d82109e772a7ffb4139ee87
Maddie Kasula <madelynkasula@gmail.com>
2019-10-07 15:27:26 -0700

    Add KNN data to sprites

--- frontend/packages/labs/oceans/src/utils/fishData.js
+++ frontend/packages/labs/oceans/src/utils/fishData.js
@@ -1,20 +1,7 @@
 import {PropTypes} from 'react';
 
-// return [
-//   body.width,
-//   body.height,
-//   COLORS.indexOf(body.color),
-//   eyes.diameter,
-//   COLORS.indexOf(fins.color),
-//   fins.topFin.widthPercent,
-//   fins.topFin.heightPercent,
-//   fins.sideFin.widthPercent,
-//   fins.sideFin.heightPercent,
-//   fins.tail.widthPercent,
-//   fins.tail.heightPercent
-// ];
-
 const fish = {
+  // BODY KNN DATA: [width (in pixels), height (in pixels), isPointy (0/1 bool)]
   bodies: {
     body1: {
       src: '/images/fish/body_1.png',
@@ -23,7 +10,8 @@ const fish = {
       mouthAnchor: [23, 15],
       sideFinAnchor: [0, 20],
       topFinAnchor: [6, -10],
-      tailAnchor: [-12, 0]
+      tailAnchor: [-12, 0],
+      knnData: [32, 32, 0]
     },
     body2: {
       src: '/images/fish/body_2.png',
@@ -32,28 +20,34 @@ const fish = {
       mouthAnchor: [37, 15],
       sideFinAnchor: [10, 18],
       topFinAnchor: [15, -10],
-      tailAnchor: [-10, -2]
+      tailAnchor: [-10, -2],
+      knnData: [48, 27, 0]
     }
   },
+  // EYE KNN DATA: [width (in pixels), height (in pixels)]
   eyes: {
-    eye1: {src: '/images/fish/eye_1.png'},
-    eye2: {src: '/images/fish/eye_2.png'}
+    eye1: {src: '/images/fish/eye_1.png', knnData: [7, 7]},
+    eye2: {src: '/images/fish/eye_2.png', knnData: [9, 8]}
   },
+  // MOUTH KNN DATA: [hasTeeth (0/1 bool)]
   mouths: {
-    mouth1: {src: '/images/fish/mouth_1.png'},
-    mouth2: {src: '/images/fish/mouth_2.png'}
+    mouth1: {src: '/images/fish/mouth_1.png', knnData: [1]},
+    mouth2: {src: '/images/fish/mouth_2.png', knnData: [0]}
   },
+  // SIDE FIN KNN DATA: [width (in pixels), height (in pixels), isPointy (0/1 bool)]
   sideFins: {
-    sideFin1: {src: '/images/fish/side_fin_1.png'},
-    sideFin2: {src: '/images/fish/side_fin_2.png'}
+    sideFin1: {src: '/images/fish/side_fin_1.png', knnData: [17, 18, 0]},
+    sideFin2: {src: '/images/fish/side_fin_2.png', knnData: [12, 13, 1]}
   },
+  // TOP FIN KNN DATA: [width (in pixels), height (in pixels), isPointy (0/1 bool)]
   topFins: {
-    topFin1: {src: '/images/fish/top_fin_1.png'},
-    topFin2: {src: '/images/fish/top_fin_2.png'}
+    topFin1: {src: '/images/fish/top_fin_1.png', knnData: [16, 15, 0]},
+    topFin2: {src: '/images/fish/top_fin_2.png', knnData: [20, 26, 1]}
   },
+  // TAIL KNN DATA: [width (in pixels), height (in pixels), isPointy (0/1 bool)]
   tails: {
-    tail1: {src: '/images/fish/tail_1.png'},
-    tail2: {src: '/images/fish/tail_2.png'}
+    tail1: {src: '/images/fish/tail_1.png', knnData: [14, 30, 0]},
+    tail2: {src: '/images/fish/tail_2.png', knnData: [14, 28, 1]}
   }
 };
 
