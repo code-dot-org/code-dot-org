726df36cc5eaaea33aebdcedc9fed5360b8dce8c
Ha Nguyen <ha@code.org>
2019-10-09 18:45:00 -0700

    Count number of enrollments to qualified workshops of a regional partner

--- dashboard/app/controllers/api/v1/regional_partners_controller.rb
+++ dashboard/app/controllers/api/v1/regional_partners_controller.rb
@@ -23,11 +23,15 @@ class Api::V1::RegionalPartnersController < ApplicationController
 
   # GET /api/v1/regional_partners/enrolled?role=:role&regional_partner_value=:regional_partner
   def enrolled
-    regional_partner_value = current_user.workshop_admin? ?
-      params[:regional_partner_value] :
-      current_user.regional_partners.first.try(:id)
+    regional_partner_id =
+      if current_user.workshop_admin?
+        # 'none', 'all', nil will become 0
+        params[:regional_partner_value].to_i
+      else
+        current_user.regional_partners.first.try(:id)
+      end
 
-    render json: {enrolled: get_partner_enrolled_cohort(regional_partner_value, params[:role])}
+    render json: {enrolled: get_partner_enrolled_cohort(regional_partner_id, params[:role])}
   end
 
   # GET /api/v1/regional_partners/show/:id
@@ -88,12 +92,39 @@ class Api::V1::RegionalPartnersController < ApplicationController
     nil
   end
 
-  def get_partner_enrolled_cohort(regional_partner_value, role)
-    return if ['none', 'all'].include? regional_partner_value
-    # TODO: remove dummy data
-    # get regional partner object
-    # query workshops of type role
-    # count enrollment
-    11
+  # Get the regional partner's number of enrollments to current-year role-specific workshops.
+  #
+  # @param regional_partner_id [Integer]
+  # @param role [String] a string contains workshop course and subject type, e.g. 'csp_teachers'
+  # @return [Integer]
+  # @see Api::V1::Pd::ApplicationsController:ROLES
+  #
+  def get_partner_enrolled_cohort(regional_partner_id, role)
+    partner = RegionalPartner.find_by_id(regional_partner_id)
+    return unless partner
+
+    workshops = partner.pd_workshops
+    workshops_by_role =
+      case role.to_sym
+      when :csf_facilitators
+        workshops.where(course: COURSE_CSF, subject: SUBJECT_CSF_FIT)
+      when :csd_facilitators
+        workshops.where(course: COURSE_CSD, subject: SUBJECT_CSD_FIT)
+      when :csp_facilitators
+        workshops.where(course: COURSE_CSP, subject: SUBJECT_CSP_FIT)
+      when :csd_teachers
+        workshops.where(course: COURSE_CSD).where.not(subject: SUBJECT_CSD_FIT)
+      when :csp_teachers
+        workshops.where(course: COURSE_CSP).where.not(subject: SUBJECT_CSP_FIT)
+      else
+        # Should never get here. This value will cause an exception on the next calculation,
+        # which expects an ActiveRecord::Relation.
+        nil
+      end
+
+    workshop_current_year_ids =
+      workshops_by_role.select {|ws| ws.school_year == APPLICATION_CURRENT_YEAR}.pluck(:id)
+
+    Pd::Enrollment.where(pd_workshop_id: [workshop_current_year_ids]).count
   end
 end
