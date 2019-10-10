cb9882af5c3e4ed586c986cf440a726836ff92d4
Brendan Reville <brendan@code.org>
2019-10-10 10:39:27 -0700

    PL: MiscSurvey updates
    Update some comments, add optional support for multiple submissions of a given survey for a given user, and add a new survey that uses this.

--- dashboard/app/models/pd/misc_survey.rb
+++ dashboard/app/models/pd/misc_survey.rb
@@ -25,18 +25,19 @@ module Pd
 
     def self.all_form_data
       [
-        {tag: "k5_f_pre",       form_id: "90525022719150", allow_embed: false}, # K-5 Facilitator Pre-Survey
-        {tag: "k5_f_post",      form_id: "90525765349163", allow_embed: false}, # K-5 Facilitator Post-Survey
-        {tag: "612_f_pre",      form_id: "90524894049162", allow_embed: false}, # 6-12 Facilitator Pre-Survey
-        {tag: "612_f_post",     form_id: "90525028928158", allow_embed: false}, # 6-12 Facilitator Post-Survey
-        {tag: "rp_pre",         form_id: "90525686494166", allow_embed: false}, # Regional Partner Pre-Survey
-        {tag: "rp_post",        form_id: "91384165042151", allow_embed: false}, # Regional Partner Post-Survey
+        {tag: "k5_f_pre",       form_id: "90525022719150", allow_embed: false}, # K-5 Facilitator Summit Pre-Survey
+        {tag: "k5_f_post",      form_id: "90525765349163", allow_embed: false}, # K-5 Facilitator Summit Post-Survey
+        {tag: "612_f_pre",      form_id: "90524894049162", allow_embed: false}, # 6-12 Facilitator Summit Pre-Survey
+        {tag: "612_f_post",     form_id: "90525028928158", allow_embed: false}, # 6-12 Facilitator Summit Post-Survey
+        {tag: "rp_pre",         form_id: "90525686494166", allow_embed: false}, # Facilitator summit Regional Partner Pre-Survey
+        {tag: "rp_post",        form_id: "91384165042151", allow_embed: false}, # Facilitator Summit Regional Partner Post-Survey
         {tag: "fit-pre-2019",   form_id: "91826716927166", allow_embed: false}, # 2019 FiT Workshop Pre-Survey
         {tag: "fit-post-2019",  form_id: "91883754303159", allow_embed: false}, # 2019 FiT Workshop Post-Survey
         {tag: "other_workshop", form_id: "91477280965166", allow_embed: true},  # Unofficial Workshop Attendance
         {tag: "virt_k12_f_2019",    form_id: "92175282703154", allow_embed: false}, # 2019 Virtual K-12 Facilitator Survey
         {tag: "virt_ay_post_1920",  form_id: "92174983603160", allow_embed: false}, # 2019-20 Virtual Academic Year Post-Survey
-        {tag: "virt_ay_m1_1920",    form_id: "92175136628158", allow_embed: false}  # 2019-20 Virtual Academic Year Survey Module 1
+        {tag: "virt_ay_m1_1920",    form_id: "92175136628158", allow_embed: false}, # 2019-20 Virtual Academic Year Survey Module 1
+        {tag: "612_f_ay_post",  form_id: "91564407894165", allow_multiple_submissions: true, allow_embed: false} # 6-12 Facilitator Academic Year post-workshop survey
       ]
     end
 
