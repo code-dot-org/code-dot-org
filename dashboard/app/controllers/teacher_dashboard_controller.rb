7a6790944784f4f8a80ef83503058b6ad696bff6
Dani McAvoy <danimcavoy@gmail.com>
2019-10-15 09:57:21 -0400

    Remove unneeded variables in teacher dashbaord controller

--- dashboard/app/controllers/teacher_dashboard_controller.rb
+++ dashboard/app/controllers/teacher_dashboard_controller.rb
@@ -1,10 +1,8 @@
 class TeacherDashboardController < ApplicationController
-  load_and_authorize_resource :section, :course
+  load_and_authorize_resource :section
 
   def show
     @section_summary = @section.summarize
-    @section = @section
-    @course = @course
     @sections = current_user.sections.map(&:summarize)
   end
 end
