9845d7a80141d8e20ad4574ec270540f2e1dd5c3
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-10-15 13:01:21 -0700

    add hoc-specific sync code to the standard sync-up

--- bin/i18n/sync-codeorg-up.rb
+++ bin/i18n/sync-codeorg-up.rb
@@ -1,15 +1,16 @@
 #!/usr/bin/env ruby
 
-# Uploads all English source files from i18n/locales/source to Code.org
-# project, and all source files from pegasus/sites.v3/code.org/public to
-# Code.org - Markdown project on Crowdin.
-# https://crowdin.com/project/codeorg
+# Uploads English source files to Crowdin for all our projects:
+# 1. from i18n/locales/source to Code.org project
+# 2. from pegasus/sites.v3/code.org/public to Code.org - Markdown project
+# 3. from i18n/locales/source/hourofcode to Hourofcode project
 
 require_relative 'i18n_script_utils'
 
 def sync_up
   system "crowdin --config #{CODEORG_CONFIG_FILE} --identity #{CODEORG_IDENTITY_FILE} upload sources"
   system "crowdin --config #{CODEORG_MARKDOWN_CONFIG_FILE} --identity #{CODEORG_MARKDOWN_IDENTITY_FILE} upload sources"
+  system "crowdin --config #{HOUROFCODE_CONFIG_FILE} --identity #{HOUROFCODE_IDENTITY_FILE} upload sources"
 end
 
 sync_up if __FILE__ == $0
