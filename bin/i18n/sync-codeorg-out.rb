ec12a81d00db999f79f44f1c6d7a3a9878a2a374
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-10-15 13:46:34 -0700

    extract sync-hourofcode-out code into standalone utils class and add to standard sync out

--- bin/i18n/sync-codeorg-out.rb
+++ bin/i18n/sync-codeorg-out.rb
@@ -13,6 +13,7 @@ require 'yaml'
 
 require_relative 'i18n_script_utils'
 require_relative 'redact_restore_utils'
+require_relative 'hoc_sync_utils'
 
 CLEAR = "\r\033[K"
 
@@ -24,6 +25,7 @@ def sync_out
   rebuild_blockly_js_files
   puts "updating TTS I18n (should usually take 2-3 minutes, may take up to 15 if there are a whole lot of translation updates)"
   I18nScriptUtils.run_standalone_script "dashboard/scripts/update_tts_i18n.rb"
+  HocSyncUtils.sync_out
 end
 
 # Files downloaded from Crowdin are organized by language name,
