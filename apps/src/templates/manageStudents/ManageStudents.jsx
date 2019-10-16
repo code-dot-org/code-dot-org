d3a344791606b164b9be634ffa469831fd896acd
Dani McAvoy <danimcavoy@gmail.com>
2019-10-16 08:59:31 -0400

    Improve check to determine if we should reload the data

--- apps/src/templates/manageStudents/ManageStudents.jsx
+++ apps/src/templates/manageStudents/ManageStudents.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import ManageStudentsTable from './ManageStudentsTable';
 import SyncOmniAuthSectionControl from '@cdo/apps/lib/ui/SyncOmniAuthSectionControl';
-import {sectionStudentsDataOnServer} from '@cdo/apps/templates/manageStudents/manageStudentsRedux';
+import {loadSectionStudentData} from '@cdo/apps/templates/manageStudents/manageStudentsRedux';
 import Spinner from '@cdo/apps/code-studio/pd/components/spinner';
 
 class ManageStudents extends React.Component {
@@ -14,11 +14,11 @@ class ManageStudents extends React.Component {
     // Provided by redux
     sectionId: PropTypes.number,
     isLoadingStudents: PropTypes.bool.isRequired,
-    sectionStudentsDataOnServer: PropTypes.func.isRequired
+    loadSectionStudentData: PropTypes.func.isRequired
   };
 
   componentDidMount() {
-    this.props.sectionStudentsDataOnServer(this.props.sectionId);
+    this.props.loadSectionStudentData(this.props.sectionId);
   }
 
   render() {
@@ -54,8 +54,8 @@ export default connect(
     isLoadingStudents: state.manageStudents.isLoadingStudents
   }),
   dispatch => ({
-    sectionStudentsDataOnServer(sectionId) {
-      dispatch(sectionStudentsDataOnServer(sectionId));
+    loadSectionStudentData(sectionId) {
+      dispatch(loadSectionStudentData(sectionId));
     }
   })
 )(ManageStudents);
