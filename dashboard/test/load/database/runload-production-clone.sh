12c94f1d065f3495fd04837a8c1d18c44743684c
Winter Dong <winter@code.org>
2019-10-17 22:02:57 +0000

    Extract setup steps to separate script

--- dashboard/test/load/database/runload-production-clone.sh
+++ dashboard/test/load/database/runload-production-clone.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Before running this script for the first time, run setup.sh.
+
 set -xe
 
 PASSWORD=$3
@@ -10,23 +12,7 @@ cd /home/ec2-user
 # Adjust process capacity
 ulimit -u 16384
 
-# Install / Update tools
-sudo yum -y -q update
-sudo yum -y -q install automake
-sudo yum -y -q install libtool
-sudo yum -y -q install mysql-devel
-sudo yum -y -q install mysql
-sudo yum -y -q install git
-
-# Download the sysbench source code
-sudo rm sysbench -rf
-git clone --branch 1.0.16 https://github.com/akopytov/sysbench.git
-
-# Build sysbench
 cd sysbench
-sudo ./autogen.sh
-sudo ./configure
-sudo make
 
 # Copy any code-dot-org load test scripts into the sysbench lua directory.
 # TODO:(suresh) Pull the latest from origin in the code-dot-org project
@@ -42,48 +28,18 @@ max_db_connections=5000
 threads=20
 
 # Each sysbench thread opens one database connection.
-let number_of_sysbench_clients=max_db_connections/threads
+let num_sysbench_clients=max_db_connections/threads
 
 # Drop / create the sysbench schema
 mysql -h$1 -udb -p$PASSWORD -e "drop schema if exists sysbench;create schema sysbench;"
 
 # Execute tests
-if [ "$2" == "oltp_read_only.lua" ]
-then
-	# Prepare for read-only test
-	/home/ec2-user/sysbench/src/sysbench ./$2 --mysql-host=$1 --mysql-port=3306 --mysql-db=sysbench --mysql-user=db --mysql-password=$PASSWORD --db-driver=mysql --tables=250 --table-size=25000 --threads=250 prepare
+# Launch multiple sysbench clients
+for i in $(seq 1 $num_sysbench_clients)
+do
+	# Execute write-only test
+	(/home/ec2-user/sysbench/src/sysbench ./$2 --mysql-host=$1 --mysql-port=3306 --mysql-db=sysbench --mysql-user=db --mysql-password=$PASSWORD --db-driver=mysql --threads=$threads --time=86400 --mysql-ignore-errors=all run)&
 
-	# Launch multiple sysbench clients
-	for i in {1..$number_of_sysbench_clients}
-	do
-		# Execute read-only test
-		(/home/ec2-user/sysbench/src/sysbench ./$2 --mysql-host=$1 --mysql-port=3306 --mysql-db=sysbench --mysql-user=db --mysql-password=$PASSWORD --db-driver=mysql --tables=250 --table-size=25000 --threads=$threads --time=86400 --range_selects=off --db-ps-mode=disable --skip_trx=on run)&
-
-		# Sleep one second
-		sleep 1
-	done
-elif [[ "$2" =~ cdo.*\.lua ]]
-then
-	# Launch multiple sysbench clients
-	for i in {1..$number_of_sysbench_clients}
-	do
-		# Execute write-only test
-		(/home/ec2-user/sysbench/src/sysbench ./$2 --mysql-host=$1 --mysql-port=3306 --mysql-db=sysbench --mysql-user=db --mysql-password=$PASSWORD --db-driver=mysql --threads=$threads --time=86400 --mysql-ignore-errors=all run)&
-
-		# Sleep one second
-		sleep 1
-	done
-else
-	# Prepare for write-only test
-	/home/ec2-user/sysbench/src/sysbench ./$2 --mysql-host=$1 --mysql-port=3306 --mysql-db=sysbench --mysql-user=db --mysql-password=$PASSWORD --db-driver=mysql --tables=250 --table-size=25000 --threads=250 --auto-inc=off prepare
-
-	# Launch multiple sysbench clients
-	for i in {1..$number_of_sysbench_clients}
-	do
-		# Execute write-only test
-		(/home/ec2-user/sysbench/src/sysbench ./$2 --mysql-host=$1 --mysql-port=3306 --mysql-db=sysbench --mysql-user=db --mysql-password=$PASSWORD --db-driver=mysql --tables=250 --table-size=25000 --threads=$threads --time=86400 --range_selects=off --db-ps-mode=disable --skip_trx=off --auto-inc=off --mysql-ignore-errors=all run)&
-
-		# Sleep one second
-		sleep 1
-	done
-fi
+	# Sleep one second
+	sleep 1
+done
