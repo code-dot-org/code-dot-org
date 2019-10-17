6c6f75b34fa8d9924cf76aab8c40de5529701d32
Jessica Kulwik <jessica@code.org>
2019-10-17 22:55:03 +0000

    Added permissions for students to view the list of their classmates projects.

--- dashboard/app/models/ability.rb
+++ dashboard/app/models/ability.rb
@@ -81,6 +81,11 @@ class Ability
       can :index, Section, user_id: user.id
       can [:get_feedbacks, :count, :increment_visit_count, :index], TeacherFeedback, student_id: user.id
 
+      can :list_projects, Section do |section|
+        return true if can? :manage, section
+        user.sections_as_student.include?(section)
+      end
+
       if user.teacher?
         can :manage, Section, user_id: user.id
         can :manage, :teacher
