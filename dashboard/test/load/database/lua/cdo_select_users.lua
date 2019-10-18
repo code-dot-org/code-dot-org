6821c3052b7459327d8c35e8bd2ca0e9445f40e1
Winter Dong <winter@code.org>
2019-10-18 05:16:59 +0000

    Use string interpolation with query template

--- dashboard/test/load/database/lua/cdo_select_users.lua
+++ dashboard/test/load/database/lua/cdo_select_users.lua
@@ -1,5 +1,7 @@
 #!/usr/bin/env sysbench
 
+-- init/teardown code taken from: https://www.percona.com/blog/2019/04/25/creating-custom-sysbench-scripts/
+
 -- Called by sysbench one time to initialize this script
 function thread_init()
  
@@ -20,16 +22,21 @@ end
  
 
 function event()
-  -- TODO:(suresh) Run this on initialization of the thread.
+  -- TODO:(suresh) Declare these as local variables?  Switch to sb_rand_unique()?
   local max_user_level_id = 2098563437
   local max_user_id = 50826205
   local max_level_id = 17741
   local max_script_id = 405
   local max_level_source_id = 861968845
   
-  -- TODO:(suresh) Declare these as local variables?  Switch to sb_rand_unique()?
-  local user_id = sb_rand(1, max_user_level_id)
-  local query = "SELECT * FROM dashboard_production.users WHERE deleted_at is null AND id = " .. user_id .. 
-  " ORDER BY id ASC limit 1"
+  local vars = {
+    userId = sb_rand(1, max_user_level_id)
+  }
+
+  local query_template = "SELECT * FROM dashboard_production.users WHERE deleted_at is null AND id = $userId ORDER BY id ASC limit 1"
+  -- Simple string interpolation from: https://hisham.hm/2016/01/04/string-interpolation-in-lua/
+  -- "variable" names must be alphanumeric characters only.
+  local query = string.gsub(query_template, "%$(%w+)", vars)
+
   con:query(query)
 end
