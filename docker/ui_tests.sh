787bf5a12c0c6c068d24d683742766be198e6cb0
David Bailey <davidsbailey@users.noreply.github.com>
2019-10-21 07:44:14 -0700

    apply all locals.yml settings before building drone ui tests

--- docker/ui_tests.sh
+++ docker/ui_tests.sh
@@ -53,18 +53,6 @@ dashboard_enable_pegasus: true
 dashboard_workers: 5
 skip_seed_all: true
 optimize_rails_assets: true
-" >> locals.yml
-echo "Wrote secrets from env vars into locals.yml."
-set -x
-
-# name: rake install
-RAKE_VERBOSE=true bundle exec rake install --trace
-
-# name: rake build
-RAKE_VERBOSE=true bundle exec rake build --trace
-
-# apply test settings for after unit tests
-echo "
 no_https_store: true
 override_dashboard: \"localhost-studio.code.org\"
 override_pegasus: \"localhost.code.org\"
@@ -75,6 +63,14 @@ assets_s3_directory: assets_circle/$CIRCLE_BUILD_NUM
 files_s3_directory: files_circle/$CIRCLE_BUILD_NUM
 sources_s3_directory: sources_circle/$CIRCLE_BUILD_NUM
 " >> locals.yml
+echo "Wrote secrets from env vars into locals.yml."
+set -x
+
+# name: rake install
+RAKE_VERBOSE=true bundle exec rake install --trace
+
+# name: rake build
+RAKE_VERBOSE=true bundle exec rake build --trace
 
 # name: seed ui tests
 bundle exec rake circle:seed_ui_test --trace
