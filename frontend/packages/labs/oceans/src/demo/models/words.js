0776ef3080fb41a20f8a8fc0ccc5703b66d0e1d6
Maddie Kasula <madelynkasula@gmail.com>
2019-10-21 15:30:01 -0700

    Prettier on words.js

--- frontend/packages/labs/oceans/src/demo/models/words.js
+++ frontend/packages/labs/oceans/src/demo/models/words.js
@@ -7,21 +7,48 @@ import {init as initScene} from '../init';
 const onChangeWord = event => {
   setState({word: event.target.value, currentMode: Modes.Training});
   initScene();
-}
+};
 
 const createDropdowns = () => {
   const listData = [
-    { type: "color", title: "Color", options: ["blue", "green", "red", "yellow"] },
-    { type: "shape", title: "Shape", options: ["narrow", "round", "sharp", "square", "wide", "spiky"] },
-    { type: "type", title: "Type", options: ["bizarre", "fast", "scary", "funny", "glitchy"] },
-    { type: "personality", title: "Personality", options: ["angry", "brave", "fierce", "friendly", "fun", "shy", "smart", "wild"] }
+    {
+      type: 'color',
+      title: 'Color',
+      options: ['blue', 'green', 'red', 'yellow']
+    },
+    {
+      type: 'shape',
+      title: 'Shape',
+      options: ['narrow', 'round', 'sharp', 'square', 'wide', 'spiky']
+    },
+    {
+      type: 'type',
+      title: 'Type',
+      options: ['bizarre', 'fast', 'scary', 'funny', 'glitchy']
+    },
+    {
+      type: 'personality',
+      title: 'Personality',
+      options: [
+        'angry',
+        'brave',
+        'fierce',
+        'friendly',
+        'fun',
+        'shy',
+        'smart',
+        'wild'
+      ]
+    }
   ];
 
   let results = [];
   for (var i = 0; i < 4; i++) {
     const listItem = listData[i];
-    var selectList = document.createElement("select");
-    $(selectList).append(`<option selected disabled>${listItem.title}</option>`);
+    var selectList = document.createElement('select');
+    $(selectList).append(
+      `<option selected disabled>${listItem.title}</option>`
+    );
     for (var j = 0; j < listItem.options.length; j++) {
       const option = listItem.options[j];
       $(selectList).append(`<option value="${option}">${option}</option>`);
@@ -39,5 +66,3 @@ const uiElements = createDropdowns();
 export const init = () => {
   setState({uiElements});
 };
-
-
