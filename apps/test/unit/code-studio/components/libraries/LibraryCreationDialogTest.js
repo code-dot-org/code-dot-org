1b4c852a3ce09c69ed379ad4dac15f127fb5da1c
Erin Peach <erin@code.org>
2019-10-21 16:26:08 -0700

    Add test for description

--- apps/test/unit/code-studio/components/libraries/LibraryCreationDialogTest.js
+++ apps/test/unit/code-studio/components/libraries/LibraryCreationDialogTest.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {mount} from 'enzyme';
 import {UnconnectedLibraryCreationDialog as LibraryCreationDialog} from '@cdo/apps/code-studio/components/libraries/LibraryCreationDialog.jsx';
 import libraryParser from '@cdo/apps/code-studio/components/libraries/libraryParser';
+import LibraryClientApi from '../../../../../src/code-studio/components/libraries/LibraryClientApi';
 
 const LIBRARY_SOURCE =
   '/*\n' +
@@ -27,59 +28,74 @@ const LIBRARY_SOURCE =
 
 describe('LibraryCreationDialog', () => {
   let wrapper;
-  beforeEach(() => {
-    wrapper = mount(
-      <LibraryCreationDialog
-        channelId={'123'}
-        dialogIsOpen={true}
-        onClose={() => {}}
-      />
-    );
-  });
-
-  afterEach(() => {
-    wrapper = null;
-  });
+  let clientApi = new LibraryClientApi('123');
 
   const SUBMIT_SELECTOR = 'input[type="submit"]';
   const CHECKBOX_SELECTOR = 'input[type="checkbox"]';
-
-  it('publish is disabled when nothing checked', () => {
-    wrapper.setState({
-      libraryName: 'testLibrary',
-      librarySource: LIBRARY_SOURCE,
-      loadingFinished: true,
-      sourceFunctionList: libraryParser.getFunctions(LIBRARY_SOURCE)
+  const DESCRIPTION_SELECTOR = 'textarea';
+  describe('UI', () => {
+    beforeEach(() => {
+      wrapper = mount(
+        <LibraryCreationDialog
+          clientApi={clientApi}
+          dialogIsOpen={true}
+          onClose={() => {}}
+        />
+      );
     });
 
-    expect(wrapper.find(SUBMIT_SELECTOR)).to.be.disabled;
-  });
-
-  it('publish is enabled when something is checked', () => {
-    wrapper.setState({
-      libraryName: 'testLibrary',
-      librarySource: LIBRARY_SOURCE,
-      loadingFinished: true,
-      sourceFunctionList: libraryParser.getFunctions(LIBRARY_SOURCE),
-      canPublish: true
+    afterEach(() => {
+      wrapper = null;
     });
 
-    expect(wrapper.find(SUBMIT_SELECTOR)).not.to.be.disabled;
-  });
+    it('publish is disabled when nothing checked', () => {
+      wrapper.setState({
+        libraryName: 'testLibrary',
+        librarySource: LIBRARY_SOURCE,
+        loadingFinished: true,
+        sourceFunctionList: libraryParser.getFunctions(LIBRARY_SOURCE)
+      });
 
-  it('checkbox is diabled for items without comments', () => {
-    wrapper.setState({
-      libraryName: 'testLibrary',
-      librarySource: LIBRARY_SOURCE,
-      loadingFinished: true,
-      sourceFunctionList: libraryParser.getFunctions(LIBRARY_SOURCE)
+      expect(wrapper.find(SUBMIT_SELECTOR)).to.be.disabled;
     });
 
-    expect(wrapper.find(CHECKBOX_SELECTOR).last()).to.be.disabled;
-  });
+    it('publish is enabled when something is checked', () => {
+      wrapper.setState({
+        libraryName: 'testLibrary',
+        librarySource: LIBRARY_SOURCE,
+        loadingFinished: true,
+        sourceFunctionList: libraryParser.getFunctions(LIBRARY_SOURCE),
+        canPublish: true
+      });
 
-  it('displays loading while in the loading state', () => {
-    expect(wrapper.find(SUBMIT_SELECTOR).exists()).to.be.false;
-    expect(wrapper.find('#loading').exists()).to.be.true;
+      expect(wrapper.find(SUBMIT_SELECTOR)).not.to.be.disabled;
+    });
+
+    it('checkbox is disabled for items without comments', () => {
+      wrapper.setState({
+        libraryName: 'testLibrary',
+        librarySource: LIBRARY_SOURCE,
+        loadingFinished: true,
+        sourceFunctionList: libraryParser.getFunctions(LIBRARY_SOURCE)
+      });
+
+      expect(wrapper.find(CHECKBOX_SELECTOR).last()).to.be.disabled;
+    });
+
+    it('displays loading while in the loading state', () => {
+      expect(wrapper.find(SUBMIT_SELECTOR).exists()).to.be.false;
+      expect(wrapper.find('#loading').exists()).to.be.true;
+    });
+
+    it('description is required', () => {
+      wrapper.setState({
+        libraryName: 'testLibrary',
+        librarySource: LIBRARY_SOURCE,
+        loadingFinished: true,
+        sourceFunctionList: libraryParser.getFunctions(LIBRARY_SOURCE)
+      });
+
+      expect(wrapper.find(DESCRIPTION_SELECTOR)).to.be.required;
+    });
   });
 });
