851a3e16e1e7fae1d8e61f0b70a51ccbdf2031c5
Erin Bond <erin.bond@code.org>
2019-10-22 15:50:52 -0700

    functional unassign button

--- apps/src/templates/teacherDashboard/SectionAssigner.jsx
+++ apps/src/templates/teacherDashboard/SectionAssigner.jsx
@@ -1,13 +1,12 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import color from '@cdo/apps/util/color';
 import i18n from '@cdo/locale';
 import {sectionForDropdownShape} from './shapes';
 import TeacherSectionSelector from './TeacherSectionSelector';
 import AssignButton from '@cdo/apps/templates/AssignButton';
+import UnassignButton from '@cdo/apps/templates/UnassignButton';
 import {selectSection} from '@cdo/apps/templates/teacherDashboard/teacherSectionsRedux';
-import FontAwesome from '@cdo/apps/templates/FontAwesome';
 
 const styles = {
   content: {
@@ -19,14 +18,6 @@ const styles = {
     fontFamily: '"Gotham 5r", sans-serif',
     paddingTop: 10,
     paddingBottom: 10
-  },
-  assigned: {
-    color: color.level_perfect,
-    fontSize: 16,
-    fontFamily: '"Gotham 5r", sans-serif',
-    lineHeight: '36px',
-    marginLeft: 10,
-    verticalAlign: 'top'
   }
 };
 
@@ -66,10 +57,7 @@ class SectionAssigner extends Component {
             selectedSection={selectedSection}
           />
           {selectedSection.isAssigned && (
-            <span style={styles.assigned}>
-              <FontAwesome icon="check" />
-              {i18n.assigned()}
-            </span>
+            <UnassignButton sectionId={selectedSection.id} />
           )}
           {!selectedSection.isAssigned && showAssignButton && (
             <AssignButton
