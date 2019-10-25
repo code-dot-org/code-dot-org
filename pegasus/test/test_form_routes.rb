ee8bf86bf8bc2ae951a0e35dafa3a54579b2a21a
Brad Buchanan <brad@code.org>
2019-10-25 12:33:35 -0700

    Prefer more recent results on the volunteer map

--- pegasus/test/test_form_routes.rb
+++ pegasus/test/test_form_routes.rb
@@ -1,5 +1,6 @@
 # Tests for the routes in form_routes.rb
 
+require 'timecop'
 require_relative './test_helper'
 require_relative 'fixtures/fake_dashboard'
 require_relative 'fixtures/mock_pegasus'
@@ -30,30 +31,57 @@ class FormRoutesTest < SequelTestCase
     end
   end
 
-  DEFAULT_DATA = {
-    email_s: 'fake@example.com',
-    name_s: 'fake_name',
-    experience_s: 'university_student_or_researcher',
-    location_s: 'somewhere',
-    location_flexibility_ss: ['onsite'],
-    description_s: 'description',
-    allow_contact_b: '1',
-    age_18_plus_b: '1',
-    email_preference_opt_in_s: 'yes'
-  }.freeze
+  describe 'volunteer_engineer_submission_2015' do
+    before do
+      # Delete all volunteer sign-ups in the test database before each test
+      ::PEGASUS_DB[:forms].where(kind: 'VolunteerEngineerSubmission2015').delete
 
-  def test_volunteer_engineer_submission_2015
-    stubs(:dashboard_user).returns(nil)
-    Pegasus.stubs(:logger).returns(nil)
-    stubs(:request).returns(stub(ip: '1.2.3.4'))
-    row = insert_or_upsert_form('VolunteerEngineerSubmission2015', DEFAULT_DATA.dup)
-    @form = Form.find(id: row[:id])
+      stubs(:dashboard_user).returns(nil)
+      Pegasus.stubs(:logger).returns(nil)
+      stubs(:request).returns(stub(ip: '1.2.3.4'))
+    end
 
-    @form.update(processed_data: {location_p: '37.774929,-122.419416'}.to_json)
+    it 'returns local results' do
+      create_volunteer name: 'Local Person', location: '37.774929,-122.419416'
+      results = search location: '37.774368,-122.428760'
+      assert_equal 0.8236209090344097, results.first['distance']
+    end
 
-    assert_equal 0.8236209090344097,
+    it 'uses reverse chronological order' do
+      Timecop.freeze 3.years.ago do
+        create_volunteer name: 'First Person', location: '35.774929,-122.419416'
+        Timecop.travel 1.year
+        create_volunteer name: 'Second Person', location: '35.774929,-122.419416'
+        Timecop.travel 1.year
+        create_volunteer name: 'Last Person', location: '35.774929,-122.419416'
+      end
+
+      results = search location: '35.774368,-122.428760'
+      assert_equal ['Last Person', 'Second Person', 'First Person'], results.map {|r| r['name_s']}
+    end
+
+    def create_volunteer(name:, location:)
+      row = insert_or_upsert_form('VolunteerEngineerSubmission2015', DEFAULT_DATA.dup.merge(name_s: name))
+      form = Form.find(id: row[:id])
+      form.update(processed_data: {location_p: location}.to_json)
+    end
+
+    def search(location:)
       JSON.parse(
-        VolunteerEngineerSubmission2015.query('coordinates' => '37.774368,-122.428760')
-      )['response']['docs'].first['distance']
+        VolunteerEngineerSubmission2015.query('coordinates' => location)
+      )['response']['docs']
+    end
+
+    DEFAULT_DATA = {
+      email_s: 'fake@example.com',
+      name_s: 'fake_name',
+      experience_s: 'university_student_or_researcher',
+      location_s: 'somewhere',
+      location_flexibility_ss: ['onsite'],
+      description_s: 'description',
+      allow_contact_b: '1',
+      age_18_plus_b: '1',
+      email_preference_opt_in_s: 'yes'
+    }.freeze
   end
 end
