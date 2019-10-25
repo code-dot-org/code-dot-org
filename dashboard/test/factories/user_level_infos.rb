e9b8191ecdf59998aff2d2a0e8733e34c87366bf
Dani McAvoy <danimcavoy@gmail.com>
2019-10-25 11:06:40 -0400

    Add one test using factory

--- dashboard/test/factories/user_level_infos.rb
+++ dashboard/test/factories/user_level_infos.rb
@@ -9,5 +9,7 @@
 
 FactoryGirl.define do
   factory :user_level_info do
+    time_spent 10
+    user_level_id 1
   end
 end
--- dashboard/test/models/user_level_info_test.rb
+++ dashboard/test/models/user_level_info_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UserLevelInfoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create user_level_info" do
+    user_level_info = create :user_level_info
+    assert_equal user_level_info.user_level_id, 1
+    assert_equal user_level_info.time_spent, 10
+  end
 end
