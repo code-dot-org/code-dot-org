9dd6f0c56c89b729877019e9ebda05249de2641e
Ha Nguyen <ha@code.org>
2019-10-25 13:37:59 -0700

    Move retriever functions from SurveyPipelineHelper to DailySurveyRetriever class

--- dashboard/lib/pd/survey_pipeline/daily_survey_retriever.rb
+++ dashboard/lib/pd/survey_pipeline/daily_survey_retriever.rb
@@ -1,6 +1,6 @@
 module Pd::SurveyPipeline
   class DailySurveyRetriever
-    # Retrieve all survey submissions and questions of a workshop.
+    # Retrieve all survey submissions and questions for a workshop.
     #
     # @param workshop_id [Integer]
     # @return [Array<Array>, nil] An array of 3 arrays: Array<Pd::SurveyQuestion>,
@@ -22,6 +22,35 @@ module Pd::SurveyPipeline
         : Pd::SurveyQuestion.where(form_id: form_ids_with_submissions)
 
       [questions, ws_submissions, facilitator_submissions]
+    end
+
+    # Retrieve facilitator-specific survey submissions and questions for selected facilitators and workshops.
+    #
+    # @param facilitator_ids [Array<Integer>] non-empty list of facilitator ids
+    # @param ws_ids [Array<Integer>] non-empty list of workshop ids
+    # @return [Hash] {:facilitator_submissions, :survey_questions => Array}
+    #
+    def self.retrieve_facilitator_surveys(facilitator_ids, ws_ids)
+      submissions = Pd::WorkshopFacilitatorDailySurvey.where(
+        facilitator_id: facilitator_ids, pd_workshop_id: ws_ids
+      )
+      form_ids = submissions.pluck(:form_id).uniq
+      questions = Pd::SurveyQuestion.where(form_id: form_ids)
+
+      [questions, submissions]
+    end
+
+    # Retrieve general workshop survey submissions and questions for selected workshops.
+    #
+    # @param ws_ids [Array<number>] non-empty list of workshop ids
+    # @return [Hash] {:workshop_submissions, :survey_questions => Array}
+    #
+    def self.retrieve_general_workshop_surveys(ws_ids)
+      submissions = Pd::WorkshopDailySurvey.where(pd_workshop_id: ws_ids)
+      form_ids = submissions.pluck(:form_id).uniq
+      questions = Pd::SurveyQuestion.where(form_id: form_ids)
+
+      [questions, submissions]
     end
   end
 end
