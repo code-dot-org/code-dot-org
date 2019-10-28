d1d1e657e89247c46a02fc4192c4c9b3ad844c72
Brendan Reville <brendan@code.org>
2019-10-28 14:18:03 -0700

    Fix JS

--- frontend/packages/labs/oceans/src/demo/renderer.js
+++ frontend/packages/labs/oceans/src/demo/renderer.js
@@ -79,6 +79,8 @@ export const render = () => {
 
   clearCanvas(state.canvas);
 
+  const timeBeforeCanSkipPredict = 5000;
+
   switch (state.currentMode) {
     case Modes.Training:
       updateTrainText(state);
@@ -86,9 +88,7 @@ export const render = () => {
       drawMovingFish(state);
       break;
     case Modes.Predicting:
-      const timeBeforeCanSkipPredict = 5000;
       setState({canSkipPredict: $time() >= currentModeStartTime + timeBeforeCanSkipPredict});
-
       drawMovingFish(state);
       break;
     case Modes.Pond:
