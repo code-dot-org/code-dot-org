03f65e64e6f12e1fb268573536a0e2171a00ff74
Ha Nguyen <ha@code.org>
2019-10-28 13:10:03 -0700

    Fix function comments

--- dashboard/lib/pd/survey_pipeline/daily_survey_retriever.rb
+++ dashboard/lib/pd/survey_pipeline/daily_survey_retriever.rb
@@ -3,9 +3,9 @@ module Pd::SurveyPipeline
     # Retrieve all survey submissions and questions for a workshop.
     #
     # @param workshop_id [Integer]
-    # @return [Array<Array>, nil] An array of 3 arrays: Array<Pd::SurveyQuestion>,
-    #   Array<Pd::WorkshopDailySurvey> and Array<Pd::WorkshopFacilitatorDailySurvey>.
-    #   Returns nil if input workshop_id is nil.
+    # @return [Array<Enumerable>, nil] An array of 3 enumerable: Enumerable<Pd::SurveyQuestion>,
+    #   Enumerable<Pd::WorkshopDailySurvey> and Enumerable<Pd::WorkshopFacilitatorDailySurvey>.
+    #   Returns nil if the input workshop_id is nil.
     #
     def self.retrieve_all_workshop_surveys(workshop_id)
       return unless workshop_id
@@ -29,8 +29,8 @@ module Pd::SurveyPipeline
     #
     # @param facilitator_id [Integer]
     # @param workshop_ids [Integer, Array<Integer>] a workshop id or a list of workshop ids
-    # @return [Array<Array>, nil] An array of 2 arrays: Array<Pd::SurveyQuestion> and
-    #   Array<Pd::WorkshopFacilitatorDailySurvey>.
+    # @return [Array<Enumerable>, nil] An array of 2 enumerable: Enumerable<Pd::SurveyQuestion> and
+    #   Enumerable<Pd::WorkshopFacilitatorDailySurvey>.
     #   Returns nil if either of the input parameter is nil.
     #
     def self.retrieve_facilitator_surveys(facilitator_id, workshop_ids)
@@ -48,8 +48,8 @@ module Pd::SurveyPipeline
     # Retrieve general workshop survey submissions and questions for selected workshops.
     #
     # @param workshop_ids [Integer, Array<Integer>] a workshop id or a list of workshop ids
-    # @return [Array<Array>] An array of 2 arrays: Array<Pd::SurveyQuestion> and
-    #   Array<Pd::WorkshopDailySurvey>. Returns nil if the input parameter is nil.
+    # @return [Array<Enumerable>] An array of 2 enumerable: Enumerable<Pd::SurveyQuestion> and
+    #   Enumerable<Pd::WorkshopDailySurvey>. Returns nil if the input parameter is nil.
     #
     def self.retrieve_general_workshop_surveys(workshop_ids)
       return unless workshop_ids
