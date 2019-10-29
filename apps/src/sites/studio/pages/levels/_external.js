00dbc99ea303acd1fe9ac7c5de89f3165d40fc9a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-10-28 20:41:04 -0700

    Revert "Render markdown in content-backed DSL-defined levels clientside"

--- apps/src/sites/studio/pages/levels/_external.js
+++ apps/src/sites/studio/pages/levels/_external.js
@@ -6,6 +6,8 @@ import {
 } from '@cdo/apps/code-studio/levels/postOnLoad';
 
 $(document).ready(() => {
+  embedDiscourseForum();
+
   const script = document.querySelector('script[data-external]');
   const data = JSON.parse(script.dataset.external);
 
@@ -21,5 +23,31 @@ $(document).ready(() => {
   postMilestoneForPageLoad();
 
   // handle click on continue (results in navigating to next puzzle)
-  $('.external').on('click', '.submitButton', onContinue);
+  $('.submitButton').click(onContinue);
 });
+
+// Embed a forum thread in an External level by adding <div id='discourse-comments' /> anywhere in the page html
+function embedDiscourseForum() {
+  if ($('#discourse-comments')[0]) {
+    window.discourseUrl =
+      location.hostname === 'studio.code.org'
+        ? '//forum.code.org/'
+        : '//discourse.code.org/';
+    window.discourseEmbedUrl = [
+      location.protocol,
+      '//',
+      location.host,
+      location.pathname
+    ].join('');
+    (function() {
+      var d = document.createElement('script');
+      d.type = 'text/javascript';
+      d.async = true;
+      d.src = window.discourseUrl + 'javascripts/embed.js';
+      (
+        document.getElementsByTagName('head')[0] ||
+        document.getElementsByTagName('body')[0]
+      ).appendChild(d);
+    })();
+  }
+}
