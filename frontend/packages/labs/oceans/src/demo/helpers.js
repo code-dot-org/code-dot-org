ebb655b2fe32911e67ca7de6ef45c60f3aefa2f0
Maddie Kasula <madelynkasula@gmail.com>
2019-10-30 13:53:42 -0700

    Add comment with example input/output

--- frontend/packages/labs/oceans/src/demo/helpers.js
+++ frontend/packages/labs/oceans/src/demo/helpers.js
@@ -73,6 +73,22 @@ export const queryStrFor = key => {
   return queryString.parse(location.search)[key];
 };
 
+/**
+ * Given fishComponents and a dataSet, will filter any components that should be excluded in that dataSet.
+ * Example input: {
+ *   bodies: {
+ *     body1: {src: 'images/body1.png'},
+ *     body2: {
+ *       src: 'images/body2.png',
+ *       exclusions: [DataSet.Small]
+ *     }
+ *   }
+ * }, DataSet.Small
+ *
+ * Example output: {
+ *   bodies: [{src: 'images/body1.png'}]
+ * }
+ */
 export const filterFishComponents = (fishComponents, dataSet) => {
   if (!dataSet) {
     return fishComponents;
