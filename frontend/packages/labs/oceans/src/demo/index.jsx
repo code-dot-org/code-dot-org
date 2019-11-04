56c5938980401fe5acdb9d3b681917c8c188bc98
Chris Pirich <chris@code.org>
2019-11-04 10:37:35 -0800

    Simple level progression UI on demo page

--- frontend/packages/labs/oceans/src/demo/index.jsx
+++ frontend/packages/labs/oceans/src/demo/index.jsx
@@ -4,26 +4,51 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import UI from './ui';
 import constants, {Modes} from './constants';
-import {setState, setSetStateCallback} from './state';
+import {setInitialState, setSetStateCallback} from './state';
 import {render as renderCanvas} from './renderer';
 import {queryStrFor} from './helpers';
 import {toMode} from './toMode';
 
+let currentAppMode = queryStrFor('mode') || 'intro-instructions';
+
+function onLevelChange(event) {
+  currentAppMode = event.target.id;
+  initAll(currentAppMode);
+}
+
+function onContinue() {
+  const nextRadioButton = $(`#${currentAppMode}`).next();
+  if (nextRadioButton) {
+    nextRadioButton.prop('checked', true);
+    onLevelChange({
+      target: {
+        id: nextRadioButton.attr('id')
+      }
+    });
+  }
+}
+
 $(document).ready(() => {
+  $(`#${currentAppMode}`).prop('checked', true);
+  $('.level-radio').change(onLevelChange);
+
+  initAll(currentAppMode);
+});
+
+export const initAll = function(appMode) {
   // Set up canvases.
   const canvas = document.getElementById('activity-canvas');
   const backgroundCanvas = document.getElementById('background-canvas');
   canvas.width = backgroundCanvas.width = constants.canvasWidth;
   canvas.height = backgroundCanvas.height = constants.canvasHeight;
 
-  // Use URL parameter to set mode.
-  const appMode = queryStrFor('mode');
-
   // Set initial state for UI elements.
-  setState({
+  setInitialState({
     appMode,
     canvas,
-    backgroundCanvas
+    backgroundCanvas,
+    currentMode: Modes.Loading,
+    onContinue
   });
 
   // Initialize our first model.
@@ -38,7 +63,7 @@ $(document).ready(() => {
 
   // And have the render UI handler be called every time state is set.
   setSetStateCallback(renderUI);
-});
+};
 
 // Tell React to explicitly render the UI.
 export const renderUI = () => {
