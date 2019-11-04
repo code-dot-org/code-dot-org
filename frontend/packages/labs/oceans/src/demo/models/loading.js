762cbdae0e5ae7304af4786b5b7a1e043eb2dfa1
Brendan Reville <brendan@code.org>
2019-11-04 00:07:28 -0800

    Use the mode property properly again

--- frontend/packages/labs/oceans/src/demo/models/loading.js
+++ frontend/packages/labs/oceans/src/demo/models/loading.js
@@ -1,16 +1,21 @@
 import 'idempotent-babel-polyfill';
 import {initRenderer} from '../renderer';
-import {getState} from '../state';
-import {Modes} from '../constants';
+import {getState, setState} from '../state';
+import {Modes, DataSet} from '../constants';
 import {initFishData} from '../../utils/fishData';
 import {getAppMode} from '../helpers';
 import {toMode} from '../toMode';
 
 export const init = async () => {
+  const [appModeBase] = getAppMode(getState());
+
+  const dataSet = appModeBase === "short" ? DataSet.Small : DataSet.Large;
+  const loadTrashImages = appModeBase === "fishvtrash";
+  setState({dataSet, loadTrashImages});
+
   initFishData();
   await initRenderer();
 
-  const [appModeBase] = getAppMode(getState());
   let mode;
   if (appModeBase === 'instructions') {
     mode = Modes.Instructions;
