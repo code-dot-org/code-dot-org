832280cf80c946c62d33a6be484208d860500b13
Dayne <dayne@code.org>
2019-11-04 15:20:21 -0800

    pausing work

--- dashboard/bin/i18n/html_to_markdown.js
+++ dashboard/bin/i18n/html_to_markdown.js
@@ -8,28 +8,32 @@ var stringify = require('remark-stringify')
 var processor = unified()
   .use(parse)
   .use(rehype2remark)
-  .use(stringify)
+  .use(stringify, {
+  });
 var json_file = process.argv[2]
-var json_markdown = {};
 fs.readFile(json_file, 
   function(err, data) {
+    var json_markdown = {};
     var jsonParsed = JSON.parse(data);
     for (var key in jsonParsed) {
+      if (key.startsWith("en.")) {
+        continue;
+      }
       var value = jsonParsed[key];
       unified()
         .use(parse)
         .use(rehype2remark)
         .use(stringify)
-        .process(value, function(err, html) {
+        .process(value, function(err, markdown_text_file) {
           if (err) {
             console.error("ERROR: " + err);
           } else {
-            json_markdown[key] = String(html);
-            console.log(key);
+            markdown_text = String(markdown_text_file).trim();
+            json_markdown[key] = markdown_text;
           }
         });
     }
+    console.log(json_markdown);
   }
 );
-console.log(json_markdown);
 
