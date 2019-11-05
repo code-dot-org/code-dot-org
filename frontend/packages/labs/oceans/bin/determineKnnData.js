6ec31b78401702b2cf3d63a14c48ec5e6f948006
Winter Dong <winter@code.org>
2019-11-05 13:19:24 -0800

    Update determineKnnData script to output json for mouth, eyes, body

--- frontend/packages/labs/oceans/bin/determineKnnData.js
+++ frontend/packages/labs/oceans/bin/determineKnnData.js
@@ -2,8 +2,19 @@
 var fs = require('fs');
 var {loadImage, createCanvas} = require('canvas');
 
+const FishBodyPart = Object.freeze({
+  DORSAL_FIN: 0,
+  TAIL: 1,
+  PECTORAL_FIN_BACK: 2,
+  BODY: 3,
+  PECTORAL_FIN_FRONT: 4,
+  MOUTH: 5,
+  EYE: 6
+});
+
 const bodyDirectory = 'public/images/fish/body/';
 const body_image_files = fs.readdirSync(bodyDirectory);
+const bodies = {}
 body_image_files.forEach(fileName => {
   const imagePath = bodyDirectory + fileName;
   loadImage(imagePath).then(function(image) {
@@ -17,12 +28,34 @@ body_image_files.forEach(fileName => {
         numPixels++;
       }
     }
-    console.log(image.src, (1.0 * image.width) / image.height, numPixels);
+    const name = fileName.split('.')[0];
+    const src = `${name}_image`;
+
+    console.log(`import ${src} from '../../${imagePath}'`);
+    //console.log(image.src, (1.0 * image.width) / image.height, numPixels);
+
+    eyes[name] = {
+      src: src,
+      anchor: [null, null],
+      eyeAnchor: [null, null],
+      mouthAnchor: [null, null],
+      pectoralFinBackAnchor: [null, null],
+      pectoralFinFrontAnchor: [null, null],
+      dorsalFinAnchor: [null, null],
+      tailAnchor: [null, null],
+      knnData: [numPixels, null],
+      type: FishBodyPart.BODY
+    };
+
+    if (Object.keys(bodies).length === body_image_files.length) {
+      console.log(JSON.stringify(bodies, null, 2));
+    }    
   });
 });
 
 const eyeDirectory = 'public/images/fish/eyes/';
 const eye_image_files = fs.readdirSync(eyeDirectory);
+const eyes = {}
 eye_image_files.forEach(fileName => {
   const imagePath = eyeDirectory + fileName;
   loadImage(imagePath).then(function(image) {
@@ -44,17 +77,33 @@ eye_image_files.forEach(fileName => {
         }
       }
     }
+
     console.log(
       image.src,
       (1.0 * image.width) / image.height,
       numPixels,
       numPixels / numPupilPixels
     );
+
+    const name = fileName.split('.')[0];
+    const src = `${name}_image`;
+
+    //console.log(`import ${src} from '../../${imagePath}'`);
+
+    eyes[name] = {
+      src: src,
+      knnData: [numPixels, numPixels / numPupilPixels],
+      type: FishBodyPart.EYE
+    };
+    if (Object.keys(eyes).length === eye_image_files.length) {
+      console.log(JSON.stringify(eyes, null, 2));
+    }
   });
 });
 
 const mouthDirectory = 'public/images/fish/mouth/';
 const mouth_image_files = fs.readdirSync(mouthDirectory);
+const mouths = {};
 mouth_image_files.forEach(fileName => {
   const imagePath = mouthDirectory + fileName;
   loadImage(imagePath).then(function(image) {
@@ -68,6 +117,21 @@ mouth_image_files.forEach(fileName => {
         numPixels++;
       }
     }
-    console.log(image.src, image.width, image.height, (1.0 * image.width) / image.height);
-  });
+
+    //console.log(image.src, image.width, image.height, (1.0 * image.width) / image.height);
+
+    const name = fileName.split('.')[0];
+    const src = `${name}_image`;
+
+    console.log(`import ${src} from '../../${imagePath}'`);
+
+    mouths[name] = {
+      src: src,
+      knnData: [null, (1.0 * image.width) / image.height, null],
+      type: FishBodyPart.MOUTH
+    };
+    if (Object.keys(mouths).length === mouth_image_files.length) {
+      console.log(JSON.stringify(mouths, null, 2));
+    }
+  }).catch(function(error) {console.log(error); console.log(imagePath);});
 });
