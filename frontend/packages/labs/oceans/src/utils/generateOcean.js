d18a5640928180c594a7a3246e0f525a1ef574b5
Bethany Connor <bethany@code.org>
2019-11-05 12:35:10 -0800

    Fix bug where only two color palettes were used in fishvtrash

--- frontend/packages/labs/oceans/src/utils/generateOcean.js
+++ frontend/packages/labs/oceans/src/utils/generateOcean.js
@@ -1,4 +1,8 @@
-import {FishOceanObject, SeaCreatureOceanObject, TrashOceanObject} from '../demo/OceanObject';
+import {
+  FishOceanObject,
+  SeaCreatureOceanObject,
+  TrashOceanObject
+} from '../demo/OceanObject';
 import {getState} from '../demo/state';
 import {fishData} from './fishData';
 import {filterFishComponents} from '../demo/helpers';
@@ -39,22 +43,24 @@ export const generateOcean = numFish => {
       // For each of these components, use the next variation on the list
       // Reshuffle the list if we've reached the end to avoid any regularity.
       object.body = bodies[i % bodies.length];
-      if (i % bodies.length === bodies.length - 1) {
-        bodies = _.shuffle(bodies);
-      }
       object.eye = eyes[i % eyes.length];
-      if (i % eyes.length === eyes.length - 1) {
-        eyes = _.shuffle(eyes);
-      }
       object.mouth = mouths[i % mouths.length];
-      if (i % mouths.length === mouths.length - 1) {
-        mouths = _.shuffle(mouths);
-      }
+
       object.colorPalette = colorPalettes[i % colorPalettes.length];
-      if (i % colorPalettes.length === colorPalettes.length - 1) {
-        colorPalettes = _.shuffle(colorPalettes);
-      }
     }
+    if (i % bodies.length === bodies.length - 1) {
+      bodies = _.shuffle(bodies);
+    }
+    if (i % eyes.length === eyes.length - 1) {
+      eyes = _.shuffle(eyes);
+    }
+    if (i % mouths.length === mouths.length - 1) {
+      mouths = _.shuffle(mouths);
+    }
+    if (i % colorPalettes.length === colorPalettes.length - 1) {
+      colorPalettes = _.shuffle(colorPalettes);
+    }
+
     object.randomize();
     ocean.push(object);
   }
