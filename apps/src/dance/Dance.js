d7899f7a1aee209643653f3d9625b8d424f39aec
Erin Peach <erin@code.org>
2019-11-06 15:42:49 -0800

    Add new songs to dance part extras

--- apps/src/dance/Dance.js
+++ apps/src/dance/Dance.js
@@ -183,7 +183,9 @@ Dance.prototype.awaitTimingMetrics = function() {
 
 Dance.prototype.initSongs = async function(config) {
   let is2019Script =
-    config.scriptName === 'dance-2019' && experiments.isEnabled('newSongs');
+    (config.scriptName === 'dance-2019' ||
+      config.scriptName === 'dance-extras-2019') &&
+    experiments.isEnabled('newSongs');
   const songManifest = await getSongManifest(
     config.useRestrictedSongs,
     is2019Script
