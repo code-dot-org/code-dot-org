f638af936988f2f8fbbd3ea867bcad67b014ff36
Dani McAvoy <danimcavoy@gmail.com>
2019-11-06 13:12:09 -0500

    Finish clean up of table name change

--- dashboard/test/models/validated_user_level_test.rb
+++ dashboard/test/models/validated_user_level_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserLevelInfoTest < ActiveSupport::TestCase
+class ValidatedUserLevelTest < ActiveSupport::TestCase
   test "can create validated_user_level" do
     validated_user_level = create :validated_user_level
     assert_equal validated_user_level.user_level_id, 1
