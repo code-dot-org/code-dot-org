59affbb309785c86c134ed25e55f7c263f7e1dff
Jessica Kulwik <jessica@code.org>
2019-11-07 01:06:03 +0000

    Added library controller that returns a class set of libraries.

--- dashboard/lib/libraries_list.rb
+++ dashboard/lib/libraries_list.rb
@@ -1,7 +1,17 @@
+include ProjectsList
 module LibrariesList
   class << self
-    def fetch_all_section_projects
-      puts "fetch_all_section_projects"
+    def fetch_all_section_projects(user)
+      sections = user.sections + user.sections_as_student
+      projects = []
+      sections.each do |section|
+        projects += ProjectsList.fetch_section_projects(section).select {|project| !project[:libraryName].nil}
+      end
+      projects.map! do |project|
+        project[:description] = project[:libraryDescription]
+        project[:name] = project[:libraryName]
+        project
+      end
     end
   end
 end
