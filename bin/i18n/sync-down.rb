11db6d76de251c3d38eab0e5a52769a45e7a8f3e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-11-07 17:15:52 -0800

    filter the output of the crowdin download operation

--- bin/i18n/sync-down.rb
+++ bin/i18n/sync-down.rb
@@ -5,13 +5,28 @@
 # https://crowdin.com/project/codeorg
 
 require_relative 'i18n_script_utils'
+require 'open3'
 
 def sync_down
   puts "Beginning sync down"
 
   CROWDIN_PROJECTS.each do |name, options|
     puts "Downloading translations from #{name} project"
-    system "crowdin --config #{options[:config_file]} --identity #{options[:identity_file]} download translations"
+    command = "crowdin --config #{options[:config_file]} --identity #{options[:identity_file]} download translations -l asfd"
+
+    # Filter the output because the crowdin translation download is _super_
+    # verbose; it includes not only a progress spinner, but also information
+    # about each individual file downloaded in each individual language.
+    #
+    # We really only care about general progress monitoring, so we only output
+    # a single (arbitrarily-chosen) line of output for each language
+    Open3.popen2(command) do |_stdin, stdout, status_thread|
+      stdout.each_line do |line|
+        puts line if line.start_with? "Building ZIP archive with the latest translations"
+      end
+
+      raise "Sync down failed"  unless status_thread.value.success?
+    end
   end
 
   puts "Sync down complete"
