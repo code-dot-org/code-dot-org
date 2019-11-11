586134e474b4bbb141b3f6a37e5ee06c72afafb1
Clare Constantine <clare@code.org>
2019-11-11 12:51:19 -0800

    homepage logs page name

--- apps/src/sites/studio/pages/home/_homepage.js
+++ apps/src/sites/studio/pages/home/_homepage.js
@@ -11,7 +11,8 @@ import {
   setValidGrades,
   setStageExtrasScriptIds,
   setAuthProviders,
-  beginEditingNewSection
+  beginEditingNewSection,
+  setPageName
 } from '@cdo/apps/templates/teacherDashboard/teacherSectionsRedux';
 import {initializeHiddenScripts} from '@cdo/apps/code-studio/hiddenStageRedux';
 import {updateQueryParam} from '@cdo/apps/code-studio/utils';
@@ -31,6 +32,7 @@ function showHomepage() {
   store.dispatch(setStageExtrasScriptIds(homepageData.stageExtrasScriptIds));
   store.dispatch(setAuthProviders(homepageData.providers));
   store.dispatch(initializeHiddenScripts(homepageData.hiddenScripts));
+  store.dispatch(setPageName('homepage'));
 
   let courseId;
   let scriptId;
