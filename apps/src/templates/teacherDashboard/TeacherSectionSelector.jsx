c280f19f4186d8493ed4bf91b6d4d18525d1d8c2
Erin Bond <erin.bond@code.org>
2019-11-11 16:17:54 -0800

    reload on section change on the script overview page

--- apps/src/templates/teacherDashboard/TeacherSectionSelector.jsx
+++ apps/src/templates/teacherDashboard/TeacherSectionSelector.jsx
@@ -7,6 +7,7 @@ import TeacherSectionSelectorMenuItem from './TeacherSectionSelectorMenuItem';
 import {sectionForDropdownShape} from './shapes';
 import SmallChevronLink from '@cdo/apps/templates/SmallChevronLink';
 import {updateQueryParam} from '@cdo/apps/code-studio/utils';
+import {reload} from '../../utils';
 
 const styles = {
   select: {
@@ -26,7 +27,10 @@ export default class TeacherSectionSelector extends Component {
   static propTypes = {
     sections: PropTypes.arrayOf(sectionForDropdownShape).isRequired,
     selectedSection: PropTypes.object,
-    onChangeSection: PropTypes.func.isRequired
+    onChangeSection: PropTypes.func.isRequired,
+    // We need to reload on section change on the script overview page to get
+    // accurate information about students in the selected section.
+    forceReload: PropTypes.bool
   };
 
   state = {
@@ -78,6 +82,9 @@ export default class TeacherSectionSelector extends Component {
     updateQueryParam('section_id', section.id);
     // If we have a user_id when we switch sections we should get rid of it
     updateQueryParam('user_id', undefined);
+    if (this.props.forceReload) {
+      reload();
+    }
     this.closeMenu();
   };
 
