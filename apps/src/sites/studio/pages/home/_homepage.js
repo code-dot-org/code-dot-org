2a81a066418ff3e2b29699184f05162c0164d97b
Clare Constantine <clare@code.org>
2019-11-11 17:53:26 -0800

    use pageTypes enum

--- apps/src/sites/studio/pages/home/_homepage.js
+++ apps/src/sites/studio/pages/home/_homepage.js
@@ -12,7 +12,8 @@ import {
   setStageExtrasScriptIds,
   setAuthProviders,
   beginEditingNewSection,
-  setPageType
+  setPageType,
+  pageTypes
 } from '@cdo/apps/templates/teacherDashboard/teacherSectionsRedux';
 import {initializeHiddenScripts} from '@cdo/apps/code-studio/hiddenStageRedux';
 import {updateQueryParam} from '@cdo/apps/code-studio/utils';
@@ -32,7 +33,7 @@ function showHomepage() {
   store.dispatch(setStageExtrasScriptIds(homepageData.stageExtrasScriptIds));
   store.dispatch(setAuthProviders(homepageData.providers));
   store.dispatch(initializeHiddenScripts(homepageData.hiddenScripts));
-  store.dispatch(setPageType('homepage'));
+  store.dispatch(setPageType(pageTypes.homepage));
 
   let courseId;
   let scriptId;
