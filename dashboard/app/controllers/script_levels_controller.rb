634d217ef3035cc25bc1eeb6e654bd9e71dc3d72
Dani McAvoy <danimcavoy@gmail.com>
2019-11-13 11:48:47 -0500

    Dont show warning if unit has not been assigned

--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -189,7 +189,7 @@ class ScriptLevelsController < ApplicationController
         @section = current_user.sections[0]
         @user = @section&.students&.find_by(id: params[:user_id])
       end
-      @show_stage_extras_warning = !@section&.stage_extras
+      @show_stage_extras_warning = !@section&.stage_extras && @section.script.name == params[:script_id]
     end
 
     # Explicitly return 404 here so that we don't get a 5xx in get_from_cache.
