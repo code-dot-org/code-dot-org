828e369e43bf7fd3314c4bca6bc65944ea38af35
Erin Peach <erin@code.org>
2019-11-14 14:22:29 -0800

    Only use songManifest2019.json

--- apps/src/dance/Dance.js
+++ apps/src/dance/Dance.js
@@ -40,7 +40,6 @@ import {
 } from './songs';
 import {SongTitlesToArtistTwitterHandle} from '../code-studio/dancePartySongArtistTags';
 import firehoseClient from '@cdo/apps/lib/util/firehose';
-import experiments from '@cdo/apps/util/experiments';
 
 const ButtonState = {
   UP: 0,
@@ -182,14 +181,7 @@ Dance.prototype.awaitTimingMetrics = function() {
 };
 
 Dance.prototype.initSongs = async function(config) {
-  let is2019Script =
-    (config.scriptName === 'dance-2019' ||
-      config.scriptName === 'dance-extras-2019') &&
-    experiments.isEnabled('newSongs');
-  const songManifest = await getSongManifest(
-    config.useRestrictedSongs,
-    is2019Script
-  );
+  const songManifest = await getSongManifest(config.useRestrictedSongs);
   const songData = parseSongOptions(songManifest);
   const selectedSong = getSelectedSong(songManifest, config);
 
