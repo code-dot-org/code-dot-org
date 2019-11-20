259b6aa2ceb44f05bace007a48ba5bf7136c2384
Brad Buchanan <brad@code.org>
2019-11-19 18:32:15 -0800

    Simplify render method

--- apps/src/code-studio/pd/workshop_dashboard/DetailsPanel.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/DetailsPanel.jsx
@@ -50,87 +50,90 @@ export default class DetailsPanel extends React.Component {
     this.context.router.push('/workshops');
   };
 
-  renderDetailsPanelHeader() {
-    const {view, workshop, isWorkshopAdmin} = this.props;
-    let button = null;
-
-    if (view === 'edit') {
-      button = (
-        <Button
-          bsSize="xsmall"
-          bsStyle="primary"
-          onClick={this.handleSaveClick}
-        >
-          Save
-        </Button>
-      );
-    } else if (workshop.state === 'Not Started') {
-      button = (
-        <Button bsSize="xsmall" onClick={this.handleEditClick}>
-          Edit
-        </Button>
-      );
-    } else if (isWorkshopAdmin) {
-      if (this.state.showAdminEditConfirmation) {
-        return (
-          <ConfirmationDialog
-            show={true}
-            onOk={this.handleAdminEditConfirmed}
-            onCancel={this.handleAdminEditCancel}
-            headerText={`Edit ${workshop.state} Workshop?`}
-            bodyText={`Are you sure you want to edit this ${workshop.state.toLowerCase()} workshop?
-            Use caution! Note that deleting a session (day)
-            will also delete all associated attendance records.
-            `}
-          />
-        );
-      }
-      button = (
-        <Button bsSize="xsmall" onClick={this.handleAdminEditClick}>
-          Edit (admin)
-        </Button>
-      );
-    }
-
-    return <span>Workshop Details: {button}</span>;
-  }
-
-  renderDetailsPanelContent() {
-    const {view, workshop, onWorkshopSaved} = this.props;
-    if (view === 'edit') {
-      return (
-        <div>
-          <WorkshopForm workshop={workshop} onSaved={onWorkshopSaved} />
-        </div>
-      );
-    }
-
-    let editButton = null;
-    if (workshop.state === 'Not Started') {
-      editButton = <Button onClick={this.handleEditClick}>Edit</Button>;
-    }
-
-    return (
-      <div>
-        <WorkshopForm workshop={workshop} readOnly>
-          <Row>
-            <Col sm={4}>
-              <ButtonToolbar>
-                {editButton}
-                <Button onClick={this.handleBackClick}>Back</Button>
-              </ButtonToolbar>
-            </Col>
-          </Row>
-        </WorkshopForm>
-      </div>
-    );
-  }
-
   render() {
+    const {view, workshop, isWorkshopAdmin, onWorkshopSaved} = this.props;
+
+    if (view === 'edit') {
+      const header = <EditHeader handleSave={this.handleSaveClick} />;
+      return (
+        <WorkshopPanel header={header}>
+          <div>
+            <WorkshopForm workshop={workshop} onSaved={onWorkshopSaved} />
+          </div>
+        </WorkshopPanel>
+      );
+    }
+
+    let header = <DetailsPanelHeader />;
+    if (workshop.state === 'Not Started') {
+      header = <NotStartedHeader handleEdit={this.handleEditClick} />;
+    } else if (isWorkshopAdmin) {
+      header = <AdminHeader handleEdit={this.handleAdminEditClick} />;
+    }
     return (
-      <WorkshopPanel header={this.renderDetailsPanelHeader()}>
-        {this.renderDetailsPanelContent()}
+      <WorkshopPanel header={header}>
+        <div>
+          <WorkshopForm workshop={workshop} readOnly>
+            <Row>
+              <Col sm={4}>
+                <ButtonToolbar>
+                  {workshop.state === 'Not Started' && (
+                    <Button onClick={this.handleEditClick}>Edit</Button>
+                  )}
+                  <Button onClick={this.handleBackClick}>Back</Button>
+                </ButtonToolbar>
+              </Col>
+            </Row>
+          </WorkshopForm>
+        </div>
+        <ConfirmationDialog
+          show={isWorkshopAdmin && this.state.showAdminEditConfirmation}
+          onOk={this.handleAdminEditConfirmed}
+          onCancel={this.handleAdminEditCancel}
+          headerText={`Edit ${workshop.state} Workshop?`}
+          bodyText={`Are you sure you want to edit this ${workshop.state.toLowerCase()} workshop?
+              Use caution! Note that deleting a session (day)
+              will also delete all associated attendance records.
+              `}
+        />
       </WorkshopPanel>
     );
   }
 }
+
+const NotStartedHeader = ({handleEdit}) => (
+  <DetailsPanelHeader>
+    <HeaderButton text="Edit" onClick={handleEdit} />
+  </DetailsPanelHeader>
+);
+NotStartedHeader.propTypes = {handleEdit: PropTypes.func.isRequired};
+
+const AdminHeader = ({handleEdit}) => (
+  <DetailsPanelHeader>
+    <HeaderButton text="Edit (admin)" onClick={handleEdit} />
+  </DetailsPanelHeader>
+);
+AdminHeader.propTypes = {handleEdit: PropTypes.func.isRequired};
+
+const EditHeader = ({handleSave}) => (
+  <DetailsPanelHeader>
+    <HeaderButton text="Save" bsStyle="primary" onClick={handleSave} />
+  </DetailsPanelHeader>
+);
+EditHeader.propTypes = {handleSave: PropTypes.func.isRequired};
+
+const DetailsPanelHeader = ({children}) => (
+  <span>Workshop Details: {children}</span>
+);
+DetailsPanelHeader.propTypes = {children: PropTypes.node};
+
+const HeaderButton = ({text, bsStyle, onClick}) => (
+  <Button bsSize="xsmall" bsStyle={bsStyle} onClick={onClick}>
+    {text}
+  </Button>
+);
+HeaderButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  bsStyle: PropTypes.string,
+  onClick: PropTypes.func.isRequired
+};
