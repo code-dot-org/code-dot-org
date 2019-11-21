eae03b51f9e254850199aff7a6e142c19c41fae6
Dani McAvoy <danimcavoy@gmail.com>
2019-11-21 15:07:24 -0500

    Remove looking for the button in test

--- apps/test/unit/code-studio/components/progress/SelectedStudentInfoTest.jsx
+++ apps/test/unit/code-studio/components/progress/SelectedStudentInfoTest.jsx
@@ -61,7 +61,8 @@ describe('SelectedStudentInfo', () => {
     );
 
     expect(wrapper.contains('Last Updated:')).to.equal(true);
-    expect(wrapper.find('Button')).to.have.length(1);
+    /* Button is hidden currently. See SelectedStudentInfo for details
+    expect(wrapper.find('Button')).to.have.length(1); */
   });
 
   it('displays time and who they worked with as navigator if paired as driver on level', () => {
