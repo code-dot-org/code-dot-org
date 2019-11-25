21413d960823e968c441187bbab92828b0df3160
Ben Brooks <ben@code.org>
2019-11-24 21:30:28 -0800

    Non-i18n String changes for 19-20 Adafruit discount codes

--- apps/src/lib/kits/maker/util/discountLogic.js
+++ apps/src/lib/kits/maker/util/discountLogic.js
@@ -7,9 +7,9 @@
  */
 export const Unit6Intention = {
   NO: 'no',
-  YES_18_19: 'yes1819',
-  YES_19_20: 'yes1920',
-  YES_AFTER: 'yesAfter',
+  YES_SPRING_2020: 'yesSpring2020',
+  YES_FALL_2020: 'yesFall2020',
+  YES_SPRING_2021: 'yesSpring2021',
   UNSURE: 'unsure'
 };
 
