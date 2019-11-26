191f5b418f23d7b33ab7fb35a832f50b5e1628b9
Jessica Kulwik <jessica@code.org>
2019-11-26 17:44:38 +0000

    Used correct method to verify whether a user is logged in

--- dashboard/app/controllers/api/v1/section_libraries_controller.rb
+++ dashboard/app/controllers/api/v1/section_libraries_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::SectionLibrariesController < Api::V1::JsonApiController
+  before_action :authenticate_user!
   check_authorization
 
   # gets the libraries from all members of all sections I am a part of
   # GET api/v1/section_libraries
   def index
-    # user is logged in
-    authorize! :manage, current_user
     sections = current_user.sections + current_user.sections_as_student
     libraries = []
     sections.each do |section|
