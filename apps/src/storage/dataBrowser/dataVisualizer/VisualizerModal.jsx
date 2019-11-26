23eea8c0480cc472eb74a57196a571af78e02f4b
Anjali <anjali@code.org>
2019-11-25 16:06:38 -0800

    chart options

--- apps/src/storage/dataBrowser/dataVisualizer/VisualizerModal.jsx
+++ apps/src/storage/dataBrowser/dataVisualizer/VisualizerModal.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import * as dataStyles from '../dataStyles';
+import * as rowStyle from '@cdo/apps/applab/designElements/rowStyle';
 import BaseDialog from '@cdo/apps/templates/BaseDialog.jsx';
+import DropdownField from './DropdownField';
 
 const INITIAL_STATE = {
-  isVisualizerOpen: false
+  isVisualizerOpen: false,
+  chartTitle: '',
+  chartType: '',
+  bucketSize: '',
+  selectedColumn1: '',
+  selectedColumn2: ''
 };
 
 class VisualizerModal extends React.Component {
@@ -28,6 +35,10 @@ class VisualizerModal extends React.Component {
   handleClose = () => this.setState({isVisualizerOpen: false});
 
   render() {
+    const isMultiColumnChart = ['Scatter Plot', 'Cross Tab'].includes(
+      this.state.chartType
+    );
+
     return (
       <span style={{display: 'inline-block'}}>
         <button
@@ -42,7 +53,65 @@ class VisualizerModal extends React.Component {
           handleClose={this.handleClose}
           fullWidth
           fullHeight
-        />
+        >
+          <div style={{overflow: 'auto', maxHeight: '90%'}}>
+            <h1> Explore {this.props.tableName} </h1>
+            <h2> Overview </h2>
+
+            <div>
+              <div style={rowStyle.container}>
+                <label style={rowStyle.description}>Chart Title</label>
+                <input
+                  style={rowStyle.input}
+                  value={this.state.chartTitle}
+                  onChange={event =>
+                    this.setState({chartTitle: event.target.value})
+                  }
+                />
+              </div>
+            </div>
+
+            <DropdownField
+              displayName="Chart Type"
+              options={['Bar Chart', 'Histogram', 'Scatter Plot', 'Cross Tab']}
+              value={this.state.chartType}
+              onChange={event => this.setState({chartType: event.target.value})}
+            />
+
+            {this.state.chartType === 'Histogram' && (
+              <div style={rowStyle.container}>
+                <label style={rowStyle.description}>Bucket Size</label>
+                <input
+                  style={rowStyle.input}
+                  value={this.state.bucketSize}
+                  onChange={event =>
+                    this.setState({bucketSize: event.target.value})
+                  }
+                />
+              </div>
+            )}
+
+            <DropdownField
+              displayName={isMultiColumnChart ? 'X Values' : 'Values'}
+              options={this.props.tableColumns}
+              value={this.state.selectedColumn1}
+              onChange={event =>
+                this.setState({selectedColumn1: event.target.value})
+              }
+            />
+
+            {isMultiColumnChart && (
+              <DropdownField
+                displayName="Y Values"
+                options={this.props.tableColumns}
+                value={this.state.selectedColumn2}
+                onChange={event =>
+                  this.setState({selectedColumn2: event.target.value})
+                }
+              />
+            )}
+          </div>
+        </BaseDialog>
       </span>
     );
   }
