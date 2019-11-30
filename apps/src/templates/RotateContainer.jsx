f051738eb0e931eee24800d29b8cbfc6512ae614
Brendan Reville <brendan@code.org>
2019-11-29 17:18:23 -0800

    Restore rotate container to full opacity

--- apps/src/templates/RotateContainer.jsx
+++ apps/src/templates/RotateContainer.jsx
@@ -14,8 +14,7 @@ const styles = {
     right: 0,
     bottom: 0,
     width: '100%',
-    height: '100%',
-    opacity: 0.5
+    height: '100%'
   },
   rotateContainerInner: {
     backgroundPosition: '50% 50%',
