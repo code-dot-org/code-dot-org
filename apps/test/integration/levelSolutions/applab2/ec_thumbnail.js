60e02d9cf4e113d075792285703e7837d1f719d9
David Bailey <davidsbailey@users.noreply.github.com>
2019-12-02 14:41:50 -0800

    Revert "skip failing thumbnail test"
    This reverts commit 2c40916d91f3584c8bf5f1e825f64bef9437ed13.

--- apps/test/integration/levelSolutions/applab2/ec_thumbnail.js
+++ apps/test/integration/levelSolutions/applab2/ec_thumbnail.js
@@ -3,7 +3,7 @@ import {TestResults} from '@cdo/apps/constants';
 import sinon from 'sinon';
 import {expect} from '../../../util/deprecatedChai';
 import project from '@cdo/apps/code-studio/initApp/project';
-// import * as htmlToCanvasWrapper from '@cdo/apps/util/htmlToCanvasWrapper';
+import * as htmlToCanvasWrapper from '@cdo/apps/util/htmlToCanvasWrapper';
 import {CAPTURE_TICK_COUNT} from '@cdo/apps/applab/constants';
 import {isCaptureComplete} from '@cdo/apps/util/thumbnail';
 
@@ -86,58 +86,57 @@ export default {
         result: true,
         testResult: TestResults.FREE_PLAY
       }
-    }
+    },
 
-    // Skip for babel 7 upgrade
-    // {
-    //   description: 'fails gracefully when image capture fails',
-    //   editCode: true,
-    //   xml: `write('hello');`,
-    //   runBeforeClick(assert) {
-    //     sinon
-    //       .stub(htmlToCanvasWrapper, 'html2canvas')
-    //       .callsFake(
-    //         () =>
-    //           new Promise((resolve, reject) =>
-    //             setTimeout(() => reject('foobar'), 0)
-    //           )
-    //       );
-    //     sinon.stub(console, 'log');
-    //
-    //     tickWrapper.runOnAppTick(Applab, CAPTURE_TICK_COUNT + 1, () => {
-    //       expect(isCaptureComplete()).to.be.false;
-    //       tickWrapper
-    //         .tickAppUntil(Applab, isCaptureComplete)
-    //         .then(() => {
-    //           expect(project.saveThumbnail).not.to.have.been.called;
-    //
-    //           expect(htmlToCanvasWrapper.html2canvas).to.have.been.calledOnce;
-    //           htmlToCanvasWrapper.html2canvas.restore();
-    //
-    //           expect(console.log).to.have.been.calledOnce;
-    //           expect(console.log.getCall(0).args[0]).to.contain('foobar');
-    //           console.log.restore();
-    //
-    //           Applab.onPuzzleComplete();
-    //         })
-    //         .catch(e => {
-    //           // Make sure any error details are visible in the test output.
-    //           setTimeout(() => {
-    //             throw new Error(`error within tickWrapper.tickAppUntil: ${e}`);
-    //           }, 0);
-    //         });
-    //     });
-    //   },
-    //   customValidator(assert) {
-    //     // No errors in output console
-    //     const debugOutput = document.getElementById('debug-output');
-    //     assert.equal(debugOutput.textContent, '');
-    //     return true;
-    //   },
-    //   expected: {
-    //     result: true,
-    //     testResult: TestResults.FREE_PLAY
-    //   }
-    // }
+    {
+      description: 'fails gracefully when image capture fails',
+      editCode: true,
+      xml: `write('hello');`,
+      runBeforeClick(assert) {
+        sinon
+          .stub(htmlToCanvasWrapper, 'html2canvas')
+          .callsFake(
+            () =>
+              new Promise((resolve, reject) =>
+                setTimeout(() => reject('foobar'), 0)
+              )
+          );
+        sinon.stub(console, 'log');
+
+        tickWrapper.runOnAppTick(Applab, CAPTURE_TICK_COUNT + 1, () => {
+          expect(isCaptureComplete()).to.be.false;
+          tickWrapper
+            .tickAppUntil(Applab, isCaptureComplete)
+            .then(() => {
+              expect(project.saveThumbnail).not.to.have.been.called;
+
+              expect(htmlToCanvasWrapper.html2canvas).to.have.been.calledOnce;
+              htmlToCanvasWrapper.html2canvas.restore();
+
+              expect(console.log).to.have.been.calledOnce;
+              expect(console.log.getCall(0).args[0]).to.contain('foobar');
+              console.log.restore();
+
+              Applab.onPuzzleComplete();
+            })
+            .catch(e => {
+              // Make sure any error details are visible in the test output.
+              setTimeout(() => {
+                throw new Error(`error within tickWrapper.tickAppUntil: ${e}`);
+              }, 0);
+            });
+        });
+      },
+      customValidator(assert) {
+        // No errors in output console
+        const debugOutput = document.getElementById('debug-output');
+        assert.equal(debugOutput.textContent, '');
+        return true;
+      },
+      expected: {
+        result: true,
+        testResult: TestResults.FREE_PLAY
+      }
+    }
   ]
 };
