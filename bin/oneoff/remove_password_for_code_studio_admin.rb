51ab14c9722e108d6f962c991cd29a2183d3dc8d
nkiru onwuneme <generalhrh@gmail.com>
2019-12-03 13:20:18 -0800

    remove batching from query

--- bin/oneoff/remove_password_for_code_studio_admin.rb
+++ bin/oneoff/remove_password_for_code_studio_admin.rb
@@ -3,12 +3,10 @@
 # Remove passwords for code studio admins
 
 batch_number = 0
-User.where(admin: true).where.not(encrypted_password: nil).find_in_batches(batch_size: 10) do |batch|
+User.where(admin: true).where.not(encrypted_password: nil).each do |admin|
   puts "PROCESSING: #{batch_number}..."
   ActiveRecord::Base.transaction do
-    batch.each do |user|
-      user.update(encrypted_password: nil)
-    end
+    admin.update!(encrypted_password: nil)
   end
   puts "PROCESSED: #{batch_number}..."
   batch_number += 1
