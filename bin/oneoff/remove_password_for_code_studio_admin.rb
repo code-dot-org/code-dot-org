47780e35703cc8baab5617abc52f3aada63a0214
nkiru onwuneme <generalhrh@gmail.com>
2019-12-03 16:42:47 -0800

    remove unused variable

--- bin/oneoff/remove_password_for_code_studio_admin.rb
+++ bin/oneoff/remove_password_for_code_studio_admin.rb
@@ -2,12 +2,10 @@
 
 # Remove passwords for code studio admins
 
-batch_number = 0
 User.where(admin: true).where.not(encrypted_password: nil).each do |admin|
   puts "PROCESSING: #{batch_number}..."
   ActiveRecord::Base.transaction do
     admin.update!(encrypted_password: nil)
   end
   puts "PROCESSED: #{batch_number}..."
-  batch_number += 1
 end
