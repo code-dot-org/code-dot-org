2c6cea2ab7ac5ebe075e6e78422a4704c5f91fc4
David Bailey <davidsbailey@users.noreply.github.com>
2019-12-03 22:45:25 -0800

    fix argument destructuring syntax

--- apps/src/code-studio/assets.js
+++ apps/src/code-studio/assets.js
@@ -1,5 +1,5 @@
 import unwrappedShowAssetManager from './assets/show';
 import listStore from './assets/assetListStore';
 // wrap showAssetManager, so that it can be stubbed in unit tests.
-const showAssetManager = (...args) => unwrappedShowAssetManager(args);
+const showAssetManager = (...args) => unwrappedShowAssetManager(...args);
 export {showAssetManager, listStore};
