b655fb5e4b03b56c7774a95d2412179d285c4481
Clare Constantine <clare@code.org>
2019-12-03 20:10:48 -0800

    log errors

--- dashboard/app/controllers/api/v1/projects/personal_projects_controller.rb
+++ dashboard/app/controllers/api/v1/projects/personal_projects_controller.rb
@@ -14,16 +14,11 @@ class Api::V1::Projects::PersonalProjectsController < ApplicationController
     begin
       name_failure = ShareFiltering.find_name_failure(params[:new_name], locale)
     rescue OpenURI::HTTPError, IO::EAGAINWaitReadable => name_checking_error
-      # If WebPurify or Geocoder fail, the name will be allowed, and we
-      # retain the name_checking_error to log it below.
-    end
-
-    if name_checking_error
-      # slog(
-      #   tag: 'name_checking_error',
-      #   error: "#{name_checking_error.class.name}: #{name_checking_error}",
-      #   level_source_id: @level_source.id <-- what to use for a project? do I even need to log this?
-      # )
+      # If WebPurify or Geocoder fail, the name will be allowed, but we log the error
+      slog(
+        tag: 'name_checking_error',
+        error: "#{name_checking_error.class.name}: #{name_checking_error}",
+      )
     end
 
     render json: {nameFailure: name_failure}, status: :ok
