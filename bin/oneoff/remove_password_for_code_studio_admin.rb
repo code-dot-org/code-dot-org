56434d2de0da9c7afb709fc852305c6ebe2e31da
nkiru onwuneme <generalhrh@gmail.com>
2019-12-03 21:28:42 -0800

    remove undefined variable batch_number

--- bin/oneoff/remove_password_for_code_studio_admin.rb
+++ bin/oneoff/remove_password_for_code_studio_admin.rb
@@ -2,10 +2,15 @@
 
 # Remove passwords for code studio admins
 
+DRY_RUN = true
+
 User.where(admin: true).where.not(encrypted_password: nil).each do |admin|
-  puts "PROCESSING: #{batch_number}..."
   ActiveRecord::Base.transaction do
     admin.update!(encrypted_password: nil)
+
+    raise ActiveRecord::Rollback.new, "Intentional rollback" if DRY_RUN
+    puts "Admin password updated - #{admin}"
   end
-  puts "PROCESSED: #{batch_number}..."
+rescue StandardError => error
+  puts "Error deleting admin's password - #{admin} / #{error.message}"
 end
