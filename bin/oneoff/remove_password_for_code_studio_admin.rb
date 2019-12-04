cdf6032605573cdad9f9539b4d3119397ec26330
nkiru onwuneme <generalhrh@gmail.com>
2019-12-04 11:22:07 -0800

    replace where method with find_by

--- bin/oneoff/remove_password_for_code_studio_admin.rb
+++ bin/oneoff/remove_password_for_code_studio_admin.rb
@@ -61,7 +61,7 @@ ADMIN_IDS = %w(
 
 ADMIN_IDS.each do |admin_id|
   ActiveRecord::Base.transaction do
-    User.where(id: admin_id).update!(encrypted_password: nil)
+    User.find_by(id: admin_id).update!(encrypted_password: nil)
 
     raise ActiveRecord::Rollback.new, "Intentional rollback" if DRY_RUN
     puts "Admin password updated - #{admin_id}"
