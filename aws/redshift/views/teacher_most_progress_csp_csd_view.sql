88c3bf3960e9e815d7ed1b29c8546f173c0df2ff
Mary <marybarnes37@gmail.com>
2019-12-05 11:25:02 -0800

    add course column to teacher_most_progress_csp_csd_view [ci skip]

--- aws/redshift/views/teacher_most_progress_csp_csd_view.sql
+++ aws/redshift/views/teacher_most_progress_csp_csd_view.sql
@@ -1,20 +1,27 @@
 DROP VIEW IF EXISTS teacher_most_progress_csp_csd_view CASCADE;
 
 CREATE OR REPLACE view analysis.teacher_most_progress_csp_csd_view AS
+
+WITH distinct_csp_csd_script_ids as
+(SELECT DISTINCT script_id, course_name_short, course_name_long from analysis.course_structure where course_name_short in ('csd', 'csp'))
+
 SELECT studio_person_id,
+       course_name_short,
        school_year,
        name script_most_progress, 
        students AS students_script_most_progress
 FROM (
 -- Rank which unit has most students in it by teacher
      SELECT studio_person_id,
+            course_name_short,
             school_year,
             name,
             students,
-            ROW_NUMBER() OVER (PARTITION BY studio_person_id, school_year ORDER BY students DESC) student_rank 
+            ROW_NUMBER() OVER (PARTITION BY studio_person_id, course_name_short, school_year ORDER BY students DESC) student_rank 
      FROM (
      -- Select # of students in each unit by teacher
        SELECT studio_person_id,
+              course_name_short,
               school_year,
               name,
               COUNT(*) students 
@@ -22,13 +29,16 @@ FROM (
          -- Rank units by most recently updated for each student
          SELECT u.studio_person_id,
                 coalesce(script_name_long, sc.name) as name,
+                course_name_short,
                 school_year,
-                ROW_NUMBER() OVER (PARTITION BY us.user_id, school_year ORDER BY us.updated_at DESC) update_rank 
+                ROW_NUMBER() OVER (PARTITION BY us.user_id, course_name_short, school_year ORDER BY us.updated_at DESC) update_rank 
          FROM dashboard_production.sections se 
                 JOIN dashboard_production.followers f 
                   ON f.section_id = se.id 
                 JOIN dashboard_production.user_scripts us 
-                  ON us.user_id = f.student_user_id AND us.script_id IN (select distinct script_id from analysis.course_structure where course_name_short in ('csd', 'csp')) 
+                  ON us.user_id = f.student_user_id
+                JOIN distinct_csp_csd_script_ids dsi
+                  ON dsi.script_id = us.script_id 
                 JOIN analysis.script_names sn 
                   ON sn.versioned_script_id = us.script_id                
                 JOIN dashboard_production_pii.users u_students
@@ -40,7 +50,7 @@ FROM (
                 JOIN analysis.school_years sy on  us.started_at between sy.started_at and sy.ended_at
        ) 
        WHERE update_rank = 1 
-       GROUP BY 1,2,3
+       GROUP BY 1,2,3,4
      )
    ) 
    WHERE student_rank = 1
