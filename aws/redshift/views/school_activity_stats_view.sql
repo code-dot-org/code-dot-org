ba5b93376cb74c9b3cb4e05d2bb552033008a7e8
Mary <marybarnes37@gmail.com>
2019-12-06 11:51:57 -0800

    add high needs column, fix group by [ci skip]

--- aws/redshift/views/school_activity_stats_view.sql
+++ aws/redshift/views/school_activity_stats_view.sql
@@ -127,5 +127,9 @@ csf_script_ids as
         ON hoc_event.school_id = ss.school_id
     LEFT JOIN applied_to_csd_csp_pd app
         ON app.school_id = ss.school_id
-  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17
+  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
   WITH NO SCHEMA BINDING;
+  
+GRANT SELECT ON analysis.school_activity_stats_view TO GROUP reader;
+GRANT ALL PRIVILEGES ON analysis.school_activity_stats_view TO GROUP admin;
+
