f604d38337c645fbe4b8c17fc90e099455f4b4da
Brendan Reville <brendan@code.org>
2019-12-09 18:51:52 -0800

    Oceans: Be responsive with React
    Rather than rely on CSS to do a variety of "stepped" responsive layouts, use JS in the React view to fill as much of the workspace as possible at every screen size.

--- apps/src/fish/FishView.jsx
+++ apps/src/fish/FishView.jsx
@@ -3,30 +3,24 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {connect} from 'react-redux';
 import StudioAppWrapper from '../templates/StudioAppWrapper';
-import ProtectedStatefulDiv from '../templates/ProtectedStatefulDiv';
 import CodeWorkspaceContainer from '../templates/CodeWorkspaceContainer';
+import _ from 'lodash';
+import $ from 'jquery';
 
 const styles = {
   container: {
     position: 'relative',
-    width: '100%',
-    minWidth: '480px',
     margin: '0 auto',
     userSelect: 'none'
   },
   containerReact: {
     position: 'absolute',
     width: '100%',
-    minWidth: '480px',
     margin: '0 auto',
     userSelect: 'none',
     fontFamily: '"Gotham 4r", arial, sans-serif',
     color: 'rgb(30,30,30)',
     lineHeight: 1.3
-    // Note that fontSize has been removed from here and is set using CSS
-    // (in fish/style.scss) instead.  That way, CSS-based media queries in the
-    // same file can successfully override the font-size throughout the
-    // inheritance chain.
   },
   backgroundCanvas: {
     position: 'absolute',
@@ -53,23 +47,68 @@ class FishView extends React.Component {
     mobilePortraitWidth: PropTypes.number.isRequired
   };
 
-  componentDidMount() {
-    this.props.onMount();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      windowWidth: $('#codeApp').width(),
+      windowHeight: $('#codeApp').height()
+    };
   }
 
+  componentDidMount() {
+    this.props.onMount();
+    window.addEventListener('resize', _.debounce(this.onResize, 100));
+    this.setState({
+      windowWidth: $('#codeApp').width(),
+      windowHeight: $('#codeApp').height()
+    });
+  }
+
+  onResize = () => {
+    const windowWidth = $('#codeApp').width();
+    const windowHeight = $('#codeApp').height();
+
+    this.setState({windowWidth, windowHeight});
+  };
+
   render() {
+    let containerWidth, containerHeight;
+
+    const maxContainerWidth = Math.min(this.state.windowWidth, 1280);
+    const maxContainerHeight = this.state.windowHeight - 27;
+
+    if (maxContainerWidth / maxContainerHeight > 16 / 9) {
+      // Constrain by height.
+      containerWidth = maxContainerHeight * (16 / 9);
+      containerHeight = maxContainerHeight;
+    } else if (maxContainerWidth / maxContainerWidth < 16 / 9) {
+      // Constrain by width.
+      containerWidth = maxContainerWidth;
+      containerHeight = maxContainerWidth * (9 / 16);
+    }
+
+    const fontSize = (18 * containerWidth) / 930;
+
     return (
       <StudioAppWrapper rotateContainerWidth={this.props.mobilePortraitWidth}>
         <CodeWorkspaceContainer topMargin={0}>
-          <ProtectedStatefulDiv
+          <div
             id="container"
-            style={styles.container}
+            style={{
+              ...styles.container,
+              width: containerWidth,
+              height: containerHeight
+            }}
             dir="ltr"
           >
-            <div id="container-react" style={styles.containerReact} />
+            <div
+              id="container-react"
+              style={{...styles.containerReact, fontSize}}
+            />
             <canvas id="background-canvas" style={styles.backgroundCanvas} />
             <canvas id="activity-canvas" style={styles.activityCanvas} />
-          </ProtectedStatefulDiv>
+          </div>
         </CodeWorkspaceContainer>
       </StudioAppWrapper>
     );
