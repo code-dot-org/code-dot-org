18ebd30bdce40ba6e39e2b7e754f94576865ce86
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-12-10 17:08:56 -0800

    exclude title from external levels (even though we don't currently actually allow it on content levels)

--- dashboard/app/dsl/external_dsl.rb
+++ dashboard/app/dsl/external_dsl.rb
@@ -2,4 +2,11 @@ class ExternalDSL < ContentDSL
   def initialize
     @hash = {href: '', options: {skip_dialog: true}}
   end
+
+  # @override
+  def self.i18n_fields
+    # external levels do not actually render their 'title' property, for
+    # whatever reason, so we actually want to _exclude_ that field from super
+    super - %w(title)
+  end
 end
