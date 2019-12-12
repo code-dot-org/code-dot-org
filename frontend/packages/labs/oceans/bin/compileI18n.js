62b35659b212aef390b6e82177951d6730ba83cf
Bethany Connor <bethany@code.org>
2019-12-12 23:47:38 +0000

    Clean up some unused (in this branch) things

--- frontend/packages/labs/oceans/bin/compileI18n.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var MessageFormat = require('messageformat');
-
-const data = fs.readFileSync('i18n/oceans.json');
-const mf = new MessageFormat('en');
-const i18n = mf.precompileObject(JSON.parse(data));
-fs.writeFileSync('i18n/oceans.js', 'export const oceans_locale = ' + i18n);
