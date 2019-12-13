a3980aa503595596d67b053e7842095eba8d2c56
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-12-13 14:54:33 -0800

    update 'missing interpolation argument' handler to behave the same as the situation where there is no interpolation

--- lib/cdo/i18n_backend.rb
+++ lib/cdo/i18n_backend.rb
@@ -1,6 +1,7 @@
 require 'i18n'
 require 'active_support/core_ext/numeric/bytes'
 require 'cdo/key_value'
+require 'cdo/honeybadger'
 
 module Cdo
   module I18n
@@ -62,8 +63,10 @@ module Cdo
       # injection, which also means we don't support using this option with
       # source strings that include HTML
       def translate(locale, key, options = ::I18n::EMPTY_HASH)
+        markdown = options.fetch(:markdown, false)
+        options.delete(:markdown)
         translation = super(locale, key, options)
-        if options.fetch(:markdown, false)
+        if markdown
           # The safe_links_only just makes sure that the URL is a "safe" web
           # one which starts with: "#", "/", "http://", "https://", "ftp://",
           # "mailto:" However, it isn't good enough because it ignores URLS
@@ -83,9 +86,63 @@ module Cdo
       end
     end
 
+    module SafeInterpolation
+      def initialize
+        super
+
+        # Override the default handler for the "missing interpolation argument"
+        # error case; default handler raises an error which will result in a
+        # 500.  We expect to end up in this situation when a string once had an
+        # interpolation argument but does no longer, and an old translation
+        # from when the string did have that argument was mistakenly applied to
+        # the new version of the string.
+        #
+        # Unfortunately, this will only actually happen when either there are
+        # multiple interpolation arguments in the string and only some of them
+        # are being used, or when there are extra arguments being passed to the
+        # I18n.t call; we want behavior to be consistent in any situation, so
+        # we update the handler here to simply return the original
+        # interpolation value.
+        #
+        # We could alternatively use something else (the name of the key, for
+        # example) as the default argument, or even fall back to the English
+        # version of the string, but we would also want to update the case
+        # where interpolation syntax goes entirely unused to match.
+        #
+        # TODO: explore the above-mentioned alternative.
+        #
+        # see: https://github.com/ruby-i18n/i18n/blob/6f3d428a5a0055ce61c5d069ce4782c4d1958bf8/lib/i18n/config.rb#L103-L116
+        ::I18n.config.missing_interpolation_argument_handler = proc do |key|
+          "%{#{key}}"
+        end
+      end
+
+      def translate(locale, key, options = ::I18n::EMPTY_HASH)
+        result = super(locale, key, options)
+        # Log unused interpolation arguments to honeybadger; these are likely
+        # the result of translations mistakenly including interpolation syntax
+        # that was removed in the source string and we want to be notified so
+        # we can update the translation.
+        if ::I18n::INTERPOLATION_PATTERN.match? result
+          Honeybadger.notify(
+            error_class: 'Interpolation Pattern present in translation',
+            error_message: "String #{result.inspect} has unused interpolation patterns after translation",
+            context: {
+              key: key,
+              locale: locale,
+              options: options,
+              result: result,
+            }
+          )
+        end
+        result
+      end
+    end
+
     class SimpleBackend < ::I18n::Backend::Simple
       include SmartTranslate
       include MarkdownTranslate
+      include SafeInterpolation
     end
 
     # I18n backend instance used by the web application.
