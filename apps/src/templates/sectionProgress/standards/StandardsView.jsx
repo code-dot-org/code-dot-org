dbd14486e6d0f6837f1775ab9012a59cf963c83b
Dani McAvoy <danimcavoy@gmail.com>
2019-12-13 15:13:44 -0500

    Move dialog to new standards component

--- apps/src/templates/sectionProgress/standards/StandardsView.jsx
+++ apps/src/templates/sectionProgress/standards/StandardsView.jsx
@@ -1,7 +1,19 @@
+import PropTypes from 'prop-types';
 import React, {Component} from 'react';
+import StandardsIntroDialog from './standards/StandardsIntroDialog';
 
 export default class StandardsView extends Component {
+  static propTypes = {
+    //Provided by redux
+    showStandardsIntroDialog: PropTypes.bool
+  };
+
   render() {
-    return <div>Coming soon...</div>;
+    return (
+      <div>
+        <StandardsIntroDialog isOpen={this.props.showStandardsIntroDialog} />
+        <p>Coming soon...</p>
+      </div>
+    );
   }
 }
