ca570c59682857aab20417ea239e7bd7662fc0f1
Dani McAvoy <danimcavoy@gmail.com>
2019-12-16 14:31:31 -0500

    Create working standards table

---
+++ apps/src/templates/sectionProgress/standards/standardsTestHelpers.js
@@ -0,0 +1,111 @@
+export const fakeStandardDescription =
+  'Compare and refine multiple algorithms for the same task and determine which is the most appropriate.';
+
+export const lessonCompletedByStandard = [
+  [
+    {
+      id: 1,
+      status: 'done'
+    },
+    {
+      id: 14,
+      status: 'done'
+    },
+    {
+      id: 15,
+      status: 'not done'
+    },
+    {
+      id: 16,
+      status: 'not done'
+    },
+    {
+      id: 17,
+      status: 'not done'
+    }
+  ],
+  [
+    {
+      id: 1,
+      status: 'done'
+    },
+    {
+      id: 2,
+      status: 'done'
+    },
+    {
+      id: 4,
+      status: 'not done'
+    },
+    {
+      id: 11,
+      status: 'not done'
+    },
+    {
+      id: 12,
+      status: 'not done'
+    },
+    {
+      id: 13,
+      status: 'not done'
+    },
+    {
+      id: 14,
+      status: 'done'
+    },
+    {
+      id: 15,
+      status: 'not done'
+    },
+    {
+      id: 16,
+      status: 'not done'
+    },
+    {
+      id: 17,
+      status: 'not done'
+    },
+    {
+      id: 18,
+      status: 'not done'
+    }
+  ]
+];
+
+export const fakeStandards = [
+  {
+    id: 1,
+    category: 'Algorithms and Programming',
+    number: '1B-AP-08',
+    description:
+      'Decompose (break down) problems into smaller, manageable subproblems to facilitate the program development process.'
+  },
+  {
+    id: 2,
+    category: 'Algorithms and Programming',
+    number: '1B-AP-11',
+    description:
+      'Compare and refine multiple algorithms for the same task and determine which is the most appropriate.'
+  },
+  {
+    id: 3,
+    category: 'Algorithms and Programming',
+    number: '1B-AP-12',
+    description:
+      'Use an iterative process to plan the development of a program by including others perspectives and considering user preferences.'
+  },
+  {
+    id: 4,
+    category: 'Networks and Internet',
+    number: '1B-NI-05',
+    description:
+      'Describe choise made during program development using code comments, presentations, and demonstrations.'
+  },
+  {
+    id: 5,
+    category: 'Networks and Internet',
+    number: '1B-N1-04',
+    description:
+      'Model how information is broken down into smaller pieces, transmitted as packets through multiple devices over networks and the Internet, and reassembled at the destination..'
+  }
+];
