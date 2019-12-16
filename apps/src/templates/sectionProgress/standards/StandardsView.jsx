d338a8f8741418c6093011b08796c98856aaccdf
Erin Bond <erin.bond@code.org>
2019-12-16 12:58:08 -0800

    Revert "Standards legend"

--- apps/src/templates/sectionProgress/standards/StandardsView.jsx
+++ apps/src/templates/sectionProgress/standards/StandardsView.jsx
@@ -1,13 +1,7 @@
 import React, {Component} from 'react';
-import StandardsLegend from './StandardsLegend';
 
 export default class StandardsView extends Component {
   render() {
-    return (
-      <div>
-        <p>Coming soon...</p>
-        <StandardsLegend />
-      </div>
-    );
+    return <div>Coming soon...</div>;
   }
 }
