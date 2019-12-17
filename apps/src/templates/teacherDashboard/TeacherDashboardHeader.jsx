25e1fc89c9192d7579ab3d3c6a69b745a952bcc4
reborn <zakaden@gmail.com>
2019-12-17 15:33:04 -0800

    Add 'edit section details' and 'select section' to section view
    This diff includes some refactoring to help facilitate the change
    -Add 'size' property to DropdownButton
    -Create RefreshAfterEditSectionDialog, like EditSectionDialog but refreshes after form submission, instead of closing
    -move out common logic between EditSectionDialog and RefreshAfterEditSectionDialog into separate functions
    -Change TeacherDashboardHeader to use Redux
    -Move TeacherDashboardNavigation from TeacherDashboardHeader into TeacherDashboard, fixing issue with using Route with Redux
    -Move code to switch between sections out of SelectSectionDropdown into library function, for use accross components
    -Initialize script selection dropdown logic with current script of section, fixing bug with sections having a scriptId but no course_id

--- apps/src/templates/teacherDashboard/TeacherDashboardHeader.jsx
+++ apps/src/templates/teacherDashboard/TeacherDashboardHeader.jsx
@@ -1,21 +1,74 @@
+import FontAwesome from './../FontAwesome';
 import React from 'react';
+import {connect} from 'react-redux';
+import {switchToSection} from './urlHelpers';
 import PropTypes from 'prop-types';
 import i18n from '@cdo/locale';
 import SmallChevronLink from '../SmallChevronLink';
-import SelectSectionDropdown from './SelectSectionDropdown';
-import TeacherDashboardNavigation from './TeacherDashboardNavigation';
+import {ReloadAfterEditSectionDialog} from './EditSectionDialog';
+import {beginEditingSection} from './teacherSectionsRedux';
+import Button from '../Button';
+import DropdownButton from '../DropdownButton';
 
 const styles = {
-  headerContainer: {
+  shortH1: {
+    lineHeight: '9px'
+  },
+  sectionPrompt: {
+    fontWeight: 'bold'
+  },
+  header: {
     display: 'flex',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    marginBottom: '5px'
+  },
+  rightColumn: {
+    display: 'flex',
+    flexDirection: 'column-reverse'
+  },
+  buttonSection: {
+    display: 'flex'
+  },
+  buttonWithMargin: {
+    marginRight: '5px'
   }
 };
 
-export default class TeacherDashboardHeader extends React.Component {
+class TeacherDashboardHeader extends React.Component {
   static propTypes = {
-    sectionName: PropTypes.string.isRequired
+    sections: PropTypes.object.isRequired,
+    selectedSectionId: PropTypes.number.isRequired,
+    selectedSectionScript: PropTypes.object.isRequired,
+    openEditSectionDialog: PropTypes.func.isRequired
   };
+
+  constructor(props) {
+    super(props);
+    this.getDropdownOptions = this.getDropdownOptions.bind(this);
+    this.selectedSection = this.props.sections[this.props.selectedSectionId];
+  }
+
+  getDropdownOptions() {
+    let self = this;
+    let sections = this.props.sections;
+    let options = Object.keys(sections).map(function(key, i) {
+      let section = sections[key];
+      let icon = undefined;
+      if (section.id === self.selectedSection.id) {
+        icon = <FontAwesome icon="check" />;
+      }
+      return (
+        <a
+          key={i}
+          id={section.id}
+          onClick={link => switchToSection(section.id, self.selectedSection.id)}
+        >
+          {icon} {section.name}
+        </a>
+      );
+    });
+    return options;
+  }
 
   render() {
     return (
@@ -26,12 +79,54 @@ export default class TeacherDashboardHeader extends React.Component {
           isRtl={true}
           chevronSide="left"
         />
-        <div style={styles.headerContainer}>
-          <h1>{this.props.sectionName}</h1>
-          <SelectSectionDropdown />
+        <div style={styles.header}>
+          <div>
+            <h1 style={styles.shortH1}>{this.selectedSection.name}</h1>
+            <div>
+              <span style={styles.sectionPrompt}>Assigned to: </span>
+              {this.props.selectedSectionScript.name}
+            </div>
+          </div>
+          <div style={styles.rightColumn}>
+            <div style={styles.buttonSection}>
+              <Button
+                onClick={() =>
+                  this.props.openEditSectionDialog(this.selectedSection.id)
+                }
+                icon="gear"
+                size="narrow"
+                color="gray"
+                text="Edit section details"
+                style={styles.buttonWithMargin}
+              />
+              <DropdownButton size="narrow" text="Select section" color="gray">
+                {this.getDropdownOptions()}
+              </DropdownButton>
+            </div>
+          </div>
         </div>
-        <TeacherDashboardNavigation />
+        <ReloadAfterEditSectionDialog />
       </div>
     );
   }
 }
+
+export const UnconnectedTeacherDashboardHeader = TeacherDashboardHeader;
+
+export default connect(
+  state => {
+    let sections = state.teacherSections.sections;
+    let selectedSectionId = state.teacherSections.selectedSectionId;
+
+    let selectedSectionScriptId = state.scriptSelection.scriptId;
+    let selectedSectionScript = state.scriptSelection.validScripts.filter(
+      script => script.id === selectedSectionScriptId
+    )[0];
+    return {sections, selectedSectionId, selectedSectionScript};
+  },
+  dispatch => {
+    return {
+      openEditSectionDialog: id => dispatch(beginEditingSection(id))
+    };
+  }
+)(TeacherDashboardHeader);
