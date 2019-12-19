623a6374c8f39375c7a07836e96d908f41353efd
Bethany Connor <bethany@code.org>
2019-12-19 13:14:02 -0800

    PR feedback

--- frontend/packages/labs/oceans/src/oceans/i18n.js
+++ frontend/packages/labs/oceans/src/oceans/i18n.js
@@ -3,10 +3,14 @@ import MessageFormat from 'messageformat'
 
 let messages;
 export const init = (i18n = {}) => {
+  // For now, use English pluralization rules.
   const mf = new MessageFormat('en');
   messages = {...mf.compile(data), ...i18n};
 };
 
 export const t = (key, options) => {
-    return messages[key](options);
+  if (!messages) {
+    throw "I18n must be initialized before calling t";
+  }
+  return messages[key](options);
 };
