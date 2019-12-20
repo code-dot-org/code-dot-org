61e274a54200254ae77ab3da7b0ab713da8c8229
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-12-19 16:22:00 -0800

    I18n.translate can in some situations have non-string returns, so be careful about when we check for unused interpolations

--- lib/cdo/i18n_backend.rb
+++ lib/cdo/i18n_backend.rb
@@ -123,7 +123,7 @@ module Cdo
         # the result of translations mistakenly including interpolation syntax
         # that was removed in the source string and we want to be notified so
         # we can update the translation.
-        if ::I18n::INTERPOLATION_PATTERN.match? result
+        if result.is_a?(String) && ::I18n::INTERPOLATION_PATTERN.match?(result)
           Honeybadger.notify(
             error_class: 'Interpolation Pattern present in translation',
             error_message: "String #{result.inspect} has unused interpolation patterns after translation",
