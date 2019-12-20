ab0cd38f6cf6cdbed4c45f9eebeb91de9365ce58
Dayne <dayne@code.org>
2019-12-19 16:07:04 -0800

    Added TODO for only loading one level's Blockly blocks

--- apps/src/sites/studio/pages/levels/_content.js
+++ apps/src/sites/studio/pages/levels/_content.js
@@ -7,15 +7,14 @@ import {convertXmlToBlockly} from '@cdo/apps/templates/instructions/utils';
 import commonBlocks from '@cdo/apps/blocksCommon';
 
 $(document).ready(() => {
-  // Content levels don't have a particular 'app' associated with them so make
-  // available all the different app specific Blockly blocks as they are needed
-  // by level creators. This will enable them to write Blockly XML and render
-  // the blocks directly in the level text.
+  // Load app specific Blockly blocks. This will enable level creators to write
+  // Blockly XML and render the blocks directly in the level text.
   Blockly.assetUrl = assetUrl;
   Blockly.Css.inject(document);
   // Install the common Blockly blocks
   commonBlocks.install(window.Blockly, {});
   // Install the custom CDO blocks for the following level/app types.
+  // TODO [FND-972] Only load the blocky content for one level type.
   var options = [{skin: {id: 'birds'}, app: 'maze'}, {skin: {}, app: 'turtle'}];
   for (var i = 0; i < options.length; i++) {
     var option = options[i];
