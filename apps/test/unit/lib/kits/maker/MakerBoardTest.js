4818e5c56752b98724da6b6ab182c1c8946f0271
Erin Peach <erin@code.org>
2020-01-06 16:16:50 -0800

    Update reference to thermometer

--- apps/test/unit/lib/kits/maker/MakerBoardTest.js
+++ apps/test/unit/lib/kits/maker/MakerBoardTest.js
@@ -104,7 +104,7 @@ export function itImplementsTheMakerBoardInterface(BoardClass) {
           'Switch',
           'Piezo',
           'Sensor',
-          'PlaygroundThermometer',
+          'Thermometer',
           'Pin',
           'Accelerometer',
           'Animation',
