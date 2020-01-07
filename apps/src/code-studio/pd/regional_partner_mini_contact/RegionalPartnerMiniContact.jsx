09f07317a2709a08381a5f7a6b4a6b5843f31b4d
Molly Moen <molly@code.org>
2020-01-07 13:19:23 -0800

    add role and grade level fields

--- apps/src/code-studio/pd/regional_partner_mini_contact/RegionalPartnerMiniContact.jsx
+++ apps/src/code-studio/pd/regional_partner_mini_contact/RegionalPartnerMiniContact.jsx
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Modal, FormGroup, Button} from 'react-bootstrap';
+import {Modal, FormGroup, Button, ControlLabel} from 'react-bootstrap';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import {SelectStyleProps} from '../constants';
 import FieldGroup from '../form_components/FieldGroup';
+import ButtonList from '../form_components/ButtonList';
 import color from '@cdo/apps/util/color';
 
 const styles = {
@@ -26,8 +30,22 @@ const styles = {
   },
   intro: {
     paddingBottom: 10
+  },
+  select: {
+    maxWidth: '500px'
   }
 };
+
+const ROLES = [
+  'Teacher',
+  'Librarian',
+  'Media Specialist',
+  'School Administrator',
+  'District Administrator',
+  'Other'
+];
+
+const GRADE_LEVEL = ['K-5', '6-8', '9-12'];
 
 export class RegionalPartnerMiniContact extends React.Component {
   static propTypes = {
@@ -35,7 +53,9 @@ export class RegionalPartnerMiniContact extends React.Component {
       user_name: PropTypes.string,
       email: PropTypes.string,
       zip: PropTypes.string,
-      notes: PropTypes.string
+      notes: PropTypes.string,
+      grade_levels: PropTypes.array,
+      role: PropTypes.string
     }),
     apiEndpoint: PropTypes.string.isRequired,
     sourcePageId: PropTypes.string.isRequired
@@ -51,7 +71,9 @@ export class RegionalPartnerMiniContact extends React.Component {
       name: this.props.options.user_name,
       email: this.props.options.email,
       zip: this.props.options.zip,
-      notes: this.props.options.notes
+      notes: this.props.options.notes,
+      role: this.props.options.role,
+      grade_levels: this.props.options.grade_levels
     };
   }
 
@@ -61,7 +83,10 @@ export class RegionalPartnerMiniContact extends React.Component {
       email: this.state.email,
       zip: this.state.zip,
       notes: this.state.notes,
-      source: this.props.sourcePageId
+      source: this.props.sourcePageId,
+      // set null or empty values to undefined so they are ignored
+      role: this.state.role || undefined,
+      grade_levels: this.state.grade_levels || undefined
     };
 
     this.setState({submitting: true});
@@ -75,10 +100,17 @@ export class RegionalPartnerMiniContact extends React.Component {
         this.onSubmitComplete(result);
       }
     });
+    this.onSubmitComplete({
+      responseJSON: {}
+    });
   };
 
   handleChange = change => {
     this.setState(change);
+  };
+
+  onRoleChange = change => {
+    this.setState({role: change.value});
   };
 
   onSubmitComplete = results => {
@@ -98,6 +130,13 @@ export class RegionalPartnerMiniContact extends React.Component {
     } else {
       this.setState({submitted: false, submitting: false});
     }
+  };
+
+  roleMap = () => {
+    const roleMap = ROLES.map(v => ({value: v, label: v}));
+    // add friendly empty value option
+    roleMap.unshift({value: '', label: '-'});
+    return roleMap;
   };
 
   render() {
@@ -163,6 +202,28 @@ export class RegionalPartnerMiniContact extends React.Component {
             onChange={this.handleChange}
             defaultValue={this.state.zip}
           />
+          <ButtonList
+            groupName="grade_levels"
+            label="Grade Level(s)"
+            type="check"
+            onChange={this.handleChange}
+            answers={GRADE_LEVEL}
+            required={false}
+            selectedItems={this.state.grade_levels}
+            style={styles.button}
+            suppressLineBreak
+          />
+          <FormGroup style={styles.select}>
+            <ControlLabel>Your role</ControlLabel>
+            <Select
+              id="role"
+              value={this.state.role}
+              onChange={this.onRoleChange}
+              placeholder="-"
+              options={this.roleMap()}
+              {...SelectStyleProps}
+            />
+          </FormGroup>
           <FieldGroup
             id="notes"
             label="Questions or notes for your local Regional Partner"
