7dd84097174cb2939c33aae8346c87cf9284e7d4
Erin Bond <erin.bond@code.org>
2020-01-08 15:41:48 -0800

    standards progress boxes link to lesson plans

--- apps/src/templates/sectionProgress/standards/ProgressBoxForLessonNumber.jsx
+++ apps/src/templates/sectionProgress/standards/ProgressBoxForLessonNumber.jsx
@@ -13,14 +13,15 @@ export default class ProgressBoxForLessonNumber extends Component {
   static propTypes = {
     completed: PropTypes.bool,
     lessonNumber: PropTypes.number,
-    tooltipId: PropTypes.string
+    tooltipId: PropTypes.string,
+    linkToLessonPlan: PropTypes.string
   };
 
   render() {
-    const {completed, lessonNumber, tooltipId} = this.props;
+    const {completed, lessonNumber, tooltipId, linkToLessonPlan} = this.props;
 
     return (
-      <div data-for={tooltipId} data-tip>
+      <a href={linkToLessonPlan} target="_blank" data-for={tooltipId} data-tip>
         <ProgressBox
           style={styles.lessonBox}
           started={completed}
@@ -29,7 +30,7 @@ export default class ProgressBoxForLessonNumber extends Component {
           perfect={completed ? 20 : 0}
           lessonNumber={lessonNumber}
         />
-      </div>
+      </a>
     );
   }
 }
