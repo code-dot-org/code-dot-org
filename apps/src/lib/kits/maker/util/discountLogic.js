9072e5b585ade307bdeb40c7d3ece6a987877576
Ben Brooks <ben@code.org>
2020-01-08 14:15:13 -0800

    New discount logic to check whether currently in redemption window

--- apps/src/lib/kits/maker/util/discountLogic.js
+++ apps/src/lib/kits/maker/util/discountLogic.js
@@ -13,6 +13,11 @@ export const Unit6Intention = {
   UNSURE: 'unsure'
 };
 
+// Dates used to establish whether a user can redeem their discount code
+export const fallEligibilityDate = new Date('2020-06-30');
+export const springEligibilityDate = new Date('2020-10-31');
+const today = new Date();
+
 /**
  * @param {string} unit6Intention
  * @returns {boolean} True if the given answer to the unit 6 question contributes to the
@@ -24,4 +29,28 @@ export function isUnit6IntentionEligible(unit6Intention) {
     Unit6Intention.YES_FALL_2020,
     Unit6Intention.YES_SPRING_2021
   ].includes(unit6Intention);
+}
+
+/**
+ * @param {string} unit6Intention
+ * @returns {boolean} True if given answer to unit 6 question
+ *   makes them eligible for a discount code, and the current
+ *   date is after the opening of the eligibility window.
+ */
+export function inDiscountRedemptionWindow(unit6Intention) {
+  if (Unit6Intention.YES_SPRING_2020 === unit6Intention) {
+    return true;
+  } else if (
+    Unit6Intention.YES_FALL_2020 === unit6Intention &&
+    today > fallEligibilityDate
+  ) {
+    return true;
+  } else if (
+    Unit6Intention.YES_SPRING_2021 === unit6Intention &&
+    today > springEligibilityDate
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
