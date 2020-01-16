3a5cd8b3e5efbff6b76d2afe50ced6eb04aa756c
Erin Peach <erin@code.org>
2020-01-16 11:12:48 -0800

    Test buttons components

--- apps/test/unit/lib/kits/maker/MicroBitComponentsTest.js
+++ apps/test/unit/lib/kits/maker/MicroBitComponentsTest.js
@@ -2,6 +2,7 @@
 import {expect} from '../../../../util/reconfiguredChai';
 import {createMicroBitComponents} from '@cdo/apps/lib/kits/maker/MicroBitComponents';
 import {MicrobitStubBoard} from './makeStubBoard';
+import five from '@code-dot-org/johnny-five';
 
 describe('MicroBit Components', () => {
   let board;
@@ -24,6 +25,60 @@ describe('MicroBit Components', () => {
           'accelerometer'
         ]);
       });
+    });
+  });
+
+  describe('buttonA', () => {
+    let buttonA;
+
+    beforeEach(() => {
+      return createMicroBitComponents(board).then(
+        components => (buttonA = components.buttonA)
+      );
+    });
+
+    it('creates a five.Button', () => {
+      expect(buttonA).to.be.an.instanceOf(five.Button);
+    });
+
+    it('bound to the board controller', () => {
+      expect(buttonA.board.mb).to.equal(board);
+    });
+
+    it('on pin 1', () => {
+      expect(buttonA.board.pin).to.equal(1);
+    });
+
+    it('with an isPressed property', () => {
+      expect(buttonA).to.haveOwnProperty('isPressed');
+      expect(buttonA.isPressed).to.be.false;
+    });
+  });
+
+  describe('buttonB', () => {
+    let buttonB;
+
+    beforeEach(() => {
+      return createMicroBitComponents(board).then(
+        components => (buttonB = components.buttonB)
+      );
+    });
+
+    it('creates a five.Button', () => {
+      expect(buttonB).to.be.an.instanceOf(five.Button);
+    });
+
+    it('bound to the board controller', () => {
+      expect(buttonB.board.mb).to.equal(board);
+    });
+
+    it('on pin 2', () => {
+      expect(buttonB.board.pin).to.equal(2);
+    });
+
+    it('with an isPressed property', () => {
+      expect(buttonB).to.haveOwnProperty('isPressed');
+      expect(buttonB.isPressed).to.be.false;
     });
   });
 });
