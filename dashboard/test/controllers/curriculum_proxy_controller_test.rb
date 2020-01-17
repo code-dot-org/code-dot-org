255de6596166292f4264975ae3adb0b3cffba228
Dani McAvoy <danimcavoy@gmail.com>
2020-01-17 14:07:48 -0500

    Use curriculum.code.org as proxy instead of docs.code.org. Get landing page working

--- dashboard/test/controllers/curriculum_proxy_controller_test.rb
+++ dashboard/test/controllers/curriculum_proxy_controller_test.rb
@@ -4,14 +4,24 @@ require_relative '../../../shared/test/spy_newrelic_agent'
 require 'test_helper'
 
 class CurriculumProxyControllerTest < ActionController::TestCase
-  test "should redirect from studio.code.org/docs path to docs.code.org path" do
-    stub_request(:get, "https://docs.code.org/csd/maker_leds/index.html").
-      to_return(body: 'docs.code.org content', headers: {})
+  test "should redirect from studio.code.org/docs to curriculum.code.org/documentation" do
+    stub_request(:get, "https://curriculum.code.org/documentation/").
+        to_return(body: 'curriculum.code.org/documentation content', headers: {})
+
+    request.host = "studio.code.org"
+    get :get_doc_landing
+    assert_response :success
+    assert_equal response.body, 'curriculum.code.org/documentation content'
+  end
+
+  test "should redirect from studio.code.org/docs path to curriculum.code.org/documentation path" do
+    stub_request(:get, "https://curriculum.code.org/documentation/csd/maker_leds/index.html").
+        to_return(body: 'curriculum.code.org/documentation content', headers: {})
 
     request.host = "studio.code.org"
     get :get_doc, params: {path: 'csd/maker_leds/index.html'}
     assert_response :success
-    assert_equal response.body, 'docs.code.org content'
+    assert_equal response.body, 'curriculum.code.org content'
   end
 
   test "should redirect from studio.code.org/curriculum path to curriculum.code.org path" do
