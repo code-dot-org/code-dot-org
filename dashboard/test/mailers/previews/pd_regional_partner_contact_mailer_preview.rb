678fed30648560d514b771ec73d8617839298560
Ben Brooks <ben@code.org>
2020-01-22 10:29:21 -0800

    DRY regional partner contact mailer previews

--- dashboard/test/mailers/previews/pd_regional_partner_contact_mailer_preview.rb
+++ dashboard/test/mailers/previews/pd_regional_partner_contact_mailer_preview.rb
@@ -4,24 +4,35 @@ class Pd::RegionalPartnerContactMailerPreview < ActionMailer::Preview
 
   def contact_receipt_with_partner
     rp = build :regional_partner
-    rp_contact = build :pd_regional_partner_contact, regional_partner: rp
-    form = rp_contact.sanitize_and_trim_form_data_hash
-
+    form = build_form_data(:pd_regional_partner_contact, regional_partner: rp)
     Pd::RegionalPartnerContactMailer.receipt(form, rp)
   end
 
   def contact_receipt_no_partner
-    rp_contact = build :pd_regional_partner_contact
-    form = rp_contact.sanitize_and_trim_form_data_hash
-
+    form = build_form_data(:pd_regional_partner_contact)
     Pd::RegionalPartnerContactMailer.receipt(form, nil)
+  end
+
+  def contact_unmatched
+    form = build_form_data(:pd_regional_partner_contact)
+    Pd::RegionalPartnerContactMailer.unmatched(form, 'test+employee@code.org')
   end
 
   def mini_contact_matched_partner
     rp_pm = build :regional_partner_program_manager
-    rp_mini_contact = build :pd_regional_partner_mini_contact, regional_partner: rp_pm.regional_partner
-    form = rp_mini_contact.sanitize_and_trim_form_data_hash
-
+    form = build_form_data(:pd_regional_partner_mini_contact, regional_partner: rp_pm.regional_partner)
     Pd::RegionalPartnerContactMailer.matched(form, rp_pm)
+  end
+
+  def mini_contact_unmatched
+    form = build_form_data(:pd_regional_partner_mini_contact)
+    Pd::RegionalPartnerContactMailer.unmatched(form, 'test+employee@code.org')
+  end
+
+  private
+
+  def build_form_data(contact_factory, factory_options={})
+    contact = build contact_factory, factory_options
+    contact.sanitize_and_trim_form_data_hash
   end
 end
