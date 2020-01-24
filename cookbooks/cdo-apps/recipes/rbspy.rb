c965e6170c65c9994ffb629faf90c48903ecb0b2
Winter Dong <winter@code.org>
2020-01-23 16:03:16 -0800

    Add comment

--- cookbooks/cdo-apps/recipes/rbspy.rb
+++ cookbooks/cdo-apps/recipes/rbspy.rb
@@ -1,3 +1,5 @@
+# Install https://rbspy.github.io/ to allow manual debugging of ruby performance issues.
+
 tar_extract 'https://github.com/rbspy/rbspy/releases/download/v0.3.8/rbspy-v0.3.8-x86_64-unknown-linux-musl.tar.gz' do
   target_dir '/usr/local/bin'
   creates '/usr/local/bin/rbspy'
