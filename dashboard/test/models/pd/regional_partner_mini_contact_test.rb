b21c9f5ac47f5d469a8fdd0473b9c5e6cd4a1490
Brendan Reville <brendan@code.org>
2020-01-27 12:54:47 -0800

    PL: test zip lookup fails for nil zip

--- dashboard/test/models/pd/regional_partner_mini_contact_test.rb
+++ dashboard/test/models/pd/regional_partner_mini_contact_test.rb
@@ -152,4 +152,8 @@ class Pd::RegionalPartnerMiniContactTest < ActiveSupport::TestCase
     assert_equal ['noreply@code.org'], mail.from
     assert_sendable mail
   end
+
+  test 'Fails lookup with nil zip' do
+    assert_equal RegionalPartner.find_by_zip(nil), [nil, nil]
+  end
 end
