f85eb19f10cb9dd0ca2821aa85407080f2557b30
Ben Brooks <ben@code.org>
2020-01-31 16:42:48 -0800

    Add test for course-specific scholarship options

--- dashboard/test/models/pd/scholarship_info_test.rb
+++ dashboard/test/models/pd/scholarship_info_test.rb
@@ -42,4 +42,9 @@ class Pd::ScholarshipInfoTest < ActiveSupport::TestCase
     scholarship_info.reload
     assert_equal Pd::ScholarshipInfoConstants::YES_OTHER, scholarship_info.scholarship_status
   end
+
+  test 'cannot create CSF scholarship info with CSP-specific scholarship status' do
+    csf_scholarship_info = build :pd_scholarship_info, course: COURSE_KEY_MAP[COURSE_CSF], scholarship_status: Pd::ScholarshipInfo::YES_EIR
+    assert_not_nil csf_scholarship_info.errors[:scholarship_status]
+  end
 end
