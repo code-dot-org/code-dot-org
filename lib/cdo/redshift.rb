6d1a312a5dd724132f921cfc29c123a5fcf565a0
Suresh Chanmugam <suresh@code.org>
2020-02-04 17:03:31 -0800

    Add error handling to query execution method.

--- lib/cdo/redshift.rb
+++ lib/cdo/redshift.rb
@@ -5,6 +5,27 @@ require 'singleton'
 # instance.
 class RedshiftClient
   include Singleton
+  class PostgreSQLQueryError < StandardError; end
+
+  # See section on PQresultStatus in https://www.postgresql.org/docs/9.1/libpq-exec.html
+  QUERY_SUCCESS_CODES = %w(
+    PGRES_COMMAND_OK
+    PGRES_TUPLES_OK
+    PGRES_COPY_OUT
+    PGRES_COPY_IN
+    PGRES_COPY_BOTH
+  ).freeze
+
+  # We don't expect to actually receive this warning https://www.postgresql.org/docs/9.1/libpq-notice-processing.html
+  QUERY_WARNING_CODES = %w(
+    PGRES_NONFATAL_ERROR
+  ).freeze
+
+  QUERY_ERROR_CODES = %w(
+    PGRES_EMPTY_QUERY
+    PGRES_BAD_RESPONSE
+    PGRES_FATAL_ERROR
+  ).freeze
 
   def initialize
     port = 5439
@@ -16,18 +37,23 @@ class RedshiftClient
   end
 
   def exec(sql_query)
-    @conn.exec(sql_query)
+    result = @conn.exec(sql_query)
+    # result_status is an undocumented method that returns the integer query status.
+    # res_status is an undocumented method that returns the string identifier for a given integer query status.
+    result_status = result.res_status(result.result_status)
+    raise PostgreSQLQueryError.new(result_status) unless QUERY_SUCCESS_CODES.include? result_status
+    result
   end
 
   def truncate_table(schema, table)
-    @conn.exec "TRUNCATE #{schema}.#{table}"
+    exec "TRUNCATE #{schema}.#{table}"
   end
 
   def drop_table(schema, table)
-    @conn.exec "DROP TABLE IF EXISTS #{schema}.#{table}"
+    exec "DROP TABLE IF EXISTS #{schema}.#{table}"
   end
 
   def move_rows(source_schema, source_table, target_schema, target_table)
-    @conn.exec "ALTER TABLE #{target_schema}.#{target_table} APPEND FROM #{source_schema}.#{source_table}"
+    exec "ALTER TABLE #{target_schema}.#{target_table} APPEND FROM #{source_schema}.#{source_table}"
   end
 end
