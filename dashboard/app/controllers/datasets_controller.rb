718dc309c4df1fc593ec3668ac961faeb19f1fb1
Anjali <anjali@code.org>
2020-02-05 15:10:30 -0800

    Show library manifest

--- dashboard/app/controllers/datasets_controller.rb
+++ dashboard/app/controllers/datasets_controller.rb
@@ -1,9 +1,17 @@
 class DatasetsController < ApplicationController
   before_action :require_levelbuilder_mode
+  before_action :initialize_firebase
 
   def show_manifest
+    @dataset_library_manifest = @firebase.get_library_manifest
   end
 
   def update_manifest
+  end
+
+  private
+
+  def initialize_firebase
+    @firebase = FirebaseHelper.new('shared')
   end
 end
