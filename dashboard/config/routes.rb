0b2fb48db7dfc0cfae2a7fd938f27ff99b747699
Ben Brooks <ben@code.org>
2020-02-07 12:21:34 -0800

    Remove api/v1 regional_partner_contacts controller

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -433,7 +433,6 @@ Dashboard::Application.routes.draw do
       post :pre_workshop_surveys, to: 'pre_workshop_surveys#create'
       post :workshop_surveys, to: 'workshop_surveys#create'
       post :teachercon_surveys, to: 'teachercon_surveys#create'
-      post :regional_partner_contacts, to: 'regional_partner_contacts#create'
       post :regional_partner_mini_contacts, to: 'regional_partner_mini_contacts#create'
       post :international_opt_ins, to: 'international_opt_ins#create'
       get :regional_partner_workshops, to: 'regional_partner_workshops#index'
