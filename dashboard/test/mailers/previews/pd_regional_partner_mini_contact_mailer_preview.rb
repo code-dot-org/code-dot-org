2a6a9bb47f4ab0f9dc0a7980c6451bca0cc47656
Ben Brooks <ben@code.org>
2020-02-07 15:03:18 -0800

    Rename regional partner contact to mini contact mailer

--- dashboard/test/mailers/previews/pd_regional_partner_mini_contact_mailer_preview.rb
+++ dashboard/test/mailers/previews/pd_regional_partner_mini_contact_mailer_preview.rb
@@ -1,32 +1,32 @@
 # This can be viewed on non-production environments at /rails/mailers/pd/regional_partner_contact_mailer
-class Pd::RegionalPartnerContactMailerPreview < ActionMailer::Preview
+class Pd::RegionalPartnerMiniContactMailerPreview < ActionMailer::Preview
   include FactoryGirl::Syntax::Methods
 
   def contact_receipt_with_partner
     rp = build :regional_partner
     form = build_form_data(:pd_regional_partner_contact, regional_partner: rp)
-    Pd::RegionalPartnerContactMailer.receipt(form, rp)
+    Pd::RegionalPartnerMiniContactMailer.receipt(form, rp)
   end
 
   def contact_receipt_no_partner
     form = build_form_data(:pd_regional_partner_contact)
-    Pd::RegionalPartnerContactMailer.receipt(form, nil)
+    Pd::RegionalPartnerMiniContactMailer.receipt(form, nil)
   end
 
   def contact_unmatched
     form = build_form_data(:pd_regional_partner_contact)
-    Pd::RegionalPartnerContactMailer.unmatched(form, 'test+employee@code.org')
+    Pd::RegionalPartnerMiniContactMailer.unmatched(form, 'test+employee@code.org')
   end
 
   def mini_contact_matched_partner
     rp_pm = create :regional_partner_program_manager
     form = build_form_data(:pd_regional_partner_mini_contact, regional_partner: rp_pm.regional_partner)
-    Pd::RegionalPartnerContactMailer.matched(form, rp_pm)
+    Pd::RegionalPartnerMiniContactMailer.matched(form, rp_pm)
   end
 
   def mini_contact_unmatched
     form = build_form_data(:pd_regional_partner_mini_contact)
-    Pd::RegionalPartnerContactMailer.unmatched(form, 'test+employee@code.org')
+    Pd::RegionalPartnerMiniContactMailer.unmatched(form, 'test+employee@code.org')
   end
 
   private
