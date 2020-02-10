bc8297a7e7bd0e3e10e4a6572ed7af674ee88d4a
Anjali <anjali@code.org>
2020-02-10 13:31:03 -0800

    Require levelbuilder permission

--- dashboard/app/controllers/datasets_controller.rb
+++ dashboard/app/controllers/datasets_controller.rb
@@ -1,6 +1,7 @@
 class DatasetsController < ApplicationController
   before_action :require_levelbuilder_mode
   before_action :initialize_firebase
+  authorize_resource class: false
 
   def show_manifest
     @dataset_library_manifest = @firebase.get_library_manifest
