425c8447e1e0667b5a7892468821559171699617
Dani McAvoy <danimcavoy@gmail.com>
2020-02-10 20:36:45 -0500

    Update StandardsReport tests to use the new window opener information

--- apps/test/unit/templates/sectionProgress/StandardsReportTest.jsx
+++ apps/test/unit/templates/sectionProgress/StandardsReportTest.jsx
@@ -9,18 +9,22 @@ describe('StandardsReport', () => {
 
   beforeEach(() => {
     DEFAULT_PROPS = {
-      scriptId: 1,
+      scriptId: 2,
       loadScript: () => {},
       teacherName: 'Awesome Teacher',
       sectionName: 'Great Section',
-      teacherComment: 'I love my class they are wonderful',
+      teacherComment: null,
       scriptDescription: 'This script teaches things',
       numStudentsInSection: 15,
       numLessonsCompleted: 5,
       numLessonsInUnit: 10,
       getStandardsCoveredForScript: () => {},
-      setTeacherCommentForReport: () => {},
-      setScriptId: () => {},
+      setTeacherCommentForReport: comment => {
+        DEFAULT_PROPS.teacherComment = comment;
+      },
+      setScriptId: scriptId => {
+        DEFAULT_PROPS.scriptId = scriptId;
+      },
       section: {
         id: 6,
         script: {
@@ -40,13 +44,6 @@ describe('StandardsReport', () => {
       },
       scriptFriendlyName: 'Express Course (2019)'
     };
-
-    replaceOnWindow('opener', {
-      teacherDashboardStoreInformation: {
-        scriptId: 1,
-        teacherComment: 'Comment!'
-      }
-    });
   });
 
   afterEach(() => {
@@ -54,24 +51,60 @@ describe('StandardsReport', () => {
   });
 
   it('report shows print buttons', () => {
+    replaceOnWindow('opener', {
+      teacherDashboardStoreInformation: {
+        scriptId: 1,
+        teacherComment: 'Comment!'
+      }
+    });
     const wrapper = shallow(<StandardsReport {...DEFAULT_PROPS} />);
+    // componentDidMount resets the value of DEFAULT_PROPS but we have to force
+    // the wrapper to reset the props
+    wrapper.setProps(DEFAULT_PROPS);
     expect(wrapper.find('PrintReportButton')).to.have.lengthOf(2);
   });
 
   it('report shows StandardsReportHeader', () => {
+    replaceOnWindow('opener', {
+      teacherDashboardStoreInformation: {
+        scriptId: 1,
+        teacherComment: 'Comment!'
+      }
+    });
     const wrapper = shallow(<StandardsReport {...DEFAULT_PROPS} />);
+    // componentDidMount resets the value of DEFAULT_PROPS but we have to force
+    // the wrapper to reset the props
+    wrapper.setProps(DEFAULT_PROPS);
     expect(wrapper.find('StandardsReportHeader')).to.have.lengthOf(1);
   });
 
   it('report shows StandardsReportCurrentCourseInfo', () => {
+    replaceOnWindow('opener', {
+      teacherDashboardStoreInformation: {
+        scriptId: 1,
+        teacherComment: 'Comment!'
+      }
+    });
     const wrapper = shallow(<StandardsReport {...DEFAULT_PROPS} />);
+    // componentDidMount resets the value of DEFAULT_PROPS but we have to force
+    // the wrapper to reset the props
+    wrapper.setProps(DEFAULT_PROPS);
     expect(wrapper.find('StandardsReportCurrentCourseInfo')).to.have.lengthOf(
       1
     );
   });
 
   it('report shows teacher comment if one exists', () => {
+    replaceOnWindow('opener', {
+      teacherDashboardStoreInformation: {
+        scriptId: 1,
+        teacherComment: 'I love my class they are wonderful'
+      }
+    });
     const wrapper = shallow(<StandardsReport {...DEFAULT_PROPS} />);
+    // componentDidMount resets the value of DEFAULT_PROPS but we have to force
+    // the wrapper to reset the props
+    wrapper.setProps(DEFAULT_PROPS);
     expect(wrapper.contains('Teacher comments')).to.equal(true);
     expect(wrapper.contains('I love my class they are wonderful')).to.equal(
       true
@@ -79,14 +112,30 @@ describe('StandardsReport', () => {
   });
 
   it('report does not show teacher comment section if there is no comment', () => {
-    const wrapper = shallow(
-      <StandardsReport {...DEFAULT_PROPS} teacherComment={null} />
-    );
+    replaceOnWindow('opener', {
+      teacherDashboardStoreInformation: {
+        scriptId: 1,
+        teacherComment: null
+      }
+    });
+    const wrapper = shallow(<StandardsReport {...DEFAULT_PROPS} />);
+    // componentDidMount resets the value of DEFAULT_PROPS but we have to force
+    // the wrapper to reset the props
+    wrapper.setProps(DEFAULT_PROPS);
     expect(wrapper.contains('Teacher comments')).to.equal(false);
   });
 
   it('report shows StandardsProgressTable', () => {
+    replaceOnWindow('opener', {
+      teacherDashboardStoreInformation: {
+        scriptId: 1,
+        teacherComment: 'Comment!'
+      }
+    });
     const wrapper = shallow(<StandardsReport {...DEFAULT_PROPS} />);
+    // componentDidMount resets the value of DEFAULT_PROPS but we have to force
+    // the wrapper to reset the props
+    wrapper.setProps(DEFAULT_PROPS);
     expect(wrapper.find('Connect(StandardsProgressTable)')).to.have.lengthOf(1);
   });
 });
