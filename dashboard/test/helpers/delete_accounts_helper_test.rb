48a850490bbf9868a6055eb618ce18ffc8161921
Ben Brooks <ben@code.org>
2020-02-11 14:37:17 -0800

    Stub call to Geocoder

--- dashboard/test/helpers/delete_accounts_helper_test.rb
+++ dashboard/test/helpers/delete_accounts_helper_test.rb
@@ -948,6 +948,8 @@ class DeleteAccountsHelperTest < ActionView::TestCase
   #
 
   test "clears form_data from pd_regional_partner_contacts" do
+    RegionalPartner.stubs(:find_by_zip).returns([nil, nil])
+
     teacher = create :teacher
     contact = create :pd_regional_partner_contact, user: teacher
     refute_equal '{}', contact.form_data
