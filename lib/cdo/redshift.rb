17c99c1d313aa2cab43be0e9b8c7433ae2b408da
Suresh Chanmugam <suresh@code.org>
2020-02-12 08:50:29 -0800

    Move logic that imports data into Redshift into a separate class.

--- lib/cdo/redshift.rb
+++ lib/cdo/redshift.rb
@@ -28,9 +28,6 @@ class RedshiftClient
     PGRES_FATAL_ERROR
   ).freeze
 
-  # Database Migration Service Replication Tasks load data from Aurora into staging Redshift tables with a prefix.
-  TEMP_TABLE_PREFIX = '_import_'.freeze
-
   def initialize
     port = 5439
     options = ''
@@ -47,48 +44,5 @@ class RedshiftClient
     result_status = result.res_status(result.result_status)
     raise PostgreSQLQueryError.new(result_status) unless QUERY_SUCCESS_CODES.include? result_status
     result
-  end
-
-  # DMS Replication Tasks import data into temporary / staging tables prefixed with "_import_".
-  # Complete import of staged data from production database into Redshift by truncating each
-  # target table, moving all of the rows from each import table to its corresponding target table, and then dropping
-  # each temporary / staging import table.
-  def complete_table_import
-    Cdo::DMS::REDSHIFT_SCHEMAS_IMPORTED_FROM_DATABASE.each do |schema|
-      temporary_import_tables(schema).each do |import_table|
-        target_table = import_table.partition(TEMP_TABLE_PREFIX).last
-        truncate_table(schema, target_table)
-        move_rows(schema, import_table, schema, target_table)
-        drop_table(schema, import_table)
-      end
-    end
-  end
-
-  # Get names of tables in a Redshift schema that were loaded via DMS to stage imported data from the database.
-  # @param schema [String] The Redshift schema to search for database import staging tables.
-  # Returns Array of Redshift table names.
-  def temporary_import_tables(schema)
-    query = <<-SQL
-SET search_path TO #{schema};
-
-SELECT DISTINCT t.tablename
-FROM pg_table_def t LEFT JOIN pg_indexes i ON t.tablename = i.indexname
-WHERE  i.indexname IS NULL -- Don't count primary keys, which appear in pg_table_def, as a table.
-  AND  t.schemaname ='#{schema}'
-  AND t.tablename LIKE '#{TEMP_TABLE_PREFIX}%';
-SQL
-    exec(query).map {|row| row['tablename']}
-  end
-
-  def truncate_table(schema, table)
-    exec "TRUNCATE #{schema}.#{table};"
-  end
-
-  def drop_table(schema, table)
-    exec "DROP TABLE IF EXISTS #{schema}.#{table};"
-  end
-
-  def move_rows(source_schema, source_table, target_schema, target_table)
-    exec "ALTER TABLE #{target_schema}.#{target_table} APPEND FROM #{source_schema}.#{source_table};"
   end
 end
