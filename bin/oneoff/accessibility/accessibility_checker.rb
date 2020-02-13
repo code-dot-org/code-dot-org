8d8d6dbd621f3d20608adfc3851e54eaf37f5891
Erin Peach <erin@code.org>
2020-02-12 16:15:33 -0800

    Accessibility script and config

--- bin/oneoff/accessibility/accessibility_checker.rb
+++ bin/oneoff/accessibility/accessibility_checker.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 require_relative '../../../deployment'
 require 'cdo/only_one'
-require 'pa11y'
+require 'json'
 
-urls = ['studio.code.org']
-
-pa11y_path = ""
+PALLY_PATH = "../../../apps/node_modules/pa11y-ci/bin/pa11y-ci.js"
+CONFIG_PATH = "./config.json"
 
 def main
+  puts "Scanning for Accessibility Errors and Warnings"
+  puts "Using WCAG2A standard using PA11Y tool {http://pa11y.org}"
+  puts "JSON output in pa11y_log.txt"
+
+  # call pa11y
+  `#{PALLY_PATH} -j -c #{CONFIG_PATH} > pa11y_log.txt`
 end
 
 main if only_one_running?(__FILE__)
