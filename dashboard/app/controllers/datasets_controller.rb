8e4372475030f0ca7388155795dd5c54def16818
Anjali <anjali@code.org>
2020-02-13 15:43:19 -0800

    index page

--- dashboard/app/controllers/datasets_controller.rb
+++ dashboard/app/controllers/datasets_controller.rb
@@ -5,8 +5,13 @@ class DatasetsController < ApplicationController
   before_action :initialize_firebase
   authorize_resource class: false
 
+  LIVE_DATASETS = ['Daily Weather', 'Top 200 USA', 'Top 200 Worldwide', 'Viral 50 USA', 'Viral 50 Worldwide']
+
   # GET /datasets
   def index
+    tables = @firebase.get_shared_table_list
+    @datasets = tables.map {|name, _| name}
+    @live_datasets = LIVE_DATASETS
   end
 
   # GET /datasets/:dataset_name/
