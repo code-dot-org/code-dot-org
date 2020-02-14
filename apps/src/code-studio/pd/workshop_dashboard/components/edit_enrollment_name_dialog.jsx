332a65ac3bc351fa394cad522bac8d29046a0b5b
Ben Brooks <ben@code.org>
2020-02-14 15:01:48 -0800

    Fix linting in new enrollment name change modal

--- apps/src/code-studio/pd/workshop_dashboard/components/edit_enrollment_name_dialog.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/edit_enrollment_name_dialog.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import {Modal, Button} from 'react-bootstrap';
 import _ from 'lodash';
 
-export default class EditEnrollmentNameDialog extends React.Component {
+export default class Edit_enrollment_name_dialog extends React.Component {
   static propTypes = {
     selectedEnrollment: PropTypes.object,
     show: PropTypes.bool,
     onCancel: PropTypes.func,
-    onMove: PropTypes.func
+    onEdit: PropTypes.func
   };
 
   constructor(props) {
@@ -27,13 +27,12 @@ export default class EditEnrollmentNameDialog extends React.Component {
     this.setState({[e.target.name]: e.target.value});
   }
 
-  // use enrollment ID here
   handleClickUpdate() {
-    this.props.onMove(_.pick(this.state, ['firstName', 'lastName']));
+    this.props.onEdit(_.pick(this.state, ['firstName', 'lastName']));
   }
 
-// need to add default values
-// defaultValue={this.props.selectedEnrollment.first_name}
+  // TO DO: add default values
+  // defaultValue={this.props.selectedEnrollment.first_name} causing errors on page load
   render() {
     return (
       <Modal show={this.props.show} onHide={this.props.onCancel}>
@@ -44,11 +43,20 @@ export default class EditEnrollmentNameDialog extends React.Component {
           <div>
             <label>
               First name:{' '}
-              <input type="text" name="firstName" onChange={this.handleInputChange} />
+              <input
+                type="text"
+                name="firstName"
+                defaultValue={this.props.selectedEnrollment.firstName}
+                onChange={this.handleInputChange}
+              />
             </label>
             <label>
               Last name:{' '}
-              <input type="text" name="lastName" onChange={this.handleInputChange} />
+              <input
+                type="text"
+                name="lastName"
+                onChange={this.handleInputChange}
+              />
             </label>
           </div>
         </Modal.Body>
