ccb01c99533570925fbab2ff4d44454c5b0be7df
Anjali <anjali@code.org>
2020-02-17 15:03:28 -0800

    import button

--- apps/src/storage/levelbuilder/Dataset.jsx
+++ apps/src/storage/levelbuilder/Dataset.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import color from '@cdo/apps/util/color';
+import {
+  changeView,
+  updateTableColumns,
+  updateTableRecords
+} from '../redux/data';
+import {DataView} from '../constants';
+import ConfirmImportButton from '../dataBrowser/ConfirmImportButton';
 import DataTable from '../dataBrowser/DataTable';
+
+const styles = {
+  error: {
+    color: color.red,
+    backgroundColor: color.lightest_red,
+    padding: 10,
+    fontSize: 14
+  },
+  success: {
+    color: color.realgreen,
+    backgroundColor: color.lighter_green,
+    padding: 10,
+    fontSize: 14
+  }
+};
 
 class Dataset extends React.Component {
   static propTypes = {
@@ -14,17 +37,58 @@ class Dataset extends React.Component {
     // it as an array."
     // https://firebase.googleblog.com/2014/04/best-practices-arrays-in-firebase.html
     tableRecords: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
-      .isRequired
+      .isRequired,
+    onUploadComplete: PropTypes.func.isRequired
   };
+
+  state = {
+    notice: null,
+    isError: false
+  };
+
+  displayNotice = (notice, isError) => {
+    this.setState({notice, isError}, () =>
+      setTimeout(() => this.setState({notice: null, isError: false}), 5000)
+    );
+    window.scrollTo(0, 0);
+  };
+
+  importCsv = (csv, onComplete) => {
+    $.ajax({
+      url: `/datasets/${this.props.tableName}`,
+      method: 'PUT',
+      contentType: 'application/json',
+      data: JSON.stringify({csv_data: csv})
+    })
+      .done(data => {
+        this.props.onUploadComplete(
+          this.props.tableName,
+          Object.values(data.records),
+          data.columns
+        );
+        this.displayNotice('Upload succeeeded.', false);
+        onComplete();
+      })
+      .fail(() => this.displayNotice('Upload failed.', false));
+  };
+
   render() {
     return (
       <div>
+        {this.state.notice && (
+          <p style={this.state.isError ? styles.error : styles.success}>
+            {this.state.notice}
+          </p>
+        )}
         <h1>{`${this.props.tableName}${
           this.props.isLive ? ' (Live)' : ''
         }`}</h1>
         <p>
           <a href="/datasets">Back to Index</a>
         </p>
+        {!this.props.isLive && (
+          <ConfirmImportButton importCsv={this.importCsv} />
+        )}
         <DataTable readOnly rowsPerPage={10} />
       </div>
     );
@@ -37,5 +101,11 @@ export default connect(
     tableRecords: state.data.tableRecords || {},
     tableName: state.data.tableName || ''
   }),
-  dispatch => ({})
+  dispatch => ({
+    onUploadComplete(tableName, records, columns) {
+      dispatch(changeView(DataView.TABLE, tableName));
+      dispatch(updateTableRecords(tableName, records));
+      dispatch(updateTableColumns(tableName, columns));
+    }
+  })
 )(Dataset);
