a76c9a3d53238d4b3655caac02b861659ff1afa7
Ben Brooks <ben@code.org>
2020-02-17 15:16:30 -0800

    Use bootstrap grid for better formatting in modal

--- apps/src/code-studio/pd/workshop_dashboard/components/edit_enrollment_name_dialog.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/edit_enrollment_name_dialog.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Modal, Button} from 'react-bootstrap';
+import {Modal, Button, Row, Col, FormGroup} from 'react-bootstrap';
 import _ from 'lodash';
 
 export default class EditEnrollmentNameDialog extends React.Component {
   static propTypes = {
     selectedEnrollment: PropTypes.object,
     show: PropTypes.bool,
+    onShow: PropTypes.func,
     onCancel: PropTypes.func,
     onEdit: PropTypes.func
   };
@@ -15,12 +16,22 @@ export default class EditEnrollmentNameDialog extends React.Component {
     super(props);
 
     this.state = {
-      firstName: null,
-      lastName: null
+      firstName: '',
+      lastName: ''
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleClickUpdate = this.handleClickUpdate.bind(this);
+  }
+
+  // set initial value for each input field when modal is shown
+  componentDidUpdate(prevProps) {
+    if (prevProps.show === false && this.props.show === true) {
+      this.setState({
+        firstName: this.props.selectedEnrollment.first_name,
+        lastName: this.props.selectedEnrollment.last_name
+      });
+    }
   }
 
   handleInputChange(e) {
@@ -38,24 +49,32 @@ export default class EditEnrollmentNameDialog extends React.Component {
           <Modal.Title>Edit Enrollee Name</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>
-            <label>
-              First name:{' '}
-              <input
-                type="text"
-                name="firstName"
-                onChange={this.handleInputChange}
-              />
-            </label>
-            <label>
-              Last name:{' '}
-              <input
-                type="text"
-                name="lastName"
-                onChange={this.handleInputChange}
-              />
-            </label>
-          </div>
+          <FormGroup>
+            <Row>
+              <Col sm={3}>First name:</Col>
+              <Col sm={3}>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={this.state.firstName}
+                  onChange={this.handleInputChange}
+                />
+              </Col>
+            </Row>
+          </FormGroup>
+          <FormGroup>
+            <Row>
+              <Col sm={3}>Last name:</Col>
+              <Col sm={3}>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={this.state.lastName}
+                  onChange={this.handleInputChange}
+                />
+              </Col>
+            </Row>
+          </FormGroup>
         </Modal.Body>
         <Modal.Footer>
           <Button bsStyle="primary" onClick={this.handleClickUpdate}>
