1c2b5e67347a025a0dcf67778967b35fb523a10d
Dani McAvoy <danimcavoy@gmail.com>
2020-02-18 22:55:22 -0500

    Extract list of levels that need the teacher only markdown editor

--- dashboard/app/models/levels/level.rb
+++ dashboard/app/models/levels/level.rb
@@ -155,6 +155,12 @@ class Level < ActiveRecord::Base
     !unplugged?
   end
 
+  # This does not include DSL levels which also use teacher markdown
+  # but access it in a different way
+  def include_teacher_only_markdown_editor?
+    uses_droplet? || is_a?(Blockly) || is_a?(ExternalLink) || is_a?(Weblab) || is_a?(CurriculumReference) || is_a?(StandaloneVideo)
+  end
+
   def enable_scrolling?
     is_a?(Blockly)
   end
