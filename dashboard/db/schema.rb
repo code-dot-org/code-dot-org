8292c3732090fd5a07a158a263cdf6b12e4e781f
Erin Bond <erin.bond@code.org>
2020-02-20 13:11:34 -0800

    create teacher_scores table with indexes

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200206194012) do
+ActiveRecord::Schema.define(version: 20200220203659) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -1435,6 +1435,17 @@ ActiveRecord::Schema.define(version: 20200206194012) do
     t.string   "other_pd"
     t.text     "properties",       limit: 65535
     t.index ["studio_person_id"], name: "index_teacher_profiles_on_studio_person_id", using: :btree
+  end
+
+  create_table "teacher_scores", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer  "user_level_id"
+    t.integer  "teacher_id"
+    t.integer  "score"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["created_at"], name: "index_teacher_scores_on_created_at", using: :btree
+    t.index ["teacher_id"], name: "index_teacher_scores_on_teacher_id", using: :btree
+    t.index ["user_level_id"], name: "index_teacher_scores_on_user_level_id", using: :btree
   end
 
   create_table "user_geos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
