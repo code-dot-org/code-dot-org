b20faa3582ea8f3718554ba635e3f1a2ddb8c3a5
Brendan Reville <brendan@code.org>
2020-02-24 13:53:33 -0800

    PL: Remove more sample Foorm implementation

--- apps/src/sites/studio/pages/pd/pre_workshop_survey/new.js
+++ apps/src/sites/studio/pages/pd/pre_workshop_survey/new.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PreWorkshopSurvey from '@cdo/apps/code-studio/pd/pre_workshop_survey/PreWorkshopSurvey';
 import getScriptData from '@cdo/apps/util/getScriptData';
-import Foorm from '@cdo/apps/code-studio/pd/foorm/Foorm';
 
 document.addEventListener('DOMContentLoaded', function(event) {
   ReactDOM.render(
     <PreWorkshopSurvey {...getScriptData('props')} />,
     document.getElementById('application-container')
-  );
-
-  ReactDOM.render(
-    <Foorm {...getScriptData('props')} />,
-    document.getElementById('new-application-container')
   );
 });
