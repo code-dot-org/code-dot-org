3613a7fff212f7c9b17151387bed082cbbd98529
Brendan Reville <brendan@code.org>
2020-02-24 11:05:07 -0800

    Foorm: Initial migration
    This is the initial database migration for the new Foorm system which will be added in https://github.com/code-dot-org/code-dot-org/pull/33182.

---
+++ dashboard/app/models/foorm/form.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: foorm_forms
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)      not null
+#  version    :integer          not null
+#  questions  :text(65535)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_foorm_forms_on_name_and_version  (name,version) UNIQUE
+#
+
+class Foorm::Form < ActiveRecord::Base
+end
