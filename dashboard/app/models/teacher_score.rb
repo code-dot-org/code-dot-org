8b67c267a2692c9ec165c7b0d578ee7b3a989403
Erin Bond <erin.bond@code.org>
2020-02-26 15:43:22 -0800

    delete unneeded code wherein I was trying to do too much on the server side

--- dashboard/app/models/teacher_score.rb
+++ dashboard/app/models/teacher_score.rb
@@ -55,27 +55,7 @@ class TeacherScore < ApplicationRecord
     )
   end
 
-  # When a teacher marks a stage complete for a section, that stage is marked
-  # complete for all students in the section; thus to check for section stage
-  # completion, we can check stage completion for any student in that section.
-  def self.teacher_marked_stage_complete_for_section(teacher_id, section_id, stage_id)
-    student_ids = Section.find(section_id).students.pluck(:id)
-    student_ids.each do |student_id|
-      unless TeacherScore.teacher_marked_stage_complete_for_student?(stage_id, student_id, teacher_id)
-        return false
-      end
-    end
-    return true
-  end
-
-  def self.teacher_marked_stage_complete_for_student?(stage_id, student_id, teacher_id)
-    stage = Stage.find(stage_id)
-    level_count = stage.script_levels.map(&:level_id).count
-    scores = TeacherScore.get_level_scores_for_stage_for_student(stage_id, student_id, teacher_id).values
-    return scores.count == level_count && scores.uniq.length == 1 && scores.first == 100
-  end
-
-  def self.get_level_scores_for_stage_for_student(stage_id, student_id, teacher_id)
+  def self.get_level_scores_for_stage_for_student(stage_id, student_id)
     stage = Stage.find(stage_id)
     script_id = stage.script.id
     level_ids = stage.script_levels.map(&:level_id)
@@ -84,8 +64,7 @@ class TeacherScore < ApplicationRecord
     user_levels.each do |user_level|
       level_scores[user_level.level_id] =
         TeacherScore.where(
-          user_level_id: user_level.id,
-          teacher_id: teacher_id
+          user_level_id: user_level.id
         ).order("created_at").last.score
     end
     level_scores
