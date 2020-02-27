2c70f1a80b9f3264960ee18634e03f9595ed9d3a
Erin Bond <erin.bond@code.org>
2020-02-27 13:40:39 -0800

    add tests for get_teacher_scores_for_script in controller

--- dashboard/test/controllers/api/v1/teacher_scores_controller_test.rb
+++ dashboard/test/controllers/api/v1/teacher_scores_controller_test.rb
@@ -7,11 +7,19 @@ class Api::V1::TeacherFeedbacksControllerTest < ActionDispatch::IntegrationTest
     @student = create :student
     @section = create :section, user: @teacher
     @stage = create :stage
+    @script = create :script
   end
 
   test 'score_stage_for_section is restricted if signed out' do
     post '/dashboardapi/v1/teacher_scores', params: {
       section_id: @section.id, stage_id: @stage.id, score: 100
+    }
+    assert_response 302
+  end
+
+  test 'get_teacher_scores_for_script is restricted if signed out' do
+    post '/dashboardapi/v1/teacher_scores/get', params: {
+      section_id: @section.id, script_id: @script.id
     }
     assert_response 302
   end
@@ -24,11 +32,27 @@ class Api::V1::TeacherFeedbacksControllerTest < ActionDispatch::IntegrationTest
     assert_response :forbidden
   end
 
+  test 'get_teacher_scores_for_script is restricted if student' do
+    sign_in @student
+    post '/dashboardapi/v1/teacher_scores/get', params: {
+      section_id: @section.id, script_id: @script.id
+    }
+    assert_response :forbidden
+  end
+
   test 'score_stage_for_section succeeds for teacher' do
     sign_in @teacher
     post '/dashboardapi/v1/teacher_scores', params: {
       section_id: @section.id, stage_id: @stage.id, score: 100
     }
     assert_response :no_content
+  end
+
+  test 'get_teacher_scores_for_script succeeds for teacher' do
+    sign_in @teacher
+    post '/dashboardapi/v1/teacher_scores/get', params: {
+      section_id: @section.id, script_id: @script.id
+    }
+    assert_response :success
   end
 end
