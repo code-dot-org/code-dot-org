b89c23a963c90ccaa06ca8bd2545eacf0ac94beb
Jessica Kulwik <jessica@code.org>
2020-03-06 00:53:08 +0000

    Removed uninted changes

--- apps/src/code-studio/components/libraries/PublishSuccessDisplay.jsx
+++ apps/src/code-studio/components/libraries/PublishSuccessDisplay.jsx
@@ -3,13 +3,6 @@ import PropTypes from 'prop-types';
 import i18n from '@cdo/locale';
 import Button from '@cdo/apps/templates/Button';
 import {Heading2} from '@cdo/apps/lib/ui/Headings';
-import {connect} from 'react-redux';
-import {
-  sectionsNameAndId
-} from '@cdo/apps/templates/teacherDashboard/teacherSectionsRedux';
-import {asyncLoadSectionData} from '@cdo/apps/templates/teacherDashboard/teacherSectionsRedux';
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
 
 const styles = {
   centerContent: {
@@ -27,38 +20,19 @@ const styles = {
   }
 };
 
-class PublishSuccessDisplay extends React.Component {
+export default class PublishSuccessDisplay extends React.Component {
   static propTypes = {
     libraryName: PropTypes.string.isRequired,
-    channelId: PropTypes.string.isRequired,
-
-    // from Redux
-    sections: PropTypes.array,
-    asyncLoadSectionData: PropTypes.func.isRequired
+    channelId: PropTypes.string.isRequired
   };
-
-  state = {
-    value: []
-  }
-
-  componentDidMount() {
-    this.props.asyncLoadSectionData();
-  }
 
   copyChannelId = () => {
     this.channelId.select();
     document.execCommand('copy');
   };
 
-
-	handleSelectChange = (value) => {
-		console.log('You\'ve selected:', value);
-		this.setState({ value });
-	};
-
   render = () => {
-    const {libraryName, channelId, sections} = this.props;
-    const {value} = this.state;
+    const {libraryName, channelId} = this.props;
     return (
       <div>
         <Heading2>
@@ -82,34 +56,8 @@ class PublishSuccessDisplay extends React.Component {
               style={styles.button}
             />
           </div>
-          {sections && sections.length > 0 &&
-            <div style={{/*height: 65*/}}>
-              <p>Hello World!!</p>
-              <div style={{/*position: 'absolute', width: 630, */}}>
-                <Select
-                  multi
-                  options={sections.map(section => {return {value: section.id, label: section.name}})}
-                  onChange={this.handleSelectChange}
-                  value={value}
-                  closeOnSelect={false}
-                  placeholder={null}
-                />
-              </div>
-            </div>
-          }
         </div>
       </div>
     );
   };
 }
-
-export const UnconnectedPublishSuccessDisplay = PublishSuccessDisplay;
-
-export default connect(
-  state => ({
-    sections: sectionsNameAndId(state.teacherSections)
-  }),
-  {
-    asyncLoadSectionData
-  }
-)(PublishSuccessDisplay);
