0bdece67fe46db91db6aced1b7ed92d6ecc68d48
Ben Brooks <ben@code.org>
2020-03-06 16:03:16 -0800

    Batch inserts to raw_contacts

--- lib/cdo/contact_rollups_v2.rb
+++ lib/cdo/contact_rollups_v2.rb
@@ -8,20 +8,30 @@ require 'json'
 class ContactRollupsV2
   def self.build_contact_rollups(log_collector)
     # Set opt_in based on information collected in Dashboard Email Preference.
-    update_email_preferences
+    log_collector.time!("truncate_raw_contacts") {truncate_raw_contacts}
+    log_collector.time!("extract_email_preferences") {extract_email_preferences}
   end
 
-  def self.update_email_preferences
-    # ben to do: add batching
-    EmailPreference.all.each do |email_preference|
-      raw_contact = {
-        email: email_preference.email,
-        sources: 'dashboard_production.email_preferences',
-        data: {opt_in: email_preference.opt_in}.to_json,
-        data_updated_at: email_preference.updated_at
-      }
+  def self.truncate_raw_contacts
+    ActiveRecord::Base.connection.execute("TRUNCATE TABLE raw_contacts")
+  end
+
+  def self.extract_email_preferences
+    EmailPreference.all.find_in_batches(batch_size: 1000) do |email_preference_batch|
+      raw_contacts = []
+      email_preference_batch.each do |email_preference|
+        raw_contact = {
+          email: email_preference.email,
+          sources: 'dashboard_production.email_preferences',
+          data: {opt_in: email_preference.opt_in}.to_json,
+          data_updated_at: email_preference.updated_at
+        }
+        raw_contacts << raw_contact
+      end
+
       # ben to do: handle if required fields are missing, or if there's a duplicate?
-      RawContact.create! raw_contact
+      # currently will fail if any required fields are missing
+      RawContact.import! raw_contacts
     end
   end
 end
