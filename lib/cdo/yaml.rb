ea6166105ffb3c9fdeed7122f3db7d716624e3bb
Elijah Hamovitz <elijahhamovitz@gmail.com>
2020-03-11 16:26:01 -0700

    implement new logic behind DCDO flag for gradual rollout

--- lib/cdo/yaml.rb
+++ lib/cdo/yaml.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 require 'cdo/erb'
+require 'cdo/pegasus/text_render'
+#require 'dynamic_config/dcdo'
 
 module Cdo
   # Extend YAML with customizations.
@@ -7,9 +9,17 @@ module Cdo
     def parse_yaml_header(content, locals={})
       match = content.match(/\A\s*^(?<yaml>---\s*\n.*?\n?)^(---\s*$\n?)/m)
       return [{}, content] unless match
-      yaml = match[:yaml]
-      yaml = ERB.new(yaml).result_with_hash(locals)
-      [YAML.safe_load(yaml), match.post_match]
+
+      # Implement new, TextRender-less parsing behind a DCDO flag so we can
+      # verify it doesn't have any unexpected side effects before switching
+      # over entirely.
+      if DCDO.get('parse_yaml_header-manually', true)
+        yaml = match[:yaml]
+        yaml = ERB.new(yaml).result_with_hash(locals)
+        [YAML.safe_load(yaml), match.post_match]
+      else
+        [TextRender.yaml(match[:yaml], locals), match.post_match]
+      end
     end
 
     # Return +nil+ if file not found.
