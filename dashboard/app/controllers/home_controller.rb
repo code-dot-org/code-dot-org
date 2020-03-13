808f9695f6d301fc54775f90b13907ab4ddeee09
Erin Bond <erin.bond@code.org>
2020-03-12 19:54:45 -0700

    remove dead gallery code related to the home page

--- dashboard/app/controllers/home_controller.rb
+++ dashboard/app/controllers/home_controller.rb
@@ -4,7 +4,6 @@ require_dependency 'queries/script_activity'
 
 class HomeController < ApplicationController
   include UsersHelper
-  before_action :authenticate_user!, only: :gallery_activities
 
   # Don't require an authenticity token on set_locale because we post to that
   # action from publicly cached page without a valid token. The worst case impact
@@ -41,8 +40,6 @@ class HomeController < ApplicationController
     render text: 'healthy!'
   end
 
-  GALLERY_PER_PAGE = 5
-
   # Signed in student, with an assigned course/script: redirect to course overview page
   # Note: the student will be redirected to the course or script in which they
   # most recently made progress, which may not be an assigned course or script.
@@ -66,14 +63,6 @@ class HomeController < ApplicationController
     authenticate_user!
     init_homepage
     render 'home/index'
-  end
-
-  def gallery_activities
-    if current_user
-      @gallery_activities =
-        current_user.gallery_activities.order(id: :desc).page(params[:page]).per(GALLERY_PER_PAGE)
-    end
-    render partial: 'home/gallery_content'
   end
 
   def certificate_link_test
