269751cd26d4ff6664fc1976ee0e3ef5b5f313fe
Ben Brooks <ben@code.org>
2020-03-17 16:52:39 -0700

    Update tests for contact_rollups_pardot_memory

--- dashboard/test/models/contact_rollups_pardot_memory_test.rb
+++ dashboard/test/models/contact_rollups_pardot_memory_test.rb
@@ -1,13 +1,76 @@
 require 'test_helper'
+require 'cdo/contact_rollups/v2/pardot'
 
 class ContactRollupsPardotMemoryTest < ActiveSupport::TestCase
   test 'update_from_new_contacts creates one row per email' do
-    # requires a) stub for get request to Pardot, and
-    # b) contents of that response to be XML similar to what Pardot currently shares?
+    response = Nokogiri::XML::Builder.new do |xml|
+      xml.rsp {
+        xml.result {
+          xml.prospect {
+            xml.id_ "1"
+            xml.email "alex@rollups.com"
+          }
+          xml.total_results "1"
+        }
+      }
+    end
+
+    Pardot.stubs(:post_with_auth_retry).returns(response.doc)
+    Pardot.stubs(:raise_if_response_error).returns(nil)
+
+    ContactRollupsPardotMemory.update_from_new_contacts
+
+    assert 1, ContactRollupsPardotMemory.find_by(email: 'alex@rollups.com').pardot_id
   end
 
-  test 'import_from_raw_table works when more than 200 records' do
-    # use part b) above,
-    # but need to be able to stub 200+ XML responses with distinct emails?
+  test 'update_from_new_contacts with two emails returns record with higher ID' do
+    create :contact_rollups_pardot_memory, email: 'alex@rollups.com', pardot_id: 1
+
+    response = Nokogiri::XML::Builder.new do |xml|
+      xml.rsp {
+        xml.result {
+          xml.prospect {
+            xml.id_ "2"
+            xml.email "alex@rollups.com"
+          }
+          xml.total_results "1"
+        }
+      }
+    end
+
+    Pardot.stubs(:post_with_auth_retry).returns(response.doc)
+    Pardot.stubs(:raise_if_response_error).returns(nil)
+
+    assert 1, ContactRollupsPardotMemory.find_by(email: 'alex@rollups.com').pardot_id
+    ContactRollupsPardotMemory.update_from_new_contacts
+    assert 2, ContactRollupsPardotMemory.find_by(email: 'alex@rollups.com').pardot_id
+  end
+
+  test 'can handle having multiple mappings in response' do
+    response = Nokogiri::XML::Builder.new do |xml|
+      xml.rsp {
+        xml.result {
+          xml.prospect {
+            xml.id_ "1"
+            xml.email "alex@rollups.com"
+          }
+          xml.prospect {
+            xml.id_ "2"
+            xml.email "becky@rollups.com"
+          }
+          xml.total_results "2"
+        }
+      }
+    end
+
+    Pardot.stubs(:post_with_auth_retry).returns(response.doc)
+    Pardot.stubs(:raise_if_response_error).returns(nil)
+
+    ContactRollupsPardotMemory.update_from_new_contacts
+
+    assert 1, ContactRollupsPardotMemory.find_by(email: 'alex@rollups.com').pardot_id
+    assert 2, ContactRollupsPardotMemory.find_by(email: 'becky@rollups.com').pardot_id
+    assert 2, ContactRollupsPardotMemory.count
   end
 end
+
