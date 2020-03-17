56c186c571130c70a6e54f8bae388d1b4c5117d7
Ha Nguyen <ha@code.org>
2020-03-16 19:02:00 -0700

    Merge ContactRollupsV2 class from 2 files into 1

--- lib/cdo/contact_rollups/v2/contact_rollups.rb
+++ lib/cdo/contact_rollups/v2/contact_rollups.rb
@@ -1,6 +1,10 @@
 class ContactRollupsV2
-  def self.build_contact_rollups
-    ContactRollupsProcessed.delete_all
-    ContactRollupsProcessed.import_from_raw_table
+  def self.build_contact_rollups(log_collector)
+    # Set opt_in based on information collected in Dashboard Email Preference.
+    log_collector.time!('ContactRollupsRaw.truncate_table') {ContactRollupsRaw.truncate_table}
+    log_collector.time!('ContactRollupsRaw.extract_email_preferences') {ContactRollupsRaw.extract_email_preferences}
+
+    log_collector.time!('ContactRollupsProcessed.delete_all') {ContactRollupsProcessed.delete_all}
+    log_collector.time!('ContactRollupsProcessed.import_from_raw_table') {ContactRollupsProcessed.import_from_raw_table}
   end
 end
