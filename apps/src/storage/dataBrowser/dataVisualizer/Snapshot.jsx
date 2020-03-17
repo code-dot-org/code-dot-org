aaa7db3bada89f827fbfdb03c6e9cc6a76f166b7
Anjali <anjali@code.org>
2020-03-17 13:43:52 -0700

    copy and save

--- apps/src/storage/dataBrowser/dataVisualizer/Snapshot.jsx
+++ apps/src/storage/dataBrowser/dataVisualizer/Snapshot.jsx
@@ -1,9 +1,12 @@
+/* global ClipboardItem */
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import moment from 'moment';
 import msg from '@cdo/locale';
+import {html2canvas} from '@cdo/apps/util/htmlToCanvasWrapper';
 import BaseDialog from '@cdo/apps/templates/BaseDialog';
+import PendingButton from '@cdo/apps/templates/PendingButton';
 import * as dataStyles from '../dataStyles';
 
 class Snapshot extends React.Component {
@@ -16,11 +19,54 @@ class Snapshot extends React.Component {
   };
 
   state = {
-    isSnapshotOpen: false
+    isSnapshotOpen: false,
+    isCopyPending: false,
+    isSavePending: false
   };
 
   handleOpen = () => this.setState({isSnapshotOpen: true});
   handleClose = () => this.setState({isSnapshotOpen: false});
+
+  getPngBlob = () => {
+    const element = document.getElementById('snapshot');
+    const options = {
+      background: '#fff'
+    };
+    return html2canvas(element, options).then(canvas => {
+      return new Promise(function(resolve, reject) {
+        canvas.toBlob(function(blob) {
+          resolve(blob);
+        });
+      });
+    });
+  };
+
+  copy = () => {
+    this.setState({isCopyPending: true});
+    this.getPngBlob().then(pngBlob => {
+      try {
+        navigator.clipboard.write([
+          new ClipboardItem({
+            'image/png': pngBlob
+          })
+        ]);
+        this.setState({isCopyPending: false});
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  };
+
+  save = () => {
+    this.setState({isSavePending: true});
+    this.getPngBlob().then(pngBlob => {
+      const download = document.createElement('a');
+      download.href = URL.createObjectURL(pngBlob);
+      download.download = 'image.png';
+      download.click();
+      this.setState({isSavePending: false});
+    });
+  };
 
   render() {
     return (
@@ -38,15 +84,32 @@ class Snapshot extends React.Component {
           fullWidth
           fullHeight
         >
-          <h1>{this.props.chartTitle}</h1>
-          <p>
-            {msg.dataVisualizerSnapshotDescription({
-              date: moment().format('YYYY/MM/DD'),
-              table: this.props.tableName,
-              project: this.props.projectName
-            })}
-          </p>
-          <p>{this.props.selectedOptions}</p>
+          <div id="snapshot">
+            <h1>{this.props.chartTitle}</h1>
+            <img src={require('./placeholder.png')} />
+            <p>
+              {msg.dataVisualizerSnapshotDescription({
+                date: moment().format('YYYY/MM/DD'),
+                table: this.props.tableName,
+                project: this.props.projectName
+              })}
+            </p>
+            <p>{this.props.selectedOptions}</p>
+          </div>
+          <PendingButton
+            isPending={this.state.isCopyPending}
+            onClick={this.copy}
+            pendingText="Please Wait"
+            style={dataStyles.blueButton}
+            text="Copy"
+          />
+          <PendingButton
+            isPending={this.state.isSavePending}
+            onClick={this.save}
+            pendingText="Please Wait"
+            style={dataStyles.blueButton}
+            text="Save"
+          />
         </BaseDialog>
       </div>
     );
