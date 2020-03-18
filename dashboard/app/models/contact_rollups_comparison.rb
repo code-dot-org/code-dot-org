dfb86bd26c00b17f3f575bc189218f10a2a48bc0
Ha Nguyen <ha@code.org>
2020-03-18 11:29:58 -0700

    Fix order of columns to insert. Update comments.

--- dashboard/app/models/contact_rollups_comparison.rb
+++ dashboard/app/models/contact_rollups_comparison.rb
@@ -16,29 +16,32 @@
 #
 
 class ContactRollupsComparison < ApplicationRecord
-  # Compiles old and new processed data, one row per email.
+  # Compiles old and new processed data then saves the results, one row per email.
   def self.compile_processed_data
-    # Finds all records in the old data. They may not be in the new data.
+    # Since Mysql 5.7 doesn't support FULL OUTER JOIN, we will simulate a FULL OUTER JOIN
+    # between contact_rollups_final (the old data) and contact_rollups_processed (the new data).
+
+    # Finds all records in the old data
     find_existing_records_query = <<-SQL.squish
-      SELECT old_data.email, old_data.data, old_data.updated_at, new_data.data, new_data.updated_at, now()
-      FROM contact_rollups_final AS old_data
-      LEFT OUTER JOIN contact_rollups_processed AS new_data
-      ON old_data.email = new_data.email
+      SELECT old.email, old.data, old.updated_at, new.data, new.updated_at, now()
+      FROM contact_rollups_final AS old
+      LEFT OUTER JOIN contact_rollups_processed AS new
+      ON old.email = new.email
     SQL
 
     # Finds all records in the new data but not in the old data
     find_new_records_query = <<-SQL.squish
-      SELECT new_data.email, old_data.data, old_data.updated_at, new_data.data, new_data.updated_at, now()
-      FROM contact_rollups_final AS old_data
-      RIGHT OUTER JOIN contact_rollups_processed AS new_data
-      ON old_data.email = new_data.email
-      WHERE old_data.email IS NULL
+      SELECT new.email, old.data, old.updated_at, new.data, new.updated_at, now()
+      FROM contact_rollups_final AS old
+      RIGHT OUTER JOIN contact_rollups_processed AS new
+      ON old.email = new.email
+      WHERE old.email IS NULL
     SQL
 
     # Saves results of the two queries above
     insert_records_query = <<-SQL.squish
       INSERT INTO #{ContactRollupsComparison.table_name}
-        (email, new_data, new_data_updated_at, old_data, old_data_updated_at, created_at)
+        (email, old_data, old_data_updated_at, new_data, new_data_updated_at, created_at)
       #{find_existing_records_query}
       UNION ALL
       #{find_new_records_query}
