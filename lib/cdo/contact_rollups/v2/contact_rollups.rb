20ba24343a31c5ba110d2fd36339f39fa115f125
Ben Brooks <ben@code.org>
2020-03-18 09:41:36 -0700

    Add pardot memory step to full contact rollups process

--- lib/cdo/contact_rollups/v2/contact_rollups.rb
+++ lib/cdo/contact_rollups/v2/contact_rollups.rb
@@ -9,5 +9,9 @@ class ContactRollupsV2
 
     log_collector.time!('ContactRollupsComparison.delete_all') {ContactRollupsComparison.delete_all}
     log_collector.time!('ContactRollupsComparison.compare_processed_data') {ContactRollupsComparison.compile_processed_data}
+
+    log_collector.time!('ContactRollupsPardotMemory.update_from_new_contacts') {ContactRollupsPardotMemory.update_from_new_contacts}
+
+    # To do: sync upstream step
   end
 end
