240615e3ccc59a9c871379cb52c6325c92e1e812
mhw32 <me@mikewuis.me>
2020-03-22 20:40:23 -0700

    small cosmetic changes-- waiting for student data before continuing

--- flask/rubric/src/trainer/models.py
+++ flask/rubric/src/trainer/models.py
@@ -8,15 +8,7 @@ import torch.nn.utils.rnn as rnn_utils
 
 
 class FeedbackNN(nn.Module):
-    """
-    Neural network responsible for ingesting a tokenized student 
-    program, and spitting out a categorical prediction.
 
-    We give you the following information:
-        vocab_size: number of unique tokens 
-        num_labels: number of output feedback labels
-    """
-    
     def __init__(
             self, 
             vocab_size, 
@@ -48,37 +40,12 @@ class FeedbackNN(nn.Module):
         self.num_layers = num_layers
 
     def forward(self, token_seq, token_length):
-        """
-        Forward pass for your feedback prediction network.
-
-        @param token_seq: batch_size x max_seq_length
-            Example: torch.Tensor([[0,6,2,3],[0,2,5,3], ...])
-            These define your PADDED programs after tokenization.
-        
-        @param token_length: batch_size
-            Example: torch.Tensor([4,4, ...])
-            These define your unpadded program lengths.
-
-        This function should return the following:
-        @param label_out: batch_size x num_labels
-            Each index in this tensor represents the likelihood of predicting
-            1. Unlike IRT, this is a multilabel prediction program so we need
-            to have a likelihood for every feedback. NOTE: this is NOT categorical
-            since we can have multiple feedback at once. 
-
-            This will be given to F.binary_cross_entropy(...), just like IRT!
-        """
-        rep = self.encoder(seq, length)
+        rep = self.encoder(token_seq, token_length)
         return self.decoder(rep)
 
 
 class BlockyEncoder(nn.Module):
-    r"""
-    @param hidden_dim: integer [default: 256]
-                       size of hidden layer
-    @param num_layers: integer [default: 2]
-                       number of hidden layers in GRU
-    """
+
     def __init__(
             self, 
             vocab_size, 
@@ -134,12 +101,7 @@ class BlockyEncoder(nn.Module):
 
 
 class LabelPredictor(nn.Module):
-    r"""
-    @param num_labels: integer
-                      number of label dimensions
-    @param hidden_dim: integer [default: 256]
-                       number of hidden dimensions
-    """
+
     def __init__(self, input_dim, num_labels, hidden_dim=256):
         super(LabelPredictor, self).__init__()
 
