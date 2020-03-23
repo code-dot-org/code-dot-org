d6fac27df540b0b22e66036c0c0083858bbb8b87
mhw32 <me@mikewuis.me>
2020-03-23 15:28:17 -0700

    Add more notes on learnings

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,10 +1,17 @@
 from app import app
+from flask import request, Response
+import json
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "rubric sampling for code.org"
 
-@app.route('/predict')
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    return "todo"
+    response = {
+        'hint': 'placeholder',
+    }
+    return Response(response=response)
