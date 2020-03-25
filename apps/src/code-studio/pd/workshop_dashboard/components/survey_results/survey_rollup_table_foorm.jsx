a0baa6da2daf8527dd55cda7b244b4f7aedc454f
molly-moen <molly@code.org>
2020-03-25 16:28:22 -0700

    clean up ux

--- apps/src/code-studio/pd/workshop_dashboard/components/survey_results/survey_rollup_table_foorm.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/survey_results/survey_rollup_table_foorm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Table} from 'react-bootstrap';
+import * as Table from 'reactabular-table';
 
 export default class SurveyRollupTableFoorm extends React.Component {
   static propTypes = {
@@ -9,9 +9,34 @@ export default class SurveyRollupTableFoorm extends React.Component {
     courseName: PropTypes.string.isRequired
   };
 
-  getParsedData() {
+  renderTableRow(title, answers, denominator, isHeader) {
+    return (
+      <tr style={isHeader ? {borderTop: 'solid'} : {}}>
+        <td style={isHeader ? {} : {paddingLeft: '60px'}}>{title}</td>
+        {answers.map((answer, i) => {
+          return (
+            <td key={i}>
+              {answer} / {denominator}
+            </td>
+          );
+        })}
+      </tr>
+    );
+  }
+
+  getTableRows() {
     const {workshopRollups} = this.props;
-    let parsedData = [];
+    let rows = [
+      {
+        question: {
+          text: 'Total Responses',
+          type: 'overall'
+        },
+        thisWorkshop: workshopRollups.single_workshop.response_count,
+        overall: workshopRollups.overall.response_count,
+        id: 'total_responses'
+      }
+    ];
     const overall = workshopRollups.overall;
     const thisWorkshop = workshopRollups.single_workshop;
     const questions = workshopRollups.questions;
@@ -20,69 +45,101 @@ export default class SurveyRollupTableFoorm extends React.Component {
         const questionData = questions[questionId];
         let overallQuestionAverages = overall.averages[questionId];
         let thisWorkshopAverages = thisWorkshop.averages[questionId];
-        let parsedQuestionAverages = {};
+        let denominator = questionData.column_count;
         // only display matrix questions for now
         if (questionData.type === 'matrix') {
-          parsedQuestionAverages[questionData.header] = {
-            thisWorkshop: thisWorkshopAverages && thisWorkshopAverages.average,
-            overall: overallQuestionAverages.average
-          };
+          // add header data
+          rows.push({
+            question: {
+              text: questionData.header,
+              type: 'overall'
+            },
+            thisWorkshop: thisWorkshopAverages
+              ? `${thisWorkshopAverages.average} / ${denominator}`
+              : '',
+            overall: `${overallQuestionAverages.average} / ${denominator}`,
+            id: `${questionId}-header`,
+            isHeader: true
+          });
+          // add top level question
+          rows.push({
+            question: {
+              text: questionData.title,
+              type: 'title'
+            },
+            id: questionId
+          });
+          // add individual rows
           let thisWorkshopRows =
             thisWorkshopAverages && thisWorkshopAverages.rows;
           Object.keys(overallQuestionAverages.rows).forEach(rowId => {
-            parsedQuestionAverages[questionData.rows[rowId]] = {
-              thisWorkshop: thisWorkshopRows && thisWorkshopRows[rowId],
-              overall: overallQuestionAverages.rows[rowId]
-            };
-          });
-          parsedData.push({
-            orderedData: parsedQuestionAverages,
-            denominator: questionData.column_count,
-            question: questionData.title,
-            header: questionData.header
+            rows.push({
+              question: {
+                text: questionData.rows[rowId],
+                type: 'question'
+              },
+              thisWorkshop: thisWorkshopRows
+                ? `${thisWorkshopRows[rowId]} / ${denominator}`
+                : '',
+              overall: `${
+                overallQuestionAverages.rows[rowId]
+              } / ${denominator}`,
+              id: rowId
+            });
           });
         }
       });
     }
-    return parsedData;
+    return rows;
+  }
+
+  getTableColumns(courseName) {
+    return [
+      {
+        property: 'question',
+        cell: {
+          formatters: [question => question.text],
+          transforms: [
+            question => ({
+              style:
+                question.type === 'overall'
+                  ? {}
+                  : question.type === 'title'
+                  ? {paddingLeft: '30px'}
+                  : {paddingLeft: '60px'}
+            })
+          ]
+        }
+      },
+      {
+        property: 'thisWorkshop',
+        header: {
+          label: 'Average for this workshop'
+        }
+      },
+      {
+        property: 'overall',
+        header: {
+          label: `Average across all ${courseName} workshops`
+        }
+      }
+    ];
   }
 
   render() {
-    const parsedData = this.getParsedData();
+    const rows = this.getTableRows();
+    const columns = this.getTableColumns(this.props.courseName);
     return (
-      <Table>
-        <thead>
-          <tr>
-            <th />
-            <th>Average for this workshop</th>
-            <th>Average across all {this.props.courseName} workshops</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Total Responses</td>
-            <td>{this.props.workshopRollups.single_workshop.response_count}</td>
-            <td>{this.props.workshopRollups.overall.response_count}</td>
-          </tr>
-          {parsedData.map(matrixData => {
-            return Object.keys(matrixData.orderedData).map(question => {
-              const answerData = matrixData.orderedData[question];
-              return (
-                <tr>
-                  <td>{question}</td>
-                  {Object.keys(answerData).map(answerId => {
-                    return (
-                      <td key={answerId}>
-                        {answerData[answerId]} / {matrixData.denominator}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            });
-          })}
-        </tbody>
-      </Table>
+      <Table.Provider className="table table-bordered" columns={columns}>
+        <Table.Header />
+        <Table.Body
+          rows={rows}
+          rowKey="id"
+          onRow={row => {
+            return {style: row.isHeader ? {borderTop: 'solid'} : {}};
+          }}
+        />
+      </Table.Provider>
     );
   }
 }
