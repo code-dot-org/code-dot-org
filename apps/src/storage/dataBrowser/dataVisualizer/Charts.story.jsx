c957945bbfd3ec8139f9fc4749af7e3d747bfce5
Brad Buchanan <brad@code.org>
2020-03-25 12:35:31 -0700

    New crosstab formatting

--- apps/src/storage/dataBrowser/dataVisualizer/Charts.story.jsx
+++ apps/src/storage/dataBrowser/dataVisualizer/Charts.story.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import CrossTabChart from './CrossTabChart';
+import _ from 'lodash';
 
 export default storybook =>
-  storybook
-    .storiesOf('Charts', module)
-    .add('CrossTab', () => (
+  storybook.storiesOf('Charts', module).add('CrossTab', () => (
+    <div style={{width: '100%', padding: 30}}>
       <CrossTabChart
-        records={[
-          {Date: '3/6 Fri', 'Wind Direction': 'E'},
-          {Date: '3/7 Sat', 'Wind Direction': 'NE'},
-          {Date: '3/8 Sun', 'Wind Direction': 'N'},
-          {Date: '3/9 Mon', 'Wind Direction': 'NW'},
-          {Date: '3/10 Tue', 'Wind Direction': 'W'},
-          {Date: '3/11 Wed', 'Wind Direction': 'SW'},
-          {Date: '3/11 Wed', 'Wind Direction': 'S'},
-          {Date: '3/11 Wed', 'Wind Direction': 'SE'}
-        ]}
+        records={makeFakeRecords()}
         numericColumns={[]}
         selectedColumn1="Date"
         selectedColumn2="Wind Direction"
+        chartTitle="Study of the wind"
       />
-    ));
+    </div>
+  ));
+
+const DATES = [
+  '3/4 Wed',
+  '3/5 Thu',
+  '3/6 Fri',
+  '3/7 Sat',
+  '3/8 Sun',
+  '3/9 Mon'
+];
+
+const DIRECTIONS = ['E', 'NE', 'N', 'Northwest', 'W', 'SW', 'S', 'SE'];
+
+function makeFakeRecords() {
+  const result = [];
+  for (let i = 0; i < 1000; i++) {
+    result.push({
+      Date: _.sample(DATES),
+      'Wind Direction': _.sample(DIRECTIONS)
+    });
+  }
+  return result;
+}
