e45826e1257959d0587ea6bb1340fe725473ce10
Ben Brooks <ben@code.org>
2020-03-25 12:14:28 -0700

    Add unison to default cdo-apps recipe

--- cookbooks/cdo-unison/README.md
+++
@@ -1,3 +0,0 @@
-# cdo-unison
-
-Installs Unison.
--- cookbooks/cdo-unison/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'cdo-unison'
-maintainer       'Code.org'
-maintainer_email 'will@code.org'
-license          'All rights reserved'
-description      'Installs/Configures Unison'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
-
-depends          'unison'
