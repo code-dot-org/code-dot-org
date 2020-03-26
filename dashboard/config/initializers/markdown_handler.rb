fa0d14357bfb72e564997c228a561ede7044ba06
Elijah Hamovitz <elijahhamovitz@gmail.com>
2020-03-26 12:26:43 -0700

    add side-by-side markdown renderer for evaluation

--- dashboard/config/initializers/markdown_handler.rb
+++ dashboard/config/initializers/markdown_handler.rb
@@ -1,6 +1,8 @@
 # Add Markdown .md support to the ActionView template system.
 
-require 'cdo/markdown/handler'
+require 'cdo/honeybadger'
+require 'cdo/markdown/renderer'
+require 'redcarpet'
 
 MARKDOWN_OPTIONS = {
   autolink: true,
@@ -8,4 +10,34 @@ MARKDOWN_OPTIONS = {
   space_after_headers: true
 }.freeze
 
-Cdo::Markdown::Handler.register(Redcarpet::Render::HTML, MARKDOWN_OPTIONS)
+# Temporarily use this instead of the standard Cdo::Markdown::Handler so we can
+# render using both the new and old renderers side-by-side and compare the
+# results. We will still return the old result, but will notify Honeybadger if
+# they are different so we can identify potential changes before switching
+# over.
+class InterimMarkdownHandler
+  def initialize
+    @old_parser = Redcarpet::Render::HTML.new(MARKDOWN_OPTIONS)
+    @new_parser = Cdo::Markdown::Renderer.new
+  end
+
+  def call(template)
+    old_result = @old_parser.render(template.source)
+    new_result = @new_parser.render(template.source)
+
+    if old_result != new_result
+      Honeybadger.notify(
+        error_class: 'Rendering differences between new and old Dashboard markdown',
+        context: {
+          old_result: old_result,
+          new_result: new_result,
+          template: template
+        }
+      )
+    end
+
+    "#{old_result.inspect}.html_safe"
+  end
+end
+
+ActionView::Template.register_template_handler :md, InterimMarkdownHandler.new
