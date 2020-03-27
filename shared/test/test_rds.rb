b2725f695310c0662ad4422c72f5a14fbbd6d6bf
Suresh Chanmugam <suresh@code.org>
2020-03-27 12:26:19 -0700

    Update `clone_cluster` method to create & use copies of the source cluster's Parameter Groups and update `delete_cluster` to delete its Parameter Groups (if it appears to own them).

--- shared/test/test_rds.rb
+++ shared/test/test_rds.rb
@@ -78,6 +78,16 @@ class TestRDS < Minitest::Test
       ]
     }
 
+    @copy_cluster_parameter_group_response = {
+      "db_cluster_parameter_group":
+        {
+          "db_cluster_parameter_group_name": "#{@clone_cluster_id}-auroraclusterdbparameters",
+          "db_parameter_group_family": "aurora-mysql5.7",
+          "description": "#{@clone_cluster_id}-auroraclusterdbparameters",
+          "db_cluster_parameter_group_arn": "arn:aws:rds:us-east-1:0987654321:cluster-pg:#{@clone_cluster_id}-auroraclusterdbparameters"
+        }
+    }
+
     @clone_cluster_response = {
       "db_cluster": @source_cluster[:db_clusters][0].deep_merge(
         {
@@ -178,6 +188,15 @@ class TestRDS < Minitest::Test
       ]
     }
 
+    @copy_instance_parameter_group_response = {
+      "db_parameter_group": {
+        "db_parameter_group_name": "#{@clone_cluster_id}-aurorawriterdbparameters",
+        "db_parameter_group_family": "aurora-mysql5.7",
+        "description": "#{@clone_cluster_id}-aurorawriterdbparameters",
+        "db_parameter_group_arn": "arn:aws:rds:us-east-1:0987654321:pg:#{@clone_cluster_id}-aurorawriterdbparameters"
+      }
+    }
+
     @create_instance_response = {
       "db_instance": @source_instance[:db_instances][0].deep_merge(
         {
@@ -215,6 +234,22 @@ class TestRDS < Minitest::Test
       }
     )
 
+    @describe_instance_to_delete_response = {
+      "db_instance": @source_instance[:db_instances][0].deep_merge(
+        {
+          "db_cluster_identifier": "#{@cluster_to_delete_id}-0",
+          "dbi_resource_id": "db-Z9Y8X7W6V5",
+          "db_instance_arn": "arn:aws:rds:us-east-1:0987654321:db:#{@cluster_to_delete_id}-0",
+          "db_parameter_groups": [
+            {
+              "db_parameter_group_name": "#{@cluster_to_delete_id}-writerdbparameters-a1b2c3de",
+              "parameter_apply_status": "in-sync"
+            }
+          ]
+        }
+      )
+    }
+
     @delete_instance_response = {
       "db_instance": @source_instance[:db_instances][0].deep_merge(
         {
@@ -236,16 +271,21 @@ class TestRDS < Minitest::Test
         "clone_group_id": "1z2y3x4w5v",
       )
     }
+
+    @delete_db_parameter_group_response = nil
+    @delete_cluster_parameter_group_response = nil
   end
 
   def test_clone_cluster
     Aws.config[:rds] = {
       stub_responses: {
         describe_db_clusters: @source_cluster,
+        copy_db_cluster_parameter_group: @copy_cluster_parameter_group_response,
         restore_db_cluster_to_point_in_time: @clone_cluster_response,
         # The 1st stub returns @source_instance when `clone_cluster` is getting information about the instance to clone.
         # The 2nd stub returns @create_instance_complete when the waiter checks to see if it has been provisioned.
         describe_db_instances: [@source_instance, @create_instance_complete],
+        copy_db_parameter_group: @copy_instance_parameter_group_response,
         create_db_instance: @create_instance_response
       }
     }
@@ -261,10 +301,16 @@ class TestRDS < Minitest::Test
   def test_delete_cluster
     Aws.config[:rds] = {
       stub_responses: {
+        # The 1st stub of describe_db_clusters returns @source_instance when `clone_cluster` is getting information about the instance to clone.
+        # The 2nd stub of describe_db_clusters raises Cluster Not Found error when the waiter checks to see if it has been deleted.
         describe_db_clusters: [@cluster_to_delete, 'DBClusterNotFoundFault'],
+        # The 1st stub of describe_db_instances returns information about the instance about to be deleted.
+        # The 2nd stub of describe_db_instances raises Instance Not Found error when the watier checks to see if it has been deleted.
+        describe_db_instances: [@describe_instance_to_delete_response, 'DBInstanceNotFoundFault'],
         delete_db_instance: @delete_instance_response,
-        describe_db_instances: 'DBInstanceNotFoundFault',
-        delete_db_cluster: @delete_cluster_response
+        delete_db_parameter_group: @delete_db_parameter_group_response,
+        delete_db_cluster: @delete_cluster_response,
+        delete_db_cluster_parameter_group: @delete_cluster_parameter_group_response
       }
     }
 
