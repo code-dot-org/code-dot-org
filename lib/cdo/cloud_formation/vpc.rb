620a8577bbf802d7aba53e2c272dc7a5d51f57b9
Will Jordan <will@code.org>
2020-03-27 17:20:43 -0700

    Remove us-east-1e subnet from frontend auto scaling group availability zone doesn't support m5 instance types. [ci skip]

--- lib/cdo/cloud_formation/vpc.rb
+++ lib/cdo/cloud_formation/vpc.rb
@@ -4,15 +4,15 @@ module Cdo::CloudFormation
     AVAILABILITY_ZONES = ('b'..'e').map {|i| "us-east-1#{i}"}
     SSH_IP = '0.0.0.0/0'.freeze
 
-    def azs
-      AVAILABILITY_ZONES.map {|zone| zone[-1].upcase}
+    def azs(zones=AVAILABILITY_ZONES)
+      zones.map {|zone| zone[-1].upcase}
     end
 
-    def subnets
+    def subnets(azs=self.azs)
       azs.map {|az| {'Fn::ImportValue': "VPC-Subnet#{az}"}}
     end
 
-    def public_subnets
+    def public_subnets(azs=self.azs)
       azs.map {|az| {'Fn::ImportValue': "VPC-PublicSubnet#{az}"}}
     end
   end
