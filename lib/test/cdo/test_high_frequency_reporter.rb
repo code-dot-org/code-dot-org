3ae449e5f9fb89fb7e90de346c99a4b6a013a112
Ben Brooks <ben@code.org>
2020-04-01 09:27:11 -0700

    Update high frequency reporter tests

--- lib/test/cdo/test_high_frequency_reporter.rb
+++ lib/test/cdo/test_high_frequency_reporter.rb
@@ -13,7 +13,7 @@ class HighFrequencyReporterTest < MiniTest::Test
     fake_slack = MiniTest::Mock.new # expects nothing
 
     Tempfile.create do |f|
-      reporter = HighFrequencyReporter.new(fake_slack, f.path)
+      reporter = HighFrequencyReporter.new(fake_slack, 'fake_channel', f.path)
       reporter.load
       reporter.record "Something happened"
       # Mock will fail here if it is called
@@ -22,8 +22,8 @@ class HighFrequencyReporterTest < MiniTest::Test
 
       # The second time the same error occurs, it should be reported
       # to Slack.  We add a mock expectation to check that.
-      fake_slack.expect :message, nil, [String]
-      reporter = HighFrequencyReporter.new(fake_slack, f.path)
+      fake_slack.expect :message, nil, [String, Hash]
+      reporter = HighFrequencyReporter.new(fake_slack, 'fake_channel', f.path)
       reporter.load
       reporter.record "Something happened"
       reporter.report!
@@ -38,18 +38,18 @@ class HighFrequencyReporterTest < MiniTest::Test
 
     Timecop.freeze(DateTime.parse('1st Jan 2020 00:05:00+00:00')) do
       Tempfile.create do |f|
-        reporter = HighFrequencyReporter.new(fake_slack, f.path)
+        reporter = HighFrequencyReporter.new(fake_slack, 'fake-channel', f.path)
         reporter.load
         reporter.record "Something happened"
         # Mock will fail here if it is called
         reporter.report! 7
         reporter.save
 
-        reporter = HighFrequencyReporter.new(fake_slack, f.path)
+        reporter = HighFrequencyReporter.new(fake_slack, 'fake-channel', f.path)
         reporter.load # Load "old events" to "new events"
         reporter.record "Something happened"
 
-        fake_slack.expect :message, nil, [String]
+        fake_slack.expect :message, nil, [String, Hash]
         reporter.report! 5
         fake_slack.verify # message was sent
       end
@@ -61,12 +61,12 @@ class HighFrequencyReporterTest < MiniTest::Test
     fake_slack = MiniTest::Mock.new # expects nothing
 
     Tempfile.create do |f|
-      reporter = HighFrequencyReporter.new(fake_slack, f.path)
+      reporter = HighFrequencyReporter.new(fake_slack, 'fake-channel', f.path)
       reporter.record "Something happened"
       reporter.record "Something else happened"
       reporter.save
 
-      reporter = HighFrequencyReporter.new(fake_slack, f.path)
+      reporter = HighFrequencyReporter.new(fake_slack, 'fake-channel', f.path)
       reporter.load
       reporter.record "Something happened"
 
