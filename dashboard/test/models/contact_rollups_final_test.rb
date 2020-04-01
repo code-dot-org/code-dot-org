3156707c168d683acbfb887aa1e1605ccebbf211
Ben Brooks <ben@code.org>
2020-04-01 15:51:08 -0700

    Update unit test

--- dashboard/test/models/contact_rollups_final_test.rb
+++ dashboard/test/models/contact_rollups_final_test.rb
@@ -1,13 +1,10 @@
 require 'test_helper'
 
 class ContactRollupsFinalTest < ActiveSupport::TestCase
-  test 'overwrite_from_processed_table! overwrites contact_rollups_final' do
+  test 'insert_from_processed_table properly inserts into contact_rollups_final' do
     clean_tables
-
     processed = create_list :contact_rollups_processed, 5
-    create_list :contact_rollups_final, 3
 
-    ContactRollupsFinal.delete_all # Stand in for ContactRollupsFinal.truncate, which tests don't allow b/c modifies DDL
     ContactRollupsFinal.insert_from_processed_table
 
     assert_equal 5, ContactRollupsFinal.count
