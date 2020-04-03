29d2f8fb7597d38f88a9b8225b2602139467ef79
Ben Brooks <ben@code.org>
2020-04-02 17:00:19 -0700

    Centralize pre-workshop survey URL logic in enrollment model

--- dashboard/app/models/pd/enrollment.rb
+++ dashboard/app/models/pd/enrollment.rb
@@ -192,6 +192,16 @@ class Pd::Enrollment < ActiveRecord::Base
     user || User.find_by_email_or_hashed_email(email)
   end
 
+  def pre_survey_url
+    if workshop.local_summer?
+      url_for(action: 'new_general', controller: 'pd/workshop_daily_survey', day: 0, enrollmentCode: code)
+    elsif workshop.pre_survey?
+      pd_new_pre_workshop_survey_url(enrollment_code: code)
+    elsif workshop.subject == Pd::Workshop::SUBJECT_CSF_201
+      'https://studio.code.org/pd/workshop_survey/csf/pre201'
+    end
+  end
+
   def exit_survey_url
     if [Pd::Workshop::COURSE_ADMIN, Pd::Workshop::COURSE_COUNSELOR].include? workshop.course
       CDO.code_org_url "/pd-workshop-survey/counselor-admin/#{code}", CDO.default_scheme
