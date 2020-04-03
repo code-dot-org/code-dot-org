28b52f9dd4b665dd9026b2ad51fc88e836f7f086
Ha Nguyen <ha@code.org>
2020-04-03 13:44:59 -0700

    Remove Pardot syncing logic from ContactRollupsComparison

--- dashboard/app/models/contact_rollups_comparison.rb
+++ dashboard/app/models/contact_rollups_comparison.rb
@@ -15,6 +15,8 @@
 #  index_contact_rollups_comparisons_on_email  (email) UNIQUE
 #
 
+require 'cdo/contact_rollups/v2/pardot'
+
 class ContactRollupsComparison < ApplicationRecord
   # Compiles old and new processed data then saves the results, one row per email.
   def self.compile_processed_data
@@ -49,27 +51,4 @@ class ContactRollupsComparison < ApplicationRecord
 
     ActiveRecord::Base.connection.exec_query(insert_records_query)
   end
-
-  def self.sync_new_contacts_to_pardot
-    # TODO: is it better to use association?
-    # Get all new contacts from contact_rollups_comparisons
-    # Join with contact_rollups_pardot_memory to find pardot_id
-    new_contacts_query = <<-SQL.squish
-      SELECT a.email, a.new_data AS data, b.pardot_id
-      FROM contact_rollups_comparisons AS a
-      LEFT OUTER JOIN contact_rollups_pardot_memory AS b
-      ON a.email = b.email
-      WHERE a.old_data_updated_at IS NULL
-    SQL
-
-    ActiveRecord::Base.connection.exec_query(new_contacts_query).each do |record|
-      # TODO: Use new instance of PardotV2 client. PardotV2 acts as an output stream
-      PardotV2.send_in_batch record['email'], record['data'], record['pardot_id']
-    end
-    PardotV2.flush
-  end
-
-  # TODO: sync contacts that change pardot mappings
-  # TODO: sync contacts with updated content
-  # TODO: sync deleted contacts
 end
