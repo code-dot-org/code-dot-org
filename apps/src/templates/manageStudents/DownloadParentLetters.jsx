8bd32f238f65dec61e4071ed31aad262fb6854f5
Clare Constantine <clare@code.org>
2020-04-06 12:29:46 -0700

    remove tooltip until we have option to download multiple letters

--- apps/src/templates/manageStudents/DownloadParentLetters.jsx
+++ apps/src/templates/manageStudents/DownloadParentLetters.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import Button from '@cdo/apps/templates/Button';
-import ReactTooltip from 'react-tooltip';
 import i18n from '@cdo/locale';
 import {teacherDashboardUrl} from '@cdo/apps/templates/teacherDashboard/urlHelpers';
 
@@ -20,28 +19,14 @@ export default class DownloadParentLetters extends Component {
   render() {
     return (
       <div style={styles.button}>
-        <span data-tip="" data-for="download-letter">
-          <Button
-            __useDeprecatedTag
-            href={teacherDashboardUrl(this.props.sectionId, '/parent_letter')}
-            target="_blank"
-            color={Button.ButtonColor.gray}
-            text={i18n.downloadParentLetter()}
-            icon="file-text"
-          />
-        </span>
-        <ReactTooltip
-          id="download-letter"
-          role="tooltip"
-          effect="solid"
-          delayShow={500}
-        >
-          <div>
-            {i18n.downloadParentLetterTooltip({
-              numStudents: this.props.numStudents
-            })}
-          </div>
-        </ReactTooltip>
+        <Button
+          __useDeprecatedTag
+          href={teacherDashboardUrl(this.props.sectionId, '/parent_letter')}
+          target="_blank"
+          color={Button.ButtonColor.gray}
+          text={i18n.downloadParentLetter()}
+          icon="file-text"
+        />
       </div>
     );
   }
