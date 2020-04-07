272fb048f730bad3d67b4624b9a3833085f8a85f
Bethany Connor <bethany@code.org>
2020-04-07 14:54:41 +0000

    Add a new SOURCE_TYPE for parent email change

--- dashboard/app/controllers/registrations_controller.rb
+++ dashboard/app/controllers/registrations_controller.rb
@@ -145,7 +145,7 @@ class RegistrationsController < Devise::RegistrationsController
         user[:parent_email_preference_email] = user[:parent_email]
         user[:parent_email_preference_opt_in_required] = '1'
         user[:parent_email_preference_request_ip] = request.ip
-        user[:parent_email_preference_source] = EmailPreference::ACCOUNT_SIGN_UP
+        user[:parent_email_preference_source] = EmailPreference::PARENT_EMAIL_CHANGE
       end.
       permit(
         :parent_email_preference_email,
