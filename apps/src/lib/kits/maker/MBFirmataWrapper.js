b2f94736db30da7d3c4a2f37813156c2331f7c30
Erin Peach <erin@code.org>
2020-04-08 13:55:49 -0700

    Add analogRead to wrapper

--- apps/src/lib/kits/maker/MBFirmataWrapper.js
+++ apps/src/lib/kits/maker/MBFirmataWrapper.js
@@ -1,20 +1,22 @@
 import MBFirmataClient from '../../../third-party/maker/MBFirmataClient';
 
 export default class MicrobitFirmataWrapper extends MBFirmataClient {
-  constructor(port) {
-    super(port);
-    this.digitalCallbacks = [];
-  }
-
   setPinMode(pin, mode) {
     // If setting a pin to input, start tracking it immediately
     if (mode === 0) {
       this.trackDigitalPin(pin);
+    } else if (mode === 2) {
+      // If setting a pin to analog_input, start tracking it immediately
+      this.streamAnalogChannel(pin);
     }
     super.setPinMode(pin, mode);
   }
 
   digitalRead(pin, callback) {
     callback(this.digitalInput[pin]);
+  }
+
+  analogRead(pin, callback) {
+    callback(this.analogChannel[pin]);
   }
 }
