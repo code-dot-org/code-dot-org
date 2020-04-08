7fdaf88fdcbd4baf27f6bcbab41fb2cc965a7df9
Ben Brooks <ben@code.org>
2020-04-07 17:10:02 -0700

    Rename pre_survey_url to pre_workshop_survey_url

--- dashboard/app/mailers/pd/workshop_mailer.rb
+++ dashboard/app/mailers/pd/workshop_mailer.rb
@@ -95,7 +95,7 @@ class Pd::WorkshopMailer < ActionMailer::Base
     @organizer = @workshop.organizer
     @cancel_url = url_for controller: 'pd/workshop_enrollment', action: :cancel, code: enrollment.code
     @is_reminder = true
-    @pre_survey_url = enrollment.pre_survey_url
+    @pre_workshop_survey_url = enrollment.pre_workshop_survey_url
     @is_first_pre_survey_email = days_before == INITIAL_PRE_SURVEY_DAYS_BEFORE
 
     return if @workshop.suppress_reminders?
