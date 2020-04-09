a57d0bafa3d1afd1cf938ac33e946835165aa952
Erin Peach <erin@code.org>
2020-04-09 14:10:50 -0700

    Remove unused ExternalLed

--- apps/src/lib/kits/maker/LedMatrix.js
+++ apps/src/lib/kits/maker/LedMatrix.js
@@ -36,26 +36,3 @@ export default class LedMatrix {
     this.board.mb.displayClear();
   }
 }
-
-export class ExternalLed {
-  constructor(opts) {
-    //TODO - limit to the GPIO pins on MB?
-    this.board = opts.board;
-    this.pin = opts.pin;
-    this.isOn = false;
-  }
-
-  on() {
-    this.board.setDigitalOutput(this.pin, 1);
-    this.isOn = true;
-  }
-
-  off() {
-    this.board.setDigitalOutput(this.pin, 0);
-    this.isOn = false;
-  }
-
-  toggle() {
-    return this.isOn ? this.off() : this.on();
-  }
-}
