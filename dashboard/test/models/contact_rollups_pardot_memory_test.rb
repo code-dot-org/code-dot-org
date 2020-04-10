7da5c1bfa718ced0c8cd7f0fd3dd75ed155a02e5
Ha Nguyen <ha@code.org>
2020-04-09 19:15:15 -0700

    Save email-pardot_id mappings to database in batches. Add tests.

--- dashboard/test/models/contact_rollups_pardot_memory_test.rb
+++ dashboard/test/models/contact_rollups_pardot_memory_test.rb
@@ -14,23 +14,40 @@ class ContactRollupsPardotMemoryTest < ActiveSupport::TestCase
     ContactRollupsPardotMemory.add_and_update_pardot_ids
 
     new_mappings.each do |mapping|
-      refute_nil ContactRollupsPardotMemory.find_by(email: mapping[:email], pardot_id: mapping[:pardot_id])
+      refute_nil ContactRollupsPardotMemory.
+        where(email: mapping[:email], pardot_id: mapping[:pardot_id]).
+        where.not(pardot_id_updated_at: nil).
+        first
     end
   end
 
-  test 'add_and_update_pardot_ids updates existing mapping' do
-    ContactRollupsPardotMemory.delete_all
-    existing_record = create :contact_rollups_pardot_memory
+  test 'add_and_update_pardot_ids updates existing mappings' do
+    assert_equal 0, ContactRollupsPardotMemory.count
 
-    new_pardot_id = existing_record.pardot_id + 1
-    PardotV2.stubs(:retrieve_new_ids).once.returns(
-      [{email: existing_record.email, pardot_id: new_pardot_id}]
-    )
+    email = 'test@domain.com'
+    base_time = Time.now.utc - 1.day
+    create :contact_rollups_pardot_memory, email: email, pardot_id: 1, pardot_id_updated_at: base_time
+
+    new_pardot_id = 2
+    PardotV2.stubs(:retrieve_new_ids).once.
+      returns([{email: email, pardot_id: new_pardot_id}])
 
     ContactRollupsPardotMemory.add_and_update_pardot_ids
 
-    assert_equal new_pardot_id,
-      ContactRollupsPardotMemory.find_by(email: existing_record.email).pardot_id
+    refute_nil ContactRollupsPardotMemory.
+      where(email: email, pardot_id: new_pardot_id).
+      where("pardot_id_updated_at > ?", base_time).
+      first
+  end
+
+  test 'add_and_update_pardot_ids save batches' do
+    assert_equal 0, ContactRollupsPardotMemory.count
+
+    new_mappings = (1..11).map {|i| {email: "test#{i}", pardot_id: i}}
+    PardotV2.stubs(:retrieve_new_ids).once.returns(new_mappings)
+    ContactRollupsPardotMemory.add_and_update_pardot_ids(nil, 3)
+
+    assert_equal new_mappings.length, ContactRollupsPardotMemory.count
   end
 
   test 'query_new_contacts' do
