4efb6475c8bdc25b2247a15b613e5c56cf0aab6b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2020-04-13 12:17:15 -0700

    render FreeResponse instructions clientside, with our safe markdown renderer

---
+++ apps/src/sites/studio/pages/levels/_free_response.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import React from 'react';
+import ReactDom from 'react-dom';
+import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
+
+$(document).ready(() => {
+  // Render Markdown
+  $('.free-response > .markdown-container').each(function() {
+    const container = this;
+    if (!container.dataset.markdown) {
+      return;
+    }
+
+    ReactDom.render(
+      React.createElement(SafeMarkdown, container.dataset, null),
+      container
+    );
+  });
+});
