51a1981de273fab51f27100f129f6c34f3d9e11e
Ben Brooks <ben@code.org>
2020-04-17 10:18:03 -0700

    Respond to PR feedback

--- lib/cdo/high_frequency_reporter.rb
+++ lib/cdo/high_frequency_reporter.rb
@@ -22,34 +22,45 @@ class HighFrequencyReporter
     @filename = log_file_name
   end
 
-  # Loads known events from previous runs from a file on disk
+  # Loads known events from previous run from a file on disk
+  # Note that this method will swallow errors, primarily intended to catch
+  # when a file containing old events doesn't exist (say, the first time this runs).
+  # @return [Boolean] true if events were successfully loaded, false if not
   def load
     # Format for log file: CSV
     # @see https://ruby-doc.org/stdlib-2.6.1/libdoc/csv/rdoc/CSV.html
+    # Header:
+    #  "reported_at", "name"
     # Each row:
-    #   "error message","timestamp"
+    #  2020-04-16 19:12:44 UTC, "error message"
     # Where error message is the full text of the error message (appropriately
     #   escaped by the CSV library)
     # And timestamp is the last time we reported this error to Slack,
     #   recorded in ISO 8601 format
-    @old_events = CSV.read(@filename, headers: true).map do |row|
-      row.to_h.transform_keys(&:to_sym).tap do |h|
-        h[:reported_at] = Time.parse(h[:reported_at])
+    if File.file? @filename
+      @old_events = CSV.read(@filename, headers: true).map do |row|
+        row.to_h.transform_keys(&:to_sym).tap do |h|
+          h[:reported_at] = Time.parse(h[:reported_at])
+        end
       end
+      true
+    else
+      false
     end
-  rescue
-    # Okay to do nothing here.
   end
 
   # Add a new event to our record for the current run
   # @param [String] event_name
   def record(event_name)
-    @new_events << {name: event_name, reported_at: Time.now}
+    @new_events << {reported_at: Time.now.utc, name: event_name}
   end
 
   # Reports alertable events to chat client
   # if current minute is multiple of throttle.
   # Defaults to always report alertable events unless throttled.
+  # Note that this really relies on your chat client having a
+  # method called "message" that takes a message and a channel as a param,
+  # so mostly limited to Slack as-is.
   # @param [Integer] throttle
   def report!(throttle = 1)
     if Time.now.min % throttle == 0
@@ -59,7 +70,7 @@ class HighFrequencyReporter
 
   # Saves known events and last report timestamps out to disk
   def save
-    CSV.open(@filename, 'wb', headers: %i[name reported_at], write_headers: true) do |csv|
+    CSV.open(@filename, 'wb', headers: %i[reported_at name], write_headers: true) do |csv|
       @new_events.each do |event|
         csv << event.values
       end
