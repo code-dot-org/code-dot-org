9e0d2b65248983a71ea7fc9a7af26c6dc1362b8f
Dayne <dayne@code.org>
2020-04-17 17:34:34 -0700

    Add validation exception for old teacher accounts.
    A few years ago, we added an email requirement for teacher accounts.
    At the time, we did not backfill existing teacher accounts, so we still
    have active teacher accounts which don't have emails recorded for them.
    These users currently experience pain because the code currently refuses
    to update change to their account because the account is currently
    invalid. This change makes it so these older accounts will be considered
    valid for the time being. There is follow up work to get an email
    recorded for the teacher accounts.

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -642,8 +642,9 @@ class User < ActiveRecord::Base
     nil
   end
 
-  validate :presence_of_email, if: -> {teacher? && purged_at.nil?}
-  validate :presence_of_email_or_hashed_email, if: :email_required?, on: :create
+  validate :presence_of_email, if: :teacher_email_required?
+  validate :presence_of_email_or_hashed_email, if:
+      :email_or_hashed_email_required?, on: :create
   validates :email, no_utf8mb4: true
   validates_email_format_of :email, allow_blank: true, if: :email_changed?, unless: -> {email.to_s.utf8mb4?}
   validate :email_and_hashed_email_must_be_unique, if: 'email_changed? || hashed_email_changed?'
@@ -808,7 +809,28 @@ class User < ActiveRecord::Base
     new_without_password || is_changing_password
   end
 
-  def email_required?
+  # FND-1130: This field will no longer be required
+  DATE_TEACHER_EMAIL_REQUIREMENT_ADDED = '2016-06-14 00:00:00'.to_datetime
+
+  # Determines if email is a required field for a teacher.
+  # Currently, we have some old teacher accounts which don't have an email
+  # address associated with them because it wasn't required when they were
+  # created. Those old accounts are allowed to skip the email validation.
+  def teacher_email_required?
+    # non-teachers are not relevant to this method.
+    return false unless teacher? && purged_at.nil?
+
+    # new teacher accounts should always require an email
+    return true unless created_at.present?
+
+    # existing accounts created after the email requirement must have an email.
+    # FND-1130: The created_at exception will no longer be required
+    # Remove the created_at > '2016-06-14 00:00:00' once all teachers have
+    # emails.
+    return created_at.to_datetime > DATE_TEACHER_EMAIL_REQUIREMENT_ADDED
+  end
+
+  def email_or_hashed_email_required?
     return true if teacher?
     return false if manual?
     return false if sponsored?
