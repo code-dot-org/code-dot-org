7d981428a80780676e3310ed209f164c8e76d4a9
Ben Brooks <ben@code.org>
2020-04-20 12:03:58 -0700

    Require specifying email column from source

--- dashboard/app/models/contact_rollups_raw.rb
+++ dashboard/app/models/contact_rollups_raw.rb
@@ -22,30 +22,37 @@ class ContactRollupsRaw < ApplicationRecord
     ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name}")
   end
 
-  def self.extract_from_table(source_table, columns)
-    email_preferences_query = extract_from_table_query(source_table, columns)
+  # Generates insert query to run, and runs that query once constructed.
+  def self.extract_from_table(source_table, data_columns, email_column)
+    email_preferences_query = extract_from_table_query(source_table, data_columns, email_column)
     ActiveRecord::Base.connection.execute(email_preferences_query)
   end
 
-  def self.extract_from_table_query(source_table, columns)
-    now = Time.now.utc
-
+  # @param source_table [String] Table from which we want to extract data
+  # @param data_columns [Array] Columns we want reshaped into a single JSON object
+  # @param email_column [String] Column in source table we want to insert ino the email column
+  # @return [String] A SQL statement to extract and reshape data from the source table.
+  def self.extract_from_table_query(source_table, data_columns, email_column)
+    # TO DO: what to do when an email appears multiple times in the same source?
     <<~SQL
       INSERT INTO #{ContactRollupsRaw.table_name} (email, sources, data, data_updated_at, created_at, updated_at)
       SELECT
-        email,
-        'dashboard.#{source_table}' as sources,
-        #{extract_columns_into_mysql_json(columns)} as data,
-        #{source_table}.updated_at as data_updated_at,
-        '#{now.strftime('%Y-%m-%d %H:%M:%S')}' as created_at,
-        '#{now.strftime('%Y-%m-%d %H:%M:%S')}' as updated_at
+        #{email_column},
+        'dashboard.#{source_table}' AS sources,
+        #{extract_columns_into_mysql_json(data_columns)} AS data,
+        #{source_table}.updated_at AS data_updated_at,
+        NOW() AS created_at,
+        NOW() AS updated_at
       FROM #{source_table}
+      WHERE #{email_column} IS NOT NULL AND #{email_column} != ''
     SQL
   end
 
-  # params -- takes an array of column names
-  # returns -- a MySQL string that can be used in a SELECT statement to return a list of
+  # @param columns [Array] Column names to reshape
+  # @return [String] MySQL JSON_OBJECT() syntax for insertion
   def self.extract_columns_into_mysql_json(columns)
+    return 'NULL' if columns.empty?
+
     last = columns.pop
     'JSON_OBJECT(' + columns.map {|column| "'#{column}', #{column}, "}.join + "'#{last}', #{last}" + ')'
   end
