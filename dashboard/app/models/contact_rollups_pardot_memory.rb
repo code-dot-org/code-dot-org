5d5ab17bbe392d2f14d1689f34850ee8950d17dd
Ben Brooks <ben@code.org>
2020-04-20 16:23:28 -0700

    Use timestamp instead of boolean for pardot deletion

--- dashboard/app/models/contact_rollups_pardot_memory.rb
+++ dashboard/app/models/contact_rollups_pardot_memory.rb
@@ -2,23 +2,23 @@
 #
 # Table name: contact_rollups_pardot_memory
 #
-#  id                   :integer          not null, primary key
-#  email                :string(255)      not null
-#  pardot_id            :integer
-#  pardot_id_updated_at :datetime
-#  data_synced          :json
-#  data_synced_at       :datetime
-#  data_rejected_at     :datetime
-#  data_rejected_reason :string(255)
-#  delete_from_pardot   :boolean
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      not null
+#  pardot_id              :integer
+#  pardot_id_updated_at   :datetime
+#  data_synced            :json
+#  data_synced_at         :datetime
+#  data_rejected_at       :datetime
+#  data_rejected_reason   :string(255)
+#  marked_for_deletion_at :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
 #
 # Indexes
 #
-#  index_contact_rollups_pardot_memory_on_delete_from_pardot  (delete_from_pardot)
-#  index_contact_rollups_pardot_memory_on_email               (email) UNIQUE
-#  index_contact_rollups_pardot_memory_on_pardot_id           (pardot_id) UNIQUE
+#  index_contact_rollups_pardot_memory_on_email                   (email) UNIQUE
+#  index_contact_rollups_pardot_memory_on_marked_for_deletion_at  (marked_for_deletion_at)
+#  index_contact_rollups_pardot_memory_on_pardot_id               (pardot_id) UNIQUE
 #
 
 require 'cdo/contact_rollups/v2/pardot'
