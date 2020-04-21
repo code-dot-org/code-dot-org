bae79b3cce2d1a340e5e388058d31e22da8879d5
Dani McAvoy <danimcavoy@gmail.com>
2020-04-21 15:53:53 -0400

    Reserve key values for plc modules

--- dashboard/app/models/script.rb
+++ dashboard/app/models/script.rb
@@ -926,12 +926,19 @@ class Script < ActiveRecord::Base
         script_lesson_groups << lesson_group
       else
         # Finds or creates Lesson Groups with the correct position.
-        # In addition it check for 2 things:
+        # In addition it check for 3 things:
         # 1. That all the lesson groups specified by the editor have a key and
         # display name.
         # 2. If the lesson group key is an existing key that the given display name
         # for that key matches the already saved display name
+        # 3. PLC courses use certain lesson group keys for module types. We reserve those
+        # keys so they can only map to the display_name for their PLC purpose
         raw_lesson_groups&.each_with_index do |raw_lesson_group, index|
+          Plc::LearningModule::RESERVED_LESSON_GROUPS_FOR_PLC.each do |reserved_lesson_group|
+            if reserved_lesson_group[:key] == raw_lesson_group[:key] && reserved_lesson_group[:display_name] != raw_lesson_group[:display_name]
+              raise "The key #{reserved_lesson_group[:key]} is a reserved key. It must have the display name: #{reserved_lesson_group[:display_name]}"
+            end
+          end
           if raw_lesson_group[:display_name].blank?
             raise "Expect all lesson groups to have display names. The following lesson group does not have a display name: #{raw_lesson_group[:key]}"
           end
