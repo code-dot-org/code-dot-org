14b6fac1e0cf44f1a82cb4281c26b589f38a55ff
Dani McAvoy <danimcavoy@gmail.com>
2020-04-21 20:52:53 -0400

    Revert "Add a default value for display name and update tests to match"
    This reverts commit aebe4eb8881cef72bb40f020c37a69312bfa007b.

--- dashboard/test/models/script_test.rb
+++ dashboard/test/models/script_test.rb
@@ -1993,7 +1993,7 @@ endvariants
     script = create :script, name: "lesson-group-test-script"
     create :lesson_group, key: 'required', script: script
     dsl = <<-SCRIPT
-      lesson_group 'content', display_name: 'not content'
+      lesson_group 'required', display_name: 'not required'
       stage 'Lesson1'
       level '#{l1.name}'
 
@@ -2006,7 +2006,7 @@ endvariants
         ScriptDSL.parse(dsl, 'a filename')[0][:stages]
       )
     end
-    assert_equal 'Expect key and display name to match. The Lesson Group with key: content has display_name: Content', raise.message
+    assert_equal 'Expect key and display name to match. The Lesson Group with key: required has display_name: translation missing: en-US.data.script.name.lesson-group-test-script.lesson_groups.required.display_name', raise.message
   end
 
   test 'raises error if some lessons have lesson groups and some do not' do
@@ -2036,13 +2036,13 @@ endvariants
     l2 = create :level
     l3 = create :level
     dsl = <<-SCRIPT
-      lesson_group 'content', display_name: 'Content'
+      lesson_group 'content', display_name: 'translation missing: en-US.data.script.name.lesson-group-test-script.lesson_groups.content.display_name'
       stage 'Lesson1'
       level '#{l1.name}'
-      lesson_group 'required', display_name: 'Content'
+      lesson_group 'required', display_name: 'translation missing: en-US.data.script.name.lesson-group-test-script.lesson_groups.required.display_name'
       stage 'Lesson2'
       level '#{l2.name}'
-      lesson_group 'content', display_name: 'Content'
+      lesson_group 'content', display_name: 'translation missing: en-US.data.script.name.lesson-group-test-script.lesson_groups.content.display_name'
       stage 'Lesson3'
       level '#{l3.name}'
     SCRIPT
@@ -2198,20 +2198,20 @@ endvariants
     l1 = create :level
     l2 = create :level
     old_dsl = <<-SCRIPT
-      lesson_group 'required', display_name: 'Content'
+      lesson_group 'required', display_name: 'translation missing: en-US.data.script.name.lesson-group-test-script.lesson_groups.required.display_name'
       stage 'Lesson1'
       level '#{l1.name}'
 
-      lesson_group 'content', display_name: 'Content'
+      lesson_group 'content', display_name: 'translation missing: en-US.data.script.name.lesson-group-test-script.lesson_groups.content.display_name'
       stage 'Lesson2'
       level '#{l2.name}'
     SCRIPT
     new_dsl = <<-SCRIPT
-      lesson_group 'content', display_name: 'Content'
+      lesson_group 'content', display_name: 'translation missing: en-US.data.script.name.lesson-group-test-script.lesson_groups.content.display_name'
       stage 'Lesson1'
       level '#{l1.name}'
 
-      lesson_group 'required', display_name: 'Content'
+      lesson_group 'required', display_name: 'translation missing: en-US.data.script.name.lesson-group-test-script.lesson_groups.required.display_name'
       stage 'Lesson2'
       level '#{l2.name}'
     SCRIPT
