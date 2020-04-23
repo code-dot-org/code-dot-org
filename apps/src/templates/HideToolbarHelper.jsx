75035290e53b41427685d0c1729ab5bc6d59a7da
Brendan Reville <brendan@code.org>
2020-04-23 15:50:26 -0700

    Hide Toolbar Helper: improvements

--- apps/src/templates/HideToolbarHelper.jsx
+++ apps/src/templates/HideToolbarHelper.jsx
@@ -1,50 +1,78 @@
 import React from 'react';
+import msg from '@cdo/locale';
 import cookies from 'js-cookie';
+import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
 import _ from 'lodash';
+
+const styles = {
+  closeX: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+    fontSize: 13
+  }
+};
 
 /**
  * An overlay with instrutions on hiding the toolbar for iPhone with iOS 13.
  */
 export default class HideToolbarHelper extends React.Component {
   state = {
-    isLandscape: false
+    showHelper: false
   };
 
-  updateOrientation = () => {
-    this.setState({isLandscape: window.orientation !== 0});
+  updateLayout = () => {
+    const isiOS13 = navigator.userAgent.indexOf('iPhone OS 13') !== -1;
+    const isHideCookieSet = cookies.get('hide_toolbar_helper');
+    const isLandscape = window.orientation !== 0;
+
+    // window.innerHeight is smaller than document.body.offsetHeight when
+    // the iOS 13 Safari toolbar is showing.  It becomes equal when the toolbar
+    // is hidden.  (Interestingly, document.documentElement.clientHeight is
+    // the same as document.body.offsetHeight in both cases.)
+    const isToolbarShowing = window.innerHeight < document.body.offsetHeight;
+
+    const showHelper =
+      isiOS13 && !isHideCookieSet && isLandscape && isToolbarShowing;
+
+    this.setState({showHelper});
   };
 
   componentDidMount() {
-    this.updateOrientation();
+    this.updateLayout();
 
-    this.orientationListener = _.throttle(this.updateOrientation, 200);
-    window.addEventListener('orientationchange', this.orientationListener);
-    window.addEventListener('resize', this.orientationListener);
+    this.updateLayoutListener = _.throttle(this.updateLayout, 200);
+    window.addEventListener('orientationchange', this.updateLayoutListener);
+
+    /* These two events catch all viewport changes. */
+    window.addEventListener('resize', this.updateLayoutListener);
+    window.addEventListener('scroll', this.updateLayoutListener);
   }
 
   componentWillUnmount() {
     document.removeEventListener(
       'orientationchange',
-      this.orientationListener,
+      this.updateLayoutListener,
       false
     );
-    document.removeEventListener('resize', this.orientationListener, false);
+    document.removeEventListener('resize', this.updateLayoutListener, false);
+    document.removeEventListener('scroll', this.updateLayoutListener, false);
   }
 
-  render() {
-    const isiOS13 = navigator.userAgent.indexOf('iPhone OS 13') !== -1;
-    const isHideCookieSet = cookies.get('hide_toolbar_helper');
-    const isToolbarShowing = window.innerHeight < document.body.offsetHeight;
+  onClick = () => {
+    cookies.set('hide_toolbar_helper', 'true', {expires: 365, path: '/'});
+    this.updateLayout();
+  };
 
-    if (
-      isiOS13 &&
-      !isHideCookieSet &&
-      this.state.isLandscape &&
-      isToolbarShowing
-    ) {
+  render() {
+    const forceShowHelper =
+      window.location.search.indexOf('force_show_toolbar_helper') !== -1;
+
+    if (this.state.showHelper || forceShowHelper) {
       return (
-        <div className="hide_toolbar_helper">
-          Press AA and choose Hide Toolbar for more space. }
+        <div className="hide_toolbar_helper" onClick={this.onClick}>
+          <SafeMarkdown markdown={msg.hideToolbarHelper()} />
+          <i className="fa fa-times" style={styles.closeX} />
         </div>
       );
     } else {
