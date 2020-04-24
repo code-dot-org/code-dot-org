933060ef2b26810d1e16331b732791758f190a79
Erin Peach <erin@code.org>
2020-04-24 15:25:20 -0700

    Refactor FakeBoard specific tests to FakeBoardTest

--- apps/test/unit/lib/kits/maker/boards/FakeBoardTest.js
+++ apps/test/unit/lib/kits/maker/boards/FakeBoardTest.js
@@ -1,9 +1,14 @@
 import {itImplementsTheMakerBoardInterface} from './MakerBoardTest';
 import FakeBoard from '@cdo/apps/lib/kits/maker/boards/FakeBoard';
 import {expect} from '../../../../../util/reconfiguredChai';
+import {itMakesComponentsAvailableFromInterpreter} from './circuitPlayground/CircuitPlaygroundMakerBoardTest';
 
 describe('FakeBoard', () => {
   itImplementsTheMakerBoardInterface(FakeBoard);
+
+  // Board-specific tests
+  // Test that the appropriate components are available on each board
+  itMakesComponentsAvailableFromInterpreter(FakeBoard);
 
   describe(`boardConnected()`, () => {
     it('always returns false', () => {
