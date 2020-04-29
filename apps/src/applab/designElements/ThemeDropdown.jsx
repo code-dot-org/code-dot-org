ec9417e9cf85da481e0c6d59ead1851edd327aa0
molly-moen <molly@code.org>
2020-04-29 10:03:35 -0700

    new theme dropdown

---
+++ apps/src/applab/designElements/ThemeDropdown.jsx
@@ -0,0 +1,84 @@
+// Dropdown for theme selection that shows sample icons of what
+// each theme will look like
+import PropTypes from 'prop-types';
+import React from 'react';
+import {themeOptionsForSelect} from '../constants';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+export default class ThemeDropdown extends React.Component {
+  static propTypes = {
+    initialValue: PropTypes.string.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    desc: PropTypes.node
+  };
+
+  state = {
+    selectedValue: this.props.initialValue
+  };
+  handleChange = event => {
+    this.props.handleChange(event.value);
+    this.setState({selectedValue: event.value});
+  };
+
+  // Need to reset theme value if screen switches
+  componentWillReceiveProps(nextProps) {
+    const {initialValue} = nextProps;
+    if (this.props.initialValue !== initialValue) {
+      this.setState({selectedValue: initialValue});
+    }
+  }
+
+  // need map of value-> {icon, display name}
+  render() {
+    const styles = {
+      outerContainer: {
+        marginBottom: 8,
+        width: 240
+      },
+      description: {
+        paddingLeft: 2,
+        paddingBottom: 2
+      },
+      label: {
+        paddingLeft: 4
+      },
+      icon: {
+        padding: 0,
+        align: 'left'
+      },
+      dropdownLabel: {
+        display: 'flex',
+        flexDeirection: 'row',
+        alignItems: 'center'
+      }
+    };
+
+    const {desc} = this.props;
+    const {selectedValue} = this.state;
+
+    const renderedOptions = themeOptionsForSelect.map(function(option, index) {
+      return {
+        value: option.option,
+        label: (
+          <div className="theme-dropdown-label" style={styles.dropdownLabel}>
+            <img style={styles.icon} src={option.icon} />
+            <div style={styles.label}>{option.displayName}</div>
+          </div>
+        )
+      };
+    });
+    return (
+      <div style={styles.outerContainer} className="theme-dropdown">
+        <div style={styles.description}>{desc}</div>
+        <Select
+          className="form-control"
+          value={selectedValue}
+          onChange={this.handleChange}
+          options={renderedOptions}
+          placeholder={''}
+        />
+      </div>
+    );
+  }
+}
