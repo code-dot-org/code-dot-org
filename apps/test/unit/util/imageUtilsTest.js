2991f661ae7922a6cc6d1fad806f800fc1e9b674
Brad Buchanan <brad@code.org>
2020-04-30 11:11:46 -0700

    Move toImageData into imageUtils

--- apps/test/unit/util/imageUtilsTest.js
+++ apps/test/unit/util/imageUtilsTest.js
@@ -1,9 +1,10 @@
 import {
   blobToDataURI,
   dataURIFromURI,
-  dataURIToFramedBlob
+  dataURIToFramedBlob,
+  toImageData
 } from '@cdo/apps/imageUtils';
-import {expect} from '../../util/deprecatedChai';
+import {assert, expect} from 'chai';
 import expectedPhantomPng from './expected-phantom.png';
 import expectedChromePng from './expected-chrome.png';
 
@@ -24,6 +25,55 @@ describe('image utils', () => {
           done();
         });
       });
+    });
+  });
+
+  describe('toImageData', () => {
+    it('returns ImageData unchanged', async () => {
+      const imageData = new ImageData(8, 8);
+      const result = await toImageData(imageData);
+      assert(imageData === result);
+    });
+
+    it('converts a canvas to an ImageData object', async () => {
+      // Expected Image data: We're going to draw a 4px opaque white square
+      // That's represented as a 64-byte image (16 pixels * 4 bytes per pixel)
+      // at its maximum value RGBA(255, 255, 255, 255)
+      const size = 4;
+      const expectedData = new Uint8ClampedArray(size * size * 4);
+      for (let i = 0; i < expectedData.length; i++) {
+        expectedData[i] = 0xff;
+      }
+
+      // Actual canvas: Make a 4x4 canvas and fill it with a white square
+      const canvas = document.createElement('canvas');
+      canvas.width = size;
+      canvas.height = size;
+      const ctx = canvas.getContext('2d');
+      ctx.fillStyle = 'white';
+      ctx.rect(0, 0, 4, 4);
+      ctx.fill();
+      const actual = await toImageData(canvas);
+
+      assert.instanceOf(actual, ImageData);
+      assert.equal(canvas.width, actual.width);
+      assert.equal(canvas.height, actual.height);
+      assert.deepEqual(expectedData, actual.data);
+    });
+
+    it('converts an image URI string to an ImageData object', async () => {
+      assert.typeOf(expectedChromePng, 'string');
+      const result = await toImageData(expectedChromePng);
+      assert.instanceOf(result, ImageData);
+      assert.equal(522000, result.data.length);
+    });
+
+    it('converts a data URI string to an ImageData object', async () => {
+      const dataURI =
+        'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"><circle cx="0" cy="0" r="1000"/></svg>';
+      const result = await toImageData(dataURI);
+      assert.instanceOf(result, ImageData);
+      assert.equal(180000, result.data.length);
     });
   });
 });
