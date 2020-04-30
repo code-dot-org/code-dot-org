923126af7aa3554f54ed4edf0d2db18753735133
Brad Buchanan <brad@code.org>
2020-04-30 11:58:18 -0700

    Add toCanvas

--- apps/test/unit/util/thumbnailTest.js
+++ apps/test/unit/util/thumbnailTest.js
@@ -1,5 +1,5 @@
 import {createThumbnail} from '@cdo/apps/util/thumbnail';
-import {canvasFromURI} from '@cdo/apps/imageUtils';
+import {toCanvas} from '@cdo/apps/imageUtils';
 import assertVisualMatch from '../../util/assertVisualMatch';
 import squarePlayspaceImage from './squarePlayspaceImage.png';
 import squarePlayspaceThumbnail from './squarePlayspaceThumbnail.png';
@@ -9,14 +9,14 @@ import tallPlayspaceThumbnail from './tallPlayspaceThumbnail.png';
 describe('createThumbnail', () => {
   it('creates a square thumbnail from a square image', async () => {
     const expectedThumbnail = squarePlayspaceThumbnail;
-    const fullSizeCanvas = await canvasFromURI(squarePlayspaceImage);
+    const fullSizeCanvas = await toCanvas(squarePlayspaceImage);
     const actualThumbnail = createThumbnail(fullSizeCanvas);
     await assertVisualMatch(expectedThumbnail, actualThumbnail);
   });
 
   it('creates square center-crop thumbnail from tall rectangular image', async () => {
     const expectedThumbnail = tallPlayspaceThumbnail;
-    const fullSizeCanvas = await canvasFromURI(tallPlayspaceImage);
+    const fullSizeCanvas = await toCanvas(tallPlayspaceImage);
     const actualThumbnail = createThumbnail(fullSizeCanvas);
     await assertVisualMatch(expectedThumbnail, actualThumbnail);
   });
