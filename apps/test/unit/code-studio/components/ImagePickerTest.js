874629685c399ab72d1e0dcccd483d4e15291e71
Ben Brooks <ben@code.org>
2020-05-05 10:21:41 -0700

    Additional unit test for ImagePicker component

--- apps/test/unit/code-studio/components/ImagePickerTest.js
+++ apps/test/unit/code-studio/components/ImagePickerTest.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import ImagePicker from '@cdo/apps/code-studio/components/ImagePicker';
 import IconLibrary from '@cdo/apps/code-studio/components/IconLibrary';
+import AssetManager from '@cdo/apps/code-studio/components/AssetManager';
 
 describe('ImagePicker', () => {
   const defaultProps = {
@@ -12,14 +13,31 @@ describe('ImagePicker', () => {
     showUnderageWarning: false,
     useFilesApi: false
   };
-  it('shows two tabs by default', () => {
+
+  it('shows mode switch', () => {
     const wrapper = shallow(<ImagePicker {...defaultProps} />);
     assert.equal(wrapper.find('#modeSwitch').length, 1);
   });
+
+  it('shows icons and files as options', () => {
+    const wrapper = shallow(<ImagePicker {...defaultProps} />);
+    const tabs = wrapper
+      .find('#modeSwitch')
+      .find('p');
+
+    assert.equal(tabs.length, 2);
+    assert.equal(tabs.find('p[children="Icons"]').length, 1);
+    assert.equal(tabs.find('p[children="My Files"]').length, 1);
+  });
+
+  it('initially shows file picker by default', () => {
+    const wrapper = shallow(<ImagePicker {...defaultProps} />);
+    assert.equal(wrapper.find(AssetManager).length, 1);
+  });
+
   it('shows icon picker when clicked', () => {
     const wrapper = shallow(<ImagePicker {...defaultProps} />);
     wrapper.find('p[children="Icons"]').simulate('click');
-    console.log(wrapper.debug());
     assert.equal(wrapper.find(IconLibrary).length, 1);
   });
 });
