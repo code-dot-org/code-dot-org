a857df4f832ac9eac48dcb45d67dbe6106e787d2
Winter Dong <winter@code.org>
2020-05-07 17:48:30 -0700

    Rename to lessonExtrasScriptIds in homepage_data

--- dashboard/app/controllers/home_controller.rb
+++ dashboard/app/controllers/home_controller.rb
@@ -94,7 +94,7 @@ class HomeController < ApplicationController
 
     @homepage_data = {}
     @homepage_data[:valid_grades] = Section.valid_grades
-    @homepage_data[:stageExtrasScriptIds] = Script.lesson_extras_script_ids
+    @homepage_data[:lessonExtrasScriptIds] = Script.lesson_extras_script_ids
     @homepage_data[:isEnglish] = request.language == 'en'
     @homepage_data[:locale] = Script.locale_english_name_map[request.locale]
     @homepage_data[:canViewAdvancedTools] = !(current_user.under_13? && current_user.terms_version.nil?)
