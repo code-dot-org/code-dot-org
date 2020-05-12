40cd24c60ab69fcc9ebb817b341767e48013065a
David Bailey <davidsbailey@users.noreply.github.com>
2020-05-12 01:25:55 -0700

    make test_clone_level_group_with_suffix stop stubbing the class under test

--- dashboard/test/models/level_group_test.rb
+++ dashboard/test/models/level_group_test.rb
@@ -285,7 +285,7 @@ MARKDOWN
   level 'level7'
   "
 
-    level_group_copy_dsl = "name 'level_group_test long assessment_copy'
+    expected_copy_dsl = "name 'level_group_test long assessment_copy'
 title 'Long Assessment'
 submittable 'true'
 
@@ -304,36 +304,42 @@ level 'level6_copy'
 level 'level7_copy'"
 
     # Create multis named level1-level7.
-    levels = {}
-    multi_stubs = Multi.any_instance.stubs(:dsl_text)
     (1..7).each do |id|
-      dsl_text = get_multi_dsl(id)
-      levels["multi_#{id}"] = Multi.create_from_level_builder({}, {dsl_text: dsl_text})
-      multi_stubs.returns(dsl_text)
+      multi_dsl = get_multi_dsl(id)
+      multi = Multi.create_from_level_builder({}, {dsl_text: multi_dsl})
+      File.stubs(:exist?).with {|filepath, _| filepath.to_s.end_with?(multi.filename)}.returns(true)
+      File.stubs(:read).with {|filepath, _| filepath.to_s.end_with?(multi.filename)}.returns(multi_dsl)
     end
 
     # Create the external level.
     external_dsl = get_external_dsl(1)
     External.create_from_level_builder({}, {dsl_text: external_dsl})
-    External.any_instance.stubs(:dsl_text).returns(external_dsl)
+    File.stubs(:exist?).with {|filepath, _| filepath.to_s.end_with?("external1.external")}.returns(true)
+    File.stubs(:read).with {|filename, _| filename.to_s.end_with?("external1.external")}.returns(external_dsl)
 
     # Create the level_group.
     level_group = LevelGroup.create_from_level_builder({}, {name: 'my_level_group', dsl_text: level_group_input_dsl})
-    level_group.stubs(:dsl_text).returns(level_group_input_dsl)
+    File.stubs(:exist?).with {|filepath, _| filepath.to_s.end_with?("level_group_test_long_assessment.level_group")}.returns(true)
+    File.stubs(:read).with {|filepath, _| filepath.to_s.end_with?("level_group_test_long_assessment.level_group")}.returns(level_group_input_dsl)
+
+    File.stubs(:write).with do |filepath, actual_dsl|
+      filepath.basename.to_s == 'level_group_test_long_assessment_copy.level_group' &&
+        expected_copy_dsl == actual_dsl
+    end.once
 
     # Copy the level group and all its sub levels.
     level_group_copy = level_group.clone_with_suffix('_copy')
 
-    assert_equal level_group_copy_dsl, level_group_copy.dsl_text
+    # Verify the result
+    assert_equal 'level_group_test long assessment_copy', level_group_copy.name
+    assert_equal 3, level_group_copy.pages.count
+
     (1..7).each do |id|
       refute_nil l = Level.find_by_name("level#{id}_copy")
       assert_equal 'What is the name of this function?', l.properties['questions'].first['text']
     end
     refute_nil l = Level.find_by_name('external1_copy')
     assert_includes l.properties['markdown'], 'Sample external'
-
-    # clean up
-    File.delete(level_group_copy.filename)
   end
 
   test 'clone previously cloned level group' do
