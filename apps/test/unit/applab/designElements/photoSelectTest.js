6a14a97a64430ebed262e10e2f57750f7fbcedb5
JillianK <jillian9012@gmail.com>
2020-05-19 10:05:29 -0700

    addressed comments

--- apps/test/unit/applab/designElements/photoSelectTest.js
+++ apps/test/unit/applab/designElements/photoSelectTest.js
@@ -1,20 +1,20 @@
-import {expect} from '../../../util/deprecatedChai';
+import {expect} from '../../../util/reconfiguredChai';
 import library from '@cdo/apps/applab/designElements/library';
 
-function getRect(e) {
+function getRect(photoSelectComponent) {
   return {
-    left: parseInt(e.style.left, 10),
-    top: parseInt(e.style.top, 10),
-    width: parseInt(e.style.width, 10),
-    height: parseInt(e.style.height, 10)
+    left: parseInt(photoSelectComponent.style.left, 10),
+    top: parseInt(photoSelectComponent.style.top, 10),
+    width: parseInt(photoSelectComponent.style.width, 10),
+    height: parseInt(photoSelectComponent.style.height, 10)
   };
 }
 
 describe('Applab designElements/photoSelect component', function() {
-  let e;
+  let photoSelectComponent;
 
   beforeEach(() => {
-    e = library.createElement(
+    photoSelectComponent = library.createElement(
       library.ElementType.PHOTO_SELECT,
       50 /* left */,
       40 /* top */,
@@ -23,14 +23,14 @@ describe('Applab designElements/photoSelect component', function() {
   });
 
   it('decreasing font size does not change size to fit', () => {
-    const oldRect = getRect(e);
-    e.style.fontSize = 5;
-    expect(getRect(e)).to.deep.equal(oldRect);
+    const oldRect = getRect(photoSelectComponent);
+    photoSelectComponent.style.fontSize = 5;
+    expect(getRect(photoSelectComponent)).to.deep.equal(oldRect);
   });
 
   it('increasing font size does not change size to fit', () => {
-    const oldRect = getRect(e);
-    e.style.fontSize = 100;
-    expect(getRect(e)).to.deep.equal(oldRect);
+    const oldRect = getRect(photoSelectComponent);
+    photoSelectComponent.style.fontSize = 100;
+    expect(getRect(photoSelectComponent)).to.deep.equal(oldRect);
   });
 });
