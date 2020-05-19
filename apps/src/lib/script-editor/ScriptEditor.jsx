7ecd8aa9df9281a9204c485a94b0b3b6555a4d97
Dani McAvoy <danimcavoy@gmail.com>
2020-05-18 20:42:12 -0400

    Add HelpTips on the script edit page and group by categories

--- apps/src/lib/script-editor/ScriptEditor.jsx
+++ apps/src/lib/script-editor/ScriptEditor.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import FlexGroup from './FlexGroup';
 import LessonDescriptions from './LessonDescriptions';
 import ScriptAnnouncementsEditor from './ScriptAnnouncementsEditor';
-import ProgressLegend from '@cdo/apps/templates/progress/ProgressLegend';
 import $ from 'jquery';
 import ResourcesEditor from '@cdo/apps/templates/courseOverview/ResourcesEditor';
 import DropdownButton from '@cdo/apps/templates/DropdownButton';
@@ -14,6 +13,7 @@ import ResourceType, {
 } from '@cdo/apps/templates/courseOverview/resourceType';
 import {announcementShape} from '@cdo/apps/code-studio/scriptAnnouncementsRedux';
 import VisibleAndPilotExperiment from './VisibleAndPilotExperiment';
+import HelpTip from '@cdo/apps/lib/ui/HelpTip';
 
 const styles = {
   input: {
@@ -161,16 +161,98 @@ export default class ScriptEditor extends React.Component {
             style={styles.input}
           />
         </label>
-        <LessonDescriptions
-          scriptName={this.props.name}
-          currentDescriptions={this.props.i18nData.stageDescriptions}
-        />
+        <h2>Basic Settings</h2>
+        <label>
+          Login Required
+          <input
+            name="login_required"
+            type="checkbox"
+            defaultChecked={this.props.loginRequired}
+            style={styles.checkbox}
+          />
+          <HelpTip>
+            <p>Require users to log in before viewing this script.</p>
+          </HelpTip>
+        </label>
+        <label>
+          Default Progress to Detail View
+          <input
+            name="student_detail_progress_view"
+            type="checkbox"
+            defaultChecked={this.props.studentDetailProgressView}
+            style={styles.checkbox}
+          />
+          <HelpTip>
+            <p>
+              By default students start in the summary view. When this box is
+              checked, we instead stick everyone into detail view to start for
+              this script.
+            </p>
+          </HelpTip>
+        </label>
+        <label>
+          Text-to-Speech
+          <input
+            name="tts"
+            type="checkbox"
+            defaultChecked={this.props.tts}
+            style={styles.checkbox}
+          />
+          <HelpTip>
+            <p>Check to enable text-to-speech for this script.</p>
+          </HelpTip>
+        </label>
+        <label>
+          Supported locales
+          <p>
+            Select additional locales supported by this script. Select
+            <a onClick={this.handleClearSupportedLocalesSelectClick}> none </a>
+            or shift-click or cmd-click to select multiple.
+          </p>
+          <select
+            name="supported_locales[]"
+            multiple
+            defaultValue={this.props.supportedLocales}
+            ref={select => (this.supportedLocaleSelect = select)}
+          >
+            {this.props.locales
+              .filter(locale => !locale[1].startsWith('en'))
+              .map(locale => (
+                <option key={locale[1]} value={locale[1]}>
+                  {locale[1]}
+                </option>
+              ))}
+          </select>
+        </label>
+        {this.props.isLevelbuilder && (
+          <label>
+            Editor Experiment.
+            <HelpTip>
+              <p>
+                If specified, users with this experiment on the levelbuilder
+                machine will be able to edit this script.
+              </p>
+            </HelpTip>
+            <input
+              name="editor_experiment"
+              defaultValue={this.props.editorExperiment}
+              style={styles.input}
+            />
+          </label>
+        )}
+        <label>
+          Wrap-up Video
+          <input
+            name="wrapup_video"
+            defaultValue={this.props.wrapupVideo}
+            style={styles.input}
+          />
+        </label>
         <ScriptAnnouncementsEditor
           defaultAnnouncements={this.props.announcements}
           inputStyle={styles.input}
         />
-        <h2>Basic Settings</h2>
-
+        <h2>Version Settings</h2>
         {this.props.isLevelbuilder && (
           <div>
             <label>
@@ -188,21 +270,20 @@ export default class ScriptEditor extends React.Component {
                   </option>
                 ))}
               </select>
-              <p>
-                By selecting one of the above, this script will have a property,
-                curriculum_umbrella, specific to that course regardless of
-                version.
-              </p>
-              <p>
-                If you select CSF, CSF-specific elements will show in the
-                progress tab of the teacher dashboard. For example, the progress
-                legend will include a separate column for levels completed with
-                too many blocks and there will be information about CSTA
-                Standards.
-              </p>
-              <ProgressLegend
-                excludeCsfColumn={this.state.curriculumUmbrella !== 'CSF'}
-              />
+              <HelpTip>
+                <p>
+                  By selecting, this script will have a property,
+                  curriculum_umbrella, specific to that course regardless of
+                  version.
+                </p>
+                <p>
+                  If you select CSF, CSF-specific elements will show in the
+                  progress tab of the teacher dashboard. For example, the
+                  progress legend will include a separate column for levels
+                  completed with too many blocks and there will be information
+                  about CSTA Standards.
+                </p>
+              </HelpTip>
             </label>
             <label>
               Family Name
@@ -234,10 +315,6 @@ export default class ScriptEditor extends React.Component {
                 ))}
               </select>
             </label>
-            <VisibleAndPilotExperiment
-              visible={!this.props.hidden}
-              pilotExperiment={this.props.pilotExperiment}
-            />
             <label>
               Can be recommended (aka stable)
               <input
@@ -246,38 +323,22 @@ export default class ScriptEditor extends React.Component {
                 defaultChecked={this.props.isStable}
                 style={styles.checkbox}
               />
-              <p>
-                If checked, this unit will be eligible to be the recommended
-                version of the unit. The most recent eligible version will be
-                the recommended version.
-              </p>
+              <HelpTip>
+                <p>
+                  If checked, this unit will be eligible to be the recommended
+                  version of the unit. The most recent eligible version will be
+                  the recommended version.
+                </p>
+              </HelpTip>
             </label>
+            <VisibleAndPilotExperiment
+              visible={!this.props.hidden}
+              pilotExperiment={this.props.pilotExperiment}
+            />
           </div>
         )}
-        <label>
-          Display project sharing column in Teacher Dashboard
-          <input
-            name="project_sharing"
-            type="checkbox"
-            defaultChecked={this.props.projectSharing}
-            style={styles.checkbox}
-          />
-          <p>
-            If checked, the "Sharing" column in the "Manage Students" tab of
-            Teacher Dashboard will be displayed by default for sections assigned
-            to this script.
-          </p>
-        </label>
-        <label>
-          Login Required
-          <input
-            name="login_required"
-            type="checkbox"
-            defaultChecked={this.props.loginRequired}
-            style={styles.checkbox}
-          />
-          <p>Require users to log in before viewing this script.</p>
-        </label>
+
+        <h2>Lesson Settings</h2>
         <label>
           Hideable Lessons
           <input
@@ -286,24 +347,12 @@ export default class ScriptEditor extends React.Component {
             defaultChecked={this.props.hideableLessons}
             style={styles.checkbox}
           />
-          <p>
-            Allow teachers to toggle whether or not specific lessons in this
-            script are visible to students in their section.
-          </p>
-        </label>
-        <label>
-          Default Progress to Detail View
-          <input
-            name="student_detail_progress_view"
-            type="checkbox"
-            defaultChecked={this.props.studentDetailProgressView}
-            style={styles.checkbox}
-          />
-          <p>
-            By default students start in the summary view. When this box is
-            checked, we instead stick everyone into detail view to start for
-            this script.
-          </p>
+          <HelpTip>
+            <p>
+              Allow teachers to toggle whether or not specific lessons in this
+              script are visible to students in their section.
+            </p>
+          </HelpTip>
         </label>
         <label>
           Lesson Extras Available
@@ -313,97 +362,13 @@ export default class ScriptEditor extends React.Component {
             defaultChecked={this.props.lessonExtrasAvailable}
             style={styles.checkbox}
           />
-          <p>
-            If also enabled by the teacher, show the lesson extras page at the
-            end of each lesson.
-          </p>
+          <HelpTip>
+            <p>
+              If also enabled by the teacher, show the lesson extras page at the
+              end of each lesson.
+            </p>
+          </HelpTip>
         </label>
-        <label>
-          Verified Resources
-          <input
-            name="has_verified_resources"
-            type="checkbox"
-            defaultChecked={this.props.hasVerifiedResources}
-            style={styles.checkbox}
-          />
-          <p>
-            Check if this course has resources for verified teachers, and we
-            want to notify non-verified teachers that this is the case.
-          </p>
-        </label>
-        <label>
-          Lesson Plan
-          <input
-            name="has_lesson_plan"
-            type="checkbox"
-            defaultChecked={this.props.hasLessonPlan}
-            style={styles.checkbox}
-          />
-          <p>
-            Check if this course has lesson plans (on Curriculum Builder or in
-            PDF form) that we should provide links to.
-          </p>
-        </label>
-        <label>
-          Text-to-Speech
-          <input
-            name="tts"
-            type="checkbox"
-            defaultChecked={this.props.tts}
-            style={styles.checkbox}
-          />
-          <p>Check to enable text-to-speech for this course.</p>
-        </label>
-        {this.props.isLevelbuilder && (
-          <label>
-            Editor Experiment. If specified, users with this experiment on the
-            levelbuilder machine will be able to edit this script.
-            <input
-              name="editor_experiment"
-              defaultValue={this.props.editorExperiment}
-              style={styles.input}
-            />
-          </label>
-        )}
-        <label>
-          Curriculum Path
-          <input
-            name="curriculum_path"
-            defaultValue={this.props.curriculumPath}
-            style={styles.input}
-          />
-        </label>
-        {this.props.isLevelbuilder && (
-          <label>
-            Professional Learning Course. When filled out, the course unit
-            associated with this script will be associated with the course named
-            in this box. If the course unit does not exist, and if the course
-            does not exist it will be created.
-            <input
-              name="professional_learning_course"
-              defaultValue={this.props.professionalLearningCourse}
-              style={styles.input}
-            />
-          </label>
-        )}
-        <label>
-          Peer Reviews to Complete. Currently only supported for professional
-          learning courses
-          <input
-            name="peer_reviews_to_complete"
-            defaultValue={this.props.peerReviewsRequired}
-            style={styles.input}
-          />
-        </label>
-        <label>
-          Wrap-up Video
-          <input
-            name="wrapup_video"
-            defaultValue={this.props.wrapupVideo}
-            style={styles.input}
-          />
-        </label>
-
         <h3>Project widget options</h3>
         <label>
           Project widget visible
@@ -413,10 +378,12 @@ export default class ScriptEditor extends React.Component {
             defaultChecked={this.props.projectWidgetVisible}
             style={styles.checkbox}
           />
-          <p>
-            If checked this script will have the projects widget (recent
-            projects and new project buttons) visible in lesson extras.
-          </p>
+          <HelpTip>
+            <p>
+              If checked this script will have the projects widget (recent
+              projects and new project buttons) visible in lesson extras.
+            </p>
+          </HelpTip>
         </label>
         <label>
           Project widget new project types
@@ -456,35 +423,54 @@ export default class ScriptEditor extends React.Component {
             <option value="gumball">Gumball</option>
           </select>
         </label>
-
         <label>
-          Supported locales
-          <p>
-            Select additional locales supported by this script. Select
-            <a onClick={this.handleClearSupportedLocalesSelectClick}> none </a>
-            or shift-click or cmd-click to select multiple.
-          </p>
-          <select
-            name="supported_locales[]"
-            multiple
-            defaultValue={this.props.supportedLocales}
-            ref={select => (this.supportedLocaleSelect = select)}
-          >
-            {this.props.locales
-              .filter(locale => !locale[1].startsWith('en'))
-              .map(locale => (
-                <option key={locale[1]} value={locale[1]}>
-                  {locale[1]}
-                </option>
-              ))}
-          </select>
+          Lesson Plan
+          <input
+            name="has_lesson_plan"
+            type="checkbox"
+            defaultChecked={this.props.hasLessonPlan}
+            style={styles.checkbox}
+          />
+          <HelpTip>
+            <p>
+              Check if this script has lesson plans (on Curriculum Builder or in
+              PDF form) that we should provide links to.
+            </p>
+          </HelpTip>
         </label>
-
+        <label>
+          Curriculum Path
+          <input
+            name="curriculum_path"
+            defaultValue={this.props.curriculumPath}
+            style={styles.input}
+          />
+        </label>
+        <LessonDescriptions
+          scriptName={this.props.name}
+          currentDescriptions={this.props.i18nData.stageDescriptions}
+        />
+        <h2>Teacher Resources Settings</h2>
+        <label>
+          Verified Resources
+          <input
+            name="has_verified_resources"
+            type="checkbox"
+            defaultChecked={this.props.hasVerifiedResources}
+            style={styles.checkbox}
+          />
+          <HelpTip>
+            <p>
+              Check if this script has resources for verified teachers, and we
+              want to notify non-verified teachers that this is the case.
+            </p>
+          </HelpTip>
+        </label>
         <div>
           <h4>Teacher Resources</h4>
           <div>
             Select the Teacher Resources buttons you'd like to have show up on
-            the top of the course overview page
+            the top of the script overview page
           </div>
           <ResourcesEditor
             inputStyle={styles.input}
@@ -504,6 +490,35 @@ export default class ScriptEditor extends React.Component {
             )}
           />
         </div>
+        <h2>Professional Learning Settings</h2>
+        {this.props.isLevelbuilder && (
+          <label>
+            Professional Learning Course.
+            <HelpTip>
+              <p>
+                When filled out, the course unit associated with this script
+                will be associated with the course named in this box. If the
+                course unit does not exist, and if the course does not exist it
+                will be created.
+              </p>
+            </HelpTip>
+            <input
+              name="professional_learning_course"
+              defaultValue={this.props.professionalLearningCourse}
+              style={styles.input}
+            />
+          </label>
+        )}
+        <label>
+          Peer Reviews to Complete. Currently only supported for professional
+          learning courses
+          <input
+            name="peer_reviews_to_complete"
+            defaultValue={this.props.peerReviewsRequired}
+            style={styles.input}
+          />
+        </label>
+
         <h2>Lessons and Levels</h2>
         {this.props.beta ? (
           <FlexGroup />
