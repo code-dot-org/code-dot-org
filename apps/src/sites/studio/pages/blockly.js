b05cc8832ea1f7238b72d53396de73f9ab8c5168
Shadi Naif <shadinaif@gmail.com>
2020-05-20 18:43:20 +0300

    RTL hack-fix - Bad position for Jigsaw Blocks

--- apps/src/sites/studio/pages/blockly.js
+++ apps/src/sites/studio/pages/blockly.js
@@ -1,2 +1,38 @@
 import Blockly from '@code-dot-org/blockly';
+
+const original_Blockly_BlockSvg_prototype_renderDraw_ =
+  Blockly.BlockSvg.prototype.renderDraw_;
+
+Blockly.BlockSvg.prototype.renderDraw_ = function(iconWidth, inputRows) {
+  const ret = original_Blockly_BlockSvg_prototype_renderDraw_.apply(
+    this,
+    arguments
+  );
+
+  // Fix a problem in jigsaw_ blocks
+  if (Blockly.RTL && this.block_.type.startsWith('jigsaw_')) {
+    const path_blocks = this.getRootElement().getElementsByClassName(
+      'blocklyPath'
+    );
+    for (const path_block of path_blocks) {
+      path_block.setAttribute('transform', 'scale(-1 1)');
+    }
+
+    const rect_blocks = this.getRootElement().parentElement.querySelectorAll(
+      'rect'
+    );
+    for (const rect_block of rect_blocks) {
+      var current_transform = rect_block.getAttribute('transform');
+      if (!current_transform.includes(' scale(-1 1)')) {
+        rect_block.setAttribute(
+          'transform',
+          current_transform + ' scale(-1 1)'
+        );
+      }
+    }
+  }
+
+  return ret;
+};
+
 window.Blockly = Blockly;
