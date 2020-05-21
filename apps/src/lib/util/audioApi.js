234cac9f05b1390b3583d2b74ac40e2e25a0c2ad
JillianK <jillian9012@gmail.com>
2020-05-21 14:30:40 -0700

    addressed comments

--- apps/src/lib/util/audioApi.js
+++ apps/src/lib/util/audioApi.js
@@ -156,7 +156,7 @@ export const commands = {
         }
         Sounds.getSingleton().playBytes(result.audioData, {
           volume: 1.0,
-          loop: !!false,
+          loop: false,
           forceHTML5: forceHTML5,
           allowHTML5Mobile: true
         });
@@ -179,7 +179,7 @@ export const executors = {
   playSound: (url, loop = false, callback) =>
     executeCmd(null, 'playSound', {url, loop, callback}),
   stopSound: url => executeCmd(null, 'stopSound', {url}),
-  playSpeech: (text, gender, language = 'en-US') =>
+  playSpeech: (text, gender, language = 'English') =>
     executeCmd(null, 'playSpeech', {text, gender, language})
 };
 // Note to self - can we use _.zipObject to map argumentNames to arguments here?
