8dc5cef1af4d43c8d257656cc4ba09eb1dd4f0e9
Anjali <anjali@code.org>
2020-06-11 12:42:03 -0700

    add comment

--- tools/scripts/updateAnimationAliases.rb
+++ tools/scripts/updateAnimationAliases.rb
@@ -11,6 +11,8 @@ include CdoCli
 # 1. Download the Sprite Lab animation library from S3 and put it in this directory
 # 2. Make any desired changes to the aliases in cdo/apps/srce/p5lab/spritelab/spriteCostumeLibrary
 # 3. Run ./updateAnimationAliases
+# Options:
+#   -s or --spritelab : Update aliases for Sprite Lab (default is Game Lab)
 # 4. Upload the new animation metadata files to S3
 #
 
