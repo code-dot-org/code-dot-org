91519d18e1fd19516eebd47554b67983b0084dae
JillianK <jillian9012@gmail.com>
2020-06-12 10:25:15 -0700

    imported all of color

--- apps/src/p5lab/gamelab/GameLab.js
+++ apps/src/p5lab/gamelab/GameLab.js
@@ -1,7 +1,7 @@
 import P5Lab from '../P5Lab';
 import project from '@cdo/apps/code-studio/initApp/project';
 import {showLevelBuilderSaveButton} from '../../code-studio/header';
-import {workspace_running_background, white} from '@cdo/apps/util/color';
+import color from '@cdo/apps/util/color';
 
 var GameLab = function() {
   P5Lab.call(this);
@@ -26,9 +26,9 @@ GameLab.prototype.init = function(config) {
 
 GameLab.prototype.resetHandler = function(ignore) {
   if (!this.studioApp_.config.readonlyWorkspace) {
-    $('.droplet-main-canvas').css('background-color', white);
-    $('.droplet-transition-container').css('background-color', white);
-    $('.ace_scroller').css('background-color', white);
+    $('.droplet-main-canvas').css('background-color', color.white);
+    $('.droplet-transition-container').css('background-color', color.white);
+    $('.ace_scroller').css('background-color', color.white);
   }
   P5Lab.prototype.resetHandler.call(this, ignore);
 };
@@ -37,13 +37,16 @@ GameLab.prototype.runButtonClick = function() {
   if (!this.studioApp_.config.readonlyWorkspace) {
     $('.droplet-main-canvas').css(
       'background-color',
-      workspace_running_background
+      color.workspace_running_background
     );
     $('.droplet-transition-container').css(
       'background-color',
-      workspace_running_background
+      color.workspace_running_background
     );
-    $('.ace_scroller').css('background-color', workspace_running_background);
+    $('.ace_scroller').css(
+      'background-color',
+      color.workspace_running_background
+    );
   }
   P5Lab.prototype.runButtonClick.call(this);
 };
