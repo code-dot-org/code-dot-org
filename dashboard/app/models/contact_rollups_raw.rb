42f9c438f81dd2d77e91e3843f35f0059e58c263
Ha Nguyen <ha@code.org>
2020-06-15 16:56:34 -0700

    Simplify method to generate extraction query

--- dashboard/app/models/contact_rollups_raw.rb
+++ dashboard/app/models/contact_rollups_raw.rb
@@ -283,31 +283,23 @@ class ContactRollupsRaw < ApplicationRecord
     SQL
   end
 
-  # @param source [String] Source from which we want to extract data (can be a dashboard table name, or subquery)
-  # @param email_column [String] Column in source table we want to insert ino the email column
-  # @param data_columns [Array] Columns we want reshaped into a single JSON object
-  # @param source_is_subquery [Boolean] (default false) Set to true if source is a subquery, rather than a table name
-  # @param source_name [String] (default nil) Name for source (non-nil if using a subquery)
+  # @param source_name [String] The source table contains data we want to extract
+  # @param select_query [String] Query to select data from the source table
+  # @param data_columns [String] Columns we want reshaped into a single JSON object
   # @return [String] A SQL statement to extract and reshape data from the source table.
-  def self.get_extraction_query(source, email_column, data_columns, source_is_subquery=false, source_name=nil)
-    if source_name.nil? && source_is_subquery
-      raise 'Source name required if source is a subquery'
-    end
-
-    source_name ||= "dashboard.#{source}"
-    wrapped_source = source_is_subquery ? "(#{source}) AS subquery" : source
-
+  def self.get_extraction_query(source_name, select_query, *data_columns)
     <<~SQL
-      INSERT INTO #{ContactRollupsRaw.table_name} (email, sources, data, data_updated_at, created_at, updated_at)
+      INSERT INTO #{ContactRollupsRaw.table_name}
+        (email, sources, data, data_updated_at, created_at, updated_at)
       SELECT
-        #{email_column},
+        email,
         '#{source_name}' AS sources,
         #{create_json_object(data_columns)} AS data,
         updated_at AS data_updated_at,
         NOW() AS created_at,
         NOW() AS updated_at
-      FROM #{wrapped_source}
-      WHERE #{email_column} IS NOT NULL AND #{email_column} != ''
+      FROM (#{select_query}) AS subquery
+      WHERE email > ''
     SQL
   end
 
