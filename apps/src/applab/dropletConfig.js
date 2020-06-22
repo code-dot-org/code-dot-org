39b635aa6b3dd5ff4ff7abfc27f7b589d1f25efc
Jillian Khoo <jillian9012@gmail.com>
2020-06-22 21:57:36 +0000

    put behind experiment

--- apps/src/applab/dropletConfig.js
+++ apps/src/applab/dropletConfig.js
@@ -18,6 +18,7 @@ import {
 } from './setPropertyDropdown';
 import {getStore} from '../redux';
 import * as applabConstants from './constants';
+import experiments from '../util/experiments';
 
 var DEFAULT_WIDTH = applabConstants.APP_WIDTH.toString();
 var DEFAULT_HEIGHT = (
@@ -242,7 +243,6 @@ export var blocks = [
   },
   {...audioApiDropletConfig.playSound, category: 'UI controls'},
   {...audioApiDropletConfig.stopSound, category: 'UI controls'},
-  {...audioApiDropletConfig.playSpeech, category: 'UI controls'},
   {
     func: 'showElement',
     parent: api,
@@ -1130,6 +1130,10 @@ export var blocks = [
     noAutocomplete: true
   }
 ];
+
+if (experiments.isEnabled(experiments.TEXT_TO_SPEECH_BLOCK)) {
+  blocks.push({...audioApiDropletConfig.playSpeech, category: 'UI controls'});
+}
 
 export const categories = {
   'UI controls': {
