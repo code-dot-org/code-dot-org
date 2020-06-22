3ee14316e9f0b9717231926875b16c4e7a419ca1
Jessica Kulwik <jessica@code.org>
2020-06-22 23:34:39 +0000

    Updated droplet error handling to communicate details to the user wherever possible.

--- apps/lib/droplet/droplet-full.js
+++ apps/lib/droplet/droplet-full.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2020 Anthony Bau.
  * MIT License.
  *
- * Date: 2020-06-18
+ * Date: 2020-06-22
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.droplet = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
@@ -15180,18 +15180,18 @@ exports.Parser = Parser = (function() {
           switch (mark.token.type) {
             case 'indentStart':
               if ((stack != null ? (ref7 = stack[stack.length - 1]) != null ? ref7.type : void 0 : void 0) !== 'block') {
-                throw new Error('Line ' + mark.location.line + '. Improper parser: indent must be inside block, but is inside ' + (stack != null ? (ref8 = stack[stack.length - 1]) != null ? ref8.type : void 0 : void 0));
+                throw new Error("Line " + mark.location.line + ". Improper parser: indent must be inside block, but is inside " + (stack != null ? (ref8 = stack[stack.length - 1]) != null ? ref8.type : void 0 : void 0));
               }
               indentDepth += mark.token.container.prefix.length;
               break;
             case 'blockStart':
               if (((ref9 = stack[stack.length - 1]) != null ? ref9.type : void 0) === 'block') {
-                throw new Error('Line ' + mark.location.line + '. Improper parser: block cannot nest immediately inside another block.');
+                throw new Error("Line " + mark.location.line + ". Improper parser: block cannot nest immediately inside another block.");
               }
               break;
             case 'socketStart':
               if (((ref10 = stack[stack.length - 1]) != null ? ref10.type : void 0) !== 'block') {
-                throw new Error('Line ' + mark.location.line + '. Improper parser: socket must be immediately inside a block.');
+                throw new Error("Line " + mark.location.line + ". Improper parser: socket must be immediately inside a block.");
               }
               break;
             case 'indentEnd':
@@ -15201,7 +15201,7 @@ exports.Parser = Parser = (function() {
             stack.push(mark.token.container);
           } else if (mark.token instanceof model.EndToken) {
             if (mark.token.container !== stack[stack.length - 1]) {
-              throw new Error("Improper parser: " + head.container.type + " ended too early.");
+              throw new Error("Line " + mark.location.line + ". Improper parser: " + head.container.type + " ended too early.");
             }
             stack.pop();
           }
--- apps/src/util/dropletCommon.js
+++ apps/src/util/dropletCommon.js
@@ -1,21 +1,62 @@
+/*globals dashboard*/
 import i18n from '@cdo/locale';
+import trackEvent from '@cdo/apps/util/trackEvent';
 
+/**
+ * Runs the user's droplet code through the droplet parser. If droplet is unable
+ * to parse the user's code, call the error callback with any information we
+ * have regarding the error. Additionally, this will return `true` if any
+ * errors are detected and `false` otherwise.
+ * @param {object} dropletEditor The droplet engine
+ * @param {function} errorCallback The function to call if an error is detected.
+ *                   Usage: errorCallback(lineNumber, errorMessage)
+ * @returns {boolean} true if an error was detected. false otherwise.
+ */
 export const findDropletParseErrors = (dropletEditor, errorCallback) => {
   if (!dropletEditor) {
-    return;
+    return false;
   }
 
   try {
     dropletEditor.parse();
   } catch (error) {
-    // error.message = Line ###. Error Message
+    // Example error message: 'Line ###. Error Message'
     let matchedLineNumber = error.message.match(/Line (\d+)./);
     if (matchedLineNumber) {
+      // We were able to find the line number. Report it to the user.
+      let errorMessage = error.message.match(/Line \d+. (.*)/)[1];
+      if (error.message.includes('indent must be inside block')) {
+        errorMessage = i18n.droplet_parsing_error();
+      } else {
+        // It's unknown what javascript text causes certain droplet errors. If
+        // we discover an unknown error, log it to Google Analytics so we can
+        // create a user-friendly error message.
+        trackEvent(
+          'Research',
+          'UnknownDropletError',
+          `DropletError:${
+            error.message
+          }--ShareURL:${dashboard.project.getShareUrl()}`
+        );
+      }
+      errorCallback(Number(matchedLineNumber[1]) + 1, errorMessage);
+      return true;
+    } else {
+      // We couldn't find the line number. Display the error to the student and
+      // log this in Google Analytics so we can update droplet to return the
+      // line number with this error.
+      trackEvent(
+        'Research',
+        'CouldNotFindDropletParseErrorLineNumber',
+        'DropletError:' + error.message
+      );
       errorCallback(
-        Number(matchedLineNumber[1]) + 1,
-        i18n.droplet_parsing_error()
+        0 /* lineNumber */,
+        i18n.droplet_parsing_error() + ' ' + error.message
       );
       return true;
     }
   }
+
+  return false;
 };
