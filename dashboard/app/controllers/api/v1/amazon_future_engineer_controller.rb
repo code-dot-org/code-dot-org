110db36d571dbd1e778fe1b3a663d292dbf2310b
Brad Buchanan <brad@code.org>
2020-06-24 10:56:23 -0700

    submit returns BAD REQUEST when params are malformed

--- dashboard/app/controllers/api/v1/amazon_future_engineer_controller.rb
+++ dashboard/app/controllers/api/v1/amazon_future_engineer_controller.rb
@@ -8,6 +8,52 @@ class Api::V1::AmazonFutureEngineerController < ApplicationController
   def submit
     return head :forbidden unless current_user
 
-    post_request('fake_pardot_url.com', params[:amazon_future_engineer])
+    post_request('fake_pardot_url.com', submit_params)
+  end
+
+  private
+
+  def submit_params
+    params.
+      require(:amazon_future_engineer).
+      permit(
+        'traffic-source',
+        'first-name',
+        'last-name',
+        'email',
+        'nces-id',
+        'street-1',
+        'street-2',
+        'street-3',
+        'city',
+        'state',
+        'zip',
+        'inspirational-marketing-kit',
+        'csta-plus',
+        'aws-educate',
+        'amazon-terms',
+        'new-code-account',
+        'registration-date-time',
+      ).tap do |submit_params|
+      submit_params.require(
+        %w(
+          traffic-source
+          first-name
+          last-name
+          email
+          nces-id
+          street-1
+          city
+          state
+          zip
+          inspirational-marketing-kit
+          csta-plus
+          aws-educate
+          amazon-terms
+          new-code-account
+          registration-date-time
+        )
+      )
+    end
   end
 end
