567658f586b4d366e8ae495fa5a304e7b58953ff
Brad Buchanan <brad@code.org>
2020-06-24 13:49:11 -0700

    Accept params in the client format

--- dashboard/test/controllers/api/v1/amazon_future_engineer_controller_test.rb
+++ dashboard/test/controllers/api/v1/amazon_future_engineer_controller_test.rb
@@ -9,10 +9,7 @@ class Api::V1::AmazonFutureEngineerControllerTest < ActionDispatch::IntegrationT
     Net::HTTP.expects(:post_form).never
 
     post '/dashboardapi/v1/amazon_future_engineer_submit',
-      params: {
-        'first-name' => 'test',
-        'last-name' => 'test'
-      }, as: :json
+      params: valid_params, as: :json
 
     assert_response :forbidden
   end
@@ -38,34 +35,60 @@ class Api::V1::AmazonFutureEngineerControllerTest < ActionDispatch::IntegrationT
   end
 
   test 'logged in can submit' do
-    Net::HTTP.expects(:post_form).returns(FakeResponse.new)
+    Timecop.freeze do
+      # Establish an expectation that we post to Pardot with the
+      # correct parameters
+      expected_call = Net::HTTP.expects(:post_form).with do |url, params|
+        url.to_s == CDO.afe_pardot_form_handler_url &&
+          params.to_h == {
+            'traffic-source' => 'AFE-code.org',
+            'first-name' => 'test',
+            'last-name' => 'test',
+            'email' => 'test@code.org',
+            'nces-id' => '123456789012',
+            'street-1' => 'test street',
+            'street-2' => 'test street 2',
+            'city' => 'seattle',
+            'state' => 'wa',
+            'zip' => '98105',
+            'inspirational-marketing-kit' => '0',
+            'csta-plus' => '0',
+            'aws-educate' => '0',
+            'amazon-terms' => '1',
+            'new-code-account' => '0',
+            'registration-date-time' => Time.now.iso8601
+          }
+      end
+      expected_call.returns FakeResponse.new
 
-    sign_in create :teacher
-    post '/dashboardapi/v1/amazon_future_engineer_submit',
-      params: valid_params, as: :json
+      sign_in create :teacher
+      post '/dashboardapi/v1/amazon_future_engineer_submit',
+        params: valid_params, as: :json
 
-    assert_response :success, "Failed response: #{response.body}"
+      assert_response :success, "Failed response: #{response.body}"
+    end
   end
 
   private
 
   def valid_params
     {
-      'traffic-source' => 'AFE-code.org',
-      'first-name' => 'test',
-      'last-name' => 'test',
+      'trafficSource' => 'AFE-code.org',
+      'firstName' => 'test',
+      'lastName' => 'test',
       'email' => 'test@code.org',
-      'nces-id' => '123456789012',
-      'street-1' => 'test street',
+      'schoolId' => '123456789012',
+      'street1' => 'test street',
+      'street2' => 'test street 2',
       'city' => 'seattle',
       'state' => 'wa',
       'zip' => '98105',
-      'inspirational-marketing-kit' => '0',
-      'csta-plus' => '0',
-      'aws-educate' => '0',
-      'amazon-terms' => '1',
-      'new-code-account' => '0',
-      'registration-date-time' => '1997-07-16T19:20:30+01:00'
+      'inspirationKit' => '0',
+      'csta' => '0',
+      'consentCSTA' => '0',
+      'awsEducate' => '0',
+      'consentAFE' => '1',
+      'newCodeAccount' => '0'
     }
   end
 
