c0012132aa0161f12f80ff1852b14d888c812de2
Brad Buchanan <brad@code.org>
2020-06-24 10:42:12 -0700

    Test cleanup

--- dashboard/test/controllers/api/v1/amazon_future_engineer_controller_test.rb
+++ dashboard/test/controllers/api/v1/amazon_future_engineer_controller_test.rb
@@ -21,55 +21,44 @@ class Api::V1::AmazonFutureEngineerControllerTest < ActionDispatch::IntegrationT
     Net::HTTP.expects(:post_form).never
     sign_in create :teacher
 
+    # Intentionally missing required field traffic-source
     post '/dashboardapi/v1/amazon_future_engineer_submit',
-      params: {
-        # Intentionally missing required field traffic-source
-        # 'traffic-source' => 'AFE-code.org',
-        'first-name' => 'test',
-        'last-name' => 'test',
-        'email' => 'test@code.org',
-        'nces-id' => '123456789012',
-        'street-1' => 'test street',
-        'city' => 'seattle',
-        'state' => 'wa',
-        'zip' => '98105',
-        'inspirational-marketing-kit' => '0',
-        'csta-plus' => '0',
-        'aws-educate' => '0',
-        'amazon-terms' => '1',
-        'new-code-account' => '0',
-        'registration-date-time' => '1997-07-16T19:20:30+01:00'
-      }, as: :json
+      params: valid_params.delete('traffic-source'), as: :json
 
     assert_response :bad_request, "Expected BAD REQUEST, got: #{response.status}\n#{response.body}"
   end
 
   test 'logged in can submit' do
-    Net::HTTP.stubs(:post_form)
     Net::HTTP.expects(:post_form).returns(FakeResponse.new)
 
     sign_in(@teacher)
 
     post '/dashboardapi/v1/amazon_future_engineer_submit',
-      params: {
-        'traffic-source' => 'AFE-code.org',
-        'first-name' => 'test',
-        'last-name' => 'test',
-        'email' => 'test@code.org',
-        'nces-id' => '123456789012',
-        'street-1' => 'test street',
-        'city' => 'seattle',
-        'state' => 'wa',
-        'zip' => '98105',
-        'inspirational-marketing-kit' => '0',
-        'csta-plus' => '0',
-        'aws-educate' => '0',
-        'amazon-terms' => '1',
-        'new-code-account' => '0',
-        'registration-date-time' => '1997-07-16T19:20:30+01:00'
-      }, as: :json
+      params: valid_params, as: :json
 
     assert_response :success, "Failed response: #{response.body}"
+  end
+
+  private
+
+  def valid_params
+    {
+      'traffic-source' => 'AFE-code.org',
+      'first-name' => 'test',
+      'last-name' => 'test',
+      'email' => 'test@code.org',
+      'nces-id' => '123456789012',
+      'street-1' => 'test street',
+      'city' => 'seattle',
+      'state' => 'wa',
+      'zip' => '98105',
+      'inspirational-marketing-kit' => '0',
+      'csta-plus' => '0',
+      'aws-educate' => '0',
+      'amazon-terms' => '1',
+      'new-code-account' => '0',
+      'registration-date-time' => '1997-07-16T19:20:30+01:00'
+    }
   end
 
   class FakeResponse
