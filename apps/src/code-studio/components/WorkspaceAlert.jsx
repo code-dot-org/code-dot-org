145d58587c7562491828b8c1aee06f4274333667
Jillian Khoo <jillian9012@gmail.com>
2020-06-24 21:39:39 +0000

    cleaned up displayWorkspaceAlert and set up to handle more cases

--- apps/src/code-studio/components/WorkspaceAlert.jsx
+++ apps/src/code-studio/components/WorkspaceAlert.jsx
@@ -2,31 +2,48 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Alert from '@cdo/apps/templates/alert';
 
-const styles = {
-  container: {
-    position: 'absolute',
-    bottom: 0,
-    right: 0
-  }
-};
-
 export default class WorkspaceAlert extends React.Component {
   static propTypes = {
-    left: PropTypes.number,
+    type: PropTypes.oneOf(['error', 'warning', 'notification']).isRequired,
     children: PropTypes.element,
-    onClose: PropTypes.func
+    onClose: PropTypes.func,
+    isBlockly: PropTypes.bool,
+    isCraft: PropTypes.bool,
+    displayBottom: PropTypes.bool
   };
 
   render() {
+    const {displayBottom} = this.props;
+    var toolbarWidth;
+    if (this.props.isBlockly && this.props.isCraft) {
+      // craft has a slightly different way of constructing the toolbox so we need to use
+      // the toolbox header's width to get the width of the actual toolbox.
+      toolbarWidth = $('#toolbox-header').width();
+    } else if (this.props.isBlockly) {
+      toolbarWidth = $('.blocklyToolboxDiv').width();
+    } else {
+      toolbarWidth =
+        $('.droplet-palette-element').width() + $('.droplet-gutter').width();
+    }
+
+    let style = {
+      position: 'absolute',
+      right: 0,
+      left: toolbarWidth
+    };
+    if (displayBottom) {
+      style.bottom = 0;
+    } else {
+      style.top = $('#headers').height();
+    }
+
     return (
-      <div style={{...styles.container, ...{left: this.props.left}}}>
+      <div style={style}>
         <Alert
           onClose={this.props.onClose}
-          type={'warning'}
-          sideMargin={0}
-          bottomMargin={0}
-          closeDelayMillis={0}
-          childPadding={'8px 14px'}
+          type={this.props.type}
+          sideMargin={displayBottom ? 0 : undefined}
+          bottomMargin={displayBottom ? 0 : undefined}
         >
           {this.props.children}
         </Alert>
@@ -35,6 +52,4 @@ export default class WorkspaceAlert extends React.Component {
   }
 }
 
-// TODO: Update styles so everything displays correctly
-// TODO: Add logic to handle craft & blockly
 // TODO: Unit tests
