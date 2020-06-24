6d23435ef3af0d2435aa40a0925879e133a2fb9c
Brad Buchanan <brad@code.org>
2020-06-24 11:02:04 -0700

    Code cleanup

--- dashboard/app/controllers/api/v1/amazon_future_engineer_controller.rb
+++ dashboard/app/controllers/api/v1/amazon_future_engineer_controller.rb
@@ -7,53 +7,38 @@ class Api::V1::AmazonFutureEngineerController < ApplicationController
 
   def submit
     return head :forbidden unless current_user
-
     post_request('fake_pardot_url.com', submit_params)
   end
 
   private
 
+  REQUIRED_PARAMETERS = %w(
+    traffic-source
+    first-name
+    last-name
+    email
+    nces-id
+    street-1
+    city
+    state
+    zip
+    inspirational-marketing-kit
+    csta-plus
+    aws-educate
+    amazon-terms
+    new-code-account
+    registration-date-time
+  )
+
+  PERMITTED_PARAMETERS = [
+    *REQUIRED_PARAMETERS,
+    'street-2',
+    'street-3'
+  ]
+
   def submit_params
-    params.
-      require(:amazon_future_engineer).
-      permit(
-        'traffic-source',
-        'first-name',
-        'last-name',
-        'email',
-        'nces-id',
-        'street-1',
-        'street-2',
-        'street-3',
-        'city',
-        'state',
-        'zip',
-        'inspirational-marketing-kit',
-        'csta-plus',
-        'aws-educate',
-        'amazon-terms',
-        'new-code-account',
-        'registration-date-time',
-      ).tap do |submit_params|
-      submit_params.require(
-        %w(
-          traffic-source
-          first-name
-          last-name
-          email
-          nces-id
-          street-1
-          city
-          state
-          zip
-          inspirational-marketing-kit
-          csta-plus
-          aws-educate
-          amazon-terms
-          new-code-account
-          registration-date-time
-        )
-      )
-    end
+    params.require(:amazon_future_engineer).
+           permit(*PERMITTED_PARAMETERS).
+           tap {|p| p.require(REQUIRED_PARAMETERS)}
   end
 end
