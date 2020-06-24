60747172e73c60202d9d80155a17a153ae54e0db
Brad Buchanan <brad@code.org>
2020-06-24 13:49:11 -0700

    Accept params in the client format

--- dashboard/app/controllers/api/v1/amazon_future_engineer_controller.rb
+++ dashboard/app/controllers/api/v1/amazon_future_engineer_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::AmazonFutureEngineerController < ApplicationController
   def submit
     return head :forbidden unless current_user
     if CDO.afe_pardot_form_handler_url
-      post_request CDO.afe_pardot_form_handler_url, submit_params
+      post_request CDO.afe_pardot_form_handler_url, afe_params
     else
       # Success, with a message:
       <<~RESPONSE
@@ -24,32 +24,53 @@ class Api::V1::AmazonFutureEngineerController < ApplicationController
   private
 
   REQUIRED_PARAMETERS = %w(
-    traffic-source
-    first-name
-    last-name
+    trafficSource
+    firstName
+    lastName
     email
-    nces-id
-    street-1
+    schoolId
+    street1
     city
     state
     zip
-    inspirational-marketing-kit
-    csta-plus
-    aws-educate
-    amazon-terms
-    new-code-account
-    registration-date-time
+    inspirationKit
+    csta
+    awsEducate
+    consentAFE
+    newCodeAccount
   )
 
   PERMITTED_PARAMETERS = [
     *REQUIRED_PARAMETERS,
-    'street-2',
-    'street-3'
+    'street2',
+    'street3'
   ]
 
   def submit_params
     params.require(:amazon_future_engineer).
            permit(*PERMITTED_PARAMETERS).
            tap {|p| p.require(REQUIRED_PARAMETERS)}
+  end
+
+  def afe_params
+    filtered_params = submit_params
+    {
+      'traffic-source' => filtered_params['trafficSource'],
+      'first-name' => filtered_params['firstName'],
+      'last-name' => filtered_params['lastName'],
+      'email' => filtered_params['email'],
+      'nces-id' => filtered_params['schoolId'],
+      'street-1' => filtered_params['street1'],
+      'street-2' => filtered_params['street2'],
+      'city' => filtered_params['city'],
+      'state' => filtered_params['state'],
+      'zip' => filtered_params['zip'],
+      'inspirational-marketing-kit' => filtered_params['inspirationKit'],
+      'csta-plus' => filtered_params['csta'],
+      'aws-educate' => filtered_params['awsEducate'],
+      'amazon-terms' => filtered_params['consentAFE'],
+      'new-code-account' => filtered_params['newCodeAccount'],
+      'registration-date-time' => Time.now.iso8601
+    }
   end
 end
