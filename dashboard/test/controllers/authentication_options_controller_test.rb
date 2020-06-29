113677764caf5f691c3539499595811f09f06926
Elijah Hamovitz <elijahhamovitz@gmail.com>
2020-06-29 16:20:08 -0700

    update unit tests for new linking functionality

--- dashboard/test/controllers/authentication_options_controller_test.rb
+++ dashboard/test/controllers/authentication_options_controller_test.rb
@@ -1,76 +1,10 @@
 require 'test_helper'
 
 class AuthenticationOptionsControllerTest < ActionDispatch::IntegrationTest
-  test 'connect: returns bad_request if user not signed in' do
-    get '/users/auth/google_oauth2/connect'
-    assert_response :bad_request
-  end
-
-  test 'connect: returns bad_request if provider not supported' do
-    user = create :user
-    sign_in user
-    get '/users/auth/some_fake_provider/connect'
-    assert_response :bad_request
-  end
-
-  test 'connect: sets connect_provider on session and redirects to google authorization' do
-    user = create :user
-    sign_in user
-
-    Timecop.freeze do
-      get '/users/auth/google_oauth2/connect'
-      assert_equal 2.minutes.from_now, session[:connect_provider]
-      assert_redirected_to '/users/auth/google_oauth2'
-    end
-  end
-
-  test 'connect: sets connect_provider on session and redirects to facebook authorization' do
-    user = create :user
-    sign_in user
-
-    Timecop.freeze do
-      get '/users/auth/facebook/connect'
-      assert_equal 2.minutes.from_now, session[:connect_provider]
-      assert_redirected_to '/users/auth/facebook'
-    end
-  end
-
-  test 'connect: sets connect_provider on session and redirects to windowslive authorization' do
-    user = create :user
-    sign_in user
-
-    Timecop.freeze do
-      get '/users/auth/windowslive/connect'
-      assert_equal 2.minutes.from_now, session[:connect_provider]
-      assert_redirected_to '/users/auth/windowslive'
-    end
-  end
-
-  test 'connect: sets connect_provider on session and redirects to clever authorization' do
-    user = create :user
-    sign_in user
-
-    Timecop.freeze do
-      get '/users/auth/clever/connect'
-      assert_equal 2.minutes.from_now, session[:connect_provider]
-      assert_redirected_to '/users/auth/clever'
-    end
-  end
-
-  test 'connect: sets connect_provider on session and redirects to powerschool authorization' do
-    user = create :user
-    sign_in user
-
-    Timecop.freeze do
-      get '/users/auth/powerschool/connect'
-      assert_equal 2.minutes.from_now, session[:connect_provider]
-      assert_redirected_to '/users/auth/powerschool'
-    end
-  end
-
-  test 'disconnect: returns bad_request if user is not signed in' do
-    delete '/users/auth/1/disconnect'
-    assert_response :bad_request
+  test 'disconnect: redirects with an error message if user is not signed in' do
+    post '/users/auth/1/disconnect'
+    assert flash[:alert].include? 'Sorry'
+    assert_response :redirect
   end
 
   test 'disconnect: destroys the AuthenticationOption if it exists and is not primary' do
@@ -79,8 +13,9 @@ class AuthenticationOptionsControllerTest < ActionDispatch::IntegrationTest
     sign_in user
 
     assert_destroys(AuthenticationOption) do
-      delete "/users/auth/#{auth_option.id}/disconnect"
-      assert_response :success
+      post "/users/auth/#{auth_option.id}/disconnect"
+      assert flash[:notice].include? "Success!"
+      assert_response :redirect
       assert_raises ActiveRecord::RecordNotFound do
         AuthenticationOption.find(auth_option.id)
       end
@@ -102,8 +37,9 @@ class AuthenticationOptionsControllerTest < ActionDispatch::IntegrationTest
     assert_equal google_option, teacher.primary_contact_info
 
     sign_in teacher
-    delete "/users/auth/#{teacher.primary_contact_info_id}/disconnect"
-    assert_response :success
+    post "/users/auth/#{teacher.primary_contact_info_id}/disconnect"
+    assert flash[:notice].include? "Success!"
+    assert_response :redirect
 
     teacher.reload
     assert_equal 2, teacher.authentication_options.size
@@ -126,8 +62,9 @@ class AuthenticationOptionsControllerTest < ActionDispatch::IntegrationTest
     refute_empty teacher.encrypted_password
 
     sign_in teacher
-    delete "/users/auth/#{teacher.primary_contact_info_id}/disconnect"
-    assert_response :success
+    post "/users/auth/#{teacher.primary_contact_info_id}/disconnect"
+    assert flash[:notice].include? "Success!"
+    assert_response :redirect
 
     teacher.reload
     assert_equal 2, teacher.authentication_options.size
@@ -145,7 +82,7 @@ class AuthenticationOptionsControllerTest < ActionDispatch::IntegrationTest
     sign_in user
 
     assert_does_not_destroy(AuthenticationOption) do
-      delete "/users/auth/1/disconnect"
+      post "/users/auth/1/disconnect"
       assert_response :not_found
     end
   end
@@ -156,7 +93,7 @@ class AuthenticationOptionsControllerTest < ActionDispatch::IntegrationTest
     sign_in user
 
     assert_does_not_destroy(AuthenticationOption) do
-      delete "/users/auth/#{auth_option.id}/disconnect"
+      post "/users/auth/#{auth_option.id}/disconnect"
       assert_response :not_found
     end
   end
