763807d32b8ba76c0142959d80fbea9885a90cf5
Jillian Khoo <jillian9012@gmail.com>
2020-07-08 20:26:28 +0000

    added nil check for profanity endpoint

--- dashboard/app/controllers/profanity_controller.rb
+++ dashboard/app/controllers/profanity_controller.rb
@@ -8,13 +8,15 @@ class ProfanityController < ApplicationController
   # @returns [Array<String>|nil] Profane words within the given string
   def find
     profanity_result = []
-    if params[:should_cache]
-      cache_key = "profanity/#{params[:language] || request.locale}/#{params[:text] || ''}"
-      profanity_result = Rails.cache.fetch(cache_key) do
-        ProfanityFilter.find_potential_profanities(params[:text] || "", params[:language] || request.locale)
+    unless params[:text].nil?
+      if params[:should_cache]
+        cache_key = "profanity/#{params[:language] || request.locale}/#{params[:text] || ''}"
+        profanity_result = Rails.cache.fetch(cache_key) do
+          ProfanityFilter.find_potential_profanities(params[:text], params[:language] || request.locale)
+        end
+      else
+        profanity_result = ProfanityFilter.find_potential_profanities(params[:text], params[:language] || request.locale)
       end
-    else
-      profanity_result = ProfanityFilter.find_potential_profanities(params[:text] || "", params[:language] || request.locale)
     end
     render json: profanity_result
   end
