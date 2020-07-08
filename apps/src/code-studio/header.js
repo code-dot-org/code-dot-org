6220f14bfb4e137b56dd4bc9de05086768e693c5
Brendan Reville <brendan@code.org>
2020-07-08 13:46:37 -0700

    Header: Avoid JSX in JS file

--- apps/src/code-studio/header.js
+++ apps/src/code-studio/header.js
@@ -95,19 +95,22 @@ header.build = function(
   // to potentially begin before we first render HeaderMiddle, giving HeaderMiddle
   // the opportunity to wait until the app is loaded before rendering.
   $(document).ready(function() {
-    ReactDOM.render(
-      <Provider store={getStore()}>
-        <HeaderMiddle
-          scriptNameData={scriptNameData}
-          lessonData={lessonData}
-          scriptData={scriptData}
-          currentLevelId={currentLevelId}
-          linesOfCodeText={linesOfCodeText}
-          hasAppOptions={hasAppOptions}
-        />
-      </Provider>,
-      document.querySelector('.header_level')
+    const headerMiddle = React.createElement(
+      Provider,
+      {
+        store: getStore()
+      },
+      React.createElement(HeaderMiddle, {
+        scriptNameData: scriptNameData,
+        lessonData: lessonData,
+        scriptData: scriptData,
+        currentLevelId: currentLevelId,
+        linesOfCodeText: linesOfCodeText,
+        hasAppOptions: hasAppOptions
+      })
     );
+
+    ReactDOM.render(headerMiddle, document.querySelector('.header_level'));
   });
 };
 
