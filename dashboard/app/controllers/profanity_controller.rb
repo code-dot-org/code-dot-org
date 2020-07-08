19480cdc00e72d775fd7cbbd51da22a9e2b89c6b
Jillian Khoo <jillian9012@gmail.com>
2020-07-08 18:02:25 +0000

    added caching to profanity filter

--- dashboard/app/controllers/profanity_controller.rb
+++ dashboard/app/controllers/profanity_controller.rb
@@ -4,8 +4,18 @@ class ProfanityController < ApplicationController
   # POST /profanity/find
   # @param [String] params[:text] String to test
   # @param [String] params[:language] Language of the text to test
+  # @param [Boolean] params[:should_cache] Whether or not we should use the cache on this request
   # @returns [Array<String>|nil] Profane words within the given string
   def find
-    render json: ProfanityFilter.find_potential_profanities(params[:text] || "", params[:language] || request.locale)
+    profanity_result = []
+    if params[:should_cache]
+      cache_key = "profanity/#{params[:language] || request.locale}/#{params[:text] || ''}"
+      profanity_result = Rails.cache.fetch(cache_key) do
+        ProfanityFilter.find_potential_profanities(params[:text] || "", params[:language] || request.locale)
+      end
+    else
+      profanity_result = ProfanityFilter.find_potential_profanities(params[:text] || "", params[:language] || request.locale)
+    end
+    render json: profanity_result
   end
 end
