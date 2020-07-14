5fc42f420bbe510292fdd110c1ee302323075915
Ben Brooks <ben@code.org>
2020-07-14 16:57:47 -0700

    Proper click handler to dismiss interstitial

--- apps/src/sites/studio/pages/layouts/_thank_donors_interstitial.js
+++ apps/src/sites/studio/pages/layouts/_thank_donors_interstitial.js
@@ -1,51 +1,15 @@
 import $ from 'jquery';
 
 $(document).ready(function() {
-  $('#user_terms_of_service_version').on('change', function(event) {
-    if ($(this).is(':checked')) {
-      $('#accept-terms-submit')
-        .prop('disabled', false)
-        .removeClass('disabled-button');
-    } else {
-      $('#accept-terms-submit')
-        .prop('disabled', true)
-        .addClass('disabled-button');
-    }
-  });
-
-  $('#edit_user').submit(function(event) {
-    event.preventDefault();
-    $.ajax({
-      type: 'POST',
-      url: $(this).attr('action'),
-      data: $(this).serialize(),
-      dataType: 'json',
-      complete: function(data) {
-        $('#terms-modal').modal('hide');
-        location.reload();
-      }
-    });
-  });
-
-  $('#later-link').click(function() {
+  $('#dismiss-thank-donors').click(function() {
     $('#terms-modal').modal('hide');
   });
 });
 
-$(document).ready(function() {
-  var printLink = $('#print-terms');
-  if (printLink) {
-    printLink.click(function() {
-      var item = $('#print-frame')[0];
-      item.contentWindow.print();
-    });
-  }
-});
-
 function setCookie(key, value) {
   var expires = new Date();
-  // Kill hide_tos cookie at midnight every night
-  // so the terms interstitial pops up once a day.
+  // Kill thank donors cookie at midnight every night
+  // so the thank donors interstitial pops up once a day.
   expires.setHours(23, 59, 59, 0);
   document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
 }
@@ -56,9 +20,9 @@ function getCookie(key) {
 }
 
 $(document).ready(function() {
-  var already_shown = !!getCookie('hide_tos');
+  var already_shown = !!getCookie('hide_thank_donors');
   if (!already_shown && screen.width > 970) {
     $('#terms-modal').modal('show');
-    setCookie('hide_tos', '1');
+    setCookie('hide_thank_donors', '1');
   }
 });
