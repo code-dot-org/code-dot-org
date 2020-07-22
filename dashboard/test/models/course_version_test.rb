cbdb3d9ca92e295f5983242800d023d24ad4ae20
Winter Dong <winter@code.org>
2020-07-21 23:52:27 -0700

    Add factory for CourseVersion and unit test on association

---
+++ dashboard/test/models/course_version_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CourseVersionTest < ActiveSupport::TestCase
+  test "course version associations" do
+    course_version = create :course_version
+    assert_instance_of UnitGroup, course_version.content_root
+    assert_equal course_version, course_version.content_root.course_version
+  end
+end
