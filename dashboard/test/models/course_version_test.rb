4fc2ed938d92df2f1b8b92af55ccf4a481a5cd9c
Winter Dong <winter@code.org>
2020-07-22 10:20:04 -0700

    Add course_version association to Script [skip ui]

--- dashboard/test/models/course_version_test.rb
+++ dashboard/test/models/course_version_test.rb
@@ -5,5 +5,9 @@ class CourseVersionTest < ActiveSupport::TestCase
     course_version = create :course_version
     assert_instance_of UnitGroup, course_version.content_root
     assert_equal course_version, course_version.content_root.course_version
+
+    course_version = create :course_version, :with_unit
+    assert_instance_of Script, course_version.content_root
+    assert_equal course_version, course_version.content_root.course_version
   end
 end
