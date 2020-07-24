6018a1510c441452900dbf0f1e0fadf88e383d24
Winter Dong <winter@code.org>
2020-07-23 23:50:25 -0700

    Add unit tests on update_course_version

--- dashboard/app/models/course_version.rb
+++ dashboard/app/models/course_version.rb
@@ -20,18 +20,25 @@ class CourseVersion < ApplicationRecord
   belongs_to :content_root, polymorphic: true
 
   def self.update_course_version(content_root)
-    # TODO: should we delete the existing CourseVersion and Course if is_course is removed from a script?
-    return nil unless content_root.is_course?
-
     # TODO: Once the Course model is added, change this to just be version_year, since then the
     # unique index will be on (course_id, key).
     key = "#{content_root.family_name}-#{content_root.version_year}"
+
+    unless content_root.is_course?
+      CourseVersion.find_by(key: key)&.destroy
+      content_root.course_version = nil
+      content_root.save
+      return nil
+    end
 
     course_version = CourseVersion.find_or_create_by(
       key: key,
       display_name: content_root.version_year,
       content_root: content_root,
     )
+
+    content_root.course_version.destroy if course_version != content_root.course_version
+    content_root.course_version = course_version
 
     # TODO: add relevant properties from content root to course_version
 
--- dashboard/test/models/course_version_test.rb
+++ dashboard/test/models/course_version_test.rb
@@ -10,4 +10,58 @@ class CourseVersionTest < ActiveSupport::TestCase
     assert_instance_of Script, course_version.content_root
     assert_equal course_version, course_version.content_root.course_version
   end
+
+  test "update_course_version creates CourseVersion for script that doesn't have one if is_course is true" do
+    # Case: "is_course true" added to a .script file
+    script = create :script, family_name: 'csz', version_year: '2050', is_course: true
+    course_version = CourseVersion.update_course_version(script)
+
+    assert_equal course_version, script.course_version
+    assert_equal course_version, CourseVersion.find_by(key: 'csz-2050')
+    assert_equal 'csz-2050', course_version.key
+    assert_equal '2050', course_version.display_name
+  end
+
+  test "update_course_version updates existing CourseVersion for script if properties change" do
+    script = create :script, family_name: 'csz', version_year: '2050', is_course: true
+    CourseVersion.update_course_version(script)
+
+    assert_equal 'csz-2050', script.course_version.key
+    assert_equal '2050', script.course_version.display_name
+
+    script.family_name = 'csx'
+    script.version_year = '2060'
+    script.save
+
+    CourseVersion.update_course_version(script)
+
+    assert_equal 'csx-2060', script.course_version.key
+    assert_equal '2060', script.course_version.display_name
+    assert_equal script.course_version, CourseVersion.find_by(key: 'csx-2060')
+    assert_nil CourseVersion.find_by(key: 'csz-2050') # old CourseVersion should be deleted
+  end
+
+  test "update_course_version deletes CourseVersion for script if is_course is changed to false" do
+    script = create :script, family_name: 'csz', version_year: '2050', is_course: true
+    CourseVersion.update_course_version(script)
+
+    assert_not_nil script.course_version
+
+    script.is_course = false
+    script.save
+    course_version = CourseVersion.update_course_version(script)
+
+    assert_nil course_version
+    assert_nil script.course_version
+    assert_nil CourseVersion.find_by(key: 'csz-2050')
+  end
+
+  test "update_course_version does nothing for script without CourseVersion if is_course is false" do
+    script = create :script, family_name: 'csz', version_year: '2050'
+    course_version = CourseVersion.update_course_version(script)
+
+    assert_nil course_version
+    assert_nil script.course_version
+    assert_nil CourseVersion.find_by(key: 'csz-2050')
+  end
 end
