0fcc79c48aeb893a91f8dbedbf6d5918c75f897b
Brad Buchanan <brad@code.org>
2020-07-24 16:19:09 -0700

    PartialRegistration only stores a cache key in the session
    This design goes a step beyond the previous design, by storing _all_ of
    the partial user's attributes in the application cache (serialized as
    JSON) and only storing a single cache key in the user's session to
    represent a partial registration in progress.

--- dashboard/app/models/concerns/partial_registration.rb
+++ dashboard/app/models/concerns/partial_registration.rb
@@ -7,125 +7,53 @@ require 'cdo/shared_cache'
 module PartialRegistration
   extend ActiveSupport::Concern
 
-  USER_ATTRIBUTES_SESSION_KEY = 'devise.user_attributes'
-
-  # A common error during registration, especially when OAuth is involved, is a
-  # Cookie Overflow.  This module stores attributes for a partially-created user
-  # in the _learn_session cookie, which cannot exceed 4KB.  In fact, it we seem
-  # to encounter issues when the cookie is more than 2KB, since Devise and other
-  # parts of our application may also be using the session.
-  # To work around this, we keep known large values out of the session and
-  # instead store them in the shared application cache.
-  ATTRIBUTES_TO_KEEP_IN_CACHE = %w(
-    school
-    full_address
-  )
-  # also these large values from the user serialized properties blob
-  PROPERTIES_TO_KEEP_IN_CACHE = %w(
-    oauth_token
-    oauth_refresh_token
-  )
-  # We also drop some user model attributes that we know we don't need to preserve
-  # if we haven't created a user yet.
-  ATTRIBUTES_TO_DROP = %w(
-    id
-    studio_person_id
-    reset_password_token
-    reset_password_sent_at
-    remember_created_at
-    sign_in_count
-    current_sign_in_at
-    last_sign_in_at
-    current_sign_in_ip
-    last_sign_in_ip
-    created_at
-    updated_at
-    username
-    admin
-    total_lines
-    secret_picture_id
-    secret_words
-    active
-    deleted_at
-    purged_at
-    invitation_token
-    invitation_created_at
-    invitation_sent_at
-    invitation_accepted_at
-    invitation_limit
-    invited_by_id
-    invited_by_type
-    invitations_count
-  )
+  SESSION_KEY = 'partial_registration'
 
   module ClassMethods
-    def new_from_partial_registration(session)
+    def new_from_partial_registration(session, &block)
       raise 'No partial registration was in progress' unless PartialRegistration.in_progress? session
-      new(session[USER_ATTRIBUTES_SESSION_KEY]) do |user|
-        cache = CDO.shared_cache
-        [*ATTRIBUTES_TO_KEEP_IN_CACHE, *PROPERTIES_TO_KEEP_IN_CACHE].each do |param|
-          next if user.send(param)
-          # Grab the value from memcached if it's there
-          cache_key = PartialRegistration.cache_key(param, user)
-          user.send("#{param}=", cache.read(cache_key)) if cache
-        end
-        yield user if block_given?
-      end
+      cache_key = session[SESSION_KEY]
+      json = CDO.shared_cache.read(cache_key)
+      attributes = JSON.parse(json)
+      new(attributes, &block)
     end
   end
 
   def self.in_progress?(session)
-    !!session[USER_ATTRIBUTES_SESSION_KEY]
+    session[SESSION_KEY] && CDO.shared_cache.exist?(session[SESSION_KEY])
   end
 
   def self.persist_attributes(session, user)
-    user = user.dup
-    user_attributes = user.attributes.to_h
+    # Push the potential user's attributes into our application cache.
+    cache_key = PartialRegistration.cache_key(user)
+    CDO.shared_cache.write(cache_key, user.attributes.compact.to_json)
 
-    # Because some user attributes (especially oauth tokens) are quite large, we pass
-    # them through via the application cache instead of the session cookie.
-    # They are pulled out again in new_from_partial_registration
-    cache = CDO.shared_cache
-    if cache
-      ATTRIBUTES_TO_KEEP_IN_CACHE.each do |attr_name|
-        attr_value = user_attributes.delete(attr_name)
-        cache_key = PartialRegistration.cache_key(attr_name, user)
-        cache.write(cache_key, attr_value)
-      end
-
-      PROPERTIES_TO_KEEP_IN_CACHE.each do |param|
-        param_value = user_attributes['properties'].delete(param)
-        cache_key = PartialRegistration.cache_key(param, user)
-        cache.write(cache_key, param_value)
-      end
-    end
-
-    # Because we want to keep the session cookie as small as possible, we also drop these
-    # user attributes that we don't need to preserve from a partial registration
-    ATTRIBUTES_TO_DROP.each do |attr_name|
-      user_attributes.delete(attr_name)
-    end
-
-    session[USER_ATTRIBUTES_SESSION_KEY] = user_attributes
-  end
-
-  def self.get_provider(session)
-    in_progress?(session) ? session[USER_ATTRIBUTES_SESSION_KEY]['provider'] : nil
+    # Put the cache key into the session, to
+    # 1. track that a partial registration is in progress
+    # 2. retrieve the attributes later when we're ready to persist the user
+    session[SESSION_KEY] = cache_key
   end
 
   def self.cancel(session)
     provider = get_provider(session) || 'email'
     SignUpTracking.log_cancel_finish_sign_up(session, provider)
     SignUpTracking.end_sign_up_tracking(session)
-    session.delete(USER_ATTRIBUTES_SESSION_KEY)
-    session
+    session.delete(SESSION_KEY)
   end
 
-  def self.cache_key(param_name, user)
+  def self.get_provider(session)
+    # Extract the provider name from the cache key to avoid actually
+    # interacting with the cache or doing deserialization.
+    # Assumption: Provider names will not contain hyphens
+    cache_key = session[SESSION_KEY]
+    /^([^-]+)-.+-partial-sso$/.match(cache_key)&.[](1)
+  end
+
+  def self.cache_key(user)
     if user.uid.present?
-      "#{user.provider}-#{user.uid}-#{param_name}"
+      "#{user.provider}-#{user.uid}-partial-sso"
     else
-      "#{User.hash_email(user.email)}-#{param_name}"
+      "#{User.hash_email(user.email)}-partial-email"
     end
   end
 end
