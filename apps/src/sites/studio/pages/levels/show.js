3cd0ddb4eedd3fe2e5e4343f8e599c7fb9c4d9e6
indhu-gunda <indhu.gunda@utexas.edu>
2020-07-25 21:31:14 -0500

    Works for students and teacher dashboard

--- apps/src/sites/studio/pages/levels/show.js
+++ apps/src/sites/studio/pages/levels/show.js
@@ -3,19 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {getStore, registerReducers} from '@cdo/apps/redux';
 import ScriptLevelRedirectDialog from '@cdo/apps/code-studio/components/ScriptLevelRedirectDialog';
-import sectionData, {setSection} from '@cdo/apps/redux/sectionDataRedux';
+import sectionData, {
+  setAutoplayEnabled
+} from '@cdo/apps/redux/sectionDataRedux';
 
 $(document).ready(initPage);
 
 function initPage() {
   const script = document.querySelector('script[data-level]');
   const config = JSON.parse(script.dataset.level);
-  const section = config.section;
+  const autoplayEnabled = config.autoplay_enabled;
   registerReducers({sectionData});
   const store = getStore();
-  if (section) {
-    store.dispatch(setSection(section));
-  }
+  store.dispatch(setAutoplayEnabled(autoplayEnabled));
   const redirectDialogMountPoint = document.getElementById('redirect-dialog');
   if (redirectDialogMountPoint && config.redirect_script_url) {
     ReactDOM.render(
