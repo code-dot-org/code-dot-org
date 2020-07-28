bc75757a28f5938e620ed8fc2367123f8e21338e
Ben Brooks <ben@code.org>
2020-07-28 13:28:52 -0700

    Add logging when a user sees the thank donors dialog

--- apps/src/sites/studio/pages/layouts/_thank_donors_interstitial.js
+++ apps/src/sites/studio/pages/layouts/_thank_donors_interstitial.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
 import cookies from 'js-cookie';
+import firehoseClient from '@cdo/apps/lib/util/firehose';
 
 $(document).ready(function() {
   var already_shown = !!cookies.get('has_seen_thank_donors');
   if (!already_shown && window.innerWidth > 720) {
     $('#thank-donors-modal').modal('show');
     cookies.set('has_seen_thank_donors', '1');
+
+    firehoseClient.putRecord(
+      {
+        study: 'thank-donors-interstitial',
+        event: 'saw-thank-donors-interstitial'
+      },
+      {includeUserId: true}
+    );
   }
 
   $('#dismiss-thank-donors').click(function() {
