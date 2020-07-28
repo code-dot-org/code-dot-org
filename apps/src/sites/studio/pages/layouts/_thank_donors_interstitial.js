ffa16c12fcb38452f95b435cbc0bab0c6b87a86c
Ben Brooks <ben@code.org>
2020-07-28 10:40:58 -0700

    Use js-cookie and combine document.ready

--- apps/src/sites/studio/pages/layouts/_thank_donors_interstitial.js
+++ apps/src/sites/studio/pages/layouts/_thank_donors_interstitial.js
@@ -1,24 +1,14 @@
 import $ from 'jquery';
+import cookies from 'js-cookie';
 
 $(document).ready(function() {
+  var already_shown = !!cookies.get('has_seen_thank_donors');
+  if (!already_shown && window.innerWidth > 720) {
+    $('#thank-donors-modal').modal('show');
+    cookies.set('has_seen_thank_donors', '1');
+  }
+
   $('#dismiss-thank-donors').click(function() {
     $('#thank-donors-modal').modal('hide');
   });
-});
-
-function setCookie(key, value) {
-  document.cookie = key + '=' + value;
-}
-
-function getCookie(key) {
-  var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-  return keyValue ? keyValue[2] : null;
-}
-
-$(document).ready(function() {
-  var already_shown = !!getCookie('hide_thank_donors');
-  if (!already_shown && window.innerWidth > 720) {
-    $('#thank-donors-modal').modal('show');
-    setCookie('hide_thank_donors', '1');
-  }
 });
