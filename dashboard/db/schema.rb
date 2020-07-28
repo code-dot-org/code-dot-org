27b8d852c2a61c1797070abee5df2888a88373e6
Dani McAvoy <danimcavoy@gmail.com>
2020-07-28 16:17:21 -0400

    Add migration to update key values to make name values

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200728012027) do
+ActiveRecord::Schema.define(version: 20200728201407) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
