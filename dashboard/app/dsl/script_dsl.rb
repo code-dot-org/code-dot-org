9428366a629db8e7180fd13ef9e43f29ee14b72b
Dani McAvoy <danimcavoy@gmail.com>
2020-07-28 07:26:34 -0400

    Update the seeding and creation methods with key

--- dashboard/app/dsl/script_dsl.rb
+++ dashboard/app/dsl/script_dsl.rb
@@ -84,9 +84,9 @@ class ScriptDSL < BaseDSL
     end
   end
 
-  def lesson(name, properties = {})
+  def lesson(key, properties = {})
     # For scripts that don't use lesson groups create a blank non-user facing lesson group
-    if name
+    if key
       if @lesson_groups.empty?
         @lesson_groups << {
           key: nil,
@@ -96,7 +96,8 @@ class ScriptDSL < BaseDSL
       end
 
       @lesson_groups.last[:lessons] << {
-        name: name,
+        key: key,
+        name: properties[:display_name],
         lockable: properties[:lockable],
         visible_after: determine_visible_after_time(properties[:visible_after]),
         script_levels: []
@@ -274,7 +275,7 @@ class ScriptDSL < BaseDSL
         i18n_lesson_group_strings[lesson_group[:key]] = {'display_name' => lesson_group[:display_name]}
       end
       lesson_group[:lessons].each do |lesson|
-        i18n_lesson_strings[lesson[:name]] = {'name' => lesson[:name]}
+        i18n_lesson_strings[lesson[:key]] = {'name' => lesson[:name]}
       end
     end
 
@@ -356,7 +357,8 @@ class ScriptDSL < BaseDSL
   def self.serialize_lesson(lesson)
     s = []
 
-    t = "lesson '#{escape(lesson.name)}'"
+    t = "lesson '#{escape(lesson.key)}'"
+    t += ", display_name: '#{escape(lesson.name)}'" if lesson.name
     t += ', lockable: true' if lesson.lockable
     t += ", visible_after: '#{escape(lesson.visible_after)}'" if lesson.visible_after
     s << t
