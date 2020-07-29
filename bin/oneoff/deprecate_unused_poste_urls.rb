15d160dd45b3e7f96f1d6a6242f8bfdcfc976517
molly-moen <molly@code.org>
2020-07-29 16:09:41 -0700

    script to deprecate most urls

--- bin/oneoff/deprecate_unused_poste_urls.rb
+++ bin/oneoff/deprecate_unused_poste_urls.rb
@@ -1,6 +1,38 @@
-ActiveRecord::Base.transaction do
-  # mark all poste_urls that haven't been clicked in the last 2 months as deleted_at = now
+#!/usr/bin/env ruby
 
-  # This script is a dry-run unless we comment out this last line
-  raise ActiveRecord::Rollback.new, "Intentional rollback"
+require 'sequel'
+require File.expand_path('../../../pegasus/src/env', __FILE__)
+require src_dir 'database'
+require_relative './poste_urls_constants'
+
+# This script is a dry-run unless we change DRY_RUN to false
+DRY_RUN = true
+
+if DRY_RUN
+  puts "DRY RUN..."
+else
+  puts "Starting update..."
+end
+
+DB.transaction do
+  # mark all poste_urls that are not in urls_to_keep as deleted_at = now
+  # this will take a long time to run as it iterates over the entire poste_urls table
+  urls_kept = []
+  deleted_at_time = DateTime.now
+  DB[:poste_urls].each do |poste_url|
+    if !POSTE_URLS_TO_KEEP.include?(poste_url[:id])
+      poste_url.update(deleted_at: deleted_at_time)
+    else
+      urls_kept << poste_url[:url]
+    end
+  end
+
+  if DRY_RUN
+    puts "Dry Run complete! Would have kept #{urls_kept.count} urls:"
+    puts urls_kept
+    puts "Rolling back..."
+    raise Sequel::Rollback
+  else
+    puts "Update complete! Kept #{urls_kept.count}"
+  end
 end
