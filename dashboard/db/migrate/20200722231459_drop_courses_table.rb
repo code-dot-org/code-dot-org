c89d8decd61f98218f9cb9c58eca8dca523f2ed9
David Bailey <davidsbailey@users.noreply.github.com>
2020-07-29 13:41:25 -0700

    do not add foreign keys to unit_groups table

--- dashboard/db/migrate/20200722231459_drop_courses_table.rb
+++ dashboard/db/migrate/20200722231459_drop_courses_table.rb
@@ -1,12 +1,8 @@
 class DropCoursesTable < ActiveRecord::Migration[5.0]
   def up
-    # As a final check, add foreign key constraints against the new table before
-    # dropping the old table. In environments such as production and
-    # levelbuilder where many sections are assigned to courses like csp and csd,
-    # this step will fail if the new table is empty or has different ids than
-    # the old table.
-    add_foreign_key :sections, :unit_groups, column: :course_id
-    add_foreign_key :plc_courses, :unit_groups, column: :course_id
+    # Do not add back foreign key constraints against the new table, because
+    # this step timed out when run against an adhoc with a production db clone,
+    # and the foreign keys aren't strictly necessary anyway.
 
     drop_table :courses
   end
