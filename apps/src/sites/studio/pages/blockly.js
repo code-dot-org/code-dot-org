2d531e5e3c8cbeb0bff23a1e7d6104f552af8804
Anjali <anjali@code.org>
2020-07-30 15:11:30 -0700

    bare bones wrapper

--- apps/src/sites/studio/pages/blockly.js
+++ apps/src/sites/studio/pages/blockly.js
@@ -1,2 +1,21 @@
 import Blockly from '@code-dot-org/blockly';
-window.Blockly = Blockly;
+
+const BlocklyWrapper = function(blocklyInstance) {
+  this.blockly_ = blocklyInstance;
+  this.Msg = blocklyInstance.Msg;
+  this.inject = this.blockly_.inject;
+  this.wrapProperty = function(propertyName) {
+    Object.defineProperty(this, propertyName, {
+      get: function() {
+        return this.blockly_[propertyName];
+      }
+    });
+  };
+};
+
+function initializeBlocklyWrapper() {
+  const blocklyWrapper = new BlocklyWrapper(Blockly);
+  return blocklyWrapper;
+}
+
+window.Blockly = initializeBlocklyWrapper();
