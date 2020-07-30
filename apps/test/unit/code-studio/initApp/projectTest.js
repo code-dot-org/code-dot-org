bebd05e6ff89225e458307a53e2758bdd0804aac
Erin Peach <erin@code.org>
2020-07-30 12:02:33 -0700

    Update tests from toggle to setMaker

--- apps/test/unit/code-studio/initApp/projectTest.js
+++ apps/test/unit/code-studio/initApp/projectTest.js
@@ -646,7 +646,7 @@ describe('project.js', () => {
     });
   });
 
-  describe('toggleMakerEnabled()', () => {
+  describe('setMakerEnabled()', () => {
     beforeEach(() => {
       sinon
         .stub(project, 'saveSourceAndHtml_')
@@ -659,30 +659,31 @@ describe('project.js', () => {
       project.saveSourceAndHtml_.restore();
     });
 
-    it('performs a save with maker enabled if it was disabled', () => {
+    it('performs a save with maker set to circuitPlayground enabled if it was disabled', () => {
       sourceHandler.getMakerAPIsEnabled.returns(false);
       project.init(sourceHandler);
-      return project.toggleMakerEnabled().then(() => {
+      return project.setMakerEnabled('circuitPlayground').then(() => {
         expect(project.saveSourceAndHtml_).to.have.been.called;
-        expect(project.saveSourceAndHtml_.getCall(0).args[0].makerAPIsEnabled)
-          .to.be.true;
+        expect(
+          project.saveSourceAndHtml_.getCall(0).args[0].makerAPIsEnabled
+        ).to.equal('circuitPlayground');
       });
     });
 
     it('performs a save with maker disabled if it was enabled', () => {
       sourceHandler.getMakerAPIsEnabled.returns(true);
       project.init(sourceHandler);
-      return project.toggleMakerEnabled().then(() => {
+      return project.setMakerEnabled(null).then(() => {
         expect(project.saveSourceAndHtml_).to.have.been.called;
         expect(project.saveSourceAndHtml_.getCall(0).args[0].makerAPIsEnabled)
-          .to.be.false;
+          .to.be.null;
       });
     });
 
     it('always results in a page reload', () => {
       project.init(sourceHandler);
       expect(utils.reload).not.to.have.been.called;
-      return project.toggleMakerEnabled().then(() => {
+      return project.setMakerEnabled(null).then(() => {
         expect(utils.reload).to.have.been.called;
       });
     });
