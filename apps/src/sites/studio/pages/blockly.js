6bb8c1cc71fd4ee2c09782c896c27734fa80590b
Anjali <anjali@code.org>
2020-07-30 15:13:27 -0700

    add properties to wrapper (1)

--- apps/src/sites/studio/pages/blockly.js
+++ apps/src/sites/studio/pages/blockly.js
@@ -15,6 +15,41 @@ const BlocklyWrapper = function(blocklyInstance) {
 
 function initializeBlocklyWrapper() {
   const blocklyWrapper = new BlocklyWrapper(Blockly);
+
+  blocklyWrapper.wrapProperty('bindEvent_');
+  blocklyWrapper.wrapProperty('Block');
+  blocklyWrapper.wrapProperty('Blocks');
+  blocklyWrapper.wrapProperty('BlockSpace');
+  blocklyWrapper.wrapProperty('BlockSvg');
+  blocklyWrapper.wrapProperty('BlockValueType');
+  blocklyWrapper.wrapProperty('FieldAngleDropdown');
+  blocklyWrapper.wrapProperty('FieldAngleInput');
+  blocklyWrapper.wrapProperty('FieldButton');
+  blocklyWrapper.wrapProperty('FieldColour');
+  blocklyWrapper.wrapProperty('FieldColourDropdown');
+  blocklyWrapper.wrapProperty('FieldDropdown');
+  blocklyWrapper.wrapProperty('FieldIcon');
+  blocklyWrapper.wrapProperty('FieldImage');
+  blocklyWrapper.wrapProperty('FieldImageDropdown');
+  blocklyWrapper.wrapProperty('FieldLabel');
+  blocklyWrapper.wrapProperty('FieldParameter');
+  blocklyWrapper.wrapProperty('FieldRectangularDropdown');
+  blocklyWrapper.wrapProperty('FieldTextInput');
+  blocklyWrapper.wrapProperty('FieldVariable');
+  blocklyWrapper.wrapProperty('Flyout');
+  blocklyWrapper.wrapProperty('FunctionalBlockUtils');
+  blocklyWrapper.wrapProperty('FunctionalTypeColors');
+  blocklyWrapper.wrapProperty('FunctionEditor');
+  blocklyWrapper.wrapProperty('Generator');
+  blocklyWrapper.wrapProperty('JavaScript');
+  blocklyWrapper.wrapProperty('mainBlockSpace');
+  blocklyWrapper.wrapProperty('mainBlockSpaceEditor');
+  blocklyWrapper.wrapProperty('Names');
+  blocklyWrapper.wrapProperty('Procedures');
+  blocklyWrapper.wrapProperty('SVG_NS');
+  blocklyWrapper.wrapProperty('Variables');
+  blocklyWrapper.wrapProperty('Xml');
+
   return blocklyWrapper;
 }
 
