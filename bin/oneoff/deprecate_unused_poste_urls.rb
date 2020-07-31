7890e1083a4c7a317ae042ccd8d838516ff171bb
molly-moen <molly@code.org>
2020-07-31 10:39:23 -0700

    remove dry run and transaction

--- bin/oneoff/deprecate_unused_poste_urls.rb
+++ bin/oneoff/deprecate_unused_poste_urls.rb
@@ -5,39 +5,23 @@ require File.expand_path('../../../pegasus/src/env', __FILE__)
 require src_dir 'database'
 require_relative './poste_urls_constants'
 
-# This script is a dry-run unless we change DRY_RUN to false
-DRY_RUN = true
+puts "Starting update..."
+# mark all poste_urls that are not in urls_to_keep as deleted_at = now
+# this will take a long time to run as it iterates over the entire poste_urls table
+urls_kept_count = 0
+deleted_at_time = DateTime.now
 
-if DRY_RUN
-  puts "DRY RUN..."
-else
-  puts "Starting update..."
-end
-
-DB.transaction do
-  # mark all poste_urls that are not in urls_to_keep as deleted_at = now
-  # this will take a long time to run as it iterates over the entire poste_urls table
-  urls_kept = []
-  deleted_at_time = DateTime.now
-  DB[:poste_urls].each do |poste_url|
-    if !POSTE_URLS_TO_KEEP.include?(poste_url[:id])
-      begin
-        DB[:poste_urls].where(id: poste_url[:id]).update(deleted_at: deleted_at_time)
-      rescue => e
-        # continue on exception
-        puts "Hit exception #{e.message} deprecating url with id #{poste_url[:id]}"
-      end
-    else
-      urls_kept << poste_url[:url]
+DB[:poste_urls].each do |poste_url|
+  if !POSTE_URLS_TO_KEEP.include?(poste_url[:id])
+    begin
+      DB[:poste_urls].where(id: poste_url[:id]).update(deleted_at: deleted_at_time)
+    rescue => e
+      # Continue on exception. We can go back and fix errors later, we don't need to update everything in one shot.
+      puts "Hit exception #{e.message} deprecating url with id #{poste_url[:id]}"
     end
-  end
-
-  if DRY_RUN
-    puts "Dry Run complete! Would have kept #{urls_kept.count} urls:"
-    puts urls_kept
-    puts "Rolling back..."
-    raise Sequel::Rollback
   else
-    puts "Update complete! Kept #{urls_kept.count} urls"
+    urls_kept_count += 1
   end
 end
+
+puts "Update complete! Did not deprecate #{urls_kept_count} urls."
