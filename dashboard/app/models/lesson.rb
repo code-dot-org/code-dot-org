9fbbd60c5cef9703ebb9472b1c2554fdbbce95f1
Dani McAvoy <danimcavoy@gmail.com>
2020-07-31 13:28:35 -0400

    Update i18n checks to use key to look up things instead of name

--- dashboard/app/models/lesson.rb
+++ dashboard/app/models/lesson.rb
@@ -197,8 +197,8 @@ class Lesson < ActiveRecord::Base
         lesson_group_display_name: lesson_group&.localized_display_name,
         lockable: !!lockable,
         levels: cached_levels.map {|l| l.summarize(false)},
-        description_student: render_codespan_only_markdown(I18n.t("data.script.name.#{script.name}.lessons.#{name}.description_student", default: '')),
-        description_teacher: render_codespan_only_markdown(I18n.t("data.script.name.#{script.name}.lessons.#{name}.description_teacher", default: '')),
+        description_student: render_codespan_only_markdown(I18n.t("data.script.name.#{script.name}.lessons.#{key}.description_student", default: '')),
+        description_teacher: render_codespan_only_markdown(I18n.t("data.script.name.#{script.name}.lessons.#{key}.description_teacher", default: '')),
         unplugged: display_as_unplugged
       }
 
@@ -238,7 +238,7 @@ class Lesson < ActiveRecord::Base
   def summarize_for_edit
     summary = summarize.dup
     # Do not let script name override lesson name when there is only one lesson
-    summary[:name] = I18n.t("data.script.name.#{script.name}.lessons.#{name}.name")
+    summary[:name] = I18n.t("data.script.name.#{script.name}.lessons.#{key}.name")
     summary.freeze
   end
 
