f07b83294d435c4ae7c0195b35f1a91351db15d7
molly-moen <molly@code.org>
2020-07-31 11:01:11 -0700

    change order of operations

--- bin/oneoff/deprecate_unused_poste_urls.rb
+++ bin/oneoff/deprecate_unused_poste_urls.rb
@@ -5,23 +5,14 @@ require File.expand_path('../../../pegasus/src/env', __FILE__)
 require src_dir 'database'
 require_relative './poste_urls_constants'
 
-puts "Starting update..."
 # mark all poste_urls that are not in urls_to_keep as deleted_at = now
-# this will take a long time to run as it iterates over the entire poste_urls table
-urls_kept_count = 0
-deleted_at_time = DateTime.now
+# this will take a long time to run as it updates the entire poste_urls table
+puts "Starting update..."
 
-DB[:poste_urls].each do |poste_url|
-  if !POSTE_URLS_TO_KEEP.include?(poste_url[:id])
-    begin
-      DB[:poste_urls].where(id: poste_url[:id]).update(deleted_at: deleted_at_time)
-    rescue => e
-      # Continue on exception. We can go back and fix errors later, we don't need to update everything in one shot.
-      puts "Hit exception #{e.message} deprecating url with id #{poste_url[:id]}"
-    end
-  else
-    urls_kept_count += 1
-  end
-end
+DB[:poste_urls].update(deleted_at: DateTime.now)
 
-puts "Update complete! Did not deprecate #{urls_kept_count} urls."
+puts "Marked all urls as deleted..."
+
+DB[:poste_urls].where(id: POSTE_URLS_TO_KEEP).update(deleted_at: nil)
+
+puts "Un-deleted specific urls. Update complete!"
