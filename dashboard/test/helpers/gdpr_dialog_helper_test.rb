b11a93722b25151e94007e979d0250b03ed61334
Ben Brooks <ben@code.org>
2020-08-04 16:53:48 -0700

    Use test-specific requests and users

--- dashboard/test/helpers/gdpr_dialog_helper_test.rb
+++ dashboard/test/helpers/gdpr_dialog_helper_test.rb
@@ -14,20 +14,24 @@ class GdprDialogHelperTest < ActionView::TestCase
   end
 
   test 'do not show GDPR dialog if user has accepted data transfer agreement' do
-    @user.data_transfer_agreement_accepted = true
+    accepted_user = build :user, data_transfer_agreement_accepted: true
 
-    refute GdprDialogHelper.show?(@user, @request)
+    refute GdprDialogHelper.show?(accepted_user, @request)
   end
 
   test 'show GDPR dialog if force show in params' do
-    @request.stubs(:params).returns({'force_in_eu' => 'true'})
+    force_eu_request = ActionDispatch::Request.new({})
+    force_eu_request.stubs(:gdpr?).returns(false)
+    force_eu_request.stubs(:params).returns({'force_in_eu' => 'true'})
 
-    assert GdprDialogHelper.show?(@user, @request)
+    assert GdprDialogHelper.show?(@user, force_eu_request)
   end
 
   test 'show GDPR dialog if in GDPR country' do
-    @request.stubs(:gdpr?).returns(true)
+    eu_request = ActionDispatch::Request.new({})
+    eu_request.stubs(:params).returns({})
+    eu_request.stubs(:gdpr?).returns(true)
 
-    assert GdprDialogHelper.show?(@user, @request)
+    assert GdprDialogHelper.show?(@user, eu_request)
   end
 end
