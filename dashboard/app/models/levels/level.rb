192cfe9020456ddaf6f55457c00df45b865a1e0f
David Bailey <davidsbailey@users.noreply.github.com>
2020-08-06 09:52:26 -0700

    fix comments on all_child_levels

--- dashboard/app/models/levels/level.rb
+++ dashboard/app/models/levels/level.rb
@@ -715,14 +715,15 @@ class Level < ActiveRecord::Base
     end
   end
 
-  # Returns all child levels of this level, which could include contained levels,
-  # project template levels, BubbleChoice sublevels, or LevelGroup sublevels.
   def all_descendant_levels
     my_child_levels = all_child_levels
     child_descendant_levels = my_child_levels.map(&:all_descendant_levels).flatten
     my_child_levels + child_descendant_levels
   end
 
+  # Returns all child levels of this level, which could include contained levels,
+  # project template levels, BubbleChoice sublevels, or LevelGroup sublevels.
+  # This method may be overridden by subclasses.
   def all_child_levels
     (contained_levels + [project_template_level]).compact
   end
