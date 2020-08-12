52bbd2ed279516e252721f6cc6c8b47f923ed81a
Anjali <anjali@code.org>
2020-08-12 15:01:43 -0700

    checkpoint

--- apps/src/sites/studio/pages/blockly.js
+++ apps/src/sites/studio/pages/blockly.js
@@ -1,4 +1,11 @@
-import Blockly from '@code-dot-org/blockly';
-import initializeCdoBlocklyWrapper from './cdoBlocklyWrapper';
+// import Blockly from '@code-dot-org/blockly';
+// import initializeCdoBlocklyWrapper from './cdoBlocklyWrapper';
 
-window.Blockly = initializeCdoBlocklyWrapper(Blockly);
+import Blockly from 'blockly/core';
+import locale from 'blockly/msg/en';
+import 'blockly/blocks';
+import 'blockly/javascript';
+Blockly.setLocale(locale);
+import initializeGoogleBlocklyWrapper from './googleBlocklyWrapper';
+
+window.Blockly = initializeGoogleBlocklyWrapper(Blockly);
---
+++ apps/src/sites/studio/pages/googleBlocklyWrapper.js
@@ -0,0 +1,167 @@
+const BlocklyWrapper = function(blocklyInstance) {
+  this.blockly_ = blocklyInstance;
+  this.Msg = blocklyInstance.Msg;
+  this.inject = this.blockly_.inject;
+  this.wrapReadOnlyProperty = function(propertyName) {
+    Object.defineProperty(this, propertyName, {
+      get: function() {
+        return this.blockly_[propertyName];
+      }
+    });
+  };
+  this.wrapSettableProperty = function(propertyName) {
+    Object.defineProperty(this, propertyName, {
+      get: function() {
+        return this.blockly_[propertyName];
+      },
+      set: function(newValue) {
+        this.blockly_[propertyName] = newValue;
+      }
+    });
+  };
+};
+
+function initializeBlocklyWrapper(blocklyInstance) {
+  const blocklyWrapper = new BlocklyWrapper(blocklyInstance);
+
+  blocklyWrapper.wrapReadOnlyProperty('ALIGN_CENTRE');
+  blocklyWrapper.wrapReadOnlyProperty('ALIGN_LEFT');
+  blocklyWrapper.wrapReadOnlyProperty('ALIGN_RIGHT');
+  blocklyWrapper.wrapReadOnlyProperty('applab_locale');
+  blocklyWrapper.wrapReadOnlyProperty('bindEvent_');
+  blocklyWrapper.wrapReadOnlyProperty('Block');
+  blocklyWrapper.wrapReadOnlyProperty('BlockFieldHelper');
+  blocklyWrapper.wrapReadOnlyProperty('Blocks');
+  blocklyWrapper.wrapReadOnlyProperty('BlockSvg');
+  blocklyWrapper.wrapReadOnlyProperty('BlockValueType');
+  blocklyWrapper.wrapReadOnlyProperty('common_locale');
+  blocklyWrapper.wrapReadOnlyProperty('Connection');
+  blocklyWrapper.wrapReadOnlyProperty('contractEditor');
+  blocklyWrapper.wrapReadOnlyProperty('createSvgElement');
+  blocklyWrapper.wrapReadOnlyProperty('Css');
+  blocklyWrapper.wrapReadOnlyProperty('disableVariableEditing');
+  blocklyWrapper.wrapReadOnlyProperty('FieldAngleDropdown');
+  blocklyWrapper.wrapReadOnlyProperty('FieldAngleInput');
+  blocklyWrapper.wrapReadOnlyProperty('FieldAngleTextInput');
+  blocklyWrapper.wrapReadOnlyProperty('FieldButton');
+  blocklyWrapper.wrapReadOnlyProperty('FieldColour');
+  blocklyWrapper.wrapReadOnlyProperty('FieldColourDropdown');
+  blocklyWrapper.wrapReadOnlyProperty('FieldDropdown');
+  blocklyWrapper.wrapReadOnlyProperty('FieldIcon');
+  blocklyWrapper.wrapReadOnlyProperty('FieldImage');
+  blocklyWrapper.wrapReadOnlyProperty('FieldImageDropdown');
+  blocklyWrapper.wrapReadOnlyProperty('FieldLabel');
+  blocklyWrapper.wrapReadOnlyProperty('FieldParameter');
+  blocklyWrapper.wrapReadOnlyProperty('FieldRectangularDropdown');
+  blocklyWrapper.wrapReadOnlyProperty('FieldTextInput');
+  blocklyWrapper.wrapReadOnlyProperty('FieldVariable');
+  blocklyWrapper.wrapReadOnlyProperty('findEmptyContainerBlock');
+  blocklyWrapper.wrapReadOnlyProperty('fireUiEvent');
+  blocklyWrapper.wrapReadOnlyProperty('fish_locale');
+  blocklyWrapper.wrapReadOnlyProperty('Flyout');
+  blocklyWrapper.wrapReadOnlyProperty('FunctionalBlockUtils');
+  blocklyWrapper.wrapReadOnlyProperty('FunctionalTypeColors');
+  blocklyWrapper.wrapReadOnlyProperty('FunctionEditor');
+  blocklyWrapper.wrapReadOnlyProperty('functionEditor');
+  blocklyWrapper.wrapReadOnlyProperty('gamelab_locale');
+  blocklyWrapper.wrapReadOnlyProperty('Generator');
+  blocklyWrapper.wrapReadOnlyProperty('getRelativeXY');
+  blocklyWrapper.wrapReadOnlyProperty('googlecode');
+  blocklyWrapper.wrapReadOnlyProperty('hasCategories');
+  blocklyWrapper.wrapReadOnlyProperty('html');
+  blocklyWrapper.wrapReadOnlyProperty('inject');
+  blocklyWrapper.wrapReadOnlyProperty('Input');
+  blocklyWrapper.wrapReadOnlyProperty('INPUT_VALUE');
+  blocklyWrapper.wrapReadOnlyProperty('js');
+  blocklyWrapper.wrapReadOnlyProperty('modalBlockSpace');
+  blocklyWrapper.wrapReadOnlyProperty('Msg');
+  blocklyWrapper.wrapReadOnlyProperty('Names');
+  blocklyWrapper.wrapReadOnlyProperty('netsim_locale');
+  blocklyWrapper.wrapReadOnlyProperty('Procedures');
+  blocklyWrapper.wrapReadOnlyProperty('removeChangeListener');
+  blocklyWrapper.wrapReadOnlyProperty('RTL');
+  blocklyWrapper.wrapReadOnlyProperty('SVG_NS');
+  blocklyWrapper.wrapReadOnlyProperty('tutorialExplorer_locale');
+  blocklyWrapper.wrapReadOnlyProperty('useContractEditor');
+  blocklyWrapper.wrapReadOnlyProperty('useModalFunctionEditor');
+  blocklyWrapper.wrapReadOnlyProperty('Variables');
+  blocklyWrapper.wrapReadOnlyProperty('weblab_locale');
+  blocklyWrapper.wrapReadOnlyProperty('Workspace');
+  blocklyWrapper.wrapReadOnlyProperty('Xml');
+
+  blocklyWrapper.wrapSettableProperty('assetUrl');
+  blocklyWrapper.wrapSettableProperty('behaviorEditor');
+  blocklyWrapper.wrapSettableProperty('BROKEN_CONTROL_POINTS');
+  blocklyWrapper.wrapSettableProperty('BUMP_UNCONNECTED');
+  blocklyWrapper.wrapSettableProperty('HSV_SATURATION');
+  blocklyWrapper.wrapSettableProperty('JavaScript');
+  blocklyWrapper.wrapSettableProperty('readOnly');
+  blocklyWrapper.wrapSettableProperty('showUnusedBlocks');
+  blocklyWrapper.wrapSettableProperty('SNAP_RADIUS');
+  blocklyWrapper.wrapSettableProperty('typeHints');
+  blocklyWrapper.wrapSettableProperty('valueTypeTabShapeMap');
+
+  blocklyWrapper.getGenerator = function() {
+    return this.JavaScript;
+  };
+  blocklyWrapper.BlockSpace = {};
+  blocklyWrapper.BlockSpace.EVENTS = {};
+  blocklyWrapper.BlockSpace.EVENTS.MAIN_BLOCK_SPACE_CREATED =
+    'mainBlockSpaceCreated';
+  blocklyWrapper.BlockSpace.EVENTS.EVENT_BLOCKS_IMPORTED = 'blocksImported';
+  blocklyWrapper.BlockSpace.EVENTS.BLOCK_SPACE_CHANGE = 'blockSpaceChange';
+  blocklyWrapper.BlockSpace.EVENTS.BLOCK_SPACE_SCROLLED = 'blockSpaceScrolled';
+  blocklyWrapper.BlockSpace.EVENTS.RUN_BUTTON_CLICKED = 'runButtonClicked';
+  blocklyWrapper.BlockSpace.onMainBlockSpaceCreated = () => {}; // TODO
+
+  blocklyWrapper.Block.prototype.setHSV = function(h, s, v) {
+    return this.setColour(h);
+  };
+
+  blocklyWrapper.Input.prototype.appendTitle = function(a, b) {
+    return this.appendField(a, b);
+  };
+
+  Object.defineProperty(blocklyWrapper, 'mainBlockSpace', {
+    get: function() {
+      return this.blockly_.mainWorkspace;
+    }
+  });
+  Object.defineProperty(blocklyWrapper, 'mainBlockSpaceEditor', {
+    get: function() {
+      return this.blockly_.mainWorkspace;
+    }
+  });
+
+  blocklyWrapper.Workspace.prototype.getToolboxWidth = function() {
+    return blocklyWrapper.mainBlockSpace.getMetrics().toolboxWidth;
+  };
+
+  blocklyWrapper.Xml.domToBlockSpace = blocklyWrapper.Xml.domToWorkspace;
+  blocklyWrapper.Workspace.prototype.addUnusedBlocksHelpListener = () => {}; // TODO
+  blocklyWrapper.Workspace.prototype.getAllUsedBlocks =
+    blocklyWrapper.Workspace.prototype.getAllBlocks; // TODO
+  blocklyWrapper.Workspace.prototype.setEnableToolbox = () => {}; // TODO
+  blocklyWrapper.Generator.blockSpaceToCode = function(name, opt_typeFilter) {
+    let blocksToGenerate = blocklyWrapper.mainBlockSpace.getTopBlocks(
+      true /* ordered */
+    );
+    if (opt_typeFilter) {
+      if (typeof opt_typeFilter === 'string') {
+        opt_typeFilter = [opt_typeFilter];
+      }
+      blocksToGenerate = blocksToGenerate.filter(block =>
+        opt_typeFilter.includes(block.type)
+      );
+    }
+    let code = [];
+    blocksToGenerate.forEach(block => {
+      code.push(blocklyWrapper.JavaScript.blockToCode(block));
+    });
+    return code.join('\n');
+  };
+
+  return blocklyWrapper;
+}
+
+module.exports = initializeBlocklyWrapper;
