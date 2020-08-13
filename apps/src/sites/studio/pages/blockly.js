20a8eee1d425d230ad18cab58a76b499b4126487
Anjali <anjali@code.org>
2020-08-13 15:28:17 -0700

    configure blockly with experiment

--- apps/src/sites/studio/pages/blockly.js
+++ apps/src/sites/studio/pages/blockly.js
@@ -1,6 +1,7 @@
-// import Blockly from '@code-dot-org/blockly';
-// import initializeCdoBlocklyWrapper from './cdoBlocklyWrapper';
-// window.Blockly = initializeCdoBlocklyWrapper(Blockly);
+import experiments from '@cdo/apps/util/experiments';
+
+import CDOBlockly from '@code-dot-org/blockly';
+import initializeCdoBlocklyWrapper from './cdoBlocklyWrapper';
 
 import Blockly from 'blockly/core';
 import locale from 'blockly/msg/en';
@@ -8,4 +9,9 @@ import 'blockly/blocks';
 import 'blockly/javascript';
 Blockly.setLocale(locale);
 import initializeGoogleBlocklyWrapper from './googleBlocklyWrapper';
-window.Blockly = initializeGoogleBlocklyWrapper(Blockly);
+
+if (experiments.isEnabled(experiments.GOOGLE_BLOCKLY)) {
+  window.Blockly = initializeGoogleBlocklyWrapper(Blockly);
+} else {
+  window.Blockly = initializeCdoBlocklyWrapper(CDOBlockly);
+}
