56826eae3f7d76f29888e3d6b3fa51b60944eb46
Ben Brooks <ben@code.org>
2020-08-18 10:24:11 -0700

    Support single select and multi select questions

--- dashboard/app/models/foorm/form.rb
+++ dashboard/app/models/foorm/form.rb
@@ -99,33 +99,84 @@ class Foorm::Form < ActiveRecord::Base
     formatted_submissions = []
 
     submissions.each do |submission|
-      formatted_submission = {}
+      formatted_submission = []
 
+      # This comes from the submission, where a respondent may or may not
+      # have answered all questions.
       parsed_answers = JSON.parse(submission.answers)
+
+      # parsed_questions comes from the survey, and as a result will generate
+      # blank entries for questions that a respondent did not answer.
       parsed_questions[:general][key].each do |question_id, question_details|
+        puts question_id
+        puts question_details
+
+        question_answer_pair = {}
+
         if question_details[:type] == 'matrix'
           section_preamble = question_details[:title]
           matrix_questions_full_text = question_details[:rows]
-          parsed_answers[question_id].each do |matrix_question_id, answer|
-            question_full_text_key = "#{section_preamble} >> #{matrix_questions_full_text[matrix_question_id]}"
-            formatted_submission[question_full_text_key] = answer
+          # Maybe pull out parsed_answers[question_id] into it's own thing
+          parsed_answers[question_id]&.each do |matrix_question_id, answer|
+            question_full_text = "#{section_preamble} >> #{matrix_questions_full_text[matrix_question_id]}"
+            question_answer_pair = {
+              question: question_full_text,
+              answer: question_details[:columns][answer]
+            }
+
+            formatted_submission << question_answer_pair
           end
+        elsif question_details[:type] == 'text'
+          question_answer_pair = {
+            question: question_details[:title],
+            answer: parsed_answers[question_id]
+          }
+
+          formatted_submission << question_answer_pair
+        elsif question_details[:type] == 'singleSelect'
+          question_answer_pair = {
+            question: question_details[:title],
+            answer: question_details[:choices][parsed_answers[question_id]]
+          }
+
+          formatted_submission << question_answer_pair
+        elsif question_details[:type] == 'multiSelect'
+          puts parsed_answers[question_id]
+          question_answer_pair = {
+            question: question_details[:title],
+            answer: parsed_answers[question_id]&.map {|selected| question_details[:choices][selected]}&.join(', ')
+          }
+
+          puts question_answer_pair
+        elsif question_details[:type] == 'scale'
+          puts 'SCALE'
         else
-          puts "#{question_details[:title]}: #{parsed_answers[question_id]}"
+          'TBD'
         end
       end
 
-      formatted_submission['user_id'] = submission.metadata&.user&.id
-      formatted_submission['pd_workshop_id'] = submission.metadata&.pd_workshop&.id
-      formatted_submission['pd_session_id'] = submission.metadata&.pd_session&.id
+      formatted_submission << {
+        question: 'user_id',
+        answer: submission.metadata&.user&.id
+      }
+
+      formatted_submission << {
+        question: 'pd_workshop_id',
+        answer: submission.metadata&.pd_workshop&.id
+      }
+
+      formatted_submission << {
+        question: 'pd_session_id',
+        answer: submission.metadata&.pd_session&.id
+      }
 
       formatted_submissions << formatted_submission
     end
 
     CSV.open("/Users/benjaminbrooks/data.csv", "wb") do |csv|
-      csv << formatted_submissions.first.keys
-      formatted_submissions.each do |hash|
-        csv << hash.values
+      csv << formatted_submissions.first.map {|question_answer_pair| question_answer_pair[:question]}
+      formatted_submissions.each do |submission|
+        csv << submission.map {|question_answer_pair| question_answer_pair[:answer]}
       end
     end
   end
