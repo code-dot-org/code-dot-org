ab5431fb708c2cab9d351a68824df653febec6e8
Anjali <anjali@code.org>
2020-08-20 12:37:35 -0700

    configure blockly version on the server side

--- apps/src/sites/studio/pages/blockly.js
+++ apps/src/sites/studio/pages/blockly.js
@@ -1,17 +1,4 @@
-import experiments from '@cdo/apps/util/experiments';
-
 import CDOBlockly from '@code-dot-org/blockly';
 import initializeCdoBlocklyWrapper from './cdoBlocklyWrapper';
 
-import GoogleBlockly from 'blockly/core';
-import locale from 'blockly/msg/en';
-import 'blockly/blocks';
-import 'blockly/javascript';
-GoogleBlockly.setLocale(locale);
-import initializeGoogleBlocklyWrapper from './googleBlocklyWrapper';
-
-if (experiments.isEnabled(experiments.GOOGLE_BLOCKLY)) {
-  window.Blockly = initializeGoogleBlocklyWrapper(GoogleBlockly);
-} else {
-  window.Blockly = initializeCdoBlocklyWrapper(CDOBlockly);
-}
+window.Blockly = initializeCdoBlocklyWrapper(CDOBlockly);
