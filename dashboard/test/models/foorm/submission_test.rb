af53defc4e0302be67744e6dcdba94d2f5548971
Ben Brooks <ben@code.org>
2020-08-19 17:19:44 -0700

    Test reshaping of foorm submissions

--- dashboard/test/models/foorm/submission_test.rb
+++ dashboard/test/models/foorm/submission_test.rb
@@ -6,4 +6,43 @@ class Foorm::SubmissionTest < ActiveSupport::TestCase
     expected_answers = "{'a': 'b', 'c': '#{panda_panda} hello'}"
     assert_equal expected_answers, submission.answers
   end
+
+  test 'format matrix question works correctly' do
+    create :foorm_form_csf_intro_post_survey
+    submission = build :csf_intro_post_foorm_submission, :answers_low
+    answers = submission.formatted_answers
+
+    assert answers.include?(
+      {
+        question: "How much do you agree or disagree with the following statements about the workshop overall? >> I feel more prepared to teach the material covered in this workshop than before I came.",
+        answer: "Strongly Disagree"
+      }
+    )
+  end
+
+  test 'format comment question works correctly' do
+    create :foorm_form_csf_intro_post_survey
+    submission = build :csf_intro_post_foorm_submission, :answers_low
+    answers = submission.formatted_answers
+
+    assert answers.include?(
+      {
+        question: "What supported your learning the most today and why?",
+        answer: "lots"
+      }
+    )
+  end
+
+  test 'format single select question works correctly' do
+    create :foorm_form_csf_intro_post_survey
+    submission = build :csf_intro_post_foorm_submission, :answers_low
+    answers = submission.formatted_answers
+
+    assert answers.include?(
+      {
+        question: "I give the workshop organizer permission to quote my written feedback from today for use on social media, promotional materials, and other communications.",
+        answer: "No, I do not give the workshop organizer my permission."
+      }
+    )
+  end
 end
