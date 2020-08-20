99621749ed0301392bf0d416b51c76ac8411e239
Winter Dong <winter@code.org>
2020-08-20 02:48:17 -0700

    WIP seeding logic for CourseOffering

--- dashboard/app/models/course_offering.rb
+++ dashboard/app/models/course_offering.rb
@@ -16,4 +16,27 @@
 
 class CourseOffering < ApplicationRecord
   has_many :course_versions
+
+  def self.add_course_offering(content_root)
+    # If content_root is not designated as the content root of a CourseVersion (in its .script or .course file),
+    # delete its associated CourseVersion object if it exists. This handles the case where a .script or .course file
+    # had "is_course true" at one point, and then later "is_course true" is removed.
+    unless content_root.is_course?
+      if content_root.course_version
+        content_root.course_version.destroy
+        content_root.reload
+      end
+      return nil
+    end
+
+    raise "family_name must be set, since is_course is true, for: #{content_root.name}" if content_root.family_name.nil_or_empty?
+    raise "version_year must be set, since is_course is true, for: #{content_root.name}" if content_root.version_year.nil_or_empty?
+
+    offering = CourseOffering.find_or_create_by(key: content_root.family_name, display_name: content_root.family_name)
+    CourseVersion.add_course_version(offering, content_root)
+
+    # TODO: add relevant properties from content root to course_version
+
+    offering
+  end
 end
