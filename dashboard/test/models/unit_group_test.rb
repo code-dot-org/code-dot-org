8d47f86bb8918664201bd955bd890a6458afe660
David Bailey <davidsbailey@users.noreply.github.com>
2020-08-21 05:03:04 -0700

    inline unit test locals

--- dashboard/test/models/unit_group_test.rb
+++ dashboard/test/models/unit_group_test.rb
@@ -222,20 +222,14 @@ class UnitGroupTest < ActiveSupport::TestCase
 
     I18n.backend.store_translations test_locale, custom_i18n
 
-    summary = unit_group.summarize
-    assert_equal 'script1-title', summary[:scripts].first[:title]
-
-    summary = script.summarize
-    assert_equal 'script1-title', summary[:title]
+    assert_equal 'script1-title', unit_group.summarize[:scripts].first[:title]
+    assert_equal 'script1-title', script.summarize[:title]
 
     unit_group.has_numbered_units = true
     unit_group.save!
 
-    summary = unit_group.summarize
-    assert_equal 'Unit 1 - script1-title', summary[:scripts].first[:title]
-
-    summary = script.summarize
-    assert_equal 'Unit 1 - script1-title', summary[:title]
+    assert_equal 'Unit 1 - script1-title', unit_group.summarize[:scripts].first[:title]
+    assert_equal 'Unit 1 - script1-title', script.summarize[:title]
   end
 
   test 'summarize_version' do
