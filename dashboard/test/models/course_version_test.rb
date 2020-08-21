2a18089ea0919abcdfbe2b01697a4910fc2af0a6
Winter Dong <winter@code.org>
2020-08-20 20:58:15 -0700

    Fix course_version_test

--- dashboard/test/models/course_version_test.rb
+++ dashboard/test/models/course_version_test.rb
@@ -12,52 +12,56 @@ class CourseVersionTest < ActiveSupport::TestCase
   end
 
   test "add_course_version creates CourseVersion for script that doesn't have one if is_course is true" do
+    offering = create :course_offering
     script = create :script, family_name: 'csz', version_year: '2050', is_course: true
-    course_version = CourseVersion.add_course_version(script)
+    course_version = CourseVersion.add_course_version(offering, script)
 
-    assert_equal course_version, CourseVersion.find_by(key: 'csz-2050')
+    assert_equal course_version, CourseVersion.find_by(course_offering: offering, key: '2050')
   end
 
   test "add_course_version updates existing CourseVersion for script if properties change" do
+    offering = create :course_offering
     script = create :script, family_name: 'csz', version_year: '2050', is_course: true
-    CourseVersion.add_course_version(script)
+    CourseVersion.add_course_version(offering, script)
 
-    assert_equal 'csz-2050', script.course_version.key
+    assert_equal '2050', script.course_version.key
     assert_equal '2050', script.course_version.display_name
 
     script.family_name = 'csx'
     script.version_year = '2060'
     script.save
 
-    CourseVersion.add_course_version(script)
+    CourseVersion.add_course_version(offering, script)
 
-    assert_equal 'csx-2060', script.course_version.key
+    assert_equal '2060', script.course_version.key
     assert_equal '2060', script.course_version.display_name
-    assert_equal script.course_version, CourseVersion.find_by(key: 'csx-2060')
-    assert_nil CourseVersion.find_by(key: 'csz-2050') # old CourseVersion should be deleted
+    assert_equal script.course_version, CourseVersion.find_by(course_offering: offering, key: '2060')
+    assert_nil CourseVersion.find_by(course_offering: offering, key: '2050') # old CourseVersion should be deleted
   end
 
   test "add_course_version deletes CourseVersion for script if is_course is changed to false" do
+    offering = create :course_offering
     script = create :script, family_name: 'csz', version_year: '2050', is_course: true
-    CourseVersion.add_course_version(script)
+    CourseVersion.add_course_version(offering, script)
 
     assert_not_nil script.course_version
 
     script.is_course = false
     script.save
-    course_version = CourseVersion.add_course_version(script)
+    course_version = CourseVersion.add_course_version(offering, script)
 
     assert_nil course_version
     assert_nil script.course_version
-    assert_nil CourseVersion.find_by(key: 'csz-2050')
+    assert_nil CourseVersion.find_by(course_offering: offering, key: '2050')
   end
 
   test "add_course_version does nothing for script without CourseVersion if is_course is false" do
+    offering = create :course_offering
     script = create :script, family_name: 'csz', version_year: '2050'
-    course_version = CourseVersion.add_course_version(script)
+    course_version = CourseVersion.add_course_version(offering, script)
 
     assert_nil course_version
     assert_nil script.course_version
-    assert_nil CourseVersion.find_by(key: 'csz-2050')
+    assert_nil CourseVersion.find_by(course_offering: offering, key: '2050')
   end
 end
