d24a19cf1eddf4dc01730b3e5d119f738902daa5
Dayne <dayne@code.org>
2020-08-27 23:07:15 -0700

    Adding I18n string tracking decorator

--- apps/src/flappy/locale.js
+++ apps/src/flappy/locale.js
@@ -1,4 +1,8 @@
 // locale for flappy
 
 import safeLoadLocale from '@cdo/apps/util/safeLoadLocale';
-module.exports = safeLoadLocale('flappy_locale');
+import localeWithI18nStringTracker from '@cdo/apps/util/i18nStringTracker';
+
+let locale = safeLoadLocale('flappy_locale');
+locale = localeWithI18nStringTracker(locale, 'flappy_locale');
+module.exports = locale;
--- apps/src/weblab/locale.js
+++ apps/src/weblab/locale.js
@@ -1,3 +1,8 @@
 // locale for weblab
+
 import safeLoadLocale from '@cdo/apps/util/safeLoadLocale';
-module.exports = safeLoadLocale('weblab_locale');
+import localeWithI18nStringTracker from '@cdo/apps/util/i18nStringTracker';
+
+let locale = safeLoadLocale('weblab_locale');
+locale = localeWithI18nStringTracker(locale, 'weblab_locale');
+module.exports = locale;
