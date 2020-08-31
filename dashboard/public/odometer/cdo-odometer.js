facbaa38f963d66415c7719067d7c0f36154dd16
Anjali <anjali@code.org>
2020-08-31 13:51:34 -0700

    fix newrelic errors

--- dashboard/public/odometer/cdo-odometer.js
+++ dashboard/public/odometer/cdo-odometer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b13824bcb3a8ff0bceeda13b4fb470b4cd1c9ee3881de72f61c70c1667437bdb
-size 2310
+oid sha256:c0f35d9247d7f83c95bb2a1f197f0e0c5b3906c092f6b2724f49ca7a696869ee
+size 2396
