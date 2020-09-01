ba822e035632ec8bcba98b2ec7472b855e179ff4
Dani McAvoy <danimcavoy@gmail.com>
2020-08-31 20:29:52 -0400

    Add teacher resources to i18n for course

--- dashboard/app/controllers/courses_controller.rb
+++ dashboard/app/controllers/courses_controller.rb
@@ -91,8 +91,12 @@ class CoursesController < ApplicationController
 
   def update
     unit_group = UnitGroup.find_by_name!(params[:course_name])
-    unit_group.persist_strings_and_scripts_changes(params[:scripts], params[:alternate_scripts], i18n_params)
     unit_group.update_teacher_resources(params[:resourceTypes], params[:resourceLinks])
+    #update i18n data for teacher resources
+    copy_i18n_params = i18n_params.deep_dup
+    copy_i18n_params["teacher_resource_types"] = unit_group.teacher_resources.map {|resource| resource[0]}
+    puts copy_i18n_params
+    unit_group.persist_strings_and_scripts_changes(params[:scripts], params[:alternate_scripts], copy_i18n_params)
     # Convert checkbox values from a string ("on") to a boolean.
     [:has_verified_resources, :has_numbered_units, :visible, :is_stable].each {|key| params[key] = !!params[key]}
     unit_group.update(course_params)
