1f2c1e4a583c01cbc9ce5f691d8892e6f879d661
Elijah Hamovitz <elijahhamovitz@gmail.com>
2020-09-02 15:54:33 -0700

    use built-in sequence helper rather than trying to roll our own version

--- dashboard/test/factories/factories.rb
+++ dashboard/test/factories/factories.rb
@@ -72,7 +72,7 @@ FactoryGirl.define do
 
   factory :user do
     birthday Time.zone.today - 21.years
-    email {("#{user_type}_#{SecureRandom.uuid}@code.org")}
+    sequence(:email) {|n| "#{user_type}_#{n}@code.org"}
     password "00secret"
     locale 'en-US'
     sequence(:name) {|n| "User#{n} Codeberg"}
