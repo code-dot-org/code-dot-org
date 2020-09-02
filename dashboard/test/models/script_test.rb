8d2ecc064b415bb396380a4827bfe8b02e86a1e1
David Bailey <davidsbailey@users.noreply.github.com>
2020-09-02 16:18:49 -0700

    fix test_create_script_from_DSL

--- dashboard/test/models/script_test.rb
+++ dashboard/test/models/script_test.rb
@@ -67,7 +67,7 @@ class ScriptTest < ActiveSupport::TestCase
     scripts, _ = Script.setup([@script_file])
     script = scripts[0]
     assert_equal 'Level 1', script.levels[0].name
-    assert_equal 'lesson2', script.script_levels[3].lesson.name
+    assert_equal 'Lesson Two', script.script_levels[3].lesson.name
 
     assert_equal 'MyProgression', script.script_levels[3].progression
     assert_equal 'MyProgression', script.script_levels[4].progression
