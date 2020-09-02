26060dae2a02568e5d1378dd68011973bbc4c90f
Winter Dong <winter@code.org>
2020-09-02 14:16:04 -0700

    Cleanup

--- dashboard/config/initializers/active_model_serializers.rb
+++ dashboard/config/initializers/active_model_serializers.rb
@@ -1,18 +1,1 @@
-# Patch to allow serializing String and Hash objects by default
-
-#class StringSerializer < ActiveModel::Serializer
-#  def attributes(obj)
-#    @object.to_s
-#  end
-#end
-#class IntegerSerializer < StringSerializer; end
-#class HashSerializer < ActiveModel::Serializer
-#  def attributes(obj)
-#    @object.as_json
-#  end
-#end
-#class ActiveModel::ErrorsSerializer < HashSerializer; end
-
-#ActiveModel::Serializer.config.adapter = :Json
-#ActiveModel::Serializer.config.adapter = nil
 ActiveModel::Serializer.config.adapter = :attributes
