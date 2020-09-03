a71825977c03f6b179f9f6f3004b977d757fb188
molly-moen <molly@code.org>
2020-09-03 16:23:01 -0700

    remove unnecessary changes

--- dashboard/app/controllers/api/v1/pd/foorm_controller.rb
+++ dashboard/app/controllers/api/v1/pd/foorm_controller.rb
@@ -13,14 +13,4 @@ class Api::V1::Pd::FoormController < ::ApplicationController
     form_questions = JSON.parse(Foorm::Form.where(name: form_name, version: form_version).first&.questions)
     render json: form_questions
   end
-
-  def validate_questions
-    form_questions = params[:form_questions].as_json
-    begin
-      Foorm::Form.validate_questions(form_questions)
-    rescue StandardError => e
-      return render json: {state: "invalid form", error: e.message}, status: 500
-    end
-    render json: {state: "valid form"}, status: 200
-  end
 end
