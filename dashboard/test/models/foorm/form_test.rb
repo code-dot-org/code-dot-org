af315b558b55433cb4e4f565d99d8a0e20de301c
Ben Brooks <ben@code.org>
2020-09-04 12:02:23 -0700

    Correct ordering of headers in facilitator-specific Foorm CSV export

--- dashboard/test/models/foorm/form_test.rb
+++ dashboard/test/models/foorm/form_test.rb
@@ -9,37 +9,4 @@ class Foorm::FormTest < ActiveSupport::TestCase
     assert_equal JSON.parse(form2.questions), questions
     assert_equal form2.version, version
   end
-
-  test 'new_headers_from_answers gets new header from answer' do
-    form = create :foorm_form_summer_post_survey
-    headers = {
-      'could_improve' => 'What could be improved?',
-      'most_enjoyed' => 'What did you enjoy most?'
-    }
-
-    answers = {
-      'could_improve' => 'The food.',
-      'pd_workshop_id' => 25
-    }
-
-    expected_new_headers = {'pd_workshop_id' => 'pd_workshop_id'}
-
-    assert_equal expected_new_headers,
-      form.new_headers_from_answers(headers, answers)
-  end
-
-  test 'new_headers_from_answers returns blank hash when no new headers in answer' do
-    form = create :foorm_form_summer_post_survey
-    headers = {
-      'could_improve' => 'What could be improved?',
-      'most_enjoyed' => 'What did you enjoy most?'
-    }
-
-    answers = {
-      'could_improve' => 'The food.',
-    }
-
-    assert_equal Hash[],
-      form.new_headers_from_answers(headers, answers)
-  end
 end
