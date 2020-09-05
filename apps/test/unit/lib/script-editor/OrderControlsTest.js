3d1443949ce409a2f04dbcb93a0d5b9f1d4eacb0
Dani McAvoy <danimcavoy@gmail.com>
2020-09-05 14:33:47 -0400

    Fix order controls import again because oops

--- apps/test/unit/lib/script-editor/OrderControlsTest.js
+++ apps/test/unit/lib/script-editor/OrderControlsTest.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {mount} from 'enzyme';
 import {expect} from '../../../util/reconfiguredChai';
 import sinon from 'sinon';
-import OrderControls from '@cdo/apps/lib/levelbuilder/lesson-editor/OrderControls';
+import OrderControls from '@cdo/apps/lib/levelbuilder/OrderControls';
 
 describe('OrderControls', () => {
   let move, remove, defaultProps;
