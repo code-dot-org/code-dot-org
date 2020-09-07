6c230e46cec0b0bf94a3c89813811b5ec31237c3
Dani McAvoy <danimcavoy@gmail.com>
2020-09-06 20:23:44 -0400

    update import for activitiesTestData

--- apps/src/sites/studio/pages/lessons/show.js
+++ apps/src/sites/studio/pages/lessons/show.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import getScriptData from '@cdo/apps/util/getScriptData';
 import LessonOverview from '@cdo/apps/templates/lessonOverview/LessonOverview';
-import {sampleActivities} from '../../../../../test/unit/code-studio/activitiesTestData';
+import {sampleActivities} from '../../../../../test/unit/lib/levelbuilder/lesson-editor/activitiesTestData';
 
 $(document).ready(function() {
   const lessonData = getScriptData('lesson');
