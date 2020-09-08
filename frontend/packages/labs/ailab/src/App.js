fb022f3518ae9136d22cb11726772d28718c54f6
Erin Bond <erin.bond@code.org>
2020-09-08 13:36:29 -0400

    add React and dependencies

---
+++ frontend/packages/labs/ailab/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a React component!</h1>
+        <h3>Sit tight, there's more ML stuff coming soon.</h3>
+      </div>
+    );
+  }
+}
