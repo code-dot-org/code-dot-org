86f59b9bb9783f6ea260db3608e3a89042965b78
molly-moen <molly@code.org>
2020-09-09 14:27:58 -0700

    update tests

--- lib/cdo/shared_constants/pd/teacher2122_application_constants.rb
+++ lib/cdo/shared_constants/pd/teacher2122_application_constants.rb
@@ -426,14 +426,12 @@ module Pd
       not_teaching_this_year: "I'm not teaching this year (Please Explain):".freeze,
       not_teaching_next_year: "I'm not teaching next year (Please Explain):".freeze,
       dont_know_if_i_will_teach_explain: "I don't know if I will teach this course (Please Explain):".freeze,
-      unable_to_attend: "No, I'm unable to attend (Please Explain):".freeze,
+      unable_to_attend: 'I’m not able to attend any of the above workshop dates. (Please Explain):',
       able_to_attend_single: "Yes, I'm able to attend".freeze,
       no_explain: "No (Please Explain):".freeze,
-      no_pay_fee: "No, my school or I will not be able to pay the summer workshop program fee.".freeze,
+      no_pay_fee: 'No, my school will not be able to pay the program fee. I would like to be considered for a scholarship.',
       i_dont_know_explain: "I don't know (Please Explain):",
-      no_pay_fee_2021: 'No, my school will not be able to pay the program fee. I would like to be considered for a scholarship.',
-      not_sure_explain: 'Not sure (Please Explain):',
-      unable_to_attend_2021: 'I’m not able to attend any of the above workshop dates. (Please Explain):'
+      not_sure_explain: 'Not sure (Please Explain):'
     }.freeze
 
     SUBJECTS_THIS_YEAR = [
