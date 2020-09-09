7b55808fc34dd1ff74dedd7c5eb66e7a7ab53a7b
Dani McAvoy <danimcavoy@gmail.com>
2020-09-09 18:11:47 -0400

    Translate all string on teaching facing lesson plan

--- apps/src/templates/lessonOverview/activities/Activity.jsx
+++ apps/src/templates/lessonOverview/activities/Activity.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import ActivitySection from '@cdo/apps/templates/lessonOverview/activities/ActivitySection';
 import color from '@cdo/apps/util/color';
+import i18n from '@cdo/locale';
 
 const styles = {
   activityHeader: {
@@ -19,9 +20,12 @@ export default class Activity extends Component {
 
     return (
       <div>
-        <h2 style={styles.activityHeader}>{`${activity.displayName} (${
-          activity.time
-        } minutes)`}</h2>
+        <h2 style={styles.activityHeader}>
+          {i18n.activityHeader({
+            activityName: activity.displayName,
+            activityTime: activity.time
+          })}
+        </h2>
         {activity.activitySections.map(item => {
           return <ActivitySection key={item.key} section={item} />;
         })}
