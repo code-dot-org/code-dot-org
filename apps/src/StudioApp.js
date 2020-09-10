fcd9c270c97f10ca8f2997799d62633cb375ef31
Jessica Kulwik <jessica@code.org>
2020-09-10 21:20:58 +0000

    Updates from CR comment

--- apps/src/StudioApp.js
+++ apps/src/StudioApp.js
@@ -1778,6 +1778,7 @@ StudioApp.prototype.builderForm_ = function(onAttemptCallback) {
  * @param {MilestoneReport} options
  */
 StudioApp.prototype.report = function(options) {
+  // We don't need to report again on reset.
   this.hasReported = true;
   const currentTime = new Date().getTime();
   // copy from options: app, level, result, testResult, program, onComplete
@@ -1843,6 +1844,7 @@ StudioApp.prototype.clearAndAttachRuntimeAnnotations = function() {
  * Click the reset button.  Reset the application.
  */
 StudioApp.prototype.resetButtonClick = function() {
+  // If we haven't reported yet, report now.
   if (!this.hasReported) {
     this.report({
       app: getStore().getState().pageConstants.appType,
