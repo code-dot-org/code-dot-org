38ea0f7a2344094f66152ada896314074a3ddba9
Anjali <anjali@code.org>
2020-09-10 09:23:39 -0700

    add special case for ??? in dropdowns

--- apps/src/blocklyAddons/cdoFieldDropdown.js
+++ apps/src/blocklyAddons/cdoFieldDropdown.js
@@ -1,6 +1,9 @@
 import GoogleBlockly from 'blockly/core';
 
 export default class FieldDropdown extends GoogleBlockly.FieldDropdown {
+  /** Turn changeHandler into validator
+   * @override
+   */
   constructor(menuGenerator, opt_changeHandler, opt_alwaysCallChangeHandler) {
     let validator;
     if (opt_changeHandler) {
@@ -15,5 +18,29 @@ export default class FieldDropdown extends GoogleBlockly.FieldDropdown {
       };
     }
     super(menuGenerator, validator);
+  }
+
+  /** Add special case for ???
+   * @override
+   */
+  doClassValidation_(newValue) {
+    if (newValue === '???') {
+      return newValue;
+    } else {
+      return super.doClassValidation_(newValue);
+    }
+  }
+
+  /** Add special case for ???
+   * @override
+   */
+  doValueUpdate_(newValue) {
+    if (newValue === '???') {
+      this.value_ = newValue;
+      this.isDirty_ = true;
+      this.selectedOption_ = ['???', ''];
+    } else {
+      super.doValueUpdate_(newValue);
+    }
   }
 }
