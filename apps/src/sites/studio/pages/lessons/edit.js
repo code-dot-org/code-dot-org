9d9d325215779ea0f8672df371917347fe13c59c
Dani McAvoy <danimcavoy@gmail.com>
2020-09-11 17:13:29 -0400

    Add ActivitiesEditor to Lesson edit page with fake data

--- apps/src/sites/studio/pages/lessons/edit.js
+++ apps/src/sites/studio/pages/lessons/edit.js
@@ -2,14 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import getScriptData from '@cdo/apps/util/getScriptData';
 import LessonEditor from '@cdo/apps/lib/levelbuilder/lesson-editor/LessonEditor';
+import {getStore, registerReducers} from '@cdo/apps/redux';
+import reducers, {
+  init
+} from '@cdo/apps/lib/levelbuilder/lesson-editor/activitiesEditorRedux';
+import {Provider} from 'react-redux';
+
+//TODO Remove once we hook up real activity data
+import {
+  activities,
+  levelKeyList
+} from '../../../../lib/levelbuilder/lesson-editor/ActivitiesEditor.story';
 
 $(document).ready(function() {
   const lessonData = getScriptData('lesson');
+
+  registerReducers({...reducers});
+  const store = getStore();
+
+  //TODO Switch to using real data once we have activity data
+  store.dispatch(init(activities, levelKeyList));
+
   ReactDOM.render(
-    <LessonEditor
-      displayName={lessonData.name}
-      overview={lessonData.overview}
-    />,
+    <Provider store={store}>
+      <LessonEditor
+        displayName={lessonData.name}
+        overview={lessonData.overview}
+      />
+    </Provider>,
     document.getElementById('edit-container')
   );
 });
