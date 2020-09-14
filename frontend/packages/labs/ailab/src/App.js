252d04d98a4fb03559e7c88d5313e610a6b4b99b
Erin Bond <erin.bond@code.org>
2020-09-14 14:48:40 -0400

    add import dataset button

--- frontend/packages/labs/ailab/src/App.js
+++ frontend/packages/labs/ailab/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import Button from "./components/Button";
 
 export default class App extends Component {
+  onClickImportDataset = () => {
+    alert("You clicked the button!");
+  };
+
   render() {
     return (
       <div>
         <h1>This is a React component!</h1>
         <h3>Sit tight, there's more ML stuff coming soon.</h3>
+        <Button text="import dataset" onClick={this.onClickImportDataset} />
       </div>
     );
   }
