68acc015ad350cafba8c738bbd07b7dddd590015
Charlie Forkish <cforkish@gmail.com>
2020-09-15 13:50:21 -0700

    remove unused user providers from current user redux now that we're getting that straight from the script data

--- apps/src/sites/studio/pages/scripts/show.js
+++ apps/src/sites/studio/pages/scripts/show.js
@@ -4,7 +4,6 @@ import {
   setVerified,
   setVerifiedResources
 } from '@cdo/apps/code-studio/verifiedTeacherRedux';
-import {setUserProviders} from '@cdo/apps/templates/currentUserRedux';
 import {getStore} from '@cdo/apps/code-studio/redux';
 import {registerReducers} from '@cdo/apps/redux';
 import plcHeaderReducer, {
@@ -41,10 +40,6 @@ function initPage() {
 
   if (scriptData.is_verified_teacher) {
     store.dispatch(setVerified());
-  }
-
-  if (scriptData.user_providers) {
-    store.dispatch(setUserProviders(scriptData.user_providers));
   }
 
   if (scriptData.script_announcements) {
