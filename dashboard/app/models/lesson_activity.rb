f5541e820d2c368c233140465209a2ac02643bb0
David Bailey <davidsbailey@users.noreply.github.com>
2020-09-18 08:42:10 -0700

    add seeding_key to models and factories

--- dashboard/app/models/lesson_activity.rb
+++ dashboard/app/models/lesson_activity.rb
@@ -2,16 +2,18 @@
 #
 # Table name: lesson_activities
 #
-#  id         :integer          not null, primary key
-#  lesson_id  :integer          not null
-#  position   :integer          not null
-#  properties :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  lesson_id   :integer          not null
+#  seeding_key :string(255)      not null
+#  position    :integer          not null
+#  properties  :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 # Indexes
 #
-#  index_lesson_activities_on_lesson_id  (lesson_id)
+#  index_lesson_activities_on_lesson_id    (lesson_id)
+#  index_lesson_activities_on_seeding_key  (seeding_key) UNIQUE
 #
 
 class LessonActivity < ApplicationRecord
