bd6d359f2182f55d6948177e9dce9fed98340bbe
David Bailey <davidsbailey@users.noreply.github.com>
2020-09-18 08:33:21 -0700

    add seeding_key to lesson_activities and activity_sections

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -30,11 +30,13 @@ ActiveRecord::Schema.define(version: 20200916194417) do
 
   create_table "activity_sections", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "lesson_activity_id", null: false
+    t.string   "seeding_key",        null: false, comment: "unique key which is stable across environments for seeding purposes"
     t.integer  "position",           null: false
     t.string   "properties"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.index ["lesson_activity_id"], name: "index_activity_sections_on_lesson_activity_id", using: :btree
+    t.index ["seeding_key"], name: "index_activity_sections_on_seeding_key", unique: true, using: :btree
   end
 
   create_table "ap_cs_offerings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
@@ -529,12 +531,14 @@ ActiveRecord::Schema.define(version: 20200916194417) do
   end
 
   create_table "lesson_activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
-    t.integer  "lesson_id",  null: false
-    t.integer  "position",   null: false
+    t.integer  "lesson_id",   null: false
+    t.string   "seeding_key", null: false, comment: "unique key which is stable across environments for seeding purposes"
+    t.integer  "position",    null: false
     t.string   "properties"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.index ["lesson_id"], name: "index_lesson_activities_on_lesson_id", using: :btree
+    t.index ["seeding_key"], name: "index_lesson_activities_on_seeding_key", unique: true, using: :btree
   end
 
   create_table "lesson_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
