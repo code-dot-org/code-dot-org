d1377a61367ea17c486bbc8e6c6c5b5ae44c90d2
Dani <dani@code.org>
2020-09-18 14:23:58 -0400

    Add tests for checking that Announcements work on Lesson Overview

--- apps/test/unit/templates/lessonOverview/LessonOverviewTest.js
+++ apps/test/unit/templates/lessonOverview/LessonOverviewTest.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {expect} from '../../../util/reconfiguredChai';
+import {assert, expect} from '../../../util/reconfiguredChai';
 import {UnconnectedLessonOverview as LessonOverview} from '@cdo/apps/templates/lessonOverview/LessonOverview';
 import {sampleActivities} from '../../lib/levelbuilder/lesson-editor/activitiesTestData';
+import {VisibilityType} from '@cdo/apps/code-studio/announcementsRedux';
+import {ViewType} from '@cdo/apps/code-studio/viewAsRedux';
+import {NotificationType} from '@cdo/apps/templates/Notification';
+
+const fakeTeacherAnnouncement = {
+  notice: 'Notice - Teacher',
+  details: 'Teachers are the best',
+  link: '/foo/bar/teacher',
+  type: NotificationType.information,
+  visibility: VisibilityType.teacher
+};
+const fakeStudentAnnouncement = {
+  notice: 'Notice - Student',
+  details: 'Students are the best',
+  link: '/foo/bar/student',
+  type: NotificationType.information,
+  visibility: VisibilityType.student
+};
+const fakeTeacherAndStudentAnnouncement = {
+  notice: 'Notice - Teacher And Student',
+  details: 'More detail here',
+  link: '/foo/bar/teacherAndStudent',
+  type: NotificationType.information,
+  visibility: VisibilityType.teacherAndStudent
+};
 
 describe('LessonOverview', () => {
   let defaultProps;
@@ -12,7 +37,7 @@ describe('LessonOverview', () => {
       overview: 'Lesson Overview',
       activities: [],
       announcements: [],
-      viewAs: 'Teacher',
+      viewAs: ViewType.Teacher,
       isSignedIn: true,
       purpose: 'The purpose of the lesson is for people to learn',
       preparation: '- One'
@@ -34,5 +59,34 @@ describe('LessonOverview', () => {
       <LessonOverview {...defaultProps} activities={sampleActivities} />
     );
     expect(wrapper.find('Activity').length).to.equal(1);
+  });
+
+  it('has no announcements if none provided', () => {
+    const wrapper = shallow(<LessonOverview {...defaultProps} />);
+    assert.equal(wrapper.find('Announcements').props().announcements.length, 0);
+  });
+
+  it('has provided teacher announcements if necessary', () => {
+    const wrapper = shallow(
+      <LessonOverview
+        {...defaultProps}
+        announcements={[
+          fakeTeacherAnnouncement,
+          fakeTeacherAndStudentAnnouncement
+        ]}
+      />
+    );
+    assert.equal(wrapper.find('Announcements').props().announcements.length, 2);
+  });
+
+  it('has student announcement if viewing as student', () => {
+    const wrapper = shallow(
+      <LessonOverview
+        {...defaultProps}
+        viewAs={ViewType.Student}
+        announcements={[fakeStudentAnnouncement]}
+      />
+    );
+    assert.equal(wrapper.find('Announcements').props().announcements.length, 1);
   });
 });
