ddb077d13d95070bf2ecbd61d1f0a0392d0a3f7b
Jessica Kulwik <jessica@code.org>
2020-09-19 00:47:50 +0000

    Updates from Code Review comments

--- apps/src/code-studio/progress.js
+++ apps/src/code-studio/progress.js
@@ -288,15 +288,6 @@ function initializeStoreWithProgress(
   // Determine if we are viewing student progress.
   var isViewingStudentAnswer = !!clientState.queryParams('user_id');
 
-  // Merge in progress saved on the client, unless we are viewing student
-  // work or are logged in.
-  const useDbProgress = store.getState().progress.shouldUseDbProgress;
-  if (!(isViewingStudentAnswer || useDbProgress)) {
-    store.dispatch(
-      mergeProgress(clientState.allLevelsProgress()[scriptData.name] || {})
-    );
-  }
-
   if (scriptData.hideable_lessons) {
     // Note: This call is async
     store.dispatch(getHiddenStages(scriptData.name, true));
@@ -304,19 +295,31 @@ function initializeStoreWithProgress(
 
   store.dispatch(setIsAge13Required(scriptData.age_13_required));
 
-  // Write progress locally unless we're a teacher viewing student work
-  if (!isViewingStudentAnswer) {
-    let lastProgress;
-    store.subscribe(() => {
-      const progressState = store.getState().progress;
-      const nextProgress = progressState.levelProgress;
-      const shouldUseDbProgress = progressState.shouldUseDbProgress;
-
-      // skip if user is logged in or progress hasn't changed
-      if (!(shouldUseDbProgress || nextProgress === lastProgress)) {
-        lastProgress = nextProgress;
-        clientState.batchTrackProgress(scriptData.name, nextProgress);
-      }
-    });
+  // The rest of these actions are only relevant if the current user is the
+  // owner of the code being viewed.
+  if (isViewingStudentAnswer) {
+    return;
   }
+
+  // We should use client state XOR database state to track user progress
+  if (!store.getState().progress.usingDbProgress) {
+    store.dispatch(
+      mergeProgress(clientState.allLevelsProgress()[scriptData.name] || {})
+    );
+  }
+
+  let lastProgress;
+  store.subscribe(() => {
+    const progressState = store.getState().progress;
+    const nextProgress = progressState.levelProgress;
+    const usingDbProgress = progressState.usingDbProgress;
+
+    // We should use client state XOR database state to track user progress.
+    // We may not know until much later, when we load the app, that there
+    // is state available from the DB.
+    if (!usingDbProgress && nextProgress !== lastProgress) {
+      lastProgress = nextProgress;
+      clientState.batchTrackProgress(scriptData.name, nextProgress);
+    }
+  });
 }
