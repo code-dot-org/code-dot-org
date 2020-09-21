46ab068c36de532ab68d42528a21c169f35deae0
Dani <dani@code.org>
2020-09-21 07:49:24 -0400

    Make section headers on LessonEditor Page collapsible

---
+++ apps/src/lib/levelbuilder/CollapsibleEditorSection.jsx
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import FontAwesome from '@cdo/apps/templates/FontAwesome';
+
+const styles = {
+  header: {
+    borderBottom: '1px solid black'
+  },
+  icon: {
+    marginRight: 10
+  },
+  editors: {
+    padding: 10
+  }
+};
+
+export default class CollapsibleEditorSection extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    collapsed: PropTypes.bool,
+    children: PropTypes.any
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collapsed: this.props.collapsed || false //Default to open if nothing provided
+    };
+  }
+
+  render() {
+    const {title} = this.props;
+    return (
+      <div>
+        <div style={styles.header}>
+          <h2>
+            <FontAwesome
+              icon={this.state.collapsed ? 'expand' : 'compress'}
+              onClick={() => {
+                this.setState({
+                  collapsed: !this.state.collapsed
+                });
+              }}
+              style={styles.icon}
+            />
+            {title}
+          </h2>
+        </div>
+        <div style={styles.editors} hidden={this.state.collapsed}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
