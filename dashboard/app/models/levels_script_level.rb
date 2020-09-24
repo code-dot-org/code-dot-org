5dcde82a8940c865995043ff625b2f8517e1bb0b
Winter Dong <winter@code.org>
2020-09-23 21:36:44 -0700

    Add comments

--- dashboard/app/models/levels_script_level.rb
+++ dashboard/app/models/levels_script_level.rb
@@ -13,11 +13,19 @@
 #
 
 # Join table.
-# Don't add anything to this model; used for convenience for ActiveRecord Import.
+# Don't add anything to this model, beyond what's needed for serialization and seeding; used for convenience for ActiveRecord Import.
 class LevelsScriptLevel < ActiveRecord::Base
   belongs_to :script_level
   belongs_to :level
 
+  # Used for seeding from JSON. Returns the full set of information needed to uniquely identify this object.
+  # If the attributes of this object alone aren't sufficient, and associated objects are needed, then data from
+  # the seeding_keys of those objects should be included as well.
+  # Ideally should correspond to a unique index for this model's table.
+  # See comments on ScriptSeed.seed_from_json for more context.
+  #
+  # @param [ScriptSeed::SeedContext] seed_context - contains preloaded data to use when looking up associated objects
+  # @return [Hash<String, String] all information needed to uniquely identify this object across environments.
   def seeding_key(seed_context)
     my_level = seed_context.levels.select {|l| l.id == level_id}.first
     raise "No Level found for #{self.class}: #{inspect}" unless my_level
