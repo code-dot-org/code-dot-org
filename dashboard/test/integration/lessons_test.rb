95b9f72b1c5eb647a8a17e9551496276341244bc
David Bailey <davidsbailey@users.noreply.github.com>
2020-09-24 17:37:06 -0700

    fix lessons integration test

--- dashboard/test/integration/lessons_test.rb
+++ dashboard/test/integration/lessons_test.rb
@@ -31,21 +31,23 @@ class LessonsTest < ActionDispatch::IntegrationTest
     get edit_lesson_path(id: @lesson.id)
     assert_response :success
     assert_select 'script[data-lesson]', 1
-    lesson_data = JSON.parse(css_select('script[data-lesson]').first.attribute('data-lesson').to_s)['lesson']
-    assert_equal 'lesson overview', lesson_data['overview']
-    assert_equal 'student overview', lesson_data['studentOverview']
+    lesson_data = JSON.parse(css_select('script[data-lesson]').first.attribute('data-lesson').to_s)
+    editable_data = lesson_data['editableData']
+    assert_equal 'lesson overview', editable_data['overview']
+    assert_equal 'student overview', editable_data['studentOverview']
   end
 
   test 'update lesson using data from edit page' do
     sign_in @levelbuilder
     get edit_lesson_path(id: @lesson.id)
     assert_response :success
-    lesson_data = JSON.parse(css_select('script[data-lesson]').first.attribute('data-lesson').to_s)['lesson']
-    lesson_data['studentOverview'] = 'new student overview'
+    lesson_data = JSON.parse(css_select('script[data-lesson]').first.attribute('data-lesson').to_s)
+    editable_data = lesson_data['editableData']
+    editable_data['studentOverview'] = 'new student overview'
 
     # Make sure the update api accepts the data in the same format as
     # the data in the edit response.
-    patch lesson_path(id: @lesson.id, as: :json, params: lesson_data)
+    patch lesson_path(id: @lesson.id, as: :json, params: editable_data)
     @lesson.reload
     assert_equal 'lesson overview', @lesson.overview
     assert_equal 'new student overview', @lesson.student_overview
