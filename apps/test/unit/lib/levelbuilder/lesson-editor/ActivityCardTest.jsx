7a9609c9165c1423aa7c03f7d2bdd58cb77b1850
David Bailey <davidsbailey@users.noreply.github.com>
2020-09-25 07:13:24 -0700

    fix ActivityCardTest syntax

--- apps/test/unit/lib/levelbuilder/lesson-editor/ActivityCardTest.jsx
+++ apps/test/unit/lib/levelbuilder/lesson-editor/ActivityCardTest.jsx
@@ -36,8 +36,8 @@ describe('ActivityCard', () => {
 
   it('renders default props', () => {
     const wrapper = shallow(<ActivityCard {...defaultProps} />);
-    expect(wrapper.contains('Activity:'));
-    expect(wrapper.contains('Time (mins):'));
+    expect(wrapper.contains('Activity:')).to.be.true;
+    expect(wrapper.contains('Time:')).to.be.true;
     expect(wrapper.find('OrderControls').length).to.equal(1);
     expect(wrapper.find('Connect(ActivitySectionCard)').length).to.equal(3);
     expect(wrapper.find('button').length).to.equal(1);
