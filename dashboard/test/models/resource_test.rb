8dab3f894bd3aa7c703ce5997cd2fa807d6b8a4c
Bethany Connor <bethany@code.org>
2020-09-29 15:54:28 +0000

    Add a very basic test

--- dashboard/test/models/resource_test.rb
+++ dashboard/test/models/resource_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ResourceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create resource" do
+    resource = create :resource, key: 'my key'
+    assert_equal 'my key', resource.key
+  end
 end
