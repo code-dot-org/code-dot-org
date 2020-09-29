34046ab026c91d23b04d1a432a28447e8b321197
Bethany Connor <bethany@code.org>
2020-09-29 15:37:27 +0000

    Migration changes from PR feedback

--- dashboard/app/models/resource.rb
+++ dashboard/app/models/resource.rb
@@ -4,11 +4,15 @@
 #
 #  id         :integer          not null, primary key
 #  name       :string(255)
-#  url        :string(255)
-#  embed_slug :string(255)
+#  url        :string(255)      not null
+#  key        :string(255)      not null
 #  properties :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_resources_on_key  (key) UNIQUE
 #
 
 class Resource < ApplicationRecord
