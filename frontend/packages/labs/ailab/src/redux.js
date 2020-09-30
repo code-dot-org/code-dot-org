7cb55468b4abd4713c9f166425266c95fbbb1c91
Erin Bond <erin.bond@code.org>
2020-09-29 20:53:53 -0400

    have user indicate what kind of data is in each column

--- frontend/packages/labs/ailab/src/redux.js
+++ frontend/packages/labs/ailab/src/redux.js
@@ -1,6 +1,7 @@
 // Action types
 
 const SET_IMPORTED_DATA = "SET_IMPORTED_DATA";
+const SET_METADATA_BY_COLUMN = "SET_METADATA_BY_COLUMN";
 const SET_SELECTED_FEATURES = "SET_SELECTED_FEATURES";
 const SET_LABEL_COLUMN = "SET_LABEL_COLUMN";
 const SET_SHOW_PREDICT = "SET_SHOW_PREDICT";
@@ -12,6 +13,13 @@ const SET_PREDICTION = "SET_PREDICTION";
 export function setImportedData(data) {
   return { type: SET_IMPORTED_DATA, data };
 }
+
+export const setMetaDataByColumn = (column, metadataField, value) => ({
+  type: SET_METADATA_BY_COLUMN,
+  column,
+  metadataField,
+  value
+});
 
 export function setSelectedFeatures(selectedFeatures) {
   return { type: SET_SELECTED_FEATURES, selectedFeatures };
@@ -34,7 +42,8 @@ export function setPrediction(prediction) {
 }
 
 const initialState = {
-  data: undefined,
+  data: [],
+  metaDataByColumn: {},
   selectedFeatures: [],
   labelColumn: undefined,
   showPredict: false,
@@ -49,6 +58,21 @@ export default function rootReducer(state = initialState, action) {
     return {
       ...state,
       data: action.data
+    };
+  }
+  if (action.type === SET_METADATA_BY_COLUMN) {
+    const prevMetaData = state.metaDataByColumn[action.column]
+      ? state.metaDataByColumn[action.column][action.metadataField]
+      : {};
+    return {
+      ...state,
+      metaDataByColumn: {
+        ...state.metaDataByColumn,
+        [action.column]: {
+          ...state.metaDataByColumn[action.column],
+          [action.metadataField]: action.value
+        }
+      }
     };
   }
   if (action.type === SET_SELECTED_FEATURES) {
@@ -82,4 +106,8 @@ export default function rootReducer(state = initialState, action) {
     };
   }
   return state;
+}
+
+export function getFeatures(state) {
+  return state.data.length > 0 ? Object.keys(state.data[0]) : [];
 }
