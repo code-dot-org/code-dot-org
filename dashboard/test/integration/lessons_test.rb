74d65e3cc44a70558fbff19f64337a6df91ffdaa
David Bailey <davidsbailey@users.noreply.github.com>
2020-09-30 20:13:48 -0700

    expand lesson integration tests to include activity data

--- dashboard/test/integration/lessons_test.rb
+++ dashboard/test/integration/lessons_test.rb
@@ -13,6 +13,15 @@ class LessonsTest < ActionDispatch::IntegrationTest
       }
     )
 
+    @activity = create(
+      :lesson_activity,
+      lesson: @lesson,
+      name: 'my activity',
+      duration: 57
+    )
+
+    assert_equal @activity, @lesson.lesson_activities.first
+
     @levelbuilder = create :levelbuilder
   end
 
@@ -33,6 +42,10 @@ class LessonsTest < ActionDispatch::IntegrationTest
     editable_data = lesson_data['editableData']
     assert_equal 'lesson overview', editable_data['overview']
     assert_equal 'student overview', editable_data['studentOverview']
+    activities_data = editable_data['activities']
+    assert_equal 1, activities_data.count
+    assert_equal 'my activity', activities_data.first['name']
+    assert_equal 57, activities_data.first['duration']
   end
 
   test 'update lesson using data from edit page' do
@@ -42,12 +55,32 @@ class LessonsTest < ActionDispatch::IntegrationTest
     lesson_data = JSON.parse(css_select('script[data-lesson]').first.attribute('data-lesson').to_s)
     editable_data = lesson_data['editableData']
     editable_data['studentOverview'] = 'new student overview'
+    activity_data = editable_data['activities'].first
+    activity_data['name'] = 'new activity name'
+    activity_data['duration'] = 58
+
+    # This part of the edit/update API is not symmetric, because the update
+    # API expects the activities field to be JSON-encoded.
+    #
+    # We don't want the update API to expect these without JSON-encoding, because
+    # we would lose the type information on nested integer values, which would be
+    # converted into strings by ActionController::Parameters. For example, see:
+    # https://stackoverflow.com/questions/59124523/actioncontrollerparameters-converts-integer-to-string
+    #
+    # To attain symmetry, the edit API could send JSON-encoded activities, but
+    # then the client would still have to JSON.parse before making any edits and
+    # then re-encoding them with JSON.stringify, which doesn't seem good either.
+    editable_data['activities'] = editable_data['activities'].to_json
 
     # Make sure the update api accepts the data in the same format as
     # the data in the edit response.
     patch lesson_path(id: @lesson.id, as: :json, params: editable_data)
+    assert_response :redirect
     @lesson.reload
     assert_equal 'lesson overview', @lesson.overview
     assert_equal 'new student overview', @lesson.student_overview
+    @activity.reload
+    assert_equal 'new activity name', @activity.name
+    assert_equal 58, @activity.duration
   end
 end
