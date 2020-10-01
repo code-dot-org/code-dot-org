aeb387a6d9d0e4c5ed1572490df9a5198755449b
Bethany Connor <bethany@code.org>
2020-10-01 16:53:29 +0000

    Migrations for resource/course version association

--- dashboard/app/models/resource.rb
+++ dashboard/app/models/resource.rb
@@ -2,17 +2,18 @@
 #
 # Table name: resources
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  url        :string(255)      not null
-#  key        :string(255)      not null
-#  properties :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                :integer          not null, primary key
+#  name              :string(255)
+#  url               :string(255)      not null
+#  key               :string(255)      not null
+#  properties        :string(255)
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  course_version_id :integer
 #
 # Indexes
 #
-#  index_resources_on_key  (key) UNIQUE
+#  index_resources_on_course_version_id_and_key  (course_version_id,key) UNIQUE
 #
 
 # A Resource represents a link to external material for a lesson
