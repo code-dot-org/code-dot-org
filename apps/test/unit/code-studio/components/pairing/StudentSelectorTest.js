50b08c9177b2cbc2a578e8cfcc798f0246ef793f
drew-beckmen <drew.beckmen@yale.edu>
2020-10-02 00:16:08 +0000

    test button class names and styles

--- apps/test/unit/code-studio/components/pairing/StudentSelectorTest.js
+++ apps/test/unit/code-studio/components/pairing/StudentSelectorTest.js
@@ -38,5 +38,32 @@ describe('StudentSelector', () => {
         })
       ).to.have.lengthOf(1);
     });
+
+    it('assigns class names to buttons correctly', () => {
+      const wrapper = shallow(
+        <StudentSelector students={students} handleSubmit={() => {}} />
+      );
+      expect(wrapper.find('.selectable')).to.have.lengthOf(5);
+      wrapper.setState({selectedStudentIds: [1, 2, 3]});
+      expect(wrapper.find('.selectable')).to.have.lengthOf(2);
+      expect(wrapper.find('.selected')).to.have.lengthOf(3);
+    });
+
+    it('assigns updated styles to clicked buttons', () => {
+      const wrapper = shallow(
+        <StudentSelector students={students} handleSubmit={() => {}} />
+      );
+      let btnBackground = wrapper
+        .find('button')
+        .at(0)
+        .props().style.backgroundColor;
+      expect(btnBackground).to.equal('white');
+      wrapper.setState({selectedStudentIds: [1]});
+      btnBackground = wrapper
+        .find('button')
+        .at(0)
+        .props().style.backgroundColor;
+      expect(btnBackground).to.equal('#0094ca');
+    });
   });
 });
