ab7ee8a4cb56a627f41c64502d5463f22e265623
David Bailey <davidsbailey@users.noreply.github.com>
2020-10-02 10:09:52 -0700

    move update_activities into Lesson

--- dashboard/app/controllers/lessons_controller.rb
+++ dashboard/app/controllers/lessons_controller.rb
@@ -24,7 +24,7 @@ class LessonsController < ApplicationController
   # PATCH/PUT /lessons/1
   def update
     @lesson.update!(lesson_params)
-    update_activities(JSON.parse(params[:activities])) if params[:activities]
+    @lesson.update_activities(JSON.parse(params[:activities])) if params[:activities]
 
     redirect_to lesson_path(id: @lesson.id)
   end
@@ -54,38 +54,5 @@ class LessonsController < ApplicationController
     )
     lp[:announcements] = JSON.parse(lp[:announcements]) if lp[:announcements]
     lp
-  end
-
-  # @param activities [Array<Hash>]
-  def update_activities(activities)
-    return unless activities
-    # use assignment to delete any missing activities.
-    @lesson.lesson_activities = activities.map do |activity|
-      lesson_activity = fetch_activity(activity)
-      lesson_activity.update!(
-        position: activity['position'],
-        name: activity['name'],
-        duration: activity['duration']
-      )
-
-      lesson_activity.update_activity_sections(activity['activitySections'])
-      lesson_activity
-    end
-  end
-
-  # Finds the LessonActivity by id, or creates a new one if id is not specified.
-  # @param activity [Hash]
-  # @returns [LessonActivity]
-  def fetch_activity(activity)
-    if activity['id']
-      lesson_activity = @lesson.lesson_activities.find(activity['id'])
-      raise "LessonActivity id #{activity['id']} not found in Lesson id #{@lesson.id}" unless lesson_activity
-      return lesson_activity
-    end
-
-    @lesson.lesson_activities.create(
-      position: activity['position'],
-      seeding_key: SecureRandom.uuid
-    )
   end
 end
