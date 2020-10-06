7dfc9e21e895c5478d2786ab9ffccc0c7b33298a
Bethany Connor <bethany@code.org>
2020-10-06 15:41:18 +0000

    Run Mapbox alongside Google Maps

--- apps/src/sites/code.org/pages/views/workshop_search.js
+++ apps/src/sites/code.org/pages/views/workshop_search.js
@@ -1,16 +1,37 @@
-/* globals mapboxgl, MapboxGeocoder */
+/* globals google, mapboxgl, MapboxGeocoder */
 
 import $ from 'jquery';
 import colors from '@cdo/apps/util/color';
+import MarkerClusterer from 'node-js-marker-clusterer';
+import getScriptData from '@cdo/apps/util/getScriptData';
+import {SubjectNames} from '@cdo/apps/generated/pd/sharedWorkshopConstants';
 
-var map;
+const markerCustererOptions = {
+  imagePath: getScriptData('workshopSearch').imagePath,
+  gridSize: 10
+};
+
+var map,
+  markersByLocation = {},
+  infoWindow,
+  markerClusterer;
 
 $(document).ready(function() {
-  initializeMap();
-  map.on('load', loadWorkshops);
+  if (window.location.search.includes('mapbox')) {
+    $('#gmap').hide();
+    $('#mapbox-map').show();
+    $('#geocomplete').hide();
+    $('#geocomplete-form').hide();
+    $('#mapboxgeocoder').show();
+    initializeMapboxMap();
+    map.on('load', loadMapboxWorkshops);
+  } else {
+    initializeGoogleMap();
+    loadGoogleMapWorkshops();
+  }
 });
 
-function initializeMap() {
+function initializeMapboxMap() {
   map = new mapboxgl.Map({
     container: 'mapbox-map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -40,14 +61,14 @@ function initializeMap() {
     marker: false,
     types: 'country,region,district,postcode,locality,place'
   });
-  document.getElementById('geocomplete').appendChild(geocoder.onAdd(map));
+  document.getElementById('mapboxgeocoder').appendChild(geocoder.onAdd(map));
 }
 
-function loadWorkshops() {
+function loadMapboxWorkshops() {
   const deepDiveOnly = $('#properties').attr('data-deep-dive-only');
-  let url = '/dashboardapi/v1/pd/k5workshops';
+  let url = '/dashboardapi/v1/pd/k5workshops?geojson=1';
   if (deepDiveOnly !== undefined) {
-    url += '?deep_dive_only=1';
+    url += '&deep_dive_only=1';
   }
   map.addSource('workshops', {
     type: 'geojson',
@@ -182,7 +203,7 @@ function placeDeepDiveWorkshops() {
 function onMarkerClick(e) {
   var coordinates = e.features[0].geometry.coordinates.slice();
   var workshops = JSON.parse(e.features[0].properties['workshops']);
-  const description = compileHtml(workshops, false);
+  const description = compileMapboxPopupHtml(workshops, false);
 
   new mapboxgl.Popup()
     .setLngLat(coordinates)
@@ -190,7 +211,7 @@ function onMarkerClick(e) {
     .addTo(map);
 }
 
-function compileHtml(workshops, first) {
+function compileMapboxPopupHtml(workshops, first) {
   // Compile HTML.
   var html = '';
 
@@ -233,4 +254,187 @@ function compileHtml(workshops, first) {
   html += '</div>';
 
   return html;
+}
+
+function initializeGoogleMap() {
+  var mapOptions = {
+    center: new google.maps.LatLng(37.6, -95.665),
+    zoom: 4,
+    minZoom: 2,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  map = new google.maps.Map(document.getElementById('gmap'), mapOptions);
+  infoWindow = new google.maps.InfoWindow();
+}
+
+function loadGoogleMapWorkshops() {
+  markerClusterer = new MarkerClusterer(map, [], markerCustererOptions);
+
+  const deepDiveOnly = $('#properties').attr('data-deep-dive-only');
+  let url = '/dashboardapi/v1/pd/k5workshops';
+  if (deepDiveOnly !== undefined) {
+    url += '?deep_dive_only=1';
+  }
+
+  $.get(url)
+    .done(function(data) {
+      processPdWorkshops(data);
+    })
+    .always(completeProcessingPdWorkshops);
+}
+
+function processPdWorkshops(workshops) {
+  $.each(workshops, function(i, workshop) {
+    var location = workshop.processed_location;
+    var latLng = new google.maps.LatLng(location.latitude, location.longitude);
+    var hash = latLng.toUrlValue();
+
+    var infoWindowContent = '';
+
+    if (markersByLocation[hash] === undefined) {
+      infoWindowContent = compileHtml(workshop, true);
+      markersByLocation[hash] = createNewMarker(
+        latLng,
+        workshop.location_name,
+        infoWindowContent,
+        workshop.subject
+      );
+    } else {
+      // Extend existing marker.
+      infoWindowContent = compileHtml(workshop, false);
+      markersByLocation[hash].infoWindowContent += infoWindowContent;
+      // Upgrade any marker containing a deep dive workshop to the deep dive icon
+      if (workshop.subject === SubjectNames.SUBJECT_CSF_201) {
+        markersByLocation[hash].icon = iconForSubject(workshop.subject);
+      }
+    }
+  });
+}
+
+function createNewMarker(latLng, title, infoWindowContent, subject) {
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    title: title,
+    infoWindowContent: infoWindowContent,
+    icon: iconForSubject(subject)
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(this.get('infoWindowContent'));
+    infoWindow.open(map, this);
+  });
+  markerClusterer.addMarker(marker);
+  return marker;
+}
+
+const iconForSubject = subject => ({
+  url:
+    subject === SubjectNames.SUBJECT_CSF_201
+      ? 'https://maps.google.com/mapfiles/kml/paddle/red-stars.png'
+      : 'https://maps.google.com/mapfiles/kml/paddle/red-blank.png',
+  scaledSize: new google.maps.Size(40, 40)
+});
+
+function completeProcessingPdWorkshops() {
+  addGeocomplete();
+}
+
+function compileHtml(workshop, first) {
+  // Compile HTML.
+  var html = '';
+
+  if (first) {
+    html += '<div class="workshop-item workshop-item-first">';
+  } else {
+    html += '<div class="workshop-item">';
+  }
+  html +=
+    '<div class="workshop-location-name"><strong>' +
+    workshop.location_name +
+    '</strong></div>';
+
+  // Add the workshop subject
+  html +=
+    '<div class="workshop-subject">' + workshop.subject + ' Workshop</div>';
+
+  // Add the date(s).
+  html += '<div class="workshop-dates">';
+  $.each(workshop.sessions, function(i, session) {
+    html +=
+      '<div class="workshop-date" style="white-space: nowrap;">' +
+      session +
+      '</div>';
+  });
+  html += '</div>';
+
+  var code_studio_root = $('#properties').attr('data-studio-url');
+  var url = code_studio_root + '/pd/workshops/' + workshop.id + '/enroll';
+  if (workshop.id) {
+    html +=
+      '<div class="workshop-link"><a style="" href=' +
+      url +
+      '>Info and Signup</a></div>';
+  }
+  html += '</div>';
+
+  return html;
+}
+
+function addGeocomplete() {
+  var geocomplete_options = {
+    country: 'us'
+  };
+
+  if (html5_storage_supported() && localStorage['geocomplete'] !== undefined) {
+    geocomplete_options.location = localStorage['geocomplete'];
+  }
+
+  $('#geocomplete')
+    .geocomplete(geocomplete_options)
+    .bind('geocode:result', function(event, result) {
+      map.fitBounds(result.geometry.viewport);
+
+      var bounds = map.getBounds();
+      var marker_found = false;
+
+      while (!marker_found && map.getZoom() > 4) {
+        $.each(markersByLocation, function(index, marker) {
+          if (bounds.contains(marker.getPosition())) {
+            marker_found = true;
+          }
+        });
+
+        if (!marker_found) {
+          map.setZoom(map.getZoom() - 1);
+          bounds = map.getBounds();
+        }
+      }
+
+      if (html5_storage_supported()) {
+        localStorage['geocomplete'] = result.formatted_address;
+      }
+    });
+
+  $('#btn-submit').click(function() {
+    $('#geocomplete').trigger('geocode');
+  });
+
+  $('#btn-reset').click(function() {
+    $('#geocomplete').val('');
+    map.setCenter(new google.maps.LatLng(37.6, -95.665));
+    map.setZoom(4);
+    infoWindow.close();
+    if (html5_storage_supported()) {
+      localStorage.removeItem('geocomplete');
+    }
+  });
+}
+
+function html5_storage_supported() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
 }
