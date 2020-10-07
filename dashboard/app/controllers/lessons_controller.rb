e9f30c725647962aa4c576a76340ba52e1dcf692
Bethany Connor <bethany@code.org>
2020-10-07 23:33:57 +0000

    Improve comments, add tests, and minor fix

--- dashboard/app/controllers/lessons_controller.rb
+++ dashboard/app/controllers/lessons_controller.rb
@@ -35,17 +35,19 @@ class LessonsController < ApplicationController
 
   # PATCH/PUT /lessons/1
   def update
-    lesson_resource_keys = JSON.parse(lesson_params['resources'])
-    resource_keys_in_lesson = @lesson.resources.map(&:key)
-    resource_keys_to_add = lesson_resource_keys - resource_keys_in_lesson
-    resource_keys_to_add.each do |resource_key|
-      persisted_resource = Resource.find_by_key(resource_key)
-      @lesson.resources << persisted_resource if persisted_resource
-    end
-    resource_keys_to_remove = resource_keys_in_lesson - lesson_resource_keys
-    resource_keys_to_remove.each do |resource_key|
-      persisted_resource = Resource.find_by_key(resource_key)
-      @lesson.resources.delete(persisted_resource) if persisted_resource
+    if lesson_params.key?('resources')
+      lesson_resource_keys = lesson_params['resources'] || []
+      resource_keys_in_lesson = @lesson.resources.map(&:key) || []
+      resource_keys_to_add = lesson_resource_keys - resource_keys_in_lesson
+      resource_keys_to_add.each do |resource_key|
+        persisted_resource = Resource.find_by_key(resource_key)
+        @lesson.resources << persisted_resource if persisted_resource
+      end
+      resource_keys_to_remove = resource_keys_in_lesson - lesson_resource_keys
+      resource_keys_to_remove.each do |resource_key|
+        persisted_resource = Resource.find_by_key(resource_key)
+        @lesson.resources.delete(persisted_resource) if persisted_resource
+      end
     end
     @lesson.update!(lesson_params.except(:resources))
 
@@ -77,6 +79,7 @@ class LessonsController < ApplicationController
       :resources
     )
     lp[:announcements] = JSON.parse(lp[:announcements]) if lp[:announcements]
+    lp[:resources] = JSON.parse(lp[:resources]) if lp[:resources]
     lp
   end
 end
