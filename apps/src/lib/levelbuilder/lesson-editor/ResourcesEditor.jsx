8b5ff4ddbc5115013dfe5176871a6be953701de0
Bethany Connor <bethany@code.org>
2020-10-07 00:09:53 +0000

    Add search functionality to resources editor

--- apps/src/lib/levelbuilder/lesson-editor/ResourcesEditor.jsx
+++ apps/src/lib/levelbuilder/lesson-editor/ResourcesEditor.jsx
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {resourceShape} from '@cdo/apps/lib/levelbuilder/shapes';
+import _ from 'lodash';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import {SelectStyleProps} from '@cdo/apps/code-studio/pd/constants';
 
 export default class ResourcesEditor extends Component {
   static propTypes = {
@@ -10,12 +14,18 @@ export default class ResourcesEditor extends Component {
   constructor(props) {
     super(props);
 
-    console.log(props);
     this.state = {
       resources: props.resources || [],
-      resourceInput: ''
+      resourceInput: '',
+      searchValue: ''
     };
   }
+
+  constructResourceOption = resource => ({
+    value: resource.key.toString(),
+    label: `${resource.name} - ${resource.url}`,
+    resource: resource
+  });
 
   addResource = e => {
     e.preventDefault();
@@ -25,7 +35,59 @@ export default class ResourcesEditor extends Component {
   };
 
   handleResourceInputChange = e => {
-    this.setState({resourceInput: e.target.value});
+    //this.setState({resourceInput: e.target.value});
+    //this.setState({searchValue: e.target.value});
+    var {resources} = this.state;
+    resources.push(e.resource);
+    this.setState({resources});
+  };
+
+  /**
+   * Debounced function that will request school search results from the server.
+   * Because this function is debounced it is not guaranteed to execute
+   * when it is called - there may be a delay of up to 200ms.
+   * @param {string} q - Search query
+   * @param {function(err, result)} callback - Function called when the server
+   *   returns results or a request error occurs.
+   */
+  debouncedSearch = _.debounce((q, callback) => {
+    const searchUrl = `/resourcesearch/${encodeURIComponent(q)}/5`;
+    // Note, we don't return the fetch promise chain because in a debounced
+    // function we're not guaranteed to return anything, and it's not a great
+    // interface to sometimes return undefined when there's still async work
+    // going on.
+    //
+    // We are including the X-Requested-With header to avoid getting a 403
+    // returned by Rack::Protection::JsonCsrf in some environments
+    fetch(searchUrl, {headers: {'X-Requested-With': 'XMLHttpRequest'}})
+      .then(response => (response.ok ? response.json() : []))
+      .then(json => {
+        const resources = json.map(resource =>
+          this.constructResourceOption(resource)
+        );
+        return {options: resources};
+      })
+      .then(result => callback(null, result))
+      .catch(err => callback(err, null));
+  }, 200);
+
+  getOptions = q => {
+    // Search
+    if (q.length < 3) {
+      return Promise.resolve();
+    }
+
+    // Wrap the debounced call in a Promise so we _always_ return a promise
+    // from this function, which resolves whenever results come back.
+    return new Promise((resolve, reject) => {
+      this.debouncedSearch(q, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
   };
 
   render() {
@@ -36,15 +98,17 @@ export default class ResourcesEditor extends Component {
           name="resources"
           value={JSON.stringify(this.state.resources.map(r => r.key))}
         />
-        <label>Select a resource or add a new one</label>
-        <input
-          type="text"
+        <label>Select a resource to add</label>
+        <Select.Async
+          id="resource_search"
+          name="resource_search"
+          loadOptions={this.getOptions}
+          value={this.state.searchValue}
           onChange={this.handleResourceInputChange}
-          value={this.state.resourceInput}
+          onValueClick={this.addResource}
+          placeholder={''}
+          {...SelectStyleProps}
         />
-        <button onClick={this.addResource} type="button">
-          Add
-        </button>
         <table style={{width: '100%'}}>
           <thead>
             <tr>
