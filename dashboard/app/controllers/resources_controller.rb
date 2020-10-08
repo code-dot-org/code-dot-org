07f627752ed19c26a416b07a2279e82d7d6ec330
Bethany Connor <bethany@code.org>
2020-10-08 17:55:53 +0000

    Fix resource search return

--- dashboard/app/controllers/resources_controller.rb
+++ dashboard/app/controllers/resources_controller.rb
@@ -2,6 +2,6 @@ class ResourcesController < ApplicationController
   # GET /resourcesearch/:q/:limit
   def search
     # TODO: use the query to provide real suggestions
-    render json: Resource.limit(params[:limit]).attributes
+    render json: Resource.limit(params[:limit]).map(&:attributes)
   end
 end
