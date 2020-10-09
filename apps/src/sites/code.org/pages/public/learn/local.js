545c5c1c72c1f10db4850f60983712db4889beff
Jillian Khoo <jillian9012@gmail.com>
2020-10-09 18:28:21 +0000

    use star-marker instead of random mapbox marker

--- apps/src/sites/code.org/pages/public/learn/local.js
+++ apps/src/sites/code.org/pages/public/learn/local.js
@@ -156,63 +156,60 @@ function loadMap(locations) {
     center: [lng, lat]
   });
   map.on('load', function() {
-    map.loadImage(
-      'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
-      function(error, image) {
-        if (error) {
-          logToCloud.addPageAction(
-            logToCloud.PageAction.MapboxMarkerLoadError,
-            {
-              error
-            }
-          );
-          throw error;
-        }
-        map.addImage('custom-marker', image);
-        map.addSource('places', {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: featureList
-          }
+    map.loadImage('/images/map-markers/star-marker.png', function(
+      error,
+      image
+    ) {
+      if (error) {
+        logToCloud.addPageAction(logToCloud.PageAction.MapboxMarkerLoadError, {
+          error
         });
-        // Add a layer showing the places.
-        map.addLayer({
-          id: 'places',
-          type: 'symbol',
-          source: 'places',
-          layout: {
-            'icon-image': 'custom-marker',
-            'icon-allow-overlap': true
-          }
-        });
-
-        // When a click event occurs on a feature in the places layer, open a popup at the
-        // location of the feature, with description HTML from its properties.
-        map.on('click', 'places', function(e) {
-          var coordinates = e.features[0].geometry.coordinates.slice();
-
-          // Ensure that if the map is zoomed out such that multiple
-          // copies of the feature are visible, the popup appears
-          // over the copy being pointed to.
-          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-          }
-          resultList(featureList, lng, lat, e.features[0].properties.index + 1);
-          createPopUp(e.features[0]);
-        });
-
-        // Change the cursor to a pointer when the mouse is over the places layer.
-        map.on('mouseenter', 'places', function() {
-          map.getCanvas().style.cursor = 'pointer';
-        });
-
-        // Change it back to a pointer when it leaves.
-        map.on('mouseleave', 'places', function() {
-          map.getCanvas().style.cursor = '';
-        });
+        throw error;
       }
-    );
+      map.addImage('star-marker', image);
+      map.addSource('places', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: featureList
+        }
+      });
+      // Add a layer showing the places.
+      map.addLayer({
+        id: 'places',
+        type: 'symbol',
+        source: 'places',
+        layout: {
+          'icon-image': 'star-marker',
+          'icon-allow-overlap': true
+        }
+      });
+
+      // When a click event occurs on a feature in the places layer, open a popup at the
+      // location of the feature, with description HTML from its properties.
+      map.on('click', 'places', function(e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        resultList(featureList, lng, lat, e.features[0].properties.index + 1);
+        createPopUp(e.features[0]);
+      });
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on('mouseenter', 'places', function() {
+        map.getCanvas().style.cursor = 'pointer';
+      });
+
+      // Change it back to a pointer when it leaves.
+      map.on('mouseleave', 'places', function() {
+        map.getCanvas().style.cursor = '';
+      });
+    });
   });
   // builds the side menu with the list of classes
   if (featureList.length > 0) {
