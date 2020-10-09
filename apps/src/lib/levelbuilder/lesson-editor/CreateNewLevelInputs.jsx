c9018b0f19ae7cefb92ef594857a16ecc89a4ff3
Dani <dani@code.org>
2020-10-08 23:21:25 -0400

    Set up create level to be able to send information to future ajax call

--- apps/src/lib/levelbuilder/lesson-editor/CreateNewLevelInputs.jsx
+++ apps/src/lib/levelbuilder/lesson-editor/CreateNewLevelInputs.jsx
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
-import Button, {ButtonColor} from '@cdo/apps/templates/Button';
+import PropTypes from 'prop-types';
 
-//TODO: Set up creating a new level. Make sure dropdown has all level types
+//TODO: Set up creating a new level.
 
 export default class CreateNewLevelInputs extends Component {
-  static propTypes = {};
+  static propTypes = {
+    levelOptions: PropTypes.array
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      levelName: '',
+      levelType: this.props.levelOptions[0][1]
+    };
+  }
+
+  handleInputChange = event => {
+    this.setState({levelName: event.target.value});
+  };
+
+  handleChangeLevelType = event => {
+    this.setState({levelType: event.target.value});
+  };
+
+  handleCreateLevel = () => {
+    console.log('create level');
+  };
 
   render() {
     return (
@@ -12,30 +35,26 @@ export default class CreateNewLevelInputs extends Component {
         <label>
           Level Type:
           <select
-            onClick={() => {
-              console.log('select type');
-            }}
+            onChange={this.handleChangeLevelType}
+            value={this.state.levelType}
           >
-            <option>App Lab</option>
-            <option>Game Lab</option>
-            <option>Standalone Video</option>
+            {this.props.levelOptions.map(levelType => (
+              <option key={levelType[0]} value={levelType[1]}>
+                {levelType[0]}
+              </option>
+            ))}
           </select>
         </label>
         <label>
           Level Name:
           <input
-            onChange={() => {
-              console.log('Change level name');
-            }}
+            onChange={this.handleInputChange}
+            value={this.state.levelName}
           />
         </label>
-        <Button
-          text="Create & Add"
-          color={ButtonColor.blue}
-          onClick={() => {
-            console.log('create and add level');
-          }}
-        />
+        <button type="button" onClick={this.handleCreateLevel}>
+          <span>Create and Add</span>
+        </button>
       </div>
     );
   }
