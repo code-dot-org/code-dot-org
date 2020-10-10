d8083513641996b5be26d4bfe08b720c27170035
Dani <dani@code.org>
2020-10-10 00:39:59 -0400

    Update LessonEditorTest

--- apps/test/unit/lib/levelbuilder/lesson-editor/LessonEditorTest.js
+++ apps/test/unit/lib/levelbuilder/lesson-editor/LessonEditorTest.js
@@ -67,7 +67,7 @@ describe('LessonEditor', () => {
     ).to.be.true;
     expect(wrapper.find('Connect(ActivitiesEditor)').length).to.equal(1);
     expect(wrapper.find('TextareaWithMarkdownPreview').length).to.equal(4);
-    expect(wrapper.find('input').length).to.equal(23);
+    expect(wrapper.find('input').length).to.equal(24);
     expect(wrapper.find('select').length).to.equal(1);
     expect(wrapper.find('AnnouncementsEditor').length).to.equal(1);
     expect(wrapper.find('CollapsibleEditorSection').length).to.equal(3);
