3f465d21c5f42bac545d9d2985610ed2c9732ed4
Dani <dani@code.org>
2020-10-10 00:50:38 -0400

    update CreateNewLevelInputsTest

--- apps/test/unit/lib/levelbuilder/lesson-editor/CreateNewLevelInputsTest.jsx
+++ apps/test/unit/lib/levelbuilder/lesson-editor/CreateNewLevelInputsTest.jsx
@@ -6,7 +6,9 @@ import CreateNewLevelInputs from '@cdo/apps/lib/levelbuilder/lesson-editor/Creat
 describe('CreateNewLevelInputs', () => {
   let defaultProps;
   beforeEach(() => {
-    defaultProps = {};
+    defaultProps = {
+      levelOptions: [['Applab', 'Applab'], ['Dancelab', 'Dancelab']]
+    };
   });
 
   it('renders default props', () => {
@@ -15,6 +17,23 @@ describe('CreateNewLevelInputs', () => {
     expect(wrapper.contains('Level Name:')).to.be.true;
     expect(wrapper.find('input').length).to.equal(1);
     expect(wrapper.find('select').length).to.equal(1);
-    expect(wrapper.find('Button').length).to.equal(1);
+    expect(wrapper.find('select').props().value).to.equal('Applab');
+    expect(wrapper.find('button').length).to.equal(1);
+  });
+
+  it('select level type', () => {
+    const wrapper = shallow(<CreateNewLevelInputs {...defaultProps} />);
+    expect(wrapper.find('select').length).to.equal(1);
+    expect(wrapper.find('select').props().value).to.equal('Applab');
+    wrapper.find('select').simulate('change', {target: {value: 'Dancelab'}});
+    expect(wrapper.find('select').props().value).to.equal('Dancelab');
+  });
+
+  it('add level name', () => {
+    const wrapper = shallow(<CreateNewLevelInputs {...defaultProps} />);
+    expect(wrapper.find('input').length).to.equal(1);
+    expect(wrapper.find('input').props().value).to.equal('');
+    wrapper.find('input').simulate('change', {target: {value: 'New Level'}});
+    expect(wrapper.find('input').props().value).to.equal('New Level');
   });
 });
