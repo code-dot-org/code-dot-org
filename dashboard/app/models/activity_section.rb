0556e6c952ae3eeb89dc77131a69da3923356449
David Bailey <davidsbailey@users.noreply.github.com>
2020-10-12 16:06:14 -0700

    modify existing script levels on lesson update

--- dashboard/app/models/activity_section.rb
+++ dashboard/app/models/activity_section.rb
@@ -53,4 +53,30 @@ class ActivitySection < ApplicationRecord
       scriptLevels: script_levels.map(&:summarize_for_edit)
     }
   end
+
+  # @param [Array<Hash>] script_levels_data - Data representing script levels
+  #   belonging to this activity section.
+  def update_script_levels(script_levels_data)
+    # We can't assign directly to self.script_levels here like we do in other
+    # places, because we want to preserve any script levels which exist in the
+    # lesson but haven't been associated with an activity section yet.
+
+    script_levels_data.each do |sl_data|
+      # TODO(dave): create new script level if id did not previously exist
+      next unless sl_data['id']
+
+      sl = ScriptLevel.find(sl_data['id'].to_i)
+      unless sl.activity_section == self
+        raise "ScriptLevel #{sl_data['id']} is not owned by ActivitySection #{id}"
+      end
+      sl.update!(
+        position: sl_data['position'],
+        assessment: !!sl_data['assessment'],
+        bonus: !!sl_data['bonus'],
+        challenge: !!sl_data['challenge'],
+      )
+
+      # TODO(dave): check and update script level variants
+    end
+  end
 end
