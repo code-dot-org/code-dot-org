08cd43afc17e76c1149f79530d10d39517b42f01
Bethany Connor <bethany@code.org>
2020-10-14 22:34:08 +0000

    Add a couple of tests and clarify some code

--- dashboard/lib/resources_autocomplete.rb
+++ dashboard/lib/resources_autocomplete.rb
@@ -3,8 +3,8 @@ class ResourcesAutocomplete < AutocompleteHelper
     limit = format_limit(limit)
 
     rows = Resource.limit(limit)
+    return [] if query.length < MIN_WORD_LENGTH
     query = format_query(query)
-    return [] if query.length < MIN_WORD_LENGTH + 2
     rows = rows.
       where("MATCH(name,url) AGAINST(? in BOOLEAN MODE)", query)
     return rows.map(&:attributes)
