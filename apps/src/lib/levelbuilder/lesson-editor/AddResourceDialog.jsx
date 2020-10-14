4f80550f4728c086bd4b69d33eb52a6721391f86
Bethany Connor <bethany@code.org>
2020-10-14 15:35:25 +0000

    Add more fields and add storybook to AddResourceDialog

--- apps/src/lib/levelbuilder/lesson-editor/AddResourceDialog.jsx
+++ apps/src/lib/levelbuilder/lesson-editor/AddResourceDialog.jsx
@@ -10,6 +10,25 @@ const styles = {
     paddingRight: 20,
     paddingBottom: 20,
     fontFamily: '"Gotham 4r", sans-serif, sans-serif'
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  dropdownRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  inputAndLabel: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  textInput: {
+    width: '100%'
+  },
+  selectInput: {
+    width: '45%'
   }
 };
 
@@ -18,7 +37,9 @@ const styles = {
 export default class AddResourceDialog extends Component {
   static propTypes = {
     isOpen: PropTypes.bool.isRequired,
-    handleConfirm: PropTypes.func.isRequired
+    handleConfirm: PropTypes.func.isRequired,
+    typeOptions: PropTypes.arrayOf(PropTypes.string),
+    audienceOptions: PropTypes.arrayOf(PropTypes.string)
   };
 
   render() {
@@ -30,14 +51,46 @@ export default class AddResourceDialog extends Component {
         style={styles.dialog}
       >
         <h2>Add Resource</h2>
-        <label>
-          <span>Add link to resource:</span>
-          <select onChange={() => {}}>
-            <option>Slides</option>
-            <option>Activity Guide</option>
-            <option>Video</option>
-          </select>
-        </label>
+        <div style={styles.container}>
+          <label style={styles.inputAndLabel}>
+            Resource Name
+            <input style={styles.textInput} />
+          </label>
+          <div style={styles.dropdownRow}>
+            <label style={styles.inputAndLabel}>
+              <span>Add link to resource:</span>
+              <select style={styles.selectInput} onChange={() => {}}>
+                {this.props.typeOptions.map(option => (
+                  <option value={option} key={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <label style={styles.inputAndLabel}>
+              Audience
+              <select style={styles.selectInput}>
+                {this.props.audienceOptions.map(option => (
+                  <option value={option} key={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <label style={styles.inputAndLabel}>
+            URL
+            <input style={styles.textInput} />
+          </label>
+          <label style={styles.inputAndLabel}>
+            Download URL
+            <input style={styles.textInput} />
+          </label>
+          <label style={styles.inputAndLabel}>
+            Embed Slug
+            <input style={styles.textInput} />
+          </label>
+        </div>
         <DialogFooter rightAlign>
           <Button
             text={'Close and Add'}
