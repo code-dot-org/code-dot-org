644d16b581f8bb3ab394147eba88a7281a07c588
Erin Bond <erin.bond@code.org>
2020-10-15 17:30:24 -0400

    appease the linter because ML is defined from a script tag in index.html

--- frontend/packages/labs/ailab/src/KNNTrainer.js
+++ frontend/packages/labs/ailab/src/KNNTrainer.js
@@ -1,26 +1,21 @@
 /* Training and prediction using a multiclassification KNN machine learning model from
 https://github.com/mljs/knn */
 
-const knnjs = require("ml-knn");
 import { store } from "./index.js";
 import { setPrediction } from "./redux";
 
 export default class KNNTrainer {
   startTraining() {
     const state = store.getState();
+    /*global ML*/
     this.knn = new ML.KNN(state.trainingExamples, state.trainingLabels, {
       k: 2
     });
   }
 
   predict(testValues) {
-    console.log("testValues in KNNTrainer", testValues);
     let prediction = {};
     prediction.predictedLabel = this.knn.predict([testValues])[0];
     store.dispatch(setPrediction(prediction));
-  }
-
-  clearAll() {
-    this.initTrainingState();
   }
 }
