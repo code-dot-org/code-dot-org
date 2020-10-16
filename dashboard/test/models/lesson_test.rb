d303dfa3b72e6b9586596aa41b98052613c81175
David Bailey <davidsbailey@users.noreply.github.com>
2020-10-16 13:41:18 -0700

    fix query count for related csf lessons

--- dashboard/test/models/lesson_test.rb
+++ dashboard/test/models/lesson_test.rb
@@ -330,7 +330,7 @@ class LessonTest < ActiveSupport::TestCase
     # of related_lessons, so that the count is not artificially reduced by
     # anything being cached from the call to related_lessons.
     summaries = nil
-    assert_queries(4) do
+    assert_queries(1) do
       summaries = lesson1.summarize_related_lessons
     end
 
