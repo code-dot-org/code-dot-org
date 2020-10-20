0849f66090b328cd03153ea5861cdbfd4b81bb64
Erin Bond <erin.bond@code.org>
2020-10-19 20:20:09 -0400

    move initial datasets and update manifest to look locally

--- frontend/packages/labs/ailab/src/datasetManifest.js
+++ frontend/packages/labs/ailab/src/datasetManifest.js
@@ -2,32 +2,27 @@ export const availableDatasets = [
   {
     id: 1,
     name: "Erin's Candy Preferences - all binary",
-    path:
-      "https://raw.githubusercontent.com/code-dot-org/ml-playground/main/datasets/erin_candy_preferences.csv"
+    path: "datasets/erin_candy_preferences.csv"
   },
   {
     id: 2,
     name: "Titanic - multiple datatypes",
-    path:
-      "https://raw.githubusercontent.com/code-dot-org/ml-playground/main/datasets/titanic.csv"
+    path: "datasets/titanic.csv"
   },
 
   {
     id: 3,
     name: "Foods",
-    path:
-      "datasets/foods.csv"
+    path: "datasets/foods.csv"
   },
   {
     id: 4,
     name: "Flight delays",
-    path:
-      "datasets/flight_delays.csv"
+    path: "datasets/flight_delays.csv"
   },
   {
     id: 5,
     name: "Videogame sales",
-    path:
-      "datasets/videogame_sales.csv"
+    path: "datasets/videogame_sales.csv"
   }
 ];
