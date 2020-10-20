670ad86e61abd814c333676330022a5361b7c915
Elijah Hamovitz <elijahhamovitz@gmail.com>
2020-10-20 13:04:59 -0700

    add custom localhost exemption for new 'hosts' config

--- dashboard/config/environments/development.rb
+++ dashboard/config/environments/development.rb
@@ -14,6 +14,9 @@ Dashboard::Application.configure do
     end
   end
 
+  # Allow rendering with our custom localhost domain
+  config.hosts << "localhost-studio.code.org"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
