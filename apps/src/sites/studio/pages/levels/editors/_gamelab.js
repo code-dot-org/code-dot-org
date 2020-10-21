db15e8e12762af8f6708365c6f7db9a0abd138a7
Anjali <anjali@code.org>
2020-10-21 16:30:09 -0700

    don't initialize code mirror in _gamelab (already done in _validation)

--- apps/src/sites/studio/pages/levels/editors/_gamelab.js
+++ apps/src/sites/studio/pages/levels/editors/_gamelab.js
@@ -38,7 +38,4 @@ $(document).ready(function() {
   if (document.getElementById('level_custom_helper_library')) {
     initializeCodeMirror('level_custom_helper_library', 'javascript');
   }
-  if (document.getElementById('level_validation_code')) {
-    initializeCodeMirror('level_validation_code', 'javascript');
-  }
 });
