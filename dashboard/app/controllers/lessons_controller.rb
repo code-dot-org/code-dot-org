16c54fe195d284a97559a850a516a0e4de9e20b0
David Bailey <davidsbailey@users.noreply.github.com>
2020-10-23 13:51:04 -0700

    use untranslated name on lesson edit and lesson overview page

--- dashboard/app/controllers/lessons_controller.rb
+++ dashboard/app/controllers/lessons_controller.rb
@@ -24,7 +24,7 @@ class LessonsController < ApplicationController
         link: @lesson.script.link,
         lessons: @lesson.script.lessons.map {|lesson| {displayName: lesson.localized_name, link: lesson_path(id: lesson.id)}}
       },
-      displayName: @lesson.localized_title,
+      displayName: @lesson.name,
       overview: @lesson.overview || '',
       announcements: @lesson.announcements,
       purpose: @lesson.purpose || '',
