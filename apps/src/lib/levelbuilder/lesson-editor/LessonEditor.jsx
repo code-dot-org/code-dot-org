3782ea340328e2e731348cad0510066bac3f6d5a
Bethany Connor <bethany@code.org>
2020-10-26 14:19:09 +0000

    Add audience to resource table and add slides as a type option

--- apps/src/lib/levelbuilder/lesson-editor/LessonEditor.jsx
+++ apps/src/lib/levelbuilder/lesson-editor/LessonEditor.jsx
@@ -204,7 +204,11 @@ export default class LessonEditor extends Component {
           />
         </CollapsibleEditorSection>
 
-        <CollapsibleEditorSection title="Resources" collapsed={true}>
+        <CollapsibleEditorSection
+          title="Resources"
+          collapsed={true}
+          fullWidth={true}
+        >
           <ResourcesEditor resources={this.props.resources} />
         </CollapsibleEditorSection>
 
