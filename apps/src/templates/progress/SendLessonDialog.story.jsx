af74f6600eda10e7df4ed728cebade6158807589
James Chen <james@code.org>
2020-10-26 16:41:08 -0700

    Hook up Share to Google button in send lesson dialog

--- apps/src/templates/progress/SendLessonDialog.story.jsx
+++ apps/src/templates/progress/SendLessonDialog.story.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {createStoreWithReducers, registerReducers} from '@cdo/apps/redux';
+import googlePlatformApi, {
+  loadGooglePlatformApi
+} from '@cdo/apps/templates/progress/googlePlatformApiRedux';
 import SendLessonDialog from './SendLessonDialog';
 import ExampleDialogButton from '@cdo/apps/util/ExampleDialogButton';
+
+const createStore = () => {
+  registerReducers({googlePlatformApi});
+  const store = createStoreWithReducers();
+  store.dispatch(loadGooglePlatformApi());
+  return store;
+};
 
 export default storybook => {
   return storybook.storiesOf('SendLessonDialog', module).addStoryTable([
     {
-      name: 'with copy to clipboard button',
-      story: () => {
-        return (
-          <ExampleDialogButton>
-            <SendLessonDialog lessonUrl="https://studio.code.org/s/coursee-2020/stage/2/puzzle/1" />
-          </ExampleDialogButton>
-        );
-      }
-    },
-    {
       name: 'with copy to clipboard and google buttons',
       story: () => {
+        const store = createStore();
         return (
-          <ExampleDialogButton>
-            <SendLessonDialog
-              lessonUrl="https://studio.code.org/s/coursee-2020/stage/2/puzzle/1"
-              showGoogleButton
-            />
-          </ExampleDialogButton>
+          <Provider store={store}>
+            <ExampleDialogButton>
+              <SendLessonDialog lessonUrl="https://studio.code.org/s/coursee-2020/stage/2/puzzle/1" />
+            </ExampleDialogButton>
+          </Provider>
         );
       }
     }
