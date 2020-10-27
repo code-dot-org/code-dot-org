e341f2294823d45d8875019f6c29de03725cc75e
Bethany Connor <bethany@code.org>
2020-10-27 18:01:00 +0000

    Remove key from AddResourceDialog and rely on the resource to generate its own key

--- apps/src/lib/levelbuilder/lesson-editor/AddResourceDialog.jsx
+++ apps/src/lib/levelbuilder/lesson-editor/AddResourceDialog.jsx
@@ -68,7 +68,6 @@ const TYPE_OPTIONS = [
 const AUDIENCE_OPTIONS = ['Student', 'Teacher', 'Verified Teacher'];
 
 const initialState = {
-  key: '',
   name: '',
   type: '',
   audience: '',
@@ -93,21 +92,18 @@ export default class AddResourceDialog extends Component {
   }
 
   validateResource = () => {
-    const {key, name, url} = this.state;
+    const {name, url} = this.state;
     let error = '';
     if (name === '') {
       error += 'Name is required. ';
     }
     if (url === '') {
-      error += 'URL is required. ';
+      error += 'URL is required.';
     }
-    if (key === '') {
-      error += 'Embed slug is required.';
-    }
+    this.setState({error});
     if (error === '') {
       return true;
     } else {
-      this.setState({error});
       return false;
     }
   };
@@ -244,16 +240,6 @@ export default class AddResourceDialog extends Component {
                 type="text"
                 name="downloadUrl"
                 value={this.state.downloadUrl}
-                onChange={this.handleInputChange}
-              />
-            </label>
-            <label style={styles.inputAndLabel}>
-              Embed Slug *
-              <input
-                style={styles.textInput}
-                type="txt"
-                name="key"
-                value={this.state.key}
                 onChange={this.handleInputChange}
               />
             </label>
