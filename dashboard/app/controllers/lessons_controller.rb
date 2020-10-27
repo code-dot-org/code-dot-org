0ce9e10954441e32212e1e74b13aa6a09194153c
David Bailey <davidsbailey@users.noreply.github.com>
2020-10-26 18:00:30 -0700

    fetch resources outside of the transaction

--- dashboard/app/controllers/lessons_controller.rb
+++ dashboard/app/controllers/lessons_controller.rb
@@ -43,8 +43,8 @@ class LessonsController < ApplicationController
 
   # PATCH/PUT /lessons/1
   def update
+    resources = (lesson_params['resources'] || []).map {|key| Resource.find_by_key(key)}
     ActiveRecord::Base.transaction do
-      resources = (lesson_params['resources'] || []).map {|key| Resource.find_by_key(key)}
       @lesson.resources = resources.compact
       @lesson.update!(lesson_params.except(:resources, :objectives))
       @lesson.update_activities(JSON.parse(params[:activities])) if params[:activities]
