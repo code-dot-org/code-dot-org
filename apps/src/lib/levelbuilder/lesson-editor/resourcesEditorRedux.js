c8d47e8e5288eb0edb996c5f72d0b1823b603977
Dani <dani@code.org>
2020-10-28 16:55:57 -0400

    WIP: resources into redux

---
+++ apps/src/lib/levelbuilder/lesson-editor/resourcesEditorRedux.js
@@ -0,0 +1,45 @@
+import _ from 'lodash';
+
+const INIT = 'resourcesEditor/INIT';
+const ADD_RESOURCE = 'resourcesEditor/ADD_RESOURCE';
+const REMOVE_RESOURCE = 'resourcesEditor/ADD_RESOURCE';
+
+export const initResources = (resources) => ({
+  type: INIT,
+  resources
+});
+
+export const addResource = (newResource) => ({
+  type: ADD_RESOURCE,
+  newResource
+});
+
+export const removeResource = (key) => ({
+  type: REMOVE_RESOURCE,
+  key
+});
+
+
+export default function resources(state = [], action) {
+  let newState = _.cloneDeep(state);
+
+  switch (action.type) {
+    case INIT:
+      console.log('here');
+      console.log(action.resources);
+      return action.resources;
+    case ADD_RESOURCE: {
+      newState.concat([action.newResource]);
+      break;
+    }
+    case REMOVE_RESOURCE: {
+      const resourceToRemove = newState.find(
+        resource => resource.key === action.key
+      );
+      newState.splice(newState.indexOf(resourceToRemove), 1);
+      break;
+    }
+  }
+
+  return newState;
+};
