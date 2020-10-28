ff436412bb9d5b4313220758d9e07be5467032b8
hacodeorg <ha@code.org>
2020-10-29 04:11:22 +0700

    Create a PWA demo page

--- dashboard/public/sw.js
+++ dashboard/public/sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98680228bd2c5fad6b254e568d218ce160ad3e71a711c553c74f4e5b2c2a1f6d
-size 3347
+oid sha256:b26ca7df9dc883372815ca48d1eb09ee4b7e5dac7c3618ddff05dd5ec0ffe6ba
+size 1132
