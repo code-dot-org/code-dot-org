655c1f150d91822da63ef2de67e3bb3ee89bba78
Dani <dani@code.org>
2020-11-02 20:21:59 -0500

    Create tests for ActivityCardAndPreview

--- apps/test/unit/lib/levelbuilder/lesson-editor/ActivityCardAndPreviewTest.jsx
+++ apps/test/unit/lib/levelbuilder/lesson-editor/ActivityCardAndPreviewTest.jsx
@@ -1,34 +1,22 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import sinon from 'sinon';
 import {expect} from '../../../../util/reconfiguredChai';
 import ActivityCardAndPreview from '@cdo/apps/lib/levelbuilder/lesson-editor/ActivityCardAndPreview';
 import {sampleActivities} from './activitiesTestData';
-import sinon from 'sinon';
 
 describe('ActivityCardAndPreview', () => {
   let defaultProps,
-    addActivitySection,
-    removeActivity,
-    moveActivity,
-    updateActivityField,
     setActivitySectionRef,
     updateTargetActivitySection,
     updateActivitySectionMetrics;
   beforeEach(() => {
-    addActivitySection = sinon.spy();
-    removeActivity = sinon.spy();
-    moveActivity = sinon.spy();
-    updateActivityField = sinon.spy();
     setActivitySectionRef = sinon.spy();
     updateTargetActivitySection = sinon.spy();
     updateActivitySectionMetrics = sinon.spy();
     defaultProps = {
       activity: sampleActivities[0],
       activitiesCount: 1,
-      addActivitySection,
-      removeActivity,
-      moveActivity,
-      updateActivityField,
       setActivitySectionRef,
       updateTargetActivitySection,
       updateActivitySectionMetrics,
@@ -39,40 +27,20 @@ describe('ActivityCardAndPreview', () => {
 
   it('renders default props', () => {
     const wrapper = shallow(<ActivityCardAndPreview {...defaultProps} />);
-    expect(wrapper.contains('Activity:')).to.be.true;
-    expect(wrapper.contains('Duration:')).to.be.true;
-    expect(wrapper.find('OrderControls').length).to.equal(1);
-    expect(wrapper.find('Connect(ActivitySectionCard)').length).to.equal(3);
-    expect(wrapper.find('button').length).to.equal(1);
+    expect(wrapper.find('Connect(ActivityCard)').length).to.equal(1);
+    expect(wrapper.find('Activity').length).to.equal(1);
   });
 
-  it('adds activity section when button pressed', () => {
+  it('hides preview when collapsed', () => {
     const wrapper = shallow(<ActivityCardAndPreview {...defaultProps} />);
-    expect(wrapper.find('button').length).to.equal(1);
+    wrapper.setState({collapsed: true});
 
-    const button = wrapper.find('button');
-    expect(button.text()).to.include('Activity Section');
-    button.simulate('mouseDown');
-    expect(addActivitySection).to.have.been.calledOnce;
-  });
-
-  it('edit activity title', () => {
-    const wrapper = shallow(<ActivityCardAndPreview {...defaultProps} />);
-
-    const titleInput = wrapper.find('input').at(0);
-    titleInput.simulate('change', {target: {value: 'New Title'}});
-    expect(updateActivityField).to.have.been.calledWith(
-      1,
-      'displayName',
-      'New Title'
-    );
-  });
-
-  it('edit activity duration', () => {
-    const wrapper = shallow(<ActivityCardAndPreview {...defaultProps} />);
-
-    const titleInput = wrapper.find('input').at(1);
-    titleInput.simulate('change', {target: {value: '1000'}});
-    expect(updateActivityField).to.have.been.calledWith(1, 'duration', 1000);
+    expect(wrapper.find('Connect(ActivityCard)').length).to.equal(1);
+    expect(wrapper.find('Activity').length).to.equal(0);
+    expect(
+      wrapper.contains(
+        'This activity has been collapsed. Expand activity to see preview.'
+      )
+    ).to.be.true;
   });
 });
