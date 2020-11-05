1c5c63446f78a4a53f5175499ffb1abfc4efef74
David Bailey <davidsbailey@users.noreply.github.com>
2020-11-05 08:29:41 -0800

    use heredoc for banner

--- bin/oneoff/import_unit_details.rb
+++ bin/oneoff/import_unit_details.rb
@@ -7,6 +7,14 @@ require_relative '../../deployment'
 # Once this script is ready, only levelbuilder should be added to this list.
 raise unless [:development, :adhoc].include? rack_env
 
+# Wait until after initial error checking before loading the rails environment.
+def require_rails_env
+  puts "loading rails environment..."
+  start_time = Time.now
+  require_relative '../../dashboard/config/environment'
+  puts "rails environment loaded in #{(Time.now - start_time).to_i} seconds."
+end
+
 def parse_options
   OpenStruct.new.tap do |options|
     options.local = false
@@ -14,10 +22,15 @@ def parse_options
     options.dry_run = false
 
     opt_parser = OptionParser.new do |opts|
-      opts.banner = "\nUsage: #{$0} [options] \n\
-        Example: runner.rb -u coursea-2021 \n\
-        Example: runner.rb -l -u csd1-2021 \n\
-        Example: runner.rb -l -u csp2-2020,csp3-2021,csp4-2021"
+      opts.banner = <<~BANNER
+
+        Usage: #{$0} [options]
+
+        Example: runner.rb -u coursea-2021
+        Example: runner.rb -l -u csd1-2021
+        Example: runner.rb -l -u csp2-2020,csp3-2021,csp4-2021
+      BANNER
+
       opts.separator ""
 
       opts.on('-l', '--local', 'Use local curriculum builder running on localhost:8000.') do
@@ -67,10 +80,5 @@ end
 options = parse_options
 raise "unit name is required. Use -h for options." unless options.unit_names.present?
 
-# Wait until after initial error checking before loading the rails environment.
-puts "loading rails environment..."
-start_time = Time.now
-require_relative '../../dashboard/config/environment'
-puts "rails environment loaded in #{(Time.now - start_time).to_i} seconds."
-
+require_rails_env
 main(options)
