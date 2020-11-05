001279c811fc1e66a75ad12ee80310b34a842a12
David Bailey <davidsbailey@users.noreply.github.com>
2020-11-05 09:45:09 -0800

    hide debug spew by default

--- bin/oneoff/import_unit_details.rb
+++ bin/oneoff/import_unit_details.rb
@@ -7,12 +7,17 @@ require_relative '../../deployment'
 # Once this script is ready, only levelbuilder should be added to this list.
 raise unless [:development, :adhoc].include? rack_env
 
+$verbose = false
+def log(str)
+  puts str if $verbose
+end
+
 # Wait until after initial error checking before loading the rails environment.
 def require_rails_env
-  puts "loading rails environment..."
+  log "loading rails environment..."
   start_time = Time.now
   require_relative '../../dashboard/config/environment'
-  puts "rails environment loaded in #{(Time.now - start_time).to_i} seconds."
+  log "rails environment loaded in #{(Time.now - start_time).to_i} seconds."
 end
 
 def parse_options
@@ -45,6 +50,10 @@ def parse_options
         options.dry_run = true
       end
 
+      opts.on('-v', '--verbose', 'Use verbose debug logging.') do
+        $verbose = true
+      end
+
       opts.on_tail("-h", "--help", "Show this message") do
         puts opts
         exit
@@ -60,12 +69,13 @@ def main(options)
 
   options.unit_names.each do |unit_name|
     script = Script.find_by_name!(unit_name)
-    puts "found code studio script name #{script.name} with id #{script.id}"
+    log "found code studio script name #{script.name} with id #{script.id}"
 
     url = "#{cb_url_prefix}/export/unit/#{unit_name}.json?format=json"
-    puts "fetching unit json from #{url}"
-    cb_unit_json = `curl #{url}`
-    puts "received #{cb_unit_json.length} bytes of unit json."
+    log "fetching unit json from #{url}"
+    curl_opts = $verbose ? '' : ' -s'
+    cb_unit_json = `curl#{curl_opts} #{url}`
+    log "received #{cb_unit_json.length} bytes of unit json."
 
     cb_unit_data = JSON.parse(cb_unit_json)
     validate_unit(script, cb_unit_data)
@@ -74,7 +84,7 @@ end
 
 def validate_unit(script, cb_unit_data)
   raise "unexpected unit_name #{cb_unit_data['unit_name']}" unless cb_unit_data['unit_name'] == script.name
-  puts "validated unit data for #{script.name}"
+  log "validated unit data for #{script.name}"
 end
 
 options = parse_options
