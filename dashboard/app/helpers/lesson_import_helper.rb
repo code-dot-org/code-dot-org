c9f232f67d547a095277a86325ef01699af91b03
Bethany Connor <bethany@code.org>
2020-11-06 22:44:13 +0000

    Got better support for tips and also the code studio pullthrough

--- dashboard/app/helpers/lesson_import_helper.rb
+++ dashboard/app/helpers/lesson_import_helper.rb
@@ -4,18 +4,26 @@ module LessonImportHelper
     #tips = parse_tips(activity_markdown)
     #return [ActivitySection.new(description: activity_markdown, seeding_key: SecureRandom.uuid, position: 1, tips: tips)]
     tip_matches = find_tips(activity_markdown).map { |m| {index:activity_markdown.index(m[0]), type: 'tip', match: m, substring: m[0]}}
+    tip_link_matches = find_tip_links(activity_markdown).map { |m| {index:activity_markdown.index(m[0]), type: 'tiplink', match: m, substring: m[0]}}
     remark_matches = find_remarks(activity_markdown).map { |m| {index:activity_markdown.index(m[0]), type: 'remark', match: m, substring: m[0]}}
-    matches = tip_matches + remark_matches
+    pullthrough_matches = find_code_studio_pullthroughs(activity_markdown).map { |m| {index:activity_markdown.index(m[0]), type: 'pullthrough', match: m, substring: m[0]}}
+    matches = tip_matches + remark_matches + tip_link_matches + pullthrough_matches
     sorted_matches = matches.sort_by{|m| m[:index]}
     return [ActivitySection.new(description: activity_markdown.strip, seeding_key: SecureRandom.uuid, position: 1)] if matches.empty?
     sorted_matches = find_markdown_chunks(activity_markdown, sorted_matches)
     sections = []
+    tip_match_map = {}
     sorted_matches.each_with_index do |match, i|
       activity_section = nil
       if match[:type] == 'tip'
-        activity_section = create_activity_section_with_tip(match[:match])
+        tip_match_map[match[:match][3]] = match
+        next
+      elsif match[:type] == 'tiplink'
+        activity_section = create_activity_section_with_tip(match[:match], tip_match_map)
       elsif match[:type] == 'remark'
         activity_section = create_activity_section_with_remark(match[:match])
+      elsif match[:type] == 'pullthrough'
+        activity_section = create_activity_section_with_levels(match[:match])
       else
         activity_section = ActivitySection.new(description: match[:substring].strip)
       end
@@ -39,21 +47,31 @@ module LessonImportHelper
     activities
   end
 
-  def self.create_lesson(lesson_json, script, lesson_group, persisted_lesson = nil)
+  def self.create_lesson(lesson_json, persisted_lesson = nil)
     lesson_data = JSON.parse(lesson_json)
     lesson = persisted_lesson || Lesson.new
     lesson.name = lesson_data['title']
     lesson.key = lesson.name.tr(' ', '_').downcase
-    lesson.script = script
-    lesson.lesson_group = lesson_group
     lesson.overview = lesson_data['overview']
     lesson.relative_position = lesson_data['number'] || 1
     lesson.lesson_activities = create_lesson_activities(lesson_data['activities'])
     lesson
   end
 
+  # https://github.com/code-dot-org/curriculumbuilder/blob/57cad8f62e50b03e4f16bf77cd9e2e1da5c3e44e/curriculumBuilder/codestudio.py
+  def self.find_code_studio_pullthroughs(markdown)
+    regex = /(\[code-studio\s*)(\d+)?-?(\d+)?\]/
+    markdown.to_enum(:scan, regex).map { Regexp.last_match }
+  end
+
+  def self.create_activity_section_with_levels(match)
+    activity_section = ActivitySection.new
+    activity_section.description = "levels #{match[2] || 0} to #{match[3] || 'end'}"
+    activity_section
+  end
+
   def self.find_remarks(markdown)
-    regex = /^!!!say\s+?[\n\r]+([\d\D]+?)$/
+    regex = /^!!!say\s+?[\n\r]+([\d\D]+?)(?=^( {0,3}|\S))/
     markdown.to_enum(:scan, regex).map { Regexp.last_match }
   end
 
@@ -64,48 +82,33 @@ module LessonImportHelper
     activity_section
   end
 
+  # https://github.com/code-dot-org/remark-plugins/blob/master/src/tip.js
   def self.find_tips(markdown)
     #regex = /^!!! ?([\w-]+)(?: "(.*?)")?(?: <(.*?)>)\n([\d\D]+?)([\w-]+)!!!(.*?)$/
-    regex = /^!!!?([\w-]+)(?: "(.*?)")?(?: <(.*?)>)[\n\r]([\d\D]+?)^([\w-]+)!!![a-zA-Z0-9\-]+(?:<!-- place where you'd like the icon -->)?(.+?)$/
+    #regex = /^!!!?([\w-]+)(?: "(.*?)")?(?: <(.*?)>)[\n\r]([\d\D]+?)^([\w-]+)!!![a-zA-Z0-9\-]+(?:<!-- place where you'd like the icon -->)?(.+?)$/
+    regex = /^!!!?([\w-]+)(?: "(.*?)")?(?: <(.*?)>)(?:[\s]+$)+([\d\D]+?)(?=^\S)/
     markdown.to_enum(:scan, regex).map { Regexp.last_match }
   end
 
-  def self.create_activity_section_with_tip(match)
-    tip_map = {
-      "tip" => "teachingTip",
-      "content" => "contentCorner",
-      "discussion" => "discussionGoal",
-      "assessment" => "assessmentOpportunity"
-    }
-    activity_section = ActivitySection.new
-    activity_section.description = match[6].strip
-    tip = {key: match[3] || "#{match[1]}-0", type: tip_map[match[1]] || "teachingTip", markdown: match[4].strip}
-    activity_section.tips = [tip]
-    activity_section
+  # https://github.com/code-dot-org/remark-plugins/blob/master/src/tiplink.js
+  def self.find_tip_links(markdown)
+    regex = /^([\w-]+)!!! ?([\w-]+)(?:<!-- place where you'd like the icon -->)?(.+?)$/
+    markdown.to_enum(:scan, regex).map { Regexp.last_match }
   end
 
-  def self.parse_tips(markdown)
+  def self.create_activity_section_with_tip(tip_link_match, tip_match_map)
     tip_map = {
       "tip" => "teachingTip",
       "content" => "contentCorner",
       "discussion" => "discussionGoal",
       "assessment" => "assessmentOpportunity"
     }
-    regex = /^!!! ?([\w-]+)(?: "(.*?)")?(?: <(.*?)>)?\n([\d\D]+?)([\w-]+)!!!(.*?)$/
-    edited_markdown = markdown
-    match = edited_markdown.match regex
-    tips = []
-    count = 0
-    while !match.nil?
-      tip = {key: match[3] || "unknown-#{count}", type: tip_map[match[1]] || "teachingTip", markdown: match[4]}
-      tips.concat [tip]
-      start_index = edited_markdown.index regex
-      edited_markdown.slice!(start_index, start_index + match[0].length)
-      match = edited_markdown.match regex
-      count += 1
-      break if count > 5
-    end
-    tips
+    tip_match = tip_match_map[tip_link_match[2]][:match]
+    activity_section = ActivitySection.new
+    activity_section.description = tip_link_match[3].strip
+    tip = {key: tip_match[3] || "#{tip_match[1]}-0", type: tip_map[tip_match[1]] || "teachingTip", markdown: tip_match[4]}
+    activity_section.tips = [tip]
+    activity_section
   end
 
   def self.find_markdown_chunks(markdown, existing_matches)
