5ab8434d8b782be87b94dd97607eac120c067fdc
Brendan Reville <brendan@code.org>
2020-11-05 16:58:00 -0800

    More tweaks

--- frontend/packages/labs/ailab/src/indexDev.js
+++ frontend/packages/labs/ailab/src/indexDev.js
@@ -4,7 +4,7 @@ import queryString from "query-string";
 
 // A list of sample modes.  Should match the dropdown in index.html.
 const sampleModes = {
-	load_foods: { id: "load_dataset", setId: 3 }
+  load_foods: { id: "load_dataset", setId: 3 }
 };
 
 // Look for a ?mode= parameter on the URL
