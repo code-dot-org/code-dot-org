530717b76d174d96a7326ae7df2fa40ca39bb3d5
Ryan Sloan <ryan@ryanjsloan.com>
2020-11-05 21:02:33 -0800

    remove debugging statements

--- apps/src/templates/projects/ProjectCard.jsx
+++ apps/src/templates/projects/ProjectCard.jsx
@@ -103,7 +103,6 @@ export default class ProjectCard extends React.Component {
     const shouldShowPublishedAt =
       isPublicGallery && isDetailView && projectData.publishedAt;
     const noTimeOnCardStyle = shouldShowPublishedAt ? {} : styles.noTime;
-    console.log(projectData);
     return (
       <div className="project_card">
         <div style={styles.card}>
