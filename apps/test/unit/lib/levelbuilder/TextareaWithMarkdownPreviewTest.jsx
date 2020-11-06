90c59a1858cc0c5309bcf2728474e300a4e510a3
Dani <dani@code.org>
2020-11-06 18:04:08 -0500

    Update TextareaWithMarkdownPreviewTest

--- apps/test/unit/lib/levelbuilder/TextareaWithMarkdownPreviewTest.jsx
+++ apps/test/unit/lib/levelbuilder/TextareaWithMarkdownPreviewTest.jsx
@@ -1,25 +1,29 @@
-import {expect} from 'chai';
+import {expect} from '../../../util/reconfiguredChai';
 import React from 'react';
 import {mount} from 'enzyme';
 import TextareaWithMarkdownPreview from '@cdo/apps/lib/levelbuilder/TextareaWithMarkdownPreview';
-
-const DEFAULT_PROPS = {
-  label: 'Section Name',
-  name: 'name',
-  markdown:
-    '# Title \n This is the unit description with [link](https://studio.code.org/home) **Bold** *italics*',
-  helpTip: 'Helpful information so you know what to do'
-};
+import sinon from 'sinon';
 
 describe('TextareaWithMarkdownPreview', () => {
+  let defaultProps, handleMarkdownChange;
+  beforeEach(() => {
+    handleMarkdownChange = sinon.spy();
+    defaultProps = {
+      label: 'Section Name',
+      markdown:
+        '# Title \n This is the unit description with [link](https://studio.code.org/home) **Bold** *italics*',
+      helpTip: 'Helpful information so you know what to do',
+      handleMarkdownChange
+    };
+  });
+
   it('has correct markdown for preview of unit description', () => {
-    const wrapper = mount(<TextareaWithMarkdownPreview {...DEFAULT_PROPS} />);
+    const wrapper = mount(<TextareaWithMarkdownPreview {...defaultProps} />);
     expect(wrapper.contains('Section Name')).to.be.true;
     expect(wrapper.find('textarea').length).to.equal(1);
-    expect(wrapper.find('textarea').prop('defaultValue')).to.equal(
+    expect(wrapper.find('textarea').prop('value')).to.equal(
       '# Title \n This is the unit description with [link](https://studio.code.org/home) **Bold** *italics*'
     );
-    expect(wrapper.find('textarea').prop('name')).to.equal('name');
     expect(wrapper.find('SafeMarkdown').length).to.equal(1);
     expect(wrapper.find('SafeMarkdown').prop('markdown')).to.equal(
       '# Title \n This is the unit description with [link](https://studio.code.org/home) **Bold** *italics*'
@@ -27,15 +31,13 @@ describe('TextareaWithMarkdownPreview', () => {
 
     expect(wrapper.find('HelpTip').length).to.equal(1);
 
-    wrapper
-      .find('textarea[name="name"]')
-      .simulate('change', {target: {value: '## Title'}});
-    expect(wrapper.find('SafeMarkdown').prop('markdown')).to.equal('## Title');
+    wrapper.find('textarea').simulate('change', {target: {value: '## Title'}});
+    expect(handleMarkdownChange).to.have.been.calledOnce;
   });
 
-  it('has no HelpTip if non passed in to props', () => {
+  it('has no HelpTip if none passed in to props', () => {
     const wrapper = mount(
-      <TextareaWithMarkdownPreview {...DEFAULT_PROPS} helpTip={null} />
+      <TextareaWithMarkdownPreview {...defaultProps} helpTip={null} />
     );
 
     expect(wrapper.find('HelpTip').length).to.equal(0);
