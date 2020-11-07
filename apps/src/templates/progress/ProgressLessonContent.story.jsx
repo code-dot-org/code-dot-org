28095d266eca14b22e7fa56967257dacb4e2d08c
Jessica Kulwik <jessica@code.org>
2020-11-07 01:30:53 +0000

    Additional storybook test fixes

--- apps/src/templates/progress/ProgressLessonContent.story.jsx
+++ apps/src/templates/progress/ProgressLessonContent.story.jsx
@@ -1,66 +1,100 @@
 import React from 'react';
 import ProgressLessonContent from './ProgressLessonContent';
-import {fakeLevels, fakeLevel} from './progressTestHelpers';
+import {
+  fakeLevels,
+  fakeLevel,
+  fakeProgressForLevels
+} from './progressTestHelpers';
 import {LevelStatus} from '@cdo/apps/util/sharedConstants';
+import progress, {setProgress} from '@cdo/apps/code-studio/progressRedux';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
 
 export default storybook => {
+  const store = createStore(combineReducers({progress}));
   storybook.storiesOf('Progress/ProgressLessonContent', module).addStoryTable([
     {
       name: 'progress lesson content',
-      story: () => (
-        <ProgressLessonContent
-          disabled={false}
-          levels={fakeLevels(5).map((level, index) => ({
-            ...level,
-            status: index === 1 ? LevelStatus.perfect : LevelStatus.not_tried,
-            name: 'Progression'
-          }))}
-        />
-      )
+      story: () => {
+        let levels = fakeLevels(5).map((level, index) => ({
+          ...level,
+          name: 'Progression'
+        }));
+        let levelProgress = fakeProgressForLevels(levels);
+        levelProgress[1].status = LevelStatus.perfect;
+        store.dispatch(setProgress(levelProgress));
+
+        return (
+          <Provider store={store}>
+            <ProgressLessonContent
+              disabled={false}
+              levels={fakeLevels(5).map((level, index) => ({
+                ...level,
+                name: 'Progression'
+              }))}
+            />
+          </Provider>
+        );
+      }
     },
     {
       name: 'with unplugged lesson',
       description: 'pill should say unplugged, because of first level',
-      story: () => (
-        <ProgressLessonContent
-          disabled={false}
-          levels={[fakeLevel({isUnplugged: true}), ...fakeLevels(5)].map(
-            level => ({...level, name: 'Progression'})
-          )}
-        />
-      )
+      story: () => {
+        let levels = [fakeLevel({isUnplugged: true}), ...fakeLevels(5)].map(
+          level => ({...level, name: 'Progression'})
+        );
+        let levelProgress = fakeProgressForLevels(levels);
+        store.dispatch(setProgress(levelProgress));
+
+        return (
+          <Provider store={store}>
+            <ProgressLessonContent disabled={false} levels={levels} />
+          </Provider>
+        );
+      }
     },
     {
       name: 'with named unplugged lesson',
       description:
         'First pill should say unplugged. second should say level 1-5',
-      story: () => (
-        <ProgressLessonContent
-          disabled={false}
-          levels={[
-            {
-              ...fakeLevel({isUnplugged: true}),
-              name: 'Fun unplugged/named level'
-            },
-            ...fakeLevels(5, {named: false})
-          ]}
-        />
-      )
+      story: () => {
+        let levels = [
+          {
+            ...fakeLevel({isUnplugged: true}),
+            name: 'Fun unplugged/named level'
+          },
+          ...fakeLevels(5, {named: false})
+        ];
+        let levelProgress = fakeProgressForLevels(levels);
+        store.dispatch(setProgress(levelProgress));
+
+        return (
+          <Provider store={store}>
+            <ProgressLessonContent disabled={false} levels={levels} />
+          </Provider>
+        );
+      }
     },
     {
       name: 'with no named levels',
       description: 'no pills',
-      story: () => (
-        <ProgressLessonContent
-          disabled={false}
-          levels={[
-            {
-              ...fakeLevel({isUnplugged: true, name: undefined})
-            },
-            ...fakeLevels(5, {named: false})
-          ]}
-        />
-      )
+      story: () => {
+        let levels = [
+          {
+            ...fakeLevel({isUnplugged: true, name: undefined})
+          },
+          ...fakeLevels(5, {named: false})
+        ];
+        let levelProgress = fakeProgressForLevels(levels);
+        store.dispatch(setProgress(levelProgress));
+
+        return (
+          <Provider store={store}>
+            <ProgressLessonContent disabled={false} levels={levels} />
+          </Provider>
+        );
+      }
     }
   ]);
 };
