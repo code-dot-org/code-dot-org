d29d3ffbc78d7f751da7341292e6819a723eac09
Dani <dani@code.org>
2020-11-09 17:55:38 -0500

    Update ScriptEditor props to have initial in front of ones used in state

--- apps/src/lib/levelbuilder/script-editor/ScriptEditor.jsx
+++ apps/src/lib/levelbuilder/script-editor/ScriptEditor.jsx
@@ -85,29 +85,29 @@ export default class ScriptEditor extends React.Component {
     curriculumPath: PropTypes.string,
     pilotExperiment: PropTypes.string,
     editorExperiment: PropTypes.string,
-    announcements: PropTypes.arrayOf(announcementShape).isRequired,
+    initialAnnouncements: PropTypes.arrayOf(announcementShape).isRequired,
     supportedLocales: PropTypes.arrayOf(PropTypes.string),
     locales: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired,
     projectSharing: PropTypes.bool,
     curriculumUmbrella: PropTypes.oneOf(CURRICULUM_UMBRELLAS),
-    familyName: PropTypes.string,
+    initialFamilyName: PropTypes.string,
     versionYear: PropTypes.string,
     scriptFamilies: PropTypes.arrayOf(PropTypes.string).isRequired,
     versionYearOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
     isLevelbuilder: PropTypes.bool,
     tts: PropTypes.bool,
     hasCourse: PropTypes.bool,
-    isCourse: PropTypes.bool
+    initialIsCourse: PropTypes.bool
   };
 
   constructor(props) {
     super(props);
 
     this.state = {
-      familyName: this.props.familyName || '',
-      isCourse: this.props.isCourse,
+      familyName: this.props.initialFamilyName || '',
+      isCourse: this.props.initialIsCourse,
       description: this.props.i18nData.description,
-      announcements: this.props.announcements
+      announcements: this.props.initialAnnouncements
     };
   }
 
