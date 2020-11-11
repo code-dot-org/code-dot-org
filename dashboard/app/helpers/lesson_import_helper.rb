5374733c0bf80b1b102019a291a19254d3f8258f
Bethany Connor <bethany@code.org>
2020-11-11 17:23:46 +0000

    Some slides improvements and lint checking

--- dashboard/app/helpers/lesson_import_helper.rb
+++ dashboard/app/helpers/lesson_import_helper.rb
@@ -1,31 +1,33 @@
+# Helper module for importing data from curriculumbuilder
 module LessonImportHelper
-
+  # Lockable lessons don't need to be merged with curriculumbuilder, but we do
+  # need the levels to be part of an activity section.
   def self.update_lockable_lesson(lesson)
-      lesson_activity = LessonActivity.new
-      lesson_activity.seeding_key = SecureRandom.uuid
-      lesson_activity.position = 1
-      lesson.lesson_activities = [lesson_activity]
-      activity_section = ActivitySection.new
-      levels = lesson.script_levels.each_with_index.map{|l,i| JSON.parse({id: l.id, assessment: l.assessment, bonus: l.bonus, challenge:l.challenge, levels: l.levels, activitySectionPosition: i, inActivitySection: false}.to_json)}
-      activity_section.seeding_key = SecureRandom.uuid
-      activity_section.position = 1
-      activity_section.lesson_activity = lesson_activity
-      activity_section.save!
-      activity_section.update_script_levels(levels)
-      lesson_activity.activity_sections = [activity_section]
-      activity_section.save!
-      lesson.script_levels = []
+    lesson_activity = LessonActivity.new
+    lesson_activity.seeding_key = SecureRandom.uuid
+    lesson_activity.position = 1
+    lesson.lesson_activities = [lesson_activity]
+    activity_section = ActivitySection.new
+    levels = lesson.script_levels.each_with_index.map {|l, i| JSON.parse({id: l.id, assessment: l.assessment, bonus: l.bonus, challenge: l.challenge, levels: l.levels, activitySectionPosition: i, inActivitySection: false}.to_json)}
+    activity_section.seeding_key = SecureRandom.uuid
+    activity_section.position = 1
+    activity_section.lesson_activity = lesson_activity
+    activity_section.save!
+    activity_section.update_script_levels(levels)
+    lesson_activity.activity_sections = [activity_section]
+    activity_section.save!
+    lesson.script_levels = []
   end
 
   def self.create_activity_sections(activity_markdown, position)
-    tip_matches = find_tips(activity_markdown).select{|m| m[1] != 'say'}.map { |m| {index:activity_markdown.index(m[0]), type: 'tip', match: m, substring: m[0]}}
-    tip_link_matches = find_tip_links(activity_markdown).map { |m| {index:activity_markdown.index(m[0]), type: 'tiplink', match: m, substring: m[0]}}
-    remark_matches = find_remarks(activity_markdown).map { |m| {index:activity_markdown.index(m[0]), type: 'remark', match: m, substring: m[0]}}
-    pullthrough_matches = find_skippable_syntax(activity_markdown).map { |m| {index:activity_markdown.index(m[0]), type: 'skippable', match: m, substring: m[0]}}
-    slide_matches = find_slides(activity_markdown).map {|m| {index:activity_markdown.index(m[0]), type: 'slide', match: m, substring: m[0]}}
-    matches = tip_matches + remark_matches + tip_link_matches + pullthrough_matches
-    sorted_matches = matches.sort_by{|m| m[:index]}
-    return [ActivitySection.new(description: activity_markdown.strip, seeding_key: SecureRandom.uuid, position: 1)] if matches.empty?
+    tip_matches = find_tips(activity_markdown).select {|m| m[1] != 'say'}.map {|m| {index: activity_markdown.index(m[0]), type: 'tip', match: m, substring: m[0]}}
+    tip_link_matches = find_tip_links(activity_markdown).map {|m| {index: activity_markdown.index(m[0]), type: 'tiplink', match: m, substring: m[0]}}
+    remark_matches = find_remarks(activity_markdown).map {|m| {index: activity_markdown.index(m[0]), type: 'remark', match: m, substring: m[0]}}
+    pullthrough_matches = find_skippable_syntax(activity_markdown).map {|m| {index: activity_markdown.index(m[0]), type: 'skippable', match: m, substring: m[0]}}
+    slide_matches = find_slides(activity_markdown).map {|m| {index: activity_markdown.index(m[0]), type: 'slide', match: m, substring: m[0]}}
+    matches = remark_matches + tip_link_matches + pullthrough_matches + slide_matches
+    sorted_matches = matches.sort_by {|m| m[:index]}
+    return [ActivitySection.new(description: activity_markdown.strip, seeding_key: SecureRandom.uuid, position: position)] if matches.empty?
     sorted_matches = find_markdown_chunks(activity_markdown, sorted_matches)
     sections = []
     tip_match_map = {}
@@ -36,9 +38,10 @@ module LessonImportHelper
     end
     sorted_matches.each_with_index do |match, i|
       activity_section = nil
-      if match[:type] == 'tip' || match[:type] == 'skippable'
+      if match[:type] == 'skippable'
         next
       elsif match[:type] == 'slide'
+        # For now, if we find the slide token, apply it to the next thing
         slide = true
         next
       elsif match[:type] == 'tiplink'
@@ -49,7 +52,7 @@ module LessonImportHelper
         activity_section = ActivitySection.new(description: match[:substring].strip)
       end
       next unless activity_section
-      activity_section.position = i+1
+      activity_section.position = i + 1
       activity_section.slide = slide
       slide = false
       activity_section.seeding_key ||= SecureRandom.uuid
@@ -88,13 +91,13 @@ module LessonImportHelper
     @lesson_activity.position = activities.length + 1
     @lesson_activity.save!
     @lesson_activity.reload
-    @lesson_activity.activity_sections = [create_activity_section_with_level_ranges(0, levels.count-1, levels)]
+    @lesson_activity.activity_sections = [create_activity_section_with_level_ranges(0, levels.count - 1, levels)]
     activities.push(@lesson_activity)
     activities.flatten
   end
 
   def self.create_lesson(lesson_data, persisted_lesson)
-    levels = persisted_lesson.script_levels.each_with_index.map{|l,i| JSON.parse({id: l.id, assessment: l.assessment, bonus: l.bonus, challenge:l.challenge, levels: l.levels, activitySectionPosition: i, inActivitySection: false}.to_json)}
+    levels = persisted_lesson.script_levels.each_with_index.map {|l, i| JSON.parse({id: l.id, assessment: l.assessment, bonus: l.bonus, challenge: l.challenge, levels: l.levels, activitySectionPosition: i, inActivitySection: false}.to_json)}
     @lesson = persisted_lesson || Lesson.new
     @lesson.name = lesson_data['title']
     @lesson.key ||= lesson.name.tr(' ', '_').downcase
@@ -110,12 +113,12 @@ module LessonImportHelper
   # https://github.com/code-dot-org/curriculumbuilder/blob/57cad8f62e50b03e4f16bf77cd9e2e1da5c3e44e/curriculumBuilder/codestudio.py
   def self.find_skippable_syntax(markdown)
     regex = /\[code-studio\s*(?:\d+)?-?(?:\d+)?\]|\[\/?guide\]/
-    markdown.to_enum(:scan, regex).map { Regexp.last_match }
+    markdown.to_enum(:scan, regex).map {Regexp.last_match}
   end
 
   def self.find_slides(markdown)
-    regex = /^slide!!!slide-\d+(?:<!-- place where you'd like the icon -->)/
-    markdown.to_enum(:scan, regex).map { Regexp.last_match }
+    regex = /^slide!!!slide-\d+(?:<!-- place where you'd like the icon -->)?/
+    markdown.to_enum(:scan, regex).map {Regexp.last_match}
   end
 
   def self.create_activity_section_with_level_ranges(range_start, range_end, levels)
@@ -125,39 +128,50 @@ module LessonImportHelper
     activity_section.lesson_activity = @lesson_activity
     activity_section.lesson_activity_id = @lesson_activity.id
     activity_section.save!
-    activity_section.description = "levels #{range_start} to #{range_end}"
     unless levels.empty?
       sl_data = levels.slice(range_start..range_end)
 
-      sl_data = sl_data.select{|sl| !sl["inActivitySection"] }
+      sl_data = sl_data.select {|sl| !sl["inActivitySection"]}
       activity_section.update_script_levels(sl_data) unless sl_data.blank?
-      sl_data.each{|sl| sl["inActivitySection"] = true}
+      sl_data.each {|sl| sl["inActivitySection"] = true}
     end
     activity_section
   end
 
   def self.find_remarks(markdown)
-    regex = /^!!!say\s+?[\n\r]+([\d\D]+?)(?=^\S)/
-    markdown.to_enum(:scan, regex).map { Regexp.last_match }
+    regex = /^!!! *say\s+?[\n\r]*([\d\D]+?)(?=(^\S|$))/
+    markdown.to_enum(:scan, regex).map {Regexp.last_match}
   end
 
   def self.create_activity_section_with_remark(match)
     activity_section = ActivitySection.new
     activity_section.remarks = true
-    activity_section.description = match[1].strip
+    description = match[1].strip
+    slide_matches = find_slides(description)
+    if slide_matches.empty? || description.index(slide_matches[0][0]) != 0
+      activity_section.description = description.strip
+    else
+      activity_section.description = description.delete_prefix(slide_matches[0][0])
+      activity_section.slide = true
+    end
     activity_section
   end
 
   # https://github.com/code-dot-org/remark-plugins/blob/master/src/tip.js
   def self.find_tips(markdown)
-    regex = /^!!! *?([\w-]+)(?: "(.*?)")?(?: <(.*?)>)?(?:[\s]+$)+([\d\D]+?)(?=^\S)/
-    markdown.to_enum(:scan, regex).map { Regexp.last_match }
+    regex = /^!!! *?([\w-]+)(?: "(.*?)")?(?: <(.*?)>)?(?:[\s]+$)+([\d\D]+?)(?=(^\S|$))/
+    markdown.to_enum(:scan, regex).map {Regexp.last_match}
   end
 
   # https://github.com/code-dot-org/remark-plugins/blob/master/src/tiplink.js
   def self.find_tip_links(markdown)
     regex = /^([\w-]+)!!! ?([\w-]+)(?:<!-- place where you'd like the icon -->)?(.+?)$/
-    markdown.to_enum(:scan, regex).map { Regexp.last_match }
+    markdown.to_enum(:scan, regex).map {Regexp.last_match}
+  end
+
+  def self.format_tip_markdown(markdown)
+    markdown = markdown.chomp.reverse.chomp.reverse
+    markdown.gsub(/^\t/, '')
   end
 
   def self.create_tip(type, key, markdown)
@@ -167,7 +181,7 @@ module LessonImportHelper
       "discussion" => "discussionGoal",
       "assessment" => "assessmentOpportunity"
     }
-    {key: key, type: tip_map[key], markdown: markdown.strip}
+    {key: key, type: tip_map[key], markdown: format_tip_markdown(markdown)}
   end
 
   def self.create_activity_section_with_tip(tip_link_match, tip_match_map)
@@ -178,7 +192,17 @@ module LessonImportHelper
     tip_match = tip[:match]
     tip_match_map.delete(tip_link_match[2]) if tip_match
     activity_section = ActivitySection.new
-    activity_section.description = tip_link_match[3].strip
+
+    # Sometimes theres the the slide icon here as well. Check for that and apply if needed.
+    description = tip_link_match[3].strip
+    slide_matches = find_slides(description)
+    if slide_matches.empty? || description.index(slide_matches[0][0]) != 0
+      activity_section.description = description.strip
+    else
+      activity_section.description = description.delete_prefix(slide_matches[0][0])
+      activity_section.slide = true
+    end
+
     activity_section.tips = [create_tip(tip_match[3] || "#{tip_match[1]}-0", tip_match[1] || "tip", tip_match[4])]
     activity_section
   end
@@ -190,20 +214,14 @@ module LessonImportHelper
     end
     (0...existing_matches.length).each do |i|
       matches.push(existing_matches[i])
-      if i < existing_matches.length - 1 && existing_matches[i][:index] + existing_matches[i][:substring].length < existing_matches[i+1][:index]
-        start_index = existing_matches[i][:index] + existing_matches[i][:substring].length
-        substring = markdown[start_index...existing_matches[i+1][:index]].strip
-        matches.push({index: start_index, type: 'markdown', substring: substring}) unless substring.empty?
-      end
+      next unless i < existing_matches.length - 1 && existing_matches[i][:index] + existing_matches[i][:substring].length < existing_matches[i + 1][:index]
+      start_index = existing_matches[i][:index] + existing_matches[i][:substring].length
+      substring = markdown[start_index...existing_matches[i + 1][:index]].strip
+      matches.push({index: start_index, type: 'markdown', substring: substring}) unless substring.empty?
     end
     unless matches.last[:index] + matches.last[:substring].length == markdown.length
       matches.push({index: 0, type: 'markdown', substring: markdown[existing_matches.last[:index] + existing_matches.last[:substring].length...markdown.length]})
     end
     matches
-  end
-
-  def self.find_level_chunks(levels, existing_level_chunks)
-    start = 1
-
   end
 end
