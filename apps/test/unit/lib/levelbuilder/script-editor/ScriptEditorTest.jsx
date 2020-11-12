e53d3cb6fdc30703a496fc1ac9d77081485d84e4
Dani <dani@code.org>
2020-11-12 17:04:19 -0500

    Update ScriptEditorTest

--- apps/test/unit/lib/levelbuilder/script-editor/ScriptEditorTest.jsx
+++ apps/test/unit/lib/levelbuilder/script-editor/ScriptEditorTest.jsx
@@ -19,7 +19,10 @@ describe('ScriptEditor', () => {
     scriptFamilies: [],
     teacherResources: [],
     versionYearOptions: [],
-    initialFamilyName: ''
+    initialFamilyName: '',
+    initialTeacherResources: [],
+    initialProjectSharing: false,
+    initialLocales: []
   };
 
   describe('Script Editor', () => {
@@ -49,7 +52,7 @@ describe('ScriptEditor', () => {
             ]}
           />
         );
-        assert.deepEqual(wrapper.state('resources'), [
+        assert.deepEqual(wrapper.state('teacherResources'), [
           {type: ResourceType.curriculum, link: '/foo'},
           {type: '', link: ''},
           {type: '', link: ''},
@@ -65,7 +68,9 @@ describe('ScriptEditor', () => {
     });
 
     it('has the correct number of each editor field type', () => {
-      const wrapper = mount(<ScriptEditor {...DEFAULT_PROPS} hidden={false} />);
+      const wrapper = mount(
+        <ScriptEditor {...DEFAULT_PROPS} initialHidden={false} />
+      );
       expect(wrapper.find('input').length).to.equal(23);
       expect(wrapper.find('input[type="checkbox"]').length).to.equal(11);
       expect(wrapper.find('textarea').length).to.equal(2);
@@ -74,7 +79,9 @@ describe('ScriptEditor', () => {
     });
 
     it('has correct markdown for preview of unit description', () => {
-      const wrapper = mount(<ScriptEditor {...DEFAULT_PROPS} hidden={false} />);
+      const wrapper = mount(
+        <ScriptEditor {...DEFAULT_PROPS} initialHidden={false} />
+      );
       expect(wrapper.find('TextareaWithMarkdownPreview').length).to.equal(1);
       expect(
         wrapper.find('TextareaWithMarkdownPreview').prop('markdown')
@@ -84,7 +91,9 @@ describe('ScriptEditor', () => {
     });
 
     it('must set family name in order to check standalone course', () => {
-      const wrapper = mount(<ScriptEditor {...DEFAULT_PROPS} hidden={false} />);
+      const wrapper = mount(
+        <ScriptEditor {...DEFAULT_PROPS} initialHidden={false} />
+      );
       let courseCheckbox = wrapper.find('input[name="is_course"]');
       let familyNameSelect = wrapper.find('select[name="family_name"]');
 
@@ -104,13 +113,17 @@ describe('ScriptEditor', () => {
 
   describe('VisibleInTeacherDashboard', () => {
     it('is checked when hidden is false', () => {
-      const wrapper = mount(<ScriptEditor {...DEFAULT_PROPS} hidden={false} />);
+      const wrapper = mount(
+        <ScriptEditor {...DEFAULT_PROPS} initialHidden={false} />
+      );
       const checkbox = wrapper.find('input[name="visible_to_teachers"]');
       expect(checkbox.prop('checked')).to.be.true;
     });
 
     it('is unchecked when hidden is true', () => {
-      const wrapper = mount(<ScriptEditor {...DEFAULT_PROPS} hidden={true} />);
+      const wrapper = mount(
+        <ScriptEditor {...DEFAULT_PROPS} initialHidden={false} />
+      );
       const checkbox = wrapper.find('input[name="visible_to_teachers"]');
       expect(checkbox.prop('checked')).to.be.false;
     });
