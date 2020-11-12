7be45e91a95eaec490e3bf147a3e7e40849cb30d
Dani <dani@code.org>
2020-11-12 16:23:18 -0500

    LessonExtrasEditorTest updated

--- apps/test/unit/lib/levelbuilder/script-editor/LessonExtrasEditorTest.jsx
+++ apps/test/unit/lib/levelbuilder/script-editor/LessonExtrasEditorTest.jsx
@@ -2,16 +2,29 @@ import {expect} from 'chai';
 import React from 'react';
 import {mount} from 'enzyme';
 import LessonExtrasEditor from '@cdo/apps/lib/levelbuilder/script-editor/LessonExtrasEditor';
-
-const DEFAULT_PROPS = {
-  projectWidgetVisible: false,
-  projectWidgetTypes: [],
-  lessonExtrasAvailable: false
-};
+import sinon from 'sinon';
 
 describe('LessonExtrasEditor', () => {
+  let defaultProps,
+    updateLessonExtrasAvailable,
+    updateProjectWidgetVisible,
+    updateProjectWidgetTypes;
+
+  beforeEach(() => {
+    updateLessonExtrasAvailable = sinon.spy();
+    updateProjectWidgetVisible = sinon.spy();
+    updateProjectWidgetTypes = sinon.spy();
+    defaultProps = {
+      projectWidgetVisible: false,
+      projectWidgetTypes: [],
+      lessonExtrasAvailable: false,
+      updateLessonExtrasAvailable,
+      updateProjectWidgetVisible,
+      updateProjectWidgetTypes
+    };
+  });
   it('project settings are not visible when lesson extras is not checked', () => {
-    const wrapper = mount(<LessonExtrasEditor {...DEFAULT_PROPS} />);
+    const wrapper = mount(<LessonExtrasEditor {...defaultProps} />);
     expect(wrapper.find('input[name="project_widget_visible"]')).to.have.length(
       0
     );
@@ -22,7 +35,7 @@ describe('LessonExtrasEditor', () => {
 
   it('project settings are visible when lesson extras is checked', () => {
     const wrapper = mount(
-      <LessonExtrasEditor {...DEFAULT_PROPS} lessonExtrasAvailable={true} />
+      <LessonExtrasEditor {...defaultProps} lessonExtrasAvailable={true} />
     );
 
     expect(wrapper.find('input[name="project_widget_visible"]')).to.have.length(
