9daa6a4012009922631816d489eca38e6330edaa
Erin Peach <erin@code.org>
2020-11-11 16:34:44 -0800

    setSerialPort in wrapper with branch for serialPort and Chrome serialPort

--- apps/src/lib/kits/maker/boards/microBit/MBFirmataWrapper.js
+++ apps/src/lib/kits/maker/boards/microBit/MBFirmataWrapper.js
@@ -1,5 +1,6 @@
 import MBFirmataClient from '../../../../../third-party/maker/MBFirmataClient';
 import {SAMPLE_INTERVAL} from './MicroBitConstants';
+import {isNodeSerialAvailable} from '../../portScanning';
 
 export const ACCEL_EVENT_ID = 27;
 
@@ -15,6 +16,52 @@ export default class MicrobitFirmataWrapper extends MBFirmataClient {
       .then(() => {
         return this.setAnalogSamplingInterval(SAMPLE_INTERVAL);
       });
+  }
+
+  // Used in setSerialPort
+  dataReceived(data) {
+    if (this.inbufCount + data.length < this.inbuf.length) {
+      this.inbuf.set(data, this.inbufCount);
+      this.inbufCount += data.length;
+      this.processFirmatMessages();
+    }
+  }
+
+  setSerialPort(port) {
+    if (isNodeSerialAvailable()) {
+      return super.setSerialPort(port);
+    } else {
+      // Use the given port. Assume the port has been opened by the caller.
+
+      this.myPort = port;
+      this.myPort.on('data', this.dataReceived.bind(this));
+      this.requestFirmataVersion();
+      this.requestFirmwareVersion();
+
+      // get the board serial number (used to determine board version)
+      this.boardVersion = '';
+      return Promise.resolve();
+
+      // The below code snippet is from the MBFirmataClient setSerialPort
+      // that is not included in this branch for the ChromeSerialPort.
+      // The Chrome Serial Port doesn't return .list() as a promise, so this\
+      // code doesn't work in that library. As of 11/11/20, we do not use the
+      // boardVersion in our MB integration.
+      // TODO - rewrite this promise code to work for the Chrome Serial Port.
+      /*
+      // get the board serial number (used to determine board version)
+      this.boardVersion = '';
+      return serialport.list()
+      .then((ports) => {
+        for (var i = 0; i < ports.length; i++) {
+          var p = ports[i];
+          if ((p.comName == this.myPort.path)) {
+            this.boardVersion = this.boardVersionFromSerialNumber(p.serialNumber);
+          }
+        }
+        return null;
+      })*/
+    }
   }
 
   setPinMode(pin, mode) {
