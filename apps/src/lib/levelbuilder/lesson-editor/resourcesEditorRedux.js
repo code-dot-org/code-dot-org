baa6b0dc90d1d7ce0df6f41ab9e43917ad146574
Bethany Connor <bethany@code.org>
2020-11-17 20:27:47 +0000

    Add update route for resources and allow the resource form to also update a resource

--- apps/src/lib/levelbuilder/lesson-editor/resourcesEditorRedux.js
+++ apps/src/lib/levelbuilder/lesson-editor/resourcesEditorRedux.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 const INIT = 'resourcesEditor/INIT';
 const ADD_RESOURCE = 'resourcesEditor/ADD_RESOURCE';
+const EDIT_RESOURCE = 'resourcesEditor/EDIT_RESOURCE';
 const REMOVE_RESOURCE = 'resourcesEditor/REMOVE_RESOURCE';
 
 export const initResources = resources => ({
@@ -12,6 +13,11 @@ export const initResources = resources => ({
 export const addResource = newResource => ({
   type: ADD_RESOURCE,
   newResource
+});
+
+export const editResource = updatedResource => ({
+  type: EDIT_RESOURCE,
+  updatedResource
 });
 
 export const removeResource = key => ({
@@ -27,6 +33,13 @@ export default function resources(state = [], action) {
       return action.resources;
     case ADD_RESOURCE: {
       newState = newState.concat([action.newResource]);
+      break;
+    }
+    case EDIT_RESOURCE: {
+      const resourceToEdit = newState.find(
+        resource => resource.key === action.editedResource.key
+      );
+      Object.assign(resourceToEdit, action.editedResource);
       break;
     }
     case REMOVE_RESOURCE: {
