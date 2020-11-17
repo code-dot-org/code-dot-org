0faa666cb84f349a3ccf53e960dabff1c1190af5
Erin Bond <erin.bond@code.org>
2020-11-16 21:09:43 -0500

    create -> save, model name in params, generate random id

--- apps/src/ailab/Ailab.js
+++ apps/src/ailab/Ailab.js
@@ -125,10 +125,11 @@ Ailab.prototype.onContinue = function() {
 Ailab.prototype.initMLActivities = function() {
   const mode = this.level.mode ? JSON.parse(this.level.mode) : null;
   const onContinue = this.onContinue.bind(this);
-  const saveTrainedModel = trainedModel => {
+  const saveTrainedModel = (model_name, trainedModel) => {
+    console.log('model_name', model_name);
     $.ajax({
       method: 'POST',
-      url: '/api/v1/ml_models/123/save',
+      url: '/api/v1/ml_models/' + model_name + '/save',
       type: 'json',
       contentType: 'application/json;charset=UTF-8',
       data: JSON.stringify(trainedModel)
--- dashboard/app/controllers/api/v1/ml_models_controller.rb
+++ dashboard/app/controllers/api/v1/ml_models_controller.rb
@@ -4,14 +4,19 @@ require 'cdo/aws/s3'
 class Api::V1::MlModelsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  # POST api/v1//ml_models/:model_id/save
+  # POST api/v1//ml_models/:model_name/save
   # Save a trained ML model to S3
-  def create
-    upload_to_s3(params[:model_id], params[:ml_model].to_json)
+  def save
+    model_id = generate_id
+    upload_to_s3(model_id, params[:ml_model].to_json)
     render json: "hooray!"
   end
 
   private
+
+  def generate_id
+    SecureRandom.alphanumeric(12)
+  end
 
   def upload_to_s3(model_id, trained_model)
     AWS::S3.upload_to_bucket('cdo-v3-trained-ml-models', model_id, trained_model, no_random: true)
@@ -19,6 +24,6 @@ class Api::V1::MlModelsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def ml_model_params
-    params.require(:ml_model).permit(:user_id, :model_id, :model_name, :trained_model)
+    params.require(:ml_model).permit(:model_name, :trained_model)
   end
 end
