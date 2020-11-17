b0ca0166684ace33c9ae5f357ca1ab830fb5219c
Dani <dani@code.org>
2020-11-17 08:29:22 -0500

    Fix props in CourseEditor on edit.js

--- apps/src/sites/studio/pages/courses/edit.js
+++ apps/src/sites/studio/pages/courses/edit.js
@@ -25,9 +25,11 @@ function showCourseEditor() {
         versionTitle={courseEditorData.course_summary.version_title}
         familyName={courseEditorData.course_summary.family_name}
         versionYear={courseEditorData.course_summary.version_year}
-        visible={courseEditorData.course_summary.visible}
+        initialVisible={courseEditorData.course_summary.visible}
         isStable={courseEditorData.course_summary.is_stable}
-        pilotExperiment={courseEditorData.course_summary.pilot_experiment}
+        initialPilotExperiment={
+          courseEditorData.course_summary.pilot_experiment
+        }
         descriptionShort={courseEditorData.course_summary.description_short}
         initialDescriptionStudent={
           courseEditorData.course_summary.description_student || ''
