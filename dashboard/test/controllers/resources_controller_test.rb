5c4b6ac1fd2546a241e440858bf6f3dc6ef285e9
Bethany Connor <bethany@code.org>
2020-11-17 23:47:53 +0000

    Fix/add test

--- dashboard/test/controllers/resources_controller_test.rb
+++ dashboard/test/controllers/resources_controller_test.rb
@@ -11,7 +11,7 @@ class ResourcesControllerTest < ActionController::TestCase
   test 'can create resource from params' do
     sign_in @levelbuilder
     assert_creates(Resource) do
-      post :create, params: {name: 'resource name', url: 'code.org', downloadUrl: 'download.url', type: 'Slides', audience: 'Teacher'}
+      post :create, params: {name: 'resource name', url: 'code.org', download_url: 'download.url', type: 'Slides', audience: 'Teacher'}
       assert_response :success
     end
     assert(@response.body.include?('resource name'))
@@ -19,5 +19,20 @@ class ResourcesControllerTest < ActionController::TestCase
     assert(@response.body.include?('Slides'))
     assert(@response.body.include?('Teacher'))
     assert(@response.body.include?('download.url'))
+  end
+
+  test 'can update resource from params' do
+    sign_in @levelbuilder
+    resource = create :resource, name: 'original name', type: 'Slides'
+    post :update, params: {id: resource.id, name: 'new name'}
+    assert_response :success
+
+    # Assert the response has both the new field with the right value
+    # and an old unchanged field
+    assert(@response.body.include?('new name'))
+    assert(@response.body.include?('Slides'))
+
+    resource.reload
+    assert_equal 'new name', resource.name
   end
 end
