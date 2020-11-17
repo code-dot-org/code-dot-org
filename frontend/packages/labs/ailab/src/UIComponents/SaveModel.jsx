2b2e32066f731338270573206e307e0247a57d2b
Erin Bond <erin.bond@code.org>
2020-11-17 10:08:53 -0500

    input boxes and save model name and description to state

--- frontend/packages/labs/ailab/src/UIComponents/SaveModel.jsx
+++ frontend/packages/labs/ailab/src/UIComponents/SaveModel.jsx
@@ -2,16 +2,27 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { setTrainedModelDetails, getTrainedModelDataToSave } from "../redux";
 
 class SaveModel extends Component {
   static propTypes = {
     saveTrainedModel: PropTypes.func,
-    trainedModel: PropTypes.object
+    trainedModel: PropTypes.object,
+    setTrainedModelDetails: PropTypes.func,
+    trainedModelDetails: PropTypes.object,
+    dataToSave: PropTypes.object
+  };
+
+  handleChange = (event, field) => {
+    const trainedModelDetails = this.props.trainedModelDetails;
+    trainedModelDetails[field] = event.target.value;
+    this.props.setTrainedModelDetails(trainedModelDetails);
   };
 
   onClickSave = () => {
+    console.log("dataToSave", this.props.dataToSave);
+    this.props.saveTrainedModel(this.props.dataToSave);
     alert("Don't get too excited, this doesn't do anything yet :)");
-    this.props.saveTrainedModel();
   };
 
   render() {
@@ -19,6 +30,19 @@ class SaveModel extends Component {
       <div>
         <br />
         <br />
+        <label>
+          Model name:
+          <input
+            type="text"
+            onChange={event => this.handleChange(event, "name")}
+          />
+        </label>
+        <label>
+          Model description:
+          <textarea
+            onChange={event => this.handleChange(event, "description")}
+          />
+        </label>
         <button type="button" onClick={this.onClickSave}>
           Save Trained Model
         </button>
@@ -27,6 +51,15 @@ class SaveModel extends Component {
   }
 }
 
-export default connect(state => ({
-  trainedModel: state.trainedModel
-}))(SaveModel);
+export default connect(
+  state => ({
+    trainedModel: state.trainedModel,
+    trainedModelDetails: state.trainedModelDetails,
+    dataToSave: getTrainedModelDataToSave(state)
+  }),
+  dispatch => ({
+    setTrainedModelDetails(trainedModelDetails) {
+      dispatch(setTrainedModelDetails(trainedModelDetails));
+    }
+  })
+)(SaveModel);
