35afe4e1efc5dc4d33c2079b3cfaa8ad05bc5e70
Elijah Hamovitz <elijahhamovitz@gmail.com>
2020-11-18 15:41:04 -0800

    use native Rail manual connection switching rather than seamless database pool for enforcing that requests take place only on the read replica

--- dashboard/app/controllers/admin_search_controller.rb
+++ dashboard/app/controllers/admin_search_controller.rb
@@ -6,50 +6,49 @@ class AdminSearchController < ApplicationController
   before_action :require_admin
   check_authorization
 
-  include SeamlessDatabasePool::ControllerFilter
-  use_database_pool find_students: :persistent
-
   MAX_PAGE_SIZE = 50
   MAX_TOTAL_SIZE = 1000
 
   def find_students
-    users = User.with_deleted
+    ActiveRecord::Base.connected_to(role: :reading) do
+      users = User.with_deleted
 
-    # If requested, filter...
-    if params[:studentNameFilter].present?
-      users = users.where("name LIKE ?", "%#{params[:studentNameFilter]}%")
-    end
-    if params[:studentEmailFilter].present?
-      hashed_email = User.hash_email params[:studentEmailFilter]
-      users = users.where(hashed_email: hashed_email)
-    end
-    if params[:teacherNameFilter].present? || params[:teacherEmailFilter].present?
-      teachers = User.
-        where("name LIKE ?", "%#{params[:teacherNameFilter]}%").
-        where("email LIKE ?", "%#{params[:teacherEmailFilter]}%").
-        all
-      if teachers.count > 1
-        flash[:alert] = 'Multiple teachers matched the name and email search criteria.'
+      # If requested, filter...
+      if params[:studentNameFilter].present?
+        users = users.where("name LIKE ?", "%#{params[:studentNameFilter]}%")
       end
-      if teachers.first
-        array_of_student_ids = teachers.first.students.pluck(:id)
-        users = users.where(id: array_of_student_ids)
+      if params[:studentEmailFilter].present?
+        hashed_email = User.hash_email params[:studentEmailFilter]
+        users = users.where(hashed_email: hashed_email)
       end
+      if params[:teacherNameFilter].present? || params[:teacherEmailFilter].present?
+        teachers = User.
+          where("name LIKE ?", "%#{params[:teacherNameFilter]}%").
+          where("email LIKE ?", "%#{params[:teacherEmailFilter]}%").
+          all
+        if teachers.count > 1
+          flash[:alert] = 'Multiple teachers matched the name and email search criteria.'
+        end
+        if teachers.first
+          array_of_student_ids = teachers.first.students.pluck(:id)
+          users = users.where(id: array_of_student_ids)
+        end
+      end
+      if params[:sectionFilter].present?
+        section = Section.with_deleted.find_by_code params[:sectionFilter]
+        if section.nil?
+          flash[:alert] = 'Section not found.'
+        elsif section.deleted?
+          flash[:alert] = 'Section is deleted.'
+        end
+        if section
+          array_of_student_ids = section.students.pluck(:id)
+          users = users.where(id: array_of_student_ids)
+        end
+      end
+      @total_count = users.limit(MAX_TOTAL_SIZE).size
+      @users = users.page(params[:page]).per(MAX_PAGE_SIZE)
     end
-    if params[:sectionFilter].present?
-      section = Section.with_deleted.find_by_code params[:sectionFilter]
-      if section.nil?
-        flash[:alert] = 'Section not found.'
-      elsif section.deleted?
-        flash[:alert] = 'Section is deleted.'
-      end
-      if section
-        array_of_student_ids = section.students.pluck(:id)
-        users = users.where(id: array_of_student_ids)
-      end
-    end
-    @total_count = users.limit(MAX_TOTAL_SIZE).size
-    @users = users.page(params[:page]).per(MAX_PAGE_SIZE)
   end
 
   def lookup_section
--- dashboard/app/controllers/application_controller.rb
+++ dashboard/app/controllers/application_controller.rb
@@ -8,9 +8,6 @@ class ApplicationController < ActionController::Base
   include LocaleHelper
   include ApplicationHelper
 
-  include SeamlessDatabasePool::ControllerFilter
-  # use_database_pool :all => :master
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
