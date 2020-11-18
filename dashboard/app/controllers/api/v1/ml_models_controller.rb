c3bec59c8e8a6a7935d4e288464b91a436565594
Erin Bond <erin.bond@code.org>
2020-11-18 15:34:42 -0500

    update comment, remove params definition

--- dashboard/app/controllers/api/v1/ml_models_controller.rb
+++ dashboard/app/controllers/api/v1/ml_models_controller.rb
@@ -4,7 +4,7 @@ require 'cdo/aws/s3'
 class Api::V1::MlModelsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  # POST api/v1//ml_models/:model_name/save
+  # POST api/v1//ml_models/save
   # Save a trained ML model to S3
   def save
     model_id = generate_id
@@ -20,10 +20,5 @@ class Api::V1::MlModelsController < ApplicationController
 
   def upload_to_s3(model_id, trained_model)
     AWS::S3.upload_to_bucket('cdo-v3-trained-ml-models', model_id, trained_model, no_random: true)
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def ml_model_params
-    params.require(:ml_model).permit(:model_name, :trained_model)
   end
 end
