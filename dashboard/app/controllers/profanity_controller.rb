3f7710d5aba529b924b39ace7e9ae0ac2706526c
Madelyn Kasula <maddie.kasula@code.org>
2020-11-19 08:18:07 -0800

    Rename language param -> locale, always cache

--- dashboard/app/controllers/profanity_controller.rb
+++ dashboard/app/controllers/profanity_controller.rb
@@ -3,21 +3,24 @@ class ProfanityController < ApplicationController
 
   # POST /profanity/find
   # @param [String] params[:text] String to test
-  # @param [String] params[:language] Language of the text to test
-  # @param [Boolean] params[:should_cache] Whether or not we should use the cache on this request
+  # @param [String] params[:locale] Locale to test in. Optional. Uses request locale if not provided.
   # @returns [Array<String>|nil] Profane words within the given string
   def find
-    profanity_result = []
-    unless params[:text].nil?
-      if params[:should_cache]
-        cache_key = "profanity/#{params[:language] || request.locale}/#{params[:text]}"
-        profanity_result = Rails.cache.fetch(cache_key) do
-          ProfanityFilter.find_potential_profanities(params[:text], params[:language] || request.locale)
-        end
-      else
-        profanity_result = ProfanityFilter.find_potential_profanities(params[:text], params[:language] || request.locale)
-      end
+    profanity_result = nil
+    if params[:text]&.present?
+      cache_key = "profanity/#{locale}/#{params[:text]}"
+      profanity_result = Rails.cache.fetch(cache_key) {find_potential_profanities}
     end
     render json: profanity_result
+  end
+
+  private
+
+  def locale
+    params[:locale] || request.locale
+  end
+
+  def find_potential_profanities
+    ProfanityFilter.find_potential_profanities(params[:text], locale)
   end
 end
