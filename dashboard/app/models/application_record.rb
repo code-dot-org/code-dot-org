19106374cf56cbabac50be0c3c641af3a31611da
Elijah Hamovitz <elijahhamovitz@gmail.com>
2020-11-24 12:30:50 -0800

    update database configuration to always configure a primary and a replica (even if they're actually the same db), and update our models to use this configuration

--- dashboard/app/models/application_record.rb
+++ dashboard/app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # Configure all of our models to distribute requests between the primary and
+  # replica databases
+  connects_to database: {writing: :primary, reading: :primary_replica}
 end
