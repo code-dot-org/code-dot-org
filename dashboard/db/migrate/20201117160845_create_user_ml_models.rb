c2fc5059425caa60bba36a5e6ae547cae0ff139e
Erin Bond <erin.bond@code.org>
2020-11-25 13:44:01 -0500

    add indexes, deleted_at and purged_at

--- dashboard/db/migrate/20201117160845_create_user_ml_models.rb
+++ dashboard/db/migrate/20201117160845_create_user_ml_models.rb
@@ -4,8 +4,12 @@ class CreateUserMlModels < ActiveRecord::Migration[5.1]
       t.integer :user_id
       t.string :model_id
       t.string :name
+      t.datetime :deleted_at
+      t.datetime :purged_at
 
       t.timestamps
     end
+    add_index :user_ml_models, :user_id
+    add_index :user_ml_models, :model_id
   end
 end
