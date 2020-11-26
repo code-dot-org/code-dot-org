d817d2b0e613637f1bc627a97dfe1cad4d244fc7
Anjali <anjali@code.org>
2020-11-25 16:23:52 -0800

    very basic FieldImageDropdown

---
+++ apps/src/blocklyAddons/cdoFieldImageDropdown.js
@@ -0,0 +1,7 @@
+import CdoFieldDropdown from './cdoFieldDropdown';
+
+export default class FieldImageDropdown extends CdoFieldDropdown {
+  constructor(menuGenerator, width, height, buttons) {
+    super(menuGenerator);
+  }
+}
