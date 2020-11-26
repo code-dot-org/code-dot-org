2fa356dd8fd3baeb9b037dd744be20e4aaf108a0
Anjali <anjali@code.org>
2020-11-25 16:45:31 -0800

    restructure menuGenerator to match google's format

--- apps/src/blocklyAddons/cdoFieldImageDropdown.js
+++ apps/src/blocklyAddons/cdoFieldImageDropdown.js
@@ -2,6 +2,26 @@ import CdoFieldDropdown from './cdoFieldDropdown';
 
 export default class FieldImageDropdown extends CdoFieldDropdown {
   constructor(menuGenerator, width, height, buttons) {
-    super(menuGenerator);
+    // Google Blockly supports images in dropdowns but has a different format,
+    // so we just need to restructure our menu items before passing through to
+    // the FieldDropdown constructor.
+    // CDO Blockly format: Each menu item is a two element array: the first is
+    // the image url; the second is the generated code.
+    // Google Blockly format: Each menu item is a two element array: the first is
+    // an object containing the image url, width, height, and alt text; the second
+    // is the generated code.
+    let menuItems = [];
+    menuGenerator.forEach(menuItem => {
+      let url = menuItem[0];
+      let code_id = menuItem[1];
+      // TODO: add better alt text. For now, it's just using the code name for the
+      // image, but that's not necessarily a student-friendly string. (for example,
+      // in Basketball, the hand dropdown has hand_1, hand_2, and hand_3, which might
+      // benefit from more descriptive alt text.)
+      let option = {src: url, width: width, height: height, alt: code_id};
+      menuItems.push([option, code_id]);
+    });
+
+    super(menuItems);
   }
 }
