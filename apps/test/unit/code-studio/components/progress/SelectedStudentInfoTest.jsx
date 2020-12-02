4b71dd7d70d4208e1f63f3eab65f409b7314450e
Jessica Kulwik <jessica@code.org>
2020-12-02 19:24:25 +0000

    Updated tests to be more mergable with the 'progress investigation' PR

--- apps/test/unit/code-studio/components/progress/SelectedStudentInfoTest.jsx
+++ apps/test/unit/code-studio/components/progress/SelectedStudentInfoTest.jsx
@@ -7,7 +7,7 @@ import {LevelStatus} from '@cdo/apps/util/sharedConstants';
 const defaultProps = {
   selectedStudent: {id: 1, name: 'Student 1'},
   level: {
-    id: 1,
+    id: 123,
     assessment: null,
     contained: false,
     driver: null,
