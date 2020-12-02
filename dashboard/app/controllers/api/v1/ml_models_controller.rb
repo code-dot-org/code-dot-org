b82a4b7bb6e1c2dfd3cc86b5205528599463c3d8
Erin Bond <erin.bond@code.org>
2020-12-02 15:51:11 -0500

    add download from s3 to ml models api

--- dashboard/app/controllers/api/v1/ml_models_controller.rb
+++ dashboard/app/controllers/api/v1/ml_models_controller.rb
@@ -4,12 +4,21 @@ require 'cdo/aws/s3'
 class Api::V1::MlModelsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  # POST api/v1//ml_models/save
+  S3_BUCKET = 'cdo-v3-trained-ml-models'
+
+  # POST api/v1/ml_models/save
   # Save a trained ML model to S3
   def save
     model_id = generate_id
     upload_to_s3(model_id, params["ml_model"].to_json)
     render json: "hooray!"
+  end
+
+  # GET api/v1/ml_models/:model_id
+  # Retrieve a trained ML model from S3
+  def get_trained_model
+    model = download_from_s3(params[:model_id])
+    render json: model
   end
 
   private
@@ -19,6 +28,10 @@ class Api::V1::MlModelsController < ApplicationController
   end
 
   def upload_to_s3(model_id, trained_model)
-    AWS::S3.upload_to_bucket('cdo-v3-trained-ml-models', model_id, trained_model, no_random: true)
+    AWS::S3.upload_to_bucket(S3_BUCKET, model_id, trained_model, no_random: true)
+  end
+
+  def download_from_s3(model_id)
+    AWS::S3.download_from_bucket(S3_BUCKET, model_id)
   end
 end
