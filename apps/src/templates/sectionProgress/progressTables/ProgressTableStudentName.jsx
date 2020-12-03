2f48d0a68012abff2626a6f3454733d4706998e1
Charlie Forkish <cforkish@gmail.com>
2020-12-03 15:29:20 -0800

    moved student tooltips into ProgressTableStudentName

--- apps/src/templates/sectionProgress/progressTables/ProgressTableStudentName.jsx
+++ apps/src/templates/sectionProgress/progressTables/ProgressTableStudentName.jsx
@@ -1,17 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
+import moment from 'moment';
 import {tooltipIdForStudent} from '../sectionProgressConstants';
 import {scriptUrlForStudent} from '@cdo/apps/templates/teacherDashboard/urlHelpers';
 import firehoseClient from '../../../lib/util/firehose';
+import i18n from '@cdo/locale';
 
+const styles = {
+  tooltip: {
+    display: 'flex',
+    textAlign: 'center'
+  }
+};
 export default class ProgressTableStudentName extends React.PureComponent {
   static propTypes = {
     name: PropTypes.string.isRequired,
     studentId: PropTypes.number.isRequired,
     sectionId: PropTypes.number.isRequired,
-
     scriptName: PropTypes.string,
-    scriptId: PropTypes.number
+    scriptId: PropTypes.number,
+    lastTimestamp: PropTypes.number,
+    localeCode: PropTypes.string
   };
 
   constructor(props) {
@@ -35,6 +45,32 @@ export default class ProgressTableStudentName extends React.PureComponent {
     );
   }
 
+  renderTooltip() {
+    const {studentId, lastTimestamp, localeCode} = this.props;
+    const tooltipId = tooltipIdForStudent(studentId);
+    if (localeCode) {
+      moment.locale(localeCode);
+    }
+    const timestamp = lastTimestamp
+      ? moment(lastTimestamp).calendar()
+      : i18n.none();
+    return (
+      <ReactTooltip
+        id={tooltipId}
+        key={tooltipId}
+        role="tooltip"
+        wrapper="span"
+        effect="solid"
+      >
+        <span style={styles.tooltip}>
+          {i18n.lastProgress()}
+          <br />
+          {timestamp}
+        </span>
+      </ReactTooltip>
+    );
+  }
+
   render() {
     const {name, studentId, sectionId, scriptName} = this.props;
     const studentUrl = scriptUrlForStudent(sectionId, scriptName, studentId);
@@ -42,6 +78,7 @@ export default class ProgressTableStudentName extends React.PureComponent {
 
     return (
       <div data-tip data-for={tooltipId} aria-describedby={tooltipId}>
+        {this.renderTooltip()}
         {studentUrl && (
           <a href={studentUrl} onClick={this.recordStudentNameClick}>
             {name}
