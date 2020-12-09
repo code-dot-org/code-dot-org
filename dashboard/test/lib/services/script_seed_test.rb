adea46fa86455da0f5edef1e2a11883b136f3537
David Bailey <davidsbailey@users.noreply.github.com>
2020-12-08 16:22:31 -0800

    seed Resources and LessonsResources

--- dashboard/test/lib/services/script_seed_test.rb
+++ dashboard/test/lib/services/script_seed_test.rb
@@ -54,7 +54,7 @@ module Services
       # this is slower for most individual Scripts, but there could be a savings when seeding multiple Scripts.
       # For now, leaving this as a potential future optimization, since it seems to be reasonably fast as is.
       # The game queries can probably be avoided with a little work, though they only apply for Blockly levels.
-      assert_queries(52) do
+      assert_queries(54) do
         ScriptSeed.seed_from_json(json)
       end
 
