abf64f1cc37b72498f8589411383beb43e81488d
David Bailey <davidsbailey@users.noreply.github.com>
2020-12-09 22:04:45 -0800

    update lesson-resources annotation

--- dashboard/app/models/lessons_resource.rb
+++ dashboard/app/models/lessons_resource.rb
@@ -7,7 +7,7 @@
 #
 # Indexes
 #
-#  index_lessons_resources_on_lesson_id_and_resource_id  (lesson_id,resource_id)
+#  index_lessons_resources_on_lesson_id_and_resource_id  (lesson_id,resource_id) UNIQUE
 #  index_lessons_resources_on_resource_id_and_lesson_id  (resource_id,lesson_id)
 #
 class LessonsResource < ApplicationRecord
