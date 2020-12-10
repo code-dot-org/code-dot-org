53e12adc6480a7d63aa0783af726e456e37bbd9d
Erin Bond <erin.bond@code.org>
2020-12-09 20:05:00 -0500

    save a reference to a trained model in the db

--- dashboard/app/controllers/api/v1/ml_models_controller.rb
+++ dashboard/app/controllers/api/v1/ml_models_controller.rb
@@ -4,10 +4,15 @@ require 'cdo/aws/s3'
 class Api::V1::MlModelsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  # POST api/v1//ml_models/save
-  # Save a trained ML model to S3
+  # POST api/v1/ml_models/save
+  # Save a trained ML model to S3 and a reference to it in the database.
   def save
     model_id = generate_id
+    UserMlModel.create!(
+      user_id: current_user.id,
+      model_id: model_id,
+      name: params["ml_model"]["name"]
+    )
     upload_to_s3(model_id, params["ml_model"].to_json)
     render json: "hooray!"
   end
