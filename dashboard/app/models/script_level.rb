ab12f59b5de1ce19a30dee714c2d97a72af2016e
David Bailey <davidsbailey@users.noreply.github.com>
2020-12-10 17:08:40 -0800

    add comment about identifying other objects

--- dashboard/app/models/script_level.rb
+++ dashboard/app/models/script_level.rb
@@ -605,7 +605,8 @@ class ScriptLevel < ApplicationRecord
     anonymous? && user.try(:teacher?) && !viewed_user.nil? && user != viewed_user
   end
 
-  # Used for seeding from JSON. Returns the full set of information needed to uniquely identify this object.
+  # Used for seeding from JSON. Returns the full set of information needed to
+  # uniquely identify this object as well as any other objects it belongs to.
   # If the attributes of this object alone aren't sufficient, and associated objects are needed, then data from
   # the seeding_keys of those objects should be included as well.
   # Ideally should correspond to a unique index for this model's table.
