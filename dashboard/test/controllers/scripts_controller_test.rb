936e5aac74a0f5c1b49cbbbb28be4275625b88a6
Dani <dani@code.org>
2020-12-11 08:02:27 -0500

    Add test to check suceeds updating if dsl matches

--- dashboard/test/controllers/scripts_controller_test.rb
+++ dashboard/test/controllers/scripts_controller_test.rb
@@ -490,6 +490,31 @@ class ScriptsControllerTest < ActionController::TestCase
     assert_includes error.message, 'Could not update the script because the contents of one of its lessons or levels has changed outside of this editor. Reload the page and try saving again.'
   end
 
+  test 'can update if database matches starting content for current edit page' do
+    sign_in @levelbuilder
+    Rails.application.config.stubs(:levelbuilder_mode).returns true
+
+    script = create :script
+    lesson_group = create :lesson_group, script: script
+    lesson = create :lesson, script: script, lesson_group: lesson_group
+    create(
+      :script_level,
+      script: script,
+      lesson: lesson,
+      levels: [create(:maze, name: 'ScriptActivity test level 1')]
+    )
+    stub_file_writes(script.name)
+
+    post :update, params: {
+      id: script.id,
+      script: {name: script.name},
+      script_text: '',
+      old_script_text: ScriptDSL.serialize_lesson_groups(script)
+    }
+
+    assert_response :success
+  end
+
   test 'updates teacher resources' do
     sign_in @levelbuilder
     Rails.application.config.stubs(:levelbuilder_mode).returns true
