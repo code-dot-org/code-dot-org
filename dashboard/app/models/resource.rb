98387f5135dd0749e252fc44678aa34dd2208e2a
David Bailey <davidsbailey@users.noreply.github.com>
2020-12-14 08:48:27 -0800

    update comments on new seeding_key methods

--- dashboard/app/models/resource.rb
+++ dashboard/app/models/resource.rb
@@ -48,11 +48,12 @@ class Resource < ApplicationRecord
     self.key = generate_key_from_name
   end
 
-  # Used for seeding from JSON. Returns the full set of information needed to uniquely identify this object.
-  # If the attributes of this object alone aren't sufficient, and associated objects are needed, then data from
-  # the seeding_keys of those objects should be included as well.
-  # Ideally should correspond to a unique index for this model's table.
-  # See comments on ScriptSeed.seed_from_json for more context.
+  # Used for seeding from JSON. Returns the full set of information needed to
+  # uniquely identify this object as well as any other objects it belongs to.
+  # If the attributes of this object alone aren't sufficient, and associated
+  # objects are needed, then data from the seeding_keys of those objects should
+  # be included as well. Ideally should correspond to a unique index for this
+  # model's table. See comments on ScriptSeed.seed_from_json for more context.
   #
   # @param [ScriptSeed::SeedContext] _seed_context - contains preloaded data to use when looking up associated objects
   # @return [Hash<String, String>] all information needed to uniquely identify this object across environments.
