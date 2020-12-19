c8403a233b8148aed6bd81adcddb994677e65697
Madelyn Kasula <maddie.kasula@code.org>
2020-12-18 16:53:11 -0800

    Move bramble URLs to constants for clarity

--- dashboard/app/controllers/weblab_host_controller.rb
+++ dashboard/app/controllers/weblab_host_controller.rb
@@ -1,8 +1,11 @@
 class WeblabHostController < ApplicationController
   layout false
 
+  BRAMBLE_URL = 'https://downloads.computinginthecore.org/bramble_0.1.29'
+  BRAMBLE_LOCALHOST_URL = 'http://127.0.0.1:8000/src'
+
   def index
     @dev_mode = false; # Change to true to point to Bramble running on localhost
-    @bramble_base_url = @dev_mode ? 'http://127.0.0.1:8000/src' : 'https://downloads.computinginthecore.org/bramble_0.1.29'
+    @bramble_base_url = @dev_mode ? BRAMBLE_LOCALHOST_URL : BRAMBLE_URL
   end
 end
