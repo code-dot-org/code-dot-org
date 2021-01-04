20e4957e2950a79b9d1a7901c38a70b77c4440f3
Bethany Connor <bethany@code.org>
2021-01-04 15:57:44 -0800

    Use onclickoutside to save objectives if the user clicks outside the input box

--- apps/src/lib/levelbuilder/lesson-editor/ObjectivesEditor.jsx
+++ apps/src/lib/levelbuilder/lesson-editor/ObjectivesEditor.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
+import onClickOutside from 'react-onclickoutside';
 import color from '@cdo/apps/util/color';
 
 const styles = {
@@ -51,88 +52,90 @@ const styles = {
   }
 };
 
-class ObjectiveLine extends React.Component {
-  static propTypes = {
-    editing: PropTypes.bool,
-    description: PropTypes.string.isRequired,
-    onSave: PropTypes.func.isRequired,
-    onEditCancel: PropTypes.func.isRequired,
-    onEditClick: PropTypes.func.isRequired,
-    onRemove: PropTypes.func.isRequired,
-    lineStyle: PropTypes.object
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      description: props.description
+const ObjectiveLine = onClickOutside(
+  class ObjectiveLine extends React.Component {
+    static propTypes = {
+      editing: PropTypes.bool,
+      description: PropTypes.string.isRequired,
+      onSave: PropTypes.func.isRequired,
+      onEditCancel: PropTypes.func.isRequired,
+      onEditClick: PropTypes.func.isRequired,
+      onRemove: PropTypes.func.isRequired,
+      lineStyle: PropTypes.object
     };
-  }
 
-  handleClickOutside = evt => {
-    if (this.state.currentlyEditingIndex) {
-      this.props.onSave(this.state.description);
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        description: props.description
+      };
     }
-  };
 
-  handleCancelClick = e => {
-    this.setState({description: this.props.description});
-    this.props.onEditCancel();
-  };
+    handleClickOutside = evt => {
+      if (this.props.editing) {
+        this.props.onSave(this.state.description);
+      }
+    };
 
-  render() {
-    return (
-      <tr style={this.props.lineStyle}>
-        <td style={{height: 30}}>
-          {this.props.editing ? (
-            <input
-              value={this.state.description}
-              onChange={e =>
-                this.setState({
-                  description: e.target.value
-                })
-              }
-              onKeyDown={e => {
-                if (e.key === 'Enter') {
-                  this.props.onSave(this.state.description);
+    handleCancelClick = e => {
+      this.setState({description: this.props.description});
+      this.props.onEditCancel();
+    };
+
+    render() {
+      return (
+        <tr style={this.props.lineStyle}>
+          <td style={{height: 30}}>
+            {this.props.editing ? (
+              <input
+                value={this.state.description}
+                onChange={e =>
+                  this.setState({
+                    description: e.target.value
+                  })
                 }
-              }}
-              style={{
-                width: '98%'
-              }}
-              type="text"
-            />
+                onKeyDown={e => {
+                  if (e.key === 'Enter') {
+                    this.props.onSave(this.state.description);
+                  }
+                }}
+                style={{
+                  width: '98%'
+                }}
+                type="text"
+              />
+            ) : (
+              <div>{this.state.description}</div>
+            )}
+          </td>
+          {this.props.editing ? (
+            <td style={styles.actionButtons}>
+              <div
+                style={styles.save}
+                onMouseDown={() => this.props.onSave(this.state.description)}
+              >
+                <i className="fa fa-check" />
+              </div>
+              <div style={styles.remove} onMouseDown={this.handleCancelClick}>
+                <i className="fa fa-times" />
+              </div>
+            </td>
           ) : (
-            <div>{this.state.description}</div>
+            <td style={styles.actionButtons}>
+              <div style={styles.edit} onMouseDown={this.props.onEditClick}>
+                <i className="fa fa-edit" />
+              </div>
+              <div style={styles.remove} onMouseDown={this.props.onRemove}>
+                <i className="fa fa-trash" />
+              </div>
+            </td>
           )}
-        </td>
-        {this.props.editing ? (
-          <td style={styles.actionButtons}>
-            <div
-              style={styles.save}
-              onMouseDown={() => this.props.onSave(this.state.description)}
-            >
-              <i className="fa fa-check" />
-            </div>
-            <div style={styles.remove} onMouseDown={this.handleCancelClick}>
-              <i className="fa fa-times" />
-            </div>
-          </td>
-        ) : (
-          <td style={styles.actionButtons}>
-            <div style={styles.edit} onMouseDown={this.props.onEditClick}>
-              <i className="fa fa-edit" />
-            </div>
-            <div style={styles.remove} onMouseDown={this.props.onRemove}>
-              <i className="fa fa-trash" />
-            </div>
-          </td>
-        )}
-      </tr>
-    );
+        </tr>
+      );
+    }
   }
-}
+);
 
 export default class ObjectivesEditor extends Component {
   static propTypes = {
