9f00a29582e98d05ef3b543d066e1ba0054ed3b2
Bethany Connor <bethany@code.org>
2021-01-07 15:41:28 -0800

    Move ObjectiveLine into its own file

--- apps/src/lib/levelbuilder/lesson-editor/ObjectivesEditor.jsx
+++ apps/src/lib/levelbuilder/lesson-editor/ObjectivesEditor.jsx
@@ -1,44 +1,11 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
-import onClickOutside from 'react-onclickoutside';
 import color from '@cdo/apps/util/color';
+import ObjectiveLine from './ObjectiveLine';
 
 const styles = {
   oddRow: {
     backgroundColor: color.lightest_gray
-  },
-  actionButtons: {
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    backgroundColor: 'white',
-    height: 30
-  },
-  remove: {
-    fontSize: 14,
-    color: 'white',
-    background: color.dark_red,
-    cursor: 'pointer',
-    textAlign: 'center',
-    minWidth: '50%',
-    lineHeight: '30px'
-  },
-  edit: {
-    fontSize: 14,
-    color: 'white',
-    background: color.default_blue,
-    cursor: 'pointer',
-    textAlign: 'center',
-    minWidth: '50%',
-    lineHeight: '30px'
-  },
-  save: {
-    fontSize: 14,
-    color: 'white',
-    background: color.green,
-    cursor: 'pointer',
-    textAlign: 'center',
-    minWidth: '50%',
-    lineHeight: '30px'
   },
   addButton: {
     background: color.cyan,
@@ -51,99 +18,6 @@ const styles = {
     marginLeft: 0
   }
 };
-
-const ObjectiveLine = onClickOutside(
-  class ObjectiveLine extends React.Component {
-    static propTypes = {
-      editing: PropTypes.bool,
-      description: PropTypes.string.isRequired,
-      onSave: PropTypes.func.isRequired,
-      onEditCancel: PropTypes.func.isRequired,
-      onEditClick: PropTypes.func.isRequired,
-      onRemove: PropTypes.func.isRequired,
-      lineStyle: PropTypes.object
-    };
-
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        description: props.description
-      };
-    }
-
-    handleClickOutside = evt => {
-      if (this.props.editing) {
-        this.props.onSave(this.state.description);
-      }
-    };
-
-    handleCancelClick = e => {
-      this.setState({description: this.props.description});
-      this.props.onEditCancel();
-    };
-
-    render() {
-      return (
-        <tr style={this.props.lineStyle}>
-          <td style={{height: 30}}>
-            {this.props.editing ? (
-              <input
-                value={this.state.description}
-                onChange={e =>
-                  this.setState({
-                    description: e.target.value
-                  })
-                }
-                onKeyDown={e => {
-                  if (e.key === 'Enter') {
-                    this.props.onSave(this.state.description);
-                  }
-                }}
-                style={{
-                  width: '98%'
-                }}
-                type="text"
-              />
-            ) : (
-              <div>{this.state.description}</div>
-            )}
-          </td>
-          {this.props.editing ? (
-            <td style={styles.actionButtons}>
-              <div
-                style={styles.save}
-                onMouseDown={() => this.props.onSave(this.state.description)}
-              >
-                <i className="fa fa-check" />
-              </div>
-              <div
-                style={styles.remove}
-                className="unit-test-cancel-edit"
-                onMouseDown={this.handleCancelClick}
-              >
-                <i className="fa fa-times" />
-              </div>
-            </td>
-          ) : (
-            <td style={styles.actionButtons}>
-              <div style={styles.edit} onMouseDown={this.props.onEditClick}>
-                <i className="fa fa-edit" />
-              </div>
-              <div
-                style={styles.remove}
-                className="unit-test-remove-objective"
-                onMouseDown={this.props.onRemove}
-              >
-                <i className="fa fa-trash" />
-              </div>
-            </td>
-          )}
-        </tr>
-      );
-    }
-  }
-);
 
 export default class ObjectivesEditor extends Component {
   static propTypes = {
