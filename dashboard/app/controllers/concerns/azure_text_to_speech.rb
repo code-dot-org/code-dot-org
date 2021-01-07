81003e4d22db4be216c2d2a5697b006c5e24d88a
Madelyn Kasula <maddie.kasula@code.org>
2021-01-07 14:29:17 -0800

    Use CDO.shared_cache ins AzureTextToSpeech instead of Rails.cache

--- dashboard/app/controllers/concerns/azure_text_to_speech.rb
+++ dashboard/app/controllers/concerns/azure_text_to_speech.rb
@@ -15,7 +15,7 @@ module AzureTextToSpeech
   def self.get_token
     return nil unless allowed?
 
-    Rails.cache.fetch("azure_speech_service/token", expires_in: TOKEN_CACHE_TTL) do
+    CDO.shared_cache.fetch("azure_speech_service/token", expires_in: TOKEN_CACHE_TTL) do
       token_uri = URI.parse("https://#{region}.api.cognitive.microsoft.com/sts/v1.0/issueToken")
       token_http_request = Net::HTTP.new(token_uri.host, token_uri.port)
       token_http_request.use_ssl = true
@@ -71,7 +71,7 @@ module AzureTextToSpeech
     token = get_token
     return nil if token.nil_or_empty?
 
-    Rails.cache.fetch("azure_speech_service/voices") do
+    CDO.shared_cache.fetch("azure_speech_service/voices") do
       voice_uri = URI.parse("https://#{region}.tts.speech.microsoft.com/cognitiveservices/voices/list")
       voice_http_request = Net::HTTP.new(voice_uri.host, voice_uri.port)
       voice_http_request.use_ssl = true
