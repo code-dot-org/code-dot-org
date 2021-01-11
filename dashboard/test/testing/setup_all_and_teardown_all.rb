66f089a2cdc1b04b78e9604c9e94f973be709899
Elijah Hamovitz <elijahhamovitz@gmail.com>
2021-01-11 11:47:27 -0800

    Rewrite setup_all and teardown_all
    To mirror the way `setup` and `teardown` are implemented in Rails 6.0 vs 5.0
    
    Also remove some of the ancillary features we currently have, for simplicity; at least some of those will likely get added back in before merging.

--- dashboard/test/testing/setup_all_and_teardown_all.rb
+++ dashboard/test/testing/setup_all_and_teardown_all.rb
@@ -1,69 +1,41 @@
-# Submitted upstream at https://github.com/rails/rails/pull/28178
-require "active_support/concern"
 require "active_support/callbacks"
 
 module ActiveSupport
   module Testing
     # Add support for +setup_all+ and +teardown_all+ callbacks.
+    # These callbacks serve as an extension to the existing <tt>#setup</tt> and
+    # <tt>#teardown</tt> methods, and allow you to define callbacks which
+    # happen just once before and after the individual tests run.
+    #
+    #   class ExampleTest < ActiveSupport::TestCase
+    #     setup_all do
+    #       # ...
+    #     end
+    #
+    #     teardown_all do
+    #       # ...
+    #     end
+    #   end
     module SetupAllAndTeardownAll
-      extend ActiveSupport::Concern
-
-      included do
-        include ActiveSupport::Callbacks
-
-        # It's not 100% clear why this is necessary, but here's what we do know:
-        #
-        # Between Rails 5.0 and 5.2, the implementation of ActiveSupport callbacks was refactored to
-        # improve the backtrace experience (https://github.com/rails/rails/pull/26147). This
-        # refactoring made some changes to the way the callbacks are stored on the class, and this
-        # change seems to have resulted in our extension not being correctly applied to descendant
-        # classes. Specifically, we found ourselves in a situation where these callbacks were
-        # defined correctly on the ActiveSupport:TestCase class, but only on that class.
-        #
-        # Our naive solution is to explicitly define the callbacks on all descendant classes here at
-        # inclusion time, rather than trying to rely on class inheritance as we did before.
-        ([self] + ActiveSupport::DescendantsTracker.descendants(self)).reverse_each do |target|
-          target.define_callbacks :setup_all, :teardown_all
-        end
+      def self.prepended(klass)
+        klass.include ActiveSupport::Callbacks
+        klass.define_callbacks :setup_all, :teardown_all
+        klass.extend ClassMethods
       end
 
       module ClassMethods
         def run(reporter, options = {})
-          @reporter = reporter
-          @instance = run_callbacks :setup_all
-          # @time is set by `time_it`, and needs to be removed before the instance is reused.
-          @instance.remove_instance_variable :@time
+          run_callbacks :setup_all
           super(reporter, options)
           run_callbacks :teardown_all
         end
 
-        # Return a singleton instance for running individual tests,
-        # so instance variables can be duplicated across all tests and callbacks.
-        def new(name)
-          @instance ||= super
-          instance = @instance.dup
-          instance.name = name
-          instance.failures = []
-          instance
-        end
-
-        private
-
-        def run_callbacks(name)
-          instance = new(name.to_s)
-          instance.time_it do
-            instance.capture_exceptions do
-              instance.run_callbacks(name)
-            end
-          end
-          @reporter.record instance if instance.failure
-          instance
-        end
-
+        # Add a callback, which runs once before any individual test is run
         def setup_all(*args, &block)
           set_callback(:setup_all, :before, *args, &block)
         end
 
+        # Add a callback, which runs once after all individual tests are run
         def teardown_all(*args, &block)
           set_callback(:teardown_all, :after, *args, &block)
         end
