ad2bdd45a1361f7b026e15c7416fca9f120be199
Dani <dani@code.org>
2021-01-12 14:46:47 -0500

    Only look for lessons in script if script exists

--- dashboard/db/migrate/20210109194325_set_has_lesson_plan_from_lockable.rb
+++ dashboard/db/migrate/20210109194325_set_has_lesson_plan_from_lockable.rb
@@ -6,8 +6,10 @@ class SetHasLessonPlanFromLockable < ActiveRecord::Migration[5.2]
       lesson.save!
     end
     scripts = ['csp1-2020', 'csp2-2020', 'csp3-2020', 'csp4-2020', 'csp5-2020', 'csp6-2020', 'csp7-2020', 'csp8-2020', 'csp9-2020', 'csp10-2020', 'csp1-2021', 'csp2-2021', 'csp3-2021', 'csp4-2021', 'csp5-2021', 'csp6-2021', 'csp7-2021', 'csp8-2021', 'csp9-2021', 'csp10-2021']
-    scripts.each do |script|
-      lesson = Script.find_by(name: script).lessons.last
+    scripts.each do |script_name|
+      script = Script.find_by(name: script_name)
+      next unless script
+      lesson = script.lessons.last
       lesson.has_lesson_plan = true
       lesson.save!
     end
