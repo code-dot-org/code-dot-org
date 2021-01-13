392b5a998ee2da400d50e86171d565af24933032
Bethany Connor <bethany@code.org>
2021-01-13 14:10:41 -0800

    Add more tests to SearchBoxTest

--- apps/test/unit/lib/levelbuilder/lesson-editor/SearchBoxTest.js
+++ apps/test/unit/lib/levelbuilder/lesson-editor/SearchBoxTest.js
@@ -1,25 +1,67 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import sinon from 'sinon';
 import {expect} from '../../../../util/reconfiguredChai';
 import SearchBox from '@cdo/apps/lib/levelbuilder/lesson-editor/SearchBox';
 
 describe('SearchBox', () => {
-  let defaultProps, onSearchSelect, constructOptions;
+  let defaultProps, constructOptions, fetch_spy;
 
   beforeEach(() => {
-    onSearchSelect = sinon.spy();
     constructOptions = sinon.spy();
+    fetch_spy = sinon.stub(window, 'fetch');
     defaultProps = {
-      onSearchSelect,
+      onSearchSelect: () => {},
       courseVersionId: 1,
       searchUrl: 'fakesearch',
       constructOptions
     };
   });
 
+  afterEach(() => {
+    fetch_spy.restore();
+  });
+
   it('renders default props', () => {
     const wrapper = shallow(<SearchBox {...defaultProps} />);
     expect(wrapper.find('Async').length).to.equal(1);
+  });
+
+  it('searches when query is 3+ letters', () => {
+    const wrapper = mount(<SearchBox {...defaultProps} />);
+    let returnData = [{result: 'res1'}];
+    fetch_spy.returns(
+      Promise.resolve({ok: true, json: () => JSON.stringify(returnData)})
+    );
+    return wrapper
+      .instance()
+      .getOptions('abc')
+      .then(() => {
+        expect(fetch_spy).to.have.been.calledWith(
+          '/fakesearch?query=abc&limit=7&courseVersionId=1'
+        );
+        expect(constructOptions).to.have.been.calledWith(
+          JSON.stringify(returnData)
+        );
+      });
+  });
+
+  it('doesnt when query is < 3 letters', () => {
+    const wrapper = mount(<SearchBox {...defaultProps} />);
+    let returnData = [{result: 'res1'}];
+    fetch_spy.returns(
+      Promise.resolve({ok: true, json: () => JSON.stringify(returnData)})
+    );
+    return wrapper
+      .instance()
+      .getOptions('ab')
+      .then(() => {
+        expect(fetch_spy).to.not.have.been.calledWith(
+          '/fakesearch?query=abc&limit=7&courseVersionId=1'
+        );
+        expect(constructOptions).to.not.have.been.calledWith(
+          JSON.stringify(returnData)
+        );
+      });
   });
 });
