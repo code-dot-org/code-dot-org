6056d708ff225c09a65a46af9b1917a0c33e008b
Dani <dani@code.org>
2021-01-13 17:59:02 -0500

    Update LessonEditorTest

--- apps/test/unit/lib/levelbuilder/lesson-editor/LessonEditorTest.js
+++ apps/test/unit/lib/levelbuilder/lesson-editor/LessonEditorTest.js
@@ -40,6 +40,7 @@ describe('LessonEditor', () => {
         studentOverview: 'Overview of the lesson for students',
         unplugged: false,
         lockable: false,
+        hasLessonPlan: true,
         assessment: false,
         creativeCommonsLicense: 'Creative Commons BY-NC-SA',
         purpose: 'The purpose of the lesson is for people to learn',
@@ -83,7 +84,7 @@ describe('LessonEditor', () => {
     ).to.be.true;
     expect(wrapper.find('Connect(ActivitiesEditor)').length).to.equal(1);
     expect(wrapper.find('TextareaWithMarkdownPreview').length).to.equal(5);
-    expect(wrapper.find('input').length).to.equal(17);
+    expect(wrapper.find('input').length).to.equal(18);
     expect(wrapper.find('select').length).to.equal(1);
     expect(wrapper.find('AnnouncementsEditor').length).to.equal(1);
     expect(wrapper.find('CollapsibleEditorSection').length).to.equal(8);
