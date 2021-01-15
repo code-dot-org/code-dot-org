8b9946086884a3c0145b946573f96b8723406c89
Anjali <anjali@code.org>
2021-01-15 15:09:31 -0800

    remove experiment

--- apps/src/p5lab/P5LabVisualizationColumn.jsx
+++ apps/src/p5lab/P5LabVisualizationColumn.jsx
@@ -66,9 +66,6 @@ class P5LabVisualizationColumn extends React.Component {
     this.spritelabPauseExperiment = experiments.isEnabled(
       experiments.SPRITELAB_PAUSE
     );
-    this.spritelabInputExperiment = experiments.isEnabled(
-      experiments.SPRITELAB_INPUT
-    );
   }
 
   // Cache app-space mouse coordinates, which we get from the
@@ -198,7 +195,7 @@ class P5LabVisualizationColumn extends React.Component {
             </VisualizationOverlay>
           </ProtectedVisualizationDiv>
           <TextConsole consoleMessages={this.props.consoleMessages} />
-          {this.spritelabInputExperiment && <SpritelabInput />}
+          <SpritelabInput />
         </div>
 
         <GameButtons>
