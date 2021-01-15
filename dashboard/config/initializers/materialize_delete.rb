f9b7e80073e4a6668f117f7104eaf58717cb7f89
Elijah Hamovitz <elijahhamovitz@gmail.com>
2021-01-15 15:14:59 -0800

    also restore materialize_transactions call on query, not just delete

--- dashboard/config/initializers/materialize_delete.rb
+++ dashboard/config/initializers/materialize_delete.rb
@@ -1,4 +1,9 @@
 module MaterializeTransactionsOnExecDelete
+  def exec_query(sql, name = "SQL", binds = [], prepare: false)
+    materialize_transactions
+    super
+  end
+
   def exec_delete(sql, name = nil, binds = [])
     materialize_transactions
     super
