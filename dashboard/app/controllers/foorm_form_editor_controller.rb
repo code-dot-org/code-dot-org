eb97adfd528fc7744b45c6284e7fe67185a32b40
Ben Brooks <ben@code.org>
2021-01-15 14:25:09 -0800

    New route for library question editor

--- dashboard/app/controllers/foorm_form_editor_controller.rb
+++ dashboard/app/controllers/foorm_form_editor_controller.rb
@@ -1,10 +1,10 @@
 # Foorm Editor is only available on levelbuilder or test, for those with levelbuilder permissions.
-class FoormEditorController < ApplicationController
+class FoormFormEditorController < ApplicationController
   before_action :require_levelbuilder_mode_or_test_env
   before_action :authenticate_user!
   authorize_resource class: false
 
-  # GET '/foorm/editor/'
+  # GET '/foorm/editor/forms'
   def index
     formatted_names_and_versions = Foorm::Form.all.map {|form| {name: form.name, version: form.version, id: form.id}}
     categories = formatted_names_and_versions.map {|data| data[:name].slice(0, data[:name].rindex('/'))}.uniq
@@ -16,6 +16,6 @@ class FoormEditorController < ApplicationController
       }.to_json
     }
 
-    render 'foorm/editor/index'
+    render 'foorm/forms_editor/index'
   end
 end
