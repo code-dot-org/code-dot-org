2ba6158541bed5557abdda82f1d67694a059620e
Ben Brooks <ben@code.org>
2021-01-22 17:28:14 -0800

    Add tests for library question editing controllers

--- dashboard/app/controllers/foorm/library_questions_controller.rb
+++ dashboard/app/controllers/foorm/library_questions_controller.rb
@@ -1,15 +1,17 @@
 module Foorm
   # Foorm Library Editor is only available on levelbuilder or test, for those with levelbuilder permissions.
-  class LibrariesController < ApplicationController
+  class LibraryQuestionsController < ApplicationController
     before_action :require_levelbuilder_mode_or_test_env
     before_action :authenticate_user!
-    authorize_resource
+    load_and_authorize_resource
 
+    # GET /foorm/library_questions/:id
     def show
       # Maybe update this to only return certain parts of the question?
       render json: @library_question.to_json
     end
 
+    # PUT /foorm/library_questions/:id/update
     def update
       @library_question.question = params[:question].to_json
 
