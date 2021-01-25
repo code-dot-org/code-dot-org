f1c3b6adeffdb8f0286b99d410ae9a61479f05c7
molly-moen <molly@code.org>
2021-01-25 13:56:06 -0800

    add ace-builds

--- apps/src/javalab/JavalabView.jsx
+++ apps/src/javalab/JavalabView.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-java';
+import 'ace-builds/src-noconflict/theme-dracula';
 
 export default class JavalabView extends React.Component {
   onChange = newValue => {
@@ -10,7 +12,7 @@ export default class JavalabView extends React.Component {
     return (
       <AceEditor
         mode="java"
-        theme="github"
+        theme="dracula"
         onChange={this.onChange}
         name="UNIQUE_ID_OF_DIV"
         editorProps={{$blockScrolling: true}}
