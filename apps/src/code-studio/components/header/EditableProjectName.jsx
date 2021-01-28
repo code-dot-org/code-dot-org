3574276693cd4f3032c1e52488506d6d73533369
Erin Peach <erin@code.org>
2021-01-28 09:37:42 -0800

    Make project header tabbable

--- apps/src/code-studio/components/header/EditableProjectName.jsx
+++ apps/src/code-studio/components/header/EditableProjectName.jsx
@@ -29,7 +29,7 @@ class UnconnectedDisplayProjectName extends React.Component {
   render() {
     return (
       <div style={styles.buttonWrapper}>
-        <div className="project_name_wrapper header_text">
+        <div className="project_name_wrapper header_text" tabIndex={0}>
           <div className="project_name header_text">
             {this.props.projectName}
           </div>
