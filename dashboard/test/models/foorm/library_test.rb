8e2de571e122f1ae218196318583e3ba7f2274b7
Ben Brooks <ben@code.org>
2021-01-29 09:58:05 -0800

    Pretty generate JSON in test

--- dashboard/test/models/foorm/library_test.rb
+++ dashboard/test/models/foorm/library_test.rb
@@ -4,18 +4,20 @@ class Foorm::LibraryTest < ActiveSupport::TestCase
   test 'library_formatted_as_json returns expected hash for library with multiple questions' do
     library = create :foorm_library, :with_questions, number_of_questions: 3
 
-    expected = {
-      'published' => true,
-      'pages' => [
-        {
-          'elements' => [
-            JSON.parse(library.library_questions.first.question),
-            JSON.parse(library.library_questions.second.question),
-            JSON.parse(library.library_questions.third.question)
-          ]
-        }
-      ]
-    }
+    expected = JSON.pretty_generate(
+      {
+        'published' => true,
+        'pages' => [
+          {
+            'elements' => [
+              JSON.parse(library.library_questions.first.question),
+              JSON.parse(library.library_questions.second.question),
+              JSON.parse(library.library_questions.third.question)
+            ]
+          }
+        ]
+      }
+    )
 
     assert_equal expected, library.formatted_for_file
   end
