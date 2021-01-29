215d4ac73a641e76690c169a941cf0693a74a9f7
Brendan Reville <brendan@code.org>
2021-01-29 11:49:05 +1100

    CrossTab: add some comments & only show when a column is not selected

--- frontend/packages/labs/ailab/src/UIComponents/CrossTab.jsx
+++ frontend/packages/labs/ailab/src/UIComponents/CrossTab.jsx
@@ -1,4 +1,7 @@
-/* React component to render a special kind of crosstab table. */
+/* React component to render a special kind of crosstab table, with a row for each
+ * active combination of features values, and corresponding percentage of label
+ * values, with a heatmap style applied. */
+
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { connect } from "react-redux";
@@ -7,6 +10,7 @@ import { ColumnTypes, styles } from "../constants.js";
 
 class CrossTab extends Component {
   static propTypes = {
+    currentColumn: PropTypes.string,
     crossTabData: PropTypes.object
   };
 
@@ -17,74 +21,77 @@ class CrossTab extends Component {
     };
   };
   render() {
-    const { crossTabData } = this.props;
+    const { currentColumn, crossTabData } = this.props;
 
     return (
       <div id="cross-tab">
-        <div style={styles.validationMessagesLight}>
-          {crossTabData && (
-            <table>
-              <thead>
-                <tr>
-                  <th colSpan={crossTabData.featureNames.length}>&nbsp;</th>
-                  <th colSpan={crossTabData.uniqueLabelValues.length}>
-                    {crossTabData.labelName}
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  {crossTabData.featureNames.map((featureName, index) => {
-                    return <td key={index}>{featureName}</td>;
-                  })}
+        {!currentColumn && (
+          <div style={styles.validationMessagesLight}>
+            {crossTabData && (
+              <table>
+                <thead>
+                  <tr>
+                    <th colSpan={crossTabData.featureNames.length}>&nbsp;</th>
+                    <th colSpan={crossTabData.uniqueLabelValues.length}>
+                      {crossTabData.labelName}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    {crossTabData.featureNames.map((featureName, index) => {
+                      return <td key={index}>{featureName}</td>;
+                    })}
 
-                  {crossTabData.uniqueLabelValues.map(
-                    (uniqueLabelValue, index) => {
-                      return (
-                        <td key={index} style={styles.dataDisplayCell}>
-                          {uniqueLabelValue}
-                        </td>
-                      );
-                    }
-                  )}
-                </tr>
-                {crossTabData.results.map((result, resultIndex) => {
-                  return (
-                    <tr key={resultIndex}>
-                      {result.featureValues.map(
-                        (featureValue, featureIndex) => {
-                          return <td key={featureIndex}>{featureValue}</td>;
-                        }
-                      )}
-                      {crossTabData.uniqueLabelValues.map(
-                        (uniqueLabelValue, labelIndex) => {
-                          return (
-                            <td
-                              key={labelIndex}
-                              style={this.getCellStyle(
-                                result.labelPercents[uniqueLabelValue]
-                              )}
-                            >
-                              {/*
-                            {result.labelCounts[uniqueLabelValue]}
-                            */}
-                              {result.labelPercents[uniqueLabelValue] || 0}%
-                            </td>
-                          );
-                        }
-                      )}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          )}
-        </div>
+                    {crossTabData.uniqueLabelValues.map(
+                      (uniqueLabelValue, index) => {
+                        return (
+                          <td key={index} style={styles.dataDisplayCell}>
+                            {uniqueLabelValue}
+                          </td>
+                        );
+                      }
+                    )}
+                  </tr>
+                  {crossTabData.results.map((result, resultIndex) => {
+                    return (
+                      <tr key={resultIndex}>
+                        {result.featureValues.map(
+                          (featureValue, featureIndex) => {
+                            return <td key={featureIndex}>{featureValue}</td>;
+                          }
+                        )}
+                        {crossTabData.uniqueLabelValues.map(
+                          (uniqueLabelValue, labelIndex) => {
+                            return (
+                              <td
+                                key={labelIndex}
+                                style={this.getCellStyle(
+                                  result.labelPercents[uniqueLabelValue]
+                                )}
+                              >
+                                {/*
+                              {result.labelCounts[uniqueLabelValue]}
+                              */}
+                                {result.labelPercents[uniqueLabelValue] || 0}%
+                              </td>
+                            );
+                          }
+                        )}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            )}
+          </div>
+        )}
       </div>
     );
   }
 }
 
 export default connect(state => ({
+  currentColumn: state.currentColumn,
   crossTabData: getCrossTabData(state)
 }))(CrossTab);
