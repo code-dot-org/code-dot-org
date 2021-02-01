af3a07bc0a18213d7a1e89494e76f3eb063f25ef
Erin Bond <erin.bond@code.org>
2021-02-01 16:56:02 -0500

    pass model id as a param to handle duplicate names and reduce ajax calls

--- apps/src/lib/util/mlApi.js
+++ apps/src/lib/util/mlApi.js
@@ -7,22 +7,16 @@ export const commands = {
   async getPrediction(opts) {
     return new Promise((resolve, reject) => {
       $.ajax({
-        url: '/api/v1/ml_models/names',
+        url: '/api/v1/ml_models/' + opts.modelId,
         method: 'GET'
       })
-        .then(data => {
-          const modelId = data.find(model => model.name === opts.model).id;
-          $.ajax({
-            url: '/api/v1/ml_models/' + modelId,
-            method: 'GET'
-          }).then(modelData => {
-            const predictParams = {
-              ...modelData,
-              testData: opts.testValues
-            };
-            const result = predict(predictParams);
-            opts.callback(result);
-          });
+        .then(modelData => {
+          const predictParams = {
+            ...modelData,
+            testData: opts.testValues
+          };
+          const result = predict(predictParams);
+          opts.callback(result);
           return resolve();
         })
         .fail((jqXhr, status) => {
