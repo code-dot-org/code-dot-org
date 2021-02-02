7e4a00423f0ff1447644145584a9a7624997eee9
David Bailey <davidsbailey@users.noreply.github.com>
2021-02-01 17:00:59 -0800

    make script_level_id column nullable in teacher_feedbacks table

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_005212) do
+ActiveRecord::Schema.define(version: 2021_02_02_005726) do
 
   create_table "activities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
@@ -1620,7 +1620,7 @@ ActiveRecord::Schema.define(version: 2021_01_30_005212) do
     t.integer "student_visit_count"
     t.datetime "student_first_visited_at"
     t.datetime "student_last_visited_at"
-    t.integer "script_level_id", null: false
+    t.integer "script_level_id"
     t.datetime "seen_on_feedback_page_at"
     t.integer "script_id", null: false
     t.index ["student_id", "level_id", "teacher_id"], name: "index_feedback_on_student_and_level_and_teacher_id"
