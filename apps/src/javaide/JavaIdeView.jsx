4ec38a4be702b5bdba9394d6fab7b8363ec5424c
molly-moen <molly@code.org>
2021-02-03 11:17:27 -0800

    update theme

--- apps/src/javaide/JavaIdeView.jsx
+++ apps/src/javaide/JavaIdeView.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-java';
-import 'ace-builds/src-noconflict/theme-dracula';
+import 'ace-builds/src-noconflict/theme-monokai';
 
 export default class JavaIdeView extends React.Component {
   onChange = newValue => {
@@ -12,9 +12,9 @@ export default class JavaIdeView extends React.Component {
     return (
       <AceEditor
         mode="java"
-        theme="dracula"
+        theme="monokai"
         onChange={this.onChange}
-        name="UNIQUE_ID_OF_DIV"
+        name="java_ide_editor"
         editorProps={{$blockScrolling: true}}
       />
     );
