32b189213ac73e0f35f786d57ab2672bd98d656d
Hannah Bergam <hannah.bergam@code.org>
2021-02-08 23:24:58 +0000

    troubleshooting unit tests

--- apps/test/unit/code-studio/components/header/SignInCalloutTest.js
+++ apps/test/unit/code-studio/components/header/SignInCalloutTest.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SignInCallout as Callout} from '@cdo/apps/code-studio/components/header/SignInCallout';
 import {shallow} from 'enzyme';
-import {expect, assert} from '../../../util/reconfiguredChai';
+import {expect, assert} from '../../../../util/reconfiguredChai';
 
 const CalloutProps = {
   showCallout: true
@@ -12,15 +12,10 @@ const CookieProps = {
 const wrapper = shallow(<Callout />);
 
 describe('ViewPopup', () => {
-  it('renders a sign in reminder popup', () => {
-    assert.equal(wrapper.find('.modal-backdrop').length, 1);
-    assert.equal(wrapper.find('.modal-backdrop').at(0), 'link');
-    assert.equal(wrapper.find('.modal-backdrop').opacity, 0.5);
-  });
-
   it('hides when dismissed', () => {
     wrapper.setProps(CalloutProps); // force a re-render
     expect(wrapper.find('.modal-backdrop').exists()).to.be.true;
+    assert.equal(wrapper.find('.modal-backdrop').opacity, 0.5);
     wrapper.setProps({showCallout: false});
     expect(wrapper.find('.modal-backdrop').exists()).to.be.false;
   });
