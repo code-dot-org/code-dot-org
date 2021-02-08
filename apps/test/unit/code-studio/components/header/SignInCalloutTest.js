465006ba49e334ada38d77f66a04024049e6e5c6
Hannah Bergam <hannah.bergam@code.org>
2021-02-08 22:59:19 +0000

    added testing for cookie cases

--- apps/test/unit/code-studio/components/header/SignInCalloutTest.js
+++ apps/test/unit/code-studio/components/header/SignInCalloutTest.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import {SignInCallout as Callout} from '@cdo/apps/code-studio/components/header/SignInCallout';
 import {shallow} from 'enzyme';
+import {expect, assert} from '../../../util/reconfiguredChai';
 
-var assert = require('assert');
+const CalloutProps = {
+  showCallout: true
+};
+const CookieProps = {
+  HideSignInCallout: true
+};
+const wrapper = shallow(<Callout />);
 
 describe('ViewPopup', () => {
   it('renders a sign in reminder popup', () => {
-    const wrapper = shallow(<Callout />);
-
     assert.equal(wrapper.find('.modal-backdrop').length, 1);
     assert.equal(wrapper.find('.modal-backdrop').at(0), 'link');
     assert.equal(wrapper.find('.modal-backdrop').opacity, 0.5);
+  });
+
+  it('hides when dismissed', () => {
+    wrapper.setProps(CalloutProps); // force a re-render
+    expect(wrapper.find('.modal-backdrop').exists()).to.be.true;
+    wrapper.setProps({showCallout: false});
+    expect(wrapper.find('.modal-backdrop').exists()).to.be.false;
+  });
+
+  it('hides when cookies are set', () => {
+    wrapper.setProps(CookieProps);
+    expect(wrapper.find('.modal-backdrop').exists()).to.be.false;
   });
 });
