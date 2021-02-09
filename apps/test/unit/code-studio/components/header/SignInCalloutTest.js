eacbc6ef2e012af74eaf7326fda1c9584dc03812
Hannah Bergam <hannah.bergam@code.org>
2021-02-09 22:59:54 +0000

    removing logic tests, adding language flexibility, WIP

--- apps/test/unit/code-studio/components/header/SignInCalloutTest.js
+++ apps/test/unit/code-studio/components/header/SignInCalloutTest.js
@@ -2,30 +2,18 @@ import React from 'react';
 import SignInCallout from '@cdo/apps/code-studio/components/header/SignInCallout';
 import {shallow} from 'enzyme';
 import {expect} from '../../../../util/reconfiguredChai';
+import i18n from '@cdo/locale';
 
-const CalloutProps = {
-  showCallout: true
-};
-const CookieProps = {
-  showCallout: false
-};
 const wrapper = shallow(<SignInCallout />);
 
 describe('ViewPopup', () => {
-  it('hides when dismissed', () => {
-    wrapper.setProps(CalloutProps); // force a re-render
-    expect(wrapper.find('div.modal-backdrop').exists()).to.be.true;
-    wrapper.setState({showCallout: false});
-    expect(wrapper.find('div.modal-backdrop').exists()).to.be.false;
-  });
-
-  it('hides when cookies are set', () => {
-    wrapper.setProps(CookieProps);
-    expect(wrapper.find('div.modal-backdrop').exists()).to.be.false;
-  });
-
   it('displays the correct background darkness', () => {
     wrapper.setState({showCallout: true});
     expect(wrapper.html().includes('opacity:0.5')).to.be.true;
+  });
+
+  it('shows the correct header and body text', () => {
+    expect(wrapper.html().includes(i18n.notSignedInHeader())).to.be.true;
+    expect(wrapper.html().includes(i18n.notSignedInBody())).to.be.true;
   });
 });
