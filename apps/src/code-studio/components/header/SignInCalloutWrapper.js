835acf97d4a850fb28db9add9078f10bf0d5f02e
Hannah Bergam <hannah.bergam@code.org>
2021-02-10 21:02:49 +0000

    renamed showCallout for clarity, renamed file to wrapper, edited return from empty div to null

--- apps/src/code-studio/components/header/SignInCalloutWrapper.js
+++ apps/src/code-studio/components/header/SignInCalloutWrapper.js
@@ -6,20 +6,19 @@ const HideSignInCallout = 'hide_signin_callout';
 
 // The use of both session storage and cookies is to check for 1 day
 // and 1 session, and display the callout again only once BOTH have passed.
-export default class SignInCalloutLogic extends React.Component {
+export default class SignInCalloutWrapper extends React.Component {
   constructor(props) {
     super(props);
     this.closeCallout = this.closeCallout.bind(this);
     this.state = {
-      showCallout: !(
+      hideCallout:
         cookies.get(HideSignInCallout) === 'true' ||
         sessionStorage.getItem(HideSignInCallout) === 'true'
-      )
     };
   }
 
   closeCallout(event) {
-    this.setState({showCallout: false});
+    this.setState({hideCallout: true});
     cookies.set(HideSignInCallout, 'true', {expires: 1, path: '/'});
     sessionStorage.setItem(HideSignInCallout, 'true');
     event.preventDefault();
@@ -29,14 +28,14 @@ export default class SignInCalloutLogic extends React.Component {
   // not supposed to be displayed. This avoids using a render statement that
   // often returns *nothing.
   render() {
-    if (this.state.showCallout) {
+    if (this.state.hideCallout) {
+      return null;
+    } else {
       return (
         <div className="uitest-signincallout">
           <SignInCallout handleClose={this.closeCallout} />
         </div>
       );
-    } else {
-      return <div />;
     }
   }
 }
