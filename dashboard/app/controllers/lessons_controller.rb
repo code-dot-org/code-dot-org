01bfeff00fbb04bc3dac727c707ae38922d65453
Dani <dani@code.org>
2021-02-11 12:35:18 -0500

    Make a helper method for finding script and lesson

--- dashboard/app/controllers/lessons_controller.rb
+++ dashboard/app/controllers/lessons_controller.rb
@@ -2,7 +2,7 @@ class LessonsController < ApplicationController
   load_and_authorize_resource
 
   before_action :require_levelbuilder_mode_or_test_env, except: [:show]
-  before_action :disallow_updating_legacy_script_levels, only: [:update]
+  before_action :disallow_legacy_script_levels, only: [:update]
   before_action :disallow_editing_legacy_script_levels, only: [:edit]
 
   # Script levels which are not in activity sections will not show up on the
@@ -10,46 +10,29 @@ class LessonsController < ApplicationController
   # script levels to be lost. Prevent this by disallowing editing in this case.
   # This helps avoid losing data from existing scripts by accidentally editing
   # them with the new lessons editor.
-  def disallow_updating_legacy_script_levels
+  def disallow_legacy_script_levels
     return unless @lesson.script_levels.reject(&:activity_section).any?
     raise CanCan::AccessDenied.new(
       "cannot edit lesson #{@lesson.id} because it contains legacy script levels"
     )
   end
 
-  # Script levels which are not in activity sections will not show up on the
-  # lesson edit page, in which case saving the edit page would cause those
-  # script levels to be lost. Prevent this by disallowing editing in this case.
-  # This helps avoid losing data from existing scripts by accidentally editing
-  # them with the new lessons editor.
   def disallow_editing_legacy_script_levels
-    @script = Script.find_by(name: params[:script_id])
-    raise ActiveRecord::RecordNotFound unless @script && @script.is_migrated
-    @lesson = Lesson.find_by(script: @script, has_lesson_plan: true, relative_position: params[:id])
-    raise ActiveRecord::RecordNotFound unless @lesson
+    @lesson = get_lesson_by_position_and_script(params[:script_id], params[:id])
 
-    return unless @lesson.script_levels.reject(&:activity_section).any?
-    raise CanCan::AccessDenied.new(
-      "cannot edit lesson #{@lesson.id} because it contains legacy script levels"
-    )
+    disallow_legacy_script_levels
   end
 
   # GET /s/script-name/lessons/1
   def show
-    @script = Script.find_by(name: params[:script_id])
-    raise ActiveRecord::RecordNotFound unless @script && @script.is_migrated
-    @lesson = Lesson.find_by(script: @script, has_lesson_plan: true, relative_position: params[:id])
-    raise ActiveRecord::RecordNotFound unless @lesson
+    @lesson = get_lesson_by_position_and_script(params[:script_id], params[:id])
 
     @lesson_data = @lesson.summarize_for_lesson_show(@current_user)
   end
 
   # GET /s/script-name/lessons/1/edit
   def edit
-    @script = Script.find_by(name: params[:script_id])
-    raise ActiveRecord::RecordNotFound unless @script && @script.is_migrated
-    @lesson = Lesson.find_by(script: @script, has_lesson_plan: true, relative_position: params[:id])
-    raise ActiveRecord::RecordNotFound unless @lesson
+    @lesson = get_lesson_by_position_and_script(params[:script_id], params[:id])
 
     @lesson_data = @lesson.summarize_for_lesson_edit
     # Return an empty list, because computing the list of related lessons here
@@ -144,5 +127,17 @@ class LessonsController < ApplicationController
     lp[:resources] = JSON.parse(lp[:resources]) if lp[:resources]
     lp[:vocabularies] = JSON.parse(lp[:vocabularies]) if lp[:vocabularies]
     lp
+  end
+
+  def get_lesson_by_position_and_script(script_name, relative_position)
+    script = Script.get_from_cache(script_name)
+    raise ActiveRecord::RecordNotFound unless script && script.is_migrated
+
+    lesson = script.lessons.find do |l|
+      l.has_lesson_plan && l.relative_position == relative_position.to_i
+    end
+    raise ActiveRecord::RecordNotFound unless lesson
+
+    lesson
   end
 end
