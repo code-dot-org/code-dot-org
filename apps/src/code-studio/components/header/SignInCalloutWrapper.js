2a7eb99f5cd1ed22a48f3865112f8b5e36dd1214
Hannah Bergam <hannah.bergam@code.org>
2021-02-11 18:25:37 +0000

    updated getContent to render, added header comment to wrapper file, and made onclick required

--- apps/src/code-studio/components/header/SignInCalloutWrapper.js
+++ apps/src/code-studio/components/header/SignInCalloutWrapper.js
@@ -4,12 +4,15 @@ import SignInCallout from './SignInCallout';
 
 const HideSignInCallout = 'hide_signin_callout';
 
-// The use of both session storage and cookies is to check for 1 day
-// and 1 session, and display the callout again only once BOTH have passed.
+// This class hold the display logic for the sign in callout, which prompts
+// students to log in in order to save their progress. The callout's freqency
+// is regulated here by the use of both session storage and cookies.
 export default class SignInCalloutWrapper extends React.Component {
   constructor(props) {
     super(props);
     this.closeCallout = this.closeCallout.bind(this);
+    // The use of both session storage and cookies is to check for 1 day
+    // and 1 session, and display the callout again only once BOTH have passed.
     this.state = {
       hideCallout:
         cookies.get(HideSignInCallout) === 'true' ||
