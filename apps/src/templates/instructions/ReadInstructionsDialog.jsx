8e60aad341b050ece50a526e2ad2e286f641570a
Maureen <maureen.sturgeon@gmail.com>
2021-02-10 17:56:46 -0800

    clean up and making dialog functional

--- apps/src/templates/instructions/ReadInstructionsDialog.jsx
+++ apps/src/templates/instructions/ReadInstructionsDialog.jsx
@@ -1,33 +1,54 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import BaseDialog from '@cdo/apps/templates/BaseDialog';
-import FontAwesome from '@cdo/apps/templates/FontAwesome';
+import Button from '@cdo/apps/templates/Button';
 
 const styles = {
   container: {
-    padding: '10px 30px'
+    padding: '0 20px 30px 20px'
   },
   buttonContainer: {
     display: 'flex'
   },
-  volumeIcon: {
-    fontSize: '75px'
+  button: {
+    height: '100%',
+    fontSize: '14px',
+    paddingBottom: '10px'
+  },
+  icon: {
+    fontSize: '75px',
+    display: 'block',
+    marginBottom: '10px'
   }
 };
 
-const ReadInstructionsDialog = () => {
+// TODO: add text to internationalization
+
+const ReadInstructionsDialog = ({
+  isOpen,
+  handleClose,
+  handleReadInstructions
+}) => {
   return (
-    <BaseDialog isOpen={true}>
+    <BaseDialog isOpen={isOpen} handleClose={handleClose}>
       <div style={styles.container}>
         <h2>Are you ready to continue?</h2>
         <div style={styles.buttonContainer}>
-          <button type="button">
-            <FontAwesome style={styles.volumeIcon} icon="volume-off" />
-            <h3>Do not read instructions</h3>
-          </button>
-          <button type="button">
-            <FontAwesome style={styles.volumeIcon} icon="volume-up" />
-            <h3>Read instructions to me</h3>
-          </button>
+          <Button
+            icon="volume-off"
+            color="gray"
+            iconStyle={styles.icon}
+            text="Do not read instructions"
+            onClick={handleClose}
+            style={{...styles.button, marginRight: '30px'}}
+          />
+          <Button
+            icon="volume-up"
+            iconStyle={styles.icon}
+            text="Read instructions to me"
+            onClick={handleReadInstructions}
+            style={styles.button}
+          />
         </div>
       </div>
     </BaseDialog>
@@ -36,4 +57,8 @@ const ReadInstructionsDialog = () => {
 
 export default ReadInstructionsDialog;
 
-ReadInstructionsDialog.propTypes = {};
+ReadInstructionsDialog.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  handleReadInstructions: PropTypes.func.isRequired
+};
