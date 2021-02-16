21f341e79216750354c50065f91bb1921bab4522
Jillian Khoo <jillian9012@gmail.com>
2021-02-16 02:45:43 +0000

    resolved comments about translations and added tests for the unit calendar dialog dropdown

--- apps/test/unit/code-studio/components/progress/UnitCalendarDialogTest.jsx
+++ apps/test/unit/code-studio/components/progress/UnitCalendarDialogTest.jsx
@@ -21,4 +21,50 @@ describe('UnitCalendarDialog', () => {
       )
     ).to.be.true;
   });
+  it('sets the provided weeklyInstructionalMinutes as default if it is already in the option list', () => {
+    const wrapper = shallow(
+      <UnitCalendarDialog
+        isOpen
+        handleClose={() => console.log('hello')}
+        lessons={testLessons}
+        weeklyInstructionalMinutes={45}
+      />
+    );
+    expect(wrapper.find('option').length).to.equal(5);
+    expect(
+      wrapper.containsMatchingElement(
+        <option value={45} key={`minutes-45`}>
+          45 minutes
+        </option>
+      )
+    ).to.be.true;
+    expect(
+      wrapper.containsMatchingElement(
+        <UnitCalendar lessons={testLessons} weeklyInstructionalMinutes={45} />
+      )
+    ).to.be.true;
+  });
+  it('adds the provided weeklyInstructionalMinutes to the dropdown and sets it as default', () => {
+    const wrapper = shallow(
+      <UnitCalendarDialog
+        isOpen
+        handleClose={() => console.log('hello')}
+        lessons={testLessons}
+        weeklyInstructionalMinutes={20}
+      />
+    );
+    expect(wrapper.find('option').length).to.equal(6);
+    expect(
+      wrapper.containsMatchingElement(
+        <option value={20} key={`minutes-20`}>
+          20 minutes
+        </option>
+      )
+    ).to.be.true;
+    expect(
+      wrapper.containsMatchingElement(
+        <UnitCalendar lessons={testLessons} weeklyInstructionalMinutes={20} />
+      )
+    ).to.be.true;
+  });
 });
