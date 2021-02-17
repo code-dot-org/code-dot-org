fee6968204a5a598e212cda58907bea0f479ee0b
Maureen <maureen.sturgeon@gmail.com>
2021-02-17 10:47:49 -0800

    rename migration, reset to auto-play

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_005212) do
+ActiveRecord::Schema.define(version: 2021_02_17_164121) do
 
   create_table "activities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
@@ -1494,7 +1494,7 @@ ActiveRecord::Schema.define(version: 2021_01_30_005212) do
     t.boolean "pairing_allowed", default: true, null: false
     t.boolean "sharing_disabled", default: false, null: false, comment: "Flag indicates the default sharing setting for a section and is used to determine students share setting when adding a new student to the section."
     t.boolean "hidden", default: false, null: false
-    t.boolean "autoplay_enabled", default: false, null: false
+    t.boolean "tts_autoplay_enabled", default: false, null: false
     t.index ["code"], name: "index_sections_on_code", unique: true
     t.index ["course_id"], name: "fk_rails_20b1e5de46"
     t.index ["user_id"], name: "index_sections_on_user_id"
