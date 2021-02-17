3b8e39882d58f4efe7a148db7b39afcb48caf04d
Hannah Bergam <hannah.bergam@code.org>
2021-02-17 21:46:27 +0000

    added session storage and cookies sinon tests

--- apps/test/unit/code-studio/components/header/SignInCalloutTest.js
+++ apps/test/unit/code-studio/components/header/SignInCalloutTest.js
@@ -3,6 +3,8 @@ import SignInCalloutWrapper from '@cdo/apps/code-studio/components/header/SignIn
 import {shallow, mount} from 'enzyme';
 import {expect} from '../../../../util/reconfiguredChai';
 import i18n from '@cdo/locale';
+import sinon from 'sinon';
+import cookies from 'js-cookie';
 
 const wrapper = shallow(<SignInCalloutWrapper />);
 const parentWrapper = mount(<SignInCalloutWrapper />);
@@ -26,6 +28,28 @@ describe('Check popup does not appear when flag is set', () => {
   it('does not obscure the current view', () => {
     wrapper.setState({hideCallout: true});
     expect(wrapper.html() === null).to.be.true;
+  });
+
+  it('cookies appear when clicked, and callout disappears', () => {
+    parentWrapper.setState({hideCallout: false});
+    expect(parentWrapper.html().includes('uitest-signincallout')).to.be.true;
+    parentWrapper.find('.modal-backdrop').simulate('click');
+    sinon
+      .stub(cookies, 'get')
+      .withArgs('hide_signin_callout')
+      .returns('true');
+    expect(parentWrapper.html() === null).to.be.true;
+  });
+
+  it('session storage appears when clicked, and callout disappears', () => {
+    parentWrapper.setState({hideCallout: false});
+    expect(parentWrapper.html().includes('uitest-signincallout')).to.be.true;
+    parentWrapper.find('.modal-backdrop').simulate('click');
+    sinon
+      .stub(sessionStorage, 'getItem')
+      .withArgs('hide_signin_callout')
+      .returns('true');
+    expect(parentWrapper.html() === null).to.be.true;
   });
 });
 
