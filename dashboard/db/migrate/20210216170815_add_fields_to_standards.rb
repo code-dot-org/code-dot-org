a6d2f5bf300356f2b244949a2be1c71e282b5735
David Bailey <davidsbailey@users.noreply.github.com>
2021-02-17 09:12:07 -0800

    explicitly set utf8 on new tables

--- dashboard/db/migrate/20210216170815_add_fields_to_standards.rb
+++ dashboard/db/migrate/20210216170815_add_fields_to_standards.rb
@@ -6,5 +6,10 @@ class AddFieldsToStandards < ActiveRecord::Migration[5.2]
       t.string :shortcode
       t.index [:framework_id, :shortcode]
     end
+    reversible do |dir|
+      dir.up do
+        execute "ALTER TABLE standards CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+      end
+    end
   end
 end
