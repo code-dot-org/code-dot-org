6778d5426ed54bcacd5b332ee3b970f7542f5eb6
molly-moen <molly@code.org>
2021-02-17 09:35:37 -0800

    add new files

---
+++ apps/src/sites/studio/pages/javalab/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {getStore} from '@cdo/apps/code-studio/redux';
+import javalab from '@cdo/apps/idelab/idelabRedux';
+import {registerReducers} from '@cdo/apps/redux';
+import JavalabView from '../../../../javalab/JavalabView';
+
+$(document).ready(function() {
+  registerReducers({javalab});
+  ReactDOM.render(
+    <Provider store={getStore()}>
+      <JavalabView />
+    </Provider>,
+    document.getElementById('java-lab-container')
+  );
+});
