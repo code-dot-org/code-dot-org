5fcc1a90b0d391ab53f5fa98e6d4f4eb5e402cac
Hannah Bergam <hannah.bergam@code.org>
2021-02-19 00:14:37 +0000

    updating class name of callout, adding wrapper def to each test

--- apps/test/unit/code-studio/components/header/SignInCalloutTest.js
+++ apps/test/unit/code-studio/components/header/SignInCalloutTest.js
@@ -6,26 +6,27 @@ import i18n from '@cdo/locale';
 import sinon from 'sinon';
 import cookies from 'js-cookie';
 
-const wrapper = shallow(<SignInCalloutWrapper />);
-const parentWrapper = mount(<SignInCalloutWrapper />);
-
 describe('ViewPopup', () => {
   it('displays the correct background darkness', () => {
+    const wrapper = shallow(<SignInCalloutWrapper />);
     wrapper.setState({hideCallout: false});
     expect(wrapper.html().includes('opacity:0.5')).to.be.true;
   });
 
   it('shows the correct header', () => {
+    const wrapper = shallow(<SignInCalloutWrapper />);
     expect(wrapper.html().includes(i18n.notSignedInHeader())).to.be.true;
   });
 
   it('shows the correct image', () => {
+    const wrapper = shallow(<SignInCalloutWrapper />);
     expect(wrapper.html().includes('user-not-signed-in.png')).to.be.true;
   });
 });
 
 describe('Check popup does not appear when flag is set', () => {
   it('does not obscure the current view', () => {
+    const wrapper = shallow(<SignInCalloutWrapper />);
     wrapper.setState({hideCallout: true});
     expect(wrapper.html() === null).to.be.true;
   });
@@ -48,9 +49,10 @@ describe('Check cookies and session storage appear on click', () => {
   });
 
   it('cookies appear when clicked', () => {
-    parentWrapper.setState({hideCallout: false});
+    const wrapper = mount(<SignInCalloutWrapper />);
+    wrapper.setState({hideCallout: false});
     var cookieSetStub = sinon.stub(cookies, 'set');
-    parentWrapper.find('.modal-backdrop').simulate('click');
+    wrapper.find('.login-callout').simulate('click');
     expect(cookieSetStub).to.have.been.calledWith(
       'hide_signin_callout',
       'true',
@@ -58,10 +60,11 @@ describe('Check cookies and session storage appear on click', () => {
     );
   });
 
-  it('session storage appears when clicked, and callout disappears', () => {
-    parentWrapper.setState({hideCallout: false});
+  it('session storage appears when clicked', () => {
+    const wrapper = mount(<SignInCalloutWrapper />);
+    wrapper.setState({hideCallout: false});
     var sessionSetStub = sinon.stub(sessionStorage, 'setItem');
-    parentWrapper.find('.modal-backdrop').simulate('click');
+    wrapper.find('.login-callout').simulate('click');
     expect(sessionSetStub).to.have.been.calledWith(
       'hide_signin_callout',
       'true'
@@ -69,27 +72,28 @@ describe('Check cookies and session storage appear on click', () => {
   });
 
   it('if cookie is set, callout does not appear', () => {
+    const wrapper = mount(<SignInCalloutWrapper />);
     sinon
       .stub(cookies, 'get')
       .withArgs('hide_signin_callout')
       .returns('true');
-    expect(parentWrapper.html() === null).to.be.true;
+    expect(wrapper.html() === null).to.be.true;
   });
 
   it('if session storage flag is set, callout does not appear', () => {
+    const wrapper = mount(<SignInCalloutWrapper />);
     sinon
       .stub(sessionStorage, 'getItem')
       .withArgs('hide_signin_callout')
       .returns('true');
-    expect(parentWrapper.html() === null).to.be.true;
+    expect(wrapper.html() === null).to.be.true;
   });
-});
 
-describe('The click to dismiss correctly updates the flag', () => {
   it('shows prior to click, and dismisses after', () => {
-    parentWrapper.setState({hideCallout: false});
-    expect(parentWrapper.html().includes('uitest-signincallout')).to.be.true;
-    parentWrapper.find('.modal-backdrop').simulate('click');
-    expect(parentWrapper.html() === null).to.be.true;
+    const wrapper = mount(<SignInCalloutWrapper />);
+    wrapper.setState({hideCallout: false});
+    expect(wrapper.html().includes('uitest-signincallout')).to.be.true;
+    wrapper.find('.login-callout').simulate('click');
+    expect(wrapper.html() === null).to.be.true;
   });
 });
