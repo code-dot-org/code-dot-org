3c5c27c2648ff1eb72c6ff531c0f50e1bc0e1172
Ben Brooks <ben@code.org>
2021-02-19 16:48:12 -0800

    Rename section ID column to make clear for analytics use

--- dashboard/db/migrate/20210217182336_add_section_id_to_teacher_feedback.rb
+++ dashboard/db/migrate/20210217182336_add_section_id_to_teacher_feedback.rb
@@ -1,5 +1,5 @@
 class AddSectionIdToTeacherFeedback < ActiveRecord::Migration[5.2]
   def change
-    add_column :teacher_feedbacks, :section_id, :integer
+    add_column :teacher_feedbacks, :analytics_section_id, :integer
   end
 end
