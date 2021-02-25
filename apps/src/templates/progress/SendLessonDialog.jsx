ec6db988812d95870baf0da8d3899403d0037497
Hannah Bergam <hannah.bergam@code.org>
2021-02-25 18:07:13 +0000

    Empty commit to get build to pass

--- apps/src/templates/progress/SendLessonDialog.jsx
+++ apps/src/templates/progress/SendLessonDialog.jsx
@@ -70,7 +70,7 @@ class SendLessonDialog extends Component {
   onCopyLink() {
     copyToClipboard(this.props.lessonUrl);
 
-    // show "Link copied!" for 4 seconds
+    // show message "Link copied!" for 4 seconds
     this.setState({showLinkCopied: true});
     setTimeout(() => {
       this.setState({showLinkCopied: false});
