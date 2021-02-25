9237b557086d11b9e326dbded159286fa6272187
Bethany Connor <bethany@code.org>
2021-02-25 14:14:36 -0800

    Fix bad merge

--- apps/src/templates/instructions/TopInstructions.jsx
+++ apps/src/templates/instructions/TopInstructions.jsx
@@ -339,7 +339,7 @@ class TopInstructions extends Component {
    * via a call to handleHeightResize from HeightResizer.
    */
   getItemTop = () => {
-    return this.refs.topInstructions.getBoundingClientRect().top;
+    return this.topInstructions.getBoundingClientRect().top;
   };
 
   /**
@@ -381,16 +381,16 @@ class TopInstructions extends Component {
     let element;
     switch (this.state.tabSelected) {
       case TabType.RESOURCES:
-        element = this.refs.helpTab;
+        element = this.helpTab;
         break;
       case TabType.INSTRUCTIONS:
-        element = this.refs.instructions;
+        element = this.instructions;
         break;
       case TabType.COMMENTS:
-        element = this.refs.commentTab;
+        element = this.commentTab;
         break;
       case TabType.TEACHER_ONLY:
-        element = this.refs.teacherOnlyTab;
+        element = this.teacherOnlyTab;
         break;
     }
     const maxNeededHeight =
@@ -605,7 +605,11 @@ class TopInstructions extends Component {
       this.props.hasContainedLevels && $('#containedLevelAnswer0').length > 0;
 
     return (
-      <div style={mainStyle} className="editor-column" ref="topInstructions">
+      <div
+        style={mainStyle}
+        className="editor-column"
+        ref={ref => (this.topInstructions = ref)}
+      >
         <PaneHeader
           hasFocus={false}
           teacherOnly={teacherOnly}
@@ -708,11 +712,21 @@ class TopInstructions extends Component {
             ]}
             id="scroll-container"
           >
-            <div ref="instructions">
+            <div
+              ref={ref => {
+                if (ref) {
+                  this.instructions = ref;
+                }
+              }}
+            >
               {this.props.hasContainedLevels && (
                 <div>
                   <ContainedLevel
-                    ref="instructions"
+                    ref={ref => {
+                      if (ref) {
+                        this.instructions = ref;
+                      }
+                    }}
                     hidden={this.state.tabSelected !== TabType.INSTRUCTIONS}
                   />
                 </div>
@@ -721,7 +735,11 @@ class TopInstructions extends Component {
                 isCSF &&
                 this.state.tabSelected === TabType.INSTRUCTIONS && (
                   <InstructionsCSF
-                    ref="instructions"
+                    ref={ref => {
+                      if (ref) {
+                        this.instructions = ref;
+                      }
+                    }}
                     handleClickCollapser={this.handleClickCollapser}
                     adjustMaxNeededHeight={this.adjustMaxNeededHeight}
                     isEmbedView={this.props.isEmbedView}
@@ -735,7 +753,11 @@ class TopInstructions extends Component {
                 this.state.tabSelected === TabType.INSTRUCTIONS && (
                   <div>
                     <Instructions
-                      ref="instructions"
+                      ref={ref => {
+                        if (ref) {
+                          this.instructions = ref;
+                        }
+                      }}
                       longInstructions={this.props.longInstructions}
                       onResize={this.adjustMaxNeededHeight}
                       inTopPane
@@ -747,7 +769,7 @@ class TopInstructions extends Component {
             </div>
             {this.state.tabSelected === TabType.RESOURCES && (
               <HelpTabContents
-                ref="helpTab"
+                ref={ref => (this.helpTab = ref)}
                 videoData={videoData}
                 mapReference={this.props.mapReference}
                 referenceLinks={this.props.referenceLinks}
@@ -763,7 +785,7 @@ class TopInstructions extends Component {
                   !this.state.teacherViewingStudentWork
                 }
                 rubric={this.state.rubric}
-                ref="commentTab"
+                ref={ref => (this.commentTab = ref)}
                 latestFeedback={this.state.feedbacks}
                 token={this.state.token}
               />
