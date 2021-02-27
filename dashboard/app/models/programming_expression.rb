673bf4dee1b0198cc154acc02113ba30c14635f4
Dani <dani@code.org>
2021-02-27 09:08:03 -0500

    Make it so we can seed environments and some expressions

--- dashboard/app/models/programming_expression.rb
+++ dashboard/app/models/programming_expression.rb
@@ -17,4 +17,32 @@
 class ProgrammingExpression < ApplicationRecord
   belongs_to :programming_environment
   has_and_belongs_to_many :lessons, join_table: :lessons_programming_expressions
+
+  def self.properties_from_file(path, content)
+    expression_config = JSON.parse(content)
+
+    environment_name = File.basename(File.dirname(path)) == 'GamelabJr' ? 'spritelab' : ''
+    programming_environment = ProgrammingEnvironment.find_by(name: environment_name)
+
+    {
+      name: expression_config['config']['func'] || expression_config['config']['name'],
+      programming_environment_id: programming_environment.id,
+      category: expression_config['category']
+    }
+  end
+
+  def self.seed_all(blob="config/blocks/GamelabJr/*.json")
+    removed_records = all.pluck(:name)
+    Dir.glob(Rails.root.join(blob)).each do |path|
+      removed_records -= [ProgrammingExpression.seed_record(path)]
+    end
+    where(name: removed_records).destroy_all
+  end
+
+  def self.seed_record(file_path)
+    properties = properties_from_file(file_path, File.read(file_path))
+    record = ProgrammingExpression.find_or_initialize_by(name: properties[:name], programming_environment_id: properties[:programming_environment_id])
+    record.update! properties
+    record.name
+  end
 end
