8aac56e435545dfef9484d7b610ca0f36b2e1c1c
Ben Brooks <ben@code.org>
2021-02-26 17:03:31 -0800

    Clean up foorm buttons component

--- apps/src/code-studio/pd/foorm/editor/components/FoormLoadButtons.jsx
+++ apps/src/code-studio/pd/foorm/editor/components/FoormLoadButtons.jsx
@@ -7,151 +7,81 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {Button, DropdownButton, MenuItem} from 'react-bootstrap';
 import {
-  resetAvailableOptions,
   setLastSaved,
   setSaveError,
-  setFormData,
   setHasJSONError,
   setLastSavedQuestions
 } from '../foormEditorRedux';
-
-const styles = {
-  loadError: {
-    fontWeight: 'bold',
-    padding: '1em'
-  }
-};
 
 class FoormLoadButtons extends React.Component {
   static propTypes = {
     resetCodeMirror: PropTypes.func,
     namesAndVersions: PropTypes.array,
-    setShowCodeMirror: PropTypes.func,
+    resetSelectedData: PropTypes.func,
+    onSelect: PropTypes.func,
+    dropdownOptions: PropTypes.array,
+    showCodeMirror: PropTypes.func,
 
     // populated by redux
-    hasJSONError: PropTypes.bool,
-    availableForms: PropTypes.array,
-    questions: PropTypes.object,
-    resetAvailableForms: PropTypes.func,
     setLastSaved: PropTypes.func,
     setSaveError: PropTypes.func,
-    setFormData: PropTypes.func,
     setHasJSONError: PropTypes.func,
-    setLastSavedFormQuestions: PropTypes.func
+    setLastSavedQuestions: PropTypes.func
   };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hasLoadError: false
-    };
-
-    this.props.resetAvailableForms(this.props.namesAndVersions);
-  }
-
   getFormattedConfigurationDropdownOptions() {
-    return this.props.availableForms.map((formNameAndVersion, i) => {
-      const formName = formNameAndVersion['name'];
-      const formVersion = formNameAndVersion['version'];
-      const formId = formNameAndVersion['id'];
-      return (
-        <MenuItem
-          key={i}
-          eventKey={i}
-          onClick={() => this.loadConfiguration(formId)}
-        >
-          {`${formName}, version ${formVersion}`}
-        </MenuItem>
+    return this.props.dropdownOptions.map((dropdownOption, i) => {
+      return this.renderMenuItem(
+        () => this.props.onSelect(dropdownOption['id']),
+        dropdownOption['text'],
+        i
       );
     });
   }
 
-  loadConfiguration(formId) {
-    this.props.setLastSaved(null);
-    this.props.setSaveError(null);
-    $.ajax({
-      url: `/api/v1/pd/foorm/forms/${formId}`,
-      type: 'get'
-    })
-      .done(result => {
-        this.updateFormData(result);
-        this.props.setShowCodeMirror(true);
-        this.setState({
-          hasLoadError: false
-        });
-      })
-      .fail(() => {
-        this.updateFormData({
-          questions: {},
-          published: null,
-          formName: null,
-          formVersion: null,
-          formId: null
-        });
-        this.setState({
-          showCodeMirror: true,
-          hasLoadError: true
-        });
-      });
+  renderMenuItem(clickHandler, textToDisplay, key) {
+    return (
+      <MenuItem key={key} eventKey={key} onClick={clickHandler}>
+        {textToDisplay}
+      </MenuItem>
+    );
   }
 
+  // There's gotta be a cleaner way than what I'm doing here
+  // Maybe a general "reset" redux action?
   initializeEmptyCodeMirror = () => {
+    this.props.resetSelectedData();
+    this.props.showCodeMirror();
+    this.props.resetCodeMirror({});
+
+    // From redux store
     this.props.setLastSaved(null);
     this.props.setSaveError(null);
-    this.updateFormData({
-      questions: {},
-      published: null,
-      formName: null,
-      formVersion: null,
-      formId: null
-    });
-    this.setState({
-      showCodeMirror: true,
-      hasLoadError: false
-    });
-  };
-
-  updateFormData = formData => {
-    this.props.setFormData(formData);
     this.props.setHasJSONError(false);
-    this.props.setLastSavedFormQuestions(formData['questions']);
-    this.props.resetCodeMirror(formData['questions']);
+    this.props.setLastSavedQuestions({});
   };
 
   render() {
     return (
       <div>
-        <div>
-          <DropdownButton id="load_config" title="Load Form..." className="btn">
-            {this.getFormattedConfigurationDropdownOptions()}
-          </DropdownButton>
-          <Button onClick={this.initializeEmptyCodeMirror} className="btn">
-            New Form
-          </Button>
-        </div>
-        {this.state.hasLoadError && (
-          <div style={styles.loadError}>Could not load the selected form.</div>
-        )}
+        <DropdownButton id="load_config" title="Load Form..." className="btn">
+          {this.getFormattedConfigurationDropdownOptions()}
+        </DropdownButton>
+        <Button onClick={this.initializeEmptyCodeMirror} className="btn">
+          New Form
+        </Button>
       </div>
     );
   }
 }
 
 export default connect(
-  state => ({
-    questions: state.foorm.questions || {},
-    availableForms: state.foorm.availableOptions || [],
-    hasJSONError: state.foorm.hasJSONError
-  }),
+  null,
   dispatch => ({
-    resetAvailableForms: formsMetadata =>
-      dispatch(resetAvailableOptions(formsMetadata)),
     setLastSaved: lastSaved => dispatch(setLastSaved(lastSaved)),
     setSaveError: saveError => dispatch(setSaveError(saveError)),
-    setFormData: formData => dispatch(setFormData(formData)),
     setHasJSONError: hasJSONError => dispatch(setHasJSONError(hasJSONError)),
-    setLastSavedFormQuestions: formQuestions =>
-      dispatch(setLastSavedQuestions(formQuestions))
+    setLastSavedQuestions: questions =>
+      dispatch(setLastSavedQuestions(questions))
   })
 )(FoormLoadButtons);
