fb41a605c685501b789745d2049de66fdf3a58f8
James Chen <james@code.org>
2021-03-02 20:20:04 -0800

    Try reverting expected query counts in HomeControllerTest

--- dashboard/test/controllers/home_controller_test.rb
+++ dashboard/test/controllers/home_controller_test.rb
@@ -362,7 +362,7 @@ class HomeControllerTest < ActionController::TestCase
   # TODO: remove this test when workshop_organizer is deprecated
   test 'workshop organizers see dashboard links' do
     sign_in create(:workshop_organizer, :with_terms_of_service)
-    query_count = 15
+    query_count = 14
     assert_queries query_count do
       get :home
     end
@@ -371,7 +371,7 @@ class HomeControllerTest < ActionController::TestCase
 
   test 'program managers see dashboard links' do
     sign_in create(:program_manager, :with_terms_of_service)
-    query_count = 17
+    query_count = 16
     assert_queries query_count do
       get :home
     end
@@ -380,7 +380,7 @@ class HomeControllerTest < ActionController::TestCase
 
   test 'workshop admins see dashboard links' do
     sign_in create(:workshop_admin, :with_terms_of_service)
-    query_count = 14
+    query_count = 13
     assert_queries query_count do
       get :home
     end
@@ -391,8 +391,6 @@ class HomeControllerTest < ActionController::TestCase
   # it will fail if you try to run it independently
   # it is also flakey on drone, passing with different query counts (14 or 15)
   test 'facilitators see dashboard links' do
-    skip 'TODO: look into why this test is passing with different query counts on drone'
-
     facilitator = create(:facilitator, :with_terms_of_service)
     sign_in facilitator
     query_count = 14
@@ -404,7 +402,7 @@ class HomeControllerTest < ActionController::TestCase
 
   test 'teachers cannot see dashboard links' do
     sign_in create(:terms_of_service_teacher)
-    query_count = 13
+    query_count = 12
     assert_queries query_count do
       get :home
     end
@@ -413,7 +411,7 @@ class HomeControllerTest < ActionController::TestCase
 
   test 'workshop admins see application dashboard links' do
     sign_in create(:workshop_admin, :with_terms_of_service)
-    query_count = 14
+    query_count = 13
     assert_queries query_count do
       get :home
     end
@@ -424,7 +422,7 @@ class HomeControllerTest < ActionController::TestCase
   # TODO: remove this test when workshop_organizer is deprecated
   test 'workshop organizers who are regional partner program managers see application dashboard links' do
     sign_in create(:workshop_organizer, :as_regional_partner_program_manager, :with_terms_of_service)
-    query_count = 17
+    query_count = 16
     assert_queries query_count do
       get :home
     end
@@ -433,10 +431,8 @@ class HomeControllerTest < ActionController::TestCase
   end
 
   test 'program managers see application dashboard links' do
-    skip 'TODO: look into why this test is passing with different query counts on drone'
-
     sign_in create(:program_manager, :with_terms_of_service)
-    query_count = 17
+    query_count = 16
     assert_queries query_count do
       get :home
     end
@@ -447,7 +443,7 @@ class HomeControllerTest < ActionController::TestCase
   # TODO: remove this test when workshop_organizer is deprecated
   test 'workshop organizers who are not regional partner program managers do not see application dashboard links' do
     sign_in create(:workshop_organizer, :with_terms_of_service)
-    query_count = 15
+    query_count = 14
     assert_queries query_count do
       get :home
     end
