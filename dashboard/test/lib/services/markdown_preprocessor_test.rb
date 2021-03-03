5bf7d6eecb0df41839b331cc78bb8e9dc388c2cd
Elijah Hamovitz <elijahhamovitz@gmail.com>
2021-03-03 11:21:54 -0800

    add caching

--- dashboard/test/lib/services/markdown_preprocessor_test.rb
+++ dashboard/test/lib/services/markdown_preprocessor_test.rb
@@ -4,6 +4,58 @@ class Services::MarkdownPreprocessorTest < ActiveSupport::TestCase
   setup do
     create :resource, key: 'first-resource', name: "First Resource", url: "example.com/first"
     create :resource, key: 'second-resource', name: "Second Resource", url: "example.com/second"
+    create :vocabulary, key: 'first-vocab', word: "First Vocabulary", definition: "The first of the vocabulary entries."
+    create :vocabulary, key: 'second-vocab', word: "Second Vocabulary", definition: "The second of the vocabulary entries."
+  end
+
+  test 'process method invokes both resource and vocab substitutions' do
+    input = "A string containing both a Resource link [r first-resource] and a Vocab link [v first-vocab]"
+    result = Services::MarkdownPreprocessor.process(input)
+    expected = "A string containing both a Resource link [First Resource](example.com/first) and a Vocab link [v first-vocab]"
+    assert_equal expected, result
+  end
+
+  test 'process is cached' do
+    Rails.cache.clear
+    input = "[r first-resource]"
+
+    # First invocation queries the database
+    assert_queries 1 do
+      Services::MarkdownPreprocessor.process(input)
+    end
+
+    # Future invocations do not
+    assert_queries 0 do
+      Services::MarkdownPreprocessor.process(input)
+    end
+
+    # Each content string is cached individually
+    assert_queries 1 do
+      Services::MarkdownPreprocessor.process("[r second-resource]")
+    end
+
+    # Clearing the cache causes us to start querying again
+    Rails.cache.clear
+    assert_queries 1 do
+      Services::MarkdownPreprocessor.process(input)
+    end
+  end
+
+  test 'process caching can be modified with options' do
+    input = "[r first-resource][v first-vocab]"
+
+    # populate the cache
+    Services::MarkdownPreprocessor.process(input)
+
+    # verify that we use the cache by default
+    assert_queries 0 do
+      Services::MarkdownPreprocessor.process(input)
+    end
+
+    # verify that the cache can be skipped
+    assert_queries 1 do
+      Services::MarkdownPreprocessor.process(input, cache_options: {force: true})
+    end
   end
 
   test 'regular method returns and does not modifiy' do
