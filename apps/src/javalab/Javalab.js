c4f0f7f77ff86e53f3097c6dc3c9bcc9c2213d9d
molly-moen <molly@code.org>
2021-03-03 16:12:22 -0800

    saving work in progress

--- apps/src/javalab/Javalab.js
+++ apps/src/javalab/Javalab.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {getStore, registerReducers} from '../redux';
 import JavalabView from './JavalabView';
-import javalab from './javalabRedux';
+import javalab, {setFileEntries} from './javalabRedux';
 import {TestResults} from '@cdo/apps/constants';
+import project from '@cdo/apps/code-studio/initApp/project';
+import {queryParams} from '@cdo/apps/code-studio/utils';
+var filesApi = require('@cdo/apps/clientApi').files;
+var assetListStore = require('../code-studio/assets/assetListStore');
 
 /**
  * On small mobile devices, when in portrait orientation, we show an overlay
@@ -41,6 +45,8 @@ Javalab.prototype.init = function(config) {
 
   this.skin = config.skin;
   this.level = config.level;
+  this.suppliedFilesVersionId = queryParams('version');
+  this.initialFilesVersionId = this.suppliedFilesVersionId;
 
   config.makeYourOwn = false;
   config.wireframeShare = true;
@@ -58,7 +64,11 @@ Javalab.prototype.init = function(config) {
 
   config.pinWorkspaceToBottom = true;
 
+  config.useFilesApi = true;
+
   config.getCode = this.getCode.bind(this);
+
+  this.loadFileEntries();
 
   const onMount = () => {
     // NOTE: Most other apps call studioApp.init().  Like WebLab, Ailab, and Fish, we don't.
@@ -119,7 +129,96 @@ Javalab.prototype.onContinue = function() {
 };
 
 Javalab.prototype.getCode = function() {
-  return '';
+  return getStore().getState().javalab.editorText;
+};
+
+Javalab.prototype.getCurrentFilesVersionId = function() {
+  return project.filesVersionId || this.initialFilesVersionId;
+};
+
+// Called by Javalab when a file has been renamed
+Javalab.prototype.renameProjectFile = function(
+  filename,
+  newFilename,
+  callback
+) {
+  filesApi.renameFile(
+    filename,
+    newFilename,
+    xhr => {
+      callback(null, project.filesVersionId);
+    },
+    xhr => {
+      console.warn(`Javalab: error file ${filename} not renamed`);
+      callback(new Error(xhr.status));
+    }
+  );
+};
+
+// Called by Javalab when a file has been changed or created
+Javalab.prototype.changeProjectFile = function(
+  filename,
+  fileData
+  //callback,
+  // skipPreWriteHook
+) {
+  filesApi.putFile(
+    filename,
+    fileData
+    // xhr => {
+    //   callback(null, project.filesVersionId);
+    // },
+    // xhr => {
+    //   console.warn(`Javalab: error file ${filename} not changed`);
+    //   callback(new Error(xhr.status));
+    // },
+    // skipPreWriteHook
+  );
+};
+
+Javalab.prototype.saveFiles = function() {
+  const files = getStore().getState().javalab.filesData;
+  for (const filename in files) {
+    this.changeProjectFile(filename, files[filename]);
+  }
+};
+
+/**
+ * Load the file entry list and store it as this.fileEntries
+ */
+Javalab.prototype.loadFileEntries = function() {
+  const onFilesReady = (files, filesVersionId) => {
+    assetListStore.reset(files);
+    const fileEntries = assetListStore.list().map(fileEntry => ({
+      name: fileEntry.filename,
+      url: filesApi.basePath(fileEntry.filename),
+      versionId: fileEntry.versionId
+    }));
+    getStore().dispatch(setFileEntries(fileEntries));
+    this.initialFilesVersionId = this.initialFilesVersionId || filesVersionId;
+
+    if (filesVersionId !== this.initialFilesVersionId) {
+      // After we've detected the first change to the version, we store this
+      // version id so that subsequent writes will continue to replace the
+      // current version (until the browser page reloads)
+      project.filesVersionId = filesVersionId;
+    }
+  };
+
+  filesApi.getFiles(
+    result => onFilesReady(result.files, result.filesVersionId),
+    xhr => {
+      if (xhr.status === 404) {
+        // No files in this project yet, proceed with an empty file
+        // list and no start version id
+        onFilesReady([], null);
+      } else {
+        console.error('files API failed, status: ' + xhr.status);
+        getStore().dispatch(setFileEntries(null));
+      }
+    },
+    this.getCurrentFilesVersionId()
+  );
 };
 
 export default Javalab;
