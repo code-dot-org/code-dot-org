a608c416275ee31b72d4a7ba7be08670050301b3
Jillian Khoo <jillian9012@gmail.com>
2021-03-04 22:45:43 +0000

    fix ui test, resolve pr comment [test all browsers]

--- dashboard/app/models/lesson.rb
+++ dashboard/app/models/lesson.rb
@@ -280,7 +280,7 @@ class Lesson < ApplicationRecord
         lesson_data[:lesson_plan_html_url] = lesson_plan_html_url
         lesson_data[:lesson_plan_pdf_url] = lesson_plan_pdf_url
         if script.include_student_lesson_plans && script.is_migrated
-          lesson_data[:student_lesson_plan_html_url] = script_lesson_path(script, self) + '/student'
+          lesson_data[:student_lesson_plan_html_url] = script_lesson_student_path(script, self)
         end
       end
 
