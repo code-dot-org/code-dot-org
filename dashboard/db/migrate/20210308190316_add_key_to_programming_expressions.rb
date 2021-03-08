9614f856cd52a4490f526c100c2653c5867242a6
Dani <dani@code.org>
2021-03-08 14:14:36 -0500

    Make sure key is not null

--- dashboard/db/migrate/20210308190316_add_key_to_programming_expressions.rb
+++ dashboard/db/migrate/20210308190316_add_key_to_programming_expressions.rb
@@ -1,5 +1,5 @@
 class AddKeyToProgrammingExpressions < ActiveRecord::Migration[5.2]
   def change
-    add_column :programming_expressions, :key, :string
+    add_column :programming_expressions, :key, :string, null: false
   end
 end
