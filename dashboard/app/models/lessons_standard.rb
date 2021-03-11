9ef807c7ea8e98460c868718ae7d31f33a57e0ce
David Bailey <davidsbailey@users.noreply.github.com>
2021-03-11 15:00:45 -0800

    serialize LessonsStandard

--- dashboard/app/models/lessons_standard.rb
+++ dashboard/app/models/lessons_standard.rb
@@ -17,4 +17,22 @@ class LessonsStandard < ApplicationRecord
 
   belongs_to :lesson, foreign_key: 'stage_id'
   belongs_to :standard
+
+  # Used for seeding from JSON. Returns the full set of information needed to
+  # uniquely identify this object as well as any other objects it belongs to.
+  # If the attributes of this object alone aren't sufficient, and associated
+  # objects are needed, then data from the seeding_keys of those objects should
+  # be included as well. Ideally should correspond to a unique index for this
+  # model's table. See comments on ScriptSeed.seed_from_hash for more context.
+  #
+  # @param [ScriptSeed::SeedContext] seed_context - contains preloaded data to use when looking up associated objects
+  # @return [Hash<String, String>] all information needed to uniquely identify this object across environments.
+  def seeding_key(seed_context)
+    my_lesson = seed_context.lessons.select {|l| l.id == stage_id}.first
+    my_standard = seed_context.standards.select {|s| s.id == standard_id}.first
+    {
+      'lesson.key' => my_lesson.key,
+      'standard.shortcode' => my_standard.shortcode
+    }.stringify_keys
+  end
 end
