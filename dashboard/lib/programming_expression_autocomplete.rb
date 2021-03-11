7cc45485d1bceed774f2afe3205bbc85fc46e112
Dani <dani@code.org>
2021-03-10 20:15:48 -0500

    Update the way we are searching to leverage programming environment

--- dashboard/lib/programming_expression_autocomplete.rb
+++ dashboard/lib/programming_expression_autocomplete.rb
@@ -1,12 +1,13 @@
 class ProgrammingExpressionAutocomplete < AutocompleteHelper
-  def self.get_search_matches(query, limit)
+  def self.get_search_matches(query, limit, programming_environment_id)
     limit = format_limit(limit)
 
     rows = ProgrammingExpression.limit(limit)
+    rows = rows.where(programming_environment_id: programming_environment_id) if programming_environment_id
     return [] if query.length < MIN_WORD_LENGTH
     query = format_query(query)
     rows = rows.
-        where("MATCH(name,programming_environment_id) AGAINST(? in BOOLEAN MODE)", query)
+        where("MATCH(name) AGAINST(? in BOOLEAN MODE)", query)
     return rows.map(&:summarize_for_lesson_edit)
   end
 end
--- dashboard/test/lib/programming_expression_autocomplete_test.rb
+++ dashboard/test/lib/programming_expression_autocomplete_test.rb
@@ -2,24 +2,32 @@ require 'test_helper'
 
 class ProgrammingExpressionAutocompleteTest < ActiveSupport::TestCase
   # We rely on fulltext indices to be used in this test.
-  # In order to get this test to work, we need to have the programming expressions created
-  # as fixtures. These are defined in test/fixtures/programming_expression.yml
+  # In order to get this test to work, we need to have the programming expressions and programming environments created
+  # as fixtures. These are defined in test/fixtures/programming_expression.yml and test/fixtures/programming_environment.yml
 
   test "finds programming_expression with matching name" do
-    matches = ProgrammingExpressionAutocomplete.get_search_matches('stopSound', 5)
+    matches = ProgrammingExpressionAutocomplete.get_search_matches('stopSound', 5, nil)
     assert_equal 1, matches.length
     assert_equal 'stopSound', matches[0][:name]
-    assert_equal 'applab', matches[0][:programming_environment].name
+    assert_equal 'applab', matches[0][:programmingEnvironmentName]
   end
 
   test "finds multiple matches" do
-    matches = ProgrammingExpressionAutocomplete.get_search_matches('Sound', 5)
-    assert_equal 2, matches.length
-    assert_equal ['stopSound', 'playSound'], matches.map {|m| m[:name]}
+    matches = ProgrammingExpressionAutocomplete.get_search_matches('Sound', 5, nil)
+    assert_equal 3, matches.length
+    assert_equal ['stopSound', 'playSound', 'playSound'], matches.map {|m| m[:name]}
   end
 
   test "restricts matches by limit" do
-    matches = ProgrammingExpressionAutocomplete.get_search_matches('Sound', 1)
+    matches = ProgrammingExpressionAutocomplete.get_search_matches('Sound', 1, nil)
     assert_equal 1, matches.length
+  end
+
+  test "restricts matches by programming environment id" do
+    # There's a blocks with Sound in them in two programming environments
+    # We should only get the one we requested
+    matches = ProgrammingExpressionAutocomplete.get_search_matches('Sound', 3, ProgrammingEnvironment.find_by(name: 'gamelab').id)
+    assert_equal 1, matches.length
+    assert_equal ProgrammingEnvironment.find_by(name: 'gamelab').id, ProgrammingExpression.find_by(key: matches[0][:key]).programming_environment_id
   end
 end
