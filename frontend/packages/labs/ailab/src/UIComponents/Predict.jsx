74829e8dd347b9c14eda72db46b264b226ebcc5d
Madeline <madelign@gmail.com>
2021-03-12 11:55:34 -0800

    Remove the result predicted when a user selects different data in test tool

--- frontend/packages/labs/ailab/src/UIComponents/Predict.jsx
+++ frontend/packages/labs/ailab/src/UIComponents/Predict.jsx
@@ -9,7 +9,8 @@ import {
   getSelectedCategoricalFeatures,
   getUniqueOptionsByColumn,
   getConvertedPredictedLabel,
-  getPredictAvailable
+  getPredictAvailable,
+  setPrediction
 } from "../redux";
 import { styles } from "../constants";
 
@@ -23,13 +24,15 @@ class Predict extends Component {
     setTestData: PropTypes.func.isRequired,
     predictedLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     confidence: PropTypes.number,
-    getPredictAvailable: PropTypes.bool
+    getPredictAvailable: PropTypes.bool,
+    setPrediction: PropTypes.func
   };
 
   handleChange = (event, feature) => {
     const testData = this.props.testData;
     testData[feature] = event.target.value;
     this.props.setTestData(testData);
+    this.props.setPrediction({});
   };
 
   onClickPredict = () => {
@@ -127,6 +130,9 @@ export default connect(
   dispatch => ({
     setTestData(testData) {
       dispatch(setTestData(testData));
+    },
+    setPrediction(prediction) {
+      dispatch(setPrediction(prediction));
     }
   })
 )(Predict);
