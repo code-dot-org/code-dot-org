70d9d65be5db126f7d8b206881c8c60e41bea410
David Bailey <davidsbailey@users.noreply.github.com>
2021-03-12 22:31:20 -0800

    make deprecated standards columns nullable

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_012741) do
+ActiveRecord::Schema.define(version: 2021_03_13_061652) do
 
   create_table "activities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
@@ -1601,8 +1601,8 @@ ActiveRecord::Schema.define(version: 2021_03_11_012741) do
   end
 
   create_table "standards", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
-    t.string "organization", null: false
-    t.string "organization_id", null: false
+    t.string "organization"
+    t.string "organization_id"
     t.text "description"
     t.text "concept"
     t.bigint "category_id"
