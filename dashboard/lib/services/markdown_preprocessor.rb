218a7c5ec41c235a5cd9835563d0d0f59dfbd762
Elijah Hamovitz <elijahhamovitz@gmail.com>
2021-03-15 12:11:11 -0700

    Revert "Add Course Offering and Version keys to the Markdown Preprocessor syntaxes"

--- dashboard/lib/services/markdown_preprocessor.rb
+++ dashboard/lib/services/markdown_preprocessor.rb
@@ -37,18 +37,8 @@ module Services
       end
     end
 
-    # Returns the key which can be used to create the reference syntax for the
-    # given Vocabulary object.
-    def self.build_vocab_key(vocab)
-      [vocab.key, vocab.course_version.course_offering.key, vocab.course_version.key].join('/')
-    end
-
     # Returns a copy of `content` with all occurrences of Vocabulary references
-    # substituted with the equivalent HTML span.
-    #
-    # Vocabulary references take the form of:
-    # `[v vocab_key/course_offering_key/course_version_key]`
-    # For example: `[v loops/coursea/2020]`
+    # substituted with the equivalent HTML span
     def self.sub_vocab_definitions(content)
       sub_vocab_definitions!(content.dup)
     end
@@ -56,11 +46,8 @@ module Services
     # Performs the substitutions of MarkdownPreprocessor#sub_vocab_definitions
     # in place
     def self.sub_vocab_definitions!(content)
-      vocab_def_re = build_key_re('v', [Vocabulary, CourseOffering, CourseVersion])
-      content.gsub!(vocab_def_re) do |match|
-        course_version = CourseVersion.joins(:course_offering).
-          find_by(key: $3, "course_offerings.key": $2)
-        vocab = Vocabulary.find_by(key: $1, course_version: course_version)
+      content.gsub!(/\[v (#{Vocabulary::KEY_CHAR_RE}+)\]/) do |match|
+        vocab = Vocabulary.find_by(key: $1)
         if vocab.present?
           "<span class=\"vocab\" title=#{vocab.definition.inspect}>#{vocab.word}</span>"
         else
@@ -69,18 +56,8 @@ module Services
       end
     end
 
-    # Returns the key which can be used to create the reference syntax for the
-    # given Resource object.
-    def self.build_resource_key(resource)
-      [resource.key, resource.course_version.course_offering.key, resource.course_version.key].join('/')
-    end
-
     # Returns a copy of `content` with all occurrences of Resource links
     # substituted with the equivalent Markdown links
-    #
-    # Resource links take the form of:
-    # `[r resource_key/course_offering_key/course_version_key]`
-    # For example: `[r example-video/csd/2021]`
     def self.sub_resource_links(content)
       sub_resource_links!(content.dup)
     end
@@ -88,30 +65,15 @@ module Services
     # Performs the substitutions of MarkdownPreprocessor#sub_resource_links in
     # place
     def self.sub_resource_links!(content)
-      resource_link_re = build_key_re('r', [Resource, CourseOffering, CourseVersion])
-      content.gsub!(resource_link_re) do |match|
-        course_version = CourseVersion.joins(:course_offering).
-          find_by(key: $3, "course_offerings.key": $2)
-        resource = Resource.find_by(key: $1, course_version: course_version)
+      content.gsub!(/\[r (#{Resource::KEY_CHAR_RE}+)\]/) do |match|
+        # apparently "$1" is how ruby expects us to get match data in a block
+        resource = Resource.find_by(key: $1)
         if resource.present?
           "[#{resource.name}](#{resource.url})"
         else
           match
         end
       end
-    end
-
-    # Simple helper which builds out a regex from the given identifier and
-    # models. Requires that the models define a KEY_CHAR_RE which can be used
-    # to identify valid `key` values for the model.
-    #
-    # Specifically, given an identifier "foo" and models Bar and Baz, we will
-    # get a regex which matches strings of the form `[foo bar_key/baz_key]`
-    def self.build_key_re(identifier, models)
-      keys = models.map do |model|
-        "(#{model::KEY_CHAR_RE}+)"
-      end
-      return /\[#{identifier} #{keys.join('/')}\]/
     end
   end
 end
