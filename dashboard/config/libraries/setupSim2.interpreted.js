2933a35a1424813c139703f6eda15f185a3fe7d3
Continuous Integration <dev@code.org>
2021-03-18 22:18:06 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/libraries/setupSim2.interpreted.js
+++ dashboard/config/libraries/setupSim2.interpreted.js
@@ -15,8 +15,83 @@ function setupSim(
       World.s3ToDelete = [];
       World.sprite1score = 0;
       World.sprite2score = 0;
+      World.s1costume=s1costume;
+      World.s2costume=s2costume;
+      World.s3costume=s3costume;
+      World.endTime=0;
     }
 
+    //Draw the scoreboard
+    push();
+    var box1x=0;
+    var box1y=350;
+    var box2x=200;
+    var box2y=350;
+    var boxWidth=200;
+    var boxHeight=50;
+    var scaler;
+    fill("rgba(0,0,0,0.5)");
+    rect(box1x,box1y,boxWidth,boxHeight);
+    rect(box2x,box2y,boxWidth,boxHeight);
+    var s1sprite = createSprite(box1x+(boxHeight/2), box1y+(boxHeight/2));
+    s1sprite.setAnimation(World.s1costume);
+    if(s1sprite.height>s1sprite.width){
+      scaler=s1sprite.width/s1sprite.height;
+      s1sprite.height=boxHeight*0.75;
+      s1sprite.width=s1sprite.height*scaler;
+    } else {
+      scaler=s1sprite.height/s1sprite.width;
+      s1sprite.width=boxHeight*0.75;
+      s1sprite.height=s1sprite.width*scaler;
+    }
+    drawSprite(s1sprite);
+    s1sprite.destroy();
+    var s2sprite = createSprite(box2x+(boxHeight/2), box2y+(boxHeight/2));
+    s2sprite.setAnimation(World.s2costume);
+    if(s2sprite.height>s2sprite.width){
+      scaler=s2sprite.width/s2sprite.height;
+      s2sprite.height=boxHeight*0.75;
+      s2sprite.width=s2sprite.height*scaler;
+    } else {
+      scaler=s2sprite.height/s2sprite.width;
+      s2sprite.width=boxHeight*0.75;
+      s2sprite.height=s2sprite.width*scaler;
+    }
+    drawSprite(s2sprite);
+    s2sprite.destroy();
+    var s3sprite = createSprite(box1x+(boxHeight*5/4), box1y+(boxHeight/2));
+    s3sprite.setAnimation(World.s3costume);
+    if(s3sprite.height>s3sprite.width){
+      scaler=s3sprite.width/s3sprite.height;
+      s3sprite.height=boxHeight*0.375;
+      s3sprite.width=s3sprite.height*scaler;
+    } else {
+      scaler=s3sprite.height/s3sprite.width;
+      s3sprite.width=boxHeight*0.375;
+      s3sprite.height=s3sprite.width*scaler;
+    }
+    drawSprite(s3sprite);
+    s3sprite.destroy();
+    var s3sprite2 = createSprite(box2x+(boxHeight*5/4), box2y+(boxHeight/2));
+    s3sprite2.setAnimation(World.s3costume);
+    if(s3sprite2.height>s3sprite2.width){
+      scaler=s3sprite2.width/s3sprite2.height;
+      s3sprite2.height=boxHeight*0.375;
+      s3sprite2.width=s3sprite2.height*scaler;
+    } else {
+      scaler=s3sprite2.height/s3sprite2.width;
+      s3sprite2.width=boxHeight*0.375;
+      s3sprite2.height=s3sprite2.width*scaler;
+    }
+    drawSprite(s3sprite2);
+    s3sprite2.destroy();
+    stroke("white");
+    fill("white");
+    textAlign(LEFT, CENTER);
+    textSize(boxHeight/2);
+    text(World.sprite1score,box1x+(boxHeight*3/2), box1y+(boxHeight/2));
+    text(World.sprite2score,box2x+(boxHeight*3/2), box2y+(boxHeight/2));
+    pop();
 
   // Wandering behavior at a certain speed, will be added to both s1 and s2 sprites.
   function movementBehavior(speed) {
@@ -47,7 +122,7 @@ function setupSim(
   for (counter_i = 0; counter_i < s3number; counter_i++) {
     makeNewSpriteAnon(s3costume, randomLocation());
   }
-  setProp({costume: s3costume}, 'scale', 50);
+  setProp({costume: s3costume}, 'scale', 40);
 
 
     
@@ -95,11 +170,11 @@ function setupSim(
       var winnerIndex = randomNumber(0, collidedSprites.length - 1);
       if (collidedSprites[winnerIndex] == s1costume) {
         World.sprite1score += 1;
-        printText(s1costume + ' has collected ' + World.sprite1score);
+        //printText(s1costume + ' has collected ' + World.sprite1score);
       }
       if (collidedSprites[winnerIndex] == s2costume) {
         World.sprite2score += 1;
-        printText(s2costume + ' has collected ' + World.sprite2score);
+        //printText(s2costume + ' has collected ' + World.sprite2score);
       }
       destroy({id: s3_sprite});
     }
@@ -116,11 +191,47 @@ function setupSim(
     if (countByAnimation({costume: s3costume}) === 0) {
       destroy({costume: s1costume});
       destroy({costume: s2costume});
-      printText('The simulation has ended after ' + World.seconds + ' seconds');
-      printText(s1costume + ' has collected ' + World.sprite1score);
-      printText(s2costume + ' has collected ' + World.sprite2score);
+      //printText('The simulation has ended after ' + World.seconds + ' seconds');
+      //printText(s1costume + ' has collected ' + World.sprite1score);
+      //printText(s2costume + ' has collected ' + World.sprite2score);
+      World.endTime=World.seconds;
     }
   }
+
+    //draw the timer
+    var timerTime=0;
+    if(World.endTime){
+      timerTime=World.endTime;
+    }else{
+      timerTime=World.seconds;
+    }
+    push();
+    var timerX=375;
+    var timerY=40;
+    noStroke();
+    fill("white");
+    if(World.endTime){fill("rgba(0,0,0,0.2)");}
+    ellipse(timerX,timerY,32,32);
+    stroke("white");
+    if(World.endTime){stroke("rgba(0,0,0,0.2)");}
+    strokeWeight(7);
+    line(timerX,timerY-22,timerX,timerY-10);
+     stroke("black");
+    strokeWeight(3);
+    line(timerX,timerY-20,timerX,timerY-10);
+    ellipse(timerX, timerY, 25, 25);
+    fill("black");
+    noStroke();
+    var angle=timerTime%60*6;
+    angle-=90;
+    arc(timerX,timerY,18,18,270,angle+10);
+    stroke("white");
+    textAlign(RIGHT, CENTER);
+    strokeWeight(1);
+    textSize(25);
+    text(timerTime,timerX-20,timerY);
+    pop();
+    
 }
 
 other.push(setupSim);
