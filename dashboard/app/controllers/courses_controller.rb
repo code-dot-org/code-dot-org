47d03da85f3ce9de351eae2c632eafb0426c40c2
Dani <dani@code.org>
2021-03-23 18:39:00 -0400

    Add a comment

--- dashboard/app/controllers/courses_controller.rb
+++ dashboard/app/controllers/courses_controller.rb
@@ -109,24 +109,28 @@ class CoursesController < ApplicationController
 
   def vocab
     unit_group = UnitGroup.find_by_name!(params[:course_course_name])
+    # Assumes if one unit in a unit group is migrated they all are
     return render :forbidden unless unit_group.units[0].is_migrated
     puts "Vocab"
   end
 
   def resources
     unit_group = UnitGroup.find_by_name!(params[:course_course_name])
+    # Assumes if one unit in a unit group is migrated they all are
     return render :forbidden unless unit_group.units[0].is_migrated
     puts "Resources"
   end
 
   def code
     unit_group = UnitGroup.find_by_name!(params[:course_course_name])
+    # Assumes if one unit in a unit group is migrated they all are
     return render :forbidden unless unit_group.units[0].is_migrated
     puts "code"
   end
 
   def standards
     unit_group = UnitGroup.find_by_name!(params[:course_course_name])
+    # Assumes if one unit in a unit group is migrated they all are
     return render :forbidden unless unit_group.units[0].is_migrated
     puts "standards"
   end
