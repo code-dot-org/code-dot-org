0876e96ccead480a31d1545f2599e34a329bb456
Dani <dani@code.org>
2021-03-24 15:54:07 -0400

    More clean up in response to feedback

--- apps/src/sites/studio/pages/courses/vocab.js
+++ apps/src/sites/studio/pages/courses/vocab.js
@@ -5,6 +5,5 @@ import ReactDOM from 'react-dom';
 $(document).ready(initPage);
 
 function initPage() {
-  console.log('here');
   ReactDOM.render(<p>Coming Soon</p>, document.getElementById('roll_up'));
 }
