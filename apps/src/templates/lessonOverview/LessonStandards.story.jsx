794a86cb11b66484da506d5a87892981cfa7d9b1
David Bailey <davidsbailey@users.noreply.github.com>
2021-03-24 15:07:54 -0700

    extract sampleStandardsData and expand storybook entries

--- apps/src/templates/lessonOverview/LessonStandards.story.jsx
+++ apps/src/templates/lessonOverview/LessonStandards.story.jsx
@@ -1,42 +1,24 @@
 import React from 'react';
 import LessonStandards from './LessonStandards';
+import {
+  cspStandards,
+  cstaStandards
+} from '../../../test/unit/templates/lessonOverview/sampleStandardsData.js';
 
 export default storybook => {
   storybook.storiesOf('LessonStandards', module).addStoryTable([
     {
-      name: 'LessonStandards with parent category',
-      story: () => (
-        <LessonStandards
-          standards={[
-            {
-              frameworkName: 'Next Generation Science Standards',
-              parentCategoryShortcode: 'ESS',
-              parentCategoryDescription: 'Earth and Space Science',
-              categoryShortcode: 'ESS1',
-              categoryDescription: "Earth's Place in the Universe",
-              shortcode: '1-ESS1-1',
-              description:
-                'Use observations of the sun, moon, and stars to describe patterns that can be predicted.'
-            }
-          ]}
-        />
-      )
+      name: 'standard with parent category',
+      story: () => <LessonStandards standards={[cspStandards[0]]} />
     },
     {
-      name: 'LessonStandards without parent category',
+      name: 'standard without parent category',
+      story: () => <LessonStandards standards={[cstaStandards[0]]} />
+    },
+    {
+      name: 'many standards from different frameworks',
       story: () => (
-        <LessonStandards
-          standards={[
-            {
-              frameworkName: 'CSTA K-12 Computer Science Standards (2017)',
-              categoryShortcode: 'AP',
-              categoryDescription: 'Algorithms & Programming',
-              shortcode: '1B-AP-09',
-              description:
-                'Create programs that use variables to store and modify data.'
-            }
-          ]}
-        />
+        <LessonStandards standards={cspStandards.concat(cstaStandards)} />
       )
     }
   ]);
