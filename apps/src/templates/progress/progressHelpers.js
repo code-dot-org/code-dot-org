a38f3c16380271dfc6e8dd34973cb3c849f3777a
Charlie Forkish <charlie@code.org>
2021-03-25 11:01:43 -0700

    reverted to seconds for timeSpent, tweaked the way we display missing data, fixed tests

--- apps/src/templates/progress/progressHelpers.js
+++ apps/src/templates/progress/progressHelpers.js
@@ -160,17 +160,20 @@ function lessonProgressForStudent(studentLevelProgress, lessonLevels) {
       attempted += levelProgress.status === LevelStatus.attempted;
       imperfect += levelProgress.status === LevelStatus.passed;
       completed += completedStatuses.includes(levelProgress.status);
-      timeSpent += levelProgress.timeSpent;
-      lastTimestamp = Math.max(lastTimestamp, levelProgress.lastTimestamp);
+      timeSpent += levelProgress.timeSpent || 0;
+      lastTimestamp = Math.max(lastTimestamp, levelProgress.lastTimestamp || 0);
     }
   });
 
   const incomplete = filteredLevels.length - completed - imperfect;
   const isLessonStarted = attempted + imperfect + completed > 0;
 
+  if (!isLessonStarted) {
+    return null;
+  }
+
   const getPercent = count => (100 * count) / filteredLevels.length;
   return {
-    isStarted: isLessonStarted,
     incompletePercent: getPercent(incomplete),
     imperfectPercent: getPercent(imperfect),
     completedPercent: getPercent(completed),
