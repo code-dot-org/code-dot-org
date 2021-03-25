e706f8a1528d42eb3c4084bd59071eac942a6d68
Elijah Hamovitz <elijahhamovitz@gmail.com>
2021-03-25 15:37:19 -0700

    DRY up pdf script a bit

--- lib/pdf/conversion.rb
+++ lib/pdf/conversion.rb
@@ -2,26 +2,26 @@ require_relative '../../deployment'
 
 module PDF
   def self.generate_from_url(url, outpath, options={})
-    script_path = "#{deploy_dir}/bin/generate-pdf"
-    cmd = [
-      'node',
-      script_path,
-      '-u', url,
-      '-o', Shellwords.escape(outpath),
-    ].join(" ")
-    puts cmd if options[:verbose]
-    @result = `#{cmd}`
-    puts "  #{@result}" if options[:verbose]
+    invoke_generation_script(
+      [
+        '-u', url,
+        '-o', Shellwords.escape(outpath),
+      ]
+    )
   end
 
   def self.generate_from_html(html, outpath, options={})
+    invoke_generation_script(
+      [
+        '-h', Shellwords.escape(html),
+        '-o', Shellwords.escape(outpath),
+      ]
+    )
+  end
+
+  def self.invoke_generation_script(args, options={})
     script_path = "#{deploy_dir}/bin/generate-pdf"
-    cmd = [
-      'node',
-      script_path,
-      '-h', Shellwords.escape(html),
-      '-o', Shellwords.escape(outpath),
-    ].join(" ")
+    cmd = (['node', script_path] + args).join(" ")
     puts cmd if options[:verbose]
     @result = `#{cmd}`
     puts "  #{@result}" if options[:verbose]
