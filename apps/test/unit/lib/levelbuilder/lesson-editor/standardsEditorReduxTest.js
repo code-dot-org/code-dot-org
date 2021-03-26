30a95f42db06594383c1a89127f49330a031596a
David Bailey <davidsbailey@users.noreply.github.com>
2021-03-25 20:39:22 -0700

    add addStandard to redux

--- apps/test/unit/lib/levelbuilder/lesson-editor/standardsEditorReduxTest.js
+++ apps/test/unit/lib/levelbuilder/lesson-editor/standardsEditorReduxTest.js
@@ -1,5 +1,6 @@
 import {assert} from 'chai';
 import standardsEditor, {
+  addStandard,
   removeStandard
 } from '@cdo/apps/lib/levelbuilder/lesson-editor/standardsEditorRedux';
 import _ from 'lodash';
@@ -28,6 +29,25 @@ const getInitialState = () => _.cloneDeep(fakeStandards);
 describe('standardsEditorRedux reducer', () => {
   let initialState;
   beforeEach(() => (initialState = getInitialState()));
+
+  it('add standard', () => {
+    const nextState = standardsEditor(
+      initialState,
+      addStandard({
+        frameworkShortcode: 'framework-1',
+        frameworkName: 'Framework One',
+        categoryShortcode: 'CS',
+        categoryDescription: 'Computing Systems',
+        shortcode: 'new-1',
+        description: 'fake description'
+      })
+    );
+    assert.deepEqual(nextState.map(s => s.shortcode), [
+      'shortcode-1',
+      'shortcode-2',
+      'new-1'
+    ]);
+  });
 
   it('removes standard', () => {
     const nextState = standardsEditor(
