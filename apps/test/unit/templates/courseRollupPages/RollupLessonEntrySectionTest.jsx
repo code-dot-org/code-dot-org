1b92063b6307e724f2c125e16a057233b8a49afe
Dani <dani@code.org>
2021-03-25 20:43:37 -0400

    Better future proof testing conditions

--- apps/test/unit/templates/courseRollupPages/RollupLessonEntrySectionTest.jsx
+++ apps/test/unit/templates/courseRollupPages/RollupLessonEntrySectionTest.jsx
@@ -20,7 +20,10 @@ describe('RollupLessonEntrySection', () => {
   it('renders list of resources when there are resources', () => {
     const wrapper = mount(<RollupLessonEntrySection {...defaultProps} />);
 
-    expect(wrapper.find('ResourceList').length).to.equal(2);
+    expect(wrapper.text()).to.include('Teacher Resource');
+    expect(wrapper.text()).to.include('Slides');
+    expect(wrapper.text()).to.include('Student Resource');
+    expect(wrapper.text()).to.include('Activity Guide');
   });
 
   it('renders no resources message when no resources', () => {
@@ -95,7 +98,7 @@ describe('RollupLessonEntrySection', () => {
       <RollupLessonEntrySection {...defaultProps} objectToRollUp={'Code'} />
     );
 
-    expect(wrapper.find('StyledCodeBlock').length).to.equal(1);
+    expect(wrapper.text()).to.include('playSound');
   });
 
   it('renders no code message when no code', () => {
