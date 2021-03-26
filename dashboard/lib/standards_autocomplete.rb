a13dbde5c38ac664228148ecaac12891509a711e
David Bailey <davidsbailey@users.noreply.github.com>
2021-03-25 17:10:51 -0700

    add fix standards autocomplete helper and add unit tests

--- dashboard/lib/standards_autocomplete.rb
+++ dashboard/lib/standards_autocomplete.rb
@@ -1,14 +1,16 @@
 class StandardsAutocomplete < AutocompleteHelper
-  def self.get_search_matches(query, limit, framework_id)
-    query = format_query(query)
-    return [] if query.length < MIN_WORD_LENGTH
-
+  def self.get_search_matches(query, limit, framework_id = nil)
     limit = format_limit(limit)
     rows = Standard.limit(limit)
     rows = rows.where(framework_id: framework_id) if framework_id
 
-    rows = rows.
-      where("MATCH(shortcode,description) AGAINST(? in BOOLEAN MODE)", query)
-    return rows.map(&:summarize_for_lesson_edit)
+    shortcode_rows = rows.where('shortcode like ?', "#{query}%").all
+    description_rows =
+      query.length > MIN_WORD_LENGTH ?
+        rows.where(
+          'MATCH(shortcode,description) AGAINST(? in BOOLEAN MODE)',
+          format_query(query)
+        ).all : []
+    return (shortcode_rows + description_rows).map(&:summarize_for_lesson_edit)
   end
 end
