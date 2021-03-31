8a187b30a5406fd75c22efe262369d36129efcbc
molly-moen <molly@code.org>
2021-03-31 11:01:08 -0700

    use rsa token format

--- dashboard/test/controllers/java_builder_sessions_controller_test.rb
+++ dashboard/test/controllers/java_builder_sessions_controller_test.rb
@@ -1,11 +1,26 @@
 require 'test_helper'
 
 class JavaBuilderSessionsControllerTest < ActionController::TestCase
-  test 'levelbuilder can get access token' do
+  setup do
+    @rsa_key_test = OpenSSL::PKey::RSA.new(2048)
+    OpenSSL::PKey::RSA.stubs(:new).returns(@rsa_key_test)
+  end
+
+  test_user_gets_response_for :get_access_token, user: :student, response: :forbidden
+  test_user_gets_response_for :get_access_token, user: :levelbuilder, response: :success
+
+  test 'can decode jwt token' do
     levelbuilder = create :levelbuilder
     sign_in(levelbuilder)
     get :get_access_token
-    assert_response :success
+
+    response = JSON.parse(@response.body)
+    token = response['token']
+    decoded_token = JWT.decode(token, @rsa_key_test.public_key, true, {algorithm: 'RS256'})
+
+    # token[0] is the JWT header
+    assert_not_nil decoded_token[0]['iat']
+    assert_not_nil decoded_token[0]['exp']
   end
 
   test 'csa pilot participant can get access token' do
@@ -14,12 +29,5 @@ class JavaBuilderSessionsControllerTest < ActionController::TestCase
     sign_in(user)
     get :get_access_token
     assert_response :success
-  end
-
-  test 'regular user cannot get access token' do
-    user = create :user
-    sign_in(user)
-    get :get_access_token
-    assert_response :forbidden
   end
 end
