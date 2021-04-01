2286fbcdd635c29e55f82a0241548f650bb7c088
David Bailey <davidsbailey@users.noreply.github.com>
2021-04-01 13:00:31 -0700

    add unit tests for standards expand/collapse

--- apps/test/unit/templates/lessonOverview/LessonStandardsTest.js
+++ apps/test/unit/templates/lessonOverview/LessonStandardsTest.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {mount} from 'enzyme';
 import {expect} from '../../../util/reconfiguredChai';
-import LessonStandards from '@cdo/apps/templates/lessonOverview/LessonStandards';
+import LessonStandards, {
+  ExpandMode
+} from '@cdo/apps/templates/lessonOverview/LessonStandards';
 import {cspStandards, cstaStandards} from './sampleStandardsData';
 
 describe('LessonStandards', () => {
@@ -37,5 +39,71 @@ describe('LessonStandards', () => {
       expect(text).to.contain(standard.shortcode);
       expect(text).to.contain(standard.description);
     });
+
+    it('renders many standards with all standards expanded', () => {
+      const standards = cspStandards.concat(cstaStandards);
+      const wrapper = mount(
+        <LessonStandards standards={standards} expandMode={ExpandMode.ALL} />
+      );
+
+      const frameworks = wrapper.find('Framework');
+      expect(frameworks.length).to.equal(2);
+      frameworks.forEach(framework => {
+        expect(isOpen(framework)).to.be.false;
+        const parentCategories = framework.find('ParentCategory');
+        parentCategories.forEach(parentCategory => {
+          expect(isOpen(parentCategory)).to.be.false;
+          const categories = parentCategory.find('Category');
+          categories.forEach(category => {
+            expect(isOpen(category)).to.be.false;
+          });
+        });
+      });
+    });
+  });
+
+  it('renders many standards with first standard expanded', () => {
+    const standards = cspStandards.concat(cstaStandards);
+    const wrapper = mount(
+      <LessonStandards standards={standards} expandMode={ExpandMode.FIRST} />
+    );
+    const frameworks = wrapper.find('Framework');
+    expect(frameworks.length).to.equal(2);
+    expect(isOpen(frameworks.at(0))).to.be.true;
+    expect(isOpen(frameworks.at(1))).to.be.false;
+
+    const parentCategories = frameworks.at(0).find('ParentCategory');
+    expect(parentCategories.length).to.equal(1);
+    expect(isOpen(parentCategories.at(0))).to.be.true;
+
+    const categories = parentCategories.at(0).find('Category');
+    expect(categories.length).to.equal(2);
+    expect(isOpen(categories.at(0))).to.be.true;
+    expect(isOpen(categories.at(1))).to.be.false;
+  });
+
+  it('renders many standards with all standards expanded', () => {
+    const standards = cspStandards.concat(cstaStandards);
+    const wrapper = mount(
+      <LessonStandards standards={standards} expandMode={ExpandMode.ALL} />
+    );
+    const frameworks = wrapper.find('Framework');
+    expect(frameworks.length).to.equal(2);
+    frameworks.forEach(framework => {
+      expect(isOpen(framework)).to.be.true;
+      const parentCategories = framework.find('ParentCategory');
+      parentCategories.forEach(parentCategory => {
+        expect(isOpen(parentCategory)).to.be.true;
+        const categories = parentCategory.find('Category');
+        categories.forEach(category => {
+          expect(isOpen(category)).to.be.true;
+        });
+      });
+    });
   });
 });
+
+function isOpen(wrapper) {
+  const details = wrapper.find('details').first();
+  return details.prop('open');
+}
