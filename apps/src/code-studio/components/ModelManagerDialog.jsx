6da304170dd600c3b8d7d00338451a73123697cb
Erin Bond <erin.bond@code.org>
2021-04-01 18:06:21 -0400

    confirmation dialog, include metadata in intial fetch, style more consistent with other dialogs

--- apps/src/code-studio/components/ModelManagerDialog.jsx
+++ apps/src/code-studio/components/ModelManagerDialog.jsx
@@ -4,8 +4,15 @@ import $ from 'jquery';
 import BaseDialog from '@cdo/apps/templates/BaseDialog';
 import Button from '@cdo/apps/templates/Button';
 import ModelCard from './ModelCard';
+import color from '@cdo/apps/util/color';
+
+const DEFAULT_MARGIN = 7;
 
 const styles = {
+  dialog: {
+    padding: '0 15px',
+    cursor: 'default'
+  },
   left: {
     float: 'left',
     width: '40%',
@@ -17,6 +24,19 @@ const styles = {
     width: '60%',
     padding: 20,
     boxSizing: 'border-box'
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 24,
+    marginTop: 20
+  },
+  message: {
+    color: color.dark_charcoal,
+    textAlign: 'left',
+    margin: DEFAULT_MARGIN,
+    overflow: 'hidden',
+    lineHeight: '15px',
+    whiteSpace: 'pre-wrap'
   }
 };
 
@@ -30,14 +50,15 @@ export default class ModelManagerDialog extends React.Component {
   };
 
   state = {
-    selectedModelId: undefined,
+    selectedModel: undefined,
     models: [],
-    isImportPending: false
+    isImportPending: false,
+    confirmDialogOpen: false
   };
 
   componentDidUpdate(prevProps) {
     if (this.props.isOpen && !prevProps.isOpen) {
-      this.setState({selectedModelId: undefined, models: []});
+      this.setState({selectedModel: undefined, models: []});
       this.getModelList();
     }
   }
@@ -54,8 +75,12 @@ export default class ModelManagerDialog extends React.Component {
       if (this.props.levelbuilderModel?.id) {
         models.unshift(this.props.levelbuilderModel);
       }
-      this.setState({models});
+      this.setState({models, selectedModel: models[0]});
     });
+  };
+
+  getModelById = id => {
+    return this.state.models.find(model => model.id === id);
   };
 
   importMLModel = async () => {
@@ -67,7 +92,27 @@ export default class ModelManagerDialog extends React.Component {
   };
 
   handleChange = e => {
-    this.setState({selectedModelId: e.target.value});
+    const model = this.getModelById(e.target.value);
+    this.setState({selectedModel: model});
+  };
+
+  showDeleteConfirmation = () => {
+    this.setState({confirmDialogOpen: true});
+    this.closeModelManager();
+  };
+
+  closeConfirmDialog = () => {
+    this.setState({confirmDialogOpen: false});
+    this.closeModelManager();
+  };
+
+  deleteModel = () => {
+    $.ajax({
+      url: `/api/v1/ml_models/${this.state.selectedModel.id}`,
+      method: 'DELETE'
+    }).then(() => {
+      this.setState({confirmDialogOpen: false});
+    });
   };
 
   render() {
@@ -80,8 +125,9 @@ export default class ModelManagerDialog extends React.Component {
           isOpen={isOpen}
           handleClose={this.closeModelManager}
           useUpdatedStyles
+          style={styles.dialog}
         >
-          <h2>AI Trained Models</h2>
+          <h1 style={styles.header}>AI Trained Models</h1>
           <div style={styles.left}>
             <select
               name="model"
@@ -104,9 +150,50 @@ export default class ModelManagerDialog extends React.Component {
               isPending={this.state.isImportPending}
               pendingText={'Importing...'}
             />
+            <Button
+              text={'Delete'}
+              color={Button.ButtonColor.red}
+              onClick={this.showDeleteConfirmation}
+              disabled={noModels}
+              icon={'trash'}
+              iconClassName={'fa-trash'}
+            />
           </div>
           <div style={styles.right}>
-            <ModelCard modelId={this.state.selectedModelId} />
+            <ModelCard model={this.state.selectedModel} />
+          </div>
+        </BaseDialog>
+        <BaseDialog
+          isOpen={this.state.confirmDialogOpen}
+          handleClose={this.closeConfirmDialog}
+          useUpdatedStyles
+          style={styles.dialog}
+        >
+          <h1 style={styles.header}>
+            Are you sure you would like to delete this model?
+          </h1>
+          <div style={styles.left}>
+            <p style={styles.message}>
+              This model will be permanently deleted, and you will not be able
+              to use this model in any App Lab projects.
+            </p>
+            <div>
+              <Button
+                text={'No'}
+                color={Button.ButtonColor.orange}
+                onClick={this.closeConfirmDialog}
+              />
+              <Button
+                text={'Delete'}
+                color={Button.ButtonColor.red}
+                onClick={this.deleteModel}
+                icon={'trash'}
+                iconClassName={'fa-trash'}
+              />
+            </div>
+          </div>
+          <div style={styles.right}>
+            <ModelCard model={this.state.selectedModel} />
           </div>
         </BaseDialog>
       </div>
