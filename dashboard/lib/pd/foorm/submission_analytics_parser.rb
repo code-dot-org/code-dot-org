dd52c1d59ec9bd02b99fd28a71c754ea273d31c2
Ben Brooks <ben@code.org>
2021-04-01 11:51:18 -0700

    Finish V0 Foorm submission parser

--- dashboard/lib/pd/foorm/submission_analytics_parser.rb
+++ dashboard/lib/pd/foorm/submission_analytics_parser.rb
@@ -1,31 +1,63 @@
 module Pd::Foorm
   HEADERS = %w(
     submission_id
-    form_question_name
+    question_name
     matrix_item_name
     response_value
     response_text
   )
 
   class SubmissionAnalyticsParser
-    def self.write_foorm_submissions_csv_to_file_and_bucket_long_for_form(form)
-      CSV.open('./test.csv', 'wb') do |csv|
-        csv << ['submission_id', 'question_key', 'answer']
-      end
-      #AWS::S3.upload_to_bucket('cdo-data-sharing-internal', file_name_from_form_name(form), IO.read('./test.csv'), no_random: true)
+    include Constants
+
+    def self.reshape_all_submissions
+      reshaped_submissions = reshape_submissions(::Foorm::Submission.all)
+      reshaped_submissions_to_csv(reshaped_submissions)
     end
 
-    def self.reshaped_submission_answer(submission)
-      reshaped_submission_answers = []
+    def self.reshaped_submissions_to_csv(reshaped_submissions)
+      CSV.open('./test_submissions.csv', 'wb') do |csv|
+        csv << HEADERS
+        reshaped_submissions.each do |answer|
+          row = answer.stringify_keys.values_at(*HEADERS)
+          csv << row
+        end
+      end
+    end
+
+    def self.reshape_submissions(submissions)
+      reshaped_submissions = []
+
+      submissions.each do |submission|
+        reshaped_submissions = reshaped_submissions.concat reshape_submission(submission)
+      end
+
+      reshaped_submissions
+    end
+
+    def self.reshape_submission(submission)
+      reshaped_submission_answers_with_metadata = []
+
+      submission_metadata = {
+        submission_id: submission.id
+      }
 
       parsed_answers = JSON.parse(submission.answers)
+      reshaped_submission_answers = reshape_submission_answers(parsed_answers, submission.form)
+
+      reshaped_submission_answers.each do |reshaped_submission_answer|
+        reshaped_submission_answers_with_metadata << submission_metadata.merge(reshaped_submission_answer)
+      end
+
+      reshaped_submission_answers_with_metadata
+    end
+
+    def self.reshape_submission_answers(parsed_answers, form)
+      reshaped_submission_answers = []
 
       parsed_answers.each do |question_name, answer|
-        question_details = submission.form.get_question_details(question_name)
-
-        reshaped_submission_answer = {
-          question_name: question_name
-        }
+        reshaped_submission_answer = {question_name: question_name}
+        question_details = form&.get_question_details(question_name)
 
         # If question isn't in the Form, return as-is.
         # This is expected for metadata about the submission.
@@ -34,6 +66,16 @@ module Pd::Foorm
           reshaped_submission_answers << reshaped_submission_answer
           next
         end
+
+        # What are we doing here? (if we want to refactor)
+        # take base hash
+        # get additional attributes
+        # merge those additional attributes into hash
+        # add hash into list of all answers
+        #
+        # Challenges:
+        # Make this work for matrix questions
+        # Make this work for questions that are not in the Form
 
         case question_details[:type]
         when ANSWER_MATRIX
@@ -44,35 +86,39 @@ module Pd::Foorm
 
             additional_attributes = {
               matrix_item_name: matrix_item_name,
-              response_value: matrix_item_name,
+              response_value: matrix_item_answer,
               response_text: choices[matrix_item_answer]
             }
 
             reshaped_matrix_item_submission.merge! additional_attributes
-
             reshaped_submission_answers << reshaped_matrix_item_submission
           end
-          #### Ended editing here
         when ANSWER_RATING, ANSWER_TEXT
-          puts 'this one is for you, rubocop'
-          # additional_attributes = {
-          #   response_text: answer
-          # }
+          reshaped_submission_answer[:response_text] = answer
+
+          reshaped_submission_answers << reshaped_submission_answer
         when ANSWER_SINGLE_SELECT
           choices = question_details[:choices]
-          return {question_id => choices[answer]}
+          additional_attributes = {
+            response_value: answer,
+            response_text: choices[answer]
+          }
+
+          reshaped_submission_answer.merge! additional_attributes
+          reshaped_submission_answers << reshaped_submission_answer
         when ANSWER_MULTI_SELECT
           choices = question_details[:choices]
-          return {question_id => answer.map {|selected| choices[selected]}.compact.sort.join(', ')}
-        end
+          additional_attributes = {
+            response_value: answer,
+            response_text: answer.map {|selected| choices[selected]}.compact.sort.join(', ')
+          }
 
-        # Return blank hash if question_type not found
-        return {}
+          reshaped_submission_answer.merge! additional_attributes
+          reshaped_submission_answers << reshaped_submission_answer
+        end
       end
-      # need to split out matrix question id
-      # need to include question key (not just plain text question)
-      rows = submission.formatted_answers.map {|question_key, answer| [submission.id, question_key, answer]}
-      rows.each {|row| csv << row}
+
+      reshaped_submission_answers
     end
   end
 end
