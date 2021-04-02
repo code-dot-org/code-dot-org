391cc916f1bd3a345abe98e6d6d20b8c2f21acfe
molly-moen <molly@code.org>
2021-04-02 15:40:53 -0700

    add more to jwt token

--- dashboard/app/controllers/java_builder_sessions_controller.rb
+++ dashboard/app/controllers/java_builder_sessions_controller.rb
@@ -1,4 +1,5 @@
 require 'jwt'
+require 'securerandom' unless defined?(SecureRandom)
 
 class JavaBuilderSessionsController < ApplicationController
   authorize_resource class: false
@@ -8,6 +9,18 @@ class JavaBuilderSessionsController < ApplicationController
 
   # GET /javabuilder/access_token
   def get_access_token
+    channel_id = params[:channelId]
+    project_version = params[:projectVersion]
+    if !channel_id || !project_version
+      return render status: :bad_request, json: {}
+    end
+
+    begin
+      storage_id, storage_app_id = storage_decrypt_channel_id(channel_id)
+    rescue ArgumentError, OpenSSL::Cipher::CipherError
+      return render status: :bad_request, json: {}
+    end
+
     issued_at_time = Time.now.to_i
     # expire token in 15 minutes
     expiration_time = (Time.now + 15.minutes).to_i
@@ -15,7 +28,13 @@ class JavaBuilderSessionsController < ApplicationController
     payload = JWT.encode(
       {
         iat: issued_at_time,
-        exp: expiration_time
+        exp: expiration_time,
+        sid: SecureRandom.hex(18), # session id
+        uid: current_user.id,
+        storage_id: storage_id,
+        storage_app_id: storage_app_id,
+        channel_id: channel_id,
+        project_version: project_version
       },
       OpenSSL::PKey::RSA.new(PRIVATE_KEY, PASSWORD),
       'RS256'
