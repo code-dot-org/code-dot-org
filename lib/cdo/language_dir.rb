0b8df8ae104c2e2592ff6a74dcf5488b21f139f4
Ameer Akashe <ameer.akashe@accenture.com>
2021-04-06 14:32:35 -0500

    linting fixes

--- lib/cdo/language_dir.rb
+++ lib/cdo/language_dir.rb
@@ -1,5 +1,5 @@
 def language_dir_class(locale=request.locale)
-    # This list of RTL languages matches those in dashboard/config/locales.yml
+  # This list of RTL languages matches those in dashboard/config/locales.yml
   if ["ar-SA", "fa-IR", "he-IL", "ur-PK"].include? locale
     "rtl"
   else
