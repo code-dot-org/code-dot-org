c2a8338520763dfaa3a0c9ba94a51b4243a553c1
Erin Bond <erin.bond@code.org>
2021-04-07 09:33:24 -0400

    return no content for ml_model destroy

--- dashboard/app/controllers/api/v1/ml_models_controller.rb
+++ dashboard/app/controllers/api/v1/ml_models_controller.rb
@@ -58,6 +58,7 @@ class Api::V1::MlModelsController < Api::V1::JsonApiController
     status =
       @user_ml_model.destroyed? && deleted_from_s3 ? "success" : "failure"
     render json: {id: @user_ml_model.id, status: status}
+    head :no_content
   end
 
   private
