ea7141d84fe448c3b8579a6dbf94e6c45feccda2
Madeline <madelign@gmail.com>
2021-04-06 17:47:38 -0700

    Code review feedback

--- frontend/packages/labs/ailab/src/datasetManifest.js
+++ frontend/packages/labs/ailab/src/datasetManifest.js
@@ -245,7 +245,7 @@ export function getAvailableDatasets(specificDatasets) {
   // Filter datasets by a specified array (in Levelbuilder) or filter out toy datasets.
   let availableDatasets = (specificDatasets && specificDatasets.length > 1) ?
     allDatasets.filter(dataset => specificDatasets.includes(dataset.id)) :
-    allDatasets.filter(dataset => dataset.isToy !== true);
+    allDatasets.filter(dataset => !dataset.isToy);
 
   // Sort dataset names by alphabetical order.
   return availableDatasets.sort((a, b) => a.name.localeCompare(b.name));
