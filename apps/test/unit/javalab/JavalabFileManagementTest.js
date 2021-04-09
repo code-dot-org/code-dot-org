49e5e3723c2114d005cd2efafeacb0199a2a964d
Jillian Khoo <jillian9012@gmail.com>
2021-04-09 23:10:43 +0000

    added tests

---
+++ apps/test/unit/javalab/JavalabFileManagementTest.js
@@ -0,0 +1,55 @@
+import sinon from 'sinon';
+import {expect} from '../../util/reconfiguredChai';
+import javalab, {
+  setFileName,
+  setEditorText
+} from '@cdo/apps/javalab/javalabRedux';
+import {
+  getStore,
+  registerReducers,
+  stubRedux,
+  restoreRedux
+} from '@cdo/apps/redux';
+import {populateFiles} from '@cdo/apps/javalab/JavalabFileManagement';
+
+describe('JavalabFileManagement', () => {
+  beforeEach(() => {
+    stubRedux();
+    registerReducers({javalab});
+    window.appOptions = {level: {}};
+    sinon.stub(getStore(), 'dispatch');
+  });
+
+  afterEach(() => {
+    restoreRedux();
+  });
+
+  describe('populateFiles', () => {
+    it('populates start_sources if there are any and there are no file entries', () => {
+      window.appOptions.level = {
+        startSources: {
+          'File.java': {
+            text: 'Some code',
+            visible: true
+          }
+        }
+      };
+      const successStub = sinon.stub();
+      populateFiles([], successStub, () => {});
+      expect(successStub).to.have.been.calledOnce;
+      expect(getStore().dispatch).to.have.been.calledWith(
+        setFileName('File.java')
+      );
+      expect(getStore().dispatch).to.have.been.calledWith(
+        setEditorText('Some code')
+      );
+    });
+
+    it('succeeds if there are no file entries and no start sources', () => {
+      window.appOptions.level = {};
+      const successStub = sinon.stub();
+      populateFiles([], successStub, () => {});
+      expect(successStub).to.have.been.calledOnce;
+    });
+  });
+});
