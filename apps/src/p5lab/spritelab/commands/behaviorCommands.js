b2e2cf34c6aec2302b13d75544c61536c6b4b725
anjali <anjali@code.org>
2021-04-09 15:55:46 -0700

    support simultaneously following and avoiding

--- apps/src/p5lab/spritelab/commands/behaviorCommands.js
+++ apps/src/p5lab/spritelab/commands/behaviorCommands.js
@@ -34,12 +34,12 @@ export const commands = {
       const sprite = coreLibrary.getSpriteArray(spriteArg)[0];
       const spritePosition = sprite.position;
 
-      if (!sprite.targetSet) {
+      if (!sprite.targetSet?.avoid) {
         return;
       }
 
       const range = 100;
-      const targetsInRange = sprite.targetSet
+      const targetsInRange = sprite.targetSet.avoid
         .map(x => coreLibrary.getSpriteArray({costume: x}))
         .flat()
         .filter(target => spritePosition.dist(target.position) < range);
@@ -70,11 +70,11 @@ export const commands = {
       const sprite = coreLibrary.getSpriteArray(spriteArg)[0];
       const spritePosition = sprite.position;
 
-      if (!sprite.targetSet) {
+      if (!sprite.targetSet?.follow) {
         return;
       }
 
-      const targets = sprite.targetSet
+      const targets = sprite.targetSet.follow
         .map(x => coreLibrary.getSpriteArray({costume: x}))
         .flat();
 
