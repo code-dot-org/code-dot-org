677cad3666d5c7b055dbebed788f76e983feb65c
Madeline <madelign@gmail.com>
2021-04-12 16:51:43 -0700

    Added statement and model card info to summary page

--- frontend/packages/labs/ailab/src/UIComponents/ModelSummary.jsx
+++ frontend/packages/labs/ailab/src/UIComponents/ModelSummary.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { saveMessages } from "../constants";
+import Statement from "./Statement";
 
 class ModelSummary extends Component {
   static propTypes = {
@@ -11,9 +12,17 @@ class ModelSummary extends Component {
   };
 
   render() {
+    const { trainedModelDetails } = this.props;
+
     return (
       <div>
-        <p>hello</p>
+        <Statement />
+        <p>Model Name: {trainedModelDetails.name}</p>
+        <p>How can this model be used? {trainedModelDetails.potentialUses}</p>
+        <p>
+          How can this model be potentially misused?
+          {trainedModelDetails.potentialMisuses}
+        </p>
         <div>
           {this.props.saveStatus && (
             <div style={{ position: "absolute", bottom: 0 }}>
