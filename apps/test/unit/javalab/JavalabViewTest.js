a4989ff8dd25112b5f2518aed42caaf0a24ad6cd
Jillian Khoo <jillian9012@gmail.com>
2021-04-13 20:52:56 +0000

    fix tests, add comments for why we need to use an unconnected component

--- apps/test/unit/javalab/JavalabViewTest.js
+++ apps/test/unit/javalab/JavalabViewTest.js
@@ -1,82 +1,40 @@
 import React from 'react';
 import {expect} from '../../util/reconfiguredChai';
-import {mount} from 'enzyme';
-import JavalabView from '@cdo/apps/javalab/JavalabView';
-import {Provider} from 'react-redux';
-import pageConstants, {setPageConstants} from '@cdo/apps/redux/pageConstants';
-import {
-  getStore,
-  registerReducers,
-  stubRedux,
-  restoreRedux
-} from '@cdo/apps/redux';
-import javalab, {toggleDarkMode} from '@cdo/apps/javalab/javalabRedux';
+import {shallow} from 'enzyme';
+// We use the UnconnectedJavalabView because shallow().dive() does not work with our version of react-redux
+import {UnconnectedJavalabView as JavalabView} from '@cdo/apps/javalab/JavalabView';
 import color from '@cdo/apps/util/color';
 global.$ = require('jquery');
 
 describe('Java Lab View Test', () => {
-  let store, defaultProps;
+  let defaultProps;
 
   beforeEach(() => {
-    stubRedux();
-    registerReducers({javalab});
-    registerReducers({pageConstants});
-    store = getStore();
-    store.dispatch(
-      setPageConstants({
-        isProjectLevel: true,
-        isReadOnlyWorkspace: false,
-        channelId: 'id',
-        isEmbedView: false,
-        isShareView: false
-      })
-    );
     defaultProps = {
       onMount: () => {},
       onContinue: () => {},
-      onCommitCode: () => {}
+      onCommitCode: () => {},
+      isProjectLevel: false,
+      isReadOnlyWorkspace: false,
+      isDarkMode: false
     };
   });
 
-  afterEach(() => {
-    restoreRedux();
-  });
-
-  const createWrapper = () => {
-    return mount(
-      <Provider store={store}>
-        <JavalabView {...defaultProps} />
-      </Provider>
-    );
-  };
-
   describe('getButtonStyles', () => {
     it('Is cyan or orange in light mode', () => {
-      const editor = createWrapper();
-      const notSettings = editor
-        .find('JavalabView')
-        .instance()
-        .getButtonStyles(false);
+      let editor = shallow(<JavalabView {...defaultProps} />);
+      const notSettings = editor.instance().getButtonStyles(false);
       expect(notSettings.backgroundColor).to.equal(color.cyan);
-      const settings = editor
-        .find('JavalabView')
-        .instance()
-        .getButtonStyles(true);
+      const settings = editor.instance().getButtonStyles(true);
       expect(settings.backgroundColor).to.equal(color.orange);
     });
 
     it('Is grey in dark mode', () => {
-      const editor = createWrapper();
-      store.dispatch(toggleDarkMode());
-      const notSettings = editor
-        .find('JavalabView')
-        .instance()
-        .getButtonStyles(false);
+      let props = {...defaultProps, isDarkMode: true};
+      let editor = shallow(<JavalabView {...props} />);
+      const notSettings = editor.instance().getButtonStyles(false);
       expect(notSettings.backgroundColor).to.equal('#272822');
-      const settings = editor
-        .find('JavalabView')
-        .instance()
-        .getButtonStyles(false);
+      const settings = editor.instance().getButtonStyles(false);
       expect(settings.backgroundColor).to.equal('#272822');
     });
   });
