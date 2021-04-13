e93e0f25e3a51eee755146ec84f60ef5c99bc05f
David Bailey <davidsbailey@users.noreply.github.com>
2021-04-13 16:01:48 -0700

    test serializing lesson group description

--- apps/test/unit/code-studio/scriptEditorReduxTest.js
+++ apps/test/unit/code-studio/scriptEditorReduxTest.js
@@ -20,6 +20,7 @@ const getInitialState = () => ({
       id: 1,
       key: 'lg-key',
       displayName: 'Display Name',
+      description: 'My Description',
       position: 1,
       userFacing: true,
       lessons: [
@@ -102,6 +103,7 @@ describe('scriptEditorRedux reducer tests', () => {
       // Verify that the JSON contains serialized lesson groups.
       expect(serializedLessonGroups).to.equal(
         "lesson_group 'lg-key', display_name: 'Display Name'\n" +
+          "lesson_group_description 'My Description'\n" +
           "lesson 'a', display_name: 'A', has_lesson_plan: true, unplugged: true\n\n" +
           "lesson 'b', display_name: 'B', has_lesson_plan: false\n\n" +
           "lesson 'c', display_name: 'C', has_lesson_plan: true\n\n" +
