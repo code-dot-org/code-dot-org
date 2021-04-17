01381bbdc58ee1397aafeec55552a7c5a1f4525a
Dani <dani@code.org>
2021-04-16 23:01:56 -0400

    Update SummaryProgresTable storybook

--- apps/src/templates/progress/progressTestHelpers.js
+++ apps/src/templates/progress/progressTestHelpers.js
@@ -97,6 +97,40 @@ export const createStoreWithHiddenLesson = (viewAs, lessonId) => {
   });
 };
 
+/**
+ * Creates the shell of a redux store with the provided lessonId being hidden
+ * @param {ViewType} viewAs
+ * @param {number?} lessonId - Lesson to hide (or null if none)
+ */
+export const createStoreWithLockedLesson = (
+  viewAs,
+  lockableAuthorized = false
+) => {
+  return createStore(state => state, {
+    stageLock: {
+      stagesBySectionId: {
+        '11': {}
+      },
+      lockableAuthorized: lockableAuthorized
+    },
+    viewAs: viewAs,
+    teacherSections: {
+      selectedSectionId: '11'
+    },
+    hiddenStage: Immutable.fromJS({
+      stagesBySection: {
+        '11': {[lessonId]: true}
+      }
+    }),
+    progress: {
+      showTeacherInfo: false
+    },
+    currentUser: {
+      userId: 1
+    }
+  });
+};
+
 const randomNumberUpTo100 = () => {
   return Math.floor(Math.random() * 100);
 };
