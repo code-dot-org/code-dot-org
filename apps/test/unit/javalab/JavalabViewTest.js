46e0d1e2151175efa65b91e335a20a4732a7122b
Jessica Kulwik <jessica@code.org>
2021-04-17 00:20:27 +0000

    Updated failing tests

--- apps/test/unit/javalab/JavalabViewTest.js
+++ apps/test/unit/javalab/JavalabViewTest.js
@@ -14,6 +14,7 @@ describe('Java Lab View Test', () => {
   beforeEach(() => {
     defaultProps = {
       onMount: () => {},
+      onRun: () => {},
       onContinue: () => {},
       onCommitCode: () => {},
       onInputMessage: () => {},
