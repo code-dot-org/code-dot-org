5c5cf54d4e634ff365f27e83f8b5631e040a077a
Erin Peach <erin@code.org>
2021-04-19 14:38:27 -0700

    Connect haml to js for UI

---
+++ apps/src/sites/studio/pages/sprite_management/sprite_upload.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+$(document).ready(function() {
+  ReactDOM.render(
+    <h2>Sprite Upload</h2>,
+    document.getElementById('sprite-upload-container')
+  );
+});
