460b9c8a4ec2471d581ad6fd7954c06aa78f5506
Maureen <maureen.sturgeon@gmail.com>
2021-04-19 16:57:43 -0700

    add tests, move script_progress_for_users to users_helper

--- dashboard/test/helpers/users_helper_test.rb
+++ dashboard/test/helpers/users_helper_test.rb
@@ -313,8 +313,11 @@ class UsersHelperTest < ActionView::TestCase
     )
   end
 
-  def test_merge_user_progress_by_level
-    user = create :user, total_lines: 150
+  def test_script_progress_for_users
+    user_1 = create :user
+    user_2 = create :user
+
+    # set up progress
     script = create :script
 
     lesson_group = create :lesson_group, script: script
@@ -325,36 +328,45 @@ class UsersHelperTest < ActionView::TestCase
     sublevel2 = create :level, name: 'choice_2'
     level = create :bubble_choice_level, sublevels: [sublevel1, sublevel2]
     create :script_level, script: script, levels: [level], lesson: lesson
-    create :user_level, user: user, level: sublevel1, script: script, best_result: ActivityConstants::BEST_PASS_RESULT, time_spent: 180
-    create :user_level, user: user, level: sublevel2, script: script, best_result: 20, time_spent: 300
 
-    user_levels_by_level = user.user_levels_by_level(script)
-    paired_user_levels = PairedUserLevel.pairs(user_levels_by_level.values.map(&:id))
+    sublevel1_user_level = create :user_level, user: user_1, level: sublevel1, script: script, best_result: ActivityConstants::BEST_PASS_RESULT, time_spent: 180
+    sublevel2_user_level = create :user_level, user: user_1, level: sublevel2, script: script, best_result: 20, time_spent: 300
 
-    progress = {
-      # BubbleChoice levels return status/result using the sublevel with the highest best_result.
-      level.id => {
-        status: LEVEL_STATUS.perfect,
-        result: ActivityConstants::BEST_PASS_RESULT,
-        time_spent: 480
+    sublevel1_last_progress = UserLevel.find(sublevel1_user_level.id).updated_at.to_i
+    sublevel2_last_progress = UserLevel.find(sublevel2_user_level.id).updated_at.to_i
+
+    expected_progress = [
+      {
+        user_1.id => {
+          level.id => {
+            status: LEVEL_STATUS.perfect,
+            result: ActivityConstants::BEST_PASS_RESULT,
+            last_progress_at: sublevel1_last_progress,
+            time_spent: 480 # sum of time spent on sublevels
+          },
+          sublevel1.id => {
+            status: LEVEL_STATUS.perfect,
+            result: ActivityConstants::BEST_PASS_RESULT,
+            last_progress_at: sublevel1_last_progress,
+            time_spent: 180
+          },
+          sublevel2.id => {
+            status: LEVEL_STATUS.passed,
+            result: 20,
+            last_progress_at: sublevel2_last_progress,
+            time_spent: 300
+          }
+        },
+        user_2.id => {}
       },
-      sublevel1.id => {
-        status: LEVEL_STATUS.perfect,
-        result: ActivityConstants::BEST_PASS_RESULT,
-        time_spent: 180
-      },
-      sublevel2.id => {
-        status: LEVEL_STATUS.passed,
-        result: 20,
-        time_spent: 300
+      {
+        user_1.id => sublevel1_last_progress,
+        user_2.id => nil
       }
-    }
+    ]
 
-    assert_equal progress, merge_user_progress_by_level(
-      script: script,
-      user: user,
-      user_levels_by_level: user_levels_by_level,
-      paired_user_levels: paired_user_levels
+    assert_equal expected_progress, script_progress_for_users(
+      [user_1, user_2], script
     )
   end
 
