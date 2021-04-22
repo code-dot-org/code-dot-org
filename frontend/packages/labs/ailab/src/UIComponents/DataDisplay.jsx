bf16f2031f50cfdcb6fdd3eeece66a163e21a4ba
Brendan Reville <brendan@code.org>
2021-04-23 09:03:24 +1000

    Scrolling no longer deselects the current column

--- frontend/packages/labs/ailab/src/UIComponents/DataDisplay.jsx
+++ frontend/packages/labs/ailab/src/UIComponents/DataDisplay.jsx
@@ -4,17 +4,15 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Statement from "./Statement";
 import DataTable from "./DataTable";
-import { setCurrentColumn } from "../redux";
 import { styles } from "../constants";
 
 class DataDisplay extends Component {
   static propTypes = {
-    data: PropTypes.array,
-    setCurrentColumn: PropTypes.func
+    data: PropTypes.array
   };
 
   render() {
-    const { data, setCurrentColumn } = this.props;
+    const { data } = this.props;
 
     if (data.length === 0) {
       return null;
@@ -23,10 +21,7 @@ class DataDisplay extends Component {
     return (
       <div id="data-display" style={styles.panel}>
         <Statement />
-        <div
-          style={styles.tableParent}
-          onScroll={() => setCurrentColumn(undefined)}
-        >
+        <div style={styles.tableParent}>
           <DataTable />
         </div>
         <div style={styles.footerText}>
@@ -40,10 +35,5 @@ class DataDisplay extends Component {
 export default connect(
   state => ({
     data: state.data
-  }),
-  dispatch => ({
-    setCurrentColumn(column) {
-      dispatch(setCurrentColumn(column));
-    }
   })
 )(DataDisplay);
