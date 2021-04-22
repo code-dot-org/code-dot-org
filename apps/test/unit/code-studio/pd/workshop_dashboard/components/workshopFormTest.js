80787bf91f5d2f8413b05d0784d7bd361c3745ac
Tim Plummer <tim@code.org>
2021-04-22 12:09:11 -0700

    roll back tests also

--- apps/test/unit/code-studio/pd/workshop_dashboard/components/workshopFormTest.js
+++ apps/test/unit/code-studio/pd/workshop_dashboard/components/workshopFormTest.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {assert, expect} from 'chai';
+import {assert} from 'chai';
 import {Factory} from 'rosie';
-import {FormControl, FormGroup} from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap';
 import Permission from '@cdo/apps/code-studio/pd/workshop_dashboard/permission';
 import {WorkshopForm} from '@cdo/apps/code-studio/pd/workshop_dashboard/components/workshop_form';
 import '../workshopFactory';
@@ -28,41 +28,5 @@ describe('WorkshopForm test', () => {
 
     const someControl = wrapper.find(FormControl);
     assert(someControl.exists());
-  });
-
-  it('displays "Location" for a non-virtual workshop', () => {
-    fakeWorkshop.virtual = false;
-    const wrapper = shallow(
-      <WorkshopForm
-        permission={new Permission()}
-        facilitatorCourses={[]}
-        workshop={fakeWorkshop}
-        onSaved={() => {}}
-        readOnly={false}
-      />,
-      {context: {router: {}}}
-    );
-
-    const locationGroup = wrapper.find('#location_name').closest(FormGroup);
-    assert(locationGroup.exists());
-    expect(locationGroup.html()).to.include('Location Name');
-  });
-
-  it('displays "Link to join" for a virtual workshop', () => {
-    fakeWorkshop.virtual = true;
-    const wrapper = shallow(
-      <WorkshopForm
-        permission={new Permission()}
-        facilitatorCourses={[]}
-        workshop={fakeWorkshop}
-        onSaved={() => {}}
-        readOnly={false}
-      />,
-      {context: {router: {}}}
-    );
-
-    const locationGroup = wrapper.find('#location_name').closest(FormGroup);
-    assert(locationGroup.exists());
-    expect(locationGroup.html()).to.include('Link to Join');
   });
 });
