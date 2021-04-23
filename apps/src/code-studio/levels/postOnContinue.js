3d9cefbd0a1c489a0d115ed2cb21f82720e4541d
James Chen <james@code.org>
2021-04-23 10:10:29 -0700

    Post milestone request when Continue is clicked for StandaloneVideo, etc.

--- apps/src/code-studio/levels/postOnContinue.js
+++ apps/src/code-studio/levels/postOnContinue.js
@@ -1,8 +1,11 @@
 /* global appOptions */
 
 /**
- * A set of helpers used for cases where we want to make our milestone post on
- * page load rather than on clicking continue
+ * A set of helpers used for cases where we want to make our milestone post
+ * when clicking 'Continue'.
+ *
+ * Note: There is similar (but more complex) functionality in levels/_dialog.js.
+ * Consider merging the two implementations at some point.
  */
 
 import {getResult} from '@cdo/apps/code-studio/levels/codeStudioLevels';
@@ -16,7 +19,7 @@ import {TestResults} from '@cdo/apps/constants';
  * Make our milestone post with some simple configuration. Note, this assumes
  * the level is a type that will always post "success".
  */
-export function postMilestoneForPageLoad() {
+export function onContinue() {
   // In cases where this is used, we register the default getResult function, so
   // this will just be { response: 'ok', result: true }
   const result = getResult();
@@ -38,23 +41,16 @@ export function postMilestoneForPageLoad() {
     level: appOptions.dialog.level,
     result: result.result,
     pass: true,
-    testResult: TestResults.ALL_PASS
+    testResult: TestResults.ALL_PASS,
+    onComplete: function() {
+      const lastServerResponse = getLastServerResponse();
+      let url = lastServerResponse && lastServerResponse.nextRedirect;
+      if (!url) {
+        const fallback = JSON.parse(appOptions.dialog.fallbackResponse);
+        url = fallback.success.redirect;
+      }
+
+      window.location.href = url;
+    }
   });
-}
-
-/**
- * Code to run when clicking continue on a page where we make our milestone
- * post on page load. In this case, if we heard back we want to check the response
- * for the next page to go to. If we didn't hear back, just use the fallback.
- * In practice, these will be identical.
- */
-export function onContinue() {
-  const lastServerResponse = getLastServerResponse();
-  let url = lastServerResponse && lastServerResponse.nextRedirect;
-  if (!url) {
-    const fallback = JSON.parse(appOptions.dialog.fallbackResponse);
-    url = fallback.success.redirect;
-  }
-
-  window.location.href = url;
 }
