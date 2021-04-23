09834b3fbf32d3072f9893540e47547290f7eb10
Will Jordan <will@code.org>
2021-04-23 11:40:22 -0700

    Filter query-string based on http-cache configuration setting Improves cacheability on pages that don't use query-string for server-side rendering.

--- cookbooks/cdo-varnish/libraries/helpers.rb
+++ cookbooks/cdo-varnish/libraries/helpers.rb
@@ -156,6 +156,9 @@ def process_request(behavior, _)
       cookies.map {|c| extract_cookie(c)}.join + "cookie.filter_except(\"#{cookies.join(',')}\");"
     end
   )
+  if behavior[:query] == false
+    out << "\n" + 'set req.url = regsub(req.url, "\?.*$", "");'
+  end
   REMOVED_HEADERS.each do |remove_header|
     name, value = remove_header.split ':'
     next if behavior[:headers].include? name
