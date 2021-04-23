19a3e5b917607d86e1b9030c08c16f122fd12f68
anjali <anjali@code.org>
2021-04-23 11:40:46 -0700

    unit tests

--- apps/test/unit/p5lab/spritelab/behaviorCommandsTest.js
+++ apps/test/unit/p5lab/spritelab/behaviorCommandsTest.js
@@ -1,4 +1,5 @@
 /* global p5 */
+import sinon from 'sinon';
 import {expect} from '../../../util/reconfiguredChai';
 import * as coreLibrary from '@cdo/apps/p5lab/spritelab/coreLibrary';
 import {commands as actionCommands} from '@cdo/apps/p5lab/spritelab/commands/actionCommands';
@@ -16,6 +17,91 @@ describe('Behavior Commands', () => {
     let sheet = new p5Wrapper.p5.SpriteSheet(image, frames);
     animation = new p5Wrapper.p5.Animation(sheet);
     coreLibrary.reset();
+  });
+
+  describe('draggableFunc', () => {
+    let mousePressedOverStub, mouseWentDownStub, mouseWentUpStub;
+    beforeEach(() => {
+      mousePressedOverStub = sinon.stub(p5Wrapper.p5, 'mousePressedOver');
+      mouseWentDownStub = sinon.stub(p5Wrapper.p5, 'mouseWentDown');
+      mouseWentUpStub = sinon.stub(p5Wrapper.p5, 'mouseWentUp');
+    });
+
+    afterEach(() => {
+      mousePressedOverStub.restore();
+      mouseWentDownStub.restore();
+      mouseWentUpStub.restore();
+    });
+
+    it('can drag a sprite', () => {
+      makeSprite({name: 'sprite', location: {x: 200, y: 200}});
+      const sprite = coreLibrary.getSpriteArray({name: 'sprite'})[0];
+      mousePressedOverStub.returns(true);
+      mouseWentDownStub.returns(true);
+      behaviorCommands.draggableFunc(p5Wrapper.p5)({name: 'sprite'});
+      expect(sprite.dragging).to.be.true;
+    });
+
+    it('dragging moves the sprite to the mouse position', () => {
+      makeSprite({name: 'sprite', location: {x: 200, y: 200}});
+      const sprite = coreLibrary.getSpriteArray({name: 'sprite'})[0];
+      p5Wrapper.p5.mouseX = 200;
+      p5Wrapper.p5.mouseY = 200;
+      // First tick, press down on the sprite to start the drag.
+      mousePressedOverStub.returns(true);
+      mouseWentDownStub.returns(true);
+      behaviorCommands.draggableFunc(p5Wrapper.p5)({name: 'sprite'});
+      expect(sprite.dragging).to.be.true;
+
+      // Next tick, sprite moves to new mouse position.
+      mouseWentDownStub.returns(false);
+      p5Wrapper.p5.mouseX = 100;
+      p5Wrapper.p5.mouseY = 100;
+      behaviorCommands.draggableFunc(p5Wrapper.p5)({name: 'sprite'});
+
+      expect(sprite.x).to.equal(100);
+      expect(sprite.y).to.equal(100);
+    });
+
+    it('drag ends when mouse goes up', () => {
+      makeSprite({name: 'sprite', location: {x: 200, y: 200}});
+      const sprite = coreLibrary.getSpriteArray({name: 'sprite'})[0];
+      p5Wrapper.p5.mouseX = 200;
+      p5Wrapper.p5.mouseY = 200;
+      // First tick, press down on the sprite to start the drag.
+      mousePressedOverStub.returns(true);
+      mouseWentDownStub.returns(true);
+      behaviorCommands.draggableFunc(p5Wrapper.p5)({name: 'sprite'});
+      expect(sprite.dragging).to.be.true;
+
+      // Next tick, mouse went up, so sprite moves to new mouse position
+      // and drag ends.
+      mouseWentDownStub.returns(false);
+      mouseWentUpStub.returns(true);
+      p5Wrapper.p5.mouseX = 100;
+      p5Wrapper.p5.mouseY = 100;
+      behaviorCommands.draggableFunc(p5Wrapper.p5)({name: 'sprite'});
+      expect(sprite.x).to.equal(100);
+      expect(sprite.y).to.equal(100);
+      expect(sprite.dragging).to.be.false;
+    });
+
+    it('only drags the top sprite', () => {
+      makeSprite({name: 'bottom', location: {x: 200, y: 200}});
+      const bottom = coreLibrary.getSpriteArray({name: 'bottom'})[0];
+
+      makeSprite({name: 'top', location: {x: 200, y: 200}});
+      const top = coreLibrary.getSpriteArray({name: 'top'})[0];
+
+      mousePressedOverStub.returns(true);
+      mouseWentDownStub.returns(true);
+
+      behaviorCommands.draggableFunc(p5Wrapper.p5)({name: 'top'});
+      expect(top.dragging).to.be.true;
+
+      behaviorCommands.draggableFunc(p5Wrapper.p5)({name: 'bottom'});
+      expect(!!bottom.dragging).to.be.false;
+    });
   });
 
   describe('followingTargetsFunc', () => {
