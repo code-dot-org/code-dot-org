e0aadfbd97a55ccb13db719006b8748061ad76ba
Charlie Forkish <charlie@code.org>
2021-04-28 12:59:09 -0700

    minor cleanup, fix ProgressTableLevelBubble tests, add CachedElement tests

--- apps/test/unit/templates/sectionProgress/ProgressTableLevelBubbleTest.jsx
+++ apps/test/unit/templates/sectionProgress/ProgressTableLevelBubbleTest.jsx
@@ -1,24 +1,24 @@
 import {expect} from '../../../util/reconfiguredChai';
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-import ProgressTableLevelBubble, {
-  unitTestExports
-} from '@cdo/apps/templates/sectionProgress/progressTables/ProgressTableLevelBubble';
+import ProgressTableLevelBubble from '@cdo/apps/templates/sectionProgress/progressTables/ProgressTableLevelBubble';
 import color from '@cdo/apps/util/color';
 import FontAwesome from '@cdo/apps/templates/FontAwesome';
 import {LevelStatus, LevelKind} from '@cdo/apps/util/sharedConstants';
+import {unitTestExports as cacheExports} from '@cdo/apps/util/CachedElement';
 import i18n from '@cdo/locale';
+import sinon from 'sinon';
 import {
   BubbleSize,
   BubbleShape
 } from '@cdo/apps/templates/progress/progressStyles';
+import {LinkWrapper} from '@cdo/apps/templates/progress/BubbleFactory';
 
 const TITLE = '1';
 
 const defaultProps = {
   levelStatus: LevelStatus.not_tried,
   levelKind: LevelKind.level,
-  isDisabled: false,
   title: TITLE,
   url: '/foo/bar'
 };
@@ -51,129 +51,125 @@ const assessmentBackgrounds = {
 };
 
 /**
- * Helper function to retrieve the style object of a rendered bubble with the
- * provided status and prpos.
+ * ProgressTableLevelBubble's primary purpose is to determine the shape, size,
+ * style, and cache key for an underlying BasicBubble. Consequently many of our
+ * tests need to verify porperties of the underlying BasicBubble. However,
+ * since what the component actually renders is the cached HTML representation
+ * of the BasicBubble, we use a spy to intercept the BasicBubble returned from
+ * ProgressTableLevelBubble.createBubbleElement for easier verification.
  */
-function bubbleContainerStyleForStatus(status, propOverrides = {}) {
-  const wrapper = mount(
-    <ProgressTableLevelBubble
-      {...defaultProps}
-      {...propOverrides}
-      levelStatus={status}
-    />
-  );
+const elementSpy = sinon.spy(
+  ProgressTableLevelBubble.prototype,
+  'createBubbleElement'
+);
 
-  return wrapper
-    .find(unitTestExports.BasicBubble)
-    .at(0)
-    .childAt(0)
-    .props().style;
+/**
+ * The spy's history is reset after each test so this will give us the first
+ * BasicBubble rendered during this test.
+ */
+function getFirstRenderedBasicBubble(propOverrides = {}) {
+  mount(<ProgressTableLevelBubble {...defaultProps} {...propOverrides} />);
+  const renderedBubble = elementSpy.returnValues[0];
+  return mount(renderedBubble);
+}
+
+/**
+ * Helper function to retrieve the style object of a rendered bubble with the
+ * provided status and props.
+ */
+function basicBubbleStyleForStatus(status, propOverrides = {}) {
+  const wrapper = getFirstRenderedBasicBubble({
+    ...propOverrides,
+    levelStatus: status
+  });
+  return wrapper.childAt(0).props().style;
+}
+
+function getCacheSize() {
+  return Object.keys(cacheExports.elementsHtmlCache['BasicBubble']).length;
 }
 
 describe('ProgressTableLevelBubble', () => {
+  afterEach(() => {
+    elementSpy.resetHistory();
+    cacheExports.clearElementsCache();
+  });
+
   it('renders a link', () => {
     const wrapper = shallow(<ProgressTableLevelBubble {...defaultProps} />);
-    expect(wrapper.find(unitTestExports.LinkWrapper)).to.have.lengthOf(1);
+    expect(wrapper.find(LinkWrapper)).to.have.lengthOf(1);
   });
 
   it('renders default bubble with circle shape', () => {
-    const wrapper = mount(<ProgressTableLevelBubble {...defaultProps} />);
-    expect(wrapper.find(unitTestExports.BasicBubble).props().shape).to.equal(
-      BubbleShape.circle
-    );
+    const wrapper = getFirstRenderedBasicBubble();
+    expect(wrapper.props().shape).to.equal(BubbleShape.circle);
   });
 
   it('renders concept bubble with diamond shape', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble {...defaultProps} isConcept={true} />
-    );
-    expect(wrapper.find(unitTestExports.BasicBubble).props().shape).to.equal(
-      BubbleShape.diamond
-    );
+    const wrapper = getFirstRenderedBasicBubble({isConcept: true});
+    expect(wrapper.props().shape).to.equal(BubbleShape.diamond);
   });
 
   it('renders unplugged bubble with pill shape', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble {...defaultProps} isUnplugged={true} />
-    );
-    expect(wrapper.find(unitTestExports.BasicBubble).props().shape).to.equal(
-      BubbleShape.pill
-    );
+    const wrapper = getFirstRenderedBasicBubble({isUnplugged: true});
+    expect(wrapper.props().shape).to.equal(BubbleShape.pill);
   });
 
   it('shows correct text in unplugged bubble', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble {...defaultProps} isUnplugged={true} />
-    );
-    expect(wrapper.find(unitTestExports.BasicBubble).text()).to.equal(
-      i18n.unpluggedActivity()
-    );
+    const wrapper = getFirstRenderedBasicBubble({isUnplugged: true});
+    expect(wrapper.text()).to.equal(i18n.unpluggedActivity());
   });
 
   it('shows title in normal bubble', () => {
-    const wrapper = mount(<ProgressTableLevelBubble {...defaultProps} />);
-    expect(wrapper.find(unitTestExports.BasicBubble).text()).to.equal(TITLE);
+    const wrapper = getFirstRenderedBasicBubble();
+    expect(wrapper.text()).to.equal(TITLE);
   });
 
   it('shows title in concept bubble', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble {...defaultProps} concept={true} />
-    );
-    expect(wrapper.find(unitTestExports.BasicBubble).text()).to.equal(TITLE);
+    const wrapper = getFirstRenderedBasicBubble({isConcept: true});
+    expect(wrapper.text()).to.equal(TITLE);
   });
 
   it('shows title in letter bubble', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble
-        {...defaultProps}
-        bubbleSize={BubbleSize.letter}
-      />
-    );
-    expect(wrapper.find(unitTestExports.BasicBubble).text()).to.equal(TITLE);
+    const wrapper = getFirstRenderedBasicBubble({
+      bubbleSize: BubbleSize.letter
+    });
+    expect(wrapper.text()).to.equal(TITLE);
   });
 
   it('does not show title in dot bubble', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble {...defaultProps} bubbleSize={BubbleSize.dot} />
-    );
-    expect(wrapper.find(unitTestExports.BasicBubble).text()).to.equal('');
+    const wrapper = getFirstRenderedBasicBubble({bubbleSize: BubbleSize.dot});
+    expect(wrapper.text()).to.equal('');
   });
 
   it('shows correct icon when locked', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble {...defaultProps} isLocked={true} />
-    );
+    const wrapper = getFirstRenderedBasicBubble({
+      isLocked: true
+    });
     const icon = wrapper.find(FontAwesome);
     expect(icon).to.have.lengthOf(1);
     expect(icon.at(0).props().icon).to.equal('lock');
   });
 
   it('shows correct icon for bonus', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble {...defaultProps} isBonus={true} />
-    );
+    const wrapper = getFirstRenderedBasicBubble({isBonus: true});
     const icon = wrapper.find(FontAwesome);
     expect(icon).to.have.lengthOf(1);
     expect(icon.at(0).props().icon).to.equal('flag-checkered');
   });
 
   it('shows correct icon for paired', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble {...defaultProps} isPaired={true} />
-    );
+    const wrapper = getFirstRenderedBasicBubble({isPaired: true});
     const icon = wrapper.find(FontAwesome);
     expect(icon).to.have.lengthOf(1);
     expect(icon.at(0).props().icon).to.equal('users');
   });
 
   it('only shows paired icon for bonus + paired', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble
-        {...defaultProps}
-        isBonus={true}
-        isPaired={true}
-      />
-    );
+    const wrapper = getFirstRenderedBasicBubble({
+      isBonus: true,
+      isPaired: true
+    });
     const icon = wrapper.find(FontAwesome);
     expect(icon).to.have.lengthOf(1);
     expect(icon.at(0).props().icon).to.equal('users');
@@ -181,21 +177,21 @@ describe('ProgressTableLevelBubble', () => {
 
   Object.keys(borderColors).forEach(status => {
     it(`shows correct border color for status ${status} - not assessment`, () => {
-      const style = bubbleContainerStyleForStatus(status);
+      const style = basicBubbleStyleForStatus(status);
       expect(style.borderColor).to.equal(borderColors[status]);
     });
   });
 
   Object.keys(backgroundColors).forEach(status => {
     it(`shows correct background color for status ${status} - not assessment`, () => {
-      const style = bubbleContainerStyleForStatus(status);
+      const style = basicBubbleStyleForStatus(status);
       expect(style.backgroundColor).to.equal(backgroundColors[status]);
     });
   });
 
   Object.keys(assessmentBorders).forEach(status => {
     it(`shows correct border color for status ${status} - assessment`, () => {
-      const style = bubbleContainerStyleForStatus(status, {
+      const style = basicBubbleStyleForStatus(status, {
         levelKind: LevelKind.assessment
       });
       expect(style.borderColor).to.equal(assessmentBorders[status]);
@@ -204,17 +200,55 @@ describe('ProgressTableLevelBubble', () => {
 
   Object.keys(assessmentBackgrounds).forEach(status => {
     it(`shows correct background color for status ${status} - assessment`, () => {
-      const style = bubbleContainerStyleForStatus(status, {
+      const style = basicBubbleStyleForStatus(status, {
         levelKind: LevelKind.assessment
       });
       expect(style.backgroundColor).to.equal(assessmentBackgrounds[status]);
     });
   });
 
-  it('renders raw HTML if useCache is true', () => {
-    const wrapper = mount(
-      <ProgressTableLevelBubble {...defaultProps} useCache={true} />
-    );
-    expect(wrapper.find('div').props().dangerouslySetInnerHTML).to.exist;
+  describe('caching', () => {
+    it('renders raw HTML instead of component tree', () => {
+      const wrapper = mount(<ProgressTableLevelBubble {...defaultProps} />);
+      expect(wrapper.find('div').props().dangerouslySetInnerHTML).to.exist;
+    });
+
+    it('only caches one element when rendering two identical bubbles', () => {
+      mount(<ProgressTableLevelBubble {...defaultProps} />);
+      mount(<ProgressTableLevelBubble {...defaultProps} />);
+      expect(elementSpy.calledOnce).to.be.true;
+      expect(getCacheSize()).to.equal(1);
+    });
+
+    it('caches two elements when rendering two different bubbles', () => {
+      mount(<ProgressTableLevelBubble {...defaultProps} />);
+      mount(<ProgressTableLevelBubble {...defaultProps} isUnplugged={true} />);
+      expect(elementSpy.calledTwice).to.be.true;
+      expect(getCacheSize()).to.equal(2);
+    });
+
+    it('renders the same cached html for different bubbles that look the same', () => {
+      // paired icon overrides title so these two should appear the same
+      const wrapperA = mount(
+        <ProgressTableLevelBubble {...defaultProps} isPaired={true} />
+      );
+      const wrapperB = mount(
+        <ProgressTableLevelBubble {...defaultProps} title="2" isPaired={true} />
+      );
+      expect(elementSpy.calledOnce).to.be.true;
+      expect(getCacheSize()).to.equal(1);
+      expect(
+        wrapperA.find('div').props().dangerouslySetInnerHTML.__html
+      ).to.equal(wrapperB.find('div').props().dangerouslySetInnerHTML.__html);
+    });
+
+    it('only caches one element when rendering same bubbles with different urls', () => {
+      mount(<ProgressTableLevelBubble {...defaultProps} />);
+      mount(
+        <ProgressTableLevelBubble {...defaultProps} url={'/foo/bar/baz'} />
+      );
+      expect(elementSpy.calledOnce).to.be.true;
+      expect(getCacheSize()).to.equal(1);
+    });
   });
 });
