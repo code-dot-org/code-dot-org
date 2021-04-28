6e4878c43af00216ca4cfedaeaa349209e4f04f7
Erin Peach <erin@code.org>
2021-04-28 13:51:54 -0700

    PR feedback

--- apps/src/code-studio/assets/SpriteUpload.js
+++ apps/src/code-studio/assets/SpriteUpload.js
@@ -1,40 +1,39 @@
 import React from 'react';
 
 export default class SpriteUpload extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      fileData: null,
-      filePreviewURL: '',
-      filename: '',
-      category: ''
-    };
-  }
+  state = {
+    fileData: null,
+    filePreviewURL: '',
+    filename: '',
+    category: ''
+  };
 
   handleSubmit = event => {
     event.preventDefault();
-    let xhr = new XMLHttpRequest();
 
-    const onError = function() {
-      console.log(new Error(`${xhr.status} ${xhr.statusText}`));
-    };
-
-    const onSuccess = function() {
-      if (xhr.status >= 400) {
-        onError();
-        return;
-      }
-    };
-
-    xhr.addEventListener('load', onSuccess);
-    xhr.addEventListener('error', onError);
-    xhr.open(
-      'POST',
+    return fetch(
       `/api/v1/animation-library/level_animations/${this.state.filename}`,
-      true
-    );
-    xhr.setRequestHeader('Content-type', 'image/png');
-    xhr.send(this.state.fileData);
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'image/png'
+        },
+        body: this.state.fileData
+      }
+    )
+      .then(response => {
+        let responseText = document.getElementById('sprite-upload-error');
+        if (response.ok) {
+          responseText.innerText = 'Image succesfully uploaded.';
+        } else {
+          throw Error(response.status + ': ' + response.statusText);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        let errorText = document.getElementById('sprite-upload-error');
+        errorText.innerText = err;
+      });
   };
 
   handleImageChange = event => {
@@ -58,7 +57,7 @@ export default class SpriteUpload extends React.Component {
         <h1>Sprite Upload</h1>
         <form onSubmit={this.handleSubmit}>
           <label>
-            <h4> Sprite Category: </h4>
+            <h4> Sprite Category:</h4>
             <input
               type="text"
               value={this.state.category}
@@ -66,7 +65,7 @@ export default class SpriteUpload extends React.Component {
             />
           </label>
           <label>
-            <h4> Select Sprite to Add to Library: </h4>
+            <h4> Select Sprite to Add to Library:</h4>
             <input
               type="file"
               accept="image/png"
@@ -76,11 +75,12 @@ export default class SpriteUpload extends React.Component {
           </label>
           <br />
           <label>
-            <h4> Image Preview: </h4>
+            <h4> Image Preview:</h4>
             <img src={this.state.filePreviewURL} />
           </label>
           <br />
           <button type="submit">Upload to Library</button>
+          <p id="sprite-upload-error" />
         </form>
       </div>
     );
