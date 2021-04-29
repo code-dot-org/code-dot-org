dfb36333607024046933c2357141a13a5670f49c
Ben Brooks <ben@code.org>
2021-04-29 12:11:19 -0700

    Make path column not null

--- dashboard/db/migrate/20210429183952_create_foorm_simple_survey_forms.rb
+++ dashboard/db/migrate/20210429183952_create_foorm_simple_survey_forms.rb
@@ -1,7 +1,7 @@
 class CreateFoormSimpleSurveyForms < ActiveRecord::Migration[5.2]
   def change
     create_table :foorm_simple_survey_forms do |t|
-      t.string  :path, index: {unique: true}
+      t.string  :path, index: {unique: true}, null: false
       t.string  :form_name, null: false
       t.integer :form_version
       t.text    :properties
