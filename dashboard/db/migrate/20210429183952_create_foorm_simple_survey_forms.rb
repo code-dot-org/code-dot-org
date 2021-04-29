84fe71375386854c0d12ffd562eef1a679cd179d
Ben Brooks <ben@code.org>
2021-04-29 12:01:25 -0700

    Create new model to store simple/miscellaneous survey configuration

---
+++ dashboard/db/migrate/20210429183952_create_foorm_simple_survey_forms.rb
@@ -0,0 +1,12 @@
+class CreateFoormSimpleSurveyForms < ActiveRecord::Migration[5.2]
+  def change
+    create_table :foorm_simple_survey_forms do |t|
+      t.string  :path, index: {unique: true}
+      t.string  :form_name, null: false
+      t.integer :form_version
+      t.text    :properties
+
+      t.timestamps
+    end
+  end
+end
