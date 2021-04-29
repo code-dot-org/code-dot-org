bc012847ffe4734b99c2c0f24315ecd3701fb7b2
Ben Brooks <ben@code.org>
2021-04-29 14:23:41 -0700

    Update controller actions and routes to simple survey

--- dashboard/app/controllers/foorm/simple_survey_controller.rb
+++ dashboard/app/controllers/foorm/simple_survey_controller.rb
@@ -1,24 +1,24 @@
 module Foorm
-  class MiscSurveyController < ApplicationController
-    MISC_FOORM_SUBMIT_API = '/dashboardapi/v1/foorm/misc_survey_submission'
+  class SimpleSurveyController < ApplicationController
+    FOORM_SIMPLE_SURVEY_SUBMIT_API = '/dashboardapi/v1/foorm/simple_survey_submission'
 
     # General misc survey.
     # GET '/form/:misc_form_path'
-    # misc_form_path references the configuration in foorm/misc_survey, which maps a
+    # misc_form_path references the configuration in foorm/simple_survey_submissions, which maps a
     # form path to a set of survey variables and a form name.
     # Survey variables can be provided in the following format:
     # '/form/:misc_form_path?survey_data[variable1]=value1&survey_data[variable2]=value2'
-    # If no variables are provided, will use survey variables from the configuration in foorm/misc_survey,
+    # If no variables are provided, will use survey variables from the configuration in foorm/simple_survey_submissions,
     # or use no variables if there are none in the configuration.
     def new
       return render :logged_out unless current_user
       return render :not_teacher unless current_user.teacher?
       return render :no_teacher_email unless current_user.email.present?
 
-      form_data = MiscSurvey.find_form_data(params[:misc_form_path])
+      form_data = SimpleSurveySubmission.find_form_data(params[:misc_form_path])
       return render_404 if !form_data || !form_data[:form_name]
 
-      return render :survey_closed if MiscSurvey.form_disabled?(params[:misc_form_path])
+      return render :survey_closed if SimpleSurveySubmission.form_disabled?(params[:misc_form_path])
 
       form_questions, latest_version = ::Foorm::Form.get_questions_and_latest_version_for_name(form_data[:form_name])
       return render_404 unless form_questions
@@ -39,7 +39,7 @@ module Foorm
           formName: form_data[:form_name],
           formVersion: latest_version,
           surveyData: params[:survey_data] || form_data[:survey_data],
-          submitApi: MISC_FOORM_SUBMIT_API,
+          submitApi: FOORM_SIMPLE_SURVEY_SUBMIT_API,
           submitParams: key_params
         }.to_json
       }
@@ -53,8 +53,8 @@ module Foorm
     def show
       return render json: {}, status: :no_content unless current_user&.teacher? && current_user.email.present?
 
-      form_data = MiscSurvey.find_form_data(params[:misc_form_path])
-      return render json: {}, status: :no_content if !form_data || !form_data[:form_name] || MiscSurvey.form_disabled?(params[:misc_form_path])
+      form_data = SimpleSurveySubmission.find_form_data(params[:misc_form_path])
+      return render json: {}, status: :no_content if !form_data || !form_data[:form_name] || SimpleSurveySubmission.form_disabled?(params[:misc_form_path])
 
       form_questions, latest_version = ::Foorm::Form.get_questions_and_latest_version_for_name(form_data[:form_name])
       key_params = {
@@ -69,7 +69,7 @@ module Foorm
           formName: form_data[:form_name],
           formVersion: latest_version,
           surveyData: params[:survey_data] || form_data[:survey_data],
-          submitApi: MISC_FOORM_SUBMIT_API,
+          submitApi: FOORM_SIMPLE_SURVEY_SUBMIT_API,
           submitParams: key_params
         }.to_json
       }
@@ -78,7 +78,7 @@ module Foorm
     protected
 
     def response_exists?(key_params)
-      MiscSurvey.exists?(
+      SimpleSurveySubmission.exists?(
         user_id: key_params[:user_id],
         misc_form_path: key_params[:misc_form_path]
       )
