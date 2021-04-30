511dc18c367efb98e1f44f126f756f7c547d755f
Elisa <bilingualedu@gmail.com>
2021-04-30 07:27:57 -0400

    Fix: formate auto changes to align with style guide

--- frontend/packages/labs/ailab/test/unit/redux.test.js
+++ frontend/packages/labs/ailab/test/unit/redux.test.js
@@ -1,67 +1,167 @@
-import { getOptionFrequenciesByColumn } from "../../src/redux.js";
+import {
+  getRange,
+  getAccuracyRegression,
+  getAccuracyClassification
+} from '../../src/redux.js';
 
-const initialState = {
+import { ResultsGrades } from '../../src/constants.js';
+
+const resultsState = {
   data: [
     {
-      name: "Plain M&Ms",
-      chocolate: "yes",
-      nuts: "no",
-      fruity: "no",
-      delicious: "yes"
+      sun: 'high',
+      height: 3.8
     },
     {
-      name: "Peanut M&Ms",
-      chocolate: "yes",
-      nuts: "yes",
-      fruity: "no",
-      delicious: "yes"
+      sun: 'high',
+      height: 3.9
     },
     {
-      name: "Snickers",
-      chocolate: "yes",
-      nuts: "yes",
-      fruity: "no",
-      delicious: "yes"
+      sun: 'medium',
+      height: 2.6
     },
     {
-      name: "Almond Joy",
-      chocolate: "yes",
-      nuts: "yes",
-      fruity: "no",
-      delicious: "yes"
+      sun: 'medium',
+      height: 2.5
     },
     {
-      name: "Black Licorice",
-      chocolate: "no",
-      nuts: "no",
-      fruity: "no",
-      delicious: "no"
+      sun: 'low',
+      height: 0.9
     },
     {
-      name: "Skittles",
-      chocolate: "no",
-      nuts: "no",
-      fruity: "yes",
-      delicious: "yes"
+      sun: 'low',
+      height: 1.6
     }
   ],
-  labelColumn: "delicious",
-  selectedFeatures: ["chocolate", "nuts"],
-  columnsByDataType: {
-    chocolate: "categorical",
-    nuts: "categorical",
-    delicious: "categorical"
-  }
+  labelColumn: 'height',
+  labelColumnCategorical: 'sun',
+  accuracyCheckPredictedLabels: [4.0, 3.75, 2.63, 2.46, 1.6, 1.0],
+  accuracyCheckLabels: [3.9, 3.8, 2.6, 2.5, 1.6, 0.9]
 };
 
-describe("redux functions", () => {
-  test("getOptionFrequenciesByColumn", async () => {
-    const frequencies = getOptionFrequenciesByColumn(initialState);
-    expect(frequencies["chocolate"]["yes"]).toBe(4);
-    expect(frequencies["chocolate"]["no"]).toBe(2);
-    expect(frequencies["nuts"]["yes"]).toBe(3);
-    expect(frequencies["nuts"]["no"]).toBe(3);
-    expect(frequencies["delicious"]["yes"]).toBe(5);
-    expect(frequencies["delicious"]["no"]).toBe(1);
+const resultsStateClassification = {
+  data: [
+    {
+      weather: 'sunny',
+      temp: 'hot',
+      play: 'no'
+    },
+    {
+      weather: 'overcast',
+      temp: 'hot',
+      play: 'yes'
+    },
+    {
+      weather: 'overcast',
+      temp: 'mild',
+      play: 'yes'
+    },
+    {
+      weather: 'overcast',
+      temp: 'cool',
+      play: 'yes'
+    },
+    {
+      weather: 'rainy',
+      temp: 'mild',
+      play: 'yes'
+    },
+    {
+      weather: 'rainy',
+      temp: 'cool',
+      play: 'no'
+    }
+  ],
+  labelColumn: 'play',
+  selectedFeatures: ['temp', 'weather'],
+  columnsByDataType: {
+    weather: 'categorical',
+    temp: 'categorical',
+    play: 'categorical'
+  },
+  accuracyCheckLabels: ['no', 'yes', 'yes', 'yes', 'yes', 'no']
+};
+
+describe('redux functions', () => {
+  test('getAccuracyClassification - all accurate', async () => {
+    const accurateResults  = ['no', 'yes', 'yes', 'yes', 'yes', 'no']
+    resultsStateClassification['accuracyCheckPredictedLabels'] = accurateResults
+
+    const accuracy = getAccuracyClassification(resultsStateClassification);
+    expect(accuracy.grades).toEqual([
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT
+    ]);
+    expect(accuracy.percentCorrect).toBe('100.00');
+  });
+
+  test('getAccuracyClassification - mostly accurate', async () => {
+    const accurateResults = ['no','yes','yes','yes','yes','yes'];
+    resultsStateClassification['accuracyCheckPredictedLabels'] = accurateResults
+
+    const accuracy = getAccuracyClassification(resultsStateClassification);
+    expect(accuracy.grades).toEqual([
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.INCORRECT
+    ]);
+    expect(accuracy.percentCorrect).toBe('83.33');
+  });
+
+  test('getAccuracyClassification - not very accurate', async () => {
+    const accurateResults = ['yes','yes','yes','no','no','yes'];
+    resultsStateClassification['accuracyCheckPredictedLabels'] = accurateResults
+
+    const accuracy = getAccuracyClassification(resultsStateClassification);
+    expect(accuracy.grades).toEqual([
+      ResultsGrades.INCORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.INCORRECT,
+      ResultsGrades.INCORRECT,
+      ResultsGrades.INCORRECT
+    ]);
+    expect(accuracy.percentCorrect).toBe('33.33');
+  });
+
+  test('getAccuracyClassification - 0% accuracy', async () => {
+    const accurateResults = ['yes','no','no','no','no','yes'];
+    resultsStateClassification['accuracyCheckPredictedLabels'] = accurateResults
+
+    const accuracy = getAccuracyClassification(resultsStateClassification);
+    expect(accuracy.grades).toEqual([
+      ResultsGrades.INCORRECT,
+      ResultsGrades.INCORRECT,
+      ResultsGrades.INCORRECT,
+      ResultsGrades.INCORRECT,
+      ResultsGrades.INCORRECT,
+      ResultsGrades.INCORRECT
+    ]);
+    expect(accuracy.percentCorrect).toBe('0.00');
+  });
+
+  test('getAccuracyRegression', async () => {
+    const maxMin = getRange(resultsState, resultsState.labelColumn);
+    const range = Math.abs(maxMin.max - maxMin.min);
+    expect(range).toBe(3);
+    const accuracy = getAccuracyRegression(resultsState);
+    expect(accuracy.grades).toEqual([
+      ResultsGrades.INCORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.CORRECT,
+      ResultsGrades.INCORRECT
+    ]);
+    // error tolerance of +/- 0.09, 4/6 correct
+    expect(accuracy.percentCorrect).toBe('66.67');
   });
 });
+
