2e35c8b88e5f504ca2930d0767859065917a240e
Ben Brooks <ben@code.org>
2021-04-30 11:52:04 -0700

    Include kind column for iterations of same form

--- dashboard/db/migrate/20210429183952_create_foorm_simple_survey_forms.rb
+++ dashboard/db/migrate/20210429183952_create_foorm_simple_survey_forms.rb
@@ -1,7 +1,8 @@
 class CreateFoormSimpleSurveyForms < ActiveRecord::Migration[5.2]
   def change
     create_table :foorm_simple_survey_forms do |t|
-      t.string  :path, index: {unique: true}, null: false
+      t.string  :kind, index: {unique: true}, null: false
+      t.string  :path
       t.string  :form_name, null: false
       t.integer :form_version
       t.text    :properties
