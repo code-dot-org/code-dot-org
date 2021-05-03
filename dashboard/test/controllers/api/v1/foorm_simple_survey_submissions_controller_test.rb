20e6c17e2628eae593b27942c9752c792dde6f7c
Ben Brooks <ben@code.org>
2021-05-03 14:19:52 -0700

    Update tests

--- dashboard/test/controllers/api/v1/foorm_simple_survey_submissions_controller_test.rb
+++ dashboard/test/controllers/api/v1/foorm_simple_survey_submissions_controller_test.rb
@@ -5,23 +5,27 @@ class Api::V1::FoormSimpleSurveySubmissionsControllerTest < ::ActionController::
   setup do
     @user = create :user
     @foorm_form = create :foorm_form
-  end
 
-  test 'can create and save survey submission' do
-    params = {
+    @params = {
       answers: {
         question1: 'answer1'
       },
       user_id: @user.id,
       form_name: @foorm_form.name,
       form_version: @foorm_form.version,
-      misc_form_path: 'sample_path'
+      simple_survey_form_id: 1
     }
+  end
 
-    response = post :create, params: params
+  test 'can create and save survey submission' do
+    response = post :create, params: @params
+
     assert_response :created
+
     response_body = JSON.parse(response.body)
+
     assert_not_nil response_body['foorm_submission_id']
     assert_not_nil response_body['simple_survey_submission_id']
+    assert_equal 1, response_body['simple_survey_form_id']
   end
 end
