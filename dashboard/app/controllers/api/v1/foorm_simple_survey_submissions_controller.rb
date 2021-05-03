1e92e519b1664aa4bfb58e9a646236f1d6d20e59
Ben Brooks <ben@code.org>
2021-05-03 14:19:52 -0700

    Update tests

--- dashboard/app/controllers/api/v1/foorm_simple_survey_submissions_controller.rb
+++ dashboard/app/controllers/api/v1/foorm_simple_survey_submissions_controller.rb
@@ -13,6 +13,10 @@ class Api::V1::FoormSimpleSurveySubmissionsController < ApplicationController
       return
     end
 
-    render json: {foorm_submission_id: submission.foorm_submission_id, simple_survey_submission_id: submission.id}, status: :created
+    render json: {
+      foorm_submission_id: submission.foorm_submission_id,
+      simple_survey_submission_id: submission.id,
+      simple_survey_form_id: submission.simple_survey_form_id
+    }, status: :created
   end
 end
