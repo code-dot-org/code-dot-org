0ac71233bf1d3030ca05f2a6d0d032e6d7f6de24
Elijah Hamovitz <elijahhamovitz@gmail.com>
2021-05-06 13:07:39 -0700

    rearrange code to make things more testable, and fix up tests

--- dashboard/test/lib/services/curriculum_pdfs/resources_test.rb
+++ dashboard/test/lib/services/curriculum_pdfs/resources_test.rb
@@ -9,7 +9,13 @@ class Services::CurriculumPdfs::ResourcesTest < ActiveSupport::TestCase
   setup do
     PDF.stubs(:invoke_generation_script)
     PDF.stubs(:merge_local_pdfs)
-    Services::CurriculumPdfs.stubs(:export_from_google)
+
+    @fake_google_drive_file = mock
+    @fake_google_drive_file.stubs(:export_as_file)
+    @fake_google_drive_file.stubs(:download_to_file)
+    @fake_google_drive_file.stubs(:available_content_types).returns([])
+    @fake_google_drive_file.stubs(:id).returns("google-drive-file-id")
+    Services::CurriculumPdfs.stubs(:google_drive_file_by_url).returns(@fake_google_drive_file)
 
     # mock some file operations for the 'download PDF from url' case
     IO.stubs(:copy_stream)
@@ -41,17 +47,18 @@ class Services::CurriculumPdfs::ResourcesTest < ActiveSupport::TestCase
   end
 
   test 'resources that are on google docs or drive can be retrieved as PDFs' do
-    resource = create(:resource, url: "https://docs.google.com/example")
-    Dir.mktmpdir('curriculum_pdfs_script_overview_test') do |tmpdir|
-      path = File.join(tmpdir, "resource.fake_name.pdf")
-      Services::CurriculumPdfs.expects(:export_from_google).with("https://docs.google.com/example", path)
-      assert Services::CurriculumPdfs.fetch_resource_pdf(resource, tmpdir)
-    end
+    google_docs_resource = create(:resource, url: "https://docs.google.com/document/d/example")
+    Services::CurriculumPdfs.expects(:google_drive_file_by_url).with(google_docs_resource.url).returns(@fake_google_drive_file)
+    Services::CurriculumPdfs.fetch_resource_pdf(google_docs_resource)
+
+    google_drive_resource = create(:resource, url: "https://drive.google.com/example")
+    Services::CurriculumPdfs.expects(:google_drive_file_by_url).with(google_drive_resource.url).returns(@fake_google_drive_file)
+    Services::CurriculumPdfs.fetch_resource_pdf(google_drive_resource)
   end
 
   test 'resources that are google forms will be ignored' do
     resource = create(:resource, url: "https://docs.google.com/forms/d/1OKy2U3F37NSgCBUez9XMi0UJtlHKPXIe_rxy0l_KEOg/view")
-    Services::CurriculumPdfs.expects(:export_from_google).never
+    Services::CurriculumPdfs.expects(:google_drive_file_by_url).never
     assert_nil Services::CurriculumPdfs.fetch_resource_pdf(resource)
   end
 
@@ -63,31 +70,14 @@ class Services::CurriculumPdfs::ResourcesTest < ActiveSupport::TestCase
     end
   end
 
-  test 'export_from_google will download or export file, as appropriate' do
-    # setup
-    Services::CurriculumPdfs.unstub(:export_from_google)
-    fake_drive_session = mock
-    GoogleDrive::Session.stubs(:from_service_account_key).returns(fake_drive_session)
+  test 'fetch_url_to_path will download or export google file, as appropriate' do
+    # test exporting
+    @fake_google_drive_file.expects(:export_as_file).with("foo.pdf", "application/pdf")
+    Services::CurriculumPdfs.fetch_url_to_path("https://docs.google.com/document/d/", "foo.pdf")
 
     # test downloading
-    mock_pdf_file = OpenStruct.new(
-      available_content_types: ["application/pdf"],
-      download_to_file: true
-    )
-    fake_drive_session.stubs(:file_by_url).returns(mock_pdf_file)
-    mock_pdf_file.expects(:download_to_file).with("foo.pdf")
-    Services::CurriculumPdfs.export_from_google("", "foo.pdf")
-
-    # test exporting
-    mock_other_file = OpenStruct.new(
-      available_content_types: [],
-      id: "id"
-    )
-    fake_drive_session.stubs(:file_by_url).returns(mock_other_file)
-    URI.expects(:open).with("https://docs.google.com/document/d/id/export?format=pdf")
-    Services::CurriculumPdfs.export_from_google("", "foo.pdf")
-
-    # teardown
-    Services::CurriculumPdfs.stubs(:export_from_google)
+    @fake_google_drive_file.stubs(:available_content_types).returns(["application/pdf"])
+    @fake_google_drive_file.expects(:download_to_file).with("foo.pdf")
+    Services::CurriculumPdfs.fetch_url_to_path("https://drive.google.com/", "foo.pdf")
   end
 end
