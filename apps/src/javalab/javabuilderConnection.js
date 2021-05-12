0b713a8f3a2dd322769eea36ae5d7595e21ec368
Jessica Kulwik <jessica@code.org>
2021-05-12 23:12:48 +0000

    Updated to use stricter protocol between client and server

--- apps/src/javalab/javabuilderConnection.js
+++ apps/src/javalab/javabuilderConnection.js
@@ -67,8 +67,6 @@ export default class JavabuilderConnection {
           console.log(data);
           break;
       }
-      // debugger;
-      // this.onOutputMessage(event.data);
     };
 
     this.socket.onclose = event => {
