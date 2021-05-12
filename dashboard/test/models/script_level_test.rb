86721f77253f61587de15eb4e47fa27ad1084b21
David Bailey <davidsbailey@users.noreply.github.com>
2021-05-12 16:42:03 -0700

    prevent adding variant to non-custom level

--- dashboard/test/models/script_level_test.rb
+++ dashboard/test/models/script_level_test.rb
@@ -815,7 +815,7 @@ class ScriptLevelTest < ActiveSupport::TestCase
     assert_equal 'Validation failed: Script level activity_section.lesson does not match lesson', error.message
   end
 
-  test 'add_variant adds variant level within migrated script' do
+  test 'adds variant to custom level in migrated script' do
     Rails.application.config.stubs(:levelbuilder_mode).returns false
 
     script = create :script, is_migrated: true
@@ -840,6 +840,25 @@ class ScriptLevelTest < ActiveSupport::TestCase
       script_level.add_variant level3
     end
     assert_includes e.message, "expected 1 existing level"
+  end
+
+  # variants do not appear to work for non-custom levels
+  test 'cannot add variant to non custom level' do
+    Rails.application.config.stubs(:levelbuilder_mode).returns false
+
+    script = create :script, is_migrated: true
+    lesson_group = create :lesson_group, script: script
+    lesson = create :lesson, lesson_group: lesson_group, script: script
+    level1 = create :level
+    level2 = create :level
+    script_level = create :script_level, script: script, lesson: lesson, levels: [level1]
+    assert_equal level1, script_level.oldest_active_level
+    assert script_level.active?(level1)
+
+    e = assert_raises do
+      script_level.add_variant level2
+    end
+    assert_equal "cannot add variant to non-custom level", e.message
   end
 
   test 'cannot add variant to legacy script' do
