68a124aed865cf4517d918ffa39c391e7bdba3ad
Erin Bond <>
2021-05-12 14:01:12 -0400

    remove option to pull test data from the beginning of the dataset

--- frontend/packages/labs/ailab/src/constants.js
+++ frontend/packages/labs/ailab/src/constants.js
@@ -21,7 +21,6 @@ export const ResultsGrades = {
 export const TEST_DATA_PERCENTS = [0, 5, 10, 15, 20];
 
 export const TestDataLocations = {
-  BEGINNING: "beginning",
   END: "end",
   RANDOM: "random"
 };
