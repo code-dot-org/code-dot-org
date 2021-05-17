05fcbd9d29a375177581337e281a004303491f5b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2021-05-17 11:22:58 -0700

    move new 'tooltipifyVocabulary' helper into existing utils file, rather than adding a new one

--- apps/src/sites/studio/pages/scripts/show.js
+++ apps/src/sites/studio/pages/scripts/show.js
@@ -1,22 +1,22 @@
 import $ from 'jquery';
 
+import announcementsReducer, {
+  addAnnouncement
+} from '@cdo/apps/code-studio/announcementsRedux';
+import plcHeaderReducer, {
+  setPlcHeader
+} from '@cdo/apps/code-studio/plc/plcHeaderRedux';
+import {getStore} from '@cdo/apps/code-studio/redux';
+import {registerReducers} from '@cdo/apps/redux';
 import {renderCourseProgress} from '@cdo/apps/code-studio/progress';
+import {setCurrentUserId} from '@cdo/apps/templates/currentUserRedux';
 import {
   setVerified,
   setVerifiedResources
 } from '@cdo/apps/code-studio/verifiedTeacherRedux';
-import {getStore} from '@cdo/apps/code-studio/redux';
-import {registerReducers} from '@cdo/apps/redux';
-import {setCurrentUserId} from '@cdo/apps/templates/currentUserRedux';
-import plcHeaderReducer, {
-  setPlcHeader
-} from '@cdo/apps/code-studio/plc/plcHeaderRedux';
-import announcementsReducer, {
-  addAnnouncement
-} from '@cdo/apps/code-studio/announcementsRedux';
+import {tooltipifyVocabulary} from '@cdo/apps/utils';
 
 import locales, {setLocaleEnglishName} from '../../../../redux/localesRedux';
-import {tooltipifyVocabulary} from '../utils';
 
 $(document).ready(initPage);
 
