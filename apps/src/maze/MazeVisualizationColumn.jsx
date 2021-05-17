d4d5f5cf0e3098dfa0bc65135f45ea6a358c2d07
Jillian Khoo <jillian9012@gmail.com>
2021-05-17 18:19:25 +0000

    remove changes to maze outside of neighborhood

--- apps/src/maze/MazeVisualizationColumn.jsx
+++ apps/src/maze/MazeVisualizationColumn.jsx
@@ -14,34 +14,6 @@ const MazeVisualizationColumn = function(props) {
       <GameButtons showFinishButton={props.showFinishButton}>
         <StepButton showStepButton={props.showStepButton} />
         {props.showCollectorGemCounter && <CollectorGemCounter />}
-        {props.showSpeedSlider && (
-          <svg id="slider" version="1.1" width="150" height="50">
-            {/* Slow icon. */}
-            <clipPath id="slowClipPath">
-              <rect width="26" height="12" x="5" y="14" />
-            </clipPath>
-            <image
-              xlinkHref={props.iconPath}
-              height="42"
-              width="84"
-              x="-21"
-              y="-10"
-              clipPath="url(#slowClipPath)"
-            />
-            {/* Fast icon. */}
-            <clipPath id="fastClipPath">
-              <rect width="26" height="16" x="120" y="10" />
-            </clipPath>
-            <image
-              xlinkHref={props.iconPath}
-              height="42"
-              width="84"
-              x="120"
-              y="-11"
-              clipPath="url(#fastClipPath)"
-            />
-          </svg>
-        )}
       </GameButtons>
       {props.searchWord && <SpellingControls searchWord={props.searchWord} />}
       <BelowVisualization />
@@ -53,9 +25,7 @@ MazeVisualizationColumn.propTypes = {
   searchWord: PropTypes.string,
   showCollectorGemCounter: PropTypes.bool,
   showFinishButton: PropTypes.bool,
-  showStepButton: PropTypes.bool.isRequired,
-  showSpeedSlider: PropTypes.bool.isRequired,
-  iconPath: PropTypes.string.isRequired
+  showStepButton: PropTypes.bool.isRequired
 };
 
 export default MazeVisualizationColumn;
