5f9dc7128c815d24aaf54a0fe88b3b3196bb8db6
Dani <dani@code.org>
2021-05-21 09:49:56 -0400

    Take out changes to admin standards controller

--- dashboard/app/controllers/admin_standards_controller.rb
+++ dashboard/app/controllers/admin_standards_controller.rb
@@ -13,21 +13,21 @@ class AdminStandardsController < ApplicationController
   def import_standards
     script = Script.find_by_name(params["unit_name"])
 
-    code_studio_lessons = {}
+    code_studio_stages = {}
     script&.lessons&.each do |lesson|
-      code_studio_lessons[lesson.localized_name] = lesson
+      code_studio_stages[lesson.localized_name] = lesson
     end
 
     missing_standards = []
-    missing_lessons = []
+    missing_stages = []
 
     curriculum_builder_lessons = params["lessons"]
 
     curriculum_builder_lessons&.each do |lesson|
-      lesson = code_studio_lessons[lesson["title"]]
+      stage = code_studio_stages[lesson["title"]]
 
-      unless lesson
-        missing_lessons << lesson["title"]
+      unless stage
+        missing_stages << lesson["title"]
       end
 
       updated_standards = []
@@ -45,16 +45,16 @@ class AdminStandardsController < ApplicationController
           missing_standards << standard["shortcode"]
         end
 
-        if code_studio_standard && lesson
+        if code_studio_standard && stage
           updated_standards << code_studio_standard
         end
       end
-      lesson&.standards = updated_standards
-      lesson&.save!
+      stage&.standards = updated_standards
+      stage&.save!
     end
 
-    if !missing_standards.empty? || !missing_lessons.empty?
-      render json: {status: 'failure', message: "Couldn't find standards: #{missing_standards}. Couldn't find lessons: #{missing_lessons}"}
+    if !missing_standards.empty? || !missing_stages.empty?
+      render json: {status: 'failure', message: "Couldn't find standards: #{missing_standards}. Couldn't find stages: #{missing_stages}"}
     else
       render json: {status: 'success', message: "Hooray! Importing standards associations for #{script&.name} worked."}
     end
