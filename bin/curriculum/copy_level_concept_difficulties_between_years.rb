274a5fe595fd56651a0d25905c517a8c8e914fb0
Dani <dani@code.org>
2021-05-20 21:02:31 -0400

    Update script after testing with level locally

--- bin/curriculum/copy_level_concept_difficulties_between_years.rb
+++ bin/curriculum/copy_level_concept_difficulties_between_years.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 require_relative '../../dashboard/config/environment'
 
-def main
-  year_to_copy_to = '2020'
-
+def main(year_to_copy_to)
   script_names = ['coursea-' + year_to_copy_to, 'courseb-' + year_to_copy_to, 'coursec-' + year_to_copy_to,
                   'coursed-' + year_to_copy_to, 'coursee-' + year_to_copy_to, 'coursef-' + year_to_copy_to,
                   'pre-express-' + year_to_copy_to, 'express-' + year_to_copy_to]
@@ -13,20 +11,21 @@ def main
     script.script_levels.each do |sl|
       puts "Level: " + sl.level.name
       next if sl.level.level_concept_difficulty
-      copy_lcd_from_parent(sl)
+      copy_lcd_from_parent(sl.level)
     end
   end
 end
 
-def copy_lcd_from_parent(sl)
-  parent_level = Level.find_by(id: sl.level.parent_level_id)
-  next unless parent_level
+def copy_lcd_from_parent(level)
+  parent_level = Level.find_by(id: level.parent_level_id)
+  return unless parent_level
   puts "Parent Level: " + parent_level.name
   #make a new level_concept_difficulty that is same as parent_levels
   new_lcd = parent_level.level_concept_difficulty.dup
   # Assign it to the level
-  sl.level.level_concept_difficulty = new_lcd
-  sl.level.save! if sl.level.changed?
+  level.level_concept_difficulty = new_lcd
+  level.save! if level.changed?
 end
 
-main
+main('2020')
+main('2021')
