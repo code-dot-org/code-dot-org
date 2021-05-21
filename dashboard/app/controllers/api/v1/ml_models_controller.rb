2de01875a84613e29360a1b555122deac7a63893
Madeline <madelign@gmail.com>
2021-05-21 15:58:01 -0700

    Removed metadata action and added more tests for names and show actions. Last test not passing.

--- dashboard/app/controllers/api/v1/ml_models_controller.rb
+++ dashboard/app/controllers/api/v1/ml_models_controller.rb
@@ -37,15 +37,6 @@ class Api::V1::MlModelsController < Api::V1::JsonApiController
     render json: user_ml_model_data.to_json
   end
 
-  # GET api/v1/ml_models/:id/metadata
-  # Retrieve a trained ML model's metadata
-  def metadata
-    metadata = UserMlModel.where(model_id: params[:id])&.first&.metadata
-    puts metadata
-    return render_404 unless metadata
-    render json: JSON.parse(metadata, quirks_mode: true)
-  end
-
   # GET api/v1/ml_models/:id
   # Retrieve a trained ML model from S3
   def show
