21c2e1874d0deb9fd2deb9bcdf71bf943bccecfa
Continuous Integration <dev@code.org>
2021-05-24 22:18:06 +0000

    levelbuilder content changes (-robo-commit)

---
+++ dashboard/config/libraries/zSpeechBubblePrototype.interpreted.js
@@ -0,0 +1,35 @@
+function spriteSay(mySprite,speech){
+  fill(rgb(50,50,50,0.5));
+  stroke("gray");
+  var textScaler=16;
+  textAlign(CENTER, TOP);
+  textSize(textScaler);
+
+  var minWidth=textWidth(speech);
+
+  var boxWidth=minWidth+5;
+  var boxHeight=textScaler+5;
+  var rectX=mySprite.x-boxWidth/2;
+  var rectY=mySprite.y;
+  if(rectX<0){
+    rectX=0;
+  }
+  if(rectX+boxWidth>400){
+    rectX=400-boxWidth;
+  }
+
+  if(rectY<0){
+    rectY=0;
+  }
+  if(rectY+boxHeight>400){
+    rectY=400-boxHeight;
+  }
+  //rectY=0;rectX=200-boxWidth/2;
+  rect(rectX, rectY, boxWidth, boxHeight);
+
+  noStroke();
+  fill("white");
+  text(speech,rectX+(boxWidth)/2,rectY+textScaler+2.5);
+}
+
+//other.push(spriteSay);
