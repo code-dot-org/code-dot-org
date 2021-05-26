5f921e84c13eb640a1e2e042f376829e5f8d2a3f
Maureen <maureen.sturgeon@gmail.com>
2021-05-25 17:10:28 -0700

    create a BubbleBadge to replace SmallAssessmentIcon

---
+++ apps/src/templates/progress/BubbleBadge.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import color from '@cdo/apps/util/color';
+import FontAwesome from '../FontAwesome';
+
+/**
+ * Icon that indicates an assessment on ProgressBubbles and ProgressPills
+ */
+
+const badgeTypes = ['assessment', 'keepWorking'];
+
+function BubbleBadge({type, isDiamond}) {
+  let backgroundColor, icon;
+
+  if (type === 'assessment') {
+    backgroundColor = color.purple;
+    icon = 'check';
+  } else {
+    backgroundColor = color.red;
+    icon = 'exclamation';
+  }
+
+  const position = isDiamond
+    ? styles.diamondBubblePosition
+    : styles.bubblePosition;
+
+  return (
+    <span className="fa-stack" style={{...styles.container, ...position}}>
+      <FontAwesome
+        icon="circle"
+        className="fa-stack-2x"
+        style={{color: backgroundColor}}
+      />
+      <FontAwesome
+        icon="circle-thin"
+        className="fa-stack-2x"
+        style={styles.border}
+      />
+      <FontAwesome
+        icon={icon}
+        className="fa-stack-1x"
+        style={styles.centerIcon}
+      />
+    </span>
+  );
+}
+
+BubbleBadge.propTypes = {
+  type: PropTypes.oneOf(badgeTypes).isRequired,
+  isDiamond: PropTypes.bool
+};
+
+const styles = {
+  container: {
+    fontSize: 10
+  },
+  bubblePosition: {
+    position: 'absolute',
+    top: -6,
+    right: -6
+  },
+  diamondBubblePosition: {
+    position: 'absolute',
+    top: -14,
+    right: 2,
+    // undo the rotation from the parent
+    transform: 'rotate(-45deg)'
+  },
+  border: {
+    color: color.white
+  },
+  centerIcon: {
+    color: color.white
+  },
+  assessmentIconBackground: {
+    color: color.purple
+  },
+  keepWorkingBackground: {
+    color: color.red
+  }
+};
+
+export default BubbleBadge;
---
+++ apps/test/unit/templates/progress/BubbleBadgeTest.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import BubbleBadge from '@cdo/apps/templates/progress/BubbleBadge';
+import {expect} from '../../../util/reconfiguredChai';
+import color from '@cdo/apps/util/color';
+
+describe('BubbleBadge', () => {
+  it('has a purple background when type is assessment', () => {
+    const wrapper = shallow(<BubbleBadge type="assessment" />);
+    expect(wrapper.find({icon: 'circle'}).props().style.color).to.equal(
+      color.purple
+    );
+  });
+
+  it('has a check icon when type is assessment', () => {
+    const wrapper = shallow(<BubbleBadge type="assessment" />);
+    expect(wrapper.find({icon: 'check'})).to.have.length(1);
+  });
+
+  it('has a red background when type is keepWorking', () => {
+    const wrapper = shallow(<BubbleBadge type="keepWorking" />);
+    expect(wrapper.find({icon: 'circle'}).props().style.color).to.equal(
+      color.red
+    );
+  });
+
+  it('has a exclamation icon when type is keepWorking', () => {
+    const wrapper = shallow(<BubbleBadge type="keepWorking" />);
+    expect(wrapper.find({icon: 'exclamation'})).to.have.length(1);
+  });
+
+  it('rotates the icon if isDiamond is true', () => {
+    const wrapper = shallow(<BubbleBadge type="assessment" isDiamond={true} />);
+    expect(wrapper.props().style.transform).to.equal('rotate(-45deg)');
+  });
+});
