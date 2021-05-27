d0c2491cc306d811dcf69441ddb0082cd9f8a9b5
Dani <dani@code.org>
2021-05-26 21:27:04 -0400

    Update standards controller

--- dashboard/app/controllers/admin_standards_controller.rb
+++ dashboard/app/controllers/admin_standards_controller.rb
@@ -13,26 +13,26 @@ class AdminStandardsController < ApplicationController
   def import_standards
     script = Script.find_by_name(params["unit_name"])
 
-    code_studio_stages = {}
+    code_studio_lessons = {}
     script&.lessons&.each do |lesson|
-      code_studio_stages[lesson.localized_name] = lesson
+      code_studio_lessons[lesson.localized_name] = lesson
     end
 
     missing_standards = []
-    missing_stages = []
+    missing_lessons = []
 
     curriculum_builder_lessons = params["lessons"]
 
-    curriculum_builder_lessons&.each do |lesson|
-      stage = code_studio_stages[lesson["title"]]
+    curriculum_builder_lessons&.each do |l|
+      lesson = code_studio_lessons[l["title"]]
 
-      unless stage
-        missing_stages << lesson["title"]
+      unless lesson
+        missing_lessons << l["title"]
       end
 
       updated_standards = []
 
-      lesson["standards"].each do |standard|
+      l["standards"].each do |standard|
         framework = Framework.find_by(shortcode: standard['framework'].downcase)
         code_studio_standard = Standard.find_by(
           {
@@ -45,16 +45,16 @@ class AdminStandardsController < ApplicationController
           missing_standards << standard["shortcode"]
         end
 
-        if code_studio_standard && stage
+        if code_studio_standard && lesson
           updated_standards << code_studio_standard
         end
       end
-      stage&.standards = updated_standards
-      stage&.save!
+      lesson&.standards = updated_standards
+      lesson&.save!
     end
 
-    if !missing_standards.empty? || !missing_stages.empty?
-      render json: {status: 'failure', message: "Couldn't find standards: #{missing_standards}. Couldn't find stages: #{missing_stages}"}
+    if !missing_standards.empty? || !missing_lessons.empty?
+      render json: {status: 'failure', message: "Couldn't find standards: #{missing_standards}. Couldn't find lessons: #{missing_lessons}"}
     else
       render json: {status: 'success', message: "Hooray! Importing standards associations for #{script&.name} worked."}
     end
