987f6fc4c8881bf7ccf313b662a2bbe7c153cd8a
Ben Brooks <ben@code.org>
2021-05-28 12:20:37 -0700

    Validate library questions in context of existing question

--- dashboard/app/controllers/api/v1/pd/foorm/library_questions_controller.rb
+++ dashboard/app/controllers/api/v1/pd/foorm/library_questions_controller.rb
@@ -3,8 +3,11 @@ class Api::V1::Pd::Foorm::LibraryQuestionsController < ApplicationController
   def validate_library_question
     authorize! :validate_library_question, :pd_foorm
 
-    library_question = Foorm::LibraryQuestion.new(question: params[:question].to_json)
-    library_question.validate_library_question
+    library_question = params[:id] ?
+                         Foorm::LibraryQuestion.find(params[:id]) :
+                         Foorm::LibraryQuestion.new
+    library_question.question = params[:question].to_json
+    library_question.validate_question
 
     if library_question.errors.empty?
       return render status: 200, json: {}
