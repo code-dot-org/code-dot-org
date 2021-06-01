9d929d3aa3bc9013f1f4c07573f8c98e96160781
David Bailey <davidsbailey@users.noreply.github.com>
2021-06-01 16:41:30 -0700

    add a first test case for LevelTokenContents

--- apps/test/unit/lib/levelbuilder/lesson-editor/LevelTokenTest.js
+++ apps/test/unit/lib/levelbuilder/lesson-editor/LevelTokenTest.js
@@ -2,13 +2,22 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from '../../../../util/reconfiguredChai';
 import sinon from 'sinon';
-import {UnconnectedLevelToken as LevelToken} from '@cdo/apps/lib/levelbuilder/lesson-editor/LevelToken';
+import {
+  UnconnectedLevelToken as LevelToken,
+  LevelTokenContents
+} from '@cdo/apps/lib/levelbuilder/lesson-editor/LevelToken';
 
 const defaultScriptLevel = {
   id: '11',
   position: 1,
   activeId: '2001',
-  levels: [],
+  levels: [
+    {
+      id: '11',
+      name: 'My Level',
+      url: '/path/to/edit/url'
+    }
+  ],
   kind: 'puzzle'
 };
 
@@ -34,5 +43,31 @@ describe('LevelToken', () => {
   it('renders a Motion component', () => {
     const wrapper = shallow(<LevelToken {...defaultProps} />);
     expect(wrapper.find('Motion').length).to.equal(1);
+  });
+});
+
+describe('LevelTokenContents', () => {
+  let handleDragStart, removeLevel, toggleExpand, defaultProps;
+
+  beforeEach(() => {
+    handleDragStart = sinon.spy();
+    removeLevel = sinon.spy();
+    toggleExpand = sinon.spy();
+    defaultProps = {
+      y: 0,
+      scale: 0,
+      shadow: 0,
+      activitySectionPosition: 1,
+      activityPosition: 1,
+      scriptLevel: defaultScriptLevel,
+      handleDragStart,
+      removeLevel,
+      toggleExpand
+    };
+  });
+
+  it('renders a ProgressBubble', () => {
+    const wrapper = shallow(<LevelTokenContents {...defaultProps} />);
+    expect(wrapper.find('ProgressBubble').length).to.equal(1);
   });
 });
