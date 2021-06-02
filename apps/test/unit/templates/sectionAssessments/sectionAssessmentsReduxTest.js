c4cc91069437f08b7ab76bac200dc5b3bfb331fb
Dani <dani@code.org>
2021-06-02 15:56:29 -0400

    Remove some changes that snuck in

--- apps/test/unit/templates/sectionAssessments/sectionAssessmentsReduxTest.js
+++ apps/test/unit/templates/sectionAssessments/sectionAssessmentsReduxTest.js
@@ -131,7 +131,7 @@ describe('sectionAssessmentsRedux', () => {
             levelNum: '5',
             performance: 'performanceLevel3',
             performanceLevelDetails: '3',
-            lessonName: 'Lesson 5: Creating Functions',
+            stageName: 'Lesson 5: Creating Functions',
             lessonNum: '5',
             studentName: 'Student',
             timestamp: '03/20/19 at 02:34:22'
@@ -143,7 +143,7 @@ describe('sectionAssessmentsRedux', () => {
             levelNum: '4',
             performance: 'performanceLevel2',
             performanceLevelDetails: 'My meets value',
-            lessonName: 'Lesson 4: Using Simple Commands',
+            stageName: 'Lesson 4: Using Simple Commands',
             lessonNum: '4',
             studentName: 'Student',
             timestamp: '03/21/19 at 11:54:17'
@@ -1527,7 +1527,7 @@ describe('sectionAssessmentsRedux', () => {
                 13: {
                   studentName: 'Mike',
                   lessonNum: '4',
-                  lessonName: 'Loops',
+                  stageName: 'Loops',
                   levelNum: '7',
                   keyConcept: 'You should be learning about loops',
                   performanceLevelDetails: 'A loop is in the code',
@@ -1538,7 +1538,7 @@ describe('sectionAssessmentsRedux', () => {
                 40: {
                   studentName: 'Anne',
                   lessonNum: '8',
-                  lessonName: 'Functions',
+                  stageName: 'Functions',
                   levelNum: '10',
                   keyConcept: '',
                   performanceLevelDetails: '',
@@ -1549,7 +1549,7 @@ describe('sectionAssessmentsRedux', () => {
                 52: {
                   studentName: 'Mike',
                   lessonNum: '3',
-                  lessonName: 'Variables',
+                  stageName: 'Variables',
                   levelNum: '3',
                   keyConcept: 'Use variables to help and make coding better.',
                   performanceLevelDetails:
@@ -1562,7 +1562,7 @@ describe('sectionAssessmentsRedux', () => {
                 61: {
                   studentName: 'Anne',
                   lessonNum: '3',
-                  lessonName: 'Variables',
+                  stageName: 'Variables',
                   levelNum: '3',
                   keyConcept: 'Use variables to help and make coding better.',
                   performanceLevelDetails: 'You uses no variables',
@@ -1580,7 +1580,7 @@ describe('sectionAssessmentsRedux', () => {
           {
             studentName: 'Mike',
             lessonNum: '4',
-            lessonName: 'Loops',
+            stageName: 'Loops',
             levelNum: '7',
             keyConcept: 'You should be learning about loops',
             performanceLevelDetails: 'A loop is in the code',
@@ -1591,7 +1591,7 @@ describe('sectionAssessmentsRedux', () => {
           {
             studentName: 'Anne',
             lessonNum: '8',
-            lessonName: 'Functions',
+            stageName: 'Functions',
             levelNum: '10',
             keyConcept: '',
             performanceLevelDetails: '',
@@ -1602,7 +1602,7 @@ describe('sectionAssessmentsRedux', () => {
           {
             studentName: 'Mike',
             lessonNum: '3',
-            lessonName: 'Variables',
+            stageName: 'Variables',
             levelNum: '3',
             keyConcept: 'Use variables to help and make coding better.',
             performanceLevelDetails:
@@ -1614,7 +1614,7 @@ describe('sectionAssessmentsRedux', () => {
           {
             studentName: 'Anne',
             lessonNum: '3',
-            lessonName: 'Variables',
+            stageName: 'Variables',
             levelNum: '3',
             keyConcept: 'Use variables to help and make coding better.',
             performanceLevelDetails: 'You uses no variables',
