80d4771960c76956bc1b3ae017e63c871812ad98
Bethany Connor <bethany@code.org>
2021-06-03 16:14:32 -0700

    Update errors to use unit instead of script, update comments, clean up script level copy and assert that progression is set correctly

--- dashboard/app/models/levels/level.rb
+++ dashboard/app/models/levels/level.rb
@@ -840,7 +840,7 @@ class Level < ApplicationRecord
     base_name
   end
 
-  # repeatedly strip any version year suffix of the form _NNNN ()e.g. _2017)
+  # repeatedly strip any version year suffix of the form _NNNN or -NNNN ()e.g. _2017 or -2017)
   # from the input string.
   def strip_version_year_suffixes(str)
     year_suffix_regex = /^(.*)[_-][0-9]{4}$/
