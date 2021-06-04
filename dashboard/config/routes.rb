0709f0e2c62c6acdc4656c13fac9d1ccc3e6feb1
Dani <dani@code.org>
2021-06-03 20:09:36 -0400

    The last couple remaining things in ruby and javascript code that needed updating from stage to lesson

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -339,7 +339,7 @@ Dashboard::Application.routes.draw do
     # /s/xxx/reset
     get 'reset', to: 'script_levels#reset'
     get 'next', to: 'script_levels#next'
-    get 'hidden_stages', to: 'script_levels#hidden_lesson_ids'
+    get 'hidden_lessons', to: 'script_levels#hidden_lesson_ids'
     post 'toggle_hidden', to: 'script_levels#toggle_hidden'
 
     member do
