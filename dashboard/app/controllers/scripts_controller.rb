8ae4712ae427340c4986e68e2ee366bea9066dac
Dani <dani@code.org>
2021-06-08 00:07:05 -0400

    Update controller tests

--- dashboard/app/controllers/scripts_controller.rb
+++ dashboard/app/controllers/scripts_controller.rb
@@ -227,7 +227,7 @@ class ScriptsController < ApplicationController
 
   def general_params
     h = params.permit(
-      :hidden,
+      :published_state,
       :deprecated,
       :curriculum_umbrella,
       :family_name,
@@ -245,7 +245,6 @@ class ScriptsController < ApplicationController
       :lesson_extras_available,
       :has_verified_resources,
       :tts,
-      :is_stable,
       :is_course,
       :show_calendar,
       :weekly_instructional_minutes,
@@ -264,6 +263,14 @@ class ScriptsController < ApplicationController
     ).to_h
     h[:peer_reviews_to_complete] = h[:peer_reviews_to_complete].to_i > 0 ? h[:peer_reviews_to_complete].to_i : nil
     h[:announcements] = JSON.parse(h[:announcements]) if h[:announcements]
+    if h[:published_state] == 'preview'
+      h[:hidden] = false
+      h.delete(:published_state)
+    elsif h[:published_state] == 'recommended'
+      h[:hidden] = false
+      h[:is_stable] = false
+      h.delete(:published_state)
+    end
     h
   end
 
