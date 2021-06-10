bf02d4b28a5f643902fe14a8f18bc312f97614b6
Tim Plummer <tim@code.org>
2021-06-10 09:16:45 -0700

    cleanup

--- dashboard/app/models/census/state_cs_offering.rb
+++ dashboard/app/models/census/state_cs_offering.rb
@@ -1513,7 +1513,7 @@ class Census::StateCsOffering < ApplicationRecord
     # "state_cs_offerings/<state_code>/<school_year>-<school_year_end>(.<update>).<file_extension>"
     _, state_code, name = object_key.split('/')
     years, update_or_extension, extension = name.split('.')
-    start_year, _ = years.split('-')
+    start_year, _ = years.split('-').map(&:to_i)
     update = extension ? update_or_extension : 1
     extension ||= update_or_extension
     [
