2f54e48a37c91ec2816311be2b1fddd98d9c46ac
Dani <dani@code.org>
2021-06-09 20:41:35 -0400

    Update a mistake in levels_controller_test

--- dashboard/test/controllers/levels_controller_test.rb
+++ dashboard/test/controllers/levels_controller_test.rb
@@ -594,7 +594,7 @@ class LevelsControllerTest < ActionController::TestCase
     script_level = create :script_level
     script = script_level.script
     level = script_level.level
-    assert_equal script.published_state, 'stable'
+    assert_equal script.published_state, 'preview'
 
     get :edit, params: {id: level.id}
     assert_response :success
