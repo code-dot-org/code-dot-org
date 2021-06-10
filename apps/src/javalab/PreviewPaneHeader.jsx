70ea144546b2aa03acf475b1c5650c2b3015bf52
Ben Brooks <ben@code.org>
2021-06-10 11:55:28 -0400

    Add asset manager button to Theater

--- apps/src/javalab/PreviewPaneHeader.jsx
+++ apps/src/javalab/PreviewPaneHeader.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import i18n from '@cdo/locale';
 import PaneHeader, {
   PaneSection,
   PaneButton
 } from '@cdo/apps/templates/PaneHeader';
 import CollapserIcon from '@cdo/apps/templates/CollapserIcon';
 
-export default function PreviewPaneHeader({isCollapsed, isFullscreen}) {
+export default function PreviewPaneHeader({
+  isCollapsed,
+  isFullscreen,
+  showAssetManagerButton = false,
+  headerTitle = i18n.preview()
+}) {
   return (
     <PaneHeader hasFocus>
       <PaneButton
@@ -18,25 +24,37 @@ export default function PreviewPaneHeader({isCollapsed, isFullscreen}) {
         style={styles.transparent}
         leftJustified
       />
-      {/* TODO: Uncomment iconClass and remove icon prop when we are ready to implement fullscreen. */}
-      {/* The empty icon element keeps everything centered in the header pane. */}
+      {headerTitle && (
+        <PaneSection style={styles.headerTitle}>{headerTitle}</PaneSection>
+      )}
+      {/* TODO: Uncomment fullscreen button when we are ready to implement fullscreen.
       <PaneButton
         headerHasFocus
-        // iconClass={isFullscreen ? 'fa fa-compress' : 'fa fa-arrows-alt'}
-        icon={<span style={{width: 13}} />}
+        iconClass={isFullscreen ? 'fa fa-compress' : 'fa fa-arrows-alt'}
         onClick={() => {}}
         label=""
         isRtl={false}
         style={styles.transparent}
       />
-      <PaneSection>Preview</PaneSection>
+       */}
+      {showAssetManagerButton && (
+        <PaneButton
+          headerHasFocus
+          onClick={() => {}}
+          iconClass="fa fa-upload"
+          label="Manage Assets"
+          isRtl={false}
+        />
+      )}
     </PaneHeader>
   );
 }
 
 PreviewPaneHeader.propTypes = {
   isCollapsed: PropTypes.bool.isRequired,
-  isFullscreen: PropTypes.bool.isRequired
+  isFullscreen: PropTypes.bool.isRequired,
+  showAssetManagerButton: PropTypes.bool,
+  headerTitle: PropTypes.string
 };
 
 const styles = {
