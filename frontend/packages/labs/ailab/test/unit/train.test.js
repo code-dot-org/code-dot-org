ad87dfbf066aea8d2dbdb3e298eb34985e696eba
Brendan Reville <brendan@code.org>
2021-06-13 13:48:47 +1000

    Add categorical unit test

--- frontend/packages/labs/ailab/test/unit/train.test.js
+++ frontend/packages/labs/ailab/test/unit/train.test.js
@@ -1,44 +1,73 @@
-import {
-  getExtrema,
-  getAccuracyRegression,
-  getAccuracyClassification
-} from "../../src/redux.js";
-
-import train from "../../src/train";
-
-import { ResultsGrades } from "../../src/constants.js";
-
 import { createStore } from "redux";
-
+import train from "../../src/train";
+import { ResultsGrades } from "../../src/constants.js";
+import { ColumnTypes } from "../../src/constants.js";
 import rootReducer, {
   setFirehoseMetricsLogger,
   setImportedData,
   setLabelColumn,
-  addSelectedFeature
+  addSelectedFeature,
+  setColumnsByDataType,
+  setTestData,
+  getConvertedPredictedLabel
 } from "../../src/redux";
 
 describe("train functions", () => {
-  test("do some training", async () => {
+  test("train and predict with numerical data", async () => {
     const store = createStore(rootReducer);
 
     store.dispatch(setFirehoseMetricsLogger(() => {}));
 
     const data = [
-      { temperature: "45", cost: "23", rain: "97" },
-      { temperature: "52", cost: "21", rain: "82" },
-      { temperature: "72", cost: "25", rain: "42" }
+      { temperature: "0", cost: "20", rain: "1000" },
+      { temperature: "50", cost: "25", rain: "1010" },
+      { temperature: "100", cost: "30", rain: "1020" }
     ];
 
     store.dispatch(setImportedData(data));
-
     store.dispatch(setLabelColumn("cost"));
     store.dispatch(addSelectedFeature("temperature"));
+    store.dispatch(addSelectedFeature("rain"));
 
     train.init(store);
     train.onClickTrain(store);
+
+    store.dispatch(setTestData({temperature: "150", rain: "1030"}));
+
     train.onClickPredict(store);
 
-    const newState = store.getState();
-    expect(newState.prediction).toBe(25);
+    const predictedValue = getConvertedPredictedLabel(store.getState());
+    expect(predictedValue).toBe(30);
+  });
+
+  test("train and predict with categorical data", async () => {
+    const store = createStore(rootReducer);
+
+    store.dispatch(setFirehoseMetricsLogger(() => {}));
+
+    const data = [
+      { color: "blue", flavor: "sour", texture: "crunchy" },
+      { color: "green", flavor: "sweet", texture: "crunchy" },
+      { color: "blue", flavor: "sweet", texture: "soft" }
+    ];
+
+    store.dispatch(setImportedData(data));
+    store.dispatch(setColumnsByDataType("color", ColumnTypes.CATEGORICAL));
+    store.dispatch(setColumnsByDataType("flavor", ColumnTypes.CATEGORICAL));
+    store.dispatch(setColumnsByDataType("texture", ColumnTypes.CATEGORICAL));
+    store.dispatch(setLabelColumn("color"));
+    store.dispatch(addSelectedFeature("flavor"));
+    store.dispatch(addSelectedFeature("texture"));
+
+    train.init(store);
+    train.onClickTrain(store);
+
+    store.dispatch(setTestData({flavor: "sweet", texture: "crunchy"}));
+
+    train.onClickPredict(store);
+
+    const predictedLabel = getConvertedPredictedLabel(store.getState());
+
+    expect(predictedLabel).toBe("green");
   });
 });
