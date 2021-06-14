e4bfd2a7b07616ef50d2d0cceea64ba51eb4c521
David Bailey <davidsbailey@users.noreply.github.com>
2021-06-14 16:13:51 -0700

    migration to dedup level keys everywhere but levelbuilder and prod

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_182035) do
+ActiveRecord::Schema.define(version: 2021_06_14_185722) do
 
   create_table "activities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
