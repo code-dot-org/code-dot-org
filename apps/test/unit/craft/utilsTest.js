bd078ca48c795d1a4bcf49943a96cf37db2cb56d
Madelyn Kasula <maddie.kasula@code.org>
2021-06-15 17:17:18 -0700

    Unit test handlePlayerSelection helper

---
+++ apps/test/unit/craft/utilsTest.js
@@ -0,0 +1,57 @@
+// Copied from ./i18nTest.js:
+// We have to include the base locale file so the craft-specific one will work,
+// even though we don't need to use the base locale directly in this test.
+import commonI18n from '@cdo/locale'; // eslint-disable-line no-unused-vars
+import craftI18n from '@cdo/apps/craft/locale'; // eslint-disable-line no-unused-vars
+
+import {expect} from '../../util/reconfiguredChai';
+import sinon from 'sinon';
+import * as utils from '@cdo/apps/craft/utils';
+
+describe('craft utils', () => {
+  describe('handlePlayerSelection', () => {
+    const defaultPlayer = 'Alex';
+
+    beforeEach(() => {
+      sinon.stub(utils, 'closePlayerSelectionDialog');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('closes player selection dialog after opening', () => {
+      sinon
+        .stub(utils, 'openPlayerSelectionDialog')
+        .callsFake(callback => callback('Steve'));
+
+      utils.handlePlayerSelection(defaultPlayer, () => {});
+
+      expect(utils.openPlayerSelectionDialog).to.have.been.calledOnce;
+      expect(utils.closePlayerSelectionDialog).to.have.been.calledOnce;
+    });
+
+    it('invokes onComplete with selectedPlayer', () => {
+      const selectedPlayer = 'Tom';
+      sinon
+        .stub(utils, 'openPlayerSelectionDialog')
+        .callsFake(callback => callback(selectedPlayer));
+      const onCompleteSpy = sinon.spy();
+
+      utils.handlePlayerSelection(defaultPlayer, onCompleteSpy);
+
+      expect(onCompleteSpy).to.have.been.calledOnceWith(selectedPlayer);
+    });
+
+    it('invokes callback with default player if no selectedPlayer is given', () => {
+      sinon
+        .stub(utils, 'openPlayerSelectionDialog')
+        .callsFake(callback => callback(undefined));
+      const onCompleteSpy = sinon.spy();
+
+      utils.handlePlayerSelection(defaultPlayer, onCompleteSpy);
+
+      expect(onCompleteSpy).to.have.been.calledOnceWith(defaultPlayer);
+    });
+  });
+});
