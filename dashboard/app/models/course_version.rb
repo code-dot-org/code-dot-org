dd5961c9a8149e25b2424b69b61a3d56031fb656
Bethany Connor <bethany@code.org>
2021-06-22 15:40:28 -0700

    Relax course offering and course version key requirements

--- dashboard/app/models/course_version.rb
+++ dashboard/app/models/course_version.rb
@@ -26,11 +26,11 @@ class CourseVersion < ApplicationRecord
   has_many :resources
   has_many :vocabularies
 
-  KEY_CHAR_RE = /\d/
+  KEY_CHAR_RE = /[a-z0-9\-]/
   KEY_RE = /\A#{KEY_CHAR_RE}+\Z/
   validates_format_of :key,
     with: KEY_RE,
-    message: "must contain only digits; got \"%{value}\"."
+    message: "must contain only digits, letters, or dashes; got \"%{value}\"."
 
   def units
     content_root_type == 'UnitGroup' ? content_root.default_scripts : [content_root]
