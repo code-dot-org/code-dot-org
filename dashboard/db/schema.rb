65f70b7c4ecc37f1b330748f4ccdf9de4570ab2a
Dani <dani@code.org>
2021-06-24 16:09:27 -0400

    Set default on database for published_state too

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_222109) do
+ActiveRecord::Schema.define(version: 2021_06_24_200622) do
 
   create_table "activities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
@@ -1506,7 +1506,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_222109) do
     t.text "properties"
     t.string "new_name"
     t.string "family_name"
-    t.string "published_state", default: "beta", null: false
+    t.string "published_state", default: "in_development", null: false
     t.index ["family_name"], name: "index_scripts_on_family_name"
     t.index ["name"], name: "index_scripts_on_name", unique: true
     t.index ["new_name"], name: "index_scripts_on_new_name", unique: true
@@ -1729,7 +1729,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_222109) do
     t.text "properties"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "published_state", default: "beta", null: false
+    t.string "published_state", default: "in_development", null: false
     t.index ["name"], name: "index_unit_groups_on_name"
     t.index ["published_state"], name: "index_unit_groups_on_published_state"
   end
