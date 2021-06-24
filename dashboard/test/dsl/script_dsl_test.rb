5b5717e526dca4e1b7ad5cb7c838c530e2fcd359
Bethany Connor <bethany@code.org>
2021-06-24 07:35:12 -0700

    Fix script dsl test

--- dashboard/test/dsl/script_dsl_test.rb
+++ dashboard/test/dsl/script_dsl_test.rb
@@ -38,7 +38,8 @@ class ScriptDslTest < ActiveSupport::TestCase
     deprecated: false,
     is_course: false,
     background: nil,
-    is_migrated: false
+    is_migrated: false,
+    is_maker_unit: false
   }
 
   test 'test Script DSL' do
