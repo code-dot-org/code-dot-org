6d119946040c75253c6ab37011060249defb5bf9
Maureen <maureen.sturgeon@gmail.com>
2021-06-24 09:19:18 -0700

    level feedback add field for latest for level

--- dashboard/app/controllers/teacher_feedbacks_controller.rb
+++ dashboard/app/controllers/teacher_feedbacks_controller.rb
@@ -20,8 +20,22 @@ class TeacherFeedbacksController < ApplicationController
       )
     end
 
+    # We need to know which feedback is the latest for a level/script in order to determine
+    # if the level is awaiting another review from the teacher. (A level is awaiting review if it was marked
+    # as keep working by the teacher and then the student made progress)
+    #
+    # Here we have a map from level/script to true if we've marked the latest feedback in our list
+    marked_latest_for_level = {}
+
     @feedbacks_as_student_with_level_info = @feedbacks_as_student.map do |feedback|
-      feedback.attributes.merge(feedback&.get_script_level&.summary_for_feedback)
+      level_feedback_key = [feedback.level_id, feedback.script_id].join('_')
+      is_latest = !marked_latest_for_level[level_feedback_key]
+
+      if is_latest
+        marked_latest_for_level[level_feedback_key] = true
+      end
+
+      feedback.summarize.merge({is_latest_for_level: is_latest}).merge(feedback&.get_script_level&.summary_for_feedback)
     end
   end
 
