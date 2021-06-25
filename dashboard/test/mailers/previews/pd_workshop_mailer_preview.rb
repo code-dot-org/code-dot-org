ebeaab4c030bbaf739f60ecc0af71e1ab9d46b2c
Tim Plummer <tim@code.org>
2021-06-25 09:26:10 -0700

    pr comments

--- dashboard/test/mailers/previews/pd_workshop_mailer_preview.rb
+++ dashboard/test/mailers/previews/pd_workshop_mailer_preview.rb
@@ -252,8 +252,15 @@ class Pd::WorkshopMailerPreview < ActionMailer::Preview
       options: {days_before: 3}
   end
 
-  def teacher_virtual_order_form_reminder
+  def teacher_virtual_order_form_reminder__csp
     mail :teacher_virtual_order_form_reminder, Pd::Workshop::COURSE_CSP, Pd::Workshop::SUBJECT_CSP_SUMMER_WORKSHOP,
+      workshop_params: {
+        virtual: true
+      }
+  end
+
+  def teacher_virtual_order_form_reminder__csd
+    mail :teacher_virtual_order_form_reminder, Pd::Workshop::COURSE_CSD, Pd::Workshop::SUBJECT_CSD_WORKSHOP_1,
       workshop_params: {
         virtual: true
       }
