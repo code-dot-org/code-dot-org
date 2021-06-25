15f6eebc7bf0032aa75bdcb84e94a51cc899d9ec
Bethany Connor <bethany@code.org>
2021-06-25 07:20:20 -0700

    Prevent level name change if level is in a script

--- dashboard/app/controllers/levels_controller.rb
+++ dashboard/app/controllers/levels_controller.rb
@@ -138,8 +138,7 @@ class LevelsController < ApplicationController
     # Make sure that the encrypted property is a boolean
     @level.properties['encrypted'] = @level.properties['encrypted'].to_bool if @level.properties['encrypted']
     scripts = @level.script_levels.map(&:script)
-    @launched = scripts.select(&:launched?).any?
-    @pilot = scripts.select(&:pilot_experiment).any?
+    @in_scripts = scripts.any?
     @standalone = ProjectsController::STANDALONE_PROJECTS.values.map {|h| h[:name]}.include?(@level.name)
     fb = FirebaseHelper.new('shared')
     @dataset_library_manifest = fb.get_library_manifest
