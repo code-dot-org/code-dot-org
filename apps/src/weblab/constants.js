9ef54064e5b397fc634314a1c6f10fe85b2e9f0e
Madelyn Kasula <maddie.kasula@code.org>
2021-06-28 13:58:36 -0700

    Move constants to i18n; add FatalErrorType

--- apps/src/weblab/constants.js
+++ apps/src/weblab/constants.js
@@ -1,12 +1,10 @@
-const SUPPORT_ARTICLE_URL =
+import {makeEnum} from '../utils';
+
+export const SUPPORT_ARTICLE_URL =
   'https://support.code.org/hc/en-us/articles/360016804871';
 
-const SUPPORT_ARTICLE_HTML = `<div>Please see our support article <a href="${SUPPORT_ARTICLE_URL}">"Troubleshooting Web Lab problems"</a> for more information.</div>`;
-
-export const LOAD_FAILURE_MESSAGE = `We're sorry, Web Lab failed to load for some reason. ${SUPPORT_ARTICLE_HTML}`;
-
-export const fatalErrorMessage = message =>
-  `Fatal Error: ${message}. If you're in Private Browsing mode, data can't be written. ${SUPPORT_ARTICLE_HTML}`;
-
-export const resetFailedMessage = message =>
-  `Failed to reset Web Lab: ${message}. ${SUPPORT_ARTICLE_HTML}`;
+export const FatalErrorType = makeEnum(
+  'Default',
+  'LoadFailure',
+  'ResetFailure'
+);
