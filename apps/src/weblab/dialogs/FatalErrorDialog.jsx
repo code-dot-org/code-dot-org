4d457e5605cd78799357467ab4e115b301867104
Madelyn Kasula <maddie.kasula@code.org>
2021-06-28 13:57:29 -0700

    Internationalize dialog; display support article in body

--- apps/src/weblab/dialogs/FatalErrorDialog.jsx
+++ apps/src/weblab/dialogs/FatalErrorDialog.jsx
@@ -1,38 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import commonI18n from '@cdo/locale';
+import weblabI18n from '@cdo/weblab/locale';
+import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
 import StylizedBaseDialog, {
   FooterButton
 } from '@cdo/apps/componentLibrary/StylizedBaseDialog';
+import {SUPPORT_ARTICLE_URL} from '@cdo/apps/weblab/constants';
 import {reload} from '@cdo/apps/utils';
 
-// TODO: i18n
 export default function FatalErrorDialog(props) {
+  const body = (
+    <div>
+      <p>{props.errorMessage}</p>
+      <SafeMarkdown
+        markdown={weblabI18n.troubleshootingSupport({url: SUPPORT_ARTICLE_URL})}
+        openExternalLinksInNewTab
+      />
+    </div>
+  );
+
   const footerButtons = [
     <FooterButton
-      text="Try Again"
+      text={commonI18n.tryAgain()}
       onClick={reload}
       key="cancel"
       type="cancel"
     />,
     <FooterButton
-      text="Reset Web Lab"
+      text={weblabI18n.reset()}
       onClick={props.handleResetProject}
       key="reset"
       color="red"
     />,
     <FooterButton
-      text="Dismiss"
+      text={commonI18n.dismiss()}
       onClick={props.handleClose}
       key="confirm"
       type="confirm"
     />
   ];
 
-  // TODO: append support article to props.body
   return (
     <StylizedBaseDialog
-      title={props.title}
-      body={props.body}
+      title={commonI18n.errorOccurredTitle()}
+      body={body}
       handleConfirmation={props.handleClose}
       renderFooter={() => footerButtons}
       {...props}
@@ -41,12 +53,7 @@ export default function FatalErrorDialog(props) {
 }
 
 FatalErrorDialog.propTypes = {
-  title: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired,
+  errorMessage: PropTypes.string.isRequired,
   handleClose: PropTypes.func.isRequired,
   handleResetProject: PropTypes.func.isRequired
-};
-
-FatalErrorDialog.defaultProps = {
-  title: 'An Error Occurred'
 };
