e391873db38e5b24956e710f60edf4d430a7db9f
Erin Bond <>
2021-06-28 16:11:44 -0400

    test contains only numbers

--- frontend/packages/labs/ailab/test/unit/currentColumnSelectors.test.js
+++ frontend/packages/labs/ailab/test/unit/currentColumnSelectors.test.js
@@ -3,7 +3,8 @@ import {
   getUniqueOptionsCurrentColumn,
   getExtremaCurrentColumn,
   getOptionFrequenciesCurrentColumn,
-  isCurrentColumnReadOnly
+  isCurrentColumnReadOnly,
+  currentColumnContainsOnlyNumbers
 } from "../../src/selectors/currentColumnSelectors";
 import {
   classificationState,
@@ -11,7 +12,7 @@ import {
   premadeDatasetState
 } from "./testData";
 
-describe("derives details about current column", () => {
+describe("derives details about current categorical column", () => {
   test("gets unique options current column", async () => {
     const uniqueOptions = getUniqueOptionsCurrentColumn.resultFunc(
       classificationState.currentColumn,
@@ -28,6 +29,16 @@ describe("derives details about current column", () => {
     expect(optionFrequencies).toEqual({ hot: 2, mild: 2, cool: 2 });
   });
 
+  test("current column does not contain only numbers", () => {
+    const containsOnlyNumbers = currentColumnContainsOnlyNumbers.resultFunc(
+      classificationState.currentColumn,
+      classificationState.data
+    )
+    expect(containsOnlyNumbers).toBe(false);
+  })
+});
+
+describe("derives details about current numerical column", () => {
   test("gets extrema current column", async () => {
     const extrema = getExtremaCurrentColumn.resultFunc(
       allNumericalState.currentColumn,
@@ -37,9 +48,17 @@ describe("derives details about current column", () => {
     expect(extrema.min).toBe(40);
     expect(extrema.range).toBe(60);
   });
+
+  test("current column contains only numbers", () => {
+    const containsOnlyNumbers = currentColumnContainsOnlyNumbers.resultFunc(
+      allNumericalState.currentColumn,
+      allNumericalState.data
+    )
+    expect(containsOnlyNumbers).toBe(true);
+  })
 });
 
-describe("is current column readOnly", () => {
+describe("derives details about current column", () => {
   test("current column is readOnly", async () => {
     const isReadOnly = isCurrentColumnReadOnly.resultFunc(
       premadeDatasetState.currentColumn,
