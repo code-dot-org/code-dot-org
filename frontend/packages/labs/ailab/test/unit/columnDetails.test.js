ec73816244de48f69a770d97c8ac09f88a5847d2
Erin Bond <>
2021-06-29 16:03:14 -0400

    test column datatype helpers

--- frontend/packages/labs/ailab/test/unit/columnDetails.test.js
+++ frontend/packages/labs/ailab/test/unit/columnDetails.test.js
@@ -1,5 +1,26 @@
-describe("example test", () => {
-  test("first", async () => {
-    expect(true).toBe(true);
+import {
+  isColumnCategorical,
+  isColumnNumerical
+} from '../../src/helpers/columnDetails.js';
+import { classificationState, allNumericalState } from './testData';
+
+describe("column data types", () => {
+  test("column is categorical", async () => {
+    const result = isColumnCategorical(classificationState, 'play');
+    expect(result).toBe(true);
+  });
+
+  test("column is not categorical", async () => {
+    const result = isColumnCategorical(allNumericalState, 'batCount');
+    expect(result).toBe(false);
+  });
+
+  test("column is numerical", async () => {
+    const result = isColumnNumerical(allNumericalState, 'batCount');
+    expect(result).toBe(true);
+  });
+  test("column is not numerical", async () => {
+    const result = isColumnNumerical(classificationState, 'play');
+    expect(result).toBe(false);
   });
 });
