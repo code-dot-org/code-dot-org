2a06aec605e22148937f77ac25ea6ad8b33f0d87
Ben Brooks <ben@code.org>
2021-06-30 17:07:22 -0400

    Basic ready for review checkbox

--- apps/src/templates/instructions/ReviewTab.jsx
+++ apps/src/templates/instructions/ReviewTab.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+import javalabMsg from '@cdo/javalab/locale';
 
 /**
  * This is a placeholder for the upcoming "Review" tab in Javalab.
  * See related task: https://codedotorg.atlassian.net/browse/CSA-361
  */
-export default function ReviewTab() {
-  return <p>Review...</p>;
+
+export default class ReviewTab extends Component {
+  state = {
+    readyForReview: false
+  };
+
+  renderReadyForReviewCheckbox() {
+    const readyForReview = this.state.readyForReview;
+
+    return (
+      <label style={styles.label}>
+        {javalabMsg.enablePeerReview()}
+        <input
+          type="checkbox"
+          checked={readyForReview}
+          onChange={() => this.setState({readyForReview: !readyForReview})}
+          style={styles.checkbox}
+        />
+      </label>
+    );
+  }
+
+  render() {
+    return this.renderReadyForReviewCheckbox();
+  }
 }
+
+const styles = {
+  label: {
+    float: 'right',
+    margin: 0
+  },
+  checkbox: {margin: '0 0 0 7px'}
+};
