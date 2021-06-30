f2c8f9fc69d945e663ed1cf905293ac9aef15935
Madelyn Kasula <maddie.kasula@code.org>
2021-06-30 09:09:29 -0700

    Add DisallowedHtmlWarningDialog

---
+++ apps/src/weblab/dialogs/DisallowedHtmlWarningDialog.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import commonI18n from '@cdo/locale';
+import weblabI18n from '@cdo/weblab/locale';
+import StylizedBaseDialog, {
+  FooterButton
+} from '@cdo/apps/componentLibrary/StylizedBaseDialog';
+import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
+
+export default function DisallowedHtmlWarningDialog(props) {
+  const body = (
+    <SafeMarkdown
+      markdown={weblabI18n.disallowedHtml({
+        filename: props.filename,
+        disallowedTags: props.disallowedTags.join(', ')
+      })}
+    />
+  );
+
+  return (
+    <StylizedBaseDialog
+      isOpen={props.isOpen}
+      title={commonI18n.warning()}
+      body={body}
+      handleClose={props.handleClose}
+      renderFooter={() => (
+        <FooterButton
+          type="confirm"
+          text={commonI18n.dialogOK()}
+          onClick={props.handleClose}
+        />
+      )}
+    />
+  );
+}
+
+DisallowedHtmlWarningDialog.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  filename: PropTypes.string.isRequired,
+  disallowedTags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  handleClose: PropTypes.func.isRequired
+};
