89bf4771764ea67d9c1048764ed05ddd53da67ad
Ben Brooks <ben@code.org>
2021-07-01 16:46:55 -0400

    Do permissioning in CanCan

--- dashboard/app/controllers/code_review_comments_controller.rb
+++ dashboard/app/controllers/code_review_comments_controller.rb
@@ -2,22 +2,18 @@ class CodeReviewCommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :decrypt_channel_id, only: [:create, :project_comments]
 
-  # TO DO: uncomment once we've moved permissioning into CanCan
-  #check_authorization
-  load_resource
+  check_authorization
+  load_and_authorize_resource :code_review_comment, only: [:resolve, :destroy]
 
   # POST /code_review_comments
   def create
-    return head :forbidden unless @project_owner == current_user ||
-      @project_owner.student_of?(current_user) ||
-      (current_user.sections_as_student & @project_owner.sections_as_student).any?
-
     additional_attributes = {
       commenter_id: current_user.id,
       storage_app_id: @storage_app_id
     }
     @code_review_comment = CodeReviewComment.new(code_review_comments_params.merge(additional_attributes))
 
+    authorize! :create, @code_review_comment, @project_owner
     if @code_review_comment.save
       return render json: @code_review_comment
     else
@@ -27,8 +23,6 @@ class CodeReviewCommentsController < ApplicationController
 
   # PATCH /code_review_comments/:id/resolve
   def resolve
-    return head :forbidden unless current_user == @code_review_comment.project_owner
-
     @code_review_comment.inspect
     if @code_review_comment.update(is_resolved: true)
       return head :ok
@@ -39,8 +33,6 @@ class CodeReviewCommentsController < ApplicationController
 
   # DELETE /code_review_comments/:id
   def destroy
-    return head :forbidden unless @code_review_comment.project_owner.student_of?(current_user)
-
     if @code_review_comment.delete
       return head :ok
     else
@@ -50,9 +42,7 @@ class CodeReviewCommentsController < ApplicationController
 
   # GET /code_review_comments/project_comments
   def project_comments
-    return head :forbidden unless @project_owner == current_user ||
-      @project_owner.student_of?(current_user) ||
-      (current_user.sections_as_student & @project_owner.sections_as_student).any?
+    authorize! :project_comments, CodeReviewComment.new, @project_owner
 
     @project_comments = CodeReviewComment.where(
       storage_app_id: @storage_app_id,
