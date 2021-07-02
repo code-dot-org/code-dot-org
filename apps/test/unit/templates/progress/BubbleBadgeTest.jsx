7af4dc8859a576cb9dce60f8419344d4695297b9
Charlie Forkish <charlie@code.org>
2021-07-02 10:21:29 -0700

    updated/added tests

--- apps/test/unit/templates/progress/BubbleBadgeTest.jsx
+++ apps/test/unit/templates/progress/BubbleBadgeTest.jsx
@@ -1,14 +1,60 @@
 import React from 'react';
 import {mount, shallow} from 'enzyme';
-import {
+import BubbleBadge, {
   BubbleBadgeWrapper,
   KeepWorkingBadge,
-  AssessmentBadge
+  AssessmentBadge,
+  BadgeType
 } from '@cdo/apps/templates/progress/BubbleBadge';
+import {
+  BubbleSize,
+  BubbleShape
+} from '@cdo/apps/templates/progress/BubbleFactory';
 import {expect} from '../../../util/reconfiguredChai';
 import color from '@cdo/apps/util/color';
 
 describe('BubbleBadge', () => {
+  it('renders an AssessmentBadge for BadgeType.assessment', () => {
+    const wrapper = shallow(
+      <BubbleBadge
+        badgeType={BadgeType.assessment}
+        bubbleSize={BubbleSize.full}
+        bubbleShape={BubbleShape.circle}
+      />
+    );
+    expect(wrapper.find(AssessmentBadge)).to.have.lengthOf(1);
+  });
+
+  it('renders a KeepWorkingBadge for BadgeType.keepWorking', () => {
+    const wrapper = shallow(
+      <BubbleBadge
+        badgeType={BadgeType.keepWorking}
+        bubbleSize={BubbleSize.full}
+        bubbleShape={BubbleShape.circle}
+      />
+    );
+    expect(wrapper.find(KeepWorkingBadge)).to.have.lengthOf(1);
+  });
+
+  it('renders nothing for small bubbles', () => {
+    const letter = shallow(
+      <BubbleBadge
+        badgeType={BadgeType.keepWorking}
+        bubbleSize={BubbleSize.letter}
+        bubbleShape={BubbleShape.circle}
+      />
+    );
+    const dot = shallow(
+      <BubbleBadge
+        badgeType={BadgeType.keepWorking}
+        bubbleSize={BubbleSize.dot}
+        bubbleShape={BubbleShape.circle}
+      />
+    );
+    expect(letter).to.be.empty;
+    expect(dot).to.be.empty;
+  });
+
   describe('BubbleBadgeWrapper', () => {
     it('positions the wrapper correctly if isDiamond is false', () => {
       const children = <div />;
@@ -24,7 +70,6 @@ describe('BubbleBadge', () => {
       const wrapper = shallow(
         <BubbleBadgeWrapper isDiamond={true}>{children}</BubbleBadgeWrapper>
       );
-      console.log(wrapper.debug());
       expect(wrapper.props().style.top).to.equal(-13);
       expect(wrapper.props().style.right).to.equal(-17);
     });
