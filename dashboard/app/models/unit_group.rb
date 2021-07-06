6255000ca0b50dde9f40f1a964bf66175c632f95
Bethany Connor <bethany@code.org>
2021-07-06 11:51:04 -0700

    Add static family name list back in

--- dashboard/app/models/unit_group.rb
+++ dashboard/app/models/unit_group.rb
@@ -34,10 +34,12 @@ class UnitGroup < ApplicationRecord
 
   validates :published_state, acceptance: {accept: SharedConstants::PUBLISHED_STATE.to_h.values, message: 'must be in_development, pilot, beta, preview or stable'}
 
-  CSD = 'csd'.freeze
-  CSP = 'csp'.freeze
-  CSA = 'csa'.freeze
-  TEST = 'ui-test-course'.freeze
+  FAMILY_NAMES = [
+    CSD = 'csd'.freeze,
+    CSP = 'csp'.freeze,
+    CSA = 'csa'.freeze,
+    TEST = 'ui-test-course'.freeze
+  ]
 
   def skip_name_format_validation
     !!plc_course
@@ -270,7 +272,7 @@ class UnitGroup < ApplicationRecord
   end
 
   def self.family_names
-    CourseVersion.course_offering_keys('UnitGroup')
+    (CourseVersion.course_offering_keys('UnitGroup') + FAMILY_NAMES).uniq.sort
   end
 
   # Get the set of valid courses for the dropdown in our sections table. This
