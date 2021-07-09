998139a03e43c8e66402e1d666acb834efacbc2e
Erin Peach <erin@code.org>
2021-07-09 11:55:17 -0700

    Routes and blank pages for sprite management directory

---
+++ apps/src/code-studio/assets/SpriteManagementDirectory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class SpriteManagementDirectory extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Manage Sprite Lab Sprites</h1>
+      </div>
+    );
+  }
+}
