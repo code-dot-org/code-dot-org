684f95d4372af70da4533dd0cf18895674957ac2
Madeline <madelign@gmail.com>
2021-07-12 16:56:39 -0400

    Check dynamic instructions by key

--- dashboard/test/ui/features/step_definitions/ailab_steps.rb
+++ dashboard/test/ui/features/step_definitions/ailab_steps.rb
@@ -4,3 +4,10 @@ When /^I select table column "([^"]*)"$/ do |column|
   @element = @elements[column.to_i]
   @element.click
 end
+
+Then /^I wait to see the dynamic instructions contain "((?:[^"\\]|\\.)*)" key$/ do |selector|
+  steps %{
+    And I wait to see ".#{selector}"
+    And element ".#{selector}" has one of css properties "opacity" equal to "1"
+  }
+end
