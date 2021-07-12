59acb912e6b0b197be8a77d2b304f64ea0839cb6
Ben Brooks <ben@code.org>
2021-07-12 15:36:41 -0700

    Add tests and use rosie

--- apps/test/unit/templates/instructions/codeReview/CommentEditorTest.jsx
+++ apps/test/unit/templates/instructions/codeReview/CommentEditorTest.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import {
+  stubRedux,
+  restoreRedux,
+  getStore,
+  registerReducers
+} from '@cdo/apps/redux';
+import {setPageConstants} from '@cdo/apps/redux/pageConstants';
+import commonReducers from '@cdo/apps/redux/commonReducers';
 import {expect} from '../../../../util/reconfiguredChai';
+import sinon from 'sinon';
+import {Factory} from 'rosie';
+import './CodeReviewTestHelper';
 import CommentEditor from '@cdo/apps/templates/instructions/codeReview/CommentEditor';
 import Button from '@cdo/apps/templates/Button';
 
+const DEFAULT_PROPS = {
+  onNewCommentSubmit: () => {},
+  token: 'abcxyz'
+};
+
 describe('Code Review Comment Editor', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    stubRedux();
+    wrapper = shallow(<CommentEditor {...DEFAULT_PROPS} />);
+  });
+
+  afterEach(() => {
+    restoreRedux();
+  });
+
   it('does not show submit and cancel buttons initially', () => {
-    const wrapper = shallow(<CommentEditor />);
     expect(wrapper.find(Button)).to.have.lengthOf(0);
   });
 
   it('shows submit and cancel buttons once user starts typing', () => {
-    const wrapper = shallow(<CommentEditor />);
-
     expect(wrapper.find(Button)).to.have.lengthOf(0);
     wrapper
       .find('textarea')
@@ -22,8 +46,6 @@ describe('Code Review Comment Editor', () => {
   });
 
   it('hides submit and cancel buttons and clears textbox if user hits cancel button', () => {
-    const wrapper = shallow(<CommentEditor />);
-
     const commentTextarea = wrapper.find('textarea').first();
     commentTextarea.simulate('change', {target: {value: 'a comment'}});
 
@@ -37,5 +59,39 @@ describe('Code Review Comment Editor', () => {
 
     expect(wrapper.find(Button)).to.have.lengthOf(0);
     expect(wrapper.state().comment).to.equal('');
+  });
+
+  it('submits request and calls appropriate event handler on submit', () => {
+    registerReducers(commonReducers);
+    getStore().dispatch(
+      setPageConstants({
+        channelId: 'test123'
+      })
+    );
+
+    const server = sinon.fakeServer.create();
+    server.respondWith('POST', '/code_review_comments', [
+      200,
+      {'Content-Type': 'application/json'},
+      JSON.stringify(Factory.build('CodeReviewComment'))
+    ]);
+
+    const spyOnSubmit = sinon.spy();
+    const overrideProps = {onNewCommentSubmit: spyOnSubmit};
+    const combinedProps = {...DEFAULT_PROPS, ...overrideProps};
+    wrapper = shallow(<CommentEditor {...combinedProps} />);
+
+    wrapper
+      .find('textarea')
+      .first()
+      .simulate('change', {target: {value: 'a comment'}});
+
+    wrapper
+      .find(Button)
+      .at(1)
+      .simulate('click');
+
+    server.respond();
+    expect(spyOnSubmit.called).to.equal(true);
   });
 });
