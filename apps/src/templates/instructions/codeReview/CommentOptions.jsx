829e4ad5561e13cbae1ef6fd25a79f469a7472e0
Ben Brooks <ben@code.org>
2021-07-12 11:37:58 -0700

    Comment options menu displays

--- apps/src/templates/instructions/codeReview/CommentOptions.jsx
+++ apps/src/templates/instructions/codeReview/CommentOptions.jsx
@@ -1,37 +1,39 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Radium from 'radium';
 import color from '@cdo/apps/util/color';
 
 const commentOptionTypes = {
   markComplete: {
-    iconClass: 'fa fa-check',
+    key: 'markComplete',
+    iconClass: 'fa fa-fw fa-check',
     text: 'Mark Complete'
   },
   delete: {
-    iconClass: 'fa fa-trash',
+    key: 'delete',
+    iconClass: 'fa fa-fw fa-trash',
     text: 'Delete'
   }
 };
 
-export default class CommentOptions extends Component {
+class CommentOptions extends Component {
   // to do: actually pass these
   static propTypes = {
     onMarkComplete: PropTypes.func.isRequired,
     onDelete: PropTypes.func.isRequired
   };
+
   renderCommentOption = commentOption => {
     return (
-      <div style={styles.commentOptionContainer}>
-        <div style={styles.commentOptionItemsContainer}>
-          <span className={commentOption.iconClass} />
-          <span>{commentOption.text}</span>
-        </div>
+      <div style={styles.commentOptionContainer} key={commentOption.key}>
+        <span style={styles.icon} className={commentOption.iconClass} />
+        <span style={styles.text}>{commentOption.text}</span>
       </div>
     );
   };
   render() {
     return (
-      <div>
+      <div style={styles.commentOptionsContainer}>
         {this.renderCommentOption(commentOptionTypes.markComplete)}
         {this.renderCommentOption(commentOptionTypes.delete)}
       </div>
@@ -39,20 +41,30 @@ export default class CommentOptions extends Component {
   }
 }
 
+export default Radium(CommentOptions);
+
 const styles = {
+  commentOptionsContainer: {
+    position: 'absolute',
+    margin: '5px 29px 0 0',
+    right: '0px',
+    boxShadow: `3px 3px 3px ${color.lighter_gray}`,
+    borderRadius: '4px',
+    backgroundColor: color.white
+  },
   commentOptionContainer: {
-    float: 'right',
-    maxWidth: '179px',
-    width: '100%',
-    borderColor: color.black,
-    border: '2px',
     height: '22px',
     fontSize: '14px',
-    color: color.dark_charcoal
-  },
-  commentOptionItemsContainer: {
+    fontFamily: '"Gotham 5r"',
+    color: color.dark_charcoal,
     padding: '5px 12px',
+    cursor: 'pointer',
+    ':hover': {
+      backgroundColor: color.lightest_gray
+    },
     display: 'flex',
     alignItems: 'center'
-  }
+  },
+  text: {padding: '0 5px'},
+  icon: {fontSize: '18px'}
 };
