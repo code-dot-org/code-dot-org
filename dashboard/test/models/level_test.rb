1598651dc9e8247e939e71a83b5d68d11d8d66b4
Elijah Hamovitz <elijahhamovitz@gmail.com>
2021-07-13 12:17:50 -0700

    extract levels within levels logic to a concern

--- dashboard/test/models/level_test.rb
+++ dashboard/test/models/level_test.rb
@@ -1068,54 +1068,6 @@ class LevelTest < ActiveSupport::TestCase
     assert_equal level.contained_level_names, ['real_name']
   end
 
-  test 'parent levels and child levels' do
-    parent = create :level
-    child = create :level
-    parent.child_levels << child
-    assert_equal [parent], child.parent_levels
-
-    # cannot add the same child a second time
-    assert_raises ActiveRecord::RecordInvalid do
-      parent.child_levels << child
-    end
-
-    # cannot add the same parent a second time
-    assert_raises ActiveRecord::RecordInvalid do
-      child.parent_levels << parent
-    end
-  end
-
-  test 'child levels are in order of position' do
-    parent = create :level
-    child3 = create :level
-    child2 = create :level
-    child1 = create :level
-    ParentLevelsChildLevel.find_or_create_by!(
-      parent_level: parent,
-      child_level: child3,
-      position: 3
-    )
-    ParentLevelsChildLevel.find_or_create_by!(
-      parent_level: parent,
-      child_level: child1,
-      position: 1
-    )
-    ParentLevelsChildLevel.find_or_create_by!(
-      parent_level: parent,
-      child_level: child2,
-      position: 2
-    )
-    assert_equal [child1, child2, child3], parent.child_levels
-  end
-
-  test 'all_descendant_levels works on self-referential project template levels' do
-    level_name = 'project-template-level'
-    level = create :level, name: level_name, properties: {project_template_level_name: level_name}
-    assert_equal level, level.project_template_level
-
-    assert_equal [], level.all_descendant_levels, 'omit self from descendant levels'
-  end
-
   test 'hint_prompt_enabled is true for levels in a script where hint_prompt_enabled is true' do
     script = create :csf_script
     assert script.hint_prompt_enabled?
