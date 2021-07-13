d52cb0c2d59d258951fdbe86366e6802f40518b2
Bethany Connor <bethany@code.org>
2021-07-13 13:22:05 -0700

    Sort course versions by key

--- dashboard/test/models/course_offering_test.rb
+++ dashboard/test/models/course_offering_test.rb
@@ -6,7 +6,7 @@ class CourseOfferingTest < ActiveSupport::TestCase
     version1 = create :course_version, course_offering: course_offering
     version2 = create :course_version, course_offering: course_offering
 
-    assert_equal [version1, version2], course_offering.course_versions
+    assert_equal [version1, version2].sort_by(&:key), course_offering.course_versions.sort_by(&:key)
     assert_equal course_offering, version1.course_offering
     assert_equal course_offering, version2.course_offering
   end
