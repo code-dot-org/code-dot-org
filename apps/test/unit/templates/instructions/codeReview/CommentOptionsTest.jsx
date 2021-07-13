6bc8898116bb7256719510f9bad8b75818ca6ea2
Ben Brooks <ben@code.org>
2021-07-13 09:51:04 -0700

    Test comment options and i18n strings

---
+++ apps/test/unit/templates/instructions/codeReview/CommentOptionsTest.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from '../../../../util/reconfiguredChai';
+import javalabMsg from '@cdo/javalab/locale';
+import CommentOptions from '@cdo/apps/templates/instructions/codeReview/CommentOptions';
+
+const DEFAULT_PROPS = {
+  isResolved: false,
+  onResolveClick: () => {},
+  onDeleteClick: () => {}
+};
+
+describe('Code Review Comment Options', () => {
+  it('renders complete option if comment is not resolved', () => {
+    const wrapper = shallow(<CommentOptions {...DEFAULT_PROPS} />);
+    expect(wrapper.text().includes(javalabMsg.markComplete())).to.be.true;
+  });
+
+  it('renders unmark complete option if comment is resolved', () => {
+    const unresolvedCommentProps = {
+      ...DEFAULT_PROPS,
+      ...{isResolved: true}
+    };
+    const wrapper = shallow(<CommentOptions {...unresolvedCommentProps} />);
+    expect(wrapper.text().includes(javalabMsg.unmarkComplete())).to.be.true;
+  });
+});
