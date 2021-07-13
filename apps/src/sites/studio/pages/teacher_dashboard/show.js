093fef38b68eb8e3cadc45bc656fb1d03fe9c7ec
Charlie Forkish <charlie@code.org>
2021-07-13 10:22:46 -0700

    wip

--- apps/src/sites/studio/pages/teacher_dashboard/show.js
+++ apps/src/sites/studio/pages/teacher_dashboard/show.js
@@ -1,8 +1,9 @@
 import $ from 'jquery';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import {Provider} from 'react-redux';
+import PropTypes from 'prop-types';
+// import ReactDOM from 'react-dom';
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
+// import {Provider} from 'react-redux';
 import {getStore, registerReducers} from '@cdo/apps/redux';
 import manageStudents, {
   setLoginType,
@@ -28,7 +29,9 @@ import sectionProgress, {
 } from '@cdo/apps/templates/sectionProgress/sectionProgressRedux';
 import sectionStandardsProgress from '@cdo/apps/templates/sectionProgress/standards/sectionStandardsProgressRedux';
 import unitSelection from '@cdo/apps/redux/unitSelectionRedux';
-import TeacherDashboard from '@cdo/apps/templates/teacherDashboard/TeacherDashboard';
+// import TeacherDashboard from '@cdo/apps/templates/teacherDashboard/TeacherDashboard';
+import TeacherDashboardNavigation from '@cdo/apps/templates/teacherDashboard/TeacherDashboardNavigation';
+import TeacherDashboardHeader from '@cdo/apps/templates/teacherDashboard/TeacherDashboardHeader';
 import currentUser, {
   setCurrentUserId,
   setCurrentUserName,
@@ -36,6 +39,7 @@ import currentUser, {
 } from '@cdo/apps/templates/currentUserRedux';
 import {setValidScripts} from '../../../../redux/unitSelectionRedux';
 import locales, {setLocaleCode} from '@cdo/apps/redux/localesRedux';
+import {render} from 'cdo-apollo-client';
 
 const script = document.querySelector('script[data-dashboard]');
 const scriptData = JSON.parse(script.dataset.dashboard);
@@ -54,7 +58,20 @@ const {
   lessonExtrasUnitIds,
   showSectionProgressDetails
 } = scriptData;
-const baseUrl = `/teacher_dashboard/sections/${section.id}`;
+// const baseUrl = `/teacher_dashboard/sections/${section.id}`;
+
+function Wrapper({children}) {
+  return (
+    <React.Fragment>
+      {/* <TeacherDashboardHeader /> */}
+      {/* <TeacherDashboardNavigation /> */}
+      {children}
+    </React.Fragment>
+  );
+}
+Wrapper.propTypes = {
+  children: PropTypes.node
+};
 
 $(document).ready(function() {
   registerReducers({
@@ -101,23 +118,26 @@ $(document).ready(function() {
     setValidScripts(validScripts, studentScriptIds, validCourses, section)
   );
 
-  ReactDOM.render(
-    <Provider store={store}>
-      <Router basename={baseUrl}>
-        <Route
-          path="/"
-          component={props => (
-            <TeacherDashboard
-              {...props}
-              studioUrlPrefix={scriptData.studioUrlPrefix}
-              sectionId={section.id}
-              sectionName={section.name}
-              studentCount={section.students.length}
-            />
-          )}
-        />
-      </Router>
-    </Provider>,
-    document.getElementById('teacher-dashboard')
-  );
+  render('teacher-dashboard', store, Wrapper);
+
+  // ReactDOM.render(
+  //   // <Provider store={store}>
+  //   //   <Router basename={baseUrl}>
+  //   //     <Route
+  //   //       path="/"
+  //   //       component={props => (
+  //   //         <TeacherDashboard
+  //   //           {...props}
+  //   //           studioUrlPrefix={scriptData.studioUrlPrefix}
+  //   //           sectionId={section.id}
+  //   //           sectionName={section.name}
+  //   //           studentCount={section.students.length}
+  //   //         />
+  //   //       )}
+  //   //     />
+  //   //   </Router>
+  //   // </Provider>,
+  //   App,
+  //   document.getElementById('teacher-dashboard')
+  // );
 });
