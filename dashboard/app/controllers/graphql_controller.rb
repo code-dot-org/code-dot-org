cef324ed62135818cf01bb333db2f407322dadf5
James Chen <james@code.org>
2021-07-14 13:27:17 -0700

    Add basic section and user resolvers

--- dashboard/app/controllers/graphql_controller.rb
+++ dashboard/app/controllers/graphql_controller.rb
@@ -9,8 +9,7 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user
     }
     result = DashboardSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
