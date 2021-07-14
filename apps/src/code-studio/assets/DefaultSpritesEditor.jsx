e28542fd73081fe951d0688473ac286cdd6e2c31
Erin Peach <erin@code.org>
2021-07-14 10:41:30 -0700

    Sprites Default Editor routes and empty pages

---
+++ apps/src/code-studio/assets/DefaultSpritesEditor.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class DefaultSpritesEditor extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Edit Default Sprites</h1>
+      </div>
+    );
+  }
+}
