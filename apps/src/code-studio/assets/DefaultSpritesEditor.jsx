4e4d3153094249330dc74ef0b2355633f50b2258
Erin Peach <erin@code.org>
2021-07-14 11:41:49 -0700

    Sprite editor blank UI

--- apps/src/code-studio/assets/DefaultSpritesEditor.jsx
+++ apps/src/code-studio/assets/DefaultSpritesEditor.jsx
@@ -5,9 +5,17 @@ export default class DefaultSpritesEditor extends React.Component {
     return (
       <div>
         <a href="/sprites">Back to Sprite Management</a>
+        <h1>Edit Default Sprites</h1>
+        <h2>
+          Edit the list to add or remove sprites from the default dropdown list
+          in Sprite Lab
+        </h2>
         <div>
-          <h1>Edit Default Sprites</h1>
+          <textarea name="default_sprites" cols="100" rows="10" />
         </div>
+        <button type="button" onClick={console.log('Update defaults')}>
+          Update Default Sprites List
+        </button>
       </div>
     );
   }
