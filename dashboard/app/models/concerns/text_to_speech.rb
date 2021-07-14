f56b023803f965dac32a8044c3daa8929a65cd40
Elijah Hamovitz <elijahhamovitz@gmail.com>
2021-07-14 14:07:14 -0700

    update containing level's tts when contained level is updated

--- dashboard/app/models/concerns/text_to_speech.rb
+++ dashboard/app/models/concerns/text_to_speech.rb
@@ -229,5 +229,10 @@ module TextToSpeech
       end
       self.authored_hints = JSON.dump(hints)
     end
+
+    # if this level is contained in another level, updating it should also
+    # trigger updates in its parents, since their content is likely at least
+    # partially based on this
+    parent_levels.contained.each(&:tts_update)
   end
 end
