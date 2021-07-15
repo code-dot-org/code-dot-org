9228a080f726b668e79c8d87ed0ea96eaa77b452
Maureen <maureen.sturgeon@gmail.com>
2021-07-15 14:28:45 -0700

    refactor SectionProjectsList to a functional component

--- apps/src/templates/projects/SectionProjectsList.jsx
+++ apps/src/templates/projects/SectionProjectsList.jsx
@@ -1,82 +1,50 @@
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import ProjectsList from './ProjectsList';
 import StudentFilterDropdown, {ALL_STUDENTS} from './StudentFilterDropdown';
-import _ from 'lodash';
 
-class SectionProjectsList extends Component {
-  static propTypes = {
-    projectsData: PropTypes.array.isRequired,
-    // The prefix for the code studio url in the current environment,
-    // e.g. '//studio.code.org' or '//localhost-studio.code.org:3000'.
-    studioUrlPrefix: PropTypes.string.isRequired,
-    showProjectThumbnails: PropTypes.bool.isRequired
-  };
+const SectionProjectsList = ({
+  projectsData,
+  studioUrlPrefix,
+  showProjectThumbnails
+}) => {
+  const [selectedStudent, setSelectedStudent] = useState(ALL_STUDENTS);
 
-  constructor(props) {
-    super(props);
+  const studentNames = projectsData.map(s => s.studentName).sort();
 
-    const studentNames = SectionProjectsList.getStudentNames(
-      props.projectsData
-    );
+  const filteredProjectsData = projectsData.filter(project =>
+    [ALL_STUDENTS, project['studentName']].includes(selectedStudent)
+  );
 
-    this.state = {
-      studentNames,
-      selectedStudent: ALL_STUDENTS
-    };
-  }
-
-  static getStudentNames(projectsData) {
-    return _(projectsData)
-      .map(p => p.studentName)
-      .uniq()
-      .sortBy()
-      .value();
-  }
-
-  onChangeStudent(selectedStudent) {
-    this.setState({selectedStudent});
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    const studentNames = SectionProjectsList.getStudentNames(
-      nextProps.projectsData
-    );
-    let newState = {studentNames};
-
-    if (!studentNames.includes(this.state.selectedStudent)) {
-      newState.selectedStudent = ALL_STUDENTS;
-    }
-    this.setState(newState);
-  }
-
-  render() {
-    const filteredProjectsData = this.props.projectsData.filter(project =>
-      [ALL_STUDENTS, project['studentName']].includes(
-        this.state.selectedStudent
-      )
-    );
-
-    return (
-      <div>
-        <div style={styles.filterRow}>
-          <StudentFilterDropdown
-            onChangeStudent={this.onChangeStudent.bind(this)}
-            selectedStudent={this.state.selectedStudent}
-            studentNames={this.state.studentNames}
-            style={styles.filterComponent}
-          />
-          <div style={styles.clearDiv} />
-        </div>
-        <ProjectsList
-          projectsData={filteredProjectsData}
-          studioUrlPrefix={this.props.studioUrlPrefix}
-          showProjectThumbnails={this.props.showProjectThumbnails}
+  return (
+    <div>
+      <div style={styles.filterRow}>
+        <StudentFilterDropdown
+          onChangeStudent={selectedStudent =>
+            setSelectedStudent(selectedStudent)
+          }
+          selectedStudent={selectedStudent}
+          studentNames={studentNames}
+          style={styles.filterComponent}
         />
+        <div style={styles.clearDiv} />
       </div>
-    );
-  }
-}
+      <ProjectsList
+        projectsData={filteredProjectsData}
+        studioUrlPrefix={studioUrlPrefix}
+        showProjectThumbnails={showProjectThumbnails}
+      />
+    </div>
+  );
+};
+
+SectionProjectsList.propTypes = {
+  projectsData: PropTypes.array.isRequired,
+  // The prefix for the code studio url in the current environment,
+  // e.g. '//studio.code.org' or '//localhost-studio.code.org:3000'.
+  studioUrlPrefix: PropTypes.string.isRequired,
+  showProjectThumbnails: PropTypes.bool.isRequired
+};
 
 const styles = {
   filterComponent: {
