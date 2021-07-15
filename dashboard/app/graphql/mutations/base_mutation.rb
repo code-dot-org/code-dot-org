0e4f839e078b224cc22f82bce47fc8ad5065f917
James Chen <james@code.org>
2021-07-15 14:28:04 -0700

    Add update user mutation

--- dashboard/app/graphql/mutations/base_mutation.rb
+++ dashboard/app/graphql/mutations/base_mutation.rb
@@ -1,8 +1,7 @@
 module Mutations
-  class BaseMutation < GraphQL::Schema::RelayClassicMutation
+  class BaseMutation < GraphQL::Schema::Mutation
     argument_class Types::BaseArgument
     field_class Types::BaseField
-    input_object_class Types::BaseInputObject
     object_class Types::BaseObject
   end
 end
