db6e37f2ca54b398f93c591e056b607c6aeef815
Ben Brooks <ben@code.org>
2021-07-14 17:03:55 -0700

    Move load and create comment tests into ReviewTab

--- apps/test/unit/templates/instructions/ReviewTabTest.jsx
+++ apps/test/unit/templates/instructions/ReviewTabTest.jsx
@@ -1,20 +1,111 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from '../../../util/reconfiguredChai';
+import sinon from 'sinon';
 import {Factory} from 'rosie';
 import './codeReview/CodeReviewTestHelper';
 import ReviewTab from '@cdo/apps/templates/instructions/ReviewTab';
-
-const DEFAULT_COMMENT = Factory.build('CodeReviewComment');
+import Comment from '@cdo/apps/templates/instructions/codeReview/Comment';
+import {
+  getStore,
+  registerReducers,
+  restoreRedux,
+  stubRedux
+} from '@cdo/apps/redux';
+import commonReducers from '@cdo/apps/redux/commonReducers';
+import {setPageConstants} from '@cdo/apps/redux/pageConstants';
 
 describe('Code Review Tab', () => {
-  it('shows new comment after one is created', () => {
-    const wrapper = shallow(
-      <ReviewTab comments={[DEFAULT_COMMENT]} token="xyz" />
+  const channelId = 'test123';
+  let wrapper, server;
+
+  beforeEach(() => {
+    server = sinon.fakeServer.create();
+
+    stubRedux();
+    registerReducers(commonReducers);
+    getStore().dispatch(
+      setPageConstants({
+        channelId: channelId
+      })
+    );
+  });
+
+  afterEach(() => {
+    restoreRedux();
+  });
+
+  it('renders without error if project has no comments', () => {
+    // projectVersion = 'latest' is a placeholder until we implement
+    // storing project version when saving comments
+    server.respondWith(
+      'GET',
+      `/code_review_comments/project_comments?channel_id=${channelId}&project_version=latest`,
+      [200, {'Content-Type': 'application/json'}, JSON.stringify([])]
     );
 
-    expect(wrapper.state().comments).to.have.lengthOf(1);
-    wrapper.instance().onNewCommentSubmit(DEFAULT_COMMENT);
-    expect(wrapper.state().comments).to.have.lengthOf(2);
+    wrapper = shallow(<ReviewTab />);
+    server.respond();
+
+    expect(wrapper.find(Comment).length).to.equal(0);
+  });
+
+  it('renders a comment fetched on mount if one exists', () => {
+    // projectVersion = 'latest' is a placeholder until we implement
+    // storing project version when saving comments
+    server.respondWith(
+      'GET',
+      `/code_review_comments/project_comments?channel_id=${channelId}&project_version=latest`,
+      [
+        200,
+        {'Content-Type': 'application/json'},
+        JSON.stringify([Factory.build('CodeReviewComment')])
+      ]
+    );
+
+    wrapper = shallow(<ReviewTab />);
+    expect(wrapper.find(Comment).length).to.equal(0);
+
+    server.respond();
+    expect(wrapper.find(Comment).length).to.equal(1);
+  });
+
+  it('submits request and shows new comment after one is created', () => {
+    // projectVersion = 'latest' is a placeholder until we implement
+    // storing project version when saving comments
+    server.respondWith(
+      'GET',
+      `/code_review_comments/project_comments?channel_id=${channelId}&project_version=latest`,
+      [
+        200,
+        {'Content-Type': 'application/json'},
+        JSON.stringify([Factory.build('CodeReviewComment')])
+      ]
+    );
+
+    const testCommentText = 'test comment text';
+    const newlyCreatedComment = Factory.build('CodeReviewComment', {
+      commentText: testCommentText
+    });
+
+    server.respondWith('POST', '/code_review_comments', [
+      200,
+      {'Content-Type': 'application/json'},
+      JSON.stringify(newlyCreatedComment)
+    ]);
+
+    wrapper = shallow(<ReviewTab />);
+    server.respond();
+    expect(wrapper.find(Comment).length).to.equal(1);
+
+    wrapper.instance().onNewCommentSubmit(testCommentText);
+    server.respond();
+    expect(wrapper.find(Comment).length).to.equal(2);
+    expect(
+      wrapper
+        .find(Comment)
+        .at(1)
+        .props().comment.commentText
+    ).to.equal(testCommentText);
   });
 });
