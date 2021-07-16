c0533aa86689156ef9536b2efb0359645ecabaa1
Erin Bond <>
2021-07-16 14:43:53 -0400

    loosen validations to handle numerical and categorical labels

--- frontend/packages/labs/ailab/src/UIComponents/shapes.js
+++ frontend/packages/labs/ailab/src/UIComponents/shapes.js
@@ -2,6 +2,6 @@ import PropTypes from 'prop-types';
 
 export const resultsShape = PropTypes.shape({
   examples: PropTypes.arrayOf(PropTypes.array).isRequired,
-  labels: PropTypes.arrayOf(PropTypes.string).isRequired,
-  predictedLabels: PropTypes.arrayOf(PropTypes.string).isRequired
+  labels: PropTypes.array.isRequired,
+  predictedLabels: PropTypes.array.isRequired
 });
