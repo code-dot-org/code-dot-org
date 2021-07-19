87bd2a96ea26d99a86495a5cd28946531f4d8ada
Sanchit Malhotra <sanchit@code.org>
2021-07-19 13:51:40 -0700

    Use full javabuilder url

--- lib/cdo/rack/upgrade_insecure_requests.rb
+++ lib/cdo/rack/upgrade_insecure_requests.rb
@@ -48,10 +48,10 @@ module Rack
             "child-src blob: ",
             "script-src 'self' https: 'unsafe-inline' https://vaas.acapela-group.com 'unsafe-eval'",
             "style-src 'self' https: 'unsafe-inline'",
-            "img-src 'self' https: data: blob: http://*.dev-code.org",
+            "img-src 'self' https: data: blob: http://javabuilderbeta-output.dev-code.org",
             "font-src 'self' https: data:",
             "connect-src 'self' https: https://api.pusherapp.com wss://ws.pusherapp.com wss://*.firebaseio.com http://localhost:8080 https://curriculum.code.org/ wss://*.code.org",
-            "media-src 'self' https: data: http://vaas.acapela-group.com http://*.dev-code.org",
+            "media-src 'self' https: data: http://vaas.acapela-group.com http://javabuilderbeta-output.dev-code.org",
             "report-uri #{CDO.code_org_url('https/mixed-content')}"
           ]
         end
