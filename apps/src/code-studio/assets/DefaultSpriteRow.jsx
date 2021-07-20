276fd02a175884dd00cb5327e9e9ac91d7870e5c
Erin Peach <erin@code.org>
2021-07-20 10:26:55 -0700

    Show pending changes UI

--- apps/src/code-studio/assets/DefaultSpriteRow.jsx
+++ apps/src/code-studio/assets/DefaultSpriteRow.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@cdo/apps/templates/Button';
-import color from '@cdo/apps/util/color';
 
 export default class DefaultSpriteRow extends React.Component {
   static propTypes = {
@@ -10,29 +9,20 @@ export default class DefaultSpriteRow extends React.Component {
     onDelete: PropTypes.func.isRequired
   };
 
-  state = {
-    isDeleted: false
-  };
-
   deleteSprite(spriteName) {
     this.props.onDelete(spriteName);
-    this.setState({isDeleted: true});
   }
 
   render() {
-    let isDeleted = this.state.isDeleted;
     return (
       <div style={styles.assetRow}>
-        {isDeleted && <h3 style={styles.deletedText}>Deleted</h3>}
-        {!isDeleted && (
-          <Button
-            text={'Delete'}
-            color={Button.ButtonColor.red}
-            onClick={() => this.deleteSprite(this.props.name)}
-            icon={'trash'}
-            iconClassName={'fa-trash'}
-          />
-        )}
+        <Button
+          text={'Delete'}
+          color={Button.ButtonColor.red}
+          onClick={() => this.deleteSprite(this.props.name)}
+          icon={'trash'}
+          iconClassName={'fa-trash'}
+        />
         <h3>
           {this.props.name}: {this.props.keyValue}
         </h3>
@@ -47,9 +37,5 @@ const styles = {
     display: 'flex',
     justifyContent: 'flex-start',
     alignItems: 'center'
-  },
-  deletedText: {
-    color: color.red,
-    padding: 10
   }
 };
