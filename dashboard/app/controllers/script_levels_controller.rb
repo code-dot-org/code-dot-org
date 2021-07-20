7f6ee0ac10d39fc9a32b1c085672f4e7efe2ca54
Ben Brooks <ben@code.org>
2021-07-20 14:03:54 -0700

    Allow students to see peer reviewable projects

--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -402,7 +402,9 @@ class ScriptLevelsController < ApplicationController
     user = User.find(params[:user_id])
 
     # TODO: This should use cancan/authorize.
-    if user.student_of?(current_user) || current_user.project_validator?
+    if user.student_of?(current_user) ||
+      current_user.project_validator? ||
+      ReviewableProject.where(user_id: user.id, script_id: params[:script_id], level_id: @script_level.oldest_active_level&.id).any?
       @user = user
     end
   end
