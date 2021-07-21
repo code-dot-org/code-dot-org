82c14ece139765472ed73abd6a7324cdb1706054
Erin Bond <>
2021-07-21 12:07:32 -0400

    use unconnected ProgressDetailToggle and pass props rather than use redux

--- apps/src/templates/progress/ProgressDetailToggle.story.jsx
+++ apps/src/templates/progress/ProgressDetailToggle.story.jsx
@@ -1,113 +1,59 @@
 import React from 'react';
-import ProgressDetailToggle from './ProgressDetailToggle';
-import {Provider} from 'react-redux';
-import {combineReducers, createStore} from 'redux';
+import {
+  UnconnectedProgressDetailToggle as ProgressDetailToggle
+} from './ProgressDetailToggle';
 import progress, {setIsSummaryView} from '@cdo/apps/code-studio/progressRedux';
 
 export default storybook => {
-  const initialState = {
-    progress: {
-      lessonGroups: [],
-      lessons: [
-        {
-          levels: []
-        }
-      ],
-      focusAreaLessonIds: [],
-      professionalLearningCourse: false
-    }
-  };
-
-  const initialStateGrouped = {
-    progress: {
-      lessonGroups: [
-        {
-          display_name: 'cat1',
-          id: 1,
-          description: 'This is a description',
-          big_questions: 'What?'
-        },
-        {
-          display_name: 'cat2',
-          id: 2,
-          description: 'This is another description',
-          big_questions: 'Why?'
-        }
-      ],
-      lessons: [
-        {
-          lesson_group_display_name: 'cat1',
-          levels: []
-        },
-        {
-          lesson_group_display_name: 'cat2',
-          levels: []
-        }
-      ],
-      focusAreaLessonIds: [],
-      professionalLearningCourse: false
-    }
-  };
-
-  function isSummaryTrue() {
-    const store = createStore(combineReducers({progress}), initialState);
-    store.dispatch(setIsSummaryView(true));
-    return {
-      name: 'isSummary is true',
-      story: () => (
-        <Provider store={store}>
-          <ProgressDetailToggle />
-        </Provider>
-      )
-    };
-  }
-
-  function isSummaryFalse() {
-    const store = createStore(combineReducers({progress}), initialState);
-    store.dispatch(setIsSummaryView(false));
-    return {
-      name: 'isSummary is false',
-      story: () => (
-        <Provider store={store}>
-          <ProgressDetailToggle />
-        </Provider>
-      )
-    };
-  }
-
-  function isSummaryTrueGrouped() {
-    const store = createStore(combineReducers({progress}), initialStateGrouped);
-    store.dispatch(setIsSummaryView(true));
-    return {
-      name: 'isSummary is true with groups',
-      story: () => (
-        <Provider store={store}>
-          <ProgressDetailToggle />
-        </Provider>
-      )
-    };
-  }
-
-  function isSummaryFalseGrouped() {
-    const store = createStore(combineReducers({progress}), initialStateGrouped);
-    store.dispatch(setIsSummaryView(false));
-    return {
-      name: 'isSummary is false with groups',
-      story: () => (
-        <Provider store={store}>
-          <ProgressDetailToggle />
-        </Provider>
-      )
-    };
-  }
-
   storybook
     .storiesOf('Progress/ProgressDetailToggle', module)
-    .withReduxStore()
+    .withReduxStore({progress})
     .addStoryTable([
-      isSummaryTrue(),
-      isSummaryFalse(),
-      isSummaryTrueGrouped(),
-      isSummaryFalseGrouped()
+      {
+        name: 'isSummary is true',
+        story: () => {
+          return (
+            <ProgressDetailToggle
+              isPlc={false}
+              isSummaryView
+              hasGroups={false}
+              setIsSummaryView={setIsSummaryView}
+            />
+          );
+        }
+      },
+      {
+        name: 'isSummary is false',
+        story: () => (
+          <ProgressDetailToggle
+            isPlc={false}
+            isSummaryView={false}
+            hasGroups={false}
+            setIsSummaryView={setIsSummaryView}
+          />
+        )
+      },
+      {
+        name: 'isSummary is true with groups',
+        story: () => (
+          <ProgressDetailToggle
+            isPlc={false}
+            isSummaryView
+            hasGroups
+            setIsSummaryView={setIsSummaryView}
+          />
+        )
+      },
+      {
+        name: 'isSummary is false with groups',
+        story: () => (
+          <ProgressDetailToggle
+            isPlc={false}
+            isSummaryView={false}
+            hasGroups
+            setIsSummaryView={setIsSummaryView}
+          />
+        )
+      }
     ]);
 };
