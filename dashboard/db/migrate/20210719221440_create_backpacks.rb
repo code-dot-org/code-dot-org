e7c3398a9ad252822f181ab15d85934be041f317
molly-moen <molly@code.org>
2021-07-21 09:12:21 -0700

    back to storage_app_id

--- dashboard/db/migrate/20210719221440_create_backpacks.rb
+++ dashboard/db/migrate/20210719221440_create_backpacks.rb
@@ -2,7 +2,7 @@ class CreateBackpacks < ActiveRecord::Migration[5.2]
   def change
     create_table :backpacks do |t|
       t.integer :user_id, index: true, unique: true, null: false
-      t.integer :channel_token_id, null: false
+      t.integer :storage_app_id, null: false
 
       t.timestamps
     end
