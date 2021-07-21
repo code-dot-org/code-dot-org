7fb17bbe15fd64ef8bf4d4fc85d37be0d3e0ad7a
molly-moen <molly@code.org>
2021-07-21 15:59:33 -0700

    add comment

--- dashboard/app/controllers/backpacks_controller.rb
+++ dashboard/app/controllers/backpacks_controller.rb
@@ -2,6 +2,8 @@ class BackpacksController < ApplicationController
   before_action :authenticate_user!
 
   # GET /backpack/channel
+  # Return the channel token for the backpack of the current user. If
+  # the current user does not have a backpack, create one.
   def get_channel
     backpack = Backpack.find_or_create(current_user.id, request.ip)
     render json: backpack.channel
