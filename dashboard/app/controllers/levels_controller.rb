7e22d210f209b90aa3964e47b5c5e1e84e564185
Bethany Connor <bethany@code.org>
2021-07-22 15:17:18 -0400

    Fix get_serialized_maze route for non-Javalab levels

--- dashboard/app/controllers/levels_controller.rb
+++ dashboard/app/controllers/levels_controller.rb
@@ -159,7 +159,7 @@ class LevelsController < ApplicationController
   # GET /levels/:id/get_serialized_maze
   # Get the serialized_maze for the level, if it exists.
   def get_serialized_maze
-    serialized_maze = @level.get_serialized_maze
+    serialized_maze = @level.try(:get_serialized_maze)
     return head :no_content unless serialized_maze
     render json: serialized_maze
   end
