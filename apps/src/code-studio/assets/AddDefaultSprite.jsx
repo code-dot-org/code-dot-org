2019460d1579ace1ed48be6dbd94cdcaa3823640
Erin Peach <erin@code.org>
2021-07-22 10:02:47 -0700

    Display add success

--- apps/src/code-studio/assets/AddDefaultSprite.jsx
+++ apps/src/code-studio/assets/AddDefaultSprite.jsx
@@ -9,18 +9,25 @@ export default class AddDefaultSprite extends React.Component {
 
   state = {
     spriteName: '',
-    spriteCategory: ''
+    spriteCategory: '',
+    displaySuccess: false
   };
 
   handleNameChange = event => {
-    this.setState({spriteName: event.target.value});
+    this.setState({spriteName: event.target.value, displaySuccess: false});
   };
 
   handleCategoryChange = event => {
-    this.setState({spriteCategory: event.target.value});
+    this.setState({spriteCategory: event.target.value, displaySuccess: false});
+  };
+
+  handleSpriteAdd = () => {
+    this.props.onAdd(this.state.spriteName, this.state.spriteCategory);
+    this.setState({spriteName: '', spriteCategory: '', displaySuccess: true});
   };
 
   render() {
+    let {displaySuccess} = this.state;
     return (
       <div style={styles.assetRow}>
         <h3>Add a sprite: </h3>
@@ -47,9 +54,15 @@ export default class AddDefaultSprite extends React.Component {
         <Button
           text="Add"
           color={Button.ButtonColor.orange}
-          onClick={() =>
-            this.props.onAdd(this.state.spriteName, this.state.spriteCategory)
-          }
+          onClick={this.handleSpriteAdd}
+        />
+        <i
+          style={{
+            ...styles.checkmark,
+            visibility: displaySuccess ? 'visible' : 'hidden'
+          }}
+          className="fa fa-check"
+          aria-hidden="true"
         />
       </div>
     );
@@ -65,5 +78,13 @@ const styles = {
   },
   addSpriteLabel: {
     padding: 10
+  },
+  checkmark: {
+    color: 'lightgreen',
+    fontSize: 18,
+    left: 5,
+    lineHeight: '25px',
+    position: 'relative',
+    top: 7
   }
 };
