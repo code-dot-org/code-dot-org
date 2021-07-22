01f6093fa0440d78ac799a9b52ebf644a2df60fd
Ben Brooks <ben@code.org>
2021-07-22 09:05:28 -0700

    Include associations in reviewable_project model for factory

--- dashboard/app/models/reviewable_project.rb
+++ dashboard/app/models/reviewable_project.rb
@@ -15,4 +15,7 @@
 #  index_reviewable_projects_on_storage_app_id_and_user_id  (storage_app_id,user_id)
 #
 class ReviewableProject < ApplicationRecord
+  belongs_to :user
+  belongs_to :level
+  belongs_to :script
 end
