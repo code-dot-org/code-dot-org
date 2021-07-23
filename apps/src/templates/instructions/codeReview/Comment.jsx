83d5d934d087e36d13c3aea9133a5afb78695ff5
Ben Brooks <ben@code.org>
2021-07-23 13:50:53 -0700

    Format timestamps and reorganize header

--- apps/src/templates/instructions/codeReview/Comment.jsx
+++ apps/src/templates/instructions/codeReview/Comment.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import javalabMsg from '@cdo/javalab/locale';
 import color from '@cdo/apps/util/color';
 import msg from '@cdo/locale';
@@ -40,6 +41,9 @@ export default class Comment extends Component {
     );
   };
 
+  renderFormattedTimestamp = timestampString =>
+    moment.utc(timestampString).format('M/D/YYYY [at] h:mm A');
+
   render() {
     const {
       id,
@@ -62,28 +66,32 @@ export default class Comment extends Component {
       >
         <div style={styles.commentHeaderContainer}>
           {this.renderName()}
-          <div
-            className="fa fa-ellipsis-h"
-            style={styles.ellipsisMenu}
-            onClick={() =>
-              this.setState({
-                isShowingCommentOptions: !isShowingCommentOptions
-              })
-            }
-          >
-            {isShowingCommentOptions && (
-              <CommentOptions
-                isResolved={isResolved}
-                onResolveStateToggle={() => {
-                  this.props.onResolveStateToggle(id);
-                  this.setState({isShowingCommentOptions: false});
-                }}
-                onDelete={() => this.onDelete(id)}
-              />
-            )}
-          </div>
-          {isResolved && <span className="fa fa-check" style={styles.check} />}
-          <span style={styles.timestamp}>{timestampString}</span>
+          <span style={styles.rightAlignedCommentHeaderSection}>
+            <span style={styles.timestamp}>
+              {this.renderFormattedTimestamp(timestampString)}
+            </span>
+            {isResolved && <i className="fa fa-check" style={styles.check} />}
+            <i
+              className="fa fa-ellipsis-h"
+              style={styles.ellipsisMenu}
+              onClick={() =>
+                this.setState({
+                  isShowingCommentOptions: !isShowingCommentOptions
+                })
+              }
+            >
+              {isShowingCommentOptions && (
+                <CommentOptions
+                  isResolved={isResolved}
+                  onResolveStateToggle={() => {
+                    this.props.onResolveStateToggle(id);
+                    this.setState({isShowingCommentOptions: false});
+                  }}
+                  onDelete={() => this.onDelete(id)}
+                />
+              )}
+            </i>
+          </span>
         </div>
         <div
           id={'code-review-comment-body'}
@@ -102,7 +110,6 @@ export default class Comment extends Component {
 }
 
 const sharedIconStyles = {
-  float: 'right',
   fontSize: '24px',
   lineHeight: '18px',
   margin: '0 0 0 5px'
@@ -138,11 +145,13 @@ const styles = {
   olderVersionCommentBackgroundColor: {backgroundColor: color.background_gray},
   timestamp: {
     fontStyle: 'italic',
-    float: 'right',
     margin: '0 5px'
   },
   commentHeaderContainer: {
     marginBottom: '5px',
-    position: 'relative'
-  }
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  rightAlignedCommentHeaderSection: {display: 'flex'}
 };
