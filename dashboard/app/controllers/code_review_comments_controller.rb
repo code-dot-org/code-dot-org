d7f24caa46c97595d7948b48c87efe99c772f189
Ben Brooks <ben@code.org>
2021-07-26 09:47:13 -0700

    Use callback to mark whether comment is from teacher

--- dashboard/app/controllers/code_review_comments_controller.rb
+++ dashboard/app/controllers/code_review_comments_controller.rb
@@ -13,8 +13,7 @@ class CodeReviewCommentsController < ApplicationController
       commenter_id: current_user.id,
       storage_app_id: @storage_app_id,
       project_version: 'temporary placeholder',
-      project_owner_id: @project_owner.id,
-      is_from_teacher: current_user.teacher?
+      project_owner_id: @project_owner.id
     }
     @code_review_comment = CodeReviewComment.new(code_review_comments_params.merge(additional_attributes))
 
