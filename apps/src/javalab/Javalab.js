0ff39037ee023e5fe4e07c1fa2f82b010f84b30b
molly-moen <molly@code.org>
2021-07-27 10:07:06 -0700

    error handling

--- apps/src/javalab/Javalab.js
+++ apps/src/javalab/Javalab.js
@@ -284,15 +284,4 @@ Javalab.prototype.onCommitCode = function() {
   project.autosave();
 };
 
-Javalab.prototype.createBackpackApi = function() {
-  $.ajax({
-    url: '/backpacks/channel',
-    type: 'get'
-  }).done(response => {
-    getStore().dispatch(
-      setBackpackApi(new BackpackClientApi(response.channel))
-    );
-  });
-};
-
 export default Javalab;
