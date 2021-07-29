5d841d65b79c24ae17dd41f317de6ea2f7ccd4da
Maureen <maureen.sturgeon@gmail.com>
2021-07-29 09:14:40 -0700

    update migration, resolve conflicts with schema

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_222333) do
+ActiveRecord::Schema.define(version: 2021_07_29_161058) do
 
   create_table "activities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
