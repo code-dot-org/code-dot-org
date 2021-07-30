5ec3d112a2f16eaccd704a28e54a45e8d730baf0
Erin Peach <erin@code.org>
2021-07-30 12:05:48 -0700

    Add UI to move to prod

--- apps/src/code-studio/assets/SpriteManagementDirectory.jsx
+++ apps/src/code-studio/assets/SpriteManagementDirectory.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '@cdo/apps/templates/Button';
+import color from '@cdo/apps/util/color';
 
 export default class SpriteManagementDirectory extends React.Component {
   render() {
@@ -11,7 +13,24 @@ export default class SpriteManagementDirectory extends React.Component {
         <h3>
           <a href="/sprites/default_sprites_editor">Edit Default Sprites</a>
         </h3>
+        <div style={styles.pageBreak}>
+          <Button
+            text="Move Levelbuilder Change to Production"
+            color={Button.ButtonColor.red}
+            onClick={() => console.log('Move to production')}
+            style={styles.button}
+          />
+        </div>
       </div>
     );
   }
 }
+
+const styles = {
+  pageBreak: {
+    borderTop: `1px solid ${color.dark_slate_gray}`
+  },
+  button: {
+    margin: 20
+  }
+};
