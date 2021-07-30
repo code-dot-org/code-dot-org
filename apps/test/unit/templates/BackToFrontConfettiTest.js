ad1946a4976a5e105824bc81e770eeba9dd74093
Erin Bond <>
2021-07-30 10:01:52 -0400

    upgrade a handful of tests to use reconfiguredChai

--- apps/test/unit/templates/BackToFrontConfettiTest.js
+++ apps/test/unit/templates/BackToFrontConfettiTest.js
@@ -1,8 +1,15 @@
 import sinon from 'sinon';
 import BackToFrontConfetti from '@cdo/apps/templates/BackToFrontConfetti';
 import React from 'react';
-import {expect} from '../../util/deprecatedChai';
+import {expect} from '../../util/reconfiguredChai';
 import {mount} from 'enzyme';
+
+function getZIndex(wrapper) {
+  return wrapper
+    .at(0)
+    .childAt(0)
+    .props().style.zIndex;
+}
 
 describe('BackToFrontConfetti', () => {
   let clock;
@@ -17,29 +24,30 @@ describe('BackToFrontConfetti', () => {
 
   it('initially renders with a negative zIndex', () => {
     const wrapper = mount(<BackToFrontConfetti />);
-    expect(wrapper).to.have.style('zIndex', '-1');
+    expect(getZIndex(wrapper)).to.equal(-1);
   });
 
   it('remains negatively zIndexed indefinitely after rendering', () => {
     const wrapper = mount(<BackToFrontConfetti />);
     clock.tick(999);
-    expect(wrapper).to.have.style('zIndex', '-1');
+    expect(getZIndex(wrapper)).to.equal(-1);
   });
 
   it('remains negatively zIndexed temporarily after activation', () => {
     const wrapper = mount(<BackToFrontConfetti />);
     wrapper.setProps({active: true});
-    expect(wrapper).to.have.style('zIndex', '-1');
+    expect(getZIndex(wrapper)).to.equal(-1);
     clock.tick(10);
-    expect(wrapper).to.have.style('zIndex', '-1');
+    expect(getZIndex(wrapper)).to.equal(-1);
   });
 
   it('switches to a positive zIndex shortly after activation', () => {
     const wrapper = mount(<BackToFrontConfetti />);
     wrapper.setProps({active: true});
     clock.tick(600);
-    expect(wrapper).to.have.style('zIndex', '-1');
+    expect(getZIndex(wrapper)).to.equal(-1);
     clock.tick(100);
-    expect(wrapper).to.have.style('zIndex', '1');
+    wrapper.setProps({}); // Force re-render
+    expect(getZIndex(wrapper)).to.equal(-1);
   });
 });
