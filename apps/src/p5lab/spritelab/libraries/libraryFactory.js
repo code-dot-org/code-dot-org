28d980e79597276cb6403a47d4d54b982e4b1263
anjali <anjali@code.org>
2021-08-02 10:38:30 -0700

    fix tests

--- apps/src/p5lab/spritelab/libraries/libraryFactory.js
+++ apps/src/p5lab/spritelab/libraries/libraryFactory.js
@@ -6,7 +6,7 @@ export default function createLibrary(levelConfig, args) {
     console.warn('cannot create sprite lab library without p5 instance');
     return;
   }
-  if (levelConfig.blockPools.includes('PoemBot')) {
+  if (levelConfig.blockPools?.includes('PoemBot')) {
     return new PoemBotLibrary(args.p5);
   } else {
     return new CoreLibrary(args.p5);
