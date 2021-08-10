af7e94028c46f0b6f33cd8b24b32131fbae13419
Bethany Connor <bethany@code.org>
2021-08-10 14:28:54 -0700

    Make storage_app_id an int and add another index

--- dashboard/app/models/project_version_metadatum.rb
+++ dashboard/app/models/project_version_metadatum.rb
@@ -3,7 +3,7 @@
 # Table name: project_version_metadata
 #
 #  id                :bigint           not null, primary key
-#  storage_app_id    :string(255)      not null
+#  storage_app_id    :integer          not null
 #  object_version_id :string(255)      not null
 #  comment           :text(16777215)
 #  created_at        :datetime         not null
@@ -11,7 +11,8 @@
 #
 # Indexes
 #
-#  index_project_version_metadata_on_storage_app_id  (storage_app_id)
+#  index_project_version_metadata_on_storage_app_id                 (storage_app_id) UNIQUE
+#  index_project_version_metadata_on_storage_app_id_and_version_id  (storage_app_id,object_version_id) UNIQUE
 #
 class ProjectVersionMetadatum < ApplicationRecord
 end
