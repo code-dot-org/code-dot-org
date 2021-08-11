95411709eb5ea7d9adde14327a706f8bf527e2f6
Madelyn Kasula <maddie.kasula@code.org>
2021-08-11 09:35:22 -0700

    DO NOT MERGE: Disable throwOnConsoleWarningsEverywhere in tests

--- apps/test/unit-tests.js
+++ apps/test/unit-tests.js
@@ -1,10 +1,7 @@
 // This gets replaced by karma webpack with the updated files on rebuild
 import '@babel/polyfill';
 import 'whatwg-fetch';
-import {
-  throwOnConsoleErrorsEverywhere,
-  throwOnConsoleWarningsEverywhere
-} from './util/throwOnConsole';
+import {throwOnConsoleErrorsEverywhere} from './util/throwOnConsole';
 import {clearTimeoutsBetweenTests} from './util/clearTimeoutsBetweenTests';
 import Adapter from 'enzyme-adapter-react-16';
 import enzyme from 'enzyme';
@@ -27,7 +24,6 @@ if (!runnable.length) {
 
 describe('unit tests', function() {
   throwOnConsoleErrorsEverywhere();
-  throwOnConsoleWarningsEverywhere();
   clearTimeoutsBetweenTests();
   runnable.forEach(testsContext);
 });
