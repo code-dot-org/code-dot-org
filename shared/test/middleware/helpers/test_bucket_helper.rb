9387db997e710ccc175da6e7c010164ee2d07e7a
Bethany Connor <bethany@code.org>
2021-08-12 13:15:44 -0700

    Add a test for bucket_helper

--- shared/test/middleware/helpers/test_bucket_helper.rb
+++ shared/test/middleware/helpers/test_bucket_helper.rb
@@ -10,4 +10,16 @@ class BucketHelperTest < Minitest::Test
     safe_string = 'a-b-c-d-e-f-g-h'
     assert_equal safe_string, BucketHelper.replace_unsafe_chars(unsafe_string)
   end
+
+  def test_can_add_persist_indefinitely_tag
+    bucket_helper = BucketHelper.new('bucket', '/tmp')
+    mock_s3 = mock
+    mock_s3.expects(:put_object).with(bucket: 'bucket', key: '/tmp/1/2/filename', body: {}, metadata: {abuse_score: '0'}, tagging: 'persistIndefinitely:true').returns({status: 200})
+    mock_s3.expects(:delete_object).never
+    bucket_helper.s3 = mock_s3
+    bucket_helper.expects(:storage_decrypt_channel_id).returns([1, 2])
+
+    resp = bucket_helper.create_or_replace('encrypted', 'filename', {}, nil, 0, 'persistIndefinitely:true')
+    assert_equal({status: 200}, resp)
+  end
 end
