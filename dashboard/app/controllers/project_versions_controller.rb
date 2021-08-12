45cdf73df6be97df30eb8381e6dcf5136eb06087
Bethany Connor <bethany@code.org>
2021-08-12 10:40:26 -0700

    Use persistIndefinitely and fix getting the channel id in the controller

--- dashboard/app/controllers/project_versions_controller.rb
+++ dashboard/app/controllers/project_versions_controller.rb
@@ -1,11 +1,11 @@
 class ProjectVersionsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
-  before_action :decrypt_channel_id, only: [:create, :project_comments]
 
   # POST /project_versions
   def create
-    project_version = ProjectVersion.new(storage_app_id: @storage_app_id, object_version_id: params[:version_id], comment: params[:comment])
+    _, storage_app_id = storage_decrypt_channel_id(params[:storage_id])
+    project_version = ProjectVersion.new(storage_app_id: storage_app_id, object_version_id: params[:version_id], comment: params[:comment])
     if project_version.save
       return head :ok
     else
