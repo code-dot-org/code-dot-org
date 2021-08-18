5bc5d241c4f59482c081fc2c003a212ab9e2f34b
molly-moen <molly@code.org>
2021-08-18 15:40:49 -0700

    add new files

---
+++ apps/src/javalab/Playground.js
@@ -0,0 +1,67 @@
+import {PlaygroundSignalType, STATUS_MESSAGE_PREFIX} from './constants';
+import javalabMsg from '@cdo/javalab/locale';
+import {assets as assetsApi} from '@cdo/apps/clientApi';
+
+export default class Playground {
+  constructor(onOutputMessage, onNewlineMessage) {
+    this.onOutputMessage = onOutputMessage;
+    this.onNewlineMessage = onNewlineMessage;
+  }
+
+  handleSignal(data) {
+    switch (data.value) {
+      case PlaygroundSignalType.ADD_IMAGE: {
+        this.generateNewImage(data.detail);
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  reset() {
+    this.getAudioElement().src = '';
+    const playground = this.getPlaygroundElement();
+    while (playground.lastElementChild) {
+      playground.removeChild(playground.lastElementChild);
+    }
+  }
+
+  getPlaygroundElement() {
+    return document.getElementById('playground');
+  }
+
+  getAudioElement() {
+    return document.getElementById('playground-audio');
+  }
+
+  onClose() {
+    this.onNewlineMessage();
+    this.onOutputMessage(
+      `${STATUS_MESSAGE_PREFIX} ${javalabMsg.programCompleted()}`
+    );
+    this.onNewlineMessage();
+  }
+
+  generateNewImage(imageData) {
+    let image = document.createElement('img');
+    image.src = assetsApi.basePath(imageData.filename);
+    image.id = imageData.id;
+    image.style.width = this.getPixelValue(imageData.width);
+    image.style.height = this.getPixelValue(imageData.height);
+    image.style.position = 'absolute';
+    image.style.zIndex = imageData.index;
+    image.style.marginTop = this.getPixelValue(imageData.y);
+    image.style.marginLeft = this.getPixelValue(imageData.x);
+    image.addEventListener('click', () => this.handleImageClick(imageData.id));
+    this.getPlaygroundElement().appendChild(image);
+  }
+
+  handleImageClick(imageId) {
+    console.log('got click event for ' + imageId);
+  }
+
+  getPixelValue(configValue) {
+    return configValue * 2 + 'px';
+  }
+}
