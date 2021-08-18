4ebd8dc9d5a920bb9e8cfebbb18b3dde50149817
Bethany Connor <bethany@code.org>
2021-08-17 17:52:03 -0700

    Remove the section update logic from the migration

--- dashboard/db/migrate/20210816222729_add_code_review_enabled_to_sections.rb
+++ dashboard/db/migrate/20210816222729_add_code_review_enabled_to_sections.rb
@@ -1,8 +1,6 @@
 class AddCodeReviewEnabledToSections < ActiveRecord::Migration[5.2]
   def up
     add_column :sections, :code_review_enabled, :boolean
-    Section.reset_column_information
-    Section.update_all(code_review_enabled: true)
   end
 
   def down
