0f0811c293ca6c6406345d986f9ca2e52211167b
anjali <anjali@code.org>
2021-08-19 11:01:50 -0700

    new poems + instructor

--- apps/src/p5lab/spritelab/libraries/PoemBotLibrary.js
+++ apps/src/p5lab/spritelab/libraries/PoemBotLibrary.js
@@ -99,47 +99,156 @@ export default class PoemBotLibrary extends CoreLibrary {
       },
 
       setPoem(key) {
-        if (key === 'wordsworth') {
-          this.poemState = {
-            ...this.poemState,
-            title: 'I Wandered Lonely as a Cloud',
-            author: 'William Wordsworth',
-            lines: [
-              'I wandered lonely as a cloud',
-              "That floats on high o'er vales and hills,",
-              'When all at once I saw a crowd,',
-              'A host, of golden daffodils;',
-              'Beside the lake, beneath the trees,',
-              'Fluttering and dancing in the breeze.'
-            ]
-          };
-        } else if (key === 'dickinson') {
-          this.poemState = {
-            ...this.poemState,
-            title: 'If I can Stop one Heart from Breaking',
-            author: 'Emily Dickinson',
-            lines: [
-              'If I can stop one heart from breaking,',
-              'I shall not live in vain;',
-              'If I can ease one life the aching,',
-              'Or cool one pain,',
-              'Or help one fainting robin',
-              'Unto his nest again,',
-              'I shall not live in vain.'
-            ]
-          };
-        } else if (key === 'silverstein') {
-          this.poemState = {
-            ...this.poemState,
-            title: 'Batty',
-            author: 'Shel Silverstein',
-            lines: [
-              'The baby bat',
-              'Screamed out in fright',
-              "'Turn on the dark;",
-              "I'm afraid of the light.'"
-            ]
-          };
+        switch (key) {
+          case 'hafez':
+            this.poemState = {
+              ...this.poemState,
+              author: 'Hafez',
+              title: 'My Brilliant Image',
+              lines: [
+                'I wish I could show you',
+                'When you are lonely or in darkness, ',
+                'The Astonishing Light ',
+                'Of your own Being!'
+              ]
+            };
+            break;
+          case 'carroll_1':
+            this.poemState = {
+              ...this.poemState,
+              author: 'Lewis Carroll',
+              title: 'Twinkle, Twinkle Little Star',
+              lines: [
+                'Twinkle, twinkle little star,',
+                'How I wonder what you are',
+                'Up above the world so high',
+                'Like a diamond in the sky '
+              ]
+            };
+            break;
+          case 'carroll_2':
+            this.poemState = {
+              ...this.poemState,
+              author: 'Lewis Carroll',
+              title: 'Crocodile',
+              lines: [
+                'How doth the little crocodile',
+                'Improve his shining tail,',
+                'And pour the waters of the Nile',
+                'On every golden scale!',
+                '',
+                'How cheerfully he seems to grin,',
+                'How neatly spreads his claws,',
+                'And welcomes little fishes in,',
+                'With gently smiling jaws!'
+              ]
+            };
+            break;
+          case 'carroll_3':
+            this.poemState = {
+              ...this.poemState,
+              author: 'Lewis Carroll',
+              title: 'Jabberwocky',
+              lines: [
+                "'Twas brillig, and the slithy toves",
+                'Did gyre and gimble in the wabe;',
+                'All mimsy were the borogoves,',
+                'And the mome raths outgrabe.',
+                '',
+                '“Beware the Jabberwock, my son!',
+                'The jaws that bite, the claws that catch!',
+                'Beware the Jubjub bird, and shun',
+                'The frumious Bandersnatch!”'
+              ]
+            };
+            break;
+          case 'rumi':
+            this.poemState = {
+              ...this.poemState,
+              author: 'Rumi',
+              title: 'Sing',
+              lines: [
+                'I want to sing',
+                'Like the birds sing,',
+                'Not worrying about',
+                'Who hears or',
+                'What they think.'
+              ]
+            };
+            break;
+          case 'field':
+            this.poemState = {
+              ...this.poemState,
+              author: 'Eugene Field',
+              title: 'Wynken, Blynken, and Nod',
+              lines: [
+                'Wynken, Blynken, and Nod one night',
+                'Sailed off in a wooden shoe,—',
+                'Sailed on a river of crystal light',
+                'Into a sea of dew.',
+                '',
+                '"Where are you going, and what do you wish?"',
+                'The old moon asked the three.',
+                '"We have come to fish for the herring-fish',
+                'That live in this beautiful sea;',
+                'Nets of silver and gold have we,"',
+                'Said Wynken,',
+                'Blynken,',
+                'And Nod.'
+              ]
+            };
+            break;
+          case 'twain':
+            this.poemState = {
+              ...this.poemState,
+              author: 'Mark Twain',
+              title: 'Warm Summer Sun',
+              lines: [
+                'Warm summer sun,',
+                'Shine kindly here,',
+                'Warm southern wind,',
+                'Blow softly here.',
+                'Green sod above,',
+                'Lie light, lie light.',
+                'Good night, dear heart,',
+                'Good night, good night.'
+              ]
+            };
+            break;
+          case 'wordsworth':
+            this.poemState = {
+              ...this.poemState,
+              author: 'William Wordsworth',
+              title: 'I Wandered Lonely as a Cloud',
+              lines: [
+                'I wandered lonely as a cloud',
+                "That floats on high o'er vales and hills,",
+                'When all at once I saw a crowd,',
+                'A host, of golden daffodils;',
+                'Beside the lake, beneath the trees,',
+                'Fluttering and dancing in the breeze.'
+              ]
+            };
+            break;
+          case 'hughes':
+            this.poemState = {
+              ...this.poemState,
+              author: 'Langston Hughes',
+              title: 'Dream Deferred',
+              lines: [
+                'What happens to a dream deferred? ',
+                'Does it dry up',
+                'LIke a raisin in the sun? ',
+                'Or fester like a sore -- ',
+                'And then run? ',
+                'Does it stink like rotten meat? ',
+                'Or crust and sugar over -- ',
+                'Like a syrupy sweet? ',
+                'Maybe it just sags ',
+                'Like a heavy load. '
+              ]
+            };
+            break;
         }
       },
 
