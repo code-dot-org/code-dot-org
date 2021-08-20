964577ffa746122eb34fa54a3079df396db7fe5e
anjali <anjali@code.org>
2021-08-20 12:49:24 -0700

    fix path

--- apps/src/p5lab/spritelab/poembot/commands/utils.js
+++ apps/src/p5lab/spritelab/poembot/commands/utils.js
@@ -1,4 +1,4 @@
-import {PALETTES} from './constants';
+import {PALETTES} from '../constants';
 // NOTE: min and max are inclusive
 export function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
