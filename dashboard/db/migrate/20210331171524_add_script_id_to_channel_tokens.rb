9a34d9a176c313d598d80190e18645493977a6e2
Maureen <maureen.sturgeon@gmail.com>
2021-08-23 12:39:04 -0700

    remove old migration

--- dashboard/db/migrate/20210331171524_add_script_id_to_channel_tokens.rb
+++
@@ -1,7 +0,0 @@
-class AddScriptIdToChannelTokens < ActiveRecord::Migration[5.2]
-  def change
-    add_column :channel_tokens, :script_id, :integer
-    remove_index :channel_tokens, [:storage_id, :level_id]
-    add_index :channel_tokens, [:storage_id, :level_id, :script_id], unique: true
-  end
-end
