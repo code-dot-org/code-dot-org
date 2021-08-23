2906dfa6b4044d76e60ac9d13e3ee7c28b48655f
Ben Brooks <ben@code.org>
2021-08-23 11:11:44 -0700

    Refactor params for workshop survey foorm submission test

--- dashboard/test/controllers/api/v1/pd/workshop_survey_foorm_submissions_controller_test.rb
+++ dashboard/test/controllers/api/v1/pd/workshop_survey_foorm_submissions_controller_test.rb
@@ -6,10 +6,7 @@ class Api::V1::Pd::WorkshopSurveyFoormSubmissionsControllerTest < ::ActionContro
     @user = create :user
     @pd_summer_workshop = create :csp_summer_workshop
     @foorm_form = create :foorm_form
-  end
-
-  test 'can create and save survey submission' do
-    params = {
+    @default_params = {
       answers: {
         question1: 'answer1'
       },
@@ -20,8 +17,10 @@ class Api::V1::Pd::WorkshopSurveyFoormSubmissionsControllerTest < ::ActionContro
       form_name: @foorm_form.name,
       form_version: @foorm_form.version
     }
+  end
 
-    response = post :create, params: params
+  test 'can create and save survey submission' do
+    response = post :create, params: @default_params
     assert_response :created
     response_body = JSON.parse(response.body)
     assert_not_nil response_body['submission_id']
@@ -40,17 +39,7 @@ class Api::V1::Pd::WorkshopSurveyFoormSubmissionsControllerTest < ::ActionContro
   end
 
   test 'return conflict if user had already submitted a response' do
-    params = {
-      answers: {
-        question1: 'answer1'
-      },
-      user_id: @user.id,
-      pd_workshop_id: @pd_summer_workshop.id,
-      pd_session_id: nil,
-      day: nil,
-      form_name: nil,
-      form_version: @foorm_form.version
-    }
+    params = @default_params.merge({day: nil, form_name: nil})
 
     response = post :create, params: params
     assert_response :created
@@ -63,17 +52,7 @@ class Api::V1::Pd::WorkshopSurveyFoormSubmissionsControllerTest < ::ActionContro
   end
 
   test 'return conflict if user had already submitted a response, using all params' do
-    params = {
-      answers: {
-        question1: 'answer1'
-      },
-      user_id: @user.id,
-      pd_workshop_id: @pd_summer_workshop.id,
-      pd_session_id: 123,
-      day: 0,
-      form_name: @foorm_form.name,
-      form_version: @foorm_form.version
-    }
+    params = @default_params.merge({pd_session_id: 123})
 
     response = post :create, params: params
     assert_response :created
