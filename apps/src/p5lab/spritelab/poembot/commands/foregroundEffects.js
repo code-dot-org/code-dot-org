2c1c93f172396e74dfa1807dd1f63e7061d7009b
anjali <anjali@code.org>
2021-08-24 17:36:18 -0700

    starburst, confetti

--- apps/src/p5lab/spritelab/poembot/commands/foregroundEffects.js
+++ apps/src/p5lab/spritelab/poembot/commands/foregroundEffects.js
@@ -1,6 +1,7 @@
 import drawHeart from '@code-dot-org/dance-party/src/shapes/heart';
 import drawLovestruck from '@code-dot-org/dance-party/src/shapes/lovestruck';
 import drawSmiley from '@code-dot-org/dance-party/src/shapes/smiley';
+import drawStar from '@code-dot-org/dance-party/src/shapes/star';
 import drawStarstruck from '@code-dot-org/dance-party/src/shapes/starstruck';
 import drawTickled from '@code-dot-org/dance-party/src/shapes/tickled';
 import drawWink from '@code-dot-org/dance-party/src/shapes/wink';
@@ -140,10 +141,72 @@ export const commands = {
 
         break;
       }
-      case 'confetti':
+      case 'confetti': {
+        let confetti = [];
+        this.foregroundEffect = () => {
+          this.p5.push();
+          confetti.push({
+            x: utils.randomInt(0, 400),
+            y: -10,
+            velocityX: this.p5.random(-2, 2),
+            size: this.p5.random(6, 12, 18),
+            spin: 1,
+            color: utils.randomColor(this.p5)
+          });
+          this.p5.noStroke();
+          confetti.forEach(confetto => {
+            this.p5.push();
+            this.p5.fill(confetto.color);
+            this.p5.translate(confetto.x, confetto.y);
+            this.p5.scale(this.p5.sin(confetto.spin), 1);
+            confetto.spin += 20;
+            this.p5.rect(0, 0, 4, confetto.size);
+            const fallSpeed = this.p5.map(confetto.size, 6, 12, 1, 3);
+            confetto.y += fallSpeed;
+            confetto.x += confetto.velocityX;
+            this.p5.pop();
+          });
+          confetti = confetti.filter(confetto => confetto.y < 420);
+
+          this.p5.pop();
+        };
         break;
-      case 'starburst':
+      }
+      case 'starburst': {
+        let stars = [];
+        const resetStars = () => {
+          for (let i = 0; i < 100; i++) {
+            const theta = utils.randomInt(0, 360);
+            const velocity = utils.randomInt(4, 12);
+            stars.push({
+              color: utils.randomColor(this.p5),
+              x: 200,
+              y: 200,
+              velocityX: velocity * this.p5.cos(theta),
+              velocityY: velocity * this.p5.sin(theta)
+            });
+          }
+        };
+        this.foregroundEffect = () => {
+          this.p5.push();
+          if (stars.length === 0) {
+            resetStars();
+          }
+
+          stars.forEach(star => {
+            this.p5.fill(star.color);
+            drawStar(this.p5, star.x, star.y, 3, 9, 5);
+            star.x += star.velocityX;
+            star.y += star.velocityY;
+          });
+          stars = stars.filter(
+            star => star.x > -10 && star.x < 410 && star.y > -10 && star.y < 410
+          );
+          this.p5.pop();
+        };
+
         break;
+      }
       case 'glass':
         break;
       case 'smoke':
