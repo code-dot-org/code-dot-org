9a940566e476eae7082a7488de53933404ca5a69
dju90 <bryan@code.org>
2021-08-31 13:10:34 -0700

    add spacing before school info formgroup

--- apps/src/code-studio/pd/international_opt_in/InternationalOptIn.jsx
+++ apps/src/code-studio/pd/international_opt_in/InternationalOptIn.jsx
@@ -325,6 +325,7 @@ class InternationalOptInComponent extends FormComponent {
 
     return (
       <FormGroup>
+        <br />
         <h4>{i18n.yourSchoolTellUs()}</h4>
         {this.buildSelectFieldGroupFromOptions({
           name: 'schoolCountry',
