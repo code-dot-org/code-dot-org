72817a1ce26f55977a9927971ef627e69bb3c90b
Erin Peach <erin@code.org>
2021-08-30 17:16:43 -0700

    Display text for current sprites in library

--- apps/src/code-studio/assets/SelectStartAnimations.jsx
+++ apps/src/code-studio/assets/SelectStartAnimations.jsx
@@ -1,11 +1,86 @@
 import React from 'react';
+import {getManifest} from '@cdo/apps/assetManagement/animationLibraryApi';
 
 export default class SelectStartAnimations extends React.Component {
+  state = {
+    spritesByCategory: {},
+    animations: [],
+    startAnimations: {},
+    expandedCategory: ''
+  };
+
+  componentDidMount() {
+    getManifest('spritelab')
+      .then(sprites => {
+        // let orderedList = Array.from(spriteDefault['default_sprites']);
+        // this.setState({defaultList: orderedList, isLoading: false});
+        const categories = sprites['categories'];
+        this.setState({spritesByCategory: categories});
+        let spriteList = [];
+        Object.keys(categories).map(category => {
+          spriteList = spriteList.concat(categories[category]);
+        });
+        this.setState({animations: spriteList});
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  expandCategory = category => {
+    this.setState({expandedCategory: category});
+  };
+
+  renderCategories = () => {
+    const categories = Object.keys(this.state.spritesByCategory);
+    return categories.map(category => {
+      return (
+        <div key={category}>
+          <h5>
+            <a onClick={() => this.expandCategory(category)}>{category}</a>
+          </h5>
+        </div>
+      );
+    });
+  };
+
+  renderSpecificCategory = category => {
+    const sprites = this.state.spritesByCategory[category];
+    return sprites.map(sprite => {
+      return <p key={sprite}>{sprite}</p>;
+    });
+  };
+
+  displayExpandedCategory = () => {
+    const {spritesByCategory, expandedCategory} = this.state;
+    let category = spritesByCategory[expandedCategory];
+    if (!category) {
+      return;
+    }
+
+    return category.map(sprite => {
+      return <p key={sprite}>{sprite}</p>;
+    });
+  };
+
   render() {
     return (
       <div>
         <h3>Select Starting Animations</h3>
+        <div style={styles.categoryRows}>
+          <div>{this.renderCategories()}</div>
+
+          <div>{this.displayExpandedCategory()}</div>
+        </div>
       </div>
     );
   }
 }
+
+const styles = {
+  categoryRows: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start'
+  }
+};
