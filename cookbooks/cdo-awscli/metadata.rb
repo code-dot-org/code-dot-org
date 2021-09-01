0be1703005cfbfcf98815dba172f977df23e3988
Continuous Integration <dev@code.org>
2021-09-01 16:23:01 +0000

    Updated cookbook versions

--- cookbooks/cdo-awscli/metadata.rb
+++ cookbooks/cdo-awscli/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@code.org'
 license          'All rights reserved'
 description      'Installs/Configures AWS command line interface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.13'
+version          '0.1.14'
 
 depends 'apt'
 depends 'poise-python'
