24a824b68517ebd0db2f45174bff23367a825c32
Maureen <maureen.sturgeon@gmail.com>
2021-09-01 10:11:17 -0700

    add LevelFeedback tests

--- apps/src/templates/feedback/LevelFeedback.jsx
+++ apps/src/templates/feedback/LevelFeedback.jsx
@@ -2,18 +2,18 @@ import React, {Component} from 'react';
 import i18n from '@cdo/locale';
 import color from '@cdo/apps/util/color';
 import {levelFeedbackShape} from './types';
-import SingleFeedback from '@cdo/apps/templates/feedback/SingleFeedback';
+import LevelFeedbackEntry from '@cdo/apps/templates/feedback/LevelFeedbackEntry';
 import Button from '@cdo/apps/templates/Button';
 
 export default class LevelFeedback extends Component {
   static propTypes = levelFeedbackShape;
 
   state = {
-    showPastComments: false
+    showingPastComments: false
   };
 
   toggleshowPastComments = () => {
-    this.setState({showPastComments: !this.state.showPastComments});
+    this.setState({showingPastComments: !this.state.showingPastComments});
   };
 
   render() {
@@ -26,39 +26,38 @@ export default class LevelFeedback extends Component {
       feedbacks
     } = this.props;
 
-    const {showPastComments} = this.state;
+    const {showingPastComments} = this.state;
 
-    const displayedFeedbacks = showPastComments
+    const displayedFeedbacks = showingPastComments
       ? feedbacks
       : feedbacks.slice(0, 1);
 
     const hasMultipleFeedbacks = feedbacks.length > 1;
-
     const marginBottom = hasMultipleFeedbacks ? 8 : 24;
 
     return (
       <div style={{...styles.levelEntry, marginBottom}}>
         <div style={styles.lessonDetails}>
-          <a href={linkToLevel}>
-            <div style={styles.lessonLevel}>
-              {i18n.feedbackNotificationLesson({
-                lessonNum,
-                lessonName,
-                levelNum
-              })}
-            </div>
+          <a href={linkToLevel} style={styles.lessonLevel}>
+            {i18n.feedbackNotificationLesson({
+              lessonNum,
+              lessonName,
+              levelNum
+            })}
           </a>
           <div style={styles.unit}>
             {i18n.feedbackNotificationUnit({unitName})}
           </div>
         </div>
         {displayedFeedbacks.map(feedback => (
-          <SingleFeedback feedback={feedback} />
+          <LevelFeedbackEntry feedback={feedback} key={feedback.id} />
         ))}
         {hasMultipleFeedbacks && (
           <Button
             text={
-              showPastComments ? 'Hide past comments' : 'Show past comments'
+              showingPastComments
+                ? i18n.hidePastComments()
+                : i18n.showPastComments()
             }
             onClick={this.toggleshowPastComments}
             displayAsText={true}
@@ -80,7 +79,6 @@ const styles = {
   lessonLevel: {
     fontSize: 18,
     lineHeight: '24px',
-    marginBottom: 4,
     color: color.teal,
     fontFamily: '"Gotham 5r", sans-serif'
   },
@@ -88,7 +86,8 @@ const styles = {
     color: color.dark_charcoal,
     fontSize: 14,
     lineHeight: '17px',
-    marginBottom: 8
+    marginBottom: 8,
+    marginTop: 4
   },
   showPastComments: {
     float: 'right',
