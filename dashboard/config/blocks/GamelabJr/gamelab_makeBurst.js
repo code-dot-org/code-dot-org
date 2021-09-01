bd137bf4394745cbe0b5ed6d7771b413ffc258da
Continuous Integration <dev@code.org>
2021-09-01 19:00:06 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/blocks/GamelabJr/gamelab_makeBurst.js
+++ dashboard/config/blocks/GamelabJr/gamelab_makeBurst.js
@@ -13,13 +13,13 @@ function makeBurst(num,costume,effect) {
     else if(effect=="rain"){
       setProp({name: 'temporarySprite'}, "speed", 0);
       setProp({name: 'temporarySprite'}, "scale", 50);
-      jumpTo({name: 'temporarySprite'}, locationAt(math_random_int(0, 400), math_random_int(450, 650)));
-      setProp({name: 'temporarySprite'}, "rotation", math_random_int(0, 359));
+      jumpTo({name: 'temporarySprite'}, locationAt(math_random_int(0, 400), math_random_int(450, 550)));
+      setProp({name: 'temporarySprite'}, "rotation", math_random_int(-10, 10));
       addBehaviorSimple({name: 'temporarySprite'}, new Behavior(rainEffectBehavior, []));
     }
     else if(effect=="burst"){
-      setProp({name: 'temporarySprite'}, "speed", math_random_int(5, 15));
-      setProp({name: 'temporarySprite'}, "delay", math_random_int(1, 60));
+      setProp({name: 'temporarySprite'}, "speed", math_random_int(10, 20));
+      setProp({name: 'temporarySprite'}, "delay", math_random_int(1, 20));
       setProp({name: 'temporarySprite'}, "scale", 0);
       setProp({name: 'temporarySprite'}, "direction", math_random_int(0, 359));
       setProp({name: 'temporarySprite'}, "rotation", math_random_int(0, 359));
@@ -32,22 +32,20 @@ function makeBurst(num,costume,effect) {
 }
 
 function burstEffectBehavior(this_sprite) {
-  console.log(getProp(this_sprite, "delay"));
-  if(getProp(this_sprite, "delay")>0){
-    changePropBy(this_sprite, "delay", -1);
-  } else if(getProp(this_sprite, "delay")===0){
-    setProp({name: 'temporarySprite'}, "scale", 10);
-  } else{
-  moveForward(this_sprite, getProp(this_sprite, "speed"));
-  changePropBy(this_sprite, "scale", 2);
-  changePropBy(this_sprite, "rotation", 1);
-  if (getProp(this_sprite, "y") < -100 || 
-     getProp(this_sprite, "y") > 500 || 
-     getProp(this_sprite, "x") < -100 || 
-     getProp(this_sprite, "x") > 500) {
-    destroy(this_sprite);
+  if(getProp(this_sprite, "delay")===0){
+    setProp(this_sprite, "scale", 40);
+  } else if(getProp(this_sprite, "delay")<0){
+    moveForward(this_sprite, getProp(this_sprite, "speed"));
+    changePropBy(this_sprite, "scale", 1);
+    changePropBy(this_sprite, "rotation", 1);
+    if (getProp(this_sprite, "y") < -100 || 
+        getProp(this_sprite, "y") > 500 || 
+        getProp(this_sprite, "x") < -100 || 
+        getProp(this_sprite, "x") > 500) {
+      destroy(this_sprite);
+    }
   }
-  }
+  changePropBy(this_sprite, "delay", -1);
 }
 
 function popEffectBehavior(this_sprite) {
@@ -63,7 +61,7 @@ function popEffectBehavior(this_sprite) {
 function rainEffectBehavior(this_sprite) {
   changePropBy(this_sprite, "y", getProp(this_sprite, "speed"));
   changePropBy(this_sprite, "speed", -0.5);
-  changePropBy(this_sprite, "rotation", 5);
+  changePropBy(this_sprite, "rotation", math_random_int(-5, 5));
   if (getProp(this_sprite, "y") < -100) {
     destroy(this_sprite);
   }
