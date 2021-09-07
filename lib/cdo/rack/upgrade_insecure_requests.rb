f43bc3e13212247bbdd0e08df7a2ebb450e3c3f3
molly-moen <molly@code.org>
2021-09-07 11:49:48 -0700

    add url to upgrade requests

--- lib/cdo/rack/upgrade_insecure_requests.rb
+++ lib/cdo/rack/upgrade_insecure_requests.rb
@@ -51,7 +51,7 @@ module Rack
             "img-src 'self' https: data: blob: https://*.code.org https://javabuilderbeta-output.dev-code.org",
             "font-src 'self' https: data:",
             "connect-src 'self' https: https://api.pusherapp.com wss://ws.pusherapp.com wss://*.firebaseio.com http://localhost:8080 https://curriculum.code.org/ wss://*.code.org wss://javabuilderbeta.dev-code.org",
-            "media-src 'self' https: data: https://*.code.org http://vaas.acapela-group.com https://javabuilderbeta-output.dev-code.org",
+            "media-src 'self' https: data: https://*.code.org http://vaas.acapela-group.com https://javabuilderbeta-output.dev-code.org wss://javabuilder-molly.dev-code.org",
             "report-uri #{CDO.code_org_url('https/mixed-content')}"
           ]
         end
