5f2b5194d8bb9ea57eaf320ca19cd57c9b528a01
Will Jordan <will.jordan@gmail.com>
2021-09-08 14:39:46 -0700

    further simplify varnish VCL

--- cookbooks/cdo-varnish/attributes/default.rb
+++ cookbooks/cdo-varnish/attributes/default.rb
@@ -1,7 +1,4 @@
 default['cdo-varnish'] = {
-  'backends' => {
-    'localhost' => '127.0.0.1',
-  },
   'secret' => '00000000-0000-0000-0000-000000000000',
   redirects: {
     'forums.code.org'   => 'support.code.org',
