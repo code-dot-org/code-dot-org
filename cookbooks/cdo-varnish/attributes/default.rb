88a29cb2b34ed6e3282666f9337c03097430a761
Will Jordan <will.jordan@gmail.com>
2021-09-09 16:06:41 -0700

    restore original varnish vcl toggle Varnish behavior with `node['cdo-varnish']['enabled']`

--- cookbooks/cdo-varnish/attributes/default.rb
+++ cookbooks/cdo-varnish/attributes/default.rb
@@ -1,5 +1,8 @@
 default['cdo-varnish'] = {
-  enabled: false,
+  enabled: true,
+  'backends' => {
+    'localhost' => '127.0.0.1',
+  },
   'secret' => '00000000-0000-0000-0000-000000000000',
   redirects: {
     'forums.code.org'   => 'support.code.org',
