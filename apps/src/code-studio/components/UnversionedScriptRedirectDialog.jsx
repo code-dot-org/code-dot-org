d22aed2dfecebff7b4278fd79dde6eb2256eaa3c
Bethany Connor <bethany@code.org>
2021-09-09 14:56:04 -0700

    Add UI test for dialog

--- apps/src/code-studio/components/UnversionedScriptRedirectDialog.jsx
+++ apps/src/code-studio/components/UnversionedScriptRedirectDialog.jsx
@@ -7,18 +7,20 @@ import StylizedBaseDialog, {
 export default function UnversionedScriptRedirectDialog(props) {
   const [isOpen, setIsOpen] = useState(true);
   return (
-    <StylizedBaseDialog
-      title={i18n.warning()}
-      body={i18n.unversionedRedirectWarning_June2022()}
-      renderFooter={() => (
-        <FooterButton
-          type="confirm"
-          onClick={() => setIsOpen(false)}
-          text={i18n.dialogOK()}
-        />
-      )}
-      handleClose={() => setIsOpen(false)}
-      isOpen={isOpen}
-    />
+    <div id="ui-test-unversioned-script-redirect-dialog">
+      <StylizedBaseDialog
+        title={i18n.warning()}
+        body={i18n.unversionedRedirectWarning_June2022()}
+        renderFooter={() => (
+          <FooterButton
+            type="confirm"
+            onClick={() => setIsOpen(false)}
+            text={i18n.dialogOK()}
+          />
+        )}
+        handleClose={() => setIsOpen(false)}
+        isOpen={isOpen}
+      />
+    </div>
   );
 }
