b3c70c45adc0a980301f3c14c037496d4ae719cd
Maureen <maureen.sturgeon@gmail.com>
2021-09-09 13:25:16 -0700

    update all feedbacks for PR feedback, move to hooks

--- apps/src/templates/feedback/LevelFeedback.jsx
+++ apps/src/templates/feedback/LevelFeedback.jsx
@@ -1,72 +1,58 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import i18n from '@cdo/locale';
 import color from '@cdo/apps/util/color';
 import {levelFeedbackType} from './types';
 import LevelFeedbackEntry from '@cdo/apps/templates/feedback/LevelFeedbackEntry';
 import Button from '@cdo/apps/templates/Button';
 
-export default class LevelFeedback extends Component {
-  static propTypes = levelFeedbackType;
+function LevelFeedback({
+  lessonName,
+  lessonNum,
+  levelNum,
+  linkToLevel,
+  unitName,
+  feedbacks
+}) {
+  const [showingOlderComments, setShowOlderComments] = useState(false);
 
-  state = {
-    showingOlderComments: false
-  };
+  const displayedFeedbacks = showingOlderComments
+    ? feedbacks
+    : feedbacks.slice(0, 1);
 
-  toggleshowOlderComments = () => {
-    this.setState({showingOlderComments: !this.state.showingOlderComments});
-  };
+  const hasMultipleFeedbacks = feedbacks.length > 1;
+  const marginBottom = hasMultipleFeedbacks ? 8 : 24;
 
-  render() {
-    const {
-      lessonName,
-      lessonNum,
-      levelNum,
-      linkToLevel,
-      unitName,
-      feedbacks
-    } = this.props;
-
-    const {showingOlderComments} = this.state;
-
-    const displayedFeedbacks = showingOlderComments
-      ? feedbacks
-      : feedbacks.slice(0, 1);
-
-    const hasMultipleFeedbacks = feedbacks.length > 1;
-    const marginBottom = hasMultipleFeedbacks ? 8 : 24;
-
-    return (
-      <div style={{...styles.levelEntry, marginBottom}}>
-        <div style={styles.lessonDetails}>
-          <a href={linkToLevel} style={styles.lessonLevel}>
-            {i18n.feedbackNotificationLesson({
-              lessonNum,
-              lessonName,
-              levelNum
-            })}
-          </a>
-          <div style={styles.unit}>
-            {i18n.feedbackNotificationUnit({unitName})}
-          </div>
+  return (
+    <div style={{...styles.levelEntry, marginBottom}}>
+      <div style={styles.lessonDetails}>
+        <a href={linkToLevel} style={styles.lessonLevel}>
+          {i18n.feedbackNotificationLesson({
+            lessonNum,
+            lessonName,
+            levelNum
+          })}
+        </a>
+        <div style={styles.unit}>
+          {i18n.feedbackNotificationUnit({unitName})}
         </div>
-        {displayedFeedbacks.map(feedback => (
-          <LevelFeedbackEntry feedback={feedback} key={feedback.id} />
-        ))}
-        {hasMultipleFeedbacks && (
-          <Button
-            text={
-              showingOlderComments
-                ? i18n.hideOlderComments()
-                : i18n.showOlderComments()
-            }
-            onClick={this.toggleshowOlderComments}
-            styleAsText={true}
-            style={styles.showOlderComments}
-          />
-        )}
       </div>
-    );
-  }
+      {displayedFeedbacks.map(feedback => (
+        <LevelFeedbackEntry feedback={feedback} key={feedback.id} />
+      ))}
+      {hasMultipleFeedbacks && (
+        <Button
+          text={
+            showingOlderComments
+              ? i18n.hideOlderComments()
+              : i18n.showOlderComments()
+          }
+          onClick={() => setShowOlderComments(!showingOlderComments)}
+          styleAsText={true}
+          style={styles.showOlderComments}
+        />
+      )}
+    </div>
+  );
 }
 
 const styles = {
@@ -96,3 +82,7 @@ const styles = {
     fontSize: 16
   }
 };
+
+LevelFeedback.propTypes = levelFeedbackType;
+
+export default LevelFeedback;
