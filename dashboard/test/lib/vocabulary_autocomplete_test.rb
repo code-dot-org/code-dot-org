1fddd8e409b78d3c7d0d6ce118f319153f59380a
Bethany Connor <bethany@code.org>
2021-09-13 07:15:05 -0700

    Remove course_version fixture

--- dashboard/test/lib/vocabulary_autocomplete_test.rb
+++ dashboard/test/lib/vocabulary_autocomplete_test.rb
@@ -6,39 +6,39 @@ class VocabularyAutocompleteTest < ActiveSupport::TestCase
   # as fixtures. These are defined in test/fixtures/vocabulary.yml
 
   setup do
-    @coursea_2017 = CourseOffering.find_by_key('coursea').course_versions.find_by_key('2017')
-    @courseb_2017 = CourseOffering.find_by_key('courseb').course_versions.find_by_key('2017')
+    @course_version_2018 = CourseVersion.find_by_key('2018')
+    @course_version_2019 = CourseVersion.find_by_key('2019')
   end
 
   test "finds vocabulary with matching word" do
-    matches = VocabularyAutocomplete.get_search_matches('programming', 5, @coursea_2017.id)
+    matches = VocabularyAutocomplete.get_search_matches('programming', 5, @course_version_2018.id)
     assert_equal 1, matches.length
     assert_equal 'Programming', matches[0][:word]
     assert_equal 'The art of creating a program.', matches[0][:definition]
   end
 
   test "finds vocabulary with match definition" do
-    matches = VocabularyAutocomplete.get_search_matches('fixing', 5, @coursea_2017)
+    matches = VocabularyAutocomplete.get_search_matches('fixing', 5, @course_version_2018)
     assert_equal 1, matches.length
     assert_equal 'Debugging', matches[0][:word]
   end
 
   test "finds multiple matches" do
-    matches = VocabularyAutocomplete.get_search_matches('pro', 5, @coursea_2017)
+    matches = VocabularyAutocomplete.get_search_matches('pro', 5, @course_version_2018)
     assert_equal 2, matches.length
     assert_equal ['Debugging', 'Programming'], matches.map {|m| m[:word]}
   end
 
   test "restricts matches by limit" do
-    matches = VocabularyAutocomplete.get_search_matches('pro', 1, @coursea_2017)
+    matches = VocabularyAutocomplete.get_search_matches('pro', 1, @course_version_2018)
     assert_equal 1, matches.length
   end
 
   test "restricts matches by course version id" do
     # There's a definition for algorithm in two course versions
     # We should only get the one we requested
-    matches = VocabularyAutocomplete.get_search_matches('algorithm', 1, @courseb_2017)
+    matches = VocabularyAutocomplete.get_search_matches('algorithm', 1, @course_version_2019)
     assert_equal 1, matches.length
-    assert_equal @courseb_2017.id, Vocabulary.find(matches[0][:id]).course_version_id
+    assert_equal @course_version_2019.id, Vocabulary.find(matches[0][:id]).course_version_id
   end
 end
