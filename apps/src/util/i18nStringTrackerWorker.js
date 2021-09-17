c2326b786bfa6d80ca03aa32a573e00860cea035
Steffan Musoke <steffanmusoke@gmail.com>
2021-09-17 14:59:32 -0500

    docs: added documentation

--- apps/src/util/i18nStringTrackerWorker.js
+++ apps/src/util/i18nStringTrackerWorker.js
@@ -66,6 +66,7 @@ class I18nStringTrackerWorker {
     this.buffer = {};
     this.pendingFlush = null;
 
+    // RNG to send only 1% of the time
     if (Math.floor(Math.random() * 100) === 0) {
       // Record the i18n string usage data.
       sendRecords(records);
