36f9cc66402d2ed9ce155139262b05057b981e05
anjali <anjali@code.org>
2021-09-20 12:20:11 -0700

    poetry level

---
+++ dashboard/app/models/levels/poetry.rb
@@ -0,0 +1,53 @@
+# == Schema Information
+#
+# Table name: levels
+#
+#  id                    :integer          not null, primary key
+#  game_id               :integer
+#  name                  :string(255)      not null
+#  created_at            :datetime
+#  updated_at            :datetime
+#  level_num             :string(255)
+#  ideal_level_source_id :bigint           unsigned
+#  user_id               :integer
+#  properties            :text(16777215)
+#  type                  :string(255)
+#  md5                   :string(255)
+#  published             :boolean          default(FALSE), not null
+#  notes                 :text(65535)
+#  audit_log             :text(65535)
+#
+# Indexes
+#
+#  index_levels_on_game_id    (game_id)
+#  index_levels_on_level_num  (level_num)
+#  index_levels_on_name       (name)
+#
+
+class Poetry < GamelabJr
+  def self.skins
+    ['poetry']
+  end
+
+  def self.create_from_level_builder(params, level_params)
+    create!(
+      level_params.merge(
+        user: params[:user],
+        game: Game.poetry,
+        level_num: 'custom',
+        properties: {
+          block_pools: [
+            "PoemBot",
+          ],
+          helper_libraries: [],
+          hide_animation_mode: true,
+          show_type_hints: true,
+          use_modal_function_editor: true,
+        }
+      )
+    )
+  end
+
+  def common_blocks(type)
+  end
+end
