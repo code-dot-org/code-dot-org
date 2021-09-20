9bac21190cf7adf737c831542c7b49a1bf70854f
molly-moen <molly@code.org>
2021-09-20 15:09:31 -0700

    setup signal handling

--- apps/src/javalab/Playground.js
+++ apps/src/javalab/Playground.js
@@ -1,1 +1,24 @@
-export default class Playground {}
+import {PlaygroundSignalType} from './constants';
+
+export default class Playground {
+  constructor(onOutputMessage, onNewlineMessage, onInputMessage) {
+    this.onOutputMessage = onOutputMessage;
+    this.onNewlineMessage = onNewlineMessage;
+    this.onInputMessage = onInputMessage;
+  }
+
+  handleSignal(data) {
+    switch (data.value) {
+      case PlaygroundSignalType.RUN:
+      case PlaygroundSignalType.EXIT:
+      case PlaygroundSignalType.ADD_CLICKABLE_ITEM:
+      case PlaygroundSignalType.ADD_IMAGE_ITEM:
+      case PlaygroundSignalType.ADD_TEXT_ITEM:
+      case PlaygroundSignalType.CHANGE_ITEM:
+      case PlaygroundSignalType.PLAY_SOUND:
+      case PlaygroundSignalType.REMOVE_ITEM:
+      case PlaygroundSignalType.SET_BACKGROUND_IMAGE:
+        break;
+    }
+  }
+}
