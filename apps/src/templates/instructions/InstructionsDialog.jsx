c0056e19a06e7ad128ec6baa38bc091abdef164c
Madelyn Kasula <maddie.kasula@code.org>
2021-09-21 16:33:55 -0700

    Add InstructionsDialog

---
+++ apps/src/templates/instructions/InstructionsDialog.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import i18n from '@cdo/locale';
+import StylizedBaseDialog, {
+  FooterButton
+} from '@cdo/apps/componentLibrary/StylizedBaseDialog';
+import DialogInstructions from '@cdo/apps/templates/instructions/DialogInstructions';
+import {closeDialog} from '@cdo/apps/redux/instructionsDialog';
+
+export function InstructionsDialog(props) {
+  return (
+    <StylizedBaseDialog
+      isOpen={props.isOpen}
+      title={
+        props.title &&
+        props.showTitle && <h1 style={{fontSize: 24}}>{props.title}</h1>
+      }
+      // TODO: make this scrollable instead of entire dialog scrolling?
+      body={<DialogInstructions />}
+      renderFooter={() => (
+        <FooterButton
+          type="confirm"
+          text={i18n.dialogOK()}
+          onClick={props.handleClose}
+        />
+      )}
+      handleClose={props.handleClose}
+      type="simple"
+    />
+  );
+}
+
+InstructionsDialog.propTypes = {
+  title: PropTypes.string,
+
+  // Provided by Redux.
+  isOpen: PropTypes.bool,
+  handleClose: PropTypes.func.isRequired,
+  showTitle: PropTypes.bool
+};
+
+InstructionsDialog.defaultProps = {
+  showTitle: true
+};
+
+export default connect(
+  state => ({
+    isOpen: state.instructionsDialog.open,
+    // The presence of longInstructions determines showTitle because
+    // the grandchild of this component, <Instructions/>, uses this same calculation
+    // in its renderMainBody method. This avoids rendering the title twice.
+    showTitle: !!state.instructions.longInstructions
+  }),
+  dispatch => ({
+    handleClose: () => dispatch(closeDialog())
+  })
+)(InstructionsDialog);
