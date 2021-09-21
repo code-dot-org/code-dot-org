038fd60fc1ead95ce139c9d6b2d26c291dd54907
Bethany Connor <bethany@code.org>
2021-09-21 15:36:50 -0700

    Create a page to create a new programming expression

---
+++ apps/src/sites/studio/pages/programming_expressions/new.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import getScriptData from '@cdo/apps/util/getScriptData';
+import NewProgrammingExpressionForm from '@cdo/apps/lib/levelbuilder/code-docs-editor/NewProgrammingExpressionForm';
+
+$(document).ready(() => {
+  const programmingEnvironmentsForSelect = getScriptData(
+    'programmingEnvironmentsForSelect'
+  );
+  console.log(programmingEnvironmentsForSelect);
+  ReactDOM.render(
+    <NewProgrammingExpressionForm
+      programmingEnvironmentsForSelect={programmingEnvironmentsForSelect}
+    />,
+    document.getElementById('form')
+  );
+});
--- dashboard/app/controllers/programming_expressions_controller.rb
+++ dashboard/app/controllers/programming_expressions_controller.rb
@@ -1,4 +1,6 @@
 class ProgrammingExpressionsController < ApplicationController
+  before_action :require_levelbuilder_mode_or_test_env, except: [:search]
+
   # GET /programming_expressions/search
   def search
     programming_environment =
@@ -8,5 +10,16 @@ class ProgrammingExpressionsController < ApplicationController
         ProgrammingEnvironment.find_by(name: params[:programmingEnvironmentName])
       end
     render json: ProgrammingExpressionAutocomplete.get_search_matches(params[:page], params[:query], programming_environment)
+  end
+
+  def new
+    @programming_environments_for_select = ProgrammingEnvironment.all.map {|env| {id: env.id, name: env.name}}
+    puts @programming_environments_for_select.inspect
+  end
+
+  def create
+    puts params[:key]
+    puts params[:programming_environment_id]
+    ProgrammingExpression.create!(key: params[:key], name: params[:key], programming_environment_id: params[:programming_environment_id])
   end
 end
