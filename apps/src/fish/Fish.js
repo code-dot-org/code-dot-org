1db7ca2c07916d1d32d1e87b741faca5d8370fe7
anjali <anjali@code.org>
2021-09-21 17:03:18 -0700

    es6ify fish

--- apps/src/fish/Fish.js
+++ apps/src/fish/Fish.js
@@ -20,120 +20,119 @@ const MOBILE_PORTRAIT_WIDTH = 600;
 /**
  * An instantiable Fish class
  */
+export default class Fish {
+  constructor() {
+    this.skin = null;
+    this.level = null;
 
-const Fish = function() {
-  this.skin = null;
-  this.level = null;
-
-  /** @type {StudioApp} */
-  this.studioApp_ = null;
-};
-
-/**
- * Inject the studioApp singleton.
- */
-Fish.prototype.injectStudioApp = function(studioApp) {
-  this.studioApp_ = studioApp;
-};
-
-/**
- * Initialize this WebLab instance.  Called on page load.
- */
-Fish.prototype.init = function(config) {
-  if (!this.studioApp_) {
-    throw new Error('Fish requires a StudioApp');
+    /** @type {StudioApp} */
+    this.studioApp_ = null;
   }
 
-  this.skin = config.skin;
-  this.level = config.level;
+  /**
+   * Inject the studioApp singleton.
+   */
+  injectStudioApp(studioApp) {
+    this.studioApp_ = studioApp;
+  }
 
-  config.makeYourOwn = false;
-  config.wireframeShare = true;
-  config.noHowItWorks = true;
-
-  config.pinWorkspaceToBottom = true;
-
-  const onMount = () => {
-    // NOTE: Other apps call studioApp.init(), except WebLab. Fish is imitating WebLab
-    this.studioApp_.setConfigValues_(config);
-    this.studioApp_.initTimeSpent();
-
-    // NOTE: if we called studioApp_.init(), the code here would be executed
-    // automatically since pinWorkspaceToBottom is true...
-    const container = document.getElementById(config.containerId);
-    const bodyElement = document.body;
-    bodyElement.style.overflow = 'hidden';
-    bodyElement.className = bodyElement.className + ' pin_bottom';
-    container.className = container.className + ' pin_bottom';
-
-    // Fixes viewport for small screens.  Also usually done by studioApp_.init().
-    var viewport = document.querySelector('meta[name="viewport"]');
-    if (viewport) {
-      this.studioApp_.fixViewportForSpecificWidthForSmallScreens_(
-        viewport,
-        MOBILE_PORTRAIT_WIDTH
-      );
+  /**
+   * Initialize this WebLab instance.  Called on page load.
+   */
+  init(config) {
+    if (!this.studioApp_) {
+      throw new Error('Fish requires a StudioApp');
     }
 
-    this.initMLActivities();
-  };
+    this.skin = config.skin;
+    this.level = config.level;
 
-  // Push initial level properties into the Redux store
-  this.studioApp_.setPageConstants(config, {
-    channelId: config.channel,
-    noVisualization: true,
-    visualizationInWorkspace: true,
-    isProjectLevel: !!config.level.isProjectLevel
-  });
+    config.makeYourOwn = false;
+    config.wireframeShare = true;
+    config.noHowItWorks = true;
 
-  ReactDOM.render(
-    <Provider store={getStore()}>
-      <FishView onMount={onMount} />
-    </Provider>,
-    document.getElementById(config.containerId)
-  );
-};
+    config.pinWorkspaceToBottom = true;
 
-// Called by the fish app when it wants to go to the next level.
-Fish.prototype.onContinue = function() {
-  const onReportComplete = result => {
-    this.studioApp_.onContinue();
-  };
+    const onMount = () => {
+      // NOTE: Other apps call studioApp.init(), except WebLab. Fish is imitating WebLab
+      this.studioApp_.setConfigValues_(config);
+      this.studioApp_.initTimeSpent();
 
-  this.studioApp_.report({
-    app: 'fish',
-    level: this.level.id,
-    result: true,
-    testResult: TestResults.ALL_PASS,
-    program: '',
-    onComplete: result => {
-      onReportComplete(result);
-    }
-  });
-};
+      // NOTE: if we called studioApp_.init(), the code here would be executed
+      // automatically since pinWorkspaceToBottom is true...
+      const container = document.getElementById(config.containerId);
+      const bodyElement = document.body;
+      bodyElement.style.overflow = 'hidden';
+      bodyElement.className = bodyElement.className + ' pin_bottom';
+      container.className = container.className + ' pin_bottom';
 
-Fish.prototype.initMLActivities = function() {
-  const {mode} = this.level;
-  const onContinue = this.onContinue.bind(this);
+      // Fixes viewport for small screens.  Also usually done by studioApp_.init().
+      const viewport = document.querySelector('meta[name="viewport"]');
+      if (viewport) {
+        this.studioApp_.fixViewportForSpecificWidthForSmallScreens_(
+          viewport,
+          MOBILE_PORTRAIT_WIDTH
+        );
+      }
 
-  // Set up initial state
-  const canvas = document.getElementById('activity-canvas');
-  const backgroundCanvas = document.getElementById('background-canvas');
+      this.initMLActivities();
+    };
 
-  setAssetPath('/blockly/media/skins/fish/');
+    // Push initial level properties into the Redux store
+    this.studioApp_.setPageConstants(config, {
+      channelId: config.channel,
+      noVisualization: true,
+      visualizationInWorkspace: true,
+      isProjectLevel: !!config.level.isProjectLevel
+    });
 
-  const {initAll} = require('@code-dot-org/ml-activities');
+    ReactDOM.render(
+      <Provider store={getStore()}>
+        <FishView onMount={onMount} />
+      </Provider>,
+      document.getElementById(config.containerId)
+    );
+  }
 
-  // Set initial state for UI elements.
-  initAll({
-    canvas,
-    backgroundCanvas,
-    appMode: mode,
-    onContinue,
-    registerSound: this.studioApp_.registerAudio.bind(this.studioApp_),
-    playSound: this.studioApp_.playAudio.bind(this.studioApp_),
-    i18n: fishMsg
-  });
-};
+  // Called by the fish app when it wants to go to the next level.
+  onContinue() {
+    const onReportComplete = result => {
+      this.studioApp_.onContinue();
+    };
 
-export default Fish;
+    this.studioApp_.report({
+      app: 'fish',
+      level: this.level.id,
+      result: true,
+      testResult: TestResults.ALL_PASS,
+      program: '',
+      onComplete: result => {
+        onReportComplete(result);
+      }
+    });
+  }
+
+  initMLActivities() {
+    const {mode} = this.level;
+    const onContinue = this.onContinue.bind(this);
+
+    // Set up initial state
+    const canvas = document.getElementById('activity-canvas');
+    const backgroundCanvas = document.getElementById('background-canvas');
+
+    setAssetPath('/blockly/media/skins/fish/');
+
+    const {initAll} = require('@code-dot-org/ml-activities');
+
+    // Set initial state for UI elements.
+    initAll({
+      canvas,
+      backgroundCanvas,
+      appMode: mode,
+      onContinue,
+      registerSound: this.studioApp_.registerAudio.bind(this.studioApp_),
+      playSound: this.studioApp_.playAudio.bind(this.studioApp_),
+      i18n: fishMsg
+    });
+  }
+}
