87bb2dc7e0f3cf288e90e177bb7551526cf657dc
Sanchit Malhotra <sanchit@code.org>
2021-09-22 12:45:41 -0700

    address comments

--- apps/test/unit/javalab/PlaygroundTest.js
+++ apps/test/unit/javalab/PlaygroundTest.js
@@ -2,6 +2,7 @@ import sinon from 'sinon';
 import {expect} from '../../util/reconfiguredChai';
 import Playground from '@cdo/apps/javalab/Playground';
 import {PlaygroundSignalType} from '@cdo/apps/javalab/constants';
+import javalabMsg from '@cdo/javalab/locale';
 
 describe('Playground', () => {
   const levelName = 'level';
@@ -65,11 +66,17 @@ describe('Playground', () => {
 
     expect(backgroundElement.src).to.be.undefined;
     expect(backgroundElement.style.opacity).to.equal(0);
+    expect(backgroundElement.onerror).to.be.undefined;
 
     playground.handleSignal(data);
 
     expect(backgroundElement.src).to.equal(`${levelName}/${starterAsset1}`);
     expect(backgroundElement.style.opacity).to.equal(1.0);
+    expect(backgroundElement.onerror).to.exist;
+
+    // Verify onerror callback
+    backgroundElement.onerror();
+    verifyOnFileLoadError(starterAsset1);
   });
 
   it('sets background image when receiving a SET_BACKGROUND_IMAGE message for an uploaded asset', () => {
@@ -83,11 +90,17 @@ describe('Playground', () => {
 
     expect(backgroundElement.src).to.be.undefined;
     expect(backgroundElement.style.opacity).to.equal(0);
+    expect(backgroundElement.onerror).to.be.undefined;
 
     playground.handleSignal(data);
 
     expect(backgroundElement.src).to.equal(`assets/${assetFile}`);
     expect(backgroundElement.style.opacity).to.equal(1.0);
+    expect(backgroundElement.onerror).to.exist;
+
+    // Verify onerror callback
+    backgroundElement.onerror();
+    verifyOnFileLoadError(assetFile);
   });
 
   it("doesn't set background image if game is over", () => {
@@ -106,11 +119,43 @@ describe('Playground', () => {
 
     expect(backgroundElement.src).to.be.undefined;
     expect(backgroundElement.style.opacity).to.equal(0);
+    expect(backgroundElement.onerror).to.be.undefined;
 
     playground.handleSignal(data);
 
     // Background should not update
     expect(backgroundElement.src).to.be.undefined;
     expect(backgroundElement.style.opacity).to.equal(0);
+    expect(backgroundElement.onerror).to.be.undefined;
   });
+
+  it('resets the background image on reset()', () => {
+    const data = {
+      value: PlaygroundSignalType.SET_BACKGROUND_IMAGE,
+      detail: {
+        filename: 'filename'
+      }
+    };
+
+    playground.handleSignal(data);
+
+    expect(backgroundElement.src).to.exist;
+    expect(backgroundElement.style.opacity).to.equal(1.0);
+    expect(backgroundElement.onerror).to.exist;
+
+    playground.reset();
+
+    expect(backgroundElement.src).to.be.undefined;
+    expect(backgroundElement.style.opacity).to.equal(0);
+    expect(backgroundElement.onerror).to.be.undefined;
+  });
+
+  function verifyOnFileLoadError(filename) {
+    sinon.assert.calledOnce(onOutputMessage);
+    sinon.assert.calledWith(
+      onOutputMessage,
+      javalabMsg.fileLoadError({filename})
+    );
+    sinon.assert.calledOnce(onNewlineMessage);
+  }
 });
