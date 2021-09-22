ee530565282933014516154e0be0335ad2175f52
anjali <anjali@code.org>
2021-09-21 17:06:08 -0700

    es6ify javalab

--- apps/src/javalab/locale-do-not-import.js
+++ apps/src/javalab/locale-do-not-import.js
@@ -11,6 +11,7 @@
 import safeLoadLocale from '@cdo/apps/util/safeLoadLocale';
 import localeWithI18nStringTracker from '@cdo/apps/util/i18nStringTracker';
 
-let locale = safeLoadLocale('javalab_locale');
-locale = localeWithI18nStringTracker(locale, 'javalab');
-module.exports = locale;
+export const locale = localeWithI18nStringTracker(
+  safeLoadLocale('javalab_locale'),
+  'javalab'
+);
