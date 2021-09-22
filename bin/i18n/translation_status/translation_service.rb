323ada79067ed89ced2b42e4c001d59801fe0c89
Steffan Musoke <steffanmusoke@gmail.com>
2021-09-22 11:06:42 -0500

    test: store after reload otherwise new translations dont stick

--- bin/i18n/translation_status/translation_service.rb
+++ bin/i18n/translation_status/translation_service.rb
@@ -5,10 +5,10 @@ require 'i18n'
 # Provides access to all the translations managed by the code-dot-org project.
 class TranslationService
   def initialize
-    store_frontend_translations
     # Normally we fallback to en-US, but we want to disable this so we detect missing strings.
     I18n.fallbacks.defaults = []
     I18n.backend.reload!
+    store_frontend_translations
   end
 
   # Returns true if a translation exists for a given key, otherwise returns false.
@@ -24,17 +24,9 @@ class TranslationService
       translations = {}
       Dir.glob(locales_dir.join("#{locale}/blockly-mooc/*.json")).each do |loc_file|
         name = File.basename(loc_file, ".*")
-        puts name if locale.include? "de"
         translations[name] = JSON.load(File.read(loc_file)).to_h
       end
       I18n.backend.store_translations(locale, translations)
     end
-    puts I18n.exists?("turtle.loopVariable", locale: "de-DE")
-    puts I18n.exists?("tutorialExplorer.startButton", locale: "tr-TR")
-    puts I18n.exists?("turtle.loopVariable", locale: "tr-TR")
-
-    puts "***"
-    puts translated?('de-DE', 'turtle.loopVariable')
-    puts translated?('de-DE', 'turtle.fill')
   end
 end
