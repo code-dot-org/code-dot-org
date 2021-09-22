bba2c075ebfc35435a8701da2cf61a57f31258c4
anjali <anjali@code.org>
2021-09-21 17:03:18 -0700

    es6ify fish

--- apps/src/fish/locale.js
+++ apps/src/fish/locale.js
@@ -3,6 +3,7 @@
 import safeLoadLocale from '@cdo/apps/util/safeLoadLocale';
 import localeWithI18nStringTracker from '@cdo/apps/util/i18nStringTracker';
 
-let locale = safeLoadLocale('fish_locale');
-locale = localeWithI18nStringTracker(locale, 'fish');
-module.exports = locale;
+export const locale = localeWithI18nStringTracker(
+  safeLoadLocale('fish_locale'),
+  'fish'
+);
