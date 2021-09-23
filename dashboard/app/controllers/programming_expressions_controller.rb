adfe213ccb2fb1ca3a311d6b00e99c913a403154
Bethany Connor <bethany@code.org>
2021-09-23 16:25:51 -0700

    Add a blank video option

--- dashboard/app/controllers/programming_expressions_controller.rb
+++ dashboard/app/controllers/programming_expressions_controller.rb
@@ -38,7 +38,7 @@ class ProgrammingExpressionsController < ApplicationController
     end
     programming_expression.name = underscored_params[:name]
     programming_expression.short_description = underscored_params[:short_description]
-    programming_expression.video_key = underscored_params[:video_key]
+    programming_expression.video_key = underscored_params[:video_key] unless underscored_params[:video_key].blank?
     programming_expression.save! if programming_expression.changed?
   end
 end
