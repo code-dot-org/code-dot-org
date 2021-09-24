d681c3ae5d0d754ade37e04f3f0928aab5bb6362
Maureen <maureen.sturgeon@gmail.com>
2021-09-24 10:52:05 -0700

    channel token backfill, rescue errors, write to csv

--- bin/oneoff/backfill_data/backfill_script_ids_channel_tokens.rb
+++ bin/oneoff/backfill_data/backfill_script_ids_channel_tokens.rb
@@ -21,6 +21,7 @@
 require_relative '../../../dashboard/config/environment'
 require 'cdo/db'
 require 'optparse'
+require 'csv'
 
 # We started writing script_id to the table here: https://github.com/code-dot-org/code-dot-org/pull/39855
 # so we don't need to backfill past that point
@@ -70,11 +71,12 @@ end.parse!
 puts "Options: #{options}"
 options.freeze
 
+# we're storing channel token ids that failed backfill in a csv for easy lookup later
+$csv_filename = "bin/oneoff/backfill_data/channel-token-backfill-failures.csv"
 $start_id = options[:start_id]
 $end_id = options[:end_id]
 $is_dry_run = options[:dry_run]
 
-$backfill_count = 0
 $unable_to_backfill = 0
 
 $level_to_script_ids = {}
@@ -82,57 +84,66 @@ $level_to_script_ids = {}
 def update_script_ids
   puts "Backfilling channel token script_ids..."
   puts "Script started at #{Time.now}"
-  # find_each uses find_in_batches with a batch size of 1000 (https://apidock.com/rails/ActiveRecord/Batches/find_each)
-  ChannelToken.where(id: $start_id..$end_id).find_each do |channel_token|
-    next if channel_token.script_id.present?
 
-    level = channel_token.level
-    user_id = user_id_for_storage_id(channel_token.storage_id)
+  CSV.open($csv_filename, "w") do |csv|
+    csv << ["channel token failed backfill"]
 
-    # get all the possible scripts the channel_token could be associated with
-    associated_script_ids = get_associated_script_ids(level)
+    # find_each uses find_in_batches with a batch size of 1000 (https://apidock.com/rails/ActiveRecord/Batches/find_each)
+    ChannelToken.where(id: $start_id..$end_id).find_each do |channel_token|
+      next if channel_token.script_id.present?
 
-    # if the level is associated with only one script_level, use that script
-    if associated_script_ids.length == 1
-      script_id = associated_script_ids[0]
-      update_channel_token(channel_token, script_id)
-      next
+      begin
+        level = channel_token.level
+        user_id = user_id_for_storage_id(channel_token.storage_id)
+
+        # get all the possible scripts the channel_token could be associated with
+        associated_script_ids = get_associated_script_ids(level)
+
+        # if the level is associated with only one script_level, use that script
+        if associated_script_ids.length == 1
+          script_id = associated_script_ids[0]
+          update_channel_token(channel_token, script_id, csv)
+          next
+        end
+
+        # it's possible the channel token was generated for a logged-out user, in which case no user_id will exist
+        if user_id.present?
+          # get user_scripts for the associated_script_ids, if there is just one user_script, backfill with the
+          # associated script_id
+          script_ids_from_user_scripts = associated_script_ids_by_user_scripts(user_id, associated_script_ids)
+          if script_ids_from_user_scripts.count == 1
+            update_channel_token(channel_token, script_ids_from_user_scripts[0], csv)
+            next
+          end
+
+          # get user_levels for the associated_script_ids, if there is just one user_level, backfill with the
+          # associated script_id
+          script_ids_from_user_levels = associated_script_ids_from_user_levels(user_id, level)
+          if script_ids_from_user_levels.count == 1
+            update_channel_token(channel_token, script_ids_from_user_levels[0], csv)
+            next
+          end
+
+          # if the user has no associated user_levels or user_scripts, it's possible that the user visited a channel backed
+          # level which generated a channel_token and then the user left the page without making any progress. In this case it doesn't
+          # matter which script_id we pick, so backfill with the first associated_script_ids
+          if associated_script_ids.count > 0 && script_ids_from_user_scripts.blank? && script_ids_from_user_levels.blank?
+            update_channel_token(channel_token, associated_script_ids[0], csv)
+            next
+          end
+        end
+      rescue StandardError
+        print "[E]"
+      end
+
+      log_backfill_failed(channel_token.id, csv)
     end
-
-    # it's possible the channel token was generated for a logged-out user, in which case no user_id will exist
-    if user_id.present?
-      # get user_scripts for the associated_script_ids, if there is just one user_script, backfill with the
-      # associated script_id
-      script_ids_from_user_scripts = associated_script_ids_by_user_scripts(user_id, associated_script_ids)
-      if script_ids_from_user_scripts.count == 1
-        update_channel_token(channel_token, script_ids_from_user_scripts[0])
-        next
-      end
-
-      # get user_levels for the associated_script_ids, if there is just one user_level, backfill with the
-      # associated script_id
-      script_ids_from_user_levels = associated_script_ids_from_user_levels(user_id, level)
-      if script_ids_from_user_levels.count == 1
-        update_channel_token(channel_token, script_ids_from_user_levels[0])
-        next
-      end
-
-      # if the user has no associated user_levels or user_scripts, it's possible that the user visited a channel backed
-      # level which generated a channel_token and then the user left the page without making any progress. In this case it doesn't
-      # matter which script_id we pick, so backfill with the first associated_script_ids
-      if associated_script_ids.count > 0 && script_ids_from_user_scripts.blank? && script_ids_from_user_levels.blank?
-        update_channel_token(channel_token, associated_script_ids[0])
-        next
-      end
-    end
-
-    log_backfill_failed(channel_token)
   end
   puts
   puts "Script ended at #{Time.now}"
   puts
-  puts "backfilled #{$backfill_count} script ids"
   puts "unable to backfill script id for #{$unable_to_backfill} channel tokens"
+  puts "Failures exported to: #{$csv_filename}"
   puts
 end
 
@@ -152,23 +163,22 @@ def get_associated_script_ids(level)
   return $level_to_script_ids[level.id]
 end
 
-def update_channel_token(channel_token, script_id)
+def update_channel_token(channel_token, script_id, csv)
   unless $is_dry_run
     did_save = channel_token.update_attributes(script_id: script_id)
 
     unless did_save
-      log_backfill_failed(channel_token.id)
+      log_backfill_failed(channel_token.id, csv)
       return
     end
   end
-  print "."
-  $backfill_count += 1
+  print "#{channel_token.id},"
 end
 
-def log_backfill_failed(channel_token_id)
-  print "F"
-  CDO.log.info("Could not update channel token with ID: #{channel_token_id}")
+def log_backfill_failed(channel_token_id, csv)
+  print "[F] #{channel_token_id},"
   $unable_to_backfill += 1
+  csv << [channel_token_id]
 end
 
 def associated_script_ids_by_user_scripts(user_id, script_ids)
