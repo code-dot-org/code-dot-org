0d1b929e2313cffef4775e64231dc3145886daba
David Bailey <davidsbailey@users.noreply.github.com>
2021-09-29 10:17:28 -0700

    early return from has_older_version_progress?

--- dashboard/app/models/script.rb
+++ dashboard/app/models/script.rb
@@ -987,6 +987,10 @@ class Script < ApplicationRecord
   # @return [Boolean] Whether the user has progress on another version of this unit.
   def has_older_version_progress?(user)
     return nil unless user && family_name && version_year
+
+    # skip more expensive checks if there are no other versions to redirect to.
+    return nil unless course_version&.course_offering&.course_versions&.many?
+
     user_unit_ids = user.user_scripts.pluck(:script_id)
 
     Script.
