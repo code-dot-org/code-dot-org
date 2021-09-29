b5b90560459eeabf905b91b5139b960a79f12afd
Dani <dani@code.org>
2021-09-29 14:55:15 -0400

    Take out the temporary changes to help with debugging

--- dashboard/app/controllers/api/v1/pd/workshops_controller.rb
+++ dashboard/app/controllers/api/v1/pd/workshops_controller.rb
@@ -12,7 +12,6 @@ class Api::V1::Pd::WorkshopsController < ::ApplicationController
 
   # GET /api/v1/pd/workshops
   def index
-    puts "CREATE NEW WORKSHOP"
     if params[:state]
       @workshops = @workshops.in_state(params[:state])
     end
@@ -31,7 +30,6 @@ class Api::V1::Pd::WorkshopsController < ::ApplicationController
 
     @workshops = @workshops.exclude_summer if params[:exclude_summer]
 
-    puts "ABOUT TO RENDER"
     render json: @workshops, each_serializer: Api::V1::Pd::WorkshopSerializer
   end
 
