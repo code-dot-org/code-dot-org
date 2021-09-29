2b906b0429bcf52ee25f362f735832a1893c550b
David Bailey <davidsbailey@users.noreply.github.com>
2021-09-29 10:40:07 -0700

    return early from summarize_versions

--- dashboard/test/integration/db_query_test.rb
+++ dashboard/test/integration/db_query_test.rb
@@ -109,7 +109,7 @@ class DBQueryTest < ActionDispatch::IntegrationTest
     student.assign_script(script)
     sign_in student
 
-    assert_cached_queries(12) do
+    assert_cached_queries(10) do
       get "/s/#{script.name}"
       assert_response :success
     end
