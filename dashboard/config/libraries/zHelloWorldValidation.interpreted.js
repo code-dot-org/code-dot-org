2be6c27b96f04e316f788ee10d378535c4f0e52e
Continuous Integration <dev@code.org>
2021-10-05 19:00:07 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/libraries/zHelloWorldValidation.interpreted.js
+++ dashboard/config/libraries/zHelloWorldValidation.interpreted.js
@@ -154,52 +154,27 @@ function level4(){
   }
 
   //check for unclicked sprites, and show hand with rings
-  if(World.seconds >1){
-    for(var i=0;i<spriteIds.length;i++){
-      var foundClick=false;
-      for(var j=0;j<eventLog.length;j++){
-        if(eventLog[j].includes(i)){
-          foundClick=true;
-          if(validationProps.clickedSprites.indexOf(i)==-1){
-            validationProps.clickedSprites.push(i);
-          }
-        }
-      }
-      if(!foundClick){
-        drawRings(getProp({id: i}, "x"),400-getProp({id: i}, "y"));
-        drawHand(getProp({id: i}, "x"),400-getProp({id: i}, "y"));
-      }
-    }
+  if(World.seconds > 1){
+    checkForUnclickedSprites(spriteIds, eventLog);
   }
 
-  // Set success time if success
-  if(validationProps.clickedSprites.length>=2 && !validationProps.successTime)
-  {
-    validationProps.successTime = World.frameCount;
+  validationProps.successCriteria.clickedAllSprites = validationProps.clickedSprites.length>=2;
+
+  setSuccessTime();
+  console.log("successTime: " + validationProps.successTime);
+
+  if (!validationProps.successTime) {
+    drawProgressBar("fail");
+  } else {
+    drawProgressBar("pass");
   }
 
-  // Delay fail time (so student can observe the wrong animation)
-  var failTime = 300;
-
-  // Check criteria and give failure feedback
-  if (World.frameCount > failTime) {
+  if (World.frameCount - validationProps.successTime >= WAIT_TIME) {
+    levelFailure(0, "genericSuccess");
+  } else if (World.frameCount > WAIT_TIME) {
     levelFailure(3, "clickAllSprites");
   }
 
-  // Pass 5 seconds after success
-  var waitTime = 150;
-  if (World.frameCount - validationProps.successTime >= waitTime) {
-    levelFailure(0, "genericSuccess");
-  }
-
-  push();
-  stroke("white");
-  if (!validationProps.successTime) {
-    drawProgress("fail",World.frameCount,failTime);
-  } else {
-    drawProgress("pass",World.frameCount - validationProps.successTime,waitTime);
-  }
-  pop();
 }
 
 
