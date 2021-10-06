60a2f616a491fdf8f3e20a0ae5d3b35f53431b81
Madelyn Kasula <maddie.kasula@code.org>
2021-10-06 16:36:26 -0700

    Update outdated comments

--- apps/src/templates/instructions/ExampleImage.jsx
+++ apps/src/templates/instructions/ExampleImage.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function ExampleImage({src}) {
-  // TODO: use style attr instead of classes?
-  return <img className="aniGif example-image" src={src} />;
+  return <img className="example-image" src={src} />;
 }
 
 ExampleImage.propTypes = {
