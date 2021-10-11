0a8b8358a6ae32714d295318de34c0ae6d96ab44
Tim Plummer <tim@code.org>
2021-10-11 11:48:45 -0700

    Add a functional form controller and refactor teacher application

--- apps/src/sites/studio/pages/pd/application/teacher_application/new.js
+++ apps/src/sites/studio/pages/pd/application/teacher_application/new.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TeacherApplication from '@cdo/apps/code-studio/pd/application/teacher/TeacherApplication';
+import TeacherApplication from '@cdo/apps/code-studio/pd/application/teacher/TeacherApplicationFunc';
 import getScriptData from '@cdo/apps/util/getScriptData';
 
 document.addEventListener('DOMContentLoaded', function(event) {
