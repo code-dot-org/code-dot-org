544a304aecb7c9d887c2d7ed0217d93b8025322a
Tim Plummer <tim@code.org>
2021-10-13 15:03:52 -0700

    Replace old classes

--- apps/src/sites/studio/pages/pd/application/teacher_application/new.js
+++ apps/src/sites/studio/pages/pd/application/teacher_application/new.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TeacherApplication from '@cdo/apps/code-studio/pd/application/teacher/TeacherApplicationFunc';
+import TeacherApplication from '@cdo/apps/code-studio/pd/application/teacher/TeacherApplication';
 import getScriptData from '@cdo/apps/util/getScriptData';
 
 document.addEventListener('DOMContentLoaded', function(event) {
