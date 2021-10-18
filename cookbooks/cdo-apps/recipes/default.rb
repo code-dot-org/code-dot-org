934d85f0bd41d6f76516506c299f32e67225d310
Suresh Chanmugam <suresh@code.org>
2021-10-18 13:42:39 -0700

    Remove pdftk-java to test provisioning an adhoc with a different change.

--- cookbooks/cdo-apps/recipes/default.rb
+++ cookbooks/cdo-apps/recipes/default.rb
@@ -25,21 +25,6 @@ apt_package %w(
 )
 
 # Used by lesson plan generator.
-pdftk_file = 'pdftk-java_3.1.1-1_all.deb'
-pdftk_local_file = "#{Chef::Config[:file_cache_path]}/#{pdftk_file}"
-remote_file pdftk_local_file do
-  source "https://mirrors.kernel.org/ubuntu/pool/universe/p/pdftk-java/#{pdftk_file}"
-  checksum "8a28ba8b100bc0b6e9f3e91c090a9b8a83a5f8a337a91150cbaadad8accb4901"
-end
-# Dependencies of pdftk-java.
-apt_package %w(
-  default-jre-headless
-  libbcprov-java
-  libcommons-lang3-java
-)
-dpkg_package("pdftk-java") {source pdftk_local_file}
-
-# Used by lesson plan generator.
 apt_package 'enscript'
 
 # Provides a Dashboard database fixture for Pegasus tests.
