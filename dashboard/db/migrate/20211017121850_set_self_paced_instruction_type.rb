52e8f4d9525aec3b6697d4e2e6aca649c07b2016
Dani <dani@code.org>
2021-10-19 23:38:56 -0400

    Update migration to match current list of self-paced scripts

--- dashboard/db/migrate/20211017121850_set_self_paced_instruction_type.rb
+++ dashboard/db/migrate/20211017121850_set_self_paced_instruction_type.rb
@@ -1,13 +1,18 @@
 class SetSelfPacedInstructionType < ActiveRecord::Migration[5.2]
   def change
     Script.all.each do |script|
-      if !script.unit_group && [].include?(script.name)
-        script.instruction_type = 'self_paced'
-      end
+      # scripts in unit_groups get their instruction type from their unit group
+      next unless script.unit_group
+      script.instruction_type = if ['self-paced-pl-csd5-2021', 'self-paced-pl-csd6-2021', 'self-paced-pl-csd7-2021', 'self-paced-pl-csd8-2021'].include?(script.name)
+                                  'self_paced'
+                                else
+                                  'teacher_led'
+                                end
     end
 
     UnitGroup.all.each do |course|
-      if [].include?(course.name)
+      # default is teacher_led so only need to update unit groups we want to be self_paced
+      if ['self-paced-pl-csp-2021', 'self-paced-pl-csd-2021'].include?(course.name)
         course.instruction_type = 'self_paced'
       end
     end
