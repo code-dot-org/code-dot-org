1b0dc1a1fd980d13df4863a6ec8fb59bb90c3836
Tim Plummer <tim@code.org>
2021-10-21 12:53:31 -0700

    fix import and debounce

--- apps/src/code-studio/pd/components/useRegionalPartner.jsx
+++ apps/src/code-studio/pd/components/useRegionalPartner.jsx
@@ -4,9 +4,9 @@ import {
   PROGRAM_CSD,
   PROGRAM_CSP,
   PROGRAM_CSA
-} from './TeacherApplicationConstants';
+} from '../application/teacher/TeacherApplicationConstants';
 import {SubjectNames} from '@cdo/apps/generated/pd/sharedWorkshopConstants';
-import debounce from 'lodash/debounce';
+import {debounce} from 'lodash';
 
 const COURSE_NAMES = {
   [PROGRAM_CSD]: 'CS Discoveries',
@@ -14,35 +14,40 @@ const COURSE_NAMES = {
   [PROGRAM_CSA]: 'Computer Science A'
 };
 
+const debouncedFetch = debounce(fetch, 500, {leading: true});
+
 // constructs query params and fetches the data, returning a promise
-const fetchRegionalPartner = debounce(
-  ({program, schoolZipCode, schoolState, school}) => {
-    const locationParams = {
-      course: COURSE_NAMES[program],
-      subject: SubjectNames.SUBJECT_SUMMER_WORKSHOP
-    };
-    if (school === '-1') {
-      locationParams.zip_code = schoolZipCode;
-      locationParams.state = schoolState;
-    } else if (school) {
-      locationParams.school = school;
-    } else {
-      return Promise.reject('parameter error');
-    }
+const fetchRegionalPartner = ({
+  program,
+  schoolZipCode,
+  schoolState,
+  school
+}) => {
+  const locationParams = {
+    course: COURSE_NAMES[program],
+    subject: SubjectNames.SUBJECT_SUMMER_WORKSHOP
+  };
+  if (school === '-1') {
+    locationParams.zip_code = schoolZipCode;
+    locationParams.state = schoolState;
+  } else if (school) {
+    locationParams.school = school;
+  } else {
+    return Promise.reject('parameter error');
+  }
 
-    const url = `/api/v1/pd/regional_partner_workshops/find?${$.param(
-      locationParams
-    )}`;
+  const url = `/api/v1/pd/regional_partner_workshops/find?${$.param(
+    locationParams
+  )}`;
 
-    return fetch(url).then(response => response.json());
-  },
-  500
-);
+  return debouncedFetch(url).then(response => response.json());
+};
 
 // takes {program, school, schoolZipCode, schoolState}
 // returns null if loading or error, otherwise: {id, name, group, workshops, has_csf, pl_programs_offered}
 // see regional_partner_workshops_serializer.rb
 export const useRegionalPartner = data => {
+  const {program, schoolZipCode, schoolState, school} = data;
   const [loadingPartner, setLoadingPartner] = useState(true);
   const [loadError, setLoadError] = useState(false);
   const [partner, setPartner] = useState(null);
@@ -66,7 +71,7 @@ export const useRegionalPartner = data => {
     return () => {
       cancelled = true;
     };
-  }, [data]);
+  }, [program, schoolZipCode, schoolState, school]);
 
   return [loadingPartner ? null : partner, loadError];
 };
