4e5dedbbf52d99b450964cc9c38b872dbfbc6d05
Dani <dani@code.org>
2021-10-21 09:53:00 -0400

    Save script and course. Update conditional

--- bin/oneoff/set_instruction_type.rb
+++ bin/oneoff/set_instruction_type.rb
@@ -14,21 +14,24 @@ def set_instruction_type
 
   Script.all.each do |script|
     # scripts in unit_groups get their instruction type from their unit group
-    next unless script.unit_group
+    next if script.unit_group
+
     script.instruction_type = if ['self-paced-pl-csd5-2021', 'self-paced-pl-csd6-2021', 'self-paced-pl-csd7-2021', 'self-paced-pl-csd8-2021'].include?(script.name)
                                 'self_paced'
                               else
                                 'teacher_led'
                               end
+    script.save!
     script.write_script_json
   end
 
   UnitGroup.all.each do |course|
     # default is teacher_led so only need to update unit groups we want to be self_paced
-    if ['self-paced-pl-csp-2021', 'self-paced-pl-csd-2021'].include?(course.name)
-      course.instruction_type = 'self_paced'
-      course.write_serialization
-    end
+    next unless ['self-paced-pl-csp-2021', 'self-paced-pl-csd-2021'].include?(course.name)
+
+    course.instruction_type = 'self_paced'
+    course.save!
+    course.write_serialization
   end
 end
 
