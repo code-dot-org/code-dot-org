95c02019105c7bb624a30663d22af93d889088f9
Dani <dani@code.org>
2021-10-22 14:37:02 -0400

    Create CODE_INSTRUCTOR permission

--- dashboard/app/models/user_permission.rb
+++ dashboard/app/models/user_permission.rb
@@ -46,6 +46,8 @@ class UserPermission < ApplicationRecord
     PROGRAM_MANAGER = 'program_manager'.freeze,
     # Grants access to review reported inaccuracies in census data
     CENSUS_REVIEWER = 'census_reviewer'.freeze,
+    # Grants ability to be the instructor of any course no matter instructor_audience
+    CODE_INSTRUCTOR = 'code_instructor'.freeze,
   ].freeze
 
   # Do not log the granting/removal of these permissions to slack
