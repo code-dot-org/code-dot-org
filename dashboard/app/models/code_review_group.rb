3b57804123404360167a2467771192a2bf897eb8
Ben Brooks <ben@code.org>
2021-10-26 14:12:02 -0700

    Create CodeReviewGroups

---
+++ dashboard/app/models/code_review_group.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: code_review_groups
+#
+#  id         :bigint           not null, primary key
+#  section_id :bigint           not null
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_code_review_groups_on_section_id  (section_id)
+#
+class CodeReviewGroup < ApplicationRecord
+end
