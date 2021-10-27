71b59197ebf1d5c70675c434f8184d9ea6ff0d66
Continuous Integration <dev@code.org>
2021-10-27 19:00:07 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/libraries/zHelloWorldValidation.interpreted.js
+++ dashboard/config/libraries/zHelloWorldValidation.interpreted.js
@@ -10,10 +10,10 @@ function level1(){
   }
 
   // Logic
-  setSuccessTime(validationPropsle.successCriteria);
+  setSuccessTime(validationProps.successCriteria);
 
   if (!validationProps.successCriteria.starterSprite ||
-    !validationProps.successCriterila.usedSpeech) {
+    !validationProps.successCriteria.usedSpeech) {
     drawProgressBar("earlyFail");
   } else {
     drawProgressBar("pass");
@@ -170,10 +170,8 @@ function level4(){
   }
 
   var newClickedSprite = getClickedSpriteIdCausedSpeech(eventLog, validationProps.vars.eventLogLength);
-  console.log("newClickedSprite: " + newClickedSprite);
   if (newClickedSprite >= 0){
     if (validationProps.clickedSprites.indexOf(newClickedSprite) != -1) {
-      console.log("IN PUSH");
       validationProps.clickedSprites.push(newClickedSprite);
       validationProps.vars.delay = World.frameCount;
     }
@@ -361,7 +359,8 @@ function level7(){
   if (!validationProps.successCriteria) {
     setSuccessCriteria({
       starterSprite: checkOneSprite(spriteIds),
-      noSpritesTouching: checkSpritesTouching(spriteIds)
+      noSpritesTouching: checkSpritesTouching(spriteIds),
+      changedCostume: false
     });
   }
 
@@ -393,6 +392,7 @@ function level7(){
           validationProps.vars.spritesCostumes[spriteId_0] = spriteCostume;
           validationProps.vars.numSpritesChangedCostume++;
           validationProps.vars.delay = World.frameCount;
+          validationProps.successCriteria.changedCostume = true;
           break;
         }
       }
