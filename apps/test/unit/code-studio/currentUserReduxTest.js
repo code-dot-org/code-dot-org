e1b7834fa68ee7233974473e8dfb77a38ae6422d
Maureen <maureen.sturgeon@gmail.com>
2021-10-27 16:38:26 -0700

    address feedback for current user fetch & add test

--- apps/test/unit/code-studio/currentUserReduxTest.js
+++ apps/test/unit/code-studio/currentUserReduxTest.js
@@ -1,10 +1,12 @@
 import {assert} from '../../util/reconfiguredChai';
+import sinon from 'sinon';
 import currentUser, {
   SignInState,
   setUserSignedIn,
   setUserType,
   setCurrentUserHasSeenStandardsReportInfo,
-  setCurrentUserName
+  setCurrentUserName,
+  __testonly__
 } from '@cdo/apps/templates/currentUserRedux';
 
 describe('currentUserRedux', () => {
@@ -18,6 +20,7 @@ describe('currentUserRedux', () => {
       assert.deepEqual(nextState.userName, 'Test Person');
     });
   });
+
   describe('setUserSignedIn', () => {
     it('can update signInState', () => {
       const signedIn = currentUser(initialState, setUserSignedIn(true));
@@ -30,6 +33,7 @@ describe('currentUserRedux', () => {
       assert.equal(initialState.signInState, SignInState.Unknown);
     });
   });
+
   describe('setUserType', () => {
     it('can set the current user type', () => {
       const action = setUserType('teacher');
@@ -38,12 +42,50 @@ describe('currentUserRedux', () => {
       assert.deepEqual(nextState.userType, 'teacher');
     });
   });
+
   describe('setCurrentUserHasSeenStandardsReportInfo', () => {
     it('can set the standards info dialog to seen', () => {
       const action = setCurrentUserHasSeenStandardsReportInfo(true);
       const nextState = currentUser(initialState, action);
 
       assert.deepEqual(nextState.hasSeenStandardsReportInfo, true);
+    });
+  });
+
+  describe('asyncLoadUserData', () => {
+    const {currentUserFromServer} = __testonly__;
+
+    it('calls /users/current and sets user data to state', async () => {
+      const dispatchSpy = sinon.spy();
+      const serverUser = {
+        id: 1,
+        username: 'test_user',
+        user_type: 'teacher',
+        is_signed_in: true
+      };
+
+      function mockApiResponse() {
+        return new window.Response(JSON.stringify(serverUser), {
+          status: 200,
+          headers: {'Content-type': 'application/json'}
+        });
+      }
+
+      const fetchStub = sinon.stub(window, 'fetch');
+      fetchStub
+        .withArgs('/api/v1/users/current')
+        .returns(Promise.resolve(mockApiResponse()));
+
+      await currentUserFromServer(dispatchSpy);
+
+      const dispatchCalls = dispatchSpy.getCalls();
+      const action1 = dispatchCalls[0].args[0];
+      assert.equal('currentUser/SET_USER_SIGNED_IN', action1.type);
+      assert.equal(true, action1.isSignedIn);
+
+      const action2 = dispatchCalls[1].args[0];
+      assert.equal('currentUser/SET_INITIAL_DATA', action2.type);
+      assert.deepEqual(serverUser, action2.serverUser);
     });
   });
 });
