b7056e98cc2eb7e98fb07f2f8de0aabf806b8804
Ben Brooks <ben@code.org>
2021-10-28 16:18:26 -0700

    Render CodeReviewGroups in modal

--- apps/src/templates/manageStudents/ManageCodeReviewGroups.jsx
+++ apps/src/templates/manageStudents/ManageCodeReviewGroups.jsx
@@ -1,44 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Button from '@cdo/apps/templates/Button';
 import i18n from '@cdo/locale';
 import StylizedBaseDialog from '@cdo/apps/componentLibrary/StylizedBaseDialog';
+import CodeReviewGroups from '@cdo/apps/templates/codeReviewGroups/CodeReviewGroups';
 
-export default class ManageCodeReviewGroups extends React.Component {
-  static propTypes = {
-    buttonStyle: PropTypes.object
-  };
+export default function ManageCodeReviewGroups({buttonContainerStyle}) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  state = {isDialogOpen: false};
+  const openDialog = () => setIsDialogOpen(true);
+  const onDialogClose = () => setIsDialogOpen(false);
 
-  openDialog = () => this.setState({isDialogOpen: true});
-  onDialogClose = () => this.setState({isDialogOpen: false});
-
-  render() {
-    return (
-      <div style={{...styles.button, ...this.props.buttonStyle}}>
-        {/* use div instead of button HTML element via __useDeprecatedTag
+  return (
+    <div style={{...styles.buttonContainer, ...buttonContainerStyle}}>
+      {/* use div instead of button HTML element via __useDeprecatedTag
           for consistent spacing with other "buttons" in ManageStudentsTable header */}
-        <Button
-          __useDeprecatedTag
-          onClick={this.openDialog}
-          color={Button.ButtonColor.gray}
-          text={i18n.manageCodeReviewGroups()}
-          icon="comment"
-        />
-        <StylizedBaseDialog
-          title={'hey'}
-          body={'hello'}
-          isOpen={this.state.isDialogOpen}
-          handleClose={this.onDialogClose}
-        />
-      </div>
-    );
-  }
+      <Button
+        __useDeprecatedTag
+        onClick={openDialog}
+        color={Button.ButtonColor.gray}
+        text={i18n.manageCodeReviewGroups()}
+        icon="comment"
+      />
+      <StylizedBaseDialog
+        title={i18n.codeReviewGroups()}
+        body={codeReviewGroups}
+        isOpen={isDialogOpen}
+        handleClose={onDialogClose}
+      />
+    </div>
+  );
 }
 
+ManageCodeReviewGroups.propTypes = {buttonContainerStyle: PropTypes.object};
+
+const names = [
+  'Sanchit',
+  'Mike',
+  'Mark',
+  'Molly',
+  'Ben',
+  'Jessie',
+  'Jamila',
+  'Hannah'
+];
+
+// Fake data generator.
+// Returns an array of objects that can be used to render a group.
+// Offset will creating objects starting at the offset indexed
+// element in the names array above, rather than the first element (default).
+const getMembers = (count, offset = 0) =>
+  Array.from({length: count}, (v, k) => k).map(k => ({
+    followerId: k + offset,
+    name: names[k + offset]
+  }));
+
+// Create two groups of four people.
+// We'll also eventually pass in a group name as a top level property.
+const groups = [
+  {id: 1, members: getMembers(4)},
+  {id: 2, members: getMembers(4, 4)}
+];
+
+const codeReviewGroups = <CodeReviewGroups initialGroups={groups} />;
+
 const styles = {
-  button: {
+  buttonContainer: {
     marginLeft: 5
   }
 };
