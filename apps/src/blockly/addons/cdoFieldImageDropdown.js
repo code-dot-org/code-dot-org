45f44497a74febe203a4796b957caf546f2e54e5
anjali <anjali@code.org>
2021-11-01 15:21:33 -0700

    calculate number of columns

--- apps/src/blockly/addons/cdoFieldImageDropdown.js
+++ apps/src/blockly/addons/cdoFieldImageDropdown.js
@@ -1,12 +1,24 @@
 import {FieldGridDropdown} from '@blockly/field-grid-dropdown';
 import color from '@cdo/apps/util/color';
 
+// Note that this class *does not* inherit from CdoFieldDropdown
 export class CdoFieldImageDropdown extends FieldGridDropdown {
   constructor(menuGenerator, width, height, buttons) {
+    // We have to decide how many columns to have when we create the block. The
+    // number of options in the block can change over time, but we can just use
+    // the initial number of options to determine how many columns to show- we're
+    // assuming that the number of options usually won't change that drastically,
+    // so the number of columns can probably stay the same.
+    const initialOptions = fixMenuGenerator(menuGenerator, width, height);
+    const numColumns =
+      initialOptions.length <= 7
+        ? 1
+        : Math.floor(Math.sqrt(initialOptions.length));
+
     super(
       () => fixMenuGenerator(menuGenerator, width, height),
       undefined /* validator */,
-      {columns: 7}
+      {columns: numColumns}
     );
   }
 
