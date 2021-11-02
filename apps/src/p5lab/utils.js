a8a718de1bbd10431874e1687775748e61587728
Mike Harvey <mike.harvey@code.org>
2021-11-02 09:23:33 -0400

    re-worked helper function export

--- apps/src/p5lab/utils.js
+++ apps/src/p5lab/utils.js
@@ -1,6 +1,4 @@
 // NOTE: min and max are inclusive
-export const utils = {
-  randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-};
+export function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
