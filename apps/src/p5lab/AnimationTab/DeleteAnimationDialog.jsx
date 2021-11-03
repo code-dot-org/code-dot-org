b73e6370f80b637a790cde8d205414baf174888c
Mike Harvey <mike.harvey@code.org>
2021-11-03 10:36:55 -0400

    Make DeleteAnimationDialog translatable
    Add strings to i18n file in common to maintain current functionality.  Use translatable strings in place of hard-coded strings in dialog.

--- apps/src/p5lab/AnimationTab/DeleteAnimationDialog.jsx
+++ apps/src/p5lab/AnimationTab/DeleteAnimationDialog.jsx
@@ -1,12 +1,8 @@
 /** @file controls below a dialog to delete animations */
 import PropTypes from 'prop-types';
 import React from 'react';
-import Dialog, {
-  Body,
-  Buttons,
-  Cancel,
-  Confirm
-} from '@cdo/apps/templates/Dialog';
+import Dialog, {Buttons, Cancel, Confirm} from '@cdo/apps/templates/Dialog';
+import i18n from '@cdo/locale';
 
 export default class DeleteAnimationDialog extends React.Component {
   static propTypes = {
@@ -20,14 +16,9 @@ export default class DeleteAnimationDialog extends React.Component {
       <Dialog
         isOpen={this.props.isOpen}
         handleClose={this.props.onCancel}
-        title="Delete animation"
+        title={i18n.deleteCostume()}
+        body={i18n.deleteCostumeConfirm()}
       >
-        <Body>
-          <div>
-            Are you sure you want to delete this animation? You cannot undo this
-            action.
-          </div>
-        </Body>
         <Buttons>
           <Cancel onClick={this.props.onCancel}>Cancel</Cancel>
           <Confirm onClick={this.props.onDelete} type="danger">
