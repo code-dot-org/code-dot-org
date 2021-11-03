a6868cecb1325b207a09150a2f7817c70ca83451
anjali <anjali@code.org>
2021-11-03 12:00:04 -0700

    clarify with comments

--- apps/src/blockly/addons/cdoToolbox.js
+++ apps/src/blockly/addons/cdoToolbox.js
@@ -5,18 +5,24 @@ export default class Toolbox extends GoogleBlockly.Toolbox {
     const container = super.createDom_(workspace);
 
     // Add the trashcan inside a holder svg element.
-    const trashcanHolder = Blockly.utils.dom.createSvgElement(
-      'svg',
-      {
-        id: 'trashcanHolder',
-        width: 70,
-        height: 90,
-        style: 'position: absolute'
-      },
-      container
-    );
-    trashcanHolder.appendChild(workspace.trashcan.svgGroup_);
+    this.trashcanHolder = Blockly.utils.dom.createSvgElement('svg', {
+      id: 'trashcanHolder',
+      height: 125,
+      style: 'position: absolute; display: none;'
+    });
+
+    // Add the trashcan as the first child of the toolbox container. If it's the
+    // second child after the categories, the trashcan renders "behind" the
+    // categories and appears clipped.
+    container.insertBefore(this.trashcanHolder, container.firstElementChild);
+
+    this.trashcanHolder.appendChild(workspace.trashcan.svgGroup_);
 
     return container;
+  }
+
+  position() {
+    this.trashcanHolder?.setAttribute('width', this.width_);
+    return super.position();
   }
 }
