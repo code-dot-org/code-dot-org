2f7f08a7eaeb8ec0a579e62d8b1482a4e59aa152
Dani <dani@code.org>
2021-11-04 14:30:06 -0400

    update one more naming thing for data refactor on lesson edit

--- apps/src/lib/levelbuilder/lesson-editor/ProgrammingExpressionsEditor.jsx
+++ apps/src/lib/levelbuilder/lesson-editor/ProgrammingExpressionsEditor.jsx
@@ -218,7 +218,7 @@ export const UnconnectedProgrammingExpressionsEditor = ProgrammingExpressionsEdi
 export default connect(
   state => ({
     programmingExpressions: state.programmingExpressions,
-    programmingEnvironments: state.levelSearching.programmingEnvironments
+    programmingEnvironments: state.levelSearchingInfo.programmingEnvironments
   }),
   {
     addProgrammingExpression,
