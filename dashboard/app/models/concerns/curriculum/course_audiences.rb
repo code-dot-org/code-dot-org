cf6821e53fbda7a475848776848f094ab77d0d87
Dani <dani@code.org>
2021-11-04 15:54:28 -0400

    Turn class methods into instance

--- dashboard/app/models/concerns/curriculum/course_audiences.rb
+++ dashboard/app/models/concerns/curriculum/course_audiences.rb
@@ -8,38 +8,36 @@ module CourseAudiences
     scope :unit_group, ->(instructor_audience, participant_audience) {where(instructor_audience: instructor_audience, participant_audience: participant_audience)}
   end
 
-  class_methods do
-    def can_be_instructor?(user)
-      # If unit is in a unit group then decide based on unit group audience
-      return unit_group.can_be_instructor?(user) if is_a?(Script) && unit_group
+  def can_be_instructor?(user)
+    # If unit is in a unit group then decide based on unit group audience
+    return unit_group.can_be_instructor?(user) if is_a?(Script) && unit_group
 
-      return false if user.student?
-      return true if user.permission?(UserPermission::CODE_INSTRUCTOR) || user.permission?(UserPermission::LEVELBUILDER)
+    return false if user.student?
+    return true if user.permission?(UserPermission::CODE_INSTRUCTOR) || user.permission?(UserPermission::LEVELBUILDER)
 
-      if instructor_audience == 'plc_reviewer'
-        return user.permission?(UserPermission::PLC_REVIEWER)
-      elsif instructor_audience == 'facilitator'
-        return user.permission?(UserPermission::FACILITATOR)
-      elsif instructor_audience == 'teacher'
-        return user.teacher?
-      end
-
-      false
+    if instructor_audience == 'plc_reviewer'
+      return user.permission?(UserPermission::PLC_REVIEWER)
+    elsif instructor_audience == 'facilitator'
+      return user.permission?(UserPermission::FACILITATOR)
+    elsif instructor_audience == 'teacher'
+      return user.teacher?
     end
 
-    def can_be_participant?(user)
-      # If unit is in a unit group then decide based on unit group audience
-      return unit_group.can_be_participant?(user) if is_a?(Script) && unit_group
+    false
+  end
 
-      if participant_audience == 'facilitator'
-        return user.permission?(UserPermission::FACILITATOR)
-      elsif participant_audience == 'teacher'
-        return user.teacher?
-      elsif participant_audience == 'student'
-        return true #if participant audience is student let anyone join
-      end
+  def can_be_participant?(user)
+    # If unit is in a unit group then decide based on unit group audience
+    return unit_group.can_be_participant?(user) if is_a?(Script) && unit_group
 
-      false
+    if participant_audience == 'facilitator'
+      return user.permission?(UserPermission::FACILITATOR)
+    elsif participant_audience == 'teacher'
+      return user.teacher?
+    elsif participant_audience == 'student'
+      return true #if participant audience is student let anyone join
     end
+
+    false
   end
 end
