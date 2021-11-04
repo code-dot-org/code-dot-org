cca12668e025195c891cc4b2b1ca9a2e2f8a7711
Dani <dani@code.org>
2021-11-04 14:48:54 -0400

    Pull out methods into a concern

---
+++ dashboard/app/models/concerns/curriculum/course_audiences.rb
@@ -0,0 +1,38 @@
+# Model concern for common curriculum audience methods
+# To use, include in a model and call the desired method.
+module CourseAudiences
+  extend ActiveSupport::Concern
+
+  def can_be_instructor?(user)
+    # If unit is in a unit group then decide based on unit group audience
+    return unit_group.can_be_instructor?(user) if is_a?(Script) && unit_group
+
+    return false if user.student?
+    return true if user.permission?(UserPermission::CODE_INSTRUCTOR) || user.permission?(UserPermission::LEVELBUILDER)
+
+    if instructor_audience == 'plc_reviewer'
+      return user.permission?(UserPermission::PLC_REVIEWER)
+    elsif instructor_audience == 'facilitator'
+      return user.permission?(UserPermission::FACILITATOR)
+    elsif instructor_audience == 'teacher'
+      return user.teacher?
+    end
+
+    false
+  end
+
+  def can_be_participant?(user)
+    # If unit is in a unit group then decide based on unit group audience
+    return unit_group.can_be_participant?(user) if is_a?(Script) && unit_group
+
+    if participant_audience == 'facilitator'
+      return user.permission?(UserPermission::FACILITATOR)
+    elsif participant_audience == 'teacher'
+      return user.teacher?
+    elsif participant_audience == 'student'
+      return true #if participant audience is student let anyone join
+    end
+
+    false
+  end
+end
