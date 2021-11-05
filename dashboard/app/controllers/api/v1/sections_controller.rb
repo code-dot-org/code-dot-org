8619ad66de4fb19cd9f86463a7bf3beadd8fb020
molly-moen <molly@code.org>
2021-11-05 11:48:14 -0700

    fix bug

--- dashboard/app/controllers/api/v1/sections_controller.rb
+++ dashboard/app/controllers/api/v1/sections_controller.rb
@@ -239,7 +239,7 @@ class Api::V1::SectionsController < Api::V1::JsonApiController
 
   # POST /api/v1/sections/<id>/code_review_enabled
   def set_code_review_enabled
-    enable_code_review = params[:enabled]
+    enable_code_review = params[:enabled].to_bool
     @section.update_code_review_expiration(enable_code_review)
     render json: {result: 'success', expiration: @section.code_review_expires_at}
   end
