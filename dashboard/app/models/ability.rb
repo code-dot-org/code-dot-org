e661ef2bf3e361452fb669284f78b2fb91d84d19
Dani <dani@code.org>
2021-11-06 15:52:41 -0400

    Update ability.rb to account for who can view different course types

--- dashboard/app/models/ability.rb
+++ dashboard/app/models/ability.rb
@@ -249,8 +249,8 @@ class Ability
       end
     end
 
-    can [:vocab, :resources, :code, :standards], UnitGroup do
-      true
+    can [:vocab, :resources, :code, :standards], UnitGroup do |unit_group|
+      unit_group.can_be_participant?(user) || unit_group.can_be_instructor?(user)
     end
 
     # Override UnitGroup, Unit, Lesson and ScriptLevel.
@@ -260,17 +260,17 @@ class Ability
       elsif unit_group.pilot?
         unit_group.has_pilot_access?(user)
       else
-        true
+        unit_group.can_be_participant?(user) || unit_group.can_be_instructor?(user)
       end
     end
 
-    can :read, Script do |script|
-      if script.in_development?
+    can :read, Script do |unit|
+      if unit.in_development?
         user.permission?(UserPermission::LEVELBUILDER)
-      elsif script.pilot?
-        script.has_pilot_access?(user)
+      elsif unit.pilot?
+        unit.has_pilot_access?(user)
       else
-        true
+        unit.can_be_participant?(user) || unit.can_be_instructor?(user)
       end
     end
 
@@ -281,6 +281,7 @@ class Ability
       elsif script.pilot?
         script.has_pilot_access?(user)
       else
+        return false unless script.can_be_participant?(user) || script.can_be_instructor?(user)
         # login is required if this script always requires it or if request
         # params were passed to authorize! and includes login_required=true
         login_required = script.login_required? || (!params.nil? && params[:login_required] == "true")
@@ -299,7 +300,7 @@ class Ability
       elsif script.pilot?
         script.has_pilot_access?(user)
       else
-        true
+        script.can_be_participant?(user) || script.can_be_instructor?(user)
       end
     end
 
