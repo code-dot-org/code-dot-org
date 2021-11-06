f05fd6fc96e36b557627f372f7fbe84f32df2b17
Dani <dani@code.org>
2021-11-06 08:33:28 -0400

    start using isolate components

--- apps/test/unit/lib/levelbuilder/lesson-editor/AddLevelDialogTopTest.jsx
+++ apps/test/unit/lib/levelbuilder/lesson-editor/AddLevelDialogTopTest.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
 import {expect} from '../../../../util/reconfiguredChai';
 import {UnconnectedAddLevelDialogTop as AddLevelDialogTop} from '@cdo/apps/lib/levelbuilder/lesson-editor/AddLevelDialogTop';
 import {searchOptions} from './activitiesTestData';
 import sinon from 'sinon';
+import {isolateComponent} from 'isolate-components';
 
 describe('AddLevelDialogTop', () => {
   let defaultProps, addLevel;
@@ -17,29 +17,27 @@ describe('AddLevelDialogTop', () => {
 
   it('shows filters once finished loading', () => {
     let returnData = {
-      data: {
-        levels: [
-          {
-            conceptDifficulty: '',
-            concepts: '',
-            icon: 'fa fa-list-ul',
-            id: 22300,
-            is_concept_level: false,
-            kind: 'puzzle',
-            name: 'csd-pulse-check-survey-1-levelgroup U6Ch1_2018_2019_2020',
-            owner: null,
-            skin: null,
-            sublevels: null,
-            title: null,
-            type: 'LevelGroup',
-            unplugged: false,
-            updated_at: '10/20/20 at 01:30:16 PM',
-            url: '/levels/22300/edit',
-            videoKey: null
-          }
-        ],
-        numPages: 1
-      }
+      levels: [
+        {
+          conceptDifficulty: '',
+          concepts: '',
+          icon: 'fa fa-list-ul',
+          id: 22300,
+          is_concept_level: false,
+          kind: 'puzzle',
+          name: 'csd-pulse-check-survey-1-levelgroup U6Ch1_2018_2019_2020',
+          owner: null,
+          skin: null,
+          sublevels: null,
+          title: null,
+          type: 'LevelGroup',
+          unplugged: false,
+          updated_at: '10/20/20 at 01:30:16 PM',
+          url: '/levels/22300/edit',
+          videoKey: null
+        }
+      ],
+      numPages: 1
     };
     let server = sinon.fakeServer.create();
     server.respondWith('GET', '/levels/get_filtered_levels?page=1', [
@@ -47,25 +45,25 @@ describe('AddLevelDialogTop', () => {
       {'Content-Type': 'application/json;charset=UTF-8'},
       JSON.stringify(returnData)
     ]);
-    const wrapper = mount(<AddLevelDialogTop {...defaultProps} />);
+    const wrapper = isolateComponent(<AddLevelDialogTop {...defaultProps} />);
     server.respond();
 
-    expect(wrapper.find('Connect(ToggleGroup)').length).to.equal(1);
-    expect(wrapper.find('Connect(AddLevelFilters)').length).to.equal(1);
-    expect(wrapper.find('AddLevelTable').length).to.equal(1);
-    expect(wrapper.find('.fa-spin').length).to.equal(0); // no spinner
+    expect(wrapper.findOne('Connect(ToggleGroup)'));
+    expect(wrapper.findOne('Connect(AddLevelFilters)'));
+    expect(wrapper.findOne('AddLevelTable'));
+    expect(!wrapper.exists('.fa-spin')); // no spinner
 
     server.restore();
   });
 
   it('getting level data show spinner', () => {
-    const wrapper = shallow(<AddLevelDialogTop {...defaultProps} />);
+    const wrapper = isolateComponent(<AddLevelDialogTop {...defaultProps} />);
 
     // Without using setLevels this test has no level data
 
-    expect(wrapper.find('ToggleGroup').length).to.equal(0);
-    expect(wrapper.find('Connect(AddLevelFilters)').length).to.equal(0);
-    expect(wrapper.find('AddLevelTable').length).to.equal(0);
-    expect(wrapper.find('.fa-spin').length).to.equal(1);
+    expect(!wrapper.exists('ToggleGroup'));
+    expect(!wrapper.exists('Connect(AddLevelFilters)'));
+    expect(!wrapper.exists('AddLevelTable'));
+    expect(wrapper.findOne('.fa-spin'));
   });
 });
