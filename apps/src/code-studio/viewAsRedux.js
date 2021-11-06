c1e539a712e8c914810aff444fdbbebee6937353
Dani <dani@code.org>
2021-11-06 12:55:42 -0400

    update comment in viewAsRedux

--- apps/src/code-studio/viewAsRedux.js
+++ apps/src/code-studio/viewAsRedux.js
@@ -37,15 +37,15 @@ export const setViewType = viewType => ({
 
 export const changeViewType = viewType => {
   return dispatch => {
+    // If changing to viewAs participant while we are a particular participant, remove
+    // the user_id and do a reload so that we're instead viewing as a generic
+    // participant
     if (viewType === ViewType.Participant && queryParams('user_id')) {
       updateQueryParam('user_id', undefined);
       // Make a stubbable call to window.location.reload
       reload();
       return;
     }
-    // If changing to viewAs student while we are a particular student, remove
-    // the user_id and do a reload so that we're instead viewing as a generic
-    // student
 
     dispatch(setViewType(viewType));
   };
