834125f03f8025e246b35599476a154ab24a3836
Dani <dani@code.org>
2021-11-06 07:34:30 -0400

    Rename CourseAudiences to CourseTypes

--- dashboard/test/models/concerns/curriculum/course_types_test.rb
+++ dashboard/test/models/concerns/curriculum/course_types_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class InstructorAndParticipantAudienceTests < ActiveSupport::TestCase
+class CourseTypesTests < ActiveSupport::TestCase
   setup do
     @student = create :student
     @teacher = create :teacher
