d17d772e2993bdc985042158c35c4c4baf840117
Sanchit Malhotra <sanchit@code.org>
2021-11-08 15:20:45 -0800

    Add Code Review groups client-side API for updating groups and setting enabled status

--- apps/src/templates/codeReviewGroups/CodeReviewGroupsDataApi.js
+++ apps/src/templates/codeReviewGroups/CodeReviewGroupsDataApi.js
@@ -8,6 +8,48 @@ export default class CodeReviewGroupsDataApi {
   getCodeReviewGroups() {
     return $.getJSON(
       `/api/v1/sections/${this.sectionId}/code_review_groups`
-    ).then(response => response.groups);
+    ).then(response => this.convertGroupsToCamelCase(response.groups));
   }
+
+  setCodeReviewGroups(groups) {
+    return $.post(`/api/v1/sections/${this.sectionId}/code_review_groups`, {
+      groups: this.convertGroupsToSnakeCase(groups)
+    });
+  }
+
+  setCodeReviewEnabled(enabled) {
+    return $.post(`/api/v1/sections/${this.sectionId}/code_review_enabled`, {
+      enabled
+    });
+  }
+
+  /**
+   * Converts code review groups JSON data from the server by performing
+   * any necessary JSON key conversions from snake case to camel case
+   */
+  convertGroupsToCamelCase = groups => {
+    for (let group of groups) {
+      group.members = group.members.map(member => {
+        let converted = {...member, followerId: member.follower_id};
+        delete converted.follower_id;
+        return converted;
+      });
+    }
+    return groups;
+  };
+
+  /**
+   * Converts code review groups data from the client into server-friendly
+   * snake case
+   */
+  convertGroupsToSnakeCase = groups => {
+    for (let group of groups) {
+      group.members = group.members.map(member => {
+        let converted = {...member, follower_id: member.followerId};
+        delete converted.followerId;
+        return converted;
+      });
+    }
+    return groups;
+  };
 }
