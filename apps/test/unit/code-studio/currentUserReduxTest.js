761092da295785d5b249ce4f6fa8335ebfddcf5b
maureensturgeon <maureen@code.org>
2021-11-11 11:57:30 -0800

    Revert "Load verified teacher async"

--- apps/test/unit/code-studio/currentUserReduxTest.js
+++ apps/test/unit/code-studio/currentUserReduxTest.js
@@ -1,11 +1,12 @@
 import {assert} from '../../util/reconfiguredChai';
+import sinon from 'sinon';
 import currentUser, {
   SignInState,
   setUserSignedIn,
   setUserType,
   setCurrentUserHasSeenStandardsReportInfo,
   setCurrentUserName,
-  setInitialData
+  __testonly__
 } from '@cdo/apps/templates/currentUserRedux';
 
 describe('currentUserRedux', () => {
@@ -51,20 +52,40 @@ describe('currentUserRedux', () => {
     });
   });
 
-  describe('setInitialData', () => {
-    it('can set the standards info dialog to seen', () => {
+  describe('asyncLoadUserData', () => {
+    const {currentUserFromServer} = __testonly__;
+
+    it('calls /users/current and sets user data to state', async () => {
+      const dispatchSpy = sinon.spy();
       const serverUser = {
         id: 1,
         username: 'test_user',
         user_type: 'teacher',
         is_signed_in: true
       };
-      const action = setInitialData(serverUser);
-      const nextState = currentUser(initialState, action);
 
-      assert.equal(nextState.userId, 1);
-      assert.equal(nextState.userName, 'test_user');
-      assert.equal(nextState.userType, 'teacher');
+      function mockApiResponse() {
+        return new window.Response(JSON.stringify(serverUser), {
+          status: 200,
+          headers: {'Content-type': 'application/json'}
+        });
+      }
+
+      const fetchStub = sinon.stub(window, 'fetch');
+      fetchStub
+        .withArgs('/api/v1/users/current')
+        .returns(Promise.resolve(mockApiResponse()));
+
+      await currentUserFromServer(dispatchSpy);
+
+      const dispatchCalls = dispatchSpy.getCalls();
+      const action1 = dispatchCalls[0].args[0];
+      assert.equal('currentUser/SET_USER_SIGNED_IN', action1.type);
+      assert.equal(true, action1.isSignedIn);
+
+      const action2 = dispatchCalls[1].args[0];
+      assert.equal('currentUser/SET_INITIAL_DATA', action2.type);
+      assert.deepEqual(serverUser, action2.serverUser);
     });
   });
 });
