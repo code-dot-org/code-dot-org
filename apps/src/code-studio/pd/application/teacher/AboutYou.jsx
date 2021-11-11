a6bdb692ff6eb7283fc3d1cc1cf7b6ccf927fe96
Tim Plummer <tim@code.org>
2021-11-10 16:41:27 -0800

    missing space

--- apps/src/code-studio/pd/application/teacher/AboutYou.jsx
+++ apps/src/code-studio/pd/application/teacher/AboutYou.jsx
@@ -169,14 +169,13 @@ const AboutYou = props => {
             please visit the{' '}
             <a href={CSD_URL} target="_blank" rel="noopener noreferrer">
               CS Discoveries
-            </a>{' '}
+            </a>
             ,{' '}
             <a href={CSP_URL} target="_blank" rel="noopener noreferrer">
               CS Principles
-            </a>{' '}
-            , and <a href={CSA_URL}>CSA</a>
-            landing pages. For additional questions regarding the program or
-            application, please{' '}
+            </a>
+            , and <a href={CSA_URL}>CSA</a> landing pages. For additional
+            questions regarding the program or application, please{' '}
             <RegionalPartnerMiniContactPopupLink
               sourcePageId="teacher-application-first-page"
               notes="Please tell me more about the professional learning program for grades 6-12!"
