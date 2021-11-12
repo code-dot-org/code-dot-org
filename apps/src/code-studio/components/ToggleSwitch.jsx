9b74e16c419046590fd9cfb383cb595181149758
molly-moen <molly@code.org>
2021-11-11 16:59:07 -0800

    styling

--- apps/src/code-studio/components/ToggleSwitch.jsx
+++ apps/src/code-studio/components/ToggleSwitch.jsx
@@ -3,55 +3,64 @@ import Radium from 'radium';
 import PropTypes from 'prop-types';
 import color from '@cdo/apps/util/color';
 
-function ToggleSwitch({isEnabled, onToggle, label}) {
-  const dynamicStyle = isEnabled ? styles.enabledIcon : styles.disabledIcon;
-  const iconStyle = {...styles.icon, ...dynamicStyle};
-  return (
-    <button
-      key="toggle-button"
-      type="button"
-      id="enableCodeReview"
-      onClick={onToggle}
-      aria-pressed={isEnabled}
-      style={styles.button}
-    >
-      <i key="toggle-icon" className={'fa fa-toggle-on'} style={iconStyle} />
-      <div>{label}</div>
-    </button>
-  );
+class ToggleSwitch extends React.Component {
+  static propTypes = {
+    isEnabled: PropTypes.bool.isRequired,
+    onToggle: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired
+  };
+
+  render() {
+    const {isEnabled, onToggle, label} = this.props;
+
+    const dynamicStyle = isEnabled ? styles.enabledIcon : styles.disabledIcon;
+    const iconStyle = {...styles.icon, ...dynamicStyle};
+    const isButtonFocused = Radium.getState(this.state, 'main', ':focus');
+    return (
+      <button
+        ref={ref => (this.buttonDiv = ref)}
+        type="button"
+        id="enableCodeReview"
+        onClick={onToggle}
+        aria-pressed={isEnabled}
+        style={styles.button}
+      >
+        <div
+          style={isButtonFocused ? styles.toggleIconDivFocus : {}}
+          ref={ref => (this.toggleIconDiv = ref)}
+        >
+          <i className={'fa fa-toggle-on'} style={iconStyle} />
+        </div>
+        <div style={styles.label}>{label}</div>
+      </button>
+    );
+  }
 }
-
-ToggleSwitch.propTypes = {
-  isEnabled: PropTypes.bool.isRequired,
-  onToggle: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired
-};
-
 export default Radium(ToggleSwitch);
 
 const styles = {
   // remove default button styling
   button: {
-    border: 0,
-    outline: 0,
-    boxShadow: 'none',
-    padding: 0,
+    border: '0px none',
+    // outline: 0,
+    // boxShadow: 'none',
+    // padding: 0,
     background: 'transparent',
     font: 'inherit',
     ':hover': {
       outline: 0,
-      boxShadow: 'none',
-      border: '0px none'
+      boxShadow: 'none'
+      //border: '0px none'
     },
     ':focus': {
       outline: 0,
-      boxShadow: 'none',
-      border: '0px none'
+      boxShadow: 'none'
+      //border: '0px none'
     },
     ':active': {
       outline: 0,
-      boxShadow: 'none',
-      border: '0px none !important'
+      boxShadow: 'none'
+      //border: '0px none !important'
     },
     textAlign: 'center',
     alignItems: 'center',
@@ -67,11 +76,11 @@ const styles = {
   icon: {
     fontSize: '24px',
     width: '32px'
-    // ':focus': {
-    //   outline: '1px solid black'
-    // },
-    // ':focus:not(:focus-visible)': {
-    //   outline: 0
-    // }
+  },
+  toggleIconDivFocus: {
+    outline: '1px solid black'
+  },
+  label: {
+    marginLeft: 5
   }
 };
