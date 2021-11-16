4ab19c34140e5e0342292ac2d04f69ee1fe1af43
Erin Peach <erin@code.org>
2021-11-16 13:54:23 -0800

    Move example sprites to constants file

--- apps/src/sites/studio/pages/blocks/index.js
+++ apps/src/sites/studio/pages/blocks/index.js
@@ -4,47 +4,15 @@ import jsonic from 'jsonic';
 import {parseElement} from '@cdo/apps/xml';
 import {installCustomBlocks} from '@cdo/apps/block_utils';
 import {customInputTypes} from '@cdo/apps/p5lab/spritelab/blocks';
-import {valueTypeTabShapeMap} from '@cdo/apps/p5lab/spritelab/constants';
+import {
+  valueTypeTabShapeMap,
+  exampleSprites
+} from '@cdo/apps/p5lab/spritelab/constants';
 import {shrinkBlockSpaceContainer} from '@cdo/apps/templates/instructions/utils';
 import animationList, {
   setInitialAnimationList
 } from '@cdo/apps/p5lab/redux/animationList';
 import {getStore, registerReducers} from '@cdo/apps/redux';
-
-// Blocks in the index are readonly. Use a single costume and a single background for previews.
-let defaultThumbnails = {
-  orderedKeys: [
-    '2223bab1-0b27-4ad1-ad2e-7eb3dd0997c2',
-    '8ca751af-ef34-4fd4-9e96-6e985f93f4c2'
-  ],
-  propsByKey: {
-    '2223bab1-0b27-4ad1-ad2e-7eb3dd0997c2': {
-      name: 'bear',
-      sourceUrl:
-        'https://studio.code.org/api/v1/animation-library/spritelab/wAQoTe9lNAp19q.JxOmT6hRtv1GceGwp/category_animals/bear.png',
-      frameSize: {x: 254, y: 333},
-      frameCount: 1,
-      looping: true,
-      frameDelay: 2,
-      version: 'wAQoTe9lNAp19q.JxOmT6hRtv1GceGwp',
-      categories: ['animals']
-    },
-    '8ca751af-ef34-4fd4-9e96-6e985f93f4c2': {
-      name: 'cave',
-      sourceUrl:
-        'https://studio.code.org/api/v1/animation-library/spritelab/3LUT4MZxHDWhZbAtYtEmQD1ZrfwQ7jFG/category_backgrounds/background_cave.png',
-      frameSize: {
-        x: 400,
-        y: 400
-      },
-      frameCount: 1,
-      looping: true,
-      frameDelay: 2,
-      version: '3LUT4MZxHDWhZbAtYtEmQD1ZrfwQ7jFG',
-      categories: ['backgrounds']
-    }
-  }
-};
 
 function renderBlock(element) {
   const name = element.id;
@@ -78,7 +46,7 @@ function renderBlock(element) {
 
 $(document).ready(() => {
   registerReducers({animationList: animationList});
-  getStore().dispatch(setInitialAnimationList(defaultThumbnails));
+  getStore().dispatch(setInitialAnimationList(exampleSprites));
   Blockly.assetUrl = assetUrl;
   Blockly.valueTypeTabShapeMap = valueTypeTabShapeMap(Blockly);
   Blockly.typeHints = true;
