359ac99028290c6099715eb43fd44e27c836ebe1
Meg Crenshaw <meg@code.org>
2021-11-17 12:21:21 -0500

    Only show button if there's a path to it

--- apps/src/lib/levelbuilder/SaveBar.jsx
+++ apps/src/lib/levelbuilder/SaveBar.jsx
@@ -2,20 +2,27 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import color from '@cdo/apps/util/color';
 import FontAwesome from '@cdo/apps/templates/FontAwesome';
+import {linkWithQueryParams, navigateToHref} from '@cdo/apps/utils';
 
 export default class SaveBar extends Component {
+  handleView = () =>
+    this.props.handleView ||
+    navigateToHref(linkWithQueryParams(this.props.pathForShowButton));
+
   render() {
     return (
       <div style={styles.saveButtonBackground} className="saveBar">
-        <button
-          className="btn"
-          type="button"
-          style={styles.saveButton}
-          onClick={this.props.handleView}
-          disabled={this.props.isSaving}
-        >
-          Show
-        </button>
+        {this.props.pathForShowButton !== '/' && (
+          <button
+            className="btn"
+            type="button"
+            style={styles.saveButton}
+            onClick={this.handleView}
+            disabled={this.props.isSaving}
+          >
+            Show
+          </button>
+        )}
         <div style={styles.saveButtonArea}>
           {this.props.lastSaved && !this.props.error && (
             <div style={styles.lastSaved} className="lastSavedMessage">
@@ -61,9 +68,14 @@ export default class SaveBar extends Component {
 SaveBar.propTypes = {
   error: PropTypes.string,
   handleSave: PropTypes.func.isRequired,
-  handleView: PropTypes.func.isRequired,
+  handleView: PropTypes.func,
   isSaving: PropTypes.bool,
-  lastSaved: PropTypes.number
+  lastSaved: PropTypes.number,
+  pathForShowButton: PropTypes.string
+};
+
+SaveBar.defaultProps = {
+  pathForShowButton: '/'
 };
 
 const styles = {
