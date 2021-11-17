2cc4cfe55244b7d1ae6c4022c30475f029efe219
Hannah Bergam <hannahbergam@gmail.com>
2021-11-17 10:26:03 -0800

    moving progress call and redux work to studentlessonoverview WIP

--- apps/src/sites/studio/pages/lessons/student_lesson_plan.js
+++ apps/src/sites/studio/pages/lessons/student_lesson_plan.js
@@ -9,8 +9,6 @@ import getScriptData from '@cdo/apps/util/getScriptData';
 import instructionsDialog from '@cdo/apps/redux/instructionsDialog';
 import {getStore} from '@cdo/apps/code-studio/redux';
 import {registerReducers} from '@cdo/apps/redux';
-import {retrieveProgress} from '@cdo/apps/code-studio/progress';
-import {groupedLessons} from '@cdo/apps/code-studio/progressRedux';
 import StudentLessonOverview from '@cdo/apps/templates/lessonOverview/StudentLessonOverview';
 
 $(document).ready(function() {
@@ -22,25 +20,10 @@ $(document).ready(function() {
  * Collect and preprocess all data students should see for the lesson, and
  * render the React component which displays them.
  */
-async function displayLessonOverview() {
+function displayLessonOverview() {
   const lessonData = getScriptData('lesson');
   const scriptName = getScriptData('scriptName');
-  await retrieveProgress(scriptName, null, null);
   const store = getStore();
-
-  const groupedLessonsResults = groupedLessons(store.getState().progress);
-  let lessonIndex;
-  const groupedLesson = groupedLessonsResults.find(groupedLesson =>
-    groupedLesson.lessons.find((lesson, index) => {
-      const isCurrentLesson = lesson.lessonNumber === lessonData.position;
-      if (isCurrentLesson) {
-        lessonIndex = index;
-        return true;
-      }
-    })
-  );
-  const levelsByLesson = groupedLesson.levelsByLesson;
-  const targetLessonLevels = levelsByLesson[lessonIndex];
 
   if (lessonData.announcements) {
     registerReducers({announcements: announcementsReducer});
@@ -57,12 +40,9 @@ async function displayLessonOverview() {
     );
   }
 
-  // !!store.progress.unitCompleted <- wait until this is true to call StudentLessonOverview
-  // ... similar to rendering logic in UnitOverview
-  //const unitProgressLoaded = store.getState().progress.unitCompleted;
   ReactDOM.render(
     <Provider store={store}>
-      <StudentLessonOverview lesson={lessonData} levels={targetLessonLevels} />
+      <StudentLessonOverview lesson={lessonData} scriptName={scriptName} />
     </Provider>,
     document.getElementById('show-container')
   );
