0728aa6859e107d2f4381b41c757b665c3525bf0
Ben Brooks <ben@code.org>
2021-11-19 16:27:21 -0800

    Working group state at top level

--- apps/src/templates/codeReviewGroups/CodeReviewGroupsLoader.jsx
+++ apps/src/templates/codeReviewGroups/CodeReviewGroupsLoader.jsx
@@ -14,6 +14,13 @@ export default function CodeReviewGroupsLoader({sectionId, groups, setGroups}) {
           .getCodeReviewGroups()
           .then(groups => {
             // are we sure this will set state before first render?
+            // we might be able to call onLoadSuccess as a callback after setGroups,
+            // but the approach is slightly complicated because useState does not support
+            // an explicit callback.
+            // might be straightforward enough to just do ourselves, and I'm not sure how well supported this is,
+            // but here's something out of the box:
+            // https://github.com/the-road-to-learn-react/use-state-with-callback
+            // https://github.com/the-road-to-learn-react/use-state-with-callback/blob/master/src/index.js
             setGroups(groups);
             onLoadSuccess([groups]);
           })
