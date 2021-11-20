3eb20d93b73bf05cdb8d226ddf2a2acaa45f1b33
James Chen <james@code.org>
2021-11-20 12:19:57 -0800

    Add unit tests

--- dashboard/test/controllers/api/v1/users_controller_test.rb
+++ dashboard/test/controllers/api/v1/users_controller_test.rb
@@ -168,6 +168,7 @@ class Api::V1::UsersControllerTest < ActionController::TestCase
   test "a get request to get current returns signed out user info" do
     get :current
     assert_response :success
+    assert_match "no-store", response.headers["Cache-Control"]
     response = JSON.parse(@response.body)
     assert_equal false, response["is_signed_in"]
   end
@@ -177,6 +178,7 @@ class Api::V1::UsersControllerTest < ActionController::TestCase
     sign_in(teacher)
     get :current
     assert_response :success
+    assert_match "no-store", response.headers["Cache-Control"]
     response = JSON.parse(@response.body)
     assert_equal true, response["is_signed_in"]
     assert_equal teacher.id, response["id"]
