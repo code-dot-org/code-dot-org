2ac82d9c39828432142a7e3f9af83240bfaee736
jamescodeorg <71532552+jamescodeorg@users.noreply.github.com>
2021-11-22 14:26:03 -0800

    Revert "Prevent api responses that depend on current_user from being cached"

--- dashboard/app/controllers/api/v1/users_controller.rb
+++ dashboard/app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,6 @@ class Api::V1::UsersController < Api::V1::JsonApiController
 
   # GET /api/v1/users/current
   def current
-    prevent_caching
     if current_user
       render json: {
         id: current_user.id,
