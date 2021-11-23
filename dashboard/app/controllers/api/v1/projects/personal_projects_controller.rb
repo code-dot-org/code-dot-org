d3e17d809d8c9386975dc2016db3994c5523f4c3
jamescodeorg <71532552+jamescodeorg@users.noreply.github.com>
2021-11-23 01:30:40 -0800

    Revert "Revert "Revert "Prevent api responses that depend on current_user from being cached"""

--- dashboard/app/controllers/api/v1/projects/personal_projects_controller.rb
+++ dashboard/app/controllers/api/v1/projects/personal_projects_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::Projects::PersonalProjectsController < ApplicationController
   # GET /api/v1/projects/personal/
   def index
-    prevent_caching
     return head :forbidden unless current_user
     render json: ProjectsList.fetch_personal_projects(current_user.id)
   rescue ArgumentError => e
