1482931fa53f7c6b2aeb688792cae93b4163cdd6
Erin Bond <>
2021-11-29 10:10:39 -0500

    test lesson extras route

--- dashboard/test/models/lesson_test.rb
+++ dashboard/test/models/lesson_test.rb
@@ -170,6 +170,21 @@ class LessonTest < ActiveSupport::TestCase
     assert_equal '/', lesson2.next_level_path_for_lesson_extras(@student)
   end
 
+  test "next_level_path_for_lesson_extras show unit overview" do
+    script = create :script
+    script.stubs(:show_unit_overview_between_lessons?).returns true
+    lesson_group = create :lesson_group, script: script
+    lesson1 = create :lesson, script: script, lesson_group: lesson_group
+    create :script_level, script: script, lesson: lesson1
+    create :script_level, script: script, lesson: lesson1
+    lesson2 = create :lesson, script: script, lesson_group: lesson_group
+    create :script_level, script: script, lesson: lesson2
+    create :script_level, script: script, lesson: lesson2
+
+    assert_equal "/s/#{script.name}", lesson1.next_level_path_for_lesson_extras(@student)
+    assert_equal "/s/#{script.name}", lesson2.next_level_path_for_lesson_extras(@student)
+  end
+
   test 'can summarize lesson with no levels' do
     script = create :script
     lesson_group = create :lesson_group, script: script
