f0c25f5f1dd7c697325e44fffc4d5f68e155bab3
Dani <dani@code.org>
2021-11-29 21:43:25 -0500

    move new course page to use similar set up that new unit page does

---
+++ apps/src/lib/levelbuilder/course-editor/NewCourseForm.jsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import RailsAuthenticityToken from '@cdo/apps/lib/util/RailsAuthenticityToken';
+import PropTypes from 'prop-types';
+import HelpTip from '@cdo/apps/lib/ui/HelpTip';
+import NewCourseFields from '../NewCourseFields';
+
+export default function NewCourseForm(props) {
+  const [familyName, setFamilyName] = useState('');
+  const [versionYear, setVersionYear] = useState('');
+
+  const getCourseName = () => {
+    const name =
+      versionYear !== 'unversioned'
+        ? familyName + '-' + versionYear
+        : familyName;
+    return name;
+  };
+
+  return (
+    <form action="/s" method="post">
+      <RailsAuthenticityToken />
+      <NewCourseFields
+        families={props.families}
+        versionYearOptions={props.versionYearOptions}
+        familyName={familyName}
+        setFamilyName={setFamilyName}
+        versionYear={versionYear}
+        setVersionYear={setVersionYear}
+      />
+      <label>
+        The Course Slug for this course will be:
+        <HelpTip>
+          <p>
+            The course slug is used to create the link to the course. It is in
+            the format of studio.code.org/courses/course-slug-here. A course
+            slug can only contain lowercase letters, numbers and dashes. Once
+            you set the slug it can not be updated.
+          </p>
+        </HelpTip>
+        <input name="name" value={getCourseName()} disabled={true} />
+      </label>
+      <div>
+        <br />
+        <button
+          className="btn btn-primary"
+          type="submit"
+          style={styles.buttonStyle}
+        >
+          Save Changes
+        </button>
+      </div>
+      )}
+    </form>
+  );
+}
+
+NewCourseForm.propTypes = {
+  families: PropTypes.arrayOf(PropTypes.string).isRequired,
+  versionYearOptions: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+const styles = {
+  buttonStyle: {
+    marginLeft: 0,
+    marginTop: 10,
+    marginBottom: 20
+  }
+};
