ba7d2c5ba30db56bab1b4ed59799845af96e3166
Ben Brooks <ben@code.org>
2021-12-01 15:42:29 -0800

    Test dialog opens when button clicked

--- apps/test/unit/templates/manageStudents/ManageCodeReviewGroupsTest.js
+++ apps/test/unit/templates/manageStudents/ManageCodeReviewGroupsTest.js
@@ -1,43 +1,50 @@
 import React, {useState, useEffect} from 'react';
 import ManageCodeReviewGroups from '@cdo/apps/templates/manageStudents/ManageCodeReviewGroups';
+import {expect} from '../../../util/reconfiguredChai';
 import sinon from 'sinon';
 import {shallow, mount} from 'enzyme';
 import Button from '@cdo/apps/templates/Button';
 import CodeReviewGroupsDataApi from '@cdo/apps/templates/codeReviewGroups/CodeReviewGroupsDataApi';
 import {isolateComponent} from 'isolate-components';
+import StylizedBaseDialog from '@cdo/apps/componentLibrary/StylizedBaseDialog';
 // import CodeReviewGroupsStatusToggle from '@cdo/apps/templates/codeReviewGroups/CodeReviewGroupsStatusToggle';
 
 describe('ManageCodeReviewGroups', () => {
+  let wrapper, api;
 
+  beforeEach(() => {
+    api = {
+      getCodeReviewGroups: () => {
+        console.log('called getCodeReviewGroups');
+        return {
+          then: callback => {
+            callback([]);
+          }
+          // fail: () => {}
+        }; //sinon.stub().callArgWith(0, [])}}
+      }
+    };
+
+    wrapper = isolateComponent(<ManageCodeReviewGroups api={api} />);
+  });
   // try with resolves and rejects
-  it('first test', () => {
-    // sinon.stub(React, 'useEffect').callsArg(0)
+  it('click of button opens dialog', () => {
+    expect(wrapper.findOne(StylizedBaseDialog).props.isOpen).to.be.false;
+    wrapper.findOne(Button).props.onClick();
+    expect(wrapper.findOne(StylizedBaseDialog).props.isOpen).to.be.true;
+  });
 
+  it('loads initial group state on render', () => {
+    // sinon.stub(React, 'useEffect').callsArg(0)
     // let api = new CodeReviewGroupsDataApi(123);
     // let dataApi = sinon.stub(api, "getCodeReviewGroups").resolves([]);
     // dataApi.rejects()
     // function callback() {
-
     // }
     // let api = {
     //   getCodeReviewGroups: sinon.stub().resolves(() => sinon.stub().callArgWith(0, []))
     // }
-    let api = {
-      getCodeReviewGroups: () => {
-        console.log("called getCodeReviewGroups");
-        return {
-          then: callback => {callback([]);},
-          // fail: () => {}
-        } //sinon.stub().callArgWith(0, [])}}
-      }
-    }
-    let wrapper = isolateComponent(
-      <ManageCodeReviewGroups
-        api={api}
-      />
-    )
     // wrapper.findOne(Button).props.onClick.simulate("click");
     // console.log(wrapper.debug());
   });
 });
-
