42af5ad9be7cca7ac08ba27e24cd8a742b294d36
Jessica Kulwik <jessica@code.org>
2021-12-01 23:25:33 +0000

    WIP2

--- apps/test/unit/templates/manageStudents/ManageCodeReviewGroupsTest.js
+++ apps/test/unit/templates/manageStudents/ManageCodeReviewGroupsTest.js
@@ -3,30 +3,41 @@ import ManageCodeReviewGroups from '@cdo/apps/templates/manageStudents/ManageCod
 import sinon from 'sinon';
 import {shallow, mount} from 'enzyme';
 import Button from '@cdo/apps/templates/Button';
+import CodeReviewGroupsDataApi from '@cdo/apps/templates/codeReviewGroups/CodeReviewGroupsDataApi';
+import {isolateComponent} from 'isolate-components';
+// import CodeReviewGroupsStatusToggle from '@cdo/apps/templates/codeReviewGroups/CodeReviewGroupsStatusToggle';
 
 describe('ManageCodeReviewGroups', () => {
 
   // try with resolves and rejects
   it('first test', () => {
-    let dataApi = {
-      getCodeReviewGroups: sinon.stub().resolves([])
-    }
-    // let dataApi = {
-    //   getCodeReviewGroups: () => {
-    //     console.log("called getCodeReviewGroups");
-    //     return {
-    //       then: callback => {callback([]);},
-    //       fail: () => {}
-    //     } //sinon.stub().callArgWith(0, [])}}
-    //   }
+    // sinon.stub(React, 'useEffect').callsArg(0)
+
+    // let api = new CodeReviewGroupsDataApi(123);
+    // let dataApi = sinon.stub(api, "getCodeReviewGroups").resolves([]);
+    // dataApi.rejects()
+    // function callback() {
+
     // }
-    let wrapper = mount(
+    // let api = {
+    //   getCodeReviewGroups: sinon.stub().resolves(() => sinon.stub().callArgWith(0, []))
+    // }
+    let api = {
+      getCodeReviewGroups: () => {
+        console.log("called getCodeReviewGroups");
+        return {
+          then: callback => {callback([]);},
+          // fail: () => {}
+        } //sinon.stub().callArgWith(0, [])}}
+      }
+    }
+    let wrapper = isolateComponent(
       <ManageCodeReviewGroups
-        api={dataApi}
+        api={api}
       />
     )
-    wrapper.find(Button).simulate("click");
-    console.log(wrapper.debug());
+    // wrapper.findOne(Button).props.onClick.simulate("click");
+    // console.log(wrapper.debug());
   });
 });
 
