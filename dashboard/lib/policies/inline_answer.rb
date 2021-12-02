29cd326b7d0a45074e0d6830469d69a4cdd3efe4
Dani <dani@code.org>
2021-12-01 22:14:51 -0500

    Allow inline answers to be shown to instructors in training

--- dashboard/lib/policies/inline_answer.rb
+++ dashboard/lib/policies/inline_answer.rb
@@ -1,5 +1,4 @@
-# This class contains policies relating to our "teachers can view levels with
-# the answers included" feature
+# This class contains policies relating to our content which only instructors can see
 class Policies::InlineAnswer
   # Returns a boolean indicating whether or not the given user should be
   # allowed to view answers for the given script_level
@@ -10,6 +9,10 @@ class Policies::InlineAnswer
 
     script = script_level.try(:script)
     return true if visible_for_unit?(user, script)
+
+    # In PL Courses if a level is marked as an instructor in training level and the user
+    # can be a participant in the course show the instructor only content
+    return true if script.pl_course? && script&.can_be_participant?(user) && script_level.instructor_in_training
 
     # Teachers can also put lessons into a readonly mode in which students are
     # able to view the answers
