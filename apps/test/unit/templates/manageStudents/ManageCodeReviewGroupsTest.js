2216dadca4876a59942a63af999a3508deb8da53
Ben Brooks <ben@code.org>
2021-12-01 17:07:18 -0800

    Test loading and saving groups

--- apps/test/unit/templates/manageStudents/ManageCodeReviewGroupsTest.js
+++ apps/test/unit/templates/manageStudents/ManageCodeReviewGroupsTest.js
@@ -1,50 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import ManageCodeReviewGroups from '@cdo/apps/templates/manageStudents/ManageCodeReviewGroups';
 import {expect} from '../../../util/reconfiguredChai';
 import sinon from 'sinon';
-import {shallow, mount} from 'enzyme';
 import Button from '@cdo/apps/templates/Button';
-import CodeReviewGroupsDataApi from '@cdo/apps/templates/codeReviewGroups/CodeReviewGroupsDataApi';
 import {isolateComponent} from 'isolate-components';
 import StylizedBaseDialog from '@cdo/apps/componentLibrary/StylizedBaseDialog';
-// import CodeReviewGroupsStatusToggle from '@cdo/apps/templates/codeReviewGroups/CodeReviewGroupsStatusToggle';
+import CodeReviewGroupsManager from '@cdo/apps/templates/codeReviewGroups/CodeReviewGroupsManager';
 
 describe('ManageCodeReviewGroups', () => {
-  let wrapper, api;
+  let wrapper, dataApi, fakeGroups;
 
   beforeEach(() => {
-    api = {
+    fakeGroups = ['fake groups'];
+    dataApi = {
       getCodeReviewGroups: () => {
-        console.log('called getCodeReviewGroups');
         return {
-          then: callback => {
-            callback([]);
-          }
-          // fail: () => {}
-        }; //sinon.stub().callArgWith(0, [])}}
-      }
+          then: callback => callback(fakeGroups)
+        };
+      },
+      setCodeReviewGroups: sinon.stub().returns({
+        then: callback => callback()
+      })
     };
 
-    wrapper = isolateComponent(<ManageCodeReviewGroups api={api} />);
+    wrapper = isolateComponent(<ManageCodeReviewGroups dataApi={dataApi} />);
   });
-  // try with resolves and rejects
+
   it('click of button opens dialog', () => {
     expect(wrapper.findOne(StylizedBaseDialog).props.isOpen).to.be.false;
     wrapper.findOne(Button).props.onClick();
     expect(wrapper.findOne(StylizedBaseDialog).props.isOpen).to.be.true;
   });
 
-  it('loads initial group state on render', () => {
-    // sinon.stub(React, 'useEffect').callsArg(0)
-    // let api = new CodeReviewGroupsDataApi(123);
-    // let dataApi = sinon.stub(api, "getCodeReviewGroups").resolves([]);
-    // dataApi.rejects()
-    // function callback() {
-    // }
-    // let api = {
-    //   getCodeReviewGroups: sinon.stub().resolves(() => sinon.stub().callArgWith(0, []))
-    // }
-    // wrapper.findOne(Button).props.onClick.simulate("click");
-    // console.log(wrapper.debug());
+  it('loads initial group state on initial render', () => {
+    expect(wrapper.findOne(CodeReviewGroupsManager).props.groups).to.equal(
+      fakeGroups
+    );
+  });
+
+  it('sends API request to update groups after confirming changes', () => {
+    const newGroups = ['new groups'];
+    wrapper.findOne(CodeReviewGroupsManager).props.setGroups(newGroups);
+
+    expect(wrapper.findOne(CodeReviewGroupsManager).props.groups).to.equal(
+      newGroups
+    );
+    wrapper.findOne(StylizedBaseDialog).props.handleConfirmation();
+    sinon.assert.calledOnceWithExactly(dataApi.setCodeReviewGroups, newGroups);
   });
 });
