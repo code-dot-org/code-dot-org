5f51d2e552cafd8a6179aaa2528d970fb1f61809
Dani <dani@code.org>
2021-12-03 16:20:14 -0500

    Make a helper method to check if should view as instructor in training

--- dashboard/app/helpers/levels_helper.rb
+++ dashboard/app/helpers/levels_helper.rb
@@ -306,7 +306,7 @@ module LevelsHelper
     end
 
     if @script_level && current_user
-      @app_options[:instructorInTraining] = @script_level.instructor_in_training && @script.pl_course? && @script.can_be_participant?(current_user)
+      @app_options[:instructorInTraining] = @script_level.view_as_instructor_in_training?(current_user)
     end
 
     # Blockly caches level properties, whereas this field depends on the user
