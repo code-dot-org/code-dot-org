558288917c7fd322ec00cbe2194a898da48c7842
Steffan Musoke <steffanmusoke@gmail.com>
2021-12-03 09:12:48 -0600

    fix: add query parameter to set locale redirect so browsers dont use old cached response

--- dashboard/app/controllers/home_controller.rb
+++ dashboard/app/controllers/home_controller.rb
@@ -19,7 +19,8 @@ class HomeController < ApplicationController
   def set_locale
     set_locale_cookie(params[:locale]) if params[:locale]
     if params[:i18npath]
-      redirect_to "/#{params[:i18npath]}"
+      # Query parameter for browser cache to be avoided and load new locale
+      redirect_to "/#{params[:i18npath]}?lang=#{params[:locale]}"
     elsif params[:user_return_to]
       redirect_to URI.parse(params[:user_return_to].to_s).path
     else
