ec0eded0a5920783ecf2d6b20968d894ba04cba8
Dani <dani@code.org>
2021-12-04 15:27:44 -0500

    Start adding testings

--- apps/test/unit/lib/levelbuilder/NewCourseFieldsTest.jsx
+++ apps/test/unit/lib/levelbuilder/NewCourseFieldsTest.jsx
@@ -12,8 +12,240 @@ describe('NewCourseFieldsTest', () => {
     };
   });
 
-  it('updates markdown', () => {
+  it('resetting familyNameSelector makes isVersionedSelector disappear', () => {
     const wrapper = shallow(<NewCourseFields {...defaultProps} />);
-    expect(wrapper.find('NewCourseFields').length).to.equal(1);
+    expect(wrapper.find('.familyNameSelector').length).to.equal(1);
+
+    expect(wrapper.find('isVersionedSelector').length).to.equal(0);
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: 'family-1'}});
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: ''}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
+  });
+
+  it('resetting isVersionedSelector makes versionYearSelector disappear', () => {
+    const wrapper = shallow(<NewCourseFields {...defaultProps} />);
+    expect(wrapper.find('.familyNameSelector').length).to.equal(1);
+
+    expect(wrapper.find('isVersionedSelector').length).to.equal(0);
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: 'family-1'}});
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: ''}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
+
+    wrapper
+      .find('.familyNameInput')
+      .simulate('change', {target: {value: 'new-family-name'}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'no'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'yes'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(false);
+
+    wrapper
+      .find('.versionYearSelector')
+      .simulate('change', {target: {value: '1991'}});
+
+    expect(wrapper.find('.versionYearSelector').value).to.equal('1991');
+  });
+
+  it('can select existing family name and unversioned course', () => {
+    const wrapper = shallow(<NewCourseFields {...defaultProps} />);
+    expect(wrapper.find('.familyNameSelector').length).to.equal(1);
+
+    expect(wrapper.find('isVersionedSelector').length).to.equal(0);
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: 'family-1'}});
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: ''}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
+
+    wrapper
+      .find('.familyNameInput')
+      .simulate('change', {target: {value: 'new-family-name'}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'no'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'yes'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(false);
+
+    wrapper
+      .find('.versionYearSelector')
+      .simulate('change', {target: {value: '1991'}});
+
+    expect(wrapper.find('.versionYearSelector').value).to.equal('1991');
+  });
+
+  it('can create new family name and unversioned course', () => {
+    const wrapper = shallow(<NewCourseFields {...defaultProps} />);
+    expect(wrapper.find('.familyNameSelector').length).to.equal(1);
+
+    expect(wrapper.find('isVersionedSelector').length).to.equal(0);
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: 'family-1'}});
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: ''}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
+
+    wrapper
+      .find('.familyNameInput')
+      .simulate('change', {target: {value: 'new-family-name'}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'no'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'yes'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(false);
+
+    wrapper
+      .find('.versionYearSelector')
+      .simulate('change', {target: {value: '1991'}});
+
+    expect(wrapper.find('.versionYearSelector').value).to.equal('1991');
+  });
+
+  it('can select existing family name and versioned course', () => {
+    const wrapper = shallow(<NewCourseFields {...defaultProps} />);
+    expect(wrapper.find('.familyNameSelector').length).to.equal(1);
+
+    expect(wrapper.find('isVersionedSelector').length).to.equal(0);
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: 'family-1'}});
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: ''}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
+
+    wrapper
+      .find('.familyNameInput')
+      .simulate('change', {target: {value: 'new-family-name'}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'no'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'yes'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(false);
+
+    wrapper
+      .find('.versionYearSelector')
+      .simulate('change', {target: {value: '1991'}});
+
+    expect(wrapper.find('.versionYearSelector').value).to.equal('1991');
+  });
+
+  it('can create new family name and versioned course', () => {
+    const wrapper = shallow(<NewCourseFields {...defaultProps} />);
+    expect(wrapper.find('.familyNameSelector').length).to.equal(1);
+
+    expect(wrapper.find('isVersionedSelector').length).to.equal(0);
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: 'family-1'}});
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.familyNameSelector')
+      .simulate('change', {target: {value: ''}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
+
+    wrapper
+      .find('.familyNameInput')
+      .simulate('change', {target: {value: 'new-family-name'}});
+
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'no'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
+
+    wrapper
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'yes'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+    expect(wrapper.find('.versionYearSelector').disabled).to.equal(false);
+
+    wrapper
+      .find('.versionYearSelector')
+      .simulate('change', {target: {value: '1991'}});
+
+    expect(wrapper.find('.versionYearSelector').value).to.equal('1991');
   });
 });
