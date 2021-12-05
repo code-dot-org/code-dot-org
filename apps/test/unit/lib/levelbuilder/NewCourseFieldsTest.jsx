2244434831f6d7fe65fc1a9c571d29b73c93b6f6
Dani <dani@code.org>
2021-12-05 07:18:12 -0500

    Finish tests for NewCourseFields

--- apps/test/unit/lib/levelbuilder/NewCourseFieldsTest.jsx
+++ apps/test/unit/lib/levelbuilder/NewCourseFieldsTest.jsx
@@ -40,37 +40,32 @@ describe('NewCourseFieldsTest', () => {
     expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
 
     wrapper
-      .find('.familyNameSelector')
+      .find('.isVersionedSelector')
+      .simulate('change', {target: {value: 'no'}});
+
+    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
+
+    wrapper
+      .find('.isVersionedSelector')
       .simulate('change', {target: {value: ''}});
 
-    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
+    expect(wrapper.find('.versionYearSelector').length).to.equal(0);
+  });
 
+  it('resetting familyNameInput makes isVersionedSelector disappear', () => {
+    const wrapper = shallow(<NewCourseFields {...defaultProps} />);
+    expect(wrapper.find('.familyNameSelector').length).to.equal(1);
+
+    expect(wrapper.find('isVersionedSelector').length).to.equal(0);
     wrapper
       .find('.familyNameInput')
       .simulate('change', {target: {value: 'new-family-name'}});
 
     expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
 
-    wrapper
-      .find('.isVersionedSelector')
-      .simulate('change', {target: {value: 'no'}});
+    wrapper.find('.familyNameInput').simulate('change', {target: {value: ''}});
 
-    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
-    expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
-    expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
-
-    wrapper
-      .find('.isVersionedSelector')
-      .simulate('change', {target: {value: 'yes'}});
-
-    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
-    expect(wrapper.find('.versionYearSelector').disabled).to.equal(false);
-
-    wrapper
-      .find('.versionYearSelector')
-      .simulate('change', {target: {value: '1991'}});
-
-    expect(wrapper.find('.versionYearSelector').value).to.equal('1991');
+    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
   });
 
   it('can select existing family name and unversioned course', () => {
@@ -84,37 +79,12 @@ describe('NewCourseFieldsTest', () => {
     expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
 
     wrapper
-      .find('.familyNameSelector')
-      .simulate('change', {target: {value: ''}});
-
-    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
-
-    wrapper
-      .find('.familyNameInput')
-      .simulate('change', {target: {value: 'new-family-name'}});
-
-    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
-
-    wrapper
       .find('.isVersionedSelector')
       .simulate('change', {target: {value: 'no'}});
 
     expect(wrapper.find('.versionYearSelector').length).to.equal(1);
     expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
     expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
-
-    wrapper
-      .find('.isVersionedSelector')
-      .simulate('change', {target: {value: 'yes'}});
-
-    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
-    expect(wrapper.find('.versionYearSelector').disabled).to.equal(false);
-
-    wrapper
-      .find('.versionYearSelector')
-      .simulate('change', {target: {value: '1991'}});
-
-    expect(wrapper.find('.versionYearSelector').value).to.equal('1991');
   });
 
   it('can create new family name and unversioned course', () => {
@@ -123,17 +93,6 @@ describe('NewCourseFieldsTest', () => {
 
     expect(wrapper.find('isVersionedSelector').length).to.equal(0);
     wrapper
-      .find('.familyNameSelector')
-      .simulate('change', {target: {value: 'family-1'}});
-    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
-
-    wrapper
-      .find('.familyNameSelector')
-      .simulate('change', {target: {value: ''}});
-
-    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
-
-    wrapper
       .find('.familyNameInput')
       .simulate('change', {target: {value: 'new-family-name'}});
 
@@ -146,19 +105,6 @@ describe('NewCourseFieldsTest', () => {
     expect(wrapper.find('.versionYearSelector').length).to.equal(1);
     expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
     expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
-
-    wrapper
-      .find('.isVersionedSelector')
-      .simulate('change', {target: {value: 'yes'}});
-
-    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
-    expect(wrapper.find('.versionYearSelector').disabled).to.equal(false);
-
-    wrapper
-      .find('.versionYearSelector')
-      .simulate('change', {target: {value: '1991'}});
-
-    expect(wrapper.find('.versionYearSelector').value).to.equal('1991');
   });
 
   it('can select existing family name and versioned course', () => {
@@ -170,26 +116,6 @@ describe('NewCourseFieldsTest', () => {
       .find('.familyNameSelector')
       .simulate('change', {target: {value: 'family-1'}});
     expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
-
-    wrapper
-      .find('.familyNameSelector')
-      .simulate('change', {target: {value: ''}});
-
-    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
-
-    wrapper
-      .find('.familyNameInput')
-      .simulate('change', {target: {value: 'new-family-name'}});
-
-    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
-
-    wrapper
-      .find('.isVersionedSelector')
-      .simulate('change', {target: {value: 'no'}});
-
-    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
-    expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
-    expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
 
     wrapper
       .find('.isVersionedSelector')
@@ -211,29 +137,10 @@ describe('NewCourseFieldsTest', () => {
 
     expect(wrapper.find('isVersionedSelector').length).to.equal(0);
     wrapper
-      .find('.familyNameSelector')
-      .simulate('change', {target: {value: 'family-1'}});
-    expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
-
-    wrapper
-      .find('.familyNameSelector')
-      .simulate('change', {target: {value: ''}});
-
-    expect(wrapper.find('.isVersionedSelector').length).to.equal(0);
-
-    wrapper
       .find('.familyNameInput')
       .simulate('change', {target: {value: 'new-family-name'}});
 
     expect(wrapper.find('.isVersionedSelector').length).to.equal(1);
-
-    wrapper
-      .find('.isVersionedSelector')
-      .simulate('change', {target: {value: 'no'}});
-
-    expect(wrapper.find('.versionYearSelector').length).to.equal(1);
-    expect(wrapper.find('.versionYearSelector').value).to.equal('unversioned');
-    expect(wrapper.find('.versionYearSelector').disabled).to.equal(true);
 
     wrapper
       .find('.isVersionedSelector')
