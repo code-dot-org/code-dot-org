843e58fa4ac7623af9a17c8e9445d6470f87ba91
Dani <dani@code.org>
2021-12-07 07:13:50 -0500

    Get match levels to show teacher only content

--- apps/src/sites/studio/pages/levels/_match.js
+++ apps/src/sites/studio/pages/levels/_match.js
@@ -2,14 +2,24 @@
 import React from 'react';
 import {showDialog} from '@cdo/apps/code-studio/levels/dialogHelper';
 import {LegacyMatchAngiGifDialog} from '@cdo/apps/lib/ui/LegacyDialogContents';
+import {getStore} from '@cdo/apps/code-studio/redux';
+import {setViewType, ViewType} from '@cdo/apps/code-studio/viewAsRedux';
+import getScriptData from '@cdo/apps/util/getScriptData';
 
 import Match from '@cdo/apps/code-studio/levels/match';
 window.Match = Match;
 
 $(function() {
+  const data = getScriptData('match');
   // This setting (pre_title) is used by only 3 levels in our application.
   if (appOptions.dialog.preTitle) {
     // Note: This dialog depends on the presence of some haml, found in _dialog.html.haml
     window.setTimeout(() => showDialog(<LegacyMatchAngiGifDialog />), 1000);
+  }
+
+  const store = getStore();
+
+  if (data.is_instructor) {
+    store.dispatch(setViewType(ViewType.Instructor));
   }
 });
