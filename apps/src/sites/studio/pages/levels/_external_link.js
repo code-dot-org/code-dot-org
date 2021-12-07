0c9333c80d3db0e17c19fe09bf22c12c128d35d1
Dani <dani@code.org>
2021-12-07 12:34:57 -0500

    Update a bunch more levels to set viewing as instructor if instructor or instructor in training

--- apps/src/sites/studio/pages/levels/_external_link.js
+++ apps/src/sites/studio/pages/levels/_external_link.js
@@ -1,5 +1,8 @@
 import {processResults} from '@cdo/apps/code-studio/levels/dialogHelper';
 import {registerGetResult} from '@cdo/apps/code-studio/levels/codeStudioLevels';
+import {getStore} from '@cdo/apps/code-studio/redux';
+import {setViewType, ViewType} from '@cdo/apps/code-studio/viewAsRedux';
+import getScriptData from '@cdo/apps/util/getScriptData';
 
 registerGetResult();
 
@@ -11,4 +14,10 @@ $(document).on('click', '.submitButton', () => {
   // Avoid multiple simultaneous submissions.
   submitButton.attr('disabled', true);
   processResults();
+
+  const data = getScriptData('externallink');
+  const store = getStore();
+  if (data.is_instructor || data.is_instructor_in_training) {
+    store.dispatch(setViewType(ViewType.Instructor));
+  }
 });
