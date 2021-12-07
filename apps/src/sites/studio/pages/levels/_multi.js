ced6799608ab16328a60de1e0949a03f97b625d6
Dani <dani@code.org>
2021-12-07 07:17:30 -0500

    Make teacher only show for multi levels

--- apps/src/sites/studio/pages/levels/_multi.js
+++ apps/src/sites/studio/pages/levels/_multi.js
@@ -1,2 +1,17 @@
+import React from 'react';
+import {getStore} from '@cdo/apps/code-studio/redux';
+import {setViewType, ViewType} from '@cdo/apps/code-studio/viewAsRedux';
+import getScriptData from '@cdo/apps/util/getScriptData';
+
 import Multi from '@cdo/apps/code-studio/levels/multi';
 window.Multi = Multi;
+
+$(function() {
+  const data = getScriptData('multi');
+
+  const store = getStore();
+
+  if (data.is_instructor) {
+    store.dispatch(setViewType(ViewType.Instructor));
+  }
+});
