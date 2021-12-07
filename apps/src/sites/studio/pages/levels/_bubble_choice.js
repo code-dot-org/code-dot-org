5e78840900d71be71564abcaf363acb028fc7171
Dani <dani@code.org>
2021-12-07 07:05:14 -0500

    Make teacher only content show on bubble choice level

--- apps/src/sites/studio/pages/levels/_bubble_choice.js
+++ apps/src/sites/studio/pages/levels/_bubble_choice.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BubbleChoice from '@cdo/apps/code-studio/components/BubbleChoice';
+import {getStore} from '@cdo/apps/code-studio/redux';
+import {setViewType, ViewType} from '@cdo/apps/code-studio/viewAsRedux';
 
 const script = document.querySelector('script[data-bubblechoice]');
 const data = JSON.parse(script.dataset.bubblechoice);
@@ -11,6 +13,12 @@ level.sublevels = data.level.sublevels.map(sublevel => {
   return sublevel;
 });
 level.id = level.id.toString();
+
+const store = getStore();
+
+if (data.is_instructor) {
+  store.dispatch(setViewType(ViewType.Instructor));
+}
 
 ReactDOM.render(
   <BubbleChoice level={level} />,
