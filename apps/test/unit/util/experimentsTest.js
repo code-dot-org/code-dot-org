fb694dc56c7dcbd036ee6bba9a33e9469c63cc30
Dayne <dayne@code.org>
2021-12-08 15:03:21 -0800

    Refactored dcdo into a class with unit tests

--- apps/test/unit/util/experimentsTest.js
+++ apps/test/unit/util/experimentsTest.js
@@ -1,22 +1,24 @@
 import experiments from '@cdo/apps/util/experiments';
 import {expect} from '../../util/reconfiguredChai';
+import DCDO from '@cdo/apps/dcdo';
 
 describe('experiments.isEnabled', () => {
+  afterEach(() => {
+    // cleanup changes the tests made to the global DCDO object.
+    DCDO.reset();
+  });
   it('returns false given unknown experiment', () => {
     expect(experiments.isEnabled('unknown-experiment')).to.equal(false);
   });
 
   it('returns false given dcdo experiment not enabled', () => {
+    DCDO.set('test-experiment', false);
     window.dcdo = {'test-experiment': false};
     expect(experiments.isEnabled('test-experiment')).to.equal(false);
-    // cleanup
-    window.dcdo = undefined;
   });
 
-  it('returns false given dcdo experiment not enabled', () => {
-    window.dcdo = {'test-experiment': true};
+  it('returns true given dcdo experiment is enabled', () => {
+    DCDO.set('test-experiment', true);
     expect(experiments.isEnabled('test-experiment')).to.equal(true);
-    // cleanup
-    window.dcdo = undefined;
   });
 });
