0a8b9b09776b55a04fd6d67f82c875d0540d81c8
Dayne <dayne@code.org>
2021-12-09 10:41:09 -0800

    Adding comments about caching concerns

--- lib/dynamic_config/dcdo.rb
+++ lib/dynamic_config/dcdo.rb
@@ -85,6 +85,9 @@ class DCDOBase
   # Generate the a mapping of DCDO configurations we want to forward to frontend code so it can have
   # access to the values. For example, boolean DCDO config could be used to turn a new feature on or
   # off.
+  # Please note that for the frontend Javascript code, this data could be stale due to the caching
+  # behavior of the page. Analyze the HTTP headers of the pages you are interested in to understand
+  # what kind of caching they use and if that will be a concern.
   # @return [Hash] A mapping of DCDO keys to values which we want frontend code to have access to.
   def frontend_config
     # Add DCDO configurations you would like to be available on the frontend/javascript.
