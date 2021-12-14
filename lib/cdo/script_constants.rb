3a7882854a5799b0608ad6f5e16d90f457c28779
Dani <dani@code.org>
2021-12-14 10:11:57 -0500

    Get rid of special case for 2016 drafts of csf

--- lib/cdo/script_constants.rb
+++ lib/cdo/script_constants.rb
@@ -132,14 +132,6 @@ module ScriptConstants
       HOC_IMPACT_STUDY_NAME = 'hoc-impact-study'.freeze,
       FLAPPY_IMPACT_STUDY_NAME = 'flappy-impact-study'.freeze
     ],
-    csf2_draft: [
-      COURSEA_DRAFT_NAME = 'coursea-draft'.freeze,
-      COURSEB_DRAFT_NAME = 'courseb-draft'.freeze,
-      COURSEC_DRAFT_NAME = 'coursec-draft'.freeze,
-      COURSED_DRAFT_NAME = 'coursed-draft'.freeze,
-      COURSEE_DRAFT_NAME = 'coursee-draft'.freeze,
-      COURSEF_DRAFT_NAME = 'coursef-draft'.freeze,
-    ],
     csd_pilot: [
       CSD1_PILOT_NAME = 'csd1-pilot'.freeze,
       CSD2_PILOT_NAME = 'csd2-pilot'.freeze,
