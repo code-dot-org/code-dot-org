3945c78cad05c7792cf3ec12f72fc36df429ea41
Dani <dani@code.org>
2021-12-14 21:50:54 -0500

    Move to using can_be_participant

--- dashboard/app/models/unit_group.rb
+++ dashboard/app/models/unit_group.rb
@@ -533,7 +533,7 @@ class UnitGroup < ApplicationRecord
 
     # Restrictions only apply to participants and logged out users.
     return false if user.nil?
-    return true unless participant?(user)
+    return true unless can_be_participant?(user)
 
     # A student can view the course version if they are assigned to it or they have progress in it.
     user.section_courses.include?(self) || has_progress?(user)
