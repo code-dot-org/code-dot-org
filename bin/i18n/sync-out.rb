55be2587f07e19be5a5c93ea21d56eacb6909607
hacodeorg <ha@code.org>
2021-12-15 17:58:09 -0800

    Adds locations of etags.json and files_to_sync_out.json to CROWDIN_PROJECTS setting. Makes it easier to manage multiple projects.
    Since files_to_sync_out.json is important and should not be deleted by accident, put it into bin/i18n/crowdin instead of /tmp.

--- bin/i18n/sync-out.rb
+++ bin/i18n/sync-out.rb
@@ -53,19 +53,17 @@ end
 #  "/dashboard/base.yml", "/blockly-mooc/maze.json",
 #  "/course_content/2018/coursea-2018.json", etc.
 def file_changed?(locale, file)
-  @change_datas ||= CROWDIN_PROJECTS.keys.map do |crowdin_project|
-    project = Crowdin::Project.new(crowdin_project, nil)
-    utils = Crowdin::Utils.new(project)
-    unless File.exist?(utils.files_to_sync_out_json)
+  @change_datas ||= CROWDIN_PROJECTS.map do |_project_identifier, project_options|
+    unless File.exist?(project_options[:files_to_sync_out_json])
       raise <<~ERR
-        File not found #{utils.files_to_sync_out_json}.
+        File not found #{project_options[:files_to_sync_out_json]}.
 
         We expect to find a file containing a list of files changed by the most
         recent sync down; if this file does not exist, it likely means that no
         sync down has been run on this machine, so there is nothing to sync out
       ERR
     end
-    JSON.load File.read(utils.files_to_sync_out_json)
+    JSON.load File.read(project_options[:files_to_sync_out_json])
   end
 
   crowdin_code = Languages.get_code_by_locale(locale)
