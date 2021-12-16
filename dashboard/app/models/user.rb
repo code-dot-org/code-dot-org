fdeb0f3da216b36b90e61742472cc89b08d31b8b
Dani <dani@code.org>
2021-12-16 14:58:09 -0500

    Add comments about the verified methods

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -1322,12 +1322,20 @@ class User < ApplicationRecord
     user_type == TYPE_TEACHER
   end
 
+  # This method just checks if a user has the authorized teacher permission
+  # if you are hoping to know if someone can access content for verified instructors
+  # you should use the verified_instructor? method instead which includes checks for a
+  # couple different permissions that should have access instructor only content such
+  # as levelbuilders
   def verified_teacher?
     permission?(UserPermission::AUTHORIZED_TEACHER)
   end
 
+  # A user is a verified instructor if you are a universal_instructor, plc_reviewer,
+  # facilitator, authorized_teacher, or levelbuilder. All of these permissions tell us someone
+  # should be trusted with locked down instructor only content. It is important to use this
+  # method instead of verified_teacher? as teachers will not be instructors for all courses
   def verified_instructor?
-    # You are an verified instructor if you are a universal_instructor, plc_reviewer, facilitator, authorized_teacher, or levelbuiler
     permission?(UserPermission::UNIVERSAL_INSTRUCTOR) || permission?(UserPermission::PLC_REVIEWER) ||
       permission?(UserPermission::FACILITATOR) || permission?(UserPermission::AUTHORIZED_TEACHER) ||
       permission?(UserPermission::LEVELBUILDER)
