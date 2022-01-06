6d9c7c23c089f145439e96182934f123abef73b9
Mike Harvey <mike.harvey@code.org>
2022-01-06 09:32:25 -0500

    add flexbox to ArrowButtons

--- apps/src/templates/ArrowButtons.jsx
+++ apps/src/templates/ArrowButtons.jsx
@@ -56,7 +56,7 @@ class ArrowButtons extends React.Component {
 
 const styles = {
   hidden: {display: 'none'},
-  visible: {display: 'inline-block'}
+  visible: {display: 'flex'}
 };
 
 export default connect(state => ({
