0d8e4c7fe22e86191c1a039966c4b7ff2aabce4f
Erin Peach <erin@code.org>
2022-01-05 16:58:44 -0800

    Remove unneeed properties

--- apps/src/lib/kits/maker/boards/microBit/ExternalButton.js
+++ apps/src/lib/kits/maker/boards/microBit/ExternalButton.js
@@ -2,8 +2,6 @@ import {EventEmitter} from 'events';
 import '../../../../../utils'; // For Function.prototype.inherits
 
 export default function ExternalButton(board) {
-  // There are six button events, ['', 'down', 'up', 'click', 'long-click', 'hold']
-  this.buttonEvents = new Array(6).fill(0);
   this.board = board;
   this.board.mb.trackDigitalPin(this.board.pin, 0);
   // Flag to only trigger event on first of type
@@ -11,7 +9,6 @@ export default function ExternalButton(board) {
 
   this.board.mb.trackDigitalComponent(this.board.pin, (sourceID, eventID) => {
     if (this.board.pin === sourceID) {
-      this.buttonEvents[eventID]++;
       if (eventID === 1 && !this.connect) {
         this.emit('down');
         this.connect = true;
@@ -25,7 +22,6 @@ export default function ExternalButton(board) {
   // Add a read-only `isPressed` property
   Object.defineProperties(this, {
     isPressed: {
-      // More 'down' events than 'up' indicates we are in a pressed state
       get: function() {
         return !this.board.mb.digitalInput[this.board.pin];
       }
