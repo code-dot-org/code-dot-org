4014a5dd45d57d0c7e26ed0d7a3f5c5659ad6dc6
Meg Crenshaw <meg@code.org>
2022-01-07 13:31:15 -0500

    Use save! for error messaging

--- bin/oneoff/update_app_ids_in_enrollments.rb
+++ bin/oneoff/update_app_ids_in_enrollments.rb
@@ -6,4 +6,4 @@ puts "Updating application_id in enrollments ...\n\n"
 
 # The method set_application_id is triggered before a save
 # Save the enrollment to update set_application_id
-Pd::Enrollment.where.not(user_id: nil).each(&:save)
+Pd::Enrollment.where.not(user_id: nil).each(&:save!)
