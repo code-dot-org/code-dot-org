4a1b038c4472ee5e4a895ecb337f14ddd2bbe60d
David Bailey <davidsbailey@users.noreply.github.com>
2022-01-07 15:38:55 -0800

    generalize wrapperWithTutorial to wrapperWithParams

--- apps/test/unit/templates/CertificateTest.js
+++ apps/test/unit/templates/CertificateTest.js
@@ -9,10 +9,10 @@ import isRtl from '@cdo/apps/code-studio/isRtlRedux';
 
 const store = createStore(combineReducers({responsive, isRtl}));
 
-function wrapperWithTutorial(tutorial) {
+function wrapperWithParams(params) {
   return mount(
     <Provider store={store}>
-      <Certificate tutorial={tutorial} />
+      <Certificate {...params} />
     </Provider>
   );
 }
@@ -32,28 +32,28 @@ describe('Certificate', () => {
   });
 
   it('renders Minecraft certificate for Minecraft Adventurer', () => {
-    const wrapper = wrapperWithTutorial('mc');
+    const wrapper = wrapperWithParams({tutorial: 'mc'});
     expect(wrapper.find('img').html()).to.include(
       'MC_Hour_Of_Code_Certificate'
     );
   });
 
   it('renders Minecraft certificate for Minecraft Designer', () => {
-    const wrapper = wrapperWithTutorial('minecraft');
+    const wrapper = wrapperWithParams({tutorial: 'minecraft'});
     expect(wrapper.find('img').html()).to.include(
       'MC_Hour_Of_Code_Certificate'
     );
   });
 
   it("renders unique certificate for Minecraft Hero's Journey", () => {
-    const wrapper = wrapperWithTutorial('hero');
+    const wrapper = wrapperWithParams({tutorial: 'hero'});
     expect(wrapper.find('img').html()).to.include(
       'MC_Hour_Of_Code_Certificate_Hero'
     );
   });
 
   it('renders unique certificate for Minecraft Voyage Aquatic', () => {
-    const wrapper = wrapperWithTutorial('aquatic');
+    const wrapper = wrapperWithParams({tutorial: 'aquatic'});
     expect(wrapper.find('img').html()).to.include(
       'MC_Hour_Of_Code_Certificate_Aquatic'
     );
@@ -61,7 +61,7 @@ describe('Certificate', () => {
 
   it('renders default certificate for all other tutorials', () => {
     ['applab-intro', 'dance', 'flappy', 'frozen'].forEach(tutorial => {
-      const wrapper = wrapperWithTutorial(tutorial);
+      const wrapper = wrapperWithParams({tutorial});
       expect(wrapper.find('img').html()).to.include('hour_of_code_certificate');
     });
   });
