7fde703289197ec6c035c3320944949d3f9f9944
David Bailey <davidsbailey@users.noreply.github.com>
2022-01-07 16:49:07 -0800

    add unit tests verifying personalized certificate image src

--- apps/test/unit/templates/CertificateTest.js
+++ apps/test/unit/templates/CertificateTest.js
@@ -6,6 +6,7 @@ import Certificate from '@cdo/apps/templates/Certificate';
 import {combineReducers, createStore} from 'redux';
 import responsive from '@cdo/apps/code-studio/responsiveRedux';
 import isRtl from '@cdo/apps/code-studio/isRtlRedux';
+import sinon from 'sinon';
 
 const store = createStore(combineReducers({responsive, isRtl}));
 
@@ -63,6 +64,90 @@ describe('Certificate', () => {
     ['applab-intro', 'dance', 'flappy', 'frozen'].forEach(tutorial => {
       const wrapper = wrapperWithParams({tutorial});
       expect(wrapper.find('img').html()).to.include('hour_of_code_certificate');
+    });
+  });
+
+  describe('personalized certificate', () => {
+    let server;
+
+    beforeEach(() => {
+      server = sinon.fakeServer.create();
+    });
+
+    afterEach(() => {
+      server.restore();
+    });
+
+    it('renders using pegasus without experiment', () => {
+      const data = {
+        certificate_sent: true,
+        name: 'Student'
+      };
+      server.respondWith('POST', `/v2/certificate`, [
+        200,
+        {'Content-Type': 'application/json'},
+        JSON.stringify(data)
+      ]);
+
+      const wrapper = wrapperWithParams({
+        tutorial: 'dance',
+        certificateId: 'sessionId'
+      });
+      let image = wrapper.find('#uitest-certificate img');
+      expect(image.prop('src')).to.match(
+        /^\/_karma_webpack_\/hour_of_code_certificate/
+      );
+
+      const input = wrapper.find('input#name');
+      input.simulate('change', {target: {value: 'Student'}});
+      const submitButton = wrapper
+        .find('button')
+        .filterWhere(button => button.text() === 'Submit');
+      submitButton.simulate('click');
+      server.respond();
+
+      wrapper.update();
+      image = wrapper.find('#uitest-certificate img');
+      expect(image.prop('src')).to.equal(
+        'https://code.org/api/hour/certificate/sessionId.jpg'
+      );
+    });
+
+    it('renders using code studio with experiment', () => {
+      const data = {
+        certificate_sent: true,
+        name: 'Student'
+      };
+      server.respondWith('POST', `/v2/certificate`, [
+        200,
+        {'Content-Type': 'application/json'},
+        JSON.stringify(data)
+      ]);
+
+      const wrapper = wrapperWithParams({
+        tutorial: 'dance',
+        certificateId: 'sessionId',
+        showStudioCertificate: true
+      });
+      let image = wrapper.find('#uitest-certificate img');
+      expect(image.prop('src')).to.match(
+        /^\/_karma_webpack_\/hour_of_code_certificate/
+      );
+
+      const input = wrapper.find('input#name');
+      input.simulate('change', {target: {value: 'Student'}});
+      const submitButton = wrapper
+        .find('button')
+        .filterWhere(button => button.text() === 'Submit');
+      submitButton.simulate('click');
+      server.respond();
+
+      wrapper.update();
+      image = wrapper.find('#uitest-certificate img');
+      const expectedData = {name: 'Student', course: 'dance'};
+      const expectedFilename = btoa(JSON.stringify(expectedData));
+      const expectedSrc = `/certificate_images/${expectedFilename}.jpg`;
+      expect(image.prop('src')).to.equal(expectedSrc);
     });
   });
 });
