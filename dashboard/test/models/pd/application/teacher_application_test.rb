f116e8fa5e99607c2f97f7d507d836db9f390948
Meg Crenshaw <meg@code.org>
2022-01-12 11:44:58 -0700

    Ensure some actions do not happen on incomplete app

--- dashboard/test/models/pd/application/teacher_application_test.rb
+++ dashboard/test/models/pd/application/teacher_application_test.rb
@@ -390,16 +390,6 @@ module Pd::Application
       )
     end
 
-    # [MEG] TODO: Test this functionality in the controller
-    test 'incomplete application is valid but does not queue an email nor score it' do
-      application = create :pd_teacher_application, :incomplete
-      assert application.valid?
-
-      application.expects(:queue_email).never
-      application.expects(:auto_score!).never
-      application.on_successful_create
-    end
-
     test 'setting an auto-email status queues up an email' do
       application = create :pd_teacher_application
       assert_empty application.emails
