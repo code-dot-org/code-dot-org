065cbd4334590de235c9e1531e5d9f9d8a52ca0e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-01-12 16:00:04 -0800

    cleanup and comments

--- dashboard/app/helpers/multiple_databases_transition_helper.rb
+++ dashboard/app/helpers/multiple_databases_transition_helper.rb
@@ -1,12 +1,13 @@
 require "active_support/concern"
 
-# to do:
-#   Gatekeeper flag support (from ReadReplicaHelper)
-#   (in progress) use_database_pool controller route-by-route support
-#   (in progress) manual use_master_connection block support from ChannelToken
+# A helper module to factilitate the transition from SeamlessDatabasePool to
+# ActiveRecord's new Multiple Databases implementation.
 module MultipleDatabasesTransitionHelper
+  # Provide a new `use_reader_connection_for_route` method on controllers to
+  # replace our existing use of `use_database_pool route_name: :persistent`
+  # which can be used to declare that a particular route should explicitly
+  # always use the read replica.
   module ControllerFilter
-    #include SeamlessDatabasePool::ControllerFilter
     extend ActiveSupport::Concern
     class_methods do
       def use_reader_connection_for_route(route)
@@ -25,24 +26,39 @@ module MultipleDatabasesTransitionHelper
     end
   end
 
-  def self.use_writer_connection(*args, &blk)
+  # Provide a new `use_writer_connection` method to replace our existing use of
+  # `use_master_connection` with the entirely-equivalent ActiveRecord
+  # implementation.
+  def self.use_writer_connection
     if MultipleDatabasesTransitionHelper.transitioned?
       ActiveRecord::Base.connected_to(role: :writing) do
         yield
       end
     else
-      SeamlessDatabasePool.use_master_connection(*args, &blk)
+      SeamlessDatabasePool.use_master_connection do
+        yield
+      end
     end
   end
 
-  def self.use_persistent_read_connection(*args, &blk)
+  # Provide a new `use_persistent_read` method to replace our existing use of
+  # the same. ActiveRecord itself has some new logic to provide similar
+  # functionality, as does ProxySQL, so it could well be that we don't actually
+  # need this at all post-transition. More investigation needed for this TODO
+  # to be resolved.
+  def self.use_persistent_read_connection
     if MultipleDatabasesTransitionHelper.transitioned?
       # TODO
     else
-      SeamlessDatabasePool.use_persistent_read_connection(*args, &blk)
+      SeamlessDatabasePool.use_persistent_read_connection do
+        yield
+      end
     end
   end
 
+  # Whether we are still on the old version (false, pre-transition) or the new
+  # version (true, post-transition). Hardcoded to the known versions for
+  # resiliency and narrow focus.
   def self.transitioned?
     case Rails.version
     when "5.2.4.4"
