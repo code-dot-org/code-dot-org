ec7918a45be0ef85606b008389134897d518a4f2
Hannah Bergam <hannahbergam@gmail.com>
2022-01-14 11:46:48 -0800

    made readonly match readonlyworkspace logic

--- apps/src/StudioApp.js
+++ apps/src/StudioApp.js
@@ -689,7 +689,7 @@ StudioApp.prototype.getVersionHistoryHandler = function(config) {
         isProjectTemplateLevel: !!config.level.projectTemplateLevelName,
         useFilesApi: !!config.useFilesApi,
         selectedVersion: queryParams('version'),
-        isReadOnly: !project.isOwner()
+        isReadOnly: config.readonlyWorkspace
       }),
       contentDiv
     );
