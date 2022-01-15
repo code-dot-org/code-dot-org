040ab78f6fd6f9fdd769776506298a9a248b5956
Meg Crenshaw <meg@code.org>
2022-01-15 12:22:56 -0700

    Update attributes if submittiing an empty incomplete application

--- dashboard/app/controllers/api/v1/pd/application/teacher_applications_controller.rb
+++ dashboard/app/controllers/api/v1/pd/application/teacher_applications_controller.rb
@@ -17,8 +17,14 @@ module Api::V1::Pd::Application
       form_data_json = form_data_hash.to_unsafe_h.to_json.strip_utf8mb4 if form_data_hash
 
       @application.form_data_hash = JSON.parse(form_data_json)
+      @application.set_status
+      @application.set_course_from_program
+      @application.update_user_school_info!
+
+      @application.on_completed_application unless @application.status == 'incomplete'
 
       if @application.save
+        @application.update_status_timestamp_change_log(current_user)
         render json: @application, status: :ok
       else
         return render json: {errors: @application.errors.full_messages}, status: :bad_request
@@ -40,6 +46,7 @@ module Api::V1::Pd::Application
     protected
 
     def on_successful_create
+      @application.set_status
       @application.on_successful_create
       @application.update_status_timestamp_change_log(current_user)
     end
