44203651a72ab011e10d1df6965d308d22365b73
Maureen <maureen.sturgeon@gmail.com>
2022-01-19 13:24:30 -0800

    convert EndOfLessonDialog to functional, make child of UnitOverview component, rename url parm

--- apps/src/templates/EndOfLessonDialog.jsx
+++ apps/src/templates/EndOfLessonDialog.jsx
@@ -1,47 +1,35 @@
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Button from './Button';
 import i18n from '@cdo/locale';
 import BaseDialog from './BaseDialog';
 import DialogFooter from './teacherDashboard/DialogFooter';
 
-export default class EndOfLessonDialog extends Component {
-  static propTypes = {
-    isDialogOpen: PropTypes.bool.isRequired,
-    lessonNumber: PropTypes.string.isRequired
-  };
+function EndOfLessonDialog(props) {
+  const [isDialogOpen, setIsDialogOpen] = useState(props.isDialogOpen);
 
-  state = {
-    isDialogOpen: this.props.isDialogOpen
-  };
-
-  closeDialog = () => {
-    this.setState({isDialogOpen: false});
-  };
-
-  render() {
-    const {lessonNumber} = this.props;
-
-    return (
-      <BaseDialog
-        useUpdatedStyles
-        isOpen={this.state.isDialogOpen}
-        style={styles.dialog}
-      >
-        <h2>{i18n.endOfLessonDialogHeading({lessonNumber})}</h2>
-        <div>{i18n.endOfLessonDialogDetails()}</div>
-        <DialogFooter>
-          <Button
-            __useDeprecatedTag
-            text={i18n.ok()}
-            color={Button.ButtonColor.gray}
-            onClick={this.closeDialog}
-          />
-        </DialogFooter>
-      </BaseDialog>
-    );
-  }
+  return (
+    <BaseDialog useUpdatedStyles isOpen={isDialogOpen} style={styles.dialog}>
+      <h2>
+        {i18n.endOfLessonDialogHeading({lessonNumber: props.lessonNumber})}
+      </h2>
+      <div>{i18n.endOfLessonDialogDetails()}</div>
+      <DialogFooter>
+        <Button
+          __useDeprecatedTag
+          text={i18n.ok()}
+          color={Button.ButtonColor.gray}
+          onClick={() => setIsDialogOpen(false)}
+        />
+      </DialogFooter>
+    </BaseDialog>
+  );
 }
+
+EndOfLessonDialog.propTypes = {
+  isDialogOpen: PropTypes.bool.isRequired,
+  lessonNumber: PropTypes.string.isRequired
+};
 
 const styles = {
   dialog: {
@@ -53,3 +41,5 @@ const styles = {
     marginTop: 20
   }
 };
+
+export default EndOfLessonDialog;
