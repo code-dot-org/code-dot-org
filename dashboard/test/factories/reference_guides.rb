9cfe5750bb6044b5c533d0baf0246ae5dd2d20d2
Tim Plummer <tim@code.org>
2022-01-19 13:54:08 -0800

    Add some unit tests

--- dashboard/test/factories/reference_guides.rb
+++ dashboard/test/factories/reference_guides.rb
@@ -1,4 +1,22 @@
+# == Schema Information
+#
+# Table name: reference_guides
+#
+#  id                :bigint           not null, primary key
+#  name              :string(255)
+#  slug              :string(255)
+#  course_version_id :integer
+#  content           :text(65535)
+#  order             :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
 FactoryGirl.define do
   factory :reference_guide do
+    name "reference guide"
+    slug "category/reference-guide"
+    course_version_id 1
+    content "Some text is here\r\nand here also"
+    order 1
   end
 end
--- dashboard/test/models/reference_guide_test.rb
+++ dashboard/test/models/reference_guide_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ReferenceGuideTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create reference guide" do
+    reference_guide = create :reference_guide
+    assert reference_guide.id
+  end
+
+  test "reference guides are unique by slug in course version" do
+    create :reference_guide, slug: 'hello/world', course_version_id: 1
+    create :reference_guide, slug: 'hello/world', course_version_id: 2
+    assert_raises ActiveRecord::RecordInvalid do
+      create :reference_guide, slug: 'hello/world', course_version_id: 1
+    end
+  end
+
+  test "serialization creates files correctly" do
+    reference_guide = create :reference_guide
+    reference_guide.write_serialization
+  end
+
+  test "reference guides seed from files correctly" do
+    guide = create :reference_guide, id: '1', name: 'World', slug: 'hello/world', content: 'hello world', course_version_id: 3, order: 2
+    serialization = guide.serialize
+    previous_guide = guide.freeze
+    guide.destroy!
+
+    File.stubs(:read).returns(serialization.to_json)
+
+    new_guide_id = ReferenceGuide.seed_record("config/reference_guides/hello/world.json")
+    new_guide = ReferenceGuide.find_by(id: new_guide_id)
+    assert_equal previous_guide.attributes.except('id', 'created_at', 'updated_at'),
+      new_guide.attributes.except('id', 'created_at', 'updated_at')
+  end
 end
