741c0f860eb01a812e6bb2926984c6d903b537b6
Maureen <maureen.sturgeon@gmail.com>
2022-01-19 16:08:36 -0800

    keep query params through redirect

--- dashboard/app/controllers/scripts_controller.rb
+++ dashboard/app/controllers/scripts_controller.rb
@@ -26,7 +26,9 @@ class ScriptsController < ApplicationController
     end
 
     if !params[:section_id] && current_user&.last_section_id
-      redirect_to "#{request.path}?section_id=#{current_user.last_section_id}"
+      query_string = request.query_string.empty? ? '' : "#{request.query_string}&"
+      section_id_query_string = "section_id=#{current_user.last_section_id}"
+      redirect_to "#{request.path}?" + query_string + section_id_query_string
       return
     end
 
