4e50dc49e002da3e42b8592e9bfef88c5a8705e7
Mike Harvey <mike.harvey@code.org>
2022-01-26 16:05:15 -0500

    validation proof of concept

--- apps/src/p5lab/spritelab/commands/validationCommands.js
+++ apps/src/p5lab/spritelab/commands/validationCommands.js
@@ -1,3 +1,5 @@
+import * as drawUtils from '@cdo/apps/p5lab/drawUtils';
+
 export const commands = {
   getAnimationsInUse() {
     return this.getAnimationsInUse();
@@ -62,5 +64,128 @@ export const commands = {
       this.getLastSpeechBubbleForSpriteId(spriteId)?.renderFrame ===
       this.currentFrame()
     );
+  },
+
+  getCriteria() {
+    return this.criteria;
+  },
+
+  setEarlyTime(frames) {
+    this.validationTimes.early = frames;
+  },
+
+  setWaitTime(frames) {
+    this.validationTimes.wait = frames;
+  },
+
+  setDelayTime(frames) {
+    this.validationTimes.delay = frames;
+  },
+
+  setCriteria(predicate, feedback) {
+    if (typeof predicate === 'function' && typeof feedback === 'string') {
+      this.criteria.push(new criteria(predicate, feedback));
+    }
+  },
+
+  validate() {
+    let state = commands.getPassState(this.criteria);
+
+    let barWidth =
+      this.currentFrame() * commands.calculateBarScale(this.validationTimes);
+    drawUtils.validationBar(this.p5, barWidth, state, {});
+
+    //check criteria and update complete status
+    if (this.currentFrame() <= this.validationTimes.wait) {
+      commands.checkAllCriteria(this.criteria);
+    } else {
+      var results = {};
+      if (state === 'fail') {
+        results = {
+          state: 'failed',
+          feedback: commands.reportFailure(this.criteria)
+        };
+      } else if (
+        this.currentFrame() >=
+        this.validationTimes.wait + this.validationTimes.delay
+      ) {
+        results = {
+          state: 'succeeded',
+          feedback: commands.reportSuccess(this.criteria)
+        };
+      }
+      return results;
+    }
+  },
+
+  getPassState(criteria) {
+    var state = 'pass';
+    for (const criterion in criteria) {
+      if (!criteria[criterion].complete) {
+        state = 'fail';
+      }
+    }
+    return state;
+  },
+
+  calculateBarScale(validationTimes) {
+    return 400 / (validationTimes.wait + validationTimes.delay);
+  },
+
+  checkAllCriteria(criteria) {
+    for (const criterion in criteria) {
+      if (!criteria[criterion].complete) {
+        if (criteria[criterion].predicate()) {
+          criteria[criterion].complete = true;
+        }
+      }
+    }
+  },
+
+  reportFailure(criteria) {
+    let firstFailed = -1;
+    for (const criterion in criteria) {
+      if (!criteria[criterion].complete && firstFailed === -1) {
+        firstFailed = criterion;
+      }
+    }
+    if (firstFailed > -1) {
+      return criteria[firstFailed].feedback;
+    }
+  },
+
+  reportSuccess(criteria) {
+    let firstFailed = -1;
+    for (const criterion in criteria) {
+      if (!criteria[criterion].complete && firstFailed === -1) {
+        firstFailed = criterion;
+      }
+    }
+    if (firstFailed === -1) {
+      return 'genericSuccess';
+    }
+  },
+
+  passLevel() {
+    return true;
+  },
+
+  minimumSprites(min) {
+    return this.getSpriteIdsInUse().length >= min;
+  },
+
+  allSpriteHaveDifferentCostumes() {
+    return this.getAnimationsInUse().length === this.getSpriteIdsInUse().length;
+  },
+
+  allSpriteHaveSameCostume() {
+    return this.getAnimationsInUse().length === 1;
   }
 };
+class criteria {
+  constructor(predicate, feedback) {
+    this.predicate = predicate;
+    this.feedback = feedback;
+    this.complete = false;
+  }
+}
