04c662162e04835cf630412b44b483f2e74422ff
David Bailey <davidsbailey@users.noreply.github.com>
2022-02-01 13:24:42 -0800

    rename to getCertificatePath

--- apps/src/templates/Certificate.jsx
+++ apps/src/templates/Certificate.jsx
@@ -51,7 +51,7 @@ function Certificate(props) {
     });
   };
 
-  const getCertificate = () => {
+  const getCertificatePath = () => {
     if (!props.showStudioCertificate) {
       return `${
         dashboard.CODE_ORG_URL
@@ -76,7 +76,7 @@ function Certificate(props) {
   } = props;
 
   const certificate = certificateId || 'blank';
-  const personalizedCertificate = getCertificate();
+  const personalizedCertificate = getCertificatePath();
   const blankCertificate =
     blankCertificates[tutorial] || blankCertificates.hourOfCode;
   const imgSrc = personalized ? personalizedCertificate : blankCertificate;
