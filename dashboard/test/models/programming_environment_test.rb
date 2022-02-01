36ac06f8e471105c8957d34fc45bc1d10a520dda
Bethany Connor <bethany@code.org>
2022-02-01 07:42:57 -0800

    Add tests, as well as an alias and dependent: :destroy annotations

--- dashboard/test/models/programming_environment_test.rb
+++ dashboard/test/models/programming_environment_test.rb
@@ -18,4 +18,20 @@ class ProgrammingEnvironmentTest < ActiveSupport::TestCase
     new_env = ProgrammingEnvironment.find_by_name(new_env_name)
     assert_equal previous_env.attributes.except('id', 'created_at', 'updated_at'), new_env.attributes.except('id', 'created_at', 'updated_at')
   end
+
+  test "can serialize and seed programming environment with categories" do
+    env = create :programming_environment, name: 'ide', editor_type: 'droplet', title: 'IDE', description: 'A description of the IDE.', image_url: 'images.code.org/ide'
+    create :programming_environment_category, programming_environment: env
+    create :programming_environment_category, programming_environment: env
+    serialization = env.serialize
+    previous_env = env.freeze
+    env.destroy!
+
+    File.stubs(:read).returns(serialization.to_json)
+
+    new_env_name = ProgrammingEnvironment.seed_record("config/programming_environments/ide.json")
+    new_env = ProgrammingEnvironment.find_by_name(new_env_name)
+    assert_equal previous_env.attributes.except('id', 'created_at', 'updated_at'), new_env.attributes.except('id', 'created_at', 'updated_at')
+    assert_equal 2, new_env.categories.count
+  end
 end
