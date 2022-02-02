df8c9dbb159f7f78de2ad95a0f45cd89fe29c11e
David Bailey <davidsbailey@users.noreply.github.com>
2022-02-01 16:02:09 -0800

    fine tune print layout

--- dashboard/app/controllers/print_certificates_controller.rb
+++ dashboard/app/controllers/print_certificates_controller.rb
@@ -4,6 +4,7 @@ class PrintCertificatesController < ApplicationController
   # GET /print_certificates/:encoded_params
   def show
     prevent_caching
+    view_options(no_header: true, no_footer: true, white_background: true, full_width: true)
 
     begin
       data = JSON.parse(Base64.urlsafe_decode64(params[:encoded_params]))
