4fc9c58467dc84e27c121bae0413a883ebcbdc48
Maureen <maureen.sturgeon@gmail.com>
2022-02-03 14:55:56 -0800

    more removal of sectionDataRedux in teacher dashboard

--- apps/src/redux/sectionDataRedux.js
+++ apps/src/redux/sectionDataRedux.js
@@ -1,30 +1,8 @@
-import PropTypes from 'prop-types';
-
 /**
  * Reducer for section data in teacher dashboard.
  * Tab specific reducers can import actions from this file
  * if they need to respond to a section changing.
  */
-
-/**
- * Shape for the section
- * The section we get directly from angular right now. This gives us a
- * different shape than some other places we use sections. For now, I'm just
- * going to document the parts of section that we use here
- */
-export const sectionDataPropType = PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  script: PropTypes.object,
-  students: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired
-    })
-  ).isRequired,
-  isAssignedCSA: PropTypes.bool,
-  lessonExtras: PropTypes.bool,
-  ttsAutoplayEnabled: PropTypes.bool
-});
 
 /**
  * Action type constants
@@ -75,14 +53,3 @@ export default function sectionData(state = initialState, action) {
 
   return state;
 }
-
-/**
- * Selector functions
- */
-export const getTotalStudentCount = state => {
-  return state.sectionData.section.students.length;
-};
-
-export const getStudentList = state => {
-  return state.sectionData.section.students;
-};
