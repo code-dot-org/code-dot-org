e4ae31bd12defc31663bb4fe6990a152b2ad880e
David Bailey <davidsbailey@users.noreply.github.com>
2022-02-02 16:33:35 -0800

    extract encode_params helper

--- dashboard/app/helpers/certificates_helper.rb
+++ dashboard/app/helpers/certificates_helper.rb
@@ -1,12 +1,15 @@
 module CertificatesHelper
-  def certificate_image_url(name, course)
-    return '/images/hour_of_code_certificate.jpg' unless course
-
+  def encode_params(name, course)
     opts = {
       name: name,
       course: course
     }.compact
-    encoded = Base64.urlsafe_encode64(opts.to_json)
+    Base64.urlsafe_encode64(opts.to_json)
+  end
+
+  def certificate_image_url(name, course)
+    return '/images/hour_of_code_certificate.jpg' unless course
+    encoded = encode_params(name, course)
     "/certificate_images/#{encoded}.jpg"
   end
 end
