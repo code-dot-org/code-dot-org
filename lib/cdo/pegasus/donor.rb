16c4ba678fb9700713d40d29c45fc679456a3e5d
David Bailey <davidsbailey@users.noreply.github.com>
2022-02-05 05:32:25 -0700

    rename to get_random_donor_by_twitter_weight

--- lib/cdo/pegasus/donor.rb
+++ lib/cdo/pegasus/donor.rb
@@ -2,7 +2,7 @@ require 'honeybadger/ruby'
 
 # Returns a random donor's twitter handle.
 def get_random_donor_twitter
-  donor, weight = get_random_donor
+  donor, weight = get_random_donor_by_twitter_weight
   if donor && donor[:twitter_s]
     donor[:twitter_s]
   else
@@ -13,7 +13,7 @@ end
 
 # @return [Hash] a donor name and their twitter handle
 def get_random_donor_name_and_twitter
-  donor, weight = get_random_donor
+  donor, weight = get_random_donor_by_twitter_weight
   if donor && donor[:twitter_s]
     {
       name: donor[:name_s],
@@ -29,7 +29,7 @@ def get_random_donor_name_and_twitter
 end
 
 # @return [Array] a donor record and the random weight used to find that record
-def get_random_donor
+def get_random_donor_by_twitter_weight
   weight = SecureRandom.random_number
   donor = DB[:cdo_donors].all.find {|d| d[:twitter_weight_f] - weight >= 0}
   [donor, weight]
