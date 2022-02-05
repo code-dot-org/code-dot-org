497fe4cf3d1e8b12102a73676e38bec63e992bd1
David Bailey <davidsbailey@users.noreply.github.com>
2022-02-05 06:30:14 -0700

    fix cert test requires

--- lib/cdo/graphics/certificate_image.rb
+++ lib/cdo/graphics/certificate_image.rb
@@ -4,6 +4,10 @@
 require 'honeybadger/ruby'
 require 'rmagick'
 require_relative '../script_constants'
+require 'cdo/pegasus/donor'
+
+# needed for force_8859_to_utf8
+require 'cdo/pegasus/string'
 
 # The area in pixels under the "Certificate of Completion" on the certificate reserved for the name.
 CERT_NAME_AREA_WIDTH = 900
