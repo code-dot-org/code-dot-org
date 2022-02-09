970ceb0cbea6afa94758b18f595cfd375b726729
Bethany Connor <bethany@code.org>
2022-02-09 09:39:12 -0800

    Fix the spritelab image and add some styling

--- apps/src/templates/codeDocs/ProgrammingEnvironmentIndex.jsx
+++ apps/src/templates/codeDocs/ProgrammingEnvironmentIndex.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import EnhancedSafeMarkdown from '@cdo/apps/templates/EnhancedSafeMarkdown';
+import Button from '@cdo/apps/templates/Button';
 
 function ProgrammingEnvironmentBox({programmingEnvironment}) {
   return (
     <div style={styles.box}>
-      {programmingEnvironment.imageUrl && (
-        <img src={programmingEnvironment.imageUrl} />
-      )}
-      {programmingEnvironment.title}
-      <EnhancedSafeMarkdown markdown={programmingEnvironment.description} />
+      <div>
+        {programmingEnvironment.imageUrl && (
+          <img src={programmingEnvironment.imageUrl} />
+        )}
+        <h2>{programmingEnvironment.title}</h2>
+        <EnhancedSafeMarkdown markdown={programmingEnvironment.description} />
+      </div>
+      <Button
+        __useDeprecatedTag
+        color={Button.Orange}
+        href={`/docs/${programmingEnvironment.name}`}
+        text="View Code Docs"
+      />
     </div>
   );
 }
@@ -40,9 +49,13 @@ const styles = {
     borderStyle: 'solid',
     borderColor: 'black',
     borderWidth: 1,
-    width: '45%',
+    width: '30%',
     padding: 5,
-    margin: 5
+    margin: 5,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between'
   },
   flex: {
     display: 'flex',
