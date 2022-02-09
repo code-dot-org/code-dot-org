078d33857c2e2b1adbcbb2339ae39bf2fdd1e750
Bethany Connor <bethany@code.org>
2022-02-08 16:57:30 -0800

    Add image urls for environments and use those

--- apps/src/templates/codeDocs/ProgrammingEnvironmentIndex.jsx
+++ apps/src/templates/codeDocs/ProgrammingEnvironmentIndex.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import EnhancedSafeMarkdown from '@cdo/apps/templates/EnhancedSafeMarkdown';
 
+function ProgrammingEnvironmentBox({programmingEnvironment}) {
+  return (
+    <div style={styles.box}>
+      {programmingEnvironment.imageUrl && (
+        <img src={programmingEnvironment.imageUrl} />
+      )}
+      {programmingEnvironment.title}
+      <EnhancedSafeMarkdown markdown={programmingEnvironment.description} />
+    </div>
+  );
+}
+
 export default function ProgrammingEnvironmentIndex({programmingEnvironments}) {
   return (
-    <div>
+    <div style={styles.flex}>
       {programmingEnvironments.map(env => (
-        <div key={env.name}>{env.title}</div>
+        <ProgrammingEnvironmentBox
+          key={env.name}
+          programmingEnvironment={env}
+        />
       ))}
     </div>
   );
@@ -14,4 +29,23 @@ export default function ProgrammingEnvironmentIndex({programmingEnvironments}) {
 
 ProgrammingEnvironmentIndex.propTypes = {
   programmingEnvironments: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+ProgrammingEnvironmentBox.propTypes = {
+  programmingEnvironment: PropTypes.object.isRequired
+};
+
+const styles = {
+  box: {
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 1,
+    width: '45%',
+    padding: 5,
+    margin: 5
+  },
+  flex: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  }
 };
