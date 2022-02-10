7b7277b5fd354f221448ef6357c57af566dac2a0
Madelyn Kasula <maddie.kasula@code.org>
2022-02-09 22:01:02 -0800

    Make SmallChevronLink functional; update API to match TextLink

--- apps/src/templates/SmallChevronLink.jsx
+++ apps/src/templates/SmallChevronLink.jsx
@@ -1,40 +1,42 @@
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import {TextLink} from '@dsco_/link';
 import FontAwesome from './FontAwesome';
-import {makeEnum} from '@cdo/apps/utils';
 
-const ChevronSide = makeEnum('left', 'right');
-
-export class SmallChevronLink extends Component {
-  static propTypes = {
-    linkText: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
-    isRtl: PropTypes.bool.isRequired,
-    chevronSide: PropTypes.oneOf(Object.values(ChevronSide))
-  };
-
-  render() {
-    const {link, linkText, chevronSide, isRtl} = this.props;
-
-    return (
-      <div style={{margin: '10px 0'}}>
-        <TextLink
-          href={link}
-          text={linkText}
-          icon={
-            <FontAwesome
-              icon={chevronSide === 'left' ? 'chevron-left' : 'chevron-right'}
-              className={isRtl && 'fa-flip-horizontal'}
-            />
-          }
-          iconBefore={chevronSide === 'left'}
-        />
-      </div>
-    );
-  }
+export function SmallChevronLink({
+  href,
+  text,
+  iconBefore = false,
+  isRtl = false,
+  style = {margin: '10px 0'}
+}) {
+  return (
+    <div style={style}>
+      <TextLink
+        href={href}
+        text={text}
+        icon={
+          <FontAwesome
+            icon={iconBefore ? 'chevron-left' : 'chevron-right'}
+            className={isRtl && 'fa-flip-horizontal'}
+          />
+        }
+        iconBefore={iconBefore}
+      />
+    </div>
+  );
 }
+
+SmallChevronLink.propTypes = {
+  text: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  iconBefore: PropTypes.bool,
+  style: PropTypes.object,
+
+  // Provided by redux
+  isRtl: PropTypes.bool
+};
 
 export default connect(state => ({
   isRtl: state.isRtl
