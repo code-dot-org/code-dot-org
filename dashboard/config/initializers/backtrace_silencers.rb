77d218362725f0076e71cb3dc35a82cc038f0679
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-02-15 17:04:52 -0800

    Remove silencing logic for deprecation warnings we no longer need to silence

--- dashboard/config/initializers/backtrace_silencers.rb
+++ dashboard/config/initializers/backtrace_silencers.rb
@@ -8,12 +8,7 @@
 
 # In addition to backtrace silencing, we also want to silence annoying deprecations:
 silenced = [
-  # Added in Rails 5.2
-  /Single arity template handlers are deprecated/,
-  /Dangerous query method \(method whose arguments are used as raw SQL\) called with non-attribute argument\(s\)/,
-  /SourceAnnotationExtractor is deprecated! Use Rails::SourceAnnotationExtractor instead/,
-
-  # Added in Rails 6.0
+  # Added in Rails 6.0; make sure to address before upgrading to Rails 6.1
   /Uniqueness validator will no longer enforce case sensitive comparison in Rails 6.1/,
   /The asset ".*" is not present in the asset pipeline.Falling back to an asset that may be in the public folder./,
   /NOT conditions will no longer behave as NOR in Rails 6.1. To continue using NOR conditions, NOT each condition individually/,
