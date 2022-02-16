cdcaf1f9c98123ddd37847291f82508e125dc035
molly-moen <molly@code.org>
2022-02-16 11:40:16 -0800

    finish file helper

--- dashboard/app/helpers/javalab_files_helper.rb
+++ dashboard/app/helpers/javalab_files_helper.rb
@@ -1,12 +1,20 @@
 
 module JavalabFilesHelper
+  # Get all files related to the project at the given channel id as a hash. The hash is in the format
+  # below. All values are StringIO.
+  # {
+  #   "main.json": <main source file for a project>
+  #   "assets": {"asset_name_1": <asset_value>, ...}
+  #   "validation": <all validation code for a project, in json format>
+  #   "maze": <serialized maze if it exists>
+  # }
+  # If the channel doesn't have validation and/or a maze, those fields will not be present.
   def self.get_project_files(channel_id)
     storage_id, storage_app_id = storage_decrypt_channel_id(channel_id)
     all_files = {}
     # get sources file
     source_data = SourceBucket.new.get(channel_id, "main.json")
-    # can call .string on this value to get raw string. Keeping as StringIO for now
-    # since we don't know how we'll use this data
+    # Note: we can call .string on this value (and all other values for files) to get the raw string.
     all_files["main.json"] = source_data[:body]
     # get starter assets
     assets = {}
@@ -28,7 +36,14 @@ module JavalabFilesHelper
     end
     all_files["assets"] = assets
     # get validation code
+    if level.respond_to?(:validation) && level.validation
+      all_files["validation"] = StringIO.new(level.validation.to_json)
+    end
     # get maze file
+    serialized_maze = level.try(:get_serialized_maze)
+    if serialized_maze
+      all_files["maze"] = StringIO.new(serialized_maze.to_s)
+    end
     return all_files
   end
 end
