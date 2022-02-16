a09fd227508755b8a2accae3cb1ddabba4212983
Maureen <maureen.sturgeon@gmail.com>
2022-02-16 12:25:37 -0800

    replace remaining references directly to the user storage id table

--- shared/test/middleware/helpers/test_storage_id.rb
+++ shared/test/middleware/helpers/test_storage_id.rb
@@ -4,10 +4,6 @@ require_relative '../../../middleware/helpers/storage_id'
 class StorageIdTest < Minitest::Test
   include SetupTest
 
-  def setup
-    @user_storage_ids_table = PEGASUS_DB[:user_storage_ids]
-  end
-
   def test_storage_id_for_current_user
     request = mock
     stubs(:request).returns(request)
@@ -16,9 +12,9 @@ class StorageIdTest < Minitest::Test
     request.stubs(:user_id).returns(nil)
     assert_nil storage_id_for_current_user
 
-    # # Gets value from table if it exists
+    # Gets value from table if it exists
     request.stubs(:user_id).returns(2)
-    table_storage_id = @user_storage_ids_table.insert(user_id: 2)
+    table_storage_id = create_storage_id_for_user(2)
     assert_equal table_storage_id, storage_id_for_current_user
 
     # Returns value from cookie if we have one
@@ -31,8 +27,7 @@ class StorageIdTest < Minitest::Test
     stubs(:take_storage_id_ownership_from_cookie).returns(nil)
     storage_id = storage_id_for_current_user
 
-    row = @user_storage_ids_table.where(user_id: 4).first
-    assert_equal row[:id], storage_id
+    assert_equal storage_id_for_user_id(4), storage_id
   end
 
   def test_storage_id_for_current_user_race_condition
@@ -74,23 +69,22 @@ class StorageIdTest < Minitest::Test
     # takes ownership if id is unused
     user_id = 7
     # this row would get created as part of create_storage_id_cookie
-    table_storage_id = @user_storage_ids_table.insert(user_id: nil, id: 123)
+    table_storage_id = create_storage_id_for_user(nil)
     stubs(:storage_id_from_cookie).returns(table_storage_id)
     response.expects(:delete_cookie)
     storage_id = take_storage_id_ownership_from_cookie(user_id)
     assert_equal table_storage_id, storage_id
-    row = @user_storage_ids_table.where(user_id: user_id).first
-    assert_equal row[:id], table_storage_id
+    assert_equal storage_id_for_user_id(user_id), table_storage_id
 
     # returns nil if owned by a different user
     other_user_id = 8
     user_id = 9
-    table_storage_id = @user_storage_ids_table.insert(user_id: other_user_id)
+    table_storage_id = create_storage_id_for_user(other_user_id)
     stubs(:storage_id_from_cookie).returns(table_storage_id)
     response.expects(:delete_cookie)
     storage_id = take_storage_id_ownership_from_cookie(user_id)
     assert_nil storage_id
-    assert_nil @user_storage_ids_table.where(user_id: user_id).first
+    assert_nil storage_id_for_user_id(user_id)
   end
 
   def test_storage_id_from_cookie
@@ -105,11 +99,14 @@ class StorageIdTest < Minitest::Test
     assert_nil storage_id_from_cookie
 
     # returns storage id from cookie if unowned
-    @user_storage_ids_table.insert(user_id: nil, id: cookie_storage_id)
-    assert_equal cookie_storage_id, storage_id_from_cookie
+    storage_id_in_table = create_storage_id_for_user(nil)
+    cookie_value = CGI.escape(storage_encrypt_id(storage_id_in_table))
+    request.stubs(:cookies).returns({storage_id_cookie_name => cookie_value})
+
+    assert_equal storage_id_in_table, storage_id_from_cookie
 
     # returns nil if storage id from cookie is owned by a user
-    @user_storage_ids_table.where(id: cookie_storage_id).update(user_id: 2)
+    update_annoymous_user_storage_id(storage_id_in_table, 2)
     assert_nil storage_id_from_cookie
   end
 
