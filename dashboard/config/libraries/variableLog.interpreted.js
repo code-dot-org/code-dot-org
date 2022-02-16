f0360092175976981412bc14f4ed50656f91635e
Mike Harvey <mike.harvey@code.org>
2022-02-16 09:00:06 -0500

    updated interpreted library

--- dashboard/config/libraries/variableLog.interpreted.js
+++ dashboard/config/libraries/variableLog.interpreted.js
@@ -1,28 +1,19 @@
 var studentVarToken = false;
-var varLog={};
-if(!validationProps){
-  var validationProps ={};
-}
+var varLog=buildVariableLog();
+var previousVarLog;// = JSON.parse(JSON.stringify(varLog));
 
-function updateLog(){
-  var start = Object.keys(window).indexOf("playSoundOptions")+1;
-  var end = Object.keys(window).indexOf("math_random_int");
+function buildVariableLog(){
+  var varLog={};
+  var start =
+    (Object.keys(window).indexOf("studentVarToken")) + 1;
+  var end = Object.keys(window).length;
   var index;
   for(var i=start;i<end;i++){
-    if(window[Object.keys(window)[i]] != undefined){
+    if (typeof window[Object.keys(window)[i]] == "number" || typeof window[Object.keys(window)[i]] == "string"|| typeof window[Object.keys(window)[i]] == "boolean") {
       varLog[Object.keys(window)[i]] = window[Object.keys(window)[i]];
     }
   }
-  varWatchers(varLog);
-  
-  if(validationProps){//is Sprite Lab
-    if(!validationProps.previous){
-      validationProps.previous = {};
-    }
-    detectVarChange();
-    validationProps.previous.varLog=JSON.parse(JSON.stringify(varLog));
-  }
-  
+  return varLog;
 }
 
 function varWatchers(varLog){
@@ -30,21 +21,20 @@ function varWatchers(varLog){
     var index = Object.keys(varLog).indexOf(key);
     var x = 5;
     var y = 32;
-    watcher(key, varLog[key], index, x, y);
+    drawWatcher(key, varLog[key], index, x, y);
   }
 }
 
-function watcher(label, value, index, x, y){
+function drawWatcher(label, value, index, x, y){
   if(!value&&value!=0&&value!=''){
     value = 'undefined';
   }
-  //rect(fontSize/2,fontSize/2+index*fontSize*2);
   var fontSize=15;
   textSize(fontSize);
   textAlign(LEFT,CENTER);
-  labelX = x + 5;
-  valueX = x + 15 + textWidth(label);
-  textY = y + (index+0.5) * fontSize*2;
+  var labelX = x + 5;
+  var valueX = x + 15 + textWidth(label);
+  var textY = y + (index+0.5) * fontSize*2;
   stroke("#c6cacd");
   fill("#e7e8ea");
   rect(x,y + index * fontSize*2,textWidth(label)+textWidth(value)+25,fontSize*2, fontSize/2);
@@ -58,18 +48,25 @@ function watcher(label, value, index, x, y){
   text(value,valueX,textY);
 }
 
-function detectVarChange(){
-  if(validationProps.previous.varLog){
-    if(JSON.stringify(validationProps.previous.varLog) != JSON.stringify(varLog)){
-      /*for (var key in validationProps.previous.varLog){
-        if(validationProps.previous.varLog[key]!=window[key]){
-          //console.log("["+ World.frameCount + "]: " + key + " changed from " + validationProps.previous.varLog[key] + " to " + window[key]);
-        }
-      }*/
+function checkLogLength(){
+  return buildVariableLog().length >0;
+}
+
+function checkLogForChanges(){
+  varLog = buildVariableLog();
+  if(!previousVarLog){
+    previousVarLog= {
+      varLog:varLog,
+      frame: World.frameCount,
+    };
+  }
+  if(previousVarLog){
+    if(JSON.stringify(previousVarLog.varLog) != JSON.stringify(varLog)){
+      if(World.frameCount>previousVarLog.frame){
+        previousVarLog=JSON.parse(JSON.stringify(varLog));
+      }
       return true;
     }
   }
   return false;
 }
-
-other.push(updateLog);
