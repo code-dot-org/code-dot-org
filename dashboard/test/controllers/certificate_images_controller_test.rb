391c7d0919334d6a6b6740770e2f8b16f2c25462
David Bailey <davidsbailey@users.noreply.github.com>
2022-02-16 09:46:51 -0800

    require student name and donor name when generating certificate image

--- dashboard/test/controllers/certificate_images_controller_test.rb
+++ dashboard/test/controllers/certificate_images_controller_test.rb
@@ -2,14 +2,6 @@ require 'test_helper'
 require 'base64'
 
 class CertificateImagesControllerTest < ActionController::TestCase
-  test 'can show certificate image given name and course' do
-    data = {name: 'student', course: 'hourofcode'}
-    filename = Base64.urlsafe_encode64(data.to_json)
-    get :show, format: 'jpg', params: {filename: filename}
-    assert_response :success
-    assert_equal "max-age=0, private, must-revalidate, no-store", @response.headers["Cache-Control"]
-  end
-
   test 'can show certificate image given name and course and sponsor' do
     data = {name: 'student', course: 'hourofcode', sponsor: 'Amazon'}
     filename = Base64.urlsafe_encode64(data.to_json)
@@ -18,29 +10,39 @@ class CertificateImagesControllerTest < ActionController::TestCase
     assert_equal "max-age=0, private, must-revalidate, no-store", @response.headers["Cache-Control"]
   end
 
+  test 'returns bad request without student name' do
+    data = {course: 'hourofcode', sponsor: 'Amazon'}
+    filename = Base64.urlsafe_encode64(data.to_json)
+    get :show, format: 'jpg', params: {filename: filename}
+    assert_response :bad_request
+    assert_includes response.body, 'student name is required'
+  end
+
+  test 'returns bad request without donor name' do
+    data = {name: 'student', course: 'hourofcode'}
+    filename = Base64.urlsafe_encode64(data.to_json)
+    get :show, format: 'jpg', params: {filename: filename}
+    assert_response :bad_request
+    assert_includes response.body, 'donor name is required'
+  end
+
   test 'returns bad request given invalid sponsor name' do
     data = {name: 'student', course: 'hourofcode', sponsor: 'bogus'}
     filename = Base64.urlsafe_encode64(data.to_json)
     get :show, format: 'jpg', params: {filename: filename}
     assert_response :bad_request
+    assert_includes response.body, 'invalid donor name'
   end
 
   test 'can show certificate image given bogus course name' do
-    data = {name: 'student', course: 'bogus'}
-    filename = Base64.urlsafe_encode64(data.to_json)
-    get :show, format: 'jpg', params: {filename: filename}
-    assert_response :success
-  end
-
-  test 'can show certificate image without name or course' do
-    data = {}
+    data = {name: 'student', course: 'bogus', sponsor: 'Amazon'}
     filename = Base64.urlsafe_encode64(data.to_json)
     get :show, format: 'jpg', params: {filename: filename}
     assert_response :success
   end
 
   test 'returns bad request given invalid format' do
-    data = {name: 'student', course: 'hourofcode'}
+    data = {name: 'student', course: 'hourofcode', sponsor: 'Amazon'}
     filename = Base64.urlsafe_encode64(data.to_json)
     get :show, format: 'bogus', params: {filename: filename}
     assert_response :bad_request
