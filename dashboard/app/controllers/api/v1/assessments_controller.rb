2d7331db23bd576a8f14bd441b15d3ed3a8fe58b
James Chen <james@code.org>
2022-02-16 11:19:01 -0800

    Batch database queries in AssessmentsController#section_responses

--- dashboard/app/controllers/api/v1/assessments_controller.rb
+++ dashboard/app/controllers/api/v1/assessments_controller.rb
@@ -79,128 +79,163 @@ class Api::V1::AssessmentsController < Api::V1::JsonApiController
   def section_responses
     responses_by_student = {}
 
+    script_id = @script.id
     assessment_script_levels = @script.get_assessment_script_levels
 
-    @section.students.each do |student|
-      # Initialize student hash
-      student_hash = {
-        student_name: student.name
-      }
-      responses_by_level_group = {}
+    @section.students.in_batches(of: 20).each do |student_batch|
+      student_ids = student_batch.pluck(:id)
 
+      # Get data from the database for each script_level for the entire batch
+      # of students. batch_data will eventually have this structure:
+      # {
+      #   script_level_id: {
+      #     "level_group_data": {
+      #       student_id: <UserLevel>
+      #     },
+      #     "sublevels_data": {
+      #       student_id: [<UserLevel>]
+      #     }
+      #   }
+      # }
+      batch_data = {}
       assessment_script_levels.each do |script_level|
-        last_attempt = student.last_attempt_for_any(script_level.levels, script_id: @script.id)
-        next unless last_attempt
+        # each level in script_level should be a LevelGroup which represents an
+        # assessment
+        level_group_ids = script_level.levels.pluck(:id)
+        sublevel_ids = script_level.levels.map {|level| level.all_child_levels.pluck(:id)}.flatten
 
-        level_group = last_attempt.level
-        sublevel_ids = level_group.all_child_levels.pluck(:id)
-        student_answers_by_level_id = UserLevel.includes(:level_source).
-          where({user_id: student.id, script_id: @script.id, level_id: sublevel_ids}).
-          map {|user_level| [user_level.level_id, user_level.level_source&.data]}.  # [key, value] in returned hash
-          to_h
-
-        # Summarize some key data.
-        multi_count = 0
-        multi_count_correct = 0
-        match_count = 0
-        match_count_correct = 0
-
-        # And construct a listing of all the individual levels and their results.
-        level_results = []
-
-        level_group.levels.each do |level|
-          if level.is_a? Multi
-            multi_count += 1
-          elsif level.is_a? Match
-            match_count += 1
-          end
-
-          student_answer = student_answers_by_level_id[level.id]
-
-          level_result = {}
-
-          case level
-          when TextMatch, FreeResponse
-            level_result[:type] = "FreeResponse"
-          when Multi
-            level_result[:type] = "Multi"
-          when Match
-            level_result[:type] = "Match"
-          end
-
-          if student_answer
-            case level
-            when TextMatch, FreeResponse
-              level_result[:student_result] = student_answer
-              level_result[:status] = ""
-            when Multi
-              answer_indexes = Multi.find_by_id(level.id).correct_answer_indexes_array
-              student_result = student_answer.split(",").map(&:to_i).sort
-              level_result[:student_result] = student_result
-
-              if student_result == [-1]
-                level_result[:student_result] = []
-                level_result[:status] = "unsubmitted"
-              # Deep comparison of arrays of indexes
-              elsif student_result.present? && student_result - answer_indexes == [] && answer_indexes.length == student_result.length
-                multi_count_correct += 1
-                level_result[:status] = "correct"
-              else
-                level_result[:status] = "incorrect"
-              end
-            when Match
-              student_result = student_answer.split(",", -1)
-              # If a student did not answer some of the matching question we will record that as nil
-              student_result = student_result.map do |result|
-                result.empty? ? nil : result.to_i
-              end
-              level_result[:student_result] = student_result
-              option_status = []
-              number_correct = 0
-              student_result.each_with_index do |answer, index|
-                if answer.nil?
-                  option_status[index] = "unsubmitted"
-                else
-                  option_status[index] = "submitted"
-                  if answer == index
-                    number_correct += 1
-                  end
-                end
-              end
-              if number_correct == student_result.length
-                match_count_correct += 1
-              end
-              level_result[:status] = option_status
-            end
-          else
-            level_result[:status] = "unsubmitted"
-          end
-
-          level_results << level_result
-        end
-
-        submitted = last_attempt[:submitted]
-        timestamp = last_attempt[:updated_at].to_datetime
-
-        responses_by_level_group[level_group.id] = {
-          lesson: script_level.lesson.localized_title,
-          puzzle: script_level.position,
-          question: level_group.properties["title"],
-          url: build_script_level_url(script_level, section_id: @section.id, user_id: student.id),
-          multi_correct: multi_count_correct,
-          multi_count: multi_count,
-          match_correct: match_count_correct,
-          match_count: match_count,
-          submitted: submitted,
-          timestamp: timestamp,
-          level_results: level_results
+        batch_data[script_level.id] = {
+          level_group_data: User.batched_last_attempt_for_any(student_ids, script_id, level_group_ids),
+          sublevels_data: User.progress_for_users(student_ids, script_id, sublevel_ids)
         }
       end
 
-      # If a student has made responses, add them to the hash
-      if responses_by_level_group.keys.count > 0
-        student_hash[:responses_by_assessment] = responses_by_level_group
-        responses_by_student[student.id] = student_hash
+      student_batch.each do |student|
+        # Initialize student hash
+        student_hash = {
+          student_name: student.name
+        }
+        responses_by_level_group = {}
+
+        assessment_script_levels.each do |script_level|
+          # Get the progress data for this assessment / student from batch_data
+          level_group_progress = batch_data[script_level.id][:level_group_data][student.id]
+          sublevels_progress = batch_data[script_level.id][:sublevels_data][student.id]
+
+          next unless level_group_progress
+
+          level_group = level_group_progress.level
+
+          # Summarize some key data.
+          multi_count = 0
+          multi_count_correct = 0
+          match_count = 0
+          match_count_correct = 0
+
+          # And construct a listing of all the individual levels and their results.
+          level_results = []
+
+          level_group.levels.each do |level|
+            if level.is_a? Multi
+              multi_count += 1
+            elsif level.is_a? Match
+              match_count += 1
+            end
+
+            # Find the UserLevel that corresponds to this level.  Note that the
+            # first two checks should always be true by this point and we're
+            # including the checks just for extra safety.
+            student_answer = sublevels_progress && sublevels_progress.find do |user_level|
+              user_level.user_id == student.id &&
+              user_level.script_id == script_id &&
+              user_level.level_id == level.id
+            end.level_source.data
+
+            level_result = {}
+
+            case level
+            when TextMatch, FreeResponse
+              level_result[:type] = "FreeResponse"
+            when Multi
+              level_result[:type] = "Multi"
+            when Match
+              level_result[:type] = "Match"
+            end
+
+            if student_answer
+              case level
+              when TextMatch, FreeResponse
+                level_result[:student_result] = student_answer
+                level_result[:status] = ""
+              when Multi
+                answer_indexes = Multi.find_by_id(level.id).correct_answer_indexes_array
+                student_result = student_answer.split(",").map(&:to_i).sort
+                level_result[:student_result] = student_result
+
+                if student_result == [-1]
+                  level_result[:student_result] = []
+                  level_result[:status] = "unsubmitted"
+                # Deep comparison of arrays of indexes
+                elsif student_result.present? && student_result - answer_indexes == [] && answer_indexes.length == student_result.length
+                  multi_count_correct += 1
+                  level_result[:status] = "correct"
+                else
+                  level_result[:status] = "incorrect"
+                end
+              when Match
+                student_result = student_answer.split(",", -1)
+                # If a student did not answer some of the matching question we will record that as nil
+                student_result = student_result.map do |result|
+                  result.empty? ? nil : result.to_i
+                end
+                level_result[:student_result] = student_result
+                option_status = []
+                number_correct = 0
+                student_result.each_with_index do |answer, index|
+                  if answer.nil?
+                    option_status[index] = "unsubmitted"
+                  else
+                    option_status[index] = "submitted"
+                    if answer == index
+                      number_correct += 1
+                    end
+                  end
+                end
+                if number_correct == student_result.length
+                  match_count_correct += 1
+                end
+                level_result[:status] = option_status
+              end
+            else
+              level_result[:status] = "unsubmitted"
+            end
+
+            level_results << level_result
+          end
+
+          submitted = level_group_progress[:submitted]
+          timestamp = level_group_progress[:updated_at].to_datetime
+
+          responses_by_level_group[level_group.id] = {
+            lesson: script_level.lesson.localized_title,
+            puzzle: script_level.position,
+            question: level_group.properties["title"],
+            url: build_script_level_url(script_level, section_id: @section.id, user_id: student.id),
+            multi_correct: multi_count_correct,
+            multi_count: multi_count,
+            match_correct: match_count_correct,
+            match_count: match_count,
+            submitted: submitted,
+            timestamp: timestamp,
+            level_results: level_results
+          }
+        end
+
+        # If a student has made responses, add them to the hash
+        if responses_by_level_group.keys.count > 0
+          student_hash[:responses_by_assessment] = responses_by_level_group
+          responses_by_student[student.id] = student_hash
+        end
       end
     end
 
