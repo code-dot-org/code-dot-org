0a750a2d51dd87b7b29a4ce4f099161326576c29
Hannah Bergam <hannahbergam@gmail.com>
2022-02-17 14:34:53 -0800

    updated all plane banners to use new superhero html

--- pegasus/src/homepage.rb
+++ pegasus/src/homepage.rb
@@ -501,15 +501,10 @@ class Homepage
     request.locale == "en-US"
   end
 
-  def self.professional_learning_banner_header
-    superhero_header = "Help students become superheroes!"
-    superhero_header
-  end
-
   def self.professional_learning_banner_text
     teacher_apps_closing_soon = DCDO.get("teacher_application_mode", CDO.default_teacher_application_mode) == "closing-soon"
     closing_soon_text = "2021 Professional Learning applications are closing soon! Sign up now."
-    sign_up_text = "Our Professional Learning program helps you teach computer science!"
+    sign_up_text = "Sign up for Professional Learning! Middle and High School applications now open."
     teacher_apps_closing_soon ? closing_soon_text : sign_up_text
   end
 
