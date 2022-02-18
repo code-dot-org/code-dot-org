601961a8c6adc1a02f934bd849a7a0805fb95293
Madelyn Kasula <maddie.kasula@code.org>
2022-02-18 11:41:18 -0800

    Delete applab ec_simple tests covered by other levelSolutions tests

--- apps/test/integration/levelSolutions/applab2/ec_simple.js
+++ apps/test/integration/levelSolutions/applab2/ec_simple.js
@@ -97,33 +97,6 @@ module.exports = {
       expect: '9'
     }),
 
-    // These exercise all of the blocks in Control category
-    // It does not validate that they behave correctly, just that we don't end
-    // up with any errors
-    testApplabConsoleOutput({
-      testName: 'Variables',
-      source: `
-        // prompt and promptNum are covered in dropletUtilsTest
-        var x = 1;
-        var y;
-        y = 2;
-        console.log("message");
-        var str = "Hello World";
-        var a = str.substring(6, 11);
-        var b = str.indexOf("World");
-        var c = str.includes("World");
-        var d = str.length;
-        var e = str.toUpperCase();
-        var f = str.toLowerCase();
-        var list = ["a", "b", "d"];
-        var g = list.length;
-        insertItem(list, 2, "c");
-        appendItem(list, "f");
-        removeItem(list, 0);
-      `,
-      expect: '"message"'
-    }),
-
     // These exercise some simple use cases for the list blocks:
     // insertItem, appendItem, and removeItem
     testApplabConsoleOutput({
@@ -137,85 +110,6 @@ module.exports = {
         console.log(g + ' ' + list.length + ' ' + list.join(','));
       `,
       expect: '"3 4 b,c,d,f"'
-    }),
-
-    // These exercise all of the blocks in canvas category
-    // It does not validate that they behave correctly, just that we don't end
-    // up with any errors
-    testApplabConsoleOutput({
-      testName: 'Canvas',
-      source: `
-        createCanvas("id", 320, 480);
-        setActiveCanvas("id");
-        line(0, 0, 160, 240);
-        circle(160, 240, 100);
-        rect(80, 120, 160, 240);
-        setStrokeWidth(3);
-        setStrokeColor("red");
-        setFillColor(rgb(255,0,0));
-        drawImage("id", 0, 0);
-        var imgData = getImageData(0, 0, 320, 480);
-        putImageData(imgData, 0, 0);
-        clearCanvas();
-        getRed(imgData, 0, 0);
-        getGreen(imgData, 0, 0);
-        getBlue(imgData, 0, 0);
-        getAlpha(imgData, 0, 0);
-        setRed(imgData, 0, 0, 255);
-        setGreen(imgData, 0, 0, 255);
-        setBlue(imgData, 0, 0, 255);
-        setAlpha(imgData, 0, 0, 255);
-        setRGB(imgData, 0, 0, 255, 255, 255);
-      `,
-      expect: ''
-    }),
-
-    // These exercise all of the blocks in Turtle category
-    // It does not validate that they behave correctly, just that we don't end
-    // up with any errors
-    testApplabConsoleOutput({
-      testName: 'Turtle',
-      source: `
-        moveForward(25);
-        moveBackward(25);
-        move(25, 25);
-        moveTo(0, 0);
-        dot(5);
-        turnRight(90);
-        turnLeft(90);
-        turnTo(0);
-        arcRight(90, 25);
-        arcLeft(90, 25);
-        getX();
-        getY();
-        getDirection();
-        penUp();
-        penDown();
-        penWidth(3);
-        penColor("red");
-        penRGB(120, 180, 200);
-        show();
-        hide();
-        speed(50);
-      `,
-      expect: ''
-    }),
-
-    // These exercise all of the blocks in Turtle category
-    // It does not validate that they behave correctly, just that we don't end
-    // up with any errors
-    testApplabConsoleOutput({
-      testName: 'Functions',
-      source: `
-        function myFunction() {
-        }
-        function myFunction2(n) {
-          return n;
-        }
-        myFunction();
-        myFunction2(1);
-      `,
-      expect: ''
     }),
 
     {
