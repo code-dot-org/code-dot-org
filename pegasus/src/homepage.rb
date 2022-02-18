987130d93cfdda1be3d01af8736e3e7170fd7e47
Hannah Bergam <hannahbergam@gmail.com>
2022-02-18 12:51:19 -0800

    pr feedback

--- pegasus/src/homepage.rb
+++ pegasus/src/homepage.rb
@@ -497,10 +497,6 @@ class Homepage
     false
   end
 
-  def self.show_professional_learning_banner(request)
-    request.locale == "en-US"
-  end
-
   def self.professional_learning_banner_text
     teacher_apps_closing_soon = DCDO.get("teacher_application_mode", CDO.default_teacher_application_mode) == "closing-soon"
     closing_soon_text = "2021 Professional Learning applications are closing soon! Sign up now."
