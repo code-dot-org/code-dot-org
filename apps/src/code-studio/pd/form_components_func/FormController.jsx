5da54de4af6dc10d77a66e91977d1d095430d89a
Meg Crenshaw <meg@code.org>
2022-02-18 07:52:39 -0600

    Add autoComputedFields

--- apps/src/code-studio/pd/form_components_func/FormController.jsx
+++ apps/src/code-studio/pd/form_components_func/FormController.jsx
@@ -130,7 +130,7 @@ const FormController = props => {
     } else {
       setHasUserChangedData(false);
     }
-  }, [data]);
+  }, [autoComputedFields, data]);
 
   useEffect(() => {
     const showWarningOnExit =
