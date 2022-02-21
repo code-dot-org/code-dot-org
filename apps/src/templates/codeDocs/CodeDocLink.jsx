ae858cd3872683a2a18b5fb73eff40f890d956e9
Bethany Connor <bethany@code.org>
2022-02-21 10:33:24 -0800

    Use links in the navigation bar instead of blocks, fix width of category banners

--- apps/src/templates/codeDocs/CodeDocLink.jsx
+++ apps/src/templates/codeDocs/CodeDocLink.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import EmbeddedBlock from '@cdo/apps/templates/codeDocs/EmbeddedBlock';
 
-export default function CodeDocLink({programmingExpression}) {
-  if (programmingExpression.blockName) {
+export default function CodeDocLink({programmingExpression, showBlocks}) {
+  if (showBlocks && programmingExpression.blockName) {
     return (
       <EmbeddedBlock
         blockName={programmingExpression.blockName}
