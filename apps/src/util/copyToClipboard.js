c90b2f653d7691df5e4ccfa38d813d091c72a00b
breville <brendan@code.org>
2022-02-22 06:33:13 +0000

    Support legacy copy-to-clipboard when new technique not available

--- apps/src/util/copyToClipboard.js
+++ apps/src/util/copyToClipboard.js
@@ -1,3 +1,31 @@
 export default function copyToClipboard(str, onSuccess, onFailure) {
-  navigator.clipboard.writeText(str).then(onSuccess, onFailure);
+  if (navigator.clipboard && window.isSecureContext) {
+    // Modern technique.
+    navigator.clipboard.writeText(str).then(onSuccess, onFailure);
+  } else {
+    // Legacy technique.
+    window.getSelection().removeAllRanges();
+
+    const tempDiv = document.createElement('pre');
+    tempDiv.innerText = str;
+    document.body.appendChild(tempDiv);
+
+    let errorOccurred = false;
+
+    try {
+      window.getSelection().selectAllChildren(tempDiv);
+      document.execCommand('copy');
+      window.getSelection().removeAllRanges();
+    } catch {
+      if (onFailure) {
+        onFailure();
+      }
+      errorOccurred = true;
+    } finally {
+      document.body.removeChild(tempDiv);
+      if (!errorOccurred && onSuccess) {
+        onSuccess();
+      }
+    }
+  }
 }
