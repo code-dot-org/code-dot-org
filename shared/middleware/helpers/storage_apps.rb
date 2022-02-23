b2b06fcbcedb41e40fb845d710b70d30d27bebc6
Maureen <maureen.sturgeon@gmail.com>
2022-02-23 12:05:58 -0800

    start consolidating references to storage apps table

--- shared/middleware/helpers/storage_apps.rb
+++ shared/middleware/helpers/storage_apps.rb
@@ -12,7 +12,7 @@ class StorageApps
   def initialize(storage_id)
     @storage_id = storage_id
 
-    @table = PEGASUS_DB[:storage_apps]
+    @table = StorageApps.table
   end
 
   def create(value, ip:, type: nil, published_at: nil, remix_parent_id: nil, standalone: true)
@@ -121,6 +121,29 @@ class StorageApps
       studentName: user && UserHelpers.initial(user[:name]),
       studentAgeRange: user && UserHelpers.age_range_from_birthday(user[:birthday]),
     )
+  end
+
+  # Returns an array of all ids for storage apps with storage id = @storage_id
+  def get_all_storage_ids
+    StorageApps.table.
+      where(storage_id: @storage_id).
+      map(:id)
+  end
+
+  # Soft-delete all of the storage apps for user with storage id @storage_id
+  def soft_delete_all
+    StorageApps.table.
+      where(storage_id: @storage_id).
+      exclude(state: 'deleted').
+      update(state: 'deleted', updated_at: Time.now)
+  end
+
+  # Restores all of this user's projects that were soft-deleted after the given time
+  def restore_if_deleted_after(deleted_time)
+    StorageApps.table.
+      where(storage_id: @storage_id, state: 'deleted').
+      where(Sequel.lit('updated_at >= ?', deleted_time.localtime)).
+      update(state: 'active', updated_at: Time.now)
   end
 
   # extracts published project data from a project (aka storage_apps table row).
@@ -334,9 +357,9 @@ class StorageApps
   def self.remix_ancestry(channel_id, depth: 1)
     [].tap do |ancestors|
       _, storage_app_id = storage_decrypt_channel_id(channel_id)
-      next_row = PEGASUS_DB[:storage_apps].where(id: storage_app_id).first
+      next_row = StorageApps.table.where(id: storage_app_id).first
       while next_row&.[](:remix_parent_id)
-        next_row = PEGASUS_DB[:storage_apps].where(id: next_row[:remix_parent_id]).first
+        next_row = StorageApps.table.where(id: next_row[:remix_parent_id]).first
         ancestors.push storage_encrypt_channel_id(next_row[:storage_id], next_row[:id]) if next_row
         break if ancestors.size >= depth
       end
@@ -347,8 +370,12 @@ class StorageApps
 
   def self.get_abuse(channel_id)
     _, storage_app_id = storage_decrypt_channel_id(channel_id)
-    project_info = PEGASUS_DB[:storage_apps].where(id: storage_app_id).first
+    project_info = StorageApps.table.where(id: storage_app_id).first
     project_info[:abuse_score]
+  end
+
+  def self.table
+    PEGASUS_DB[:storage_apps]
   end
 
   private
