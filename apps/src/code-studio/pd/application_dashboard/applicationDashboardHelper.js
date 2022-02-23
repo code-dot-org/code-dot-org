5f9f7be8fa2ad607db23a14136875721887d129f
Meg Crenshaw <meg@code.org>
2022-02-22 22:17:46 -0500

    Add helper for quick view

--- apps/src/code-studio/pd/application_dashboard/applicationDashboardHelper.js
+++ apps/src/code-studio/pd/application_dashboard/applicationDashboardHelper.js
@@ -1,4 +1,4 @@
-import {mapValues, omit} from 'lodash';
+import {mapValues, omit, filter} from 'lodash';
 
 export const dataByStatus = {
   unreviewed: {locked: 0, total: 0},
@@ -36,7 +36,46 @@ export const dataByStatus = {
 //     incomplete: {locked: 0, total: 0},
 //     pending: {locked: 0, total: 0},
 //   }
-// and is used for the "Summary" View on the Application Dashboard.
+// }
+// and is used for the "Summary" component on the Application Dashboard.
 // Anyone who is not a workshop admin should not see incomplete data, so we strip it here.
 export const removeIncompleteAppsFromSummaryView = data =>
   mapValues(data, data_by_status => omit(data_by_status, ['incomplete']));
+
+// Returns a new array of the form
+// [
+//   {
+//      applicant_name: "Severus",
+//      id: 30,
+//      status: "unreviewed"
+//   },
+//   {
+//      applicant_name: "Jenny",
+//      id: 13,
+//      status: "withdrawn"
+//   }
+// ]
+// The `applications` come in from
+// /api/v1/pd/applications/quick_view?role=:role&regional_partner_filter=:regional_partner_filter
+// with the shape
+// applications = [
+//   {
+//      applicant_name: "Severus",
+//      id: 30,
+//      status: "unreviewed"
+//   },
+//   {
+//      applicant_name: "Harry",
+//      id: 16,
+//      status: "incomplete"
+//   },
+//   {
+//      applicant_name: "Jenny",
+//      id: 13,
+//      status: "withdrawn"
+//   }
+// ]
+// and is used for the "QuickView" on the Application Dashboard.
+// Anyone who is not a workshop admin should not see incomplete applications, so we strip them here.
+export const removeIncompleteAppsFromQuickView = applications =>
+  filter(applications, application => application.status !== 'incomplete');
