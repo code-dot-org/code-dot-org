5c4af318f6c7f977c5912abc999c0e8768a63363
Tim Plummer <tim@code.org>
2022-02-24 11:46:52 -0800

    reorganize reference guide controller

--- dashboard/app/controllers/reference_guides_controller.rb
+++ dashboard/app/controllers/reference_guides_controller.rb
@@ -1,15 +1,26 @@
 class ReferenceGuidesController < ApplicationController
   include CurriculumHelper
-  authorize_resource
+  before_action :find_reference_guide, only: [:show]
+  load_and_authorize_resource
 
-  before_action :require_levelbuilder_mode_or_test_env
+  before_action :require_levelbuilder_mode_or_test_env, except: [:show]
 
   def show
-    if params[:course_course_name] && params[:key]
-      course_version_id = find_matching_course_version(params[:course_course_name])&.id
-      @reference_guide = ReferenceGuide.find_by_course_version_id_and_key(course_version_id, params[:key])
-      return render :show if @reference_guide
+    render :not_found if !params[:course_course_name] || !params[:key]
+  end
+
+  private
+
+  def find_reference_guide
+    course_version_id = find_matching_course_version(params[:course_course_name])&.id
+    unless course_version_id
+      flash[:alert] = 'No matching course version found.'
+      render :not_found
     end
-    render :not_found
+    @reference_guide = ReferenceGuide.find_by_course_version_id_and_key(course_version_id, params[:key])
+    unless @reference_guide
+      flash[:alert] = 'No matching reference guide found.'
+      render :not_found
+    end
   end
 end
