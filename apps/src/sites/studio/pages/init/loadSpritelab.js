979d8d3f476e38eff3bb7e263c96b3872496f5f7
Erin Peach <erin@code.org>
2022-02-25 15:29:14 -0800

    Use back-up sprites in SpriteLab

--- apps/src/sites/studio/pages/init/loadSpritelab.js
+++ apps/src/sites/studio/pages/init/loadSpritelab.js
@@ -5,24 +5,37 @@ import blocks from '@cdo/apps/p5lab/spritelab/blocks';
 import skins from '@cdo/apps/p5lab/skins';
 import levels from '@cdo/apps/p5lab/levels';
 import {getDefaultListMetadata} from '@cdo/apps/assetManagement/animationLibraryApi';
+import defaultSprites from '@cdo/apps/p5lab/spritelab/defaultSprites.json';
 
 export default function loadSpritelab(options) {
   options.skinsModule = skins;
   options.blocksModule = blocks;
-  return getDefaultListMetadata().then(defaultSprites => {
-    var spritelab = new SpriteLab(defaultSprites);
+  return getDefaultListMetadata()
+    .then(defaultSpritesList => {
+      var spritelab = new SpriteLab(defaultSpritesList);
+      return initializeOptionsAndSpritelab(spritelab, options);
+    })
+    .catch(() => {
+      // If the S3 request for defaultSpriteList fails, use a backup list of just two sprites
+      var spritelab = new SpriteLab(defaultSprites);
 
-    // Bind helper that provides project metadata for gamelab autosave
-    options.getAnimationList = spritelab.getSerializedAnimationList.bind(
-      spritelab
-    );
-    options.getGeneratedProperties = spritelab.getGeneratedProperties.bind(
-      spritelab
-    );
+      // ToDo: log failed fetch
 
-    spritelab.injectStudioApp(studioApp());
-    appMain(spritelab, levels, options);
+      return initializeOptionsAndSpritelab(spritelab);
+    });
+}
 
-    return spritelab;
-  });
+function initializeOptionsAndSpritelab(spritelab, options) {
+  // Bind helper that provides project metadata for gamelab autosave
+  options.getAnimationList = spritelab.getSerializedAnimationList.bind(
+    spritelab
+  );
+  options.getGeneratedProperties = spritelab.getGeneratedProperties.bind(
+    spritelab
+  );
+
+  spritelab.injectStudioApp(studioApp());
+  appMain(spritelab, levels, options);
+
+  return spritelab;
 }
