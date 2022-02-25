9f7caf7bbc3a1e6223c4cdcca6ef76ebd4ef8bee
Bethany Connor <bethany@code.org>
2022-02-25 10:32:23 -0800

    Set error to null if save is successful

--- apps/src/lib/levelbuilder/code-docs-editor/ProgrammingExpressionEditor.jsx
+++ apps/src/lib/levelbuilder/code-docs-editor/ProgrammingExpressionEditor.jsx
@@ -88,6 +88,7 @@ export default function ProgrammingExpressionEditor({
             navigateToHref(showPath);
           } else {
             setLastUpdated(Date.now());
+            setError(null);
           }
         } else {
           setError(response.statusText);
