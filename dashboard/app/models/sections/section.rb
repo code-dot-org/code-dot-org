bd8b581be1acb6b56d74bd5bb05d529880bd19df
Dani <dani@code.org>
2022-02-25 09:19:56 -0500

    Revert "Pass down assigned course offering and course version"
    This reverts commit ab8547d3c203f8d72433bda41bbbebc3a3d534aa.

--- dashboard/app/models/sections/section.rb
+++ dashboard/app/models/sections/section.rb
@@ -328,9 +328,6 @@ class Section < ApplicationRecord
       tts_autoplay_enabled: tts_autoplay_enabled,
       sharing_disabled: sharing_disabled?,
       login_type: login_type,
-      course_offering_id: unit_group ? unit_group&.course_version&.course_offering : script&.course_version&.course_offering,
-      course_version_id: unit_group ? unit_group&.course_version : script&.course_version,
-      unit_id: script_id,
       course_id: course_id,
       script: {
         id: script_id,
