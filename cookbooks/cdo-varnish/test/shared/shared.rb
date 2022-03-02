8064af2291b0e5610282af8380ce54cf9e4718bc
Maureen <maureen.sturgeon@gmail.com>
2022-03-02 10:44:40 -0800

    fix shared test again

--- cookbooks/cdo-varnish/test/shared/shared.rb
+++ cookbooks/cdo-varnish/test/shared/shared.rb
@@ -490,6 +490,7 @@ module HttpCacheTest
       end
 
       it 'Strips cookies from the penultimate dance level' do
+        assert strips_session_specific_cookies_from_request? '/s/dance/lessons/1/levels/12'
         assert strips_session_specific_cookies_from_request? '/s/dance-2019/lessons/1/levels/9'
       end
 
