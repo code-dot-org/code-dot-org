7346da49e405eb29dc3f94cd0606fdd878bf43ab
Dani <dani@code.org>
2022-03-06 20:00:00 -0500

    Fix the way lesson extras gets calculated

--- dashboard/app/models/sections/section.rb
+++ dashboard/app/models/sections/section.rb
@@ -332,8 +332,7 @@ class Section < ApplicationRecord
       script: {
         id: script_id,
         name: script.try(:name),
-        project_sharing: script.try(:project_sharing),
-        lesson_extras_available: script.lesson_extras_available?
+        project_sharing: script.try(:project_sharing)
       },
       studentCount: num_students,
       grade: grade,
