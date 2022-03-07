a5e07c47bff0e522f94f1da50ec39b44ebf0eb1a
Turner Riley <turnertriley@gmail.com>
2022-03-07 14:57:27 -0800

    update ManageStudentsTableTest to test for students with the same name and ensure they are sorted properly

--- apps/test/unit/templates/manageStudents/ManageStudentsTableTest.js
+++ apps/test/unit/templates/manageStudents/ManageStudentsTableTest.js
@@ -47,9 +47,12 @@ describe('ManageStudentsTable', () => {
   it('sortRows orders table in the following order: add, newStudent, student', () => {
     const rowData = [
       {id: 1, name: 'studentb', rowType: RowType.STUDENT},
-      {id: 3, name: 'studenta', rowType: RowType.STUDENT},
+      {id: 5, name: 'studenta', rowType: RowType.STUDENT},
       {id: 0, name: '', rowType: RowType.ADD},
-      {id: 2, name: 'studentf', rowType: RowType.NEW_STUDENT}
+      {id: 2, name: 'studentf', rowType: RowType.NEW_STUDENT},
+      {id: 4, name: 'studenta', rowType: RowType.STUDENT},
+      {id: 3, name: 'studenta', rowType: RowType.STUDENT},
+      {id: 6, name: 'studentf', rowType: RowType.STUDENT}
     ];
     const columnIndexList = [];
     const orderList = ['asc'];
@@ -57,7 +60,10 @@ describe('ManageStudentsTable', () => {
     expect(sortedList[0].id).to.equal(0);
     expect(sortedList[1].id).to.equal(2);
     expect(sortedList[2].id).to.equal(3);
-    expect(sortedList[3].id).to.equal(1);
+    expect(sortedList[3].id).to.equal(4);
+    expect(sortedList[4].id).to.equal(5);
+    expect(sortedList[5].id).to.equal(1);
+    expect(sortedList[6].id).to.equal(6);
   });
 
   describe('appropriate buttons render', () => {
