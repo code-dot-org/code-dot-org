68abe0bba1471e3cc7b5073000816edb58004155
Dani <dani@code.org>
2022-03-07 08:33:56 -0500

    update student grade levels to array

--- apps/src/templates/teacherDashboard/OwnedSectionsTable.jsx
+++ apps/src/templates/teacherDashboard/OwnedSectionsTable.jsx
@@ -159,12 +159,7 @@ class OwnedSectionsTable extends Component {
     if (this.state.sortingColumns[gradeCol]) {
       const mult = directionArray[0] === 'asc' ? 1 : -1;
       return sortBy(data, function(obj) {
-        return (
-          mult *
-          Object.values(StudentGradeLevels)
-            .concat(null)
-            .indexOf(obj.grade)
-        );
+        return mult * StudentGradeLevels.concat(null).indexOf(obj.grade);
       });
     } else {
       return orderBy(data, activeColumn, directionArray);
