cfee50556d14ce0af5a3d7c27a3dae4aeb5f3448
Dayne <dayne@code.org>
2022-03-09 14:44:05 -0800

    Service worker loads files listed in /cached-files.json

--- apps/src/offline/offline-service-worker.js
+++ apps/src/offline/offline-service-worker.js
@@ -1,3 +1,5 @@
+import 'whatwg-fetch';
+
 /**
  * The Service Worker which enables the offline PWA experience for Code.org. It is responsible for
  * caching html, javascript, images, etc so that they can be used when students don't have an
@@ -5,20 +7,28 @@
  */
 const cacheVersion = 2;
 const cacheName = `code-org-pwa-v${cacheVersion}`;
-// const webpackManifest = require('../../build/package/js/webpackManifest.json');
-
-//TODO this path doesn't work
-const cachedFiles = require('./cachedFiles.json').cachedFiles;
-
-console.log('ANNA CACHEDFILES', cachedFiles);
 
 self.addEventListener('install', e => {
   console.log('[Service Worker] installing offline service worker!');
   e.waitUntil(
     (async () => {
       const cache = await caches.open(cacheName);
+      const cachedFiles = await getCachedFilesList();
       console.log('[Service Worker] Caching:', cachedFiles);
       await cache.addAll(cachedFiles);
     })()
   );
 });
+
+async function getCachedFilesList() {
+  // Request the manifest of all the files which should be cached.
+  const response = await fetch('/cached-files.json', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  // TODO Add check for 404 status
+  const responseJson = await response.json();
+  return responseJson.cachedFiles;
+}
