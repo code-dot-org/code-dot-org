c7a9fd8a21990c37fcc626723afdb19f08b8de3f
Maureen <maureen.sturgeon@gmail.com>
2022-03-14 16:43:43 -0700

    create view in drone for consistency

--- pegasus/migrations/137_rename_user_storage_ids.rb
+++ pegasus/migrations/137_rename_user_storage_ids.rb
@@ -1,9 +1,9 @@
 Sequel.migration do
   up do
     pegasus_user_storage_ids = "#{CDO.pegasus_db_name}__user_storage_ids".to_sym
+    dashboard_user_project_storage_ids = "#{CDO.dashboard_db_name}__user_project_storage_ids".to_sym
 
     unless [:production].include?(rack_env) || ENV['CI']
-      dashboard_user_project_storage_ids = "#{CDO.dashboard_db_name}__user_project_storage_ids".to_sym
 
       # Rename the table to move it from the Pegasus schema to Dashboard
       # Also change the name from user_storage_ids to user_project_storage_ids
@@ -17,9 +17,10 @@ Sequel.migration do
 
     # In Drone CI, the table will be created from from schema.rb file. After that, this migration
     # will get run, we can't do a table rename at that point because user_project_storage_ids will already
-    # exist. So we just want to drop the old table.
+    # exist. So we just need to drop the old table and create the view for consistency between envs.
     if ENV['CI']
       drop_table(pegasus_user_storage_ids)
+      create_view(pegasus_user_storage_ids, DB[dashboard_user_project_storage_ids].select_all)
     end
   end
 
