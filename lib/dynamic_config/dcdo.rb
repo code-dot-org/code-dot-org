555bc59c0509c194919951c076b60d9bcee7bd10
Turner Riley <turner.riley@code.org>
2022-03-14 14:00:14 -0700

    add DCDO frontend flag

--- lib/dynamic_config/dcdo.rb
+++ lib/dynamic_config/dcdo.rb
@@ -94,7 +94,8 @@ class DCDOBase
     # For example:
     # 'my-new-feature': DCDO.get('my-new-feature', false)
     {
-      'frontend-i18n-tracking': DCDO.get('frontend-i18n-tracking', false)
+      'frontend-i18n-tracking': DCDO.get('frontend-i18n-tracking', false),
+      'clearerSignUpUserType': DCDO.get('clearerSignUpUserType', false)
     }
   end
 end
