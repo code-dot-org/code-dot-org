e3a45a0542c7c0e530d8e475a69f6229d77e8910
Bethany Connor <bethany@code.org>
2022-03-14 13:10:34 -0700

    Add tests for index access and for get_filtered_expressions, also fix access

--- dashboard/app/models/ability.rb
+++ dashboard/app/models/ability.rb
@@ -66,7 +66,6 @@ class Ability
       ReviewableProject
     ]
     cannot :index, Level
-    cannot :index, ProgrammingExpression
 
     # If you can see a level, you can also do these things:
     can [:embed_level, :get_rubric, :get_serialized_maze], Level do |level|
@@ -85,6 +84,7 @@ class Ability
     can [:read, :show_by_keys], ProgrammingExpression do |expression|
       can? :read, expression.programming_environment
     end
+    cannot :index, ProgrammingExpression
 
     if user.persisted?
       can :manage, user
