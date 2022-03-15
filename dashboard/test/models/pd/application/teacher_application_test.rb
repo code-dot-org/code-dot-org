47f845cb948f3484f4176852ce8bc2b8a3254fbf
Meg Crenshaw <meg@code.org>
2022-03-15 15:23:07 -0400

    Update tests to reflect applied_at date

--- dashboard/test/models/pd/application/teacher_application_test.rb
+++ dashboard/test/models/pd/application/teacher_application_test.rb
@@ -109,6 +109,8 @@ module Pd::Application
       assert_equal Pd::Application::TeacherApplication::REVIEWING_INCOMPLETE, teacher_application.meets_criteria
     end
 
+    # [MEG] TODO: Remove this test once the applied_at column exists, i.e. once
+    # ActiveRecord::Base.connection.column_exists?(:pd_applications, :applied_at) is true
     test 'date_applied finds date of first unreviewed status' do
       tomorrow = Date.tomorrow.to_time
       next_day = tomorrow + 1.day
