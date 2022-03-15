8cd9fc807c128f950f4f8f1e81ec48524745c38f
Dani <dani@code.org>
2022-03-15 09:52:51 -0400

    Remove valid_scripts controller actions that is no longer in use

--- dashboard/app/controllers/api/v1/sections_controller.rb
+++ dashboard/app/controllers/api/v1/sections_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SectionsController < Api::V1::JsonApiController
   load_resource :section, find_by: :code, only: [:join, :leave]
   before_action :find_follower, only: :leave
-  load_and_authorize_resource except: [:join, :leave, :membership, :valid_scripts, :create, :update, :require_captcha]
+  load_and_authorize_resource except: [:join, :leave, :membership, :create, :update, :require_captcha]
 
   skip_before_action :verify_authenticity_token, only: [:update_sharing_disabled, :update]
 
@@ -179,14 +179,6 @@ class Api::V1::SectionsController < Api::V1::JsonApiController
   def membership
     return head :forbidden unless current_user
     render json: current_user.sections_as_student, each_serializer: Api::V1::SectionNameAndIdSerializer
-  end
-
-  # GET /api/v1/sections/valid_scripts
-  def valid_scripts
-    return head :forbidden unless current_user
-
-    scripts = Script.valid_scripts(current_user).map(&:assignable_info)
-    render json: scripts
   end
 
   # GET /api/v1/sections/valid_course_offerings
