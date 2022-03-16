dfad53747f4eb340975360eaee0db1d240b76a72
Katleiah <katleiah@code.org>
2022-03-16 11:35:13 -0700

    Fix registerGlobalVariables

--- apps/src/blockly/addons/cdoVariableMap.js
+++ apps/src/blockly/addons/cdoVariableMap.js
@@ -2,6 +2,7 @@ import GoogleBlockly from 'blockly/core';
 
 export default class CdoVariableMap extends GoogleBlockly.VariableMap {
   addVariables(variableList) {
+    // createVariable() is a blockly method
     variableList.forEach(varName => this.createVariable(varName));
   }
 }
