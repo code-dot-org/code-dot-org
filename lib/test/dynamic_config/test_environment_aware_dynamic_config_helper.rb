3c12e9d90c9805164c46e0b087bfe82093a0f737
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-03-15 17:34:26 -0700

    consolidate some repeated code in a unit test into a helper method

--- lib/test/dynamic_config/test_environment_aware_dynamic_config_helper.rb
+++ lib/test/dynamic_config/test_environment_aware_dynamic_config_helper.rb
@@ -4,48 +4,30 @@ require 'dynamic_config/environment_aware_dynamic_config_helper'
 class EnvironmentAwareDynamicConfigHelperTest < Minitest::Test
   def test_create_datastore_cache_logic_on_test
     # By default, unit tests (which this is) use the memory adapter
-    datastore = EnvironmentAwareDynamicConfigHelper.
-      create_datastore_cache(:test_create_datastore_cache).
-      instance_variable_get(:@datastore)
-    assert datastore.is_a? MemoryAdapter
+    assert created_datastore_for_current_environment.is_a? MemoryAdapter
 
     # If tests detect that they are running in a web application context,
     # they'll instead use DynamoDB to mirror production
     EnvironmentAwareDynamicConfigHelper.stubs(:running_web_application?).returns(true)
-    datastore = EnvironmentAwareDynamicConfigHelper.
-      create_datastore_cache('test_create_datastore_cache').
-      instance_variable_get(:@datastore)
-    assert datastore.is_a? DynamoDBAdapter
+    assert created_datastore_for_current_environment.is_a? DynamoDBAdapter
   end
 
   def test_create_datastore_cache_logic_on_production
     # Production uses DynamoDB whether or not the web application is running
     Rails.stubs(:env).returns(:production)
-    datastore = EnvironmentAwareDynamicConfigHelper.
-      create_datastore_cache('test_create_datastore_cache').
-      instance_variable_get(:@datastore)
-    assert datastore.is_a? DynamoDBAdapter
+    assert created_datastore_for_current_environment.is_a? DynamoDBAdapter
 
     EnvironmentAwareDynamicConfigHelper.stubs(:running_web_application?).returns(true)
-    datastore = EnvironmentAwareDynamicConfigHelper.
-      create_datastore_cache('test_create_datastore_cache').
-      instance_variable_get(:@datastore)
-    assert datastore.is_a? DynamoDBAdapter
+    assert created_datastore_for_current_environment.is_a? DynamoDBAdapter
   end
 
   def test_create_datastore_cache_logic_default
     # Everything else uses the local filesystem
     Rails.stubs(:env).returns(:development)
-    datastore = EnvironmentAwareDynamicConfigHelper.
-      create_datastore_cache('test_create_datastore_cache').
-      instance_variable_get(:@datastore)
-    assert datastore.is_a? JSONFileDatastoreAdapter
+    assert created_datastore_for_current_environment.is_a? JSONFileDatastoreAdapter
 
     Rails.stubs(:env).returns(:unknown)
-    datastore = EnvironmentAwareDynamicConfigHelper.
-      create_datastore_cache('test_create_datastore_cache').
-      instance_variable_get(:@datastore)
-    assert datastore.is_a? JSONFileDatastoreAdapter
+    assert created_datastore_for_current_environment.is_a? JSONFileDatastoreAdapter
   end
 
   def test_rack_or_rails_env
@@ -59,5 +41,14 @@ class EnvironmentAwareDynamicConfigHelperTest < Minitest::Test
     # returns the Rails env if defined
     Rails.stubs(:env).returns(:development)
     assert_equal EnvironmentAwareDynamicConfigHelper.rack_or_rails_env, "development"
+  end
+
+  private
+
+  # Simple helper method to DRY up the code
+  def created_datastore_for_current_environment
+    return EnvironmentAwareDynamicConfigHelper.
+      create_datastore_cache('test_create_datastore_cache').
+      instance_variable_get(:@datastore)
   end
 end
