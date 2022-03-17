2482f4cc83a05a22d67348cbec9c57b0e6714ca6
Tim Plummer <tim@code.org>
2022-03-16 18:25:25 -0700

    move edit all to levelbuilder folder

--- apps/src/sites/studio/pages/reference_guides/edit_all.js
+++ apps/src/sites/studio/pages/reference_guides/edit_all.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import getScriptData from '@cdo/apps/util/getScriptData';
-import ReferenceGuideEditAll from '@cdo/apps/templates/referenceGuides/ReferenceGuideEditAll';
+import ReferenceGuideEditAll from '@cdo/apps/lib/levelbuilder/reference-guide-editor/ReferenceGuideEditAll';
 
 $(() => {
   const referenceGuides = getScriptData('referenceGuides');
