817a5b26a72954787245d2a7194c26dee389bf42
Meg Crenshaw <meg@code.org>
2022-03-17 13:41:10 -0400

    Simplify migration

--- dashboard/db/migrate/20220316125751_applied_at_column.rb
+++ dashboard/db/migrate/20220316125751_applied_at_column.rb
@@ -1,15 +1,5 @@
 class AppliedAtColumn < ActiveRecord::Migration[5.2]
   def change
-    reversible do |dir|
-      dir.up do
-        add_column :pd_applications, :applied_at, :datetime
-        Pd::Application::ApplicationBase.all.each do |application|
-          application.update_column(:applied_at, application.created_at)
-        end
-      end
-      dir.down do
-        remove_column :pd_applications, :applied_at, :datetime
-      end
-    end
+    add_column :pd_applications, :applied_at, :datetime
   end
 end
