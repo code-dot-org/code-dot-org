78312eb8fbfdca83068536ef1af534e47b204499
Tim Plummer <tim@code.org>
2022-03-17 12:09:30 -0700

    move edit all and add tests for both pages

--- apps/test/unit/lib/levelbuilder/reference-guide-editor/ReferenceGuidesEditAllTest.js
+++ apps/test/unit/lib/levelbuilder/reference-guide-editor/ReferenceGuidesEditAllTest.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {isolateComponent} from 'isolate-react';
-import {expect} from '../../../util/reconfiguredChai';
-import ReferenceGuideEditAll from '@cdo/apps/templates/referenceGuides/ReferenceGuideEditAll';
+import sinon from 'sinon';
+import {expect} from '../../../../util/reconfiguredChai';
+import ReferenceGuideEditAll from '@cdo/apps/lib/levelbuilder/reference-guide-editor/ReferenceGuideEditAll';
 
 const makeReferenceGuide = (key, parent = null, pos = 0) => ({
   display_name: key,
@@ -11,6 +12,16 @@ const makeReferenceGuide = (key, parent = null, pos = 0) => ({
 });
 
 describe('ReferenceGuideEditAll', () => {
+  let fetchSpy;
+
+  beforeEach(() => {
+    fetchSpy = sinon.stub(window, 'fetch');
+  });
+
+  afterEach(() => {
+    fetchSpy.restore();
+  });
+
   it('displays the name of the reference guide', () => {
     const referenceGuides = [makeReferenceGuide('hello_world')];
     const wrapper = isolateComponent(
@@ -91,5 +102,87 @@ describe('ReferenceGuideEditAll', () => {
     expect(
       wrapper.findAll('.guide-box').map(box => box.props.style.paddingLeft)
     ).to.deep.equal(['4px', '24px', '24px', '44px', '44px', '24px']); // a b c e f d
+  });
+
+  it('allows guides to be moved', () => {
+    fetchSpy.returns(
+      Promise.resolve({
+        ok: true
+      })
+    );
+    const referenceGuides = [
+      makeReferenceGuide('a', null, 0),
+      makeReferenceGuide('b', null, 1),
+      makeReferenceGuide('c', null, 2)
+    ];
+    const wrapper = isolateComponent(
+      <ReferenceGuideEditAll referenceGuides={referenceGuides} />
+    );
+    expect(
+      wrapper.findAll('.guide-box').map(box => box.content())
+    ).to.deep.equal(['a', 'b', 'c']);
+
+    // click down on b
+    wrapper
+      .findAll('.actions-box')[1]
+      .findAll('MiniIconButton')[3]
+      .props.func();
+    expect(
+      wrapper.findAll('.guide-box').map(box => box.content())
+    ).to.deep.equal(['a', 'c', 'b']);
+    expect(fetchSpy).to.have.callCount(2);
+
+    // click up on a
+    wrapper
+      .findAll('.actions-box')[0]
+      .findAll('MiniIconButton')[2]
+      .props.func();
+    expect(
+      wrapper.findAll('.guide-box').map(box => box.content())
+    ).to.deep.equal(['a', 'c', 'b']);
+    expect(fetchSpy).to.have.callCount(2);
+
+    // click down on b
+    wrapper
+      .findAll('.actions-box')[2]
+      .findAll('MiniIconButton')[3]
+      .props.func();
+    expect(
+      wrapper.findAll('.guide-box').map(box => box.content())
+    ).to.deep.equal(['a', 'c', 'b']);
+    expect(fetchSpy).to.have.callCount(2);
+
+    // click down on a
+    wrapper
+      .findAll('.actions-box')[0]
+      .findAll('MiniIconButton')[3]
+      .props.func();
+    expect(
+      wrapper.findAll('.guide-box').map(box => box.content())
+    ).to.deep.equal(['c', 'a', 'b']);
+    expect(fetchSpy).to.have.callCount(4);
+  });
+
+  it('allows guides to be deleted', () => {
+    const referenceGuides = [
+      makeReferenceGuide('a', null, 0),
+      makeReferenceGuide('b', null, 1),
+      makeReferenceGuide('c', null, 2)
+    ];
+    const wrapper = isolateComponent(
+      <ReferenceGuideEditAll referenceGuides={referenceGuides} />
+    );
+    expect(
+      wrapper.findAll('.guide-box').map(box => box.content())
+    ).to.deep.equal(['a', 'b', 'c']);
+
+    // click delete on second
+    wrapper
+      .findAll('.actions-box')[1]
+      .findAll('MiniIconButton')[1]
+      .props.func();
+    expect(
+      wrapper.findAll('.guide-box').map(box => box.content())
+    ).to.deep.equal(['a', 'c']);
   });
 });
