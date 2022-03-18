12c101632bd43047f7483a5b75620909bdf88ae0
Anna Xu <annaxu@wustl.edu>
2022-03-18 15:01:56 -0700

    delete old caches

--- apps/src/offline/offline-service-worker.js
+++ apps/src/offline/offline-service-worker.js
@@ -8,7 +8,10 @@ import 'whatwg-fetch';
 
 // cacheVersion should be updated anytime we want users to start over with a new cache.
 const cacheVersion = 2;
-const cacheName = `code-org-pwa-express-2021-lessons-1-v${cacheVersion}`;
+const pwaNamespace = 'code-org-pwa';
+const lessonName = 'express-2021-lessons-1';
+const cacheVersionName = `${pwaNamespace}-v${cacheVersion}`;
+const cacheName = `${cacheVersionName}-${lessonName}`;
 
 /**
  * Runs when the service worker is updated or registered for the first time.
@@ -17,7 +20,11 @@ self.addEventListener('install', e => {
   console.log('[Service Worker] installing offline service worker!');
   // TODO - Add retry logic if cache.addAll fails for one of the assets.
   // TODO - Add ability to clear the cache and/or reinstall.
-  e.waitUntil(caches.open(cacheName).then(cache => cacheFiles(cache)));
+  e.waitUntil(
+    deleteOldCaches()
+      .then(() => caches.open(cacheName))
+      .then(cache => cacheFiles(cache))
+  );
 });
 
 /**
@@ -64,11 +71,11 @@ async function cacheFiles(cache) {
  */
 function cacheFilesHelper(cache, filepath, attempt, retries) {
   if (attempt > retries) {
-    throw `Failed to cache ${filepath} after ${retries} retries`;
+    throw `[Service Worker] Failed to cache ${filepath} after ${retries} retries`;
   }
   return cache.add(filepath).catch(err => {
     console.log(
-      `Failed to cache ${filepath}, attempt ${attempt}/${retries}`,
+      `[Service Worker] Failed to cache ${filepath}, attempt ${attempt}/${retries}`,
       err
     );
     cacheFilesHelper(cache, filepath, attempt + 1, retries);
@@ -91,4 +98,21 @@ async function getOfflineFilesList() {
   // TODO Add check for 404 status
   const responseJson = await response.json();
   return responseJson.files;
+}
+
+/**
+ * Deletes all caches that don't match the namespace and current cache version
+ * @returns {Promise<void>}
+ */
+async function deleteOldCaches() {
+  caches.keys().then(keys =>
+    Promise.all(
+      keys.map(async key => {
+        if (key.startsWith(pwaNamespace) && !key.startsWith(cacheVersionName)) {
+          console.log('[Service Worker] deleting out-of-date cache: ', key);
+          return caches.delete(key);
+        }
+      })
+    )
+  );
 }
