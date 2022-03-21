b8a0be9beebb979cff6477fc38017dd0c23d5403
Hannah Bergam <hannahbergam@gmail.com>
2022-03-21 12:32:24 -0700

    safe search added back

--- dashboard/app/controllers/api/v1/users_controller.rb
+++ dashboard/app/controllers/api/v1/users_controller.rb
@@ -51,7 +51,7 @@ class Api::V1::UsersController < Api::V1::JsonApiController
 
   # GET /api/v1/users/<user_id>/using_text_mode
   def get_using_text_mode
-    render json: {using_text_mode: @user&.using_text_mode}
+    render json: {using_text_mode: !!@user&.using_text_mode}
   end
 
   # GET /api/v1/users/<user_id>/display_theme
@@ -61,7 +61,7 @@ class Api::V1::UsersController < Api::V1::JsonApiController
 
   # GET /api/v1/users/<user_id>/mute_music
   def get_mute_music
-    render json: {mute_music: @user&.mute_music}
+    render json: {mute_music: !!@user&.mute_music}
   end
 
   # GET /api/v1/users/<user_id>/get_donor_teacher_banner_details
@@ -104,7 +104,7 @@ class Api::V1::UsersController < Api::V1::JsonApiController
     @user.using_text_mode = !!params[:using_text_mode].try(:to_bool)
     @user.save
 
-    render json: {using_text_mode: !!@user.using_text_mode}
+    render json: {using_text_mode: !!@user&.using_text_mode}
   end
 
   # POST /api/v1/users/<user_id>/mute_music
@@ -112,7 +112,7 @@ class Api::V1::UsersController < Api::V1::JsonApiController
     @user.mute_music = !!params[:mute_music].try(:to_bool)
     @user.save
 
-    render json: {mute_music: !!@user.mute_music}
+    render json: {mute_music: !!@user&.mute_music}
   end
 
   # POST /api/v1/users/<user_id>/display_theme
