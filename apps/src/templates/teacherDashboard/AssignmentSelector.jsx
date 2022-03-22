88749e6de626b50d2a4ef70bfacbe49de4cb184a
Dani <dani@code.org>
2022-03-21 22:11:31 -0400

    Fix decide later

--- apps/src/templates/teacherDashboard/AssignmentSelector.jsx
+++ apps/src/templates/teacherDashboard/AssignmentSelector.jsx
@@ -74,7 +74,7 @@ export default class AssignmentSelector extends Component {
     ) {
       this.setState(
         {
-          selectedCourseOfferingId: noAssignment,
+          selectedCourseOfferingId: event.target.value,
           selectedCourseVersionId: noAssignment,
           selectedUnitId: noAssignment
         },
