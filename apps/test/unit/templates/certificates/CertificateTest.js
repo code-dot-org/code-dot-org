91426ce2ed851212ba5f8c8cbb0b5e906c48b3bd
David Bailey <davidsbailey@users.noreply.github.com>
2022-03-23 16:18:33 -0700

    also move certificate unit tests into subdir

--- apps/test/unit/templates/certificates/CertificateTest.js
+++ apps/test/unit/templates/certificates/CertificateTest.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {mount} from 'enzyme';
-import {expect} from '../../util/reconfiguredChai';
+import {expect} from '../../../util/reconfiguredChai';
 import Certificate from '@cdo/apps/templates/certificates/Certificate';
 import {combineReducers, createStore} from 'redux';
 import responsive from '@cdo/apps/code-studio/responsiveRedux';
