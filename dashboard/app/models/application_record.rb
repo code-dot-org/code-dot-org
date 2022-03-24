be074e26c9e2ef7c39ac501399f4a7f681a54fe6
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-03-24 13:13:39 -0700

    stop special-casing test

--- dashboard/app/models/application_record.rb
+++ dashboard/app/models/application_record.rb
@@ -1,5 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  connects_to database: {writing: :primary, reading: Rails.env.test? ? :primary : :primary_replica}
+  connects_to database: {writing: :primary, reading: :primary_replica}
 end
