458a890a0830bec80a34c73c096a325db1122498
Hannah Bergam <hannahbergam@gmail.com>
2022-03-23 19:56:14 -0700

    making separate controller for this, updating routes, building out save operation

---
+++ dashboard/app/controllers/admin_nps_controller.rb
@@ -0,0 +1,25 @@
+require 'cdo/env'
+require 'set'
+
+# The controller for manipulating NPS surveys
+class AdminNpsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin
+  check_authorization
+
+  # PUT /admin/nps_update
+  def nps_update
+    audience = params[:audience]
+    if audience == "none"
+      ## in survey_results_helper.rb set the NPS_SURVEY_ENABLED = false
+    else
+      ## in survey_results_helper.rb set the NPS_SURVEY_ENABLED = true
+      if audience == "odd"
+        # set audience according to odd in survey_results_helper.rb show_nps_survey?
+      end
+      if audience == "even"
+        # set audience according to odd in survey_results_helper.rb show_nps_survey?
+      end
+    end
+  end
+end
