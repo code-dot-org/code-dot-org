fb4b4acfc78b1a4cb08a9f5e566826d1676c313e
Mike Harvey <mike.harvey@code.org>
2022-04-04 15:36:55 -0400

    prevent regressions in cdo blockly labs

--- apps/src/blockly/addons/cdoUtils.js
+++ apps/src/blockly/addons/cdoUtils.js
@@ -1,3 +1,7 @@
 export function setHSV(block, h, s, v) {
   block.setColour(Blockly.utils.colour.hsvToHex(h, s, v * 255));
 }
+
+export function getBlockFields(block) {
+  return block.getTitles();
+}
