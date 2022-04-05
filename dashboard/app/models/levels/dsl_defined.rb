1fc75134a34c9125895c64d92d77c4f61deb352a
Maureen <maureen.sturgeon@gmail.com>
2022-04-04 17:01:02 -0700

    update to run test locally and date comparison fix

--- dashboard/app/models/levels/dsl_defined.rb
+++ dashboard/app/models/levels/dsl_defined.rb
@@ -88,7 +88,7 @@ class DSLDefined < Level
     level = find_or_create_by({name: data[:name]})
     level.send(:write_attribute, 'properties', {})
 
-    level.update!(name: data[:name], game_id: Game.find_by(name: to_s).id, properties: data[:properties])
+    level.update!(name: data[:name], game_id: Game.find_by(name: to_s)&.id, properties: data[:properties])
 
     level
   end
