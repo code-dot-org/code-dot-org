802b82a919b59d0213de153b7714a2066db43a66
Bethany Connor <bethany@code.org>
2022-04-07 10:59:24 -0700

    Rename MethodToken to MethodNameEditor

--- apps/test/unit/lib/levelbuilder/code-docs-editor/MethodNameEditorTest.jsx
+++ apps/test/unit/lib/levelbuilder/code-docs-editor/MethodNameEditorTest.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from '../../../../util/reconfiguredChai';
-import MethodToken from '@cdo/apps/lib/levelbuilder/code-docs-editor/MethodToken';
+import MethodNameEditor from '@cdo/apps/lib/levelbuilder/code-docs-editor/MethodNameEditor';
 import sinon from 'sinon';
 
-describe('MethodToken', () => {
+describe('MethodNameEditor', () => {
   let defaultProps, updateSpy;
 
   beforeEach(() => {
@@ -18,13 +18,13 @@ describe('MethodToken', () => {
   });
 
   it('displays input for name method', () => {
-    const wrapper = shallow(<MethodToken {...defaultProps} />);
+    const wrapper = shallow(<MethodNameEditor {...defaultProps} />);
     const nameMethod = wrapper.find('input').at(0);
     expect(nameMethod.props().value).to.equal('method 1');
   });
 
   it('calls update when name is updated', () => {
-    const wrapper = shallow(<MethodToken {...defaultProps} />);
+    const wrapper = shallow(<MethodNameEditor {...defaultProps} />);
     const nameMethod = wrapper.find('input').at(0);
     nameMethod.simulate('change', {target: {value: 'new name'}});
     expect(updateSpy).to.be.calledOnce.and.calledWith('name', 'new name');
