7b6a068c9aeafffe7d31a3d97270c277255c7f41
Dani <dani@code.org>
2022-04-07 22:32:47 -0400

    Update StudentSectionsTest

--- apps/test/unit/templates/studioHomepages/JoinSectionAreaTest.jsx
+++ apps/test/unit/templates/studioHomepages/JoinSectionAreaTest.jsx
@@ -1,10 +1,10 @@
 import {expect} from '../../../util/reconfiguredChai';
 import React from 'react';
-import {shallow} from 'enzyme';
 import JoinSectionArea from '@cdo/apps/templates/studioHomepages/JoinSectionArea';
-import {joinedSections} from './homepagesTestData';
+import {joinedSections, joinedPlSections} from './homepagesTestData';
+import {isolateComponent} from 'isolate-react';
 
-const DEFAULT_PROPS = {
+const defaultProps = {
   initialJoinedPlSections: [],
   initialJoinedStudentSections: [],
   isTeacher: false
@@ -12,26 +12,26 @@ const DEFAULT_PROPS = {
 
 describe('JoinSectionArea', () => {
   it('shows student sections if has joined student sections', () => {
-    const wrapper = shallow(
+    const wrapper = isolateComponent(
       <JoinSectionArea
-        {...DEFAULT_PROPS}
+        {...defaultProps}
         initialJoinedStudentSections={joinedSections}
       />
     );
-    expect(wrapper.find('JoinSection').length).to.equal(1);
-    expect(wrapper.find('StudentSections').length).to.equal(1);
-    expect(wrapper.find('StudentSections').props().isTeacher).to.equal(false);
+    expect(wrapper.findAll('JoinSection').length).to.equal(1);
+    expect(wrapper.findAll('StudentSections').length).to.equal(1);
+    expect(wrapper.findAll('StudentSections').props.isTeacher).to.equal(false);
   });
   it('shows participant sections for pl if has joined pl sections', () => {
-    const wrapper = shallow(
+    const wrapper = isolateComponent(
       <JoinSectionArea
-        {...DEFAULT_PROPS}
+        {...defaultProps}
         isTeacher={true}
-        initialJoinedPlSections={[]}
+        initialJoinedPlSections={joinedPlSections}
       />
     );
-    expect(wrapper.find('JoinSection').length).to.equal(1);
-    expect(wrapper.find('StudentSections').length).to.equal(1);
-    expect(wrapper.find('StudentSections').props().isTeacher).to.equal(true);
+    expect(wrapper.findAll('JoinSection').length).to.equal(1);
+    expect(wrapper.findAll('StudentSections').length).to.equal(1);
+    expect(wrapper.findAll('StudentSections').props.isTeacher).to.equal(true);
   });
 });
