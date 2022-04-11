bf91fcbd397a4162bb81b891e028f99505d3eaac
Maureen <maureen.sturgeon@gmail.com>
2022-04-11 09:45:37 -0700

    update schema for index fix

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_07_200052) do
+ActiveRecord::Schema.define(version: 2022_04_08_173143) do
 
   create_table "activities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
