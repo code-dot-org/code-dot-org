8ddc3d03b5d7ef0427bbaea4aec44064793d458a
Meg Crenshaw <meg@code.org>
2022-04-13 12:09:48 -0400

    Add preliminary styling

--- apps/src/templates/certificates/PetitionSignUp.jsx
+++ apps/src/templates/certificates/PetitionSignUp.jsx
@@ -3,10 +3,27 @@ import React from 'react';
 export default function PetitionSignUp() {
   return (
     <>
-      <h1>
-        "Every student in every school should have the opportunity to learn
-        computer science"
-      </h1>
+      <div id="petition-block" style={styles.block}>
+        <h1 id="petition-message" style={styles.message}>
+          "Every student in every school should have the opportunity to learn
+          computer science"
+        </h1>
+      </div>
     </>
   );
 }
+
+const styles = {
+  block: {
+    textAlign: 'center',
+    backgroundColor: 'white',
+    padding: '10px 0px',
+    display: 'block'
+  },
+  message: {
+    fontFamily: '"Gotham 5r", sans-serif',
+    color: 'rgb(91,103,112)',
+    fontSize: '22px',
+    lineHeight: '28px'
+  }
+};
