398410829e2859985707ac7656ab2927f311d659
Dani <dani@code.org>
2022-04-13 20:49:20 -0400

    Add tests in TeacherSectionsTest

--- apps/test/unit/templates/studioHomepages/TeacherSectionsTest.jsx
+++ apps/test/unit/templates/studioHomepages/TeacherSectionsTest.jsx
@@ -5,12 +5,70 @@ import {UnconnectedTeacherSections as TeacherSections} from '@cdo/apps/templates
 
 describe('TeacherSections', () => {
   const defaultProps = {
-    asyncLoadSectionData: () => {}
+    asyncLoadSectionData: () => {},
+    studentSectionIds: [],
+    plSectionIds: [],
+    asyncLoadComplete: true
   };
 
-  it('renders an OwnedSections component', () => {
+  it('renders spinner if still loading', () => {
+    const wrapper = shallow(
+      <TeacherSections
+        {...defaultProps}
+        plSectionIds={null}
+        studentSectionIds={null}
+        asyncLoadComplete={false}
+      />
+    );
+    expect(wrapper.find('Connect(ContentContainer)').length).to.equal(1);
+    expect(wrapper.find('Spinner').length).to.equal(1);
+    expect(wrapper.find('Connect(OwnedSections)').length).to.equal(0);
+  });
+
+  it('renders create section area if done loading', () => {
     const wrapper = shallow(<TeacherSections {...defaultProps} />);
+    expect(wrapper.find('Connect(ContentContainer)').length).to.equal(1);
+    expect(wrapper.find('Connect(SetupSections)').length).to.equal(1);
+    expect(wrapper.find('Connect(OwnedSections)').length).to.equal(0);
+  });
+
+  it('does not render sections tables if there are no section ids', () => {
+    const wrapper = shallow(<TeacherSections {...defaultProps} />);
+    expect(wrapper.find('Connect(ContentContainer)').length).to.equal(1);
+    expect(wrapper.find('Connect(OwnedSections)').length).to.equal(0);
+  });
+
+  it('renders student sections area if there are student sections', () => {
+    const wrapper = shallow(
+      <TeacherSections {...defaultProps} studentSectionIds={[1]} />
+    );
     expect(wrapper.find('Connect(ContentContainer)').length).to.equal(2);
+    expect(wrapper.find('Connect(OwnedSections)').length).to.equal(1);
+    expect(
+      wrapper.find('Connect(OwnedSections)').props().isPlSections
+    ).to.equal(undefined);
+  });
+
+  it('renders pl sections area if there are pl sections', () => {
+    const wrapper = shallow(
+      <TeacherSections {...defaultProps} plSectionIds={[1]} />
+    );
+    expect(wrapper.find('Connect(ContentContainer)').length).to.equal(2);
+    expect(wrapper.find('Connect(OwnedSections)').length).to.equal(1);
+    expect(
+      wrapper.find('Connect(OwnedSections)').props().isPlSections
+    ).to.equal(true);
+  });
+
+  it('renders both sections area if there are student and pl sections', () => {
+    const wrapper = shallow(
+      <TeacherSections
+        {...defaultProps}
+        studentSectionIds={[2]}
+        plSectionIds={[1]}
+      />
+    );
+    expect(wrapper.find('Connect(ContentContainer)').length).to.equal(3);
     expect(wrapper.find('Connect(OwnedSections)').length).to.equal(2);
   });
 });
