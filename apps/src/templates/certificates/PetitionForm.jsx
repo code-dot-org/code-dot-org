95f9761fd0f6cb85e1d74a0b161fe84207da9a27
Meg Crenshaw <meg@code.org>
2022-04-15 11:48:27 -0400

    Create FieldGroup component

--- apps/src/templates/certificates/PetitionForm.jsx
+++ apps/src/templates/certificates/PetitionForm.jsx
@@ -1,109 +1,86 @@
 import React from 'react';
-import {Button, FormControl, FormGroup, Form} from 'react-bootstrap';
-import {range} from 'lodash';
+import {Button, FormControl, FormGroup, Form, HelpBlock} from 'react-bootstrap';
+import {range, assign} from 'lodash';
+import PropTypes from 'prop-types';
 
-export default function PetitionForm() {
+const FieldGroup = ({id, label, help, componentClass, children, ...props}) => {
+  const fieldStyle = assign(styles.elementText, styles.field, styles.dropdown);
   return (
-    <>
-      <Form inline id="petition-form" style={styles.form}>
-        <FormGroup controlId="petitionForm">
-          <FormControl
-            id="name"
-            placeholder="Name"
-            type="text"
-            style={{
-              ...styles.element,
-              ...styles.elementText,
-              ...styles.field,
-              ...styles.input
-            }}
-          />
-          <FormControl
-            id="email"
-            placeholder="Email"
-            type="text"
-            style={{
-              ...styles.element,
-              ...styles.elementText,
-              ...styles.field,
-              ...styles.input
-            }}
-          />
-          <FormControl
-            id="zip-or-country"
-            placeholder="ZIP code or country"
-            type="number"
-            style={{
-              ...styles.element,
-              ...styles.elementText,
-              ...styles.field,
-              ...styles.input
-            }}
-          />
-          <span style={{...styles.element, ...styles.label}}>
-            Age
-            <FormControl
-              id="age"
-              componentClass="select"
-              style={{
-                ...styles.elementText,
-                ...styles.field,
-                ...styles.dropdown
-              }}
-            >
-              {['-', ...range(1, 101)].map((age, index) => (
-                <option key={index} value={age}>
-                  {age}
-                </option>
-              ))}
-            </FormControl>
-          </span>
-          <span style={{...styles.element, ...styles.label}}>
-            I am a
-            <FormControl
-              id="profession"
-              componentClass="select"
-              style={{
-                ...styles.elementText,
-                ...styles.field,
-                ...styles.dropdown
-              }}
-            >
-              {[
-                '- Select -',
-                'Student',
-                'Parent',
-                'Educator',
-                'Software Engineer',
-                'None of the Above'
-              ].map((profession, index) => (
-                <option key={index} value={profession}>
-                  {profession}
-                </option>
-              ))}
-            </FormControl>
-          </span>
-          <Button
-            style={styles.element}
-            bsStyle="primary"
-            key="submit"
-            id="submit"
-            type="submit"
-          >
-            I agree
-          </Button>
-        </FormGroup>
-      </Form>
-    </>
+    <FormGroup style={styles.element} controlId={id}>
+      {label && <span style={styles.label}>{label}</span>}
+      <FormControl
+        componentClass={componentClass}
+        style={fieldStyle}
+        {...props}
+      >
+        {children}
+      </FormControl>
+      {help && <HelpBlock style={styles.help}>{help}</HelpBlock>}
+    </FormGroup>
   );
-}
+};
+
+FieldGroup.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  help: PropTypes.string,
+  children: PropTypes.node,
+  componentClass: PropTypes.string
+};
+
+const PetitionForm = () => (
+  <>
+    <Form inline id="petition-form" style={styles.form}>
+      <FieldGroup id="name" placeholder="Name" type="text" />
+      <FieldGroup id="email" placeholder="Email" type="text" />
+      <FieldGroup
+        id="zip-or-country"
+        placeholder="ZIP code or country"
+        type="number"
+      />
+      <FieldGroup id="zip-or-country" label="Age" componentClass="select">
+        {['-', ...range(1, 101)].map((age, index) => (
+          <option key={index} value={age}>
+            {age}
+          </option>
+        ))}
+      </FieldGroup>
+      <FieldGroup id="profession" label="I am a" componentClass="select">
+        {[
+          '- Select -',
+          'Student',
+          'Parent',
+          'Educator',
+          'School Administrator',
+          'Software Engineer',
+          'None of the Above'
+        ].map((profession, index) => (
+          <option key={index} value={profession}>
+            {profession}
+          </option>
+        ))}
+      </FieldGroup>
+      <Button
+        style={styles.element}
+        bsStyle="primary"
+        key="submit"
+        id="submit"
+        type="submit"
+      >
+        I agree
+      </Button>
+    </Form>
+  </>
+);
 
 const styles = {
   form: {
-    flex: '1 100%'
+    display: 'flex',
+    flexFlow: 'row wrap',
+    flex: '1 100%',
+    justifyContent: 'space-around'
   },
   element: {
-    justifyContent: 'space-between',
     margin: '10px',
     padding: '5px'
   },
@@ -113,7 +90,8 @@ const styles = {
   },
   label: {
     whiteSpace: 'nowrap',
-    color: 'rgb(91,103,112)'
+    color: 'rgb(91,103,112)',
+    width: '100px'
   },
   field: {
     color: 'rgb(89, 89, 89, 89)',
@@ -123,7 +101,16 @@ const styles = {
     width: '160px'
   },
   dropdown: {
-    width: '100px',
+    width: '120px',
     marginLeft: '4px'
+  },
+  help: {
+    whiteSpace: 'nowrap',
+    fontSize: '12px',
+    lineHeight: '18px',
+    color: 'rgb(91,103,112)',
+    marginLeft: '10px'
   }
 };
+
+export default PetitionForm;
