670f1450d48d9de399cc3931f301d599509848d1
Meg Crenshaw <meg@code.org>
2022-04-15 13:18:04 -0400

    Remove styles from js file

--- apps/src/templates/certificates/PetitionCallToAction.jsx
+++ apps/src/templates/certificates/PetitionCallToAction.jsx
@@ -5,7 +5,7 @@ export default function PetitionCallToAction() {
   return (
     <>
       <div id="petition-block" style={styles.block}>
-        <h1 id="petition-message" style={styles.message}>
+        <h1 id="petition-message" className="petition_message">
           Every student in every school should have the opportunity to learn
           computer science
         </h1>
@@ -26,14 +26,6 @@ const styles = {
     backgroundColor: 'white',
     padding: '10px 0px',
     flex: '1 100%'
-  },
-  message: {
-    flex: '1 100%',
-    fontFamily: '"Gotham 5r", sans-serif',
-    color: 'rgb(91,103,112)',
-    fontSize: '22px',
-    lineHeight: '28px',
-    margin: '0px'
   },
   sign: {
     flex: '1 100%',
