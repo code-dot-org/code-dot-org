83fa2420a1962202cc3cd8da5ae36a56bfd77411
Meg Crenshaw <meg@code.org>
2022-04-18 18:15:17 -0400

    Use kebab-case for class names

--- apps/src/templates/certificates/PetitionCallToAction.jsx
+++ apps/src/templates/certificates/PetitionCallToAction.jsx
@@ -4,12 +4,12 @@ import PetitionForm from './PetitionForm';
 export default function PetitionCallToAction() {
   return (
     <>
-      <div id="petition-block" className="petition_block">
-        <h1 id="petition-message" className="petition_message">
+      <div id="petition-block" className="petition-block">
+        <h1 id="petition-message" className="petition-message">
           Every student in every school should have the opportunity to learn
           computer science
         </h1>
-        <h2 id="sign-message" className="petition_sign_message">
+        <h2 id="sign-message" className="petition-sign-message">
           If you agree, sign your name and join our mailing list.
         </h2>
         <PetitionForm />
