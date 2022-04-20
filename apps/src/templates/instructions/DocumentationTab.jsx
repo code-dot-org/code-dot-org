7fa38aeb11c5b04a5d109514ff4fe95c187260fd
molly-moen <molly@code.org>
2022-04-20 14:37:47 -0700

    get documentation on tab load

--- apps/src/templates/instructions/DocumentationTab.jsx
+++ apps/src/templates/instructions/DocumentationTab.jsx
@@ -1,32 +1,47 @@
 import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import Spinner from '@cdo/apps/code-studio/pd/components/spinner';
 
 /**
  * This is a placeholder for the upcoming "Documentation" tab in Javalab.
  * See related task: https://codedotorg.atlassian.net/browse/CSA-361
  */
 const UnconnectedDocumentationTab = function({programmingEnvironment}) {
-  // const [documentation, setDocumentation] = useState(null);
+  const [documentation, setDocumentation] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [validDocumentationEnvironment, setValidDocumentationEnvironment] = useState(true);
+  const [loadError, setLoadError] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true);
     if (!programmingEnvironment) {
-      setValidDocumentationEnvironment(false);
+      setLoadError(true);
       setIsLoading(false);
       return;
     }
+    setIsLoading(true);
+    setLoadError(false);
     $.ajax({
-      url: `/programming_environments/get_summary_by_name/${programmingEnvironment}`,
+      url: `/programming_environments/${programmingEnvironment}/get_summary_by_name`,
       method: 'GET'
-    }).done((data, _, request) => {
-      // TODO: fill in
-    });
+    })
+      .done(data => {
+        setDocumentation(data);
+        setIsLoading(false);
+      })
+      .fail(() => {
+        setIsLoading(false);
+        setLoadError(true);
+        setDocumentation(null);
+      });
   }, [programmingEnvironment]);
 
-  return <p>Documentation for {programmingEnvironment}</p>;
+  return (
+    <div>
+      {isLoading && <Spinner />}
+      {loadError && <p>Could not load documentation.</p>}
+      {!isLoading && documentation && <p>Found documentation!</p>}
+    </div>
+  );
 };
 
 UnconnectedDocumentationTab.propTypes = {
