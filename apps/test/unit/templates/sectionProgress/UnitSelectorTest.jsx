02256f7c599ada9c9ae98f647edaa4bd02014d5f
Dani <dani@code.org>
2022-04-20 13:11:45 -0400

    Fix up apps tests based on changes to structure of information

--- apps/test/unit/templates/sectionProgress/UnitSelectorTest.jsx
+++ apps/test/unit/templates/sectionProgress/UnitSelectorTest.jsx
@@ -13,10 +13,9 @@ let defaultProps = {
 describe('UnitSelector', () => {
   it('loads the correct number of course versions', () => {
     const wrapper = shallow(<UnitSelector {...defaultProps} />);
-    expect(wrapper.find('optgroup').length).to.equal(4);
+    expect(wrapper.find('optgroup').length).to.equal(3);
     assert.deepEqual(wrapper.find('optgroup').map(o => o.props().label), [
-      'Course A (2017)',
-      'Course A (2018)',
+      'Course A',
       'CS Discoveries 2018',
       'Flappy'
     ]);
