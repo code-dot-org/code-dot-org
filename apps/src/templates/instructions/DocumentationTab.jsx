5dd79181296fc14705debe284e5bfc69f3f87e73
molly-moen <molly@code.org>
2022-04-20 12:39:16 -0700

    work in progress

--- apps/src/templates/instructions/DocumentationTab.jsx
+++ apps/src/templates/instructions/DocumentationTab.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -7,6 +7,25 @@ import PropTypes from 'prop-types';
  * See related task: https://codedotorg.atlassian.net/browse/CSA-361
  */
 const UnconnectedDocumentationTab = function({documentationEnvironment}) {
+  const [documentation, setDocumentation] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [validDocumentationEnvironment, setValidDocumentationEnvironment] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    if (!documentationEnvironment) {
+      setValidDocumentationEnvironment(false);
+      setIsLoading(false);
+      return;
+    }
+    $.ajax({
+      url: `/programming_environments/get_summary_by_name/${documentationEnvironment}`,
+      method: 'GET',
+    }).done((data, _, request) => {
+      // TODO: fill in
+    });
+  }, [documentationEnvironment]);
+
   return <p>Documentation for {documentationEnvironment}</p>;
 };
 
