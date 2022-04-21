c9c7d7f396d7d3926591dc67ea282f6497d4a053
Meg Crenshaw <meg@code.org>
2022-04-20 21:21:32 -0400

    Add name prop and put const outside

--- apps/src/templates/certificates/PetitionForm.jsx
+++ apps/src/templates/certificates/PetitionForm.jsx
@@ -4,50 +4,58 @@ import {Button, Form} from 'react-bootstrap';
 import FieldGroup from './FieldGroup';
 import {range} from 'lodash';
 
-const PetitionForm = () => {
-  const fields = [
-    <FieldGroup id="name" placeholder="Name" type="text" />,
-    <FieldGroup
-      id="email"
-      placeholder="Email"
-      type="text"
-      help="Only used for infrequent updates"
-    />,
-    <FieldGroup
-      id="zip-or-country"
-      placeholder="ZIP code or country"
-      type="text"
-      help="Enter country if outside the United States"
-    />,
-    <FieldGroup
-      id="age"
-      label="Age"
-      componentClass="select"
-      help={<a href="/privacy">See our privacy practices for children</a>}
-    >
-      {['-', ...range(1, 101)].map((age, index) => (
-        <option key={index} value={age}>
-          {age}
-        </option>
-      ))}
-    </FieldGroup>,
-    <FieldGroup id="profession" label="I am a" componentClass="select">
-      {[
-        '-',
-        'Student',
-        'Parent',
-        'Educator',
-        'School Administrator',
-        'Software Engineer',
-        'None of the Above'
-      ].map((profession, index) => (
-        <option key={index} value={profession}>
-          {profession}
-        </option>
-      ))}
-    </FieldGroup>
-  ];
+const fields = [
+  <FieldGroup id="name" name="name" placeholder="Name" type="text" />,
+  <FieldGroup
+    id="email"
+    name="email"
+    placeholder="Email"
+    type="text"
+    help="Only used for infrequent updates"
+  />,
+  <FieldGroup
+    id="zip-or-country"
+    name="zip-or-country"
+    placeholder="ZIP code or country"
+    type="text"
+    help="Enter country if outside the United States"
+  />,
+  <FieldGroup
+    id="age"
+    name="age"
+    label="Age"
+    componentClass="select"
+    help={<a href="/privacy">See our privacy practices for children</a>}
+  >
+    {['-', ...range(1, 101)].map((age, index) => (
+      <option key={index} value={age}>
+        {age}
+      </option>
+    ))}
+  </FieldGroup>,
+  <FieldGroup
+    id="profession"
+    name="profession"
+    label="I am a"
+    componentClass="select"
+  >
+    {[
+      '-',
+      'Student',
+      'Parent',
+      'Educator',
+      'School Administrator',
+      'Software Engineer',
+      'None of the Above'
+    ].map((profession, index) => (
+      <option key={index} value={profession}>
+        {profession}
+      </option>
+    ))}
+  </FieldGroup>
+];
 
+const PetitionForm = () => {
   const hasError = false;
 
   return (
