b629c2080456150208817a97a78d83ee38de75a1
molly-moen <molly@code.org>
2022-04-21 14:57:57 -0700

    use useFetch

--- apps/src/templates/instructions/DocumentationTab.jsx
+++ apps/src/templates/instructions/DocumentationTab.jsx
@@ -1,45 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import Spinner from '@cdo/apps/code-studio/pd/components/spinner';
+import {useFetch} from '@cdo/apps/util/useFetch';
 
 /**
  * This is a placeholder for the upcoming "Documentation" tab in Javalab.
  * See related task: https://codedotorg.atlassian.net/browse/CSA-361
  */
 const UnconnectedDocumentationTab = function({programmingEnvironment}) {
-  const [documentation, setDocumentation] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadError, setLoadError] = useState(false);
-
-  useEffect(() => {
-    if (!programmingEnvironment) {
-      setLoadError(true);
-      setIsLoading(false);
-      return;
-    }
-    setIsLoading(true);
-    setLoadError(false);
-    $.ajax({
-      url: `/programming_environments/${programmingEnvironment}/get_summary_by_name`,
-      method: 'GET'
-    })
-      .done(data => {
-        setDocumentation(data);
-        setIsLoading(false);
-      })
-      .fail(() => {
-        setIsLoading(false);
-        setLoadError(true);
-        setDocumentation(null);
-      });
-  }, [programmingEnvironment]);
+  const {loading, data, error} = useFetch(
+    `/programming_environments/${programmingEnvironment}/get_summary_by_name`,
+    null,
+    [programmingEnvironment]
+  );
 
   return (
     <div>
-      {isLoading && <Spinner />}
-      {loadError && <p>Could not load documentation.</p>}
-      {!isLoading && documentation && <p>Found documentation!</p>}
+      {loading && <Spinner />}
+      {error && <p>Could not load documentation.</p>}
+      {!loading && data && (
+        <p>{`Found documentation! Found ${data.length} categories.`}</p>
+      )}
     </div>
   );
 };
