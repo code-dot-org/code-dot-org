b426f8c197b5ba433bf9bc159088d849fe856568
James Chen <james@code.org>
2022-04-21 17:16:42 -0700

    Create tables for code review v2

---
+++ dashboard/db/migrate/20220421224748_create_code_review_notes.rb
@@ -0,0 +1,20 @@
+class CreateCodeReviewNotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :code_review_notes do |t|
+      t.integer :code_review_request_id, null: false
+      t.integer :commenter_id, null: false
+      t.boolean :is_resolved, null: false
+      t.text :comment, null: false
+
+      t.timestamps
+
+      t.index [:code_review_request_id]
+    end
+
+    reversible do |dir|
+      dir.up do
+        execute "ALTER TABLE code_review_notes CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci"
+      end
+    end
+  end
+end
