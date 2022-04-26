30c509627855329047e600654afea75fb064fd09
Mike Harvey <mike.harvey@code.org>
2022-04-26 16:36:31 -0400

    Update spriteCommands.js

--- apps/src/p5lab/spritelab/commands/spriteCommands.js
+++ apps/src/p5lab/spritelab/commands/spriteCommands.js
@@ -139,7 +139,7 @@ export const commands = {
             initialAngle:
               (i * 360) / num - 180 * ((i + 1) % 2) + spiralRandomizer,
             delay: (i * 30) / num,
-            lifetime: 91
+            lifetime: 90
           };
           break;
         }
