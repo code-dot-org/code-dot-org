be5aa8b85dc46fe7e02393f7212724652c9a12df
hacodeorg <ha@code.org>
2022-04-26 11:14:21 -0700

    Add a few tests, add comments.

--- bin/i18n/test/translation_monitor/test_translation_validator.rb
+++ bin/i18n/test/translation_monitor/test_translation_validator.rb
@@ -5,28 +5,42 @@ class TranslationValidatorTest < Minitest::Test
   include TranslationValidator
 
   def test_validate_markdown_link
+    # valid cases
     assert_nil validate_markdown_link('[text](url)')
+    assert_nil validate_markdown_link('[.](http://go.somewhere)')
+
+    # invalid cases
     refute_nil validate_markdown_link('[text] (url)')
     refute_nil validate_markdown_link("[text]\t(url)")
   end
 
   def test_validate_redacted_blocks
+    # valid cases
     assert_nil validate_redacted_blocks('[A][0]')
     assert_nil validate_redacted_blocks('[A][0] [B][1]')
+
+    # invalid cases
     refute_nil validate_redacted_blocks('[A] [0]')
     refute_nil validate_redacted_blocks("[A]\t[0]")
+    refute_nil validate_redacted_blocks("[] []")
   end
 
   def test_validate_language
+    # valid cases
     assert_nil validate_language("this is a sentence in english", 'en')
     assert_nil validate_language("esta es una oracion en español", 'es')
     assert_nil validate_language("c'est une phrase en français", 'fr')
     assert_nil validate_language("これは日本語の文章です", 'ja')
     assert_nil validate_language("đây là một câu trong tiếng việt", 'vi')
     assert_nil validate_language("هذه جملة باللغة العربية", 'ar')
-    assert_nil validate_language("1 2 3 4 5 6 7 8 9 10", 'en')
-    assert_nil validate_language('string too short', 'unknown_code')
 
+    # cases we cannot detect languages reliably
+    assert_nil validate_language("1 2 3 4 5 6 7 8 9 10 11", 'any_language_code')
+    assert_nil validate_language("string too short", 'any_language_code')
+    assert_nil validate_language("", 'any_language_code')
+
+    # invalid cases
     refute_nil validate_language("இது தமிழில் ஒரு வாக்கியம்", 'it')
+    refute_nil validate_language("ini adalah kalimat dalam bahasa indonesia", 'es')
   end
 end
