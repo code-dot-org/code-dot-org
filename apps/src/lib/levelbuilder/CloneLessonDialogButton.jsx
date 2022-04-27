39a14c714fcc0620cf1981ad3923d0d3c84c3c5e
David Bailey <davidsbailey@users.noreply.github.com>
2022-04-27 15:00:08 -0700

    improve styling

--- apps/src/lib/levelbuilder/CloneLessonDialogButton.jsx
+++ apps/src/lib/levelbuilder/CloneLessonDialogButton.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import Button, {ButtonColor} from '@cdo/apps/templates/Button';
 import CloneLessonDialog from '@cdo/apps/lib/levelbuilder/unit-editor/CloneLessonDialog';
 
 export default function CloneLessonDialogButton(props) {
@@ -12,9 +13,12 @@ export default function CloneLessonDialogButton(props) {
   const lessonId = isOpen ? props.lessonId : undefined;
   return (
     <div>
-      <button type="button" onClick={onClick}>
-        {props.buttonText}
-      </button>
+      <Button
+        color={ButtonColor.white}
+        text={props.buttonText}
+        style={styles.button}
+        onClick={onClick}
+      />
       <CloneLessonDialog
         lessonId={lessonId}
         lessonName={props.lessonName}
@@ -28,4 +32,12 @@ CloneLessonDialogButton.propTypes = {
   lessonId: PropTypes.number.isRequired,
   lessonName: PropTypes.string.isRequired,
   buttonText: PropTypes.string.isRequired
+};
+
+const styles = {
+  button: {
+    height: 23,
+    padding: 5,
+    margin: 0
+  }
 };
