c67e31b5b95195b5bdde291c985eba147b3295b1
hacodeorg <ha@code.org>
2022-04-26 22:11:13 -0700

    Add more unit tests

--- lib/test/cdo/crowdin/test_client_extensions.rb
+++ lib/test/cdo/crowdin/test_client_extensions.rb
@@ -9,24 +9,64 @@ class CrowdinClientExtensionsTest < Minitest::Test
   end
 
   def test_download_languages
-    # An array of stubbed responses, one for each call.
+    # Download 1 record per API call
+    limit = 1
+    # An array of stubbed responses, one for each API call.
     fake_responses = [
       {'data' => [{'data' => {'id' => 'language1'}}]},
       {'data' => [{'data' => {'id' => 'language2'}}]},
-      {'data' => [{'data' => {'id' => 'language3'}}]},
+      # this response should stop the downloading loop
       {'data' => []}
     ]
     @crowdin_client.stubs(:list_languages).returns(*fake_responses)
 
-    results = @crowdin_client.download_languages(1)
+    results = @crowdin_client.download_languages(limit)
+    assert_equal fake_responses.size - 1, results.size
+  end
 
-    # Expected results = [{id => ..}, {id => ...}, ...]
-    expected_results = fake_responses.map do |response|
-      response['data'].map do |language|
-        language['data']
-      end
-    end.flatten
+  def test_download_translations
+    # Download 1 record per API call
+    limit = 1
+    # An array of stubbed responses, one for each API call.
+    fake_responses = [
+      {'data' => [{'data' => {'stringId' => 1, 'translationId' => 1}}]},
+      {'data' => [{'data' => {'stringId' => 2, 'translationId' => 2}}]},
+      # this response should stop the downloading loop
+      {'data' => []}
+    ]
+    @crowdin_client.stubs(:list_language_translations).returns(*fake_responses)
 
-    assert_equal expected_results, results
+    results = @crowdin_client.download_translations(
+      'project_name',
+      'crowdin_language_id',
+      'user_name',
+      'start_date',
+      'end_date',
+      limit
+    )
+    assert_equal fake_responses.size - 1, results.size
+  end
+
+  def test_download_source_strings
+    # Download 1 record per API call
+    limit = 1
+    # An array of stubbed responses, one for each API call.
+    fake_responses = [
+      {'data' => [{'data' => {'id' => 1, 'text' => 'string1'}}]},
+      {'data' => [{'data' => {'id' => 2, 'text' => 'string2'}}]},
+      # this response should stop the downloading loop
+      {'data' => []}
+    ]
+    @crowdin_client.stubs(:list_strings).returns(*fake_responses)
+
+    results = @crowdin_client.download_source_strings(
+      'project_name',
+      'crowdin_language_id',
+      'user_name',
+      'start_date',
+      'end_date',
+      limit
+    )
+    assert_equal fake_responses.size - 1, results.size
   end
 end
