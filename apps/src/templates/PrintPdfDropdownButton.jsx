4c4bc948b5c8364aadf2ef528a30b0afa28f430b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-04-27 15:26:32 -0700

    comments & cleanup

--- apps/src/templates/PrintPdfDropdownButton.jsx
+++ apps/src/templates/PrintPdfDropdownButton.jsx
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
-//import onClickOutside from 'react-onclickoutside';
 
 import DropdownButton from '@cdo/apps/templates/DropdownButton';
 import Button from '@cdo/apps/templates/Button';
 import firehoseClient from '@cdo/apps/lib/util/firehose';
 import i18n from '@cdo/locale';
 
-class PrintPdfDropdownButton extends Component {
+/**
+ * Simple wrapper to DropdownButton for the PDF-backed "Printing Options"
+ * dropdowns we render on the Unit and Lesson overview pages.
+ *
+ * Note that although this is quite a simple component, it does need to be a
+ * Class Component rather than a Functional Component so DropdownButton's
+ * onClickOutside handler still works. Once we upgrade to React 16+, this can
+ * be simplified.
+ *
+ * See https://www.npmjs.com/package/react-onclickoutside#functional-component-with-usestate-hook
+ */
+export default class PrintPdfDropdownButton extends Component {
   static propTypes = {
     color: PropTypes.oneOf(Object.values(Button.ButtonColor)).isRequired,
     dropdownOptions: PropTypes.arrayOf(
@@ -21,6 +31,33 @@ class PrintPdfDropdownButton extends Component {
     studyGroup: PropTypes.string.isRequired
   };
 
+  /**
+   * Record on firehose that a user has clicked on a PDF link, then navigate
+   * them to that PDF.
+   */
+  recordAndNavigateToPdf(e, firehoseKey, url) {
+    // Prevent navigation to url until callback
+    e.preventDefault();
+    firehoseClient.putRecord(
+      {
+        study: 'pdf-click',
+        study_group: this.props.studyGroup,
+        event: 'open-pdf',
+        data_json: JSON.stringify({
+          name: this.props.name,
+          pdfType: firehoseKey
+        })
+      },
+      {
+        includeUserId: true,
+        callback: () => {
+          window.location.href = url;
+        }
+      }
+    );
+    return false;
+  }
+
   render() {
     return (
       <div style={{marginRight: 5}}>
@@ -30,13 +67,7 @@ class PrintPdfDropdownButton extends Component {
               href={option.url}
               key={option.key}
               onClick={e =>
-                recordAndNavigateToPdf(
-                  e,
-                  option.key,
-                  option.url,
-                  this.props.studyGroup,
-                  this.props.name
-                )
+                this.recordAndNavigateToPdf(e, option.key, option.url)
               }
             >
               {option.name}
@@ -47,29 +78,3 @@ class PrintPdfDropdownButton extends Component {
     );
   }
 }
-
-const recordAndNavigateToPdf = (e, firehoseKey, url, studyGroup, name) => {
-  // Prevent navigation to url until callback
-  e.preventDefault();
-  firehoseClient.putRecord(
-    {
-      study: 'pdf-click',
-      study_group: studyGroup,
-      event: 'open-pdf',
-      data_json: JSON.stringify({
-        name: name,
-        pdfType: firehoseKey
-      })
-    },
-    {
-      includeUserId: true,
-      callback: () => {
-        window.location.href = url;
-      }
-    }
-  );
-  return false;
-};
-
-//export default onClickOutside(PrintPdfDropdownButton);
-export default PrintPdfDropdownButton;
