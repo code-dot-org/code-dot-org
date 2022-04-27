d3aae080948b12a68de35b71d7ba3b576ebb5c9f
David Bailey <davidsbailey@users.noreply.github.com>
2022-04-27 14:26:09 -0700

    add CloneLessonDialogButton

---
+++ apps/src/lib/levelbuilder/CloneLessonDialogButton.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import CloneLessonDialog from '@cdo/apps/lib/levelbuilder/unit-editor/CloneLessonDialog';
+
+export default function CloneLessonDialogButton(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => setIsOpen(false);
+
+  const onClick = () => setIsOpen(true);
+
+  const lessonId = isOpen ? props.lessonId : undefined;
+  return (
+    <div>
+      <button type="button" onClick={onClick}>
+        Copy
+      </button>
+      <CloneLessonDialog
+        lessonId={lessonId}
+        lessonName={props.lessonName}
+        handleClose={handleClose}
+      />
+    </div>
+  );
+}
+
+CloneLessonDialogButton.propTypes = {
+  lessonId: PropTypes.number.isRequired,
+  lessonName: PropTypes.string.isRequired
+};
