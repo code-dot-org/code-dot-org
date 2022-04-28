443671db478e0f977af648405ca50f65621f4c82
Bethany Connor <bethany@code.org>
2022-04-27 17:08:20 -0700

    Remove parent banner from Sprint 2020

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -817,7 +817,6 @@ Dashboard::Application.routes.draw do
       post 'users/:user_id/postpone_census_banner', to: 'users#postpone_census_banner'
       post 'users/:user_id/dismiss_census_banner', to: 'users#dismiss_census_banner'
       post 'users/:user_id/dismiss_donor_teacher_banner', to: 'users#dismiss_donor_teacher_banner'
-      post 'users/:user_id/dismiss_parent_email_banner', to: 'users#dismiss_parent_email_banner'
 
       get 'school-districts/:state', to: 'school_districts#index', defaults: {format: 'json'}
       get 'schools/:school_district_id/:school_type', to: 'schools#index', defaults: {format: 'json'}
