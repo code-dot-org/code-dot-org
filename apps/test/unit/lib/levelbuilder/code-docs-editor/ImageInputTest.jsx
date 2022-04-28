25bbbb5895e3b99bff39a81e91d2cf729d70e5eb
Bethany Connor <bethany@code.org>
2022-04-28 14:38:58 -0700

    Use ImageInput in bubble choice sublevel field to be able to upload images and add a preview option

--- apps/test/unit/lib/levelbuilder/code-docs-editor/ImageInputTest.jsx
+++ apps/test/unit/lib/levelbuilder/code-docs-editor/ImageInputTest.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from '../../../../util/reconfiguredChai';
+import sinon from 'sinon';
 import ImageInput from '@cdo/apps/lib/levelbuilder/code-docs-editor/ImageInput';
 
 describe('ImageInput', () => {
@@ -8,30 +9,52 @@ describe('ImageInput', () => {
     const wrapper = shallow(<ImageInput updateImageUrl={() => {}} />);
     const uploadButton = wrapper.find('Button').first();
     expect(uploadButton).to.not.be.null;
-    uploadButton.simulate('click');
+    uploadButton.simulate('click', {preventDefault: () => {}});
     expect(wrapper.find('UploadImageDialog').length).to.equal(1);
   });
 
-  it('shows imageUrl if one is provided', () => {
+  it('uses initialImageUrl if one is provided', () => {
     const wrapper = shallow(
       <ImageInput
         updateImageUrl={() => {}}
-        imageUrl="code.org/images/spritelab.png"
+        initialImageUrl="code.org/images/spritelab.png"
       />
     );
-    expect(wrapper.text().includes('code.org/images/spritelab.png')).to.be.true;
+    expect(
+      wrapper
+        .find('input')
+        .first()
+        .props().value
+    ).to.equal('code.org/images/spritelab.png');
   });
 
-  it('shows confirmation dialog if remove image button is pressed', () => {
+  it('calls callback after image is updated', () => {
+    const updateImageUrlSpy = sinon.spy();
+    const wrapper = shallow(
+      <ImageInput
+        updateImageUrl={updateImageUrlSpy}
+        initialImageUrl="code.org/images/spritelab.png"
+      />
+    );
+    wrapper
+      .find('input')
+      .first()
+      .simulate('change', {target: {value: 'code.org/images/applab.png'}});
+    wrapper.update();
+    expect(updateImageUrlSpy).to.be.called.once;
+  });
+
+  it('shows preview is showPreview is true', () => {
     const wrapper = shallow(
       <ImageInput
         updateImageUrl={() => {}}
-        imageUrl="code.org/images/spritelab.png"
+        initialImageUrl="code.org/images/spritelab.png"
+        showPreview
       />
     );
-    expect(wrapper.find('Button').length).to.equal(2);
-    const removeButton = wrapper.find('Button').last();
-    removeButton.simulate('click');
-    expect(wrapper.find('StylizedBaseDialog').length).to.equal(1);
+    expect(wrapper.find('img').length).to.equal(1);
+    expect(wrapper.find('img').props().src).to.equal(
+      'code.org/images/spritelab.png'
+    );
   });
 });
