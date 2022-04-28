cbc321881b56b9faedf869c333755ae9b8ec45ee
Bethany Connor <bethany@code.org>
2022-04-28 15:59:20 -0700

    Move styles to the bottom of the page

--- apps/src/code-studio/components/AlertExclamation.jsx
+++ apps/src/code-studio/components/AlertExclamation.jsx
@@ -1,56 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import color from '@cdo/apps/util/color';
 
 /**
  * A big blue box with an exclamation mark on the left and text on the right.
  */
 export default function AlertExclamation({children}) {
-  const cyan = '#0094ca';
-  const style = {
-    backgroundColor: cyan,
-    color: 'white',
-    maxWidth: 600,
-    margin: '0 auto',
-    marginTop: 20,
-    borderRadius: 15
-  };
-
-  const circleStyle = {
-    width: 100,
-    height: 100,
-    background: 'gold',
-    borderRadius: 50,
-    MozBorderRadius: 50,
-    WebkitBorderRadius: 50,
-    margin: 20,
-    position: 'relative'
-  };
-
-  const exclamationStyle = {
-    fontSize: 80,
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)'
-  };
-
-  const bodyStyle = {
-    paddingLeft: 0,
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingRight: 20
-  };
-
   return (
-    <table style={style}>
+    <table style={styles.tableStyle}>
       <tbody>
         <tr>
           <td>
-            <div style={circleStyle}>
-              <div style={exclamationStyle}>!</div>
+            <div style={styles.circleStyle}>
+              <div style={styles.exclamationStyle}>!</div>
             </div>
           </td>
-          <td style={bodyStyle}>{children}</td>
+          <td style={styles.bodyStyle}>{children}</td>
         </tr>
       </tbody>
     </table>
@@ -59,4 +24,38 @@ export default function AlertExclamation({children}) {
 
 AlertExclamation.propTypes = {
   children: PropTypes.node.isRequired
+};
+
+const styles = {
+  tableStyle: {
+    backgroundColor: color.cyan,
+    color: 'white',
+    maxWidth: 600,
+    margin: '0 auto',
+    marginTop: 20,
+    borderRadius: 15
+  },
+  circleStyle: {
+    width: 100,
+    height: 100,
+    background: 'gold',
+    borderRadius: 50,
+    MozBorderRadius: 50,
+    WebkitBorderRadius: 50,
+    margin: 20,
+    position: 'relative'
+  },
+  exclamationStyle: {
+    fontSize: 80,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  },
+  bodyStyle: {
+    paddingLeft: 0,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 20
+  }
 };
