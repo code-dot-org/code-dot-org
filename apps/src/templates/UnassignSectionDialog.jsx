fd3dc9f34644a75d3344e2f8bb29c86d4ae17ab2
Hannah Bergam <hannahbergam@gmail.com>
2022-05-02 12:25:59 -0700

    updating firehose logging by distinguishing between close and cancel

--- apps/src/templates/UnassignSectionDialog.jsx
+++ apps/src/templates/UnassignSectionDialog.jsx
@@ -10,6 +10,7 @@ class UnassignSectionDialog extends Component {
     isOpen: PropTypes.bool.isRequired,
     sectionId: PropTypes.number.isRequired,
     onClose: PropTypes.func.isRequired,
+    cancelUnassign: PropTypes.func.isRequired,
     unassignSection: PropTypes.func.isRequired,
     courseName: PropTypes.string,
     sectionName: PropTypes.string
@@ -21,11 +22,17 @@ class UnassignSectionDialog extends Component {
   };
 
   render() {
-    const {isOpen, courseName, sectionName, onClose} = this.props;
+    const {
+      isOpen,
+      courseName,
+      sectionName,
+      onClose,
+      cancelUnassign
+    } = this.props;
     return (
       <BaseDialog
         isOpen={isOpen}
-        handleClose={onClose}
+        handleClose={cancelUnassign}
         useUpdatedStyles
         style={styles.dialog}
       >
