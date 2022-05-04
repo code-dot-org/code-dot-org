e231b610d2ecdca4ecfc373ecb1e3c559cc475c7
Meg Crenshaw <meg@code.org>
2022-05-04 18:02:09 -0400

    Modify image src

--- apps/src/templates/certificates/congratsNextLevelActivityCards.js
+++ apps/src/templates/certificates/congratsNextLevelActivityCards.js
@@ -1,11 +1,10 @@
-import {pegasus} from '@cdo/apps/lib/util/urlHelpers';
 import i18n from '@cdo/locale';
 
 const Course2 = {
   title: i18n.course2Title(),
   description: i18n.course2Desc(),
   link: '/s/course2',
-  image: pegasus('/shared/images/courses/fit-200/logo_course2.jpg'),
+  image: 'course2',
   buttonText: i18n.viewCourse()
 };
 
