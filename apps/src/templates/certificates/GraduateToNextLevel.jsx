819fdf5e1c8c499a79ce9e1683f339edff5fa8e1
Meg Crenshaw <meg@code.org>
2022-05-04 14:02:06 -0400

    Add GraduateToNextLevel component

---
+++ apps/src/templates/certificates/GraduateToNextLevel.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import i18n from '@cdo/locale';
+import VerticalImageResourceCard from '@cdo/apps/templates/VerticalImageResourceCard';
+import {nextLevelCourses} from '@cdo/apps/templates/certificates/congratsNextLevelActivityCards';
+
+const GraduateToNextLevel = ({nextCourse}) => {
+  const nextCourseInfo = nextLevelCourses.find(
+    course => course.name === nextCourse
+  );
+
+  return (
+    <>
+      <div id="next-level-block">
+        <h1 id="next-level-title">{i18n.congratsNextLevelHeading()}</h1>
+        <VerticalImageResourceCard
+          id={`course-card-${nextCourseInfo.title}`}
+          title={nextCourseInfo.title}
+          description={nextCourseInfo.description}
+          link={nextCourseInfo.link}
+          image={nextCourseInfo.image}
+          buttonText={nextCourseInfo.buttonText}
+          jumbo={true}
+        />
+      </div>
+    </>
+  );
+};
+
+GraduateToNextLevel.propTypes = {
+  nextCourse: PropTypes.string.isRequired
+};
+
+export default GraduateToNextLevel;
