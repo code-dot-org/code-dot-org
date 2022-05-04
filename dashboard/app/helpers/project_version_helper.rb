2ad01eea7d9334560d639c08a960d7db60ba9009
Bethany Connor <bethany@code.org>
2022-05-04 14:29:38 -0700

    Add comment as to where 1.year comes from

--- dashboard/app/helpers/project_version_helper.rb
+++ dashboard/app/helpers/project_version_helper.rb
@@ -1,4 +1,6 @@
 module ProjectVersionHelper
+  # We delete old versions after one year, unless they are the current version
+  # See https://s3.console.aws.amazon.com/s3/management/cdo-v3-sources/lifecycle/
   PROJECT_TTL = 1.year
 
   def version_expired?(version_date)
