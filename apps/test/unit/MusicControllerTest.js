8a588f6cb44ab506708c5175ef4f317b3640d374
Hannah Bergam <hannahbergam@gmail.com>
2022-05-05 16:30:46 -0700

    passing, still an issue with sound

--- apps/test/unit/MusicControllerTest.js
+++ apps/test/unit/MusicControllerTest.js
@@ -4,20 +4,30 @@ import sinon from 'sinon';
 import Sounds from '@cdo/apps/Sounds';
 import winMp3 from '!!file-loader!../audio/assets/win.mp3';
 
-const defaultProps = {
-  audioPlayer: new Sounds.getSingleton(),
-  assetUrl: winMp3,
-  trackDefinitions: [],
-  loopRandomWithDelay: null,
-  muteMusic: false
-};
-
 describe('MusicController', () => {
-  let musicController;
+  let musicController, sound, sounds, sourceURL;
 
-  beforeEach(() => {
-    //sinon.stub(sound, 'playAfterLoad');
-  });
+  sounds = new Sounds();
+  sourceURL = winMp3;
+  sounds.register({id: sourceURL, mp3: sourceURL});
+  sound = sounds.soundsById[sourceURL];
+
+  const defaultProps = {
+    audioPlayer: new Sounds.getSingleton(),
+    assetUrl: winMp3,
+    trackDefinitions: [track],
+    loopRandomWithDelay: null,
+    muteMusic: false
+  };
+
+  const track = {
+    name: 'track',
+    assetUrls: winMp3,
+    volume: 1,
+    sound: sinon.stub(sound, 'play'),
+    isLoaded: true,
+    group: null
+  };
 
   afterEach(() => {
     sinon.restore();
@@ -26,15 +36,17 @@ describe('MusicController', () => {
   it('mutes itself when requested', () => {
     musicController = new MusicController(defaultProps);
     musicController.setMuteMusic(true);
+
     musicController.play();
-    expect(musicController.muteMusic).to.be.true;
-    //expect(sound.playAfterLoad).to.not.have.been.called;
+    expect(musicController.muteMusic_).to.be.true;
+    expect(sound.play).to.not.have.been.called;
   });
 
   it('updates status and plays music when unmuted', () => {
     musicController = new MusicController(defaultProps, {muteMusic: true});
     musicController.setMuteMusic(false);
-    expect(musicController.muteMusic).to.be.false;
-    //expect(sound.playAfterLoad).to.have.been.called;
+
+    expect(musicController.muteMusic_).to.be.false;
+    //expect(sound.play).to.have.been.called;
   });
 });
