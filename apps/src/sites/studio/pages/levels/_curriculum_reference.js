2848b3cad7794b4799a34adb10a0e34914059191
Tim Plummer <tim@code.org>
2022-05-05 11:59:44 -0700

    render react element for ref guides

--- apps/src/sites/studio/pages/levels/_curriculum_reference.js
+++ apps/src/sites/studio/pages/levels/_curriculum_reference.js
@@ -1,12 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import {registerGetResult} from '@cdo/apps/code-studio/levels/codeStudioLevels';
 import {onContinue} from '@cdo/apps/code-studio/levels/postOnContinue';
+import getScriptData from '@cdo/apps/util/getScriptData';
+import ReferenceGuide from '@cdo/apps/templates/referenceGuides/ReferenceGuide';
 
 $(document).ready(() => {
   registerGetResult();
 
   // handle click on continue (results in navigating to next puzzle)
   $('.submitButton').click(onContinue);
+
+  const refGuideElement = document.getElementById('reference-guide');
+
+  if (refGuideElement) {
+    const referenceGuide = getScriptData('referenceGuide');
+    ReactDOM.render(
+      <ReferenceGuide referenceGuide={referenceGuide} />,
+      refGuideElement
+    );
+  }
 });
 
 /**
