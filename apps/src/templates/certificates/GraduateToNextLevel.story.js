46b6964edb8e1d9015f42fedbf01d47d4a419455
Meg Crenshaw <meg@code.org>
2022-05-05 15:30:46 -0400

    Pass down title and desc as props

--- apps/src/templates/certificates/GraduateToNextLevel.story.js
+++ apps/src/templates/certificates/GraduateToNextLevel.story.js
@@ -8,7 +8,13 @@ export default storybook => {
     .addStoryTable([
       {
         name: 'Course 2',
-        story: () => <GraduateToNextLevel nextCourse="course2" />
+        story: () => (
+          <GraduateToNextLevel
+            scriptName="course2"
+            courseTitle="Course 2"
+            courseDesc="For students with basic reading skills, this course builds on Course 1. Students will create programs to solve problems and develop interactive games or stories they can share. Recommended for grades 2-5."
+          />
+        )
       }
     ]);
 };
--- apps/src/templates/certificates/congratsNextLevelActivityCards.js
+++ apps/src/templates/certificates/congratsNextLevelActivityCards.js
@@ -1,8 +1,7 @@
 import i18n from '@cdo/locale';
 
 const Course2 = {
-  title: i18n.course2Title(),
-  description: i18n.course2Desc(),
+  scriptName: 'course2',
   link: '/s/course2',
   image: 'course2',
   buttonText: i18n.viewCourse()
