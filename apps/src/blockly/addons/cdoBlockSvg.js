854abb4c0564fd2b0756efb8a76118457d6961eb
Mike Harvey <mike.harvey@code.org>
2022-05-09 15:47:06 -0400

    remove functions from extended classes
    these functions are now defined (or overridden) in ../googleBlocklyWrapper.js

--- apps/src/blockly/addons/cdoBlockSvg.js
+++ apps/src/blockly/addons/cdoBlockSvg.js
@@ -1,42 +1,7 @@
 import GoogleBlockly from 'blockly/core';
-import BlockSvgUnused from './blockSvgUnused';
 
 export default class BlockSvg extends GoogleBlockly.BlockSvg {
   constructor(workspace, prototypeName, opt_id) {
     super(workspace, prototypeName, ++Blockly.uidCounter_); // Use counter instead of randomly generated IDs
-  }
-
-  addUnusedBlockFrame(helpClickFunc) {
-    if (!this.unusedSvg_) {
-      this.unusedSvg_ = new BlockSvgUnused(this, helpClickFunc);
-    }
-    this.unusedSvg_.render(this.svgGroup_);
-  }
-
-  isUnused() {
-    const isTopBlock = this.previousConnection === null;
-    const hasParentBlock = !!this.parentBlock_;
-    return !(isTopBlock || hasParentBlock);
-  }
-
-  dispose() {
-    super.dispose();
-    this.removeUnusedBlockFrame();
-  }
-
-  render(opt_bubble) {
-    super.render(opt_bubble);
-    this.removeUnusedBlockFrame();
-  }
-
-  removeUnusedBlockFrame() {
-    if (this.unusedSvg_) {
-      this.unusedSvg_.dispose();
-      this.unusedSvg_ = null;
-    }
-  }
-
-  getHexColour() {
-    return super.getColour();
   }
 }
