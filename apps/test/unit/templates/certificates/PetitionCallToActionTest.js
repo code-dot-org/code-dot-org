da35f0e8eedd507f17964a233c97aaa99fc4df21
Meg Crenshaw <meg@code.org>
2022-05-09 11:58:30 -0400

    Revise unit tests

--- apps/test/unit/templates/certificates/PetitionCallToActionTest.js
+++ apps/test/unit/templates/certificates/PetitionCallToActionTest.js
@@ -1,10 +1,7 @@
 import PetitionCallToAction from '@cdo/apps/templates/certificates/petition/PetitionCallToAction';
-import PetitionForm from '@cdo/apps/templates/certificates/petition/PetitionForm';
 import React from 'react';
 import {isolateComponent} from 'isolate-react';
 import {expect} from '../../../util/reconfiguredChai';
-import sinon from 'sinon';
-import $ from 'jquery';
 
 const isolateCallToAction = props =>
   isolateComponent(
@@ -13,34 +10,14 @@ const isolateCallToAction = props =>
 
 describe('PetitionCallToAction', () => {
   it('has a petition message', () => {
-    expect(
-      isolateCallToAction()
-        .findOne('#petition-message')
-        .content().length
-    ).to.be.greaterThan(0);
+    expect(isolateCallToAction().findAll('#petition-message')).to.have.length(
+      1
+    );
   });
   it('has a message to sign the petition', () => {
-    expect(
-      isolateCallToAction()
-        .findOne('#sign-message')
-        .content().length
-    ).to.be.greaterThan(0);
+    expect(isolateCallToAction().findAll('#sign-message')).to.have.length(1);
   });
-  it('has a form with submit button', () => {
-    const callToActionWithForm = isolateCallToAction();
-    callToActionWithForm.inline(PetitionForm);
-    const form = callToActionWithForm.findOne('form');
-    expect(form.findOne('Button').content().length).to.be.greaterThan(0);
-  });
-  it('does not send request if there are invalid fields', () => {
-    const requestSpy = sinon.spy($, 'ajax');
-
-    const callToActionWithForm = isolateCallToAction();
-    callToActionWithForm.inline(PetitionForm);
-    const form = callToActionWithForm.findOne('form');
-    form.props.onSubmit({preventDefault: sinon.stub()});
-
-    expect(requestSpy).to.have.been.notCalled;
-    $.ajax.restore();
+  it('has a petition form', () => {
+    expect(isolateCallToAction().findAll('PetitionForm')).to.have.length(1);
   });
 });
