397faf2e180c2ab78b21b01bebcc2e59cf11caf4
maureensturgeon <maureen@code.org>
2022-05-10 16:31:31 -0700

    Revert "Code review v2 - Comment textbox"

--- apps/src/templates/instructions/codeReviewV2/CodeReviewDataApi.js
+++ apps/src/templates/instructions/codeReviewV2/CodeReviewDataApi.js
@@ -1,28 +1,8 @@
-import {findProfanity} from '@cdo/apps/utils';
-import javalabMsg from '@cdo/javalab/locale';
-
-export const timelineElementType = {
-  review: 'review',
-  commit: 'commit'
-};
-
 export default class CodeReviewDataApi {
-  constructor(channelId, levelId, scriptId, locale) {
+  constructor(channelId, levelId, scriptId) {
     this.channelId = channelId;
     this.levelId = levelId;
     this.scriptId = scriptId;
-    this.locale = locale;
-  }
-
-  // Temp method only used to set this.token
-  getCodeReviewCommentsForProject(onDone) {
-    return $.ajax({
-      url: `/code_review_comments/project_comments`,
-      method: 'GET',
-      data: {channel_id: this.channelId}
-    }).done((data, _, request) => {
-      this.token = request.getResponseHeader('csrf-token');
-    });
   }
 
   getReviewablePeers() {
@@ -50,42 +30,6 @@ export default class CodeReviewDataApi {
     ];
   }
 
-  getInitialTimelineData = async () => {
-    const [codeReviews, commits] = await Promise.all([
-      this.getCodeReviews(),
-      this.getCommits()
-    ]);
-
-    // Separates the closed reviews from the open review (if present)
-    // and labels the data as timelineElementType.review
-    let labeledClosedReviewData = [];
-    let openReview = null;
-    codeReviews.forEach(review => {
-      review.timelineElementType = timelineElementType.review;
-      if (review.isClosed) {
-        labeledClosedReviewData.push(review);
-      } else {
-        openReview = review;
-      }
-    });
-
-    // Labels the commit data as timelineElementType.commit
-    const labeledCommitData = commits.map(commit => {
-      commit.timelineElementType = timelineElementType.commit;
-      return commit;
-    });
-
-    // Combines the closed reviews with the commits and orders the elements by
-    // created at asc, the order that they will be rendered on the timeline.
-    const timelineData = labeledClosedReviewData.concat(labeledCommitData);
-    timelineData.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-
-    return {
-      timelineData, // Sorted closed reviews and commits
-      openReview // Single open review if present
-    };
-  };
-
   getCodeReviews() {
     // Enable when the API is ready
     // return $.ajax({
@@ -97,8 +41,6 @@ export default class CodeReviewDataApi {
     //     script_id: this.scriptId
     //   }
     // });
-
-    this.getCodeReviewCommentsForProject();
 
     // For now returning stub data
     return [
@@ -192,49 +134,5 @@ export default class CodeReviewDataApi {
         isVersionExpired: false
       }
     ];
-  }
-
-  submitNewCodeReviewComment(commentText) {
-    return new Promise((resolve, reject) => {
-      findProfanity(commentText, this.locale, this.token)
-        .done(profaneWords => {
-          if (profaneWords?.length > 0) {
-            reject({
-              profanityFoundError: javalabMsg.commentProfanityFound({
-                wordCount: profaneWords.length,
-                words: profaneWords.join(', ')
-              })
-            });
-          } else {
-            // $.ajax({
-            //   url: `/code_review_notes`,
-            //   type: 'POST',
-            //   headers: {'X-CSRF-Token': this.token},
-            //   data: {
-            //     channel_id: this.channelId,
-            //     script_id: this.scriptId,
-            //     level_id: this.levelId,
-            //     comment: commentText
-            //   }
-            // })
-            //   .done(newComment => resolve(newComment))
-            //   .fail(result => reject(result));
-          }
-          const fakeNewComment = {
-            id: 789,
-            commentText: commentText,
-            name: 'Steve',
-            timestampString: '2022-03-31T04:58:42.000Z',
-            isResolved: false
-          };
-          resolve(fakeNewComment);
-        })
-        .fail(error => reject(error));
-    });
-  }
-
-  closeReview(review) {
-    // TODO: call API for close review
-    return {...review, isClosed: true};
   }
 }
