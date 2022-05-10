3a256e44801d94107c3d712679ae85f74d9fae39
Mike Harvey <mike.harvey@code.org>
2022-05-10 16:41:51 -0400

    move addUnusedBlocksHelpListener to wrapper

--- apps/src/blockly/addons/cdoWorkspaceSvg.js
+++ apps/src/blockly/addons/cdoWorkspaceSvg.js
@@ -1,20 +1,6 @@
 import GoogleBlockly from 'blockly/core';
 
 export default class WorkspaceSvg extends GoogleBlockly.WorkspaceSvg {
-  addUnusedBlocksHelpListener(helpClickFunc) {
-    Blockly.bindEvent_(
-      Blockly.mainBlockSpace.getCanvas(),
-      Blockly.BlockSpace.EVENTS.RUN_BUTTON_CLICKED,
-      Blockly.mainBlockSpace,
-      function() {
-        this.getTopBlocks().forEach(block => {
-          if (block.disabled) {
-            block.addUnusedBlockFrame(helpClickFunc);
-          }
-        });
-      }
-    );
-  }
   getAllUsedBlocks() {
     return super.getAllBlocks().filter(block => !block.disabled);
   }
