585871dd84b32b1f70fee078df01fd5623a446fb
Mike Harvey <mike.harvey@code.org>
2022-05-10 16:46:36 -0400

    move getAllUsedBlocks to wrapper

--- apps/src/blockly/addons/cdoWorkspaceSvg.js
+++
@@ -1,7 +0,0 @@
-import GoogleBlockly from 'blockly/core';
-
-export default class WorkspaceSvg extends GoogleBlockly.WorkspaceSvg {
-  getAllUsedBlocks() {
-    return super.getAllBlocks().filter(block => !block.disabled);
-  }
-}
