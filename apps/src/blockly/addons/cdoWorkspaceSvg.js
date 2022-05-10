5b8f3b12c52dcc5b5c8886ebf3f30ee8c31c2958
Mike Harvey <mike.harvey@code.org>
2022-05-10 15:30:44 -0400

    move clear to wrapper

--- apps/src/blockly/addons/cdoWorkspaceSvg.js
+++ apps/src/blockly/addons/cdoWorkspaceSvg.js
@@ -1,16 +1,6 @@
 import GoogleBlockly from 'blockly/core';
 
 export default class WorkspaceSvg extends GoogleBlockly.WorkspaceSvg {
-  clear() {
-    super.clear();
-
-    // After clearing the workspace, we need to reinitialize global variables
-    // if there are any.
-    if (this.globalVariables) {
-      this.getVariableMap().addVariables(this.globalVariables);
-    }
-  }
-
   addUnusedBlocksHelpListener(helpClickFunc) {
     Blockly.bindEvent_(
       Blockly.mainBlockSpace.getCanvas(),
