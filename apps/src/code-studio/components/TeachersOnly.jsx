11deab94f1fff5b61c525e28525511b9d92816ea
Dani <dani@code.org>
2022-05-11 09:59:40 -0400

    WIP hiding teacher panel for participants

--- apps/src/code-studio/components/TeachersOnly.jsx
+++ apps/src/code-studio/components/TeachersOnly.jsx
@@ -1,9 +1,10 @@
 import {connect} from 'react-redux';
+import {ViewType} from '@cdo/apps/code-studio/viewAsRedux';
 
 const TeachersOnly = ({isTeacher, children}) => {
   return isTeacher ? children : null;
 };
 
 export default connect(state => ({
-  isTeacher: state.currentUser.userType === 'teacher'
+  isInstructor: state.viewAs === ViewType.Instructor
 }))(TeachersOnly);
