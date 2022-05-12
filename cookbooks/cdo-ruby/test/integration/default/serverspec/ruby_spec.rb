bb31a5974f1f5539089a2da985d49ca0851af751
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-05-12 14:30:34 -0700

    update to Ruby 2.7

--- cookbooks/cdo-ruby/test/integration/default/serverspec/ruby_spec.rb
+++ cookbooks/cdo-ruby/test/integration/default/serverspec/ruby_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../../kitchen/data/helper_spec'
 
-file_exist '/usr/bin/ruby2.6'
-cmd 'ruby -v', 'ruby 2.6'
+file_exist '/usr/bin/ruby2.7'
+cmd 'ruby -v', 'ruby 2.7'
 cmd 'gem -v', '2.7.4'
 cmd 'bundler -v', '1.16.1'
