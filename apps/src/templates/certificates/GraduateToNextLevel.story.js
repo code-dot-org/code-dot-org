2d9f4f12d27e6df2cda0bcb04f4027e57d14c15b
Meg Crenshaw <meg@code.org>
2022-05-16 10:45:42 -0400

    Add to storybook for image checks

--- apps/src/templates/certificates/GraduateToNextLevel.story.js
+++ apps/src/templates/certificates/GraduateToNextLevel.story.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import GraduateToNextLevel from '@cdo/apps/templates/certificates/GraduateToNextLevel';
+import i18n from '@cdo/locale';
 
 export default storybook => {
   return storybook
@@ -11,8 +12,88 @@ export default storybook => {
         story: () => (
           <GraduateToNextLevel
             scriptName="course2"
-            courseTitle="Course 2"
-            courseDesc="For students with basic reading skills, this course builds on Course 1. Students will create programs to solve problems and develop interactive games or stories they can share. Recommended for grades 2-5."
+            courseTitle={i18n.course2_name()}
+            courseDesc={i18n.course2_shortdescription_congrats()}
+          />
+        )
+      },
+      {
+        name: 'Course 3',
+        story: () => (
+          <GraduateToNextLevel
+            scriptName="course3"
+            courseTitle={i18n.course3_name()}
+            courseDesc={i18n.course3_shortdescription_congrats()}
+          />
+        )
+      },
+      {
+        name: 'Course 4',
+        story: () => (
+          <GraduateToNextLevel
+            scriptName="course4"
+            courseTitle={i18n.course4_name()}
+            courseDesc={i18n.course4_shortdescription_congrats()}
+          />
+        )
+      },
+      {
+        name: 'Course B',
+        story: () => (
+          <GraduateToNextLevel
+            scriptName="courseb"
+            courseTitle={i18n.courseb_name()}
+            courseDesc={i18n.courseb_shortdescription_congrats()}
+          />
+        )
+      },
+      {
+        name: 'Course C',
+        story: () => (
+          <GraduateToNextLevel
+            scriptName="coursec"
+            courseTitle={i18n.coursec_name()}
+            courseDesc={i18n.coursec_shortdescription_congrats()}
+          />
+        )
+      },
+      {
+        name: 'Course D',
+        story: () => (
+          <GraduateToNextLevel
+            scriptName="coursed"
+            courseTitle={i18n.coursed_name()}
+            courseDesc={i18n.coursed_shortdescription_congrats()}
+          />
+        )
+      },
+      {
+        name: 'Course E',
+        story: () => (
+          <GraduateToNextLevel
+            scriptName="coursee"
+            courseTitle={i18n.coursee_name()}
+            courseDesc={i18n.coursee_shortdescription_congrats()}
+          />
+        )
+      },
+      {
+        name: 'Course F',
+        story: () => (
+          <GraduateToNextLevel
+            scriptName="coursef"
+            courseTitle={i18n.coursef_name()}
+            courseDesc={i18n.coursef_shortdescription_congrats()}
+          />
+        )
+      },
+      {
+        name: 'App Lab Intro',
+        story: () => (
+          <GraduateToNextLevel
+            scriptName="applab-intro"
+            courseTitle={i18n.applab_tile_title()}
+            courseDesc={i18n.applab_tile_subtitle()}
           />
         )
       }
