dae658a7c252cc6e83d82709a6364376b8ad7b9f
David Bailey <davidsbailey@users.noreply.github.com>
2022-05-17 18:40:59 -0700

    use localized_name_for_lesson_show directly in lesson.localized_name

--- dashboard/app/models/lesson.rb
+++ dashboard/app/models/lesson.rb
@@ -216,12 +216,8 @@ class Lesson < ApplicationRecord
   def localized_name
     # The behavior to show the script title instead of the lesson name in
     # single-lesson scripts is deprecated.
-    #
-    # TODO(dave): once all scripts with exactly one lesson are migrated and no longer
-    # using legacy lesson plans, remove this condition and consolidate with
-    # localized_name_for_lesson_show.
-    if script.lessons.many? || (script.is_migrated && !script.use_legacy_lesson_plans)
-      get_localized_property(:name) || ''
+    if script.lessons.many?
+      localized_name_for_lesson_show
     else
       I18n.t "data.script.name.#{script.name}.title"
     end
