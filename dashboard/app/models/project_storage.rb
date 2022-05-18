a727912ce5caccc4beb26430318bf95d92ef81a1
James Chen <james@code.org>
2022-05-17 20:35:19 -0700

    Move comment in ProjectStorage model so it won't get overwritten by the schema annotation

--- dashboard/app/models/project_storage.rb
+++ dashboard/app/models/project_storage.rb
@@ -10,13 +10,13 @@
 #  user_id                         (user_id) UNIQUE
 #  user_storage_ids_user_id_index  (user_id)
 #
-# Conceptually, an instance of this class represents blob storage for all of
-# the projects belonging to a single user. The user may have an account (user_id
-# points to the user) or may be unsigned-in (user_id is nil). Under the covers,
-# metadata for this storage is stored in the 'user_project_storage_ids' table
-# and the blobs are stored in several S3 buckets depending on the blob type
-# (e.g. cdo-v3-sources/sources/<storage id>, cdo-v3-files/files/<storage id>, etc.).
 class ProjectStorage < ApplicationRecord
+  # Conceptually, an instance of this class represents blob storage for all of
+  # the projects belonging to a single user. The user may have an account (user_id
+  # points to the user) or may be unsigned-in (user_id is nil). Under the covers,
+  # metadata for this storage is stored in the 'user_project_storage_ids' table
+  # and the blobs are stored in several S3 buckets depending on the blob type
+  # (e.g. cdo-v3-sources/sources/<storage id>, cdo-v3-files/files/<storage id>, etc.).
   self.table_name = 'user_project_storage_ids'
 
   belongs_to :user
