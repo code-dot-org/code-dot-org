4a9ae4f09f4a9d9367fbb41fc1508e7cf1735c27
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-05-23 18:48:41 -0700

    Load Rails 6.0 Defaults
    The most significant of which is enabling Zeitwerk

--- dashboard/config/initializers/inflections.rb
+++ dashboard/config/initializers/inflections.rb
@@ -14,3 +14,8 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym "CDO" # "code dot org"
+  inflect.acronym "DSL" # "Domain-specific language"
+end
