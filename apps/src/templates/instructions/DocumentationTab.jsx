8106c2ba19dbea5a5348e7b17b1a7541508a6b0d
molly-moen <molly@code.org>
2022-05-25 15:58:51 -0700

    show a class

--- apps/src/templates/instructions/DocumentationTab.jsx
+++ apps/src/templates/instructions/DocumentationTab.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import Spinner from '@cdo/apps/code-studio/pd/components/spinner';
 import {useFetch} from '@cdo/apps/util/useFetch';
+import ProgrammingClassOverview from '../codeDocs/ProgrammingClassOverview';
 
-/**
- * This is a placeholder for the upcoming "Documentation" tab in Javalab.
- * See related task: https://codedotorg.atlassian.net/browse/CSA-361
- */
+const DEFAULT_CLASS = 'General - Basics';
+
 const UnconnectedDocumentationTab = function({programmingEnvironment}) {
   const {loading, data, error} = useFetch(
     `/programming_environments/${programmingEnvironment}/get_summary_by_name`,
@@ -15,12 +14,38 @@ const UnconnectedDocumentationTab = function({programmingEnvironment}) {
     [programmingEnvironment]
   );
 
+  const [selectedClass, setSelectedClass] = useState(null);
+
+  const classMap = {};
+  let firstKey = null;
+  if (data) {
+    for (const category of data) {
+      const categoryName = category.name;
+      for (const classDoc of category.docs) {
+        const key = `${categoryName} - ${classDoc.name}`;
+        if (!firstKey) {
+          firstKey = key;
+        }
+        classMap[key] = classDoc;
+      }
+    }
+    console.log(classMap);
+
+    if (!selectedClass) {
+      if (classMap[DEFAULT_CLASS] && !selectedClass) {
+        setSelectedClass(DEFAULT_CLASS);
+      } else {
+        setSelectedClass(firstKey);
+      }
+    }
+  }
+
   return (
     <div>
       {loading && <Spinner />}
       {error && <p>Could not load documentation.</p>}
-      {!loading && data && (
-        <p>{`Found documentation! Found ${data.length} categories.`}</p>
+      {!loading && data && selectedClass && (
+        <ProgrammingClassOverview programmingClass={classMap[selectedClass]} />
       )}
     </div>
   );
