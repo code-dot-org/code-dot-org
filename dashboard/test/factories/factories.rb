ae4485ce89b3aef395993090ef90ce8b08d6cc94
Bethany Connor <bethany@code.org>
2022-05-26 15:11:48 -0700

    Remove extra factory

--- dashboard/test/factories/factories.rb
+++ dashboard/test/factories/factories.rb
@@ -1601,14 +1601,6 @@ FactoryGirl.define do
     comment 'a comment about your project'
   end
 
-  factory :code_review_note do
-    association :commenter, factory: :student
-    association :code_review_request, factor: code_review
-
-    comment 'a comment about your project'
-    is_resolved false
-  end
-
   factory :code_review_group do
     sequence(:name) {|n| "group_name_#{n}"}
     association :section
