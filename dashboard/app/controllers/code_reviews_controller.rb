ea0feba53f1189ee47ac5dbd5f1bd646299f62f2
James Chen <james@code.org>
2022-05-26 17:15:17 -0700

    Use projectLevelId for retrieving peer reviews

--- dashboard/app/controllers/code_reviews_controller.rb
+++ dashboard/app/controllers/code_reviews_controller.rb
@@ -22,7 +22,7 @@ class CodeReviewsController < ApplicationController
 
   # POST /code_reviews
   def create
-    params.require([:channelId, :version, :scriptId, :levelId])
+    params.require([:channelId, :version, :scriptId, :levelId, :projectLevelId])
 
     project = Project.find_by_channel_id(params[:channelId])
     # TODO: Should we check that this is a valid version for this project?
@@ -33,7 +33,7 @@ class CodeReviewsController < ApplicationController
       project_id: project.id,
       script_id: params[:scriptId],
       level_id: params[:levelId],
-      project_level_id: params[:levelId],  # TODO: send projectLevelId from the client
+      project_level_id: params[:projectLevelId],
       project_version: params[:version],
       storage_id: project.storage_id
     )
@@ -66,12 +66,12 @@ class CodeReviewsController < ApplicationController
   # Returns the list of open code reviews for the given script and level
   # from peers in the user's code review groups.
   def peers_with_open_reviews
-    params.require([:scriptId, :levelId])
+    params.require([:scriptId, :projectLevelId])
 
     code_reviews = CodeReview.open_reviews.where(
       user_id: peer_user_ids(current_user),
-      level_id: params[:levelId],
-      script_id: params[:scriptId]
+      script_id: params[:scriptId],
+      project_level_id: params[:projectLevelId]
     ).includes(:owner)
 
     return render json: code_reviews.map(&:summarize_owner_info)
