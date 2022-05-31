1523227a89731c6d88d2f45bdc677be63808caf3
Meg Crenshaw <meg@code.org>
2022-05-31 10:07:11 -0500

    Update schema file after migration

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_181706) do
+ActiveRecord::Schema.define(version: 2022_05_25_230111) do
 
   create_table "activities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
