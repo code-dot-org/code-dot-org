d00e67b680e7b3eb03eb625009a122e755e59f21
Bethany Connor <bethany@code.org>
2022-06-01 14:09:29 -0700

    Prettify hosted.js

--- pegasus/sites.v3/codeprojects.org/public/scripts/hosted.js
+++ pegasus/sites.v3/codeprojects.org/public/scripts/hosted.js
@@ -1,37 +1,38 @@
 var footerContents;
 var ready;
-var onReadyStateChange = function (evt) {
+var onReadyStateChange = function(evt) {
   var readyState = evt.target.readyState;
-  if (!ready && readyState === 'interactive' || readyState === 'complete') {
+  if ((!ready && readyState === "interactive") || readyState === "complete") {
     ready = true;
     if (footerContents) {
       applyFooter();
     }
   }
 };
-document.addEventListener('readystatechange', onReadyStateChange, false);
+document.addEventListener("readystatechange", onReadyStateChange, false);
 
 var xhr = new XMLHttpRequest();
-xhr.open('GET', '/hosted/footer');
-xhr.onload = function () {
-  if (xhr.status === 200 ) {
+xhr.open("GET", "/hosted/footer");
+xhr.onload = function() {
+  if (xhr.status === 200) {
     footerContents = xhr.responseXML.body;
     if (footerContents && ready) {
       applyFooter();
     }
   }
 };
-xhr.responseType = 'document';
+xhr.responseType = "document";
 xhr.send();
 
-var applyFooter = function () {
+var applyFooter = function() {
   while (footerContents.childNodes.length > 0) {
     document.body.appendChild(footerContents.childNodes[0]);
   }
   if (window.encrypted_channel_id) {
-    var link = document.getElementById('pagefooter_view_code');
-    link.onclick = function () {
-      window.location.href = link.href + '/' + window.encrypted_channel_id + '/view';
+    var link = document.getElementById("pagefooter_view_code");
+    link.onclick = function() {
+      window.location.href =
+        link.href + "/" + window.encrypted_channel_id + "/view";
       return false;
     };
   }
