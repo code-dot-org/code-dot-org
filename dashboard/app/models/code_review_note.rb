5fa09e597e6af880e6173ba4c2868bb3e4b96489
Maureen <maureen.sturgeon@gmail.com>
2022-06-01 12:33:15 -0700

    display commenter name, refactors

--- dashboard/app/models/code_review_note.rb
+++ dashboard/app/models/code_review_note.rb
@@ -29,9 +29,11 @@ class CodeReviewNote < ApplicationRecord
     {
       id: id,
       commenterName: commenter&.name,
+      commenterId: commenter&.id,
       comment: comment,
       isResolved: is_resolved,
-      createdAt: created_at
+      createdAt: created_at,
+      isFromTeacher: code_review.owner.teachers.include?(commenter), # TODO: memoize teacher list on user?
     }
   end
 end
