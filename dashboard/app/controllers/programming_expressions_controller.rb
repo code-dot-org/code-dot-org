b6cbe861cbda32d23ce39640319df2fd79c34024
Tim Plummer <tim@code.org>
2022-06-02 12:47:35 -0700

    remove some extra includes

--- dashboard/app/controllers/programming_expressions_controller.rb
+++ dashboard/app/controllers/programming_expressions_controller.rb
@@ -1,7 +1,5 @@
 class ProgrammingExpressionsController < ApplicationController
   include Rails.application.routes.url_helpers
-  include ProxyHelper
-  EXPIRY_TIME = 30.minutes
 
   before_action :require_levelbuilder_mode_or_test_env, except: [:search, :show, :show_by_keys, :docs_show]
   before_action :set_expression_by_keys, only: [:show_by_keys, :docs_show]
