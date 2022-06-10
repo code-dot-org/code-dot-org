f9f112007841aed81127d1ce0cfd78a97e3ff2e8
David Bailey <davidsbailey@users.noreply.github.com>
2022-06-10 16:25:02 -0700

    add tests for unpersonalized csf certs

--- dashboard/test/controllers/certificates_controller_test.rb
+++ dashboard/test/controllers/certificates_controller_test.rb
@@ -29,6 +29,24 @@ class CertificatesControllerTest < ActionController::TestCase
     assert_equal '//test.code.org/images/hour_of_code_certificate.jpg', response_data['imageUrl']
   end
 
+  test 'shows static image for unpersonalized hoc course' do
+    data = {course: 'mc'}
+    encoded_params = Base64.urlsafe_encode64(data.to_json)
+    get :show, params: {encoded_params: encoded_params}
+    assert_response :success
+    response_data = JSON.parse(css_select('script[data-certificate]').first.attribute('data-certificate').to_s)
+    assert_equal '//test.code.org/images/MC_Hour_Of_Code_Certificate.png', response_data['imageUrl']
+  end
+
+  test 'shows custom image for unpersonalized csf course' do
+    data = {course: 'course1'}
+    encoded_params = Base64.urlsafe_encode64(data.to_json)
+    get :show, params: {encoded_params: encoded_params}
+    assert_response :success
+    response_data = JSON.parse(css_select('script[data-certificate]').first.attribute('data-certificate').to_s)
+    assert_match %r{^/certificate_images/}, response_data['imageUrl']
+  end
+
   test 'returns bad request given invalid base64' do
     get :show, params: {encoded_params: 'bogus'}
     assert_response :bad_request
