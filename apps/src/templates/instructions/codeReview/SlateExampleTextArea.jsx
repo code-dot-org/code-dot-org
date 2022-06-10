5e2b26567820ab052abbac395ba131b1a367e919
Maureen <maureen.sturgeon@gmail.com>
2022-06-09 17:00:16 -0700

    more work on slate example

--- apps/src/templates/instructions/codeReview/SlateExampleTextArea.jsx
+++ apps/src/templates/instructions/codeReview/SlateExampleTextArea.jsx
@@ -36,34 +36,11 @@ const SlateExampleTextArea = () => {
   return (
     <div style={{border: '1px solid #59cad3'}}>
       <Slate editor={editor} value={initialValue} onChange={onChange}>
-        {/* <Toolbar>
-        <MarkButton format="bold" icon="format_bold" />
-        <MarkButton format="italic" icon="format_italic" />
-        <MarkButton format="underline" icon="format_underlined" />
-        <MarkButton format="code" icon="code" />
-        <BlockButton format="heading-one" icon="looks_one" />
-        <BlockButton format="heading-two" icon="looks_two" />
-        <BlockButton format="block-quote" icon="format_quote" />
-        <BlockButton format="numbered-list" icon="format_list_numbered" />
-        <BlockButton format="bulleted-list" icon="format_list_bulleted" />
-        <BlockButton format="left" icon="format_align_left" />
-        <BlockButton format="center" icon="format_align_center" />
-        <BlockButton format="right" icon="format_align_right" />
-        <BlockButton format="justify" icon="format_align_justify" />
-      </Toolbar> */}
         <div style={{borderBottom: '1px solid #59cad3'}}>
           <button
             onMouseDown={event => {
               event.preventDefault();
-              toggleMark(editor, 'bold');
-            }}
-          >
-            bold
-          </button>
-          <button
-            onMouseDown={event => {
-              event.preventDefault();
-              toggleMark(editor, 'code');
+              toggleBlock(editor, 'code_block');
             }}
           >
             code
@@ -91,36 +68,15 @@ const SlateExampleTextArea = () => {
 };
 
 const toggleBlock = (editor, format) => {
-  const isActive = isBlockActive(
-    editor,
-    format,
-    TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
-  );
-  const isList = LIST_TYPES.includes(format);
+  const isActive = isBlockActive(editor, format);
 
-  Transforms.unwrapNodes(editor, {
-    match: n =>
-      !Editor.isEditor(n) &&
-      SlateElement.isElement(n) &&
-      LIST_TYPES.includes(n.type) &&
-      !TEXT_ALIGN_TYPES.includes(format),
-    split: true
-  });
-  let newProperties;
-  if (TEXT_ALIGN_TYPES.includes(format)) {
-    newProperties = {
-      align: isActive ? undefined : format
-    };
+  if (isActive) {
+    Transforms.unwrapNodes(editor, {match: n => n.type === format});
   } else {
-    newProperties = {
-      type: isActive ? 'paragraph' : isList ? 'list-item' : format
-    };
-  }
-  Transforms.setNodes(editor, newProperties);
-
-  if (!isActive && isList) {
-    const block = {type: format, children: []};
-    Transforms.wrapNodes(editor, block);
+    Transforms.wrapNodes(editor, {
+      type: format,
+      children: []
+    });
   }
 };
 
@@ -159,41 +115,11 @@ const isMarkActive = (editor, format) => {
 const Element = ({attributes, children, element}) => {
   const style = {textAlign: element.align};
   switch (element.type) {
-    case 'block-quote':
+    case 'code_block':
       return (
-        <blockquote style={style} {...attributes}>
-          {children}
-        </blockquote>
-      );
-    case 'bulleted-list':
-      return (
-        <ul style={style} {...attributes}>
-          {children}
-        </ul>
-      );
-    case 'heading-one':
-      return (
-        <h1 style={style} {...attributes}>
-          {children}
-        </h1>
-      );
-    case 'heading-two':
-      return (
-        <h2 style={style} {...attributes}>
-          {children}
-        </h2>
-      );
-    case 'list-item':
-      return (
-        <li style={style} {...attributes}>
-          {children}
-        </li>
-      );
-    case 'numbered-list':
-      return (
-        <ol style={style} {...attributes}>
-          {children}
-        </ol>
+        <pre {...attributes}>
+          <code>{children}</code>
+        </pre>
       );
     default:
       return (
@@ -204,95 +130,14 @@ const Element = ({attributes, children, element}) => {
   }
 };
 
-const Leaf = ({attributes, children, leaf}) => {
-  if (leaf.bold) {
-    children = <strong>{children}</strong>;
-  }
-
-  if (leaf.code) {
-    children = (
-      <pre {...attributes}>
-        <code>{children}</code>
-      </pre>
-    );
-  }
-
-  if (leaf.italic) {
-    children = <em>{children}</em>;
-  }
-
-  if (leaf.underline) {
-    children = <u>{children}</u>;
-  }
-
+const Leaf = ({attributes, children}) => {
   return <span {...attributes}>{children}</span>;
-};
-
-const BlockButton = ({format, icon}) => {
-  const editor = useSlate();
-  return (
-    <Button
-      active={isBlockActive(
-        editor,
-        format,
-        TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
-      )}
-      onMouseDown={event => {
-        event.preventDefault();
-        toggleBlock(editor, format);
-      }}
-    >
-      <Icon>{icon}</Icon>
-    </Button>
-  );
-};
-
-const MarkButton = ({format, icon}) => {
-  const editor = useSlate();
-  return (
-    <Button
-      active={isMarkActive(editor, format)}
-      onMouseDown={event => {
-        event.preventDefault();
-        toggleMark(editor, format);
-      }}
-    >
-      <Icon>{icon}</Icon>
-    </Button>
-  );
 };
 
 const initialValue = [
   {
     type: 'paragraph',
-    children: [
-      {text: 'This is editable '},
-      {text: 'rich', bold: true},
-      {text: ' text, '},
-      {text: 'much', italic: true},
-      {text: ' better than a '},
-      {text: '<textarea>', code: true},
-      {text: '!'}
-    ]
-  },
-  {
-    type: 'paragraph',
-    children: [
-      {
-        text:
-          "Since it's rich text, you can do things like turn a selection of text "
-      },
-      {text: 'bold', bold: true},
-      {
-        text:
-          ', or add a semantically rendered block quote in the middle of the page, like this:'
-      }
-    ]
-  },
-  {
-    type: 'paragraph',
-    align: 'center',
-    children: [{text: 'Try it out for yourself!'}]
+    children: [{text: ''}]
   }
 ];
 
