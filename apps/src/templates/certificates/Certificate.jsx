f13af0b8915f1f2c872e72290e32852e13adec3a
David Bailey <davidsbailey@users.noreply.github.com>
2022-06-13 11:27:53 -0700

    make initialCertificateImageUrl prop required

--- apps/src/templates/certificates/Certificate.jsx
+++ apps/src/templates/certificates/Certificate.jsx
@@ -190,7 +190,7 @@ Certificate.propTypes = {
   under13: PropTypes.bool,
   children: PropTypes.node,
   showStudioCertificate: PropTypes.bool,
-  initialCertificateImageUrl: PropTypes.string
+  initialCertificateImageUrl: PropTypes.string.isRequired
 };
 
 const styles = {
