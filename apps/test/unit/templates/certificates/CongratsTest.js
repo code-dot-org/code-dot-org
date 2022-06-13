fa35d06ee703e4c03f20c77bdfd2d6284db66249
David Bailey <davidsbailey@users.noreply.github.com>
2022-06-13 11:35:40 -0700

    extract defaultProps for Congrats

--- apps/test/unit/templates/certificates/CongratsTest.js
+++ apps/test/unit/templates/certificates/CongratsTest.js
@@ -6,66 +6,37 @@ import Certificate from '@cdo/apps/templates/certificates/Certificate';
 import StudentsBeyondHoc from '@cdo/apps/templates/certificates/StudentsBeyondHoc';
 import TeachersBeyondHoc from '@cdo/apps/templates/certificates/TeachersBeyondHoc';
 
-const initialCertificateImageUrl = '/images/placeholder-hoc-image.jpg';
-
 describe('Congrats', () => {
+  const initialCertificateImageUrl = '/images/placeholder-hoc-image.jpg';
+  const defaultProps = {
+    completedTutorialType: 'other',
+    userType: 'signedOut',
+    language: 'en',
+    initialCertificateImageUrl
+  };
+
   it('renders a Certificate component', () => {
-    const wrapper = shallow(
-      <Congrats
-        completedTutorialType="other"
-        userType="signedOut"
-        language="en"
-        initialCertificateImageUrl={initialCertificateImageUrl}
-      />
-    );
+    const wrapper = shallow(<Congrats {...defaultProps} />);
     expect(wrapper.find(Certificate).exists()).to.be.true;
   });
 
   it('renders a StudentsBeyondHoc component, regardless of user type', () => {
-    const wrapper = shallow(
-      <Congrats
-        completedTutorialType="other"
-        userType="signedOut"
-        language="en"
-        initialCertificateImageUrl={initialCertificateImageUrl}
-      />
-    );
+    const wrapper = shallow(<Congrats {...defaultProps} />);
     expect(wrapper.find(StudentsBeyondHoc).exists()).to.be.true;
   });
 
   it('renders a TeachersBeyondHoc component, for teachers', () => {
-    const wrapper = shallow(
-      <Congrats
-        completedTutorialType="other"
-        userType="teacher"
-        language="en"
-        initialCertificateImageUrl={initialCertificateImageUrl}
-      />
-    );
+    const wrapper = shallow(<Congrats {...defaultProps} userType="teacher" />);
     expect(wrapper.find(TeachersBeyondHoc).exists()).to.be.true;
   });
 
   it('renders a TeachersBeyondHoc component, for signed out', () => {
-    const wrapper = shallow(
-      <Congrats
-        completedTutorialType="other"
-        userType="signedOut"
-        language="en"
-        initialCertificateImageUrl={initialCertificateImageUrl}
-      />
-    );
+    const wrapper = shallow(<Congrats {...defaultProps} />);
     expect(wrapper.find(TeachersBeyondHoc).exists()).to.be.true;
   });
 
   it('does not render a TeachersBeyondHoc component, for students', () => {
-    const wrapper = shallow(
-      <Congrats
-        completedTutorialType="other"
-        userType="student"
-        language="en"
-        initialCertificateImageUrl={initialCertificateImageUrl}
-      />
-    );
+    const wrapper = shallow(<Congrats {...defaultProps} userType="student" />);
     expect(wrapper.find(TeachersBeyondHoc).exists()).to.be.false;
   });
 });
