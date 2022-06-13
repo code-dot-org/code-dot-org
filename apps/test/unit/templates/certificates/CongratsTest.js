b383955d1b7f689b0a4a5e4a5a171ad88eba26f4
David Bailey <davidsbailey@users.noreply.github.com>
2022-06-13 14:06:33 -0700

    Revert "extract defaultProps for Congrats"
    This reverts commit 5ada958bf17e3799ee87cb45de7cc59470bd02ef.

--- apps/test/unit/templates/certificates/CongratsTest.js
+++ apps/test/unit/templates/certificates/CongratsTest.js
@@ -6,37 +6,66 @@ import Certificate from '@cdo/apps/templates/certificates/Certificate';
 import StudentsBeyondHoc from '@cdo/apps/templates/certificates/StudentsBeyondHoc';
 import TeachersBeyondHoc from '@cdo/apps/templates/certificates/TeachersBeyondHoc';
 
-describe('Congrats', () => {
-  const initialCertificateImageUrl = '/images/placeholder-hoc-image.jpg';
-  const defaultProps = {
-    completedTutorialType: 'other',
-    userType: 'signedOut',
-    language: 'en',
-    initialCertificateImageUrl
-  };
+const initialCertificateImageUrl = '/images/placeholder-hoc-image.jpg';
 
+describe('Congrats', () => {
   it('renders a Certificate component', () => {
-    const wrapper = shallow(<Congrats {...defaultProps} />);
+    const wrapper = shallow(
+      <Congrats
+        completedTutorialType="other"
+        userType="signedOut"
+        language="en"
+        initialCertificateImageUrl={initialCertificateImageUrl}
+      />
+    );
     expect(wrapper.find(Certificate).exists()).to.be.true;
   });
 
   it('renders a StudentsBeyondHoc component, regardless of user type', () => {
-    const wrapper = shallow(<Congrats {...defaultProps} />);
+    const wrapper = shallow(
+      <Congrats
+        completedTutorialType="other"
+        userType="signedOut"
+        language="en"
+        initialCertificateImageUrl={initialCertificateImageUrl}
+      />
+    );
     expect(wrapper.find(StudentsBeyondHoc).exists()).to.be.true;
   });
 
   it('renders a TeachersBeyondHoc component, for teachers', () => {
-    const wrapper = shallow(<Congrats {...defaultProps} userType="teacher" />);
+    const wrapper = shallow(
+      <Congrats
+        completedTutorialType="other"
+        userType="teacher"
+        language="en"
+        initialCertificateImageUrl={initialCertificateImageUrl}
+      />
+    );
     expect(wrapper.find(TeachersBeyondHoc).exists()).to.be.true;
   });
 
   it('renders a TeachersBeyondHoc component, for signed out', () => {
-    const wrapper = shallow(<Congrats {...defaultProps} />);
+    const wrapper = shallow(
+      <Congrats
+        completedTutorialType="other"
+        userType="signedOut"
+        language="en"
+        initialCertificateImageUrl={initialCertificateImageUrl}
+      />
+    );
     expect(wrapper.find(TeachersBeyondHoc).exists()).to.be.true;
   });
 
   it('does not render a TeachersBeyondHoc component, for students', () => {
-    const wrapper = shallow(<Congrats {...defaultProps} userType="student" />);
+    const wrapper = shallow(
+      <Congrats
+        completedTutorialType="other"
+        userType="student"
+        language="en"
+        initialCertificateImageUrl={initialCertificateImageUrl}
+      />
+    );
     expect(wrapper.find(TeachersBeyondHoc).exists()).to.be.false;
   });
 });
