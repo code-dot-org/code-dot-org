7414edbf8c16cb180e2157466ebe0ab351f4f980
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-06-13 14:59:03 -0700

    Enable and Configure Zeitwerk

--- dashboard/config/initializers/zeitwerk.rb
+++ dashboard/config/initializers/zeitwerk.rb
@@ -2,7 +2,7 @@
 # globally-defined acronyms in config/initializers/inflections.rb
 #
 # Based on https://guides.rubyonrails.org/classic_to_zeitwerk_howto.html#acronyms
-Rails.autoloaders.main.inflector.inflect(
+Rails.autoloaders.main&.inflector&.inflect(
   "afe_enrollment" => "AFEEnrollment",
   "csta_enrollment" => "CSTAEnrollment",
   "payment_calculator_csf" => "PaymentCalculatorCSF"
