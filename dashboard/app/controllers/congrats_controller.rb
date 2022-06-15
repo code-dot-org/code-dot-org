02dc2f913c85a11d289ead54c1ee1653752fa592
Meg Crenshaw <meg@code.org>
2022-06-15 14:57:55 -0500

    Pass in data from controller

--- dashboard/app/controllers/congrats_controller.rb
+++ dashboard/app/controllers/congrats_controller.rb
@@ -4,6 +4,17 @@ class CongratsController < ApplicationController
   def index
     view_options(full_width: true, responsive_content: true, has_i18n: true)
 
+    begin
+      course = Base64.urlsafe_decode64(params[:s])
+    rescue ArgumentError, OpenSSL::Cipher::CipherError
+      return render status: :bad_request, json: {message: 'invalid base64'}
+    end
+
+    @next_course_script_name = ScriptConstants.csf_next_course_recommendation(course)
+    next_script = Script.get_from_cache(@next_course_script_name)
+    @next_course_title = next_script.localized_title
+    @next_course_description = next_script.localized_description
+
     # Select two different donors, because the first must have a twitter
     # handle and the second must be equally weighted across all donors.
     @random_donor_twitter = CdoDonor.get_random_donor_twitter
