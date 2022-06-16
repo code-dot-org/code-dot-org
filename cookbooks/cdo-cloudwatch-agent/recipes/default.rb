8c200ae0847a4eb934ec279ed4ad27731ba13e69
Suresh Chanmugam <suresh@code.org>
2022-06-15 22:28:47 -0700

    Simplify to just installing the CloudWatch Agent. (+1 squashed commit) Squashed commits: [a9728df5aa4] Second attempt.

--- cookbooks/cdo-cloudwatch-agent/recipes/default.rb
+++ cookbooks/cdo-cloudwatch-agent/recipes/default.rb
@@ -5,14 +5,4 @@
 
 aws_cloudwatch_agent 'default' do
   action [:install]
-  json_config 'amazon-cloudwatch-agent.json.erb'
-end
-
-template 'amazon-cloudwatch-agent.json' do
-  path "/opt/aws/amazon-cloudwatch-agent/etc/"
-  source "amazon-cloudwatch-agent.json.erb"
-end
-
-aws_cloudwatch_agent 'default' do
-  actiom [:restart]
 end
