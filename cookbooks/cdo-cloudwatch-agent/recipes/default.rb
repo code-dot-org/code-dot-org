87e337e37b7c2271cf23bab26d001afe537ab7f0
Suresh Chanmugam <suresh@code.org>
2022-06-16 13:04:44 -0700

    Render configuration file and place in configuration directory.

--- cookbooks/cdo-cloudwatch-agent/recipes/default.rb
+++ cookbooks/cdo-cloudwatch-agent/recipes/default.rb
@@ -6,3 +6,8 @@
 aws_cloudwatch_agent 'default' do
   action [:install]
 end
+
+template 'amazon-cloudwatch-agent.json' do
+  path "/opt/aws/amazon-cloudwatch-agent/etc"
+  source "amazon-cloudwatch-agent.json.erb"
+end
