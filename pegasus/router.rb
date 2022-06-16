d78e17aa72b22fea8f7f677ea4127d18032661c1
James Chen <james@code.org>
2022-06-16 15:34:06 -0700

    Support sass files in pegasus/sites.v3/code.org/public/css

--- pegasus/router.rb
+++ pegasus/router.rb
@@ -110,9 +110,12 @@ class Documents < Sinatra::Base
       settings.template_extnames +
       settings.exclude_extnames +
       ['.fetch']
+    # Note: shared_resources.rb has additional configuration for Sass::Plugin
     Sass::Plugin.options[:cache_location] = pegasus_dir('cache', '.sass-cache')
-    Sass::Plugin.options[:css_location] = pegasus_dir('cache', 'css')
-    Sass::Plugin.options[:template_location] = shared_dir('css')
+    Sass::Plugin.add_template_location(
+      sites_v3_dir('code.org', 'public', 'css'),
+      sites_v3_dir('code.org', 'public', 'css', 'generated')
+    )
     set :mustermann_opts, check_anchors: false, ignore_unknown_options: true
 
     # Haml/Temple engine doesn't recognize the `path` option
