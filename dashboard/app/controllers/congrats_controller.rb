994964d34f7d8af943bc82e5b0695bc35233f2f9
Meg Crenshaw <meg@code.org>
2022-06-22 09:58:43 -0500

    Remove duplicate decode

--- dashboard/app/controllers/congrats_controller.rb
+++ dashboard/app/controllers/congrats_controller.rb
@@ -4,17 +4,6 @@ class CongratsController < ApplicationController
   def index
     view_options(full_width: true, responsive_content: true, has_i18n: true)
 
-    begin
-      course = Base64.urlsafe_decode64(params[:s])
-    rescue ArgumentError, OpenSSL::Cipher::CipherError
-      return render status: :bad_request, json: {message: 'invalid base64'}
-    end
-
-    @next_course_script_name = ScriptConstants.csf_next_course_recommendation(course)
-    next_script = Script.get_from_cache(@next_course_script_name) if @next_course_script_name
-    @next_course_title = next_script.localized_title if next_script
-    @next_course_description = next_script.localized_description if next_script
-
     # Select two different donors, because the first must have a twitter
     # handle and the second must be equally weighted across all donors.
     @random_donor_twitter = CdoDonor.get_random_donor_twitter
@@ -22,9 +11,15 @@ class CongratsController < ApplicationController
     begin
       course_name = params[:s] && Base64.urlsafe_decode64(params[:s])
       @certificate_image_url = certificate_image_url(nil, course_name, nil)
-    rescue OpenSSL::Cipher::CipherError
+    rescue ArgumentError, OpenSSL::Cipher::CipherError
       return render status: :bad_request, json: {message: 'invalid base64'}
     end
+
     @is_hoc_tutorial = CertificateImage.hoc_course?(course_name)
+
+    @next_course_script_name = ScriptConstants.csf_next_course_recommendation(course_name)
+    next_script = Script.get_from_cache(@next_course_script_name) if @next_course_script_name
+    @next_course_title = next_script.localized_title if next_script
+    @next_course_description = next_script.localized_description if next_script
   end
 end
