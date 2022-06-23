d18229e26b31f040e171fa04b56e26d670bf80a3
Meg Crenshaw <meg@code.org>
2022-06-23 13:11:01 -0500

    Test that bogus scriptName gets app lab

--- apps/test/unit/templates/certificates/GraduateToNextLevelTest.js
+++ apps/test/unit/templates/certificates/GraduateToNextLevelTest.js
@@ -3,14 +3,23 @@ import React from 'react';
 import {isolateComponent} from 'isolate-react';
 import {expect} from '../../../util/reconfiguredChai';
 
-const isolateGraduateToNextLevel = props =>
-  isolateComponent(<GraduateToNextLevel {...props} />);
+const propsPassedToVerticalImageResourceCard = props =>
+  isolateComponent(<GraduateToNextLevel {...props} />).findOne(
+    'VerticalImageResourceCard'
+  ).props;
 
 describe('GraduateToNextLevel', () => {
   it('passes App Lab course info to ResourceCard by default', () => {
-    const propsPassed = isolateGraduateToNextLevel().findOne(
-      'VerticalImageResourceCard'
-    ).props;
+    const propsPassed = propsPassedToVerticalImageResourceCard();
+    const {id, title, description, link, image} = propsPassed;
+    [id, title, description, link, image].forEach(prop => {
+      expect(prop.toLowerCase()).to.contain('app');
+    });
+  });
+  it('passes App Lab course info to ResourceCard if bogus scriptName is given', () => {
+    const propsPassed = propsPassedToVerticalImageResourceCard({
+      scriptName: 'bogusScript'
+    });
     const {id, title, description, link, image} = propsPassed;
     [id, title, description, link, image].forEach(prop => {
       expect(prop.toLowerCase()).to.contain('app');
