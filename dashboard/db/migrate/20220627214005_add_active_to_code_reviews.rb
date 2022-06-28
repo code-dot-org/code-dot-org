f7ef393ec7c81a5024f4012884464ce55280695f
Maureen <maureen.sturgeon@gmail.com>
2022-06-28 15:26:07 -0700

    wording update

--- dashboard/db/migrate/20220627214005_add_active_to_code_reviews.rb
+++ dashboard/db/migrate/20220627214005_add_active_to_code_reviews.rb
@@ -3,12 +3,12 @@ class AddActiveToCodeReviews < ActiveRecord::Migration[6.0]
     # Previously, the unique index was on the columns user_id, project_id, closed_at and deleted_at. There should
     # only be one open code review per user per project at any given time. The issue with the original
     # unique index is that in SQL NULL != NULL and since an open code review is represented by closed_at=NULL and
-    # deleted_at=NULL this will be considered different than another record with the same values. So the old unique index
+    # deleted_at=NULL this is considered different than another record with the same values. So the old unique index
     # was not properly guarding against duplicate open code review rows.
     #
     # The solution is to create inverse virtual columns for both nullable columns and include those in the unique
     # index instead. The new index will have user_id, project_id, open and active. When a code review is open
-    # none of the columns in the unique index will be null (closed_at=NULL => open=true, deleted_at=null => active=true)
+    # none of the columns in the unique index will be null (closed_at=NULL => open=true, deleted_at=null => active=true),
     # so it is guaranteed to be distinct from other open code reviews.
 
     # The active column is the inverse of deleted_at, it is null when deleted_at is populated
