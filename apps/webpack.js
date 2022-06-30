1cf32bc0caecb21bb750846431f6c58c0fe40879
James Chen <james@code.org>
2022-06-30 14:43:08 -0700

    Work around react-loading-skeleton import / bundling issue

--- apps/webpack.js
+++ apps/webpack.js
@@ -36,7 +36,8 @@ var toTranspileWithinNodeModules = [
     'node_modules',
     'microsoft-cognitiveservices-speech-sdk'
   ),
-  path.resolve(__dirname, 'node_modules', 'slate')
+  path.resolve(__dirname, 'node_modules', 'slate'),
+  path.resolve(__dirname, 'node_modules', 'react-loading-skeleton')
 ];
 
 const scssIncludePath = path.resolve(__dirname, '..', 'shared', 'css');
