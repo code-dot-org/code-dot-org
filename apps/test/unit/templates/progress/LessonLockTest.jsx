50079feafbaeb9f0051ac404458a573f57624aa2
Maureen <maureen.sturgeon@gmail.com>
2022-07-05 16:54:24 -0700

    update LessonLock component unit test

--- apps/test/unit/templates/progress/LessonLockTest.jsx
+++ apps/test/unit/templates/progress/LessonLockTest.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {expect} from '../../../util/deprecatedChai';
 import {shallow} from 'enzyme';
 import LessonLock from '@cdo/apps/templates/progress/LessonLock';
+import LessonLockDialog from '@cdo/apps/code-studio/components/progress/LessonLockDialog';
+import Button from '@cdo/apps/templates/Button';
+import i18n from '@cdo/locale';
 
 const FAKE_LESSON_ID = 33;
 const FAKE_UNIT_ID = 1;
 const DEFAULT_PROPS = {
-  unitId: FAKE_UNIT_ID,
-  lessonId: FAKE_LESSON_ID
+  lessonId: FAKE_LESSON_ID,
+  unitId: FAKE_UNIT_ID
 };
 
 const setUp = (overrideProps = {}) => {
@@ -16,15 +19,21 @@ const setUp = (overrideProps = {}) => {
 };
 
 describe('LessonLock', () => {
-  it('renders only a button initially', () => {
+  it('renders an open dialog button and no LessonLockDialog by default', () => {
     const wrapper = setUp();
-    expect(wrapper.find('Button')).to.have.length(1);
-    expect(wrapper.find('LessonLockDialog')).to.have.length(0);
+    const openDialogButton = wrapper.find(Button);
+    expect(openDialogButton).to.have.length(1);
+    expect(openDialogButton.props().text).to.equal(i18n.lockSettings());
+    expect(wrapper.find(LessonLockDialog)).to.have.length(0);
   });
 
-  it('renders the dialog when the button is clicked', () => {
+  it('renders the LessonLockDialog when button is clicked', () => {
     const wrapper = setUp();
-    wrapper.find('Button').simulate('click');
-    expect(wrapper.find('Connect(LessonLockDialog)')).to.have.length(1);
+    wrapper.find(Button).simulate('click');
+
+    const lockDialog = wrapper.find(LessonLockDialog);
+    expect(lockDialog).to.have.length(1);
+    expect(lockDialog.props().unitId).to.equal(FAKE_UNIT_ID);
+    expect(lockDialog.props().lessonId).to.equal(FAKE_LESSON_ID);
   });
 });
