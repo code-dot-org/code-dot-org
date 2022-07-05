068e0c743e4aa4054d83fe8cf8274223740278c4
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-07-05 16:59:58 -0700

    disable unknown asset fallback everywhere, not just when we aren't optimizing assets

--- dashboard/config/application.rb
+++ dashboard/config/application.rb
@@ -27,6 +27,7 @@ module Dashboard
     # Eventually, we want to simply call:
     #config.load_defaults 6.0
     config.active_record.belongs_to_required_by_default = true
+    config.assets.unknown_asset_fallback = false
     config.autoloader = :zeitwerk
 
     unless CDO.chef_managed
@@ -124,7 +125,6 @@ module Dashboard
 
     # Whether to fallback to assets pipeline if a precompiled asset is missed.
     config.assets.compile = !CDO.optimize_rails_assets
-    config.assets.unknown_asset_fallback = !CDO.optimize_rails_assets
 
     # Generate digests for assets URLs which do not contain webpack hashes.
     config.assets.digest = CDO.optimize_rails_assets
