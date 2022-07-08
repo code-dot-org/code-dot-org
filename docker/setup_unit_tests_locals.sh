3d01b7b9faddf7bb9a37cc055aaeec0f41e59a8d
Bethany Connor <bethany@code.org>
2022-07-08 13:11:52 -0700

    Move setting env variables

--- docker/setup_unit_tests_locals.sh
+++ docker/setup_unit_tests_locals.sh
@@ -4,16 +4,7 @@
 # In most cases, you will not run this script directly, but instead
 # use docker-compose to run using the unit-tests-compose.yml file in this directory. See instructions in that file.
 
-set -xe
-
-export CI=true
-export RAILS_ENV=test
-export RACK_ENV=test
-export DISABLE_SPRING=1
-export LD_LIBRARY_PATH=/usr/local/lib
-
 # set up locals.yml
-set +x
 echo "
 bundler_use_sudo: false
 cloudfront_key_pair_id: $CLOUDFRONT_KEY_PAIR_ID
@@ -34,4 +25,3 @@ google_maps_api_key: boguskey
 dashboard_db_reader: \"mysql://readonly@localhost/dashboard_test\"
 " >> locals.yml
 echo "Wrote secrets from env vars into locals.yml."
-set -x
