daa12145ee6100bff66840a498b2092fc1a32faa
Bethany Connor <bethany@code.org>
2022-07-08 10:55:17 -0700

    Setup locals

---
+++ docker/setup_unit_tests_locals.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Script for running unit tests within a docker container.
+# In most cases, you will not run this script directly, but instead
+# use docker-compose to run using the unit-tests-compose.yml file in this directory. See instructions in that file.
+
+set -xe
+
+mispipe "echo 'Starting timestamp'" ts
+
+export CI=true
+export RAILS_ENV=test
+export RACK_ENV=test
+export DISABLE_SPRING=1
+export LD_LIBRARY_PATH=/usr/local/lib
+
+# set up locals.yml
+set +x
+echo "
+bundler_use_sudo: false
+cloudfront_key_pair_id: $CLOUDFRONT_KEY_PAIR_ID
+cloudfront_private_key: \"$CLOUDFRONT_PRIVATE_KEY\"
+ignore_eyes_mismatches: true
+disable_all_eyes_running: true
+use_my_apps: true
+build_dashboard: true
+build_pegasus: true
+build_apps: true
+localize_apps: true
+dashboard_enable_pegasus: true
+dashboard_workers: 5
+skip_seed_all: true
+optimize_webpack_assets: false
+optimize_rails_assets: false
+google_maps_api_key: boguskey
+dashboard_db_reader: \"mysql://readonly@localhost/dashboard_test\"
+" >> locals.yml
+echo "Wrote secrets from env vars into locals.yml."
+set -x
