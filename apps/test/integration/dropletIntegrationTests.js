7555fcb0cfd441dcdc63b8d0220842a050c1ef5b
Ben Brooks <ben@code.org>
2022-07-12 15:26:04 -0700

    Use minified file extension for all affected JS files

--- apps/test/integration/dropletIntegrationTests.js
+++ apps/test/integration/dropletIntegrationTests.js
@@ -10,6 +10,7 @@ import loadSource from './util/loadSource';
 describe('setParamAtIndex', () => {
   let editor, parser, plainTree, arrayTree;
 
+  // do we need to update these?
   before(function() {
     // Load droplet sources.
     return loadSource('/base/lib/ace/src-noconflict/ace.js')
