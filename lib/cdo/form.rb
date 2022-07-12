d04b225b914228576ed5ade77a4156eaed210ac5
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-07-11 17:22:46 -0700

    Fix and Reenable `Security/JSONLoad` Rubocop
    Specifically, I ran `bundle exec rubocop -A --only Security/JSONLoad`, which replaced all instance of `JSON.load` with `JSON.parse`. In all cases where we were passing a string, that's just fine, but `JSON.load` supports StringIO and File inputs, so I then had to step through each change and manually update a few to convert the inputs to a parsable string.

--- lib/cdo/form.rb
+++ lib/cdo/form.rb
@@ -3,8 +3,8 @@ require 'json'
 class Form2 < OpenStruct
   def initialize(params={})
     params = params.dup
-    params[:data] = JSON.load(params[:data])
-    params[:processed_data] = JSON.load(params[:processed_data])
+    params[:data] = JSON.parse(params[:data])
+    params[:processed_data] = JSON.parse(params[:processed_data])
     super params
   end
 
