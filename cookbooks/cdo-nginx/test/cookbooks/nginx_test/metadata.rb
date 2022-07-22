cb2a8ebd96fefc6d6cce2ca93deab10d3a5f41ef
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-07-22 13:59:52 -0700

    Fix Violations of `Security/IoMethods` and Reenable
    Specifically, I ran `bundle exec rubocop -A --only Security/IoMethods` to automatically replace all appropriate uses of `IO.` methods with comparable `File.` methods.
    
    According to https://docs.rubocop.org/rubocop/cops_security.html#securityiomethods, this is preferable because:
    
    > If argument starts with a pipe character ('|') and the receiver is the `IO` class, a subprocess is created in the same way as `Kernel#open`, and its output is returned. `Kernel#open` may allow unintentional command injection, which is the reason these `IO` methods are a security risk.  Consider to use `File.read` to disable the behavior of subprocess invocation.

--- cookbooks/cdo-nginx/test/cookbooks/nginx_test/metadata.rb
+++ cookbooks/cdo-nginx/test/cookbooks/nginx_test/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Code.org'
 maintainer_email 'will@code.org'
 license          'All rights reserved'
 description      'Installs/Configures nginx_test'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description File.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
 depends 'cdo-nginx'
