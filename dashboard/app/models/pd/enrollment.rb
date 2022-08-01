55be7d1d51d6b9c98aeb7ec51a89bc55153a6bea
David Bailey <davidsbailey@users.noreply.github.com>
2022-08-01 15:33:46 -0700

    remove filter_for_pegasus_survey_completion

--- dashboard/app/models/pd/enrollment.rb
+++ dashboard/app/models/pd/enrollment.rb
@@ -153,8 +153,8 @@ class Pd::Enrollment < ApplicationRecord
   end
 
   # Filters a list of enrollments for survey completion, for the workshop types we are able to filter for
-  # survey completion: CSD/CSP Summer, CSP Workshop for Returning Teachers, CSF Intro/Deep Dive, Counselor
-  # and Admin. We will always return [] for Academic year workshops as we want facilitators to handle those surveys.
+  # survey completion: CSD/CSP Summer, CSP Workshop for Returning Teachers, CSF Intro/Deep Dive.
+  # We will always return [] for Academic year workshops as we want facilitators to handle those surveys.
   # @param enrollments [Enumerable<Pd::Enrollment>] list of enrollments to filter.
   # @param select_completed [Boolean] if true, return only enrollments with completed surveys,
   #   otherwise return only those without completed surveys. Defaults to true.
@@ -165,24 +165,16 @@ class Pd::Enrollment < ApplicationRecord
 
     # Local summer, CSP Workshop for Returning Teachers, or CSF Intro after 5/8/2020 will use Foorm for survey completion.
     # CSF Deep Dive after 9/1 also uses Foorm. CSF District workshops will always use Foorm
-    foorm_enrollments, other_enrollments = enrollments.partition do |enrollment|
+    foorm_enrollments, _ = enrollments.partition do |enrollment|
       (enrollment.workshop.workshop_ending_date >= Date.new(2020, 5, 8) &&
         (enrollment.workshop.csf_intro? || enrollment.workshop.local_summer? || enrollment.workshop.csp_wfrt?)) ||
         (enrollment.workshop.workshop_ending_date >= Date.new(2020, 9, 1) && enrollment.workshop.csf_201?) || enrollment.workshop.csf_district?
     end
 
-    # Admin and Counselor still use Pegasus form
-    pegasus_enrollments, _ = other_enrollments.partition do |enrollment|
-      enrollment.workshop.course == COURSE_ADMIN || enrollment.workshop.course == COURSE_COUNSELOR
-    end
-
     # We do not want to check survey completion for the following workshop types: Legacy (non-Foorm) summer,
     # CSF Intro, and CSF Deep Dive (surveys would be too out of date), teachercon (deprecated), or any academic year workshop
     # (there are multiple post-survey options, therefore the facilitators must provide a link themselves).
-    (
-      filter_for_pegasus_survey_completion(pegasus_enrollments, select_completed) +
-      filter_for_foorm_survey_completion(foorm_enrollments, select_completed)
-    )
+    filter_for_foorm_survey_completion(foorm_enrollments, select_completed)
   end
 
   before_create :assign_code
@@ -373,24 +365,6 @@ class Pd::Enrollment < ApplicationRecord
 
   def authorize_teacher_account
     user.permission = UserPermission::AUTHORIZED_TEACHER if user&.teacher? && [COURSE_CSD, COURSE_CSP, COURSE_CSA].include?(workshop.course)
-  end
-
-  private_class_method def self.filter_for_pegasus_survey_completion(enrollments, select_completed)
-    ids_with_processed_surveys, ids_without_processed_surveys =
-      enrollments.partition {|e| e.completed_survey_id.present?}.map {|list| list.map(&:id)}
-
-    ids_with_unprocessed_surveys = PEGASUS_DB[:forms].where(
-      kind: 'PdWorkshopSurvey',
-      source_id: ids_without_processed_surveys
-    ).map do |survey|
-      survey[:source_id].to_i
-    end
-
-    filtered_ids = select_completed ?
-                     ids_with_processed_surveys + ids_with_unprocessed_surveys :
-                     ids_without_processed_surveys - ids_with_unprocessed_surveys
-
-    enrollments.select {|e| filtered_ids.include? e.id}
   end
 
   private_class_method def self.filter_for_foorm_survey_completion(enrollments, select_completed)
