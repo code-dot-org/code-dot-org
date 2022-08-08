9c1a2dcd8fadf5b92a6e6a36b6c53df6e98140a1
molly-moen <molly@code.org>
2022-08-08 10:35:27 -0700

    remove theme-one-dark and add new theme file

---
+++ apps/src/javalab/editorThemes.js
@@ -0,0 +1,161 @@
+import {EditorView} from '@codemirror/view';
+import {
+  HighlightStyle,
+  tags,
+  defaultHighlightStyle
+} from '@codemirror/highlight';
+
+// modified from @codemirror/theme-one-dark
+const chalky = '#e5c07b',
+  coral = '#e06c75',
+  cyan = '#56b6c2',
+  invalid = '#ffffff',
+  ivory = '#abb2bf',
+  stone = '#7d8799',
+  malibu = '#61afef',
+  sage = '#98c379',
+  whiskey = '#d19a66',
+  violet = '#c678dd',
+  darkBackground = '#21252b',
+  highlightBackground = '#2c313a',
+  background = '#282c34',
+  selection = '#3E4451',
+  cursor = '#528bff';
+
+/**
+The editor theme styles for dark mode.
+*/
+export const darkTheme = EditorView.theme(
+  {
+    '&': {
+      color: ivory,
+      backgroundColor: background
+    },
+    '.cm-content': {
+      caretColor: cursor
+    },
+    '&.cm-focused .cm-cursor': {borderLeftColor: cursor},
+    '&.cm-focused .cm-selectionBackground, .cm-selectionBackground, ::selection': {
+      backgroundColor: selection
+    },
+    '.cm-panels': {backgroundColor: darkBackground, color: ivory},
+    '.cm-panels.cm-panels-top': {borderBottom: '2px solid black'},
+    '.cm-panels.cm-panels-bottom': {borderTop: '2px solid black'},
+    '.cm-searchMatch': {
+      backgroundColor: '#72a1ff59',
+      outline: '1px solid #457dff'
+    },
+    '.cm-searchMatch.cm-searchMatch-selected': {
+      backgroundColor: '#6199ff2f'
+    },
+    '.cm-activeLine': {backgroundColor: highlightBackground},
+    '.cm-selectionMatch': {backgroundColor: '#aafe661a'},
+    '.cm-matchingBracket, .cm-nonmatchingBracket': {
+      backgroundColor: '#bad0f847',
+      outline: '1px solid #515a6b'
+    },
+    '.cm-gutters': {
+      backgroundColor: background,
+      color: stone,
+      border: 'none'
+    },
+    '.cm-activeLineGutter': {
+      backgroundColor: highlightBackground
+    },
+    '.cm-foldPlaceholder': {
+      backgroundColor: 'transparent',
+      border: 'none',
+      color: '#ddd'
+    },
+    '.cm-tooltip': {
+      border: '1px solid #181a1f',
+      backgroundColor: darkBackground
+    },
+    '.cm-tooltip-autocomplete': {
+      '& > ul > li[aria-selected]': {
+        backgroundColor: highlightBackground,
+        color: ivory
+      }
+    }
+  },
+  {dark: true}
+);
+
+/**
+The highlighting style for code in the dark theme.
+*/
+export const darkHighlightStyle = HighlightStyle.define([
+  {tag: tags.keyword, color: violet},
+  {
+    tag: [
+      tags.name,
+      tags.deleted,
+      tags.character,
+      tags.propertyName,
+      tags.macroName,
+      tags.definition(tags.name)
+    ],
+    color: coral
+  },
+  {
+    tag: [tags.function(tags.variableName), tags.labelName],
+    color: malibu
+  },
+  {
+    tag: [tags.color, tags.constant(tags.name), tags.standard(tags.name)],
+    color: whiskey
+  },
+  {
+    tag: [tags.separator],
+    color: ivory
+  },
+  {
+    tag: [
+      tags.typeName,
+      tags.className,
+      tags.number,
+      tags.changed,
+      tags.annotation,
+      tags.modifier,
+      tags.self,
+      tags.namespace
+    ],
+    color: chalky
+  },
+  {
+    tag: [
+      tags.operator,
+      tags.operatorKeyword,
+      tags.url,
+      tags.escape,
+      tags.regexp,
+      tags.link,
+      tags.special(tags.string)
+    ],
+    color: cyan
+  },
+  {tag: [tags.meta, tags.comment], color: stone},
+  {tag: tags.strong, fontWeight: 'bold'},
+  {tag: tags.emphasis, fontStyle: 'italic'},
+  {tag: tags.strikethrough, textDecoration: 'line-through'},
+  {tag: tags.link, color: stone, textDecoration: 'underline'},
+  {tag: tags.heading, fontWeight: 'bold', color: coral},
+  {
+    tag: [tags.atom, tags.bool, tags.special(tags.variableName)],
+    color: whiskey
+  },
+  {tag: [tags.processingInstruction, tags.string, tags.inserted], color: sage},
+  {tag: tags.invalid, color: invalid}
+]);
+
+/**
+Extension to enable the dark theme (both the editor theme and
+the highlight style).
+*/
+export const darkMode = [darkTheme, darkHighlightStyle];
+
+// The default light theme styles for codemirror
+export const lightTheme = EditorView.theme({}, {dark: false});
+
+// Extension to enable the light theme (both the editor theme and the highlight style).
+export const lightMode = [lightTheme, defaultHighlightStyle];
