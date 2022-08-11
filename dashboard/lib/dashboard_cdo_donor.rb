3654b49817634d134dbd83188a569219beedc4af
Pablo M <108825710+pablo-code-org@users.noreply.github.com>
2022-08-11 12:41:37 -0700

    Fixing table table

--- dashboard/lib/dashboard_cdo_donor.rb
+++ dashboard/lib/dashboard_cdo_donor.rb
@@ -3,6 +3,6 @@ require 'cdo/cdo_donor'
 
 class DashboardCdoDonor < CdoDonor
   def self.all_donors
-    @@all_donors ||= DASHBOARD_DB[:cdo_donors].all
+    @@all_donors ||= DASHBOARD_DB[:google_sheets_shared_cdo_donors].all
   end
 end
