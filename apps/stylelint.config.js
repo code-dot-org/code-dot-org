df23703eb00669b3ba47c002105f44e516a9485f
Meg Crenshaw <meg@code.org>
2022-08-15 13:24:49 -0600

    Revert "Enable length-zero-no-unit rule"

--- apps/stylelint.config.js
+++ apps/stylelint.config.js
@@ -8,6 +8,7 @@ module.exports = {
     'no-descending-specificity': null,
     'rule-empty-line-before': null,
     'selector-class-pattern': null,
+    'length-zero-no-unit': null,
     'selector-id-pattern': null,
     'property-no-vendor-prefix': null,
     'declaration-empty-line-before': null,
