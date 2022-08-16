2bcaca39f14d5fa946822ea7d08a7a13b263b39a
Meg Crenshaw <meg@code.org>
2022-08-16 15:25:21 -0600

    Use safe nav to address error [skip apps]

--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -532,7 +532,7 @@ class ScriptLevelsController < ApplicationController
 
     view_options(
       full_width: true,
-      small_footer: @game.uses_small_footer? || @level.enable_scrolling?,
+      small_footer: @game&.uses_small_footer? || @level.enable_scrolling?,
       has_i18n: @game.has_i18n?,
       is_challenge_level: @script_level.challenge,
       is_bonus_level: @script_level.bonus,
