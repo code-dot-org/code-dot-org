21b2c7c3913759bad5ce492d7ea286d98d9d1823
wilkie <wilkie05@gmail.com>
2022-08-25 13:28:55 -0400

    Update test to DRY it up a little.

--- apps/test/unit/blockTooltips/DropletAutocompleteParameterTooltipManagerTest.js
+++ apps/test/unit/blockTooltips/DropletAutocompleteParameterTooltipManagerTest.js
@@ -12,15 +12,16 @@ describe('DropletAutocompleteParameterTooltipManager', () => {
   });
 
   describe('getTooltipHTML', () => {
-    it('should render localized string for "Choose..."', () => {
-      // Stub the i18n calls.
-      sinon.stub(commonI18n, 'choosePrefix').returns('i18n-choose-prefix');
+    var manager = null;
+    var tooltipInfo = null;
 
-      // Mock a DropletTooltipManager.
-      // We need to describe the function as having at least one parameter.
+    beforeEach(() => {
+      // Build a basic droplet config.
       let dropletConfig = {
         showExamplesLink: 'http://example.com/examples'
       };
+
+      // Create the bare essentials for a block description.
       let blockInfo = {
         parameterInfos: [
           {
@@ -29,13 +30,21 @@ describe('DropletAutocompleteParameterTooltipManager', () => {
           }
         ]
       };
-      let manager = DropletTooltipManagerStub(dropletConfig, blockInfo);
+
+      // Mock a DropletTooltipManager.
+      // We need to describe the function as having at least one parameter.
+      manager = DropletTooltipManagerStub(dropletConfig, blockInfo);
+      tooltipInfo = manager.getDropletTooltip('functionName');
+    });
+
+    it('should render localized string for "Choose..."', () => {
+      // Stub the i18n calls.
+      sinon.stub(commonI18n, 'choosePrefix').returns('i18n-choose-prefix');
 
       // Create the parameter tooltip manager class.
       let tooltip = new DropletAutocompleteParameterTooltipManager(manager);
 
       // Render the tooltip (the first parameter).
-      let tooltipInfo = manager.getDropletTooltip('functionName');
       let html = tooltip.getTooltipHTML(tooltipInfo, 0);
 
       // It should contain a link with the "Choose..." localized text.
@@ -49,26 +58,10 @@ describe('DropletAutocompleteParameterTooltipManager', () => {
       // Stub the i18n calls.
       sinon.stub(commonI18n, 'examples').returns('i18n-examples');
 
-      // Mock a DropletTooltipManager.
-      // We need to describe the function as having at least one parameter.
-      let dropletConfig = {
-        showExamplesLink: 'http://example.com/examples'
-      };
-      let blockInfo = {
-        parameterInfos: [
-          {
-            description: 'parameter description',
-            assetTooltip: 'assetTooltip'
-          }
-        ]
-      };
-      let manager = DropletTooltipManagerStub(dropletConfig, blockInfo);
-
       // Create the parameter tooltip manager class.
       let tooltip = new DropletAutocompleteParameterTooltipManager(manager);
 
       // Render the tooltip (the first parameter).
-      let tooltipInfo = manager.getDropletTooltip('functionName');
       let html = tooltip.getTooltipHTML(tooltipInfo, 0);
 
       // It should contain a link with the "Examples" localized text.
