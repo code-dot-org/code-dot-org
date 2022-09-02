7c00e531332a5d8f5861ac1493536c2c482ba29f
David Bailey <davidsbailey@users.noreply.github.com>
2022-09-02 06:17:25 -0700

    move most auth helpers from shared/middleware to dashboard/legacy

--- dashboard/legacy/test/middleware/helpers/test_auth_helpers.rb
+++ dashboard/legacy/test/middleware/helpers/test_auth_helpers.rb
@@ -1,4 +1,4 @@
-require_relative '../../test_helper'
+require_relative '../middleware_test_helper'
 require_relative '../../../middleware/helpers/auth_helpers'
 
 class AuthHelpersTest < Minitest::Test
