8af47a5d683400995a4eb32eba2881abb5269267
breville <brendan@code.org>
2022-09-04 21:12:50 +0000

    Hacky if_even_then block
    It only checks against a hardcoded variable called measure.

--- apps/src/music/MusicView.jsx
+++ apps/src/music/MusicView.jsx
@@ -225,6 +225,10 @@ class MusicView extends React.Component {
         {
           kind: 'block',
           type: 'variable_get'
+        },
+        {
+          kind: 'block',
+          type: 'if_even_then'
         }
       ]
     };
@@ -360,6 +364,32 @@ class MusicView extends React.Component {
       }
     };
 
+    Blockly.Blocks['if_even_then'] = {
+      init: function() {
+        this.jsonInit({
+          type: 'if_even_then',
+          message0: 'if %1 is even then %2',
+          args0: [
+            {
+              type: 'field_variable',
+              name: 'measure',
+              variable: 'measure'
+            },
+            {
+              type: 'input_statement',
+              name: 'code'
+            }
+          ],
+          inputsInline: true,
+          previousStatement: null,
+          nextStatement: null,
+          colour: 230,
+          tooltip: 'does something if the measure is even',
+          helpUrl: ''
+        });
+      }
+    };
+
     Blockly.Blocks['variable_get'] = {
       init: function() {
         this.jsonInit({
@@ -423,6 +453,16 @@ class MusicView extends React.Component {
         ctx.getFieldValue('to') +
         '; measure++) {\n' +
         //ctx.getFieldValue('code') +
+        Blockly.JavaScript.statementToCode(ctx, 'code') +
+        '\n}\n'
+      );
+    };
+
+    Blockly.JavaScript.if_even_then = function(ctx) {
+      return (
+        'if(' +
+        'measure % 2 == 0' +
+        ') {\n' +
         Blockly.JavaScript.statementToCode(ctx, 'code') +
         '\n}\n'
       );
