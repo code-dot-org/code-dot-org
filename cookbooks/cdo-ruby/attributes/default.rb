158ee976a3d89b81362046b0e30a20afd9b83925
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-09-06 13:26:46 -0700

    pin to the same version of rubygems as is installed by apt

--- cookbooks/cdo-ruby/attributes/default.rb
+++ cookbooks/cdo-ruby/attributes/default.rb
@@ -1,7 +1,6 @@
 default['cdo-ruby'] = {
   version: '2.7',
-  old_version: '2.6',
-  rubygems_version: '3.3.21',
+  rubygems_version: '3.1.6',
   bundler_version: '1.17.3',
   rake_version: '11.3.0'
 }
