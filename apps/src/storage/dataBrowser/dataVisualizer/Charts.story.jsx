90c8fbb886615edb34193531b6f2f3f3ceedbb9d
molly-moen <molly@code.org>
2022-09-09 13:39:59 -0700

    migrate cross tab chart

--- apps/src/storage/dataBrowser/dataVisualizer/Charts.story.jsx
+++ apps/src/storage/dataBrowser/dataVisualizer/Charts.story.jsx
@@ -2,31 +2,32 @@ import React from 'react';
 import CrossTabChart from './CrossTabChart';
 import _ from 'lodash';
 
-export default storybook =>
-  storybook.storiesOf('Charts', module).add('CrossTab', () => (
-    <div style={{width: '100%', padding: 30}}>
-      <CrossTabChart
-        records={makeFakeRecords()}
-        numericColumns={[]}
-        selectedColumn1="Date"
-        selectedColumn2="Wind Direction"
-        chartTitle="Study of the wind"
-      />
-    </div>
-  ));
+export default {
+  title: 'CrossTabCharts',
+  component: CrossTabChart
+};
 
-const DATES = [
-  '3/4 Wed',
-  '3/5 Thu',
-  '3/6 Fri',
-  '3/7 Sat',
-  '3/8 Sun',
-  '3/9 Mon'
-];
+const Template = args => <CrossTabChart {...args} />;
 
-const DIRECTIONS = ['E', 'NE', 'N', 'Northwest', 'W', 'SW', 'S', 'SE'];
+export const BasicExample = Template.bind({});
+BasicExample.args = {
+  records: makeFakeRecords(),
+  numericColumns: [],
+  selectedColumn1: 'Date',
+  selectedColumn2: 'Wind Direction',
+  chartTitle: 'Study of the wind'
+};
 
 function makeFakeRecords() {
+  const DATES = [
+    '3/4 Wed',
+    '3/5 Thu',
+    '3/6 Fri',
+    '3/7 Sat',
+    '3/8 Sun',
+    '3/9 Mon'
+  ];
+  const DIRECTIONS = ['E', 'NE', 'N', 'Northwest', 'W', 'SW', 'S', 'SE'];
   const result = [];
   for (let i = 0; i < 1000; i++) {
     result.push({
