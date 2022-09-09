7d5827b1e657d4245b17a1789ea8efb2c4a4ba9b
Meg Crenshaw <meg@code.org>
2022-09-09 13:55:49 -0400

    Extract tests to separate helper file

--- dashboard/test/models/data_doc_test.rb
+++ dashboard/test/models/data_doc_test.rb
@@ -7,42 +7,10 @@ class DataDocTest < ActiveSupport::TestCase
     assert data_doc.key
   end
 
-  class KeyConstraintTests < ActiveSupport::TestCase
-    test "data doc key cannot be blank" do
-      assert_raises ActiveRecord::RecordInvalid do
-        DataDoc.create!(key: '', name: 'invalid block')
-      end
-    end
-
-    test "data doc key cannot have invalid characters" do
-      assert_raises ActiveRecord::RecordInvalid do
-        DataDoc.create!(key: 'a space character', name: 'invalid block')
-      end
-    end
-
-    test "data doc key cannot start with a period" do
-      assert_raises ActiveRecord::RecordInvalid do
-        DataDoc.create!(key: '.key', name: 'invalid block')
-      end
-    end
-
-    test "data doc key cannot end with a period" do
-      assert_raises ActiveRecord::RecordInvalid do
-        DataDoc.create!(key: 'key.', name: 'invalid block')
-      end
-    end
-
-    test "data doc key uniqueness ignores casing" do
-      create :data_doc, key: 'myBlock', name: 'invalid block'
-      assert_raises ActiveRecord::RecordInvalid do
-        DataDoc.create!(key: 'myblock', name: 'invalid block')
-      end
-    end
-
-    test "data doc key cannot be `new`" do
-      assert_raises ActiveRecord::RecordInvalid do
-        DataDoc.create!(key: 'new', name: 'invalid block')
-      end
+  test "data doc key uniqueness ignores casing" do
+    create :data_doc, key: 'myBlock', name: 'invalid block'
+    assert_raises ActiveRecord::RecordInvalid do
+      DataDoc.create!(key: 'myblock', name: 'invalid block')
     end
   end
 end
