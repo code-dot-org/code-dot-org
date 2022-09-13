d36be08b49c60adda7edc9e63b744ce0175f1350
Meg Crenshaw <meg@code.org>
2022-09-13 13:23:23 -0400

    Add show action in controller with :key param

--- dashboard/app/controllers/data_docs_controller.rb
+++ dashboard/app/controllers/data_docs_controller.rb
@@ -1,5 +1,5 @@
 class DataDocsController < ApplicationController
-  before_action :require_levelbuilder_mode_or_test_env
+  before_action :require_levelbuilder_mode_or_test_env # TODO [meg]: add except: [:show] to launch
   load_and_authorize_resource
 
   # GET /data_docs/new
@@ -16,5 +16,18 @@ class DataDocsController < ApplicationController
     else
       render :not_acceptable, json: @data_doc.errors
     end
+  end
+
+  def to_param
+    key
+  end
+
+  # GET /data_docs/:key
+  def show
+    @data_doc = DataDoc.find_by(key: params[:key])
+    return render :not_found unless @data_doc
+
+    @data_doc_name = @data_doc.name
+    @data_doc_content = @data_doc.content
   end
 end
