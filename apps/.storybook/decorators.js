c47995432c11e6399d444f6a3e9790a71347e864
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-09-14 09:11:28 -0500

    moved createStore function into story

--- apps/.storybook/decorators.js
+++ apps/.storybook/decorators.js
@@ -1,8 +1,7 @@
 import {createStore, combineReducers} from 'redux';
 import isRtl from '@cdo/apps/code-studio/isRtlRedux';
 import responsive from '@cdo/apps/code-studio/responsiveRedux';
-import {selectGallery} from '@cdo/apps/templates/projects/projectsRedux';
 
 export const reduxStore = (reducers = {}, state = {}) => {
-  return createStore(combineReducers({isRtl, responsive, selectGallery, ...reducers}), state);
+  return createStore(combineReducers({isRtl, responsive, ...reducers}), state);
 };
--- apps/src/templates/projects/ProjectsGallery.story.jsx
+++ apps/src/templates/projects/ProjectsGallery.story.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import ProjectsGallery from './ProjectsGallery';
 import {Galleries} from './projectConstants';
-import {reduxStore} from '@cdo/storybook/decorators';
+import projects, {selectGallery} from './projectsRedux';
+import currentUser from '../currentUserRedux';
+import publishDialog from './publishDialog/publishDialogRedux';
+import deleteDialog from './deleteDialog/deleteProjectDialogRedux';
+import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 
+const createProjectsStore = (reducers = {}, state = {}) => {
+  return createStore(
+    combineReducers({projects, currentUser, publishDialog, deleteDialog}, state)
+  );
+};
+
 const Template = args => (
-  <Provider store={reduxStore()}>
+  <Provider store={createProjectsStore()}>
     <ProjectsGallery canShare limitedGallery {...args} />;
   </Provider>
 );
 
 export const MyProjectsSelected = Template.bind({});
 MyProjectsSelected.args = {
-  selectedGallery: Galleries.PRIVATE
+  selectedGallery: Galleries.PRIVATE,
+  selectGallery
 };
 
 export const PublicGallerySelected = Template.bind({});
 PublicGallerySelected.args = {
-  selectedGallery: Galleries.PUBLIC
+  selectedGallery: Galleries.PUBLIC,
+  selectGallery
 };
