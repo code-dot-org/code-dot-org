16e6a3f1cff8e4da02ee2b49219775293d3dbc0e
Erin Peach <erin@code.org>
2022-09-14 16:16:48 -0700

    Add preview file and refactor AddAssetButtonRow

--- apps/src/code-studio/components/AddAssetButtonRow.story.jsx
+++ apps/src/code-studio/components/AddAssetButtonRow.story.jsx
@@ -13,40 +13,32 @@ const mockApi = {
   }
 };
 
-export default storybook =>
-  storybook.storiesOf('AddAssetButtonRow', module).addStoryTable([
-    {
-      name: 'Just Buttons',
-      story: () => (
-        <div>
-          <AddAssetButtonRow
-            uploadsEnabled={true}
-            allowedExtensions=""
-            api={mockApi}
-            onUploadStart={() => console.log('onUploadStart')}
-            onUploadDone={() => console.log('onUploadDone')}
-            onUploadError={() => console.log('onUploadError')}
-            onSelectRecord={() => console.log('onSelectRecord')}
-            statusMessage=""
-          />
-        </div>
-      )
-    },
-    {
-      name: 'Buttons and status message',
-      story: () => (
-        <div>
-          <AddAssetButtonRow
-            uploadsEnabled={true}
-            allowedExtensions=""
-            api={mockApi}
-            onUploadStart={() => console.log('onUploadStart')}
-            onUploadDone={() => console.log('onUploadDone')}
-            onUploadError={() => console.log('onUploadError')}
-            onSelectRecord={() => console.log('onSelectRecord')}
-            statusMessage="This is a status message"
-          />
-        </div>
-      )
-    }
-  ]);
+export default {
+  title: 'AddAssetButtonRow',
+  component: AddAssetButtonRow
+};
+
+// Template
+const Template = args => (
+  <AddAssetButtonRow
+    uploadsEnabled={true}
+    allowedExtensions=""
+    api={mockApi}
+    onUploadStart={() => console.log('onUploadStart')}
+    onUploadDone={() => console.log('onUploadDone')}
+    onUploadError={() => console.log('onUploadError')}
+    onSelectRecord={() => console.log('onSelectRecord')}
+    {...args}
+  />
+);
+
+// Stories
+export const JustButtons = Template.bind({});
+JustButtons.args = {
+  statusMessage: ''
+};
+
+export const ButtonsWithStatus = Template.bind({});
+ButtonsWithStatus.args = {
+  statusMessage: 'This is a status message'
+};
