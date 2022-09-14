8b7e029581b2371d57154d9fd0e220ef4af2caa9
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-09-13 23:44:30 -0500

    updated decorators and storybook

--- apps/.storybook/decorators.js
+++ apps/.storybook/decorators.js
@@ -1,7 +1,8 @@
 import {createStore, combineReducers} from 'redux';
 import isRtl from '@cdo/apps/code-studio/isRtlRedux';
 import responsive from '@cdo/apps/code-studio/responsiveRedux';
+import {selectGallery} from '@cdo/apps/templates/projects/projectsRedux';
 
 export const reduxStore = (reducers = {}, state = {}) => {
-  return createStore(combineReducers({isRtl, responsive, ...reducers}), state);
+  return createStore(combineReducers({isRtl, responsive, selectGallery, ...reducers}), state);
 };
--- apps/src/templates/projects/ProjectsGallery.story.jsx
+++ apps/src/templates/projects/ProjectsGallery.story.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import ProjectsGallery from './ProjectsGallery';
 import {Galleries} from './projectConstants';
-import projects /*, {selectGallery} */ from './projectsRedux';
-import currentUser from '../currentUserRedux';
-import publishDialog from './publishDialog/publishDialogRedux';
-import deleteDialog from './deleteDialog/deleteProjectDialogRedux';
-import {createStore, combineReducers} from 'redux';
+import {reduxStore} from '@cdo/storybook/decorators';
 import {Provider} from 'react-redux';
 
-const createProjectsStore = function() {
-  return createStore(
-    combineReducers({projects, currentUser, publishDialog, deleteDialog})
-  );
-};
-
 const Template = args => (
-  <Provider store={createProjectsStore()}>
+  <Provider store={reduxStore()}>
     <ProjectsGallery canShare limitedGallery {...args} />;
   </Provider>
 );
 
 export const MyProjectsSelected = Template.bind({});
 MyProjectsSelected.args = {
-  selectGallery: () => {},
   selectedGallery: Galleries.PRIVATE
 };
 
 export const PublicGallerySelected = Template.bind({});
 PublicGallerySelected.args = {
-  selectGallery: () => {},
   selectedGallery: Galleries.PUBLIC
 };
