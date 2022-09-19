4ff6129e2672654c7663701b9851ba1c510d731a
Molly Moen <molly@code.org>
2022-09-19 14:13:13 -0700

    rename code_review_notes (#48136)

--- dashboard/app/controllers/code_review_comments_controller.rb
+++ dashboard/app/controllers/code_review_comments_controller.rb
@@ -1,13 +1,13 @@
-class CodeReviewNotesController < ApplicationController
+class CodeReviewCommentsController < ApplicationController
   before_action :authenticate_user!
 
-  load_and_authorize_resource :code_review_note, only: [:update, :destroy]
+  load_and_authorize_resource :code_review_comment, only: [:update, :destroy]
 
-  # POST /code_review_notes
+  # POST /code_review_comments
   def create
     params.require([:comment, :codeReviewId])
 
-    @code_review_note = CodeReviewNote.new(
+    @code_review_comment = CodeReviewComment.new(
       {
         comment: params[:comment],
         code_review_request_id: params[:codeReviewId],
@@ -18,26 +18,26 @@ class CodeReviewNotesController < ApplicationController
 
     # We wait to authorize until this point because we need to know
     # who owns the project that the comment is associated with.
-    authorize! :create, @code_review_note
-    @code_review_note.save!
-    render json: @code_review_note.summarize
+    authorize! :create, @code_review_comment
+    @code_review_comment.save!
+    render json: @code_review_comment.summarize
   end
 
-  # PATCH /code_review_notes/:id
+  # PATCH /code_review_comments/:id
   # Currently, updating is_resolved the note is the only allowed update.
   def update
     params.require(:id)
 
     if params[:isResolved]
-      @code_review_note.update!(is_resolved: params[:isResolved])
+      @code_review_comment.update!(is_resolved: params[:isResolved])
     end
 
-    render json: @code_review_note.summarize
+    render json: @code_review_comment.summarize
   end
 
-  # DELETE /code_review_notes/:id
+  # DELETE /code_review_comments/:id
   def destroy
-    if @code_review_note.delete
+    if @code_review_comment.delete
       return head :ok
     else
       return head :bad_request
