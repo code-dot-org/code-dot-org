a2d8cda86d5a1d5ef25d30681907a27202af36f0
Meg Crenshaw <meg@code.org>
2022-09-20 09:37:15 -0400

    Use asset in CopyrightInfo

--- apps/src/templates/CopyrightInfo.jsx
+++ apps/src/templates/CopyrightInfo.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
 import i18n from '@cdo/locale';
+import CreativeCommons from '@cdo/static/creative-commons-by-nc-sa.png';
 
-const CopyrightInfo = () => (
-  <div>
-    <a href="https://creativecommons.org/">
-      <img
-        src="https://curriculum.code.org/static/img/creativeCommons-by-nc-sa.png"
-        alt="This curriculum is available under a Creative Commons License (CC BY-NC-SA 4.0)."
+const CopyrightInfo = () => {
+  const licenseURL = 'http://creativecommons.org/licenses/by-nc-sa/4.0/';
+
+  return (
+    <div>
+      <a rel="license" href={licenseURL}>
+        <img
+          style={{height: 30, margin: '10px 10px 5px 0'}}
+          src={CreativeCommons}
+          alt="Creative Commons License (CC BY-NC-SA 4.0)."
+        />
+      </a>
+      This work is licensed under a{' '}
+      <a rel="license" href={licenseURL}>
+        Creative Commons BY-NC-SA 4.0 International License
+      </a>
+      .
+      <SafeMarkdown
+        markdown={i18n.licenseMaterials({link: 'https://code.org/contact'})}
       />
-    </a>
-    <SafeMarkdown
-      markdown={i18n.licenseMaterials({link: 'https://code.org/contact'})}
-    />
-  </div>
-);
+    </div>
+  );
+};
 
 export default CopyrightInfo;
