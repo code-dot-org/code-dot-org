16e38e8ae26b7f91cd4a754113e8e393fabc0ba3
Meg Crenshaw <meg@code.org>
2022-09-22 13:30:03 -0400

    Add serialization to data doc model

--- dashboard/app/models/data_doc.rb
+++ dashboard/app/models/data_doc.rb
@@ -19,4 +19,29 @@ class DataDoc < ApplicationRecord
 
   validates_uniqueness_of :key, case_sensitive: false
   validate :validate_key_format
+
+  def file_path
+    Rails.root.join("config/data_docs/#{key.parameterize(preserve_case: false)}.json")
+  end
+
+  def serialize
+    {
+      key: key,
+      name: name,
+      content: content
+    }
+  end
+
+  # writes data doc to a seed file in the config directory
+  def write_serialization
+    return unless Rails.application.config.levelbuilder_mode
+    directory_name = File.dirname(file_path)
+    Dir.mkdir(directory_name) unless File.exist?(directory_name)
+    File.write(file_path, JSON.pretty_generate(serialize))
+  end
+
+  def remove_serialization
+    return unless Rails.application.config.levelbuilder_mode
+    File.delete(file_path) if File.exist?(file_path)
+  end
 end
