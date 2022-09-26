d97ffe4f49744058994fa7780b842272d4a0ab45
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-09-26 12:55:56 -0700

    pin to the same version of rubygems as is installed by apt

--- cookbooks/cdo-ruby/test/integration/default/serverspec/ruby_spec.rb
+++ cookbooks/cdo-ruby/test/integration/default/serverspec/ruby_spec.rb
@@ -2,5 +2,5 @@ require_relative '../../../kitchen/data/helper_spec'
 
 file_exist '/usr/bin/ruby2.6'
 cmd 'ruby -v', 'ruby 2.6'
-cmd 'gem -v', '3.3.21'
+cmd 'gem -v', '3.1.6'
 cmd 'bundler -v', '1.17.3'
