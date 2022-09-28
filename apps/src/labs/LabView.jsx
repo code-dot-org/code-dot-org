a7269187e182816aec7bbaf149a36cd2f8ae3023
molly-moen <molly@code.org>
2022-09-28 16:44:55 -0700

    work in progress

--- apps/src/labs/LabView.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function LabView() {
-  return <div>Hello from lab lab</div>;
-}
--- apps/webpack.js
+++ apps/webpack.js
@@ -70,7 +70,7 @@ const nodePolyfillConfig = {
 var baseConfig = {
   plugins: [...nodePolyfillConfig.plugins],
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     fallback: {...nodePolyfillConfig.resolve.fallback},
     alias: {
       '@cdo/locale': path.resolve(
@@ -219,6 +219,11 @@ var baseConfig = {
           cacheDirectory: path.resolve(__dirname, '.babel-cache'),
           compact: false
         }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       }
     ],
     noParse: [/html2canvas/]
