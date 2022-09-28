12ecc4e7bd4de11a80ac2df2268bc10a000b4615
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2022-09-28 16:36:46 -0700

    Round block (#48359)

--- apps/src/music/blockly/blocks/math.js
+++ apps/src/music/blockly/blocks/math.js
@@ -24,3 +24,54 @@ export const number = {
     return [code, order];
   }
 };
+
+export const round = {
+  definition: {
+    type: BlockTypes.ROUND,
+    message0: '%1 %2',
+    args0: [
+      {
+        type: 'field_dropdown',
+        name: 'OP',
+        options: [
+          ['%{BKY_MATH_ROUND_OPERATOR_ROUND}', 'ROUND'],
+          ['%{BKY_MATH_ROUND_OPERATOR_ROUNDUP}', 'ROUNDUP'],
+          ['%{BKY_MATH_ROUND_OPERATOR_ROUNDDOWN}', 'ROUNDDOWN']
+        ]
+      },
+      {
+        type: 'input_value',
+        name: 'NUM',
+        check: 'Number'
+      }
+    ],
+    output: 'Number',
+    style: 'math_blocks',
+    helpUrl: '%{BKY_MATH_ROUND_HELPURL}',
+    tooltip: '%{BKY_MATH_ROUND_TOOLTIP}'
+  },
+  generator: ctx => {
+    const operator = ctx.getFieldValue('OP');
+    let code;
+    const arg =
+      Blockly.JavaScript.valueToCode(
+        ctx,
+        'NUM',
+        Blockly.JavaScript.ORDER_NONE
+      ) || '0';
+
+    switch (operator) {
+      case 'ROUND':
+        code = 'Math.round(' + arg + ')';
+        break;
+      case 'ROUNDUP':
+        code = 'Math.ceil(' + arg + ')';
+        break;
+      case 'ROUNDDOWN':
+        code = 'Math.floor(' + arg + ')';
+        break;
+    }
+
+    return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+  }
+};
