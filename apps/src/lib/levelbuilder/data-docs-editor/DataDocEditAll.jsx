6b939ea3e0d022cd64ab43605dd6857f05d756f6
Turner Riley <turnertriley@gmail.com>
2022-09-29 15:46:31 -0700

    add edit all table with working edit functionality

--- apps/src/lib/levelbuilder/data-docs-editor/DataDocEditAll.jsx
+++ apps/src/lib/levelbuilder/data-docs-editor/DataDocEditAll.jsx
@@ -1,22 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from '@dsco_/link';
+import {Link, TextLink} from '@dsco_/link';
+import FontAwesome from '@cdo/apps/templates/FontAwesome';
 
 const DataDocEditAll = ({dataDocs}) => {
+  const initiateDeleteDataDoc = docKeyToDelete => {
+    console.log('Doc to delete: ' + docKeyToDelete);
+  };
+
   return (
     <div>
-      <p>Data Docs Edit All</p>
-      <ul>
-        {dataDocs.map(
-          ({key, name, content}) =>
-            name &&
-            content && (
-              <li key={key}>
+      <h1 style={{marginBottom: 30}}>Data Docs</h1>
+      <div className="page-actions">
+        <TextLink
+          className="create-btn"
+          icon={<FontAwesome icon="plus" />}
+          iconBefore={true}
+          href={'/data_docs/new'}
+          text="Create New Data Doc"
+        />
+      </div>
+      <div className="guides-table">
+        <span className="header">Actions</span>
+        <span className="header">Data Docs</span>
+        {dataDocs.map(({key, name, content}) => {
+          return [
+            <React.Fragment key={`${key}_row`}>
+              <div className="actions-box">
+                <TextLink
+                  icon={<FontAwesome icon={'pencil-square-o'} title={'edit'} />}
+                  href={`/data_docs/${key}/edit`}
+                />
+                <TextLink
+                  icon={<FontAwesome icon={'trash'} title={'delete'} />}
+                  onClick={() => initiateDeleteDataDoc(key)}
+                />
+              </div>
+              <div className="guide-box">
                 <Link href={`/data_docs/${key}`}>{name}</Link>
-              </li>
-            )
-        )}
-      </ul>
+              </div>
+            </React.Fragment>
+          ];
+        })}
+      </div>
     </div>
   );
 };
