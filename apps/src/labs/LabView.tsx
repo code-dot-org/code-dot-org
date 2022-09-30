3f7935cdc1ffe5e916336266d859ffbb23a62e20
Madelyn Kasula <maddie.kasula@code.org>
2022-09-30 14:59:11 -0700

    WIP: Add some interfaces; load Level from backend

--- apps/src/labs/LabView.tsx
+++ apps/src/labs/LabView.tsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
-export default function LabView() {
-  return <div>Hello from lab lab</div>;
+// ingest appOptions from backend into AppOptions type
+
+interface Editor {
+  run: () => String
 }
+
+class CodeMirror implements Editor {
+  run() {
+    return 'codemirror'
+  }
+}
+
+class Droplet implements Editor {
+  run() {
+    return 'droplet'
+  }
+}
+
+interface Lab {
+  editor: Editor
+}
+
+class Applab implements Lab {
+  editor: Editor
+
+  constructor() {
+    this.editor = new Droplet()
+  }
+}
+
+class Javalab implements Lab {
+  editor: Editor
+
+  constructor() {
+    this.editor = new CodeMirror()
+  }
+}
+
+const LabView: FunctionComponent = () => {
+  return <div>Hello from lab lab</div>
+}
+
+export default LabView
--- apps/src/sites/studio/pages/labs/show.js
+++ apps/src/sites/studio/pages/labs/show.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {getStore} from '../../../../code-studio/redux';
 import LabView from '../../../../labs/LabView';
+import getScriptData from '../../../../util/getScriptData';
 
 $(document).ready(function() {
+  const level = getScriptData('data-level');
+
   ReactDOM.render(
     <Provider store={getStore()}>
       <LabView />
