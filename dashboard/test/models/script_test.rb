d02a7dd3c7a53bb6afffffa890c99e5125e7f28c
David Bailey <davidsbailey@users.noreply.github.com>
2022-09-30 16:37:06 -0700

    fix unit family name redirects

--- dashboard/test/models/script_test.rb
+++ dashboard/test/models/script_test.rb
@@ -71,7 +71,7 @@ class ScriptTest < ActiveSupport::TestCase
     UnitGroup.course_cache
 
     CourseVersion.stubs(:should_cache?).returns true
-    CourseVersion.course_offering_keys('Script')
+    CourseVersion.course_offering_keys('Unit')
 
     CourseOffering.all.pluck(:key).each do |key|
       CourseOffering.get_from_cache(key)
