115b88969cc7d4fb6c7ff602b21b2117709e2af2
Erin Peach <erin@code.org>
2022-10-06 16:41:04 -0700

    Remove duplicate UI

--- apps/src/code-studio/assets/SpriteManagementDirectory.jsx
+++ apps/src/code-studio/assets/SpriteManagementDirectory.jsx
@@ -33,59 +33,45 @@ export default function SpriteManagementDirectory() {
 
   return (
     <div>
-      <h1>Manage Sprite Lab Sprites</h1>
-      <h3>
-        <a href="/sprites/sprite_upload">Upload New Sprites</a>
-      </h3>
-      <h3>
-        <a href="/sprites/default_sprites_editor">Edit Default Sprites</a>
-      </h3>
-      <h3>
+      <h1>Manage Sprite Lab Assets</h1>
+      <p>This is an internal set of tools for Code.org content authors.</p>
+      <h2>New Animations</h2>
+      <li>
+        <a href="/sprites/sprite_upload">Upload New Animations</a>
+      </li>
+      <p>
+        <br />
+        Make new costumes and backgrounds available in Sprite Lab projects and
+        curriculum levels.
+      </p>
+      <h2>Animation JSON</h2>
+      <li>
         <a href="/sprites/select_start_animations">
-          Select Start Animations From Library
+          Generate Animation JSON From Library Animations only
         </a>
-      </h3>
-      <div>
-        <h1>Manage Sprite Lab Assets</h1>
-        <p>This is an internal set of tools for Code.org content authors.</p>
-        <h2>New Animations</h2>
-        <li>
-          <a href="/sprites/sprite_upload">Upload New Animations</a>
-        </li>
-        <p>
-          <br />
-          Make new costumes and backgrounds available in Sprite Lab projects and
-          curriculum levels.
-        </p>
-        <h2>Animation JSON</h2>
-        <li>
-          <a href="/sprites/select_start_animations">
-            Generate Animation JSON From Library Animations only
-          </a>
-        </li>
-        <li>
-          <a href="/sprites/select_start_animations?library=all">
-            Generate Animation JSON From Library AND Level-Specific Animations
-          </a>{' '}
-          (Slower)
-        </li>
-        <p>
-          <br />
-          For level builders. Generate Animation JSON that can be copied and
-          pasted into curriculum levels.
-        </p>
-        <h2>Default Animations</h2>
-        <li>
-          <a href="/sprites/default_sprites_editor">
-            Edit Default Costumes and Backgrounds
-          </a>
-        </li>
-        <p>
-          <br />
-          Updates the default set of costumes and backgrounds featured in most
-          Sprite Lab projects.
-        </p>
-      </div>
+      </li>
+      <li>
+        <a href="/sprites/select_start_animations?library=all">
+          Generate Animation JSON From Library AND Level-Specific Animations
+        </a>{' '}
+        (Slower)
+      </li>
+      <p>
+        <br />
+        For level builders. Generate Animation JSON that can be copied and
+        pasted into curriculum levels.
+      </p>
+      <h2>Default Animations</h2>
+      <li>
+        <a href="/sprites/default_sprites_editor">
+          Edit Default Costumes and Backgrounds
+        </a>
+      </li>
+      <p>
+        <br />
+        Updates the default set of costumes and backgrounds featured in most
+        Sprite Lab projects.
+      </p>
       <div style={styles.pageBreak}>
         <Button
           text="Release Changes to Production"
