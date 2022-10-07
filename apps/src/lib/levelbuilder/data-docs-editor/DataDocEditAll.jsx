e4cd1b20f2b0243c4e4a4e5fb8a2b228d7bcafb3
Turner Riley <56283563+TurnerRiley@users.noreply.github.com>
2022-10-07 09:54:01 -0700

    Edit all data doc backend (#48433)
    * get working landing page for edit all
    
    * add edit all table with working edit functionality
    
    * merge in renaming of reference_guides styling
    
    * add edit_all delete functionality
    
    * use renamed styling file
    
    * add backend unit tests
    
    * split PR into backend and frontend
    
    * remove changes to apps/package
    
    * add status response for destroy action
    
    * add destroy permissions tests
    
    * consolidate data_docs routes in file
    
    * use destroy!
    
    * remove :only since all routes are used
    
    * fix removal of :only in routes
    
    * simplify destroy function by removing unreachable branch
    
    * add unit test for deleting non existent data doc

---
+++ apps/src/lib/levelbuilder/data-docs-editor/DataDocEditAll.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DataDocEditAll = props => {
+  return (
+    <div>
+      <h1 style={{marginBottom: 30}}>Data Docs Edit All Page</h1>
+    </div>
+  );
+};
+
+export default DataDocEditAll;
