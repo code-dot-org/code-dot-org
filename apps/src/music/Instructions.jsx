4091a83d981e645169fe473914413ad1389d7b22
breville <brendan@code.org>
2022-10-11 06:09:06 +0000

    Music prototype: instructions

---
+++ apps/src/music/Instructions.jsx
@@ -0,0 +1,95 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class Instructions extends React.Component {
+  static propTypes = {
+    instructions: PropTypes.object,
+    baseUrl: PropTypes.string.isRequired
+  };
+
+  state = {
+    currentPanel: 0
+  };
+
+  changePanel = nextPanel => {
+    if (
+      nextPanel &&
+      this.state.currentPanel + 1 <
+        this.props.instructions?.groups[0].panels.length
+    ) {
+      this.setState({currentPanel: this.state.currentPanel + 1});
+    } else if (!nextPanel && this.state.currentPanel > 0) {
+      this.setState({currentPanel: this.state.currentPanel - 1});
+    }
+  };
+
+  render() {
+    const {instructions, baseUrl} = this.props;
+
+    return (
+      <div
+        id="instructions"
+        style={{
+          backgroundColor: '#222',
+          width: '100%',
+          height: '100%',
+          borderRadius: 4,
+          backgroundSize: '100% 200%',
+          padding: 10,
+          boxSizing: 'border-box',
+          position: 'relative'
+        }}
+      >
+        {instructions?.groups[0].panels.map((panel, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                position: 'absolute',
+                top: 10,
+                opacity: index === this.state.currentPanel ? 1 : 0
+              }}
+            >
+              {panel.imageSrc && (
+                <div style={{float: 'left'}}>
+                  <img
+                    src={
+                      baseUrl +
+                      instructions.groups[0].path +
+                      '/' +
+                      panel.imageSrc
+                    }
+                    style={{height: 70, marginRight: 10}}
+                  />
+                </div>
+              )}
+              <div style={{float: 'left'}}>{panel.text}</div>
+            </div>
+          );
+        })}
+        <div
+          style={{
+            bottom: 4,
+            right: 4,
+            position: 'absolute'
+          }}
+        >
+          <button
+            type="button"
+            onClick={() => this.changePanel(false)}
+            style={{fontSize: 13, padding: '4px 8px'}}
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            onClick={() => this.changePanel(true)}
+            style={{fontSize: 13, padding: '4px 8px'}}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
