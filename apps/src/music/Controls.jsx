e78995b08f5be435eccb8e06278ed0c684953b5d
Sanchit Malhotra <sanchit@code.org>
2022-10-12 12:14:17 -0700

    Tweak beat pad UI, make closeable

--- apps/src/music/Controls.jsx
+++ apps/src/music/Controls.jsx
@@ -1,55 +1,89 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import classNames from 'classnames';
 import FontAwesome from '../templates/FontAwesome';
 import {Triggers} from '@cdo/apps/music/constants';
 import moduleStyles from './controls.module.scss';
 import BeatPad from './BeatPad';
 
-export default class Controls extends React.Component {
-  static propTypes = {
-    isPlaying: PropTypes.bool.isRequired,
-    setPlaying: PropTypes.func.isRequired,
-    playTrigger: PropTypes.func.isRequired,
-    top: PropTypes.bool.isRequired
-  };
+const Controls = ({isPlaying, setPlaying, playTrigger, top}) => {
+  const [isShowingBeatBad, setBeatPadShowing] = useState(false);
+  useEffect(() => {
+    if (isPlaying) {
+      setBeatPadShowing(true);
+    }
+  }, [isPlaying]);
 
-  renderBeatPad = () => {
+  const renderBeatPad = () => {
     return (
       <div
         style={{
           position: 'absolute',
-          [this.props.top ? 'bottom' : 'top']: -175,
+          [top ? 'bottom' : 'top']: -175,
           right: 10
         }}
       >
         <BeatPad
           triggers={Triggers}
-          playTrigger={this.props.playTrigger}
+          playTrigger={playTrigger}
           onClose={() => {
-            console.log('TODO close Beat Pad');
+            setBeatPadShowing(false);
           }}
         />
       </div>
     );
   };
 
-  render() {
-    const {isPlaying, setPlaying} = this.props;
-
-    return (
-      <div id="controls" className={moduleStyles.controlsContainer}>
-        {isPlaying && this.renderBeatPad()}
-        <br />
+  return (
+    <div id="controls" className={moduleStyles.controlsContainer}>
+      {isPlaying && isShowingBeatBad && renderBeatPad()}
+      {isPlaying && (
+        // Placeholder button area, currently hidden
         <div
-          className={moduleStyles.playPauseButton}
-          onClick={() => setPlaying(!isPlaying)}
+          className={classNames(
+            moduleStyles.controlButtons,
+            moduleStyles.side,
+            moduleStyles.hide
+          )}
         >
           <FontAwesome
-            icon={isPlaying ? 'stop-circle' : 'play-circle'}
+            icon={'th'}
             style={{fontSize: 30}}
+            className={moduleStyles.iconButton}
           />
         </div>
+      )}
+      <div
+        className={classNames(moduleStyles.controlButtons, moduleStyles.center)}
+      >
+        <FontAwesome
+          icon={isPlaying ? 'stop-circle' : 'play-circle'}
+          style={{fontSize: 30}}
+          onClick={() => setPlaying(!isPlaying)}
+          className={moduleStyles.iconButton}
+        />
       </div>
-    );
-  }
-}
+      {isPlaying && (
+        <div
+          className={classNames(moduleStyles.controlButtons, moduleStyles.side)}
+        >
+          <FontAwesome
+            icon={'th'}
+            style={{fontSize: 30}}
+            onClick={() => setBeatPadShowing(!isShowingBeatBad)}
+            className={moduleStyles.iconButton}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+Controls.propTypes = {
+  isPlaying: PropTypes.bool.isRequired,
+  setPlaying: PropTypes.func.isRequired,
+  playTrigger: PropTypes.func.isRequired,
+  top: PropTypes.bool.isRequired
+};
+
+export default Controls;
