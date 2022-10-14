346297526885a55ffc2885149b83a9362b44a150
rshipp <ryan.shipp@code.org>
2022-10-14 14:45:45 -0500

    Fix dropdown icon style, remove js styles

--- apps/src/templates/DropdownButton.js
+++ apps/src/templates/DropdownButton.js
@@ -1,45 +1,9 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import color from '@cdo/apps/util/color';
 import Button from '@cdo/apps/templates/Button';
 import onClickOutside from 'react-onclickoutside';
+import classNames from 'classnames';
 import moduleStyles from './dropdown-button.module.scss';
-
-const styles = {
-  main: {
-    display: 'inline-block'
-  },
-  icon: {
-    fontSize: 24,
-    // we want our icon text to be a different size than our button text, which
-    // requires we manually offset to get it centered properly
-    position: 'relative',
-    top: 3
-  },
-  dropdown: {
-    border: `1px solid ${color.charcoal}`,
-    position: 'absolute',
-    // without this, this will be below some content, such as ProgressBubble.
-    zIndex: 1000
-  },
-  anchor: {
-    padding: 10,
-    color: color.charcoal,
-    backgroundColor: color.white,
-    fontFamily: '"Gotham 5r", sans-serif',
-    display: 'block',
-    textDecoration: 'none',
-    lineHeight: '20px',
-    transition: 'background-color .2s ease-out',
-    ':hover': {
-      backgroundColor: color.lightest_gray,
-      cursor: 'pointer'
-    }
-  },
-  nonFirstAnchor: {
-    borderTop: `1px solid ${color.charcoal}`
-  }
-};
 
 /**
  * A button that drops down to a set of clickable links, and closes itself if
@@ -122,7 +86,7 @@ export const DropdownButton = class DropdownButtonComponent extends Component {
           size={size}
           onClick={this.toggleDropdown}
           icon={dropdownOpen ? 'caret-up' : 'caret-down'}
-          iconStyle={styles.icon}
+          iconClassName={moduleStyles.icon}
           color={color}
           className={this.props.className}
         >
@@ -141,9 +105,11 @@ export const DropdownButton = class DropdownButtonComponent extends Component {
                 {...child.props}
                 onClick={event => this.onClickChild(event, child.props)}
                 key={index}
-                className={moduleStyles.anchor}
+                className={classNames(
+                  moduleStyles.anchor,
+                  index > 0 && moduleStyles.nonFirstAnchor
+                )}
                 style={{
-                  ...(index > 0 && styles.nonFirstAnchor),
                   ...child.props.style
                 }}
               />
