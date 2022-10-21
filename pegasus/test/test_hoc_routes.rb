81fb9debcd041c4b9672e01ab329c2eeacca8b91
David Bailey <davidsbailey@users.noreply.github.com>
2022-10-21 11:52:14 -0700

    preserve query string when redirecting congrats page

--- pegasus/test/test_hoc_routes.rb
+++ pegasus/test/test_hoc_routes.rb
@@ -81,6 +81,13 @@ class HocRoutesTest < Minitest::Test
       assert_equal expected_url, @pegasus.last_response['Location']
     end
 
+    it 'preserves url params when redirecting congrats page' do
+      @pegasus.get CDO.code_org_url('/congrats?i=foo&s=bar')
+      assert_equal 302, @pegasus.last_response.status
+      expected_url = CDO.studio_url('/congrats?i=foo&s=bar', CDO.default_scheme)
+      assert_equal expected_url, @pegasus.last_response['Location']
+    end
+
     it 'starts and ends given tutorial, tracking company and tutorial' do
       DB.transaction(rollback: :always) do
         with_test_company 'testcompany'
