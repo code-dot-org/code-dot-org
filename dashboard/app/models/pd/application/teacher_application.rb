b1948b07fc1376d42c7ae6c2b6365a4c8470005d
Meg Crenshaw <meg@code.org>
2022-10-25 18:01:33 -0400

    Remove facilitator mention in teacher app

--- dashboard/app/models/pd/application/teacher_application.rb
+++ dashboard/app/models/pd/application/teacher_application.rb
@@ -80,7 +80,6 @@ module Pd::Application
     has_many :emails, class_name: 'Pd::Application::Email', foreign_key: 'pd_application_id'
 
     before_validation :set_course_from_program, if: -> {form_data_changed?}
-    validates :status, exclusion: {in: ['interview'], message: '%{value} is reserved for facilitator applications.'}
     validates :course, presence: true, inclusion: {in: VALID_COURSES}, unless: -> {status == 'incomplete'}
     validate :workshop_present_if_required_for_status, if: -> {status_changed?}
 
