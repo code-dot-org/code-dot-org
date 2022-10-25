4be44883f2b654b7fc765cbdc49c1765baf9d37a
David Bailey <davidsbailey@users.noreply.github.com>
2022-10-25 15:09:46 -0700

    fix migration

--- dashboard/db/migrate/20221024233909_create_pd_applications_status_log.rb
+++ dashboard/db/migrate/20221024233909_create_pd_applications_status_log.rb
@@ -3,7 +3,7 @@ class CreatePdApplicationsStatusLog < ActiveRecord::Migration[6.0]
     create_table :pd_applications_status_logs do |t|
       t.integer :pd_application_id, null: false
       t.string :status, null: false
-      t.timestamp :timestamp, null: false
+      t.datetime :timestamp, null: false
       t.integer :position, null: false
     end
   end
