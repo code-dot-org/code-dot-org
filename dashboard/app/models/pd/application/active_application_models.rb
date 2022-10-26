0dc65e9670a5627f87e121f466194590379a13ca
Meg Crenshaw <meg@code.org>
2022-10-25 23:38:53 -0400

    Update application models

--- dashboard/app/models/pd/application/active_application_models.rb
+++ dashboard/app/models/pd/application/active_application_models.rb
@@ -11,14 +11,11 @@ module Pd
       # Active (this year's) application classes and factories
       TEACHER_APPLICATION_CLASS = TeacherApplication
       PRINCIPAL_APPROVAL_APPLICATION_CLASS = PrincipalApprovalApplication
-      FACILITATOR_APPLICATION_CLASS = Facilitator1920Application
 
       TEACHER_APPLICATION_FACTORY = :pd_teacher_application
       TEACHER_APPLICATION_HASH_FACTORY = :pd_teacher_application_hash
       PRINCIPAL_APPROVAL_FACTORY = :pd_principal_approval_application
       PRINCIPAL_APPROVAL_HASH_FACTORY = :pd_principal_approval_application_hash
-      FACILITATOR_APPLICATION_FACTORY = :pd_facilitator1920_application
-      FACILITATOR_APPLICATION_HASH_FACTORY = :pd_facilitator1920_application_hash
 
       FIT_WEEKEND_REGISTRATION_CLASS = Pd::FitWeekend1920Registration
 
