8f5e65fcc55a367f36170193f78247d81f439667
Bethany Connor <bethany@code.org>
2022-10-28 10:11:19 -0400

    Speculative fix for bug in weblab footer

--- dashboard/public/weblab/footer.js
+++ dashboard/public/weblab/footer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77561bc0aaaff298c949abcbffd627bdb0de330b4995eac5792ea8665ee4a46f
-size 1140
+oid sha256:94b6be75d655f96a5a6519381a0bbe32a62ef525d83262874bcb7f2eb9a15d68
+size 1281
