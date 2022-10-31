15cb5ac1d927ae8b078df74d3d587ce12ea56ce2
Mario Gil Correa <66776217+mgc1194@users.noreply.github.com>
2022-10-31 17:01:09 -0500

    Fnd 2075 i18n maker toolkit setup (#48344)
    * markdown content moved from haml to jsx
    
    * markdown content moved from haml to jsx
    
    * refactor classes into functions
    
    * styling fixed
    
    * porpTypes definition moved
    
    * refactoring description component
    
    * style name changed to descriptionFlexCard
    
    * i18n tests added
    
    * i18n tests modified for classes
    
    * i18n tests modified for experiments
    
    * refactored Setup Guide components
    
    * reverted unnecessary changes in ui test helpers
    
    * removed unused styles
    
    * first attempt to testing
    
    * renaming of files and components
    
    * completed tests for translatable content
    
    * make stub i18n test
    
    * adding button text to i18n
    
    * wrapping stubs in befor-after blocks
    
    * removing exclamation point from tested strings
    
    * unmerging staging
    
    * change before-after to beforeEach-afterEach

---
+++ apps/src/lib/kits/maker/ui/SetupGuide.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import experiments from '@cdo/apps/util/experiments';
+import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
+import SetupInstructions from '@cdo/apps/lib/kits/maker/ui/SetupInstructions';
+import applabI18n from '@cdo/applab/locale';
+
+const style = {
+  twoColumns: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  descriptionFlexCard: {
+    width: '45%'
+  },
+  circuitPlaygroundImg: {
+    float: 'right',
+    margin: '0 0 15px 10px',
+    borderRadius: '50%'
+  },
+  microbitImg: {
+    float: 'right',
+    margin: '0 0 15px 10px'
+  }
+};
+
+export default class SetupGuide extends React.Component {
+  setupGuideContent = content => {
+    switch (content) {
+      case 'microbit':
+        return {
+          id: 'microbit-description',
+          title: applabI18n.makerSetupMicrobitTitle(),
+          href: 'https://microbit.org/',
+          imgSrc: '../assets/maker/microbit-drawing-green.png',
+          description: applabI18n.makerSetupMicrobitDescription(),
+          imgStyle: style.microbitImg
+        };
+      case 'circuitPlayground':
+        return {
+          id: 'circuit-playground-description',
+          title: applabI18n.makerSetupCircuitPlaygroundTitle(),
+          href:
+            'https://learn.adafruit.com/introducing-circuit-playground/overview',
+          imgSrc: '../assets/maker/circuit-playground-200.jpg',
+          description: applabI18n.makerSetupCircuitPlaygroundDescription(),
+          imgStyle: style.circuitPlaygroundImg
+        };
+    }
+  };
+  render() {
+    // Experiment 'microbit', displays Circuit Playground and Micro:Bit descriptions.
+    let isMicrobit = experiments.isEnabled('microbit');
+    return (
+      <div>
+        <h1>{applabI18n.makerSetupPageTitle()}</h1>
+        {isMicrobit ? (
+          <div style={style.twoColumns}>
+            <DescriptionCard
+              {...this.setupGuideContent('circuitPlayground')}
+              divStyle={style.descriptionFlexCard}
+            />
+            <DescriptionCard
+              {...this.setupGuideContent('microbit')}
+              divStyle={style.descriptionFlexCard}
+            />
+          </div>
+        ) : (
+          <DescriptionCard {...this.setupGuideContent('circuitPlayground')} />
+        )}
+        <div id="setup-status-mount">
+          <SetupInstructions />
+        </div>
+      </div>
+    );
+  }
+}
+
+function DescriptionCard(props) {
+  return (
+    <div id={props.id} style={props.divStyle}>
+      <h2>{props.title}</h2>
+      <center>
+        <a href={props.href}>
+          <img src={props.imgSrc} width={200} style={props.imgStyle} />
+        </a>
+      </center>
+      <div className="description-content">
+        <SafeMarkdown markdown={props.description} />
+      </div>
+    </div>
+  );
+}
+DescriptionCard.propTypes = {
+  id: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  imgStyle: PropTypes.object,
+  description: PropTypes.string.isRequired,
+  divStyle: PropTypes.object
+};
