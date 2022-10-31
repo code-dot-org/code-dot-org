896199be6885bf588d861fb3de7810d685396e66
Bethany Connor <bethany@code.org>
2022-10-31 09:47:30 -0400

    Fix typo

--- dashboard/public/weblab/footer.js
+++ dashboard/public/weblab/footer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94b6be75d655f96a5a6519381a0bbe32a62ef525d83262874bcb7f2eb9a15d68
-size 1281
+oid sha256:8cbd5c49cea460e0a2f410b7831bc2fe65b10c28ac7c8e527f51190504c9f507
+size 1283
