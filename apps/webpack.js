20eb811e36c7edbc3dd45b3276916a93db5cc11a
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-11-01 13:13:53 -0500

    resolved to firmata 2.2.0, remove johnny-five-deprecated, add firmata to packages that ship in ES6

--- apps/webpack.js
+++ apps/webpack.js
@@ -20,6 +20,7 @@ var toTranspileWithinNodeModules = [
   path.resolve(__dirname, 'node_modules', '@code-dot-org', 'dance-party'),
   path.resolve(__dirname, 'node_modules', '@code-dot-org', 'johnny-five'),
   path.resolve(__dirname, 'node_modules', '@code-dot-org', 'remark-plugins'),
+  path.resolve(__dirname, 'node_modules', 'firmata'),
   // parse5 ships in ES6: https://github.com/inikulin/parse5/issues/263#issuecomment-410745073
   path.resolve(__dirname, 'node_modules', 'parse5'),
   path.resolve(__dirname, 'node_modules', 'vmsg'),
@@ -149,7 +150,8 @@ var baseConfig = {
       '@cdo/apps': path.resolve(__dirname, 'src'),
       '@cdo/static': path.resolve(__dirname, 'static'),
       repl: path.resolve(__dirname, 'src/noop'),
-      '@cdo/storybook': path.resolve(__dirname, '.storybook')
+      '@cdo/storybook': path.resolve(__dirname, '.storybook'),
+      serialport: false
     }
   },
   module: {
@@ -306,8 +308,8 @@ function storybookConfig(sbConfig) {
           envConstants.NODE_ENV || 'development'
         ),
         PISKEL_DEVELOPMENT_MODE: JSON.stringify(false)
-      }),
-      new webpack.IgnorePlugin({resourceRegExp: /^serialport$/})
+      })
+      //new webpack.IgnorePlugin({resourceRegExp: /^serialport$/})
     ]
   };
 }
@@ -464,7 +466,7 @@ function create(options) {
         PISKEL_DEVELOPMENT_MODE: JSON.stringify(piskelDevMode),
         DEBUG_MINIFIED: envConstants.DEBUG_MINIFIED || 0
       }),
-      new webpack.IgnorePlugin({resourceRegExp: /^serialport$/}),
+      //new webpack.IgnorePlugin({resourceRegExp: /^serialport$/}),
       ...plugins
     ],
     watch: watch,
