88206f72d7c20ad6d1bb6fc660d3b06d03bd125d
Ryan Shipp <1382374+rshipp@users.noreply.github.com>
2022-11-03 14:17:53 -0500

    Revert "Add Make a Copy feature for Resources (#48642)" (#48990)
    This reverts commit 1fa9b92aba68239934c48f112a614308aa2a9657.

--- apps/src/templates/DropdownButton.story.jsx
+++ apps/src/templates/DropdownButton.story.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import DropdownButton from './DropdownButton';
 import Button from '@cdo/apps/templates/Button';
 
-export default {
-  title: 'DropdownButton',
-  component: DropdownButton
+export default storybook => {
+  storybook.storiesOf('Buttons/DropdownButton', module).addStoryTable([
+    {
+      name: 'DropdownButton',
+      story: () => (
+        <DropdownButton text="Assign unit" color={Button.ButtonColor.orange}>
+          <a href="asdf">Child with href</a>
+          <a onClick={() => console.log('click')}>Child with onClick</a>
+        </DropdownButton>
+      )
+    },
+    {
+      name: 'blue DropdownButton',
+      story: () => (
+        <DropdownButton text="Assign unit" color={Button.ButtonColor.blue}>
+          <a href="asdf">Child with href</a>
+          <a onClick={() => console.log('click')}>Child with onClick</a>
+        </DropdownButton>
+      )
+    }
+  ]);
 };
-
-export const OrangeDropdownButton = () => (
-  <DropdownButton text="Assign unit" color={Button.ButtonColor.orange}>
-    <a href="asdf">Child with href</a>
-    <a onClick={() => console.log('click')}>Child with onClick</a>
-  </DropdownButton>
-);
-
-export const BlueDropdownButton = () => (
-  <DropdownButton text="Assign unit" color={Button.ButtonColor.blue}>
-    <a href="asdf">Child with href</a>
-    <a onClick={() => console.log('click')}>Child with onClick</a>
-  </DropdownButton>
-);
