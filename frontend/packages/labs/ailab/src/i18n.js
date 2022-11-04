494818ad16cd62731b1975cb941ef3103c6a2171
Dayne <dayne@code.org>
2022-11-03 17:52:14 -0700

    Added 'scope' options for nested translation keys

--- frontend/packages/labs/ailab/src/i18n.js
+++ frontend/packages/labs/ailab/src/i18n.js
@@ -13,7 +13,27 @@ const t = (key, options) => {
   if (!messages) {
     throw "I18n must be initialized before calling t";
   }
-  return messages[key](options);
+  const scope = options["scope"] || [];
+  let scopedMessages = messages;
+  scope.forEach(s => {
+    if (scopedMessages !== undefined) {
+      scopedMessages = scopedMessages[s]
+    }
+  });
+
+  if (scopedMessages === undefined) {
+    console.warn("Couldn't find string for given key", key, options, scopedMessages);
+    return undefined;
+  }
+
+  // All strings should be represented by a Function. If it isn't, the scope probaly needs to be
+  // defined.
+  if (!(scopedMessages[key] instanceof Function)) {
+    console.warn("Given key doesn't point to a single string", key, options, scopedMessages);
+    return undefined;
+  }
+
+  return scopedMessages[key](options);
 };
 
 /*
