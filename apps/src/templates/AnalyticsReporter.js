1b1efecd4d330efe8ae3654b7c46cf29dbf8a683
Hannah Bergam <hannahbergam@gmail.com>
2022-11-08 15:49:00 -0800

    pulling over relevant analytics functions from analyticsReporter in music lab

---
+++ apps/src/templates/AnalyticsReporter.js
@@ -0,0 +1,66 @@
+import {
+  init,
+  track,
+  Identify,
+  identify,
+  setUserId,
+  setSessionId
+} from '@amplitude/analytics-browser';
+
+const API_KEY = '12345';
+
+export default class AnalyticsReporter {
+  constructor() {
+    // TODO: API Key
+    init(API_KEY);
+    this.sessionStartTime = null;
+    this.maxInstructionsSeen = 1;
+    this.currentInstructionsPage = 1;
+    this.blockStats = {};
+  }
+
+  setUserProperties(userId, userType, signInState) {
+    const identifyObj = new Identify();
+    setUserId(userId || 'none');
+    identifyObj.set('userType', userType);
+    identifyObj.set('signInState', signInState);
+
+    this.log(
+      `User properties: userId: ${userId}, userType: ${userType}, signInState: ${signInState}`
+    );
+    identify(identifyObj);
+  }
+
+  onButtonClicked(buttonName) {
+    track('Button clicked', {buttonName});
+    this.log(`Button clicked. Payload: ${JSON.stringify({buttonName})}`);
+  }
+
+  onSessionStart() {
+    this.sessionStartTime = Date.now();
+    setSessionId(this.sessionStartTime);
+
+    this.log(`Session start. Session ID: ${this.sessionStartTime}`);
+  }
+
+  onSessionEnd() {
+    if (this.sessionStartTime === null) {
+      return;
+    }
+    const duration = Date.now() - this.sessionStartTime;
+    // report duration event, and instructions seen events
+    this.sessionStartTime = null;
+    const payload = {
+      durationSeconds: duration / 1000,
+      mostInstructionsVisited: this.maxInstructionsSeen,
+      lastInstructionsVisited: this.currentInstructionsPage,
+      blockStats: this.blockStats
+    };
+    track('Session end', payload);
+    this.log(`Session end. Payload: ${JSON.stringify(payload)}`);
+  }
+
+  log(message) {
+    console.log(`[AMPLITUDE ANALYTICS EVENT]: ${message}`);
+  }
+}
