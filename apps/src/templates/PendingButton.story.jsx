a955145673a70ea52cdbcb209db8d9301d6f041e
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-11-14 14:19:42 -0800

    migrate PendingButton story

--- apps/src/templates/PendingButton.story.jsx
+++ apps/src/templates/PendingButton.story.jsx
@@ -2,32 +2,31 @@ import React from 'react';
 import PendingButton from './PendingButton';
 import dataStyles from '../storage/dataBrowser/data-styles.module.scss';
 import classNames from 'classnames';
+import {reduxStore} from '../../.storybook/decorators';
+import {Provider} from 'react-redux';
 
-export default storybook => {
-  storybook.storiesOf('Buttons/PendingButton', module).addStoryTable([
-    {
-      name: 'PendingButton - not pending',
-      story: () => (
-        <PendingButton
-          isPending={false}
-          onClick={() => console.log('click')}
-          pendingText="Adding"
-          className={classNames(dataStyles.button, dataStyles.buttonBlue)}
-          text="Add pair"
-        />
-      )
-    },
-    {
-      name: 'PendingButton - pending',
-      story: () => (
-        <PendingButton
-          isPending={true}
-          onClick={() => console.log('click')}
-          pendingText="Adding"
-          className={classNames(dataStyles.button, dataStyles.buttonBlue)}
-          text="Add pair"
-        />
-      )
-    }
-  ]);
+export default {
+  title: 'PendingButton',
+  component: PendingButton
+};
+
+const Template = args => (
+  <Provider store={reduxStore()}>
+    <PendingButton
+      onClick={() => console.log('click')}
+      pendingText="Adding"
+      className={classNames(dataStyles.button, dataStyles.buttonBlue)}
+      text="Add pair"
+      {...args}
+    />
+  </Provider>
+);
+export const PendingButtonNotPending = Template.bind({});
+PendingButton.args = {
+  isPending: false
+};
+
+export const PendingButtonPending = Template.bind({});
+PendingButtonPending.args = {
+  isPending: true
 };
