fa070b24615bddf48b068a7073224b367316a39d
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-11-14 21:28:12 -0800

    remove unnec reduxStore

--- apps/src/templates/PendingButton.story.jsx
+++ apps/src/templates/PendingButton.story.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PendingButton from './PendingButton';
 import dataStyles from '../storage/dataBrowser/data-styles.module.scss';
 import classNames from 'classnames';
-import {reduxStore} from '../../.storybook/decorators';
-import {Provider} from 'react-redux';
 
 export default {
   title: 'PendingButton',
@@ -11,16 +9,14 @@ export default {
 };
 
 const Template = args => (
-  <Provider store={reduxStore()}>
-    <PendingButton
-      onClick={() => console.log('click')}
-      pendingText="Adding"
-      className={classNames(dataStyles.button, dataStyles.buttonBlue)}
-      text="Add pair"
-      isPending={false}
-      {...args}
-    />
-  </Provider>
+  <PendingButton
+    onClick={() => console.log('click')}
+    pendingText="Adding"
+    className={classNames(dataStyles.button, dataStyles.buttonBlue)}
+    text="Add pair"
+    isPending={false}
+    {...args}
+  />
 );
 export const PendingButtonNotPending = Template.bind({});
 
