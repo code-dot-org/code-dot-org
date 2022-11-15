01a66a7648fe58c447d0631d5d57859107c46f1e
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-11-14 19:05:10 -0800

    re-added StorybookHarness to PaginationWrapper

--- apps/src/templates/PaginationWrapper.story.jsx
+++ apps/src/templates/PaginationWrapper.story.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
 import PaginationWrapper from './PaginationWrapper';
 
+class StorybookHarness extends React.Component {
+  state = {currentPage: 1};
+
+  onValueChange = newValue => this.setState({currentPage: newValue});
+
+  render() {
+    return (
+      <PaginationWrapper
+        totalPages={3}
+        currentPage={this.state.currentPage}
+        onChangePage={this.onValueChange}
+      />
+    );
+  }
+}
+
 export default {
   title: 'PaginationWrapper',
   component: PaginationWrapper
 };
 
-function defaultFunction() {}
+const Template = args => {
+  return <StorybookHarness />;
+};
 
-function Template(args) {
-  return (
-    <PaginationWrapper
-      onChangePage={defaultFunction}
-      totalPages={3}
-      {...args}
-    />
-  );
-}
-
-export const Page1Example = Template.bind({});
-Page1Example.args = {
+export const BasicExample = Template.bind({});
+BasicExample.args = {
   currentPage: 1
-};
-
-export const Page2Example = Template.bind({});
-Page2Example.args = {
-  currentPage: 2
-};
-
-export const Page3Example = Template.bind({});
-Page3Example.args = {
-  currentPage: 3
 };
