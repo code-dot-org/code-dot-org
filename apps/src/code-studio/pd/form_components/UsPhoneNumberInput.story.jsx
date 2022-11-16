afb183df59f91dc2eecad8345ae33fca84caa566
Turner Riley <turner.riley@code.org>
2022-11-16 11:48:04 -0800

    add us phone number back in

---
+++ apps/src/code-studio/pd/form_components/UsPhoneNumberInput.story.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UsPhoneNumberInput from './UsPhoneNumberInput';
+import reactBootstrapStoryDecorator from '../reactBootstrapStoryDecorator';
+
+export default storybook => {
+  storybook
+    .storiesOf('UsPhoneNumberInput', module)
+    .addDecorator(reactBootstrapStoryDecorator)
+    .add('Default', () => (
+      <UsPhoneNumberInput name="Storybook" label="Enter a phone number" />
+    ));
+};
