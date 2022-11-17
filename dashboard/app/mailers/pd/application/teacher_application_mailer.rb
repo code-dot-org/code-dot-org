e450a57b7c9db50780e2a21221c69f8c12404359
Meg Crenshaw <meg@code.org>
2022-11-17 16:15:41 -0500

    Add mailer methods and send them with cron job

--- dashboard/app/mailers/pd/application/teacher_application_mailer.rb
+++ dashboard/app/mailers/pd/application/teacher_application_mailer.rb
@@ -130,6 +130,24 @@ module Pd::Application
       )
     end
 
+    def complete_application_soft_reminder(teacher_application)
+      @application = teacher_application
+
+      mail(
+        to: @application.formatted_applicant_email,
+        subject: "Reminder: Complete your application for Code.org’s Professional Learning Program"
+      )
+    end
+
+    def complete_application_final_reminder(teacher_application)
+      @application = teacher_application
+
+      mail(
+        to: @application.formatted_applicant_email,
+        subject: "Follow Up: Complete your application for Code.org’s Professional Learning Program"
+      )
+    end
+
     def declined(teacher_application)
       @application = teacher_application
 
