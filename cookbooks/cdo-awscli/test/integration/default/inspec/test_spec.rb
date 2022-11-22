01cbc3c9d07cbd0340230074d4e1a7af07c0ebeb
Elijah Hamovitz <elijahhamovitz@gmail.com>
2022-11-21 17:42:28 -0800

    remove connectivity check since it requires credentials, replace with a config file check

--- cookbooks/cdo-awscli/test/integration/default/inspec/test_spec.rb
+++ cookbooks/cdo-awscli/test/integration/default/inspec/test_spec.rb
@@ -3,13 +3,8 @@ describe command('aws --version') do
   its(:stderr) {should match('aws-cli/1.15.81')}
 end
 
-# Basic AWS CLI connectivity check, based on
-# https://stackoverflow.com/a/42241040/1810460; we don't actually care about
-# caller identity here, it's just an arbitrary choice.
-describe command('aws sts get-caller-identity') do
-  its(:exit_status) {should eq 0}
-  its(:stdout) {should match '"Account": "123456789012"'}
-  its(:stdout) {should match '"UserId": "AR#####:#####"'}
-  its(:stdout) {should match '"Arn": "arn:aws:sts::123456789012:assumed-role/role-name/role-session-name"'}
-  its(:stderr) {should eq ''}
+describe file('/root/.aws/config') do
+  its('content') {should match "region = us-east-1"}
+  its('content') {should match "metadata_service_num_attempts = 3"}
+  its('content') {should match "metadata_service_timeout = 5"}
 end
