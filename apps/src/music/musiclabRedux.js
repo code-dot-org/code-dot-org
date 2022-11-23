892534ac3230a69b17301a9185f756019f6b667c
Sanchit Malhotra <sanchit@code.org>
2022-11-23 11:09:37 -0300

    WIP music lab redux

---
+++ apps/src/music/musiclabRedux.js
@@ -0,0 +1,62 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {registerReducers} from '@cdo/apps/redux';
+
+export const InstructionsPositions = {
+  TOP: 'TOP',
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT'
+};
+
+const instructionPositionOrder = [
+  InstructionsPositions.TOP,
+  InstructionsPositions.LEFT,
+  InstructionsPositions.RIGHT
+];
+
+const initialState = {
+  isPlaying: false,
+  timelineAtTop: false,
+  showInstructions: true,
+  showBeatPad: false,
+  instructionsPosition: InstructionsPositions.LEFT
+};
+
+const musicSlice = createSlice({
+  name: 'music',
+  initialState,
+  reducers: {
+    setIsPlaying: (state, action) => {
+      state.isPlaying = action.payload;
+    },
+    toggleInstructions: state => {
+      state.showInstructions = !state.showInstructions;
+    },
+    toggleTimelinePosition: state => {
+      state.timelineAtTop = !state.timelineAtTop;
+    },
+    shiftInstructionsPosition: state => {
+      state.instructionsPosition =
+        instructionPositionOrder[
+          (instructionPositionOrder.indexOf(state.instructionsPosition) + 1) %
+            instructionPositionOrder.length
+        ];
+    },
+    setBeatPadShowing: (state, action) => {
+      state.showBeatPad = action.payload;
+    },
+    toggleBeatPad: state => {
+      state.showBeatPad = !state.showBeatPad;
+    }
+  }
+});
+
+export const {
+  setIsPlaying,
+  toggleInstructions,
+  toggleTimelinePosition,
+  shiftInstructionsPosition,
+  setBeatPadShowing,
+  toggleBeatPad
+} = musicSlice.actions;
+
+registerReducers({music: musicSlice.reducer});
