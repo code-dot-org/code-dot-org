190c32a6b2d23dc7be6d04bea419a69cefdfc918
Erin Peach <erin@code.org>
2022-11-23 16:19:01 -0800

    [StoryBook Refactor] CourseCard, ResourceCard, ImageResourceCard, VerticalImageResourceCard (#49145)
    * Refactor CourseCard storybook
    
    * Refactor ResourceCard storybook
    
    * Refactor ImageResourceCard Storybook
    
    * Refactor VerticalImageResourceCard storybook
    
    * Remove empty args
    
    * Remove unneccessary comment

--- apps/src/templates/studioHomepages/ImageResourceCard.story.jsx
+++ apps/src/templates/studioHomepages/ImageResourceCard.story.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import ImageResourceCard from './ImageResourceCard';
+import {Provider} from 'react-redux';
+import {reduxStore} from '@cdo/storybook/decorators';
 
-export default storybook => {
-  return storybook
-    .storiesOf('Cards/ImageResourceCard', module)
-    .withReduxStore()
-    .addStoryTable([
-      {
-        name: 'basic resource card',
-        description: `This is an example resource card.`,
-        story: () => (
-          <ImageResourceCard
-            title="Teacher Community"
-            description="Ask questions about curriculum, share ideas from your lessons, and get help from other teachers"
-            image="teachercommunity.png"
-            buttonText="Connect Today"
-            link="link to teacher community"
-          />
-        )
-      }
-    ]);
+export default {
+  title: 'ImageResourceCard',
+  component: ImageResourceCard
+};
+
+const Template = args => (
+  <Provider store={reduxStore()}>
+    <ImageResourceCard {...args} />
+  </Provider>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Teacher Community',
+  description:
+    'Ask questions about curriculum, share ideas from your lessons, and get help from other teachers',
+  image: 'teachercommunity.png',
+  buttonText: 'Connect Today',
+  link: 'link to teacher community'
 };
