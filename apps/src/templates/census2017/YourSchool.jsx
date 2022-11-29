9419935b9f1708c445b2b0d1a8dc679b779e57c6
Meg Crenshaw <meg@code.org>
2022-11-29 08:57:56 -0500

    Add back in ProfessionalLearningBanner to YourSchool

--- apps/src/templates/census2017/YourSchool.jsx
+++ apps/src/templates/census2017/YourSchool.jsx
@@ -13,6 +13,7 @@ import {SpecialAnnouncementActionBlock} from '../studioHomepages/TwoColumnAction
 import i18n from '@cdo/locale';
 import SchoolAutocompleteDropdown from '../SchoolAutocompleteDropdown';
 import CensusMapReplacement from './CensusMapReplacement';
+import ProfessionalLearningApplyBanner from '../ProfessionalLearningApplyBanner';
 
 class YourSchool extends Component {
   static propTypes = {
@@ -23,6 +24,7 @@ class YourSchool extends Component {
     prefillData: censusFormPrefillDataShape,
     hideMap: PropTypes.bool,
     currentCensusYear: PropTypes.number,
+    showProfessionalLearningBanner: PropTypes.bool,
     teacherApplicationMode: PropTypes.string
   };
 
@@ -98,6 +100,14 @@ class YourSchool extends Component {
               width="100%"
             />
           )}
+        {this.props.showProfessionalLearningBanner && (
+          <ProfessionalLearningApplyBanner
+            nominated={false}
+            style={styles.banner}
+            linkSuffix={'program-information'}
+            teacherApplicationMode={this.props.teacherApplicationMode}
+          />
+        )}
         <h1 style={styles.heading}>{i18n.yourSchoolHeading()}</h1>
         <h3 style={styles.description}>{i18n.yourSchoolDescription()}</h3>
         <YourSchoolResources />
