a72811790027d3431e989160402f1f83687436af
Bethany Connor <bethany@code.org>
2022-11-30 13:39:32 -0500

    Fix EditSectionForm usage of AnalyticsReporter

--- apps/src/templates/teacherDashboard/EditSectionForm.jsx
+++ apps/src/templates/teacherDashboard/EditSectionForm.jsx
@@ -28,7 +28,7 @@ import {
   StudentGradeLevels
 } from '@cdo/apps/util/sharedConstants';
 import firehoseClient from '@cdo/apps/lib/util/firehose';
-import AnalyticsReporter from '../AnalyticsReporter';
+import analyticsReporter from '../AnalyticsReporter';
 import {ParticipantAudience} from '../../generated/curriculum/sharedCourseConstants';
 import GetVerifiedBanner from './GetVerifiedBanner';
 
@@ -42,7 +42,6 @@ class EditSectionForm extends Component {
     title: PropTypes.string.isRequired,
     //Whether the user is adding a brand new section or editing an existing one.
     isNewSection: PropTypes.bool,
-    analyticsReporter: new AnalyticsReporter(),
 
     //Comes from redux
     initialUnitId: PropTypes.number,
@@ -70,7 +69,7 @@ class EditSectionForm extends Component {
   };
 
   onSaveClick = () => {
-    this.analyticsReporter.onButtonClicked(SAVE);
+    analyticsReporter.onButtonClicked(SAVE);
     const {section, hiddenLessonState} = this.props;
     const sectionId = section.id;
     const scriptId = section.unitId;
