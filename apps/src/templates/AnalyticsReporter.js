6151be1c8001a279960a13d858b9c46480823613
Bethany Connor <bethany@code.org>
2022-11-30 14:54:37 -0500

    Remove more code from AnalyticsReporter

--- apps/src/templates/AnalyticsReporter.js
+++ apps/src/templates/AnalyticsReporter.js
@@ -3,22 +3,19 @@ import {
   track,
   Identify,
   identify,
-  setUserId,
-  setSessionId
+  setUserId
 } from '@amplitude/analytics-browser';
 
 const API_KEY = '12345';
 
 class AnalyticsReporter {
   constructor() {
-    // TODO: API Key
     init(API_KEY);
-    this.sessionStartTime = null;
   }
 
   setUserProperties(userId, userType, signInState) {
     const identifyObj = new Identify();
-    setUserId(userId || 'none');
+    setUserId(`dev-${userId || 'none'}`);
     identifyObj.set('userType', userType);
     identifyObj.set('signInState', signInState);
 
@@ -28,31 +25,9 @@ class AnalyticsReporter {
     identify(identifyObj);
   }
 
-  onButtonClicked(buttonName) {
-    track('Button clicked', {buttonName});
-    this.log(`Button clicked. Payload: ${JSON.stringify({buttonName})}`);
-  }
-
-  onSessionStart() {
-    this.sessionStartTime = Date.now();
-    setSessionId(this.sessionStartTime);
-
-    this.log(`Session start. Session ID: ${this.sessionStartTime}`);
-  }
-
-  onSessionEnd() {
-    if (this.sessionStartTime === null) {
-      return;
-    }
-    const duration = Date.now() - this.sessionStartTime;
-    // report duration event, and instructions seen events
-    this.sessionStartTime = null;
-    const payload = {
-      durationSeconds: duration / 1000,
-      blockStats: this.blockStats
-    };
-    track('Session end', payload);
-    this.log(`Session end. Payload: ${JSON.stringify(payload)}`);
+  sendEvent(eventName, payload) {
+    track(eventName, payload);
+    this.log(`${eventName}. Payload: ${JSON.stringify({payload})}`);
   }
 
   log(message) {
