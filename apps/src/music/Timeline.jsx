36ec4807d0ad747887e375800c2f00e9f2289ca5
breville <brendan@code.org>
2022-12-10 21:57:07 +0000

    Music: sticky timeline sound rows
    The timeline maintains the row given to a specific sound between renders
    whenever possible.

--- apps/src/music/Timeline.jsx
+++ apps/src/music/Timeline.jsx
@@ -13,6 +13,12 @@ export default class Timeline extends React.Component {
     sounds: PropTypes.array
   };
 
+  constructor(props) {
+    super(props);
+
+    this.previousUniqueSounds = null;
+  }
+
   getEventHeight = () => {
     const numUniqueSounds = this.getUniqueSounds().length;
     const actualHeight = 110;
@@ -35,7 +41,11 @@ export default class Timeline extends React.Component {
   };
 
   getUniqueSounds = () => {
-    // Each unique sound gets its own color/row.
+    // Each unique sound gets its own row (and therefore color).
+    // If a sound was showing previously, then we'll attempt to keep
+    // it in the same row, though we won't have empty rows.
+
+    // First, generate a list of all unique sounds.
     const uniqueSounds = [];
     for (const songEvent of this.props.songData.events) {
       const id = songEvent.id;
@@ -43,7 +53,48 @@ export default class Timeline extends React.Component {
         uniqueSounds.push(id);
       }
     }
-    return uniqueSounds;
+
+    // This is the actual output.  It will be the same length as uniqueSounds.
+    // Fill it with undefined entries.
+    let outputSounds = new Array(uniqueSounds.length).fill(undefined);
+
+    // If we have a previous output from this function, then we'll attempt
+    // to keep those entries in the same rows as they were before.
+    if (this.previousUniqueSounds) {
+      // For each of those previous entries...
+      for (let i = 0; i < this.previousUniqueSounds.length; i++) {
+        // ...if it's still in use...
+        if (uniqueSounds.indexOf(this.previousUniqueSounds[i]) !== -1) {
+          // ... put it back in that row.
+          outputSounds[i] = this.previousUniqueSounds[i];
+        }
+      }
+
+      // For each of our known current sounds...
+      for (let j = 0; j < uniqueSounds.length; j++) {
+        // ... if it's not yet in a row...
+        if (outputSounds.indexOf(uniqueSounds[j]) === -1) {
+          // ...then put it in the first row available.
+          let row = outputSounds.findIndex(x => x === undefined);
+          outputSounds[row] = uniqueSounds[j];
+
+          // (One scenario where this helps is when a sound is renamed.
+          // The new  entry will replace the old one in the same row.)
+        }
+      }
+
+      // Remove empty rows.
+      outputSounds = outputSounds.filter(s => {
+        return s !== undefined;
+      });
+    } else {
+      outputSounds = uniqueSounds;
+    }
+
+    // Remember this set for next time.
+    this.previousUniqueSounds = outputSounds;
+
+    return outputSounds;
   };
 
   getUniqueIndexForEventId = id => {
