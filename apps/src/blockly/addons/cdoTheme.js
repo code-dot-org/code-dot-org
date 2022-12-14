5760c781882907c4fbc61c7ce956843227bf93ce
Mike Harvey <mike.harvey@code.org>
2022-12-14 14:17:44 -0500

    update styles

--- apps/src/blockly/addons/cdoTheme.js
+++ apps/src/blockly/addons/cdoTheme.js
@@ -4,7 +4,7 @@ import DeuteranopiaTheme from '@blockly/theme-deuteranopia';
 import HighContrastTheme from '@blockly/theme-highcontrast';
 import TritanopiaTheme from '@blockly/theme-tritanopia';
 
-const cdoBlockStyles = {
+export const cdoBlockStyles = {
   default: {
     colourPrimary: '00b0bc'
   },
@@ -12,47 +12,50 @@ const cdoBlockStyles = {
     // Duplicates definition from core Blockly
     colourPrimary: '#0093c9'
   },
-  procedure_blocks: {
-    // Overrides definition from core Blockly
-    colourPrimary: '#509918'
+  logic_blocks: {
+    // Duplicates definition from core Blockly
+    colourPrimary: '#5d80a3'
   },
   loop_blocks: {
-    // Overrides definition from core Blockly
+    // Intentionally overrides definition from core Blockly
     colourPrimary: '#f218a2'
   },
-  event_blocks: {
-    colourPrimary: '00bc3e'
+  math_blocks: {
+    // Duplicates definition from core Blockly
+    colourPrimary: '#77669e'
+  },
+  procedure_blocks: {
+    // Intentionally overrides definition from core Blockly
+    colourPrimary: '#509918'
+  },
+  text_blocks: {
+    // Duplicates definition from core Blockly
+    colourPrimary: '#5ba58c'
+  },
+  variable_blocks: {
+    // Duplicates definition from core Blockly
+    colourPrimary: '#9e6b93'
+  },
+  behavior_blocks: {
+    colourPrimary: '#20cc4e'
   },
   dance_blocks: {
     colourPrimary: '#7435b2'
   },
-  number_blocks: {
-    // Duplicates definition from core Blockly
-    colourPrimary: '77669e'
+  event_blocks: {
+    colourPrimary: '#00bc3e'
   },
-  condition_blocks: {
-    // Called 'logic_blocks' in core Blockly, TODO: Rename here and across all blocks
-    colourPrimary: '5d80a3'
+  music_blocks: {
+    colourPrimary: '#5b67a5'
   },
   sprite_blocks: {
-    colourPrimary: 'b2353f'
-  },
-  behavior_blocks: {
-    colourPrimary: '20cc4e'
-  },
-  world_blocks: {
-    colourPrimary: '5b5ba5'
+    colourPrimary: '#b2353f'
   },
   setup_blocks: {
-    colourPrimary: 'fca400'
+    colourPrimary: '#fca400'
   },
-  text_blocks: {
-    // Duplicates definition from core Blockly
-    colourPrimary: '5ba58c'
-  },
-  variable_blocks: {
-    // Duplicates definition from core Blockly
-    colourPrimary: '9e6b93'
+  world_blocks: {
+    colourPrimary: '#5b5ba5'
   }
 };
 
