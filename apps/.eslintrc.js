cdcaa99f6b50ad4dadd2765b81514d36fde7398f
Hannah Bergam <hannahbergam@gmail.com>
2022-12-13 17:36:15 -0800

    second attempt at the linter, using yarn

--- apps/.eslintrc.js
+++ apps/.eslintrc.js
@@ -1,34 +1,40 @@
 // This config only defines globals available especially in apps,
 // and enables es6. See the root .eslintrc.js for linting rules.
 module.exports = {
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true,
-      "modules": true,
-      "ecmaVersion": 6,
-      "experimentalObjectRestSpread": true
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      ecmaVersion: 6,
+      experimentalObjectRestSpread: true
     }
   },
-  "env": {
-    "es6": true
+  env: {
+    es6: true
   },
-  "globals": {
-    "Blockly": true,
-    "Phaser": true,
-    "Studio": true,
-    "Maze": true,
-    "Turtle": true,
-    "Bounce": true,
-    "Eval": true,
-    "Flappy": true,
-    "Applab": true,
-    "Calc": true,
-    "Jigsaw": true,
-    "$": true,
-    "jQuery": true,
-    "IN_UNIT_TEST": true,
-    "IN_STORYBOOK": true,
+  globals: {
+    Blockly: true,
+    Phaser: true,
+    Studio: true,
+    Maze: true,
+    Turtle: true,
+    Bounce: true,
+    Eval: true,
+    Flappy: true,
+    Applab: true,
+    Calc: true,
+    Jigsaw: true,
+    $: true,
+    jQuery: true,
+    IN_UNIT_TEST: true,
+    IN_STORYBOOK: true
+  },
+  plugins: ['jsx-a11y'],
+  extends: ['plugin:jsx-a11y/recommended'],
+  rules: {
+    strict: 'error',
+    'jsx-a11y/no-onchange': 0
   }
 };
