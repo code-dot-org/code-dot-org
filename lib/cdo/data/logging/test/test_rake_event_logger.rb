95b2ee424ad6c0c8357928c022bd312f92e6634d
pablo-code-org <pablo@code.org>
2022-12-15 12:33:45 -0800

    Adding integration test v2.

--- lib/cdo/data/logging/test/test_rake_event_logger.rb
+++ lib/cdo/data/logging/test/test_rake_event_logger.rb
@@ -7,15 +7,40 @@ include TimedTaskWithLogging
 
 class RakeEventLoggerTest < ActiveSupport::TestCase
   def test_failed_task
+    RakeTaskEventLogger.any_instance.expects(:start_task_logging).once
+    RakeTaskEventLogger.any_instance.expects(:end_task_logging).never
+    RakeTaskEventLogger.any_instance.expects(:exception_task_logging).once
     error = assert_raises(RuntimeError) do
-      Rake::Task['test_task'].invoke
+      Rake::Task['test_exception_timed_task'].invoke
     end
     assert_equal 'This is really bad', error.message
-    RakeTaskEventLogger.any_instance.expects(:exception_task_logging).once.with
-    RakeTaskEventLogger.any_instance.expects(:start_task_logging).once.with
   end
 
-  timed_task_with_logging :test_task do
+  def test_successful_task
+    RakeTaskEventLogger.any_instance.expects(:start_task_logging).once
+    RakeTaskEventLogger.any_instance.expects(:end_task_logging).once
+    RakeTaskEventLogger.any_instance.expects(:exception_task_logging).never
+    Rake::Task['test_successful_timed_task'].invoke
+  end
+
+  def test_failed_logger_task
+    RakeTaskEventLogger.any_instance.expects(:start_task_logging).once
+    RakeTaskEventLogger.any_instance.expects(:end_task_logging).once
+    RakeTaskEventLogger.any_instance.expects(:exception_task_logging).never
+    RakeTaskEventLogger.any_instance.stubs(:log_event).raises(RuntimeError, "failed logger")
+    Rake::Task['test_failed_logger_timed_task'].invoke
+    RakeTaskEventLogger.any_instance.unstub(:start_task_logging)
+  end
+
+  timed_task_with_logging :test_failed_logger_timed_task do
+    puts "This will have a bad logger"
+  end
+
+  timed_task_with_logging :test_successful_timed_task do
+    puts "This is a successful task"
+  end
+
+  timed_task_with_logging :test_exception_timed_task do
     raise 'This is really bad'
   end
 end
