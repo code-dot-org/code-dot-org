8356ff89fdf81262d80221a0dab1dd60de8a75d4
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-01-03 17:04:27 -0800

    install node 16 with chef

--- cookbooks/cdo-nodejs/test/integration/default/inspec/node_spec.rb
+++ cookbooks/cdo-nodejs/test/integration/default/inspec/node_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../../shared/helper_spec'
 
 file_exist '/usr/bin/node'
-cmd 'node -v', 'v14.'
+cmd 'node -v', 'v16.'
 
 cmd 'which yarn', '/usr/bin/yarn'
 cmd 'yarn --version', '1.22.5'
