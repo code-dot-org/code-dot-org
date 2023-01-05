4c5dbbf1fe410c63f514f173d203126bfe74edb5
Darin Webb <darin@code.org>
2023-01-05 08:34:12 -0600

    backfill some tests for countAsg resourcce

--- aws/cloudformation/count_asg.js
+++ aws/cloudformation/count_asg.js
@@ -4,79 +4,80 @@
 
 var response = require('cfn-response');
 var AWS = require('aws-sdk');
-var autoscaling = new AWS.AutoScaling();
 
-exports.handler = function (event, context) {
-  console.log('REQUEST RECEIVED:\n', JSON.stringify(event));
+function convertToAssocTags(tags) {
+  var assocTags = {};
+  tags.forEach(tag => {
+    assocTags[tag.Key] = tag.Value;
+  });
+  return assocTags;
+}
 
-  if (event.RequestType == 'Delete') {
-    response.send(event, context, response.SUCCESS);
-    return;
-  }
-
-  var tags = event.ResourceProperties.AutoScalingGroupTags || errorExit('No AutoScalingGroupTags found', event, context);
-  var defaultCount = event.ResourceProperties.Default || {MinSize: 1, MaxSize: 10, DesiredCapacity: 1};
-
-  var responseData = {};
-
-  var findAutoScalingGroupByTags = function(tags, callback) {
-    var result = defaultCount;
-    autoscaling.describeAutoScalingGroups({}, function(err, data) {
-      if (err) {
-        callback(err, data);
-      } else {
-        data.AutoScalingGroups.forEach(function (item) {
-          if (match(tags, convertToAssocTags(item.Tags))) {
-            result = {
-              MinSize: item.MinSize,
-              MaxSize: item.MaxSize,
-              DesiredCapacity: item.DesiredCapacity
-            };
-          }
-        });
-        callback(null, result);
-      }
-    })
-  };
-
-  var match = function(a, b) {
-    var match = true;
+function findAutoScalingGroupByTags(tags, callback) {
+  /**
+   * Return true if all key/value pairs in `a` are present in `b`
+   */
+  const match = (a, b) => {
     Object.keys(a).forEach(function(key) {
       if (a[key] != b[key]) {
-        match = false;
+        return false;
       }
     });
-    return match;
+    return true;
   };
 
-  var convertToAssocTags = function (tags) {
-    var assocTags = {};
-    tags.forEach(function(tag) {
-      assocTags[tag.Key] = tag.Value;
+  const autoscaling = new AWS.AutoScaling();
+  autoscaling.describeAutoScalingGroups({}, function(err, data) {
+    let result = {};
+    if (err) {
+      callback(err, data);
+    } else {
+      data.AutoScalingGroups.forEach(item => {
+        if (match(tags, convertToAssocTags(item.Tags))) {
+          result = {
+            MinSize: item.MinSize,
+            MaxSize: item.MaxSize,
+            DesiredCapacity: item.DesiredCapacity
+          };
+        }
+      });
+      callback(null, result);
+    }
+  })
+}
+
+exports.handler = (event, context) => {
+  console.log('REQUEST RECEIVED:\n', JSON.stringify(event));
+  
+  const tags = event.ResourceProperties?.AutoScalingGroupTags;
+  if (!tags) {
+    response.send(event, context, response.SUCCESS, {
+      Error: "No AutoScalingGroupTags found"
     });
-    return assocTags;
-  };
-
+    return;
+  }
+  
+  const defaultCount = event.ResourceProperties.Default || {MinSize: 1, MaxSize: 10, DesiredCapacity: 1};
 
   if (event.RequestType == 'Create' || event.RequestType == 'Update') {
-    findAutoScalingGroupByTags(convertToAssocTags(tags), function(err, result) {
+    findAutoScalingGroupByTags(convertToAssocTags(tags), (err, result) => {
+      let responseData = {}
+
       if (err) {
         responseData.Error = 'No AutoScalingGroup found';
         responseData.OriginalError = err;
         result = defaultCount;
       }
+
       responseData.MinSize = result.MinSize;
       responseData.MaxSize = result.MaxSize;
       responseData.DesiredCapacity = result.DesiredCapacity;
       response.send(event, context, response.SUCCESS, responseData);
+      return;
     });
   } else {
-    response.send(event, context, response.SUCCESS, responseData);
+    // The other RequestType is Delete, which is a no-op
+    response.send(event, context, response.SUCCESS);
+    return;
   }
-};
-
-var errorExit = function (message, event, context) {
-  responseData = {Error: message};
-  console.log(responseData.Error);
-  response.send(event, context, response.FAILED, responseData);
 };
--- aws/cloudformation/test/testCountAsg.js
+++ aws/cloudformation/test/testCountAsg.js
@@ -1,25 +1,32 @@
-const CountAsg = require("../count_asg");
 const {assert, expect} = require("chai");
 const sinon = require("sinon");
-
-
+const AWS = require('aws-sdk-mock');
 const CfnResponse = require('cfn-response');
-// sinon.stub(CfnResponse, "send");
+
+const CountAsg = require("../count_asg");
 
 describe('CountAsg', () => {
     const context = {};
 
+    const defaultAsgDetails = {
+        MinSize: 1,
+        MaxSize: 10,
+        DesiredCapacity: 1
+    };
+
     beforeEach(() => {
-        sinon.stub(CfnResponse, "send")
-    })
+        sinon.stub(CfnResponse, "send");
+        sinon.stub()
+    });
 
     afterEach(() => {
-        CfnResponse.send.restore()
-    })
+        CfnResponse.send.restore();
+        AWS.restore('AutoScaling');
+    });
 
 
     describe('handler', () => {
-        it('sends CloudFormation success for Delete requests', async () => {
+        it('receiving Delete, sends CloudFormation success', async () => {
             const event = {RequestType: 'Delete'};
 
             await CountAsg.handler(event, context);
@@ -31,26 +38,102 @@ describe('CountAsg', () => {
             // expect(response.status).to.equal(200);
         });
 
-        it.only('sends CloudFormation error if no AutoScaling Group specified', async () => {
-            const event = {
-                RequestType: "Unknown",
-                ResourceProperties: {
-                    AutoScalingGroupTags: undefined
-                }
-            };
+        ['Create', 'Update'].forEach(requestType => {
+            describe('receiving ${requestType},', () => {
+                it('sends CloudFormation error if no AutoScaling Group specified', async () => {
+                    const event = {
+                        RequestType: requestType,
+                        ResourceProperties: {
+                            AutoScalingGroupTags: undefined
+                        }
+                    };
+                    
+                    const expectedResponseData = {
+                        "Error": "No AutoScalingGroupTags found"
+                    };
+        
+                    await CountAsg.handler(event, context);
+        
+                    assert(CfnResponse.send.calledOnce);
+                    expect(CfnResponse.send.getCall(0).args[2])
+                        .to.equal(CfnResponse.SUCCESS);
+                    expect(CfnResponse.send.getCall(0).args[3])
+                        .to.deep.equal(expectedResponseData);
+                });
+    
+                it('sends default values and error details if AWS library returns an error', async () => {
+                    const event = {
+                        RequestType: requestType,
+                        ResourceProperties: {
+                            AutoScalingGroupTags: [
+                                {
+                                    Key: "tagKey1",
+                                    Value: "tagValue1"
+                                },
+                            ]
+                        }
+                    };
 
-            await CountAsg.handler(event, context);
+                    const awsError = { Msg: 'original error' };
+                    AWS.mock('AutoScaling', 'describeAutoScalingGroups', function (params, callback){
+                        callback(awsError);
+                    });
 
-            const expectedResponseData = {
-                // TODO
-            }
 
-            assert(CfnResponse.send.calledOnce);
-            // expect(CfnResponse.send.getCall(0).args[2])
-            //     .to.equal(CfnResponse.FAILED);
-            console.log("it:", CfnResponse.send.getCall(0).args[3]);
-            expect(CfnResponse.send.getCall(0).args[3])
-                .to.equal(expectedResponseData);
-        })
+                    const expectedResponseData = {
+                        Error: "No AutoScalingGroup found",
+                        OriginalError: awsError,
+                        ...defaultAsgDetails
+                    };
+
+                    await CountAsg.handler(event, context);
+
+                    assert(CfnResponse.send.calledOnce);
+                    expect(CfnResponse.send.getCall(0).args[2])
+                        .to.equal(CfnResponse.SUCCESS);
+                    expect(CfnResponse.send.getCall(0).args[3])
+                        .to.deep.equal(expectedResponseData);
+                });
+
+                // Skipping, not implemented
+                it.skip('sends default values if AWS library returns no ASGs', async () => {
+                    const event = {
+                        RequestType: requestType,
+                        ResourceProperties: {
+                            AutoScalingGroupTags: [
+                                {
+                                    Key: "tagKey1",
+                                    Value: "tagValue1"
+                                },
+                            ]
+                        }
+                    };
+
+                    AWS.mock('AutoScaling', 'describeAutoScalingGroups', function (params, callback){
+                        data = {
+                            AutoScalingGroups: []
+                        }
+                        callback(null, data);
+                    });
+
+
+                    const expectedResponseData = {
+                        Error: "No AutoScalingGroups found",
+                        ...defaultAsgDetails
+                    };
+
+                    await CountAsg.handler(event, context);
+
+                    assert(CfnResponse.send.calledOnce);
+                    expect(CfnResponse.send.getCall(0).args[2])
+                        .to.equal(CfnResponse.SUCCESS);
+                    expect(CfnResponse.send.getCall(0).args[3])
+                        .to.deep.equal(expectedResponseData);
+                });
+
+                it.skip('sends default values when no matching ASG is found');
+                it.skip('sends ASG values when ASG with matching tags is found');
+            });
+        });
     });
 });
