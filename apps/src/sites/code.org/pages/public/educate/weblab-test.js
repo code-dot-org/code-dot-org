2f474ffd636dcdecae458790a46ff229e84c5fd3
Emily Eastlake <emily.eastlake@code.org>
2023-01-10 10:28:53 -0800

    remove unused responsive code and delete unused html file

--- apps/src/sites/code.org/pages/public/educate/weblab-test.js
+++ apps/src/sites/code.org/pages/public/educate/weblab-test.js
@@ -1,29 +1,11 @@
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {getStore, registerReducers} from '@cdo/apps/redux';
-import isRtl from '@cdo/apps/code-studio/isRtlRedux';
-import initResponsive from '@cdo/apps/code-studio/responsive';
-import responsive from '@cdo/apps/code-studio/responsiveRedux';
 import WebLabTest from '@cdo/apps/templates/testPages/WebLabTest.jsx';
 
-registerReducers({isRtl, responsive});
-
-$(document).ready(initWebLabTest);
+$(document).ready(showWebLabTest);
 
 function showWebLabTest() {
-  const webLabTestElement = $('#weblab-test');
-
-  ReactDOM.render(
-    <Provider store={getStore()}>
-      <WebLabTest />
-    </Provider>,
-    webLabTestElement[0]
-  );
-}
-
-function initWebLabTest() {
-  initResponsive();
-  showWebLabTest();
+  const container = document.getElementById('weblab-test');
+  ReactDOM.render(<WebLabTest />, container);
 }
