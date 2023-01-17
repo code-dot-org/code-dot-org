1d265ec044e4f405a1aa878cf6ca91126bcc16f9
Meg Crenshaw <meg@code.org>
2023-01-17 09:54:52 -0500

    Add oneoff script

---
+++ bin/oneoff/remove_non_teacher_application_ids.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+puts "Removing application ids that are not teacher applications ..."
+modified_enrollments = []
+
+Pd::Enrollment.where.not(application_id: nil).each do |enrollment|
+  original_app_id = enrollment.application_id
+
+  begin
+    application = Pd::Application::ApplicationBase.find(original_app_id)
+    if application.type != "Pd::Application::TeacherApplication"
+      enrollment.set_application_id
+      modified_enrollments << enrollment.id
+    end
+  rescue ActiveRecord::SubclassNotFound # Happens with Facilitator applications
+    enrollment.set_application_id
+    modified_enrollments << enrollment.id
+  end
+end
+
+puts "modified #{modified_enrollments.length} enrollments"
+puts "enrollments modified: #{modified_enrollments}"
