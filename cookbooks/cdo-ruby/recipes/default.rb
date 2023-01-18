3d803276eac92170466ea0099113b9e52433719a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-01-18 15:05:26 -0800

    control installed ruby version with attribute

--- cookbooks/cdo-ruby/recipes/default.rb
+++ cookbooks/cdo-ruby/recipes/default.rb
@@ -3,27 +3,28 @@
 # https://www.ruby-lang.org/en/documentation/installation/#ruby-build
 # https://github.com/rbenv/ruby-build#readme
 
-# Arbitrarily use the latest version of ruby build at time this code was written
-RUBY_BUILD_VERSION = "20221225".freeze
+# Arbitrarily use the latest version of ruby build at time this code was
+# written; this probably doesn't matter, but we need to pick something.
+RUBY_BUILD_VERSION = '20221225'.freeze
 
-remote_file "/tmp/ruby-build.tar.gz" do
+remote_file '/tmp/ruby-build.tar.gz' do
   source "https://github.com/rbenv/ruby-build/archive/refs/tags/v#{RUBY_BUILD_VERSION}.tar.gz"
 end
 
-archive_file "/tmp/ruby-build.tar.gz" do
+archive_file '/tmp/ruby-build.tar.gz' do
   destination '/tmp/ruby-build'
 end
 
-execute "install ruby-build" do
+execute 'install ruby-build' do
   cwd "/tmp/ruby-build/ruby-build-#{RUBY_BUILD_VERSION}"
   command './install.sh'
 end
 
-execute "install ruby with ruby build" do
+execute 'install ruby with ruby build' do
   # target /usr specifically because that's where our old apt approach
   # installed ruby; could instead consider /usr/local if we figure out a good
   # way to clean up existing installations.
-  command 'ruby-build 2.7.7 /usr'
+  command "ruby-build #{node['cdo-ruby']['rubygems_version']} /usr"
 end
 
 # TODO: Remove old Ruby version packages if present.
