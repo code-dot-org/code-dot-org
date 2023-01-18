3ff4c37b75786bbb8242ef36770c9e266e1159da
Bethany Connor <bethany@code.org>
2023-01-18 08:56:00 -0500

    Add the entrypoint for the new section page

---
+++ apps/src/sites/studio/pages/sections/new.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SectionsSetUp from '@cdo/apps/templates/sectionsRefresh/SectionsSetUp';
+
+$(document).ready(() => {
+  ReactDOM.render(<SectionsSetUp />, document.getElementById('form'));
+});
