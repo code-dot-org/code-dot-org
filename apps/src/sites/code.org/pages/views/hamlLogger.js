c9d26cb6c9db04d81d890dd15c4bf91f2176e742
Turner Riley <turner.riley@code.org>
2023-01-18 10:25:42 -0800

    remove haml logger files to separate PRs

--- apps/src/sites/code.org/pages/views/hamlLogger.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-
-window.hamlLog = function() {
-  const regionalPartnerSearchElement = $('#haml-logger');
-  const sourcePageId = regionalPartnerSearchElement.data('page-visited');
-  console.log('Logger: ' + sourcePageId);
-};
