fcc6c943b203942c75cf06a30d888a5bdecb5089
Turner Riley <turnertriley@gmail.com>
2023-01-19 09:45:07 -0800

    update event name organization

--- apps/src/lib/util/AnalyticsReporter.js
+++ apps/src/lib/util/AnalyticsReporter.js
@@ -139,6 +139,9 @@ export const EVENTS = {
   UNIT_OVERVIEW_PAGE_VISITED_EVENT: 'Unit Overview Page Visited',
   TRY_NOW_BUTTON_CLICK_EVENT: 'Try Now Button Clicked',
 
+  // Workshop enrollment
+  WORKSHOP_ENROLLMENT_COMPLETED_EVENT: 'Workshop Enrollment Completed',
+
   // PD Application flow
   TEACHER_APP_STARTED_EVENT: '6-12 Teacher Application Started',
   PAGE_CHANGED_EVENT: 'Page Changed',
@@ -149,8 +152,7 @@ export const EVENTS = {
   APPLICATION_SUBMITTED_EVENT: 'Application Submitted',
   APP_STATUS_CHANGE_EVENT: 'Application Status Changed',
   ADMIN_APPROVAL_RECEIVED_EVENT: 'Administrator Approval Received',
-  SUBMIT_RP_CONTACT_FORM_EVENT: 'Submit Regional Partner Contact Form',
-  WORKSHOP_ENROLLMENT_COMPLETED_EVENT: 'Workshop Enrollment Completed'
+  SUBMIT_RP_CONTACT_FORM_EVENT: 'Submit Regional Partner Contact Form'
 };
 
 const analyticsReporter = new AnalyticsReporter();
