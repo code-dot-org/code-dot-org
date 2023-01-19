24f4bbd6eb1faa9002b1236e0a1be36c2cc0295f
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2023-01-19 11:46:11 -0600

    update file to include changes made to upstream (#49854)
    Update MBFirmataClient.js to include changes made to upstream

--- apps/src/third-party/maker/MBFirmataClient.js
+++ apps/src/third-party/maker/MBFirmataClient.js
@@ -112,6 +112,7 @@ class MicrobitFirmataClient {
     this.MB_SCROLL_INTEGER			= 0x05
     this.MB_SET_TOUCH_MODE			= 0x06
     this.MB_DISPLAY_ENABLE			= 0x07
+    this.MB_COMPASS_CALIBRATE   = 0x08
     // 0x08-0x0C reserved for additional micro:bit messages
     this.MB_REPORT_EVENT			= 0x0D
     this.MB_DEBUG_STRING			= 0x0E
@@ -188,6 +189,18 @@ class MicrobitFirmataClient {
       }
       return null;
     })
+  }
+
+  isConnected() {
+    // Return true or false if port connected
+
+    if (this.myPort) {
+      console.log('Is Connected True', this.myPort.path);
+      return true;
+    } else {
+      console.log('Is Connected False', this.myPort.path);
+      return false;
+    }
   }
 
   disconnect() {
@@ -517,6 +530,12 @@ class MicrobitFirmataClient {
     this.myPort.write([this.SYSEX_START, this.SAMPLING_INTERVAL,
       samplingMSecs & 0x7F, (samplingMSecs >> 7) & 0x7F,
       this.SYSEX_END]);
+  }
+
+  compassCalibration() {
+    // Request that the micro:bit perform a compass calibration cycle
+
+    this.myPort.write([this.SYSEX_START, this.MB_COMPASS_CALIBRATE, this.SYSEX_END]);
   }
 
   enableLightSensor() {
