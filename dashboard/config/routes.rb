151cb479896bab194d4830b1f9074a83b487aefa
rshipp <ryan.shipp@code.org>
2023-01-23 15:18:22 -0600

    Move abuse APIs to Rails

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -1017,6 +1017,11 @@ Dashboard::Application.routes.draw do
     get 'project_commits/get_token', to: 'project_commits#get_token'
     get 'project_commits/:channel_id', to: 'project_commits#project_commits'
 
+    # partial ports of legacy v3 APIs
+    get '/v3/channels/:channel_id/abuse', to: 'channels_api#show_abuse'
+    post '/v3/channels/:channel_id/abuse', to: 'channels_api#update_abuse'
+    patch '/v3/(:endpoint)/:channel_id', constraints: {endpoint: /(animations|assets|sources|files|libraries)/}, to: 'files_api#update'
+
     # offline-service-worker*.js needs to be loaded the the root level of the
     # domain('studio.code.org/').
     # Matches on ".js" or ".map" in order to serve source-map files for the service worker javascript.
