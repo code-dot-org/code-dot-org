61004bf418a161c972df229d1061c79729e70160
rshipp <ryan.shipp@code.org>
2023-01-24 23:10:43 -0600

    Add channels tests

--- dashboard/app/controllers/channels_api_controller.rb
+++ dashboard/app/controllers/channels_api_controller.rb
@@ -21,9 +21,10 @@ class ChannelsApiController < ApplicationController
     #
     # Temporarily ignore anonymous reports and only allow verified teachers
     # and signed in users to report.
+    channel_id = params[:channel_id]
     restrict_reporting_to_verified_users = DCDO.get('restrict-abuse-reporting-to-verified', true)
     amount =
-      if verified_teacher?
+      if current_user&.verified_teacher?
         20
       elsif current_user && !restrict_reporting_to_verified_users
         10
@@ -31,10 +32,34 @@ class ChannelsApiController < ApplicationController
         0
       end
     begin
-      value = Projects.new(get_storage_id).increment_abuse(id, amount)
+      value = Projects.new(get_storage_id).increment_abuse(channel_id, amount)
     rescue ArgumentError, OpenSSL::Cipher::CipherError
       raise ActionController::BadRequest.new, "Bad channel_id"
     end
     render json: {abuse_score: value}
+  end
+
+  # DELETE /v3/channels/:channel_id/abuse
+  # POST /v3/channels/:channel_id/abuse/delete
+  # Clear an abuse score. Requires project_validator permission
+  def destroy_abuse
+    return head :unauthorized unless project_validator?
+
+    channel_id = params[:channel_id]
+
+    begin
+      value = Projects.new(get_storage_id).reset_abuse(channel_id)
+    rescue ArgumentError, OpenSSL::Cipher::CipherError
+      raise ActionController::BadRequest.new, "Bad channel_id"
+    end
+    render json: {abuse_score: value}
+  end
+
+  private
+
+  def project_validator?
+    return false unless current_user
+    return true if current_user.permission?(UserPermission::PROJECT_VALIDATOR)
+    false
   end
 end
---
+++ dashboard/test/controllers/channels_api_controller_test.rb
@@ -0,0 +1,76 @@
+require 'test_helper'
+
+class ChannelsApiControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+
+  setup do
+    @storage = create(:project_storage)
+    @storage_id = @storage.id
+    @projects = Projects.new(@storage_id)
+    @channel_id = @projects.create({}, ip: '10.0.0.1')
+    ChannelsApiController.any_instance.stubs(:get_storage_id).returns(@storage_id)
+  end
+
+  test "get abuse score" do
+    response = get :show_abuse, params: {channel_id: @channel_id}
+    assert response.ok?
+    assert_equal 0, JSON.parse(response.body)['abuse_score']
+  end
+
+  test "post abuse score" do
+    response = post :update_abuse, params: {channel_id: @channel_id}
+    assert response.ok?
+  end
+
+  test "delete abuse score" do
+    response = get :show_abuse, params: {channel_id: @channel_id}
+    assert response.ok?
+    assert_equal 0, JSON.parse(response.body)['abuse_score']
+
+    response = delete :destroy_abuse, params: {channel_id: @channel_id}
+    assert response.unauthorized?
+
+    user = create(:project_validator)
+    sign_in user
+
+    response = delete :destroy_abuse, params: {channel_id: @channel_id}
+    assert response.ok?
+    assert_equal 0, JSON.parse(response.body)['abuse_score']
+  end
+
+  test "signed in abuse" do
+    DCDO.stubs(:get).with('restrict-abuse-reporting-to-verified', true).returns(false)
+
+    user = create(:student)
+    sign_in user
+
+    # check initial state
+    get :show_abuse, params: {channel_id: @channel_id}
+    assert response.ok?
+    assert_equal 0, JSON.parse(response.body)['abuse_score']
+
+    # authenticated non-teacher should get a score of 10
+    post :update_abuse, params: {channel_id: @channel_id}
+    assert response.ok?
+    assert_equal 10, JSON.parse(response.body)['abuse_score']
+  end
+
+  test "abuse frozen" do
+    # freeze the project
+    @projects.update(@channel_id, {frozen: true}, '10.0.0.1')
+
+    get :show_abuse, params: {channel_id: @channel_id}
+    assert response.ok?
+    assert_equal 0, JSON.parse(response.body)['abuse_score']
+
+    user = create(:student)
+    sign_in user
+
+    post :update_abuse, params: {channel_id: @channel_id}
+    assert response.ok?
+
+    get :show_abuse, params: {channel_id: @channel_id}
+    assert response.ok?
+    assert_equal 0, JSON.parse(response.body)['abuse_score']
+  end
+end
