63010a4af8cf5ada224a1834b000169cd393f108
Pablo M <108825710+pablo-code-org@users.noreply.github.com>
2023-01-24 18:50:56 -0800

    Revert "Add index for slow query in projects table"

--- dashboard/db/migrate/20230124093901_create_project_state_and_storage_id_index.rb
+++
@@ -1,9 +0,0 @@
-class CreateProjectStateAndStorageIdIndex < ActiveRecord::Migration[6.0]
-  def up
-    add_index :projects, [:storage_id, :state], name: "storage_apps_storage_id_state_index"
-  end
-
-  def down
-    remove_index :projects, [:storage_id, :state]
-  end
-end
