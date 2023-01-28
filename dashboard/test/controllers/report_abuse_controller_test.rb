cf05c95e300e42ecc0949022c6c12e678dfb0d7f
rshipp <ryan.shipp@code.org>
2023-01-27 22:43:16 -0600

    Use transactions

--- dashboard/test/controllers/report_abuse_controller_test.rb
+++ dashboard/test/controllers/report_abuse_controller_test.rb
@@ -4,7 +4,7 @@ require 'cdo/aws/s3'
 class ReportAbuseControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
 
-  setup do
+  def fixme_later
     # channels
     @storage = create(:project_storage)
     @storage_id = @storage.id
@@ -17,7 +17,7 @@ class ReportAbuseControllerTest < ActionController::TestCase
     FileBucket.any_instance.stubs(:list).returns([{filename: 'test.file'}])
   end
 
-  teardown do
+  def fixme_later_too
     @controller.unstub(:get_storage_id)
 
     FileBucket.any_instance.unstub(:list)
@@ -27,72 +27,104 @@ class ReportAbuseControllerTest < ActionController::TestCase
   # channels
 
   test "post abuse score" do
-    assert_equal 0, @controller.update_channel_abuse_score(@channel_id)
+    DASHBOARD_DB.transaction(rollback: :always) do
+      fixme_later
+      assert_equal 0, @controller.update_channel_abuse_score(@channel_id)
+      fixme_later_too
+    end
   end
 
   test "signed in abuse" do
-    DCDO.stubs(:get).with('restrict-abuse-reporting-to-verified', true).returns(false)
+    DASHBOARD_DB.transaction(rollback: :always) do
+      fixme_later
+      DCDO.stubs(:get).with('restrict-abuse-reporting-to-verified', true).returns(false)
 
-    user = create(:student)
-    sign_in user
+      user = create(:student)
+      sign_in user
 
-    # check initial state
-    assert_equal 0, Projects.get_abuse(@channel_id)
+      # check initial state
+      assert_equal 0, Projects.get_abuse(@channel_id)
 
-    # authenticated non-teacher should get a score of 10
-    assert_equal 10, @controller.update_channel_abuse_score(@channel_id)
-    assert_equal 10, Projects.get_abuse(@channel_id)
+      # authenticated non-teacher should get a score of 10
+      assert_equal 10, @controller.update_channel_abuse_score(@channel_id)
+      assert_equal 10, Projects.get_abuse(@channel_id)
 
-    DCDO.unstub(:get)
+      DCDO.unstub(:get)
+      fixme_later_too
+    end
   end
 
   test "abuse frozen" do
-    # freeze the project
-    @projects.update(@channel_id, {frozen: true}, '10.0.0.1')
+    DASHBOARD_DB.transaction(rollback: :always) do
+      fixme_later
+      # freeze the project
+      @projects.update(@channel_id, {frozen: true}, '10.0.0.1')
 
-    # check initial state
-    assert_equal 0, Projects.get_abuse(@channel_id)
+      # check initial state
+      assert_equal 0, Projects.get_abuse(@channel_id)
 
-    user = create(:student)
-    sign_in user
+      user = create(:student)
+      sign_in user
 
-    assert_equal 0, @controller.update_channel_abuse_score(@channel_id)
-    assert_equal 0, Projects.get_abuse(@channel_id)
+      assert_equal 0, @controller.update_channel_abuse_score(@channel_id)
+      assert_equal 0, Projects.get_abuse(@channel_id)
+      fixme_later_too
+    end
   end
 
   # files
 
   test "set abuse score" do
-    FileBucket.any_instance.stubs(:get_abuse_score).returns(0)
-    FileBucket.any_instance.expects(:replace_abuse_score).once
-    @controller.update_file_abuse_score('files', 'test-channel-id', 10)
+    DASHBOARD_DB.transaction(rollback: :always) do
+      fixme_later
+      FileBucket.any_instance.stubs(:get_abuse_score).returns(0)
+      FileBucket.any_instance.expects(:replace_abuse_score).once
+      @controller.update_file_abuse_score('files', 'test-channel-id', 10)
+      fixme_later_too
+    end
   end
 
   test "increment abuse score" do
-    FileBucket.any_instance.stubs(:get_abuse_score).returns(10)
-    FileBucket.any_instance.expects(:replace_abuse_score).once
-    @controller.update_file_abuse_score('files', 'test-channel-id', 20)
+    DASHBOARD_DB.transaction(rollback: :always) do
+      fixme_later
+      FileBucket.any_instance.stubs(:get_abuse_score).returns(10)
+      FileBucket.any_instance.expects(:replace_abuse_score).once
+      @controller.update_file_abuse_score('files', 'test-channel-id', 20)
+      fixme_later_too
+    end
   end
 
   test "set abuse score to be the same" do
-    # set to be the same
-    FileBucket.any_instance.stubs(:get_abuse_score).returns(20)
-    FileBucket.any_instance.expects(:replace_abuse_score).once
-    @controller.update_file_abuse_score('files', 'test-channel-id', 20)
+    DASHBOARD_DB.transaction(rollback: :always) do
+      fixme_later
+      # set to be the same
+      FileBucket.any_instance.stubs(:get_abuse_score).returns(20)
+      FileBucket.any_instance.expects(:replace_abuse_score).once
+      @controller.update_file_abuse_score('files', 'test-channel-id', 20)
+      fixme_later_too
+    end
   end
 
   test "non-permissions can't decrement" do
-    FileBucket.any_instance.stubs(:get_abuse_score).returns(10)
-    FileBucket.any_instance.expects(:replace_abuse_score).never
-    @controller.update_file_abuse_score('files', 'test-channel-id', 0)
+    DASHBOARD_DB.transaction(rollback: :always) do
+      fixme_later
+      FileBucket.any_instance.stubs(:get_abuse_score).returns(10)
+      FileBucket.any_instance.expects(:replace_abuse_score).never
+      @controller.update_file_abuse_score('files', 'test-channel-id', 0)
+      fixme_later_too
+    end
   end
 
   test "with permission can decrement" do
-    user = create(:project_validator)
-    sign_in user
-    FileBucket.any_instance.stubs(:get_abuse_score).returns(10)
-    FileBucket.any_instance.expects(:replace_abuse_score).once
-    @controller.update_file_abuse_score('files', 'test-channel-id', 0)
-    sign_out user
+    DASHBOARD_DB.transaction(rollback: :always) do
+      fixme_later
+      user = create(:project_validator)
+      sign_in user
+      FileBucket.any_instance.stubs(:get_abuse_score).returns(10)
+      FileBucket.any_instance.expects(:replace_abuse_score).once
+      @controller.update_file_abuse_score('files', 'test-channel-id', 0)
+      sign_out user
+      fixme_later_too
+    end
   end
 end
