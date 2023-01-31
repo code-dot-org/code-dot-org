233a9d7e7c4f8bd63d5101e64df51a4b059bae58
Emily Eastlake <emily.eastlake@code.org>
2023-01-31 10:36:06 -0800

    more PR feedback

--- apps/src/sites/studio/pages/weblab_host/test.js
+++ apps/src/sites/studio/pages/weblab_host/test.js
@@ -3,12 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WebLabTest from '@cdo/apps/templates/testPages/WebLabTest';
 
-const scriptData = document.querySelector('script[data-bramble]');
-const brambleConfig = JSON.parse(scriptData.dataset.bramble);
-
 $(document).ready(function() {
+  const scriptData = document.querySelector('script[data-bramble]');
+  const brambleConfig = JSON.parse(scriptData.dataset.bramble);
+
   ReactDOM.render(
-    <WebLabTest brambleConfig={brambleConfig} />,
+    <WebLabTest studioUrl={brambleConfig.studioUrl} />,
     document.getElementById('weblab-test-container')
   );
 });
