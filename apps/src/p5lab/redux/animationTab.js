02ae17fb9ab1388db074e51f3cb333c8a615fd6d
Mike Harvey <mike.harvey@code.org>
2023-02-01 13:34:41 -0500

    simplify reducer

--- apps/src/p5lab/redux/animationTab.js
+++ apps/src/p5lab/redux/animationTab.js
@@ -1,23 +1,22 @@
 /** @file Redux actions and reducer for the AnimationTab */
-import {combineReducers} from 'redux';
 
 const SELECT_ANIMATION = 'AnimationTab/SELECT_ANIMATION';
 const SET_COLUMN_SIZES = 'AnimationTab/SET_COLUMN_SIZES';
 
-export default combineReducers({
-  columnSizes,
-  selectedAnimation
-});
+const initialState = {
+  selectedAnimation: '',
+  columnSizes: [150, undefined]
+};
 
-function selectedAnimation(state, action) {
-  state = state || '';
-  switch (action.type) {
-    case SELECT_ANIMATION:
-      return action.animationKey;
-    default:
-      return state;
+export default (state = initialState, action) => {
+  if (action.type === SELECT_ANIMATION) {
+    return {...state, selectedAnimation: action.animationKey};
   }
-}
+  if (action.type === SET_COLUMN_SIZES) {
+    return {...state, columnSizes: action.sizes};
+  }
+  return state;
+};
 
 /**
  * Select an animation in the animation list.
@@ -26,21 +25,6 @@ function selectedAnimation(state, action) {
  */
 export function selectAnimation(animationKey) {
   return {type: SELECT_ANIMATION, animationKey};
-}
-
-/**
- * Subreducer to set animation tab column widths.  Expected format for
- * widths is an array of numbers, with an 'undefined' entry for a column
- * that grows to fill available space.
- */
-function columnSizes(state, action) {
-  state = state || [150, undefined];
-  switch (action.type) {
-    case SET_COLUMN_SIZES:
-      return action.sizes;
-    default:
-      return state;
-  }
 }
 
 /**
