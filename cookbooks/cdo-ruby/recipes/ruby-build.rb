4668a06e6f4fe5ec88a1858dbb60cca227c71968
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-02-01 17:22:16 -0800

    Add some guard clauses to prevent unnecessary time-consuming builds
    Also add an overwrite option to the tarball extraction; otherwise,
    trying to update to a newer version of ruby-build on an existing server
    which already has /tmp/ data will fail

--- cookbooks/cdo-ruby/recipes/ruby-build.rb
+++ cookbooks/cdo-ruby/recipes/ruby-build.rb
@@ -30,14 +30,17 @@ end
 
 archive_file '/tmp/ruby-build.tar.gz' do
   destination '/tmp/ruby-build'
+  overwrite :auto
 end
 
 execute 'install ruby-build' do
   cwd "/tmp/ruby-build/ruby-build-#{RUBY_BUILD_VERSION}"
   command './install.sh'
+  not_if "which ruby-build && ruby-build --version | grep --quiet --fixed-strings 'ruby-build #{RUBY_BUILD_VERSION}'"
 end
 
 execute 'install ruby with ruby build' do
   # target /usr for consistency; that's where our old apt approach targeted
   command "ruby-build #{node['cdo-ruby']['version']} /usr"
+  not_if "which ruby && ruby --version | grep --quiet '^ruby #{node['cdo-ruby']['version']}'"
 end
