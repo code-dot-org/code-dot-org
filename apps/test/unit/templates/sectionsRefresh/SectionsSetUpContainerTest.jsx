8f0fae7da5bb3ab1a4ddb6abac6cc82beb16bb3c
Bethany Connor <46464143+bethanyaconnor@users.noreply.github.com>
2023-02-02 12:26:05 -0500

    New section create page (#49782)
    * Initial set up work for a new section creation page
    
    * Add form and buttons
    
    * Use state for sections
    
    * Add tests
    
    * Restrict page to admin users and add a test
    
    * Add the entrypoint for the new section page
    
    * Update link and add comments
    
    * Rename components

---
+++ apps/test/unit/templates/sectionsRefresh/SectionsSetUpContainerTest.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from '../../../util/reconfiguredChai';
+import SectionsSetUpContainer from '@cdo/apps/templates/sectionsRefresh/SectionsSetUpContainer';
+
+describe('SectionsSetUpContainer', () => {
+  it('renders an initial set up section form', () => {
+    const wrapper = shallow(<SectionsSetUpContainer />);
+
+    expect(wrapper.find('SingleSectionSetUp').length).to.equal(1);
+  });
+
+  it('renders headers and button', () => {
+    const wrapper = shallow(<SectionsSetUpContainer />);
+
+    expect(wrapper.find('h1').length).to.equal(1);
+    expect(wrapper.find('Button').length).to.equal(2);
+    expect(
+      wrapper
+        .find('Button')
+        .at(0)
+        .props().text
+    ).to.equal('Add Another Class Section');
+    expect(
+      wrapper
+        .find('Button')
+        .at(1)
+        .props().text
+    ).to.equal('Save class sections');
+  });
+});
