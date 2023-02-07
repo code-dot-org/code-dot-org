bad90981459f18d573e8ac6859bffa35ae7dc239
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-02-06 18:17:17 -0800

    recursively link everything in the blockly/css directory, to pick up the droplet and tooltipster stylesheets

--- dashboard/app/assets/config/manifest.js
+++ dashboard/app/assets/config/manifest.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a24206830157fcd17152eae09889f6ea3223695bb51cb23122fc01d87db531cb
-size 793
+oid sha256:ba8f7ec2f0ff0bb1212db45464abdc25f507bc6868db53475158b0a5b3620031
+size 788
