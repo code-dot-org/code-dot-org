cab7c2c746b9c550c09d56f16ca4efd65d156f8f
breville <brendan@code.org>
2023-02-07 18:47:07 +0000

    Add some comments.

--- apps/src/music/player/ProgramSequencer.js
+++ apps/src/music/player/ProgramSequencer.js
@@ -1,3 +1,11 @@
+// The ProgramSequencer supports the play_together and play_sequential blocks
+// in the Simple2 programming model.  It maintains a stack with context for
+// each play_together or play_sequential grouping, can return the current
+// measure whenever we want to play, and can update the playhead whenever a
+// sound has been played.  It is intended to be called by the generator functions
+// in the block model, in the order that blocks are encountered when the code
+// to be run is generated.
+
 export default class ProgramSequencer {
   constructor() {
     this.stack = [];
@@ -7,6 +15,8 @@ export default class ProgramSequencer {
     this.stack = [];
   }
 
+  // Internal helper to get the entry at the top of the stack, or null
+  // if the stack is empty.
   getCurrentStackEntry() {
     if (this.stack.length > 0) {
       return this.stack[this.stack.length - 1];
@@ -15,17 +25,20 @@ export default class ProgramSequencer {
     }
   }
 
+  // Beginning of a play_sequential block.
   playSequential() {
     const measure =
       this.stack.length === 0 ? 1 : this.getCurrentStackEntry().measure;
     this.stack.push({measure: measure, together: false});
   }
 
+  // End of a play_sequential block.
   endSequential() {
     const currentStackEntry = this.getCurrentStackEntry();
 
     const nextMeasure = currentStackEntry.measure;
 
+    // We are returning to the previous stack frame.
     this.stack.pop();
 
     const nextStackEntry = this.getCurrentStackEntry();
@@ -40,6 +53,7 @@ export default class ProgramSequencer {
     }
   }
 
+  // Beginning of a play_together block.
   playTogether() {
     var nextMeasure =
       this.stack.length === 0 ? 1 : this.getCurrentStackEntry().measure;
@@ -50,18 +64,19 @@ export default class ProgramSequencer {
     });
   }
 
+  // End of an play_together block.
   endTogether() {
     const currentStackEntry = this.getCurrentStackEntry();
 
     var nextMeasure = Math.max.apply(Math, currentStackEntry.lastMeasures);
 
-    // we are returning to the previous stack frame.
+    // We are returning to the previous stack frame.
     this.stack.pop();
 
     const nextStackEntry = this.getCurrentStackEntry();
 
     if (nextStackEntry) {
-      // now the frame we are returning to has to absorb this information.
+      // Now the frame we are returning to has to absorb this information.
       if (nextStackEntry.together) {
         nextStackEntry.lastMeasures.push(nextMeasure);
       } else {
@@ -70,10 +85,15 @@ export default class ProgramSequencer {
     }
   }
 
+  // When the caller wants to play a sound, it calls this to determine at
+  // which measure it should be placed.
   getCurrentMeasure() {
     return this.getCurrentStackEntry().measure;
   }
 
+  // When the caller has played a sound, it calls this so that the system
+  // can update the playhead for the length of sound that was played at
+  // what was the current measure.
   updateMeasureForPlayByLength(length) {
     const currentStackEntry = this.getCurrentStackEntry();
 
