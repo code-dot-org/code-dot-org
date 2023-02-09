455907fa042e87242d396edd9f7be014bad8fd6a
Turner Riley <turnertriley@gmail.com>
2023-02-09 14:36:41 -0800

    remove unnecessary custom event listener

--- apps/src/sites/code.org/pages/views/analytics_event_log_helper.js
+++ apps/src/sites/code.org/pages/views/analytics_event_log_helper.js
@@ -2,11 +2,6 @@ import $ from 'jquery';
 import analyticsReporter from '@cdo/apps/lib/util/AnalyticsReporter';
 import {EVENTS} from '@cdo/apps/lib/util/AnalyticsConstants';
 
-// Listen for triggered events to be logged.
-document.addEventListener('customLogEvent', e => {
-  logEvent(e?.detail?.eventName, e?.detail?.properties);
-});
-
 // Log visits to .erb file pages on document.ready.
 $(document).ready(() => {
   const eventLogger = document.getElementById('analytics-event-log-helper');
