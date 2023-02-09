de2317b14abe06f9434f3478116510379efa5202
Turner Riley <turnertriley@gmail.com>
2023-02-09 12:12:49 -0800

    update entry point and remove javascript in haml for sign_up user login type

--- apps/Gruntfile.js
+++ apps/Gruntfile.js
@@ -596,6 +596,10 @@ describe('entry tests', () => {
       './src/sites/studio/pages/programming_environments/show.js',
     'programming_expressions/show':
       './src/sites/studio/pages/programming_expressions/show.js',
+    'devise/registrations/_sign_up':
+      './src/sites/studio/pages/devise/registrations/_sign_up.js',
+    'devise/shared/_oauth_links':
+      './src/sites/studio/pages/devise/shared/_oauth_links.js',
     'devise/registrations/_finish_sign_up':
       './src/sites/studio/pages/devise/registrations/_finish_sign_up.js',
     'devise/registrations/edit':
