6dc48c8cfdd6b1255c46d9db02b20a795b6bc64a
breville <brendan@code.org>
2023-02-10 18:26:50 +0000

    Add comment

--- apps/src/music/blockly/themes.js
+++ apps/src/music/blockly/themes.js
@@ -6,6 +6,9 @@ const blockStyles = cdoBlockStyles;
 const categoryStyles = {};
 const componentStyles = {
   toolboxBackgroundColour: '#424242', // gray-800
+  // The flyout background is especially dark so that workspace blocks
+  // underneath the flyout (which is semi-transparent) are obscured enough
+  // to make the blocks in the flyout easy to see.
   flyoutBackgroundColour: '#121212',
   workspaceBackgroundColour: '#212121' // gray-900
 };
