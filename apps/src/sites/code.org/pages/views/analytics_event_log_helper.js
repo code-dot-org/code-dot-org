4e500ef0d30a982f2f5a5c82b04275b39529672b
Turner Riley <turnertriley@gmail.com>
2023-02-10 14:20:25 -0800

    update comment

--- apps/src/sites/code.org/pages/views/analytics_event_log_helper.js
+++ apps/src/sites/code.org/pages/views/analytics_event_log_helper.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import analyticsReporter from '@cdo/apps/lib/util/AnalyticsReporter';
 
-// Log visits to .erb file pages on document.ready.
+// Log page visits on document.ready.
 $(document).ready(() => {
   const eventLogger = document.getElementById('analytics-event-log-helper');
   const pageVisitEventName = eventLogger?.dataset?.eventName;
