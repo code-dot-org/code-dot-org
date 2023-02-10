ebce4de0c755f1310444784dce8269cdd1b6fb4e
Mario Gil Correa <mario.gil-correa@code.org>
2023-02-10 16:05:50 -0600

    students beyond code storybook updated

--- apps/src/templates/certificates/Congrats.story.jsx
+++ apps/src/templates/certificates/Congrats.story.jsx
@@ -4,7 +4,7 @@ import {reduxStore} from '@cdo/storybook/decorators';
 import {Provider} from 'react-redux';
 
 export default {
-  title: 'Congrats',
+  title: 'Congrats/Congrats',
   component: Congrats
 };
 
@@ -12,7 +12,6 @@ const Template = args => (
   <Provider store={reduxStore()}>
     <Congrats
       {...args}
-      language={'en'}
       initialCertificateImageUrl={'/images/placeholder-hoc-image.jpg'}
     />
   </Provider>
@@ -21,35 +20,41 @@ const Template = args => (
 export const CongratsAppLabSignedOut = Template.bind({});
 CongratsAppLabSignedOut.args = {
   tutorial: 'applab-intro',
-  userType: 'signedOut'
+  userType: 'signedOut',
+  language: 'en'
 };
 
 export const CongratsAppLabStudent = Template.bind({});
 CongratsAppLabStudent.args = {
   tutorial: 'applab-intro',
-  userType: 'student'
+  userType: 'student',
+  language: 'en'
 };
 
 export const CongratsMinecraftPre2017SignedOut = Template.bind({});
 CongratsMinecraftPre2017SignedOut.args = {
-  tutorial: 'minecraft'
+  tutorial: 'minecraft',
+  language: 'en'
 };
 
 export const CongratsMinecraftPre2017Student = Template.bind({});
 CongratsMinecraftPre2017Student.args = {
   tutorial: 'minecraft',
-  userType: 'student'
+  userType: 'student',
+  language: 'en'
 };
 
 export const Congrats2017MinecraftSignedout = Template.bind({});
 Congrats2017MinecraftSignedout.args = {
-  tutorial: 'hero'
+  tutorial: 'hero',
+  language: 'en'
 };
 
 export const Congrats2017MinecraftStudent = Template.bind({});
 Congrats2017MinecraftStudent.args = {
   tutorial: 'hero',
-  userType: 'student'
+  userType: 'student',
+  language: 'en'
 };
 
 export const Congrats2017MinecraftStudentKorean = Template.bind({});
@@ -61,17 +66,23 @@ Congrats2017MinecraftStudentKorean.args = {
 
 export const Congrats2018MinecraftSignedOut = Template.bind({});
 Congrats2018MinecraftSignedOut.args = {
-  tutorial: 'aquatic'
+  tutorial: 'aquatic',
+  language: 'en'
 };
 
 export const CongratsOtherSignedOut = Template.bind({});
+CongratsOtherSignedOut.args = {
+  language: 'en'
+};
 
 export const CongratsOtherStudent = Template.bind({});
 CongratsOtherSignedOut.args = {
-  userType: 'student'
+  userType: 'student',
+  language: 'en'
 };
 
 export const CongratsOtherTeacher = Template.bind({});
 CongratsOtherTeacher.args = {
-  userType: 'teacher'
+  userType: 'teacher',
+  language: 'en'
 };
