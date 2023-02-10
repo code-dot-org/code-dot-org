70b493c301f74e3dd2d98d90818ee2c78ed4b1b0
Meg Crenshaw <meg@code.org>
2023-02-10 06:50:48 -0500

    Only lint mailer rb files

--- tools/customLinters/rubocop-consistent-quotation-mark.rb
+++ tools/customLinters/rubocop-consistent-quotation-mark.rb
@@ -4,14 +4,12 @@ module CustomCops
       (send nil? :argument (:sym $_) ...)
     PATTERN
 
-    # rubocop:disable CustomCops/ConsistentQuotationMark
     NOT_ALLOWED_CHARACTERS = [
       '“', # U+201C Left Double Quotation Mark
       '”', # U+201D Right Double Quotation Mark
       '‘', # U+2018 Left Single Quotation Mark
       '’', # U+2019 Right Single Quotation Mark
     ].freeze
-    # rubocop:enable CustomCops/ConsistentQuotationMark
 
     NOT_ALLOWED_REGEX = /[#{NOT_ALLOWED_CHARACTERS.join}]/
 
