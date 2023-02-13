a029e3e9e3422b90d680b5aa4cf6d1e582b474cb
Meg Crenshaw <meg@code.org>
2023-02-13 10:33:45 -0500

    Add test for CSD Summer Workshop

---
+++ dashboard/test/integration/enrollment_forms_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class EnrollmentFormsTest < ActionDispatch::IntegrationTest
+  setup do
+    @teacher = create :teacher
+
+    @base_params = {
+      first_name: "mx",
+      last_name: "teacher",
+      email: "teacher@test.xx",
+      school_info: {school_id: School.first.id},
+    }
+  end
+
+  test 'can enroll in workshop CSD Summer Workshop' do
+    sign_in @teacher
+
+    workshop = create :csd_summer_workshop
+
+    assert_nil Pd::Enrollment.find_by(pd_workshop_id: workshop.id)
+
+    post "/api/v1/pd/workshops/#{workshop.id}/enrollments", params: @base_params
+
+    assert_response :success
+
+    refute_nil Pd::Enrollment.find_by(pd_workshop_id: workshop.id)
+  end
+end
