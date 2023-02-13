0b12c86336cdde1a37cb4cb128eb864ab0887730
breville <brendan@code.org>
2023-02-13 07:58:17 +0000

    Removed unused value

--- apps/src/music/views/TimelineElement.jsx
+++ apps/src/music/views/TimelineElement.jsx
@@ -9,8 +9,7 @@ const typeToColorClass = {
   beat: moduleStyles.timelineElementPurple,
   bass: moduleStyles.timelineElementBlue,
   lead: moduleStyles.timelineElementGreen,
-  fx: moduleStyles.timelineElementYellow,
-  active: moduleStyles.timelineElementWhite
+  fx: moduleStyles.timelineElementYellow
 };
 
 /**
