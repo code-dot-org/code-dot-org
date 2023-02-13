5c060640b28db5f20ee09a420ad6b34666611b0b
rshipp <ryan.shipp@code.org>
2023-02-13 16:32:18 -0600

    Add basic multi grade selector

--- apps/src/templates/sectionsRefresh/SingleSectionSetUp.jsx
+++ apps/src/templates/sectionsRefresh/SingleSectionSetUp.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import MultiSelectGroup from '@cdo/apps/templates/teacherDashboard/MultiSelectGroup';
 import moduleStyles from './sections-refresh.module.scss';
 import i18n from '@cdo/locale';
 
@@ -8,6 +9,25 @@ export default function SingleSectionSetUp({
   section,
   updateSection
 }) {
+  const options = [
+    'K',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12'
+  ].map(g => ({label: g, value: g}));
+  const [values, setValues] = useState(
+    Object.fromEntries(options.map(o => [o.value, false]))
+  );
+
   return (
     <div>
       <h2>{i18n.classSectionNum({num: sectionNum})}</h2>
@@ -20,6 +40,14 @@ export default function SingleSectionSetUp({
           onChange={e => updateSection('name', e.target.value)}
         />
       </label>
+      <MultiSelectGroup
+        label="Pick at least one grade"
+        name="grades"
+        required={true}
+        options={options}
+        values={values}
+        setValues={setValues}
+      />
       <hr />
     </div>
   );
