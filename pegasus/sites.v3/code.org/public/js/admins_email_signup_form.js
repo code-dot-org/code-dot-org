87d95acc80f31094b06199f72cd0b8cec577d22a
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2023-02-14 11:44:21 -0800

    add link to recaptcha instructions

--- pegasus/sites.v3/code.org/public/js/admins_email_signup_form.js
+++ pegasus/sites.v3/code.org/public/js/admins_email_signup_form.js
@@ -1,4 +1,5 @@
 // Code provided from Google reCAPTCHA
+// https://www.sfdcpoint.com/salesforce/salesforce-web-to-lead-form-with-recaptcha/
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
 
 setInterval(timestamp, 500);
