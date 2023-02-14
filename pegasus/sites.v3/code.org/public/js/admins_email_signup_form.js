dd288f1b07cbbeefe1aad70495117cbfe9a35e82
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2023-02-14 11:03:33 -0800

    clean up js

--- pegasus/sites.v3/code.org/public/js/admins_email_signup_form.js
+++ pegasus/sites.v3/code.org/public/js/admins_email_signup_form.js
@@ -1,23 +1,11 @@
 // Code provided from Google reCAPTCHA
-function timestamp() {
-  var response = document.getElementById("g-recaptcha-response");
-
-  if (response === null || response.value.trim() === "") {
-    var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
-    elems["ts"] = JSON.stringify(new Date().getTime());
-    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
-  }
-}
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
 
 setInterval(timestamp, 500);
 
 // Set a disabled attribute on button until
 // the reCAPTCHA checkbox is clicked
 function recaptchaCallback() {
-  // Check reCAPTCHA callback response
-  // docs: https://developers.google.com/recaptcha/docs/display
-  console.log(grecaptcha.getResponse());
-
   let btnSubmit = document.querySelector("button");
 
   if (btnSubmit.hasAttribute("disabled")) {
