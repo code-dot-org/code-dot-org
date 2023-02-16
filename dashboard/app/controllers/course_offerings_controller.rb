46d7cfdf6fb7b4dacac7c0ba3d2cff5f273daa7d
Bethany Connor <bethany@code.org>
2023-02-16 09:45:26 -0500

    Use shared constants

--- dashboard/app/controllers/course_offerings_controller.rb
+++ dashboard/app/controllers/course_offerings_controller.rb
@@ -26,40 +26,40 @@ class CourseOfferingsController < ApplicationController
     offerings = {}
 
     offerings[:elementary] = {
-      Course: {
-        'CS Fundamentals': [
+      Curriculum::SharedCourseConstants::COURSE_OFFERING_CURRICULUM_TYPES.course => {
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.csf => [
           CourseOffering.find_by_key('coursea').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('courseb').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('coursec').summarize_for_quick_assign(current_user, request.locale)
         ],
-        'Express Courses': [
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.express => [
           CourseOffering.find_by_key('express').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('pre-express').summarize_for_quick_assign(current_user, request.locale)
         ]
       },
-      Module: {
-        'CS Connections': [
+      Curriculum::SharedCourseConstants::COURSE_OFFERING_CURRICULUM_TYPES.module => {
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.csc => [
           CourseOffering.find_by_key('poetry').summarize_for_quick_assign(current_user, request.locale)
         ]
       }
     }
 
     offerings[:middle] = {
-      Course: {
-        'Express Courses': [
+      Curriculum::SharedCourseConstants::COURSE_OFFERING_CURRICULUM_TYPES.course => {
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.express => [
           CourseOffering.find_by_key('express').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('pre-express').summarize_for_quick_assign(current_user, request.locale)
         ],
-        'Year-Long Courses': [
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.year_long => [
           CourseOffering.find_by_key('csd').summarize_for_quick_assign(current_user, request.locale)
         ]
       },
-      'Standalone Unit': {
-        'Self-Paced': [
+      Curriculum::SharedCourseConstants::COURSE_OFFERING_CURRICULUM_TYPES.standalone_unit => {
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.self_paced => [
           CourseOffering.find_by_key('csd3-virtual').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('csp3-virtual').summarize_for_quick_assign(current_user, request.locale)
         ],
-        'Teacher-Led': [
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.teacher_led => [
           CourseOffering.find_by_key('aiml').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('devices').summarize_for_quick_assign(current_user, request.locale)
         ]
@@ -67,23 +67,23 @@ class CourseOfferingsController < ApplicationController
     }
 
     offerings[:high] = {
-      Course: {
-        'Year-Long Courses': [
+      Curriculum::SharedCourseConstants::COURSE_OFFERING_CURRICULUM_TYPES.course => {
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.year_long => [
           CourseOffering.find_by_key('csa').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('csd').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('csp').summarize_for_quick_assign(current_user, request.locale)
         ]
       },
-      'Standalone Unit': {
-        'CSA Labs': [
+      Curriculum::SharedCourseConstants::COURSE_OFFERING_CURRICULUM_TYPES.standalone_unit => {
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.csa_labs => [
           CourseOffering.find_by_key('csa-collegeboard-labs').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('csa-data-lab').summarize_for_quick_assign(current_user, request.locale)
         ],
-        'Self-Paced': [
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.self_paced => [
           CourseOffering.find_by_key('csd3-virtual').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('csp3-virtual').summarize_for_quick_assign(current_user, request.locale)
         ],
-        'Teacher-Led': [
+        Curriculum::SharedCourseConstants::COURSE_OFFERING_HEADERS.teacher_led => [
           CourseOffering.find_by_key('aiml').summarize_for_quick_assign(current_user, request.locale),
           CourseOffering.find_by_key('devices').summarize_for_quick_assign(current_user, request.locale)
         ]
