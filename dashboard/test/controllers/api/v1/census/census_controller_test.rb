bcba586960e09788bbf1eb0279c9cfff7b3b5bc4
Turner Riley <turnertriley@gmail.com>
2023-02-21 15:38:14 -0800

    update census controller unit tests to check year

--- dashboard/test/controllers/api/v1/census/census_controller_test.rb
+++ dashboard/test/controllers/api/v1/census/census_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'census_helper'
 
 class Api::V1::Census::CensusControllerTest < ActionController::TestCase
   test 'census submission with bad version fails' do
@@ -47,6 +48,45 @@ class Api::V1::Census::CensusControllerTest < ActionController::TestCase
     assert_response 201
     response = JSON.parse(@response.body)
     refute response['census_submission_id'].nil?, "census_submission_id expected in response: #{response}"
+  end
+
+  test 'HoC census submission uses current_census_year' do
+    post :create,
+      params: {
+        form_version: 'CensusHoc2017v3',
+        nces_school_s: '60000113717',
+        submitter_email_address: "fake@email.address",
+        submitter_name: "Somebody",
+        opt_in: true
+      }
+    assert_response 201
+    response = JSON.parse(@response.body)
+    submission_id = response['census_submission_id']
+    submission = Census::CensusSubmission.find(submission_id)
+    refute submission.nil?
+    assert_equal current_census_year, submission.school_year
+  end
+
+  test 'yourschool census submission uses current_census_year' do
+    post :create,
+      params: {
+        form_version: 'CensusYourSchool2017v4',
+        nces_school_s: '60000113717',
+        submitter_email_address: "fake@email.address",
+        submitter_name: "Somebody",
+        submitter_role: 'OTHER',
+        how_many_do_hoc: 'NONE',
+        how_many_after_school: 'NONE',
+        how_many_10_hours: 'NONE',
+        how_many_20_hours: 'NONE',
+        opt_in: true
+      }
+    assert_response 201, @response.body.to_s
+    response = JSON.parse(@response.body)
+    submission_id = response['census_submission_id']
+    submission = Census::CensusSubmission.find(submission_id)
+    refute submission.nil?
+    assert_equal current_census_year, submission.school_year
   end
 
   test 'yourschool census submission with good school succeeds' do
