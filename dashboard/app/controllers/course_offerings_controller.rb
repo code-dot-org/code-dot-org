356694e25aabcf3a9e4a8bb250de39018a1c1686
Bethany Connor <bethany@code.org>
2023-02-23 09:33:23 -0500

    Move business logic out of the controller

--- dashboard/app/controllers/course_offerings_controller.rb
+++ dashboard/app/controllers/course_offerings_controller.rb
@@ -25,17 +25,7 @@ class CourseOfferingsController < ApplicationController
   def quick_assign_course_offerings
     return head :forbidden unless current_user
 
-    offerings = {}
-
-    assignable_offerings = CourseOffering.assignable_course_offerings(current_user)
-    assignable_elementary_offerings = assignable_offerings.filter(&:elementary_school_level?)
-    assignable_middle_offerings = assignable_offerings.filter(&:middle_school_level?)
-    assignable_high_offerings = assignable_offerings.filter(&:high_school_level?)
-
-    offerings[:elementary] = group_offerings_for_quick_assign(assignable_elementary_offerings)
-    offerings[:middle] = group_offerings_for_quick_assign(assignable_middle_offerings)
-    offerings[:high] = group_offerings_for_quick_assign(assignable_high_offerings)
-
+    offerings = CourseOffering.quick_assign_course_offerings(current_user, request.locale)
     render :ok, json: offerings.to_json
   end
 
@@ -43,25 +33,5 @@ class CourseOfferingsController < ApplicationController
 
   def course_offering_params
     params.permit(:display_name, :is_featured, :category, :assignable, :grade_levels, :curriculum_type, :header, :marketing_initiative).to_h
-  end
-
-  def group_offerings_for_quick_assign(course_offerings)
-    data = {}
-    course_offerings.each do |co|
-      next if co.header.blank? || co.curriculum_type.blank?
-
-      data[co.curriculum_type] ||= {}
-      data[co.curriculum_type][co.header] ||= []
-      data[co.curriculum_type][co.header].append(co.summarize_for_quick_assign(current_user, request.locale))
-    end
-
-    data.keys.each do |curriculum_type|
-      data[curriculum_type].keys.each do |header|
-        data[curriculum_type][header].sort_by! {|co| co[:display_name]}
-      end
-      data[curriculum_type] = data[curriculum_type].sort.to_h
-    end
-
-    data
   end
 end
