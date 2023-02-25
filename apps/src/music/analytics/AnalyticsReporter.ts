1f80e1d8215cbcb83bfc12a8e3c584f63f6e7bf2
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-02-24 16:10:40 -0800

    Music Lab: convert AnalyticsReporter to TS

--- apps/src/music/analytics/AnalyticsReporter.ts
+++ apps/src/music/analytics/AnalyticsReporter.ts
@@ -6,10 +6,22 @@ import {
   setSessionId,
   flush
 } from '@amplitude/analytics-browser';
-import {BlockTypes} from '@cdo/apps/music/blockly/blockTypes';
-import {FIELD_SOUNDS_NAME} from '../blockly/constants';
+import {Block} from 'blockly';
+
+// Using require() to import JS in TS files
+const BlockTypes = require('../blockly/blockTypes').BlockTypes;
+const BlocklyConstants = require('../blockly/constants');
 
 const API_KEY_ENDPOINT = '/musiclab/analytics_key';
+
+interface BlockStats {
+  endingBlockCount: number;
+  endingTriggerBlockCount: number;
+  endingTriggerBlocksWithCode: number;
+  maxBlockCount: number;
+  maxTriggerBlockCount: number;
+  maxTriggerBlocksWithCode: number;
+}
 
 /**
  * An analytics reporter specifically used for the Music Lab prototype, which logs analytics
@@ -17,11 +29,19 @@ const API_KEY_ENDPOINT = '/musiclab/analytics_key';
  * outside of Music Lab, check {@link apps/src/lib/util/AnalyticsReporter}.
  */
 export default class AnalyticsReporter {
+  private sessionInProgress: boolean;
+  private identifyObj: Identify;
+  private sessionStartTime: number;
+  private maxInstructionsSeen: number;
+  private currentInstructionsPage: number;
+  private soundsUsed: Set<string>;
+  private blockStats: BlockStats;
+
   constructor() {
     this.sessionInProgress = false;
 
     this.identifyObj = new Identify();
-    this.sessionStartTime = null;
+    this.sessionStartTime = -1;
     this.maxInstructionsSeen = 1;
     this.currentInstructionsPage = 1;
     this.soundsUsed = new Set();
@@ -46,13 +66,13 @@ export default class AnalyticsReporter {
     this.sessionInProgress = true;
   }
 
-  async initialize() {
+  private async initialize() {
     const response = await fetch(API_KEY_ENDPOINT);
     const responseJson = await response.json();
     return init(responseJson.key, undefined, {minIdLength: 1}).promise;
   }
 
-  setUserProperties(userId, userType, signInState) {
+  setUserProperties(userId: string, userType: string, signInState: string) {
     if (!this.sessionInProgress) {
       this.log('No session in progress');
       return;
@@ -71,7 +91,7 @@ export default class AnalyticsReporter {
     );
   }
 
-  onButtonClicked(buttonName, properties) {
+  onButtonClicked(buttonName: string, properties: any) {
     if (!this.sessionInProgress) {
       this.log('No session in progress');
       return;
@@ -83,7 +103,7 @@ export default class AnalyticsReporter {
     track('Button clicked', {buttonName, ...properties}).promise;
   }
 
-  onInstructionsVisited(page) {
+  onInstructionsVisited(page: number) {
     if (!this.sessionInProgress) {
       this.log('No session in progress');
       return;
@@ -93,7 +113,7 @@ export default class AnalyticsReporter {
     this.maxInstructionsSeen = Math.max(this.maxInstructionsSeen, page);
   }
 
-  onBlocksUpdated(blocks) {
+  onBlocksUpdated(blocks: Block[]) {
     if (!this.sessionInProgress) {
       this.log('No session in progress');
       return;
@@ -112,13 +132,15 @@ export default class AnalyticsReporter {
         ].includes(block.type)
       ) {
         triggerBlocksCount++;
-        if (block.getChildren().length > 0) {
+        if (block.getChildren(false).length > 0) {
           triggerBlocksWithCode++;
         }
       }
 
-      if (block.getField(FIELD_SOUNDS_NAME)) {
-        this.soundsUsed.add(block.getFieldValue(FIELD_SOUNDS_NAME));
+      if (block.getField(BlocklyConstants.FIELD_SOUNDS_NAME)) {
+        this.soundsUsed.add(
+          block.getFieldValue(BlocklyConstants.FIELD_SOUNDS_NAME)
+        );
       }
     });
 
@@ -144,7 +166,7 @@ export default class AnalyticsReporter {
       return;
     }
     const duration = Date.now() - this.sessionStartTime;
-    this.sessionStartTime = null;
+    this.sessionStartTime = -1;
     this.sessionInProgress = false;
 
     identify(this.identifyObj);
@@ -161,7 +183,7 @@ export default class AnalyticsReporter {
     this.log(`Session end. Payload: ${JSON.stringify(payload)}`);
   }
 
-  log(message) {
+  private log(message: string) {
     console.log(`[AMPLITUDE ANALYTICS EVENT]: ${message}`);
   }
 }
