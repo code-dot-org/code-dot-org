be805c73eb6505946cc4d77a64a075f97be28567
Mike Harvey <mike.harvey@code.org>
2023-02-27 11:05:03 -0500

    remove thrasos overrides

--- apps/src/blockly/addons/cdoRendererThrasos.js
+++ apps/src/blockly/addons/cdoRendererThrasos.js
@@ -1,13 +1,9 @@
 import GoogleBlockly from 'blockly/core';
-import CdoPathObject from './cdoPathObjectThrasos';
 
 export default class CdoRendererThrasos extends GoogleBlockly.thrasos.Renderer {
-  /**
-   * @override
-   * Use our PathObject class instead of the default. Our PathObject has
-   * different styles for highlighted and disabled blocks than the geras default.
+  /*
+   TODO:
+   Use our PathObject class instead of the default. Our PathObject has
+   different styles for highlighted and disabled blocks than the geras default.
    */
-  makePathObject(root, style) {
-    return new CdoPathObject(root, style, this.getConstants());
-  }
 }
