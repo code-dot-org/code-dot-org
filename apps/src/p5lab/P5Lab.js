10498ab00e6b9ecf3dcc980aab0ae9e1ad9dab99
Molly Moen <molly@code.org>
2023-03-01 11:06:09 -0800

    Allow student upload of images in Sprite Lab (experiment) (#50269)
    * Add Backgrounds Tab to Sprite Lab
    
    * add backgrounds tab to sprite lab
    
    * add background tab for spritelab
    
    * expand width for spritelab backgrounds tab
    
    * put backgrounds tab behind experiment flag
    
    * remove unused option
    
    * code review feedback
    
    * simplify reducer
    
    * change current animation state from string to object
    
    * store selected animation for costume/background mode
    
    * use enum for animation types
    
    * update animation tab tests for backgrounds tab
    
    * update animation list tests for backgrounds tab
    
    * remove console.log
    
    * code review feedback
    
    * move currentAnimationType from prop to redux state
    
    * simplify currentAnimation logic from redux state
    
    * Update AnimationTabTest.js
    
    * use P5LabInterfaceMode to determine current animation type
    
    * rely on interfaceMode from top level of store
    
    * remove log
    
    * add new prop and enable button
    
    * use destructuring shorthand and fix typo in test
    
    * extract out upload button
    
    * destructure
    
    * use enum for interface mode
    
    * hide animation picker after selecting single new frame
    
    * add field to project
    
    * work in progress
    
    * get button flow working
    
    * fix bug
    
    * get all components working
    
    * update modal and add backgrounds field
    
    * fix start over
    
    * set empty methods on pixelation
    
    * clean up and fix test
    
    * update and add tests
    
    * logic clean up
    
    * use experiment constant
    
    * restrict sharing for sprite lab
    
    * add remix check (work in progress)
    
    * fix remix logic
    
    * add tests
    
    * block upload if already published
    
    * fix failed test
    
    * address comments
    
    * add channel test
    
    * clean up
    
    * string updates
    
    ---------
    
    Co-authored-by: Mike Harvey <mike.harvey@code.org>
    Co-authored-by: Mike Harvey <43474485+mikeharv@users.noreply.github.com>

--- apps/src/p5lab/P5Lab.js
+++ apps/src/p5lab/P5Lab.js
@@ -285,6 +285,9 @@ export default class P5Lab {
           this.isBlockly
         )
       );
+      // If we reset a puzzle, it no longer has any custom uploads.
+      // Therefore we can set restricted share mode to false.
+      project.sourceHandler.setInRestrictedShareMode(false);
       this.studioApp_.resetButtonClick();
     }.bind(this);
 
