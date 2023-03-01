e9977e8474bc75038cbde4ae84eaebba9a2e77be
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-03-01 10:57:14 -0800

    Music Lab: add additional telemetry

--- apps/src/music/context.ts
+++ apps/src/music/context.ts
@@ -1,8 +1,9 @@
 import React from 'react';
+import AnalyticsReporter from './analytics/AnalyticsReporter';
 import MusicPlayer from './player/MusicPlayer';
 
 /** Provides access to the Analytics reporter object */
-export const AnalyticsContext = React.createContext(null);
+export const AnalyticsContext: React.Context<AnalyticsReporter | null> = React.createContext<AnalyticsReporter | null>(null);
 
 type PlayerUtils = Pick<
   MusicPlayer,
