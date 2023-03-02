bd6be058eb1f5e4fbe78f37c3d8df169489b0f60
breville <brendan@code.org>
2023-03-02 04:46:48 +0000

    Move function to new file

--- apps/src/music/blockly/blocks/simple2.js
+++ apps/src/music/blockly/blocks/simple2.js
@@ -7,6 +7,7 @@ import {
   FIELD_SOUNDS_TYPE
 } from '../constants';
 import {DEFAULT_SOUND} from '../../constants';
+import {getCodeForSingleBlock} from '../blockUtils';
 
 // Some helpers used when generating code to be used by the interpreter.
 // Called by executeSong().
@@ -312,56 +313,6 @@ export const playSoundsRandom = {
       `;
   }
 };
-
-/**
- * Generate code for the specified block but not following blocks.
- * Adapted from this thread: https://groups.google.com/g/blockly/c/uXewhtr-mvM
- * @param {Blockly.Block} block The block to generate code for.
- * @return {string|!Array} For statement blocks, the generated code.
- *     For value blocks, an array containing the generated code and an
- *     operator order value.  Returns '' if block is null.
- */
-function getCodeForSingleBlock(block) {
-  if (!block) {
-    return '';
-  }
-  if (block.disabled) {
-    // Skip past this block if it is disabled.
-    return getCodeForSingleBlock(block.getNextBlock());
-  }
-
-  var func = Blockly.JavaScript[block.type];
-  if (typeof func !== 'function') {
-    throw Error(
-      'Language "JavaScript" does not know how to generate ' +
-        'code for block type: ' +
-        block.type
-    );
-  }
-  // First argument to func.call is the value of 'this' in the generator.
-  // Prior to 24 September 2013 'this' was the only way to access the block.
-  // The current preferred method of accessing the block is through the second
-  // argument to func.call, which becomes the first parameter to the generator.
-  var code = func.call(block, block);
-  if (Array.isArray(code)) {
-    // Value blocks return tuples of code and operator order.
-    if (!block.outputConnection) {
-      throw Error('Expecting string from statement block: ' + block.type);
-    }
-    return [code[0], code[1]];
-  } else if (typeof code === 'string') {
-    //var id = block.id.replace(/\$/g, '$$$$'); // Issue 251.
-    //if (this.STATEMENT_PREFIX) {
-    //  code = this.STATEMENT_PREFIX.replace(/%1/g, "'" + id + "'") + code;
-    //}
-    return code;
-  } else if (code === null) {
-    // Block has handled code generation itself.
-    return '';
-  } else {
-    throw Error('Invalid code generated: ' + code);
-  }
-}
 
 export const repeatSimple2 = {
   definition: {
