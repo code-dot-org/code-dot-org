d9f56e7155c1f4d7ab0dda8c8d88c30f230226e5
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-03-02 13:04:39 -0800

    Music Lab: use dropdown in rest block (#50542)
    * Music Lab: use dropdown in rest block
    
    * add dropdown to tracks
    
    * Remove value block from toolbox
    
    * remove value block, convert to field

--- apps/src/music/blockly/MusicBlocklyWorkspace.js
+++ apps/src/music/blockly/MusicBlocklyWorkspace.js
@@ -195,7 +195,7 @@ export default class MusicBlocklyWorkspace {
         ].includes(block.type)
       ) {
         if (!this.compiledEvents.tracks) {
-          this.events.tracks = {code: ''};
+          this.compiledEvents.tracks = {code: ''};
         }
         this.compiledEvents.tracks.code += Blockly.JavaScript.blockToCode(
           block
