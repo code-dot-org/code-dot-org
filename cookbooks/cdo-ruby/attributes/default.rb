e472966c3b757f560a82886db435ff0b71c4cd5d
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-03-03 12:52:37 -0800

    Revert "Revert "Revert "Install Ruby with `ruby-build` in Chef"""

--- cookbooks/cdo-ruby/attributes/default.rb
+++ cookbooks/cdo-ruby/attributes/default.rb
@@ -1,6 +1,6 @@
 default['cdo-ruby'] = {
-  version: '2.7.5',
-  old_version: '2.7',
+  version: '2.7',
+  old_version: '2.6',
   rubygems_version: '3.3.22',
   bundler_version: '2.3.22',
   rake_version: '13.0.1'
