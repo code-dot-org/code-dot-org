1fd22c13bdf2e5d7f656b9a7dd3376fae8002e45
Continuous Integration <dev@code.org>
2023-03-06 07:30:06 +0000

    levelbuilder content changes (-robo-commit)

--- dashboard/config/blocks/AmysBlocks/AmysBlocks_goldilocks.js
+++ dashboard/config/blocks/AmysBlocks/AmysBlocks_goldilocks.js
@@ -1,3 +1,20 @@
-function goldilocks(NUM, STATEMENT, SPRITE){
-	countByAnimation(NUM, STATEMENT, SPRITE);
+function goldilocks(condition, group, callback) {
+  var count = countByAnimation(group);
+  switch(condition) {
+    case "too much":
+      if(count > 15) {
+        callback();
+      }
+      break;
+    case "just right":
+      if(count >=5 && count <= 15) {
+        callback();
+      }
+      break;
+    case "too little":
+      if(count < 5) {
+        callback();
+      }
+      break;
+  }
 }
