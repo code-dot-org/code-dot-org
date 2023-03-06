9d275e8926a015b3ec695191793cc400ecda5a69
wilkie <wilkie05@gmail.com>
2023-03-06 22:55:37 +0000

    Add storybook API updates for several components.

--- apps/src/lib/ui/SchoolInfoConfirmationDialog.story.jsx
+++ apps/src/lib/ui/SchoolInfoConfirmationDialog.story.jsx
@@ -2,6 +2,34 @@ import React from 'react';
 import SchoolInfoConfirmationDialog from './SchoolInfoConfirmationDialog';
 import {action} from '@storybook/addon-actions';
 
+export default {
+  title: 'SchoolInfoConfirmationDialog',
+  component: SchoolInfoConfirmationDialog
+};
+
+//
+// TEMPLATE
+//
+
+const Template = args => <SchoolInfoConfirmationDialog {...args} />;
+
+//
+// STORIES
+//
+
+export const DisplaySchoolInfoConfirmationDialog = Template.bind({});
+DisplaySchoolInfoConfirmationDialog.args = {
+  scriptData: {
+    formUrl: '',
+    authTokenName: 'auth_token',
+    authTokenValue: 'fake_auth_token',
+    existingSchoolInfo: {}
+  },
+  onClose: action('onClose callback'),
+  isOpen: true
+};
+
+/*
 export default storybook => {
   storybook
     .storiesOf('Dialogs/SchoolInfoConfirmationDialog', module)
@@ -24,3 +52,4 @@ export default storybook => {
       }
     ]);
 };
+*/
