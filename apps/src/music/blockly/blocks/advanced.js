5429579ce007eac071c03319881f36bb1a4c9e4c
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-03-07 10:40:10 -0800

    Music Lab: reorganize block definitions

---
+++ apps/src/music/blockly/blocks/advanced.js
@@ -0,0 +1,36 @@
+import {BlockTypes} from '../blockTypes';
+import {isBlockInsideWhenRun} from '../blockUtils';
+import {FIELD_SOUNDS_NAME} from '../constants';
+import {fieldSoundsDefinition} from '../fields';
+
+export const playSound = {
+  definition: {
+    type: BlockTypes.PLAY_SOUND,
+    style: 'music_blocks',
+    message0: 'play %1 at measure %2',
+    args0: [
+      fieldSoundsDefinition,
+      {
+        type: 'input_value',
+        name: 'measure'
+      }
+    ],
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    tooltip: 'play sound',
+    helpUrl: ''
+  },
+  generator: ctx =>
+    'MusicPlayer.playSoundAtMeasureById("' +
+    ctx.getFieldValue(FIELD_SOUNDS_NAME) +
+    '", ' +
+    Blockly.JavaScript.valueToCode(
+      ctx,
+      'measure',
+      Blockly.JavaScript.ORDER_ASSIGNMENT
+    ) +
+    ', ' +
+    (isBlockInsideWhenRun(ctx) ? 'true' : 'false') +
+    ');\n'
+};
---
+++ apps/src/music/blockly/blocks/simple.js
@@ -0,0 +1,40 @@
+import {BlockTypes} from '../blockTypes';
+import {isBlockInsideWhenRun} from '../blockUtils';
+import {FIELD_SOUNDS_NAME} from '../constants';
+import {fieldSoundsDefinition} from '../fields';
+
+export const playSoundAtCurrentLocation = {
+  definition: {
+    type: BlockTypes.PLAY_SOUND_AT_CURRENT_LOCATION,
+    message0: 'play %1',
+    args0: [fieldSoundsDefinition],
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    colour: 230,
+    tooltip: 'play sound',
+    helpUrl: ''
+  },
+  generator: ctx =>
+    'MusicPlayer.playSoundAtMeasureById("' +
+    ctx.getFieldValue(FIELD_SOUNDS_NAME) +
+    '", ' +
+    'currentMeasureLocation' +
+    ', ' +
+    (isBlockInsideWhenRun(ctx) ? 'true' : 'false') +
+    ');\n'
+};
+
+export const setCurrentLocationNextMeasure = {
+  definition: {
+    type: BlockTypes.SET_CURRENT_LOCATION_NEXT_MEASURE,
+    message0: 'go to next measure',
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    style: 'music_blocks',
+    tooltip: 'go to next measure',
+    helpUrl: ''
+  },
+  generator: ctx => 'currentMeasureLocation++\n'
+};
