33f27628e7f03ba72f1be333b49cdd5004f35cfb
Dayne <dayne@code.org>
2023-03-07 15:57:49 -0800

    Small fix for the Video Thumbnail storybook

--- apps/src/templates/VideoThumbnail.story.jsx
+++ apps/src/templates/VideoThumbnail.story.jsx
@@ -18,6 +18,7 @@ Simple.args = {
     name: 'Introduction to Functions',
     src:
       'https://www.youtube-nocookie.com/embed/yPWQfa4CHbw/?autoplay=1&enablejsapi=1&iv_load_policy=3&modestbranding=1&rel=0&showinfo=1&v=yPWQfa4CHbw&wmode=transparent',
-    thumbnail: '/c/video_thumbnails/csp_applab_conditionals_2B.jpg'
+    thumbnail:
+      'https://studio.code.org/c/video_thumbnails/csp_applab_conditionals_2B.jpg'
   }
 };
