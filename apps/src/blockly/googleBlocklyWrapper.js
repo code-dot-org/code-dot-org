7a23fcd978758efe83a15646e697bd34a2348e43
Mike Harvey <mike.harvey@code.org>
2023-03-10 09:57:12 -0500

    add try...catch

--- apps/src/blockly/googleBlocklyWrapper.js
+++ apps/src/blockly/googleBlocklyWrapper.js
@@ -270,19 +270,24 @@ function initializeBlocklyWrapper(blocklyInstance) {
     CdoRendererZelos,
     true /* opt_allowOverrides */
   );
-  // cleanUp() doesn't currently account for immovable blocks.
-  blocklyWrapper.blockly_.ContextMenuRegistry.registry.unregister(
-    'cleanWorkspace'
-  );
-  blocklyWrapper.blockly_.ContextMenuRegistry.registry.unregister(
-    'collapseWorkspace'
-  );
-  blocklyWrapper.blockly_.ContextMenuRegistry.registry.unregister(
-    'expandWorkspace'
-  );
-  blocklyWrapper.blockly_.ContextMenuRegistry.registry.unregister(
-    'workspaceDelete'
-  );
+
+  // Remove some default context menu options, if they are present.
+  try {
+    // cleanUp() doesn't currently account for immovable blocks.
+    blocklyWrapper.blockly_.ContextMenuRegistry.registry.unregister(
+      'cleanWorkspace'
+    );
+    blocklyWrapper.blockly_.ContextMenuRegistry.registry.unregister(
+      'collapseWorkspace'
+    );
+    blocklyWrapper.blockly_.ContextMenuRegistry.registry.unregister(
+      'expandWorkspace'
+    );
+    blocklyWrapper.blockly_.ContextMenuRegistry.registry.unregister(
+      'workspaceDelete'
+    );
+  } catch (error) {}
+
   registerAllContextMenuItems();
   // These are also wrapping read only properties, but can't use wrapReadOnlyProperty
   // because the alias name is not the same as the underlying property name.
