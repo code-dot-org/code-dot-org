1003ddc2281b4bed5a1d4473c2e4104bafc476f8
Molly Moen <molly@code.org>
2023-03-10 13:38:51 -0800

    Allow levelbuilders to customize the list of poems in a poetry level (#50574)
    * initial setup
    
    * add project file
    
    * fix level
    
    * add poems
    
    * don't alphabetize time capsule poems
    
    * attempt at customizing dropdown
    
    * only show selected poems
    
    * fix time capsule poem dropdown
    
    * get things working
    
    * fix up validation
    
    * fix merge conflict (continued)
    
    * clean up
    
    * fix edit experience
    
    * clean up
    
    * fix null issue
    
    * rename field
    
    * fix typo

--- apps/src/sites/studio/pages/levels/editors/fields/_poetry_fields.js
+++ apps/src/sites/studio/pages/levels/editors/fields/_poetry_fields.js
@@ -5,12 +5,8 @@ $(document).ready(initPage);
 function initPage() {
   const script = document.querySelector('script[data-poems]');
   const poems = JSON.parse(script.dataset.poems);
-  const hocOptions = poems.poetry_hoc.map(
-    option => new Option(option[0], option[1])
-  );
-  const timeCapsuleOptions = poems.time_capsule.map(
-    option => new Option(option[0], option[1])
-  );
+  const hocPoems = poems.poetry_hoc;
+  const timeCapsulePoems = poems.time_capsule;
 
   function updatePoemSelection() {
     const defaultPoem = $('#defaultPoem');
@@ -18,22 +14,31 @@ function initPage() {
       .val()
       .toLowerCase();
     const defaultPoemSelect = $('#level_default_poem');
+    const availablePoemsSelect = $('#level_available_poems');
     if (selectionValue === 'poetry') {
       defaultPoem.addClass('collapse');
     } else {
       defaultPoem.removeClass('collapse');
-      // reset dropdown options
-      defaultPoemSelect.empty();
-      let poems = hocOptions;
+      let poems = hocPoems;
       if (selectionValue === 'time_capsule') {
-        poems = timeCapsuleOptions;
+        poems = timeCapsulePoems;
       }
-      poems.forEach(poem => {
-        defaultPoemSelect.append(poem);
-      });
+      resetPoemList(defaultPoemSelect, poems);
+      resetPoemList(availablePoemsSelect, poems);
     }
-    // always unset selected poem
+    // always unset selected poem and dropdown poems
     defaultPoemSelect.val([]);
+    availablePoemsSelect.val([]);
   }
+
+  function resetPoemList(selectElement, poems) {
+    // reset dropdown options
+    selectElement.empty();
+    // append all poems
+    poems.forEach(poem => {
+      selectElement.append(new Option(poem[0], poem[1]));
+    });
+  }
+
   $('.poem-subtype-dropdown').on('change', updatePoemSelection);
 }
