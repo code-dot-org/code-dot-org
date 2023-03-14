fb3647a12b7ddbd096973771c322924e571129e3
breville <brendan@code.org>
2023-03-14 07:26:15 +0000

    Update formatting

--- dashboard/app/controllers/pusher_controller.rb
+++ dashboard/app/controllers/pusher_controller.rb
@@ -1,23 +1,23 @@
 require 'pusher'
 
 class PusherController < ApplicationController
-	skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
-	def auth
-		puts "auth"
-		if current_user
-			puts "cur"
-			response = Pusher.authenticate(params[:channel_name], params[:socket_id], {
-				user_id: current_user.id, # => required
-				user_info: { # => optional - for example
-					name: current_user.name,
-					email: current_user.email
-				}
-			})
-			render json: response
-		else
-			puts "403"
-			render text: 'Forbidden', status: '403'
-		end
-	end
+  def auth
+    puts "auth"
+    if current_user
+      puts "cur"
+      response = Pusher.authenticate(params[:channel_name], params[:socket_id], {
+        user_id: current_user.id, # => required
+        user_info: { # => optional - for example
+          name: current_user.name,
+          email: current_user.email
+        }
+      })
+      render json: response
+    else
+      puts "403"
+      render text: 'Forbidden', status: '403'
+    end
+  end
 end
