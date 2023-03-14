a95dca08728b50e8ad7bbddbc80888989c1a1575
wilkie <wilkie05@gmail.com>
2023-03-14 18:32:15 +0000

    Remove commented code.

--- apps/src/lib/ui/SchoolInfoConfirmationDialog.story.jsx
+++ apps/src/lib/ui/SchoolInfoConfirmationDialog.story.jsx
@@ -28,28 +28,3 @@ DisplaySchoolInfoConfirmationDialog.args = {
   onClose: action('onClose callback'),
   isOpen: true
 };
-
-/*
-export default storybook => {
-  storybook
-    .storiesOf('Dialogs/SchoolInfoConfirmationDialog', module)
-    .addStoryTable([
-      {
-        name: 'Display school info confirmation dialog',
-        description: 'Teacher can view modal to update school information',
-        story: () => (
-          <SchoolInfoConfirmationDialog
-            scriptData={{
-              formUrl: '',
-              authTokenName: 'auth_token',
-              authTokenValue: 'fake_auth_token',
-              existingSchoolInfo: {}
-            }}
-            onClose={action('onClose callback')}
-            isOpen={true}
-          />
-        )
-      }
-    ]);
-};
-*/
