4d91f59bb65385285b5f47f0c0b04a052cd28205
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2023-03-15 09:07:16 -0400

    [Sprite Lab] add backgroundPicker block and field type (#50702)
    * add backgroundPicker block and field type
    
    * add edit button
    
    * prevent crash on views without a main workspace
    
    * add no/new behavior option
    
    * add minimal support for google blockly
    
    * bump cdo blockly version

--- apps/src/p5lab/spritelab/blocks.js
+++ apps/src/p5lab/spritelab/blocks.js
@@ -57,6 +57,31 @@ function costumeList() {
 function backgroundList() {
   return animations(true);
 }
+function getAllBehaviors() {
+  let allowBehaviorEditing = Blockly.useModalFunctionEditor;
+  const noBehaviorLabel = allowBehaviorEditing
+    ? `${i18n.createBlocklyBehavior()}\u2026`
+    : i18n.behaviorsNotFound();
+  const behaviors = [];
+  Blockly.mainBlockSpace?.getAllBlocks().forEach(function(block) {
+    if (
+      block.type === 'behavior_definition' &&
+      block.getProcedureInfo()?.name &&
+      block.getProcedureInfo()?.id
+    ) {
+      const newOption = [
+        block.getProcedureInfo().name,
+        block.getProcedureInfo().id
+      ];
+      behaviors.push(newOption);
+    }
+  });
+  behaviors.sort();
+  if (allowBehaviorEditing || behaviors.length === 0) {
+    behaviors.push([noBehaviorLabel, Blockly.FieldDropdown.NO_OPTIONS_MESSAGE]);
+  }
+  return behaviors;
+}
 
 // This color palette is limited to colors which have different hues, therefore
 // it should not contain different shades of the same color such as
@@ -350,6 +375,64 @@ const customInputTypes = {
       return `{name: '${Blockly.JavaScript.translateVarName(
         block.getFieldValue(arg.name)
       )}'}`;
+    }
+  },
+  behaviorPicker: {
+    addInput(blockly, block, inputConfig, currentInputRow) {
+      currentInputRow
+        .appendField(inputConfig.label)
+        .appendField(
+          new Blockly.FieldDropdown(getAllBehaviors, undefined, undefined),
+          inputConfig.name
+        );
+      let allowBehaviorEditing = Blockly.useModalFunctionEditor;
+      if (
+        window.appOptions && // global appOptions is not available on level edit page
+        appOptions.level.toolbox &&
+        !appOptions.readonlyWorkspace &&
+        !Blockly.hasCategories
+      ) {
+        allowBehaviorEditing = false;
+      }
+      if (allowBehaviorEditing) {
+        const editLabel = new Blockly.FieldIcon(Blockly.Msg.FUNCTION_EDIT);
+        Blockly.cdoUtils.bindBrowserEvent(
+          editLabel.fieldGroup_,
+          'mousedown',
+          block,
+          this.openEditor
+        );
+        currentInputRow.appendField(editLabel);
+      }
+    },
+    generateCode(block, arg) {
+      const invalidBehavior =
+        block.getFieldValue(arg.name) ===
+        Blockly.FieldDropdown.NO_OPTIONS_MESSAGE;
+      const behaviorId = Blockly.JavaScript.variableDB_?.getName(
+        block.getFieldValue(arg.name),
+        'PROCEDURE'
+      );
+      if (invalidBehavior) {
+        console.warn('No behaviors available');
+        return undefined;
+      } else {
+        return `new Behavior(${behaviorId}, [])`;
+      }
+    },
+    openEditor(e) {
+      e.stopPropagation();
+      if (
+        this.getFieldValue('BEHAVIOR') ===
+        Blockly.FieldDropdown.NO_OPTIONS_MESSAGE
+      ) {
+        Blockly.behaviorEditor.openWithNewFunction();
+      } else {
+        Blockly.behaviorEditor.openEditorForFunction(
+          this,
+          this.getFieldValue('BEHAVIOR')
+        );
+      }
     }
   },
   limitedColourPicker: {
