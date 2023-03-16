aa045aaf94075c6ba8c46fc113637758cfd3327c
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2023-03-16 17:09:16 -0400

    [Google Blockly] Add util to get student code (Save sources in JSON, pt 1) (#50786)
    * at cdoUtil to get code and return as string
    
    * use getCode util across blockly labs
    
    * use xml for now in google blockly util

--- apps/src/code-studio/initApp/loadApp.js
+++ apps/src/code-studio/initApp/loadApp.js
@@ -115,8 +115,7 @@ export function setupApp(appOptions) {
       // in the contained level case, unless we're editing blocks.
       if (appOptions.level.edit_blocks || !appOptions.hasContainedLevels) {
         if (appOptions.hasContainedLevels) {
-          var xml = Blockly.Xml.blockSpaceToDom(Blockly.mainBlockSpace);
-          report.program = Blockly.Xml.domToText(xml);
+          report.program = Blockly.cdoUtils.getCode(Blockly.mainBlockSpace);
         }
         report.callback = appOptions.report.callback;
       }
@@ -479,9 +478,7 @@ const sourceHandler = {
         // If we're readOnly, source hasn't changed at all
         source = Blockly.cdoUtils.isWorkspaceReadOnly(Blockly.mainBlockSpace)
           ? currentLevelSource
-          : Blockly.Xml.domToText(
-              Blockly.Xml.blockSpaceToDom(Blockly.mainBlockSpace)
-            );
+          : Blockly.cdoUtils.getCode(Blockly.mainBlockSpace);
         resolve(source);
       } else if (appOptions.getCode) {
         source = appOptions.getCode();
@@ -542,7 +539,7 @@ export function getAppOptions() {
  * Loads the "appOptions" object from the dom and augments it with additional
  * information needed by apps to run.
  *
- * This should only be called once per page load, with appoptions specified as a
+ * This should only be called once per page load, with appOptions specified as a
  * data attribute on the script tag.
  *
  * @return {Promise.<AppOptionsConfig>} a Promise object which resolves to the fully populated appOptions
--- apps/src/dance/Dance.js
+++ apps/src/dance/Dance.js
@@ -475,9 +475,10 @@ Dance.prototype.onPuzzleComplete = function(result, message) {
   // If we know they succeeded, mark `levelComplete` true.
   const levelComplete = result;
 
-  // We're using blockly, report the program as xml.
-  var xml = Blockly.Xml.blockSpaceToDom(Blockly.mainBlockSpace);
-  let program = encodeURIComponent(Blockly.Xml.domToText(xml));
+  // We're using blockly, report the program as JSON.
+  let program = encodeURIComponent(
+    Blockly.cdoUtils.getCode(Blockly.mainBlockSpace)
+  );
 
   if (this.testResults >= TestResults.FREE_PLAY) {
     this.studioApp_.playAudio('win');
