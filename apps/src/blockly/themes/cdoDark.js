c14743ef27f4e484f1fcaa2ac760c0e93e23ecd6
denyslevada <levada.denys@gmail.com>
2023-03-17 22:04:09 +0200

    add opacity to flyout

--- apps/src/blockly/themes/cdoDark.js
+++ apps/src/blockly/themes/cdoDark.js
@@ -11,7 +11,8 @@ export default GoogleBlockly.Theme.defineTheme(Themes.DARK, {
   componentStyles: {
     workspaceBackgroundColour: color.neutral_dark90,
     blackBackground: color.neutral_dark80,
-    flyoutBackgroundColour: color.neutral_dark70
+    flyoutBackgroundColour: color.neutral_dark70,
+    flyoutOpacity: 0.8
   },
   fontStyle: {
     family: '"Gotham 4r", sans-serif'
