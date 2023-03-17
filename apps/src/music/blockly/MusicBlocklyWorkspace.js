a49092b91995b3b033a2741db1e8c129f2187577
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-03-17 12:29:20 -0700

    change to 'my function'

--- apps/src/music/blockly/MusicBlocklyWorkspace.js
+++ apps/src/music/blockly/MusicBlocklyWorkspace.js
@@ -89,7 +89,7 @@ export default class MusicBlocklyWorkspace {
     delete Blockly.Blocks.procedures_ifreturn;
 
     // Rename the new function placeholder text for Music Lab specifically.
-    Blockly.Msg['PROCEDURES_DEFNORETURN_PROCEDURE'] = 'make new block';
+    Blockly.Msg['PROCEDURES_DEFNORETURN_PROCEDURE'] = 'my function';
 
     Blockly.setInfiniteLoopTrap();
 
