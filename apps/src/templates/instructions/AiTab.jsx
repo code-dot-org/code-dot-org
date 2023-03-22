e6f3683521a380aaa4b7f5e1bace440104b611cc
David Bailey <davidsbailey@users.noreply.github.com>
2023-03-22 14:18:22 -0700

    add auto grading tab

---
+++ apps/src/templates/instructions/AiTab.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+export const AiTab = props => {
+  // const {
+  //   teacherOnly,
+  // } = props;
+
+  return <div>coming soon!</div>;
+};
