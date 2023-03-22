7499e701af459109163f9a0d0e1f5bdde09b8146
David Bailey <davidsbailey@users.noreply.github.com>
2023-03-22 13:50:34 -0700

    add openai api helper

---
+++ apps/src/util/openai.js
@@ -0,0 +1,22 @@
+import {queryParams} from '@cdo/apps/code-studio/utils';
+
+const SYSTEM_PROMPT = '';
+
+export const openaiCompletion = prompt => {
+  const key = queryParams('key');
+  $.ajax({
+    url: 'https://api.openai.com/v1/chat/completions',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key}`
+    },
+    data: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {role: 'system', content: SYSTEM_PROMPT},
+        {role: 'user', content: prompt}
+      ]
+    }).then(response => response.choices[0].message.content)
+  });
+};
