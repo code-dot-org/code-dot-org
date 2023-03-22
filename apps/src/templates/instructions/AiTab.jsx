6074f518e85b2389cf07d825c430796f27ec7dc4
David Bailey <davidsbailey@users.noreply.github.com>
2023-03-22 14:34:43 -0700

    hit openai api from auto grading tab

--- apps/src/templates/instructions/AiTab.jsx
+++ apps/src/templates/instructions/AiTab.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 // import PropTypes from 'prop-types';
+import {openaiCompletion} from '@cdo/apps/util/openai';
 
 export const AiTab = props => {
   // const {
   //   teacherOnly,
   // } = props;
 
-  return <div>coming soon!</div>;
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    const prompt =
+      'in two sentences, tell my student they did great on their coding assigment.';
+    openaiCompletion(prompt).then(setResult);
+  }, []);
+
+  return (
+    <div>
+      {!result && <div>loading...</div>}
+      {result && <div>{result}</div>}
+    </div>
+  );
 };
