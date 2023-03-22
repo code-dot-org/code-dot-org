140798af90cd5d6d3e4bd98698a7f11d48ebf693
Bethany Connor <bethany@code.org>
2023-03-22 11:18:41 -0400

    Add dialog footer and move strings to en_us.json

--- apps/src/templates/sectionsRefresh/CelebrationDialog.jsx
+++ apps/src/templates/sectionsRefresh/CelebrationDialog.jsx
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
 import BaseDialog from '@cdo/apps/templates/BaseDialog';
+import DialogFooter from '@cdo/apps/templates/teacherDashboard/DialogFooter';
+import Button from '@cdo/apps/templates/Button';
+import i18n from '@cdo/locale';
 import moduleStyles from './sections-refresh.module.scss';
 const CelebrationGif = require('@cdo/static/section_creation_celebration.gif');
 
@@ -12,13 +15,17 @@ export default function CelebrationDialog() {
       isOpen={isOpen}
     >
       <div className={moduleStyles.celebrationDialogBody}>
-        <h2>Congratulations!</h2>
-        <div>
-          Your class sections have been saved. Next, use the teacher dashboard
-          to add students to your class sections.
-        </div>
+        <h2>{i18n.congratulations()}</h2>
+        <div>{i18n.sectionCreationCelebrationDialogMessage()}</div>
         <img src={CelebrationGif} className={moduleStyles.celebrationGif} />
       </div>
+      <DialogFooter>
+        <Button
+          onClick={() => setIsOpen(false)}
+          text={i18n.goToDashboard()}
+          color={Button.ButtonColor.brandSecondaryDefault}
+        />
+      </DialogFooter>
     </BaseDialog>
   );
 }
