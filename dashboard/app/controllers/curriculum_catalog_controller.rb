828f7b5e102dc47663c0a2890006c5b84c721585
Turner Riley <turnertriley@gmail.com>
2023-03-23 14:40:03 -0700

    style header banner

--- dashboard/app/controllers/curriculum_catalog_controller.rb
+++ dashboard/app/controllers/curriculum_catalog_controller.rb
@@ -1,6 +1,7 @@
 class CurriculumCatalogController < ApplicationController
   # GET /catalog
   def index
+    view_options(full_width: true, responsive_content: true, no_padding_container: true)
     @curricula_data = CourseOffering.all.order(:display_name).map(&:serialize)
   end
 end
