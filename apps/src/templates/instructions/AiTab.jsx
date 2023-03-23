47cc5deafd44e0bc97912045d4ea3e3977756e63
David Bailey <davidsbailey@users.noreply.github.com>
2023-03-22 17:09:00 -0700

    tweak prompts and api params

--- apps/src/templates/instructions/AiTab.jsx
+++ apps/src/templates/instructions/AiTab.jsx
@@ -14,10 +14,12 @@ export const AiTab = props => {
 
   useEffect(() => {
     const prompt = `Please evaluate and briefly summarize whether each of the
-    following statements are true or false with respect to the code for my
+    following statements are true or false with respect to the javascript code for my
     program which follows. Please give your response formatted as a markdown
     table with the following columns: "Statement", "True/False", and "Reason". 
-    please use backticks to escape any code in your reason.
+    please use backticks to escape any code in your reason. the statement column
+    should contain the entire original statement. please be sure to ignore any
+    commented-out code when deciding what the program does or does not do.
 
     statements:
     
@@ -44,7 +46,7 @@ export const AiTab = props => {
 
 const exampleStatements = `
 1. You used multiple sprites
-2. Your program uses at least one random number
+2. Your program uses at least one random number to set the value of a property on a sprite or other object.
 3. Your program uses multiple conditionals inside the draw loop 
 4. At least one variable or property uses the counter pattern
 `;
