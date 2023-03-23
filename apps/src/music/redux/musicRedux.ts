7aaf3a5f35bbeadcbba2d968f88479b80026483f
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-03-23 10:02:57 -0700

    Project Beats: Initial redux setup, move isPlaying to redux

---
+++ apps/src/music/redux/musicRedux.ts
@@ -0,0 +1,33 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+const registerReducers = require('@cdo/apps/redux').registerReducers;
+
+/**
+ * State, reducer, and actions for Music Lab.
+ */
+
+interface MusicState {
+  isPlaying: boolean;
+}
+
+const initialState: MusicState = {
+  isPlaying: false
+};
+
+const musicSlice = createSlice({
+  name: 'music',
+  initialState,
+  reducers: {
+    setIsPlaying: (state, action: PayloadAction<boolean>) => {
+      state.isPlaying = action.payload;
+    }
+  }
+});
+
+// TODO: If/when a top-level component is created that wraps {@link MusicView}, then
+// registering reducers should to happen there. We are registering reducers here for now
+// because MusicView is currently the top-level entrypoint into Music Lab and also needs
+// to be connected to this state.
+registerReducers({music: musicSlice.reducer});
+
+export const {setIsPlaying} = musicSlice.actions;
