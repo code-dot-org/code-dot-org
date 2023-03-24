54cc2240ad9706c7e5fd89e63ab0a48c9fada48f
Turner Riley <turnertriley@gmail.com>
2023-03-24 12:23:53 -0700

    add filtering in controller for applicable course offerings

--- dashboard/app/models/course_offering.rb
+++ dashboard/app/models/course_offering.rb
@@ -108,6 +108,12 @@ class CourseOffering < ApplicationRecord
     course_versions.any? {|cv| cv.content_root.is_a?(Unit) && cv.has_editor_experiment?(user)}
   end
 
+  # Checks if any course version has a published_state of 'preview' or 'stable'
+  def any_version_is_in_published_state?
+    published_states = ['preview', 'stable']
+    course_versions.any? {|cv| published_states.include?(cv.published_state)}
+  end
+
   def self.all_course_offerings
     if should_cache?
       @@course_offerings ||= CourseOffering.all.includes(course_versions: :content_root)
