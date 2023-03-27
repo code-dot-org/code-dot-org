9adba449e11511e9857657ed4c9009113c642c95
Dave Bailey <davidsbailey@users.noreply.github.com>
2023-03-27 15:43:26 -0700

    Revert "require teacher application when enrolling in certain workshops (#50862)" (#50968)
    This reverts commit e7cee54f165e0e611894c84651fd3d6c58c23e69.

--- dashboard/app/models/pd/workshop.rb
+++ dashboard/app/models/pd/workshop.rb
@@ -128,14 +128,6 @@ class Pd::Workshop < ApplicationRecord
     end
   end
 
-  # Whether enrollment in this workshop requires an application
-  def require_application?
-    courses = [COURSE_CSP, COURSE_CSD, COURSE_CSA]
-    subjects = ACADEMIC_YEAR_SUBJECTS.concat([SUBJECT_SUMMER_WORKSHOP])
-    courses.include?(course) && subjects.include?(subject) &&
-      regional_partner && regional_partner.link_to_partner_application.blank?
-  end
-
   def self.organized_by(organizer)
     where(organizer_id: organizer.id)
   end
