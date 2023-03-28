8ff8db5a5d193a03d2aa0b011c9d985af299cb7d
Turner Riley <turnertriley@gmail.com>
2023-03-28 13:32:15 -0700

    update styles and shapes

--- apps/src/templates/curriculumCatalog/CurriculumCatalog.jsx
+++ apps/src/templates/curriculumCatalog/CurriculumCatalog.jsx
@@ -1,30 +1,14 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {curriculaDataShape} from './curriculumCatalogShapes';
 import i18n from '@cdo/locale';
-import '../../../style/code-studio/curriculum_catalog.scss';
+import '../../../style/code-studio/curriculum_catalog_container.scss';
 import HeaderBanner from '../HeaderBanner';
 import CourseCatalogIllustration01 from '../../../static/curriculum_catalog/course-catalog-illustration-01.png';
 
 class CurriculumCatalog extends Component {
   static propTypes = {
-    curriculaData: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        key: PropTypes.string,
-        display_name: PropTypes.string,
-        category: PropTypes.string,
-        is_featured: PropTypes.bool,
-        assignable: PropTypes.bool,
-        curriculum_type: PropTypes.string,
-        marketing_initiative: PropTypes.string,
-        grade_levels: PropTypes.string,
-        header: PropTypes.string,
-        image: PropTypes.string,
-        cs_topic: PropTypes.string,
-        school_subject: PropTypes.string,
-        device_compatibility: PropTypes.string
-      })
-    )
+    curriculaData: PropTypes.arrayOf(curriculaDataShape)
   };
 
   render() {
@@ -38,8 +22,8 @@ class CurriculumCatalog extends Component {
             imageUrl={CourseCatalogIllustration01}
           />
         </div>
-        <div className={'contentContainer'}>
-          <div className={'content'}>
+        <div className={'catalogContentContainer'}>
+          <div className={'catalogContent'}>
             {this.props.curriculaData.map(curriculum => (
               <li key={curriculum.key}>{curriculum.display_name}</li>
             ))}
