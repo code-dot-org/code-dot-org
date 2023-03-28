914ce81137637e3d31db8bca613c422d69fe5300
David Bailey <davidsbailey@users.noreply.github.com>
2023-03-28 13:28:23 -0700

    set up server-side openai api

---
+++ dashboard/app/controllers/openai_controller.rb
@@ -0,0 +1,33 @@
+class OpenaiController < ApplicationController
+  SYSTEM_PROMPT = ''
+
+  # POST /openai/chat_completion
+  def chat_completion
+    # Set up the API endpoint URL and request headers
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+      "Content-Type" => "application/json",
+      "Authorization" => "Bearer #{CDO.openai_api_key}"
+    }
+
+    # Build the request body
+    prompt = "Hello, how are you?"
+    data = {
+      model: 'gpt-3.5-turbo',
+      temperature: 0,
+      messages: [
+        {role: 'system', content: SYSTEM_PROMPT},
+        {role: 'user', content: prompt}
+      ],
+    }
+
+    # Send the request to the API endpoint
+    response = HTTParty.post(url, headers: headers, body: data.to_json)
+
+    # Parse the response JSON and return the completed text
+    response_body = JSON.parse(response.body)
+    completed_text = response_body['choices'][0]['message']['content']
+
+    render plain: completed_text
+  end
+end
