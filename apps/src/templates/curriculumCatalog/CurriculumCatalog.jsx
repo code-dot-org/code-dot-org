79e2f0475b2363d8030ac080a7cf692c06e2b0b2
Turner Riley <turner.riley@code.org>
2023-03-29 10:35:35 -0700

    update styles to css module

--- apps/src/templates/curriculumCatalog/CurriculumCatalog.jsx
+++ apps/src/templates/curriculumCatalog/CurriculumCatalog.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {curriculaDataShape} from './curriculumCatalogShapes';
 import i18n from '@cdo/locale';
-import '../../../style/code-studio/curriculum_catalog_container.scss';
+import style from '../../../style/code-studio/curriculum_catalog_container.module.scss';
 import HeaderBanner from '../HeaderBanner';
 import CourseCatalogIllustration01 from '../../../static/curriculum_catalog/course-catalog-illustration-01.png';
 
@@ -14,7 +14,7 @@ class CurriculumCatalog extends Component {
   render() {
     return (
       <>
-        <div className={'catalogHeader'}>
+        <div className={style.catalogHeader}>
           <HeaderBanner
             headingText={i18n.curriculumCatalogHeaderTitle()}
             subHeadingText={i18n.curriculumCatalogHeaderSubtitle()}
@@ -22,8 +22,8 @@ class CurriculumCatalog extends Component {
             imageUrl={CourseCatalogIllustration01}
           />
         </div>
-        <div className={'catalogContentContainer'}>
-          <div className={'catalogContent'}>
+        <div className={style.catalogContentContainer}>
+          <div className={style.catalogContent}>
             {this.props.curriculaData.map(curriculum => (
               <li key={curriculum.key}>{curriculum.display_name}</li>
             ))}
