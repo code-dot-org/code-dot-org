643a7fcfb25a2aaed06a9fac3da93081862b52fb
denyslevada <levada.denys@gmail.com>
2023-03-31 01:24:35 +0300

    wip2

--- apps/src/componentLibrary/typography/Heading1.tsx
+++ apps/src/componentLibrary/typography/Heading1.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 type Heading1Props = {
   children: React.ReactNode;
 };
 
 const Heading1 = ({children}: Heading1Props) => {
-  // const {children} = props;
   return <h1>{children}</h1>;
 };
 
+Heading1.propTypes = {
+  children: PropTypes.node
+};
 export default Heading1;
