443b77c3142db901b79adec1a56ab56a77a11c6b
Ryan Shipp <1382374+rshipp@users.noreply.github.com>
2023-03-30 11:58:35 -0500

    Revert "[CFU] Add summary entry point for FreeResponse contained levels (#50836)"
    This reverts commit 4ccc0b4a7371ef7e9f3cf61d3f502a7d1f6a6245.

--- apps/src/sites/studio/pages/levels/_free_response.js
+++ apps/src/sites/studio/pages/levels/_free_response.js
@@ -4,29 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import getScriptData from '@cdo/apps/util/getScriptData';
 import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
-import SummaryEntryPoint from '@cdo/apps/templates/levelSummary/SummaryEntryPoint';
 import Attachments from '@cdo/apps/code-studio/components/Attachments';
 import {reportTeacherReviewingStudentNonLabLevel} from '@cdo/apps/lib/util/analyticsUtils';
-import {Provider} from 'react-redux';
-import InstructorsOnly from '@cdo/apps/code-studio/components/InstructorsOnly';
-import {getStore} from '@cdo/apps/redux';
 
 $(document).ready(() => {
-  const scriptData = getScriptData('freeresponse');
-
-  $('#containedLevel0 > #summaryEntryPoint').each(function() {
-    const container = this;
-    const store = getStore();
-
-    ReactDOM.render(
-      <Provider store={store}>
-        <InstructorsOnly>
-          <SummaryEntryPoint scriptData={scriptData} />
-        </InstructorsOnly>
-      </Provider>,
-      container
-    );
-  });
+  const data = getScriptData('freeresponse');
 
   $('.free-response > .markdown-container').each(function() {
     const container = this;
@@ -45,7 +27,7 @@ $(document).ready(() => {
   }
 
   const attachmentsMountPoint = document.querySelector('#free-response-upload');
-  const attachmentsProps = scriptData.attachments_props;
+  const attachmentsProps = data.attachments_props;
   if (attachmentsMountPoint && attachmentsProps) {
     dashboard.project.getCurrentId = function() {
       return appOptions.channel;
