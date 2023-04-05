db10b35b09decc5f2ead3c815aadab9e723247e6
Turner Riley <turner.riley@code.org>
2023-04-05 09:06:31 -0700

    start sorting workshops

--- apps/src/code-studio/pd/workshop_dashboard/workshopConstants.js
+++ apps/src/code-studio/pd/workshop_dashboard/workshopConstants.js
@@ -2,7 +2,17 @@ const TIME_FORMAT = 'h:mma';
 const DATE_FORMAT = 'YYYY-MM-DD';
 const DATETIME_FORMAT = `${DATE_FORMAT} ${TIME_FORMAT}`;
 const COURSE_CSF = 'CS Fundamentals';
+const COURSE_CSP = 'CS Principles';
+const COURSE_CSA = 'Computer Science A';
 
 const MAX_SESSIONS = 10;
 
-export {TIME_FORMAT, DATE_FORMAT, DATETIME_FORMAT, MAX_SESSIONS, COURSE_CSF};
+export {
+  TIME_FORMAT,
+  DATE_FORMAT,
+  DATETIME_FORMAT,
+  MAX_SESSIONS,
+  COURSE_CSF,
+  COURSE_CSP,
+  COURSE_CSA
+};
