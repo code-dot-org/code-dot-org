4ffef7b090794db93d2b1dbb94071dbcc589a5d1
breville <brendan@code.org>
2023-04-06 17:41:19 +0000

    Rough intermediate step to load music minimally

--- apps/src/sites/studio/pages/levels-music-main.js
+++ apps/src/sites/studio/pages/levels-music-main.js
@@ -1,4 +1,18 @@
+/*
 import loadAppOptions from '@cdo/apps/code-studio/initApp/loadApp';
 import loadMusic from './init/loadMusic';
 
 loadAppOptions().then(loadMusic);
+*/
+
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MusicLabView from '@cdo/apps/music/views/MusicView';
+
+$(document).ready(function () {
+  ReactDOM.render(
+    <MusicLabView />,
+    document.getElementById('musiclab-container')
+  );
+});
