478244c06af626216151e7d849a8ff744e56b45f
breville <brendan@code.org>
2023-04-06 17:43:32 +0000

    Clean up prior step

--- apps/src/sites/studio/pages/levels-music-main.js
+++ apps/src/sites/studio/pages/levels-music-main.js
@@ -1,10 +1,3 @@
-/*
-import loadAppOptions from '@cdo/apps/code-studio/initApp/loadApp';
-import loadMusic from './init/loadMusic';
-
-loadAppOptions().then(loadMusic);
-*/
-
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
