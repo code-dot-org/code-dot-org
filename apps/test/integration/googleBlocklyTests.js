9ab4df7061d04a40c7ee38592d4d9f5887d26199
Mike Harvey <mike.harvey@code.org>
2023-04-06 14:19:31 -0400

    expand test scenario to cover cdoXml for unknown blocks

--- apps/test/integration/googleBlocklyTests.js
+++ apps/test/integration/googleBlocklyTests.js
@@ -1,12 +1,36 @@
 import {assert} from '../util/reconfiguredChai';
 import {setupTestGoogleBlockly} from './util/testGoogleBlockly';
 
+let previousBlockly;
+
 describe('test google blockly', function () {
   beforeEach(function () {
+    previousBlockly = window.Blockly ? Blockly : undefined;
     setupTestGoogleBlockly();
   });
 
-  it('can confirm Blockly version is google', function () {
-    assert(Blockly.version === 'Google');
+  it('can use domToBlock to create known blocks', function () {
+    assert(Blockly.getMainWorkspace().getAllBlocks().length === 0);
+    Blockly.Xml.domToBlock(
+      Blockly.Xml.textToDom('<block type="empty_block"/>'),
+      Blockly.getMainWorkspace()
+    );
+    assert(Blockly.getMainWorkspace().getAllBlocks().length === 1);
+    assert(Blockly.getMainWorkspace().getAllBlocks()[0].type === 'empty_block');
+    //   });
+
+    //   it('can use domToBlock to create unknown blocks', function () {
+    //     assert(Blockly.getMainWorkspace().getAllBlocks().length === 0);
+    Blockly.Xml.domToBlock(
+      Blockly.Xml.textToDom('<block type="some_nonexistent_block"/>'),
+      Blockly.getMainWorkspace()
+    );
+    assert(Blockly.getMainWorkspace().getAllBlocks().length === 2);
+    assert(Blockly.getMainWorkspace().getAllBlocks()[1].type === 'unknown');
+  });
+
+  afterEach(function () {
+    Blockly.blockly_.ContextMenuRegistry.registry.reset();
+    Blockly = previousBlockly;
   });
 });
