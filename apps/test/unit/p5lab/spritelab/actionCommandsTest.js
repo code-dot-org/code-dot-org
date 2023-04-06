0c5d8a78c952f60b37b456152c2cc69ccf439d88
Ben Brooks <ben@code.org>
2023-04-05 18:12:33 -0700

    Fix test

--- apps/test/unit/p5lab/spritelab/actionCommandsTest.js
+++ apps/test/unit/p5lab/spritelab/actionCommandsTest.js
@@ -488,13 +488,13 @@ describe('Action Commands', () => {
 
       const spritesBeforeLayout = coreLibrary.getSpriteArray({costume: 'cat'});
       expect(spritesBeforeLayout.length).to.equal(1);
-      expect(spritesBeforeLayout[0].scale).to.equal(1);
+      expect(spritesBeforeLayout[0].getScale()).to.equal(1);
 
       commands.layoutSprites.apply(coreLibrary, ['cat', 'border']);
 
       const spritesAfterLayout = coreLibrary.getSpriteArray({costume: 'cat'});
       expect(spritesAfterLayout.length).to.equal(1);
-      expect(spritesAfterLayout[0].scale).to.equal(0.3);
+      expect(spritesAfterLayout[0].getScale()).to.equal(0.3);
     });
 
     it('circle layout works with 1 sprite', () => {
