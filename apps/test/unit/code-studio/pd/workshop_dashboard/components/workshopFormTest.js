07ffe1c8eecc5cbe6da61b558bc645fe839c947c
David Bailey <davidsbailey@users.noreply.github.com>
2023-04-07 16:08:50 -0700

    render csp summer workshop with enzyme mount

--- apps/test/unit/code-studio/pd/workshop_dashboard/components/workshopFormTest.js
+++ apps/test/unit/code-studio/pd/workshop_dashboard/components/workshopFormTest.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import {assert} from 'chai';
 import {Factory} from 'rosie';
 import {FormControl} from 'react-bootstrap';
-import Permission from '@cdo/apps/code-studio/pd/workshop_dashboard/permission';
+import Permission, {
+  WorkshopAdmin
+} from '@cdo/apps/code-studio/pd/workshop_dashboard/permission';
 import {WorkshopForm} from '@cdo/apps/code-studio/pd/workshop_dashboard/components/workshop_form';
 import '../workshopFactory';
+import {Provider} from 'react-redux';
+import {MemoryRouter} from 'react-router-dom';
+import mapboxReducer from '@cdo/apps/redux/mapbox';
+import {createStore, combineReducers} from 'redux';
 
 describe('WorkshopForm test', () => {
   let fakeWorkshop;
@@ -14,7 +20,7 @@ describe('WorkshopForm test', () => {
     fakeWorkshop = Factory.build('workshop');
   });
 
-  it('renders csf intro workshop', () => {
+  it('renders csf intro workshop ', () => {
     const wrapper = shallow(
       <WorkshopForm
         permission={new Permission()}
@@ -31,16 +37,25 @@ describe('WorkshopForm test', () => {
   });
 
   it('renders csp summer workshop', () => {
+    const store = createStore(
+      combineReducers({
+        mapbox: mapboxReducer
+      })
+    );
+
     const cspSummerWorkshop = Factory.build('csp summer workshop');
-    const wrapper = shallow(
-      <WorkshopForm
-        permission={new Permission()}
-        facilitatorCourses={[]}
-        workshop={cspSummerWorkshop}
-        onSaved={() => {}}
-        readOnly={false}
-      />,
-      {context: {router: {}}}
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <WorkshopForm
+            permission={new Permission([WorkshopAdmin])}
+            facilitatorCourses={[]}
+            workshop={cspSummerWorkshop}
+            onSaved={() => {}}
+            readOnly={false}
+          />
+        </MemoryRouter>
+      </Provider>
     );
 
     const someControl = wrapper.find(FormControl);
