9f3c38b83ae61f1bf32013b76f9e30fa252ad574
David Bailey <davidsbailey@users.noreply.github.com>
2023-04-07 17:24:52 -0700

    wip

--- dashboard/app/controllers/openai_controller.rb
+++ dashboard/app/controllers/openai_controller.rb
@@ -13,7 +13,8 @@ class OpenaiController < ApplicationController
 
     # Build the request body
     data = {
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-4',
+      # model: 'gpt-3.5-turbo',
       temperature: 0,
       messages: [
         {role: 'system', content: SYSTEM_PROMPT},
@@ -21,12 +22,20 @@ class OpenaiController < ApplicationController
       ],
     }
 
+    puts "model: #{data[:model]}"
+    puts "Request headers: #{headers}"
+
     # Send the request to the API endpoint
     response = HTTParty.post(url, headers: headers, body: data.to_json)
+
+    puts "Response status: #{response.code}"
+    puts "Response body: #{response.body}"
 
     # Parse the response JSON and return the completed text
     response_body = JSON.parse(response.body)
     completed_text = response_body['choices'][0]['message']['content']
+
+    puts "completion: #{completed_text}"
 
     render plain: completed_text
   end
