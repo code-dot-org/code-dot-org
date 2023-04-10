b6d53ac93c064ce68c1d2a37171b4a653a2fa05f
Turner Riley <turner.riley@code.org>
2023-04-10 12:26:41 -0700

    add initial test coverage

--- apps/test/unit/code-studio/pd/workshop_dashboard/components/workshopFormTest.js
+++ apps/test/unit/code-studio/pd/workshop_dashboard/components/workshopFormTest.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {assert} from 'chai';
+import {isolateComponent} from 'isolate-react';
+import {expect, assert} from 'chai';
 import {Factory} from 'rosie';
 import {FormControl} from 'react-bootstrap';
-import Permission from '@cdo/apps/code-studio/pd/workshop_dashboard/permission';
+import Permission, {
+  WorkshopAdmin,
+} from '@cdo/apps/code-studio/pd/workshop_dashboard/permission';
 import {WorkshopForm} from '@cdo/apps/code-studio/pd/workshop_dashboard/components/workshop_form';
 import '../workshopFactory';
 
@@ -28,5 +31,24 @@ describe('WorkshopForm test', () => {
 
     const someControl = wrapper.find(FormControl);
     assert(someControl.exists());
+  });
+
+  it('CSP and CSA virtual field disabled for non-ws-admin if within a month of starting', () => {
+    const csp_in_person_ws = Factory.build('workshop_csp_in_person');
+    const wrapper = isolateComponent(
+      <WorkshopForm
+        permission={new Permission(WorkshopAdmin)}
+        facilitatorCourses={[]}
+        workshop={csp_in_person_ws}
+        onSaved={() => {}}
+        readOnly={false}
+      />,
+      {context: {router: {}}}
+    );
+
+    // NEED TO MAKE THE SESSION START WITHIN A MONTH THEN THIS SHOULD PASS
+
+    const virtualFormControl = wrapper.findOne('SelectIsVirtual');
+    expect(virtualFormControl.props.readOnly).to.equal(true);
   });
 });
