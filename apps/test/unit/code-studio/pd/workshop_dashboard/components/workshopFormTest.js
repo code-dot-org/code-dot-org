fdc37c0783cb69a58eda111c4edc3130701c0ed0
Turner Riley <turnertriley@gmail.com>
2023-04-12 16:18:17 -0700

    add tests

--- apps/test/unit/code-studio/pd/workshop_dashboard/components/workshopFormTest.js
+++ apps/test/unit/code-studio/pd/workshop_dashboard/components/workshopFormTest.js
@@ -4,7 +4,8 @@ import {assert} from 'chai';
 import {Factory} from 'rosie';
 import {FormControl} from 'react-bootstrap';
 import Permission, {
-  WorkshopAdmin
+  WorkshopAdmin,
+  ProgramManager
 } from '@cdo/apps/code-studio/pd/workshop_dashboard/permission';
 import {WorkshopForm} from '@cdo/apps/code-studio/pd/workshop_dashboard/components/workshop_form';
 import '../workshopFactory';
@@ -62,5 +63,93 @@ describe('WorkshopForm test', () => {
 
     const someControl = wrapper.find(FormControl);
     assert(someControl.exists());
+  });
+
+  it('virtual field disabled for non-ws-admin for CSP/CSA workshop within a month of starting', () => {
+    const cspSummerWorkshopStartSoon = Factory.build(
+      'csp summer workshop starting within month'
+    );
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <WorkshopForm
+            permission={new Permission([ProgramManager])}
+            facilitatorCourses={[]}
+            workshop={cspSummerWorkshopStartSoon}
+            onSaved={() => {}}
+            readOnly={false}
+          />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const virtualFormController = wrapper.find('#virtual').first();
+    assert(virtualFormController.props().disabled);
+  });
+
+  it('virtual field enabled for ws-admin for CSP/CSA workshop within a month of starting', () => {
+    const cspSummerWorkshopStartSoon = Factory.build(
+      'csp summer workshop starting within month'
+    );
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <WorkshopForm
+            permission={new Permission([WorkshopAdmin])}
+            facilitatorCourses={[]}
+            workshop={cspSummerWorkshopStartSoon}
+            onSaved={() => {}}
+            readOnly={false}
+          />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const virtualFormController = wrapper.find('#virtual').first();
+    assert(!virtualFormController.props().disabled);
+  });
+
+  it('virtual field enabled for non-ws-admin for non-CSP/CSA workshop within a month of starting', () => {
+    const csdSummerWorkshopStartSoon = Factory.build(
+      'csd summer workshop starting within month'
+    );
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <WorkshopForm
+            permission={new Permission([ProgramManager])}
+            facilitatorCourses={[]}
+            workshop={csdSummerWorkshopStartSoon}
+            onSaved={() => {}}
+            readOnly={false}
+          />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const virtualFormController = wrapper.find('#virtual').first();
+    assert(!virtualFormController.props().disabled);
+  });
+
+  it('virtual field enabled for non-ws-admin for CSP/CSA workshop over a month from starting', () => {
+    const cspSummerWorkshopStartOverMonth = Factory.build(
+      'csp summer workshop starting in over a month'
+    );
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter>
+          <WorkshopForm
+            permission={new Permission([ProgramManager])}
+            facilitatorCourses={[]}
+            workshop={cspSummerWorkshopStartOverMonth}
+            onSaved={() => {}}
+            readOnly={false}
+          />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const virtualFormController = wrapper.find('#virtual').first();
+    assert(!virtualFormController.props().disabled);
   });
 });
--- apps/test/unit/code-studio/pd/workshop_dashboard/workshopFactory.js
+++ apps/test/unit/code-studio/pd/workshop_dashboard/workshopFactory.js
@@ -6,6 +6,11 @@ import {States} from '@cdo/apps/generated/pd/sharedWorkshopConstants';
  * as retrieved from /api/v1/pd/workshops/<workshop-id>
  * @see also `workshopShape` and `enrollmentShape` in types.js
  */
+const today = new Date();
+
+/**
+ * WORKSHOPS
+ */
 Factory.define('workshop')
   .sequence('id', n => n)
   .attr('course', 'CS Fundamentals')
@@ -14,7 +19,7 @@ Factory.define('workshop')
   .attr('state', States[0])
   .attr('account_required_for_attendance?', false)
   .attr('scholarship_workshop?', false)
-  .attr('created_at', () => new Date().toISOString())
+  .attr('created_at', () => today.toISOString())
   .attr('capacity', 10)
   .attr('facilitators', [])
   .attr('location_name', 'virtual')
@@ -33,14 +38,74 @@ Factory.define('csp summer workshop')
   .attr('scholarship_workshop?', true)
   .attr('location_name', 'physical');
 
+Factory.define('csp summer workshop starting within month')
+  .extend('csp summer workshop')
+  .attr('sessions', () =>
+    Factory.buildList('session starting within month', 2)
+  );
+
+Factory.define('csd summer workshop starting within month')
+  .extend('csp summer workshop starting within month')
+  .attr('course', 'CS Discoveries');
+
+Factory.define('csp summer workshop starting in over a month')
+  .extend('csp summer workshop')
+  .attr('sessions', () =>
+    Factory.buildList('session starting in over a month', 1)
+  );
+
+/**
+ * SESSIONS
+ */
 Factory.define('session')
   .sequence('id')
   .attr('code', 'TEST')
-  .attr('start', new Date().toISOString())
-  .attr('end', new Date().toISOString())
+  .attr('start', today.toISOString())
+  .attr('end', today.toISOString())
   .attr('attendance_count', 0)
   .attr('show_link?', false);
 
+Factory.define('session starting within month')
+  .extend('session')
+  .attr(
+    'start',
+    new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 15
+    ).toISOString()
+  )
+  .attr(
+    'end',
+    new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 16
+    ).toISOString()
+  );
+
+Factory.define('session starting in over a month')
+  .extend('session')
+  .attr(
+    'start',
+    new Date(
+      today.getFullYear(),
+      today.getMonth() + 2,
+      today.getDate()
+    ).toISOString()
+  )
+  .attr(
+    'end',
+    new Date(
+      today.getFullYear(),
+      today.getMonth() + 2,
+      today.getDate()
+    ).toISOString()
+  );
+
+/**
+ * ENROLLMENTS
+ */
 Factory.define('enrollment')
   .sequence('id')
   .attr('email', 'fake@example.com')
