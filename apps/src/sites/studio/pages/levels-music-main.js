37867a201eec28908377e34f33168b10ccabc132
molly-moen <molly@code.org>
2023-04-12 16:53:19 -0700

    make it work in allthethings

--- apps/src/sites/studio/pages/levels-music-main.js
+++ apps/src/sites/studio/pages/levels-music-main.js
@@ -2,10 +2,13 @@ import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MusicLabView from '@cdo/apps/music/views/MusicView';
+import getScriptData from '../../../util/getScriptData';
 
 $(document).ready(function () {
+  const appOptions = getScriptData('appoptions');
+
   ReactDOM.render(
-    <MusicLabView />,
+    <MusicLabView channelId={appOptions.channel} />,
     document.getElementById('musiclab-container')
   );
 });
