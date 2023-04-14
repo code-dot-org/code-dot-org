4a3c73c3a8a6d737aedb3f35b8e38d945c95cc1d
denyslevada <levada.denys@gmail.com>
2023-04-14 17:09:12 +0300

    wip

--- apps/src/templates/studioHomepages/ImageResourceCard.jsx
+++ apps/src/templates/studioHomepages/ImageResourceCard.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Button from '../Button';
 import color from '../../util/color';
-import Typography from '../../componentLibrary/typography';
+import Typography, {Heading1} from '../../componentLibrary/typography';
 import style from './../dropdown-button.module.scss';
 class ImageResourceCard extends Component {
   static propTypes = {
@@ -40,7 +40,9 @@ class ImageResourceCard extends Component {
             </div>
             <div style={styles.description}>{description}</div>
           </div>
-          {/*<Heading1>THIS IS A TESt oF TS IN JS</Heading1>*/}
+          <Heading1 visualApproach="overline">
+            THIS IS A TESt oF TS IN JS
+          </Heading1>
           <Typography
             semanticTag="h1"
             visualApproach="body-one"
