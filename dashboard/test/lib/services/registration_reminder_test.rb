ba409be7f5b7d2d7ab741e869b0d495abbf0f3db
Meg Crenshaw <meg@code.org>
2023-04-19 13:29:09 -0400

    Update tests

--- dashboard/test/lib/services/registration_reminder_test.rb
+++ dashboard/test/lib/services/registration_reminder_test.rb
@@ -18,10 +18,9 @@ class Services::RegistrationReminderTest < ActiveSupport::TestCase
       Services::RegistrationReminder.send_registration_reminders!
       assert_empty application.emails.where(email_type: 'registration_reminder')
 
-      # Fake sending first accepted email by updating the status to accepted and setting sent_at field
+      # Fake send first accepted email
       Timecop.travel 1.day
-      application.update!(status: 'accepted')
-      application.emails.where(email_type: 'accepted').last.mark_sent!
+      create :pd_application_email, application: application, email_type: 'accepted', sent_at: DateTime.now
       Services::RegistrationReminder.send_registration_reminders!
       assert_empty application.emails.where(email_type: 'registration_reminder')
 
@@ -60,10 +59,6 @@ class Services::RegistrationReminderTest < ActiveSupport::TestCase
     end
   end
 
-  test 'applications_needing_first_reminder omits applications with no registration email' do
-    assert_equal 0, Services::RegistrationReminder.applications_needing_first_reminder.count
-  end
-
   test 'applications_needing_first_reminder omits applications with unsent registration email' do
     application = create :pd_teacher_application
     create :pd_application_email, application: application, email_type: 'accepted', sent_at: nil
@@ -95,18 +90,6 @@ class Services::RegistrationReminderTest < ActiveSupport::TestCase
     application = create :pd_teacher_application, created_at: DateTime.new(2019, 9, 30)
     create :pd_application_email, application: application, email_type: 'accepted', sent_at: 1.week.ago
     assert_equal 0, Services::RegistrationReminder.applications_needing_first_reminder.count
-  end
-
-  test 'applications_needing_first_reminder omits applications without an RP since the accepted email does not get sent' do
-    Timecop.freeze do
-      application = create :pd_teacher_application
-      application.update!(status: 'accepted')
-      assert_empty application.emails.where(email_type: 'accepted')
-
-      Timecop.travel 7.days
-      Services::RegistrationReminder.send_registration_reminders!
-      assert_equal 0, Services::RegistrationReminder.applications_needing_first_reminder.count
-    end
   end
 
   test 'applications_needing_first_reminder includes eligible applications' do
