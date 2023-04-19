9b0173905b3392d6f6b77b75734a3020a7e8cbd9
Meg Crenshaw <meg@code.org>
2023-04-19 15:05:30 -0400

    Change json file names

--- pegasus/test/test_homepage.rb
+++ pegasus/test/test_homepage.rb
@@ -11,15 +11,15 @@ class HomepageTest < Minitest::Test
     end
 
     it 'banner with showInternationally false is seen only by users in the US and with unknown country' do
-      Homepage.class_variable_set(:@@json_path, File.join("#{pegasus_dir}/test/fixtures/homepage", 'homepage_test_in_US_or_unknown_location_banner.json'))
+      Homepage.class_variable_set(:@@json_path, File.join("#{pegasus_dir}/test/fixtures/homepage", 'show_banner_in_US_and_unknown_location.json'))
 
       @request.stubs(:country).returns("US")
       banner = Homepage.get_announcement_for_page("homepage", @request)
-      assert_equal("show-in-US-or-unknown-location", banner[:id])
+      assert_equal("show-in-US-and-unknown-location", banner[:id])
 
       @request.stubs(:country).returns(nil)
       banner = Homepage.get_announcement_for_page("homepage", @request)
-      assert_equal("show-in-US-or-unknown-location", banner[:id])
+      assert_equal("show-in-US-and-unknown-location", banner[:id])
 
       @request.stubs(:country).returns("AF")
       banner = Homepage.get_announcement_for_page("homepage", @request)
@@ -27,7 +27,7 @@ class HomepageTest < Minitest::Test
     end
 
     it 'banner with showInternationally true is seen only by users we know are outside the US' do
-      Homepage.class_variable_set(:@@json_path, File.join("#{pegasus_dir}/test/fixtures/homepage", 'homepage_test_outside_US_banner.json'))
+      Homepage.class_variable_set(:@@json_path, File.join("#{pegasus_dir}/test/fixtures/homepage", 'show_banner_outside_US.json'))
 
       @request.stubs(:country).returns("US")
       banner = Homepage.get_announcement_for_page("homepage", @request)
@@ -43,7 +43,7 @@ class HomepageTest < Minitest::Test
     end
 
     it 'banner without showInternationally flag is seen by everyone' do
-      Homepage.class_variable_set(:@@json_path, File.join("#{pegasus_dir}/test/fixtures/homepage", 'homepage_test_everywhere_banner.json'))
+      Homepage.class_variable_set(:@@json_path, File.join("#{pegasus_dir}/test/fixtures/homepage", 'show_banner_everywhere.json'))
 
       @request.stubs(:country).returns("US")
       banner = Homepage.get_announcement_for_page("homepage", @request)
