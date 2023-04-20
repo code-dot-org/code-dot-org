6e4ef0bfbb60225d88a61c16918ec064550d518e
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2023-04-20 13:20:10 -0700

    remove dcdo flag
    will be merged separately

--- lib/dynamic_config/dcdo.rb
+++ lib/dynamic_config/dcdo.rb
@@ -46,8 +46,7 @@ class DCDOBase < DynamicConfigBase
       'csa-skinny-banner': DCDO.get('csa-skinny-banner', false),
       'webserial-on-chromeos': DCDO.get('webserial-on-chromeos', true),
       'csta-form-extension': DCDO.get('csta-form-extension', false),
-      'pl-launch-hero-banner': DCDO.get('pl-launch-hero-banner', false),
-      'teach-ai-launch-2023': DCDO.get('teach-ai-launch-2023', false)
+      'pl-launch-hero-banner': DCDO.get('pl-launch-hero-banner', false)
     }
   end
 end
