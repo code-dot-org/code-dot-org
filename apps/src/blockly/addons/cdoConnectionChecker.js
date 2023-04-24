ec38a85e1b7af69d6dbeac0c7d7ce624f313c039
Mike Harvey <mike.harvey@code.org>
2023-04-24 11:27:52 -0400

    add comments

--- apps/src/blockly/addons/cdoConnectionChecker.js
+++ apps/src/blockly/addons/cdoConnectionChecker.js
@@ -2,12 +2,12 @@ import GoogleBlockly from 'blockly/core';
 import {customConnectionBlockTypes} from './cdoConstants';
 
 /**
- * A connection checker that imposes stricter typing rules than the default
- * checker in Blockly, but uses the same rules for dragging and safety.
+ * A connection checker that imposes stricter typing rules for CDO custom
+ * connection type checks, but uses the same rules for dragging and safety.
  * This checker still expects nullable arrays of string for connection
  * type checks, and still looks for intersections in the arrays. Unlike the
- * default checker, null checks arrays are only compatible with other null
- * arrays.
+ * default checker, check arrays that contain a custom connection type are
+ * only compatible with other check arrays with the same connection type.
  * @implements {Blockly.IConnectionChecker}
  */
 
@@ -22,9 +22,7 @@ export default class CdoConnectionChecker extends GoogleBlockly.ConnectionChecke
   /**
    * Check whether this connection is compatible with another connection with
    * respect to the value type system.  E.g. square_root("Hello") is not
-   * compatible. For our custom implementation, blocks with custom connection
-   * types (Behaviors, Locations, Sprites) should are not compatible with null
-   * connection types.
+   * compatible. Includes custom logic for CDO custom connection type checks.
    *
    * @param a Connection to compare.
    * @param b Connection to compare against.
@@ -34,18 +32,22 @@ export default class CdoConnectionChecker extends GoogleBlockly.ConnectionChecke
     const checkArrayOne = a.getCheck();
     const checkArrayTwo = b.getCheck();
 
+    // For our custom implementation, connections are rejected if only one
+    // check array contains a CDO custom connection type (Behaviors, Locations,
+    // Sprites). E.g. A block with a sprite input should not accept a number.
     const customConnectionChecks = Object.values(customConnectionBlockTypes);
-    const foundInOne = !!checkArrayOne?.some(check =>
+    const checkArrayOneContainsCustomType = !!checkArrayOne?.some(check =>
       customConnectionChecks.includes(check)
     );
-    const foundInTwo = !!checkArrayTwo?.some(check =>
+    const checkArrayTwoContainsCustomType = !!checkArrayTwo?.some(check =>
       customConnectionChecks.includes(check)
     );
-
-    if (foundInOne !== foundInTwo) {
+    if (checkArrayOneContainsCustomType !== checkArrayTwoContainsCustomType) {
       return false;
     }
 
+    // Based on:
+    // https://github.com/google/blockly/blob/ec6e951e687953dedfb0072f0941b44ad6278994/core/connection_checker.ts#L187-L199
     if (!checkArrayOne || !checkArrayTwo) {
       // One or both sides are promiscuous enough that anything will fit.
       return true;
