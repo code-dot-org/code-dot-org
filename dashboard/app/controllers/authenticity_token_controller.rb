228d0309297a58f9fcfd3a63ce7d120775237d45
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-04-25 11:06:43 -0700

    Add Gatekeeper

--- dashboard/app/controllers/authenticity_token_controller.rb
+++ dashboard/app/controllers/authenticity_token_controller.rb
@@ -1,6 +1,7 @@
 class AuthenticityTokenController < ApplicationController
   # GET /get_token
   def get_token
+    return head :forbidden unless Gatekeeper.allows('csrf-token-endpoint', default: true)
     headers['csrf-token'] = form_authenticity_token
     return head :ok
   end
