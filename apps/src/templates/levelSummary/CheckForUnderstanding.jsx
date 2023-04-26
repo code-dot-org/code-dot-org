ffd6b4e3ce3b1f51df9474463ca93b7f7bf4eed6
Ryan Shipp <1382374+rshipp@users.noreply.github.com>
2023-04-26 09:58:01 -0500

    Revert "[CFU] Add eyes tests for summary view, second try (#51517)"
    This reverts commit 19c906849549f3a17440e93b9cd08e141f1ad5d7.

--- apps/src/templates/levelSummary/CheckForUnderstanding.jsx
+++ apps/src/templates/levelSummary/CheckForUnderstanding.jsx
@@ -67,7 +67,7 @@ const CheckForUnderstanding = ({
   };
 
   return (
-    <div className={styles.summaryContainer} id="summary-container">
+    <div className={styles.summaryContainer}>
       {/* Top Nav Links */}
       <p className={styles.navLinks}>
         <a
