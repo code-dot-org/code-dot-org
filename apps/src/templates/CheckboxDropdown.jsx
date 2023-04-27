f1061c69c723f7c7f45fd9266f5844a6e647d903
Turner Riley <turnertriley@gmail.com>
2023-04-27 10:41:58 -0700

    add SelectAll and ClearAll filter buttons

--- apps/src/templates/CheckboxDropdown.jsx
+++ apps/src/templates/CheckboxDropdown.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from './FontAwesome';
+import i18n from '@cdo/locale';
+import style from './checkbox-dropdown.module.scss';
 
 const CheckboxDropdown = ({
   name,
@@ -8,6 +10,8 @@ const CheckboxDropdown = ({
   allOptions,
   checkedOptions = [],
   onChange,
+  handleSelectAll,
+  handleClearAll,
 }) => (
   <div id={`${name}-dropdown`} className="dropdown">
     <button
@@ -25,17 +29,35 @@ const CheckboxDropdown = ({
           <li key={optionKey} className="checkbox form-group">
             <input
               type="checkbox"
-              id={`${name}-${optionKey}-check`}
+              id={`${optionKey}-check`}
               name={optionKey}
               value={optionKey}
               checked={checkedOptions.includes(optionKey)}
               onChange={onChange}
             />
-            <label htmlFor={`${name}-${optionKey}-check`}>
+            <label htmlFor={`${optionKey}-check`}>
               {allOptions[optionKey]}
             </label>
           </li>
         ))}
+        {handleSelectAll && (
+          <button
+            className={style.affectAllButton}
+            type="button"
+            onClick={() => handleSelectAll(name)}
+          >
+            {i18n.selectAll()}
+          </button>
+        )}
+        {handleClearAll && (
+          <button
+            className={style.affectAllButton}
+            type="button"
+            onClick={() => handleClearAll(name)}
+          >
+            {i18n.clearAll()}
+          </button>
+        )}
       </form>
     </ul>
   </div>
@@ -46,6 +68,8 @@ CheckboxDropdown.propTypes = {
   allOptions: PropTypes.objectOf(PropTypes.string).isRequired,
   checkedOptions: PropTypes.arrayOf(PropTypes.string),
   onChange: PropTypes.func.isRequired,
+  handleSelectAll: PropTypes.func,
+  handleClearAll: PropTypes.func,
 };
 
 export default CheckboxDropdown;
