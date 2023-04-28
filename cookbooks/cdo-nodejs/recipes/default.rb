93031473121e33433605e9ec2388a75e62761bc6
Seth Nickell <snickell@gmail.com>
2023-04-28 10:30:43 -1000

    Consistently use single-quotes

--- cookbooks/cdo-nodejs/recipes/default.rb
+++ cookbooks/cdo-nodejs/recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-is_ubuntu_18_04 = node["platform"] == "ubuntu" && node["platform_version"] == "18.04"
+is_ubuntu_18_04 = node['platform'] == 'ubuntu' && node['platform_version'] == '18.04'
 is_node_18 = node['cdo-nodejs']['version'] == '18.x'
 
 if is_ubuntu_18_04 && is_node_18
@@ -14,16 +14,21 @@ if is_ubuntu_18_04 && is_node_18
 
   package %w(libssl-dev python g++ g++-8 gcc-8)
 
-  ark "nodejs-source" do
-    url "https://nodejs.org/dist/v18.16.0/node-v18.16.0.tar.gz"
-    version "18.16.0"
-    make_opts ["-j 8"]
+  ark 'nodejs-source' do
+    url 'https://nodejs.org/dist/v18.16.0/node-v18.16.0.tar.gz'
+    checksum '6a4f5c5d76e5c50cef673099e56f19bc3266ae363f56ca0ab77dd2f3c5088c6d'
+    version '18.16.0'
+    make_opts ['-j 8']
     action :install_with_make
     environment(
       C: '/usr/bin/gcc-8',
       CXX: '/usr/bin/g++-8',
       PYTHON: 'python3'
     )
+  end
+
+  package 'nodejs' do
+    action :remove
   end
 else
   node.default['nodejs']['repo'] = "https://deb.nodesource.com/node_#{node['cdo-nodejs']['version']}"
