603cb9796930713dd32ce83a140845f6e1055ca6
Emily Eastlake <emily.eastlake@code.org>
2023-04-28 08:14:27 -0700

    updates

--- apps/src/p5lab/AnimationPicker/animationPickerImageGeneratorDropdowns.js
+++ apps/src/p5lab/AnimationPicker/animationPickerImageGeneratorDropdowns.js
@@ -14,7 +14,7 @@ export const select = {
       <option value="calm">Calm</option>
       <option value="anxious">Anxious</option>
       <option value="surprised">Surprised</option>
-      <option value="bored">Bored</option>
+      <option value="silly">Silly</option>
     </select>
   ),
   colors: changeHandler => (
@@ -23,13 +23,14 @@ export const select = {
       id={`${GENERATOR_DROPDOWNS_PREFIX}-colors`}
       onChange={changeHandler}
     >
-      <option value="rainbow">rainbow</option>
-      <option value="pink">pink</option>
-      <option value="peach">peach</option>
-      <option value="green">green</option>
-      <option value="aqua">aqua</option>
-      <option value="fuchsia">fuchsia</option>
-      <option value="yellow">sunflower</option>
+      <option value="rainbow">Rainbow</option>
+      <option value="red">Red</option>
+      <option value="orange">Orange</option>
+      <option value="yellow">Yellow</option>
+      <option value="green">Green</option>
+      <option value="blue">Blue</option>
+      <option value="purple">Purple</option>
+      <option value="pink">Pink</option>
     </select>
   ),
   animals: changeHandler => (
@@ -41,42 +42,12 @@ export const select = {
       <option value="unicorn">Unicorn</option>
       <option value="narwhal">Narwhal</option>
       <option value="panda">Panda</option>
-      <option value="platypus">Platypus</option>
-      <option value="meerkat">Meerkat</option>
-      <option value="chameleon">Chameleon</option>
       <option value="hedgehog">Hedgehog</option>
       <option value="dragon">Dragon</option>
-      <option value="griffin">Griffin</option>
       <option value="dolphin">Dolphin</option>
-      <option value="sloth">Sloth</option>
       <option value="axolotl">Axolotl</option>
-    </select>
-  ),
-  number: changeHandler => (
-    <select
-      type="number"
-      id={`${GENERATOR_DROPDOWNS_PREFIX}-number`}
-      name="number"
-      onChange={changeHandler}
-    >
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-    </select>
-  ),
-  bodyParts: changeHandler => (
-    <select
-      name="bodyParts"
-      id={`${GENERATOR_DROPDOWNS_PREFIX}-bodyParts`}
-      onChange={changeHandler}
-    >
-      <option value="paws">Paws</option>
-      <option value="wings">Wings</option>
-      <option value="tail">Tail</option>
-      <option value="beak">Beak</option>
-      <option value="fins">Fins</option>
-      <option value="antennae">Antennae</option>
+      <option value="astronaut">Astronaut</option>
+      <option value="dancer">Dancer</option>
     </select>
   ),
   artisticStyles: changeHandler => (
@@ -88,14 +59,11 @@ export const select = {
       <option value="realistic">Realistic</option>
       <option value="cartoon">Cartoon</option>
       <option value="abstract">Abstract</option>
-      <option value="surreal">Surreal</option>
-      <option value="minimalist">Minimalist</option>
-      <option value="stained_glass">Stained Glass</option>
       <option value="watercolor">Watercolor</option>
-      <option value="pointillist">Pointillist</option>
-      <option value="popArt">Pop Art</option>
       <option value="graffiti">Graffiti</option>
       <option value="pixelArt">Pixel Art</option>
+      <option value="sketch">Sketch</option>
+      <option value="robot">Robot</option>
     </select>
   )
 };
