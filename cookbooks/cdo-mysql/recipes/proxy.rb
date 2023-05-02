9096432c49c8aca6755262849d7a838cf80a053b
Seth Nickell <snickell@gmail.com>
2023-05-01 16:46:13 -1000

    Replace `wjordan/*` GitHub refs with `code-dot-org/*` forks

--- cookbooks/cdo-mysql/recipes/proxy.rb
+++ cookbooks/cdo-mysql/recipes/proxy.rb
@@ -18,7 +18,7 @@
 proxysql_filename = 'proxysql_2.0.15-ubuntu18_amd64.deb'
 proxysql_file = "#{Chef::Config[:file_cache_path]}/#{proxysql_filename}"
 remote_file proxysql_file do
-  source "https://github.com/wjordan/proxysql/releases/download/v2.0.15-aurora_2.09_fix/#{proxysql_filename}"
+  source "https://github.com/code-dot-org/proxysql/releases/download/v2.0.15-aurora_2.09_fix/#{proxysql_filename}"
   checksum "29fe79e54bce0f532084bfd8e5a13a55f1f8530f92be8a0e7db847aefcb1762f"
 end
 dpkg_package('proxysql') do
