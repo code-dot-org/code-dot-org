a98e7c53b8a6489ffcea1f8fd0f3f8c07dd50f0f
Dani <dani@code.org>
2023-05-02 17:52:03 -0400

    Add a comment to the tests

--- apps/test/unit/templates/studioHomepages/TeacherHomepageTest.js
+++ apps/test/unit/templates/studioHomepages/TeacherHomepageTest.js
@@ -144,6 +144,11 @@ describe('TeacherHomepage', () => {
     assert(!wrapper.find('Notification').exists());
   });
 
+  /*
+    We have disabled the Census Banner on the Teacher Homepage (March 2023) to conserve
+    space. If we decide to show the banner again this test will need to be updated. See
+    TeacherHomepage.jsx to make the banner show.
+   */
   it('does not render a CensusTeacherBanner even if showCensusBanner is true', () => {
     const wrapper = setUp({showCensusBanner: true});
     assert.equal(wrapper.find('CensusTeacherBanner').length, 0);
