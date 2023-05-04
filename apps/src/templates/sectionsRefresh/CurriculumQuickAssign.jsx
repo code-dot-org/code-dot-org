a742b8a872e11e3cd9d21600435f847cc83d6d73
denyslevada <levada.denys@gmail.com>
2023-05-04 15:54:11 +0300

    * add logic for switch active curriculum audience * refactor CurriculumQuickAssignTopRow.jsx * optimize CurriculumQuickAssignTopRow.jsx

--- apps/src/templates/sectionsRefresh/CurriculumQuickAssign.jsx
+++ apps/src/templates/sectionsRefresh/CurriculumQuickAssign.jsx
@@ -165,10 +165,13 @@ export default function CurriculumQuickAssign({
   };
 
   // When selecting a marketing audience, ensure 'decide later' is unchecked
-  const updateMarketingAudience = marketingAudience => {
-    setMarketingAudience(marketingAudience);
-    setDecideLater(false);
-  };
+  const updateMarketingAudience = useCallback(
+    marketingAudience => {
+      setMarketingAudience(marketingAudience);
+      setDecideLater(false);
+    },
+    [setDecideLater, setMarketingAudience]
+  );
 
   // To distinguish between types of tables: HOC & PL vs Grade Bands
   const isPlOrHoc = () => {
