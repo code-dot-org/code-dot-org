86917494d12e8c2affb3be5319907703ba820cdf
molly-moen <molly@code.org>
2023-05-08 16:00:14 -0700

    remove commented out code

--- apps/src/sites/studio/pages/levels-music-main.js
+++ apps/src/sites/studio/pages/levels-music-main.js
@@ -5,10 +5,8 @@ import {Provider} from 'react-redux';
 import {getStore} from '@cdo/apps/redux';
 import LabContainer from '@cdo/apps/code-studio/components/LabContainer';
 import MusicLabView from '@cdo/apps/music/views/MusicView';
-// import getScriptData from '@cdo/apps/util/getScriptData';
 
 $(document).ready(function () {
-  // const appOptions = getScriptData('appoptions');
   // Some Music Lab-specific configuration.
   const appConfig = {
     'load-progression': 'true',
