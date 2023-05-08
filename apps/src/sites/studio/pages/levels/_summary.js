bc861e4e73d181bf014756e896647b2e0d6cfb10
Ryan Shipp <1382374+rshipp@users.noreply.github.com>
2023-05-08 08:35:58 -0500

    [CFU] Implement summary view for Multi levels (#51653)
    * Implement summary view for Multi levels
    
    * Fix test
    
    * Review feedback and a11y fixes
    
    * Fix tests
    
    * Replace container ID
    
    * Fix code and fix tests better

--- apps/src/sites/studio/pages/levels/_summary.js
+++ apps/src/sites/studio/pages/levels/_summary.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import {getStore} from '@cdo/apps/redux';
 import {Provider} from 'react-redux';
 import getScriptData from '@cdo/apps/util/getScriptData';
+import i18n from '@cdo/locale';
 import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
+import Notification, {NotificationType} from '@cdo/apps/templates/Notification';
 import SummaryTopLinks from '@cdo/apps/templates/levelSummary/SummaryTopLinks';
 import SummaryResponses from '@cdo/apps/templates/levelSummary/SummaryResponses';
 import SummaryTeacherInstructions from '@cdo/apps/templates/levelSummary/SummaryTeacherInstructions';
@@ -42,6 +44,22 @@ $(document).ready(() => {
       </InstructorsOnly>
     </Provider>,
     document.getElementById('summary-responses')
+  );
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <InstructorsOnly>
+        <Notification
+          type={NotificationType.feedback}
+          notice={i18n.feedbackShareBannerTitle()}
+          details={i18n.feedbackShareBannerDesc()}
+          buttonText={i18n.feedbackShareBannerButton()}
+          buttonLink={'https://forms.gle/XsjRL9L3Mo5aC3KbA'}
+          dismissible={false}
+        />
+      </InstructorsOnly>
+    </Provider>,
+    document.getElementById('summary-feedback-banner')
   );
 
   ReactDOM.render(
