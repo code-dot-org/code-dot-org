3e5ea1b6b348838c6f36ffe7d58c5522706031d6
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2023-05-10 14:57:48 -0500

    add ability to initialize value not in menuGenerator

--- apps/src/blockly/addons/cdoFieldDropdown.js
+++ apps/src/blockly/addons/cdoFieldDropdown.js
@@ -6,27 +6,39 @@ import {
 } from '@cdo/apps/p5lab/utils';
 
 export default class CdoFieldDropdown extends GoogleBlockly.FieldDropdown {
-  /** Add special case for ???
+  /**
+   * Add ability to initialize field value with an option not included
+   * in the `menuGenerator`.
    * @override
    */
   doClassValidation_(newValue) {
-    if (newValue === EMPTY_OPTION) {
-      return newValue;
-    } else {
-      return super.doClassValidation_(newValue);
-    }
+    return newValue;
   }
 
-  /** Add special case for ???
+  /**
+   * Add special case for ??? and options not defined in `menuGenerator`
    * @override
    */
   doValueUpdate_(newValue) {
+    this.value_ = newValue;
+    this.isDirty_ = true;
     if (newValue === EMPTY_OPTION) {
-      this.value_ = newValue;
-      this.isDirty_ = true;
       this.selectedOption_ = [EMPTY_OPTION, ''];
     } else {
-      super.doValueUpdate_(newValue);
+      const options = this.getOptions(true);
+      this.selectedOption_ = null;
+      for (let i = 0, option; (option = options[i]); i++) {
+        console.log(i, option);
+        if (option[1] === this.value_) {
+          this.selectedOption_ = option;
+        }
+      }
+      if (!this.selectedOption_) {
+        this.selectedOption_ = [
+          newValue.replace(/['"]+/g, '').toLowerCase(),
+          newValue,
+        ];
+      }
     }
   }
 
@@ -66,7 +78,7 @@ export default class CdoFieldDropdown extends GoogleBlockly.FieldDropdown {
 
       options = options.split(',').map(val => {
         val = val.trim();
-        // If val is a one of the options in this.menuGenerator_,
+        // If val is one of the options in this.menuGenerator_,
         // human-readable string is displayed.
         if (originalOptionsMap[val]) {
           return [originalOptionsMap[val], val];
