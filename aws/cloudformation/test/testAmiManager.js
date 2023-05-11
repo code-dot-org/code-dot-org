487863eb630d6395a3f2a764286de79df367cca4
Darin Webb <darin@code.org>
2023-05-10 23:09:30 -0500

    mark incomplete tests as skipped

--- aws/cloudformation/test/testAmiManager.js
+++ aws/cloudformation/test/testAmiManager.js
@@ -2,7 +2,7 @@ const AmiManager = require("../ami-manager");
 
 describe('AmiManager', () => {
     describe('handler', () => {
-        it('is an untested function', async () => {
+        it.skip('is an untested function', async () => {
             try {
                 await AmiManager.handler();
             } catch (error) {
--- aws/cloudformation/test/testS3Object.js
+++ aws/cloudformation/test/testS3Object.js
@@ -2,7 +2,7 @@ const S3Object = require("../s3Object");
 
 describe('S3Object', () => {
     describe('handler', () => {
-        it('is an untested function', async () => {
+        it.skip('is an untested function', async () => {
             try {
                 await S3Object.handler();
             } catch (error) {
