fe26ea286c88bd1ecd74e03f810f326411eca276
pablo-code-org <pablo@code.org>
2023-05-12 13:55:37 -0700

    Adding more API stuff

--- lib/cdo/honeybadger/honeybadger_url_builder.rb
+++ lib/cdo/honeybadger/honeybadger_url_builder.rb
@@ -12,6 +12,20 @@ class HoneybadgerUrlBuilder
     JSON.parse response
   end
 
+  def get_affected_users(fault)
+    return get_api_response("affected_users", params[fault: fault])
+  end
+
+  def get_faults(project_id)
+    return get_api_response("affected_users", params[project_id: project_id])
+  end
+
+  def get_report_by_user(project_id)
+    return get_api_response("reports_by_user", params[project_id: project_id])
+  end
+
+  private
+
   def get_api_response(type, params)
     operation_url = get_api_url_request(type, params)
     call_api_response_from_url(operation_url)
@@ -24,9 +38,10 @@ class HoneybadgerUrlBuilder
     if type == 'faults'
       return get_faults_url(params[:project_id])
     end
+    if type == "reports_by_user"
+      return get_reports_by_user_url(params[:project_id])
+    end
   end
-
-  private
 
   # join the filters by a 'white space' or "%20"
   # This is used while a better query builder for honeybadger is implemented
@@ -37,6 +52,14 @@ class HoneybadgerUrlBuilder
 
   def get_time_filter_query
     @time_filters.join('&').to_s
+  end
+
+  def get_summary_url(project_id)
+    "/v2/projects/#{project_id}/faults/summary?#{get_time_filter_query}&#{get_filters_query}"
+  end
+
+  def get_reports_by_user_url(project_id)
+    "/v2/projects/#{project_id}/reports/notices_by_user?#{get_time_filter_query}&#{get_filters_query}"
   end
 
   def get_affected_users_url(fault)
