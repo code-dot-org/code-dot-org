61a96bd3abe10539b40a74f92e9084dadb0bb85d
Suresh Chanmugam <suresh@code.org>
2023-05-15 10:32:28 -0700

    Update link to download compiled rbyspy binary for Linux on x86 to latest release.

--- cookbooks/cdo-apps/recipes/rbspy.rb
+++ cookbooks/cdo-apps/recipes/rbspy.rb
@@ -1,7 +1,7 @@
 # Install https://rbspy.github.io/ to allow manual debugging of ruby performance issues.
 
 ark 'rbspy' do
-  url "https://github.com/rbspy/rbspy/releases/download/v0.3.8/#{name}-v0.3.8-x86_64-unknown-linux-musl.tar.gz"
+  url "https://github.com/rbspy/rbspy/releases/download/v0.17.0/#{name}-x86_64-musl.tar.gz"
   strip_components 0
   has_binaries ['rbspy']
 end
