477ac2db0d86c14d57f5b0bfacbd2dea9eea8c93
Seth Nickell <seth.nickell@code.org>
2023-05-17 02:35:35 -1000

    Add drone-rerun-failed()

--- bin/drone.sh
+++ bin/drone.sh
@@ -24,3 +24,26 @@ drone-vscode-tunnel () {
     exit
 CMDS
 }
+
+drone-rerun-failed () {
+  drone-do $1 <<- 'CMDS'
+    export CI=true
+    export CIRCLECI=true
+    export RAILS_ENV=test
+    export RACK_ENV=test
+    export DISABLE_SPRING=1
+    export LD_LIBRARY_PATH=/usr/local/lib
+    # If running on Drone.io, DRONE_BUILD_NUMBER will be set: https://docs.drone.io/reference/environ/drone-build-number/
+    # otherwise, use a random number instead. CIRCLE_BUILD_NUM determines where UI test cucumber logs are stored in S3.
+    export CIRCLE_BUILD_NUM=${DRONE_BUILD_NUMBER:-$RANDOM$RANDOM}
+    export CIRCLE_NODE_INDEX=1
+    rm -rf /home/circleci/test_reports
+    rm -rf /home/circleci/artifacts
+    export CIRCLE_TEST_REPORTS=/home/circleci/test_reports
+    export CIRCLE_ARTIFACTS=/home/circleci/artifacts
+
+    mkdir -p $CIRCLE_ARTIFACTS
+    
+    CIRCLE_RERUN_FAILED=1 bundle exec rake circle:run_ui_tests
+CMDS
+}
