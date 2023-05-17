48a288632b1d9576453d3d7da3e3b17f4c14c989
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-05-17 16:21:58 -0700

    Revert "Revert "Update `selenium-webdriver` gem to 4.2.0""

--- dashboard/test/ui/features/step_definitions/steps.rb
+++ dashboard/test/ui/features/step_definitions/steps.rb
@@ -299,15 +299,13 @@ When /^I wait for (\d+(?:\.\d*)?) seconds?$/ do |seconds|
   sleep seconds.to_f
 end
 
-When /^I rotate to landscape$/ do
+When /^I rotate to (landscape|portrait)$/ do |orientation|
   if ENV['BS_ROTATABLE'] == "true"
-    @browser.rotate(:landscape)
-  end
-end
-
-When /^I rotate to portrait$/ do
-  if ENV['BS_ROTATABLE'] == "true"
-    @browser.rotate(:portrait)
+    $http_client.call(
+      :post,
+      "/wd/hub/session/#{@browser.session_id}/orientation",
+      {orientation: orientation.upcase}
+    )
   end
 end
 
@@ -503,7 +501,7 @@ When /^I click selector "([^"]*)" if I see it$/ do |selector|
     @browser.execute_script("return $(\"#{selector}:visible\").length != 0;")
   end
   @browser.execute_script("$(\"#{selector}:visible\")[0].click();")
-rescue Selenium::WebDriver::Error::TimeOutError
+rescue Selenium::WebDriver::Error::TimeoutError
   # Element never appeared, ignore it
 end
 
@@ -554,7 +552,7 @@ When /^I type "([^"]*)" into "([^"]*)" if I see it$/ do |input_text, selector|
     @browser.execute_script("return $(\"#{selector}:visible\").length != 0;")
   end
   type_into_selector("\"#{input_text}\"", selector)
-rescue Selenium::WebDriver::Error::TimeOutError
+rescue Selenium::WebDriver::Error::TimeoutError
   # Element never appeared, ignore it
 end
 
@@ -930,7 +928,7 @@ end
 def wait_for_jquery
   wait_until do
     @browser.execute_script("return (typeof jQuery !== 'undefined');")
-  rescue Selenium::WebDriver::Error::ScriptTimeOutError
+  rescue Selenium::WebDriver::Error::ScriptTimeoutError
     puts "execute_script timed out after 30 seconds, likely because this is \
 Safari and the browser was still on about:blank when wait_for_jquery \
 was called. Ignoring this error and continuing to wait..."
