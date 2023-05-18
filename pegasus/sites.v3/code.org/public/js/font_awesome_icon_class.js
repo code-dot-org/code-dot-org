ac9cb153fa8747499f71cb73e4b4371128abc531
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2023-05-18 12:19:01 -0700

    add fa-fw class on all icon elements

---
+++ pegasus/sites.v3/code.org/public/js/font_awesome_icon_class.js
@@ -0,0 +1,4 @@
+// Adds a .fa-fw class to all icon elements
+// Used to maintain consistent spacing when
+// icons have varying widths and heights
+document.querySelectorAll("i").forEach((icon) => icon.classList.add("fa-fw"));
