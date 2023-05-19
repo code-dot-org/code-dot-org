5055795a5b076cce4b6888002a042ac977cddf0e
Erin Ferreirae <erin.ferreirae@code.org>
2023-05-19 07:42:56 -0700

    Add reminders_sent, index on uuid, shorten uuid length

--- dashboard/app/models/parental_permission_request.rb
+++ dashboard/app/models/parental_permission_request.rb
@@ -2,16 +2,18 @@
 #
 # Table name: parental_permission_requests
 #
-#  id           :bigint           not null, primary key
-#  user_id      :integer          not null
-#  parent_email :string(255)      not null
-#  uuid         :string(255)      not null
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id             :bigint           not null, primary key
+#  user_id        :integer          not null
+#  parent_email   :string(255)      not null
+#  uuid           :string(36)       not null
+#  reminders_sent :integer          default(0), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
 #
 # Indexes
 #
 #  index_parental_permission_requests_on_user_id  (user_id)
+#  index_parental_permission_requests_on_uuid     (uuid)
 #
 class ParentalPermissionRequest < ApplicationRecord
   before_create :set_uuid
