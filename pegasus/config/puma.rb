f9e1a634944fa177cda4e2d3f1ca974551d16d39
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-05-23 14:47:43 -0700

    Revert "Revert "Update `puma` Gem to 5.6.5""

--- pegasus/config/puma.rb
+++ pegasus/config/puma.rb
@@ -38,9 +38,3 @@ unless DCDO.get('oobgc_middleware_disabled', false)
   require 'gctools/oobgc'
   out_of_band {GC::OOB.run}
 end
-
-# Log thread backtraces and GC stats from all worker processes every second when enabled.
-plugin :log_stats
-LogStats.threshold = -> {DCDO.get('logStatsPegasus', nil)}
-filter_gems = %w(puma sinatra actionview activesupport honeybadger newrelic rack)
-LogStats.backtrace_filter = ->(bt) {CDO.filter_backtrace(bt, filter_gems: filter_gems)}
