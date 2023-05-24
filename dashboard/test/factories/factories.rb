2f273fd3d8f37ec528e3ec97ea178a48dd062309
Nick Lathe <nick.lathe@code.org>
2023-05-24 10:44:52 -0700

    Add additional lti_user_identities model files
      * Add factory
      * Add unit tests
      * Add LTI User Identity model
    
    Signed-off-by: Nick Lathe <nick.lathe@code.org>

--- dashboard/test/factories/factories.rb
+++ dashboard/test/factories/factories.rb
@@ -1697,6 +1697,12 @@ FactoryBot.define do
     access_token_url {"access_token_url"}
   end
 
+  factory :lti_user_identity do
+    subject {"subject"}
+    lti_integration {create :lti_integration}
+    user {create :student}
+  end
+
   factory :parental_permission_request do
     user {create :student}
     parent_email {"contact@example.domain"}
