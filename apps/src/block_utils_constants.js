d1d7bec847ea4444436c8b235d9987ccdbdc4722
Ben Brooks <ben@code.org>
2023-05-26 15:48:37 -0700

    Break up block utils

---
+++ apps/src/block_utils_constants.js
@@ -0,0 +1,7 @@
+export const DROPDOWN_INPUT = 'dropdown';
+export const VALUE_INPUT = 'value';
+export const INLINE_DUMMY_INPUT = 'inlineDummy';
+export const DUMMY_INPUT = 'dummy';
+export const STATEMENT_INPUT = 'statement';
+export const FIELD_INPUT = 'field';
+export const VARIABLE_INPUT = 'variable';
