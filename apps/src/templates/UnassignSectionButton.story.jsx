8c16194d2fb757a63dc49651f92a59afc1cc9e95
Hannah Bergam <hannahbergam@gmail.com>
2023-05-30 11:14:03 -0700

    Translating some of the last LP stories (#52090)
    * translating unassignbutton
    
    * lessonstatuslist translated
    
    * wip
    
    * fixing arguments
    
    * sections loading
    
    * fixing name
    
    * fixing tests

--- apps/src/templates/UnassignSectionButton.story.jsx
+++ apps/src/templates/UnassignSectionButton.story.jsx
@@ -4,16 +4,15 @@ import {UnconnectedUnassignSectionButton as UnassignSectionButton} from './Unass
 import {fakeTeacherSectionsForDropdown} from '@cdo/apps/templates/teacherDashboard/sectionAssignmentTestHelper';
 
 const assignedSection = fakeTeacherSectionsForDropdown[1];
-export default storybook => {
-  storybook.storiesOf('Buttons/UnassignSectionButton', module).addStoryTable([
-    {
-      name: 'UnassignSectionButton',
-      story: () => (
-        <UnassignSectionButton
-          sectionId={assignedSection.id}
-          unassignSection={action('unassignSection')}
-        />
-      ),
-    },
-  ]);
+
+export default {
+  name: 'UnassignSectionButton',
+  component: UnassignSectionButton,
 };
+
+export const Default = () => (
+  <UnassignSectionButton
+    sectionId={assignedSection.id}
+    unassignSection={action('unassignSection')}
+  />
+);
