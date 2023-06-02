b830c585b5a6a9647d1ff5872e4039faf00ea191
Turner Riley <turnertriley@gmail.com>
2023-06-02 10:06:31 -0700

    combine describes

--- apps/test/unit/templates/curriculumCatalog/CurriculumCatalogTest.jsx
+++ apps/test/unit/templates/curriculumCatalog/CurriculumCatalogTest.jsx
@@ -28,42 +28,64 @@ import {
 
 describe('CurriculumCatalog', () => {
   const defaultProps = {curriculaData: allCurricula, isEnglish: false};
+  let store;
+
+  let replacedLocation;
+  let replaceStateOrig = window.history.replaceState;
 
   beforeEach(() => {
-    const store = configureStore({reducer: {responsive}});
+    store = configureStore({reducer: {responsive}});
     store.dispatch(setResponsiveSize(ResponsiveSize.lg));
-    window.history.replaceState = () => {};
+
+    replacedLocation = undefined;
+    window.history.replaceState = (_, __, newLocation) => {
+      replacedLocation = newLocation;
+    };
+  });
+
+  afterEach(() => {
+    resetWindowLocation();
+    window.history.replaceState = replaceStateOrig;
+  });
+
+  function renderDefault() {
     render(
       <Provider store={store}>
         <CurriculumCatalog {...defaultProps} />
       </Provider>
     );
-  });
-
-  afterEach(() => {
-    resetWindowLocation();
-  });
+  }
 
   it('renders page title', () => {
+    renderDefault();
+
     screen.getByRole('heading', {name: 'Curriculum Catalog'});
   });
 
   it('renders page subtitle', () => {
+    renderDefault();
+
     screen.getByText('Code.org courses, tutorials, and more', {exact: false});
   });
 
   it('renders name of each curriculum with grade levels and path', () => {
+    renderDefault();
+
     allShownCurricula
       .map(curriculum => curriculum.display_name)
       .forEach(courseName => screen.getByRole('heading', {name: courseName}));
   });
 
   it('does not render any curriculum without grade levels and path', () => {
+    renderDefault();
+
     expect(screen.queryByText(noGradesCurriculum.display_name)).to.be.null;
     expect(screen.queryByText(noPathCurriculum.display_name)).to.be.null;
   });
 
   it('all curricula show an image, including curricula without a specific image', () => {
+    renderDefault();
+
     const images = screen.getAllByRole('img');
     const imagesStr = images.map(image => image.outerHTML).toString();
 
@@ -79,6 +101,8 @@ describe('CurriculumCatalog', () => {
   });
 
   it('filtering by grade level shows any shown course that supports one of the selected grades', () => {
+    renderDefault();
+
     const numTotalCurriculumCards = screen.getAllByText('Learn more', {
       exact: false,
     }).length;
@@ -104,6 +128,8 @@ describe('CurriculumCatalog', () => {
   });
 
   it('filtering by duration shows any shown course that is one of the selected durations', () => {
+    renderDefault();
+
     const numTotalCurriculumCards = screen.getAllByText('Learn more', {
       exact: false,
     }).length;
@@ -126,6 +152,8 @@ describe('CurriculumCatalog', () => {
   });
 
   it('filtering by topic shows any course with at least 1 of the selected topics', () => {
+    renderDefault();
+
     const numTotalCurriculumCards = screen.getAllByText('Learn more', {
       exact: false,
     }).length;
@@ -149,6 +177,8 @@ describe('CurriculumCatalog', () => {
   });
 
   it('filtering by Interdisciplinary topic shows any course labeled with school subjects', () => {
+    renderDefault();
+
     const numTotalCurriculumCards = screen.getAllByText('Learn more', {
       exact: false,
     }).length;
@@ -172,6 +202,8 @@ describe('CurriculumCatalog', () => {
   });
 
   it('filtering by device compatibility shows any course with at least 1 of the selected devices', () => {
+    renderDefault();
+
     const numTotalCurriculumCards = screen.getAllByText('Learn more', {
       exact: false,
     }).length;
@@ -197,6 +229,8 @@ describe('CurriculumCatalog', () => {
   });
 
   it('filtering by each filter shows subset of courses that match the filters', () => {
+    renderDefault();
+
     const numTotalCurriculumCards = screen.getAllByText('Learn more', {
       exact: false,
     }).length;
@@ -243,6 +277,8 @@ describe('CurriculumCatalog', () => {
   });
 
   it('applying every filter only filters out courses that have null for one of the filtered properties', () => {
+    renderDefault();
+
     const numTotalCurriculumCards = screen.getAllByText('Learn more', {
       exact: false,
     }).length;
@@ -266,6 +302,8 @@ describe('CurriculumCatalog', () => {
   });
 
   it('applying filters that yield no results shows no results message', () => {
+    renderDefault();
+
     // Does not show the no results message before filtering
     expect(
       screen.queryAllByText('No matching curricula', {
@@ -293,193 +331,164 @@ describe('CurriculumCatalog', () => {
       }).length
     ).to.equal(1);
   });
-});
 
-describe('CurriculumCatalog with url params', () => {
-  const defaultProps = {curriculaData: allCurricula, isEnglish: false};
-  let store;
-  let replaceStateOrig = window.history.replaceState;
-  let fakeWindowLocation = {
-    search: '',
-    pathname: '',
-  };
-  let replacedLocation;
+  describe('with url params', () => {
+    function renderWithUrlParams(urlParams) {
+      setWindowLocation({search: urlParams});
+      renderDefault();
+    }
 
-  beforeEach(() => {
-    store = configureStore({reducer: {responsive}});
-    store.dispatch(setResponsiveSize(ResponsiveSize.lg));
+    it('no url params applies no filters on load', () => {
+      renderWithUrlParams('');
 
-    setWindowLocation(fakeWindowLocation);
-    replacedLocation = undefined;
-    window.history.replaceState = (_, __, newLocation) => {
-      replacedLocation = newLocation;
-    };
-  });
-
-  afterEach(() => {
-    resetWindowLocation();
-    window.history.replaceState = replaceStateOrig;
-  });
-
-  function renderWithUrlParams(urlParams) {
-    setWindowLocation({search: urlParams});
-    render(
-      <Provider store={store}>
-        <CurriculumCatalog {...defaultProps} />
-      </Provider>
-    );
-  }
-
-  it('no url params applies no filters on load', () => {
-    renderWithUrlParams('');
-
-    expect(
-      screen.getAllByText('Learn more', {
-        exact: false,
-      }).length
-    ).to.equal(allShownCurricula.length);
-    allShownCurricula.forEach(curriculum => {
-      expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      expect(
+        screen.getAllByText('Learn more', {
+          exact: false,
+        }).length
+      ).to.equal(allShownCurricula.length);
+      allShownCurricula.forEach(curriculum => {
+        expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      });
     });
-  });
 
-  it('param with invalid filter key does not filter anything on load', () => {
-    renderWithUrlParams('?fakeKey=fakeValue');
+    it('param with invalid filter key does not filter anything on load', () => {
+      renderWithUrlParams('?fakeKey=fakeValue');
 
-    expect(
-      screen.getAllByText('Learn more', {
-        exact: false,
-      }).length
-    ).to.equal(allShownCurricula.length);
-    allShownCurricula.forEach(curriculum => {
-      expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      expect(
+        screen.getAllByText('Learn more', {
+          exact: false,
+        }).length
+      ).to.equal(allShownCurricula.length);
+      allShownCurricula.forEach(curriculum => {
+        expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      });
     });
-  });
 
-  it('param with valid filter key but no value does not filter anything on load', () => {
-    renderWithUrlParams('?duration=');
+    it('param with valid filter key but no value does not filter anything on load', () => {
+      renderWithUrlParams('?duration=');
 
-    expect(
-      screen.getAllByText('Learn more', {
-        exact: false,
-      }).length
-    ).to.equal(allShownCurricula.length);
-    allShownCurricula.forEach(curriculum => {
-      expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      expect(
+        screen.getAllByText('Learn more', {
+          exact: false,
+        }).length
+      ).to.equal(allShownCurricula.length);
+      allShownCurricula.forEach(curriculum => {
+        expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      });
     });
-  });
 
-  it('param with valid filter key but invalid value does not filter anything on load', () => {
-    renderWithUrlParams('?duration=fakeValue');
+    it('param with valid filter key but invalid value does not filter anything on load', () => {
+      renderWithUrlParams('?duration=fakeValue');
 
-    expect(
-      screen.getAllByText('Learn more', {
-        exact: false,
-      }).length
-    ).to.equal(allShownCurricula.length);
-    allShownCurricula.forEach(curriculum => {
-      expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      expect(
+        screen.getAllByText('Learn more', {
+          exact: false,
+        }).length
+      ).to.equal(allShownCurricula.length);
+      allShownCurricula.forEach(curriculum => {
+        expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      });
     });
-  });
 
-  it('params with valid filter key and value applies filters on load', () => {
-    renderWithUrlParams('?duration=week');
+    it('params with valid filter key and value applies filters on load', () => {
+      renderWithUrlParams('?duration=week');
 
-    expect(
-      screen.getAllByText('Learn more', {
-        exact: false,
-      }).length
-    ).to.equal(weeklongShownCurricula.length);
-    weeklongShownCurricula.forEach(curriculum => {
-      expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      expect(
+        screen.getAllByText('Learn more', {
+          exact: false,
+        }).length
+      ).to.equal(weeklongShownCurricula.length);
+      weeklongShownCurricula.forEach(curriculum => {
+        expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      });
     });
-  });
 
-  it('params with some valid filter keys and some valid values applies valid filters on load', () => {
-    renderWithUrlParams(
-      '?grade=kindergarten&fakeKey=grade_4&grade=fakeValue&grade=grade_2'
-    );
+    it('params with some valid filter keys and some valid values applies valid filters on load', () => {
+      renderWithUrlParams(
+        '?grade=kindergarten&fakeKey=grade_4&grade=fakeValue&grade=grade_2'
+      );
 
-    expect(
-      screen.getAllByText('Learn more', {
-        exact: false,
-      }).length
-    ).to.equal(gradesKAnd2ShownCurricula.length);
-    gradesKAnd2ShownCurricula.forEach(curriculum => {
-      expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      expect(
+        screen.getAllByText('Learn more', {
+          exact: false,
+        }).length
+      ).to.equal(gradesKAnd2ShownCurricula.length);
+      gradesKAnd2ShownCurricula.forEach(curriculum => {
+        expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      });
     });
-  });
 
-  it('params with different valid filter keys and different valid values are all applied on load', () => {
-    renderWithUrlParams(
-      '?grade=grade_2&grade=grade_3&topic=interdisciplinary&topic=physical_computing&device=tablet&device=no_device'
-    );
+    it('params with different valid filter keys and different valid values are all applied on load', () => {
+      renderWithUrlParams(
+        '?grade=grade_2&grade=grade_3&topic=interdisciplinary&topic=physical_computing&device=tablet&device=no_device'
+      );
 
-    expect(
-      screen.getAllByText('Learn more', {
-        exact: false,
-      }).length
-    ).to.equal(multipleFiltersAppliedShownCurricula.length);
-    multipleFiltersAppliedShownCurricula.forEach(curriculum => {
-      expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      expect(
+        screen.getAllByText('Learn more', {
+          exact: false,
+        }).length
+      ).to.equal(multipleFiltersAppliedShownCurricula.length);
+      multipleFiltersAppliedShownCurricula.forEach(curriculum => {
+        expect(screen.getAllByText(curriculum.display_name).length).to.equal(1);
+      });
     });
-  });
 
-  it('params update when first filter checkbox is selected', () => {
-    renderWithUrlParams('');
+    it('params update when first filter checkbox is selected', () => {
+      renderWithUrlParams('');
 
-    // Select "Week" in grade level filter
-    const weekFilterCheckbox = screen.getByDisplayValue('week');
-    fireEvent.click(weekFilterCheckbox);
-    assert(weekFilterCheckbox.checked);
+      // Select "Week" in grade level filter
+      const weekFilterCheckbox = screen.getByDisplayValue('week');
+      fireEvent.click(weekFilterCheckbox);
+      assert(weekFilterCheckbox.checked);
 
-    assert(replacedLocation.includes('duration=week'));
-  });
+      assert(replacedLocation.includes('duration=week'));
+    });
 
-  it('params update when filter checkbox is selected with others in same filter already selected', () => {
-    renderWithUrlParams('?duration=lesson');
+    it('params update when filter checkbox is selected with others in same filter already selected', () => {
+      renderWithUrlParams('?duration=lesson');
 
-    // Select "Week" in grade level filter
-    const weekFilterCheckbox = screen.getByDisplayValue('week');
-    fireEvent.click(weekFilterCheckbox);
-    assert(weekFilterCheckbox.checked);
+      // Select "Week" in grade level filter
+      const weekFilterCheckbox = screen.getByDisplayValue('week');
+      fireEvent.click(weekFilterCheckbox);
+      assert(weekFilterCheckbox.checked);
 
-    assert(replacedLocation.includes('duration=lesson&duration=week'));
-  });
+      assert(replacedLocation.includes('duration=lesson&duration=week'));
+    });
 
-  it('params update when filter checkbox is selected with others in different filter already selected', () => {
-    renderWithUrlParams('?grade=grade_2&grade=grade_3');
+    it('params update when filter checkbox is selected with others in different filter already selected', () => {
+      renderWithUrlParams('?grade=grade_2&grade=grade_3');
 
-    // Select "Week" in grade level filter
-    const weekFilterCheckbox = screen.getByDisplayValue('week');
-    fireEvent.click(weekFilterCheckbox);
-    assert(weekFilterCheckbox.checked);
+      // Select "Week" in grade level filter
+      const weekFilterCheckbox = screen.getByDisplayValue('week');
+      fireEvent.click(weekFilterCheckbox);
+      assert(weekFilterCheckbox.checked);
 
-    assert(replacedLocation.includes('grade=grade_2&grade=grade_3'));
-    assert(replacedLocation.includes('duration=week'));
-  });
+      assert(replacedLocation.includes('grade=grade_2&grade=grade_3'));
+      assert(replacedLocation.includes('duration=week'));
+    });
 
-  it('params update when only checked filter checkbox is deselected', () => {
-    renderWithUrlParams('?duration=lesson');
+    it('params update when only checked filter checkbox is deselected', () => {
+      renderWithUrlParams('?duration=lesson');
 
-    // Deselect "Lesson" in grade level filter
-    const lessonFilterCheckbox = screen.getByDisplayValue('lesson');
-    fireEvent.click(lessonFilterCheckbox);
-    assert(!lessonFilterCheckbox.checked);
+      // Deselect "Lesson" in grade level filter
+      const lessonFilterCheckbox = screen.getByDisplayValue('lesson');
+      fireEvent.click(lessonFilterCheckbox);
+      assert(!lessonFilterCheckbox.checked);
 
-    // When no params are present, replacedLocation is set to undefined
-    expect(replacedLocation).to.be.undefined;
-  });
+      // When no params are present, replacedLocation is set to undefined
+      expect(replacedLocation).to.be.undefined;
+    });
 
-  it('params update when one of checked filter checkboxes is deselected', () => {
-    renderWithUrlParams('?duration=lesson&duration=week');
+    it('params update when one of checked filter checkboxes is deselected', () => {
+      renderWithUrlParams('?duration=lesson&duration=week');
 
-    // Deselect "Lesson" in grade level filter
-    const lessonFilterCheckbox = screen.getByDisplayValue('lesson');
-    fireEvent.click(lessonFilterCheckbox);
-    assert(!lessonFilterCheckbox.checked);
+      // Deselect "Lesson" in grade level filter
+      const lessonFilterCheckbox = screen.getByDisplayValue('lesson');
+      fireEvent.click(lessonFilterCheckbox);
+      assert(!lessonFilterCheckbox.checked);
 
-    assert(!replacedLocation.includes('lesson'));
-    assert(replacedLocation.includes('duration=week'));
+      assert(!replacedLocation.includes('lesson'));
+      assert(replacedLocation.includes('duration=week'));
+    });
   });
 });
