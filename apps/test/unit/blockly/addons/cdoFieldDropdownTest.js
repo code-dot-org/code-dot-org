02f6c9537450914a2aaaa3b89e8158d631c8c16f
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2023-06-05 10:28:58 -0500

    add newlines

--- apps/test/unit/blockly/addons/cdoFieldDropdownTest.js
+++ apps/test/unit/blockly/addons/cdoFieldDropdownTest.js
@@ -4,28 +4,34 @@ import {expect} from '../../../util/reconfiguredChai';
 describe('Testing CdoFieldDropdown function - getUpdateOptionsFromConfig', () => {
   const getUpdatedOptionsFromConfig =
     __TestInterface.getUpdatedOptionsFromConfig;
+
   describe('Test config string with printer-style number range', () => {
     it('Config only has a number range', () => {
       const config = '6-8';
       const options = getUpdatedOptionsFromConfig(config);
+
       expect(options).to.deep.equal([
         ['6', '6'],
         ['7', '7'],
         ['8', '8'],
       ]);
     });
+
     it('Config has only numbers separated by commas', () => {
       const config = '2, 5, 11';
       const options = getUpdatedOptionsFromConfig(config);
+
       expect(options).to.deep.equal([
         ['2', '2'],
         ['5', '5'],
         ['11', '11'],
       ]);
     });
+
     it('Config has number range and numbers separated by commas', () => {
       const config = '2, 4, 16-18';
       const options = getUpdatedOptionsFromConfig(config);
+
       expect(options).to.deep.equal([
         ['2', '2'],
         ['4', '4'],
@@ -35,6 +41,7 @@ describe('Testing CdoFieldDropdown function - getUpdateOptionsFromConfig', () =>
       ]);
     });
   });
+
   describe('Test config string that does not contain numbers', () => {
     it('Config has with fewer options than menuGenerator', () => {
       const config = "'CAT', 'SLOTH'";
@@ -45,11 +52,13 @@ describe('Testing CdoFieldDropdown function - getUpdateOptionsFromConfig', () =>
         ['bear', "'BEAR'"],
       ];
       const options = getUpdatedOptionsFromConfig(config, menuGenerator);
+
       expect(options).to.deep.equal([
         ['cat', "'CAT'"],
         ['sloth', "'SLOTH'"],
       ]);
     });
+
     it('Config has an option not included in menuGenerator', () => {
       const config = "'CAT', 'SLOTH', 'GIRAFFE'";
       const menuGenerator = [
@@ -59,6 +68,7 @@ describe('Testing CdoFieldDropdown function - getUpdateOptionsFromConfig', () =>
         ['bear', "'BEAR'"],
       ];
       const options = getUpdatedOptionsFromConfig(config, menuGenerator);
+
       expect(options).to.deep.equal([
         ['cat', "'CAT'"],
         ['sloth', "'SLOTH'"],
