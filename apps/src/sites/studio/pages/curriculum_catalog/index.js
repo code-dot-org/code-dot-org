a7a0a068309ced295b7fdbb7302187d2e2e6732e
Meg Crenshaw <meg@code.org>
2023-06-05 17:25:13 -0500

    Pass in sections and dispatch to store

--- apps/src/sites/studio/pages/curriculum_catalog/index.js
+++ apps/src/sites/studio/pages/curriculum_catalog/index.js
@@ -5,13 +5,17 @@ import {Provider} from 'react-redux';
 import {getStore} from '@cdo/apps/redux';
 import CurriculumCatalog from '../../../../templates/curriculumCatalog/CurriculumCatalog';
 import getScriptData from '@cdo/apps/util/getScriptData';
+import {setSections} from '@cdo/apps/templates/teacherDashboard/teacherSectionsRedux';
 
 $(document).ready(function () {
   const catalogData = getScriptData('catalog');
-  const {curriculaData, isEnglish} = catalogData;
+  const {curriculaData, isEnglish, sections} = catalogData;
+
+  const store = getStore();
+  store.dispatch(setSections(sections));
 
   ReactDOM.render(
-    <Provider store={getStore()}>
+    <Provider store={store}>
       <CurriculumCatalog curriculaData={curriculaData} isEnglish={isEnglish} />
     </Provider>,
     document.getElementById('curriculum-catalog-container')
