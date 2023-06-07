18ba8fa80e696b8fa65ec8c1945fc999116f22c1
Mike Harvey <mike.harvey@code.org>
2023-06-07 16:18:39 -0400

    add google blockly implementation of miniToolboxBlocks

--- apps/src/block_utils.js
+++ apps/src/block_utils.js
@@ -1070,60 +1070,86 @@ exports.createJsWrapperBlockCreator = function (
           miniToolboxBlocks &&
           (!window.appOptions || window.appOptions.level.miniToolbox)
         ) {
-          var toggle = new Blockly.FieldIcon('+');
-          if (Blockly.cdoUtils.isWorkspaceReadOnly(this.blockSpace)) {
-            toggle.setReadOnly();
-          }
-
-          var miniToolboxXml = '<xml>';
-          miniToolboxBlocks.forEach(block => {
-            miniToolboxXml += `\n <block type="${block}"></block>`;
-          });
-          miniToolboxXml += '\n</xml>';
-          // Block.isMiniFlyoutOpen is used in the blockly repo to track whether or not the horizontal flyout is open.
-          this.isMiniFlyoutOpen = false;
-          // On button click, open/close the horizontal flyout, toggle button text between +/-, and re-render the block.
-          Blockly.cdoUtils.bindBrowserEvent(
-            toggle.fieldGroup_,
-            'mousedown',
-            this,
-            () => {
-              if (Blockly.cdoUtils.isWorkspaceReadOnly(this.blockSpace)) {
-                return;
-              }
-
-              if (this.isMiniFlyoutOpen) {
-                toggle.setValue('+');
-              } else {
-                toggle.setValue('-');
-              }
-              this.isMiniFlyoutOpen = !this.isMiniFlyoutOpen;
-              this.render();
-              // If the mini flyout just opened, make sure mini-toolbox blocks are updated with the right thumbnails.
-              // This has to happen after render() because some browsers don't render properly if the elements are not
-              // visible. The root cause is that getComputedTextLength returns 0 if a text element is not visible, so
-              // the thumbnail image overlaps the label in Firefox, Edge, and IE.
-              if (this.isMiniFlyoutOpen) {
-                let miniToolboxBlocks = this.miniFlyout.blockSpace_.topBlocks_;
-                let rootInputBlocks = this.getConnections_(true /* all */)
-                  .filter(function (connection) {
-                    return connection.type === Blockly.INPUT_VALUE;
-                  })
-                  .map(function (connection) {
-                    return connection.targetBlock();
-                  });
-                miniToolboxBlocks.forEach(function (block, index) {
-                  block.shadowBlockValue_(rootInputBlocks[index]);
-                });
-              }
+          if (this.jsonInit) {
+            this.jsonInit({
+              message0: '%1',
+              args0: [
+                {
+                  type: 'field_flyout',
+                  name: 'FLYOUT',
+                  flyoutKey: 'blockName',
+                  foldoutText: '+',
+                  sizingBehavior: 'fitContent',
+                },
+              ],
+            });
+            if (this.workspace.rendered) {
+              this.workspace.registerToolboxCategoryCallback(
+                'blockName',
+                function (workspace) {
+                  let blocks = [];
+                  miniToolboxBlocks.forEach(blockType =>
+                    blocks.push(
+                      Blockly.Xml.textToDom(`<block type="${blockType}"/>`)
+                    )
+                  );
+                  return blocks;
+                }
+              );
             }
-          );
-
-          this.appendDummyInput()
-            .appendField(toggle, 'toggle')
-            .appendField(' ');
-
-          this.initMiniFlyout(miniToolboxXml);
+          } else {
+            var toggle = new Blockly.FieldIcon('+');
+            if (Blockly.cdoUtils.isWorkspaceReadOnly(this.blockSpace)) {
+              toggle.setReadOnly();
+            }
+            var miniToolboxXml = '<xml>';
+            miniToolboxBlocks.forEach(block => {
+              miniToolboxXml += `\n <block type="${block}"></block>`;
+            });
+            miniToolboxXml += '\n</xml>';
+            // Block.isMiniFlyoutOpen is used in the blockly repo to track whether or not the horizontal flyout is open.
+            this.isMiniFlyoutOpen = false;
+            // On button click, open/close the horizontal flyout, toggle button text between +/-, and re-render the block.
+            Blockly.cdoUtils.bindBrowserEvent(
+              toggle.fieldGroup_,
+              'mousedown',
+              this,
+              () => {
+                if (Blockly.cdoUtils.isWorkspaceReadOnly(this.blockSpace)) {
+                  return;
+                }
+                if (this.isMiniFlyoutOpen) {
+                  toggle.setValue('+');
+                } else {
+                  toggle.setValue('-');
+                }
+                this.isMiniFlyoutOpen = !this.isMiniFlyoutOpen;
+                this.render();
+                // If the mini flyout just opened, make sure mini-toolbox blocks are updated with the right thumbnails.
+                // This has to happen after render() because some browsers don't render properly if the elements are not
+                // visible. The root cause is that getComputedTextLength returns 0 if a text element is not visible, so
+                // the thumbnail image overlaps the label in Firefox, Edge, and IE.
+                if (this.isMiniFlyoutOpen) {
+                  let miniToolboxBlocks =
+                    this.miniFlyout.blockSpace_.topBlocks_;
+                  let rootInputBlocks = this.getConnections_(true /* all */)
+                    .filter(function (connection) {
+                      return connection.type === Blockly.INPUT_VALUE;
+                    })
+                    .map(function (connection) {
+                      return connection.targetBlock();
+                    });
+                  miniToolboxBlocks.forEach(function (block, index) {
+                    block.shadowBlockValue_(rootInputBlocks[index]);
+                  });
+                }
+              }
+            );
+            this.appendDummyInput()
+              .appendField(toggle, 'toggle')
+              .appendField(' ');
+            this.initMiniFlyout(miniToolboxXml);
+          }
         }
 
         // These blocks should not be loaded into a Google Blockly level.
