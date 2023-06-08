5f29049e038553bddb83ff18b26392f9677bf95e
Nick Lathe <nick.lathe@code.org>
2023-06-08 12:02:18 -0700

    PR feedback changes

--- dashboard/app/controllers/lti_v1_controller.rb
+++ dashboard/app/controllers/lti_v1_controller.rb
@@ -12,54 +12,51 @@ class LtiV1Controller < ApplicationController
       return unauthorized_status
     end
 
-    begin
-      lti_integration = LtiIntegration.find_by!(query_params)
-    rescue ActiveRecord::RecordNotFound
-      return unauthorized_status
-    end
+    lti_integration = LtiIntegration.find_by(query_params)
+    return unauthorized_status unless lti_integration
 
-    auth_request_body = {
-      scope: "openid",
-      response_type: "id_token",
+    auth_redirect_url = URI(lti_integration[:auth_redirect_url])
+    auth_redirect_url.query = {
+      scope: 'openid',
+      response_type: 'id_token',
       client_id: lti_integration[:client_id],
-      redirect_uri: "https://studio.code.org/lti/v1/authenticate", # TODO: where can we configure this to be dynamic depending on deployment environment, i.e. prod, localhost, etc?
+      redirect_uri: CDO.studio_url('/lti/v1/authenticate', CDO.default_scheme),
       login_hint:  params[:login_hint],
-      lti_message_hint: params[:lti_message_hint] ? params[:lti_message_hint] : "", # TODO: is this needed? Canvas sends it, what happens if we don't send it back? What about other LTI Platforms?
+      lti_message_hint: params[:lti_message_hint].to_s, # TODO: is this needed? Canvas sends it, what happens if we don't send it back? What about other LTI Platforms?
       state: create_and_set_state,
-      response_mode: "form_post",
+      response_mode: 'form_post',
       nonce: create_and_set_nonce,
-      prompt: "none",
-    }
+      prompt: 'none',
+    }.to_query
 
-    auth_request_params = auth_request_body.to_query
-    auth_redirect_url = lti_integration[:auth_redirect_url]
-
-    redirect_to "#{auth_redirect_url}?#{auth_request_params}"
+    redirect_to auth_redirect_url.to_s
   end
 
-  private def unauthorized_status
-    render(status: :unauthorized, json: {error: "Unauthorized"})
+  private
+
+  def unauthorized_status
+    render(status: :unauthorized, json: {error: 'Unauthorized'})
   end
 
-  private def create_and_set_nonce
+  def create_and_set_nonce
     # generate nonce, this will be set as a cookie.
     nonce = generate_random_string 10
     # generate hashed nonce, this will be returned to the LTI Platform in the
     # request params https://openid.net/specs/openid-connect-core-1_0.html#NonceNotes
     hashed_nonce = Digest::SHA2.hexdigest nonce
-    set_cookie("nonce", nonce)
+    set_cookie('nonce', nonce)
 
-    return hashed_nonce
+    hashed_nonce
   end
 
-  private def create_and_set_state
+  def create_and_set_state
     state = generate_random_string 10
-    set_cookie("state", state)
+    set_cookie('state', state)
 
-    return state
+    state
   end
 
-  private def set_cookie(name, value)
+  def set_cookie(name, value)
     cookies[:"#{name}"] = {
       value: value,
       httponly: true,
@@ -67,7 +64,7 @@ class LtiV1Controller < ApplicationController
     }
   end
 
-  private def generate_random_string(length)
+  def generate_random_string(length)
     SecureRandom.alphanumeric length
   end
 end
