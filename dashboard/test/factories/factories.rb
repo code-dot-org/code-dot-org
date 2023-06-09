8e26ba829263e3402d73abb142c87cb47dff8806
breville <brendan@code.org>
2023-06-09 09:01:29 +0000

    Remove unnecessary change

--- dashboard/test/factories/factories.rb
+++ dashboard/test/factories/factories.rb
@@ -692,11 +692,6 @@ FactoryBot.define do
     level_num {'custom'}
   end
 
-  factory :music, parent: :level, class: Music do
-    game {Game.music}
-    level_num {'custom'}
-  end
-
   factory :multi, parent: :level, class: Multi do
     game {create(:game, app: "multi")}
     transient do
