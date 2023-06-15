7ff79e3d199c04cfa491b45eb1532dd428402b72
Meg Crenshaw <meg@code.org>
2023-06-15 00:49:51 -0500

    Allow for custom styling via prop

--- apps/src/templates/AccessibleDialog.jsx
+++ apps/src/templates/AccessibleDialog.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 import FocusTrap from 'focus-trap-react';
 import CloseOnEscape from '@cdo/apps/templates/CloseOnEscape';
 import style from './accessible-dialogue.module.scss';
+import classnames from 'classnames';
 
-const AccessibleDialog = ({onClose, children}) => (
+const AccessibleDialog = ({onClose, children, className}) => (
   <>
     <div className={style.modalBackdrop} />
     <CloseOnEscape handleClose={onClose}>
       <FocusTrap>
-        <div aria-modal role="dialog" className={style.modal}>
+        <div
+          aria-modal
+          className={classnames(style.modal, className)}
+          role="dialog"
+        >
           {children}
         </div>
       </FocusTrap>
@@ -20,6 +25,7 @@ const AccessibleDialog = ({onClose, children}) => (
 AccessibleDialog.propTypes = {
   onClose: PropTypes.func.isRequired,
   children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 export default AccessibleDialog;
