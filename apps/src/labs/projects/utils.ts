9eb0b037ace22b5eab40d071fdf320eeeaf33749
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-06-22 11:08:39 -0700

    add comment

--- apps/src/labs/projects/utils.ts
+++ apps/src/labs/projects/utils.ts
@@ -14,6 +14,11 @@ interface PartialAppOptions {
  * if currently on a standalone project level (i.e. under /projects).
  *
  * @returns channel ID if on a standalone project level, null if not.
+ *
+ * Note: We are trying to use app options as little as possible.
+ * TODO: We should determine the value if 'isProjectLevel' from the level definition.
+ * We will still need to read channel ID from the app options object, but
+ * longer term, we may rework how this information is served from the backend.
  */
 export function getStandaloneProjectId(): string | null {
   const appOptions = getScriptData('appoptions') as PartialAppOptions;
