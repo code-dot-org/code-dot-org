a3c8b7ddcfc5e9b40e5c1008fa06bb3a802acf13
pablo-code-org <pablo@code.org>
2023-06-28 10:43:40 -0700

    [Google analytics] - Re enable google tag manager ID for google universal

--- apps/src/lib/util/GoogleAnalyticsReporter.js
+++ apps/src/lib/util/GoogleAnalyticsReporter.js
@@ -36,9 +36,26 @@ class GoogleAnalyticsReporter {
     this.initializeGoogleAnalytics();
 
     this.addEventListener();
-
+    this.initializeTagManager(
+      window,
+      document,
+      'script',
+      'dataLayer',
+      'GTM-TZZBRK5'
+    );
     window.trackEvent = this.trackEvent;
     window.readCookie = this.readCookie;
+  }
+
+  initializeTagManager(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+    const f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l !== 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
   }
 
   /**
