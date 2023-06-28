3ec9ee6606518bc4eba57b6f9e2cf71dc0681e31
Dayne <dayne@code.org>
2023-06-27 20:09:26 -0700

    Revert "Adding redirect to /lockout for young students who don't have permiss… (#52444)"
    This reverts commit d182f6e3ff686011c4260ae85ba9ac88d1bfdc59.

--- dashboard/test/controllers/sessions_controller_test.rb
+++ dashboard/test/controllers/sessions_controller_test.rb
@@ -6,9 +6,6 @@ class SessionsControllerTest < ActionController::TestCase
 
   setup do
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    CPA.stubs(:cpa_experience).
-      with(any_parameters).
-      returns(CPA::NEW_USER_LOCKOUT)
   end
 
   test 'login error derives locale from cdo.locale' do
