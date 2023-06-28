fb566d9dfb20780e8457be24b4b05270d290a714
Molly Moen <molly@code.org>
2023-06-28 12:40:52 -0700

    Convert projectRedux to redux toolkit (#52467)
    * convert projectRedux to redux toolkit
    
    * update comment
    
    * fix test
    
    * better types

--- apps/src/Globals.d.ts
+++ apps/src/Globals.d.ts
@@ -6,6 +6,9 @@ declare module '*.module.scss' {
   export default classes;
 }
 
+// Declaring dashboard as 'any' because it is not well documented.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare const dashboard: any;
 declare const IN_UNIT_TEST: boolean;
 declare const IN_STORYBOOK: boolean;
 declare const PISKEL_DEVELOPMENT_MODE: string;
