a015ca1da5ce9565feaf570bdfb6572e6a238b06
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2023-06-29 11:19:38 -0400

    use chrome ver constant

--- apps/src/lib/kits/maker/ui/SetupGuide.jsx
+++ apps/src/lib/kits/maker/ui/SetupGuide.jsx
@@ -78,16 +78,18 @@ export default class SetupGuide extends React.Component {
             dismissible
           />
         )}
-        {!isCodeOrgBrowser() && chromeVersion && chromeVersion < 90 && (
-          <Notification
-            type={NotificationType.warning}
-            notice={i18n.makerSetupDeprecationNoticeOldChromeTitle()}
-            details={i18n.makerSetupDeprecationNoticeOldChromeDetails()}
-            detailsLinkText={i18n.makerDeprecationNoticeLinkText()}
-            detailsLink={MAKER_DEPRECATION_SUPPORT_URL}
-            dismissible
-          />
-        )}
+        {!isCodeOrgBrowser() &&
+          chromeVersion &&
+          chromeVersion < MIN_CHROME_VERSION && (
+            <Notification
+              type={NotificationType.warning}
+              notice={i18n.makerSetupDeprecationNoticeOldChromeTitle()}
+              details={i18n.makerSetupDeprecationNoticeOldChromeDetails()}
+              detailsLinkText={i18n.makerDeprecationNoticeLinkText()}
+              detailsLink={MAKER_DEPRECATION_SUPPORT_URL}
+              dismissible
+            />
+          )}
         {!isCodeOrgBrowser() &&
           chromeVersion &&
           chromeVersion < MIN_CHROME_VERSION && (
