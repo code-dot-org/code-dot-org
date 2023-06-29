eb996669f4c858e6cddd9b4011793876fa0a51d3
molly-moen <molly@code.org>
2023-06-29 15:40:10 -0700

    new files

---
+++ apps/src/labs/header/ProjectWrappedShareDialog.tsx
@@ -0,0 +1,68 @@
+import ShareDialog from '@cdo/apps/code-studio/components/ShareDialog';
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {LabState} from '../labRedux';
+import {isSignedIn as getIsSignedIn} from '@cdo/apps/templates/currentUserRedux';
+import popupWindow from '@cdo/apps/code-studio/popup-window';
+
+const ProjectWrappedShareDialog: React.FunctionComponent<
+  ProjectWrappedShareDialogProps
+> = ({shareUrl}) => {
+  // TODO: add selector
+  const isProjectLevel = false;
+  const appType = useSelector(
+    (state: {lab: LabState}) => state.lab.channel?.projectType
+  );
+  const channelId = useSelector(
+    (state: {lab: LabState}) => state.lab.channel?.id
+  );
+  const isSignedIn: boolean = useSelector(
+    (state: {
+      currentUser: {signInState: 'Unknown' | 'SignedIn' | 'SignedOut'};
+    }) => getIsSignedIn(state)
+  );
+  const is13Plus = useSelector(
+    (state: {currentUser: {under13: boolean}}) => !state.currentUser.under13
+  );
+  // We don't current support dance party projects in labs.
+  const selectedSong = null;
+  // TODO: support thumbnail url.
+  const thumbnailUrl = null;
+  // TODO: support abuse reporting.
+  const exceedsAbuseThreshold = false;
+  const canPublish = false;
+  // TODO: When we support publishing, we can use this logic to determine if we can publish
+  //const canPublish = isSignedIn && appType && AllPublishableProjectTypes.includes(appType);
+  // TODO: this should come from ProjectManager when we support publishing.
+  const isPublished = false;
+  const canShareSocial = isSignedIn && is13Plus;
+
+  if (!channelId || !appType) {
+    return null;
+  }
+
+  return (
+    <ShareDialog
+      isProjectLevel={isProjectLevel}
+      allowSignedOutShare={appType === 'dance'}
+      shareUrl={shareUrl}
+      selectedSong={selectedSong}
+      thumbnailUrl={thumbnailUrl}
+      isAbusive={exceedsAbuseThreshold}
+      canPrint={appType === 'artist'}
+      canPublish={canPublish}
+      isPublished={isPublished}
+      channelId={channelId}
+      appType={appType}
+      onClickPopup={popupWindow}
+      canShareSocial={canShareSocial}
+      userSharingDisabled={false}
+    />
+  );
+};
+
+interface ProjectWrappedShareDialogProps {
+  shareUrl: string;
+}
+
+export default ProjectWrappedShareDialog;
---
+++ apps/src/labs/header/headerShareV2.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import LabRegistry from '../LabRegistry';
+import {getStore} from '@cdo/apps/redux';
+import ProjectWrappedShareDialog from './ProjectWrappedShareDialog';
+import {showShareDialog} from '@cdo/apps/code-studio/components/shareDialogRedux';
+
+export function shareProjectV2(shareUrl) {
+  const projectManager = LabRegistry.getInstance().getProjectManager();
+  if (!projectManager) {
+    return null;
+  }
+
+  projectManager.flushSave().then(() => {
+    var dialogDom = document.getElementById('project-share-dialog');
+    if (!dialogDom) {
+      dialogDom = document.createElement('div');
+      dialogDom.setAttribute('id', 'project-share-dialog');
+      document.body.appendChild(dialogDom);
+    }
+    ReactDOM.render(
+      <Provider store={getStore()}>
+        <ProjectWrappedShareDialog shareUrl={shareUrl} />
+      </Provider>,
+      dialogDom
+    );
+
+    getStore().dispatch(showShareDialog());
+  });
+}
