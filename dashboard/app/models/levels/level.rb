ed0e182b757b46e51dc1bd5d8665917a9cb69bd8
breville <brendan@code.org>
2023-07-03 22:20:51 +0000

    Add comment

--- dashboard/app/models/levels/level.rb
+++ dashboard/app/models/levels/level.rb
@@ -803,6 +803,11 @@ class Level < ApplicationRecord
     }
   end
 
+  # Summarize the properties for a lab2 level.
+  # Called by ScriptLevelsController.level_properties.
+  # These properties are usually just the serialized properties for
+  # the level, which usually include levelData.  If this level is a
+  # StandaloneVideo then we put its properties into levelData.
   def summarize_for_lab2_properties
     video = specified_autoplay_video&.summarize(false)&.camelize_keys
     properties_camelized = properties.camelize_keys
