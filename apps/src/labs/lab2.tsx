0d046bf1f5cb355a56381fc561cacd3227dc006b
breville <brendan@code.org>
2023-07-04 02:35:13 +0000

    LabContainer -> Lab2Wrapper

--- apps/src/labs/lab2.tsx
+++ apps/src/labs/lab2.tsx
@@ -1,13 +1,22 @@
+// Lab2
+//
+// A React component used for a ScriptLevel that uses Lab2.  It examines the
+// set of levels in the current lesson, determines the set of apps they use,
+// and instantiates a React component for each app that it supports.  This
+// allows level switching between those levels without a page reload.
+
 import React from 'react';
+import {useSelector} from 'react-redux';
 import {levelsForLessonId} from '@cdo/apps/code-studio/progressReduxSelectors';
 import {ProgressState} from '@cdo/apps/code-studio/progressRedux';
 import {LevelWithProgress} from '@cdo/apps/types/progressTypes';
 import {getStandaloneProjectId} from '@cdo/apps/labs/projects/utils';
+import {logError} from '@cdo/apps/music/utils/MusicMetrics';
+import Lab2Wrapper from '@cdo/apps/code-studio/components/Lab2Wrapper';
 import ProjectContainer from '@cdo/apps/labs/projects/ProjectContainer';
 import ProgressContainer from '@cdo/apps/labs/progress/ProgressContainer';
 import StandaloneVideo from '@cdo/apps/standaloneVideo/StandaloneVideo';
 import MusicView from '@cdo/apps/music/views/MusicView';
-import {useSelector} from 'react-redux';
 
 interface AppProperties {
   name: string;
@@ -56,37 +65,43 @@ const Lab2: React.FunctionComponent = () => {
     : undefined;
 
   return (
-    <ProjectContainer channelId={channelId}>
-      {currentLessonAppProperties.map(appProperty => {
-        if (appProperty.backgroundMode && appProperty.usesProgressManager) {
-          return (
-            <div
-              id={`lab2-${appProperty.name}`}
-              key={appProperty.name}
-              style={{
-                width: '100%',
-                height: '100%',
-                visibility: currentAppName === 'music' ? 'visible' : 'hidden',
-              }}
-            >
-              <ProgressContainer appType={appProperty.name}>
+    <Lab2Wrapper
+      onError={(error, componentStack) =>
+        logError({error: error.toString(), componentStack})
+      }
+    >
+      <ProjectContainer channelId={channelId}>
+        {currentLessonAppProperties.map(appProperty => {
+          if (appProperty.backgroundMode && appProperty.usesProgressManager) {
+            return (
+              <div
+                id={`lab2-${appProperty.name}`}
+                key={appProperty.name}
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  visibility: currentAppName === 'music' ? 'visible' : 'hidden',
+                }}
+              >
+                <ProgressContainer appType={appProperty.name}>
+                  {appProperty.node}
+                </ProgressContainer>
+              </div>
+            );
+          } else if (appProperty.name === currentAppName) {
+            return (
+              <div
+                id={`lab2-${appProperty.name}`}
+                key={appProperty.name}
+                style={{width: '100%', height: '100%'}}
+              >
                 {appProperty.node}
-              </ProgressContainer>
-            </div>
-          );
-        } else if (appProperty.name === currentAppName) {
-          return (
-            <div
-              id={`lab2-${appProperty.name}`}
-              key={appProperty.name}
-              style={{width: '100%', height: '100%'}}
-            >
-              {appProperty.node}
-            </div>
-          );
-        }
-      })}
-    </ProjectContainer>
+              </div>
+            );
+          }
+        })}
+      </ProjectContainer>
+    </Lab2Wrapper>
   );
 };
 
--- apps/src/sites/studio/pages/levels-lab2-main.js
+++ apps/src/sites/studio/pages/levels-lab2-main.js
@@ -3,22 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {getStore} from '@cdo/apps/redux';
-import LabContainer from '@cdo/apps/code-studio/components/LabContainer';
 import Lab2 from '@cdo/apps/labs/lab2';
-import {logError} from '@cdo/apps/music/utils/MusicMetrics';
 
 $(document).ready(function () {
   ReactDOM.render(
     <Provider store={getStore()}>
-      <LabContainer
-        onError={(error, componentStack) =>
-          logError({error: error.toString(), componentStack})
-        }
-      >
-        <Lab2 />
-      </LabContainer>
+      <Lab2 />
     </Provider>,
-
     document.getElementById('lab2-container')
   );
 });
