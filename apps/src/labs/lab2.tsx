c659bf1b094cde0d9d478512cb0e6360739ffffb
breville <brendan@code.org>
2023-07-04 00:08:39 +0000

    First pass at lab2 app properties

--- apps/src/labs/lab2.tsx
+++ apps/src/labs/lab2.tsx
@@ -9,33 +9,83 @@ import StandaloneVideo from '@cdo/apps/standaloneVideo/StandaloneVideo';
 import MusicView from '@cdo/apps/music/views/MusicView';
 import {useSelector} from 'react-redux';
 
+interface AppProperties {
+  name: string;
+  usesChannel: boolean;
+  usesProgressManager: boolean;
+  backgroundMode: boolean;
+  node: React.ReactNode;
+}
+
+const appsProperties: AppProperties[] = [
+  {
+    name: 'music',
+    usesChannel: true,
+    usesProgressManager: true,
+    backgroundMode: true,
+    node: <MusicView />,
+  },
+  {
+    name: 'standalone_video',
+    usesChannel: false,
+    usesProgressManager: true,
+    backgroundMode: false,
+    node: <StandaloneVideo />,
+  },
+];
+
 const Lab2: React.FunctionComponent = () => {
-  const currentApp = useSelector(
-    (state: {progress: ProgressState}) =>
-      levelsForLessonId(state.progress, state.progress.currentLessonId).find(
-        (level: LevelWithProgress) => level.isCurrentLevel
-      ).app
+  const currentLessonLevels = useSelector((state: {progress: ProgressState}) =>
+    levelsForLessonId(state.progress, state.progress.currentLessonId)
+  );
+  const currentAppName: string = currentLessonLevels.find(
+    (level: LevelWithProgress) => level.isCurrentLevel
+  ).app;
+  const currentLessonAppNames: string[] = Array.from(
+    new Set(currentLessonLevels.map((level: LevelWithProgress) => level.app))
+  );
+  const currentLessonAppProperties: AppProperties[] = appsProperties.filter(
+    appProperties => currentLessonAppNames.includes(appProperties.name)
+  );
+  const currentAppProperties: AppProperties | undefined = appsProperties.find(
+    appProperties => appProperties.name === currentAppName
   );
 
-  const channelId: string | undefined =
-    currentApp === 'music' ? getStandaloneProjectId() : undefined;
+  const channelId: string | undefined = currentAppProperties?.usesChannel
+    ? getStandaloneProjectId()
+    : undefined;
 
   return (
     <ProjectContainer channelId={channelId}>
-      <div
-        id="music-container"
-        style={{
-          width: '100%',
-          height: '100%',
-          visibility: currentApp === 'music' ? 'visible' : 'hidden',
-        }}
-      >
-        <ProgressContainer appType={'music'}>
-          <MusicView />
-        </ProgressContainer>
-      </div>
-
-      {currentApp === 'standalone_video' && <StandaloneVideo />}
+      {currentLessonAppProperties.map(appProperty => {
+        if (appProperty.backgroundMode && appProperty.usesProgressManager) {
+          return (
+            <div
+              id={`lab2-${appProperty.name}`}
+              key={appProperty.name}
+              style={{
+                width: '100%',
+                height: '100%',
+                visibility: currentAppName === 'music' ? 'visible' : 'hidden',
+              }}
+            >
+              <ProgressContainer appType={appProperty.name}>
+                {appProperty.node}
+              </ProgressContainer>
+            </div>
+          );
+        } else if (appProperty.name === currentAppName) {
+          return (
+            <div
+              id={`lab2-${appProperty.name}`}
+              key={appProperty.name}
+              style={{width: '100%', height: '100%'}}
+            >
+              {appProperty.node}
+            </div>
+          );
+        }
+      })}
     </ProjectContainer>
   );
 };
