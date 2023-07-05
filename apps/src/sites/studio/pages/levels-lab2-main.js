4472a8ae6da1fff94a33f198cedace3a964f11b6
breville <brendan@code.org>
2023-07-05 22:17:34 +0000

    Lab2 provides its own Provider

--- apps/src/sites/studio/pages/levels-lab2-main.js
+++ apps/src/sites/studio/pages/levels-lab2-main.js
@@ -1,15 +1,8 @@
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {getStore} from '@cdo/apps/redux';
 import Lab2 from '@cdo/apps/labs/views/lab2';
 
 $(document).ready(function () {
-  ReactDOM.render(
-    <Provider store={getStore()}>
-      <Lab2 />
-    </Provider>,
-    document.getElementById('lab2-container')
-  );
+  ReactDOM.render(<Lab2 />, document.getElementById('lab2-container'));
 });
