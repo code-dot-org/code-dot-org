554ce1e109dadb8a4192d779b85ec45f0fe95bfb
Turner Riley <turnertriley@gmail.com>
2023-07-06 13:08:22 -0700

    add Curriculum Catalog Sign In Clicked In Assign Dialog event

--- apps/src/lib/util/AnalyticsConstants.js
+++ apps/src/lib/util/AnalyticsConstants.js
@@ -65,6 +65,8 @@ const EVENTS = {
   CURRICULUM_CATALOG_VISITED_EVENT: 'Curriculum Catalog Visited',
   CURRICULUM_CATALOG_LEARN_MORE_CLICKED_EVENT: 'Learn More Clicked',
   CURRICULUM_CATALOG_ASSIGN_CLICKED_EVENT: 'Assign Clicked',
+  CURRICULUM_CATALOG_SIGN_IN_CLICKED_IN_ASSIGN_DIALOG:
+    'Sign In Or Create Account Clicked In Assign Dialog',
   CURRICULUM_CATALOG_ASSIGN_COMPLETED_EVENT:
     'Course Offering Assignment Completed',
 };
