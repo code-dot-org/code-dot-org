71bfa3d38c2aa50fdb344c43de9bb75d0b5718d7
denyslevada <levada.denys@gmail.com>
2023-07-07 19:52:14 +0300

    wip

--- apps/.storybook/preview.js
+++ apps/.storybook/preview.js
@@ -1,4 +1,18 @@
 import $ from 'jquery';
+import {initializeRTL} from 'storybook-addon-rtl';
+
+initializeRTL();
+
+// export const parameters = {
+//     // actions: {argTypesRegex: '^on[A-Z].*'},
+//     // controls: {
+//     //     expanded: true,
+//     //     // matchers: {
+//     //     //     color: /(background|color)$/i,
+//     //     //     date: /Date$/,
+//     //     // },
+//     // }
+// }
 
 //Stub jquery fileupload library function
 $.fn.fileupload = () => {};
