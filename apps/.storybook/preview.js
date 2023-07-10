2a7065dc2e09b81f8cabb2785afac84e3c9e8ee8
denyslevada <levada.denys@gmail.com>
2023-07-10 21:46:01 +0300

    review fix

--- apps/.storybook/preview.js
+++ apps/.storybook/preview.js
@@ -3,16 +3,5 @@ import {initializeRTL} from 'storybook-addon-rtl';
 
 initializeRTL();
 
-// export const parameters = {
-//     // actions: {argTypesRegex: '^on[A-Z].*'},
-//     // controls: {
-//     //     expanded: true,
-//     //     // matchers: {
-//     //     //     color: /(background|color)$/i,
-//     //     //     date: /Date$/,
-//     //     // },
-//     // }
-// }
-
 //Stub jquery fileupload library function
 $.fn.fileupload = () => {};
