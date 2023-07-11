d5c27b2534c1b015d96c9b7a301cb5fd76b8960c
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2023-07-11 17:32:43 -0500

    rename icon names

--- apps/src/blockly/addons/cdoFieldToggle.js
+++ apps/src/blockly/addons/cdoFieldToggle.js
@@ -7,26 +7,26 @@ export default class CdoFieldToggle extends GoogleBlockly.Field {
    *
    * @param {Object} options - The options for constructing the class.
    * @param {Function} options.onClick - The function that handles the field's editor.
-   * @param {SVGElement} options.icon1 - SVG <tspan> element - this is the icon that is initially displayed on the button.
-   * @param {SVGElement} options.icon2 - SVG <tspan> element - this is the icon that is displayed on the button after the first click.
+   * @param {SVGElement} options.defaultIcon - SVG <tspan> element - this is the icon that is initially displayed on the button.
+   * @param {SVGElement} options.alternateIcon - SVG <tspan> element - this is the icon that is displayed on the button after the first click.
    * @param {boolean} options.useDefaultIcon - Indicates which icon to use.
-   * @param {Function} [options.callback] - A function to call if icon2 is used.
+   * @param {Function} [options.callback] - A function to call if alternateIcon is used.
    * @param {Object} [options.colorOverrides] - An optional set of colors to use instead of the sourceBlock's styles.
    * @param {string} [options.colorOverrides.icon] - An override for the color of the icons.
    * @param {string} [options.colorOverrides.button] - An override for the toggle button color.
    */
   constructor({
     onClick,
-    icon1,
-    icon2,
+    defaultIcon,
+    alternateIcon,
     useDefaultIcon,
     callback,
     colorOverrides,
   }) {
     super();
     this.onClick = onClick;
-    this.icon1 = icon1;
-    this.icon2 = icon2;
+    this.defaultIcon = defaultIcon;
+    this.alternateIcon = alternateIcon;
     this.useDefaultIcon = useDefaultIcon;
     this.callback = callback;
     this.colorOverrides = colorOverrides;
@@ -50,9 +50,9 @@ export default class CdoFieldToggle extends GoogleBlockly.Field {
   initView() {
     super.initView();
     if (this.useDefaultIcon) {
-      this.textElement_.appendChild(this.icon1);
+      this.textElement_.appendChild(this.defaultIcon);
     } else {
-      this.textElement_.appendChild(this.icon2);
+      this.textElement_.appendChild(this.alternateIcon);
     }
   }
 
@@ -87,9 +87,9 @@ export default class CdoFieldToggle extends GoogleBlockly.Field {
    */
   showEditor_() {
     if (this.useDefaultIcon) {
-      this.textElement_.replaceChild(this.icon2, this.icon1);
+      this.textElement_.replaceChild(this.alternateIcon, this.defaultIcon);
     } else {
-      this.textElement_.replaceChild(this.icon1, this.icon2);
+      this.textElement_.replaceChild(this.defaultIcon, this.alternateIcon);
     }
     this.useDefaultIcon = !this.useDefaultIcon;
     this.onClick();
@@ -107,9 +107,9 @@ export default class CdoFieldToggle extends GoogleBlockly.Field {
     // If an override is not provided, use the block style to determine colors.
     // Primary and secondary colors are used to provide contrast between
     // the button and the icons.
-    this.icon1.style.fill =
+    this.defaultIcon.style.fill =
       this.colorOverrides?.icon || sourceBlock.style.colourPrimary;
-    this.icon2.style.fill =
+    this.alternateIcon.style.fill =
       this.colorOverrides?.icon || sourceBlock.style.colourPrimary;
     // applyColour is also called when creating insertion markers
     if (borderRect) {
