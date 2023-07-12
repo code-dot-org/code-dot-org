70695b129f86e146334080af6c410dc8b435a2ee
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2023-07-12 12:33:10 -0500

    add openai.js

---
+++ apps/src/util/openai.js
@@ -0,0 +1,13 @@
+export const openaiCompletion = messages => {
+  console.log('messages', messages);
+  return $.ajax({
+    url: '/openai/chat_completion',
+    method: 'POST',
+    data: JSON.stringify({messages}),
+    contentType: 'application/json',
+    headers: {
+      'X-CSRF-Token': appOptions.authenticityToken,
+    },
+    dataType: 'json',
+  });
+};
