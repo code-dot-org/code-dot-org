57f0136cf11ec9302ecda1983fb4866c85aca9b0
breville <brendan@code.org>
2023-07-14 20:56:28 +0000

    Adjust logic

--- apps/src/music/player/MusicLibrary.ts
+++ apps/src/music/player/MusicLibrary.ts
@@ -28,12 +28,14 @@ export default class MusicLibrary {
   getDefaultSound(): string | undefined {
     const firstGroup: FolderGroup = this.groups[0];
 
+    // Return the specified default sound if there is one.
+    if (firstGroup?.defaultSound) {
+      return firstGroup?.defaultSound;
+    }
+
     // The fallback is the first non-instrument/kit folder's first sound.
     const firstFolder = firstGroup?.folders.find(group => !group.type);
-    const firstSoundFullPath = `${firstFolder?.path}/${firstFolder?.sounds[0].src}`;
-
-    // Return the specified default sound or the fallback sound.
-    return firstGroup?.defaultSound || firstSoundFullPath;
+    return `${firstFolder?.path}/${firstFolder?.sounds[0].src}`;
   }
 
   getSoundForId(id: string): SoundData | null {
