a58d7a898a4f85074f8d4fb1bff83cb98ed64104
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-07-18 17:58:11 +0000

    clean up

--- dashboard/test/controllers/openai_sessions_controller_test.rb
+++ dashboard/test/controllers/openai_sessions_controller_test.rb
@@ -2,15 +2,6 @@ require 'test_helper'
 
 class OpenaiSessionsControllerTest < ActionController::TestCase
   setup do
-    @rsa_key_test = OpenSSL::PKey::RSA.new(2048)
-    OpenSSL::PKey::RSA.stubs(:new).returns(@rsa_key_test)
-    @fake_channel_id = storage_encrypt_channel_id(1, 1)
-
-    # JavalabFilesHelper.stubs(:get_project_files).returns({})
-    # JavalabFilesHelper.stubs(:get_project_files_with_override_sources).returns({})
-    # JavalabFilesHelper.stubs(:get_project_files_with_override_validation).returns({})
-    # put_response = Net::HTTPResponse.new(nil, '200', nil)
-    # JavalabFilesHelper.stubs(:upload_project_files).returns(put_response)
   end
 
   test_user_gets_response_for :chat_completion,
