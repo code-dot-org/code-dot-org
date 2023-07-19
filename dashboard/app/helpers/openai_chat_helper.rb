debf71adf9f6be6d1e517f39d1b6c69b0020c313
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-07-19 02:31:26 +0000

    remove helper

--- dashboard/app/helpers/openai_chat_helper.rb
+++
@@ -1,7 +0,0 @@
-module OpenaiChatHelper
-  OPEN_AI_URL = "https://api.openai.com/v1/chat/completions"
-  def self.get_chat_completion_response(headers, data)
-    puts "helper"
-    HTTParty.post(OPEN_AI_URL, headers: headers, body: data.to_json)
-  end
-end
