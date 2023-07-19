ae7c21459b81dd634e3663c2e98c320ffa2063e0
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-07-19 02:31:26 +0000

    remove helper

--- dashboard/app/controllers/openai_sessions_controller.rb
+++ dashboard/app/controllers/openai_sessions_controller.rb
@@ -1,19 +1,17 @@
 class OpenaiSessionsController < ApplicationController
-  include OpenaiChatHelper
   authorize_resource class: false
 
   OPENAI_CHAT_API_KEY = CDO.openai_chat_api_key
   CODE_ORG_ID = CDO.openai_org_id
   TEMPERATURE = 0
   GPT_MODEL = 'gpt-3.5-turbo'
+  OPEN_AI_URL = "https://api.openai.com/v1/chat/completions"
 
   # POST /openai/chat_completion
   def chat_completion
     puts "chat_completion"
-    puts "#{request.body}"
-    puts "#{request.body.read}"
     body = JSON.parse(request.body.read)
-    messages_to_send = body["messages"]
+    messages = body["messages"]
 
     # Set up the API endpoint URL and request headers
     headers = {
@@ -21,17 +19,16 @@ class OpenaiSessionsController < ApplicationController
       "Authorization" => "Bearer #{OPENAI_CHAT_API_KEY}",
       "OpenAI-Organization" => CODE_ORG_ID
     }
-    
 
     # Set up the API endpoint URL and request headers
     data = {
       model: GPT_MODEL,
       temperature: TEMPERATURE,
-      messages: message_to_send
+      messages: messages
     }
 
     # Send the request to the API endpoint
-    response = OpenaiChatHelper.get_chat_completion_response(headers, data)
+    response = HTTParty.post(OPEN_AI_URL, headers: headers, body: data.to_json)
 
     # Parse the response JSON and return the completed text
     if response.code == 200
@@ -41,14 +38,5 @@ class OpenaiSessionsController < ApplicationController
     else
       render json: {error: "Chat completion failed: #{response.to_json}"}, status: :bad_request
     end
-  end
-
-  private def has_required_params?(params)
-    begin
-      params.require(params)
-    rescue ActionController::ParameterMissing
-      return false
-    end
-    true
   end
 end
--- dashboard/test/controllers/openai_sessions_controller_test.rb
+++ dashboard/test/controllers/openai_sessions_controller_test.rb
@@ -2,23 +2,20 @@ require 'test_helper'
 
 class OpenaiSessionsControllerTest < ActionController::TestCase
   setup do
-    put_response = Net::HTTPResponse.new(nil, '200', {key:"value"})
-    puts "setup"
-    puts "#{put_response.body}"
-    OpenaiChatHelper.stubs(:get_chat_completion_response).returns(put_response)
   end
 
-  test_user_gets_response_for :chat_completion,
-  method: :post,
-  user: :student,
-  response: :forbidden
+  test 'chat_completion' do
+    user = create :ai_chat_access
+    sign_in(user)
+    post :chat_completion, params: {
+      "model": "gpt-3.5-turbo",
+      "messages": [{"role": "system", "content": "You are a helpful assistant."}]
+    }
+    assert_response :success
+  end
 
-  test_user_gets_response_for :chat_completion,
-  user: :teacher,
-  response: :forbidden
-
-  test_user_gets_response_for :chat_completion,
-  user: :ai_chat_access,
-  params: {"messages": [{"content":"","role":""}]},
-  response: :success
+  # test_user_gets_response_for :chat_completion,
+  # user: :ai_chat_access,
+  # params: {"messages": [{"content":"","role":""}]},
+  # response: :success
 end
