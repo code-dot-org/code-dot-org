c46a78dbd69fd71772166bc700888ac8a846d0d0
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-07-24 16:43:34 +0000

    add throttle

--- dashboard/app/controllers/openai_chat_controller.rb
+++ dashboard/app/controllers/openai_chat_controller.rb
@@ -2,6 +2,11 @@ class OpenaiChatController < ApplicationController
   include OpenaiChatHelper
   authorize_resource class: false
 
+  # Allowed number of unique requests per minute before that client is throttled.
+  # These values are fallbacks for DCDO-configured values used below.
+  REQUEST_LIMIT_PER_MIN_DEFAULT = 100
+  REQUEST_LIMIT_PER_MIN_IP = 1000
+
   # POST /openai/chat_completion
   def chat_completion
     unless has_required_messages_param?
@@ -9,10 +14,21 @@ class OpenaiChatController < ApplicationController
     end
 
     messages = params[:messages]
-    response = OpenaiChatHelper.request_chat_completion(messages)
-    chat_completion_return_message = OpenaiChatHelper.get_chat_completion_response_message(response)
-    return render(json: chat_completion_return_message[:json]) if chat_completion_return_message[:status] == 200
-    return render(status: chat_completion_return_message[:status], json: chat_completion_return_message[:json])
+    id = current_user&.id || session.id
+
+    # Only throttle by IP if no user or session ID is available.
+    throttle_ip = id.blank?
+    id ||= request.IP
+    limit = throttle_ip ?
+      DCDO.get('openai_chat_request_limit_per_min_ip', REQUEST_LIMIT_PER_MIN_IP) :
+      DCDO.get('openai_chat_request_limit_per_min_default', REQUEST_LIMIT_PER_MIN_DEFAULT)
+    period = 60
+
+    OpenaiChatHelper.throttled_request_chat_completion(messages, id, limit, period) do |response|
+      chat_completion_return_message = OpenaiChatHelper.get_chat_completion_response_message(response)
+      return render(status: chat_completion_return_message[:status], json: chat_completion_return_message[:json])
+      # Add a response header to indicate whether the request was throttled.
+    end
   end
 
   private def has_required_messages_param?
--- dashboard/app/helpers/openai_chat_helper.rb
+++ dashboard/app/helpers/openai_chat_helper.rb
@@ -1,11 +1,21 @@
+require 'cdo/throttle'
+
 module OpenaiChatHelper
+  OPENAI_CHAT_PREFIX = "openai_chat/".freeze
   OPEN_AI_URL = "https://api.openai.com/v1/chat/completions"
   OPENAI_CHAT_API_KEY = CDO.openai_chat_api_key
   CODE_ORG_ID = CDO.openai_org_id
   TEMPERATURE = 0
   GPT_MODEL = 'gpt-3.5-turbo'
 
-  def self.request_chat_completion(messages)
+  # Requests a chat completion from OpenAI's API.
+  # This method is throttled because it makes a third-party request to OpenAI.
+  # @param [String] messages - Array of messages
+  # @param [String] id - Unique identifier for throttling.
+  # @param [Integer] limit - Number of requests allowed over period.
+  # @param [Integer] period - Period of time in seconds.
+  def self.throttled_request_chat_completion(messages, id, limit, period)
+    return if Cdo::Throttle.throttle(OPENAI_CHAT_PREFIX + id.to_s, limit, period)
     # Set up the API endpoint URL and request headers
     headers = {
       "Content-Type" => "application/json",
