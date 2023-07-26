6916b5f36c7e9c9e76b72a21a56ed58a3a03cf74
Bethany Connor <bethany@code.org>
2023-07-26 13:37:49 -0400

    Add the menu bar and move content to a new component

--- apps/src/templates/rubrics/RubricFloatingActionButton.jsx
+++ apps/src/templates/rubrics/RubricFloatingActionButton.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import style from './rubrics.module.scss';
+const icon = require('@cdo/static/AI-FAB.png');
+import RubricContainer from './RubricContainer';
 
 export default function RubricFloatingActionButton() {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,11 +10,12 @@ export default function RubricFloatingActionButton() {
     <div id="fab-contained">
       <button
         className={style.floatingActionButton}
-        style={{}}
+        // I couldn't get an image url to work in the SCSS module, so using an inline style for now
+        style={{backgroundImage: `url(${icon})`}}
         onClick={() => setIsOpen(!isOpen)}
         type="button"
       />
-      {isOpen && <div className={style.rubricContainer}>hi</div>}
+      {isOpen && <RubricContainer />}
     </div>
   );
 }
