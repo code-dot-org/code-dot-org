71d873799bdc8a7ac31a584648982d9f27337557
Anna Xu <annaxu@wustl.edu>
2023-07-26 14:30:10 -0700

    cleaning up syntax and adding comments

--- bin/test/i18n/test_i18n_metrics.rb
+++ bin/test/i18n/test_i18n_metrics.rb
@@ -1,5 +1,5 @@
 require_relative '../test_helper'
-require_relative '../../i18n/i18n_metrics'
+require_relative '../../i18n/metrics'
 require 'cdo/aws/metrics'
 
 class I18nMetricsTest < Minitest::Test
@@ -9,11 +9,11 @@ class I18nMetricsTest < Minitest::Test
   end
 
   def expect_metric(name, value, dimensions)
-    Cdo::Metrics.expects(:put_metric).with("I18n", {metric_name: name, value: value, dimensions: dimensions})
+    Cdo::Metrics.expects(:put_metric).with('I18n', {metric_name: name, value: value, dimensions: dimensions}).once
   end
 
   def test_report_runtime
-    expect_metric(:Runtime, 1, [{name: "dim1", value: 1}, {name: "dim2", value: 2}, {name: "Environment", value: :test}, {name: "MachineId", value: "local_machine"}])
-    I18n::Metrics.report_runtime([{name: "dim1", value: 1}, {name: "dim2", value: 2}]) {1}
+    expect_metric(:Runtime, 1, [{name: 'dim1', value: 1}, {name: 'dim2', value: 2}, {name: 'Environment', value: :test}, {name: 'MachineId', value: 'local_machine'}])
+    I18n::Metrics.report_runtime([{name: 'dim1', value: 1}, {name: 'dim2', value: 2}]) {1}
   end
 end
