454b3f84f338969b63029a5833a1e1584f939b72
Juan Manzo <juan.manzo@code.org>
2023-07-26 09:45:06 -0700

    Ternary Operator

--- apps/src/lib/levelbuilder/CourseOfferingEditor.jsx
+++ apps/src/lib/levelbuilder/CourseOfferingEditor.jsx
@@ -30,10 +30,7 @@ const translatedNoneOption = `(${i18n.none()})`;
 const useCourseOffering = initialCourseOffering => {
   const [courseOffering, setCourseOffering] = useState(initialCourseOffering);
   const updateCourseOffering = (key, value) => {
-    if (value === '') {
-      value = null;
-    }
-    setCourseOffering({...courseOffering, [key]: value});
+    setCourseOffering({...courseOffering, [key]: value !== '' ? value : null});
   };
 
   return [courseOffering, updateCourseOffering];
