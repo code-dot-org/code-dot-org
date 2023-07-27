c160d3a11a8c5e4fa3f37c21d957173601c9dd53
Bethany Connor <bethany@code.org>
2023-07-27 10:06:04 -0400

    I18n-ize eventual user-facing string

--- apps/src/templates/rubrics/RubricContainer.jsx
+++ apps/src/templates/rubrics/RubricContainer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from './rubrics.module.scss';
+import i18n from '@cdo/locale';
 import {Heading6} from '@cdo/apps/componentLibrary/typography';
 
 export default function RubricContainer() {
   return (
     <div className={style.rubricContainer}>
       <div className={style.rubricHeader}>
-        <Heading6>Rubrics</Heading6>
+        <Heading6>{i18n.rubrics()}</Heading6>
       </div>
       <div className={style.rubricContent}>Rubric Content to Come</div>
     </div>
