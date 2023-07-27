c53738f5fdc598f78a05b0f189174c2ad2e0be07
Hannah Bergam <hannahbergam@gmail.com>
2023-07-26 18:52:10 -0700

    adding test file

---
+++ apps/test/unit/templates/SortByNameDropdownTest.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from '../../util/reconfiguredChai';
+import SortByNameDropdown from '@cdo/apps/templates/SortByNameDropdown';
+
+describe('SortByNameDropdown', () => {
+  it('renders dropdown', () => {
+    const wrapper = shallow(<SortByNameDropdown />);
+    expect(wrapper.find('select').length).to.equal(1);
+  });
+
+  it('makes call to update familyName sorting in localStorage', () => {
+    localStorage.setItem('sortByFamilyName', 'false');
+    const wrapper = shallow(<SortByNameDropdown />);
+    wrapper.find('select').simulate('change', {value: 'true'});
+    expect(localStorage.getItem('sortByFamilyName')).to.equal('true');
+  });
+});
