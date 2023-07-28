f9b26f9a3d91d5181be196ecd7700c49a9db94cc
Nick Lathe <nick.lathe@code.org>
2023-07-28 12:36:52 -0700

    Minor updates, trying to get this to work

--- dashboard/app/helpers/lti_access_token_helper.rb
+++ dashboard/app/helpers/lti_access_token_helper.rb
@@ -1,8 +1,8 @@
-module LtiAccessToken
+module LtiAccessTokenHelper
   NAMESPACE = "lti_v1_controller".freeze
 
   # client_id and issuer are present in the id_token we cache or store as a cookie from the LTI launch
-  def self.get_access_token(client_id, issuer)
+  def get_access_token(client_id, issuer)
     cache_key = "#{NAMESPACE}/#{client_id}-#{issuer}"
 
     # check for valid access token
@@ -15,9 +15,9 @@ module LtiAccessToken
 
     # assemble JWT payload
     jwt_payload = {
-      iss: 'studio.code.org',
-      sub: integration[:issuer],
-      aud: [access_token_url],
+      iss: CDO.studio_url('', CDO.default_scheme),
+      sub: client_id,
+      aud: access_token_url,
       iat: Time.now.to_i,
       # this can be short, since the JWT will be validated as part of the access_token request handshake. Start at 5 minutes, reduce it if we feel like it.
       exp: (5).minutes.from_now.to_i,
@@ -34,23 +34,19 @@ module LtiAccessToken
     pri = OpenSSL::PKey::RSA.new(private_key)
     jwt = JWT.encode(jwt_payload, pri, 'RS256', kid: kid)
 
-    # format request body (must be url form encoded)
-    req_access_token_url = URI(access_token_url)
-    req_access_token_url.query = {
+    query = {
       grant_type: 'client_credentials',
       client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
       client_assertion: jwt,
       scope: 'https://purl.imsglobal.org/spec/lti-ags/scope/lineitem https://purl.imsglobal.org/spec/lti-ags/scope/result/read',
-    }.to_query
+    }
 
-    # POST request to access_token_url
-    res = HTTParty.post(req_access_token_url.to_s)
+    res = HTTParty.post(access_token_url, query: query, headers: {'Content-Type' => 'application/x-www-form-urlencoded'})
     # get access_token and exp from response
     access_token = res.body[:access_token]
-    exp = res.body[:exp]
+    exp = res.body[:expires_in]
 
     # cache access_token, set expiration
-    # TODO: need to convert exp probably
     CDO.shared_cache.write(cache_key, access_token, expires_in: exp)
 
     return access_token
