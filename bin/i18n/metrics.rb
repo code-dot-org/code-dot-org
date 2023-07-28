c340ce62ee88867c005a55ff0f18e35c8f3e601d
Mario Gil Correa <mario.gil-correa@code.org>
2023-07-27 20:46:07 -0500

    merging with metrics PR

--- bin/i18n/metrics.rb
+++ bin/i18n/metrics.rb
@@ -2,13 +2,21 @@ require 'cdo/aws/metrics'
 require 'aws-sdk-ec2'
 require 'net/http'
 require 'socket'
+require 'benchmark'
 
 module I18n
   module Metrics
     I18N_METRICS_NAMESPACE = 'I18n'.freeze
 
+    # loggin to clodwach runtime of a yield block
+    # @param addtl_dimensions [Array<Hash>] additional dimensions
+    # addtl_dimension should include {name: "SyncStep", value: } and {name: "MethodName", value: }
     def self.report_runtime(addtl_dimensions = [])
-      log_metric(:Runtime, yield, addtl_dimensions)
+      result = nil
+      runtime = Benchmark.realtime {result = yield}
+      log_metric(:RuntimeTest, runtime, addtl_dimensions, 'Seconds')
+
+      result
     end
 
     # returns the EC2 instance ID if we are running the sync from an EC2,
@@ -21,7 +29,14 @@ module I18n
       end
     end
 
-    def self.log_metric(metric_name, value, addtl_dimensions = [])
+    # logging metrics into cloudwatch under i18n name space
+    # @param metric_name [Symbol] A metric name
+    # @param metric_value [Object] A metric value
+    # @param addtl_dimensions [Array<Hash>] additional dimensions
+    # @option metric_units [String] Units of the value. Ex. Seconds, Kilobytes, etc.
+    # Fo more valid units see CloudWatch MetricDatum:
+    # https://docs.aws.amazon.com/AmazonCloudWatch/latest/APIReference/API_MetricDatum.html
+    def self.log_metric(metric_name, metric_value, addtl_dimensions = [], metric_units='None')
       # add machine_id and environment dimensions to addtl_dimensions
       addtl_dimensions << {name: 'Environment', value: CDO.rack_env}
       addtl_dimensions << {name: 'MachineId', value: machine_id}
@@ -31,7 +46,8 @@ module I18n
           {
             metric_name: metric_name,
             dimensions: addtl_dimensions,
-            value: value
+            value: metric_value,
+            unit: metric_units
           }
       )
     end
