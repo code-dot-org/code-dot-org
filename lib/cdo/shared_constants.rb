19950b7572262cbd1cc546ccd6475b5cfe18e1f1
Bethany Connor <bethany@code.org>
2023-07-28 12:10:32 -0400

    Create shared constants, strings, and function to convert between them for evidence levels

--- lib/cdo/shared_constants.rb
+++ lib/cdo/shared_constants.rb
@@ -597,4 +597,13 @@ module SharedConstants
     # An unhandleable error that results in a program crash.
     FATAL: 4
   }.freeze
+
+  RUBRIC_UNDERSTANDING_LEVELS = OpenStruct.new(
+    {
+      EXTENSIVE: 3,
+      CONVINCING: 2,
+      LIMITED: 1,
+      NONE: 0
+    }
+  ).freeze
 end
