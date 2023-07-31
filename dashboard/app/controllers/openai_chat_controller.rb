c81e0ae65d30bf1321c53f5140fe8f5ce6696590
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-07-31 22:25:27 +0000

    remove ip limit - user must be signed in

--- dashboard/app/controllers/openai_chat_controller.rb
+++ dashboard/app/controllers/openai_chat_controller.rb
@@ -5,9 +5,8 @@ class OpenaiChatController < ApplicationController
   authorize_resource class: false
 
   # Allowed number of unique requests per minute before that client is throttled.
-  # These values are fallbacks for DCDO-configured values used below.
+  # Theis value is a fallback for the DCDO-configured value used below.
   REQUEST_LIMIT_PER_MIN_DEFAULT = 100
-  REQUEST_LIMIT_PER_MIN_IP = 1000
 
   # POST /openai/chat_completion
   def chat_completion
@@ -18,12 +17,7 @@ class OpenaiChatController < ApplicationController
     messages = params[:messages]
     id = current_user&.id || session.id
 
-    # Only throttle by IP if neither user nor session ID is available.
-    throttle_ip = id.blank?
-    id ||= request.IP
-    limit = throttle_ip ?
-      DCDO.get('openai_chat_request_limit_per_min_ip', REQUEST_LIMIT_PER_MIN_IP) :
-      DCDO.get('openai_chat_request_limit_per_min_default', REQUEST_LIMIT_PER_MIN_DEFAULT)
+    limit = DCDO.get('openai_chat_request_limit_per_min_default', REQUEST_LIMIT_PER_MIN_DEFAULT)
     period = 60
     OpenaiChatHelper.throttled_request_chat_completion(messages, id, limit, period) do |response|
       chat_completion_return_message = OpenaiChatHelper.get_chat_completion_response_message(response)
@@ -34,7 +28,7 @@ class OpenaiChatController < ApplicationController
     Honeybadger.notify(
       error_class: 'RequestThrottledWarning',
       error_message: "Client throttled for POST #{request.path}",
-      context: {throttle_id: id, is_ip: throttle_ip, limit: limit, period: period}
+      context: {throttle_id: id, limit: limit, period: period}
     )
     head :too_many_requests
   end
