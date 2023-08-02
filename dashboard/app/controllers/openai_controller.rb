cdb9ad9b5f24a82d872de385976440e78046aa99
Emily Eastlake <emily.eastlake@code.org>
2023-08-02 13:26:23 -0700

    remove merge conflict code

--- dashboard/app/controllers/openai_controller.rb
+++ dashboard/app/controllers/openai_controller.rb
@@ -4,6 +4,7 @@ class OpenaiController < ApplicationController
     puts "got here!"
     # Set up the API endpoint URL and request headers
     url = "https://api.openai.com/v1/chat/completions"
+    puts CDO.openai_api_key
     headers = {
       "Content-Type" => "application/json",
       "Authorization" => "Bearer #{CDO.openai_api_key}"
@@ -23,7 +24,11 @@ class OpenaiController < ApplicationController
 
     # Parse the response JSON and return the completed text
     response_body = JSON.parse(response.body)
+    puts "body:"
+    puts response_body
     response = response_body['choices'][0]['message']
+    puts "response:"
+    puts response
     render json: response
   end
 end
