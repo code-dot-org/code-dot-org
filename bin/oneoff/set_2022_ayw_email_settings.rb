81e3a44572ed74e1148ea237d4f177ff19fbb135
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-08-02 14:51:07 -0700

    Fix Violations of and Reenable `Style/RedundantBegin`
    > This cop checks for redundant `begin` blocks.
    
    I used `bundle exec rubocop --autocorrect --only Style/RedundantBegin` to identify and remove the relevant `begin` and `end` statements, then manually fixed the resulting whitespace issues.
    
    - https://rubystyle.guide/#begin-implicit
    - https://docs.rubocop.org/rubocop/cops_style.html#styleredundantbegin
    - https://www.rubydoc.info/gems/rubocop/0.27.0/RuboCop/Cop/Style/RedundantBegin

--- bin/oneoff/set_2022_ayw_email_settings.rb
+++ bin/oneoff/set_2022_ayw_email_settings.rb
@@ -3,12 +3,10 @@
 require_relative '../../dashboard/config/environment'
 
 Pd::Workshop.where("subject LIKE '%Academic Year Workshop%'").select {|w| w.created_at.strftime("%Y") == '2022'}.each do |workshop|
-  begin
-    if workshop.suppress_email?
-      workshop.update!(suppress_email: false)
-      puts "Successfully updated workshop id #{workshop.id}"
-    end
-  rescue => exception
-    puts "Error updating workshop id #{workshop.id}: #{exception}"
+  if workshop.suppress_email?
+    workshop.update!(suppress_email: false)
+    puts "Successfully updated workshop id #{workshop.id}"
   end
+rescue => exception
+  puts "Error updating workshop id #{workshop.id}: #{exception}"
 end
--- lib/cdo/shared_cache.rb
+++ lib/cdo/shared_cache.rb
@@ -34,9 +34,7 @@ module Cdo
     # Generic shared cache.
     # Use memcached if available, with FileStore as fallback.
     def self.cache
-      @@cache ||= begin
-        memcached || ActiveSupport::Cache::FileStore.new(dashboard_dir('tmp', 'cache', 'shared'))
-      end
+      @@cache ||= (memcached || ActiveSupport::Cache::FileStore.new(dashboard_dir('tmp', 'cache', 'shared')))
     end
   end
 end
