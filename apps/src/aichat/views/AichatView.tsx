ddd2b085b7a089b2d72a9fd51f6502377620bc60
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-08-02 22:53:47 +0000

    clean up styling

--- apps/src/aichat/views/AichatView.tsx
+++ apps/src/aichat/views/AichatView.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-// import ChatWorkspace from './ChatWorkspace';
 import moduleStyles from './aichat.module.scss';
-import classNames from 'classnames';
-import {demoChatMessages} from './demoMessages'; // demo chat messages - remove when connected to backend
-import ChatMessage from './ChatMessage';
-import UserChatMessageEditor from './UserChatMessageEditor';
-import PanelContainer from '@cdo/apps/lab2/views/components/PanelContainer';
-import aichatI18n from '../locale';
 import ChatWorkspace from './ChatWorkspace';
 
 const AichatView: React.FunctionComponent = () => {
