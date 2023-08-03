413f3dc52547805e516f3c38326608dd273a44c6
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-08-03 00:01:48 +0000

    remove state for now

--- apps/src/aichat/views/UserChatMessageEditor.tsx
+++ apps/src/aichat/views/UserChatMessageEditor.tsx
@@ -1,17 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Button from '@cdo/apps/templates/Button';
 import moduleStyles from './userChatMessageEditor.module.scss';
 
 const UserChatMessageEditor: React.FunctionComponent = () => {
-  const [userMessage, setUserMessage] = useState<string>('');
-
   return (
     <div className={moduleStyles.UserChatMessageEditor}>
       <textarea
         className={moduleStyles.textArea}
         placeholder="Add a chat message..."
-        onChange={e => setUserMessage(e.target.value)}
-        value={userMessage}
       />
       <Button
         key="submit"
