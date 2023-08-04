15e7ee1fb37e50ab3e30a25d3330932d156873e6
Nick Lathe <nick.lathe@code.org>
2023-08-04 10:35:48 -0700

    Move module and update comments
      * Move module from controller helper to lib
      * Update comments with TEMP to be removed after local deving is done

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -598,6 +598,7 @@ Dashboard::Application.routes.draw do
 
     # LTI API endpoints
     match '/lti/v1/login(/:platform_id)', to: 'lti_v1#login', via: [:get, :post]
+    # TEMP: Remvoe before merging
     post '/lti/v1/authenticate', to: 'lti_v1#authenticate'
 
     # OAuth endpoints
