3d3256821a83a33a8be02bb1083b1f29009823e5
Dayne <dayne@code.org>
2023-08-08 20:11:58 -0700

    Refactor school_owned_types to a constant

--- dashboard/lib/policies/child_account.rb
+++ dashboard/lib/policies/child_account.rb
@@ -37,21 +37,24 @@ class Policies::ChildAccount
     personal_account?(user)
   end
 
+  # Authentication option types which we consider to be "owned" by the school
+  # the student attends because the school has admin control of the account.
+  SCHOOL_OWNED_TYPES = [AuthenticationOption::CLEVER].freeze
+
   # Does the user login using credentials they personally control?
   # For example, some accounts are created and owned by schools (Clever).
   private_class_method def self.personal_account?(user)
     return false if user.sponsored?
     # List of credential types which we believe schools have ownership of.
-    school_owned_types = [AuthenticationOption::CLEVER]
     # Does the user have an authentication method which is not controlled by
     # their school? The presence of at least one authentication method which
     # is owned by the student/parent means this is a "personal account".
     if user.migrated?
       user.authentication_options.any? do |option|
-        school_owned_types.exclude?(option.credential_type)
+        SCHOOL_OWNED_TYPES.exclude?(option.credential_type)
       end
     else
-      school_owned_types.exclude?(user.provider)
+      SCHOOL_OWNED_TYPES.exclude?(user.provider)
     end
   end
 end
