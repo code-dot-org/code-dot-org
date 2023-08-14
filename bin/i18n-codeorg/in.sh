bc4d97b2cf565049b72a04727ed78fdf9ffc9f20
Artem Vavilov <artem.vavilov.7@gmail.com>
2023-08-14 16:59:28 +0300

    P20-343: Modulize Apps i18n resource sync in

--- bin/i18n-codeorg/in.sh
+++ bin/i18n-codeorg/in.sh
@@ -26,18 +26,6 @@ cp_in $orig_dir/restricted.en.yml $loc_dir/restricted.yml
 cp_in $orig_dir/slides.en.yml $loc_dir/slides.yml
 cp_in $orig_dir/unplugged.en.yml $loc_dir/unplugged.yml
 
-### Blockly Core
-
-orig_dir=apps/node_modules/@code-dot-org/blockly/i18n/locales/en-US
-loc_dir=i18n/locales/source/blockly-core
-mkdir -p $loc_dir
-
-# Copy JSON files.
-for file in $(find $orig_dir -name '*.json'); do
-  relname=${file#$orig_dir}
-  cp_in $file $loc_dir$relname
-done
-
 ### Pegasus
 
 orig_dir=pegasus/cache/i18n
--- bin/i18n/sync-in.rb
+++ bin/i18n/sync-in.rb
@@ -11,19 +11,17 @@ module I18n
   module SyncIn
     def self.perform
       puts "Sync in starting"
+      I18n::Resources::Apps.sync_in
       I18n::Resources::Dashboard::CurriculumContent.sync_in
       I18n::Resources::Pegasus::HourOfCode.sync_in
       I18n::Resources::Dashboard::CourseContent.sync_in
       I18n::Resources::Dashboard::Blocks.sync_in
-      I18n::Resources::Apps::Animations.sync_in
       I18n::Resources::Dashboard::SharedFunctions.sync_in
       I18n::Resources::Dashboard::CourseOfferings.sync_in
       I18n::Resources::Dashboard::Standards.sync_in
       I18n::Resources::Dashboard::Docs.sync_in
-      I18n::Resources::Apps::ExternalSources.sync_in
       I18n::Resources::Dashboard::Scripts.sync_in
       I18n::Resources::Dashboard::Courses.sync_in
-      I18n::Resources::Apps::Labs.sync_in
       I18n::Resources::Pegasus::Markdown.sync_in
       puts "Copying source files"
       I18nScriptUtils.run_bash_script "bin/i18n-codeorg/in.sh"
