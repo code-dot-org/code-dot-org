081615f51f5a2eb6b37c9550fcc863a895537382
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-08-14 21:18:43 +0000

    refactor onSubmit function and move to chatApi.ts

--- apps/src/aichat/views/ChatWorkspace.tsx
+++ apps/src/aichat/views/ChatWorkspace.tsx
@@ -5,61 +5,38 @@ import UserChatMessageEditor from './UserChatMessageEditor';
 import moduleStyles from './chatWorkspace.module.scss';
 import aichatI18n from '../locale';
 import {ChatCompletionMessage} from '../types';
-import {Role, Status} from '../constants';
 import {demoChatMessages} from './demoMessages'; // demo chat messages - remove when connected to backend
-import {openaiCompletion} from '../openai';
+import {getChatCompletionMessage} from '../chatApi';
 
 const ChatWorkspace: React.FunctionComponent = () => {
   const [storedMessages, setStoredMessages] =
     useState<ChatCompletionMessage[]>(demoChatMessages);
 
   const onSubmit = async (message: string) => {
-    let lastMessageID =
+    const lastMessageId =
       storedMessages.length === 0
         ? 1
         : storedMessages[storedMessages.length - 1].id;
 
-    const newMessage: ChatCompletionMessage = {
-      id: lastMessageID + 1,
-      name: 'User', // Get name from signed in user.
-      role: Role.USER,
-      status: Status.UNKNOWN,
-      chatMessageText: message,
-    };
-    lastMessageID++;
-
+    // TODO: Filter inappropriate and too personal messages.
+    const appropriateChatMessages = [...storedMessages];
     // Retrieve system prompt from levebuilder - assign for now.
     const systemPrompt =
       'You are a chatbot for a middle school classroom where they can chat with a historical figure. You must answer only questions about the formation of America and the founding fathers. You will act as George Washington; every question you answer must be from his perspective. Wait for the student to ask a question before responding.';
-
-    const systemPromptMessage: ChatCompletionMessage = {
-      id: 0,
-      name: 'System',
-      role: Role.SYSTEM,
-      chatMessageText: systemPrompt,
-      status: Status.OK,
-    };
-
-    const messagesToSend = [systemPromptMessage];
-    // TODO: Filter out messages that are inappropriate or too personal in messagesToSend.
-    storedMessages.forEach(message => messagesToSend.push(message));
-    messagesToSend.push(newMessage);
-    const response = await openaiCompletion(messagesToSend);
-
-    // TODO: Check if response was successful. Update user message status accordingly.
-    newMessage.status = Status.OK;
-    const assistantMessage = response.content;
-
-    // TODO: If user message was inappropriate or too personal, update status.
-    const assistantChatMessage: ChatCompletionMessage = {
-      id: lastMessageID + 1,
-      name: 'HistoryBot',
-      role: Role.ASSISTANT,
-      status: Status.OK,
-      chatMessageText: assistantMessage,
-    };
-
-    setStoredMessages([...storedMessages, newMessage, assistantChatMessage]);
+    const updatedUserAndAssistantChatMessages = await getChatCompletionMessage(
+      systemPrompt,
+      lastMessageId,
+      message,
+      appropriateChatMessages
+    );
+    if (updatedUserAndAssistantChatMessages.length === 2) {
+      // Update assistant chat message with appropriate name.
+      updatedUserAndAssistantChatMessages[1].name = 'EduBot'; // TODO: assign name from levelbuilder settings.
+    }
+    setStoredMessages([
+      ...storedMessages,
+      ...updatedUserAndAssistantChatMessages,
+    ]);
   };
 
   return (
