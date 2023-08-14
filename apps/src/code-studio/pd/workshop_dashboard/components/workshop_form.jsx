05ef182a228fcd7f59f5994c6cb444a78f20caf0
Turner Riley <turner.riley@code.org>
2023-08-14 11:04:11 -0700

    add id to organizer selector

--- apps/src/code-studio/pd/workshop_dashboard/components/workshop_form.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/workshop_form.jsx
@@ -1129,6 +1129,7 @@ export class WorkshopForm extends React.Component {
           )}
           {this.props.permission.has(WorkshopAdmin) && this.props.workshop && (
             <OrganizerFormPart
+              id="organizer-selector"
               workshopId={this.props.workshop.id}
               organizerId={this.state.organizer.id}
               organizerName={this.state.organizer.name}
