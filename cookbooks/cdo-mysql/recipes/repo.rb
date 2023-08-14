bbca716e9b9ce4c7a0dc84506f75b4f8c2603adf
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-08-14 14:01:01 -0700

    hardcode mysql package distribution to bionic

--- cookbooks/cdo-mysql/recipes/repo.rb
+++ cookbooks/cdo-mysql/recipes/repo.rb
@@ -1,7 +1,14 @@
 apt_repository 'mysql' do
   uri 'http://repo.mysql.com/apt/ubuntu'
-  distribution node['lsb']['codename']
+
+  # hardcode the distribution we target to Ubuntu 18 rather than whatever our
+  # current is, since that's the last LTS for which a MySQL 5.7 package was
+  # distributed. Once we update to MySQL 8+, this can be restored.
+  distribution 'bionic'
+  #distribution node['lsb']['codename']
+
   components ['mysql-5.7']
+
   # https://dev.mysql.com/doc/refman/5.7/en/checking-gpg-signature.html
   key '3A79BD29'
   keyserver 'keyserver.ubuntu.com'
