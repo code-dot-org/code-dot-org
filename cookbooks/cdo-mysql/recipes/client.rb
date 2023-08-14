1fc01028495c668b273b7e79d27c2db53d9aead8
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-08-14 14:49:41 -0700

    explicitly target mysql 5.7

--- cookbooks/cdo-mysql/recipes/client.rb
+++ cookbooks/cdo-mysql/recipes/client.rb
@@ -1,4 +1,9 @@
 include_recipe 'cdo-mysql::repo'
 
-apt_package 'libmysqlclient-dev'
-apt_package 'mysql-client'
+apt_package 'libmysqlclient-dev' do
+  version '5.7*'
+end
+
+apt_package 'mysql-client' do
+  version '5.7*'
+end
