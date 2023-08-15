9d664754a21fcdeade06dbbfe096450ef12289b9
Hannah Bergam <hannahbergam@gmail.com>
2023-08-15 13:03:09 -0700

    added unit tests, removed redundant story

---
+++ apps/test/unit/templates/sectionAssessments/FreeResponsesSurveyTableTest.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import {expect} from '../../../util/reconfiguredChai';
+import FreeResponsesSurveyTable from '@cdo/apps/templates/sectionAssessments/FreeResponsesSurveyTable';
+import {
+  surveyOne,
+  surveyTwo,
+} from '@cdo/apps/templates/sectionAssessments/assessmentsTestHelpers';
+
+describe('FreeResponsesAssessmentsTable', () => {
+  it('renders a table', () => {
+    const wrapper = mount(
+      <FreeResponsesSurveyTable freeResponses={surveyTwo} />
+    );
+
+    expect(wrapper.find('table')).to.exist;
+  });
+
+  it('renders the correct number of rows and headers', () => {
+    const wrapper = mount(
+      <FreeResponsesSurveyTable freeResponses={surveyOne} />
+    );
+
+    const tableHeaders = wrapper.find('th');
+    expect(tableHeaders).to.have.length(1);
+
+    const tableRows = wrapper.find('tr');
+    // Depends on length of the fixture - one row per answer, plus a header row.
+    expect(tableRows).to.have.length(surveyOne.length + 1);
+  });
+});
