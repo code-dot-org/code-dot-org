f22487ab3b12944d54ef34b7297c1c354f0910d4
denyslevada <levada.denys@gmail.com>
2023-08-15 16:29:05 +0300

    * adjust styles for ChatWarningModal.tsx * add ChatWarningModal.story.tsx * add $light_gray_300 to colors.scss

--- apps/src/aichat/views/ChatWarningModal.tsx
+++ apps/src/aichat/views/ChatWarningModal.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
+
+import aichatI18n from '@cdo/apps/aichat/locale';
 import moduleStyles from './chatWarningModal.module.scss';
 import {
   BodyTwoText,
   Heading3,
   StrongText,
 } from '@cdo/apps/componentLibrary/typography';
-import aichatI18n from '@cdo/apps/aichat/locale';
 import Button from '@cdo/apps/templates/Button';
 import AccessibleDialog from '@cdo/apps/templates/AccessibleDialog';
 
-interface ChatWarningModalProps {
+export interface ChatWarningModalProps {
   onClose: () => void;
 }
 
 const ChatWarningModal: React.FunctionComponent<ChatWarningModalProps> = ({
   onClose,
 }) => (
-  <AccessibleDialog onClose={onClose}>
+  <AccessibleDialog onClose={onClose} className={moduleStyles.chatWarningModal}>
     <Heading3>{aichatI18n.warningModalHeader()}</Heading3>
+
+    <button
+      type="button"
+      onClick={onClose}
+      className={moduleStyles.xCloseButton}
+    >
+      <i id="x-close" className="fa-solid fa-xmark" />
+    </button>
     <hr />
     <BodyTwoText>
       <StrongText>{aichatI18n.allMessagesSentAreRecorded()}</StrongText>
     </BodyTwoText>
     <BodyTwoText>{aichatI18n.inappropriateMessagesAreFlagged()}</BodyTwoText>
+    <br />
     <BodyTwoText>{aichatI18n.anythingPersonalCanNotBeSubmitted()}</BodyTwoText>
     <a href="#">{aichatI18n.whatIsPersonalInformation()}</a>
     <hr />
-    <div>
+    <div className={moduleStyles.bottomSection}>
       <Button
         onClick={onClose}
         color={Button.ButtonColor.brandSecondaryDefault}
