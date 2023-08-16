a14fd9bf817c1b44791dbed26288569239be9a1c
Seth Nickell <seth.nickell@code.org>
2023-08-15 15:38:39 -1000

    Use metadata.rb rather than explicit Berksfile for packagecloud

--- cookbooks/cdo-github-access/metadata.rb
+++ cookbooks/cdo-github-access/metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'dev@code.org'
 license          'All rights reserved'
 description      'Installs/Configures cdo-github-access'
 long_description File.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.10'
+version          '0.1.11'
+
+depends 'packagecloud'
