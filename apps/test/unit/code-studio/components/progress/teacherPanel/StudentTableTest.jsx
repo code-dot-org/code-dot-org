61eb82a04cf725f735b4936b3830bd0e93dd2681
Ryan Shipp <1382374+rshipp@users.noreply.github.com>
2023-08-16 14:05:03 -0500

    Implement teacher panel sorting (#53296)
    * Implement teacher panel sorting
    
    * Drone?
    
    * Drone?
    
    * Drone?
    
    * Fix/add tests
    
    * Try to fix another test?
    
    * Fix tests more

--- apps/test/unit/code-studio/components/progress/teacherPanel/StudentTableTest.jsx
+++ apps/test/unit/code-studio/components/progress/teacherPanel/StudentTableTest.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from '../../../../../util/reconfiguredChai';
-import StudentTable from '@cdo/apps/code-studio/components/progress/teacherPanel/StudentTable';
+import {UnconnectedStudentTable as StudentTable} from '@cdo/apps/code-studio/components/progress/teacherPanel/StudentTable';
 import {LevelStatus} from '@cdo/apps/util/sharedConstants';
 import i18n from '@cdo/locale';
 import sinon from 'sinon';
 
 const DEFAULT_PROPS = {
   students: [
-    {id: 1, name: 'Student 1'},
-    {id: 2, name: 'Student 2'},
+    {id: 1, name: 'Student 1', familyName: 'FamNameB'},
+    {id: 2, name: 'Student 2', familyName: 'FamNameA'},
   ],
   onSelectUser: () => {},
   getSelectedUserId: () => {},
   levelsWithProgress: [],
   sectionId: 1,
   unitName: 'A Unit',
+  isSortedByFamilyName: false,
 };
 
 const levelsWithProgress = [
@@ -78,5 +79,19 @@ describe('StudentTable', () => {
     firstStudentRow.simulate('click');
 
     expect(onSelectUserStub).to.have.been.calledWith(1);
+  });
+
+  it('sorts by display name by default', () => {
+    const wrapper = setUp();
+
+    const firstStudentRow = wrapper.find('tr').at(1);
+    expect(firstStudentRow.text()).to.match(/^Student 1/);
+  });
+
+  it('sorts by family name if toggled', () => {
+    const wrapper = setUp({isSortedByFamilyName: true});
+
+    const firstStudentRow = wrapper.find('tr').at(1);
+    expect(firstStudentRow.text()).to.match(/^Student 2/);
   });
 });
