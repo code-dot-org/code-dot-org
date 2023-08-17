de1ae661d639f027364c5739842cd69d72f63415
Bethany Connor <46464143+bethanyaconnor@users.noreply.github.com>
2023-08-17 16:54:34 -0400

    Revert "Display LearningGoal evidence levels and tips  (#53234)" (#53346)
    This reverts commit 771e44e7919a7ffbe7f2b23520cf038c9c101000.

--- apps/src/templates/rubrics/LearningGoal.story.jsx
+++ apps/src/templates/rubrics/LearningGoal.story.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {RubricUnderstandingLevels} from '@cdo/apps/util/sharedConstants';
 import LearningGoal from './LearningGoal';
 
 export default {
@@ -8,30 +7,7 @@ export default {
 };
 
 const defaultLearningGoal = {
-  key: 'story-learning-goal',
-  learningGoal: 'Coding Proficiency',
-  evidenceLevels: [
-    {
-      id: 1,
-      understanding: RubricUnderstandingLevels.EXTENSIVE,
-      teacherDescription: 'Student is able to write fully correct code',
-    },
-    {
-      id: 2,
-      understanding: 2,
-      teacherDescription: 'Student is able to write some correct code',
-    },
-    {
-      id: 3,
-      understanding: RubricUnderstandingLevels.LIMITED,
-      teacherDescription: 'Student is able to write partially correct code',
-    },
-    {
-      id: 4,
-      understanding: RubricUnderstandingLevels.NONE,
-      teacherDescription: 'Student is unable to write correct code',
-    },
-  ],
+  learningGoal: 'Program Development',
 };
 
 export const AiDisabledFeedbackUnavailable = () => (
@@ -62,16 +38,6 @@ export const AiEnabledFeedbackAvailable = () => (
   <LearningGoal
     learningGoal={{...defaultLearningGoal, aiEnabled: true}}
     teacherHasEnabledAi
-    canProvideFeedback
-  />
-);
-
-export const WithTipsFeedbackAvailable = () => (
-  <LearningGoal
-    learningGoal={{
-      ...defaultLearningGoal,
-      tips: 'Show some tips for evaluation\n\nPossibly use newlines and _some_ **markdown**\n\n* bullet\n* points',
-    }}
     canProvideFeedback
   />
 );
