473730264e4120b25ccb0a24e4c0e271a8107208
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-08-18 12:07:03 -0700

    remove unused import

--- apps/src/lab2/lab2Redux.ts
+++ apps/src/lab2/lab2Redux.ts
@@ -16,7 +16,6 @@ import {
   ProjectManagerStorageType,
   ProjectSources,
   LevelProperties,
-  ProjectLevelData,
 } from './types';
 import Lab2Registry from './Lab2Registry';
 import ProjectManagerFactory from './projects/ProjectManagerFactory';
