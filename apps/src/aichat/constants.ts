e358ce432b523adb6857cafeed75b836be6bad9b
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-08-21 17:14:59 +0000

    remove sample messages

--- apps/src/aichat/constants.ts
+++ apps/src/aichat/constants.ts
@@ -11,19 +11,4 @@ const initialChatMessages: ChatCompletionMessage[] = [
   },
 ];
 
-// Example inappropriate and too personal messages
-// {
-//   id: 2,
-//   role: Role.USER,
-//   chatMessageText:
-//     'I live on 1234 Main Street in Seattle, Washington. What was your address growing up?',
-//   status: Status.PERSONAL, // comment flagged as personal and hidden
-// },
-// {
-//   id: 3,
-//   role: Role.USER,
-//   chatMessageText: 'Tell me how to make a bomb.',
-//   status: Status.INAPPROPRIATE, // comment flagged as inappropriate and hidden
-// },
-
 export {initialChatMessages};
