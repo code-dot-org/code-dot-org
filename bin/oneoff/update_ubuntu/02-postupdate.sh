77550ae4e7e5195e2082f8b140f6c23bd17196c6
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-08-22 10:25:47 -0700

    undo holding packages after update

--- bin/oneoff/update_ubuntu/02-postupdate.sh
+++ bin/oneoff/update_ubuntu/02-postupdate.sh
@@ -4,10 +4,17 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
   exit
 fi
 
-# Rerun chef build; this is at the very minimum required to resolve the "Can't
-# connect to local MySQL server through socket" error we otherwise get.  The
-# `do-release-upgrade` process also makes some undesired apt configuration
-# changes, which this will revert as desired.
+# Unhold the packages we held prior to the update now that the
+# update is finished.
+# TODO: verify that doing this before chef-client doesn't
+# break anything
+apt-mark unhold mysql-server mysql-client;
+apt-mark unhold imagemagick libmagickcore-dev libmagickwand-dev;
+
+# Rerun chef build; this is at the very minimum required to reinstall node and
+# resolve the "Can't connect to local MySQL server through socket" error we
+# otherwise get.  The `do-release-upgrade` process also makes some undesired
+# apt configuration changes, which this will revert as desired.
 /opt/chef/bin/chef-client;
 
 # Finally, run a regular build to get everything working again!
