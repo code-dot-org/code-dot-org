adee16579c97d8522b2b22e986a00ddb7316d778
Seth Nickell <seth.nickell@code.org>
2023-08-21 15:25:37 -1000

    Support MariaDB by avoiding one use of MySQL operators in pegasus

--- pegasus/migrations/130_add_forms_country_code_index.rb
+++ pegasus/migrations/130_add_forms_country_code_index.rb
@@ -2,7 +2,6 @@
 Sequel.migration do
   up do
     # Generated column feature required for this migration.
-    # JSON ->> operator also required: https://dev.mysql.com/doc/refman/5.7/en/json-search-functions.html#operator_json-inline-path
     unless database_type == :mysql && supports_generated_columns?
       raise "MySQL JSON functions and generated column features required.
 You are currently using #{database_type} version #{server_version}.
@@ -12,7 +11,14 @@ Upgrade your MySQL server to a version which supports these features and retry t
     alter_table(:forms) do
       add_column :location_country_code_s,
         String, size: 2, fixed: true,
-        generated_always_as: Sequel.lit('processed_data->>"$.location_country_code_s"')
+        # According to: https://dev.mysql.com/doc/refman/5.7/en/json-search-functions.html#operator_json-inline-path
+        # processed_data->>"$.location_country_code_s")
+        # is equivalent to
+        # JSON_UNQUOTE(JSON_EXTRACT(processed_data, "$.location_country_code_s"))
+        #
+        # But the latter works on both mysql AND mariadb
+        # mariadb does not plan to support mysql's json operators
+        generated_always_as: Sequel.lit('JSON_UNQUOTE(JSON_EXTRACT(processed_data, "$.location_country_code_s"))')
       add_index [:kind, :location_country_code_s]
     end
   end
