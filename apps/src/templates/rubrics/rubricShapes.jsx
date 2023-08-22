07e46de5394e4abbb3383c533c625501b3a20836
Bethany Connor <46464143+bethanyaconnor@users.noreply.github.com>
2023-08-22 09:41:19 -0400

    Add Amplitude events for opening and closing FAB and LearningGoals (#53329)
    * Add Amplitude events for opening and closing FAB and LearningGoals
    
    * Fix test

--- apps/src/templates/rubrics/rubricShapes.jsx
+++ apps/src/templates/rubrics/rubricShapes.jsx
@@ -16,3 +16,9 @@ export const learningGoalShape = PropTypes.shape({
 export const rubricShape = PropTypes.shape({
   learningGoals: PropTypes.arrayOf(learningGoalShape),
 });
+
+// Used for any data that is only for reporting purposes. Other data may be used for event reporting
+export const reportingDataShape = PropTypes.shape({
+  unitName: PropTypes.string,
+  levelName: PropTypes.string,
+});
