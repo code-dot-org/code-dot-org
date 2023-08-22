fe40f59dd86f9811c30c7363c404d6064000d162
Ubuntu <ubuntu@ip-10-0-0-55.ec2.internal>
2023-08-22 04:42:24 +0000

    remove unnec import

--- apps/src/aichat/views/ChatWorkspace.tsx
+++ apps/src/aichat/views/ChatWorkspace.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, {useState, useCallback} from 'react';
 import {useAppDispatch} from '@cdo/apps/util/reduxHooks';
 import {useSelector} from 'react-redux';
 import ChatWarningModal from '@cdo/apps/aichat/views/ChatWarningModal';
