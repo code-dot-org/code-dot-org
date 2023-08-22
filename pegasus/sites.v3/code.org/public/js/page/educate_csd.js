5b844210742e0c136c90e1678c33c455e7a647b5
Turner Riley <turner.riley@code.org>
2023-08-22 12:35:23 -0700

    fix missing file

--- pegasus/sites.v3/code.org/public/js/page/educate_csd.js
+++ pegasus/sites.v3/code.org/public/js/page/educate_csd.js
@@ -1,27 +1,27 @@
 const windowWidth = window.innerWidth;
 
 function reorganizeAndWrapSlides(carouselId, cardsList) {
-  const carouselContainer = document.getElementById(carouselId);
-  if (!carouselContainer) {
-    console.error(`Carousel container with id "${carouselId}" not found.`);
-    return;
-  }
+    const carouselContainer = document.getElementById(carouselId);
+    if (!carouselContainer) {
+      console.error(`Carousel container with id "${carouselId}" not found.`);
+      return;
+    }
 
-  const slidesContainer = carouselContainer.querySelector('.slides');
-  if (!slidesContainer) {
-    console.error(`Slides container not found inside the carousel.`);
-    return;
-  }
+    const slidesContainer = carouselContainer.querySelector('.slides');
+    if (!slidesContainer) {
+      console.error(`Slides container not found inside the carousel.`);
+      return;
+    }
 
-  const slideItems = Array.from(slidesContainer.querySelectorAll('.slide'));
+    const slideItems = Array.from(slidesContainer.querySelectorAll('.slide'));
 
-  const actionBlocks = Array.from(carouselContainer.querySelectorAll('.action-block.action-block--one-col.flex-space-between'));
-  actionBlocks.forEach(block => block.remove());
+    const actionBlocks = Array.from(carouselContainer.querySelectorAll('.action-block.action-block--one-col.flex-space-between'));
+    actionBlocks.forEach(block => block.remove());
 
-  const slides = [];
-  while (actionBlocks.length > 0) {
-    slides.push(actionBlocks.splice(0, cardsList));
-  }
+    const slides = [];
+    while (actionBlocks.length > 0) {
+      slides.push(actionBlocks.splice(0, cardsList));
+    }
 
   slides.forEach(slide => {
     const slideWrapper = document.createElement('div');
@@ -37,47 +37,47 @@ function reorganizeAndWrapSlides(carouselId, cardsList) {
 }
 
 function handleScreenSize(carouselId) {
-  if (windowWidth > 639 && windowWidth < 799) {
-    reorganizeAndWrapSlides(carouselId, 2);
-    console.log("639 - 799")
+    if (windowWidth > 639 && windowWidth < 799) {
+      reorganizeAndWrapSlides(carouselId, 2);
+      console.log("639 - 799")
 
-  } else if (windowWidth > 799) {
-    reorganizeAndWrapSlides(carouselId, 3);
-    console.log("> 800")
-  }
+    } else if (windowWidth > 799) {
+      reorganizeAndWrapSlides(carouselId, 3);
+      console.log("> 800")
+    }
 }
 
 $(document).ready(function () {
-  let idCollections = '#collections_carousel';
-  handleScreenSize('collections_carousel');
-  $(idCollections + ' .slides').carouFredSel({
-    auto: false,
-    pagination: '#collections_carousel-pagination',
-    prev: '#prev_2',
-    next: '#next_2',
-    responsive: true,
-    width: '100%',
-    height: '490',
-    scroll: 1,
-    swipe: {
-      onTouch: true,
-      onMouse: true
-    }
-  });
-  let idFlexible = '#flexible_units_carousel';
-  handleScreenSize('flexible_units_carousel');
-  $(idFlexible + ' .slides').carouFredSel({
-    auto: false,
-    pagination: '#flexible_units_carousel-pagination',
-    prev: '#prev_1',
-    next: '#next_1',
-    responsive: true,
-    width: '100%',
-    height: '490',
-    scroll: 1,
-    swipe: {
-      onTouch: true,
-      onMouse: true
-    }
-  });
+    let idCollections = '#collections_carousel';
+    handleScreenSize('collections_carousel');
+    $(idCollections + ' .slides').carouFredSel({
+      auto: false,
+      pagination: '#collections_carousel-pagination',
+      prev: '#prev_2',
+      next: '#next_2',
+      responsive: true,
+      width: '100%',
+      height: '490',
+      scroll: 1,
+      swipe: {
+        onTouch: true,
+        onMouse: true
+      }
+    });
+    let idFlexible = '#flexible_units_carousel';
+    handleScreenSize('flexible_units_carousel');
+    $(idFlexible + ' .slides').carouFredSel({
+      auto: false,
+      pagination: '#flexible_units_carousel-pagination',
+      prev: '#prev_1',
+      next: '#next_1',
+      responsive: true,
+      width: '100%',
+      height: '490',
+      scroll: 1,
+      swipe: {
+        onTouch: true,
+        onMouse: true
+      }
+    });
 });
