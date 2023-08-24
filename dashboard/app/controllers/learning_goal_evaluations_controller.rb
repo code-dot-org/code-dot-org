27b5a88d7c84e3cebe2a8b55b5d1bfbe3fd6c11c
Mark Barnes <mark.barnes@code.org>
2023-08-24 03:07:08 -0500

    updated controller and created unit test

--- dashboard/app/controllers/learning_goal_evaluations_controller.rb
+++ dashboard/app/controllers/learning_goal_evaluations_controller.rb
@@ -1,9 +1,15 @@
 class LearningGoalEvaluationsController < ApplicationController
   def create
-    @learning_goal_evaluation = LearningGoalEvaluation.new(learning_goal_evaluation_params)
+    learning_goal_evaluation = LearningGoalEvaluation.new(learning_goal_evaluation_params)
+
+    if learning_goal_evaluation.save
+      render json: learning_goal_evaluation
+    else
+      render status: :bad_request, json: {error: learning_goal_evaluation.errors.full_message.to_json}
+    end
   end
 
   def learning_goal_evaluation_params
-    params.require(:user_id, :level_id).permit(:teacher_id, :unit_id, :learning_goal_id, :understanding, :feedback)
+    params.transform_keys(&:underscore).require(:user_id, :level_id, :unit_id, :learning_goal_id).permit(:teacher_id, :understanding, :feedback, :context)
   end
 end
---
+++ dashboard/test/controllers/learning_goal_evaluations_controller_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class LearningGoalEvaluationControllerTest < ActionController::TestCase
+  test 'create learning goal evaluation' do
+    learning_goal = create :learning_goal
+    user_id = 1
+    level_id = 2
+    unit_id = 3
+    understanding = 5
+    feedback = 'abc'
+    context = 'def'
+
+    post :create, params: {
+      userId: user_id,
+      levelId: level_id,
+      unitId: unit_id,
+      learningGoalId: learning_goal.id,
+      understanding: understanding,
+      feedback: feedback,
+      context: context
+    }
+    assert_response :success
+
+    response_json = JSON.parse(response.body)
+    assert_not_nil response_json['id']
+    assert_equal user_id, response_json['user_id']
+    assert_equal level_id, response_json['level_id']
+    assert_equal unit_id, response_json['unit_id']
+    assert_equal learning_goal.id, response_json['learning_goal_id']
+    assert_equal understanding, response_json['understanding']
+    assert_equal feedback, response_json['feedback']
+    assert_equal context, response_json['context']
+    assert_not_nil response_json['created_at']
+  end
+end
