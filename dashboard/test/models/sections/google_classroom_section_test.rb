da891721d0f86a0b22d9b9aa0ec4fd68cc877c1f
Eric Aderhold <eric.aderhold@fullharvest.com>
2023-08-24 13:19:21 -0700

    Stub 18n flag

--- dashboard/test/models/sections/google_classroom_section_test.rb
+++ dashboard/test/models/sections/google_classroom_section_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class GoogleClassroomSectionTest < ActiveSupport::TestCase
   test 'from google classroom service without family name import' do
     DCDO.stubs(:get).with('google_classroom_family_name', false).returns(false)
-    owner = create :teacher
+    DCDO.stubs(:get).with(I18nStringUrlTracker::I18N_STRING_TRACKING_DCDO_KEY, false).returns(false)    owner = create :teacher
     student_list = Google::Apis::ClassroomV1::ListStudentsResponse.from_json(
       {
         students: (1..50).map do |i|
@@ -43,7 +43,7 @@ class GoogleClassroomSectionTest < ActiveSupport::TestCase
 
   test 'from google classroom service with family name import' do
     DCDO.stubs(:get).with('google_classroom_family_name', false).returns(true)
-    owner = create :teacher
+    DCDO.stubs(:get).with(I18nStringUrlTracker::I18N_STRING_TRACKING_DCDO_KEY, false).returns(false)    owner = create :teacher
     student_list = Google::Apis::ClassroomV1::ListStudentsResponse.from_json(
       {
         students: (1..50).map do |i|
