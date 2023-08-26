743caba8f455538b9b337e929bc77a996aa55b96
Mark Barnes <mark.barnes@code.org>
2023-08-26 00:10:01 -0500

    additional bug fix for update method

--- dashboard/app/controllers/learning_goal_evaluations_controller.rb
+++ dashboard/app/controllers/learning_goal_evaluations_controller.rb
@@ -14,12 +14,14 @@ class LearningGoalEvaluationsController < ApplicationController
     params.require(:id)
     learning_goal_evaluation = LearningGoalEvaluation.find_by_id(params[:id])
 
-    if learning_goal_evaluation &&
-      learning_goal_evaluation.teacher_id == current_user.id &&
-      learning_goal_evaluation.update(learning_goal_evaluation_params)
-      render json: learning_goal_evaluation
+    if learning_goal_evaluation&.teacher_id == current_user.id
+      if learning_goal_evaluation.update(learning_goal_evaluation_params)
+        render json: learning_goal_evaluation
+      else
+        head :bad_request
+      end
     else
-      render status: :not_found
+      head :not_found
     end
   end
 
