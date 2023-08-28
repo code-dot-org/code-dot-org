c93e6657a9d3facc90f70fd0aad7ce6c7ba46477
Darin Webb <darin@code.org>
2023-08-28 09:47:34 -0500

    rename custom resource function

--- lib/cdo/cloud_formation/lambda.rb
+++ lib/cdo/cloud_formation/lambda.rb
@@ -80,7 +80,7 @@ module Cdo::CloudFormation
 
     # Helper function to call a Lambda-function-based AWS::CloudFormation::CustomResource.
     # Ref: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-cfn-customresource.html
-    def lambda_fn(function_name, properties={})
+    def lambda_custom_resource(function_name, properties={})
       custom_type = properties.delete(:CustomType)
       depends_on = properties.delete(:DependsOn)
       custom_resource = {
