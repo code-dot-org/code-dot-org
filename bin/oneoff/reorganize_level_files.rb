4681818f4dba90d9f15acf432705f798f0ba5d9d
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-08-30 16:55:04 -0700

    clean up comment

--- bin/oneoff/reorganize_level_files.rb
+++ bin/oneoff/reorganize_level_files.rb
@@ -5,20 +5,19 @@ abort 'Script already running' unless only_one_running?(__FILE__)
 
 require_relative '../../dashboard/config/environment'
 
-# We currently have ~923 Units and ~31592 un-reorganized level files; we want
-# to batch this update a bit so GitHub doesn't have to deal with 30k files in a
-# single PR. Artibtrarily batch by 200 Units worth of Levels at a time.
-
+# We currently have ~31592 un-reorganized level files; we want to batch this
+# update a bit so GitHub doesn't have to deal with 30k files in a single PR.
+#
 # GitHub doesn't like a large number of single files in a single directory
 # (which is why we're doing this work in the first place), but also doesn't
-# like too many files in a single PR.
+# like too many files in a single PR. By default, GitHub will only load the
+# first 3000 changed files in a PR, so we want to limit the number of files we
+# reorganize at once to be under that limit so the PRs in which these changes
+# happen are still at least somewhat viewable.
 #
-# By default, GitHub will only load the first 3000 changed files in a PR, so we
-# want to limit the number of files we reorganize at once to be under that
-# limit so the PRs in which these changes happen are still at least somewhat
-# viewable. We plan to execute this script on the levelbuilder machine and use
-# the regularly scheduled staging scoop PRs, specifically, so we want to leave
-# a little headroom for our regular levelbuilder changes as well. Therefore, we
+# We plan to execute this script on the levelbuilder machine and capture the
+# change with the regularly scheduled staging scoop PRs, so we want to leave a
+# little headroom for our regular levelbuilder changes as well. Therefore, we
 # limit this script to updating 2500 files per invocation.
 files_updated = 0
 file_update_limit = 2500
