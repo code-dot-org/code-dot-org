283021c9d651b29f12a3cb26f90d8a156d182ae6
denyslevada <levada.denys@gmail.com>
2023-08-30 17:47:37 +0300

    * added test for disabled checkbox * added @testing-library/user-event for better event handling tests

--- apps/test/unit/componentLibrary/CheckboxTest.jsx
+++ apps/test/unit/componentLibrary/CheckboxTest.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {render, screen, fireEvent} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import {expect} from '../../util/reconfiguredChai';
 
@@ -19,7 +20,9 @@ describe('Design System - Checkbox', () => {
     expect(screen.getByText('Checkbox label')).to.exist;
   });
 
-  it('Checkbox - changes checked state on click', () => {
+  it('Checkbox - changes checked state on click', async () => {
+    const user = userEvent.setup();
+
     render(
       <Checkbox
         name="test-checkbox"
@@ -32,14 +35,23 @@ describe('Design System - Checkbox', () => {
     expect(checkbox).to.exist;
 
     expect(checkbox.checked).to.be.false;
+    expect(checkbox.disabled).to.be.false;
+    expect(checkbox.indeterminate).to.be.false;
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(checkbox.checked).to.be.true;
-    fireEvent.click(checkbox);
+    expect(checkbox.disabled).to.be.false;
+    expect(checkbox.indeterminate).to.be.false;
+
+    await user.click(checkbox);
     expect(checkbox.checked).to.be.false;
+    expect(checkbox.disabled).to.be.false;
+    expect(checkbox.indeterminate).to.be.false;
   });
 
-  it('Checkbox - renders indeterminate checkbox, changes on click', () => {
+  it('Checkbox - renders indeterminate checkbox, changes on click', async () => {
+    const user = userEvent.setup();
+
     render(
       <Checkbox
         name="test-checkbox"
@@ -51,14 +63,49 @@ describe('Design System - Checkbox', () => {
 
     const checkbox = screen.getByDisplayValue('test-checkbox');
     expect(checkbox).to.exist;
+
     expect(checkbox.checked).to.be.false;
+    expect(checkbox.disabled).to.be.false;
     expect(checkbox.indeterminate).to.be.true;
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(checkbox.checked).to.be.true;
+    expect(checkbox.disabled).to.be.false;
     expect(checkbox.indeterminate).to.be.false;
-    fireEvent.click(checkbox);
+
+    await user.click(checkbox);
     expect(checkbox.checked).to.be.false;
+    expect(checkbox.disabled).to.be.false;
+    expect(checkbox.indeterminate).to.be.false;
+  });
+
+  it("Checkbox - renders disabled checkbox, doesn't change on click", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Checkbox
+        name="test-checkbox"
+        value="test-checkbox"
+        label="Checkbox label"
+        disabled={true}
+      />
+    );
+
+    const checkbox = screen.getByDisplayValue('test-checkbox');
+    expect(checkbox).to.exist;
+
+    expect(checkbox.checked).to.be.false;
+    expect(checkbox.disabled).to.be.true;
+    expect(checkbox.indeterminate).to.be.false;
+
+    await user.click(checkbox);
+    expect(checkbox.checked).to.be.false;
+    expect(checkbox.disabled).to.be.true;
+    expect(checkbox.indeterminate).to.be.false;
+
+    await user.click(checkbox);
+    expect(checkbox.checked).to.be.false;
+    expect(checkbox.disabled).to.be.true;
     expect(checkbox.indeterminate).to.be.false;
   });
 });
