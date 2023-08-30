01094fb9511632541956c34419251ac0900f91b0
Artem Vavilov <artem.vavilov.7@gmail.com>
2023-08-31 00:39:46 +0300

    P20-348: Optimize animations i18n sync-out

--- bin/test/i18n/resources/apps/animation/test_sync_out.rb
+++ bin/test/i18n/resources/apps/animation/test_sync_out.rb
@@ -16,18 +16,20 @@ class I18n::Resources::Apps::Animations::SyncOutTest < Minitest::Test
     PegasusLanguages.stub(:get_crowdin_name_and_locale, [{crowdin_name_s: 'Not English', locale_s: 'not-EN'}]) do
       exec_seq = sequence('execution')
 
-      expected_crowdin_sprintlab_file_path = CDO.dir('i18n/locales/Not English/animations/spritelab_animation_library.json')
-      expected_i18n_sprintlab_file_path = CDO.dir('i18n/locales/not-EN/animations/spritelab_animation_library.json')
+      expected_crowdin_spritelab_file_path = CDO.dir('i18n/locales/Not English/animations/spritelab_animation_library.json')
+      expected_i18n_spritelab_file_path = CDO.dir('i18n/locales/not-EN/animations/spritelab_animation_library.json')
 
-      File.expects(:exist?).with(expected_crowdin_sprintlab_file_path).in_sequence(exec_seq).returns(true)
+      expected_manifest_builder = stub
+      ManifestBuilder.expects(:new).with({spritelab: true, upload_to_s3: true, quiet: true}).in_sequence(exec_seq).returns(expected_manifest_builder)
+      expected_manifest_builder.expects(:initial_animation_metadata).in_sequence(exec_seq)
 
-      FileUtils.expects(:mv).with(expected_crowdin_sprintlab_file_path, expected_i18n_sprintlab_file_path, force: true).in_sequence(exec_seq).returns(true)
+      File.expects(:exist?).with(expected_crowdin_spritelab_file_path).in_sequence(exec_seq).returns(true)
+
+      FileUtils.expects(:mv).with(expected_crowdin_spritelab_file_path, expected_i18n_spritelab_file_path, force: true).in_sequence(exec_seq).returns(true)
       FileUtils.expects(:rm_r).with(CDO.dir('i18n/locales/Not English/animations')).in_sequence(exec_seq).returns(true)
 
       I18nScriptUtils.expects(:to_js_locale).with('not-EN').in_sequence(exec_seq).returns('expected_js_locale')
-      JSON.expects(:load_file).with(expected_i18n_sprintlab_file_path).in_sequence(exec_seq).returns('expected_translations')
-      expected_manifest_builder = stub
-      ManifestBuilder.expects(:new).with({spritelab: true, upload_to_s3: true, quiet: true}).once.returns(expected_manifest_builder)
+      JSON.expects(:load_file).with(expected_i18n_spritelab_file_path).in_sequence(exec_seq).returns('expected_translations')
       expected_manifest_builder.expects(:upload_localized_manifest).with('expected_js_locale', 'expected_translations').in_sequence(exec_seq)
 
       I18n::Resources::Apps::Animations::SyncOut.new.execute
@@ -38,40 +40,44 @@ class I18n::Resources::Apps::Animations::SyncOutTest < Minitest::Test
     PegasusLanguages.stub(:get_crowdin_name_and_locale, [{crowdin_name_s: 'English', locale_s: 'en-US'}]) do
       exec_seq = sequence('execution')
 
-      expected_crowdin_sprintlab_file_path = CDO.dir('i18n/locales/English/animations/spritelab_animation_library.json')
-      expected_i18n_sprintlab_file_path = CDO.dir('i18n/locales/en-US/animations/spritelab_animation_library.json')
+      expected_crowdin_spritelab_file_path = CDO.dir('i18n/locales/English/animations/spritelab_animation_library.json')
+      expected_i18n_spritelab_file_path = CDO.dir('i18n/locales/en-US/animations/spritelab_animation_library.json')
 
-      File.expects(:exist?).with(expected_crowdin_sprintlab_file_path).in_sequence(exec_seq).returns(true)
+      expected_manifest_builder = stub
+      ManifestBuilder.expects(:new).with({spritelab: true, upload_to_s3: true, quiet: true}).in_sequence(exec_seq).returns(expected_manifest_builder)
+      expected_manifest_builder.expects(:initial_animation_metadata).in_sequence(exec_seq)
 
-      FileUtils.expects(:mv).with(expected_crowdin_sprintlab_file_path, expected_i18n_sprintlab_file_path, force: true).in_sequence(exec_seq).returns(true)
+      File.expects(:exist?).with(expected_crowdin_spritelab_file_path).in_sequence(exec_seq).returns(true)
+
+      FileUtils.expects(:mv).with(expected_crowdin_spritelab_file_path, expected_i18n_spritelab_file_path, force: true).in_sequence(exec_seq).returns(true)
       FileUtils.expects(:rm_r).with(CDO.dir('i18n/locales/English/animations')).in_sequence(exec_seq).returns(true)
 
       I18nScriptUtils.expects(:to_js_locale).with('en-US').never.returns('expected_js_locale')
-      JSON.expects(:load_file).with(expected_i18n_sprintlab_file_path).never.returns('expected_translations')
-      expected_manifest_builder = stub
-      ManifestBuilder.expects(:new).with({spritelab: true, upload_to_s3: true, quiet: true}).never.returns(expected_manifest_builder)
+      JSON.expects(:load_file).with(expected_i18n_spritelab_file_path).never.returns('expected_translations')
       expected_manifest_builder.expects(:upload_localized_manifest).with('expected_js_locale', 'expected_translations').never
 
       I18n::Resources::Apps::Animations::SyncOut.new.execute
     end
   end
 
-  def test_execution_with_non_existing_crowdin_sprintlab_file
+  def test_execution_with_non_existing_crowdin_spritelab_file
     PegasusLanguages.stub(:get_crowdin_name_and_locale, [{crowdin_name_s: 'Not English', locale_s: 'not-EN'}]) do
       exec_seq = sequence('execution')
 
-      expected_crowdin_sprintlab_file_path = CDO.dir('i18n/locales/Not English/animations/spritelab_animation_library.json')
-      expected_i18n_sprintlab_file_path = CDO.dir('i18n/locales/not-EN/animations/spritelab_animation_library.json')
+      expected_crowdin_spritelab_file_path = CDO.dir('i18n/locales/Not English/animations/spritelab_animation_library.json')
+      expected_i18n_spritelab_file_path = CDO.dir('i18n/locales/not-EN/animations/spritelab_animation_library.json')
 
-      File.expects(:exist?).with(expected_crowdin_sprintlab_file_path).in_sequence(exec_seq).returns(false)
+      expected_manifest_builder = stub
+      ManifestBuilder.expects(:new).with({spritelab: true, upload_to_s3: true, quiet: true}).in_sequence(exec_seq).returns(expected_manifest_builder)
+      expected_manifest_builder.expects(:initial_animation_metadata).in_sequence(exec_seq)
 
-      FileUtils.expects(:mv).with(expected_crowdin_sprintlab_file_path, expected_i18n_sprintlab_file_path, force: true).never.returns(true)
+      File.expects(:exist?).with(expected_crowdin_spritelab_file_path).in_sequence(exec_seq).returns(false)
+
+      FileUtils.expects(:mv).with(expected_crowdin_spritelab_file_path, expected_i18n_spritelab_file_path, force: true).never.returns(true)
       FileUtils.expects(:rm_r).with(CDO.dir('i18n/locales/Not English/animations')).never.returns(true)
 
       I18nScriptUtils.expects(:to_js_locale).with('not-EN').never.returns('expected_js_locale')
-      JSON.expects(:load_file).with(expected_i18n_sprintlab_file_path).never.returns('expected_translations')
-      expected_manifest_builder = stub
-      ManifestBuilder.expects(:new).with({spritelab: true, upload_to_s3: true, quiet: true}).never.returns(expected_manifest_builder)
+      JSON.expects(:load_file).with(expected_i18n_spritelab_file_path).never.returns('expected_translations')
       expected_manifest_builder.expects(:upload_localized_manifest).with('expected_js_locale', 'expected_translations').never
 
       I18n::Resources::Apps::Animations::SyncOut.new.execute
