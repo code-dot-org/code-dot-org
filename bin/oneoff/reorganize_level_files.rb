38301a6aa6a3649cf9415f6d46c7af0706597356
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-08-31 15:36:30 -0700

    update comment and limit to reflect new plan

--- bin/oneoff/reorganize_level_files.rb
+++ bin/oneoff/reorganize_level_files.rb
@@ -15,12 +15,19 @@ require_relative '../../dashboard/config/environment'
 # reorganize at once to be under that limit so the PRs in which these changes
 # happen are still at least somewhat viewable.
 #
-# We plan to execute this script on the levelbuilder machine and capture the
-# change with the regularly scheduled staging scoop PRs, so we want to leave a
-# little headroom for our regular levelbuilder changes as well. Therefore, we
-# limit this script to updating 2500 files per invocation.
+# We plan to execute this script on the levelbuilder machine to minimize the
+# potential for merge conflicts or files being duplicated between directories.
+# To also avoid making the standard levelbuilder content scoop commit
+# overwhelmingly large, we should also plan to run `bin/content-push` before
+# and after this script.
+#
+# Note that because the script takes several minutes to complete, it will still
+# be possible for some regular changes to get mixed in, so we want to leave a
+# little bit of headroom. Therefore, we limit this script to updating 2900
+# files per invocation, which should allow us to complete the reorganization in
+# 11 batches.
 files_updated = 0
-file_update_limit = 2500
+file_update_limit = 2900
 Level.includes(:game).find_each do |level|
   file_moved = Services::LevelFiles.reorganize_level_file_into_subdirectory(level)
   files_updated += 1 if file_moved
