6fb3c764868d685f5e4fe648a9e8037a0e40b5e0
breville <brendan@code.org>
2023-08-31 15:43:48 +0000

    AI Dance: helper initial (WIP)

--- apps/src/templates/CodeWorkspaceContainer.jsx
+++ apps/src/templates/CodeWorkspaceContainer.jsx
@@ -22,6 +22,10 @@ class CodeWorkspaceContainer extends React.Component {
     noVisualization: PropTypes.bool.isRequired,
   };
 
+  state = {
+    showDetails: false,
+  };
+
   /**
    * Called externally
    * @returns {number} The height of the rendered contents in pixels
@@ -34,6 +38,17 @@ class CodeWorkspaceContainer extends React.Component {
     if (this.props.style?.top !== prevProps.style?.top) {
       utils.fireResizeEvent();
     }
+  }
+
+  addAlien() {
+    const w = Blockly.getMainWorkspace();
+    const b = w.newBlock('Dancelab_makeAnonymousDanceSprite');
+    b.initSvg();
+    w.render() ;
+    const p = w.getBlocksByType('Dancelab_whenSetup')
+    const pc = p[0].getChildren()[0].nextConnection;
+    const bc = b.previousConnection;
+    pc.connect(bc);
   }
 
   render() {
@@ -49,6 +64,84 @@ class CodeWorkspaceContainer extends React.Component {
 
     return (
       <div style={mainStyle} className="editor-column">
+        <div
+          id="llm"
+          style={{
+            backgroundColor: 'white',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '90px',
+          }}
+        >
+          <div
+            style={{
+              width: '100%',
+              height: 30,
+              backgroundColor: '#292f36',
+              color: 'white',
+              lineHeight: '33px',
+              paddingLeft: 35,
+            }}
+          >
+            AI
+          </div>
+          <div style={{fontSize: 18}}>
+            {!this.state.showDetails && (
+              <div>
+                <div
+                  style={styles.aiChoice}
+                  onClick={() => this.setState({showDetails: 'dancer'})}
+                >
+                  Create a dancer...
+                </div>
+                <div
+                  style={styles.aiChoice}
+                  onClick={() => this.setState({showDetails: 'background'})}
+                >
+                  Create a background...
+                </div>
+                <div style={styles.aiChoice}>Create an effect...</div>
+              </div>
+            )}
+
+            {this.state.showDetails === 'dancer' && (
+              <div>
+                <div style={{...styles.aiChoice, ...styles.aiSelectedChoice}}>
+                  Create a dancer who looks like a
+                </div>
+                <div
+                  style={styles.aiChoice}
+                  onClick={() => {
+                    this.setState({showDetails: false});
+                    this.addAlien();
+                  }}
+                >
+                  alien
+                </div>
+                <div style={styles.aiChoice}>robot</div>
+                <div style={styles.aiChoice}>frog</div>
+              </div>
+            )}
+
+            {this.state.showDetails === 'background' && (
+              <div>
+                <div style={{...styles.aiChoice, ...styles.aiSelectedChoice}}>
+                  Create a background that looks like a
+                </div>
+                <div
+                  style={styles.aiChoice}
+                  onClick={() => this.setState({showDetails: false})}
+                >
+                  kaleidoscope
+                </div>
+                <div style={styles.aiChoice}>rainbow</div>
+                <div style={styles.aiChoice}>laser</div>
+              </div>
+            )}
+          </div>
+        </div>
         <div id="codeWorkspace" style={styles.codeWorkspace}>
           {children}
         </div>
@@ -87,17 +180,20 @@ const styles = {
     marginRight: 15,
   },
   codeWorkspace: {
+    opacity: 1,
     position: 'absolute',
     left: 0,
     right: 0,
     bottom: 0,
-    top: 0,
+    top: 90,
     borderBottomStyle: 'none',
     borderRightStyle: 'none',
     borderLeftStyle: 'none',
     borderTopWidth: 1,
     borderTopStyle: 'solid',
     borderTopColor: '#ddd',
+    //pointerEvents: 'none',
+    height: 'calc(100% - 90px)',
   },
   noVisualization: {
     // Overrides left set in css
@@ -106,5 +202,19 @@ const styles = {
   },
   noVisualizationRtl: {
     right: 0,
+  },
+  aiChoice: {
+    backgroundColor: '#5b8df2',
+    padding: 5,
+    borderRadius: 5,
+    padding: '10px 25px',
+    color: 'white',
+    margin: 8,
+    display: 'inline-block',
+    cursor: 'pointer',
+  },
+  aiSelectedChoice: {
+    //color: 'black',
+    backgroundColor: 'rgb(67 105 182)',
   },
 };
