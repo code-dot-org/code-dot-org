caf6deec036d1a352b26090d4c6c02640561152d
dju90 <bryan@code.org>
2023-08-31 11:35:35 -0700

    update test

--- apps/test/unit/templates/teacherDashboard/StatsTableTest.js
+++ apps/test/unit/templates/teacherDashboard/StatsTableTest.js
@@ -4,9 +4,9 @@ import {expect} from '../../../util/reconfiguredChai';
 import {UnconnectedStatsTable as StatsTable} from '@cdo/apps/templates/teacherDashboard/StatsTable';
 
 const students = [
-  {id: 3, name: 'Student C'},
-  {id: 2, name: 'Student B'},
-  {id: 1, name: 'Student A'},
+  {id: 3, name: 'Student C', familyName: 'Lastname A'},
+  {id: 2, name: 'Student B', familyName: 'Lastname B'},
+  {id: 1, name: 'Student A', familyName: 'Lastname C'},
 ];
 const studentsCompletedLevelCount = {
   1: 15,
@@ -40,7 +40,7 @@ describe('StatsTable', () => {
     expect(studentRows).to.have.length(3);
   });
 
-  it('sorts students by name upon clicking student name header cell', () => {
+  it('sorts students by the correct name upon clicking the name header cells', () => {
     const wrapper = mount(
       <StatsTable
         sectionId={1}
@@ -50,17 +50,31 @@ describe('StatsTable', () => {
     );
 
     // first click should sort students A-Z
-    wrapper.find('.uitest-name-header').simulate('click');
-    let nameCells = wrapper.find('.uitest-name-cell');
+    wrapper.find('.uitest-display-name-header').simulate('click');
+    let nameCells = wrapper.find('.uitest-display-name-cell');
     expect(nameCells.at(0).text()).to.equal('Student A');
     expect(nameCells.at(1).text()).to.equal('Student B');
     expect(nameCells.at(2).text()).to.equal('Student C');
 
     // second click should sort students Z-A
-    wrapper.find('.uitest-name-header').simulate('click');
-    nameCells = wrapper.find('.uitest-name-cell');
+    wrapper.find('.uitest-display-name-header').simulate('click');
+    nameCells = wrapper.find('.uitest-display-name-cell');
     expect(nameCells.at(0).text()).to.equal('Student C');
     expect(nameCells.at(1).text()).to.equal('Student B');
     expect(nameCells.at(2).text()).to.equal('Student A');
+
+    // first click should sort students by family name A-Z
+    wrapper.find('.uitest-family-name-header').simulate('click');
+    nameCells = wrapper.find('.uitest-family-name-cell');
+    expect(nameCells.at(0).text()).to.equal('Lastname A');
+    expect(nameCells.at(1).text()).to.equal('Lastname B');
+    expect(nameCells.at(2).text()).to.equal('Lastname C');
+
+    // second click should sort students Z-A
+    wrapper.find('.uitest-family-name-header').simulate('click');
+    nameCells = wrapper.find('.uitest-family-name-cell');
+    expect(nameCells.at(0).text()).to.equal('Lastname C');
+    expect(nameCells.at(1).text()).to.equal('Lastname B');
+    expect(nameCells.at(2).text()).to.equal('Lastname A');
   });
 });
