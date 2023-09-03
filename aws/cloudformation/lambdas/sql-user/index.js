a7111d98c570010957ec6d155f803f85a8b46862
Suresh Chanmugam <suresh@code.org>
2023-09-03 13:05:20 -0400

    Implementing changes suggested in Pull Request.

--- aws/cloudformation/lambdas/sql-user/index.js
+++ aws/cloudformation/lambdas/sql-user/index.js
@@ -1,13 +1,23 @@
 const mysql = require("mysql");
 const { sendCfnResponse } = require("./common/cfn-response");
 
+const queryPromise = (connection, query) => {
+  return new Promise((resolve, reject) => {
+    connection.query(query, (err, results) => {
+      if (err) return reject(err);
+      resolve(results);
+    });
+  });
+};
+
 exports.handler = async (event, context) => {
   // TODO: Mask password fields.
   console.log("REQUEST RECEIVED:\n", JSON.stringify(event));
-
   const props = event.ResourceProperties;
-  
+
   let physicalResourceId = event.PhysicalResourceId || props.Name;
+
+  var results;
 
   const connection = mysql.createConnection({
     host: props.DBServerHost,
@@ -20,25 +30,23 @@ exports.handler = async (event, context) => {
     switch (event.RequestType) {
       case "Create":
       case "Update":
-        await createOrUpdateSQLUser(connection, {
-          name,
-          clientHost,
-          password,
-          databases,
-          privileges,
-        })
-          .then((results) => {
-            console.log(results.createUser); // Results from the CREATE USER query.
-            console.log(results.updateUser); // Results from the UPDATE USER query.
-            console.log(results.grantResults); // Array of results from each GRANT operation.
-          })
-          .catch((error) => {
-            console.error("Error creating/updating SQL user:", error);
-            // TODO: Throw the error again here?
-          });
+        results = await createOrUpdateSQLUser(connection, {
+          name: props.Name,
+          clientHost: props.ClientHost,
+          password: props.Password,
+          databases: props.Databases,
+          privileges: props.Privileges,
+        });
+        console.log(results.createUser); // Results from the CREATE USER query.
+        console.log(results.updateUser); // Results from the UPDATE USER query.
+        console.log(results.grantResults); // Array of results from each GRANT operation.
         break;
       case "Delete":
-        await deleteSQLUser(connection, { name, clientHost });
+        results = await deleteSQLUser(connection, {
+          name: props.Name,
+          clientHost: props.ClientHost,
+        });
+        console.log(results);
         break;
       default:
         throw new Error("Unsupported Resource Event type.");
@@ -46,6 +54,7 @@ exports.handler = async (event, context) => {
 
     await sendCfnResponse(event, context, "SUCCESS", {}, physicalResourceId);
   } catch (error) {
+    console.error("Error:", error);
     await sendCfnResponse(
       event,
       context,
@@ -59,65 +68,43 @@ exports.handler = async (event, context) => {
   }
 };
 
-const createOrUpdateSQLUser = (
+const createOrUpdateSQLUser = async (
   connection,
   { name, clientHost, password, databases, privileges }
 ) => {
-  return new Promise((resolve, reject) => {
-    const createUser = `
-            CREATE USER IF NOT EXISTS '${name}'@'${clientHost}'
-            IDENTIFIED WITH mysql_native_password
-            BY ${connection.escape(password)};
+  const createUser = `
+        CREATE USER IF NOT EXISTS '${name}'@'${clientHost}'
+        IDENTIFIED WITH mysql_native_password
+        BY ${connection.escape(password)};
+    `;
+  const createResults = await queryPromise(connection, createUser);
+
+  const updateUser = `
+        ALTER USER '${name}'@'${clientHost}'
+        IDENTIFIED WITH mysql_native_password
+        BY ${connection.escape(password)};
+    `;
+  const updateResults = await queryPromise(connection, updateUser);
+
+  const grantPromises = databases.map((database) => {
+    const grantPrivileges = `
+            GRANT ${privileges.join(",")}
+            ON ${database}.*
+            TO '${name}'@'${clientHost}';
         `;
-
-    connection.query(createUser, (err, createResults) => {
-      if (err) return reject(err);
-
-      const updateUser = `
-                ALTER USER '${name}'@'${clientHost}'
-                IDENTIFIED WITH mysql_native_password
-                BY ${connection.escape(password)};
-            `;
-
-      connection.query(updateUser, (err, updateResults) => {
-        if (err) return reject(err);
-
-        const promises = databases.map((database) => {
-          return new Promise((resolve, reject) => {
-            const grantPrivileges = `
-                            GRANT ${privileges.join(",")}
-                            ON ${database}.*
-                            TO '${name}'@'${clientHost}';
-                        `;
-
-            connection.query(grantPrivileges, (err, grantResults) => {
-              if (err) return reject(err);
-              resolve(grantResults);
-            });
-          });
-        });
-
-        Promise.all(promises)
-          .then((grantResultsArray) => {
-            resolve({
-              createUser: createResults,
-              updateUser: updateResults,
-              grantResults: grantResultsArray,
-            });
-          })
-          .catch(reject);
-      });
-    });
+    return queryPromise(connection, grantPrivileges);
   });
+
+  const grantResultsArray = await Promise.all(grantPromises);
+
+  return {
+    createUser: createResults,
+    updateUser: updateResults,
+    grantResults: grantResultsArray,
+  };
 };
 
-const deleteSQLUser = (connection, { name, clientHost }) => {
-  return new Promise((resolve, reject) => {
-    const dropUser = `DROP USER IF EXISTS '${name}'@'${clientHost}';`;
-
-    connection.query(dropUser, (err) => {
-      if (err) return reject(err);
-      resolve();
-    });
-  });
+const deleteSQLUser = async (connection, { name, clientHost }) => {
+  const dropUser = `DROP USER IF EXISTS '${name}'@'${clientHost}';`;
+  return await queryPromise(connection, dropUser);
 };
