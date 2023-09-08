97a75f26a68c5d917d3e5e4a9e3692f542d9be74
Mario Gil Correa <mario.gil-correa@code.org>
2023-09-08 12:09:58 -0700

    calling i18n metrics instead of just metrics

--- bin/i18n/sync-in.rb
+++ bin/i18n/sync-in.rb
@@ -16,7 +16,7 @@ module I18n
       I18n::Metrics.report_runtime('Apps', 'in') {I18n::Resources::Apps.sync_in}
       I18n::Metrics.report_runtime('Dashboard', 'in') {I18n::Resources::Dashboard.sync_in}
       I18n::Metrics.report_runtime('Pegasus', 'in') {I18n::Resources::Pegasus.sync_in}
-      Metrics.report_success(true, 'in', 'sync-in')
+      I18n::Metrics.report_success(true, 'in', 'sync-in')
       puts "Sync in completed successfully"
     rescue => exception
       I18n::Metrics.report_success(false, 'in', 'sync-in', "Sync in failed from the error: #{exception}")
