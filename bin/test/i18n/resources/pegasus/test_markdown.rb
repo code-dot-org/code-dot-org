7ad79455a65a53fcb876de8f316f7ee771f9e91b
Artem Vavilov <artem.vavilov.7@gmail.com>
2023-09-12 16:51:45 +0300

    P20-384: Create unit tests for Pegasus markdown i18n sync-out

--- bin/test/i18n/resources/pegasus/test_markdown.rb
+++ bin/test/i18n/resources/pegasus/test_markdown.rb
@@ -1,81 +1,20 @@
 require_relative '../../../test_helper'
 require_relative '../../../../i18n/resources/pegasus/markdown'
 
-class I18n::Resources::Pegasus::MarkdownTest < Minitest::Test
-  class I18n::Resources::Pegasus::Markdown::Documents; end
+describe I18n::Resources::Pegasus::Markdown do
+  describe '.sync_in' do
+    it 'sync-in Markdown resource' do
+      I18n::Resources::Pegasus::Markdown::SyncIn.expects(:perform).once
 
-  def setup
-    I18n::Resources::Pegasus::Markdown::Documents.stubs(new: stub(helpers: stub))
-  end
-
-  def test_sync_in
-    exec_seq = sequence('execution')
-
-    expected_sync_in_file_paths.each do |expected_original_file_path, expected_i18n_source_path|
-      FileUtils.expects(:mkdir_p).with(File.dirname(expected_i18n_source_path)).in_sequence(exec_seq)
-      FileUtils.expects(:cp).with(expected_original_file_path, expected_i18n_source_path).in_sequence(exec_seq)
-
-      I18n::Resources::Pegasus::Markdown::Documents.new.helpers.expects(:parse_yaml_header).with(expected_i18n_source_path).returns(%w[expected_header expected_content expected_line]).in_sequence(exec_seq)
-
-      I18nScriptUtils.expects(:sanitize_markdown_header).with('expected_header').returns('sanitized_header').in_sequence(exec_seq)
-      I18nScriptUtils.expects(:write_markdown_with_header).with('expected_content', 'sanitized_header', expected_i18n_source_path).in_sequence(exec_seq)
+      I18n::Resources::Pegasus::Markdown.sync_in
     end
-
-    I18n::Resources::Pegasus::Markdown.sync_in
   end
 
-  private
+  describe '.sync_out' do
+    it 'sync-out Markdown resource' do
+      I18n::Resources::Pegasus::Markdown::SyncOut.expects(:perform).once
 
-  def expected_sync_in_file_paths
-    [
-      [
-        CDO.dir('pegasus/sites.v3/code.org/public/athome.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/athome.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/public/break.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/break.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/public/coldplay.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/coldplay.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/public/curriculum/unplugged.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/curriculum/unplugged.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/public/educate/curriculum/csf-transition-guide.md'),
-        CDO.dir('i18n/locales/source/markdown/public/educate/curriculum/csf-transition-guide.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/public/educate/it.md'),
-        CDO.dir('i18n/locales/source/markdown/public/educate/it.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/public/helloworld.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/helloworld.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/public/international/about.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/international/about.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/public/poetry.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/poetry.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/views/hoc2022_create_activities.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/views/hoc2022_create_activities.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/views/hoc2022_play_activities.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/views/hoc2022_play_activities.md')
-      ],
-      [
-        CDO.dir('pegasus/sites.v3/code.org/views/hoc2022_explore_activities.md.partial'),
-        CDO.dir('i18n/locales/source/markdown/public/views/hoc2022_explore_activities.md')
-      ],
-    ]
+      I18n::Resources::Pegasus::Markdown.sync_out
+    end
   end
 end
--- bin/test/i18n/resources/test_pegasus.rb
+++ bin/test/i18n/resources/test_pegasus.rb
@@ -10,9 +10,8 @@ describe I18n::Resources::Pegasus do
       I18n::Resources::Pegasus::Markdown.expects(:sync_in).in_sequence(execution_sequence)
 
       # Copying Pegasus source file
-      FileUtils.expects(:mkdir_p).with(CDO.dir('i18n/locales/source/pegasus')).in_sequence(execution_sequence)
       I18nScriptUtils.expects(:fix_yml_file).with(CDO.dir('pegasus/cache/i18n/en-US.yml')).in_sequence(execution_sequence)
-      FileUtils.expects(:cp).with(CDO.dir('pegasus/cache/i18n/en-US.yml'), CDO.dir('i18n/locales/source/pegasus/mobile.yml')).in_sequence(execution_sequence)
+      I18nScriptUtils.expects(:copy_file).with(CDO.dir('pegasus/cache/i18n/en-US.yml'), CDO.dir('i18n/locales/source/pegasus/mobile.yml')).in_sequence(execution_sequence)
 
       I18n::Resources::Pegasus.sync_in
     end
@@ -23,6 +22,7 @@ describe I18n::Resources::Pegasus do
       execution_sequence = sequence('execution')
 
       I18n::Resources::Pegasus::HourOfCode.expects(:sync_out).in_sequence(execution_sequence)
+      I18n::Resources::Pegasus::Markdown.expects(:sync_out).in_sequence(execution_sequence)
 
       I18n::Resources::Pegasus.sync_out
     end
