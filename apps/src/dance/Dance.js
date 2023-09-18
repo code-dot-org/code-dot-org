37102315e81e07feebf19aaf5f2636fa22862893
breville <brendan@code.org>
2023-09-18 17:55:05 +0000

    Alternate calling pattern

--- apps/src/dance/Dance.js
+++ apps/src/dance/Dance.js
@@ -694,7 +694,7 @@ Dance.prototype.captureThumbnailImage = function () {
   }
 };
 
-Dance.prototype.doAi = function (input) {
+Dance.prototype.doAi = async function (input) {
   const url = '/openai/chat_completion';
   const systemPrompt =
     'You are a helper which can accept a request for a mood or atmosphere, and you then generate JSON like the following format: {backgroundColor: "black", backgroundEffect: "splatter", foregroundEffect: "rain"}.  The only valid values for backgroundEffect are circles, color_cycle, diamonds, disco_ball, fireworks, swirl, kaleidoscope, lasers, splatter, rainbow, snowflakes, galaxy, sparkles, spiral, disco, stars.  The only valid values for backgroundColor are rave, cool, electronic, iceCream, neon, tropical, vintage, warm.  The only valid values for foregroundEffect are bubbles, confetti, hearts_red, music_notes, pineapples, pizzas, smiling_poop, rain, floating_rainbows, smile_face, spotlight, color_lights, raining_tacos.  Make sure you always generate all three of those values.  Also, if you receive a request to place a dancer somewhere, then add {setDancer: "true"} to the result JSON.  Also, add a field called "explanation" to the result JSON, which contains a single-sentence explanation of why you chose the values that you did, at the reading level of a 5th-grade school student.';
@@ -710,7 +710,19 @@ Dance.prototype.doAi = function (input) {
     },
   ];
 
-  return HttpClient.post(url, JSON.stringify({messages}), true, {
-    'Content-Type': 'application/json; charset=UTF-8',
-  });
+  const response = await HttpClient.post(
+    url,
+    JSON.stringify({messages}),
+    true,
+    {
+      'Content-Type': 'application/json; charset=UTF-8',
+    }
+  );
+
+  if (response.status === 200) {
+    const res = await response.json();
+    return res.content;
+  } else {
+    return null;
+  }
 };
