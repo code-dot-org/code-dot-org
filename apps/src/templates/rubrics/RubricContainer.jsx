409c927a9f54cf9dd352378da56d2a11cf7db936
Bethany Connor <bethany@code.org>
2023-09-18 14:25:37 -0400

    Add tabs to rubric

--- apps/src/templates/rubrics/RubricContainer.jsx
+++ apps/src/templates/rubrics/RubricContainer.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import style from './rubrics.module.scss';
+import classnames from 'classnames';
 import i18n from '@cdo/locale';
 import {Heading6} from '@cdo/apps/componentLibrary/typography';
 import {
@@ -10,6 +11,11 @@ import {
 } from './rubricShapes';
 import RubricContent from './RubricContent';
 
+const TAB_NAMES = {
+  RUBRIC: 'rubric',
+  SETTINGS: 'settings',
+};
+
 export default function RubricContainer({
   rubric,
   studentLevelInfo,
@@ -17,10 +23,22 @@ export default function RubricContainer({
   currentLevelName,
   reportingData,
 }) {
+  const [selectedTab, setSelectedTab] = useState(TAB_NAMES.RUBRIC);
   return (
     <div className={style.rubricContainer}>
       <div className={style.rubricHeader}>
-        <Heading6>{i18n.rubrics()}</Heading6>
+        <div className={style.rubricHeaderLeftSide}>
+          <HeaderTab
+            text={i18n.rubric()}
+            isSelected={selectedTab === TAB_NAMES.RUBRIC}
+            onClick={() => setSelectedTab(TAB_NAMES.RUBRIC)}
+          />
+          <HeaderTab
+            text={i18n.settings()}
+            isSelected={selectedTab === TAB_NAMES.SETTINGS}
+            onClick={() => setSelectedTab(TAB_NAMES.SETTINGS)}
+          />
+        </div>
       </div>
       <RubricContent
         rubric={rubric}
@@ -39,4 +57,24 @@ RubricContainer.propTypes = {
   studentLevelInfo: studentLevelInfoShape,
   teacherHasEnabledAi: PropTypes.bool,
   currentLevelName: PropTypes.string,
+};
+
+const HeaderTab = ({text, isSelected, onClick}) => {
+  return (
+    <div
+      className={classnames(style.rubricHeaderTab, {
+        [style.selectedTab]: isSelected,
+        [style.unselectedTab]: !isSelected,
+      })}
+      onClick={onClick}
+    >
+      <Heading6>{text}</Heading6>
+    </div>
+  );
+};
+
+HeaderTab.propTypes = {
+  text: PropTypes.string.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
