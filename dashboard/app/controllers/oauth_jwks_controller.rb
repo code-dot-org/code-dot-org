39854842523894d4e72ba0577b96dc351c6423b2
Carl Reiner <carl.reiner@code.org>
2023-09-19 08:46:52 -0700

    remove temp test code

--- dashboard/app/controllers/oauth_jwks_controller.rb
+++ dashboard/app/controllers/oauth_jwks_controller.rb
@@ -1,21 +1,10 @@
 require 'jwt'
 require 'json'
 
-include LtiAccessToken
-
 class OauthJwksController < ApplicationController
   # GET /oauth/jwks
   def jwks
     jwks_data = CDO.jwks_data
     render json: jwks_data
-  end
-
-  # TEMP: Remove before merging.
-  # Use this endpoint to invoke the get_access_token. You can configure your
-  # local LTI Integration client_id
-  def access_token
-    access_token = get_access_token("10000000000003", "https://canvas.instructure.com")
-    p access_token
-    render json: access_token
   end
 end
