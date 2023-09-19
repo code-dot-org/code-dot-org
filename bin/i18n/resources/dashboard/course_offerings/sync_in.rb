1ba055cf0f3bf72522dc56a29e65c619df990b5b
Artem Vavilov <artem.vavilov.7@gmail.com>
2023-09-19 19:34:06 +0300

    P20-396: Implement a base class for I18n sync-in process

--- bin/i18n/resources/dashboard/course_offerings/sync_in.rb
+++ bin/i18n/resources/dashboard/course_offerings/sync_in.rb
@@ -1,34 +1,22 @@
 #!/usr/bin/env ruby
 
-require 'fileutils'
 require 'json'
 
 require_relative '../../../../../dashboard/config/environment'
 require_relative '../../../i18n_script_utils'
+require_relative '../../../utils/sync_in_base'
 require_relative '../course_offerings'
 
 module I18n
   module Resources
     module Dashboard
       module CourseOfferings
-        class SyncIn
-          def self.perform
-            new.execute
-          end
-
-          def execute
-            progress_bar.start
-
+        class SyncIn < I18n::Utils::SyncInBase
+          process do
             I18nScriptUtils.write_file(I18N_SOURCE_FILE_PATH, JSON.pretty_generate(i18n_data))
-
-            progress_bar.finish
           end
 
           private
-
-          def progress_bar
-            @progress_bar ||= I18nScriptUtils.create_progress_bar(title: 'Dashboard/course_offerings sync-in')
-          end
 
           def i18n_data
             CourseOffering.find_each.each_with_object({}) do |course_offering, i18n_data|
