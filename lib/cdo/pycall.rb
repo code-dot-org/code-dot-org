a1e48c18dfa51e594be051013f035c6f0198089c
David Bailey <davidsbailey@users.noreply.github.com>
2023-09-20 15:32:30 -0700

    touch up cdo/pycall

--- lib/cdo/pycall.rb
+++ lib/cdo/pycall.rb
@@ -1,8 +1,14 @@
 # Configures python to work via pycall.
 
+# This file must be loaded before application.rb automatically loads all gems
+# in the Gemfile, for two reasons:
+# 1. python must be configured before pycall is loaded
+# 2. on Mac OS, PyCall.sys.path must be configured after pycall is loaded but
+#    before any other python-related gems are loaded, such as numpy
+
 pybin_path = File.expand_path('../../../pybin', __FILE__)
 pybin_bin_path = "#{pybin_path}/bin"
-ENV['PATH'] = "#{pybin_bin_path}:#{ENV['PATH']}" unless ENV['PATH'].include? pybin_bin_path
+ENV['PATH'] = "#{pybin_bin_path}:#{ENV['PATH']}" unless ENV['PATH'].split(':').include? pybin_bin_path
 ENV['PYTHON'] = "#{pybin_bin_path}/python3.8"
 
 which_python = `which python3.8`.strip
@@ -11,4 +17,6 @@ unless which_python == ENV['PYTHON']
 end
 
 require 'pycall'
+
+# Needed for python3.8 on Mac to find any packages in the virtual environment.
 PyCall.sys.path.append("#{pybin_path}/lib/python3.8/site-packages")
