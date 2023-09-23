5625b82696bc266b7aad3552e0e6097fd40cf427
Seth Nickell <seth.nickell@code.org>
2023-09-23 03:44:31 -1000

    And remove eslint pragma

--- apps/test/util/KARMA_CLI_FLAGS.js
+++ apps/test/util/KARMA_CLI_FLAGS.js
@@ -1,8 +1,6 @@
-/* eslint-disable import/no-commonjs */
 // Passed via karma.conf.js using the client.KARMA_CLI_FLAGS property.
 //
 // For example running `karma start --levelType=maze` will set
 // KARMA_CLI_FLAGS.levelType = 'maze'.
-
 const KARMA_CLI_FLAGS = window.__karma__.config.KARMA_CLI_FLAGS;
 export default KARMA_CLI_FLAGS;
