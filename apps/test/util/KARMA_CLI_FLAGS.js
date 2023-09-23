eb26c353d20f4f3a3cd416fa9cccf80ea0e9c626
Seth Nickell <seth.nickell@code.org>
2023-09-23 03:25:42 -1000

    Use ESM syntax for KARMA_CLI_FLAGS.js

--- apps/test/util/KARMA_CLI_FLAGS.js
+++ apps/test/util/KARMA_CLI_FLAGS.js
@@ -1,7 +1,8 @@
+/* eslint-disable import/no-commonjs */
 // Passed via karma.conf.js using the client.KARMA_CLI_FLAGS property.
 //
 // For example running `karma start --levelType=maze` will set
 // KARMA_CLI_FLAGS.levelType = 'maze'.
 
 const KARMA_CLI_FLAGS = window.__karma__.config.KARMA_CLI_FLAGS;
-module.exports = KARMA_CLI_FLAGS;
+export default KARMA_CLI_FLAGS;
