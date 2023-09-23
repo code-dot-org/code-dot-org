9cac9999b96878db94212d70fcec778269779b78
Seth Nickell <seth.nickell@code.org>
2023-09-23 03:08:50 -1000

    Enable eslint 'plugin:import/errors' and 'plugin:import/typescript'

--- apps/.eslintrc.js
+++ apps/.eslintrc.js
@@ -1,15 +1,32 @@
 // This config defines globals available especially in apps,
 // enables es6, and enables apps-specific plugins and rules.
 // See the root .eslintrc.js for generic eslint linting rules.
+
+// const import_error = 2; // import plugin uses numbers for warning/error
+
 module.exports = {
   parser: '@babel/eslint-parser',
-  plugins: ['cdo-custom-rules', 'react', 'react-hooks', 'mocha', 'babel'],
+  plugins: [
+    'babel',
+    'cdo-custom-rules',
+    'import',
+    'mocha',
+    'react-hooks',
+    'react',
+  ],
   parserOptions: {
     babelOptions: {
       presets: ['@babel/preset-react'],
     },
   },
-  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:import/errors',
+    // TODO: fix over 9000 warnings and enable this
+    // 'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
   env: {
     es6: true,
   },
@@ -62,6 +79,10 @@ module.exports = {
   rules: {
     'babel/semi': 'error', // autofixable
     'cdo-custom-rules/style-blocks-below-class': 'error',
+    // 'import/no-amd': import_error,
+    // 'import/no-commonjs': import_error,
+    // 'import/no-import-module-exports': import_error,
+    // 'import/unambiguous': import_error,
     'mocha/no-exclusive-tests': 'error',
     'react/button-has-type': 'error',
     'react/display-name': 'off',
@@ -85,6 +106,16 @@ module.exports = {
     react: {
       version: 'detect',
     },
+    // Load module import aliases like `@cdo/apps` from webpack config
+    'import/resolver': {
+      typescript: true,
+      webpack: true,
+    },
+    'import/core-modules': [
+      // Ignore `No default export found in imported module "blockly/core"` errors
+      'blockly/core',
+      'blockly/msg/en',
+    ],
   },
   overrides: [
     {
