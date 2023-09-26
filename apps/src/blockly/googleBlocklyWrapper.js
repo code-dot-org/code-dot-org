aad329a59e03ff730f78c805954a8017bb28fcfc
breville <brendan@code.org>
2023-09-26 01:22:40 +0000

    Remove unused code

--- apps/src/blockly/googleBlocklyWrapper.js
+++ apps/src/blockly/googleBlocklyWrapper.js
@@ -19,7 +19,6 @@ import CdoFieldMultilineInput from './addons/cdoFieldMultilineInput';
 import CdoFieldNumber from './addons/cdoFieldNumber';
 import CdoFieldTextInput from './addons/cdoFieldTextInput';
 import CdoFieldVariable from './addons/cdoFieldVariable';
-//import CdoFieldDanceAi from './addons/cdoFieldDanceAi';
 import FunctionEditor from './addons/functionEditor';
 import initializeGenerator from './addons/cdoGenerator';
 import CdoMetricsManager from './addons/cdoMetricsManager';
@@ -200,7 +199,6 @@ function initializeBlocklyWrapper(blocklyInstance) {
   blocklyWrapper.wrapReadOnlyProperty('FieldLabel');
   blocklyWrapper.wrapReadOnlyProperty('FieldParameter');
   blocklyWrapper.wrapReadOnlyProperty('FieldRectangularDropdown');
-  //blocklyWrapper.wrapReadOnlyProperty('FieldDanceAi');
   blocklyWrapper.wrapReadOnlyProperty('fieldRegistry');
   blocklyWrapper.wrapReadOnlyProperty('fish_locale');
   blocklyWrapper.wrapReadOnlyProperty('Flyout');
@@ -262,7 +260,6 @@ function initializeBlocklyWrapper(blocklyInstance) {
     ['field_number', 'FieldNumber', CdoFieldNumber],
     ['field_angle', 'FieldAngle', CdoFieldAngle],
     ['field_multilinetext', 'FieldMultilineInput', CdoFieldMultilineInput],
-    //['field_dance_ai', 'FieldDanceAi', CdoFieldDanceAi],
   ];
   blocklyWrapper.overrideFields(fieldOverrides);
 
@@ -276,7 +273,6 @@ function initializeBlocklyWrapper(blocklyInstance) {
   blocklyWrapper.FieldImageDropdown = CdoFieldImageDropdown;
   blocklyWrapper.FieldToggle = CdoFieldToggle;
   blocklyWrapper.FieldFlyout = CdoFieldFlyout;
-  //blocklyWrapper.FieldDanceAi = CdoFieldDanceAi;
 
   blocklyWrapper.blockly_.registry.register(
     blocklyWrapper.blockly_.registry.Type.FLYOUTS_VERTICAL_TOOLBOX,
