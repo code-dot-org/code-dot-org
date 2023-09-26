e5b83f0c22926f7c32cdccbe67d489f547bbf8f1
Bethany Connor <bethany@code.org>
2023-09-26 11:47:03 -0400

    Clean up action

--- dashboard/app/controllers/learning_goal_ai_evaluations_controller.rb
+++ dashboard/app/controllers/learning_goal_ai_evaluations_controller.rb
@@ -1,15 +1,13 @@
 class LearningGoalAiEvaluationsController < ApplicationController
+  # GET /learning_goal_ai_evaluations/get_evaluation
   def get_evaluation
     student = User.find(learning_goal_ai_evaluation_params[:user_id])
     return head :not_found unless student
     return head :forbidden unless can?(:manage, student)
 
-    learning_goal = LearningGoal.find(learning_goal_ai_evaluation_params[:learning_goal_id])
-    return head :not_found unless learning_goal
-
     evaluation = LearningGoalAiEvaluation.find_by(
       user_id: student.id,
-      learning_goal_id: learning_goal.id
+      learning_goal_id: learning_goal_ai_evaluation_params[:learning_goal_id]
     )
 
     return head :not_found unless evaluation
