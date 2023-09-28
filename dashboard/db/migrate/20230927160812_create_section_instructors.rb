395906625e2615da8f1e76aa9ee38918c4666056
Eric Aderhold <eric.aderhold@fullharvest.com>
2023-09-28 11:49:14 -0700

    s/teacher/instructor/

--- dashboard/db/migrate/20230927160812_create_section_instructors.rb
+++ dashboard/db/migrate/20230927160812_create_section_instructors.rb
@@ -1,13 +1,13 @@
-class CreateSectionTeachers < ActiveRecord::Migration[6.1]
+class CreateSectionInstructors < ActiveRecord::Migration[6.1]
   def change
-    create_table :section_teachers do |t|
-      t.references :teacher, index: true, type: :integer, null: false, foreign_key: {to_table: :users}
+    create_table :section_instructors do |t|
+      t.references :instructor, index: true, type: :integer, null: false, foreign_key: {to_table: :users}
       t.references :section, index: true, type: :integer, null: false
       t.references :invited_by, index: true, type: :integer, null: true, foreign_key: {to_table: :users}
       t.column :deleted_at, :datetime, index: true, null: true
       t.integer :status, null: false
 
-      t.index [:teacher_id, :section_id], unique: true
+      t.index [:instructor_id, :section_id], unique: true
 
       t.timestamps
     end
