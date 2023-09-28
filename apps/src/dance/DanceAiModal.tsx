42806f8e61bb75871a21c0c072bb8cdd67202848
breville <brendan@code.org>
2023-09-28 05:27:10 +0000

    hoc2023: AI dancers

--- apps/src/dance/DanceAiModal.tsx
+++ apps/src/dance/DanceAiModal.tsx
@@ -19,7 +19,7 @@ const promptString = 'Generate a scene using this mood:';
 
 const doAi = async (input: string) => {
   const systemPrompt =
-    'You are a helper which can accept a request for a mood or atmosphere, and you then generate JSON like the following format: {backgroundColor: "black", backgroundEffect: "splatter", foregroundEffect: "rain"}.  The only valid values for backgroundEffect are circles, color_cycle, diamonds, disco_ball, fireworks, swirl, kaleidoscope, lasers, splatter, rainbow, snowflakes, galaxy, sparkles, spiral, disco, stars.  The only valid values for backgroundColor are rave, cool, electronic, iceCream, neon, tropical, vintage, warm.  The only valid values for foregroundEffect are bubbles, confetti, hearts_red, music_notes, pineapples, pizzas, smiling_poop, rain, floating_rainbows, smile_face, spotlight, color_lights, raining_tacos.  Make sure you always generate all three of those values.  Also, add a field called "explanation" to the result JSON, which contains a simple explanation of why you chose the values that you did, at the reading level of a 5th-grade school student, in one sentence of less than forty words.';
+    'You are a helper which can accept a request for a mood or atmosphere, and you then generate JSON like the following format: {backgroundColor: "black", backgroundEffect: "splatter", foregroundEffect: "rain", dancers: {type: "frogs", count: 3, layout: "circle"}}.  The only valid values for backgroundEffect are circles, color_cycle, diamonds, disco_ball, fireworks, swirl, kaleidoscope, lasers, splatter, rainbow, snowflakes, galaxy, sparkles, spiral, disco, stars.  The only valid values for backgroundColor are rave, cool, electronic, iceCream, neon, tropical, vintage, warm.  The only valid values for foregroundEffect are bubbles, confetti, hearts_red, music_notes, pineapples, pizzas, smiling_poop, rain, floating_rainbows, smile_face, spotlight, color_lights, raining_tacos.  The dancers sub-object has three parameters of its own: type must be one of the following: alien, bear, cat, dog, duck, frog, moose, pineapple, robot, shark, sloth, unicorn; count must be between 2 and 40 inclusive; layout must be one of the following: border, inner, diamond, circle, grid, top, row, bottom, left, column, right, x, plus, spiral, random.  Make sure you always generate all four of the top-level values.  Also, add a field called "explanation" to the result JSON, which contains a simple explanation of why you chose the values that you did, at the reading level of a 5th-grade school student, in one sentence of less than forty words.';
 
   const messages = [
     {
@@ -139,7 +139,10 @@ const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
       backgroundEffect,
       backgroundColor,
       foregroundEffect,
-    }) => ({backgroundEffect, backgroundColor, foregroundEffect}))(params);
+      dancers,
+    }) => ({backgroundEffect, backgroundColor, foregroundEffect, dancers}))(
+      params
+    );
 
     const pickedString = JSON.stringify(picked)
       .replace(/":"/g, '": "')
