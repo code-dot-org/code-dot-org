af828e8ec6ed5565ed273ba6d555b09b54515a58
Tyrone Shaffer <tyrone.shaffer@code.org>
2023-10-02 11:13:47 -0400

    Added prototype client-side cached AI support for background color

--- apps/src/dance/DanceAiModal.tsx
+++ apps/src/dance/DanceAiModal.tsx
@@ -11,6 +11,7 @@ import {StrongText, Heading5} from '@cdo/apps/componentLibrary/typography';
 import classNames from 'classnames';
 import FontAwesome from '@cdo/apps/templates/FontAwesome';
 import {AiModalItem, AiModalReturnedItem} from './types';
+import {array} from 'prop-types';
 const Typist = require('react-typist').default;
 
 const aiBotBorder = require('@cdo/static/dance/ai-bot-border.png');
@@ -50,6 +51,55 @@ const doAi = async (input: string) => {
     return null;
   }
 };
+
+// Output: JSON containing {backgroundColor:string, backgroundEffect:string, foregroundEffect:string}
+// ToDo: Break down into helper functions for each step rather than repeat code for each field
+const cachedAi = async (input: string) => {
+  const cachedPalettes: {[key: string]: any} = (
+    await fetch('cdo-ai/hoc2023/cachedpalettemap.json')
+  ).json();
+  const inputWords: string[] = input.split(',');
+  const trimmedInput: string[] = inputWords.map(value => value.trim());
+  const individualPaletteMap: {[key: string]: any} = {};
+
+  // Trim input json for emoji keywords
+  trimmedInput.forEach(value => {
+    if (Object.prototype.hasOwnProperty.call(cachedPalettes, value)) {
+      individualPaletteMap[value] = cachedPalettes[value];
+    }
+  });
+
+  // Sum word-association vectors of all input words
+  const palettesLength = cachedPalettes['palettemap'].length;
+  const paletteAssociation: number[] = new Array(palettesLength).fill(0);
+  for (const key in individualPaletteMap) {
+    if (Object.prototype.hasOwnProperty.call(individualPaletteMap, key)) {
+      const vector: number[] = individualPaletteMap[key]['palettemap'];
+      for (let i = 0; i < vector.length; i++) {
+        paletteAssociation[i] += vector[i];
+      }
+    }
+  }
+
+  // Return max value of map vector as the output parameter e.g. backgroundColor="neon"
+  const finalColor: string =
+    cachedPalettes['palettemap'][
+      paletteAssociation.indexOf(Math.max.apply(null, paletteAssociation))
+    ];
+  const cachedAiResponse = {backgroundColor: finalColor};
+  return JSON.stringify(cachedAiResponse);
+};
+
+interface Item {
+  id: string;
+  name: string;
+}
+
+interface ReturnedItem {
+  id: string;
+  name: string;
+  url: string;
+}
 
 export enum Mode {
   SELECT_INPUTS = 'selectInputs',
