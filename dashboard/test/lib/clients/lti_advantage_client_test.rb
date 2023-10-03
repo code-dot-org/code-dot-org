c9d46d6cee3fb3996d02c4dc1af6d0f082786edb
Carl Reiner <carl.reiner@code.org>
2023-10-02 17:18:55 -0700

    WIP

---
+++ dashboard/test/lib/clients/lti_advantage_client_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+require_relative '../../../lib/clients/lti_advantage_client'
+
+class LtiAdvantageClientTest < ActiveSupport::TestCase
+  test 'throws an error if the API returns a non-200 response' do
+    HTTParty.stubs(:get).returns(OpenStruct.new({code: 400}))
+    LtiAccessToken.stubs(:get_access_token).returns('fake_access_token')
+    assert_raises RuntimeError do
+      LtiAdvantageClient.new('client_id', 'issuer').get_context_membership('https://foolms.com/api/sections/1')
+    end
+  end
+
+  test 'throws an error if no client_id or issuer is provided' do
+    assert_raises ArgumentError do
+      LtiAdvantageClient.new(nil, nil)
+    end
+  end
+end
