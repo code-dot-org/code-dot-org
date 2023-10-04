ece543c0aff8e01389afb569c760472fd831c879
Alice Fisher <alice.fisher@code.org>
2023-10-04 15:24:19 +0000

    just install blocks

--- apps/src/dance/blockly/setup.js
+++ apps/src/dance/blockly/setup.js
@@ -1,6 +1,5 @@
 import danceBlocks from '@cdo/apps/dance/blockly/blocks';
 import * as commonBlocks from '@cdo/apps/blocksCommon';
-import {createJsWrapperBlockCreator} from '@cdo/apps/block_utils';
 /**
  * Set up the global Blockly environment for Dance Party Lab2. This should
  * only be called once per page load, as it configures the global
@@ -18,40 +17,5 @@ export function setUpBlocklyForDanceLab(levelProperties) {
   };
   commonBlocks.install(Blockly, blockInstallOptions);
   blocksModule.install(Blockly, blockInstallOptions);
-  const sharedBlocksConfig = levelProperties.sharedBlocks || [];
-  if (sharedBlocksConfig.length > 0) {
-    installCustomBlocks({
-      blockly: Blockly,
-      customInputTypes: blocksModule.customInputTypes,
-    });
-  }
   Blockly.setInfiniteLoopTrap();
-}
-
-function installCustomBlocks({blockly, customInputTypes}) {
-  console.log('installCustomBlocks');
-  createJsWrapperBlockCreator(
-    blockly,
-    [
-      // Strict Types
-      blockly.BlockValueType.SPRITE,
-      blockly.BlockValueType.BEHAVIOR,
-      blockly.BlockValueType.LOCATION,
-    ],
-    blockly.BlockValueType.SPRITE,
-    customInputTypes
-  );
-  if (
-    blockly.Blocks.gamelab_location_variable_set &&
-    blockly.Blocks.gamelab_location_variable_get
-  ) {
-    Blockly.Variables.registerGetter(
-      Blockly.BlockValueType.LOCATION,
-      'gamelab_location_variable_get'
-    );
-    Blockly.Variables.registerSetter(
-      Blockly.BlockValueType.LOCATION,
-      'gamelab_location_variable_set'
-    );
-  }
 }
