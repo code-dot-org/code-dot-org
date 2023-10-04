08321f51ffd2b572083e688c944fae29dcfc4dce
denyslevada <levada.denys@gmail.com>
2023-10-04 18:31:04 +0300

    * wip

---
+++ apps/src/componentLibrary/chips/index.ts
@@ -0,0 +1,5 @@
+// export {default as Chip} from './Chip';
+// // export type {ChipProps} from './Chip';
+// export {default as ChipsGroup} from './ChipsGroup';
+// // export type {ChipsGroupProps} from './ChipsGroup';
+// export {default as default} from './Chip';
