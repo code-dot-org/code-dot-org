3a256d5fc13addb8e3f1b1eada395a2eaa95966c
Eric Aderhold <eric.aderhold@fullharvest.com>
2023-10-04 11:48:39 -0700

    Implement remaining routes

--- dashboard/app/controllers/api/v1/section_instructors_controller.rb
+++ dashboard/app/controllers/api/v1/section_instructors_controller.rb
@@ -1,6 +1,7 @@
 require 'cdo/firehose'
 
 class Api::V1::SectionInstructorsController < Api::V1::JSONApiController
+  # Returns list of current user's SectionInstructor records
   # GET /section_instructors
   def index
     section_instructors = SectionInstructor.where(instructor_id: current_user&.id)
@@ -8,6 +9,7 @@ class Api::V1::SectionInstructorsController < Api::V1::JSONApiController
     render json: section_instructors, each_serializer: Api::V1::SectionInstructorSerializer
   end
 
+  # Returns SectionInstructor records for the given section
   # GET /section_instructors/:id
   def show
     section = Section.find(params.require(:id))
@@ -17,16 +19,79 @@ class Api::V1::SectionInstructorsController < Api::V1::JSONApiController
     render json: section_instructors, each_serializer: Api::V1::SectionInstructorSerializer
   end
 
-  # TODO: Finish this
+  # Adds a new teacher as an invited instructor
   # POST /section_instructors
   def create
     section = Section.find(params.require(:section_id))
     authorize! :manage, section
 
-    # instructor = User.find(params.require(:email))
+    # Enforce maximum co-instructor count (the limit is 5 plus the main teacher
+    # for a total of 6)
+    return head :bad_request if section.instructors.length >= 6
+
+    instructor = User.find_by(params.require(:email))
+    return head :not_found if instructor.blank?
+    return head :bad_request unless instructor.teacher?
+
+    si = SectionInstructor.with_deleted.find_by(instructor: instructor, section: section)
+
+    # Actually delete the instructor if they were soft-deleted so they can be re-invited.
+    if si.deleted_at.present?
+      si.really_destroy!
+    # Can't re-add someone who is already an instructor (or invited/declined)
+    elsif si.present?
+      return head :bad_request
+    end
+
+    si = SectionInstructor.create(
+      section: section,
+      instructor: instructor,
+      status: :invited,
+      invited_by: current_user
+    )
+
+    render json: si, serializer: Api::V1::SectionInstructorSerializer
   end
 
-  # TODO: Destroy
+  # Removes an instructor from the section (soft-deleting the record).
+  # DELETE /section_instructors/:id
+  def destroy
+    si = SectionInstructor.find(params.require(:id))
+    authorize! :manage, si.section
 
-  # TODO: Accept invitation
+    # Can't remove the section owner
+    return head :forbidden if si.instructor_id == si.section.user_id
+
+    si.status = :removed
+    si.save!
+    si.destroy!
+
+    head :ok
+  end
+
+  # Accept one's own pending co-instructor invitation
+  # PUT /section_instructors/:id/accept
+  def accept
+    si = SectionInstructor.find(params.require(:id))
+    return head :forbidden if si.instructor_id != current_user.id
+    return head :bad_request unless si.status == :invited
+
+    si.status = :active
+    si.save!
+
+    render json: si, serializer: Api::V1::SectionInstructorSerializer
+  end
+
+  # Decline one's own pending co-instructor invitation
+  # PUT /section_instructors/:id/decline
+  def decline
+    si = SectionInstructor.find(params.require(:id))
+    return head :forbidden if si.instructor_id != current_user.id
+    return head :bad_request unless si.status == :invited
+
+    si.status = :declined
+    si.save!
+
+    render json: si, serializer: Api::V1::SectionInstructorSerializer
+  end
 end
