aa0ab99b4e8e0f6d825e8065e4e63025dee897f7
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-10-04 11:13:34 -0700

    minor tweaks & comments

--- lib/dynamic_config/datastore_cache.rb
+++ lib/dynamic_config/datastore_cache.rb
@@ -36,7 +36,13 @@ class DatastoreCache
     end
   end
 
-  # Gets the data associated with a given key
+  # Gets the data associated with a given key.
+  #
+  # Will return the cached data if it hasn't yet expired; if it has, this
+  # method will fetch a fresh value, update the cache, and then return.
+  # Notably, this means this method has inconsistent response time; it's
+  # usually quick, but sometimes has to fetch data from a remote store.
+  #
   # @param key [String]
   # @returns stored value
   def get(key)
@@ -44,12 +50,12 @@ class DatastoreCache
     old_value = @cache[key]
     return old_value if @expirations.key?(key) && @expirations[key] > Time.now
     new_value = @datastore.get(key)
-    set_local(key, new_value)
-    notify_change_listeners if new_value != old_value
+    set(key, new_value)
     return new_value
   end
 
   # Sets the given value for the key in both the local cache and datastore
+  #
   # @param key [String]
   # @param value [JSONable]
   def set(key, value)
@@ -89,7 +95,8 @@ class DatastoreCache
     end
   end
 
-  # Sets the given value for the key in the local cache
+  # Sets the given value for the key in the local cache, with an expiration.
+  #
   # @param key [String]
   # @param value [String]
   private def set_local(key, value)
