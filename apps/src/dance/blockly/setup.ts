bef5da57985d37f4ade13fdd00a780c8f2110681
Alice Fisher <alice.fisher@code.org>
2023-10-04 15:35:33 +0000

    convert to TS - only install, no category blocks

--- apps/src/dance/blockly/setup.ts
+++ apps/src/dance/blockly/setup.ts
@@ -1,11 +1,14 @@
 import danceBlocks from '@cdo/apps/dance/blockly/blocks';
 import * as commonBlocks from '@cdo/apps/blocksCommon';
+import {LevelProperties} from '@cdo/apps/lab2/types';
 /**
  * Set up the global Blockly environment for Dance Party Lab2. This should
  * only be called once per page load, as it configures the global
  * Blockly state.
  */
-export function setUpBlocklyForDanceLab(levelProperties) {
+export function setUpBlocklyForDanceLab(
+  levelProperties: LevelProperties | undefined
+) {
   console.log('setting up dance lab');
   const blocksModule = danceBlocks;
   const skin = levelProperties?.skin || undefined;
