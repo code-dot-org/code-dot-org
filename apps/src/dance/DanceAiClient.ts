77285863b85fce7d7758b8f485adbcdf9de01b4c
Ben Brooks <ben@code.org>
2023-10-05 16:24:20 -0700

    Call cached AI with hard coded emoji list, return palette

--- apps/src/dance/DanceAiClient.ts
+++ apps/src/dance/DanceAiClient.ts
@@ -1,26 +1,50 @@
+import UntypedCachedPalettes from '@cdo/static/dance/ai/model/cachedpalettemap.json';
+import {PaletteMap} from './types';
+
+const CachedPalettes: PaletteMap = UntypedCachedPalettes;
+const palettes: string[] = [
+  'cool',
+  'black and white',
+  'electronic',
+  'ice cream',
+  'light',
+  'neon',
+  'tropical',
+  'vintage',
+  'warm',
+  'autumn',
+  'dawn',
+  'dusk',
+  'grey',
+  'ocean',
+  'rainbow',
+  'roses',
+  'sky',
+  'spring',
+  'winter',
+  'summer',
+];
+
 // Output: JSON containing {backgroundColor:string, backgroundEffect:string, foregroundEffect:string}
 // ToDo: Break down into helper functions for each step rather than repeat code for each field
-const cachedAi = async (input: string) => {
-  const cachedPalettes: {[key: string]: any} = (
-    await fetch('cdo-ai/hoc2023/cachedpalettemap.json')
-  ).json();
+export default function cachedAi(input: string) {
   const inputWords: string[] = input.split(',');
   const trimmedInput: string[] = inputWords.map(value => value.trim());
   const individualPaletteMap: {[key: string]: any} = {};
 
   // Trim input json for emoji keywords
   trimmedInput.forEach(value => {
-    if (Object.prototype.hasOwnProperty.call(cachedPalettes, value)) {
-      individualPaletteMap[value] = cachedPalettes[value];
+    if (Object.prototype.hasOwnProperty.call(CachedPalettes, value)) {
+      individualPaletteMap[value] = CachedPalettes[value];
     }
   });
 
   // Sum word-association vectors of all input words
-  const palettesLength = cachedPalettes['palettemap'].length;
+  const palettesLength = palettes.length;
   const paletteAssociation: number[] = new Array(palettesLength).fill(0);
   for (const key in individualPaletteMap) {
     if (Object.prototype.hasOwnProperty.call(individualPaletteMap, key)) {
-      const vector: number[] = individualPaletteMap[key]['palettemap'];
+      const vector: number[] = individualPaletteMap[key]['association'];
       for (let i = 0; i < vector.length; i++) {
         paletteAssociation[i] += vector[i];
       }
@@ -29,20 +53,9 @@ const cachedAi = async (input: string) => {
 
   // Return max value of map vector as the output parameter e.g. backgroundColor="neon"
   const finalColor: string =
-    cachedPalettes['palettemap'][
+    palettes[
       paletteAssociation.indexOf(Math.max.apply(null, paletteAssociation))
     ];
   const cachedAiResponse = {backgroundColor: finalColor};
   return JSON.stringify(cachedAiResponse);
-};
-
-interface Item {
-  id: string;
-  name: string;
-}
-
-interface ReturnedItem {
-  id: string;
-  name: string;
-  url: string;
 }
