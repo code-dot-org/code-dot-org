4409185a2396d2f5d010ff61ee129f74954f4690
David Bailey <davidsbailey@users.noreply.github.com>
2023-10-05 21:27:14 -0700

    extract EvaluateRubricJobTest setup method

--- dashboard/test/jobs/evaluate_rubric_job_test.rb
+++ dashboard/test/jobs/evaluate_rubric_job_test.rb
@@ -1,23 +1,24 @@
 require "test_helper"
 
 class EvaluateRubricJobTest < ActiveJob::TestCase
+  setup do
+    @student = create :student
+    @script_level = create :script_level
+  end
+
   test "job succeeds on ai-enabled level" do
-    user = create :user
-    script_level = create :script_level
-    EvaluateRubricJob.stubs(:get_lesson_s3_name).with(script_level).returns('fake-lesson-s3-name')
+    EvaluateRubricJob.stubs(:get_lesson_s3_name).with(@script_level).returns('fake-lesson-s3-name')
 
     perform_enqueued_jobs do
-      EvaluateRubricJob.perform_later(user_id: user.id, script_level_id: script_level.id)
+      EvaluateRubricJob.perform_later(user_id: @student.id, script_level_id: @script_level.id)
     end
   end
 
   test "job fails on non-ai level" do
-    user = create :user
-    script_level = create :script_level
-    EvaluateRubricJob.stubs(:get_lesson_s3_name).with(script_level).returns(nil)
+    EvaluateRubricJob.stubs(:get_lesson_s3_name).with(@script_level).returns(nil)
 
     exception = assert_raises RuntimeError do
-      EvaluateRubricJob.new.perform(user_id: user.id, script_level_id: script_level.id)
+      EvaluateRubricJob.new.perform(user_id: @student.id, script_level_id: @script_level.id)
     end
     assert_includes exception.message, 'lesson_s3_name not found'
   end
