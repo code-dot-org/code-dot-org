32de73c00709bde2feadd7987fa0fc5a960cb030
Artem Vavilov <artem.vavilov.7@gmail.com>
2023-10-06 14:05:42 +0300

    P20-398: Fix ScriptConstantsTest

--- dashboard/test/lib/script_constants_test.rb
+++ dashboard/test/lib/script_constants_test.rb
@@ -33,35 +33,15 @@ class ScriptConstantsTest < ActiveSupport::TestCase
     assert_nil ScriptConstants.csf_next_course_recommendation("something-unknown")
   end
 
-  [
-    *CATEGORIES[:csd],
-    *CATEGORIES[:csd_2018],
-    *CATEGORIES[:csd_2019],
-    *CATEGORIES[:csd_2021],
-    *CATEGORIES[:csd_2022],
-    *CATEGORIES[:csd_2023],
-
-    *CATEGORIES[:csf],
-    *CATEGORIES[:csf_2018],
-    *CATEGORIES[:csf_2019],
-    *CATEGORIES[:csf_2020],
-    *CATEGORIES[:csf_2021],
-    *CATEGORIES[:csf_2022],
-    *CATEGORIES[:csf_2023],
-    *CATEGORIES[:csf_international],
-
-    *CATEGORIES[:twenty_hour],
-    *CATEGORIES[:hoc],
-    *ADDITIONAL_I18N_UNITS,
-    *TRANSLATEABLE_CSC_UNITS,
-    JIGSAW_NAME,
-  ].uniq.each do |script|
-    test "ScriptConstants.i18n? returns true when the script is #{script}" do
-      assert ScriptConstants.i18n?(script)
+  test "ScriptConstants.i18n? returns true when the script is translatable" do
+    ScriptConstants.stub_const(:TRANSLATEABLE_UNITS, %w[translatable]) do
+      assert ScriptConstants.i18n?('translatable')
     end
   end
 
   test 'ScriptConstants.i18n? returns false when the script is not translatable' do
-    refute ScriptConstants.i18n?('untranslatable')
+    ScriptConstants.stub_const(:TRANSLATEABLE_UNITS, []) do
+      refute ScriptConstants.i18n?('untranslatable')
+    end
   end
 end
