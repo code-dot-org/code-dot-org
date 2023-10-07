853c05a051f5c748d8318b2e1c388780314ef176
David Bailey <davidsbailey@users.noreply.github.com>
2023-10-06 22:36:24 -0700

    test job fails if rubric not found

--- dashboard/test/jobs/evaluate_rubric_job_test.rb
+++ dashboard/test/jobs/evaluate_rubric_job_test.rb
@@ -68,6 +68,23 @@ class EvaluateRubricJobTest < ActiveJob::TestCase
     assert_equal 0, LearningGoalAiEvaluation.where(user_id: @student.id).count
   end
 
+  test "job fails if rubric not found" do
+    EvaluateRubricJob.stubs(:get_lesson_s3_name).with(@script_level).returns('fake-lesson-s3-name')
+    @rubric.destroy
+
+    # create a project
+    channel_token = ChannelToken.find_or_create_channel_token(@script_level.level, @fake_ip, @storage_id, @script_level.script_id)
+    channel_id = channel_token.channel
+
+    stub_project_source_data(channel_id)
+
+    exception = assert_raises ActiveRecord::RecordNotFound do
+      EvaluateRubricJob.new.perform(user_id: @student.id, script_level_id: @script_level.id)
+    end
+    assert_includes exception.message, "Couldn't find Rubric"
+    assert_equal 0, LearningGoalAiEvaluation.where(user_id: @student.id).count
+  end
+
   # stub out the calls to fetch project data from S3
   private def stub_project_source_data(channel_id, code: 'fake-code', version_id: 'fake-version-id')
     fake_main_json = {source: code}.to_json
