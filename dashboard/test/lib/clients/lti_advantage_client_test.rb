a311845d588028742469d5842ffe237ca58c0664
Carl Reiner <carl.reiner@code.org>
2023-10-09 15:05:38 -0700

    instantiate class to use stub

--- dashboard/test/lib/clients/lti_advantage_client_test.rb
+++ dashboard/test/lib/clients/lti_advantage_client_test.rb
@@ -2,18 +2,17 @@ require 'test_helper'
 require_relative '../../../lib/clients/lti_advantage_client'
 
 class LtiAdvantageClientTest < ActiveSupport::TestCase
-  include LtiAccessToken
+  # include LtiAccessToken
 
   setup do
-    CDO.stubs(:jwk_private_key_data).returns('fake_private_key')
-    CDO.shared_cache.stubs(:read).returns('fake_access_token')
-    LtiAccessToken.stubs(:get_access_token).returns('fake_access_token')
+    @lti_client = LtiAdvantageClient.new('client_id', 'issuer')
+    @lti_client.stubs(:get_access_token).returns('fake_access_token')
   end
 
   test 'throws an error if the API returns a non-200 response' do
     HTTParty.stubs(:get).returns(OpenStruct.new({code: 400}))
     assert_raises RuntimeError do
-      LtiAdvantageClient.new('client_id', 'issuer').get_context_membership('https://foolms.com/api/sections/1')
+      @lti_client.get_context_membership('https://foolms.com/api/sections/1')
     end
   end
 
