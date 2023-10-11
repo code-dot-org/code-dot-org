8537585a2db6b39e3736da260516e898237c6b6f
wilkie <wilkie05@gmail.com>
2023-10-11 14:06:26 -0700

    Adds migration and new schema.

--- dashboard/app/models/learning_goal_ai_evaluation.rb
+++ dashboard/app/models/learning_goal_ai_evaluation.rb
@@ -5,6 +5,7 @@
 #  id               :bigint           not null, primary key
 #  user_id          :integer
 #  learning_goal_id :integer
+#  requester_id     :integer
 #  project_id       :integer
 #  project_version  :string(255)
 #  understanding    :integer
@@ -15,10 +16,12 @@
 #
 #  index_learning_goal_ai_evaluations_on_learning_goal_id  (learning_goal_id)
 #  index_learning_goal_ai_evaluations_on_user_id           (user_id)
+#  index_learning_goal_ai_evaluations_on_requester_id      (requester_id)
 #
 class LearningGoalAiEvaluation < ApplicationRecord
   belongs_to :learning_goal
   belongs_to :user
+  belongs_to :requester, class_name: 'User'
 
   def summarize_for_instructor
     {
