21bc7207bf7769e6b6c7ce10191e8a6cf79e42bd
Alice Fisher <alice.fisher@code.org>
2023-10-12 03:28:16 +0000

    Revert "remove unnec code"
    This reverts commit c1d51e9631ef6cbf394f2567f3ec1fa933ba15a7.

--- apps/src/dance/ai/DanceAiModal.tsx
+++ apps/src/dance/ai/DanceAiModal.tsx
@@ -399,8 +399,8 @@ const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
         >
           {mode === Mode.RESULTS && (
             <Typist
-              startDelay={100}
-              avgTypingDelay={0}
+              startDelay={1500}
+              avgTypingDelay={20}
               cursor={{show: false}}
               onTypingDone={() => {
                 setTypingDone(true);
