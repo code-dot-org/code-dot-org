823ecae38d09cbb779de48a770918b73bf71614a
Alice Fisher <alice.fisher@code.org>
2023-10-13 20:27:47 +0000

    change numWords back to 40

--- apps/src/dance/ai/utils.ts
+++ apps/src/dance/ai/utils.ts
@@ -17,7 +17,7 @@ const doAi = async (input: string) => {
     'bubbles, confetti, hearts_red, music_notes, pineapples, pizzas, smiling_poop, rain, floating_rainbows, smile_face, spotlight, color_lights, raining_tacos';
   const danceExampleJson =
     '{backgroundColor: "roses", backgroundEffect: "starburst", foregroundEffect: "rain"}';
-  const numWords = 20;
+  const numWords = 40;
   const systemPrompt = `You are a helper which can accept a request for a mood or atmosphere, and you then generate JSON like the following format: ${danceExampleJson}.  The only valid values for backgroundEffect are ${allDanceBackgroundEffects}. The only valid values for backgroundColor are ${allDanceBackgroundColors}. The only valid values for foregroundEffect are ${danceForegroundEffects}. Make sure you always generate all three of these values.  Also, add a field called "explanation" to the result JSON, which contains a simple explanation of why you chose the values that you did, at the reading level of a 5th-grade school student, in one sentence of less than ${numWords} words.`;
   const messages = [
     {
