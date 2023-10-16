ff975bbdcc09d2d48617f94ca70e5f2c2f1c76c6
breville <brendan@code.org>
2023-10-16 23:24:25 +0000

    Add comment

--- apps/src/blockly/addons/cdoFieldImage.js
+++ apps/src/blockly/addons/cdoFieldImage.js
@@ -36,6 +36,8 @@ export default class CdoFieldImage extends GoogleBlockly.FieldImage {
   }
 
   shouldAllowImageChange() {
+    // This can be called during object construction, in which case
+    // an undefined value will be treated as true.
     return this.allowImageChange !== false;
   }
 
