c830d52a16b9d10c9243e191b58192ee7ec9740c
denyslevada <levada.denys@gmail.com>
2023-10-16 23:25:39 +0300

    * update Chips documentation * remove unneeded Chip id that was constantly updating * refactor

--- apps/src/componentLibrary/chips/Chips.tsx
+++ apps/src/componentLibrary/chips/Chips.tsx
@@ -1,17 +1,24 @@
 import React, {useEffect, useRef} from 'react';
-import _ from 'lodash';
+import uniq from 'lodash/uniq';
 
 const commonI18n = require('@cdo/locale');
 import {ComponentSizeXSToL} from '@cdo/apps/componentLibrary/common/types';
 import moduleStyles from './chip.module.scss';
 
 export interface ChipProps {
+  /** Chip label */
   label: string;
+  /** Chip name*/
   name: string;
+  /** Chip value */
   value: string;
+  /** Chip checked state */
   checked: boolean;
+  /** Chip required state */
   required: boolean;
+  /** Chip disabled state */
   disabled?: boolean;
+  /** Chip onChange handler*/
   onCheckedChange: (checked: boolean) => void;
 }
 
@@ -25,7 +32,6 @@ export const Chip: React.FunctionComponent<ChipProps> = ({
   onCheckedChange,
 }) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const uniqueId = `multi-${_.uniqueId()}`;
 
   useEffect(() => {
     // Reset validity on every render so it gets checked again.
@@ -36,12 +42,15 @@ export const Chip: React.FunctionComponent<ChipProps> = ({
     }
   }, [inputRef, onCheckedChange]);
 
+  // TODO:
+  //  - add sizes
+  // - remove multiSelectGroup
+
   return (
     <div>
       <label className={moduleStyles.chip}>
         <input
           ref={inputRef}
-          id={uniqueId}
           type="checkbox"
           name={name}
           value={value}
@@ -61,33 +70,35 @@ export const Chip: React.FunctionComponent<ChipProps> = ({
         />
         {label}
       </label>
-
-      {/*<label htmlFor={uniqueId}></label>*/}
     </div>
   );
 };
 
 export interface ChipsProps {
+  /** Chips group label */
   label?: string;
+  /** Chips group name */
   name: string;
+  /** Chips required state */
   required?: boolean;
+  /** Chips disabled state */
   disabled?: boolean;
+  /** List of chips to render ({value: string, label: string}[]) */
   options: {value: string; label: string}[];
+  /** List of selected chips values */
   values: string[];
+  /** Callback to update selected chips values */
   setValues: (values: string[]) => void;
+  /** Size of chips */
   size?: ComponentSizeXSToL;
 }
 
-// NOTE: The `name` will show up in the DOM with an appended `[]`, so Rails
-// natively understands it as an array. Set `required` to `true` if you want
-// the user to have to select at least one of the options to proceed.
-// You probably want `values` to start out as an empty array.
 // TODO: add different sizes
 /**
  * ### Production-ready Checklist:
  * * (?) implementation of component approved by design team;
  * * (?) has storybook, covered with stories and documentation;
- * * (?) has tests: test every prop, every state and every interaction that's js related;
+ * * (✔) has tests: test every prop, every state and every interaction that's js related;
  * * (see apps/test/unit/componentLibrary/ChipsTest.jsx)
  * * (?) passes accessibility checks;
  *
@@ -106,6 +117,10 @@ const Chips: React.FunctionComponent<ChipsProps> = ({
   setValues,
   size = 'm',
 }) => {
+  // NOTE: The `name` will show up in the DOM with an appended `[]`, so Rails
+  // natively understands it as an array. Set `required` to `true` if you want
+  // the user to have to select at least one of the options to proceed.
+  // You probably want `values` to start out as an empty array.
   const inputName = `${name}[]`;
 
   return (
@@ -130,7 +145,7 @@ const Chips: React.FunctionComponent<ChipsProps> = ({
             onCheckedChange={checked => {
               if (checked) {
                 // Add this value to the `values` array.
-                setValues(_.uniq([...values, option.value]));
+                setValues(uniq([...values, option.value]));
               } else {
                 // Remove this value from the `values` array.
                 setValues(values.filter(v => v !== option.value));
