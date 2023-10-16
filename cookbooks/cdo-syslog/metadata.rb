d23be92ecea67d393cf1f35d946b3320b9e851e3
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-10-16 12:11:13 -0700

    Migrate `sudo_user` Ohai Plugin (#53914)
    * Migrate `sudo_user` Ohai Plugin
    
    Following instructions at https://docs.chef.io/ohai_custom/#install-ohai-plugins
    
    Installing Ohai plugins with the cookbook is deprecated; Chef now supports loading plugins natively.
    
    * Extract `sudo-user` Functionality
    
    Out of a dedicated cookbook, and into a plugin for our `cdo-apps` cookbook.
    
    * update berksfile.lock after berks changes

--- cookbooks/cdo-syslog/metadata.rb
+++ cookbooks/cdo-syslog/metadata.rb
@@ -5,5 +5,3 @@ license          'All rights reserved'
 description      'Installs/Configures syslog'
 long_description File.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.13'
-
-depends 'sudo-user'
