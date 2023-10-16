72e0eb639d245b8757a1f991e7ee2a8f0ddf6b47
Tyrone Shaffer <tyrone.shaffer@code.org>
2023-10-16 17:25:04 -0400

    Reformated algorithm, incorporated top 3 random

--- apps/src/dance/ai/DanceAiClient.js
+++ apps/src/dance/ai/DanceAiClient.js
@@ -1,6 +1,7 @@
 import CachedPalettes from '@cdo/static/dance/ai/model/cached-spacy-palette-map.json';
 import CachedBackgrounds from '@cdo/static/dance/ai/model/cached-spacy-background-map.json';
 import CachedForegrounds from '@cdo/static/dance/ai/model/cached-spacy-foreground-map.json';
+import {forEach} from 'lodash';
 
 /**
  * Chooses the foreground, background, and palette effects that are most closely associated with
@@ -10,93 +11,68 @@ import CachedForegrounds from '@cdo/static/dance/ai/model/cached-spacy-foregroun
  * example: {"backgroundEffect":"sparkles","backgroundColor":"cool","foregroundEffect":"bubbles"}
  */
 export function chooseEffects(emojis) {
-  var foregroundValues = {};
-  var backgroundValues = {};
-  var paletteValues = {};
-  reformatInputs(foregroundValues, CachedForegrounds);
-  reformatInputs(backgroundValues, CachedBackgrounds);
-  reformatInputs(paletteValues, CachedPalettes);
+  var backgroundValues = [
+    CachedBackgrounds['emojiAssociations'][emojis[0]],
+    CachedBackgrounds['emojiAssociations'][emojis[1]],
+    CachedBackgrounds['emojiAssociations'][emojis[2]],
+  ];
 
-  const bestBackground = chooseHighestSum(backgroundValues, emojis);
-  const bestForeground = chooseHighestSum(foregroundValues, emojis);
-  const bestPalette = chooseHighestSum(paletteValues, emojis);
+  var foregroundValues = [
+    CachedForegrounds['emojiAssociations'][emojis[0]],
+    CachedForegrounds['emojiAssociations'][emojis[1]],
+    CachedForegrounds['emojiAssociations'][emojis[2]],
+  ];
 
-  const chosenEffects = {
-    backgroundEffect: bestBackground.title,
-    backgroundColor: bestPalette.title,
-    foregroundEffect: bestForeground.title,
-  };
-  return JSON.stringify(chosenEffects);
-}
+  var paletteValues = [
+    CachedPalettes['emojiAssociations'][emojis[0]],
+    CachedPalettes['emojiAssociations'][emojis[1]],
+    CachedPalettes['emojiAssociations'][emojis[2]],
+  ];
 
-/**
- * Sums the values in valueMap at each given emoji. Picks the highest value and returns an object
- * formatted like the following example: {title: "circles", value: 0.59}
- * @param {object} valueMap: the set of AI-calculated values for each emoji, for example, all
- * calculated values for backgrounds.
- * @param {array} chosenEmojis: the emojis chosen by the user
- * @returns: an object describing the value with the highest weight.
- */
-function chooseHighestSum(valueMap, chosenEmojis) {
-  const minimumPossibleValue = -3;
-  return Object.keys(valueMap[chosenEmojis[0]]).reduce(
-    (highest, effect) => {
-      let sum =
-        valueMap[chosenEmojis[0]][effect] +
-        valueMap[chosenEmojis[1]][effect] +
-        valueMap[chosenEmojis[2]][effect];
-
-      return sum > highest.value ? {title: effect, value: sum} : highest;
-    },
-    {title: '', value: minimumPossibleValue - 1}
+  // Sum element-wise vectors of selected emojis
+  const bg_sum = backgroundValues.reduce((a, b) =>
+    a.map((val, index) => val + b[index])
   );
-}
+  const fg_sum = foregroundValues.reduce((a, b) =>
+    a.map((val, index) => val + b[index])
+  );
+  const palette_sum = paletteValues.reduce((a, b) =>
+    a.map((val, index) => val + b[index])
+  );
 
-/**
- * Transforms data between the following formats
-  {
-    "emojiAssociations": {
-      "poopy": {
-        "association": [
-          -0.06, 0.03
-        ]
-      },
-      "romantic": {
-        "association": [
-          0.25, 0.17
-        ]
-      }
-    },
-    "output": [
-      "circles",
-      "color_cycle"
-    ]
-  }
- *
- *  ^^ Old format ^^ New format below
- *
-  {
-    "poopy": {
-      "circles": -0.06,
-      "color_cycle": 0.03
-    }
-    "romantic": {
-      "circles": 0.25,
-      "color_cycle": 0.17
-    }
-  }
- *
- * @param newObject: an object where the newly formatted data will be stored
- * @param jsonInput: the old data that should be reformatted
- * @returns: null. Instead, it transforms newObject
- */
-function reformatInputs(newObject, jsonInput) {
-  Object.keys(jsonInput.emojiAssociations).forEach(emoji => {
-    newObject[emoji] = {};
-    jsonInput.emojiAssociations[emoji].association.forEach(
-      (calculatedValue, index) => {
-        newObject[emoji][jsonInput.output[index]] = calculatedValue;
-      }
-    );
-  });
+  // Sort and slice top scoring options, mapped to their output identifiers (e.g. [[0.25, 'squiggles'], ...])
+  const numRandomOptions = 3;
+  const bg_options = bg_sum
+    .map(function (sum, index) {
+      return [sum, CachedBackgrounds['output'][index]];
+    })
+    .sort()
+    .reverse()
+    .slice(0, numRandomOptions);
+  const fg_options = fg_sum
+    .map(function (sum, index) {
+      return [sum, CachedForegrounds['output'][index]];
+    })
+    .sort()
+    .reverse()
+    .slice(0, numRandomOptions);
+  const palette_options = palette_sum
+    .map(function (sum, index) {
+      return [sum, CachedPalettes['output'][index]];
+    })
+    .sort()
+    .reverse()
+    .slice(0, numRandomOptions);
+
+  // Choose random value from top scoring options
+  const chosenEffects = {
+    backgroundEffect:
+      bg_options[Math.floor(Math.random() * bg_options.length)][1],
+    backgroundColor:
+      palette_options[Math.floor(Math.random() * palette_options.length)][1],
+    foregroundEffect:
+      fg_options[Math.floor(Math.random() * fg_options.length)][1],
+  };
+
+  return JSON.stringify(chosenEffects);
 }
