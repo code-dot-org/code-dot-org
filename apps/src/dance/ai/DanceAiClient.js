39f896dbd0ef64eaf0cc1971ac2b5b91093ec40a
Tyrone Shaffer <tyrone.shaffer@code.org>
2023-10-17 16:29:53 -0400

    Cleaned up loop, generalized functions

--- apps/src/dance/ai/DanceAiClient.js
+++ apps/src/dance/ai/DanceAiClient.js
@@ -1,7 +1,6 @@
-import CachedPalettes from '@cdo/static/dance/ai/model/cached-spacy-palette-map.json';
 import CachedBackgrounds from '@cdo/static/dance/ai/model/cached-spacy-background-map.json';
 import CachedForegrounds from '@cdo/static/dance/ai/model/cached-spacy-foreground-map.json';
-import {forEach} from 'lodash';
+import CachedPalettes from '@cdo/static/dance/ai/model/cached-spacy-palette-map.json';
 
 /**
  * Chooses the foreground, background, and palette effects that are most closely associated with
@@ -11,50 +10,25 @@ import {forEach} from 'lodash';
  * example: {"backgroundEffect":"sparkles","backgroundColor":"cool","foregroundEffect":"bubbles"}
  */
 export function chooseEffects(emojis) {
-  backgroundValues = [];
-  foregroundValues = [];
-  paletteValues = [];
-  emojis.forEach(emojiName => {
-    backgroundValues.concat(CachedBackgrounds['emojiAssociations'][emojiName]);
-    foregroundValues.concat(CachedForegrounds['emojiAssociations'][emojiName]);
-    paletteValues.concat(CachedPalettes['emojiAssociations'][emojiName]);
+  const outputTypes = [CachedBackgrounds, CachedForegrounds, CachedPalettes];
+  const outputWeights = [];
+  outputTypes.forEach(outputJson => {
+    outputWeights.push(calculateOutputWeightsVector(emojis, outputJson));
   });
-
-  // Sum element-wise vectors of selected emojis
-  const backgroundSum = backgroundValues.reduce((firstList, secondList) =>
-    firstList.map((value, index) => value + secondList[index])
-  );
-  const foregroundSum = foregroundValues.reduce((firstList, secondList) =>
-    firstList.map((value, index) => value + secondList[index])
-  );
-  const paletteSum = paletteValues.reduce((firstList, secondList) =>
-    firstList.map((value, index) => value + secondList[index])
-  );
 
   // Sort and slice top scoring options, mapped to their output identifiers (e.g. [[0.25, 'squiggles'], ...])
   const numRandomOptions = 3;
-  const backgroundOptions = backgroundSum
-    .map(function (sum, index) {
-      return [sum, CachedBackgrounds['output'][index]];
-    })
-    .sort()
-    .reverse()
-    .slice(0, numRandomOptions);
-  const foregroundOptions = foregroundSum
-    .map(function (sum, index) {
-      return [sum, CachedForegrounds['output'][index]];
-    })
-    .sort()
-    .reverse()
-    .slice(0, numRandomOptions);
-  const paletteOptions = paletteSum
-    .map(function (sum, index) {
-      return [sum, CachedPalettes['output'][index]];
-      F;
-    })
-    .sort()
-    .reverse()
-    .slice(0, numRandomOptions);
+  const outputOptions = [];
+  outputWeights.forEach((weightVector, i) => {
+    outputOptions.push(
+      obtainTopOptions(numRandomOptions, weightVector, outputTypes[i])
+    );
+  });
+
+  // Clarification of which array correlates to which option
+  const backgroundOptions = outputOptions[0];
+  const foregroundOptions = outputOptions[1];
+  const paletteOptions = outputOptions[2];
 
   // Choose random value from top scoring options
   const chosenEffects = {
@@ -70,7 +44,43 @@ export function chooseEffects(emojis) {
       ][1],
   };
 
-  debugger;
-
   return JSON.stringify(chosenEffects);
+}
+
+/**
+ * Looks up and sums element-wise precalculated vector weights for each input as a condensed final output
+ * Emulates summation process for final classifier output layer of a machine learning model
+ * @param {*} emojis, associated input keywords that the model has precalculated
+ * @param {*} associatedOutputJson, precalculated vector weights for each possible type of output (e.g. BackgroundsEffects, ForegroundEffects, etc.)
+ * @returns 1d array of values between [-3, 3] with indexes that map to output "classes" (e.g. "circles" for BackgroundEffects) Higher values = stronger correlation
+ */
+function calculateOutputWeightsVector(emojis, associatedOutputJson) {
+  const individualInputVectors = [];
+  emojis.forEach(emojiName => {
+    individualInputVectors.push(
+      associatedOutputJson['emojiAssociations'][emojiName]
+    );
+  });
+  const sumWeights = individualInputVectors.reduce((firstList, secondList) =>
+    firstList.map((value, index) => value + secondList[index])
+  );
+
+  return sumWeights;
+}
+
+/**
+ * Simple selection function that returns the top N number of "classifications"
+ * @param {*} numOptions number of top scoring options wanted
+ * @param {*} outputWeights desired output weight vector to precalculate based on
+ * @param {*} associatedOutputJson, precalculated vector weights for each possible type of output (e.g. BackgroundsEffects, ForegroundEffects, etc.)
+ * @returns 1d array of tuples in the format ([score], [output key]) e.g. ("0.51", "circles")
+ */
+function obtainTopOptions(numOptions, outputWeights, associatedOutputJson) {
+  const topOptions = outputWeights
+    .map(function (sum, index) {
+      return [sum, associatedOutputJson['output'][index]];
+    })
+    .sort((item1, item2) => item2[0] - item1[0])
+    .slice(0, numOptions);
+  return topOptions;
 }
