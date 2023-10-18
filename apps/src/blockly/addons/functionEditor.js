17000290e0d3d8dd835d26dffaa5de3525f91d48
molly-moen <molly@code.org>
2023-10-18 15:13:04 -0700

    metrics manager

--- apps/src/blockly/addons/functionEditor.js
+++ apps/src/blockly/addons/functionEditor.js
@@ -10,6 +10,7 @@ import {
   MODAL_EDITOR_DELETE_ID,
 } from './functionEditorConstants';
 import {disableOrphans} from '@cdo/apps/blockly/eventHandlers';
+import CdoMetricsManager from './cdoMetricsManager';
 
 // This class creates the modal function editor, which is used by Sprite Lab and Artist.
 export default class FunctionEditor {
@@ -50,6 +51,9 @@ export default class FunctionEditor {
           vertical: true,
         },
         wheel: true,
+      },
+      plugins: {
+        metricsManager: CdoMetricsManager,
       },
       renderer: options.renderer,
       theme: Blockly.cdoUtils.getUserTheme(options.theme),
