069aa96c64405aa5dd6b9899efbf16745374285c
molly-moen <molly@code.org>
2023-10-23 13:32:26 -0700

    get rid of helper

--- apps/src/blockly/components/ModalFunctionEditor.jsx
+++ apps/src/blockly/components/ModalFunctionEditor.jsx
@@ -10,20 +10,9 @@ import Button from '@cdo/apps/templates/Button';
 import msg from '@cdo/locale';
 
 export default function ModalFunctionEditor() {
-  function renderButton(id, text, color) {
-    /** functionEditor.js handles setting the click handlers on these buttons. */
-    return (
-      <Button
-        type="button"
-        id={id}
-        onClick={() => {}}
-        color={color}
-        size={Button.ButtonSize.narrow}
-      >
-        {text}
-      </Button>
-    );
-  }
+  const buttonSize = Button.ButtonSize.narrow;
+  // functionEditor.js handles setting the click handlers on these buttons.
+  const emptyOnClick = () => {};
 
   return (
     <div
@@ -35,16 +24,24 @@ export default function ModalFunctionEditor() {
     >
       <div className={classNames('toolbar', moduleStyles.toolbar)}>
         <div className={moduleStyles.buttons}>
-          {renderButton(
-            MODAL_EDITOR_DELETE_ID,
-            msg.delete(),
-            Button.ButtonColor.neutralDark
-          )}
-          {renderButton(
-            MODAL_EDITOR_CLOSE_ID,
-            msg.closeDialog(),
-            Button.ButtonColor.brandSecondaryDefault
-          )}
+          <Button
+            type="button"
+            id={MODAL_EDITOR_DELETE_ID}
+            onClick={emptyOnClick}
+            color={Button.ButtonColor.neutralDark}
+            size={buttonSize}
+          >
+            {msg.delete()}
+          </Button>
+          <Button
+            type="button"
+            id={MODAL_EDITOR_CLOSE_ID}
+            onClick={emptyOnClick}
+            color={Button.ButtonColor.brandSecondaryDefault}
+            size={buttonSize}
+          >
+            {msg.closeDialog()}
+          </Button>
         </div>
       </div>
     </div>
