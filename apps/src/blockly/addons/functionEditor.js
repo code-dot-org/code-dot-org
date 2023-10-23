ec232545325b5c68d1d4b5ff41a3bfa6f0bafee9
Molly Moen <molly@code.org>
2023-10-23 15:57:05 -0700

    [Google Blockly] Clean up modal function editor styling (#54342)
    * remove non-module styles
    
    * fix button positioning
    
    * use new buttons
    
    * metrics manager
    
    * get rid of helper

--- apps/src/blockly/addons/functionEditor.js
+++ apps/src/blockly/addons/functionEditor.js
@@ -10,6 +10,7 @@ import {
   MODAL_EDITOR_DELETE_ID,
 } from './functionEditorConstants';
 import {disableOrphans} from '@cdo/apps/blockly/eventHandlers';
+import CdoMetricsManager from './cdoMetricsManager';
 
 // This class creates the modal function editor, which is used by Sprite Lab and Artist.
 export default class FunctionEditor {
@@ -50,6 +51,9 @@ export default class FunctionEditor {
           vertical: true,
         },
         wheel: true,
+      },
+      plugins: {
+        metricsManager: CdoMetricsManager,
       },
       renderer: options.renderer,
       theme: Blockly.cdoUtils.getUserTheme(options.theme),
@@ -313,8 +317,8 @@ export default class FunctionEditor {
   addEditorWorkspaceBlockConfig(blockConfig) {
     const returnValue = {
       ...blockConfig,
-      x: 50,
-      y: 50,
+      x: 20,
+      y: 20,
     };
     return returnValue;
   }
