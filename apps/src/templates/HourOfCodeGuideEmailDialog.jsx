78e777e8b3baca88b5ff3d44b9d5787645cfaf33
Hannah Bergam <hannahbergam@gmail.com>
2023-10-24 15:51:57 -0700

    refactoring to start

--- apps/src/templates/HourOfCodeGuideEmailDialog.jsx
+++ apps/src/templates/HourOfCodeGuideEmailDialog.jsx
@@ -1,29 +1,30 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
 import i18n from '@cdo/locale';
-import {SignInState} from '@cdo/apps/templates/currentUserRedux';
 import Button from '@cdo/apps/templates/Button';
 import AccessibleDialog from '@cdo/apps/templates/AccessibleDialog';
 import {Heading1, Heading3} from '@cdo/apps/componentLibrary/typography';
 import experiments from '@cdo/apps/util/experiments';
 import style from './accessible-dialogue.module.scss';
 
-function HourOfCodeGuideEmailDialog(signedIn) {
+function HourOfCodeGuideEmailDialog({isSignedIn}) {
   const [isOpen, setIsOpen] = useState(true);
 
   const onClose = () => {
     setIsOpen(false);
   };
 
-  const bodyText = () =>
-    signedIn === true ? i18n.weHaveEverything() : i18n.signUpToReceiveGuide();
+  const bodyText = isSignedIn
+    ? i18n.weHaveEverything()
+    : i18n.signUpToReceiveGuide();
 
-  const emailGuideButtonText = () =>
-    signedIn === true ? i18n.emailMeAGuide() : i18n.getGuideContinue();
+  const emailGuideButtonText = isSignedIn
+    ? i18n.emailMeAGuide()
+    : i18n.getGuideContinue();
 
-  const continueWithoutEmailButtonText = () =>
-    signedIn === true ? i18n.continueToActivity() : i18n.continueWithoutGuide();
+  const continueWithoutEmailButtonText = isSignedIn
+    ? i18n.continueToActivity()
+    : i18n.continueWithoutGuide();
 
   return (
     <div>
@@ -34,12 +35,12 @@ function HourOfCodeGuideEmailDialog(signedIn) {
           </div>
           <div className={style.middle}>
             <Heading3>{i18n.learnHowToHost()}</Heading3>
-            {bodyText()}
+            {bodyText}
           </div>
           <div className={style.buttonsBottom}>
             <Button
               id="uitest-no-email-guide"
-              text={continueWithoutEmailButtonText()}
+              text={continueWithoutEmailButtonText}
               onClick={() => {
                 onClose();
               }}
@@ -47,7 +48,7 @@ function HourOfCodeGuideEmailDialog(signedIn) {
             />
             <Button
               id="uitest-email-guide"
-              text={emailGuideButtonText()}
+              text={emailGuideButtonText}
               onClick={() => {
                 onClose();
               }}
@@ -61,12 +62,9 @@ function HourOfCodeGuideEmailDialog(signedIn) {
 }
 
 HourOfCodeGuideEmailDialog.propTypes = {
-  // Provided by redux
-  signedIn: PropTypes.bool,
+  isSignedIn: PropTypes.bool.isRequired,
 };
 
 export const UnconnectedHourOfCodeGuideEmailDialog = HourOfCodeGuideEmailDialog;
 
-export default connect(state => ({
-  signedIn: state.currentUser.signInState === SignInState.SignedIn,
-}))(HourOfCodeGuideEmailDialog);
+export default HourOfCodeGuideEmailDialog;
