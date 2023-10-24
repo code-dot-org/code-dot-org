d7150954d3c96492a9ed80094a1c2a065032055b
denyslevada <levada.denys@gmail.com>
2023-10-24 21:08:20 +0300

    * tests done

--- apps/test/unit/componentLibrary/TagsTest.jsx
+++ apps/test/unit/componentLibrary/TagsTest.jsx
@@ -1,247 +1,44 @@
-// TODO: Once start working on Button component - uncomment the code and use it as a template for Button component tests
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-// import React from 'react';
-// import {render, screen} from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import sinon from 'sinon';
-//
-// import {expect} from '../../util/reconfiguredChai';
-//
-// import Checkbox from '@cdo/apps/componentLibrary/checkbox';
-// describe('Design System - Checkbox', () => {
-//   it('Checkbox - renders with correct label', () => {
-//     render(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//       />
-//     );
-//
-//     const checkbox = screen.getByDisplayValue('test-checkbox');
-//     expect(checkbox).to.exist;
-//     expect(screen.getByText('Checkbox label')).to.exist;
-//   });
-//
-//   it('Checkbox - changes checked state on click', async () => {
-//     const user = userEvent.setup();
-//     const spyOnChange = sinon.spy();
-//
-//     let checked = false;
-//     const onChange = () => {
-//       checked = !checked;
-//       spyOnChange(checked);
-//     };
-//
-//     // Initial render
-//     const {rerender} = render(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//       />
-//     );
-//
-//     let checkbox = screen.getByDisplayValue('test-checkbox');
-//     expect(checkbox).to.exist;
-//
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's first click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.have.been.calledOnce;
-//     expect(spyOnChange).to.have.been.calledWith(true);
-//     expect(checkbox.checked).to.be.true;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's second click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.have.been.calledTwice;
-//     expect(spyOnChange).to.have.been.calledWith(false);
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//   });
-//
-//   it('Checkbox - renders indeterminate checkbox, changes on click', async () => {
-//     const user = userEvent.setup();
-//     const spyOnChange = sinon.spy();
-//
-//     let checked = false;
-//     let indeterminate = true;
-//     const onChange = () => {
-//       if (indeterminate) {
-//         // Default browser behavior for clicking an indeterminate checkbox.
-//         indeterminate = false;
-//         checked = true;
-//       } else {
-//         checked = !checked;
-//       }
-//       spyOnChange(checked);
-//     };
-//
-//     // Initial render
-//     const {rerender} = render(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         indeterminate={indeterminate}
-//       />
-//     );
-//
-//     let checkbox = screen.getByDisplayValue('test-checkbox');
-//     expect(checkbox).to.exist;
-//
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.true;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's first click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         indeterminate={indeterminate}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.have.been.calledOnce;
-//     expect(spyOnChange).to.have.been.calledWith(true);
-//     expect(checkbox.checked).to.be.true;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's second click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         indeterminate={indeterminate}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.have.been.calledTwice;
-//     expect(spyOnChange).to.have.been.calledWith(false);
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//   });
-//
-//   it("Checkbox - renders disabled checkbox, doesn't change on click", async () => {
-//     const user = userEvent.setup();
-//     const spyOnChange = sinon.spy();
-//
-//     let checked = false;
-//     const onChange = () => {
-//       checked = !checked;
-//       spyOnChange(checked);
-//     };
-//
-//     // Initial render
-//     const {rerender} = render(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         disabled={true}
-//       />
-//     );
-//
-//     let checkbox = screen.getByDisplayValue('test-checkbox');
-//     expect(checkbox).to.exist;
-//
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.true;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's first click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         disabled={true}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.not.have.been.called;
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.true;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's second click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         disabled={true}
-//       />
-//     );
-//
-//     expect(spyOnChange).to.not.have.been.called;
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.true;
-//     expect(checkbox.indeterminate).to.be.false;
-//   });
-// });
+import {expect} from '../../util/reconfiguredChai';
+
+import Tags from '@cdo/apps/componentLibrary/tags';
+
+describe('Design System - Tags', () => {
+  it('Tags - renders with correct label', () => {
+    render(<Tags tagsList={['tag1', 'tag2']} />);
+
+    const tags = screen.getByTestId('tags');
+    expect(tags).to.exist;
+    expect(screen.getByText('tag1')).to.exist;
+    expect(screen.getAllByText('tag1')).to.have.lengthOf(1);
+  });
+
+  it('Tags - renders tooltip on hover, removes tooltip on unhover; shows +1 if there are two tags', async () => {
+    const user = userEvent.setup();
+
+    // Initial render
+    const {rerender} = render(<Tags tagsList={['tag1', 'tag2']} />);
+
+    let tags = screen.getByTestId('tags');
+    expect(tags).to.exist;
+    expect(screen.getByText('tag1')).to.exist;
+    expect(screen.getByText('+1')).to.exist;
+    expect(screen.queryByText('tag2')).to.not.exist;
+
+    await user.hover(screen.getByText('+1'));
+
+    // Re-render after user's hover
+    rerender(<Tags tagsList={['tag1', 'tag2']} />);
+
+    expect(screen.queryByText('tag2')).to.exist;
+
+    await user.unhover(screen.getByText('+1'));
+
+    // Re-render after user's unhover
+    rerender(<Tags tagsList={['tag1', 'tag2']} />);
+    expect(screen.queryByText('tag2')).to.not.exist;
+  });
+});
