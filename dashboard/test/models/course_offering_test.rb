9076ac7e6c3f8b7f1c953425cf26c43d19adab5c
Artem Vavilov <artem.vavilov.7@gmail.com>
2023-10-25 17:25:34 +0300

    P20-546: Update CourseOfferingTest

--- dashboard/test/models/course_offering_test.rb
+++ dashboard/test/models/course_offering_test.rb
@@ -663,11 +663,11 @@ class CourseOfferingTest < ActiveSupport::TestCase
   test 'query count for assignable_course_offerings' do
     Unit.stubs(:should_cache?).returns true
 
-    assert_cached_queries(2) do
+    assert_cached_queries(5) do
       CourseOffering.assignable_course_offerings_info(@teacher)
     end
 
-    assert_cached_queries(2) do
+    assert_cached_queries(6) do
       CourseOffering.assignable_course_offerings_info(@facilitator)
     end
 
