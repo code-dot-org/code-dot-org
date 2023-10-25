0ccfcc3f78b6ce40c337f0198e7892e927f12209
Hannah Bergam <hannahbergam@gmail.com>
2023-10-25 10:48:03 -0700

    fixing test

--- apps/test/unit/templates/HourOfCodeGuideEmailDialogTest.js
+++ apps/test/unit/templates/HourOfCodeGuideEmailDialogTest.js
@@ -1,12 +1,9 @@
 import {assert, expect} from 'chai';
 import React from 'react';
-import sinon from 'sinon';
 import {UnconnectedHourOfCodeGuideEmailDialog as HourOfCodeGuideEmailDialog} from '@cdo/apps/templates/HourOfCodeGuideEmailDialog';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import i18n from '@cdo/locale';
 import experiments from '@cdo/apps/util/experiments';
-import analyticsReporter from '@cdo/apps/lib/util/AnalyticsReporter';
-import {EVENTS} from '@cdo/apps/lib/util/AnalyticsConstants';
 
 describe('HourOfCodeGuideEmailDialog', () => {
   const defaultProps = {
@@ -31,15 +28,5 @@ describe('HourOfCodeGuideEmailDialog', () => {
     experiments.setEnabled(experiments.HOC_TUTORIAL_DIALOG, false);
     const wrapper = shallow(<HourOfCodeGuideEmailDialog {...defaultProps} />);
     assert.equal(wrapper.children().length, 0);
-  });
-
-  it('sends Amplitude event', () => {
-    const analyticsSpy = sinon.spy(analyticsReporter, 'sendEvent');
-    const wrapper = mount(<HourOfCodeGuideEmailDialog {...defaultProps} />);
-    wrapper.find('button#uitest-email-guide').simulate('click');
-
-    expect(analyticsSpy).to.have.been.calledOnce;
-    expect(analyticsSpy.firstCall.args).to.deep.eq([EVENTS.GUIDE_SENT_EVENT]);
-    analyticsSpy.restore();
   });
 });
