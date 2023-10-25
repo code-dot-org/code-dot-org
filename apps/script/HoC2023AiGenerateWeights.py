61f871ca9d2f85c8e95eafba0bfa4e55503fe5fb
Alice Fisher <alice.fisher@code.org>
2023-10-25 21:37:13 +0000

    add blockly ids

--- apps/script/HoC2023AiGenerateWeights.py
+++ apps/script/HoC2023AiGenerateWeights.py
@@ -12,81 +12,82 @@ nlp = spacy.load("en_core_web_md")
 emoji_ids = ["poopy", "romantic", "party", "silly", "sparkle", "happy", "magic", "spooky", "cute", "funky", "wavy", "lights", "rainbow", "robot", "chaotic", "disco", "zen", "fast", "evil", "cold", "cosmic", "sad", "black-and-white", "warm", "cool"]
 
 # color_palettes dictionary contains python name as key and blockly id as value.
+# User-facing name is added as a comment for each color palette.
 color_palettes_map = {
-  'rainbow pastel': 'default',  
-  'green blue': 'cool',
-  'electronic': 'elecronic',
-  'creamy pastel': 'iceCream',
-  'neon': 'neon',
-  'black and white': 'rave',
-  'sweet candy rainbow': 'tropical',
-  'retro': 'vintage',
-  'red orange': 'warm',
-  'grayscale': 'grayscale',
-  'pink blue': 'sky',
-  'blue': 'ocean',
-  'coral': 'sunrise',
-  'purple': 'sunset',
-  'green': 'spring',
-  'flower': 'summer',
-  'autumn': 'autumn',
-  'icy blue': 'winter',
-  'orange': 'twinkling',
-  'bright rainbow': 'rainbow',
-  'roses': 'roses',
+  'rainbow pastel': 'default', # Light
+  'green blue': 'cool', # Cool
+  'electronic': 'elecronic', # Electronic
+  'creamy pastel': 'iceCream', # Ice Cream
+  'neon': 'neon', # Neon
+  'black and white': 'rave', # Black and White
+  'sweet candy rainbow': 'tropical', # Tropical
+  'retro': 'vintage', # Vintage
+  'red orange': 'warm', # Warm
+  'grayscale': 'grayscale', # Grayscale
+  'pink blue': 'sky', # Sky
+  'blue': 'ocean', # Ocean
+  'coral': 'sunrise', # Sunrise
+  'purple': 'sunset', # Sunset
+  'green': 'spring', # Spring
+  'flower': 'summer', # Summer
+  'autumn': 'autumn', # Autumn
+  'icy blue': 'winter', # Winter
+  'orange': 'twinkling', # Twinkling
+  'bright rainbow': 'rainbow', # Rainbow
+  'roses': 'roses', # Roses
 }
 
 # background_effects_map contains python name as key and blockly id as value.
 background_effects_map = {
-    'pulse': 'circles',
-    'solid colors': 'color_cycle',
-    'diamonds': 'diamonds',
-    'disco':'disco_ball',
-    'firework': 'fireworks',
-    'swirl': 'swirl',
-    'kaleidoscope': 'kaleidoscope',
-    'laser beam': 'lasers',
-    'paint splatter': 'splatter',
-    'rainbow': 'rainbow',
-    'snow fall': 'snowflakes',
-    'words': 'text',
-    'galaxy': 'galaxy',
-    'sparkles': 'sparkles',
-    'spiral': 'spiral',
-    'squares': 'disco',
-    'twinkle': 'stars',
-    'sound wave': 'music_wave',
-    'circles': 'ripples',
-    'dots': 'ripples_random',
-    'moving shapes': 'quads',
-    'flowers': 'flowers',
-    'squiggles': 'squiggles',
-    'stars': 'growing_stars',
-    'flower petals': 'blooming_petals',
-    'clouds': 'clouds',
-    'light grid': 'frosted_grid',
-    'starburst': 'starburst',
-    'groovy shapes': 'higher_power',
+    'pulse': 'circles', # Circles
+    'solid colors': 'color_cycle', # Colors
+    'diamonds': 'diamonds', # Diamonds
+    'disco':'disco_ball', # Disco Ball
+    'firework': 'fireworks', # Fireworks
+    'swirl': 'swirl', # Hypno
+    'kaleidoscope': 'kaleidoscope', # Kaleidoscope
+    'laser beam': 'lasers', # Laser Dance Floor
+    'paint splatter': 'splatter', # Paint Splatter
+    'rainbow': 'rainbow', # Rainbow
+    'snow fall': 'snowflakes', # Snow
+    'words': 'text', # Song Names
+    'galaxy': 'galaxy', # Space
+    'sparkles': 'sparkles', # Sparkles
+    'spiral': 'spiral', # Spiral
+    'squares': 'disco', # Squares
+    'twinkle': 'stars', # Stars
+    'sound wave': 'music_wave', # Waves
+    'circles': 'ripples', # Ripples
+    'dots': 'ripples_random', # Random Ripples
+    'moving shapes': 'quads', # Angles
+    'flowers': 'flowers', # Flowers
+    'squiggles': 'squiggles', # Squiggles
+    'stars': 'growing_stars', # Growing Stars
+    'flower petals': 'blooming_petals', # Blooming Petals
+    'clouds': 'clouds', # Clouds
+    'light grid': 'frosted_grid', # Frosted Grid
+    'starburst': 'starburst', # Starburst
+    'groovy shapes': 'higher_power', # Higher Power
 }
 
-foreground_effects = {
-    'bubbles': 'bubbles',
-    'confetti': 'confetti',
-    'red hearts': 'hearts_red',
-    'music notes': 'music_notes',
-    'pineapple': 'pineapples',
-    'pizza': 'pizzas',
-    'poop': 'smiling_poop',
-    'rain': 'rain',
-    'rainbow': 'floating_rainbows',
-    'smily face': 'smile_face',
-    'spotlight': 'spotlight',
-    'stage lights': 'color_lights',
-    'taco': 'raining_tacos',
-    'emojis': 'emojis',
-    'colorful hearts': 'hearts_colorful',
-    'stars': 'exploding_stars'
-    'rainbow paint': 'paint_drip',
+foreground_effects_map = {
+    'bubbles': 'bubbles', # Bubbles
+    'confetti': 'confetti', # Confetti
+    'red hearts': 'hearts_red', # Hearts
+    'music notes': 'music_notes', # Music Notes
+    'pineapple': 'pineapples', # Pineapples
+    'pizza': 'pizzas', # Pizzas
+    'poop': 'smiling_poop', # Poop
+    'rain': 'rain', # Rain
+    'rainbow': 'floating_rainbows', # Rainbows
+    'smily face': 'smile_face', # Smiles
+    'spotlight': 'spotlight', # Spotlight
+    'stage lights': 'color_lights', # Stage Lights
+    'taco': 'raining_tacos', # Tacos
+    'emojis': 'emojis', # Emojis
+    'colorful hearts': 'hearts_colorful', # Colorful Hearts
+    'stars': 'exploding_stars' # Starburst
+    'rainbow paint': 'paint_drip', # Paint Drip
 }
 
 palettedict = {}
