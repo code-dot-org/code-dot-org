6a05c7d85edbd17c5824e37b4ca90ccb4bd0b617
Artem Vavilov <artem.vavilov.7@gmail.com>
2023-10-27 21:04:21 +0300

    P20-403: Refactor Apps/ExternalSources resource sync-in

--- bin/i18n/resources/apps/external_sources.rb
+++ bin/i18n/resources/apps/external_sources.rb
@@ -1,14 +1,11 @@
 require_relative '../../i18n_script_utils'
 
-Dir[File.expand_path('../external_sources/**/*.rb', __FILE__)].sort.each {|file| require file}
-
 module I18n
   module Resources
     module Apps
       module ExternalSources
         DIR_NAME = 'external-sources'.freeze
         BLOCKLY_CORE_DIR_NAME = 'blockly-core'.freeze
-        I18N_SOURCE_DIR_PATH = CDO.dir(File.join(I18N_SOURCE_DIR, DIR_NAME)).freeze
 
         def self.sync_in
           SyncIn.perform
@@ -21,3 +18,5 @@ module I18n
     end
   end
 end
+
+Dir[File.expand_path('../external_sources/**/*.rb', __FILE__)].sort.each {|file| require file}
