0d2bbccc958ee0aa9d7103fe98fb9a90581929a5
Turner Riley <turnertriley@gmail.com>
2023-10-30 15:50:00 -0700

    remove extra function from sync_out

--- bin/i18n/resources/pegasus/emails/sync_out.rb
+++ bin/i18n/resources/pegasus/emails/sync_out.rb
@@ -11,29 +11,6 @@ module I18n
     module Pegasus
       module Emails
         class SyncOut < I18n::Utils::SyncOutBase
-          DIR_NAME = 'emails'.freeze
-
-          def execute
-            progress_bar.start
-
-            I18nScriptUtils.process_in_threads(pegasus_languages) do |pegasus_lang|
-              crowdin_locale_dir = I18nScriptUtils.locale_dir(pegasus_lang[:crowdin_name_s])
-              crowdin_locale_resource_dir = File.join(crowdin_locale_dir, DIR_NAME)
-              next unless File.directory?(crowdin_locale_resource_dir)
-
-              locale = pegasus_lang[:locale_s]
-              distribute(locale, crowdin_locale_resource_dir) unless locale == 'en-US'
-
-              FileUtils.rm_r(crowdin_locale_resource_dir)
-            ensure
-              I18nScriptUtils.remove_empty_dir(crowdin_locale_dir)
-
-              mutex.synchronize {progress_bar.increment}
-            end
-
-            progress_bar.finish
-          end
-
           def process(language)
             crowdin_file_path = I18nScriptUtils.locale_dir(language[:crowdin_name_s], DIR_NAME, FILE_NAME)
             return unless File.file?(crowdin_file_path)
