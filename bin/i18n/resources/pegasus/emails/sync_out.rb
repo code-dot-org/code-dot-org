dddefa7f744ced1208a3fe1c57febb3f614968a4
Turner Riley <turnertriley@gmail.com>
2023-10-30 14:38:16 -0700

    extend class for sync out

--- bin/i18n/resources/pegasus/emails/sync_out.rb
+++ bin/i18n/resources/pegasus/emails/sync_out.rb
@@ -4,18 +4,14 @@ require 'fileutils'
 
 require_relative '../../../i18n_script_utils'
 require_relative '../../../utils/pegasus_markdown'
-require_relative '../markdown'
+require_relative '../emails'
 
 module I18n
   module Resources
     module Pegasus
-      module Markdown
-        class SyncOut
+      module Emails
+        class SyncOut < I18n::Utils::SyncOutBase
           DIR_NAME = 'emails'.freeze
-
-          def self.perform
-            new.execute
-          end
 
           def execute
             progress_bar.start
@@ -38,45 +34,18 @@ module I18n
             progress_bar.finish
           end
 
-          private
+          def process(language)
+            crowdin_file_path = I18nScriptUtils.locale_dir(language[:crowdin_name_s], DIR_NAME, FILE_NAME)
+            return unless File.file?(crowdin_file_path)
 
-          def pegasus_languages
-            @pegasus_languages ||= PegasusLanguages.get_crowdin_name_and_locale
-          end
-
-          def progress_bar
-            @progress_bar ||= I18nScriptUtils.create_progress_bar(
-              title: 'Pegasus/emails sync-out',
-              total: pegasus_languages.size
-            )
-          end
-
-          def mutex
-            @mutex ||= Thread::Mutex.new
-          end
-
-          def distribute(locale, crowdin_locale_resource_dir)
-            Dir.glob(File.join(crowdin_locale_resource_dir, '**/*.md')) do |crowdin_file_path|
-              crowdin_file_subpath = crowdin_file_path.delete_prefix(File.join(crowdin_locale_resource_dir, '/'))
-
-              crowdin_file_subdir = crowdin_file_subpath.delete_suffix(File.basename(crowdin_file_subpath))
-              markdown_file_subdir = crowdin_file_subdir.start_with?('views') ? crowdin_file_subdir : File.join('public', crowdin_file_subdir)
-
-              origin_markdown_file_path = File.join(ORIGIN_DIR_PATH, markdown_file_subdir, File.basename(crowdin_file_subpath))
-              # Because we give _all_ files coming from crowdin the partial
-              # extension, we can't know for sure whether or not the origin also uses
-              # that extension unless we check both with and without.
-              origin_markdown_file_path += PARTIAL_EXTNAME unless File.exist?(origin_markdown_file_path)
-              next unless File.exist?(origin_markdown_file_path)
-
-              markdown_name = File.basename(crowdin_file_subpath, '.md')
-              markdown_i18n_file_path = File.join(
-                ORIGIN_I18N_DIR_PATH, markdown_file_subdir, "#{markdown_name}.#{locale}.md#{PARTIAL_EXTNAME}"
-              )
-
-              I18nScriptUtils.copy_file(crowdin_file_path, markdown_i18n_file_path)
-              I18n::Utils::PegasusMarkdown.restore_file_header(origin_markdown_file_path, markdown_i18n_file_path)
+            unless I18nScriptUtils.source_lang?(language)
+              # Distributes the localization
+              target_i18n_file_path = File.join(ORIGIN_I18N_DIR_PATH, "#{language[:locale_s]}.yml")
+              I18nScriptUtils.sanitize_file_and_write(crowdin_file_path, target_i18n_file_path)
             end
+
+            i18n_file_path = I18nScriptUtils.locale_dir(language[:locale_s], DIR_NAME, FILE_NAME)
+            I18nScriptUtils.move_file(crowdin_file_path, i18n_file_path)
           end
         end
       end
@@ -84,4 +53,4 @@ module I18n
   end
 end
 
-I18n::Resources::Pegasus::Markdown::SyncOut.perform if __FILE__ == $0
+I18n::Resources::Pegasus::Emails::SyncOut.perform if __FILE__ == $0
