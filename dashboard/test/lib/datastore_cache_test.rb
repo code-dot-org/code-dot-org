daf2fbea1fa3fd0f0905016d6a082e14d6adf865
Elijah Hamovitz <elijahhamovitz@gmail.com>
2023-10-31 16:31:59 -0700

    remove no-longer-supported datastore cache change listeners from tests

--- dashboard/test/lib/datastore_cache_test.rb
+++ dashboard/test/lib/datastore_cache_test.rb
@@ -3,97 +3,55 @@ require 'dynamic_config/datastore_cache'
 require 'dynamic_config/adapters/memory_adapter'
 
 class DatastoreCacheTest < ActiveSupport::TestCase
-  class FakeListener
-    attr_reader :changed
-
-    def initialize
-      reset
-    end
-
-    def reset
-      @changed = false
-    end
-
-    def on_change
-      @changed = true
-    end
-  end
-
   def setup
     @data_adapter = MemoryAdapter.new
+    @datastore_cache = DatastoreCache.new(@data_adapter, 'datastore cache unit test')
   end
 
   test 'simple get and set' do
-    cache = DatastoreCache.new @data_adapter
     key = "key"
     value = [1, 3, 2]
 
-    cache.set(key, value)
-    assert_equal value, cache.get(key)
+    @datastore_cache.set(key, value)
+    assert_equal value, @datastore_cache.get(key)
   end
 
-  test 'simple get and set with multiple listeners' do
-    cache = DatastoreCache.new @data_adapter
-
-    listener = FakeListener.new
-    cache.add_change_listener(listener)
-
+  test 'simple get and set' do
     key = "key"
-    value = [1, 3, 2]
+    first_value = [1, 3, 2]
+    second_value = [2, 4, 3]
 
-    refute listener.changed, 'on_change should not be called before change'
-    cache.set(key, value)
-    assert_equal value, cache.get(key)
-    assert listener.changed, 'on_change should be called after local set'
-    listener.reset
+    assert_nil @datastore_cache.get(key)
 
-    # Add another listener, and verify that it receives change notifications too.
-    value2 = [2, 4, 3]
-    listener2 = FakeListener.new
-    cache.add_change_listener(listener2)
-    cache.set(key, value2)
-    assert_equal value2, cache.get(key)
-    assert listener.changed
-    assert listener2.changed
-    listener.reset
-    listener2.reset
+    @datastore_cache.set(key, first_value)
+    assert_equal first_value, @datastore_cache.get(key)
 
-    # Set the same value and verify that the change listeners are not called.
-    cache.set(key, value2)
-    refute listener.changed
-    refute listener2.changed
+    @datastore_cache.set(key, second_value)
+    assert_equal second_value, @datastore_cache.get(key)
   end
 
   test 'cache is refreshed after expiration time' do
     key = "key"
-    value = "1, 2, 3"
+    initial_value = 'initial value'
+    updated_value = "1, 2, 3"
 
-    @data_adapter.set(key, 'not the real value')
-    cache = DatastoreCache.new @data_adapter, cache_expiration: 0.01
+    @data_adapter.set(key, initial_value)
+    cache = DatastoreCache.new @data_adapter, 'fast expiration', cache_expiration: 0.01
+    assert_equal initial_value, cache.get(key)
 
-    listener = FakeListener.new
-    cache.add_change_listener(listener)
+    # Data is not updated immediately
+    @data_adapter.set(key, updated_value)
+    assert_equal initial_value, cache.get(key)
 
-    @data_adapter.set(key, value)
+    # Data is eventually updated
     sleep 0.1
-
-    assert_equal value, cache.get(key)
-    assert listener.changed, 'on_change should be called after polled updated'
-    listener.reset
-    sleep 0.1
-    refute listener.changed, 'on_change should not be called when polling detects no changes'
-
-    @data_adapter.set(key, 'new value')
-    sleep 0.1
-    assert_equal 'new value', cache.get(key)
-    assert listener.changed, 'on_change should be called after second polled updated'
+    assert_equal updated_value, cache.get(key)
   end
 
   test "set updates datastore and local cache" do
-    cache = DatastoreCache.new @data_adapter
     @data_adapter.expects(:set).once
-    cache.expects(:set_local).once
-    cache.set("whatever", "yo")
+    @datastore_cache.expects(:set_local).once
+    @datastore_cache.set("whatever", "yo")
   end
 
   test 'seeding data on init' do
@@ -102,11 +60,11 @@ class DatastoreCacheTest < ActiveSupport::TestCase
 
     @data_adapter.set(k, v)
     @data_adapter.expects(:all).returns({}).once
-    DatastoreCache.new @data_adapter
+    DatastoreCache.new @data_adapter, 'test initialization'
   end
 
   test 'datastore failure on init' do
     @data_adapter.stubs(:all).throws(StandardError)
-    DatastoreCache.new @data_adapter
+    DatastoreCache.new @data_adapter, 'test initialization failure'
   end
 end
