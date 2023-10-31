908f4a262e9623a189480de16ab3d7b7d7b3d49b
Turner Riley <turnertriley@gmail.com>
2023-10-31 12:43:58 -0700

    repalce DIR_NAME with emails

--- bin/i18n/resources/pegasus/emails.rb
+++ bin/i18n/resources/pegasus/emails.rb
@@ -6,9 +6,9 @@ module I18n
   module Resources
     module Pegasus
       module Emails
-        DIR_NAME = 'pegasus/emails'.freeze
-        ORIGIN_DIR_PATH = CDO.dir(DIR_NAME).freeze
-        I18N_SOURCE_DIR_PATH = CDO.dir(I18N_SOURCE_DIR, 'emails').freeze
+        DIR_NAME = 'emails'.freeze
+        ORIGIN_DIR_PATH = CDO.dir("pegasus/#{DIR_NAME}").freeze
+        I18N_SOURCE_DIR_PATH = CDO.dir(I18N_SOURCE_DIR, DIR_NAME).freeze
 
         def self.sync_in
           SyncIn.perform
