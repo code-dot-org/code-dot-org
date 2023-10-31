22f7dd88c0a156a42a8ed43989d3f05277e15cc9
Turner Riley <turnertriley@gmail.com>
2023-10-31 11:50:15 -0700

    PR feedback and removing file name usage

--- bin/i18n/resources/pegasus/emails.rb
+++ bin/i18n/resources/pegasus/emails.rb
@@ -6,10 +6,9 @@ module I18n
   module Resources
     module Pegasus
       module Emails
-        DIR_NAME = 'emails'
-        ORIGIN_DIR_PATH = CDO.dir('pegasus/emails').freeze
-        ORIGIN_I18N_DIR_PATH = File.join(ORIGIN_DIR_PATH, 'i18n').freeze
-        I18N_SOURCE_DIR_PATH = CDO.dir(I18N_SOURCE_DIR, 'emails').freeze
+        DIR_NAME = 'pegasus/emails'.freeze
+        ORIGIN_DIR_PATH = CDO.dir(DIR_NAME).freeze
+        I18N_SOURCE_DIR_PATH = CDO.dir(I18N_SOURCE_DIR, DIR_NAME).freeze
 
         def self.sync_in
           SyncIn.perform
