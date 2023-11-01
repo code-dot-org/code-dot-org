cabb02624513d6cc97e2560c5f10f0c1927ad04d
Alice Fisher <alice.fisher@code.org>
2023-11-01 19:07:08 +0000

    add more clarifying comments

--- apps/script/HoC2023AiGenerateWeights.py
+++ apps/script/HoC2023AiGenerateWeights.py
@@ -1,22 +1,40 @@
 # Script to generate the associated output weights contained in files like cached-spacy-background-map.json
 # that are used to calculate final effects output HoC2023.
 # Run this Python script from the code-dot-org root directory.
+# python apps/script/HoC2023AiGenerateWeights.py
+# At the bottom of this file are background effects, palettes and foreground effects in the order they appear in the dropdowns.
+# When this file is run and the three cached data files are replaced in apps/static/dance/ai/model,
+# please update comments lists below with output of this script.
+
 import json
 from HoC2023AiHelperFunctions import *
 
 # Get the list of emoji names and their corresponding emoji ids
 emojis_list, emojis_map = get_ai_emoji_inputs()
 
+# get the background effects, foreground effects, and palettes lists (blockly_ids and user_facing_names)
 background_effect_blockly_ids_list, background_effect_user_facing_names_list = get_background_effects()
 palette_blockly_ids_list, palette_user_facing_names_list = get_palettes()
 foreground_effect_blockly_ids_list, foreground_effect_user_facing_names_list = get_foreground_effects()
+
+# Constants for 4 option lists.
+EMOJIS = 'emojis'
+PALETTES = 'palettes'
+BACKGROUND_EFFECTS = 'background_effects'
+FOREGROUND_EFFECTS = 'foreground_effects'
+
+blockly_ids_lists_map = {
+    PALETTES: palette_blockly_ids_list,
+    BACKGROUND_EFFECTS: background_effect_blockly_ids_list,
+    FOREGROUND_EFFECTS: foreground_effect_blockly_ids_list
+}
 
 # We rename foreground/background effects and palettes as their model_descriptive_name
 # to better reflect the actual output of the effect or color.
 # Below, we store them in maps for which the key is the model_descriptive_name and the
 # value is the corresponding blockly_id.
-
-background_effect_model_descriptive_names = [
+model_descriptive_names_map = {}
+model_descriptive_names_map[BACKGROUND_EFFECTS] = [
     'moving shapes',
     'flower petals',
     'pulse',
@@ -47,9 +65,8 @@ background_effect_model_descriptive_names = [
     'twinkle',
     'sound wave'
 ]
-background_effects_map = create_map_print_options('background_effects', background_effect_model_descriptive_names, background_effect_blockly_ids_list, background_effect_user_facing_names_list)
 
-palette_model_descriptive_names = [
+model_descriptive_names_map[PALETTES] = [
     'autumn',
     'black and white',
     'green blue',
@@ -72,9 +89,8 @@ palette_model_descriptive_names = [
     'red orange',
     'icy blue'
 ]
-palettes_map = create_map_print_options('palettes', palette_model_descriptive_names, palette_blockly_ids_list, palette_user_facing_names_list)
 
-foreground_effect_model_descriptive_names = [
+model_descriptive_names_map[FOREGROUND_EFFECTS] = [
     'bubbles',
     'hearts',
     'confetti',
@@ -93,7 +109,13 @@ foreground_effect_model_descriptive_names = [
     'stars',
     'taco'
 ]
-foreground_effects_map = create_map_print_options('foreground_effects', foreground_effect_model_descriptive_names, foreground_effect_blockly_ids_list, foreground_effect_user_facing_names_list)
+
+# Create maps of model_descriptive_name to blockly_id and print lists
+# as 'model_descriptive_name | blockly_id | blockly_user_facing_name'
+# Updates lists in comments at bottom of this file.
+create_map_print_options(PALETTES, model_descriptive_names_map[PALETTES], palette_blockly_ids_list, palette_user_facing_names_list),
+create_map_print_options(BACKGROUND_EFFECTS, model_descriptive_names_map[BACKGROUND_EFFECTS], background_effect_blockly_ids_list, background_effect_user_facing_names_list),
+create_map_print_options(FOREGROUND_EFFECTS, model_descriptive_names_map[FOREGROUND_EFFECTS], foreground_effect_blockly_ids_list, foreground_effect_user_facing_names_list)
 
 # We use cache files to store ada's raw embedding outputs to reduce Open AI query costs.
 # An embedding is a vector (list) of floating point numbers, and embeddings are used to
@@ -101,10 +123,10 @@ foreground_effects_map = create_map_print_options('foreground_effects', foregrou
 # their relatedness. https://platform.openai.com/docs/guides/embeddings
 # These caches are stored as pickle files; python's native way to serialize data.
 embeddings_paths = {
-    'emojis': 'apps/static/dance/ai/model/input_embeddings.pkl',
-    'palettes': 'apps/static/dance/ai/model/palette_embeddings.pkl',
-    'background_effects': 'apps/static/dance/ai/model/background_embeddings.pkl',
-    'foreground_effects': 'apps/static/dance/ai/model/foreground_embeddings.pkl'
+    EMOJIS: 'apps/static/dance/ai/model/input_embeddings.pkl',
+    PALETTES: 'apps/static/dance/ai/model/palette_embeddings.pkl',
+    BACKGROUND_EFFECTS: 'apps/static/dance/ai/model/background_embeddings.pkl',
+    FOREGROUND_EFFECTS: 'apps/static/dance/ai/model/foreground_embeddings.pkl'
 }
 
 embeddings_caches = {}
@@ -114,57 +136,62 @@ for path in embeddings_paths.keys():
 # Retrieve embeddings for input emojis, palettes, background efects, and foreground effects.
 # Use emojis as inputs.
 options_lists = {
-    'emojis': emojis_list,
-    'palettes': palette_model_descriptive_names,
-    'background_effects': background_effect_model_descriptive_names,
-    'foreground_effects': foreground_effect_model_descriptive_names
+    EMOJIS: emojis_list,
+    PALETTES: model_descriptive_names_map[PALETTES],
+    BACKGROUND_EFFECTS: model_descriptive_names_map[BACKGROUND_EFFECTS],
+    FOREGROUND_EFFECTS: model_descriptive_names_map[FOREGROUND_EFFECTS]
 }
 
 embeddings = {}
 # embeddings is a dictionary with four lists of embeddings:
-# 'emojis': [[emoji1_embedding], [emoji2_embedding], ...]
-# 'palettes: [[palette1+embedding], [palette2_embedding], ...]
-# 'background_effects': [[background_effect1_embedding], [background_effect2_embedding], ...]
-# 'foreground_effects': [[foreground_effect1_embedding], [foreground_effect2_embedding], ...]
+# EMOJIS: [[emoji1_embedding], [emoji2_embedding], ...]
+# PALETTES: [[palette1+embedding], [palette2_embedding], ...]
+# BACKGROUND_EFFECTS: [[background_effect1_embedding], [background_effect2_embedding], ...]
+# FOREGROUND_EFFECTS: [[foreground_effect1_embedding], [foreground_effect2_embedding], ...]
 for options_list_name in options_lists.keys():
     embeddings[options_list_name] = [retrieve_embedding(string=item,
-                                                        cache_path=embedding_paths[options_list_name],
+                                                        cache_path=embeddings_paths[options_list_name],
                                                         embedding_cache=embeddings_caches[options_list_name])
                                                         for item in options_lists[options_list_name]]
-                                    
 
-palette_dict = calculate_similarity_score(embeddings['emojis'], embeddings['palettes'], emojis_map)
-background_dict = calculate_similarity_score(embeddings['emojis'], embeddings['background_effects'], emojis_map)
-foreground_dict = calculate_similarity_score(embeddings['emojis'], embeddings['foreground_effects'], emojis_map)
+similarities_map = {}
+# similarities_map is a dictionary of dictionaries, one dictionary for each output (palette, background effect, and foreground effect).
+# Each of the output's dictionary contains keys which are the emoji id names and values which are the
+# similarity score comparing the emoji (key) and each of the output items.
+# For example, similarities_map[PALETTES] is a dictionary with first key-value assigned as
+# 'poopy': [0.772, 0.769, 0.775, 0.785, ...]
+# 0.772 is the similarity score between the 'poopy' emoji and the first palette, 'autumn'.
+# 0.769 is the similarity score between the 'poopy' emoji and the second palette, 'black and white'., etc.
+for outputs_list_name in [PALETTES, BACKGROUND_EFFECTS, FOREGROUND_EFFECTS]:
+    similarities_map[outputs_list_name] = calculate_similarity_score(embeddings[EMOJIS], embeddings[outputs_list_name], emojis_map)
 
-background_output = {'emojiAssociations': background_dict, 'output': background_effect_model_descriptive_names}
-palette_output = {'emojiAssociations': palette_dict, 'output': palette_model_descriptive_names}
-foreground_output = {'emojiAssociations': foreground_dict, 'output': foreground_effect_model_descriptive_names}
-
-# Modify output to reflect blockly id names.
-background_output['output'] = [background_effects_map[bg] for bg in background_output['output']]
-foreground_output['output'] = [foreground_effects_map[fg] for fg in foreground_output['output']]
-palette_output['output'] = [palettes_map[pal] for pal in palette_output['output']]
+# association_output_map contains 3 dictionary per output (palette, background effect, and foreground effect)..
+# The first key in each dictionary is 'emojiAssociations' and the value is the corresponding similarities_map from above.
+# The second key in each dictionary is the 'output' and the value is the corresponding blockly_ids list for the output.
+association_output_map = {}
+for outputs_list_name in [PALETTES, BACKGROUND_EFFECTS, FOREGROUND_EFFECTS]:
+    association_output_map[outputs_list_name] = {'emojiAssociations': similarities_map[outputs_list_name], 'output': blockly_ids_lists_map[outputs_list_name]}
 
 # For testing purposes (temporary until model is decided):
 ada_emoji = 'ada-emoji'
 ada_phrase = 'ada-phrase'
 spacy_emoji = 'space-emoji'
 spacy_phrase = 'spacy-phrase'
-test_model = ada_emoji
-# Write output to cached files
-cached_palette_map = 'apps/static/dance/ai/model/' + test_model + '/cached-palette-map-' + test_model + '.json'
-cached_background_map = 'apps/static/dance/ai/model/' + test_model + '/cached-background-map-' + test_model + '.json'
-cached_foreground_map = 'apps/static/dance/ai/model/' + test_model + '/cached-foreground-map-' + test_model + '.json'
+TEST_MODEL = ada_emoji
 
-with open(cached_palette_map, "w") as json_file:
-    json_file.write(json.dumps(palette_output))
+# Write output to cache files storing associations and used by client.
+cached_palette_associations = 'apps/static/dance/ai/model/' + TEST_MODEL + '/cached-palette-map-' + TEST_MODEL + '.json'
+cached_background_associations = 'apps/static/dance/ai/model/' + TEST_MODEL + '/cached-background-map-' + TEST_MODEL + '.json'
+cached_foreground_associations = 'apps/static/dance/ai/model/' + TEST_MODEL + '/cached-foreground-map-' + TEST_MODEL + '.json'
 
-with open(cached_background_map, "w") as json_file:
-    json_file.write(json.dumps(background_output))
+with open(cached_palette_associations, "w") as json_file:
+    json_file.write(json.dumps(association_output_map[PALETTES]))
+
+with open(cached_background_associations, "w") as json_file:
+    json_file.write(json.dumps(association_output_map[BACKGROUND_EFFECTS]))
     
-with open(cached_foreground_map, "w") as json_file:
-    json_file.write(json.dumps(foreground_output))
+with open(cached_foreground_associations, "w") as json_file:
+    json_file.write(json.dumps(association_output_map[FOREGROUND_EFFECTS]))
 
 # Below are background effects, palettes and foreground effects in the order they appear in the dropdowns.
 # When this file is run and the three cached data files are replaced in apps/static/dance/ai/model,
