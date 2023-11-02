cc776c56bfe9e8e242f1d2809243f998a0b25862
Erin Bond <erin.bond@code.org>
2023-11-02 15:27:42 -0400

    remove validation code

--- apps/src/code-studio/components/aiTutor/aiTutorPanel.jsx
+++ apps/src/code-studio/components/aiTutor/aiTutorPanel.jsx
@@ -2,23 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AITutorPanelContainer from '@cdo/apps/code-studio/components/aiTutor/aiTutorPanelContainer';
 import CompilationAssistant from './compilationAssistant';
-import ValidationAssistant from './validationAssistant';
 
 export default class AITutorPanel extends React.Component {
   static propTypes = {
     levelType: PropTypes.string,
-    isLevelValidated: PropTypes.bool,
   };
 
   render() {
-    const {levelType, isLevelValidated} = this.props;
+    const {levelType} = this.props;
     const isCodingLevel = levelType === 'Javalab';
 
     return (
       <AITutorPanelContainer>
         <h3 id="ai_tutor_panel">AI Tutor</h3>
         {isCodingLevel && <CompilationAssistant />}
-        {isLevelValidated && <ValidationAssistant />}
       </AITutorPanelContainer>
     );
   }
