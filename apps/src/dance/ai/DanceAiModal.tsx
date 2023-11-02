d4a88c9cecb4e9ad15fbd7496dfb37c7ea163368
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2023-11-02 13:59:03 -0700

    Dance AI: use emoji characters as button text (#54520)
    * Dance AI: use emoji characters as button text
    
    * common component for emoji icon
    
    * cleanup
    
    * rename A.I. Bot -> A.I.

--- apps/src/dance/ai/DanceAiModal.tsx
+++ apps/src/dance/ai/DanceAiModal.tsx
@@ -14,9 +14,9 @@ import AiBlockPreview from './AiBlockPreview';
 import AiExplanationView from './AiExplanationView';
 import {AiOutput, FieldKey} from '../types';
 import {generateBlocks, generateBlocksFromResult, getLabelMap} from './utils';
-
-const aiBotBorder = require('@cdo/static/dance/ai/ai-bot-border.png');
-const aiBotBeam = require('@cdo/static/dance/ai/blue-scanner.png');
+import inputLibraryJson from '@cdo/static/dance/ai/ai-inputs.json';
+import aiBotBorder from '@cdo/static/dance/ai/ai-bot-border.png';
+import aiBotBeam from '@cdo/static/dance/ai/blue-scanner.png';
 
 enum Mode {
   SELECT_INPUTS = 'selectInputs',
@@ -30,26 +30,21 @@ enum Mode {
 type AiModalItem = {
   id: string;
   name: string;
-};
-
-type AiModalReturnedItem = {
-  id: string;
-  name: string;
-  url: string;
-  available: boolean;
+  emoji: string;
 };
 
 interface DanceAiProps {
   onClose: () => void;
 }
 
+const getImageUrl = (id: string) => {
+  return `/blockly/media/dance/ai/emoji/${id}.svg`;
+};
+
 const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
   const dispatch = useAppDispatch();
 
   const SLOT_COUNT = 3;
-
-  const inputLibraryFilename = 'ai-inputs';
-  const inputLibrary = require(`@cdo/static/dance/ai/${inputLibraryFilename}.json`);
 
   const [mode, setMode] = useState(Mode.SELECT_INPUTS);
   const [currentInputSlot, setCurrentInputSlot] = useState(0);
@@ -108,30 +103,21 @@ const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
     };
   };
 
-  const getImageUrl = (id: string) => {
-    return `/blockly/media/dance/ai/emoji/${id}.svg`;
-  };
-
   const getAllItems = (slotIndex: number) => {
     if (slotIndex >= SLOT_COUNT) {
       return [];
     }
 
-    return inputLibrary.items.map((item: AiModalItem) => {
+    return inputLibraryJson.items.map(item => {
       return {
-        id: item.id,
-        name: item.name,
-        url: getImageUrl(item.id),
+        ...item,
         available: !inputs.includes(item.id),
       };
     });
   };
 
-  const getItemName = (id: string) => {
-    return inputLibrary.items.find(
-      (item: AiModalReturnedItem) => item.id === id
-    )?.name;
-  };
+  const getItem = (id: string) =>
+    inputLibraryJson.items.find(item => item.id === id);
 
   const handleItemClick = (id: string) => {
     if (currentInputSlot < SLOT_COUNT) {
@@ -289,20 +275,22 @@ const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
       initialFocus={false}
     >
       <div id="ai-modal-header-area" className={moduleStyles.headerArea}>
-        <img src={aiBotBorder} className={moduleStyles.botImage} />
+        <img src={aiBotBorder} className={moduleStyles.botImage} alt="A.I." />
         generate &nbsp;
         <div className={moduleStyles.inputsContainer}>
-          {Array.from(Array(SLOT_COUNT).keys()).map(index => (
-            <div
-              key={index}
-              style={{
-                backgroundImage:
-                  inputs[index] && `url(${getImageUrl(inputs[index])}`,
-              }}
-              className={moduleStyles.emojiSlot}
-              title={getItemName(inputs[index])}
-            />
-          ))}
+          {Array.from(Array(SLOT_COUNT).keys()).map(index => {
+            const item = getItem(inputs[index]);
+            return (
+              <div key={index} className={moduleStyles.emojiSlot}>
+                {item && (
+                  <EmojiIcon
+                    item={item}
+                    className={moduleStyles.emojiSlotIcon}
+                  />
+                )}
+              </div>
+            );
+          })}
         </div>
         &nbsp; effects
         <div
@@ -366,25 +354,18 @@ const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
         >
           {mode === Mode.SELECT_INPUTS && currentInputSlot < SLOT_COUNT && (
             <div className={moduleStyles.itemContainer}>
-              {getAllItems(currentInputSlot).map(
-                (item: AiModalReturnedItem) => {
-                  return (
-                    <button
-                      type={'button'}
-                      key={item.id}
-                      onClick={() => item.available && handleItemClick(item.id)}
-                      style={{
-                        backgroundImage: `url(${item.url})`,
-                      }}
-                      className={classNames(
-                        moduleStyles.item,
-                        item.available && moduleStyles.itemAvailable
-                      )}
-                      title={item.name}
-                    />
-                  );
-                }
-              )}
+              {getAllItems(currentInputSlot).map(item => {
+                return (
+                  <EmojiIcon
+                    item={item}
+                    onClick={() => item.available && handleItemClick(item.id)}
+                    className={classNames(
+                      moduleStyles.item,
+                      item.available && moduleStyles.itemAvailable
+                    )}
+                  />
+                );
+              })}
             </div>
           )}
         </div>
@@ -401,6 +382,7 @@ const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
             (mode === Mode.PROCESSING && !processingDone)) && (
             <div className={moduleStyles.prompt}>
               {Array.from(Array(SLOT_COUNT).keys()).map(index => {
+                const item = getItem(inputs[index]);
                 return (
                   <div key={index} className={moduleStyles.inputContainer}>
                     {index === currentInputSlot && (
@@ -416,13 +398,10 @@ const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
                     )}
                     <div className={moduleStyles.inputBackground}>&nbsp;</div>
 
-                    {inputs[index] && (
-                      <div
-                        style={{
-                          backgroundImage: `url(${getImageUrl(inputs[index])}`,
-                        }}
+                    {item && (
+                      <EmojiIcon
+                        item={item}
                         className={moduleStyles.inputItem}
-                        title={getItemName(inputs[index])}
                       />
                     )}
                   </div>
@@ -461,8 +440,13 @@ const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
                       !processingDone &&
                       moduleStyles.beamImageVisible
                   )}
+                  alt="A.I. Beam"
                 />
-                <img src={aiBotBorder} className={moduleStyles.image} />
+                <img
+                  src={aiBotBorder}
+                  className={moduleStyles.image}
+                  alt="A.I."
+                />
               </div>
             </div>
           )}
@@ -604,6 +588,37 @@ const DanceAiModal: React.FunctionComponent<DanceAiProps> = ({onClose}) => {
         </div>
       </div>
     </AccessibleDialog>
+  );
+};
+
+interface EmojiIconProps {
+  item: AiModalItem;
+  onClick?: () => void;
+  className?: string;
+}
+
+const EmojiIcon: React.FunctionComponent<EmojiIconProps> = ({
+  item,
+  onClick,
+  className,
+}) => {
+  const isButton = onClick !== undefined;
+  const Tag = isButton ? 'button' : 'div';
+  return (
+    <Tag
+      type={isButton ? 'button' : undefined}
+      key={item.id}
+      onClick={onClick}
+      style={{
+        backgroundImage: `url(${getImageUrl(item.id)})`,
+      }}
+      className={classNames(
+        moduleStyles.emojiIcon,
+        isButton && moduleStyles.emojiIconButton,
+        className
+      )}
+      title={item.emoji}
+    />
   );
 };
 
