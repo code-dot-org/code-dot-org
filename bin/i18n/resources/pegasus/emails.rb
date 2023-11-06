eabc260e07abed16bc38118b0547f5578a92c318
Mario Gil Correa <mario.gil-correa@code.org>
2023-11-06 11:27:17 -0800

    changed location of synced-in emails

--- bin/i18n/resources/pegasus/emails.rb
+++ bin/i18n/resources/pegasus/emails.rb
@@ -6,8 +6,8 @@ module I18n
   module Resources
     module Pegasus
       module Emails
-        DIR_NAME = 'emails'.freeze
-        ORIGIN_DIR_PATH = CDO.dir("pegasus/#{DIR_NAME}").freeze
+        DIR_NAME = 'pegasus/emails'.freeze
+        ORIGIN_DIR_PATH = CDO.dir(DIR_NAME).freeze
         I18N_SOURCE_DIR_PATH = CDO.dir(I18N_SOURCE_DIR, DIR_NAME).freeze
 
         def self.sync_in
