d137be58e5ea8f4288d7766837c52d4c90e56689
Juan Manzo <juan.manzo@code.org>
2023-11-07 14:32:22 -0800

    Added tests for DCDO flag in announcements

--- dashboard/lib/announcements.rb
+++ dashboard/lib/announcements.rb
@@ -22,7 +22,7 @@ class Announcements
 
     banner = banners[banner_id_for_page]
 
-    #Returns banner if no DCDO flag or if DCDO flag is present and set to true
+    #Returns banner if no DCDO property or if DCDO property is present and set to true
     unless banner && banner["dcdo"] && !DCDO.get(banner["dcdo"], false)
       banner&.merge({id: banner_id_for_page})
     end
