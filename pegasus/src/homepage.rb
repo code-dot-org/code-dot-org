593324f775d4cfce59f4643ea2045ded3512ff4d
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2023-11-08 09:55:15 -0800

    update artists list

--- pegasus/src/homepage.rb
+++ pegasus/src/homepage.rb
@@ -579,11 +579,11 @@ class Homepage
   def self.get_dance_stars
     [
       "Beyoncé", "BTS", "Harry Styles", "Lizzo", "Post Malone", "Disney's \"Encanto\"", "Nicky Youre",
-      "Katy Perry", "Lil Nas X", "Jonas Brothers", "Panic! At The Disco",
+      "Katy Perry", "Lil Nas X", "Jonas Brothers", "FIFTY FIFTY", "Steve Lacy", "Panic! At The Disco",
       "Shawn Mendes", "Nicki Minaj", "Pedro Capó", "Francesco Gabbani", "Sia",
       "Ariana Grande", "Avicii and Aloe Blacc", "Calvin Harris",
       "Carly Rae Jepsen", "Coldplay", "Ed Sheeran", "Imagine Dragons",
-      "J Balvin and Willy William", "Justin Bieber", "Keith Urban", "Lady Gaga",
+      "J Balvin and Willy William", "Justin Bieber", "Lady Gaga",
       "Los del Río", "Madonna", "Mark Ronson (ft. Bruno Mars)", "MC Hammer",
       "Miley Cyrus", "Selena Gomez", "The Weeknd", "Yolanda Be Cool"
     ]
