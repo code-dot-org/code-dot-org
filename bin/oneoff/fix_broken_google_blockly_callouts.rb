5b72769291eb4e3cdc2336831f98214a40ac3159
molly-moen <molly@code.org>
2023-11-08 15:07:40 -0800

    update to keep old ids

--- bin/oneoff/fix_broken_google_blockly_callouts.rb
+++ bin/oneoff/fix_broken_google_blockly_callouts.rb
@@ -142,9 +142,11 @@ def add_ids_to_blocks(ids_to_add, level, broken_callouts)
   toolbox_xml = Nokogiri::XML(level.toolbox_blocks)
   start_blocks_xml = Nokogiri::XML(level.start_blocks)
   long_instructions_block_count = level.long_instructions.scan('<block').count
-  # Remove all ids so we do not have any duplicate ids in the xml. Old ids are not used for anything.
-  clear_ids_from_blocks(toolbox_xml.xpath('//*'))
-  clear_ids_from_blocks(start_blocks_xml.xpath('//*'))
+  existing_ids = []
+  # Find all existing ids on blocks. If the id we are looking for already exists, don't add it.
+  find_existing_ids(toolbox_xml.xpath('//*'), existing_ids)
+  find_existing_ids(start_blocks_xml.xpath('//*'), existing_ids)
+  ids_to_add = ids_to_add.difference(existing_ids)
   # This xpath syntax ignores namespaces, which some xml nodes use.
   toolbox_blocks = toolbox_xml.xpath("*[local-name()='xml']//*[local-name()='block']")
   start_blocks = start_blocks_xml.xpath("*[local-name()='xml']//*[local-name()='block']")
@@ -155,7 +157,11 @@ def add_ids_to_blocks(ids_to_add, level, broken_callouts)
     is_toolbox_block = callout_data[:new_element_id].include? 'blocklyFlyout'
     if is_toolbox_block
       if toolbox_blocks.length <= index
-        puts "#{level.name} INVALID TOOLBOX INDEX #{id_to_add}, toolbox blocks length is #{toolbox_blocks.length}"
+        puts "#{level.name} INVALID TOOLBOX INDEX #{id_to_add}"
+        could_match = false
+      elsif toolbox_blocks[index]['id']
+        # If the block already has an id, we can't add a new one. Fail this update.
+        puts "#{level.name} Toolbox block with index #{id_to_add} already has id #{toolbox_blocks[index]['id']}"
         could_match = false
       else
         toolbox_blocks[index]['id'] = id_to_add
@@ -163,8 +169,11 @@ def add_ids_to_blocks(ids_to_add, level, broken_callouts)
     else # Otherwise it's a start block.
       index = id_to_add.to_i - toolbox_blocks.length - 1 - long_instructions_block_count
       if start_blocks.length <= index
-        puts "#{level.name} INVALID START_BLOCKS INDEX #{id_to_add}, index is #{index}, toolbox blocks length is #{toolbox_blocks.length} start blocks length is #{start_blocks.length}"
-        puts "number of long instructions blocks: #{long_instructions_block_count}"
+        puts "#{level.name} INVALID START_BLOCKS INDEX #{id_to_add}"
+        could_match = false
+      elsif start_blocks[index]['id']
+        # If the block already has an id, we can't add a new one. Fail this update.
+        puts "#{level.name} Start block with index #{id_to_add} already has id #{toolbox_blocks[index]['id']}"
         could_match = false
       else
         start_blocks[index]['id'] = id_to_add
@@ -179,9 +188,11 @@ def add_ids_to_blocks(ids_to_add, level, broken_callouts)
   return false
 end
 
-def clear_ids_from_blocks(blocks)
+def find_existing_ids(blocks, ids)
   blocks.each do |block|
-    block.remove_attribute('id')
+    if block['id']
+      ids << block['id']
+    end
   end
 end
 
