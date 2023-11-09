8dcfea3c56f04663a45bd27b9e7956f8a3d0ae88
Erin Bond <erin.bond@code.org>
2023-11-08 21:36:53 -0500

    add ai bot image, tweak a few styles

--- apps/src/code-studio/components/aiTutor/aiTutorPanel.jsx
+++ apps/src/code-studio/components/aiTutor/aiTutorPanel.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import i18n from '@cdo/locale';
+import style from './ai-tutor.module.scss';
 import AITutorPanelContainer from '@cdo/apps/code-studio/components/aiTutor/aiTutorPanelContainer';
 import CompilationAssistant from './compilationAssistant';
+const icon = require('@cdo/static/ai-bot.png');
 
 export default class AITutorPanel extends React.Component {
   static propTypes = {
@@ -13,8 +16,9 @@ export default class AITutorPanel extends React.Component {
     const isCodingLevel = levelType === 'Javalab';
 
     return (
-      <AITutorPanelContainer>
+      <AITutorPanelContainer className={style.aiTutorContainer}>
         <h3 id="ai_tutor_panel">AI Tutor</h3>
+        <img alt={i18n.aiBot()} src={icon} className={style.aiBotImg} />
         {isCodingLevel && <CompilationAssistant />}
       </AITutorPanelContainer>
     );
