8a95764e0bdeaf89449d6aa41903106316d4addb
Carl Reiner <carl.reiner@code.org>
2023-11-14 13:21:47 -0800

    add soft deletion to LTI sections

--- dashboard/app/models/lti_section.rb
+++ dashboard/app/models/lti_section.rb
@@ -8,13 +8,16 @@
 #  lms_section_id :string(255)
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
+#  deleted_at     :datetime
 #
 # Indexes
 #
+#  index_lti_sections_on_deleted_at     (deleted_at)
 #  index_lti_sections_on_lti_course_id  (lti_course_id)
 #  index_lti_sections_on_section_id     (section_id)
 #
 class LtiSection < ApplicationRecord
+  acts_as_paranoid
   belongs_to :lti_course
   belongs_to :section
   validates :section_id, uniqueness: true
