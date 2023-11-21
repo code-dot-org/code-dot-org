140539c65be8261927d769ae747b297ebf778c1d
Nick Lathe <nick.lathe@code.org>
2023-11-21 10:30:08 -0800

    Refactor Services::Lti.initialize_lti_user
      - Add support to handle root level values as well as values nested in
        custom claims
    
    Signed-off-by: Nick Lathe <nick.lathe@code.org>

--- dashboard/lib/services/lti.rb
+++ dashboard/lib/services/lti.rb
@@ -4,24 +4,30 @@ require 'authentication_option'
 
 class Services::Lti
   def self.initialize_lti_user(id_token)
-    custom_claims = id_token[Policies::Lti::LTI_CUSTOM_CLAIMS]
     user_type = Policies::Lti.get_account_type(id_token)
+    teacher_keys = [:name, :display_name, :full_name, :family_name, :given_name]
+    student_keys = [:name, :display_name, :full_name, :given_name]
+
     user = User.new
     user.provider = User::PROVIDER_MIGRATED
     user.user_type = user_type
     if user_type == User::TYPE_TEACHER
       user.age = '21+'
-      user.name = custom_claims[:display_name] || custom_claims[:full_name] || custom_claims[:family_name] || custom_claims[:given_name]
+      user.name = teacher_keys.filter_map {|key| get_value(id_token, key)}.compact.first
     else
-      user.name = custom_claims[:display_name] || custom_claims[:full_name] || custom_claims[:given_name]
-      user.family_name = custom_claims[:family_name]
+      user.name = student_keys.filter_map {|key| get_value(id_token, key)}.compact.first
+      user.family_name = get_value(id_token, :family_name)
     end
     ao = AuthenticationOption.new(
       authentication_id: Policies::Lti.generate_auth_id(id_token),
       credential_type: AuthenticationOption::LTI_V1,
-      email: custom_claims[:email],
+      email: get_value(id_token, :email),
     )
     user.authentication_options = [ao]
     user
+  end
+
+  def self.get_value(id_token, key)
+    id_token[key] || id_token.dig(Policies::Lti::LTI_CUSTOM_CLAIMS, key)
   end
 end
