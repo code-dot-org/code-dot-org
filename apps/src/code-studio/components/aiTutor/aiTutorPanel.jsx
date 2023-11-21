2fa0ba421cea9587b5f986709487af45f24b7149
Erin Bond <erin.bond@code.org>
2023-11-21 11:50:09 -0500

    switch checkboxes for radio buttons

--- apps/src/code-studio/components/aiTutor/aiTutorPanel.jsx
+++ apps/src/code-studio/components/aiTutor/aiTutorPanel.jsx
@@ -3,59 +3,49 @@ import PropTypes from 'prop-types';
 import {useDispatch} from 'react-redux';
 import i18n from '@cdo/locale';
 import style from './ai-tutor.module.scss';
-import Checkbox from '@cdo/apps/componentLibrary/checkbox';
 import AITutorPanelContainer from '@cdo/apps/code-studio/components/aiTutor/aiTutorPanelContainer';
 import CompilationTutor from './compilationTutor';
 import ValidationTutor from './validationTutor';
 import GeneralChatTutor from './generalChatTutor';
 import {addAIResponse} from '@cdo/apps/aiTutor/redux/aiTutorRedux';
+import {RadioButtonsGroup} from '@cdo/apps/componentLibrary/radioButton';
 const icon = require('@cdo/static/ai-bot.png');
 
 const AITutorPanel = ({level}) => {
   const dispatch = useDispatch();
   const isCodingLevel = level.type === 'Javalab';
 
-  const initialCheckboxes = [
+  const [selected, setSelected] = useState('');
+
+  const radioButtons = [
     {
-      key: 'compilation',
+      name: 'compilation',
       label: 'Code compilation',
-      checked: false,
-      hidden: !isCodingLevel,
+      value: 'compilation',
+      disabled: !isCodingLevel,
     },
     {
       key: 'validation',
       label: 'Failing tests',
-      checked: false,
-      hidden: !level.hasValidation,
+      value: 'validation',
+      disabled: !level.hasValidation,
     },
     {
       key: 'question',
       label: 'I have a question',
-      checked: false,
-      hidden: false,
+      value: 'question',
+      disabled: false,
     },
   ];
 
-  const [checkboxes, updateCheckboxes] = useState(initialCheckboxes);
-
-  const handleCheckboxChange = key => {
+  const onChange = event => {
     dispatch(addAIResponse(''));
-    const updatedCheckboxes = checkboxes.map(checkbox =>
-      checkbox.key === key
-        ? {...checkbox, checked: !checkbox.checked}
-        : {...checkbox, checked: false}
-    );
-    updateCheckboxes(updatedCheckboxes);
+    setSelected(event.target.value);
   };
 
-  const getChecked = checkboxes => {
-    return checkboxes.find(checkbox => checkbox.checked);
-  };
-
-  const checked = getChecked(checkboxes);
-  const compilationSelected = checked && checked.key === 'compilation';
-  const validationSelected = checked && checked.key === 'validation';
-  const questionSelected = checked && checked.key === 'question';
+  const compilationSelected = selected === 'compilation';
+  const validationSelected = selected === 'validation';
+  const questionSelected = selected === 'question';
 
   return (
     <AITutorPanelContainer level={level}>
@@ -63,17 +53,10 @@ const AITutorPanel = ({level}) => {
       <img alt={i18n.aiBot()} src={icon} className={style.aiBotImg} />
       <div>
         <h4> What would you like AI Tutor to help you with?</h4>
-        {checkboxes.map(
-          cb =>
-            !cb.hidden && (
-              <Checkbox
-                key={cb.key}
-                label={cb.label}
-                checked={cb.checked}
-                onChange={() => handleCheckboxChange(cb.key)}
-              />
-            )
-        )}
+        <RadioButtonsGroup
+          radioButtons={radioButtons}
+          onChange={() => onChange(event)}
+        />
       </div>
       {compilationSelected && <CompilationTutor levelId={level.id} />}
       {validationSelected && <ValidationTutor levelId={level.id} />}
