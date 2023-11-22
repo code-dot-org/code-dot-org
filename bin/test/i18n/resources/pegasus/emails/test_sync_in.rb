02464ae8d7666212d562d3e5c636f716da53fbe0
Turner Riley <turner.riley@code.org>
2023-11-22 13:44:36 -0800

    PR feedback

--- bin/test/i18n/resources/pegasus/emails/test_sync_in.rb
+++ bin/test/i18n/resources/pegasus/emails/test_sync_in.rb
@@ -2,8 +2,11 @@ require_relative '../../../../test_helper'
 require_relative '../../../../../i18n/resources/pegasus/emails/sync_in'
 
 describe I18n::Resources::Pegasus::Emails::SyncIn do
-  def around
-    FakeFS.with_fresh {yield}
+  let(:described_class) {I18n::Resources::Pegasus::Emails::SyncIn}
+  let(:described_instance) {described_class.new}
+
+  around do |test|
+    FakeFS.with_fresh {test.call}
   end
 
   before do
@@ -11,16 +14,12 @@ describe I18n::Resources::Pegasus::Emails::SyncIn do
     I18n::Utils::PegasusMarkdown.stubs(:sanitize_file_header)
   end
 
-  describe '.perform' do
-    it 'calls #process' do
-      I18n::Resources::Pegasus::Emails::SyncIn.any_instance.expects(:process).once
-
-      I18n::Resources::Pegasus::Emails::SyncIn.perform
-    end
+  it 'inherits from I18n::Utils::SyncInBase' do
+    assert_equal I18n::Utils::SyncInBase, described_class.superclass
   end
 
   describe '#process' do
-    let(:sync_in) {I18n::Resources::Pegasus::Emails::SyncIn.new}
+    let(:run_process) {described_instance.process}
 
     before do
       FileUtils.mkdir_p(File.dirname(origin_emails_file_path))
@@ -39,7 +38,7 @@ describe I18n::Resources::Pegasus::Emails::SyncIn do
           I18nScriptUtils.expects(:copy_file).with(origin_emails_file_path, expected_i18n_source_file_path).in_sequence(execution_sequence)
           I18n::Utils::PegasusMarkdown.expects(:sanitize_file_header).with(expected_i18n_source_file_path).in_sequence(execution_sequence)
 
-          sync_in.process
+          run_process
         end
       end
     end
