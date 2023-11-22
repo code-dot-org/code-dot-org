5a2032751d73c4176f27a3b9054acde9f71f092a
Turner Riley <turner.riley@code.org>
2023-11-22 13:44:36 -0800

    PR feedback

--- bin/i18n/resources/pegasus/emails/sync_out.rb
+++ bin/i18n/resources/pegasus/emails/sync_out.rb
@@ -29,12 +29,12 @@ module I18n
             Dir.glob('*.md', base: crowdin_locale_resource_dir) do |crowdin_file_subpath|
               # `hoc_signup_2015_receipt.md` => `hoc_signup_2015_receipt_es-ES.md`
               # `hoc_signup_2023_receipt_en.md` => `hoc_signup_2023_receipt_es-ES.md`
-              target_file_subpath = crowdin_file_subpath.sub(/(_en.md|.md)$/, "_#{locale}.md")
-              target_file_path = File.join(ORIGIN_DIR_PATH, 'i18n', target_file_subpath)
-
-              crowdin_file_path = File.join(crowdin_locale_resource_dir, crowdin_file_subpath)
               origin_markdown_file_path = File.join(ORIGIN_DIR_PATH, crowdin_file_subpath)
               next unless File.exist?(origin_markdown_file_path)
+
+              crowdin_file_path = File.join(crowdin_locale_resource_dir, crowdin_file_subpath)
+              target_file_subpath = crowdin_file_subpath.sub(/(_en.md|.md)$/, "_#{locale}.md")
+              target_file_path = File.join(ORIGIN_DIR_PATH, 'i18n', target_file_subpath)
 
               I18nScriptUtils.copy_file(crowdin_file_path, target_file_path)
               I18n::Utils::PegasusMarkdown.restore_file_header(origin_markdown_file_path, target_file_path)
