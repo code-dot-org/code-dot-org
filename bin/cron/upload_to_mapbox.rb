87ab563ad795101d08507701b41477550068469f
Turner Riley <turnertriley@gmail.com>
2023-11-27 15:52:44 -0800

    add more puts

--- bin/cron/upload_to_mapbox.rb
+++ bin/cron/upload_to_mapbox.rb
@@ -18,11 +18,14 @@ end
 # Returns response code
 def create_upload(config)
   CDO.log.info "Creating upload"
+  puts 'Mapbox upload token'
+  puts CDO.mapbox_upload_token
   RestClient.post("https://api.mapbox.com/uploads/v1/codeorg?access_token=#{CDO.mapbox_upload_token}", config.to_json, {content_type: :json, accept: :json}).code
 end
 
 # Return true if upload was successful
 def put_file_on_s3(tileset_path, credentials)
+  puts "Putting file on s3 method"
   CDO.log.info "Putting file on s3"
   s3_client = Aws::S3::Resource.new(
     access_key_id: credentials['accessKeyId'],
@@ -36,7 +39,11 @@ end
 
 # Return true if the upload completed without errors
 def upload_maptiles(tileset_path, tileset_name)
+  puts 'Tileset'
+  puts tileset_path
+  puts tileset_name
   credentials = create_upload_credentials
+  puts credentials
   return false unless put_file_on_s3(tileset_path, credentials)
   response_code = create_upload(
     {
@@ -45,5 +52,7 @@ def upload_maptiles(tileset_path, tileset_name)
       name: "codeorg.#{tileset_name}"
     }
   )
+  puts 'Past put file on s3'
+  puts response_code
   return [200, 201].include?(response_code)
 end
