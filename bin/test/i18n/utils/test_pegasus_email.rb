d8478bd4c318b1f307185bfd071cd552c3e6e809
Turner Riley <turnertriley@gmail.com>
2023-11-27 11:26:57 -0800

    simplify sanitize_header calls between pegasus_markdown and pegasus_email

---
+++ bin/test/i18n/utils/test_pegasus_email.rb
@@ -0,0 +1,14 @@
+require_relative '../../test_helper'
+require_relative '../../../i18n/utils/pegasus_markdown'
+
+describe I18n::Utils::PegasusEmail do
+  describe '.sanitize_header' do
+    subject {I18n::Utils::PegasusEmail.sanitize_header(markdown_header)}
+
+    let(:markdown_header) {{'subject' => 'Expects only subject', 'title' => 'Unexpected header', 'invalid' => 'Unexpected header'}}
+
+    it 'returns hash with only the `subject` key' do
+      assert_equal({'subject' => 'Expects only subject'}, subject)
+    end
+  end
+end
