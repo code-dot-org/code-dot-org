53a486314b17ec198b83852a5ab0b4aaf543394c
denyslevada <levada.denys@gmail.com>
2023-11-27 20:09:08 +0200

    wip

--- apps/src/componentLibrary/link/Link.story.tsx
+++ apps/src/componentLibrary/link/Link.story.tsx
@@ -1,52 +1,61 @@
-// TODO: Once start working on Link component - uncomment the code and use it as a template for Link component stories
-
-// import React from 'react';
-// import Link, {LinkProps} from './index';
-// import {Meta, Story} from '@storybook/react';
+import React from 'react';
+import Link, {LinkProps} from './index';
+import {Meta, Story} from '@storybook/react';
 // import {ComponentSizeXSToL} from '@cdo/apps/componentLibrary/common/types';
+
+export default {
+  title: 'DesignSystem/Link Component',
+  component: Link,
+} as Meta;
+
 //
-// export default {
-//   title: 'DesignSystem/Link Component',
-//   component: Link,
-// } as Meta;
+// TEMPLATE
 //
-// //
-// // TEMPLATE
-// //
-// // This is needed to fix children type error (passing string instead of React.ReactNode type)
-// // eslint-disable-next-line
-// const SingleTemplate: Story<LinkProps> = args => <Link {...args} />;
-//
-// const MultipleTemplate: Story<{
-//   components: LinkProps[];
-// }> = args => (
-//   <>
-//     {args.components?.map(componentArg => (
-//       // TODO: fix key
-//       <Link key={componentArg.size} {...componentArg} />
-//     ))}
-//   </>
-// );
-//
-// export const DefaultLink = SingleTemplate.bind({});
-// DefaultLink.args = {
-//   size: 'm',
-// };
-//
-// export const GroupOfSizesOfLinks = MultipleTemplate.bind({});
-// GroupOfSizesOfLinks.args = {
-//   components: [
-//     {
-//       size: 'xs',
-//     },
-//     {
-//       size: 's',
-//     },
-//     {
-//       size: 'm',
-//     },
-//     {
-//       size: 'l',
-//     },
-//   ],
-// };
+// This is needed to fix children type error (passing string instead of React.ReactNode type)
+// eslint-disable-next-line
+const SingleTemplate: Story<LinkProps> = args => <Link {...args} />;
+
+const MultipleTemplate: Story<{
+  components: LinkProps[];
+}> = args => (
+  <>
+    {args.components?.map(componentArg => (
+      // TODO: fix key
+      <Link key={componentArg.size} {...componentArg} />
+    ))}
+  </>
+);
+
+export const DefaultLink = SingleTemplate.bind({});
+DefaultLink.args = {
+  children: 'Default Link',
+  size: 'm',
+};
+
+export const GroupOfSizesOfLinks = MultipleTemplate.bind({});
+GroupOfSizesOfLinks.args = {
+  components: [
+    {
+      children: 'Link XS',
+      href: '#',
+      size: 'xs',
+    },
+    {
+      children: 'Link S',
+      href: '#',
+
+      size: 's',
+    },
+    {
+      children: 'Link M',
+      href: '#',
+
+      size: 'm',
+    },
+    {
+      children: 'Link L',
+      href: '#',
+      size: 'l',
+    },
+  ],
+};
--- apps/src/componentLibrary/link/Link.tsx
+++ apps/src/componentLibrary/link/Link.tsx
@@ -5,9 +5,28 @@ import {ComponentSizeXSToL} from '@cdo/apps/componentLibrary/common/types';
 import moduleStyles from './link.module.scss';
 
 export interface LinkProps {
+  /** Link content */
+  children: React.ReactNode;
+  /** Custom class name */
+  className?: string;
+  /** Does the link go to an external source? */
+  external?: boolean;
+  /** Link destination */
+  href: string;
+  /** Link id */
+  id?: string;
+  /** Callback for click event */
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+  /** Should the link open in a new tab? */
+  openInNewTab?: boolean;
   /** Size of link */
   size?: ComponentSizeXSToL;
 }
+
+// TODO:
+// - How to handle disabled state?
+// - Add storybook
+// - Add tests
 
 /**
  * ### Production-ready Checklist:
@@ -22,10 +41,31 @@ export interface LinkProps {
  * Design System: Link Component.
  * Can be used to render a checkbox or as a part of bigger/more complex components (e.g. Checkbox Dropdown).
  */
-const Link: React.FunctionComponent<LinkProps> = ({size = 'm'}) => {
+const Link: React.FunctionComponent<LinkProps> = ({
+  children,
+  className,
+  external,
+  href,
+  id,
+  onClick,
+  openInNewTab,
+  size = 'm',
+}) => {
   return (
-    <a className={classNames(moduleStyles.link, moduleStyles[`link-${size}`])}>
-      someText
+    // eslint-disable-next-line react/jsx-no-target-blank
+    <a
+      className={classNames(
+        moduleStyles.link,
+        moduleStyles[`link-${size}`],
+        className
+      )}
+      href={href}
+      id={id}
+      onClick={onClick}
+      rel={openInNewTab || external ? 'noopener noreferrer' : undefined}
+      target={openInNewTab ? '_blank' : undefined}
+    >
+      {children}
     </a>
   );
 };
