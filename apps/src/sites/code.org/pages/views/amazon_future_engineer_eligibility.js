174562b8222b516b445a2b7c967fa3f89b378825
Hannah Bergam <hannahbergam@gmail.com>
2023-11-27 10:55:39 -0800

    commenting out afe amplitude events

--- apps/src/sites/code.org/pages/views/amazon_future_engineer_eligibility.js
+++ apps/src/sites/code.org/pages/views/amazon_future_engineer_eligibility.js
@@ -1,6 +1,6 @@
 import firehoseClient from '@cdo/apps/lib/util/firehose';
-import analyticsReporter from '@cdo/apps/lib/util/AnalyticsReporter';
-import {EVENTS} from '@cdo/apps/lib/util/AnalyticsConstants';
+//import analyticsReporter from '@cdo/apps/lib/util/AnalyticsReporter';
+//import {EVENTS} from '@cdo/apps/lib/util/AnalyticsConstants';
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -29,7 +29,7 @@ function showAmazonFutureEngineerEligibility() {
         study: 'amazon-future-engineer-eligibility',
         event: 'start',
       });
-      analyticsReporter.sendEvent(EVENTS.AFE_START);
+      //analyticsReporter.sendEvent(EVENTS.AFE_START);
 
       amazonFutureEngineerEligibilityElements.each(
         (index, amazonFutureEngineerEligibilityElement) => {
