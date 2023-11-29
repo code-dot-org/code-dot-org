c8ca4f71e8ddcd4339795b05df7679b35c57a4a7
David Bailey <davidsbailey@users.noreply.github.com>
2023-11-29 10:47:45 -0800

    touch up TestAiProxyController and add unit test

--- dashboard/app/controllers/test_ai_proxy_controller.rb
+++ dashboard/app/controllers/test_ai_proxy_controller.rb
@@ -1,3 +1,5 @@
+# Provides a stub for the AI proxy API, for testing purposes. Routes here
+# should match the routes defined in https://github.com/code-dot-org/aiproxy
 class TestAiProxyController < ApplicationController
   layout false
 
@@ -6,6 +8,9 @@ class TestAiProxyController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   # POST /api/test/ai_proxy/assessment
+  #
+  # Provides a fake assessment where every key concept is given a score of
+  # Convincing Evidence.
   def assessment
     rubric_rows = CSV.parse(params[:rubric], headers: true).map(&:to_h)
     key_concepts = rubric_rows.map {|row| row['Key Concept']}
