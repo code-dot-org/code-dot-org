4edd1a46c60f49025ff5a695b768e211cd2ac145
Emily Eastlake <emily.eastlake@code.org>
2023-12-01 21:38:39 -0800

    [Google Blockly] CT-197: Correctly read userCreated flag (#55234)
    * add comment about string values
    
    * minor refactor in show for function
    
    * read userCreated property properly
    
    * use helper in cdoXml
    
    * refactor other usage of boolean
    
    * add jsdocs for helper function
    
    * update comment

--- apps/src/blockly/addons/cdoXml.js
+++ apps/src/blockly/addons/cdoXml.js
@@ -1,5 +1,6 @@
 import {PROCEDURE_DEFINITION_TYPES} from '../constants';
 import {partitionBlocksByType} from './cdoUtils';
+import {readBooleanAttribute} from '../utils';
 
 export default function initializeBlocklyXml(blocklyWrapper) {
   // Clear xml namespace
@@ -134,8 +135,7 @@ export function addMutationToBehaviorDefBlocks(blockElement) {
   // (e.g. shared behaviors).
   // In CDO Blockly, the 'usercreated' flag was set on the block. Google Blockly
   // expects this kind of extra state in a mutator.
-  const usercreatedAttribute = blockElement.getAttribute('usercreated');
-  const userCreated = usercreatedAttribute === 'true';
+  const userCreated = readBooleanAttribute(blockElement, 'usercreated');
   mutationElement.setAttribute('userCreated', userCreated);
 
   // In CDO Blockly, behavior ids were stored on the field. Google Blockly
