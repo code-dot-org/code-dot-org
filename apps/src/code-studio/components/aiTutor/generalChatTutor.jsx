87989bddea19899c11bf0497451803db46b3c690
Erin Bond <erin.bond@code.org>
2023-12-04 12:26:07 -0500

    use aiTutor state

--- apps/src/code-studio/components/aiTutor/generalChatTutor.jsx
+++ apps/src/code-studio/components/aiTutor/generalChatTutor.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import style from './ai-tutor.module.scss';
 import ChatWorkspace from './chatWorkspace';
-// import AichatView from '@cdo/apps/aichat/views/AichatView';
 
 // AI Tutor feature that allows students to ask general questions about the curriculum.
 const GeneralChatTutor = () => {
