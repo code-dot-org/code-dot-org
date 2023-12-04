434548e0bb979735b9c9d6b30349b517deb3be49
molly-moen <molly@code.org>
2023-12-04 13:28:06 -0800

    clean up

--- apps/src/pythonlab/PythonEditor.tsx
+++ apps/src/pythonlab/PythonEditor.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import PanelContainer from '@cdo/apps/lab2/views/components/PanelContainer';
 import classNames from 'classnames';
 import {EditorView, ViewUpdate} from '@codemirror/view';
