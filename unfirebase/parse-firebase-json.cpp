842be1c12be2c884152f9a2eafc30833a9ae2f7c
Seth Nickell <seth.nickell@code.org>
2023-12-05 00:15:07 -1000

    Lower transaction isolation for perf

--- unfirebase/parse-firebase-json.cpp
+++ unfirebase/parse-firebase-json.cpp
@@ -606,6 +606,7 @@ int main(int argc, char *argv[]) {
     stmt->execute("SET unique_checks=0;");
     stmt->execute("SET foreign_key_checks=0;");
     stmt->execute("SET sql_log_bin=0;");
+    stmt->execute("SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;");
     stmt->execute("COMMIT;");
 
     insertUnfirebaseStatement = db->prepareStatement("INSERT INTO `" + TABLE_NAME + "` VALUES (?, ?)");
