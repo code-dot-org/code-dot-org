a13aff078299843c5cc46215c6f342479ad72e51
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-01-04 08:34:41 +0200

    P20-626: Create Dashboard/Courses i18n sync-up script

--- bin/i18n/resources/dashboard/courses.rb
+++ bin/i18n/resources/dashboard/courses.rb
@@ -1,17 +1,15 @@
 require_relative '../../i18n_script_utils'
 require_relative '../dashboard'
 
-Dir[File.expand_path('../courses/**/*.rb', __FILE__)].sort.each {|file| require file}
-
 module I18n
   module Resources
     module Dashboard
       module Courses
         FILE_NAME = 'courses.yml'.freeze
 
-        ORIGIN_I18N_FILE_PATH = File.join(I18n::Resources::Dashboard::ORIGIN_I18N_DIR_PATH, 'courses.en.yml').freeze
-        I18N_SOURCE_FILE_PATH = File.join(I18n::Resources::Dashboard::I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
-        I18N_BACKUP_FILE_PATH = File.join(I18n::Resources::Dashboard::I18N_BACKUP_DIR_PATH, FILE_NAME).freeze
+        ORIGIN_I18N_FILE_PATH = File.join(ORIGIN_I18N_DIR_PATH, 'courses.en.yml').freeze
+        I18N_SOURCE_FILE_PATH = File.join(I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
+        I18N_BACKUP_FILE_PATH = File.join(I18N_BACKUP_DIR_PATH, FILE_NAME).freeze
 
         # Currently, csd is the only fully translatable course that has resources in this directory
         TRANSLATABLE_COURSES = %w[csd].freeze
@@ -22,6 +20,10 @@ module I18n
           SyncIn.perform
         end
 
+        def self.sync_up
+          SyncUp.perform
+        end
+
         def self.sync_out
           SyncOut.perform
         end
@@ -29,3 +31,5 @@ module I18n
     end
   end
 end
+
+Dir[File.expand_path('../courses/**/*.rb', __FILE__)].sort.each {|file| require file}
