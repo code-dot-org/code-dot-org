436e6534ad41f2b520a7d269eaf226a068cc3a6d
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-01-04 08:49:53 +0200

    P20-626: Create Dashboard/Scripts i18n sync-up script

--- bin/i18n/resources/dashboard/scripts.rb
+++ bin/i18n/resources/dashboard/scripts.rb
@@ -2,17 +2,15 @@ require_relative '../../i18n_script_utils'
 require_relative '../../redact_restore_utils'
 require_relative '../dashboard'
 
-Dir[File.expand_path('../scripts/**/*.rb', __FILE__)].sort.each {|file| require file}
-
 module I18n
   module Resources
     module Dashboard
       module Scripts
         FILE_NAME = 'scripts.yml'.freeze
 
-        ORIGIN_I18N_FILE_PATH = File.join(I18n::Resources::Dashboard::ORIGIN_I18N_DIR_PATH, 'scripts.en.yml')
-        I18N_SOURCE_FILE_PATH = File.join(I18n::Resources::Dashboard::I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
-        I18N_BACKUP_FILE_PATH = File.join(I18n::Resources::Dashboard::I18N_BACKUP_DIR_PATH, FILE_NAME).freeze
+        ORIGIN_I18N_FILE_PATH = File.join(ORIGIN_I18N_DIR_PATH, 'scripts.en.yml')
+        I18N_SOURCE_FILE_PATH = File.join(I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
+        I18N_BACKUP_FILE_PATH = File.join(I18N_BACKUP_DIR_PATH, FILE_NAME).freeze
 
         REDACTABLE_DATA_KEYS = %w[description student_description description_student description_teacher].freeze
         REDACT_PLUGINS = %w[resourceLink vocabularyDefinition].freeze
@@ -22,6 +20,10 @@ module I18n
           SyncIn.perform
         end
 
+        def self.sync_up
+          SyncUp.perform
+        end
+
         def self.sync_out
           SyncOut.perform
         end
@@ -29,3 +31,5 @@ module I18n
     end
   end
 end
+
+Dir[File.expand_path('../scripts/**/*.rb', __FILE__)].sort.each {|file| require file}
