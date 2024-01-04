8451f8666eb005664476a7cf0f3fc75cb62f8007
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-01-04 08:46:25 +0200

    P20-626: Create Dashboard/MarketingAnnouncements i18n sync-up script

--- bin/i18n/resources/dashboard/marketing_announcements.rb
+++ bin/i18n/resources/dashboard/marketing_announcements.rb
@@ -1,8 +1,6 @@
 require_relative '../../i18n_script_utils'
 require_relative '../dashboard'
 
-Dir[File.expand_path('../marketing_announcements/**/*.rb', __FILE__)].sort.each {|file| require file}
-
 module I18n
   module Resources
     module Dashboard
@@ -10,10 +8,14 @@ module I18n
         FILE_NAME = 'marketing_announcements.json'.freeze
 
         ORIGIN_I18N_FILE_PATH = CDO.dir('dashboard/config/marketing/announcements.json').freeze
-        I18N_SOURCE_FILE_PATH = File.join(I18n::Resources::Dashboard::I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
+        I18N_SOURCE_FILE_PATH = File.join(I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
 
         def self.sync_in
           SyncIn.perform
+        end
+
+        def self.sync_up
+          SyncUp.perform
         end
 
         def self.sync_out
@@ -23,3 +25,5 @@ module I18n
     end
   end
 end
+
+Dir[File.expand_path('../marketing_announcements/**/*.rb', __FILE__)].sort.each {|file| require file}
