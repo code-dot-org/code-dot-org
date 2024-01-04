22ac71a04f50b6350eef5df4e4fa217cc7d3160e
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-01-04 08:38:17 +0200

    P20-626: Create Dashboard/DataContent i18n sync-up script

--- bin/test/i18n/resources/dashboard/test_data_content.rb
+++ bin/test/i18n/resources/dashboard/test_data_content.rb
@@ -2,17 +2,26 @@ require_relative '../../../test_helper'
 require_relative '../../../../i18n/resources/dashboard/data_content'
 
 describe I18n::Resources::Dashboard::DataContent do
+  let(:described_class) {I18n::Resources::Dashboard::DataContent}
+
   describe '.sync_in' do
     it 'sync-in DataContent resource' do
-      I18n::Resources::Dashboard::DataContent::SyncIn.expects(:perform).once
-      I18n::Resources::Dashboard::DataContent.sync_in
+      described_class::SyncIn.expects(:perform).once
+      described_class.sync_in
+    end
+  end
+
+  describe '.sync_up' do
+    it 'sync-up DataContent resource' do
+      described_class::SyncUp.expects(:perform).once
+      described_class.sync_up
     end
   end
 
   describe '.sync_out' do
     it 'sync-out DataContent resource' do
-      I18n::Resources::Dashboard::DataContent::SyncOut.expects(:perform).once
-      I18n::Resources::Dashboard::DataContent.sync_out
+      described_class::SyncOut.expects(:perform).once
+      described_class.sync_out
     end
   end
 end
