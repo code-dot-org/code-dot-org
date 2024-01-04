a9bbbbfe39449ebc83f7124b90b4547531d1d87b
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-01-04 08:33:44 +0200

    P20-626: Create Dashboard/CourseOfferings i18n sync-up script

--- bin/i18n/resources/dashboard/course_offerings.rb
+++ bin/i18n/resources/dashboard/course_offerings.rb
@@ -1,17 +1,19 @@
 require_relative '../../i18n_script_utils'
 require_relative '../dashboard'
 
-Dir[File.expand_path('../course_offerings/**/*.rb', __FILE__)].sort.each {|file| require file}
-
 module I18n
   module Resources
     module Dashboard
       module CourseOfferings
         FILE_NAME = 'course_offerings.json'.freeze
-        I18N_SOURCE_FILE_PATH = File.join(I18n::Resources::Dashboard::I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
+        I18N_SOURCE_FILE_PATH = File.join(I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
 
         def self.sync_in
           SyncIn.perform
+        end
+
+        def self.sync_up
+          SyncUp.perform
         end
 
         def self.sync_out
@@ -21,3 +23,5 @@ module I18n
     end
   end
 end
+
+Dir[File.expand_path('../course_offerings/**/*.rb', __FILE__)].sort.each {|file| require file}
