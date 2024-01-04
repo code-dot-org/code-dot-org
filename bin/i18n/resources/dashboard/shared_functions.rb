2d6e9c8c9431ee9d498f29fbb137b15aa36ad8d5
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-01-04 08:51:07 +0200

    P20-626: Create Dashboard/SharedFunctions i18n sync-up script

--- bin/i18n/resources/dashboard/shared_functions.rb
+++ bin/i18n/resources/dashboard/shared_functions.rb
@@ -2,17 +2,19 @@ require_relative '../../../../dashboard/config/environment'
 require_relative '../../i18n_script_utils'
 require_relative '../dashboard'
 
-Dir[File.expand_path('../shared_functions/**/*.rb', __FILE__)].sort.each {|file| require file}
-
 module I18n
   module Resources
     module Dashboard
       module SharedFunctions
         FILE_NAME = 'shared_functions.yml'.freeze
-        I18N_SOURCE_FILE_PATH = File.join(I18n::Resources::Dashboard::I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
+        I18N_SOURCE_FILE_PATH = File.join(I18N_SOURCE_DIR_PATH, FILE_NAME).freeze
 
         def self.sync_in
           SyncIn.perform
+        end
+
+        def self.sync_up
+          SyncUp.perform
         end
 
         def self.sync_out
@@ -22,3 +24,5 @@ module I18n
     end
   end
 end
+
+Dir[File.expand_path('../shared_functions/**/*.rb', __FILE__)].sort.each {|file| require file}
