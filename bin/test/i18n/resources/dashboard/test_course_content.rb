63e85aa28d131ad66b2c7bc65378e98ecf7e839c
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-01-04 08:32:24 +0200

    P20-626: Create Dashboard/CourseContent i18n sync-up script

--- bin/test/i18n/resources/dashboard/test_course_content.rb
+++ bin/test/i18n/resources/dashboard/test_course_content.rb
@@ -2,17 +2,26 @@ require_relative '../../../test_helper'
 require_relative '../../../../i18n/resources/dashboard/course_content'
 
 describe I18n::Resources::Dashboard::CourseContent do
+  let(:described_class) {I18n::Resources::Dashboard::CourseContent}
+
   describe '.sync_in' do
     it 'sync-in Course content resource' do
-      I18n::Resources::Dashboard::CourseContent::SyncIn.expects(:perform).once
-      I18n::Resources::Dashboard::CourseContent.sync_in
+      described_class::SyncIn.expects(:perform).once
+      described_class.sync_in
+    end
+  end
+
+  describe '.sync_up' do
+    it 'sync-up Course resource' do
+      described_class::SyncUp.expects(:perform).once
+      described_class.sync_up
     end
   end
 
   describe '.sync_out' do
     it 'sync-out Course content resource' do
-      I18n::Resources::Dashboard::CourseContent::SyncOut.expects(:perform).once
-      I18n::Resources::Dashboard::CourseContent.sync_out
+      described_class::SyncOut.expects(:perform).once
+      described_class.sync_out
     end
   end
 end
