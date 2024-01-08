a782803ec994a775f71f3e6241d64c072360218c
Liam Frye-Mason <liam.fryemason@code.org>
2024-01-08 14:38:22 -0800

    hook up sort dropdown

--- apps/src/templates/sectionProgressV2/ProgressTableHeader.jsx
+++ apps/src/templates/sectionProgressV2/ProgressTableHeader.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SortByNameDropdown from '../SortByNameDropdown';
 
+const SECTION_PROGRESS_V2 = 'SectionProgressV2';
+
 export default function ProgressTableHeader() {
   return (
     <div>
       <div>Progress Header</div>
-      <SortByNameDropdown />
+      <SortByNameDropdown source={SECTION_PROGRESS_V2} />
     </div>
   );
 }
--- apps/src/templates/sectionProgressV2/ProgressTableV2.jsx
+++ apps/src/templates/sectionProgressV2/ProgressTableV2.jsx
@@ -6,26 +6,36 @@ import StudentColumn from './StudentColumn';
 import ProgressTableHeader from './ProgressTableHeader';
 import ProgressDataV2 from './ProgressDataV2';
 import styles from './progress-table-v2.module.scss';
+import stringKeyComparator from '@cdo/apps/util/stringKeyComparator';
 
-function ProgressTableV2({students}) {
+function ProgressTableV2({students, isSortedByFamilyName}) {
+  const sortedStudents = React.useMemo(() => {
+    const comparator = keys => stringKeyComparator(keys);
+    return isSortedByFamilyName
+      ? students.sort(comparator(['familyName', 'name']))
+      : students.sort(comparator(['name', 'familyName']));
+  }, [students, isSortedByFamilyName]);
+
   return (
     <div className={styles.progressTableV2}>
       <div>Progress Table V2</div>
       <ProgressTableHeader />
       <div className={styles.table}>
-        <StudentColumn sortedStudents={students} />
-        <ProgressDataV2 sortedStudents={students} />
+        <StudentColumn sortedStudents={sortedStudents} />
+        <ProgressDataV2 sortedStudents={sortedStudents} />
       </div>
     </div>
   );
 }
 
 ProgressTableV2.propTypes = {
+  isSortedByFamilyName: PropTypes.bool,
   students: PropTypes.arrayOf(studentShape),
 };
 
 export default connect(
   state => ({
+    isSortedByFamilyName: state.currentUser.isSortedByFamilyName,
     students: state.teacherSections.selectedStudents,
   }),
   dispatch => ({})
