a2f05b88e57776cce43d3e24a8b7ebd7e3f45b0a
Liam Frye-Mason <liam.fryemason@code.org>
2024-01-08 15:35:46 -0800

    Some styling

--- apps/src/templates/sectionProgressV2/ProgressTableHeader.jsx
+++ apps/src/templates/sectionProgressV2/ProgressTableHeader.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SortByNameDropdown from '../SortByNameDropdown';
+import styles from './progress-header.module.scss';
+import {Heading3} from '@cdo/apps/componentLibrary/typography';
 
 const SECTION_PROGRESS_V2 = 'SectionProgressV2';
 
 export default function ProgressTableHeader({unitName, sectionId}) {
   return (
-    <div>
-      <div>Students</div>
-      <SortByNameDropdown
-        sectionId={sectionId}
-        unitName={unitName}
-        source={SECTION_PROGRESS_V2}
-      />
+    <div className={styles.header}>
+      <div className={styles.sortDropdown}>
+        <Heading3>Students</Heading3>
+        <SortByNameDropdown
+          sectionId={sectionId}
+          unitName={unitName}
+          source={SECTION_PROGRESS_V2}
+        />
+      </div>
     </div>
   );
 }
--- apps/src/templates/sectionProgressV2/StudentColumn.jsx
+++ apps/src/templates/sectionProgressV2/StudentColumn.jsx
@@ -1,16 +1,30 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {studentShape} from '@cdo/apps/templates/teacherDashboard/teacherSectionsRedux';
+import styles from './progress-table-v2.module.scss';
+import classNames from 'classnames';
 
 export default function StudentColumn({sortedStudents}) {
+  const getFullName = student =>
+    student.familyName ? `${student.name} ${student.familyName}` : student.name;
+
+  const studentColumnBox = (student, ind) => {
+    return (
+      <div
+        className={classNames(styles.gridBox, styles.gridBoxStudent)}
+        key={ind}
+      >
+        {getFullName(student)}
+      </div>
+    );
+  };
+
   return (
     <div>
       <div>Student Column</div>
-      <ul>
-        {sortedStudents.map(student => (
-          <li key={student.id}>{student.name}</li>
-        ))}
-      </ul>
+      <div className={styles.grid}>
+        {sortedStudents.map((student, ind) => studentColumnBox(student, ind))}
+      </div>
     </div>
   );
 }
