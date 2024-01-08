591b17c336ef766104790fa0abf76bb7268f33ec
Liam Frye-Mason <liam.fryemason@code.org>
2024-01-08 14:47:11 -0800

    cleanup

--- apps/src/templates/sectionProgressV2/ProgressTableHeader.jsx
+++ apps/src/templates/sectionProgressV2/ProgressTableHeader.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SortByNameDropdown from '../SortByNameDropdown';
 
 const SECTION_PROGRESS_V2 = 'SectionProgressV2';
 
-export default function ProgressTableHeader() {
+export default function ProgressTableHeader({unitName, sectionId}) {
   return (
     <div>
-      <div>Progress Header</div>
-      <SortByNameDropdown source={SECTION_PROGRESS_V2} />
+      <div>Students</div>
+      <SortByNameDropdown
+        sectionId={sectionId}
+        unitName={unitName}
+        source={SECTION_PROGRESS_V2}
+      />
     </div>
   );
 }
+
+ProgressTableHeader.propTypes = {
+  sectionId: PropTypes.number,
+  unitName: PropTypes.string,
+};
