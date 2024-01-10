087aaa6350420801e5465e6ad95d358d3ca475e8
Erin Bond <erin.bond@code.org>
2024-01-10 15:01:27 -0500

    style messages based on content

--- dashboard/app/controllers/openai_chat_controller.rb
+++ dashboard/app/controllers/openai_chat_controller.rb
@@ -10,8 +10,9 @@ class OpenaiChatController < ApplicationController
 
     # Check for PII / Profanity
     locale = params[:locale] || "en"
-    messages = params[:messages].collect {|msg| msg[:content]}.join(' ')
-    filter_result = ShareFiltering.find_failure(messages, locale) if messages
+    # Just look at the most recent message from the student.
+    message = params[:messages].last[:content]
+    filter_result = ShareFiltering.find_failure(message, locale) if message
     # If the content is inappropriate, we skip sending to OpenAI and instead hardcode a warning response on the front-end.
     return render(status: :ok, json: {status: filter_result.type, flagged_content: filter_result.content}) if filter_result
 
