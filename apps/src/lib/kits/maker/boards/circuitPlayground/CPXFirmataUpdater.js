6ee72bb4bb23081f5cfe736342a62db9575c9108
Alice Fisher <alice.fisher@code.org>
2024-01-10 22:03:04 +0000

    try uint8array

--- apps/src/lib/kits/maker/boards/circuitPlayground/CPXFirmataUpdater.js
+++ apps/src/lib/kits/maker/boards/circuitPlayground/CPXFirmataUpdater.js
@@ -16,29 +16,31 @@ export default class CPXFirmataUpdater {
         },
       ],
     });
+    console.log('device', device);
     const result = await fetch(CIRCUIT_PLAYGROUND_EXPRESS_FIRMATA_URL);
 
     if (!result.ok) {
       throw new Error('Failed to download file');
     }
-    const resultText = await result.text();
-    console.log('resultText', resultText);
+    const resultArrayBuffer = await result.arrayBuffer();
     console.log('result', result);
+    console.log('resultArrayBuffer', resultArrayBuffer);
+    const resultArray = new Uint8Array(resultArrayBuffer);
+    console.log('resultArray', resultArray);
 
     const transport = new WebUSB(device);
     const target = new DAPLink(transport);
     console.log('transport', transport);
     console.log('target', target);
 
-    this.firmataUpdatePercent = 0;
     target.on(DAPLink.EVENT_PROGRESS, progress => {
-      this.setPercentUpdateComplete(progress);
+      console.log('progress', progress);
     });
     try {
       console.log('connect');
       await target.connect();
       console.log('flash');
-      await target.flash(resultText);
+      await target.flash(resultArray);
       await target.disconnect();
     } catch (error) {
       console.log(error);
