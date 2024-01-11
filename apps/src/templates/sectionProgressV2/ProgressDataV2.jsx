5ec487c55628f7c371a62404c77d8eacaefc4df3
Liam Frye-Mason <liam.fryemason@code.org>
2024-01-11 09:59:33 -0800

    Refactor for clarity

--- apps/src/templates/sectionProgressV2/ProgressDataV2.jsx
+++ apps/src/templates/sectionProgressV2/ProgressDataV2.jsx
@@ -3,49 +3,24 @@ import React from 'react';
 import {studentShape} from '@cdo/apps/templates/teacherDashboard/teacherSectionsRedux';
 import {studentLessonProgressType} from '../progress/progressTypes';
 import {connect} from 'react-redux';
-import {lessonHasLevels} from '../progress/progressHelpers';
 import styles from './progress-table-v2.module.scss';
-import classNames from 'classnames';
+import LessonDataCell from './LessonDataCell';
 
 function ProgressDataV2({sortedStudents, lessons, lessonProgressByStudent}) {
-  const lessonDataCell = React.useCallback(
-    (studentId, lesson) => {
-      if (!lessonHasLevels(lesson)) {
-        return (
-          <div
-            className={classNames(styles.gridBox, styles.gridBoxLessonHeader)}
-          >
-            nolev
-          </div>
-        );
-      }
-
-      const studentLessonProgress =
-        lessonProgressByStudent[studentId][lesson.id];
-      if (!studentLessonProgress) {
-        return (
-          <div
-            className={classNames(styles.gridBox, styles.gridBoxLessonHeader)}
-          />
-        );
-      }
-      return (
-        <div
-          key={studentId + '.' + lesson.id}
-          className={classNames(styles.gridBox, styles.gridBoxLessonHeader)}
-        >
-          {Math.round(studentLessonProgress.completedPercent)}%
-        </div>
-      );
-    },
-    [lessonProgressByStudent]
-  );
-
   return (
     <div className={styles.dataTable}>
       {lessons.map(lesson => (
         <div className={styles.dataColumn} key={lesson.id}>
-          {sortedStudents.map(student => lessonDataCell(student.id, lesson))}
+          {sortedStudents.map(student => (
+            <LessonDataCell
+              studentId={student.id}
+              lesson={lesson}
+              studentLessonProgress={
+                lessonProgressByStudent[student.id][lesson.id]
+              }
+              key={student.id + '.' + lesson.id}
+            />
+          ))}
         </div>
       ))}
     </div>
@@ -57,7 +32,7 @@ ProgressDataV2.propTypes = {
   lessonProgressByStudent: PropTypes.objectOf(
     PropTypes.objectOf(studentLessonProgressType)
   ).isRequired,
-  lessons: PropTypes.array.isRequired,
+  lessons: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default connect(state => ({
