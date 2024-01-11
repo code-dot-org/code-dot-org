441a60625e7519046efc2114d02c573ca80d9fd6
Seth Nickell <seth.nickell@code.org>
2024-01-10 19:54:58 -1000

    Delete KeyValuePair if setting value to null

--- dashboard/app/controllers/project_data_db_controller.rb
+++ dashboard/app/controllers/project_data_db_controller.rb
@@ -14,8 +14,27 @@ class ProjectDataDbController < ApplicationController
   end
 
   def set_key_value
-    kvp = KeyValuePair.create(channel_id: params[:channel_id], key: params[:key], value: params[:value])
-    render :json => kvp.as_json
+    value = JSON.parse params[:value]
+
+    if value.nil?
+      # Setting a key to null deletes it
+      KeyValuePair.delete_all(channel_id: params[:channel_id], key: params[:key])
+    else
+      # This should generate a single MySQL insert statement using the `ON DUPLICATE KEY UPDATE`
+      # syntax. Should be faster than a find round-trip followed by an update or insert.
+      # But we should check the SQL output to make sure its what we expect, since this is
+      # mainly designed Rails-wise as a bulk insert method.
+      KeyValuePair.upsert_all(
+        [
+          {channel_id: params[:channel_id], key: params[:key], value: value}
+        ]
+      )
+    end
+
+    # kvp = KeyValuePair.create(channel_id: params[:channel_id], key: params[:key], value: params[:value])
+    # render :json => kvp.as_json
+
+    render json: {key: params[:key], value: params[:value]}
   end
 
   def create_record
