a96710e8738f2535949c69f069694d7a26c34d28
Seth Nickell <seth.nickell@code.org>
2024-01-10 22:39:39 -1000

    Sketch out create_record call that needs only one round trip

--- dashboard/app/controllers/project_data_db_controller.rb
+++ dashboard/app/controllers/project_data_db_controller.rb
@@ -87,6 +87,26 @@ class ProjectDataDbController < ApplicationController
     render json: record
   end
 
+  # This would require MySQL option MULTI_STATEMENTS set on the connection, which
+  # has lots of pitfalls and isn't particularly well supported with the mysql2 gem
+  # See: https://github.com/rails/rails/issues/31569
+  #
+  # def create_record_one_round_trip
+  #   channel_id_quoted = Record.connection.quote(params[:channel_id])
+  #   table_name_quoted = Record.connection.quote(params[:table_name])
+  #   json_quoted  = Record.connection.quote JSON.parse params[:json]
+  #   record = Record.find_by_sql(<<-SQL
+  #     BEGIN;
+  #       SELECT MIN(record_id) FROM #{Record.table_name} WHERE channel_id=#{channel_id_quoted} AND table_name=#{table_name_quoted} LIMIT 1 FOR UPDATE;
+  #       SELECT @id := IFNULL(MAX(record_id),0)+1 FROM #{Record.table_name} WHERE channel_id=#{channel_id_quoted} AND table_name=#{table_name_quoted};
+  #       INSERT INTO #{Record.table_name} VALUES (#{channel_id_quoted}, #{table_name_quoted}, @id, #{json_quoted}});
+  #     END;
+  #     SELECT * FROM #{Record.table_name} WHERE channel_id=#{channel_id_quoted} AND table_name=#{table_name_quoted} AND record_id=@id;
+  #   SQL)
+
+  #   render json: record
+  # end
+
   def read_records
     records = Record.where(channel_id: params[:channel_id], table_name: params[:table_name])
 
