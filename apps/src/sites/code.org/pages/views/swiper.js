dd718ed4fdcc5162d4b42d4207fd1b8d97fcef32
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2024-01-12 14:17:39 -0800

    Add Swiper carousel library (#55719)
    * add swiper library
    
    * add swiper.js import file and corresponding view and entry point

---
+++ apps/src/sites/code.org/pages/views/swiper.js
@@ -0,0 +1,7 @@
+// This installs the Swiper library WebComponent for use across code.org.
+// See docs here: https://swiperjs.com/swiper-api#web-component.
+
+// import function to register Swiper custom elements
+import {register} from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
