a7e0b7ab2eb24bec7ceba023bb225271572b2d73
denyslevada <levada.denys@gmail.com>
2024-01-12 12:17:55 +0200

    add tests

--- apps/test/unit/componentLibrary/DropdownMenuTest.jsx
+++ apps/test/unit/componentLibrary/DropdownMenuTest.jsx
@@ -1,35 +1,28 @@
-// TODO: Uncomment and fix tests once ready
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import sinon from 'sinon';
+import userEvent from '@testing-library/user-event';
+import sinon from 'sinon';
 
 import {expect} from '../../util/reconfiguredChai';
 
 import Dropdown from '@cdo/apps/componentLibrary/dropdownMenu';
-//
-//
-// {
-//     name: 'default-dropdown',
-//         items: [
-//     {value: 'option-1', label: 'Option 1'},
-//     {value: 'option-2', label: 'Option 2'},
-// ],
-//     onChange: args => console.log(args),
-//     size: 'm',
-// }
-//
+
+const valuesMap = {};
+const onDropdownChange = (name, value) => (valuesMap[name] = value);
 
 describe('Design System - Dropdown Select Component', () => {
   it('Dropdown Select - renders with correct text and options', () => {
     render(
       <Dropdown
-        name="test-dropdown"
+        name="test1-dropdown"
         items={[
           {value: 'option-1', label: 'option1'},
           {value: 'option-2', label: 'option2'},
           {value: 'option-3', label: 'option3'},
         ]}
+        selectedValue={valuesMap['test1-dropdown']}
+        onChange={e => onDropdownChange('test1-dropdown', e.target.value)}
+        labelText="Dropdown label"
       />
     );
 
@@ -44,190 +37,109 @@ describe('Design System - Dropdown Select Component', () => {
     expect(option3).to.exist;
   });
 
-  // it('SemgentedButtons - changes selected button on click', async () => {
-  //   const user = userEvent.setup();
-  //   const spyOnChange = sinon.spy();
-  //   const onChange = value => {
-  //     onSegmentedButtonsChange('label', value);
-  //     spyOnChange(value);
-  //   };
-  //
-  //   const {rerender} = render(
-  //     <SegmentedButtons
-  //       selectedButtonValue="label"
-  //       buttons={[
-  //         {label: 'Label', value: 'label'},
-  //         {label: 'Label2', value: 'label-2'},
-  //       ]}
-  //       onChange={onChange}
-  //     />
-  //   );
-  //
-  //   let segmentedButton1 = screen.getByText('Label');
-  //   const segmentedButton2 = screen.getByText('Label2');
-  //
-  //   expect(segmentedButton1).to.exist;
-  //   expect(segmentedButton2).to.exist;
-  //   expect(valuesMap.label || 'label').to.equal('label');
-  //
-  //   await user.click(segmentedButton2);
-  //
-  //   // Re-render after user's first click
-  //   rerender(
-  //     <SegmentedButtons
-  //       selectedButtonValue="label"
-  //       buttons={[
-  //         {label: 'Label', value: 'label'},
-  //         {label: 'Label2', value: 'label-2'},
-  //       ]}
-  //       onChange={onChange}
-  //     />
-  //   );
-  //
-  //   segmentedButton1 = screen.getByText('Label');
-  //
-  //   expect(spyOnChange).to.have.been.calledOnce;
-  //   expect(spyOnChange).to.have.been.calledWith('label-2');
-  //   expect(valuesMap.label).to.equal('label-2');
-  //
-  //   await user.click(segmentedButton1);
-  //
-  //   // Re-render after user's second click
-  //   rerender(
-  //     <SegmentedButtons
-  //       selectedButtonValue="label"
-  //       buttons={[
-  //         {label: 'Label', value: 'label'},
-  //         {label: 'Label2', value: 'label-2'},
-  //       ]}
-  //       onChange={onChange}
-  //     />
-  //   );
-  //
-  //   expect(spyOnChange).to.have.been.calledTwice;
-  //   expect(spyOnChange).to.have.been.calledWith('label');
-  //   expect(valuesMap.label).to.equal('label');
-  // });
+  it('Dropdown Select - renders with correct text and options, changes selected value on when one is selected', async () => {
+    const user = userEvent.setup();
+    const spyOnChange = sinon.spy();
+    // set dropdown default value
+    onDropdownChange('test2-dropdown', '');
+    const onChange = e => {
+      onDropdownChange('test2-dropdown', e.target.value);
+      spyOnChange(e.target.value);
+    };
+    const DropdownToRender = () => (
+      <Dropdown
+        name="test2-dropdown"
+        items={[
+          {value: 'option-1', label: 'option1'},
+          {value: 'option-2', label: 'option2'},
+          {value: 'option-3', label: 'option3'},
+        ]}
+        selectedValue={valuesMap['test2-dropdown']}
+        onChange={onChange}
+        labelText="Dropdown2 label"
+      />
+    );
 
-  // it("SegmentedButtons - renders disabled button, doesn't change on click", async () => {
-  //   const user = userEvent.setup();
-  //   const spyOnChange = sinon.spy();
-  //   const onChange = value => {
-  //     onSegmentedButtonsChange('label', value);
-  //     spyOnChange(value);
-  //   };
-  //
-  //   // Initial render
-  //   const {rerender} = render(
-  //     <SegmentedButtons
-  //       selectedButtonValue="label"
-  //       buttons={[
-  //         {label: 'Label', value: 'label'},
-  //         {label: 'Label2', value: 'label-2', disabled: true},
-  //       ]}
-  //       onChange={onChange}
-  //     />
-  //   );
-  //
-  //   let segmentedButton1 = screen.getByText('Label');
-  //   const segmentedButton2 = screen.getByText('Label2');
-  //
-  //   expect(segmentedButton1).to.exist;
-  //   expect(segmentedButton2).to.exist;
-  //   expect(valuesMap.label || 'label').to.equal('label');
-  //
-  //   await user.click(segmentedButton2);
-  //
-  //   // Re-render after user's first click
-  //   rerender(
-  //     <SegmentedButtons
-  //       selectedButtonValue="label"
-  //       buttons={[
-  //         {label: 'Label', value: 'label'},
-  //         {label: 'Label2', value: 'label-2', disabled: true},
-  //       ]}
-  //       onChange={onChange}
-  //     />
-  //   );
-  //
-  //   segmentedButton1 = screen.getByText('Label');
-  //
-  //   expect(spyOnChange).to.not.have.been.called;
-  //   expect(valuesMap.label || 'label').to.equal('label');
-  //
-  //   await user.click(segmentedButton1);
-  //
-  //   // Re-render after user's second click
-  //   rerender(
-  //     <SegmentedButtons
-  //       selectedButtonValue="label"
-  //       buttons={[
-  //         {label: 'Label', value: 'label'},
-  //         {label: 'Label2', value: 'label-2', disabled: true},
-  //       ]}
-  //       onChange={onChange}
-  //     />
-  //   );
-  //
-  //   expect(spyOnChange).to.have.been.called.once;
-  //   expect(spyOnChange).to.have.been.calledWith('label');
-  // });
+    const {rerender} = render(<DropdownToRender />);
 
-  // it('Link - openInNewTab adds target attribute', () => {
-  //   render(
-  //     <Link href="https://studio.code.org/home" openInNewTab>
-  //       Home
-  //     </Link>
-  //   );
-  //
-  //   const link = screen.getByRole('link', {name: 'Home'});
-  //   expect(link).to.exist;
-  //   expect(link.target).to.equal('_blank');
-  //   expect(link.href).to.equal('https://studio.code.org/home');
-  // });
+    const label = screen.getByText('Dropdown2 label');
+    const selectElement = screen.getByRole('combobox');
+    const option1 = screen.getByText('option1');
+    const option2 = screen.getByText('option2');
 
-  // it('Link - external adds rel attribute', () => {
-  //   render(
-  //     <Link href="https://studio.code.org/home" external>
-  //       Home
-  //     </Link>
-  //   );
-  //
-  //   const link = screen.getByRole('link', {name: 'Home'});
-  //   expect(link).to.exist;
-  //   expect(link.rel).to.equal('noopener noreferrer');
-  //   expect(link.href).to.equal('https://studio.code.org/home');
-  // });
+    expect(label).to.exist;
+    expect(selectElement).to.exist;
+    expect(option1).to.exist;
+    expect(option2).to.exist;
+    expect(valuesMap['test2-dropdown']).to.equal('');
 
-  // it('Link - onClick is correctly called when clicked', async () => {
-  //   const user = userEvent.setup();
-  //   const spyOnClick = sinon.spy();
-  //
-  //   const linkToRender = <Link onClick={spyOnClick}>Home</Link>;
-  //
-  //   const {rerender} = render(linkToRender);
-  //
-  //   await user.click(screen.getByText('Home'));
-  //
-  //   rerender(linkToRender);
-  //
-  //   expect(spyOnClick).to.have.been.calledOnce;
-  // });
+    await user.selectOptions(selectElement, 'option-1');
 
-  // it('Link - doesn`t call onClick when disabled', async () => {
-  //   const user = userEvent.setup();
-  //   const spyOnClick = sinon.spy();
-  //   const linkToRender = (
-  //     <Link disabled onClick={spyOnClick}>
-  //       Home
-  //     </Link>
-  //   );
-  //   const {rerender} = render(linkToRender);
-  //
-  //   rerender(linkToRender);
-  //
-  //   await user.click(screen.getByText('Home'));
-  //
-  //   expect(spyOnClick).not.to.have.been.called;
-  // });
+    rerender(<DropdownToRender />);
+
+    expect(spyOnChange).to.have.been.calledOnce;
+    expect(spyOnChange).to.have.been.calledWith('option-1');
+    expect(valuesMap['test2-dropdown']).to.equal('option-1');
+
+    await user.selectOptions(selectElement, 'option-2');
+
+    rerender(<DropdownToRender />);
+
+    expect(spyOnChange).to.have.been.calledTwice;
+    expect(spyOnChange).to.have.been.calledWith('option-2');
+    expect(valuesMap['test2-dropdown']).to.equal('option-2');
+  });
+
+  it("Dropdown Select - renders disabled dropdown, doesn't change on click", async () => {
+    const user = userEvent.setup();
+    const spyOnChange = sinon.spy();
+    // set dropdown default value
+    onDropdownChange('test2-dropdown', '');
+    const onChange = e => {
+      onDropdownChange('test2-dropdown', e.target.value);
+      spyOnChange(e.target.value);
+    };
+
+    const DropdownToRender = () => (
+      <Dropdown
+        name="test2-dropdown"
+        disabled={true}
+        items={[
+          {value: 'option-1', label: 'option1'},
+          {value: 'option-2', label: 'option2'},
+          {value: 'option-3', label: 'option3'},
+        ]}
+        selectedValue={valuesMap['test2-dropdown']}
+        onChange={onChange}
+        labelText="Dropdown2 label"
+      />
+    );
+
+    const {rerender} = render(<DropdownToRender />);
+
+    const label = screen.getByText('Dropdown2 label');
+    const selectElement = screen.getByRole('combobox');
+    const option1 = screen.getByText('option1');
+    const option2 = screen.getByText('option2');
+
+    expect(label).to.exist;
+    expect(selectElement).to.exist;
+    expect(option1).to.exist;
+    expect(option2).to.exist;
+    expect(valuesMap['test2-dropdown']).to.equal('');
+
+    await user.selectOptions(selectElement, 'option-1');
+
+    rerender(<DropdownToRender />);
+
+    expect(spyOnChange).to.have.not.been.called;
+    expect(valuesMap['test2-dropdown']).to.equal('');
+
+    await user.selectOptions(selectElement, 'option-2');
+
+    rerender(<DropdownToRender />);
+
+    expect(spyOnChange).to.have.not.been.called;
+    expect(valuesMap['test2-dropdown']).to.equal('');
+  });
 });
