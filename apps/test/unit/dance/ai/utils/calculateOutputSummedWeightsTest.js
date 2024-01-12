31a7787d618532752aed59cbf902f6746cd1627c
bencodeorg <ben@code.org>
2024-01-12 15:50:35 -0800

    Dance AI: unit tests for choosing effects utils (#55726)
    * Test for calculateOutputSummedWeights
    
    * Passing chooseEffects test
    
    * Include effect name in weight, remove sorting util
    
    * Update comments, remove uneeded test data
    
    * Allow override for number of options, update comments
    
    * Update comment
    
    * Update comments, shorthand for allowing undefined in parameter
    
    * Stub util function
    
    * Remove numOptions param
    
    * Undo constant rename
    
    * Add more comments

---
+++ apps/test/unit/dance/ai/utils/calculateOutputSummedWeightsTest.js
@@ -0,0 +1,25 @@
+import {expect} from '../../../../util/reconfiguredChai';
+import {calculateOutputSummedWeights} from '@cdo/apps/dance/ai/utils/calculateOutputSummedWeights';
+
+const outputWeightsMapping = {
+  emojiAssociations: {
+    cyclone: [0.2, 0.3],
+    'smiling-face-with-hearts': [0.2, 0.3],
+    'party-popper': [0.1, 0.6],
+  },
+  output: ['quads', 'blooming_petals'],
+};
+
+describe('outputWeightsMapping', () => {
+  it('sums weights for each output given selected emojis', () => {
+    const summedWeights = calculateOutputSummedWeights(
+      ['cyclone', 'smiling-face-with-hearts', 'party-popper'],
+      outputWeightsMapping
+    );
+
+    expect(summedWeights[0][0]).to.be.closeTo(0.5, 0.00001);
+    expect(summedWeights[0][1]).to.equal('quads');
+    expect(summedWeights[1][0]).to.be.closeTo(1.2, 0.00001);
+    expect(summedWeights[1][1]).to.equal('blooming_petals');
+  });
+});
---
+++ apps/test/unit/dance/ai/utils/chooseEffectsTest.js
@@ -0,0 +1,50 @@
+import {expect} from '../../../../util/reconfiguredChai';
+import sinon from 'sinon';
+import {chooseEffects} from '@cdo/apps/dance/ai/utils/chooseEffects';
+import * as calculateOutputSummedWeights from '@cdo/apps/dance/ai/utils/calculateOutputSummedWeights';
+import {EffectsQuality} from '@cdo/apps/dance/ai/types';
+
+describe('chooseEffects', () => {
+  beforeEach(() => {
+    sinon.stub(Math, 'random').returns(0.99);
+    sinon
+      .stub(calculateOutputSummedWeights, 'calculateOutputSummedWeights')
+      .returns([
+        [2.9, 'blooming_petals (1st)'],
+        [1.2, 'quads (3rd)'],
+        [0.3, 'circles (4th)'],
+        [0.1, 'clouds (5th)'],
+        [2.1, 'color_cycle (2nd)'],
+      ]);
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('chooses a good effect', () => {
+    const chosenEffects = chooseEffects(
+      ['cyclone', 'smiling-face-with-hearts', 'party-popper'],
+      EffectsQuality.GOOD
+    );
+
+    // With Math.random stubbed to return 0.99 and NUM_RANDOM_TOP_OPTIONS set to 3,
+    // we select the third highest scoring effect.
+    expect(chosenEffects.backgroundEffect).to.equal('quads (3rd)');
+    expect(chosenEffects.foregroundEffect).to.equal('quads (3rd)');
+    expect(chosenEffects.backgroundColor).to.equal('quads (3rd)');
+  });
+
+  it('chooses a bad effect', () => {
+    const chosenEffects = chooseEffects(
+      ['cyclone', 'smiling-face-with-hearts', 'party-popper'],
+      EffectsQuality.BAD
+    );
+
+    // With Math.random stubbed to return 0.99 and NUM_RANDOM_BOTTOM_OPTIONS set to 20,
+    // we select the lowest scoring effect among the effects available (less than 20 in this case).
+    expect(chosenEffects.backgroundEffect).to.equal('clouds (5th)');
+    expect(chosenEffects.foregroundEffect).to.equal('clouds (5th)');
+    expect(chosenEffects.backgroundColor).to.equal('clouds (5th)');
+  });
+});
