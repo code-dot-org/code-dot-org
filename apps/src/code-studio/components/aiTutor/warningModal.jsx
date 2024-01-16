098f5eb2d9da8e22dec0bb4c655c5d64d73e4edc
Erin Bond <erin.bond@code.org>
2024-01-16 15:07:26 -0500

    put a placeholder button in the modal so there is something to focus on

--- apps/src/code-studio/components/aiTutor/warningModal.jsx
+++ apps/src/code-studio/components/aiTutor/warningModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AccessibleDialog from '@cdo/apps/templates/AccessibleDialog';
+import Button from '@cdo/apps/templates/Button';
 import {Heading3} from '@cdo/apps/componentLibrary/typography';
 
 const onClose = () => {
@@ -8,10 +9,11 @@ const onClose = () => {
 
 const WarningModal = () => {
   return (
-    <AccessibleDialog onClose={onClose} initialFocus={false}>
+    <AccessibleDialog onClose={onClose}>
       <div>
         <Heading3>I'm rendering the warning modal!</Heading3>
       </div>
+      <Button onClick={onClose} text="Click Me!" />
     </AccessibleDialog>
   );
 };
