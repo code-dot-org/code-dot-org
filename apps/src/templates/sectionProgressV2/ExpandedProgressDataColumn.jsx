55c554aa2a902f625972f6752cd67b737f9f259c
Liam Frye-Mason <liam.fryemason@code.org>
2024-01-18 14:38:53 -0800

    [Progress] Refactor progress V2 to render in columns (#55809)
    * Refactor to have columns
    
    * Add tests
    
    * Small fixes
    
    * Remove styles
    
    * CR comment
    
    * Fix
    
    * Fix cr comment
    
    * Fix test

---
+++ apps/src/templates/sectionProgressV2/ExpandedProgressDataColumn.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {studentShape} from '../teacherDashboard/teacherSectionsRedux';
+import {studentLessonProgressType} from '../progress/progressTypes';
+import {connect} from 'react-redux';
+
+// TODO fill out later
+function ExpandedProgressDataColumn({
+  lesson,
+  lessonProgressByStudent,
+  sortedStudents,
+  removeExpandedLesson,
+}) {
+  return <div>{lesson.id}</div>;
+}
+
+ExpandedProgressDataColumn.propTypes = {
+  sortedStudents: PropTypes.arrayOf(studentShape),
+  lessonProgressByStudent: PropTypes.objectOf(
+    PropTypes.objectOf(studentLessonProgressType)
+  ).isRequired,
+  lesson: PropTypes.object.isRequired,
+  removeExpandedLesson: PropTypes.func.isRequired,
+};
+
+export default connect(state => ({
+  lessonProgressByStudent:
+    state.sectionProgress.studentLessonProgressByUnit[
+      state.unitSelection.scriptId
+    ],
+}))(ExpandedProgressDataColumn);
