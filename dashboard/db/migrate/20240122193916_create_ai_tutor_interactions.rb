274cb6db5e626edf8021fb095b18646a192bf3e4
Erin Bond <erin.bond@code.org>
2024-01-22 14:58:07 -0500

    migration to create ai_tutor_interactions table

---
+++ dashboard/db/migrate/20240122193916_create_ai_tutor_interactions.rb
@@ -0,0 +1,22 @@
+class CreateAiTutorInteractions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ai_tutor_interactions do |t|
+      t.integer :user_id, null: false, foreign_key: true
+      t.integer :level_id, foreign_key: true
+      t.integer :script_id, foreign_key: true
+      t.string :ai_model_version
+      t.string :type
+      t.string :project_id
+      t.string :project_version_id
+      t.text :prompt
+      t.string :status
+      t.text :ai_response
+
+      t.timestamps
+    end
+
+    add_index :ai_tutor_interactions, :user_id
+    add_index :ai_tutor_interactions, :level_id
+    add_index :ai_tutor_interactions, :script_id
+  end
+end
