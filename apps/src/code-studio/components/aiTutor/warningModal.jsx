e3e0c038a23570be0a18d6edc35090e286391321
Ken Akiha <95503833+kakiha11@users.noreply.github.com>
2024-01-23 12:43:45 -0800

    Style added and open/close functionality added via both buttons on AI Tutor Warning Modal

--- apps/src/code-studio/components/aiTutor/warningModal.jsx
+++ apps/src/code-studio/components/aiTutor/warningModal.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+import style from './ai-tutor.module.scss';
+import {
+  BodyTwoText,
+  Heading2,
+  StrongText,
+} from '@cdo/apps/componentLibrary/typography';
 import AccessibleDialog from '@cdo/apps/templates/AccessibleDialog';
-import {Heading3} from '@cdo/apps/componentLibrary/typography';
-
-const onClose = () => {
-  console.log('Closed!');
-};
+import Button from '@cdo/apps/templates/Button';
 
 const WarningModal = () => {
+  const [open, setOpen] = useState(true);
+  console.log(open);
+
+  const onClose = () => {
+    setOpen(false);
+  };
   return (
-    <AccessibleDialog onClose={onClose} initialFocus={false}>
-      <div>
-        <Heading3>I'm rendering the warning modal!</Heading3>
-      </div>
-    </AccessibleDialog>
+    open && (
+      <AccessibleDialog onClose={onClose} className={style.chatWarningModal}>
+        <Heading2>Remember to chat responsibly!</Heading2>
+
+        <button type="button" onClick={onClose} className={style.xCloseButton}>
+          <i id="x-close" className="fa-solid fa-xmark" />
+        </button>
+        <hr />
+        <BodyTwoText>
+          <StrongText>All messages sent to AI Tutor are recorded</StrongText>
+        </BodyTwoText>
+        <BodyTwoText>
+          Inappropriate messages will be flagged and your teacher will be
+          notified.
+        </BodyTwoText>
+        <br />
+        <BodyTwoText>
+          In order to protect your privacy, anything considered personal
+          information (name, phone number, address, etc.) should not be
+          submitted to AI Tutor.
+        </BodyTwoText>
+        <hr />
+        <div className={style.bottomSection}>
+          <Button
+            onClick={onClose}
+            color={Button.ButtonColor.brandSecondaryDefault}
+            text="Ask AI Tutor"
+          />
+        </div>
+      </AccessibleDialog>
+    )
   );
 };
 
