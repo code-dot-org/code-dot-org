be71b85bb3b1ca6b96737b61113d6019b6cc7eb0
Eric Aderhold <eric.aderhold@code.org>
2024-01-22 16:43:53 -0800

    Harmonize markup and styling with Figma

--- apps/src/templates/sectionProgressV2/TeacherActionsBox.jsx
+++ apps/src/templates/sectionProgressV2/TeacherActionsBox.jsx
@@ -6,55 +6,37 @@ import styles from './section-progress-refresh.module.scss';
 import {StrongText} from '@cdo/apps/componentLibrary/typography';
 
 export default function TeacherActionsBox({isViewingLevelProgress}) {
-  const legendIcons = () => {
-    if (isViewingLevelProgress) {
-      return (
-        <div className={styles.icons}>
-          <div className={styles.legendColumn}>
-            <div className={styles.legendItemContainer}>
-              <LegendItem
-                itemType={ITEM_TYPE.NEEDS_FEEDBACK}
-                labelText={i18n.needsFeedback()}
-              />
-            </div>
-            <div className={styles.legendItemContainer}>
-              <LegendItem
-                itemType={ITEM_TYPE.VIEWED}
-                labelText={i18n.viewed()}
-              />
-            </div>
-          </div>
-          <div className={styles.legendColumn}>
-            <div className={styles.legendItemContainer}>
-              <LegendItem
-                itemType={ITEM_TYPE.FEEDBACK_GIVEN}
-                labelText={i18n.feedbackGiven()}
-              />
-            </div>
-            <div className={styles.legendItemContainer}>
-              <LegendItem
-                itemType={ITEM_TYPE.KEEP_WORKING}
-                labelText={i18n.markedAsKeepWorking()}
-              />
-            </div>
-          </div>
+  const legendIcons = () =>
+    isViewingLevelProgress ? (
+      <div className={styles.icons}>
+        <div className={styles.legendColumn}>
+          <LegendItem
+            itemType={ITEM_TYPE.NEEDS_FEEDBACK}
+            labelText={i18n.needsFeedback()}
+          />
+          <LegendItem
+            itemType={ITEM_TYPE.FEEDBACK_GIVEN}
+            labelText={i18n.feedbackGiven()}
+          />
         </div>
-      );
-    } else {
-      return (
-        <div className="icons">
-          <div>
-            <div>
-              <LegendItem
-                itemType={ITEM_TYPE.NEEDS_FEEDBACK}
-                labelText={i18n.needsFeedback()}
-              />
-            </div>
-          </div>
+        <div className={styles.legendColumn}>
+          <LegendItem itemType={ITEM_TYPE.VIEWED} labelText={i18n.viewed()} />
+          <LegendItem
+            itemType={ITEM_TYPE.KEEP_WORKING}
+            labelText={i18n.markedAsKeepWorking()}
+          />
         </div>
-      );
-    }
-  };
+      </div>
+    ) : (
+      <div className={styles.icons}>
+        <div className={styles.legendColumn}>
+          <LegendItem
+            itemType={ITEM_TYPE.NEEDS_FEEDBACK}
+            labelText={i18n.needsFeedback()}
+          />
+        </div>
+      </div>
+    );
 
   return (
     <div className={styles.legend}>
