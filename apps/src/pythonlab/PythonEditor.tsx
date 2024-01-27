c8ab012a2835fedcba15d2b3d3de9ae64ed5c9b0
Molly Moen <molly@code.org>
2024-01-26 16:25:45 -0800

    Refactor CodeMirror code for lab2 (#55980)
    * make codemirror code generic
    
    * make codemirror component generic
    
    * remove accidentally included change

--- apps/src/pythonlab/PythonEditor.tsx
+++ apps/src/pythonlab/PythonEditor.tsx
@@ -1,24 +1,19 @@
-import React, {useEffect, useRef} from 'react';
-import PanelContainer from '@cdo/apps/lab2/views/components/PanelContainer';
-import classNames from 'classnames';
-import {EditorView, ViewUpdate} from '@codemirror/view';
-import {EditorState} from '@codemirror/state';
-import {editorSetup} from '../javalab/editorSetup';
-import {darkMode} from '../javalab/editorThemes';
+import React from 'react';
+import {darkMode} from '@cdo/apps/lab2/views/components/editor/editorThemes';
 import {python} from '@codemirror/lang-python';
 import moduleStyles from './python-editor.module.scss';
 import {useDispatch, useSelector} from 'react-redux';
 import {PythonlabState, resetOutput, setCode} from './pythonlabRedux';
-import Button from '../templates/Button';
+import Button from '@cdo/apps/templates/Button';
 import {runPythonCode} from './pyodideRunner';
 import {useFetch} from '@cdo/apps/util/useFetch';
+import CodeEditor from '@cdo/apps/lab2/views/components/editor/CodeEditor';
 
 interface PermissionResponse {
   permissions: string[];
 }
 
 const PythonEditor: React.FunctionComponent = () => {
-  const editorRef = useRef<HTMLDivElement>(null);
   const code = useSelector(
     (state: {pythonlab: PythonlabState}) => state.pythonlab.code
   );
@@ -28,31 +23,8 @@ const PythonEditor: React.FunctionComponent = () => {
   const {loading, data} = useFetch('/api/v1/users/current/permissions');
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (editorRef.current === null) {
-      return;
-    }
-
-    const onEditorUpdate = EditorView.updateListener.of(
-      (update: ViewUpdate) => {
-        dispatch(setCode(update.state.doc.toString()));
-      }
-    );
-
-    const editorExtensions = [
-      ...editorSetup,
-      python(),
-      darkMode,
-      onEditorUpdate,
-    ];
-    new EditorView({
-      state: EditorState.create({
-        doc: '',
-        extensions: editorExtensions,
-      }),
-      parent: editorRef.current,
-    });
-  }, [dispatch, editorRef]);
+  const onCodeUpdate = (code: string) => dispatch(setCode(code));
+  const editorExtensions = [python(), darkMode];
 
   const handleRun = () => {
     const parsedData = data ? (data as PermissionResponse) : {permissions: []};
@@ -70,13 +42,11 @@ const PythonEditor: React.FunctionComponent = () => {
 
   return (
     <div className={moduleStyles.editorContainer}>
-      <PanelContainer
-        id="python-editor"
-        headerText="Editor"
-        hideHeaders={false}
-      >
-        <div ref={editorRef} className={classNames('codemirror-container')} />
-      </PanelContainer>
+      <CodeEditor
+        onCodeChange={onCodeUpdate}
+        startCode={'print("Hello world!")'}
+        editorConfigExtensions={editorExtensions}
+      />
       <div>
         <Button
           type={'button'}
