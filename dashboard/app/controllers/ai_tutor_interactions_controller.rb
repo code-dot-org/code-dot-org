5e78a351f61d39eb6b6fe98166849961ee1223df
Erin Bond <erin.bond@code.org>
2024-01-29 16:04:56 -0500

    add slash to fetch for /ai_tutor_interactions

--- dashboard/app/controllers/ai_tutor_interactions_controller.rb
+++ dashboard/app/controllers/ai_tutor_interactions_controller.rb
@@ -1,7 +1,11 @@
 class AiTutorInteractionsController < ApplicationController
+  include Rails.application.routes.url_helpers
+  skip_before_action :verify_authenticity_token
+
   # POST /ai_tutor_interactions
   def create
     puts params
+    render json: {message: "I rendered some json!"}
     # @ai_tutor_interaction = AiTutorInteraction.new(params)
 
     # @rubric = Rubric.new(rubric_params)
