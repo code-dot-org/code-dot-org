28fad2b07b878432c115169ea227e73c2c929bc9
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2024-01-30 15:46:30 -0800

    remove provider

--- apps/src/code-studio/pd/workshop_dashboard/components/date_picker.story.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/date_picker.story.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import moment from 'moment';
 import DatePicker from './date_picker';
 import {action} from '@storybook/addon-actions';
-import {reduxStore} from '@cdo/storybook/decorators';
-import {Provider} from 'react-redux';
 
 export default {
   title: 'WorkshopDashboard/DatePicker',
@@ -11,15 +9,13 @@ export default {
 };
 
 const Template = args => (
-  <Provider store={reduxStore()}>
-    {/* Currently the Bootstrap 3 styles required by React-Bootstrap are
-    only applied inside div#workshop-container.
-    This is to prevent conflicts with other parts of Code Studio using Bootstrap 2.
-    See pd.scss. Without this container div it won't render properly. */}
-    <div id="workshop-container" style={{width: 300}}>
-      <DatePicker {...args} />
-    </div>
-  </Provider>
+  // Currently the Bootstrap 3 styles required by React-Bootstrap are
+  // only applied inside div#workshop-container.
+  // This is to prevent conflicts with other parts of Code Studio using Bootstrap 2.
+  // See pd.scss. Without this container div it won't render properly.
+  <div id="workshop-container" style={{width: 300}}>
+    <DatePicker {...args} />
+  </div>
 );
 
 export const Basic = Template.bind({});
