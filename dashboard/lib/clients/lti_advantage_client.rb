a081f2528933f5a54ef33a90d22ce39d5b00bc36
Carl Reiner <carl.reiner@code.org>
2024-01-29 20:40:58 -0800

    working test

--- dashboard/lib/clients/lti_advantage_client.rb
+++ dashboard/lib/clients/lti_advantage_client.rb
@@ -11,7 +11,7 @@ class LtiAdvantageClient
   # The URL for the API will vary between LMS platforms, and is initially contained in the launch context.
   # See the LTI spec for details:
   # https://www.imsglobal.org/spec/lti-nrps/v2p0/
-  def get_context_membership(url, resource_link_id)
+  def get_context_membership(url, resource_link_id, page_limit = 100)
     options = {
       headers: {
         'Accept' => Policies::Lti::MEMBERSHIP_CONTAINER_CONTENT_TYPE,
@@ -19,32 +19,34 @@ class LtiAdvantageClient
       },
       query: {
         rlid: resource_link_id,
+        limit: page_limit,
       },
     }
-    res = HTTParty.get(url, options)
-    raise "Error getting context membership: #{res.code} #{res.body}" unless res[:code] == HTTP::Status::OK
-    parsed_res = JSON.parse(res, symbolize_names: true)
-    next_page = next_page_url(parsed_res)
+    res = make_request(url, options)
+    next_page = next_page_url(res[:headers])
+    parsed_res = res[:body]
     while next_page
-      current_page = JSON.parse(HTTParty.get(next_page, options), symbolize_names: true)
-      parsed_res[:members].concat(current_page[:members])
+      current_page = make_request(next_page, options)
+      parsed_res[:members].concat(current_page[:body][:members])
       return parsed_res unless parsed_res[:members].length <= Policies::Lti::MAX_COURSE_MEMBERSHIP
-      next_page = next_page_url(current_page)
+      next_page = next_page_url(current_page[:headers])
     end
     parsed_res
   end
 end
 
-private
-
 def make_request(url, options)
   res = HTTParty.get(url, options)
-  raise "Error getting context membership: #{res.code} #{res.body}" unless res[:code] == HTTP::Status::OK
-  res
+  raise "Error getting context membership: #{res.code} #{res.body}" unless res.code == HTTP::Status::OK
+  return {headers: res.headers, body: JSON.parse(res.body, symbolize_names: true)}
 end
 
-def next_page_url(nrps_response)
-  link_header = nrps_response.headers['link']
+private
+
+# Get the next page URL from the Link header in the response.
+# Returns nil if no next page is present.
+def next_page_url(headers)
+  link_header = headers&.[](:link)
   return nil unless link_header
 
   next_url = nil
