12050cf549332eec98cf98db62a1388201c030d0
Liam Frye-Mason <liam.fryemason@code.org>
2024-01-30 14:19:32 -0800

    Refactor expanded header to file

--- apps/src/templates/sectionProgressV2/ExpandedProgressDataColumn.jsx
+++ apps/src/templates/sectionProgressV2/ExpandedProgressDataColumn.jsx
@@ -4,10 +4,8 @@ import styles from './progress-table-v2.module.scss';
 import {studentShape} from '../teacherDashboard/teacherSectionsRedux';
 import {studentLevelProgressType} from '../progress/progressTypes';
 import {connect} from 'react-redux';
-import classNames from 'classnames';
-import FontAwesome from '../FontAwesome';
 import LevelDataCell from './LevelDataCell';
-import i18n from '@cdo/locale';
+import ExpandedProgressColumnHeader from './ExpandedProgressColumnHeader.jsx';
 
 function ExpandedProgressDataColumn({
   lesson,
@@ -15,57 +13,6 @@ function ExpandedProgressDataColumn({
   sortedStudents,
   removeExpandedLesson,
 }) {
-  const header = React.useMemo(() => {
-    // If there are only 2 levels, we only show the number so that the text fits the cell.
-    const headerText =
-      lesson.levels.length < 3
-        ? lesson.relative_position
-        : i18n.lessonNumbered({
-            lessonNumber: lesson.relative_position,
-            lessonName: lesson.name,
-          });
-
-    // Manual width is necessary so that overflow text is hidden and lesson header exactly fits levels.
-    // Add (numLevels + 1)px to account for borders.
-    const width =
-      lesson.levels.length * styles.levelCellWidth +
-      lesson.levels.length +
-      1 +
-      'px';
-    return (
-      <div className={styles.expandedHeader}>
-        <div
-          className={classNames(
-            styles.gridBox,
-            styles.expandedHeaderLessonCell
-          )}
-          style={{width}}
-          onClick={() => removeExpandedLesson(lesson.id)}
-          aria-label={headerText}
-        >
-          <FontAwesome
-            icon="caret-down"
-            className={styles.expandedHeaderCaret}
-          />
-          <div className={styles.expandedHeaderLessonText}>{headerText}</div>
-        </div>
-        <div className={styles.expandedHeaderSecondRow}>
-          {lesson.levels.map(level => (
-            <div
-              className={classNames(
-                styles.gridBox,
-                styles.expandedHeaderLevelCell
-              )}
-              key={lesson.id + '.' + level.bubbleText + '-h'}
-            >
-              {lesson.relative_position + '.' + level.bubbleText}
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  }, [removeExpandedLesson, lesson]);
-
   const progress = React.useMemo(
     () => (
       <div className={styles.expandedTable}>
@@ -93,7 +40,10 @@ function ExpandedProgressDataColumn({
 
   return (
     <div key={lesson.id} className={styles.expandedColumn}>
-      {header}
+      <ExpandedProgressColumnHeader
+        lesson={lesson}
+        removeExpandedLesson={removeExpandedLesson}
+      />
       {progress}
     </div>
   );
