89be0fe24188fa81ec7024e720a82a0f7bfd4993
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2024-01-30 13:16:19 -0800

    add comment

--- apps/src/code-studio/pd/workshop_dashboard/components/date_picker.story.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/date_picker.story.jsx
@@ -12,6 +12,10 @@ export default {
 
 const Template = args => (
   <Provider store={reduxStore()}>
+    {/* Currently the Bootstrap 3 styles required by React-Bootstrap are
+    only applied inside div#workshop-container.
+    This is to prevent conflicts with other parts of Code Studio using Bootstrap 2.
+    See pd.scss. Without this container div it won't render properly. */}
     <div id="workshop-container" style={{width: 300}}>
       <DatePicker {...args} />
     </div>
