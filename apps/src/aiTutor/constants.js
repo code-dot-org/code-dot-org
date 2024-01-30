8c7a510f2820bdb6a18130c7c55be006e5272022
Erin Bond <erin.bond@code.org>
2024-01-30 10:23:42 -0500

    remove unused constants

--- apps/src/aiTutor/constants.js
+++ apps/src/aiTutor/constants.js
@@ -4,9 +4,3 @@ export const validationSystemPrompt =
   'You are a tutor in a high school computer science class. Students in the class are studying Java and they would like to know in age-appropriate, clear language why their tests are not passing.';
 export const generalChatSystemPrompt =
   'You are a tutor in a high school classroom where the students are learning Java using the Code.org curriculum. Answer their questions in plain, easy-to-understanding English. Do not write any code in your response.';
-
-export const aiTutorTypes = {
-  COMPILATION: 'compilation',
-  VALIDATION: 'validation',
-  GENERAL_CHAT: 'general_chat',
-};
