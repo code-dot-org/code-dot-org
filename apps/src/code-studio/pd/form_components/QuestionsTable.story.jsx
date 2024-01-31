8c970de0d2ada4b05b315c361d8de09b4acbf8ac
Turner Riley <turner.riley@code.org>
2024-01-31 11:34:53 -0800

    refactor questions table story

--- apps/src/code-studio/pd/form_components/QuestionsTable.story.jsx
+++ apps/src/code-studio/pd/form_components/QuestionsTable.story.jsx
@@ -1,62 +1,54 @@
 import React from 'react';
 import QuestionsTable from './QuestionsTable';
-import reactBootstrapStoryDecorator from '../reactBootstrapStoryDecorator';
 import {action} from '@storybook/addon-actions';
 
-export default storybook => {
-  storybook
-    .storiesOf('FormComponents/QuestionsTable', module)
-    .addDecorator(reactBootstrapStoryDecorator)
-    .addStoryTable([
-      {
-        name: 'simple questions table',
-        story: () => (
-          <QuestionsTable
-            options={['this is cool', 'this is okay', 'this is useless']}
-            questions={[
-              {
-                label: 'what do you think of this component?',
-                name: 'thinkOfComponent',
-                required: true,
-              },
-              {
-                label: 'what do you think of this story?',
-                name: 'thinkOfStory',
-              },
-              {
-                label: 'what do you think of this question?',
-                name: 'thinkOfQuestion',
-              },
-            ]}
-          />
-        ),
-      },
-      {
-        name: 'controlled questions table',
-        story: () => (
-          <QuestionsTable
-            data={{
-              theOneThatIsSelected: 'first',
-            }}
-            errors={['theOneWithTheError']}
-            onChange={action('onChange')}
-            options={['first', 'second', 'third']}
-            questions={[
-              {
-                label: 'this one should have something selected',
-                name: 'theOneThatIsSelected',
-              },
-              {
-                label: 'this one should have an error',
-                name: 'theOneWithTheError',
-              },
-              {
-                label: 'this one should be plain',
-                name: 'theOtherOne',
-              },
-            ]}
-          />
-        ),
-      },
-    ]);
+export default {
+  title: 'FormComponents/QuestionsTable',
+  component: QuestionsTable,
+};
+
+const Template = args => <QuestionsTable {...args} />;
+
+const simpleQuestionsTable = Template.bind({});
+simpleQuestionsTable.args = {
+  options: ['this is cool', 'this is okay', 'this is useless'],
+  questions: [
+    {
+      label: 'what do you think of this component?',
+      name: 'thinkOfComponent',
+      required: true,
+    },
+    {
+      label: 'what do you think of this story?',
+      name: 'thinkOfStory',
+    },
+    {
+      label: 'what do you think of this question?',
+      name: 'thinkOfQuestion',
+    },
+  ],
+};
+
+const controlledQuestionsTable = Template.bind({});
+controlledQuestionsTable.args = {
+  data: {
+    theOneThatIsSelected: 'first',
+  },
+  errors: ['theOneWithTheError'],
+  onChange: action('onChange'),
+  options: ['first', 'second', 'third'],
+  questions: [
+    {
+      label: 'this one should have something selected',
+      name: 'theOneThatIsSelected',
+    },
+    {
+      label: 'this one should have an error',
+      name: 'theOneWithTheError',
+    },
+    {
+      label: 'this one should be plain',
+      name: 'theOtherOne',
+    },
+  ],
 };
