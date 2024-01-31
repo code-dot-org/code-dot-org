34b13f1e42fbb332e506c948588b17b66bc8a173
Bethany Connor <bethany@code.org>
2024-01-31 14:54:12 -0500

    Move CORS config to application.rb

--- dashboard/config/initializers/cors.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins CDO.pegasus_site_host
-    resource '/dashboardapi/*', headers: :any, methods: [:get]
-  end
-end
