a1d2b4b3ef87ba16947e95157cb79dab18a91f2d
kobryan0619 <kaitie.obryan@gmail.com>
2024-02-01 13:08:19 -0600

    updated based on feedback

--- dashboard/scripts/delete_groups_of_user_progress.rb
+++ dashboard/scripts/delete_groups_of_user_progress.rb
@@ -12,20 +12,14 @@ def delete_all_progress_for_multiple_usernames(teacher_id, usernames)
   user_ids = User.where(username: usernames).pluck(:id)
 
   user_ids.each do |user_id|
-    next unless follower_ids.include?(student.id)
+    next unless follower_ids.include?(user_id)
     # Retrieve storage ID for the user
     user_storage_id = storage_id_for_user_id(user_id)
-    # Find all script IDs associated with this user
-    user_script_ids = UserScript.where(user_id: user_id).pluck(:id)
-
-    user_script_ids.each do |script_id|
-      # Delete all associated records for each script
-      # taken from: https://github.com/code-dot-org/code-dot-org/blob/375e794083094cf128e9fac67ba09ec5adcd436b/dashboard/app/controllers/admin_users_controller.rb#L193
-      UserScript.where(user_id: user_id, script_id: script_id).destroy_all
-      UserLevel.where(user_id: user_id, script_id: script_id).destroy_all
-      ChannelToken.where(storage_id: user_storage_id, script_id: script_id).destroy_all unless user_storage_id.nil?
-      TeacherFeedback.where(student_id: user_id, script_id: script_id).destroy_all
-      CodeReview.where(user_id: user_id, script_id: script_id).destroy_all
-    end
+    # inspired from: https://github.com/code-dot-org/code-dot-org/blob/375e794083094cf128e9fac67ba09ec5adcd436b/dashboard/app/controllers/admin_users_controller.rb#L193
+    UserScript.where(user_id: user_id).destroy_all
+    UserLevel.where(user_id: user_id).destroy_all
+    ChannelToken.where(storage_id: user_storage_id).destroy_all unless user_storage_id.nil?
+    TeacherFeedback.where(student_id: user_id).destroy_all
+    CodeReview.where(user_id: user_id).destroy_all
   end
 end
