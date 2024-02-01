a02ba66c0e1bc90759a316c827cbc21bbcd1c2ef
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-02-01 15:43:17 -0800

    improve comments and add tests

--- dashboard/config/initializers/mysql_avoid_utf8_alias_in_schema_dump.rb
+++ dashboard/config/initializers/mysql_avoid_utf8_alias_in_schema_dump.rb
@@ -6,27 +6,31 @@
 # entirely and instead use the appropriate explicit value for the current
 # verison.
 #
+# TODO infra: once we have fully upgraded to MySQL 8+, we can remove this.
+#
 # See https://dev.mysql.com/doc/refman/8.0/en/charset-unicode-utf8.html
 module MysqlAvoidUtf8AliasInSchemaDump
   # Reimplement ActiveRecord::SchemaDumper::format_options to replace instances
   # of the `utf8` alias with the version-specific value it is aliasing.
-  # See https://github.com/rails/rails/blob/7c1db8db9ea62f8e4b66bbeb1691ecbead80fdfe/activerecord/lib/active_record/schema_dumper.rb#L321-L323
+  # See https://github.com/rails/rails/blob/v6.1.7.3/activerecord/lib/active_record/schema_dumper.rb#L276-L278
   def format_options(options)
-    if options.key?(:charset) && options[:charset] == 'utf8'
-      options[:charset] = mysql_version < 8 ? 'utf8mb3' : 'utf8mb4'
-    end
+    options.map do |key, value|
+      if key == :charset && value == 'utf8'
+        value = current_mysql_version < 8 ? 'utf8mb3' : 'utf8mb4'
+      end
 
-    if options.key?(:collation) && options[:collation] == 'utf8_unicode_ci'
-      options[:collation] = mysql_version < 8 ? 'utf8mb3_unicode_ci' : 'utf8mb4_unicode_ci'
-    end
+      if key == :collation && value == 'utf8_unicode_ci'
+        value = current_mysql_version < 8 ? 'utf8mb3_unicode_ci' : 'utf8mb4_unicode_ci'
+      end
 
-    options.map {|key, value| "#{key}: #{value.inspect}"}.join(', ')
+      "#{key}: #{value.inspect}"
+    end.join(', ')
   end
 
   private
 
-  def mysql_version
-    @mysql_version ||= begin
+  def current_mysql_version
+    @current_mysql_version ||= begin
       raw_version = ActiveRecord::Base.connection.select_value('SELECT VERSION()')
       case raw_version
       when /^8.0.\d+/
