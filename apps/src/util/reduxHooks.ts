15506de29a5b51bdd2381f8dbd6fd170e0d2962b
molly-moen <molly@code.org>
2024-02-01 15:01:29 -0800

    update comment

--- apps/src/util/reduxHooks.ts
+++ apps/src/util/reduxHooks.ts
@@ -4,11 +4,9 @@ import {RootState} from '@cdo/apps/types/reduxTypes';
 
 // Redux toolkit recommends getting this type from store.dispatch:
 // https://redux-toolkit.js.org/usage/usage-with-typescript#getting-the-dispatch-type
-// Right now our store isn't in typescript, and even if it was we would face the issue
-// that we define it as a singleton and therefore need to type it manually anyway, as it
-// could be null. Therefore we are manually defining the App Dispatch type here, and need
-// to use any because that's what redux-toolkit uses.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// Right now our full store isn't in typescript, and we manually type what is in TypeScript
+// using RootState because our store is updated programmatically.
+// Therefore we are manually defining the App Dispatch type here.
 export type AppDispatch = ThunkDispatch<RootState, undefined, AnyAction> &
   Dispatch<AnyAction>;
 
