cae96daddd5ccf312ca3eb2ba0e6ed9789e0e760
Alice Fisher <alice.fisher@code.org>
2024-02-01 16:51:59 +0000

    migrate speedslider story

--- apps/src/templates/SpeedSlider.story.jsx
+++ apps/src/templates/SpeedSlider.story.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import SpeedSlider from './SpeedSlider';
 
-class StorybookHarness extends React.Component {
+class SpeedSliderHarness extends React.Component {
   state = {value: 0.5};
 
   onValueChange = newValue => this.setState({value: newValue});
 
   render() {
     return (
-      <SpeedSlider
-        hasFocus={false}
-        value={this.state.value}
-        onChange={this.onValueChange}
-      />
+      <div style={{display: 'flex', gap: '20px'}}>
+        <div style={{background: 'black', padding: 10}}>
+          <SpeedSlider
+            hasFocus={true}
+            value={this.state.value}
+            lineWidth={200}
+            onChange={this.onValueChange}
+          />
+        </div>
+      </div>
     );
   }
 }
 
-export default storybook => {
-  return storybook
-    .storiesOf('SpeedSlider', module)
-    .add('Default', () => <StorybookHarness />);
+export default {
+  title: 'SpeedSlider',
+  component: SpeedSlider,
 };
+
+const Template = () => <SpeedSliderHarness />;
+
+export const Default = Template.bind({});
