ae257198f4c9fad2814f21f4613287d64a1acdb4
kobryan0619 <kaitie.obryan@gmail.com>
2024-02-02 12:13:55 -0600

    changes based on testing

--- dashboard/scripts/delete_groups_of_user_progress.rb
+++ dashboard/scripts/delete_groups_of_user_progress.rb
@@ -1,25 +1,33 @@
-## TODO: Give this file execute permissions.
-##!/usr/bin/env ruby
+#!/usr/bin/env ruby
 
-require_relative '../../config/environment'
+require_relative '../config/environment'
 
 # This script was developed to allow us to delete progress for multiple users quickly.
 # Prior to the development of this script, it was a manual process for our Customer Success team
 # Depending on where this goes, tests should be added as deleting progress is not undoable
-def delete_all_progress_for_multiple_usernames(teacher_id, usernames)
-  follower_ids = teacher_id.followers.pluck(:student_user_id)
-  # Fetch user IDs based on usernames
-  user_ids = User.where(username: usernames).pluck(:id)
 
-  user_ids.each do |user_id|
-    next unless follower_ids.include?(user_id)
-    # Retrieve storage ID for the user
-    user_storage_id = storage_id_for_user_id(user_id)
-    # inspired from: https://github.com/code-dot-org/code-dot-org/blob/375e794083094cf128e9fac67ba09ec5adcd436b/dashboard/app/controllers/admin_users_controller.rb#L193
-    UserScript.where(user_id: user_id).destroy_all
-    UserLevel.where(user_id: user_id).destroy_all
-    ChannelToken.where(storage_id: user_storage_id).destroy_all unless user_storage_id.nil?
-    TeacherFeedback.where(student_id: user_id).destroy_all
-    CodeReview.where(user_id: user_id).destroy_all
-  end
+# When run, replace the teacher_id and usernames list below
+teacher_id = 547
+usernames = ["crazy_kate", "coder_mark197", "stacy", "studentkt"]
+
+teacher_user = User.find_by(id: teacher_id)
+follower_ids = teacher_user.followers.pluck(:student_user_id)
+# Fetch user IDs based on usernames
+
+puts follower_ids
+puts "-------"
+user_ids = User.where(username: usernames).pluck(:id)
+
+puts user_ids
+
+user_ids.each do |user_id|
+  next unless follower_ids.include?(user_id)
+  # Retrieve storage ID for the user
+  user_storage_id = storage_id_for_user_id(user_id)
+  # inspired from: https://github.com/code-dot-org/code-dot-org/blob/375e794083094cf128e9fac67ba09ec5adcd436b/dashboard/app/controllers/admin_users_controller.rb#L193
+  UserScript.where(user_id: user_id).destroy_all
+  UserLevel.where(user_id: user_id).destroy_all
+  ChannelToken.where(storage_id: user_storage_id).destroy_all unless user_storage_id.nil?
+  TeacherFeedback.where(student_id: user_id).destroy_all
+  CodeReview.where(user_id: user_id).destroy_all
 end
