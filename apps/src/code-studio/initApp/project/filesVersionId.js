46bb5307f7340b17846c8591b202d7b3b8eb8a47
Jim Thomason <jim@japanesewartuba.com>
2024-02-02 12:20:36 -0600

    jimt/LABS-472 - added comments

--- apps/src/code-studio/initApp/project/filesVersionId.js
+++ apps/src/code-studio/initApp/project/filesVersionId.js
@@ -1,3 +1,8 @@
+// this file should only be loaded directly if necessary to break a circular dependency
+// otherwise import the full project object from the directory path.
+//
+// import project from "./code-studio/initApp/project"
+
 let filesVersionIdVal = undefined;
 
 export const filesVersionId = () => filesVersionIdVal;
--- apps/src/code-studio/initApp/project/index.js
+++ apps/src/code-studio/initApp/project/index.js
@@ -3,6 +3,21 @@ import unEnhancedProject from './internal';
 import {filesVersionId, setFilesVersionId} from './filesVersionId';
 import {getCurrentId, setCurrentId} from './currentId';
 
+/*
+  filesVersionId and getCurrentId were creating circular dependencies in the clientApi file, which needed both of them.
+  but clientApi and all of its child objects were imported into project.js (now project/internal.js), so we couldn't break the link.
+
+  This moves the old object to the wrappered `internal.js` file, and moves filesVersionId and current.id into separate getter/setter files
+  contained within this directory.
+
+  This file then re-exports everything from the internal objet, along with the new setters/getters.
+
+  Further, current.id is set via two functions which replace the entire current object. So the re-export here will wrap them so  they receive
+  a callback as their last argument which will set the current.id value in the proper location so direct loading of the `currentId` file will work properly.
+
+  Externally, the only change to code required is that project.filesVersionId is deprecated and replaced by project.filesVersionId()
+*/
+
 export default {
   ...unEnhancedProject,
   filesVersionId,
