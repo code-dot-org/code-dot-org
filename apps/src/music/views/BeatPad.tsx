b3890cbcdfd3313470570ed90824292fd1a1b439
Sanchit Malhotra <sanchit@code.org>
2024-02-02 10:39:58 -0800

    Music: consolidate BeatPad code and convert to TypeScript

---
+++ apps/src/music/views/BeatPad.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './beatpad.module.scss';
+import {Trigger} from '../constants';
+import {useMusicSelector} from './types';
+
+interface BeatPadProps {
+  triggers: Trigger[];
+  playTrigger: (id: string) => void;
+}
+
+/**
+ * Renders the Beat Pad component, which can be used to play numbered triggers during playback
+ */
+const BeatPad: React.FunctionComponent<BeatPadProps> = ({
+  triggers,
+  playTrigger,
+}) => {
+  const selectedTriggerId = useMusicSelector(
+    state => state.music.selectedTriggerId
+  );
+  const isPlaying = useMusicSelector(state => state.music.isPlaying);
+
+  return (
+    <div id="beat-pad" className={styles.triggersContainer}>
+      {triggers.map(trigger => {
+        return (
+          <button
+            type="button"
+            id={trigger.id}
+            className={classNames(
+              styles.triggerButton,
+              trigger.id === selectedTriggerId && styles.triggerButtonSelected,
+              isPlaying && styles.triggerButtonActive
+            )}
+            onClick={!isPlaying ? undefined : () => playTrigger(trigger.id)}
+          >
+            {trigger.buttonLabel}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BeatPad;
