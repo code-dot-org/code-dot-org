21e7b67b73031dca0f789f4c2ca1eaa72e53a2ee
molly-moen <molly@code.org>
2024-02-05 13:55:16 -0800

    update redux

--- apps/src/redux/blockly.ts
+++ apps/src/redux/blockly.ts
@@ -2,12 +2,15 @@ import {PayloadAction, createSlice} from '@reduxjs/toolkit';
 
 interface BlocklyState {
   hasIncompatibleSources: boolean;
+  failedToLoad: boolean;
 }
 
 const initialState: BlocklyState = {
   // hasIncompatibleSources is set to true if we try to load json sources in
   // CDO Blockly, which only supports xml.
   hasIncompatibleSources: false,
+  // If we failed to load blockly sources for any reason.
+  failedToLoad: false,
 };
 
 const blocklySlice = createSlice({
@@ -17,9 +20,13 @@ const blocklySlice = createSlice({
     setHasIncompatibleSources(state, action: PayloadAction<boolean>) {
       state.hasIncompatibleSources = action.payload;
     },
+    setFailedToLoad(state, action: PayloadAction<boolean>) {
+      state.failedToLoad = action.payload;
+    },
   },
 });
 
-export const {setHasIncompatibleSources} = blocklySlice.actions;
+export const {setHasIncompatibleSources, setFailedToLoad} =
+  blocklySlice.actions;
 
 export default blocklySlice.reducer;
