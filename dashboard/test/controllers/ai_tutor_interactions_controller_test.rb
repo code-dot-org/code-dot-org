7d138b182e88f1529eaed792cba345dd50a46660
Ken Akiha <95503833+kakiha11@users.noreply.github.com>
2024-02-06 14:40:43 -0800

    added three tests

--- dashboard/test/controllers/ai_tutor_interactions_controller_test.rb
+++ dashboard/test/controllers/ai_tutor_interactions_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class AiTutorInteractionsControllerTest < ActionController::TestCase
     setup do
         @student_with_ai_tutor_access = create :student_with_ai_tutor_access
+        @student = create :student
     end
 
     test "create AI Tutor Interaction with valid params" do
@@ -18,4 +19,36 @@ class AiTutorInteractionsControllerTest < ActionController::TestCase
             }
         end
     end
+
+    test "student without access tries to create AI Tutor Interaction" do
+        sign_in @student
+        assert_does_not_create(AiTutorInteraction) do
+            post :create, params: {
+                level_id: 5678,
+                script_id: 246,
+                type: "general_chat",
+                prompt: "Can you help me?",
+                status: "ok",
+                ai_response: "Yes, I can help."
+            }
+        end
+        assert_response :forbidden
+    end
+
+    test "create AI Tutor Interaction with invalid type param" do
+        sign_in @student_with_ai_tutor_access 
+        assert_does_not_create(AiTutorInteraction) do
+            post :create, params: {
+                level_id: 1234,
+                script_id: 987,
+                type: "trash can",
+                prompt: "Can you help me?",
+                status: "ok",
+                ai_response: "Yes, I can help."
+            }
+        end
+        assert_includes(@response.body, "Enjoy.") 
+    end
+
+
 end
