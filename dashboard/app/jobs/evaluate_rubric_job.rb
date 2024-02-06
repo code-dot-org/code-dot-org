53f2bac3099f27810a9aa7a4e5a48145c2d47b9c
David Bailey <davidsbailey@users.noreply.github.com>
2024-02-06 04:56:58 -0800

    extract S3_AI_RELEASE_PATH

--- dashboard/app/jobs/evaluate_rubric_job.rb
+++ dashboard/app/jobs/evaluate_rubric_job.rb
@@ -5,6 +5,7 @@ class EvaluateRubricJob < ApplicationJob
   queue_as :default
 
   S3_AI_BUCKET = 'cdo-ai'.freeze
+  S3_AI_RELEASE_PATH = 'teaching_assistant/lessons/'.freeze
   STUB_AI_PROXY_PATH = '/api/test/ai_proxy'.freeze
 
   # 2D Map from unit name and level name, to the name of the lesson files in S3
@@ -291,12 +292,12 @@ class EvaluateRubricJob < ApplicationJob
   end
 
   private def read_file_from_s3(lesson_s3_name, key_suffix)
-    key = "teaching_assistant/lessons/#{lesson_s3_name}/#{key_suffix}"
+    key = "#{S3_AI_RELEASE_PATH}#{lesson_s3_name}/#{key_suffix}"
     s3_client.get_object(bucket: S3_AI_BUCKET, key: key)[:body].read
   end
 
   private def read_examples(lesson_s3_name)
-    prefix = "teaching_assistant/lessons/#{lesson_s3_name}/examples/"
+    prefix = "#{S3_AI_RELEASE_PATH}#{lesson_s3_name}/examples/"
     response = s3_client.list_objects_v2(bucket: S3_AI_BUCKET, prefix: prefix)
     file_names = response.contents.map(&:key)
     file_names = file_names.map {|name| name.gsub(prefix, '')}
