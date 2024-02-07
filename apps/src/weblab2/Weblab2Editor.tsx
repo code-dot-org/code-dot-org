4a6a326d77f9f8614cd6f572013f08f87944e1c9
molly-moen <molly@code.org>
2024-02-06 16:03:45 -0800

    add codemirror to weblab2

---
+++ apps/src/weblab2/Weblab2Editor.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import CodeEditor from '@cdo/apps/lab2/views/components/editor/CodeEditor';
+import {html} from '@codemirror/lang-html';
+
+const Weblab2Editor: React.FunctionComponent = () => {
+  const editorExtensions = [html()];
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const onCodeChange = () => {};
+
+  return (
+    <CodeEditor
+      onCodeChange={onCodeChange}
+      startCode={'<h1>Hello, world!</h1>'}
+      editorConfigExtensions={editorExtensions}
+    />
+  );
+};
+
+export default Weblab2Editor;
