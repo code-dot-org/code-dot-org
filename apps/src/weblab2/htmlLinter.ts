46e5c99018924cf96d057dc70329c4f7bebeda21
molly-moen <molly@code.org>
2024-02-07 13:21:12 -0800

    add htmlhint

--- apps/src/weblab2/htmlLinter.ts
+++ apps/src/weblab2/htmlLinter.ts
@@ -1,8 +1,11 @@
 import {linter, Diagnostic} from '@codemirror/lint';
+import {HTMLHint} from 'htmlhint';
 
 export const htmlLinter = linter(view => {
   console.log('linting!');
   console.log({doc: view.state.doc.toString()});
+  const errors = HTMLHint.verify(view.state.doc.toString());
+  console.log({errors});
   const diagnostics: Diagnostic[] = [];
   console.log('done linting!');
   return diagnostics;
