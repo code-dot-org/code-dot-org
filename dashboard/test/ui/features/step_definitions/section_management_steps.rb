49ed4fdb6735df4c934abb1fea4c87f81813729d
Mark Barnes <mark.barnes@code.org>
2024-02-07 09:47:05 -0600

    added id check to investigate test failure

--- dashboard/test/ui/features/step_definitions/section_management_steps.rb
+++ dashboard/test/ui/features/step_definitions/section_management_steps.rb
@@ -103,7 +103,9 @@ And(/^I create a(n authorized)? teacher-associated( under-13)? student named "([
   section = JSON.parse(browser_request(url: '/dashboardapi/sections', method: 'POST', body: {login_type: 'email', participant_type: 'student'}))
   section_code = section['code']
   @section_url = "http://studio.code.org/join/#{section_code}"
-  create_user(name, url: "/join/#{section_code}", code: 200, age: under_13 ? '10' : '16')
+  @student = create_user(name, url: "/join/#{section_code}", code: 200, age: under_13 ? '10' : '16')
+  puts @student
+  @student
 end
 
 And(/^I save the student section url$/) do
