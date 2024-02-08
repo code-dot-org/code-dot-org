4e16344d8bd52b592574c36e7db784287a883b5b
Liam Frye-Mason <liam.fryemason@code.org>
2024-02-08 15:56:55 -0800

    Fix height

--- apps/src/templates/sectionProgressV2/LessonTitleTooltip.jsx
+++ apps/src/templates/sectionProgressV2/LessonTitleTooltip.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactTooltip from 'react-tooltip';
 import PropTypes from 'prop-types';
 import i18n from '@cdo/locale';
+import styles from './progress-table-v2.module.scss';
 
 export const getTooltipId = lesson => `tooltip-${lesson.id}`;
 
@@ -12,13 +13,15 @@ export default function LessonTitleTooltip({lesson}) {
       id={tooltipId}
       key={tooltipId}
       role="tooltip"
-      wrapper="span"
+      wrapper="div"
       effect="solid"
     >
-      {i18n.lessonNumbered({
-        lessonNumber: lesson.relative_position,
-        lessonName: lesson.name,
-      })}
+      <div className={styles.lessonHeaderTooltip}>
+        {i18n.lessonNumbered({
+          lessonNumber: lesson.relative_position,
+          lessonName: lesson.name,
+        })}
+      </div>
     </ReactTooltip>
   );
 }
