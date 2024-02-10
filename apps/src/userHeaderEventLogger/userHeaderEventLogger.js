c6af7b79a0069d5d81e8112b4f6bf8206cd116a8
Turner Riley <turnertriley@gmail.com>
2024-02-09 16:40:13 -0800

    try no waiting wrapper

--- apps/src/userHeaderEventLogger/userHeaderEventLogger.js
+++ apps/src/userHeaderEventLogger/userHeaderEventLogger.js
@@ -2,30 +2,26 @@ import getScriptData from '@cdo/apps/util/getScriptData';
 import analyticsReporter from '@cdo/apps/lib/util/AnalyticsReporter';
 import {EVENTS} from '@cdo/apps/lib/util/AnalyticsConstants';
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('HEADER LOGGER');
-  const headerCreateMenu = document.getElementById('header_create_menu');
-  if (getScriptData('isSignedOut') && headerCreateMenu) {
-    // Log if a signed-out user clicks the "Create" menu dropdown
-    headerCreateMenu.addEventListener('click', () => {
-      analyticsReporter.sendEvent(
-        EVENTS.SIGNED_OUT_USER_CLICKS_CREATE_DROPDOWN
-      );
-    });
+console.log('Javascript file loads');
+const headerCreateMenu = document.getElementById('header_create_menu');
+if (getScriptData('isSignedOut') && headerCreateMenu) {
+  // Log if a signed-out user clicks the "Create" menu dropdown
+  headerCreateMenu.addEventListener('click', () => {
+    analyticsReporter.sendEvent(EVENTS.SIGNED_OUT_USER_CLICKS_CREATE_DROPDOWN);
+  });
 
-    // Log if a signed-out user clicks an option in the "Create" menu dropdown
-    const createMenuOptions = getScriptData('createMenuOptions');
-    createMenuOptions.forEach(option => {
-      document
-        .getElementById(`create_menu_option_${option}`)
-        .addEventListener('click', () => {
-          analyticsReporter.sendEvent(
-            EVENTS.SIGNED_OUT_USER_SELECTS_CREATE_DROPDOWN_OPTION,
-            {
-              option: option,
-            }
-          );
-        });
-    });
-  }
-});
+  // Log if a signed-out user clicks an option in the "Create" menu dropdown
+  const createMenuOptions = getScriptData('createMenuOptions');
+  createMenuOptions.forEach(option => {
+    document
+      .getElementById(`create_menu_option_${option}`)
+      .addEventListener('click', () => {
+        analyticsReporter.sendEvent(
+          EVENTS.SIGNED_OUT_USER_SELECTS_CREATE_DROPDOWN_OPTION,
+          {
+            option: option,
+          }
+        );
+      });
+  });
+}
