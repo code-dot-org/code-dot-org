142c93a22f35eb06b9074e10ac91d1314886f53c
Sanchit Malhotra <sanchit@code.org>
2024-02-15 10:52:54 -0800

    Music: fix sequencer library lookup

--- apps/test/unit/music/player/sequencer/Simple2SequencerTest.ts
+++ apps/test/unit/music/player/sequencer/Simple2SequencerTest.ts
@@ -27,8 +27,13 @@ describe('Simple2Sequencer', () => {
   beforeEach(() => {
     library = Sinon.createStubInstance(MusicLibrary);
     library.getSoundForId.returns(testSound);
+    Sinon.stub(MusicLibrary, 'getInstance').returns(library);
 
     sequencer = new Simple2Sequencer(library);
+  });
+
+  afterEach(() => {
+    Sinon.restore();
   });
 
   it('starts a new sequence at the given measure', () => {
