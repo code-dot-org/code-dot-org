d52ca0a4656c8118506e3e2024ad1efae8c8b0d7
Seth Nickell <snickell@gmail.com>
2024-02-16 00:01:11 -1000

    Fix library manifest seed method to cast from Firebase JSON shape

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_16_053618) do
+ActiveRecord::Schema.define(version: 2024_02_16_071353) do
 
   create_table "activities", id: :integer, charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
