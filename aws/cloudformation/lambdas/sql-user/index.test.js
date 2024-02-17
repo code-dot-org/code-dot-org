ee7477dfec996df0812f483a8364810242feb303
suresh <suresh@code.org>
2024-02-17 15:16:08 -0800

    Undo some changes that are in a separate feature branch that updates the Lambda.

--- aws/cloudformation/lambdas/sql-user/index.test.js
+++ aws/cloudformation/lambdas/sql-user/index.test.js
@@ -25,12 +25,6 @@ describe("SQL User Custom Resource", () => {
     const event = {
       RequestType: "Create",
       ResourceProperties: {
-        DBWriterEndpointSecret:
-          "arn:aws:secretsmanager:REGION:ACCOUNT:secret:CfnStack/stack/db_endpoint_writer-123456",
-        DBCredentialAdminSecret:
-          "arn:aws:secretsmanager:REGION:ACCOUNT:secret:CfnStack/stack/db_credential_admin-abcdef",
-        DBCredentialSecret:
-          "arn:aws:secretsmanager:REGION:ACCOUNT:secret:CfnStack/stack/db_credential_writer-zyxvuw",
         Privileges: ["ALL PRIVILEGES"],
       },
       PhysicalResourceId: "someId",
@@ -54,10 +48,6 @@ describe("SQL User Custom Resource", () => {
           username: "sqlUser",
           password: "sqlUserPass",
         }),
-      })
-      .onThirdCall()
-      .resolves({
-        SecretString: "database.cluster.writer.endpoint.example.net",
       });
 
     // Mock MySQL calls
