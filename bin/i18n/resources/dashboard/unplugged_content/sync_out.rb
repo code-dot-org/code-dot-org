d906b2b174faa444453e8a9cacb744fb5d49beb2
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-02-20 18:59:45 +0200

    P20-629: Create the `Dashboard/UnpluggedContent` i18n sync-down script

--- bin/i18n/resources/dashboard/unplugged_content/sync_out.rb
+++ bin/i18n/resources/dashboard/unplugged_content/sync_out.rb
@@ -10,14 +10,14 @@ module I18n
       module UnpluggedContent
         class SyncOut < I18n::Utils::SyncOutBase
           def process(language)
-            crowdin_file_path = I18nScriptUtils.locale_dir(language[:crowdin_name_s], DIR_NAME, FILE_NAME)
+            crowdin_file_path = I18nScriptUtils.locale_dir(language[:crowdin_name_s], FILE_PATH)
             return unless File.file?(crowdin_file_path)
 
             # Distributes the localization
             target_i18n_file_path = File.join(ORIGIN_I18N_DIR_PATH, "unplugged.#{language[:locale_s]}.yml")
             I18nScriptUtils.sanitize_file_and_write(crowdin_file_path, target_i18n_file_path)
 
-            i18n_file_path = I18nScriptUtils.locale_dir(language[:locale_s], DIR_NAME, FILE_NAME)
+            i18n_file_path = I18nScriptUtils.locale_dir(language[:locale_s], FILE_PATH)
             I18nScriptUtils.move_file(crowdin_file_path, i18n_file_path)
           end
         end
--- bin/test/i18n/resources/dashboard/test_unplugged_content.rb
+++ bin/test/i18n/resources/dashboard/test_unplugged_content.rb
@@ -18,6 +18,13 @@ describe I18n::Resources::Dashboard::UnpluggedContent do
     end
   end
 
+  describe '.sync_down' do
+    it 'sync-down UnpluggedContent resource' do
+      described_class::SyncDown.expects(:perform).once
+      described_class.sync_down
+    end
+  end
+
   describe '.sync_out' do
     it 'sync-out UnpluggedContent resource' do
       described_class::SyncOut.expects(:perform).once
