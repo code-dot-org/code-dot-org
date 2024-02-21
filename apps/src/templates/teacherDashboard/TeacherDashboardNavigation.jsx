abfacf2e4ee363305d871b417173b19e53257a61
Liam Frye-Mason <liam.fryemason@code.org>
2024-02-21 13:19:09 -0800

    Revert " Change teacher dashboard page width (#56330)"
    This reverts commit b7f380da20f7f295484502739fbb439677cdd28b.

--- apps/src/templates/teacherDashboard/TeacherDashboardNavigation.jsx
+++ apps/src/templates/teacherDashboard/TeacherDashboardNavigation.jsx
@@ -131,7 +131,7 @@ export default class TeacherDashboardNavigation extends Component {
     const links = this.props.links || teacherDashboardLinks;
     const containerStyles = this.state.shouldScroll
       ? {...styles.container, ...styles.scrollableContainer}
-      : styles.container;
+      : {...styles.container, ...styles.centerContainer};
     const chevronStyles = userAgentParser.isSafari()
       ? {...styles.chevron, ...styles.safariSticky}
       : styles.chevron;
@@ -184,6 +184,9 @@ const styles = {
     marginBottom: 10,
     overflow: 'hidden',
     position: 'relative',
+  },
+  centerContainer: {
+    justifyContent: 'center',
   },
   scrollableContainer: {
     overflowX: 'scroll',
