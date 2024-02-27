941f12c36740efd12cb93a98c408596d8c1b0060
molly-moen <molly@code.org>
2024-02-27 14:56:28 -0800

    remove function

--- apps/src/blockly/addons/cdoVariables.ts
+++ apps/src/blockly/addons/cdoVariables.ts
@@ -43,32 +43,4 @@ export default function initializeVariables(
       blocklyWrapper.Blocks.variables_get
     );
   }
-
-  /**
-   * Note: We should be able to remove this post-migration: https://codedotorg.atlassian.net/browse/CT-215
-   * 
-   * Return a new variable name that is not yet being used. The CDO implementation
-   * of this function accepted a baseName parameter. We are using Google Blockly's
-   * implementation here, which does the same thing. It accepts a workspace as an
-   * argument, so we ignore the _baseName argument for labs migrated to Google Blockly
-   * in favor of pulling the workspace off the associated block.
-   * Blockly's implementation can be found here:
-   * https://github.com/google/blockly/blob/45cc1e8feaebdc90792e56faf6f0d8364df024e6/core/variables.ts#L169
-   * CDO implementation can be found here:
-   * https://github.com/code-dot-org/blockly/blob/f012d8262f21bae3e54fb11dd8bc29cf0d29f3cd/core/utils/variables.js#L259
-
-   * @param {string} _baseName Ignored/for backwards compatibility only; the
-   *   base name to use when generating a unique name.
-   * @param {Blockly.Block} block Block from which to pull the current workspace.
-   * @return {string} New variable name.
-   */
-  // const originalGenerateUniqueName =
-  //   blocklyWrapper.Variables.generateUniqueName; // Core Blockly function is originally stored here and then gets overwritten
-  // blocklyWrapper.Variables.generateUniqueName = function (
-  //   _baseName = null,
-  //   block
-  // ) {
-  //   const workspace = block.workspace;
-  //   return originalGenerateUniqueName.call(this, workspace);
-  // };
 }
