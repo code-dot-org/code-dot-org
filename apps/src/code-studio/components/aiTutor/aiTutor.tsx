d77e5940d118c254c8c0bd0094b8d672cb6dfb02
Erin Bond <erin.bond@code.org>
2024-02-28 11:59:07 -0500

    move initial chat messages to constants file

--- apps/src/code-studio/components/aiTutor/aiTutor.tsx
+++ apps/src/code-studio/components/aiTutor/aiTutor.tsx
@@ -2,7 +2,17 @@ import React, {useCallback, useEffect} from 'react';
 import style from './ai-tutor.module.scss';
 import ChatWorkspace from './chatWorkspace';
 import {useAppDispatch, useAppSelector} from '@cdo/apps/util/reduxHooks';
-import {Role, Status, TutorType} from '@cdo/apps/aiTutor/types';
+import {TutorType} from '@cdo/apps/aiTutor/types';
+import {
+  compilationError,
+  compilationErrorFirst,
+  compilationSuccess,
+  generalChatMessage,
+  runCode,
+  testCode,
+  vaildationError,
+  validationSuccess,
+} from '@cdo/apps/aiTutor/constants';
 import {addChatMessage} from '@cdo/apps/aiTutor/redux/aiTutorRedux';
 
 // AI Tutor feature that allows students to ask for help with compilation errors
@@ -23,27 +33,6 @@ const AITutor: React.FunctionComponent = () => {
   );
 
   const setCompilationChatMessages = useCallback(() => {
-    const runCode = {
-      id: 0,
-      role: Role.ASSISTANT,
-      status: Status.OK,
-      chatMessageText: 'Run your code first and see what happens.',
-    };
-
-    const compilationSuccess = {
-      id: 0,
-      role: Role.ASSISTANT,
-      status: Status.OK,
-      chatMessageText: '🎉 Your code is compiling successfully. Great work!',
-    };
-
-    const compilationError = {
-      id: 0,
-      role: Role.ASSISTANT,
-      status: Status.OK,
-      chatMessageText:
-        'Ah! You do have an error. Submit your code, and I will try to help.',
-    };
     if (!isRunning) {
       if (!hasRunOrTestedCode) {
         dispatch(addChatMessage(runCode));
@@ -58,42 +47,12 @@ const AITutor: React.FunctionComponent = () => {
   }, [dispatch, hasCompilationError, hasRunOrTestedCode, isRunning]);
 
   const setValidationChatMessages = useCallback(() => {
-    const testCode = {
-      id: 0,
-      role: Role.ASSISTANT,
-      status: Status.OK,
-      chatMessageText: 'Test your code first and see what happens.',
-    };
-
-    const validationSuccess = {
-      id: 0,
-      role: Role.ASSISTANT,
-      status: Status.OK,
-      chatMessageText: '🎉 Your tests are passing. Wahoo!',
-    };
-
-    const compilationError = {
-      id: 0,
-      role: Role.ASSISTANT,
-      status: Status.OK,
-      chatMessageText:
-        'Uh oh! Your code has to compile successfully before we can work on passing tests.',
-    };
-
-    const vaildationError = {
-      id: 0,
-      role: Role.ASSISTANT,
-      status: Status.OK,
-      chatMessageText:
-        'Your tests are failing. Submit your code and tests, and I will try to help.',
-    };
-
     if (!isRunning) {
       if (!hasRunOrTestedCode && !validationPassed) {
         dispatch(addChatMessage(testCode));
       }
       if (hasRunOrTestedCode && hasCompilationError) {
-        dispatch(addChatMessage(compilationError));
+        dispatch(addChatMessage(compilationErrorFirst));
       }
       if (validationPassed) {
         dispatch(addChatMessage(validationSuccess));
@@ -111,12 +70,6 @@ const AITutor: React.FunctionComponent = () => {
   ]);
 
   const setGeneralChatMessages = useCallback(() => {
-    const generalChatMessage = {
-      id: 0,
-      role: Role.ASSISTANT,
-      status: Status.OK,
-      chatMessageText: 'Type your question below, and I will try to help.',
-    };
     dispatch(addChatMessage(generalChatMessage));
   }, [dispatch]);
 
