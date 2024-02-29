e1aa62f3935df5fba41c6b07f26b8f5cfabd613b
Seth Nickell <snickell@gmail.com>
2024-02-28 20:52:22 -1000

    Remove debug cruft

--- dashboard/test/controllers/datablock_storage_controller_test.rb
+++ dashboard/test/controllers/datablock_storage_controller_test.rb
@@ -353,7 +353,6 @@ class DatablockStorageControllerTest < ActionDispatch::IntegrationTest
 
     put _url(:coerce_column), params: {table_name: 'mytable', column_name: 'foo', column_type: 'boolean'}
 
-    # skip "FIXME: controller bug, coerce_column does not return a warning, AND it modifies invalid booleans whereas it should leave them alone (stay as a string in this case)"
     assert_response :bad_request
     assert_equal 'CANNOT_CONVERT_COLUMN_TYPE', JSON.parse(@response.body)['type']
 
