f0b144bcf4db92ee90b261a17720ebb4694edbb0
Hannah Bergam <hannahbergam@gmail.com>
2024-02-28 22:10:02 -0800

    looking at label errors

--- apps/.eslintrc.js
+++ apps/.eslintrc.js
@@ -7,8 +7,8 @@
 // reenabling.
 const rulesToEventuallyReenable = {
   'jsx-a11y/anchor-is-valid': 'off',
+  'jsx-a11y/blob': 'off',
   'jsx-a11y/click-events-have-key-events': 'off',
-  'jsx-a11y/label-has-associated-control': 'off',
   'jsx-a11y/mouse-events-have-key-events': 'off',
   'jsx-a11y/no-noninteractive-element-interactions': 'off',
   'jsx-a11y/no-static-element-interactions': 'off',
