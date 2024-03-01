97fc62030cf3947cf3283cdefb80173589646b21
Alice Fisher <alice.fisher@code.org>
2024-03-01 15:44:14 +0000

    with menu break

--- apps/src/templates/projects/PersonalProjectsTableActionsCell.jsx
+++ apps/src/templates/projects/PersonalProjectsTableActionsCell.jsx
@@ -82,14 +82,12 @@ export class PersonalProjectsTableActionsCell extends Component {
             <PopUpMenu.Item onClick={this.onRemix}>
               {i18n.remix()}
             </PopUpMenu.Item>
+            <MenuBreak />
             {!this.props.isFrozen && (
-              <div style={styles.deleteOption}>
-                <MenuBreak />
-                <PopUpMenu.Item onClick={this.onDelete} color={color.red}>
-                  <FontAwesome icon="times-circle" style={styles.xIcon} />
-                  {i18n.delete()}
-                </PopUpMenu.Item>
-              </div>
+              <PopUpMenu.Item onClick={this.onDelete} color={color.red}>
+                <FontAwesome icon="times-circle" style={styles.xIcon} />
+                {i18n.delete()}
+              </PopUpMenu.Item>
             )}
           </QuickActionsCell>
         )}
