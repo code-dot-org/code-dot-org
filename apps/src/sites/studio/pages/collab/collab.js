8e2451dc7aa8ace3f98e284273d537188f9070ba
Seth Nickell <snickell@gmail.com>
2024-03-01 23:38:13 -1000

    basic init of codemirror works

--- apps/src/sites/studio/pages/collab/collab.js
+++ apps/src/sites/studio/pages/collab/collab.js
@@ -1,1 +1,76 @@
-alert('collab.js loaded!');
+import {EditorState} from '@codemirror/state';
+import {EditorView} from '@codemirror/view';
+import {javascript} from '@codemirror/lang-javascript';
+import {editorConfig} from '@cdo/apps/lab2/views/components/editor/editorConfig';
+
+import {collab, receiveUpdates, sendableUpdates} from '@codemirror/collab';
+import {createConsumer} from '@rails/actioncable';
+
+class ActionCableConnection {
+  constructor(documentId, onReceived) {
+    this.documentId = documentId;
+    this.onReceived = onReceived;
+    this.setupChannel();
+  }
+
+  setupChannel() {
+    const consumer = createConsumer(); // Ensure you've correctly configured your Action Cable consumer
+
+    this.channel = consumer.subscriptions.create(
+      {channel: 'DocumentsChannel', document_id: this.documentId},
+      {
+        received: this.onReceived,
+      }
+    );
+  }
+
+  send(data) {
+    this.channel.send(data);
+  }
+}
+
+function setupEditor(documentId) {
+  console.log(`Initializing collaborative editor with collabId: ${documentId}`);
+
+  const connection = new ActionCableConnection(documentId, data => {
+    if (data.updates) {
+      const updates = data.updates.map(u => Update.fromJSON(u));
+      view.dispatch(receiveUpdates(view.state, updates, data.clientID));
+    }
+  });
+
+  const startState = EditorState.create({
+    doc: '',
+    extensions: [
+      ...editorConfig,
+      javascript(),
+      //collab({clientID: Math.random().toString(36).substring(2)}),
+    ],
+  });
+
+  const view = new EditorView({
+    state: startState,
+    parent: document.querySelector('#editor'),
+    dispatchTransaction(transaction) {
+      let newState = view.state.apply(transaction);
+      view.updateState(newState);
+
+      let updates = sendableUpdates(newState);
+      if (updates.length) {
+        connection.send({
+          document_id: documentId,
+          updates: updates.map(u => u.toJSON()),
+          clientID: newState.facet(collab).clientID,
+        });
+      }
+    },
+  });
+
+  console.log('Done with setupEditor(): success');
+
+  return view;
+}
+
+document.addEventListener('DOMContentLoaded', event => {
+  setupEditor(window.collabId);
+});
--- dashboard/app/controllers/collab_controller.rb
+++ dashboard/app/controllers/collab_controller.rb
@@ -1,4 +1,5 @@
 class CollabController < ApplicationController
   def collab
+    @collab_id = params[:id]
   end
 end
