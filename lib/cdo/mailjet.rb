984daf71bd8efcd0b9f4a64df68c849e067c7155
Bethany Connor <bethany@code.org>
2024-03-05 13:38:53 -0500

    Create wrapper around mailjet api

---
+++ lib/cdo/mailjet.rb
@@ -0,0 +1,46 @@
+require 'mailjet'
+
+module MailJet
+  API_KEY = CDO.mailjet_api_key.freeze
+  SECRET_KEY = CDO.mailjet_secret_key.freeze
+
+  def self.create_contact(email, name)
+    configure
+    variable = Mailjet::Contact.create(
+      is_excluded_from_campaigns: "true",
+      email: email,
+      name: name
+    )
+    p variable.attributes['Data']
+  end
+
+  def self.send_template_email(to_email, template_id)
+    configure
+    variable = Mailjet::Send.create(messages:
+      [{
+        From: {
+          Email: 'bethany@code.org',
+          Name: 'Me'
+        },
+        To: [
+          {
+            Email: to_email,
+            Name: 'You'
+          }
+        ],
+        TemplateID: template_id,
+        TemplateLanguage: true,
+        Subject: 'My first Mailjet Email!',
+      }]
+    )
+    p variable.attributes['Messages']
+  end
+
+  def self.configure
+    Mailjet.configure do |config|
+      config.api_key = API_KEY
+      config.secret_key = SECRET_KEY
+      config.api_version = "v3.1"
+    end
+  end
+end
