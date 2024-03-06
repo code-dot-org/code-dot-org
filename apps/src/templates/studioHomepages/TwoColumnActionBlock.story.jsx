5fdc88b17b97f9df458249622dfb281464f7832c
breville <brendan@code.org>
2024-03-06 01:35:50 +0000

    Prettier

--- apps/src/templates/studioHomepages/TwoColumnActionBlock.story.jsx
+++ apps/src/templates/studioHomepages/TwoColumnActionBlock.story.jsx
@@ -44,13 +44,15 @@ export const ExtraButtonText = () => (
         id: 'your_school_professional_learning',
         url: 'https://code.org/professional-learning',
         text: 'Learn more',
-        extraText: 'Click here to learn more about professional learning options.'
+        extraText:
+          'Click here to learn more about professional learning options.',
       },
       {
         id: 'your_school_administrators',
         url: 'https://code.org/administrators',
         text: 'Try this',
-        extraText: 'School administrators can learn more about our curriculum options.'
+        extraText:
+          'School administrators can learn more about our curriculum options.',
       },
     ]}
     marginBottom={'64px'}
