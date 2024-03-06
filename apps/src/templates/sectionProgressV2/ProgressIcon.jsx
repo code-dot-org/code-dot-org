a2a31abff7eff528dea2ef27b989196876e78756
Kaitie O <kaitie@code.org>
2024-03-06 14:37:52 -0600

    Update apps/src/templates/sectionProgressV2/ProgressIcon.jsx
    Co-authored-by: Eric Aderhold <eric.aderhold@code.org>

--- apps/src/templates/sectionProgressV2/ProgressIcon.jsx
+++ apps/src/templates/sectionProgressV2/ProgressIcon.jsx
@@ -52,10 +52,7 @@ export default function ProgressIcon({itemType, className}) {
         <FontAwesome
           id={'uitest-' + itemType[0]}
           icon={itemType[0]}
-          style={
-            className && itemType === ITEM_TYPE.CHOICE_LEVEL
-              ? {color: itemType[2]}
-              : {color: itemType[1]}
+          style={{color: colorOverride ? colorOverride : itemType[1]}}
           }
           className={className ? className : styles.fontAwesomeIcon}
           aria-label={PROGRESS_ICON_TITLE_PREFIX + itemType[0]}
