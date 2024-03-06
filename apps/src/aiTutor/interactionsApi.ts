9528eaec0840167e207eaf3a408d4858bfbdb6e2
Erin Bond <erin.bond@code.org>
2024-03-05 19:14:05 -0500

    add endpoint to interactionsApi

--- apps/src/aiTutor/interactionsApi.ts
+++ apps/src/aiTutor/interactionsApi.ts
@@ -23,8 +23,8 @@ export async function savePromptAndResponse(
   }
 }
 
-// Fetch student chat messages to display to teachers.
-export const fetchStudentChatMessages = async (sectionId: number) => {
+// Fetch student chat messages for a section to display to teachers.
+export const fetchChatMessagesForSection = async (sectionId: number) => {
   try {
     const response = await fetch(
       `/ai_tutor_interactions?sectionId=${sectionId}`,
@@ -40,7 +40,27 @@ export const fetchStudentChatMessages = async (sectionId: number) => {
     return data;
   } catch (error) {
     MetricsReporter.logError({
-      event: MetricEvent.AI_TUTOR_CHAT_FETCH_FAIL,
+      event: MetricEvent.AI_TUTOR_CHAT_FETCH_FAIL_FOR_SECTION,
+      errorMessage: error,
+    });
+  }
+};
+
+// Fetch student chat messages for one student.
+export const fetchChatMessagesForStudent = async (userId: number) => {
+  try {
+    const response = await fetch(`/ai_tutor_interactions?userId=${userId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': await getAuthenticityToken(),
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    MetricsReporter.logError({
+      event: MetricEvent.AI_TUTOR_CHAT_FETCH_FAIL_FOR_STUDENT,
       errorMessage: error,
     });
   }
