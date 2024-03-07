c6280d1723518d628cd8ea98ec33eb54b51fe38f
levadadenys <levada.denys@gmail.com>
2024-03-07 10:15:36 +0200

    [Design2-86] Icons Dropdown, rework Checkbox Dropdown (#56683)
    * Created IconDropdown component
    * created stories for IconDropdown component
    * created tests for IconDropdown component
    * created documentation for IconDropdown component
    * Moved CheckboxDropdown and IconDropdown to /dropdown folder
    * created _CustomDropdown private component (for DSCO internal usage only)

---
+++ apps/src/componentLibrary/dropdown/index.ts
@@ -0,0 +1,6 @@
+import CheckboxDropdown from './checkboxDropdown';
+import IconDropdown from './iconDropdown';
+
+export {dropdownColors} from '@cdo/apps/componentLibrary/common/constants';
+
+export {CheckboxDropdown, IconDropdown};
