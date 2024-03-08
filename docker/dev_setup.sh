167672e2605e7c891f24dd4318bd08f3b0484719
Darin Webb <darin@code.org>
2024-03-08 11:22:56 -0600

    WIP revert docker dev setup script

--- docker/dev_setup.sh
+++ docker/dev_setup.sh
@@ -11,9 +11,9 @@ curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | 
 
 bundle install --verbose
 
-ls ~/.aws
-cat ~/.aws/credentials
+echo "AWS_PROFILE is $AWS_PROFILE"
+# aws sts get-caller-identity
 
-bundle exec rake install --trace
+bundle exec rake install:locals_yml --trace
 
 bundle exec rake build --trace
