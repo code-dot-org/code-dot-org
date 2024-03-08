4570e57f836ad8d9e269b18e84512baec745272d
Jessica Kulwik <jessica@code.org>
2024-03-08 13:37:54 -0800

    Migrated LibraryIdCopierTest to RTL

--- apps/test/unit/code-studio/components/libraries/LibraryIdCopierTest.js
+++ apps/test/unit/code-studio/components/libraries/LibraryIdCopierTest.js
@@ -1,16 +1,15 @@
 import {expect} from '../../../../util/reconfiguredChai';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import LibraryIdCopier from '@cdo/apps/code-studio/components/libraries/LibraryIdCopier.jsx';
 
 describe('LibraryIdCopier', () => {
-  const CHANNEL_ID_SELECTOR = 'input[type="text"]';
   const channelId = '123';
-  it('displays the channel id', () => {
-    let wrapper = shallow(
-      <LibraryIdCopier libraryName="name" channelId={channelId} />
-    );
-
-    expect(wrapper.find(CHANNEL_ID_SELECTOR).props().value).to.equal(channelId);
+  it('displays the channel id', async () => {
+    render(<LibraryIdCopier channelId={channelId} />);
+    const user = userEvent.setup();
+    await user.click(screen.getByRole('button'));
+    navigator.clipboard.readText().then(clipText => expect(clipText).to.equal(channelId));
   });
 });
