109bbec02de4811ca100e14530de3bc4e2538bfb
Hannah Bergam <hannahbergam@gmail.com>
2024-03-08 12:34:52 -0500

    statsig js integration progress

--- apps/src/lib/util/StatsigReporter.js
+++ apps/src/lib/util/StatsigReporter.js
@@ -6,72 +6,42 @@ import {
   isDevelopmentEnvironment,
 } from '../../utils';
 
-import {EVENT_GROUPS} from './AnalyticsConstants';
-
-import DCDO from '@cdo/apps/dcdo';
-
 // A flag that can be toggled to send events regardless of environment
 const ALWAYS_SEND = false;
 const Statsig = require('statsig-js');
 
 class StatsigReporter {
   constructor() {
-    const api_key = 'getAPIKey'; //CDO.statsig_api_client_key;
-    Statsig.initialize(
-      api_key,
-      {userID: 'some_user_id'},
-      {environment: {tier: 'staging'}} // optional, pass options here if needed
-    );
+    const local_mode = isProductionEnvironment ? false : true;
+    const options = {
+      environment: {tier: getEnvironment()},
+      network_timeout: 5,
+      local_mode: local_mode,
+    };
+    const api_key = document.querySelector('script[statsig_api_client_key]');
+    Statsig.initialize(api_key, options);
   }
 
-  setUserProperties(userId, userType, enabledExperiments) {
+  // Utilizes Statsig's function for updating a user once we've recognized a sign in
+  setUserProperties(userId, userType) {
+    let user;
     const formattedUserId = this.formatUserId(userId);
-    // enabledExperiments sometimes has duplicates
-    const uniqueExperiments = [...new Set(enabledExperiments)].sort();
+    user.id = formattedUserId;
+    user.type = userType;
+    Statsig.updateUser(user);
 
     if (!this.shouldPutRecord(ALWAYS_SEND)) {
       this.log(
-        `User properties: userId: ${formattedUserId}, userType: ${userType}, signInState: ${!!userId}, enabledExperiments: ${uniqueExperiments}`
+        `User properties: userId: ${formattedUserId}, userType: ${userType}, signInState: ${!!userId}`
       );
     }
   }
 
   sendEvent(eventName, payload) {
-    //we can enable/disable sampling based upon the event's group, which is defined in the AnalyticsConstants
-    //file, in the EVENT_GROUPS variable. If an event is not listed in that mapping, it defaults to 'ungrouped'
-
-    // event groups can define a sample rate, as part of the 'amplitude-event-sample-rates' DCDO flag.
-    // e.g. { 'ungrouped' : 1, 'video-events' : 0.25, 'expensive-events' : -1 }
-    // which would, respectively, log:
-    //   all events w/o sampling for 'ungrouped'
-    //   video-events at a 25% sampling rate
-    //   and explicitly turn off expensive-events
-    // sample rate should be in the range (0,1), exclusive
-
-    // if a eventGroup is not defined or set to 0 or 1, then all events will be logged w/o sampling.
-    // if set to -1, then nothing will be logged.
-
-    const sampleRates = DCDO.get('amplitude-event-sample-rates', {});
-    const eventGroup = EVENT_GROUPS[eventName] || 'ungrouped';
-    const sampleRate = sampleRates[eventGroup] || 1;
-
-    const sampleRateString =
-      sampleRate !== 1 ? `[SAMPLE RATE ${sampleRate} for ${eventGroup}]` : '';
-    if (sampleRate < 0 || Math.random() > sampleRate) {
-      this.log(
-        `[SKIPPED SAMPLED EVENT]${sampleRateString}${eventName}. Payload: ${JSON.stringify(
-          {
-            payload,
-          }
-        )}`
-      );
-      return;
-    }
-
     if (this.shouldPutRecord(ALWAYS_SEND)) {
       if (!eventName) {
         logToCloud.addPageAction(
-          logToCloud.PageAction.NoValidAmplitudeEventNameError,
+          logToCloud.PageAction.NoValidStatsigEventNameError,
           {
             payload: payload,
           }
@@ -82,7 +52,7 @@ class StatsigReporter {
       }
     } else {
       this.log(
-        `${sampleRateString}${eventName}. Payload: ${JSON.stringify({
+        `${eventName}. Payload: ${JSON.stringify({
           payload,
         })}`
       );
@@ -91,7 +61,7 @@ class StatsigReporter {
 
   log(message) {
     if (isDevelopmentEnvironment() && !IN_UNIT_TEST) {
-      console.log(`[AMPLITUDE ANALYTICS EVENT]: ${message}`);
+      console.log(`[STATSIG ANALYTICS EVENT]: ${message}`);
     }
   }
 
