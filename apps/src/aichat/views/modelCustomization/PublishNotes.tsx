582cf493a7b3a4b6326788011fc85173d4762fc8
Ben Brooks <ben@code.org>
2024-03-07 16:39:43 -0800

    Rough customization from levelbuilder for prompt and model card tabs

--- apps/src/aichat/views/modelCustomization/PublishNotes.tsx
+++ apps/src/aichat/views/modelCustomization/PublishNotes.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
 
 import {StrongText} from '@cdo/apps/componentLibrary/typography/TypographyElements';
 import styles from '../model-customization-workspace.module.scss';
+import {LabState} from '@cdo/apps/lab2/lab2Redux';
+import {AichatLevelProperties} from '@cdo/apps/aichat/types';
 
 const INPUTS = [
-  {id: 'intended-uses', text: 'Intended Uses'},
-  {id: 'limitations-and-warnings', text: 'Limitations and Warnings'},
-  {id: 'testing-and-evaluation', text: 'Testing and Evaluation'},
+  {id: 'description', text: 'Description'},
+  {id: 'intendedUse', text: 'Intended Use'},
+  {id: 'limitationsAndWarnings', text: 'Limitations and Warnings'},
+  {id: 'testingAndEvaluation', text: 'Testing and Evaluation'},
 ];
 
+// what does it mean for this to be hidden? hide whole tab?
 const PublishNotes: React.FunctionComponent = () => {
+  // deal with AiCustomizations vs LevelAiCustomizations distinction in requiring model
+  const {modelCardInfo} = useSelector(
+    (state: {lab: LabState}) =>
+      (state.lab.levelProperties as AichatLevelProperties | undefined)
+        ?.initialAiCustomizations || {
+        botName: {value: 'bot name', visibility: 'editable'},
+        temperature: {value: 0.5, visibility: 'editable'},
+        systemPrompt: {value: 'a system prompt', visibility: 'editable'},
+        retrievalContexts: {
+          value: ['retrieval 1', 'retrieval 2'],
+          visibility: 'editable',
+        },
+        modelCardInfo: {
+          value: {
+            description: 'a description',
+            intendedUse: 'intended use',
+            limitationsAndWarnings: 'limitations and warnings',
+            testingAndEvaluation: 'testing and evaluation',
+            askAboutTopics: 'ask about topics',
+          },
+          visibility: 'editable',
+        },
+      }
+  );
+
+  console.log(modelCardInfo);
+
   return (
     <div className={styles.verticalFlexContainer}>
       <div>
         {INPUTS.map(input => {
           return (
-            <div className={styles.inputContainer} key={input.id}>
-              <label htmlFor={input.id}>
-                <StrongText>{input.text}</StrongText>
-              </label>
-              <textarea id={input.id} />
-            </div>
+            modelCardInfo.visibility !== 'hidden' && (
+              <div className={styles.inputContainer} key={input.id}>
+                <label htmlFor={input.id}>
+                  <StrongText>{input.text}</StrongText>
+                </label>
+                <textarea
+                  id={input.id}
+                  disabled={modelCardInfo.visibility === 'readonly'}
+                  value={modelCardInfo.value[input.id]}
+                />
+              </div>
+            )
           );
         })}
       </div>
--- apps/src/aichat/views/modelCustomization/RetrievalCustomization.tsx
+++ apps/src/aichat/views/modelCustomization/RetrievalCustomization.tsx
@@ -1,12 +1,41 @@
 import React, {useState} from 'react';
+import {useSelector} from 'react-redux';
 
 import FontAwesomeV6Icon from '@cdo/apps/componentLibrary/fontAwesomeV6Icon/FontAwesomeV6Icon';
 import {StrongText} from '@cdo/apps/componentLibrary/typography/TypographyElements';
 import modelCustomizationStyles from '../model-customization-workspace.module.scss';
 import styles from './retrieval-customization.module.scss';
+import {LabState} from '@cdo/apps/lab2/lab2Redux';
+import {AichatLevelProperties} from '@cdo/apps/aichat/types';
 
+// what does it mean for retrieval to be hidden? hide whole tab?
 const RetrievalCustomization: React.FunctionComponent = () => {
-  const [messages, setMessages] = useState<string[]>([]);
+  // deal with AiCustomizations vs LevelAiCustomizations distinction in requiring model
+  const initialAiCustomizations = useSelector(
+    (state: {lab: LabState}) =>
+      (state.lab.levelProperties as AichatLevelProperties | undefined)
+        ?.initialAiCustomizations || {
+        botName: {value: 'bot name', visibility: 'editable'},
+        temperature: {value: 0.5, visibility: 'editable'},
+        systemPrompt: {value: 'a system prompt', visibility: 'editable'},
+        retrievalContexts: {
+          value: ['retrieval 1', 'retrieval 2'],
+          visibility: 'editable',
+        },
+        modelCardInfo: {
+          description: 'a description',
+          intendedUse: 'intended use',
+          limitationsAndWarnings: 'limitations and warnings',
+          testingAndEvaluation: 'testing and evaluation',
+          askAboutTopics: 'ask about topics',
+          visibility: 'editable',
+        },
+      }
+  );
+
+  const [messages, setMessages] = useState<string[]>(
+    initialAiCustomizations.retrievalContexts.value
+  );
   const [newMessage, setNewMessage] = useState('');
 
   const onAdd = () => {
