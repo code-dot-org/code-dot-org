c1e8ac7ca39979173f9287aae1f278aafdb95fd5
Alice Fisher <alice.fisher@code.org>
2024-03-08 16:14:44 +0000

    fix storybook

--- apps/src/templates/projects/PersonalProjectsTable.story.jsx
+++ apps/src/templates/projects/PersonalProjectsTable.story.jsx
@@ -5,14 +5,14 @@ import {Provider} from 'react-redux';
 import {reduxStore} from '@cdo/storybook/decorators';
 import publishDialog from '@cdo/apps/templates/projects/publishDialog/publishDialogRedux';
 import deleteDialog from '@cdo/apps/templates/projects/deleteDialog/deleteProjectDialogRedux';
-import projectInfoDialog from '@cdo/apps/templates/projects/projectInfoDialog/projectInfoDialogRedux';
+import frozenProjectInfoDialog from '@cdo/apps/templates/projects/frozenProjectInfoDialog/frozenProjectInfoDialogRedux';
 export default {
   component: PersonalProjectsTable,
 };
 
 const Template = args => (
   <Provider
-    store={reduxStore({publishDialog, deleteDialog, projectInfoDialog})}
+    store={reduxStore({publishDialog, deleteDialog, frozenProjectInfoDialog})}
   >
     <PersonalProjectsTable {...args} />
   </Provider>
