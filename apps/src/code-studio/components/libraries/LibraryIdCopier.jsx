3f1e1cc5859efc9c377335639d82106a9f4fb168
Jessica Kulwik <jessica@code.org>
2024-03-12 15:35:20 -0700

    Migrated to functional component

--- apps/src/code-studio/components/libraries/LibraryIdCopier.jsx
+++ apps/src/code-studio/components/libraries/LibraryIdCopier.jsx
@@ -1,37 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import style from './library.module.scss';
 import i18n from '@cdo/locale';
 import Button from '@cdo/apps/templates/Button';
 
-export default class LibraryIdCopier extends React.Component {
-  static propTypes = {
-    channelId: PropTypes.string.isRequired,
-  };
+export default function LibraryIdCopier({channelId}) {
+  let refObject;
 
-  copyChannelId = () => {
-    this.channelId.select();
-    navigator.clipboard.writeText(this.props.channelId);
-  };
-
-  render() {
-    const {channelId} = this.props;
-    return (
-      <div>
-        <input
-          type="text"
-          ref={channelId => (this.channelId = channelId)}
-          onClick={event => event.target.select()}
-          readOnly
-          value={channelId}
-          className={style.idTextbox}
-        />
-        <Button
-          onClick={this.copyChannelId}
-          text={i18n.copyId()}
-          className={style.copyButton}
-        />
-      </div>
-    );
-  }
+  return (
+    <>
+      <input
+        type="text"
+        ref={channelId => (refObject = channelId)}
+        onClick={event => event.target.select()}
+        readOnly
+        value={channelId}
+        className={style.idTextbox}
+      />
+      <Button
+        onClick={() => {
+          refObject.select();
+          navigator.clipboard.writeText(channelId);
+        }}
+        text={i18n.copyId()}
+        className={style.copyButton}
+      />
+    </>
+  );
 }
