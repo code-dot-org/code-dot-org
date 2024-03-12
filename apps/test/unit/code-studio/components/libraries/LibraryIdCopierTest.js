267feaea24e39958dea83cb016da4e75ec612f9a
Jessica Kulwik <jessica@code.org>
2024-03-12 15:09:07 -0700

    Re-imported react

--- apps/test/unit/code-studio/components/libraries/LibraryIdCopierTest.js
+++ apps/test/unit/code-studio/components/libraries/LibraryIdCopierTest.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {expect} from '../../../../util/reconfiguredChai';
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
