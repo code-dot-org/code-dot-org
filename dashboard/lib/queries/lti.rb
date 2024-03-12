664d89148446f42f6a27bf41ae1eab4661ff84c6
Mario Gil Correa <mario.gil-correa@code.org>
2024-03-11 21:10:31 -0700

    refactor get_user to query LtiUserIdentity table

--- dashboard/lib/queries/lti.rb
+++ dashboard/lib/queries/lti.rb
@@ -3,8 +3,7 @@ require 'authentication_option'
 
 class Queries::Lti
   def self.get_user(id_token)
-    auth_id = Policies::Lti.generate_auth_id(id_token)
-    User.find_by_credential(type: AuthenticationOption::LTI_V1, id: auth_id)
+    LtiUserIdentity.find_by(subject: id_token[:sub])&.user
   end
 
   def self.get_lti_integration(issuer, client_id)
