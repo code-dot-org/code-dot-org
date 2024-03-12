8e4b9d4880fca3faee3ecff42bbef6050ae26e73
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2024-03-12 14:49:31 -0700

    reorder videos and update naming conventions on carousel_how_ai_works_video.haml

--- pegasus/sites.v3/code.org/public/js/page/video_page.js
+++ pegasus/sites.v3/code.org/public/js/page/video_page.js
@@ -67,11 +67,11 @@ $(document).ready(function () {
     },
   });
 
-  let idFlexible = "#flexible_units_carousel";
-  handleScreenSize("flexible_units_carousel");
-  $(idFlexible + " .slides").carouFredSel({
+  let idHowAiWorks = "#how-ai-works-videos";
+  handleScreenSize("how-ai-works-videos");
+  $(idHowAiWorks + " .slides").carouFredSel({
     auto: false,
-    pagination: "#flexible_units_carousel-pagination",
+    pagination: "#how-ai-works-videos-pagination",
     prev: "#prev_1",
     next: "#next_1",
     responsive: true,
