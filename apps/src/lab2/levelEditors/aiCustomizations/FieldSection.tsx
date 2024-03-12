232a71214127a0138e619ed795dd0e671f25048c
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2024-03-12 09:55:36 -0700

    Gen AI: make level edit sub-sections collapsible (#57180)
    * Gen AI: make level edit sub-sections collapsible
    
    * use fragment

--- apps/src/lab2/levelEditors/aiCustomizations/FieldSection.tsx
+++ apps/src/lab2/levelEditors/aiCustomizations/FieldSection.tsx
@@ -1,10 +1,11 @@
 import {AiCustomizations} from '@cdo/apps/aichat/types';
 import React, {useContext} from 'react';
-import {BodyFourText, BodyOneText} from '@cdo/apps/componentLibrary/typography';
+import {BodyFourText} from '@cdo/apps/componentLibrary/typography';
 import classNames from 'classnames';
 import moduleStyles from './edit-ai-customizations.module.scss';
 import VisibilityDropdown from './VisibilityDropdown';
 import {UpdateContext} from './UpdateContext';
+import CollapsibleSection from './CollapsibleSection';
 
 interface FieldSectionProps {
   fieldName: keyof AiCustomizations;
@@ -32,38 +33,39 @@ const FieldSection: React.FunctionComponent<FieldSectionProps> = ({
   return (
     <div className={moduleStyles.fieldSection}>
       <hr />
-      <BodyOneText>{labelText}</BodyOneText>
-      {description && (
-        <BodyFourText>
-          <i>{description}</i>
-        </BodyFourText>
-      )}
-      <div className={moduleStyles.fieldRow}>
-        <div className={moduleStyles['field-value']}>
-          <label htmlFor={fieldName}>Initial Value(s)</label>
-          {inputType === 'custom' && inputNode ? (
-            inputNode
-          ) : (
-            <InputTag
-              id={fieldName}
-              type={inputType}
-              value={
-                (aiCustomizations[fieldName]?.value as string | number) ||
-                (inputType === 'number' ? 0 : '')
-              }
-              onChange={e => setPropertyValue(fieldName, e.target.value)}
-              className={classNames(
-                inputType === 'textarea' && moduleStyles.textarea
-              )}
-              {...(inputType === 'number' && {min, max, step})}
-            />
-          )}
+      <CollapsibleSection title={labelText}>
+        {description && (
+          <BodyFourText>
+            <i>{description}</i>
+          </BodyFourText>
+        )}
+        <div className={moduleStyles.fieldRow}>
+          <div className={moduleStyles['field-value']}>
+            <label htmlFor={fieldName}>Initial Value(s)</label>
+            {inputType === 'custom' && inputNode ? (
+              inputNode
+            ) : (
+              <InputTag
+                id={fieldName}
+                type={inputType}
+                value={
+                  (aiCustomizations[fieldName]?.value as string | number) ||
+                  (inputType === 'number' ? 0 : '')
+                }
+                onChange={e => setPropertyValue(fieldName, e.target.value)}
+                className={classNames(
+                  inputType === 'textarea' && moduleStyles.textarea
+                )}
+                {...(inputType === 'number' && {min, max, step})}
+              />
+            )}
+          </div>
+          <VisibilityDropdown
+            value={aiCustomizations[fieldName]?.visibility || 'editable'}
+            property={fieldName}
+          />
         </div>
-        <VisibilityDropdown
-          value={aiCustomizations[fieldName]?.visibility || 'editable'}
-          property={fieldName}
-        />
-      </div>
+      </CollapsibleSection>
     </div>
   );
 };
