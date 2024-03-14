9931968ae6793458467f37c9d0acb7a0e8a87267
Seth Nickell <snickell@gmail.com>
2024-03-13 23:43:28 -1000

    Fix tests: Mysql2::Error: Cannot truncate a table referenced in a foreign key constraint Don't impose an FK constraint on the projects table Because common_test_helper.rb was doing truncate operations on the Projects table, you can't have an FK to it or you get the mysql error.

--- dashboard/db/migrate/20240215100626_create_project_use_datablock_storages.rb
+++ dashboard/db/migrate/20240215100626_create_project_use_datablock_storages.rb
@@ -6,7 +6,7 @@
 class CreateProjectUseDatablockStorages < ActiveRecord::Migration[6.0]
   def change
     create_table :project_use_datablock_storages do |t|
-      t.references :project, null: false, foreign_key: true, type: :integer, index: true
+      t.integer :project_id, null: false, index: true
       t.boolean :use_datablock_storage, default: false, null: false
     end
   end
