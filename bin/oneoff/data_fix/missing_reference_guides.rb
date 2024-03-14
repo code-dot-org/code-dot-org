a8f6e8cf9a3f478bf3d25e66435e522572f1eb13
Vijaya Manohararaj <vijaya.manohararaj@code.org>
2024-03-14 10:27:26 -0700

    PR comments

--- bin/oneoff/data_fix/missing_reference_guides.rb
+++ bin/oneoff/data_fix/missing_reference_guides.rb
@@ -1,12 +1,10 @@
 #!/usr/bin/env ruby
 
-# Link to reference guides for csd-2023 and csd-2024 don't work
-# The underlying root cause is that the clone for csd-2023 missed
-# some set of standard steps resulting in missing links, reference
-# guides being one of them. csd-2024 was cloned off of that resulting
-# in the same issue.
-# This script clones reference guides and links them to the appropriate
-# courses.
+# https://codedotorg.atlassian.net/browse/TEACH-840
+# CSD-2023 and CSD-2024 don't have any reference guides linked to them
+# because of an issue in the way these units were originally cloned.
+# This script clones the reference guides which were missed and links
+#  them to the appropriate courses.
 
 require_relative '../../../dashboard/config/environment'
 
@@ -14,14 +12,13 @@ def data_fix_reference_guides
   raise unless Rails.application.config.levelbuilder_mode
 
   # List of course versions to be fixed
-  course_versions_to_fix = ['csd-2023', 'csd-2024']
+  course_version_to_fix = CurriculumHelper.find_matching_course_version('csd-2024')
+
+  # Source course version with the correct set of reference guides
   source_course_version = CurriculumHelper.find_matching_course_version('csd-2022')
 
-  course_versions_to_fix.each do |course|
-    destination_course_version = CurriculumHelper.find_matching_course_version(course)
-    source_course_version&.reference_guides&.each do |reference_guide|
-      reference_guide.copy_to_course_version(destination_course_version)
-    end
+  source_course_version&.reference_guides&.each do |reference_guide|
+    reference_guide.copy_to_course_version(course_version_to_fix)
   end
 end
 
