bbc8911c74f0084313ab990712e91397b277b21d
Darin Webb <darin@code.org>
2024-03-19 16:35:45 -0700

    publish standard metrics for all jobs

--- dashboard/app/jobs/application_job.rb
+++ dashboard/app/jobs/application_job.rb
@@ -1,7 +1,72 @@
+require 'cdo/aws/metrics'
+
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+
+  METRICS_NAMESPACE = 'code-dot-org/ActiveJob'
+
+  def self.common_dimensions
+    [
+      {
+        name: 'Environment',
+        value: CDO.rack_env
+      },
+      {
+        name: 'JobName',
+        value: name
+      }
+    ]
+  end
+
+  # Parent class callbacks are called in addition to any callbacks defined in the job subclass
+  # https://guides.rubyonrails.org/v6.0/active_job_basics.html#available-callbacks
+
+  before_perform do |job|
+    @perform_started_at = Time.now
+    wait_time = @perform_started_at - Time.parse(job.enqueued_at)
+
+    metric = {
+      metric_name: 'WaitTime',
+      value: wait_time,
+      unit: 'Seconds',
+      dimensions: job.class.common_dimensions,
+    }
+
+    pp metric
+    Cdo::Metrics.put_metric(METRICS_NAMESPACE, metric)
+  end
+
+  after_perform do |job|
+    perform_complete_at = Time.now
+    execution_time = perform_complete_at - @perform_started_at
+    total_time = perform_complete_at - Time.parse(job.enqueued_at)
+    jobs_in_queue = Delayed::Job.where(queue: job.queue_name).where("handler LIKE ?", "%job_class: #{job.class.name}%").count
+
+    metrics = [
+      {
+        metric_name: 'ExecutionTime',
+        value: execution_time,
+        unit: 'Seconds',
+        dimensions: job.class.common_dimensions,
+      },
+      {
+        metric_name: 'TotalTime',
+        value: total_time,
+        unit: 'Seconds',
+        dimensions: job.class.common_dimensions,
+      },
+      {
+        metric_name: 'JobsInQueue',
+        value: jobs_in_queue,
+        unit: 'Count',
+        dimensions: job.class.common_dimensions,
+      }
+    ]
+
+    Cdo::Metrics.push(METRICS_NAMESPACE, metrics)
+  end
 end
