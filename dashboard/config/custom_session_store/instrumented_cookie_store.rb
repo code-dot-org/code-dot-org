0c9ce6357f8d54bfa9562d31763b45153ad657c9
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-03-19 11:40:14 -0700

    misc tweaks

--- dashboard/config/custom_session_store/instrumented_cookie_store.rb
+++ dashboard/config/custom_session_store/instrumented_cookie_store.rb
@@ -4,24 +4,26 @@ require 'objspace'
 module ActionDispatch
   module Session
     class InstrumentedCookieStore < CookieStore
-      METRICS_NAMESPACE = 'Session Store'.freeze
+      METRICS_NAMESPACE = 'code-dot-org/ActionDispatch'.freeze
 
       def delete_session(req, session_id, options)
-        Cdo::Metrics.put("#{METRICS_NAMESPACE}/delete_session", 1, {SessionId: session_id})
+        Cdo::Metrics.put(METRICS_NAMESPACE, 'delete_session_count', 1, {Environment: CDO.rack_env})
         super(req, session_id, options)
       end
 
       def load_session(req)
         session_id, session_data = super(req)
+        Cdo::Metrics.put(METRICS_NAMESPACE, 'load_session_count', 1, {Environment: CDO.rack_env})
         session_size = ObjectSpace.memsize_of(session_data)
-        Cdo::Metrics.put("#{METRICS_NAMESPACE}/load_session", 1, {SessionId: session_id, SessionSizeInBytes: session_size})
+        Cdo::Metrics.put(METRICS_NAMESPACE, 'load_session_size', session_size, {Environment: CDO.rack_env})
         [session_id, session_data]
       end
 
       private def write_session(req, sid, session_data, options)
         data = super(req, sid, session_data, options)
+        Cdo::Metrics.put(METRICS_NAMESPACE, 'write_session_count', 1, {Environment: CDO.rack_env})
         session_size = ObjectSpace.memsize_of(session_data)
-        Cdo::Metrics.put("#{METRICS_NAMESPACE}/write_session", 1, {SessionId: sid, SessionSizeInBytes: session_size})
+        Cdo::Metrics.put(METRICS_NAMESPACE, 'write_session_size', session_size, {Environment: CDO.rack_env})
         data
       end
     end
