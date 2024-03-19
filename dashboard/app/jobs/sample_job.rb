a5f0ae6b138af04f848e6e063b217305090302b5
Darin Webb <darin@code.org>
2024-03-19 16:42:57 -0700

    add a sample job class for local dev

---
+++ dashboard/app/jobs/sample_job.rb
@@ -0,0 +1,24 @@
+# This is a sample job handy for exercising the queue locally From
+# dashboard-console, enqueue via SampleJob.perform_later('world') By default
+# you'll be using the 'async' adapter, which will run the job immediately If you
+# want to test the job with the 'delayed_job' adapter, you can do so by editing
+# "dashboard/config/environments/development.rb". See comments near
+# `config.active_job.queue_adapter` for more detail.
+
+class SampleJob < ApplicationJob
+  queue_as :default
+
+  def perform(*message)
+    puts "Hello, #{message}"
+    sleep(rand(1..5))
+    puts "Goodbye, #{message}"
+  end
+
+  before_perform do |job|
+    puts "Before perform in #{job.class.name}"
+  end
+
+  after_perform do |job|
+    puts "After perform in #{job.class.name}"
+  end
+end
