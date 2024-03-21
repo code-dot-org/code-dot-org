f51c7d1f357546fcde9bd40df8017f80cb917ab3
Juan Manzo <juan.manzo@code.org>
2024-03-21 13:24:51 -0700

    CHange url link placeholder

--- apps/src/lib/ui/lti/sync/LtiSectionSyncDialogHelpers.tsx
+++ apps/src/lib/ui/lti/sync/LtiSectionSyncDialogHelpers.tsx
@@ -3,7 +3,7 @@ import i18n from '@cdo/locale';
 export const getRosterSyncErrorMessage = (error: string) => {
   switch (error) {
     case 'wrong_context':
-      return i18n.ltiSectionSyncDialogErrorWrongContext({url: 'example.com'});
+      return i18n.ltiSectionSyncDialogErrorWrongContext({url: '/'});
     case 'no_integration':
       return i18n.ltiSectionSyncDialogErrorNoIntegration();
     case 'no_section':
