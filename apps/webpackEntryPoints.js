2348b05cd03aa47036400e629d0419beb914b5b6
Turner Riley <56283563+TurnerRiley@users.noreply.github.com>
2024-03-21 14:50:01 -0700

    Update /poetry page (#57255)
    * setup new page
    
    * add all sections
    
    * finish page
    
    * pr feedback
    
    * pr feedback and using white text tag
    
    * simplified styles and responded to pr feedback

--- apps/webpackEntryPoints.js
+++ apps/webpackEntryPoints.js
@@ -190,6 +190,7 @@ const PEGASUS_ENTRIES = {
   'code.org/views/swiper': './src/sites/code.org/pages/views/swiper.js',
   'code.org/views/swiper_page_csd': './src/sites/code.org/pages/views/swiper_page_csd.js',
   'code.org/views/swiper_page_self_paced_pl': './src/sites/code.org/pages/views/swiper_page_self_paced_pl.js',
+  'code.org/public/poetry': './src/sites/code.org/pages/public/poetry.js',
 
   // hourofcode.com
   'hourofcode.com/public/events/index': './src/sites/hourofcode.com/pages/public/events/index.js',
