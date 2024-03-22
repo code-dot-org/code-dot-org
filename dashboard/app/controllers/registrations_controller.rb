2ca850a62938f670e24e380284b07db7633a3154
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-03-22 09:51:34 -0700

    feat: add DCDO flag for posting email param to finish signup page
    This PR adds scaffolding to enable email addresses to bypass the partial
    registration cache and go directly into the finish signup page. This is
    accomplished by adding a new DCDO flag `student-email-post-enabled`
    which is off by default.

--- dashboard/app/controllers/registrations_controller.rb
+++ dashboard/app/controllers/registrations_controller.rb
@@ -24,7 +24,13 @@ class RegistrationsController < Devise::RegistrationsController
     if PartialRegistration.in_progress?(session)
       user_params = params[:user] || ActionController::Parameters.new
       user_params[:user_type] ||= session[:default_sign_up_user_type]
-      @user = User.new_with_session(user_params.permit(:user_type), session)
+      if DCDO.get('student-email-post-enabled', false)
+        user_params[:email] ||= params[:email]
+
+        @user = User.new_with_session(user_params.permit(:user_type, :email), session)
+      else
+        @user = User.new_with_session(user_params.permit(:user_type), session)
+      end
     else
       save_default_sign_up_user_type
       SignUpTracking.begin_sign_up_tracking(session, split_test: true)
@@ -53,13 +59,16 @@ class RegistrationsController < Devise::RegistrationsController
     @user = User.new(begin_sign_up_params)
     @user.validate_for_finish_sign_up
     SignUpTracking.log_begin_sign_up(@user, session)
+    is_signup_post_enabled = DCDO.get('student-email-post-enabled', false)
 
     if @user.errors.blank?
       PartialRegistration.persist_attributes(session, @user)
-      redirect_to new_user_registration_path
+      redirect_to new_user_registration_path unless is_signup_post_enabled
     else
-      render 'new' # Re-render form to display validation errors
+      render 'new' unless is_signup_post_enabled # Re-render form to display validation errors
     end
+
+    render 'new' if is_signup_post_enabled
   end
 
   #
