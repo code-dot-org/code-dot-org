281b29ea5d5bbf225f14c04d111bfd1729df675a
Alice Fisher <alice.fisher@code.org>
2024-03-25 03:03:26 +0000

    remove skip and add transient project data

--- dashboard/test/controllers/featured_projects_controller_test.rb
+++ dashboard/test/controllers/featured_projects_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class FeaturedProjectsControllerTest < ActionController::TestCase
   setup do
     @project_validator = create :project_validator
+    @project = create :project, owner: @project_validator, id: 456, value: {frozen: false, hidden: false, updatedAt: DateTime.now}.to_json
     # @featured_project has a project_id of 456
     @featured_project = create :featured_project
     @teacher = create :teacher
@@ -24,7 +25,6 @@ class FeaturedProjectsControllerTest < ActionController::TestCase
   end
 
   test 'project validators can unfeature projects' do
-    skip 'Investigate flaky test'
     sign_in @project_validator
     @controller.expects(:storage_decrypt_channel_id).with("789").returns([123, 456])
     put :unfeature, params: {project_id: "789"}
@@ -84,7 +84,6 @@ class FeaturedProjectsControllerTest < ActionController::TestCase
   end
 
   test 'unfeaturing a featured project should unfeature the project' do
-    skip 'Investigate flaky test'
     sign_in @project_validator
     @controller.expects(:storage_decrypt_channel_id).with("789").returns([123, 456])
     @featured_project.update! featured_at: DateTime.now
