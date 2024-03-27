540cb5913291dc86ecca6a54caf2853e5e7260e6
Bethany Connor <bethany@code.org>
2024-03-27 12:39:35 -0400

    Update contants for staging and to be from Hadi

--- lib/cdo/shared_constants/mailjet_constants.rb
+++ lib/cdo/shared_constants/mailjet_constants.rb
@@ -3,11 +3,11 @@ module MailJetConstants
     welcome: {
       template_id: {
         production: 5_421_128,
-        staging: 5_421_128,
+        staging: 5_826_411,
         development: 5_808_078
       },
-      from_address: 'bethany@code.org',
-      from_name: 'Bethany',
+      from_address: 'hadi_partovi@code.org',
+      from_name: 'Hadi Partovi',
     }
   }.freeze
 end
