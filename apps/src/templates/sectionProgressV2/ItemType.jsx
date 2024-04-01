4388495e0ee7c0675dfef9c03e1752c1e99cc316
kobryan0619 <kaitie.obryan@gmail.com>
2024-04-01 14:05:29 -0500

    changed color

--- apps/src/templates/sectionProgressV2/ItemType.jsx
+++ apps/src/templates/sectionProgressV2/ItemType.jsx
@@ -6,7 +6,7 @@ export const ITEM_TYPE = Object.freeze({
   VIEWED: 1,
   NEEDS_FEEDBACK: 2,
   FEEDBACK_GIVEN: 3,
-  ASSESSMENT_LEVEL: ['star', color.neutral_dark60],
+  ASSESSMENT_LEVEL: ['star', color.neutral_dark],
   CHOICE_LEVEL: ['split', color.neutral_dark60],
   KEEP_WORKING: ['rotate-left', color.neutral_dark],
   NO_ONLINE_WORK: ['dash', color.neutral_dark],
