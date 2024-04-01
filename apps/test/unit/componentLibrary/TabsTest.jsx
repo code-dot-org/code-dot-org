0426445486abc9bdf6c70328730760967e932cb6
denyslevada <levada.denys@gmail.com>
2024-04-01 18:50:35 +0300

    add tests

--- apps/test/unit/componentLibrary/TabsTest.jsx
+++ apps/test/unit/componentLibrary/TabsTest.jsx
@@ -1,247 +1,143 @@
-// TODO: IMPLEMENT ONCE LOGIC IS READY
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import sinon from 'sinon';
 
-// import React from 'react';
-// import {render, screen} from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import sinon from 'sinon';
-//
-// import {expect} from '../../util/reconfiguredChai';
-//
-// import Checkbox from '@cdo/apps/componentLibrary/checkbox';
-// describe('Design System - Checkbox', () => {
-//   it('Checkbox - renders with correct label', () => {
-//     render(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//       />
-//     );
-//
-//     const checkbox = screen.getByDisplayValue('test-checkbox');
-//     expect(checkbox).to.exist;
-//     expect(screen.getByText('Checkbox label')).to.exist;
-//   });
-//
-//   it('Checkbox - changes checked state on click', async () => {
-//     const user = userEvent.setup();
-//     const spyOnChange = sinon.spy();
-//
-//     let checked = false;
-//     const onChange = () => {
-//       checked = !checked;
-//       spyOnChange(checked);
-//     };
-//
-//     // Initial render
-//     const {rerender} = render(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//       />
-//     );
-//
-//     let checkbox = screen.getByDisplayValue('test-checkbox');
-//     expect(checkbox).to.exist;
-//
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's first click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.have.been.calledOnce;
-//     expect(spyOnChange).to.have.been.calledWith(true);
-//     expect(checkbox.checked).to.be.true;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's second click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.have.been.calledTwice;
-//     expect(spyOnChange).to.have.been.calledWith(false);
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//   });
-//
-//   it('Checkbox - renders indeterminate checkbox, changes on click', async () => {
-//     const user = userEvent.setup();
-//     const spyOnChange = sinon.spy();
-//
-//     let checked = false;
-//     let indeterminate = true;
-//     const onChange = () => {
-//       if (indeterminate) {
-//         // Default browser behavior for clicking an indeterminate checkbox.
-//         indeterminate = false;
-//         checked = true;
-//       } else {
-//         checked = !checked;
-//       }
-//       spyOnChange(checked);
-//     };
-//
-//     // Initial render
-//     const {rerender} = render(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         indeterminate={indeterminate}
-//       />
-//     );
-//
-//     let checkbox = screen.getByDisplayValue('test-checkbox');
-//     expect(checkbox).to.exist;
-//
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.true;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's first click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         indeterminate={indeterminate}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.have.been.calledOnce;
-//     expect(spyOnChange).to.have.been.calledWith(true);
-//     expect(checkbox.checked).to.be.true;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's second click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         indeterminate={indeterminate}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.have.been.calledTwice;
-//     expect(spyOnChange).to.have.been.calledWith(false);
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.false;
-//     expect(checkbox.indeterminate).to.be.false;
-//   });
-//
-//   it("Checkbox - renders disabled checkbox, doesn't change on click", async () => {
-//     const user = userEvent.setup();
-//     const spyOnChange = sinon.spy();
-//
-//     let checked = false;
-//     const onChange = () => {
-//       checked = !checked;
-//       spyOnChange(checked);
-//     };
-//
-//     // Initial render
-//     const {rerender} = render(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         disabled={true}
-//       />
-//     );
-//
-//     let checkbox = screen.getByDisplayValue('test-checkbox');
-//     expect(checkbox).to.exist;
-//
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.true;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's first click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         disabled={true}
-//       />
-//     );
-//
-//     checkbox = screen.getByDisplayValue('test-checkbox');
-//
-//     expect(spyOnChange).to.not.have.been.called;
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.true;
-//     expect(checkbox.indeterminate).to.be.false;
-//
-//     await user.click(checkbox);
-//
-//     // Re-render after user's second click
-//     rerender(
-//       <Checkbox
-//         name="test-checkbox"
-//         value="test-checkbox"
-//         label="Checkbox label"
-//         checked={checked}
-//         onChange={onChange}
-//         disabled={true}
-//       />
-//     );
-//
-//     expect(spyOnChange).to.not.have.been.called;
-//     expect(checkbox.checked).to.be.false;
-//     expect(checkbox.disabled).to.be.true;
-//     expect(checkbox.indeterminate).to.be.false;
-//   });
-// });
+import {expect} from '../../util/reconfiguredChai';
+
+import Tabs from '@cdo/apps/componentLibrary/tabs';
+
+const valuesMap = {};
+const onSelectedTabChange = (name, value) => (valuesMap[name] = value);
+
+describe('Design System - Tabs', () => {
+  it('Tabs - renders with correct tabs labels', () => {
+    // set segmentedButton default value
+    onSelectedTabChange('test1', 'tab1');
+
+    render(
+      <Tabs
+        selectedTabValue={valuesMap.test1}
+        tabs={[
+          {text: 'tab1', value: 'tab1', tabContent: <div>tab1 content</div>},
+          {text: 'tab2', value: 'tab2', tabContent: <div>tab2 content</div>},
+        ]}
+        onChange={value => onSelectedTabChange('test1', value)}
+      />
+    );
+
+    const tab1 = screen.getByText('tab1');
+    const tab2 = screen.getByText('tab2');
+
+    expect(tab1).to.exist;
+    expect(tab2).to.exist;
+    expect(valuesMap.test1).to.equal('tab1');
+    expect(screen.getByText('tab1 content')).to.exist;
+  });
+
+  it('Tabs - changes selected tab on click', async () => {
+    const user = userEvent.setup();
+    const spyOnChange = sinon.spy();
+    // set tab default value
+    onSelectedTabChange('test2', 'tab1');
+    const onChange = value => {
+      onSelectedTabChange('test2', value);
+      spyOnChange(value);
+    };
+
+    const TabsToRender = () => (
+      <Tabs
+        selectedTabValue={valuesMap.test1}
+        tabs={[
+          {text: 'tab1', value: 'tab1', tabContent: <div>tab1 content</div>},
+          {text: 'tab2', value: 'tab2', tabContent: <div>tab2 content</div>},
+        ]}
+        onChange={onChange}
+      />
+    );
+
+    const {rerender} = render(<TabsToRender />);
+
+    let tab1 = screen.getByText('tab1');
+    const tab2 = screen.getByText('tab2');
+
+    expect(tab1).to.exist;
+    expect(tab2).to.exist;
+    expect(valuesMap.test2).to.equal('tab1');
+
+    await user.click(tab2);
+
+    // Re-render after user's first click
+    rerender(<TabsToRender />);
+
+    tab1 = screen.getByText('tab1');
+
+    expect(spyOnChange).to.have.been.calledOnce;
+    expect(spyOnChange).to.have.been.calledWith('tab2');
+    expect(valuesMap.test2).to.equal('tab2');
+
+    await user.click(tab1);
+
+    // Re-render after user's second click
+    rerender(<TabsToRender />);
+
+    expect(spyOnChange).to.have.been.calledTwice;
+    expect(spyOnChange).to.have.been.calledWith('tab1');
+    expect(valuesMap.test2).to.equal('tab1');
+  });
+
+  it("Tabs - renders disabled tab, doesn't change on click", async () => {
+    const user = userEvent.setup();
+    const spyOnChange = sinon.spy();
+    // set segmentedButton default value
+    onSelectedTabChange('test3', 'tab1');
+    const onChange = value => {
+      onSelectedTabChange('test3', value);
+      spyOnChange(value);
+    };
+
+    const TabsToRender = () => (
+      <Tabs
+        selectedTabValue={valuesMap.test1}
+        tabs={[
+          {text: 'tab1', value: 'tab1', tabContent: <div>tab1 content</div>},
+          {
+            text: 'tab2',
+            value: 'tab2',
+            tabContent: <div>tab2 content</div>,
+            disabled: true,
+          },
+        ]}
+        onChange={onChange}
+      />
+    );
+
+    // Initial render
+    const {rerender} = render(<TabsToRender />);
+
+    let tab1 = screen.getByText('tab1');
+    const tab2 = screen.getByText('tab2');
+
+    expect(tab1).to.exist;
+    expect(tab2).to.exist;
+    expect(valuesMap.test3).to.equal('tab1');
+
+    await user.click(tab2);
+
+    // Re-render after user's first click
+    rerender(<TabsToRender />);
+
+    tab1 = screen.getByText('tab1');
+
+    expect(spyOnChange).to.not.have.been.called;
+    expect(valuesMap.test3).to.equal('tab1');
+
+    await user.click(tab1);
+
+    // Re-render after user's second click
+    rerender(<TabsToRender />);
+
+    expect(spyOnChange).to.have.been.called.once;
+    expect(spyOnChange).to.have.been.calledWith('tab1');
+  });
+});
