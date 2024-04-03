7c18f5c2409429824a7bf0c6eabdbd2badd7bc9a
Ben Brooks <ben@code.org>
2024-04-03 12:01:15 -0700

    Only use one styling file

--- apps/src/aichat/views/modelCustomization/CompareModelsDialog.tsx
+++ apps/src/aichat/views/modelCustomization/CompareModelsDialog.tsx
@@ -6,56 +6,81 @@ import {
   StrongText,
   BodyThreeText,
 } from '@cdo/apps/componentLibrary/typography/TypographyElements';
-import moduleStyles from '@cdo/apps/aichat/views/chatWarningModal.module.scss';
-import styles2 from './compare-models-dialog.module.scss';
+import styles from './compare-models-dialog.module.scss';
 import SimpleDropdown from '@cdo/apps/componentLibrary/dropdown/simpleDropdown';
-import styles from '@cdo/apps/aichat/views/model-customization-workspace.module.scss';
 import Button from '@cdo/apps/componentLibrary/button/Button';
+
+type ModelDescription = {
+  id: string;
+  name: string;
+  overview: string;
+  trainingData: string;
+};
 
 const loremIpsum =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
+const models: ModelDescription[] = [
+  {
+    id: 'llama2',
+    name: 'LLama 2',
+    overview: `llama2 + ${loremIpsum}`,
+    trainingData: `llama2 + ${loremIpsum}`,
+  },
+  {
+    id: 'mistral',
+    name: 'Mistral',
+    overview: `mistral + ${loremIpsum}`,
+    trainingData: `mistral + ${loremIpsum}`,
+  },
+];
+
 const CompareModelsDialog: React.FunctionComponent<{onClose: () => void}> = ({
   onClose,
 }) => {
+  const [chosenModelLeft, setChosenModelLeft] = useState<string>('llama2');
+  const [chosenModelRight, setChosenModelRight] = useState<string>('mistral');
+
   return (
-    <AccessibleDialog
-      onClose={onClose}
-      modalId={styles2.modelComparisonContainer}
-    >
+    <AccessibleDialog onClose={onClose} modalId={styles.modelComparisonDialog}>
       <div>
         <div style={{display: 'flex', justifyContent: 'center'}}>
           <Heading3>Compare Models</Heading3>
         </div>
-        <button
-          type="button"
-          onClick={onClose}
-          className={moduleStyles.xCloseButton}
-        >
+        <button type="button" onClick={onClose} className={styles.xCloseButton}>
           <i id="x-close" className="fa-solid fa-xmark" />
         </button>
       </div>
       <hr />
-      <div style={{display: 'flex', position: 'relative', overflowY: 'hidden'}}>
-        <ModelDescriptionPanel />
-        <ModelDescriptionPanel />
+      <div className={styles.modelComparisonContainer}>
+        <ModelDescriptionPanel
+          onChange={setChosenModelLeft}
+          selectedModelName={chosenModelLeft}
+          dropdownName="choose-model-1"
+        />
+        <ModelDescriptionPanel
+          onChange={setChosenModelRight}
+          selectedModelName={chosenModelRight}
+          dropdownName="choose-model-2"
+        />
       </div>
       <hr />
-      <div className={moduleStyles.bottomSection}>
+      <div className={styles.rightAlign}>
         <Button onClick={onClose} text="Finish" />
       </div>
     </AccessibleDialog>
   );
 };
 
-const ModelDescriptionPanel: React.FunctionComponent = () => {
+const ModelDescriptionPanel: React.FunctionComponent<{
+  onChange: (modelId: string) => void;
+  selectedModelName: string;
+  dropdownName: string;
+}> = ({onChange, selectedModelName, dropdownName}) => {
   const [userWantsScroll, setUserWantsScroll] = useState<boolean>(false);
   const [contentNeedsScroll, setContentNeedsScroll] = useState<boolean>(false);
 
   const descriptionsContainerRef = useRef<HTMLDivElement>(null);
-
-  const showViewMoreButton = !userWantsScroll && contentNeedsScroll;
-  const shouldScroll = userWantsScroll && contentNeedsScroll;
 
   useEffect(() => {
     if (
@@ -67,45 +92,51 @@ const ModelDescriptionPanel: React.FunctionComponent = () => {
     }
   }, [setContentNeedsScroll, descriptionsContainerRef]);
 
-  // update labeltext and name
+  const showViewMoreButton = !userWantsScroll && contentNeedsScroll;
+  const shouldScroll = userWantsScroll && contentNeedsScroll;
+
+  const selectedModel =
+    models.find(model => model.id === selectedModelName) || models[0];
+
   return (
-    <div className={styles2.modelDescriptionContainer}>
+    <div className={styles.modelDescriptionContainer}>
       <SimpleDropdown
-        labelText="Pick a model:"
+        labelText="Choose a model"
         isLabelVisible={false}
-        onChange={() => {}}
-        items={[
-          {value: 'llama2', text: 'LLama 2'},
-          {value: 'gpt', text: 'ChatGPT'},
-        ]}
-        selectedValue={'llama2'}
-        name="choose-model-1"
+        onChange={event => onChange(event.target.value)}
+        items={models.map(model => {
+          return {value: model.id, text: model.name};
+        })}
+        selectedValue={selectedModelName}
+        name={dropdownName}
         size="s"
-        className={styles.updateButton}
+        className={styles.fullWidth}
       />
       <div
         ref={descriptionsContainerRef}
-        style={{overflowY: shouldScroll ? 'scroll' : 'hidden'}}
+        className={
+          shouldScroll ? styles.overflowYScroll : styles.overflowYHidden
+        }
       >
         <StrongText>Overview</StrongText>
-        <div className={styles2.descriptionContainer}>
-          <BodyThreeText>{loremIpsum}</BodyThreeText>
+        <div className={styles.textContainer}>
+          <BodyThreeText>{selectedModel.overview}</BodyThreeText>
         </div>
         <br />
         <StrongText>Training Data</StrongText>
-        <div className={styles2.descriptionContainer}>
-          <BodyThreeText>{loremIpsum}</BodyThreeText>
+        <div className={styles.textContainer}>
+          <BodyThreeText>{selectedModel.trainingData}</BodyThreeText>
         </div>
       </div>
       {showViewMoreButton && (
-        <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
+        <div className={styles.rightAlign}>
           <Button
             size="xs"
             type="secondary"
             onClick={() => setUserWantsScroll(true)}
             text="view more"
             iconRight={{iconName: 'chevron-down'}}
-            className={styles2.viewMoreButton}
+            className={styles.viewMoreButton}
           />
         </div>
       )}
