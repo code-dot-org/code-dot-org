56f464e9ae09bde489f14176d5e73025ab2ec122
Ceara Chewning <ceara.chewning@code.org>
2024-04-03 18:23:53 -0400

    Add a statsig event when a student submits a project that is evaluated by ai

--- apps/src/lib/util/AnalyticsConstants.js
+++ apps/src/lib/util/AnalyticsConstants.js
@@ -140,7 +140,8 @@ const EVENTS = {
   TA_RUBRIC_CSV_DOWNLOADED: 'TA Rubric CSV Downloaded',
   TA_RUBRIC_INDIVIDUAL_AI_EVAL: 'TA Rubric Individual AI Eval Requested',
   TA_RUBRIC_SECTION_AI_EVAL: 'TA Rubric Section AI Eval Requested',
-  TA_RUBRIC_AI_PAGE_VISITED: 'TA Rubrics AI Level Page Visited',
+  TA_RUBRIC_AI_PAGE_VISITED: 'TA Rubric AI Level Page Visited',
+  TA_RUBRIC_STUDENT_AI_SUBMITTED: 'TA Rubric Student AI Level Submitted',
 
   // AI Tutor
   AI_TUTOR_PANEL_OPENED: 'AI Tutor Panel Opened',
