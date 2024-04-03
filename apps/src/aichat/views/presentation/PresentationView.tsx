90ab0008f2b29e4f482bcf057771b2b66cb0968b
Alice Fisher <alice.fisher@code.org>
2024-04-03 18:22:56 +0000

    review feedback

--- apps/src/aichat/views/presentation/PresentationView.tsx
+++ apps/src/aichat/views/presentation/PresentationView.tsx
@@ -6,31 +6,24 @@ import {Heading4} from '@cdo/apps/componentLibrary/typography';
 import moduleStyles from './presentation-view.module.scss';
 
 const PresentationView: React.FunctionComponent = () => {
-  const modelCardIcons = [
-    'memo',
-    'bullseye-pointer',
-    'diamond-exclamation',
-    'vial-circle-check',
-  ];
-
   return (
     <div className={styles.verticalFlexContainer}>
       <div>
         <Heading4 className={moduleStyles.modelCardTitle}>
           Title of Model Card
         </Heading4>
-        {MODEL_CARD_FIELDS_AND_LABELS.map(([property, label], index) => {
-          return (
-            <>
+        {MODEL_CARD_FIELDS_AND_LABELS.map(
+          ([property, label, iconName], index) => {
+            return (
               <ModelCardRow
                 keyName={property}
                 title={label}
-                titleIcon={modelCardIcons[index]}
+                titleIcon={iconName}
                 expandedContent={`Example TEXT ${label}`}
               />
-            </>
-          );
-        })}
+            );
+          }
+        )}
         <ModelCardRow
           keyName="examplePrompts"
           title="Example Prompts and Topics"
