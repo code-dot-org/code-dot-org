2a3d1cc46586b6360bd0898eaa967b018d666e77
Mike Harvey <mike.harvey@code.org>
2024-04-05 08:53:06 -0400

    check point: plugin class extended

---
+++ apps/src/blockly/addons/cdoFieldAngleTextInput.ts
@@ -0,0 +1,14 @@
+import {FieldAngle} from '@blockly/field-angle';
+
+// The second parameter of CDO Blockly implementation of this class is
+// a config object, which we are not currently using. In Google Blockly,
+// the second parameter is an optional string for a class name. If the
+// config option was used, errors would occur.
+// The config option is specified for certain blocks in Jigsaw, Maze,
+// Artist, and Play Lab. We can potentially add additional handling of
+// this argument to this class in the future should we need it.
+export default class CdoFieldAngleTextInput extends FieldAngle {
+  constructor(value?: undefined, options?: undefined, config?: undefined) {
+    super(value, options, config);
+  }
+}
