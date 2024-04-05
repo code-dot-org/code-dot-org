52816d10ad98c4818ba499c8d29ac30901fd452f
David Bailey <davidsbailey@users.noreply.github.com>
2024-04-05 11:29:50 -0700

    allow teacher to rerun ai eval when previous attempt resulted in error

--- dashboard/app/controllers/test_ai_proxy_controller.rb
+++ dashboard/app/controllers/test_ai_proxy_controller.rb
@@ -21,5 +21,6 @@ class TestAiProxyController < ApplicationController
       }
     end
     render json: {data: response_data}
+    # render json: {data: {foo: 'bar'}}, status: :bad_request
   end
 end
