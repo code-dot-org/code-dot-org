769c412212126123ebaa152ceaa5050bc06c6dbf
Juan Manzo <juan.manzo@code.org>
2024-04-08 13:23:52 -0700

    Add amplitude tracking for CPA state modal

---
+++ apps/src/sites/studio/pages/layouts/_student_information_interstitial.js
@@ -0,0 +1,65 @@
+import $ from 'jquery';
+import getScriptData from '@cdo/apps/util/getScriptData';
+import analyticsReporter from '@cdo/apps/lib/util/AnalyticsReporter';
+import {EVENTS} from '@cdo/apps/lib/util/AnalyticsConstants';
+
+const retrieveInfoForCap = getScriptData('retrieveInfoForCap');
+
+$(document).ready(function () {
+  const pathName = window.location.pathname;
+
+  if (pathName !== '/lti/v1/authenticate') {
+    $('#student-information-modal').modal('show');
+    retrieveInfoForCap &&
+      analyticsReporter.sendEvent(EVENTS.STUDENT_INFORMATION_INTERSTITIAL_SHOW);
+  }
+
+  function checkInputs() {
+    const ageValue = $('#user_age').val();
+    const stateValue = $('#user_us_state').val();
+    if (ageValue !== '') {
+      $('.age-required').hide();
+    } else {
+      $('.age-required').show();
+    }
+    if (stateValue !== '') {
+      $('.state-required').hide();
+    } else {
+      $('.state-required').show();
+    }
+    $('#edit_user #submit-btn').prop(
+      'disabled',
+      ageValue === '' || stateValue === ''
+    );
+  }
+
+  $('#edit_user select').on('change', function (event) {
+    checkInputs();
+  });
+
+  $('#edit_user').submit(function (event) {
+    event.preventDefault($(this).serialize());
+    const stateValue = $('#user_us_state').val();
+    if (stateValue !== '')
+      analyticsReporter.sendEvent(
+        EVENTS.STUDENT_INFORMATION_INTERSTITIAL_STATE_PROVIDED
+      );
+    $.ajax({
+      type: 'POST',
+      url: $(this).attr('action') + '/set_student_information',
+      data: $(this).serialize(),
+      dataType: 'json',
+      success: function (data) {
+        retrieveInfoForCap
+          ? location.reload()
+          : $('#student-information-modal').modal('hide');
+      },
+    });
+  });
+
+  $('#sign-out-btn').click(function (event) {
+    window.location = '#{destroy_user_session_url}';
+  });
+
+  checkInputs();
+});
