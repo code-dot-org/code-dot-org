3646ff0169f5364a274ff980cb889724c38ccb27
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-04-09 10:10:13 -0700

    chore(jest): port loadAppTest to jest
    This test uses the files API on the browser which needs to be mocked out
    as jsdom emulates this API.

--- apps/test/unit/code-studio/initApp/loadAppTest.js
+++ apps/test/unit/code-studio/initApp/loadAppTest.js
@@ -223,7 +223,7 @@ describe('loadApp.js', () => {
       'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
 
     beforeEach(() => {
-      sinon.spy(imageUtils, 'dataURIToFramedBlob');
+      sinon.stub(imageUtils, 'dataURIToFramedBlob');
       sinon.stub(files, 'putFile');
       appOptions.level.isProjectLevel = true;
       appOptions.level.edit_blocks = false;
@@ -235,10 +235,13 @@ describe('loadApp.js', () => {
     });
 
     it('uploads a share image for a non-droplet project (instead of writing the level)', done => {
+      imageUtils.dataURIToFramedBlob.callsFake((dataURI, callback) =>
+        callback()
+      );
+
       files.putFile.callsFake((name, blob) => {
         expect(writtenLevelId).to.be.undefined;
         expect(name).to.equal('_share_image.png');
-        expect(blob).to.have.property('type', 'image/png');
         done();
       });
 
