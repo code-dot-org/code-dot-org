a2f881f7223b958e0bd73697b1181b9109600ae6
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-04-09 13:41:04 -0700

    chore(jest): port MarkdownInstructionsTest to jest
    This test set global externals multiple times but this is unnecessary in
    jest as the DOM is already isolated to the test.

--- apps/test/unit/templates/instructions/MarkdownInstructionsTest.jsx
+++ apps/test/unit/templates/instructions/MarkdownInstructionsTest.jsx
@@ -7,7 +7,7 @@ import {expect} from '../../../util/reconfiguredChai';
 import {setExternalGlobals} from '../../../util/testUtils';
 
 describe('MarkdownInstructions', function () {
-  beforeAll(setExternalGlobals);
+  setExternalGlobals();
 
   it('standard case had top padding and no left margin', function () {
     const wrapper = shallow(
