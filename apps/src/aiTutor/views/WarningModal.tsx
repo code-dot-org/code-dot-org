7890387e74f1c5e6ffecd48e71b090e3ad174d1b
Emily Eastlake <emily.eastlake@code.org>
2024-04-10 11:49:39 -0700

    always return jsx element

--- apps/src/aiTutor/views/WarningModal.tsx
+++ apps/src/aiTutor/views/WarningModal.tsx
@@ -16,37 +16,43 @@ const WarningModal = () => {
   };
 
   return (
-    open && (
-      <AccessibleDialog onClose={onClose} className={style.chatWarningModal}>
-        <Heading2>Remember to chat responsibly!</Heading2>
+    <>
+      {open && (
+        <AccessibleDialog onClose={onClose} className={style.chatWarningModal}>
+          <Heading2>Remember to chat responsibly!</Heading2>
 
-        <button type="button" onClick={onClose} className={style.xCloseButton}>
-          <i id="x-close" className="fa-solid fa-xmark" />
-        </button>
-        <hr />
-        <BodyTwoText>
-          <StrongText>All messages sent to AI Tutor are recorded</StrongText>
-        </BodyTwoText>
-        <BodyTwoText>
-          Inappropriate messages will be flagged and your teacher will be
-          notified.
-        </BodyTwoText>
-        <br />
-        <BodyTwoText>
-          In order to protect your privacy, anything considered personal
-          information (name, phone number, address, etc.) should not be
-          submitted to AI Tutor.
-        </BodyTwoText>
-        <hr />
-        <div className={style.bottomSection}>
-          <Button
+          <button
+            type="button"
             onClick={onClose}
-            color={Button.ButtonColor.brandSecondaryDefault}
-            text="Ask AI Tutor"
-          />
-        </div>
-      </AccessibleDialog>
-    )
+            className={style.xCloseButton}
+          >
+            <i id="x-close" className="fa-solid fa-xmark" />
+          </button>
+          <hr />
+          <BodyTwoText>
+            <StrongText>All messages sent to AI Tutor are recorded</StrongText>
+          </BodyTwoText>
+          <BodyTwoText>
+            Inappropriate messages will be flagged and your teacher will be
+            notified.
+          </BodyTwoText>
+          <br />
+          <BodyTwoText>
+            In order to protect your privacy, anything considered personal
+            information (name, phone number, address, etc.) should not be
+            submitted to AI Tutor.
+          </BodyTwoText>
+          <hr />
+          <div className={style.bottomSection}>
+            <Button
+              onClick={onClose}
+              color={Button.ButtonColor.brandSecondaryDefault}
+              text="Ask AI Tutor"
+            />
+          </div>
+        </AccessibleDialog>
+      )}
+    </>
   );
 };
 
