9d4537677379919d43ad3a316d609e259a84c24a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-04-09 19:07:21 -0700

    simplify custom rubocop rule

--- tools/customLinters/rubocop_grouped_inline_private_methods.rb
+++ tools/customLinters/rubocop_grouped_inline_private_methods.rb
@@ -1,45 +1,23 @@
 module CustomCops
-  class InlinePlusGroupedAccessModifierDeclarations < RuboCop::Cop::Style::AccessModifierDeclarations
-    MESSAGE = '`%<access_modifier>s` methods should all be grouped together.'.freeze
+  # Based on https://github.com/rubocop/rubocop/blob/v1.52.1/lib/rubocop/cop/style/access_modifier_declarations.rb
+  class InlinePlusGroupedAccessModifierDeclarations < RuboCop::Cop::Base
+    RESTRICT_ON_SEND = %i[private].freeze
+    MESSAGE = 'inlined private methods should all be grouped together at the end of the file.'.freeze
 
-    def offense?(node)
-      inline = access_modifier_is_inlined?(node)
-      group = !right_sibling_same_inline_method?(node)
-      return inline && group
+    # @!method access_modifier_with_symbol?(node)
+    def_node_matcher :access_modifier_with_symbol?, <<~PATTERN
+      (send nil? {:private} (sym _))
+    PATTERN
+
+    def on_send(node)
+      return unless node.access_modifier?
+      add_offense(node.loc.selector, message: MESSAGE) unless right_sibling_same_inline_method?(node)
     end
 
-    def autocorrect(corrector, node)
-      def_node = find_corresponding_def_node(node)
-      return unless def_node
-
-      replace_def(corrector, node, def_node)
-    end
-
-    def opposite_style_detected
-      # disable styles
-    end
-
-    def correct_style_detected
-      # disable styles
-    end
-
-    def right_sibling_same_inline_method?(node)
+    private def right_sibling_same_inline_method?(node)
       return true if node.right_siblings.empty?
       sibling = node.right_siblings.first
       return sibling.send_type? && sibling.method?(node.method_name) && !sibling.arguments.empty?
-    end
-
-    def message(range)
-      access_modifier = range.source
-      format(MESSAGE, access_modifier: access_modifier)
-    end
-
-    def group_style?
-      true
-    end
-
-    def inline_style?
-      true
     end
   end
 end
