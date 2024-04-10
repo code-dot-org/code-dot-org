03525b05025582e2433adc52eec4721a7912e863
David Bailey <davidsbailey@users.noreply.github.com>
2024-04-10 14:08:23 -0700

    remove other top level tests, yes repro, plus VersionHistory failures

--- apps/test/unit/ejsTest.js
+++
@@ -1,8 +0,0 @@
-import {assert} from '../util/reconfiguredChai';
-// nonstandard EJS behavior we rely upon in our templates
-describe('ejs test', function () {
-  it('renders empty string on undefined object property access', function () {
-    var ejs = require('./empty-data.ejs')({data: {}});
-    assert.equal(ejs.trim(), '');
-  });
-});
