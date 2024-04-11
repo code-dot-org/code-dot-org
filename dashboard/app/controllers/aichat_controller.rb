54e372a83105865f0d5af9ab8f266a1821379b00
Alice Fisher <alice.fisher@code.org>
2024-04-11 18:05:41 +0000

    fix variable name

--- dashboard/app/controllers/aichat_controller.rb
+++ dashboard/app/controllers/aichat_controller.rb
@@ -10,7 +10,7 @@ class AichatController < ApplicationController
     # Check only the newest message from the user for inappropriate content.
     new_message_text = params[:chatContext]["userMessage"]
     puts "new_message_text = #{new_message_text}"
-    filter_result = ShareFiltering.find_failure(newMessageText, locale) if newMessageText
+    filter_result = ShareFiltering.find_failure(new_message_text, locale) if new_message_text
     # If the content is inappropriate, we skip sending to endpoint and instead hardcode a warning response on the front-end.
     return render(status: :ok, json: {status: filter_result.type, flagged_content: filter_result.content}) if filter_result
     # TODO: Format input to send to Sagemaker.
