5b179812712892dfc325622d0483af7a920b9189
Molly Moen <molly@code.org>
2024-04-12 09:01:16 -0700

    Basic integration of Python Lab with CDO IDE (#57935)
    * basic setup
    
    * work in progress
    
    * try to fix null issue
    
    * use extension as key
    
    * add console
    
    * clean up
    
    * style updates
    
    * clean up and update styling

--- apps/src/pythonlab/PythonConsole.tsx
+++ apps/src/pythonlab/PythonConsole.tsx
@@ -1,36 +1,22 @@
 import React from 'react';
-import {darkMode} from '@cdo/apps/lab2/views/components/editor/editorThemes';
-import {python} from '@codemirror/lang-python';
-import moduleStyles from './python-editor.module.scss';
+import moduleStyles from './python-console.module.scss';
 import {useDispatch} from 'react-redux';
-import {appendSystemMessage, resetOutput, setSource} from './pythonlabRedux';
+import {appendSystemMessage, resetOutput} from './pythonlabRedux';
 import Button from '@cdo/apps/templates/Button';
 import {runPythonCode} from './pyodideRunner';
 import {useFetch} from '@cdo/apps/util/useFetch';
-import CodeEditor from '@cdo/apps/lab2/views/components/editor/CodeEditor';
 import {useAppSelector} from '@cdo/apps/util/reduxHooks';
-import Lab2Registry from '@cdo/apps/lab2/Lab2Registry';
-import {MultiFileSource} from '@cdo/apps/lab2/types';
 import {getFileByName} from '@cdo/apps/lab2/projects/utils';
 
 interface PermissionResponse {
   permissions: string[];
 }
 
-const PythonEditor: React.FunctionComponent = () => {
+const PythonConsole: React.FunctionComponent = () => {
   const source = useAppSelector(state => state.pythonlab.source);
   const codeOutput = useAppSelector(state => state.pythonlab.output);
   const {loading, data} = useFetch('/api/v1/users/current/permissions');
   const dispatch = useDispatch();
-  const editorExtensions = [python(), darkMode];
-  const initialSources = useAppSelector(state => state.lab.initialSources);
-  let startCode = 'print("Hello world!")';
-
-  if (initialSources?.source && typeof initialSources.source !== 'string') {
-    startCode =
-      getFileByName(initialSources.source.files, 'main.py')?.contents ||
-      startCode;
-  }
 
   const handleRun = () => {
     const parsedData = data ? (data as PermissionResponse) : {permissions: []};
@@ -50,46 +36,12 @@ const PythonEditor: React.FunctionComponent = () => {
     }
   };
 
-  const onCodeUpdate = (updatedCode: string) => {
-    // TODO: handle multiple files. For now everything is "main.py".
-    const updatedSource: MultiFileSource = {
-      files: {
-        '0': {
-          id: '0',
-          name: 'main.py',
-          language: 'python',
-          contents: updatedCode,
-          folderId: '1',
-        },
-      },
-      folders: {
-        '1': {
-          id: '1',
-          name: 'src',
-          parentId: '0',
-        },
-      },
-    };
-    dispatch(setSource(updatedSource));
-    if (Lab2Registry.getInstance().getProjectManager()) {
-      const projectSources = {
-        source: updatedSource,
-      };
-      Lab2Registry.getInstance().getProjectManager()?.save(projectSources);
-    }
-  };
-
   const clearOutput = () => {
     dispatch(resetOutput());
   };
 
   return (
-    <div className={moduleStyles.editorContainer}>
-      <CodeEditor
-        onCodeChange={onCodeUpdate}
-        startCode={startCode}
-        editorConfigExtensions={editorExtensions}
-      />
+    <div className={moduleStyles.consoleContainer}>
       <div>
         <Button
           type={'button'}
@@ -124,4 +76,4 @@ const PythonEditor: React.FunctionComponent = () => {
   );
 };
 
-export default PythonEditor;
+export default PythonConsole;
