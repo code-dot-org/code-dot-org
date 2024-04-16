c3380933ead9404016adee345f249d0b3788e870
Alice Fisher <alice.fisher@code.org>
2024-04-16 16:32:43 +0000

    add pilot student test

--- dashboard/test/controllers/aichat_controller_test.rb
+++ dashboard/test/controllers/aichat_controller_test.rb
@@ -4,10 +4,11 @@ class AichatControllerTest < ActionController::TestCase
   GENAI_PILOT = "genai-pilot"
 
   setup_all do
-    @teacher = create :teacher
     @genai_pilot = create :pilot
     @genai_pilot.name = GENAI_PILOT
     @pilot_teacher = create :teacher, pilot_experiment: GENAI_PILOT
+    @section = create(:section, user: @pilot_teacher)
+    @pilot_student = create(:follower, section: @section).student_user
   end
 
   test_user_gets_response_for :chat_completion,
@@ -15,6 +16,7 @@ class AichatControllerTest < ActionController::TestCase
     user: :student,
     method: :post,
     response: :forbidden
+
   test_user_gets_response_for :chat_completion,
     name: "teacher_no_access_test",
     user: :teacher,
@@ -24,11 +26,22 @@ class AichatControllerTest < ActionController::TestCase
   test 'pilot teacher has access test' do
     sign_in(@pilot_teacher)
     post :chat_completion, params: {
-                newMessage: "hello",
-                storedMessages: [{role: "user", content: "this is a test!"}],
-                aichatParameters: {temperature: 0.5, retrievalContexts: ["test"], systemPrompt: "test"},
-                chatContext: {userId: 1, currentLevelId: "test", scriptId: 1, channelId: "test"}
-            }
+      newMessage: "hello",
+      storedMessages: [{role: "user", content: "this is a test!"}],
+      aichatParameters: {temperature: 0.5, retrievalContexts: ["test"], systemPrompt: "test"},
+      chatContext: {userId: 1, currentLevelId: "test", scriptId: 1, channelId: "test"}
+    }
+    assert_response :success
+  end
+
+  test 'pilot student has access test' do
+    sign_in(@pilot_student)
+    post :chat_completion, params: {
+      newMessage: "hello",
+      storedMessages: [{role: "user", content: "this is a test!"}],
+      aichatParameters: {temperature: 0.5, retrievalContexts: ["test"], systemPrompt: "test"},
+      chatContext: {userId: 1, currentLevelId: "test", scriptId: 1, channelId: "test"}
+    }
     assert_response :success
   end
 
