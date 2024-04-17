bbf0d9b2e0d3d4996ca676e17a70502f80b6373d
Alice Fisher <alice.fisher@code.org>
2024-04-17 01:44:55 +0000

    use more unique names

--- dashboard/test/controllers/aichat_controller_test.rb
+++ dashboard/test/controllers/aichat_controller_test.rb
@@ -4,12 +4,12 @@ class AichatControllerTest < ActionController::TestCase
   GENAI_PILOT = "gen-ai-customizing-llms"
 
   setup_all do
-    @genai_pilot = create :pilot
-    @genai_pilot.name = GENAI_PILOT
-    @pilot_teacher = create :teacher, pilot_experiment: GENAI_PILOT
-    @section = create(:section, user: @pilot_teacher)
-    @pilot_student = create(:follower, section: @section).student_user
-    @params = {
+    genai_pilot = create :pilot
+    genai_pilot.name = GENAI_PILOT
+    @genai_pilot_teacher = create :teacher, pilot_experiment: GENAI_PILOT
+    pilot_section = create(:section, user: @genai_pilot_teacher)
+    @genai_pilot_student = create(:follower, section: pilot_section).student_user
+    @common_params = {
       storedMessages: [{role: "user", content: "this is a test!"}],
       aichatParameters: {temperature: 0.5, retrievalContexts: ["test"], systemPrompt: "test"},
       chatContext: {userId: 1, currentLevelId: "test", scriptId: 1, channelId: "test"}
@@ -17,9 +17,9 @@ class AichatControllerTest < ActionController::TestCase
     valid_message = "hello"
     pii_violation_message = "my email is l.lovepadel@sports.edu"
     profanity_violation_message = "Damn you, robot"
-    @valid_params = @params.merge(newMessage: valid_message)
-    @pii_violation_params = @params.merge(newMessage: pii_violation_message)
-    @profanity_violation_params = @params.merge(newMessage: profanity_violation_message)
+    @valid_params = @common_params.merge(newMessage: valid_message)
+    @pii_violation_params = @common_params.merge(newMessage: pii_violation_message)
+    @profanity_violation_params = @common_params.merge(newMessage: profanity_violation_message)
   end
 
   test_user_gets_response_for :chat_completion,
@@ -35,26 +35,26 @@ class AichatControllerTest < ActionController::TestCase
     response: :forbidden
 
   test 'pilot teacher has access test' do
-    sign_in(@pilot_teacher)
+    sign_in(@genai_pilot_teacher)
     post :chat_completion, params: @valid_params
     assert_response :success
   end
 
   test 'pilot student has access test' do
-    sign_in(@pilot_student)
+    sign_in(@genai_pilot_student)
     post :chat_completion, params: @valid_params
     assert_response :success
   end
 
   test 'Bad request if required params are not included' do
-    sign_in(@pilot_teacher)
+    sign_in(@genai_pilot_teacher)
     post :chat_completion, params: {newMessage: "hello"}
     assert_response :bad_request
   end
 
   # Post request with a profane messages param returns a failure
   test 'returns failure when chat message contains profanity' do
-    sign_in(@pilot_student)
+    sign_in(@genai_pilot_student)
     ShareFiltering.stubs(:find_failure).returns(ShareFailure.new(ShareFiltering::FailureType::PROFANITY, 'damn'))
     post :chat_completion, params: @profanity_violation_params
     assert_equal json_response["status"], ShareFiltering::FailureType::PROFANITY
@@ -63,7 +63,7 @@ class AichatControllerTest < ActionController::TestCase
 
   # Post request with a messages param containing PII returns a failure
   test 'returns failure when chat message contains PII' do
-    sign_in(@pilot_student)
+    sign_in(@genai_pilot_student)
     ShareFiltering.stubs(:find_failure).returns(ShareFailure.new(ShareFiltering::FailureType::EMAIL, 'l.lovepadel@sports.edu'))
     post :chat_completion, params: @pii_violation_params
     assert_equal json_response["status"], ShareFiltering::FailureType::EMAIL
