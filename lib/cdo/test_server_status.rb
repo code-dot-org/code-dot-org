77368e577675cb8f265b6c645a7e6b8c7598c6a7
suresh <suresh@code.org>
2024-04-18 14:16:10 -0700

    Remove references to the module that logs various build metrics to a dedicated MySQL database.

--- lib/cdo/test_server_status.rb
+++ lib/cdo/test_server_status.rb
@@ -1,13 +1,11 @@
 require_relative './developers_topic'
 require_relative './infra_test_topic'
-require_relative './metrics_helper'
 
 module TestServerStatus
   # Notes that a new DTT has started, updating Slack topics to set "DTT: no"
   # and recording relevant metrics.
   def self.mark_started(sha)
     DevelopersTopic.set_dtt 'no (manual DTT in progress)'
-    Metrics.write_metric('dtt_start', sha, Metrics::MANUAL)
   end
 
   # Marks the most recent DTT green, updating Slack #infra-test and
@@ -15,7 +13,6 @@ module TestServerStatus
   def self.mark_green(sha, source = Metrics::MANUAL)
     DevelopersTopic.set_dtt 'yes'
     InfraTestTopic.set_green_commit sha
-    Metrics.write_metric('dtt_green', sha, source)
   end
 
   # Marks the most recent DTT red, updating the Slack #deploy-status topic to
@@ -25,6 +22,5 @@ module TestServerStatus
   def self.mark_red(sha, source = Metrics::MANUAL)
     DevelopersTopic.set_dtt 'yes'
     InfraTestTopic.set_red_commit sha
-    Metrics.write_metric('dtt_red', sha, source)
   end
 end
