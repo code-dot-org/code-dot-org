13c691453a8dc5aea01c2c15239a940b3325a1f8
Alice Fisher <alice.fisher@code.org>
2024-04-18 04:22:45 +0000

    update aichat_helper

--- dashboard/app/helpers/aichat_helper.rb
+++ dashboard/app/helpers/aichat_helper.rb
@@ -1,29 +1,24 @@
 module AichatHelper
   SAGEMAKER_CLIENT = Aws::SageMakerRuntime::Client.new
+  SAGEMAKER_MODEL_ENDPOINT = "mistral-7b-inst-v01" # "BioMistral-7B"
   BASE_PROMPT = "You are a helpful chatbot for children. Limit your responses to a small paragraph."
 
   # The instruction-tuned version of Mistral accepts formatted instructions where conversation roles
   # must start with a user prompt and alternate between user and assistant.
   def self.format_input_for_sagemaker_request(aichat_params, stored_messages, new_message)
+    stored_messages.push({ role: 'user', content: new_message })
     inputs = BASE_PROMPT + aichat_params[:systemPrompt]
     inputs += aichat_params[:retrievalContexts].join(" ") if aichat_params[:retrievalContexts]
-
-    if stored_messages.empty?
-      inputs = "<s>[INST] #{inputs + " " + new_message} [/INST]"
-    else
-      inputs = "<s>[INST] #{inputs} [/INST]"
-      # Filter only messages from user or assistant - ignore model update messages.
-      stored_messages.each do |msg|
-        if msg[:role] == 'user'
-          inputs += "[INST] #{msg[:content]} [/INST] "
-        elsif msg[:role] == 'assistant'
-          inputs += msg[:content] + "</s>"
-        end
+    inputs = "<s>[INST] #{inputs} [/INST]"
+    # Filter only messages from user or assistant - ignore model update messages.
+    stored_messages.each do |msg|
+      if msg[:role] == 'user'
+        inputs += "[INST] #{msg[:content]} [/INST] "
+      elsif msg[:role] == 'assistant'
+        inputs += msg[:content] + "</s>"
       end
-      inputs += "[INST] #{new_message} [/INST] "
     end
-    puts "inputs: #{inputs}"
-    input_json = {
+    {
       inputs: inputs,
       parameters: {
         temperature: aichat_params[:temperature].to_f,
@@ -31,14 +26,11 @@ module AichatHelper
         top_p: 0.9
       }
     }
-    rescue => exception
-      p exception
   end
 
   def self.request_sagemaker_chat_completion(input_json)
-    response = SAGEMAKER_CLIENT.invoke_endpoint(
-      # endpoint_name: "BioMistral-7B",
-      endpoint_name: "mistral-7b-inst-v01", # required
+    SAGEMAKER_CLIENT.invoke_endpoint(
+      endpoint_name: SAGEMAKER_MODEL_ENDPOINT, # required
       body: input_json.to_json.to_s, # required
       # target_model: 'mistral-7b-inst.tar.gz',
       content_type: "application/json"
