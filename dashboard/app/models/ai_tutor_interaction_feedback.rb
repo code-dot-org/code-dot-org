6697483ab9d6973454f0d0a90979ccc97ad986ee
Emily Eastlake <emily.eastlake@code.org>
2024-04-19 14:00:43 -0700

    remove validation

--- dashboard/app/models/ai_tutor_interaction_feedback.rb
+++ dashboard/app/models/ai_tutor_interaction_feedback.rb
@@ -17,7 +17,4 @@
 class AiTutorInteractionFeedback < ApplicationRecord
   belongs_to :ai_tutor_interaction
   belongs_to :user
-
-  validates :thumbs_up, inclusion: {in: [true, false]}, allow_nil: true
-  validates :thumbs_down, inclusion: {in: [true, false]}, allow_nil: true
 end
