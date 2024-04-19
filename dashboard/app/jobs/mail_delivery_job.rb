15d9dd22f83e4bc0d9214b9d990b7472fc7d6a9d
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-04-19 12:01:37 +0300

    P20-299: Add doc for `MailDeliveryJob`

--- dashboard/app/jobs/mail_delivery_job.rb
+++ dashboard/app/jobs/mail_delivery_job.rb
@@ -1,3 +1,11 @@
+# This class is used to handle the delivery of enqueued emails through Active Job.
+#
+# To enqueue an email delivery, use the `deliver_later` method on any mailer object.
+# This method allows scheduling options such as setting a wait time before sending.
+#
+# @example Enqueue an email to be sent one hour later
+#   Notifier.welcome(User.first).deliver_later(wait: 1.hour)
+# @see https://apidock.com/rails/v6.1.7.7/ActionMailer/MessageDelivery/deliver_later
 class MailDeliveryJob < ActionMailer::MailDeliveryJob
   include ActiveJobMetrics
 end
