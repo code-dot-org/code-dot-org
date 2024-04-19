bc69f406c007988e625fa64944c5ee3f7b042be8
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-04-19 20:16:12 +0300

    P20-811: Update design of Permission Granted page

--- apps/src/templates/policy_compliance/ChildAccountConsent/index.jsx
+++ apps/src/templates/policy_compliance/ChildAccountConsent/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import i18n from '@cdo/locale';
-import * as color from '../../util/color';
 import cookies from 'js-cookie';
 import {
-  Heading1,
+  Heading4,
+  BodyThreeText,
   BodyTwoText,
   EmText,
   StrongText,
 } from '@cdo/apps/componentLibrary/typography';
+import Button from '@cdo/apps/templates/Button';
+
+import './index.scss';
+
+function returnToCdoButton() {
+  return (
+    <Button
+      className="return-to-cdo"
+      text={i18n.returnToCdo()}
+      onClick={() => location.replace('/')}
+    />
+  );
+}
 
 function permissionGrantedMessage(date) {
   // Get the current locale.
@@ -22,32 +35,32 @@ function permissionGrantedMessage(date) {
     date: date.toLocaleDateString(locale, {...dateOptions}),
   });
   return (
-    <div id="permission_granted_container" style={styles.container}>
-      <Heading1 visualAppearance="heading-lg">
-        {i18n.childAccountConsentValidHeader()}
-      </Heading1>
-      <BodyTwoText>
-        <StrongText>{i18n.childAccountConsentValidPermission()} </StrongText>
-        <span style={styles.grantDate}>{grantedDateString}</span>
+    <div id="permission_granted_container">
+      <Heading4>{i18n.childAccountConsentValidHeader()}</Heading4>
+      <BodyTwoText className="permission-granted-date">
+        <StrongText className="title">
+          {i18n.childAccountConsentValidPermission()}
+        </StrongText>{' '}
+        <StrongText className="date">{grantedDateString}</StrongText>
       </BodyTwoText>
-      <BodyTwoText>{i18n.childAccountConsentValidMessage()}</BodyTwoText>
-      <BodyTwoText>
+      <BodyThreeText>{i18n.childAccountConsentValidMessage()}</BodyThreeText>
+      <BodyThreeText>
         <EmText>{i18n.childAccountConsentEmailUnknown()}</EmText>
-      </BodyTwoText>
+      </BodyThreeText>
+      {returnToCdoButton()}
     </div>
   );
 }
 
 function expiredTokenMessage() {
   return (
-    <div id="expired_token_container" style={styles.container}>
-      <Heading1 visualAppearance="heading-lg">
-        {i18n.childAccountConsentExpiredHeader()}
-      </Heading1>
-      <BodyTwoText>{i18n.childAccountConsentExpiredMessage()}</BodyTwoText>
-      <BodyTwoText>
+    <div id="expired_token_container">
+      <Heading4>{i18n.childAccountConsentExpiredHeader()}</Heading4>
+      <BodyThreeText>{i18n.childAccountConsentExpiredMessage()}</BodyThreeText>
+      <BodyThreeText>
         <EmText>{i18n.childAccountConsentEmailUnknown()}</EmText>
-      </BodyTwoText>
+      </BodyThreeText>
+      {returnToCdoButton()}
     </div>
   );
 }
@@ -62,20 +75,4 @@ export default function ChildAccountConsent(props) {
 ChildAccountConsent.propTypes = {
   permissionGranted: PropTypes.bool,
   permissionGrantedDate: PropTypes.instanceOf(Date),
-};
-
-const styles = {
-  container: {
-    border: '1px solid rgb(233 233 233)',
-    boxShadow: '5px 5px 3px 0px #ccc',
-    boxSizing: 'border-box',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: 10,
-    maxWidth: 700,
-    padding: 20,
-  },
-  grantDate: {
-    color: color.realgreen,
-  },
 };
