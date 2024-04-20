74b0ae8926d02f3cf214b9d937a6ed6ce2f3c640
Alice Fisher <alice.fisher@code.org>
2024-04-20 22:56:01 +0000

    use constants, clean up aichat_helper

--- dashboard/app/helpers/aichat_helper.rb
+++ dashboard/app/helpers/aichat_helper.rb
@@ -1,31 +1,37 @@
 module AichatHelper
+  ASSISTANT = "assistant"
+  USER = "user"
+  BASE_PROMPT = "You are a helpful chatbot for children. Limit your responses to a small paragraph."
+  INSTRUCTIONS_START_TAG = "[INST]"
+  INSTRUCTIONS_END_TAG = "[/INST]"
+  SENTENCE_START_TAG = "<s>"
+  SENTENCE_END_TAG = "</s>"
+  MAX_NEW_TOKENS = 300
   SAGEMAKER_CLIENT = Aws::SageMakerRuntime::Client.new
   SAGEMAKER_MODEL_ENDPOINT = "mistral-7b-inst-v01" # "BioMistral-7B"
-  BASE_PROMPT = "You are a helpful chatbot for children. Limit your responses to a small paragraph."
-  ASSISTANT = 'assistant'
-  USER = 'user'
+  TOP_P = 0.9
 
   # The instruction-tuned version of Mistral accepts formatted instructions where conversation roles
   # must start with a user prompt and alternate between user and assistant.
   def self.format_inputs_for_sagemaker_request(aichat_params, stored_messages, new_message)
-    stored_messages.push({role: USER, content: new_message})
+    all_messages = [*stored_messages, {role: USER, content: new_message}]
     inputs = BASE_PROMPT + aichat_params[:systemPrompt]
     inputs += aichat_params[:retrievalContexts].join(" ") if aichat_params[:retrievalContexts]
-    inputs = "<s> " + wrap_as_instructions(inputs)
+    inputs = SENTENCE_START_TAG + wrap_as_instructions(inputs)
     # Filter only messages from user or assistant - ignore model update messages.
-    stored_messages.each do |msg|
+    all_messages.each do |msg|
       if msg[:role] == USER
         inputs += wrap_as_instructions(msg[:content])
       elsif msg[:role] == ASSISTANT
-        inputs += msg[:content] + "</s>"
+        inputs += msg[:content] + SENTENCE_END_TAG
       end
     end
     {
       inputs: inputs,
       parameters: {
         temperature: aichat_params[:temperature].to_f,
-        max_new_tokens: 300,
-        top_p: 0.9
+        max_new_tokens: MAX_NEW_TOKENS,
+        top_p: TOP_P
       }
     }
   end
@@ -33,8 +39,7 @@ module AichatHelper
   def self.request_sagemaker_chat_completion(input_json)
     SAGEMAKER_CLIENT.invoke_endpoint(
       endpoint_name: SAGEMAKER_MODEL_ENDPOINT, # required
-      body: input_json.to_json.to_s, # required
-      # target_model: 'mistral-7b-inst.tar.gz',
+      body: input_json.to_json, # required
       content_type: "application/json"
     )
   end
@@ -42,15 +47,15 @@ module AichatHelper
   def self.get_sagemaker_assistant_response(sagemaker_response)
     parsed_response = JSON.parse(sagemaker_response.body.string)
     generated_text = parsed_response[0]["generated_text"]
-    parts = generated_text.split("[/INST]")
+    parts = generated_text.split(INSTRUCTIONS_END_TAG)
     parts.last
   end
 
   def self.wrap_as_instructions(message)
-    "[INST] #{message} [/INST]"
+    INSTRUCTIONS_START_TAG + "#{message}" + INSTRUCTIONS_END_TAG
   end
 
   def self.can_request_aichat_chat_completion?
-    DCDO.get('aichat_chat_completion', true)
+    DCDO.get("aichat_chat_completion", true)
   end
 end
