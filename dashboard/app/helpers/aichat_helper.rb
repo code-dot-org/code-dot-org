d0edc7f32ab1c3308b2cc55cfced362f1e39cecf
Alice Fisher <alice.fisher@code.org>
2024-04-22 14:00:18 +0000

    use token instead of tag, add comment

--- dashboard/app/helpers/aichat_helper.rb
+++ dashboard/app/helpers/aichat_helper.rb
@@ -2,10 +2,10 @@ module AichatHelper
   ASSISTANT = "assistant"
   USER = "user"
   BASE_PROMPT = "You are a helpful chatbot for children. Limit your responses to a small paragraph. "
-  INSTRUCTIONS_START_TAG = "[INST]"
-  INSTRUCTIONS_END_TAG = "[/INST]"
-  SENTENCE_START_TAG = "<s>"
-  SENTENCE_END_TAG = "</s>"
+  INSTRUCTIONS_BEGIN_TOKEN = "[INST]"
+  INSTRUCTIONS_END_TOKEN = "[/INST]"
+  SENTENCE_BEGIN_TOKEN = "<s>"
+  SENTENCE_END_TOKEN = "</s>"
   MAX_NEW_TOKENS = 300
   SAGEMAKER_CLIENT = Aws::SageMakerRuntime::Client.new
   SAGEMAKER_MODEL_ENDPOINT = "mistral-7b-inst-v01" # "BioMistral-7B"
@@ -18,13 +18,15 @@ module AichatHelper
     all_messages = [*stored_messages, {role: USER, content: new_message}]
     inputs = BASE_PROMPT + aichat_params[:systemPrompt]
     inputs += aichat_params[:retrievalContexts].join(" ") if aichat_params[:retrievalContexts]
-    inputs = SENTENCE_START_TAG + wrap_as_instructions(inputs)
+    inputs = SENTENCE_BEGIN_TOKEN + wrap_as_instructions(inputs)
     # Filter only messages from user or assistant - ignore model update messages.
     all_messages.each do |msg|
       if msg[:role] == USER
         inputs += wrap_as_instructions(msg[:content])
       elsif msg[:role] == ASSISTANT
-        inputs += msg[:content] + SENTENCE_END_TAG
+        # Note that each assistant message in the conversation history is followed by
+        # the end-of-sentence token but a begin-of-sentence token is not required.
+        inputs += msg[:content] + SENTENCE_END_TOKEN
       end
     end
     {
@@ -48,12 +50,12 @@ module AichatHelper
   def self.get_sagemaker_assistant_response(sagemaker_response)
     parsed_response = JSON.parse(sagemaker_response.body.string)
     generated_text = parsed_response[0]["generated_text"]
-    parts = generated_text.split(INSTRUCTIONS_END_TAG)
+    parts = generated_text.split(INSTRUCTIONS_END_TOKEN)
     parts.last
   end
 
   def self.wrap_as_instructions(message)
-    INSTRUCTIONS_START_TAG + message + INSTRUCTIONS_END_TAG
+    INSTRUCTIONS_BEGIN_TOKEN + message + INSTRUCTIONS_END_TOKEN
   end
 
   def self.can_request_aichat_chat_completion?
