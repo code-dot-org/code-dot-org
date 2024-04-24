b3cd1b57c51e7d0856027476221817024fdb95b8
kobryan0619 <kaitie.obryan@gmail.com>
2024-04-24 17:07:02 -0500

    updated tests

--- apps/test/unit/templates/sectionProgressV2/ProgressFeedbackBannerTest.jsx
+++ apps/test/unit/templates/sectionProgressV2/ProgressFeedbackBannerTest.jsx
@@ -12,20 +12,47 @@ describe('UnconnectedProgressFeedbackBanner', () => {
   const fakeCreate = sinon.spy();
   const defaultProps = {
     currentUser: {isAdmin: false},
-    canShow: false,
+    canShow: true,
     isLoading: false,
     progressV2Feedback: {empty: true},
-    fetchProgressV2Feedback: fakeFetch,
-    createProgressV2Feedback: fakeCreate,
+    fetchProgressV2Feedback: () => {},
+    createProgressV2Feedback: () => {},
     errorWhenCreatingOrLoading: null,
   };
 
   afterEach(() => {
     fakeCreate.resetHistory();
+    fakeFetch.resetHistory();
   });
 
-  it('renders correctly with initial state', () => {
-    render(<UnconnectedProgressFeedbackBanner {...defaultProps} />);
+  it('renders empty if it can not show', () => {
+    render(
+      <UnconnectedProgressFeedbackBanner
+        {...defaultProps}
+        canShow={false}
+        fetchProgressV2Feedback={fakeFetch}
+      />
+    );
+    expect(fakeFetch).to.have.been.calledOnce;
+    const questionText = screen.queryByText(
+      i18n.progressV2_feedback_question()
+    );
+    const shareMoreText = screen.queryByText(
+      i18n.progressV2_feedback_shareMore()
+    );
+    expect(questionText).to.not.exist;
+    expect(shareMoreText).to.not.exist;
+  });
+
+  it('renders empty if user already answered the feedback question', () => {
+    render(
+      <UnconnectedProgressFeedbackBanner
+        {...defaultProps}
+        progressV2Feedback={{empty: false}}
+        fetchProgressV2Feedback={fakeFetch}
+      />
+    );
+    expect(fakeFetch).to.have.been.calledOnce;
     const questionText = screen.queryByText(
       i18n.progressV2_feedback_question()
     );
@@ -37,9 +64,7 @@ describe('UnconnectedProgressFeedbackBanner', () => {
   });
 
   it('renders correctly when user has not answered survey question', () => {
-    render(
-      <UnconnectedProgressFeedbackBanner {...defaultProps} canShow={true} />
-    );
+    render(<UnconnectedProgressFeedbackBanner {...defaultProps} />);
     expect(screen.getByText(i18n.progressV2_feedback_question())).to.be.visible;
     expect(screen.getByTitle(i18n.progressV2_feedback_thumbsUp())).to.be
       .visible;
@@ -49,11 +74,7 @@ describe('UnconnectedProgressFeedbackBanner', () => {
 
   it('renders correctly when the data is loading', () => {
     render(
-      <UnconnectedProgressFeedbackBanner
-        {...defaultProps}
-        canShow={true}
-        isLoading={true}
-      />
+      <UnconnectedProgressFeedbackBanner {...defaultProps} isLoading={true} />
     );
     const questionText = screen.queryByText(
       i18n.progressV2_feedback_question()
@@ -67,7 +88,10 @@ describe('UnconnectedProgressFeedbackBanner', () => {
 
   it('clicking thumbs up attempts to send feedback and asks for more detailed feedback', async () => {
     render(
-      <UnconnectedProgressFeedbackBanner {...defaultProps} canShow={true} />
+      <UnconnectedProgressFeedbackBanner
+        {...defaultProps}
+        createProgressV2Feedback={fakeCreate}
+      />
     );
     const thumbsUpButton = screen.getByTitle(
       i18n.progressV2_feedback_thumbsUp()
@@ -90,7 +114,10 @@ describe('UnconnectedProgressFeedbackBanner', () => {
 
   it('clicking thumbs down attempts to send feedback and asks for more detailed feedback', async () => {
     render(
-      <UnconnectedProgressFeedbackBanner {...defaultProps} canShow={true} />
+      <UnconnectedProgressFeedbackBanner
+        {...defaultProps}
+        createProgressV2Feedback={fakeCreate}
+      />
     );
     const thumbsDownButton = screen.getByTitle(
       i18n.progressV2_feedback_thumbsDown()
@@ -113,7 +140,10 @@ describe('UnconnectedProgressFeedbackBanner', () => {
 
   it('user is able to close the banner', async () => {
     render(
-      <UnconnectedProgressFeedbackBanner {...defaultProps} canShow={true} />
+      <UnconnectedProgressFeedbackBanner
+        {...defaultProps}
+        createProgressV2Feedback={fakeCreate}
+      />
     );
 
     // Give feedback
@@ -140,5 +170,52 @@ describe('UnconnectedProgressFeedbackBanner', () => {
       expect(questionText).to.not.exist;
       expect(shareMoreText).to.not.exist;
     });
+  });
+
+  it('attempts reset and reload if error from createFeedback', async () => {
+    console.log('---------------');
+    const {rerender} = render(
+      <UnconnectedProgressFeedbackBanner {...defaultProps} />
+    );
+    rerender(
+      <UnconnectedProgressFeedbackBanner
+        {...defaultProps}
+        errorWhenCreatingOrLoading={'This produced an error.'}
+      />
+    );
+    const questionText = screen.queryByText(
+      i18n.progressV2_feedback_question()
+    );
+    const shareMoreText = screen.queryByText(
+      i18n.progressV2_feedback_shareMore()
+    );
+    expect(questionText).to.not.exist;
+    expect(shareMoreText).to.not.exist;
+    // console.log('before first await');
+    // await waitFor(() => {
+    //   const questionText = screen.queryByText(
+    //     i18n.progressV2_feedback_question()
+    //   );
+    //   const shareMoreText = screen.queryByText(
+    //     i18n.progressV2_feedback_shareMore()
+    //   );
+    //   expect(questionText).to.not.exist;
+    //   expect(shareMoreText).to.not.exist;
+    // });
+    console.log('before second await');
+    await waitFor(
+      expect(screen.getByText(i18n.progressV2_feedback_question())).to.be
+        .visible
+    );
+    console.log('after last await');
+    screen.debug();
+    // await waitFor(() => {
+    //   expect(screen.getByText(i18n.progressV2_feedback_question())).to.be
+    //     .visible;
+    //   expect(screen.getByTitle(i18n.progressV2_feedback_thumbsUp())).to.be
+    //     .visible;
+    //   expect(screen.getByTitle(i18n.progressV2_feedback_thumbsDown())).to.be
+    //     .visible;
+    // });
   });
 });
