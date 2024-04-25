999db98bee69445f6a310312667f89647372761f
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-04-25 07:31:06 -0700

    prototype troubleshooter

--- apps/src/troubleshooter/TroubleshooterPage.tsx
+++ apps/src/troubleshooter/TroubleshooterPage.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import * as Table from 'reactabular-table';
 
+import supportedBrowsers from '@cdo/apps/generated/supportedBrowsers';
+
 const TroubleshooterPage = () => {
   const columns = [
     {
       property: 'name',
       header: {
         label: 'Name',
+      },
+    },
+    {
+      property: 'description',
+      header: {
+        label: 'Description',
       },
     },
     {
@@ -20,11 +28,41 @@ const TroubleshooterPage = () => {
   const rows = [
     {
       name: 'Browser',
-      status: 'Pass',
+      description:
+        'See support article for minimum supported browser versions.',
+      status: supportedBrowsers.test(navigator.userAgent) ? 'Pass' : 'Fail',
+    },
+    {
+      name: 'Screen Resolution/Viewport Size',
+      description: 'Minimum supported is 1024x768',
+      status:
+        window.outerWidth >= 1024 && window.outerHeight >= 768
+          ? 'Pass'
+          : 'Fail',
     },
     {
       name: 'Can reach Code.org?',
-      status: 'Pass',
+      status: 'Pending...',
+    },
+    {
+      name: 'Can load captchas?',
+      status: 'Pending...',
+    },
+    {
+      name: 'Can connect to YouTube?',
+      status: 'Pending...',
+    },
+    {
+      name: 'Can access Code.org hosted videos?',
+      status: 'Pending...',
+    },
+    {
+      name: 'Can use Web Lab?',
+      status: 'Pending...',
+    },
+    {
+      name: 'Can use App Lab Data sets?',
+      status: 'Pending...',
     },
   ];
 
