fae396c60671abf4e267956ec25f2ba0245f066e
Bethany Connor <bethany@code.org>
2024-04-26 11:48:05 -0400

    Update other environments too

--- lib/cdo/shared_constants/mailjet_constants.rb
+++ lib/cdo/shared_constants/mailjet_constants.rb
@@ -3,8 +3,8 @@ module MailJetConstants
     welcome: {
       template_id: {
         production: 5_831_384,
-        staging: 5_826_411,
-        development: 5_808_078
+        staging: 5_917_989,
+        development: 5_917_988
       },
       from_address: 'hadi_partovi@code.org',
       from_name: 'Hadi Partovi',
