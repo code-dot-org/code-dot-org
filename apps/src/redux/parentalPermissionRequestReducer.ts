8451021fb833b9732259a9e80c89ac1b20f2e721
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-05-01 23:08:10 +0300

    P20-57: Replace promises with async/await in reducer

--- apps/src/redux/parentalPermissionRequestReducer.ts
+++ apps/src/redux/parentalPermissionRequestReducer.ts
@@ -109,74 +109,63 @@ export default function parentalPermissionRequestReducer(
   }
 }
 
-export const fetchPendingPermissionRequest = (
+export const fetchPendingPermissionRequest = async (
   dispatch: Dispatch<ParentalPermissionRequestAction>
 ) => {
-  dispatch({type: FETCH_PENDING_PERMISSION_REQUEST_PERFORM});
+  try {
+    dispatch({type: FETCH_PENDING_PERMISSION_REQUEST_PERFORM});
 
-  fetch(FETCH_PENDING_PERMISSION_REQUEST_URL, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => {
-      if (!response.ok) throw i18n.formServerError();
-      if (response.status === 204) return null;
-      return response.json();
-    })
-    .then(parentalPermissionRequest => {
-      dispatch({
-        type: FETCH_PENDING_PERMISSION_REQUEST_SUCCESS,
-        parentalPermissionRequest,
-      });
-    })
-    .catch(error => {
-      console.error(FETCH_PENDING_PERMISSION_REQUEST_FAILURE, error);
-
-      dispatch({
-        type: FETCH_PENDING_PERMISSION_REQUEST_FAILURE,
-        error: error instanceof Error ? i18n.formServerError() : error,
-      });
+    const response = await fetch(FETCH_PENDING_PERMISSION_REQUEST_URL, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
     });
+
+    if (!response.ok) throw i18n.formServerError();
+
+    dispatch({
+      type: FETCH_PENDING_PERMISSION_REQUEST_SUCCESS,
+      parentalPermissionRequest:
+        response.status === 204 ? null : await response.json(),
+    });
+  } catch (error) {
+    dispatch({
+      type: FETCH_PENDING_PERMISSION_REQUEST_FAILURE,
+      error: error instanceof Error ? i18n.formServerError() : error,
+    });
+  }
 };
 
 export const requestParentalPermission = async (
   dispatch: Dispatch<ParentalPermissionRequestAction>,
   parentEmail: string
 ) => {
-  dispatch({type: REQUEST_PARENTAL_PERMISSION_PERFORM});
+  try {
+    dispatch({type: REQUEST_PARENTAL_PERMISSION_PERFORM});
 
-  fetch(REQUEST_PARENTAL_PERMISSION_URL, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      'X-CSRF-Token': await getAuthenticityToken(),
-    },
-    body: JSON.stringify({'parent-email': parentEmail}),
-  })
-    .then(async response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        const responseData = await response.json();
-        throw responseData.error || i18n.formServerError();
-      }
-    })
-    .then(parentalPermissionRequest => {
-      dispatch({
-        type: REQUEST_PARENTAL_PERMISSION_SUCCESS,
-        parentalPermissionRequest,
-      });
-    })
-    .catch(error => {
-      console.error(REQUEST_PARENTAL_PERMISSION_FAILURE, error);
-
-      dispatch({
-        type: REQUEST_PARENTAL_PERMISSION_FAILURE,
-        error: error instanceof Error ? i18n.formServerError() : error,
-      });
+    const response = await fetch(REQUEST_PARENTAL_PERMISSION_URL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': await getAuthenticityToken(),
+      },
+      body: JSON.stringify({'parent-email': parentEmail}),
     });
+
+    const responseData = await response.json();
+    if (!response.ok) throw responseData.error || i18n.formServerError();
+
+    dispatch({
+      type: REQUEST_PARENTAL_PERMISSION_SUCCESS,
+      parentalPermissionRequest: responseData,
+    });
+  } catch (error) {
+    dispatch({
+      type: REQUEST_PARENTAL_PERMISSION_FAILURE,
+      error: error instanceof Error ? i18n.formServerError() : error,
+    });
+  }
 };
