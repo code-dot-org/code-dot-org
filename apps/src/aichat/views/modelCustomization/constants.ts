5c5433a01e4840b44dc239d64d7eb136d1fedd81
bencodeorg <ben@code.org>
2024-05-01 16:43:29 -0700

    Set min temp to 0.1 (#58356)

--- apps/src/aichat/views/modelCustomization/constants.ts
+++ apps/src/aichat/views/modelCustomization/constants.ts
@@ -6,7 +6,7 @@ import {
 } from '../../types';
 import {modelDescriptions} from '@cdo/apps/aichat/constants';
 
-export const MIN_TEMPERATURE = 0;
+export const MIN_TEMPERATURE = 0.1;
 export const MAX_TEMPERATURE = 1;
 export const SET_TEMPERATURE_STEP = 0.1;
 export const MAX_RETRIEVAL_CONTEXTS = 20;
