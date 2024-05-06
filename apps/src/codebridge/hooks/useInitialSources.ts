fbf29c5d131b605b6bd31620c0dcc54b94315db8
Molly Moen <molly@code.org>
2024-05-06 16:59:52 -0700

    work in progress

--- apps/src/codebridge/hooks/useInitialSources.ts
+++ apps/src/codebridge/hooks/useInitialSources.ts
@@ -6,21 +6,30 @@ import {ProjectSources} from '@cdo/apps/lab2/types';
 import {useAppSelector} from '@cdo/apps/util/reduxHooks';
 
 export const useInitialSources = (defaultSources: ProjectSources) => {
-  const labState = useAppSelector(state => state.lab);
+  const labInitialSources = useAppSelector(state => state.lab.initialSources);
+  const levelStartSource = useAppSelector(
+    state => state.lab.levelProperties?.source
+  );
   const isStartMode = getAppOptionsEditBlocks() === START_SOURCES;
 
   const initialSources = useMemo(() => {
-    const startSources = labState.levelProperties?.source
-      ? labState.levelProperties
+    console.log({
+      levelStartSource,
+      defaultSources,
+      isStartMode,
+      labInitialSources,
+    });
+    const startSources = levelStartSource
+      ? {source: levelStartSource}
       : defaultSources;
 
     if (isStartMode) {
       return startSources;
     }
 
-    const projectSources = labState.initialSources;
+    const projectSources = labInitialSources;
     return projectSources || startSources;
-  }, [labState, isStartMode, defaultSources]);
+  }, [levelStartSource, defaultSources, isStartMode, labInitialSources]);
 
   return initialSources;
 };
--- apps/src/codebridge/hooks/useSource.ts
+++ apps/src/codebridge/hooks/useSource.ts
@@ -7,27 +7,33 @@ import {getAppOptionsEditBlocks} from '@cdo/apps/lab2/projects/utils';
 import {MultiFileSource, ProjectSources} from '@cdo/apps/lab2/types';
 import {useAppDispatch, useAppSelector} from '@cdo/apps/util/reduxHooks';
 
+// import {useInitialSources} from './useInitialSources';
+
 export const useSource = (defaultSources: ProjectSources) => {
   const dispatch = useAppDispatch();
   const projectSource = useAppSelector(state => state.lab.projectSource);
   const labInitialSources = useAppSelector(state => state.lab.initialSources);
   const source = projectSource?.source as MultiFileSource;
-  const levelProperties = useAppSelector(state => state.lab.levelProperties);
+  const levelSource = useAppSelector(
+    state => state.lab.levelProperties?.source
+  );
   const channelId = useAppSelector(state => state.lab.channel?.id);
   const isStartMode = getAppOptionsEditBlocks() === START_SOURCES;
+  //const initialSources = useInitialSources(defaultSources);
 
-  const initialSources = useMemo(() => {
-    const startSources = levelProperties?.source
-      ? levelProperties
-      : defaultSources;
+  // const initialSources = useMemo(() => {
+  //   console.log('in useMemo');
+  //   const startSources = levelProperties?.source
+  //     ? {source: levelProperties.source}
+  //     : defaultSources;
 
-    if (isStartMode) {
-      return startSources;
-    }
+  //   if (isStartMode) {
+  //     return startSources;
+  //   }
 
-    const projectSources = labInitialSources;
-    return projectSources || startSources;
-  }, [levelProperties, labInitialSources, isStartMode, defaultSources]);
+  //   const projectSources = labInitialSources;
+  //   return projectSources || startSources;
+  // }, [levelProperties, labInitialSources, isStartMode, defaultSources]);
 
   const setProject = useMemo(
     () => (newProject: MultiFileSource) => {
@@ -45,11 +51,24 @@ export const useSource = (defaultSources: ProjectSources) => {
   }, [isStartMode, projectSource]);
 
   useEffect(() => {
+    console.log('in setup use effect');
+    const startSources = levelSource ? {source: levelSource} : defaultSources;
+
+    const initialSources = isStartMode
+      ? startSources
+      : labInitialSources || startSources;
     // We reset the project when the channelId changes, as this means we are on a new level.
     if (initialSources) {
       setProject(initialSources.source as MultiFileSource);
     }
-  }, [channelId, initialSources, setProject]);
+  }, [
+    channelId,
+    levelSource,
+    isStartMode,
+    defaultSources,
+    setProject,
+    labInitialSources,
+  ]);
 
   return {source, setProject};
 };
