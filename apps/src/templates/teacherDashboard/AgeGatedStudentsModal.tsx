baf09c7004217fcbd2765d6cee47af207a8b95fd
Juan Manzo <juan.manzo@code.org>
2024-05-05 22:31:02 -0700

    Create Age gated student Modal

--- apps/src/templates/teacherDashboard/AgeGatedStudentsModal.tsx
+++ apps/src/templates/teacherDashboard/AgeGatedStudentsModal.tsx
@@ -1,45 +1,71 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
+import Spinner from '@cdo/apps/code-studio/pd/components/spinner';
 import i18n from '@cdo/locale';
 
+import BaseDialog from '../BaseDialog';
 import SafeMarkdown from '../SafeMarkdown';
 
-import style from '@cdo/apps/templates/teacherDashboard/age-gated-students-modal.module.scss';
+import AgeGatedStudentsTable from './AgeGatedStudentsTable';
 
-interface Props {
+import styles from '@cdo/apps/templates/teacherDashboard/age-gated-students-modal.module.scss';
+
+interface ReduxState {
+  manageStudents: {
+    isLoadingStudents?: boolean;
+  };
+}
+interface AgeGatedStudentsProps {
   onClose: () => void;
+  isOpen: boolean;
 }
 
-const AgeGatedStudentsModal: React.FC<Props> = ({onClose}) => {
+type Props = ReduxState & AgeGatedStudentsProps;
+
+const AgeGatedStudentsModal: React.FC<Props> = ({
+  manageStudents: isLoadingStudents,
+  isOpen,
+  onClose,
+}) => {
   return (
-    <div className={style.modalContainer} id="modal" onClick={onClose}>
-      <div className={style.modalContent}>
-        <h2>{i18n.childAccountPolicy_studentParentalConsentStatus()}</h2>
-        <p>{i18n.childAccountPolicy_studentParentalConsentNotice()}</p>
-        <br />
-        <SafeMarkdown
-          markdown={i18n.childAccountPolicy_consentProcessReadMore({url: '/'})}
-        />
-        <table>
-          <thead>
-            <tr>
-              <th>Header 1</th>
-              <th>Header 2</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Data 1</td>
-              <td>Data 2</td>
-            </tr>
-          </tbody>
-        </table>
-        <button type="button" onClick={onClose}>
-          Close
-        </button>
+    <BaseDialog
+      isOpen={isOpen}
+      handleClose={onClose}
+      useUpdatedStyles={true}
+      fixedWidth={800}
+    >
+      <div className={styles.modalContainer} id="modal">
+        <div>
+          <h2 className={styles.modalHeader}>
+            {i18n.childAccountPolicy_studentParentalConsentStatus()}
+          </h2>
+          <hr />
+          <p>{i18n.childAccountPolicy_studentParentalConsentNotice()}</p>
+          <br />
+          <SafeMarkdown
+            markdown={i18n.childAccountPolicy_consentProcessReadMore({
+              url: '/',
+            })}
+          />
+          {isLoadingStudents && <Spinner />}
+          {!isLoadingStudents && <AgeGatedStudentsTable />}
+          <hr />
+          <div className={styles.modalButton}>
+            <button type="button" onClick={onClose}>
+              Close
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </BaseDialog>
   );
 };
+export const UnconnectedAgeGatedStudentsModal = AgeGatedStudentsModal;
 
-export default AgeGatedStudentsModal;
+export default connect(
+  (state: ReduxState) => ({
+    isLoadingStudents: state.manageStudents.isLoadingStudents,
+  }),
+  {}
+)(AgeGatedStudentsModal);
