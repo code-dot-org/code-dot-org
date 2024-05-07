2dc65b372db039608014d7b33fb3b2ed4c580568
Sanchit Malhotra <sanchit@code.org>
2024-05-07 09:44:24 -0700

    Lab2: add source validator for labs with JSON sources

--- apps/src/lab2/responseValidators.ts
+++ apps/src/lab2/responseValidators.ts
@@ -6,7 +6,7 @@ import {
   ProjectSources,
 } from './types';
 import Lab2Registry from './Lab2Registry';
-import {BLOCKLY_LABS} from './constants';
+import {BLOCKLY_LABS, LABS_WITH_JSON_SOURCES} from './constants';
 
 // Validator for Blockly sources.
 const BlocklySourceResponseValidator: ResponseValidator<
@@ -46,6 +46,20 @@ const PythonSourceResponseValidator: ResponseValidator<
   return sourceValidatorHelper(response, pythonValidator);
 };
 
+const JsonSourceResponseValidator: ResponseValidator<
+  ProjectSources
+> = response => {
+  const jsonValidator = (responseToValidate: Record<string, unknown>) => {
+    try {
+      JSON.parse(responseToValidate.source as string);
+    } catch (e) {
+      throw new ValidationError('Error parsing JSON: ' + e);
+    }
+  };
+
+  return sourceValidatorHelper(response, jsonValidator);
+};
+
 // Default source validator. This just checks if there is a source field.
 const DefaultSourceResponseValidator: ResponseValidator<
   ProjectSources
@@ -62,6 +76,8 @@ export const SourceResponseValidator: ResponseValidator<
   } else if (appName !== null && BLOCKLY_LABS.includes(appName)) {
     // Blockly labs
     return BlocklySourceResponseValidator(response);
+  } else if (appName !== null && LABS_WITH_JSON_SOURCES.includes(appName)) {
+    return JsonSourceResponseValidator(response);
   } else {
     // Everything else uses the default validator
     return DefaultSourceResponseValidator(response);
