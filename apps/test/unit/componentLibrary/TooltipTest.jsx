80946467671ab9aaa053ce9a7e258925f93f6a2a
denyslevada <levada.denys@gmail.com>
2024-05-07 21:54:19 +0300

    update test

--- apps/test/unit/componentLibrary/TooltipTest.jsx
+++ apps/test/unit/componentLibrary/TooltipTest.jsx
@@ -1,155 +1,28 @@
-// TODO: Uncomment and fix tests
-// import {render, screen} from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import React from 'react';
-// import sinon from 'sinon';
-//
-// import Toggle from '@cdo/apps/componentLibrary/toggle';
-//
-// import {expect} from '../../util/reconfiguredChai';
-//
-// describe('Design System - Toggle', () => {
-//   it('Toggle - renders with correct label', () => {
-//     const toggleLabel = 'Toggle label';
-//     render(
-//       <Toggle name="test-toggle" value="test-toggle" label={toggleLabel} />
-//     );
-//
-//     const toggle = screen.getByDisplayValue('test-toggle');
-//     expect(toggle).to.exist;
-//     expect(screen.getByText(toggleLabel)).to.exist;
-//   });
-//
-//   it('Toggle - changes checked state on click', async () => {
-//     const user = userEvent.setup();
-//     const spyOnChange = sinon.spy();
-//
-//     let checked = false;
-//     const onChange = () => {
-//       checked = !checked;
-//       spyOnChange(checked);
-//     };
-//
-//     // Initial render
-//     const {rerender} = render(
-//       <Toggle
-//         name="test-toggle"
-//         value="test-toggle"
-//         label="Toggle label"
-//         checked={checked}
-//         onChange={onChange}
-//       />
-//     );
-//
-//     let toggle = screen.getByDisplayValue('test-toggle');
-//
-//     expect(toggle).to.exist;
-//     expect(toggle.checked).to.be.false;
-//     expect(toggle.disabled).to.be.false;
-//
-//     await user.click(toggle);
-//
-//     // Re-render after user's first click
-//     rerender(
-//       <Toggle
-//         name="test-toggle"
-//         value="test-toggle"
-//         label="Toggle label"
-//         checked={checked}
-//         onChange={onChange}
-//       />
-//     );
-//
-//     toggle = screen.getByDisplayValue('test-toggle');
-//
-//     expect(spyOnChange).to.have.been.calledOnce;
-//     expect(spyOnChange).to.have.been.calledWith(true);
-//     expect(toggle.checked).to.be.true;
-//     expect(toggle.disabled).to.be.false;
-//
-//     await user.click(toggle);
-//
-//     // Re-render after user's second click
-//     rerender(
-//       <Toggle
-//         name="test-toggle"
-//         value="test-toggle"
-//         label="Toggle label"
-//         checked={checked}
-//         onChange={onChange}
-//       />
-//     );
-//
-//     toggle = screen.getByDisplayValue('test-toggle');
-//
-//     expect(spyOnChange).to.have.been.calledTwice;
-//     expect(toggle.checked).to.be.false;
-//     expect(toggle.disabled).to.be.false;
-//   });
-//
-//   it("Toggle - renders disabled toggle, doesn't change on click", async () => {
-//     const user = userEvent.setup();
-//     const spyOnChange = sinon.spy();
-//
-//     let checked = false;
-//     const onChange = () => {
-//       checked = !checked;
-//       spyOnChange(checked);
-//     };
-//
-//     // Initial render
-//     const {rerender} = render(
-//       <Toggle
-//         name="test-toggle"
-//         value="test-toggle"
-//         label="Toggle label"
-//         checked={checked}
-//         onChange={onChange}
-//         disabled={true}
-//       />
-//     );
-//
-//     let toggle = screen.getByDisplayValue('test-toggle');
-//     expect(toggle).to.exist;
-//
-//     expect(toggle.checked).to.be.false;
-//     expect(toggle.disabled).to.be.true;
-//
-//     await user.click(toggle);
-//
-//     // Re-render after user's first click
-//     rerender(
-//       <Toggle
-//         name="test-toggle"
-//         value="test-toggle"
-//         label="Toggle label"
-//         checked={checked}
-//         onChange={onChange}
-//         disabled={true}
-//       />
-//     );
-//
-//     toggle = screen.getByDisplayValue('test-toggle');
-//
-//     expect(toggle.checked).to.be.false;
-//     expect(toggle.disabled).to.be.true;
-//
-//     await user.click(toggle);
-//
-//     // Re-render after user's second click
-//     rerender(
-//       <Toggle
-//         name="test-toggle"
-//         value="test-toggle"
-//         label="Toggle label"
-//         checked={checked}
-//         onChange={onChange}
-//         disabled={true}
-//       />
-//     );
-//
-//     toggle = screen.getByDisplayValue('test-toggle');
-//     expect(toggle.checked).to.be.false;
-//     expect(toggle.disabled).to.be.true;
-//   });
-// });
+import {render, screen} from '@testing-library/react';
+import React from 'react';
+
+import {WithTooltip} from '@cdo/apps/componentLibrary/tooltip';
+
+import {expect} from '../../util/reconfiguredChai';
+
+describe('Design System - Tooltip', () => {
+  it('Tooltip - renders with correct label', () => {
+    const WithTooltipToRender = () => (
+      <WithTooltip tooltipProps={{tooltipId: 'tooltip1', text: 'tooltipText'}}>
+        <button type="button">hover me</button>
+      </WithTooltip>
+    );
+    const {rerender} = render(<WithTooltipToRender />);
+
+    let tooltip = screen.getByText('tooltipText');
+    let tooltipTrigger = screen.getByText('hover me');
+
+    expect(tooltip).to.exist;
+    expect(tooltipTrigger).to.exist;
+
+    rerender(<WithTooltipToRender />);
+
+    tooltip = screen.getByText('tooltipText');
+    expect(tooltip).to.exist;
+  });
+});
