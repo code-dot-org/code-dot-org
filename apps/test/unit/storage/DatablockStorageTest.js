d7250e8504ace333368573c555f4a5e150b02f15
Seth Nickell <snickell@gmail.com>
2024-05-08 12:24:35 -1000

    Empty test, empty promises

---
+++ apps/test/unit/storage/DatablockStorageTest.js
@@ -0,0 +1,10 @@
+import {expect} from '../../util/reconfiguredChai';
+
+describe('DatablockStorage', () => {
+  describe('rate limiting', () => {
+    it('warns and succeeds on keys with invalid characters', done => {
+      expect(true).to.equal.true;
+      done();
+    });
+  });
+});
