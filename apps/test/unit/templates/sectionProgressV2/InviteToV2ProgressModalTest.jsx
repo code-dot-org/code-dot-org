4cdc137c580d1ce1168e29637aaf82cf1dee19b2
kobryan0619 <kaitie.obryan@gmail.com>
2024-05-09 00:00:18 -0500

    added all tests

--- apps/test/unit/templates/sectionProgressV2/InviteToV2ProgressModalTest.jsx
+++ apps/test/unit/templates/sectionProgressV2/InviteToV2ProgressModalTest.jsx
@@ -1,25 +1,12 @@
 import {fireEvent, render, screen} from '@testing-library/react';
 import $ from 'jquery';
 import React from 'react';
-import {Provider} from 'react-redux';
 import sinon from 'sinon';
 
-import {
-  getStore,
-  registerReducers,
-  restoreRedux,
-  stubRedux,
-} from '@cdo/apps/redux';
-import currentUser, {
-  setHasSeenProgressTableInvite,
-} from '@cdo/apps/templates/currentUserRedux';
 import {UnconnectedInviteToV2ProgressModal} from '@cdo/apps/templates/sectionProgressV2/InviteToV2ProgressModal';
 import i18n from '@cdo/locale';
 
 import {expect} from '../../../util/reconfiguredChai';
-
-// const setShowProgressTableV2Stub = sinon.stub();
-// const setHasSeenProgressTableInviteStub = sinon.stub();
 
 const DEFAULT_PROPS = {
   setShowProgressTableV2: () => {},
@@ -28,36 +15,22 @@ const DEFAULT_PROPS = {
 };
 
 describe('UnconnectedInviteToV2ProgressModal', () => {
-  let store;
   let postStub;
 
   beforeEach(() => {
     postStub = sinon.stub($, 'post').returns(Promise.resolve({}));
-
-    stubRedux();
-    registerReducers({
-      currentUser,
-    });
-
-    store = getStore();
-    store.dispatch(setHasSeenProgressTableInvite(false));
   });
 
   afterEach(() => {
-    restoreRedux();
     postStub.restore();
-    // setHasSeenProgressTableInviteStub.restore();
-    // setShowProgressTableV2Stub.restore();
   });
 
   function renderDefault(propOverrides = {}) {
     render(
-      <Provider store={store}>
-        <UnconnectedInviteToV2ProgressModal
-          {...DEFAULT_PROPS}
-          {...propOverrides}
-        />
-      </Provider>
+      <UnconnectedInviteToV2ProgressModal
+        {...DEFAULT_PROPS}
+        {...propOverrides}
+      />
     );
   }
 
@@ -119,22 +92,37 @@ describe('UnconnectedInviteToV2ProgressModal', () => {
     expect(screen.queryByText(i18n.remindMeLater())).to.be.null;
   });
 
-  // it('allows user to delay the invitation', () => {
-  //   renderDefault();
+  it('allows user to delay the invitation', () => {
+    renderDefault();
 
-  //   const delayButton = screen.getByText(i18n.remindMeLater());
-  //   fireEvent.click(delayButton);
+    const delayButton = screen.getByText(i18n.remindMeLater());
+    fireEvent.click(delayButton);
 
-  //   expect(postStub).calledWith(
-  //     '/api/v1/users/date_progress_table_invitation_last_delayed'
-  //   ).to.be.true;
-  //   expect(screen.queryByText(i18n.tryItNow())).to.be.null;
-  //   expect(screen.queryByText(i18n.progressTrackingAnnouncement())).to.be.null;
-  //   expect(screen.queryByText(i18n.remindMeLater())).to.be.null;
-  // });
+    sinon.assert.calledOnce(postStub);
+    sinon.assert.calledWithMatch(
+      postStub,
+      '/api/v1/users/date_progress_table_invitation_last_delayed'
+    );
+
+    expect(screen.queryByText(i18n.tryItNow())).to.be.null;
+    expect(screen.queryByText(i18n.progressTrackingAnnouncement())).to.be.null;
+    expect(screen.queryByText(i18n.remindMeLater())).to.be.null;
+  });
 
   it('does not show the dialog if they have already accepted or rejected the invitation', () => {
     renderDefault({hasSeenProgressTableInvite: true});
+
+    expect(screen.queryByText(i18n.tryItNow())).to.be.null;
+    expect(screen.queryByText(i18n.progressTrackingAnnouncement())).to.be.null;
+    expect(screen.queryByText(i18n.remindMeLater())).to.be.null;
+  });
+
+  it('does not show the dialog if they have already accepted or rejected the invitation after delaying invitation', () => {
+    renderDefault({
+      hasSeenProgressTableInvite: true,
+      dateProgressTableInvitationDelayed:
+        'Wed May 01 2024 14:22:23 GMT-0500 (Central Daylight Time)',
+    });
 
     expect(screen.queryByText(i18n.tryItNow())).to.be.null;
     expect(screen.queryByText(i18n.progressTrackingAnnouncement())).to.be.null;
