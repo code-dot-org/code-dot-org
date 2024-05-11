f6d02e3dfe48a91c5df6b4810efa8e7979d9af14
Seth Nickell <snickell@gmail.com>
2024-05-11 06:10:13 -1000

    Add pyodide build to Rails assets path

--- dashboard/config/initializers/assets.rb
+++ dashboard/config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('apps', 'build', 'package', 'js', 'pyodide')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
