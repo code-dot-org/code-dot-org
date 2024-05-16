071976c221798730d395340ddb1e6d1a0e3ae148
juanmanzojr <137838584+juanmanzojr@users.noreply.github.com>
2024-05-16 16:46:34 -0700

    Require US state during account signup and on update (#58697)
    * Add us_state validation back and adding unit tests
    
    * WIP
    
    * Tests for country code inclusion in state modal
    
    * Add country code field patch params to test country persists
    
    ---------
    
    Co-authored-by: Dayne <dayne@code.org>

--- dashboard/app/controllers/registrations_controller.rb
+++ dashboard/app/controllers/registrations_controller.rb
@@ -257,6 +257,7 @@ class RegistrationsController < Devise::RegistrationsController
     student_information[:us_state] = params[:user][:us_state] unless current_user.user_provided_us_state
     student_information[:user_provided_us_state] = params[:user][:us_state].present? unless current_user.user_provided_us_state
     student_information[:gender_student_input] = params[:user][:gender_student_input] if current_user.gender.blank?
+    student_information[:country_code] = params[:user][:country_code] if current_user.country_code.blank?
 
     current_user.update(student_information) unless student_information.empty?
   end
