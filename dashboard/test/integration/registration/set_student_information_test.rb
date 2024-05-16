293249e5a782bf55415d944a9cf5872bd4a7cd2c
Juan Manzo <juan.manzo@code.org>
2024-05-16 14:24:50 -0700

    Tests for country code inclusion in state modal

--- dashboard/test/integration/registration/set_student_information_test.rb
+++ dashboard/test/integration/registration/set_student_information_test.rb
@@ -62,18 +62,19 @@ module RegistrationsControllerTests
       assert_nil student.user_provided_us_state
     end
 
-    test "set_student_information sets age if user is signed in and age is blank" do
+    test "set_student_information sets age and state if user is signed in and age and state is blank" do
       student = create :student_in_picture_section, birthday: nil
       assert student.age.blank?
 
       sign_in student
-      patch '/users/set_student_information', params: {user: {age: '20', us_state: 'AL', gender_student_input: 'he'}}
+      patch '/users/set_student_information', params: {user: {age: '20', us_state: 'AL', gender_student_input: 'he', country_code: 'US'}}
       assert_response :success
 
       student.reload
       assert_equal 20, student.age
       assert_equal 'AL', student.us_state
       assert_equal 'm', student.gender
+      assert_equal 'US', student.country_code
     end
   end
 end
