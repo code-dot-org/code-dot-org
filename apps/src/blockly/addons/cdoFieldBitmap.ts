592ae98e20f00739e7fe528187826aadbfbb80f4
Mike Harvey <mike.harvey@code.org>
2024-05-22 09:55:06 -0400

    [Sprite Lab] redo fix bitmap field for smaller images

--- apps/src/blockly/addons/cdoFieldBitmap.ts
+++ apps/src/blockly/addons/cdoFieldBitmap.ts
@@ -20,6 +20,20 @@ export class CdoFieldBitmap extends FieldBitmap {
   }
 
   /**
+   * Called when a new value has been validated and is about to be set.
+   * We extend this to ensure that the field height is always respected.
+   * See: https://github.com/google/blockly-samples/issues/2372
+   * @param newValue The value that's about to be set.
+   * @override
+   */
+  doValueUpdate_(newValue: number[][]) {
+    super.doValueUpdate_(newValue);
+    // This calculation is performed once in the constructor of the base class.
+    if (newValue && this.fieldHeight) {
+      this.pixelSize = this.fieldHeight / this.imgHeight;
+    }
+  }
+  /**
    * Converts the field's value to XML representation.
    * @param {Element} fieldElement - The XML element to populate with field data.
    * @returns {Element} The populated XML element.
@@ -37,12 +51,5 @@ export class CdoFieldBitmap extends FieldBitmap {
   fromXml(fieldElement: Element): void {
     const bitmap = JSON.parse(fieldElement.textContent || '[]');
     this.setValue(bitmap);
-  }
-
-  setValue(newValue: number[][], fireChangeEvent = true) {
-    super.setValue(newValue, fireChangeEvent);
-    if (newValue?.length) {
-      this.pixelSize = this.fieldHeight / newValue.length;
-    }
   }
 }
