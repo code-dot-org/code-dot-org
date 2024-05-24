6d8436dd365b4b6bd7b180d712835607484f22af
David Bailey <davidsbailey@users.noreply.github.com>
2024-05-24 14:59:26 -0700

    remove unused AssignmentSelector component

--- apps/src/templates/sectionProgress/UnitSelector.js
+++ apps/src/templates/sectionProgress/UnitSelector.js
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 
-// TODO: Can/should we share any logic with AssignmentSelector?
-
 export const dropdownStyles = {
   dropdown: {
     display: 'block',
