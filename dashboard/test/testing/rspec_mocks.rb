c4425647201533952802f0b7e5f652b63c3ee7e4
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-05-24 14:56:13 +0300

    P20-548: Fix RSpec Mocks `expect` method

--- dashboard/test/testing/rspec_mocks.rb
+++ dashboard/test/testing/rspec_mocks.rb
@@ -1,5 +1,15 @@
 require 'rspec/mocks'
 
+class Minitest::Spec
+  module DSL
+    module InstanceMethods
+      # Removes the MiniTest Spec `expect` alias for the method `_`
+      # to ensure compatibility with the RSpec Mocks `expect` method
+      remove_method(:expect) if method_defined?(:expect)
+    end
+  end
+end
+
 class ActiveSupport::TestCase
   include RSpec::Mocks::ExampleMethods
 
