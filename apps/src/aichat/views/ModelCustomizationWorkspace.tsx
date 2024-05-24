ca6b9c00ec508637c9405f9e04099b579bc8ddee
Alice Fisher <alice.fisher@code.org>
2024-05-24 01:20:33 +0000

    use DSCO tabs

--- apps/src/aichat/views/ModelCustomizationWorkspace.tsx
+++ apps/src/aichat/views/ModelCustomizationWorkspace.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Tabs, {Tab} from './tabs/Tabs';
+import Tabs, {TabsProps} from '@cdo/apps/componentLibrary/tabs/Tabs';
 import SetupCustomization from './modelCustomization/SetupCustomization';
 import RetrievalCustomization from './modelCustomization/RetrievalCustomization';
 import PublishNotes from './modelCustomization/PublishNotes';
@@ -10,6 +10,7 @@ import {useAppSelector} from '@cdo/apps/util/reduxHooks';
 import {AichatLevelProperties} from '@cdo/apps/aichat/types';
 import {isReadOnlyWorkspace} from '@cdo/apps/lab2/lab2Redux';
 import {useSelector} from 'react-redux';
+import {FontAwesomeV6IconProps} from '@cdo/apps/componentLibrary/fontAwesomeV6Icon';
 
 const ModelCustomizationWorkspace: React.FunctionComponent = () => {
   const {
@@ -37,31 +38,57 @@ const ModelCustomizationWorkspace: React.FunctionComponent = () => {
     isDisabled(systemPrompt) &&
     isDisabled(selectedModelId);
 
+  const iconValue: FontAwesomeV6IconProps = {
+    iconName: 'lock',
+    iconStyle: 'solid',
+  };
+  const setupCustomizationIcon =
+    isSetupCustomizationReadOnly || isReadOnly ? iconValue : undefined;
+  const retrievalIcon =
+    isDisabled(retrievalContexts) || isReadOnly ? iconValue : undefined;
+  const publishIcon =
+    isDisabled(modelCardInfo) || isReadOnly ? iconValue : undefined;
+
+  const visibleTabs = [];
+  if (showSetupCustomization) {
+    visibleTabs.push({
+      value: 'setup',
+      text: 'Setup',
+      tabContent: <SetupCustomization />,
+      iconRight: setupCustomizationIcon,
+    });
+  }
+  if (isVisible(retrievalContexts)) {
+    visibleTabs.push({
+      value: 'retrieval',
+      text: 'Retrieval',
+      tabContent: <RetrievalCustomization />,
+      iconRight: retrievalIcon,
+    });
+  }
+  if (isVisible(modelCardInfo) && !hidePresentationPanel) {
+    visibleTabs.push({
+      value: 'modelCardInfo',
+      text: 'Publish',
+      tabContent: <PublishNotes />,
+      iconRight: publishIcon,
+    });
+  }
+
+  const tabArgs: TabsProps = {
+    name: 'modelCustomizationTabs',
+    tabs: visibleTabs,
+    defaultSelectedTabValue: visibleTabs[0].value,
+    onChange: () => null,
+    type: 'secondary',
+    size: 'l',
+  };
+  console.log('defaultSelectedTabValue', tabArgs.defaultSelectedTabValue);
+  console.log('visibleTabs', visibleTabs);
+
   return (
     <div className={styles.modelCustomizationWorkspace}>
-      <Tabs
-        tabs={
-          [
-            showSetupCustomization && {
-              title: 'Setup',
-              content: <SetupCustomization />,
-              isReadOnly: isSetupCustomizationReadOnly || isReadOnly,
-            },
-            isVisible(retrievalContexts) && {
-              title: 'Retrieval',
-              content: <RetrievalCustomization />,
-              isReadOnly: isDisabled(retrievalContexts) || isReadOnly,
-            },
-            isVisible(modelCardInfo) &&
-              !hidePresentationPanel && {
-                title: 'Publish',
-                content: <PublishNotes />,
-                isReadOnly: isDisabled(modelCardInfo) || isReadOnly,
-              },
-          ].filter(Boolean) as Tab[]
-        }
-        name="model-customization"
-      />
+      <Tabs {...tabArgs} />
     </div>
   );
 };
