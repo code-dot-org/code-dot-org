b3246f0efc1fd4de79b7259e77bf31222ea24539
Alice Fisher <alice.fisher@code.org>
2024-05-29 20:15:00 +0000

    one more refactor for dialog msg

--- apps/src/lab2/views/dialogs/StartOverDialog.tsx
+++ apps/src/lab2/views/dialogs/StartOverDialog.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import {BaseDialogProps} from './DialogManager';
 import moduleStyles from './confirm-dialog.module.scss';
 import {useAppSelector} from '@cdo/apps/util/reduxHooks';
-import {TEXT_BASED_LABS, AppName} from '@cdo/apps/lab2/types';
+import {TEXT_BASED_LABS} from '@cdo/apps/lab2/types';
 const commonI18n = require('@cdo/locale');
+
+// Lab-specific messages for starting over.
+const LAB_SPECIFIC_MESSAGES: {[key: string]: string} = {
+  aichat: commonI18n.startOverAichatModelCustomizations(),
+};
 
 /**
  * Start Over dialog used in Lab2 labs.
@@ -13,22 +18,17 @@ const StartOverDialog: React.FunctionComponent<BaseDialogProps> = ({
   handleConfirm,
   handleCancel,
 }) => {
-  const currentAppName = useAppSelector(
-    state => state.lab.levelProperties?.appName
-  );
+  const currentAppName =
+    useAppSelector(state => state.lab.levelProperties?.appName) || '';
 
-  const generaDialogMessage =
-    currentAppName && TEXT_BASED_LABS.includes(currentAppName)
+  const textBasedLab =
+    currentAppName && TEXT_BASED_LABS.includes(currentAppName);
+
+  const dialogMessage =
+    LAB_SPECIFIC_MESSAGES[currentAppName] ||
+    (textBasedLab
       ? commonI18n.startOverWorkspaceText()
-      : commonI18n.startOverWorkspace();
-
-  // Lab-specific messages for starting over.
-  const LAB_SPECIFIC_MESSAGES: Partial<Record<AppName, string>> = {
-    aichat: commonI18n.startOverAichatModelCustomizations(),
-  };
-  const appSpecificMessage =
-    currentAppName && LAB_SPECIFIC_MESSAGES[currentAppName];
-  const dialogMessage = appSpecificMessage || generaDialogMessage;
+      : commonI18n.startOverWorkspace());
 
   return (
     <div className={moduleStyles.confirmDialog}>
