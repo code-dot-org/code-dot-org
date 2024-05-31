f1843a87000b8460f191e4c0be8d2d25d5a7051b
Alice Fisher <alice.fisher@code.org>
2024-05-31 16:37:45 +0000

    check for channels param

--- dashboard/app/controllers/musiclab_controller.rb
+++ dashboard/app/controllers/musiclab_controller.rb
@@ -29,10 +29,7 @@ class MusiclabController < ApplicationController
     view_options(no_header: true, no_footer: true, full_width: true, no_padding_container: true)
 
     featured_music_projects = ProjectsList.fetch_active_published_featured_projects('music')
-    channel_ids = []
-    featured_music_projects.each do |project|
-      channel_ids << project['channel']
-    end
+    channel_ids = params[:channels] ? params[:channels].split(',') : featured_music_projects.map {|project| project['channel']}
 
     project_ids = channel_ids.map do |channel_id|
       _, project_id = storage_decrypt_channel_id(channel_id)
