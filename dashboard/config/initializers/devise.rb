9685117852cadf4be239b3fa714ab91f8800285c
Hannah Bergam <hannah.bergam@code.org>
2024-06-05 12:56:43 -0700

    Revert "feat(clever): add clever as a login and signup option"

--- dashboard/config/initializers/devise.rb
+++ dashboard/config/initializers/devise.rb
@@ -376,12 +376,6 @@ Devise.setup do |config|
     end
   end
 
-  OmniAuth.config.before_request_phase do |env|
-    Metrics::Events.log_event(
-      event_name: "#{env['omniauth.strategy'].options[:name]}-begin-auth",
-      )
-  end
-
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
   # is mountable, there are some extra configurations to be taken into account.
