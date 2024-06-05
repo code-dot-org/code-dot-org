71f515c5065eb828594db13936c8e8336722ca9b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-06-05 15:10:01 -0700

    Revert "use actual target version for apt package"
    This reverts commit 69923955bf2fea53c62f57fa7808f21ce8c8f88d.

--- cookbooks/cdo-mysql/recipes/client.rb
+++ cookbooks/cdo-mysql/recipes/client.rb
@@ -6,6 +6,6 @@ apt_package 'mysql-client' do
   if node['cdo-mysql']['target_version'] == '5.7'
     version '5.7.42-1ubuntu18.04'
   elsif node['cdo-mysql']['target_version'] == '8.0'
-    version '8.0.37-1ubuntu20.04'
+    version '8.0.36-0ubuntu0.22.04.1'
   end
 end
