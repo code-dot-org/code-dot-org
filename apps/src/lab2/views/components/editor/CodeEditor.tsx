5fd1d0d02fd7cbba5fdefee50bca26f6bc1138b8
Molly Moen <molly@code.org>
2024-06-06 13:39:17 -0700

    make editor able to be read-only

--- apps/src/lab2/views/components/editor/CodeEditor.tsx
+++ apps/src/lab2/views/components/editor/CodeEditor.tsx
@@ -1,13 +1,14 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
 import classNames from 'classnames';
-import {EditorState, Extension} from '@codemirror/state';
+import {Compartment, EditorState, Extension} from '@codemirror/state';
 import {EditorView, ViewUpdate} from '@codemirror/view';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {editorConfig} from './editorConfig';
 import {darkMode as darkModeTheme} from './editorThemes';
 import {autocompletion} from '@codemirror/autocomplete';
 import {useAppSelector} from '@cdo/apps/util/reduxHooks';
 import moduleStyles from './code-editor.module.scss';
+import {isReadOnlyWorkspace} from '@cdo/apps/lab2/lab2Redux';
 
 interface CodeEditorProps {
   onCodeChange: (code: string) => void;
@@ -27,6 +28,13 @@ const CodeEditor: React.FunctionComponent<CodeEditorProps> = ({
   const [didInit, setDidInit] = useState(false);
   const [editorView, setEditorView] = useState<EditorView | null>(null);
   const channelId = useAppSelector(state => state.lab.channel?.id);
+  const isReadOnly = useSelector(isReadOnlyWorkspace);
+
+  // These two compartments control read-only settings.
+  // Controls if you can type in the editor or not.
+  const editorReadOnlyCompartment = useMemo(() => new Compartment(), []);
+  // Controls if the dom is focusable or not (and therefore if a cursor is visible in the editor or not).
+  const editorEditableCompartment = useMemo(() => new Compartment(), []);
 
   useEffect(() => {
     if (editorRef.current === null || didInit) {
@@ -46,6 +54,13 @@ const CodeEditor: React.FunctionComponent<CodeEditorProps> = ({
       autocompletion(),
       ...editorConfigExtensions,
     ];
+
+    console.log(`Setting editor read only to ${isReadOnly}`);
+
+    editorExtensions.push(
+      editorReadOnlyCompartment.of(EditorState.readOnly.of(isReadOnly)),
+      editorEditableCompartment.of(EditorView.editable.of(!isReadOnly))
+    );
     if (darkMode) {
       editorExtensions.push(darkModeTheme);
     }
@@ -67,6 +82,9 @@ const CodeEditor: React.FunctionComponent<CodeEditorProps> = ({
     startCode,
     didInit,
     darkMode,
+    editorReadOnlyCompartment,
+    isReadOnly,
+    editorEditableCompartment,
   ]);
 
   // When we have a new channelId and/or start code, reset the editor with the start code.
@@ -82,6 +100,27 @@ const CodeEditor: React.FunctionComponent<CodeEditorProps> = ({
       });
     }
   }, [startCode, editorView, channelId]);
+
+  useEffect(() => {
+    if (editorView) {
+      console.log(`Changing editor read only to ${isReadOnly}`);
+      editorView.dispatch({
+        effects: [
+          editorReadOnlyCompartment.reconfigure(
+            EditorState.readOnly.of(isReadOnly)
+          ),
+          editorEditableCompartment.reconfigure(
+            EditorView.editable.of(!isReadOnly)
+          ),
+        ],
+      });
+    }
+  }, [
+    isReadOnly,
+    editorView,
+    editorReadOnlyCompartment,
+    editorEditableCompartment,
+  ]);
 
   return (
     <div
