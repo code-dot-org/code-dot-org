4cbe043baca37ce68790aa347c4e6c89ea670658
Molly Moen <molly@code.org>
2024-06-06 09:24:23 -0700

    undo adding locale

--- apps/src/lab2/locale.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A TypeScript wrapper for the Lab2 locale object which casts
- * it to the {@link Locale} type.
- */
-import {Locale} from '@cdo/apps/types/locale';
-
-export default require('@cdo/lab2/locale') as Locale<
-  typeof import('@cdo/i18n/lab2/en_us.json')
->;
