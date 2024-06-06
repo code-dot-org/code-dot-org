bba6ada32cdb150c9dd7d16f11e8645a858dd9ed
denyslevada <levada.denys@gmail.com>
2024-06-06 19:12:24 +0300

    * cleanup

--- apps/src/componentLibrary/tooltip/WithTooltip.tsx
+++ apps/src/componentLibrary/tooltip/WithTooltip.tsx
@@ -10,6 +10,15 @@ import {createPortal} from 'react-dom';
 
 import Tooltip, {TooltipOverlay, TooltipProps} from './Tooltip';
 
+// Define the tail offset and length values
+const tailOffset = 4;
+const tailLengths = {
+  l: 12,
+  m: 9,
+  s: 6,
+  xs: 6,
+};
+
 export interface WithTooltipProps {
   children: ReactNode;
   tooltipOverlayClassName?: string;
@@ -47,14 +56,6 @@ const WithTooltip: React.FunctionComponent<WithTooltipProps> = ({
     setNodePosition(null);
   };
 
-  // Define the tail offset and length values
-  const tailOffset = 4;
-  const tailLengths = {
-    l: 12,
-    m: 9,
-    s: 6,
-    xs: 6,
-  };
   const tailLength = tailLengths[tooltipProps.size || 'm'];
 
   const updateTooltipStyles = useCallback(() => {
@@ -65,6 +66,7 @@ const WithTooltip: React.FunctionComponent<WithTooltipProps> = ({
 
       let styles: React.CSSProperties = {};
 
+      // Calculate the tooltip position based on the direction and it's tail length
       switch (tooltipProps.direction) {
         case 'onRight':
           styles = {
@@ -148,7 +150,6 @@ const WithTooltip: React.FunctionComponent<WithTooltipProps> = ({
     showTooltip,
     nodePosition,
     tooltipProps.direction,
-    tailOffset,
     tailLength,
     updateTooltipStyles,
   ]);
@@ -157,12 +158,6 @@ const WithTooltip: React.FunctionComponent<WithTooltipProps> = ({
     visibility: showTooltip ? 'visible' : 'hidden',
     ...tooltipStyles,
   };
-
-  console.log('-- WithTooltip --');
-
-  console.log(tooltipStyleProps);
-  console.log(tooltipProps.direction);
-  console.log(tooltipStyles);
 
   // Check if children is a valid React element and clone it with ariaDescribedBy attribute
   // and additional event handlers to make sure the tooltip is displayed correctly
