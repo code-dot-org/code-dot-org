7108e199aa191a6ccd1dc27d00bcc7ee14530f6c
Alice Fisher <alice.fisher@code.org>
2024-06-07 03:18:01 +0000

    update unit test

--- dashboard/test/controllers/musiclab_controller_test.rb
+++ dashboard/test/controllers/musiclab_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class MusiclabControllerTest < ActionController::TestCase
-  test 'When DCDO flag is set to `true` channel ids from constant returns true ' do
-    DCDO.stubs(:get).with('get_channel_ids_from_constant', false).returns(true)
-    assert_equal true, @controller.send(:get_channel_ids_from_constant?)
+  test 'When DCDO flag is set to `false` get_channel_ids_from_featured_projects_gallery returns false' do
+    DCDO.stubs(:get).with('get_channel_ids_from_featured_projects_gallery', true).returns(false)
+    assert_equal false, @controller.send(:get_channel_ids_from_featured_projects_gallery?)
   end
 end
