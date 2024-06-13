1fa16a6cc84dd94d4827c1ff34a92a0dd6088021
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-06-13 22:32:59 +0300

    P20-987: Make logs less noisy

--- lib/cdo/rack/cookie_dcdo.rb
+++ lib/cdo/rack/cookie_dcdo.rb
@@ -14,27 +14,30 @@ module Rack
       # Stores the cookie DCDO data per request.
       RequestStore.store[:DCDO] = JSON.parse(Rack::Request.new(env).cookies['DCDO'] || '{}')
 
-      Rails.logger.info "\n" * 2
-      Rails.logger.info "=" * 80
-      Rails.logger.info "Request Cookies: #{Rack::Request.new(env).cookies.inspect}"
-      Rails.logger.info '-' * 80
-      Rails.logger.info "RequestStore ID: #{RequestStore.store.object_id}"
-      Rails.logger.info "RequestStore DCDO: #{RequestStore.store[:DCDO].inspect}"
-      Rails.logger.info "=" * 80
-      Rails.logger.info "\n" * 2
+      if Rack::Request.new(env).cookies['DCDO']
+        Rails.logger.info "\n"
+        Rails.logger.info "=" * 80
+        Rails.logger.info "Request Cookies: #{Rack::Request.new(env).cookies.inspect}"
+        Rails.logger.info '-' * 80
+        Rails.logger.info "request_store_dcdo[#{RequestStore.store.object_id}]: #{RequestStore.store[:DCDO].inspect}"
+        Rails.logger.info "=" * 80
+        Rails.logger.info "\n"
+      end
 
       unless DCDO.instance_variable_get(:@_redefined)
         # Redefines `DCDO#get` to return the cookie DCDO value if it exists.
         DCDO.define_singleton_method(:get) do |key, *args|
-          Rails.logger.info "\n" * 2
-          Rails.logger.info '*' * 80
-          Rails.logger.info "DCDO INSTANCE ID: #{object_id.inspect}"
-          Rails.logger.info "DCDO KEY: #{key.inspect}"
-          Rails.logger.info '-' * 80
-          Rails.logger.info "RequestStore ID: #{RequestStore.store.object_id}"
-          Rails.logger.info "RequestStore DCDO: #{RequestStore.store[:DCDO].inspect}"
-          Rails.logger.info '*' * 80
-          Rails.logger.info "\n" * 2
+          if key == 'cookie_dcdo_test'
+            Rails.logger.info "\n" * 2
+            Rails.logger.info '*' * 80
+            Rails.logger.info "DCDO INSTANCE ID: #{object_id.inspect}"
+            Rails.logger.info "DCDO KEY: #{key.inspect}"
+            Rails.logger.info '-' * 80
+            Rails.logger.info "Request key: request_store_dcdo[#{RequestStore.store.object_id}]"
+            Rails.logger.info "RequestStore DCDO: #{RequestStore.store[:DCDO].inspect}"
+            Rails.logger.info '*' * 80
+            Rails.logger.info "\n" * 2
+          end
 
           RequestStore.store[:DCDO]&.key?(key) ? RequestStore.store[:DCDO][key] : super(key, *args)
         end
