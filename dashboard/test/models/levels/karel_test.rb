c163b9dc7679e2cc868d949ba0eeb929b4c412fb
Mike Harvey <mike.harvey@code.org>
2024-06-24 11:09:42 -0400

    tests

---
+++ dashboard/test/models/levels/karel_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class KarelTest < ActiveSupport::TestCase
+  def setup
+    @karel = Karel.new
+  end
+
+  test 'uses_google_blockly? returns true when DCDO flag is true' do
+    DCDO.stubs(:get).with('maze_sw_google_blockly', true).returns(true)
+    assert @karel.uses_google_blockly?
+  end
+
+  test 'uses_google_blockly? returns false when DCDO flag is false' do
+    DCDO.stubs(:get).with('maze_sw_google_blockly', true).returns(false)
+    refute @karel.uses_google_blockly?
+  end
+end
