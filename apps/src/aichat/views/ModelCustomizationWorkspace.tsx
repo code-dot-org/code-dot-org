941632896cd64febf607b81c8ba1871d9030ba39
Alice Fisher <alice.fisher@code.org>
2024-06-26 19:08:50 +0000

    use dsco tabs

--- apps/src/aichat/views/ModelCustomizationWorkspace.tsx
+++ apps/src/aichat/views/ModelCustomizationWorkspace.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 
 import {AichatLevelProperties} from '@cdo/apps/aichat/types';
+import {FontAwesomeV6IconProps} from '@cdo/apps/componentLibrary/fontAwesomeV6Icon';
+import Tabs, {TabsProps} from '@cdo/apps/componentLibrary/tabs/Tabs';
 import {isReadOnlyWorkspace} from '@cdo/apps/lab2/lab2Redux';
 import {useAppSelector} from '@cdo/apps/util/reduxHooks';
 
@@ -9,11 +11,14 @@ import PublishNotes from './modelCustomization/PublishNotes';
 import RetrievalCustomization from './modelCustomization/RetrievalCustomization';
 import SetupCustomization from './modelCustomization/SetupCustomization';
 import {isVisible, isDisabled} from './modelCustomization/utils';
-import Tabs, {Tab} from './tabs/Tabs';
 
 import styles from './model-customization-workspace.module.scss';
 
 const ModelCustomizationWorkspace: React.FunctionComponent = () => {
+  const [setupTabName, setSetupTabName] = useState('Setup');
+  const [retrievalTabName, setRetrievalTabName] = useState('Retrieval');
+  const [publishTabName, setPublishTabName] = useState('Publish');
+  const [selectedTab, setSelectedTab] = useState('setup');
   const {
     temperature,
     systemPrompt,
@@ -39,31 +44,74 @@ const ModelCustomizationWorkspace: React.FunctionComponent = () => {
     isDisabled(systemPrompt) &&
     isDisabled(selectedModelId);
 
+  const iconValue: FontAwesomeV6IconProps = {
+    iconName: 'lock',
+    iconStyle: 'solid',
+  };
+  const setupCustomizationIcon =
+    isSetupCustomizationReadOnly || isReadOnly ? iconValue : undefined;
+  const retrievalIcon =
+    isDisabled(retrievalContexts) || isReadOnly ? iconValue : undefined;
+  const publishIcon =
+    isDisabled(modelCardInfo) || isReadOnly ? iconValue : undefined;
+
+  const visibleTabs = [];
+  if (showSetupCustomization) {
+    visibleTabs.push({
+      value: 'setup',
+      text: setupTabName,
+      tabContent: <SetupCustomization />,
+      iconLeft: setupCustomizationIcon,
+    });
+  }
+  if (isVisible(retrievalContexts)) {
+    visibleTabs.push({
+      value: 'retrieval',
+      text: retrievalTabName,
+      tabContent: <RetrievalCustomization />,
+      iconLeft: retrievalIcon,
+    });
+  }
+  if (isVisible(modelCardInfo) && !hidePresentationPanel) {
+    visibleTabs.push({
+      value: 'modelCardInfo',
+      text: publishTabName,
+      tabContent: <PublishNotes />,
+      iconLeft: publishIcon,
+    });
+  }
+
+  const handleOnChange = (value: string) => {
+    console.log('value', value);
+    setSelectedTab(value);
+  };
+  const tabArgs: TabsProps = {
+    name: 'modelCustomizationTabs',
+    tabs: visibleTabs,
+    defaultSelectedTabValue: visibleTabs[0].value,
+    onChange: handleOnChange,
+    type: 'secondary',
+    tabsContainerClassName: styles.tabsContainer,
+  };
+
+  useEffect(() => {
+    if (selectedTab === 'setup') {
+      setSetupTabName('Setup (view only)');
+      setRetrievalTabName('Retrieval');
+      setPublishTabName('Publish');
+    } else if (selectedTab === 'retrieval') {
+      setRetrievalTabName('Retrieval (view only)');
+      setSetupTabName('Setup');
+      setPublishTabName('Publish');
+    } else if (selectedTab === 'modelCardInfo') {
+      setPublishTabName('Publish (view only)');
+      setSetupTabName('Setup');
+      setRetrievalTabName('Retrieval');
+    }
+  }, [setSelectedTab, selectedTab]);
   return (
     <div className={styles.modelCustomizationWorkspace}>
-      <Tabs
-        tabs={
-          [
-            showSetupCustomization && {
-              title: 'Setup',
-              content: <SetupCustomization />,
-              isReadOnly: isSetupCustomizationReadOnly || isReadOnly,
-            },
-            isVisible(retrievalContexts) && {
-              title: 'Retrieval',
-              content: <RetrievalCustomization />,
-              isReadOnly: isDisabled(retrievalContexts) || isReadOnly,
-            },
-            isVisible(modelCardInfo) &&
-              !hidePresentationPanel && {
-                title: 'Publish',
-                content: <PublishNotes />,
-                isReadOnly: isDisabled(modelCardInfo) || isReadOnly,
-              },
-          ].filter(Boolean) as Tab[]
-        }
-        name="model-customization"
-      />
+      <Tabs {...tabArgs} />
     </div>
   );
 };
