2a13a685a7894f982b4d78ab4fea1e250acb1ba1
Carl Reiner <carl.reiner@code.org>
2024-06-26 22:02:23 -0700

    update tests to pass json

--- dashboard/test/controllers/lti/v1/lti_sections_controller_test.rb
+++ dashboard/test/controllers/lti/v1/lti_sections_controller_test.rb
@@ -14,7 +14,7 @@ class Lti::V1::SectionsControllerTest < ActionController::TestCase
     create :section_instructor, section: section_one, instructor: new_owner
     create :section_instructor, section: section_two, instructor: new_owner
 
-    patch :bulk_update_owners, params: {section_owners: {lti_section_one.id => new_owner.id, lti_section_two.id => new_owner.id}}, format: :json
+    patch :bulk_update_owners, params: {section_owners: {lti_section_one.id => new_owner.id, lti_section_two.id => new_owner.id}.to_json}, format: :json
     assert_response :ok
     assert_equal new_owner.id, section_one.reload.user_id
     assert_equal new_owner.id, section_two.reload.user_id
@@ -24,7 +24,7 @@ class Lti::V1::SectionsControllerTest < ActionController::TestCase
     other_teacher = create :teacher
     section = create :section, user: other_teacher
     lti_section = create :lti_section, section: section
-    patch :bulk_update_owners, params: {section_owners: {lti_section.id => @user.id}}, format: :json
+    patch :bulk_update_owners, params: {section_owners: {lti_section.id => @user.id}.to_json}, format: :json
     assert_response :forbidden
     assert_equal other_teacher.id, section.reload.user_id
   end
@@ -35,14 +35,14 @@ class Lti::V1::SectionsControllerTest < ActionController::TestCase
     lti_section = create :lti_section, section: section
     create :section_instructor, section: section, instructor: new_owner
 
-    patch :bulk_update_owners, params: {section_owners: {lti_section.id => new_owner.id}}, format: :json
+    patch :bulk_update_owners, params: {section_owners: {lti_section.id => new_owner.id}.to_json}, format: :json
     assert_response :ok
     refute section.reload.user_id == @user.id
     assert section.instructors.include? @user
   end
 
   test 'returns a 404 if a section is not found' do
-    patch :bulk_update_owners, params: {section_owners: {'foo' => @user.id}}, format: :json
+    patch :bulk_update_owners, params: {section_owners: {'foo' => @user.id}.to_json}, format: :json
     assert_response :not_found
   end
 end
