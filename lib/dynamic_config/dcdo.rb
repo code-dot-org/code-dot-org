e736539b03283250aee7a049ec27bd986bfed6fd
kobryan0619 <kaitie.obryan@gmail.com>
2024-06-28 09:57:36 -0500

    incorporate feedback

--- lib/dynamic_config/dcdo.rb
+++ lib/dynamic_config/dcdo.rb
@@ -56,7 +56,6 @@ class DCDOBase < DynamicConfigBase
       'student-email-post-enabled': DCDO.get('student-email-post-enabled', false),
       'progress-v2-metadata-enabled': DCDO.get('progress-v2-metadata-enabled', false),
       'show-updated-lms-content': DCDO.get('show-updated-lms-content', false),
-      'use-ghostscript-to-generate-pdfs': DCDO.get('use-ghostscript-to-generate-pdfs', false),
       'show-age-gated-students-banner': DCDO.get('show-age-gated-students-banner', true),
     }
   end
