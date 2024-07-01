0ec934baefaa2072aa5588dc639eeeefd9e915f4
Carl Reiner <carl.reiner@code.org>
2024-07-01 13:49:18 -0700

    switch to DateTime, fix tests

--- dashboard/app/controllers/sessions_controller.rb
+++ dashboard/app/controllers/sessions_controller.rb
@@ -74,7 +74,7 @@ class SessionsController < Devise::SessionsController
     end
 
     # Determine the deletion date as the lockout date of the account + 7 days
-    @delete_date = Date.parse(current_user.child_account_compliance_lock_out_date).since(7.days)
+    @delete_date = DateTime.parse(current_user.child_account_compliance_lock_out_date).since(7.days)
 
     # Find any existing permission request for this user
     # Students might have issued a few requests. We render the latest one.
