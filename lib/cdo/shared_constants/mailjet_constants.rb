eb8f4ad043987a739385bc48902acd322e688f4c
Bethany Connor <bethany@code.org>
2024-07-02 17:21:08 -0400

    Use translated email templates

--- lib/cdo/shared_constants/mailjet_constants.rb
+++ lib/cdo/shared_constants/mailjet_constants.rb
@@ -2,9 +2,18 @@ module MailJetConstants
   EMAILS = {
     welcome: {
       template_id: {
-        production: 5_831_384,
-        staging: 5_917_989,
-        development: 5_917_988
+        production: {
+          default: 5_831_384,
+          'es-MX': 5_973_069,
+          'ex-ES': 5_973_070
+        },
+        staging: {
+          default: 5_917_989
+        },
+        development: {
+          default: 5_917_988,
+          'es-MX': 6_104_709,
+        }
       },
       from_address: 'hadi_partovi@code.org',
       from_name: 'Hadi Partovi',
