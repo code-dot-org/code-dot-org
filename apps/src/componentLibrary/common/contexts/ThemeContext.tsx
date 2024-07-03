004cf868890e3b0b08f3632f06804f892885eb28
denyslevada <levada.denys@gmail.com>
2024-07-03 10:26:00 +0300

    * added ThemeContext.tsx

---
+++ apps/src/componentLibrary/common/contexts/ThemeContext.tsx
@@ -0,0 +1,33 @@
+import React, {createContext, useContext, useState, ReactNode} from 'react';
+
+type Theme = 'light' | 'dark';
+
+interface ThemeContextProps {
+  theme: Theme;
+  toggleTheme: () => void;
+  setTheme: (theme: Theme) => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
+
+export const ThemeProvider = ({children}: {children: ReactNode}) => {
+  const [theme, setTheme] = useState<Theme>('light');
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{theme, toggleTheme, setTheme}}>
+      <div className={`theme-${theme}`}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
