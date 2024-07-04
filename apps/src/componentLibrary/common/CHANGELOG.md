bf9015aef92664dc44c2b2182581435bfe0e9588
denyslevada <levada.denys@gmail.com>
2024-07-04 15:25:18 +0300

    * updated ThemeContext.tsx * added documentation

--- apps/src/componentLibrary/common/CHANGELOG.md
+++ apps/src/componentLibrary/common/CHANGELOG.md
@@ -2,15 +2,22 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.3.0]()
+
+* added ThemeContext.tsx to support themes change
+
 ## [0.2.1](https://github.com/code-dot-org/code-dot-org/pull/54064)
+
 * minor README.md updates
 
 ## [0.2.0]()
+
 * added ```styles/mixins.scss```
 * added README.md
 * added CHANGELOG.md
 
 ## [0.1.0](https://github.com/code-dot-org/code-dot-org/pull/52753)
+
 * added ```constants.ts```
 * added ```types.ts```
 * Initial commit
--- apps/src/componentLibrary/common/contexts/ThemeContext.tsx
+++ apps/src/componentLibrary/common/contexts/ThemeContext.tsx
@@ -1,15 +1,33 @@
 import React, {createContext, useContext, useState, ReactNode} from 'react';
 
-type Theme = 'light' | 'dark';
+type Theme = 'Light' | 'Dark';
 
 interface ThemeContextProps {
+  /** Theme value */
   theme: Theme;
+  /** Toggle between themes (light/dark)*/
   toggleTheme: () => void;
+  /** In case we'll have more than 2 themes - we'll be able to update theme value via this callback */
   setTheme: (theme: Theme) => void;
 }
 
+/**
+ * Handles multiple themes logic.
+ * Can ThemeProvider can be stacked, meaning that you can have different theme in existing themeContext by
+ * simply wrapping needed section into another ThemeProvider which will
+ * break this section out of higher level ThemeProvider. In other words - you can stack ThemeProviders multiple times,
+ * the theme value for the styles will be taken from the closest ThemeProvider or html element with data-theme
+ * attribute.
+ */
 const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 
+/** Provides code with values of ThemeContext inside of React component.
+ * Please note that in order to work correctly - it requires component you're working in to be wrapped by ThemeProvider
+ * anywhere on higher level
+ *
+ * Usage example:
+ *    const {theme, toggleTheme} = useTheme();
+ * */
 export const useTheme = () => {
   const context = useContext(ThemeContext);
   if (!context) {
@@ -18,16 +36,30 @@ export const useTheme = () => {
   return context;
 };
 
+/**
+ * Provides code with theme context. Can be used on the higher level (e.g. React app entry point)
+ * or on any level you need.
+ * Usage example:
+ *  <ThemeProvider>
+ *    <App {...args} />
+ *  </ThemeProvider>
+ *
+ *  AND/OR
+ *
+ *  <ThemeProvider>
+ *    <Alert {...args} />
+ *  </ThemeProvider>
+ * */
 export const ThemeProvider = ({children}: {children: ReactNode}) => {
-  const [theme, setTheme] = useState<Theme>('light');
+  const [theme, setTheme] = useState<Theme>('Light');
 
   const toggleTheme = () => {
-    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme(prevTheme => (prevTheme === 'Light' ? 'Dark' : 'Light'));
   };
 
   return (
     <ThemeContext.Provider value={{theme, toggleTheme, setTheme}}>
-      <div className={`theme-${theme}`}>{children}</div>
+      <div data-theme={theme}>{children}</div>
     </ThemeContext.Provider>
   );
 };
