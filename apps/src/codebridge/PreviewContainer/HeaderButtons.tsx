ba2a1e2aa87a25723fb73b68220e9bc0dbea9062
Jim Thomason <jim@japanesewartuba.com>
2024-07-07 20:22:25 -0500

    jimt/codebridge-layout-buttons

---
+++ apps/src/codebridge/PreviewContainer/HeaderButtons.tsx
@@ -0,0 +1,12 @@
+import SwapLayoutButton from '@codebridge/SwapLayoutButton';
+import React from 'react';
+
+const PreviewContainerHeaderButtons: React.FunctionComponent = () => {
+  return (
+    <div>
+      <SwapLayoutButton />
+    </div>
+  );
+};
+
+export default PreviewContainerHeaderButtons;
