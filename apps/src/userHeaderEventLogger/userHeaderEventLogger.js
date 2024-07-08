4128eb393517b3c12205862830c35d776705b4a8
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2024-07-08 13:06:44 -0700

    add function to work on both header and hamburger links

--- apps/src/userHeaderEventLogger/userHeaderEventLogger.js
+++ apps/src/userHeaderEventLogger/userHeaderEventLogger.js
@@ -4,6 +4,22 @@ import {EVENTS, PLATFORMS} from '@cdo/apps/lib/util/AnalyticsConstants';
 import analyticsReporter from '@cdo/apps/lib/util/AnalyticsReporter';
 import getScriptData from '@cdo/apps/util/getScriptData';
 
+// Adds an event to each header or hamburger link
+function addClickEventToLinks(selector, eventName) {
+  const links = document.querySelectorAll(`.${selector}`);
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      analyticsReporter.sendEvent(
+        eventName,
+        {
+          [selector]: link.innerText,
+        },
+        PLATFORMS.STATSIG
+      );
+    });
+  });
+}
+
 $(document).ready(function () {
   const headerCreateMenu = document.getElementById('header_create_menu');
   const pageUrl = document.querySelector('meta[property="og:url"]')?.content;
@@ -16,18 +32,15 @@ $(document).ready(function () {
     );
 
     // Adds event to each header link when clicked
-    const headerLinks = document.querySelectorAll('.headerlink');
-    headerLinks.forEach(headerLink => {
-      headerLink.addEventListener('click', () => {
-        analyticsReporter.sendEvent(
-          EVENTS.SIGNED_OUT_USER_CLICKS_HEADER_LINK,
-          {
-            headerLink: headerLink.innerText,
-          },
-          PLATFORMS.STATSIG
-        );
-      });
-    });
+    addClickEventToLinks(
+      'headerlink',
+      EVENTS.SIGNED_OUT_USER_CLICKS_HEADER_LINK
+    );
+    // Adds event to each hamburger link when clicked
+    addClickEventToLinks(
+      'hamburgerlink',
+      EVENTS.SIGNED_OUT_USER_CLICKS_HAMBURGER_LINK
+    );
   }
 
   if (getScriptData('isSignedOut') && headerCreateMenu) {
