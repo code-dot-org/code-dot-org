5f3fa867876bb0b24b56c47e7710b9bc50faba88
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-07-09 16:54:33 -0700

    Add `unlock_token` to Users
    In preparation for eventually adding the ability to lock out user
    accounts on repeated failed authentication attempts, we first add a
    column with an index on the Users table which will allow users to unlock
    their accounts by clicking a link in an email we'll send them.
    
    Because we are on MySQL 8, [adding a column is now an instant
    operation](https://dev.mysql.com/doc/refman/8.0/en/innodb-online-ddl-operations.html#online-ddl-column-operations-table),
    even on a table as large as our Users table. Unfortunately, [adding an
    index is
    not](https://dev.mysql.com/doc/refman/8.0/en/innodb-online-ddl-operations.html#online-ddl-index-operations-table).
    
    I did some manual testing on a clone of the production database using an
    adhoc, and was able to confirm that adding the new column takes less
    than a second and adding the index for that column takes about 35
    minutes. During that time, the table is still available for reads and
    writes; only schema changes are locked. This means we can safely apply
    the change in production without needing to use `gh-ost`, but we will
    need to execute the query to add the index outside of the context of our
    Rails migrations, which have a 30-second limit.

---
+++ dashboard/db/migrate/20240321204728_add_devise_lockable_to_users.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Adds all fields required for enabling Devise's Lockable functionality to the
+# User model, in preparation for enabling the feature.
+#
+# See https://github.com/heartcombo/devise/wiki/How-To:-Add-:lockable-to-Users
+class AddDeviseLockableToUsers < ActiveRecord::Migration[6.1]
+  def self.up
+    # Note that we do not add `failed_attempts` or `locked_at` here.
+    # Because neither field is used for querying, neither needs to be an actual
+    # column; we therefore implement both in the properties blob.
+    add_column :users, :unlock_token, :string
+
+    # On MySQL 8.0, adding an index does not lock the table but it does still
+    # take quite a while to apply successfully; see
+    # https://dev.mysql.com/doc/refman/8.0/en/innodb-online-ddl-operations.html#online-ddl-column-syntax-notes
+    #
+    # When tested against a clone of our production database, this operation
+    # took approximately 35 minutes, much longer than the 30 seconds migrations
+    # will wait before erroring out. To avoid this migration failing in
+    # production, we plan to execute the index creation query manually after
+    # the rest of this migration has completed.
+    #
+    # Specifically, we should from a bash shell on production-console run:
+    #     nohup mysql-client-dashboard-writer "CREATE UNIQUE INDEX index_users_on_unlock_token ON users (unlock_token)" &
+    unless Rails.env.production?
+      add_index :users, :unlock_token, unique: true
+    end
+  end
+
+  def self.down
+    remove_column :users, :unlock_token
+    remove_index :users, :unlock_token
+  end
+end
