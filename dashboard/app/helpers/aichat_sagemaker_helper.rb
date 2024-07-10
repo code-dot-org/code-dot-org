804fc2e691af9bc9d5147d96e0a19e4b7eecea30
Alice Fisher <alice.fisher@code.org>
2024-07-10 14:23:07 +0000

    add more comments and add parentheses

--- dashboard/app/helpers/aichat_sagemaker_helper.rb
+++ dashboard/app/helpers/aichat_sagemaker_helper.rb
@@ -2,11 +2,11 @@ module AichatSagemakerHelper
   ASSISTANT = "assistant"
   USER = "user"
   SYSTEM = "system"
+  KAREN_PRETEXT = "Edit the following text for spelling and grammar mistakes: "
   INSTRUCTIONS_BEGIN_TOKEN = "[INST]"
   INSTRUCTIONS_END_TOKEN = "[/INST]"
   SENTENCE_BEGIN_TOKEN = "<s>"
   SENTENCE_END_TOKEN = "</s>"
-  KAREN_PRETEXT = "Edit the following text for spelling and grammar mistakes: "
   CHAT_ML_BEGIN_TOKEN = "<|im_start|>"
   CHAT_ML_END_TOKEN = "<|im_end|>"
   NEWLINE = "\n"
@@ -34,19 +34,20 @@ module AichatSagemakerHelper
       # Format input for Arithmo model as detailed at https://huggingface.co/akjindal53244/Arithmo-Mistral-7B.
       # Note the Question-Answer format - prior user and assistant messages are NOT included.
       inputs = "Question: " + inputs + " " + new_message[:chatMessageText]
-      inputs << + NEWLINE + "Answer:"
+      inputs << (NEWLINE + "Answer:")
     elsif selected_model_id == FINE_TUNED_MODELS[:KAREN]
-      # Format input for Karen model using ChatML. Details at https://huggingface.co/FPHam/Karen_TheEditor_V2_CREATIVE_Mistral_7B.
+      # Format input for Karen model using ChatML as detailed at https://huggingface.co/FPHam/Karen_TheEditor_V2_CREATIVE_Mistral_7B.
       # Note that prior user and assistant messages are NOT included and the customized
       # pretext is always used so that the given user message is edited and not responded to.
       inputs = CHAT_ML_BEGIN_TOKEN + SYSTEM + NEWLINE + inputs + CHAT_ML_BEGIN_TOKEN + NEWLINE
-      inputs << CHAT_ML_BEGIN_TOKEN + USER + NEWLINE + KAREN_PRETEXT + new_message[:chatMessageText] + CHAT_ML_END_TOKEN + NEWLINE
-      inputs << CHAT_ML_BEGIN_TOKEN + ASSISTANT
+      inputs << (CHAT_ML_BEGIN_TOKEN + USER + NEWLINE + KAREN_PRETEXT + new_message[:chatMessageText] + CHAT_ML_END_TOKEN + NEWLINE)
+      inputs << (CHAT_ML_BEGIN_TOKEN + ASSISTANT)
     else
       # The instruction-tuned version of Mistral accepts formatted instructions where conversation roles
       # must start with a user prompt and alternate between user and assistant.
       # Mistral-7B-Instruction LLM instruction format doc at https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.1.
-      inputs << SENTENCE_BEGIN_TOKEN + wrap_as_instructions(inputs)
+      # Biomistral and Pirate models also use this input format.
+      inputs << (SENTENCE_BEGIN_TOKEN + wrap_as_instructions(inputs))
       # Add user and assistant messages including most recent user message.
       all_messages = [*stored_messages, new_message]
       all_messages.each do |msg|
@@ -104,12 +105,14 @@ module AichatSagemakerHelper
       end
     last = parts.last
     if selected_model_id == FINE_TUNED_MODELS[:PIRATE]
-      # These characters is used to separate the assistant's response from the rest of the generated text
-      # which sometimes includes jargon, extraneous characters or code snippets.   
+      # These characters is used to separate the assistant response received from the Pirate model
+      # from the rest of the generated text which sometimes includes jargon, extraneous characters
+      # or code snippets.
       last = last.split(/[}~*`]/).first
-      # Remove double quotes in assistant's response.
+      # Remove double quotes in assistant response.
       last = last.delete("\"")
     elsif selected_model_id == FINE_TUNED_MODELS[:KAREN]
+      # Remove extraneous characters at end of assistant response from Karen model.
       last = last.split("}").first
     end
     last
