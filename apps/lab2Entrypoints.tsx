bb5e1f47917562cd10cc78d097d436a81f9a813c
Seth Nickell <snickell@gmail.com>
2024-07-10 08:53:20 -1000

    Redirect from to webpackEntrypoints.js

--- apps/lab2Entrypoints.tsx
+++ apps/lab2Entrypoints.tsx
@@ -5,6 +5,8 @@
 // - Here's an example PR: https://github.com/code-dot-org/code-dot-org/pull/55952/files
 // - Read the Lab2 Architecture doc: https://docs.google.com/document/d/1a8fPxd4mvV7LdhxGOCuCzTs4cmNt0o18CwpcH2KeGBU/edit#heading=h.td4mrmujd2i9
 // - Please use lazy-loading
+//
+// Note: old-style "lab1" labs use a different system, see webpackEntryPoints.js
 
 import React, {ComponentType, LazyExoticComponent, lazy} from 'react';
 
