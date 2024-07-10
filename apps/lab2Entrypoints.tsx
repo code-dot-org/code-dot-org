e442eb6a2806ba7b09d93c13737817c8ff86f153
Seth Nickell <snickell@gmail.com>
2024-07-10 07:37:16 -1000

    Short docs about defining new labs

---
+++ apps/lab2Entrypoints.tsx
@@ -0,0 +1,7 @@
+// Define the set of entrypoints for Lab2-based labs
+//
+// Want to add a new lab?
+// - This is one of the main files you'll have to edit
+// - Here's an example PR: https://github.com/code-dot-org/code-dot-org/pull/55952/files
+// - Read the Lab2 Architecture doc: https://docs.google.com/document/d/1a8fPxd4mvV7LdhxGOCuCzTs4cmNt0o18CwpcH2KeGBU/edit#heading=h.td4mrmujd2i9
+// - Please use lazy-loading
