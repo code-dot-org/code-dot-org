b64c07f48aff6cefca49738d4abc8f7f328046a5
Erin Bond <erin.bond@code.org>
2024-07-10 14:16:17 -0400

    delete rolled back migration

--- dashboard/db/migrate/20240530170255_add_flagged_inappropriate_to_ai_tutor_interaction_feedbacks.rb
+++
@@ -1,6 +0,0 @@
-class AddFlaggedInappropriateToAiTutorInteractionFeedbacks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :ai_tutor_interaction_feedbacks, :inappropriate, :boolean
-    add_index :ai_tutor_interaction_feedbacks, :inappropriate
-  end
-end
