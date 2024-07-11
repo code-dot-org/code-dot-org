c227fc3820ca06da0bd1cd6b61f71f111d2c8515
Liam Frye-Mason <liam.fryemason@code.org>
2024-07-11 14:02:07 -0700

    Better comment

--- apps/src/dance/ai/DanceAiModal.tsx
+++ apps/src/dance/ai/DanceAiModal.tsx
@@ -21,7 +21,9 @@ import aiBotHeadYes from '@cdo/static/dance/ai/bot/ai-bot-head-yes.png';
 import danceMetricsReporter from '../danceMetricsReporter';
 import {closeAiModal, DanceState} from '../danceRedux';
 
-// Require statement kept above import order to preserve functionality.
+// Disabling import order in order to add require statements first
+// Require statements can change behavior based on the order they are called.
+// This might be safe to remove but needs investigation whether any behavior is changed by order.
 /* eslint-disable import/order */
 const ToggleGroup = require('@cdo/apps/templates/ToggleGroup').default;
 
