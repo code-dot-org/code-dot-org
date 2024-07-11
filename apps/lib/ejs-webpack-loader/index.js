5025a8dadad76b2d272ee509cc18b3ee2a6da822
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-07-11 09:09:41 -0700

    chore: add MIT license header and code.org comment on loader

--- apps/lib/ejs-webpack-loader/index.js
+++ apps/lib/ejs-webpack-loader/index.js
@@ -1,3 +1,38 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Vasily Ostanin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * This is a fork of https://github.com/rorkflash/ejs-webpack-loader
+ *
+ * This fork can be removed when Code.org removes the usage of EJS in the code base.
+ *
+ * Code.org changes:
+ * 1. Remove dependency on HTML minifier (not used by Code.org)
+ * 2. Update to be compatible with webpack v5
+ * 3. Update loader-utils to 2.x
+ */
+
 var ejs = require('ejs'),
   utils = require('loader-utils'),
   merge = require('merge'),
