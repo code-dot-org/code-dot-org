992c49cd3f33f5039ec54574f35188c5bbc3e4af
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-07-11 16:55:20 -0700

    reorder the presentation of information in a comment

--- dashboard/db/migrate/20240321204728_add_devise_lockable_to_users.rb
+++ dashboard/db/migrate/20240321204728_add_devise_lockable_to_users.rb
@@ -11,17 +11,20 @@ class AddDeviseLockableToUsers < ActiveRecord::Migration[6.1]
     # column; we therefore implement both in the properties blob.
     add_column :users, :unlock_token, :string
 
-    # On MySQL 8.0, adding an index does not lock the table but it does still
-    # take quite a while to apply successfully; see
-    # https://dev.mysql.com/doc/refman/8.0/en/innodb-online-ddl-operations.html#online-ddl-column-syntax-notes
+    # Attempting to create this index via a migration will fail on a table as
+    # large as the one we have in production. To avoid that, we plan to execute
+    # the index creation query manually after the rest of this migration has
+    # completed.
     #
+    # Specifically: on MySQL 8.0, adding an index does not lock the table but
+    # it does still take quite a while to apply successfully (see
+    # https://dev.mysql.com/doc/refman/8.0/en/innodb-online-ddl-operations.html#online-ddl-column-syntax-notes).
     # When tested against a clone of our production database, this operation
     # took approximately 35 minutes, much longer than the 30 seconds migrations
-    # will wait before erroring out. To avoid this migration failing in
-    # production, we plan to execute the index creation query manually after
-    # the rest of this migration has completed.
+    # will wait before erroring out. To create the index without the time limit
+    # enforced by migrations, we should run this command from a bash shell on
+    # production-console:
     #
-    # Specifically, we should from a bash shell on production-console run:
     #     nohup mysql-client-dashboard-writer "CREATE UNIQUE INDEX index_users_on_unlock_token ON users (unlock_token)" &
     unless Rails.env.production?
       add_index :users, :unlock_token, unique: true
