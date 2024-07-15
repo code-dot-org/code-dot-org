7dd1460d1e18286419da5aa174236c7b1197d52e
Liam Frye-Mason <liam.fryemason@code.org>
2024-07-15 13:59:34 -0700

    Add do-nothing button for hide pin menu

--- apps/src/templates/levelSummary/FreeResponseResponses.jsx
+++ apps/src/templates/levelSummary/FreeResponseResponses.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Button, {buttonColors} from '@cdo/apps/componentLibrary/button';
+
 import styles from './summary.module.scss';
 
 const FreeResponseResponses = ({responses}) => (
@@ -8,6 +10,15 @@ const FreeResponseResponses = ({responses}) => (
     {responses.map(response => (
       <div key={response.user_id} className={styles.studentAnswer}>
         <p>{response.text}</p>
+        <Button
+          onClick={() => {}}
+          isIconOnly={true}
+          icon={{iconName: 'ellipsis-vertical'}}
+          color={buttonColors.black}
+          size="xs"
+          type="tertiary"
+          className={styles.studentAnswerMenuButton}
+        />
       </div>
     ))}
   </div>
