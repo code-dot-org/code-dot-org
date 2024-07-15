4aa4498d5912f973c5cbdeb1f87ff4d2669bdb21
Liam Frye-Mason <liam.fryemason@code.org>
2024-07-15 10:32:20 -0700

    Added better error message for save timeout (#59746)
    * Added better error message for save timeout
    
    * Fix test

--- apps/src/lib/levelbuilder/unit-editor/UnitEditor.jsx
+++ apps/src/lib/levelbuilder/unit-editor/UnitEditor.jsx
@@ -387,6 +387,14 @@ class UnitEditor extends React.Component {
         }
       })
       .fail(error => {
+        if (error.status === 504) {
+          this.setState({
+            isSaving: false,
+            error:
+              'The save request timed out. Please refresh the page and verify your changes have been saved correctly.',
+          });
+          return;
+        }
         this.setState({isSaving: false, error: error.responseText});
       });
   };
