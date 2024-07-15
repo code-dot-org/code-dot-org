cd56b4d38fc683dfb5cb47fdf89c6f4f9e5f649c
kobryan0619 <kaitie.obryan@gmail.com>
2024-07-15 13:37:59 -0500

    starting to locate where to add names - need to add to scriptData

--- apps/src/templates/levelSummary/FreeResponseResponses.jsx
+++ apps/src/templates/levelSummary/FreeResponseResponses.jsx
@@ -6,8 +6,13 @@ import styles from './summary.module.scss';
 const FreeResponseResponses = ({responses}) => (
   <div className={styles.studentResponsesColumns}>
     {responses.map(response => (
-      <div key={response.user_id} className={styles.studentAnswer}>
-        <p>{response.text}</p>
+      <div key={response.user_id}>
+        <div className={styles.studentAnswer}>
+          <p>{response.text}</p>
+        </div>
+        <div className={styles.studentAnswer}>
+          <p>{response.user_id}</p>
+        </div>
       </div>
     ))}
   </div>
