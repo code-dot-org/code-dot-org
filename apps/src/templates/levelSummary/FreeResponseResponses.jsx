6b2ee727577fe1bff459f80987ff5ba6a140ecb6
Liam Frye-Mason <liam.fryemason@code.org>
2024-07-16 15:06:54 -0700

    More style

--- apps/src/templates/levelSummary/FreeResponseResponses.jsx
+++ apps/src/templates/levelSummary/FreeResponseResponses.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import Button from '@cdo/apps/componentLibrary/button';
+import Alert from '@cdo/apps/componentLibrary/alert/Alert';
 import i18n from '@cdo/locale';
 
 import ResponseMenuDropdown from './ResponseMenuDropdown';
@@ -12,27 +12,40 @@ const FreeResponseResponses = ({responses}) => {
   const [hiddenResponses, setHiddenResponses] = React.useState([]);
 
   return (
-    <div className={styles.studentResponsesColumns}>
-      {responses
-        .filter(response => !hiddenResponses.includes(response.user_id))
-        .map(response => (
-          <div key={response.user_id} className={styles.studentAnswer}>
-            <p>{response.text}</p>
-            <ResponseMenuDropdown
-              response={response}
-              hideResponse={response =>
-                setHiddenResponses(prevHidden => [
-                  ...prevHidden,
-                  response.user_id,
-                ])
-              }
-            />
-          </div>
-        ))}
-      <Button
-        onClick={() => setHiddenResponses([])}
-        text={i18n.showAllResponses()}
-      />
+    <div className={styles.studentResponsesContent}>
+      <div className={styles.studentResponsesColumns}>
+        {responses
+          .filter(response => !hiddenResponses.includes(response.user_id))
+          .map(response => (
+            <div key={response.user_id} className={styles.studentAnswer}>
+              <p>{response.text}</p>
+              <ResponseMenuDropdown
+                response={response}
+                hideResponse={response =>
+                  setHiddenResponses(prevHidden => [
+                    ...prevHidden,
+                    response.user_id,
+                  ])
+                }
+              />
+            </div>
+          ))}
+      </div>
+      {hiddenResponses.length > 0 && (
+        <Alert
+          icon={{iconName: 'eye-slash'}}
+          onClick={() => setHiddenResponses([])}
+          link={{
+            text: i18n.showAllResponses(),
+            onClick: () => setHiddenResponses([]),
+            href: null,
+          }}
+          text={i18n.hiddenResponses({
+            numHiddenResponses: hiddenResponses.length,
+          })}
+          className={styles.hiddenResponsesAlert}
+        />
+      )}
     </div>
   );
 };
