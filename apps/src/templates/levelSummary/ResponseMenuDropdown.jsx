b88c872a63515c8828c40b5e8146ed0ef69b06e1
Liam Frye-Mason <liam.fryemason@code.org>
2024-07-16 08:43:41 -0700

    add dcdo

--- apps/src/templates/levelSummary/ResponseMenuDropdown.jsx
+++ apps/src/templates/levelSummary/ResponseMenuDropdown.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 
 import Button, {buttonColors} from '@cdo/apps/componentLibrary/button';
 import FontAwesomeV6Icon from '@cdo/apps/componentLibrary/fontAwesomeV6Icon/FontAwesomeV6Icon';
+import DCDO from '@cdo/apps/dcdo';
 
 import styles from './summary.module.scss';
 
 const ResponseMenuDropdown = ({response}) => {
   const [isOpen, setIsOpen] = React.useState(false);
+
+  if (!DCDO.get('cfu-pin-hide-enabled', false)) {
+    return null;
+  }
 
   return (
     <div className={styles.studentAnswerMenuDropdownContainer}>
@@ -24,13 +29,21 @@ const ResponseMenuDropdown = ({response}) => {
         <div className={styles.studentAnswerMenuDropdown}>
           <ul>
             <li>
-              <button type="button" className={styles.dropdownOption}>
+              <button
+                type="button"
+                className={styles.dropdownOption}
+                onClick={() => setIsOpen(false)}
+              >
                 <FontAwesomeV6Icon iconName="thumbtack" />
                 Pin response
               </button>
             </li>
             <li>
-              <button className={styles.dropdownOption} type="button">
+              <button
+                className={styles.dropdownOption}
+                type="button"
+                onClick={() => setIsOpen(false)}
+              >
                 <FontAwesomeV6Icon iconName="eye-slash" />
                 Hide response
               </button>
