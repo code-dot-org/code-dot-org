42d0155bdb1e9de2c4d4aba2c7715c7eabd170a9
Liam Frye-Mason <liam.fryemason@code.org>
2024-07-16 14:04:06 -0700

    Basic hide functionality

--- apps/src/templates/levelSummary/FreeResponseResponses.jsx
+++ apps/src/templates/levelSummary/FreeResponseResponses.jsx
@@ -1,19 +1,38 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Button from '@cdo/apps/componentLibrary/button';
+import i18n from '@cdo/locale';
+
 import ResponseMenuDropdown from './ResponseMenuDropdown';
 
 import styles from './summary.module.scss';
 
 const FreeResponseResponses = ({responses}) => {
+  const [hiddenResponses, setHiddenResponses] = React.useState([]);
+
   return (
     <div className={styles.studentResponsesColumns}>
-      {responses.map(response => (
-        <div key={response.user_id} className={styles.studentAnswer}>
-          <p>{response.text}</p>
-          <ResponseMenuDropdown response={response} />
-        </div>
-      ))}
+      {responses
+        .filter(response => !hiddenResponses.includes(response.user_id))
+        .map(response => (
+          <div key={response.user_id} className={styles.studentAnswer}>
+            <p>{response.text}</p>
+            <ResponseMenuDropdown
+              response={response}
+              hideResponse={response =>
+                setHiddenResponses(prevHidden => [
+                  ...prevHidden,
+                  response.user_id,
+                ])
+              }
+            />
+          </div>
+        ))}
+      <Button
+        onClick={() => setHiddenResponses([])}
+        text={i18n.showAllResponses()}
+      />
     </div>
   );
 };
