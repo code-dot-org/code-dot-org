b81b6b98f1ecc01972bf2ae43589b06c40e6183c
Seth Nickell <snickell@gmail.com>
2024-07-15 21:24:26 -1000

    Be careful with case-sensitivity (mac vs linux)

--- apps/lab2EntryPoints.ts
+++ apps/lab2EntryPoints.ts
@@ -1,10 +1,6 @@
-// Define the set of entrypoints for Lab2-based labs
+// Registration of code entry-point for all Lab2-based labs
 //
-// Want to add a new lab?
-// - This is one of the main files you'll have to edit
-// - Here's an example PR: https://github.com/code-dot-org/code-dot-org/pull/55952/files
-// - Read the Lab2 Architecture doc: https://docs.google.com/document/d/1a8fPxd4mvV7LdhxGOCuCzTs4cmNt0o18CwpcH2KeGBU/edit#heading=h.td4mrmujd2i9
-// - Please use lazy-loading
+// Want to add a new lab? See [lab2/README.md](./lab2/README.md)
 //
 // Note: old-style "lab1" labs use a different system, see webpackEntryPoints.js
 
