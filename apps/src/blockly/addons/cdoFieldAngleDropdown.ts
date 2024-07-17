c133b33b6ed0c8323dff62efbabc070b0e5f4722
Mike Harvey <mike.harvey@code.org>
2024-07-17 11:48:37 -0400

    refactor

--- apps/src/blockly/addons/cdoFieldAngleDropdown.ts
+++ apps/src/blockly/addons/cdoFieldAngleDropdown.ts
@@ -1,3 +1,5 @@
+import {Block} from 'blockly';
+
 import {ExtendedBlockSvg} from '../types';
 
 import CdoAngleHelper from './cdoAngleHelper';
@@ -13,15 +15,16 @@ export default class CdoFieldAngleDropdown extends CdoFieldDropdown {
   angleHelper?: CdoAngleHelper | null;
   direction: string | undefined;
   directionFieldName: string | undefined;
+
   /**
    * Class for an editable field with an angle picker.
-   * @param {Object} opt_options Legacy options, supported by CDO Blockly
-   * @param {string} opt_options.direction a hardcoded direction setting
-   * @param {string} opt_options.directionTitleName the name of the field from which
+   * @param {AngleDropdownOptions} [opt_options] Legacy options, supported by CDO Blockly
+   * @param {string} [opt_options.direction] a hardcoded direction setting
+   * @param {string} [opt_options.directionTitleName] the name of the field from which
    *     to obtain direction information
-   * @param {CustomMenuGenerator} opt_options.menuGenerator An array of valid
+   * @param {CustomMenuGenerator} [opt_options.menuGenerator] An array of valid
    *     value options.
-   * @extends {Blockly.FieldAngleDropdown}
+   * @extends {CdoFieldDropdown}
    * @constructor
    */
   constructor(opt_options?: AngleDropdownOptions) {
@@ -34,66 +37,107 @@ export default class CdoFieldAngleDropdown extends CdoFieldDropdown {
   /**
    * Override to allow for clockwise orientation based on a hard-coded direction, or
    * a separate direction field on the block.
-   * @param {MouseEvent} e
+   * @param {MouseEvent} [e]
    * @override
    */
   showEditor_(e?: MouseEvent) {
-    let direction = this.direction;
-    if (!direction && this.directionFieldName) {
-      const sourceBlock = this.getSourceBlock() as ExtendedBlockSvg;
-      direction = sourceBlock.getFieldValue(this.directionFieldName);
-    }
-    if (!direction) {
-      direction = 'turnRight';
-    }
+    const direction = this.getDirection();
     super.showEditor_(e);
+
+    // See cdoCss for custom styling of the dropdown.
     Blockly.utils.dom.addClass(
       Blockly.DropDownDiv.getContentDiv(),
       'fieldAngleDropDownContainer'
     );
 
+    const angleHelperContainer = this.createAngleHelperContainer();
+    this.initializeAngleHelper(direction, angleHelperContainer);
+
+    this.animateAngleHelperOnMenuOptionHighlight();
+  }
+
+  /**
+   * Get the direction from either the hardcoded setting or the direction field.
+   * @returns {string} The direction value.
+   * @private
+   */
+  private getDirection(): string {
+    let direction = this.direction;
+    if (!direction && this.directionFieldName) {
+      const sourceBlock = this.getSourceBlock() as ExtendedBlockSvg;
+      direction = sourceBlock.getFieldValue(this.directionFieldName);
+    }
+    return direction || 'turnRight';
+  }
+
+  /**
+   * Create the container element for the angle helper.
+   * @returns {HTMLDivElement} The angle helper container element.
+   * @private
+   */
+  private createAngleHelperContainer(): HTMLDivElement {
     const angleHelperContainer = document.createElement('div');
     Blockly.utils.dom.addClass(
       angleHelperContainer,
       'blocklyAngleHelperContainer'
     );
-    const sourceBlock = this.getSourceBlock();
+    return angleHelperContainer;
+  }
+
+  /**
+   * Initialize the angle helper with the provided direction and container.
+   * @param {string} direction The direction for the angle helper.
+   * @param {HTMLDivElement} container The container element for the angle helper.
+   * @private
+   */
+  private initializeAngleHelper(direction: string, container: HTMLDivElement) {
+    const sourceBlock = this.getSourceBlock() as Block;
     this.angleHelper = new Blockly.AngleHelper(direction, {
-      onUpdate: () => {
-        const angleValue = this.angleHelper?.getAngle();
-        this.setValue(`${angleValue}`);
-        // Blockly has no public methods for getting/highlighting/selecting
-        // dropdown menu items, so this is a necessary hack.
-        const menuItems =
-          Blockly.DropDownDiv.getContentDiv().querySelectorAll(
-            '.blocklyMenuItem'
-          );
-        // Mirror the selected angle  value to the dropdown menu options.
-        menuItems.forEach((menuItem, index) => {
-          // Deselect the current menu item.
-          Blockly.utils.dom.removeClass(menuItem, 'blocklyMenuItemHighlight');
-          Blockly.utils.dom.removeClass(menuItem, 'blocklyMenuItemSelected');
-          // Set any matching option to selected.
-          const menuOptionValue = this.getOptions()[index][1];
-          if (menuOptionValue === `${angleValue}`) {
-            Blockly.utils.dom.addClass(menuItem, 'blocklyMenuItemSelected');
-          }
-        });
-      },
-      snapPoints: this.getOptions().map(function (option) {
-        return parseInt(option[1]);
-      }),
+      onUpdate: this.updateDropdownMenuOptions.bind(this),
+      snapPoints: this.getOptions().map(option => parseInt(option[1])),
       arcColour: (sourceBlock as ExtendedBlockSvg)?.style.colourPrimary,
-      // height: angleHelperHeight,
-      // width: angleHelperWidth,
       angle: parseInt(`${this.getValue()}`),
       enableBackgroundRotation: true,
     });
-    Blockly.DropDownDiv.getContentDiv().appendChild(angleHelperContainer);
-    this.angleHelper.init(angleHelperContainer);
+    Blockly.DropDownDiv.getContentDiv().appendChild(container);
+    this.angleHelper.init(container);
+  }
 
-    // In order to preview dropdown menu option, we detect changes to the highlighted
-    // menu option and animate the angle helper based on the highlighted option's value.
+  /**
+   * Update the dropdown menu options to reflect the selected angle value.
+   * @param {string | number} angleValue The selected angle value.
+   * @private
+   */
+  private updateDropdownMenuOptions() {
+    const angleValue = this.angleHelper?.getAngle() as number;
+    this.setValue(`${angleValue}`);
+    // Blockly has no public methods for getting dropdown menu items, so this
+    // is a necessary hack.
+    const menuItems =
+      Blockly.DropDownDiv.getContentDiv().querySelectorAll('.blocklyMenuItem');
+    // Mirror the selected angle value to the dropdown menu options.
+    menuItems.forEach((menuItem, index) => {
+      // Block doesn't directly support selecting a new menu option while keeping
+      // the dropdown open, so we modify the classes directly.
+      Blockly.utils.dom.removeClass(menuItem, 'blocklyMenuItemHighlight');
+      Blockly.utils.dom.removeClass(menuItem, 'blocklyMenuItemSelected');
+      // Set any matching option(s) to selected.
+      const menuOptionValue = this.getOptions()[index][1];
+      if (menuOptionValue === `${angleValue}`) {
+        Blockly.utils.dom.addClass(menuItem, 'blocklyMenuItemSelected');
+      }
+    });
+  }
+
+  /**
+   * Observe the menu element for changes and animate the angle helper based on the
+   * highlighted option's value.
+   * @private
+   */
+  private animateAngleHelperOnMenuOptionHighlight() {
+    // In order to preview dropdown a highlighted menu option, we detect changes to its
+    // class and animate the angle helper based on its value. A user might do this my
+    // mousing over, pressing up or down, or using keyboard navigation commands.
     // This approach uses the MutationObserver API, which allows us to observe changes
     // to the DOM. We do this because Blockly swallows ArrowUp and ArrowDown key events.
     const menuElement = document.querySelector('.blocklyMenu');
