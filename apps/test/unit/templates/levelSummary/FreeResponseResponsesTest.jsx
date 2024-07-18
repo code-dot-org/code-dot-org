c4a033c06719471b21b162fdada49c2d161c931b
kobryan0619 <kaitie.obryan@gmail.com>
2024-07-18 08:03:37 -0500

    added tests for free responses

--- apps/test/unit/templates/levelSummary/FreeResponseResponsesTest.jsx
+++ apps/test/unit/templates/levelSummary/FreeResponseResponsesTest.jsx
@@ -1,26 +1,46 @@
-import {shallow} from 'enzyme'; // eslint-disable-line no-restricted-imports
+import {render, screen} from '@testing-library/react';
 import React from 'react';
 
+import DCDO from '@cdo/apps/dcdo';
 import FreeResponseResponses from '@cdo/apps/templates/levelSummary/FreeResponseResponses';
 
-import styles from '@cdo/apps/templates/levelSummary/summary.module.scss';
+const RESPONSES = [
+  {user_id: 0, text: 'student response 1', student_name: 'student 1'},
+  {user_id: 1, text: 'student response 2', student_name: 'student 2'},
+  {user_id: 3, text: 'student response 3', student_name: 'student 3'},
+  {user_id: 2, text: 'student response 4', student_name: 'student 4'},
+  {user_id: 9, text: 'student response 5', student_name: 'student 5'},
+];
+const DEFAULT_PROPS = {
+  responses: RESPONSES,
+  showStudentNames: false,
+};
 
 describe('FreeResponseResponses', () => {
-  it('renders responses', () => {
-    const responses = [
-      {user_id: 0, text: 'student response 1'},
-      {user_id: 1, text: 'student response 2'},
-      {user_id: 3, text: 'student response 3'},
-      {user_id: 2, text: 'student response 4'},
-      {user_id: 9, text: 'student response 5'},
-    ];
-    const wrapper = shallow(<FreeResponseResponses responses={responses} />);
+  const renderDefault = (propOverrides = {}) => {
+    const props = {...DEFAULT_PROPS, ...propOverrides};
+    return render(<FreeResponseResponses {...props} />);
+  };
 
-    expect(wrapper.find(`.${styles.studentAnswer}`)).toHaveLength(
-      responses.length
+  it('renders responses', () => {
+    DCDO.set('cfu-pin-hide-enabled', true);
+    renderDefault();
+
+    expect(screen.getAllByText(/student response [1-5]/)).toHaveLength(
+      RESPONSES.length
     );
-    expect(wrapper.find(`.${styles.studentAnswer}`).at(0).text()).toContain(
-      'student response 1'
+
+    const studentNameElements = screen.queryAllByText(/student [1-5]/);
+    expect(studentNameElements).toHaveLength(0);
+  });
+
+  it('renders responses with names', () => {
+    DCDO.set('cfu-pin-hide-enabled', true);
+    renderDefault({showStudentNames: true});
+
+    expect(screen.getAllByText(/student response [1-5]/)).toHaveLength(
+      RESPONSES.length
     );
+    expect(screen.getAllByText(/student [1-5]/)).toHaveLength(RESPONSES.length);
   });
 });
