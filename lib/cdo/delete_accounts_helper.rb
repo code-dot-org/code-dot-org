83e86884efe4df658549e07c7c3b13ef46a20e6a
Seth Nickell <seth.nickell@code.org>
2024-07-18 12:57:09 -1000

    Revert "Revert "Delete all datablock storage tables and kvps for user on account deletion"" (#59695)
    * Revert "Revert "Delete all datablock storage tables and kvps for a given user…"
    
    This reverts commit 418d9d73d7ec063d122492889c423e65397984e2.
    
    * Use ActiveRecords instead of direct SQL. This fixes the SQL timeouts in tests on my machine.
    
    * Remove commented lines to try to avoid merge conflicts with another PR
    
    ---------
    
    Co-authored-by: Cassi Brenci <cassi.brenci@code.org>

--- lib/cdo/delete_accounts_helper.rb
+++ lib/cdo/delete_accounts_helper.rb
@@ -77,6 +77,14 @@ class DeleteAccountsHelper
     @log.puts "Deleting Firebase contents for #{channel_count} channels"
     FirebaseHelper.delete_channels encrypted_channel_ids
 
+    # Clear Datablock Storage contents for user's projects
+    @log.puts "Deleting Datablock Storage contents for #{project_ids.count} projects"
+    project_ids.each do |project_id|
+      DatablockStorageTable.where(project_id: project_id).delete_all
+      DatablockStorageKvp.where(project_id: project_id).delete_all
+      DatablockStorageRecord.where(project_id: project_id).delete_all
+    end
+
     @log.puts "Deleted #{channel_count} channels" if channel_count > 0
   end
 
