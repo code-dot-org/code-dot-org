48e63080973385fec8f5cfc8ccec210040b9aaad
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-07-18 12:43:35 -0700

    fix(MiniViewTopRow): allow cold loading storybook

--- apps/src/code-studio/components/progress/MiniViewTopRow.story.jsx
+++ apps/src/code-studio/components/progress/MiniViewTopRow.story.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 
+import isRtl from '@cdo/apps/code-studio/isRtlRedux';
 import progress from '@cdo/apps/code-studio/progressRedux';
+import {registerReducers} from '@cdo/apps/redux';
+import * as commonReducers from '@cdo/apps/redux/commonReducers';
 import {reduxStore} from '@cdo/storybook/decorators';
 
 import MiniViewTopRow from './MiniViewTopRow';
@@ -23,6 +26,8 @@ const initialState = {
 export default {
   component: MiniViewTopRow,
 };
+
+registerReducers({...commonReducers, isRtl});
 
 // Template
 const Template = args => (
