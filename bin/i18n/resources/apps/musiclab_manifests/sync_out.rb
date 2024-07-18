20eb511533de89a0c5ac39bf5a8c6e9f778df14a
Ben Brooks <ben@code.org>
2024-07-18 14:42:33 -0700

    sync out

---
+++ bin/i18n/resources/apps/musiclab_manifests/sync_out.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require_relative '../../../i18n_script_utils'
+require_relative '../../../utils/sync_out_base'
+# require_relative '../animations'
+
+module I18n
+  module Resources
+    module Apps
+      module MusiclabManifest
+        class SyncOut < I18n::Utils::SyncOutBase
+          def process(language)
+            crowdin_file_path = I18nScriptUtils.crowdin_locale_dir(language[:locale_s], 'musiclab_manifests', 'music-library-intro2024.json')
+            return unless File.exist?(crowdin_file_path)
+
+            # deal with empty translation files?
+            js_locale = I18nScriptUtils.to_js_locale(language[:locale_s])
+            i18n_data = I18nScriptUtils.parse_file(crowdin_file_path)
+            upload_localized_strings(js_locale, i18n_data) unless options[:testing]
+
+            # do we need this?
+            # i18n_file_path = I18nScriptUtils.locale_dir(language[:locale_s], DIR_NAME, FILE_NAME)
+            # I18nScriptUtils.move_file(crowdin_file_path, i18n_file_path)
+            # I18nScriptUtils.remove_empty_dir File.dirname(crowdin_file_path)
+          end
+
+          private def upload_localized_strings(js_locale, i18n_data)
+            AWS::S3.upload_to_bucket(
+              'cdo-curriculum',
+              "media/musiclab-test/loc-intro2024/#{js_locale}.json",
+              JSON.pretty_generate(i18n_data),
+              acl: 'public-read',
+              no_random: true,
+              content_type: 'json',
+            )
+          end
+        end
+      end
+    end
+  end
+end
+
+I18n::Resources::Apps::MusiclabManifest::SyncOut.perform if __FILE__ == $0
