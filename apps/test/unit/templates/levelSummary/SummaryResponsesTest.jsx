77b7e44fe2b9d97085431346d680b885a98f9415
kobryan0619 <kaitie.obryan@gmail.com>
2024-07-18 15:48:10 -0500

    modified exisiting tests to RTL and Jest

--- apps/test/unit/templates/levelSummary/SummaryResponsesTest.jsx
+++ apps/test/unit/templates/levelSummary/SummaryResponsesTest.jsx
@@ -1,4 +1,4 @@
-import {mount} from 'enzyme'; // eslint-disable-line no-restricted-imports
+import {render, screen} from '@testing-library/react';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {combineReducers, createStore} from 'redux';
@@ -8,10 +8,6 @@ import progress from '@cdo/apps/code-studio/progressRedux';
 import viewAs from '@cdo/apps/code-studio/viewAsRedux';
 import SummaryResponses from '@cdo/apps/templates/levelSummary/SummaryResponses';
 import teacherSections from '@cdo/apps/templates/teacherDashboard/teacherSectionsRedux';
-
-import {expect} from '../../../util/reconfiguredChai';
-
-import styles from '@cdo/apps/templates/levelSummary/summary.module.scss';
 
 const JS_DATA = {
   level: {
@@ -42,7 +38,7 @@ const INITIAL_STATE = {
   },
 };
 
-const setUpWrapper = (state = {}, jsData = {}) => {
+const renderWithProviders = (state = {}, jsData = {}) => {
   const store = createStore(
     combineReducers({
       isRtl,
@@ -53,53 +49,27 @@ const setUpWrapper = (state = {}, jsData = {}) => {
     {...INITIAL_STATE, ...state}
   );
 
-  const wrapper = mount(
+  return render(
     <Provider store={store}>
       <SummaryResponses scriptData={{...JS_DATA, ...jsData}} />
     </Provider>
   );
-
-  return wrapper;
 };
 
 describe('SummaryResponses', () => {
   it('renders elements', () => {
-    const wrapper = setUpWrapper();
+    renderWithProviders();
 
-    // Student responses.
-    expect(wrapper.find(`.${styles.studentsSubmittedRight}`).text()).to.eq(
-      '1/1 students answered'
-    );
-    expect(wrapper.find(`div.${styles.studentAnswer}`).length).to.eq(1);
-    // Section selector, with one section.
-    expect(wrapper.find('SectionSelector').length).to.eq(1);
-    expect(wrapper.find('SectionSelector option').length).to.eq(2);
-  });
-
-  it('applies correct classes when rtl', () => {
-    const wrapper = setUpWrapper({
-      isRtl: true,
-      progress: {
-        currentLessonId: 0,
-        currentLevelId: '0',
-        lessons: [
-          {
-            id: 0,
-            levels: [
-              {activeId: '0', position: 1},
-              {activeId: '1', position: 2},
-            ],
-          },
-        ],
-      },
-    });
-
-    expect(wrapper.find(`.${styles.studentsSubmittedRight}`).length).to.eq(0);
-    expect(wrapper.find(`.${styles.studentsSubmittedLeft}`).length).to.eq(1);
+    // Student responses
+    screen.getByText('1/1 students answered');
+    screen.getByText('student answer');
+    // // Section selector, with one section.
+    screen.getByRole('combobox');
+    expect(screen.getAllByRole('option')).toHaveLength(2);
   });
 
   it('does not render response counter/text if no section selected', () => {
-    const wrapper = setUpWrapper({
+    renderWithProviders({
       teacherSections: {
         selectedStudents: [{id: 0}],
         selectedSectionId: null,
@@ -108,12 +78,11 @@ describe('SummaryResponses', () => {
       },
     });
 
-    expect(wrapper.find(`.${styles.studentsSubmittedRight}`).length).to.eq(0);
-    expect(wrapper.find(`.${styles.studentsSubmittedLeft}`).length).to.eq(0);
+    expect(screen.queryByText('/')).toBeNull();
   });
 
   it('renders toggle when appropriate', () => {
-    const wrapper = setUpWrapper(
+    renderWithProviders(
       {},
       {
         level: {
@@ -127,11 +96,11 @@ describe('SummaryResponses', () => {
       }
     );
 
-    expect(wrapper.find('ToggleSwitch').length).to.eq(1);
+    screen.getByText('Show answer');
   });
 
   it('does not render toggle for Free Response', () => {
-    const wrapper = setUpWrapper(
+    renderWithProviders(
       {},
       {
         level: {
@@ -145,11 +114,11 @@ describe('SummaryResponses', () => {
       }
     );
 
-    expect(wrapper.find('ToggleSwitch').length).to.eq(0);
+    expect(screen.queryByText('Show answer')).toBeNull();
   });
 
   it('does not render toggle without policy permission', () => {
-    const wrapper = setUpWrapper(
+    renderWithProviders(
       {},
       {
         level: {
@@ -162,6 +131,6 @@ describe('SummaryResponses', () => {
       }
     );
 
-    expect(wrapper.find('ToggleSwitch').length).to.eq(0);
+    expect(screen.queryByText('Show answer')).toBeNull();
   });
 });
