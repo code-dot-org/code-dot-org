f8aba8e61dab57d05772ebb5f5f67e43d6326060
Seth Nickell <snickell@gmail.com>
2024-07-18 16:32:43 -1000

    jest.config.js: add p-queue to transformIgnorePatterns

--- apps/jest.config.js
+++ apps/jest.config.js
@@ -234,7 +234,7 @@ const config = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-    'node_modules/(?!vmsg|@code-dot-org/johnny-five|@code-dot-org/js-interpreter|blockly/core)',
+    'node_modules/(?!vmsg|@code-dot-org/johnny-five|@code-dot-org/js-interpreter|blockly/core|p-queue)',
   ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
