1b5e86806c741ca167496414c32086dcb15ac401
Seth Nickell <snickell@gmail.com>
2024-07-19 15:18:53 -1000

    Revert "Datablock Storage: only allow up to 3x concurrent fetch() connections from data blocks (#59879)"
    This reverts commit aedc5b014e4c604a721cc67334118efaddaa46e8.

--- apps/src/storage/rateLimit.js
+++ apps/src/storage/rateLimit.js
@@ -1,17 +1,9 @@
-import PQueue from 'p-queue';
-
-export const MAX_CONCURRENT_BLOCK_IOPS = 3;
-const blockIOQueue = new PQueue({concurrency: MAX_CONCURRENT_BLOCK_IOPS});
-
 // rateLimit() throws an error if called more than RATE_LIMIT times per RATE_LIMIT_INTERVAL_MS
 // we use this in Applab commands.js to rate limit access to Datablock Storage from student code
-//
-// Additionally, rateLimit throttles IO operations to MAX_CONCURRENT_BLOCK_IOPS concurrently
-// to prevent student data blocks from overwhelming the server with parallel connections.
 let rateLimitAccessLog = [];
 export const RATE_LIMIT = 600;
 export const RATE_LIMIT_INTERVAL_MS = 60000;
-export async function rateLimit(operation = () => undefined, now = Date.now()) {
+export function rateLimit(now = Date.now()) {
   const timeSinceEarliestLog = () => now - rateLimitAccessLog[0];
 
   // Drop log entries older than RATE_LIMIT_INTERVAL_MS
@@ -32,15 +24,10 @@ export async function rateLimit(operation = () => undefined, now = Date.now()) {
         'The app is reading/writing data too many times per second. ' +
         "If you were trying to write data, it wasn't written."
     );
+  } else {
+    // Log the current access
+    rateLimitAccessLog.push(now);
   }
-
-  // Log the current access
-  rateLimitAccessLog.push(now);
-
-  // Throttle rateLimit-ed operations to ${MAX_CONCURRENT_BLOCK_IOPS}x concurrency
-  // this prevents students from overwhelming the server with too many simultaneous
-  // requests from data blocks.
-  return blockIOQueue.add(operation);
 }
 
 export function resetRateLimit() {
