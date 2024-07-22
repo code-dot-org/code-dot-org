01be5a00a825ba0957a493042864d14afa93f30c
Turner Riley <turnertriley@gmail.com>
2024-07-22 11:37:47 -0700

    fix test with removing sinon

--- apps/test/unit/lib/ui/simpleSignUp/workshop/WorkshopLinkAccountPageTest.tsx
+++ apps/test/unit/lib/ui/simpleSignUp/workshop/WorkshopLinkAccountPageTest.tsx
@@ -1,18 +1,8 @@
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-  within,
-} from '@testing-library/react';
+import {render, screen, within} from '@testing-library/react';
 import React from 'react';
-import sinon from 'sinon';
 
 import WorkshopLinkAccountPage from '@cdo/apps/lib/ui/simpleSignUp/workshop/WorkshopLinkAccountPage';
-import * as utils from '@cdo/apps/utils';
 import i18n from '@cdo/locale';
-
-import {expect} from '../../../../../util/reconfiguredChai';
 
 describe('Workshop Link Account Page Tests', () => {
   const defaultProps = {
@@ -22,14 +12,6 @@ describe('Workshop Link Account Page Tests', () => {
     continueAccountUrl: 'continueAccountUrl.com',
     existingAccountUrlHref: 'existingAccountUrl.com',
   };
-
-  beforeEach(() => {
-    sinon.stub(utils, 'navigateToHref');
-  });
-
-  afterEach(() => {
-    (utils.navigateToHref as sinon.SinonStub).restore();
-  });
 
   function renderDefault(overrideProps?: object) {
     const props = {...defaultProps, ...overrideProps};
@@ -52,15 +34,12 @@ describe('Workshop Link Account Page Tests', () => {
         i18n.accountExistingAccountCardContentWorkshopEnroll()
       );
       // Should have button to link new account
-      const existingAccountButton = withinExistingAccountCard.getByText(
+      withinExistingAccountCard.getByText(
         i18n.ltiLinkAccountExistingAccountCardActionLabel()
       );
-
-      fireEvent.click(existingAccountButton);
-
-      expect(utils.navigateToHref).to.have.been.calledWith(
-        defaultProps.existingAccountUrlHref
-      );
+      expect(
+        withinExistingAccountCard.getByRole('link').getAttribute('href')
+      ).toBe(defaultProps.existingAccountUrlHref);
     });
   });
 
@@ -80,18 +59,10 @@ describe('Workshop Link Account Page Tests', () => {
         i18n.accountNewAccountCardContentWorkshopEnroll()
       );
       // Should have button to link new account
-      const newAccountButton = withinNewAccountCard.getByText(
-        i18n.createAccount()
-      );
+      withinNewAccountCard.getByText(i18n.createAccount());
 
-      fireEvent.click(newAccountButton);
-
-      await waitFor(
-        () =>
-          expect(utils.navigateToHref).to.have.been.calledWith(
-            defaultProps.newAccountUrl
-          ),
-        {timeout: 999999}
+      expect(withinNewAccountCard.getByRole('link').getAttribute('href')).toBe(
+        defaultProps.newAccountUrl
       );
     });
   });
@@ -112,19 +83,11 @@ describe('Workshop Link Account Page Tests', () => {
         i18n.ltiLinkAccountContinueAccountCardContent()
       );
       // Should have button to continue to code.org
-      const continueAccountButton = withinContinueAccountCard.getByText(
-        i18n.ltiIframeCallToAction()
-      );
+      withinContinueAccountCard.getByText(i18n.ltiIframeCallToAction());
 
-      fireEvent.click(continueAccountButton);
-
-      await waitFor(
-        () =>
-          expect(utils.navigateToHref).to.have.been.calledWith(
-            defaultProps.continueAccountUrl
-          ),
-        {timeout: 999999}
-      );
+      expect(
+        withinContinueAccountCard.getByRole('link').getAttribute('href')
+      ).toBe(defaultProps.continueAccountUrl);
     });
   });
 
@@ -132,21 +95,17 @@ describe('Workshop Link Account Page Tests', () => {
     it('should link to the cancel controller when newCtaType is new', () => {
       renderDefault();
 
-      const cancelButton = screen.getByText(i18n.cancel());
-
-      fireEvent.click(cancelButton);
-
-      expect(utils.navigateToHref).to.have.been.calledWith(`/users/cancel`);
+      expect(
+        screen.getByRole('link', {name: i18n.cancel()}).getAttribute('href')
+      ).toBe('/users/cancel');
     });
 
     it('should link to the sign out controller when newCtaType is continue', () => {
       renderDefault({newCtaType: 'continue'});
 
-      const cancelButton = screen.getByText(i18n.cancel());
-
-      fireEvent.click(cancelButton);
-
-      expect(utils.navigateToHref).to.have.been.calledWith(`/users/sign_out`);
+      expect(
+        screen.getByRole('link', {name: i18n.cancel()}).getAttribute('href')
+      ).toBe('/users/sign_out');
     });
   });
 });
