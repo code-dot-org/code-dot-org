dfd7ba2e40f077ca8a9345bfc12691460666b2f3
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-07-22 15:22:12 -0700

    fix broken test

--- dashboard/test/controllers/devise_unlocks_controller_test.rb
+++ dashboard/test/controllers/devise_unlocks_controller_test.rb
@@ -6,10 +6,11 @@ class DeviseUnlocksControllerTest < ActionController::TestCase
 
   test 'locked-out account can be successfully unlocked by token' do
     teacher = create(:teacher)
-    teacher.lock_access!
+    refute teacher.access_locked?
 
+    token = teacher.lock_access!
     assert teacher.access_locked?
-    token = Devise.token_generator.digest(teacher, :unlock_token, teacher.unlock_token)
+
     get :show, params: {unlock_token: token}
     refute teacher.reload.access_locked?
   end
