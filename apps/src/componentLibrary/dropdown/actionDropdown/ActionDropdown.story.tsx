d03e14fd0c36eb4e0ad85f0e179557372fb1cc83
denyslevada <levada.denys@gmail.com>
2024-07-23 15:42:15 +0300

    * update stories

--- apps/src/componentLibrary/dropdown/actionDropdown/ActionDropdown.story.tsx
+++ apps/src/componentLibrary/dropdown/actionDropdown/ActionDropdown.story.tsx
@@ -1,12 +1,7 @@
 import {Meta, StoryFn} from '@storybook/react';
-import React, {useState, useCallback} from 'react';
+import React from 'react';
 
-import {dropdownColors} from '@cdo/apps/componentLibrary/dropdown';
-
-import ActionDropdown, {
-  ActionDropdownProps,
-  ActionDropdownOption,
-} from './index';
+import ActionDropdown, {ActionDropdownProps} from './index';
 
 export default {
   title: 'DesignSystem/Dropdown/Action Dropdown', // eslint-disable-line storybook/no-title-property-in-meta
@@ -17,33 +12,12 @@ export default {
 // TEMPLATE
 //
 const SingleTemplate: StoryFn<ActionDropdownProps> = args => {
-  const [selectedValue, setValue] = useState(
-    args.selectedOption as ActionDropdownOption
-  );
-  const onChange = useCallback(
-    (option: ActionDropdownOption) => {
-      setValue(option);
-      args.onChange(option);
-    },
-    [args, setValue]
-  );
-
-  return (
-    <ActionDropdown
-      {...args}
-      selectedOption={selectedValue}
-      onChange={onChange}
-    />
-  );
+  return <ActionDropdown {...args} />;
 };
 
 const MultipleTemplate: StoryFn<{
   components: ActionDropdownProps[];
 }> = args => {
-  const [values, setValues] = useState(
-    {} as Record<string, ActionDropdownOption>
-  );
-
   return (
     <>
       <p>
@@ -53,28 +27,8 @@ const MultipleTemplate: StoryFn<{
       <p>Multiple Dropdown:</p>
       <div style={{display: 'flex', gap: '20px'}}>
         {args.components?.map(componentArg => {
-          if (values[componentArg.name] === undefined) {
-            setValues({
-              ...values,
-              [componentArg.name]: componentArg.selectedOption,
-            });
-          }
-
-          const onChange = (option: ActionDropdownOption) => {
-            setValues({
-              ...values,
-              [componentArg.name]: option,
-            });
-            componentArg.onChange(option);
-          };
-
           return (
-            <ActionDropdown
-              key={`${componentArg.name}`}
-              {...componentArg}
-              selectedOption={values[componentArg.name]}
-              onChange={onChange}
-            />
+            <ActionDropdown key={`${componentArg.name}`} {...componentArg} />
           );
         })}
       </div>
@@ -96,82 +50,80 @@ DefaultActionDropdown.args = {
       value: 'option-1',
       label: 'Option 1',
       icon: {iconName: 'check', iconStyle: 'solid'},
+      onClick: () => console.log('option 1'),
     },
     {
       value: 'option-2',
       label: 'Option 2',
       icon: {iconName: 'xmark', iconStyle: 'solid'},
+      onClick: () => console.log('option 2'),
     },
   ],
   labelText: 'Default Dropdown',
-  selectedOption: {
-    value: 'option-1',
-    label: 'Option 1',
-    icon: {iconName: 'check', iconStyle: 'solid'},
-  },
   disabled: false,
-  color: dropdownColors.black,
-  onChange: args => null,
   size: 'm',
 };
 
 export const DisabledActionDropdown = SingleTemplate.bind({});
 DisabledActionDropdown.args = {
   name: 'disabled-dropdown',
+  triggerButtonProps: {
+    color: 'purple',
+    type: 'primary',
+    isIconOnly: true,
+    icon: {iconName: 'smile', iconStyle: 'solid'},
+  },
   options: [
     {
       value: 'option-1',
       label: 'Option 1',
       icon: {iconName: 'check', iconStyle: 'solid'},
+      onClick: () => null,
     },
     {
       value: 'option-2',
       label: 'Option 2',
       icon: {iconName: 'xmark', iconStyle: 'solid'},
+      onClick: () => null,
     },
   ],
-  selectedOption: {
-    value: 'option-1',
-    label: 'Option 1',
-    icon: {iconName: 'check', iconStyle: 'solid'},
-  },
   labelText: 'Disabled Dropdown',
-  onChange: args => null,
   disabled: true,
-  color: dropdownColors.black,
   size: 'm',
 };
 
 export const WithDisabledOptionActionDropdown = SingleTemplate.bind({});
 WithDisabledOptionActionDropdown.args = {
   name: 'withDisabledOption-dropdown',
+  triggerButtonProps: {
+    color: 'purple',
+    type: 'primary',
+    isIconOnly: true,
+    icon: {iconName: 'smile', iconStyle: 'solid'},
+  },
   options: [
     {
       value: 'option-1',
       label: 'Option 1',
-      isOptionDisabled: true,
-      icon: {iconName: 'xmark', iconStyle: 'solid'},
+      icon: {iconName: 'check', iconStyle: 'solid'},
+      onClick: () => null,
     },
     {
+      isOptionDisabled: true,
       value: 'option-2',
       label: 'Option 2',
-      icon: {iconName: 'check', iconStyle: 'solid'},
+      icon: {iconName: 'xmark', iconStyle: 'solid'},
+      onClick: () => null,
     },
     {
       value: 'option-3',
       label: 'Option 3',
-      icon: {iconName: 'check', iconStyle: 'solid'},
+      icon: {iconName: 'xmark', iconStyle: 'solid'},
+      onClick: () => null,
     },
   ],
   disabled: false,
-  color: dropdownColors.black,
-  selectedOption: {
-    value: 'option-1',
-    label: 'Option 1',
-    icon: {iconName: 'xmark', iconStyle: 'solid'},
-  },
-  labelText: 'Dropdown with disabled option',
-  onChange: args => null,
+  labelText: 'Default Dropdown',
   size: 'm',
 };
 
@@ -179,78 +131,55 @@ export const GroupOfActionDropdownColors = MultipleTemplate.bind({});
 GroupOfActionDropdownColors.args = {
   components: [
     {
-      name: 'default-dropdown-white',
+      name: 'default-dropdown-light',
+      triggerButtonProps: {
+        color: 'purple',
+        type: 'primary',
+        isIconOnly: true,
+        icon: {iconName: 'smile', iconStyle: 'solid'},
+      },
       options: [
         {
           value: 'option-1',
           label: 'Option 1',
           icon: {iconName: 'check', iconStyle: 'solid'},
+          onClick: () => null,
         },
         {
           value: 'option-2',
           label: 'Option 2',
           icon: {iconName: 'xmark', iconStyle: 'solid'},
+          onClick: () => null,
         },
       ],
-      selectedOption: {
-        value: 'option-1',
-        label: 'Option 1',
-        icon: {iconName: 'check', iconStyle: 'solid'},
-      },
-      labelText: 'White Dropdown',
-      onChange: args => null,
+      labelText: 'Light Dropdown',
       size: 'm',
       disabled: false,
-      color: dropdownColors.white,
     },
     {
-      name: 'default-dropdown-black',
+      name: 'default-dropdown-dark',
+      triggerButtonProps: {
+        color: 'white',
+        type: 'primary',
+        isIconOnly: true,
+        icon: {iconName: 'smile', iconStyle: 'solid'},
+      },
       options: [
         {
           value: 'option-1',
           label: 'Option 1',
           icon: {iconName: 'check', iconStyle: 'solid'},
+          onClick: () => null,
         },
         {
           value: 'option-2',
           label: 'Option 2',
           icon: {iconName: 'xmark', iconStyle: 'solid'},
+          onClick: () => null,
         },
       ],
-      selectedOption: {
-        value: 'option-1',
-        label: 'Option 1',
-        icon: {iconName: 'check', iconStyle: 'solid'},
-      },
       labelText: 'Black Dropdown',
-      onChange: args => null,
       size: 'm',
-      color: dropdownColors.black,
-      disabled: false,
-    },
-    {
-      name: 'default-dropdown-gray',
-      options: [
-        {
-          value: 'option-1',
-          label: 'Option 1',
-          icon: {iconName: 'check', iconStyle: 'solid'},
-        },
-        {
-          value: 'option-2',
-          label: 'Option 2',
-          icon: {iconName: 'xmark', iconStyle: 'solid'},
-        },
-      ],
-      selectedOption: {
-        value: 'option-1',
-        label: 'Option 1',
-        icon: {iconName: 'check', iconStyle: 'solid'},
-      },
-      labelText: 'Gray Dropdown',
-      onChange: args => null,
-      size: 'm',
-      color: dropdownColors.gray,
       disabled: false,
     },
   ],
@@ -260,103 +189,108 @@ GroupOfSizesOfActionDropdown.args = {
   components: [
     {
       name: 'default-dropdown-xs',
+      triggerButtonProps: {
+        color: 'purple',
+        type: 'primary',
+        isIconOnly: true,
+        icon: {iconName: 'smile', iconStyle: 'solid'},
+      },
       options: [
         {
           value: 'option-1',
           label: 'Option 1',
           icon: {iconName: 'check', iconStyle: 'solid'},
+          onClick: () => null,
         },
         {
           value: 'option-2',
           label: 'Option 2',
           icon: {iconName: 'xmark', iconStyle: 'solid'},
+          onClick: () => null,
         },
       ],
-      selectedOption: {
-        value: 'option-1',
-        label: 'Option 1',
-        icon: {iconName: 'check', iconStyle: 'solid'},
-      },
       labelText: 'XS Dropdown',
-      onChange: args => null,
       size: 'xs',
       disabled: false,
-      color: dropdownColors.black,
     },
     {
       name: 'default-dropdown-s',
+      triggerButtonProps: {
+        color: 'purple',
+        type: 'primary',
+        isIconOnly: true,
+        icon: {iconName: 'smile', iconStyle: 'solid'},
+      },
       options: [
         {
           value: 'option-1',
           label: 'Option 1',
           icon: {iconName: 'check', iconStyle: 'solid'},
+          onClick: () => null,
         },
         {
           value: 'option-2',
           label: 'Option 2',
           icon: {iconName: 'xmark', iconStyle: 'solid'},
+          onClick: () => null,
         },
       ],
-      selectedOption: {
-        value: 'option-1',
-        label: 'Option 1',
-        icon: {iconName: 'check', iconStyle: 'solid'},
-      },
       labelText: 'S Dropdown',
-      onChange: args => null,
       size: 's',
       disabled: false,
-      color: dropdownColors.black,
     },
     {
       name: 'default-dropdown-m',
+      triggerButtonProps: {
+        color: 'purple',
+        type: 'primary',
+        isIconOnly: true,
+        icon: {iconName: 'smile', iconStyle: 'solid'},
+      },
       options: [
         {
           value: 'option-1',
           label: 'Option 1',
           icon: {iconName: 'check', iconStyle: 'solid'},
+          onClick: () => null,
         },
         {
           value: 'option-2',
           label: 'Option 2',
-          icon: {iconName: 'smile', iconStyle: 'solid'},
+          icon: {iconName: 'xmark', iconStyle: 'solid'},
+          onClick: () => null,
         },
       ],
-      selectedOption: {
-        value: 'option-1',
-        label: 'Option 1',
-        icon: {iconName: 'check', iconStyle: 'solid'},
-      },
       labelText: 'M Dropdown',
-      onChange: args => null,
       size: 'm',
       disabled: false,
-      color: dropdownColors.black,
     },
     {
-      name: 'default-dropdown-white',
+      name: 'default-dropdown-l',
+      triggerButtonProps: {
+        color: 'purple',
+        type: 'primary',
+        isIconOnly: true,
+        icon: {iconName: 'smile', iconStyle: 'solid'},
+      },
       options: [
         {
           value: 'option-1',
           label: 'Option 1',
           icon: {iconName: 'check', iconStyle: 'solid'},
+          onClick: () => null,
         },
         {
           value: 'option-2',
           label: 'Option 2',
-          icon: {iconName: 'smile', iconStyle: 'solid'},
+          icon: {iconName: 'xmark', iconStyle: 'solid'},
+          onClick: () => null,
         },
       ],
-      selectedOption: {
-        value: 'option-1',
-        label: 'Option 1',
-        icon: {iconName: 'check', iconStyle: 'solid'},
-      },
+
       labelText: 'L Dropdown',
-      onChange: args => null,
       size: 'l',
       disabled: false,
-      color: dropdownColors.black,
     },
   ],
 };
