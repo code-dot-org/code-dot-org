b0ca41c827bbfd2e5e9194dc759660fd8e161000
Liam Frye-Mason <liam.fryemason@code.org>
2024-07-23 07:34:57 -0700

    Fix comments on import/order (#59862)
    * Fix comments on import/order
    
    * fix

--- apps/src/p5lab/P5Wrapper.js
+++ apps/src/p5lab/P5Wrapper.js
@@ -2,13 +2,12 @@ import p5 from '@code-dot-org/p5';
 
 import {getStore} from '@cdo/apps/redux';
 
+import {allAnimationsSingleFrameSelector} from './redux/animationList';
 // Disabling import order in order to maintain p5 import first.
 // This might be safe to remove but needs investigation whether any behavior is changed by order.
 /* eslint-disable import/order */
-import {allAnimationsSingleFrameSelector} from './redux/animationList';
 window.p5 = p5;
 import '@code-dot-org/p5.play/lib/p5.play';
-
 import p5SpriteWrapper from './P5SpriteWrapper';
 import p5GroupWrapper from './P5GroupWrapper';
 import {backgrounds} from './spritelab/backgrounds.json';
