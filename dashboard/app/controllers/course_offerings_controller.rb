a9f02df18e4fe94e43c8367f7fb6658c0b2dc3d8
Hannah Bergam <hannahbergam@gmail.com>
2024-07-24 12:54:04 -0700

    fixing ruby course offerings function

--- dashboard/app/controllers/course_offerings_controller.rb
+++ dashboard/app/controllers/course_offerings_controller.rb
@@ -37,7 +37,9 @@ class CourseOfferingsController < ApplicationController
 
   def self_paced_pl_course_offerings
     return head :bad_request unless current_user
-    offerings = CourseOffering.assignable_course_offerings(current_user).select {|co| co.get_participant_audience == 'teacher' && co.instruction_type == 'self_paced' && Curriculum::SharedCourseConstants::PARTICIPANT_AUDIENCES_BY_TYPE[participant_type].include?('teacher')}
+    offerings = CourseOffering.assignable_course_offerings(current_user).filter do |co|
+      co.get_participant_audience == 'teacher' && co.instruction_type == 'self_paced' && co.header.present?
+    end
     render :ok, json: offerings&.map(&:summarize_self_paced_pl).to_json
   end
 
