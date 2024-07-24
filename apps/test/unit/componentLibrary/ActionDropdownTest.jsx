24f7dc1dc9dd6dafca34cd1bad3272f79cf3e436
denyslevada <levada.denys@gmail.com>
2024-07-24 10:53:02 +0300

    * updated test

--- apps/test/unit/componentLibrary/ActionDropdownTest.jsx
+++ apps/test/unit/componentLibrary/ActionDropdownTest.jsx
@@ -9,26 +9,30 @@ const allOptions = [
     value: 'option-1',
     label: 'option1',
     icon: {iconName: 'check', iconStyle: 'solid'},
+    onClick: jest.fn(),
   },
   {
     value: 'option-2',
     label: 'option2',
     icon: {iconName: 'check', iconStyle: 'solid'},
+    onClick: jest.fn(),
   },
   {
     value: 'option-3',
     label: 'option3',
     icon: {iconName: 'check', iconStyle: 'solid'},
+    onClick: jest.fn(),
   },
 ];
-let selectedValue = {};
-const onActionDropdownChange = option => {
-  selectedValue = option;
+
+const triggerButtonProps = {
+  isIconOnly: true,
+  icon: {iconName: 'check', iconStyle: 'solid'},
 };
 
 describe('Design System - Action Dropdown Component', () => {
   beforeEach(() => {
-    onActionDropdownChange({});
+    allOptions.forEach(option => option.onClick.mockClear());
   });
 
   it('Action Dropdown - renders with correct text and options', () => {
@@ -36,108 +40,87 @@ describe('Design System - Action Dropdown Component', () => {
       <ActionDropdown
         name="test1-dropdown"
         options={allOptions}
-        selectedOption={selectedValue}
-        onChange={onActionDropdownChange}
         labelText="Dropdown label"
+        triggerButtonProps={triggerButtonProps}
       />
     );
 
-    const label = screen.getByText('Dropdown label');
+    const triggerButton = screen.getByRole('button', {name: 'Dropdown label'});
     const option1 = screen.getByText('option1');
     const option2 = screen.getByText('option2');
     const option3 = screen.getByText('option3');
 
-    expect(label).toBeDefined();
-    expect(option1).toBeDefined();
-    expect(option2).toBeDefined();
-    expect(option3).toBeDefined();
+    expect(triggerButton).toBeTruthy();
+    expect(option1).toBeTruthy();
+    expect(option2).toBeTruthy();
+    expect(option3).toBeTruthy();
   });
 
-  it('Action Dropdown - renders with correct text and options, changes selected value on when one is selected', async () => {
+  it('Action Dropdown - calls onClick when an option is selected', async () => {
     const user = userEvent.setup();
-    const spyOnChange = jest.fn();
-    const onChange = option => {
-      onActionDropdownChange(option);
-      spyOnChange(option);
-    };
-    const DropdownToRender = () => (
+    render(
       <ActionDropdown
         name="test2-dropdown"
         options={allOptions}
-        selectedOption={selectedValue}
-        onChange={onChange}
         labelText="Dropdown2 label"
+        triggerButtonProps={triggerButtonProps}
       />
     );
 
-    const {rerender} = render(<DropdownToRender />);
+    const triggerButton = screen.getByRole('button', {name: 'Dropdown2 label'});
+    await user.click(triggerButton);
 
-    const label = screen.getByText('Dropdown2 label');
     const option1 = screen.getByText('option1');
     const option2 = screen.getByText('option2');
 
-    expect(label).toBeDefined();
-    expect(option1).toBeDefined();
-    expect(option2).toBeDefined();
-    expect(selectedValue.value).toBeUndefined();
-
     await user.click(option1);
-
-    rerender(<DropdownToRender />);
-
-    expect(spyOnChange).toHaveBeenCalledTimes(1);
-    expect(selectedValue.value).toBe('option-1');
+    expect(allOptions[0].onClick).toHaveBeenCalledTimes(1);
 
     await user.click(option2);
-
-    rerender(<DropdownToRender />);
-
-    expect(spyOnChange).toHaveBeenCalledTimes(2);
-    expect(selectedValue.value).toBe('option-2');
+    expect(allOptions[1].onClick).toHaveBeenCalledTimes(1);
   });
 
-  it("Action Dropdown - renders disabled dropdown, doesn't change on click", async () => {
+  it("Action Dropdown - renders disabled dropdown, doesn't call onClick on click", async () => {
     const user = userEvent.setup();
-    const spyOnChange = jest.fn();
-    const onChange = option => {
-      onActionDropdownChange(option);
-      spyOnChange(option);
-    };
-
-    const DropdownToRender = () => (
+    render(
       <ActionDropdown
         name="test2-dropdown"
         disabled={true}
         options={allOptions}
-        selectedOption={selectedValue}
-        onChange={onChange}
         labelText="Dropdown2 label"
+        triggerButtonProps={triggerButtonProps}
       />
     );
 
-    const {rerender} = render(<DropdownToRender />);
+    const triggerButton = screen.getByRole('button', {name: 'Dropdown2 label'});
+    await user.click(triggerButton);
 
-    const label = screen.getByText('Dropdown2 label');
     const option1 = screen.getByText('option1');
     const option2 = screen.getByText('option2');
 
-    expect(label).toBeDefined();
-    expect(option1).toBeDefined();
-    expect(option2).toBeDefined();
-    expect(selectedValue.value).toBeUndefined();
-
     await user.click(option1);
-
-    rerender(<DropdownToRender />);
-
-    expect(spyOnChange).not.toHaveBeenCalled();
-    expect(selectedValue.value).toBeUndefined();
+    expect(allOptions[0].onClick).not.toHaveBeenCalled();
 
     await user.click(option2);
+    expect(allOptions[1].onClick).not.toHaveBeenCalled();
+  });
 
-    rerender(<DropdownToRender />);
+  it('Action Dropdown - renders trigger button with custom props', () => {
+    render(
+      <ActionDropdown
+        name="test3-dropdown"
+        options={allOptions}
+        labelText="Dropdown3 label"
+        triggerButtonProps={{
+          ...triggerButtonProps,
+          className: 'custom-trigger-button',
+        }}
+      />
+    );
 
-    expect(spyOnChange).not.toHaveBeenCalled();
-    expect(selectedValue.value).toBeUndefined();
+    const triggerButton = screen.getByRole('button', {name: 'Dropdown3 label'});
+    expect(triggerButton.classList.contains('custom-trigger-button')).toBe(
+      true
+    );
   });
 });
