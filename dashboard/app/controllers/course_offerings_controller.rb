176caf655353836698d70d998e8930aea8e165a0
Hannah Bergam <hannahbergam@gmail.com>
2024-07-24 16:08:42 -0700

    adding check for published state

--- dashboard/app/controllers/course_offerings_controller.rb
+++ dashboard/app/controllers/course_offerings_controller.rb
@@ -38,7 +38,7 @@ class CourseOfferingsController < ApplicationController
   def self_paced_pl_course_offerings
     return head :bad_request unless current_user
     offerings = CourseOffering.assignable_course_offerings(current_user).filter do |co|
-      co.get_participant_audience == 'teacher' && co.instruction_type == 'self_paced' && co.header.present?
+      co.get_participant_audience == 'teacher' && co.any_version_is_in_published_state? && co.instruction_type == 'self_paced' && co.header.present?
     end
     render :ok, json: offerings&.map(&:summarize_self_paced_pl).to_json
   end
