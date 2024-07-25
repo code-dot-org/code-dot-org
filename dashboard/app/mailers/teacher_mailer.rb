5ee7f998b95a224afe3a6e195970e1a1d9991da1
Bethany Connor <bethany@code.org>
2024-07-25 10:01:35 -0400

    Remove new teacher email and mailer

--- dashboard/app/mailers/teacher_mailer.rb
+++ dashboard/app/mailers/teacher_mailer.rb
@@ -2,21 +2,6 @@ class TeacherMailer < ApplicationMailer
   default from: 'Hadi Partovi <hadi_partovi@code.org>'
   default reply_to: 'Code.org <support@code.org>'
 
-  # Send newly registered teachers a welcome email
-  def new_teacher_email(teacher, teacher_locale = 'en-US')
-    @teacher = teacher
-
-    # We currently have two flavors for the new teacher welcome email,
-    # en-US (the default) and es-MX.
-    email_locale = teacher_locale.to_s == 'es-MX' ? teacher_locale : 'en-US'
-    email_template = teacher_locale.to_s == 'es-MX' ? 'new_teacher_email_es-MX' : 'new_teacher_email'
-
-    mail to: teacher.email,
-      subject: I18n.t('teacher_mailer.new_teacher_subject', locale: email_locale),
-      template_path: 'teacher_mailer',
-      template_name: email_template
-  end
-
   def delete_teacher_email(teacher, removed_students)
     @teacher = teacher
     @removed_students = removed_students
