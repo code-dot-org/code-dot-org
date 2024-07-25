e4522b66645beb7f30679f79634fcf9a5f4db712
denyslevada <levada.denys@gmail.com>
2024-07-25 15:49:43 +0300

    * fixed failing tests by updating jest config

--- apps/jest.config.js
+++ apps/jest.config.js
@@ -170,15 +170,10 @@ const config = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: [
-    './test/setupJest.js',
-    'jest-canvas-mock',
-    './test/setupJquery.js',
-  ],
+  setupFiles: ['jest-canvas-mock'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
-
+  setupFilesAfterEnv: ['./test/setupJest.js', './test/setupJquery.js'],
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
 
