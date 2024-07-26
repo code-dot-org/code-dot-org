88fe8b04d71da88864df80620586b69b2129398a
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2024-07-26 14:49:17 -0400

    [Google] move number validators to cdoUtils (+ show "invisible" blocks in start mode) (#60014)
    * move number validators to cdoUtils
    
    * add examples to comment

--- apps/src/blockly/addons/cdoUtils.ts
+++ apps/src/blockly/addons/cdoUtils.ts
@@ -213,7 +213,9 @@ export function moveHiddenBlocks(
 
   source.blocks.blocks.forEach(block => {
     const {invisible, procedureId} = block.extraState || {};
-    const hideBlock = procedureTypesToHide.includes(block.type) || invisible;
+    const hideBlock =
+      procedureTypesToHide.includes(block.type) ||
+      (invisible && !Blockly.isStartMode);
     const destination = hideBlock ? hiddenDefinitionSource : mainSource;
     destination.blocks.blocks.push(block);
 
@@ -443,6 +445,38 @@ export function getCode(workspace: WorkspaceSvg, getSourceAsJson: boolean) {
   }
 }
 
+/**
+ * Ensure that only a number may be entered.
+ * @param {string} text The user's text.
+ * @returns {?string} A string representing a valid number, or null if invalid.
+ *   Returns 0 for null or empty string.
+ * @static
+ */
+export function numberValidator(text: string): string | null {
+  text = text || '';
+  // TODO: Handle cases like 'ten', '1.203,14', etc.
+  // 'O' is sometimes mistaken for '0' by inexperienced users.
+  text = text.replace(/O/gi, '0');
+  // Strip out thousands separators.
+  text = text.replace(/,/g, '');
+  const n = parseFloat(text || '0');
+  return isNaN(n) ? null : String(n);
+}
+
+/**
+ * Ensure that only a nonnegative integer may be entered.
+ * @param {string} text The user's text.
+ * @returns {?string} A string representing a valid int, or null if invalid.
+ *   Returns '0' for negative numbers and null for truthy strings that do not contain numbers.
+ * @static
+ */
+export function nonnegativeIntegerValidator(text: string): string | null {
+  const n = numberValidator(text);
+  if (n) {
+    return String(Math.max(0, Math.floor(parseFloat(n))));
+  }
+  return n;
+}
 export function soundField(
   onClick: () => void,
   transformText?: (text: string) => string,
