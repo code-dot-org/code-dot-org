1a0678cf79358f480c40a7841eaf21f377ecad9e
Erin Bond <erin.bond@code.org>
2024-07-29 21:44:11 -0400

    remove unused import

--- apps/test/unit/templates/sectionsRefresh/AdvancedSettingTogglesTest.jsx
+++ apps/test/unit/templates/sectionsRefresh/AdvancedSettingTogglesTest.jsx
@@ -1,5 +1,4 @@
 import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import AdvancedSettingToggles from '@cdo/apps/templates/sectionsRefresh/AdvancedSettingToggles';
