587dbbec462f91a09b7accb426b750c740b42bfd
Dayne <dayne@code.org>
2024-07-31 14:53:15 -0700

    Removing the CPA exceptions added by p20-937

--- dashboard/test/factories/factories.rb
+++ dashboard/test/factories/factories.rb
@@ -436,15 +436,7 @@ FactoryBot.define do
         child_account_compliance_state_last_updated {DateTime.now}
       end
 
-      trait :before_p20_937_exception_date do
-        created_at {Cpa::CREATED_AT_EXCEPTION_DATE.ago(1.second)}
-      end
-
-      trait :p20_937_exception_date do
-        created_at {Cpa::CREATED_AT_EXCEPTION_DATE}
-      end
-
-      factory :cpa_non_compliant_student, traits: [:U13, :in_colorado, :p20_937_exception_date], aliases: %i[non_compliant_child] do
+      factory :cpa_non_compliant_student, traits: [:U13, :in_colorado], aliases: %i[non_compliant_child] do
         trait :predates_policy do
           created_at {Policies::ChildAccount.state_policies.dig('CO', :start_date).ago(1.second)}
         end
