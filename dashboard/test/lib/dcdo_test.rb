a6859c54dd045d99396375ab85c11388452141d0
Carl Reiner <carl.reiner@code.org>
2024-07-31 11:03:52 -0700

    add retry and timeout to dcdo test

--- dashboard/test/lib/dcdo_test.rb
+++ dashboard/test/lib/dcdo_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'dynamic_config/dcdo'
+require 'timeout'
 
 class DCDOTest < ActiveSupport::TestCase
   test 'basic set and get' do
@@ -19,7 +20,20 @@ class DCDOTest < ActiveSupport::TestCase
     }
     key = 'random'
     DCDO.set(key, to_store)
-    assert_equal DCDO.get(key, nil), to_store
+
+    result = nil
+    Timeout.timeout(10) do
+      loop do
+        result = DCDO.get(key, nil)
+        if result.present?
+          break
+        else
+          sleep 1
+        end
+      end
+    end
+
+    assert_equal result, to_store
   end
 
   test 'storing a non-jsonable object fails' do
