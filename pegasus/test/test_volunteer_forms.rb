6ab6761a92670f94a837920085f96b6777b94061
Mario Gil Correa <mario.gil-correa@code.org>
2024-08-01 15:51:34 -0500

    updated test for testing

--- pegasus/test/test_volunteer_forms.rb
+++ pegasus/test/test_volunteer_forms.rb
@@ -8,7 +8,7 @@ class VolunteerFormsTest < Minitest::Test
     old_locale = I18n.locale
     I18n.locale = 'en-US'
     en_experiences = VolunteerEngineerSubmission2015.experiences
-    I18n.locale = 'es-ES'
+    I18n.locale = 'fr-FR'
     es_experiences = VolunteerEngineerSubmission2015.experiences
     I18n.locale = old_locale
     refute_equal en_experiences['unspecified'], es_experiences['unspecified']
