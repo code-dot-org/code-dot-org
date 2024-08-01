f11733ef25befff7a792ba68aa1dd57e7b1f36a0
Turner Riley <turnertriley@gmail.com>
2024-08-01 11:44:00 -0700

    update js render file

--- apps/src/sites/studio/pages/pd/workshop_enrollment/students_cannot_enroll.js
+++ apps/src/sites/studio/pages/pd/workshop_enrollment/students_cannot_enroll.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 
 import WorkshopStudentEnrollPage from '@cdo/apps/lib/ui/simpleSignUp/workshop/WorkshopStudentEnrollPage';
 
-document.addEventListener(
-  'DOMContentLoaded',
+document.addEventListener('DOMContentLoaded', function () {
   ReactDOM.render(
     <WorkshopStudentEnrollPage />,
     document.getElementById('workshop-enroll-students-cannot-enroll')
-  )
-);
+  );
+});
