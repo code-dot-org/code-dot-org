7201eb0c3151e8310583e65664f9819a47b6229f
Turner Riley <turnertriley@gmail.com>
2024-08-01 11:25:14 -0700

    utilize new WorkshopAccountCard

--- apps/src/lib/ui/simpleSignUp/workshop/WorkshopStudentEnrollPage.tsx
+++ apps/src/lib/ui/simpleSignUp/workshop/WorkshopStudentEnrollPage.tsx
@@ -2,22 +2,37 @@ import React from 'react';
 
 import i18n from '@cdo/locale';
 
-import WorkshopKeepStudentAccountCard from './WorkshopKeepStudentAccountCard';
-import WorkshopSwitchToTeacherAccountCard from './WorkshopSwitchToTeacherAccountCard';
-import WorkshopWelcomeBanner from './WorkshopWelcomeBanner';
+import WorkshopAccountBanner from './WorkshopAccountBanner';
+import WorkshopAccountCard from './WorkshopAccountCard';
 
 import styles from '../link-account.module.scss';
 
 const WorkshopStudentEnrollPage: React.FunctionComponent = () => (
   <main>
     <div className={styles.contentContainer}>
-      <WorkshopWelcomeBanner
+      <WorkshopAccountBanner
         heading={i18n.accountNeedTeacherAccountWelcomeBannerHeaderLabel()}
         desc={i18n.accountNeedTeacherAccountWelcomeBannerHeaderDesc()}
       />
       <div className={styles.cardContainer}>
-        <WorkshopKeepStudentAccountCard />
-        <WorkshopSwitchToTeacherAccountCard />
+        <WorkshopAccountCard
+          id={'keep-student-account-card'}
+          icon={'child'}
+          title={i18n.accountKeepStudentAccountCardTitle()}
+          content={i18n.accountKeepStudentAccountCardContent()}
+          buttonText={i18n.accountKeepStudentAccountCardButton()}
+          buttonType="secondary"
+          href="/home"
+        />
+        <WorkshopAccountCard
+          id={'switch-to-teacher-account-card'}
+          icon={'chalkboard-user'}
+          title={i18n.accountSwitchTeacherAccountCardTitle()}
+          content={i18n.accountSwitchTeacherAccountCardContent()}
+          buttonText={i18n.accountSwitchTeacherAccountCardButton()}
+          buttonType="primary"
+          href="/users/edit"
+        />
       </div>
     </div>
   </main>
