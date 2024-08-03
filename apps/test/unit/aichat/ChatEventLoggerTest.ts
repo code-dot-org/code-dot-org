1620942e90cd2127019ae21f1b249b4414084e6a
Alice Fisher <alice.fisher@code.org>
2024-08-03 02:48:32 +0000

    use beforeEach, afterEach

--- apps/test/unit/aichat/ChatEventLoggerTest.ts
+++ apps/test/unit/aichat/ChatEventLoggerTest.ts
@@ -3,17 +3,21 @@ import sinon from 'ts-sinon';
 
 import * as aichatApi from '@cdo/apps/aichat/aichatApi';
 import ChatEventLogger from '@cdo/apps/aichat/chatEventLogger';
-import {ChatMessage} from '@cdo/apps/aichat/types';
+import {AichatContext, ChatMessage} from '@cdo/apps/aichat/types';
+import {Role} from '@cdo/apps/aiComponentLibrary/chatMessage/types';
 
 describe('ChatEventLogger', () => {
-  it('logChatEvent calls on postLogChatEvent', async () => {
-    const userChatMessage = {
-      role: 'user',
+  let userChatMessage: ChatMessage;
+  let aichatContext: AichatContext;
+
+  beforeEach(() => {
+    userChatMessage = {
+      role: Role.USER,
       chatMessageText: 'hello',
       status: 'OK',
       timestamp: Date.now(),
     };
-    const aichatContext = {
+    aichatContext = {
       currentLevelId: 123,
       scriptId: 321,
       channelId: 'abc123',
@@ -23,6 +27,13 @@ describe('ChatEventLogger', () => {
       chat_event_id: 1,
       chat_event: userChatMessage as ChatMessage,
     });
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('logChatEvent calls on postLogChatEvent', async () => {
     const chatEventLogger = ChatEventLogger.getInstance();
     expect(chatEventLogger).to.not.be.undefined;
     chatEventLogger.logChatEvent(userChatMessage as ChatMessage, aichatContext);
@@ -31,23 +42,6 @@ describe('ChatEventLogger', () => {
   });
 
   it('sendChatEvent not called when sendingInProgress is true', async () => {
-    const userChatMessage = {
-      role: 'user',
-      chatMessageText: 'hello',
-      status: 'OK',
-      timestamp: Date.now(),
-    };
-    const aichatContext = {
-      currentLevelId: 123,
-      scriptId: 321,
-      channelId: 'abc123',
-    };
-
-    sinon.stub(aichatApi, 'postLogChatEvent').resolves({
-      chat_event_id: 2,
-      chat_event: userChatMessage as ChatMessage,
-    });
-
     const chatEventLogger = ChatEventLogger.getInstance();
     chatEventLogger.setSendingInProgress(true);
     chatEventLogger.logChatEvent(userChatMessage as ChatMessage, aichatContext);
@@ -56,36 +50,9 @@ describe('ChatEventLogger', () => {
   });
 
   it('postLogChatEvent called twice when logChatEvent called twice', async () => {
-    const userChatMessage = {
-      role: 'user',
-      chatMessageText: 'hello',
-      status: 'OK',
-      timestamp: Date.now(),
-    };
-    const assistantChatMessage = {
-      role: 'assistant',
-      chatMessageText: 'How can I assist you?',
-      status: 'OK',
-      timestamp: Date.now(),
-    };
-    const aichatContext = {
-      currentLevelId: 123,
-      scriptId: 321,
-      channelId: 'abc123',
-    };
-
-    sinon.stub(aichatApi, 'postLogChatEvent').resolves({
-      chat_event_id: 3,
-      chat_event: userChatMessage as ChatMessage,
-    });
-
     const chatEventLogger = ChatEventLogger.getInstance();
     chatEventLogger.logChatEvent(userChatMessage as ChatMessage, aichatContext);
-    chatEventLogger.logChatEvent(
-      assistantChatMessage as ChatMessage,
-      aichatContext
-    );
+    chatEventLogger.logChatEvent(userChatMessage as ChatMessage, aichatContext);
     expect(aichatApi.postLogChatEvent).to.have.been.calledTwice;
-    sinon.restore();
   });
 });
