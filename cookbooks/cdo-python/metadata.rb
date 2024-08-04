1a14809385ce466d329a3ac06cb27e34c3604c9e
Seth Nickell <snickell@gmail.com>
2024-08-04 03:46:39 -1000

    cdo-python: bump verison to 0.1.1

--- cookbooks/cdo-python/metadata.rb
+++ cookbooks/cdo-python/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'will@code.org'
 license          'All rights reserved'
 description      'Installs Python'
 long_description File.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'apt'
