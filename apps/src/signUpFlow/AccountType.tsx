e0eddb85e395a2ca450e33b6b8d2cd8caa34c4f8
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2024-08-05 15:00:06 -0700

    add showLogo boolean to AccountBanner.tsx

--- apps/src/signUpFlow/AccountType.tsx
+++ apps/src/signUpFlow/AccountType.tsx
@@ -13,6 +13,7 @@ const AccountType: React.FunctionComponent = () => {
       <AccountBanner
         heading="Create your free account"
         desc="Start creating your free account by selecting the account type that best matches your needs."
+        showLogo={false}
       />
     </main>
   );
