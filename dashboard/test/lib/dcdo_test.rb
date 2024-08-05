8805548ed07243fbec78bda6271cd52dfb8afee7
Carl Reiner <carl.reiner@code.org>
2024-08-05 08:24:24 -0700

    use separate instance of DCDOBase

--- dashboard/test/lib/dcdo_test.rb
+++ dashboard/test/lib/dcdo_test.rb
@@ -2,14 +2,25 @@ require 'test_helper'
 require 'dynamic_config/dcdo'
 
 class DCDOTest < ActiveSupport::TestCase
+  setup do
+    # DCDO needs to be created using a unique cache key, so that it doesn't conflict
+    # with the "real" DCDO, which is cleared between tests in the test_helper.
+    @dcdo = DCDOBase.new(EnvironmentAwareDynamicConfigHelper.create_datastore_cache(SecureRandom.hex))
+  end
+
+  teardown do
+    # Since this is a separate instance of DCDOBase, we need to clear it here.
+    @dcdo.clear
+  end
+
   test 'basic set and get' do
-    assert_nil DCDO.get('key', nil)
-    DCDO.set('key', 'okay')
-    assert_equal DCDO.get('key', nil), 'okay'
+    assert_nil @dcdo.get('key', nil)
+    @dcdo.set('key', 'okay')
+    assert_equal @dcdo.get('key', nil), 'okay'
   end
 
   test 'returns default if key is not stored' do
-    assert_equal DCDO.get('UNSET_key', 123), 123
+    assert_equal @dcdo.get('UNSET_key', 123), 123
   end
 
   test 'storing hashes' do
@@ -18,8 +29,8 @@ class DCDOTest < ActiveSupport::TestCase
       'c' => [1, 2, 3]
     }
     key = 'random'
-    DCDO.set(key, to_store)
-    assert_equal DCDO.get(key, nil), to_store
+    @dcdo.set(key, to_store)
+    assert_equal @dcdo.get(key, nil), to_store
   end
 
   test 'storing a non-jsonable object fails' do
@@ -27,11 +38,11 @@ class DCDOTest < ActiveSupport::TestCase
     end
 
     assert_raises do
-      DCDO.set(key, RandomClass.new)
+      @dcdo.set(key, RandomClass.new)
     end
   end
 
   test 'frontend_config should return a hash' do
-    assert_instance_of(Hash, DCDO.frontend_config)
+    assert_instance_of(Hash, @dcdo.frontend_config)
   end
 end
