f4f86f5ad5e63b7bbe0fc2571d42875c842b3ea8
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2024-08-05 14:58:33 -0700

    set up AccountType.tsx page

---
+++ apps/src/signUpFlow/AccountType.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import AccountBanner from '@cdo/apps/templates/account/AccountBanner';
+// import i18n from '@cdo/locale';
+
+import './accountType.module.scss';
+
+// import i18n from '@cdo/locale';
+
+const AccountType: React.FunctionComponent = () => {
+  return (
+    <main>
+      <AccountBanner
+        heading="Create your free account"
+        desc="Start creating your free account by selecting the account type that best matches your needs."
+      />
+    </main>
+  );
+};
+
+export default AccountType;
--- apps/src/sites/studio/pages/devise/registrations/account_type.js
+++ apps/src/sites/studio/pages/devise/registrations/account_type.js
@@ -1,1 +1,9 @@
-console.log('I am on the account type page');
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AccountType from '@cdo/apps/signUpFlow/accountType';
+
+$(document).ready(function () {
+  ReactDOM.render(<AccountType />, document.getElementById('account-type'));
+});
