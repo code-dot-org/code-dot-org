218a202ae19d6e19b2d096331aaa4e7e3b941208
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2024-08-06 09:43:23 -0700

    add cards

--- apps/src/signUpFlow/AccountType.tsx
+++ apps/src/signUpFlow/AccountType.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
 
 import AccountBanner from '@cdo/apps/templates/account/AccountBanner';
+
+import AccountCard from '../templates/account/AccountCard';
 // import i18n from '@cdo/locale';
 
-import './accountType.module.scss';
+import styles from './accountType.module.scss';
 
 // import i18n from '@cdo/locale';
 
 const AccountType: React.FunctionComponent = () => {
   return (
-    <main>
-      <AccountBanner
-        heading="Create your free account"
-        desc="Start creating your free account by selecting the account type that best matches your needs."
-        showLogo={false}
-      />
+    <main className={styles.wrapper}>
+      <div className={styles.contentContainer}>
+        <AccountBanner
+          heading="Create your free account"
+          desc="Start creating your free account by selecting the account type that best matches your needs."
+          showLogo={false}
+        />
+        <div className={styles.cardContainer}>
+          <AccountCard
+            id={'student-card'}
+            icon={'child-reaching'}
+            title="I'm a Student"
+            content="Explore our courses and activities, plus:"
+            buttonText="Sign up as a student"
+            buttonType="primary"
+            href="#"
+          />
+          <AccountCard
+            id={'teacher-card'}
+            icon={'person-chalkboard'}
+            title="I'm a Teacher"
+            content="All student account features, plus:"
+            buttonText="Sign up as a teacher"
+            buttonType="primary"
+            href="#"
+          />
+        </div>
+      </div>
     </main>
   );
 };
