b128c6de5292c2adda47def2999e4a12ca3a9d41
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2024-08-06 13:00:16 -0700

    fix build error

--- apps/src/sites/studio/pages/devise/registrations/account_type.js
+++ apps/src/sites/studio/pages/devise/registrations/account_type.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AccountType from '@cdo/apps/signUpFlow/accountType';
+import AccountType from '@cdo/apps/signUpFlow/AccountType';
 
 $(document).ready(function () {
   ReactDOM.render(<AccountType />, document.getElementById('account-type'));
