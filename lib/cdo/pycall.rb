920957d1b3fc367a318ceebad8f91ac3e790226e
Seth Nickell <snickell@gmail.com>
2024-08-06 00:57:47 -1000

    Support installing local dev packages with -e, required calling site.addsitedir() manually.

--- lib/cdo/pycall.rb
+++ lib/cdo/pycall.rb
@@ -11,23 +11,24 @@ unless File.exist? ENV['PYTHON']
   raise "Python bin not found at #{ENV['PYTHON']}. Please run `pdm install` again."
 end
 
-# pycall.rb is following symlinks from the virtualenv, and finding the underlying
-# interpreter directory, which means its missing the site-packages directory for
-# our virtualenv. As a result, we specify it manually.
-ENV['PYTHONPATH'] = PythonVenv.site_packages_path
-
 require 'pycall'
 
 # Put `pyimport` and `pyfrom` methods in the global namespace.
 require 'pycall/import'
 include PyCall::Import
 
-# Do an import to ensure the Python interpreter is initialized.
-pyimport 'sys'
+# Python initializes its site-packages directory using site.py:
+# https://github.com/python/cpython/blob/main/Lib/site.py
+# Which looks for pyenv.cfg relative to sys.executable, and uses that to properly
+# initialize the site-packages directory. Because pycall.rb uses libpython,
+# sys.executable is set to the ruby interpreter, and site.py doesn't properly
+# add site-packages for our venv to the sys.path. So, we do it manually:
+pyimport 'site'
+site.addsitedir(PythonVenv.site_packages_path)
 
-# Now unset PYTHON, PYTHONPATH & PYTHONHOME so we don't mess up python3-using apps
+# Now unset PYTHON & PYTHONHOME so we don't mess up python3-using apps
 # launched from our Ruby processes (like the aws cli)
 # see: https://github.com/code-dot-org/code-dot-org/pull/60048#issuecomment-2267510208
-ENV.delete('PYTHONPATH')
+pyimport 'sys' # ensure libpython is already loaded before unsetting env vars
 ENV.delete('PYTHONHOME')
 ENV.delete('PYTHON')
