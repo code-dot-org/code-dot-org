c7cf1ad16cfc6e85b8b84d20b2b7d7b68db6e981
Kaitie O <kaitie@code.org>
2024-08-07 14:43:58 -0500

    NAV - Create sidebar (#60157)
    * initial commit
    
    * changes
    
    * add string
    
    * still missing header and dcdo flags
    
    * moved behind all the flags
    
    * removed header from nav playground
    
    * clean up
    
    * clean up useState
    
    * incorporated feedback
    
    * updated application file
    
    * update file based on merging in staging
    
    * try reverting application
    
    * revert refinement
    
    * final refinment
    
    * delete styles

---
+++ apps/src/templates/teacherNavigation/TeacherNavigationBar.tsx
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
+
+import {SimpleDropdown} from '@cdo/apps/componentLibrary/dropdown';
+import Typography from '@cdo/apps/componentLibrary/typography';
+import i18n from '@cdo/locale';
+
+import styles from './teacher-navigation.module.scss';
+
+interface SectionsData {
+  [sectionId: number]: {
+    name: string;
+    hidden: boolean;
+  };
+}
+
+const TeacherNavigationBar: React.FunctionComponent = () => {
+  const sections = useSelector(
+    (state: {teacherSections: {sections: SectionsData}}) =>
+      state.teacherSections.sections
+  );
+
+  const [sectionArray, setSectionArray] = useState<
+    {value: string; text: string}[]
+  >([]);
+  const [selectedSectionId, setSelectedSectionId] = useState<string>('');
+
+  useEffect(() => {
+    const updatedSectionArray = Object.entries(sections)
+      .filter(([id, section]) => !section.hidden)
+      .map(([id, section]) => ({
+        value: String(id),
+        text: section.name,
+      }));
+
+    setSectionArray(updatedSectionArray);
+
+    // Set initial selectedSectionId if not already set
+    if (updatedSectionArray.length > 0 && !selectedSectionId) {
+      setSelectedSectionId(updatedSectionArray[0].value);
+    }
+  }, [sections, selectedSectionId]);
+
+  return (
+    <nav className={styles.sidebarContainer}>
+      <div className={styles.sidebarContent}>
+        <Typography
+          semanticTag={'h2'}
+          visualAppearance={'overline-two'}
+          className={styles.sectionHeader}
+        >
+          {i18n.classSections()}
+        </Typography>
+        <SimpleDropdown
+          items={sectionArray}
+          onChange={value => setSelectedSectionId(value.target.value)}
+          labelText=""
+          size="m"
+          selectedValue={selectedSectionId}
+          name="section-dropdown"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default TeacherNavigationBar;
