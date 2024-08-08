245fe9d5b15d28854d7964cb5c2e31b185b4f28a
Erin Bond <erin.bond@code.org>
2024-08-08 09:37:19 -0400

    remove debugging statements

--- dashboard/app/helpers/aichat_safety_helper.rb
+++ dashboard/app/helpers/aichat_safety_helper.rb
@@ -1,9 +1,7 @@
 module AichatSafetyHelper
-  # Tyrone's EC2 instance
+  # Tyrone's EC2 instance- TODO update when we have a better long-term solution
   LLMGUARD_URL = "http://ec2-44-223-70-67.compute-1.amazonaws.com:8080/run-script"
   def self.get_llmguard_response(message)
-    puts
-    puts "message in get_llmguard_response: #{message}"
     headers = {
       "Content-Type" => "application/json",
       "Accept" => "application/json"
