05f77d7c5890e9c9b81478d06d9889c3b8f59485
Seth Nickell <snickell@gmail.com>
2024-08-08 22:01:16 -1000

    AiChatSafetyJob: always use :delayed_job

--- dashboard/app/jobs/aichat_safety_job.rb
+++ dashboard/app/jobs/aichat_safety_job.rb
@@ -1,21 +1,16 @@
 require "cdo/job_with_results"
 
 class AichatSafetyJob < JobWithResults
+  self.queue_adapter = :delayed_job
   queue_as :default
 
   @@timeout_s = 15
 
   def perform(prompt:)
-    results = PythonVenv.run_python_block(args_dict: {prompt: prompt}) do |args_dict|
-      <<~PYTHON
-        from pycdo.aichat.safety import is_prompt_safe
-        _return(is_prompt_safe(**#{args_dict}))
-      PYTHON
+    results = Python.run do
+      pyimport "pycdo.aichat.safety", as: :safety
+      safety.is_prompt_safe(prompt: prompt)
     end
-
-    # Eventually:
-    # pyimport "pycdo.aichat.safety", as: :safety
-    # safety.is_prompt_safe(prompt: prompt)
 
     write_results(results)
   end
