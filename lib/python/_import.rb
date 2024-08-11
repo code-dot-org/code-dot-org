d9c9224cce28aea004b2696fee5d1a50aaa2d537
Seth Nickell <snickell@gmail.com>
2024-08-10 22:50:42 -1000

    import => _import + add scary warnings about direct requiring

--- lib/python/_import.rb
+++ lib/python/_import.rb
@@ -1,5 +1,11 @@
-require 'pycall/import'
+## DO NOT REQUIRE THIS FILE FROM OUTSIDE THIS MODULE: NOT THREAD-SAFE ##
 
+require 'pycall/import'
+include PyCall::Import
+
+# Python::Import aliases PyCall::Import to ensure that pyimport and pyfrom
+# are only called from within a Python.run(&block). This is because they
+# are not thread-safe to invoke directly.
 module Python
   module Import
     include PyCall::Import
