1083200f1bf7930f0c4f90798e5663e76b90a1e8
wilkie <wilkie05@gmail.com>
2024-08-13 16:26:29 +0000

    Fixes LearningGoal component.

--- apps/src/templates/rubrics/LearningGoal.jsx
+++ apps/src/templates/rubrics/LearningGoal.jsx
@@ -298,6 +298,7 @@ export default function LearningGoal({
             submittedEvaluation={submittedEvaluation}
             isStudent={isStudent}
             isAutosaving={autosaveStatus === STATUS.IN_PROGRESS}
+            arrowPositionCallback={_ => {}}
           />
           {learningGoal.tips && !isStudent && (
             <div>
