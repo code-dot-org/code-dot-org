f477247fc219dc4530f2bd4afded46f67c6eee84
David Bailey <davidsbailey@users.noreply.github.com>
2024-08-14 13:27:35 -0700

    be more careful locating teacher before deleting progress

--- bin/oneoff/reset_student_progress_in_bulk/delete_user_progress_by_unit.rb
+++ bin/oneoff/reset_student_progress_in_bulk/delete_user_progress_by_unit.rb
@@ -17,7 +17,14 @@ require 'csv'
 csv_file_path = ARGV[1]
 
 teacher_id = ARGV[0]
-teacher_user = User.find_by(id: teacher_id) || User.find_by_email(teacher_id)
+teacher_user = nil
+if teacher_id.include?('@')
+  teacher_user = User.find_by_email(teacher_id)
+elsif teacher_id.to_i.to_s == teacher_id
+  teacher_user = User.find_by_id(teacher_id)
+else
+  raise "teacher id must be an id or email: #{teacher_id.inspect}"
+end
 raise "Teacher with id or email " + teacher_id.to_s.dump + " not found" if teacher_user.nil?
 
 rows = CSV.read(csv_file_path, headers: true)
