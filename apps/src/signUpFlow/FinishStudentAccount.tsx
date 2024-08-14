f9b5477adea80a88e55bb0cbd966b2f0fdedc70c
Turner Riley <turnertriley@gmail.com>
2024-08-14 09:50:59 -0700

    add more questions

--- apps/src/signUpFlow/FinishStudentAccount.tsx
+++ apps/src/signUpFlow/FinishStudentAccount.tsx
@@ -1,21 +1,37 @@
 import React, {useState} from 'react';
 
 import {Button, buttonColors} from '@cdo/apps/componentLibrary/button';
+import Checkbox from '@cdo/apps/componentLibrary/checkbox/Checkbox';
 import SimpleDropdown from '@cdo/apps/componentLibrary/dropdown/simpleDropdown';
 import TextField from '@cdo/apps/componentLibrary/textField/TextField';
 import {Heading2, BodyTwoText} from '@cdo/apps/componentLibrary/typography';
 import locale from '@cdo/apps/signup/locale';
 
 import {
+  IS_PARENT_SESSION_KEY,
   USER_NAME_SESSION_KEY,
   USER_AGE_SESSION_KEY,
+  USER_STATE_SESSION_KEY,
+  USER_GENDER_SESSION_KEY,
 } from './signUpFlowConstants';
 
 import style from './finish-account.module.scss';
 
 const FinishStudentAccount: React.FunctionComponent = () => {
+  const [isParent, setIsParent] = useState(false);
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
+  const [state, setState] = useState('');
+  const [gender, setGender] = useState('');
+
+  const onIsParentChange = (): void => {
+    const newIsParentCheckedChoice = !isParent;
+    setIsParent(newIsParentCheckedChoice);
+    sessionStorage.setItem(
+      IS_PARENT_SESSION_KEY,
+      `${newIsParentCheckedChoice}`
+    );
+  };
 
   const onNameChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const newName = e.target.value;
@@ -29,6 +45,18 @@ const FinishStudentAccount: React.FunctionComponent = () => {
     sessionStorage.setItem(USER_AGE_SESSION_KEY, newAge);
   };
 
+  const onStateChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    const newState = e.target.value;
+    setState(newState);
+    sessionStorage.setItem(USER_STATE_SESSION_KEY, newState);
+  };
+
+  const onGenderChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const newGender = e.target.value;
+    setGender(newGender);
+    sessionStorage.setItem(USER_GENDER_SESSION_KEY, newGender);
+  };
+
   return (
     <div className={style.finishAccountContainer}>
       <div className={style.headerTextContainer}>
@@ -36,6 +64,19 @@ const FinishStudentAccount: React.FunctionComponent = () => {
         <BodyTwoText>{locale.tailor_experience()}</BodyTwoText>
       </div>
       <div className={style.inputContainer}>
+        <span className={aaa}>
+          <div className={aaa}>
+            <Checkbox
+              label=""
+              checked={isParent}
+              onChange={onIsParentChange}
+              name="isParent"
+            />
+          </div>
+          <BodyTwoText className={aa} visualAppearance={'body-two'}>
+            {locale.i_am_a_parent_or_guardian()}
+          </BodyTwoText>
+        </span>
         <BodyTwoText visualAppearance={'heading-xs'}>
           {locale.display_name_eg()}
         </BodyTwoText>
@@ -60,6 +101,28 @@ const FinishStudentAccount: React.FunctionComponent = () => {
           selectedValue={age}
           onChange={onAgeChange}
           size="m"
+        />
+        <BodyTwoText visualAppearance={'heading-xs'}>
+          {locale.what_state_are_you_in()}
+        </BodyTwoText>
+        <SimpleDropdown
+          id="uitest-state-dropdown"
+          name="userState"
+          labelText=""
+          isLabelVisible={false}
+          items={[{value: 'WA', text: 'Washington'}, {value: 'CO', text: 'Colorado'}]}
+          selectedValue={state}
+          onChange={onStateChange}
+          size="m"
+        />
+        <BodyTwoText visualAppearance={'heading-xs'}>
+          {locale.what_is_your_gender()}
+        </BodyTwoText>
+        <TextField
+          name="userGender"
+          onChange={onGenderChange}
+          value={gender}
+          placeholder={locale.female()}
         />
       </div>
       <div className={style.finishSignUpButtonContainer}>
