106397f9d507646cceb35541a120f215c3b3b1d2
Seth Nickell <snickell@gmail.com>
2024-08-14 22:28:25 -1000

    config/initializers/script_preload.rb: fix uninitialized constant Unit (NameError)

--- dashboard/config/initializers/script_preload.rb
+++ dashboard/config/initializers/script_preload.rb
@@ -2,16 +2,18 @@
 # This speeds up load time of new Unicorn child worker processes
 # and Spring application preloader (Rails console, unit tests).
 
-# Skip if this is running a Rake task (e.g. rake db:setup) or when caching is disabled
-if File.basename($0) != 'rake' &&
-    Unit.should_cache? &&
-    !ENV['SKIP_SCRIPT_PRELOAD']
-  # Populate the shared in-memory cache from the database.
-  Unit.unit_cache_to_cache unless Rails.cache.is_a?(ActiveSupport::Cache::MemoryStore)
-  Unit.script_cache
-  Unit.script_level_cache
-  Unit.level_cache
-  Unit.unit_family_cache
-  UnitGroup.course_cache_to_cache unless Rails.cache.is_a?(ActiveSupport::Cache::MemoryStore)
-  UnitGroup.course_cache
+Rails.application.config.after_initialize do
+  # Skip if this is running a Rake task (e.g. rake db:setup) or when caching is disabled
+  if File.basename($0) != 'rake' &&
+      Unit.should_cache? &&
+      !ENV['SKIP_SCRIPT_PRELOAD']
+    # Populate the shared in-memory cache from the database.
+    Unit.unit_cache_to_cache unless Rails.cache.is_a?(ActiveSupport::Cache::MemoryStore)
+    Unit.script_cache
+    Unit.script_level_cache
+    Unit.level_cache
+    Unit.unit_family_cache
+    UnitGroup.course_cache_to_cache unless Rails.cache.is_a?(ActiveSupport::Cache::MemoryStore)
+    UnitGroup.course_cache
+  end
 end
