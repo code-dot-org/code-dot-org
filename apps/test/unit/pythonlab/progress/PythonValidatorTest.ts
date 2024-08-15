3c0732b3fe1a71c48130c13b41f2fc5d7186ac82
Molly Moen <molly@code.org>
2024-08-15 14:20:07 -0700

    Integrate ProgressManager with Python Lab (#60422)
    * work in progress
    
    * update package
    
    * figure out if all passed
    
    * first attempt
    
    * better architecture
    
    * clean up
    
    * updates
    
    * work in progress
    
    * improvements
    
    * add tests
    
    * clean up
    
    * clean up boolean logic and add tests
    
    * update unittest_runner package

---
+++ apps/test/unit/pythonlab/progress/PythonValidatorTest.ts
@@ -0,0 +1,59 @@
+import {Condition} from '@cdo/apps/lab2/types';
+import PythonValidationTracker from '@cdo/apps/pythonlab/progress/PythonValidationTracker';
+import PythonValidator from '@cdo/apps/pythonlab/progress/PythonValidator';
+
+describe('PythonValidator', () => {
+  const PASSED_TESTS = [
+    new Map<string, string>([
+      ['name', 'test1'],
+      ['result', 'PASS'],
+    ]),
+    new Map<string, string>([
+      ['name', 'test2'],
+      ['result', 'PASS'],
+    ]),
+  ];
+
+  const SOME_FAILED_TESTS = [
+    new Map<string, string>([
+      ['name', 'test1'],
+      ['result', 'PASS'],
+    ]),
+    new Map<string, string>([
+      ['name', 'test2'],
+      ['result', 'FAIL'],
+    ]),
+  ];
+
+  const PASSED_TESTS_CONDITION: Condition[] = [
+    {
+      name: 'PASSED_ALL_TESTS',
+    },
+  ];
+
+  it('should meet all passed condition if all tests pass', () => {
+    const validationTracker = new PythonValidationTracker();
+    const validator = new PythonValidator(validationTracker);
+    validationTracker.setTestResults(PASSED_TESTS);
+    expect(validator.conditionsMet(PASSED_TESTS_CONDITION)).toBe(true);
+  });
+
+  it('should not meet all passed condition if some tests failed', () => {
+    const validationTracker = new PythonValidationTracker();
+    const validator = new PythonValidator(validationTracker);
+    validationTracker.setTestResults(SOME_FAILED_TESTS);
+    expect(validator.conditionsMet(PASSED_TESTS_CONDITION)).toBe(false);
+  });
+
+  it('should not meet all passed condition if tests results are null', () => {
+    const validationTracker = new PythonValidationTracker();
+    const validator = new PythonValidator(validationTracker);
+    expect(validator.conditionsMet(PASSED_TESTS_CONDITION)).toBe(false);
+  });
+
+  it('should meet all passed condition if test results are an empty list', () => {
+    const validationTracker = new PythonValidationTracker();
+    const validator = new PythonValidator(validationTracker);
+    expect(validator.conditionsMet(PASSED_TESTS_CONDITION)).toBe(false);
+  });
+});
