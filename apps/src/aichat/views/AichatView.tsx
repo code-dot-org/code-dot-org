d54bc29ecf8beee96f96ea1072934d8e9484b097
Cassi Brenci <cassi.brenci@code.org>
2024-08-15 12:42:34 -0700

    Remove extra comments. Since light/dark mode is based on lab type, we don't need to support switching between them for AichatView at this time.

--- apps/src/aichat/views/AichatView.tsx
+++ apps/src/aichat/views/AichatView.tsx
@@ -158,7 +158,7 @@ const AichatView: React.FunctionComponent = () => {
   const chatWorkspaceHeader = (
     <div>
       {projectTemplateLevel && (
-        <ProjectTemplateWorkspaceIcon tooltipPlace="bottom" dark /> // todo: dark should be conditional based on the theme (light/dark)
+        <ProjectTemplateWorkspaceIcon tooltipPlace="bottom" dark />
       )}
       {viewMode === ViewMode.EDIT
         ? aichatI18n.aichatWorkspaceHeader()
@@ -285,7 +285,7 @@ const renderModelCustomizationHeaderRight = (onStartOver: () => void) => {
       <Button
         icon={{iconStyle: 'solid', iconName: 'refresh'}}
         isIconOnly={true}
-        color={'black'} // todo: This should be conditional based on the theme (light/dark)
+        color={'black'}
         onClick={onStartOver}
         ariaLabel={'Start Over'}
         size={'xs'}
