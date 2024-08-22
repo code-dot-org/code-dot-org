fbd01410bfb93d114772e679838d79cdfd118280
Seth Nickell <snickell@gmail.com>
2024-08-22 03:37:22 -1000

    test_helper.rb: remove redundant invocations of check_pending! see: https://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html#changes-to-test-helper

--- dashboard/test/test_helper.rb
+++ dashboard/test/test_helper.rb
@@ -1,9 +1,5 @@
 require 'test_reporter'
 
-if defined? ActiveRecord
-  ActiveRecord::Migration&.check_pending!
-end
-
 # This is a workaround for https://github.com/kern/minitest-reporters/issues/230
 Minitest.load_plugins
 Minitest.extensions.delete('rails')
@@ -59,8 +55,6 @@ require 'testing/rspec_mocks'
 require 'parallel_tests/test/runtime_logger'
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
-
   setup do
     AWS::S3.stubs(:upload_to_bucket).raises("Don't actually upload anything to S3 in tests... mock it if you want to test it")
     AWS::S3.stubs(:download_from_bucket).raises("Don't actually download anything to S3 in tests... mock it if you want to test it")
