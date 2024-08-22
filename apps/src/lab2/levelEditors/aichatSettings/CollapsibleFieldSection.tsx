846a1e337de28f2284e74d731fcd40f6168c8794
Alice Fisher <alice.fisher@code.org>
2024-08-22 22:53:39 +0000

    remove redundancy in props

--- apps/src/lab2/levelEditors/aichatSettings/CollapsibleFieldSection.tsx
+++ apps/src/lab2/levelEditors/aichatSettings/CollapsibleFieldSection.tsx
@@ -7,30 +7,13 @@ import FieldSection, {FieldSectionProps} from './FieldSection';
 import moduleStyles from './edit-aichat-settings.module.scss';
 
 const CollapsibleFieldSection: React.FunctionComponent<
-  FieldSectionProps & {labelText: string}
-> = ({
-  labelText,
-  fieldName,
-  description,
-  inputType,
-  inputNode,
-  min,
-  max,
-  step,
-}) => {
+  {labelText: string} & FieldSectionProps
+> = ({labelText, ...rest}) => {
   return (
     <div className={moduleStyles.collapsibleFieldSection}>
       <hr />
       <CollapsibleSection headerContent={labelText}>
-        <FieldSection
-          fieldName={fieldName}
-          description={description}
-          inputType={inputType}
-          inputNode={inputNode}
-          min={min}
-          max={max}
-          step={step}
-        />
+        <FieldSection {...rest} />
       </CollapsibleSection>
     </div>
   );
