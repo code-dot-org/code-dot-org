daadecd56e166ceed1e0b96b248ecfe367140855
Liam Frye-Mason <liam.fryemason@code.org>
2024-08-23 15:41:46 -0700

    Fix latest_stable_version and recommended for unit and unit_group

--- dashboard/app/models/course_version.rb
+++ dashboard/app/models/course_version.rb
@@ -145,12 +145,28 @@ class CourseVersion < ApplicationRecord
     end
   end
 
+  def latest_stable_version(family_name, locale: 'en-us')
+    latest_stable_unit = Unit.latest_stable_version(family_name, locale: locale_code)
+    latest_stable_unitgroup = UnitGroup.latest_stable_version(family_name, locale: locale_code)
+
+    if latest_stable_unit && !latest_stable_unitgroup
+      return latest_stable_unit
+    elsif !latest_stable_unit && latest_stable_unitgroup
+      return latest_stable_unitgroup
+    elsif latest_stable_unit && latest_stable_unitgroup
+      return latest_stable_unit.version_year > latest_stable_unitgroup.version_year ? latest_stable_unit : latest_stable_unitgroup
+    end
+
+    nil
+  end
+
   def recommended?(locale_code = 'en-us')
     return false unless stable?
     return true if course_offering.course_versions.length == 1
 
     family_name = course_offering.key
-    latest_stable_version = content_root_type == 'UnitGroup' ? UnitGroup.latest_stable_version(family_name, locale: locale_code) : Unit.latest_stable_version(family_name, locale: locale_code)
+
+    latest_stable_version = latest_stable_version(family_name, locale: locale_code)
 
     latest_stable_version == content_root
   end
