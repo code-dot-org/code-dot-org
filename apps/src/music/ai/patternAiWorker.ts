0c7b37a410f10d215cd7aa86b3410ae3b7502205
breville <brendan@code.org>
2024-08-23 19:01:08 +0000

    Update

--- apps/src/music/ai/patternAiWorker.ts
+++ apps/src/music/ai/patternAiWorker.ts
@@ -7,6 +7,18 @@ import {PatternTickEvent} from '../player/interfaces/PatternEvent';
 
 let model: MusicRNN | undefined = undefined;
 
+// This component is adapted from Neural Drum Machine by Tero Parviainen.
+//
+// Its license is reproduced here:
+//
+// Copyright (c) 2024 by Tero Parviainen (https://codepen.io/teropa/pen/JLjXGK)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 const midiDrums = [36, 38, 42, 46, 41, 43, 45, 49, 51];
 const reverseMidiMapping = new Map([
   [36, 0],
