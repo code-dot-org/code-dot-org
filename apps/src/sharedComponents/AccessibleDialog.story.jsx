47e2eef970635b316dc461851e8a96860129c038
Mario Gil Correa <mario.gil-correa@code.org>
2024-08-23 12:37:52 -0500

    updated stories

--- apps/src/sharedComponents/AccessibleDialog.story.jsx
+++ apps/src/sharedComponents/AccessibleDialog.story.jsx
@@ -1,3 +1,4 @@
+import {action} from '@storybook/addon-actions';
 import React from 'react';
 
 import AccessibleDialog from './AccessibleDialog';
@@ -12,41 +13,79 @@ export default {
   },
 };
 
+// Add custom CSS
+const customStyle = document.createElement('style');
+customStyle.innerHTML = `
+  .custom-modal-style {
+    color: #fff;
+    font-size: 26px;
+    background-color: rgb(86 96 101);
+    border: solid 1px rgb(121 124 128);
+    p {
+      font-size: 30px;
+      font-family: "Barlow Semi Condensed Medium", sans-serif;
+    }
+  }
+
+  .custom-backdrop-style {
+    background-color: #000;
+    opacity: 0.3;
+    z-index: 1250;
+  }
+
+  .custom-close-button-style {
+    background: #fff;
+    i {
+      color: #121212;
+    }
+  }
+`;
+document.head.appendChild(customStyle);
+
 const Template = args => <AccessibleDialog {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   children: <div>Default Accessible Dialog</div>,
-  onClose: () => {},
+  onClose: action('closed'),
+  onDismiss: undefined,
 };
 
 export const CustomStyles = Template.bind({});
 CustomStyles.args = {
+  children: (
+    <div>
+      <p>Custom Styled Accessible Dialog</p>
+    </div>
+  ),
+  onClose: action('closed'),
+  onDismiss: undefined,
   styles: {
     modal: defaultStyle.modal + ' custom-modal-style',
     modalBackdrop: defaultStyle.modalBackdrop + ' custom-backdrop-style',
-    xCloseButton: defaultStyle.xCloseButton + ' custom-close-icon-style',
+    xCloseButton: defaultStyle.xCloseButton + ' custom-close-button-style',
   },
-  children: <div>Custom Styled Accessible Dialog</div>,
-  onClose: () => {},
 };
 
 export const WithOnDismiss = Template.bind({});
 WithOnDismiss.args = {
   children: <div>Accessible Dialog with onDismiss</div>,
-  onDismiss: () => {},
+  onClose: action('closed'),
+  onDismiss: action('dismissed'),
 };
 
 export const WithoutInitialFocus = Template.bind({});
 WithoutInitialFocus.args = {
   children: <div>Accessible Dialog without initial focus</div>,
-  onClose: () => {},
   initialFocus: false,
+  onClose: action('closed'),
+  onDismiss: undefined,
 };
 
 export const ClickBackdropToClose = Template.bind({});
 ClickBackdropToClose.args = {
   children: <div>Accessible Dialog with backdrop click to close</div>,
-  onClose: () => {},
   closeOnClickBackdrop: true,
+  onClose: action('closed'),
+  onDismiss: undefined,
 };
