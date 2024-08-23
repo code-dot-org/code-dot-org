a770eb58bece72c3809c2c31f99ac1dcd12a6991
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-08-23 16:12:52 -0700

    don't try to fix the attribute method shadowing just yet

--- dashboard/app/models/concerns/serialized_properties.rb
+++ dashboard/app/models/concerns/serialized_properties.rb
@@ -15,14 +15,14 @@ module SerializedProperties
 
   def assign_attributes(new_attributes)
     init_properties
-    new_attributes.stringify_keys!
-    new_properties = new_attributes.delete('properties').try(:stringify_keys)
+    attributes = new_attributes.stringify_keys
+    new_properties = attributes.delete('properties').try(:stringify_keys)
 
-    super(new_attributes)
+    super(attributes)
 
     # If the properties hash is explicitly assigned, merge its keys with
     # existing properties instead of replacing the entire hash. Make sure to
-    # invoke `init_properties` again just in case the previous `super` call
+    # invoke `init_properties` again first, in case the previous `super` call
     # reset the attributes cache (see refresh_activemodel_attributes_cache.rb).
     init_properties
     super(properties: properties.merge(new_properties)) if new_properties
