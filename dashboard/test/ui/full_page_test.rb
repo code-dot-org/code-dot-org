f6087d90edbd15e03820a82faadc1458e05acb0c
Seth Nickell <snickell@gmail.com>
2024-08-26 04:41:32 -1000

    full_page_test.rb: rubocop

--- dashboard/test/ui/full_page_test.rb
+++ dashboard/test/ui/full_page_test.rb
@@ -11,26 +11,20 @@ sauce_url = "http://#{CDO.saucelabs_username}:#{CDO.saucelabs_authkey}@ondemand.
 driver = Selenium::WebDriver.for(:remote,
   url: sauce_url,
   capabilities: Selenium::WebDriver::Remote::Capabilities.new({
-    "browserName": "chrome",
-    "browserVersion": "105",
-    "platformName": "Windows 10",
-    "sauce:options": {
-      "screenResolution": "1280x1024",
-      "extendedDebugging": true,
-      "seleniumVersion": Selenium::WebDriver::VERSION,
+                                                                'browserName': "chrome",
+    'browserVersion': "105",
+    'platformName': "Windows 10",
+    'sauce:options': {
+      'screenResolution': "1280x1024",
+      'extendedDebugging': true,
+      'seleniumVersion': Selenium::WebDriver::VERSION,
       idleTimeout: 60,
     }
-  }),
+                                                              }
+),
 )
 
-begin
-  eyes.open(driver: driver, app_name: 'Google News', test_name: 'Google News Full Page Snapshot')
-  driver.get 'https://slashdot.org'
-
-  stitch_mode = "none"
-  identifier = "initial load"
-
-  #####################
+def then_i_see_no_difference_for(identifier, stitch_mode)
   if stitch_mode == "none"
     eyes.force_full_page_screenshot = false
   else
@@ -39,10 +33,25 @@ begin
       Applitools::STITCH_MODE[:css]
   end
 
-  eyes.check_window(identifier, 5, false)
+  # eyes.check_window(identifier + "-this doesn't work", 5, false)
+  eyes.check_window(identifier + "-this doesn't work", 5)
+
   eyes.force_full_page_screenshot = true
   eyes.stitch_mode = Applitools::STITCH_MODE[:css]
-  #####################
+end
+
+begin
+  eyes.open(driver: driver, app_name: 'Google News', test_name: 'Google News Full Page Snapshot')
+  driver.get 'https://slashdot.org'
+
+  identifier = "initial load"
+
+  eyes.check(identifier + "-this works", Applitools::Selenium::Target.window.fully)
+
+  then_i_see_no_difference_for(identifier)
+  then_i_see_no_difference_for(identifier + "-none", "none")
+  then_i_see_no_difference_for(identifier + "-scroll", "scroll")
+  then_i_see_no_difference_for(identifier + "-css", "css")
 
   eyes.close
 ensure
