c4c7185638bd7c9a33f4e86e5e5eec62240d5cb6
Darin Webb <darin@code.org>
2024-08-26 10:48:54 -0500

    WIP testing autoscaling

--- lib/cdo/aws/ec2_autoscaling.rb
+++ lib/cdo/aws/ec2_autoscaling.rb
@@ -2,7 +2,7 @@ require 'aws-sdk-autoscaling'
 
 module AWS
   class EC2Autoscaling
-    def self.refresh_instances(group_name, wait: false)
+    def self.refresh_instances_in_group(group_name, wait: false)
       asg = Aws::AutoScaling::Client.new
 
       groups_matching_name = asg.describe_auto_scaling_groups(auto_scaling_group_names: [group_name]).auto_scaling_groups
@@ -13,6 +13,26 @@ module AWS
 
       return unless wait
       asg.wait_until(:instances_healthy, auto_scaling_group_names: [group_name])
+    end
+
+    # Returns the name of the autoscaling group for the current environment.
+    def self.get_autoscaling_group_for_current_environment
+      stack_name = CDO.stack_name
+      logical_id = 'Frontends'
+
+      puts "fetching autoscaling group for stack: #{stack_name} and logical id: #{logical_id}"
+
+      asg_client = Aws::AutoScaling::Client.new
+
+      group = asg_client.describe_auto_scaling_groups.auto_scaling_groups.find do |g|
+        has_stack_name = g.tags.any? {|tag| tag.key == 'aws:cloudformation:stack-name' && tag.value == stack_name}
+        has_logical_id = g.tags.any? {|tag| tag.key == 'aws:cloudformation:logical-id' && tag.value == logical_id}
+        has_stack_name && has_logical_id
+      end
+
+      raise "No AutoScaling group found for stack \"#{stack_name}\" and logical id \"#{logical_id}\"" unless group
+
+      group&.auto_scaling_group_name
     end
   end
 end
--- lib/test/cdo/aws/test_ec2_autoscaling.rb
+++ lib/test/cdo/aws/test_ec2_autoscaling.rb
@@ -26,22 +26,22 @@ class TestEC2Autoscaling < Minitest::Test
     return groups
   end
 
-  def test_refresh_instances_raises_when_no_groups_found
+  def test_refresh_instances_in_group_raises_when_no_groups_found
     empty_response = Aws::AutoScaling::Types::AutoScalingGroupsType.new(auto_scaling_groups: [])
     Aws::AutoScaling::Client.any_instance.stubs(:describe_auto_scaling_groups).with(auto_scaling_group_names: ['nonexistent-group']).returns(empty_response)
 
-    assert_raises(RuntimeError) {AWS::EC2Autoscaling.refresh_instances('nonexistent-group')}
+    assert_raises(RuntimeError) {AWS::EC2Autoscaling.refresh_instances_in_group('nonexistent-group')}
   end
 
-  def test_refresh_instances_raises_when_too_many_groups_found
+  def test_refresh_instances_in_group_raises_when_too_many_groups_found
     # The implementation searches for the group by name, which returns an array. Lets make sure it's only got one item.
     multiple_response = Aws::AutoScaling::Types::AutoScalingGroupsType.new(auto_scaling_groups: mock_autoscaling_groups(2))
     Aws::AutoScaling::Client.any_instance.expects(:describe_auto_scaling_groups).with(auto_scaling_group_names: ['overloaded-group']).returns(multiple_response)
 
-    assert_raises(RuntimeError) {AWS::EC2Autoscaling.refresh_instances('overloaded-group')}
+    assert_raises(RuntimeError) {AWS::EC2Autoscaling.refresh_instances_in_group('overloaded-group')}
   end
 
-  def test_refresh_instances_triggers_instance_refresh_with_default_parameters
+  def test_refresh_instances_in_group_triggers_instance_refresh_with_default_parameters
     # The implementation should call describe_auto_scaling_groups with the given group name.
     describe_response = Aws::AutoScaling::Types::AutoScalingGroupsType.new(auto_scaling_groups: mock_autoscaling_groups)
     Aws::AutoScaling::Client.any_instance.expects(:describe_auto_scaling_groups).with(auto_scaling_group_names: ['test-group']).returns(describe_response)
@@ -53,24 +53,31 @@ class TestEC2Autoscaling < Minitest::Test
       params[:auto_scaling_group_name] == 'test-group' && params.keys.length == 1
     end
 
-    AWS::EC2Autoscaling.refresh_instances('test-group')
+    AWS::EC2Autoscaling.refresh_instances_in_group('test-group')
   end
 
-  def test_refresh_instances_does_not_wait_for_instance_refresh_to_complete_by_default
+  def test_refresh_instances_in_group_does_not_wait_for_instance_refresh_to_complete_by_default
     describe_response = Aws::AutoScaling::Types::AutoScalingGroupsType.new(auto_scaling_groups: mock_autoscaling_groups)
     Aws::AutoScaling::Client.any_instance.stubs(:describe_auto_scaling_groups).returns(describe_response)
     Aws::AutoScaling::Client.any_instance.expects(:start_instance_refresh).with(auto_scaling_group_name: 'test-group')
 
     Aws::AutoScaling::Client.any_instance.expects(:wait_until).never
-    AWS::EC2Autoscaling.refresh_instances('test-group')
+    AWS::EC2Autoscaling.refresh_instances_in_group('test-group')
   end
 
-  def test_refresh_instances_waits_until_instance_refresh_is_complete_if_flag_passed
+  def test_refresh_instances_in_group_waits_until_instance_refresh_is_complete_if_flag_passed
     describe_response = Aws::AutoScaling::Types::AutoScalingGroupsType.new(auto_scaling_groups: mock_autoscaling_groups)
     Aws::AutoScaling::Client.any_instance.stubs(:describe_auto_scaling_groups).returns(describe_response)
     Aws::AutoScaling::Client.any_instance.expects(:start_instance_refresh).with(auto_scaling_group_name: 'test-group')
 
     Aws::AutoScaling::Client.any_instance.expects(:wait_until).with(:instances_healthy, auto_scaling_group_names: ['test-group'])
-    AWS::EC2Autoscaling.refresh_instances('test-group', wait: true)
+    AWS::EC2Autoscaling.refresh_instances_in_group('test-group', wait: true)
+  end
+
+  def test_get_autoscaling_group_for_current_environment_raises_when_no_matching_group_found
+    empty_response = Aws::AutoScaling::Types::AutoScalingGroupsType.new(auto_scaling_groups: [])
+    Aws::AutoScaling::Client.any_instance.stubs(:describe_auto_scaling_groups).returns(empty_response)
+
+    assert_raises(RuntimeError) {AWS::EC2Autoscaling.get_autoscaling_group_for_current_environment}
   end
 end
