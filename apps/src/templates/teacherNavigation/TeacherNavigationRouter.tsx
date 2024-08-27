6b184d06fcf504365f0fda5e4f76610de006c2c8
Kaitie O <kaitie@code.org>
2024-08-27 09:37:51 -0500

    NAV - settings page working (#60616)
    * settings page working
    
    * modify data

--- apps/src/templates/teacherNavigation/TeacherNavigationRouter.tsx
+++ apps/src/templates/teacherNavigation/TeacherNavigationRouter.tsx
@@ -16,6 +16,7 @@ import SectionProjectsListWithData from '../projects/SectionProjectsListWithData
 import SectionAssessments from '../sectionAssessments/SectionAssessments';
 import StandardsReport from '../sectionProgress/standards/StandardsReport';
 import SectionProgressSelector from '../sectionProgressV2/SectionProgressSelector';
+import SectionsSetUpContainer from '../sectionsRefresh/SectionsSetUpContainer';
 import SectionLoginInfo from '../teacherDashboard/SectionLoginInfo';
 import StatsTableWithData from '../teacherDashboard/StatsTableWithData';
 import {sectionProviderName} from '../teacherDashboard/teacherSectionsRedux';
@@ -258,13 +259,12 @@ const TeacherNavigationRouter: React.FC<TeacherNavigationRouterProps> = ({
         />
         <Route
           path={TEACHER_NAVIGATION_PATHS.settings}
-          element={
-            <ElementOrEmptyPage
-              showNoStudents={studentCount === 0}
-              showNoCurriculumAssigned={!anyStudentHasProgress}
-              element={applyV1TeacherDashboardWidth(<TemporaryBlankPage />)}
+          element={applyV1TeacherDashboardWidth(
+            <SectionsSetUpContainer
+              isUsersFirstSection={false}
+              sectionToBeEdited={selectedSection}
             />
-          }
+          )}
         />
         {showAITutorTab && (
           <Route
