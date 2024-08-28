1430bf79367d94b92246d2537808f96c21e0ff67
Ben Brooks <ben@code.org>
2024-08-28 16:24:56 -0700

    Pull out repeated dashbaord config that is repeated per model, untested script to create dashboard using it

---
+++ apps/script/createNewAichatCloudwatchDashboard.js
@@ -0,0 +1,19 @@
+const {
+  CloudWatchClient,
+  PutDashboardCommand,
+} = require('@aws-sdk/client-cloudwatch');
+
+const dashboardConfig = require('./singleAichatModelDashboardConfig.json');
+
+const putDashboard = async () => {
+  const client = new CloudWatchClient({region: 'us-east-1'});
+  const input = {
+    DashboardName: 'GenAIBenTest',
+    DashboardBody: JSON.stringify(dashboardConfig),
+  };
+  const command = new PutDashboardCommand(input);
+  const response = await client.send(command);
+  console.log(response);
+};
+
+putDashboard();
