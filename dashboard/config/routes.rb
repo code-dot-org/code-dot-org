fb22a813aa8fbd7e8b0cab71fc24eeb248a3a8a3
Turner Riley <56283563+TurnerRiley@users.noreply.github.com>
2024-08-28 12:41:10 -0700

    Fixes the order of the new signup flow pages (#60698)
    * fix order
    
    * update tests

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -190,9 +190,8 @@ Dashboard::Application.routes.draw do
       get '/oauth_sign_out/:provider', to: 'sessions#oauth_sign_out', as: :oauth_sign_out
       post '/users/begin_sign_up', to: 'registrations#begin_sign_up'
       post '/users/finish_sign_up', to: 'registrations#new'
-      get '/users/new_sign_up', to: 'registrations#new_sign_up'
-      # Part of the new sign up flow - work in progress
       get '/users/new_sign_up/account_type', to: 'registrations#account_type'
+      get '/users/new_sign_up/login_type', to: 'registrations#login_type'
       get '/users/new_sign_up/finish_student_account', to: 'registrations#finish_student_account'
       get '/users/new_sign_up/finish_teacher_account', to: 'registrations#finish_teacher_account'
       patch '/dashboardapi/users', to: 'registrations#update'
