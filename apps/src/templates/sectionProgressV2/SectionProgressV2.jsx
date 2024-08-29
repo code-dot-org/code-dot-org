678288c837e7ea738564cb7e612b1755ae545b03
Kaitie O <kaitie@code.org>
2024-08-29 14:03:32 -0500

    Revert "NAV - fix top left corner alignment (#60692)"
    This reverts commit 77b368d63434fe07c5537d63bcb85ef9bb28ec91.

--- apps/src/templates/sectionProgressV2/SectionProgressV2.jsx
+++ apps/src/templates/sectionProgressV2/SectionProgressV2.jsx
@@ -30,7 +30,6 @@ function SectionProgressV2({
   isLevelProgressLoaded,
   expandedLessonIds,
   loadExpandedLessonsFromLocalStorage,
-  hideTopHeading,
 }) {
   React.useEffect(() => {
     loadExpandedLessonsFromLocalStorage(scriptId, sectionId);
@@ -60,7 +59,7 @@ function SectionProgressV2({
 
   return (
     <div className={styles.progressV2Page} data-testid="section-progress-v2">
-      {!hideTopHeading && <Heading1>{i18n.progressBeta()}</Heading1>}
+      <Heading1>{i18n.progressBeta()}</Heading1>
       <IconKey
         isViewingValidatedLevel={isViewingValidatedLevel}
         expandedLessonIds={expandedLessonIds}
@@ -89,7 +88,6 @@ SectionProgressV2.propTypes = {
   isLevelProgressLoaded: PropTypes.bool.isRequired,
   expandedLessonIds: PropTypes.array,
   loadExpandedLessonsFromLocalStorage: PropTypes.func.isRequired,
-  hideTopHeading: PropTypes.bool,
 };
 
 export default connect(
