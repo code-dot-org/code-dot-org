649849f6647c622815bd0693636dd9c37714f091
denyslevada <levada.denys@gmail.com>
2024-09-03 15:26:01 +0300

    * move skeletonize-content.module.scss from componentLibrary to sharedComponents since it's not a part of DSCO

--- apps/src/templates/teacherDashboard/OwnedSectionsTable.jsx
+++ apps/src/templates/teacherDashboard/OwnedSectionsTable.jsx
@@ -23,7 +23,7 @@ import SectionActionDropdown from './SectionActionDropdown';
 import {sortableSectionShape} from './shapes';
 import {getSectionRows} from './teacherSectionsRedux';
 
-import skeletonizeContent from '@cdo/apps/componentLibrary/skeletonize-content.module.scss';
+import skeletonizeContent from '@cdo/apps/sharedComponents/skeletonize-content.module.scss';
 
 /** @enum {number} */
 export const COLUMNS = {
