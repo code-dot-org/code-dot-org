887fb41132cc5b3ba9e387279e3898af3bd101e4
Jim Thomason <jim@japanesewartuba.com>
2024-09-03 14:02:49 -0500

    jimt/remove-useSynchronizedProject - added helpful comment

--- apps/src/codebridge/hooks/useReducerWithCallback.ts
+++ apps/src/codebridge/hooks/useReducerWithCallback.ts
@@ -4,6 +4,11 @@ export const useReducerWithCallback = <StateType, ActionType>(
 ) => {
   return (state: StateType, action: ActionType) => {
     const newState = reducer(state, action);
+    // It's quite possible that the callback doesn't need to be in the setTimeout, but this is just explicitly not calling it until the newState
+    // has come back from the reducer and been processed by dispatch, by deferring the call until the next tick of the run loop.
+    //
+    // If it was called synchronously here, then theoretically there may be a way to invoke the callback and then be able to access a stale
+    // old reducer state before dispatch has finished. Or maybe it's not possible. :shrug:
     setTimeout(() => callback(newState), 0);
     return newState;
   };
