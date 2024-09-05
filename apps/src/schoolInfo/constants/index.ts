90629a57625ad2096fdd736f7d643dc52a93cea1
Ryan Driscoll <driscollrp@gmail.com>
2024-09-05 14:16:20 -0600

    refactor: moved constants to their own file

---
+++ apps/src/schoolInfo/constants/index.ts
@@ -0,0 +1,7 @@
+export const SCHOOL_COUNTRY = 'schoolCountry';
+export const US_COUNTRY_CODE = 'US';
+export const ZIP_REGEX = new RegExp(/(^\d{5}$)/);
+export const SELECT_A_SCHOOL = 'selectASchool';
+export const CLICK_TO_ADD = 'clickToAdd';
+export const NO_SCHOOL_SETTING = 'noSchoolSetting';
+export const SCHOOL_ZIP_SEARCH = '/dashboardapi/v1/schoolzipsearch/';
