07035a9292221e0050ea6470f785b6519fc664d0
Mario Gil Correa <mario.gil-correa@code.org>
2024-09-06 17:16:08 -0500

    updated dialog styles to match design

--- apps/src/sharedComponents/footer/CopyrightDialog.tsx
+++ apps/src/sharedComponents/footer/CopyrightDialog.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Link from '@cdo/apps/componentLibrary/link';
-import {BodyTwoText, Heading3} from '@cdo/apps/componentLibrary/typography';
+import {BodyThreeText, Heading3} from '@cdo/apps/componentLibrary/typography';
 import {pegasus} from '@cdo/apps/lib/util/urlHelpers';
 import AccessibleDialog from '@cdo/apps/sharedComponents/AccessibleDialog';
 import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
@@ -11,15 +11,15 @@ import styles from '@cdo/apps/sharedComponents/footer/copyright-dialog.module.sc
 
 export interface CopyrightDialogProps {
   isOpen: boolean;
-  closeModal: () => void;
+  closeModal: (e: React.ChangeEvent) => void;
 }
 
 const CopyrightDialog: React.FC<CopyrightDialogProps> = ({
   isOpen,
   closeModal,
 }) => {
-  const onClose = () => {
-    closeModal();
+  const onClose = (e: React.ChangeEvent) => {
+    closeModal(e);
   };
 
   const getDialogBody = () => {
@@ -27,39 +27,44 @@ const CopyrightDialog: React.FC<CopyrightDialogProps> = ({
       <div id="copyright-scroll-area">
         <Heading3>{i18n.copyright()}</Heading3>
         <hr />
-        <SafeMarkdown
-          markdown={i18n.copyright_thanks({
-            donors_link: pegasus('about/donors'),
-            partners_link: pegasus('about/partners'),
-            team_link: pegasus('about/team'),
-          })}
-        />
-        <BodyTwoText>{i18n.copyright_specialRecognition()}</BodyTwoText>
-        <SafeMarkdown
-          markdown={i18n.copyright_artFrom({
-            current_year: new Date().getFullYear(),
-          })}
-        />
-        <SafeMarkdown
-          markdown={i18n.copyright_codeLicense({
-            gnu_license_link:
-              'https://www.gnu.org/licenses/old-licenses/lgpl-2.1-standalone.html',
-          })}
-        />
-        <BodyTwoText>{i18n.copyright_builtOnGithub()}</BodyTwoText>
-        <Link href="https://aws.amazon.com/what-is-cloud-computing">
-          <img
-            src="/shared/images/Powered-By_logo-horiz_RGB.png"
-            alt="Powered by AWS Cloud Computing"
-            className={styles.awsLogo}
+        <BodyThreeText className={styles.copyrightContainer}>
+          <SafeMarkdown
+            markdown={i18n.copyright_thanks({
+              donors_link: pegasus('about/donors'),
+              partners_link: pegasus('about/partners'),
+              team_link: pegasus('about/team'),
+            })}
           />
-        </Link>
-        <SafeMarkdown
-          markdown={i18n.copyright_trademark({
-            current_year: new Date().getFullYear(),
-            cs_discoveries: 'CS Discoveries&reg;',
-          })}
-        />
+          {i18n.copyright_specialRecognition()}
+          <SafeMarkdown
+            markdown={i18n.copyright_artFrom({
+              current_year: new Date().getFullYear(),
+            })}
+          />
+          <SafeMarkdown
+            markdown={i18n.copyright_codeLicense({
+              gnu_license_link:
+                'https://www.gnu.org/licenses/old-licenses/lgpl-2.1-standalone.html',
+            })}
+          />
+          {i18n.copyright_builtOnGithub()}
+          <Link
+            href="https://aws.amazon.com/what-is-cloud-computing"
+            className={styles.awsLogoContainer}
+          >
+            <img
+              src="/shared/images/Powered-By_logo-horiz_RGB.png"
+              alt="Powered by AWS Cloud Computing"
+              className={styles.awsLogo}
+            />
+          </Link>
+          <SafeMarkdown
+            markdown={i18n.copyright_trademark({
+              current_year: new Date().getFullYear(),
+              cs_discoveries: 'CS Discoveries&reg;',
+            })}
+          />
+        </BodyThreeText>
       </div>
     );
   };
