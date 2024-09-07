97dd02277bc94bad8f9fca494a2d02ba5192a065
breville <brendan@code.org>
2024-09-07 02:20:24 +0000

    Remove call to function not available in this version

--- apps/src/standaloneVideo/VideoJS.tsx
+++ apps/src/standaloneVideo/VideoJS.tsx
@@ -41,7 +41,7 @@ export const VideoJS = (props: {options: VideoJsPlayerOptions}) => {
     const player = playerRef.current;
 
     return () => {
-      if (player && !player.isDisposed()) {
+      if (player) {
         player.dispose();
         playerRef.current = null;
       }
