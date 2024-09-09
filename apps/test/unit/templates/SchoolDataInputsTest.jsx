d4e620cd97fb3e2b2014a41f57c8b458332e1087
Ryan Driscoll <driscollrp@gmail.com>
2024-09-09 12:12:08 -0600

    refactor: use renderDefault for clarity on which props are changing in each test

--- apps/test/unit/templates/SchoolDataInputsTest.jsx
+++ apps/test/unit/templates/SchoolDataInputsTest.jsx
@@ -12,25 +12,30 @@ describe('SchoolDataInputs', () => {
   const mockSetSchoolName = jest.fn();
   const mockSetSchoolZip = jest.fn();
 
+  const defaultProps = {
+    schoolId: '1',
+    country: 'US',
+    schoolName: 'School Name',
+    schoolZip: '12345',
+    schoolsList: [{value: '1', text: 'School 1'}],
+    schoolsListLoading: false,
+    schoolZipIsValid: true,
+    setSchoolId: mockSetSchoolId,
+    setCountry: mockSetCountry,
+    setSchoolName: mockSetSchoolName,
+    setSchoolZip: mockSetSchoolZip,
+  };
+
+  function renderDefault(propOverrides = {}) {
+    render(<SchoolDataInputs {...defaultProps} {...propOverrides} />);
+  }
+
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it('should render with initial props and default state', () => {
-    render(
-      <SchoolDataInputs
-        schoolId="123"
-        country="US"
-        schoolName="School Name"
-        schoolZip="12345"
-        schoolsList={[{value: '1', text: 'School 1'}]}
-        schoolZipIsValid={true}
-        setSchoolId={mockSetSchoolId}
-        setCountry={mockSetCountry}
-        setSchoolName={mockSetSchoolName}
-        setSchoolZip={mockSetSchoolZip}
-      />
-    );
+    renderDefault();
 
     expect(screen.getByLabelText(i18n.whatCountry())).toBeInTheDocument();
     expect(screen.getByText(i18n.censusHeading())).toBeInTheDocument();
@@ -42,20 +47,7 @@ describe('SchoolDataInputs', () => {
   });
 
   it('should call setCountry when country dropdown changes', () => {
-    render(
-      <SchoolDataInputs
-        schoolId="123"
-        country="US"
-        schoolName="School Name"
-        schoolZip="12345"
-        schoolsList={[{value: '1', text: 'School 1'}]}
-        schoolZipIsValid={true}
-        setSchoolId={mockSetSchoolId}
-        setCountry={mockSetCountry}
-        setSchoolName={mockSetSchoolName}
-        setSchoolZip={mockSetSchoolZip}
-      />
-    );
+    renderDefault();
 
     fireEvent.change(screen.getByLabelText(i18n.whatCountry()), {
       target: {value: 'CA'},
@@ -65,20 +57,7 @@ describe('SchoolDataInputs', () => {
   });
 
   it('should render SchoolZipSearch when country is US', () => {
-    render(
-      <SchoolDataInputs
-        schoolId="123"
-        country="US"
-        schoolName="School Name"
-        schoolZip="12345"
-        schoolsList={[{value: '1', text: 'School 1'}]}
-        schoolZipIsValid={true}
-        setSchoolId={mockSetSchoolId}
-        setCountry={mockSetCountry}
-        setSchoolName={mockSetSchoolName}
-        setSchoolZip={mockSetSchoolZip}
-      />
-    );
+    renderDefault();
 
     expect(
       screen.getByLabelText(i18n.enterYourSchoolZip())
@@ -86,23 +65,12 @@ describe('SchoolDataInputs', () => {
   });
 
   it('should call setSchoolId when school dropdown changes', () => {
-    render(
-      <SchoolDataInputs
-        schoolId="1"
-        country="US"
-        schoolName="School Name"
-        schoolZip="12345"
-        schoolsList={[
-          {value: '1', text: 'School 1'},
-          {value: '2', text: 'School 2'},
-        ]}
-        schoolZipIsValid={true}
-        setSchoolId={mockSetSchoolId}
-        setCountry={mockSetCountry}
-        setSchoolName={mockSetSchoolName}
-        setSchoolZip={mockSetSchoolZip}
-      />
-    );
+    renderDefault({
+      schoolsList: [
+        {value: '1', text: 'School 1'},
+        {value: '2', text: 'School 2'},
+      ],
+    });
 
     fireEvent.change(screen.getByLabelText(i18n.selectYourSchool()), {
       target: {value: '2'},
@@ -112,40 +80,16 @@ describe('SchoolDataInputs', () => {
   });
 
   it('should render SchoolNameInput and "Return to results" button when country is US and inputManually is true', () => {
-    render(
-      <SchoolDataInputs
-        schoolId={CLICK_TO_ADD}
-        country="US"
-        schoolName="School Name"
-        schoolZip="12345"
-        schoolsList={[{value: '1', text: 'School 1'}]}
-        schoolZipIsValid={true}
-        setSchoolId={mockSetSchoolId}
-        setCountry={mockSetCountry}
-        setSchoolName={mockSetSchoolName}
-        setSchoolZip={mockSetSchoolZip}
-      />
-    );
+    renderDefault({
+      schoolId: CLICK_TO_ADD,
+    });
 
     expect(screen.getByDisplayValue('School Name')).toBeInTheDocument();
     expect(screen.getByText(i18n.returnToResults())).toBeInTheDocument();
   });
 
   it('should call handleSchoolChange when "No school setting" button is clicked', () => {
-    render(
-      <SchoolDataInputs
-        schoolId="123"
-        country="US"
-        schoolName="School Name"
-        schoolZip="12345"
-        schoolsList={[{value: '1', text: 'School 1'}]}
-        schoolZipIsValid={true}
-        setSchoolId={mockSetSchoolId}
-        setCountry={mockSetCountry}
-        setSchoolName={mockSetSchoolName}
-        setSchoolZip={mockSetSchoolZip}
-      />
-    );
+    renderDefault();
 
     fireEvent.click(screen.getByRole('button', i18n.noSchoolSetting()));
 
@@ -153,20 +97,9 @@ describe('SchoolDataInputs', () => {
   });
 
   it('should not render SchoolZipSearch when country is not US', () => {
-    render(
-      <SchoolDataInputs
-        schoolId="123"
-        country="CA"
-        schoolName="School Name"
-        schoolZip="12345"
-        schoolsList={[{value: '1', text: 'School 1'}]}
-        schoolZipIsValid={true}
-        setSchoolId={mockSetSchoolId}
-        setCountry={mockSetCountry}
-        setSchoolName={mockSetSchoolName}
-        setSchoolZip={mockSetSchoolZip}
-      />
-    );
+    renderDefault({
+      country: 'CA',
+    });
 
     expect(
       screen.queryByLabelText(i18n.enterYourSchoolZip())
