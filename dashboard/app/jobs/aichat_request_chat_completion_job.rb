b8352a793fd093fc43dad11ea99f9fd60056b099
Alice Fisher <alice.fisher@code.org>
2024-09-09 19:52:08 +0000

    use aws comprehend to moderate user input and model output

--- dashboard/app/jobs/aichat_request_chat_completion_job.rb
+++ dashboard/app/jobs/aichat_request_chat_completion_job.rb
@@ -1,6 +1,9 @@
 class AichatRequestChatCompletionJob < ApplicationJob
   queue_as :default
 
+  DEFAULT_TOXICITY_THRESHOLD_USER_INPUT = 0.25
+  DEFAULT_TOXICITY_THRESHOLD_MODEL_OUTPUT = 0.6
+
   before_enqueue do |job|
     request = job.arguments.first[:request]
     request.update!(execution_status: SharedConstants::AI_REQUEST_EXECUTION_STATUS[:QUEUED])
@@ -18,12 +21,7 @@ class AichatRequestChatCompletionJob < ApplicationJob
     end
 
     request = arguments.first[:request]
-    execution_status_code = SharedConstants::AI_REQUEST_EXECUTION_STATUS[:FAILURE]
-    if exception.message.include? "must have less than 3000 tokens"
-      execution_status_code = SharedConstants::AI_REQUEST_EXECUTION_STATUS[:USER_INPUT_TOO_LARGE]
-    end
-
-    request.update!(response: exception.message, execution_status: execution_status_code)
+    request.update!(response: exception.message, execution_status: SharedConstants::AI_REQUEST_EXECUTION_STATUS[:FAILURE])
     Honeybadger.notify(
       "AichatRequestChatCompletionJob failed with unexpected error: #{exception.message}",
       context: {
@@ -44,10 +42,25 @@ class AichatRequestChatCompletionJob < ApplicationJob
     request.update!(response: response, execution_status: status)
   end
 
+  private def create_comprehend_client
+    Aws::Comprehend::Client.new
+  end
+
+  private def get_toxicity_threshold_user_input
+    DCDO.get("aws_comprehend_toxicity_threshold_user_input", DEFAULT_TOXICITY_THRESHOLD_USER_INPUT)
+  end
+
+  private def get_toxicity_threshold_model_output
+    DCDO.get("aws_comprehend_toxicity_threshold_model_output", DEFAULT_TOXICITY_THRESHOLD_MODEL_OUTPUT)
+  end
+
   private def get_execution_status_and_response(model_customizations, stored_messages, new_message, level_id, locale)
-    # Check input for profanity and PII
-    user_profanity = find_profanity(new_message[:chatMessageText], locale)
-    return [SharedConstants::AI_REQUEST_EXECUTION_STATUS[:USER_PROFANITY], "Profanity detected in user input: #{user_profanity}"] if user_profanity
+    comprehend_client = create_comprehend_client
+
+    # Moderate user input for toxicity and PII
+    user_comprehend_response = comprehend_toxicity(new_message[:chatMessageText], locale, comprehend_client)
+    puts "user_comprehend_response: #{user_comprehend_response}"
+    return [SharedConstants::AI_REQUEST_EXECUTION_STATUS[:USER_PROFANITY], "Profanity detected in user input: #{user_comprehend_response}"] if user_comprehend_response[:toxicity] > get_toxicity_threshold_user_input
 
     user_pii = find_pii(new_message[:chatMessageText], locale)
     return [SharedConstants::AI_REQUEST_EXECUTION_STATUS[:USER_PII], "PII detected in user input: #{user_pii}"] if user_pii
@@ -55,17 +68,18 @@ class AichatRequestChatCompletionJob < ApplicationJob
     # Make the request
     response = AichatSagemakerHelper.get_sagemaker_assistant_response(model_customizations, stored_messages, new_message, level_id)
 
-    # Check output for profanity and PII. Report to HoneyBadger if the model returned profanity.
-    model_profanity = find_profanity(response, locale)
-    if model_profanity
+    # Moderate model output for toxicity and PII. Report to HoneyBadger if the model returned toxicity.
+    model_comprehend_response = comprehend_toxicity(response, locale, comprehend_client)
+    puts "model_comprehend_response: #{model_comprehend_response}"
+    if model_comprehend_response[:toxicity] > get_toxicity_threshold_model_output
       Honeybadger.notify(
-        'Profanity returned from aichat model (blocked before reaching student)',
+        'Toxicity returned from aichat model (blocked before reaching student)',
         context: {
           response: response,
-          flagged_content: model_profanity,
+          flagged_content: model_comprehend_response,
         }
       )
-      return [SharedConstants::AI_REQUEST_EXECUTION_STATUS[:MODEL_PROFANITY], "Profanity detected in model output: #{model_profanity}"]
+      return [SharedConstants::AI_REQUEST_EXECUTION_STATUS[:MODEL_PROFANITY], "Profanity detected in model output: #{model_comprehend_response}"]
     end
 
     model_pii = find_pii(response, locale)
@@ -74,11 +88,20 @@ class AichatRequestChatCompletionJob < ApplicationJob
     [SharedConstants::AI_REQUEST_EXECUTION_STATUS[:SUCCESS], response]
   end
 
-  # Check the given text for profanity
-  private def find_profanity(text, locale)
-    # TODO: Use llm-guard instead of WebPurify to check for profanity / toxicity.
-    filter_result = ShareFiltering.find_profanity_failure(text, locale)
-    filter_result.content if filter_result&.type == ShareFiltering::FailureType::PROFANITY
+  # Moderate given text for inappropriate/toxic content using AWS Comprehend client.
+  private def comprehend_toxicity(text, locale, comprehend_client)
+    comprehend_response = comprehend_client.detect_toxic_content(
+      {
+        text_segments: [{text: text}],
+        language_code: locale,
+      }
+    )
+    categories = comprehend_response.result_list[0].labels
+    {
+      text: text,
+      toxicity: comprehend_response.result_list[0].toxicity,
+      max_category: categories.max_by(&:score),
+    }
   end
 
   # Check the given text for PII
