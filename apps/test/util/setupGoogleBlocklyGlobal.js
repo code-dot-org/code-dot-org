87286fbc6061f951a1d5a16febd51343571391a4
Mike Harvey <mike.harvey@code.org>
2024-09-09 08:24:03 -0400

    skip locale

--- apps/test/util/setupGoogleBlocklyGlobal.js
+++ apps/test/util/setupGoogleBlocklyGlobal.js
@@ -1,6 +1,6 @@
 /**
- * Provides the basic frame for running Blockly.  In particular, this will
- * create a basic dom, load blockly.js  and put the contents into the global
+ * Provides the basic frame for running Google Blockly.  In particular, this
+ * will create a basic dom, load Blockly,  and put the contents into the global
  * space as global.Blockly.
  */
 
@@ -10,10 +10,5 @@ export default function setBlocklyGlobal() {
   // locale file requires Blockly as a global
   const blockly = require('blockly');
   const initializeGoogleBlocklyWrapper = require('../../src/blockly/googleBlocklyWrapper');
-
-  const messages = require(`blockly/msg/en.js`);
-
-  blockly.setLocale(messages);
-
   window.Blockly = initializeGoogleBlocklyWrapper(blockly);
 }
