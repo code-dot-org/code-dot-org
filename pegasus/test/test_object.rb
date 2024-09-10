dab7beb636a9e40e9dbe9538b6f002512f1e9aa6
Elijah Hamovitz <elijahhamovitz@gmail.com>
2024-09-10 10:41:47 -0700

    fix broken test

--- pegasus/test/test_object.rb
+++ pegasus/test/test_object.rb
@@ -3,13 +3,13 @@ require 'minitest/autorun'
 
 class ObjectTest < Minitest::Test
   def test_nil_or_empty
-    assert nil.nil_or_empty?
-    assert ''.nil_or_empty?
-    assert [].nil_or_empty?
-    assert {}.nil_or_empty?
+    assert(nil.nil_or_empty?)
+    assert(''.nil_or_empty?)
+    assert([].nil_or_empty?)
+    assert({}.nil_or_empty?)
 
-    assert !'a'.nil_or_empty?
-    assert ![1].nil_or_empty?
-    assert !{a: 1}.nil_or_empty?
+    refute('a'.nil_or_empty?)
+    refute([1].nil_or_empty?)
+    refute({a: 1}.nil_or_empty?)
   end
 end
