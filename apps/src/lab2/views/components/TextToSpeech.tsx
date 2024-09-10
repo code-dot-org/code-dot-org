38bbea965ba531aafdfebf1c9a60a425ba155d4d
breville <brendan@code.org>
2024-09-10 04:27:58 +0000

    Updates

--- apps/src/lab2/views/components/TextToSpeech.tsx
+++ apps/src/lab2/views/components/TextToSpeech.tsx
@@ -2,6 +2,7 @@ import {markdownToTxt} from 'markdown-to-txt';
 import React, {useCallback} from 'react';
 
 import FontAwesomeV6Icon from '@cdo/apps/componentLibrary/fontAwesomeV6Icon/FontAwesomeV6Icon';
+import {getCurrentLocale} from '@cdo/apps/lab2/projects/utils';
 
 import moduleStyles from './TextToSpeech.module.scss';
 
@@ -14,8 +15,23 @@ interface TextToSpeechProps {
  */
 const TextToSpeech: React.FunctionComponent<TextToSpeechProps> = ({text}) => {
   const playText = useCallback(() => {
+    const currentLocale = getCurrentLocale();
+    const voices = speechSynthesis.getVoices();
+    if (voices.length === 0) {
+      return;
+    }
+    const voicesFiltered = voices.filter(voice => voice.lang === currentLocale);
+    console.log(
+      `TexttoSpeech: found ${voicesFiltered.length} of ${voices.length} possible voices`
+    );
+    if (voicesFiltered.length === 0) {
+      return;
+    }
+    const voice = voicesFiltered[0];
     const plainText = markdownToTxt(text);
     const utterance = new SpeechSynthesisUtterance(plainText);
+    utterance.voice = voice;
+    utterance.lang = currentLocale;
     speechSynthesis.cancel();
     speechSynthesis.speak(utterance);
   }, [text]);
