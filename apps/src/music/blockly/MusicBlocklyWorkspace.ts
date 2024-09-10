532fe1875a9ed38e7c3c7d594d1be9e77a434e05
Mike Harvey <mike.harvey@code.org>
2024-09-10 14:20:10 -0400

    Update MusicBlocklyWorkspace.ts

--- apps/src/music/blockly/MusicBlocklyWorkspace.ts
+++ apps/src/music/blockly/MusicBlocklyWorkspace.ts
@@ -88,6 +88,7 @@ export default class MusicBlocklyWorkspace {
 
     const toolboxBlocks = getToolbox(getBlockMode(), toolbox);
 
+    // This dialog is used for naming variables, which are only present in advanced mode.
     // Other Blockly labs use FeedbackUtils.prototype.showSimpleDialog to create a prettier dialog.
     // See StudioApp.prototype.inject for more information.
     const customSimpleDialog = function (options: {
