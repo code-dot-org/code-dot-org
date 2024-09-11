4e168f113489966faa21213b2eb68e22ae74d980
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2024-09-11 14:47:52 -0400

    [Music] Add pattern and chord blocks to advanced mode (#60982)
    * Add pattern block to advanced mode
    
    * Add chord block advanced mode

--- apps/src/music/blockly/blocks/advanced.js
+++ apps/src/music/blockly/blocks/advanced.js
@@ -1,8 +1,16 @@
 import musicI18n from '../../locale';
 import {BlockTypes} from '../blockTypes';
 import {isBlockInsideWhenRun} from '../blockUtils';
-import {FIELD_SOUNDS_NAME} from '../constants';
-import {fieldSoundsDefinition} from '../fields';
+import {
+  FIELD_CHORD_NAME,
+  FIELD_PATTERN_NAME,
+  FIELD_SOUNDS_NAME,
+} from '../constants';
+import {
+  fieldChordDefinition,
+  fieldPatternDefinition,
+  fieldSoundsDefinition,
+} from '../fields';
 
 export const playSound = {
   definition: {
@@ -39,4 +47,66 @@ export const playSound = {
     ', "' +
     ctx.id +
     '");\n',
+};
+
+export const playPatternAtMeasure = {
+  definition: {
+    type: BlockTypes.PLAY_PATTERN_AT_MEASURE,
+    message0: musicI18n.blockly_blockPlayPatternAtMeasure({
+      pattern: '%1',
+      measure: '%2',
+    }),
+    args0: [
+      fieldPatternDefinition,
+      {
+        type: 'input_value',
+        name: 'measure',
+      },
+    ],
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    style: 'lab_blocks',
+    tooltip: musicI18n.blockly_blockPlayPatternAtMeasureTooltip(),
+    helpUrl: '',
+  },
+  generator: block =>
+    `Sequencer.playPatternAtMeasureById(${JSON.stringify(
+      block.getFieldValue(FIELD_PATTERN_NAME)
+    )}, ${Blockly.JavaScript.valueToCode(
+      block,
+      'measure',
+      Blockly.JavaScript.ORDER_ASSIGNMENT
+    )}, "${block.id}");`,
+};
+
+export const playChordAtMeasure = {
+  definition: {
+    type: BlockTypes.PLAY_CHORD_AT_MEASURE,
+    message0: musicI18n.blockly_blockPlayChordAtMeasure({
+      chord: '%1',
+      measure: '%2',
+    }),
+    args0: [
+      fieldChordDefinition,
+      {
+        type: 'input_value',
+        name: 'measure',
+      },
+    ],
+    inputsInline: true,
+    previousStatement: null,
+    nextStatement: null,
+    style: 'lab_blocks',
+    tooltip: musicI18n.blockly_blockPlayChordAtMeasureTooltip(),
+    helpUrl: '',
+  },
+  generator: block =>
+    `Sequencer.playChordAtMeasureById(${JSON.stringify(
+      block.getFieldValue(FIELD_CHORD_NAME)
+    )}, ${Blockly.JavaScript.valueToCode(
+      block,
+      'measure',
+      Blockly.JavaScript.ORDER_ASSIGNMENT
+    )}, "${block.id}");`,
 };
