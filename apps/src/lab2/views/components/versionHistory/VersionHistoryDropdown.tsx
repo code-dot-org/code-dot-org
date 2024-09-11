637666077299922f93f37d6428f800a5dcf4e335
Molly Moen <molly@code.org>
2024-09-11 13:15:29 -0700

    Python Lab version history: Allow teacher to view student's old versions (#60948)
    * allow viewing student's old versions
    
    * update comment
    
    * fix bug

--- apps/src/lab2/views/components/versionHistory/VersionHistoryDropdown.tsx
+++ apps/src/lab2/views/components/versionHistory/VersionHistoryDropdown.tsx
@@ -67,6 +67,10 @@ const VersionHistoryDropdown: React.FunctionComponent<
     state => state.lab2Project.viewingOldVersion
   );
 
+  // If this is a teacher viewing a student's project, we hide the restore button,
+  // but still allow viewing old versions.
+  const viewAsUserId = useAppSelector(state => state.progress.viewAsUserId);
+
   const dialogControl = useDialogControl();
 
   const dateFormatter = useMemo(() => {
@@ -246,14 +250,16 @@ const VersionHistoryDropdown: React.FunctionComponent<
             <i className="fa fa-spinner fa-spin" />
           </div>
         )}
-        <Button
-          text={commonI18n.restore()}
-          color={'purple'}
-          size={'m'}
-          onClick={restoreSelectedVersion}
-          disabled={loading}
-          className={moduleStyles.actionButton}
-        />
+        {!viewAsUserId && (
+          <Button
+            text={commonI18n.restore()}
+            color={'purple'}
+            size={'m'}
+            onClick={restoreSelectedVersion}
+            disabled={loading}
+            className={moduleStyles.actionButton}
+          />
+        )}
         <Button
           text={commonI18n.cancel()}
           color={'white'}
