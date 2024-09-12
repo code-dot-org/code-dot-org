14affdf57090bdca56e38ed77f57fb9a2296f7c7
Mario Gil Correa <mario.gil-correa@code.org>
2024-09-12 18:11:11 -0500

    created i18n dropdown component

---
+++ apps/src/sharedComponents/footer/I18nDropdown/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import {
+  SimpleDropdown,
+  SimpleDropdownProps,
+} from '@cdo/apps/componentLibrary/dropdown';
+import currentLocale from '@cdo/apps/util/currentLocale';
+
+interface I18nDropdownProps {
+  localeUrl: string;
+  optionsForLocaleSelect: SimpleDropdownProps['items'];
+}
+
+const I18nDropdown: React.FC<I18nDropdownProps> = ({
+  localeUrl,
+  optionsForLocaleSelect,
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    event.target.form?.submit();
+  };
+
+  return (
+    <form
+      action={localeUrl}
+      method="post"
+      id="localeForm"
+      style={{marginBottom: '0px'}}
+    >
+      <input type="hidden" name="user_return_to" value={window.location.href} />
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <SimpleDropdown
+          name="locale-dropdown"
+          selectedValue={currentLocale()}
+          onChange={handleChange}
+          items={optionsForLocaleSelect}
+          labelText="Select Language"
+          isLabelVisible={false}
+          size="xs"
+          color="gray"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default I18nDropdown;
