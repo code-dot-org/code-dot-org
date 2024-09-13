bf6e5be9e43c2bc4a609538d8cd57aa0b2b1e12b
Mike Harvey <mike.harvey@code.org>
2024-09-13 11:23:26 -0400

    add comment

--- apps/src/music/blockly/advancedProcedures.ts
+++ apps/src/music/blockly/advancedProcedures.ts
@@ -4,5 +4,6 @@ export function installAdvancedProcedures() {
   Blockly.Extensions.unregister('procedure_caller_mutator');
   Blockly.Extensions.unregister('procedure_def_mutator');
   require('@blockly/block-plus-minus');
+  // We use the label 'parameter' to align to CSTA standards and our curriculum.
   Blockly.Msg['PROCEDURE_VARIABLE'] = musicI18n.parameter();
 }
