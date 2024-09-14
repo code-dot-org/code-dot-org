116a50921e8b6dc323a84d5a2bad0f2c6a3930cb
Mike Harvey <43474485+mikeharv@users.noreply.github.com>
2024-09-13 21:26:50 -0400

    [Music Lab] Add setEffect block (#61025)
    * [Music Lab] Add setEffect block
    
    * clear effects

--- apps/src/music/blockly/toolbox/toolboxBlocks.ts
+++ apps/src/music/blockly/toolbox/toolboxBlocks.ts
@@ -60,6 +60,30 @@ const toolboxBlocks: {[blockType in BlockTypes | string]: BlockInfoWithText} = {
       },
     },
   },
+  [BlockTypes.SET_VOLUME_EFFECT]: {
+    kind: 'block',
+    type: BlockTypes.SET_EFFECT,
+    fields: {
+      [FIELD_EFFECTS_NAME]: 'volume',
+      [FIELD_EFFECTS_VALUE]: DEFAULT_EFFECT_VALUE,
+    },
+  },
+  [BlockTypes.SET_FILTER_EFFECT]: {
+    kind: 'block',
+    type: BlockTypes.SET_EFFECT,
+    fields: {
+      [FIELD_EFFECTS_NAME]: 'filter',
+      [FIELD_EFFECTS_VALUE]: DEFAULT_EFFECT_VALUE,
+    },
+  },
+  [BlockTypes.SET_DELAY_EFFECT]: {
+    kind: 'block',
+    type: BlockTypes.SET_EFFECT,
+    fields: {
+      [FIELD_EFFECTS_NAME]: 'delay',
+      [FIELD_EFFECTS_VALUE]: DEFAULT_EFFECT_VALUE,
+    },
+  },
   [BlockTypes.PLAY_SOUND_AT_CURRENT_LOCATION]: {
     levelbuilderText: 'unused',
     kind: 'block',
