0558ac5e461ddda16e156a97e25f54a77a5f07be
Turner Riley <turnertriley@gmail.com>
2024-09-16 10:58:35 -0400

    add registrations tests

--- dashboard/test/controllers/registrations_controller_test.rb
+++ dashboard/test/controllers/registrations_controller_test.rb
@@ -24,6 +24,16 @@ class RegistrationsControllerTest < ActionController::TestCase
       age: '13',
       user_type: 'student'
     }
+
+    @default_params_new_signup = {
+      name: 'A name',
+      password: 'apassword',
+      email: 'an@email.address',
+      gender: 'F',
+      age: '13',
+      user_type: 'student',
+      new_sign_up: true
+    }
   end
 
   test "update: returns bad_request if user param is nil" do
@@ -222,33 +232,13 @@ class RegistrationsControllerTest < ActionController::TestCase
     end
   end
 
-  test "finish_creating_user saves return to url in session" do
-    # Note that we currently have no restrictions on what the domain of the
-    # redirect url can be; we may at some point want to add domain
-    # restrictions, but if we do so we want to make sure that both
-    # studio.code.org and code.org are supported.
-    #
-    # See also the "sign in page saves return to url in session" test in
-    # sessions_controller_test
-    urls = [
-      "/foo",
-      "//studio.code.org/foo",
-      "//code.org/foo",
-      "//some_other_domain.com/foo"
-    ]
-
-    urls.each do |url|
-      session.delete(:user_return_to)
-      post :finish_creating_user, params: {user_return_to: url}
-      assert_equal url, session[:user_return_to]
-    end
-  end
+  # USING OLD SIGN-UP FLOW [START]
 
   test "teachers go to specified return to url after signing up" do
     session[:user_return_to] = user_return_to = '//test.code.org/the-return-to-url'
 
     assert_creates(User) do
-      post :create, params: {user: @default_params}
+      post :create, params: {user: @default_params.update(user_type: 'teacher', age: '21+', email_preference_opt_in: true)}
     end
 
     assert_redirected_to user_return_to
@@ -537,6 +527,306 @@ class RegistrationsControllerTest < ActionController::TestCase
     assert_equal 1, sign_in.sign_in_count
     assert_equal frozen_time, sign_in.sign_in_at
   end
+
+  # USING OLD SIGN-UP FLOW [END]
+
+  # USING NEW SIGN-UP FLOW [START]
+
+  test "teachers go to specified return to url after signing up [new sign up flow]" do
+    session[:user_return_to] = user_return_to = '//test.code.org/the-return-to-url'
+
+    assert_creates(User) do
+      post :create, params: {user:  @default_params_new_signup.update(user_type: 'teacher', age: '21+', email_preference_opt_in: true)}
+    end
+
+    assert_redirected_to user_return_to
+  end
+
+  test "create retries on Duplicate exception [new sign up flow]" do
+    # some Mocha shenanigans to simulate throwing a duplicate entry
+    # error and then succeeding by returning the existing user
+
+    exception = ActiveRecord::RecordNotUnique.new(Mysql2::Error.new("Duplicate entry 'coder1234574782' for key 'index_users_on_username'"))
+    User.any_instance.stubs(:save).raises(exception).then.returns(true)
+    User.any_instance.stubs(:persisted?).returns(true)
+
+    post :create, params: {user: @default_params_new_signup}
+
+    assert_redirected_to '/'
+
+    # we are still stubbing user.save (even though we returned true so
+    # we can't actually check that the user was created)
+  end
+
+  test "create as student with age [new sign up flow]" do
+    Timecop.travel Time.local(2013, 9, 1, 12, 0, 0) do
+      assert_creates(User) do
+        post :create, params: {user: @default_params_new_signup}
+      end
+
+      assert_redirected_to '/'
+
+      assert_equal 'A name', assigns(:user).name
+      assert_equal 'f', assigns(:user).gender
+      assert_equal Time.zone.today - 13.years, assigns(:user).birthday
+      assert_equal AuthenticationOption::EMAIL, assigns(:user).primary_contact_info.credential_type
+      assert_equal User::TYPE_STUDENT, assigns(:user).user_type
+      assert_equal '', assigns(:user).email
+      assert_equal User.hash_email('an@email.address'), assigns(:user).hashed_email
+    end
+  end
+
+  test "create as under 13 student with client side hashed email [new sign up flow]" do
+    Timecop.travel Time.local(2013, 9, 1, 12, 0, 0) do
+      @default_params_new_signup.delete(:email)
+      params_with_hashed_email = @default_params_new_signup.merge(
+        {hashed_email: User.hash_email('an@email.address')}
+      )
+
+      assert_creates(User) do
+        post :create, params: {user: params_with_hashed_email}
+      end
+
+      assert_redirected_to '/'
+
+      assert_equal 'A name', assigns(:user).name
+      assert_equal 'f', assigns(:user).gender
+      assert_equal Time.zone.today - 13.years, assigns(:user).birthday
+      assert_equal AuthenticationOption::EMAIL, assigns(:user).primary_contact_info.credential_type
+      assert_equal User::TYPE_STUDENT, assigns(:user).user_type
+      assert_equal '', assigns(:user).email
+      assert_equal User.hash_email('an@email.address'), assigns(:user).hashed_email
+    end
+  end
+
+  test "create as student requires age [new sign up flow]" do
+    params_without_age = @default_params_new_signup.update(age: '')
+
+    assert_does_not_create(User) do
+      post :create, params: {user: params_without_age}
+    end
+
+    assert_equal ["Age is required"], assigns(:user).errors.full_messages
+  end
+
+  test "create does not allow pandas in name [new sign up flow]" do
+    params_with_panda_name = @default_params_new_signup.update(name: panda_panda)
+
+    assert_does_not_create(User) do
+      post :create, params: {user: params_with_panda_name}
+    end
+
+    assert_equal ["Display Name is invalid"], assigns(:user).errors.full_messages
+  end
+
+  test "create does not allow pandas in email [new sign up flow]" do
+    params_with_panda_email = @default_params_new_signup.update(
+      email: "#{panda_panda}@panda.com"
+    )
+
+    # don't ask the db for existing panda emails
+    User.expects(:find_by_email_or_hashed_email).never
+
+    assert_does_not_create(User) do
+      post :create, params: {user: params_with_panda_email}
+    end
+
+    assert_equal ["Email is invalid"], assigns(:user).errors.full_messages
+  end
+
+  test "create allows chinese in name [new sign up flow]" do
+    params_with_chinese_name = @default_params_new_signup.update(
+      name: '樊瑞'
+    )
+
+    assert_creates(User) do
+      post :create, params: {user: params_with_chinese_name}
+    end
+  end
+
+  test "create as teacher requires age [new sign up flow]" do
+    teacher_params = @default_params_new_signup.update(user_type: 'teacher', age: '', email_preference_opt_in: 'yes')
+
+    assert_does_not_create(User) do
+      post :create, params: {user: teacher_params}
+    end
+
+    assert_equal ["Age is required"], assigns(:user).errors.full_messages
+  end
+
+  test "create new teacher with MailJet enabled sends welcome email [new sign up flow]" do
+    teacher_params = @default_params_new_signup.update(user_type: 'teacher', email_preference_opt_in: 'yes')
+    Geocoder.stubs(:search).returns([OpenStruct.new(country_code: 'CA')])
+    MailJet.stubs(:enabled?).returns(true)
+    MailJet.expects(:create_contact_and_add_to_welcome_series).once
+    assert_creates(User) do
+      post :create, params: {user: teacher_params}
+    end
+
+    assert_empty ActionMailer::Base.deliveries
+  end
+
+  test "create new teacher with opt-in option as yes writes email preference as yes [new sign up flow]" do
+    teacher_params = @default_params_new_signup.update(user_type: 'teacher', email_preference_opt_in: 'yes')
+    Geocoder.stubs(:search).returns([OpenStruct.new(country_code: 'CA')])
+    assert_creates(User) do
+      assert_creates(EmailPreference) do
+        post :create, params: {user: teacher_params}
+      end
+    end
+
+    email_preference = EmailPreference.last
+    assert_equal "an@email.address", email_preference[:email]
+    assert email_preference[:opt_in]
+    assert_equal EmailPreference::ACCOUNT_SIGN_UP, email_preference[:source]
+  end
+
+  test "create new teacher with opt-in option as no writes email preference as no [new sign up flow]" do
+    teacher_params = @default_params_new_signup.update(user_type: 'teacher', email_preference_opt_in: 'no')
+    Geocoder.stubs(:search).returns([OpenStruct.new(country_code: 'CA')])
+    assert_creates(User) do
+      assert_creates(EmailPreference) do
+        post :create, params: {user: teacher_params}
+      end
+    end
+
+    email_preference = EmailPreference.last
+    assert_equal "an@email.address", email_preference[:email]
+    refute email_preference[:opt_in]
+    assert_equal EmailPreference::ACCOUNT_SIGN_UP, email_preference[:source]
+  end
+
+  test "create new teacher with us ip with opt-in to sharing email with regional partners set share_teacher_email_regional_partner_opt_in value to DateTime value [new sign up flow]" do
+    teacher_params = @default_params_new_signup.update(user_type: 'teacher', email_preference_opt_in: 'no', share_teacher_email_reg_partner_opt_in_radio_choice: 'yes')
+    Geocoder.stubs(:search).returns([OpenStruct.new(country_code: 'US')])
+    assert_creates(User) do
+      post :create, params: {user: teacher_params}
+    end
+
+    teacher = User.last
+    refute_nil teacher.share_teacher_email_regional_partner_opt_in
+  end
+
+  test "create new teacher with us ip with opt-out to sharing email with regional partners ensure share_teacher_email_regional_partner_opt_in value is nil [new sign up flow]" do
+    teacher_params = @default_params_new_signup.update(user_type: 'teacher', email_preference_opt_in: 'no', share_teacher_email_reg_partner_opt_in_radio_choice: 'no')
+    Geocoder.stubs(:search).returns([OpenStruct.new(country_code: 'US')])
+    assert_creates(User) do
+      post :create, params: {user: teacher_params}
+    end
+
+    teacher = User.last
+    assert_nil teacher.share_teacher_email_regional_partner_opt_in
+  end
+
+  test "create new teacher with us ip with no selection on sharing email with regional partners ensure share_teacher_email_regional_partner_opt_in value is nil [new sign up flow]" do
+    teacher_params = @default_params_new_signup.update(user_type: 'teacher', email_preference_opt_in: 'no')
+    Geocoder.stubs(:search).returns([OpenStruct.new(country_code: 'US')])
+    assert_creates(User) do
+      post :create, params: {user: teacher_params}
+    end
+
+    teacher = User.last
+    assert_nil teacher.share_teacher_email_regional_partner_opt_in
+  end
+
+  test "create new teacher with non-us ip ensure share_teacher_email_regional_partner_opt_in value is nil [new sign up flow]" do
+    teacher_params = @default_params_new_signup.update(user_type: 'teacher', email_preference_opt_in: 'no')
+    Geocoder.stubs(:search).returns([OpenStruct.new(country_code: 'CA')])
+    assert_creates(User) do
+      post :create, params: {user: teacher_params}
+    end
+
+    teacher = User.last
+    assert_nil teacher.share_teacher_email_regional_partner_opt_in
+  end
+
+  test "create new student ensure share_teacher_email_regional_partner_opt_in value is nil [new sign up flow]" do
+    student_params = @default_params_new_signup
+    assert_creates(User) do
+      post :create, params: {user: student_params}
+    end
+
+    student = User.last
+    assert_nil student.share_teacher_email_regional_partner_opt_in
+  end
+
+  test "create new student in eu fails when missing value [new sign up flow]" do
+    eu_student_params = @default_params_new_signup.update(
+      data_transfer_agreement_required: "1"
+    )
+
+    assert_does_not_create(User) do
+      post :create, params: {user: eu_student_params}
+    end
+  end
+
+  test "create new student in eu succeeds with value [new sign up flow]" do
+    eu_student_params = @default_params_new_signup.update(
+      data_transfer_agreement_required: "1",
+      data_transfer_agreement_accepted: "1",
+    )
+
+    assert_creates(User) do
+      post :create, params: {user: eu_student_params}
+    end
+  end
+
+  test "create new student does not send email [new sign up flow]" do
+    student_params = @default_params_new_signup
+
+    assert_creates(User) do
+      post :create, params: {user: student_params}
+    end
+    assert ActionMailer::Base.deliveries.empty?
+  end
+
+  test "create as student requires email [new sign up flow]" do
+    @default_params_new_signup.delete(:email)
+
+    assert_does_not_create(User) do
+      post :create, params: {user: @default_params_new_signup}
+    end
+
+    assert_equal ["Email is required"], assigns(:user).errors.full_messages
+  end
+
+  test "create requires case insensitive unique email [new sign up flow]" do
+    create(:student, email: 'not_a@unique.email')
+    params_with_non_unique_email = @default_params_new_signup.update(
+      email: 'not_a@unique.email'
+    )
+
+    assert_does_not_create(User) do
+      post :create, params: {user: params_with_non_unique_email}
+    end
+
+    assert_equal ["Email has already been taken"], assigns(:user).errors.full_messages
+  end
+
+  test "create causes UserGeo creation [new sign up flow]" do
+    request.remote_addr = '1.2.3.4'
+    assert_creates(UserGeo) do
+      post :create, params: {user: @default_params_new_signup}
+    end
+
+    user_geo = UserGeo.last
+    assert user_geo
+    assert user_geo.ip_address = '1.2.3.4'
+  end
+
+  test "create causes SignIn creation [new sign up flow]" do
+    frozen_time = Date.parse('1985-10-26 01:20:00')
+    DateTime.stubs(:now).returns(frozen_time)
+    assert_creates(SignIn) do
+      post :create, params: {user: @default_params_new_signup}
+    end
+    sign_in = SignIn.last
+    assert sign_in
+    assert_equal 1, sign_in.sign_in_count
+    assert_equal frozen_time, sign_in.sign_in_at
+  end
+
+  # USING NEW SIGN-UP FLOW [END]
 
   test "display name edit field absent for picture account" do
     picture_student = create(:student_in_picture_section)
