191276911d7fcc2de5ca35ad112eefd20626eb2c
breville <brendan@code.org>
2024-09-17 17:38:04 +0000

    Handle RTL

--- apps/src/code-studio/components/header/ScriptName.jsx
+++ apps/src/code-studio/components/header/ScriptName.jsx
@@ -103,12 +103,18 @@ class ScriptName extends React.Component {
           ref="scriptName"
           style={{...styles.headerInner, height: 40}}
         >
-          <div style={styles.outerContainer}>
+          <div
+            style={
+              this.props.isRtl
+                ? styles.outerContainerRtl
+                : styles.outerContainer
+            }
+          >
             <div style={styles.containerWithUpdatedAt}>
               {this.renderScriptLink()}
               <ProjectUpdatedAt
                 onContentUpdated={this.onProjectUpdatedAtContentUpdated}
-                floatRight={true}
+                floatRight={!this.props.isRtl}
               />
             </div>
           </div>
@@ -124,6 +130,7 @@ const styles = {
     position: 'relative',
     overflow: 'hidden',
     height: 40,
+    top: 3,
   },
   headerInner: {
     position: 'absolute',
@@ -133,6 +140,9 @@ const styles = {
   },
   outerContainer: {
     textAlign: 'right',
+  },
+  outerContainerRtl: {
+    textAlign: 'left',
   },
   containerWithUpdatedAt: {
     verticalAlign: 'bottom',
