b6a0d0595ea190ce79ea5e1b8805fa0b6dbd6bbb
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-09-18 21:44:43 +0300

    P20-1165: Update tests

--- dashboard/test/integration/global_edition_test.rb
+++ dashboard/test/integration/global_edition_test.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class GlobalEditionTest < ActionDispatch::IntegrationTest
   describe 'routing' do
-    let(:global_page_path) {incubator_path}
+    let(:international_page_path) {'/incubator'}
     let(:ge_region) {'fa'}
-    let(:ge_region_page_path) {File.join('/global', ge_region, global_page_path)}
+    let(:regional_page_path) {File.join('/global', ge_region, international_page_path)}
 
-    describe 'global page' do
+    describe 'international page' do
       it 'is accessible' do
-        get global_page_path
+        get international_page_path
 
-        assert_equal 200, status
-        assert_equal global_page_path, path
-        assert_select '#incubator-container'
+        must_respond_with 200
+        _(path).must_equal international_page_path
       end
 
       context 'when ge_region cookie is set' do
@@ -20,54 +21,56 @@ class GlobalEditionTest < ActionDispatch::IntegrationTest
           cookies[:ge_region] = ge_region
         end
 
-        it 'redirects to region page' do
-          get global_page_path
+        it 'redirects to regional page with params' do
+          params = {foo: 'bar'}
+          get international_page_path, params: params
 
-          assert_equal 302, status
-          assert_redirected_to ge_region_page_path
+          must_respond_with 302
+          must_redirect_to "#{regional_page_path}?#{params.to_query}"
 
           follow_redirect!
 
-          assert_equal 200, status
-          assert_equal ge_region_page_path, path
-          assert_select '#incubator-container'
+          must_respond_with 200
+          _(path).must_equal regional_page_path
+          _(request.params[:foo]).must_equal params[:foo]
         end
 
         context 'if ge_region is unavailable' do
-          let(:ge_region) {'aa'}
+          let(:ge_region) {'_'}
 
-          it 'stays on global page' do
-            get global_page_path
+          it 'stays on international page' do
+            get international_page_path
 
-            assert_equal 200, status
-            assert_equal global_page_path, path
-            assert_select '#incubator-container'
+            must_respond_with 200
+            _(path).must_equal international_page_path
           end
         end
       end
     end
 
-    describe 'region page' do
+    describe 'regional page' do
       it 'is accessible' do
-        get ge_region_page_path
+        get regional_page_path
 
-        assert_equal 200, status
-        assert_equal ge_region_page_path, path
-        assert_select '#incubator-container'
+        must_respond_with 200
+        _(path).must_equal regional_page_path
+      end
+
+      it 'contains ge_region param' do
+        get regional_page_path
+        _(request.params[:ge_region]).must_equal ge_region
       end
 
       it 'routing helpers generates region version of urls' do
-        assert_change -> {incubator_path}, from: '/incubator', to: ge_region_page_path do
-          get ge_region_page_path
-        end
+        _ {get regional_page_path}.must_change -> {incubator_path}, from: international_page_path, to: regional_page_path
       end
 
       context 'when ge_region is unavailable' do
-        let(:ge_region) {'aa'}
+        let(:ge_region) {'_'}
 
         it 'is not accessible' do
-          actual_error = assert_raises(ActionController::RoutingError) {get ge_region_page_path}
-          assert_equal %Q[No route matches [GET] "#{ge_region_page_path}"], actual_error.message
+          actual_error = _ {get regional_page_path}.must_raise(ActionController::RoutingError)
+          _(actual_error.message).must_equal %Q[No route matches [GET] "#{regional_page_path}"]
         end
       end
     end
