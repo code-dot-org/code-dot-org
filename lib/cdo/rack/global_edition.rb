fd3358462a8ed7a401f358d53d453dd5e126b4bd
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-09-18 23:22:15 +0300

    P20-1165: Add comments

--- lib/cdo/rack/global_edition.rb
+++ lib/cdo/rack/global_edition.rb
@@ -29,25 +29,34 @@ module Rack
         if PATH_PATTERN.match?(request.path_info)
           path_vars = PATH_PATTERN.match(request.path_info)
 
+          # Strips the Global Edition subpath (e.g., `/global/fa`) from the request path.
           # request.path == request.script_name + request.path_info
+          # - `request.script_name` strips the prefix from the request path
+          #   so the application processes requests as if it were running at the root level.
+          # - `request.path_info` provides the specific path that should be handled by the application.
           request.script_name += path_vars[:ge_path]
           request.path_info = path_vars[:main_path]
 
           request.update_param(REGION_KEY, path_vars[:ge_region])
         elsif AVAILABLE_REGIONS.include?(request.cookies[REGION_KEY]) && request.get? && !request.xhr?
-          ge_region = request.cookies[REGION_KEY]
-
-          redirect_uri = URI(::File.join(request.script_name, ROOT_PATH, ge_region, request.path_info))
-          redirect_uri.query = URI.encode_www_form(request.params) unless request.params.empty?
-
+          # Redirects to the regional page if the `ge_region` from cookies is available
+          # and the request is a non-AJAX GET request (reflected in the browser address bar).
           response = Response.new
-          response.redirect(redirect_uri.to_s, 302)
-
+          response.redirect(regional_path_for(request), 302)
           return response.finish
         end
       end
 
       @app.call(env)
+    end
+
+    private def regional_path_for(request)
+      ge_region = request.cookies[REGION_KEY]
+
+      regional_page_uri = URI(::File.join(request.script_name, ROOT_PATH, ge_region, request.path_info))
+      regional_page_uri.query = URI.encode_www_form(request.params) unless request.params.empty?
+
+      regional_page_uri.to_s
     end
   end
 end
