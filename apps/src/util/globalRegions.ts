4aa9f6719222da62ad95c339714143b977fe31c1
wilkie <wilkie05@gmail.com>
2024-09-19 20:58:12 +0000

    Updates globalRegions.ts documentation.

--- apps/src/util/globalRegions.ts
+++ apps/src/util/globalRegions.ts
@@ -15,7 +15,36 @@ export const currentGlobalConfiguration: () => object = () =>
   (Regions as {[key: string]: unknown})[currentGlobalRegion()] || {};
 
 /**
- * This queries the global configuration for the given key
+ * This queries the current global configuration for the given key.
+ *
+ * For instance, if our region config has:
+ *
+ * ```
+ * teacher-homepage:
+ *   incubator-banner: false
+ * ```
+ *
+ * We can get "false" back by calling this function as:
+ *
+ * ```
+ * currentGlobalConfigurationFor("teacherHomepage.incubatorBanner")
+ * ```
+ *
+ * If it is something more complicated:
+ *
+ * ```
+ * teacher-homepage:
+ *   header-image:
+ *     src: "/images/farsi-header.png"
+ * ```
+ *
+ * Then, calling the function with `teacherHomepage.headerImage` will give you
+ * the object inside. In this case: `{src: "/images/farsi-header.png"}`.
+ *
+ * If `false` is the value, the function will always return a false-y result,
+ * and otherwise will always return a true-y result.
+ *
+ * @param {string} key - The '.'-delimited key to read from the region configuration.
  */
 export const currentGlobalConfigurationFor: (key: string) => object = key => {
   // Split by '.' and then tap into the object
