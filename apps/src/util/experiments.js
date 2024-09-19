9bb169cbecfda25dfff97770e6cf0613fde97fc1
carl-codeorg <131809324+carl-codeorg@users.noreply.github.com>
2024-09-19 10:47:26 -0700

    LTI unlinking (#61052)
    Account unlinking for LTI

--- apps/src/util/experiments.js
+++ apps/src/util/experiments.js
@@ -66,6 +66,7 @@ experiments.TEACHER_LOCAL_NAV_V2 = 'teacher-local-nav-v2';
 // Enables LMS cards in the LoginTypePicker during section creation
 experiments.SECTION_CREATE_LMS_CARDS = 'section_create_lms_cards';
 experiments.AI_ASSESSMENTS_ANNOUNCEMENT = 'ai-assessments-announcement';
+experiments.LTI_ACCOUNT_UNLINKING = 'lti_account_unlinking';
 
 /**
  * This was a gamified version of the finish dialog, built in 2018,
