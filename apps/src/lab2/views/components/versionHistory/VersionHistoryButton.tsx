fab7e60da37d26fbfbec31ce9c127d41b57922b8
Erin Bond <erin.bond@code.org>
2024-09-20 13:10:49 -0400

    add tooltip to version history button in codebridge

--- apps/src/lab2/views/components/versionHistory/VersionHistoryButton.tsx
+++ apps/src/lab2/views/components/versionHistory/VersionHistoryButton.tsx
@@ -3,6 +3,7 @@ import React, {useCallback, useState} from 'react';
 
 import Alert from '@cdo/apps/componentLibrary/alert';
 import {Button} from '@cdo/apps/componentLibrary/button';
+import WithTooltip from '@cdo/apps/componentLibrary/tooltip/WithTooltip';
 import {isReadOnlyWorkspace} from '@cdo/apps/lab2/lab2Redux';
 import Lab2Registry from '@cdo/apps/lab2/Lab2Registry';
 import lab2I18n from '@cdo/apps/lab2/locale';
@@ -85,17 +86,24 @@ const VersionHistoryButton: React.FunctionComponent<VersionHistoryProps> = ({
     [isVersionHistoryOpen, loadError, loading]
   );
 
+  const tooltipProps = {
+    text: commonI18n.versionHistory_header(),
+    direction: 'onLeft',
+    tooltipId: 'version-history-tooltip',
+  };
   return (
     <>
-      <Button
-        isIconOnly
-        icon={{iconStyle: 'solid', iconName: 'history'}}
-        color={'black'}
-        onClick={toggleVersionHistory}
-        ariaLabel={commonI18n.versionHistory_header()}
-        size={'xs'}
-        disabled={buttonDisabled}
-      />
+      <WithTooltip tooltipProps={tooltipProps}>
+        <Button
+          isIconOnly
+          icon={{iconStyle: 'solid', iconName: 'history'}}
+          color={'black'}
+          onClick={toggleVersionHistory}
+          ariaLabel={commonI18n.versionHistory_header()}
+          size={'xs'}
+          disabled={buttonDisabled}
+        />
+      </WithTooltip>
       {(loading || loadError) && (
         <div className={moduleStyles.versionHistoryDropdown} ref={menuRef}>
           {loading && (
