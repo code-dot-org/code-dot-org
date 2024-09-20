824b507747cdc2683ca925d8291cc649efbc6ea7
Seth Nickell <snickell@gmail.com>
2024-09-20 13:43:18 -1000

    undo changes to aichat_request_chat_completion_job.rb

--- dashboard/app/jobs/aichat_request_chat_completion_job.rb
+++ dashboard/app/jobs/aichat_request_chat_completion_job.rb
@@ -112,12 +112,8 @@ class AichatRequestChatCompletionJob < ApplicationJob
     # TODO: Check for PII. Currently we don't check for PII but we plan to add post-launch.
   end
 
-  private def gpt_enabled?(role)
-    DCDO.get("aichat_safety_gpt_enabled_#{role}", true)
-  end
-
   private def comprehend_enabled?(role)
-    DCDO.get("aichat_safety_comprehend_enabled_#{role}", false)
+    DCDO.get("aichat_safety_comprehend_enabled_#{role}", true)
   end
 
   private def webpurify_enabled?(role)
