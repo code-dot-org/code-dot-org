0b169ceeb3ee9b6add9867440ae87ff27da8b409
Molly Moen <molly@code.org>
2024-09-20 14:21:37 -0700

    update colors

--- apps/src/codebridge/components/SwapLayoutDropdown.tsx
+++ apps/src/codebridge/components/SwapLayoutDropdown.tsx
@@ -12,15 +12,10 @@ import moduleStyles from './swap-layout-dropdown.module.scss';
 
 /*
   Please note - this is a fairly brittle component in that it's only allowing toggling between
-  horizontal and vertical layouts. That's...kinda fine? For now? Probably?
+  horizontal and vertical layouts.
 
   At some point in the future, we may need to expand the functionality to select between arbitrary
-  layouts which are available - we'll probably want to iterate through the keys of the labeledGridLayouts
-  and have each of them provide an icon (to add to the button) and a nextState (to show which layout to toggle to)
-  or alternatively, maybe this whole component should become a pop up list to let the user select it? Maybe this one
-  stays around as the easy way to flip between just horizontal and vertical?
-
-  Anyway, again, it's fine as is, but will require refactoring if we expand use cases.
+  layouts which are available.
 */
 
 const SwapLayoutDropdown: React.FunctionComponent = () => {
