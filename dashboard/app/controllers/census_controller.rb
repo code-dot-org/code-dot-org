80457eb8952efbe8dcf4937ed78ca648c550130f
Bethany Connor <bethany@code.org>
2024-09-24 12:29:25 -0400

    Allow embedding the map in an iframe

--- dashboard/app/controllers/census_controller.rb
+++ dashboard/app/controllers/census_controller.rb
@@ -1,5 +1,6 @@
 class CensusController < ApplicationController
   def embeddable_census_map
     view_options(no_header: true, no_footer: true, white_background: true, full_width: true)
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
   end
 end
