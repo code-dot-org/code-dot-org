0d04fc2547b9933c92373493ec5f90339480f10f
Bethany Connor <bethany@code.org>
2024-09-24 14:22:32 -0400

    Another attempt to make this embeddable

--- dashboard/app/controllers/census_controller.rb
+++ dashboard/app/controllers/census_controller.rb
@@ -1,6 +1,7 @@
 class CensusController < ApplicationController
   def embeddable_census_map
-    view_options(no_header: true, no_footer: true, white_background: true, full_width: true)
+    view_options(no_header: true, no_footer: true, full_width: true, no_padding_container: true)
     response.headers['X-Frame-Options'] = 'ALLOWALL'
+    response.headers['Content-Security-Policy'] = "frame-ancestors *;"
   end
 end
