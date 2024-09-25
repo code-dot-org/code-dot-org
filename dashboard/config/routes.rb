fa86256455b83c23e75a3761ac23f2c990ba965d
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-09-25 11:51:06 -0700

    isolate lti module

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -1,6 +1,7 @@
 # For documentation see, e.g., http://guides.rubyonrails.org/routing.html.
 
 Dashboard::Application.routes.draw do
+  mount Lti::Engine => '/lti/v1'
   # Override Error Codes
   get "404", to: "application#render_404", via: :all
 
@@ -623,36 +624,6 @@ Dashboard::Application.routes.draw do
     post '/admin/gatekeeper/set', to: 'dynamic_config#gatekeeper_set', as: 'gatekeeper_set'
     get '/admin/dcdo', to: 'dynamic_config#dcdo_show', as: 'dcdo_show'
     post '/admin/dcdo/set', to: 'dynamic_config#dcdo_set', as: 'dcdo_set'
-
-    # LTI API endpoints
-    match '/lti/v1/login(/:platform_id)', to: 'lti_v1#login', via: [:get, :post]
-    match '/lti/v1/authenticate', to: 'lti_v1#authenticate', via: [:get, :post]
-    match '/lti/v1/sync_course', to: 'lti_v1#sync_course', via: [:get, :post]
-    post '/lti/v1/integrations', to: 'lti_v1#create_integration'
-    get '/lti/v1/integrations', to: 'lti_v1#new_integration'
-    post '/lti/v1/upgrade_account', to: 'lti_v1#confirm_upgrade_account'
-    namespace :lti do
-      namespace :v1 do
-        resource :feedback, controller: :feedback, only: %i[create show]
-        controller :dynamic_registration do
-          get 'dynamic_registration', action: :new_registration
-          post 'dynamic_registration', action: :create_registration
-        end
-        resources :sections, only: [] do
-          collection do
-            patch :bulk_update_owners
-          end
-        end
-        namespace :account_linking do
-          get :landing
-          get :existing_account
-          get :finish_link
-          post :link_email
-          post :new_account
-          post :unlink
-        end
-      end
-    end
 
     # OAuth endpoints
     get '/oauth/jwks', to: 'oauth_jwks#jwks'
