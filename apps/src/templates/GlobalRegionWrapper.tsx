bb28a5d547f2b45c6d7901a9d3e313d58349e145
wilkie <wilkie05@gmail.com>
2024-09-25 07:05:21 +0000

    Updates configuration to allow ordering of filters and fixes UI test.

--- apps/src/templates/GlobalRegionWrapper.tsx
+++ apps/src/templates/GlobalRegionWrapper.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import {currentGlobalConfiguration} from '@cdo/apps/util/globalRegions';
+import {
+  currentGlobalConfiguration,
+  RegionConfigurationPageObject,
+} from '@cdo/apps/util/globalRegions';
 
 interface GlobalRegionWrapperProps {
   component: React.FunctionComponent;
@@ -14,19 +17,23 @@ export const GlobalRegionWrapper: React.FunctionComponent<
   const EmptyComponent = () => null;
 
   // Get the 'pages' object from the current global region configuration
-  const pages = currentGlobalConfiguration().pages || {};
+  const pages = currentGlobalConfiguration().pages || [];
 
-  // Filters the config down to the first matching page
-  const pageConfigKey: string | undefined = (
-    Object.keys(pages) as Array<string>
-  ).find(pageMask =>
-    RegExp('^/global/.*' + pageMask + '(?:/.*)?').test(window.location.pathname)
-  );
+  // Filters the config down to the first matching page with a setting for
+  // the given component
+  const pageConfig: RegionConfigurationPageObject | undefined = pages
+    .filter(
+      filterInfo =>
+        RegExp('^/global/.*' + filterInfo.path + '(?:/.*)?').test(
+          window.location.pathname
+        ) && componentId in filterInfo.components
+    )
+    .slice(-1)[0];
 
   // The component is visible if the key does not exist or the value for that
   // component is explicitly set to 'false'
   const visible: boolean =
-    !pageConfigKey || pages[pageConfigKey][componentId] !== false;
+    !pageConfig || pageConfig.components[componentId] !== false;
 
   // If it is not visible, we use an "EmptyComponent" instead of the normal one
   // and ignore the given properties.
@@ -37,8 +44,8 @@ export const GlobalRegionWrapper: React.FunctionComponent<
   // We then allow overriding properties with the ones given in the region
   // configuration if it is given
   const wrappedProps = visible
-    ? pageConfigKey && pages[pageConfigKey][componentId]
-      ? {...props, ...(pages[pageConfigKey as string][componentId] as object)}
+    ? pageConfig && pageConfig.components[componentId]
+      ? {...props, ...(pageConfig.components[componentId] as object)}
       : props
     : {};
 
