c0c1f682a309b4d40373f92c68fe08753916a527
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-09-26 13:25:57 -0700

    add lti route

--- base/config/routes.rb
+++ base/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Lti::Engine, at: "/lti/v1"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
