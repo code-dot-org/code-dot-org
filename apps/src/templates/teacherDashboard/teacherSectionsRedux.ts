0a764fd352080f3d495c6f11520cfe1445563e0e
Ceara Chewning <ceara.chewning@code.org>
2024-09-27 12:27:54 -0400

    Implement the rest of the reducers

--- apps/src/templates/teacherDashboard/teacherSectionsRedux.ts
+++ apps/src/templates/teacherDashboard/teacherSectionsRedux.ts
@@ -4,13 +4,15 @@ import _ from 'lodash';
 import {OAuthSectionTypes} from '@cdo/apps/accounts/constants';
 import {ParticipantAudience} from '@cdo/apps/generated/curriculum/sharedCourseConstants';
 import {
+  PlGradeValue,
   SectionLoginType,
-  // PlGradeValue,
 } from '@cdo/generated-scripts/sharedConstants';
 
 import {
   sectionFromServerSection,
   studentFromServerStudent,
+  newSectionData,
+  USER_EDITABLE_SECTION_PROPS,
 } from './teacherSectionsReduxSelectors';
 import {
   AssignmentCourseOffering,
@@ -20,6 +22,8 @@ import {
   ServerSection,
   SectionMap,
   Student,
+  UserEditableSection,
+  SectionInstructor,
 } from './types/teacherSectionTypes';
 
 type RosterProvider = string | keyof typeof SectionLoginType | null;
@@ -51,7 +55,7 @@ interface TeacherSectionState {
   // We can edit exactly one section at a time.
   // While editing we store that section's 'in-progress' state separate from
   // its persisted state in the sections map.
-  sectionBeingEdited: number | null;
+  sectionBeingEdited: Section | null;
   showSectionEditDialog: boolean;
   saveInProgress: boolean;
   // Track whether we've async-loaded our section and assignment data
@@ -65,13 +69,20 @@ interface TeacherSectionState {
   // Not populated until the RosterDialog is opened.
   classrooms: null;
   // Error that occurred while loading oauth classrooms
-  loadError: null;
+  loadError: {status: number; message: string} | null;
   // The page where the action is occurring
   pageType: string;
   // DCDO Flag - show/hide Lock Section field
-  showLockSectionField: null;
+  showLockSectionField: boolean | null;
   ltiSyncResult: null;
   isLoadingSectionData: boolean;
+  initialCourseId?: number | null;
+  initialUnitId?: number | null;
+  initialCourseOfferingId?: number | null;
+  initialCourseVersionId?: number | null;
+  initialLoginType?: keyof typeof SectionLoginType;
+  coteacherInvite?: SectionInstructor;
+  coteacherInviteForPl?: SectionInstructor;
 }
 
 /** @const {null} null used to indicate no section selected */
@@ -280,8 +291,170 @@ const sectionSlice = createSlice({
         };
       },
     },
+    setCourseOfferings(
+      state,
+      action: PayloadAction<AssignmentCourseOffering[]>
+    ) {
+      state.courseOfferings = action.payload;
+      state.courseOfferingsAreLoaded = true;
+    },
+    setAvailableParticipantTypes(state, action: PayloadAction<string[]>) {
+      state.availableParticipantTypes = action.payload;
+    },
+    setShowLockSectionField(state, action: PayloadAction<boolean>) {
+      state.showLockSectionField = action.payload;
+    },
+    setSectionCodeReviewExpiresAt(
+      state,
+      action: PayloadAction<{
+        sectionId: number;
+        codeReviewExpiresAt: string;
+      }>
+    ) {
+      // const {sectionId, codeReviewExpiresAt} = action.payload;
+      const section = state.sections[action.payload.sectionId];
+      if (!section) {
+        throw new Error('section does not exist');
+      }
+
+      state.sections = {
+        ...state.sections,
+        [action.payload.sectionId]: {
+          ...state.sections[action.payload.sectionId],
+          codeReviewExpiresAt: action.payload.codeReviewExpiresAt
+            ? Date.parse(action.payload.codeReviewExpiresAt)
+            : null,
+        },
+      };
+    },
+    removeSection(state, action: PayloadAction<number>) {
+      const sectionId = action.payload;
+      const section = state.sections[sectionId];
+      if (!section) {
+        throw new Error('section does not exist');
+      }
+      state.sectionIds = _.without(state.sectionIds, sectionId);
+      state.studentSectionIds = _.without(state.studentSectionIds, sectionId);
+      state.plSectionIds = _.without(state.plSectionIds, sectionId);
+      state.sections = _.omit(state.sections, sectionId);
+    },
+    beginCreatingSection(
+      state,
+      action: PayloadAction<{
+        courseOfferingId: number;
+        courseVersionId: number;
+        unitId: number;
+        participantType: string;
+      }>
+    ) {
+      const initialSectionData = newSectionData(
+        action.payload.participantType
+      ) as Section;
+      if (action.payload.courseOfferingId) {
+        initialSectionData.courseOfferingId = action.payload.courseOfferingId;
+      }
+      if (action.payload.courseVersionId) {
+        initialSectionData.courseVersionId = action.payload.courseVersionId;
+      }
+      if (action.payload.unitId) {
+        initialSectionData.unitId = action.payload.unitId;
+      }
+      state.initialCourseId = initialSectionData.courseId;
+      state.initialUnitId = initialSectionData.unitId;
+      state.initialCourseOfferingId = initialSectionData.courseOfferingId;
+      state.initialCourseVersionId = initialSectionData.courseVersionId;
+      state.initialLoginType = initialSectionData.loginType;
+      state.sectionBeingEdited = initialSectionData;
+    },
+    beginEditingSection(
+      state,
+      action: PayloadAction<{sectionId?: number | null; silent?: boolean}>
+    ) {
+      const silent = action.payload.silent ? action.payload.silent : false;
+      const initialParticipantType =
+        state.availableParticipantTypes.length === 1
+          ? state.availableParticipantTypes[0]
+          : undefined;
+      const initialSectionData = action.payload.sectionId
+        ? {...state.sections[action.payload.sectionId]}
+        : newSectionData(initialParticipantType);
+      return {
+        ...state,
+        initialCourseId: initialSectionData.courseId,
+        initialUnitId: initialSectionData.unitId,
+        initialCourseOfferingId: initialSectionData.courseOfferingId,
+        initialCourseVersionId: initialSectionData.courseVersionId,
+        initialLoginType: initialSectionData.loginType,
+        sectionBeingEdited: initialSectionData,
+        showSectionEditDialog: !silent,
+      };
+    },
+    editSectionProperties(state, action: PayloadAction<UserEditableSection>) {
+      if (!state.sectionBeingEdited) {
+        throw new Error(
+          'Cannot edit section properties; no section is' +
+            ' currently being edited.'
+        );
+      }
+
+      for (const key in action.payload) {
+        if (!USER_EDITABLE_SECTION_PROPS.includes(key)) {
+          throw new Error(`Cannot edit property ${key}; it's not allowed.`);
+        }
+      }
+
+      // PL Sections must use email logins and its grade value should be "pl"
+      if (
+        action.payload.participantType &&
+        action.payload.participantType !== ParticipantAudience.student
+      ) {
+        state.sectionBeingEdited.loginType = SectionLoginType.email;
+        state.sectionBeingEdited.grades = [PlGradeValue];
+      }
+
+      if (action.payload.unitId && action.payload.lessonExtras === undefined) {
+        state.sectionBeingEdited.lessonExtras = true;
+      }
+
+      if (
+        action.payload.unitId &&
+        action.payload.ttsAutoplayEnabled === undefined
+      ) {
+        state.sectionBeingEdited.ttsAutoplayEnabled = false;
+      }
+
+      state.sectionBeingEdited = {
+        ...state.sectionBeingEdited,
+        ...action.payload,
+      };
+    },
+    cancelEditingSession(state) {
+      state.sectionBeingEdited = null;
+    },
+    setCoteacherInvite(state, action: PayloadAction<SectionInstructor>) {
+      state.coteacherInvite = action.payload;
+    },
+    setCoteacherInviteForPl(state, action: PayloadAction<SectionInstructor>) {
+      state.coteacherInviteForPl = action.payload;
+    },
+    cancelImportRosterFlow(state) {
+      state.isRosterDialogOpen = false;
+      state.rosterProvider = null;
+      state.classrooms = null;
+    },
+    rosterImportFailed(
+      state,
+      action: PayloadAction<{status: number; message: string}>
+    ) {
+      state.loadError = {
+        status: action.payload.status,
+        message: action.payload.message,
+      };
+    },
   },
 });
+
+//Thunks
 
 // pageType describes the current route the user is on. Used only for logging.
 // Enum of allowed values:
@@ -298,8 +471,6 @@ export const {
   setRosterProvider,
   setPageType,
   updateSectionAiTutorEnabled,
-
-  // Not implemented yet
   selectSection,
   setSections,
   updateSelectedSection,
@@ -308,10 +479,26 @@ export const {
   setRosterProviderName,
   finishLoadingSectionData,
 
+  // Not implemented yet
+  setCourseOfferings,
+  setAvailableParticipantTypes,
+  setShowLockSectionField,
+  setSectionCodeReviewExpiresAt,
+  removeSection,
+  beginCreatingSection,
+  beginEditingSection,
+  editSectionProperties,
+  cancelEditingSession,
+  setCoteacherInvite,
+  setCoteacherInviteForPl,
+  cancelImportRosterFlow,
+  rosterImportFailed,
   // Lots of other reducers need to be implemented too, but not essential for
   // type-checking (perhaps not included from any TS files?)
 } = {
   ...sectionSlice.actions,
+
+  // Method signatures to allow compiler type-checking to pass. Delete when implemented.
 };
 
 export default sectionSlice.reducer;
