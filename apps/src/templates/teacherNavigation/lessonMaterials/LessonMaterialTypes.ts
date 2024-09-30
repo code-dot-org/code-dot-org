6fb108b39e9fe35aa91aa497e52393318a29343c
David Bailey <davidsbailey@users.noreply.github.com>
2024-09-30 11:36:19 -0700

    extract MaterialType and computeMaterialType into LessonmaterialTypes.ts

--- apps/src/templates/teacherNavigation/lessonMaterials/LessonMaterialTypes.ts
+++ apps/src/templates/teacherNavigation/lessonMaterials/LessonMaterialTypes.ts
@@ -1,3 +1,5 @@
+import {isGDocsUrl} from '@cdo/apps/templates/lessonOverview/googleDocsUtils';
+
 export type Lesson = {
   name: string;
   id: number;
@@ -18,4 +20,36 @@ export type Resource = {
   downloadUrl?: string;
   audience: string;
   type: string;
+};
+
+export type MaterialType =
+  | 'SLIDES'
+  | 'GOOGLE_DOC'
+  | 'VIDEO'
+  | 'LESSON_PLAN'
+  | 'STANDARDS'
+  | 'VOCABULARY'
+  | 'LINK';
+
+export const computeMaterialType = (
+  resourceType: string,
+  resourceUrl: string
+): MaterialType => {
+  if (isGDocsUrl(resourceUrl)) {
+    if (resourceType === 'Slides') {
+      return 'SLIDES';
+    } else {
+      return 'GOOGLE_DOC';
+    }
+  } else if (resourceType === 'Video') {
+    return 'VIDEO';
+  } else if (resourceType === 'Lesson Plan') {
+    return 'LESSON_PLAN';
+  } else if (resourceType === 'Standards') {
+    return 'STANDARDS';
+  } else if (resourceType === 'Vocabulary') {
+    return 'VOCABULARY';
+  } else {
+    return 'LINK';
+  }
 };
--- apps/src/templates/teacherNavigation/lessonMaterials/ResourceIcon.tsx
+++ apps/src/templates/teacherNavigation/lessonMaterials/ResourceIcon.tsx
@@ -2,8 +2,8 @@ import classNames from 'classnames';
 import React from 'react';
 
 import FontAwesomeV6Icon from '@cdo/apps/componentLibrary/fontAwesomeV6Icon/FontAwesomeV6Icon';
-import {isGDocsUrl} from '@cdo/apps/templates/lessonOverview/googleDocsUtils';
 
+import {computeMaterialType} from './LessonMaterialTypes';
 import {RESOURCE_TYPE} from './ResourceIconType';
 
 import styles from './lesson-materials.module.scss';
@@ -13,41 +13,9 @@ type ResourceIconProps = {
   resourceUrl: string;
 };
 
-type MaterialType =
-  | 'SLIDES'
-  | 'GOOGLE_DOC'
-  | 'VIDEO'
-  | 'LESSON_PLAN'
-  | 'STANDARDS'
-  | 'VOCABULARY'
-  | 'LINK';
-
 const computeIconType = (resourceType: string, resourceUrl: string) => {
   const materialType = computeMaterialType(resourceType, resourceUrl);
   return RESOURCE_TYPE[materialType];
-};
-
-const computeMaterialType = (
-  resourceType: string,
-  resourceUrl: string
-): MaterialType => {
-  if (isGDocsUrl(resourceUrl)) {
-    if (resourceType === 'Slides') {
-      return 'SLIDES';
-    } else {
-      return 'GOOGLE_DOC';
-    }
-  } else if (resourceType === 'Video') {
-    return 'VIDEO';
-  } else if (resourceType === 'Lesson Plan') {
-    return 'LESSON_PLAN';
-  } else if (resourceType === 'Standards') {
-    return 'STANDARDS';
-  } else if (resourceType === 'Vocabulary') {
-    return 'VOCABULARY';
-  } else {
-    return 'LINK';
-  }
 };
 
 const ResourceIcon: React.FC<ResourceIconProps> = ({
