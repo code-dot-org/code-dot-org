ac3900331dd9acbd96e367ff0524bc40687f8470
Molly Moen <molly@code.org>
2024-09-30 15:35:54 -0700

    Revert "[JIMT][CT-446] upload files to codebridge (#61403)"
    This reverts commit cfc8059820e7d7e2a83764e2145dcdf6470211e2.

--- apps/src/codebridge/utils/viewableImageFileType.ts
+++
@@ -1,8 +0,0 @@
-const defaultViewableImageFileTypesArray = ['png', 'jpg', 'gif'];
-export const viewableImageFileType = (
-  extension: string,
-  viewableImageFileTypesArray = defaultViewableImageFileTypesArray
-) => {
-  const viewableImageFileTypes = new Set(viewableImageFileTypesArray);
-  return viewableImageFileTypes.has(extension);
-};
