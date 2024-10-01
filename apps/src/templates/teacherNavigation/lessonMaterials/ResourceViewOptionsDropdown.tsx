f06ce95b4c2bfd0767e99b6f34f055d9d8b9cbf9
David Bailey <davidsbailey@users.noreply.github.com>
2024-10-01 12:34:53 -0700

    customize resource dropdown options

--- apps/src/templates/teacherNavigation/lessonMaterials/ResourceViewOptionsDropdown.tsx
+++ apps/src/templates/teacherNavigation/lessonMaterials/ResourceViewOptionsDropdown.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import {ActionDropdown} from '@cdo/apps/componentLibrary/dropdown';
+import {isGDocsUrl} from '@cdo/apps/templates/lessonOverview/googleDocsUtils';
 
-import {Resource} from './LessonMaterialTypes';
+import {Resource, computeMaterialType} from './LessonMaterialTypes';
 
 type ResourceViewOptionsDropdownProps = {
   resource: Resource;
@@ -11,52 +12,68 @@ type ResourceViewOptionsDropdownProps = {
 const ResourceViewOptionsDropdown: React.FC<
   ResourceViewOptionsDropdownProps
 > = ({resource}) => {
-  // This is a dummy options array that will be replaced with actual options in TEACH-1326
-  const dummyOptions = [
-    {
-      value: 'view',
-      label: 'View',
-      icon: {iconName: 'check'},
-      onClick: () => console.log('view clicked'),
-    },
-    {
-      value: 'download-video',
-      label: 'Download',
-      icon: {iconName: 'download'},
-      onClick: () => console.log('download-video clicked'),
-    },
-    {
-      value: 'download-pdf',
-      label: 'Download (PDF)',
-      icon: {iconName: 'download'},
-      onClick: () => console.log('download-pdf clicked'),
-    },
-    {
-      value: 'download-office',
-      label: 'Download (Microsoft Office)',
-      icon: {iconName: 'download'},
-      onClick: () => console.log('download-office clicked'),
-    },
-    {
-      value: 'copy-google-docs',
-      label: 'Make a Copy (Google Docs)',
-      icon: {iconName: 'copy'},
-      onClick: () => console.log('copy-google-docs clicked'),
-    },
-    {
-      value: 'watch-youtube',
-      label: 'Watch on Youtube',
-      icon: {iconName: 'youtube'},
-      onClick: () => console.log('watch-youtube clicked'),
-    },
-  ];
+  const materialType = computeMaterialType(resource.type, resource.url);
+
+  const getDropdownOptions = () => {
+    if (materialType === 'VIDEO') {
+      return [
+        {
+          value: 'download-video',
+          label: 'Download',
+          icon: {iconName: 'download'},
+          onClick: () => console.log('download-video clicked'),
+        },
+        {
+          value: 'watch-youtube',
+          label: 'Watch on Youtube',
+          icon: {iconName: 'youtube'},
+          onClick: () => console.log('watch-youtube clicked'),
+        },
+      ];
+    }
+
+    const options = [
+      {
+        value: 'view',
+        label: 'View',
+        icon: {iconName: 'check'},
+        onClick: () => console.log('view clicked'),
+      },
+    ];
+    if (resource.downloadUrl) {
+      options.push({
+        value: 'download',
+        label: 'Download',
+        icon: {iconName: 'download'},
+        onClick: () => console.log('download clicked'),
+      });
+    }
+
+    if (isGDocsUrl(resource.url)) {
+      options.push({
+        value: 'download-office',
+        label: 'Download (Microsoft Office)',
+        icon: {iconName: 'download'},
+        onClick: () => console.log('download-office clicked'),
+      });
+      options.push({
+        value: 'copy-google-docs',
+        label: 'Make a Copy (Google Docs)',
+        icon: {iconName: 'copy'},
+        onClick: () => console.log('copy-google-docs clicked'),
+      });
+    }
+
+    return options;
+  };
+  const dropdownOptions = getDropdownOptions();
 
   return (
     <div data-testid={'view-options-dropdown'}>
       <ActionDropdown
         name="view-options"
         labelText="View options dropdown"
-        options={dummyOptions}
+        options={dropdownOptions}
         size="s"
         menuPlacement="right"
         triggerButtonProps={{
