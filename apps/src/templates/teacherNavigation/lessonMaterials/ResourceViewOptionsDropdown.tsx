ed8cfc9a8e98f471f3b8a1d10321f28244b3e259
David Bailey <davidsbailey@users.noreply.github.com>
2024-10-01 14:38:22 -0700

    implement specific behavior of each dropdown item

--- apps/src/templates/teacherNavigation/lessonMaterials/ResourceViewOptionsDropdown.tsx
+++ apps/src/templates/teacherNavigation/lessonMaterials/ResourceViewOptionsDropdown.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 
 import {ActionDropdown} from '@cdo/apps/componentLibrary/dropdown';
-import {isGDocsUrl} from '@cdo/apps/templates/lessonOverview/googleDocsUtils';
+import {
+  isGDocsUrl,
+  gDocsMsOfficeUrl,
+  gDocsCopyUrl,
+  gDocsPdfUrl,
+} from '@cdo/apps/templates/lessonOverview/googleDocsUtils';
 
 import {Resource, computeMaterialType} from './LessonMaterialTypes';
 
 type ResourceViewOptionsDropdownProps = {
   resource: Resource;
+};
+
+// includes test for leading slash in lesson plan link
+const normalizeUrl = (url: string) => {
+  const httpRegex = /https?:\/\//;
+  if (httpRegex.test(url) || url.startsWith('/')) {
+    return url;
+  } else {
+    return 'https://' + url;
+  }
+};
+
+const openDownloadUrl = (url: string) => {
+  url = normalizeUrl(url);
+  window.open(url, '_self');
+};
+
+const openInNewTab = (url: string) => {
+  url = normalizeUrl(url);
+  window.open(url, '_blank', 'noopener,noreferrer');
 };
 
 const ResourceViewOptionsDropdown: React.FC<
@@ -16,52 +41,80 @@ const ResourceViewOptionsDropdown: React.FC<
 
   const getDropdownOptions = () => {
     if (materialType === 'VIDEO') {
-      return [
-        {
+      const options = [];
+      if (resource.downloadUrl) {
+        options.push({
           value: 'download-video',
           label: 'Download',
           icon: {iconName: 'download'},
-          onClick: () => console.log('download-video clicked'),
+          onClick: () => {
+            if (resource.downloadUrl) {
+              openDownloadUrl(resource.downloadUrl);
+            }
+          },
+        });
+      }
+      options.push({
+        value: 'watch',
+        label: 'Watch',
+        icon: {iconName: 'video'},
+        onClick: () => {
+          openInNewTab(resource.url);
         },
-        {
-          value: 'watch-youtube',
-          label: 'Watch on Youtube',
-          icon: {iconName: 'youtube'},
-          onClick: () => console.log('watch-youtube clicked'),
-        },
-      ];
+      });
+      return options;
     }
 
     const options = [
       {
         value: 'view',
         label: 'View',
-        icon: {iconName: 'check'},
-        onClick: () => console.log('view clicked'),
+        icon: {iconName: 'eye'},
+        onClick: () => {
+          openInNewTab(resource.url);
+        },
       },
     ];
-    if (resource.downloadUrl) {
-      options.push({
-        value: 'download',
-        label: 'Download',
-        icon: {iconName: 'download'},
-        onClick: () => console.log('download clicked'),
-      });
-    }
 
     if (isGDocsUrl(resource.url)) {
+      options.push({
+        value: 'download-pdf',
+        label: 'Download (PDF)',
+        icon: {iconName: 'download'},
+        onClick: () => {
+          openDownloadUrl(gDocsPdfUrl(resource.url));
+        },
+      });
       options.push({
         value: 'download-office',
         label: 'Download (Microsoft Office)',
         icon: {iconName: 'download'},
-        onClick: () => console.log('download-office clicked'),
+        onClick: () => {
+          openDownloadUrl(gDocsMsOfficeUrl(resource.url));
+        },
       });
       options.push({
         value: 'copy-google-docs',
         label: 'Make a Copy (Google Docs)',
         icon: {iconName: 'copy'},
-        onClick: () => console.log('copy-google-docs clicked'),
+        onClick: () => {
+          openInNewTab(gDocsCopyUrl(resource.url));
+        },
       });
+    } else {
+      // Exclude download link for gdocs, which already have PDF download links.
+      if (resource.downloadUrl) {
+        options.push({
+          value: 'download',
+          label: 'Download',
+          icon: {iconName: 'download'},
+          onClick: () => {
+            if (resource.downloadUrl) {
+              openDownloadUrl(resource.downloadUrl);
+            }
+          },
+        });
+      }
     }
 
     return options;
