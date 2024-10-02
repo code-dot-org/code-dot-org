fc2c990b70acc88873f0a8717c5a2bb98c8e1392
Alice Fisher <alice.fisher@code.org>
2024-10-02 19:41:18 +0000

    add TeacherOnboardingModal file

---
+++ apps/src/aiComponentLibrary/warningModal/TeacherOnboardingModal.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import Button, {buttonColors} from '@cdo/apps/componentLibrary/button/Button';
+import {
+  BodyTwoText,
+  Heading3,
+  StrongText,
+} from '@cdo/apps/componentLibrary/typography';
+import AccessibleDialog from '@cdo/apps/sharedComponents/AccessibleDialog';
+import i18n from '@cdo/locale';
+
+import moduleStyles from './onboarding-modal.module.scss';
+
+/**
+ * Renders a modal that warns the user to chat responsibly with AI.
+ */
+
+export interface TeacherOnboardingModalProps {
+  onClose: () => void;
+}
+
+const TeacherOnboardingModal: React.FunctionComponent<
+  TeacherOnboardingModalProps
+> = ({onClose}) => (
+  <AccessibleDialog
+    onClose={onClose}
+    className={moduleStyles.TeacherOnboardingModal}
+  >
+    <div className={moduleStyles.headerContainer}>
+      <Heading3>{i18n.aiWarningModalHeader()}</Heading3>
+    </div>
+    <hr />
+    <div className={moduleStyles.onboardingModuleTextContainer}>
+      <BodyTwoText>
+        <StrongText>{i18n.aiWarningModalMessagesAreRecorded()}</StrongText>
+      </BodyTwoText>
+      <BodyTwoText>{i18n.aiWarningModalInappropriateFlagged()}</BodyTwoText>
+      <BodyTwoText>{i18n.aiWarningUnsaved()}</BodyTwoText>
+      <BodyTwoText>{i18n.aiWarningModalPersonalNotSubmitted()}</BodyTwoText>
+    </div>
+    <hr />
+    <div className={moduleStyles.bottomSection}>
+      <Button
+        onClick={onClose}
+        color={buttonColors.purple}
+        text={i18n.aiWarningModalOk()}
+      />
+    </div>
+  </AccessibleDialog>
+);
+export default TeacherOnboardingModal;
