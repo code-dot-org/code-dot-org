6eac2608937d1d59d226a6288b0c0d6270dca09b
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-10-03 07:40:25 -0700

    use native scripts

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -3,14 +3,7 @@
 # Read PATH from devcontainer defaults
 . $HOME/.profile
 
-bundle exec rake install
-bundle exec rake package
-
-cd dashboard
-bundle exec rake dashboard:setup_db
-cd ..
-
-cd apps
-yarn install
-yarn build
-cd ..
+git lfs pull && \
+bundle exec rake install:hooks && \
+bundle exec rake install && \
+bundle exec rake build
