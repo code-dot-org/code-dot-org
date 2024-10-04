e36c947b97b2528caca3f93bfa8ce2acdd17db95
Turner Riley <turnertriley@gmail.com>
2024-10-04 16:06:16 -0400

    remove sessionstorage used on same page for student signup

--- apps/test/unit/signUpFlow/FinishStudentAccountTest.tsx
+++ apps/test/unit/signUpFlow/FinishStudentAccountTest.tsx
@@ -4,14 +4,6 @@ import sinon from 'sinon'; // eslint-disable-line no-restricted-imports
 
 import FinishStudentAccount from '@cdo/apps/signUpFlow/FinishStudentAccount';
 import locale from '@cdo/apps/signUpFlow/locale';
-import {
-  IS_PARENT_SESSION_KEY,
-  PARENT_EMAIL_SESSION_KEY,
-  PARENT_EMAIL_OPT_IN_SESSION_KEY,
-  USER_AGE_SESSION_KEY,
-  USER_STATE_SESSION_KEY,
-  USER_GENDER_SESSION_KEY,
-} from '@cdo/apps/signUpFlow/signUpFlowConstants';
 
 describe('FinishStudentAccount', () => {
   let fetchStub: sinon.SinonStub;
@@ -102,72 +94,6 @@ describe('FinishStudentAccount', () => {
 
     expect(screen.queryByText(locale.what_state_are_you_in())).toBe(null);
     expect(screen.queryByText(locale.state_error_message())).toBe(null);
-  });
-
-  it('userAge is tracked in sessionStorage', () => {
-    renderDefault();
-    const userAgeInput = screen.getAllByRole('combobox')[0];
-
-    expect(sessionStorage.getItem(USER_AGE_SESSION_KEY)).toBe(null);
-
-    fireEvent.change(userAgeInput, {target: {value: '5'}});
-    expect(sessionStorage.getItem(USER_AGE_SESSION_KEY)).toBe('5');
-  });
-
-  it('userState is tracked in sessionStorage', () => {
-    renderDefault();
-    const userStateInput = screen.getAllByRole('combobox')[1];
-
-    expect(sessionStorage.getItem(USER_STATE_SESSION_KEY)).toBe(null);
-
-    fireEvent.change(userStateInput, {target: {value: 'WA'}});
-    expect(sessionStorage.getItem(USER_STATE_SESSION_KEY)).toBe('WA');
-  });
-
-  it('userGender is tracked in sessionStorage', () => {
-    renderDefault();
-    const userGender = 'Female';
-    const userGenderInput = screen.getAllByRole('textbox')[1];
-
-    expect(sessionStorage.getItem(USER_GENDER_SESSION_KEY)).toBe(null);
-
-    fireEvent.change(userGenderInput, {target: {value: userGender}});
-    expect(sessionStorage.getItem(USER_GENDER_SESSION_KEY)).toBe(userGender);
-  });
-
-  it('userParentEmail is tracked in sessionStorage', () => {
-    renderDefault();
-
-    // Click parent checkbox to show parent email field
-    fireEvent.click(screen.getAllByRole('checkbox')[0]);
-    expect(sessionStorage.getItem(IS_PARENT_SESSION_KEY)).toBe('true');
-
-    // Check parent email field
-    const userParentEmail = 'parent@adult.com';
-    const userParentEmailInput = screen.getAllByRole('textbox')[0];
-
-    expect(sessionStorage.getItem(PARENT_EMAIL_SESSION_KEY)).toBe(null);
-
-    fireEvent.change(userParentEmailInput, {target: {value: userParentEmail}});
-    expect(sessionStorage.getItem(PARENT_EMAIL_SESSION_KEY)).toBe(
-      userParentEmail
-    );
-  });
-
-  it('userParentEmailOptIn is tracked in sessionStorage', () => {
-    renderDefault();
-
-    // Click parent checkbox to show parent email opt-in field
-    fireEvent.click(screen.getAllByRole('checkbox')[0]);
-    expect(sessionStorage.getItem(IS_PARENT_SESSION_KEY)).toBe('true');
-
-    // Check parent email opt-in field
-    expect(sessionStorage.getItem(PARENT_EMAIL_OPT_IN_SESSION_KEY)).toBe(null);
-
-    fireEvent.click(screen.getAllByRole('checkbox')[1]);
-    expect(sessionStorage.getItem(PARENT_EMAIL_OPT_IN_SESSION_KEY)).toBe(
-      'true'
-    );
   });
 
   it('finish student signup button starts disabled', () => {
