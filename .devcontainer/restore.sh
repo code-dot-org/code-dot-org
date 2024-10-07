7c83e0ca7bef3aa7f6e415407950c15636a77da4
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-10-07 14:39:40 -0700

    add restore script

--- .devcontainer/restore.sh
+++ .devcontainer/restore.sh
@@ -7,9 +7,11 @@ set -x
 
 git lfs pull && \
 bundle exec rake install:hooks && \
+bundle exec rake install:locals_yml && \
 mysql -uroot -hdb < ../bootstrap-osx.sql && \
+bundle exec rake package && \
 cd apps && \
+rm -rf node_modules && \
 yarn install && \
 yarn build && \
-cd .. && \
-bundle exec rake package
+cd ..
