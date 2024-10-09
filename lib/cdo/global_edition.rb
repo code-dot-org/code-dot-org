a44305e034058aca1dad05a1337114870a4357dc
wilkie <wilkie05@gmail.com>
2024-10-09 22:39:04 +0000

    Makes GlobalEdition.regions a constant REGIONS.

--- lib/cdo/global_edition.rb
+++ lib/cdo/global_edition.rb
@@ -19,20 +19,11 @@ module Cdo
     end
 
     # Retrieves a list a global region names.
-    def self.regions
-      unless defined?(@@regions)
-        root = File.expand_path(File.join('..', '..'), __dir__)
-        @@regions = Dir.glob(File.join(root, "config", "global", "*.yml")).map do |path|
-          File.basename(path, File.extname(path)).intern
-        end.freeze
-      end
-
-      @@regions
-    end
+    REGIONS = Dir.glob('*.yml', base: CDO.dir('config', 'global')).map {|f| File.basename(f, '.yml')}.freeze
 
     # Returns the parsed configuration for the given region.
     def self.load_config(region)
-      raise ArgumentError, "Region #{region} is not available" unless regions.include?(region.intern)
+      raise ArgumentError, "Region #{region} is not available" unless REGIONS.include?(region.to_s)
       configs = YAML.load_file(CDO.dir('config', 'global', "#{region}.yml")) || {}
       configs.deep_symbolize_keys
     end
