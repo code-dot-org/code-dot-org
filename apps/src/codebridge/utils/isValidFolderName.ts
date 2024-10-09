3b0cac82cb351ba42d673e9bae188d61c3a0c87a
Jim Thomason <jim.thomason@code.org>
2024-10-09 18:01:57 -0500

    jimt/CT-617 - tests and docs (#61665)

--- apps/src/codebridge/utils/isValidFolderName.ts
+++ apps/src/codebridge/utils/isValidFolderName.ts
@@ -1,3 +1,10 @@
-// restrict typed in input to what we consider to be valid names, which for now are just word characters or hyphens
+/**
+ * Checks if a given string is a valid folder name.
+ *
+ * A valid folder name consists of one or more word characters (letters, numbers, underscores) or hyphens.
+ *
+ * @param name - The string to check.
+ * @returns `true` if the string is a valid folder name, `false` otherwise.
+ */
 export const isValidFolderName = (name: string = '') =>
   Boolean(name.match(/^[\w-]+$/));
--- apps/src/codebridge/utils/previewFileType.ts
+++ apps/src/codebridge/utils/previewFileType.ts
@@ -1,4 +1,12 @@
 const defaultPreviewFileTypesArray = ['html'];
+
+/**
+ * Determines if a given language is supported for previewing.
+ *
+ * @param language - The language to check.
+ * @param previewFileTypesArray - An optional array of supported preview file types. If not provided, the default array `['html']` is used.
+ * @returns `true` if the language is supported for previewing, `false` otherwise.
+ */
 export const previewFileType = (
   language: string,
   previewFileTypesArray = defaultPreviewFileTypesArray
