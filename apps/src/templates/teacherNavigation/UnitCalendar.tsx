574ecb925395b333be18d7237edd6be7e1cc3bc4
kobryan0619 <kaitie.obryan@gmail.com>
2024-10-10 16:27:14 -0500

    add in endpoint to load data

--- apps/src/templates/teacherNavigation/UnitCalendar.tsx
+++ apps/src/templates/teacherNavigation/UnitCalendar.tsx
@@ -1,27 +1,61 @@
 import React, {useState, useEffect} from 'react';
+// import {useSelector} from 'react-redux';
 import {useSelector} from 'react-redux';
 
 import UnitCalendarGrid from '@cdo/apps//code-studio/components/progress/UnitCalendarGrid';
+import {getAuthenticityToken} from '@cdo/apps/util/AuthenticityTokenStore';
 import i18n from '@cdo/locale';
-
-import {getCurrentUnitData} from '../sectionProgress/sectionProgressRedux';
 
 const WEEKLY_INSTRUCTIONAL_MINUTES_OPTIONS = [
   45, 90, 135, 180, 225, 270, 315, 360, 405, 450,
 ];
 export const WEEK_WIDTH = 585;
 
+interface CalendarLesson {
+  id: number;
+  lessonNumber: number;
+  title: string;
+  duration: number;
+  assessment: boolean;
+  unplugged: boolean;
+  url: string;
+}
+
 const UnitCalendar: React.FC = () => {
-  const lessons = useSelector(state => getCurrentUnitData(state)?.lessons);
+  const [lessons, setLessons] = useState<CalendarLesson[] | null>(null);
+  const [hasCalendar, setHasCalendar] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+
   const [weeklyInstructionalMinutes, setWeeklyInstructionalMinutes] =
     useState<number>(WEEKLY_INSTRUCTIONAL_MINUTES_OPTIONS[4]);
 
+  const unitName = useSelector(
+    (state: {unitSelection: {unitName: string}}) => state.unitSelection.unitName
+  );
+
   useEffect(() => {
-    if (lessons) {
-      setIsLoading(false);
+    if (!unitName) {
+      return;
     }
-  }, [lessons]);
+
+    getAuthenticityToken()
+      .then(token =>
+        fetch(`/dashboardapi/unit_summary/${unitName}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': token,
+          },
+        })
+      )
+      .then(response => response.json())
+      .then(responseJson => {
+        // initializeRedux(responseJson, dispatch, userType, userId);  commented out to see if we can just get the data
+        setLessons(responseJson.unitData.calendarLessons);
+        setHasCalendar(responseJson.unitData.showCalendar);
+        setIsLoading(false);
+      });
+  }, [unitName, lessons]);
 
   const generateDropdownOptions = () => {
     const options = WEEKLY_INSTRUCTIONAL_MINUTES_OPTIONS;
@@ -52,7 +86,7 @@ const UnitCalendar: React.FC = () => {
           {generateDropdownOptions()}
         </select>
       </div>
-      {!isLoading && (
+      {!isLoading && hasCalendar && (
         <UnitCalendarGrid
           lessons={lessons}
           weeklyInstructionalMinutes={weeklyInstructionalMinutes}
