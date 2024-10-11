226038229722ab02e8fb266b450ec77be1bade40
alex-m-brown <alex.brown@code.org>
2024-10-11 14:30:26 -0400

    Add "paranoid" flash message translation

--- dashboard/app/controllers/passwords_controller.rb
+++ dashboard/app/controllers/passwords_controller.rb
@@ -17,7 +17,7 @@ class PasswordsController < Devise::PasswordsController
     if current_user.try(:admin)
       new_user_password_path
     else
-      flash[:notice] = "If we find your email address in our system, you will receive a password recovery link in a few minutes."
+      flash[:notice] = I18n.t('password.reset_form.confirmation')
       new_session_path(resource_name) if is_navigational_format?
     end
   end
