e534d4b186ea3a950deae8fd22cdf088a8940e7a
alex-m-brown <alex.brown@code.org>
2024-10-14 16:29:18 -0400

    refactor service object

--- dashboard/lib/services/user/password_resetter.rb
+++ dashboard/lib/services/user/password_resetter.rb
@@ -1,23 +1,14 @@
 module Services
   module User
     class PasswordResetter < Services::Base
-      attr_reader :email, :user
+      attr_reader :email
 
       def initialize(email:)
         @email = email
-        @user = nil
       end
 
       def call
-        if email.blank?
-          @user = ::User.new
-          user.errors.add :email, ::I18n.t('activerecord.errors.messages.blank')
-          return user
-        end
-
-        # We are no longer sending an email to parents, so grab the first user we find
-        # (a user with an Email auth option first, otherwise any user that has that email)
-        @user = ::User.find_by_email_or_hashed_email(email)
+        return user if user.errors.present?
 
         if !user.nil? && user.authentication_options.any?(&:email?)
           user.raw_token = send_reset_password_instructions
@@ -31,18 +22,27 @@ module Services
         user
       end
 
+      private def user
+        return @user if defined? @user
+        if email.blank?
+          @user = ::User.new
+          @user.errors.add :email, ::I18n.t('activerecord.errors.messages.blank')
+        else
+          # We are no longer sending an email to parents, so grab the first user we find
+          # (a user with an Email auth option first, otherwise any user that has that email)
+          @user = ::User.find_by_email_or_hashed_email(email)
+        end
+        @user
+      end
+
       private def send_reset_password_instructions
-        raw, enc = Devise.token_generator.generate(::User, :reset_password_token)
-
-        user.reset_password_token   = enc
-        user.reset_password_sent_at = Time.now.utc
-        user.save(validate: false)
-
-        user.send_reset_email(:reset_password_instructions, raw, email)
+        raw = user.send(:set_reset_password_token)
+        user.send(:send_devise_notification, :reset_password_instructions, user.raw_token, {to: email})
         raw
       rescue ArgumentError
-        errors.add :base, I18n.t('password.reset_errors.invalid_email')
-        return nil
+        user.errors.add :base, I18n.t('password.reset_errors.invalid_email')
+        user.send(clear_reset_password_token)
+        nil
       end
     end
   end
