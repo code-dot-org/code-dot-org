abfbbe7c997d3dff2bc84252cc4b01dc8435d91e
Sanchit Malhotra <sanchit@code.org>
2024-10-15 15:52:34 -0700

    Lab2: consolidate lesson continue/finish logic

---
+++ apps/src/lab2/progress/progressThunks.tsx
@@ -0,0 +1,45 @@
+import {AnyAction, ThunkAction} from '@reduxjs/toolkit';
+
+import {
+  navigateToNextLevel,
+  sendSuccessReport,
+} from '@cdo/apps/code-studio/progressRedux';
+import {nextLevelId} from '@cdo/apps/code-studio/progressReduxSelectors';
+import {RootState} from '@cdo/apps/types/redux';
+
+import {shareLab2Project} from '../header/lab2HeaderShare';
+
+/**
+ * Handles all logic for continuing lesson progression, either to the next level or finishing the lesson.
+ */
+export const continueOrFinishLesson =
+  (): ThunkAction<void, RootState, undefined, AnyAction> =>
+  (dispatch, getState) => {
+    const levelProperties = getState().lab.levelProperties;
+    if (!levelProperties) {
+      // Level has not been set up yet.
+      return;
+    }
+
+    // If there are no validation conditions, go ahead and send a success report when we continue.
+    // Otherwise, success reports are managed by the ProgressContainer and ProgressManager.
+    if (!getState().lab.validationState.hasConditions) {
+      dispatch(sendSuccessReport(levelProperties.appName));
+    }
+
+    // If we are not at the last level, continue to the next level.
+    if (nextLevelId(getState()) !== undefined) {
+      dispatch(navigateToNextLevel());
+      return;
+    }
+
+    const {finishUrl, finishDialog} = levelProperties;
+    // If we have a finish URL, show the finish dialog if present, or redirect to the finish URL.
+    if (finishUrl) {
+      if (finishDialog) {
+        shareLab2Project(finishDialog, finishUrl);
+      } else {
+        window.location.href = finishUrl;
+      }
+    }
+  };
--- apps/src/panels/PanelsLabView.tsx
+++ apps/src/panels/PanelsLabView.tsx
@@ -5,14 +5,12 @@
 
 import React, {useCallback} from 'react';
 
-import {
-  sendSuccessReport,
-  navigateToNextLevel,
-} from '@cdo/apps/code-studio/progressRedux';
 import {useDialogControl, DialogType} from '@cdo/apps/lab2/views/dialogs';
 import {useAppDispatch, useAppSelector} from '@cdo/apps/util/reduxHooks';
 
+import {sendSuccessReport} from '../code-studio/progressRedux';
 import {queryParams} from '../code-studio/utils';
+import {continueOrFinishLesson} from '../lab2/progress/progressThunks';
 import useWindowSize from '../util/hooks/useWindowSize';
 
 import PanelsView from './PanelsView';
@@ -40,12 +38,12 @@ const PanelsLabView: React.FunctionComponent = () => {
   const onContinue = useCallback(
     (nextUrl?: string) => {
       if (nextUrl) {
-        // This is a short-term solution for the Music Lab progression in incubation.  We will not attempt
-        // to send a success report for a level that uses this feature.
+        // This is a short-term solution for the Music Lab progression in incubation.
+        // Send a success report so we turn the bubble green.
+        dispatch(sendSuccessReport(appName));
         window.location.href = nextUrl;
       } else {
-        dispatch(sendSuccessReport(appName));
-        dispatch(navigateToNextLevel());
+        dispatch(continueOrFinishLesson());
       }
     },
     [dispatch]
