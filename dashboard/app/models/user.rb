2b00d3b0bab312beee4e2788fa61ab6b8246ed4a
alex-m-brown <alex.brown@code.org>
2024-10-16 09:22:57 -0400

    P20-1206/Only send password reset to users with email auth (#61691)
    * Only send password reset to users with email auth
    
    * change passwords_controller admin behavior nd test
    
    * Update user.rb comment
    
    * Add "paranoid" flash message
    
    * Add "paranoid" flash message translation
    
    * refactor password reset into service object
    
    * refactor test to spec syntax
    
    * merge metric tracking
    
    * refactor service object
    
    * password_resetter can't return nil
    
    * update passwords_controller_test to use new string
    
    * refactor password_resetter_test.rb
    
    Co-authored-by: Artem Vavilov <artem.vavilov.7@gmail.com>
    
    * Update dashboard/lib/services/user/password_resetter.rb
    
    Co-authored-by: Artem Vavilov <artem.vavilov.7@gmail.com>
    
    ---------
    
    Co-authored-by: Artem Vavilov <artem.vavilov.7@gmail.com>

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -1714,67 +1714,7 @@ class User < ApplicationRecord
 
   def self.send_reset_password_instructions(attributes = {})
     # override of Devise method
-    if attributes[:email].blank?
-      user = User.new
-      user.errors.add :email, I18n.t('activerecord.errors.messages.blank')
-      return user
-    end
-
-    email = attributes[:email]
-    associated_users = User.associated_users(email)
-    return User.new(email: email).send_reset_password_for_users(email, associated_users)
-  end
-
-  def send_reset_password_for_users(email, users)
-    if users.empty?
-      not_found_user = User.new(email: email)
-      not_found_user.errors.add :email, :not_found
-      return not_found_user
-    end
-
-    unique_users = users.uniq
-
-    # Normal case: single user, owner of the email attached to this account
-    if unique_users.length == 1 && (unique_users.first.email == email || unique_users.first.hashed_email == User.hash_email(email))
-      primary_user = unique_users.first
-      primary_user.raw_token = primary_user.send_reset_password_instructions(email) # protected in the superclass
-      return primary_user
-    end
-
-    # One or more users are associated with parent email, generate reset tokens for each one
-    unique_users.each do |user|
-      raw, enc = Devise.token_generator.generate(User, :reset_password_token)
-      user.raw_token = raw
-      user.reset_password_token   = enc
-      user.reset_password_sent_at = Time.now.utc
-      user.save(validate: false)
-    end
-
-    begin
-      # Send the password reset to the parent
-      raw, _enc = Devise.token_generator.generate(User, :reset_password_token)
-      self.child_users = unique_users
-      send_devise_notification(:reset_password_instructions, raw, {to: email})
-      return self
-    rescue ArgumentError
-      errors.add :base, I18n.t('password.reset_errors.invalid_email')
-      return nil
-    end
-  end
-
-  # Send a password reset email to the user (not to their parent)
-  def send_reset_password_instructions(email)
-    raw, enc = Devise.token_generator.generate(self.class, :reset_password_token)
-
-    self.reset_password_token   = enc
-    self.reset_password_sent_at = Time.now.utc
-    save(validate: false)
-
-    send_devise_notification(:reset_password_instructions, raw, {to: email})
-    raw
-  rescue ArgumentError
-    errors.add :base, I18n.t('password.reset_errors.invalid_email')
-    return nil
+    Services::User::PasswordResetter.call(email: attributes[:email])
   end
 
   def reset_secrets
