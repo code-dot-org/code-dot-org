4df38f9e42566096350c1499fa918eed7c9cb054
wilkie <wilkie05@gmail.com>
2024-10-17 06:44:26 +0000

    Wraps the TeacherResources component on the TeacherHomepage.

--- apps/src/templates/studioHomepages/TeacherHomepage.jsx
+++ apps/src/templates/studioHomepages/TeacherHomepage.jsx
@@ -290,7 +290,10 @@ export const UnconnectedTeacherHomepage = ({
             isProfessionalLearningCourse={true}
           />
         )}
-        <TeacherResources />
+        <GlobalRegionWrapper
+          component={TeacherResources}
+          componentId="TeacherResources"
+        />
         {showIncubatorBanner && (
           <GlobalRegionWrapper
             component={IncubatorBanner}
