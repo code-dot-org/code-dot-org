6d122b1d4181db9ff0b2aff751faef6c5289fb3b
Vijaya Manohararaj <vijaya.manohararaj@code.org>
2024-10-21 15:26:41 -0700

    Ignoring the name suffix when computing base name to only strip off trailing digits.

--- dashboard/app/models/levels/level.rb
+++ dashboard/app/models/levels/level.rb
@@ -896,10 +896,6 @@ class Level < ApplicationRecord
   # represent a version year.
   private def base_name
     base_name = name
-    if name_suffix
-      strip_suffix_regex = /^(.*)#{Regexp.escape(name_suffix)}$/
-      base_name = name[strip_suffix_regex, 1] || name
-    end
     base_name = strip_version_year_suffixes(base_name)
     base_name
   end
