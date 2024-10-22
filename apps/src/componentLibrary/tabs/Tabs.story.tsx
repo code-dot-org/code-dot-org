97297e3b5ad2aea70e509641ff55b9e9ad5f13bc
denyslevada <levada.denys@gmail.com>
2024-10-22 14:24:32 +0300

    * update stories

--- apps/src/componentLibrary/tabs/Tabs.story.tsx
+++ apps/src/componentLibrary/tabs/Tabs.story.tsx
@@ -689,5 +689,61 @@ GroupOfTabsWithClosableTabs.args = {
       onChange: () => null,
       type: 'secondary',
     },
+    {
+      name: 'primary_tabs_with_closable_tabs_dark',
+      tabs: [
+        {
+          value: 'tab1',
+          text: 'Tab 1',
+          tabContent: <div>Tab 1 Content</div>,
+          isClosable: true,
+        },
+        {
+          value: 'tab2',
+          text: 'Tab 2',
+          tabContent: <div>Tab 2 Content</div>,
+          isClosable: true,
+        },
+        {
+          value: 'tab3',
+          text: 'Tab 3',
+          tabContent: <div>Tab 3 Content</div>,
+          isClosable: true,
+        },
+      ],
+      defaultSelectedTabValue: 'tab1',
+      onChange: () => null,
+      type: 'primary',
+      mode: 'dark',
+    },
+    {
+      name: 'secondary_tabs_with_closable_tabs_dark',
+      tabs: [
+        {
+          value: 'tab1',
+          text: 'Tab 1',
+          tabContent: <div>Tab 1 Content</div>,
+          isClosable: true,
+        },
+        {
+          value: 'tab2',
+          text: 'Tab 2',
+          iconLeft: {iconName: 'check', iconStyle: 'solid'},
+          iconRight: {iconName: 'check', iconStyle: 'solid'},
+          tabContent: <div>Tab 2 Content</div>,
+          isClosable: true,
+        },
+        {
+          value: 'tab3',
+          text: 'Tab 3',
+          tabContent: <div>Tab 3 Content</div>,
+          isClosable: true,
+        },
+      ],
+      defaultSelectedTabValue: 'tab1',
+      onChange: () => null,
+      type: 'secondary',
+      mode: 'dark',
+    },
   ],
 };
