c15a0d37110e6db150c5533e07eb7f56b5c5d1d4
Nick Lathe <nick.lathe@code.org>
2024-10-24 11:48:53 -0700

    Fix/improve functionality after revert
      - Add safe navigation operator when accessing session key
      - Remove unused private method

--- dashboard/lib/metrics/events.rb
+++ dashboard/lib/metrics/events.rb
@@ -30,7 +30,7 @@ module Metrics
         event_value = event_name if event_value.nil?
         enabled_experiments = get_enabled_experiments && user.present? ? user.get_active_experiment_names : nil
         managed_test_environment = CDO.running_web_application? && CDO.test_system?
-        statsig_stable_id = session[:statsig_stable_id]
+        statsig_stable_id = session&.dig(:statsig_stable_id)
 
         if CDO.rack_env?(:development)
           log_event_to_stdout(user: user, event_name: event_name, event_value: event_value, metadata: metadata, enabled_experiments: enabled_experiments, statsig_stable_id: statsig_stable_id)
@@ -86,17 +86,6 @@ module Metrics
             enabled_experiments: enabled_experiments,
             stableID: statsig_stable_id,
           }.compact,
-          event_name: event_name,
-          event_value: event_value,
-          metadata: metadata,
-        }
-        puts "Logging Event: #{event_details.to_json}"
-      end
-
-      # Logs an event to stdout, useful for development and debugging
-      private def log_event_to_stdout_with_session(session:, event_name:, event_value:, metadata:)
-        event_details = {
-          user_id: session[:statsig_stable_id],
           event_name: event_name,
           event_value: event_value,
           metadata: metadata,
