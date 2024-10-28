feef9fdfb9e8361115ba7ae643d1b7dee9ce519f
Liam Frye-Mason <liam.fryemason@code.org>
2024-10-28 12:25:23 -0700

    Fix

--- apps/src/templates/teacherNavigation/TeacherNavFlagUtils.ts
+++ apps/src/templates/teacherNavigation/TeacherNavFlagUtils.ts
@@ -3,8 +3,9 @@ import _ from 'lodash';
 import DCDO from '@cdo/apps/dcdo';
 import experiments from '@cdo/apps/util/experiments';
 
-export const showV2TeacherDashboard = () =>
-  _.once(() => {
+export const showV2TeacherDashboard: () => boolean = _.once(() => {
+  return (
     DCDO.get('teacher-local-nav-v2', false) ||
-      experiments.isEnabled('teacher-local-nav-v2');
-  });
+    experiments.isEnabled('teacher-local-nav-v2')
+  );
+});
