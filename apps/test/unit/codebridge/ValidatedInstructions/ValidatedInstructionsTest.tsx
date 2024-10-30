e17a9c79358f65c673f9cb588cd1a42cd7fc91b3
Molly Moen <molly@code.org>
2024-10-30 16:26:35 -0700

    add validated level test

--- apps/test/unit/codebridge/ValidatedInstructions/ValidatedInstructionsTest.tsx
+++ apps/test/unit/codebridge/ValidatedInstructions/ValidatedInstructionsTest.tsx
@@ -9,7 +9,7 @@ import progress, {
 } from '@cdo/apps/code-studio/progressRedux';
 import {CodebridgeContextProvider} from '@cdo/apps/codebridge';
 import ValidatedInstructions from '@cdo/apps/codebridge/InfoPanel/ValidatedInstructions';
-import lab, {onLevelChange} from '@cdo/apps/lab2/lab2Redux';
+import lab, {onLevelChange, setValidationState} from '@cdo/apps/lab2/lab2Redux';
 import lab2Project, {setHasEdited} from '@cdo/apps/lab2/redux/lab2ProjectRedux';
 import predictLevel from '@cdo/apps/lab2/redux/predictLevelRedux';
 import lab2System, {setHasRun} from '@cdo/apps/lab2/redux/systemRedux';
@@ -54,7 +54,7 @@ describe('ValidatedInstructions', () => {
     );
   }
 
-  it('continue button is visible for an already-passed level', () => {
+  it('Continue button is visible for an already-passed level', () => {
     // Default progress state is on a level that has already passed.
     renderDefault();
 
@@ -62,7 +62,7 @@ describe('ValidatedInstructions', () => {
     screen.getByRole('button', {name: commonI18n.continue()});
   });
 
-  it('for a non-validated level, continue button shows up when you have edited and run code', () => {
+  it('For a non-validated level, continue button shows up when you have edited and run code', () => {
     // Level 1 in the progression, which is "in progress"
     store.dispatch(setCurrentLevelId('1'));
     renderDefault();
@@ -74,6 +74,29 @@ describe('ValidatedInstructions', () => {
     // Update edit and run flags in redux
     store.dispatch(setHasRun(true));
     store.dispatch(setHasEdited(true));
+
+    // Continue button should be present.
+    screen.getByRole('button', {name: commonI18n.continue()});
+  });
+
+  it('For a validated level, continue button shows up when you have passed tests', () => {
+    // Level 3 in the progression is validated and not yet passed
+    store.dispatch(setCurrentLevelId('3'));
+    renderDefault();
+
+    expect(
+      screen.queryByRole('button', {name: commonI18n.continue()})
+    ).toBeNull();
+
+    // Set the validation to passed
+    store.dispatch(
+      setValidationState({
+        hasConditions: true,
+        satisfied: true,
+        message: 'Passed',
+        index: 0,
+      })
+    );
 
     // Continue button should be present.
     screen.getByRole('button', {name: commonI18n.continue()});
