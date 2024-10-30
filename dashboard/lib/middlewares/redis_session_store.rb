cf4c80158be0e36084d20269a5760533bc51ef14
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-10-31 01:09:45 +0200

    Update middleware

--- dashboard/lib/middlewares/redis_session_store.rb
+++ dashboard/lib/middlewares/redis_session_store.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Middlewares
   class RedisSessionStore < ActionDispatch::Session::RedisStore
     # Extends +ActionDispatch::Request::Session+ to allow identifying whether a session has been changed.
@@ -8,6 +6,7 @@ module Middlewares
         @changed.present?
       end
 
+      # @see https://github.com/rails/rails/blob/v6.1.7.7/actionpack/lib/action_dispatch/request/session.rb#L229-L231
       private def load_for_write!
         @changed = true
         super
@@ -30,6 +29,7 @@ module Middlewares
       # it's likely due to the presence of options like +:max_age+ and +:expire_after+,
       # which are intended to prolong the session.
       # In this case, we can skip committing for AJAX requests to reduce Redis traffic.
+      # See: https://github.com/rack/rack/blob/v2.2.9/lib/rack/session/abstract/id.rb#L355-L357
       result = !req.xhr? if result && !session.changed? && forced_session_update?(session, options)
 
       result
