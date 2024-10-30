ef88a0597ce5443a34c1da9bbac130a02f742dce
Jim Thomason <jim@japanesewartuba.com>
2024-10-30 12:46:34 -0500

    jimt/CT-822 - MaybeDraggable

--- apps/src/codebridge/FileBrowser/index.tsx
+++ apps/src/codebridge/FileBrowser/index.tsx
@@ -37,7 +37,7 @@ import {
   DndDataContextProvider,
   useDndDataContext,
 } from './DnDDataContextProvider';
-import {Draggable} from './Draggable';
+import {Draggable, NotDraggable} from './Draggable';
 import {Droppable} from './Droppable';
 import {FileBrowserHeaderPopUpButton} from './FileBrowserHeaderPopUpButton';
 import FileRow from './FileRow';
@@ -230,16 +230,15 @@ const InnerFileBrowser = React.memo(
               setFileType,
               enableMenu: !dragData?.id || isDraggingLocked,
             };
-            return isDraggingLocked ? (
-              <FileRow {...fileRowProps} />
-            ) : (
-              <Draggable
+            const MaybeDraggable = isDraggingLocked ? NotDraggable : Draggable;
+            return (
+              <MaybeDraggable
                 data={{id: f.id, type: DragType.FILE, parentId: f.folderId}}
                 key={f.id}
                 Component="li"
               >
                 <FileRow {...fileRowProps} />
-              </Draggable>
+              </MaybeDraggable>
             );
           })}
       </>
