2c5657455fc02d2ad4ec5a8fadab3d1946df790a
Hannah Bergam <hannahbergam@gmail.com>
2024-10-31 14:21:01 -0700

    pr feedback

--- apps/src/sites/studio/pages/devise/sessions/_login.js
+++ apps/src/sites/studio/pages/devise/sessions/_login.js
@@ -4,6 +4,7 @@ import {EVENTS, PLATFORMS} from '@cdo/apps/metrics/AnalyticsConstants';
 import analyticsReporter from '@cdo/apps/metrics/AnalyticsReporter';
 import statsigReporter from '@cdo/apps/metrics/StatsigReporter';
 import {USER_RETURN_TO_SESSION_KEY} from '@cdo/apps/signUpFlow/signUpFlowConstants';
+import getScriptData from '@cdo/apps/util/getScriptData';
 
 $(document).ready(() => {
   analyticsReporter.sendEvent(EVENTS.LOGIN_PAGE_VISITED, {}, PLATFORMS.STATSIG);
@@ -14,10 +15,11 @@ $(document).ready(() => {
     false
   );
   const signupLink = document.getElementById('signup-link');
+  const {userReturnTo} = getScriptData('data');
 
   if (isInSignupExperiment) {
-    if (window.userReturnTo) {
-      sessionStorage.setItem(USER_RETURN_TO_SESSION_KEY, window.userReturnTo);
+    if (userReturnTo) {
+      sessionStorage.setItem(USER_RETURN_TO_SESSION_KEY, userReturnTo);
     }
     signupLink.href = './new_sign_up/account_type';
   }
