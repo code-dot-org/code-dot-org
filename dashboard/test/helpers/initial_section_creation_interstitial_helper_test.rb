e5c2374f2fb0362dc28aa944992980e7b9eef1e0
Turner Riley <turnertriley@gmail.com>
2024-11-01 15:31:22 -0400

    dont show two dialogs

--- dashboard/test/helpers/initial_section_creation_interstitial_helper_test.rb
+++ dashboard/test/helpers/initial_section_creation_interstitial_helper_test.rb
@@ -3,31 +3,45 @@ require 'test_helper'
 # Tests following spec:
 # We should pop up this interstitial only for teachers on their first sign-in.
 class InitialSectionCreationInterstitialHelperTest < ActiveSupport::TestCase
+  setup do
+    @country_code = 'US'
+    @request = ActionDispatch::Request.new({'HTTP_CLOUDFRONT_VIEWER_COUNTRY' => @country_code.downcase})
+  end
+
   test 'does not show a dialog if user is not a teacher' do
     @user = create :user
 
     refute @user.teacher?
-    refute InitialSectionCreationInterstitialHelper.show?(@user)
+    refute InitialSectionCreationInterstitialHelper.show?(@user, @request)
   end
 
   test 'does not show a dialog if the user has signed in before' do
     @teacher = create :teacher
     @teacher.update(sign_in_count: 2)
 
-    refute InitialSectionCreationInterstitialHelper.show?(@teacher)
+    refute InitialSectionCreationInterstitialHelper.show?(@teacher, @request)
   end
 
   test 'shows the dialog if the teacher signed in for the first time' do
     @teacher = create :teacher
     @teacher.update(sign_in_count: 1)
 
-    assert InitialSectionCreationInterstitialHelper.show?(@teacher)
+    assert InitialSectionCreationInterstitialHelper.show?(@teacher, @request)
   end
 
   test 'does not show the dialog if the teacher is an LTI user' do
     @teacher = create :teacher, :with_lti_auth
     @teacher.update(sign_in_count: 1)
 
-    refute InitialSectionCreationInterstitialHelper.show?(@teacher)
+    refute InitialSectionCreationInterstitialHelper.show?(@teacher, @request)
+  end
+
+  test 'does not show the dialog if gdpr dialog is showing' do
+    eu_country_code = 'FR'
+    eu_request = ActionDispatch::Request.new({'HTTP_CLOUDFRONT_VIEWER_COUNTRY' => eu_country_code.downcase})
+    @teacher = create :teacher
+    @teacher.update(sign_in_count: 1)
+
+    refute InitialSectionCreationInterstitialHelper.show?(@teacher, eu_request)
   end
 end
