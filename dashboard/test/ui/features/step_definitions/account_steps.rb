ec3512fcd3609f1f790ba280e444961e00cb9397
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-11-05 15:07:25 +0200

    P20-906: Remove ui test

--- dashboard/test/ui/features/step_definitions/account_steps.rb
+++ dashboard/test/ui/features/step_definitions/account_steps.rb
@@ -37,23 +37,6 @@ Given(/^I sign in as "([^"]*)" from the sign in page$/) do |name|
   GHERKIN
 end
 
-Given /^I sign in and out as "([^"]*)" (\d+) times$/ do |name, times|
-  repeated_steps = ''
-
-  times.times do
-    repeated_steps += <<~GHERKIN
-      When I am on "http://studio.code.org/users/sign_in"
-      And element ".alert-danger:contains(You are already signed in)" is not visible
-      Then I sign in as "#{name}" from the sign in page
-      And I click selector "#header_display_name" once I see it
-      And I click selector "#user-signout" to load a new page
-      And I wait to see "#header_user_signin"
-    GHERKIN
-  end
-
-  steps repeated_steps
-end
-
 Given(/^I am a (student|teacher)( and go home)?$/) do |user_type, home|
   random_name = "Test#{user_type.capitalize} " + SecureRandom.base64
   steps "And I create a #{user_type} named \"#{random_name}\"#{home}"
