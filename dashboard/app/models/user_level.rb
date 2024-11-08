d364862d6f1e9b2c4e55d97da34a7e0bde2ae3e0
Artem Vavilov <artem.vavilov.7@gmail.com>
2024-11-08 16:34:17 +0200

    P20-1273: Validate locale only when it changes

--- dashboard/app/models/user_level.rb
+++ dashboard/app/models/user_level.rb
@@ -42,7 +42,7 @@ class UserLevel < ApplicationRecord
   after_save :after_submit, if: :submitted_or_resubmitted?
   before_save :before_unsubmit, if: ->(ul) {ul.submitted_changed? from: true, to: false}
 
-  validates :locale, inclusion: {in: I18n.available_locales.as_json}, allow_nil: true
+  validates :locale, inclusion: {in: I18n.available_locales.as_json}, allow_nil: true, if: :locale_changed?
 
   # TODO(asher): Consider making these scopes and the methods below more consistent, in tense and in
   # word choice.
