55e788e55e015d74b0f628411846e34262942309
alex-m-brown <alex.brown@code.org>
2024-11-14 14:34:40 -0500

    improve tests

--- dashboard/test/lib/services/user/password_resetter_by_email_test.rb
+++ dashboard/test/lib/services/user/password_resetter_by_email_test.rb
@@ -9,25 +9,27 @@ class Services::User::PasswordResetterByEmailTest < ActiveSupport::TestCase
     subject(:reset_password) {described_instance.call}
 
     let(:mail) {ActionMailer::Base.deliveries.first}
+    let!(:user) {create(:user, email: email)}
 
-    context 'for email without an existing user' do
-      let!(:user) {nil}
-      it 'does not send password reset' do
-        reset_password
-        _(mail).must_be_nil
-      end
-    end
-    context 'for email with an existing user' do
-      let!(:user) {create(:user, email: email)}
-      it 'returns user' do
-        reset_password.must_equal user
-      end
+    it 'returns user with generated reset password tokens and sends an email' do
+      _reset_password.must_equal user
+      _(reset_password.reset_password_token).wont_be_nil
+      _(reset_password.reset_password_sent_at).wont_be_nil
+      _(reset_password.raw_token).wont_be_nil
+
+      _(mail).wont_be_nil
+      _(mail.to).must_equal [email]
+      _(mail.subject).must_equal 'Code.org reset password instructions'
     end
 
-    context 'for de-migrated account' do
-      let!(:user) {create(:teacher, :demigrated, email: email)}
-      it 'sends password reset instructions' do
-        reset_password
+    context 'when account is unmigrated' do
+      let!(:user) {create(:user, :demigrated, email: email)}
+
+      it 'returns user with generated reset password tokens and sends an email' do
+        _reset_password.must_equal user
+        _(reset_password.reset_password_token).wont_be_nil
+        _(reset_password.reset_password_sent_at).wont_be_nil
+        _(reset_password.raw_token).wont_be_nil
 
         _(mail).wont_be_nil
         _(mail.to).must_equal [email]
@@ -35,25 +37,46 @@ class Services::User::PasswordResetterByEmailTest < ActiveSupport::TestCase
       end
     end
 
-    context 'for lti account' do
-      let!(:user) {create(:teacher, :with_lti_auth, email: email)}
+    context 'when account is an lti account' do
+      let!(:user) {create(:teacher, :with_lti_auth)}
+      # Email is changed by lti auth option, so we need to reference that email
+      let!(:email) {user.email}
 
-      context 'without email authentication' do
-        it 'does not send password reset' do
+      context 'when account does not have email authentication' do
+        it 'tracks PasswordResetEmailAuthNotFound metric' do
+          expected_env = 'expected_env'
+
+          CDO.expects(:rack_env).returns(expected_env)
+          Cdo::Metrics.
+            expects(:put).
+            with('User', 'PasswordResetEmailAuthNotFound', 1, {Environment: expected_env}).
+            once
+
           reset_password
+        end
+
+        it 'returns new user instance without reset password tokens and does not send email' do
+          _reset_password.must_be_instance_of User
+          _(reset_password.reset_password_token).must_be_nil
+          _(reset_password.reset_password_sent_at).must_be_nil
+          _(reset_password.raw_token).must_be_nil
+
           _(mail).must_be_nil
         end
       end
 
-      context 'with email authentication' do
+      context 'when account has email authentication' do
         let(:email_auth_option) {create(:authentication_option, email: email)}
 
         before do
           user.authentication_options.append(email_auth_option)
         end
 
-        it 'sends password reset email' do
-          reset_password
+        it 'returns user with generated reset password tokens and sends an email' do
+          _reset_password.must_equal user
+          _(reset_password.reset_password_token).wont_be_nil
+          _(reset_password.reset_password_sent_at).wont_be_nil
+          _(reset_password.raw_token).wont_be_nil
 
           _(mail).wont_be_nil
           _(mail.to).must_equal [email]
@@ -62,28 +85,69 @@ class Services::User::PasswordResetterByEmailTest < ActiveSupport::TestCase
       end
     end
 
-    context 'for google account' do
+    context 'when account is a google account' do
       let!(:user) {create(:teacher, :with_google_authentication_option, email: email)}
 
-      context 'without email authentication' do
+      context 'when account does not have email authentication' do
         before do
           user.authentication_options.find_by(credential_type: 'email').destroy
         end
 
-        it 'does not send password reset' do
+        it 'tracks PasswordResetEmailAuthNotFound metric' do
+          expected_env = 'expected_env'
+
+          CDO.expects(:rack_env).returns(expected_env)
+          Cdo::Metrics.
+            expects(:put).
+            with('User', 'PasswordResetEmailAuthNotFound', 1, {Environment: expected_env}).
+            once
+
           reset_password
+        end
+
+        it 'returns new user instance without reset password tokens and does not send email' do
+          _reset_password.must_be_instance_of User
+          _(reset_password.reset_password_token).must_be_nil
+          _(reset_password.reset_password_sent_at).must_be_nil
+          _(reset_password.raw_token).must_be_nil
+
           _(mail).must_be_nil
         end
       end
 
-      context 'with email authentication' do
-        it 'sends password reset email' do
-          reset_password
+      context 'when account has email authentication' do
+        it 'returns user with generated reset password tokens and sends an email' do
+          _reset_password.must_equal user
+          _(reset_password.reset_password_token).wont_be_nil
+          _(reset_password.reset_password_sent_at).wont_be_nil
+          _(reset_password.raw_token).wont_be_nil
 
           _(mail).wont_be_nil
           _(mail.to).must_equal [email]
           _(mail.subject).must_equal 'Code.org reset password instructions'
         end
+      end
+    end
+    context 'when user does not exist' do
+      let(:user) {nil}
+
+      it 'tracks PasswordResetUserNotFound metric' do
+        expected_env = 'expected_env'
+
+        CDO.expects(:rack_env).returns(expected_env)
+        Cdo::Metrics.
+          expects(:put).
+          with('User', 'PasswordResetUserNotFound', 1, {Environment: expected_env}).
+          once
+
+        reset_password
+      end
+
+      it 'returns new user instance without reset password tokens' do
+        _reset_password.must_be_instance_of User
+        _(reset_password.reset_password_token).must_be_nil
+        _(reset_password.reset_password_sent_at).must_be_nil
+        _(reset_password.raw_token).must_be_nil
       end
     end
   end
