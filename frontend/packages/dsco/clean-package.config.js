17523e3318103cc35b4dd2c619608d66d8fff4a9
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-11-19 17:21:22 +0000

    chore(dsco): add clean-package entrypoint support

---
+++ frontend/packages/dsco/clean-package.config.js
@@ -0,0 +1,34 @@
+const glob = require('glob');
+const {basename, dirname} = require('path');
+
+const entryPoints = glob.sync('./components/**/index.ts', {
+    posix: true,
+    nodir: true
+})
+
+const componentExports = {};
+
+for (const entryPoint of entryPoints) {
+    const componentName = basename(dirname(entryPoint))
+
+    componentExports[`./${componentName}`] = {
+        "types": `./dist/${componentName}/index.d.ts`,
+        "import": `./dist/${componentName}/index.mjs`,
+        "require": `./dist/${componentName}/index.js`,
+    };
+
+    componentExports[`./${componentName}/index.css`] = `./dist/${componentName}/index.css`;
+}
+
+module.exports = {
+    "remove": ["devDependencies"],
+    "replace": {
+        "main": "dist/index.js",
+        "module": "dist/index.mjs",
+        "types": "dist/index.d.ts",
+        "exports": {
+            "./package.json": "./package.json",
+            ...componentExports
+        }
+    }
+}
