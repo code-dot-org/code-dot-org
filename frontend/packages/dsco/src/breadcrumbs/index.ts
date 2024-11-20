7a172db91816b40ec627b309e67f6ec736e4de44
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-11-20 19:41:56 +0000

    chore: remove dsco components for PR

--- frontend/packages/dsco/src/breadcrumbs/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as Breadcrumbs} from './Breadcrumbs';
-export type {BreadcrumbsProps} from './Breadcrumbs';
-export {default as default} from './Breadcrumbs';
--- frontend/packages/dsco/src/chips/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as Chips} from './Chips';
-export type {ChipsProps} from './Chips';
-export {default as default} from './Chips';
