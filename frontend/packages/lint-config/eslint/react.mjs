c9375e86c76cc6b4c3094fdd1375703d108f061a
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-11-22 15:37:10 -0800

    chore(eslint): use jsx-runtime as Code.org is on react 17+
    Prior to React 17, an `import React from "react"` statement was
    required. However, after React 17, a new jsx runtime was introduced
    eliminating this requirement. As such, change to use the `jsx-runtime`
    recommended configuration.

--- frontend/packages/lint-config/eslint/react.mjs
+++ frontend/packages/lint-config/eslint/react.mjs
@@ -10,7 +10,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ...pluginReact.configs.flat.recommended,
+    ...pluginReact.configs.flat['jsx-runtime'],
     settings: {react: {version: 'detect'}},
   }
 ];
