0e9342c59afb4949e6c38a92f8f00cecb861e67d
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-11-22 10:52:38 -0800

    chore(tsc): add typechecking

--- frontend/apps/design-system-storybook/.storybook/main.ts
+++ frontend/apps/design-system-storybook/.storybook/main.ts
@@ -5,7 +5,7 @@ import {StorybookConfig} from "@storybook/react-webpack5";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
+function getAbsolutePath(value: string) {
   return dirname(require.resolve(join(value, "package.json")));
 }
 
