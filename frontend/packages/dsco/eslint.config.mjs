752c5aaa5399b8413156b83aaa24cdcf4a9fef26
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-11-22 09:00:35 -0800

    feat(eslint): add linter configs for `frontend`

---
+++ frontend/packages/dsco/eslint.config.mjs
@@ -0,0 +1,18 @@
+import cdoReactConfig from '@code-dot-org/lint-config/eslint/react.mjs';
+import storybook from 'eslint-plugin-storybook'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ['dist/*'],
+  },
+  ...storybook.configs['flat/recommended'],
+  ...cdoReactConfig,
+  {
+    // Allow `clean-package` to use require as it does not work in ESM today.
+    files: ["clean-package.config.cjs"],
+    rules: {
+      '@typescript-eslint/no-require-imports': "off"
+    }
+  }
+];
---
+++ frontend/packages/lint-config/eslint/node.mjs
@@ -0,0 +1,9 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: {globals: {...globals.node}}},
+  pluginJs.configs.recommended,
+
+];
