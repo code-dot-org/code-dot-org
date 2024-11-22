d7fdc3020d4c643d21d869c6d0403ce74d4f4e63
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-11-22 13:54:17 -0800

    feat(jest): add jest unit testing support to `frontend`
    This change adds jest and react-testing-library to allow for unit testing.
    The stub samples were updated to include copilot generated unit tests,
    these are meant to be removed when actual components are created.
    
    The following tools were added:
    
    * jest: unit testing framework
    * @swc/jest: transpile typescript jest code to js
    * jsdom: emulate the dom in unit tests

--- frontend/packages/dsco/eslint.config.mjs
+++ frontend/packages/dsco/eslint.config.mjs
@@ -1,4 +1,5 @@
 import cdoReactConfig from '@code-dot-org/lint-config/eslint/react.mjs';
+import cdoJestConfig from '@code-dot-org/lint-config/eslint/jest.mjs';
 import storybook from 'eslint-plugin-storybook';
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -8,6 +9,7 @@ export default [
   },
   ...storybook.configs['flat/recommended'],
   ...cdoReactConfig,
+  ...cdoJestConfig,
   {
     // Allow `clean-package` to use require as it does not work in ESM today.
     files: ['clean-package.config.cjs'],
