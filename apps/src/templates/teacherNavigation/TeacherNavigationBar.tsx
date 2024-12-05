723665965ce20fb8a94e2300168b8d2c9c204f5b
Mark Barnes <mark.barnes@code.org>
2024-12-05 14:03:05 -0600

    change redirect logic

--- apps/src/templates/teacherNavigation/TeacherNavigationBar.tsx
+++ apps/src/templates/teacherNavigation/TeacherNavigationBar.tsx
@@ -26,14 +26,6 @@ import {
 
 import styles from './teacher-navigation.module.scss';
 
-// interface SectionsData {
-//   [sectionId: number]: {
-//     name: string;
-//     hidden: boolean;
-//     courseVersionName: string;
-//   };
-// }
-
 const TeacherNavigationBar: React.FunctionComponent = () => {
   const sections = useAppSelector(state => state.teacherSections.sections);
 
