9770309864a623e50c3c44e7bdca66e50b5cc2ac
Molly Moen <molly@code.org>
2024-12-12 14:12:36 -0800

    add basic painter class

--- python/pythonlab/neighborhood/tests/test_painter.py
+++ python/pythonlab/neighborhood/tests/test_painter.py
@@ -2,7 +2,13 @@ from neighborhood.painter import Painter
 
 def test_initialize_painter():
   painter = Painter()
-  assert painter.x == 0
-  assert painter.y == 0
-  assert painter.direction == "East"
-  assert painter.paint == 0
+  assert painter.getX() == 0
+  assert painter.getY() == 0
+  assert painter.getDirection() == "East"
+  assert painter.getMyPaint() == 0
+
+def test_set_paint():
+  painter = Painter()
+  assert painter.getMyPaint() == 0
+  painter.setPaint(5)
+  assert painter.getMyPaint() == 5
