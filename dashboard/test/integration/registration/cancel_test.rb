7532f482ba85692cfa9f19bcf7cd95310782660d
Hannah Bergam <hannahbergam@gmail.com>
2024-12-17 15:00:23 -0800

    removing tests and commenting out ones that should be replaced

--- dashboard/test/integration/registration/cancel_test.rb
+++ dashboard/test/integration/registration/cancel_test.rb
@@ -12,7 +12,7 @@ module RegistrationsControllerTests
       PartialRegistration.expects(:delete)
 
       get '/users/cancel'
-      assert_redirected_to users_new_sign_up_account_type_path
+      assert_redirected_to new_user_registration_path
       follow_redirect!
       # User should currently see old signup flow if they
       # cancel an in-progress registration and want to start over.
