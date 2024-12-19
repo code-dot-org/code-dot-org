85265cab424d0532a136a15b2cf819182522d033
Molly Moen <molly@code.org>
2024-12-19 15:55:55 -0800

    basic attempt at not losing data

--- apps/src/codebridge/Console/ConsoleV2.tsx
+++ apps/src/codebridge/Console/ConsoleV2.tsx
@@ -10,6 +10,7 @@ import '@xterm/xterm/css/xterm.css';
 import CodebridgeRegistry from '../CodebridgeRegistry';
 import {usePreviewPanel} from '../hooks/usePreviewPanel';
 
+import {writeConsoleMessage} from './ConsoleHelper';
 import ControlButtons from './ControlButtons';
 import RightButtons from './RightButtons';
 
@@ -54,14 +55,12 @@ const ConsoleV2: React.FunctionComponent = () => {
     if (!terminalRef || terminalRef.current === null || didInit) {
       return;
     }
-    // if (CodebridgeRegistry.getInstance().getTerminal()) {
-    //   const existingTerminal = CodebridgeRegistry.getInstance().getTerminal();
-    //   existingTerminal?.open(terminalRef.current);
-    //   const fitAddon = CodebridgeRegistry.getInstance().getTerminalFitAddon();
-    //   fitAddon?.fit();
-    //   setDidInit(true);
-    //   return;
-    // }
+    const existingTerminal = CodebridgeRegistry.getInstance().getTerminal();
+    let existingTerminalData: string | undefined;
+    if (existingTerminal) {
+      existingTerminal.selectAll();
+      existingTerminalData = existingTerminal.getSelection().trim();
+    }
 
     const terminal = new Terminal({
       screenReaderMode: true,
@@ -75,6 +74,15 @@ const ConsoleV2: React.FunctionComponent = () => {
     terminal.open(terminalRef.current);
     terminal.onData(onData);
     fitAddon.fit();
+
+    // Right now we lose some data (specifically error colors)
+    // when the terminal is re-mounted. This may be fixable after
+    // this pr goes in: https://github.com/xtermjs/xterm.js/pull/5253
+    // If not, we may need to fetch the existing terminal data from redux and re-play it into the terminal :(
+    // We also can lose data due to a race condition.
+    if (existingTerminalData) {
+      writeConsoleMessage(existingTerminalData);
+    }
 
     // Prevent keyboard trap.
     terminal.attachCustomKeyEventHandler(ignoreEscapeAndTab);
