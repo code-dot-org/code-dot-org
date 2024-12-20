b8c132ed6791167a9b913d9fe22d20a6b07ed520
Carl Reiner <carl.reiner@code.org>
2024-12-20 09:44:42 -0800

    fix memoization in tests

--- dashboard/test/lib/services/user/gender_normalizer_test.rb
+++ dashboard/test/lib/services/user/gender_normalizer_test.rb
@@ -10,10 +10,12 @@ class Services::User::GenderNormalizerTest < ActiveSupport::TestCase
     context 'when female' do
       f_values = %w(f F female Female girl gal woman feminine she her mujer femail fem 여자 여 여성 kız kadın perempuan أنثى بنت انثي žena weiblich vrouw meisje kobieta fille féminin donna chica kvinde pige tjej kvinna ស្រី famale gril)
       f_values.each do |value|
-        let(:raw_input) {value}
+        context "with input #{value}" do
+          let(:raw_input) {value}
 
-        it "returns #{Policies::Gender::NormalizedValues::FEMALE} for #{value}" do
-          _(normalize).must_equal Policies::Gender::NormalizedValues::FEMALE
+          it "returns #{Policies::Gender::NormalizedValues::FEMALE}" do
+            _(normalize).must_equal Policies::Gender::NormalizedValues::FEMALE
+          end
         end
       end
     end
@@ -21,10 +23,12 @@ class Services::User::GenderNormalizerTest < ActiveSupport::TestCase
     context 'when male' do
       m_values = ['m', 'M', 'male', 'Male', 'boy', 'guy', 'man', 'masculine', 'him', 'he', 'hombre', 'dude', 'mail', '남자', '남성', 'erkek', 'laki laki', 'laki-laki', 'männlich', 'mänlich', 'jongen', 'muž', 'mężczyzna', 'homme', 'maschio', 'uomo', 'macho', 'chico', 'pojke', 'mal', 'homem']
       m_values.each do |value|
-        let(:raw_input) {value}
+        context "with input #{value}" do
+          let(:raw_input) {value}
 
-        it "returns #{Policies::Gender::NormalizedValues::MALE} for #{value}" do
-          _(normalize).must_equal Policies::Gender::NormalizedValues::MALE
+          it "returns #{Policies::Gender::NormalizedValues::MALE}" do
+            _(normalize).must_equal Policies::Gender::NormalizedValues::MALE
+          end
         end
       end
     end
@@ -32,10 +36,12 @@ class Services::User::GenderNormalizerTest < ActiveSupport::TestCase
     context 'when non-binary' do
       n_values = ['n', 'they', 'them', 'nonbinary', 'NonBinary', 'non-binary', 'non binary', 'intersex', 'inter sex', 'genderfluid', 'gender fluid', 'intergender', 'inter gender', 'agender', 'boyflux', 'trans', 'transgender', 'x']
       n_values.each do |value|
-        let(:raw_input) {value}
+        context "with input #{value}" do
+          let(:raw_input) {value}
 
-        it "returns #{Policies::Gender::NormalizedValues::NON_BINARY} for #{value}" do
-          _(normalize).must_equal Policies::Gender::NormalizedValues::NON_BINARY
+          it "returns #{Policies::Gender::NormalizedValues::NON_BINARY} for #{value}" do
+            _(normalize).must_equal Policies::Gender::NormalizedValues::NON_BINARY
+          end
         end
       end
     end
@@ -43,10 +49,12 @@ class Services::User::GenderNormalizerTest < ActiveSupport::TestCase
     context 'when other' do
       o_values = ['o', 'O', 'notlisted', 'some nonsense']
       o_values.each do |value|
-        let(:raw_input) {value}
+        context "with input #{value}" do
+          let(:raw_input) {value}
 
-        it "returns #{Policies::Gender::NormalizedValues::OTHER} for #{value}" do
-          _(normalize).must_equal Policies::Gender::NormalizedValues::OTHER
+          it "returns #{Policies::Gender::NormalizedValues::OTHER} for #{value}" do
+            _(normalize).must_equal Policies::Gender::NormalizedValues::OTHER
+          end
         end
       end
     end
