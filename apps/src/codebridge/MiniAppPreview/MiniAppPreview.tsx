537f75f1a65e8ea98674ae64fb2a275278ab27e3
Molly Moen <molly@code.org>
2025-01-06 15:54:09 -0800

    work in progress

--- apps/src/codebridge/MiniAppPreview/MiniAppPreview.tsx
+++ apps/src/codebridge/MiniAppPreview/MiniAppPreview.tsx
@@ -4,12 +4,24 @@ import React from 'react';
 
 import codebridgeI18n from '@cdo/apps/codebridge/locale';
 import PanelContainer from '@cdo/apps/lab2/views/components/PanelContainer';
+import NeighborhoodVisualization from '@cdo/apps/miniApps/neighborhood/NeighborhoodVisualization';
+import {useAppSelector} from '@cdo/apps/util/reduxHooks';
 
 import moduleStyles from './mini-app-preview.module.scss';
 
 const MiniAppPreview: React.FunctionComponent = () => {
   const {config} = useCodebridgeContext();
   const isHorizontal = config.activeGridLayout === 'horizontal';
+  const miniApp = useAppSelector(state => state.lab.levelProperties?.miniApp);
+
+  // need to inject Neighborhood.ts
+  const miniAppComponent =
+    miniApp === 'neighborhood' ? (
+      <NeighborhoodVisualization
+        fullIconPath={'/blockly/media/turtle/icons_white.png'}
+      />
+    ) : null;
+
   return (
     <PanelContainer
       id="codebridge-preview"
@@ -22,7 +34,7 @@ const MiniAppPreview: React.FunctionComponent = () => {
       }
       headerClassName={moduleStyles.previewHeader}
     >
-      <div />
+      {miniAppComponent}
     </PanelContainer>
   );
 };
