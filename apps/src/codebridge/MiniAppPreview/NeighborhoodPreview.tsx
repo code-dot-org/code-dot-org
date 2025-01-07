9ee706be280d3c65bc87bdb2d5ad5a2c38fd6b1d
Molly Moen <molly@code.org>
2025-01-07 12:21:45 -0800

    style vertical

--- apps/src/codebridge/MiniAppPreview/NeighborhoodPreview.tsx
+++ apps/src/codebridge/MiniAppPreview/NeighborhoodPreview.tsx
@@ -1,3 +1,5 @@
+import {useCodebridgeContext} from '@codebridge/codebridgeContext';
+import {appendSystemOutMessage} from '@codebridge/redux/consoleRedux';
 import React, {useEffect} from 'react';
 
 import {setIsRunning} from '@cdo/apps/lab2/redux/systemRedux';
@@ -6,13 +8,13 @@ import Neighborhood from '@cdo/apps/miniApps/neighborhood/Neighborhood';
 import NeighborhoodVisualization from '@cdo/apps/miniApps/neighborhood/NeighborhoodVisualization';
 import {useAppDispatch, useAppSelector} from '@cdo/apps/util/reduxHooks';
 
-import {appendSystemOutMessage} from '../redux/consoleRedux';
-
 const NeighborhoodPreview: React.FunctionComponent = () => {
   const levelProperties = useAppSelector(state => state.lab.levelProperties);
   const dispatch = useAppDispatch();
+  const {config} = useCodebridgeContext();
+  const isVertical = config.activeGridLayout === 'vertical';
+
   useEffect(() => {
-    console.log({levelProperties});
     if (!levelProperties) {
       return;
     }
@@ -20,7 +22,7 @@ const NeighborhoodPreview: React.FunctionComponent = () => {
       message => dispatch(appendSystemOutMessage(message)),
       () => dispatch(appendSystemOutMessage('')),
       isRunning => dispatch(setIsRunning(isRunning)),
-      '[PYTHONLAB]'
+      '[PYTHON LAB]'
     );
 
     const neighborhoodSkin = skins.load(
@@ -38,16 +40,24 @@ const NeighborhoodPreview: React.FunctionComponent = () => {
       () => {}
     );
 
-    // const visualizationDiv = $('#visualization');
-    // if (visualizationDiv) {
-    //   $('#visualization').css({
-    //     'max-width': '400px',
-    //     'max-height': '400px',
-    //     height: 'inherit',
-    //     width: 'inherit',
-    //   });
-    // }
-  }, [dispatch, levelProperties]);
+    // The vertical version of the mini app is a static size for now,
+    // so we can hard-code the css. The horizontal version is resizable,
+    // and the css is handled by WorkspaceAndOutput
+    if (isVertical) {
+      $('#visualization').css({
+        'max-width': '400px',
+        'max-height': '400px',
+        height: '400px',
+        width: '400px',
+      });
+
+      $('#svgMaze').css({
+        transform: 'scale(0.5)',
+        'transform-origin': '0 0',
+        position: 'absolute',
+      });
+    }
+  }, [dispatch, levelProperties, isVertical]);
 
   return (
     <NeighborhoodVisualization
