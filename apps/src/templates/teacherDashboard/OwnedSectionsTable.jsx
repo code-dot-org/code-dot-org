a93485cf1906cfb8e3325b7ba78b6a5c57b7d734
Mark Barnes <mark.barnes@code.org>
2025-01-07 16:15:01 -0600

    fix standalone links

--- apps/src/templates/teacherDashboard/OwnedSectionsTable.jsx
+++ apps/src/templates/teacherDashboard/OwnedSectionsTable.jsx
@@ -55,7 +55,12 @@ export const courseLinkFormatter = function (course, {rowData}) {
           <a
             href={
               showV2TeacherDashboard()
-                ? teacherDashboardUrl(rowData.id, assignmentPaths[0])
+                ? assignmentPaths[0].includes('/s/')
+                  ? teacherDashboardUrl(
+                      rowData.id,
+                      assignmentPaths[0].replace('/s/', '/unit/')
+                    )
+                  : teacherDashboardUrl(rowData.id, assignmentPaths[0])
                 : `${assignmentPaths[0]}${stringifyQueryParams({
                     section_id: rowData.id,
                   })}`
