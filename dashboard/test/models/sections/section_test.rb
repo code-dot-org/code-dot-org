a537115258be03f7793bbc923836ef9b5f84c26f
kobryan0619 <kaitie.obryan@gmail.com>
2025-01-08 10:14:54 -0600

    fixed tests

--- dashboard/test/models/sections/section_test.rb
+++ dashboard/test/models/sections/section_test.rb
@@ -496,6 +496,7 @@ class SectionTest < ActiveSupport::TestCase
     end
   end
 
+  # BROKEN
   test 'concise_summarize: section with a script assigned' do
     # Use an existing script so that it has a translation
     script = Unit.find_by_name('jigsaw')
@@ -522,13 +523,6 @@ class SectionTest < ActiveSupport::TestCase
         course_display_name: script.course_version.localized_title,
         course_offering_id: script.course_version.course_offering.id,
         course_version_id: script.course_version.id,
-        course: {
-          course_offering_id: script.course_version.course_offering.id,
-          version_id: script.course_version.id,
-          unit_id: script.id,
-          lesson_extras_available: script.try(:lesson_extras_available),
-          text_to_speech_enabled: script.try(:text_to_speech_enabled?),
-        },
         unit_id: script.id,
         course_id: nil,
         hidden: false,
@@ -734,6 +728,7 @@ class SectionTest < ActiveSupport::TestCase
           version_id: unit_group.course_version.id,
           lesson_extras_available: nil,
           text_to_speech_enabled: nil,
+          unit_id: section.unit_group ? section.script_id : nil,
         }
       }
       # Compare created_at separately because the object's created_at microseconds
@@ -758,6 +753,13 @@ class SectionTest < ActiveSupport::TestCase
         script: {id: script.id, name: script.name, project_sharing: nil},
         students: [],
         any_student_has_progress: false,
+        course: {
+          course_offering_id: script.course_version.course_offering.id,
+          version_id: script.course_version.id,
+          lesson_extras_available: script.lesson_extras_available,
+          text_to_speech_enabled: script.text_to_speech_enabled?,
+          unit_id: section.unit_group ? section.script_id : nil,
+        }
       }
       # Compare created_at separately because the object's created_at microseconds
       # don't match Time.zone.now's microseconds (different levels of precision)
