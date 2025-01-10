679e76f9b69148ec4804ee0b00733febc84801aa
denyslevada <levada.denys@gmail.com>
2025-01-10 12:07:27 +0200

    feat(dropdown/checkboxDropdown): copied dropdown/checkboxDropdown to dsco/dropdown/checkboxDropdown

--- frontend/packages/dsco/src/dropdown/index.ts
+++ frontend/packages/dsco/src/dropdown/index.ts
@@ -1,6 +1,7 @@
 import ActionDropdown, {
   ActionDropdownProps,
 } from './actionDropdown/ActionDropdown';
+import CheckboxDropdown, {CheckboxDropdownProps} from './checkboxDropdown';
 import SimpleDropdown, {SimpleDropdownProps} from './simpleDropdown';
 
 export {dropdownColors} from '@/common/constants';
@@ -8,6 +9,8 @@ export {dropdownColors} from '@/common/constants';
 export {
   ActionDropdown,
   ActionDropdownProps,
+  CheckboxDropdown,
+  CheckboxDropdownProps,
   SimpleDropdown,
   SimpleDropdownProps,
 };
