6a51a53e71b91bc40b41c9b0eb902c0ba30b9393
Molly Moen <molly@code.org>
2025-01-10 15:30:51 -0800

    it is working

--- apps/src/lab2/levelEditors/neighborhood/NeighborhoodGridGenerator.tsx
+++ apps/src/lab2/levelEditors/neighborhood/NeighborhoodGridGenerator.tsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 
+import {Button} from '@cdo/apps/componentLibrary/button';
 import {SimpleDropdown} from '@cdo/apps/componentLibrary/dropdown';
 
 import {MazeCell} from '../../types';
@@ -22,6 +23,19 @@ const NeighborhoodGridGenerator: React.FunctionComponent<
     [number, number] | undefined
   >(undefined);
   const [selectedCategory, setSelectedCategory] = useState<string>('Benches');
+
+  const categoryTiles = useMemo(() => {
+    const tileDefinitions = categories[selectedCategory];
+    if (Array.isArray(tileDefinitions)) {
+      return tileDefinitions;
+    } else {
+      const tiles = [];
+      for (let i = tileDefinitions.min; i <= tileDefinitions.max; i++) {
+        tiles.push(i);
+      }
+      return tiles;
+    }
+  }, [selectedCategory]);
 
   useEffect(() => {
     if (gridSize) {
@@ -50,44 +64,88 @@ const NeighborhoodGridGenerator: React.FunctionComponent<
     value: category,
     text: category,
   }));
+  const categoryColumns = 8;
+  const categoryRows = Math.ceil(categoryTiles.length / categoryColumns);
+
+  const selectTile = (tile: number) => {
+    if (selectedCell) {
+      let value = 0;
+      if (tile === 303) {
+        const promptResult = prompt('How much paint?');
+        value = promptResult ? parseInt(promptResult) : 0;
+      }
+      const newGrid = grid.map((row, rowIndex) =>
+        row.map((cell, columnIndex) => {
+          if (rowIndex === selectedCell[0] && columnIndex === selectedCell[1]) {
+            return {...cell, assetId: tile, value};
+          }
+          return cell;
+        })
+      );
+      setGrid(newGrid);
+    }
+  };
 
   return (
-    <div className={moduleStyles.gridGenerator}>
-      <div
-        className={moduleStyles.gridContainer}
-        style={{
-          gridTemplateColumns: `repeat(${gridDimension}, 1fr)`,
-          gridTemplateRows: `repeat(${gridDimension}, 1fr)`,
-        }}
-      >
-        {grid.map((row, rowIndex) =>
-          row.map((cell, columnIndex) => {
-            const isSelected =
-              (selectedCell &&
-                selectedCell[0] === rowIndex &&
-                selectedCell[1] === columnIndex) ||
-              false;
-            return (
+    <div>
+      <div className={moduleStyles.gridGenerator}>
+        <div
+          className={moduleStyles.gridContainer}
+          style={{
+            gridTemplateColumns: `repeat(${gridDimension}, 1fr)`,
+            gridTemplateRows: `repeat(${gridDimension}, 1fr)`,
+          }}
+        >
+          {grid.map((row, rowIndex) =>
+            row.map((cell, columnIndex) => {
+              const isSelected =
+                (selectedCell &&
+                  selectedCell[0] === rowIndex &&
+                  selectedCell[1] === columnIndex) ||
+                false;
+              return (
+                <img
+                  src={imageTiles[cell.assetId]}
+                  alt="neighborhood cell"
+                  width={cellSize}
+                  height={cellSize}
+                  className={isSelected ? moduleStyles.selectedCell : undefined}
+                  onClick={() => setSelectedCell([rowIndex, columnIndex])}
+                />
+              );
+            })
+          )}
+        </div>
+        <div>
+          <SimpleDropdown
+            items={categoryOptions}
+            onChange={e => setSelectedCategory(e.target.value)}
+            labelText={'Category'}
+            name={'category'}
+          />
+          <div
+            className={moduleStyles.categoryTiles}
+            style={{
+              gridTemplateRows: `repeat(${categoryRows}, 1fr)`,
+              gridTemplateColumns: `repeat(${categoryColumns}, 1fr)`,
+            }}
+          >
+            {categoryTiles.map((tile, index) => (
               <img
-                src={imageTiles[cell.assetId]}
-                alt="neighborhood cell"
-                width={cellSize}
-                height={cellSize}
-                className={isSelected ? moduleStyles.selectedCell : undefined}
-                onClick={() => setSelectedCell([rowIndex, columnIndex])}
+                src={imageTiles[tile]}
+                alt="neighborhood tile"
+                onClick={() => selectTile(tile)}
               />
-            );
-          })
-        )}
+            ))}
+          </div>
+        </div>
       </div>
-      <div>
-        <SimpleDropdown
-          items={categoryOptions}
-          onChange={e => setSelectedCategory(e.target.value)}
-          labelText={'Category'}
-          name={'category'}
-        />
-      </div>
+      <Button
+        onClick={() => setMaze(JSON.stringify(grid))}
+        text="Save grid to serialized maze"
+        className={moduleStyles.saveButton}
+        type={'secondary'}
+      />
     </div>
   );
 };
