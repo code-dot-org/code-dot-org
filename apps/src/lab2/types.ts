51f94b392f3810538ad5c1255b2971fd521a7e80
Molly Moen <molly@code.org>
2025-01-13 11:03:22 -0800

    update comment

--- apps/src/lab2/types.ts
+++ apps/src/lab2/types.ts
@@ -138,7 +138,8 @@ export interface ProjectFile {
  * Project file types are as follows:
  * Starter: Files that come from level start code that are editable by the user.
  * Support: Files that come from level start code that are hidden and not editable by the user.
- * Validation: The file that contain the level's validation code. This file is hidden from users.
+ * Validation: The file that contain the level's validation code, which is a code file that will be
+ * run by the lab. This file is hidden from users.
  * Locked Starter: Files that come from level start code that are editable by the user, but cannot be
  *  deleted or renamed.
  * System Support: Files that are used for running code and for share/remix, but are hidden from the user.
