142d1187a33892f2264c2cf734911cca74fb791f
Ryan Driscoll <driscollrp@gmail.com>
2025-01-13 08:42:04 -0700

    feat: validate that educator_role is only set for teacher users

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -222,6 +222,8 @@ class User < ApplicationRecord
 
   validates_inclusion_of :educator_role, in: Policies::User::ALLOWED_EDUCATOR_ROLES, if: :educator_role?
 
+  validate :educator_role_allowed_for_teacher
+
   belongs_to :studio_person, optional: true
   has_many :hint_view_requests
 
@@ -2770,6 +2772,14 @@ class User < ApplicationRecord
         errors.add(:us_state,  I18n.t('activerecord.errors.models.user.attributes.lockout_flow'))
       end
       errors.add(:age,  I18n.t('activerecord.errors.models.user.attributes.lockout_flow')) if birthday_changed?
+    end
+  end
+
+  private def educator_role_allowed_for_teacher
+    return if educator_role.blank?
+
+    unless teacher?
+      errors.add(:educator_role, "can only be assigned to teachers")
     end
   end
 
