eea08567563a8ed9e2e6677071e04ec0043fb9b0
Bethany Connor <bethany@code.org>
2025-01-15 14:44:48 -0500

    Add comment as to why preventDefault is needed

--- apps/src/code-studio/components/header/SignInCalloutWrapper.jsx
+++ apps/src/code-studio/components/header/SignInCalloutWrapper.jsx
@@ -43,6 +43,7 @@ export default class SignInCalloutWrapper extends React.Component {
     this.setState({hideCallout: true});
     cookies.set(HideSignInCallout, 'true', {expires: 1, path: '/'});
     sessionStorage.setItem(HideSignInCallout, 'true');
+    // Needed to prevent sending the user to the sign up page when dismissing the callout
     event.preventDefault();
     // Needed to prevent triggering the Create Account Button Clicked analytics event firing
     event.stopPropagation();
