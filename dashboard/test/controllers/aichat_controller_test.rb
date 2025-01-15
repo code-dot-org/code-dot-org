9a26e4a8c0f2d348b3c9d69d98adf6054e82baec
Sanchit Malhotra <sanchit@code.org>
2025-01-15 15:52:10 -0800

    Fix and add tests

--- dashboard/test/controllers/aichat_controller_test.rb
+++ dashboard/test/controllers/aichat_controller_test.rb
@@ -68,6 +68,7 @@ class AichatControllerTest < ActionController::TestCase
     :student_chat_history,
     :start_chat_completion,
     :find_toxicity,
+    :submit_teacher_feedback,
     [:chat_request, :get, {id: 1}]
   ].each do |action, method = :post, params = {}|
     users.each do |user|
@@ -173,8 +174,9 @@ class AichatControllerTest < ActionController::TestCase
     post :log_chat_event, params: @valid_params_log_chat_event, as: :json
 
     assert_response :success
-    assert_equal json_response.keys, ['chat_event_id', 'chat_event']
-    aichat_event_row = AichatEvent.find(json_response['chat_event_id'])
+    event_id = json_response['id']
+    assert_equal json_response, @valid_params_log_chat_event[:newChatEvent].merge(id: event_id).stringify_keys
+    aichat_event_row = AichatEvent.find(event_id)
     stored_aichat_event = aichat_event_row.aichat_event
     assert_equal stored_aichat_event['timestamp'], @valid_params_log_chat_event[:newChatEvent][:timestamp]
   end
@@ -189,8 +191,9 @@ class AichatControllerTest < ActionController::TestCase
     post :log_chat_event, params: params, as: :json
 
     assert_response :success
-    assert_equal json_response.keys, ['chat_event_id', 'chat_event']
-    aichat_event_row = AichatEvent.find(json_response['chat_event_id'])
+    event_id = json_response['id']
+    assert_equal json_response, params[:newChatEvent].merge(id: event_id).stringify_keys
+    aichat_event_row = AichatEvent.find(event_id)
     stored_aichat_event = aichat_event_row.aichat_event
     assert_equal request_id, stored_aichat_event['requestId']
   end
@@ -229,13 +232,9 @@ class AichatControllerTest < ActionController::TestCase
     assert_equal chat_events_array.length, 2
     chat_event1_response = chat_events_array.first
     chat_event2_response = chat_events_array.last
-    assert_equal chat_event1_response.keys, ['chat_event_id', 'chat_event']
-
-    chat_event1_response = chat_event1_response['chat_event']
-    chat_event2_response = chat_event2_response['chat_event']
     chat_event1_stored = AichatEvent.find(@student1_aichat_event1.id).aichat_event
     chat_event2_stored = AichatEvent.find(@student1_aichat_event2.id).aichat_event
-    assert_equal chat_event1_response.keys.sort, ['role', 'chatMessageText', 'status', 'timestamp'].sort
+    assert_equal chat_event1_response.keys.sort, %w(id role chatMessageText status timestamp).sort
 
     assert_equal chat_event1_response["chatMessageText"], chat_event1_stored["chatMessageText"]
     assert_equal chat_event2_response["chatMessageText"], chat_event2_stored["chatMessageText"]
@@ -371,5 +370,50 @@ class AichatControllerTest < ActionController::TestCase
     post :find_toxicity, params: {systemPrompt: system_prompt, retrievalContexts: retrieval_contexts, locale: locale}, as: :json
     assert_response :success
     assert_equal expected_response, json_response
+  end
+
+  # Submit teacher feedback tests
+  test 'submit_teacher_feedback returns bad request if eventId is missing' do
+    sign_in(@authorized_teacher1)
+    post :submit_teacher_feedback, params: {feedback: SharedConstants::AI_CHAT_TEACHER_FEEDBACK[:CLEAN_DISAGREE]}, as: :json
+    assert_response :bad_request
+  end
+
+  test 'submit_teacher_feedback returns bad request if feedback is invalid' do
+    sign_in(@authorized_teacher1)
+    post :submit_teacher_feedback, params: {eventId: @student1_aichat_event1.id, feedback: 'invalid'}, as: :json
+    assert_response :bad_request
+  end
+
+  test 'submit_teacher_feedback returns forbidden if user is not an authorized teacher' do
+    sign_in(@authorized_student1)
+    post :submit_teacher_feedback, params: {eventId: @student1_aichat_event1.id, feedback: SharedConstants::AI_CHAT_TEACHER_FEEDBACK[:CLEAN_DISAGREE]}, as: :json
+    assert_response :forbidden
+  end
+
+  test 'submit_teacher_feedback returns forbidden if the user is not the teacher of the requested student' do
+    sign_in(@authorized_teacher2)
+    post :submit_teacher_feedback, params: {eventId: @student1_aichat_event1.id, feedback: SharedConstants::AI_CHAT_TEACHER_FEEDBACK[:CLEAN_DISAGREE]}, as: :json
+    assert_response :forbidden
+  end
+
+  test 'submit_teacher_feedback updates teacher feedback in AichatEvent' do
+    feedback = SharedConstants::AI_CHAT_TEACHER_FEEDBACK[:CLEAN_DISAGREE]
+    sign_in(@authorized_teacher1)
+    post :submit_teacher_feedback, params: {eventId: @student1_aichat_event1.id, feedback: feedback}, as: :json
+    assert_response :success
+    @student1_aichat_event1.reload
+    assert_equal @student1_aichat_event1.aichat_event['teacherFeedback'], feedback
+  end
+
+  test 'submit_teacher_feedback clears feedback if no feedback is provided' do
+    @student1_aichat_event1.aichat_event['teacherFeedback'] = SharedConstants::AI_CHAT_TEACHER_FEEDBACK[:CLEAN_DISAGREE]
+    @student1_aichat_event1.save!
+
+    sign_in(@authorized_teacher1)
+    post :submit_teacher_feedback, params: {eventId: @student1_aichat_event1.id}, as: :json
+    assert_response :success
+    @student1_aichat_event1.reload
+    assert_nil @student1_aichat_event1.aichat_event['teacherFeedback']
   end
 end
