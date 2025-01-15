b8c2c80dbc64e55d5c76cb980c3d3afe2cc1238a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-01-15 15:03:39 -0800

    Restore `nil` Return to `any_urm?` Predicate

--- dashboard/test/lib/policies/races_test.rb
+++ dashboard/test/lib/policies/races_test.rb
@@ -2,16 +2,16 @@ require 'test_helper'
 
 class Policies::RacesTest < ActiveSupport::TestCase
   test 'any_urm? with nil' do
-    refute Policies::Races.any_urm?(nil)
+    assert_nil Policies::Races.any_urm?(nil)
   end
 
   test 'any_urm? with empty string' do
-    refute Policies::Races.any_urm?('')
+    assert_nil Policies::Races.any_urm?('')
   end
 
   test 'any_urm? with non-answer responses' do
     %w(opt_out nonsense closed_dialog).each do |response|
-      refute Policies::Races.any_urm?(response)
+      assert_nil Policies::Races.any_urm?(response)
     end
   end
 
