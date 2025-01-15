9e4abfd60c3f3367422ac8a238b10b307585e40b
Ryan Driscoll <driscollrp@gmail.com>
2025-01-15 11:30:16 -0700

    refactor: update EDUCATOR_ROLES shared constant, so the value and category match translate string keys and can be used to access the appropriate translation function

--- apps/src/signUpFlow/FinishTeacherAccount.tsx
+++ apps/src/signUpFlow/FinishTeacherAccount.tsx
@@ -45,9 +45,11 @@ const roleItemGroups = [
     groupItems: [{value: '', text: locale.select_a_role()}],
   },
   ...Object.entries(
-    EducatorRoles.reduce((groups, {category, value, label}) => {
-      groups[category] = groups[category] ?? [];
-      groups[category].push({value, text: label});
+    EducatorRoles.reduce((groups, {category, value}) => {
+      const label = locale[value]();
+      const categoryLabel = locale[category]();
+      groups[categoryLabel] = groups[categoryLabel] ?? [];
+      groups[categoryLabel].push({value, text: label});
       return groups;
     }, {} as Record<string, {value: string; text: string}[]>)
   ).map(([label, groupItems]) => ({label, groupItems})),
--- lib/cdo/shared_constants.rb
+++ lib/cdo/shared_constants.rb
@@ -810,14 +810,14 @@ module SharedConstants
   }
 
   EDUCATOR_ROLES = [
-    {value: "classroom_teacher", label: "Classroom Teacher", category: 'Educator'},
-    {value: "stem_tech_teacher", label: "STEM/Technology Teacher", category: 'Educator'},
-    {value: "subject_area_teacher", label: "Subject Area Teacher", category: 'Educator'},
-    {value: "librarian_media_specialist", label: "Librarian/School Media Specialist", category: 'Educator'},
-    {value: "homeschool_teacher", label: "Homeschool Teacher", category: 'Educator'},
-    {value: "school_admin", label: "School Administrator", category: "Administrator"},
-    {value: "district_admin", label: "District Administrator", category: "Administrator"},
-    {value: "parent", label: "Parent", category: 'Other'},
-    {value: "other", label: "Other", category: 'Other'}
+    {value: "classroom_teacher", category: 'educator'},
+    {value: "stem_tech_teacher", category: 'educator'},
+    {value: "subject_area_teacher", category: 'educator'},
+    {value: "librarian_media_specialist", category: 'educator'},
+    {value: "homeschool_teacher", category: 'educator'},
+    {value: "school_admin", category: "admin"},
+    {value: "district_admin", category: "admin"},
+    {value: "parent", category: 'other'},
+    {value: "other", category: 'other'}
   ].freeze
 end
