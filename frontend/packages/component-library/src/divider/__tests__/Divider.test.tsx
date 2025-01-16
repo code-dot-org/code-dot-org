6d1e773090fefa817b137dfa625ad710269b0fc2
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-01-16 11:04:36 -0800

    remove jest className tests

--- frontend/packages/component-library/src/divider/__tests__/Divider.test.tsx
+++ frontend/packages/component-library/src/divider/__tests__/Divider.test.tsx
@@ -6,46 +6,4 @@ describe('Divider Component', () => {
     render(<Divider />);
     expect(screen.getByRole('separator')).toBeInTheDocument();
   });
-
-  it('applies the primary color class by default', () => {
-    render(<Divider />);
-    const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider-primary');
-  });
-
-  it('applies the strong color class when color prop is strong', () => {
-    render(<Divider color="strong" />);
-    const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider-strong');
-  });
-
-  it('applies the no margin class by default', () => {
-    render(<Divider />);
-    const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider-margin-none');
-  });
-
-  it('applies the small margin class when margin prop is small', () => {
-    render(<Divider margin="xs" />);
-    const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider-margin-small');
-  });
-
-  it('applies the medium margin class when margin prop is medium', () => {
-    render(<Divider margin="s" />);
-    const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider-margin-medium');
-  });
-
-  it('applies the large margin class when margin prop is large', () => {
-    render(<Divider margin="m" />);
-    const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider-margin-large');
-  });
-
-  it('applies the extra-large margin class when margin prop is extra-large', () => {
-    render(<Divider margin="l" />);
-    const dividerElement = screen.getByRole('separator');
-    expect(dividerElement).toHaveClass('divider-margin-extra-large');
-  });
 });
