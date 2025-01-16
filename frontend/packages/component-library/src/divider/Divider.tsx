cba18134b06acee92f7941b74b8d71bc773858e8
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-01-16 12:09:35 -0800

    update ComponentSizeXSToL path so props work

--- frontend/packages/component-library/src/divider/Divider.tsx
+++ frontend/packages/component-library/src/divider/Divider.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import {HTMLAttributes} from 'react';
 
-import {ComponentSizeXSToL} from '@/common/types';
+import {ComponentSizeXSToL} from './../common/types';
 import moduleStyles from './divider.module.scss';
 
 export interface DividerProps extends HTMLAttributes<HTMLElement> {
