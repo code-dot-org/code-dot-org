2839e7ce38b9d15e447697cb5b97a8046447a385
Molly Moen <molly@code.org>
2025-01-16 15:30:39 -0800

    clean up and comments

--- python/pythonlab/neighborhood/neighborhood/__init__.py
+++ python/pythonlab/neighborhood/neighborhood/__init__.py
@@ -1,3 +1,3 @@
 from .painter import Painter as Painter
-# We export world so we can clear the world in pythonlab_setup
+# We export World so we can clear the world in pythonlab_setup
 from .support.world import World as World
