10378d0dcc6b7485f2f7e586508813f7db69b665
suresh <suresh@code.org>
2025-01-16 10:54:16 -0800

    Remove Sequel dashboard reporting connection because it's not being used.

--- lib/cdo/db.rb
+++ lib/cdo/db.rb
@@ -13,13 +13,3 @@ PEGASUS_DB.singleton_class.prepend StaticModels
 # rubocop:enable CustomCops/PegasusDbUsage
 
 DASHBOARD_DB = Cdo::Sequel.database_connection_pool CDO.dashboard_db_writer, CDO.dashboard_db_reader
-
-credentials = CDO.db_credential_reader
-dashboard_reporting_connection_settings = URI::Generic.build(
-  scheme: 'mysql',
-  userinfo: credentials['username'] + ':' + credentials['password'],
-  host: CDO.db_endpoint_proxy_reporting,
-  port: CDO.db_endpoint_proxy_reporting_port,
-  path: '/' + CDO.dashboard_db_name
-)
-DASHBOARD_REPORTING_DB_READER = Cdo::Sequel.database_connection_pool dashboard_reporting_connection_settings, dashboard_reporting_connection_settings
