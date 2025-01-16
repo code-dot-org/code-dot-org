aef71d4392db94bd1f5d78b0d9ebc7cf763c7663
Carl Reiner <carl.reiner@code.org>
2025-01-16 15:38:47 -0800

    pass newAccountUrl

--- apps/src/sites/studio/pages/lti/v1/account_linking/landing.js
+++ apps/src/sites/studio/pages/lti/v1/account_linking/landing.js
@@ -16,7 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const scriptData = getScriptData('json');
   const ltiProvider = scriptData['lti_provider'];
   const ltiProviderName = scriptData['lti_provider_name'];
-  const finishSignUpUrl = new URL(scriptData['finish_sign_up_url']);
+  const finishSignUpUrl = scriptData['finish_sign_up_url'];
+  const newAccountUrl = new URL(scriptData['new_account_url']);
   const existingAccountUrl = new URL(scriptData['existing_account_url']);
   const emailAddress = scriptData['email'];
   const newCtaType = scriptData['new_cta_type'];
@@ -26,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ltiProviderContext = {
     ltiProvider,
     ltiProviderName,
+    newAccountUrl,
     finishSignUpUrl,
     existingAccountUrl,
     emailAddress,
