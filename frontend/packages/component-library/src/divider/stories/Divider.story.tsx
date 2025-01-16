cb08de0636bd442769a7410ed2cf38dd2c38c0b2
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-01-16 11:09:53 -0800

    update Divider.story.tsx to have Storybook testing

--- frontend/packages/component-library/src/divider/stories/Divider.story.tsx
+++ frontend/packages/component-library/src/divider/stories/Divider.story.tsx
@@ -1,4 +1,5 @@
-import {Meta, StoryFn} from '@storybook/react';
+import type {Meta, StoryObj} from '@storybook/react';
+import {within, expect} from '@storybook/test';
 
 import Divider, {DividerProps} from '../index';
 
@@ -6,55 +7,102 @@ export default {
   title: 'DesignSystem/Divider',
   component: Divider,
 } as Meta;
+type Story = StoryObj<typeof Divider>;
 
 //
 // TEMPLATE
 //
-const SingleTemplate: StoryFn<DividerProps> = args => <Divider {...args} />;
+export const DefaultDivider: Story = {
+  args: {
+    color: 'primary',
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    const divider = canvas.getByRole('separator');
+    const expectedColor = window
+      .getComputedStyle(document.body)
+      .getPropertyValue('--background-neutral-quaternary');
 
-const MultipleTemplate: StoryFn<{
+    // applies the primary color class by default
+    expect(divider).toHaveStyle(`border-top-color: ${expectedColor};`);
+
+    // applies the no margin class by default
+    expect(divider).toHaveStyle('margin: 0');
+  },
+};
+
+const MultipleTemplate: StoryObj<{
   components: DividerProps[];
-}> = args => (
-  <>
-    <p>
-      {args.components.some(componentArg => componentArg.margin)
-        ? '* Gray background exists to show spacing, it will not appear with component *'
-        : "* Margins on this screen do not represent Component's margins, and are only added to improve storybook view *"}
-    </p>
+}> = {
+  render: args => (
+    <>
+      <p>
+        {args.components.some(componentArg => componentArg.margin)
+          ? '* Gray background exists to show spacing, it will not appear with component *'
+          : "* Margins on this screen do not represent Component's margins, and are only added to improve storybook view *"}
+      </p>
 
-    <p>Multiple Dividers:</p>
-    <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
-      {args.components?.map((componentArg, index) => (
-        <div
-          key={index}
-          style={{
-            background: componentArg.margin ? '#646464' : 'transparent',
-          }}
-        >
-          <Divider {...componentArg} />
-        </div>
-      ))}
-    </div>
-  </>
-);
-
-export const DefaultDivider = SingleTemplate.bind({});
-DefaultDivider.args = {
-  color: 'primary',
+      <p>Multiple Dividers:</p>
+      <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+        {args.components?.map((componentArg, index) => (
+          <div
+            key={index}
+            style={{
+              background: componentArg.margin ? '#646464' : 'none',
+            }}
+          >
+            <Divider {...componentArg} />
+          </div>
+        ))}
+      </div>
+    </>
+  ),
 };
 
-export const GroupOfDividers = MultipleTemplate.bind({});
-GroupOfDividers.args = {
-  components: [{color: 'primary'}, {color: 'strong'}],
+export const GroupOfDividers: StoryObj<{
+  components: DividerProps[];
+}> = {
+  ...MultipleTemplate,
+  args: {
+    components: [{color: 'primary'}, {color: 'strong'}],
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    const dividers = canvas.getAllByRole('separator');
+    const getComputedStyleValue = (property: string) =>
+      window.getComputedStyle(document.body).getPropertyValue(property);
+
+    const primary = getComputedStyleValue('--background-neutral-quaternary');
+    const strong = getComputedStyleValue('--background-neutral-senary');
+
+    expect(dividers[0]).toHaveStyle(`border-top-color: ${primary};`);
+    expect(dividers[1]).toHaveStyle(`border-top-color: ${strong};`);
+  },
 };
 
-export const GroupOfDividersWithMargin = MultipleTemplate.bind({});
-GroupOfDividersWithMargin.args = {
-  components: [
-    {color: 'primary', margin: 'none'},
-    {color: 'primary', margin: 'xs'},
-    {color: 'primary', margin: 's'},
-    {color: 'primary', margin: 'm'},
-    {color: 'primary', margin: 'l'},
-  ],
+export const GroupOfDividersWithMargin: StoryObj<{
+  components: DividerProps[];
+}> = {
+  ...MultipleTemplate,
+  args: {
+    components: [
+      {color: 'primary', margin: 'xs'},
+      {color: 'primary', margin: 's'},
+      {color: 'primary', margin: 'm'},
+      {color: 'primary', margin: 'l'},
+    ],
+  },
+  play: async ({canvasElement}) => {
+    const canvas = within(canvasElement);
+    const dividers = await canvas.getAllByRole('separator');
+    const expectedColor = window
+      .getComputedStyle(document.body)
+      .getPropertyValue('--background-neutral-quaternary');
+    dividers.forEach(divider => {
+      // applies the primary color class by default
+      expect(divider).toHaveStyle(`border-top-color: ${expectedColor};`);
+      // applies a margin
+      expect(divider).not.toHaveStyle('margin: 0');
+    });
+  },
 };
