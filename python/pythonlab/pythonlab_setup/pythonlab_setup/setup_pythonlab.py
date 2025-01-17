f008ed2a67f61e7dbc1480f3785021100763ccb6
Molly Moen <molly@code.org>
2025-01-17 11:33:12 -0800

    move reset to teardown method

--- python/pythonlab/pythonlab_setup/pythonlab_setup/setup_pythonlab.py
+++ python/pythonlab/pythonlab_setup/pythonlab_setup/setup_pythonlab.py
@@ -1,6 +1,4 @@
 from .patch_matplotlib import patch_matplotlib
-from .reset_neighborhood import reset_neighborhood
 
 def setup_pythonlab(matplotlib_img_tag):
   patch_matplotlib(matplotlib_img_tag)
-  reset_neighborhood()
