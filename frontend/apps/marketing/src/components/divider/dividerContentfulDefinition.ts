dbda43b05d7aa1bd2b7d7782ac78dcfceaf6ea62
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-01-23 10:59:16 -0800

    add thumbnail and tooltip images

--- frontend/apps/marketing/src/components/divider/dividerContentfulDefinition.ts
+++ frontend/apps/marketing/src/components/divider/dividerContentfulDefinition.ts
@@ -6,10 +6,13 @@ export const DividerContentfulComponentDefinition: ComponentDefinition = {
   name: 'Divider',
   category: 'Custom Components',
   builtInStyles: 'cfMargin',
-  // thumbnailUrl: 'https://code.org/images/logo.svg',
+  thumbnailUrl:
+    'https://images.ctfassets.net/90t6bu6vlf76/2szIrFB6A7UrWF4wQLW2zV/0531f1a17546c6a92ccbc07a87231f85/component_divider_thumbnail.png',
   tooltip: {
-    description: 'A horizontal line to separate content',
-    // imageUrl: 'https://code.org/images/logo.svg',
+    description:
+      'Use a divider to visually separate content sections. It spans the full width and helps improve readability and layout structure.',
+    imageUrl:
+      'https://images.ctfassets.net/90t6bu6vlf76/3gRz7bA5miAVaFwJqM6w18/075ca1479e4c79c3969e3cb4a87a9992/component_divider_tooltip.png',
   },
   variables: {
     color: {
