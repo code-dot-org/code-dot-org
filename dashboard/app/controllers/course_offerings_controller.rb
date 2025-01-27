927714d0a25887f41186324f83e56a6df1ac32f2
root <root@DESKTOP-SUAAGI8.localdomain>
2025-01-27 13:50:46 -0500

    undo change for adhoc

--- dashboard/app/controllers/course_offerings_controller.rb
+++ dashboard/app/controllers/course_offerings_controller.rb
@@ -36,11 +36,11 @@ class CourseOfferingsController < ApplicationController
   end
 
   def self_paced_pl_course_offerings
-    # return head :bad_request unless current_user
-    # offerings = CourseOffering.all_course_offerings.filter do |co|
-    #   co.get_participant_audience == 'teacher' && co.any_version_is_in_published_state? && co.instruction_type == 'self_paced' && co.header.present?
-    # end
-    render :ok, json: [{id: 'test-pl-id', display_name: 'PL TOPIC EXAMPLE'}].to_json
+    return head :bad_request unless current_user
+    offerings = CourseOffering.all_course_offerings.filter do |co|
+      co.get_participant_audience == 'teacher' && co.any_version_is_in_published_state? && co.instruction_type == 'self_paced' && co.header.present?
+    end
+    render :ok, json: offerings&.map(&:summarize_self_paced_pl).to_json
   end
 
   private def course_offering_params
