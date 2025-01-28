09f23a47684654b57a4482d61c5bd971987d86d0
Molly Moen <molly@code.org>
2025-01-28 09:59:29 -0800

    improvements

--- python/pythonlab/unittest_runner/unittest_runner/validation_procotol.py
+++ python/pythonlab/unittest_runner/unittest_runner/validation_procotol.py
@@ -1,10 +1,17 @@
 from neighborhood import World, NeighborhoodContextType
 
 class ValidationProtocol(object):
-  def invoke_main_method(self):
+  def invoke_main(self, file_path = 'main.py'):
     world = World()
     world.set_context_type(NeighborhoodContextType.VALIDATE)
     # invoke main method
-    with open('main.py') as main_file:
-      exec(main_file.read())
+    try:
+      with open(file_path) as main_file:
+        exec(main_file.read())
+    except FileNotFoundError:
+      print('No main file found')
+    except Exception as e:
+      print('Error executing main file: ', e)
+
+    # Reset context type
     world.set_context_type(NeighborhoodContextType.RUN)
