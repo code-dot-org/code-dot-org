a4b30b7ff70980133f80b4d14380e1b3d98cb246
Molly Moen <molly@code.org>
2025-01-30 13:20:14 -0800

    fix default value

--- python/pythonlab/unittest_runner/unittest_runner/validation_protocol.py
+++ python/pythonlab/unittest_runner/unittest_runner/validation_protocol.py
@@ -6,12 +6,13 @@ class ValidationProtocol(object):
   def __init__(self):
     self.stdout_tracker = StdoutTracker()
 
-  def invoke_main(self, file_path = 'main.py'):
+  def invoke_main(self, file_path = None):
     world = World()
     world.set_context_type(NeighborhoodContextType.VALIDATE)
     self.stdout_tracker.clean_up()
     self.stdout_tracker.start_tracking()
     # Invoke main method. Catch all exceptions so we can reset the context type afterwards.
+    file_path = file_path or 'main.py'
     try:
       with open(file_path) as main_file:
         exec(main_file.read())
