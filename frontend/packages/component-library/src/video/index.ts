f369d2222915fbc575f034945317d5edff529444
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-01-31 16:32:06 -0800

    add comment to use client directive

--- frontend/packages/component-library/src/video/index.ts
+++ frontend/packages/component-library/src/video/index.ts
@@ -1,4 +1,5 @@
-'use client'
+// Add this to prevent a Next.js build error in Contentful
+'use client';
 
 export {default as Video, VideoProps} from './Video';
 export {default as default} from './Video';
