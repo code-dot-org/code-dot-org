f7506133264fcb16593793d359bf287f71719c9b
Ben Brooks <ben@code.org>
2025-02-03 10:51:22 -0800

    Use existing copyToClipboard util

--- apps/src/aiComponentLibrary/copyButton/CopyButton.tsx
+++ apps/src/aiComponentLibrary/copyButton/CopyButton.tsx
@@ -1,30 +1,19 @@
 import Button from '@code-dot-org/component-library/button';
 import React, {useState} from 'react';
 
+import copyToClipboard from '@cdo/apps/util/copyToClipboard';
+
 import style from './copy-button.module.scss';
 
-// Fallback method for browsers that do not support navigator.clipboard
-const copyToClipboard = (text: string) => {
-  const textField = document.createElement('textarea');
-  textField.innerText = text;
-  document.body.appendChild(textField);
-  textField.select();
-  document.execCommand('copy');
-  textField.remove();
-};
+const CONFIRM_TIMEOUT_MS = 1500;
 
 const CopyButton: React.FC<{copyText: string}> = ({copyText}) => {
-  const CONFIRM_TIMEOUT_MS = 1500;
   const [showCopyConfirmation, setShowCopyConfirmation] = useState(false);
 
   return (
     <Button
       onClick={() => {
-        if (navigator.clipboard) {
-          navigator.clipboard.writeText(copyText);
-        } else {
-          copyToClipboard(copyText);
-        }
+        copyToClipboard(copyText);
         setShowCopyConfirmation(true);
         setTimeout(() => setShowCopyConfirmation(false), CONFIRM_TIMEOUT_MS);
       }}
