0d24a0ffcd18167f4e84370f198f91837ef59a08
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-02-04 10:33:31 -0800

    add followup ticket to comment

--- lib/dynamic_config/dcdo.rb
+++ lib/dynamic_config/dcdo.rb
@@ -59,7 +59,7 @@ class DCDOBase < DynamicConfigBase
       'browser-tts-button-enabled-locales': DCDO.get('browser-tts-button-enabled-locales', ['en-US']),
       'lab2-submit-project-enabled': DCDO.get('lab2-submit-project-enabled', true),
       'cdo-blockly-usage': DCDO.get('cdo-blockly-usage', false),
-      # Remove this after the Exploring Gen AI launch
+      # TODO ACQ-3074 - Remove this after the Exploring Gen AI launch
       'exploring-gen-ai-launch': DCDO.get('exploring-gen-ai-launch', false),
     }
   end
