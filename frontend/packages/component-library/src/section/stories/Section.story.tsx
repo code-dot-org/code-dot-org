a48da895dfeb374059da424c8c01511439ab29a1
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-02-05 11:29:15 -0800

    refactor stories

--- frontend/packages/component-library/src/section/stories/Section.story.tsx
+++ frontend/packages/component-library/src/section/stories/Section.story.tsx
@@ -1,8 +1,8 @@
-import type {Meta, StoryObj} from '@storybook/react';
+import type {Meta, StoryObj, StoryFn} from '@storybook/react';
 import {within, expect} from '@storybook/test';
 
-import Section from '../index';
-import {BodyOneText, Heading1} from '@/typography';
+import Section, {SectionProps} from '../index';
+import {BodyOneText, Heading2} from '@/typography';
 
 export default {
   title: 'CMS/Section',
@@ -13,26 +13,53 @@ type Story = StoryObj<typeof Section>;
 //
 // TEMPLATE
 //
+const MultipleTemplate: StoryFn<{components: SectionProps[]}> = args => (
+  <>
+    {args.components.map((component, index) => (
+      <Section key={index} {...component} />
+    ))}
+  </>
+);
+
+const createPlayFunction =
+  (headingText: string) =>
+  async ({canvasElement}: {canvasElement: HTMLElement}) => {
+    const canvas = within(canvasElement);
+    const sections = await canvas.findAllByTestId('section');
+    const containers = await canvas.findAllByTestId('container');
+    const headings = canvas.getAllByText(headingText);
+
+    // check if sections are in the document
+    sections.forEach(section => {
+      expect(section).toBeInTheDocument();
+    });
+
+    // check if containers are in the document
+    containers.forEach(container => {
+      expect(container).toBeInTheDocument();
+    });
+
+    // check if children content is in the document
+    headings.forEach(heading => {
+      expect(heading).toBeInTheDocument();
+    });
+  };
+
+//
+// STORIES
+//
 export const DefaultSection: Story = {
   args: {
     padding: 'l',
     alignment: 'left',
     children: (
-      <div>
-        <Heading1 visualAppearance="heading-xl">
-          This is a default section.
-        </Heading1>
+      <>
+        <Heading2>This is a default section</Heading2>
         <BodyOneText>I'm just a sentence.</BodyOneText>
-      </div>
+      </>
     ),
   },
-  play: async ({canvasElement}) => {
-    const canvas = within(canvasElement);
-    const section = await canvas.findByTestId('section');
-    const container = await canvas.findByTestId('container');
-    await expect(section).toBeInTheDocument();
-    await expect(container).toBeInTheDocument();
-  },
+  play: createPlayFunction('This is a default section'),
 };
 
 export const SectionWithBackgroundColor: Story = {
@@ -41,24 +68,51 @@ export const SectionWithBackgroundColor: Story = {
     padding: 'l',
     alignment: 'left',
     children: (
-      <div>
-        <Heading1 visualAppearance="heading-xl">
-          This is a section with a background color.
-        </Heading1>
+      <>
+        <Heading2>This is a section with a background color</Heading2>
         <BodyOneText>I'm just a sentence.</BodyOneText>
-      </div>
+      </>
     ),
   },
-  play: async ({canvasElement}) => {
-    const canvas = within(canvasElement);
-    const section = await canvas.findByTestId('section');
-    const container = await canvas.findByTestId('container');
-    await expect(section).toBeInTheDocument();
-    await expect(container).toBeInTheDocument();
+  play: createPlayFunction('This is a section with a background color'),
+};
+
+export const SectionWithCenteredContent: Story = {
+  args: {
+    backgroundColor: 'brand-light-primary',
+    padding: 'l',
+    alignment: 'center',
+    children: (
+      <>
+        <Heading2>This is a section with centered content</Heading2>
+        <BodyOneText>I'm just a sentence.</BodyOneText>
+      </>
+    ),
   },
+  play: createPlayFunction('This is a section with centered content'),
 };
 
 export const SectionWithBackgroundImage: Story = {
+  args: {
+    backgroundColor: 'dark',
+    backgroundImage: 'https://code.org/images/banners/banner-bg-music.png',
+    backgroundImageRepeat: false,
+    backgroundSize: 'cover',
+    padding: 'l',
+    alignment: 'left',
+    children: (
+      <>
+        <Heading2 style={{color: 'white'}}>
+          This is a section with a background image
+        </Heading2>
+        <BodyOneText style={{color: 'white'}}>I'm just a sentence.</BodyOneText>
+      </>
+    ),
+  },
+  play: createPlayFunction('This is a section with a background image'),
+};
+
+export const SectionWithAPatternedBackgroundImage: Story = {
   args: {
     backgroundColor: 'dark',
     backgroundImage:
@@ -68,12 +122,12 @@ export const SectionWithBackgroundImage: Story = {
     padding: 'l',
     alignment: 'left',
     children: (
-      <div>
-        <Heading1 style={{color: 'white'}} visualAppearance="heading-xl">
-          This is a section with a patterned background image.
-        </Heading1>
+      <>
+        <Heading2 style={{color: 'white'}}>
+          This is a section with a patterned background image
+        </Heading2>
         <BodyOneText style={{color: 'white'}}>I'm just a sentence.</BodyOneText>
-      </div>
+      </>
     ),
   },
   parameters: {
@@ -84,11 +138,55 @@ export const SectionWithBackgroundImage: Story = {
       },
     },
   },
-  play: async ({canvasElement}) => {
-    const canvas = within(canvasElement);
-    const section = await canvas.findByTestId('section');
-    const container = await canvas.findByTestId('container');
-    await expect(section).toBeInTheDocument();
-    await expect(container).toBeInTheDocument();
-  },
+  play: createPlayFunction(
+    'This is a section with a patterned background image',
+  ),
 };
+
+export const MultipleSections = MultipleTemplate.bind({});
+MultipleSections.args = {
+  components: [
+    {
+      backgroundColor: 'primary',
+      padding: 'l',
+      alignment: 'left',
+      children: (
+        <>
+          <Heading2>This is section one</Heading2>
+          <BodyOneText>I'm just a sentence.</BodyOneText>
+        </>
+      ),
+    },
+    {
+      backgroundColor: 'secondary',
+      padding: 'l',
+      alignment: 'center',
+      children: (
+        <>
+          <Heading2>This is section two</Heading2>
+          <BodyOneText>I'm just a sentence.</BodyOneText>
+        </>
+      ),
+    },
+    {
+      backgroundColor: 'dark',
+      backgroundImage:
+        'https://code.org/images/banners/banner-bg-lines-neutral-dark.png',
+      backgroundImageRepeat: true,
+      backgroundSize: 'contain',
+      padding: 'l',
+      alignment: 'left',
+      children: (
+        <>
+          <Heading2 style={{color: 'white'}}>This is section three</Heading2>
+          <BodyOneText style={{color: 'white'}}>
+            I'm just a sentence.
+          </BodyOneText>
+        </>
+      ),
+    },
+  ],
+};
+MultipleSections.play = createPlayFunction('This is section one');
+MultipleSections.play = createPlayFunction('This is section two');
+MultipleSections.play = createPlayFunction('This is section three');
