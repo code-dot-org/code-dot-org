aec40daca6797eaf6ee52b6669d94954914d86dd
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-02-06 13:52:51 -0800

    update paths to include /cms/

--- frontend/apps/marketing/src/components/section/index.ts
+++ frontend/apps/marketing/src/components/section/index.ts
@@ -1,5 +1,5 @@
-import '@code-dot-org/component-library/section/index.css';
+import '@code-dot-org/component-library/cms/section/index.css';
 
 // Export the Component Definition for use in Contentful Studio
 export {SectionContentfulComponentDefinition} from './sectionContentfulDefinition';
-export {default} from '@code-dot-org/component-library/section';
+export {default} from '@code-dot-org/component-library/cms/section';
