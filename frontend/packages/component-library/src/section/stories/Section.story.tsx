82b17f088e69bac2b6cdf0af74885c61152667c3
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-02-06 10:05:40 -0800

    remove alignment and background image props

--- frontend/packages/component-library/src/section/stories/Section.story.tsx
+++ frontend/packages/component-library/src/section/stories/Section.story.tsx
@@ -51,7 +51,6 @@ const createPlayFunction =
 export const DefaultSection: Story = {
   args: {
     padding: 'l',
-    alignment: 'left',
     children: (
       <>
         <Heading2>This is a default section</Heading2>
@@ -66,7 +65,6 @@ export const SectionWithBackgroundColor: Story = {
   args: {
     backgroundColor: 'secondary',
     padding: 'l',
-    alignment: 'left',
     children: (
       <>
         <Heading2>This is a section with a background color</Heading2>
@@ -77,87 +75,12 @@ export const SectionWithBackgroundColor: Story = {
   play: createPlayFunction('This is a section with a background color'),
 };
 
-export const SectionWithCenteredContent: Story = {
-  args: {
-    backgroundColor: 'brand-light-primary',
-    padding: 'l',
-    alignment: 'center',
-    children: (
-      <>
-        <Heading2>This is a section with centered content</Heading2>
-        <BodyOneText>I'm just a sentence.</BodyOneText>
-      </>
-    ),
-  },
-  play: createPlayFunction('This is a section with centered content'),
-};
-
-export const SectionWithBackgroundImage: Story = {
-  args: {
-    backgroundColor: 'dark',
-    backgroundImage: 'https://code.org/images/banners/banner-bg-music.png',
-    backgroundImageRepeat: false,
-    backgroundSize: 'cover',
-    padding: 'l',
-    alignment: 'left',
-    children: (
-      <>
-        <Heading2 style={{color: 'white'}}>
-          This is a section with a background image
-        </Heading2>
-        <BodyOneText style={{color: 'white'}}>I'm just a sentence.</BodyOneText>
-      </>
-    ),
-  },
-  parameters: {
-    docs: {
-      description: {
-        story:
-          'Show a solid `backgroundImage`. Can be used alone or on top of `backgroundColor` if a transparent png is used. Set `backgroundImageRepeat` to `false` and `backgroundSize` to `cover` for best results.',
-      },
-    },
-  },
-  play: createPlayFunction('This is a section with a background image'),
-};
-
-export const SectionWithAPatternedBackgroundImage: Story = {
-  args: {
-    backgroundColor: 'dark',
-    backgroundImage:
-      'https://code.org/images/banners/banner-bg-lines-neutral-dark.png',
-    backgroundImageRepeat: true,
-    backgroundSize: 'contain',
-    padding: 'l',
-    alignment: 'left',
-    children: (
-      <>
-        <Heading2 style={{color: 'white'}}>
-          This is a section with a patterned background image
-        </Heading2>
-        <BodyOneText style={{color: 'white'}}>I'm just a sentence.</BodyOneText>
-      </>
-    ),
-  },
-  parameters: {
-    docs: {
-      description: {
-        story:
-          'Show a patterned `backgroundImage` over `backgroundColor`. Use a transparent png and set `backgroundImageRepeat` to `true` and `backgroundSize` to `contain` for best results.',
-      },
-    },
-  },
-  play: createPlayFunction(
-    'This is a section with a patterned background image',
-  ),
-};
-
 export const MultipleSections = MultipleTemplate.bind({});
 MultipleSections.args = {
   components: [
     {
       backgroundColor: 'primary',
       padding: 'l',
-      alignment: 'left',
       children: (
         <>
           <Heading2>This is section one</Heading2>
@@ -168,7 +91,6 @@ MultipleSections.args = {
     {
       backgroundColor: 'secondary',
       padding: 'l',
-      alignment: 'center',
       children: (
         <>
           <Heading2>This is section two</Heading2>
@@ -177,19 +99,12 @@ MultipleSections.args = {
       ),
     },
     {
-      backgroundColor: 'dark',
-      backgroundImage:
-        'https://code.org/images/banners/banner-bg-lines-neutral-dark.png',
-      backgroundImageRepeat: true,
-      backgroundSize: 'contain',
+      backgroundColor: 'brand-light-primary',
       padding: 'l',
-      alignment: 'left',
       children: (
         <>
-          <Heading2 style={{color: 'white'}}>This is section three</Heading2>
-          <BodyOneText style={{color: 'white'}}>
-            I'm just a sentence.
-          </BodyOneText>
+          <Heading2>This is section three</Heading2>
+          <BodyOneText>I'm just a sentence.</BodyOneText>
         </>
       ),
     },
