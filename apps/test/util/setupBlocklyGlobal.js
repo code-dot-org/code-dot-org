9841a2d882b2aca988a0dd93bb3b9bc5f70b46a7
Mike Harvey <mike.harvey@code.org>
2025-02-06 15:25:28 -0500

    try using google blockly for unit tests

--- apps/test/util/setupBlocklyGlobal.js
+++ apps/test/util/setupBlocklyGlobal.js
@@ -1,3 +1,8 @@
+import cookies from 'js-cookie';
+
+import {blocklyLocaleMap} from '../../src/sites/studio/pages/blocklyLocaleImports.js';
+
+import 'blockly/blocks';
 /**
  * Provides the basic frame for running Blockly.  In particular, this will
  * create a basic dom, load blockly.js  and put the contents into the global
@@ -8,13 +13,12 @@ export default function setBlocklyGlobal() {
   // Initialize browser environment.
   document.body.innerHTML = '<div id="codeApp"><div id="app"></div></div>';
   // locale file requires Blockly as a global
-  var blockly = require('@code-dot-org/blockly');
-  var initializeCdoBlocklyWrapper = require('../../src/blockly/cdoBlocklyWrapper');
-  window.Blockly = initializeCdoBlocklyWrapper(blockly);
-
-  try {
-    require('../../lib/blockly/en_us');
-  } catch (err) {
-    console.log(err);
-  }
+  var googleBlockly = require('blockly/core');
+  var initializeGoogleBlocklyWrapper = require('../../src/blockly/googleBlocklyWrapper');
+  var localeFromCookies = cookies.get('language_') || 'en-US';
+  var messages =
+    blocklyLocaleMap[localeFromCookies.toLocaleLowerCase()] ||
+    blocklyLocaleMap['en-us'];
+  googleBlockly.setLocale(messages);
+  window.Blockly = initializeGoogleBlocklyWrapper(googleBlockly);
 }
