33daa4d6b7ecfb402ccac84c4692a6abaec2d67d
Mark Barnes <mark.barnes@code.org>
2025-02-06 08:22:10 -0600

    added redirect and unit test

--- dashboard/app/controllers/home_controller.rb
+++ dashboard/app/controllers/home_controller.rb
@@ -78,11 +78,10 @@ class HomeController < ApplicationController
   def home
     authenticate_user!
     init_homepage
-    # if current_user.teacher? && Experiment.enabled?(user: current_user, experiment_name: 'teacher-homepage-v2')
-    #   redirect_to '/teacher_dashboard/home'
-    # else
-    #   render 'home/index'
-    # end
+    if current_user.teacher? && Experiment.enabled?(user: current_user, experiment_name: 'teacher-homepage-v2')
+      redirect_to '/teacher_dashboard/home'
+      return
+    end
     render 'home/index'
   end
 
