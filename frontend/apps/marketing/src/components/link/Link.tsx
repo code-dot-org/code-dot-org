ed792b16d0e6bd4be65dc4c6aa9568c1e10b36b6
denyslevada <levada.denys@gmail.com>
2025-02-10 22:10:50 +0200

    feat(marketing/link): added Link component

---
+++ frontend/apps/marketing/src/components/link/Link.tsx
@@ -0,0 +1,57 @@
+import '@code-dot-org/component-library/link/index.css';
+import {default as DSCOLink} from '@code-dot-org/component-library/link';
+import React, {ReactNode} from 'react';
+import {ComponentSizeXSToL} from '@code-dot-org/component-library/common/types';
+import FontAwesomeV6Icon from '@code-dot-org/component-library/fontAwesomeV6Icon';
+import classNames from 'classnames';
+
+import moduleStyles from './link.module.scss';
+
+export type LinkProps = {
+  /** Link content */
+  children: ReactNode;
+  /** Link URL */
+  href: string;
+  /** Link color */
+  color: 'purple' | 'white';
+  /** Link vertical margin */
+  bottomMargin: 'none' | 'xs' | 's' | 'm';
+  /** Link size */
+  size: ComponentSizeXSToL;
+  /** Whether Link is for internal code.org pages, or external web page. (external links are opened in new tab) */
+  isLinkExternal: boolean;
+  /** ClassName passed by contentful to apply styles that are set through contentful native editor*/
+  className?: string;
+};
+
+const Link: React.FunctionComponent<LinkProps> = ({
+  children,
+  href,
+  color,
+  size,
+  bottomMargin,
+  isLinkExternal,
+  className,
+}) => {
+  return (
+    <DSCOLink
+      href={href}
+      openInNewTab={isLinkExternal}
+      size={size}
+      className={classNames(
+        moduleStyles.link,
+        moduleStyles[`link-${color}`],
+        moduleStyles[`link-${size}`],
+        moduleStyles[`link-bottomMargin-${bottomMargin}`],
+        className,
+      )}
+    >
+      {children}
+      {isLinkExternal && (
+        <FontAwesomeV6Icon iconName="up-right-from-square" iconStyle="solid" />
+      )}
+    </DSCOLink>
+  );
+};
+
+export default Link;
---
+++ frontend/apps/marketing/src/components/link/LinkContentfulDefinition.ts
@@ -0,0 +1,81 @@
+// Creates a definition for the Typography component to be used in Contentful Studio
+import {ComponentDefinition} from '@contentful/experiences-sdk-react';
+
+export const LinkContentfulComponentDefinition: ComponentDefinition = {
+  id: 'link',
+  name: 'Link',
+  category: 'Typography',
+  thumbnailUrl:
+    'https://images.ctfassets.net/90t6bu6vlf76/2CPKrKCB3KxD1n6wG9JTn9/aab22373a39e9cc5305b21c08bba588d/component_link_thumbnail.png',
+  tooltip: {
+    description:
+      'A standalone text that directs users to internal or external pages, with options for styling and accessibility.',
+    imageUrl:
+      'https://images.ctfassets.net/90t6bu6vlf76/3Nx2ASyu1g0rwVAPrc7Srb/9c2c3e85246d8572326b741a02b69a04/component_link_tooltip.png',
+  },
+  builtInStyles: [],
+  variables: {
+    color: {
+      displayName: 'Color',
+      type: 'Text',
+      defaultValue: 'purple',
+      group: 'style',
+      validations: {
+        in: [
+          {value: 'purple', displayName: 'Purple'},
+          {value: 'white', displayName: 'White'},
+        ],
+      },
+    },
+    size: {
+      displayName: 'Size',
+      type: 'Text',
+      defaultValue: 'm',
+      group: 'style',
+      validations: {
+        in: [
+          {value: 'l', displayName: 'Large'},
+          {value: 'm', displayName: 'Medium'},
+          {value: 's', displayName: 'Small'},
+          {value: 'xs', displayName: 'Extra Small'},
+        ],
+      },
+    },
+    bottomMargin: {
+      displayName: 'Bottom Margin',
+      type: 'Text',
+      defaultValue: 'xs',
+      group: 'style',
+      validations: {
+        in: [
+          {value: 'none', displayName: '0px (None)'},
+          {value: 'xs', displayName: '8px (Extra Small)'},
+          {value: 's', displayName: '16px (Small)'},
+          {value: 'm', displayName: '24px (Medium)'},
+        ],
+      },
+    },
+    isLinkExternal: {
+      displayName:
+        'Is this link external? (Does this link leave the code.org site?)',
+      description:
+        'External links will be opened in a new tab, while internal links will be opened in the same tab.',
+      type: 'Boolean',
+      defaultValue: false,
+      group: 'style',
+    },
+    href: {
+      displayName: 'Link URL',
+      type: 'Text',
+      defaultValue: 'https://code.org',
+      group: 'content',
+    },
+    children: {
+      displayName: 'Content',
+      type: 'Text',
+      defaultValue: 'Link',
+      group: 'content',
+      required: true,
+    },
+  },
+};
