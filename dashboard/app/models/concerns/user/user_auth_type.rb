c39f53a076a2a3881f5dc4cc1f1bd6572efc4af5
Nick Lathe <nick.lathe@code.org>
2025-02-10 12:59:50 -0800

    Move UserAuthType methods to concern

--- dashboard/app/models/concerns/user/user_auth_type.rb
+++ dashboard/app/models/concerns/user/user_auth_type.rb
@@ -1,17 +1,17 @@
 module User::UserAuthType
   extend ActiveSupport::Concern
   # True if user is a student in a section that has Google Classroom login type
-  def google_classroom_student_2?
+  def google_classroom_student?
     sections_as_student.find_by_login_type(Section::LOGIN_TYPE_GOOGLE_CLASSROOM).present?
   end
 
   # True if user is a student in a section that has Clever login type
-  def clever_student_2?
+  def clever_student?
     sections_as_student.find_by_login_type(Section::LOGIN_TYPE_CLEVER).present?
   end
 
   # True if user is a student in a section that uses any oauth login type
-  def oauth_student_2?
+  def oauth_student?
     sections_as_student.find_by_login_type(Section::LOGIN_TYPES_OAUTH).present?
   end
 end
