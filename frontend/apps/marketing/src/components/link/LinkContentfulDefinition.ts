15f9b474afa8ac203c7bd25a92197de4c14983ce
levadadenys <levada.denys@gmail.com>
2025-02-11 12:28:45 +0200

    Update frontend/apps/marketing/src/components/link/LinkContentfulDefinition.ts
    Co-authored-by: Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>

--- frontend/apps/marketing/src/components/link/LinkContentfulDefinition.ts
+++ frontend/apps/marketing/src/components/link/LinkContentfulDefinition.ts
@@ -1,4 +1,4 @@
-// Creates a definition for the Typography component to be used in Contentful Studio
+// Creates a definition for the Link component to be used in Contentful Studio
 import {ComponentDefinition} from '@contentful/experiences-sdk-react';
 
 export const LinkContentfulComponentDefinition: ComponentDefinition = {
