2ce8d841763094b121027c5a12b80fd25907e39a
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-02-10 16:11:31 -0800

    add public path to tsconfig.json and update image path on /section/index.tsx

--- frontend/apps/marketing/src/components/section/index.tsx
+++ frontend/apps/marketing/src/components/section/index.tsx
@@ -1,7 +1,7 @@
 import '@code-dot-org/component-library/cms/section/index.css';
 
 // Import the background image used in the pattern backgrounds on Contentful
-import bgPatternImage from '../../../public/images/bg-pattern-lines.png';
+import bgPatternImage from '@public/images/bg-pattern-lines.png';
 
 // Export the Component Definition for use in Contentful Studio
 export {SectionContentfulComponentDefinition} from './sectionContentfulDefinition';
