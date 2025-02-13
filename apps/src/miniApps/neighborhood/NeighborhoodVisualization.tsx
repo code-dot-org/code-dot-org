4a045bb55cbd3f7d06898bfa1a81d20631c8c459
Molly Moen <molly@code.org>
2025-02-13 09:41:15 -0800

    add slider

--- apps/src/miniApps/neighborhood/NeighborhoodVisualization.tsx
+++ apps/src/miniApps/neighborhood/NeighborhoodVisualization.tsx
@@ -1,3 +1,4 @@
+import Slider from '@code-dot-org/component-library/slider';
 import React from 'react';
 
 import MazeVisualization from '@cdo/apps/maze/Visualization';
@@ -9,53 +10,47 @@ interface NeighborhoodVisualizationProps {
   isDarkMode: boolean;
   useProtectedDiv?: boolean;
 }
-const ICON_PATH = '/blockly/media/turtle/';
 
 const NeighborhoodVisualization: React.FunctionComponent<
   NeighborhoodVisualizationProps
 > = ({className, isDarkMode, useProtectedDiv = true}) => {
-  const fullIconPath = isDarkMode
-    ? ICON_PATH + 'icons_white.png'
-    : ICON_PATH + 'icons.png';
+  const [sliderValue, setSliderValue] = React.useState(50);
+  const handleSpeedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('Speed changed to: ', e.target.value);
+    setSliderValue(parseInt(e.target.value));
+  };
 
   return (
     <div className={className}>
       <div className={moduleStyles.neighborhoodPreviewBackground}>
         <MazeVisualization useProtectedDiv={useProtectedDiv} />
       </div>
-      <div className={moduleStyles.sliderBackground}>
-        <svg
-          id="slider"
-          version="1.1"
-          width="150"
-          height="50"
+      <div className={moduleStyles.sliderContainer}>
+        <Slider
+          name="neighborhood-speed"
+          value={sliderValue}
+          onChange={handleSpeedChange}
+          color={'white'}
+          isPercentMode={true}
+          hideValue={true}
+          leftButtonProps={{
+            'aria-label': 'Decrease',
+            icon: {
+              iconName: 'turtle',
+              title: 'Decrease',
+            },
+            size: 's',
+          }}
+          rightButtonProps={{
+            'aria-label': 'Increase',
+            icon: {
+              iconName: 'rabbit',
+              title: 'Increase',
+            },
+            size: 's',
+          }}
           className={moduleStyles.slider}
-        >
-          {/* Slow icon. */}
-          <clipPath id="slowClipPath">
-            <rect width="26" height="12" x="5" y="14" />
-          </clipPath>
-          <image
-            xlinkHref={fullIconPath}
-            height="42"
-            width="84"
-            x="-21"
-            y="-10"
-            clipPath="url(#slowClipPath)"
-          />
-          {/* Fast icon. */}
-          <clipPath id="fastClipPath">
-            <rect width="26" height="16" x="120" y="10" />
-          </clipPath>
-          <image
-            xlinkHref={fullIconPath}
-            height="42"
-            width="84"
-            x="120"
-            y="-11"
-            clipPath="url(#fastClipPath)"
-          />
-        </svg>
+        />
       </div>
     </div>
   );
