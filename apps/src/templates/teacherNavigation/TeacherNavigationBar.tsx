72bf978d6714c8bf2a61c74d8daa0c42b2285b1f
Liam Frye-Mason <liam.fryemason@code.org>
2025-02-13 11:18:14 -0800

    Fix course overview highlight in navbar

--- apps/src/templates/teacherNavigation/TeacherNavigationBar.tsx
+++ apps/src/templates/teacherNavigation/TeacherNavigationBar.tsx
@@ -22,6 +22,7 @@ import {selectedSectionSelector} from '../teacherDashboard/teacherSectionsReduxS
 import {asyncLoadSelectedSection} from './selectedSectionLoader';
 import {
   LABELED_TEACHER_NAVIGATION_PATHS,
+  TEACHER_NAVIGATION_PATH_NAMES,
   TEACHER_NAVIGATION_PATHS,
 } from './TeacherNavigationPaths';
 
@@ -155,6 +156,19 @@ const TeacherNavigationBar: React.FunctionComponent = () => {
     }
   };
 
+  const isOptionSelected = React.useCallback(
+    (key: string) => {
+      return (
+        currentPathName === key ||
+        (currentPathName === TEACHER_NAVIGATION_PATH_NAMES.courseOverview &&
+          key === TEACHER_NAVIGATION_PATH_NAMES.unitOverview) ||
+        (currentPathName === TEACHER_NAVIGATION_PATH_NAMES.unitOverview &&
+          key === TEACHER_NAVIGATION_PATH_NAMES.courseOverview)
+      );
+    },
+    [currentPathName]
+  );
+
   const getSidebarOptionsForSection = (
     sidebarKeys: (keyof typeof LABELED_TEACHER_NAVIGATION_PATHS)[]
   ) => {
@@ -164,7 +178,7 @@ const TeacherNavigationBar: React.FunctionComponent = () => {
     return sidebarKeys.map(key => (
       <SidebarOption
         key={'ui-test-sidebar-' + key}
-        isSelected={currentPathName === key}
+        isSelected={isOptionSelected(key)}
         sectionId={selectedSection.id}
         courseVersionName={selectedSection.courseVersionName}
         unitName={selectedSection.unitName}
