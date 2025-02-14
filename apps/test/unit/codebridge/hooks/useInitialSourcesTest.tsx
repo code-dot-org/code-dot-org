a85bddd69cc21ddf41b9892c0f93a77732d2fa75
Molly Moen <molly@code.org>
2025-02-13 16:50:51 -0800

    more tests

--- apps/test/unit/codebridge/hooks/useInitialSourcesTest.tsx
+++ apps/test/unit/codebridge/hooks/useInitialSourcesTest.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {Store} from 'redux';
 
+import {
+  DEFAULT_FOLDER_ID,
+  MAZE_FILE_NAME,
+} from '@cdo/apps/codebridge/constants';
 import {useInitialSources} from '@cdo/apps/codebridge/hooks';
-import lab from '@cdo/apps/lab2/lab2Redux';
+import lab, {onLevelChange} from '@cdo/apps/lab2/lab2Redux';
+import {MazeCell, MultiFileSource, ProjectFileType} from '@cdo/apps/lab2/types';
 import {
   getStore,
   registerReducers,
@@ -12,7 +17,28 @@ import {
   stubRedux,
 } from '@cdo/apps/redux';
 
-import {smallProjectSources} from '../test-files';
+import {
+  neighborhoodLevelProperties,
+  nonValidatedLevelProperties,
+  smallProjectSources,
+  templateBackedLevelProperties,
+} from '../test-files';
+
+const expectedParsedDefaultSources = {
+  ...smallProjectSources,
+  labConfig: undefined,
+};
+
+const generateMazeFile = (mazeContents: MazeCell[][], fileId: string) => {
+  return {
+    id: fileId,
+    name: MAZE_FILE_NAME,
+    contents: JSON.stringify(mazeContents),
+    type: ProjectFileType.SYSTEM_SUPPORT,
+    language: 'txt',
+    folderId: DEFAULT_FOLDER_ID,
+  };
+};
 
 describe('useInitialSources', () => {
   let store: Store;
@@ -28,30 +54,123 @@ describe('useInitialSources', () => {
     restoreRedux();
   });
 
-  function getWrapper() {
-    return ({children}: {children?: React.ReactNode}) => (
+  function renderDefault() {
+    const wrapper = ({children}: {children?: React.ReactNode}) => (
       <Provider store={store}>{children}</Provider>
     );
-  }
-
-  it('returns default sources if none are found', () => {
-    const wrapper = getWrapper();
     const {result} = renderHook(() => useInitialSources(smallProjectSources), {
       wrapper,
     });
-    const expectedSources = {
-      ...smallProjectSources,
-      labConfig: undefined,
-    };
     const {
       initialSources,
       levelStartSources,
       templateStartSources,
       parsedDefaultSources,
     } = result.current;
-    expect(initialSources).toEqual(expectedSources);
+    return {
+      initialSources,
+      levelStartSources,
+      templateStartSources,
+      parsedDefaultSources,
+    };
+  }
+
+  it('returns default sources if none are found', () => {
+    const {
+      initialSources,
+      levelStartSources,
+      templateStartSources,
+      parsedDefaultSources,
+    } = renderDefault();
+    expect(initialSources).toEqual(expectedParsedDefaultSources);
     expect(levelStartSources).toBeUndefined();
     expect(templateStartSources).toBeUndefined();
-    expect(parsedDefaultSources).toEqual(expectedSources);
+    expect(parsedDefaultSources).toEqual(expectedParsedDefaultSources);
+  });
+
+  it('returns start sources for a non-template level', () => {
+    store.dispatch(
+      onLevelChange({levelProperties: nonValidatedLevelProperties})
+    );
+    const {
+      initialSources,
+      levelStartSources,
+      templateStartSources,
+      parsedDefaultSources,
+    } = renderDefault();
+    const expectedSources = {
+      source: nonValidatedLevelProperties.startSources,
+      labConfig: undefined,
+    };
+    expect(initialSources).toEqual(expectedSources);
+    expect(levelStartSources).toEqual(expectedSources);
+    expect(templateStartSources).toBeUndefined();
+    expect(parsedDefaultSources).toEqual(expectedParsedDefaultSources);
+  });
+
+  it('returns template sources for a template-backed level', () => {
+    store.dispatch(
+      onLevelChange({levelProperties: templateBackedLevelProperties})
+    );
+    const {
+      initialSources,
+      levelStartSources,
+      templateStartSources,
+      parsedDefaultSources,
+    } = renderDefault();
+    const expectedLevelSources = {
+      source: templateBackedLevelProperties.startSources,
+      labConfig: undefined,
+    };
+    const expectedSources = {
+      source: templateBackedLevelProperties.templateSources,
+      labConfig: undefined,
+    };
+    expect(initialSources).toEqual(expectedSources);
+    expect(levelStartSources).toEqual(expectedLevelSources);
+    expect(templateStartSources).toEqual(expectedSources);
+    expect(parsedDefaultSources).toEqual(expectedParsedDefaultSources);
+  });
+
+  it('populates labConfig and serializedMaze for a neighborhood level', () => {
+    store.dispatch(
+      onLevelChange({levelProperties: neighborhoodLevelProperties})
+    );
+    const {
+      initialSources,
+      levelStartSources,
+      templateStartSources,
+      parsedDefaultSources,
+    } = renderDefault();
+    const expectedSources = {
+      source: {
+        ...neighborhoodLevelProperties.startSources,
+        files: {
+          ...neighborhoodLevelProperties.startSources?.files,
+          '1': generateMazeFile(
+            neighborhoodLevelProperties.serializedMaze!,
+            '1'
+          ),
+        },
+      },
+      labConfig: {miniApp: {name: 'neighborhood'}},
+    };
+    const expectedNeighborhoodDefaultSources = {
+      source: {
+        ...(expectedParsedDefaultSources.source as MultiFileSource),
+        files: {
+          ...(expectedParsedDefaultSources.source as MultiFileSource).files,
+          '1': generateMazeFile(
+            neighborhoodLevelProperties.serializedMaze!,
+            '1'
+          ),
+        },
+      },
+      labConfig: {miniApp: {name: 'neighborhood'}},
+    };
+    expect(initialSources).toEqual(expectedSources);
+    expect(levelStartSources).toEqual(expectedSources);
+    expect(templateStartSources).toBeUndefined();
+    expect(parsedDefaultSources).toEqual(expectedNeighborhoodDefaultSources);
   });
 });
--- apps/test/unit/codebridge/test-files/index.ts
+++ apps/test/unit/codebridge/test-files/index.ts
@@ -9,6 +9,7 @@ import {InitProgressPayload, LevelResults} from '@cdo/apps/types/progressTypes';
 
 const initProgressPayload: InitProgressPayload = require('./initProgressPayload.json');
 const levelResults: LevelResults = require('./levelResults.json');
+const neighborhoodLevelProperties: LevelProperties = require('./neighborhoodLevelProperties.json');
 const nonValidatedLevelProperties: LevelProperties = require('./nonValidatedLevelProperties.json');
 const predictLevelProperties: LevelProperties = require('./predictLevelProperties.json');
 const testProject: MultiFileSource = require('./project.json');
@@ -17,12 +18,14 @@ const smallProjectSources: ProjectSources = require('./smallProjectSources.json'
 const starterFile: ProjectFile = require('./starterFile.json');
 const submittableLevelProperties: LevelProperties = require('./submittableLevelProperties.json');
 const supportFile: ProjectFile = require('./supportFile.json');
+const templateBackedLevelProperties: LevelProperties = require('./templateBackedLevelProperties.json');
 const validatedLevelProperties: LevelProperties = require('./validatedLevelProperties.json');
 const validationFile: ProjectFile = require('./validationFile.json');
 
 export {
   initProgressPayload,
   levelResults,
+  neighborhoodLevelProperties,
   nonValidatedLevelProperties,
   predictLevelProperties,
   testProject,
@@ -31,6 +34,7 @@ export {
   starterFile,
   submittableLevelProperties,
   supportFile,
+  templateBackedLevelProperties,
   validatedLevelProperties,
   validationFile,
 };
