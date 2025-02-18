681d7dd7e67c88dc135343e37ef29fd41e17289b
Ryan Driscoll <driscollrp@gmail.com>
2025-02-18 10:22:31 -0700

    feat(workshops): update workshop with new fields
    add new fields to workshop serializer, allowed params, and workshop config shared constant

--- dashboard/app/controllers/api/v1/pd/workshops_controller.rb
+++ dashboard/app/controllers/api/v1/pd/workshops_controller.rb
@@ -340,7 +340,12 @@ class Api::V1::Pd::WorkshopsController < ApplicationController
       :third_party_provider,
       {sessions_attributes: [:id, :start, :end, :session_format, :_destroy]},
       :module,
-      :participant_group_type
+      :participant_group_type,
+      :description,
+      :registration_link,
+      :prereq,
+      :hidden,
+      :grades
     ]
 
     allowed_params.delete :regional_partner_id unless can_update_regional_partner
