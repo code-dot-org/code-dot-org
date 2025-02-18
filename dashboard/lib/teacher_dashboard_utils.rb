685974749aacc372ef302d68df9a18102e6700b1
Liam Frye-Mason <liam.fryemason@code.org>
2025-02-18 14:26:26 -0800

    Don't redirect from courses/units on levelbuilder

---
+++ dashboard/lib/teacher_dashboard_utils.rb
@@ -0,0 +1,6 @@
+module TeacherDashboardUtils
+  def self.can_redirect_to_teacher_dashboard?
+    (!rack_env?(:levelbuilder)) &&
+      (Experiment.enabled?(user: current_user, experiment_name: 'teacher-local-nav-v2') || DCDO.get('teacher-local-nav-v2', false))
+  end
+end
