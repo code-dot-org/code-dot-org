c0b1cfac18443c64955bfcf34ac9f26b317ae64d
Ben Brooks <ben@code.org>
2025-02-19 14:49:01 -0800

    Script to update AI Chat temperatures for levels using gpt-4o-mini

---
+++ bin/oneoff/update_aichat_openai_temperature.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require_relative '../../dashboard/config/environment'
+
+def main
+  # As of writing, there are only about 300 Aichat levels.
+  Aichat.all.each do |level|
+    initial_customizations = level.properties.dig('aichat_settings', 'initialCustomizations')
+    if initial_customizations && initial_customizations['selectedModelId'] == 'gpt-4o-mini'
+      current_temperature = initial_customizations['temperature']
+      updated_temperature = (current_temperature.to_f / 2).round(1)
+      level.properties['aichat_settings']['initialCustomizations']['temperature'] = updated_temperature
+      level.save!
+    end
+  end
+end
+
+main
