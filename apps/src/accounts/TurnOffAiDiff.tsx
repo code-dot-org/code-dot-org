1785786fc717c6e0c397303fb14ea920ede249df
kobryan0619 <kaitie.obryan@gmail.com>
2025-02-20 12:43:16 -0600

    modify strings displayed

--- apps/src/accounts/TurnOffAiDiff.tsx
+++ apps/src/accounts/TurnOffAiDiff.tsx
@@ -7,6 +7,7 @@ import React from 'react';
 
 import UserPreferences from '@cdo/apps/lib/util/UserPreferences';
 import {setAiDifferentiationEnabled} from '@cdo/apps/templates/currentUserRedux';
+import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
 import {useAppDispatch, useAppSelector} from '@cdo/apps/util/reduxHooks';
 import i18n from '@cdo/locale';
 
@@ -44,7 +45,12 @@ const TurnOffAiDiff: React.FC = () => {
       <Heading5 className={moduleStyles.sectionHeader}>
         {i18n.aiTeachingAssistantSettings()}
       </Heading5>
-      <BodyTwoText>{i18n.aiTeachingAssistantSettingsDescription()}</BodyTwoText>
+      <BodyTwoText>
+        <SafeMarkdown
+          unwrapped
+          markdown={i18n.aiTeachingAssistantSettingsDescription()}
+        />
+      </BodyTwoText>
       <Toggle
         checked={hasAIDiffAccess}
         onChange={handleToggle}
