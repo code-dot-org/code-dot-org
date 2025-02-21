f4fdd75cf97e5c0a91f998c2598f0288a592753d
kobryan0619 <kaitie.obryan@gmail.com>
2025-02-21 15:14:00 -0600

    DIFF - add events for toggle on/off

--- apps/src/accounts/TurnOffAiDiff.tsx
+++ apps/src/accounts/TurnOffAiDiff.tsx
@@ -6,6 +6,8 @@ import {
 import React from 'react';
 
 import UserPreferences from '@cdo/apps/lib/util/UserPreferences';
+import {EVENTS} from '@cdo/apps/metrics/AnalyticsConstants';
+import analyticsReporter from '@cdo/apps/metrics/AnalyticsReporter';
 import {setAiDifferentiationEnabled} from '@cdo/apps/templates/currentUserRedux';
 import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
 import {useAppDispatch, useAppSelector} from '@cdo/apps/util/reduxHooks';
@@ -17,6 +19,8 @@ const TurnOffAiDiff: React.FC = () => {
   const reduxState = useAppSelector(
     state => state.currentUser.aiDifferentiationEnabled
   );
+
+  const currentUserId = useAppSelector(state => state.currentUser.userId);
 
   const startingState = () => {
     if (reduxState === null) {
@@ -33,6 +37,10 @@ const TurnOffAiDiff: React.FC = () => {
     dispatch(setAiDifferentiationEnabled(!hasAIDiffAccess));
     new UserPreferences().setAiDifferentiationEnabled(!hasAIDiffAccess);
     setHasAIDiffAccess(!hasAIDiffAccess);
+    analyticsReporter.sendEvent(EVENTS.AI_DIFF_CHAT_TOGGLED, {
+      'user id': currentUserId,
+      state: !hasAIDiffAccess ? 'on' : 'off',
+    });
   };
 
   const dispatch = useAppDispatch();
