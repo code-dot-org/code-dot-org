084bf188a2d898f3c178ecdbeb14986e92ea0764
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-02-21 15:51:11 -0800

    import swiper css and add use client directive

--- frontend/apps/marketing/src/components/carousel/VideoCarousel.tsx
+++ frontend/apps/marketing/src/components/carousel/VideoCarousel.tsx
@@ -1,6 +1,13 @@
+'use client';
+
 import '@code-dot-org/component-library/carousel/index.css';
 import {Carousel} from '@code-dot-org/component-library/carousel';
 import React, {ReactNode} from 'react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 type VideoCarouselProps = {
   /** Carousel content */
