f3d08c4872a14960035bcce2d3dd3e0b4a0d7feb
alex-m-brown <alex.brown@code.org>
2025-02-24 10:23:59 -0500

    temporarily disable readonly attributes

--- dashboard/app/models/course_version.rb
+++ dashboard/app/models/course_version.rb
@@ -28,8 +28,8 @@ class CourseVersion < ApplicationRecord
   has_many :vocabularies
   has_many :reference_guides
 
-  attr_readonly :content_root_type
-  attr_readonly :content_root_id
+  # attr_readonly :content_root_type
+  # attr_readonly :content_root_id
 
   KEY_CHAR_RE = /[a-z0-9\-]/
   KEY_RE = /\A#{KEY_CHAR_RE}+\Z/
