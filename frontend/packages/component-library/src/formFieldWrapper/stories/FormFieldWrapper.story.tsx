920adcd1affe903e3c3e385cbb615caa228e7150
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-02-24 21:02:07 +0200

    CMS-363: Refactor story

--- frontend/packages/component-library/src/formFieldWrapper/stories/FormFieldWrapper.story.tsx
+++ frontend/packages/component-library/src/formFieldWrapper/stories/FormFieldWrapper.story.tsx
@@ -1,15 +1,29 @@
-import {Meta, StoryFn} from '@storybook/react';
+import type {Meta, StoryObj} from '@storybook/react';
 
-import FormFieldWrapper, {FormFieldWrapperProps} from '../index';
+import FormFieldWrapper, {FormFieldWrapperProps} from '..';
+
+type Story = StoryObj<typeof FormFieldWrapper> & {
+  args: FormFieldWrapperProps | FormFieldWrapperProps[];
+};
 
 export default {
   title: 'DesignSystem/FormFieldWrapper',
   component: FormFieldWrapper,
+  render: args => {
+    const components = args[0] ? Object.values(args) : [args];
+    return (
+      <div style={{display: 'flex', gap: '1em'}}>
+        {components.map((component, index) => (
+          <FormFieldWrapper key={index} {...component} />
+        ))}
+      </div>
+    );
+  },
 } as Meta;
 
-const defaultArgs = {
+const defaultArgs: FormFieldWrapperProps = {
   label: 'Label text',
-  children: <input type="text" value="Child component" />,
+  children: <input type="text" placeholder="Child component" />,
 };
 
 const defaultComponents: FormFieldWrapperProps[] = [
@@ -27,62 +41,48 @@ const defaultComponents: FormFieldWrapperProps[] = [
   },
 ];
 
-//
-// TEMPLATE
-//
-const SingleTemplate: StoryFn<FormFieldWrapperProps> = args => (
-  <FormFieldWrapper {...args} />
-);
-const MultipleTemplate: StoryFn<{
-  components: FormFieldWrapperProps[];
-}> = args => (
-  <div style={{display: 'flex', gap: '1rem'}}>
-    {args.components?.map((componentArg, index) => (
-      <FormFieldWrapper key={index} {...componentArg} />
-    ))}
-  </div>
-);
-
-export const Default = SingleTemplate.bind({});
-Default.args = {
-  ...defaultArgs,
-};
-
-export const WithHelperMessage = SingleTemplate.bind({});
-WithHelperMessage.args = {
-  ...defaultArgs,
-  helperMessage: 'Helper message',
-};
-
-export const WithHelperIconAndMessage = SingleTemplate.bind({});
-WithHelperIconAndMessage.args = {
-  ...defaultArgs,
-  helperMessage: 'Helper message',
-  helperIcon: {iconName: 'tag'},
-};
-
-export const WithError = SingleTemplate.bind({});
-WithError.args = {
-  ...defaultArgs,
-  errorMessage: 'Error message',
-};
-
-export const WithDisabledChildElement = MultipleTemplate.bind({});
-WithDisabledChildElement.args = {
-  components: defaultComponents.map(component => ({
-    ...component,
-    children: <input type="text" disabled />,
-  })),
-};
-WithDisabledChildElement.parameters = {
-  backgrounds: {
-    default: 'dark',
+export const Default: Story = {
+  args: {
+    ...defaultArgs,
   },
 };
 
-export const Sizes = MultipleTemplate.bind({});
-Sizes.args = {
-  components: [
+export const WithHelperMessage: Story = {
+  args: {
+    ...defaultArgs,
+    helperMessage: 'Helper message',
+  },
+};
+
+export const WithHelperIconAndMessage: Story = {
+  args: {
+    ...defaultArgs,
+    helperMessage: 'Helper message',
+    helperIcon: {iconName: 'tag'},
+  },
+};
+
+export const WithError: Story = {
+  args: {
+    ...defaultArgs,
+    errorMessage: 'Error message',
+  },
+};
+
+export const WithDisabledChildElement: Story = {
+  args: defaultComponents.map(component => ({
+    ...component,
+    children: <input type="text" disabled />,
+  })),
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+};
+
+export const Sizes: Story = {
+  args: [
     {
       ...defaultArgs,
       size: 's',
@@ -107,31 +107,28 @@ Sizes.args = {
   ],
 };
 
-export const Black = MultipleTemplate.bind({});
-Black.args = {
-  components: defaultComponents.map(component => ({
+export const Black: Story = {
+  args: defaultComponents.map(component => ({
     ...component,
     color: 'black',
   })),
 };
 
-export const Gray = MultipleTemplate.bind({});
-Gray.args = {
-  components: defaultComponents.map(component => ({
+export const Gray: Story = {
+  args: defaultComponents.map(component => ({
     ...component,
     color: 'gray',
   })),
 };
 
-export const White = MultipleTemplate.bind({});
-White.args = {
-  components: defaultComponents.map(component => ({
+export const White: Story = {
+  args: defaultComponents.map(component => ({
     ...component,
     color: 'white',
   })),
-};
-White.parameters = {
-  backgrounds: {
-    default: 'dark',
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
   },
 };
