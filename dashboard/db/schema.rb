2a0a44c58ba4f71db81fe06baea060698aab7f32
Ceara Chewning <ceara.chewning@code.org>
2025-02-24 11:16:12 -0500

    Forgot to commit the date change on the schema

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_02_14_202758) do
+ActiveRecord::Schema.define(version: 2025_02_21_170834) do
 
   create_table "activities", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
