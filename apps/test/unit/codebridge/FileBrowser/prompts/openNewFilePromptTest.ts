5e682458780a2ac43c8b3bbfef30ac416987774a
Alice Fisher <alice.fisher@code.org>
2025-02-26 18:07:02 +0000

    revert change to openNewFilePromptTest

--- apps/test/unit/codebridge/FileBrowser/prompts/openNewFilePromptTest.ts
+++ apps/test/unit/codebridge/FileBrowser/prompts/openNewFilePromptTest.ts
@@ -1,14 +1,24 @@
+import {NewFileFunction} from '@codebridge/codebridgeContext/types';
 import {DEFAULT_FOLDER_ID} from '@codebridge/constants';
 import {openNewFilePrompt} from '@codebridge/FileBrowser/prompts/openNewFilePrompt';
+import {FolderId, ProjectFile} from '@codebridge/types';
 
 import {EVENTS} from '@cdo/apps/metrics/AnalyticsConstants';
 
 import {testProject, validationFile} from '../../test-files/';
-import {
-  getDialogControlMock,
-  getAnalyticsMock,
-  getNewFileMock,
-} from '../../test_utils';
+import {getDialogControlMock, getAnalyticsMock} from '../../test_utils';
+
+const getNewFileMock = (parentId: FolderId): [ProjectFile, NewFileFunction] => {
+  const newFileData = {} as ProjectFile;
+  const mock: NewFileFunction = ({fileName, folderId}) => {
+    newFileData.name = fileName;
+    if (folderId) {
+      newFileData.folderId = folderId;
+    }
+  };
+
+  return [newFileData, mock];
+};
 
 describe('openNewFilePrompt', function () {
   it('can successfully add a new file to root w/o validation file', async function () {
@@ -16,7 +26,7 @@ describe('openNewFilePrompt', function () {
     const newFileName = 'valid_file.txt';
     const folderId = DEFAULT_FOLDER_ID;
 
-    const [newFileData, newFileDataMock] = getNewFileMock();
+    const [newFileData, newFileDataMock] = getNewFileMock(folderId);
 
     await openNewFilePrompt({
       folderId,
@@ -39,7 +49,7 @@ describe('openNewFilePrompt', function () {
     const newFileName = 'valid_file.txt';
     const folderId = DEFAULT_FOLDER_ID;
 
-    const [newFileData, newFileDataMock] = getNewFileMock();
+    const [newFileData, newFileDataMock] = getNewFileMock(folderId);
 
     await openNewFilePrompt({
       dialogControl: getDialogControlMock(newFileName),
@@ -61,7 +71,7 @@ describe('openNewFilePrompt', function () {
     const newFileName = 'valid_file.txt';
     const folderId = '1';
 
-    const [newFileData, newFileDataMock] = getNewFileMock();
+    const [newFileData, newFileDataMock] = getNewFileMock(folderId);
 
     await openNewFilePrompt({
       folderId,
@@ -84,7 +94,7 @@ describe('openNewFilePrompt', function () {
     const newFileName = 'valid_file.txt';
     const folderId = DEFAULT_FOLDER_ID;
 
-    const [newFileData, newFileDataMock] = getNewFileMock();
+    const [newFileData, newFileDataMock] = getNewFileMock(folderId);
 
     await openNewFilePrompt({
       folderId,
@@ -107,7 +117,7 @@ describe('openNewFilePrompt', function () {
     const newFileName = 'invalid_file';
     const folderId = DEFAULT_FOLDER_ID;
 
-    const [newFileData, newFileDataMock] = getNewFileMock();
+    const [newFileData, newFileDataMock] = getNewFileMock(folderId);
 
     await openNewFilePrompt({
       folderId,
