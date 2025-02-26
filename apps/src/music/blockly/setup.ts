da1db2ed4df9dd1e40191d18259e7fe659423eb1
Mike Harvey <mike.harvey@code.org>
2025-02-26 10:49:01 -0500

    safe register helpers

--- apps/src/music/blockly/setup.ts
+++ apps/src/music/blockly/setup.ts
@@ -38,41 +38,16 @@ import {BlockConfig} from './types';
 export function setUpBlocklyForMusicLab() {
   backupFunctionDefinitons();
 
-  if (Blockly.Extensions.isRegistered(DEFAULT_TRACK_NAME_EXTENSION)) {
-    Blockly.Extensions.unregister(DEFAULT_TRACK_NAME_EXTENSION);
-  }
-  Blockly.Extensions.register(
+  safeRegisterExtension(
     DEFAULT_TRACK_NAME_EXTENSION,
     getDefaultTrackNameExtension()
   );
+  safeRegisterExtension(FIELD_EFFECTS_EXTENSION, effectsFieldExtension);
+  safeRegisterExtension(FIELD_SOUNDS_VALIDATOR, fieldSoundsValidator);
+  safeRegisterExtension(FIELD_PATTERNS_VALIDATOR, fieldPatternsValidator);
 
-  if (Blockly.Extensions.isRegistered(FIELD_EFFECTS_EXTENSION)) {
-    Blockly.Extensions.unregister(FIELD_EFFECTS_EXTENSION);
-  }
-  Blockly.Extensions.register(FIELD_EFFECTS_EXTENSION, effectsFieldExtension);
-
-  if (Blockly.Extensions.isRegistered(FIELD_SOUNDS_VALIDATOR)) {
-    Blockly.Extensions.unregister(FIELD_SOUNDS_VALIDATOR);
-  }
-  Blockly.Extensions.register(FIELD_SOUNDS_VALIDATOR, fieldSoundsValidator);
-
-  if (Blockly.Extensions.isRegistered(FIELD_PATTERNS_VALIDATOR)) {
-    Blockly.Extensions.unregister(FIELD_PATTERNS_VALIDATOR);
-  }
-  Blockly.Extensions.register(FIELD_PATTERNS_VALIDATOR, fieldPatternsValidator);
-
-  if (Blockly.Extensions.isRegistered(PLAY_MULTI_MUTATOR)) {
-    Blockly.Extensions.unregister(PLAY_MULTI_MUTATOR);
-  }
-  Blockly.Extensions.registerMutator(PLAY_MULTI_MUTATOR, playMultiMutator);
-
-  if (Blockly.Extensions.isRegistered(NEXT_CONNECTION_MUTATOR)) {
-    Blockly.Extensions.unregister(NEXT_CONNECTION_MUTATOR);
-  }
-  Blockly.Extensions.registerMutator(
-    NEXT_CONNECTION_MUTATOR,
-    nextConnectionMutator
-  );
+  safeRegisterMutator(PLAY_MULTI_MUTATOR, playMultiMutator);
+  safeRegisterMutator(NEXT_CONNECTION_MUTATOR, nextConnectionMutator);
 
   // Needed for TypeScript to recognize the type of the MUSIC_BLOCKS. Remove
   // after converting musicBlocks to TypeScript.
@@ -99,4 +74,21 @@ export function setUpBlocklyForMusicLab() {
   // Rename the new function placeholder text for Music Lab specifically.
   Blockly.Msg['PROCEDURES_DEFNORETURN_PROCEDURE'] =
     musicI18n.blockly_functionNamePlaceholder();
+}
+
+// Register an extension after checking if it is already registered.
+function safeRegisterExtension(name: string, initFn: () => void) {
+  if (Blockly.Extensions.isRegistered(name)) {
+    Blockly.Extensions.unregister(name);
+  }
+  Blockly.Extensions.register(name, initFn);
+}
+
+// Mixin objects use an any type in Blockly core
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function safeRegisterMutator(name: string, mixinObj: any) {
+  if (Blockly.Extensions.isRegistered(name)) {
+    Blockly.Extensions.unregister(name);
+  }
+  Blockly.Extensions.registerMutator(name, mixinObj);
 }
