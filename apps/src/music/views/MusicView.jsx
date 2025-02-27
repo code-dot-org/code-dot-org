dc8fa81baf3ec16e380fed97865801fdfbd15a95
Mike Harvey <mike.harvey@code.org>
2025-02-27 15:14:01 -0500

    support for templates

--- apps/src/music/views/MusicView.jsx
+++ apps/src/music/views/MusicView.jsx
@@ -510,11 +510,15 @@ class UnconnectedMusicView extends React.Component {
   };
 
   getStartSources = () => {
-    if (!this.props.levelProperties?.levelData?.startSources) {
-      const startSourcesFilename = 'startSources' + this.props.blockMode;
-      return require(`@cdo/static/music/${startSourcesFilename}.json`);
+    const templateSources = this.props.levelProperties?.templateSources;
+    const levelSources = this.props.levelProperties?.levelData?.startSources;
+    if (templateSources) {
+      return templateSources;
+    } else if (levelSources) {
+      return levelSources;
     } else {
-      return this.props.levelProperties?.levelData.startSources;
+      const defaultStartSourcesFilename = 'startSources' + this.props.blockMode;
+      return require(`@cdo/static/music/${defaultStartSourcesFilename}.json`);
     }
   };
 
