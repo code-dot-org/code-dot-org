957110c0f6978df27a804ed990888900d382d93c
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-02-26 17:16:21 -0800

    avoid setting window-size on mobile browsers; be more consistent with existing implementation

--- dashboard/test/ui/utils/selenium_browser.rb
+++ dashboard/test/ui/utils/selenium_browser.rb
@@ -2,7 +2,8 @@ require 'selenium/webdriver'
 
 module SeleniumBrowser
   def self.webdriver_options_object(browser: :chrome, headless: false)
-    options = Selenium::WebDriver::Options.send(browser, args: ['window-size=1280,1024'])
+    options = Selenium::WebDriver::Options.send(browser)
+    options.add_argument('window-size=1280,1024') if [:chrome, :firefox].include?(browser)
     options.add_argument('headless') if headless
     return options
   end
