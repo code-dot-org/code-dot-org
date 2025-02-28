4384b54a2589a15d94cfee90771e7a27ce766206
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-02-28 13:21:31 -0800

    second attempt at a custom network within drone

--- dashboard/test/ui/features/support/connect.rb
+++ dashboard/test/ui/features/support/connect.rb
@@ -69,23 +69,12 @@ def get_browser(test_run_name)
   browser = nil
   $selenium_http_client ||= SeleniumBrowser::Client.new(read_timeout: 2.minutes)
   if ENV['CI'] == 'true' # TODO: only on first try
+    ENV['PEGASUS_TEST_DOMAIN'] = 'ui-tests.code.org:3000'
+    ENV['DASHBOARD_TEST_DOMAIN'] = 'ui-tests-studio.code.org:3000'
+    ENV['HOUROFCODE_TEST_DOMAIN'] = 'ui-tests.hourofcode.com:3000'
+    ENV['CSEDWEEK_TEST_DOMAIN'] = 'ui-tests.csedweek.org:3000'
     browser = Retryable.retryable(tries: MAX_CONNECT_RETRIES) do
       SeleniumBrowser.remote(CI_SELENIUM_URL, http_client: $http_client) # TODO: headless?
-    end
-    # based on https://github.com/code-dot-org/code-dot-org/blob/158ed7c6a7f1d5ab8cfadd0e24ac5f11b83ca391/lib/cdo/sauce_connect.rb#L29
-    domains_to_redirect = [
-      %q(.*\.code.org),
-      %q(.*\.csedweek.org),
-      %q(.*\.hourofcode.com),
-      %q(.*\.codeprojects.org),
-    ]
-    domain_redirection_re = /^https?:\/\/(#{domains_to_redirect.join('|')})/
-    # based on https://github.com/SeleniumHQ/selenium/blob/selenium-4.25.0/rb/lib/selenium/webdriver/common/driver_extensions/has_network_interception.rb#L35
-    browser.intercept do |request, &continue|
-      puts "request.url before intercept: #{request.url.inspect}"
-      request.url.sub!(domain_redirection_re, 'http://ui-tests')
-      puts "request.url after intercept: #{request.url.inspect}"
-      continue.call(request)
     end
   elsif ENV['TEST_LOCAL'] == 'true'
     headless = ENV['TEST_LOCAL_HEADLESS'] == 'true'
