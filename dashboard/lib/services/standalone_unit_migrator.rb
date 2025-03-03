72b2ab42914ada2685e577f28c6ef3ca555e3ae4
alex-m-brown <alex.brown@code.org>
2025-03-03 11:23:23 -0500

    add rollback function to migrator

--- dashboard/lib/services/standalone_unit_migrator.rb
+++ dashboard/lib/services/standalone_unit_migrator.rb
@@ -5,7 +5,7 @@ module Services
     def initialize(unit, verbose: false, log_file: nil)
       @unit = unit
       @verbose = verbose
-      @logger = log_file ? Logger.new(File.open(log_file, 'w')) : Logger.new($stdout)
+      @logger = log_file ? Logger.new(log_file) : Logger.new($stdout)
     end
 
     def call
@@ -14,49 +14,74 @@ module Services
         return false
       end
 
-      @unit_copy = @unit.dup
+      unit_copy = @unit.dup
       log_initial_info if @verbose
       i18n_params = set_i18n_params
 
       create_new_unit_group
-      if @new_unit_group.errors.present?
-        log "Migration failed for #{@unit.name}: #{@new_unit_group.errors.full_messages.join(', ')}", type: "error"
+      if @unit_group.errors.present?
+        log "Migration failed for #{@unit.name}: #{@unit_group.errors.full_messages.join(', ')}", type: "error"
         return false
       end
 
       # Get existing Unit's course version
-      @course_version = @unit.course_version
-      if @course_version.nil?
+      course_version = @unit.course_version
+      if course_version.nil?
         log "Existing Unit's course version not found: #{@unit.name}", type: "error"
+        @unit_group.destroy!
         return false
       end
-      @original_course_version_id = @course_version.id
+      original_course_version_id = course_version.id
 
       # Point existing CourseVersion to the new UnitGroup
-      @course_version.update!(content_root_id: @new_unit_group.id, content_root_type: 'UnitGroup')
+      course_version.update!(content_root_id: @unit_group.id, content_root_type: 'UnitGroup')
       @unit.reload
-      @new_unit_group.reload
-
-      puts "line 40 #{@unit.course_version.inspect}"
-      puts "line 41 #{@new_unit_group.course_version.inspect}"
-
-      # @new_unit_group.update!(course_version: @course_version)
-      puts "Updated CourseVersion: #{@course_version.inspect}"
-      puts "Unit Group CourseVersion: #{@new_unit_group.course_version.inspect}"
-
-      puts "After reload: #{@new_unit_group.course_version.inspect}"
-      raise "WHY?" if @new_unit_group.course_version.nil?
+      @unit_group.reload
 
       # Clear "course" settings from the unit
       @unit.update!(is_course: false, version_year: nil, family_name: nil, published_state: nil, instruction_type: nil, instructor_audience: nil, participant_audience: nil)
-      @new_unit_group.reload
-      puts "After reload: #{@new_unit_group.course_version.inspect}"
-      raise "WHY?" if @new_unit_group.course_version.nil?
 
-      update_unit_group(i18n_params, @unit_copy.published_state)
+      update_unit_group(i18n_params, unit_copy.published_state)
 
       update_section_assignments
-      run_checks(@unit_copy, @original_course_version_id)
+
+      passed_checks = run_checks(course_version, unit_copy, original_course_version_id)
+      rollback unless passed_checks
+      passed_checks
+    end
+
+    def rollback
+      unless ENV['MIGRATE_STANDALONE_UNITS']
+        log "MIGRATE_STANDALONE_UNITS is not set", type: "error"
+        return false
+      end
+
+      log "Rolling back migration for unit #{@unit.name}"
+
+      @unit_group ||= @unit.unit_group
+      if @unit_group.nil?
+        log "Unit's unit_group not found: #{@unit.name}", type: "error"
+        return false
+      end
+
+      unit_group_id = @unit_group.id
+      course_version = @unit_group.course_version
+
+      # Set sections back to their original course
+      rollback_section_assignments
+
+      # Add "course" settings back to unit
+      @unit.update!(is_course: true, version_year: @unit_group.version_year, family_name: @unit_group.family_name, published_state: @unit_group.published_state, instruction_type: @unit_group.instruction_type, instructor_audience: @unit_group.instructor_audience, participant_audience: @unit_group.participant_audience)
+
+      course_version.update!(content_root_id: @unit.id, content_root_type: 'Unit')
+
+      # Make sure we are referencing the most recent data
+      @unit_group.reload
+      @unit.reload
+
+      @unit_group.destroy!
+
+      rollback_checks(unit_group_id)
     end
 
     private def log_initial_info
@@ -77,7 +102,7 @@ module Services
     end
 
     private def create_new_unit_group
-      @new_unit_group = UnitGroup.new(
+      @unit_group = UnitGroup.new(
         name: @unit.name,
         family_name: @unit.family_name,
         version_year: @unit.version_year,
@@ -86,68 +111,93 @@ module Services
         participant_audience: @unit.participant_audience,
         has_numbered_units: false
       )
-      @new_unit_group.save
+      @unit_group.save
     end
 
     private def update_unit_group(i18n_params, published_state)
-      puts "course version: #{@new_unit_group.course_version.inspect}"
-      @new_unit_group.reload
-      puts "After reload: #{@new_unit_group.course_version.inspect}"
-      raise "WHY?" if @new_unit_group.course_version.nil?
       # Add existing unit to new unit group and update strings
       # Dir.chdir(Rails.root) do
       #   @new_unit_group.persist_strings_and_units_changes([@unit.name], i18n_params)
       # end
-      @new_unit_group.update_scripts([@unit.name])
-      raise "WHY?" if @new_unit_group.course_version.nil?
-      puts "course version: #{@new_unit_group.course_version.inspect}"
+      @unit_group.update_scripts([@unit.name])
+
       # Publish the new unit group
-      @new_unit_group.update!(published_state: published_state)
-      puts "course version: #{@new_unit_group.course_version.inspect}"
+      @unit_group.update!(published_state: published_state)
     end
 
     private def update_section_assignments
       sections_updated = 0
       Section.where(script_id: @unit.id).each do |section|
-        section.update!(course_id: @new_unit_group.id)
+        section.update!(course_id: @unit_group.id)
         sections_updated += 1
       end
       log "Updated #{sections_updated} sections for unit #{@unit.name}" if @verbose
     end
 
     private def log(message, type: 'info')
-      puts message # CLEAN THIS UP
       @logger.send(type, message)
     end
 
-    private def run_checks(dupe_unit, original_course_version_id)
+    private def run_checks(course_version, dupe_unit, original_course_version_id)
       checks = {
-        "New UnitGroup is valid" => @new_unit_group.valid?,
+        "New UnitGroup is valid" => @unit_group.valid?,
         "Existing unit is valid" => @unit.valid?,
-        "CourseVersion is valid" => @course_version.valid?,
-        "New UnitGroup has the same name as the existing unit" => @new_unit_group.name == dupe_unit.name,
-        "New UnitGroup has the same family_name as the existing unit" => @new_unit_group.family_name == dupe_unit.family_name,
-        "New UnitGroup has the same version_year as the existing unit" => @new_unit_group.version_year == dupe_unit.version_year,
-        "New UnitGroup has the same instruction_type as the existing unit" => @new_unit_group.instruction_type == dupe_unit.instruction_type,
-        "New UnitGroup has the same instructor_audience as the existing unit" => @new_unit_group.instructor_audience == dupe_unit.instructor_audience,
-        "New UnitGroup has the same participant_audience as the existing unit" => @new_unit_group.participant_audience == dupe_unit.participant_audience,
-        "New UnitGroup has the same published_state as the existing unit" => @new_unit_group.published_state == dupe_unit.published_state,
-        "New UnitGroup is assigned to the existing unit" => @new_unit_group.default_units.first.id == @unit.id,
-        "New UnitGroup is a single unit course" => @new_unit_group.single_unit_course?,
-        "New UnitGroup has the same course_version as the existing unit" => @new_unit_group.course_version.id == original_course_version_id,
-        "CourseVersion has a content_root_type of 'UnitGroup'" => @course_version.content_root_type == 'UnitGroup',
-        "CourseVersion has a content_root of the new UnitGroup" => @course_version.content_root_id == @new_unit_group.id
+        "CourseVersion is valid" => course_version.valid?,
+        "New UnitGroup has the same name as the existing unit" => @unit_group.name == dupe_unit.name,
+        "New UnitGroup has the same family_name as the existing unit" => @unit_group.family_name == dupe_unit.family_name,
+        "New UnitGroup has the same version_year as the existing unit" => @unit_group.version_year == dupe_unit.version_year,
+        "New UnitGroup has the same instruction_type as the existing unit" => @unit_group.instruction_type == dupe_unit.instruction_type,
+        "New UnitGroup has the same instructor_audience as the existing unit" => @unit_group.instructor_audience == dupe_unit.instructor_audience,
+        "New UnitGroup has the same participant_audience as the existing unit" => @unit_group.participant_audience == dupe_unit.participant_audience,
+        "New UnitGroup has the same published_state as the existing unit" => @unit_group.published_state == dupe_unit.published_state,
+        "New UnitGroup is assigned to the existing unit" => @unit_group.default_units.first.id == @unit.id,
+        "New UnitGroup is a single unit course" => @unit_group.single_unit_course?,
+        "New UnitGroup has the same course_version as the existing unit" => @unit_group.course_version.id == original_course_version_id,
+        "CourseVersion has a content_root_type of 'UnitGroup'" => course_version.content_root_type == 'UnitGroup',
+        "CourseVersion has a content_root of the new UnitGroup" => course_version.content_root_id == @unit_group.id
       }
 
       # Determine if all checks passed
       all_passed = checks.values.all?
       unless all_passed
-        log "Incorrect Unit: #{dupe_unit.name}"
+        log("Checks failed for unit migration: #{@unit.name}", type: "error")
         failing_checks = checks.select {|_, result| !result}
         failing_checks.each {|description, result| log "#{description}: #{result}"}
       end
 
-      log "View the new UnitGroup here: https:#{CDO.studio_url(@new_unit_group.link)}" if @verbose
+      log "View the new UnitGroup here: https:#{CDO.studio_url(@unit_group.link)}" if @verbose
+      all_passed
+    end
+
+    private def rollback_section_assignments
+      sections_rollback = 0
+      Section.where(course_id: @unit_group.id).each do |section|
+        section.update!(course_id: nil)
+        sections_rollback += 1
+      end
+      log "Rolled back #{sections_rollback} sections for unit #{@unit.name}" if @verbose
+    end
+
+    private def rollback_checks(unit_group_id)
+      checks = {
+        "UnitGroup is destroyed" => UnitGroup.find_by(id: unit_group_id).nil?,
+        "Unit is valid" => @unit.valid?,
+        "CourseVersion is valid" => @unit.course_version.valid?,
+        "Unit is a course" => @unit.is_course?,
+        "Unit does not have a unit_group" => @unit.unit_group.nil?,
+        "CourseVersion has a content_root_type of 'UnitGroup'" => @unit.course_version.content_root_type == 'Unit',
+        "CourseVersion has a content_root of the new UnitGroup" => @unit.course_version.content_root_id == @unit.id
+      }
+
+      # Determine if all checks passed
+      all_passed = checks.values.all?
+      if all_passed
+        log("Migration successfully rolled back for unit #{@unit.name}")
+      else
+        log("Checks failed for unit migration rollback: #{@unit.name}", type: "error")
+        failing_checks = checks.select {|_, result| !result}
+        failing_checks.each {|description, result| log "#{description}: #{result}"}
+      end
       all_passed
     end
   end
--- dashboard/test/lib/services/standalone_unit_migrator_test.rb
+++ dashboard/test/lib/services/standalone_unit_migrator_test.rb
@@ -12,7 +12,7 @@ class Services::StandaloneUnitMigratorTest < ActiveSupport::TestCase
     before do
       ENV['MIGRATE_STANDALONE_UNITS'] = 'true'
       @course_offering = create :course_offering, key: family_name, display_name: family_name
-      @unit = create :standalone_unit, name: "#{family_name}-2025", family_name: family_name, version_year: "2025", published_state: "stable"
+      @unit = create :standalone_unit, name: "#{family_name}-2025", family_name: family_name, version_year: "2025", published_state: Curriculum::SharedCourseConstants::PUBLISHED_STATE.stable
       @course_version = create :course_version, course_offering: @course_offering, content_root: @unit, key: "2025", display_name: "2025", published_state: "stable"
 
       @temp_log_file = Tempfile.new("unit_migration.log")
@@ -26,6 +26,8 @@ class Services::StandaloneUnitMigratorTest < ActiveSupport::TestCase
 
     it 'successfully migrates a standalone unit to a UnitGroup' do
       assert migrate_unit
+      @course_version.reload
+      @unit.reload
 
       # Verify that the unit was successfully migrated
       new_unit_group = UnitGroup.find_by(name: @unit.name)
@@ -35,12 +37,10 @@ class Services::StandaloneUnitMigratorTest < ActiveSupport::TestCase
       assert_equal "teacher_led", new_unit_group.instruction_type
 
       # Verify the course_version now points to the new UnitGroup
-      @course_version.reload
       assert_equal new_unit_group, @course_version.content_root
       assert_equal @course_version, new_unit_group.course_version
 
       # Verify the unit's course-related settings are cleared
-      @unit.reload
       refute @unit.is_course, "Unit should have is_course set to false"
       assert_nil @unit.version_year, "Unit version_year should be cleared"
       assert_nil @unit.family_name, "Unit family_name should be cleared"
@@ -64,6 +64,47 @@ class Services::StandaloneUnitMigratorTest < ActiveSupport::TestCase
 
       log_contents = File.read(@temp_log_file.path)
       assert_match(/Migration failed for/, log_contents)
+    end
+
+    it 'rolls back migration when checks fail' do
+      described_instance.expects(:run_checks).returns(false)
+      refute migrate_unit
+
+      log_contents = File.read(@temp_log_file.path)
+      assert_match(/Rolling back migration for unit/, log_contents)
+    end
+  end
+  describe '#rollback' do
+    before do
+      ENV['MIGRATE_STANDALONE_UNITS'] = 'true'
+      @course_offering = create :course_offering, key: family_name, display_name: family_name
+      @unit = create :unit, name: "#{family_name}-2025", published_state: "stable"
+      @unit_group = create :single_unit_course, name: @unit.name, family_name: family_name, version_year: "2025", published_state: Curriculum::SharedCourseConstants::PUBLISHED_STATE.stable, unit: @unit
+      @course_version = create :course_version, course_offering: @course_offering, content_root: @unit_group, key: "2025", display_name: "2025", published_state: "stable"
+      @unit.reload
+
+      @temp_log_file = Tempfile.new("unit_migration.log")
+    end
+
+    after do
+      ENV.delete('MIGRATE_STANDALONE_UNITS')
+      @temp_log_file.close!
+      @temp_log_file.unlink
+    end
+
+    it 'successfully rolls back a migrated standalone unit' do
+      described_instance.rollback
+      @course_version.reload
+      @unit.reload
+
+      # Verify that the unit was successfully rolled back
+      refute UnitGroup.find_by(name: @unit.name), "UnitGroup should be destroyed"
+      assert_nil @unit.unit_group, "Unit's unit_group should be cleared"
+      assert_equal @unit, @course_version.content_root
+
+      # Verify that the log file contains the dynamically generated URL
+      log_contents = File.read(@temp_log_file.path)
+      assert_match(/Migration successfully rolled back for unit/, log_contents)
     end
   end
 end
