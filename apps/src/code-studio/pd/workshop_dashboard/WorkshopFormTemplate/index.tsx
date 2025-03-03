dc60b86ea15974e515b744b257f76475a55af40a
Ryan Driscoll <driscollrp@gmail.com>
2025-03-03 09:55:31 -0700

    feat: add state for workshop form

--- apps/src/code-studio/pd/workshop_dashboard/WorkshopFormTemplate/index.tsx
+++ apps/src/code-studio/pd/workshop_dashboard/WorkshopFormTemplate/index.tsx
@@ -1,6 +1,46 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
 
-import {WorkshopCourseConfig} from './types';
+import {
+  Session,
+  SessionFormState,
+  Workshop,
+  WorkshopCourseConfig,
+  WorkshopFormState,
+} from './types';
+
+export const workshopDataToState = (data: Workshop): WorkshopFormState => ({
+  course: data.course ?? null,
+  capacity: data.capacity ?? null,
+  description: data.description ?? null,
+  facilitators: data.facilitators ?? [],
+  fee: data.fee ?? null,
+  grades: data.grades ?? [],
+  hidden: data.hidden ?? null,
+  name: data.name ?? null,
+  notes: data.notes ?? null,
+  organizerId: data.organizer?.id ?? null,
+  prereq: data.prereq ?? null,
+  regionalPartnerId: data.regional_partner_id ?? null,
+  registrationLink: data.registration_link ?? null,
+  subject: data.subject ?? null,
+  suppressEmail: data.suppress_email ?? null,
+  courseOfferings: data.course_offerings ?? [],
+  participantGroupType: data.participant_group_type ?? null,
+  timeZone: data.time_zone ?? null,
+});
+
+export const sessionDataToState = (data: Session[]): SessionFormState[] =>
+  data.map(session => ({
+    id: session.id ?? null,
+    start: session.start ?? null,
+    end: session.end ?? null,
+    code: session.code ?? null,
+    locationAddress: session.location_address ?? null,
+    locationName: session.location_name ?? null,
+    meetingLink: session.meeting_link ?? null,
+    sessionFormat: session.session_format ?? null,
+  }));
 
 export const WorkshopFormTemplate: FC<WorkshopCourseConfig> = ({
   slug,
@@ -8,5 +48,53 @@ export const WorkshopFormTemplate: FC<WorkshopCourseConfig> = ({
   session_fields,
   fields,
 }) => {
+  const {workshopId} = useParams();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [formState, setFormState] = useState<WorkshopFormState>({
+    course: null,
+    capacity: null,
+    description: null,
+    facilitators: [],
+    fee: null,
+    grades: [],
+    hidden: false,
+    name: null,
+    notes: null,
+    organizerId: null,
+    prereq: null,
+    regionalPartnerId: null,
+    registrationLink: null,
+    subject: null,
+    suppressEmail: false,
+    courseOfferings: [],
+    participantGroupType: null,
+    timeZone: null,
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [sessions, setSessions] = useState<SessionFormState[]>([]);
+
+  useEffect(() => {
+    let mounted = true;
+    if (!workshopId) return;
+    const fetchWorkshop = async () => {
+      const response = await fetch(`/api/v1/pd/workshops/${workshopId}`);
+      if (response.ok) {
+        const data = await response.json();
+        if (mounted) {
+          setFormState(workshopDataToState(data));
+          setSessions(sessionDataToState(data.sessions));
+        }
+      }
+    };
+
+    fetchWorkshop();
+
+    return () => {
+      mounted = false;
+    };
+  }, [workshopId]);
+
   return <h1>{label}</h1>;
 };
--- apps/src/code-studio/pd/workshop_dashboard/WorkshopFormTemplate/types.ts
+++ apps/src/code-studio/pd/workshop_dashboard/WorkshopFormTemplate/types.ts
@@ -40,3 +40,74 @@ export interface WorkshopCourseConfig {
   session_fields: SessionFields;
   fields: WorkshopFields;
 }
+
+export interface Organizer {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface Session {
+  id: number;
+  start: string;
+  end: string;
+  code?: string;
+  location_address?: string;
+  location_name?: string;
+  meeting_link?: string;
+  session_format?: 'virtual' | 'in_person';
+}
+
+export interface SessionFormState {
+  id: number | null;
+  start: string | null;
+  end: string | null;
+  code: string | null;
+  locationAddress: string | null;
+  locationName: string | null;
+  meetingLink: string | null;
+  sessionFormat: 'virtual' | 'in_person' | null;
+}
+
+export interface Workshop {
+  course: string;
+  name: string;
+  capacity: number;
+  grades?: string[];
+  description?: string;
+  notes?: string;
+  suppress_email?: boolean;
+  regional_partner_id?: number;
+  organizer?: Organizer;
+  facilitators?: number[];
+  subject?: string;
+  fee?: string;
+  prereq?: string;
+  hidden?: boolean;
+  registration_link?: string;
+  sessions: Session[];
+  course_offerings?: string[];
+  participant_group_type?: string;
+  time_zone?: string;
+}
+
+export interface WorkshopFormState {
+  course: string | null;
+  capacity: number | null;
+  description: string | null;
+  facilitators: number[];
+  fee: string | null;
+  grades: string[];
+  hidden: boolean | null;
+  name: string | null;
+  notes: string | null;
+  organizerId: number | null;
+  prereq: string | null;
+  regionalPartnerId: number | null;
+  registrationLink: string | null;
+  subject: string | null;
+  suppressEmail: boolean | null;
+  courseOfferings: string[];
+  participantGroupType: string | null;
+  timeZone: string | null;
+}
