6da2952945cfe0601bc079e459b3d64870ca28a1
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-03-04 15:05:52 -0800

    remove hydration fix with useEffect

--- frontend/apps/marketing/src/components/carousels/videoCarousel/VideoCarousel.tsx
+++ frontend/apps/marketing/src/components/carousels/videoCarousel/VideoCarousel.tsx
@@ -4,7 +4,7 @@ import '@code-dot-org/component-library/carousel/index.css';
 import DSCOCarousel, {
   CarouselProps,
 } from '@code-dot-org/component-library/carousel';
-import React, {ReactNode, useState, useEffect} from 'react';
+import React, {ReactNode} from 'react';
 
 import Video from '@code-dot-org/component-library/video';
 
@@ -26,36 +26,27 @@ interface VideoCarouselProps extends CarouselProps {
 }
 
 const VideoCarousel: React.FC<VideoCarouselProps> = ({slides, ...props}) => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
+  console.log(slides);
   return (
     <>
-      {isClient && (
-        <DSCOCarousel
-          {...props}
-          showNavArrows={true}
-          slides={slides?.map(slide => {
-            const {videoTitle, youTubeId} = slide.fields || {};
-            return {
-              id: slide.fields.youTubeId,
-              slide: (
-                <Video
-                  videoTitle={videoTitle}
-                  youTubeId={youTubeId}
-                  showCaption={true}
-                  videoFallback={
-                    slide.fields.videoFallbackFile?.fields.file.url
-                  }
-                />
-              ),
-            };
-          })}
-        />
-      )}
+      <DSCOCarousel
+        {...props}
+        showNavArrows={true}
+        slides={slides?.map(slide => {
+          const {videoTitle, youTubeId} = slide.fields || {};
+          return {
+            id: slide.fields.youTubeId,
+            slide: (
+              <Video
+                videoTitle={videoTitle}
+                youTubeId={youTubeId}
+                showCaption={true}
+                videoFallback={slide.fields.videoFallbackFile?.fields.file.url}
+              />
+            ),
+          };
+        })}
+      />
     </>
   );
 };
