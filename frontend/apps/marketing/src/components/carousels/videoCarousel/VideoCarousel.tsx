c3837fe190e50f4d1643c28bf7252eaab6436e3d
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-03-04 14:17:33 -0800

    get carousels working and fix hydration error

--- frontend/apps/marketing/src/components/carousels/videoCarousel/VideoCarousel.tsx
+++ frontend/apps/marketing/src/components/carousels/videoCarousel/VideoCarousel.tsx
@@ -4,27 +4,61 @@ import '@code-dot-org/component-library/carousel/index.css';
 import DSCOCarousel, {
   CarouselProps,
 } from '@code-dot-org/component-library/carousel';
-import React from 'react';
+import React, {ReactNode, useState, useEffect} from 'react';
 
-// import Video from '@code-dot-org/component-library/video';
+import Video from '@code-dot-org/component-library/video';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const VideoCarousel: React.FC<CarouselProps> = ({slides, ...props}) => (
-  <>
-    <pre>{JSON.stringify(slides, null, 2)}</pre>
-    <DSCOCarousel
-      {...props}
-      showNavArrows={true}
-      // slides={slides?.map(slide => ({
-      //   id: 'id-' + crypto.randomUUID(),
-      //   slide: <Video {...slide.props} />,
-      // }))}
-    />
-  </>
-);
+interface VideoCarouselProps extends CarouselProps {
+  carouselId: string;
+  slides: {
+    id: string;
+    slide: ReactNode;
+    fields: {
+      videoTitle: string;
+      youTubeId: string;
+      videoFallbackFile: {fields: {file: {url: string}}};
+    };
+  }[];
+}
+
+const VideoCarousel: React.FC<VideoCarouselProps> = ({slides, ...props}) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <>
+      {isClient && (
+        <DSCOCarousel
+          {...props}
+          showNavArrows={true}
+          slides={slides?.map(slide => {
+            const {videoTitle, youTubeId} = slide.fields || {};
+            return {
+              id: slide.fields.youTubeId,
+              slide: (
+                <Video
+                  videoTitle={videoTitle}
+                  youTubeId={youTubeId}
+                  showCaption={true}
+                  videoFallback={
+                    slide.fields.videoFallbackFile?.fields.file.url
+                  }
+                />
+              ),
+            };
+          })}
+        />
+      )}
+    </>
+  );
+};
 
 export default VideoCarousel;
