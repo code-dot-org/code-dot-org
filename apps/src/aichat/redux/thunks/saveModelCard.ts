7235b5d06d9926e70660c293707be8d721e584a2
Ben Brooks <ben@code.org>
2025-03-04 13:14:25 -0800

    Wait for state update before save

--- apps/src/aichat/redux/thunks/saveModelCard.ts
+++ apps/src/aichat/redux/thunks/saveModelCard.ts
@@ -13,19 +13,18 @@ import {saveAiCustomization} from './helpers/saveAiCustomization';
 export const saveModelCard = createAsyncThunk(
   'aichat/saveModelCard',
   async (_, {dispatch, getState}) => {
-    const state = getState() as RootState;
-
-    const {currentAiCustomizations} = state.aichat;
+    const {currentAiCustomizations} = (getState() as RootState).aichat;
     const modelCardInfo = currentAiCustomizations.modelCardInfo;
     if (!hasFilledOutModelCard(modelCardInfo)) {
       dispatch(setModelCardProperty({property: 'isPublished', value: false}));
     }
 
+    const postUpdateState = getState() as RootState;
     await saveAiCustomization(
-      currentAiCustomizations,
+      postUpdateState.aichat.currentAiCustomizations,
       'saveModelCard',
       dispatch as AppDispatch,
-      parseInt(state.progress.currentLevelId || '')
+      parseInt(postUpdateState.progress.currentLevelId || '')
     );
   }
 );
