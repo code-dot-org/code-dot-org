6fd8ce4dd2fb3985fdf3f8a3c00b6afd66f8c376
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-03-05 12:35:46 -0800

    add waitBeforeCapture to youtube thumbnail element

--- frontend/packages/component-library/src/carousel/stories/Carousel.story.tsx
+++ frontend/packages/component-library/src/carousel/stories/Carousel.story.tsx
@@ -363,8 +363,7 @@ VideoCarousels.parameters = {
         'Videos carousels can show or hide captions based on the `showCaption` prop on the `Video` component. There are margins applied between carousels so this displays nicely in Storybook, but this is not a part of the component itself.',
     },
   },
-  // Turning this off because this test is very flaky
-  eyes: {include: false},
+  eyes: {waitBeforeCapture: '.ytp-cued-thumbnail-overlay-image'},
 };
 VideoCarousels.play = async ({canvasElement}: {canvasElement: HTMLElement}) => {
   const canvas = within(canvasElement);
