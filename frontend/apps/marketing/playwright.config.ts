ddc8f90542ced03a7a39bf0cd6e0eb44000b1c96
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-03-04 20:46:56 -0800

    enhance eyes

--- frontend/apps/marketing/playwright.config.ts
+++ frontend/apps/marketing/playwright.config.ts
@@ -1,13 +1,6 @@
+import 'dotenv/config';
 import {defineConfig, devices} from '@playwright/test';
 import {EyesFixture} from '@applitools/eyes-playwright/fixture';
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -23,7 +16,7 @@ export default defineConfig<EyesFixture>({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? '75%' : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: '@applitools/eyes-playwright/reporter',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
