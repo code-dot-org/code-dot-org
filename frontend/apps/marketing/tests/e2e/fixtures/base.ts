72d0249d41f08626007e6feed4eb6f092d16a749
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-03-04 19:49:20 -0800

    skip eyes

--- frontend/apps/marketing/tests/e2e/fixtures/base.ts
+++ frontend/apps/marketing/tests/e2e/fixtures/base.ts
@@ -3,7 +3,8 @@ import {test as eyesTest} from '@applitools/eyes-playwright/fixture';
 
 export const test = mergeTests(eyesTest);
 
-test.beforeEach((args, testInfo) => {
+// eslint-disable-next-line no-empty-pattern
+test.beforeEach(({}, testInfo) => {
   if (testInfo.tags.includes('@eyes') || testInfo.title === 'eyes') {
     // Eyes tests will differ from the baseline when run locally due to operating system rendering differences
     // Instead, run eyes on Github Actions to ensure a consistent render
