02bd58f300c87014fb24e46e097cc8e83969a5b9
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-03-04 16:29:42 -0800

    use useId for consistent hydration and safely handle empty slides

--- frontend/apps/marketing/src/components/carousels/videoCarousel/VideoCarousel.tsx
+++ frontend/apps/marketing/src/components/carousels/videoCarousel/VideoCarousel.tsx
@@ -4,7 +4,7 @@ import '@code-dot-org/component-library/carousel/index.css';
 import DSCOCarousel, {
   CarouselProps,
 } from '@code-dot-org/component-library/carousel';
-import React, {ReactNode} from 'react';
+import React, {ReactNode, useId} from 'react';
 
 import Video from '@code-dot-org/component-library/video';
 
@@ -26,26 +26,48 @@ interface VideoCarouselProps extends CarouselProps {
 }
 
 const VideoCarousel: React.FC<VideoCarouselProps> = ({slides, ...props}) => {
-  console.log(slides);
+  const carouselId = `id-${useId().replaceAll(':', '')}`;
+
+  // Workaround for the experience builder not working with Array
+  if (slides[0] == null) {
+    return <div>Please open the preview tab to preview the carousel.</div>;
+  }
+
+  const getSlides = () => {
+    const carouselSlides = [];
+
+    for (const carouselSlide of slides) {
+      if (!carouselSlide) {
+        continue;
+      }
+
+      const {videoTitle, youTubeId} = carouselSlide.fields;
+
+      carouselSlides.push({
+        id: carouselSlide.fields.youTubeId,
+        slide: (
+          <Video
+            videoTitle={videoTitle}
+            youTubeId={youTubeId}
+            showCaption={true}
+            videoFallback={
+              carouselSlide.fields.videoFallbackFile?.fields.file.url
+            }
+          />
+        ),
+      });
+    }
+
+    return carouselSlides;
+  };
+
   return (
     <>
       <DSCOCarousel
         {...props}
+        carouselId={carouselId}
         showNavArrows={true}
-        slides={slides?.map(slide => {
-          const {videoTitle, youTubeId} = slide.fields || {};
-          return {
-            id: slide.fields.youTubeId,
-            slide: (
-              <Video
-                videoTitle={videoTitle}
-                youTubeId={youTubeId}
-                showCaption={true}
-                videoFallback={slide.fields.videoFallbackFile?.fields.file.url}
-              />
-            ),
-          };
-        })}
+        slides={getSlides()}
       />
     </>
   );
