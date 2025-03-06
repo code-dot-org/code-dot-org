33d8937a0903445e50bf66eac25740b3f7bef8d3
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-03-06 12:43:37 -0800

    get slides with useMemo

--- frontend/apps/marketing/src/components/carousels/videoCarousel/VideoCarousel.tsx
+++ frontend/apps/marketing/src/components/carousels/videoCarousel/VideoCarousel.tsx
@@ -4,14 +4,9 @@ import '@code-dot-org/component-library/carousel/index.css';
 import DSCOCarousel, {
   CarouselProps,
 } from '@code-dot-org/component-library/carousel';
-import React, {ReactNode} from 'react';
+import React, {ReactNode, useMemo} from 'react';
 
 import Video from '@code-dot-org/component-library/video';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 interface VideoCarouselProps extends CarouselProps {
   slides: {
@@ -40,39 +35,25 @@ const VideoCarousel: React.FC<VideoCarouselProps> = ({slides, ...props}) => {
     );
   }
 
-  const getSlides = () => {
-    const carouselSlides = [];
-
-    for (const carouselSlide of slides) {
-      if (!carouselSlide) {
-        continue;
-      }
-
-      const {videoTitle, youTubeId} = carouselSlide.fields;
-
-      carouselSlides.push({
-        id: carouselSlide.fields.youTubeId,
-        slide: (
-          <Video
-            videoTitle={videoTitle}
-            youTubeId={youTubeId}
-            showCaption={true}
-            videoFallback={
-              carouselSlide.fields.videoFallbackFile?.fields.file.url
-            }
-          />
-        ),
-      });
-    }
-
-    return carouselSlides;
-  };
-
-  return (
-    <>
-      <DSCOCarousel {...props} showNavArrows={true} slides={getSlides()} />
-    </>
+  const slidesData = useMemo(
+    () =>
+      slides
+        .filter(Boolean) // Removes any falsy values
+        .map(({fields: {videoTitle, youTubeId, videoFallbackFile}}) => ({
+          id: youTubeId,
+          slide: (
+            <Video
+              videoTitle={videoTitle}
+              youTubeId={youTubeId}
+              showCaption={true}
+              videoFallback={videoFallbackFile?.fields.file.url}
+            />
+          ),
+        })),
+    [slides], // Dependencies: recompute only when `slides` changes
   );
+
+  return <DSCOCarousel {...props} showNavArrows={true} slides={slidesData} />;
 };
 
 export default VideoCarousel;
