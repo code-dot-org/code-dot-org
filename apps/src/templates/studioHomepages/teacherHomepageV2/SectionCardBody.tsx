feaf683b8b300c8a8517e2f1d4f5c5817e330838
Mark Barnes <mark.barnes@code.org>
2025-03-07 09:12:12 -0600

    add task buttons and update component styling

--- apps/src/templates/studioHomepages/teacherHomepageV2/SectionCardBody.tsx
+++ apps/src/templates/studioHomepages/teacherHomepageV2/SectionCardBody.tsx
@@ -1,6 +1,9 @@
+import FontAwesomeV6Icon from '@code-dot-org/component-library/fontAwesomeV6Icon';
+import {BodyThreeText} from '@code-dot-org/component-library/typography';
 import React from 'react';
 
 import {Section} from '@cdo/apps/templates/teacherDashboard/types/teacherSectionTypes';
+import i18n from '@cdo/locale';
 
 import {CourseContentDropdown} from './CourseContentDropdown';
 
@@ -14,6 +17,42 @@ export const SectionCardBody: React.FC<SectionCardBodyProps> = ({section}) => {
   return (
     <div className={styles.sectionCardBody}>
       <CourseContentDropdown section={section} />
+      <div className={styles.sectionCardBodyRight}>
+        <button type={'button'} className={styles.taskButtons}>
+          <div className={styles.taskButtonLeft}>
+            <FontAwesomeV6Icon
+              className={styles.taskButtonIcons}
+              iconName={'chart-line'}
+              iconStyle={'solid'}
+            />
+            <BodyThreeText style={{marginBottom: 0}}>
+              {i18n.viewProgressButton()}
+            </BodyThreeText>
+          </div>
+          <FontAwesomeV6Icon
+            className={styles.taskButtonArrow}
+            iconName={'arrow-right'}
+            iconStyle={'solid'}
+          />
+        </button>
+        <button type={'button'} className={styles.taskButtons}>
+          <div className={styles.taskButtonLeft}>
+            <FontAwesomeV6Icon
+              className={styles.taskButtonIcons}
+              iconName={'folder-open'}
+              iconStyle={'solid'}
+            />
+            <BodyThreeText style={{marginBottom: 0}}>
+              {i18n.viewLessonMaterialsButton()}
+            </BodyThreeText>
+          </div>
+          <FontAwesomeV6Icon
+            className={styles.taskButtonArrow}
+            iconName={'arrow-right'}
+            iconStyle={'solid'}
+          />
+        </button>
+      </div>
     </div>
   );
 };
