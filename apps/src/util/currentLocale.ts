fdc00ce75cc55eb4422ee3a3ca65c5981072dde9
Continuous Integration <dev@code.org>
2025-03-08 06:57:27 +0000

    Updates shims.

--- apps/src/util/currentLocale.ts
+++ apps/src/util/currentLocale.ts
@@ -2,4 +2,7 @@ import {get} from 'js-cookie';
 
 import {DefaultLocale} from '@cdo/generated-scripts/sharedConstants';
 
-export default () => get('language_') || DefaultLocale;
+export default () => {
+  console.log("getting locale", document.querySelector("script#localize-js")?.getAttribute("data-locale"));
+  return document.querySelector("script#localize-js")?.getAttribute("data-locale") || get('language_') || DefaultLocale;
+};
