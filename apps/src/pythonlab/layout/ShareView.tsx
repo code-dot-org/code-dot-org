7099ac1260b7415309fb785cadb49c5f2958073b
Molly Moen <molly@code.org>
2025-03-10 15:24:08 -0700

    set up sidebar

--- apps/src/pythonlab/layout/ShareView.tsx
+++ apps/src/pythonlab/layout/ShareView.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import Button from '@code-dot-org/component-library/button';
+import Toggle from '@code-dot-org/component-library/toggle';
+import React, {useState} from 'react';
 
 import {hasPreview} from '@cdo/apps/codebridge';
+import Lab2Registry from '@cdo/apps/lab2/Lab2Registry';
+import pythonlabI18n from '@cdo/apps/pythonlab/locale';
 import {useAppSelector} from '@cdo/apps/util/reduxHooks';
+import commonI18n from '@cdo/locale';
 
 import ConsoleAndPreviewShare from './ConsoleAndPreviewShare';
 import ConsoleShare from './ConsoleShare';
@@ -12,11 +17,53 @@ const ShareView: React.FunctionComponent = () => {
   const miniApp = useAppSelector(
     state => state.lab2Project.projectSources?.labConfig?.miniApp?.name
   );
+  const projectManager = Lab2Registry.getInstance().getProjectManager();
+  const onViewCode = () => {
+    projectManager?.redirectToView();
+  };
+
+  const onRemix = () => {
+    projectManager?.redirectToRemix();
+  };
+  const [consoleVisible, setConsoleVisible] = useState(false);
+
+  const showPreview = hasPreview(miniApp);
 
   return (
-    <div className={moduleStyles.shareContainer}>
-      <div className={moduleStyles.sidebar}>Sidebar</div>
-      {hasPreview(miniApp) ? <ConsoleAndPreviewShare /> : <ConsoleShare />}
+    <div>
+      <div className={moduleStyles.shareContainer}>
+        <div className={moduleStyles.sidebar} data-theme="Dark">
+          {showPreview && (
+            <div className={moduleStyles.consoleToggle}>
+              <Toggle
+                checked={consoleVisible}
+                onChange={e => setConsoleVisible(e.target.checked)}
+                name={pythonlabI18n.changeConsoleVisibility()}
+                label={pythonlabI18n.console()}
+                position={'right'}
+                size={'xs'}
+              />
+            </div>
+          )}
+          <Button
+            text={commonI18n.viewCode()}
+            type="tertiary"
+            color="black"
+            size="xs"
+            iconLeft={{iconStyle: 'solid', iconName: 'code'}}
+            onClick={onViewCode}
+          />
+          <Button
+            text={commonI18n.makeMyOwn()}
+            type="tertiary"
+            color="black"
+            size="xs"
+            iconLeft={{iconStyle: 'regular', iconName: 'pen-to-square'}}
+            onClick={onRemix}
+          />
+        </div>
+        {showPreview ? <ConsoleAndPreviewShare /> : <ConsoleShare />}
+      </div>
     </div>
   );
 };
