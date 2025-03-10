9dae0d4d884dfb93012a344f76022d0dbe320e00
Molly Moen <molly@code.org>
2025-03-10 09:41:45 -0700

    create share view component

---
+++ apps/src/pythonlab/layout/ShareView.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import moduleStyles from './share-view.module.scss';
+
+const ShareView: React.FunctionComponent = () => {
+  return <div className={moduleStyles.container} />;
+};
+
+export default ShareView;
