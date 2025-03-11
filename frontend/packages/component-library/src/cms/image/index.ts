f310319d0b47198c8b35210aaaed9f5dc5f6a8a8
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-03-11 08:57:59 -0700

    uncomment the /index.css import

--- frontend/packages/component-library/src/cms/image/index.ts
+++ frontend/packages/component-library/src/cms/image/index.ts
@@ -1,6 +1,5 @@
-// UNCOMMENT THIS WHEN THIS PR IS MERGED: https://github.com/code-dot-org/code-dot-org/pull/64353
 // Auto-import SASS generated CSS
-// import './index.css';
+import './index.css';
 
 export {default as Image, ImageProps} from './Image';
 export {default as default} from './Image';
