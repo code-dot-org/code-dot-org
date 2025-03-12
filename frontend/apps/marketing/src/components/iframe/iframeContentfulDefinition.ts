0d1152d623155041fa4ae8f8667344e5d8a2f3d9
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-03-12 18:50:55 +0200

    CMS-394: Update image URLs

--- frontend/apps/marketing/src/components/iframe/iframeContentfulDefinition.ts
+++ frontend/apps/marketing/src/components/iframe/iframeContentfulDefinition.ts
@@ -6,12 +6,12 @@ export const IframeContentfulComponentDefinition: ComponentDefinition = {
   name: 'iFrame',
   category: 'Advanced',
   thumbnailUrl:
-    'https://images.ctfassets.net/90t6bu6vlf76/1qy9FC9Bqb4ADrpyszIa5M/5e791afb110fc5bc8caa95710c1dea16/component_code_thumbnail.png',
+    'https://images.ctfassets.net/90t6bu6vlf76/1qy9FC9Bqb4ADrpyszIa5M/eb4c9dde9c1c90ab40036e8fa4412697/component_iframe_thumbnail.png',
   tooltip: {
     description:
       'Embed external content using an iframe. Ideal for embedding videos, interactive tools, or third-party widgets within a page.',
     imageUrl:
-      'https://images.ctfassets.net/90t6bu6vlf76/75ulYKJrhP83vfIre5Rm88/8e94a8b30dc113217b6399f19ebaf3c1/component_code_tooltip.png',
+      'https://images.ctfassets.net/90t6bu6vlf76/75ulYKJrhP83vfIre5Rm88/4a64acf16b04ac82f94dc0e6cb6c8b77/component_iframe_tooltip.png',
   },
   // Adding an empty array here so no default style options show in the Design tab.
   builtInStyles: [],
