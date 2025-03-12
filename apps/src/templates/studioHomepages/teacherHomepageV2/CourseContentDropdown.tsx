4c52d293c1a685da86c72c26666f0ebe68942e5b
Mark Barnes <mark.barnes@code.org>
2025-03-12 16:24:28 -0500

    implement dropdown

--- apps/src/templates/studioHomepages/teacherHomepageV2/CourseContentDropdown.tsx
+++ apps/src/templates/studioHomepages/teacherHomepageV2/CourseContentDropdown.tsx
@@ -1,3 +1,4 @@
+import {SimpleDropdown} from '@code-dot-org/component-library/dropdown';
 import {BodyThreeText} from '@code-dot-org/component-library/typography';
 import React from 'react';
 
@@ -13,12 +14,47 @@ interface CourseContentDropdownProps {
 export const CourseContentDropdown: React.FC<CourseContentDropdownProps> = ({
   section,
 }) => {
+  const dropdownItems = [
+    {
+      value: 'Go to a lesson',
+      text: 'Go to a lesson',
+    },
+    {
+      value: 'test1',
+      text: 'test1',
+    },
+    {
+      value: 'test2',
+      text: 'test2',
+    },
+    {
+      value: 'test3',
+      text: 'test3',
+    },
+  ];
+
+  const onDropdownChange = (args: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(args.target.value);
+  };
+
   return (
     <div className={styles.courseContentDropdownContainer}>
       <BodyThreeText>
         <b>{`${i18n.course()}: `}</b>
         {section.courseDisplayName}
       </BodyThreeText>
+      <SimpleDropdown
+        className={styles.courseContentDropdown}
+        name="go-to-lesson-dropdown"
+        labelText="Go to a lesson"
+        isLabelVisible={false}
+        items={dropdownItems}
+        selectedValue="Go to a lesson"
+        size="m"
+        color="gray"
+        dropdownTextThickness="thin"
+        onChange={args => onDropdownChange(args)}
+      />
     </div>
   );
 };
