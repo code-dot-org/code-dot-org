73da66c0f239c138d8a95add272e169a1526269a
Carl Reiner <carl.reiner@code.org>
2025-03-13 10:48:29 -0700

    modify controller to handle new content type shape

--- dashboard/engines/marketing/app/controllers/marketing/teacher/promotions_controller.rb
+++ dashboard/engines/marketing/app/controllers/marketing/teacher/promotions_controller.rb
@@ -14,13 +14,11 @@ module Marketing
         return head :not_found unless entry && entry.content_type.id == TEACHER_HOMEPAGE_SIDEBAR_CONTENT_TYPE
 
         result = entry.fields.clone
-        result.transform_values! do |v|
-          v.fields.transform_values do |f|
-            f.is_a?(Contentful::Asset) ? f.image_url : f
-          end
+        ads = result[:sidebar_ads].map do |ad|
+          ad.fields.transform_values! {|v| v.is_a?(Contentful::Asset) ? v.image_url : v}
         end
 
-        render json: result
+        render json: ads
       rescue ArgumentError => exception
         render json: {error: exception.message}, status: :bad_request
       end
