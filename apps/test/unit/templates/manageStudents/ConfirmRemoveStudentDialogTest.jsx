73b90188fee95333eea600714a8aa7e1e6cfed85
Mario Gil Correa <mario.gil-correa@code.org>
2025-03-13 13:41:46 -0400

    reorganized tests in ConfirmRemoveStudentDialog test

--- apps/test/unit/templates/manageStudents/ConfirmRemoveStudentDialogTest.jsx
+++ apps/test/unit/templates/manageStudents/ConfirmRemoveStudentDialogTest.jsx
@@ -7,13 +7,17 @@ import ConfirmRemoveStudentDialog, {
 } from '@cdo/apps/templates/manageStudents/ConfirmRemoveStudentDialog';
 import i18n from '@cdo/locale';
 
-const studentName = MINIMUM_TEST_PROPS.studentName;
-
 describe('ConfirmRemoveStudentDialog', () => {
-  const defaultProps = {
+  const DEFAULT_PROPS = {
     ...MINIMUM_TEST_PROPS,
     onConfirm: jest.fn(),
     onCancel: jest.fn(),
+  };
+
+  const studentName = DEFAULT_PROPS.studentName;
+
+  const renderComponent = (props = {}) => {
+    return render(<ConfirmRemoveStudentDialog {...DEFAULT_PROPS} {...props} />);
   };
 
   const getRemoveStudentButton = () =>
@@ -25,88 +29,82 @@ describe('ConfirmRemoveStudentDialog', () => {
     jest.clearAllMocks();
   });
 
-  it('renders the dialog with the correct header when the student has signed in', () => {
+  describe('when the student has never signed in', () => {
     i18n.removeStudentBody1 = jest.fn(
       () => 'This will remove the student and their records.'
     );
-    render(
-      <ConfirmRemoveStudentDialog {...defaultProps} hasEverSignedIn={true} />
-    );
+    it('renders the dialog with the correct header', () => {
+      renderComponent({hasEverSignedIn: false});
 
-    expect(
-      screen.getByText(i18n.removeStudentAndRecordsHeader({studentName}))
-    ).toBeInTheDocument();
-    expect(screen.getByText(i18n.removeStudentBody1())).toBeInTheDocument();
-    expect(screen.getByText(i18n.learnMore())).toBeInTheDocument();
+      expect(
+        screen.getByText(i18n.removeUnusedStudentHeader({studentName}))
+      ).toBeInTheDocument();
+      expect(
+        screen.queryByText(i18n.removeStudentBody1())
+      ).not.toBeInTheDocument();
+    });
+
+    it('does not render the "learn more" link', () => {
+      renderComponent({hasEverSignedIn: false});
+
+      expect(screen.queryByText(i18n.learnMore())).not.toBeInTheDocument();
+    });
   });
 
-  it('renders the dialog with the correct header when the student has never signed in', () => {
-    render(
-      <ConfirmRemoveStudentDialog {...defaultProps} hasEverSignedIn={false} />
+  describe('when students has signed in', () => {
+    i18n.removeStudentBody1 = jest.fn(
+      () => 'This will remove the student and their records.'
     );
+    it('renders the dialog with the correct header', () => {
+      renderComponent({hasEverSignedIn: true});
 
-    expect(
-      screen.getByText(i18n.removeUnusedStudentHeader({studentName}))
-    ).toBeInTheDocument();
-    expect(
-      screen.queryByText(i18n.removeStudentBody1())
-    ).not.toBeInTheDocument();
-  });
+      expect(
+        screen.getByText(i18n.removeStudentAndRecordsHeader({studentName}))
+      ).toBeInTheDocument();
+      expect(screen.getByText(i18n.removeStudentBody1())).toBeInTheDocument();
+      expect(screen.getByText(i18n.learnMore())).toBeInTheDocument();
+    });
 
-  it('renders the "send home instructions" button when the student depends on this section for login', () => {
-    render(
-      <ConfirmRemoveStudentDialog
-        {...defaultProps}
-        hasEverSignedIn={true}
-        dependsOnThisSectionForLogin={true}
-      />
-    );
+    it('calls onConfirm when the confirm button is clicked', async () => {
+      const user = userEvent.setup();
+      renderComponent({hasEverSignedIn: true});
+      await user.click(getRemoveStudentButton());
 
-    expect(screen.getByText(i18n.removeStudentBody2())).toBeInTheDocument();
-    expect(
-      screen.getByText(i18n.removeStudentSendHomeInstructions())
-    ).toBeInTheDocument();
-  });
+      expect(DEFAULT_PROPS.onConfirm).toHaveBeenCalled();
+    });
 
-  it('calls onConfirm when the confirm button is clicked', async () => {
-    const user = userEvent.setup();
-    render(
-      <ConfirmRemoveStudentDialog {...defaultProps} hasEverSignedIn={true} />
-    );
-    await user.click(getRemoveStudentButton());
+    it('calls onCancel when the cancel button is clicked', async () => {
+      const user = userEvent.setup();
+      renderComponent({hasEverSignedIn: true});
+      await user.click(getCancelButton());
 
-    expect(defaultProps.onConfirm).toHaveBeenCalled();
-  });
+      expect(DEFAULT_PROPS.onCancel).toHaveBeenCalled();
+    });
 
-  it('calls onCancel when the cancel button is clicked', async () => {
-    const user = userEvent.setup();
-    render(
-      <ConfirmRemoveStudentDialog {...defaultProps} hasEverSignedIn={true} />
-    );
+    describe('when the student depends on this section for login', () => {
+      it('renders the "send home instructions" button', () => {
+        renderComponent({
+          hasEverSignedIn: true,
+          dependsOnThisSectionForLogin: true,
+        });
 
-    await user.click(getCancelButton());
+        expect(screen.getByText(i18n.removeStudentBody2())).toBeInTheDocument();
+        expect(
+          screen.getByText(i18n.removeStudentSendHomeInstructions())
+        ).toBeInTheDocument();
+      });
+    });
 
-    expect(defaultProps.onCancel).toHaveBeenCalled();
-  });
+    describe('when the disable prop is true', () => {
+      it('disables the confirm and cancel buttons', () => {
+        renderComponent({
+          hasEverSignedIn: true,
+          disabled: true,
+        });
 
-  it('disables the confirm and cancel buttons when the disabled prop is true', () => {
-    render(
-      <ConfirmRemoveStudentDialog
-        {...defaultProps}
-        hasEverSignedIn={true}
-        disabled={true}
-      />
-    );
-
-    expect(getRemoveStudentButton()).toBeDisabled();
-    expect(getCancelButton()).toBeDisabled();
-  });
-
-  it('does not render the "learn more" link when the student has never signed in', () => {
-    render(
-      <ConfirmRemoveStudentDialog {...defaultProps} hasEverSignedIn={false} />
-    );
-
-    expect(screen.queryByText(i18n.learnMore())).not.toBeInTheDocument();
+        expect(getRemoveStudentButton()).toBeDisabled();
+        expect(getCancelButton()).toBeDisabled();
+      });
+    });
   });
 });
