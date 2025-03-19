fac6091f1b60c6345eabe33c8441bf8bf7138b1a
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-03-19 09:53:20 -0700

    create index.ts

---
+++ frontend/packages/component-library/src/actionBlock/index.ts
@@ -0,0 +1,5 @@
+// Auto-import SASS generated CSS
+import './index.css';
+
+export {default as ActionBlock, ActionBlockProps} from './ActionBlock';
+export {default as default} from './ActionBlock';
