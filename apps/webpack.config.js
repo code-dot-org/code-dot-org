4e1371bf6eeb19239324987fe1aa43a7cf608f60
Molly Moen <molly@code.org>
2025-03-19 16:40:01 -0700

    still does not work

--- apps/webpack.config.js
+++ apps/webpack.config.js
@@ -330,6 +330,10 @@ const WEBPACK_BASE_CONFIG = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.wasm$/,
+        use: ['wasm-loader'],
+      },
       ...(process.env.DEV
         ? [
             // Enable source maps locally for Blockly for easier debugging.
