60d3a3210c35666122f76df3f2adbe01a135643d
Mark Barnes <mark.barnes@code.org>
2025-03-19 10:53:19 -0500

    Add empty states and unit tests

--- apps/src/templates/studioHomepages/teacherHomepageV2/SectionCardBody.tsx
+++ apps/src/templates/studioHomepages/teacherHomepageV2/SectionCardBody.tsx
@@ -6,6 +6,7 @@ import i18n from '@cdo/locale';
 import {TEACHER_NAVIGATION_PATHS} from '../../teacherNavigation/TeacherNavigationPaths';
 
 import {CourseContentDropdown} from './CourseContentDropdown';
+import {EmptyStateButton} from './EmptyStateButton';
 import {TaskButton} from './TaskButton';
 
 import styles from './teacherHomepage.module.scss';
@@ -17,20 +18,42 @@ interface SectionCardBodyProps {
 export const SectionCardBody: React.FC<SectionCardBodyProps> = ({section}) => {
   return (
     <div className={styles.sectionCardBody}>
-      <CourseContentDropdown section={section} />
+      <div className={styles.sectionCardBodyLeft}>
+        {section.courseId ? (
+          <CourseContentDropdown section={section} />
+        ) : (
+          <EmptyStateButton
+            buttonText={i18n.assignACourseButton()}
+            icon={'book-open-cover'}
+            sectionId={section.id}
+            path={TEACHER_NAVIGATION_PATHS.settings}
+          />
+        )}
+      </div>
       <div className={styles.sectionCardBodyRight}>
-        <TaskButton
-          buttonText={i18n.viewProgressButton()}
-          icon={'chart-line'}
-          sectionId={section.id}
-          path={TEACHER_NAVIGATION_PATHS.progress}
-        />
-        <TaskButton
-          buttonText={i18n.viewLessonMaterialsButton()}
-          icon={'folder-open'}
-          sectionId={section.id}
-          path={TEACHER_NAVIGATION_PATHS.lessonMaterials}
-        />
+        {section.studentCount > 0 ? (
+          <TaskButton
+            buttonText={i18n.viewProgressButton()}
+            icon={'chart-line'}
+            sectionId={section.id}
+            path={TEACHER_NAVIGATION_PATHS.progress}
+          />
+        ) : (
+          <EmptyStateButton
+            buttonText={i18n.addStudents()}
+            icon={'users'}
+            sectionId={section.id}
+            path={TEACHER_NAVIGATION_PATHS.roster}
+          />
+        )}
+        {section.courseId && (
+          <TaskButton
+            buttonText={i18n.viewLessonMaterialsButton()}
+            icon={'folder-open'}
+            sectionId={section.id}
+            path={TEACHER_NAVIGATION_PATHS.lessonMaterials}
+          />
+        )}
       </div>
     </div>
   );
