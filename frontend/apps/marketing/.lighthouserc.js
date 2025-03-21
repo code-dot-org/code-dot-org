da14e49bd4245613ddc83860e880a5c73afee776
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-03-21 15:17:30 -0700

    feat(marketing): turn off image related Lighthouse checks

--- frontend/apps/marketing/.lighthouserc.js
+++ frontend/apps/marketing/.lighthouserc.js
@@ -2,13 +2,15 @@ const assertions = {
   'bf-cache': 'off',
   'color-contrast': 'off',
   'inspector-issues': 'off',
-  'offscreen-images': ['error', {minScore: 0.5, maxLength: 1}],
+  'offscreen-images': 'off',
   'total-byte-weight': ['error', {minScore: 0.5}],
   'unused-css-rules': ['error', {maxLength: 5}],
   'unused-javascript': ['error', {maxLength: 10}],
   'uses-text-compression': ['error', {maxLength: 5}],
   'third-party-cookies': 'off',
   'uses-rel-preconnect': 'off',
+  'unsized-images': 'off',
+  'uses-responsive-images': 'off',
 };
 
 if (process.env.STAGE !== 'production') {
