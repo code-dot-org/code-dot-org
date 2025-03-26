c7211c5c2321b15feb3f21487e79a723fad36bff
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-03-26 18:58:48 +0200

    CMS-426: Enable CORS to `/api/v1/users/current` from `code.org`

--- dashboard/app/controllers/api/v1/users_controller.rb
+++ dashboard/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 require 'cdo/firehose'
 
 class Api::V1::UsersController < Api::V1::JSONApiController
+  before_action :allow_cdo_cors, only: %i[current]
   before_action :load_user
   skip_before_action :verify_authenticity_token
   skip_before_action :load_user, only: [:current, :netsim_signed_in, :post_sort_by_family_name, :cached_page_auth_redirect, :post_show_progress_table_v2, :post_ai_rubrics_disabled, :post_ai_differentiation_enabled, :post_has_seen_ai_assessments_announcement, :post_date_progress_table_invitation_last_delayed, :post_has_seen_progress_table_v2_invitation, :get_current_permissions, :post_disable_lti_roster_sync, :update_ai_tutor_access, :set_seen_ta_scores, :post_has_completed_ai_differentiation_welcome]
