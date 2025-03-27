eb1dcf3353df3bf09493e056db0340ba60d01bc0
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-03-27 13:01:31 -0700

    add all the props

--- frontend/apps/marketing/src/components/actionBlocks/defaultActionBlock/ActionBlock.tsx
+++ frontend/apps/marketing/src/components/actionBlocks/defaultActionBlock/ActionBlock.tsx
@@ -1,66 +1,71 @@
-import {EntryFields} from 'contentful';
+import {BaseEntry, EntryFields} from 'contentful';
 
-import DSCOActionBlock from '@code-dot-org/component-library/actionBlock';
+import DSCOActionBlock, {
+  ActionBlockProps,
+} from '@code-dot-org/component-library/actionBlock';
 
-export type ActionBlockContentfulProps = {
-  primaryButton: {
+export type ActionBlockContentfulProps = ActionBlockProps & {
+  overline: EntryFields.Text;
+  title: EntryFields.Text;
+  description: EntryFields.Text;
+  image: BaseEntry & {
+    fields: {
+      description?: string;
+      title?: string;
+      file: {url: string};
+    };
+  };
+  primaryButton: BaseEntry & {
     fields: {
       label: EntryFields.Text;
       primaryTarget: EntryFields.Text;
       ariaLabel: EntryFields.Text;
     };
   };
-  secondaryButton: {
+  secondaryButton: BaseEntry & {
     fields: {
       label: EntryFields.Text;
       primaryTarget: EntryFields.Text;
       ariaLabel: EntryFields.Text;
     };
   };
+  background: EntryFields.Text;
 };
 
 const ActionBlock: React.FC<ActionBlockContentfulProps> = ({
+  overline,
+  title,
+  description,
+  image,
   primaryButton,
   secondaryButton,
-  ...props
-}) => {
-  const actionBlockProps = {
-    primaryButton: {
-      label: primaryButton?.fields?.label,
-      primaryTarget: primaryButton?.fields?.primaryTarget,
-      ariaLabel: primaryButton?.fields?.ariaLabel,
-    },
-    secondaryButton: {
-      label: secondaryButton?.fields?.label,
-      primaryTarget: secondaryButton?.fields?.primaryTarget,
-      ariaLabel: secondaryButton?.fields?.ariaLabel,
-    },
-  };
-
-  return (
-    <DSCOActionBlock
-      {...props}
-      primaryButton={
-        actionBlockProps.primaryButton.label
-          ? {
-              text: actionBlockProps.primaryButton.label || 'Primary button',
-              href: actionBlockProps.primaryButton.primaryTarget || '#',
-              ariaLabel: actionBlockProps.primaryButton.ariaLabel || '',
-            }
-          : undefined
-      }
-      secondaryButton={
-        actionBlockProps.secondaryButton.label
-          ? {
-              text:
-                actionBlockProps.secondaryButton.label || 'Secondary button',
-              href: actionBlockProps.secondaryButton.primaryTarget || '#',
-              ariaLabel: actionBlockProps.secondaryButton.ariaLabel || '',
-            }
-          : undefined
-      }
-    />
-  );
-};
+  background,
+}) => (
+  <DSCOActionBlock
+    overline={overline}
+    title={title}
+    description={description}
+    image={image}
+    primaryButton={
+      primaryButton?.fields?.label
+        ? {
+            text: primaryButton.fields.label,
+            href: primaryButton.fields.primaryTarget || '#',
+            ariaLabel: primaryButton.fields.ariaLabel || '',
+          }
+        : undefined
+    }
+    secondaryButton={
+      secondaryButton?.fields?.label
+        ? {
+            text: secondaryButton.fields.label,
+            href: secondaryButton.fields.primaryTarget || '#',
+            ariaLabel: secondaryButton.fields.ariaLabel || '',
+          }
+        : undefined
+    }
+    background={background}
+  />
+);
 
 export default ActionBlock;
