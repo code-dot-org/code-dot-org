1d9c8dadaa275c716450abc23faf02a657c6bf81
Carl Reiner <carl.reiner@code.org>
2025-04-02 16:18:47 -0700

    handle more AZs in loop

--- aws/cloudformation/standalone/marketing/deployment/config.rb
+++ aws/cloudformation/standalone/marketing/deployment/config.rb
@@ -2,7 +2,29 @@ require "ostruct"
 
 module Config
   AVAILABILITY_ZONES = [
-    'us-west-2a',
-    'us-west-2b'
+    'us-east-1a',
+    'us-east-1b',
+    'us-east-1c',
+    'us-east-1d',
+    'us-east-1e',
+    'us-east-1f'
+  ].freeze
+
+  PUBLIC_SUBNETS = [
+    '10.0.0.0/20',
+    '10.0.16.0/20',
+    '10.0.32.0/20',
+    '10.0.48.0/20',
+    '10.0.64.0/20',
+    '10.0.80.0/20'
+  ].freeze
+
+  PRIVATE_SUBNETS = [
+    '10.0.128.0/20',
+    '10.0.144.0/20',
+    '10.0.160.0/20',
+    '10.0.176.0/20',
+    '10.0.192.0/20',
+    '10.0.208.0/20'
   ].freeze
 end
