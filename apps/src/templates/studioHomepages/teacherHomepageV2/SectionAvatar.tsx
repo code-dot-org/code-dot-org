e8e1b31f03e5c154131e77913ce3022a3953d956
Mark Barnes <mark.barnes@code.org>
2025-04-02 14:37:41 -0500

    add component and base styling

---
+++ apps/src/templates/studioHomepages/teacherHomepageV2/SectionAvatar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './teacherHomepage.module.scss';
+
+const SectionAvatar: React.FC = () => {
+  return <div className={styles.sectionAvatar}>{'test'}</div>;
+};
+
+export default SectionAvatar;
