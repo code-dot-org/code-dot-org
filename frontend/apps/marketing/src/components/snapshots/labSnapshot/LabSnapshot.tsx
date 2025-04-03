e96ee44cbf0fd57d7c544fa471d6cf885f6d698c
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-04-03 23:13:06 +0300

    CMS-495: Remove debug

--- frontend/apps/marketing/src/components/snapshots/labSnapshot/LabSnapshot.tsx
+++ frontend/apps/marketing/src/components/snapshots/labSnapshot/LabSnapshot.tsx
@@ -26,8 +26,6 @@ const LabSnapshot: React.FunctionComponent<LabSnapshotProps> = ({
   languages,
   ...props
 }) => {
-  console.log('accessibility', accessibility);
-
   const initItem = (
     label: string,
     iconName: string,
