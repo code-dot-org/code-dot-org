a90a5a3bafecfb61b07545a37b9129821799f309
Liam Frye-Mason <liam.fryemason@code.org>
2025-04-03 09:04:53 -0700

    Fix test

--- apps/src/templates/studioHomepages/teacherHomepageV2/SkeletonTeacherPromo.tsx
+++ apps/src/templates/studioHomepages/teacherHomepageV2/SkeletonTeacherPromo.tsx
@@ -7,12 +7,13 @@ import classNames from 'classnames';
 import React from 'react';
 
 import Skeleton from '@cdo/apps/util/loadingSkeleton';
+import i18n from '@cdo/locale';
 
 import styles from './teacherHomepage.module.scss';
 
 export const SkeletonTeacherPromo: React.FC = () => {
   return (
-    <div className={styles.promotion}>
+    <div className={styles.promotion} aria-label={i18n.loading()}>
       <OverlineTwoText className={styles.promotionType}>
         <Skeleton />
       </OverlineTwoText>
