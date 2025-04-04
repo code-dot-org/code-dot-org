eea140e861acc4cb5eee9b970b4c05c8acc9caeb
alex-m-brown <alex.brown@code.org>
2025-04-04 09:49:40 -0400

    update section creation in tests

--- dashboard/app/controllers/test_controller.rb
+++ dashboard/app/controllers/test_controller.rb
@@ -75,7 +75,10 @@ class TestController < ApplicationController
     return unless (user = current_user)
     script = Unit.find_by_name(params.require(:script_name))
 
-    section = Section.create!(name: "New Section", user: user, script: script, participant_type: Curriculum::SharedCourseConstants::PARTICIPANT_AUDIENCE.student)
+    section = script.unit_group&.single_unit_course? ?
+                Section.create!(name: "New Section", user: user, script: script, course_id: script.unit_group.id, participant_type: Curriculum::SharedCourseConstants::PARTICIPANT_AUDIENCE.student) :
+                Section.create!(name: "New Section", user: user, script: script, participant_type: Curriculum::SharedCourseConstants::PARTICIPANT_AUDIENCE.student)
+
     render json: {section_code: section.code}
   end
 
@@ -135,7 +138,7 @@ class TestController < ApplicationController
     end
 
     # Need to also assign the course if the script is a part of a single-unit course
-    section = script.unit_group.single_unit_course? ?
+    section = script.unit_group&.single_unit_course? ?
                 Section.create(name: "New Section", user: teacher_user, script: script, course_id: script.unit_group.id, participant_type: Curriculum::SharedCourseConstants::PARTICIPANT_AUDIENCE.student) :
                 Section.create(name: "New Section", user: teacher_user, script: script, participant_type: Curriculum::SharedCourseConstants::PARTICIPANT_AUDIENCE.student)
     section.students << user
