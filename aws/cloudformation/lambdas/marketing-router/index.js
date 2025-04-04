bafa72ea9f71172d6d0dbbae9c6a5cf4510a0404
Carl Reiner <carl.reiner@code.org>
2025-04-04 15:26:11 -0700

    add try-catch to ensure the request is passed through

--- aws/cloudformation/lambdas/marketing-router/index.js
+++ aws/cloudformation/lambdas/marketing-router/index.js
@@ -10,27 +10,34 @@ const marketingPaths = {
 }
 
 module.exports.handler = (event, context, callback) => {
-  const request = event?.Records?.[0]?.cf?.request;
-  const uri = request?.uri;
+  try {
+    const request = event?.Records?.[0]?.cf?.request;
+    const uri = request?.uri;
+  
+    // Set CMS origin if the requested path matches
+    if (marketingPaths[uri]) {
+      request.origin = {
+        custom: {
+          domainName: marketingDomain,
+          port: 443,
+          protocol: 'https',
+          path: '',
+          sslProtocols: ['TLSv1.2'],
+          readTimeout: 30,
+          keepaliveTimeout: 5,
+          customHeaders: {},
+        },
+      };
+  
+      // Update the Host header to match the new origin
+      request.headers['host'] = [{ key: 'Host', value: marketingDomain }];
+    }
+  
+    callback(null, request);
+  } catch (error) {
+    console.error('Marketing router lambda error:', error);
 
-  // Set CMS origin if the requested path matches
-  if (marketingPaths[uri]) {
-    request.origin = {
-      custom: {
-        domainName: marketingDomain,
-        port: 443,
-        protocol: 'https',
-        path: '',
-        sslProtocols: ['TLSv1.2'],
-        readTimeout: 30,
-        keepaliveTimeout: 5,
-        customHeaders: {},
-      },
-    };
-
-    // Update the Host header to match the new origin
-    request.headers['host'] = [{ key: 'Host', value: marketingDomain }];
+    // If an error occurs, don't block. Pass through the request.
+    callback(null, request);
   }
-
-  callback(null, request);
 };
