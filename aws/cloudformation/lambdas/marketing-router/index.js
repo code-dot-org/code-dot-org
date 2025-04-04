e5a849fa3da330917c258adc207d428e255097c3
Carl Reiner <carl.reiner@code.org>
2025-04-04 08:57:39 -0700

    remove lambda environment variable

--- aws/cloudformation/lambdas/marketing-router/index.js
+++ aws/cloudformation/lambdas/marketing-router/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const { MARKETING_DOMAIN } = process.env;
+const MARKETING_DOMAINS = {
+  'marketing-test': 'dev.marketing.dev-code.org',
+};
 
 const marketingPaths = {
   // Add key-value pairs for each path that should be served by the CMS
@@ -9,13 +11,15 @@ const marketingPaths = {
 
 module.exports.handler = (event, context, callback) => {
   const request = event?.Records?.[0]?.cf?.request;
+  const requestEnvHeader = request?.origin?.custom?.customHeaders?.['x-cdo-env']?.[0]?.value;
+  const marketingDomain = MARKETING_DOMAINS[requestEnvHeader];
   const uri = request?.uri;
 
   // Set CMS origin if the requested path matches
   if (marketingPaths[uri]) {
     request.origin = {
       custom: {
-        domainName: MARKETING_DOMAIN,
+        domainName: marketingDomain,
         port: 443,
         protocol: 'https',
         path: '',
@@ -27,7 +31,7 @@ module.exports.handler = (event, context, callback) => {
     };
 
     // Update the Host header to match the new origin
-    request.headers['host'] = [{ key: 'Host', value: MARKETING_DOMAIN }];
+    request.headers['host'] = [{ key: 'Host', value: marketingDomain }];
   }
 
   callback(null, request);
