e9fccc0bfb72366caf252178f3597d709422a700
alex-m-brown <alex.brown@code.org>
2025-04-04 16:52:00 -0400

    only delete units/courses in first migration

--- dashboard/db/migrate/20250403185752_cleanup_after_standalone_unit_migration.rb
+++ dashboard/db/migrate/20250403185752_cleanup_after_standalone_unit_migration.rb
@@ -1,9 +1,5 @@
 class CleanupAfterStandaloneUnitMigration < ActiveRecord::Migration[6.1]
   def up
-    file_system_changes = Rails.application.config.levelbuilder_mode
-    migrated_units = []
-    ENV['MIGRATE_STANDALONE_UNITS'] = 'true'
-
     # We need to delete the following units
     units_to_delete = %w[computer-systems-and-devices-2024 networks-and-the-internet-2024 amys-playground csa-frq-practice hello-world-food-one hoc-music-ai-pilot]
     units_to_delete.each do |unit_name|
@@ -36,10 +32,9 @@ class CleanupAfterStandaloneUnitMigration < ActiveRecord::Migration[6.1]
       end
     end
 
-    # We need to delete these unit groups and migrate the associated units
-    unit_groups_to_delete = {"sandbox-pl-cv" => "sandbox-pl-cv",
-                             "introduction-to-programming-in-music-lab-for-elementary-2025" => "introduction-to-programming-in-music-lab-for-elementary"}
-    unit_groups_to_delete.each do |unit_group_name, unit_name|
+    # We need to delete these unit groups
+    unit_groups_to_delete = %w[sandbox-pl-cv introduction-to-programming-in-music-lab-for-elementary-2025]
+    unit_groups_to_delete.each do |unit_group_name|
       unit_group = UnitGroup.find_by(name: unit_group_name)
       if unit_group.nil?
         print_message "UnitGroup not found: #{unit_group_name}"
@@ -71,30 +66,7 @@ class CleanupAfterStandaloneUnitMigration < ActiveRecord::Migration[6.1]
           print_message "ERROR: Caught an exception while deleting UnitGroup #{unit_name}: #{exception.class} - #{exception.message}"
         end
       end
-
-      unit = Unit.find_by(name: unit_name)
-      if unit.nil?
-        print_message "Unit not found: #{unit_name}"
-        next
-      end
-
-      unless unit.is_course?
-        print_message "Unit [#{unit_name}] is not a standalone unit. No migration needed."
-        next
-      end
-
-      begin
-        result = Services::StandaloneUnitMigrator.call(unit, file_system_changes: file_system_changes)
-      rescue Exception => exception
-        filtered_backtrace = exception.backtrace.select {|line| line.include?("standalone_unit_migrator.rb")}
-        print_message "ERROR: Caught an exception while migrating #{unit.name}: #{exception.class} - #{exception.message}\n\tBacktrace: #{filtered_backtrace}"
-      end
-      migrated_units << unit.name if result
     end
-    ENV.delete('MIGRATE_STANDALONE_UNITS')
-    print_message "Units Migrated: #{migrated_units.count}"
-    message = Unit.count(&:is_course?) == 0 ? "There are no more standalone-units!" : "There are still unmigrated units! Unmigrated Units: #{Unit.count(&:is_course?)}"
-    print_message message
   end
 
   def down
