59f54fdbaf1fdd78772261f0177219b7ddcc5c7b
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-04-04 17:23:06 -0700

    add required mysql fix

--- bin/oneoff/update_ubuntu/02-postupdate.sh
+++ bin/oneoff/update_ubuntu/02-postupdate.sh
@@ -6,6 +6,14 @@ fi
 
 set -e
 
+# On an adhoc, the mysql 'root' user's password got reset, so we need to
+# reassign it to the desired value.
+# TODO: figure out what we should do here for our build pipeline servers
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';";
+
+# Reinstall rmagick to resolve the error:
+#   This installation of RMagick was configured with ImageMagick 6.9.10 but
+#   ImageMagick 6.9.11-60 is in use.
 bundle exec gem uninstall rmagick;
 bundle install;
 
