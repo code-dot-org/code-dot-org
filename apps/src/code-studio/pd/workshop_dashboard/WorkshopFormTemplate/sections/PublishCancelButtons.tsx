d01a1ae33e2fbae7cc9f5c61df10b56d5454e275
Ryan Driscoll <driscollrp@gmail.com>
2025-04-07 15:17:26 -0600

    feat: style publish cancel button row

--- apps/src/code-studio/pd/workshop_dashboard/WorkshopFormTemplate/sections/PublishCancelButtons.tsx
+++ apps/src/code-studio/pd/workshop_dashboard/WorkshopFormTemplate/sections/PublishCancelButtons.tsx
@@ -1,15 +1,18 @@
 import {Button} from '@code-dot-org/component-library/button';
+import classNames from 'classnames';
 import React, {FC, memo} from 'react';
 
 import {PublishCancelButtonsProps} from '../types';
+
+import commonStyles from '../styles.module.scss';
 
 export const PublishCancelButtons: FC<PublishCancelButtonsProps> = ({
   publish,
   cancel,
 }) => {
   return (
-    <div>
-      <Button onClick={publish} text="Publish" />
+    <div className={classNames(commonStyles.row, commonStyles.formSubmitRow)}>
+      <Button onClick={() => publish()} text="Publish" />
       <Button
         onClick={cancel}
         text="Cancel and exit"
