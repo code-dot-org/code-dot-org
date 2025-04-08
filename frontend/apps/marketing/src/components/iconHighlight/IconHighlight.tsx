c1e73d2541bbfbc30f6a2a9464011d48274e13b7
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-04-08 20:47:43 +0300

    feat(IconHighlight): add `Single Link` field

--- frontend/apps/marketing/src/components/iconHighlight/IconHighlight.tsx
+++ frontend/apps/marketing/src/components/iconHighlight/IconHighlight.tsx
@@ -3,47 +3,41 @@ import {useMemo} from 'react';
 
 import IconHighlight, {
   IconHighlightLinkProps,
-  IconHighlightProps,
 } from '@code-dot-org/component-library/cms/iconHighlight';
 
 import {fontAwesomeV6BrandIconsMap} from '@/components/common/constants';
+import type {LinkEntry} from '@/contentful/types/entries';
 
-export type IconHighlightContentfulLinkEntry = {
-  sys: {
-    id: EntryFields.Text;
-  };
-  fields: {
-    label: EntryFields.Text;
-    primaryTarget: EntryFields.Text;
-    ariaLabel?: EntryFields.Text;
-    isThisAnExternalLink?: EntryFields.Boolean;
-  };
-};
-
-export interface IconHighlightContentfulProps extends IconHighlightProps {
+export interface IconHighlightContentfulProps {
+  heading: EntryFields.Text;
+  text: EntryFields.Text;
   iconName: EntryFields.Text;
-  linkEntries?: IconHighlightContentfulLinkEntry[];
+  linkEntry?: LinkEntry;
+  linkEntries?: LinkEntry[];
 }
 
 const IconHighlightContentful: React.FunctionComponent<
   IconHighlightContentfulProps
-> = ({iconName, linkEntries = [], ...props}) => {
-  const links: IconHighlightLinkProps[] = useMemo(
-    () =>
-      linkEntries.filter(Boolean).map(linkEntry => ({
-        key: linkEntry.sys.id,
-        text: linkEntry.fields.label,
-        href: linkEntry.fields.primaryTarget,
-        external: linkEntry.fields.isThisAnExternalLink,
-        target: linkEntry.fields.isThisAnExternalLink ? '_blank' : undefined,
-        'aria-label': linkEntry.fields.ariaLabel || undefined,
-      })),
-    [linkEntries],
-  );
+> = ({heading, text, iconName, linkEntry, linkEntries = []}) => {
+  const links: IconHighlightLinkProps[] = useMemo(() => {
+    const links: LinkEntry[] = linkEntries.filter(Boolean);
+
+    if (!links.length && linkEntry) links.push(linkEntry);
+
+    return links.map(linkEntry => ({
+      key: linkEntry.sys.id,
+      text: linkEntry.fields.label,
+      href: linkEntry.fields.primaryTarget,
+      external: linkEntry.fields.isThisAnExternalLink,
+      target: linkEntry.fields.isThisAnExternalLink ? '_blank' : undefined,
+      'aria-label': linkEntry.fields.ariaLabel || undefined,
+    }));
+  }, [linkEntry, linkEntries]);
 
   return (
     <IconHighlight
-      {...props}
+      heading={heading}
+      text={text}
       links={links}
       icon={{
         iconName,
