2e6f2802f745e28607bb4fed5615c4e6636fe6b3
kobryan0619 <kaitie.obryan@gmail.com>
2025-04-08 12:04:11 -0500

    add skill table

---
+++ dashboard/app/models/skill.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: skills
+#
+#  id                  :bigint           not null, primary key
+#  description         :string(255)      not null
+#  evaluation_criteria :text(65535)
+#  concept             :string(255)
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+class Skill < ApplicationRecord
+  validates :description, presence: true
+end
