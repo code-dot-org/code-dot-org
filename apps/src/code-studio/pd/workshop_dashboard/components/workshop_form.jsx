515a81f94ab55d215edf44a4312a7a6d02cd9c3f
Ryan Driscoll <driscollrp@gmail.com>
2025-04-09 09:31:03 -0600

    feat: use workshop configs to validate workshops, with exception for archived courses and legacy workshops created/updated with the old form

--- apps/src/code-studio/pd/workshop_dashboard/components/workshop_form.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/components/workshop_form.jsx
@@ -847,6 +847,7 @@ export class WorkshopForm extends React.Component {
       course_offerings: this.state.course_offerings,
       participant_group_type: this.state.participant_group_type,
       time_zone: this.workshopTimezone,
+      legacy: true,
     };
 
     if (this.state.organizer) {
