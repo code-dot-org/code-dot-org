ecadaa093c319b5c4e92aa97167eab35fca30cf0
Molly Moen <molly@code.org>
2025-04-09 15:06:25 -0700

    first attempt

--- python/pythonlab/unittest_runner/unittest_runner/validation_runner.py
+++ python/pythonlab/unittest_runner/unittest_runner/validation_runner.py
@@ -22,11 +22,13 @@ class ValidationTestResult(TextTestResult):
       self.simplified_results.append({'name': self.getDescription(test), 'result': "PASS"})
 
   def addError(self, test, err):
-      super(ValidationTestResult, self).addError(test, err)
+      self.errors.append(test, self.parseError(err))
+      self.write_status(test, "ERROR", "E")
       self.simplified_results.append({'name': self.getDescription(test), 'result': "ERROR"})
 
   def addFailure(self, test, err):
-      super(ValidationTestResult, self).addFailure(test, err)
+      self.failures.append(test, self.parseError(err))
+      self.write_status(test, "FAILURE", "F")
       self.simplified_results.append({'name': self.getDescription(test), 'result': "FAIL"})
 
   def addSkip(self, test, reason):
@@ -34,9 +36,21 @@ class ValidationTestResult(TextTestResult):
       self.simplified_results.append({'name': self.getDescription(test), 'result': "SKIP"})
 
   def addExpectedFailure(self, test, err):
-      super(ValidationTestResult, self).addExpectedFailure(test, err)
+      self.expectedFailures.append(test, self.parseError(err))
+      self.write_status(test, "expected failure", "x")
       self.simplified_results.append({'name': self.getDescription(test), 'result': "EXPECTED_FAILURE"})
 
   def addUnexpectedSuccess(self, test):
       super(ValidationTestResult, self).addUnexpectedSuccess(test)
       self.simplified_results.append({'name': self.getDescription(test), 'result': "UNEXPECTED_SUCCESS"})
+
+  def parseError(self, err):
+     exception_type, value, traceback = err
+     return value
+  
+  def write_status(self, test, status_long, status_short):
+    if self.showAll:
+      self._write_status(test, status_long)
+    elif self.dots:
+      self.stream.write(status_short)
+      self.stream.flush()
