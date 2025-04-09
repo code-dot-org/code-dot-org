fa93bd6c470595edf7ffc714aa54f0df5800b5de
Ryan Driscoll <driscollrp@gmail.com>
2025-04-09 14:41:23 -0600

    feat: remove workshop form from DetailsPanel and remove tests on the form

--- apps/test/unit/code-studio/pd/workshop_dashboard/DetailsPanelTest.jsx
+++ apps/test/unit/code-studio/pd/workshop_dashboard/DetailsPanelTest.jsx
@@ -8,13 +8,6 @@ import {States} from '@cdo/apps/generated/pd/sharedWorkshopConstants';
 import './workshopFactory';
 
 describe('DetailsPanel', () => {
-  it('shows a readonly WorkshopForm by default', () => {
-    const wrapper = shallowDetailsPanel();
-    const workshopForm = wrapper.find('Connect(WorkshopForm)');
-    assert.isOk(workshopForm, 'WorkshopForm was rendered');
-    assert.isTrue(workshopForm.prop('readOnly'), 'WorkshopForm is readonly');
-  });
-
   it('shows an edit button when the workshop is in "Not Started" state', () => {
     const wrapper = shallowDetailsPanel({
       workshop: Factory.build('workshop', {state: 'Not Started'}),
@@ -37,23 +30,6 @@ describe('DetailsPanel', () => {
         .filterWhere(n => 'Edit' === n.text());
       assert.isFalse(editButton.exists(), 'Edit button was not rendered');
     });
-  });
-
-  it('shows an editable WorkshopForm in edit mode', () => {
-    const wrapper = shallowDetailsPanel({
-      view: 'edit',
-    });
-    const workshopForm = wrapper.find('Connect(WorkshopForm)');
-    assert.isOk(workshopForm, 'WorkshopForm was rendered');
-    assert.isFalse(!!workshopForm.prop('readOnly'), 'WorkshopForm is editable');
-  });
-
-  it('edit mode includes a save button in the header', () => {
-    const wrapper = shallowDetailsPanel({
-      view: 'edit',
-    });
-    const headerSaveButton = findHeaderButton(wrapper, 'Save');
-    assert.isTrue(headerSaveButton.exists(), 'Save button is present');
   });
 
   it('Admin has an edit button in all workshop states', () => {
