2459f60707110a22cedf1df7650e1812436da291
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-04-09 18:25:17 -0700

    update tests

--- dashboard/test/controllers/dynamic_config_controller_test.rb
+++ dashboard/test/controllers/dynamic_config_controller_test.rb
@@ -1,56 +1,23 @@
 require 'test_helper'
 
 class DynamicConfigControllerTest < ActionController::TestCase
-  GET_ROUTES = %i[show gatekeeper_show].freeze
-  POST_ROUTES = %i[gatekeeper_set gatekeeper_delete].freeze
-  ALL_ROUTES = (GET_ROUTES + POST_ROUTES).freeze
-
-  # The routes currently defined by this controller are pretty consistently
-  # structured, so it's possible to define a basic helper that naively executes
-  # a "default" HTTP interaction for a given route.
-  #
-  # This can be used in individual tests to easily verify functionality for
-  # every single route as a bulk operation.
-  def perform_default_http_interaction_for_route(route)
-    case route.to_sym
-    when *GET_ROUTES
-      get route
-    when *POST_ROUTES
-      post route, params: {feature: 'test access', where: '{}', value: 'true'}
-    else
-      raise "Don't know what the default HTTP interaction is for #{route.inspect}"
-    end
-  end
-
   test 'inaccessible by non-admins' do
     # Including both anonymous users
-    ALL_ROUTES.each do |route|
-      perform_default_http_interaction_for_route(route)
-      assert_response :redirect
-      assert_redirected_to :new_user_session
-    end
+    get :show
+    assert_response :redirect
+    assert_redirected_to :new_user_session
 
     # And signed-in users without elevated permissions
     sign_in(create(:user))
-    ALL_ROUTES.each do |route|
-      perform_default_http_interaction_for_route(route)
-      assert_response :forbidden
-    end
+    get :show
+    assert_response :forbidden
   end
 
   test 'accessible by admins' do
     sign_in(create(:admin))
 
-    GET_ROUTES.each do |route|
-      perform_default_http_interaction_for_route(route)
-      assert_response :success
-    end
-
-    POST_ROUTES.each do |route|
-      perform_default_http_interaction_for_route(route)
-      assert_response :redirect
-      assert_redirected_to action: 'gatekeeper_show', params: {feature: 'test access'}
-    end
+    get :show
+    assert_response :success
   end
 
   test 'can render dynamic config values' do
@@ -66,24 +33,5 @@ class DynamicConfigControllerTest < ActionController::TestCase
     assert_response :success
     assert_includes @response.body, "# Gatekeeper Config\n ---\ntest gatekeeper:"
     assert_includes @response.body, "# DCDO Config\n---\ntest dcdo: baz"
-  end
-
-  test 'can update gatekeeper values' do
-    sign_in(create(:admin))
-    refute Gatekeeper.allows('test update', where: {foo: 'bar'})
-    post :gatekeeper_set, params: {feature: 'test update', where: {foo: 'bar'}.to_json, value: 'true'}
-    assert_response :redirect # this endpoint redirects on successful update
-    assert_redirected_to action: 'gatekeeper_show', params: {feature: 'test update'}
-    assert Gatekeeper.allows('test update', where: {foo: 'bar'})
-  end
-
-  test 'can delete gatekeeper values' do
-    sign_in(create(:admin))
-    Gatekeeper.set('test delete', where: {foo: 'bar'}, value: true)
-    assert Gatekeeper.allows('test delete', where: {foo: 'bar'})
-    post :gatekeeper_delete, params: {feature: 'test delete', where: {foo: 'bar'}.to_json}
-    assert_response :redirect # this endpoint redirects on successful deletion
-    assert_redirected_to action: 'gatekeeper_show', params: {feature: 'test delete'}
-    refute Gatekeeper.allows('test delete', where: {foo: 'bar'})
   end
 end
---
+++ dashboard/test/controllers/gatekeeper_controller_test.rb
@@ -0,0 +1,72 @@
+require 'test_helper'
+
+class GatekeeperControllerTest < ActionController::TestCase
+  ACTIONS = [:show, :update, :destroy]
+
+  # The routes currently defined by this controller are pretty consistently
+  # structured, so it's possible to define a basic helper that naively executes
+  # a "default" HTTP interaction for a given action.
+  #
+  # This can be used in individual tests to easily verify functionality for
+  # every single route as a bulk operation.
+  def request_default(action)
+    case action.to_sym
+    when :show
+      get :show
+    when :update
+      patch :update, params: {feature: 'test access', where: {foo: 'bar'}.to_json, value: 'true'}
+    when :destroy
+      delete :destroy, params: {feature: 'test access', where: {foo: 'bar'}.to_json}
+    else
+      raise "Don't know what the default HTTP interaction is for #{action.inspect}"
+    end
+  end
+
+  test 'inaccessible by non-admins' do
+    # Including both anonymous users
+    ACTIONS.each do |action|
+      request_default(action)
+      assert_response(:redirect)
+      assert_redirected_to(:new_user_session)
+    end
+
+    # And signed-in users without elevated permissions
+    sign_in(create(:user))
+    ACTIONS.each do |action|
+      request_default(action)
+      assert_response(:forbidden)
+    end
+  end
+
+  test 'accessible by admins' do
+    sign_in(create(:admin))
+
+    request_default(:show)
+    assert_response(:success)
+
+    [:update, :destroy].each do |action|
+      request_default(action)
+      assert_response :redirect
+      assert_redirected_to action: 'show', params: {feature: 'test access'}
+    end
+  end
+
+  test 'can update gatekeeper values' do
+    sign_in(create(:admin))
+    refute Gatekeeper.allows('test access', where: {foo: 'bar'})
+    request_default(:update)
+    assert_response :redirect # this endpoint redirects on successful update
+    assert_redirected_to action: 'show', params: {feature: 'test access'}
+    assert Gatekeeper.allows('test access', where: {foo: 'bar'})
+  end
+
+  test 'can delete gatekeeper values' do
+    sign_in(create(:admin))
+    Gatekeeper.set('test access', where: {foo: 'bar'}, value: true)
+    assert Gatekeeper.allows('test access', where: {foo: 'bar'})
+    request_default(:destroy)
+    assert_response :redirect # this endpoint redirects on successful deletion
+    assert_redirected_to action: 'show', params: {feature: 'test access'}
+    refute Gatekeeper.allows('test access', where: {foo: 'bar'})
+  end
+end
