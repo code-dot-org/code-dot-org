4239da90a0b828ef9fd299a80874c1e6d6ecd4d7
Alice Fisher <alice.fisher@code.org>
2025-04-10 22:32:48 +0000

    update comments again

--- apps/src/lib/util/UserPreferences.js
+++ apps/src/lib/util/UserPreferences.js
@@ -113,8 +113,7 @@ export default class UserPreferences extends Record({userId: 'me'}) {
 
   /**
    * Fetch the user's console font size selection.
-   * @param {string} fontSize
-   * @param {string} appType
+   * @param {string} appName
    */
   async getConsoleFontSize(appName) {
     const consoleFontSizeResponse = await HttpClient.fetchJson(
@@ -127,8 +126,7 @@ export default class UserPreferences extends Record({userId: 'me'}) {
 
   /**
    * Fetch the user's editor font size selection.
-   * @param {string} editorFontSize
-   * @param {string} appType
+   * @param {string} appName
    */
   async getEditorFontSize(appName) {
     const editorFontSizeResponse = await HttpClient.fetchJson(
