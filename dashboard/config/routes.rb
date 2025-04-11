587513b65fc0bb49d1cc2754ae1f5d437d630c51
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-04-11 11:36:14 -0700

    Update `/experiments` Routes to Use Resourceful Routing (#65164)
    * Update `/experiments` Routes to Use Resourceful Routing
    
    Part of an ongoing initiative within the engineering standards working group.
    
    https://docs.google.com/spreadsheets/d/1of9F1_I3Q_U1JVeceC7YRMN8U68lBOycwtUxV-97kfc/edit?gid=0#gid=0
    
    * minor cleanup

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -741,8 +741,10 @@ Dashboard::Application.routes.draw do
     post '/sms/send', to: 'sms#send_to_phone', as: 'send_to_phone'
 
     # Experiments are get requests so that a user can click on a link to join or leave an experiment
-    get '/experiments/set_single_user_experiment/:experiment_name', to: 'experiments#set_single_user_experiment'
-    get '/experiments/disable_single_user_experiment/:experiment_name', to: 'experiments#disable_single_user_experiment'
+    resource :experiments, only: [] do
+      get 'set_single_user_experiment/:experiment_name', action: :set_single_user_experiment
+      get 'disable_single_user_experiment/:experiment_name', action: :disable_single_user_experiment
+    end
 
     get '/peer_reviews/dashboard', to: 'peer_reviews#dashboard'
     resources :peer_reviews
