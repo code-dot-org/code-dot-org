767c910957381f7989f6a55a373e2d47b19986f9
kobryan0619 <kaitie.obryan@gmail.com>
2025-04-16 06:37:54 -0500

    add comment

--- dashboard/app/models/skill.rb
+++ dashboard/app/models/skill.rb
@@ -9,6 +9,9 @@
 #  created_at          :datetime         not null
 #  updated_at          :datetime         not null
 #
+
+# Note: "concept" already exists as connected to a video "concept" and is also used in some CSF materials in our codebase
+# the "concept" in this skill model is not the same as the "concept" in the video model.
 class Skill < ApplicationRecord
   validates :description, presence: true
 end
