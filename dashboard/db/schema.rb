0c42a058f5f9c587b2c2ab0977e27f909066997c
kobryan0619 <kaitie.obryan@gmail.com>
2025-04-17 11:36:52 -0500

    Undo conflict

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_04_11_155821) do
+ActiveRecord::Schema.define(version: 2025_04_08_164816) do
 
   create_table "activities", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
