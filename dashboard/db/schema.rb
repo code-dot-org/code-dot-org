503b480ee53d471d6c7e999821a2f61fbcbec6da
alex-m-brown <alex.brown@code.org>
2025-04-17 11:20:33 -0400

    update migration

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_04_15_155719) do
+ActiveRecord::Schema.define(version: 2025_04_17_151841) do
 
   create_table "activities", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
