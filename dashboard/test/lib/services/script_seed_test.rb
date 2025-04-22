f2c2cfdf1ca883e0be787f3855f7328d7b7a6910
alex-m-brown <alex.brown@code.org>
2025-04-22 10:41:05 -0400

    fix script seed test

--- dashboard/test/lib/services/script_seed_test.rb
+++ dashboard/test/lib/services/script_seed_test.rb
@@ -160,20 +160,23 @@ module Services
       script.student_resources.destroy_all
       script.freeze
       script.unit_group.course_version.resources.destroy_all
+      script.unit_group.course_version.vocabularies.destroy_all
       expected_counts = get_counts
 
       # destroy the script and its unit group, so that no course version will
       # be available during seed.
       script_to_destroy = Unit.find(script.id)
+      unit_group_to_destroy = script_to_destroy.unit_group
       script_to_destroy.unit_group.course_version.destroy!
-      script_to_destroy.unit_group.destroy!
       script_to_destroy.destroy!
+      unit_group_to_destroy.destroy!
 
       ScriptSeed.seed_from_json(json)
 
       assert_equal expected_counts, get_counts
       script_after_seed = Unit.with_seed_models.find_by!(name: script.name)
-      assert_script_trees_equal(script, script_after_seed)
+      # original_unit_group_id is set when the unit_group is seeded, unless a script already exists with an original_unit_group_id
+      assert_script_trees_equal(script, script_after_seed, ['original_unit_group_id'])
     end
 
     test 'seed with no changes is no-op' do
@@ -1217,6 +1220,19 @@ module Services
       assert_equal serialized['serialized_at'].to_i, Time.parse(script.seeded_from).to_i
     end
 
+    test 'import_script does not overwrite original_unit_group_id' do
+      script = create_script_tree(with_unit_group: true)
+      original_unit_group_id = script.unit_group.id
+      assert_equal original_unit_group_id, script.original_unit_group_id
+
+      serialized = ScriptSeed::ScriptSerializer.new(script, scope: {seed_context: {}}).as_json.stringify_keys
+      assert serialized['serialized_at'].present?
+
+      ScriptSeed.import_script(serialized)
+      script.reload
+      assert_equal original_unit_group_id, script.original_unit_group_id
+    end
+
     test 'seed rejects bad plc module name' do
       unit = create :script, :with_levels
       unit.lesson_groups.first.update!(key: 'bad_module_type',  display_name: "Bad Module Type")
@@ -1270,11 +1286,11 @@ module Services
       ].map {|c| [c.name, c.count]}.to_h
     end
 
-    def assert_script_trees_equal(s1, s2)
+    def assert_script_trees_equal(s1, s2, script_excludes = [])
       # Make sure the scripts and their associations are already in memory,
       # because fetching data from the DB could lead to false positive matches.
       assert_queries(0) do
-        assert_scripts_equal s1, s2
+        assert_scripts_equal s1, s2, script_excludes
         assert_lesson_groups_equal s1.lesson_groups, s2.lesson_groups
         assert_lessons_equal s1.lessons, s2.lessons
         assert_lesson_activities_equal(
@@ -1329,8 +1345,8 @@ module Services
       end
     end
 
-    def assert_scripts_equal(script1, script2)
-      assert_attributes_equal(script1, script2, ['properties'])
+    def assert_scripts_equal(script1, script2, additional_excludes = [])
+      assert_attributes_equal(script1, script2, additional_excludes + ['properties'])
       assert_equal script1.properties.except('seeded_from'),
         script2.properties.except('seeded_from')
     end
