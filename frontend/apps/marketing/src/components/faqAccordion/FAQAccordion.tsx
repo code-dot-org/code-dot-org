9b5a5dd5f74366ed53ae882deca13411bf52ea51
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-04-24 23:57:43 +0300

    CMS-518: Update Rich Text type check

--- frontend/apps/marketing/src/components/faqAccordion/FAQAccordion.tsx
+++ frontend/apps/marketing/src/components/faqAccordion/FAQAccordion.tsx
@@ -1,4 +1,5 @@
 import {documentToHtmlString} from '@contentful/rich-text-html-renderer';
+import {BLOCKS} from '@contentful/rich-text-types';
 import {EntryFields, BaseEntry} from 'contentful';
 import {useMemo} from 'react';
 import TurndownService from 'turndown';
@@ -20,14 +21,10 @@ export type FAQAccordionContentfulProps = {
   })[];
 };
 
-const checkIfEntryFieldIsRichText = (
-  entry: BaseEntry & {
-    fields: {[key: string]: EntryFields.Text | EntryFields.RichText};
-  },
-  fieldName: string,
-) =>
-  typeof entry.fields[fieldName] !== 'string' &&
-  'content' in entry.fields[fieldName];
+const isRichText = (
+  field: EntryFields.Text | EntryFields.RichText,
+): field is EntryFields.RichText =>
+  typeof field === 'object' && field?.nodeType === BLOCKS.DOCUMENT;
 
 const FAQAccordionContentful: React.FunctionComponent<
   FAQAccordionContentfulProps
@@ -48,26 +45,24 @@ const FAQAccordionContentful: React.FunctionComponent<
       faqs?.filter(Boolean).map(faq => {
         let question, questionString, answer, answerString;
 
-        if (checkIfEntryFieldIsRichText(faq, 'question')) {
-          const richTextQuestion = faq.fields.question as EntryFields.RichText;
-          question = <RichText content={richTextQuestion} />;
+        if (isRichText(faq.fields.question)) {
+          question = <RichText content={faq.fields.question} />;
           questionString = htmlToMarkdownConverter.turndown(
-            documentToHtmlString(richTextQuestion),
+            documentToHtmlString(faq.fields.question),
           );
         } else {
           question = faq.fields.question as string;
           questionString = question;
         }
 
-        if (checkIfEntryFieldIsRichText(faq, 'answer')) {
-          const richTextAnswer = faq.fields.answer as EntryFields.RichText;
+        if (isRichText(faq.fields.answer)) {
           answer = (
             <div className={moduleStyles.faqAccordionAnswer}>
-              <RichText content={richTextAnswer} />
+              <RichText content={faq.fields.answer} />
             </div>
           );
           answerString = htmlToMarkdownConverter.turndown(
-            documentToHtmlString(richTextAnswer),
+            documentToHtmlString(faq.fields.answer),
           );
         } else {
           answer = faq.fields.answer as string;
