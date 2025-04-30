d830949fd0b3b5b4cf1fd13b4504d75353ad93a1
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-04-30 23:57:27 +0300

    CMS-524: Replace Pegasus methods

--- dashboard/app/controllers/promotes_controller.rb
+++ dashboard/app/controllers/promotes_controller.rb
@@ -1,3 +1,4 @@
+require 'cdo/db'
 require 'state_abbr'
 
 class PromotesController < ApplicationController
@@ -7,6 +8,12 @@ class PromotesController < ApplicationController
 
   # GET /promotes/map(/:us_state)
   def map
+    # rubocop:disable CustomCops/PegasusDbUsage
+    @hs_access_count = PEGASUS_DB[:cdo_state_promote].where(require_hs_s: 'Yes').count
+    @k12_access_count = PEGASUS_DB[:cdo_state_promote].where(require_k12_s: 'Yes').count
+    @jobs_nationwide = PEGASUS_DB[:cdo_state_promote].sum(:cs_jobs_i)
+    @grads_nationwide = PEGASUS_DB[:cdo_state_promote].sum(:cs_graduates_i)
+    # rubocop:enable CustomCops/PegasusDbUsage
   end
 
   # GET /promotes/state/:us_state
@@ -14,7 +21,9 @@ class PromotesController < ApplicationController
     # Ensures this action is only accessible via AJAX requests
     return render_404 unless request.xhr?
 
-    @us_state_data = DB[:cdo_state_promote].where(state_code_s: @us_state).first
+    # rubocop:disable CustomCops/PegasusDbUsage
+    @us_state_data = PEGASUS_DB[:cdo_state_promote].where(state_code_s: @us_state).first
+    # rubocop:enable CustomCops/PegasusDbUsage
     render_404 if @us_state_data.blank?
   end
 
