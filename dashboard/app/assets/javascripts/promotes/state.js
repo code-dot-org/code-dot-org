5b45b8fd2af886c385e295da645c36b4cc4e28b0
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-05-01 18:42:07 +0300

    CMS-524: Reformat js files

--- dashboard/app/assets/javascripts/promotes/state.js
+++ dashboard/app/assets/javascripts/promotes/state.js
@@ -2,11 +2,11 @@ var showStatsBtn = document.getElementById('show-ap-stats');
 var hideStatsBtn = document.getElementById('hide-ap-stats');
 
 showStatsBtn.addEventListener('click', function () {
-    showStatsBtn.style.setProperty('display', 'none');
-    hideStatsBtn.style.removeProperty('display');
+  showStatsBtn.style.setProperty('display', 'none');
+  hideStatsBtn.style.removeProperty('display');
 });
 
 hideStatsBtn.addEventListener('click', function () {
-    hideStatsBtn.style.setProperty('display', 'none');
-    showStatsBtn.style.removeProperty('display');
+  hideStatsBtn.style.setProperty('display', 'none');
+  showStatsBtn.style.removeProperty('display');
 });
