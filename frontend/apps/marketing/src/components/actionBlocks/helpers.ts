499dd95ca4ef737a9a04ca3efda4f77d304dcea1
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-05-05 14:54:28 -0700

    update showNewTag function

--- frontend/apps/marketing/src/components/actionBlocks/helpers.ts
+++ frontend/apps/marketing/src/components/actionBlocks/helpers.ts
@@ -1,14 +1,21 @@
-// Show a 'New' tag if the content was published in the last 3 months
+// Shows a "New" tag on Action Blocks with content that was published in the last 3 months.
 export const showNewTag = (publishedDate: string) => {
   if (!publishedDate) return false;
 
-  // Get the content entry publishedDate from Contentful
+  // Uses the "publishedDate" field from Contentful
   const contentPublished = new Date(publishedDate);
-  // Get the current date
+
+  // Get today's date
   const now = new Date();
-  // Get the date 3 months ago from the current date
-  const threeMonthsAgo = new Date();
-  threeMonthsAgo.setMonth(now.getMonth() - 3);
+
+  // Get the date from 3 months ago
+  const threeMonthsAgo = new Date(now);
+  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
+
+  // If the date is invalid (ex: Feb 31), set it to the last day of the previous month
+  if (threeMonthsAgo.getDate() !== now.getDate()) {
+    threeMonthsAgo.setDate(0);
+  }
 
   return contentPublished >= threeMonthsAgo && contentPublished <= now;
 };
