7837dd81da37f054a1110ae2dc1788fca0aeaa16
Bethany Connor <bethany@code.org>
2025-05-06 09:00:45 -0400

    Use display_name_with_latest_year for workshop topic names

--- dashboard/app/controllers/pd/workshop_enrollment_controller.rb
+++ dashboard/app/controllers/pd/workshop_enrollment_controller.rb
@@ -93,7 +93,7 @@ class Pd::WorkshopEnrollmentController < ApplicationController
               location_name: @workshop.location_name,
               virtual: @workshop.virtual?,
               format: @workshop.format,
-              course_offerings: @workshop.course_offerings
+              course_offerings: @workshop.course_offerings.map(&:summarize_self_paced_pl)
             }
           ),
           workshop_location_for_calendar: @workshop.location_address.presence || @workshop.location_name,
