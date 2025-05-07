7de0a4de26cd1472184310f65a09c22e1db52c8f
Erin Bond <erin.bond@code.org>
2025-05-07 09:33:51 -0400

    migrate to add key to skills

--- dashboard/test/lib/services/script_seed_test.rb
+++ dashboard/test/lib/services/script_seed_test.rb
@@ -1531,7 +1531,7 @@ module Services
       end
 
       sl_num = 1
-      skill = create :skill, key: "#{lesson.name}-skill-#{1}"
+      skill = create :skill, key: "#{name_prefix}-skill-#{1}"
       script.lessons.each do |lesson|
         (1..num_activities_per_lesson).each do |activity_pos|
           activity = lesson.lesson_activities.create(
