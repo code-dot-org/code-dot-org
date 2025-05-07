916760d25555643a867ade240f4175c47903f8a6
Erin Bond <erin.bond@code.org>
2025-05-07 12:26:59 -0400

    remove stray lne in schema

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -2422,7 +2422,6 @@ ActiveRecord::Schema.define(version: 2025_05_05_164605) do
     t.datetime "updated_at", precision: 6, null: false
     t.json "editor_font_size"
     t.json "console_font_size"
-    t.json "theme"
     t.index ["user_id"], name: "index_user_preferences_on_user_id"
   end
 
