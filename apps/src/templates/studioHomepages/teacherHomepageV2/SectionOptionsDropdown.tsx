984b9ece769da531f0668284930ec54e1a1ac017
Mark Barnes <mark.barnes@code.org>
2025-05-08 12:37:23 -0500

    change from buttons to links

--- apps/src/templates/studioHomepages/teacherHomepageV2/SectionOptionsDropdown.tsx
+++ apps/src/templates/studioHomepages/teacherHomepageV2/SectionOptionsDropdown.tsx
@@ -1,8 +1,6 @@
 import {CustomDropdown} from '@code-dot-org/component-library/dropdown';
 import FontAwesomeV6Icon from '@code-dot-org/component-library/fontAwesomeV6Icon';
 import React, {useMemo} from 'react';
-// import {useNavigate, NavigateFunction, Link} from 'react-router-dom';
-import {Link} from 'react-router-dom';
 
 import RailsAuthenticityToken from '@cdo/apps/lib/util/RailsAuthenticityToken';
 import {EVENTS, PLATFORMS} from '@cdo/apps/metrics/AnalyticsConstants.js';
@@ -17,6 +15,8 @@ import {Student} from '@cdo/apps/types/redux';
 import HttpClient from '@cdo/apps/util/HttpClient';
 import {useAppDispatch, AppDispatch} from '@cdo/apps/util/reduxHooks';
 import i18n from '@cdo/locale';
+
+import LinkOption from './LinkOption';
 
 import styles from './teacherHomepage.module.scss';
 
@@ -45,38 +45,6 @@ const onDeleteClick = (
     PLATFORMS.BOTH
   );
   onDeleteClickCallback(sectionId);
-};
-
-interface LinkElementProps {
-  value: string;
-  label: string;
-  iconName: string;
-  url: string;
-  eventName?: string;
-}
-
-const LinkOption: React.FC<LinkElementProps> = ({
-  value,
-  label,
-  iconName,
-  url,
-  eventName,
-}) => {
-  return (
-    <li key={value}>
-      <Link
-        to={url}
-        className={styles.dropdownMenuItem}
-        onClick={() => {
-          if (eventName)
-            analyticsReporter.sendEvent(eventName, {}, PLATFORMS.BOTH);
-        }}
-      >
-        <FontAwesomeV6Icon iconName={iconName} iconStyle="solid" />
-        <span>{label}</span>
-      </Link>
-    </li>
-  );
 };
 
 const SectionOptionsDropdown: React.FC<SectionOptionsDropdownProps> = ({
@@ -112,21 +80,21 @@ const SectionOptionsDropdown: React.FC<SectionOptionsDropdownProps> = ({
   const dropdownOptions = useMemo(() => {
     const options = [
       <LinkOption
-        value="sectionSettings"
+        key="sectionSettings"
         label={i18n.sectionSettings()}
         iconName="gear"
         url={`../${TEACHER_NAVIGATION_SECTIONS_URL}/${section.id}/${TEACHER_NAVIGATION_PATHS.settings}`}
         eventName={EVENTS.SECTION_CARD_SETTINGS_CLICKED}
       />,
       <LinkOption
-        value="roster"
+        key="roster"
         label={i18n.roster()}
         iconName="user"
         url={`../${TEACHER_NAVIGATION_SECTIONS_URL}/${section.id}/${TEACHER_NAVIGATION_PATHS.roster}`}
         eventName={EVENTS.SECTION_CARD_ROSTER_CLICKED}
       />,
       <LinkOption
-        value="loginCards"
+        key="loginCards"
         label={i18n.loginCards()}
         iconName="id-card"
         url={`../${TEACHER_NAVIGATION_SECTIONS_URL}/${section.id}/${TEACHER_NAVIGATION_PATHS.loginInfo}`}
