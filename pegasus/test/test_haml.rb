4cd91f150e2d3a0464c33027dee2075cc8b6529b
David Bailey <davidsbailey@users.noreply.github.com>
2025-05-11 18:06:47 -0700

    undelete some tests and skip them conditionally

---
+++ pegasus/test/test_haml.rb
@@ -0,0 +1,21 @@
+require 'rack/test'
+require 'minitest/autorun'
+require_relative '../router'
+
+class HamlTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Documents.new
+  end
+
+  def test_resources_videos
+    skip unless CDO.pegasus_content_enabled
+    path = '/educate/resources/videos'
+    resp = get(path)
+    assert_equal 200, resp.status, path
+    match = /&quot;/
+    quot = resp.body.match(match)
+    assert_nil quot, "Javascript block quoted incorrectly in #{path}"
+  end
+end
