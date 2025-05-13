4c5269cb5c158d1407f6b575aac974616d06a21e
Alice Fisher <alice.fisher@code.org>
2025-05-13 17:20:52 +0000

    remove editor height

--- apps/src/pythonlab/layout/WidgetView.tsx
+++ apps/src/pythonlab/layout/WidgetView.tsx
@@ -11,9 +11,8 @@ import moduleStyles from '@cdo/apps/lab2/views/components/layout/layout.module.s
 const MIN_RIGHT_PANEL_WIDTH = 300;
 const MIN_LEFT_PANEL_WIDTH = 150;
 const MIN_OUTPUT_HEIGHT = 120;
-const MIN_EDITOR_HEIGHT = 200;
 const INITIAL_INFO_PANEL_WIDTH = 300;
-const INITIAL_OUTPUT_HEIGHT = 300;
+const INITIAL_OUTPUT_HEIGHT = 800;
 
 const WidgetView: React.FunctionComponent<LayoutProps> = () => {
   const {
@@ -30,7 +29,7 @@ const WidgetView: React.FunctionComponent<LayoutProps> = () => {
       name: 'instructions',
     },
     rightTopPanel: {
-      minHeight: MIN_EDITOR_HEIGHT,
+      minHeight: 0,
       name: 'editor',
     },
     rightBottomPanel: {
