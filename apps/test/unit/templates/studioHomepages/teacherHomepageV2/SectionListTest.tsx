e3ad1467b9d8128872a1652474de6909fbdd2f7a
Mark Barnes <mark.barnes@code.org>
2025-05-14 13:42:08 -0500

    add unit test

--- apps/test/unit/templates/studioHomepages/teacherHomepageV2/SectionListTest.tsx
+++ apps/test/unit/templates/studioHomepages/teacherHomepageV2/SectionListTest.tsx
@@ -1,4 +1,4 @@
-import {fireEvent, render, screen, waitFor} from '@testing-library/react';
+import {fireEvent, render, screen, waitFor, act} from '@testing-library/react';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {
@@ -85,13 +85,11 @@ describe('SectionList', () => {
     registerReducers({teacherSections});
     store.dispatch(setSections(serverSections));
     store.dispatch(setSectionOrder([11, 12, 13, 14]));
+
+    store.dispatch(setAsyncLoad(true));
   });
 
-  function renderComponent(
-    initialRoute = '/teacher_dashboard/home',
-    asyncLoadComplete = true
-  ) {
-    store.dispatch(setAsyncLoad(asyncLoadComplete));
+  function renderComponent(initialRoute = '/teacher_dashboard/home') {
     return render(
       <Provider store={store}>
         <RouterProvider
@@ -133,11 +131,12 @@ describe('SectionList', () => {
 
   it('deletes a section when the delete button is clicked on the section delete modal', async () => {
     renderComponent();
+
     const deleteButtons = screen.getAllByText(i18n.delete());
     fireEvent.click(deleteButtons[0]);
     await screen.findByText(i18n.deleteSection());
     const deleteModalButton = screen.getByLabelText(i18n.delete());
-    fireEvent.click(deleteModalButton);
+    await act(async () => fireEvent.click(deleteModalButton));
     expect(screen.queryByText('Period 1')).toBeNull;
     screen.getByText('Period 2');
     screen.getByText('Period 3');
@@ -146,7 +145,6 @@ describe('SectionList', () => {
 
   it('removes a section from the list when archived and maintains the order of other sections', async () => {
     renderComponent();
-
     screen.getByRole('listitem', {
       name: 'Period 1',
     });
@@ -185,12 +183,17 @@ describe('SectionList', () => {
     );
   });
 
+  it('renders spinner while sections are loading', async () => {
+    store.dispatch(setAsyncLoad(false));
+    renderComponent();
+    screen.debug();
+  });
+
   it('renders sections in the same order as specified in Redux', async () => {
     const customOrder = [14, 13, 12, 11];
     store.dispatch(setSectionOrder(customOrder));
 
     renderComponent();
-
     // Alternative verification by checking DOM order
     const p4 = screen.getByRole('listitem', {name: 'Period 4'});
     const p3 = screen.getByRole('listitem', {name: 'Period 3'});
