fb8d3d18c14d56c4f450c8381b7e996aae278aba
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-05-14 15:57:23 -0700

    move CI scripts into their own subdirectory

---
+++ docker/ci/scripts/prepare_tests_common.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Script for doing common preparation needed for all operations within a docker
+# container. Used by prepare_ui_tests.sh and prepare_unit_tests.sh
+
+set -e
+
+export CI=true
+export CI_BUILD_NUMBER=${CI_BUILD_NUMBER:-$RANDOM$RANDOM} # determines where test logs are stored in S3.
+export CI_TEST_REPORTS=${CI_TEST_REPORTS:-/home/ci/test_reports}
+
+export RAILS_ENV=test
+export RACK_ENV=test
+export DISABLE_SPRING=1
+export LD_LIBRARY_PATH=/usr/local/lib
+
+# set up locals.yml
+echo "
+# Shared settings and secrets
+build_apps: true
+build_dashboard: true
+build_i18n: false
+build_pegasus: true
+bundler_use_sudo: false
+cloudfront_key_pair_id: $CLOUDFRONT_KEY_PAIR_ID
+cloudfront_private_key: \"$CLOUDFRONT_PRIVATE_KEY\"
+dashboard_db_reader: \"mysql://readonly@localhost/dashboard_test\"
+dashboard_enable_pegasus: true
+dashboard_workers: 5
+disable_all_eyes_running: true
+ignore_eyes_mismatches: true
+localize_apps: true
+use_my_apps: true
+skip_seed_all: true
+" >> locals.yml
+echo "Wrote shared settings and secrets from env vars into locals.yml."
---
+++ docker/ci/scripts/prepare_ui_tests.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Script for preparing ui tests within a docker container. Used by ui_tests.sh.
+
+source docker/prepare_tests_common.sh
+
+ulimit -n 4096
+
+export CI_JOB=ui_tests
+
+# set up locals.yml
+echo "
+# UI test settings and secrets
+override_dashboard: \"localhost-studio.code.org\"
+override_pegasus: \"localhost.code.org\"
+dashboard_port: 3000
+pegasus_port: 3000
+animations_s3_directory: animations_circle/$CI_BUILD_NUMBER
+assets_s3_directory: assets_circle/$CI_BUILD_NUMBER
+files_s3_directory: files_circle/$CI_BUILD_NUMBER
+libraries_s3_directory: libraries_circle/$CI_BUILD_NUMBER
+sources_s3_directory: sources_circle/$CI_BUILD_NUMBER
+session_store_server: 'redis://ui-tests-redis:6379/0/session'
+no_https_store: true
+netsim_redis_groups:
+- master: redis://ui-tests-redis:6379
+saucelabs_authkey: $SAUCE_ACCESS_KEY
+saucelabs_username: $SAUCE_USERNAME
+saucelabs_tunnel_name: cdo-tunnel-$CI_BUILD_NUMBER
+properties_encryption_key: $PROPERTIES_ENCRYPTION_KEY
+aiproxy_api_key: 'fake_key'
+" >> locals.yml
+echo "Wrote UI-test-specific settings and secrets from env vars into locals.yml."
+
+set -x
+
+bundle install --quiet
+bundle exec rake install
+bundle exec rake build
+
+bundle exec rake ci:seed_ui_test
