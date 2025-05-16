342ae8b387fe685a6e825ee7dac010550a0c77d4
Carl Reiner <carl.reiner@code.org>
2025-05-16 15:42:35 -0700

    convert old User queries class to module

--- dashboard/lib/queries/user.rb
+++ dashboard/lib/queries/user.rb
@@ -1,7 +1,7 @@
 module Queries
   module User
-    def self.dependent_students_count(user_id)
-      user = User.find(user_id)
+    def dependent_students_count(user_id)
+      user = ::User.find(user_id)
       user.sections.where.not(login_type: 'email').distinct.joins(:students).select(:'users.id').count
     end
   end
