da4838a5971c54809343fa2a130acddb4edf6bb8
breville <brendan@code.org>
2025-05-17 04:29:27 +0000

    Python Lab: Validation duration

--- apps/src/codebridge/InfoPanel/ValidatedInstructions.tsx
+++ apps/src/codebridge/InfoPanel/ValidatedInstructions.tsx
@@ -43,6 +43,8 @@ import ValidationResults from './ValidationResults';
 
 import moduleStyles from '@codebridge/InfoPanel/styles/validated-instructions.module.scss';
 
+const MINIMUM_VALIDATION_TIME = 1500; // 1 second
+
 interface InstructionsProps {
   /** Additional callback to fire before navigating to the next level. */
   beforeNextLevel?: () => void;
@@ -167,9 +169,10 @@ const ValidatedInstructions: React.FunctionComponent<InstructionsProps> = ({
         scriptId: scriptId,
         interaction: UserLevelInteractions.click_validate,
       });
-      onRun(true, dispatch, source).finally(() =>
-        dispatch(setIsValidating(false))
-      );
+      Promise.allSettled([
+        onRun(true, dispatch, source),
+        new Promise(resolve => setTimeout(resolve, MINIMUM_VALIDATION_TIME)),
+      ]).then(() => dispatch(setIsValidating(false)));
       dispatch(setHasValidated(true));
       dispatch(setShowSuggestedPrompts(true));
     } else {
