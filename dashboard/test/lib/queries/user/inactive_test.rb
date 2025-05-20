8aa657d45eac32aeebd5cd7199839da1ec1d0b8d
Carl Reiner <carl.reiner@code.org>
2025-05-20 16:52:16 -0700

    PR feedback

--- dashboard/test/lib/queries/user/inactive_test.rb
+++ dashboard/test/lib/queries/user/inactive_test.rb
@@ -3,29 +3,30 @@ require 'test_helper'
 class Queries::User::InactiveTest < ActiveSupport::TestCase
   let(:described_instance) {described_class.new(scope: scope, inactive_since: inactive_since)}
   let(:inactive_since) {42.months.ago}
-  let(:current_sign_in_at) {1.day.ago}
-  let(:created_at) {1.day.ago}
-  let(:active_user) {create(:user)}
-  let(:inactive_user) {create(:user, current_sign_in_at: inactive_since)}
-  let(:inactive_user_2) {create(:user, current_sign_in_at: 5.years.ago)}
-  let(:custom_date_user) {create(:user, current_sign_in_at: current_sign_in_at, created_at: created_at)}
-  let(:scope) {User.where(id: [active_user.id, inactive_user.id, inactive_user_2.id, custom_date_user.id])}
+  let(:scope) {User.all}
 
   describe '#call' do
     subject(:inactive_users) {described_instance.call}
 
     it 'returns users who have not signed in since the given date' do
-      _(inactive_users).must_include inactive_user
-      _(inactive_users).must_include inactive_user_2
-      _(inactive_users).wont_include active_user
+      expected_user = create(:user, current_sign_in_at: inactive_since - 1.day)
+      _(inactive_users).must_include expected_user
     end
 
-    context 'when an inactive user has never signed in' do
-      let(:current_sign_in_at) {nil}
-      let(:created_at) {inactive_since - 1.day}
+    it 'does not return users who have signed in since the given date' do
+      expected_user = create(:user, current_sign_in_at: inactive_since + 1.day)
+      _(inactive_users).wont_include expected_user
+    end
 
+    context 'when a user has never signed in' do
       it 'returns the user who never signed in but was created before inactive_since' do
-        _(inactive_users).must_include custom_date_user
+        expected_user = create(:user, current_sign_in_at: nil, created_at: inactive_since - 1.day)
+        _(inactive_users).must_include expected_user
+      end
+
+      it 'does not return the user who never signed in but was created after inactive_since' do
+        expected_user = create(:user, current_sign_in_at: nil, created_at: inactive_since + 1.day)
+        _(inactive_users).wont_include expected_user
       end
     end
 
