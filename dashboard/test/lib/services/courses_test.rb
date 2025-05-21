5fb4c078fcb5434a3011dd67c0268c88d9dc5dcf
Dayne <dayne@code.org>
2025-05-20 22:40:39 -0700

    Passing unit_name_or_id explicitly to canonical_path

--- dashboard/test/lib/services/courses_test.rb
+++ dashboard/test/lib/services/courses_test.rb
@@ -4,10 +4,11 @@ class Services::CoursesTest < ActiveSupport::TestCase
   include Minitest::RSpecMocks
 
   describe '.canonical_path' do
+    let(:subject) {described_class.canonical_path(path, unit_name, current_user)}
     let(:current_user) {instance_double(User)}
-    let(:path) {'/s/script-1/some-path'}
-    let(:params) {{}}
+    let(:path) {"/s/#{unit_name}/some-path"}
     let(:modularity_enabled) {false}
+    let(:unit_name) {'script-1'}
     let(:unit) {nil}
     let(:unit_group) {nil}
     let(:unit_group_unit) {nil}
@@ -21,65 +22,45 @@ class Services::CoursesTest < ActiveSupport::TestCase
 
     before do
       allow(Policies::Courses).to receive(:modularity_enabled?).with(any_args).and_return(modularity_enabled)
-      allow(Queries::Courses).to receive(:get_course_context).and_return(course_context)
+      # allow(Queries::Courses).to receive(:get_course_context).and_return(course_context)
     end
 
     context 'the modularity experiment is not enabled' do
       it 'returns the original path' do
-        _(described_class.canonical_path(path, params, current_user)).must_equal path
+        _(subject).must_equal path
       end
     end
 
     context 'the modularity experiment is enabled' do
       let(:modularity_enabled) {true}
 
-      context 'script_name is not present in params' do
+      context 'unit_name_or_id is nil' do
+        let(:unit_name) {nil}
         it 'returns the original path' do
-          _(described_class.canonical_path(path, params, current_user)).must_equal path
+          _(subject).must_equal path
         end
       end
 
-      context 'script_name is present in params[:script_id]' do
-        let(:script_name) {'script-1'}
-        let(:params) {{script_id: script_name}}
-        let(:unit) {create :unit, name: script_name}
+      context 'unit_name_or_id is defined' do
+        let!(:unit) {create :unit, name: unit_name}
+
+        before do
+          unit.reload
+        end
 
         context 'unit_group is not found' do
           it 'returns the original path' do
-            _(described_class.canonical_path(path, params, current_user)).must_equal path
+            _(subject).must_equal path
           end
         end
 
         context 'unit_group is found' do
           let(:unit_group) {create :unit_group, name: 'cool-course'}
           let(:position) {2}
-          let(:unit_group_unit) {create :unit_group_unit, script_id: unit.id, course_id: unit_group.id, position: position}
+          let!(:unit_group_unit) {create :unit_group_unit, script_id: unit.id, course_id: unit_group.id, position: position}
 
           it 'returns the modified path with /courses/.../units/.../' do
-            _(described_class.canonical_path(path, params, current_user)).must_equal '/courses/cool-course/units/2/some-path'
-          end
-        end
-      end
-
-      context 'script_name is present in params[:id]' do
-        let(:script_name) {'script-1'}
-        let(:params) {{id: script_name}}
-        let(:path) {'/s/script-1/some-path'}
-        let(:unit) {create :unit, name: script_name}
-
-        context 'unit_group is not found' do
-          it 'returns the original path' do
-            _(described_class.canonical_path(path, params, current_user)).must_equal path
-          end
-        end
-
-        context 'unit_group is found' do
-          let(:unit_group) {create :unit_group, name: 'cool-course'}
-          let(:position) {2}
-          let(:unit_group_unit) {create :unit_group_unit, script_id: unit.id, course_id: unit_group.id, position: position}
-
-          it 'returns the modified path with /courses/.../units/.../' do
-            _(described_class.canonical_path(path, params, current_user)).must_equal '/courses/cool-course/units/2/some-path'
+            _(subject).must_equal '/courses/cool-course/units/2/some-path'
           end
         end
       end
