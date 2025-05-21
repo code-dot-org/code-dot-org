ddc7740be8aa07dea833f10f30cbdad31f92867d
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-05-21 11:14:22 -0700

    update const names to be more clear

--- frontend/apps/marketing/src/components/snapshots/helpers.ts
+++ frontend/apps/marketing/src/components/snapshots/helpers.ts
@@ -3,16 +3,16 @@
 export const sortInAscendingOrder = (a: string, b: string) => {
   const itemA = parseInt(a, 10);
   const itemB = parseInt(b, 10);
-  const isItemA = !isNaN(itemA);
-  const isItemB = !isNaN(itemB);
+  const isItemANumeric = !isNaN(itemA);
+  const isItemBNumeric = !isNaN(itemB);
 
   // If one is a string (not a number), sort that first.
   // It will likely be a grade range like "K-2".
-  if (!isItemA && isItemB) return -1;
-  if (isItemA && !isItemB) return 1;
+  if (!isItemANumeric && isItemBNumeric) return -1;
+  if (isItemANumeric && !isItemBNumeric) return 1;
 
   // If both are numbers, sort numerically
-  if (isItemA && isItemB) return itemA - itemB;
+  if (isItemANumeric && isItemBNumeric) return itemA - itemB;
 
   return a < b ? -1 : a > b ? 1 : 0;
 };
