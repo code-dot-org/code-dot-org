85e982e7ce6d662da9c54457b19f6d322db2558b
Cassi Brenci <cassi.brenci@code.org>
2025-05-22 15:33:06 -0700

    cleanup oopsies

--- dashboard/app/models/ability.rb
+++ dashboard/app/models/ability.rb
@@ -511,20 +511,15 @@ class Ability
         user.has_ai_tutor_access?
       end
 
-      can [:start_chat_completion, :chat_request], :aichat_request do
-        # Change this to just must be logged in?
-        user.teacher_can_access_ai_chat? || user.student_can_access_ai_chat?
-      end
-
       can :find_toxicity, :aichat do
         user.teacher_can_access_ai_chat? || user.student_can_access_ai_chat?
       end
 
       # Additional logic that confirms that a given teacher or student should have access
       # to a given student (or their own, in the case of a student viewer) chat history is in aichat_events_controller.
-      # can [:log_chat_event, :chat_history], :aichat_event do
-      #   user.teacher_can_access_ai_chat? || user.student_can_access_ai_chat?
-      # end
+      can [:log_chat_event, :chat_history], :aichat_event do
+        user.teacher_can_access_ai_chat? || user.student_can_access_ai_chat?
+      end
       can :submit_teacher_feedback, :aichat_event do
         user.teacher_can_access_ai_chat?
       end
