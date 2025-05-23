cdf6795df6e42e3a07f432567bba1df7ece599fc
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-05-23 21:41:47 +0300

    feat(Testimonial): CMS - Add Testimonial component

---
+++ frontend/packages/component-library/src/cms/testimonial/stories/Testimonial.story.tsx
@@ -0,0 +1,43 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
+import Testimonial, {
+  TESTIMONIAL_BACKGROUNDS,
+  TestimonialProps,
+} from './../Testimonial';
+
+type Story = StoryObj<TestimonialProps>;
+
+export default {
+  title: 'CMS/Testimonial',
+  component: Testimonial,
+} as Meta<TestimonialProps>;
+
+const defaultProps: TestimonialProps = {
+  quote:
+    'We’re not just teaching students — we are systemically, school by school, classroom by classroom, making this baked into education... This has really become a global movement. Powered by teachers.',
+  source: 'Hadi Partovi',
+  context: 'CEO of Code.org',
+};
+
+//
+// STORIES
+//
+export const Playground: Story = {
+  args: {
+    ...defaultProps,
+  },
+};
+
+export const WithDarkPatternBackground: Story = {
+  args: {
+    ...defaultProps,
+    background: TESTIMONIAL_BACKGROUNDS.PATTERN_DARK,
+  },
+};
+
+export const WithPrimaryPatterBackground: Story = {
+  args: {
+    ...defaultProps,
+    background: TESTIMONIAL_BACKGROUNDS.PATTERN_PRIMARY,
+  },
+};
