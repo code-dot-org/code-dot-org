9dcfe488ea38a6bca2f956b6add45b413adb2b9d
alex-m-brown <alex.brown@code.org>
2025-05-23 16:45:07 -0400

    Revert "update test fixtures"
    This reverts commit 41a8c585704a47d17203fde16828bdc64b313593.

--- dashboard/scripts/generate_fixtures.rb
+++ dashboard/scripts/generate_fixtures.rb
@@ -39,7 +39,6 @@ scripts_map = {
 @course_offerings = {}
 @course_versions = {}
 @unit_groups = {}
-@unit_group_units = {}
 @plc_courses = {}
 @scripts = {}
 @plc_course_units = {}
@@ -55,10 +54,6 @@ scripts_map.each do |_script_id, name|
 
   script.unit_groups&.each do |unit_group|
     @unit_groups[unit_group.name] = unit_group.attributes
-  end
-
-  script.unit_group_units&.each do |ugu|
-    @unit_group_units["ugu-#{script.name}-#{ugu.course_id}"] = ugu.attributes
   end
 
   course_version = script.get_course_version
@@ -111,7 +106,6 @@ prefix = Rails.root.join('test/fixtures/')
 File.new("#{prefix}course_offerings.yml", 'w').write(yamlize(@course_offerings))
 File.new("#{prefix}course_versions.yml", 'w').write(yamlize(@course_versions))
 File.new("#{prefix}unit_groups.yml", 'w').write(yamlize(@unit_groups))
-File.new("#{prefix}unit_group_units.yml", 'w').write(yamlize(@unit_group_units))
 File.new("#{prefix}plc_courses.yml", 'w').write(yamlize(@plc_courses))
 File.new("#{prefix}unit.yml", 'w').write(yamlize(@scripts))
 File.new("#{prefix}plc_course_units.yml", 'w').write(yamlize(@plc_course_units))
