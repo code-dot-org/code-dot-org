d2f710e56f7cc1861035c339c8a80fd2b991eaba
wilkie <david.wilkinson@code.org>
2025-05-24 00:31:06 -0400

    Removes 'require' in dance setup file.

--- apps/src/dance/blockly/setup.ts
+++ apps/src/dance/blockly/setup.ts
@@ -1,8 +1,7 @@
+import * as blockUtils from '@cdo/apps/block_utils';
 import {BlockDefinition} from '@cdo/apps/blockly/types';
 import * as commonBlocks from '@cdo/apps/blocksCommon';
 import danceBlocks from '@cdo/apps/dance/blockly/blocks';
-
-const blockUtils = require('@cdo/apps/block_utils');
 
 // Install common blocks for Dance Party Lab2.
 export function installCommonBlocks(
@@ -24,7 +23,7 @@ export function installDanceBlocks(
   danceBlocks.install(Blockly);
   const blocksByCategory = blockUtils.installCustomBlocks({
     blockly: Blockly,
-    blockDefinitions: sharedBlocks,
+    blockDefinitions: sharedBlocks || [],
     customInputTypes: danceBlocks.customInputTypes,
   });
   return blocksByCategory; // TODO: use when implementing pathway for toolbox editing for levelbuilders
