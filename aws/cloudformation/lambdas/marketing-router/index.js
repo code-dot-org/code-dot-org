6d26634098f74146d3f8f66201400a756e9d777b
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-05-28 06:10:22 -0700

    Revert "Revert students after failing test"
    This reverts commit f6ff7b93c7cf7f632e44c5e6a372b0339111990a.

--- aws/cloudformation/lambdas/marketing-router/index.js
+++ aws/cloudformation/lambdas/marketing-router/index.js
@@ -148,7 +148,7 @@ const marketingPaths = {
   "/student/middle-high": true,
   "/student/elementary": true,
   "/about/hear-from-us": true,
-  "/students": false,
+  "/students": true,
   "/curriculum/elementary-school": true,
   "/about": true,
   "/educate/gamelab": true,
