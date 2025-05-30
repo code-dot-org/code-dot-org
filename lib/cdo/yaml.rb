5011dd6b12b3a4d2d4bb9ff10b333a79c192e87e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-05-30 12:37:53 -0700

    update another YAML.load call to add compatibility for Psych 4

--- lib/cdo/yaml.rb
+++ lib/cdo/yaml.rb
@@ -15,6 +15,13 @@ module Cdo
 
     # Return +nil+ if file not found.
     def load_file(path)
+      # Temporarily accommodate both Psych 3 and Psych 4 syntax while we
+      # navigate the transition from Ruby 3.0 to Ruby 3.1.
+      # See: https://stackoverflow.com/a/71192990/1810460
+      # TODO infra: simplify down to just the Psych 4 case once we're fully
+      # upgraded to Ruby 3.1.
+      super(path, aliases: true)
+    rescue ArgumentError
       super
     rescue Errno::ENOENT
       nil
