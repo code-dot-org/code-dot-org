eb4e5f26c191e71aa2959a2cdb3a9221b67cc4c1
David Bailey <davidsbailey@users.noreply.github.com>
2025-06-02 16:51:12 -0700

    seed only hoc in SessionCookieTest

--- dashboard/test/integration/session_cookie_test.rb
+++ dashboard/test/integration/session_cookie_test.rb
@@ -3,7 +3,7 @@ require 'cdo/script_config'
 
 class SessionCookieTest < ActionDispatch::IntegrationTest
   setup_all do
-    seed_deprecated_unit_fixtures
+    seed_deprecated_unit_fixtures(unit_names: [Unit::HOC_NAME])
   end
 
   test 'session cookie name contains environment' do
