c3f4cf34564287794c937140114a06a02040cfaa
Liam Frye-Mason <liam.fryemason@code.org>
2025-06-02 16:48:22 -0700

    Add user levels and feedback

--- dashboard/lib/test_section.rb
+++ dashboard/lib/test_section.rb
@@ -7,8 +7,18 @@ class TestSection
   DEFAULT_SECTION_NAME = 'Generated Test Section'
   DEFAULT_NUM_STUDENTS = 10
 
+  SAMPLE_STUDENT_NAME_FORMAT = 'Student%s Generated'.freeze
+  SAMPLE_STUDENT_NAME_REGEX = /Student\d* Generated/
+
   DEFAULT_UNIT = 'csp3-2024'
   DEFAULT_COURSE = 'csp-2024'
+
+  @@rng = nil
+
+  # Returns a seeded random number generator for consistent test data
+  def self.rng
+    @@rng ||= Random.new(0)
+  end
 
   def self.seed(options)
     teacher_id = options[:teacher_id] || nil
@@ -43,8 +53,12 @@ class TestSection
     end
 
     # Create the test teacher
-    create :teacher, email: DEFAULT_TEACHER_EMAIL, name: DEFAULT_TEACHER_NAME,
+    teacher = create :teacher, email: DEFAULT_TEACHER_EMAIL, name: DEFAULT_TEACHER_NAME,
       password: DEFAULT_TEACHER_PASSWORD, terms_of_service_version: 1
+
+    # Make the teacher an authorized teacher
+    UserPermission.create(user: teacher, permission: UserPermission::AUTHORIZED_TEACHER)
+    teacher
   end
 
   def self.delete_existing_teacher(user)
@@ -52,7 +66,6 @@ class TestSection
       # Hard-delete all students in each section.
       section.students.each do |student_user|
         raise "Not a sample student - #{student_user.name}" unless SAMPLE_STUDENT_NAME_REGEX.match?(student_user.name)
-        environment_check!
         UserGeo.where(user_id: student_user.id).destroy_all
         student_user.really_destroy!
       end
@@ -74,6 +87,124 @@ class TestSection
     section = create :section, script: unit, unit_group: unit_group,
       **options.slice(:teacher, :name, :login_type, :grade)
 
+    current_student = 0
+    students = []
+    level_count = unit.script_levels.count
+
+    # Create students in section
+    (1..options[:num_students]).each do
+      # Choose random properties and create student
+      current_student += 1
+
+      name = format(SAMPLE_STUDENT_NAME_FORMAT, current_student)
+      student_user = create :student, name: name, age: 10, gender: nil
+
+      # Add student to section
+      create :follower, section: section, student_user: student_user
+      students << student_user
+    end
+
+    students.each do |student_user|
+      max_level = max_level_for_student(level_count)
+      create_user_levels(student_user, max_level, unit)
+      create_teacher_feedback(student_user, options[:teacher], max_level, unit)
+    end
     section
+  end
+
+  def self.max_level_for_student(level_count)
+    if rng.rand(100) < 90
+      (level_count.to_f * rng.rand(0.2..0.8)).to_i
+    else
+      # To simulate real-world data, some students have no progress
+      0
+    end
+  end
+
+  def self.create_user_levels(student_user, max_level, unit)
+    script_levels = unit.script_levels.includes(:levels)
+    current_level = 0
+
+    pct_skipped = rng.rand(15)
+
+    script_levels.each do |script_level|
+      break if current_level == max_level
+
+      # Roll the dice to decide if progress is completed, perfect, or
+      # skipped for this level
+      rand_val = rng.rand(100)
+      best_result =
+        if rand_val < pct_skipped
+          nil
+        elsif rand_val < 0
+          ActivityConstants::MINIMUM_PASS_RESULT
+        else
+          ActivityConstants::BEST_PASS_RESULT
+        end
+
+      # Save progress for this level if not skipping
+      if best_result
+        create :user_level, user: student_user, script_id: unit.id,
+          level_id: script_level.levels.first.id, attempts: 1,
+          best_result: best_result
+
+        # Create a backing channel for this level if it's a type that needs it
+        if script_level.levels.first.channel_backed?
+          ChannelToken.find_or_create_channel_token(
+            script_level.levels.first,
+            '127.0.0.1',
+            find_or_create_storage_id_for_user_id(student_user.id),
+            unit.id,
+            {
+              hidden: true
+            }
+          )
+        end
+      end
+
+      current_level += 1
+    end
+  end
+
+  def self.create_teacher_feedback(student_user, teacher, max_level, unit)
+    script_levels = unit.script_levels.includes(:levels)
+    current_level = 0
+
+    script_levels.each do |script_level|
+      break if current_level == max_level
+
+      if rng.rand(100) < 20 # 20% chance of feedback
+        create :teacher_feedback,
+          student: student_user,
+          teacher: teacher,
+          level: script_level.levels.first,
+          script: unit,
+          comment: tiny_lipsum
+      end
+
+      current_level += 1
+    end
+  end
+
+  # Look up or create a storage id for the sample data (used for testing purposes)
+  def self.find_or_create_storage_id_for_user_id(user_id)
+    storage_id = storage_id_for_user_id(user_id)
+    return storage_id if storage_id
+    create_storage_id_for_user(user_id)
+  end
+
+  # Helper that generates a few sentences of plausible latin-esqe text, for use as obviously
+  # fake text data.
+  def self.tiny_lipsum
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " \
+    "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco " \
+    "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in " \
+    "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat " \
+    "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".
+      split(/[.,]/).
+      sample(rng.rand(3..6)).
+      filter_map(&:strip).
+      map(&:capitalize).
+      join('. ') + '.'
   end
 end
