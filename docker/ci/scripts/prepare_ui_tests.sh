1b09dad088eaeb978e5d1013a3f317c5bd5ca346
David Bailey <davidsbailey@users.noreply.github.com>
2025-06-02 17:41:04 -0700

    redo changes to docker scripts

--- docker/ci/scripts/prepare_ui_tests.sh
+++ docker/ci/scripts/prepare_ui_tests.sh
@@ -54,6 +54,20 @@ echo "Wrote settings and secrets from env vars into locals.yml."
 
 set -x
 
+bundle exec rake ci:sparse_checkout
+
+# Example file which will not be present if pegasus content is omitted
+# via sparse checkout.
+EXAMPLE_FILE="pegasus/sites.v3/code.org/homepage.json"
+
+# Disable Pegasus content if the example file is not present.
+if [ -f "$EXAMPLE_FILE" ]; then
+  echo "Pegasus homepage.json present"
+else
+  export HAS_PEGASUS_CONTENT=false
+  echo "Pegasus homepage.json missing – HAS_PEGASUS_CONTENT set to false"
+fi
+
 bundle install --quiet
 bundle exec rake install
 bundle exec rake build
