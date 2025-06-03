db8bc537643a18580d49d7c7f8e981e441dcc588
David Bailey <davidsbailey@users.noreply.github.com>
2025-06-02 17:45:12 -0700

    extract to prepare_tests_common.sh

--- docker/ci/scripts/prepare_tests_common.sh
+++ docker/ci/scripts/prepare_tests_common.sh
@@ -13,3 +13,17 @@ export RAILS_ENV=test
 export RACK_ENV=test
 export DISABLE_SPRING=1
 export LD_LIBRARY_PATH=/usr/local/lib
+
+bundle exec rake ci:sparse_checkout
+
+# Example file which will not be present if pegasus content is omitted
+# via sparse checkout.
+EXAMPLE_FILE="pegasus/sites.v3/code.org/homepage.json"
+
+# Disable Pegasus content if the example file is not present.
+if [ -f "$EXAMPLE_FILE" ]; then
+  echo "Pegasus homepage.json present"
+else
+  export HAS_PEGASUS_CONTENT=false
+  echo "Pegasus homepage.json missing – HAS_PEGASUS_CONTENT set to false"
+fi
