c80dc91ea82c701f5480b26f5c618e63a8f6929a
alex-m-brown <alex.brown@code.org>
2025-06-05 12:27:51 -0400

    restore factory fields

--- dashboard/test/integration/db_query_test.rb
+++ dashboard/test/integration/db_query_test.rb
@@ -125,6 +125,7 @@ class DBQueryTest < ActionDispatch::IntegrationTest
     script = create(
       :script,
       :with_levels,
+      :in_unit_group,
       levels_count: 10
     )
     course = create(:single_unit_course, unit: script, published_state: Curriculum::SharedCourseConstants::PUBLISHED_STATE.stable, version_year: 'unversioned', family_name: 'hoc-family')
