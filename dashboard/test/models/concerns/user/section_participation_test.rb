2d11c419bfbd5c43d0d0d8bf0aae7da4fb744596
Nick Lathe <nick.lathe@code.org>
2025-06-06 11:14:44 -0700

    Improve tests
       - Use subject where appropriate to streamline tests

--- dashboard/test/models/concerns/user/section_participation_test.rb
+++ dashboard/test/models/concerns/user/section_participation_test.rb
@@ -2,12 +2,13 @@ require 'test_helper'
 
 class SectionParticipationTest < ActiveSupport::TestCase
   describe '#all_sections' do
+    subject(:all_sections) {user.all_sections}
     context 'when the user is a studnet' do
       let(:user) {create :student}
 
       it 'returns an empty array' do
-        _(user.all_sections).must_equal []
-        _(user.all_sections.length).must_equal 0
+        _(all_sections).must_equal []
+        _(all_sections.length).must_equal 0
       end
     end
 
@@ -24,17 +25,17 @@ class SectionParticipationTest < ActiveSupport::TestCase
       end
 
       it 'returns sections the user teaches' do
-        _(user.all_sections).must_include section1
-        _(user.all_sections).must_include section2
+        _(all_sections).must_include section1
+        _(all_sections).must_include section2
       end
 
       it 'returns sections the user is enrolled in as a student' do
-        _(user.all_sections).must_include section3
+        _(all_sections).must_include section3
       end
 
       it 'returns all sections the user is part of, regardless of role' do
-        _(user.all_sections).must_equal [section1, section2, section3]
-        _(user.all_sections.size).must_equal 3
+        _(all_sections).must_equal [section1, section2, section3]
+        _(all_sections.size).must_equal 3
       end
     end
   end
@@ -44,13 +45,17 @@ class SectionParticipationTest < ActiveSupport::TestCase
     let(:student) {create :student}
     let(:grand_teacher) {create :teacher}
     let(:unit_group) {create :unit_group, name: 'csd'}
+    subject(:section_courses) {teacher.section_courses}
 
     context 'when a teacher exists as a student in a section' do
       let!(:grand_section) {create :section, user: grand_teacher, unit_group: unit_group}
+      let(:courses) {section_courses}
+
+      before do
+        create :follower, section: grand_section, student_user_id: teacher.id, user: grand_teacher
+      end
 
       it 'returns the courses in which they are a part of' do
-        create :follower, section: grand_section, student_user_id: teacher.id, user: grand_teacher
-        courses = teacher.section_courses
         _(courses.length).must_equal 1
         _(courses.first.name).must_equal 'csd'
       end
@@ -58,10 +63,13 @@ class SectionParticipationTest < ActiveSupport::TestCase
 
     context 'when a teacher exists as a teacher in a section' do
       let!(:section) {create :section, user: teacher, unit_group: unit_group}
+      let(:courses) {section_courses}
+
+      before do
+        create :follower, section: section, student_user_id: student.id, user: teacher
+      end
 
       it 'returns the courses in which they are a part of' do
-        create :follower, section: section, student_user_id: student.id, user: teacher
-        courses = teacher.section_courses
         _(courses.length).must_equal 1
         _(courses.first.name).must_equal 'csd'
       end
@@ -69,10 +77,13 @@ class SectionParticipationTest < ActiveSupport::TestCase
 
     context 'when a student exists in a section' do
       let!(:section) {create :section, user: teacher, unit_group: unit_group}
+      let(:courses) {student.section_courses}
+
+      before do
+        create :follower, section: section, student_user_id: student.id, user: teacher
+      end
 
       it 'returns the courses in which they are a part of' do
-        create :follower, section: section, student_user_id: student.id, user: teacher
-        courses = student.section_courses
         _(courses.length).must_equal 1
         _(courses.first.name).must_equal 'csd'
       end
@@ -85,6 +96,7 @@ class SectionParticipationTest < ActiveSupport::TestCase
     let!(:section1) {create :section, user: section_teacher}
     let!(:section2) {create :section, :teacher_participants, user: section_teacher}
     let!(:section3) {create :section, :facilitator_participants, user: section_teacher}
+    subject(:sections_as_student_participant) {user.sections_as_student_participant}
 
     before do
       create :follower, section: section1, student_user_id: user.id, user: section_teacher
@@ -94,13 +106,13 @@ class SectionParticipationTest < ActiveSupport::TestCase
 
     context 'when the user is in multiple sections' do
       it 'returns the sections where participation_type is "student"' do
-        _(user.sections_as_student_participant).must_equal [section1]
-        _(user.sections_as_student_participant.length).must_equal 1
+        _(sections_as_student_participant).must_equal [section1]
+        _(sections_as_student_participant.length).must_equal 1
       end
 
       it 'does not return the pl sections' do
-        _(user.sections_as_student_participant).wont_include section2
-        _(user.sections_as_student_participant).wont_include section3
+        _(sections_as_student_participant).wont_include section2
+        _(sections_as_student_participant).wont_include section3
       end
     end
   end
@@ -111,6 +123,7 @@ class SectionParticipationTest < ActiveSupport::TestCase
     let!(:section1) {create :section, user: section_teacher}
     let!(:section2) {create :section, :teacher_participants, user: section_teacher}
     let!(:section3) {create :section, :facilitator_participants, user: section_teacher}
+    subject(:sections_as_pl_participant) {user.sections_as_pl_participant}
 
     before do
       create :follower, section: section1, student_user_id: user.id, user: section_teacher
@@ -120,12 +133,12 @@ class SectionParticipationTest < ActiveSupport::TestCase
 
     context 'when the user is in multiple sections' do
       it 'returns the pl sections' do
-        _(user.sections_as_pl_participant).must_equal [section2, section3]
-        _(user.sections_as_pl_participant.length).must_equal 2
+        _(sections_as_pl_participant).must_equal [section2, section3]
+        _(sections_as_pl_participant.length).must_equal 2
       end
 
       it 'does not return sections where participation type is "student"' do
-        _(user.sections_as_pl_participant).wont_include section1
+        _(sections_as_pl_participant).wont_include section1
       end
     end
   end
