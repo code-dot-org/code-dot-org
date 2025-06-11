893ddd144233ee53e035e174d41d42f9f7cfddb3
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-06-10 17:13:01 -0700

    configure bundle to use deployment mode and execute bundle install as non-root user

--- bin/oneoff/update_ubuntu/02-postupdate.sh
+++ bin/oneoff/update_ubuntu/02-postupdate.sh
@@ -14,8 +14,8 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY
 # Reinstall rmagick to resolve the error:
 #   This installation of RMagick was configured with ImageMagick 6.9.10 but
 #   ImageMagick 6.9.11-60 is in use.
-bundle exec gem uninstall rmagick;
-bundle install;
+su ubuntu bundle exec gem uninstall rmagick;
+su ubuntu bundle install;
 
 # Finally, run a regular build to get everything working again!
 echo "now kick off a regular build with something like 'start-build'";
--- cookbooks/cdo-apps/recipes/bundle_bootstrap.rb
+++ cookbooks/cdo-apps/recipes/bundle_bootstrap.rb
@@ -20,7 +20,9 @@ env = {
   # http://bundler.io/man/bundle-config.1.html
   'BUNDLE_IGNORE_CONFIG' => '1',
   # Avoid writing 'remembered options' to the default local config (./bundle/config).
-  'BUNDLE_APP_CONFIG' => "#{Chef::Config[:file_cache_path]}/.bundle"
+  'BUNDLE_APP_CONFIG' => "#{Chef::Config[:file_cache_path]}/.bundle",
+  # Install gems to vendor/bundle as per https://bundler.io/guides/deploying.html
+  'BUNDLE_DEPLOYMENT' => 'true'
 }
 node.default['cdo-apps']['bundle_env'] = env
 directory(env['BUNDLE_APP_CONFIG']) {owner user; group user}
@@ -39,5 +41,7 @@ execute 'bundle-install' do
   command 'bundle install'
   cwd root
   environment node['cdo-apps']['bundle_env']
+  group user
+  user user
   not_if 'bundle check', cwd: root
 end
