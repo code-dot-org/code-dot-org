cd6ffcc2fd8c0098165436452f397f45d13bba16
Seth Nickell <snickell@gmail.com>
2025-06-18 11:38:53 -1000

    remove redundancy

--- dashboard/config/initializers/devise.rb
+++ dashboard/config/initializers/devise.rb
@@ -373,9 +373,7 @@ end
 
 Rails.application.config.to_prepare do
   # See lib/devise/models/custom_lockable.rb
-  Rails.application.config.to_prepare do
-    unless Devise::Models::Lockable.ancestors.include?(Devise::Models::CustomLockable)
-      Devise::Models::Lockable.prepend(Devise::Models::CustomLockable)
-    end
+  unless Devise::Models::Lockable.ancestors.include?(Devise::Models::CustomLockable)
+    Devise::Models::Lockable.prepend(Devise::Models::CustomLockable)
   end
 end
