e01cffacd6276b9fbc1f481d76dcbc7840074fd8
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-06-23 16:01:16 -0700

    fix 'root running as normal user' implementation

--- bin/oneoff/update_ubuntu/02-postupdate.sh
+++ bin/oneoff/update_ubuntu/02-postupdate.sh
@@ -14,8 +14,9 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY
 # Reinstall rmagick to resolve the error:
 #   This installation of RMagick was configured with ImageMagick 6.9.10 but
 #   ImageMagick 6.9.11-60 is in use.
-su ubuntu bundle exec gem uninstall rmagick;
-su ubuntu bundle install;
+# Bundle should be run as local rather than root user
+sudo -u ubuntu bundle exec gem uninstall rmagick;
+sudo -u ubuntu bundle install;
 
 # Finally, run a regular build to get everything working again!
 echo "now kick off a regular build with something like 'start-build'";
