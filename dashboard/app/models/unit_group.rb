c1c6f951d3c45a0d0324e07526cf78f43f13bb30
David Bailey <davidsbailey@users.noreply.github.com>
2025-06-25 16:10:17 -0700

    allow course edit page to load without course offering

--- dashboard/app/models/unit_group.rb
+++ dashboard/app/models/unit_group.rb
@@ -325,7 +325,7 @@ class UnitGroup < ApplicationRecord
         course_offering_id: course_version&.course_offering&.id,
         course_version_id: course_version&.id,
         course_path: link,
-        course_offering_edit_path: for_edit && course_version ? edit_course_offering_path(course_version.course_offering.key) : nil
+        course_offering_edit_path: for_edit && course_version&.course_offering ? edit_course_offering_path(course_version.course_offering.key) : nil
       }
     end
   end
