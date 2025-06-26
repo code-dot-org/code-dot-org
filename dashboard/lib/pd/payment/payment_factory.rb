b1c6ad35f4b68a387992b50a0c8932ebd794c2d8
Ryan Driscoll <ryan.driscoll@code.org>
2025-06-26 08:46:33 -0600

    Revert "Revert "chore(workshops): deprecate csf course (#66489)" (#66731)"
    This reverts commit a795769e4ccdb303292b222c49be7a95faf8e1c5.

--- dashboard/lib/pd/payment/payment_factory.rb
+++ dashboard/lib/pd/payment/payment_factory.rb
@@ -7,8 +7,6 @@ module Pd::Payment
 
     def self.get_calculator_class(workshop)
       if workshop.funded
-        return PaymentCalculatorCSF if workshop.course == Pd::Workshop::COURSE_CSF
-
         return PaymentCalculatorStandard if [
           Pd::Workshop::COURSE_ECS,
           Pd::Workshop::COURSE_CSP,
