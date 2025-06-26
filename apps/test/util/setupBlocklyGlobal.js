bfd59c205d552ee5e14e7068769fa416d69d8d64
Mike Harvey <mike.harvey@code.org>
2025-06-26 15:27:08 -0400

    try patching keycodes

--- apps/test/util/setupBlocklyGlobal.js
+++ apps/test/util/setupBlocklyGlobal.js
@@ -15,6 +15,22 @@ export default function setBlocklyGlobal() {
   document.body.innerHTML = '<div id="codeApp"><div id="app"></div></div>';
   // locale file requires Blockly as a global
   var googleBlockly = require('blockly/core');
+
+  // Patch KeyCodes if missing (used by plugins like @blockly/keyboard-navigation)
+  googleBlockly.utils ??= {};
+  googleBlockly.utils.KeyCodes ??= {
+    A: 65,
+    S: 83,
+    D: 68,
+    W: 87,
+    SHIFT: 'Shift',
+    ESC: 'Escape',
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40,
+  };
+
   var initializeGoogleBlocklyWrapper = require('../../src/blockly/googleBlocklyWrapper');
   var localeFromCookies = cookies.get('language_') || 'en-US';
   var messages =
