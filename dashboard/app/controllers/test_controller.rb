e15a69b172c1330e8bb85f8bb2813f425a8cf64b
David Bailey <davidsbailey@users.noreply.github.com>
2025-06-26 11:38:01 -0700

    remove assign_script_as_student api route [skip unit]

--- dashboard/app/controllers/test_controller.rb
+++ dashboard/app/controllers/test_controller.rb
@@ -149,36 +149,6 @@ class TestController < ApplicationController
     head :ok
   end
 
-  def assign_script_as_student
-    return unless (user = current_user)
-    script = Unit.find_by_name(params.require(:script_name))
-
-    teacher_email = params[:teacher_email]
-    if teacher_email
-      teacher_user = User.find_by_email(teacher_email)
-    else
-      name = "Fake User"
-      email = "user#{Time.now.to_i}_#{rand(1_000_000)}@test.xx"
-      password = name + "password"
-      attributes = {
-        name: name,
-        email: email,
-        password: password,
-        user_type: "teacher",
-        age: "21+"
-      }
-      teacher_user = User.create!(attributes)
-    end
-
-    # Need to also assign the course if the script is a part of a single-unit course
-    section = script.unit_group&.single_unit_course? ?
-                Section.create(name: "New Section", user: teacher_user, script: script, course_id: script.unit_group.id, participant_type: Curriculum::SharedCourseConstants::PARTICIPANT_AUDIENCE.student) :
-                Section.create(name: "New Section", user: teacher_user, script: script, participant_type: Curriculum::SharedCourseConstants::PARTICIPANT_AUDIENCE.student)
-    section.students << user
-    section.save!
-    head :ok
-  end
-
   def get_i18n_t
     locale = params[:locale] || request.env['cdo.locale']
     render plain: I18n.t(params.require(:key), locale: locale)
