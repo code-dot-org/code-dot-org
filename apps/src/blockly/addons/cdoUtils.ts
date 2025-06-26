b32258311092d96ea8dee5c7bf673582589bd76f
wilkie <david.wilkinson@code.org>
2025-06-26 12:48:36 -0400

    Update cdoUtils.ts

--- apps/src/blockly/addons/cdoUtils.ts
+++ apps/src/blockly/addons/cdoUtils.ts
@@ -642,15 +642,12 @@ export function createBlockLimitMap() {
 export function getSimplifiedStateForFlyout(
   serialization: WorkspaceSerialization
 ) {
-  console.log('getSimplifiedStateForFlyout');
-
   const blocksList = [] as object[];
 
   const {variables, blocks} = serialization;
 
   // Create a map of variable ids and names from the serialization.
   const serializedVariableMap: Map<string, string> = new Map();
-  console.log('BLOCKLY LOCALIZE', variables, serializedVariableMap);
   variables?.forEach(variable => {
     serializedVariableMap.set(variable.id, variable.name);
   });
@@ -837,7 +834,6 @@ export function getCodeFromBlockXmlSource(blockXmlString: string) {
 // This is used in order to merge XML toolbox blocks with the dynamically created
 // blocks in auto-populated categories, such as Behaviors, Functions, and Variables.
 export function getCategoryBlocksJson(category: string) {
-  console.log('BLOCKLY LOCALIZE getCategoryBlocksJson', category);
   const levelToolboxBlocks = getLevelToolboxBlocks(category);
   if (!levelToolboxBlocks?.querySelector('xml')?.hasChildNodes()) {
     return [];
@@ -851,7 +847,6 @@ export function getCategoryBlocksJson(category: string) {
 
   // Localize the flyout variables
   // These are sourced from the XML and are always in the source language
-  console.log('BLOCKLY LOCALIZE blocks converted', blocksConvertedJson);
   (blocksConvertedJson.variables || []).forEach(variable => {
     Blockly.SourceVariables[variable.id] ||= variable.name;
     const oldName = Blockly.SourceVariables[variable.id];
@@ -862,7 +857,7 @@ export function getCategoryBlocksJson(category: string) {
     if (newName.startsWith('[variable] ')) {
       newName = newName.substring(11);
     } else {
-      console.log(
+      console.error(
         'Global variable translation does not have the [variable] tag (category block variable)',
         oldName,
         newName
