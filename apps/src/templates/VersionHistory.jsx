141a50dd4129c36554e4aeb783dce000f527bda3
Erin Bond <erin.bond@code.org>
2025-06-30 17:16:34 -0400

    WIP to add code diff details

--- apps/src/templates/VersionHistory.jsx
+++ apps/src/templates/VersionHistory.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 import project from '@cdo/apps/code-studio/initApp/project';
 import firehoseClient from '@cdo/apps/metrics/firehose';
+import CollapsibleSection from '@cdo/apps/templates/CollapsibleSection';
 import i18n from '@cdo/locale';
 
 import {sources as sourcesApi, files as filesApi} from '../clientApi';
 import * as utils from '../utils';
 
+import VersionHistoryDetails from './VersionHistoryDetails';
 import VersionRow from './VersionRow';
 
 /**
@@ -226,6 +228,9 @@ export default class VersionHistory extends React.Component {
               </tbody>
             </table>
           </div>
+          <CollapsibleSection headerContent="Details">
+            <VersionHistoryDetails versions={this.state.versions} />
+          </CollapsibleSection>
         </div>
       );
     }
