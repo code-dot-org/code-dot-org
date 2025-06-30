26e06789bf825293e688ee164be280168f4b7c01
David Bailey <davidsbailey@users.noreply.github.com>
2025-06-30 09:16:36 -0700

    update assign_course_and_unit_as_student to take unit position

--- dashboard/test/ui/features/step_definitions/steps.rb
+++ dashboard/test/ui/features/step_definitions/steps.rb
@@ -1165,11 +1165,11 @@ Given(/^I am assigned to unit "([^"]*)"(?: with teacher "([^"]*)")?$/) do |scrip
   )
 end
 
-Given(/^I am assigned to course "([^"]*)" and unit "([^"]*)"(?: with teacher "([^"]*)")?$/) do |course_name, script_name, teacher_name|
+Given(/^I am assigned to course "([^"]*)" unit (\d+)(?: with teacher "([^"]*)")?$/) do |course_name, unit_position, teacher_name|
   browser_request(
     url: '/api/test/assign_course_and_unit_as_student',
     method: 'POST',
-    body: {script_name: script_name, course_name: course_name, teacher_email: teacher_name ? (@users[teacher_name][:email]).to_s : nil}
+    body: {course_name: course_name, unit_position: unit_position, teacher_email: teacher_name ? (@users[teacher_name][:email]).to_s : nil}
   )
 end
 
