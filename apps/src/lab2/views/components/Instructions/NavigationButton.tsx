c912b202780f70cf2453e40a30b01940934c269e
Molly Moen <molly@code.org>
2025-07-01 13:03:42 -0700

    update icon and scrollbar

--- apps/src/lab2/views/components/Instructions/NavigationButton.tsx
+++ apps/src/lab2/views/components/Instructions/NavigationButton.tsx
@@ -1,5 +1,6 @@
 import {Button} from '@code-dot-org/component-library/button';
 import {ComponentSizeXSToL} from '@code-dot-org/component-library/common/types';
+import {FontAwesomeV6IconProps} from '@code-dot-org/component-library/fontAwesomeV6Icon';
 import React from 'react';
 
 import {sendSubmitReport} from '@cdo/apps/code-studio/progressRedux';
@@ -96,6 +97,10 @@ const ContinueButton: React.FC<ContinueButtonProps> = ({
       ? (['secondary', 'black'] as const)
       : (['primary', 'purple'] as const);
 
+  const iconRight: FontAwesomeV6IconProps | undefined = hasNextLevel
+    ? {iconName: 'arrow-right', iconStyle: 'solid'}
+    : undefined;
+
   if (!canShow) {
     return null;
   }
@@ -105,7 +110,7 @@ const ContinueButton: React.FC<ContinueButtonProps> = ({
       id="instructions-continue-button"
       {...{className, size, text, type, color}}
       onClick={() => dispatch(continueOrFinishLesson())}
-      iconRight={{iconName: 'arrow-right', iconStyle: 'solid'}}
+      iconRight={iconRight}
     />
   );
 };
