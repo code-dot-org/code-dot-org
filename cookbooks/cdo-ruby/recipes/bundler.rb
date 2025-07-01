792b62fcc8fd7d2bdf0da3811f776a0a1dbf6cf0
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-07-01 11:13:49 -0700

    add basic chef script to automatically re-own existing .bundle directories

--- cookbooks/cdo-ruby/recipes/bundler.rb
+++ cookbooks/cdo-ruby/recipes/bundler.rb
@@ -2,6 +2,18 @@
 include_recipe 'apt'
 apt_package 'git'
 
+# In preparation for no longer running `bundle install` as root, re-own the
+# existing root-owned .bundle directories on our persisitent managed servers.
+#
+# TODO infra: remove this block once we've updated all servers
+root_dir = File.join(node[:home], node.chef_environment)
+['dashboard', 'cookbooks'].each do |subdir|
+  execute "change ownership of #{subdir} bundle directory" do
+    command "chown -R #{node[:user]}:#{node[:user]} #{File.join(root_dir, subdir, '.bundle')}"
+    user 'root'
+  end
+end
+
 gem_package 'bundler' do
   action :upgrade
   version node['cdo-ruby']['bundler_version']
