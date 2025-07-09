8b6affdc3917623f18ec4a7dcbf8abfb05c92a64
Alice Fisher <alice.fisher@code.org>
2025-07-09 14:03:43 +0000

    add comment

--- apps/src/pythonlab/pyodideWebWorker.ts
+++ apps/src/pythonlab/pyodideWebWorker.ts
@@ -28,6 +28,8 @@ async function loadPyodideAndPackages() {
     env: {
       HOME: `/${HOME_FOLDER}/`,
     },
+    // Remove all JS globals so Python can’t call browser APIs (like fetch) unless we
+    // explicitly add them.
     jsglobals: {},
   });
   pyodide.setStdout(getStreamHandlerOptions('sysout'));
