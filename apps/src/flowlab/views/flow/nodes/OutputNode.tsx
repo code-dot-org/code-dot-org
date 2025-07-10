14f26638235053a4babb93283e3938427f7b4ca7
breville <brendan@code.org>
2025-07-10 19:22:10 +0000

    Markdown output support

--- apps/src/flowlab/views/flow/nodes/OutputNode.tsx
+++ apps/src/flowlab/views/flow/nodes/OutputNode.tsx
@@ -6,6 +6,8 @@ import {
 } from '@xyflow/react';
 import React, {memo} from 'react';
 
+import SafeMarkdown from '@cdo/apps/templates/SafeMarkdown';
+
 import {isTextNode, type MyNode} from '../../../flow/initialElements';
 
 // This node simply displays any text sent to its input handle.
@@ -19,17 +21,22 @@ function OutputNode() {
   );
   const textNodes = nodesData.filter(isTextNode);
 
+  const text =
+    textNodes.length > 0
+      ? textNodes.map(({data}, i) =>
+          data && 'text' in data ? (
+            <div key={i}>
+              {<SafeMarkdown openExternalLinksInNewTab markdown={data.text} />}
+            </div>
+          ) : null
+        )
+      : 'none';
+
   return (
     <div style={{maxWidth: 400}}>
       <Handle type="target" position={Position.Left} />
       <div>Output</div>
-      <div>
-        {textNodes.length > 0
-          ? textNodes.map(({data}, i) =>
-              data && 'text' in data ? <div key={i}>{data.text}</div> : null
-            )
-          : 'none'}
-      </div>
+      <div>{text}</div>
     </div>
   );
 }
