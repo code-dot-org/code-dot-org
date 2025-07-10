8ecb6a3a67913871653eb1991e82c6f6e855278a
Molly Moen <molly@code.org>
2025-07-10 16:37:31 -0700

    delete unused code

--- apps/test/unit/codebridge/hooks/useSourceTest.tsx
+++ apps/test/unit/codebridge/hooks/useSourceTest.tsx
@@ -1,4 +1,4 @@
-import {act, renderHook} from '@testing-library/react-hooks';
+import {renderHook} from '@testing-library/react-hooks';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {Store} from 'redux';
@@ -22,7 +22,7 @@ import {
 import {
   nonValidatedLevelProperties,
   smallProject,
-  smallProjectSources,
+  // smallProjectSources,
   templateBackedLevelProperties,
 } from '../test-files';
 import {mockAppOptions} from '../test_utils';
@@ -85,42 +85,42 @@ describe('useSource', () => {
     return result.current;
   }
 
-  it('set project saves to project manager in standard mode', () => {
-    const {setProject} = renderDefault();
-    act(() => {
-      setProject(smallProjectSources);
-    });
-    expect(mockedProjectManager.save).toHaveBeenCalled();
-  });
+  // it('set project saves to project manager in standard mode', () => {
+  //   const {setProject} = renderDefault();
+  //   act(() => {
+  //     setProject(smallProjectSources);
+  //   });
+  //   expect(mockedProjectManager.save).toHaveBeenCalled();
+  // });
 
-  it('does not save to project manager in readonly mode', () => {
-    store.dispatch(
-      setChannel({
-        id: '1',
-        name: '1',
-        // If the user is not the owner, we are in readonly mode.
-        isOwner: false,
-        projectType: 'pythonlab',
-        publishedAt: null,
-        createdAt: '',
-        updatedAt: '',
-      })
-    );
-    const {setProject} = renderDefault();
-    act(() => {
-      setProject(smallProjectSources);
-    });
-    expect(mockedProjectManager.save).toHaveBeenCalledTimes(0);
-  });
+  // it('does not save to project manager in readonly mode', () => {
+  //   store.dispatch(
+  //     setChannel({
+  //       id: '1',
+  //       name: '1',
+  //       // If the user is not the owner, we are in readonly mode.
+  //       isOwner: false,
+  //       projectType: 'pythonlab',
+  //       publishedAt: null,
+  //       createdAt: '',
+  //       updatedAt: '',
+  //     })
+  //   );
+  //   const {setProject} = renderDefault();
+  //   act(() => {
+  //     setProject(smallProjectSources);
+  //   });
+  //   expect(mockedProjectManager.save).toHaveBeenCalledTimes(0);
+  // });
 
-  it('sets project as edited on first edit', () => {
-    expect(store.getState().lab2Project.hasEdited).toBe(false);
-    const {setProject} = renderDefault();
-    act(() => {
-      setProject(smallProjectSources);
-    });
-    expect(store.getState().lab2Project.hasEdited).toBe(true);
-  });
+  // it('sets project as edited on first edit', () => {
+  //   expect(store.getState().lab2Project.hasEdited).toBe(false);
+  //   const {setProject} = renderDefault();
+  //   act(() => {
+  //     setProject(smallProjectSources);
+  //   });
+  //   expect(store.getState().lab2Project.hasEdited).toBe(true);
+  // });
 
   it('returns level start sources in start mode', () => {
     mockAppOptions({editBlocks: 'start_sources'});
