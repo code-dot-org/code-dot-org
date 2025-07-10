cd4130ad9b1dcbb69dff2b2564b1a713ea970f6d
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-07-10 13:39:58 -0700

    Revert "feat(marketing): refactor Overline component to use MUI and theme upd…"
    This reverts commit 270fa22ef162f212707675389ea8a5c510fd744d.

--- frontend/apps/marketing/src/components/contentful/overline/Overline.tsx
+++ frontend/apps/marketing/src/components/contentful/overline/Overline.tsx
@@ -1,32 +1,39 @@
-import Typography from '@mui/material/Typography';
+import classNames from 'classnames';
 import React, {ReactNode} from 'react';
 
-import {FONT_SIZE} from '@/components/common/constants';
+import {ComponentSizeXSToL} from '@code-dot-org/component-library/common/types';
 import {
-  ComponentSize,
-  RemoveMarginBottomProps,
-} from '@/components/common/types';
+  default as Typography,
+  VisualAppearance,
+} from '@code-dot-org/component-library/typography';
+
+import {RemoveMarginBottomProps} from '@/components/common/types';
+
+import moduleStyles from './overline.module.scss';
+
+type OverlineVisualAppearance = Extract<
+  VisualAppearance,
+  'overline-one' | 'overline-two' | 'overline-three'
+>;
 
 type OverlineProps = RemoveMarginBottomProps & {
   /** Overline content */
   children: ReactNode;
   /** Overline size */
-  size: Exclude<ComponentSize, 'xs'>;
+  size: Exclude<ComponentSizeXSToL, 'xs'>;
   /** Overline color */
   color: 'primary' | 'secondary';
   /** ClassName passed by Contentful to apply styles that are set through Contentful native editor*/
   className?: string;
 };
 
-// Define font sizes based on the existing Contentful Overline size values
-// that were set before using the MUI Typography component.
-//
-// The mapping below intentionally does not match the ComponentSize type one-to-one.
-// This is to provide clearer dropdown options in the Contentful editor.
-const overlineFontSizes: Record<Exclude<ComponentSize, 'xs'>, string> = {
-  s: FONT_SIZE.xxs,
-  m: FONT_SIZE.xs,
-  l: FONT_SIZE.s,
+const overlineSizeToVisualAppearance: Record<
+  Exclude<ComponentSizeXSToL, 'xs'>,
+  OverlineVisualAppearance
+> = {
+  s: 'overline-three',
+  m: 'overline-two',
+  l: 'overline-one',
 };
 
 const Overline: React.FunctionComponent<OverlineProps> = ({
@@ -38,12 +45,14 @@ const Overline: React.FunctionComponent<OverlineProps> = ({
 }) => {
   return (
     <Typography
-      className={className}
-      component="p"
-      variant="overline"
-      color={color === 'primary' ? 'primary.main' : 'text.secondary'}
-      fontSize={overlineFontSizes[size]}
-      gutterBottom={!removeMarginBottom}
+      className={classNames(
+        moduleStyles.overline,
+        moduleStyles[`overline-color-${color}`],
+        removeMarginBottom && moduleStyles['overline-removeMarginBottom'],
+        className,
+      )}
+      semanticTag="p"
+      visualAppearance={overlineSizeToVisualAppearance[size]}
     >
       {children}
     </Typography>
