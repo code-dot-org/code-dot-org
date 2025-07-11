32b296d2f08c65b282b57936b50a257f06f3e9ae
breville <brendan@code.org>
2025-07-11 19:45:24 +0000

    Levelbuilder users only

--- dashboard/app/controllers/flowlab_controller.rb
+++ dashboard/app/controllers/flowlab_controller.rb
@@ -1,5 +1,8 @@
 class FlowlabController < ApplicationController
   def index
+    # Ensure that user has levelbuilder permissions.
+    authorize! :create, Level
+
     view_options(no_footer: true)
   end
 end
