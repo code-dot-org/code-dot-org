aaa69743f2596ae5f8dfc272f73dddb6e2389930
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-07-11 09:11:49 -0700

    feat(testing): isolate testing to development env per branch

--- frontend/apps/marketing/tests/e2e/scripts/commands/fork.ts
+++ frontend/apps/marketing/tests/e2e/scripts/commands/fork.ts
@@ -1,5 +1,5 @@
 import {readJSON} from 'fs-extra';
-import os from 'node:os';
+import {simpleGit} from 'simple-git';
 
 import {SNAPSHOT_DIR, ALL_THE_THINGS_ENTRY_ID} from '../config';
 import {CreateOrUpdateEntryType, SerializedComponent} from '../types';
@@ -10,9 +10,10 @@ export async function fork(createOrUpdateEntry: CreateOrUpdateEntryType) {
   const snapshotData = serializedComponent.entryContent;
 
   // Change the slug and title for clarity
-  const username = os.userInfo().username;
-  const title = `[${username}] All The Things - ${new Date().toISOString()}`;
-  const slug = `/engineering/all-the-things-${username}-${Date.now()}`;
+  const branchName = (await simpleGit().branch()).current.replaceAll('/', '-');
+
+  const title = `[Fork] All The Things - ${branchName}`;
+  const slug = `/engineering/all-the-things-${branchName}`;
 
   snapshotData.fields.slug['en-US'] = slug;
   snapshotData.fields.title['en-US'] = title;
