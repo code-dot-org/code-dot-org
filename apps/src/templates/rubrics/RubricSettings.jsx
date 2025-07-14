8a7698e49fb91885e2fa4b1ad54a7002d18d1ef5
Liam Frye-Mason <liam.fryemason@code.org>
2025-07-14 15:39:42 -0700

    Remove another section

--- apps/src/templates/rubrics/RubricSettings.jsx
+++ apps/src/templates/rubrics/RubricSettings.jsx
@@ -282,45 +282,47 @@ function RubricSettings({
       </div>
 
       <div className={style.settingsContent}>
-        <div className={style.settingsGroup}>
-          <Heading4>{i18n.aiAssessment()}</Heading4>
-          <div className={style.settingsContainers}>
-            <div className={style.runAiAllStatuses}>
-              <BodyTwoText>
-                <StrongText>{summaryText()}</StrongText>
-              </BodyTwoText>
-              {statusAllText() && (
-                <BodyTwoText className="uitest-eval-status-all-text">
-                  {statusAllText()}
+        {teacherHasEnabledAi && (
+          <div className={style.settingsGroup}>
+            <Heading4>{i18n.aiAssessment()}</Heading4>
+            <div className={style.settingsContainers}>
+              <div className={style.runAiAllStatuses}>
+                <BodyTwoText>
+                  <StrongText>{summaryText()}</StrongText>
                 </BodyTwoText>
-              )}
-            </div>
-            <Button
-              className="uitest-run-ai-assessment-all"
-              text={i18n.runAiAssessmentClass()}
-              color={Button.ButtonColor.brandSecondaryDefault}
-              onClick={handleRunAiAssessmentAll}
-              style={{margin: 0}}
-              disabled={statusAll !== STATUS_ALL.READY}
-            >
-              {statusAll === STATUS_ALL.EVALUATION_PENDING && (
-                <i className="fa fa-spinner fa-spin" />
-              )}
-            </Button>
-            <div className={style.detailsGroup}>
-              <BodyTwoText
-                className={
-                  displayDetails ? style.detailsVisible : style.detailsHidden
-                }
+                {statusAllText() && (
+                  <BodyTwoText className="uitest-eval-status-all-text">
+                    {statusAllText()}
+                  </BodyTwoText>
+                )}
+              </div>
+              <Button
+                className="uitest-run-ai-assessment-all"
+                text={i18n.runAiAssessmentClass()}
+                color={Button.ButtonColor.brandSecondaryDefault}
+                onClick={handleRunAiAssessmentAll}
+                style={{margin: 0}}
+                disabled={statusAll !== STATUS_ALL.READY}
               >
-                {i18n.aiEvaluationDetails()}
-              </BodyTwoText>
-              <Link onClick={showHideDetails}>
-                {displayDetails ? i18n.hideDetails() : i18n.showDetails()}
-              </Link>
+                {statusAll === STATUS_ALL.EVALUATION_PENDING && (
+                  <i className="fa fa-spinner fa-spin" />
+                )}
+              </Button>
+              <div className={style.detailsGroup}>
+                <BodyTwoText
+                  className={
+                    displayDetails ? style.detailsVisible : style.detailsHidden
+                  }
+                >
+                  {i18n.aiEvaluationDetails()}
+                </BodyTwoText>
+                <Link onClick={showHideDetails}>
+                  {displayDetails ? i18n.hideDetails() : i18n.showDetails()}
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
+        )}
 
         <div className={style.settingsGroup}>
           <Heading4>{i18n.rubricSummaryClassScore()}</Heading4>
