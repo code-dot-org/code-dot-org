fce0d97fc91aba85dabe08a7d3ab2ada7719d302
wilkie <david.wilkinson@code.org>
2025-07-15 23:10:49 -0400

    Replaces plotly with Apache ECharts and adds student listing to tooltip.

--- apps/src/templates/rubrics/RubricClassSummary.tsx
+++ apps/src/templates/rubrics/RubricClassSummary.tsx
@@ -1,18 +1,46 @@
-import classnames from 'classnames';
-import React, {useMemo} from 'react';
-import Plot from 'react-plotly.js';
-
 import {
   BodyTwoText,
   Heading4,
   StrongText,
-} from '@cdo/apps/componentLibrary/typography';
+} from '@code-dot-org/component-library/typography';
+import classnames from 'classnames';
+// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+import {PieChart} from 'echarts/charts';
+import {
+  // @ts-expect-error - this is OK because the bundler will handle the ambiguity
+  GridComponent,
+  // @ts-expect-error - this is OK because the bundler will handle the ambiguity
+  LegendComponent,
+  // @ts-expect-error - this is OK because the bundler will handle the ambiguity
+  TooltipComponent,
+  // @ts-expect-error - this is OK because the bundler will handle the ambiguity
+} from 'echarts/components';
+// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+import * as echarts from 'echarts/core';
+// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+import {LabelLayout} from 'echarts/features';
+// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+import {SVGRenderer} from 'echarts/renderers';
+import ReactEChartsCore from 'echarts-for-react/lib/core';
+import React, {useMemo} from 'react';
+
 import Spinner from '@cdo/apps/sharedComponents/Spinner';
+import {useAppSelector} from '@cdo/apps/util/reduxHooks';
 import i18n from '@cdo/locale';
 
 import {Rubric, TeacherEvaluations} from './types';
 
 import style from './rubrics.module.scss';
+
+// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+echarts.use([
+  SVGRenderer,
+  LabelLayout,
+  GridComponent,
+  LegendComponent,
+  TooltipComponent,
+  PieChart,
+]);
 
 const COLORS = {
   NOT_STARTED: '#D1D4D8',
@@ -32,6 +60,9 @@ const RubricClassSummary: React.FunctionComponent<RubricClassSummaryProps> = ({
   teacherEval,
 }) => {
   const learningGoals = rubric.learningGoals;
+  const students = useAppSelector(
+    state => state.teacherSections.selectedStudents
+  );
 
   return useMemo(
     () => (
@@ -39,30 +70,49 @@ const RubricClassSummary: React.FunctionComponent<RubricClassSummaryProps> = ({
         <Heading4>{i18n.rubricClassroomScoreSummary()}</Heading4>
         <div className={style.summaryContainer}>
           {learningGoals.map(learningGoal => {
-            // Get the assigned score for every student for the given learning goal
-            const results = (teacherEval || []).map(
-              studentEval => studentEval[learningGoal.id]
-            );
             // The possible scores
             const labels = [
-              'Not Started',
+              'Unsubmitted',
               'None',
               'Limited',
               'Convincing',
               'Extensive',
             ];
-            // Count the occurances for each score for this learning goal
-            const values = labels.map(label =>
-              results.reduce(
-                (acc, value) =>
-                  value === label || (value === '' && label === 'Not Started')
-                    ? acc + 1
-                    : acc,
-                0
-              )
+
+            // Get the assigned score for every student for the given learning goal
+            const results = (teacherEval || []).map(
+              studentEval => studentEval[learningGoal.id] || labels[0]
             );
 
-            const haveData = values.some(n => n !== 0);
+            // Count the occurances for each score for this learning goal
+            const values = labels.map(label => {
+              const value = results.reduce(
+                (acc, value) => (value === label ? acc + 1 : acc),
+                0
+              );
+
+              return {
+                name: label,
+                value,
+              };
+            });
+
+            // Get the student names that match each data point on the graph so we can
+            // show them within the popover tooltip.
+            const names = labels.map(label => {
+              return (
+                results
+                  .map((value, i) => [value, i])
+                  .filter(([value, _]) => value === label) as [string, number][]
+              ).map(
+                ([_, i]) =>
+                  `${students[i]?.name || ''}${
+                    students[i]?.familyName ? ` ${students[i].familyName}` : ''
+                  }`
+              );
+            });
+
+            const haveData = values.some(n => n.value !== 0);
 
             return (
               <div
@@ -77,63 +127,73 @@ const RubricClassSummary: React.FunctionComponent<RubricClassSummaryProps> = ({
                 </BodyTwoText>
                 {!haveData && <Spinner />}
                 {haveData && (
-                  <Plot
-                    data={[
-                      {
-                        type: 'pie',
-                        // The value (number of students) for each pie wedge
-                        values: values,
-                        // The labels corresponding to each value
-                        labels: labels,
-                        // The actual text to display on each pie wedge
-                        texttemplate:
-                          '%{label}<br><b>%{value}</b> <b>(%{percent:.0%})</b>',
-                        // The position of the text label. When specified, it will place a label even if
-                        // the value is 0, so we need to handle that accordingly:
-                        //   outside: external to the wedge
-                        //   none: do not display the label (we want this for when there's no students)
-                        textposition: values.map(value =>
-                          value === 0 ? 'none' : 'outside'
-                        ),
-                        // Render the wedges in the order they are specified instead of sorting them
-                        sort: false,
-                        // Use the given colors for the corresponding values, given in the order the
-                        // values are also given.
-                        marker: {
-                          colors: [
-                            COLORS.NOT_STARTED,
-                            COLORS.NONE,
-                            COLORS.LIMITED,
-                            COLORS.CONVINCING,
-                            COLORS.EXTENSIVE,
-                          ],
-                          // The outline around all shapes
-                          line: {
-                            color: '#FFF',
-                            width: 2,
+                  <ReactEChartsCore
+                    echarts={echarts}
+                    style={{
+                      height: '250px',
+                      width: '100%',
+                    }}
+                    option={{
+                      renderer: 'svg',
+                      avoidLabelOverlap: true,
+                      color: Object.values(COLORS),
+                      legend: {
+                        show: true,
+                        bottom: 0,
+                      },
+                      series: [
+                        {
+                          name: learningGoal.learningGoal,
+                          type: 'pie',
+                          radius: '70%',
+                          stillShowZeroSum: false,
+                          data: values,
+                          center: ['50%', '37.5%'],
+                          label: {
+                            show: false,
+                          },
+                          emphasis: {
+                            label: {
+                              show: false,
+                            },
+                            labelLine: {
+                              show: false,
+                            },
+                          },
+                          labelLine: {
+                            show: false,
                           },
                         },
-                        // Shrinks the pie chart in extreme cases to ensure labels fit nicely
-                        automargin: true,
+                      ],
+                      tooltip: {
+                        confine: true,
+                        trigger: 'item',
+                        formatter: (params: {
+                          data: {
+                            name: string;
+                            value: number;
+                          };
+                          percent: number;
+                          seriesName: string;
+                          dataIndex: number;
+                          color: string;
+                        }) =>
+                          [
+                            // Tooltip firsts prints the name of the graph essentially
+                            `<strong>${params.seriesName}</strong>`,
+                            // Then the label and value
+                            `<strong style="color: ${params.color};">${
+                              params.data.name
+                            }</strong>: ${
+                              params.data.value
+                            } (${params.percent.toFixed(2)}%)`,
+                            // Then a set of student names
+                            ...names[params.dataIndex],
+                          ].join('<br/>'),
+                        axisPointer: {
+                          type: 'shadow',
+                        },
                       },
-                    ]}
-                    layout={{
-                      showlegend: false,
-                      width: 315,
-                      height: 170,
-                      hovermode: false,
-                      font: {
-                        size: 10,
-                      },
-                      margin: {
-                        b: 24,
-                        l: 63,
-                        t: 24,
-                        r: 63,
-                      },
-                    }}
-                    config={{
-                      displayModeBar: false,
                     }}
                   />
                 )}
@@ -143,7 +203,7 @@ const RubricClassSummary: React.FunctionComponent<RubricClassSummaryProps> = ({
         </div>
       </div>
     ),
-    [teacherEval, learningGoals]
+    [teacherEval, learningGoals, students]
   );
 };
 
