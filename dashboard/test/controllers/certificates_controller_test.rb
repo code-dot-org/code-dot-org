8d6632db528e437bc6c7660b4fd5c0b7b1383d7a
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-07-16 13:20:43 +0300

    CMS-736: Update CertificatesControllerTest

--- dashboard/test/controllers/certificates_controller_test.rb
+++ dashboard/test/controllers/certificates_controller_test.rb
@@ -7,6 +7,10 @@ class CertificatesControllerTest < ActionController::TestCase
     @teacher.freeze
   end
 
+  before do
+    CDO.stubs(:default_scheme).returns('http:')
+  end
+
   test 'can show given name and course' do
     data = {name: 'student', course: 'hourofcode'}
     encoded_params = Base64.urlsafe_encode64(data.to_json)
@@ -30,7 +34,7 @@ class CertificatesControllerTest < ActionController::TestCase
     encoded_params = Base64.urlsafe_encode64(data.to_json)
     get :show, params: {encoded_params: encoded_params}
     assert_response :success
-    expected_image_url = ApplicationController.helpers.image_url('certificates/hour_of_code_certificate.jpg', host: 'https://test-studio.code.org')
+    expected_image_url = ApplicationController.helpers.image_url('certificates/hour_of_code_certificate.jpg', host: 'http://test-studio.code.org')
     response_data = JSON.parse(css_select('script[data-certificate]').first.attribute('data-certificate').to_s)
     assert_equal expected_image_url, response_data['imageUrl']
   end
@@ -40,7 +44,7 @@ class CertificatesControllerTest < ActionController::TestCase
     encoded_params = Base64.urlsafe_encode64(data.to_json)
     get :show, params: {encoded_params: encoded_params}
     assert_response :success
-    expected_image_url = ApplicationController.helpers.image_url('certificates/MC_Hour_Of_Code_Certificate.png', host: 'https://test-studio.code.org')
+    expected_image_url = ApplicationController.helpers.image_url('certificates/MC_Hour_Of_Code_Certificate.png', host: 'http://test-studio.code.org')
     response_data = JSON.parse(css_select('script[data-certificate]').first.attribute('data-certificate').to_s)
     assert_equal expected_image_url, response_data['imageUrl']
   end
@@ -63,7 +67,7 @@ class CertificatesControllerTest < ActionController::TestCase
   test 'shows static image for blank certificate' do
     get :blank
     assert_response :success
-    expected_image_url = ApplicationController.helpers.image_url('certificates/hour_of_code_certificate.jpg', host: 'https://test-studio.code.org')
+    expected_image_url = ApplicationController.helpers.image_url('certificates/hour_of_code_certificate.jpg', host: 'http://test-studio.code.org')
     response_data = JSON.parse(css_select('script[data-certificate]').first.attribute('data-certificate').to_s)
     assert_equal expected_image_url, response_data['imageUrl']
   end
@@ -76,7 +80,7 @@ class CertificatesControllerTest < ActionController::TestCase
     sign_in @teacher
     get :batch
     assert_response :success
-    expected_image_url = ApplicationController.helpers.image_url('certificates/hour_of_code_certificate.jpg', host: 'https://test-studio.code.org')
+    expected_image_url = ApplicationController.helpers.image_url('certificates/hour_of_code_certificate.jpg', host: 'http://test-studio.code.org')
     response_data = JSON.parse(css_select('script[data-certificate]').first.attribute('data-certificate').to_s)
     assert_equal 'hourofcode', response_data['courseName']
     assert_equal expected_image_url, response_data['imageUrl']
@@ -91,7 +95,7 @@ class CertificatesControllerTest < ActionController::TestCase
     encoded_course_name = Base64.urlsafe_encode64('oceans')
     get :batch, params: {course: encoded_course_name}
     assert_response :success
-    expected_image_url = ApplicationController.helpers.image_url('certificates/oceans_hoc_certificate.png', host: 'https://test-studio.code.org')
+    expected_image_url = ApplicationController.helpers.image_url('certificates/oceans_hoc_certificate.png', host: 'http://test-studio.code.org')
     response_data = JSON.parse(css_select('script[data-certificate]').first.attribute('data-certificate').to_s)
     assert_equal 'oceans', response_data['courseName']
     assert_equal expected_image_url, response_data['imageUrl']
