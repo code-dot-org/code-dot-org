448109a7d504d243c5ea5159f9963a34395296a5
wilkie <david.wilkinson@code.org>
2025-07-16 11:41:12 -0400

    Adds ignores instead of ts-expect-error unfortunately.

--- apps/src/templates/rubrics/RubricClassSummary.tsx
+++ apps/src/templates/rubrics/RubricClassSummary.tsx
@@ -5,24 +5,43 @@ import {
   StrongText,
 } from '@code-dot-org/component-library/typography';
 import classnames from 'classnames';
-// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+// Our build system bundles typescript in a way that allows importing these types
+// of packages without require, but the type checker doesn't know that, so it
+// errors when linting/type-checking. So we ignore the type error and then ignore
+// the lint error about using ts-ignore.
+//
+// This is generally because ECharts is built as modules and the type checker
+// believes incorrectly that we are building our application as common-js.
+//
+// When our build system improves such that we are building modules first-class,
+// we can re-visit this.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - this is OK because the bundler will handle the ambiguity
 import {BarChart, PieChart} from 'echarts/charts';
 import {
-  // @ts-expect-error - this is OK because the bundler will handle the ambiguity
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - this is OK because the bundler will handle the ambiguity
   GridComponent,
-  // @ts-expect-error - this is OK because the bundler will handle the ambiguity
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - this is OK because the bundler will handle the ambiguity
   LegendComponent,
-  // @ts-expect-error - this is OK because the bundler will handle the ambiguity
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - this is OK because the bundler will handle the ambiguity
   PolarComponent,
-  // @ts-expect-error - this is OK because the bundler will handle the ambiguity
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - this is OK because the bundler will handle the ambiguity
   TooltipComponent,
-  // @ts-expect-error - this is OK because the bundler will handle the ambiguity
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - this is OK because the bundler will handle the ambiguity
 } from 'echarts/components';
-// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - this is OK because the bundler will handle the ambiguity
 import * as echarts from 'echarts/core';
-// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - this is OK because the bundler will handle the ambiguity
 import {LabelLayout} from 'echarts/features';
-// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - this is OK because the bundler will handle the ambiguity
 import {SVGRenderer} from 'echarts/renderers';
 import ReactEChartsCore from 'echarts-for-react/lib/core';
 import React, {useMemo, useState} from 'react';
@@ -35,7 +54,8 @@ import {Rubric, TeacherEvaluations} from './types';
 
 import style from './rubrics.module.scss';
 
-// @ts-expect-error - this is OK because the bundler will handle the ambiguity
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - this is OK because the bundler will handle the ambiguity
 echarts.use([
   SVGRenderer,
   LabelLayout,
