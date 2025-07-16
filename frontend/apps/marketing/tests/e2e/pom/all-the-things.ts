6b9399fe5fc9f471baa80e6402e7ced3eba7e389
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-07-16 10:34:50 -0700

    Revert "try finding sections based on the Contentful section div"
    This reverts commit 8cbf257cd03625c765ba22e1e6ae3a2955a929fa.

--- frontend/apps/marketing/tests/e2e/pom/all-the-things.ts
+++ frontend/apps/marketing/tests/e2e/pom/all-the-things.ts
@@ -65,8 +65,6 @@ export class AllTheThingsPage extends MarketingPage {
     });
 
     // Go through the top-level sections, finding the one that has this heading
-    return this.page
-      .locator('contentful-container')
-      .filter({has: headingLocator});
+    return this.page.locator('section').filter({has: headingLocator});
   }
 }
