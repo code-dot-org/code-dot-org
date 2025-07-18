ffe98c496db41c946945e7d3fed1778057825843
Alice Fisher <alice.fisher@code.org>
2025-07-18 21:44:32 +0000

    add report abuse tests

--- dashboard/app/controllers/report_abuse_controller.rb
+++ dashboard/app/controllers/report_abuse_controller.rb
@@ -100,9 +100,10 @@ class ReportAbuseController < ApplicationController
 
   # POST /v3/channels/:channel_id/abuse/image
   def update_abuse_image_moderation
+    return head :unauthorized unless current_user
+
     begin
       value = Projects.new(get_storage_id).increment_abuse(params[:channel_id], 15, current_user&.project_validator?)
-      puts "value #{value}"
     rescue ArgumentError, OpenSSL::Cipher::CipherError
       raise ActionController::BadRequest.new, "Bad channel_id"
     end
