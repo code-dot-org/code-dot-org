77a20b2a868821dc96304c7e9330f5d410f81ae3
Alice Fisher <alice.fisher@code.org>
2025-07-18 21:26:13 +0000

    add client call to abuse increment

--- apps/src/p5lab/AnimationPicker/AnimationPicker.jsx
+++ apps/src/p5lab/AnimationPicker/AnimationPicker.jsx
@@ -172,9 +172,17 @@ class AnimationPicker extends React.Component {
       'Content-Type': file.type,
     })
       .then(response => response.json())
-      .then(json =>
-        console.log('In AnimationPickerImage moderation rating:', json.rating)
-      )
+      .then(json => {
+        console.log('In AnimationPickerImage moderation rating:', json.rating);
+        HttpClient.post(`/v3/channels/${this.props.channelId}/abuse/image`, file, true, {
+          'Content-Type': file.type,
+        })
+        .then(response => response.json())
+        .then(json => {
+          console.log('json', json);
+        })
+        .catch(err => console.log('update abuse error', err));
+      })
       .catch(err => console.error('Moderation error:', err));
 
     // Continue upload start.
