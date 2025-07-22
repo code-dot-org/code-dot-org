7596d888d854f5986ec01b8d46cb155154744436
Ryan Driscoll <driscollrp@gmail.com>
2025-07-22 15:17:57 -0600

    feat: use recursive renderRoute to handle deep nested child routes when they are eventually needed

--- apps/src/code-studio/pd/workshop_dashboard/workshop_dashboard.jsx
+++ apps/src/code-studio/pd/workshop_dashboard/workshop_dashboard.jsx
@@ -122,29 +122,6 @@ const routeConfigs = [
     childRoutes: workshopChildRouteConfigs,
   },
   {
-    // remove /temp for switch over
-    path: 'workshops/:workshopId/temp',
-    breadcrumbs: 'Workshops,Workshop Overview',
-    noRouter: true,
-    component: WorkshopTabs,
-    children: [
-      {
-        index: true,
-        component: WorkshopOverview,
-      },
-      {
-        path: 'enrollments',
-        component: WorkshopEnrollments,
-        breadcrumbs: 'Workshops,Workshop Overview,Enrollments',
-      },
-      {
-        path: 'surveys',
-        component: WorkshopSurveys,
-        breadcrumbs: 'Workshops,Workshop Overview,Surveys',
-      },
-    ],
-  },
-  {
     path: 'workshops/:workshopId/edit',
     breadcrumbs: 'Workshops,Edit Workshop',
     component: WorkshopFormTemplate,
