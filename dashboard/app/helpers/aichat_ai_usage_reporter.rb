db625357834fb94d6b541864ebf5ec47d2cbf3f9
Ed Baafi <edward.baafi@code.org>
2025-07-22 10:57:33 -0400

    Add usage reporter as separate file/class based on new config/request/context format

---
+++ dashboard/app/helpers/aichat_ai_usage_reporter.rb
@@ -0,0 +1,118 @@
+class AichatAiUsageReporter
+  DEFAULT_TOKEN_LIMIT_PER_DAY = 10_000_000
+  ONE_DAY_S = 60 * 60 * 24
+
+  def initialize(model_id, user_id, project_id, level_id)
+    @model_id = model_id
+    @user_id = user_id
+    @project_id = project_id
+    @level_id = level_id
+  end
+
+  def report_usage_and_throttling_metrics(usage, config, request, context, response_time)
+    unless usage
+      Honeybadger.notify("Response detected without usage statistics, which are required for throttling.")
+      return
+    end
+
+    # Pull out token counts.
+    prompt_tokens = usage['prompt_tokens']
+    completion_tokens = usage['completion_tokens']
+    cached_prompt_tokens = usage['cached_prompt_tokens']
+
+    report_token_usage(prompt_tokens)
+
+    # Calculate costs.
+    input_rate = 0.15 / 1_000_000 # $0.15 per million tokens.
+    cached_input_rate = 0.075 / 1_000_000 # $0.075 per million tokens.
+    output_rate = 0.60 / 1_000_000 # $0.60 per million tokens.
+
+    input_cost = (prompt_tokens * input_rate) + (cached_prompt_tokens * cached_input_rate)
+    output_cost = completion_tokens * output_rate
+    total_cost = input_cost + output_cost
+
+    message_and_file_counts = get_messages_and_files_counts(config, request, context)
+    is_multimodal = message_and_file_counts[:withAssets] > 0
+
+    log_payload = {
+      event: 'aichat_openai_usage',
+      multimodal: is_multimodal,
+      usage: usage,
+      messages: message_and_file_counts,
+      cost: {
+        input: "$#{format("%.6f", input_cost)}",
+        output: "$#{format("%.6f", output_cost)}",
+        total: "$#{format("%.6f", total_cost)}"
+      },
+      responseTime: response_time,
+      levelId: @level_id,
+      projectId: @project_id,
+      userId: @user_id
+    }
+
+    CDO.log.info log_payload.to_json.to_s if DCDO.get('log_aichat_openai_usage', false)
+
+    metrics = [
+      ['PromptTokens', prompt_tokens], ['CompletionTokens', completion_tokens], ['CachedTokens', cached_prompt_tokens]
+    ].map do |key, value|
+      {
+        metric_name: "AichatOpenaiRequest.#{key}",
+        value: value,
+        unit: 'Count',
+        timestamp: Time.now,
+        dimensions: [
+          {name: 'Environment', value: CDO.rack_env},
+          {name: 'Multimodal', value: is_multimodal.to_s},
+        ]
+      }
+    end
+    Cdo::Metrics.push(SharedConstants::AICHAT_METRICS_NAMESPACE, metrics)
+  end
+
+  # Helper to get message and asset counts used for AiUsageReporter.
+  private def get_messages_and_files_counts(config, request, context)
+    combined_parts = [
+      # Add systemInstructions parts if not nil.
+      *(config[:systemInstructions] ? config[:systemInstructions] : []),
+
+      # Add request parts.
+      *request,
+
+      # Add each context item's parts array.
+      *context.flat_map {|c| c[:parts]}
+    ]
+
+    files_count = combined_parts.count {|p| p[:type] == 'file'}
+    pdfs_count = combined_parts.count {|p| p[:type] == 'file' && p[:content][:mime_type] == 'application/pdf'}
+
+    # TODO - make a helper for this!
+    # Currently we don't have a shared (between frontend and backd) list of image mime types
+    # so for now, we just assume if not a pdf, then it's an image
+    images_count = files_count - pdfs_count
+
+    return_value = {
+      total: combined_parts.count,
+      withAssets: files_count,
+      pdfs: pdfs_count,
+      images: images_count
+    }
+
+    return_value
+  end
+
+  private def report_token_usage(prompt_tokens)
+    # Typical usage of our throttling module calls throttle at the point where it's deciding whether to throttle or not.
+    # In this case, we are just reporting token usage, and subsequent calls to our aichat_request endpoint check whether
+    # the user has been throttled.
+    #
+    # Prompt tokens are by far and away our largest cost driver (and the piece that users actually control),
+    # so we throttle on that.
+    limit = DCDO.get('aichat_token_limit_per_day', DEFAULT_TOKEN_LIMIT_PER_DAY)
+    Cdo::Throttle.throttle(AichatAiHelper.token_throttling_key(@model_id, @user_id),
+      limit,
+      ONE_DAY_S,
+      throttle_for: ONE_DAY_S,
+      count: prompt_tokens
+    )
+  end
+end
