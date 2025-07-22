991a99e40f3ee732f124d1e7015ba2ca7436196a
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-07-22 14:52:52 -0700

    remove redundant replace

--- frontend/apps/marketing/tests/e2e/config/path.ts
+++ frontend/apps/marketing/tests/e2e/config/path.ts
@@ -3,8 +3,7 @@ import {simpleGit} from 'simple-git';
 export async function getAllTheThingsPagePath() {
   if (process.env.BRANCHED_TESTING_ENABLED === 'true') {
     const maybeBranchName =
-      process.env.PR_HEAD_REF ??
-      (await simpleGit().branch()).current.replaceAll('/', '-');
+      process.env.PR_HEAD_REF ?? (await simpleGit().branch());
 
     const branchName = maybeBranchName.replaceAll('/', '-');
 
