6891f54b9ba7c7adbc2aff6c027613f93f990ea8
Hannah Bergam <hannah.bergam@code.org>
2025-07-22 13:01:05 -0700

    Revert "Revert "[MusicLab] Test that feedback receives focus post-run via new…"
    This reverts commit 700ecfe5bab26953f5ed87a9bafef65037525ff0.

--- apps/src/lab2/views/components/InstructionsPanel.tsx
+++ apps/src/lab2/views/components/InstructionsPanel.tsx
@@ -188,7 +188,7 @@ const InstructionsPanel: React.FunctionComponent<InstructionsPanelProps> = ({
                 <TextToSpeech text={useMessage} />
               )}
               {useMessage && (
-                <div ref={feedbackRef} tabIndex={-1}>
+                <div id="focusable-message" ref={feedbackRef} tabIndex={-1}>
                   <EnhancedSafeMarkdown
                     markdown={useMessage}
                     className={moduleStyles.markdownText}
