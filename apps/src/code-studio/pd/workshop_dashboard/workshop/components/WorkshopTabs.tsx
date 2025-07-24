ea4330592410a41fca4167f814462681c4c2fa68
Ryan Driscoll <driscollrp@gmail.com>
2025-07-24 13:38:58 -0600

    feat: replace mui unstyled components with dsco Tabs
    unfortunately this means tabs are not semantic anchor tags and some unused html elements for TabsPanel are rendering empty divs

--- apps/src/code-studio/pd/workshop_dashboard/workshop/components/WorkshopTabs.tsx
+++ apps/src/code-studio/pd/workshop_dashboard/workshop/components/WorkshopTabs.tsx
@@ -1,43 +1,42 @@
-import Link from '@mui/material/Link';
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import React, {FC, forwardRef} from 'react';
-import {
-  Link as RouterLink,
-  LinkProps,
-  Outlet,
-  useParams,
-  useLocation,
-} from 'react-router-dom';
+import Tabs from '@code-dot-org/component-library/tabs';
+import React, {FC, useMemo} from 'react';
+import {Outlet, useLocation, useNavigate} from 'react-router-dom';
 
 import {WorkshopTabsProps} from '../types';
 
-const TabLink = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
-  <Link ref={ref} {...props} component={RouterLink} />
-));
+import styles from '../workshop.module.scss';
 
 export const WorkshopTabs: FC<WorkshopTabsProps> = ({tabList}) => {
-  const {workshopId} = useParams();
   const {pathname} = useLocation();
+  const navigate = useNavigate();
 
-  const currentTabValue =
-    tabList.find(tab =>
-      pathname.endsWith(`/workshops/${workshopId}/${tab.path}`)
-    )?.path || '';
+  const currentTabValue = useMemo(
+    () =>
+      // filter out index path
+      tabList.filter(t => t.path).find(tab => pathname.includes(tab.path))
+        ?.path ?? '',
+    [pathname, tabList]
+  );
+
+  const handleChange = (value: string) => {
+    navigate(value);
+  };
 
   return (
     <nav aria-label="Workshop sections">
-      <Tabs value={currentTabValue}>
-        {tabList.map(tab => (
-          <Tab
-            key={tab.label}
-            label={tab.label}
-            value={tab.path}
-            component={TabLink}
-            to={`/workshops/${workshopId}${tab.path ? `/${tab.path}` : ''}`}
-          />
-        ))}
-      </Tabs>
+      <Tabs
+        defaultSelectedTabValue={currentTabValue}
+        tabsContainerClassName={styles.tabList}
+        name="default_tabs"
+        onChange={handleChange}
+        onTabClose={() => {}}
+        tabs={tabList.map(tab => ({
+          // tabContent is null because Outlet will render appropriate components based on route
+          tabContent: null,
+          text: tab.label,
+          value: tab.path,
+        }))}
+      />
       <Outlet />
     </nav>
   );
