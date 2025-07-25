3cbe29c26effdc6d9fc3b39f6196723bcfcf890d
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-07-25 09:37:45 -0700

    fix type error on ImageCarousel

--- frontend/apps/marketing/src/components/contentful/carousels/imageCarousel/ImageCarousel.tsx
+++ frontend/apps/marketing/src/components/contentful/carousels/imageCarousel/ImageCarousel.tsx
@@ -42,7 +42,7 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({
 
           return {
             id: title,
-            slide: <Image src={file?.url} altText={description} />,
+            slide: <Image src={file?.url ?? ''} altText={description ?? ''} />,
           };
         }),
     [slides],
