bdc8c1c803bbc4994a1c328bb09a97b519dca264
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-07-25 10:52:30 -0700

    otel

--- dashboard/config/initializers/opentelemetry.rb
+++ dashboard/config/initializers/opentelemetry.rb
@@ -1,31 +1,31 @@
 require 'opentelemetry/sdk'
 require 'opentelemetry/instrumentation/all'
 require 'opentelemetry-exporter-otlp'
+# require 'services/opentelemetry/filtering_sampler'
 
-# Only configure OpenTelemetry if enabled in CDO config, to avoid unnecessary overhead in environments where it's not needed
 if CDO.enable_opentelemetry
   OpenTelemetry::SDK.configure do |c|
     c.service_name = 'dashboard'
 
-    # Enable all ruby instrumentation, with a custom configuration for Rack.
     c.use_all(
       'OpenTelemetry::Instrumentation::Rack' => {
         url_quantization: ->(path, _) {path.to_s},
         untraced_requests: lambda {|env|
           path = env['PATH_INFO']
-          # Don't trace requests for static assets. They are very high volume and noisy and not applicable for most application performance monitoring use cases.
           path.start_with?('/assets/', '/onetrust/', '/fonts/') || path.starts_with?('/shared/') || path.end_with?('.js') || path.ends_with?('.css')
         },
       }
     )
 
-    # Configure a batch span processor with the OTLP exporter to send telemetry data to our OpenTelemetry Collector.
-    # By default, this is sent to the local OpenTelemetry Collector installed by the cdo-otel-collector cookbook.
-    # See: cookbooks/cdo-otel-collector
     c.add_span_processor(
       OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
         OpenTelemetry::Exporter::OTLP::Exporter.new
       )
     )
   end
+
+  # parent_sampler = OpenTelemetry::SDK::Trace::Samplers::ALWAYS_ON
+  #OpenTelemetry::SDK::Trace::Samplers::TraceIdRatioBased.new(0.001)
+
+  # OpenTelemetry.tracer_provider.sampler = Services::OpenTelemetry::FilteringSampler.new(parent_sampler)
 end
