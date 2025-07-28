73828e0fc2adec499501ffc9d864b9b186a82034
Mark Barnes <mark.barnes@code.org>
2025-07-28 10:46:09 -0500

    experiment

--- dashboard/app/helpers/ai_evaluation_openai_helper.rb
+++ dashboard/app/helpers/ai_evaluation_openai_helper.rb
@@ -26,8 +26,9 @@ module AiEvaluationOpenaiHelper
               type: "object",
               properties: {
                 evaluationCriteria: {type: "string"},
-                aiEvaluation: {type: "string"},
+                observations: {type: "string"},
                 aiReasoning: {type: "string"},
+                aiEvaluation: {type: "string"},
                 skillEvaluations: {type: "array", items: {type: "object"}},
               },
             }
