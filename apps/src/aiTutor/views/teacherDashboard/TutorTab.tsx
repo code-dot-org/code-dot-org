ffbb85872e1271b6a78bce72e4d3ee4bb2787a32
Cassi Brenci <cassi.brenci@code.org>
2025-07-30 14:22:09 -0700

    Replace/refactor the teacher dashboard AI Tutor tab to support Tutor2 instead. Still needs a bit of work around checking whether the section contains levels with tutor available.

--- apps/src/aiTutor/views/teacherDashboard/TutorTab.tsx
+++ apps/src/aiTutor/views/teacherDashboard/TutorTab.tsx
@@ -1,10 +1,6 @@
-import SegmentedButtons from '@code-dot-org/component-library/segmentedButtons';
-import React, {useState} from 'react';
+import React from 'react';
 
 import AccessControls from './AccessControls';
-import InteractionsTable from './InteractionsTable';
-
-import style from './interactions-table.module.scss';
 
 /**
  * Renders table of student chat messages and toggles to control student access to AI Tutor.
@@ -13,41 +9,8 @@ interface TutorTabProps {
   sectionId: number;
 }
 
-export const TAB_NAMES = {
-  ACCESS: 'access',
-  INTERACTIONS: 'interactions',
-};
-
 const TutorTab: React.FC<TutorTabProps> = ({sectionId}) => {
-  const [selectedTab, setSelectedTab] = useState(TAB_NAMES.ACCESS);
-
-  return (
-    <div>
-      <div className={style.interactionsElement}>
-        <SegmentedButtons
-          className="ai-tutor-tab-buttons"
-          selectedButtonValue={selectedTab}
-          size="s"
-          buttons={[
-            {label: 'View Access Controls', value: TAB_NAMES.ACCESS},
-            {
-              label: 'View Interactions',
-              value: TAB_NAMES.INTERACTIONS,
-            },
-          ]}
-          onChange={setSelectedTab}
-        />
-      </div>
-      <div>
-        {selectedTab === TAB_NAMES.ACCESS && (
-          <AccessControls sectionId={sectionId} />
-        )}
-        {selectedTab === TAB_NAMES.INTERACTIONS && (
-          <InteractionsTable sectionId={sectionId} />
-        )}
-      </div>
-    </div>
-  );
+  return <AccessControls sectionId={sectionId} />;
 };
 
 export default TutorTab;
--- apps/src/sites/studio/pages/teacher_dashboard/show.js
+++ apps/src/sites/studio/pages/teacher_dashboard/show.js
@@ -37,7 +37,7 @@ const {
   sections,
   localeCode,
   hasSeenStandardsReportInfo,
-  canViewStudentAIChatMessages,
+  canEnableAITutor,
   sectionOrder,
   providers,
 } = scriptData;
@@ -70,7 +70,7 @@ $(document).ready(function () {
   store.dispatch(setLocaleCode(localeCode));
   store.dispatch(setAuthProviders(providers));
 
-  const showAITutorTab = canViewStudentAIChatMessages;
+  const showAITutorTab = canEnableAITutor;
 
   if (sections.length > 0) {
     const selectedSectionFromList = window.location.pathname.includes(
