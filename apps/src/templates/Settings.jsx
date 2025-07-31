0ff415952d7888cb677748f46d853bc8293ed6b6
Mike Harvey <mike.harvey@code.org>
2025-07-31 13:43:20 -0400

    add settings modal to blockly labs

---
+++ apps/src/templates/Settings.jsx
@@ -0,0 +1,74 @@
+import SimpleDropdown from '@code-dot-org/component-library/dropdown/simpleDropdown';
+import React, {useState, useEffect} from 'react';
+
+import {BLOCKLY_THEME, Themes} from '@cdo/apps/blockly/constants';
+import {commonI18n} from '@cdo/apps/types/locale';
+
+import {setAllWorkspacesTheme} from '../blockly/utils';
+import UserPreferences from '../lib/util/UserPreferences';
+
+const themeOptions = [
+  {value: Themes.MODERN, text: commonI18n.blocklyModernTheme()},
+  {value: Themes.HIGH_CONTRAST, text: commonI18n.blocklyHighContrastTheme()},
+  {value: Themes.PROTANOPIA, text: commonI18n.blocklyProtanopiaTheme()},
+  {value: Themes.DEUTERANOPIA, text: commonI18n.blocklyDeuteranopiaTheme()},
+  {value: Themes.TRITANOPIA, text: commonI18n.blocklyTritanopiaTheme()},
+];
+
+const SettingsModal = () => {
+  const [selectedTheme, setSelectedTheme] = useState(undefined);
+
+  useEffect(() => {
+    new UserPreferences()
+      .getBlocklyTheme(() => ({
+        blockly: localStorage.getItem(BLOCKLY_THEME) || Themes.MODERN,
+      }))
+      .then(theme => setSelectedTheme(theme));
+  }, []);
+
+  const handleChange = value => {
+    const theme = Blockly.themes[value];
+    if (theme) {
+      setSelectedTheme(value);
+      setAllWorkspacesTheme(theme, Blockly.getMainWorkspace()?.getTheme());
+      new UserPreferences().setBlocklyTheme(value, () =>
+        localStorage.setItem(BLOCKLY_THEME, value)
+      );
+    }
+  };
+
+  return (
+    <div className="modal-content" style={{margin: 0}}>
+      <h5 className="dialog-title">{commonI18n.settings()}</h5>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          gap: '6px',
+        }}
+      >
+        <label
+          htmlFor="theme"
+          style={{
+            minWidth: '150px',
+            fontSize: '14px',
+            fontWeight: 500,
+            margin: '2px',
+          }}
+        >
+          {commonI18n.blocklyTheme()}
+        </label>
+        <SimpleDropdown
+          name="theme"
+          items={themeOptions}
+          selectedValue={selectedTheme}
+          onChange={e => handleChange(e.target.value)}
+          isLabelVisible={false}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsModal;
