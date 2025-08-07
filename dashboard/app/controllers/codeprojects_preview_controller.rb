64305ff4ffd3a04e8be2aeaa483b34f9b17d91be
Molly Moen <molly@code.org>
2025-08-07 15:32:26 -0700

    clean up

--- dashboard/app/controllers/codeprojects_preview_controller.rb
+++ dashboard/app/controllers/codeprojects_preview_controller.rb
@@ -1,5 +1,4 @@
 class CodeprojectsPreviewController < ApplicationController
-  skip_before_action :verify_authenticity_token
   # Public preview page, static content for now.
   def show
     render 'show', layout: false
