529562ed94ba0d0a0b29ce0838f9295fd87c0370
Ryan Driscoll <driscollrp@gmail.com>
2025-08-08 13:44:58 -0600

    feat: create and style workshop overview and WorkshopInformationSection

--- apps/src/code-studio/pd/workshop_dashboard/workshops/overview/WorkshopOverview.tsx
+++ apps/src/code-studio/pd/workshop_dashboard/workshops/overview/WorkshopOverview.tsx
@@ -1,5 +1,37 @@
-import React, {FC} from 'react';
+import {Box, Stack, Alert} from '@mui/material';
+import React from 'react';
+import {useSelector} from 'react-redux';
 
-export const WorkshopOverview: FC = () => {
-  return <div>Overview</div>;
+import {WorkshopAdmin} from '../../permission';
+import {useWorkshopContext} from '../context/WorkshopContext';
+
+import {WorkshopInformationSection} from './sections/WorkshopInformationSection';
+
+export const WorkshopOverview: React.FC = () => {
+  const {workshop} = useWorkshopContext();
+  const permission = useSelector(
+    (state: {
+      workshopDashboard: {permission: {has: (permission: string) => boolean}};
+    }) => state.workshopDashboard.permission
+  );
+  const isWorkshopAdmin = permission.has(WorkshopAdmin);
+
+  if (!workshop) {
+    return (
+      <Box>
+        <Alert severity="warning">No workshop data available</Alert>
+      </Box>
+    );
+  }
+
+  return (
+    <Box>
+      <Stack spacing={3}>
+        <WorkshopInformationSection
+          workshop={workshop}
+          isWorkshopAdmin={isWorkshopAdmin}
+        />
+      </Stack>
+    </Box>
+  );
 };
