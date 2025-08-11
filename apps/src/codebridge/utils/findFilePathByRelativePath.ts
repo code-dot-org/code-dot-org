015b4917307f211ded68e6038aca509c59ea7739
Molly Moen <molly@code.org>
2025-08-11 16:02:07 -0700

    update method and add tests

--- apps/src/codebridge/utils/findFilePathByRelativePath.ts
+++ apps/src/codebridge/utils/findFilePathByRelativePath.ts
@@ -1,3 +1,7 @@
+// Given a file path and the current file (which is a full path), convert the file
+// path to a fully qualified path relative to the root folder. This supports both absolute
+// and relative paths.
+// The returned path will not have a leading slash.
 export const findFilePathByRelativePath = (
   filePath: string,
   currentFile: string
@@ -10,17 +14,14 @@ export const findFilePathByRelativePath = (
     ? currentFile.substring(0, currentFile.lastIndexOf('/'))
     : '';
 
-  // Split the new path into segments
   const newPathSegments = filePath.split('/');
-
   // If the path is relative, we will resolve it against the current directory.
   // We start with the current directory split into segments, ignoring the current file name.
   const resolvedPathSegments = currentDir ? currentDir.split('/') : [];
 
-  // Process each segment
   for (const segment of newPathSegments) {
-    if (segment === '..') {
-      // Go up one directory
+    if (segment === '..' && resolvedPathSegments.length > 0) {
+      // Go up one directory if we can. Ignore if at root.
       resolvedPathSegments.pop();
     } else if (segment !== '.' && segment !== '') {
       // Add segment (ignore current directory '.' and empty segments)
