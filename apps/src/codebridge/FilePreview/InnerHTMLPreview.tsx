936447395b7f4a19520a93ccbf30d20a5b2130eb
Molly Moen <molly@code.org>
2025-08-12 14:09:04 -0700

    remove todo

--- apps/src/codebridge/FilePreview/InnerHTMLPreview.tsx
+++ apps/src/codebridge/FilePreview/InnerHTMLPreview.tsx
@@ -118,7 +118,6 @@ const InnerHTMLPreview = () => {
 
   // TODOs:
   // Support other file types (images, etc.): https://codedotorg.atlassian.net/browse/CT-1255
-  // More robust file paths: https://codedotorg.atlassian.net/browse/CT-1256
   // Better regeneration logic: https://codedotorg.atlassian.net/browse/CT-1259
   useEffect(() => {
     if (source) {
