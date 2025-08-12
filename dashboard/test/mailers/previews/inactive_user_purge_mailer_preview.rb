a8a141dda36e32892f42186a5eb3acb2d0152158
juanmanzojr <137838584+juanmanzojr@users.noreply.github.com>
2025-08-12 10:45:25 -0700

    Revert "Create mailer and template for inactive teacher purge (#67445)"
    This reverts commit a459b95e8d331bf7e68d0d090dffc294e11a5da3.

--- dashboard/test/mailers/previews/inactive_user_purge_mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-class InactiveUserPurgeMailerPreview < ActionMailer::Preview
-  include FactoryBot::Syntax::Methods
-
-  def teacher_inactivity_soft_delete_warning_email
-    teacher = build :teacher
-    InactiveUserPurgeMailer.teacher_inactivity_soft_delete_warning_email(teacher)
-  end
-end
