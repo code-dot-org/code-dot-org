035a9fbfa193bfe8140658a2d2027e60a57b016f
denyslevada <levada.denys@gmail.com>
2025-08-12 13:43:29 +0300

    feat(VideoLegacy): add VideoLegacy component with support for YouTube and fallback options

---
+++ frontend/apps/marketing/src/components/contentful/corporateSite/videoLegacy/index.ts
@@ -0,0 +1,3 @@
+// Export the Component Definition for use in Contentful Studio
+export {VideoContentfulComponentDefinition} from './VideoLegacyContentfulDefinition';
+export {default} from './VideoLegacy';
