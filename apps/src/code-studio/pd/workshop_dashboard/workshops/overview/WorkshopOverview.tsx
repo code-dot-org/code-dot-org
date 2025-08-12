aef150512b74a2c7c36dcf8c46c33dfacad7c7c2
Ryan Driscoll <driscollrp@gmail.com>
2025-08-12 09:03:51 -0600

    feat: move data fetching from context to WorkshopLayout component and use outlet context. add loading spinner

--- apps/src/code-studio/pd/workshop_dashboard/workshops/overview/WorkshopOverview.tsx
+++ apps/src/code-studio/pd/workshop_dashboard/workshops/overview/WorkshopOverview.tsx
@@ -1,17 +1,21 @@
 import Alert from '@code-dot-org/component-library/alert';
+import FontAwesomeV6Icon from '@code-dot-org/component-library/fontAwesomeV6Icon';
 import {Box, Stack} from '@mui/material';
 import React from 'react';
 import {useSelector} from 'react-redux';
+import {useOutletContext} from 'react-router-dom';
+
+import {UseFetchResult} from '@cdo/apps/util/useFetch';
 
 import {WorkshopAdmin} from '../../permission';
-import {useWorkshopContext} from '../context/WorkshopContext';
+import {Workshop} from '../../WorkshopFormTemplate/types';
+import {WorkshopData} from '../types';
 
 import {TakeAttendanceSection} from './sections/TakeAttendanceSection';
 import {WorkshopInformationSection} from './sections/WorkshopInformationSection';
 import {WorkshopLinksSection} from './sections/WorkshopLinksSection';
 
 export const WorkshopOverview: React.FC = () => {
-  const {workshop} = useWorkshopContext();
   const permission = useSelector(
     (state: {
       workshopDashboard: {permission: {has: (permission: string) => boolean}};
@@ -19,8 +23,22 @@ export const WorkshopOverview: React.FC = () => {
   );
   const isWorkshopAdmin = permission.has(WorkshopAdmin);
 
-  if (!workshop) {
+  const {workshop, loading, error} = useOutletContext<
+    Omit<UseFetchResult<Workshop>, 'data'> & {
+      workshop: WorkshopData;
+    }
+  >();
+
+  if (!workshop && loading) {
+    return <FontAwesomeV6Icon iconName="spinner" animationType="spin" />;
+  }
+
+  if (error) {
     return <Alert size="m" text="Workshop not found" type="warning" />;
+  }
+
+  if (!workshop) {
+    return null;
   }
 
   return (
