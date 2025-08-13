9c42e01e0cb618ab0b8ad2fbce8251ce32bd701f
Liam Frye-Mason <liam.fryemason@code.org>
2025-08-13 15:44:56 -0700

    Fix mark as read and index

--- dashboard/app/controllers/notifications_controller.rb
+++ dashboard/app/controllers/notifications_controller.rb
@@ -7,33 +7,99 @@ class NotificationsController < ApplicationController
     locale = params[:locale] || I18n.default_locale
 
     contentful_entries = Marketing::ContentfulClient.entries(locale, 'dashboard-notification')
+    contentful_result = contentful_entries.filter_map do |notification|
+      format_contentful_notification(notification)
+    end
 
-    rails_external_notifications = current_user.external_notifications.not_dismissed.order(created_at: :desc).all
+    contentful_ids = contentful_result.pluck(:external_id)
 
-    # TODO(lfm): call contentful and add contentful notifications to the list
+    rails_external_notifications = ExternalNotification.where(external_id: contentful_ids, user: current_user)
 
-    render json: {rails: rails_external_notifications.as_json.map {|notification| notification.deep_transform_keys {|key| key.to_s.camelize(:lower)}},
-     contentful: contentful_entries}, status: :ok
+    results = contentful_result.filter_map do |notification|
+      rails_notification = rails_external_notifications.find {|n| n.external_id == notification[:external_id]}
+
+      next nil if rails_notification&.is_dismissed
+
+      read_at = rails_notification&.read_at || nil
+      notification.merge(
+        read_at: read_at
+)
+    end
+
+    render json: results.as_json.map {|notification| notification.deep_transform_keys {|key| key.to_s.camelize(:lower)}}, status: :ok
   end
 
   def mark_as_read
-    notification_ids = params[:notification_ids] || []
+    external_notifications = params[:external_notifications] || []
 
-    if notification_ids.empty?
+    if  external_notifications.empty?
       render json: {status: 'error', message: 'No notification IDs provided'}, status: :bad_request
       return
     end
 
-    external_notifications = current_user.external_notifications.where(id: notification_ids)
+    found_external_notifications = current_user.external_notifications.where(external_id: external_notifications)
 
-    external_notifications.where(read_at: nil).update_all(read_at: Time.current)
+    found_external_notifications.where(read_at: nil).update_all(read_at: Time.current)
+    found_ids = found_external_notifications.pluck(:external_id)
+    notifications_to_create = external_notifications - found_ids
+    notifications_to_create.each do |external_id|
+      ExternalNotification.create!(user_id: current_user.id, external_id: external_id, read_at: Time.current)
+    end
 
     response_data = {
       status: 'success',
-      message: "#{external_notifications.count} notification(s) marked as read",
-      marked_count: external_notifications.count,
+      message: "#{found_external_notifications.count} notification(s) marked as read",
+      marked_count: found_external_notifications.count,
     }
 
     render json: response_data, status: :ok
+  end
+
+  private def format_contentful_notification(notification)
+    fields = notification.fields || {}
+    formatted_notification = {
+      external_id: notification.id,
+      title: fields[:title] || nil,
+      description: fields[:description] || nil,
+      icon_name: fields[:icon_name] || nil,
+      href_links: fields[:href_links] || [],
+      ai_prompts: fields[:ai_prompts] || [],
+      priority: fields[:priority] || 0,
+      expires_at: fields[:expires_at],
+    }
+
+    if formatted_notification[:external_id].blank? || formatted_notification[:title].blank? || formatted_notification[:description].blank? || formatted_notification[:icon_name].blank?
+      Honeybadger.notify(
+        'Unable to format Contentful notification',
+        context: {
+          contentful_id: notification.id,
+          has_id: formatted_notification[:external_id].blank?,
+          has_title: formatted_notification[:title].blank?,
+          has_description: formatted_notification[:description].blank?,
+          has_icon_name: formatted_notification[:icon_name].blank?
+        }
+      )
+      return nil
+    end
+
+    # Ensure data types are correct
+    formatted_notification[:priority] = formatted_notification[:priority].to_i
+    formatted_notification[:expires_at] = formatted_notification[:expires_at].is_a?(Time) ? formatted_notification[:expires_at] : nil
+    formatted_notification[:href_links] = formatted_notification[:href_links].filter_map do |link|
+      !!link['url'] && !!link['text'] ? {url: link['url'], text: link['text']} : nil
+    end
+    formatted_notification[:ai_prompts] = formatted_notification[:ai_prompts].filter_map do |prompt|
+      !!prompt['text'] && !!prompt['prompt'] ? {text: prompt['text'], prompt: prompt['prompt']} : nil
+    end
+
+    formatted_notification
+  rescue
+    Honeybadger.notify(
+      'Error trying to format Contentful notification',
+        context: {
+          contentful_id: notification.id,
+        }
+      )
+    nil
   end
 end
