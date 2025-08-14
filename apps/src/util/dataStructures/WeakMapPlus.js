afb20f9d75d15b188d2261f56fc7bfcfa86cdbb2
Ed Baafi <edward.baafi@code.org>
2025-08-14 19:30:04 -0400

    convert WeakMapPlus to js until TypeScript target can be set to at least ES6 as the ES5 output is buggy

--- apps/src/util/dataStructures/WeakMapPlus.js
+++ apps/src/util/dataStructures/WeakMapPlus.js
@@ -3,39 +3,34 @@
  * for `undefined` as a key.  `WeakMap` does not support primitive keys other than
  * Symbol (and this only in later specs).
  **/
-
-export class WeakMapPlus<K extends WeakKey, V> extends WeakMap<K, V> {
+export class WeakMapPlus extends WeakMap {
   /*
    * WeakMap compatiple key to swap for `undefined`.  Currently uses an object
    * but could use a symbol if our minimum supported browsers implement the spec
    * or if our transpilers (babel/tsc) can add a polyfill implementation .
    * See: https://github.com/tc39/ecma262/pull/2777
    **/
-  private static _undefinedKey = {};
-
+  static {
+    this._undefinedKey = {};
+  }
   /*
    * Get a key that works with the native WeakMap implementation.  I.e. if
    * key is undefined, we'll use `_undefinedKey` as the key
    * which is an object (non-primative) and thus allowed as WeakMap key.
    **/
-
-  private getNativeKey(key: K | undefined) {
-    return key ? key : (WeakMapPlus._undefinedKey as K);
+  getNativeKey(key) {
+    return key ? key : WeakMapPlus._undefinedKey;
   }
-
-  override get(key: K | undefined): V | undefined {
+  get(key) {
     return super.get(this.getNativeKey(key));
   }
-
-  override delete(key: K | undefined): boolean {
+  delete(key) {
     return super.delete(this.getNativeKey(key));
   }
-
-  override has(key: K | undefined): boolean {
+  has(key) {
     return super.has(this.getNativeKey(key));
   }
-
-  override set(key: K | undefined, value: V): this {
+  set(key, value) {
     return super.set(this.getNativeKey(key), value);
   }
 }
