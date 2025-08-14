84a7f0a0b9e34a7fe934cdbe479cbf3d6c8f7bd6
wilkie <wilkie05@gmail.com>
2025-08-14 15:41:53 -0400

    Adds <Lab> wrapper.

--- frontend/packages/lab-base/src/components/index.ts
+++ frontend/packages/lab-base/src/components/index.ts
@@ -8,3 +8,4 @@ export {default as ProjectBlockedUI} from './projectBlockedUI';
 export * from './errorFallbackPage';
 export {default as ErrorFallbackPage} from './errorFallbackPage';
 export {default as ErrorBoundary} from './ErrorBoundary';
+export {default as Lab} from './Lab';
