8cda79759e18dbfa78a574ccc5c2573e43768fc3
Liam Frye-Mason <liam.fryemason@code.org>
2025-08-15 09:23:39 -0700

    Try reset instance

--- dashboard/test/controllers/notifications_controller_test.rb
+++ dashboard/test/controllers/notifications_controller_test.rb
@@ -73,8 +73,7 @@ class NotificationsControllerTest < ActionController::TestCase
 
   describe 'contentful tests' do
     before do
-      mock_client = mock
-      Marketing::ContentfulClient.any_instance.stubs(:client).returns(mock_client)
+      Marketing::ContentfulClient.instance_variable_set(:@singleton__instance__, nil)
       Marketing::ContentfulClient.any_instance.expects(:entries).with(LOCALE, NOTIFICATION_CONTENTFUL_CONTENT_TYPE).returns([entry_1, entry_2])
     end
 
