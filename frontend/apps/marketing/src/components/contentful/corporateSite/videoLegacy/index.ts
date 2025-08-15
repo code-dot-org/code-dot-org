6da60d4be6bb1763de9057e51d0a4b7264835ce1
denyslevada <levada.denys@gmail.com>
2025-08-15 10:30:55 +0300

    feat(Video): remove videoLegacy

--- frontend/apps/marketing/src/components/contentful/corporateSite/videoLegacy/index.ts
+++
@@ -1,3 +0,0 @@
-// Export the Component Definition for use in Contentful Studio
-export {VideoContentfulComponentDefinition} from './VideoLegacyContentfulDefinition';
-export {default} from './VideoLegacy';
