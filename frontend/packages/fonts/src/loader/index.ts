0e7b5fa7965f49903b1cc12c38788b6c1a0a8f9b
Cassi Brenci <cassi.brenci@code.org>
2025-08-15 16:24:58 -0700

    Update paths and icon name to use the new icon

--- frontend/packages/fonts/src/loader/index.ts
+++ frontend/packages/fonts/src/loader/index.ts
@@ -15,14 +15,14 @@ export function injectFont(locale: InternationalFontLocale) {
 
 export function injectFontAwesome() {
   const stylesheets = [
-    'https://dsco.code.org/assets/font-awesome-pro/1752693338/css/fontawesome.min.css',
-    'https://dsco.code.org/assets/font-awesome-pro/1752693338/css/brands.min.css',
-    'https://dsco.code.org/assets/font-awesome-pro/1752693338/css/solid.min.css',
-    'https://dsco.code.org/assets/font-awesome-pro/1752693338/css/regular.min.css',
-    'https://dsco.code.org/assets/font-awesome-pro/1752693338/css/v4-font-face.min.css',
-    'https://dsco.code.org/assets/font-awesome-pro/1752693338/css/v4-shims.min.css',
-    'https://dsco.code.org/assets/font-awesome-pro/1752693338/css/duotone.min.css',
-    'https://dsco.code.org/assets/font-awesome-pro/1752693338/css/custom-icons.min.css',
+    'https://dsco.code.org/assets/font-awesome-pro/1755299638/css/fontawesome.min.css',
+    'https://dsco.code.org/assets/font-awesome-pro/1755299638/css/brands.min.css',
+    'https://dsco.code.org/assets/font-awesome-pro/1755299638/css/solid.min.css',
+    'https://dsco.code.org/assets/font-awesome-pro/1755299638/css/regular.min.css',
+    'https://dsco.code.org/assets/font-awesome-pro/1755299638/css/v4-font-face.min.css',
+    'https://dsco.code.org/assets/font-awesome-pro/1755299638/css/v4-shims.min.css',
+    'https://dsco.code.org/assets/font-awesome-pro/1755299638/css/duotone.min.css',
+    'https://dsco.code.org/assets/font-awesome-pro/1755299638/css/custom-icons.min.css',
   ];
 
   stylesheets.forEach(stylesheetHref => {
