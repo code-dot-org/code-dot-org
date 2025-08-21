a53a326203db51ccd27c7a9d31d8d9096882a453
Ed Baafi <edward.baafi@code.org>
2025-08-21 17:47:44 -0400

    add line between exports

--- apps/src/aichat/types/chatButton.ts
+++ apps/src/aichat/types/chatButton.ts
@@ -12,6 +12,7 @@ export type ChatButtonClickHandler = (
   userMessage: string,
   analyticsProperties?: AnalyticsProperties
 ) => void;
+
 export type ChatButtonProps = {
   onClick: ChatButtonClickHandler;
 };
