0e9acc680b29d61800de2fc1beeb75627f216ae8
Ryan Driscoll <driscollrp@gmail.com>
2025-08-21 14:56:18 -0600

    chore: move question sub text from form json to front end constant based on question type

--- apps/src/code-studio/pd/workshop_dashboard/workshops/surveys/constants.ts
+++ apps/src/code-studio/pd/workshop_dashboard/workshops/surveys/constants.ts
@@ -1,1 +1,4 @@
 export const MIN_RESPONSE_COUNT = 5;
+
+export const LIKERT_QUESTION_FOOTER = 'Based on a 7-point agreement scale';
+export const PROMOTER_QUESTION_FOOTER = 'Based on a 10-point rating';
