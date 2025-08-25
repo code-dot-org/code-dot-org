379e4580a54d777e2ade3baf5a44b787bf5431e5
Molly Moen <molly@code.org>
2025-08-25 11:19:02 -0700

    Lab2 Resource Panel: update locale logic (#67907)
    * use values from localization instead
    
    * use localization to get locale info
    
    * clean up
    
    * use hook

--- apps/src/lab2/views/components/Instructions/ResourcePanel/SettingsPanel.tsx
+++ apps/src/lab2/views/components/Instructions/ResourcePanel/SettingsPanel.tsx
@@ -2,27 +2,36 @@ import CloseButton from '@code-dot-org/component-library/closeButton';
 import {useTheme} from '@code-dot-org/component-library/common/contexts';
 import {SimpleDropdown} from '@code-dot-org/component-library/dropdown';
 import {Heading2} from '@code-dot-org/component-library/typography';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-import {LocaleProps} from '@cdo/apps/lab2/types';
 import {Setting} from '@cdo/apps/lab2/views/components/Instructions/ResourcePanel';
+import localization, {useLocalization} from '@cdo/apps/localization';
+import {LanguageInfo} from '@cdo/apps/localization/Localization';
 import {commonI18n} from '@cdo/apps/types/locale';
 
 import styles from './settings-panel.module.scss';
 interface SettingsPanelProps {
   settings: Setting[];
   closePanel: () => void;
-  localeProps?: LocaleProps;
 }
 
 const SettingsPanel: React.FunctionComponent<SettingsPanelProps> = ({
   settings,
   closePanel,
-  localeProps,
 }) => {
   const {theme} = useTheme();
   // SimpleDropdown isn't themed properly, so we have to manually set the color.
   const dropdownColor = theme === 'Dark' ? 'white' : 'black';
+  const locale = useLocalization();
+  const [localeOptions, setLocaleOptions] = useState<LanguageInfo[]>(
+    localization.locales
+  );
+
+  useEffect(() => {
+    localization.on('change', info => {
+      setLocaleOptions(localization.locales);
+    });
+  }, []);
 
   const handleLanguageChange = (
     event: React.ChangeEvent<HTMLSelectElement>
@@ -42,33 +51,31 @@ const SettingsPanel: React.FunctionComponent<SettingsPanelProps> = ({
         />
       </div>
       <div className={styles.settingsList}>
-        {localeProps && (
-          <form
-            action={localeProps.localeUrl}
-            method="post"
-            style={{marginBottom: '0px'}}
-            data-notranslate=""
-            className={styles.languageForm}
-          >
-            <input
-              type="hidden"
-              name="user_return_to"
-              value={window.location.href}
-            />
-            <SimpleDropdown
-              name="locale"
-              selectedValue={localeProps.currentLocale}
-              onChange={handleLanguageChange}
-              items={localeProps.localeOptions}
-              labelText={commonI18n.language()}
-              isLabelVisible={true}
-              size="s"
-              color={dropdownColor}
-              dropdownTextThickness="thin"
-              className={styles.dropdown}
-            />
-          </form>
-        )}
+        <form
+          action={'/locale'}
+          method="post"
+          style={{marginBottom: '0px'}}
+          data-notranslate=""
+          className={styles.languageForm}
+        >
+          <input
+            type="hidden"
+            name="user_return_to"
+            value={window.location.href}
+          />
+          <SimpleDropdown
+            name="locale"
+            selectedValue={locale}
+            onChange={handleLanguageChange}
+            items={localeOptions}
+            labelText={commonI18n.language()}
+            isLabelVisible={true}
+            size="s"
+            color={dropdownColor}
+            dropdownTextThickness="thin"
+            className={styles.dropdown}
+          />
+        </form>
         {settings.map(setting => (
           <SimpleDropdown
             key={setting.id}
