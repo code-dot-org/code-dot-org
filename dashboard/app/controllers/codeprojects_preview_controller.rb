844f95329b70e302d1eb5e26703af1a53c0a5583
Molly Moen <molly@code.org>
2025-08-26 09:30:09 -0700

    add csp

--- dashboard/app/controllers/codeprojects_preview_controller.rb
+++ dashboard/app/controllers/codeprojects_preview_controller.rb
@@ -1,6 +1,16 @@
 class CodeprojectsPreviewController < ApplicationController
   # Public preview page, static content for now.
   def show
+    code_studio_url = CDO.dashboard_site_host
+
+    if rack_env?(:development)
+      # dashboard_site_host is set to use port 3000 in development, but we want to also allow port 9000.
+      port_9000_url = code_studio_url.split(":").first + ":9000"
+      code_studio_url += " #{port_9000_url}"
+    end
+
+    # TODO: limit requests here too with connect-src.
+    response.headers['Content-Security-Policy'] = "frame-ancestors #{code_studio_url}"
     render 'show', layout: false
   end
 end
