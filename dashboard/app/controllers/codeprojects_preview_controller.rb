e6a91fd207713c20db5fbffd59a71db6e6a58e15
Molly Moen <molly@code.org>
2025-08-26 14:27:57 -0700

    explicitly use http

--- dashboard/app/controllers/codeprojects_preview_controller.rb
+++ dashboard/app/controllers/codeprojects_preview_controller.rb
@@ -3,7 +3,7 @@ class CodeprojectsPreviewController < ApplicationController
   # Public preview page, static content for now.
   def show
     code_studio_url = CDO.dashboard_site_host
-    allowed_connect_src = ALLOWED_HOSTNAME_SUFFIXES.join(" ")
+    allowed_connect_src = ALLOWED_HOSTNAME_SUFFIXES.map {|hostname| "http://#{hostname}"}.join(" ")
 
     if rack_env?(:development)
       # dashboard_site_host is set to use port 3000 in development, but we want to also allow port 9000.
