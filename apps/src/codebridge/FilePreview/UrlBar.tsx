46184c6e9d05c5f874a48c7490b154e9f74e4fc4
Alice Fisher <alice.fisher@code.org>
2025-08-26 14:39:32 +0000

    update style including making buttons smaller

--- apps/src/codebridge/FilePreview/UrlBar.tsx
+++ apps/src/codebridge/FilePreview/UrlBar.tsx
@@ -13,53 +13,55 @@ interface UrlBarProps {
 export const UrlBar: React.FC<UrlBarProps> = ({value, onChange}) => {
   return (
     <div className={moduleStyles.urlBarContainer}>
-      <div
-        className={classNames(
-          moduleStyles.urlButtonsWrapper,
-          moduleStyles.navButtonsWrapper
-        )}
-      >
-        <Button
-          onClick={() => {}}
-          aria-label="Navigate back"
-          size="xs"
-          type="tertiary"
-          isIconOnly={true}
-          icon={{iconName: 'chevron-left'}}
-          className={moduleStyles.urlButton}
+      <div className={moduleStyles.urlBarContent}>
+        <div
+          className={classNames(
+            moduleStyles.urlButtonsWrapper,
+            moduleStyles.navButtonsWrapper
+          )}
+        >
+          <Button
+            onClick={() => {}}
+            aria-label="Navigate back"
+            size="xs"
+            type="tertiary"
+            isIconOnly={true}
+            icon={{iconName: 'chevron-left'}}
+            className={moduleStyles.urlButton}
+          />
+          <Button
+            onClick={() => {}}
+            aria-label="Navigate forward"
+            size="xs"
+            type="tertiary"
+            isIconOnly={true}
+            icon={{iconName: 'chevron-right'}}
+            className={moduleStyles.urlButton}
+          />
+        </div>
+        <TextField
+          onChange={e => onChange(e.target.value)}
+          value={value}
+          name={'url-input'}
+          size={'s'}
+          className={moduleStyles.urlBarInput}
         />
-        <Button
-          onClick={() => {}}
-          aria-label="Navigate forward"
-          size="xs"
-          type="tertiary"
-          isIconOnly={true}
-          icon={{iconName: 'chevron-right'}}
-          className={moduleStyles.urlButton}
-        />
-      </div>
-      <TextField
-        onChange={e => onChange(e.target.value)}
-        value={value}
-        name={'url-input'}
-        size={'s'}
-        className={moduleStyles.urlBarInput}
-      />
-      <div
-        className={classNames(
-          moduleStyles.urlButtonsWrapper,
-          moduleStyles.refreshButtonWrapper
-        )}
-      >
-        <Button
-          onClick={() => {}}
-          aria-label="Refresh"
-          size="xs"
-          type="tertiary"
-          isIconOnly={true}
-          icon={{iconName: 'refresh'}}
-          className={moduleStyles.urlButton}
-        />
+        <div
+          className={classNames(
+            moduleStyles.urlButtonsWrapper,
+            moduleStyles.refreshButtonWrapper
+          )}
+        >
+          <Button
+            onClick={() => {}}
+            aria-label="Refresh"
+            size="xs"
+            type="tertiary"
+            isIconOnly={true}
+            icon={{iconName: 'refresh'}}
+            className={moduleStyles.urlButton}
+          />
+        </div>
       </div>
     </div>
   );
