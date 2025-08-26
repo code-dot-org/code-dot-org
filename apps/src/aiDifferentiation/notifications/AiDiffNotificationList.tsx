eff7f9e3e0fb3337e29e984680f1eb7a59495ff4
Liam Frye-Mason <liam.fryemason@code.org>
2025-08-26 09:15:13 -0700

    [Notifications] Add notifications list to AI TA (#67931)
    * Add a notification list UI
    
    * Add loading state
    
    * Small rename
    
    * Fix loading
    
    * Copilot suggestion

---
+++ apps/src/aiDifferentiation/notifications/AiDiffNotificationList.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import HttpClient from '@cdo/apps/util/HttpClient';
+
+import Notification from './Notification';
+
+import styles from './notifications.module.scss';
+
+export interface AiDiffNotification {
+  id: string;
+  title: string;
+  description: string;
+  readAt: Date | null;
+  iconName: string;
+  publishedAt: Date;
+}
+
+const AiDiffNotificationList: React.FC = () => {
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [notifications, setNotifications] = React.useState<
+    AiDiffNotification[]
+  >([]);
+
+  React.useEffect(() => {
+    HttpClient.fetchJson<AiDiffNotification[]>('/notifications', {}, undefined)
+      .then(response => {
+        setLoading(false);
+        const loadedNotifications = response.value.map(n => ({
+          ...n,
+          publishedAt: new Date(n.publishedAt),
+          readAt: n.readAt ? new Date(n.readAt) : null,
+        }));
+        setNotifications(loadedNotifications);
+      })
+      .catch(error => {
+        console.error('Error fetching notifications:', error);
+      });
+  }, []);
+
+  if (!loading && (!notifications || notifications.length === 0)) {
+    // TODO(lfm): add empty state design
+    return <div className={styles.listContainer}>{'no notifications'}</div>;
+  }
+
+  return (
+    <div className={styles.listContainer}>
+      <div className={styles.list}>
+        {loading ? (
+          <>
+            <Notification notification={null} key={1} />
+            <Notification notification={null} key={2} />
+            <Notification notification={null} key={3} />
+          </>
+        ) : (
+          notifications.map(notification => (
+            <Notification notification={notification} key={notification.id} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AiDiffNotificationList;
