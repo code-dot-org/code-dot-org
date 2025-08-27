e228c11ea97d03bb86255e37801438047e5e1869
kobryan0619 <kaitie.obryan@gmail.com>
2025-08-27 10:15:18 -0500

    remove index

--- dashboard/db/migrate/20250825215226_create_teaching_profile_data.rb
+++ dashboard/db/migrate/20250825215226_create_teaching_profile_data.rb
@@ -1,7 +1,7 @@
 class CreateTeachingProfileData < ActiveRecord::Migration[6.1]
   def change
     create_table :teaching_profile_data do |t|
-      t.references :user, null: false, foreign_key: true, type: :integer, index: true
+      t.references :user, null: false, foreign_key: true, type: :integer
       t.json :individual_data
 
       t.timestamps
