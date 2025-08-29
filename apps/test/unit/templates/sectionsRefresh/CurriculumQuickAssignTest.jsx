e4b27882255a86ad2a11fbff0214cb7d6cae19bb
Bethany Connor <bethany@code.org>
2025-08-29 15:13:46 -0700

    Update test

--- apps/test/unit/templates/sectionsRefresh/CurriculumQuickAssignTest.jsx
+++ apps/test/unit/templates/sectionsRefresh/CurriculumQuickAssignTest.jsx
@@ -36,7 +36,7 @@ describe('CurriculumQuickAssign', () => {
     expect(wrapper.find('h3').length).toBe(1);
     expect(wrapper.find('p').length).toBe(1);
     // We haven't specified participantType = student, so all 5 buttons appear
-    expect(wrapper.find('Button').length).toBe(5);
+    expect(wrapper.find('Button').length).toBe(6);
     expect(wrapper.find('Button').at(0).props().text).toBe(
       i18n.courseBlocksGradeBandsElementary()
     );
