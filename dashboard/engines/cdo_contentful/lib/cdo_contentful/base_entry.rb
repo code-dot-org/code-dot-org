f61e1b8e918722c11fba1e14e88dc82cb530728d
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-09-01 17:02:51 +0300

    CMS-951: Add support for `:skip` argument in fetch entry methods

--- dashboard/engines/cdo_contentful/lib/cdo_contentful/base_entry.rb
+++ dashboard/engines/cdo_contentful/lib/cdo_contentful/base_entry.rb
@@ -7,11 +7,20 @@ module CdoContentful
         @content_type || raise(NotImplementedError, "#{self}.content_type is not set")
       end
 
-      def where(limit: 100, **fields)
+      # Fetch entries of this content type matching the given fields
+      #
+      # @param limit [Integer] maximum number of entries to return (default: 100, max: 1000)
+      # @param skip [Integer] number of entries to skip, acting as an offset (default: 0)
+      # @param fields [Hash] field-value pairs to filter entries by, e.g. `where(tutorialID: 'my-id')`
+      # @return [Contentful::Array] of entries matching the given fields
+      def where(limit: 100, skip: 0, **fields)
         entry_fields = fields.transform_keys {"fields.#{_1}"}
-        client.entries(**entry_fields, content_type:, limit:)
+        client.entries(**entry_fields, content_type:, limit:, skip:)
       end
 
+      # Fetch a single entry of this content type matching the given fields
+      #
+      # @return [Contentful::Entry, nil] the first entry matching the given fields
       def find_by(**fields)
         where(**fields, limit: 1).first
       end
