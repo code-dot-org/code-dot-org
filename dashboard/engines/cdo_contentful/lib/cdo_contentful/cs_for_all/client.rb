3fd604ced5f2644d04a8a5045b86ce541647092f
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-09-03 15:25:02 +0300

    CMS-951: Fix client configs

--- dashboard/engines/cdo_contentful/lib/cdo_contentful/cs_for_all/client.rb
+++ dashboard/engines/cdo_contentful/lib/cdo_contentful/cs_for_all/client.rb
@@ -3,7 +3,7 @@
 module CdoContentful
   module CsForAll
     class Client < BaseClient
-      self.config = CdoContentful.config.cs_for_all.client
+      self.config = CdoContentful.config.cs_for_all.client&.deep_symbolize_keys!
     end
   end
 end
