d216a63dda5e0fcec5c287a65d45098217c541af
Ben Brooks <ben@code.org>
2025-09-04 14:34:57 -0700

    Revert "Grayer text"
    This reverts commit 81cfd665d39554ae1328b334cfbcea094b4a7f4e.

--- apps/src/weblab2/layout/VerticalLayout.tsx
+++ apps/src/weblab2/layout/VerticalLayout.tsx
@@ -160,7 +160,7 @@ const VerticalLayout: React.FunctionComponent<LayoutProps> = ({
             ) : (
               <SegmentedButtons {...viewModeButtonsProps} />
             )}
-            <OverlineTwoText noMargin className={moduleStyles.headerText}>
+            <OverlineTwoText noMargin>
               {weblab2I18n.workspace()}
             </OverlineTwoText>
             <HeaderButtons />
