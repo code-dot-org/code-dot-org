4a32988b744463ffa631d2944b4eb18eec5aa41e
Ryan Driscoll <driscollrp@gmail.com>
2025-09-05 14:05:14 -0600

    refactor: use sx prop with color map constant rather than module styles

--- apps/src/code-studio/pd/workshop_dashboard/workshops/surveys/components/PercentageBarGroup.tsx
+++ apps/src/code-studio/pd/workshop_dashboard/workshops/surveys/components/PercentageBarGroup.tsx
@@ -6,7 +6,8 @@ import {Box} from '@mui/material';
 import classNames from 'classnames';
 import React, {FC, useEffect, useState} from 'react';
 
-import {Breakdown} from '../../../WorkshopFormTemplate/types';
+import {Breakdown, ColorMapKey} from '../../../WorkshopFormTemplate/types';
+import {COLOR_MAP} from '../constants';
 
 import styles from './PercentageBarGroupStyles.module.scss';
 import commonStyles from '../../workshop.module.scss';
@@ -19,7 +20,7 @@ interface PercentageBarGroupProps {
 
 interface PercentageBarProps {
   percentage: number;
-  className?: string;
+  color?: ColorMapKey;
 }
 
 export const PercentageBarGroup: FC<PercentageBarGroupProps> = ({
@@ -35,10 +36,7 @@ export const PercentageBarGroup: FC<PercentageBarGroupProps> = ({
             <StrongText>{item.label}</StrongText>
           </BodyThreeText>
           <Box className={styles.barRow}>
-            <PercentageBar
-              percentage={item.percentage}
-              className={item.status && styles[item.status]}
-            />
+            <PercentageBar percentage={item.percentage} color={item.color} />
             <BodyThreeText noMargin className={styles.barLabel}>{`${
               item.count
             }${barLabel ? ` ${barLabel}` : ''}`}</BodyThreeText>
@@ -49,7 +47,10 @@ export const PercentageBarGroup: FC<PercentageBarGroupProps> = ({
   );
 };
 
-const PercentageBar: FC<PercentageBarProps> = ({percentage, className}) => {
+const PercentageBar: FC<PercentageBarProps> = ({
+  percentage,
+  color = 'teal',
+}) => {
   const [width, setWidth] = useState(0);
 
   useEffect(() => {
@@ -62,12 +63,15 @@ const PercentageBar: FC<PercentageBarProps> = ({percentage, className}) => {
     return () => clearTimeout(timer);
   }, [percentage]);
 
+  const fillColor = COLOR_MAP.get(color);
+
   return (
     <Box className={styles.barContainer}>
       <Box
-        className={classNames(styles.indicator, className)}
-        style={{
+        className={styles.indicator}
+        sx={{
           width: `${width}%`,
+          backgroundColor: fillColor,
         }}
       />
     </Box>
