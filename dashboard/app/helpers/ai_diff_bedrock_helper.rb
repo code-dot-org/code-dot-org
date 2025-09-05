cd7837efd1d8f31f9807362e798e11a9f7710196
Ceara Chewning <ceara.chewning@code.org>
2025-09-05 16:26:07 -0400

    Change model to sonnet 4 with correct inference profile

--- dashboard/app/helpers/ai_diff_bedrock_helper.rb
+++ dashboard/app/helpers/ai_diff_bedrock_helper.rb
@@ -1,8 +1,8 @@
 module AiDiffBedrockHelper
   MAX_TOKENS = 1500
   TEMP = 0.5
-  MODEL_ID = 'anthropic.claude-3-sonnet-20240229-v1:0'
-  MODEL_ARN = 'arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0'
+  MODEL_ID = 'us.anthropic.claude-sonnet-4-20250514-v1:0'
+  MODEL_ARN = 'arn:aws:bedrock:us-east-1:475661607190:inference-profile/us.anthropic.claude-sonnet-4-20250514-v1:0'
   # TODO: extract this to a secret or other centralized parameter once KB is deployed via cloudformation.
   KB_ID = 'ODWSNBOEZG'
   RETRIEVAL_LIMIT = 10
