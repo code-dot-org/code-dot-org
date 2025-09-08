2e40f8448d1f604f8d194c275f6fff3a016e4377
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-09-08 16:11:14 -0700

    explicitly specify schema version

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_10_13_190700) do
-
+ActiveRecord::Schema[7.0].define(version: 2025_10_13_190700) do
   create_table "activities", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "level_id"
