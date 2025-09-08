943edcd44f5aecaf27112fc55b0efd16371c8682
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-09-08 11:06:26 -0700

    add explicit requires for code used in initializers

--- dashboard/config/initializers/devise.rb
+++ dashboard/config/initializers/devise.rb
@@ -1,3 +1,5 @@
+require_relative '../../app/helpers/locale_helper'
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
