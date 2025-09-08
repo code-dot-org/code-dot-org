94ca91682c7a514adb56786a7413b2de9279f2cc
Molly Moen <molly@code.org>
2025-09-08 16:15:23 -0700

    add comment

--- apps/src/pythonlab/progress/PythonValidationTracker.ts
+++ apps/src/pythonlab/progress/PythonValidationTracker.ts
@@ -36,6 +36,9 @@ export default class PythonValidationTracker {
     if (isChangingLevels) {
       this.validationResults = undefined;
     } else {
+      // If we are not changing levels, keep the test names but set all results to pending.
+      // This lets us show the user the test names in the table while the tests are running.
+      // Since tests are defined on the level, they can't change in a single page load.
       this.validationResults = this.validationResults?.map(result => ({
         message: result.message,
         result: 'PENDING',
