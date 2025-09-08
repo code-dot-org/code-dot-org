25807cc6581adca716dd3ee6a04b91a533e150e5
Juan Manzo <juan.manzo@code.org>
2025-09-07 21:04:28 -0700

    Add test coverage

--- dashboard/test/lib/queries/user/inactive_test.rb
+++ dashboard/test/lib/queries/user/inactive_test.rb
@@ -62,13 +62,18 @@ class Queries::User::InactiveTest < ActiveSupport::TestCase
 
       subject(:inactive_users) {described_instance.call}
 
-      it 'returns users who have not signed in since the given date' do
+      it 'returns inactive users who have not been sent deletion warning email' do
+        expected_user = create(:teacher, current_sign_in_at: inactive_since - 1.day, user_data_retention_status: create(:user_data_retention_status))
+        _(inactive_users).must_include expected_user
+      end
+
+      it 'returns inactive users who have been sent deletion warning email more than 40 months ago' do
         expected_user = create(:teacher, current_sign_in_at: inactive_since - 1.day, user_data_retention_status: create(:user_data_retention_status))
         expected_user.user_data_retention_status.update!(deletion_warning_email_sent_at: 42.months.ago)
         _(inactive_users).must_include expected_user
       end
 
-      it 'returns users who have not signed in since the given date' do
+      it 'does not return inactive user who has been sent deletion warning email less than 40 months ago' do
         expected_user = create(:teacher, current_sign_in_at: inactive_since - 1.day, user_data_retention_status: create(:user_data_retention_status))
         expected_user.user_data_retention_status.update!(deletion_warning_email_sent_at: 40.months.ago)
         _(inactive_users).wont_include expected_user
