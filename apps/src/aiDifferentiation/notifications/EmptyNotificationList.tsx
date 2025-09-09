e2772bc261599cba2316fb39cc5c422288265d5f
Liam Frye-Mason <liam.fryemason@code.org>
2025-09-09 13:44:14 -0700

    Change image

--- apps/src/aiDifferentiation/notifications/EmptyNotificationList.tsx
+++ apps/src/aiDifferentiation/notifications/EmptyNotificationList.tsx
@@ -2,7 +2,7 @@ import {BodyTwoText} from '@code-dot-org/component-library/typography';
 import React from 'react';
 
 import i18n from '@cdo/locale';
-import aiBot from '@cdo/static/ai-bot-ta.png';
+import emptyPng from '@cdo/static/empty-notification-list.png';
 
 import styles from './notifications.module.scss';
 
@@ -10,7 +10,7 @@ const EmptyNotificationList: React.FC = () => {
   return (
     <div className={styles.emptyNotificationListContainer}>
       <div className={styles.emptyNotificationList}>
-        <img alt="AI bot - unread notifications" src={aiBot} />
+        <img alt="AI bot - unread notifications" src={emptyPng} />
         <BodyTwoText>{i18n.noNotifications()}</BodyTwoText>
       </div>
     </div>
