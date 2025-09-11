eb02cccab3aa26892a63cd6b02c835adb17d4a96
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-09-11 13:48:27 -0700

    bypass csp for pw

--- frontend/apps/marketing/playwright.config.ts
+++ frontend/apps/marketing/playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig<EyesFixture>({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    bypassCSP: true,
     eyesConfig: {
       appName: 'Marketing',
       matchLevel: 'Strict',
