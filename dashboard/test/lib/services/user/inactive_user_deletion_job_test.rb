46ac3908c5c4a07a29973ff4c9c2141a0bf2d4db
Juan Manzo <juan.manzo@code.org>
2025-09-15 09:39:09 -0700

    WIP

--- dashboard/test/lib/services/user/inactive_user_deletion_job_test.rb
+++ dashboard/test/lib/services/user/inactive_user_deletion_job_test.rb
@@ -7,8 +7,8 @@ class User::InactiveUserDeletionJobTest < ActiveJob::TestCase
   subject(:perform_later) {User::InactiveUserDeletionJob.perform_later}
 
   let(:email) {Faker::Internet.unique.email}
-  let(:student) {create(:student, current_sign_in_at: 54.months.ago - 1.day)}
-  let(:teacher) {create(:teacher, current_sign_in_at: 54.months.ago - 1.day)}
+  let!(:student) {create(:student, current_sign_in_at: 54.months.ago - 1.day)}
+  let!(:teacher) {create(:teacher, current_sign_in_at: 54.months.ago - 1.day)}
 
   it 'enqueues job to "default" queue' do
     assert_enqueued_with(job: described_class, queue: 'default') do
@@ -18,6 +18,9 @@ class User::InactiveUserDeletionJobTest < ActiveJob::TestCase
 
   it 'sends' do
     perform_enqueued_jobs {perform_later}
+    student.reload
+    teacher.reload
+    puts student.summarize
     _(student.deleted_at).wont_be_nil
     _(teacher.deleted_at).wont_be_nil
   end
