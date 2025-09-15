0bb6f20cc77c7a017cd4a74f60c23f995adaf291
Molly Moen <molly@code.org>
2025-09-15 10:11:31 -0700

    add tests and update default behavior

--- apps/src/weblab2/helpers/aiTutorHelper.ts
+++ apps/src/weblab2/helpers/aiTutorHelper.ts
@@ -50,6 +50,8 @@ export const getPromptNameFromMode = (mode: string | undefined) => {
 
 // Given a list of modes, return the corresponding system prompt options.
 // Any mode in the list that is not a valid mode will be ignored.
+// If there are no valid modes in the list, return a single option with the
+// default mode.
 export const getPromptOptionsFromModes = (modes: string[]) => {
   const possibleModes = Object.keys(MODE_MAP);
   const options = modes.map(mode => {
@@ -61,9 +63,17 @@ export const getPromptOptionsFromModes = (modes: string[]) => {
       };
     }
   });
-  return options.filter(Boolean) as {
+  const result = options.filter(Boolean) as {
     displayName: string;
     icon: FontAwesomeV6IconProps;
     promptName: string;
   }[];
+  if (result.length === 0) {
+    result.push({
+      displayName: MODE_MAP.suggest.displayName,
+      icon: {iconName: MODE_MAP.suggest.iconName},
+      promptName: 'weblab2-suggest',
+    });
+  }
+  return result;
 };
