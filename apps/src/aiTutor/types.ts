3b1e113aca19f7d207f96c4581dd9ffad5c815c2
Cassi Brenci <cassi.brenci@code.org>
2025-09-16 15:11:48 -0700

    Remove interactionsApi and related ruby code.

--- apps/src/aiTutor/types.ts
+++ apps/src/aiTutor/types.ts
@@ -1,29 +1,3 @@
-/** TODO-AITUTOR: Clean up and remove deprecated types along with chatApi and interactionsApi cleanup
- **/
-
-import {ValueOf} from '@cdo/apps/types/utils';
-import {
-  AiTutorInteractionStatus as AITutorInteractionStatus,
-  AiTutorTypes as AITutorActions,
-} from '@cdo/generated-scripts/sharedConstants';
-
-export type AITutorAction = ValueOf<typeof AITutorActions>;
-export type AITutorInteractionStatusValue = ValueOf<
-  typeof AITutorInteractionStatus
->;
-
-export interface AITutorInteraction {
-  userId?: number;
-  levelId?: number;
-  scriptId?: number;
-  type: AITutorAction | undefined;
-  prompt: string;
-  status: AITutorInteractionStatusValue;
-  aiResponse?: string;
-}
-
-// below are used in new tutor!
-
 export interface StudentServerData {
   id: number;
   name: string;
