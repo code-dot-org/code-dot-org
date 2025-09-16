9746ad74b736073aafbcf279c0cbd921363ed3ef
Juan Manzo <juan.manzo@code.org>
2025-09-16 09:13:37 -0700

    Inactive user deletion job

--- dashboard/app/jobs/user/inactive_user_deletion_job.rb
+++ dashboard/app/jobs/user/inactive_user_deletion_job.rb
@@ -2,8 +2,16 @@
 
 class User
   class InactiveUserDeletionJob < ApplicationJob
-    def perform
-      inactive_users.find_each(&:destroy!)
+    LOGGING_NAMESPACE = 'Platform/InactiveUserDeletion'
+    def perform(dry_run: false)
+      if dry_run
+        log_message("Would delete #{inactive_users.count}")
+        inactive_users
+      else
+        count = inactive_users.count
+        inactive_users.find_each(&:destroy!)
+        log_message("Deleted #{count} users")
+      end
     end
 
     private def inactive_users
@@ -11,8 +19,10 @@ class User
         inactive_since: 54.months.ago,
       )
       @inactive_users ||= inactive_query.call
-      puts @inactive_users.count
-      @inactive_users
+    end
+
+    private def log_message(message)
+      CDO.log.info({event: message, namespace: LOGGING_NAMESPACE})
     end
   end
 end
