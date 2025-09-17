1dff2eb0179cda84dedc221a6676c2d11e4412c4
Eric Aderhold <eric.aderhold@code.org>
2025-09-17 13:53:05 -0700

    Truncate student code in prompt to stay under length limit

--- dashboard/test/helpers/ai_diff_bedrock_helper_test.rb
+++ dashboard/test/helpers/ai_diff_bedrock_helper_test.rb
@@ -130,6 +130,39 @@ User: oh no"
     assert_equal prompt, expected_prompt
   end
 
+  test 'Testing prompt formatting for level, not preset, with very long student code' do
+    context = SharedConstants::AI_DIFF_CONTEXT[:LEVEL]
+    course_display_name = "Computer Science Discoveries"
+    unit_name = "CSD Unit 3"
+    lesson_name = "Test Lesson Name"
+    is_preset = false
+    section_contexts = nil
+    level_instructions = 'sudo make me a sandwich'
+    student_code = {student_code: 'a' * 100000}
+    prompt = AiDiffBedrockHelper.get_prompt_for_context(
+      context,
+      course_display_name,
+      unit_name,
+      lesson_name,
+      is_preset,
+      section_contexts,
+      level_instructions,
+      student_code
+    )
+    expected_prompt = "You are a teaching assistant named Aida. It's your job to help K-12 computer science teachers using the code.org platform plan their lessons and adjust lesson plans to fit class time requirements, help students that are ahead or behind, provide alternate explanations of the material, and other relevant lesson planning tasks. Your focus is on helping teachers with lesson plans for lesson in the Computer Science Discoveries course. The teacher will either ask you questions about the current lesson plan and resources or ask you to make changes to or create new material for the lesson. When creating new material for the lesson, you must provide all the information a teacher needs. For example, if asked to create a quiz you should also provide the answer key. Your job is to use the information from the search results to help the teacher to the best of your ability, asking clarifying questions if needed. Your responses should be warm and helpful because you're the best lesson planner there could be, and you know all about computer science education.
+            The current lesson this teacher is working on is Computer Science Discoveries CSD Unit 3, Test Lesson Name.
+
+            The teacher is currently working on a level within that lesson. The instructions for this task are: sudo make me a sandwich
+
+            The student code the teacher is viewing is: #{'a' * 75000}
+
+            Here are the search results in numbered order:
+            $search_results$
+
+      $output_format_instructions$"
+    assert_equal prompt, expected_prompt
+  end
+
   test 'Testing prompt formatting for lesson, not preset' do
     context = SharedConstants::AI_DIFF_CONTEXT[:LESSON]
     course_display_name = "Computer Science Discoveries"
