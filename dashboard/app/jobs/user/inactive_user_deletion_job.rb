431420fc2ae22e6d20f013e1b291bc879412c021
Juan Manzo <juan.manzo@code.org>
2025-09-17 13:27:33 -0700

    Remove frozen_string_literal comment

--- dashboard/app/jobs/user/inactive_user_deletion_job.rb
+++ dashboard/app/jobs/user/inactive_user_deletion_job.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class User
   class InactiveUserDeletionJob < ApplicationJob
     def perform(dry_run: false, limit: nil)
--- dashboard/lib/inactive_user_deleter.rb
+++ dashboard/lib/inactive_user_deleter.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'stringio'
 require 'cdo/aws/metrics'
 require 'cdo/aws/s3'
