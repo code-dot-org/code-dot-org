615fa282351f7a8b4a20411dfc1943d7e8adbbe0
Liam Frye-Mason <liam.fryemason@code.org>
2025-09-17 12:53:45 -0700

    Fix compile

--- apps/src/sites/studio/pages/levels-lab2-main.js
+++ apps/src/sites/studio/pages/levels-lab2-main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import React from 'react';
 
 import Lab2 from '@cdo/apps/lab2/views/Lab2';
-import createReactRoot from '@cdo/apps/util/GlobalBootstrapper';
+import {createReactRoot} from '@cdo/apps/util/GlobalBootstrapper';
 
 $(document).ready(function () {
   createReactRoot(<Lab2 />, document.getElementById('lab2-container'));
