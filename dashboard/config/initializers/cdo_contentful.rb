6fe2ae9909d450024d5ba688feab402ddf73e706
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-09-18 17:49:18 +0300

    CMS-951: Configure each key separately for cdo_contentful engine client

--- dashboard/config/initializers/cdo_contentful.rb
+++ dashboard/config/initializers/cdo_contentful.rb
@@ -3,5 +3,10 @@
 require 'cdo_contentful'
 
 CdoContentful.configure do |config|
-  config.cs_for_all.client = CDO.cdo_contentful_cs_for_all_client
+  config.cs_for_all.client.tap do |client|
+    client.access_token = CDO.contentful_cs_for_all_access_token
+    client.space        = CDO.contentful_cs_for_all_space if CDO.contentful_cs_for_all_space
+    client.api_url      = CDO.contentful_cs_for_all_api_url if CDO.contentful_cs_for_all_api_url
+    client.namespace    = CDO.contentful_cs_for_all_namespace if CDO.contentful_cs_for_all_namespace
+  end
 end
