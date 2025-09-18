05cb3af91939e90ab0b611640cd61fe394b0c570
alex-m-brown <alex.brown@code.org>
2025-09-18 14:41:46 -0400

    Revert "use cached default unit group units (#68404)"
    This reverts commit b9b1089c13619eed41f5326548135434f11a0b28.

--- dashboard/app/models/unit_group.rb
+++ dashboard/app/models/unit_group.rb
@@ -638,7 +638,7 @@ class UnitGroup < ApplicationRecord
   # rubocop:enable Naming/PredicateName
 
   def single_unit_course?
-    cached.default_unit_group_units.one?
+    default_unit_group_units.one?
   end
 
   def first_unit
