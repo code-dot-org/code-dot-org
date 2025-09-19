eca872eb5413409f7f1cefb1399f0dd95cce74a7
Molly Moen <molly@code.org>
2025-09-19 12:09:10 -0700

    refactor

--- apps/src/lab2/ai/aiTutorConstants.ts
+++
@@ -1,5 +0,0 @@
-// A list of labs for which AI Tutor is always on, regardless of
-// level properties or experiment flags. Usage of AI tutor is still
-// constrained by user permissions; if the user cannot use AI Tutor
-// they will see an error message when trying to chat with it.
-export const LABS_ALWAYS_USING_AI_TUTOR = ['weblab2'];
--- apps/src/lab2/ai/shouldShowAiTutor.ts
+++ apps/src/lab2/ai/shouldShowAiTutor.ts
@@ -1,13 +1,17 @@
 import {queryParams} from '@cdo/apps/code-studio/utils';
 
-import {LABS_ALWAYS_USING_AI_TUTOR} from './aiTutorConstants';
+// A list of app names for which AI Tutor is always on, regardless of
+// level properties or experiment flags. Usage of AI tutor is still
+// constrained by user permissions; if the user cannot use AI Tutor
+// they will see an error message when trying to chat with it.
+export const APPS_ALWAYS_USING_AI_TUTOR = ['weblab2'];
 
 export const shouldShowAiTutor = (
   appName: string,
   aiTutorAvailable: boolean | undefined
 ) => {
   return (
-    LABS_ALWAYS_USING_AI_TUTOR.includes(appName) ||
+    APPS_ALWAYS_USING_AI_TUTOR.includes(appName) ||
     aiTutorAvailable ||
     queryParams('show-ai-tutor2') === 'true'
   );
