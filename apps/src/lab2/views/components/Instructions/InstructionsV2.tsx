52319464da6651ebe0c4ce0e04e08b54a530c1ee
breville <brendan@code.org>
2025-09-22 00:19:07 +0000

    Fix Validate button padding

--- apps/src/lab2/views/components/Instructions/InstructionsV2.tsx
+++ apps/src/lab2/views/components/Instructions/InstructionsV2.tsx
@@ -143,12 +143,14 @@ const Instructions: React.FunctionComponent<InstructionsProps> = ({
             </div>
           )}
           {includeValidation && (
-            <ValidationButton
-              onValidate={validationSettings.onValidate}
-              onStopValidation={validationSettings.onStopValidation}
-              isValidating={validationSettings.isValidating}
-              isValidateDisabled={validationSettings.isValidateDisabled}
-            />
+            <div className={moduleStyles.nonScrollingSubContent}>
+              <ValidationButton
+                onValidate={validationSettings.onValidate}
+                onStopValidation={validationSettings.onStopValidation}
+                isValidating={validationSettings.isValidating}
+                isValidateDisabled={validationSettings.isValidateDisabled}
+              />
+            </div>
           )}
           {bottomComponent && (
             <div className={moduleStyles.bottomComponent}>
