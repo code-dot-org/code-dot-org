2ed8b3960611002226f3dc6cee1fc56ce2c55e15
Ryan Driscoll <driscollrp@gmail.com>
2025-09-23 11:33:49 -0600

    chore: remove queryParam and props for disabling ai chat from AiTutor2Chat
    moved to context

--- apps/src/lab2/views/components/AiTutor2Chat.tsx
+++ apps/src/lab2/views/components/AiTutor2Chat.tsx
@@ -99,9 +99,6 @@ const AiTutor2Chat: React.FunctionComponent<AiTutor2ChatProps> = ({
 }) => {
   const [systemPrompt, setSystemPrompt] = useState<string>();
 
-  // Temporarily use query param to disable ai chat
-  const disableAiChat = queryParams('disable-ai-chat');
-
   const fetchPrompt = (promptName: string) => {
     fetchCustomPrompt(promptName)
       .then(prompt => {
@@ -189,7 +186,6 @@ const AiTutor2Chat: React.FunctionComponent<AiTutor2ChatProps> = ({
         levelName={levelName}
         channelId={channelId}
         hideModelChangeMessage={true}
-        disabled={!!disableAiChat}
       />
     </div>
   ) : (
