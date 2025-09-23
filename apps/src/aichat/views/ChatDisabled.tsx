2fbd6198d8945ec177b6ae4e589502a22b4f40a3
Ryan Driscoll <driscollrp@gmail.com>
2025-09-23 11:32:05 -0600

    refactor: move default chat disabled message to ChatDisabled component and remove tutor specific message for now

--- apps/src/aichat/views/ChatDisabled.tsx
+++ apps/src/aichat/views/ChatDisabled.tsx
@@ -3,13 +3,17 @@ import React, {FC} from 'react';
 
 import aiBotLockedIcon from '@cdo/static/aichat/ai-bot-locked-icon.png';
 
+import aichatI18n from '../locale';
+
 import styles from './chatWorkspace.module.scss';
 
-export const ChatDisabled: FC<{text: string}> = ({text}) => {
+export const ChatDisabled: FC<{message?: string}> = ({
+  message = aichatI18n.aiChatDisabled(),
+}) => {
   return (
     <div className={styles.chatDisabledContainer}>
       <img src={aiBotLockedIcon} alt="" className={styles.chatDisabledIcon} />
-      <BodyThreeText>{text}</BodyThreeText>
+      <BodyThreeText>{message}</BodyThreeText>
     </div>
   );
 };
