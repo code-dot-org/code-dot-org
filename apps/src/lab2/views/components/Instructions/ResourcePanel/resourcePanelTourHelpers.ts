8e52cf09dee1103033c428203c9b857003d45476
Alice Fisher <alice.fisher@code.org>
2025-09-24 19:26:04 +0000

    remove dontShowAgain

--- apps/src/lab2/views/components/Instructions/ResourcePanel/resourcePanelTourHelpers.ts
+++ apps/src/lab2/views/components/Instructions/ResourcePanel/resourcePanelTourHelpers.ts
@@ -6,26 +6,22 @@ export const STEPS = [
     element: '#resource-panel-instructions',
     title: lab2I18n.resourcePanelOnboarding_title(),
     intro: lab2I18n.resourcePanelOnboarding_text(),
-    dontShowAgain: true,
   },
   {
     element: '#resource-panel-tabs',
     title: lab2I18n.resourcePanelOnboarding_tabsTitle(),
     intro: lab2I18n.resourcePanelOnboarding_tabsText(),
     position: 'right',
-    dontShowAgain: true,
   },
   {
     element: '#resource-panel-links',
     title: lab2I18n.resourcePanelOnboarding_linksTitle(),
     intro: lab2I18n.resourcePanelOnboarding_linksText(),
     position: 'top-right',
-    dontShowAgain: true,
   },
   {
     element: '#resource-panel-navigation-button',
     title: lab2I18n.resourcePanelOnboarding_finishTitle(),
     intro: lab2I18n.resourcePanelOnboarding_finishText(),
-    dontShowAgain: true,
   },
 ];
