2a85a86b9550dd781affea6436322d91ae7d6dde
Liam Frye-Mason <liam.fryemason@code.org>
2025-09-24 14:39:56 -0700

    Only use en-US

--- dashboard/app/controllers/notifications_controller.rb
+++ dashboard/app/controllers/notifications_controller.rb
@@ -3,8 +3,10 @@ class NotificationsController < ApplicationController
 
   # Returns a list of contentful notifications for the user that have not expired or been dismissed
   def index
-    locale = params[:locale] || I18n.default_locale
-
+    # TODO: Contentful needs to support full list of locales. For now, we only use the default, 'en-US'.
+    # We should use request.locale when that happens.
+    # contentful docs: https://www.contentful.com/developers/docs/tutorials/general/setting-locales/
+    locale =  I18n.default_locale
     results = Notifications.get_all(current_user.id, locale)
 
     render json: results.as_json.map {|notification| notification.deep_transform_keys {|key| key.to_s.camelize(:lower)}}, status: :ok
@@ -13,7 +15,10 @@ class NotificationsController < ApplicationController
   def mark_as_read
     external_notification_ids = (params[:external_notification_ids] || []).compact_blank
 
-    locale = params[:locale] || I18n.default_locale
+    # TODO: Contentful needs to support full list of locales. For now, we only use the default, 'en-US'.
+    # We should use request.locale when that happens.
+    # contentful docs: https://www.contentful.com/developers/docs/tutorials/general/setting-locales/
+    locale = I18n.default_locale
 
     if external_notification_ids.empty?
       render json: {status: 'error', message: 'No notification IDs provided'}, status: :bad_request
