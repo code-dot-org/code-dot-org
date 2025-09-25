d75b065bb1aa2582348867710402c7e8cae62953
Darin Webb <darin@code.org>
2025-09-24 21:35:27 -0500

    remove set-last-dtt-green

--- pegasus/test/test_dev_routes.rb
+++ pegasus/test/test_dev_routes.rb
@@ -149,36 +149,6 @@ class DevRoutesTest < Minitest::Test
       end
     end
 
-    describe 'api/dev/set-last-dtt-green' do
-      before do
-        $log.level = Logger::ERROR
-
-        CDO.stubs(slack_set_last_dtt_green_token: FAKE_SLACK_SLASH_TOKEN)
-      end
-
-      it 'is forbidden on non-test environments' do
-        [:development, :staging, :adhoc, :levelbuilder, :production].each do |env|
-          with_rack_env(env) do
-            pegasus = make_test_pegasus
-            pegasus.post '/api/dev/set-last-dtt-green', DEFAULT_PARAMS
-            assert_equal 403, pegasus.last_response.status
-          end
-        end
-      end
-
-      it 'succeeds on test environment' do
-        with_rack_env(:test) do
-          fake_sha = 'abcdef'
-          GitHub.expects(:sha).returns(fake_sha)
-          DevelopersTopic.expects(:set_dtt).with('yes')
-          InfraTestTopic.expects(:set_green_commit).with(fake_sha)
-          pegasus = make_test_pegasus
-          pegasus.post '/api/dev/set-last-dtt-green', DEFAULT_PARAMS
-          assert_equal 200, pegasus.last_response.status
-        end
-      end
-    end
-
     describe 'api/dev/check-dts' do
       GITHUB_PAYLOAD = {
         action: 'opened',
