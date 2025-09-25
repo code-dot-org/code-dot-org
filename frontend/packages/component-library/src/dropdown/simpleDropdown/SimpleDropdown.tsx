4c895c4e21ba331799fc6ebbe2d4f47956e4615e
moshebaricdo <168464139+moshebaricdo@users.noreply.github.com>
2025-09-25 18:28:51 -0400

    refactor(dropdown/simple): minimize diff by reverting to param destructuring

--- frontend/packages/component-library/src/dropdown/simpleDropdown/SimpleDropdown.tsx
+++ frontend/packages/component-library/src/dropdown/simpleDropdown/SimpleDropdown.tsx
@@ -69,30 +69,28 @@ export interface SimpleDropdownProps
  * Used to render simple SimpleDropdowns with styled select (SimpleDropdown button)
  * and browser's native select options.
  */
-const SimpleDropdown: React.FunctionComponent<SimpleDropdownProps> = props => {
-  const {
-    items = [],
-    itemGroups = [],
-    selectedValue,
-    onChange,
-    name,
-    id,
-    className,
-    labelText,
-    iconLeft,
-    helperMessage,
-    helperIcon,
-    errorMessage,
-    dropdownTextThickness = 'thick',
-    isLabelVisible = true,
-    disabled = false,
-    readOnly = false,
-    color = 'black',
-    size = 'm',
-    styleAsFormField = false,
-    ...rest
-  } = props;
-
+const SimpleDropdown: React.FunctionComponent<SimpleDropdownProps> = ({
+  items = [],
+  itemGroups = [],
+  selectedValue,
+  onChange,
+  name,
+  id,
+  className,
+  labelText,
+  iconLeft,
+  helperMessage,
+  helperIcon,
+  errorMessage,
+  dropdownTextThickness = 'thick',
+  isLabelVisible = true,
+  disabled = false,
+  readOnly = false,
+  color = 'black',
+  size = 'm',
+  styleAsFormField = false,
+  ...rest
+}) => {
   useEffect(() => {
     if (color === 'white') {
       console.warn(
