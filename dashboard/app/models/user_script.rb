d9d3feb05654683c5ec487ad7b88376d31451b4a
David Bailey <davidsbailey@users.noreply.github.com>
2025-09-26 14:17:04 +0000

    simplify the case with nil unit group

--- dashboard/app/models/user_script.rb
+++ dashboard/app/models/user_script.rb
@@ -63,27 +63,26 @@ class UserScript < ApplicationRecord
   # Helper method which provides find-or-create functionality, with additional logic to prevent
   # duplicate rows from being created while we work on migrating existing rows to have a
   # unit_group_id. More specifically:
-  # - if an existing record is found with a nil unit_group, and the unit has an original_unit_group,
-  #   we will update that record to set the unit_group to the original_unit_group.
-  # - when searching for existing records associated with the original unit group id, be sure to
-  #   find any row that is migrated (original unit group) or unmigrated (nil unit group).
+  # - default unit_group to the unit's original_unit_group if not provided
+  # - if searching for original unit group, also find rows with nil unit_group to migrate them to
+  #   the original unit group
   #
-  # TODO: TEACH-2168 once unit_group_id is a required field, remove this helper and call
-  # find_or_create_by! instead.
+  # TODO: TEACH-2168 once unit_group_id has been backfilled and has been marked as a required field,
+  # remove the migration logic from this method and rename the method.
   def self.find_and_migrate_or_create_by!(user_id:, unit:, unit_group: nil)
     unless unit_group.nil? || unit.old_professional_learning_course? || unit.cached.unit_groups.include?(unit_group)
       raise "Unit #{unit.name} must belong to Unit Group #{unit_group&.name}"
     end
 
-    # When we find a user_script, should we migrate it to the original_unit_group?
-    # 1. If the unit has no original unit group, do not migrate.
-    # 2. If the unit_group provided is nil or the original unit group, we should migrate.
-    # 3. If the unit_group provided is a non-original unit group, do not migrate.
+    # default to original unit group if none provided
     original_unit_group = unit.get_original_unit_group
-    should_migrate = original_unit_group && (unit_group.nil? || unit_group == original_unit_group)
+    unit_group ||= original_unit_group
+
+    # Whether to migrate existing rows with nil unit_group to the original unit group
+    should_migrate = unit_group == original_unit_group
 
     # find
-    unit_groups_to_find_by = should_migrate ? [nil, original_unit_group] : [unit_group]
+    unit_groups_to_find_by = should_migrate ? [nil, unit_group] : [unit_group]
     us = find_by(user_id: user_id, script: unit, unit_group: unit_groups_to_find_by)
 
     # migrate
@@ -94,7 +93,6 @@ class UserScript < ApplicationRecord
     return us if us
 
     # create
-    unit_group_to_create = should_migrate ? original_unit_group : unit_group
-    create!(user_id: user_id, script: unit, unit_group: unit_group_to_create)
+    create!(user_id: user_id, script: unit, unit_group: unit_group)
   end
 end
