90380d5a89576ec1d15eb8eca2347e859b9043e4
breville <brendan@code.org>
2025-09-29 07:15:32 +0000

    hoai2025: adlib in level

--- apps/src/music/types.ts
+++ apps/src/music/types.ts
@@ -1,11 +1,14 @@
 import * as GoogleBlockly from 'blockly/core';
 
+import {AdlibType} from '@cdo/apps/lab2/views/components/guide/Adlib';
+
 import {ExemplarSettings, LabConfig, ProjectLevelData} from '../lab2/types';
 import {ValueOf} from '../types/utils';
 
 import {ToolboxData} from './blockly/toolbox/types';
 import {BlockMode} from './constants';
 import {Sounds} from './player/MusicLibrary';
+
 // TODO: Use this interface when converting MusicView to TypeScript
 export interface MusicLevelData extends ProjectLevelData {
   toolbox?: ToolboxData;
@@ -23,7 +26,8 @@ export interface MusicLevelData extends ProjectLevelData {
   toolboxDefinition?: GoogleBlockly.utils.toolbox.ToolboxInfo;
   validationTimeout?: number;
   aiCodeGenerate?: boolean;
-  aiCodeGenerateAdlib?: string;
+  aiCodeGenerateAdlibId?: string;
+  aiCodeGenerateAdlib?: AdlibType;
 }
 
 export type ExemplarValidationMode = 'default' | 'type';
