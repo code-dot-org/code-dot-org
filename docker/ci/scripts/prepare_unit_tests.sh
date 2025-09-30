2ecd5bdf742acdbb6c015c0605d4863ab5429ee3
Seth Nickell <snickell@gmail.com>
2025-09-29 16:18:30 -1000

    Replace CDO.geocoder_redis_url with CDO.redis_url

--- docker/ci/scripts/prepare_unit_tests.sh
+++ docker/ci/scripts/prepare_unit_tests.sh
@@ -29,7 +29,6 @@ skip_seed_all: true
 
 # Unit test settings and secrets
 build_i18n: true
-geocoder_redis_url: 'redis://unit-tests-redis:6379/0/geocoder'
 optimize_rails_assets: false
 optimize_webpack_assets: false
 session_store_server: 'redis://unit-tests-redis:6379/0/session'
