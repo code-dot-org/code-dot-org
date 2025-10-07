11d088861cedb8902bd7c4f1a7385149991c0a0e
Ryan Driscoll <ryan.driscoll@code.org>
2025-10-07 15:29:46 -0600

    feat(broad tutor): wire up sidebar prompt buttons (#68708)
    * refactor: move ai tutor model parameters to hook for reuse in quick access sidebar prompt buttons
    
    * feat: pass hiddenContextCallback and toggleAiChat functions through to sidebar suggested prompts component
    
    * feat: wire up suggested prompt buttons to toggle open chat panel and submit chat contents
    
    * test: add unit test for new useAiTutorModelParameters hook
    
    * chore: update test and types after rebase
    
    * feat: add real hidden context function

--- apps/src/aiTutor/views/legacyLabs/AiTutorContainer.tsx
+++ apps/src/aiTutor/views/legacyLabs/AiTutorContainer.tsx
@@ -69,6 +69,7 @@ export const AiTutorContainer: FC<{
         <AiTutorSidebar
           toggleAiChat={toggleAiChat}
           suggestedPrompts={allPrompts}
+          hiddenContextCallback={getHiddenContext}
         />
       )}
     </>
