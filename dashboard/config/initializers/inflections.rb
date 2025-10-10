cec2cffbaeb6bd2bb163ea12c2cb6c5b78c1e873
Mario Gil Correa <mario.gil-correa@code.org>
2025-10-10 12:41:42 -0700

    Removed changes in  inflections

--- dashboard/config/initializers/inflections.rb
+++ dashboard/config/initializers/inflections.rb
@@ -16,8 +16,8 @@
 # end
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.acronym 'CAP' # Child Account Policy
-  inflect.acronym 'DSL' # Domain-Specific Language
-  inflect.acronym 'JSON' # JavaScript Object Notation
+  inflect.acronym "DSL" # Domain-Specific Language
+  inflect.acronym "JSON" # JavaScript Object Notation
   inflect.acronym 'NRPS' # Names and Role Provisioning Services
+  inflect.acronym 'CAP' # Child Account Policy
 end
