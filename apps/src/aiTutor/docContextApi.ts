0017972bb688570043430c485797307708344cdd
Ryan Driscoll <ryan.driscoll@code.org>
2025-10-17 15:27:03 -0600

    refactor: remove promise wrapper and replace iife with separate function for easier testing

--- apps/src/aiTutor/docContextApi.ts
+++ apps/src/aiTutor/docContextApi.ts
@@ -9,29 +9,26 @@ export const tryFetchDocsForClass = async (programmingClassKey: string) => {
   if (docsCache.has(programmingClassKey)) {
     return await docsCache.get(programmingClassKey);
   }
-  docsCache.set(
-    programmingClassKey,
-    new Promise(resolve => {
-      (async () => {
-        try {
-          const url = `/docs/ide/pythonlab/classes/${programmingClassKey}/get_serialized`;
-          const response = await HttpClient.get(url);
-          const classDocs = JSON.stringify(await response.json());
-          resolve(classDocs);
-        } catch (error) {
-          console.error(
-            `🤖: error getting docs for ${programmingClassKey}: `,
-            error
-          );
-          MetricsReporter.logError({
-            event: MetricEvent.AI_TUTOR_FETCH_DOCS_FOR_CLASS_FAIL,
-            errorMessage: JSON.stringify(error),
-          });
-          resolve('');
-        }
-      })();
-    })
-  );
+
+  docsCache.set(programmingClassKey, fetchDocsForClass(programmingClassKey));
 
   return await docsCache.get(programmingClassKey);
+};
+
+export const fetchDocsForClass = async (
+  programmingClassKey: string
+): Promise<string> => {
+  try {
+    const url = `/docs/ide/pythonlab/classes/${programmingClassKey}/get_serialized`;
+    const response = await HttpClient.get(url);
+    const classDocs = JSON.stringify(await response.json());
+    return classDocs;
+  } catch (error) {
+    console.error(`🤖: error getting docs for ${programmingClassKey}: `, error);
+    MetricsReporter.logError({
+      event: MetricEvent.AI_TUTOR_FETCH_DOCS_FOR_CLASS_FAIL,
+      errorMessage: JSON.stringify(error),
+    });
+    return '';
+  }
 };
