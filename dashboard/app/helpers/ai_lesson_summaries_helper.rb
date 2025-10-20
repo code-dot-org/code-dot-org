b08b7a5ab461a31526cfabd6607a61347cdaa47b
Mark Barnes <mark.barnes@code.org>
2025-10-20 11:31:49 -0500

    set up API call and save response to db

--- dashboard/app/helpers/ai_lesson_summaries_helper.rb
+++ dashboard/app/helpers/ai_lesson_summaries_helper.rb
@@ -1,19 +1,76 @@
 module AiLessonSummariesHelper
-  def self.get_lesson_materials(lesson_id)
-    lesson = Lesson.find(lesson_id).summarize_for_lesson_show(current_user, true)
-    @lesson_materials = {}
-    @lesson_materials[:overview] = lesson.overview
-    @lesson_materials[:purpose] = lesson.purpose
-    @lesson_materials[:assessment_opportunities] = lesson.assessment_opportunities
-    @lesson_materials[:standards] = []
-    lesson.standards.each do |s|
-      @lesson_materials[:standards] << s.description
+  # API_KEY = CDO.openai_lesson_summaries_api_key
+  API_KEY = CDO.openai_measures_of_learning_api_key
+  MODEL = SharedConstants::EVALUATE_STUDENT_LEARNING_MODEL_VERSION
+
+  def self.get_ai_lesson_summary(lesson_id)
+    system_prompt = AiSystemPrompts::LessonSummariesSystemPromptHelper.get_system_prompt(lesson_id)
+
+    client = Client.new(API_KEY, MODEL)
+    response = client.request_lesson_summary(system_prompt)
+
+    response_body = JSON.parse(response.body)
+    response_body = response_body['choices'][0]['message']['content'] if response.code == 200
+    evaluation =  {status: response.code, json: response_body}
+    return {status: evaluation[:status], json: evaluation[:json]}
+  end
+
+  def self.retrieve_and_save_ai_lesson_summary(lesson_id, user_id)
+    ai_lesson_summary = get_ai_lesson_summary(lesson_id)
+    if ai_lesson_summary[:status] == 200
+      AiLessonSummary.create!({user_id: user_id, lesson_id: lesson_id, lesson_summary: ai_lesson_summary[:json]})
     end
-    @lesson_materials[:opportunity_standards] = []
-    lesson.opportunity_standards.each do |s|
-      @lesson_materials[:opportunity_standards] << s.description
+  end
+
+  class Client
+    attr_accessor :api_key, :model
+
+    OPEN_AI_URL = "https://api.openai.com/v1/chat/completions"
+
+    def initialize(api_key, model)
+      @api_key = api_key
+      @model = model
     end
-    @lesson_materials[:activities] = lesson.activities
-    @lesson_materials
+
+    def request_lesson_summary(prompt)
+      headers = {
+        "Content-Type" => "application/json",
+        "Authorization" => "Bearer #{api_key}"
+      }
+
+      data = {
+        model: model,
+        messages: [{
+          role: "system",
+          content: "You are an expert teaching assistant in a computer science classroom who has been asked to summarize the upcoming lesson to help the teacher prepare for class."
+        },
+                   {
+                     role: "user",
+                     content: prompt
+                   }],
+        response_format: {
+          type: "json_schema",
+          json_schema: {
+            name: "lesson_summary",
+            schema: {
+              type: "object",
+              properties: {
+                evaluationCriteria: {type: "string"},
+                aiEvaluation: {type: "string"},
+                aiReasoning: {type: "string"},
+              },
+            }
+          }
+        }
+      }
+
+      HTTParty.post(
+        OPEN_AI_URL,
+        headers: headers,
+        body: data.to_json,
+        open_timeout: DCDO.get('openai_http_open_timeout', 5),
+        read_timeout: DCDO.get('openai_http_read_timeout', 30)
+      )
+    end
   end
 end
--- dashboard/app/helpers/ai_system_prompts/lesson_summaries_system_prompt_helper.rb
+++ dashboard/app/helpers/ai_system_prompts/lesson_summaries_system_prompt_helper.rb
@@ -1,15 +1,24 @@
 module AiSystemPrompts::LessonSummariesSystemPromptHelper
   def self.get_system_prompt(lesson_id)
     lesson_plan = get_lesson_materials(lesson_id)
-    prompt = "You are an expert teaching assistant in a computer science classroom who has been asked to summarize the upcoming lesson to help the teacher prepare for class. Use the following lesson plan to generate your summary:
-Lesson Plan
+    prompt = "Use the following lesson plan to generate your summary:
+
+Lesson Name: #{lesson_plan[:name]}
 Lesson Overview: #{lesson_plan[:overview]}
 Learning Objectives: #{lesson_plan[:objectives]}
 #{'Lesson Purpose: '+lesson_plan[:purpose] if lesson_plan[:purpose]}
-Your summary should be composed of the following parts:
-1. Learning Objective: this should be a brief, one paragraph summary of the lesson, focusing on each of the Learning Objectives and how they will be achieved
-2. Key Lesson Beats: an ordered list of the main parts of the lesson, including activities and new vocabulary terms
-3. Common Misconceptions: an unordered list including 2 - 3 misconceptions students might have about the material being covered"
+#{'Assessment Opportunities: '+lesson_plan[:assessment_opportunities].to_s if lesson_plan[:assessment_opportunities]}
+Standards: #{lesson_plan[:standards]}
+#{'Opportunity Standards: '+lesson_plan[:opportunity_standards].to_s if lesson_plan[:opportunity_standards]}
+Activities: #{lesson_plan[:activities]}
+Preparation: #{lesson_plan[:preparation]}
+Vocabulary: #{lesson_plan[:vocabularies]}
+
+Your summary should be returned in JSON format and should be composed as follows:
+{learning_objective: this should be a brief, one paragraph summary of the lesson, focusing on each of the Learning Objectives and how they will be achieved,
+lesson_beats: an ordered list of the main parts of the lesson, including activities and new vocabulary terms,
+misconceptions: an unordered list including 2 - 3 misconceptions students might have about the material being covered,
+tips: additional strategies or ideas to help with teaching the lesson"
     prompt
   end
 
@@ -17,7 +26,12 @@ Your summary should be composed of the following parts:
     lesson = Lesson.find(lesson_id)
     lesson_materials = lesson.summarize_for_lesson_show(User.find_by_email('mark.teacher@code.org'), true)
     @lesson_plan = {}
+    @lesson_plan[:name] = lesson.name
     @lesson_plan[:overview] = lesson_materials[:overview]
+    @lesson_plan[:objectives] = []
+    lesson.objectives.each do |o|
+      @lesson_plan[:objectives] << o.description
+    end
     @lesson_plan[:purpose] = lesson.purpose
     @lesson_plan[:assessment_opportunities] = lesson.assessment_opportunities
     @lesson_plan[:standards] = []
@@ -45,21 +59,12 @@ Your summary should be composed of the following parts:
         end
         activity[:sections] << sect
       end
-
       @lesson_plan[:activities] << activity
-    end
-    @lesson_plan[:objectives] = []
-    lesson.objectives.each do |o|
-      @lesson_plan[:objectives] << o.description
     end
     @lesson_plan[:preparation] = lesson.preparation
     @lesson_plan[:vocabularies] = []
     lesson_materials[:vocabularies].each do |v|
       @lesson_plan[:vocabularies] << {word: v[:word], definition: v[:definition]}
-    end
-    @lesson_plan[:programming_expressions] = []
-    lesson_materials[:programmingExpressions].each do |pe|
-      @lesson_plan[:programming_expressions] << pe[:name]
     end
     @lesson_plan
   end
