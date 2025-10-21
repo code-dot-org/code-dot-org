b218b736afba1eefa94e07ffc43d1206a72bb376
breville <brendan@code.org>
2025-10-21 22:52:15 +0000

    Hide instructions if guide is showing

--- apps/src/music/views/MusicLabView.tsx
+++ apps/src/music/views/MusicLabView.tsx
@@ -348,41 +348,43 @@ const MusicLabView: React.FunctionComponent<MusicLabViewProps> = ({
             instructionsPosition === InstructionsPosition.RIGHT,
         })}
       >
-        <div
-          id="instructions-area"
-          className={classNames(
-            moduleStyles.instructionsArea,
-            moduleStyles.instructionsSide,
-            isStandaloneCollapsed && moduleStyles.instructionsCollapsed
-          )}
-        >
-          <ResourcePanel
-            isRunning={isPlaying}
-            handleInstructionsTextClick={onInstructionsTextClick}
-            bottomComponent={
-              exemplarPlayerInsideInstructions &&
-              showExemplarPlayer && (
-                <ExemplarPlayerView
-                  playbackEvents={exemplarPlaybackEvents}
-                  title={exemplarSettings.playerTitle!}
-                  player={player}
-                  insideInstructions={exemplarPlayerInsideInstructions}
-                />
-              )
-            }
-            hasRun={hasRun}
-            hasEdited={hasEdited}
-            fixedDarkBackground={true}
-            overrideTheme={'Light'}
-            includeFooterSpacing={false}
-            levelProperties={levelProperties}
-            headerClassName={moduleStyles.headerWithBorder}
-            settings={settings}
-            hideContinueIfDisabled={true}
-            hideNavigation={false}
-            styleNavigationAsBubble={true}
-          />
-        </div>
+        {!guideMode && (
+          <div
+            id="instructions-area"
+            className={classNames(
+              moduleStyles.instructionsArea,
+              moduleStyles.instructionsSide,
+              isStandaloneCollapsed && moduleStyles.instructionsCollapsed
+            )}
+          >
+            <ResourcePanel
+              isRunning={isPlaying}
+              handleInstructionsTextClick={onInstructionsTextClick}
+              bottomComponent={
+                exemplarPlayerInsideInstructions &&
+                showExemplarPlayer && (
+                  <ExemplarPlayerView
+                    playbackEvents={exemplarPlaybackEvents}
+                    title={exemplarSettings.playerTitle!}
+                    player={player}
+                    insideInstructions={exemplarPlayerInsideInstructions}
+                  />
+                )
+              }
+              hasRun={hasRun}
+              hasEdited={hasEdited}
+              fixedDarkBackground={true}
+              overrideTheme={'Light'}
+              includeFooterSpacing={false}
+              levelProperties={levelProperties}
+              headerClassName={moduleStyles.headerWithBorder}
+              settings={settings}
+              hideContinueIfDisabled={true}
+              hideNavigation={false}
+              styleNavigationAsBubble={true}
+            />
+          </div>
+        )}
 
         <div id="blockly-area" className={moduleStyles.blocklyArea}>
           <PanelContainer
