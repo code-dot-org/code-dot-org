2696c6984c994c7722a0f1d8fc0a1ba699e0187a
David Bailey <davidsbailey@users.noreply.github.com>
2025-10-21 16:14:13 -0700

    go back to original parallelization

--- apps/run-tests-in-parallel.sh
+++ apps/run-tests-in-parallel.sh
@@ -91,29 +91,18 @@ PARALLEL="parallel --will-cite --halt 2 -j ${PROCS} --joblog - :::"
 
 # Each line in this SCRIPT block will be run as a parallel test job
 # If any line fails, the whole block will fail and exit early
-# ${PARALLEL} <<SCRIPT || (echo && echo && echo "One of the parallel test jobs FAILED, exiting early." && echo && exit 1)
-
-echo "timing and running yarn lint" >&2
-time yarn lint > /dev/null 2>&1
-echo "timing and running unit tests" >&2
-time npx karma start --testType=unit --port=9876 > /dev/null 2>&1
-echo "timing and running storybook tests" >&2
-time npx karma start --testType=storybook --port=9877 > /dev/null 2>&1
-echo "timing and running integration tests - turtle" >&2
-time npx karma start --testType=integration --levelType='turtle' --port=9879 > /dev/null 2>&1
-echo "timing and running integration tests - maze" >&2
-time npx karma start --testType=integration --levelType='maze' --port=9880 > /dev/null 2>&1
-echo "timing and running integration tests - gamelab" >&2
-time npx karma start --testType=integration --levelType='gamelab' --port=9881 > /dev/null 2>&1
-echo "timing and running integration tests - craft" >&2
-time npx karma start --testType=integration --levelType='craft' --port=9882 > /dev/null 2>&1
-echo "timing and running integration tests - applab1" >&2
-time npx karma start --testType=integration --levelType='applab1' --port=9883 > /dev/null 2>&1
-echo "timing and running integration tests - applab2" >&2
-time npx karma start --testType=integration --levelType='applab2' --port=9884 > /dev/null 2>&1
-echo "timing and running integration tests - studio" >&2
-time npx karma start --testType=integration --levelType='studio' --port=9885 > /dev/null 2>&1
-# SCRIPT
+${PARALLEL} <<SCRIPT || (echo && echo && echo "One of the parallel test jobs FAILED, exiting early." && echo && exit 1)
+  yarn lint
+  npx karma start --testType=unit --port=9876
+  npx karma start --testType=storybook --port=9877
+  npx karma start --testType=integration --levelType='turtle' --port=9879
+  npx karma start --testType=integration --levelType='maze' --port=9880
+  npx karma start --testType=integration --levelType='gamelab' --port=9881
+  npx karma start --testType=integration --levelType='craft' --port=9882
+  npx karma start --testType=integration --levelType='applab1' --port=9883
+  npx karma start --testType=integration --levelType='applab2' --port=9884
+  npx karma start --testType=integration --levelType='studio' --port=9885
+SCRIPT
 
 echo && echo
 echo "###################################################"
