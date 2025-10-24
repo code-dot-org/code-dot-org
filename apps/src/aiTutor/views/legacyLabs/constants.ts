b1a823dd3286e8bcee8094d29adc6b01f350db66
Ryan Driscoll <ryan.driscoll@code.org>
2025-10-24 15:01:59 -0600

    feat: add examplesLocation to ai tutor context for legacy labs

--- apps/src/aiTutor/views/legacyLabs/constants.ts
+++ apps/src/aiTutor/views/legacyLabs/constants.ts
@@ -1,1 +1,41 @@
-export const AI_TUTOR_LABS = ['applab', 'gamelab', 'weblab'];
+import {studio} from '@cdo/apps/lib/util/urlHelpers';
+
+export const AI_TUTOR_LEGACY_LABS = ['applab', 'gamelab', 'weblab'];
+
+export const PROJECT_EXAMPLES: Record<string, string[]> = {
+  applab: [
+    `[Choose Your Adventure](${studio(
+      '/projects/applab/nI9iumSHbTn5mHBXl-wxMw/remix'
+    )})`,
+    `[Landmark Flashcards](${studio(
+      '/projects/applab/sNZOTB_PiXAHt3Q5h_aRgQ/remix'
+    )})`,
+    `[Pensive Painter](${studio(
+      '/projects/applab/IAK_Qyc23QrkAB61KGvn1g/remix'
+    )})`,
+    `[Pet Poll](${studio('/projects/applab/TjHfwdUd3ENlyImb7VX5Cg/remix')})`,
+    `[Poke the Pig](${studio(
+      '/projects/applab/r469vc1FFSOxOlUruTFlUA/remix'
+    )})`,
+    `[Slider Sketch](${studio(
+      '/projects/applab/JNbplBXMisx2aMefUkh39g/remix'
+    )})`,
+  ],
+  gamelab: [
+    `[Bounce](${studio('/projects/gamelab/J2R-PHcvE4Wgb2xO2bjk0Q/remix')})`,
+    `[Make Your Own Character](${studio(
+      '/projects/gamelab/XVEFITms8-c7_F-ND-FKL8w3A-oEpFD_GKAJCB2VPCQ/remix'
+    )})`,
+    `[Sunset Shapes](${studio(
+      '/projects/gamelab/Z9QRSmJBfNY25T3ViLp0MTy4uAuXNNXpZrmhPsUdEro/remix'
+    )})`,
+  ],
+  weblab: [
+    `[Catering website](${studio(
+      '/projects/weblab/Z3m2fWClc10iTgxiLnOfTqZHApYrFXgx8YqgCwAvXvs/view'
+    )})`,
+    `[Photo Gallery](${studio(
+      '/projects/weblab/kkb1upGQja7Hy4NAa72uQxwu87jDa_DO1aiav2Qhzk8/view'
+    )})`,
+  ],
+};
--- apps/src/templates/instructions/InstructionsWithWorkspace.jsx
+++ apps/src/templates/instructions/InstructionsWithWorkspace.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {connect} from 'react-redux';
 
-import {AI_TUTOR_LABS} from '@cdo/apps/aiTutor/views/legacyLabs/constants';
+import {AI_TUTOR_LEGACY_LABS} from '@cdo/apps/aiTutor/views/legacyLabs/constants';
 
 import {AiTutorContainer} from '../../aiTutor/views/legacyLabs/AiTutorContainer';
 import {setInstructionsMaxHeightAvailable} from '../../redux/instructions';
@@ -114,7 +114,7 @@ export class UnwrappedInstructionsWithWorkspace extends React.Component {
     } = this.props;
 
     const showAiTutor =
-      AI_TUTOR_LABS.includes(labType) &&
+      AI_TUTOR_LEGACY_LABS.includes(labType) &&
       experiments.isEnabled(experiments.LEGACY_LAB_AI_TUTOR);
 
     const chatContainerSpace = 335; // 325px chat container + 10px margin = 335px
