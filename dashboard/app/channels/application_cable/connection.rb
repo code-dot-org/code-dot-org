3967f2f2c1d2304cce90ed661bc28054a36677c9
Liam Frye-Mason <liam.fryemason@code.org>
2025-10-24 10:29:40 -0700

    Add authentication to chatter

--- dashboard/app/channels/application_cable/connection.rb
+++ dashboard/app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private def find_verified_user
+      if verified_user = env['warden']&.verified_user
+        return verified_user
+      end
+
+      reject_unauthorized_connection
+    end
   end
 end
