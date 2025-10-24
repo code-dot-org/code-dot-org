0f3a5d00d44238d2d12b72e8cbc3f1097839e073
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-10-24 23:36:27 +0000

    Revert "feat(theme): add dsco & mui support" (#69081)

--- frontend/apps/studio/src/App.tsx
+++ frontend/apps/studio/src/App.tsx
@@ -1,16 +1,5 @@
-import '@code-dot-org/component-library-styles/colors.scss';
-import {LinkButton} from '@code-dot-org/component-library/button';
-import {Typography} from '@mui/material';
-
 function App() {
-  return (
-    <>
-      <Typography variant="body1" gutterBottom>
-        Anybody can learn!
-      </Typography>
-      <LinkButton href="https://code.org" text="Go to Code.org" />
-    </>
-  );
+  return <p>Anybody can learn!</p>;
 }
 
 export default App;
