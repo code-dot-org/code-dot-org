7630c565bb31ca02afe74068d4d8903a2958283d
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-10-24 17:40:18 -0700

    Revert "Revert "feat(theme): add dsco & mui support" (#69081)"
    This reverts commit 0f3a5d00d44238d2d12b72e8cbc3f1097839e073.

--- frontend/apps/studio/src/App.tsx
+++ frontend/apps/studio/src/App.tsx
@@ -1,5 +1,16 @@
+import '@code-dot-org/component-library-styles/colors.scss';
+import {LinkButton} from '@code-dot-org/component-library/button';
+import {Typography} from '@mui/material';
+
 function App() {
-  return <p>Anybody can learn!</p>;
+  return (
+    <>
+      <Typography variant="body1" gutterBottom>
+        Anybody can learn!
+      </Typography>
+      <LinkButton href="https://code.org" text="Go to Code.org" />
+    </>
+  );
 }
 
 export default App;
