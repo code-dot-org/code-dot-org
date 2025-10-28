cb837c707b1aee08be67cd4b3cac605962757190
Alice Fisher <alice.fisher@code.org>
2025-10-28 14:55:06 +0000

    update music start over dialog text

--- apps/src/lab2/views/dialogs/StartOverDialog.tsx
+++ apps/src/lab2/views/dialogs/StartOverDialog.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import aichatI18n from '@cdo/apps/aichat/locale';
+import musicI18n from '@cdo/apps/music/locale';
 import {commonI18n} from '@cdo/apps/types/locale';
 import {useAppSelector} from '@cdo/apps/util/reduxHooks';
 
@@ -12,6 +13,7 @@ import GenericDialog, {GenericDialogProps} from './GenericDialog';
 // Lab-specific messages for starting over.
 const LAB_SPECIFIC_MESSAGES: {[appName in AppName]?: string} = {
   aichat: aichatI18n.startOverAichatModelCustomizations(),
+  music: musicI18n.startOverMusicWorkspace(),
 };
 
 /**
@@ -30,6 +32,7 @@ const StartOverDialog: React.FunctionComponent<StartOverDialogProps> = ({
   const currentAppName = useAppSelector(
     state => state.lab.levelProperties?.appName
   );
+  console.log('currentAppName', currentAppName);
 
   const isTextWorkspace =
     currentAppName && TEXT_BASED_LABS.includes(currentAppName);
