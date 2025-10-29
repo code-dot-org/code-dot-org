f8e63f854d3f66569f962abf5eb31617588448bd
David Bailey <davidsbailey@users.noreply.github.com>
2025-10-29 10:37:45 -0700

    add comment about why install step is needed in unit pipeline

--- docker/ci/scripts/unit_tests.sh
+++ docker/ci/scripts/unit_tests.sh
@@ -6,7 +6,12 @@
 # `docker/unit-tests-compose.yml`. See instructions in that file.
 
 source docker/ci/scripts/prepare_ci_tests.sh
+
+# The install step is currently needed for unit (but not ui) because the database needed
+# by ui tests is prepopulated by the cache-staging-build step.
+# TODO: remove this line once cache-staging-build prepopulates dashboard unit test databases.
 source docker/ci/scripts/install_ci_tests.sh
+
 source docker/ci/scripts/build_ci_tests.sh
 
 
