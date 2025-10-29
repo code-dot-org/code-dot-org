0586363960a5db3232b12e96d17b8f1739e620ac
David Bailey <davidsbailey@users.noreply.github.com>
2025-10-28 21:00:58 -0700

    remove unnecessary diff output

--- lib/cdo/test_run_utils.rb
+++ lib/cdo/test_run_utils.rb
@@ -109,8 +109,6 @@ module TestRunUtils
     else
       seed_2_file = Tempfile.new(['db_seed', '.sql'])
       File.write(seed_2_file, cloned_data)
-      puts "Diff:\n"
-      puts `diff #{seed_file.path} #{seed_2_file.path}`
 
       # Clone single DB across all databases
       require 'parallel_tests'
