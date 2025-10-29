5f517a6136d866362e91fffb8a78b053d178ea82
David Bailey <davidsbailey@users.noreply.github.com>
2025-10-29 10:56:08 -0700

    simplify logic for enumerating test databases, and avoid recreating db 1

--- lib/cdo/test_run_utils.rb
+++ lib/cdo/test_run_utils.rb
@@ -114,7 +114,7 @@ module TestRunUtils
       require 'parallel_tests'
       procs = ParallelTests.determine_number_of_processes(nil)
       CDO.log.info "Test data modified, cloning across #{procs} databases..."
-      databases = Array.new(procs) {|i| "#{database}#{i + 1}"}
+      databases = (2..procs).map {|i| "#{database}#{i}"}
       databases.each do |db|
         recreate_db = "DROP DATABASE IF EXISTS #{db}; CREATE DATABASE IF NOT EXISTS #{db};"
         RakeUtils.system_stream_output "echo '#{recreate_db}' | mysql #{opts}"
