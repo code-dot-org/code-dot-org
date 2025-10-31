afe7ccd94577ef52336f9fcaa50eea2c5136ccf8
Mark Barnes <mark.barnes@code.org>
2025-10-31 11:23:57 -0500

    update perform and helper functions

--- dashboard/app/controllers/ai_lesson_summaries_controller.rb
+++ dashboard/app/controllers/ai_lesson_summaries_controller.rb
@@ -15,7 +15,36 @@ class AiLessonSummariesController < ApplicationController
     end
   end
 
+  def perform_ai_lesson_summaries_by_unit(unit_id)
+    unit = Unit.find(unit_id)
+    lesson_ids = []
+    unit.lessons.each do |lesson|
+      if lesson.has_lesson_plan
+        lesson_ids << lesson.id
+      end
+    end
+    request = {
+      execution_status: SharedConstants::AI_REQUEST_EXECUTION_STATUS[:NOT_STARTED],
+      user_id: current_user.id,
+      lesson_ids: lesson_ids,
+      unit_id: unit_id
+    }
+    AiLessonSummariesJob.perform_later(request)
+  end
+
+  def perform_ai_lesson_summary_by_lesson(lesson_id)
+    if lesson.has_lesson_plan
+      request = {
+        execution_status: SharedConstants::AI_REQUEST_EXECUTION_STATUS[:NOT_STARTED],
+        user_id: current_user.id,
+        lesson_id: [lesson.id],
+        unit_id: nil
+      }
+      AiLessonSummariesJob.perform_later(request)
+    end
+  end
+
   private def ai_lesson_summary_params
-    params.transform_keys(&:underscore).permit(:lesson_id, :user_id, :lesson_summary)
+    params.transform_keys(&:underscore).permit(:lesson_id, :unit_id, :user_id, :lesson_summary)
   end
 end
--- dashboard/app/jobs/ai_lesson_summaries_job.rb
+++ dashboard/app/jobs/ai_lesson_summaries_job.rb
@@ -11,12 +11,13 @@ class AiLessonSummariesJob < ApplicationJob
   before_perform do |job|
     request = job.arguments.first[:request]
     request.update!(execution_status: SharedConstants::AI_REQUEST_EXECUTION_STATUS[:RUNNING])
-    report_job_start(request)
+    @start_time = Time.now
   end
 
   after_perform do |job|
     request = job.arguments.first[:request]
-    report_job_finish(request)
+    request.update!(execution_status: SharedConstants::AI_REQUEST_EXECUTION_STATUS[:SUCCESS])
+    @execution_time = Time.now - @start_time
   end
 
   # Catch any exceptions that occur during the job and update the request status accordingly.
@@ -34,90 +35,13 @@ class AiLessonSummariesJob < ApplicationJob
       }
     )
 
-    # Report metrics for the failed job (after_perform doesn't run on failure).
-    report_job_finish(request)
-
     # Re-raise error to notify our system of the failed job.
     raise exception
   end
 
-  def perform(request:, locale:)
-    status, response = get_execution_status_and_response(request, locale)
-    request.update!(response: response, execution_status: status)
-  end
-
-  private def get_execution_status_and_response(request, locale)
-    begin
-      response = AiLessonSummariesHelper.get_ai_lesson_summary(request[:lesson_id])
-    rescue OpenaiUserInputResponseTimeout => exception
-      return [SharedConstants::AI_REQUEST_EXECUTION_STATUS[:MODEL_TIMEOUT], exception.message]
+  def perform(request:)
+    request[:lesson_ids].each do |lesson_id|
+      AiLessonSummariesHelper.retrieve_and_save_ai_lesson_summary(request[:user_id], lesson_id)
     end
-
-    [SharedConstants::AI_REQUEST_EXECUTION_STATUS[:SUCCESS], response]
-  end
-
-  private def get_model_id(request)
-    request.model_customizations['selectedModelId']
-  end
-
-  private def report_job_start(request)
-    @start_time = Time.now
-
-    Cdo::Metrics.push(SharedConstants::AICHAT_METRICS_NAMESPACE,
-      [
-        {
-          metric_name: "#{self.class.name}.Start",
-          value: 1,
-          unit: 'Count',
-          timestamp: Time.now,
-          dimensions: [
-            {name: 'Environment', value: CDO.rack_env},
-            {name: 'ModelId', value: get_model_id(request)},
-          ],
-        }
-      ]
-    )
-  end
-
-  private def report_job_finish(request)
-    execution_time = Time.now - @start_time
-    status_name = SharedConstants::AI_REQUEST_EXECUTION_STATUS.key(request.execution_status).to_s
-
-    execution_time_metric_base = {
-      metric_name: "#{self.class.name}.ExecutionTime",
-      value: execution_time,
-      unit: 'Seconds',
-      timestamp: Time.now,
-      dimensions: [],
-    }
-
-    execution_time_dimensions_base = [
-      {name: 'Environment', value: CDO.rack_env},
-      {name: 'ModelId', value: get_model_id(request)},
-    ]
-
-    metrics = [
-      {
-        metric_name: "#{self.class.name}.Finish",
-        value: 1,
-        unit: 'Count',
-        timestamp: Time.now,
-        dimensions: [
-          {name: 'Environment', value: CDO.rack_env},
-          {name: 'ModelId', value: get_model_id(request)},
-          {name: 'ExecutionStatus', value: status_name},
-        ],
-      },
-      execution_time_metric_base.merge({dimensions: execution_time_dimensions_base}),
-    ]
-
-    model_id = get_model_id(request)
-    if openai_or_gemini?(model_id)
-      multimodal_dimension = {name: 'Multimodal', value: request_multimodal?(request).to_s}
-      execution_time_metric_multimodal = execution_time_metric_base.merge({dimensions: execution_time_dimensions_base + [multimodal_dimension]})
-      metrics.push(execution_time_metric_multimodal)
-    end
-
-    Cdo::Metrics.push(SharedConstants::AICHAT_METRICS_NAMESPACE, metrics)
   end
 end
