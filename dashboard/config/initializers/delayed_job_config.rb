5c77a2d96e11661d0266982cab4d3be390c80210
Carl Reiner <carl.reiner@code.org>
2025-10-30 17:18:35 -0700

    switch delayed job to use Rails logger

--- dashboard/config/initializers/delayed_job_config.rb
+++ dashboard/config/initializers/delayed_job_config.rb
@@ -9,4 +9,4 @@ Delayed::Worker.destroy_failed_jobs = false
 # max_attempts variable.
 Delayed::Worker.max_attempts = 1
 
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+Delayed::Worker.logger = Rails.logger
