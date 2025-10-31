1a5d04e8bc4cbac9f8d8f14cb85a33741829d67d
Mario Gil Correa <mario.gil-correa@code.org>
2025-10-30 23:12:31 -0400

    added metrics and covered test cases

--- dashboard/lib/services/user/user_type_setter.rb
+++ dashboard/lib/services/user/user_type_setter.rb
@@ -11,14 +11,31 @@ module Services
       end
 
       def call
-        case user_type
-        when ::User::TYPE_TEACHER
-          Services::User::UpgradeToTeacher.call(user: user, email: email, email_preference: email_preference)
-        when ::User::TYPE_STUDENT
-          Services::User::DowngradeToStudent.call(user: user)
-        else
-          false # Unexpected user type
+        previous_user_type = user.user_type
+
+        result = case user_type
+                 when ::User::TYPE_TEACHER
+                   Services::User::UpgradeToTeacher.call(user: user, email: email, email_preference: email_preference)
+                 when ::User::TYPE_STUDENT
+                   Services::User::DowngradeToStudent.call(user: user)
+                 else # Unexpected user type
+                   false
+                 end
+
+        # upgrade_to_teacher and downgrade_to_student have inconsistent behavior so we need to
+        # check that the new user type is different than the previous, before logging an event.
+        if result && previous_user_type != user_type
+          Metrics::Events.log_event(
+            user: user,
+            event_name: 'user_type_changed',
+            metadata: {
+              from_user_type: previous_user_type,
+              to_user_type: user_type
+            }
+          )
         end
+
+        result
       end
     end
   end
