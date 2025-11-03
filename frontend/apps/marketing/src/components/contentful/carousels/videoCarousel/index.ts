af7333086200db90c6e76704ec1729a327ff6d25
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-11-03 13:45:19 -0800

    chore: remove marketing from main repo
    The marketing code has been moved to https://github.com/code-dot-org/marketing-sites

--- frontend/apps/marketing/src/components/contentful/carousels/videoCarousel/index.ts
+++
@@ -1,5 +0,0 @@
-import '@code-dot-org/component-library/carousel/index.css';
-
-// Export the Component Definition for use in Contentful Studio
-export {VideoCarouselContentfulComponentDefinition} from './videoCarouselContentfulDefinition';
-export {default} from './VideoCarousel';
