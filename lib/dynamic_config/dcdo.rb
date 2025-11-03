3a73d2b647ec146343fa33b9d701677f5254a8f9
Liam Frye-Mason <liam.fryemason@code.org>
2025-11-03 11:13:09 -0800

    Add DCDO flag to disable progress v1

--- lib/dynamic_config/dcdo.rb
+++ lib/dynamic_config/dcdo.rb
@@ -68,6 +68,7 @@ class DCDOBase < DynamicConfigBase
       'actioncable-repeat-interval': DCDO.get('actioncable-repeat-interval', 1000),
       'actioncable-repeat': DCDO.get('actioncable-repeat', true),
       'actioncable-disconnect-timeout': DCDO.get('actioncable-disconnect-timeout', 30 * 1000),
+      'disable-progress-v1': DCDO.get('disable-progress-v1', false)
     }
   end
 end
