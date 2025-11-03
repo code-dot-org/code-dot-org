63b8d791acf2d64fbf9c092a53be841677b173d8
breville <brendan@code.org>
2025-11-03 01:49:02 +0000

    Use instructinos sub-components to avoid slides etc.

--- apps/src/lab2/views/components/Instructions/InstructionsV2.tsx
+++ apps/src/lab2/views/components/Instructions/InstructionsV2.tsx
@@ -31,8 +31,6 @@ export interface InstructionsProps {
   handleInstructionsTextClick?: (id: string) => void;
   /** Optional classname for the container */
   className?: string;
-  /** Optional classname for the markdown instructions */
-  markdownClassName?: string;
   /** Optional component to render at the bottom of the main instructions. */
   bottomComponent?: React.ReactNode;
   /** Props for in-panel validation button and results table. */
@@ -71,7 +69,6 @@ const Instructions: React.FunctionComponent<InstructionsProps> = ({
   layout = 'vertical',
   handleInstructionsTextClick,
   className,
-  markdownClassName,
   bottomComponent,
   validationSettings,
   fixedDarkBackground,
@@ -123,7 +120,6 @@ const Instructions: React.FunctionComponent<InstructionsProps> = ({
           <div className={moduleStyles.scrollingContent}>
             <MainInstructionsContent
               instructionsText={longInstructions}
-              markdownClassName={markdownClassName}
               handleInstructionsTextClick={handleInstructionsTextClick}
             />
             <PredictQuestion className={moduleStyles.predictQuestion} />
@@ -160,7 +156,6 @@ const Instructions: React.FunctionComponent<InstructionsProps> = ({
             {...feedbackProps}
             overrideTheme={overrideTheme}
             levelProperties={levelProperties}
-            markdownClassName={markdownClassName}
             handleInstructionsTextClick={handleInstructionsTextClick}
             hideContinueIfDisabled={hideContinueIfDisabled}
             styleAsBubble
