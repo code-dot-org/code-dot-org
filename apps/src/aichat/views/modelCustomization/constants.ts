9111fd8c98ff88b768e532f83f313a725435d6fb
Turner Riley <turner.riley@code.org>
2025-11-04 11:00:12 -0500

    remove changes to string constants file

--- apps/src/aichat/views/modelCustomization/constants.ts
+++ apps/src/aichat/views/modelCustomization/constants.ts
@@ -1,16 +1,19 @@
 import {modelDescriptions} from '@cdo/apps/aichat/constants';
 
+import aichatI18n from '../../locale';
 import {
   AiCustomizations,
   LevelAichatSettings,
   ModelCardInfo,
   Visibility,
 } from '../../types';
+
 export const MIN_TEMPERATURE = 0.1;
 export const MAX_TEMPERATURE = 1;
 export const SET_TEMPERATURE_STEP = 0.1;
 export const MAX_RETRIEVAL_CONTEXTS = 20;
 export const MAX_ASK_ABOUT_TOPICS = 10;
+
 export const MODEL_CARD_FIELDS_LABELS_ICONS: {
   property: keyof ModelCardInfo;
   label: string;
@@ -20,54 +23,54 @@ export const MODEL_CARD_FIELDS_LABELS_ICONS: {
 }[] = [
   {
     property: 'botName',
-    label: 'Filler',
-    editTooltip: 'Filler',
+    label: aichatI18n.modelCard_botNameHeader(),
+    editTooltip: aichatI18n.modelCard_botNameEditTooltip(),
     displayTooltip: '',
   },
   {
     property: 'description',
-    label: 'Filler',
+    label: aichatI18n.modelCard_descriptionHeader(),
     icon: 'memo',
-    editTooltip: 'Filler',
-    displayTooltip: 'Filler',
+    editTooltip: aichatI18n.modelCard_descriptionEditTooltip(),
+    displayTooltip: aichatI18n.modelCard_descriptionDisplayTooltip(),
   },
   {
     property: 'intendedUse',
-    label: 'Filler',
+    label: aichatI18n.modelCard_intendedUseHeader(),
     icon: 'bullseye-pointer',
-    editTooltip: 'Filler',
-    displayTooltip: 'Filler',
+    editTooltip: aichatI18n.modelCard_intendedUseEditTooltip(),
+    displayTooltip: aichatI18n.modelCard_intendedUseDisplayTooltip(),
   },
   {
     property: 'limitationsAndWarnings',
-    label: 'Filler',
+    label: aichatI18n.modelCard_limitationsHeaader(),
     icon: 'diamond-exclamation',
-    editTooltip: 'Filler',
-    displayTooltip: 'Filler',
+    editTooltip: aichatI18n.modelCard_limitationsEditTooltip(),
+    displayTooltip: aichatI18n.modelCard_limitationsDisplayTooltip(),
   },
   {
     property: 'testingAndEvaluation',
-    label: 'Filler',
+    label: aichatI18n.modelCard_testingHeader(),
     icon: 'vial-circle-check',
-    editTooltip: 'Filler',
-    displayTooltip: 'Filler',
+    editTooltip: aichatI18n.modelCard_testingEditTooltip(),
+    displayTooltip: aichatI18n.modelCard_testingDisplayTooltip(),
   },
   {
     property: 'exampleTopics',
-    label: 'Filler',
+    label: aichatI18n.modelCard_exampleTopicsHeader(),
     icon: 'message-lines',
-    editTooltip: 'Filler',
-    displayTooltip: 'Filler',
+    editTooltip: aichatI18n.modelCard_exampleTopicsEditTooltip(),
+    displayTooltip: aichatI18n.modelCard_exampleTopicsDisplayTooltip(),
   },
 ];
 
 export const TECHNICAL_INFO_FIELDS = [
-  'Model Name',
-  'Overview',
-  'Training Data',
-  'System Prompt',
-  'Temperature',
-  'Retrieval Used',
+  aichatI18n.technicalInfoHeader_modelName(),
+  aichatI18n.technicalInfoHeader_overview(),
+  aichatI18n.technicalInfoHeader_trainingData(),
+  aichatI18n.technicalInfoHeader_systemPrompt(),
+  aichatI18n.technicalInfoHeader_temperature(),
+  aichatI18n.technicalInfoHeader_retrievalUsed(),
 ] as const;
 
 export const EMPTY_MODEL_CARD_INFO: ModelCardInfo = {
@@ -79,6 +82,7 @@ export const EMPTY_MODEL_CARD_INFO: ModelCardInfo = {
   exampleTopics: [],
   isPublished: false,
 };
+
 export const EMPTY_AI_CUSTOMIZATIONS: AiCustomizations = {
   selectedModelId: modelDescriptions[0].id,
   temperature: 0.5,
@@ -86,6 +90,7 @@ export const EMPTY_AI_CUSTOMIZATIONS: AiCustomizations = {
   retrievalContexts: [],
   modelCardInfo: EMPTY_MODEL_CARD_INFO,
 };
+
 export const DEFAULT_VISIBILITIES: {
   [key in keyof AiCustomizations]: Visibility;
 } = {
@@ -95,6 +100,7 @@ export const DEFAULT_VISIBILITIES: {
   retrievalContexts: Visibility.EDITABLE,
   modelCardInfo: Visibility.EDITABLE,
 };
+
 export const DEFAULT_LEVEL_AICHAT_SETTINGS: LevelAichatSettings = {
   initialCustomizations: EMPTY_AI_CUSTOMIZATIONS,
   visibilities: DEFAULT_VISIBILITIES,
@@ -102,14 +108,15 @@ export const DEFAULT_LEVEL_AICHAT_SETTINGS: LevelAichatSettings = {
   hidePresentationPanel: false,
   availableModelIds: [modelDescriptions[0].id],
 };
+
 export const AI_CUSTOMIZATIONS_LABELS: {
   [key in keyof AiCustomizations]: string;
 } = {
-  selectedModelId: 'Filler',
-  temperature: 'Filler',
-  systemPrompt: 'Filler',
-  retrievalContexts: 'Filler',
-  modelCardInfo: 'Filler',
+  selectedModelId: aichatI18n.aiCustomizations_selectedModel(),
+  temperature: aichatI18n.aiCustomizations_temperature(),
+  systemPrompt: aichatI18n.aiCustomizations_systemPrompt(),
+  retrievalContexts: aichatI18n.aiCustomizations_retrieval(),
+  modelCardInfo: aichatI18n.aiCustomizations_modelCardInfo(),
 };
 
 // Model customization fields that are checked for toxicity before updating.
