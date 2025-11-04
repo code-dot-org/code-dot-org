deadcf00394bcd4c6707629e8825b27f51ade1f1
Alice Fisher <alice.fisher@code.org>
2025-11-04 21:00:37 +0000

    Revert "update backpack analytics name to lab2"
    This reverts commit d85f72ead6464954f3660138cfa486e1f339ad69.

--- apps/src/codebridge/FileBrowser/prompts/openSaveToBackpackPrompt.tsx
+++ apps/src/codebridge/FileBrowser/prompts/openSaveToBackpackPrompt.tsx
@@ -83,12 +83,12 @@ export const openSaveToBackpackPrompt = async ({
       const selectedFileName =
         results.type === 'confirm' ? file.name : fileNameCopy;
 
-      let successMetric = EVENTS.LAB2_SAVE_TO_BACKPACK_NEW;
+      let successMetric = EVENTS.CODEBRIDGE_SAVE_TO_BACKPACK_NEW;
       if (isDuplicateFileName) {
         successMetric =
           selectedFileName === file.name
-            ? EVENTS.LAB2_SAVE_TO_BACKPACK_REPLACE
-            : EVENTS.LAB2_SAVE_TO_BACKPACK_RENAME;
+            ? EVENTS.CODEBRIDGE_SAVE_TO_BACKPACK_REPLACE
+            : EVENTS.CODEBRIDGE_SAVE_TO_BACKPACK_RENAME;
       }
       const successCallback = () => sendLab2AnalyticsEvent(successMetric);
 
