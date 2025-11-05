f1f350a318ea3310e13fb73cc93681c5262013bf
Ben Brooks <ben@code.org>
2025-11-05 14:03:14 -0800

    Update comment

--- dashboard/app/models/levels/level.rb
+++ dashboard/app/models/levels/level.rb
@@ -1050,7 +1050,7 @@ class Level < ApplicationRecord
   end
 
   def uses_theme_preference?
-    # Only python lab and web lab 2 set and use the theme preference in UserPreferences right now.
+    # These are the level types that set and use the theme preference in UserPreferences right now.
     is_a?(Pythonlab) || is_a?(Weblab2) || is_a?(Sketchlab)
   end
 
