e02e345e08f89dc515b3433684d033efcc28f97a
breville <brendan@code.org>
2025-11-05 07:27:05 +0000

    Code review feedback

--- apps/src/dance/lab2/views/GenerateDancer.tsx
+++ apps/src/dance/lab2/views/GenerateDancer.tsx
@@ -8,6 +8,7 @@ import {useParentLevelProperties} from '@cdo/apps/bubbleChoice/customModes/Music
 import {queryParams} from '@cdo/apps/code-studio/utils';
 import {DanceLevelProperties} from '@cdo/apps/dance/types';
 import useLifecycleNotifier from '@cdo/apps/lab2/hooks/useLifecycleNotifier';
+import Lab2Registry from '@cdo/apps/lab2/Lab2Registry';
 import continueOrFinishLesson from '@cdo/apps/lab2/progress/continueOrFinishLesson';
 import {LifecycleEvent} from '@cdo/apps/lab2/utils/LifecycleNotifier';
 import Adlib, {
@@ -28,85 +29,13 @@ import dancerEmptyHeadShoulders from '@cdo/static/dance/dancer-empty-head-should
 
 import {getConfigValue} from '../../lottie/LottieDancerUtils';
 
+import adlibsDefault from './dancerAdlibsDefault';
+
 import moduleStyles from './generate-dancer.module.scss';
 
 const BODY_VARIANT_COUNT = 5;
 
 const GENERATE_DELAY_DURATION = 5000;
-
-const adlibOptions = {
-  creature: [
-    {id: 'axolotl', text: 'axolotl'},
-    {id: 'cat', text: 'cat'},
-    {id: 'dog', text: 'dog'},
-    {id: 'flame', text: 'flame'},
-    {id: 'fox', text: 'fox'},
-    {id: 'frilled_lizard', text: 'frilled lizard'},
-    {id: 'frog', text: 'frog'},
-    {id: 'giraffe', text: 'giraffe'},
-    {id: 'koala', text: 'koala'},
-    {id: 'moose', text: 'moose'},
-    {id: 'rabbit', text: 'rabbit'},
-    {id: 'squirrel', text: 'squirrel'},
-    {id: 'tiger', text: 'tiger'},
-    {id: 'wolf', text: 'wolf'},
-  ],
-  attire: [
-    {id: 'beanie', text: 'beanie'},
-    {id: 'colorful_hair', text: 'colorful hair'},
-    {id: 'crown', text: 'crown'},
-    {id: 'headphones', text: 'headphones'},
-    {id: 'headscarf', text: 'headscarf'},
-    {id: 'sunglasses', text: 'sunglasses'},
-    {id: 'no_accessories', text: 'no accessories'},
-  ],
-  mood: [
-    {id: 'confused', text: 'confused'},
-    {id: 'fierce', text: 'fierce'},
-    {id: 'happy', text: 'happy'},
-    {id: 'silly', text: 'silly'},
-    {id: 'sleepy', text: 'sleepy'},
-    {id: 'surprised', text: 'surprised'},
-  ],
-  style: [
-    {id: 'classic', text: 'classic'},
-    {id: 'fantasy', text: 'fantasy'},
-    {id: 'kpop', text: 'K-pop'},
-    {id: 'preppy', text: 'preppy'},
-    {id: 'retro', text: 'retro'},
-    {id: 'rock', text: 'rock'},
-    {id: 'scifi', text: 'sci-fi'},
-    {id: 'sporty', text: 'sporty'},
-    {id: 'streetwear', text: 'streetwear'},
-  ],
-};
-
-const adlibsDefault: AdlibsType = {
-  'creature-05': {
-    template: 'Design a {creature}.',
-    options: {creature: adlibOptions.creature},
-    variantCount: 7,
-  },
-  'creature-attire-05': {
-    template: 'Design a {creature} wearing {attire}.',
-    options: {
-      creature: adlibOptions.creature,
-      attire: adlibOptions.attire,
-    },
-    variantCount: 7,
-  },
-  'creature-attire-mood-style-05': {
-    template:
-      'Design a {creature} wearing {attire}, in a {mood} mood, with a {style} style.',
-    options: {
-      creature: adlibOptions.creature,
-      attire: adlibOptions.attire,
-      mood: adlibOptions.mood,
-      style: adlibOptions.style,
-    },
-    variantCount: 7,
-  },
-};
 
 interface DancerGenerateProps {
   adlibOption: string;
@@ -169,6 +98,10 @@ const GenerateDancer: React.FunctionComponent<DancerGenerateProps> = ({
         adlibsValue = value;
       } catch (error) {
         console.log("Couldn't retrieve adlib manifest.");
+        Lab2Registry.getInstance().getMetricsReporter().logWarning({
+          message: 'Error loading adlib manifest',
+          manifestFilename,
+        });
       }
 
       setAiGenerateState('none');
