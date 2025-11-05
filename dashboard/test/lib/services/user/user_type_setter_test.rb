073c1bc4086ae130fba4497d9473db9f8ee9d454
Mario Gil Correa <mario.gil-correa@code.org>
2025-11-05 17:36:25 -0500

    moved metric from service object to controller action

--- dashboard/test/lib/services/user/user_type_setter_test.rb
+++ dashboard/test/lib/services/user/user_type_setter_test.rb
@@ -29,42 +29,6 @@ class Services::User::UserTypeSetterTest < ActiveSupport::TestCase
 
         _user_type_setter_call.must_equal true
       end
-
-      it 'logs user_type_changed event with correct metadata' do
-        allow(Services::User::UpgradeToTeacher).to receive(:call).and_return(user)
-
-        expect(Metrics::Events).to receive(:log_event).with(
-          user: user,
-          event_name: 'user_type_changed',
-          metadata: {
-            from_user_type: ::User::TYPE_STUDENT,
-            to_user_type: ::User::TYPE_TEACHER
-          }
-        )
-
-        user_type_setter_call
-      end
-
-      it 'does not log event when user is already a teacher' do
-        teacher_user = create(:teacher)
-
-        expect(Metrics::Events).not_to receive(:log_event)
-
-        Services::User::UserTypeSetter.call(
-          user: teacher_user,
-          user_type: ::User::TYPE_TEACHER,
-          email: email,
-          email_preference: email_preference
-        )
-      end
-
-      it 'does not log event when service returns false (failure)' do
-        allow(Services::User::UpgradeToTeacher).to receive(:call).and_return(false)
-
-        expect(Metrics::Events).not_to receive(:log_event)
-
-        user_type_setter_call
-      end
     end
 
     context 'when user_type is student' do
@@ -78,42 +42,6 @@ class Services::User::UserTypeSetterTest < ActiveSupport::TestCase
 
         _user_type_setter_call.must_equal true
       end
-
-      it 'logs user_type_changed event with correct metadata' do
-        allow(Services::User::DowngradeToStudent).to receive(:call).and_return(user)
-
-        expect(Metrics::Events).to receive(:log_event).with(
-          user: user,
-          event_name: 'user_type_changed',
-          metadata: {
-            from_user_type: ::User::TYPE_TEACHER,
-            to_user_type: ::User::TYPE_STUDENT
-          }
-        )
-
-        user_type_setter_call
-      end
-
-      it 'does not log event when user is already a student' do
-        student_user = create(:student)
-
-        expect(Metrics::Events).not_to receive(:log_event)
-
-        Services::User::UserTypeSetter.call(
-          user: student_user,
-          user_type: ::User::TYPE_STUDENT,
-          email: email,
-          email_preference: email_preference
-        )
-      end
-
-      it 'does not log event when service returns false (failure)' do
-        allow(Services::User::DowngradeToStudent).to receive(:call).and_return(false)
-
-        expect(Metrics::Events).not_to receive(:log_event)
-
-        user_type_setter_call
-      end
     end
 
     context 'when user_type is unknown' do
@@ -121,12 +49,6 @@ class Services::User::UserTypeSetterTest < ActiveSupport::TestCase
       let(:user_type) {'invalid-type'}
 
       it {_user_type_setter_call.must_equal false}
-
-      it 'does not log event for invalid user type' do
-        expect(Metrics::Events).not_to receive(:log_event)
-
-        user_type_setter_call
-      end
     end
   end
 end
