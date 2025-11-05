edc0599d5c9fbab69dae3823d3c6b1b292ce50ea
Molly Moen <molly@code.org>
2025-11-05 15:24:25 -0800

    remove window objects

--- apps/src/sites/studio/pages/codeprojects_preview/page_not_found.js
+++ apps/src/sites/studio/pages/codeprojects_preview/page_not_found.js
@@ -2,9 +2,6 @@ import PageNotFound from '@codebridge/FilePreview/PageNotFound';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-window.React = require('react');
-window.ReactDOM = require('react-dom');
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <PageNotFound />,
