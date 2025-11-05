7587f19c80bc26e4c2504c75949faf2a189c0bc6
Mike Harvey <mike.harvey@code.org>
2025-11-05 16:53:28 -0500

    update scroll height when we load blocks

--- apps/src/dance/lab2/views/DanceView.tsx
+++ apps/src/dance/lab2/views/DanceView.tsx
@@ -421,6 +421,10 @@ const DanceView: React.FunctionComponent<{
         workspace.current,
         JSON.stringify(currentSources.source)
       );
+      // Provide extra scroll height to account for bottom-anchored guide overlay.
+      if (guideMode === 'aiCodeGenerate') {
+        Blockly.extraScrollHeight = 200;
+      }
       const toolboxFromStorage = localStorage.getItem(
         `flyout-${levelProperties.id}`
       );
@@ -432,7 +436,12 @@ const DanceView: React.FunctionComponent<{
         } catch {}
       }
     }
-  }, [currentSources.source, levelProperties.id, updateBlocklyFlyout]);
+  }, [
+    currentSources.source,
+    guideMode,
+    levelProperties.id,
+    updateBlocklyFlyout,
+  ]);
 
   useEffect(() => {
     const songKeys = Object.keys(songData);
