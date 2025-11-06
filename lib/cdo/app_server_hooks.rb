0bfb684ebb0ded21a89500d143513893aff85704
Seth Nickell <snickell@gmail.com>
2025-11-05 16:59:43 -1000

    Log ActionCable.server.connections.count to CloudWatch in a background thread
    Co-authored-by: Liam Frye-Mason <liam.fryemason@code.org>

--- lib/cdo/app_server_hooks.rb
+++ lib/cdo/app_server_hooks.rb
@@ -34,6 +34,49 @@ module Cdo
       # intialized in config/initializers/statsig.rb
       require 'cdo/statsig'
       Cdo::StatsigInitializer.init
+
+      # FIXME: after_fork is called in puma.rb by on_worker_boot, which doesn't fire when we run Puma
+      # in single mode (=local dev, also obvi on thin, since, yeah puma.rb and its a puma API).
+      #
+      # We maybe need to do this in Puma's "new" on_booted hook
+      # see feature request in: https://github.com/puma/puma/issues/1230#issuecomment-839953916
+      # merge into puma by this PR in early 2023, probably too new of a puma version for us to take advantage: https://github.com/puma/puma/pull/2709
+      # we're on puma 5.6.5 released August 23, 2022, so we could upgrade puma? idk pretty scary for getting a hook
+      #
+      # The alternative approach is we could detect single-mode, and start the thread directly in that case in puma.rb, and then start it here
+      # for after_fork. That might be cleaner anyway, idk, on_booted would potentially start the metrics thread on the cluster master process too?
+      start_metric_thread(host: host)
+    end
+
+    def self.start_metric_thread(host:)
+      puts "Starting metrics thread"
+      dimensions = {
+        PID: Process.pid,
+        Host: host,
+      }
+      begin
+        dimensions[:InstanceId] = AWS::EC2.instance_id
+      rescue
+        # We're not on EC2, ignore
+      end
+
+      Thread.new do
+        loop do
+          puts "Getting metrics for PID #{Process.pid}"
+          # print a line to /tmp/boo.log:
+          # File.open("/tmp/boo.log", "a") do |f|
+          #   f.puts "ActionCable connections: #{ActionCable.server.connections.count}"
+          # end
+          Cdo::Metrics.put(
+            'ActionCable',
+            'ServerConnectionsCount',
+            ActionCable.server.connections.count,
+            dimensions,
+            unit: 'Count'
+          )
+          sleep 1
+        end
+      end
     end
   end
 end
