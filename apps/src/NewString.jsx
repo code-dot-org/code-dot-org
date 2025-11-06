f37a0f23ee06f24c20d6a4cf0f5a2c2088f2db41
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-06 21:25:40 +0000

    add react component which references the string

---
+++ apps/src/NewString.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import i18n from '@cdo/locale';
+
+export const NewString = () => {
+  return <div>{i18n.newString()}</div>;
+};
--- apps/test/unit/NewStringTest.js
+++ apps/test/unit/NewStringTest.js
@@ -1,7 +1,17 @@
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+
+import {NewString} from '@cdo/apps/NewString';
 import i18n from '@cdo/locale';
 
 describe('newString test', function () {
   it('returns "New String"', function () {
     expect(i18n.newString()).toBe('New String');
+  });
+
+  it('renders the component with "New String"', function () {
+    render(<NewString />);
+    expect(screen.getByText('New String')).toBeInTheDocument();
   });
 });
