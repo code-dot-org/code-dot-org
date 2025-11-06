04d5a4e2d2229cde26b722ebea66901307106385
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-06 12:52:03 -0800

    Revert "temporarily skip unrelated lib test failure"
    This reverts commit fe29696aad101212dc861753993887934fc77557.

--- lib/test/test_deliverer.rb
+++ lib/test/test_deliverer.rb
@@ -32,7 +32,6 @@ class DelivererTest < Minitest::Test
   end
 
   def test_deliverer_send
-    skip
     email = "test@deliverer.send"
     contact = Poste2.create_recipient(email, {ip_address: '5.6.7.8.'})
 
