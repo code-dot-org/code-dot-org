eedd638653e0d30e6007b966c03fc499362f3ad3
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-06 17:00:12 -0800

    limit http logs to proxy errors

--- lib/cdo/sauce_connect.rb
+++ lib/cdo/sauce_connect.rb
@@ -49,7 +49,7 @@ module Cdo
         # In CI, enable verbose logging to help diagnose connection issues
         if ENV['CI']
           cmd += ["--log-level", "debug"]
-          cmd += ["--log-http", "proxy:headers"]
+          cmd += ["--log-http", "proxy:errors"]
         end
 
         env = {
