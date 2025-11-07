25109d8e87eff4fa297a3d441154479109ea51a7
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-06 22:13:26 -0800

    reduce log level to INFO

--- lib/cdo/sauce_connect.rb
+++ lib/cdo/sauce_connect.rb
@@ -48,7 +48,7 @@ module Cdo
 
         # In CI, enable verbose logging to help diagnose connection issues
         if ENV['CI']
-          cmd += ["--log-level", "debug"]
+          cmd += ["--log-level", "info"]
           cmd += ["--log-http", "proxy:errors"]
         end
 
