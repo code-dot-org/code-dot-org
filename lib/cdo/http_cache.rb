048db22078c9342426093e8b47493e0526da16b4
Brendan Reville <breville@users.noreply.github.com>
2025-11-08 18:43:05 +1100

    Revert "Revert "hoai2025: cache unit""

--- lib/cdo/http_cache.rb
+++ lib/cdo/http_cache.rb
@@ -77,6 +77,7 @@ class HttpCache
     dance-ai-2023
     mc
     music-jam-2024
+    mix-move-ai-2025
   ).map do |script_name|
     # Assume all cached units are in single unit courses.
     [script_name, "/courses/#{script_name}/units/1/lessons/*"]
