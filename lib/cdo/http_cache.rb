068dc3012297d7fa32f80ed42058f8ed4f4df805
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-11-10 22:31:15 +0200

    chore(http-cache): remove Pegasus HoC API proxy to Dashboard

--- lib/cdo/http_cache.rb
+++ lib/cdo/http_cache.rb
@@ -361,16 +361,6 @@ class HttpCache
             },
           ],
         },
-        pegasus: {
-          behaviors: [
-            {
-              path: "#{HocLegacy::API_ROOT_PATH}/*",
-              proxy: 'dashboard',
-              headers: ALLOWLISTED_HEADERS,
-              cookies: allowlisted_cookies,
-            },
-          ],
-        }
       )
     end
 
