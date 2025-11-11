92c2a23c17acd28697224e400b581014d4d123ce
Sanchit Malhotra <sanchit@code.org>
2025-11-11 05:09:45 +0000

    hoai: generate analytics

--- apps/src/dance/lab2/views/GenerateDancer.tsx
+++ apps/src/dance/lab2/views/GenerateDancer.tsx
@@ -177,100 +177,110 @@ const GenerateDancer: React.FunctionComponent<DancerGenerateProps> = ({
     setHasGenerated(false);
   });
 
-  const generateDancerCache = useCallback(async () => {
-    const startTime = Date.now();
+  const generateDancerCache = useCallback(
+    async (regenerate = false) => {
+      const startTime = Date.now();
 
-    if (!adlibs || !adlibChoices) {
-      return;
-    }
-
-    // Special case: for the creature-attire-mood-style-05 adlib only,
-    // move mood from choices to choicesExtra, and use a unique path.
-    // This is because the style option is not used in retrieving the
-    // head image, and is instead used to retrieve the body.
-    let choicesToSave;
-    let choicesExtraToSave;
-    let pathToSave;
-    if (
-      [
-        'creature-attire-mood-style-04',
-        'creature-attire-mood-style-05',
-      ].includes(adlibOption)
-    ) {
-      pathToSave =
-        adlibOption === 'creature-attire-mood-style-04'
-          ? 'creature-attire-mood-04'
-          : 'creature-attire-mood-05';
-      choicesToSave = Object.keys(adlibChoices)
-        .slice(0, -1)
-        .map(key => adlibChoices[key]);
-      choicesExtraToSave = Object.keys(adlibChoices)
-        .slice(-1)
-        .map(key => adlibChoices[key]);
-    } else {
-      pathToSave = adlibOption;
-      choicesToSave = Object.keys(adlibChoices).map(key => adlibChoices[key]);
-      choicesExtraToSave = undefined;
-    }
-
-    // Build a list of available variants, excluding blocked ones.
-    const variants = [];
-    for (
-      let variant = 0;
-      variant <= adlibs[adlibOption].variantCount - 1;
-      variant++
-    ) {
-      // Generate a filename that will match an entry in the block list.
-      const assetFilename = `${choicesToSave?.join('-')}-${variant
-        .toString()
-        .padStart(2, '0')}`;
-
-      if (!blockList.current?.[adlibOption].includes(assetFilename)) {
-        variants.push(variant);
+      if (!adlibs || !adlibChoices) {
+        return;
       }
-    }
 
-    // Build a smaller set of available variants, by excluding recently-shown
-    // ones.
-    const newVariants = variants.filter(
-      variant => !variantHistory.current.includes(variant)
-    );
+      // Special case: for the creature-attire-mood-style-05 adlib only,
+      // move mood from choices to choicesExtra, and use a unique path.
+      // This is because the style option is not used in retrieving the
+      // head image, and is instead used to retrieve the body.
+      let choicesToSave;
+      let choicesExtraToSave;
+      let pathToSave;
+      if (
+        [
+          'creature-attire-mood-style-04',
+          'creature-attire-mood-style-05',
+        ].includes(adlibOption)
+      ) {
+        pathToSave =
+          adlibOption === 'creature-attire-mood-style-04'
+            ? 'creature-attire-mood-04'
+            : 'creature-attire-mood-05';
+        choicesToSave = Object.keys(adlibChoices)
+          .slice(0, -1)
+          .map(key => adlibChoices[key]);
+        choicesExtraToSave = Object.keys(adlibChoices)
+          .slice(-1)
+          .map(key => adlibChoices[key]);
+      } else {
+        pathToSave = adlibOption;
+        choicesToSave = Object.keys(adlibChoices).map(key => adlibChoices[key]);
+        choicesExtraToSave = undefined;
+      }
 
-    const variant = sample(newVariants) as number;
-    const bodyVariant = getRandomInt(0, BODY_VARIANT_COUNT - 1);
+      // Build a list of available variants, excluding blocked ones.
+      const variants = [];
+      for (
+        let variant = 0;
+        variant <= adlibs[adlibOption].variantCount - 1;
+        variant++
+      ) {
+        // Generate a filename that will match an entry in the block list.
+        const assetFilename = `${choicesToSave?.join('-')}-${variant
+          .toString()
+          .padStart(2, '0')}`;
 
-    // Keep the recently-shown array length at a maximum that ensures
-    // there are still two choices to be made each time.
-    const availableVariantCount = variants.length;
-    const lengthOfVariantsHistory = Math.max(availableVariantCount - 2, 2);
-    const newVariantsHistory: number[] = [...variantHistory.current, variant];
-    if (newVariantsHistory.length > lengthOfVariantsHistory) {
-      newVariantsHistory.shift(); // Remove the oldest entry.
-    }
-    variantHistory.current = newVariantsHistory;
+        if (!blockList.current?.[adlibOption].includes(assetFilename)) {
+          variants.push(variant);
+        }
+      }
 
-    const newDancerMetadata: GeneratedDancerMetadata = {
-      adlibOption,
-      path: (queryParams('ai-dancer-path') as string) || pathToSave,
-      choices: choicesToSave,
-      choicesExtra: choicesExtraToSave,
-      variant,
-      extraVariant: bodyVariant,
-    };
-    updateSources(
-      {...currentSources, generatedDancer: newDancerMetadata},
-      true
-    );
+      // Build a smaller set of available variants, by excluding recently-shown
+      // ones.
+      const newVariants = variants.filter(
+        variant => !variantHistory.current.includes(variant)
+      );
 
-    const elapsedTime = Date.now() - startTime;
-    const remainingDelayDuration = Math.max(
-      GENERATE_TOTAL_DELAY_DURATION -
-        GENERATE_INITIAL_DELAY_DURATION -
-        elapsedTime,
-      0
-    );
-    await new Promise(res => setTimeout(res, remainingDelayDuration));
-  }, [adlibChoices, adlibOption, adlibs, updateSources, currentSources]);
+      const variant = sample(newVariants) as number;
+      const bodyVariant = getRandomInt(0, BODY_VARIANT_COUNT - 1);
+
+      // Keep the recently-shown array length at a maximum that ensures
+      // there are still two choices to be made each time.
+      const availableVariantCount = variants.length;
+      const lengthOfVariantsHistory = Math.max(availableVariantCount - 2, 2);
+      const newVariantsHistory: number[] = [...variantHistory.current, variant];
+      if (newVariantsHistory.length > lengthOfVariantsHistory) {
+        newVariantsHistory.shift(); // Remove the oldest entry.
+      }
+      variantHistory.current = newVariantsHistory;
+
+      const newDancerMetadata: GeneratedDancerMetadata = {
+        adlibOption,
+        path: (queryParams('ai-dancer-path') as string) || pathToSave,
+        choices: choicesToSave,
+        choicesExtra: choicesExtraToSave,
+        variant,
+        extraVariant: bodyVariant,
+      };
+      analyticsReporter.sendEvent(
+        EVENTS[`${regenerate ? 'REGENERATE' : 'GENERATE'}_DANCER_CLICKED`],
+        {
+          ...newDancerMetadata,
+          levelPath: window.location.pathname,
+        }
+      );
+      updateSources(
+        {...currentSources, generatedDancer: newDancerMetadata},
+        true
+      );
+
+      const elapsedTime = Date.now() - startTime;
+      const remainingDelayDuration = Math.max(
+        GENERATE_TOTAL_DELAY_DURATION -
+          GENERATE_INITIAL_DELAY_DURATION -
+          elapsedTime,
+        0
+      );
+      await new Promise(res => setTimeout(res, remainingDelayDuration));
+    },
+    [adlibChoices, adlibOption, adlibs, updateSources, currentSources]
+  );
 
   // Update local storage whenever the generated dancer metadata changes and update the canvas key so the canvas refreshes.
   const [canvasKey, setCanvasKey] = useState<string>();
@@ -301,16 +311,21 @@ const GenerateDancer: React.FunctionComponent<DancerGenerateProps> = ({
     });
   }, [levelProperties, signedIn, scriptName]);
 
-  const generateDancer = useCallback(async () => {
-    if (!hasGenerated) {
-      logLevelActivity();
-    }
-    setAiGenerateState('generating');
-    await new Promise(res => setTimeout(res, GENERATE_INITIAL_DELAY_DURATION));
-    await generateDancerCache();
-    setAiGenerateState('reviewing');
-    setHasGenerated(true);
-  }, [generateDancerCache, hasGenerated, logLevelActivity]);
+  const generateDancer = useCallback(
+    async (regenerate = false) => {
+      if (!hasGenerated) {
+        logLevelActivity();
+      }
+      setAiGenerateState('generating');
+      await new Promise(res =>
+        setTimeout(res, GENERATE_INITIAL_DELAY_DURATION)
+      );
+      await generateDancerCache(regenerate);
+      setAiGenerateState('reviewing');
+      setHasGenerated(true);
+    },
+    [generateDancerCache, hasGenerated, logLevelActivity]
+  );
 
   const glowSpeed = aiGenerateState === 'generating' ? 'fast' : 'normal';
 
@@ -429,7 +444,7 @@ const GenerateDancer: React.FunctionComponent<DancerGenerateProps> = ({
                     iconLeft={{iconName: 'sparkles'}}
                     isPending={aiGenerateState === 'generating'}
                     disabled={aiGenerateState === 'generating'}
-                    onClick={generateDancer}
+                    onClick={() => generateDancer()}
                     className={moduleStyles.buttonWide}
                   />
                 </div>
@@ -452,7 +467,15 @@ const GenerateDancer: React.FunctionComponent<DancerGenerateProps> = ({
                   type="secondary"
                   color="black"
                   size="s"
-                  onClick={() => setAiGenerateState('none')}
+                  onClick={() => {
+                    analyticsReporter.sendEvent(
+                      EVENTS.GENERATE_DANCER_BACK_TO_PROMPT_CLICKED,
+                      {
+                        levelPath: window.location.pathname,
+                      }
+                    );
+                    setAiGenerateState('none');
+                  }}
                   className={moduleStyles.buttonWide}
                 />
 
@@ -463,7 +486,7 @@ const GenerateDancer: React.FunctionComponent<DancerGenerateProps> = ({
                   color="black"
                   size="s"
                   iconLeft={{iconName: 'sparkles'}}
-                  onClick={generateDancer}
+                  onClick={() => generateDancer(true)}
                   className={moduleStyles.buttonWide}
                 />
 
