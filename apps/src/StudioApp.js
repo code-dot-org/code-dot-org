0f8649dd1be1259e6ae317a2de8d2b4579030e70
wilkie <david.wilkinson@code.org>
2025-11-10 19:39:39 -0500

    Update StudioApp.js

--- apps/src/StudioApp.js
+++ apps/src/StudioApp.js
@@ -532,9 +532,6 @@ StudioApp.prototype.init = function (config) {
     this.resizeVisualization(e.detail);
   });
 
-  // Ensure we also resize any of the application panels when the locale changes
-  localization.on('change', this.onResize.bind(this));
-
   this.reset(true);
 
   // Add display of blocks used.
