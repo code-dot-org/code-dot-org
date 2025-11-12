2b21a69cb639383a6b7429f3c218bc6a4602814b
Juan Manzo <juan.manzo@code.org>
2025-11-12 14:50:27 -0800

    Refactor Inactive Teacher Deletion Warning Emailer

--- dashboard/test/lib/services/user/inactive_teacher_deletion_warning_emailer_test.rb
+++ dashboard/test/lib/services/user/inactive_teacher_deletion_warning_emailer_test.rb
@@ -2,68 +2,71 @@
 
 require 'test_helper'
 
-class User::InactiveTeacherDeletionWarningJobTest < ActiveJob::TestCase
-  describe '.perform_later' do
-    subject(:perform_later) {described_class.perform_later}
+class User::InactiveTeacherDeletionWarningEmailerTest < ActiveJob::TestCase
+  subject(:described_class) {InactiveTeacherDeletionWarningEmailer}
+  subject(:described_instance) {described_class.new(dry_run: dry_run, limit: limit)}
+  let(:dry_run) {false}
+  let(:limit) {nil}
 
-    let(:teacher_email) {Faker::Internet.unique.email}
-    let(:teacher_name) {Faker::Name.unique.name}
-    let!(:teacher) {create(:teacher, email: teacher_email, name: teacher_name, current_sign_in_at: 48.months.ago, user_data_retention_status: create(:user_data_retention_status))}
+  let(:teacher_email) {Faker::Internet.unique.email}
+  let(:teacher_name) {Faker::Name.unique.name}
+  let!(:teacher) {create(:teacher, email: teacher_email, name: teacher_name, current_sign_in_at: 48.months.ago, user_data_retention_status: create(:user_data_retention_status))}
 
-    let(:expect_teacher_warning_to_be_sent) do
-      MailJet.expects(:send_email).with(
-        :inactive_teacher_deletion_warning,
-        teacher_email,
-        teacher_name,
-        vars: {first_name: teacher.given_name || teacher.name},
-      )
-    end
+  let(:expect_teacher_warning_to_be_sent) do
+    MailJet.expects(:send_email).with(
+      :inactive_teacher_deletion_warning,
+      teacher_email,
+      teacher_name,
+      vars: {first_name: teacher.given_name || teacher.name},
+    )
+  end
 
-    let(:expect_event_logging) do
-      Metrics::Events.expects(:log_event).with(
-        event_name: 'inactive_teacher_deletion_warning_sent',
-        metadata: {
-          teacher_id: teacher.id,
-        },
-      )
-    end
+  let(:expect_event_logging) do
+    Metrics::Events.expects(:log_event).with(
+      event_name: 'inactive_teacher_deletion_warning_sent',
+      metadata: {
+        teacher_id: teacher.id,
+      },
+    )
+  end
 
-    around do |test|
-      Timecop.freeze {test.call}
-    end
+  around do |test|
+    Timecop.freeze {test.call}
+  end
 
-    it 'enqueues job to "default" queue' do
-      assert_enqueued_with(job: described_class, queue: 'default') do
-        perform_later
-      end
-    end
-
+  describe '#call' do
+    subject(:send_warning_emails) {described_instance.call}
     it 'sends an email using MailJet with expected arguments' do
       expect_teacher_warning_to_be_sent.once
-      perform_enqueued_jobs {perform_later}
+      send_warning_emails
     end
 
     it 'sets deletion_warning_email_sent_at' do
-      perform_enqueued_jobs {perform_later}
+      send_warning_emails
       teacher.reload
       _(teacher.user_data_retention_status.deletion_warning_email_sent_at).wont_be_nil
+    end
+
+    it 'increments num_teachers_warned' do
+      send_warning_emails
+      _(described_instance.send(:num_teachers_warned)).must_equal 1
     end
 
     it 'resends a deletion warning email if inactivity and last warning both exceed 3 years' do
       teacher.user_data_retention_status.update!(deletion_warning_email_sent_at: 42.months.ago)
       expect_teacher_warning_to_be_sent.once
-      perform_enqueued_jobs {perform_later}
+      send_warning_emails
     end
 
     it 'does not send a deletion warning email if last warning does not exceed 41 months' do
       teacher.user_data_retention_status.update!(deletion_warning_email_sent_at: 40.months.ago)
       expect_teacher_warning_to_be_sent.never
-      perform_enqueued_jobs {perform_later}
+      send_warning_emails
     end
 
     it 'logs event' do
       expect_event_logging.once
-      perform_enqueued_jobs {perform_later}
+      send_warning_emails
     end
 
     context 'when the first attempt raises TooManyRequests' do
@@ -71,7 +74,7 @@ class User::InactiveTeacherDeletionWarningJobTest < ActiveJob::TestCase
 
       it 'it will try to send the email again' do
         expect_teacher_warning_to_be_sent.twice.raises(exception).then.returns(nil)
-        perform_enqueued_jobs {perform_later}
+        send_warning_emails
       end
     end
 
@@ -84,7 +87,7 @@ class User::InactiveTeacherDeletionWarningJobTest < ActiveJob::TestCase
 
       it 'rescues from exception with #report_exception' do
         described_class.any_instance.expects(:report_exception).with(exception).once
-        perform_enqueued_jobs {perform_later}
+        send_warning_emails
       end
     end
 
@@ -94,7 +97,15 @@ class User::InactiveTeacherDeletionWarningJobTest < ActiveJob::TestCase
 
       it 'does not warn teacher' do
         expect_teacher_warning_to_be_sent.never
-        perform_enqueued_jobs {perform_later}
+        send_warning_emails
+      end
+    end
+
+    context 'when dry run' do
+      let(:dry_run) {true}
+      it 'does not send emails in dry run mode' do
+        expect_teacher_warning_to_be_sent.never
+        send_warning_emails
       end
     end
   end
