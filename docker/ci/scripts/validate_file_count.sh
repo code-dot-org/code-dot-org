a06d098577547d035f806440b61f85068dc9bbc9
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-12 06:13:16 +0000

    move file count validation back to drone yml

--- docker/ci/scripts/validate_file_count.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Validates cached file count matches actual file count
-
-if [ -f .cached-staging-build-file-count ]; then
-  expected=$(cat .cached-staging-build-file-count)
-  actual=$(find . -type f -not -path './.git/*' | wc -l)
-  diff=$((actual - expected))
-  [ "$expected" = "$actual" ] || printf 'WARN: File count mismatch. Expected: %s Actual: %s Diff: %s\n' "$expected" "$actual" "$diff"
-fi
