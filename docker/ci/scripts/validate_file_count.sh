d932cfb57373577981bed8dfeb39910b1bb496e0
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-12 03:59:16 +0000

    move git integrity check back into drone yml

---
+++ docker/ci/scripts/validate_file_count.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Validates cached file count matches actual file count
+
+if [ -f .cache-staging-build-file-count ]; then
+  expected=$(cat .cache-staging-build-file-count)
+  actual=$(find . -type f | wc -l)
+  diff=$((actual - expected))
+  [ "$expected" = "$actual" ] || printf 'WARN: File count mismatch. Expected: %s Actual: %s Diff: %s\n' "$expected" "$actual" "$diff"
+fi
