53eedf326454321a35cc95e757ecfdccb87df06c
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-13 23:42:20 +0000

    print NODE_OPTIONS when PROFILE_APPS_BUILD is set

--- apps/webpack.config.js
+++ apps/webpack.config.js
@@ -19,6 +19,14 @@ const {StatsWriterPlugin} = require('webpack-stats-plugin');
 
 const circularDependencies = require('./circular_dependencies.json');
 const envConstants = require('./envConstants');
+
+if (envConstants.PROFILE_APPS_BUILD) {
+  console.log(
+    'Webpack configured with NODE_OPTIONS:',
+    envConstants.NODE_OPTIONS
+  );
+}
+
 const {
   ALL_APPS,
   appsEntriesFor,
