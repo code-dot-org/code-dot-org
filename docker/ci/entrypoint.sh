1e3fa9bed754698938cebda3ac666552f9f44e9e
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-17 14:45:01 -0800

    remove cd and chown commands from entrypoint.sh

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -5,15 +5,6 @@ set -x
 # Run https://github.com/boxboat/fixuid allow writes to bind-mounted code-dot-org directory
 eval $( fixuid )
 
-cd $HOME/code-dot-org
-
-# Need to change ownership of volume mounts which are not bind-mounted to the uid/gid after fixuid is applied
-ls -l /home/ci/.rbenv/versions/3.1.7/bin/bundler
-sudo chown -R $USER:$GROUP $HOME/.rbenv
-ls -l /home/ci/.rbenv/versions/3.1.7/bin/bundler
-sudo chown -R $USER:$GROUP $HOME/.config
-sudo chown -R $USER:$GROUP $HOME/.cache
-
 eval "$(rbenv init -)"
 
 # start mysql
