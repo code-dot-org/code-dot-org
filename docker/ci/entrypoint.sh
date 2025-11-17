c62f3e8889d42f0df3663ff634cbb26d187d3486
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-17 14:17:09 -0800

    split up chown commands

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -8,10 +8,9 @@ eval $( fixuid )
 cd $HOME/code-dot-org
 
 # Need to change ownership of volume mounts which are not bind-mounted to the uid/gid after fixuid is applied
-sudo chown -R $USER:$GROUP \
-        $HOME/.rbenv \
-        $HOME/.config \
-        $HOME/.cache
+sudo chown -R $USER:$GROUP $HOME/.rbenv
+sudo chown -R $USER:$GROUP $HOME/.config
+sudo chown -R $USER:$GROUP $HOME/.cache
 
 eval "$(rbenv init -)"
 
