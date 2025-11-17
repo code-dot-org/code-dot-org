8351ae35c2e739349bad9f52f793db9165ca39e5
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-17 14:31:34 -0800

    debug whether chowning rbenv is necessary

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -8,7 +8,9 @@ eval $( fixuid )
 cd $HOME/code-dot-org
 
 # Need to change ownership of volume mounts which are not bind-mounted to the uid/gid after fixuid is applied
+ls -l /home/ci/.rbenv/versions/3.1.7/bin/bundler
 sudo chown -R $USER:$GROUP $HOME/.rbenv
+ls -l /home/ci/.rbenv/versions/3.1.7/bin/bundler
 sudo chown -R $USER:$GROUP $HOME/.config
 sudo chown -R $USER:$GROUP $HOME/.cache
 
