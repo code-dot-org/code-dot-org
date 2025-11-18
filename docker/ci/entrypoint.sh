688280157a9ee4c754049298dda342fadd60beb9
David Bailey <davidsbailey@users.noreply.github.com>
2025-11-18 10:05:23 -0800

    only run entrypoint cd and chown steps outside of ci

--- docker/ci/entrypoint.sh
+++ docker/ci/entrypoint.sh
@@ -5,6 +5,17 @@ set -x
 # Run https://github.com/boxboat/fixuid allow writes to bind-mounted code-dot-org directory
 eval $( fixuid )
 
+# Only run local development setup when FIXUID is set (see ui-tests-compose.yml).
+# In CI, Drone handles the working directory and file ownership, and skipping
+# these steps saves valuable time.
+if [ -n "$FIXUID" ]; then
+    cd $HOME/code-dot-org
+    sudo chown -R $USER:$GROUP \
+            $HOME/.rbenv \
+            $HOME/.config \
+            $HOME/.cache
+fi
+
 eval "$(rbenv init -)"
 
 # start mysql
