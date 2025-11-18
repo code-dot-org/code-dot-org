49ea5a72fa42bba97b2746eb567208106d52ab16
kobryan0619 <kaitie.obryan@gmail.com>
2025-11-17 21:40:49 -0600

    remove console logs

--- apps/src/aiDifferentiation/personalization/teachingProfileApi.js
+++ apps/src/aiDifferentiation/personalization/teachingProfileApi.js
@@ -18,15 +18,11 @@ export const saveTeachingProfileData = async personalizationData => {
         dataToSave.dateYearsTeachingSet.toISOString();
     }
 
-    console.log('Saving teaching profile data:', dataToSave);
-
     const requestBody = JSON.stringify({
       teaching_profile_data: {
         individual_data: dataToSave,
       },
     });
-
-    console.log('Request body:', requestBody);
 
     const csrfToken = document
       .querySelector('meta[name="csrf-token"]')
