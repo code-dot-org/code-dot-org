c2056653109f2b0d21f07d3466cb13dc7df35e07
Liam Frye-Mason <liam.fryemason@code.org>
2025-11-19 15:11:40 -0800

    Remove header changes

--- apps/src/templates/studentSnapshot/StudentSnapshot.tsx
+++ apps/src/templates/studentSnapshot/StudentSnapshot.tsx
@@ -1,15 +1,94 @@
+import {Button} from '@code-dot-org/component-library/button';
+import {SimpleDropdown} from '@code-dot-org/component-library/dropdown';
 import {Typography} from '@mui/material';
-import React from 'react';
+import React, {useState} from 'react';
 
-import Header from './header';
 import WidgetTemplate from './widgetTemplate';
 
 import styles from './studentSnapshot.module.scss';
 
 const StudentSnapshot: React.FC = () => {
+  const [selectedOption, setSelectedOption] = useState<string>('');
+  const dropdownOptions = [
+    {value: 'option1', text: 'Option 1'},
+    {value: 'option2', text: 'Option 2'},
+    {value: 'option3', text: 'Option 3'},
+  ];
   return (
     <div className={styles.snapshotContainer}>
-      <Header />
+      <div className={styles.header}>
+        <div className={styles.headerColumn}>
+          <SimpleDropdown
+            labelText="Unit"
+            name="test"
+            items={dropdownOptions}
+            selectedValue={selectedOption}
+            onChange={event => setSelectedOption(event.target.value)}
+            placeholder="Select an option"
+            className={styles.dropdown}
+          />
+          <SimpleDropdown
+            labelText="Lesson"
+            name="test"
+            items={dropdownOptions}
+            selectedValue={selectedOption}
+            onChange={event => setSelectedOption(event.target.value)}
+            placeholder="Select an option"
+            className={styles.dropdown}
+          />
+          <div className={styles.buttonGroup}>
+            <Button
+              className={styles.button}
+              text="< Previous lesson"
+              onClick={() => alert('Button clicked!')}
+              color="gray"
+              type="secondary"
+            />
+            <Button
+              className={styles.button}
+              text="Next lesson >"
+              onClick={() => alert('Button clicked!')}
+              color="gray"
+              type="secondary"
+            />
+          </div>
+        </div>
+
+        <div className={styles.headerColumn}>
+          <SimpleDropdown
+            labelText="Show students by"
+            name="test"
+            items={dropdownOptions}
+            selectedValue={selectedOption}
+            onChange={event => setSelectedOption(event.target.value)}
+            placeholder="Select an option"
+            className={styles.dropdown}
+          />
+          <SimpleDropdown
+            labelText="Student"
+            name="test"
+            items={dropdownOptions}
+            selectedValue={selectedOption}
+            onChange={event => setSelectedOption(event.target.value)}
+            placeholder="Select an option"
+            className={styles.dropdown}
+          />
+          <div className={styles.buttonGroup}>
+            <Button
+              className={styles.button}
+              text="< Previous student"
+              onClick={() => alert('Button clicked!')}
+              color="gray"
+              type="secondary"
+            />
+            <Button
+              className={styles.button}
+              text="Next student >"
+              onClick={() => alert('Button clicked!')}
+            />
+          </div>
+        </div>
+      </div>
 
       <Typography
         variant="h4"
