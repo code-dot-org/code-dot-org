1074a4c3e32744f6e085869284ae530ceb0a29b4
Artem Vavilov <artem.vavilov.7@gmail.com>
2025-11-19 04:02:33 +0200

    CMS-553: Enable `/certificates/:session_id` in Marketing router (#69648)

--- aws/cloudformation/lambdas/marketing-router/index.js
+++ aws/cloudformation/lambdas/marketing-router/index.js
@@ -795,6 +795,7 @@ const pathPatterns = [
   /^\/_next\/static\//, // Next.js static assets
   /^\/_next\/image/,  // Next.js dynamic images, /_next/image*
   /^\/api\/hour\//,     // /api/hour/*
+  /^\/certificates\//,  // /certificates/*
   /^\/congrats\//,      // /congrats/*
   /^\/forms\//,         // /forms/*
   /^\/schools\//,       // /schools/*
