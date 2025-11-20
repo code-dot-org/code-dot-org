ac3cd1b524a36c2a3b74b2e2d25180db6d4a2a52
Ryan Driscoll <ryan.driscoll@code.org>
2025-11-20 12:13:59 -0700

    fix: remove short circuit level check for ai_tutor_available_for_pilot? method
    lab2 levels do not re-fetch appOptions on level changes so ai tutor being available on the level would be stale

--- dashboard/app/helpers/levels_helper.rb
+++ dashboard/app/helpers/levels_helper.rb
@@ -550,9 +550,7 @@ module LevelsHelper
 
   # Teachers: enabled if they are in the pilot single user experiment
   # Students: enabled if any of their teachers are in the pilot
-  def ai_tutor_enabled_for_pilot?(user = current_user, level = @level)
-    return false unless user && level&.ai_tutor_available?
-
+  def ai_tutor_enabled_for_pilot?(user = current_user)
     if user.teacher?
       Experiment.enabled?(user: user, experiment_name: AI_TUTOR_PILOT_NAME)
     else
