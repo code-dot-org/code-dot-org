f40507a3935e1f392f9131e5e98cbfc7e92296ff
Mark Barnes <mark.barnes@code.org>
2025-11-21 13:06:57 -0600

    add experiment and dcdo flag to endpoint

--- dashboard/app/controllers/ai_lesson_summary_podcasts_controller.rb
+++ dashboard/app/controllers/ai_lesson_summary_podcasts_controller.rb
@@ -2,15 +2,19 @@ class AiLessonSummaryPodcastsController < ApplicationController
   before_action :authenticate_user!
 
   def generate_podcast
-    # Placeholder to be replaced with generated script
-    script = "[energetic] You're listening to AI Teaching Assistant's Daily Byte, your quick check-in before class."
+    if SingleUserExperiment.enabled?(user: self, experiment_name: 'ai_lesson_summaries') || DCDO.get('show-aita-lesson-summaries', false)
+      # Placeholder to be replaced with generated script
+      script = "[energetic] You're listening to AI Teaching Assistant's Daily Byte, your quick check-in before class."
 
-    podcast = AiLessonSummariesPodcastHelper.get_podcast_from_script(script)
+      podcast = AiLessonSummariesPodcastHelper.get_podcast_from_script(script)
 
-    send_data podcast, :type => 'audio/mpeg', :dispensation => 'attachment', :filename => 'podcast.mp3'
+      send_data podcast, :type => 'audio/mpeg', :dispensation => 'attachment', :filename => 'podcast.mp3'
+    else
+      head :forbidden
+    end
   end
 
-  private def ai_lesson_summary_params
+  private def podcast_params
     params.transform_keys(&:underscore).permit(:lesson_id, :unit_id, :lesson_summary)
   end
 end
