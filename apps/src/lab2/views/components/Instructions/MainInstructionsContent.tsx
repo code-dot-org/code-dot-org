aaa2eda88338c3c2520ff12ad48a8c84fe4dcc44
wilkie <david.wilkinson@code.org>
2025-11-21 03:00:49 -0500

    Adds further documentation for the MainInstructionsContent component.

--- apps/src/lab2/views/components/Instructions/MainInstructionsContent.tsx
+++ apps/src/lab2/views/components/Instructions/MainInstructionsContent.tsx
@@ -18,8 +18,21 @@ interface MainInstructionsContentProps {
  * This is extracted out to a component so it can be used both in Instructions
  * and in MainInstructionsBubblePreview.
  *
+ * It assumes the `instructionsText` field is markdown.
+ *
  * You can also get a reference to the rendered div, which can be useful when
- * passed to a TextToSpeech component to render the contents as audio.
+ * passed to a TextToSpeech component to render the contents as audio. Just
+ * pass in a `ref` to capture the element.
+ *
+ * If you pass showTts as `true`, then it will render its own TextToSpeech play
+ * button at the top-right of the container. It will ensure that the first item
+ * in the markdown content is rendered with enough of a margin to accommodate
+ * the button. It is therefore best if that is a header of some kind.
+ *
+ * If you want to use your own TextToSpeech button component, just pass the
+ * `ref` from this component along to the TextToSpeech's `contentRef`
+ * property and the TextToSpeech will read the text content of this component
+ * aloud when played.
  */
 const MainInstructionsContent = forwardRef<
   HTMLElement,
