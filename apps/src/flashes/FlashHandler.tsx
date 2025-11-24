1e49cbb69ac81c1252fc0e7112e2e00fb80aa3ea
Ryan Driscoll <ryan.driscoll@code.org>
2025-11-24 10:39:35 -0700

    feat: add FlashHandler component that renders a mui Snackbar wrapping a DSCO Alert to show alert and notice rails flashes

---
+++ apps/src/flashes/FlashHandler.tsx
@@ -0,0 +1,54 @@
+import Alert, {AlertProps} from '@code-dot-org/component-library/alert';
+import {Snackbar} from '@mui/material';
+import React, {FC, useState} from 'react';
+
+type FlashType = 'notice' | 'alert';
+
+type FlashMessage = string | string[];
+
+type Flash = [FlashType, FlashMessage][];
+
+export const FlashHandler: FC<{flash?: Flash; autoHideDuration?: number}> = ({
+  flash,
+  autoHideDuration,
+}) => {
+  const [showFlash, setShowFlash] = useState(!!flash?.length);
+
+  const getAlertType = (flashType: FlashType): AlertProps['type'] => {
+    switch (flashType) {
+      case 'alert':
+        return 'danger';
+      case 'notice':
+        return 'success';
+      default:
+        return 'primary';
+    }
+  };
+
+  const handleClose = () => {
+    setShowFlash(false);
+  };
+
+  if (!flash?.length) {
+    return null;
+  }
+
+  // rails flash could contain multiple flash types and multiple messages per type
+  // presently only supporting the first type and message
+  const [type, message] = flash[0];
+  const text = Array.isArray(message) ? message[0] : message;
+
+  return (
+    <Snackbar
+      anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+      open={showFlash}
+      autoHideDuration={autoHideDuration}
+      onClose={(_, reason) => {
+        if (reason === 'clickaway') return;
+        handleClose();
+      }}
+    >
+      <Alert type={getAlertType(type)} text={text} onClose={handleClose} />
+    </Snackbar>
+  );
+};
