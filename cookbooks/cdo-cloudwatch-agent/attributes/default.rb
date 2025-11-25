f61c47901e1483c3bd520180ba2b6bf3d8d0da38
alex-m-brown <alex.brown@code.org>
2025-11-25 16:18:47 -0500

    move puma logs to cloudwatch

--- cookbooks/cdo-cloudwatch-agent/attributes/default.rb
+++ cookbooks/cdo-cloudwatch-agent/attributes/default.rb
@@ -1,6 +1,6 @@
 default['cdo-cloudwatch-agent'] = {
   log_files: {
     amazon_cloudwatch_agent: '/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log',
-    syslog: '/var/log/syslog'
+    syslog: %W[/var/log/syslog #{node[:home]}/#{node.chef_environment}/dashboard/logs/puma_stdout.log #{node[:home]}/#{node.chef_environment}/dashboard/logs/puma_stderr.log]
   }
 }
