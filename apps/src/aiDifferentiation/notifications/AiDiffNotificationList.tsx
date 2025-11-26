9c5579c5cb76eb317efdab5a759be02857435da8
Liam Frye-Mason <liam.fryemason@code.org>
2025-11-25 16:34:13 -0800

    Wire up mark_as_read

--- apps/src/aiDifferentiation/notifications/AiDiffNotificationList.tsx
+++ apps/src/aiDifferentiation/notifications/AiDiffNotificationList.tsx
@@ -41,8 +41,17 @@ const AiDiffNotificationList: React.FC<AiDiffNotificationListProps> = ({
   React.useEffect(() => {
     const unreadNotifications = notifications.filter(n => n.readAt === null);
     if (unreadNotifications.length > 0) {
-      const unreadIds = unreadNotifications.map(n => n.externalId);
-      const payload = {external_notification_ids: unreadIds};
+      const unreadExternalIds = unreadNotifications
+        .filter(n => !!n.externalId)
+        .map(n => n.externalId);
+
+      const unreadTeacherNotificationIds = unreadNotifications
+        .filter(n => !n.externalId)
+        .map(n => n.id);
+      const payload = {
+        external_notification_ids: unreadExternalIds,
+        teacher_notification_ids: unreadTeacherNotificationIds,
+      };
 
       // We don't mark the notifications locally as read so that we still get the `unread`
       // UI state until the user refreshes.
