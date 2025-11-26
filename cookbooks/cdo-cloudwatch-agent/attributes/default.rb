cc052a9ed4e42c398c93ab156e91602beb6ded62
alex-m-brown <alex.brown@code.org>
2025-11-26 10:08:59 -0500

    fix typo

--- cookbooks/cdo-cloudwatch-agent/attributes/default.rb
+++ cookbooks/cdo-cloudwatch-agent/attributes/default.rb
@@ -1,6 +1,6 @@
 default['cdo-cloudwatch-agent'] = {
   log_files: {
     amazon_cloudwatch_agent: '/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log',
-    syslog: %W[/var/log/syslog #{node[:home]}/#{node.chef_environment}/dashboard/logs/puma_stdout.log #{node[:home]}/#{node.chef_environment}/dashboard/logs/puma_stderr.log]
+    syslog: %W[/var/log/syslog #{node[:home]}/#{node.chef_environment}/dashboard/log/puma_stdout.log #{node[:home]}/#{node.chef_environment}/dashboard/log/puma_stderr.log]
   }
 }
