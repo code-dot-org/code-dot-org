88f4c7a932d407cf83ab187c19696ef329a03592
Elijah Hamovitz <elijahhamovitz@gmail.com>
2025-11-25 16:28:31 -0800

    put initializer operations which reference autoloaded constants into to_prepare blocks

--- dashboard/config/initializers/session_store.rb
+++ dashboard/config/initializers/session_store.rb
@@ -2,17 +2,20 @@ require 'connection_pool'
 require 'cdo/cookie_helpers'
 
 session_cookie_key = environment_specific_cookie_name('_learn_session')
-Dashboard::Application.config.session_store Middlewares::RedisSessionStore,
-  key: session_cookie_key,
-  servers: ["#{CDO.redis_url}/session"],
-  secure: !CDO.no_https_store && (!Rails.env.development? || CDO.https_development),
-  domain: :all,
 
-  # Users who interact with the site at least once a month will remain logged in.
-  expire_after: CDO.dashboard_session_ttl_days.days,
+Rails.application.config.to_prepare do
+  Dashboard::Application.config.session_store Middlewares::RedisSessionStore,
+    key: session_cookie_key,
+    servers: ["#{CDO.redis_url}/session"],
+    secure: !CDO.no_https_store && (!Rails.env.development? || CDO.https_development),
+    domain: :all,
 
-  # Enable pooling by specifying a pool size; we arbitrarily use the value this
-  # would default to anyway, for simplicity. In practice, we run with enough
-  # parallel worker processes in production that we only use an average of
-  # ~1.34 connections per pool, so this could be reduced if desired.
-  pool_size: 5
+    # Users who interact with the site at least once a month will remain logged in.
+    expire_after: CDO.dashboard_session_ttl_days.days,
+
+    # Enable pooling by specifying a pool size; we arbitrarily use the value this
+    # would default to anyway, for simplicity. In practice, we run with enough
+    # parallel worker processes in production that we only use an average of
+    # ~1.34 connections per pool, so this could be reduced if desired.
+    pool_size: 5
+end
