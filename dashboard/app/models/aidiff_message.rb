dab026f1fab216c9cf71fea122b2d4b18445243b
Ceara Chewning <ceara.chewning@code.org>
2025-12-01 14:16:37 -0500

    Update aidiff message model with potential artifact

--- dashboard/app/models/aidiff_message.rb
+++ dashboard/app/models/aidiff_message.rb
@@ -2,17 +2,19 @@
 #
 # Table name: aidiff_messages
 #
-#  id               :bigint           not null, primary key
-#  aidiff_thread_id :bigint           not null
-#  external_id      :text(65535)      not null
-#  role             :integer          not null
-#  content          :text(65535)      not null
-#  is_preset        :boolean          not null
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  preset_chip_text :text(65535)
-#  raw_content      :text(65535)
-#  source_links     :json
+#  id                      :bigint           not null, primary key
+#  aidiff_thread_id        :bigint           not null
+#  external_id             :text(65535)      not null
+#  role                    :integer          not null
+#  content                 :text(65535)      not null
+#  is_preset               :boolean          not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  preset_chip_text        :text(65535)
+#  raw_content             :text(65535)
+#  source_links            :json
+#  is_artifact_candidate   :boolean          default(FALSE)
+#  artifact_candidate_type :string(255)
 #
 # Indexes
 #
