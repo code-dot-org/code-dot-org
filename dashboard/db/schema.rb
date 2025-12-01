7820ce47ad3a98026d8af8657ab04238c4980559
Ceara Chewning <ceara.chewning@code.org>
2025-12-01 13:55:17 -0500

    Schema update

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_11_12_160346) do
+ActiveRecord::Schema.define(version: 2025_11_13_202435) do
 
   create_table "activities", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
@@ -180,6 +180,8 @@ ActiveRecord::Schema.define(version: 2025_11_12_160346) do
     t.text "preset_chip_text"
     t.text "raw_content"
     t.json "source_links"
+    t.boolean "is_artifact_candidate", default: false
+    t.string "artifact_candidate_type"
     t.index ["aidiff_thread_id"], name: "index_aidiff_messages_on_aidiff_thread_id"
   end
 
