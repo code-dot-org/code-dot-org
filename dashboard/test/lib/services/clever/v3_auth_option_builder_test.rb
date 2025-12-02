b35e6cf995ebc02f2f0c1b84368505940a41ce80
Dayne <dayne@code.org>
2025-12-02 01:35:59 +0000

    Service for migrating Clever v2 -> v3.1 auth

--- dashboard/test/lib/services/clever/v3_auth_option_builder_test.rb
+++ dashboard/test/lib/services/clever/v3_auth_option_builder_test.rb
@@ -58,6 +58,7 @@ class Services::Clever::V3AuthOptionBuilderTest < ActiveSupport::TestCase
         _(result).must_be_nil
       end
     end
+<<<<<<< HEAD
 
     context 'when v3 auth option already exists' do
       let(:user) {create(:student, :with_clever_authentication_option)}
@@ -78,5 +79,7 @@ class Services::Clever::V3AuthOptionBuilderTest < ActiveSupport::TestCase
         _(result).must_be_nil
       end
     end
+=======
+>>>>>>> f857e6ed65a (Service for migrating Clever v2 -> v3.1 auth)
   end
 end
