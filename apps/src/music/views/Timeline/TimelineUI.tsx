cf95c77e1e7c31c683a17e43e2eb60bcb13b3088
breville <brendan@code.org>
2025-12-03 05:53:23 +0000

    Update value

--- apps/src/music/views/Timeline/TimelineUI.tsx
+++ apps/src/music/views/Timeline/TimelineUI.tsx
@@ -243,8 +243,15 @@ const Timeline: React.FunctionComponent<TimelineProps> = props => {
       return;
     }
     const resizeObserver = new ResizeObserver(() => {
+      // Don't use the entire height of the bar line in event height calculations.
+      // Currently, assuming it's 4 pixels shorter is sufficient in subsequent
+      // calculations to avoid events from being cut off.
+      const barlineHeightCompensation = 4;
+
       const firstBarLineHeight = firstBarLineRef.current?.offsetHeight;
-      setAvailableHeight(firstBarLineHeight ? firstBarLineHeight - 2 : 0);
+      setAvailableHeight(
+        firstBarLineHeight ? firstBarLineHeight - barlineHeightCompensation : 0
+      );
     });
     resizeObserver.observe(firstBarLineRef?.current);
     return () => {
