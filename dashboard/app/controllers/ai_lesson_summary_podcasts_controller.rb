5ac6a2c42b62a194feea2c22030acc476cadaef0
Mark Barnes <mark.barnes@code.org>
2025-12-04 12:32:12 -0600

    complete podcast generation

--- dashboard/app/controllers/ai_lesson_summary_podcasts_controller.rb
+++ dashboard/app/controllers/ai_lesson_summary_podcasts_controller.rb
@@ -4,11 +4,11 @@ class AiLessonSummaryPodcastsController < ApplicationController
   def generate_podcast
     if current_user && (SingleUserExperiment.enabled?(user: current_user, experiment_name: 'ai_lesson_summaries') || DCDO.get('show-aita-lesson-summaries', false))
       # Placeholder to be replaced with generated script
-      script = AiLessonSummariesHelper.get_ai_lesson_summary(params[:lesson_id], current_user.id, AiSystemPrompts::LessonSummariesSystemPromptHelper::RESPONSE_FORMATS[:PODCAST_SCRIPT])[:json]
-      puts script[:podcast_script]
-      #podcast = AiLessonSummariesPodcastHelper.get_podcast_from_script(script['podcast_script'])
+      script = AiLessonSummariesHelper.generate_lesson_summary(params[:lesson_id], current_user.id, AiSystemPrompts::LessonSummariesSystemPromptHelper::RESPONSE_FORMATS[:PODCAST_SCRIPT])[:json]
+      script = JSON.parse(script)['podcast_script']
+      podcast = AiLessonSummariesPodcastHelper.get_podcast_from_script(script)
 
-      #send_data podcast, :type => 'audio/mpeg', :dispensation => 'attachment', :filename => 'podcast.mp3'
+      send_data podcast, :type => 'audio/mpeg', :dispensation => 'attachment', :filename => 'podcast.mp3'
     else
       head :forbidden
     end
