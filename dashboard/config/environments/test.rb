1f515b9048e7fa8c175e93fd58292294db072ec5
David Bailey <davidsbailey@users.noreply.github.com>
2025-12-03 16:53:35 -0800

    use full rails cache size in ci ui tests

--- dashboard/config/environments/test.rb
+++ dashboard/config/environments/test.rb
@@ -30,8 +30,8 @@ Dashboard::Application.configure do
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
-  # Disable Rails.cache when running unit tests.
-  config.cache_store = :memory_store, {size: 64.megabytes} if ci_test
+  # Use smaller cache size when running unit tests.
+  config.cache_store = :memory_store, {size: 64.megabytes} if unit_test
 
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :smtp
