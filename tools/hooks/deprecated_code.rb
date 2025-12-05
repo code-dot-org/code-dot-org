70131fd62986cb33e38ae1ae3ec1ab948e4b8c1b
alex-m-brown <alex.brown@code.org>
2025-12-05 11:32:56 -0500

    add comments to hook

--- tools/hooks/deprecated_code.rb
+++ tools/hooks/deprecated_code.rb
@@ -3,6 +3,7 @@ REPO_DIR = File.expand_path('../../../', __FILE__)
 require_relative "#{REPO_DIR}/lib/cdo/cdo_cli"
 include CdoCli
 
+# List of deprecated code patterns to detect
 DEPRECATED = [
   'FirehoseClient',
 ]
@@ -10,11 +11,14 @@ DEPRECATED = [
 def detect_deprecated_code
   Dir.chdir REPO_DIR do
     diff_output = `git diff --cached -- . ':(exclude)tools/hooks/deprecated_code.rb' --unified=0`
+
+    # Only consider added lines (those starting with '+', excluding the diff metadata lines)
     @added_lines = diff_output.each_line.select {|line| line.start_with?('+') && !line.start_with?('+++')}.map {|line| line[1..]}.join
   end
 
   DEPRECATED.each do |deprecated_code|
     @added_lines.each_line do |line|
+      # Detect usage of deprecated code, ignoring comments
       if line.match?(/^[^#\/]*\b#{Regexp.escape(deprecated_code)}\b/i)
         puts red <<-EOS
             Looks like you are using deprecated code: #{deprecated_code}
