7a8f7b9dbef335ca6cc4a5938ca7011471c46224
alex-m-brown <alex.brown@code.org>
2025-12-05 11:01:32 -0500

    apply recommendations

--- tools/hooks/deprecated_methods.rb
+++ tools/hooks/deprecated_methods.rb
@@ -9,16 +9,19 @@ DEPRECATED = [
 
 def detect_deprecated_methods
   Dir.chdir REPO_DIR do
-    @added_lines = `git diff --cached -- . ':(exclude)tools/hooks/deprecated_methods.rb' --unified=0 | grep '^\+'`
+    diff_output = `git diff --cached -- . ':(exclude)tools/hooks/deprecated_methods.rb' --unified=0`
+    @added_lines = diff_output.each_line.select {|line| line.start_with?('+') && !line.start_with?('+++')}.map {|line| line[1..]}.join
   end
 
   DEPRECATED.each do |deprecated_method|
-    if @added_lines.match?(/^[^\n#]*#{Regexp.escape(deprecated_method)}/i)
-      puts red <<-EOS
-          Looks like you are using a deprecated method: #{deprecated_method}
-          Please avoid using this method as it is planned to be removed in the near future.
-      EOS
-      raise "Commit blocked."
+    @added_lines.each_line do |line|
+      if line.match?(/^[^\n#\/]*#{Regexp.escape(deprecated_method)}/i)
+        puts red <<-EOS
+            Looks like you are using a deprecated method: #{deprecated_method}
+            Please avoid using this method as it is planned to be removed in the near future.
+        EOS
+        raise "Commit blocked due to deprecated method usage: #{deprecated_method}."
+      end
     end
   end
 end
