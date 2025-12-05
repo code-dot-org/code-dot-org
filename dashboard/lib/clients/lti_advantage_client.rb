fd3989650066d7f2d29ee6f867535716f1cddc6e
Nick Lathe <nick.lathe@code.org>
2025-12-04 16:01:50 -0800

    Fixes issue with concatenated nrps url param
      - De-duplicates possible dupplicated params as part of the NRPS
        response from certain LMS platforms
      - Adds Schoology specific query param logic due to a bug in thier
        API
    
    Signed-off-by: Nick Lathe <nick.lathe@code.org>

--- dashboard/lib/clients/lti_advantage_client.rb
+++ dashboard/lib/clients/lti_advantage_client.rb
@@ -1,35 +1,40 @@
 class Clients::LtiAdvantageClient
   include LtiAccessToken
 
+  PAGE_LIMIT = 100
+
   def initialize(client_id, issuer)
     raise ArgumentError unless client_id && issuer
     @client_id = client_id
     @issuer = issuer
+    @page_limit = PAGE_LIMIT
   end
 
   # Call the LTI Advantage API to get the membership (roster) for a given context (course/section).
   # The URL for the API will vary between LMS platforms, and is initially contained in the launch context.
   # See the LTI spec for details:
   # https://www.imsglobal.org/spec/lti-nrps/v2p0/
-  def get_context_membership(url, resource_link_id, page_limit = 100)
+  def get_context_membership(url, resource_link_id)
     options = {
       headers: {
         'Accept' => Policies::Lti::MEMBERSHIP_CONTAINER_CONTENT_TYPE,
         'Authorization' => "Bearer #{get_access_token(@client_id, @issuer)}",
       },
-      query: {
-        limit: page_limit,
-      },
     }
-    options[:query][:rlid] = resource_link_id if Policies::Lti.issuer_accepts_resource_link?(@issuer)
-    res = make_request(url, options)
+    initial_url = build_uri(url, resource_link_id)
+    res = make_request(initial_url, options)
     next_page = next_page_url(res[:headers])
     parsed_res = res[:body]
+    # For Schoology, we need to pass the next page number due to a bug in their API implementation.
+    # Since we've alredy fetched the first page, we start at page 2.
+    page = 2
     while next_page
+      next_page = @issuer == Policies::Lti::LMS_PLATFORMS[:schoology][:issuer] ?  build_uri_schoology(next_page, resource_link_id, page) : build_uri(next_page, resource_link_id)
       current_page = make_request(next_page, options)
       parsed_res[:members].concat(current_page[:body][:members])
       return parsed_res unless parsed_res[:members].length <= Policies::Lti::MAX_COURSE_MEMBERSHIP
       next_page = next_page_url(current_page[:headers])
+      page += 1
     end
     parsed_res
   end
@@ -39,6 +44,26 @@ def make_request(url, options)
   res = HTTParty.get(url, options)
   raise "Error getting context membership: #{res.code} #{res.body}" unless res.code == HTTP::Status::OK
   return {headers: res.headers, body: JSON.parse(res.body, symbolize_names: true)}
+end
+
+private def build_uri(url, resource_link_id)
+  uri = URI(url)
+  existing_params = uri.query ? Rack::Utils.parse_query(uri.query) : {}
+  # Rack::Utils.parse_query returns string keys, so we need to use string keys when merging
+  rlid_param = Policies::Lti.issuer_accepts_resource_link?(@issuer) ? {'rlid' => resource_link_id} : {}
+  page_limit_param = {'limit' => @page_limit}
+  query_params = existing_params.merge(page_limit_param, rlid_param)
+  uri.query = query_params.to_query
+  uri.to_s
+end
+
+private def build_uri_schoology(url, resource_link_id, page)
+  uri = URI(url)
+  uri.query = {
+    limit: @page_limit,
+    page: page,
+  }.to_query
+  uri.to_s
 end
 
 # Get the next page URL from the Link header in the response.
