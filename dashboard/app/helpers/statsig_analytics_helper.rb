93d4b0a9e10f398b36d429517c34c4937c848ec4
breville <brendan@code.org>
2025-12-08 19:40:19 +0000

    Update URLs

--- dashboard/app/helpers/statsig_analytics_helper.rb
+++ dashboard/app/helpers/statsig_analytics_helper.rb
@@ -1,8 +1,9 @@
 module StatsigAnalyticsHelper
   def self.load_web_analytics?(request)
     paths_of_interest = [
-      '/courses/mix-move-ai-2025/units/1/lessons/1/levels/',
-      '/courses/music-jam-2024/units/1/lessons/1/levels/'
+      '/courses/express-2025/units/1/lessons/1/levels/2',
+      '/courses/csd-2025/units/3/lessons/2/levels/1',
+      '/courses/csp-2025/units/6/lessons/2/levels/1'
     ]
 
     return paths_of_interest.any? {|path_of_interest| request.path.include?(path_of_interest)}
