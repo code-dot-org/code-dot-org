b7d9b3acbde60b40a23b1718577214aba16ef9e8
Mario Gil Correa <mario.gil-correa@code.org>
2025-12-10 21:02:06 -0500

    specs tests

--- dashboard/test/controllers/student_snapshots_controller_test.rb
+++ dashboard/test/controllers/student_snapshots_controller_test.rb
@@ -1,97 +1,118 @@
 require 'test_helper'
 
 class StudentSnapshotsControllerTest < ActionController::TestCase
-  setup do
-    sign_in(create(:teacher))
-    @unit = create(:unit, name: 'test-unit')
-    @lesson_group = create(:lesson_group, script: @unit)
-    @lesson1 = create(:lesson,
-      script: @unit,
-      lesson_group: @lesson_group,
-      name: 'Test Lesson 1',
-      has_lesson_plan: true,
-      lockable: false,
-      relative_position: 1
-    )
-    @lesson2 = create(:lesson,
-      script: @unit,
-      lesson_group: @lesson_group,
-      name: 'Test Lesson 2',
-      has_lesson_plan: false,
-      lockable: true,
-      relative_position: 2
+  include MiniTest::RSpecMocks
+
+  let(:teacher) {create(:teacher)}
+  let!(:unit) {create(:unit, name: 'test-unit')}
+  let!(:lesson_group) {create(:lesson_group, script: unit)}
+  let!(:lesson1) do
+    create(:lesson,
+           script: unit,
+           lesson_group: lesson_group,
+           name: 'Test Lesson 1',
+           has_lesson_plan: true,
+           lockable: false,
+           relative_position: 1
     )
   end
-
-  test "lessons endpoint returns correct format with unit_id" do
-    get :lessons, params: {unit_id: @unit.id}
-
-    assert_response :ok
-
-    response_data = JSON.parse(response.body)
-    lessons_data = response_data['lessons']
-    assert_equal 2, lessons_data.length
-
-    lesson_data = lessons_data.first
-    assert_equal @lesson1.id, lesson_data['id'].to_i
-    assert_equal 'Test Lesson 1', lesson_data['name']
-    assert_equal true, lesson_data['hasLessonPlan']
-    assert_equal false, lesson_data['isLockable']
-    assert_equal 1, lesson_data['position']
-    assert_nil lesson_data['levelData']
+  let!(:lesson2) do
+    create(:lesson,
+           script: unit,
+           lesson_group: lesson_group,
+           name: 'Test Lesson 2',
+           has_lesson_plan: false,
+           lockable: true,
+           relative_position: 2
+    )
   end
 
-  test "lessons endpoint returns error for invalid unit_id" do
-    get :lessons, params: {unit_id: 99999}
-
-    assert_response :bad_request
-
-    response_data = JSON.parse(response.body)
-    assert_includes response_data['error'], "Can't find Unit id=99999"
+  before do
+    sign_in(teacher)
   end
 
-  test "lessons endpoint returns empty array for unit with no lessons" do
-    empty_unit = create(:unit, name: 'empty-unit')
-    get :lessons, params: {unit_id: empty_unit.id}
+  describe 'GET #lessons' do
+    subject(:get_lessons) {get :lessons, params: {unit_id: unit_id}}
 
-    assert_response :ok
+    let(:unit_id) {unit.id}
+    let(:response_data) {JSON.parse(response.body)}
+    let(:lessons_data) {response_data['lessons']}
 
-    response_data = JSON.parse(response.body)
-    assert_equal [], response_data['lessons']
-    assert_equal false, response_data['hasUnnumberedLessons']
-  end
+    it 'returns correct format with unit_id' do
+      get_lessons
 
-  test "lessons endpoint includes levelData when lesson has Pythonlab level" do
-    pythonlab_level = create(:level, type: 'Pythonlab', name: 'Test Pythonlab')
-    create(:script_level, script: @unit, lesson: @lesson1, levels: [pythonlab_level])
+      _(response).must_be :ok?
+      _(lessons_data.length).must_equal 2
 
-    get :lessons, params: {unit_id: @unit.id}
+      lesson_data = lessons_data.first
+      _(lesson_data['id'].to_i).must_equal lesson1.id
+      _(lesson_data['name']).must_equal 'Test Lesson 1'
+      _(lesson_data['hasLessonPlan']).must_equal true
+      _(lesson_data['isLockable']).must_equal false
+      _(lesson_data['position']).must_equal 1
+      _(lesson_data['levelData']).must_be_nil
+    end
 
-    assert_response :ok
+    context 'with invalid unit_id' do
+      let(:unit_id) {99999}
 
-    response_data = JSON.parse(response.body)
-    lesson_data = response_data['lessons'].first
+      it 'returns error' do
+        get_lessons
 
-    refute_nil lesson_data['levelData']
-    assert_equal pythonlab_level.id, lesson_data['levelData']['id']
-    assert_equal 'Test Pythonlab', lesson_data['levelData']['name']
-    assert_equal pythonlab_level.exemplar_sources, lesson_data['levelData']['exemplarSources']
-  end
+        _(response).must_be :bad_request?
+        _(response_data['error']).must_include "Can't find Unit id=99999"
+      end
+    end
 
-  test "lessons endpoint uses last Pythonlab level when multiple exist" do
-    pythonlab_level1 = create(:level, type: 'Pythonlab', name: 'First Pythonlab')
-    pythonlab_level2 = create(:level, type: 'Pythonlab', name: 'Last Pythonlab')
-    create(:script_level, script: @unit, lesson: @lesson1, levels: [pythonlab_level1])
-    create(:script_level, script: @unit, lesson: @lesson1, levels: [pythonlab_level2])
+    context 'with unit with no lessons' do
+      let(:unit_id) {create(:unit, name: 'empty-unit').id}
 
-    get :lessons, params: {unit_id: @unit.id}
+      it 'returns empty array' do
+        get_lessons
 
-    assert_response :ok
+        _(response).must_be :ok?
+        _(response_data['lessons']).must_equal []
+        _(response_data['hasUnnumberedLessons']).must_equal false
+      end
+    end
 
-    response_data = JSON.parse(response.body)
-    lesson_data = response_data['lessons'].first
+    context 'when lesson has Pythonlab level' do
+      let!(:pythonlab_level) {create(:pythonlab, name: 'Test Pythonlab')}
 
-    assert_equal pythonlab_level2.id, lesson_data['levelData']['id']
-    assert_equal 'Last Pythonlab', lesson_data['levelData']['name']
+      before do
+        create(:script_level, script: unit, lesson: lesson1, levels: [pythonlab_level])
+      end
+
+      it 'includes levelData' do
+        get_lessons
+
+        _(response).must_be :ok?
+
+        lesson_data = lessons_data.first
+        _(lesson_data['levelData']).wont_be_nil
+        _(lesson_data['levelData']['id']).must_equal pythonlab_level.id
+        _(lesson_data['levelData']['name']).must_equal 'Test Pythonlab'
+      end
+    end
+
+    context 'when lesson has multiple Pythonlab levels' do
+      let!(:pythonlab_level1) {create(:pythonlab, name: 'First Pythonlab')}
+      let!(:pythonlab_level2) {create(:pythonlab, name: 'Last Pythonlab')}
+
+      before do
+        create(:script_level, script: unit, lesson: lesson1, levels: [pythonlab_level1])
+        create(:script_level, script: unit, lesson: lesson1, levels: [pythonlab_level2])
+      end
+
+      it 'uses last Pythonlab level' do
+        get_lessons
+
+        _(response).must_be :ok?
+
+        lesson_data = lessons_data.first
+        _(lesson_data['levelData']['id']).must_equal pythonlab_level2.id
+        _(lesson_data['levelData']['name']).must_equal 'Last Pythonlab'
+      end
+    end
   end
 end
