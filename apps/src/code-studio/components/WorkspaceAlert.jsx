934a2243ba2342d3f19aae087861266e85a5b073
Mike Harvey <mike.harvey@code.org>
2025-12-12 13:21:38 -0500

    no jQuery for StudioAppTest

--- apps/src/code-studio/components/WorkspaceAlert.jsx
+++ apps/src/code-studio/components/WorkspaceAlert.jsx
@@ -18,7 +18,7 @@ export default class WorkspaceAlert extends React.Component {
     var toolbarWidth;
     if (isBlockly) {
       // use the toolbox header's width to get the width of the actual toolbox.
-      toolbarWidth = $('#toolbox-header').width();
+      toolbarWidth = document.getElementById('toolbox-header').offsetWidth;
     } else {
       toolbarWidth =
         $('.droplet-palette-element').width() + $('.droplet-gutter').width();
