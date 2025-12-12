1295032f7f23cf110fffba8d72e7e2cdc79582b8
Ceara Chewning <ceara.chewning@code.org>
2025-12-12 15:48:43 -0500

    Add lesson hook schema, update artifact prompt templates to include the previous artifact

--- dashboard/app/helpers/aidiff_prompt_helper.rb
+++ dashboard/app/helpers/aidiff_prompt_helper.rb
@@ -32,38 +32,35 @@ module AidiffPromptHelper
     }
   }
 
-  # LESSON_HOOK_SCHEMA = {
-  #   "type" => "object",
-  #   "required" => ["comment", "exit_ticket_items"],
-  #   "properties" => {
-  #     "comment" => {
-  #       "type" => "string",
-  #       "description" => "Your chat comment to the teacher addressing their question and introducing the exit_ticket content"
-  #     },
-  #     "exit_ticket_items" => {
-  #       "type" => "array",
-  #       "required" => ["question", "type", "answer"],
-  #       "items" => {
-  #         "type" => "object",
-  #         "properties" => {
-  #           "question" => {
-  #             "type" => "string",
-  #             "description" => "One exit ticket question for the students. This should be markdown formatted. If it is multiple choice, it must include lettered answer options"
-  #           },
-  #           "type" => {
-  #             "type" => "string",
-  #             "enum" => ["short_answer", "multiple_choice", "free_response"],
-  #             "description" => "The type of question this is, either short answer, multiple choice, or free response"
-  #           },
-  #           "answer" => {
-  #             "type" => "string",
-  #             "description" => "The correct answer to this question, or for free response, a bulleted list of elements an answer should have"
-  #           }
-  #         }
-  #       }
-  #     }
-  #   }
-  # }
+  LESSON_HOOK_SCHEMA = {
+    "type" => "object",
+    "required" => ["comment", "lesson_hook"],
+    "properties" => {
+      "comment" => {
+        "type" => "string",
+        "description" => "Your chat comment to the teacher addressing their question and introducing the lesson hook content to the teacher."
+      },
+      "lesson_hook" => {
+        "type" => "object",
+        "required" => ["introduction", "activity", "wrap_up"],
+        "description"=> "The lesson hook content that will be shown to students. It should be age-appropriate, generate interest in the lesson, take 1-5 minutes, and geared toward student interests.",
+        "properties" => {
+          "introduction" => {
+            "type" => "string",
+              "description" => "The introduction of the lesson hook's activity to the students. It should get student attention."
+          },
+          "activity" =>{
+            "type" => "string",
+                "description" => "A short activity that engages students and frames the lesson content. This should be markdown formatted."
+          },
+          "wrap_up" => {
+            "type" => "string",
+              "description" => "A wrap-up that connects the activity they just did to the lesson content and segues into the lesson. This can include questions to think about as they start the lesson."
+          }
+        }
+      }
+    }
+  }
 
   def self.get_prompt_supplement(section_contexts)
     return "" unless section_contexts
@@ -82,9 +79,24 @@ module AidiffPromptHelper
     new_input_text << "\n\n\n**The current message that you should respond to is:**\nUser: #{input}"
   end
 
-  def self.add_exit_ticket_formatting(prompt)
+  def self.add_exit_ticket_formatting(prompt, previous_message = nil)
     prompt = format('%{prompt}
-      This teacher is asking you to produce an exit ticket- a short set of 1-5 questions for the end of class that checks if students understand the major points of the lesson.
+
+      This teacher is asking you to produce or modify an exit ticket. An exit ticket is a short set of 1-5 questions for the end of class that checks if students understand the major points of the lesson. If the teacher requests a specific number of questions you must only generate the number of questions requested.
+      If the teacher asks for a change, addition, or subtraction to the exit ticket (e.g adding or removing a question), you must combine this with the content of the previous exit ticket by repeating exactly the content of the previous exit ticket items with the change, addition, or subtraction they requested. This might change the number of questions you should generate- for example adding or subtracting to the number of questions.', prompt: prompt
+      )
+
+    if previous_message&.is_artifact_candidate
+      prompt = format('%{prompt}
+        Here is the previous exit ticket that you should make modifications to:
+
+        %{previous_exit_ticket}
+
+        ', prompt: prompt, previous_exit_ticket: previous_message.content
+        )
+    end
+
+    prompt = format('%{prompt}
       Format your response in JSON using the following schema:
 
       %{json_schema}
@@ -106,7 +118,44 @@ module AidiffPromptHelper
     prompt
   end
 
-  def self.get_prompt_for_context(context, course_name, unit_name, lesson_name, is_preset, section_contexts, level_instructions, student_code, is_exit_ticket = false)
+  def self.add_lesson_hook_formatting(prompt, previous_message = nil)
+    prompt = format('%{prompt}
+
+      This teacher is asking you to produce or modify a lesson hook. A lesson hook is a short activity or fun introduction for the beginning of class that introduces the lesson, engages students, and connects the lesson with previous lessons, student interests, or other subjects. A lesson hook should take 1-5 minutes of class time. If the teacher requests a specific lesson hook subject or activity type, you must follow their request.
+      If the teacher asks for a change, addition, or subtraction to the lesson hook, you must combine this with the content of the previous lesson hook and only make the changes the teacher requested and no other changes.', prompt: prompt
+      )
+
+    if previous_message&.is_artifact_candidate
+      prompt = format('%{prompt}
+        Here is the previous lesson hook that you should make modifications to:
+
+        %{previous_lesson_hook}
+
+        ', prompt: prompt, previous_lesson_hook: previous_message.content
+        )
+    end
+
+    prompt = format('%{prompt}
+      Format your response in JSON using the following schema:
+
+      %{json_schema}
+
+      Example response:
+
+      {
+        "comment" : "Here\'s a lesson hook that uses an activity to introduce this lesson",
+        "lesson_hook" : {
+            "introduction" : "Hey students! We are doing an activity called duck duck goose to learn about birds",
+            "activity" : "Duck Duck Goose: sit in a circle, one person is it, if they say goose, then you chase them",
+            "wrap_up" : "Think about what other birds live in the water like ducks and geese. How are they similar?"
+        }
+      }
+    ', prompt: prompt, json_schema: LESSON_HOOK_SCHEMA.to_json
+      )
+    prompt
+  end
+
+  def self.get_prompt_for_context(context, course_name, unit_name, lesson_name, is_preset, section_contexts, level_instructions, student_code, artifact_type, previous_message = nil)
     case context
     when SharedConstants::AI_DIFF_CONTEXT[:LEVEL]
       prompt =
@@ -174,15 +223,18 @@ module AidiffPromptHelper
       $search_results$", section_contexts: get_prompt_supplement(section_contexts)
       )
     end
-    if is_exit_ticket
-      prompt = add_exit_ticket_formatting(prompt)
+    if artifact_type == SharedConstants::AI_DIFF_ARTIFACT_TYPE[:EXIT_TICKET]
+      prompt = add_exit_ticket_formatting(prompt, previous_message)
     end
-    # unless is_preset
-    #   prompt = format("%{prompt}
+    if artifact_type == SharedConstants::AI_DIFF_ARTIFACT_TYPE[:LESSON_HOOK]
+      prompt = add_lesson_hook_formatting(prompt, previous_message)
+    end
+    unless is_preset || artifact_type
+      prompt = format("%{prompt}
 
-    #   $output_format_instructions$", prompt: prompt
-    #   )
-    # end
+      $output_format_instructions$", prompt: prompt
+      )
+    end
     prompt
   end
 end
