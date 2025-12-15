cfda332226ef9ebf2df42925e5949d79fe063fb6
Liam Frye-Mason <liam.fryemason@code.org>
2025-12-15 14:12:05 -0800

    Add tests

--- apps/src/templates/teacherDashboardShared/WebSocketUtils.ts
+++ apps/src/templates/teacherDashboardShared/WebSocketUtils.ts
@@ -31,7 +31,7 @@ export function createTeacherNotificationSubscription(
       {
         connected() {
           setTimeout(() => {
-            this.unsubscribe();
+            subscription.unsubscribe();
           }, timeoutMs);
         },
 
