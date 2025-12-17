78000e41b4a4528c10799f884270983c8b6d3801
Molly Moen <molly@code.org>
2025-12-17 14:13:40 -0800

    add tab index to images

--- apps/src/codebridge/Editor/Editor.tsx
+++ apps/src/codebridge/Editor/Editor.tsx
@@ -103,7 +103,8 @@ export const Editor = ({langMapping, editableFileTypes}: EditorProps) => {
   if (file?.url && viewableImageFileType(file.language)) {
     return (
       <div>
-        <img src={file.url} alt={file.name} />
+        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
+        <img src={file.url} alt={file.name} tabIndex={0} />
       </div>
     );
   }
