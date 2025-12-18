85de0affcd5a075193e1b6b5df21e47b99bc05b4
Turner Riley <turner.riley@code.org>
2025-12-18 13:21:18 -0500

    also test ui

--- apps/src/aiDifferentiation/AiDiffFloatingActionButton.tsx
+++ apps/src/aiDifferentiation/AiDiffFloatingActionButton.tsx
@@ -41,7 +41,6 @@ interface AiDiffFloatingActionButtonProps {
   canStartOpen?: boolean;
   canDefaultOpen?: boolean;
 }
-
 const SESSION_STORAGE_KEY = 'AiDiffFabOpenStateKey';
 const LOCAL_STORAGE_OPENED_KEY = 'AiDiffHasOpenedKey';
 const LOCAL_STORAGE_CLOSED_KEY = 'AiDiffHasClosedKey';
