149106b9eb7f11c45468a37376a869708c0ae55f
wilkie <david.wilkinson@code.org>
2025-12-18 13:49:30 -0500

    Updates variant on GuideInstructions.

--- apps/src/lab2/views/components/guide/GuideInstructions.tsx
+++ apps/src/lab2/views/components/guide/GuideInstructions.tsx
@@ -50,7 +50,7 @@ const GuideInstructions: React.FunctionComponent<GuideInstructionsProps> = ({
         hasRun={hasRun}
         hasEdited={hasEdited}
         hideContinueIfDisabled={true}
-        variant={'simpleText'}
+        textVariant={'simple'}
       />
     </Guide>
   );
