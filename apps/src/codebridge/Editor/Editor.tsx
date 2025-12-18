e475a5c2e768b4449066dff2ae4362b15ea875df
Molly Moen <molly@code.org>
2025-12-17 16:40:52 -0800

    remove div

--- apps/src/codebridge/Editor/Editor.tsx
+++ apps/src/codebridge/Editor/Editor.tsx
@@ -101,12 +101,8 @@ export const Editor = ({langMapping, editableFileTypes}: EditorProps) => {
   ]);
 
   if (file?.url && viewableImageFileType(file.language)) {
-    return (
-      <div>
-        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
-        <img src={file.url} alt={file.name} tabIndex={0} />
-      </div>
-    );
+    // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+    return <img src={file.url} alt={file.name} tabIndex={0} />;
   }
 
   if (file && !editableFileType(file.language, editableFileTypes)) {
