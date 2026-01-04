212f1d19ec39e5afb28b1ce978981ab9284fac1e
wilkie <wilkie05@gmail.com>
2026-01-04 10:03:20 -0500

    Adds localization package.

---
+++ frontend/packages/localization/src/index.ts
@@ -0,0 +1,5 @@
+import {Localization, localization} from './Localization';
+import {useLocalization} from './useLocalization';
+export {Localization, useLocalization};
+const localizationInstance = localization();
+export default localizationInstance;
