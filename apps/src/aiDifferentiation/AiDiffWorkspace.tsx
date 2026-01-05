14ac8e98fbffd8fe04f3fcc5c9c94190c0d71c74
Turner Riley <turner.riley@code.org>
2026-01-05 15:29:41 -0500

    remove unnecessary import

--- apps/src/aiDifferentiation/AiDiffWorkspace.tsx
+++ apps/src/aiDifferentiation/AiDiffWorkspace.tsx
@@ -2,7 +2,6 @@ import React, {useCallback, useEffect, useState} from 'react';
 
 import {fetchThreadMessages} from '@cdo/apps/aichat/redux/thunks';
 import {PersonalizationData} from '@cdo/apps/aiDifferentiation/hooks/useTeachingProfileData';
-import experiments from '@cdo/apps/util/experiments';
 import {useAppDispatch} from '@cdo/apps/util/reduxHooks';
 
 import HttpClient from '../util/HttpClient';
