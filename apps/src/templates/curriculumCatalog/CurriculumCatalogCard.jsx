957bcf42a17372e08f79c0a329b93362a9dea2c5
denyslevada <levada.denys@gmail.com>
2026-01-06 19:59:39 +0200

    feat: replace custom Button components with MUI Button in CurriculumCatalogCard and ExpandedCurriculumCatalogCard

--- apps/src/templates/curriculumCatalog/CurriculumCatalogCard.jsx
+++ apps/src/templates/curriculumCatalog/CurriculumCatalogCard.jsx
@@ -1,12 +1,8 @@
 import {
-  Button,
-  buttonColors,
-  LinkButton,
-} from '@code-dot-org/component-library/button';
-import {
   BodyThreeText,
   Heading4,
 } from '@code-dot-org/component-library/typography';
+import {Button as MuiButton} from '@mui/material';
 import classNames from 'classnames';
 import {concat, intersection} from 'lodash';
 import PropTypes from 'prop-types';
@@ -349,48 +345,58 @@ const CustomizableCurriculumCatalogCard = ({
               )}
             >
               {onQuickViewClick && (
-                <Button
+                <MuiButton
                   onClick={onQuickViewClick}
-                  ariaLabel={quickViewButtonDescription}
-                  text={i18n.quickView()}
+                  variant="outlined"
+                  color="secondary"
+                  size="medium"
                   className={`${style.buttonFlex} ${style.quickViewButton}`}
-                  type="secondary"
-                  color={buttonColors.black}
-                />
+                  aria-label={quickViewButtonDescription}
+                  type="button"
+                >
+                  {i18n.quickView()}
+                </MuiButton>
               )}
               {isTeacherOrSignedOut && (
                 <>
-                  <LinkButton
-                    color={buttonColors.black}
-                    type="secondary"
-                    href={pathToCourse}
-                    ariaLabel={i18n.learnMoreDescription({
+                  <MuiButton
+                    variant="outlined"
+                    color="secondary"
+                    size="medium"
+                    className={`${style.buttonFlex} ${style.teacherAndSignedOutLearnMoreButton}`}
+                    aria-label={i18n.learnMoreDescription({
                       course_name: courseDisplayName,
                     })}
-                    text={i18n.learnMore()}
-                    className={`${style.buttonFlex} ${style.teacherAndSignedOutLearnMoreButton}`}
-                  />
-                  <Button
-                    color={buttonColors.purple}
-                    type="primary"
-                    onClick={() => handleClickAssign('top-card')}
-                    ariaLabel={assignButtonDescription}
-                    text={assignButtonText}
+                    href={pathToCourse}
+                  >
+                    {i18n.learnMore()}
+                  </MuiButton>
+                  <MuiButton
+                    variant="contained"
+                    color="primary"
+                    size="medium"
                     className={style.buttonFlex}
-                  />
+                    onClick={() => handleClickAssign('top-card')}
+                    aria-label={assignButtonDescription}
+                    type="button"
+                  >
+                    {assignButtonText}
+                  </MuiButton>
                 </>
               )}
               {!isTeacherOrSignedOut && (
-                <LinkButton
-                  color={buttonColors.purple}
-                  type="primary"
-                  href={pathToCourse}
-                  ariaLabel={i18n.tryCourseNow({
+                <MuiButton
+                  variant="contained"
+                  color="primary"
+                  size="medium"
+                  className={`${style.buttonFlex} ${style.studentLearnMoreButton}`}
+                  aria-label={i18n.tryCourseNow({
                     course_name: courseDisplayName,
                   })}
-                  text={i18n.tryNow()}
-                  className={`${style.buttonFlex} ${style.studentLearnMoreButton}`}
-                />
+                  href={pathToCourse}
+                >
+                  {i18n.tryNow()}
+                </MuiButton>
               )}
             </div>
           </div>
