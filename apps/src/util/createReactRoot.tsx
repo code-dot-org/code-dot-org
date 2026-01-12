aed644710a37779dac45a70d3f0865783643a928
levadadenys <levada.denys@gmail.com>
2026-01-12 15:07:24 +0200

    Revert "Revert "[DESIGN2-238] Rewrite font mixins/variables to be css variables"" (#70274)
    * Revert "Revert "[DESIGN2-238] Rewrite font mixins/variables to be css variabl…"
    
    This reverts commit 17da5fbd08a55baaad3b595595c4354b57e21ce4.
    
    * feat: import font variables from component library styles

--- apps/src/util/createReactRoot.tsx
+++ apps/src/util/createReactRoot.tsx
@@ -1,3 +1,4 @@
+import '@code-dot-org/component-library-styles/fontVariables.css';
 import {CdoTheme} from '@code-dot-org/component-library/themes';
 import {ThemeProvider as MuiThemeProvider} from '@mui/material/styles';
 import React, {ReactElement} from 'react';
