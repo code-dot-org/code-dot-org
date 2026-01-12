8e543d95173d3b35e337a19d9577d55e13c8eeba
alex-m-brown <alex.brown@code.org>
2026-01-12 15:35:32 -0500

    move to projects helper

--- dashboard/app/models/project.rb
+++ dashboard/app/models/project.rb
@@ -39,18 +39,6 @@ class Project < ApplicationRecord
     self.uuid ||= SecureRandom.uuid
   end
 
-  # Projects can be created via Sequel/Sinatra (projects.rb) or Rails (project.rb),
-  # depending on where they've been created. For example, real user projects are often created
-  # in Sinatra, but test projects are often created via Rails. The following two methods are
-  # helper methods to simplify finding projects elsewhere in the code.
-  def self.find_by_id(id)
-    Projects.table.where(id: id).first || Project.find_by(id: id)
-  end
-
-  def self.find_by_uuid(uuid)
-    Projects.table.where(uuid: uuid).first || Project.find_by(uuid: uuid)
-  end
-
   # Finds a project by channel id. Like `find`, this method raises an
   # ActiveRecord::RecordNotFound error if the corresponding project cannot
   # be found.
