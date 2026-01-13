65ad7128d9a28b2980af6f45bf9612be38c0c1b0
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2026-01-13 07:31:41 -0800

    Lab2 GenericDialog: set Dialog as default component and add useModal prop (#70256)
    * refactor to use CustomDialog
    
    * refactor to use DSCO Modal
    
    * update PendingDialog to also use Modal
    
    * clean up
    
    * set isPending on button in PendingDialog.tsx
    
    * remove titleComponent props
    
    * fix keyboard nav focus issue
    
    * add useModal prop to GenericDialog.tsx
    
    * add bg theme support to Dialog
    
    * fix accessibility console error
    
    * use Dialog instead of Modal on PendingDialog.tsx
    
    * add Modals where needed
    
    * add accessibility warning fix to PendingDialog.tsx
    
    * update secondary button color
    
    * remove useEnterKeyboardTrap
    
    * add comment clarifying dialog vs. modal

--- apps/src/lab2/hooks/index.ts
+++ apps/src/lab2/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useLevelActivityMetrics';
 export * from './useFileUploader';
-export * from './useKeyboardTrap';
 export * from './useLifecycleNotifier';
 export * from './usePartialApply';
 export * from './usePanelPosition';
