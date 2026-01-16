4fb62132d03efe094b348b01c157e994acca7162
wilkie <wilkie05@gmail.com>
2026-01-15 21:42:54 -0500

    Adds toolbox styling for music lab.

--- frontend/packages/labs/music/src/blockly/toolbox/index.ts
+++ frontend/packages/labs/music/src/blockly/toolbox/index.ts
@@ -1,8 +1,17 @@
 import type * as Blockly from 'blockly/core';
+import classNames from 'classnames';
 
 import simple2Toolbox from './simple2';
 
 import {BlockMode} from '../../constants';
+
+import moduleStyles from './toolbox.module.scss';
+
+const baseCategoryCssConfig = {
+  container: moduleStyles.toolboxCategoryContainer,
+  row: classNames(moduleStyles.toolboxRow, 'blocklyTreeRow'),
+  label: moduleStyles.toolboxLabel,
+};
 
 // Convert a simple toolbox array into a blockly toolbox info
 const convert: (map: {
@@ -12,6 +21,7 @@ const convert: (map: {
   contents: Object.entries(map).map(([name, blocks]) => ({
     kind: 'category',
     name,
+    cssconfig: baseCategoryCssConfig,
     contents: blocks.map(type => ({
       kind: 'block',
       type,
