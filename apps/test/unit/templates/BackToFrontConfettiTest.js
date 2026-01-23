1a88555a9ec045bb1e4db015774198fa10a898aa
Ben Brooks <ben@code.org>
2026-01-23 14:50:38 -0800

    Fix BackToFrontConfettiTest

--- apps/test/unit/templates/BackToFrontConfettiTest.js
+++ apps/test/unit/templates/BackToFrontConfettiTest.js
@@ -1,5 +1,6 @@
 import {mount} from 'enzyme'; // eslint-disable-line no-restricted-imports
 import React from 'react';
+import {act} from 'react-dom/test-utils';
 
 import BackToFrontConfetti from '@cdo/apps/templates/BackToFrontConfetti';
 
@@ -19,26 +20,43 @@ describe('BackToFrontConfetti', () => {
     expect(wrapper).to.have.style('zIndex', '-1');
   });
 
-  it('remains negatively zIndexed indefinitely after rendering', () => {
+  it('remains negatively zIndexed indefinitely after rendering', async () => {
     const wrapper = mount(<BackToFrontConfetti />);
-    jest.advanceTimersByTime(999);
+    await act(async () => {
+      jest.advanceTimersByTime(999);
+    });
+    wrapper.update();
     expect(wrapper).to.have.style('zIndex', '-1');
   });
 
-  it('remains negatively zIndexed temporarily after activation', () => {
+  it('remains negatively zIndexed temporarily after activation', async () => {
     const wrapper = mount(<BackToFrontConfetti />);
-    wrapper.setProps({active: true});
+    await act(async () => {
+      wrapper.setProps({active: true});
+    });
+    wrapper.update();
     expect(wrapper).to.have.style('zIndex', '-1');
-    jest.advanceTimersByTime(10);
+    await act(async () => {
+      jest.advanceTimersByTime(10);
+    });
+    wrapper.update();
     expect(wrapper).to.have.style('zIndex', '-1');
   });
 
-  it('switches to a positive zIndex shortly after activation', () => {
+  it('switches to a positive zIndex shortly after activation', async () => {
     const wrapper = mount(<BackToFrontConfetti />);
-    wrapper.setProps({active: true});
-    jest.advanceTimersByTime(600);
+    await act(async () => {
+      wrapper.setProps({active: true});
+    });
+    await act(async () => {
+      jest.advanceTimersByTime(600);
+    });
+    wrapper.update();
     expect(wrapper).to.have.style('zIndex', '-1');
-    jest.advanceTimersByTime(100);
+    await act(async () => {
+      jest.advanceTimersByTime(100);
+    });
+    wrapper.update();
     expect(wrapper).to.have.style('zIndex', '1');
   });
 });
