50a2891c8ae16f98097dbdd74a5dba7285783b6c
Molly Moen <molly@code.org>
2026-01-26 15:30:44 -0800

    change to has_many and see what breaks

--- dashboard/app/models/lesson.rb
+++ dashboard/app/models/lesson.rb
@@ -57,7 +57,7 @@ class Lesson < ApplicationRecord
   has_many :lessons_opportunity_standards,  dependent: :destroy
   has_many :opportunity_standards, through: :lessons_opportunity_standards, source: :standard
 
-  has_one :rubric, dependent: :destroy
+  has_many :rubric, dependent: :destroy
 
   self.table_name = 'stages'
 
