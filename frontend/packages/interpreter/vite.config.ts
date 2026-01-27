31d6547e90f0e16825edc1b38a820f980893eb50
wilkie <wilkie05@gmail.com>
2026-01-27 17:08:59 -0500

    Adds interpreter package and uses it to generate some code in music-lab.

---
+++ frontend/packages/interpreter/vite.config.ts
@@ -0,0 +1,54 @@
+import path from 'node:path';
+import type {OutputOptions} from 'rollup';
+import {defineConfig} from 'vite';
+import dts from 'vite-plugin-dts';
+import {externalizeDeps} from 'vite-plugin-externalize-deps';
+
+/**
+ * Get Rollup output configuration.
+ * @param format es or cjs
+ * @returns Rollup output configuration
+ */
+function getRollupOutputConfig(format: 'es' | 'cjs'): OutputOptions {
+  return {
+    format,
+    exports: 'auto',
+    entryFileNames: format === 'es' ? '[name].mjs' : '[name].js',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+  };
+}
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    // Generate Typescript declaration files using the Vite default tsconfig
+    dts({
+      tsconfigPath: './tsconfig.json',
+      rollupTypes: false, // Disable rolling up types to a single file
+      entryRoot: 'src',
+      insertTypesEntry: false, // Prevent inserting a single types entry
+      exclude: ['**/__tests__/**', '**/*.test.tsx'],
+    }),
+    // Ensure dependencies are externalized for library build
+    // Libraries such as react, react-dom, lodash, etc. should not be bundled by the library.
+    // Instead, they are expected to be provided by the host application.
+    externalizeDeps(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    sourcemap: true,
+    cssCodeSplit: true,
+    lib: {
+      entry: 'src/index.ts',
+      name: 'core',
+    },
+    rollupOptions: {
+      output: [getRollupOutputConfig('es'), getRollupOutputConfig('cjs')],
+    },
+  },
+});
