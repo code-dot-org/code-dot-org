b040afd2d54606832d273bc5486955a2e743255d
Ed Baafi <edward.baafi@code.org>
2026-01-28 01:51:07 -0500

    update prompt formatting

--- apps/src/json-video/json-video-prompt.ts
+++ apps/src/json-video/json-video-prompt.ts
@@ -17,5 +17,5 @@ const videoDescriptions = Object.entries(jsonAssets)
   .join('\n');
 
 export const jsonVideoPrompt = showAiTutorVideos
-  ? `The following videos are available for the user to watch if they are helpful(provided in format: [VIDEO-FILENAME]::[VIDEO-DESCRIPTION]) \n${videoDescriptions} \n. Please add a video using "[video](https://json-video.org/[VIDEO-FILENAME])" (where [VIDEO-FILENAME] is swapped out with the filename) to your response ONLY if the video's description matches what the student needs to understand currently.\n`
+  ? `The following videos are available for the user to watch if they are helpful (provided in format: [VIDEO-FILENAME]::[VIDEO-DESCRIPTION]) \n${videoDescriptions} \n. Please add a video using "[video](https://json-video.org/[VIDEO-FILENAME])" (where [VIDEO-FILENAME] is swapped out with the filename) to your response ONLY if the video's description matches what the student needs to understand currently.\n`
   : ``;
