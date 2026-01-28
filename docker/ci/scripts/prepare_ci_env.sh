892d1b61ecf61871d429027514a5ff0a5bc8bbfd
Dave Bailey <davidsbailey@users.noreply.github.com>
2026-01-28 10:39:25 -0800

    Consolidate config variables from prepare_ci_env.sh to test.yml.erb (#69968)
    * move CI_JOB check inside CI check in test yml erb
    
    * move ci config var definitions into test yml erb
    
    * temporarily trigger cache-staging-ci-build pipeline
    
    * improve comment on CI env vars
    
    * remove redundant build_dashboard and build_pegasus declarations
    
    * skip setting localize_apps and ignore_eyes_mismatches
    
    * remove custom dashboard_db_reader
    
    * try removing aiproxy_api_key
    
    * group and explain ci config values with comments
    
    * remove redundant build_i18n
    
    * break test.yml.erb into sections with header comments
    
    * move active job config into ci section, and add unit-specific header comment
    
    * fold statsig keys in with other secrets
    
    * ensure no stale locals.yml from cache
    
    * fix typo on SAUCE_ACCESS_KEY
    
    * Revert "temporarily trigger cache-staging-ci-build pipeline"
    
    This reverts commit 0e9caa68e27f05373ff532f57165fa5ce2442f51.
    
    * temporarily run tests against temp build cache
    
    * remove custom locals.yml from github actions and workflows
    
    * fix docker compose files
    
    * remove unused localize_apps config var from config.yml.erb
    
    * Revert "temporarily run tests against temp build cache"
    
    This reverts commit e1280310dfb3d688ef5a5a872b6c7f609c7c021f.
    
    * Revert "fold statsig keys in with other secrets"
    
    This reverts commit d3a6a426458cef2b7ece98825ab45f6bcb4d873e.
    
    * provide chef secrets to non-unit-test rake tasks on test system
    
    * rerun drone
    
    * fix comment on test system config
    
    * Revert "remove unused localize_apps config var from config.yml.erb"
    
    This reverts commit cfbfbd78113f7dd72677ad94e7f0fc4d099e7fe7.
    
    * regen temp build cache
    
    * Revert "regen temp build cache"
    
    This reverts commit 2b5db55d5e39ec2362aee8b8118f09fc1637d91b.
    
    * test against latest temp cache build
    
    * rerun drone
    
    * Revert "test against latest temp cache build"
    
    This reverts commit 7c9cb2c437bac9745cd89ef2dcc4be270ed78a92.

--- docker/ci/scripts/prepare_ci_env.sh
+++ docker/ci/scripts/prepare_ci_env.sh
@@ -46,43 +46,4 @@ fi
 
 ulimit -n 16000
 
-# Set up locals.yml.
-# TODO: move all of this into test.yml.erb
-echo "
-build_apps: true
-build_dashboard: true
-build_pegasus: true
-cloudfront_key_pair_id: $CLOUDFRONT_KEY_PAIR_ID
-cloudfront_private_key: \"$CLOUDFRONT_PRIVATE_KEY\"
-contentful_cs_for_all_access_token: $CONTENTFUL_CS_FOR_ALL_ACCESS_TOKEN
-dashboard_db_reader: \"mysql://readonly@localhost/dashboard_test\"
-dashboard_enable_pegasus: true
-dashboard_workers: 5
-disable_all_eyes_running: true
-ignore_eyes_mismatches: true
-localize_apps: true
-use_my_apps: true
-skip_seed_all: true
-override_dashboard: \"localhost-studio.code.org\"
-override_pegasus: \"localhost.code.org\"
-dashboard_port: 3000
-pegasus_port: 3000
-build_i18n: true
-animations_s3_directory: animations_circle/$CI_BUILD_NUMBER
-assets_s3_directory: assets_circle/$CI_BUILD_NUMBER
-files_s3_directory: files_circle/$CI_BUILD_NUMBER
-libraries_s3_directory: libraries_circle/$CI_BUILD_NUMBER
-sources_s3_directory: sources_circle/$CI_BUILD_NUMBER
-redis_url: 'redis://ci-tests-redis:6379/0'
-no_https_store: true
-netsim_redis_groups:
-- master: redis://ci-tests-redis:6379
-saucelabs_authkey: $SAUCE_ACCESS_KEY
-saucelabs_username: $SAUCE_USERNAME
-saucelabs_tunnel_name: cdo-tunnel-$CI_BUILD_NUMBER
-properties_encryption_key: $PROPERTIES_ENCRYPTION_KEY
-aiproxy_api_key: 'fake_key'
-" >> locals.yml
-echo "Wrote settings and secrets from env vars into locals.yml."
-
 set -x
