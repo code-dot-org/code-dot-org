c7f776eea58eb75ecf5df06f48cd5032fbdcd8dd
Liam Frye-Mason <liam.fryemason@code.org>
2026-01-28 15:50:46 -0800

    [Snapshot] Add Lesson Insight get endpoint and widget UI (#70540)
    * First commit for displaying lesson insight
    
    * Small fixes
    
    * Add lesson insight widget UI
    
    * Small ui changes and animated border
    
    * Small fixes

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -1190,6 +1190,7 @@ Dashboard::Application.routes.draw do
         get 'cfu_responses/:lesson_id', controller: :student_snapshots, action: :cfu_responses # GET /student_snapshots/cfu_responses/{lesson_id}?student_id=123
         get 'exemplar_code/:lesson_id', action: :exemplar_code # GET /student_snapshots/exemplar_code/{lesson_id}
         get 'units/:unit_id/lessons/:lesson_id/students/:student_id/code', action: :student_code # GET /student_snapshots/units/:unit_id/lessons/:lesson_id/students/:student_id/code
+        get 'lesson_insight', controller: :student_snapshots, action: :lesson_insight # GET /student_snapshots/lesson_insight
       end
     end
 
