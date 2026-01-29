1cab38caf6e1c296da4fdc4829cccd5b8074fd41
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-01-29 17:19:19 +0200

    Revert "P20-1699: Remove unnecessary configurations"
    This reverts commit dbd4ed9570dfea702db113b0d8fb2a126fec2151.

--- docker/ci/scripts/prepare_ci_env.sh
+++ docker/ci/scripts/prepare_ci_env.sh
@@ -63,7 +63,6 @@ ignore_eyes_mismatches: true
 localize_apps: true
 use_my_apps: true
 skip_seed_all: true
-default_scheme: \"http:\"
 override_dashboard: \"localhost-studio.code.org\"
 override_pegasus: \"localhost.code.org\"
 dashboard_port: 3000
