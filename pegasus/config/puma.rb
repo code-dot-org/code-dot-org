39d493398d75e3eaafb29378a714d4c2be795765
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-01-30 05:36:40 +0200

    chore(newrelic): move agent reinit to CDO after_forkhook

--- pegasus/config/puma.rb
+++ pegasus/config/puma.rb
@@ -27,16 +27,5 @@ before_fork do
 end
 
 on_worker_boot do |_index|
-  # Puma runs this hook in each worker process after it forks (cluster mode).
-  # When the master preloads the app, the forked worker inherits the parent's in memory New Relic state,
-  # including connection and background thread state. New Relic recommends calling `after_fork` in the worker
-  # to reset that inherited state and start the agent cleanly inside the worker process.
-  #
-  # `force_reconnect: true` ensures the worker opens its own connection to the New Relic collector rather than
-  # attempting to reuse any inherited connection state from the master process.
-  #
-  # @see https://www.rubydoc.info/gems/newrelic_rpm/8.16.0/NewRelic%2FAgent:after_fork
-  NewRelic::Agent.after_fork(force_reconnect: true) if defined?(NewRelic::Agent)
-
   Cdo::AppServerHooks.after_fork(host: CDO.pegasus_hostname)
 end
