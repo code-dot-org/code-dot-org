238466a4252192e27ce3dd9abf218db480a4c4fc
suresh <suresh@code.org>
2026-01-30 15:49:00 -0800

    Use native puma stats instead of Raindrops for web application server metrics.

--- lib/cdo/app_server_metrics.rb
+++ lib/cdo/app_server_metrics.rb
@@ -1,54 +1,28 @@
-require 'raindrops'
 require 'cdo/aws/metrics'
 require 'honeybadger/ruby'
 require 'concurrent/timer_task'
-require 'active_support/core_ext/module/attribute_accessors'
 
 module Cdo
-  # AppServerMetrics extends the Raindrops::Middleware class,
-  # which instruments a Rack application to collect the number of
-  # currently-executing requests using an atomic counter shared across
-  # all forked worker-processes.
-  #
-  # Every :interval seconds (default 1), metrics are collected.
-  #
-  # The following metrics are collected and reported:
-  # `active` - the number of active TCP/socket connections
-  # `queued` - the number of queued TCP/socket requests
-  # `calling` - the maximum number of currently-executing requests at any point during the interval.
-  #
-  # The :listeners option accepts an array of strings (TCP or Unix domain socket names).
-  # By default, all listeners used by the Unicorn master process are monitored.
-  #
-  # Any errors are forwarded to Honeybadger for logging and notifying.
-  class AppServerMetrics < Raindrops::Middleware
-    cattr_accessor :instance
-
-    def initialize(app, opts = {})
-      # Track max_calling using a modified Stats implementation.
-      opts[:stats] ||= StatsWithMax.new
-      # Disable the reporting endpoint with an empty-string :path by default.
-      opts[:path] ||= ''
-      super(app, opts)
-      @metrics = %i(active queued calling).map {|name| [name, []]}.to_h
-
+  # Collect puma web application server statistics and publish to AWS CloudWatch.
+  class AppServerMetrics
+    def initialize(opts = {})
       @namespace = opts[:namespace] || 'App Server'
       @dimensions = opts[:dimensions] || {}
+
+      # Default to 1 if not provided, or use the dynamic values from hooks.
       @interval = opts[:interval] || 1
+      @resolution = opts[:resolution] || 1
+
       @instance_id = 'UNKNOWN'
-      self.instance = self
     end
 
     # Fetch or retry the ID.
     def instance_id
-      # Fast path: return the ID if it's already known.
-      unless @instance_id == 'UNKNOWN'
-        return @instance_id
-      end
+      return @instance_id unless @instance_id == 'UNKNOWN'
 
-      if @instance_id == 'UNKNOWN'
-        fetched_id = AWS::EC2.instance_id
-        @instance_id = fetched_id if fetched_id # Update only on success.
+      # Attempt fetch.
+      if (fetched_id = AWS::EC2.instance_id)
+        @instance_id = fetched_id
       end
 
       @instance_id
@@ -58,62 +32,56 @@ module Cdo
       @spawn_reporting_task&.shutdown
     end
 
-    def spawn_reporting_task
-      @spawn_reporting_task ||= Concurrent::TimerTask.new(execution_interval: @interval) {|task| collect_metrics(task)}.
+    def start_puma_reporting
+      # If we already have a running task, don't start another.
+      return @spawn_reporting_task if @spawn_reporting_task&.running?
+
+      @spawn_reporting_task = Concurrent::TimerTask.new(execution_interval: @interval) {|task| collect_puma_stats(task)}.
         with_observer {|_, _, ex| Honeybadger.notify(ex) if ex}.
         execute
     end
 
-    # Periodically collect unicorn-listener metrics.
-    def collect_metrics(*_)
-      # Retrieve the best available ID (UNKNOWN or real i-xxxx)
-      current_instance_id = instance_id
+    # Periodically collect puma metrics.
+    def collect_puma_stats(*_)
+      namespace = @namespace
+      dimensions = @dimensions
 
-      collect_listener_stats.each do |name, value|
+      # Puma.stats_hash provides the true internal state of all workers
+      stats = Puma.stats_hash
+      worker_statuses = stats[:worker_status].map {|w| w[:last_status]}
+
+      # Aggregate metrics across the entire cluster on this instance
+      metrics = {
+        'backlog' => worker_statuses.sum {|s| s[:backlog] || 0},
+        'running' => worker_statuses.sum {|s| s[:running] || 0},
+        'pool_capacity' => worker_statuses.sum {|s| s[:pool_capacity] || 0},
+        'booted_workers' => stats[:booted_workers] || 0
+      }
+
+      metrics.each do |name, value|
+        # Public Deployment-level (Environment + Host) metrics.
         Cdo::Metrics.put(
-          @namespace,
+          namespace,
           name,
           value,
-          @dimensions,
-          storage_resolution: 1,
+          dimensions,
+          storage_resolution: @resolution,
           unit: 'Count'
         )
-        # Also publish active/queued/calling listener metrics with an EC2 Instance ID Dimension.
+
+        # Retrieve the best available ID (UNKNOWN or real i-xxxx).
+        current_instance_id = instance_id
+
+        # Public Instance-level (Environment + Host + InstanceId) metrics.
         Cdo::Metrics.put(
-          @namespace,
+          namespace,
           name,
           value,
-          @dimensions.merge(InstanceId: current_instance_id),
-          storage_resolution: 1,
+          dimensions.merge(InstanceId: current_instance_id),
+          storage_resolution: @resolution,
           unit: 'Count'
         )
       end
-    end
-
-    # Collect current snapshot of tcp/unix listener stats.
-    # @return [Hash{Symbol => Number}]
-    def collect_listener_stats
-      stats = {}
-      stats.merge! Raindrops::Linux.tcp_listener_stats(@tcp.uniq) if @tcp
-      stats.merge! Raindrops::Linux.unix_listener_stats(@unix.uniq) if @unix
-      stats = %i(active queued).map do |name|
-        [name, stats.values.sum(&name)]
-      end.to_h
-      stats[:calling] = @stats.max_calling.tap {@stats.max_calling = 0}
-      stats
-    end
-  end
-
-  # Extends Raindrops::Middleware::Stats (which defines :calling and :writing)
-  # with an additional :max_calling atomic counter.
-  # This allows us to record the peak number of currently-executing
-  # worker-processes at any point, which a one-second sampling interval
-  # might not otherwise capture.
-  class StatsWithMax < Raindrops::Struct.new(:calling, :writing, :max_calling)
-    # Override incr_calling to keep max_calling updated.
-    def incr_calling
-      calling = super
-      self.max_calling = calling if calling > max_calling
     end
   end
 end
