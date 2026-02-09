1cfdc28b5088191638dc10eda1076f634bcfdb8c
Molly Moen <molly@code.org>
2026-02-09 13:07:07 -0800

    basic functionality

---
+++ apps/src/levelbuilder/rubrics/LinkToRubricEditor.tsx
@@ -0,0 +1,63 @@
+import {LinkButton} from '@code-dot-org/component-library/button';
+import {
+  BodyFourText,
+  BodyThreeText,
+  Heading5,
+} from '@code-dot-org/component-library/typography';
+import React from 'react';
+
+import moduleStyles from './link-to-rubric-editor.module.scss';
+
+interface LinkToRubricEditorProps {
+  lessons: {
+    id: number;
+    name: string;
+    rubric_id: number | null;
+    script_name: string | null;
+  }[];
+}
+
+export default function LinkToRubricEditor({lessons}: LinkToRubricEditorProps) {
+  return (
+    <div className={moduleStyles.linkContainer}>
+      <Heading5>Add or Edit Rubrics</Heading5>
+      <BodyThreeText>
+        Rubrics are defined on the lesson. The level the rubric is assigned to
+        must be submittable. Below are links to the lessons that this level is a
+        part of. You must make this level submittable and save it before you
+        will be able to assign a rubric to it. The edit/add links will open in a
+        new tab.
+      </BodyThreeText>
+      {lessons.length === 0 && (
+        <BodyThreeText>
+          This level is not in any lessons. Assign it to a lesson to add or edit
+          the rubric for the lesson.
+        </BodyThreeText>
+      )}
+      <div className={moduleStyles.rubricButtonContainer}>
+        {lessons.map(lesson => (
+          <div key={lesson.id}>
+            <BodyThreeText>
+              Script: {lesson.script_name || 'NONE'}, Lesson: {lesson.name}
+            </BodyThreeText>
+            {lesson.rubric_id ? (
+              <LinkButton
+                href={'/rubrics/' + lesson.rubric_id + '/edit'}
+                target="_blank"
+                text={`Edit Rubric`}
+                size={'s'}
+              />
+            ) : (
+              <LinkButton
+                href={'/rubrics/new?lessonId=' + lesson.id}
+                target="_blank"
+                text={`Add Rubric`}
+                size={'s'}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- apps/src/sites/studio/pages/levels/editors/fields/_special_level_types.js
+++ apps/src/sites/studio/pages/levels/editors/fields/_special_level_types.js
@@ -2,9 +2,13 @@ import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import LinkToRubricEditor from '@cdo/apps/levelbuilder/rubrics/LinkToRubricEditor';
+import getScriptData from '@cdo/apps/util/getScriptData';
+
 $(document).ready(initPage);
 
 function initPage() {
+  const lessons = getScriptData('lessons');
   $('#plusAnswerContainedLevel').on('click', () => {
     $('#plusAnswerContainedLevel')
       .prev()
@@ -13,7 +17,7 @@ function initPage() {
   });
 
   ReactDOM.render(
-    <div>test</div>,
+    <LinkToRubricEditor lessons={lessons} />,
     document.getElementById('link-to-rubric-editor')
   );
 }
