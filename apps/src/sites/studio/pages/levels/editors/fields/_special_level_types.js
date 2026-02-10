621187a2eac7957c10f4104e53a04a9a0c6c1ce0
Molly Moen <molly@code.org>
2026-02-10 14:46:59 -0800

    Make rubrics editing easier (#70705)
    * wip
    
    * basic functionality
    
    * better ui
    
    * increase size of no lesson message
    
    * clean up
    
    * fix for levels with parents

--- apps/src/sites/studio/pages/levels/editors/fields/_special_level_types.js
+++ apps/src/sites/studio/pages/levels/editors/fields/_special_level_types.js
@@ -1,12 +1,23 @@
 import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import LinkToRubricEditor from '@cdo/apps/levelbuilder/rubrics/LinkToRubricEditor';
+import getScriptData from '@cdo/apps/util/getScriptData';
 
 $(document).ready(initPage);
 
 function initPage() {
+  const lessons = getScriptData('lessons');
   $('#plusAnswerContainedLevel').on('click', () => {
     $('#plusAnswerContainedLevel')
       .prev()
       .clone()
       .insertBefore('#plusAnswerContainedLevel');
   });
+
+  ReactDOM.render(
+    <LinkToRubricEditor lessons={lessons} />,
+    document.getElementById('link-to-rubric-editor')
+  );
 }
