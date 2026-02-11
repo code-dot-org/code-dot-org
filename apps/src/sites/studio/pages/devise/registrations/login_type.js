1b26ea5a10acc99371ef66e6954b59d05fc342ce
Ryan Driscoll <ryan.driscoll@code.org>
2026-02-11 03:44:26 -0700

    [Design2-231] chore: adds the mui theme provider throughout the application (#70393)
    * chore: adds the mui theme provider throughout the application
    
    Replaces ReactDOM.render with bootstrap function createReactRoot
    
    * fix: fix tests
    
    * fix: lint fix
    
    * feat: add ref handling for PairingDialog and ContractForm components (should fix the ui tests)
    
    ---------
    
    Co-authored-by: levadadenys <levada.denys@gmail.com>

--- apps/src/sites/studio/pages/devise/registrations/login_type.js
+++ apps/src/sites/studio/pages/devise/registrations/login_type.js
@@ -1,14 +1,14 @@
 import $ from 'jquery';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import LoginTypeSelection from '@cdo/apps/signUpFlow/LoginTypeSelection';
+import {createReactRoot} from '@cdo/apps/util/createReactRoot';
 import getScriptData from '@cdo/apps/util/getScriptData';
 
 $(document).ready(() => {
   const isSignedOut = getScriptData('isSignedOut');
   const passwordMinLength = getScriptData('passwordMinLength');
-  ReactDOM.render(
+  createReactRoot(
     <LoginTypeSelection
       isSignedOut={isSignedOut}
       passwordMinLength={passwordMinLength}
