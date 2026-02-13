fcafc96d64b2575e373b049e7ce20a78adb1662c
Ed Baafi <edward.baafi@code.org>
2026-02-12 20:07:39 -0500

    switch other instances of 'gpt-4o-mini' to shared constant

--- dashboard/app/helpers/aichat_ai_helper.rb
+++ dashboard/app/helpers/aichat_ai_helper.rb
@@ -11,7 +11,7 @@ module AichatAiHelper
 
   def self.get_api_model(model_id)
     # For now we just assume it's one of the gemini models if not 'gpt-4o-mini'.
-    model_id == "gpt-4o-mini" ? SharedConstants::AICHAT_MODEL_VERSION : model_id
+    model_id == SharedConstants::AI_CHAT_MODEL_IDS[:CHATGPT] ? SharedConstants::AICHAT_MODEL_VERSION : model_id
   end
 
   def self.format_message_parts(message, encrypted_channel_id, level_name)
@@ -112,7 +112,7 @@ module AichatAiHelper
 
     system_instructions <<  AichatAiClientTypes::TextMessagePart.new(type: 'text', content: new_message['hiddenContext']) if new_message['hiddenContext']
 
-    temperature *= if model_id == "gpt-4o-mini"
+    temperature *= if model_id == SharedConstants::AI_CHAT_MODEL_IDS[:CHATGPT]
                      # If OpenAI:
                      #   We expose a temperature scale of 0.1-1 to users, but OpenAI's API allows a scale of 0-2.
                      #   As of 7/11/25, testing revealed temperatures exceeding 1.5 generate garbage and trigger timeouts/false moderation calls
@@ -176,8 +176,8 @@ module AichatAiHelper
   # Remove after new Vertex implementation is tested on production
   # Create an instance of the appropriate ai-client-derived class based on model id.
   def self.create_ai_client_instance_legacy(client_type, model_id, usage_reporter = nil)
-    # We assume it's one of the gemini models if not 'gpt-4o-mini'.
-    if model_id == "gpt-4o-mini"
+    # We assume it's one of the gemini models if not ChatGPT.
+    if model_id == SharedConstants::AI_CHAT_MODEL_IDS[:CHATGPT]
       return AichatOpenaiResponsesClient.new(CDO.openai_student_learning_api_key, SharedConstants::AICHAT_MODEL_VERSION, usage_reporter)
     else
       # We use separate keys per-client for Gemini so that we can more easily allocate donated credits appropriately.
