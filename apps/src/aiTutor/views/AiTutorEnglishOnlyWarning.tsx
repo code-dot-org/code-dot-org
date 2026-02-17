c709a33cfdd997f9d5c2b350e717a0e95edf21e5
Erin Bond <erin.bond@code.org>
2026-02-17 14:17:15 -0500

    tweak wording to specify this tool

--- apps/src/aiTutor/views/AiTutorEnglishOnlyWarning.tsx
+++ apps/src/aiTutor/views/AiTutorEnglishOnlyWarning.tsx
@@ -21,8 +21,8 @@ const AiTutorEnglishOnlyWarning: React.FunctionComponent = () => {
     <>
       {showEnglishOnlyWarning && (
         <p className={styles.message}>
-          Code.org AI chat tools support English only; use in other languages is
-          not currently recommended.
+          This Code.org AI chat tool only supports English; use in other
+          languages is not currently recommended.
         </p>
       )}
     </>
