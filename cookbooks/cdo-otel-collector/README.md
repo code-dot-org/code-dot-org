f77fe83dc126636fec020474abb43ce2b4a12f9e
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2026-02-18 20:01:59 +0000

    cookbook test

---
+++ cookbooks/cdo-otel-collector/README.md
@@ -0,0 +1,5 @@
+cdo-otel-collector Cookbook
+=====================================
+
+Installs/configures OpenTelemetry Collector, following setup instructions defined at
+https://opentelemetry.io/docs/collector/installation/
---
+++ cookbooks/cdo-otel-collector/metadata.rb
@@ -0,0 +1,10 @@
+name             'cdo-otel-collector'
+maintainer       'Code.org'
+maintainer_email 'will@code.org'
+license          'All rights reserved'
+description      'Installs/Configures cdo-otel-collector'
+long_description File.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
+depends 'ark'
