9f619ed34969b4ed51ce0103a3a31dbcdfd7cfd3
Molly Moen <molly@code.org>
2026-02-18 14:33:56 -0800

    add clear button

---
+++ apps/src/weblab2/debugPanel/DebugPanelRightHeaderButtons.tsx
@@ -0,0 +1,47 @@
+import {Button} from '@code-dot-org/component-library/button';
+import CloseButton from '@code-dot-org/component-library/closeButton';
+import {WithTooltip} from '@code-dot-org/component-library/tooltip';
+import React from 'react';
+
+import {useAppDispatch} from '@cdo/apps/util/reduxHooks';
+
+import {clearRequests} from '../redux/networkRedux';
+import {setDebugPanelOpen} from '../weblab2Redux';
+
+import moduleStyles from './debug-panel-right-header-buttons.module.scss';
+
+const DebugPanelRightHeaderButtons = () => {
+  const dispatch = useAppDispatch();
+  const handleClear = () => {
+    dispatch(clearRequests());
+  };
+
+  return (
+    <div className={moduleStyles.buttonContainer}>
+      <WithTooltip
+        tooltipProps={{
+          text: 'Clear network requests',
+          direction: 'onBottom',
+          tooltipId: 'clear-debug-tooltip',
+          size: 'xs',
+        }}
+      >
+        <Button
+          isIconOnly={true}
+          icon={{iconName: 'eraser'}}
+          onClick={handleClear}
+          aria-label="Clear network requests"
+          size={'xs'}
+          type={'secondary'}
+          color={'gray'}
+        />
+      </WithTooltip>
+      <CloseButton
+        onClick={() => dispatch(setDebugPanelOpen(false))}
+        aria-label="Close debug panel"
+      />
+    </div>
+  );
+};
+
+export default DebugPanelRightHeaderButtons;
