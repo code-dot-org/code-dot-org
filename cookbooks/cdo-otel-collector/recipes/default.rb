fc1443e67e4504dee4f416e54e71f6f50531e36a
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2026-02-18 21:56:57 +0000

    switch to deb

--- cookbooks/cdo-otel-collector/recipes/default.rb
+++ cookbooks/cdo-otel-collector/recipes/default.rb
@@ -5,55 +5,45 @@
 
 include_recipe 'apt'
 
-# Create otel user and group
-group node['cdo-otel-collector']['group'] do
-  action :create
-  system true
-end
-
-user node['cdo-otel-collector']['user'] do
-  group node['cdo-otel-collector']['group']
-  home node['cdo-otel-collector']['home_dir']
-  shell '/bin/false'
-  system true
-  manage_home true
-  action :create
-end
-
-# Create configuration directory
-directory node['cdo-otel-collector']['config_dir'] do
-  owner 'root'
-  group node['cdo-otel-collector']['group']
-  mode '0750'
-  recursive true
-end
-
-# Download and install OpenTelemetry Collector
-ark 'otelcol' do
-  url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v#{node['cdo-otel-collector']['version']}/otelcol_#{node['cdo-otel-collector']['version']}_linux_amd64.tar.gz"
-  version node['cdo-otel-collector']['version']
-  path '/opt'
-  owner 'root'
-  group 'root'
+# Ensure wget is available for downloading the DEB package
+apt_package 'wget' do
   action :install
 end
 
-# Create symlink to binary
-link node['cdo-otel-collector']['binary_path'] do
-  to "/opt/otelcol-#{node['cdo-otel-collector']['version']}/otelcol"
+# Download OpenTelemetry Collector DEB package
+remote_file "#{Chef::Config[:file_cache_path]}/otelcol_#{node['cdo-otel-collector']['version']}_linux_amd64.deb" do
+  source "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v#{node['cdo-otel-collector']['version']}/otelcol_#{node['cdo-otel-collector']['version']}_linux_amd64.deb"
+  mode '0644'
+  action :create
+  not_if "dpkg -l | grep -q '^ii  otelcol '"
+end
+
+# Install OpenTelemetry Collector DEB package
+dpkg_package 'otelcol' do
+  source "#{Chef::Config[:file_cache_path]}/otelcol_#{node['cdo-otel-collector']['version']}_linux_amd64.deb"
+  action :install
+  notifies :restart, 'service[otelcol]', :delayed
+end
+
+# Create configuration directory with proper permissions (if not exists)
+directory node['cdo-otel-collector']['config_dir'] do
+  owner 'root'
+  group 'otelcol'
+  mode '0750'
+  recursive true
 end
 
 # Generate configuration file
 template node['cdo-otel-collector']['config_file'] do
   source 'otel-collector-config.yaml.erb'
   owner 'root'
-  group node['cdo-otel-collector']['group']
+  group 'otelcol'
   mode '0640'
   variables(
     config: node['cdo-otel-collector'],
     environment: node.chef_environment
   )
-  notifies :restart, 'service[otel-collector]', :delayed
+  notifies :restart, 'service[otelcol]', :delayed
 end
 
 # Validate the configuration
@@ -62,31 +52,13 @@ script 'otel-collector-config-validation' do
   interpreter 'bash'
 
   code <<-EOH
-    #{node['cdo-otel-collector']['binary_path']} --config=#{node['cdo-otel-collector']['config_file']} --dry-run
+    /usr/bin/otelcol --config=#{node['cdo-otel-collector']['config_file']} --dry-run
     exit $?
   EOH
 end
 
-# Generate systemd service file
-template '/lib/systemd/system/otel-collector.service' do
-  source 'otel-collector.service.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-  variables(
-    config: node['cdo-otel-collector']
-  )
-  notifies :run, 'execute[systemctl daemon-reload]', :immediately
-  notifies :restart, 'service[otel-collector]', :delayed
-end
-
-execute 'systemctl daemon-reload' do
-  command 'systemctl daemon-reload'
-  action :nothing
-end
-
-# Enable and start the service
-service 'otel-collector' do
+# Enable and start the service (service is created by the DEB package)
+service 'otelcol' do
   supports restart: true, reload: true, status: true
   action [:enable, :start]
   provider Chef::Provider::Service::Systemd
