adc352cf712ce0681f92f879fb96afe8c81c4bd0
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2026-02-19 22:40:55 +0000

    fix reference

--- cookbooks/cdo-otel-collector/attributes/default.rb
+++ cookbooks/cdo-otel-collector/attributes/default.rb
@@ -2,5 +2,5 @@ default['cdo-otel-collector'] = {
   # Version of OpenTelemetry Collector to install
   version: '0.146.0',
   # Configuration file path (package creates user/group/directories)
-  config_file: '/etc/otelcol/config.yaml'
+  config_file: '/etc/otelcol-contrib/config.yaml'
 }
