eacdb18d68c27bae824976de6be9c83ed5c057b9
Mark Barnes <mark.barnes@code.org>
2026-02-19 13:14:45 -0600

    bug fix

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -1181,7 +1181,7 @@ Dashboard::Application.routes.draw do
       collection do
         get :show # GET /ai_lesson_summaries/show?lesson_id=2
         get :ai_lesson_summary_podcast_script, controller: :ai_lesson_summaries, action: :ai_lesson_summary_podcast_script # GET /ai_lesson_summaries/ai_lesson_summary_podcast_script?lesson_id=2
-        get :request_ai_lesson_summaries, controller: :ai_lesson_summaries, action: :request_ai_lesson_summaries # GET ai_lesson_summaries/perform_ai_lesson_summaries_by_unit?unit_id=1 [optional: &lesson_id=1]
+        get :request_ai_lesson_summaries, controller: :ai_lesson_summaries, action: :request_ai_lesson_summaries # GET ai_lesson_summaries/request_ai_lesson_summaries?unit_id=1 [optional: &lesson_id=1]
       end
     end
 
