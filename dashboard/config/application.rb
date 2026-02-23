618c7153383fa19583f448336740f0f1068c26c0
Elijah Hamovitz <elijahhamovitz@gmail.com>
2026-02-23 13:34:29 -0800

    update some config values to their new Rails 7.0 defaults

--- dashboard/config/application.rb
+++ dashboard/config/application.rb
@@ -36,9 +36,17 @@ module Dashboard
     config.load_defaults 6.1
 
     # Manually configure some values to match defaults for the next version of
-    # Rails; see config/initializers/new_framework_defaults_7_0.rb for more.
+    # Rails; see config/initializers/new_framework_defaults_7_0.rb and
+    # https://guides.rubyonrails.org/v7.0/upgrading_ruby_on_rails.html#new-activesupport-cache-serialization-format
+    # for more.
     # TODO infra: remove these values once we're loading defaults for 7.0 above
     config.active_support.disable_to_s_conversion = true
+    config.active_support.cache_format_version = 7.0
+
+    # Convert cookies from old (:marshall) to new (:json) default format
+    # TODO infra: remove this override after 40 days in production (as
+    # determined by CDO.dashboard_session_ttl_days)
+    config.action_dispatch.cookies_serializer = :hybrid
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
