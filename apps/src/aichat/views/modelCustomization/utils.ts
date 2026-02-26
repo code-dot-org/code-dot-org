566f3ea33cba809d56ba116d3151e1ccc125daf4
Sanchit Malhotra <sanchit@code.org>
2026-02-26 18:52:47 +0000

    update docs

--- apps/src/aichat/views/modelCustomization/utils.ts
+++ apps/src/aichat/views/modelCustomization/utils.ts
@@ -16,8 +16,13 @@ export const isDisabled = (visibility: Visibility) =>
 export const isEditable = (visibility: Visibility) =>
   visibility === Visibility.EDITABLE;
 
-// Ensures that the given model ID is part of the available models.
-// If not, returns the first available model ID.
+/**
+ * Ensures that the given model ID is supported and allowed on this level (if applicable).
+ * If not, returns the first supported model ID.
+ * @param modelId model ID to check
+ * @param allowedModelIds list of model IDs allowed on this level
+ * @param supportedModelIds for testing only; override list of model IDs our system supports
+ */
 export const validateModelId = (
   modelId: ModelId,
   allowedModelIds: ModelId[] = [],
