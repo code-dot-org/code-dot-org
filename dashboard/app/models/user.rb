324d1be27cfe42b38a3d43776de2cdbf504a5e38
Liam Frye-Mason <liam.fryemason@code.org>
2026-02-26 11:25:16 -0800

    Small change

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -421,7 +421,10 @@ class User < ApplicationRecord
   end
 
   after_create :migrate_to_multi_auth
-  after_create :create_demo_section, if: :teacher?
+
+  after_create if: :teacher? do
+    DemoSectionUtils.create_demo_section(self)
+  end
 
   after_update if: -> {cap_status? && property_previously_changed?(:us_state)} do
     Services::ChildAccount.remove_compliance(self)
@@ -1194,10 +1197,6 @@ class User < ApplicationRecord
 
   def within_united_states?
     user_geos.first&.country == 'United States'
-  end
-
-  def create_demo_section
-    DemoSectionUtils.create_demo_section(self)
   end
 
   def associate_with_potential_pd_enrollments
