cbd3ac6215516484a032faccd80a6c73aadfbccc
Ben Brooks <ben@code.org>
2026-02-27 13:40:19 -0800

    Include service worker info in console

--- apps/src/weblab2/htmlPreview/useProjectServiceWorker.ts
+++ apps/src/weblab2/htmlPreview/useProjectServiceWorker.ts
@@ -60,6 +60,18 @@ function useProjectServiceWorker(
           serviceWorkerRegistration = registration;
           setServiceWorkerRegistration(registration);
         });
+    } else if (
+      window.location.hostname ===
+      'localtesting.preview.localhost.codeprojects.org'
+    ) {
+      console.error(
+        `
+        Unable to use service workers in your development environment. The easiest way to access this functionality locally by using Chrome, then navigating to: chrome://flags/#unsafely-treat-insecure-origin-as-secure.
+        Once you're there, set the value to the following: http://localhost-studio.code.org:9000,http://localhost-studio.code.org:3000,http://localtesting.preview.localhost.codeprojects.org:9000,http://localtesting.preview.localhost.codeprojects.org:3000
+        More information is available in the README in apps/src/weblab2 directory.
+        `
+      );
+      setServiceWorkerUnavailable(true);
     } else {
       console.error('Service workers are not supported in this browser.');
       setServiceWorkerUnavailable(true);
