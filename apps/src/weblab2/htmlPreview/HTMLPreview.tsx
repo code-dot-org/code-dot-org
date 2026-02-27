a4920e5c80b22aa7e19efef3350a4a9c0734b16d
Molly Moen <molly@code.org>
2026-02-27 12:35:29 -0800

    fix for predict levels

--- apps/src/weblab2/htmlPreview/HTMLPreview.tsx
+++ apps/src/weblab2/htmlPreview/HTMLPreview.tsx
@@ -137,7 +137,8 @@ export const HTMLPreview: React.FC = () => {
   const hasSubmittedPredictResponse = useAppSelector(
     isPredictResponseSubmitted
   );
-  const allowUserScripts = !isPredictLevel || hasSubmittedPredictResponse;
+  const allowUserScripts =
+    !isPredictLevel || hasSubmittedPredictResponse || isStartMode;
   const canNavigateBack = navigationHistoryIndex > 0;
   const canNavigateForward =
     navigationHistoryIndex < navigationHistory.length - 1;
