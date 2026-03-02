2e81fecacc4ebc6538282c2918ef831d2aa55efd
Ben Brooks <ben@code.org>
2026-03-02 12:12:46 -0800

    Support and migrate XML and HTML in CM6 wrapper

--- apps/src/code-studio/initializeCodeMirror6.ts
+++ apps/src/code-studio/initializeCodeMirror6.ts
@@ -1,6 +1,8 @@
+import {html} from '@codemirror/lang-html';
 import {javascript} from '@codemirror/lang-javascript';
 import {json} from '@codemirror/lang-json';
 import {markdown} from '@codemirror/lang-markdown';
+import {xml} from '@codemirror/lang-xml';
 import {EditorState, Extension} from '@codemirror/state';
 import {EditorView, ViewUpdate} from '@codemirror/view';
 
@@ -23,12 +25,14 @@ interface Options {
   callback?: (editor: CodeMirrorLegacyAdapter, update: ViewUpdate) => void;
 }
 
-type EditorMode = 'javascript' | 'json' | 'markdown';
+type EditorMode = 'javascript' | 'json' | 'markdown' | 'xml' | 'html';
 
 const languageExtensionMap: Record<EditorMode, Extension> = {
   javascript: javascript(),
   json: json(),
   markdown: markdown(),
+  xml: xml(),
+  html: html(),
 };
 
 function getLanguageExtension(mode: EditorMode): Extension {
