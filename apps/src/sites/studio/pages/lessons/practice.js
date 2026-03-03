240e0473e6664c2935e46c6a2e69a023b7968a25
Erin Bond <erin.bond@code.org>
2026-03-03 14:44:41 -0500

    import ordering

--- apps/src/sites/studio/pages/lessons/practice.js
+++ apps/src/sites/studio/pages/lessons/practice.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 
+import {LessonPractice} from '@cdo/apps/aiTutor/views/lessonPractice/LessonPractice';
 import {getStore} from '@cdo/apps/redux';
 import {createReactRoot} from '@cdo/apps/util/createReactRoot';
 import getScriptData from '@cdo/apps/util/getScriptData';
-import {LessonPractice} from '@cdo/apps/aiTutor/views/lessonPractice/LessonPractice';
 
 $(document).ready(() => {
   const lessonPracticeData = getScriptData('lessonPracticeData');
   console.log('lessonPracticeData', lessonPracticeData);
   createReactRoot(
     <Provider store={getStore()}>
-      <LessonPractice
-        lessonName={lessonPracticeData.lessonName}
-        lessonSummary={lessonPracticeData.lessonSummary}
-        vocabulary={lessonPracticeData.vocabulary}
-      />
+      <LessonPractice lessonPracticeData={lessonPracticeData} />
     </Provider>,
     document.getElementById('lesson-practice-container')
   );
