2cd5221b602e48df8649274ba2485920d6b5f4ff
Alice Fisher <alice.fisher@code.org>
2026-03-03 16:41:22 +0000

    Revert "simplify getModel"
    This reverts commit a38ba2c67f32e51cc406c6b24cbf272c2956a1d5.

--- apps/src/aichat/api/client/helpers/modelHelpers.ts
+++ apps/src/aichat/api/client/helpers/modelHelpers.ts
@@ -1,11 +1,33 @@
+import {createGoogleGenerativeAI} from '@ai-sdk/google';
+import {createOpenAI} from '@ai-sdk/openai';
+import {type LanguageModel} from 'ai';
+
 import {ValueOf} from '@cdo/apps/types/utils';
 import {AiChatModelIds} from '@cdo/generated-scripts/sharedConstants';
 
-const supportedModelIds = new Set<string>(Object.values(AiChatModelIds));
+const googleProvider = createGoogleGenerativeAI({
+  apiKey: '',
+});
 
-export function getModel(modelId: ValueOf<typeof AiChatModelIds>): string {
-  if (!supportedModelIds.has(modelId)) {
+const openAiProvider = createOpenAI({
+  apiKey: '',
+});
+
+const modelMap: {
+  [key in ValueOf<typeof AiChatModelIds>]?: LanguageModel;
+} = {
+  [AiChatModelIds.GEMINI_2_5_FLASH_IMAGE]: googleProvider(
+    AiChatModelIds.GEMINI_2_5_FLASH_IMAGE
+  ),
+  [AiChatModelIds.GEMINI_2_5_FLASH]: googleProvider(
+    AiChatModelIds.GEMINI_2_5_FLASH
+  ),
+  [AiChatModelIds.CHATGPT]: openAiProvider(AiChatModelIds.CHATGPT),
+};
+
+export function getModel(modelId: ValueOf<typeof AiChatModelIds>) {
+  if (!modelMap[modelId]) {
     throw new Error('Unsupported model ID: ' + modelId);
   }
-  return modelId;
+  return modelMap[modelId]!;
 }
