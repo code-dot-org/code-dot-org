0704a06b62b39e973bfc744bf816eb6c758ca3c1
Mark Barnes <mark.barnes@code.org>
2026-03-03 08:11:11 -0600

    bug fix and style update (#71097)

--- apps/src/sites/studio/pages/teacher_dashboard/show.js
+++ apps/src/sites/studio/pages/teacher_dashboard/show.js
@@ -15,6 +15,7 @@ import unitSelection from '@cdo/apps/redux/unitSelectionRedux';
 import currentUser, {
   setShowAITALessonSummary,
   setHasCompletedPersonalizationQuiz,
+  setShowAITAPodcasts,
 } from '@cdo/apps/templates/currentUserRedux';
 import manageStudents from '@cdo/apps/templates/manageStudents/manageStudentsRedux';
 import sectionAssessments from '@cdo/apps/templates/sectionAssessments/sectionAssessmentsRedux';
@@ -40,6 +41,7 @@ const {
   sections,
   localeCode,
   showAITALessonSummary,
+  showAITAPodcasts,
   hasCompletedPersonalizationQuiz,
   sectionOrder,
   providers,
@@ -67,6 +69,7 @@ $(document).ready(function () {
   const store = getStore();
   if (showAITALessonSummary) {
     store.dispatch(setShowAITALessonSummary(true));
+    store.dispatch(setShowAITAPodcasts(showAITAPodcasts));
     store.dispatch(
       setHasCompletedPersonalizationQuiz(hasCompletedPersonalizationQuiz)
     );
