a40b7e17534762939dc72c47ee9fc89cc5c811bd
Darin Webb <darin@code.org>
2026-03-04 13:39:10 -0600

    fix bugs in gen_ai deployment code

--- aws/cloudformation/standalone/gen_ai_curriculum/deployment/config.rb
+++ aws/cloudformation/standalone/gen_ai_curriculum/deployment/config.rb
@@ -1,5 +1,5 @@
 require "ostruct"
-require_relative '../../../../lib/cdo/shared_constants'
+require_relative '../../../../../lib/cdo/shared_constants'
 
 module Config
   # Configuration for each endpoint used in the Gen AI Curriculum. Required properties:
