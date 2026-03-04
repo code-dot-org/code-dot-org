af4c755dd55a4ea1fbc3470fb29bafbfae2a279d
Erin Bond <erin.bond@code.org>
2026-03-04 11:28:48 -0500

    i fixed it :)

--- apps/src/sites/studio/pages/lessons/practice.js
+++ apps/src/sites/studio/pages/lessons/practice.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 
 import {LessonPractice} from '@cdo/apps/aiTutor/views/lessonPractice/LessonPractice';
-
-import {Provider} from 'react-redux';
 import {getStore} from '@cdo/apps/redux';
 import {createReactRoot} from '@cdo/apps/util/createReactRoot';
 import getScriptData from '@cdo/apps/util/getScriptData';
 
 $(document).ready(() => {
   const lessonPracticeData = getScriptData('lessonPracticeData');
-  console.log('lessonPracticeData', lessonPracticeData);
   createReactRoot(
     <Provider store={getStore()}>
       <LessonPractice lessonPracticeData={lessonPracticeData} />
