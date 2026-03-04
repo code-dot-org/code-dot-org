3d2ab93eb881b4b6f3c03a633726fb48112a6338
Hannah <hannahbergam@gmail.com>
2026-03-04 11:37:23 -0800

    removing svg version attributes

--- apps/src/templates/VisualizationOverlay.jsx
+++ apps/src/templates/VisualizationOverlay.jsx
@@ -129,7 +129,6 @@ export class VisualizationOverlay extends React.Component {
         ref="root"
         id="visualizationOverlay"
         className={this.props.className}
-        version="1.1"
         baseProfile="full"
         width={this.props.width}
         height={this.props.height}
