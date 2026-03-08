2303a97760366e7768a972d9714dba367b8abd17
Seth Nickell <snickell@gmail.com>
2026-03-08 02:13:02 -1000

    Remove k8s/tofu directory

--- k8s/tofu/codeai-k8s/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "seth-tmp-opentofu-state"
-    key          = "k8s/tofu/codeai-k8s/tofu.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-  }
-}
