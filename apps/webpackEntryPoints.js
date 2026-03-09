e5fbc0d2b6989e4208f99be3e80936d654558643
David Bailey <davidsbailey@users.noreply.github.com>
2026-03-09 11:51:40 -0700

    remove pegasus content from apps build

--- apps/webpackEntryPoints.js
+++ apps/webpackEntryPoints.js
@@ -200,41 +200,6 @@ const INTERNAL_ENTRIES = {
 };
 
 // prettier-ignore
-const PEGASUS_ENTRIES = {
-  // code.org
-  'code.org/public/dance': './src/sites/code.org/pages/public/dance.js',
-  'code.org/public/helloworld': './src/sites/code.org/pages/public/helloworld.js',
-  'code.org/public/transformersone': './src/sites/code.org/pages/public/transformersone.js',
-  'code.org/public/teacher-dashboard/index': './src/sites/code.org/pages/public/teacher-dashboard/index.js',
-  'code.org/views/admins_email_signup_form': './src/sites/code.org/pages/views/admins_email_signup_form.js',
-  'code.org/views/theme_common_head_after': './src/sites/code.org/pages/views/theme_common_head_after.js',
-  'code.org/views/theme_google_analytics': './src/sites/code.org/pages/views/theme_google_analytics.js',
-  'code.org/views/job_board': './src/sites/code.org/pages/views/job_board.js',
-  'code.org/views/analytics_event_log_helper': './src/sites/code.org/pages/views/analytics_event_log_helper.js',
-  'code.org/views/swiper': './src/sites/code.org/pages/views/swiper.js',
-  'code.org/views/swiper_page_csd': './src/sites/code.org/pages/views/swiper_page_csd.js',
-  'code.org/views/swiper_page_self_paced_pl': './src/sites/code.org/pages/views/swiper_page_self_paced_pl.js',
-  'code.org/views/swiper_page_ai': './src/sites/code.org/pages/views/swiper_page_ai.js',
-  'code.org/views/swiper_page_tools': './src/sites/code.org/pages/views/swiper_page_tools.js',
-  'code.org/views/swiper_page_applab': './src/sites/code.org/pages/views/swiper_page_applab.js',
-  'code.org/views/swiper_page_videos': './src/sites/code.org/pages/views/swiper_page_videos.js',
-  'code.org/views/swiper_farsi_page_videos': './src/sites/code.org/pages/views/swiper_farsi_page_videos.js',
-  'code.org/views/swiper_page_minecraft': './src/sites/code.org/pages/views/swiper_page_minecraft.js',
-  'code.org/views/swiper_page_music_lab': './src/sites/code.org/pages/views/swiper_page_music_lab.js',
-  'code.org/views/swiper_page_promote': './src/sites/code.org/pages/views/swiper_page_promote.js',
-  'code.org/views/tabs_section': './src/sites/code.org/pages/views/tabs_section.js',
-  'code.org/views/classy_embed_form': './src/sites/code.org/pages/views/classy_embed_form.js',
-  'code.org/public/poetry': './src/sites/code.org/pages/public/poetry.js',
-  'code.org/public/cms-demo': './src/sites/code.org/pages/public/cms-demo.js',
-
-  // hourofcode.com
-  'hourofcode.com/views/theme_common_head_after': './src/sites/hourofcode.com/pages/views/theme_common_head_after.js',
-  'hourofcode.com/views/theme_google_analytics': './src/sites/hourofcode.com/pages/views/theme_google_analytics.js',
-  'hourofcode.com/views/analytics_event_log_helper': './src/sites/hourofcode.com/pages/views/analytics_event_log_helper.js',
-  'hourofcode.com/views/swiper_page_resources': './src/sites/hourofcode.com/pages/views/swiper_page_resources.js',
-};
-
-// prettier-ignore
 const PROFESSIONAL_DEVELOPMENT_ENTRIES = {
   'pd/_jotform_loader': './src/sites/studio/pages/pd/_jotform_loader.js',
   'pd/_jotform_embed': './src/sites/studio/pages/pd/_jotform_embed.js',
@@ -338,7 +303,6 @@ module.exports = {
   appsEntriesFor,
   CODE_STUDIO_ENTRIES,
   INTERNAL_ENTRIES,
-  PEGASUS_ENTRIES,
   PROFESSIONAL_DEVELOPMENT_ENTRIES,
   SHARED_ENTRIES,
   OTHER_ENTRIES,
