cc54e86524263406dbd78f5035d48330a3ce258c
Molly Moen <molly@code.org>
2026-03-09 14:48:20 -0700

    add new file

---
+++ apps/src/lab2/views/components/Instructions/ResourcePanel/validationTourShepherdSteps.ts
@@ -0,0 +1,50 @@
+import {type StepOptions, type Tour} from 'shepherd.js';
+
+import lab2I18n from '@cdo/apps/lab2/locale';
+import {
+  backButton,
+  doneButton,
+  nextButton,
+} from '@cdo/apps/sharedComponents/productTour/productTourHelpers';
+
+import {
+  resourcePanelTabValidationElementId,
+  resourcePanelValidateButtonElementId,
+  resourcePanelValidationTableElementId,
+} from './constants';
+
+export const createValidationTourSteps = (tour: Tour): StepOptions[] => [
+  {
+    id: 'validation-tab',
+    attachTo: {
+      element: `#${resourcePanelTabValidationElementId}`,
+      on: 'right',
+    },
+    title: lab2I18n.validationTour_tabTitle(),
+    text: lab2I18n.validationTour_tabText(),
+    buttons: [nextButton(tour)],
+    scrollTo: false,
+  },
+  {
+    id: 'validate-button',
+    attachTo: {
+      element: `#${resourcePanelValidateButtonElementId}`,
+      on: 'right',
+    },
+    title: lab2I18n.validationTour_buttonTitle(),
+    text: lab2I18n.validationTour_buttonText(),
+    buttons: [backButton(tour), nextButton(tour)],
+    scrollTo: false,
+  },
+  {
+    id: 'validation-results',
+    attachTo: {
+      element: `#${resourcePanelValidationTableElementId}`,
+      on: 'bottom',
+    },
+    title: lab2I18n.validationTour_resultsTitle(),
+    text: lab2I18n.validationTour_resultsText(),
+    buttons: [backButton(tour), doneButton(tour)],
+    scrollTo: false,
+  },
+];
