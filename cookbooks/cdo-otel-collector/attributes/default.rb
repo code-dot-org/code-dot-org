108ddcb20cb07d9af89e7e57dffa722da00a6f2c
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2026-03-11 11:51:03 -0700

    feat(otel): convert to otel contrib
    The Datadog OpenTelemtry Collector (DDOT) is very lightweight and focused on just datadog. However, we need a couple more features that aren't provided by DDOT out of the box.
    
    This commit converts DDOT to OpenTelemetry Contrib Collector (which includes Datadog) switching us to the "vanilla" OpenTelemetry collector implementaiton.

--- cookbooks/cdo-otel-collector/attributes/default.rb
+++ cookbooks/cdo-otel-collector/attributes/default.rb
@@ -1,7 +1,11 @@
 default['cdo-otel-collector'] = {
-  # Set to false to disable DataDog and stop/disable all related services.
+  # Set to false to disable the OTel Collector and stop/disable all related services.
   'enabled' => false,
-  # US1 is optimized for AWS operations.
+  # DataDog site for the exporter (US1 is optimized for AWS operations).
   'site' => 'datadoghq.com',
-  'logs_enabled' => true
+  # OTel Contrib collector version to install.
+  # When upgrading, update otelcol_deb_sha256 to match the new release's checksums.txt.
+  'otelcol_version' => '0.147.0',
+  # SHA256 of otelcol-contrib_{version}_linux_amd64.deb from the release's checksums.txt.
+  'otelcol_deb_sha256' => 'c4683a3149632a867a257c02187ec88080892ea5e18d4c930a0540ed1db31518'
 }
