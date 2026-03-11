2ef6b0c4572654e6cd735f17d2e48fbd043bdf6b
Seth Nickell <snickell@gmail.com>
2026-03-11 03:46:36 -1000

    DRONE SHOULD FAIL, missing a secret override for testing

--- dashboard/test/lib/aws_creds_must_be_optional_test.rb
+++ dashboard/test/lib/aws_creds_must_be_optional_test.rb
@@ -8,29 +8,40 @@ class AWSCredsMustBeOptionalTest < ActiveSupport::TestCase
   # that is not invoked when all the ruby code is loaded.
   test 'dashboard boots in RAILS_ENV=development without requiring !Secret AWS credentials' do
     ENV_VARS = {
+      'RAILS_ENV' => 'development',
+      'DISABLE_SPRING' => '1',
+
       # This is a hack, but it effectively disables access to existing AWS creds:
       'AWS_PROFILE' => 'nope',
-      'DISABLE_SPRING' => '1',
-      'RAILS_ENV' => 'development',
+      'AWS_EC2_METADATA_DISABLED' => 'true', # defend against IMDS
+      'AWS_ACCESS_KEY_ID' => '',
+      'AWS_SECRET_ACCESS_KEY' => '',
+      'AWS_SESSION_TOKEN' => '',
     }
 
     # Please do not add to this list. Instead, seek to remove from the list.
     # Then you will see that it is not the list that grows, it is your mind.
-    LOCALOVERRIDE_LEGACY_SECRETS = %w(
+    LEGACY_SECRETS_THAT_ALREADY_BREAK_THINGS = %w(
       slack_bot_token
       openai_lesson_summaries_api_key
       openai_student_learning_api_key
       langfuse_secret_key
       langfuse_public_key
       openai_measures_of_learning_api_key
-    ).index_with {'localoverride'}.transform_keys {|key| "CDO_#{key}"}
+    ).map {|secret| "CDO_#{secret}"}.index_with('')
 
-    # Start a new `rails runner` process and make sure it boots
+    # Start a new `rails runner` process and make sure it boots w/o AWS access
     stdout, stderr, status = Open3.capture3(
-      {**ENV_VARS, **LOCALOVERRIDE_LEGACY_SECRETS},
+      {**ENV_VARS, **LEGACY_SECRETS_THAT_ALREADY_BREAK_THINGS},
       'bin/rails',
       'runner',
-      'puts :rails_booted_ok',
+      <<~RUBY,
+        # Getting here means nothing if we hadn't correctly disabled AWS access for this process
+        we_have_aws_access = Aws::STS::Client.new.get_caller_identity rescue false
+        raise "Test invalidated: we have AWS access here and should not" if we_have_aws_access
+
+        puts :rails_booted_without_aws
+      RUBY
       chdir: Rails.root.to_s
     )
 
@@ -47,6 +58,6 @@ class AWSCredsMustBeOptionalTest < ActiveSupport::TestCase
       #{stderr}
     MSG
 
-    assert_includes stdout, 'rails_booted_ok'
+    assert_includes stdout, 'rails_booted_without_aws'
   end
 end
