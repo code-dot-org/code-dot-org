ef18b6dce867d78f8bb9b8e59f1f769d8f30421a
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2026-03-11 13:11:33 -0700

    AI Chat image moderation follow-ups (#71243)
    * AI Chat image moderation follow-ups
    
    * remove spaces
    
    * restore separate appName param

--- apps/src/lab2/utils/moderateImage.ts
+++ apps/src/lab2/utils/moderateImage.ts
@@ -6,11 +6,21 @@ import {WEBLAB2_IMAGE_FILE_TYPES} from '@cdo/apps/weblab2/constants';
 
 const LAB2_LABS_MODERATE_IMAGES = ['weblab2', 'aichat'];
 
+interface AnalyticsData {
+  uploaderType?: 'Lab2FileUploader' | 'n/a';
+  moderateEvent?: string;
+  flaggedEvent?: string;
+}
+
 export const moderateImage = async (
   file: File,
   ext: string,
   appName: string,
-  isModelOutputImage?: boolean
+  {
+    uploaderType = 'n/a',
+    moderateEvent = EVENTS.MODERATE_CUSTOM_IMAGE,
+    flaggedEvent = EVENTS.FLAGGED_CUSTOM_IMAGE,
+  }: AnalyticsData
 ): Promise<'ok' | 'flagged' | 'skipped'> => {
   if (
     !LAB2_LABS_MODERATE_IMAGES.includes(appName ?? '') ||
@@ -19,51 +29,24 @@ export const moderateImage = async (
     return 'skipped';
   }
   const metricsReporter = Lab2Registry.getInstance().getMetricsReporter();
-  const uploaderType = isModelOutputImage ? 'n/a' : 'Lab2FileUploader';
-  metricsReporter.incrementCounter('ModerateCustomImage.Attempt', [
-    {name: 'UploaderType', value: uploaderType},
-  ]);
-  const moderateImageStatsigEvent = isModelOutputImage
-    ? EVENTS.MODERATE_MODEL_OUTPUT_IMAGE_AZURE
-    : EVENTS.MODERATE_CUSTOM_IMAGE;
-  sendLab2AnalyticsEvent(
-    moderateImageStatsigEvent,
-    !isModelOutputImage ? {UploaderType: 'Lab2 File Uploader'} : {}
-  );
+  const dimensions = [{name: 'UploaderType', value: uploaderType}];
+  metricsReporter.incrementCounter('ModerateCustomImage.Attempt', dimensions);
+  sendLab2AnalyticsEvent(moderateEvent, {UploaderType: uploaderType});
   try {
     const response = await HttpClient.post(`/v3/images/moderate`, file, true, {
       'Content-Type': file.type || 'application/octet-stream',
     });
-    if (!response.ok) {
-      metricsReporter.logError('Error with image moderation: HTTP error');
-      metricsReporter.incrementCounter('ModerateCustomImage.Error', [
-        {name: 'UploaderType', value: uploaderType},
-      ]);
-      return 'skipped';
-    }
     const json = await response.json();
-    metricsReporter.incrementCounter('ModerateCustomImage.Success', [
-      {name: 'UploaderType', value: uploaderType},
-    ]);
+    metricsReporter.incrementCounter('ModerateCustomImage.Success', dimensions);
     if (json?.rating === 'everyone' || json?.rating === 'unknown') {
       return 'ok';
     }
-    metricsReporter.incrementCounter('ModerateCustomImage.Flagged', [
-      {name: 'UploaderType', value: uploaderType},
-    ]);
-    const flaggedImageStatsigEvent = isModelOutputImage
-      ? EVENTS.FLAGGED_MODEL_OUTPUT_IMAGE_AZURE
-      : EVENTS.FLAGGED_CUSTOM_IMAGE;
-    sendLab2AnalyticsEvent(
-      flaggedImageStatsigEvent,
-      !isModelOutputImage ? {UploaderType: 'Lab2 File Uploader'} : {}
-    );
+    metricsReporter.incrementCounter('ModerateCustomImage.Flagged', dimensions);
+    sendLab2AnalyticsEvent(flaggedEvent, {UploaderType: uploaderType});
     return 'flagged';
   } catch (error) {
     metricsReporter.logError('Error with image moderation: ' + error);
-    metricsReporter.incrementCounter('ModerateCustomImage.Error', [
-      {name: 'UploaderType', value: uploaderType},
-    ]);
+    metricsReporter.incrementCounter('ModerateCustomImage.Error', dimensions);
     return 'skipped';
   }
 };
