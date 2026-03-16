4010fd16f13732984bec9204898c1f2ecf33a94a
Hannah <hannahbergam@gmail.com>
2026-03-16 16:37:03 -0700

    adding tests

--- apps/test/unit/craft/craftTest.js
+++ apps/test/unit/craft/craftTest.js
@@ -1,15 +1,65 @@
 import sinon from 'sinon'; // eslint-disable-line no-restricted-imports
 
 import Craft from '@cdo/apps/craft/code-connection/craft';
+import SimpleCraft from '@cdo/apps/craft/simple/craft';
 import {
-  getStore,
   registerReducers,
   stubRedux,
   restoreRedux,
+  getStore,
 } from '@cdo/apps/redux';
 import pageConstants from '@cdo/apps/redux/pageConstants';
+import {singleton as studioApp} from '@cdo/apps/StudioApp';
+import currentUser from '@cdo/apps/templates/currentUserRedux';
 
+import {TestResults} from '../../../src/constants';
 import {assert} from '../../util/reconfiguredChai'; // eslint-disable-line no-restricted-imports
+
+jest.mock('@cdo/apps/blockly/utils', () => ({
+  getCode: jest.fn().mockReturnValue(''),
+  getCodeBlocks: jest.fn().mockReturnValue([]),
+}));
+
+describe('Test Craft on freeplay levels', () => {
+  beforeEach(() => {
+    stubRedux();
+    registerReducers({currentUser});
+    SimpleCraft.initialConfig = {level: {freePlay: true, id: 'test-level'}};
+    SimpleCraft.gameController = {getScreenshot: jest.fn()};
+    global.Blockly = {};
+    jest
+      .spyOn(studioApp(), 'getTestResults')
+      .mockReturnValue(TestResults.ALL_PASS);
+    jest.spyOn(studioApp(), 'report').mockImplementation();
+    jest.spyOn(studioApp(), 'displayFeedback').mockImplementation();
+  });
+
+  afterEach(() => {
+    restoreRedux();
+    jest.restoreAllMocks();
+    SimpleCraft.initialConfig = undefined;
+    delete global.Blockly;
+  });
+
+  it('calls studioApp().report() to save progress', () => {
+    SimpleCraft.reportResult(true, true);
+    expect(studioApp().report).toHaveBeenCalledTimes(1);
+  });
+
+  it('does not show the completed level dialog when suppressDialog is true', () => {
+    SimpleCraft.reportResult(true, true);
+    const {onComplete} = studioApp().report.mock.calls[0][0];
+    onComplete({});
+    expect(studioApp().displayFeedback).not.toHaveBeenCalled();
+  });
+
+  it('shows the completed level dialog when suppressDialog is false', () => {
+    SimpleCraft.reportResult(true, false);
+    const {onComplete} = studioApp().report.mock.calls[0][0];
+    onComplete({});
+    expect(studioApp().displayFeedback).toHaveBeenCalledTimes(1);
+  });
+});
 
 describe('Craft', () => {
   beforeAll(() => sinon.stub(Craft, 'render'));
