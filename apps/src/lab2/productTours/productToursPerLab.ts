8a1d834f4c5a06ee76c2ba38910bbbc0932392b7
Molly Moen <molly@code.org>
2026-03-16 13:35:49 -0700

    create enum

---
+++ apps/src/lab2/productTours/productToursPerLab.ts
@@ -0,0 +1,5 @@
+export enum ProductTour {
+  ResourcePanelOnboarding = 'resource_panel_onboarding',
+  ResourcePanelValidation = 'resource_panel_validation',
+  SketchlabIntro = 'sketchlab_intro',
+}
