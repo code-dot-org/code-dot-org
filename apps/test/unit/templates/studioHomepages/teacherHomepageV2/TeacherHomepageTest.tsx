b65fd7e0b11722aedee6fc9e16064aa03811355b
Erin Bond <erin.bond@code.org>
2026-03-17 13:36:09 -0400

    Update TeacherHomepageTest.tsx

--- apps/test/unit/templates/studioHomepages/teacherHomepageV2/TeacherHomepageTest.tsx
+++ apps/test/unit/templates/studioHomepages/teacherHomepageV2/TeacherHomepageTest.tsx
@@ -223,6 +223,7 @@ describe('TeacherHomepage', () => {
 
     await screen.findByText('Picture password', {}, {timeout: 5000});
     screen.getByRole('button', {name: 'Cancel'});
+    // This is a hail mary quick flakiness mitigation attempt. If we see continued failures, we should look for underlying causes.
   }, 25000);
 
   it('teaching/archived toggle', async () => {
