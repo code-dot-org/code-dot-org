e9c7308a3eb004f7b53636f8a552b1d40ce6fe53
Erin Bond <erin.bond@code.org>
2026-03-17 15:06:37 -0400

    Update langfuse_helper.rb
    remove unused TA add_dataset_item method

--- dashboard/app/helpers/langfuse_helper.rb
+++ dashboard/app/helpers/langfuse_helper.rb
@@ -21,16 +21,6 @@ module LangfuseHelper
     end
   end
 
-  def self.ta_add_dataset_item(dataset_item)
-    response = ta_client.add_dataset_item(dataset_item)
-
-    if response.code == 200
-      {status: :ok, json: JSON.parse(response.body)}
-    else
-      {status: response.code, json: {error: response.body}}
-    end
-  end
-
   def self.tutor_client
     LangfuseClientHelper::Client.new(CDO.tutor_langfuse_secret_key, CDO.tutor_langfuse_public_key)
   end
