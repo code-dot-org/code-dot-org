2c81b8c2367c87b2bf47c5f4f231f63b546fa21a
denyslevada <levada.denys@gmail.com>
2026-03-17 09:01:34 +0200

    fix: update icon references in SendLesson and ItemType components

--- apps/src/templates/progress/SendLesson.jsx
+++ apps/src/templates/progress/SendLesson.jsx
@@ -37,7 +37,7 @@ export default class SendLesson extends React.Component {
         <Button
           onClick={this.openDialog}
           text={i18n.sendLessonButton()}
-          icon="share-square-o"
+          icon="share-square"
           color={Button.ButtonColor.gray}
           style={this.props.buttonStyle}
         />
