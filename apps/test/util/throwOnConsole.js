881fc61677584be4a9ff70b4b24faea0c7a490c6
Ben Brooks <ben@code.org>
2026-03-18 10:44:41 -0700

    Add to / simplify error list, add comment

--- apps/test/util/throwOnConsole.js
+++ apps/test/util/throwOnConsole.js
@@ -7,11 +7,16 @@
  * Thank you!
  */
 
+// These are warnings that React surfaces in React 18 as console.error messages.
+// Please do not add to this list!
 const ERROR_ALLOW_LIST = [
-  "Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot",
-  'uses the legacy childContextTypes API which is no longer supported and will be removed in the next major release. Use React.createContext() instead',
-  'Support for defaultProps will be removed from function components in a future major release. Use JavaScript default parameters instead',
-  'findDOMNode is deprecated and will be removed in the next major release. Instead, add a ref directly to the element you want to reference. Learn more about using refs safely here: https://reactjs.org/link/strict-mode-find-node',
+  'Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead.',
+  'uses the legacy childContextTypes API which is no longer supported and will be removed in the next major release.',
+  'uses the legacy contextTypes API which is no longer supported and will be removed in the next major release.',
+  'Support for defaultProps will be removed from function components in a future major release.',
+  'Support for string refs will be removed in a future major release.',
+  'findDOMNode is deprecated and will be removed in the next major release.',
+  '`ReactDOMTestUtils.act` is deprecated in favor of `React.act`.',
 ];
 
 /**
