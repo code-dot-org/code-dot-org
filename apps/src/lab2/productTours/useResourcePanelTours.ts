47aa0a21575dc1d49e0d4c61d076f7f5adc0718d
Molly Moen <molly@code.org>
2026-03-19 16:15:23 -0700

    Remove outdated code and locales for lab2 introjs flows (#71498)
    * delete tour helpers and rename files
    
    * additional rename change
    
    * remove lab2 i18n strings
    
    * remove sketch lab locale
    
    * update test
    
    * fix indent
    
    * fix other linter issue

--- apps/src/lab2/productTours/useResourcePanelTours.ts
+++ apps/src/lab2/productTours/useResourcePanelTours.ts
@@ -14,11 +14,11 @@ import {EVENTS} from '@cdo/apps/metrics/AnalyticsConstants';
 import useStartTourWhenAvailable from '@cdo/apps/sharedComponents/productTour/useStartTourWhenAvailable';
 import {tryGetLocalStorage} from '@cdo/apps/utils';
 
-import {createOnboardingTourSteps} from './onboardingTourShepherdSteps';
+import {createOnboardingTourSteps} from './onboardingTourSteps';
 import {ProductTour, isTourEnabledOnLevel} from './productToursPerLab';
-import {createValidationTourSteps} from './validationTourShepherdSteps';
+import {createValidationTourSteps} from './validationTourSteps';
 
-interface UseResourcePanelShepherdToursParams {
+interface UseResourcePanelToursParams {
   appName: string | undefined;
   productToursForLevel: string[] | undefined;
   isStandaloneCollapsed?: boolean;
@@ -38,13 +38,13 @@ const onTourCancel = (flowName: string) => (stepIndex: number) =>
     step: stepIndex.toString(),
   });
 
-const useResourcePanelShepherdTours = ({
+const useResourcePanelTours = ({
   appName,
   productToursForLevel,
   isStandaloneCollapsed,
   hasValidationConditions,
   validationSettings,
-}: UseResourcePanelShepherdToursParams) => {
+}: UseResourcePanelToursParams) => {
   // We track level load state to avoid starting tours while the level is still loading.
   // This can cause multiple tours to show up if we load one for the previous level and
   // then one for the new level.
@@ -131,4 +131,4 @@ const useResourcePanelShepherdTours = ({
   useStartTourWhenAvailable(validationTour);
 };
 
-export default useResourcePanelShepherdTours;
+export default useResourcePanelTours;
