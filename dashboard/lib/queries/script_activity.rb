3295eecf655475210ee1aeb91469f55e77eacd13
Carl Reiner <carl.reiner@code.org>
2026-03-19 09:37:16 -0700

    optimize working_on_user_scripts

--- dashboard/lib/queries/script_activity.rb
+++ dashboard/lib/queries/script_activity.rb
@@ -24,13 +24,9 @@ class Queries::ScriptActivity
   #
   # return [UserScript]
   def self.working_on_user_scripts(user)
-    user.user_scripts.where(user_scripts: {completed_at: nil}).reject do |user_script|
-      user_script.script.nil?
-    rescue ActiveRecord::RecordNotFound
-      # Getting user_script.script can raise if the script does not exist.
-      # In that case we should also reject this user_script.
-      true
-    end
+    user.user_scripts.
+      joins(:script).
+      where(user_scripts: {completed_at: nil})
   end
 
   # Retrieve all UserScripts for scripts this user has completed
