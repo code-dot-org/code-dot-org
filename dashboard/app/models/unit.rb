537d218f6719cbc9f4aea03c239cd98be1cd7dc2
Dave Bailey <davidsbailey@users.noreply.github.com>
2026-03-24 13:44:25 -0700

    Revert "Revert "Migrate UNIT_AND_LEVEL_TO_LESSON_S3_NAME to ai_rubric_s3_conf…" (#71285)
    This reverts commit daa3981e9f019a7392390150149803e8a7b2c2c0.

--- dashboard/app/models/unit.rb
+++ dashboard/app/models/unit.rb
@@ -278,6 +278,7 @@ class Unit < ApplicationRecord
     content_area
     topic_tags
     enable_blockly_keyboard_navigation
+    ai_rubric_s3_config
   )
 
   def self.hoc_2014_unit
@@ -1318,6 +1319,7 @@ class Unit < ApplicationRecord
         scriptPath: unit_path,
         showCalendar: is_migrated ? show_calendar : false, #prevent calendar from showing for non-migrated units for now
         weeklyInstructionalMinutes: weekly_instructional_minutes,
+        aiRubricS3Config: ai_rubric_s3_config,
         includeStudentLessonPlans: is_migrated ? include_student_lesson_plans : false,
         useLegacyLessonPlans: is_migrated && use_legacy_lesson_plans,
         courseVersionId: unit_group_unit&.cached_unit_group&.course_version&.id,
@@ -1613,7 +1615,8 @@ class Unit < ApplicationRecord
       :curriculum_umbrella,
       :weekly_instructional_minutes,
       :content_area,
-      :topic_tags
+      :topic_tags,
+      :ai_rubric_s3_config
     ]
     boolean_keys = [
       :has_unnumbered_lessons,
