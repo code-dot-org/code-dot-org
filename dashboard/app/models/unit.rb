d76a5825110236ea854383f33df28caead019101
Dave Bailey <davidsbailey@users.noreply.github.com>
2026-03-24 12:15:46 -0700

    disable unit caching specifically in rake tasks (#71582)

--- dashboard/app/models/unit.rb
+++ dashboard/app/models/unit.rb
@@ -351,6 +351,12 @@ class Unit < ApplicationRecord
   def self.should_cache?
     return false if Rails.application.config.levelbuilder_mode
     return false unless Rails.application.config.cache_classes
+    # Unit caching is designed for use within the running web application, but we
+    # can't easily limit it to that scenario until the CDO.running_web_application?
+    # helper is fixed (see https://github.com/code-dot-org/code-dot-org/pull/71525).
+    # In the interim, make sure we skip caching during rake seed tasks, which
+    # sometimes break when caching is enabled.
+    return false if File.basename($0) == 'rake'
     return false if ENV['UNIT_TEST'] || ENV['CI']
     true
   end
