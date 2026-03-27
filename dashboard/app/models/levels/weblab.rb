2463af235e3c35047a4ddeaa30253edca3df0b33
wilkie <wilkie05@gmail.com>
2026-03-27 16:01:29 -0400

    Moves the specialized code to reform weblab 1 startSources from weblab2 to weblab 1.

--- dashboard/app/models/levels/weblab.rb
+++ dashboard/app/models/levels/weblab.rb
@@ -39,4 +39,47 @@ class Weblab < Weblab2
   def self.create_from_level_builder(params, level_params)
     raise "Do not create Weblab levels"
   end
+
+  def summarize_for_lab2_properties(script, script_level = nil, current_user = nil, unit_group_unit: nil, widget2_start_sources: nil)
+    properties_camelized = super(script, script_level, current_user, unit_group_unit: unit_group_unit)
+
+    # Reform any legacy string startSources
+    # This converts it into something that the codebridge useInitialSources can understand.
+    if properties_camelized["startSources"].is_a?(String)
+      begin
+        properties_camelized["startSources"] = JSON.parse(properties_camelized["startSources"])
+
+        # Ensure startSources has folders
+        properties_camelized["startSources"]["folders"] ||= {}
+
+        # Ensure file data is correct
+        properties_camelized["startSources"]["files"] ||= {}
+
+        if properties_camelized["startSources"]["files"].is_a?(Array)
+          files = properties_camelized["startSources"]["files"]
+          properties_camelized["startSources"]["files"] = {}
+
+          id = 0
+          files.each do |file|
+            # Ensurre the file has an id
+            id += 1
+
+            properties_camelized["startSources"]["files"][id] = {
+              # Ensure 'data' becomes 'contents'
+              id: id.to_s,
+              active: id == 1,
+              folderId: "0",
+              name:  file["name"] || "index.html",
+              contents: file["data"] || "",
+            }
+          end
+        end
+      rescue
+        # We don't understand the startSources... let's ensure they are nulled out
+        properties_camelized.delete("startSources")
+      end
+    end
+
+    properties_camelized
+  end
 end
