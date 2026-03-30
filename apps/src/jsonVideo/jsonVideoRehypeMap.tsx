b74195bce5656273bc66e576380447f478606a47
David Bailey <davidsbailey@users.noreply.github.com>
2026-03-30 13:04:11 -0700

    Support blockly JSON tutor video links

--- apps/src/jsonVideo/jsonVideoRehypeMap.tsx
+++ apps/src/jsonVideo/jsonVideoRehypeMap.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 import '@cdo/apps/jsonVideo/jsonVideoElement';
 import TutorVideo from './TutorVideo';
 
+const JSON_VIDEO_PATH_PREFIXES = ['/assets/js/json/', '/blockly/js/json/'];
+
 const LinkWrapper: React.FunctionComponent<
   React.AnchorHTMLAttributes<HTMLAnchorElement>
 > = ({children, ...props}) => {
-  if (props.href?.includes('assets/js/json')) {
+  if (
+    props.href &&
+    JSON_VIDEO_PATH_PREFIXES.some(prefix => props.href?.includes(prefix))
+  ) {
     return <TutorVideo href={props.href} />;
   }
   return <a {...props}>{children}</a>;
