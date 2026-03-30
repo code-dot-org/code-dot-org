3d0e0b7a7105e43036a900c7754af0eaa880cc5f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2026-03-30 20:31:07 +0000

    Revert "Move `start-build` Endpoint to Dashboard (#71710)" (#71771)
    This reverts commit 51ee22159ac329057ab218dae673f790f5d11aa3.

--- pegasus/test/test_dev_routes.rb
+++ pegasus/test/test_dev_routes.rb
@@ -5,6 +5,7 @@ require_relative '../../lib/cdo/github'
 require_relative '../../lib/cdo/infra_test_topic'
 require_relative 'fixtures/mock_pegasus'
 
+BUILD_STARTED_PATH = deploy_dir('build-started').freeze
 FAKE_SLACK_SLASH_TOKEN = 'fake-start-build-token'.freeze
 
 class DevRoutesTest < Minitest::Test
@@ -17,6 +18,134 @@ class DevRoutesTest < Minitest::Test
     def make_test_pegasus
       mock_session = Rack::MockSession.new(MockPegasus.new, 'studio.code.org')
       Rack::Test::Session.new(mock_session)
+    end
+
+    describe '/api/dev/start-build' do
+      def assert_forbidden_on_rack_env(forbidden_env)
+        with_rack_env(forbidden_env) do
+          pegasus = make_test_pegasus
+
+          pegasus.post '/api/dev/start-build', DEFAULT_PARAMS
+          assert_equal 403, pegasus.last_response.status
+          refute File.file? BUILD_STARTED_PATH
+        end
+      end
+
+      def assert_allowed_on_rack_env(allowed_env)
+        with_rack_env(allowed_env) do
+          pegasus = make_test_pegasus
+          begin
+            pegasus.post '/api/dev/start-build', DEFAULT_PARAMS
+            assert_equal 200, pegasus.last_response.status
+          ensure
+            system "rm -f #{BUILD_STARTED_PATH}"
+          end
+        end
+      end
+
+      before do
+        $log.level = Logger::ERROR
+
+        # Fake slack token for tests
+        CDO.stubs(slack_start_build_token: FAKE_SLACK_SLASH_TOKEN)
+
+        # Should have no build-started file at the beginning of tests
+        system "rm -f #{BUILD_STARTED_PATH}"
+        refute File.file?(BUILD_STARTED_PATH),
+          'Precondition failure: File start-build should not exist.'
+      end
+
+      it 'is forbidden on production environments' do
+        assert_forbidden_on_rack_env :production
+      end
+
+      it 'is forbidden on development environments' do
+        assert_forbidden_on_rack_env :development
+      end
+
+      it 'is allowed on staging' do
+        assert_allowed_on_rack_env(:staging)
+      end
+
+      it 'is allowed on test' do
+        assert_allowed_on_rack_env(:test)
+      end
+
+      it 'is allowed on adhoc' do
+        assert_allowed_on_rack_env(:adhoc)
+      end
+
+      it 'is allowed on levelbuilder' do
+        assert_allowed_on_rack_env(:levelbuilder)
+      end
+
+      it 'is forbidden with a missing token' do
+        with_rack_env(:staging) do
+          pegasus = make_test_pegasus
+          pegasus.post(
+            '/api/dev/start-build',
+            {user_name: 'Dave'}
+          )
+          assert_equal 403, pegasus.last_response.status
+        end
+      end
+
+      it 'is forbidden with an incorrect token' do
+        with_rack_env(:staging) do
+          pegasus = make_test_pegasus
+          pegasus.post(
+            '/api/dev/start-build',
+            {
+              token: 'incorrect-token',
+              user_name: 'Dave'
+            }
+          )
+          assert_equal 403, pegasus.last_response.status
+        end
+      end
+
+      it 'generates a start_build file if none exists' do
+        with_rack_env(:test) do
+          pegasus = make_test_pegasus
+          begin
+            refute File.file? BUILD_STARTED_PATH
+            pegasus.post '/api/dev/start-build', DEFAULT_PARAMS
+            assert File.file? BUILD_STARTED_PATH
+
+            # Check appropriate response to whole room, too
+            assert_equal 200, pegasus.last_response.status
+            response_body = JSON.parse(pegasus.last_response.body)
+            assert_equal 'Test build restarted by Dave', response_body['text']
+            assert_equal 'in_channel', response_body['response_type']
+          ensure
+            system "rm -f #{BUILD_STARTED_PATH}"
+          end
+        end
+      end
+
+      it 'succeeds without action if start_build exists' do
+        with_rack_env(:test) do
+          pegasus = make_test_pegasus
+          begin
+            system "touch #{BUILD_STARTED_PATH}"
+            assert File.file? BUILD_STARTED_PATH
+            original_modify_time = File.mtime(BUILD_STARTED_PATH)
+
+            pegasus.post '/api/dev/start-build', DEFAULT_PARAMS
+            assert File.file? BUILD_STARTED_PATH
+            assert_equal original_modify_time, File.mtime(BUILD_STARTED_PATH)
+
+            # Check response to requester
+            assert_equal 200, pegasus.last_response.status
+            assert_equal(
+              "I can't do that Dave - a build is already queued",
+              pegasus.last_response.body
+            )
+          ensure
+            system "rm -f #{BUILD_STARTED_PATH}"
+          end
+        end
+      end
     end
 
     describe 'api/dev/set-last-dtt-green' do
