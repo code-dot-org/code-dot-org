4745b8f0e0edd68baf71532c25fe9c407919b802
Erin Bond <erin.bond@code.org>
2026-04-01 14:01:51 -0400

    apps/src/aiTutor/views/lessonDeepDive/LessonSummaryBox.tsx

--- apps/src/aiTutor/views/lessonDeepDive/LessonSummaryBox.tsx
+++ apps/src/aiTutor/views/lessonDeepDive/LessonSummaryBox.tsx
@@ -16,7 +16,7 @@ const LessonSummaryBox: FC<LessonSummaryBoxProps> = ({
     <Typography variant="h2" sx={{fontSize: {xs: '1.5rem', sm: '2rem'}}}>
       You worked on: {lessonName}
     </Typography>
-    The big ideas from this lesson were:
+    <br />
     <SafeMarkdown markdown={lessonSummary} />
     <Typography variant="body1">
       Let's take a look at your work from the lesson...
