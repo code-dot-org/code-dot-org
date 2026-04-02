868bb5567478642fd30ae64dabe47040e7bbc666
Alice Fisher <alice.fisher@code.org>
2026-04-02 22:04:41 +0000

    remove azure content moderator files

--- lib/cdo/image_moderation.rb
+++ lib/cdo/image_moderation.rb
@@ -3,24 +3,6 @@ require 'cdo/azure_ai_content_safety'
 require 'honeybadger/ruby'
 
 module ImageModeration
-  # Returns a content rating from an external service.
-  # @param [IO] image_data - binary image data to be rated
-  # @param [String] content_type - image/gif, image/jpeg, image/png
-  # @return [:everyone|:racy|:adult|:unknown] Whether the image is suitable for everyone
-  def self.rate_image(image_data, content_type)
-    return :everyone unless CDO.azure_content_moderation_key
-    AzureContentModerator.new(
-      endpoint: CDO.azure_content_moderation_endpoint,
-      api_key: CDO.azure_content_moderation_key
-    ).rate_image(image_data, content_type)
-  rescue AzureContentModerator::AzureError => exception
-    # If something goes wrong with the image moderation service our fallback
-    # behavior is to allow everything through, but we also want to notify
-    # Honeybadger so that we can figure out exactly what is going wrong.
-    Honeybadger.notify(exception)
-    :unknown
-  end
-
   # @param [IO] image_data - binary image data to be rated
   # @param [String] content_type - image/gif, image/jpeg, image/png
   # @return [Hash, nil] raw categoriesAnalysis response from Azure, or nil on error
--- lib/test/cdo/test_image_moderation.rb
+++ lib/test/cdo/test_image_moderation.rb
@@ -1,29 +1,33 @@
 require_relative '../test_helper'
 require 'cdo/image_moderation'
-require 'cdo/azure_content_moderator'
+require 'cdo/azure_ai_content_safety'
 
 class ImageModerationTest < Minitest::Test
   def setup
     @image_body = StringIO.new('fake-image-body')
     @content_type = 'image/png'
-    CDO.stubs(azure_content_moderation_key: 'fakekey')
+    CDO.stubs(
+      azure_ai_content_safety_key: 'fakekey',
+      azure_ai_content_safety_endpoint: 'https://fake.endpoint'
+    )
   end
 
-  def test_returns_everyone_when_missing_api_key
-    CDO.stubs(azure_content_moderation_key: nil)
-    AzureContentModerator.expects(:rate_image).never
-    assert_equal :everyone, ImageModeration.rate_image(@image_body, @content_type)
+  def test_returns_nil_when_missing_api_key
+    CDO.stubs(azure_ai_content_safety_key: nil)
+    AzureAiContentSafety.expects(:new).never
+    assert_nil ImageModeration.moderate_image(@image_body, @content_type)
   end
 
   def test_uses_azure_when_api_key_present
-    AzureContentModerator.any_instance.stubs(:rate_image).returns(:adult).once
-    assert_equal :adult, ImageModeration.rate_image(@image_body, @content_type)
+    sample = {'categoriesAnalysis' => []}
+    AzureAiContentSafety.any_instance.expects(:moderate_image).with(@image_body, @content_type).returns(sample).once
+    assert_equal sample, ImageModeration.moderate_image(@image_body, @content_type)
   end
 
-  def test_returns_unknown_when_moderation_fails
-    test_err = AzureContentModerator::AzureError.new('Test error')
-    AzureContentModerator.any_instance.stubs(:rate_image).raises(test_err)
+  def test_returns_nil_when_moderation_fails
+    test_err = AzureAiContentSafety::RequestFailed.new('Test error')
+    AzureAiContentSafety.any_instance.expects(:moderate_image).raises(test_err)
     Honeybadger.expects(:notify).once.with(test_err)
-    assert_equal :unknown, ImageModeration.rate_image(@image_body, @content_type)
+    assert_nil ImageModeration.moderate_image(@image_body, @content_type)
   end
 end
