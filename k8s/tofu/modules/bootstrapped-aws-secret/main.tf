0f7b1081c9334ff330d9328c87d99f231a05056b
Seth Nickell <snickell@gmail.com>
2026-04-02 17:37:30 -1000

    bootstrapped-aws-secret: fix semantics on non-bootstrap

--- k8s/tofu/modules/bootstrapped-aws-secret/main.tf
+++ k8s/tofu/modules/bootstrapped-aws-secret/main.tf
@@ -1,3 +1,18 @@
+resource "aws_secretsmanager_secret" "this" {
+  name = var.aws_secret_name
+}
+
+locals {
+  # Prefer an explicitly passed `secret_value_to_bootstrap`, then generate a random password
+  # if `bootstrap_with_random_value`, then fallback to the current AWS value.
+  secret_value = coalesce(
+    var.secret_value_to_bootstrap,
+    try(random_password.this[0].result, null),
+    try(data.aws_secretsmanager_secret_version.current_aws_version[0].secret_string, null),
+  )
+}
+
+# Generate a value only when the caller explicitly specifies `bootstrap_with_random_value`
 resource "random_password" "this" {
   count = var.bootstrap_with_random_value ? 1 : 0
 
@@ -5,30 +20,25 @@ resource "random_password" "this" {
   special = false
 }
 
-locals {
-  secret_value_to_write = coalesce(var.secret_value_to_bootstrap, try(random_password.this[0].result, null))
-}
+# We fetch the current AWS version in case no values are passed, and use that to keep the
+# managed_aws_version stable, so it doesn't get deleted when we aren't bootstrapping.
+data "aws_secretsmanager_secret_version" "current_aws_version" {
+  count = var.secret_value_to_bootstrap == null && !var.bootstrap_with_random_value ? 1 : 0
 
-resource "aws_secretsmanager_secret" "this" {
-  name = var.aws_secret_name
-}
-
-resource "aws_secretsmanager_secret_version" "this" {
-  count = local.secret_value_to_write != null ? 1 : 0
-
-  secret_id                = aws_secretsmanager_secret.this.id
-  secret_string_wo         = local.secret_value_to_write
-  secret_string_wo_version = parseint(substr(md5(nonsensitive(local.secret_value_to_write)), 0, 8), 16)
+  secret_id = aws_secretsmanager_secret.this.id
 
   depends_on = [
     aws_secretsmanager_secret.this,
   ]
 }
 
-data "aws_secretsmanager_secret_version" "this" {
-  secret_id = aws_secretsmanager_secret.this.id
+
+resource "aws_secretsmanager_secret_version" "managed_aws_version" {
+  secret_id                = aws_secretsmanager_secret.this.id
+  secret_string_wo         = local.secret_value
+  secret_string_wo_version = parseint(substr(md5(nonsensitive(local.secret_value)), 0, 8), 16)
 
   depends_on = [
-    aws_secretsmanager_secret_version.this,
+    aws_secretsmanager_secret.this,
   ]
 }
