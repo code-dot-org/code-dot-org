c26208b00ba48cb9f1e5cdee84a0f52f9ed115f6
Molly Moen <molly@code.org>
2026-04-03 16:50:16 -0700

    add comment

--- dashboard/legacy/test/middleware/test_channels_base64_error.rb
+++ dashboard/legacy/test/middleware/test_channels_base64_error.rb
@@ -20,6 +20,7 @@ class ChannelsBase64ErrorTest < Minitest::Test
   end
 
   private def run_test_cases(channel_id)
+    # We explicitly 404 on GET requests, because those are where users can input invalid channel IDs.
     @channels.get "/v3/channels/#{channel_id}"
     assert_equal 404, @channels.last_response.status
 
