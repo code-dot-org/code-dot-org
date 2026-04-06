e8dfe8a72d897aa3f6d83e5f34cd301b0b64ead6
Seth Nickell <snickell@gmail.com>
2026-04-06 06:52:06 -1000

    codeai-k8s-pre-reorg: DELETED

--- k8s/tofu/codeai-k8s-pre-reorg/eks-cluster-addons/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "codeai-tofu-state"
-    key          = "codeai-k8s-pre-reorg/eks-cluster-addons.tfstate"
-    region       = "us-west-2"
-    use_lockfile = true
-  }
-}
