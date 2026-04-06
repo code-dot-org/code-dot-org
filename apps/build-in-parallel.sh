60c1929b4364ae577f17c6e92055b7c733f3c510
David Bailey <davidsbailey@users.noreply.github.com>
2026-04-06 09:38:11 -0700

    reduce number of apps workers to 8

--- apps/build-in-parallel.sh
+++ apps/build-in-parallel.sh
@@ -3,14 +3,14 @@
 # parallelism and memory settings for optimal CI performance.
 #
 # Environment variables:
-#   APPS_BUILD_WORKERS - Number of workers for thread-loader and TerserPlugin (default: 10)
+#   APPS_BUILD_WORKERS - Number of workers for thread-loader and TerserPlugin (default: 8)
 #   APPS_BUILD_MAX_MEMORY - Max memory in MB for Node.js heap (default: 16384 MB / 16 GB)
 
 set -e
 
 # Default values optimized to run on staging and drone (m7i.4xlarge).
 # The test machine also runs the apps build and is much larger.
-DEFAULT_WORKERS=10
+DEFAULT_WORKERS=8
 DEFAULT_MAX_MEMORY=16384
 
 # Read environment variables or use defaults
