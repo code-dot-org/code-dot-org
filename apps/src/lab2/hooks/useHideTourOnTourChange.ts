c954e4ad71c746a22c65402ef4d193d985cb7725
Molly Moen <molly@code.org>
2026-04-06 13:06:46 -0700

    clean up hide logic and add tests

--- apps/src/lab2/hooks/useHideTourOnTourChange.ts
+++ apps/src/lab2/hooks/useHideTourOnTourChange.ts
@@ -1,18 +1,10 @@
-import {useEffect, useRef} from 'react';
+import {useEffect} from 'react';
 import {Tour} from 'shepherd.js';
 
-// Automatically hides the previous tour when the tour instance changes (i.e. on level change),
-// or the component unmounts.
+// Automatically hides the previous tour when the tour instance changes (i.e. on level change).
 // This will avoid showing tours that are no longer relevant.
 const useHideTourOnTourChange = (tour: Tour | null) => {
-  const prevTourRef = useRef<Tour | null>(null);
-
   useEffect(() => {
-    const prevTour = prevTourRef.current;
-    prevTourRef.current = tour;
-    if (prevTour && prevTour !== tour) {
-      prevTour.hide();
-    }
     return () => {
       if (tour) {
         tour.hide();
