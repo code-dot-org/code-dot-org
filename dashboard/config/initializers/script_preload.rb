b3371feea3c7865a61d678f1b1787566a4088d15
David Bailey <davidsbailey@users.noreply.github.com>
2026-04-07 10:53:33 -0700

    debug unit cache setting in drone logs

--- dashboard/config/initializers/script_preload.rb
+++ dashboard/config/initializers/script_preload.rb
@@ -2,7 +2,9 @@
 # This speeds up load time of new Unicorn child worker processes
 # and Spring application preloader (Rails console, unit tests).
 Rails.application.config.to_prepare do
+  ChatClient.log "Unit.should_cache?=#{Unit.should_cache?}, SKIP_SCRIPT_PRELOAD=#{ENV.fetch('SKIP_SCRIPT_PRELOAD', nil)}"
   next unless Unit.should_cache? && !ENV['SKIP_SCRIPT_PRELOAD']
+  ChatClient.log "Preloading script cache in #{Rails.env} environment"
 
   # Populate the shared in-memory cache from the database.
   Unit.unit_cache_to_cache unless Rails.cache.is_a?(ActiveSupport::Cache::MemoryStore)
