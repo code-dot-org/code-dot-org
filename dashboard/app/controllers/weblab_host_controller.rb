192324668dd09b9b92f507f9552330bec622f4ce
Molly Moen <molly@code.org>
2026-04-08 15:30:21 -0700

    use new version

--- dashboard/app/controllers/weblab_host_controller.rb
+++ dashboard/app/controllers/weblab_host_controller.rb
@@ -1,10 +1,10 @@
 class WeblabHostController < ApplicationController
-  BRAMBLE_URL = 'https://downloads.computinginthecore.org/bramble_0.1.32'
+  BRAMBLE_URL = 'https://downloads.computinginthecore.org/bramble_0.1.33'
   BRAMBLE_LOCALHOST_URL = 'http://127.0.0.1:8000/src'
   STUDIO_URL = CDO.studio_url('', CDO.default_scheme)
 
   def index
-    @dev_mode = true # Change to true to point to Bramble running on localhost
+    @dev_mode = false # Change to true to point to Bramble running on localhost
     @bramble_base_url = @dev_mode ? BRAMBLE_LOCALHOST_URL : BRAMBLE_URL
     @studio_url = STUDIO_URL
     @skip_files = params[:skip_files] == "true" # Special case for checking that Bramble can be initialized without loading files
