a6d6bce539e123a947de1bf83ba029ddb8bac3bd
Elijah Hamovitz <elijahhamovitz@gmail.com>
2026-04-10 10:37:09 -0700

    Revert "Stub writes for datasets/datablock tests to avoid serializing to disk…"
    This reverts commit f775eeaba0fb8cd0cc7adbf14182d829745d5950.

--- dashboard/test/controllers/datasets_controller_test.rb
+++ dashboard/test/controllers/datasets_controller_test.rb
@@ -2,9 +2,6 @@ require 'test_helper'
 
 class DatasetsControllerTest < ActionController::TestCase
   setup do
-    # stub writes so none of the models serialize to disk during testing
-    File.stubs(:write)
-
     @levelbuilder = create(:levelbuilder)
     Rails.application.config.stubs(:levelbuilder_mode).returns true
     sign_in @levelbuilder
