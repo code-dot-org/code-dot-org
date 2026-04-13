d9be11954ac76d913620b90f09d2344823df5b2b
Alice Fisher <alice.fisher@code.org>
2026-04-13 16:28:39 +0000

    handle empty image file on frontend and backend

--- apps/src/aichat/api/client/generateChatResponse.ts
+++ apps/src/aichat/api/client/generateChatResponse.ts
@@ -102,6 +102,9 @@ export async function generateChatResponse(
     }
     assets.push(asset);
     if (file.mediaType.startsWith('image/')) {
+      if (file.uint8Array.length === 0) {
+        return {response: text, status: AiRequestExecutionStatus.FAILURE};
+      }
       sendLab2AnalyticsEvent(EVENTS.MODEL_OUTPUT_IMAGE_CREATED);
       // Check generated images for safety.
       const imageModerationStatus = await getImageModerationStatus(
