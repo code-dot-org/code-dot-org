e125cf0245a90e40cbf9e490872487b667a0d6c6
denyslevada <levada.denys@gmail.com>
2026-04-14 17:40:20 +0300

    Use design system dialog for teacher section setup

--- apps/src/templates/teacherDashboard/AddSectionDialog.jsx
+++ apps/src/templates/teacherDashboard/AddSectionDialog.jsx
@@ -1,3 +1,4 @@
+import {Dialog} from '@code-dot-org/component-library/dialog';
 import PropTypes from 'prop-types';
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
@@ -7,11 +8,10 @@ import {navigateToHref} from '@cdo/apps/utils';
 import {SectionLoginType} from '@cdo/generated-scripts/sharedConstants';
 import i18n from '@cdo/locale';
 
-import BaseDialog from '../BaseDialog';
-
 import {NON_LMS_LOGIN_TYPES} from './LoginTypeConstants';
-import LoginTypePicker from './LoginTypePicker';
-import PadAndCenter from './PadAndCenter';
+import LoginTypePicker, {
+  recordLoginTypePickerCancelled,
+} from './LoginTypePicker';
 import ParticipantTypePicker from './ParticipantTypePicker';
 import {sectionShape} from './shapes';
 import {
@@ -24,6 +24,8 @@ import {
   isAddingSection,
   assignedCourseOffering,
 } from './teacherSectionsReduxSelectors';
+
+import styles from './sectionSetup.module.scss';
 
 // Navigates to the new section setup page if both params are non-null.
 const redirectToNewSectionPage = (participantType, loginType) => {
@@ -80,9 +82,23 @@ const AddSectionDialog = ({
     setLoginType(loginType);
   };
 
+  const onDialogCancel = () => {
+    if (participantType && !loginType) {
+      recordLoginTypePickerCancelled();
+    }
+    handleCancel();
+  };
+
   const getDialogContent = () => {
     if (!asyncLoadComplete) {
-      return <Spinner size="large" style={{padding: 50}} />;
+      return (
+        <div className={styles.loadingState}>
+          <span id="dsco-dialog-description" className={styles.srOnly}>
+            {i18n.loading()}
+          </span>
+          <Spinner size="large" />
+        </div>
+      );
     }
     /*
     The Participant Type Picker will be skipped if someone only have permissions to create sections for one
@@ -93,7 +109,6 @@ const AddSectionDialog = ({
         <ParticipantTypePicker
           title={title}
           setParticipantType={onParticipantTypeSelection}
-          handleCancel={handleCancel}
           availableParticipantTypes={availableParticipantTypes}
         />
       );
@@ -111,20 +126,22 @@ const AddSectionDialog = ({
     }
   };
 
-  if (participantType && loginType) {
+  if (!isOpen || (participantType && loginType)) {
     return null;
   } else {
     return (
-      <BaseDialog
-        useUpdatedStyles
-        fixedWidth={1010}
-        isOpen={isOpen}
-        overflow="auto"
-        uncloseable
-        style={{overflow: 'hidden'}}
-      >
-        <PadAndCenter>{getDialogContent()}</PadAndCenter>
-      </BaseDialog>
+      <Dialog
+        aria-label={title}
+        className={styles.dialog}
+        title={title}
+        primaryButtonProps={{
+          color: 'black',
+          type: 'secondary',
+          onClick: onDialogCancel,
+          text: i18n.dialogCancel(),
+        }}
+        customContent={getDialogContent()}
+      />
     );
   }
 };
--- apps/src/templates/teacherDashboard/LoginTypePicker.jsx
+++ apps/src/templates/teacherDashboard/LoginTypePicker.jsx
@@ -1,4 +1,7 @@
-import {Typography} from '@mui/material';
+import Alert from '@code-dot-org/component-library/alert';
+import Dialog from '@code-dot-org/component-library/dialog';
+import Link from '@code-dot-org/component-library/link';
+import {Button as MuiButton, Typography} from '@mui/material';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
@@ -8,16 +11,10 @@ import {
   LmsLoginTypeNames,
   LmsLoginInstructionUrls,
 } from '@cdo/apps/accounts/constants';
-import fontConstants from '@cdo/apps/fontConstants';
-import Button from '@cdo/apps/legacySharedComponents/Button';
 import analyticsReporter from '@cdo/apps/metrics/AnalyticsReporter';
 import {getStore} from '@cdo/apps/redux';
-import StylizedBaseDialog from '@cdo/apps/sharedComponents/StylizedBaseDialog';
-import color from '@cdo/apps/util/color';
 import {SectionLoginType} from '@cdo/generated-scripts/sharedConstants';
 import i18n from '@cdo/locale';
-
-import styleConstants from '../../styleConstants';
 
 import CardContainer from './CardContainer';
 import LmsInformationalCard from './LmsInformationalCard';
@@ -28,9 +25,17 @@ import {
   schoologyLogo,
 } from './LmsInformationalCard/assets';
 import LoginTypeCard from './LoginTypeCard';
+
+import styles from './sectionSetup.module.scss';
 const LOGIN_TYPE_SELECTED_EVENT = 'Login Type Selected';
 const CANCELLED_EVENT = 'Section Setup Cancelled';
 const SELECT_LOGIN_TYPE = 'Login Type Selection';
+
+export const recordLoginTypePickerCancelled = () => {
+  analyticsReporter.sendEvent(CANCELLED_EVENT, {
+    source: SELECT_LOGIN_TYPE,
+  });
+};
 
 /**
  * UI for selecting the login type of a class section:
@@ -40,10 +45,9 @@ class LoginTypePicker extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     handleImportOpen: PropTypes.func,
+    handleCancel: PropTypes.func.isRequired,
     setRosterProvider: PropTypes.func,
     setLoginType: PropTypes.func.isRequired,
-    handleCancel: PropTypes.func.isRequired,
-    disabled: PropTypes.bool,
     // Provided by Redux
     providers: PropTypes.arrayOf(PropTypes.string),
   };
@@ -61,12 +65,6 @@ class LoginTypePicker extends Component {
     });
   };
 
-  recordSectionSetupExitEvent = eventName => {
-    analyticsReporter.sendEvent(eventName, {
-      source: SELECT_LOGIN_TYPE,
-    });
-  };
-
   openImportDialog = provider => {
     this.reportLoginTypeSelection(provider);
     this.props.setRosterProvider(provider);
@@ -79,13 +77,8 @@ class LoginTypePicker extends Component {
     this.props.setLoginType(provider);
   };
 
-  cancel = () => {
-    this.recordSectionSetupExitEvent(CANCELLED_EVENT);
-    this.props.handleCancel();
-  };
-
   render() {
-    const {title, providers, disabled} = this.props;
+    const {providers} = this.props;
     const withGoogle =
       providers && providers.includes(OAuthSectionTypes.google_classroom);
     const withMicrosoft =
@@ -106,121 +99,72 @@ class LoginTypePicker extends Component {
     const showStudentsToSectionPermissionWarning =
       inUSA && currentUser.isTeacher;
 
-    const style = {
-      container: {
-        width: styleConstants['content-width'],
-        left: '20px',
-        right: '20px',
-      },
-      scroll: {
-        overflowX: 'hidden',
-        overflowY: 'auto',
-        marginBottom: '16px',
-      },
-      thirdPartyProviderUpsell: {
-        marginBottom: '10px',
-      },
-      warningIcon: {
-        color: 'red',
-        marginLeft: '5px',
-        marginRight: '5px',
-      },
-      warningHeader: {
-        color: 'red',
-      },
-      footer: {
-        width: styleConstants['content-width'],
-        height: '100px',
-        left: 0,
-        bottom: '-51px',
-        backgroundColor: '#fff',
-        borderRadius: '5px',
-      },
-      mediumText: {
-        fontSize: '.75em',
-        color: color.neutral_dark,
-        ...fontConstants['main-font-semi-bold'],
-      },
-      emailPolicyNote: {
-        marginBottom: '31px',
-        paddingTop: '8px',
-        borderTop: `1px solid ${color.neutral_dark}`,
-      },
-      subheader: {
-        color: color.charcoal,
-      },
-      lmsInfoCardsContainer: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'flex-start',
-        rowGap: '16px',
-        columnGap: '16px',
-        paddingBottom: '16px',
-      },
-    };
-
     return (
-      <div style={style.container}>
-        <Typography variant="h3" gutterBottom>
-          {title}
+      <div className={styles.screen}>
+        <Typography
+          id="dsco-dialog-description"
+          className={styles.bodyText}
+          variant="body2"
+        >
+          {i18n.addStudentsToSectionInstructionsUpdated()}
         </Typography>
-        <p>{i18n.addStudentsToSectionInstructionsUpdated()}</p>
         {showStudentsToSectionPermissionWarning && (
-          <p>
-            <span
-              className="fa-solid fa-triangle-exclamation"
-              aria-hidden="true"
-              style={style.warningIcon}
-            />
-            <span style={style.warningHeader}>
-              <strong>
-                {i18n.addStudentsToSectionPermissionHeader() + ' '}
-              </strong>
-            </span>
-            {i18n.addStudentsToSectionPermissionWarning() + ' '}
-            <Button
-              styleAsText={true}
-              onClick={() => this.setState({isLearnMoreOpen: true})}
-            >
-              {i18n.learnMore()}
-            </Button>
-          </p>
-        )}
-        {this.state.isLearnMoreOpen && (
-          <StylizedBaseDialog
-            isOpen={true}
-            hideFooter={true}
-            renderFooter={() => {}}
-            confirmationButtonText="OK"
-            cancellationButtonText="Cancel"
-            body={<p>{i18n.addStudentsToSectionPermissionExplanation()}</p>}
-            handleClose={() => this.setState({isLearnMoreOpen: false})}
+          <Alert
+            className={styles.warningAlert}
+            type="warning"
+            size="s"
+            isImmediateImportance={false}
+            text={
+              <>
+                <strong>{i18n.addStudentsToSectionPermissionHeader()}</strong>{' '}
+                {i18n.addStudentsToSectionPermissionWarning()}{' '}
+                <MuiButton
+                  className={styles.learnMoreButton}
+                  variant="text"
+                  color="primary"
+                  size="small"
+                  type="button"
+                  onClick={() => this.setState({isLearnMoreOpen: true})}
+                >
+                  {i18n.learnMore()}
+                </MuiButton>
+              </>
+            }
           />
         )}
-        <Typography style={style.subheader} variant="h6" gutterBottom>
+        {this.state.isLearnMoreOpen && (
+          <Dialog
+            title={i18n.addStudentsToSectionPermissionHeader()}
+            description={i18n.addStudentsToSectionPermissionExplanation()}
+            primaryButtonProps={{
+              text: i18n.ok(),
+              onClick: () => this.setState({isLearnMoreOpen: false}),
+            }}
+            onClose={() => this.setState({isLearnMoreOpen: false})}
+          />
+        )}
+        <Typography className={styles.sectionTitle} variant="h6">
           {i18n.loginTypes()}
         </Typography>
-        <div style={style.scroll}>
-          <CardContainer>
-            {withGoogle && (
-              <GoogleClassroomCard onClick={this.openImportDialog} />
-            )}
-            {withMicrosoft && (
-              <MicrosoftClassroomCard onClick={this.openImportDialog} />
-            )}
-            {withClever && <CleverCard onClick={this.openImportDialog} />}
-            <PictureLoginCard onClick={this.onLoginTypeSelect} />
-            <WordLoginCard onClick={this.onLoginTypeSelect} />
-            <EmailLoginCard onClick={this.onLoginTypeSelect} />
-          </CardContainer>
-        </div>
+        <CardContainer>
+          {withGoogle && (
+            <GoogleClassroomCard onClick={this.openImportDialog} />
+          )}
+          {withMicrosoft && (
+            <MicrosoftClassroomCard onClick={this.openImportDialog} />
+          )}
+          {withClever && <CleverCard onClick={this.openImportDialog} />}
+          <PictureLoginCard onClick={this.onLoginTypeSelect} />
+          <WordLoginCard onClick={this.onLoginTypeSelect} />
+          <EmailLoginCard onClick={this.onLoginTypeSelect} />
+        </CardContainer>
         {!withAllLmsProviders && (
           <>
-            <Typography style={style.subheader} variant="h6" gutterBottom>
+            <Typography className={styles.sectionTitle} variant="h6">
               {i18n.lmsIntegrations()}
             </Typography>
             <div
-              style={style.lmsInfoCardsContainer}
+              className={styles.lmsCards}
               // eslint-disable-next-line react/forbid-dom-props
               data-testid={'lms-info-cards-container'}
             >
@@ -251,18 +195,12 @@ class LoginTypePicker extends Component {
             </div>
           </>
         )}
-        <div style={style.footer}>
-          <p style={{...style.mediumText, ...style.emailPolicyNote}}>
+        <div className={styles.footer}>
+          <Typography className={styles.note} variant="body2">
             {i18n.note()}
             {' ' + i18n.emailAddressPolicy() + ' '}
-            <a href="https://code.org/privacy">{i18n.moreInfo()}</a>
-          </p>
-          <Button
-            onClick={this.cancel}
-            text={i18n.dialogCancel()}
-            color={Button.ButtonColor.neutralDark}
-            disabled={disabled}
-          />
+            <Link href="https://code.org/privacy">{i18n.moreInfo()}</Link>
+          </Typography>
         </div>
       </div>
     );
