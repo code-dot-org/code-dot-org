5fb867d5ea157e055f13fcf9013da5a7766a6cca
denyslevada <levada.denys@gmail.com>
2026-04-15 16:22:31 +0300

    Rename section setup modal styles

--- apps/src/templates/teacherDashboard/AddSectionDialog.jsx
+++ apps/src/templates/teacherDashboard/AddSectionDialog.jsx
@@ -130,7 +130,7 @@ const AddSectionDialog = ({
     return (
       <Modal
         aria-label={title}
-        className={`${styles.dialog} uitest-new-section-dialog`}
+        className={`${styles.modal} uitest-new-section-dialog`}
         title={title}
         primaryButtonProps={{
           // TODO: Wait till Modal uses mui button and replace this props with MUI Button props
