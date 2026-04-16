c252b7bf72d55eb76d1985d69365fdf73d990306
wilkie <wilkie05@gmail.com>
2026-04-16 16:56:07 -0400

    Adds localization of legacy init()-based custom blocks.

--- apps/src/StudioApp.js
+++ apps/src/StudioApp.js
@@ -16,6 +16,7 @@ import {
   highlightBlock,
   loadBlocksToWorkspace,
   processToolboxXml,
+  localizeBlockInitDefinition,
 } from '@cdo/apps/blockly/utils';
 import {addCallouts} from '@cdo/apps/code-studio/callouts';
 import {createLibraryClosure} from '@cdo/apps/code-studio/components/libraries/libraryParser';
@@ -551,7 +552,10 @@ StudioApp.prototype.init = function (config) {
   // TODO (cpirich): implement block count for droplet (for now, blockly only)
   if (this.isUsingBlockly()) {
     // Hook the blockly environment into the localization engine
-    if (experiments.isEnabledAllowingQueryString(experiments.LOCALIZEJS)) {
+    if (
+      localization.isLocalizeJS() ||
+      experiments.isEnabledAllowingQueryString(experiments.LOCALIZEJS)
+    ) {
       localization.on('change', info => {
         const blockDefinitions =
           BlocklyUtils.getBlockDefinitionsForUpdatedLocale(info.rtl);
@@ -559,6 +563,14 @@ StudioApp.prototype.init = function (config) {
           blockly: Blockly,
           blockDefinitions,
           customInputTypes: Blockly.SourceCustomInputTypes,
+        });
+
+        // Also localize the base init blocks
+        Object.entries(BlocklyCore.Blocks).forEach(([key, blockDefinition]) => {
+          if (blockDefinition.init) {
+            BlocklyCore.Blocks[key] =
+              localizeBlockInitDefinition(blockDefinition);
+          }
         });
         BlocklyUtils.refreshWorkspacesForUpdatedLocale(info.rtl);
       });
