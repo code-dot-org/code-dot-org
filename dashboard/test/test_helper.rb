46b678b9a70cc0949348aee8db84d6232974b56a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2026-04-16 13:49:34 -0700

    disable query cache

--- dashboard/test/test_helper.rb
+++ dashboard/test/test_helper.rb
@@ -99,6 +99,9 @@ class ActiveSupport::TestCase
     # how come this doesn't work:
     Dashboard::Application.config.action_controller.perform_caching = false
     # as in, I still need to clear the cache even though we are not 'performing' caching
+    # Because, that just enabled the controller cache. There's other forms of caching, including:
+    ActiveRecord::Base.connection.disable_query_cache!
+    # we should probably use a NullStore instead of doing this
     Rails.cache.clear
 
     # clear log of 'delivered' mails
