aa5c05ecfdbc6df898b992c3cd72e50898924aa5
Ed Baafi <edward.baafi@code.org>
2026-04-17 13:41:39 -0400

    add updated json video files

--- apps/src/weblab2/weblab2Videos.ts
+++ apps/src/weblab2/weblab2Videos.ts
@@ -1,4 +1,5 @@
-import addToAiTutorUrl from '@cdo/static/json/jsonVideo/weblab2/add-to-ai-tutor.json';
+import addToAiTutorUrl from '@cdo/static/json/jsonVideo/weblab2/add-ai-tutor-v2.json';
+import weblabDifferentViews from '@cdo/static/json/jsonVideo/weblab2/web-lab-different-views.json';
 
 import type {JsonVideoFileMetadata} from '@cdo/apps/jsonVideo/jsonVideoPrompt';
 
@@ -12,5 +13,10 @@ export const weblab2VideoFiles: JsonVideoFileMetadata[] = [
     description:
       "This video covers how to use the 'Add to AI Tutor' function within Web Lab. It demonstrates how to highlight text to add to AI tutor, and how to add an entire file to AI tutor via the menu in the file pane. The video is a clear walkthrough of how to use the Add to AI Tutor feature in Web Lab",
     url: addToAiTutorUrl as unknown as string,
+  },
+  {
+    description:
+      "This video covers how to switch between Code, Preview, and Split View within Web Lab. It demonstrates how to focus the workspace on certain areas, which can make it easier to view and edit code or see the full design of a webpage. The video is a clear walkthrough of how to navigate these view settings",
+    url: weblabDifferentViews as unknown as string,
   },
 ];
