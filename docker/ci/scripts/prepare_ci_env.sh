74e067a0834a4c73903aaa2054d6a9529daae308
Elijah Hamovitz <elijahhamovitz@gmail.com>
2026-04-20 12:34:51 -0700

    don't put jemalloc in a background thread, to avoid breaking apps tests

--- docker/ci/scripts/prepare_ci_env.sh
+++ docker/ci/scripts/prepare_ci_env.sh
@@ -15,10 +15,11 @@ export DISABLE_SPRING=1
 export LD_LIBRARY_PATH=/usr/local/lib
 
 # Enable jemalloc to prevent runaway memory growth during dashboard unit tests.
-# Configuration options based on the defaults used in chef; see
-# cookbooks/cdo-jemalloc/attributes/default.rb
+# Configuration options based on the defaults used in chef (see
+# cookbooks/cdo-jemalloc/attributes/default.rb), with the notable exception of
+# `background_thread:true` since that breaks chromedriver.
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.2
-export MALLOC_CONF=narenas:2,background_thread:true,thp:never,dirty_decay_ms:1000,muzzy_decay_ms:0
+export MALLOC_CONF=narenas:2,thp:never,dirty_decay_ms:1000,muzzy_decay_ms:0
 
 # Number of parallel processes for dashboard ruby unit tests,
 # optimized for drone m7i.4xlarge workers with 16 vCPUs and 64 GB RAM.
