2445a1e4d7988e995e9be69e4c09e8e3247fd466
denyslevada <levada.denys@gmail.com>
2026-04-20 16:45:11 +0300

    feat: refactor VerifiedResourcesNotification to use NotificationBanner and Link components

--- apps/src/templates/courseOverview/VerifiedResourcesNotification.js
+++ apps/src/templates/courseOverview/VerifiedResourcesNotification.js
@@ -1,31 +1,41 @@
+import Link from '@code-dot-org/component-library/link';
+import NotificationBanner from '@code-dot-org/component-library/notification-banner';
 import PropTypes from 'prop-types';
 import React, {PureComponent} from 'react';
 
-import Notification, {
-  NotificationType,
-} from '@cdo/apps/sharedComponents/Notification';
 import i18n from '@cdo/locale';
+
+import styles from './courseScript.module.scss';
+
+const VERIFIED_RESOURCES_URL =
+  'https://support.code.org/hc/en-us/articles/115001550131';
 
 export default class VerifiedResourcesNotification extends PureComponent {
   render() {
     return (
-      <Notification
-        type={NotificationType.warning}
-        notice={i18n.verifiedResourcesNotice()}
-        details={
+      <NotificationBanner
+        variant="warning"
+        title={i18n.verifiedResourcesNotice()}
+        className={styles.verifiedResourcesNotification}
+        description={
           this.props.inLesson
             ? i18n.verifiedResourcesLessonDetails()
             : i18n.verifiedResourcesDetails()
         }
-        buttonText={i18n.learnMore()}
-        buttonLink="https://support.code.org/hc/en-us/articles/115001550131"
-        dismissible={true}
-        width={this.props.width}
+        icon={{iconName: 'triangle-exclamation', iconStyle: 'solid'}}
+        actions={
+          <Link
+            href={VERIFIED_RESOURCES_URL}
+            text={i18n.learnMore()}
+            type="secondary"
+            size="s"
+            openInNewTab
+          />
+        }
       />
     );
   }
 }
 VerifiedResourcesNotification.propTypes = {
-  width: PropTypes.number,
   inLesson: PropTypes.bool,
 };
