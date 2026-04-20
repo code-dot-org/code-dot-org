7142d6a140c428b2c249294606e7e88bb97f1201
Ceara Chewning <ceara.chewning@code.org>
2026-04-20 10:32:57 -0400

    Remove comment from copy-paste that doesn't apply

--- apps/src/aiTutor/views/lessonDeepDive/DraggableOptions.tsx
+++ apps/src/aiTutor/views/lessonDeepDive/DraggableOptions.tsx
@@ -13,17 +13,6 @@ interface DraggableOptionProps {
   showAnswer: boolean;
 }
 
-/**
- * A React component that makes its children draggable using the `useDraggable` hook. Should wrap around a FileBrowserRow.
- * If you -don't- want a row to be draggable, but still want a wrapper, you can wrap it in NotDraggable, below.
- *
- * @param props - The props for the `Draggable` component.
- * @param props.children - The content to be made draggable.
- * @param props.data - An object containing data associated with the draggable element. data must be of type DragDataType.
- * @param props.Component - (Optional) The underlying HTML element to use as the draggable container.
- *                         - Defaults to `'div'`.
- * @returns A React element with the provided children, styled for dragging and handling drag events.
- */
 export const DraggableOptions: React.FunctionComponent<
   DraggableOptionProps
 > = ({id, option, correct, showAnswer}) => {
