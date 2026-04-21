772abf62788b610980220047e110c79527240e16
kobryan0619 <kaitie.obryan@gmail.com>
2026-04-21 15:24:39 -0500

    Fix issue with tests

--- dashboard/app/models/lesson.rb
+++ dashboard/app/models/lesson.rb
@@ -33,6 +33,7 @@ class Lesson < ApplicationRecord
 
   belongs_to :script, class_name: 'Unit', inverse_of: :lessons, optional: true
   belongs_to :lesson_group, optional: true
+  has_many :rubrics, dependent: :destroy
   has_many :lesson_activities, -> {order(:position)}, dependent: :destroy
   has_many :activity_sections, through: :lesson_activities
   has_many :script_levels, -> {order(:chapter)}, foreign_key: 'stage_id', dependent: :destroy
@@ -42,7 +43,6 @@ class Lesson < ApplicationRecord
   has_and_belongs_to_many :programming_expressions, join_table: :lessons_programming_expressions
   has_and_belongs_to_many :jit_pl_concepts, join_table: :jit_pl_concepts_lessons
   has_many :objectives, dependent: :destroy
-  has_many :rubrics, dependent: :destroy
 
   # join tables needed for seeding logic
   has_many :lessons_resources
