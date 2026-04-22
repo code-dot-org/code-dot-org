bfb217356a50d4f8e2bbd86e2a6b0a13f3aafad2
Hannah Bergam <hannah.bergam@code.org>
2026-04-21 18:21:36 -0700

    redoing work in new files

--- apps/src/aichatLab/views/AichatView.tsx
+++ apps/src/aichatLab/views/AichatView.tsx
@@ -236,10 +236,18 @@ const AichatView: React.FunctionComponent<LabProps<AichatLevelProperties>> = ({
   const chatWorkspaceHeader = (
     <div className={moduleStyles.workspaceHeaderContent}>
       {viewMode === ViewMode.EDIT ? 'AI Chat' : botName}
-      {projectTemplateLevel && (
-        <ProjectTemplateWorkspaceIconV2 tooltipPlace="onBottom" />
-      )}
     </div>
+  );
+
+  const chatWorkspaceHeaderRight = (
+    <>
+      {projectTemplateLevel && (
+        <span className={moduleStyles.templateIconWrapper}>
+          <ProjectTemplateWorkspaceIconV2 tooltipPlace="onBottom" />
+        </span>
+      )}
+      <AiChatHeaderButtons />
+    </>
   );
 
   const resetProject = useCallback(() => {
@@ -360,7 +368,7 @@ const AichatView: React.FunctionComponent<LabProps<AichatLevelProperties>> = ({
               headerContent={chatWorkspaceHeader}
               className={moduleStyles.panelContainer}
               headerClassName={moduleStyles.panelHeader}
-              rightHeaderContent={<AiChatHeaderButtons />}
+              rightHeaderContent={chatWorkspaceHeaderRight}
             >
               {chatWorkspaceInitialized && (
                 <ChatWorkspace
