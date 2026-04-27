8215af8f9f71babc03805376dce263549cc1dfea
Cassi Brenci <cassi.brenci@code.org>
2026-04-27 14:08:43 -0700

    add links to the aichat disabled state (#72323)
    * add links to the aichat disabled state
    
    * Refactor AiChatDisabledState into types and pass AiChatDisabledState in place of destructured props.
    
    * add links to expected results in tests
    
    * make links secondary and open in new tab
    
    * set openInNewTab when defining the link so it follows wherever the link is used
    
    * ope, update uts
    
    * Merge remote-tracking branch 'origin/staging' into cassi/tutor/links-in-disabled-state

--- apps/src/aichatLab/views/AichatView.tsx
+++ apps/src/aichatLab/views/AichatView.tsx
@@ -279,7 +279,7 @@ const AichatView: React.FunctionComponent<LabProps<AichatLevelProperties>> = ({
     savedAiCustomizations.systemPrompt,
   ]);
 
-  const {disabled, disabledMessage} = useAiChatDisabledState({
+  const disabledState = useAiChatDisabledState({
     appName: levelProperties.appName,
     isPredictLevel: !!levelProperties.predictSettings?.isPredictLevel,
     hasSubmittedPredictResponse,
@@ -442,8 +442,7 @@ const AichatView: React.FunctionComponent<LabProps<AichatLevelProperties>> = ({
                     }
                     multimodalEnabled={levelAichatSettings?.multimodalEnabled}
                     logLevelActivity={logLevelActivity}
-                    disabled={disabled}
-                    disabledMessage={disabledMessage}
+                    disabledState={disabledState}
                     ref={chatWorkspaceRef}
                   />
                 )}
