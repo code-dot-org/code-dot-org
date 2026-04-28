81661908e484f41a70ce70a2a6328771e3c51650
wilkie <david.wilkinson@code.org>
2026-04-28 02:16:12 -0400

    Apply suggestion from @wilkie

--- frontend/packages/core/src/api/dashboard/auth/auth.api.ts
+++ frontend/packages/core/src/api/dashboard/auth/auth.api.ts
@@ -16,7 +16,6 @@ export function createAuthApi(transport: Transport) {
      * GET /dashboardapi/sign_cookies
      */
     async signCookies(params?: {buster: boolean}): Promise<unknown> {
-      // TODO: this needs credentials. how does that work
       return transport.request<unknown>({
         method: 'GET',
         url: `/dashboardapi/sign_cookies${params?.buster ? `?bust=${Date.now()}` : ''}`,
