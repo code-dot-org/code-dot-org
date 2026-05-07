806b89c02e3d11d23cf7b3e8f546ac6cac970dbd
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-05-07 17:53:42 +0300

    fix(global-edition): fix url parsing

--- dashboard/test/ui/features/step_definitions/global_edition_steps.rb
+++ dashboard/test/ui/features/step_definitions/global_edition_steps.rb
@@ -2,7 +2,7 @@ require 'uri'
 require 'rack/utils'
 
 When 'I switch to the Global Edition region {string}' do |region_code|
-  uri = URI(@browser.current_url&.start_with('http') ? @browser.current_url : 'http://studio.code.org')
+  uri = URI(@browser.current_url&.start_with?('http') ? @browser.current_url : 'http://studio.code.org')
 
   params = Rack::Utils.parse_nested_query(uri.query)
   params['ge_region'] = region_code
