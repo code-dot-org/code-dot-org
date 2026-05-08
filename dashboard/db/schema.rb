ad986dc2684c049d38f444f40f95287e109b009a
Ceara Chewning <ceara.chewning@code.org>
2026-05-08 11:56:49 -0400

    Update migration timestamp

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_06_120000) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_07_204057) do
   create_table "activities", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "level_id"
