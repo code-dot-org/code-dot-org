24cffbe9517685bafe2856827fdcc2ab225aeb82
Liam Frye-Mason <liam.fryemason@code.org>
2026-05-20 14:44:40 -0700

    Small changes

--- dashboard/db/migrate/20260520120000_create_demo_students.rb
+++ dashboard/db/migrate/20260520120000_create_demo_students.rb
@@ -1,12 +1,4 @@
 class CreateDemoStudents < ActiveRecord::Migration[7.0]
-  # In any environment that previously set CDO.demo_student_ids in locals.yml,
-  # backfill rows from the legacy config from a Rails console after deploy:
-  #
-  #   (CDO.respond_to?(:demo_student_ids) ? CDO.demo_student_ids : nil)&.each do |type, ids|
-  #     Array(ids).each do |id|
-  #       DemoStudent.find_or_create_by!(user_id: id.to_i, demo_type: type.to_s)
-  #     end
-  #   end
   def change
     create_table :demo_students do |t|
       t.references :user,
