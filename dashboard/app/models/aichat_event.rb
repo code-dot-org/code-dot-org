ef3e80a1bad61443a648a6d3892218708d683fad
Erin Bond <erin.bond@code.org>
2026-05-27 11:40:49 -0400

    remove stray files

--- dashboard/app/models/aichat_event.rb
+++ dashboard/app/models/aichat_event.rb
@@ -15,13 +15,12 @@
 #
 # Indexes
 #
-#  index_ace_lesson_user                         (lesson_id,user_id)
-#  index_ace_user_level_script                   (user_id,level_id,script_id)
-#  index_ace_user_level_script_id                (user_id,level_id,script_id,id)
-#  index_ace_user_project                        (user_id,project_id)
-#  index_ace_user_project_id                     (user_id,project_id,id)
-#  index_aichat_events_on_request_id             (request_id)
-#  index_aichat_events_on_user_id_and_lesson_id  (user_id)
+#  index_ace_lesson_user              (lesson_id,user_id)
+#  index_ace_user_level_script        (user_id,level_id,script_id)
+#  index_ace_user_level_script_id     (user_id,level_id,script_id,id)
+#  index_ace_user_project             (user_id,project_id)
+#  index_ace_user_project_id          (user_id,project_id,id)
+#  index_aichat_events_on_request_id  (request_id)
 #
 class AichatEvent < ApplicationRecord
   belongs_to :user
