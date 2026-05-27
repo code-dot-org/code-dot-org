fa4de96b5293d795a1fa54b06e004d86026f4eeb
Alice Fisher <alice.fisher@code.org>
2026-05-27 17:51:26 +0000

    add unit test for webp

--- dashboard/test/helpers/aichat_ai_helper_test.rb
+++ dashboard/test/helpers/aichat_ai_helper_test.rb
@@ -50,6 +50,22 @@ class AichatAiHelperTest < ActionView::TestCase
       _(parts.third.content.mimeType).must_equal 'application/pdf'
       _(parts.third.content.data).must_equal 'pdf-base64'
     end
+
+    it 'uses image/webp for webp filenames' do
+      webp_payload = {
+        'chatMessageText' => 'Describe this',
+        'assets' => [{'filename' => 'shot.webp', 'source' => 'project'}]
+      }
+      AichatAssetHelper.expects(:get_asset_base64_string).
+        with('shot.webp', 'project', encrypted_channel_id, level_name).
+        returns('webp-base64')
+
+      parts = AichatAiHelper.format_message_parts(webp_payload, encrypted_channel_id, level_name)
+
+      _(parts.length).must_equal 2
+      _(parts.second.content.mimeType).must_equal 'image/webp'
+      _(parts.second.content.data).must_equal 'webp-base64'
+    end
   end
 
   describe '.convert_json_schema_to_ruby_types' do
