76fdf64524540b38037c5e597eb4d7920b8ae555
carl-codeorg <131809324+carl-codeorg@users.noreply.github.com>
2026-05-28 10:49:40 -0700

    port user delete event to Statsig (#72913)

--- dashboard/app/controllers/registrations_controller.rb
+++ dashboard/app/controllers/registrations_controller.rb
@@ -1,4 +1,3 @@
-require 'cdo/firehose'
 require 'cdo/honeybadger'
 require 'cdo/mailjet'
 require_relative '../../../shared/middleware/helpers/experiments'
@@ -648,45 +647,20 @@ class RegistrationsController < Devise::RegistrationsController
       )
   end
 
-  private def log_account_deletion_to_firehose(current_user, dependent_users)
-    # Log event for user initiating account deletion.
-    FirehoseClient.instance.put_record(
-      :analysis,
-      {
-        study: 'user-soft-delete-audit-v2',
-        event: 'initiated-account-deletion',
-        user_id: current_user.id,
-        data_json: {
-          user_type: current_user.user_type,
-          dependent_user_ids: dependent_users.pluck(:id),
-        }.to_json
-      }
-    )
-
-    # Log separate events for dependent users destroyed in user-initiated account deletion.
-    # This should only happen for teachers.
-    dependent_users.each do |user|
-      FirehoseClient.instance.put_record(
-        :analysis,
-        {
-          study: 'user-soft-delete-audit-v2',
-          event: 'dependent-account-deletion',
-          user_id: user[:id],
-          data_json: {
-            user_type: user[:user_type],
-            deleted_by_id: current_user.id,
-          }.to_json
-        }
-      )
-    end
-  end
-
   private def destroy_users(current_user, dependent_users)
     users = [current_user] + dependent_users
     user_ids_to_destroy = users.pluck(:id)
     User.ignore_deleted_at_index.destroy(user_ids_to_destroy)
 
-    log_account_deletion_to_firehose(current_user, dependent_users)
+    Metrics::Events.log_event(
+      user: current_user,
+      event_name: 'user_self_deleted',
+      metadata: {
+        user_type: current_user.user_type,
+        dependent_user_count: dependent_users&.count,
+        dependent_user_ids: dependent_users&.pluck(:id),
+      },
+    )
   end
 
   private def us_ip?
