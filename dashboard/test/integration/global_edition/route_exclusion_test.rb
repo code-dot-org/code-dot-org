48f98628ec971d5f91aed3527a020885983ef1b4
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-05-29 20:13:17 +0300

    GEPW-40: Fix integration tests

--- dashboard/test/integration/global_edition/route_exclusion_test.rb
+++ dashboard/test/integration/global_edition/route_exclusion_test.rb
@@ -3,11 +3,16 @@
 require 'test_helper'
 
 class GlobalEditionRouteExclusionTest < ActionDispatch::IntegrationTest
+  include Minitest::RSpecMocks
+
   let(:ge_region) {'fa'}
   let(:document) {Nokogiri::HTML(response.body)}
   let(:ge_region_html_data) {document.at('html[data-ge-region]').try(:[], 'data-ge-region')}
 
   before do
+    allow(Cdo::GlobalEdition).to receive(:target_host?).and_call_original
+    allow(Cdo::GlobalEdition).to receive(:target_host?).with('test-studio.code.org').and_return(true)
+
     cookies[:ge_region] = ge_region
   end
 
