181bd962d8c7ed55159d7cc792748a22ea18cf98
denyslevada <levada.denys@gmail.com>
2026-06-02 15:54:02 +0300

    refactor: replace legacy Button components with MUI Button and update dialog components

--- apps/src/code-studio/components/progress/UnitOverviewHeader.jsx
+++ apps/src/code-studio/components/progress/UnitOverviewHeader.jsx
@@ -1,3 +1,5 @@
+import NotificationBanner from '@code-dot-org/component-library/notification-banner';
+import {Button as MuiButton, Typography} from '@mui/material';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
@@ -6,9 +8,6 @@ import {connect} from 'react-redux';
 import {announcementShape} from '@cdo/apps/code-studio/announcementsRedux';
 import PlcHeader from '@cdo/apps/code-studio/plc/header';
 import {ViewType} from '@cdo/apps/code-studio/viewAsRedux';
-import Notification, {
-  NotificationType,
-} from '@cdo/apps/sharedComponents/Notification';
 import VerifiedResourcesNotification from '@cdo/apps/templates/courseOverview/VerifiedResourcesNotification';
 import {SignInState} from '@cdo/apps/templates/currentUserRedux';
 import ParticipantFeedbackNotification from '@cdo/apps/templates/feedback/ParticipantFeedbackNotification';
@@ -24,6 +23,8 @@ import i18n from '@cdo/locale';
 import Announcements from './Announcements';
 
 import styles from './unit-overview.module.scss';
+
+const WARNING_ICON = {iconName: 'triangle-exclamation'};
 
 /**
  * This component takes some of the HAML generated content on the script overview
@@ -149,39 +150,53 @@ class UnitOverviewHeader extends Component {
         {userId && <ParticipantFeedbackNotification studentId={userId} />}
         {displayVerifiedResources && <VerifiedResourcesNotification />}
         {displayVersionWarning && (
-          <Notification
-            type={NotificationType.warning}
-            notice=""
-            details={i18n.redirectCourseVersionWarningDetails()}
-            dismissible={true}
-            onDismiss={() => onDismissRedirectWarning(courseName || scriptName)}
+          <NotificationBanner
+            variant="warning"
+            icon={WARNING_ICON}
+            title={i18n.redirectCourseVersionWarningDetails()}
+            onClose={() => onDismissRedirectWarning(courseName || scriptName)}
           />
         )}
         {versionWarningDetails && (
-          <Notification
-            type={NotificationType.warning}
-            notice={i18n.wrongCourseVersionWarningNotice()}
-            details={versionWarningDetails}
-            dismissible={true}
-            onDismiss={this.onDismissVersionWarning}
+          <NotificationBanner
+            variant="warning"
+            icon={WARNING_ICON}
+            title={i18n.wrongCourseVersionWarningNotice()}
+            description={versionWarningDetails}
+            onClose={this.onDismissVersionWarning}
           />
         )}
         {showHiddenUnitWarning && (
-          <Notification
-            type={NotificationType.warning}
-            notice={i18n.hiddenUnitWarningNotice()}
-            details={i18n.hiddenUnitWarningDetails()}
-            dismissible={false}
-            buttonText={i18n.learnMore()}
-            buttonLink="https://support.code.org/hc/en-us/articles/115001479372-Hiding-units-and-lessons-in-Code-org-s-CS-Principles-and-CS-Discoveries-courses"
+          <NotificationBanner
+            variant="warning"
+            icon={WARNING_ICON}
+            title={i18n.hiddenUnitWarningNotice()}
+            description={i18n.hiddenUnitWarningDetails()}
+            actions={
+              <MuiButton
+                href="https://support.code.org/hc/en-us/articles/115001479372-Hiding-units-and-lessons-in-Code-org-s-CS-Principles-and-CS-Discoveries-courses"
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="outlined"
+                color="secondary"
+                size="small"
+              >
+                {i18n.learnMore()}
+              </MuiButton>
+            }
           />
         )}
         <div id="lesson">
           <div className={styles.heading}>
             <div className={styles.titleWrapper}>
-              <h1 className={styles.title} id="script-title">
+              <Typography
+                variant="h1"
+                component="h1"
+                className={styles.title}
+                id="script-title"
+              >
                 {unitTitle}
-              </h1>
+              </Typography>
             </div>
             {children}
             <div />
