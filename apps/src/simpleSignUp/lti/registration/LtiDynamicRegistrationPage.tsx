0faa37ea0fcb73844cd60f825dd7d05bbced0d61
Carl Reiner <carl.reiner@code.org>
2026-06-03 15:36:18 -0700

    linting

--- apps/src/simpleSignUp/lti/registration/LtiDynamicRegistrationPage.tsx
+++ apps/src/simpleSignUp/lti/registration/LtiDynamicRegistrationPage.tsx
@@ -1,10 +1,10 @@
+import FormFieldWrapper from '@code-dot-org/component-library/formFieldWrapper';
 import {Typography, Button as MuiButton} from '@mui/material';
 import $ from 'jquery';
 import React, {useState} from 'react';
 
 import {EVENTS} from '@cdo/apps/metrics/AnalyticsConstants';
 import analyticsReporter from '@cdo/apps/metrics/AnalyticsReporter';
-import FormFieldWrapper from '@code-dot-org/component-library/formFieldWrapper';
 import i18n from '@cdo/locale';
 
 import styles from './styles.module.scss';
