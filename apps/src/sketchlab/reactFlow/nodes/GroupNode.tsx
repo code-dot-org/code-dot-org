5b24ecc5d5feed39dde285529a3d03dafcce04df
Ben Brooks <ben@code.org>
2026-06-08 14:08:13 -0700

    No resize on group

--- apps/src/sketchlab/reactFlow/nodes/GroupNode.tsx
+++ apps/src/sketchlab/reactFlow/nodes/GroupNode.tsx
@@ -1,22 +1,12 @@
-import {NodeResizer, type NodeProps} from '@xyflow/react';
+import {type NodeProps} from '@xyflow/react';
 import React, {memo} from 'react';
 
-import {MIN_NODE_HEIGHT, MIN_NODE_WIDTH} from '../constants';
-import {useSketchLabReadOnly} from '../context';
 import {GroupNodeType} from '../types';
 
 import styles from './group-node.module.scss';
 
-function GroupNode({selected, data}: NodeProps<GroupNodeType>) {
-  const readOnly = useSketchLabReadOnly();
-  const showResizer = selected && !readOnly && !data.locked;
-  return (
-    <div className={styles.groupNode} aria-label="Group">
-      {showResizer && (
-        <NodeResizer minWidth={MIN_NODE_WIDTH} minHeight={MIN_NODE_HEIGHT} />
-      )}
-    </div>
-  );
+function GroupNode(_props: NodeProps<GroupNodeType>) {
+  return <div className={styles.groupNode} aria-label="Group" />;
 }
 
 export default memo(GroupNode);
