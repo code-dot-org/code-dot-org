dbf0c5bbd61952391429f6af4221de84b9b49239
Mark Barnes <mark.barnes@code.org>
2026-06-09 13:28:04 -0500

    update colors and fontawesome icons

--- apps/src/templates/rubrics/RubricContent.jsx
+++ apps/src/templates/rubrics/RubricContent.jsx
@@ -1,9 +1,9 @@
+import FontAwesomeV6Icon from '@code-dot-org/component-library/fontAwesomeV6Icon';
 import {Typography} from '@mui/material';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React, {useState} from 'react';
 
-import FontAwesome from '@cdo/apps/legacySharedComponents/FontAwesome';
 import i18n from '@cdo/locale';
 
 import LearningGoals from './LearningGoals';
@@ -96,7 +96,7 @@ export default function RubricContent({
                       variant="body3"
                       gutterBottom
                     >
-                      <FontAwesome icon="clock" />
+                      <FontAwesomeV6Icon iconName="clock" />
                       <span>{formatTimeSpent(studentLevelInfo.timeSpent)}</span>
                     </Typography>
                   )}
@@ -105,7 +105,7 @@ export default function RubricContent({
                     variant="body3"
                     gutterBottom
                   >
-                    <FontAwesome icon="rocket" />
+                    <FontAwesomeV6Icon iconName="rocket" />
                     {i18n.numAttempts({
                       numAttempts: studentLevelInfo.attempts || 0,
                     })}
@@ -116,7 +116,7 @@ export default function RubricContent({
                       variant="body3"
                       gutterBottom
                     >
-                      <FontAwesome icon="calendar" />
+                      <FontAwesomeV6Icon iconName="calendar" />
                       <span>
                         {formatLastAttempt(studentLevelInfo.lastAttempt)}
                       </span>
@@ -190,8 +190,8 @@ export const InfoAlert = ({text, dismissable}) => {
       data-testid="info-alert"
     >
       <div className={style.infoAlertLeft}>
-        <FontAwesome
-          icon="circle-info"
+        <FontAwesomeV6Icon
+          iconName="circle-info"
           className={style.infoAlertIcon}
           title="info circle icon"
         />
