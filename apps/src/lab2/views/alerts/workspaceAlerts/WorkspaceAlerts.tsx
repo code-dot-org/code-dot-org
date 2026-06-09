244b126a45f05fd84d03cf02fc889452c628db99
Molly Moen <molly@code.org>
2026-06-09 09:37:49 -0700

    refactor and adjust alert placement

---
+++ apps/src/lab2/views/alerts/workspaceAlerts/WorkspaceAlerts.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import {useAppSelector} from '@cdo/apps/util/reduxHooks';
+
+import PreviousVersionAlert from '../previousVersion';
+import TeacherViewingStudentProjectAlert from '../teacherViewingStudentProject';
+
+type WorkspaceAlertsProps = {
+  /** Is the alert displayed within the workspace area */
+  inWorkspaceContainer?: boolean;
+};
+
+// Bundles the alerts shown at the top of a lab2 workspace: the
+// teacher-viewing-student banner (gated on viewing a student's project) and
+// the previous-version banner (self-gating).
+const WorkspaceAlerts: React.FC<WorkspaceAlertsProps> = ({
+  inWorkspaceContainer,
+}) => {
+  const teacherViewingStudent = Boolean(
+    useAppSelector(state => state.progress.viewAsUserId)
+  );
+
+  return (
+    <>
+      {teacherViewingStudent && (
+        <TeacherViewingStudentProjectAlert
+          inWorkspaceContainer={inWorkspaceContainer}
+        />
+      )}
+      <PreviousVersionAlert />
+    </>
+  );
+};
+
+export default WorkspaceAlerts;
