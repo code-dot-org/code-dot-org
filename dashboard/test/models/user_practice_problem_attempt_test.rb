701085ad81c6240fb40be7b44f227cdacc4bd11a
Ceara Chewning <ceara.chewning@code.org>
2026-06-11 18:05:00 -0400

    Add model unit tests for user practice problem attempts

--- dashboard/test/models/user_practice_problem_attempt_test.rb
+++ dashboard/test/models/user_practice_problem_attempt_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class UserPracticeProblemAttemptTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:student)
+    @practice_problem = create(:practice_problem)
+    @attempt = create(:user_practice_problem_attempt, user: @user, practice_problem: @practice_problem)
+  end
+
+  test 'attempt is accessible through the user record' do
+    assert_includes @user.user_practice_problem_attempts, @attempt
+  end
+
+  test 'associated practice problem is accessible through the attempt record' do
+    assert_equal @practice_problem, @attempt.practice_problem
+  end
 end
