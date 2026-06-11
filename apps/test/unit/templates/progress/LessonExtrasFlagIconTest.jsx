b8923f53288aa65f98775beb208054e3a7b01b6a
denyslevada <levada.denys@gmail.com>
2026-06-11 20:17:55 +0300

    refactor: replace input elements with RadioButton component in StudentRowTest for improved accessibility

--- apps/test/unit/templates/progress/LessonExtrasFlagIconTest.jsx
+++ apps/test/unit/templates/progress/LessonExtrasFlagIconTest.jsx
@@ -7,7 +7,7 @@ describe('LessonExtrasFlagIcon', () => {
   it('has a grey flag icon when not selected, not perfect', () => {
     const wrapper = shallow(<LessonExtrasFlagIcon />);
     expect(wrapper.find('i').at(1).props().style.color).toEqual(
-      'var(--text-neutral-secondary)'
+      'var(--text-neutral-tertiary)'
     );
   });
 
