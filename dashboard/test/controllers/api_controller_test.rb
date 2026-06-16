d1253d6e9697e68c2981da725681cfed4c540ff6
Alice Fisher <alice.fisher@code.org>
2026-06-16 20:59:10 +0000

    crossing fingers

--- dashboard/test/controllers/api_controller_test.rb
+++ dashboard/test/controllers/api_controller_test.rb
@@ -1910,13 +1910,14 @@ class ApiControllerTest < ActionController::TestCase
 
   test 'sign_cookies uses unprefixed resource URL regardless of GE region' do
     expected_resource = CDO.studio_url('/restricted/*', ge_region: nil)
-    any_time = Object.new.tap {|o| o.define_singleton_method(:==) {|_| true}}
-    AWS::CloudFront.stubs(:signed_cookies).with(expected_resource, any_time).returns({})
     Cdo::GlobalEdition.stubs(:current_region).returns('es')
+    allow(AWS::CloudFront).to receive(:signed_cookies).and_return({})
 
     sign_out :user
     get :sign_cookies
     assert_response :success
+
+    expect(AWS::CloudFront).to have_received(:signed_cookies).with(expected_resource, anything)
   end
 
   describe '#unit_summary' do
