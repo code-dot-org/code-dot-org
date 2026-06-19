fe02cada94cc992f6c1af6f451e157edac641679
Alice Fisher <alice.fisher@code.org>
2026-06-19 16:50:55 +0000

    add console logs to debug

--- apps/src/lab2/hooks/useLoadLevelProperties.ts
+++ apps/src/lab2/hooks/useLoadLevelProperties.ts
@@ -25,6 +25,7 @@ async function loadLevelProperties(path: string) {
     undefined,
     LevelPropertiesMapValidator
   );
+  console.log('response.value', response.value);
   return response.value;
 }
 
@@ -55,9 +56,11 @@ export default function useLoadLevelProperties() {
 
   useEffect(() => {
     if (path) {
+      console.log('path', path);
       loadLevelProperties(path)
         .then(setPropertiesMap)
         .catch(error => {
+          console.log('error', error);
           dispatch(
             setPageError({
               errorMessage: 'Error loading level properties',
--- apps/src/lab2/responseValidators.ts
+++ apps/src/lab2/responseValidators.ts
@@ -97,6 +97,7 @@ export const LevelPropertiesMapValidator: ResponseValidator<
 
   for (const levelId of Object.keys(response)) {
     const properties = response[levelId] as Record<string, unknown>;
+    console.log('properties', properties);
     if (typeof properties !== 'object' || properties === null) {
       throw new Error(
         `Level properties should be an object (received ${typeof properties}).`
