6d1c50a7aacff3f76a385ea1d4ba710b466cf55a
Alice Fisher <alice.fisher@code.org>
2026-06-19 16:52:18 +0000

    skip games that have to app defined since these would be legacy labs

--- dashboard/app/models/lesson.rb
+++ dashboard/app/models/lesson.rb
@@ -740,9 +740,11 @@ class Lesson < ApplicationRecord
     properties = {}
     script_levels.each do |script_level|
       level = script_level.level
+      next unless level.game&.app
       properties[level.id] = level.summarize_for_lab2_properties(script, script_level, current_user, unit_group_unit: unit_group_unit)
       next unless level.is_a?(BubbleChoice)
       level.sublevels.each do |sublevel|
+        next unless sublevel.game&.app
         properties[sublevel.id] = sublevel.summarize_for_lab2_properties(script, script_level, current_user, unit_group_unit: unit_group_unit)
       end
     end
