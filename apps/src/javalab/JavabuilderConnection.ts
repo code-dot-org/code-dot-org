11ba28c1ae29c4a8e200c14c1907bce84054567a
Molly Moen <molly@code.org>
2026-06-22 16:01:17 -0700

    Java Lab2: theater part 1 (#73353)
    * claude first pass
    
    * fix build error
    
    * fix for run state
    
    * fix layout
    
    * sizing fix
    
    * test update
    
    * add theater level to allthethings
    
    * undo unintended change
    
    * update typing
    
    * fallback for prompter
    
    * clean ups
    
    * more clean ups
    
    * constant update
    
    * small tidys
    
    * fix for level change bug
    
    * type change
    
    * add generic mini app class
    
    * fix test

--- apps/src/javalab/JavabuilderConnection.ts
+++ apps/src/javalab/JavabuilderConnection.ts
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 
 import {ValidationResult} from '@cdo/apps/lab2/progress/ProgressManager';
+import MiniApp from '@cdo/apps/miniApps/MiniApp';
 import {ConsoleSignalType} from '@cdo/apps/miniApps/neighborhood/constants';
 import {SignInState} from '@cdo/apps/templates/currentUserRedux';
 import {CurrentUserState} from '@cdo/apps/templates/CurrentUserState';
@@ -27,13 +28,6 @@ const logToCloud = require('@cdo/apps/logToCloud');
 
 const WEBSOCKET_CLOSED_NORMAL_CODE = 1000;
 const SERVER_WAIT_TIME_MS = 10000;
-
-interface MiniApp {
-  isRunning(): boolean;
-  handleSignal(signal: {value: string; detail?: unknown} | null): void;
-  onCompile?(): void;
-  onClose?(): void;
-}
 
 // Shape of the JSON messages Javabuilder sends over the websocket. Not all
 // fields are used in every message.
@@ -173,7 +167,7 @@ export default class JavabuilderConnection {
       // so we use PARTIAL_LOG to avoid double newlines. Java Lab ignores
       // PARTIAL_LOG vs CONSOLE_LOG and will add newlines whenever it sees an explicit newline.
       this.onOutputMessage = message => {
-        if (this.miniApp?.isRunning()) {
+        if (this.miniApp?.isRunning?.()) {
           this.miniApp.handleSignal({
             value: ConsoleSignalType.PARTIAL_LOG,
             detail: message,
@@ -184,7 +178,7 @@ export default class JavabuilderConnection {
       };
 
       this.onNewlineMessage = () => {
-        if (this.miniApp?.isRunning()) {
+        if (this.miniApp?.isRunning?.()) {
           this.miniApp.handleSignal({
             value: ConsoleSignalType.PARTIAL_LOG,
             detail: '\n',
