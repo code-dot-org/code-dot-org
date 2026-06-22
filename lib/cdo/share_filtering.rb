a6e65ac64f111e26e357cd9fad4487053daf0235
Alice Fisher <alice.fisher@code.org>
2026-06-22 22:13:48 +0000

    remove commented code

--- lib/cdo/share_filtering.rb
+++ lib/cdo/share_filtering.rb
@@ -65,21 +65,6 @@ module ShareFiltering
 
     # Temporarily disable address filtering to prevent false positives.
     # TODO: re-enable this when we improve the user experience of the address filter.
-    # Address: check each text block independently to prevent cross-block bleeding.
-    # candidate = nil
-    # texts.each do |text|
-    #   candidate = Geocoder.extract_address_candidate(text)
-    #   break if candidate
-    # end
-    # if candidate
-    #   address = Geocoder.find_potential_street_address(candidate)
-    #   if address
-    #     failure = ShareFailure.new(FailureType::ADDRESS, address)
-    #     raise PIIFilterException.new("Address PII Filter Violation", failure) if exceptions
-    #     return failure
-    #   end
-    # end
-
     find_profanity_failure(program_text, locale, {}, exceptions: exceptions)
   end
 
