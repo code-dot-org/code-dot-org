6723d5131af462f5274653df566ac85ac38a2096
Hannah Bergam <hannah.bergam@code.org>
2026-06-24 11:03:14 -0700

    undo attempted bug fix for angle helper

--- apps/src/blockly/addons/cdoFieldNumber.ts
+++ apps/src/blockly/addons/cdoFieldNumber.ts
@@ -67,24 +67,9 @@ export default class CdoFieldNumber extends BlocklyCore.FieldNumber {
   }
 
   protected showEditor_(e?: Event, quietInput?: boolean): void {
-    const openEditor = () => {
-      super.showEditor_(e, quietInput);
-      if (this.shouldShowAngleHelper()) {
-        this.initializeAngleHelper();
-      }
-    };
-
-    try {
-      openEditor();
-    } catch (error) {
-      if (
-        error instanceof Error &&
-        error.message.includes('FocusManager state changes cannot happen')
-      ) {
-        setTimeout(openEditor, 0);
-      } else {
-        throw error;
-      }
+    super.showEditor_(e, quietInput);
+    if (this.shouldShowAngleHelper()) {
+      this.initializeAngleHelper();
     }
   }
 
