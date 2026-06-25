53e19bfa75028f3e3762fb09de68b03d93e452c5
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-06-25 21:26:48 +0300

    chore(shared): remove legacy shared/js support (#73473)

--- dashboard/config/application.rb
+++ dashboard/config/application.rb
@@ -153,7 +153,6 @@ module Dashboard
     config.assets.gzip = false # cloudfront gzips everything for us on the fly.
     config.assets.paths << Rails.root.join('./public/blockly')
     config.assets.paths << Rails.root.join('../shared/css')
-    config.assets.paths << Rails.root.join('../shared/js')
 
     # Whether to fallback to assets pipeline if a precompiled asset is missed.
     config.assets.compile = !CDO.optimize_rails_assets
