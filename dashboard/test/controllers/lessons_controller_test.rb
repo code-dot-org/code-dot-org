a6e447a02b402b847b0365a7f2ee9327f4d22e42
Alice Fisher <alice.fisher@code.org>
2026-06-26 14:43:54 +0000

    revert tests no longer needed

--- dashboard/test/controllers/lessons_controller_test.rb
+++ dashboard/test/controllers/lessons_controller_test.rb
@@ -257,49 +257,6 @@ class LessonsControllerTest < ActionController::TestCase
 
   test_user_gets_response_for :level_properties_by_id, user: nil, response: :success, params: -> {{id: @lesson2.id}}, name: 'anyone can fetch lesson level properties by ID on a lesson without a lesson plan'
 
-  test 'level_properties in teacher view ignores user_id when user is student of teacher' do
-    teacher = create(:teacher)
-    student = create(:student)
-    section = create(:section, user: teacher, script: @script)
-    create(:follower, section: section, student_user: student)
-    level = create(:music)
-    create(:script_level, script: @script, lesson: @lesson, levels: [level])
-
-    sign_in teacher
-    get :level_properties, params: {
-      course_course_name: @course.name,
-      unit_position: 1,
-      lesson_position: @lesson.relative_position,
-      user_id: student.id
-    }
-    assert_response :success
-
-    level_properties = JSON.parse(response.body)[level.id.to_s]
-    assert_nil level_properties['isNavigator']
-    assert_nil level_properties['pairingDriver']
-    assert_nil level_properties['pairingAttempt']
-  end
-
-  test 'level_properties in teacher view ignores user_id when user is not teacher student' do
-    teacher = create(:teacher)
-    other_user = create(:student)
-    level = create(:music)
-    create(:script_level, script: @script, lesson: @lesson, levels: [level])
-
-    sign_in teacher
-    get :level_properties, params: {
-      course_course_name: @course.name,
-      unit_position: 1,
-      lesson_position: @lesson.relative_position,
-      user_id: other_user.id
-    }
-    assert_response :success
-
-    level_properties = JSON.parse(response.body)[level.id.to_s]
-    assert_nil level_properties['isNavigator']
-    assert_nil level_properties['pairingDriver']
-  end
-
   test 'show includes correct SEO data' do
     get :show, params: {
       course_course_name: @course.name,
