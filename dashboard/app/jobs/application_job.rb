167f2875566d986e958e2ff4578fe6e9ae0a3a91
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-07-07 20:56:33 +0300

    Update dashboard/app/jobs/application_job.rb
    Co-authored-by: suresh <suresh@code.org>

--- dashboard/app/jobs/application_job.rb
+++ dashboard/app/jobs/application_job.rb
@@ -8,6 +8,7 @@ class ApplicationJob < ActiveJob::Base
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
 
+  # Ensure that read-only controller actions using the `:reading` ActiveRecord Role can still write to the database to enqueue jobs.
   around_enqueue do |_job, block|
     ActiveRecord::Base.connected_to(role: :writing, &block)
   end
