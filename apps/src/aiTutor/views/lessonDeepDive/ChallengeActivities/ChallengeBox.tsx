2024066fdda67b61e7948ffbd8380275cb8a75e1
Ceara Chewning <ceara.chewning@code.org>
2026-07-10 13:29:05 -0400

    Fetch challenges for this lesson and display the first challenge (for now) if available

--- apps/src/aiTutor/views/lessonDeepDive/ChallengeActivities/ChallengeBox.tsx
+++ apps/src/aiTutor/views/lessonDeepDive/ChallengeActivities/ChallengeBox.tsx
@@ -1,12 +1,49 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useCallback, useEffect, useState} from 'react';
+
+import HttpClient from '@cdo/apps/util/HttpClient';
+
+import {Challenge, challengeValidator} from '../types';
 
 import VideoChallenge from './VideoChallenge';
 
-const ChallengeBox: FC = () => {
+interface ChallengeBoxProps {
+  lessonId: number;
+  lessonName: string;
+}
+
+const ChallengeBox: FC<ChallengeBoxProps> = ({lessonId, lessonName}) => {
   const [submitted, setSubmitted] = useState(false);
+  const [selectedChallenge, setSelectedChallenge] = useState<null | Challenge>(
+    null
+  );
+
+  const fetchChallenges = useCallback(() => {
+    const params = new URLSearchParams();
+    params.append('lesson_id', lessonId.toString());
+    const query = params.toString();
+    HttpClient.fetchJson<Challenge[]>(
+      `/challenges?${query}`,
+      {},
+      challengeValidator
+    ).then(response => {
+      const challenges = response.value;
+      if (!challenges || challenges.length === 0) return;
+      //for now, just show the first challenge question
+      setSelectedChallenge(challenges[0]);
+    });
+  }, [lessonId]);
+
+  useEffect(() => {
+    fetchChallenges();
+  }, [fetchChallenges]);
+
   return (
     <div>
-      <VideoChallenge submitted={submitted} submitCallback={setSubmitted} />
+      <VideoChallenge
+        submitted={submitted}
+        submitCallback={setSubmitted}
+        challenge={selectedChallenge}
+      />
     </div>
   );
 };
