f20c973476bc519276c9a326ff392cd9453eb93f
David Bailey <davidsbailey@users.noreply.github.com>
2026-07-15 22:42:55 +0000

    improve naming in callouts.cases.ts

--- frontend/packages/e2e-tests/tests/activities/callouts.cases.ts
+++ frontend/packages/e2e-tests/tests/activities/callouts.cases.ts
@@ -21,7 +21,7 @@ export interface CalloutCloseButtonTestCase {
 export const CALLOUT_TARGET_TEST_CASES: CalloutTargetTestCase[] = [
   {
     title:
-      'callout 0 (run button) has correct content and is dismissable via run button on maze l2/7',
+      'callout 0 (run button) has correct content and is dismissable via run button on maze level 7',
     url: labLevelUrl({lesson: 2, level: 7, showCallouts: true}),
     calloutId: 0,
     text: 'After snapping all the blocks together, press "Run" to start your program.',
@@ -29,7 +29,7 @@ export const CALLOUT_TARGET_TEST_CASES: CalloutTargetTestCase[] = [
   },
   {
     title:
-      'callout 1 (show-code) has correct content and is dismissable via show-code-header on maze l2/7',
+      'callout 1 (show-code) has correct content and is dismissable via show-code-header on maze level 7',
     url: labLevelUrl({lesson: 2, level: 7, showCallouts: true}),
     calloutId: 1,
     text: "Click here to see the code for the program you're making",
@@ -37,7 +37,7 @@ export const CALLOUT_TARGET_TEST_CASES: CalloutTargetTestCase[] = [
   },
   {
     title:
-      'callout 1 (run button) has correct content and is dismissable via run button on ui-test-csf l1/4',
+      'callout 1 (run button) has correct content and is dismissable via run button on ui-test-csf level 4',
     url: labLevelUrl({course: 'ui-test-csf', lesson: 1, level: 4}),
     calloutId: 1,
     text: 'Hit "Run" to try your program',
@@ -45,7 +45,7 @@ export const CALLOUT_TARGET_TEST_CASES: CalloutTargetTestCase[] = [
   },
   {
     title:
-      'callout 0 (moveForward) has correct content and is dismissable via moveForward block on ui-test-csf l1/4',
+      'callout 0 (moveForward) has correct content and is dismissable via moveForward block on ui-test-csf level 4',
     url: labLevelUrl({course: 'ui-test-csf', lesson: 1, level: 4}),
     calloutId: 0,
     text: 'Drag a "move" block and snap it below the other block',
@@ -53,7 +53,7 @@ export const CALLOUT_TARGET_TEST_CASES: CalloutTargetTestCase[] = [
   },
   {
     title:
-      'callout 0 (grey block) has correct content and is dismissable via g element on ui-test-csf l1/7',
+      'callout 0 (grey block) has correct content and is dismissable via g element on ui-test-csf level 7',
     url: labLevelUrl({course: 'ui-test-csf', lesson: 1, level: 7}),
     calloutId: 0,
     text: "Blocks that are grey can't be deleted. Can you solve the puzzle anyway?",
@@ -64,14 +64,14 @@ export const CALLOUT_TARGET_TEST_CASES: CalloutTargetTestCase[] = [
 export const CALLOUT_CLOSE_BUTTON_TEST_CASES: CalloutCloseButtonTestCase[] = [
   {
     title:
-      'callout 0 has correct content and is dismissable via x-button on ui-test-csf l1/6',
+      'callout 0 has correct content and is dismissable via x-button on ui-test-csf level 6',
     url: labLevelUrl({course: 'ui-test-csf', lesson: 1, level: 6}),
     calloutId: 0,
     text: 'Click here to watch the video again',
   },
   {
     title:
-      'callout 0 has correct content and is dismissable via x-button on artist l3/7',
+      'callout 0 has correct content and is dismissable via x-button on artist level 7',
     url: labLevelUrl({lesson: 3, level: 7, showCallouts: true}),
     calloutId: 0,
     text: "You have all the same blocks but they've now been arranged in categories",
