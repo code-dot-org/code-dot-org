021acb1ba3d66d38ac5108c62c4d3ab967812127
Eric Aderhold <eric.aderhold@code.org>
2026-07-22 16:58:58 -0700

    lint

--- apps/test/unit/aiTeacherDrawer/PrepareListTest.tsx
+++ apps/test/unit/aiTeacherDrawer/PrepareListTest.tsx
@@ -1,5 +1,4 @@
-import {act, render, screen, waitFor} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import {act, render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
 
