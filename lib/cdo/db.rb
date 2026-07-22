6ea835d6d77b8ae53efe04fb3b5b00cb1ad891ba
Carl Reiner <carl.reiner@code.org>
2026-07-22 11:45:25 -0700

    k8s: clean up backend profile implementation

--- lib/cdo/db.rb
+++ lib/cdo/db.rb
@@ -12,11 +12,4 @@ Sequel::Model.db = PEGASUS_DB
 PEGASUS_DB.singleton_class.prepend StaticModels
 # rubocop:enable CustomCops/PegasusDbUsage
 
-# rubocop:disable CustomCops/PegasusDbUsage
-PEGASUS_DB = Cdo::Sequel.database_connection_pool CDO.pegasus_db_writer, CDO.pegasus_db_reader
-POSTE_DB = PEGASUS_DB
-# Use Pegasus as the default database for Sequel Models.
-Sequel::Model.db = PEGASUS_DB
-# rubocop:enable CustomCops/PegasusDbUsage
-
 DASHBOARD_DB = Cdo::Sequel.database_connection_pool CDO.dashboard_db_writer, CDO.dashboard_db_reader
