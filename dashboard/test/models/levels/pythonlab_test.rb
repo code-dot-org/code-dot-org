8fcb71c508009c14bdf49ad60c3b96298fa5573b
Molly Moen <molly@code.org>
2026-07-23 10:26:30 -0700

    theater plumbing + level

---
+++ dashboard/test/models/levels/pythonlab_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class PythonlabTest < ActiveSupport::TestCase
+  test 'theater is an available mini app' do
+    values = Pythonlab.mini_apps.map(&:last)
+    assert_includes values, 'theater'
+    assert_includes values, 'neighborhood'
+  end
+
+  test 'theater level does not require a serialized maze' do
+    level = Pythonlab.new(name: 'theater level', mini_app: 'theater')
+    # parse_maze raises only for a neighborhood level missing its maze.
+    assert_nothing_raised do
+      level.send(:parse_maze)
+    end
+  end
+
+  test 'neighborhood level still requires a serialized maze' do
+    level = Pythonlab.new(name: 'neighborhood level', mini_app: 'neighborhood')
+    assert_raises ArgumentError do
+      level.send(:parse_maze)
+    end
+  end
+
+  test 'a stray serialized maze is stripped from a theater level' do
+    level = Pythonlab.new(name: 'theater level', mini_app: 'theater')
+    level.properties['serialized_maze'] = [[{'tileType' => 1}]]
+    level.send(:clean_up_mini_app_settings)
+    assert_nil level.properties['serialized_maze']
+  end
+end
