88e981aca8a7d1496b7f7a6279ece0f3851da916
Ben Brooks <ben@code.org>
2026-07-28 12:04:41 -0700

    Move image URL input to standard input

--- apps/src/code-studio/components/ImageURLInput.jsx
+++ apps/src/code-studio/components/ImageURLInput.jsx
@@ -1,4 +1,5 @@
-import {Typography as MuiTypography} from '@mui/material';
+import TextField from '@code-dot-org/component-library/textField';
+import {Button as MuiButton, Typography as MuiTypography} from '@mui/material';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -6,7 +7,6 @@ import React from 'react';
 import {ABSOLUTE_REGEXP} from '@cdo/apps/assetManagement/assetPrefix';
 import {EVENTS} from '@cdo/apps/metrics/AnalyticsConstants';
 import analyticsReporter from '@cdo/apps/metrics/AnalyticsReporter';
-import InputPrompt from '@cdo/apps/templates/InputPrompt';
 import i18n from '@cdo/locale';
 
 export default class ImageURLInput extends React.Component {
@@ -15,7 +15,22 @@ export default class ImageURLInput extends React.Component {
     allowedExtensions: PropTypes.string,
     currentValue: PropTypes.string,
   };
-  state = {showError: false};
+  state = {showError: false, value: this.props.currentValue || ''};
+
+  inputRef = React.createRef();
+
+  componentDidMount() {
+    this.inputRef.current?.focus();
+  }
+
+  handleChange = event => {
+    this.setState({value: event.target.value});
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.handleSubmitWrapper(this.state.value);
+  };
 
   handleSubmitWrapper = url => {
     if (ABSOLUTE_REGEXP.test(url)) {
@@ -37,11 +52,28 @@ export default class ImageURLInput extends React.Component {
         >
           {i18n.imageURLInputDescription()}
         </MuiTypography>
-        <InputPrompt
-          question={i18n.imageURLInputPrompt()}
-          onInputReceived={this.handleSubmitWrapper}
-          currentValue={this.props.currentValue}
-        />
+        <form
+          onSubmit={this.handleSubmit}
+          style={{display: 'flex', flexDirection: 'column', gap: 8}}
+        >
+          <TextField
+            ref={this.inputRef}
+            name="imageUrl"
+            label={i18n.imageURLInputPrompt()}
+            value={this.state.value}
+            onChange={this.handleChange}
+            style={{width: '100%'}}
+          />
+          <MuiButton
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="small"
+            sx={{alignSelf: 'flex-start'}}
+          >
+            {i18n.submit()}
+          </MuiButton>
+        </form>
         {this.state.showError && (
           <MuiTypography
             variant="body2"
