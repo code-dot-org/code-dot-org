6e50a53ce31192b58a7d28a7be5efbd8bde823bf
wilkie <wilkie05@gmail.com>
2026-07-30 14:04:07 -0700

    Replaces CraftDialog with a CustomDialog styled appropriately.
    The only consumer of a CraftDialog is the PlayerSelectionDialog.

--- apps/src/craft/PlayerSelectionDialog.jsx
+++ apps/src/craft/PlayerSelectionDialog.jsx
@@ -1,16 +1,31 @@
+import {
+  Box,
+  Button as MuiButton,
+  Typography as MuiTypography,
+} from '@mui/material';
 import PropTypes from 'prop-types';
 import React from 'react';
 import {connect} from 'react-redux';
 
+import alexImg from '../../static/craft/Sliced_Parts/Alex_Character_Select.png';
+import steveImg from '../../static/craft/Sliced_Parts/Steve_Character_Select.png';
+
 import CraftDialog from './CraftDialog';
 import i18n from './locale';
 
+const characterSelectImg = {
+  steve: {
+    src: steveImg,
+    alt: '',
+  },
+  alex: {
+    src: alexImg,
+    alt: '',
+  },
+};
+
 /**
  * Dialog for selecting a player in Minecraft tutorials.
- *
- * Note: This component relies on styles defined in apps/style/craft/style.scss.
- * Example: #steve-portrait renders Steve's image, so any players will require
- * a similar CSS selector to render properly.
  */
 function PlayerSelectionDialog({
   isOpen,
@@ -23,16 +38,36 @@ function PlayerSelectionDialog({
   function renderPlayer(name) {
     const formattedName = name.toLowerCase();
     return (
-      <div key={formattedName} className="minecraft-character">
-        <h1 className="minecraft-big-yellow-header">{name}</h1>
-        <div className="character-portrait" id={`${formattedName}-portrait`} />
-        <div
-          className="choose-character-button"
+      <Box
+        key={formattedName}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <MuiTypography component="h1" variant="h3" sx={{color: '#f4d00b'}}>
+          {name}
+        </MuiTypography>
+        <Box>
+          <img
+            alt={characterSelectImg[formattedName].alt}
+            src={characterSelectImg[formattedName].src}
+            id={`${formattedName}-portrait`}
+            style={{maxHeight: 270}}
+          />
+        </Box>
+        <MuiButton
+          variant="contained"
+          color="secondary"
+          size="medium"
           onClick={() => handlePlayerSelection(name)}
         >
           {i18n.selectChooseButton()}
-        </div>
-      </div>
+        </MuiButton>
+      </Box>
     );
   }
 
@@ -41,14 +76,36 @@ function PlayerSelectionDialog({
       id="craft-popup-player-selection"
       isOpen={isOpen}
       handleClose={() => handlePlayerSelection(undefined)}
+      style={{
+        minWidth: '50%',
+      }}
     >
-      <h1 className={titleClassName} id="getting-started-header">
+      <MuiTypography
+        component="h1"
+        variant="h3"
+        id="getting-started-header"
+        sx={{color: '#f4d00b'}}
+      >
         {title}
-      </h1>
+      </MuiTypography>
       {!hideSubtitle && (
-        <h2 id="select-character-text">{i18n.playerSelectChooseCharacter()}</h2>
+        <MuiTypography component="h2" variant="h4" id="select-character-text">
+          {i18n.playerSelectChooseCharacter()}
+        </MuiTypography>
       )}
-      <div id="choose-character-container">{players.map(renderPlayer)}</div>
+      <Box
+        id="choose-character-container"
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 1,
+          alignItems: 'stretch',
+          justifyContent: 'space-around',
+          mt: 2,
+        }}
+      >
+        {players.map(renderPlayer)}
+      </Box>
     </CraftDialog>
   );
 }
