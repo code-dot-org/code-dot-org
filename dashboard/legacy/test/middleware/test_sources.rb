8595e998c28ec8262b41f23d354822bb29f15fff
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-07-31 00:10:10 +0300

    SL-1950: Add logging to get_storage_id method (#74272)

--- dashboard/legacy/test/middleware/test_sources.rb
+++ dashboard/legacy/test/middleware/test_sources.rb
@@ -329,8 +329,6 @@ class SourcesTest < FilesApiTestBase
   end
 
   def test_replace_version
-    CDO.expects(:log).never
-
     # Upload a source file.
     filename = MAIN_JSON
     file_data = '{"source":"version 1"}'
@@ -380,6 +378,7 @@ class SourcesTest < FilesApiTestBase
 
     Timecop.travel 1
 
+    CDO.log.stubs(:info)
     # log when replacing non-current version.
     CDO.log.expects(:info).with do |data|
       data = JSON.parse(data)
