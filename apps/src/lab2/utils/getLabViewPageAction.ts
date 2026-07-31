a640cc29503d5f60f70cc898312b12b7e859c48e
Molly Moen <molly@code.org>
2026-07-31 08:35:34 -0700

    fix for project blocked ui bug

--- apps/src/lab2/utils/getLabViewPageAction.ts
+++ apps/src/lab2/utils/getLabViewPageAction.ts
@@ -1,5 +1,6 @@
 // Returns the page action from the project URL, e.g., 'share', 'view', or 'edit'.
-// or 'level' if the URL is a level URL, e.g., '/courses/allthethingscourse/units/1/lessons/51/levels/1'.
+// or 'level' if the URL is a level URL, e.g., '/courses/allthethingscourse/units/1/lessons/51/levels/1'
+// or /levels/12345
 // For example, if the URL is '/projects/pythonlab/abc123/view', returns 'view'.
 // If the URL is '/projects/pythonlab/abc123', returns 'share'.
 // If the URL is not neither a project nor level URL, returns undefined.
@@ -14,10 +15,11 @@ export const getLabViewPageAction = (): string | undefined => {
   const [, first_, , third, , fifth, , seventh] =
     window.location.pathname.split('/');
   if (
-    first_ === 'courses' &&
-    third === 'units' &&
-    fifth === 'lessons' &&
-    seventh === 'levels'
+    (first_ === 'courses' &&
+      third === 'units' &&
+      fifth === 'lessons' &&
+      seventh === 'levels') ||
+    first_ === 'levels'
   ) {
     return 'level';
   }
