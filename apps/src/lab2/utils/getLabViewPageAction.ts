197a4ceb313ee8928aaae9a3c3d9dcc5495d0e5f
Molly Moen <molly@code.org>
2026-07-31 08:39:14 -0700

    update formatting

--- apps/src/lab2/utils/getLabViewPageAction.ts
+++ apps/src/lab2/utils/getLabViewPageAction.ts
@@ -1,6 +1,6 @@
 // Returns the page action from the project URL, e.g., 'share', 'view', or 'edit'.
 // or 'level' if the URL is a level URL, e.g., '/courses/allthethingscourse/units/1/lessons/51/levels/1'
-// or /levels/12345
+// or '/levels/12345'
 // For example, if the URL is '/projects/pythonlab/abc123/view', returns 'view'.
 // If the URL is '/projects/pythonlab/abc123', returns 'share'.
 // If the URL is not neither a project nor level URL, returns undefined.
