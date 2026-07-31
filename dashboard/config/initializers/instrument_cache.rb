16160ed57687afc93c0d7b971b28227a85265210
Elijah Hamovitz <elijahhamovitz@gmail.com>
2026-07-30 18:40:42 -0700

    add some temporary instrumentation for the rails cache

---
+++ dashboard/config/initializers/instrument_cache.rb
@@ -0,0 +1,47 @@
+require 'objspace'
+
+# Track hit rate.
+ActiveSupport::Notifications.subscribe "cache_read.active_support" do |event|
+  Cdo::Metrics.put(
+    'Infrastructure',
+    'ActiveSupportCacheRead',
+    1,
+    {
+      Environment: CDO.rack_env,
+      Hit: event.payload[:hit],
+      Store: event.payload[:store]
+    }
+  )
+end
+
+# Track total amount of data being read out of the cache, mostly to get insight
+# into how difficult it would be to host this cache on a networked server (ie,
+# redis). We can't do this with ActiveSupport instrumentation, because none of
+# the available events let us inspect the returned value itself.
+#
+# Could alternatively use ObjectSpace.memsize_of
+#
+# TODO infra: remove this module once we've finished investigating our runaway
+# memory usage issues. The hit rate tracker above might be useful enough to
+# keep, but this has too much potential performance impact for long-term use.
+module Cdo
+  module LogCacheBytesRead
+    private def read_entry(key, **options)
+      result = super
+      Cdo::Metrics.put(
+        'Infrastructure',
+        'ActiveSupportCacheBytesRead',
+        result.bytesize,
+        {
+          Environment: CDO.rack_env,
+          Store: self.class.name
+        }
+      )
+      result
+    end
+  end
+end
+
+Rails.application.config.before_configuration do
+  Rails.cache.class.prepend(Cdo::LogCacheBytesRead) unless Rails.cache.class <= Cdo::LogCacheBytesRead
+end
