6d144be23dc270e8c8cdaa4c83c84a073a3fed6f
Elijah Hamovitz <elijahhamovitz@gmail.com>
2026-08-05 11:00:39 -0700

    only publish metrics from environments for which it'll be useful

--- dashboard/config/initializers/instrument_cache.rb
+++ dashboard/config/initializers/instrument_cache.rb
@@ -1,43 +1,45 @@
-# Count hits and misses per cache store.
-ActiveSupport::Notifications.subscribe "cache_read.active_support" do |event|
-  metric_name = "ActiveSupportCache#{event.payload[:hit] ? 'Hit' : 'Miss'}"
-  Cdo::Metrics.put(
-    'Infrastructure',
-    metric_name,
-    1,
-    {
-      Host: CDO.dashboard_hostname,
-      Store: event.payload[:store]
-    }
-  )
-end
+if CDO.rack_env?(:production, :adhoc) || CDO.test_system?
+  # Count hits and misses per cache store.
+  ActiveSupport::Notifications.subscribe "cache_read.active_support" do |event|
+    metric_name = "ActiveSupportCache#{event.payload[:hit] ? 'Hit' : 'Miss'}"
+    Cdo::Metrics.put(
+      'Infrastructure',
+      metric_name,
+      1,
+      {
+        Host: CDO.dashboard_hostname,
+        Store: event.payload[:store]
+      }
+    )
+  end
 
-# Track total amount of data being read out of each cache cache, mostly to get
-# insight into what it would take to host the Rails cache on a networked server
-# (ie, redis). We can't do this with ActiveSupport instrumentation, because
-# none of the available events let us inspect the returned value itself.
-#
-# TODO infra: remove this module once we've finished investigating our runaway
-# memory usage issues. The hit rate tracker above might be useful enough to
-# keep, but this is too case-specific and too much of a hack for long-term use.
-module Cdo
-  module LogCacheBytesRead
-    private def read_entry(key, **options)
-      result = super
-      Cdo::Metrics.put(
-        'Infrastructure',
-        'ActiveSupportCacheBytesRead',
-        result.try(:bytesize) || 0,
-        {
-          Host: CDO.dashboard_hostname,
-          Store: self.class.name
-        }
-      )
-      result
+  # Track total amount of data being read out of each cache cache, mostly to get
+  # insight into what it would take to host the Rails cache on a networked server
+  # (ie, redis). We can't do this with ActiveSupport instrumentation, because
+  # none of the available events let us inspect the returned value itself.
+  #
+  # TODO infra: remove this module once we've finished investigating our runaway
+  # memory usage issues. The hit rate tracker above might be useful enough to
+  # keep, but this is too case-specific and too much of a hack for long-term use.
+  module Cdo
+    module LogCacheBytesRead
+      private def read_entry(key, **options)
+        result = super
+        Cdo::Metrics.put(
+          'Infrastructure',
+          'ActiveSupportCacheBytesRead',
+          result.try(:bytesize) || 0,
+          {
+            Host: CDO.dashboard_hostname,
+            Store: self.class.name
+          }
+        )
+        result
+      end
     end
   end
-end
 
-Rails.application.config.before_configuration do
-  Rails.cache.class.prepend(Cdo::LogCacheBytesRead) unless Rails.cache.class <= Cdo::LogCacheBytesRead
+  Rails.application.config.before_configuration do
+    Rails.cache.class.prepend(Cdo::LogCacheBytesRead) unless Rails.cache.class <= Cdo::LogCacheBytesRead
+  end
 end
