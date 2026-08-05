bc5eaeda11ace339ef97821ce6db0ef7656b7fb2
denyslevada <levada.denys@gmail.com>
2026-08-05 21:11:50 +0300

    fix: keep course card rows aligned

--- apps/test/unit/templates/studioHomepages/SeeMoreCoursesTest.js
+++ apps/test/unit/templates/studioHomepages/SeeMoreCoursesTest.js
@@ -2,7 +2,6 @@ import {Button as MuiButton} from '@mui/material';
 import {shallow} from 'enzyme'; // eslint-disable-line no-restricted-imports
 import React from 'react';
 
-import ContentContainer from '@cdo/apps/templates/ContentContainer';
 import CourseCard from '@cdo/apps/templates/studioHomepages/CourseCard';
 import SeeMoreCourses from '@cdo/apps/templates/studioHomepages/SeeMoreCourses';
 
@@ -21,37 +20,13 @@ describe('SeeMoreCourses', () => {
     expect(wrapper.find(MuiButton).exists()).toBe(true);
     wrapper.find(MuiButton).simulate('click');
     expect(wrapper.find(MuiButton).exists()).toBe(false);
-    expect(
-      wrapper.containsMatchingElement(
-        <div>
-          <ContentContainer heading="" linkText="" link="" showLink={false}>
-            <div>
-              <CourseCard
-                title={courses[0].title}
-                description={courses[0].description}
-                link={courses[0].link}
-              />
-              <div
-                style={{
-                  width: 20,
-                  float: 'left',
-                  color: 'transparent',
-                }}
-              >
-                .
-              </div>
-            </div>
-            <div>
-              <CourseCard
-                title={courses[1].title}
-                description={courses[1].description}
-                link={courses[1].link}
-              />
-            </div>
-          </ContentContainer>
-        </div>
-      )
-    ).toBeTruthy();
+    const courseCards = wrapper.find(CourseCard);
+    expect(courseCards).toHaveLength(courses.length);
+    expect(courseCards.at(0).props()).toMatchObject({
+      title: courses[0].title,
+      description: courses[0].description,
+      link: courses[0].link,
+    });
   });
 
   it('shows PL CourseCards when clicked for PL Recent Courses area', () => {
@@ -61,38 +36,13 @@ describe('SeeMoreCourses', () => {
     expect(wrapper.find(MuiButton).exists()).toBe(true);
     wrapper.find(MuiButton).simulate('click');
     expect(wrapper.find(MuiButton).exists()).toBe(false);
-    expect(
-      wrapper.containsMatchingElement(
-        <div>
-          <ContentContainer heading="" linkText="" link="" showLink={false}>
-            <div>
-              <CourseCard
-                title={courses[0].title}
-                description={courses[0].description}
-                link={courses[0].link}
-                isProfessionalLearningCourse={true}
-              />
-              <div
-                style={{
-                  width: 20,
-                  float: 'left',
-                  color: 'transparent',
-                }}
-              >
-                .
-              </div>
-            </div>
-            <div>
-              <CourseCard
-                title={courses[1].title}
-                description={courses[1].description}
-                link={courses[1].link}
-                isProfessionalLearningCourse={true}
-              />
-            </div>
-          </ContentContainer>
-        </div>
-      )
-    ).toBeTruthy();
+    const courseCards = wrapper.find(CourseCard);
+    expect(courseCards).toHaveLength(courses.length);
+    expect(courseCards.at(0).props()).toMatchObject({
+      title: courses[0].title,
+      description: courses[0].description,
+      link: courses[0].link,
+      isProfessionalLearningCourse: true,
+    });
   });
 });
