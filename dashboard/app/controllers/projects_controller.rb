29b2c8118055f27ee0a0314638a5ae0f47ea61ac
Alice Fisher <alice.fisher@code.org>
2026-08-12 17:24:23 +0000

    validate subprojects for abuse before remixing

--- dashboard/app/controllers/projects_controller.rb
+++ dashboard/app/controllers/projects_controller.rb
@@ -526,22 +526,31 @@ class ProjectsController < ApplicationController
     return head :forbidden if Projects.in_restricted_share_mode(src_channel_id, project_type)
     return head :forbidden if abuse_score_blocks_remix?(src_channel_id)
 
-    new_channel_id = remix_project(src_channel_id, project_type)
-
     project = Projects.new(get_storage_id)
     src_project = project.get(src_channel_id)
 
+    # Validate BubbleChoice subprojects before remixing parent or children,
+    # so a forbidden subproject does not leave orphaned remix channels.
+    bubble_choice_subprojects = nil
+    if src_project["subprojects"] && @level.is_a?(BubbleChoice)
+      bubble_choice_subprojects = src_project["subprojects"].first(SharedConstants::BUBBLE_CHOICE_CUSTOM_MODE_MAX_SUBPROJECTS)
+      bubble_choice_subprojects.each do |entry|
+        subproject_src_channel_id = entry['channel_id']
+        subproject = project.get(subproject_src_channel_id)
+        subproject_type = subproject["projectType"]
+        return head :forbidden if Projects.in_restricted_share_mode(subproject_src_channel_id, subproject_type)
+        return head :forbidden if abuse_score_blocks_remix?(subproject_src_channel_id)
+      end
+    end
+
+    new_channel_id = remix_project(src_channel_id, project_type)
+
     if src_project["subprojects"]
-      if @level.is_a?(BubbleChoice)
-        # Only process a reasonable number of subprojects.
-        sub_projects = src_project["subprojects"].first(SharedConstants::BUBBLE_CHOICE_CUSTOM_MODE_MAX_SUBPROJECTS)
-        # Remix each subproject and update the parent channel.
-        new_subprojects = sub_projects.map do |entry|
+      if bubble_choice_subprojects
+        new_subprojects = bubble_choice_subprojects.map do |entry|
           subproject_src_channel_id = entry['channel_id']
           subproject = project.get(subproject_src_channel_id)
           subproject_type = subproject["projectType"]
-          return head :forbidden if Projects.in_restricted_share_mode(subproject_src_channel_id, subproject_type)
-          return head :forbidden if abuse_score_blocks_remix?(subproject_src_channel_id)
           new_subproject_channel_id = remix_project(subproject_src_channel_id, subproject_type, is_subproject: true)
           {level_id: entry['level_id'], channel_id: new_subproject_channel_id}
         end
@@ -552,7 +561,7 @@ class ProjectsController < ApplicationController
           request.ip
         )
       else
-        # Remove subprojects entirely for non-music_dance_ai project types.
+        # Remove subprojects entirely for non-BubbleChoice project types.
         value = project.get(new_channel_id)
         value.delete("subprojects")
         project.update(new_channel_id, value, request.ip)
