cd8096c28c33ea2d6eb592f49808781ba1f9b1be
Molly Moen <molly@code.org>
2026-08-14 13:26:12 -0700

    update capitalization

--- apps/src/weblab2/htmlPreview/PreviewConnectionError.tsx
+++ apps/src/weblab2/htmlPreview/PreviewConnectionError.tsx
@@ -13,7 +13,7 @@ const PreviewConnectionError = () => {
           Your browser may be blocking the setup of Web Lab. You may need to
           adjust your firewall settings. See the{' '}
           <Link href={IT_REQUIREMENTS_URL} size="s" external openInNewTab>
-            Technical requirements page
+            technical requirements page
           </Link>{' '}
           for which site(s) you need to unblock. If you need assistance, please
           reach out to support@code.org.
