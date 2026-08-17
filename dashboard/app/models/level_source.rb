ab2c13a2231a6645922d785dc7b7b4df3b7bf567
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-08-17 09:24:23 +0300

    refactor(level-source): remove cache

--- dashboard/app/models/level_source.rb
+++ dashboard/app/models/level_source.rb
@@ -47,31 +47,17 @@ class LevelSource < ApplicationRecord
   before_save :recompute_md5
 
   def recompute_md5
-    self.md5 = Digest::MD5.hexdigest(data)
+    self.md5 = self.class.compute_md5(data)
   end
 
-  def self.cache_key(level_id, md5)
-    "#{level_id}-#{md5}"
-  end
-
-  def self.find_or_create_for(level:, data:, md5: nil)
-    md5 ||= Digest::MD5.hexdigest(data)
-    LevelSource.where(level:, md5:).first_or_create do |ls|
-      ls.data = data
-    end
+  def self.compute_md5(data)
+    Digest::MD5.hexdigest(data)
   end
 
   def self.find_identical_or_create(level, data)
-    md5 = Digest::MD5.hexdigest(data)
-    level_source_cache_key = cache_key(level.id, md5)
-
-    if DCDO.get('level_source_cache_disabled', false)
-      Rails.cache.delete(level_source_cache_key)
-      find_or_create_for(level:, data:, md5:)
-    else
-      Rails.cache.fetch(level_source_cache_key) do
-        find_or_create_for(level:, data:, md5:)
-      end
+    md5 = compute_md5(data)
+    LevelSource.find_or_create_by(level:, md5:) do |level_source|
+      level_source.data = data
     end
   end
 
