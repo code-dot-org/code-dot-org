753e81dc5880e90a929d3752f2b2846621504ee1
Molly Moen <molly@code.org>
2026-08-19 12:52:19 -0700

    bump version

--- apps/src/pythonlab/pyodideWebWorker.ts
+++ apps/src/pythonlab/pyodideWebWorker.ts
@@ -217,7 +217,7 @@ async function loadPackages() {
       `/blockly/js/pyodide/${version}/unittest_runner-0.3.0-py3-none-any.whl`,
       `/blockly/js/pyodide/${version}/pythonlab_setup-0.3.0-py3-none-any.whl`,
       `/blockly/js/pyodide/${version}/neighborhood-0.5.0-py3-none-any.whl`,
-      `/blockly/js/pyodide/${version}/theater-0.1.0-py3-none-any.whl`,
+      `/blockly/js/pyodide/${version}/theater-0.2.0-py3-none-any.whl`,
     ],
     {
       errorCallback: (message: string) => {
