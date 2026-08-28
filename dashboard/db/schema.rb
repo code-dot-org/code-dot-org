e5fcf289f729f0878b6df65dff86b98ca19728f2
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-08-28 11:31:28 +0300

    Revert "DATA-316: Add user_id to anonymous_level_progresses"
    This reverts commit 218815242ff86d970d284048c495ce6f372f8308.

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -202,7 +202,6 @@ ActiveRecord::Schema[7.0].define(version: 2026_08_27_094239) do
     t.integer "level_id", null: false
     t.integer "unit_group_id"
     t.bigint "level_source_id", unsigned: true
-    t.integer "user_id"
     t.integer "attempts", default: 0, null: false
     t.integer "best_result"
     t.boolean "submitted"
