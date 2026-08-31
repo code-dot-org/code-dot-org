71a9d34aa5775ebc547db0619fcd5b9bd7029a9c
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-08-31 20:32:57 +0300

    DATA-319: Rename `stable_id` to `anon_user_id`

--- dashboard/db/migrate/20260828084822_add_stable_id_to_sign_ins.rb
+++
@@ -1,5 +0,0 @@
-class AddStableIdToSignIns < ActiveRecord::Migration[7.0]
-  def change
-    add_column :sign_ins, :stable_id, :string
-  end
-end
--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_08_28_120000) do
+ActiveRecord::Schema[7.0].define(version: 2026_08_28_120001) do
   create_table "activities", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "level_id"
@@ -2502,7 +2502,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_08_28_120000) do
     t.integer "user_id", null: false
     t.datetime "sign_in_at", precision: nil, null: false
     t.integer "sign_in_count", null: false
-    t.string "stable_id"
+    t.string "anon_user_id"
     t.index ["sign_in_at"], name: "index_sign_ins_on_sign_in_at"
     t.index ["user_id"], name: "index_sign_ins_on_user_id"
   end
