c94cf50b83acd51c399caa90e94cb57508b0472f
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-09-03 12:56:44 +0300

    DATA-317: Capture `anon_user_id` for anonymous project storage (#75046)
    * DATA-317: Capture `anon_user_id` for anonymous project storage
    
    * DATA-317: Cleanup

---
+++ lib/cdo/anon_user_id.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'securerandom'
+
+module Cdo
+  module AnonUserId
+    # UUID V4 format
+    FORMAT = /\A[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i
+
+    def self.generate
+      SecureRandom.uuid
+    end
+
+    def self.valid?(value)
+      value.is_a?(String) && FORMAT.match?(value)
+    end
+
+    def self.valid_value(value)
+      value if valid?(value)
+    end
+  end
+end
