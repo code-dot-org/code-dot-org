45fb83be49f95408434b96648614462faa0c2216
Alice Fisher <alice.fisher@code.org>
2026-09-04 17:33:57 +0000

    add check for if quiz question-building

--- apps/src/quiz/Quiz.tsx
+++ apps/src/quiz/Quiz.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import {getAppOptionsIsBuildingQuizQuestions} from '@cdo/apps/lab2/projects/utils';
 import {LabProps} from '@cdo/apps/lab2/types';
 
-// Placeholder view - real quiz-taking UI isn't built yet.
+// Placeholder views - real building/taking UI isn't built yet.
 const Quiz: React.FunctionComponent<LabProps> = () => {
+  if (getAppOptionsIsBuildingQuizQuestions()) {
+    return <div>This is the quiz question-building page.</div>;
+  }
   return <div>This is a Quiz level.</div>;
 };
 
