dac73f7311c27e1a8bd57aafd0d0d3dc2597266c
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2026-09-04 07:12:08 -0500

    Add Quiz Level editing (#75101)
    * shorten comments in quiz_questions_controller
    
    * shorten comments in quiz_question_placements_controller
    
    * add level edit
    
    * override clone_with_suffix to clone placements also
    
    * add quiz level to lab2 showcase lesson
    
    * register quiz lab2 entry and add to registry
    
    * simplify quiz level edit for now

--- dashboard/test/models/levels/quiz_test.rb
+++ dashboard/test/models/levels/quiz_test.rb
@@ -105,4 +105,59 @@ class QuizTest < ActiveSupport::TestCase
     assert_nil attempt.level
     assert QuizQuestionResponse.exists?(response.id)
   end
+
+  test "clone_with_suffix copies placements and shares questions" do
+    quiz = create(:quiz)
+    first_question = create(:quiz_question)
+    second_question = create(:quiz_question)
+    create(:quiz_question_placement, level: quiz, quiz_question: first_question, page: 1, position: 1)
+    create(:quiz_question_placement, level: quiz, quiz_question: second_question, page: 2, position: 1)
+
+    copy = quiz.clone_with_suffix('_copy')
+
+    refute_equal quiz, copy
+    assert_equal [first_question, second_question], copy.questions
+    assert_equal [[1, 1], [2, 1]], (copy.placements.map {|p| [p.page, p.position]})
+    assert_equal 1, QuizQuestionPlacement.where(level: quiz, quiz_question: first_question).count
+    assert_equal 1, QuizQuestionPlacement.where(level: copy, quiz_question: first_question).count
+  end
+
+  test "clone_with_suffix returns an existing clone without duplicating placements" do
+    quiz = create(:quiz)
+    question = create(:quiz_question)
+    create(:quiz_question_placement, level: quiz, quiz_question: question)
+
+    first = quiz.clone_with_suffix('_copy')
+    second = quiz.clone_with_suffix('_copy')
+
+    assert_equal first, second
+    assert_equal 1, first.placements.count
+  end
+
+  test "clone_with_suffix with allow_existing false creates a new quiz with placements" do
+    quiz = create(:quiz)
+    question = create(:quiz_question)
+    create(:quiz_question_placement, level: quiz, quiz_question: question)
+
+    first = quiz.clone_with_suffix('_copy')
+    second = quiz.clone_with_suffix('_copy', allow_existing: false)
+
+    refute_equal first, second
+    assert_equal [question], second.questions
+  end
+
+  test "clone_with_suffix truncates long names before checking for an existing clone" do
+    old_name = 'x' * 67
+    suffix = '_long_suffix'
+    quiz = create(:quiz, name: old_name)
+    question = create(:quiz_question)
+    create(:quiz_question_placement, level: quiz, quiz_question: question)
+
+    first = quiz.clone_with_suffix(suffix)
+    second = quiz.clone_with_suffix(suffix)
+
+    assert_equal 70, first.name.length
+    assert_equal first, second
+    assert_equal 1, first.placements.count
+  end
 end
