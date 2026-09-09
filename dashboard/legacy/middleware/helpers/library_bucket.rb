e291daeacd77cff932fab05e7fcfd6eb5b289610
Alice Fisher <alice.fisher@code.org>
2026-09-09 18:33:51 +0000

    allow webp in library bucket

--- dashboard/legacy/middleware/helpers/library_bucket.rb
+++ dashboard/legacy/middleware/helpers/library_bucket.rb
@@ -8,8 +8,8 @@ class LibraryBucket < BucketHelper
 
   def allowed_file_types
     # The Library bucket is used by App Lab (json), Java Lab (java, csv, txt),
-    # Python Lab (py, csv, txt, json, wav), and Web Lab 2 (html, css, js, json, md, jpeg, jpg, png, gif).
-    %w(.json .java .py .csv .txt .js .html .css .md .jpeg .jpg .png .gif .wav)
+    # Python Lab (py, csv, txt, json, wav), and Web Lab 2 (html, css, js, json, md, jpeg, jpg, png, gif, webp).
+    %w(.json .java .py .csv .txt .js .html .css .md .jpeg .jpg .png .gif .wav .webp)
   end
 
   def cache_duration_seconds
