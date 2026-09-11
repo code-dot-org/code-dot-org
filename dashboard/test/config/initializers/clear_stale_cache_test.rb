8c6021bb443c3546c7814cc97a2a773a5f03b0f4
Elijah Hamovitz <elijahhamovitz@protonmail.com>
2026-09-10 17:06:18 -0700

    verify with unit test that repeated restarts work as expected

--- dashboard/test/config/initializers/clear_stale_cache_test.rb
+++ dashboard/test/config/initializers/clear_stale_cache_test.rb
@@ -1,26 +1,30 @@
 require 'test_helper'
 
 class ClearStaleCacheTest < ActiveSupport::TestCase
-  setup do
+  include Minitest::RSpecMocks
+
+  before do
     GitUtils.stubs(:git_revision).returns("first commit")
   end
 
-  test 'will not clear the cache on a fresh start or when restarting from the same commit' do
+  it 'will not clear the cache on a fresh start or when restarting from the same commit' do
     Rails.cache.with_local_cache do
-      Rails.cache.expects(:clear).never
+      expect(Rails.cache).not_to receive(:clear)
       Cdo::ClearStaleCache.call # fresh start
       Cdo::ClearStaleCache.call # restart with same commit
     end
   end
 
-  test 'will clear the cache when starting on a new commit' do
+  it 'will clear the cache when starting on a new commit' do
     Rails.cache.with_local_cache do
-      Rails.cache.expects(:clear).never
-      Cdo::ClearStaleCache.call # fresh start
+      Cdo::ClearStaleCache.call
 
-      GitUtils.stubs(:git_revision).returns("new commit")
-      Rails.cache.expects(:clear).once
-      Cdo::ClearStaleCache.call # restart with new commit
+      expect(Rails.cache).to receive(:clear).twice.and_call_original
+      GitUtils.stubs(:git_revision).returns("second commit")
+      Cdo::ClearStaleCache.call
+
+      GitUtils.stubs(:git_revision).returns("third commit")
+      Cdo::ClearStaleCache.call
     end
   end
 end
