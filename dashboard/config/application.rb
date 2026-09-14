6cf4835fda4f8c9704ee5d3c11e42cf503b3c624
Elijah Hamovitz <elijahhamovitz@protonmail.com>
2026-09-14 15:24:21 -0700

    Revert "Move All Remaining Sprockets-Managed Styles Into Apps (#75098)"
    This reverts commit 4a0ac226a3c0e78d262d4461af511cf99cadc7c1.

--- dashboard/config/application.rb
+++ dashboard/config/application.rb
@@ -161,6 +161,9 @@ module Dashboard
       emulate-print-media.js
       jquery.handsontable.full.js
       video-js/*.css
+      legacy-prerequisites.css
+      legacy-styles.css
+      brand-fonts.css
     )
 
     # Support including code from directories outside of the normal Rails directory
