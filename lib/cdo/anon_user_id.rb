af666ce215de8b0abec589333c8b4c1e9ad12250
Artem Vavilov <artem.vavilov.7@gmail.com>
2026-09-15 20:53:07 +0300

    DATA-314: Fix `Cdo::AnonUserId::FORMAT`

--- lib/cdo/anon_user_id.rb
+++ lib/cdo/anon_user_id.rb
@@ -4,8 +4,8 @@ require 'securerandom'
 
 module Cdo
   module AnonUserId
-    # UUID V4 format
-    FORMAT = /\A[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i
+    # RFC4122-compliant UUID format
+    FORMAT = /\A[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[0-9a-f]{4}-[0-9a-f]{12}\z/i
 
     def self.generate
       SecureRandom.uuid
