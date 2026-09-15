a4f77511750bebc08cd470cefb1ebd100884b883
Emily Eastlake <emily.eastlake@code.org>
2026-09-14 21:08:26 -0700

    Match legacy tag tooltip wrapper element

--- frontend/packages/component-library/src/tags/_Tag.tsx
+++ frontend/packages/component-library/src/tags/_Tag.tsx
@@ -78,7 +78,7 @@ const Tag: FunctionComponent<TagProps> = props => {
     (children: ReactElement) =>
       tooltipContent && tooltipId ? (
         <Tooltip id={tooltipId} title={tooltipContent} placement="top">
-          <span className={moduleStyles.tooltipAnchor}>{children}</span>
+          <div className={moduleStyles.tooltipAnchor}>{children}</div>
         </Tooltip>
       ) : (
         children
