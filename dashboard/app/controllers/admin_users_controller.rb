8ee9e6326a9bc82e67ff2accfbe13388b66037fe
Mark Barnes <mark.barnes@code.org>
2026-09-16 10:11:17 -0700

    update script request

--- dashboard/app/controllers/admin_users_controller.rb
+++ dashboard/app/controllers/admin_users_controller.rb
@@ -153,7 +153,7 @@ class AdminUsersController < ApplicationController
         @sections = @target_user.sections_instructed.includes(:students).order(:name)
         all_student_ids = @sections.flat_map {|s| s.students.map(&:id)}.uniq
         script_ids = UserScript.where(user_id: all_student_ids).distinct.pluck(:script_id)
-        @all_scripts = Unit.where(id: script_ids).order(:name)
+        @all_scripts = Unit.where(id: script_ids).order(:name).sort.uniq
       else
         @user_scripts = UserScript.
           where(user_id: @target_user.id).
