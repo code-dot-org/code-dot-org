526775aa1eb5abe9df5f6156f569c25866b82c31
Elijah Hamovitz <elijahhamovitz@protonmail.com>
2026-09-17 12:25:12 -0700

    reapply removal of override stubs

--- dashboard/test/test_helper.rb
+++ dashboard/test/test_helper.rb
@@ -59,11 +59,7 @@ I18n.load_path += Dir[Rails.root.join('test', 'en.yml')]
 I18n.backend.reload!
 I18n.fallbacks[:'te-ST'] = [:'te-ST', :'en-US', :en]
 
-Mocha::Mockery.setup # enable stubbing
-CDO.stubs(override_pegasus: nil)
-CDO.stubs(override_dashboard: nil)
-
-Dashboard::Application.routes.default_url_options = {protocol: 'https', host: CDO.dashboard_hostname, port: nil}
+Dashboard::Application.routes.default_url_options = {protocol: 'https', host: 'test-studio.code.org', port: nil}
 Dashboard::Application.config.action_mailer.default_url_options = Dashboard::Application.routes.default_url_options
 Devise.mailer.default_url_options = Dashboard::Application.routes.default_url_options
 
