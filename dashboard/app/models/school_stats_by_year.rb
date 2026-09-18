f5f24ef3a0496838a2a6c4226f77c5f2e9ebc9dc
Elijah Hamovitz <elijahhamovitz@protonmail.com>
2026-09-18 12:04:07 -0700

    update a few new classes to use Rails' new native composite primary keys

--- dashboard/app/models/school_stats_by_year.rb
+++ dashboard/app/models/school_stats_by_year.rb
@@ -117,7 +117,7 @@ class SchoolStatsByYear < ApplicationRecord
     ActiveRecord::Base.transaction do
       CSV.read(filename, **options).each do |row|
         parsed = yield row
-        loaded = find_by(primary_keys.map(&:to_sym).index_with {|k| parsed[k]})
+        loaded = find_by(primary_key.map(&:to_sym).index_with {|k| parsed[k]})
         if loaded.nil?
           begin
             SchoolStatsByYear.new(parsed).save!
