64aafee251e0ea520aeceba914341cbdffa4e0fe
Elijah Hamovitz <elijahhamovitz@protonmail.com>
2026-09-17 18:10:58 -0700

    Revert "Revert "Adaptive level: initial scaffolding (#75305)" (#75380)"
    This reverts commit bba30d746dee7d338651f73f6d7750e99cdceaa9.

--- dashboard/app/models/levels/level.rb
+++ dashboard/app/models/levels/level.rb
@@ -364,6 +364,7 @@ class Level < ApplicationRecord
   end
 
   TYPES_WITHOUT_IDEAL_LEVEL_SOURCE = [
+    'Adaptive', # no ideal solution
     'Aichat', # no ideal solution
     'Ailab', # no ideal solution
     'Applab', # freeplay
