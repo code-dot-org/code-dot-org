4b4c10f078576988648f38589e70efc44318d230
Elijah Hamovitz <elijahhamovitz@gmail.com>
2026-09-21 13:07:49 -0700

    remove redundant require
    Co-authored-by: Artem Vavilov <artem.vavilov.7@gmail.com>

--- dashboard/legacy/test/middleware/files_api_test_base.rb
+++ dashboard/legacy/test/middleware/files_api_test_base.rb
@@ -1,5 +1,4 @@
 require_relative 'middleware_test_helper' # Must be required first to establish load paths
-require 'mocha/minitest'
 require_relative '../../middleware/files_api'
 require_relative '../../middleware/channels_api'
 require 'cdo/aws/s3'
