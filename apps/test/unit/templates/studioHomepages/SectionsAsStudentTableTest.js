1c70f6417e9a202e35bcce9f7cfd9737bfc2eb97
Nick Lathe <nick.lathe@code.org>
2026-09-24 13:51:36 -0700

    Add ClassLink check to unit test

--- apps/test/unit/templates/studioHomepages/SectionsAsStudentTableTest.js
+++ apps/test/unit/templates/studioHomepages/SectionsAsStudentTableTest.js
@@ -52,7 +52,7 @@ describe('SectionsAsStudentTable', () => {
     const wrapper = wrapped(
       <SectionsAsStudentTable sections={joinedSections} canLeave={false} />
     );
-    expect(wrapper.find('.test-row')).toHaveLength(4);
+    expect(wrapper.find('.test-row')).toHaveLength(5);
     expect(wrapper.containsMatchingElement(<div>Current unit:</div>));
     joinedSections.forEach(section => {
       expect(
@@ -100,6 +100,7 @@ describe('SectionsAsStudentTable', () => {
     expect(wrapper.containsMatchingElement(<td>Google Classroom</td>));
     expect(wrapper.containsMatchingElement(<td>DoNotShowThis</td>)).toBe(false);
     expect(wrapper.containsMatchingElement(<td>Clever</td>));
+    expect(wrapper.containsMatchingElement(<td>ClassLink</td>));
     expect(wrapper.containsMatchingElement(<td>OrThisEither</td>)).toBe(false);
   });
 
